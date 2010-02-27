a8dd5e8d5e70dfecf0a04e23520cd7556e55c8c7
unknown <Ayende@.(none)>
2010-02-27 08:37:44 +0000

    Created a simpler API for starting a divan server

---
+++ Rhino.DivanDB.Server/DivanServer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Kayak;
+using Rhino.DivanDB.Server.Responders;
+
+namespace Rhino.DivanDB.Server
+{
+    public class DivanServer : IDisposable
+    {
+        private readonly DocumentDatabase database;
+        private readonly KayakServer server;
+
+        public DivanServer(string directory, int port)
+        {
+            database = new DocumentDatabase(directory);
+            database.SpinBackgroundWorkers();
+            server = new KayakServer
+            {
+                Responders = new List<IKayakResponder>
+                    (
+                    typeof(KayakResponder).Assembly.GetTypes()
+                        .Where(t => typeof(KayakResponder).IsAssignableFrom(t) && t.IsAbstract == false)
+                        .Select(t => (KayakResponder)Activator.CreateInstance(t))
+                        .Select(r =>
+                        {
+                            r.Database = database;
+                            return r as IKayakResponder;
+                        })
+                    )
+            };
+            server.Start(new IPEndPoint(IPAddress.Any, port));
+
+        }
+
+        public void Dispose()
+        {
+            server.Stop();
+            database.Dispose();
+        }
+    }
+}
--- Rhino.DivanDB.Server/Program.cs
+++ Rhino.DivanDB.Server/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using Kayak;
-using Rhino.DivanDB.Server.Responders;
-using System.Linq;
 
 namespace Rhino.DivanDB.Server
 {
@@ -10,30 +6,10 @@ namespace Rhino.DivanDB.Server
     {
         static void Main()
         {
-            using (var db = new DocumentDatabase("Db"))
+            using (new DivanServer("Db",8080))
             {
-                db.SpinBackgroundWorkers();
-
-                var server = new KayakServer
-                {
-                    Responders = new List<IKayakResponder>
-                    (
-                        typeof(KayakResponder).Assembly.GetTypes()
-                            .Where(t=>typeof(KayakResponder).IsAssignableFrom(t) && t.IsAbstract == false)
-                            .Select(t => (KayakResponder)Activator.CreateInstance(t))
-                            .Select(r =>
-                            {
-                                r.Database = db;
-                                return r as IKayakResponder;
-                            })
-                    )
-                };
-                server.Start();
-
                 Console.WriteLine("Ready to process requests...");
                 Console.ReadLine();
-
-                server.Stop();
             }
         }
     }
