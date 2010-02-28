3185d6cea1ea29216b2e23e7f4d92927a6ffae81
Ayende Rahien <ayende@ayende.com>
2010-02-28 23:43:26 +0000

    Adding /divan handlers

---
+++ Rhino.DivanDB.Server/Responders/DivanRootResponser.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace Rhino.DivanDB.Server.Responders
+{
+    public class DivanRootResponser : RequestResponder
+    {
+        public override string UrlPattern
+        {
+            get { return "/divan$"; }
+        }
+
+        public override string[] SupportedVerbs
+        {
+            get { return new[] { "GET" }; }
+        }
+
+        public override void Respond(HttpListenerContext context)
+        {
+            context.Response.Redirect("/divan/index.html");
+        }
+    }
+}
