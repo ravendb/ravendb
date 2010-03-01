2ed7b99788024ba0d0f9acbfcc1e297339b350d3
Andrew Stewart <Andrew.Stewart@i-nnovate.net>
2010-03-01 22:09:11 +0000

    Added functionality to store Entity to Server

--- Rhino.DivanDB.Client.Tests/DocumentStoreServerTests.cs
+++ Rhino.DivanDB.Client.Tests/DocumentStoreServerTests.cs
@@ -17,31 +17,13 @@ namespace Rhino.DivanDB.Client.Tests
             using (var server = new DivanServer(DbName, 8080))
             {
                 var documentStore = new DocumentStore("localhost", 8080);
+                documentStore.Initialise();
+
                 var session = documentStore.OpenSession();
                 var entity = new Company { Name = "Pap" };
                 session.Store(entity);
 
-                Assert.Equal(Guid.Empty.ToString(), entity.Id);
-//                var request = WebRequest.Create("http://localhost:8080" + "/docs");
-//                request.Method = "POST";
-//
-//                
-//                var json = JsonConvert.SerializeObject(entity);
-//                byte[] byteArray = Encoding.UTF8.GetBytes(json);
-//                request.ContentType = "application/json";
-//
-//                var dataStream = request.GetRequestStream();
-//                // Write the data to the request stream.
-//                dataStream.Write(byteArray, 0, byteArray.Length);
-//
-//                // Close the Stream object.
-//                dataStream.Close();
-//
-//                var response = request.GetResponse();
-//                var responseString = response.GetResponseStream();
-//                Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-//                var reader = new StreamReader(responseString);
-//                var returned = reader.ReadToEnd();
+                Assert.NotEqual(Guid.Empty.ToString(), entity.Id);
             }
         }
     }
