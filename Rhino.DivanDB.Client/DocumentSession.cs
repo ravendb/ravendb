4b89343954c7f91f2069c2ea545956e2c2f227db
Andrew Stewart <Andrew.Stewart@i-nnovate.net>
2010-03-01 20:18:10 +0000

    Refactored code

--- Rhino.DivanDB.Client/DocumentSession.cs
+++ Rhino.DivanDB.Client/DocumentSession.cs
@@ -25,19 +25,26 @@ namespace Rhino.DivanDB.Client
         {
             var documentFound = database.Get(id);
             var jsonString = Encoding.UTF8.GetString(documentFound);
-            var entity = JsonConvert.DeserializeObject(jsonString, typeof(T));
-            var map = documentDb.GetMap<T>();
-            map.IdentityProperty.SetValue(entity, id, null);
+            var entity = convertToEntity<T>(id, jsonString);
             entities.Add(entity);
             return (T)entity;
         }
 
+        private object convertToEntity<T>(string id, string documentFound)
+        {
+            var entity = JsonConvert.DeserializeObject(documentFound, typeof(T));
+            var map = documentDb.GetMap<T>();
+            map.IdentityProperty.SetValue(entity, id, null);
+            return entity;
+        }
+
         public void Store<T>(T entity)
         {
-            var objectAsJson = JObject.FromObject(entity);
-            objectAsJson.Add("type", JToken.FromObject(entity.GetType()));
-            var key = database.Put(objectAsJson);
+            var json = convertEntityToJson(entity);
+
+            var key = database.Put(json);
             entities.Add(entity);
+
             var map = documentDb.GetMap<T>();
             map.IdentityProperty.SetValue(entity, key, null);
         }
@@ -46,14 +53,21 @@ namespace Rhino.DivanDB.Client
         {
             foreach (var entity in entities)
             {
-                var objectAsJson = JObject.FromObject(entity);
-                var map = documentDb.GetMap(entity.GetType());
-                var value = map.IdentityProperty.GetValue(entity, null);
-                objectAsJson.Remove(map.IdentityProperty.Name);
-                objectAsJson.Add("_id", JToken.FromObject(value));
-                objectAsJson.Add("type", JToken.FromObject(entity.GetType()));
+                JObject objectAsJson = convertEntityToJson(entity);
                 database.Put(objectAsJson);
             }
+        }
+
+        private JObject convertEntityToJson(object entity)
+        {
+            var map = documentDb.GetMap(entity.GetType());
+            var value = map.IdentityProperty.GetValue(entity, null);
+                
+            var objectAsJson = JObject.FromObject(entity);
+            objectAsJson.Remove(map.IdentityProperty.Name);
+            objectAsJson.Add("_id", JToken.FromObject(value));
+            objectAsJson.Add("type", JToken.FromObject(entity.GetType()));
+            return objectAsJson;
         }
 
         public IQueryable<T> Query<T>()
@@ -67,15 +81,14 @@ namespace Rhino.DivanDB.Client
             QueryResult result;
             do
             {
-                result = database.Query("getByType", "type:" + typeof(T), 0, int.MaxValue);
+                result = database.Query("getByType", "type:" + typeof(T), 0, int.MaxValue); // To be replaced with real paging
             } while (result.IsStale);
 
             return result.Results.Select(q =>
             {
                 var entity = JsonConvert.DeserializeObject(q.ToString(), typeof(T));
-                var map = documentDb.GetMap<T>();
                 var id = q.Value<string>("_id");
-                map.IdentityProperty.SetValue(entity, id, null);
+                convertToEntity<T>(id, q.ToString());
                 return (T)entity;
             }).ToList();
         }
