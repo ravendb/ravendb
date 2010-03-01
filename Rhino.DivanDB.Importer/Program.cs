4f31bb470228e94e0a1297de29e445037cdfc33f
Ayende Rahien <ayende@ayende.com>
2010-03-01 00:48:16 +0000

    Adding importer project

---
+++ Rhino.DivanDB.Importer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace Rhino.DivanDB.Importer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: importer.exe db-dir docs-dir");
+                return;
+            }
+            var files = Directory.GetFiles(args[1]);
+            Console.WriteLine("Parsing {0:#,#} docs", files.Length);
+            var array = files.Select(x => JObject.Parse(File.ReadAllText(x))).ToArray();
+            Console.WriteLine("Inserting {0:#,#} docs", files.Length);
+            var sw = Stopwatch.StartNew();
+            int count = 0;
+            using (var db = new DocumentDatabase(args[0]))
+            {
+                foreach (var doc in array)
+                {
+                    count++;
+                    db.Put(doc);
+                }
+            }
+            Console.WriteLine("{0} doc inserts in {1}", count, sw.Elapsed);
+        }
+    }
+}
