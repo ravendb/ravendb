dd4319f813b73af693b9f8c7a8cdfb4c3755c9c4
Ayende Rahien <ayende@ayende.com>
2010-03-02 13:15:27 +0000

    Removing old UI Updating references to RDB

--- default.ps1
+++ default.ps1
@@ -122,7 +122,7 @@ task Test -depends Compile {
   exec "$tools_dir\xUnit\xunit.console.exe" "$build_dir\Raven.Tests.dll"
   exec "$tools_dir\xUnit\xunit.console.exe" "$build_dir\Raven.Scenarios.dll"
  # DISABLING UNTIL BUILD PASSES AGAIN
- # exec "$tools_dir\xUnit\xunit.console.exe" "$build_dir\Rhino.DivanDB.Client.Tests.dll"
+ # exec "$tools_dir\xUnit\xunit.console.exe" "$build_dir\Raven.Client.Tests.dll"
   cd $old
 }
 
