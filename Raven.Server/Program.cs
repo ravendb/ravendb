7edd1fafba22a8eecc84d8818f657ad90401d679
bjarte.skogoy <bjarte.skogoy@gmail.com>
2010-03-03 10:15:53 +0100

    - service support - psake build script works on paths iwth white spaces

--- Raven.Server/Program.cs
+++ Raven.Server/Program.cs
@@ -1,19 +1,91 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Configuration.Install;
+using System.Reflection;
 
 namespace Raven.Server
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            DivanServer.EnsureCanListenToWhenInNonAdminContext(8080);
-            Console.WriteLine(Path.GetFullPath(@"..\..\..\Data"));
-            using (new DivanServer(@"..\..\..\Data", 8080))
+            if (Environment.UserInteractive)
             {
-                Console.WriteLine("Ready to process requests...");
-                Console.ReadLine();
+                if (args.Length == 1)
+                {
+                    switch (args[0])
+                    {
+                        case "/service":
+                            RunAsService();
+                            break;
+
+                        case "/install":
+                            InstallAndStart();
+                            break;
+
+                        case "/uninstall":
+                            EnsureStoppedAndUninstall();
+                            break;
+                    }
+                }
+                else
+                {
+                    DivanServer.EnsureCanListenToWhenInNonAdminContext(8080);
+                    Console.WriteLine(Path.GetFullPath(@"..\..\..\Data"));
+                    using (new DivanServer(@"..\..\..\Data", 8080))
+                    {
+                        Console.WriteLine("Ready to process requests...");
+                        Console.ReadLine();
+                    }
+                }
             }
+            else
+            {
+                ServiceBase.Run(new RavenService());
+            }
+        }
+
+        private static void EnsureStoppedAndUninstall()
+        {
+            if (ServiceIsInstalled() == false)
+            {
+                Console.WriteLine("Service is not installed");
+            }
+            else
+            {
+                var stopController = new ServiceController(ProjectInstaller.SERVICE_NAME);
+
+                if (stopController.Status == ServiceControllerStatus.Running)
+                    stopController.Stop();
+
+                ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
+            }
+        }
+
+        private static void InstallAndStart()
+        {
+            if (ServiceIsInstalled() == true)
+            {
+                Console.WriteLine("Service is already installed");
+            }
+            else
+            {
+                ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+                var startController = new ServiceController(ProjectInstaller.SERVICE_NAME);
+                startController.Start();
+            }
+        }
+
+        private static void RunAsService()
+        {
+            //TODO: not sure about this one
+        }
+
+        private static bool ServiceIsInstalled()
+        {
+            return (ServiceController.GetServices().Count(s => s.ServiceName == ProjectInstaller.SERVICE_NAME) > 0);
         }
     }
 }
---
+++ Raven.Server/RavenService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Raven.Server
+{
+    partial class RavenService : ServiceBase
+    {
+        DivanServer m_Server = null;
+
+        public RavenService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            m_Server = new DivanServer(@"..\..\..\Data", 8080);
+        }
+
+        protected override void OnStop()
+        {
+            if (m_Server != null)
+                m_Server.Dispose();
+        }
+    }
+}
