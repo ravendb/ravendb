32a180fc198c2167d1b5c3287586b19fa9e79506
Ayende Rahien <ayende@ayende.com>
2010-03-06 08:51:09 +0000

    Adding server error file (was missing because of gitignore settings) Adding a class that enable robust enumeration

---
+++ Raven.Database/Data/ServerError.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Raven.Database.Data
+{
+    public class ServerError
+    {
+        public string Index { get; set; }
+        public string Error { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public string Document { get; set; }
+    }
+}
---
+++ Raven.Database/Indexing/StatefulEnumerableWrapper.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Raven.Database.Indexing
+{
+    public class StatefulEnumerableWrapper<T> : IEnumerable<T>
+    {
+        private readonly IEnumerator<T> inner;
+
+        public StatefulEnumerableWrapper(IEnumerator<T> inner)
+        {
+            this.inner = inner;
+        }
+
+        public class StatefulbEnumeratorWrapper : IEnumerator<T>
+        {
+            private readonly IEnumerator<T> inner;
+
+            public StatefulbEnumeratorWrapper(IEnumerator<T> inner)
+            {
+                this.inner = inner;
+            }
+
+            public void Dispose()
+            {
+                inner.Dispose();
+            }
+
+            public bool MoveNext()
+            {
+                return inner.MoveNext();
+            }
+
+            public void Reset()
+            {
+                inner.Reset();
+            }
+
+            public T Current
+            {
+                get { return inner.Current; }
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+        }
+
+        public T Current
+        {
+            get { return inner.Current; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new StatefulbEnumeratorWrapper(inner);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
