cc8528ad309cc3712768f1f1febeb00e2faa8911
Ayende Rahien <Ayende@ayende.com>
2010-04-12 08:50:59 +0300

    Cleanup in ctor & dtor - ensure cleanup and ensure that failures will be close to where they should be

--- Raven.Tests/Storage/AbstractDocumentStorageTest.cs
+++ Raven.Tests/Storage/AbstractDocumentStorageTest.cs
@@ -5,6 +5,12 @@ namespace Raven.Tests.Storage
 {
 	public class AbstractDocumentStorageTest : WithDebugging, IDisposable
 	{
+		public AbstractDocumentStorageTest()
+		{
+			if (Directory.Exists("raven.db.test.esent"))
+				Directory.Delete("raven.db.test.esent", true);
+		}
+
 		public virtual void Dispose()
 		{
 			if (Directory.Exists("raven.db.test.esent"))
