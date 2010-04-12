49fd8b1eb225eb0269085d0d36200a598613c66b
Ayende Rahien <Ayende@ayende.com>
2010-04-12 03:27:30 +0300

    Updating asm info for the no-commit mode

--- Raven.Sample.ShardClient/ShardStrategy.cs
+++ Raven.Sample.ShardClient/ShardStrategy.cs
@@ -1,41 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Raven.Client.Shard;
-using Raven.Client;
 using Raven.Client.Shard.ShardStrategy.ShardAccess;
 
 namespace Raven.Sample.ShardClient
 {
     public class ShardStrategy : IShardStrategy
     {
-        private IShardSelectionStrategy _ShardSelectionStrategy = new ShardSelectionByRegion();
-        public IShardSelectionStrategy ShardSelectionStrategy
-        {
-            get
-            {
-                return _ShardSelectionStrategy;
-            }
-        }
+    	public IShardSelectionStrategy ShardSelectionStrategy { get; set; }
 
-        //since we are using all shards for companies, always search all shards
-        private IShardResolutionStrategy _ShardResolutionStrategy = new AllShardsResolutionStrategy();
-        public IShardResolutionStrategy ShardResolutionStrategy
-        {
-            get
-            {
-                return _ShardResolutionStrategy;
-            }
-        }
+    	public IShardResolutionStrategy ShardResolutionStrategy { get; set; }
 
-        private IShardAccessStrategy _ShardAccessStrategy = new ParallelShardAccessStrategy();
-        public IShardAccessStrategy ShardAccessStrategy
-        {
-            get
-            {
-                return _ShardAccessStrategy;
-            }
-        }
+		public IShardAccessStrategy ShardAccessStrategy { get; set; }
+
+    	public ShardStrategy()
+    	{
+    		ShardAccessStrategy = new ParallelShardAccessStrategy();
+    		ShardSelectionStrategy = new ShardSelectionByRegion();
+    		ShardResolutionStrategy = new AllShardsResolutionStrategy();
+    	}
+
     }
 }
