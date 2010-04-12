a513692f2869dc0c6e458a8c5abb0e5a9841fe78
Ayende Rahien <Ayende@ayende.com>
2010-04-12 08:11:14 +0300

    Better error handling at the client. Can now send metadata to the server from the client API

--- Raven.Client/Client/HttpJsonRequest.cs
+++ Raven.Client/Client/HttpJsonRequest.cs
@@ -1,6 +1,8 @@
+using System;
 using System.IO;
 using System.Net;
 using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace Raven.Client.Client
 {
@@ -9,21 +11,59 @@ namespace Raven.Client.Client
 		private readonly WebRequest webRequest;
 
 		public HttpJsonRequest(string url, string method)
+			: this(url, method, new JObject())
+		{
+			
+		}
+
+		public HttpJsonRequest(string url, string method, JObject metadata)
 		{
 			webRequest = WebRequest.Create(url);
+			WriteMetadata(metadata);
 			webRequest.Method = method;
 			webRequest.ContentType = "application/json";
 		}
 
 		public string ReadResponseString()
 		{
-			var response = webRequest.GetResponse();
+			WebResponse response;
+			try
+			{
+				response = webRequest.GetResponse();
+			}
+			catch (WebException e)
+			{
+				var httpWebResponse = e.Response as HttpWebResponse;
+				if (httpWebResponse == null || httpWebResponse.StatusCode == HttpStatusCode.NotFound)
+					throw;
+				using(var sr = new StreamReader(e.Response.GetResponseStream()))
+				{
+					throw new InvalidOperationException(sr.ReadToEnd(), e);
+				}
+			}
 			using (var responseString = response.GetResponseStream())
 			{
 				var reader = new StreamReader(responseString);
 				var text = reader.ReadToEnd();
 				reader.Close();
 				return text;
+			}
+		}
+
+		private void WriteMetadata(JObject metadata)
+		{
+			if (metadata == null)
+				return;
+
+			foreach (JProperty prop in metadata)
+			{
+				if(prop.Value == null)
+					continue;
+					
+				if (prop.Value.Type == JsonTokenType.Object || prop.Value.Type == JsonTokenType.Array)
+					continue;
+
+				webRequest.Headers[prop.Name] = prop.Value.Value<object>().ToString();
 			}
 		}
 
