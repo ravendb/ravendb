f0ea3f48a3163bfbc244e393a9147d084c71d576
Ayende Rahien <ayende@ayende.com>
2010-04-15 03:38:35 +0300

    More tests

---
+++ Raven.Tests/Transactions/MuiltipleDocuments.cs
@@ -0,0 +1,53 @@
+using System;
+using Newtonsoft.Json.Linq;
+using Raven.Database;
+using Raven.Tests.Storage;
+using Xunit;
+
+namespace Raven.Tests.Transactions
+{
+    public class MuiltipleDocuments : AbstractDocumentStorageTest
+    {
+        private readonly DocumentDatabase db;
+
+		public MuiltipleDocuments()
+		{
+			db = new DocumentDatabase(new RavenConfiguration {DataDirectory = "raven.db.test.esent"});
+		}
+
+        public override void Dispose()
+        {
+            db.Dispose();
+            base.Dispose();
+        }
+
+        [Fact]
+        public void PutTwoDocumentsAndThenCommit()
+        {
+            var transactionInformation = new TransactionInformation { Id = Guid.NewGuid(), Timeout = TimeSpan.FromMinutes(1) };
+            db.Put("ayende1", null, JObject.Parse("{ayende:'rahien'}"), new JObject(), transactionInformation);
+            db.Put("ayende2", null, JObject.Parse("{ayende:'rahien'}"), new JObject(), transactionInformation);
+
+            db.Commit(transactionInformation);
+
+            Assert.NotNull(db.Get("ayende1", null));
+            Assert.NotNull(db.Get("ayende2", null));
+        }
+
+        [Fact]
+        public void WhileUpdatingSeveralDocumentsCannotAccessAnyOfThem()
+        {
+            var transactionInformation = new TransactionInformation { Id = Guid.NewGuid(), Timeout = TimeSpan.FromMinutes(1) };
+            db.Put("ayende1", null, JObject.Parse("{ayende:'rahien'}"), new JObject(), transactionInformation);
+            Assert.Null(db.Get("ayende1", null));
+            Assert.Null(db.Get("ayende2", null)); 
+            db.Put("ayende2", null, JObject.Parse("{ayende:'rahien'}"), new JObject(), transactionInformation);
+            Assert.Null(db.Get("ayende1", null));
+            Assert.Null(db.Get("ayende2", null));
+            db.Commit(transactionInformation);
+
+            Assert.NotNull(db.Get("ayende1", null));
+            Assert.NotNull(db.Get("ayende2", null));
+        }
+    }
+}
---
+++ Raven.Tests/Transactions/WriteConflicts.cs
@@ -0,0 +1,72 @@
+using System;
+using Newtonsoft.Json.Linq;
+using Raven.Database;
+using Raven.Database.Exceptions;
+using Raven.Tests.Storage;
+using Xunit;
+
+namespace Raven.Tests.Transactions
+{
+    public class WriteConflicts : AbstractDocumentStorageTest
+    {
+        private readonly DocumentDatabase db;
+
+        public WriteConflicts()
+        {
+            db = new DocumentDatabase(new RavenConfiguration { DataDirectory = "raven.db.test.esent" });
+        }
+
+        public override void Dispose()
+        {
+            db.Dispose();
+            base.Dispose();
+        }
+
+        [Fact]
+        public void WhileDocumentIsBeingUpdatedInTransactionCannotUpdateOutsideTransaction()
+        {
+            db.Put("ayende", null, JObject.Parse("{ayende:'oren'}"), new JObject(), null);
+            var transactionInformation = new TransactionInformation { Id = Guid.NewGuid(), Timeout = TimeSpan.FromMinutes(1) };
+            db.Put("ayende", null, JObject.Parse("{ayende:'rahien'}"), new JObject(), transactionInformation);
+            Assert.Throws<ConcurrencyException>(
+                () => db.Put("ayende", null, JObject.Parse("{ayende:'oren'}"), new JObject(), null));
+        }
+
+        [Fact]
+        public void WhileDocumentIsBeingUpdatedInTransactionCannotUpdateInsideAnotherTransaction()
+        {
+            db.Put("ayende", null, JObject.Parse("{ayende:'oren'}"), new JObject(), null);
+            var transactionInformation = new TransactionInformation { Id = Guid.NewGuid(), Timeout = TimeSpan.FromMinutes(1) };
+            db.Put("ayende", null, JObject.Parse("{ayende:'rahien'}"), new JObject(), transactionInformation);
+            Assert.Throws<ConcurrencyException>(
+                () => db.Put("ayende", null, JObject.Parse("{ayende:'oren'}"), new JObject(), new TransactionInformation
+                {
+                    Id = Guid.NewGuid(),
+                    Timeout = TimeSpan.FromMinutes(1)
+                }));
+        }
+
+
+        [Fact]
+        public void WhileCreatingDocumentInTransactionTryingToWriteInAnotherTransactionFail()
+        {
+            var transactionInformation = new TransactionInformation { Id = Guid.NewGuid(), Timeout = TimeSpan.FromMinutes(1) };
+            db.Put("ayende", null, JObject.Parse("{ayende:'rahien'}"), new JObject(), transactionInformation);
+            Assert.Throws<ConcurrencyException>(
+                () => db.Put("ayende", null, JObject.Parse("{ayende:'oren'}"), new JObject(), new TransactionInformation
+                {
+                    Id = Guid.NewGuid(),
+                    Timeout = TimeSpan.FromMinutes(1)
+                }));
+        }
+
+        [Fact]
+        public void WhileCreatingDocumentInTransactionTryingToWriteOutsideTransactionFail()
+        {
+            var transactionInformation = new TransactionInformation { Id = Guid.NewGuid(), Timeout = TimeSpan.FromMinutes(1) };
+            db.Put("ayende", null, JObject.Parse("{ayende:'rahien'}"), new JObject(), transactionInformation);
+            Assert.Throws<ConcurrencyException>(
+                () => db.Put("ayende", null, JObject.Parse("{ayende:'oren'}"), new JObject(), null));
+        }
+    }
+}
