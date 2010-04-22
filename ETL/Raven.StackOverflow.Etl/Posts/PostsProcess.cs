03a238e1efd9c44d860067a27d128b1a3eea9f62
Ayende Rahien <Ayende@ayende.com>
2010-04-23 00:18:50 +0300

    Use single threaded pipeline - easier to understand what is going on

--- ETL/Raven.StackOverflow.Etl/Posts/PostsProcess.cs
+++ ETL/Raven.StackOverflow.Etl/Posts/PostsProcess.cs
@@ -3,6 +3,7 @@ using System.IO;
 using Raven.Database;
 using Raven.StackOverflow.Etl.Generic;
 using Rhino.Etl.Core;
+using Rhino.Etl.Core.Pipelines;
 
 namespace Raven.StackOverflow.Etl.Posts
 {
@@ -19,9 +20,8 @@ namespace Raven.StackOverflow.Etl.Posts
 
 		protected override void Initialize()
 		{
+			PipelineExecuter = new SingleThreadedPipelineExecuter();
 			Register(new XmlRowOperationFile(Path.Combine(path, "posts.xml")));
-			Register(new TryConvert<DateTime>(DateTime.TryParse));
-			Register(new TryConvert<long>(long.TryParse));
 			Register(new RowToDatabase(database, "Posts", doc => "posts/" + doc["Id"]));
 		}
 	}
