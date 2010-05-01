16e137118bb6fb309f68246ee7f4503c6a066370
Ayende Rahien <Ayende@ayende.com>
2010-05-01 12:43:16 +0300

    Fix query to reflect that tag are not tokenized

--- Raven.Client/RavenExtensions.cs
+++ Raven.Client/RavenExtensions.cs
@@ -6,7 +6,7 @@ namespace Raven.Client
 	{
 		public static IDocumentQuery<T> Query<T>(this IDocumentSession session)
 		{
-			var whereClause = "Tag:" + Inflector.Pluralize(typeof(T).Name);
+			var whereClause = "Tag:`" + Inflector.Pluralize(typeof(T).Name) +"`";
             return session.Query<T>("Raven/DocumentsByEntityName")
 				.Where(whereClause);
 		}
