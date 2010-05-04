d51dae4493d2c52b311009f62c0a93257a22f130
Ayende Rahien <Ayende@ayende.com>
2010-05-05 00:25:08 +0300

    Fixing a problem when NOT running in a virtual directory

--- Raven.Database/Server/Abstractions/IHttpContext.cs
+++ Raven.Database/Server/Abstractions/IHttpContext.cs
@@ -17,7 +17,7 @@ namespace Raven.Database.Server.Abstractions
 		public static string GetRequestUrl(this IHttpContext context)
 		{
 			string localPath = context.Request.Url.LocalPath;
-			if (string.IsNullOrEmpty(context.Configuration.VirtualDirectory) == false &&
+			if (context.Configuration.VirtualDirectory != "/" &&
 				localPath.StartsWith(context.Configuration.VirtualDirectory, StringComparison.InvariantCultureIgnoreCase))
 			{
 				localPath = localPath.Substring(context.Configuration.VirtualDirectory.Length);
