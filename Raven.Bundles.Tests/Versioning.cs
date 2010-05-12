b9a2198fca411aab936ded4f88d3368ad137f221
Ayende Rahien <ayende@ayende.com>
2010-05-12 08:51:52 +0100

    Adding the concept of bundles Adding the Versioning bundle

---
+++ Raven.Bundles.Tests/Versioning.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+using Raven.Client.Document;
+using Raven.Client.Tests;
+using Raven.Client.Tests.Document;
+using Xunit;
+
+namespace Raven.Bundles.Tests
+{
+    public class Versioning : BaseTest, IDisposable
+    {
+        private string path;
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            Directory.Delete(path, true);
+        }
+
+        #endregion
+
+        private DocumentStore NewDocumentStore()
+        {
+            path = Path.GetDirectoryName(Assembly.GetAssembly(typeof(Versioning)).CodeBase);
+            path = Path.Combine(path, "TestDb").Substring(6);
+            var documentStore = new DocumentStore
+            {
+                Configuration =
+                    {
+                        DataDirectory = path,
+                        RunInUnreliableYetFastModeThatIsNotSuitableForProduction = true,
+                        Catalog =
+                            {
+                                Catalogs =
+                                    {
+                                        new AssemblyCatalog(typeof (Bundles.Versioning.PutTrigger).Assembly)
+                                    }
+                            }
+                    }
+            };
+            documentStore.Initialise();
+            return documentStore;
+        }
+
+        [Fact]
+        public void Will_automatically_set_metadata()
+        {
+            using (var documentStore = NewDocumentStore())
+            {
+                var company = new Company { Name = "Company Name" };
+                using(var session = documentStore.OpenSession())
+                {
+                    session.Store(company);
+                    session.SaveChanges();
+                }
+
+                using (var session = documentStore.OpenSession())
+                {
+                    var company2 = session.Load<Company>(company.Id);
+                    var metadata = session.GetMetadataFor(company2);
+                    Assert.Equal("Current", metadata.Value<string>("Raven-Document-Revision-Status"));
+                    Assert.Equal(1, metadata.Value<int>("Raven-Document-Revision"));
+                }
+            }
+        }
+
+        [Fact]
+        public void Will_automatically_update_metadata_on_next_insert()
+        {
+            using (var documentStore = NewDocumentStore())
+            {
+                var company = new Company { Name = "Company Name" };
+                using (var session = documentStore.OpenSession())
+                {
+                    session.Store(company);
+                    session.SaveChanges();
+                    company.Name = "Hibernating Rhinos";
+                    session.SaveChanges();
+                }
+
+                using (var session = documentStore.OpenSession())
+                {
+                    var company2 = session.Load<Company>(company.Id);
+                    var metadata = session.GetMetadataFor(company2);
+                    Assert.Equal("Current", metadata.Value<string>("Raven-Document-Revision-Status"));
+                    Assert.Equal(2, metadata.Value<int>("Raven-Document-Revision"));
+                }
+            }
+        }
+
+        [Fact]
+        public void Will_automatically_craete_duplicate_on_first_insert()
+        {
+            using (var documentStore = NewDocumentStore())
+            {
+                var company = new Company { Name = "Company Name" };
+                using (var session = documentStore.OpenSession())
+                {
+                    session.Store(company);
+                    session.SaveChanges();
+                }
+
+                using (var session = documentStore.OpenSession())
+                {
+                    var company2 = session.Load<Company>(company.Id + "/revisions/1");
+                    var metadata = session.GetMetadataFor(company2);
+                    Assert.Equal(company.Name, company2.Name);
+                    Assert.Equal("Historical", metadata.Value<string>("Raven-Document-Revision-Status"));
+                    Assert.Equal(1, metadata.Value<int>("Raven-Document-Revision"));
+                }
+            }
+        }
+
+        [Fact]
+        public void Will_automatically_craete_duplicate_on_next_insert()
+        {
+            using (var documentStore = NewDocumentStore())
+            {
+                var company = new Company { Name = "Company Name" };
+                using (var session = documentStore.OpenSession())
+                {
+                    session.Store(company);
+                    session.SaveChanges();
+                    company.Name = "Hibernating Rhinos";
+                    session.SaveChanges();
+                }
+
+                using (var session = documentStore.OpenSession())
+                {
+                    var company2 = session.Load<Company>(company.Id + "/revisions/1");
+                    var metadata = session.GetMetadataFor(company2);
+                    Assert.Equal("Company Name", company2.Name);
+                    Assert.Equal("Historical", metadata.Value<string>("Raven-Document-Revision-Status"));
+                    Assert.Equal(1, metadata.Value<int>("Raven-Document-Revision"));
+
+                    company2 = session.Load<Company>(company.Id + "/revisions/2");
+                    metadata = session.GetMetadataFor(company2);
+                    Assert.Equal("Hibernating Rhinos", company2.Name);
+                    Assert.Equal("Historical", metadata.Value<string>("Raven-Document-Revision-Status"));
+                    Assert.Equal(2, metadata.Value<int>("Raven-Document-Revision"));
+                }
+            }
+        }
+    }
+}
---
+++ Raven.Bundles.Versioning/PutTrigger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using Raven.Database;
+using Raven.Database.Plugins;
+using System.Linq;
+
+namespace Raven.Bundles.Versioning
+{
+    public class PutTrigger : IPutTrigger, IRequiresDocumentDatabaseInitialization
+    {
+        private const string RavenDocumentRevision = "Raven-Document-Revision";
+        private const string RavenDocumentRevisionStatus = "Raven-Document-Revision-Status";
+        private DocumentDatabase docDb;
+
+        public VetoResult AllowPut(string key, JObject document, JObject metadata, TransactionInformation transactionInformation)
+        {
+            if (metadata.Value<string>(RavenDocumentRevisionStatus) != "Historical")
+                return VetoResult.Allowed;
+            if(  docDb.Get(key, transactionInformation) == null)
+                return VetoResult.Allowed;
+          
+            return VetoResult.Deny("Modifying a historical revision is not allowed");
+        }
+
+        public void OnPut(string key, JObject document, JObject metadata, TransactionInformation transactionInformation)
+        {
+            if (metadata.Value<string>(RavenDocumentRevisionStatus) == "Historical")
+                return;
+
+            int revision = 0;
+            if (metadata[RavenDocumentRevision] != null)
+                revision = metadata.Value<int>(RavenDocumentRevision);
+
+            var copyMetadata = new JObject(metadata);
+            copyMetadata[RavenDocumentRevisionStatus] = JToken.FromObject("Historical");
+            copyMetadata[RavenDocumentRevision] = JToken.FromObject(revision +1);
+            PutResult newDoc = docDb.Put(key + "/revisions/", null, document, copyMetadata,
+                                         transactionInformation);
+            revision = int.Parse(newDoc.Key.Split('/').Last());
+
+            metadata[RavenDocumentRevisionStatus] = JToken.FromObject("Current");
+            metadata[RavenDocumentRevision] = JToken.FromObject(revision);
+        }
+
+        public void AfterCommit(string key, JObject document, JObject metadata)
+        {
+        }
+
+        public void Initialize(DocumentDatabase database)
+        {
+            docDb = database;
+        }
+    }
+}
