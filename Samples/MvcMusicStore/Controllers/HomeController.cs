b3badc84168ff742bcfdfc164068d0630144e01b
Ayende Rahien <ayende@ayende.com>
2010-05-15 00:03:08 +0100

    Adding code to port the data over from the relational store to the document store

--- Samples/MvcMusicStore/Controllers/HomeController.cs
+++ Samples/MvcMusicStore/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Web.Mvc;
 using MvcMusicStore.Models;
+using Raven.Client.Document;
 
 namespace MvcMusicStore.Controllers
 {
@@ -18,6 +19,54 @@ namespace MvcMusicStore.Controllers
             var albums = GetTopSellingAlbums(5);
 
             return View(albums);
+        }
+
+
+        public ActionResult Port()
+        {
+using (var documentStore = new DocumentStore
+{
+    Url = "http://localhost:8080",
+    Conventions =
+        {
+            FindTypeTagName = type =>
+            {
+                if (type.GetProperty("Genre") == null)
+                    return "Genres";
+                return "Albums";
+            }
+        }
+})
+{
+    documentStore.Initialise();
+    using (var session = documentStore.OpenSession())
+    {
+        session.OnEntityConverted += (entity, document, metadata) => metadata.Remove("Raven-Clr-Type");
+        foreach (var album in storeDB.Albums.Include("Artist").Include("Genre"))
+        {
+            session.Store(new
+            {
+                Id = "albums/" + album.AlbumId,
+                album.AlbumArtUrl,
+                Arist = new { album.Artist.Name, Id = "artists/" + album.Artist.ArtistId },
+                Genre = new { album.Genre.Name, Id = "genres/" + album.Genre.GenreId },
+                album.Price,
+                album.Title,
+            });
+        }
+        foreach (var genre in storeDB.Genres)
+        {
+            session.Store(new
+            {
+                genre.Description,
+                genre.Name,
+                Id = "genres/" + genre.GenreId
+            });
+        }
+        session.SaveChanges();
+    }
+}
+            return Content("OK");
         }
 
         private List<Album> GetTopSellingAlbums(int count)
