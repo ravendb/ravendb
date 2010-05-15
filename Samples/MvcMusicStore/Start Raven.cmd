777c3c5971731db4ab7605f3e0a6c0dec3781db2
Ayende Rahien <ayende@ayende.com>
2010-05-15 16:34:02 +0100

    Updated build script to include sample app Added a way to start raven directly off the sample directory Added dump of default sample database

---
+++ Samples/MvcMusicStore/Start Raven.cmd
@@ -0,0 +1,4 @@
+# this scripts assumes the directory structure of the released zip
+# starts the server in debug mode in the current directory, meaning that
+# it will look for a default.raven file and load it if it exists
+..\..\Server\RavenDB
