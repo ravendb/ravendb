acd3b591a442601cabc2738035dabbed0b12869e
Ayende Rahien <ayende@ayende.com>
2010-05-15 04:04:00 +0100

    Fixing view models to new standards

--- Samples/MvcMusicStore/ViewModels/StoreBrowseViewModel.cs
+++ Samples/MvcMusicStore/ViewModels/StoreBrowseViewModel.cs
@@ -6,6 +6,6 @@ namespace MvcMusicStore.ViewModels
     public class StoreBrowseViewModel
     {
         public Genre Genre { get; set; }
-        public List<Album> Albums { get; set; }
+        public IList<Album> Albums { get; set; }
     }
 }
--- Samples/MvcMusicStore/ViewModels/StoreIndexViewModel.cs
+++ Samples/MvcMusicStore/ViewModels/StoreIndexViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore.ViewModels
 {
     public class StoreIndexViewModel
     {
         public int NumberOfGenres { get; set; }
-        public List<string> Genres { get; set; }
+        public IList<Genre> Genres { get; set; }
     }
 }
