90c506b8c82913d9d5442507d28bc7ccf2ebf398
Ayende Rahien <ayende@ayende.com>
2010-05-15 11:38:42 +0100

    EF Order to Raven's Order

--- Samples/MvcMusicStore/Models/Order.cs
+++ Samples/MvcMusicStore/Models/Order.cs
@@ -1,69 +1,70 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MvcMusicStore.Models
 {
-    [MetadataType(typeof(OrderMetadata))]
-    public partial class Order
+    public class Order
     {
-        // Validation rules for the Order class
-
-        [Bind(Exclude = "OrderId")]
-        public class OrderMetadata
+        public Order()
         {
-            [Required(ErrorMessage = "First Name is required")]
-            [DisplayName("First Name")]
-            [StringLength(160)]
-            public object FirstName { get; set; }
-
-            [Required(ErrorMessage = "Last Name is required")]
-            [DisplayName("Last Name")]
-            [StringLength(160)]
-            public object LastName { get; set; }
-
-            [Required(ErrorMessage = "Address is required")]
-            [StringLength(70)]
-            public object Address { get; set; }
-
-            [Required(ErrorMessage = "City is required")]
-            [StringLength(40)]
-            public object City { get; set; }
-
-            [Required(ErrorMessage = "State is required")]
-            [StringLength(40)]
-            public object State { get; set; }
-
-            [Required(ErrorMessage = "Postal Code is required")]
-            [DisplayName("Postal Code")]
-            [StringLength(10)]
-            public object PostalCode { get; set; }
-
-            [Required(ErrorMessage = "Country is required")]
-            [StringLength(40)]
-            public object Country { get; set; }
-
-            [Required(ErrorMessage = "Phone is required")]
-            [StringLength(24)]
-            public object Phone { get; set; }
-
-            [Required(ErrorMessage = "Email Address is required")]
-            [DisplayName("Email Address")]
-            [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Email is is not valid.")]
-            [DataType(DataType.EmailAddress)]
-            public object Email { get; set; }
-
-            [ScaffoldColumn(false)]
-            public object OrderId { get; set; }
-
-            [ScaffoldColumn(false)]
-            public object OrderDate { get; set; }
-
-            [ScaffoldColumn(false)]
-            public object Username { get; set; }
-
-            [ScaffoldColumn(false)]
-            public object Total { get; set; }
+            Lines = new List<OrderLine>();
         }
+
+        public decimal Total
+        {
+            get
+            {
+                // this executes in memory, no database queries here!
+                return Lines.Sum(x => x.Price);
+            }
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public string Username { get; set; }
+
+        public List<OrderLine> Lines { get; set; }
+
+        public string Id { get; set; }
+
+        #region Nested type: OrderLine
+
+        public class OrderLine
+        {
+            public int Quantity { get; set; }
+            public decimal Price { get; set; }
+
+            public OrderAlbum Album { get; set; }
+
+            #region Nested type: OrderAlbum
+
+            public class OrderAlbum
+            {
+                public string Id { get; set; }
+                public string Title { get; set; }
+            }
+
+            #endregion
+        }
+
+        #endregion
     }
 }
