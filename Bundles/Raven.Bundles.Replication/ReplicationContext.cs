356a8a7ea27b96dbbfd09dffcf52e77bcb1af204
Ayende Rahien <ayende@ayende.com>
2010-05-16 15:00:57 +0100

    Adding replication context, which will let us know when to skip replication tasks because we are executing replication code

---
+++ Bundles/Raven.Bundles.Replication/ReplicationContext.cs
@@ -0,0 +1,25 @@
+using System;
+using Raven.Database.Indexing;
+
+namespace Raven.Bundles.Replication
+{
+    public static class ReplicationContext
+    {
+        [ThreadStatic] private static bool _currentlyInContext;
+
+        public static bool IsInReplicationContext
+        {
+            get
+            {
+                return _currentlyInContext;
+            }
+        }
+
+        public static IDisposable Enter()
+        {
+            var old = _currentlyInContext;
+            _currentlyInContext = true;
+            return new WorkContext.DisposableAction(() => _currentlyInContext = old);
+        }
+    }
+}
