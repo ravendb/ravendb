7101cccb17c961da17ba9951e6e5e1d0fd76b085
Ayende Rahien <ayende@ayende.com>
2010-05-16 15:06:22 +0100

    Minor

--- Bundles/Raven.Bundles.Replication/ReplicationConstants.cs
+++ Bundles/Raven.Bundles.Replication/ReplicationConstants.cs
@@ -2,7 +2,7 @@
 {
     public class ReplicationConstants
     {
-        public const string RavenAncestry = "Raven-Replication-Ancestry";
+        public const string RavenReplicationSource = "Raven-Replication-Source";
         public const string RavenReplicationConflict = "Raven-Replication-Conflict";
         public const string RavenReplicationSourcesBasePath = "Raven/Replication/Sources";
         public const string RavenReplicationDestinations = "Raven/Replication/Destinations";
