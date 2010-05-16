bc20d59eb8cc954eeed211d37e4bec7d338b4313
Ayende Rahien <ayende@ayende.com>
2010-05-16 17:29:30 +0100

    Refactoring test to extract common test class

--- Bundles/Raven.Bundles.Tests/Replication/SimpleReplication.cs
+++ Bundles/Raven.Bundles.Tests/Replication/SimpleReplication.cs
@@ -1,106 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition.Hosting;
-using System.IO;
 using System.Threading;
-using log4net.Appender;
-using log4net.Config;
-using log4net.Filter;
-using log4net.Layout;
 using Raven.Bundles.Replication;
 using Raven.Client;
-using Raven.Client.Document;
-using Raven.Database;
-using Raven.Server;
 using Xunit;
 
 namespace Raven.Bundles.Tests.Replication
 {
-    public class SimpleReplication : IDisposable
+    public class SimpleReplication : ReplicationBase
     {
-        private const int PortRangeStart = 9101;
-
-        private readonly List<IDocumentStore> stores = new List<IDocumentStore>();
-        private readonly List<RavenDbServer> servers = new List<RavenDbServer>();
-        private int RetriesCount = 150;
-
-        public SimpleReplication()
-        {
-            for (int i = 0; i < 15; i++)
-            {
-                if(Directory.Exists("Data #" + i))
-                    Directory.Delete("Data #" + i, true);
-            }
-
-            var outputDebugStringAppender = new OutputDebugStringAppender
-            {
-                Layout = new SimpleLayout(),
-            };
-            outputDebugStringAppender.AddFilter(new LoggerMatchFilter
-            {
-                AcceptOnMatch = true,
-                LoggerToMatch = "Raven.Bundles"
-            });
-            outputDebugStringAppender.AddFilter(new DenyAllFilter());
-            BasicConfigurator.Configure(outputDebugStringAppender);
-        }
-
-        public IDocumentStore CreateStore()
-        {
-            var port = PortRangeStart + servers.Count;
-            RavenDbServer.EnsureCanListenToWhenInNonAdminContext(port);
-            var ravenDbServer = new RavenDbServer(new RavenConfiguration
-            {
-                AnonymousUserAccessMode = AnonymousUserAccessMode.All,
-                Catalog = {Catalogs = {new AssemblyCatalog(typeof (AncestryPutTrigger).Assembly)}},
-                DataDirectory = "Data #" + servers.Count,
-                RunInUnreliableYetFastModeThatIsNotSuitableForProduction = true,
-                Port = port
-            });
-            servers.Add(ravenDbServer);
-            var documentStore = new DocumentStore{Url = ravenDbServer.Database.Configuration.ServerUrl};
-            documentStore.Initialise();
-            stores.Add(documentStore);
-            return documentStore;
-        }
-
-        public void Dispose()
-        {
-            foreach (var documentStore in stores)
-            {
-                documentStore.Dispose();
-            }
-
-            foreach (var ravenDbServer in servers)
-            {
-                ravenDbServer.Dispose();
-                try
-                {
-                    Directory.Delete(ravenDbServer.Database.Configuration.DataDirectory);
-                }
-                catch (Exception)
-                {
-                }
-            }
-        }
-
         [Fact]
         public void Can_replicate_between_two_instances()
         {
             var store1 = CreateStore();
             var store2 = CreateStore();
 
-            using (var session = store1.OpenSession())
-            {
-                session.Store(new ReplicationDocument()
-                {
-                    Destinations = {new ReplicationDestination
-                    {
-                        Url = servers[1].Database.Configuration.ServerUrl
-                    }}
-                });
-                session.SaveChanges();
-            }
+            TellFirstInstanceToReplicateToSecondInstance();
 
             using(var session = store1.OpenSession())
             {
@@ -124,23 +37,14 @@ namespace Raven.Bundles.Tests.Replication
             }
         }
 
+
         [Fact]
         public void Can_replicate_large_number_of_documents_between_two_instances()
         {
             var store1 = CreateStore();
             var store2 = CreateStore();
 
-            using (var session = store1.OpenSession())
-            {
-                session.Store(new ReplicationDocument()
-                {
-                    Destinations = {new ReplicationDestination
-                    {
-                        Url = servers[1].Database.Configuration.ServerUrl
-                    }}
-                });
-                session.SaveChanges();
-            }
+            TellFirstInstanceToReplicateToSecondInstance();
 
             using (var session = store1.OpenSession())
             {
@@ -180,35 +84,9 @@ namespace Raven.Bundles.Tests.Replication
             var store1 = CreateStore();
             var store2 = CreateStore();
 
-            using (var session = store1.OpenSession())
-            {
-                session.Store(new ReplicationDocument()
-                {
-                    Destinations =
-                                  {
-                                      new ReplicationDestination
-                                      {
-                                          Url = servers[1].Database.Configuration.ServerUrl
-                                      }
-                                  }
-                });
-                session.SaveChanges();
-            }
+            TellFirstInstanceToReplicateToSecondInstance();
 
-            using (var session = store2.OpenSession())
-            {
-                session.Store(new ReplicationDocument()
-                {
-                    Destinations =
-                                  {
-                                      new ReplicationDestination
-                                      {
-                                          Url = servers[0].Database.Configuration.ServerUrl
-                                      }
-                                  }
-                });
-                session.SaveChanges();
-            }
+            TellSecondInstanceToReplicateToFirstInstance();
 
             string etag;
             string id;
@@ -256,20 +134,7 @@ namespace Raven.Bundles.Tests.Replication
             var store1 = CreateStore();
             var store2 = CreateStore();
 
-            using (var session = store1.OpenSession())
-            {
-                session.Store(new ReplicationDocument()
-                {
-                    Destinations =
-                                  {
-                                      new ReplicationDestination
-                                      {
-                                          Url = servers[1].Database.Configuration.ServerUrl
-                                      }
-                                  }
-                });
-                session.SaveChanges();
-            }
+            TellFirstInstanceToReplicateToSecondInstance();
 
             using (var session = store1.OpenSession())
             {
