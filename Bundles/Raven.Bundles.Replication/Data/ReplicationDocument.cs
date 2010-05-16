8d05172c96b4fad8394b651d1e6b627cfebb38f3
Ayende Rahien <ayende@ayende.com>
2010-05-16 22:05:05 +0100

    Removing Id

--- Bundles/Raven.Bundles.Replication/Data/ReplicationDocument.cs
+++ Bundles/Raven.Bundles.Replication/Data/ReplicationDocument.cs
@@ -4,14 +4,11 @@ namespace Raven.Bundles.Replication.Data
 {
     public class ReplicationDocument
     {
-        public string Id { get; set; }
-
         public List<ReplicationDestination> Destinations { get; set; }
 
         public ReplicationDocument()
         {
             Destinations = new List<ReplicationDestination>();
-            Id = ReplicationConstants.RavenReplicationDestinations;
         }
     }
 }
