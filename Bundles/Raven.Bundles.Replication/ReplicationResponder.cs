56ade452475250e5a2e1640a30fb6a84b76a4451
Ayende Rahien <ayende@ayende.com>
2010-05-16 17:29:07 +0100

    - Adding logging - Ensuring that the replication source is always set

--- Bundles/Raven.Bundles.Replication/ReplicationResponder.cs
+++ Bundles/Raven.Bundles.Replication/ReplicationResponder.cs
@@ -1,4 +1,5 @@
 using System;
+using log4net;
 using Newtonsoft.Json.Linq;
 using Raven.Database.Server.Abstractions;
 using Raven.Database.Server.Responders;
@@ -8,6 +9,8 @@ namespace Raven.Bundles.Replication
 {
     public class ReplicationResponder : RequestResponder
     {
+        private ILog log = LogManager.GetLogger(typeof (ReplicationResponder));
+
         public override void Respond(IHttpContext context)
         {
             var src = context.Request.QueryString["from"];
@@ -19,16 +22,23 @@ namespace Raven.Bundles.Replication
             var array = context.ReadJsonArray();
             using (ReplicationContext.Enter())
             {
+                log.DebugFormat("Got replication batch of {0} documents from {1}", array.Count, src);
                 Database.TransactionalStorage.Batch(actions =>
                 {
                     string lastEtag = Guid.Empty.ToString();
                     foreach (JObject document in array)
                     {
                         var metadata = document.Value<JObject>("@metadata");
+                        if(metadata[ReplicationConstants.RavenReplicationSource] == null)
+                        {
+                            // not sure why, old document from when the user didn't have replciation
+                            // that we suddenly decided to replicate, choose the source for that
+                            metadata[ReplicationConstants.RavenReplicationSource] = JToken.FromObject(src);
+                        }
                         lastEtag = metadata.Value<string>("@etag");
                         var id = metadata.Value<string>("@id");
                         document.Remove("@metadata");
-                        ReplicateDocument(actions, id, metadata, document);
+                        ReplicateDocument(actions, id, metadata, document, src);
                     }
 
                     Database.Put(ReplicationConstants.RavenReplicationSourcesBasePath + "/" + src, null,
@@ -38,29 +48,24 @@ namespace Raven.Bundles.Replication
             }
         }
 
-        private static void ReplicateDocument(DocumentStorageActions actions, string id, JObject metadata, JObject document)
+        private void ReplicateDocument(DocumentStorageActions actions, string id, JObject metadata, JObject document, string src)
         {
-
             var existingDoc = actions.DocumentByKey(id, null);
+            if (existingDoc == null)
+            {
+                log.DebugFormat("New document {0} replicated successfully from {1}", id, src);
+                actions.AddDocument(id, Guid.Empty, document, metadata);
+                return;
+            }
             var replicationSourceId = metadata.Value<string>(ReplicationConstants.RavenReplicationSource);
-            if (existingDoc == null || replicationSourceId == null)
-            {
-                actions.AddDocument(id, null, document, metadata);
-                return;
-            }
+
             var existingDocumentReplicationSourceId = existingDoc.Metadata.Value<string>(ReplicationConstants.RavenReplicationSource);
-            if(existingDocumentReplicationSourceId == null)
-            {
-                actions.AddDocument(id, null, document, metadata);
-                return;
-            }
-
-            var existingDocumentIsInConflict = existingDoc.Metadata[ReplicationConstants.RavenReplicationConflict] != null;
-
             
+            var existingDocumentIsInConflict = existingDoc.Metadata[ReplicationConstants.RavenReplicationConflict] != null;
             if (existingDocumentIsInConflict == false &&                    // if the current document is in conflict, we have to keep conflict semantics
-                replicationSourceId == existingDocumentReplicationSourceId) // our last update from that server too, so we are fine with overwriting this
+                (replicationSourceId == existingDocumentReplicationSourceId)) // our last update from that server too, so we are fine with overwriting this
             {
+                log.DebugFormat("Existing document {0} replicated successfully from {1}", id, src);
                 actions.AddDocument(id, null, document, metadata);
                 return;
             }
@@ -71,12 +76,15 @@ namespace Raven.Bundles.Replication
 
             if (existingDocumentIsInConflict) // the existing document is in conflict
             {
+                log.DebugFormat("Conflicted document {0} has a new version from {1}, adding to conflicted documents", id, src);
+                
                 // just update the current doc with the new conflict document
                 existingDoc.DataAsJson.Value<JArray>("Conflicts").Add(JToken.FromObject(newDocumentConflictId));
                 actions.AddDocument(id, existingDoc.Etag, existingDoc.DataAsJson, existingDoc.Metadata);
                 return;
             }
-
+            log.DebugFormat("Existing document {0} is in conflict with replicated version from {1}, marking document as conflicted", id, src);
+                
             // we have a new conflict
             // move the existing doc to a conflict and create a conflict document
             var existingDocumentConflictId = id +"/conflicts/"+existingDoc.Etag;
