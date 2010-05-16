3c4b429be1b1ea0ee66f8eae186929603fe0428d
Ayende Rahien <ayende@ayende.com>
2010-05-16 15:02:43 +0100

    Will not hide documents from replication code

--- Bundles/Raven.Bundles.Replication/HideVirtuallyDeletedDocumentsReadTrigger.cs
+++ Bundles/Raven.Bundles.Replication/HideVirtuallyDeletedDocumentsReadTrigger.cs
@@ -9,6 +9,8 @@ namespace Raven.Bundles.Replication
         public override ReadVetoResult AllowRead(string key, JObject document, JObject metadata, ReadOperation operation,
                                                  TransactionInformation transactionInformation)
         {
+            if (ReplicationContext.IsInReplicationContext)
+                return ReadVetoResult.Allowed;
             JToken value;
             if (metadata.TryGetValue("Raven-Delete-Marker", out value))
                 return ReadVetoResult.Ignore;
