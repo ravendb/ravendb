b720e0de5a988917fec63cabf7451bc8cb7432df
Ayende Rahien <ayende@ayende.com>
2010-05-17 13:44:57 +0100

    Perf test

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -1,89 +1,70 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Threading;
 using Raven.Client.Document;
-using Raven.Client.Tests.Document;
-using Raven.Database;
-using Raven.Database.Backup;
-using Raven.Database.Indexing;
-using Raven.Database.Json;
-using Raven.Client;
+using System.IO;
+using System.Diagnostics;
 
-namespace Raven.Tryouts
+namespace RavenTestbed
 {
-	internal class Program
-	{
-	
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Directory.Exists("ravendb")) Directory.Delete("ravendb", true);
+            var documentStore = new DocumentStore
+            {
+                Configuration =
+                {
+                    DataDirectory = "ravendb"
+                }
+            };
+            documentStore.Initialise();
+            documentStore.DatabaseCommands.PutIndex("FooByName",
+                new IndexDefinition<Foo>()
+                {
+                    Map = docs => from doc in docs select new { Name = doc.Name }
+                });
 
-		public static void Main()
-		{
-			
-			try
-			{
-				using (var documentStore = new DocumentStore { Url = "http://localhost:8080" })
-				{
-					documentStore.Initialise();
+            var numberOfFoos = 10000;
 
-					using (var session = documentStore.OpenSession())
-					{
-						//Only add the index if there are not posts in the database, i.e. the 1st time this is run!!
-						if (session.Query<TagCloud.Post>().Count() == 0)
-						{
-							Console.WriteLine("First time usage, creating indexes");
+            // Insert Foos
+            using (var session = documentStore.OpenSession())
+            {
+                for (var i = 0; i < numberOfFoos; i++)
+                {
+                    var newFoo = new Foo { Name = i.ToString() };
+                    session.Store(newFoo);
+                }
+                session.SaveChanges();
+            }
 
-							documentStore.DatabaseCommands.PutIndex("TagCloud",
-																	new IndexDefinition
-																	{
-																		Map = @"from post in docs.Posts                                                                    
-                                                        from Tag in post.Tags
-                                                        select new { Tag, Count = 1 }",
+            // Query a single foo to wait for the index
+            using (var session = documentStore.OpenSession())
+            {
+                var foo = session.Query<Foo>("FooByName").Where("Name:1").WaitForNonStaleResults(TimeSpan.FromMinutes(1)).First();
+            }
+            Console.WriteLine("starting querying");
+            // Query all foos twice
+            for (var queryRun = 0; queryRun < 15; queryRun++)
+            {
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+                for (var i = 0; i < numberOfFoos; i++)
+                {
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var foo = session.Query<Foo>("FooByName").Where("Name:" + i.ToString()).First();
+                    }
+                }
+                stopWatch.Stop();
+                Console.WriteLine("{0}. run: Querying {1} Foos in {2} ({3}ms per query)", queryRun, numberOfFoos, stopWatch.Elapsed, stopWatch.ElapsedMilliseconds / numberOfFoos);
+            }
+        }
+    }
 
-																		Reduce = @"from result in results
-                                                        group result by result.Tag into g
-                                                        select new { Tag = g.Key, Count = g.Sum(x => (long)x.Count) }"
-																	});
-						}
-
-						session.Store(new TagCloud.Post { Title = "Title 1", Content = "something", PostedAt = RandomDateTime(), Tags = CreateTags("C#", ".NET") });
-						session.Store(new TagCloud.Post { Title = "Title 2", Content = "blah blah", PostedAt = RandomDateTime(), Tags = CreateTags("C#", ".NET") });
-						session.SaveChanges();
-
-						var tagCloud = session.Query<TagCloud.TagAndCount>("TagCloud").WaitForNonStaleResults().ToArray();
-
-						Console.WriteLine("\n\nBEFORE: TagCloud has " + tagCloud.Count() + " items :");
-						foreach (var tagCount in tagCloud)
-							Console.WriteLine("  " + tagCount);
-
-						session.Store(new TagCloud.Post { Title = "Title 1", Content = "something", PostedAt = RandomDateTime(), Tags = CreateTags("C#", ".NET") });
-						session.Store(new TagCloud.Post { Title = "Title 2", Content = "blah blah", PostedAt = RandomDateTime(), Tags = CreateTags("C#", ".NET") });
-						session.SaveChanges();
-
-						tagCloud = session.Query<TagCloud.TagAndCount>("TagCloud").WaitForNonStaleResults().ToArray();
-
-						Console.WriteLine("\n\nAFTER: TagCloud has " + tagCloud.Count() + " items :");
-						foreach (var tagCount in tagCloud)
-							Console.WriteLine("  " + tagCount);
-
-					}
-				}
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e);
-			}
-		}
-
-		private static List<string> CreateTags(params string [] arr)
-		{
-			return new List<string>(arr);
-		}
-
-		private static DateTime RandomDateTime()
-		{
-			return DateTime.Now;
-		}
-	}
-
+    public class Foo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
 }
