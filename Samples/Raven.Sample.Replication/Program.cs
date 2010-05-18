7e96f5b983d75c9d81f604094506f89fe1ee2c1d
Ayende Rahien <ayende@ayende.com>
2010-05-18 08:21:09 +0100

    Adding new replication sample Git-Ignoring files that we copy for the sample Added scripts to make the samples

---
+++ Samples/Raven.Sample.Replication/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Raven.Client.Document;
+using Raven.Client.Exceptions;
+using Raven.Database;
+
+namespace Raven.Sample.Replication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var documentStore1 = new DocumentStore { Url = "http://localhost:8080" }.Initialise();
+            var documentStore2 = new DocumentStore { Url = "http://localhost:8081" }.Initialise();
+
+            using(var session1 = documentStore1.OpenSession())
+            {
+                session1.Store(new User { Id = "users/ayende", Name = "Ayende" });
+                session1.SaveChanges();
+            }
+
+            using (var session2 = documentStore2.OpenSession())
+            {
+                session2.Store(new User { Id = "users/ayende", Name = "Oren" });
+                session2.SaveChanges();
+            }
+            Console.WriteLine("Conflicted documents set up");
+            Console.WriteLine("Please setup replicaton now...");
+            Console.ReadLine();
+
+            using (var session2 = documentStore2.OpenSession())
+            {
+                try
+                {
+                    session2.Load<User>("users/ayende");
+                }
+                catch (ConflictException e)
+                {
+                    Console.WriteLine("Choose which document you want to preserver:");
+                    var list = new List<JsonDocument>();
+                    for (int i = 0; i < e.ConflictedVersionIds.Length; i++)
+                    {
+                        var doc = documentStore2.DatabaseCommands.Get(e.ConflictedVersionIds[i]);
+                        list.Add(doc);
+                        Console.WriteLine("{0}. {1}", i, doc.DataAsJson.ToString(Formatting.None));
+                    }
+                    var select = int.Parse(Console.ReadLine());
+                    var resolved = list[select];
+                    documentStore2.DatabaseCommands.Put("users/ayende", null, resolved.DataAsJson, resolved.Metadata);
+                }
+            }
+
+            Console.WriteLine("Conflict resolved...");
+            Console.ReadLine();
+
+            using (var session2 = documentStore2.OpenSession())
+            {
+                var user = session2.Load<User>("users/ayende");
+                Console.WriteLine(user.Name);
+                user.Name = "Ayende Rahien";
+                session2.SaveChanges();
+            }
+        }
+    }
+
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
