e0f5676ff02cb1923c912dff8e0a1cd3305d7799
Ayende Rahien <Ayende@ayende.com>
2010-05-30 22:25:08 +0300

    Detect errors in building in the new script

--- release.ps1
+++ release.ps1
@@ -1,3 +1,13 @@
 .\psake.ps1 default.ps1 -task Upload
 
+if($lastexitcode -ne 0)
+{
+	throw 'Could not build OSS Version successfully'
+}
+
 .\psake.ps1 default.ps1 -task UploadCommercial
+
+if($lastexitcode -ne 0)
+{
+	throw 'Could not build Commercial Version successfully'
+}
