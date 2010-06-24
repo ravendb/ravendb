a8119341e514733cc81f50893ce3fe503f85e5b5
mckamey <stephen@mckamey.com>
2010-06-24 14:02:19 -0700

    Adding some tests for QueryBuilder

---
+++ Raven.Tests/Indexes/UsingQueryBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Raven.Database.Indexing;
+using Xunit;
+
+namespace Raven.Tests.Indexes
+{
+	public class UsingQueryBuilder
+	{
+		[Fact]
+		public void Can_parse_Analyzed_simple_single_term()
+		{
+			var query = QueryBuilder.BuildQuery("Name:SingleTerm");
+
+			Assert.Equal("Name:singleterm", query.ToString());
+		}
+
+		[Fact]
+		public void Can_parse_Analyzed_simple_phrase()
+		{
+			var query = QueryBuilder.BuildQuery("Name:\"Simple Phrase\"");
+
+			Assert.Equal("Name:\"simple phrase\"", query.ToString());
+		}
+
+		[Fact]
+		public void Can_parse_Analyzed_escaped_phrase()
+		{
+			var query = QueryBuilder.BuildQuery("Name:\"Escaped\\+\\-\\&\\|\\!\\(\\)\\{\\}\\[\\]\\^\\\"\\~\\*\\?\\:\\\\Phrase\"");
+
+			Assert.Equal("Name:\"escaped phrase\"", query.ToString());
+		}
+
+		[Fact]
+		public void Can_parse_NotAnalyzed_simple_single_term()
+		{
+			var query = QueryBuilder.BuildQuery("Name:[[SingleTerm]]");
+
+			Assert.Equal("Name:SingleTerm", query.ToString());
+		}
+
+		[Fact]
+		public void Can_parse_NotAnalyzed_simple_phrase()
+		{
+			var query = QueryBuilder.BuildQuery("Name:[[\"Simple Phrase\"]]");
+
+			Assert.Equal("Name:\"Simple Phrase\"", query.ToString());
+		}
+
+		[Fact]
+		public void Can_parse_NotAnalyzed_escaped_phrase()
+		{
+			var query = QueryBuilder.BuildQuery("Name:[[\"Escaped\\+\\-\\&\\|\\!\\(\\)\\{\\}\\[\\]\\^\\\"\\~\\*\\?\\:\\\\Phrase\"]]");
+
+			Assert.Equal("Name:\"Escaped\\+\\-\\&\\|\\!\\(\\)\\{\\}\\[\\]\\^\\\"\\~\\*\\?\\:\\\\Phrase\"", query.ToString());
+		}
+	}
+}
