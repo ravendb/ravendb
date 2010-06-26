8a4b0bff8846daf6413913da2d4ce6fda137f4c3
mckamey <stephen@mckamey.com>
2010-06-26 10:34:20 -0700

    Adding range query tests for DateTime and int

--- Raven.Tests/Indexes/UsingQueryBuilder.cs
+++ Raven.Tests/Indexes/UsingQueryBuilder.cs
@@ -56,5 +56,37 @@ namespace Raven.Tests.Indexes
 			// QueryBuilder should know how to properly unescape
 			Assert.Equal("Name:Escaped+-&|!(){}[]^\"~*?:\\Phrase", query.ToString());
 		}
+
+		[Fact]
+		public void Can_parse_LessThan_on_date()
+		{
+			var query = QueryBuilder.BuildQuery("Birthday:{NULL TO 20100515000000000}");
+
+			Assert.Equal("Birthday:{null TO 20100515000000000}", query.ToString());
+		}
+
+		[Fact]
+		public void Can_parse_LessThanOrEqual_on_date()
+		{
+			var query = QueryBuilder.BuildQuery("Birthday:[NULL TO 20100515000000000]");
+
+			Assert.Equal("Birthday:[null TO 20100515000000000]", query.ToString());
+		}
+
+		[Fact]
+		public void Can_parse_GreaterThan_on_int()
+		{
+			var query = QueryBuilder.BuildQuery("Age_Range:{0x00000003 TO NULL}");
+
+			Assert.Equal("Age_Range:{3 TO 2147483647}", query.ToString());
+		}
+
+		[Fact]
+		public void Can_parse_GreaterThanOrEqual_on_int()
+		{
+			var query = QueryBuilder.BuildQuery("Age_Range:[0x00000003 TO NULL]");
+
+			Assert.Equal("Age_Range:[3 TO 2147483647]", query.ToString());
+		}
 	}
 }
