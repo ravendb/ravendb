4edbe3a37063280d9cbdd2f4223653835485f185
mckamey <stephen@mckamey.com>
2010-06-26 14:55:18 -0700

    Swapping Regex with actual Lucene extensions to enable richer nested queries. Now all tests are passing.

--- Raven.Database/Indexing/QueryBuilder.cs
+++ Raven.Database/Indexing/QueryBuilder.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Text;
 using System.Text.RegularExpressions;
+
+using Lucene.Net.Analysis;
 using Lucene.Net.Analysis.Standard;
-using Lucene.Net.Index;
-using Lucene.Net.QueryParsers;
 using Lucene.Net.Search;
 using Version = Lucene.Net.Util.Version;
 
@@ -12,115 +12,56 @@ namespace Raven.Database.Indexing
 	[CLSCompliant(false)]
 	public static class QueryBuilder
 	{
-		static readonly Regex untokenizedQuery = new Regex(@"([+-]?)([\w\d_]+?):\[\[(.+?)\]\]", RegexOptions.Compiled);
-		static readonly Regex rangeQuery = new Regex(@"([+-]?)([\w\d_]+_Range?):(({|\[)[ \w\d.]+?(}|\]))", RegexOptions.Compiled);
-		static readonly Regex rangeValue = new Regex(@"({|\[) \s* (([\w\d]x[\w\d.]+)|(NULL)) \s* TO  \s* (([\w\d]x[\w\d.]+)|(NULL)) \s* (}|\])", RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace);
-
-		static readonly Regex hangingConditionAtStart = new Regex(@"^ \s* (AND|OR)", RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace);
-		static readonly Regex hangingConditionAtEnd = new Regex(@"(AND|OR) \s* $", RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace);
+		static readonly Regex untokenizedQuery = new Regex(@"([\w\d_]+?):(\[\[.+?\]\])", RegexOptions.Compiled);
 
 		public static Query BuildQuery(string query)
 		{
-			var untokenizedMatches = untokenizedQuery.Matches(query);
-			var rangeMatches = rangeQuery.Matches(query);
-		    var standardAnalyzer = new StandardAnalyzer(Version.LUCENE_29);
+			PerFieldAnalyzerWrapper analyzer = new PerFieldAnalyzerWrapper(new StandardAnalyzer(Version.LUCENE_29));
 		    try
 		    {
-                if (untokenizedMatches.Count == 0 && rangeMatches.Count == 0)
-                    return new QueryParser(Version.LUCENE_29, "", standardAnalyzer).Parse(query);
-                var sb = new StringBuilder(query);
-                var booleanQuery = new BooleanQuery();
-                AddUntokenizedTerms(untokenizedMatches, booleanQuery, sb, query);
-		    	AddRangeTerms(rangeMatches, booleanQuery, sb, query);
-                var remaining = sb.ToString();
-		    	remaining = hangingConditionAtStart.Replace(remaining, "");
-		    	remaining = hangingConditionAtEnd.Replace(remaining, "");
-				remaining = remaining.Trim();
-		    	if (remaining.Length > 0)
-                {
-                    booleanQuery.Add(new QueryParser(Version.LUCENE_29, "", standardAnalyzer).Parse(remaining), BooleanClause.Occur.SHOULD);
-                }
-                return booleanQuery;
-		    }
+				query = PreProcessUntokenizedTerms(analyzer, query);
+				return new RangeQueryParser(Version.LUCENE_29, "", analyzer).Parse(query);
+			}
 		    finally
 		    {
-		        standardAnalyzer.Close();
+		        analyzer.Close();
 		    }
 		}
 
-		private static void AddRangeTerms(MatchCollection rangeMatches, BooleanQuery booleanQuery, StringBuilder sb, string query)
+		/// <summary>
+		/// Detects untokenized fields and sets as NotAnalyzed in analyzer
+		/// </summary>
+		/// <param name="analyzer"></param>
+		/// <param name="sb"></param>
+		private static string PreProcessUntokenizedTerms(PerFieldAnalyzerWrapper analyzer, string query)
 		{
-			foreach (Match match in rangeMatches)
+			var untokenizedMatches = untokenizedQuery.Matches(query);
+			if (untokenizedMatches.Count < 1)
 			{
-				var fieldName = match.Groups[2].Value;
-				var rangeValueMatch = rangeValue.Match(match.Groups[3].Value);
-				if(rangeValueMatch.Success == false) // this is a date, no change is required
-					continue;
-
-				var inclusiveRange = rangeValueMatch.Groups[1].Value == "[";
-
-				var from = NumberUtil.StringToNumber(rangeValueMatch.Groups[2].Value);
-				var to = NumberUtil.StringToNumber(rangeValueMatch.Groups[5].Value);
-
-				NumericRangeQuery range = null;
-
-				if (from is int || to is int)
-					range = NumericRangeQuery.NewIntRange(fieldName, (int) (from ?? int.MinValue), (int) (to ?? int.MaxValue), inclusiveRange, inclusiveRange);
-
-				if (from is long || to is long)
-					range = NumericRangeQuery.NewLongRange(fieldName, (long)(from ?? long.MinValue), (long)(to ?? long.MaxValue), inclusiveRange, inclusiveRange);
-
-				if (from is double || to is double)
-					range = NumericRangeQuery.NewDoubleRange(fieldName, (double) (from ?? double.MinValue), (double) (to ?? double.MaxValue),
-					                                         inclusiveRange, inclusiveRange);
-			
-				if (from is float || to is float)
-					range = NumericRangeQuery.NewFloatRange(fieldName, (float)(from ?? float.MinValue), (float)(to ?? float.MaxValue), inclusiveRange, inclusiveRange);
-				
-				if(range == null)
-					throw new InvalidOperationException("Could not understand how to parse: " + match.Value);
-
-				booleanQuery.Add(range, GetOccur(query, match));
-
-				sb.Replace(match.Value, "");
+				return query;
 			}
-		}
 
-		private static void AddUntokenizedTerms(MatchCollection untokenizedMatches, BooleanQuery booleanQuery, StringBuilder sb, string query)
-		{
-			foreach (Match match in untokenizedMatches)
+			var sb = new StringBuilder(query);
+
+			// KeywordAnalyzer will not tokenize the values
+			KeywordAnalyzer keywordAnlyzer = new KeywordAnalyzer();
+
+			// process in reverse order to leverage match string indexes
+			for (int i=untokenizedMatches.Count; i>0; i--)
 			{
-				booleanQuery.Add(new TermQuery(new Term(match.Groups[2].Value, match.Groups[3].Value)), GetOccur(query, match));
-				sb.Replace(match.Value, "");
+				Match match = untokenizedMatches[i-1];
+
+				// specify that term for this field should not be tokenized
+				analyzer.AddAnalyzer(match.Groups[1].Value, keywordAnlyzer);
+
+				Group term = match.Groups[2];
+
+				// remove enclosing "[[" "]]" from term value (again in reverse order)
+				sb.Remove(term.Index+term.Length-2, 2);
+				sb.Remove(term.Index, 2);
 			}
-		}
 
-		private static BooleanClause.Occur GetOccur(string query, Match match)
-		{
-			BooleanClause.Occur occur;
-			switch (match.Groups[1].Value)
-			{
-				case "+":
-					occur = BooleanClause.Occur.MUST;
-					break;
-				case "-":
-					occur = BooleanClause.Occur.MUST_NOT;
-					break;
-				default:
-					var followedByAnd = query.Substring(match.Index + match.Length).TrimStart().StartsWith("AND",StringComparison.InvariantCultureIgnoreCase);
-					var prefixedByAnd = query.Substring(0, match.Index).TrimEnd().EndsWith("AND", StringComparison.InvariantCultureIgnoreCase);
-					var prefixedByNot = query.Substring(0, match.Index).TrimEnd().EndsWith("NOT", StringComparison.InvariantCultureIgnoreCase);
-
-					if(followedByAnd || prefixedByAnd)
-						occur = BooleanClause.Occur.MUST;
-					else if (prefixedByNot)
-						occur = BooleanClause.Occur.MUST_NOT;
-					else
-						occur = BooleanClause.Occur.SHOULD;
-
-					break;
-			}
-			return occur;
+			return sb.ToString();
 		}
 	}
 }
---
+++ Raven.Database/Indexing/RangeQueryParser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+using Lucene.Net.Analysis;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Version = Lucene.Net.Util.Version;
+
+namespace Raven.Database.Indexing
+{
+	[CLSCompliant(false)]
+	public class RangeQueryParser : QueryParser
+	{
+		static readonly Regex rangeValue = new Regex(@"^[\w\d]x[\w\d.]+$", RegexOptions.Compiled);
+		public RangeQueryParser(Version matchVersion, string f, Analyzer a)
+			: base(matchVersion, f, a)
+		{
+		}
+
+		/// <summary>
+		/// Detects numeric range terms and expands range expressions accordingly
+		/// </summary>
+		/// <param name="field"></param>
+		/// <param name="lower"></param>
+		/// <param name="upper"></param>
+		/// <param name="inclusive"></param>
+		/// <returns></returns>
+		protected override Query GetRangeQuery(string field, string lower, string upper, bool inclusive)
+		{
+			if (!rangeValue.IsMatch(lower) && !rangeValue.IsMatch(upper))
+			{
+				return base.GetRangeQuery(field, lower, upper, inclusive);
+			}
+
+			var from = NumberUtil.StringToNumber(lower);
+			var to = NumberUtil.StringToNumber(upper);
+
+			TypeCode numericType;
+
+			if (from != null)
+				numericType = Type.GetTypeCode(from.GetType());
+			else if (to != null)
+				numericType = Type.GetTypeCode(to.GetType());
+			else
+				numericType = TypeCode.Empty;
+
+			switch (numericType)
+			{
+				case TypeCode.Int32:
+				{
+					return NumericRangeQuery.NewIntRange(field, (int)(from ?? Int32.MinValue), (int)(to ?? Int32.MaxValue), inclusive, inclusive);
+				}
+				case TypeCode.Int64:
+				{
+					return NumericRangeQuery.NewLongRange(field, (long)(from ?? Int64.MinValue), (long)(to ?? Int64.MaxValue), inclusive, inclusive);
+				}
+				case TypeCode.Double:
+				{
+					return NumericRangeQuery.NewDoubleRange(field, (double)(from ?? Double.MinValue), (double)(to ?? Double.MaxValue), inclusive, inclusive);
+				}
+				case TypeCode.Single:
+				{
+					return NumericRangeQuery.NewFloatRange(field, (float)(from ?? Single.MinValue), (float)(to ?? Single.MaxValue), inclusive, inclusive);
+				}
+				default:
+				{
+					return base.GetRangeQuery(field, lower, upper, inclusive);
+				}
+			}
+		}
+	}
+}
