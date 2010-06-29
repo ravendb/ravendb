d7de79437e13f239e146e35560072cfa894cd011
Ayende Rahien <Ayende@ayende.com>
2010-06-29 09:16:52 +0300

    Add passing test

---
+++ Raven.Client.Tests/Bugs/SinglePropertyDocument.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+
+namespace Raven.Client.Tests.Bugs
+{
+	public class SinglePropertyDocument : BaseClientTest
+	{
+		[Fact]
+		public void CanSaveDocumentWithJustId()
+		{
+			using (var store = NewDocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					session.Store(new Email());
+					session.SaveChanges();
+				}
+
+				using (var session = store.OpenSession())
+				{
+					Assert.NotNull(session.Load<Email>("emails/1"));
+				}
+			}
+		}
+
+		public class Email
+		{
+			public string Id { get; set; }
+		}
+	}
+}
