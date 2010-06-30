ef13270aa82fb040a68e7929dcc3b2c9d18d9e2a
Ayende Rahien <Ayende@ayende.com>
2010-06-30 13:22:31 +0300

    Adding passing tests

--- Raven.Client.Tests/Bugs/CanDetectChanges.cs
+++ Raven.Client.Tests/Bugs/CanDetectChanges.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Xunit;
 
 namespace Raven.Client.Tests.Bugs
@@ -48,6 +49,72 @@ namespace Raven.Client.Tests.Bugs
 					session.SaveChanges();
 					Assert.False(session.HasChanges);
 					Assert.False(session.HasChanged(registration));
+				}
+			}
+		}
+
+		[Fact]
+		public void CanDetectChangesOnExistingItemFromQuery()
+		{
+			using (var store = NewDocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					session.Store(new ProjectingDates.Registration
+					{
+						RegisteredAt = new DateTime(2010, 1, 1)
+					});
+					session.SaveChanges();
+				}
+
+				using (var session = store.OpenSession())
+				{
+					var registration = session.LuceneQuery<ProjectingDates.Registration>()
+						.WaitForNonStaleResults()
+						.Single();
+					Assert.False(session.HasChanges);
+					Assert.False(session.HasChanged(registration));
+					registration.RegisteredAt = new DateTime(2010, 2, 1);
+					Assert.True(session.HasChanges);
+					Assert.True(session.HasChanged(registration));
+					session.SaveChanges();
+					Assert.False(session.HasChanges);
+					Assert.False(session.HasChanged(registration));
+				}
+			}
+		}
+
+		[Fact]
+		public void WillNotCreateNewDocuments()
+		{
+			using (var store = NewDocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					session.Store(new ProjectingDates.Registration
+					{
+						RegisteredAt = new DateTime(2010, 1, 1)
+					});
+					session.Store(new ProjectingDates.Registration
+					{
+						RegisteredAt = new DateTime(2010, 1, 1)
+					});
+					session.SaveChanges();
+				}
+
+				using (var session = store.OpenSession())
+				{
+					for (int i = 0; i < 15; i++)
+					{
+						session.LuceneQuery<ProjectingDates.Registration>().WaitForNonStaleResults().ToArray();
+
+						session.SaveChanges();
+					}
+				}
+
+				using (var session = store.OpenSession())
+				{
+					Assert.Equal(2, session.LuceneQuery<ProjectingDates.Registration>().WaitForNonStaleResults().Count());
 				}
 			}
 		}
