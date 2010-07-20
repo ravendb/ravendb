a7372fbf9dad9d058bb9cba44e656c7c1c161b52
Ayende Rahien <Ayende@ayende.com>
2010-07-20 10:24:14 +0300

    Fixing samples to work with the new unmerged option

--- Samples/MvcMusicStore/Start Raven.cmd
+++
@@ -1,5 +0,0 @@
-REM this scripts assumes the directory structure of the released zip
-REM  starts the server in debug mode in the current directory, meaning that
-REM  it will look for a default.raven file and load it if it exists
-copy ..\..\Server\RavenDB.* .\
-RavenDB.exe
--- Samples/MvcMusicStore/Start Raven.ps1
+++ Samples/MvcMusicStore/Start Raven.ps1
@@ -1,12 +1,23 @@
 # starts the server in debug mode in the shard1 and shard2 directories, 
 # meaning that they will use the specified configuration
 
-$raven = "..\..\Server\RavenDB.exe" #release path
+$raven = "..\..\" #release path
 if( (test-path $raven) -eq $false)
 {
-  $raven = "..\..\build\RavenDB.exe" #src path
+  $raven = "..\..\build" #src path
 }
 
 rd Data -force -recurse -erroraction silentlycontinue
-cp  $raven .\
-start .\RavenDB.exe
+
+cp $raven\Raven.Server.exe .\
+cp $raven\log4net.dll .\
+cp $raven\Newtonsoft.Json.dll .\
+cp $raven\Lucene.Net.dll .\
+cp $raven\ICSharpCode.NRefactory.dll .\
+cp $raven\Rhino.Licensing.dll .\
+cp $raven\Esent.Interop.dll .\
+cp $raven\Raven.Database.dll .\
+cp $raven\Raven.Storage.Managed.dll .\
+cp $raven\Raven.Storage.Esent.dll .\
+
+start .\Raven.Server.exe
