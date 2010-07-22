54168698ce9fa92b6be131cf67c701cd37c3d0ca
Ayende Rahien <Ayende@ayende.com>
2010-07-22 10:13:22 +0300

    Include etag in the data passed to Put triggers

--- Raven.Database/Plugins/AbstractPutTrigger.cs
+++ Raven.Database/Plugins/AbstractPutTrigger.cs
@@ -37,25 +37,28 @@ namespace Raven.Database.Plugins
             
         }
 
-        /// <summary>
-        ///  Allow the trigger to perform any logic after the document was put but still in the 
-        ///  same transaction as the put
-        ///  </summary><remarks>
-        ///  Any call to the provided <seealso cref="DocumentDatabase" /> instance will be done under the
-        ///  same transaction as the PUT operation.
-        ///  </remarks><param name="key">The document key</param><param name="document">The new document about to be put into Raven</param><param name="metadata">The new document metadata</param><param name="transactionInformation">The current transaction, if it exists</param>
-        public virtual void AfterPut(string key, JObject document, JObject metadata, TransactionInformation transactionInformation)
+    	/// <summary>
+    	///  Allow the trigger to perform any logic after the document was put but still in the 
+    	///  same transaction as the put
+    	///  </summary><remarks>
+    	///  Any call to the provided <seealso cref="DocumentDatabase" /> instance will be done under the
+    	///  same transaction as the PUT operation.
+    	///  </remarks><param name="key">The document key</param><param name="document">The new document about to be put into Raven</param><param name="metadata">The new document metadata</param>
+    	/// <param name="etag">The etag of the just put document</param>
+    	/// <param name="transactionInformation">The current transaction, if it exists</param>
+    	public virtual void AfterPut(string key, JObject document, JObject metadata, Guid etag, TransactionInformation transactionInformation)
         {
             
         }
 
-        /// <summary>
-        ///  Allow the trigger to perform any logic _after_ the transaction was committed.
-        ///  For example, by notifying interested parties.
-        ///  </summary><remarks>
-        ///  This method SHOULD NOT modify either the document or the metadata
-        ///  </remarks><param name="key">The document key</param><param name="document">The document that was put into Raven</param><param name="metadata">The document metadata</param>
-        public virtual void AfterCommit(string key, JObject document, JObject metadata)
+    	/// <summary>
+    	///  Allow the trigger to perform any logic _after_ the transaction was committed.
+    	///  For example, by notifying interested parties.
+    	///  </summary><remarks>
+    	///  This method SHOULD NOT modify either the document or the metadata
+    	///  </remarks><param name="key">The document key</param><param name="document">The document that was put into Raven</param><param name="metadata">The document metadata</param>
+    	/// <param name="etag">The etag of the just put document</param>
+    	public virtual void AfterCommit(string key, JObject document, JObject metadata, Guid etag)
         {
             
         }
