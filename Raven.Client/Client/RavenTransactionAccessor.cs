670fcf0201899f13f24d38d5ce2fde017364ed3e
Ayende Rahien <Ayende@ayende.com>
2010-07-23 16:53:03 +0300

    Fixing build for 3.5

--- Raven.Client/Client/RavenTransactionAccessor.cs
+++ Raven.Client/Client/RavenTransactionAccessor.cs
@@ -10,6 +10,7 @@ namespace Raven.Client.Client
 {
 	public static class RavenTransactionAccessor
 	{
+#if !NET_3_5
 		private static readonly ThreadLocal<Stack<TransactionInformation>> currentRavenTransactions = new ThreadLocal<Stack<TransactionInformation>>(() => new Stack<TransactionInformation>());
 
 		public static IDisposable StartTransaction()
@@ -26,11 +27,13 @@ namespace Raven.Client.Client
 			});
 			return new DisposableAction(() => currentRavenTransactions.Value.Pop());
 		}
-
+#endif
 		public static TransactionInformation GetTransactionInformation()
 		{
+			#if !NET_3_5
 			if (currentRavenTransactions.Value.Count >0)
 				return currentRavenTransactions.Value.Peek();
+			#endif
 			if (Transaction.Current == null)
 				return null;
 			return new TransactionInformation
