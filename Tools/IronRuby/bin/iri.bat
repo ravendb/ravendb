ca3c2e2d077951227937763c9af1795bdf077560
Ayende Rahien <Ayende@ayende.com>
2010-08-06 14:58:45 +0200

    Adding the actual binaries for IronRuby, were filtered out by .gitignore

---
+++ Tools/IronRuby/bin/iri.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+
+set IR_CMD="%~dp0ir.exe"
+if defined DLR_ROOT (
+  REM - This is a dev environment. See http://wiki.github.com/ironruby/ironruby
+  set IR_CMD="%DLR_ROOT%\bin\Debug\ir.exe"
+)
+
+%IR_CMD% "%~dp0ri" %*
