e54241bbe6c99efab787e055ca2e3679cff10578
Ayende Rahien <Ayende@ayende.com>
2010-08-07 18:14:16 +0200

    Changing the way we handle indexing of new / modified documents to use etag instead of explicit task to do so

--- Raven.Database/Indexing/TaskExecuter.cs
+++ Raven.Database/Indexing/TaskExecuter.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Diagnostics;
+using System.Linq;
 using log4net;
 using Raven.Database.Extensions;
+using Raven.Database.Json;
 using Raven.Database.Storage;
 using Raven.Database.Tasks;
 
@@ -47,6 +49,18 @@ namespace Raven.Database.Indexing
 						}
 					});
 					context.PerformanceCounters.IncrementProcessedTask(tasks);
+					transactionalStorage.Batch(actions =>
+					{
+						foreach (var indexesStat in actions.Indexing.GetIndexesStats())
+						{
+							if (!actions.Tasks.IsIndexStale(indexesStat.Name, null)) 
+								continue;
+							// in order to ensure fairness, we only process one stale index 
+							// then move to process pending tasks, then process more staleness
+							if (IndexDocuments(actions, indexesStat.Name, indexesStat.LastIndexedEtag))
+								break;
+						}
+					});
 				}
 				catch (Exception e)
 				{
@@ -55,6 +69,48 @@ namespace Raven.Database.Indexing
 				if (foundWork == false)
 					context.WaitForWork(TimeSpan.FromSeconds(1));
 			}
+		}
+
+		public bool IndexDocuments(IStorageActionsAccessor actions, string index, Guid etagToIndexFrom)
+		{
+
+			var viewGenerator = context.IndexDefinitionStorage.GetViewGenerator(index);
+			if (viewGenerator == null)
+				return false; // index was deleted, probably
+
+			var jsonDocs = actions.Documents.GetDocumentsAfter(etagToIndexFrom)
+				.Where(x => x != null)
+				.ToArray();
+
+			if(jsonDocs.Length == 0)
+				return false;
+			
+			try
+			{
+				log.DebugFormat("Indexing {0} documents for index: {1}", jsonDocs.Length, index);
+
+				var failureRate = actions.Indexing.GetFailureRate(index);
+				if (failureRate.IsInvalidIndex)
+				{
+					log.InfoFormat("Skipped indexing documents for index: {0} because failure rate is too high: {1}",
+					                  index,
+					                  failureRate.FailureRate);
+					return false;
+				}
+
+				context.IndexStorage.Index(index, viewGenerator, jsonDocs.Select(x => JsonToExpando.Convert(x.ToJson())),
+				                           context, actions);
+
+				var last = jsonDocs.Last();
+				actions.Indexing.UpdateLastIndexed(index, last.Etag, last.LastModified);
+				return true;
+			}
+			catch (Exception e)
+			{
+				log.WarnFormat(e, "Failed to index documents for index: {0}", index);
+				return false;
+			}
+			
 		}
 	}
 }
