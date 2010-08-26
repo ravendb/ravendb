4689aa1445c9d3f69435bd86167872e26844d335
Ayende Rahien <Ayende@ayende.com>
2010-08-26 21:48:10 +0300

    Adding passing test

---
+++ Raven.Client.Tests/Bugs/Patching.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using Raven.Database.Data;
+using Raven.Database.Json;
+using Xunit;
+
+namespace Raven.Client.Tests.Bugs
+{
+	public class Patching : BaseClientTest
+	{
+		public class Post
+		{
+			public string Id { get; set; }
+			public List<Comment> Comments { get; set; }
+		}
+
+		public class Comment
+		{
+			public string AuthorId { get; set; }
+		}
+
+		[Fact]
+		public void CanModifyValue()
+		{
+			using(var store = NewDocumentStore())
+			{
+				using(var s = store.OpenSession())
+				{
+					s.Store(new Post
+					{
+						Comments = new List<Comment>
+						{
+							new Comment{ AuthorId = "authors/123"}
+						}
+					});
+					s.SaveChanges();
+				}
+
+				store.DatabaseCommands.Batch(new[]
+				{
+					new PatchCommandData
+					{
+						Key = "posts/1",
+						Patches = new PatchRequest[]
+						{
+							new PatchRequest
+							{
+								Name = "Comments",
+								Type = "modify",
+								Position = 0,
+								Nested = new PatchRequest[]
+								{
+									new PatchRequest
+									{
+										Name = "AuthorId",
+										Type = "Set",
+										Value = "authors/456"
+
+									},
+								}
+							},
+						}
+					}
+				});
+
+				using (var s = store.OpenSession())
+				{
+					Assert.Equal("authors/456", s.Load<Post>("posts/1").Comments[0].AuthorId);
+				}
+			}
+		}
+	}
+}
