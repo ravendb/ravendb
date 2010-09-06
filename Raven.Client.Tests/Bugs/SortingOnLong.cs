8515240221cc2981b86f3296b4a08a3a1482f6d1
fschwiet <fschwiet@gmail.com>
2010-09-06 13:00:46 -0700

    adding failing OrderByDescending tests

--- Raven.Client.Tests/Bugs/SortingOnLong.cs
+++ Raven.Client.Tests/Bugs/SortingOnLong.cs
@@ -1,3 +1,4 @@
+using System;
 using Raven.Database.Indexing;
 using Xunit;
 using System.Linq;
@@ -6,12 +7,11 @@ namespace Raven.Client.Tests.Bugs
 {
 	public class SortingOnLong : LocalClientTest
 	{
-		[Fact]
-		public void CanSortOnLong()
+		void UsingDatabaseOfFoos(Action<IDocumentSession> action)
 		{
-			using(var store = NewDocumentStore())
+			using (var store = NewDocumentStore())
 			{
-				using(var session = store.OpenSession())
+				using (var session = store.OpenSession())
 				{
 					session.Store(new Foo
 					{
@@ -32,28 +32,73 @@ namespace Raven.Client.Tests.Bugs
 				}
 
 				store.DatabaseCommands.PutIndex("long",
-				                                new IndexDefinition
-				                                {
-				                                	Map = "from doc in docs select new { doc.Value }",
-													SortOptions = {{"Value", SortOptions.Long}}
-				                                });
+												new IndexDefinition
+												{
+													Map = "from doc in docs select new { doc.Value }",
+													SortOptions = { { "Value", SortOptions.Long } }
+												});
 
 				using (var session = store.OpenSession())
 				{
-					var foos = session.LuceneQuery<Foo>("long")
-						.WaitForNonStaleResults()
-						.OrderBy("Value_Range")
-						.ToList();
-
-					Assert.Equal(3, foos.Count);
-
-					Assert.Equal(25, foos[0].Value);
-					Assert.Equal(3147483647, foos[1].Value);
-					Assert.Equal(30000000000, foos[2].Value);
+					action(session);
 				}
 			}
 		}
 
+		[Fact]
+		public void CanSortOnLong()
+		{
+			UsingDatabaseOfFoos(delegate(IDocumentSession session)
+			{
+				var foos1 = session.LuceneQuery<Foo>("long")
+					.WaitForNonStaleResults()
+					.OrderBy("Value_Range")
+					.ToList();
+
+				Assert.Equal(3, foos1.Count);
+
+				Assert.Equal(25, foos1[0].Value);
+				Assert.Equal(3147483647, foos1[1].Value);
+				Assert.Equal(30000000000, foos1[2].Value);
+			});
+		}
+
+		[Fact(Skip = "Fails with foos1[0]==25, not 30000000000")]
+		public void CanSortOnLongDescending()
+		{
+			UsingDatabaseOfFoos(delegate(IDocumentSession session)
+			{
+				var foos1 = session.LuceneQuery<Foo>("long")
+					.WaitForNonStaleResults()
+					.OrderBy("-Value_Range")
+					.ToList();
+
+				Assert.Equal(3, foos1.Count);
+
+				Assert.Equal(30000000000, foos1[0].Value);
+				Assert.Equal(3147483647, foos1[1].Value);
+				Assert.Equal(25, foos1[2].Value);
+			});
+		}
+
+		[Fact(Skip = "Fails with foos1[0]==25, not 30000000000")]
+		public void CanLinqSortOnLongDescending()
+		{
+			UsingDatabaseOfFoos(delegate(IDocumentSession session)
+			{
+				var foos1 = session.Query<Foo>("long")
+					.Customize(q => q.WaitForNonStaleResults())
+					.OrderByDescending(f => f.Value)
+					.ToList();
+
+				Assert.Equal(3, foos1.Count);
+
+				Assert.Equal(30000000000, foos1[0].Value);
+				Assert.Equal(3147483647, foos1[1].Value);
+				Assert.Equal(25, foos1[2].Value);
+			});
+		}
+		
 		public class Foo
 		{
 			public string Id { get; set; }
