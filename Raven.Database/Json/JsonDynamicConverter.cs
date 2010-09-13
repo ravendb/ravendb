2cf6be1532e09ad21fe8b1fdbc57f5e7b09b5ef2
Ayende Rahien <Ayende@ayende.com>
2010-09-13 11:56:30 +0200

    Fixing usage of dynamic objects in RavenDB client API

---
+++ Raven.Database/Json/JsonDynamicConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Dynamic;
+using System.Linq.Expressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Raven.Database.Linq;
+
+namespace Raven.Database.Json
+{
+	public class JsonDynamicConverter : JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			var dynamicValue = ((IDynamicMetaObjectProvider) value).GetMetaObject(Expression.Constant(value));
+
+			writer.WriteStartObject();
+			foreach (var dynamicMemberName in dynamicValue.GetDynamicMemberNames())
+			{
+				writer.WritePropertyName(dynamicMemberName);
+				var memberValue = DynamicUtil.GetValueDynamically(value, dynamicMemberName);
+				if(memberValue == null || memberValue is ValueType || memberValue is string)
+					writer.WriteValue(memberValue);
+				else
+					serializer.Serialize(writer, memberValue);
+			}
+			writer.WriteEndObject();
+
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			return new DynamicJsonObject((JObject)JToken.ReadFrom(reader));
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			return typeof(IDynamicMetaObjectProvider).IsAssignableFrom(objectType);
+		}
+	}
+}
