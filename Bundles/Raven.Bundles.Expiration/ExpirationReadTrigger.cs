c94f76b296185c68b0e3de686ff1e7a8b0771e54
Ayende Rahien <Ayende@ayende.com>
2010-09-21 18:41:03 +0200

    Adding new Expiration bundle

---
+++ Bundles/Raven.Bundles.Expiration/ExpirationReadTrigger.cs
@@ -0,0 +1,26 @@
+using System;
+using Newtonsoft.Json.Linq;
+using Raven.Database;
+using Raven.Database.Plugins;
+
+namespace Raven.Bundles.Expiration
+{
+    public class ExpirationReadTrigger : AbstractReadTrigger
+    {
+        public const string RavenExpirationDate = "Raven-Expiration-Date"; 
+
+        public override ReadVetoResult AllowRead(string key, JObject document, JObject metadata, ReadOperation operation,
+                                                 TransactionInformation transactionInformation)
+        {
+            var property = metadata.Property(RavenExpirationDate);
+            if (property == null)
+                return ReadVetoResult.Allowed;
+            var dateTime = property.Value.Value<DateTime>();
+            if(dateTime > GetCurrentUtcDate())
+                return ReadVetoResult.Allowed;
+            return ReadVetoResult.Ignore;
+        }
+
+        public static Func<DateTime> GetCurrentUtcDate = () => DateTime.UtcNow;
+    }
+}
