2055cbdd5b8f8de79b3f79509f8d259053765589
Ayende Rahien <Ayende@ayende.com>
2010-09-27 02:17:17 +0200

    Moving task to proper place

--- Raven.Database/Plugins/Builtins/DeleteTemporaryIndexes.cs
+++ Raven.Database/Plugins/Builtins/DeleteTemporaryIndexes.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using Raven.Database.Extensions;
 
-namespace Raven.Database.Plugins
+namespace Raven.Database.Plugins.Builtins
 {
     public class DeleteTemporaryIndexes : IStartupTask
     {
         public void Execute(DocumentDatabase database)
         {
             database.IndexDefinitionStorage.IndexNames.Where(x => x.StartsWith("Temp_"))
-                .Apply(x => database.DeleteIndex(x));
+                .Apply(database.DeleteIndex);
         }
     }
 }
