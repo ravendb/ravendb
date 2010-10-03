171803b5e6046e5fcc8e7b0ac52f6899c22dd197
Ayende Rahien <Ayende@ayende.com>
2010-10-03 16:11:50 +0200

    Fixing escape of url parameters

--- Raven.Database/Data/IndexQuery.cs
+++ Raven.Database/Data/IndexQuery.cs
@@ -98,7 +98,7 @@ namespace Raven.Database.Data
             }
             if (Cutoff != null)
             {
-                path = path + "&cutOff=" + Uri.EscapeDataString(Cutoff.Value.ToString("o", CultureInfo.InvariantCulture));
+                path = path + "&cutOff=" + Uri.EscapeUriString(Uri.EscapeDataString(Cutoff.Value.ToString("o", CultureInfo.InvariantCulture)));
             }
 			var vars = GetCustomQueryStringVariables();
 
@@ -107,7 +107,7 @@ namespace Raven.Database.Data
 				path += vars.StartsWith("&") ? vars : ("&" + vars);
 			}
 		
-			return Uri.EscapeUriString(path);
+			return path;
 		}
 
 		/// <summary>
