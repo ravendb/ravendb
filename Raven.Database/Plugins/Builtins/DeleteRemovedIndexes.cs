88e8ff99f88ef58aadddb1cab02cc109d0f548c4
Ayende Rahien <ayende@ayende.com>
2010-10-05 20:36:23 +0200

    Delete index record and index directory if the index definition directory was removed

---
+++ Raven.Database/Plugins/Builtins/DeleteRemovedIndexes.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+
+namespace Raven.Database.Plugins.Builtins
+{
+    public class DeleteRemovedIndexes : IStartupTask
+    {
+        public void Execute(DocumentDatabase database)
+        {
+            database.TransactionalStorage.Batch(actions =>
+            {
+                var indexNames = actions.Indexing.GetIndexesStats().Select(x => x.Name).ToList();
+                foreach (var indexName in indexNames)
+                {
+                    if(database.IndexDefinitionStorage.Contains(indexName) )
+                        continue;
+
+                    // index is not found on disk, better kill for good
+                    // Even though technically we are running into a situation that is considered to be corrupt data
+                    // we can safely recover from it by removing the other parts of the index.
+                    database.IndexStorage.DeleteIndex(indexName);
+                    actions.Indexing.DeleteIndex(indexName);
+                }
+            });
+        }
+    }
+}
