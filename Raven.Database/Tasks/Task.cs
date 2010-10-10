b5485b21af10e0fee8b8afa13151f973d94d6052
Ayende Rahien <ayende@ayende.com>
2010-10-10 18:21:07 +0200

    Can resolve external tasks as well now

--- Raven.Database/Tasks/Task.cs
+++ Raven.Database/Tasks/Task.cs
@@ -43,7 +43,7 @@ namespace Raven.Database.Tasks
 
 		public static Task ToTask(string taskType, byte[] task)
 		{
-			var type = typeof(Task).Assembly.GetType(taskType);
+			var type = typeof(Task).Assembly.GetType(taskType) ?? System.Type.GetType(taskType);
 			return (Task) new JsonSerializer().Deserialize(new BsonReader(new MemoryStream(task)), type);
 		}
 
