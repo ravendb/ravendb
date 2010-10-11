5d08747ee53f02f94cf773db31beef6f827e606a
Ayende Rahien <Ayende@ayende.com>
2010-10-11 17:59:26 +0200

    object -> dynamic - fixing file split issue

--- Raven.Database/Linq/TranslatorFunc.cs
+++ Raven.Database/Linq/TranslatorFunc.cs
@@ -6,5 +6,5 @@ namespace Raven.Database.Linq
     /// 	Defining the translator function for a set of results
     ///     about to sent to the user and apply final processing
     /// </summary>
-    public delegate IEnumerable<object> TranslatorFunc(ITranslatorDatabaseAccessor database, IEnumerable<object> source);
+    public delegate IEnumerable<dynamic> TranslatorFunc(ITranslatorDatabaseAccessor database, IEnumerable<dynamic> source);
 }
