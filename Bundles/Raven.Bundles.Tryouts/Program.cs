5c236040548ad588adf1e54aa59a6f9f1ee67920
Ayende Rahien <Ayende@ayende.com>
2010-10-11 23:50:02 +0200

    Adding try out project

---
+++ Bundles/Raven.Bundles.Tryouts/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Raven.Bundles.Tests.Expiration;
+
+namespace Raven.Bundles.Tryouts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.Write(i+"\r");
+                using (var x = new Expiration())
+                {
+                    x.After_expiry_passed_document_will_be_physically_deleted();
+                }
+            }
+        }
+    }
+}
