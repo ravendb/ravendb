e06811f6b46cccb0596b4e2b4711f7771ba811ac
Ayende Rahien <ayende@ayende.com>
2010-10-18 16:50:44 +0200

    Refactored managed storage into two projects. * Raven.Munin - transactional, managed storage. * Raven.Storage.Managed - using Raven.Munin to build storage specific for RavenDB.

---
+++ Optimize.cs
@@ -0,0 +1,90 @@
+using System;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Raven.Tests.ManagedStorage.Impl
+{
+    public class Optimize : SimpleFileTest
+    {
+        [Fact]
+        public void AfterManyModificationsFileSizeWillGoDownOnCommit()
+        {
+            var txId = Guid.NewGuid();
+            for (int i = 0; i < 16; i++)
+            {
+                persistentDictionary.Put(JToken.FromObject(i), new byte[512], txId);
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                persistentDictionary.Remove(JToken.FromObject(i), txId);
+            }
+
+
+            Commit(txId);
+
+            var oldSize = persistentSource.Log.Length;
+            PerformIdleTasks();
+
+            Assert.True(oldSize > persistentSource.Log.Length);
+        }
+
+        [Fact]
+        public void AfterOptimizeCommittedDataIsStillThere()
+        {
+            var txId = Guid.NewGuid();
+            for (int i = 0; i < 16; i++)
+            {
+                persistentDictionary.Put(JToken.FromObject(i), new byte[512], txId);
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                persistentDictionary.Remove(JToken.FromObject(i), txId);
+            }
+
+            persistentDictionary.Put(JToken.FromObject("a"), new byte[512], txId);
+
+
+            Commit(txId);
+            var oldSize = persistentSource.Log.Length;
+            PerformIdleTasks();
+
+            Assert.True(oldSize > persistentSource.Log.Length);
+
+            Assert.NotNull(
+                persistentDictionary.Read(JToken.FromObject("a"), Guid.NewGuid())
+                );
+        }
+
+
+        [Fact]
+        public void AfterOptimizeUnCommittedDataIsStillThere()
+        {
+            var txId = Guid.NewGuid();
+            for (int i = 0; i < 16; i++)
+            {
+                persistentDictionary.Put(JToken.FromObject(i), new byte[512], txId);
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                persistentDictionary.Remove(JToken.FromObject(i), txId);
+            }
+
+            var txId2 = Guid.NewGuid();
+            persistentDictionary.Put(JToken.FromObject("a"), new byte[512], txId2);
+
+
+            Commit(txId);
+            var oldSize = persistentSource.Log.Length;
+            PerformIdleTasks();
+
+            Assert.True(oldSize > persistentSource.Log.Length);
+
+            Assert.NotNull(
+                persistentDictionary.Read(JToken.FromObject("a"), txId2)
+                );
+        }
+    }
+}
