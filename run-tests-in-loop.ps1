1262c6eefb61b46eba41e093d247458e63c604c1
Ayende Rahien <ayende@ayende.com>
2010-10-23 02:15:07 +0200

    Adding a way to run all the tests in a loop, to ensure we don't have concurrency issues.

---
+++ run-tests-in-loop.ps1
@@ -0,0 +1,23 @@
+for($i = 0; $i -le 250; $i++) 
+{
+    Write-Host "Starting test #$i" -foregroundcolor Cyan
+    ..\Tools\XUnit\xunit.console.clr4.exe "Raven.Munin.Tests.dll" 
+    if ($lastExitCode -ne 0)
+      { throw "err" }
+    ..\Tools\XUnit\xunit.console.clr4.exe "Raven.Tests.dll" 
+    if ($lastExitCode -ne 0)
+      { throw "err" }
+    ..\Tools\XUnit\xunit.console.clr4.exe "Raven.Scenarios.dll" 
+    if ($lastExitCode -ne 0)
+      { throw "err" }
+    ..\Tools\XUnit\xunit.console.clr4.exe "Raven.Client.Tests.dll" 
+    if ($lastExitCode -ne 0)
+      { throw "err" }
+    ..\Tools\XUnit\xunit.console.clr4.exe "Raven.Client.VisualBasic.Tests.dll" 
+    if ($lastExitCode -ne 0)
+      { throw "err" }
+    ..\Tools\XUnit\xunit.console.clr4.exe "Raven.Bundles.Tests.dll" 
+    if ($lastExitCode -ne 0)
+      { throw "err" }
+    
+}
