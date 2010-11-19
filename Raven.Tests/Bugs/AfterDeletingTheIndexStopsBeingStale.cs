457ceb0d2c8d0e5706430b140f4568e663363b4f
Ayende Rahien <ayende@ayende.com>
2010-11-19 15:09:07 +0000

    Adding failing test showing a problem with managed storage

---
+++ Raven.Tests/Bugs/AfterDeletingTheIndexStopsBeingStale.cs
@@ -0,0 +1,42 @@
+using System;
+using Xunit;
+using System.Linq;
+
+namespace Raven.Tests.Bugs
+{
+    public class AfterDeletingTheIndexStopsBeingStale : LocalClientTest
+    {
+        [Fact]
+        public void Deletion()
+        {
+            using (var store = NewDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        session.Store(new User { Name = "Ayende" });
+                    }
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var users = session.Query<User>().Customize(x => x.WaitForNonStaleResults()).ToList();
+                    Assert.NotEmpty(users);
+                    foreach (var user in users)
+                    {
+                        session.Delete(user);
+                    }
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var users = session.Query<User>().Customize(x => x.WaitForNonStaleResults(TimeSpan.FromMinutes(20))).ToList();
+                    Assert.Empty(users);
+                }
+            }
+        }
+    }
+}
