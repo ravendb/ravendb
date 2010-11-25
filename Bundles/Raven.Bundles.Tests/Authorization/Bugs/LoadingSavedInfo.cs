f2791788bf4226adc4ff51f99a5b55e0ea6165dc
Ayende Rahien <Ayende@ayende.com>
2010-11-25 19:17:58 +0200

    More tests

--- Bundles/Raven.Bundles.Tests/Authorization/Bugs/LoadingSavedInfo.cs
+++ Bundles/Raven.Bundles.Tests/Authorization/Bugs/LoadingSavedInfo.cs
@@ -2,6 +2,7 @@ using Raven.Bundles.Authorization.Model;
 using Raven.Bundles.Tests.Versioning;
 using Raven.Client.Authorization;
 using Xunit;
+using System.Linq;
 
 namespace Raven.Bundles.Tests.Authorization.Bugs
 {
@@ -53,7 +54,64 @@ namespace Raven.Bundles.Tests.Authorization.Bugs
                 }
 
             }
-            
+
+            using (var s = store.OpenSession())
+            {
+                s.SecureFor(UserId, "Company/Bid");
+
+                var load = s.Load<Company>(company.Id);
+                Assert.NotNull(load);
+                Assert.Equal("other 14", load.Name);
+            }
+        }
+
+        [Fact]
+        public void BugWhenSavingDocumentWithPreviousAuthorization_WithQuery()
+        {
+            var company = new Company
+            {
+                Name = "Hibernating Rhinos"
+            };
+            using (var s = store.OpenSession())
+            {
+                s.Store(new AuthorizationUser
+                {
+                    Id = UserId,
+                    Name = "Ayende Rahien",
+                });
+
+                s.Store(company);
+
+                s.SetAuthorizationFor(company, new DocumentAuthorization
+                {
+                    Permissions =
+                                       {
+                                               new DocumentPermission
+                                               {
+                                                       User = UserId,
+                                                       Allow = true,
+                                                       Operation = "Company/Bid"
+                                               }
+                                       }
+                });
+
+                s.SaveChanges();
+            }
+
+            for (int i = 0; i < 15; i++)
+            {
+                using (var s = store.OpenSession())
+                {
+                    s.SecureFor(UserId, "Company/Bid");
+
+                    var c = s.Query<Company>().Customize(x => x.WaitForNonStaleResults()).First();
+                    c.Name = "other " + i;
+
+                    s.SaveChanges();
+                }
+
+            }
+
             using (var s = store.OpenSession())
             {
                 s.SecureFor(UserId, "Company/Bid");
