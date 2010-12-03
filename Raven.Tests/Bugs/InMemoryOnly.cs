52b87446cbd0ded1daa5a41b28705d98ed7a45a8
Ayende Rahien <ayende@ayende.com>
2010-12-03 12:39:21 +1100

    Adding passing test

---
+++ Raven.Tests/Bugs/InMemoryOnly.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using Raven.Client.Client;
+using Raven.Http;
+using Xunit;
+
+namespace Raven.Tests.Bugs
+{
+    public class InMemoryOnly
+    {
+        [Fact]
+        public void InMemoryDoesNotCreateDataDir()
+        {
+            if(Directory.Exists("Data"))
+                Directory.Delete("Data", true);
+
+            NonAdminHttp.EnsureCanListenToWhenInNonAdminContext(8080);
+            using (var store = new EmbeddableDocumentStore
+            {
+                RunInMemory = true,
+                UseEmbeddedHttpServer = true,
+                Configuration = 
+                {
+                    Port = 8000,
+                    RunInMemory = true
+                }
+            })
+            {
+                store.Initialize();
+
+                Assert.False(Directory.Exists("Data"));
+            }
+        }
+    }
+}
