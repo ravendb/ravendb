75871083264679af0246dd08de89432edf952cf0
Ayende Rahien <ayende@ayende.com>
2010-12-21 19:13:43 +0200

    More passing tests

--- Raven.Tests/Bugs/Indexing/CanIndexAllDocsWhenThereAreMoreDocsThanTheBatchSize.cs
+++ Raven.Tests/Bugs/Indexing/CanIndexAllDocsWhenThereAreMoreDocsThanTheBatchSize.cs
@@ -7,7 +7,7 @@ namespace Raven.Tests.Bugs.Indexing
 	public class CanIndexAllDocsWhenThereAreMoreDocsThanTheBatchSize : LocalClientTest
 	{
 		[Fact]
-		public void WillIndexAll()
+		public void WillIndexAllWhenCreatingIndex()
 		{
 			using (var store = NewDocumentStore())
 			{
@@ -31,6 +31,38 @@ namespace Raven.Tests.Bugs.Indexing
 				using (var session = store.OpenSession())
 				{
 					var users = session.Query<User>("test").Customize(x=>x.WaitForNonStaleResults()).ToArray();
+
+					Assert.Equal(15, users.Length);
+				}
+			}
+		}
+
+		[Fact]
+		public void WillIndexAllAfterCreatingIndex()
+		{
+			using (var store = NewDocumentStore())
+			{
+				store.DocumentDatabase.Configuration.MaxNumberOfItemsToIndexInSignleBatch = 3;
+
+			
+				store.DatabaseCommands.PutIndex("test",
+												new IndexDefinition
+												{
+													Map = "from doc in docs select new { doc.Name}"
+												});
+
+				using (var session = store.OpenSession())
+				{
+					for (int i = 0; i < 15; i++)
+					{
+						session.Store(new User { Name = "1" });
+					}
+					session.SaveChanges();
+				}
+				
+				using (var session = store.OpenSession())
+				{
+					var users = session.Query<User>("test").Customize(x => x.WaitForNonStaleResults()).ToArray();
 
 					Assert.Equal(15, users.Length);
 				}
