ffe21397363baf634026448bb75a8a837d06677b
Ayende Rahien <ayende@ayende.com>
2010-12-29 19:38:17 +0200

    Adding support for SL client access policy

---
+++ Raven.Database/Server/Responders/ClientAccessPolicy.cs
@@ -0,0 +1,39 @@
+using System;
+using Raven.Http.Abstractions;
+using Raven.Http.Extensions;
+
+namespace Raven.Database.Server.Responders
+{
+	public class ClientAccessPolicy : RequestResponder
+	{
+		public override string UrlPattern
+		{
+			get { return "^/clientaccesspolicy.xml$"; }
+		}
+
+		public override string[] SupportedVerbs
+		{
+			get { return new[]{"GET"}; }
+		}
+
+		public override void Respond(IHttpContext context)
+		{
+			context.Response.Headers["ETag"] = typeof (ClientAccessPolicy).FullName;
+			context.Response.SetPublicCachability();
+			context.Response.ContentType = "text/xml";
+			context.Write(@"<?xml version='1.0' encoding='utf-8'?>
+<access-policy>
+ <cross-domain-access>
+   <policy>
+     <allow-from>
+       <domain uri='*' />
+     </allow-from>
+     <grant-to>
+       <resource include-subpaths='true' path='/' />
+     </grant-to>
+   </policy>
+ </cross-domain-access>
+</access-policy>");
+		}
+	}
+}
