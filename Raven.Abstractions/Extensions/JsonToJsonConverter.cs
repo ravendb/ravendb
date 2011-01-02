d7efdc581b930c52cec9c710d543905eefeae7d8
Ayende Rahien <ayende@ayende.com>
2011-01-02 11:14:34 +0200

    Fixing serialization of complex values from Transform Results

---
+++ Raven.Abstractions/Extensions/JsonToJsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Raven.Database.Linq;
+
+namespace Raven.Database.Json
+{
+#if !NET_3_5
+	public class JsonToJsonConverter : JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			if (value is JObject)
+				((JObject)value).WriteTo(writer);
+			else
+				((DynamicJsonObject)value).Inner.WriteTo(writer);
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			return objectType == typeof(JObject) || objectType == typeof(DynamicJsonObject);
+		}
+	}
+#endif
+}
--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -1,51 +1,77 @@
-//-----------------------------------------------------------------------
-// <copyright file="Program.cs" company="Hibernating Rhinos LTD">
-//     Copyright (c) Hibernating Rhinos LTD. All rights reserved.
-// </copyright>
-//-----------------------------------------------------------------------
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
+using System.Text;
 using Raven.Client.Client;
-using Raven.Client.Document;
 using Raven.Client.Indexes;
-using Raven.Storage.Esent;
 
-namespace etobi.MemLeakTest
+namespace LiveProjectionsBug
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			using (var store = new DocumentStore
-			{
-				Url = "http://localhost:8080"
-			}.Initialize())
-			{
-				for (int i = 0; i < 5000; i++)
-				{
-					using (var s = store.OpenSession())
-					{
-						for (int j = 0; j < 128; j++)
-						{
-							s.Store(new { Id = "item/" + i + "/" + j, Language = new { Name = "English" } });
-						}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Working ...");
 
-						s.SaveChanges();
-					}
-				}
-				using (var s = store.OpenSession())
-				{
-					var objects = s.Advanced.LuceneQuery<object>() 
-						.WhereEquals("Language.Name", "English")
-						.ToArray();
+            write_then_read_from_stack_over_flow_types();
 
-					Console.WriteLine(objects.Length);
-				}
-			}
-		}
+            Console.WriteLine("Finished !");
+            Console.ReadLine();
 
-	}
+        }
+        private static void write_then_read_from_stack_over_flow_types()
+        {            
+            EmbeddableDocumentStore documentStore = GetDocumentStore();
+            IndexCreation.CreateIndexes(typeof(QuestionWithVoteTotalIndex).Assembly, documentStore);
+
+            var questionId = @"question\259";
+            using (var session = documentStore.OpenSession())
+            {
+                var user = new User();
+                user.Id = @"user\222";
+                user.DisplayName = "John Doe";
+                session.Store(user);
+
+                var question = new Question();
+                question.Id = questionId;
+                question.Title = "How to do this in RavenDb?";
+                question.Content = "I'm trying to find how to model documents for better DDD support.";
+                question.UserId = @"user\222";
+                session.Store(question);
+
+                var vote1 = new QuestionVote();
+                vote1.QuestionId = questionId;
+                vote1.Delta = 2;
+                session.Store(vote1);
+                var vote2 = new QuestionVote();
+                vote2.QuestionId = questionId;
+                vote2.Delta = 3;
+                session.Store(vote2);
+
+                session.SaveChanges();
+            }
+
+            using (var session = documentStore.OpenSession())
+            {
+                var questionInfo = session.Query<QuestionView, QuestionWithVoteTotalIndex>()
+                    .Customize(x => x.WaitForNonStaleResultsAsOfNow())
+                    .Where(x => x.QuestionId == questionId)
+                    .SingleOrDefault();
+                if (questionInfo == null)
+                    Console.WriteLine(" question not found ");
+                else
+                {
+                    Console.WriteLine("Question Title: {0}, votes: {1}",
+                        questionInfo.QuestionTitle, questionInfo.VoteTotal);
+                    Console.WriteLine("User name {0}", questionInfo.User.DisplayName);
+                    Assert.Assert(!string.IsNullOrEmpty(questionInfo.User.DisplayName), "User.DisplayName was not loaded");
+                    Console.WriteLine("Content: {0}", questionInfo.Question.Content);
+                    Debug.Assert(!string.IsNullOrEmpty(questionInfo.Question.Content), "Question.Content was not loaded");
+                }
+            }
+        }
+
+      
+    }
 }
