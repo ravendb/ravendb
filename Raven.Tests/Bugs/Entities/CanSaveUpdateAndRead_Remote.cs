be7e4eda7151b119a3d46345c3d06150287ba3d9
Ayende Rahien <ayende@ayende.com>
2011-01-03 17:23:56 +0200

    Fixing an issue where we weren't saving the http headers in the cache, leading to data corruption on the client

--- Raven.Tests/Bugs/Entities/CanSaveUpdateAndRead_Remote.cs
+++ Raven.Tests/Bugs/Entities/CanSaveUpdateAndRead_Remote.cs
@@ -60,6 +60,37 @@ namespace Raven.Tests.Bugs.Entities
 				}
 			}
 		}
+
+		[Fact]
+		public void Can_read_entity_name_after_update_from_query_after_entity_is_in_cache()
+		{
+			using (GetNewServer())
+			using (var store = new DocumentStore { Url = "http://localhost:8080" }.Initialize())
+			{
+				using (var s = store.OpenSession())
+				{
+					s.Store(new Event { Happy = true });
+					s.SaveChanges();
+				}
+
+				using (var s = store.OpenSession())
+				{
+					s.Load<Event>("events/1");//load into cache
+				}
+
+				using (var s = store.OpenSession())
+				{
+					s.Load<Event>("events/1").Happy = false;
+					s.SaveChanges();
+				}
+
+				using (var s = store.OpenSession())
+				{
+					var events = s.Query<Event>().Customize(x => x.WaitForNonStaleResults()).ToArray();
+					Assert.NotEmpty(events);
+				}
+			}
+		}
 		public class Event
 		{
 			public string Id { get; set; }
