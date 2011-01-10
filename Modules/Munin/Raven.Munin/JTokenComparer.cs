5fe140c79082f6f86b37e0b0c9b7d4c0b5c9fb4b
Benjamin Gram <ski.surf@gmail.com>
2011-01-10 08:39:15 -0800

    merging ayende's updates

--- Modules/Munin/Raven.Munin/JTokenComparer.cs
+++ Modules/Munin/Raven.Munin/JTokenComparer.cs
@@ -1,4 +1,4 @@
-﻿//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // <copyright file="JTokenComparer.cs" company="Hibernating Rhinos LTD">
 //     Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 // </copyright>
