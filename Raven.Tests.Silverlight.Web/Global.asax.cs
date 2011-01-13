405ced72a061ee67e1fbbcc6d7a426255194ab64
Christopher Bennage <christopher@bennage.com>
2011-01-13 16:31:00 -0500

    removed some junk code from the sl test host project

--- Raven.Tests.Silverlight.Web/Global.asax.cs
+++ Raven.Tests.Silverlight.Web/Global.asax.cs
@@ -3,19 +3,9 @@
 	using System.Web;
 	using System.Web.Mvc;
 	using System.Web.Routing;
-	using Client;
-	using Client.Client;
 
 	public class MvcApplication : HttpApplication
 	{
-		const string RavenSessionKey = "Raven.Session";
-		static EmbeddableDocumentStore _documentStore;
-
-		public static IDocumentSession CurrentSession
-		{
-			get { return (IDocumentSession) HttpContext.Current.Items[RavenSessionKey]; }
-		}
-
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
@@ -29,10 +19,6 @@
 
 		protected void Application_Start()
 		{
-			//_documentStore = new EmbeddableDocumentStore {Url = "http://localhost:8080/",UseEmbeddedHttpServer = true};
-			//_documentStore.Configuration.DataDirectory ="MyData";
-			//_documentStore.Initialize();
-
 			AreaRegistration.RegisterAllAreas();
 
 			RegisterRoutes(RouteTable.Routes);
