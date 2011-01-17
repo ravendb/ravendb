a0fa80ee7954f507eb282bc6cd51cac2398b8e1b
Christopher Bennage <christopher@bennage.com>
2011-01-17 00:23:20 -0500

    integrated the new testing syntax as an external assembly

--- Raven.Tests.Silverlight/LinqQueryTests.cs
+++ Raven.Tests.Silverlight/LinqQueryTests.cs
@@ -6,42 +6,8 @@
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
 	using Assert = Xunit.Assert;
 
-	public class LinqQueryTests : AsyncTestsBase
+	public class LinqQueryTests : RavenTestBase
 	{
-		[Asynchronous]
-		[TestMethod]
-		public void Can_insert_async_and_multi_get_async()
-		{
-			var dbname = GenerateNewDatabaseName();
-			var documentStore = EnqueueEnsuringNewDatabase(dbname);
 
-			EnqueueCallback(() =>
-			                	{
-			                		var entity1 = new Company {Name = "Async Company #1"};
-			                		var entity2 = new Company {Name = "Async Company #2"};
-			                		using (var session_for_storing = documentStore.OpenAsyncSession(dbname))
-			                		{
-			                			session_for_storing.Store(entity1);
-			                			session_for_storing.Store(entity2);
-			                			EnqueueTaskCompleted(session_for_storing.SaveChangesAsync());
-			                		}
-
-			                		EnqueueCallback(() =>
-			                		                	{
-			                		                		using (var session_for_loading = documentStore.OpenAsyncSession(dbname))
-			                		                		{
-			                		                			var task =
-			                		                				session_for_loading.MultiLoadAsync<Company>(new[] {entity1.Id, entity2.Id});
-			                		                			EnqueueTaskCompleted(task);
-			                		                			EnqueueCallback(() =>
-			                		                			                	{
-			                		                			                		Assert.Equal(entity1.Name, task.Result[0].Name);
-			                		                			                		Assert.Equal(entity2.Name, task.Result[1].Name);
-			                		                			                		EnqueueTestComplete();
-			                		                			                	});
-			                		                		}
-			                		                	});
-			                	});
-		}
 	}
 }
--- Raven.Tests.Silverlight/RavenTestBase.cs
+++ Raven.Tests.Silverlight/RavenTestBase.cs
@@ -1,61 +1,16 @@
 ﻿namespace Raven.Tests.Silverlight
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Reflection;
-    using System.Threading.Tasks;
-    using Microsoft.Silverlight.Testing;
+	using System;
+	using UnitTestProvider;
 
-    public abstract class RavenTestBase : SilverlightTest
-    {
-        protected const int Port = 8080;
-        protected const string Url = "http://localhost:";
+	public abstract class RavenTestBase : AsynchronousTaskTest
+	{
+		protected const int Port = 8080;
+		protected const string Url = "http://localhost:";
 
-        protected static string GenerateNewDatabaseName()
-        {
-            return Guid.NewGuid().ToString();
-        }
-
-        protected Task Delay(int milliseconds)
-        {
-            return TaskEx.Delay(TimeSpan.FromMilliseconds(milliseconds));
-        }
-
-        public void ExecuteTest(MethodInfo test)
-        {
-            var tasks = (IEnumerable<Task>) test.Invoke(this, new object[] {});
-            IEnumerator<Task> enumerator = tasks.GetEnumerator();
-            ExecuteTestStep(enumerator);
-        }
-
-        private void ExecuteTestStep(IEnumerator<Task> enumerator)
-        {
-            bool moveNextSucceeded = false;
-            try
-            {
-                moveNextSucceeded = enumerator.MoveNext();
-            }
-            catch (Exception ex)
-            {
-                EnqueueTestComplete();
-                return;
-            }
-
-            if (moveNextSucceeded)
-            {
-                try
-                {
-                    Task next = enumerator.Current;
-                    EnqueueConditional(() => next.IsCompleted || next.IsFaulted);
-                    EnqueueCallback(() => ExecuteTestStep(enumerator));
-                }
-                catch (Exception ex)
-                {
-                    EnqueueTestComplete();
-                    return;
-                }
-            }
-            else EnqueueTestComplete();
-        }
-    }
+		protected static string GenerateNewDatabaseName()
+		{
+			return Guid.NewGuid().ToString();
+		}
+	}
 }
