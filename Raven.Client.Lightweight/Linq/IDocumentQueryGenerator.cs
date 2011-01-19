3ffcbe59debdddf340ae7f2cc687320b1004204f
Christopher Bennage <christopher@bennage.com>
2011-01-18 21:38:50 -0500

    removed an unused interface

--- Raven.Client.Lightweight/Linq/IDocumentQueryGenerator.cs
+++ Raven.Client.Lightweight/Linq/IDocumentQueryGenerator.cs
@@ -10,15 +10,4 @@ namespace Raven.Client.Linq
 		/// </summary>
 		IDocumentQuery<T> Query<T>(string indexName);
 	}
-
-	///<summary>
-	/// Generate a new asynchronous document query
-	///</summary>
-	public interface IAsyncDocumentQueryGenerator
-	{
-		/// <summary>
-		/// Create a new asynchronous query for <typeparam name="T"/>
-		/// </summary>
-		IAsyncDocumentQuery<T> Query<T>(string indexName);
-	}
 }
