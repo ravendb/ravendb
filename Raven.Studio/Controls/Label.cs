dba0309f82dfc3f8fa64ea22c22658a04064c95e
Christopher Bennage <christopher@bennage.com>
2011-02-02 14:30:45 -0500

    basic layout of index editor; added a couple of custom controls for better semantics

---
+++ Raven.Studio/Controls/Label.cs
@@ -0,0 +1,12 @@
+﻿namespace Raven.Studio.Controls
+{
+	using System.Windows.Controls;
+
+	public class Label : ContentControl
+	{
+		public Label()
+		{
+			DefaultStyleKey = typeof (Label);
+		}
+	}
+}
