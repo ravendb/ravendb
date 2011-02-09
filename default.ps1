5a9b7be47edcf36d1f46b61e0b3cbd1e51d33527
Ayende Rahien <ayende@ayende.com>
2011-02-09 15:14:35 +0200

    Adding silverlight dlls to the deployed build

--- default.ps1
+++ default.ps1
@@ -18,7 +18,9 @@ properties {
   $client_dlls_3_5 = @( "Newtonsoft.Json.Net35.???", "Raven.Abstractions-3.5.???", "Raven.Client.Lightweight-3.5.???", "MissingBitsFromClientProfile.???" );
      
   $client_dlls = @( "Newtonsoft.Json.???", "Raven.Abstractions.???", "Raven.Client.Lightweight.???", "MissingBitsFromClientProfile.???", "AsyncCtpLibrary.???" );
-     
+  
+  $silverlight_dlls = @( "Raven.Client.Silverlight.???", "AsyncCtpLibrary_Silverlight.???", "Newtonsoft.Json.Silverlight.???");   
+  
   $all_client_dlls = @( "Raven.Client.Lightweight.???", "Raven.Client.Embedded.???", "Raven.Abstractions.???", "Raven.Http.???", "Raven.Database.???", "Raven.Http.???", `
       "Esent.Interop.???", "ICSharpCode.NRefactory.???", "Lucene.Net.???", "Spatial.Net.???", "SpellChecker.Net.???", "log4net.???", "Newtonsoft.Json.???", `
       "Raven.Storage.Esent.???", "Raven.Storage.Managed.???", "Raven.Munin.???", "AsyncCtpLibrary.???" );
@@ -180,6 +182,7 @@ task CreateOutpuDirectories -depends CleanOutputDirectory {
 	mkdir $build_dir\Output\Web\bin
 	mkdir $build_dir\Output\Server
 	mkdir $build_dir\Output\EmbeddedClient
+	mkdir $build_dir\Output\Silverlight
 	mkdir $build_dir\Output\Client-3.5
 	mkdir $build_dir\Output\Client
 	mkdir $build_dir\Output\Bundles
@@ -194,6 +197,13 @@ task CopyEmbeddedClient {
 
   foreach($client_dll in $all_client_dlls) {
     cp "$build_dir\$client_dll" $build_dir\Output\EmbeddedClient
+  }
+}
+
+task CopySilverlight{ 
+
+  foreach($silverlight_dll in $silverlight_dlls) {
+    cp "$build_dir\$silverlight_dll" $build_dir\Output\Silverlight
   }
 }
 
@@ -297,6 +307,7 @@ task DoRelease -depends Compile, `
 	CopyEmbeddedClient, `
 	CopySmuggler, `
 	CopyClient, `
+	CopySilverlight, `
 	CopyClient35, `
 	CopyWeb, `
 	CopyBundles, `
