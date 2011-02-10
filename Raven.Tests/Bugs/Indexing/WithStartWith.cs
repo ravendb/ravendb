38552566cc841e2006e0db339142b51232209db4
Ayende Rahien <ayende@ayende.com>
2011-02-10 15:14:59 +0200

    adding passing test

---
+++ Raven.Tests/Bugs/Indexing/WithStartWith.cs
@@ -0,0 +1,37 @@
+using Raven.Database.Indexing;
+using Xunit;
+using System.Linq;
+
+namespace Raven.Tests.Bugs.Indexing
+{
+	public class WithStartWith : LocalClientTest
+	{
+		[Fact]
+		public void CanQueryDocumentsFilteredByMap()
+		{
+			using(var store = NewDocumentStore())
+			{
+				store.DatabaseCommands.PutIndex("test",
+				                                new IndexDefinition
+				                                {
+				                                	Map =
+				                                		"from doc in docs let Name = doc[\"@metadata\"][\"Name\"] where Name.StartsWith(\"Raven\") select new { Name }"
+				                                });
+
+				using(var s = store.OpenSession())
+				{
+					var entity = new {Name = "Ayende"};
+					s.Store(entity);
+					s.Advanced.GetMetadataFor(entity)["Name"] = "RavenDB";
+					s.SaveChanges();
+				}
+
+				using(var s = store.OpenSession())
+				{
+					Assert.Equal(1, s.Query<object>("test").Customize(x => x.WaitForNonStaleResults()).Count());
+				}
+			}
+		}
+
+	}
+}
