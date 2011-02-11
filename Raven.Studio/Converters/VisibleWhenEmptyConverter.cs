15d8a5b3e6f14254553a0f51cf693cda8aaf1258
Christopher Bennage <christopher@bennage.com>
2011-02-11 15:11:13 -0500

    added several messages indicidating when not results are present for documents, collections, etc.

---
+++ Raven.Studio/Converters/VisibleWhenEmptyConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace Raven.Studio.Converters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public class VisibleWhenEmptyConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var items = value as IEnumerable<object>;
+            return (items == null) ||!items.Any() 
+                ? Visibility.Visible 
+                : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
