3b9495e3a3baeea7f8669fba47ada109159c888c
Christopher Bennage <christopher@bennage.com>
2011-02-14 11:17:49 -0500

    generating some sample errors

--- Raven.Tests.Silverlight/Generate.cs
+++ Raven.Tests.Silverlight/Generate.cs
@@ -1,13 +1,18 @@
 ﻿namespace Raven.Tests.Silverlight
 {
+	using System;
 	using System.Collections.Generic;
 	using System.Linq;
+	using System.Threading;
 	using System.Threading.Tasks;
 	using Client.Document;
+	using Database.Data;
+	using Database.Indexing;
 	using Document;
 	using Entities;
 	using Microsoft.Silverlight.Testing;
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using Newtonsoft.Json.Linq;
 
 	/// <summary>
 	/// Not actually a test, just an easy way for me to insert some sample data
@@ -40,6 +45,58 @@
 				session.Store(new Company {Name = "My Company"});
 
 				yield return session.SaveChangesAsync();
+			}
+		}
+
+		[Ignore]
+		[Asynchronous]
+		public IEnumerable<Task> Some_indexing_errors()
+		{
+			var store = new DocumentStore { Url = Url + Port };
+			store.Initialize();
+
+			yield return store.AsyncDatabaseCommands
+				.PutIndexAsync("pagesByTitle2",
+						new IndexDefinition
+						{
+							Map = @"
+                    from doc in docs
+                    where doc.type == ""page""
+                    select new {  f = 2 / doc.size };"
+						}, true);
+
+			yield return store.AsyncDatabaseCommands
+			.PutAsync("1", null,
+					   JObject.Parse(
+						@"{
+								type: 'page', 
+								some: 'val', 
+								other: 'var', 
+								content: 'this is the content', 
+								title: 'hello world', 
+								size: 0,
+								'@metadata': {'@id': 1}
+							}"),
+					   new JObject());
+
+			for (int i = 0; i < 50; i++)
+			{
+				var query = store.AsyncDatabaseCommands
+					.QueryAsync("pagesByTitle2", new IndexQuery
+						{
+							Query = "f:val",
+							Start = 0,
+							PageSize = 10
+						}, null);
+				yield return (query);
+
+				if (query.Result.IsStale)
+				{
+					yield return Delay(100);
+					continue;
+				}
+				Assert.AreNotEqual(0, query.Result.TotalResults);
+				yield break;
 			}
 		}
 	}
