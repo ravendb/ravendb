64584773485d76363f1a006805cdd3255f72e10b
Ayende Rahien <ayende@ayende.com>
2011-02-14 14:44:23 +0200

    Fixing nullable to default value bug

---
+++ Raven.Tests/Bugs/Nullables.cs
@@ -0,0 +1,44 @@
+using Newtonsoft.Json.Linq;
+using Raven.Client.Client;
+using Xunit;
+
+namespace Raven.Tests.Bugs
+{
+	public class Nullables : LocalClientTest
+	{
+		[Fact]
+		public void CanWriteNullablesProperly()
+		{
+			using (var store = NewDocumentStore())
+			{
+				//_db is document store
+				var fooJson = JObject.FromObject(new Foo
+				{
+					Id = "foo/10",
+					BarItem = new Bar {Name = "My Bar Item X"}
+
+				}, store.Conventions.CreateSerializer());
+
+				var fooObject = fooJson.Deserialize(typeof (Foo),
+				                                    store.Conventions);
+				decimal? size = ((Foo) fooObject).BarItem.Size;
+
+				Assert.Null(size);
+			}
+		}
+
+
+		public class Foo
+		{
+			public string Id { get; set; }
+			public Bar BarItem { get; set; }
+		}
+
+		public class Bar
+		{
+			public string Name { get; set; }
+			public decimal? Size { get; set; }
+		}
+
+	}
+}
