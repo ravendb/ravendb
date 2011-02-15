ee8d5b7c47f772958e398c16dc009e56c156134c
Christopher Bennage <christopher@bennage.com>
2011-02-15 18:21:06 -0500

    added an error message when studio can't connect to the server

---
+++ Raven.Studio/Shell/ErrorViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Raven.Studio.Shell
+{
+	using Caliburn.Micro;
+
+	public class ErrorViewModel : Screen
+	{
+		public ErrorViewModel()
+		{
+			DisplayName = "Error!";
+		}
+
+		public string Message { get; set; }
+		public string Details { get; set; }
+	}
+}
