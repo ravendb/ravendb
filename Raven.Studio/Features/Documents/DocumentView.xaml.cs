571176fd9f2b0f4469fea875b93df41c16da4a40
Christopher Bennage <christopher@bennage.com>
2011-02-15 12:25:54 -0500

    replaced the commercial syntax highlighting textbox with the OSS one

--- Raven.Studio/Features/Documents/DocumentView.xaml.cs
+++ Raven.Studio/Features/Documents/DocumentView.xaml.cs
@@ -1,17 +1,12 @@
 ﻿namespace Raven.Studio.Features.Documents
 {
 	using System.Windows.Controls;
-	using ActiproSoftware.Windows.ProductSamples.SyntaxEditorSamples.QuickStart.CodeOutliningCollapsedText;
 
 	public partial class DocumentView : UserControl
 	{
 		public DocumentView()
 		{
 			InitializeComponent();
-
-			var language = new JavascriptSyntaxLanguage();
-			dataEditor.Document.Language = language;
-			metadataEditor.Document.Language = language;
 		}
 	}
 }
