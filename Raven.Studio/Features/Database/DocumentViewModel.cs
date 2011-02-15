e92736b3e3fc36d44e59189bd526fb6c0aea5310
Christopher Bennage <christopher@bennage.com>
2011-02-15 13:23:10 -0500

    document editor allows you to prettify the json

--- Raven.Studio/Features/Database/DocumentViewModel.cs
+++ Raven.Studio/Features/Database/DocumentViewModel.cs
@@ -8,6 +8,7 @@
 	using Caliburn.Micro;
 	using Framework;
 	using Messages;
+	using Newtonsoft.Json;
 	using Newtonsoft.Json.Linq;
 	using Raven.Database;
 	using Shell;
@@ -15,19 +16,21 @@
 	[Export]
 	[PartCreationPolicy(CreationPolicy.NonShared)]
 	public class DocumentViewModel : Screen,
-		IHandle<DocumentDeleted>
+	                                 IHandle<DocumentDeleted>
 	{
-		readonly DocumentTemplateProvider templateProvider;
-		readonly NavigationViewModel navigation;
-		readonly IEventAggregator events;
 		public const int SummaryLength = 150;
-		JsonDocument jsonDocument;
 
-		IDictionary<string, JToken> data;
+		readonly IDictionary<string, JToken> data;
+		readonly IEventAggregator events;
+		readonly NavigationViewModel navigation;
+		readonly DocumentTemplateProvider templateProvider;
+		string jsonData;
+		JsonDocument jsonDocument;
 		IDictionary<string, JToken> metadata;
 
 		[ImportingConstructor]
-		public DocumentViewModel(DocumentTemplateProvider templateProvider, NavigationViewModel navigation, IEventAggregator events)
+		public DocumentViewModel(DocumentTemplateProvider templateProvider, NavigationViewModel navigation,
+		                         IEventAggregator events)
 		{
 			this.templateProvider = templateProvider;
 			this.navigation = navigation;
@@ -36,7 +39,79 @@
 			metadata = new Dictionary<string, JToken>();
 
 			events.Subscribe(this);
+		}
 
+		public DataTemplate DataTemplate { get; private set; }
+		public string ClrType { get; private set; }
+		public string CollectionType { get; private set; }
+		public DateTime LastModified { get; private set; }
+
+		public string Id { get; private set; }
+
+		public string DisplayId
+		{
+			get
+			{
+				var collectionType = CollectionType + "/";
+				var id = Id
+					.Replace(collectionType, string.Empty)
+					.Replace(collectionType.ToLower(), string.Empty)
+					.Replace("Raven/", string.Empty);
+
+				Guid guid;
+				if (Guid.TryParse(id, out guid))
+				{
+					id = id.Substring(0, 8);
+				}
+				return id;
+			}
+		}
+
+		public string JsonData
+		{
+			get { return jsonData; }
+			set
+			{
+				jsonData = value;
+				NotifyOfPropertyChange(() => JsonData);
+			}
+		}
+
+		public string JsonMetadata { get; private set; }
+
+		public string Summary
+		{
+			get
+			{
+				return (JsonData.Length > SummaryLength
+				        	? JsonData.Substring(0, SummaryLength) + "..."
+				        	: JsonData)
+					.Replace("\r", "").Replace("\n", " ");
+			}
+		}
+
+		public IDictionary<string, JToken> Data
+		{
+			get { return data; }
+		}
+
+		public IDictionary<string, JToken> Metadata
+		{
+			get { return metadata; }
+		}
+
+		public JsonDocument JsonDocument
+		{
+			get { return jsonDocument; }
+		}
+
+		public void Handle(DocumentDeleted message)
+		{
+			//TODO: I suspect this isn't a good idea...
+			if (message.DocumentId == Id)
+			{
+				navigation.GoBack();
+			}
 		}
 
 		public DocumentViewModel Initialize(JsonDocument document)
@@ -56,16 +131,16 @@
 			templateProvider
 				.GetTemplateFor(CollectionType ?? "default")
 				.ContinueOnSuccess(x =>
-				{
-					DataTemplate = x.Result;
-					NotifyOfPropertyChange(() => DataTemplate);
-				});
-			
+				                   	{
+				                   		DataTemplate = x.Result;
+				                   		NotifyOfPropertyChange(() => DataTemplate);
+				                   	});
+
 			DisplayName = DisplayId;
 
 			return this;
 		}
-		
+
 		public void PrepareForSave()
 		{
 			//if (!ValidateJson(JsonData))
@@ -85,76 +160,23 @@
 			jsonDocument.Key = Id;
 		}
 
+		public void Prettify()
+		{
+			//NOTE: is there a better way to reformat the json? This seems heavy.
+			var obj = JsonConvert.DeserializeObject(JsonData);
+			JsonData = JsonConvert.SerializeObject(obj, Formatting.Indented);
+		}
+
 		//NOTE: quick hack to get me focused on more important things
 		public DocumentViewModel CloneUsing(JsonDocument document)
 		{
-			var doc = new DocumentViewModel(templateProvider,navigation,events);
+			var doc = new DocumentViewModel(templateProvider, navigation, events);
 			return doc.Initialize(document);
 		}
 
 		string DetermineCollectionType()
 		{
 			return metadata.IfPresent<string>("Raven-Entity-Name") ?? (Id.StartsWith("Raven/") ? "System" : null) ?? "document";
-		}
-
-		public DataTemplate DataTemplate {get;private set;}
-		public string ClrType {get; private set;}
-		public string CollectionType {get; private set;}
-		public DateTime LastModified {get; private set;}
-
-		public string Id { get; private set; }
-		public string DisplayId
-		{
-			get
-			{
-				var collectionType = CollectionType + "/";
-				var id = Id
-					.Replace(collectionType, string.Empty)
-					.Replace(collectionType.ToLower(), string.Empty)
-					.Replace("Raven/",string.Empty);
-
-				Guid guid;
-				if(Guid.TryParse(id,out guid))
-				{
-					id = id.Substring(0,8);
-				}
-				return id;
-			}
-		}
-
-		string jsonData;
-		public string JsonData
-		{
-			get { return jsonData; }
-			set { jsonData = value; NotifyOfPropertyChange( ()=> JsonData); }
-		}
-
-		public string JsonMetadata { get; private set; }
-
-		public string Summary
-		{
-			get
-			{
-				return (JsonData.Length > SummaryLength
-				       	? JsonData.Substring(0, SummaryLength) + "..."
-				       	: JsonData)
-						.Replace("\r", "").Replace("\n", " ");
-			}
-		}
-
-		public IDictionary<string, JToken> Data
-		{
-			get { return data; }
-		}
-
-		public IDictionary<string, JToken> Metadata
-		{
-			get { return metadata; }
-		}
-
-		public JsonDocument JsonDocument
-		{
-			get { return jsonDocument; }
 		}
 
 		static IDictionary<string, JToken> ParseJsonToDictionary(JObject dataAsJson)
@@ -180,15 +202,6 @@
 			result.Append("}");
 
 			return result.ToString();
-		}
-
-		public void Handle(DocumentDeleted message)
-		{
-			//TODO: I suspect this isn't a good idea...
-			if(message.DocumentId == Id)
-			{
-				navigation.GoBack();
-			}
 		}
 	}
 }
