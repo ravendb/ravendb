2c44092c6b3d18f59507d66c8cb3d92c5f7ff156
Christopher Bennage <christopher@bennage.com>
2011-02-15 19:05:01 -0500

    added a responder (/studio/) that will display the Silverlight admin UI. Also, the process to build the xap copies it into the Database project.

---
+++ Raven.Http/Responders/StudioUI.cs
@@ -0,0 +1,30 @@
+﻿namespace Raven.Http.Responders
+{
+	using Abstractions;
+	using Extensions;
+
+	public class StudioUI : AbstractRequestResponder
+	{
+		public override string UrlPattern
+		{
+			get { return "^/studio/"; }
+		}
+
+		public override bool IsUserInterfaceRequest
+		{
+			get { return true; }
+		}
+
+		public override string[] SupportedVerbs
+		{
+			get { return new[] {"GET"}; }
+		}
+
+		public override void Respond(IHttpContext context)
+		{
+			var docPath = context.GetRequestUrl().Replace("/studio/", "");
+			if(string.IsNullOrEmpty(docPath)) docPath = "studio.html";
+			context.WriteEmbeddedFile(ResourceStore.GetType().Assembly, Settings.WebDir, docPath);
+		}
+	}
+}
