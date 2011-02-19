9c42a9070def75b319d80ed989f37d7eeacf8e78
Alex Duggleby <alex@duggleby.biz>
2011-02-19 12:26:29 +0100

    Added unit tests for bugs in string equality comparisons

---
+++ Raven.Tests/Bugs/Queries/EqualsIgnoreCase.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Raven.Client.Client;
+using Raven.Client.Indexes;
+using Raven.Database.Extensions;
+using Raven.Database.Indexing;
+using Xunit;
+using Raven.Database.Data;
+using Raven.Client;
+using System.IO;
+using Raven.Client.Document;
+using Raven.Client.Linq;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Raven.Tests.Bugs.Queries
+{
+    public class EqualsIgnoreCase : LocalClientTest
+    {
+        [Fact]
+        public void QueryWithEquals_StringWithDifferentCasing_ReturnsFalse()
+        {
+            using (var db = NewDocumentStore())
+            {
+                db.Initialize();
+
+                using (var session = db.OpenSession())
+                {
+                    session.Store(new User() { Name = "Matt" });
+                    session.SaveChanges();
+
+                    bool testQuery;
+
+                    // Equals w/o InvariantCultureIgnoreCase
+                    testQuery = session.Query<User>()
+                            .Customize(x => x.WaitForNonStaleResults())
+                            .Any(x => x.Name.Equals("matt"));
+
+                    Assert.False(testQuery);
+                }
+            }
+        }
+
+        [Fact]
+        public void QueryWithEquals_StringWithSameCasing_ReturnsTrue()
+        {
+            using (var db = NewDocumentStore())
+            {
+                db.Initialize();
+
+                using (var session = db.OpenSession())
+                {
+                    session.Store(new User() { Name = "Matt" });
+                    session.SaveChanges();
+
+                    bool testQuery;
+
+                    // Equals w/o InvariantCultureIgnoreCase
+                    testQuery = session.Query<User>()
+                            .Customize(x => x.WaitForNonStaleResults())
+                            .Any(x => x.Name.Equals("Matt"));
+
+                    Assert.True(testQuery);
+
+                }
+            }
+        }
+
+        [Fact]
+        public void QueryWithEqualsInvariantCultureIgnoreCase_StringWithDifferentCasing_ReturnsTrue()
+        {
+            using (var db = NewDocumentStore())
+            {
+                db.Initialize();
+
+                using (var session = db.OpenSession())
+                {
+                    session.Store(new User() { Name = "Matt" });
+                    session.SaveChanges();
+
+                    bool testQuery;
+
+                    // Equals with InvariantCultureIgnoreCase
+                    testQuery = session.Query<User>()
+                            .Customize(x => x.WaitForNonStaleResults())
+                            .Any(x => x.Name.Equals("matt", StringComparison.InvariantCultureIgnoreCase));
+
+                    Assert.True(testQuery);
+
+                }
+            }
+        }
+
+        [Fact]
+        public void QueryWithEqualsInvariantCultureIgnoreCase_StringWithSameCasing_ReturnsTrue()
+        {
+            using (var db = NewDocumentStore())
+            {
+                db.Initialize();
+
+                using (var session = db.OpenSession())
+                {
+                    session.Store(new User() { Name = "Matt" });
+                    session.SaveChanges();
+
+                    bool testQuery;
+
+                    // Equals with InvariantCultureIgnoreCase
+                    testQuery = session.Query<User>()
+                            .Customize(x => x.WaitForNonStaleResults())
+                            .Any(x => x.Name.Equals("Matt", StringComparison.InvariantCultureIgnoreCase));
+
+                    Assert.True(testQuery);
+
+                }
+            }
+        }
+       
+    }
+}
