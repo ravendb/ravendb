32b796fe841c3d9365b3232fb71a5c050213e117
Christopher Bennage <christopher@bennage.com>
2011-03-02 18:01:16 -0800

    added an indexer in order to allow databinding to underlying json properties

--- Raven.Studio/Features/Documents/DocumentViewModel.cs
+++ Raven.Studio/Features/Documents/DocumentViewModel.cs
@@ -1,108 +1,121 @@
 ﻿namespace Raven.Studio.Features.Documents
 {
-	using System;
-	using Collections;
-	using Framework;
-	using Newtonsoft.Json.Linq;
-	using Raven.Database;
+    using System;
+    using Collections;
+    using Framework;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using Raven.Database;
 
-	/// <summary>
-	/// This view model is for displaying documents in bulk. There is no change notification and no behaviours related to editing
-	/// </summary>
-	public class DocumentViewModel : ISupportDocumentTemplate
-	{
-		const int SummaryLength = 150;
-		readonly JsonDocument inner;
+    /// <summary>
+    /// This view model is for displaying documents in bulk. There is no change notification and no behaviours related to editing
+    /// </summary>
+    public class DocumentViewModel : ISupportDocumentTemplate
+    {
+        const int SummaryLength = 150;
+        readonly JsonDocument inner;
 
-		public DocumentViewModel(JsonDocument inner)
-		{
-			this.inner = inner;
-			Id = inner.Metadata.IfPresent<string>("@id");
-			LastModified = inner.Metadata.IfPresent<DateTime>("Last-Modified");
-			ClrType = inner.Metadata.IfPresent<string>("Raven-Clr-Type");
-			CollectionType = DetermineCollectionType();
-		}
+        public DocumentViewModel(JsonDocument inner)
+        {
+            this.inner = inner;
+            Id = inner.Metadata.IfPresent<string>("@id");
+            LastModified = inner.Metadata.IfPresent<DateTime>("Last-Modified");
+            ClrType = inner.Metadata.IfPresent<string>("Raven-Clr-Type");
+            CollectionType = DetermineCollectionType();
+        }
 
-		public string Id { get; private set; }
-		public string ClrType { get; private set; }
-		public string CollectionType { get; private set; }
-		public DateTime LastModified { get; private set; }
+        public string Id { get; private set; }
+        public string ClrType { get; private set; }
+        public string CollectionType { get; private set; }
+        public DateTime LastModified { get; private set; }
 
-		public string DisplayId
-		{
-			get
-			{
-				if (string.IsNullOrEmpty(Id)) return string.Empty;
+        public string DisplayId
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Id)) return string.Empty;
 
-				var collectionType = CollectionType + "/";
-				var display = Id
-					.Replace(collectionType, string.Empty)
-					.Replace(collectionType.ToLower(), string.Empty)
-					.Replace("Raven/", string.Empty);
+                var collectionType = CollectionType + "/";
+                var display = Id
+                    .Replace(collectionType, string.Empty)
+                    .Replace(collectionType.ToLower(), string.Empty)
+                    .Replace("Raven/", string.Empty);
 
-				Guid guid;
-				if (Guid.TryParse(display, out guid))
-				{
-					display = display.Substring(0, 8);
-				}
-				return display;
-			}
-		}
+                Guid guid;
+                if (Guid.TryParse(display, out guid))
+                {
+                    display = display.Substring(0, 8);
+                }
+                return display;
+            }
+        }
 
-		public JObject Contents
-		{
-			get { return JsonDocument.DataAsJson; }
-		}
+        public JObject Contents
+        {
+            get { return JsonDocument.DataAsJson; }
+        }
 
-		public string Summary
-		{
-			get
-			{
-				var json = JsonDocument.DataAsJson.ToString();
-				json = (json.Length > SummaryLength)
-				       	? json.Substring(0, SummaryLength) + "..."
-				       	: json;
+        public string this[string path]
+        {
+            get
+            {
+                JToken selectToken = JsonDocument.DataAsJson.SelectToken(path);
+                if (selectToken == null || 
+                    selectToken.Type == JTokenType.Null || 
+                    selectToken.Type == JTokenType.Undefined)
+                    return "null";
+                if (selectToken.Type == JTokenType.Object ||
+                    selectToken.Type == JTokenType.Array)
+                    return selectToken.ToString(Formatting.Indented);
+                return ((JValue)selectToken).Value.ToString();
+            }
+        }
 
-				return json
-					.Replace("\r", "")
-					.Replace("\n", " ")
-					.TrimStart('{', ' ')
-					.TrimEnd('}');
-			}
-		}
+        public string Summary
+        {
+            get
+            {
+                var json = JsonDocument.DataAsJson.ToString();
+                json = (json.Length > SummaryLength)
+                        ? json.Substring(0, SummaryLength) + "..." + Environment.NewLine + "}"
+                        : json;
 
-		public JObject Metadata
-		{
-			get { return JsonDocument.Metadata; }
-		}
+                return json;
+            }
+        }
 
-		public JsonDocument JsonDocument
-		{
-			get { return inner; }
-		}
+        public JObject Metadata
+        {
+            get { return JsonDocument.Metadata; }
+        }
 
-		string ISupportDocumentTemplate.TemplateKey
-		{
-			get { return CollectionType; }
-		}
+        public JsonDocument JsonDocument
+        {
+            get { return inner; }
+        }
 
-		string DetermineCollectionType()
-		{
-			return DetermineCollectionType(Metadata);
-		}
+        string ISupportDocumentTemplate.TemplateKey
+        {
+            get { return CollectionType; }
+        }
 
-		public static string DetermineCollectionType(JObject metadata)
-		{
-			var id = metadata.IfPresent<string>("@id") ?? string.Empty;
+        string DetermineCollectionType()
+        {
+            return DetermineCollectionType(Metadata);
+        }
 
-			if (string.IsNullOrEmpty(id)) 
-				return BuiltinCollectionName.Projection; // meaning that the document is a projection and not a 'real' document
+        public static string DetermineCollectionType(JObject metadata)
+        {
+            var id = metadata.IfPresent<string>("@id") ?? string.Empty;
 
-			var entity = metadata.IfPresent<string>("Raven-Entity-Name");
-			return entity ?? 
-				(id.StartsWith("Raven/") 
-					? BuiltinCollectionName.System 
-					: BuiltinCollectionName.Document);
-		}
-	}
+            if (string.IsNullOrEmpty(id))
+                return BuiltinCollectionName.Projection; // meaning that the document is a projection and not a 'real' document
+
+            var entity = metadata.IfPresent<string>("Raven-Entity-Name");
+            return entity ??
+                (id.StartsWith("Raven/")
+                    ? BuiltinCollectionName.System
+                    : BuiltinCollectionName.Document);
+        }
+    }
 }
