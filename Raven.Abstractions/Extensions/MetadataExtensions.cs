25782d7fa42c0b13bc7158ea4a728da7d74e62d2
Ayende Rahien <ayende@ayende.com>
2011-03-14 12:38:13 +0200

    Adding missing namespace

--- Raven.Abstractions/Extensions/MetadataExtensions.cs
+++ Raven.Abstractions/Extensions/MetadataExtensions.cs
@@ -5,6 +5,7 @@
 //-----------------------------------------------------------------------
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
