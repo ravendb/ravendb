484b5bb6dd16d2c055cd7560c94bf23e58462588
Ayende Rahien <ayende@ayende.com>
2011-03-14 18:09:55 +0200

    Adding passing tests

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
-using Newtonsoft.Json.Bson;
-using Newtonsoft.Json.Linq;
-using Raven.Client.Document;
-using Raven.Tests.Bugs.DTC;
-using Raven.Tests.Document;
-using Raven.Tests.Stress;
-using Xunit;
+using Raven.Tests.Bugs;
 
 namespace Raven.Tryouts
 {
@@ -17,8 +9,7 @@ namespace Raven.Tryouts
 		{
 			while (true)
 			{
-				using (var x = new DocumentStoreServerTests())
-					x.Can_create_index_using_linq_from_client();
+				new VeryBigResultSet().CanGetVeryBigResultSetsEvenThoughItIsBadForYou();
 				Console.WriteLine(DateTime.Now);
 			}
 		}
