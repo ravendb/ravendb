d6efe13d2a0b38827109a84ae1f298e1ae2c7493
Christopher Bennage <christopher@bennage.com>
2011-03-18 10:15:39 -0400

    the db initializer for studio now using the index creation tasks

---
+++ Raven.Client.Lightweight/Extensions/DatabaseCommandsExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Raven.Client.Extensions
+{
+	using System.Threading.Tasks;
+	using Client.Async;
+	using Indexes;
+
+	///<summary>
+	/// Extension methods that make certain database command operations more convenient to use
+	///</summary>
+	public static class DatabaseCommandsExtensions
+	{
+		/// <summary>
+		/// Asynchronously creates an index
+		/// </summary>
+		/// <typeparam name="T">The type that defines the index to be create.</typeparam>
+		/// <param name="commands">The hook to the database commands.</param>
+		/// <param name="overwrite">Should the index be overwritten if it already exists.</param>
+		/// <returns></returns>
+		public static Task<string> PutIndexAsync<T>(this IAsyncDatabaseCommands commands, bool overwrite)
+			where T : AbstractIndexCreationTask, new()
+		{
+			var indexCreationTask = new T();
+
+			return commands.PutIndexAsync(
+				indexCreationTask.IndexName,
+				indexCreationTask.CreateIndexDefinition(), overwrite);
+		}
+	}
+}
