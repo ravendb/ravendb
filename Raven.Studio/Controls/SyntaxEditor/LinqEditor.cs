48a4024cfb4cf7369803ccfc8ea7c15e6392ae7a
Christopher Bennage <christopher@bennage.com>
2011-03-21 17:34:56 -0400

    closes #17, addss syntax highlighting for json, linq, and xaml

---
+++ Raven.Studio/Controls/SyntaxEditor/LinqEditor.cs
@@ -0,0 +1,20 @@
+﻿namespace Raven.Studio.Controls.SyntaxEditor
+{
+    using ActiproSoftware.Text;
+    using ActiproSoftware.Windows.Controls.SyntaxEditor;
+
+    public class LinqEditor : SyntaxEditor
+    {
+        private static readonly ISyntaxLanguage DefaultLanguage;
+
+        static LinqEditor()
+        {
+            DefaultLanguage = SyntaxEditorHelper.LoadLanguageDefinitionFromResourceStream("CSharp.langdef");
+        }
+
+        public LinqEditor()
+        {
+            Document.Language = DefaultLanguage;
+        }
+    }
+}
