a8cbfe220a668d8601e85134e9afa4887d20bc46
Ayende Rahien <ayende@ayende.com>
2011-03-22 00:31:16 +0200

    More care in disposing properly the persistent source

--- Modules/Munin/Raven.Munin/FileBasedPersistentSource.cs
+++ Modules/Munin/Raven.Munin/FileBasedPersistentSource.cs
@@ -118,8 +118,12 @@ namespace Raven.Munin
 
     	public override void Dispose()
         {
-            log.Dispose();
-            base.Dispose();
+			Action parentDispose = base.Dispose;
+            Write(_ =>
+            {
+				log.Dispose();
+				parentDispose();
+            });
         }
 
         public void Delete()
