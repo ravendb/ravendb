9544c08b670ba92613553aa48633f340fb02d907
Ayende Rahien <ayende@ayende.com>
2011-03-23 02:01:59 +0200

    Filtering metadata when loading documents

--- Raven.Abstractions/Extensions/MetadataExtensions.cs
+++ Raven.Abstractions/Extensions/MetadataExtensions.cs
@@ -22,7 +22,6 @@ namespace Raven.Database.Data
         private static readonly HashSet<string> HeadersToIgnoreServerDocument =
             new HashSet<string>(StringComparer.OrdinalIgnoreCase)
     		{
-				"Non-Authoritive-Information",
 				"Content-Type",
 				
     		};
@@ -31,6 +30,7 @@ namespace Raven.Database.Data
 		{
 			// Raven internal headers
 			"Raven-Server-Build",
+			"Non-Authoritive-Information",
 
 
             "Allow",
@@ -96,6 +96,27 @@ namespace Raven.Database.Data
 			"Via",
 			"Warning",
 		};
+
+		/// <summary>
+		/// Filters the headers from unwanted headers
+		/// </summary>
+		/// <param name="self">The self.</param>
+		/// <param name="isServerDocument">if set to <c>true</c> [is server document].</param>
+		/// <returns></returns>public static JObject FilterHeaders(this System.Collections.Specialized.NameValueCollection self, bool isServerDocument)
+		public static JObject FilterHeaders(this JObject self, bool isServerDocument)
+		{
+			var metadata = new JObject();
+			foreach (var header in self)
+			{
+				if (HeadersToIgnoreClient.Contains(header.Key))
+					continue;
+				if (isServerDocument && HeadersToIgnoreServerDocument.Contains(header.Key))
+					continue;
+				var headerName = CaptureHeaderName(header.Key);
+				metadata.Add(headerName, header.Value);
+			}
+			return metadata;
+		}
 
 #if SILVERLIGHT
 		/// <summary>
