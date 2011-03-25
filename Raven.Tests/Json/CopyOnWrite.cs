ade7085579b8e2ddf9a84a7ebd82c211fbfaf8b7
Itamar Syn-Hershko <itamar@ayende.com>
2011-03-25 13:58:57 +0200

    Some Raven.Json tests

---
+++ Raven.Tests/Json/CopyOnWrite.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Raven.Json.Linq;
+using Xunit;
+
+namespace Raven.Tests.Json
+{
+    public class CopyOnWrite
+    {
+        [Fact]
+        public void CopyOnWriteWorksCorrectly()
+        {
+            var f = new RavenJObject();
+            f.Properties["1"] = new RavenJValue(1);
+            f.Properties["2"] = new RavenJValue(2);
+
+            var f1 = (RavenJObject)f.CloneToken();
+            f1.Properties["2"] = new RavenJValue(3);
+
+            var val = (RavenJValue) f.Properties["2"];
+            Assert.Equal(2, val.Value);
+            val = (RavenJValue)f1.Properties["2"];
+            Assert.Equal(3, val.Value);
+
+            var f2 = (RavenJObject)f1.CloneToken();
+            val = (RavenJValue)f2.Properties["2"];
+            Assert.Equal(3, val.Value);
+
+            f.Properties["2"] = f2;
+            f1 = (RavenJObject) f.CloneToken();
+            f.Properties.Remove("2");
+            Assert.Null(f.Properties["2"]);
+            Assert.NotNull(f1.Properties["2"]);
+        }
+    }
+}
