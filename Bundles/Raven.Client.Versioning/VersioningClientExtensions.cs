9dad609e8549c602be3673e1c726934aacfe5e67
Ayende Rahien <ayende@ayende.com>
2011-03-31 12:10:03 +0100

    Adding a way to get all the revisions from the databases using the versioning bundle

---
+++ Bundles/Raven.Client.Versioning/VersioningClientExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Raven.Client.Document;
+
+namespace Raven.Client.Versioning
+{
+	public static class VersioningClientExtensions
+	{
+		public static T[] GetRevisionsFor<T>(this ISyncAdvancedSessionOperation session, string id, int start, int pageSize)
+		{
+			var inMemoryDocumentSessionOperations = ((InMemoryDocumentSessionOperations)session);
+			var jsonDocuments = session.DatabaseCommands.StartsWith(id + "/revisions/", start, pageSize);
+			return jsonDocuments
+				.Select(jsonDocument => inMemoryDocumentSessionOperations.TrackEntity<T>(jsonDocument.Key, jsonDocument.DataAsJson, jsonDocument.Metadata))
+				.ToArray();
+		}
+	}
+}
