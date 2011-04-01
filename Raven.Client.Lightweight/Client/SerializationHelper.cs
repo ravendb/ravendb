dd0fd7257827645746cb8be001e82621bbb1a158
Christopher Bennage <christopher@bennage.com>
2011-04-01 14:23:51 -0400

    additional tests regarding serialization helper

--- Raven.Client.Lightweight/Client/SerializationHelper.cs
+++ Raven.Client.Lightweight/Client/SerializationHelper.cs
@@ -28,8 +28,8 @@ namespace Raven.Client.Client
 					let _ = doc.Remove("@metadata")
 					let key = Extract(metadata, "@id", string.Empty)
 					let lastModified = Extract(metadata, "Last-Modified", DateTime.Now, (string d) => ConvertToUtcDate(d))
-					let etag = Extract(metadata, "@etag", Guid.Empty)
-					let nai = Extract(metadata, "Non-Authoritive-Information", false)
+					let etag = Extract(metadata, "@etag", Guid.Empty, (string g) => Guid.Parse(g))
+					let nai = Extract(metadata, "Non-Authoritive-Information", false, (string b) => Convert.ToBoolean(b))
 					select new JsonDocument
 					{
 						Key = key,
--- Raven.Tests.Silverlight/SerializationHelperTests.cs
+++ Raven.Tests.Silverlight/SerializationHelperTests.cs
@@ -48,7 +48,7 @@
 		[TestMethod]
 		public void Extracts_last_modified_date_from_metadata()
 		{
-			var april_fools = new DateTime(2011,4,1,4,20,0,DateTimeKind.Utc);
+			var april_fools = new DateTime(2011, 4, 1, 4, 20, 0, DateTimeKind.Utc);
 
 			var doc = new JObject();
 			doc["@metadata"] = new JObject();
@@ -64,15 +64,63 @@
 		{
 			var doc = new JObject();
 			doc["@metadata"] = new JObject();
-			
+
 			var now = DateTime.Now;
 
 			var output = SerializationHelper.JObjectsToJsonDocuments(new List<JObject> { doc });
 
 			var last_modified = output.First().LastModified;
-			var delta = Math.Abs( (last_modified - now).Seconds );
+			var delta = Math.Abs((last_modified - now).Seconds);
 
 			Assert.IsTrue(delta < 1);
+		}
+
+		[TestMethod]
+		public void Extracts_etag_from_metadata()
+		{
+			var etag = Guid.NewGuid();
+
+			var doc = new JObject();
+			doc["@metadata"] = new JObject();
+			doc["@metadata"]["@etag"] = etag.ToString();
+
+			var output = SerializationHelper.JObjectsToJsonDocuments(new List<JObject> { doc });
+
+			Assert.AreEqual(etag, output.First().Etag);
+		}
+
+		[TestMethod]
+		public void Assumes_empty_guid_if_etag_is_not_in_metadata()
+		{
+			var doc = new JObject();
+			doc["@metadata"] = new JObject();
+
+			var output = SerializationHelper.JObjectsToJsonDocuments(new List<JObject> { doc });
+
+			Assert.AreEqual(Guid.Empty, output.First().Etag);
+		}
+
+		[TestMethod]
+		public void Extracts_Non_Authoritive_flag_from_metadata()
+		{
+			var doc = new JObject();
+			doc["@metadata"] = new JObject();
+			doc["@metadata"]["Non-Authoritive-Information"] = true;
+
+			var output = SerializationHelper.JObjectsToJsonDocuments(new List<JObject> { doc });
+
+			Assert.AreEqual(true, output.First().NonAuthoritiveInformation);
+		}
+
+		[TestMethod]
+		public void Assumes_false_if_Non_Authoritive_flag_is_not_in_metadata()
+		{
+			var doc = new JObject();
+			doc["@metadata"] = new JObject();
+
+			var output = SerializationHelper.JObjectsToJsonDocuments(new List<JObject> { doc });
+
+			Assert.AreEqual(false, output.First().NonAuthoritiveInformation);
 		}
 	}
 }
