3826914827dc40721018136e184f3fc5ce4e1817
Itamar Syn-Hershko <itamar@ayende.com>
2011-04-04 20:32:33 +0300

    Should only clone if the value is an inheritted one

--- Utilities/CopyOnWriteDictionary.cs
+++ Utilities/CopyOnWriteDictionary.cs
@@ -113,10 +113,9 @@ namespace Raven.Json.Utilities
 			value = null;
 			RavenJToken unsafeVal;
 			if (localChanges != null && localChanges.TryGetValue(key, out unsafeVal))
-			{
-				if (unsafeVal == DeletedMarker) return false;
-			}
-			else if (inherittedValues == null || !inherittedValues.TryGetValue(key, out unsafeVal) || unsafeVal == DeletedMarker)
+				return unsafeVal != DeletedMarker;
+
+			if (inherittedValues == null || !inherittedValues.TryGetValue(key, out unsafeVal) || unsafeVal == DeletedMarker)
 				return false;
 
 			// Will also perform a copy-on-write clone on object supporting this
