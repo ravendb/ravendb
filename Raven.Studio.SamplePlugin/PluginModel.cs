11c3d09b95d0390cab31fda6e30cb5a36f17a93f
Christopher Bennage <christopher@bennage.com>
2011-04-05 15:37:24 -0400

    demo sample plugin that deletes a document with a known id

--- Raven.Studio.SamplePlugin/PluginModel.cs
+++ Raven.Studio.SamplePlugin/PluginModel.cs
@@ -1,9 +1,43 @@
 ﻿namespace Raven.Studio.SamplePlugin
 {
+	using System.ComponentModel.Composition;
+	using Caliburn.Micro;
+	using Plugins;
 	using Plugins.Tasks;
 
 	[ExportTask("Sample Plugin")]
-	public class PluginModel 
+	public class PluginModel : PropertyChangedBase
 	{
+		readonly IServer server;
+
+		[ImportingConstructor]
+		public PluginModel(IServer server)
+		{
+			this.server = server;
+			Status = "Ready to do something!";
+		}
+
+		public void DeleteTemporaryDocument()
+		{
+			using (var session = server.OpenSession())
+			{
+				session.Advanced.AsyncDatabaseCommands
+					.DeleteDocumentAsync("someKnownId")
+					.ContinueWith(_ =>
+					{
+						Status = _.IsFaulted
+							? "Um, bad thing or something..."
+							 : "Deleted!";
+					});
+			}
+		}
+
+		string status;
+
+		public string Status
+		{
+			get { return status; }
+			set { status = value; NotifyOfPropertyChange(() => Status); }
+		}
 	}
 }
