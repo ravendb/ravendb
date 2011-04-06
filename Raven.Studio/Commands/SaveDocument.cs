6315d2c757e25b98b1ccc098d8d0abb245c7959a
Christopher Bennage <christopher@bennage.com>
2011-04-06 13:07:01 -0400

    closes #48, the new document id is displayed on save

--- Raven.Studio/Commands/SaveDocument.cs
+++ Raven.Studio/Commands/SaveDocument.cs
@@ -36,12 +36,13 @@
 				.PutAsync(document.Id, jdoc.Etag, jdoc.DataAsJson, jdoc.Metadata)
 				.ContinueOnSuccess(put =>
 				                   	{
-										document.Id = put.Result.Key;
-				                   		document.JsonDocument.Metadata =
-				                   			document.JsonDocument.Metadata.FilterHeaders(isServerDocument: false);
-				                   	    document.JsonDocument.Etag = put.Result.ETag;
-										document.JsonDocument.LastModified = DateTime.Now;
+										var inner = document.JsonDocument;
+										inner.Key = put.Result.Key;
+										inner.Metadata = inner.Metadata.FilterHeaders(isServerDocument: false);
+										inner.Etag = put.Result.ETag;
+										inner.LastModified = DateTime.Now;
 				                        document.UpdateDocumentFromJsonDocument();
+
                                         events.Publish(new DocumentUpdated(document));
 				                   	});
 			
