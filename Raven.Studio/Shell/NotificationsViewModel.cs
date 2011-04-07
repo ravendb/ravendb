34ada60515f173101f8236ed05ba39de959e96ea
Christopher Bennage <christopher@bennage.com>
2011-04-07 17:01:40 -0400

    began improving the notification system

--- Raven.Studio/Shell/NotificationsViewModel.cs
+++ Raven.Studio/Shell/NotificationsViewModel.cs
@@ -13,38 +13,47 @@
 	                                      IHandle<NotificationRaised>
 	{
 		readonly DispatcherTimer notificationTimer;
+		readonly TimeSpan tick = new TimeSpan(0, 0, 0, 7);
 
 		[ImportingConstructor]
 		public NotificationsViewModel(IEventAggregator events)
 		{
 			events.Subscribe(this);
-			notificationTimer = new DispatcherTimer {Interval = new TimeSpan(0, 0, 0, 5)};
+			notificationTimer = new DispatcherTimer {Interval = tick};
 			notificationTimer.Tick += UpdateNotifications;
 			notificationTimer.Start();
 			Notifications = new BindableCollection<NotificationRaised>();
 		}
 
-		public NotificationRaised MostRecent
-		{
-			get { return Notifications.Any() ? Notifications[0] : null; }
-		}
+		public NotificationRaised MostRecent { get { return Notifications.Any() ? Notifications[0] : null; } }
+
+		public bool HasErrors { get { return Notifications.Any(_ => _.Level == NotificationLevel.Error); } }
 
 		public BindableCollection<NotificationRaised> Notifications { get; private set; }
 
 		void IHandle<NotificationRaised>.Handle(NotificationRaised message)
 		{
 			Notifications.Insert(0, message);
-			NotifyOfPropertyChange( ()=> MostRecent);
+			NotifyOfPropertyChange(() => MostRecent);
+		}
+
+		public void Dismiss(NotificationRaised message)
+		{
+			Notifications.Remove(message);
+			NotifyOfPropertyChange(() => MostRecent);
+			NotifyOfPropertyChange(() => HasErrors);
 		}
 
 		void UpdateNotifications(object sender, EventArgs e)
 		{
 			var remove = from item in Notifications
-			             where DateTime.Now - item.CreatedAt > new TimeSpan(0,0,0,5)
+			             let age = DateTime.Now - item.CreatedAt
+			             where age > tick && item.Level != NotificationLevel.Error
 			             select item;
 			remove.ToList().Apply(x => Notifications.Remove(x));
 
 			NotifyOfPropertyChange(() => MostRecent);
+			NotifyOfPropertyChange(() => HasErrors);
 		}
 	}
 }
