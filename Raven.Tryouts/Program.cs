8a9054e3a4288ca53493f6489b95589876f0bbd3
Ayende Rahien <ayende@ayende.com>
2011-04-07 15:48:55 +0300

    Creating an easy way to reproduce the race condition

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Raven.Tryouts
+﻿using System;
+using Raven.Tests.Bugs;
+
+namespace Raven.Tryouts
 {
 	class Program
 	{
 		static void Main()
 		{
+			for (int i = 0; i < 1000; i++)
+			{
+				Console.WriteLine(i);
+				new MultipleResultsPerDocumentAndPaging().WhenOutputingMultipleResultsPerDocAndPagingWillGetCorrectSize();
+			}
 		}
 	}
 }
