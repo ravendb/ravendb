c84b92bb401169998991d404827e56e04ec43e97
Ayende Rahien <ayende@ayende.com>
2011-04-07 15:54:45 +0300

    Removing Raven.Json reference

--- Raven.Tests/LocalClientTest.cs
+++ Raven.Tests/LocalClientTest.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Threading;
+using Newtonsoft.Json.Linq;
 using Raven.Client.Client;
 using Raven.Client.Indexes;
 using Raven.Database.Config;
@@ -72,8 +73,8 @@ namespace Raven.Tests
 				return;
 
 			documentStore.DatabaseCommands.Put("Pls Delete Me", null,
-											   RavenJObject.FromObject(new { StackTrace = new StackTrace(true) }),
-											   new RavenJObject());
+											   JObject.FromObject(new { StackTrace = new StackTrace(true) }),
+											   new JObject());
 			using (var server = new RavenDbHttpServer(documentStore.Configuration, documentStore.DocumentDatabase))
 			{
 				server.Start();
