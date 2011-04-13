31bf7e94433c7aa324ba801e3f534f98b7c1992d
Ayende Rahien <ayende@ayende.com>
2011-04-13 16:04:31 +0300

    Removing unused constants

--- Utilities/ValidationUtils.cs
+++ Utilities/ValidationUtils.cs
@@ -7,12 +7,6 @@ namespace Raven.Json.Utilities
 {
     internal static class ValidationUtils
     {
-        public const string EmailAddressRegex = @"^([a-zA-Z0-9_'+*$%\^&!\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9:]{2,4})+$";
-        public const string CurrencyRegex = @"(^\$?(?!0,?\d)\d{1,3}(,?\d{3})*(\.\d\d)?)$";
-        public const string DateRegex =
-            @"^(((0?[1-9]|[12]\d|3[01])[\.\-\/](0?[13578]|1[02])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}|\d))|((0?[1-9]|[12]\d|30)[\.\-\/](0?[13456789]|1[012])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}|\d))|((0?[1-9]|1\d|2[0-8])[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}|\d))|(29[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00|[048])))$";
-        public const string NumericRegex = @"\d*";
-
         public static void ArgumentNotNullOrEmpty(string value, string parameterName)
         {
             if (value == null)
