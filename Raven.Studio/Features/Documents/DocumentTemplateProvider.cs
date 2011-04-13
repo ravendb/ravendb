f42ea18a94e92592c3acc4967d7dcac8fd3d00b4
Christopher Bennage <christopher@bennage.com>
2011-04-13 14:51:54 -0400

    fixed a bug where the default template was hidden when an attempt to retrieve one from the server returned a 404

--- Raven.Studio/Features/Documents/DocumentTemplateProvider.cs
+++ Raven.Studio/Features/Documents/DocumentTemplateProvider.cs
@@ -74,13 +74,16 @@
 					.GetAttachmentAsync(key + "Template")
 					.ContinueWith(get =>
 					{
-						var template = GenerateTemplateFrom(get.Result,key);
+						var template = (get.Result != null)
+							? GenerateTemplateFrom(get.Result,key)
+							: GetDefaultTemplate(key);
+
 						lock (templates)
 						{
 							if (!templates.ContainsKey(key)) templates[key] = template;
 						}
 
-						// let those waiting know we've go it now 
+						// let those waiting know we've got it now 
 						DataTemplateRetrieved(this,new EventArgs<DataTemplate>(template));
 
 						tcs.TrySetResult(template);
@@ -140,9 +143,7 @@
 			return @"
 <Grid xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
 	  xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml"" 
-	  Margin=""0""
-	  Width=""120""
-	  Height=""60"">
+	  Margin=""0"">
 	<Rectangle Fill=""#FFF4F4F5"" />
 	<Rectangle Fill=""" +
 	fill +
@@ -166,9 +167,7 @@
 		static string GetProjectionTemplateXaml()
 		{
 			return
-				@"<Grid Margin=""0""
-				      Width=""120""
-				      Height=""60"">
+				@"<Grid Margin=""0"">
 				<Rectangle Fill=""#FFF4F4F5"" />
 		
 				<Grid Margin=""2,0,0,0"">
