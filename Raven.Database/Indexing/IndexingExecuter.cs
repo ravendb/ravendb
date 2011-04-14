62658380234bed1ec66814b3118a387b41db47f7
Ayende Rahien <Ayende@ayende.com>
2011-04-14 21:25:20 +0300

    Refactoring indexing executor to a separate class, preparing for reducing executor

--- Raven.Database/Indexing/IndexingExecuter.cs
+++ Raven.Database/Indexing/IndexingExecuter.cs
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------
-// <copyright file="TaskExecuter.cs" company="Hibernating Rhinos LTD">
+// <copyright file="IndexingExecuter.cs" company="Hibernating Rhinos LTD">
 //     Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using log4net;
 using Raven.Abstractions.Data;
 using Raven.Database.Extensions;
 using Raven.Database.Impl;
@@ -17,114 +16,13 @@ using Raven.Database.Storage;
 
 namespace Raven.Database.Indexing
 {
-	public class IndexingExecuter
-	{
-		private readonly WorkContext context;
-		private readonly TaskScheduler scheduler;
-		private readonly ILog log = LogManager.GetLogger(typeof(IndexingExecuter));
-		private readonly ITransactionalStorage transactionalStorage;
+    public class IndexingExecuter : AbstractIndexingExecuter
+    {
+        public IndexingExecuter(ITransactionalStorage transactionalStorage, WorkContext context, TaskScheduler scheduler) : base(transactionalStorage, context, scheduler)
+        {
+        }
 
-		public IndexingExecuter(ITransactionalStorage transactionalStorage, WorkContext context, TaskScheduler scheduler)
-		{
-			this.transactionalStorage = transactionalStorage;
-			this.context = context;
-			this.scheduler = scheduler;
-		}
-
-		int workCounter;
-		private int lastFlushedWorkCounter;
-
-		public void Execute()
-		{
-			while (context.DoWork)
-			{
-				var foundWork = false;
-				try
-				{
-					foundWork = ExecuteIndexing();
-				}
-				catch (Exception e)
-				{
-					log.Error("Failed to execute indexing", e);
-				}
-				if (foundWork == false)
-				{
-					context.WaitForWork(TimeSpan.FromHours(1), ref workCounter, FlushIndexes);
-				}
-				else // notify the tasks executer that it has work to do
-				{
-					context.NotifyAboutWork();
-				}
-			}
-		}
-
-		private void FlushIndexes()
-		{
-			if (lastFlushedWorkCounter == workCounter || context.DoWork == false)
-				return;
-			lastFlushedWorkCounter = workCounter;
-			context.IndexStorage.FlushAllIndexes();
-		}
-
-		private bool ExecuteIndexing()
-		{
-			var indexesToWorkOn = new List<IndexToWorkOn>();
-			transactionalStorage.Batch(actions =>
-			{
-				foreach (var indexesStat in actions.Indexing.GetIndexesStats())
-				{
-					var failureRate = actions.Indexing.GetFailureRate(indexesStat.Name);
-					if (failureRate.IsInvalidIndex)
-					{
-						log.InfoFormat("Skipped indexing documents for index: {0} because failure rate is too high: {1}",
-									   indexesStat.Name,
-									   failureRate.FailureRate);
-						continue;
-					}
-					if (!actions.Staleness.IsIndexStale(indexesStat.Name, null, null))
-						continue;
-					indexesToWorkOn.Add(new IndexToWorkOn
-					{
-						IndexName = indexesStat.Name,
-						LastIndexedEtag = indexesStat.LastIndexedEtag
-					});
-				}
-			});
-
-			if (indexesToWorkOn.Count == 0)
-				return false;
-
-			if(context.Configuration.MaxNumberOfParallelIndexTasks == 1)
-				ExecuteIndexingWorkOnSingleThread(indexesToWorkOn);
-			else
-				ExecuteIndexingWorkOnMultipleThreads(indexesToWorkOn);
-
-			return true;
-		}
-
-		private void ExecuteIndexingWorkOnMultipleThreads(IEnumerable<IndexToWorkOn> indexesToWorkOn)
-		{
-			ExecuteIndexingInternal(indexesToWorkOn, documents => Parallel.ForEach(indexesToWorkOn, new ParallelOptions
-			{
-				MaxDegreeOfParallelism = context.Configuration.MaxNumberOfParallelIndexTasks,
-				TaskScheduler = scheduler
-			}, indexToWorkOn => transactionalStorage.Batch(actions => IndexDocuments(actions, indexToWorkOn.IndexName, documents))));
-		}
-
-		private void ExecuteIndexingWorkOnSingleThread(IEnumerable<IndexToWorkOn> indexesToWorkOn)
-		{
-			ExecuteIndexingInternal(indexesToWorkOn, jsonDocs =>
-			{
-				foreach (var indexToWorkOn in indexesToWorkOn)
-				{
-					var copy = indexToWorkOn;
-					transactionalStorage.Batch(
-						actions => IndexDocuments(actions, copy.IndexName, jsonDocs));
-				}
-			});
-		}
-
-		private void ExecuteIndexingInternal(IEnumerable<IndexToWorkOn> indexesToWorkOn, Action<JsonDocument[]> indexingOp)
+        protected override void ExecuteIndexingInternal(IEnumerable<IndexToWorkOn> indexesToWorkOn, Action<JsonDocument[]> indexingOp)
 		{
 			var lastIndexedGuidForAllIndexes = indexesToWorkOn.Min(x => new ComparableByteArray(x.LastIndexedEtag.ToByteArray())).ToGuid();
 
@@ -171,18 +69,13 @@ namespace Raven.Database.Indexing
 			}
 		}
 
-		public class IndexToWorkOn
-		{
-			public string IndexName { get; set; }
-			public Guid LastIndexedEtag { get; set; }
+        protected override bool IsValidIndex(IndexStats indexesStat)
+        {
+            return true;
+        }
 
-			public override string ToString()
-			{
-				return string.Format("IndexName: {0}, LastIndexedEtag: {1}", IndexName, LastIndexedEtag);
-			}
-		}
 
-		private void IndexDocuments(IStorageActionsAccessor actions, string index, JsonDocument[] jsonDocs)
+        protected override void IndexDocuments(IStorageActionsAccessor actions, string index, JsonDocument[] jsonDocs)
 		{
 			var viewGenerator = context.IndexDefinitionStorage.GetViewGenerator(index);
 			if (viewGenerator == null)
@@ -209,36 +102,5 @@ namespace Raven.Database.Indexing
 			}
 		}
 
-		private class ComparableByteArray : IComparable<ComparableByteArray>, IComparable
-		{
-			private readonly byte[] inner;
-
-			public ComparableByteArray(byte[] inner)
-			{
-				this.inner = inner;
-			}
-
-			public int CompareTo(ComparableByteArray other)
-			{
-				if (inner.Length != other.inner.Length)
-					return inner.Length - other.inner.Length;
-				for (int i = 0; i < inner.Length; i++)
-				{
-					if (inner[i] != other.inner[i])
-						return inner[i] - other.inner[i];
-				}
-				return 0;
-			}
-
-			public int CompareTo(object obj)
-			{
-				return CompareTo((ComparableByteArray)obj);
-			}
-
-			public Guid ToGuid()
-			{
-				return new Guid(inner);
-			}
-		}
 	}
 }
