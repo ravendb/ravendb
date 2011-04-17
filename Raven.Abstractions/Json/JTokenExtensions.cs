5f1579d9f702b326e39ee7bbfd6e51522f3458f3
Ayende Rahien <ayende@ayende.com>
2011-04-17 20:09:19 +0300

    Moving 3.5 tuple class to Raven.Json project

--- Raven.Abstractions/Json/JTokenExtensions.cs
+++ Raven.Abstractions/Json/JTokenExtensions.cs
@@ -8,30 +8,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Raven.Json.Linq;
 
-#if NET_3_5
-namespace System
-{
-	public class Tuple<T, U>
-	{
-		public T Item1 { get; private set; }
-		public U Item2 { get; private set; }
-
-		public Tuple(T item1, U item2)
-		{
-			Item1 = item1;
-			Item2 = item2;
-		}
-	}
-
-	public static class Tuple
-	{
-		public static Tuple<T, U> Create<T, U>(T item1, U item2)
-		{
-			return new Tuple<T, U>(item1, item2);
-		}
-	}
-}
-#endif
 
 namespace Raven.Abstractions.Json
 {
