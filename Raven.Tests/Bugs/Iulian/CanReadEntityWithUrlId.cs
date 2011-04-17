28c4ad823618aa791b13fefd52613ea9ac823402
Ayende Rahien <ayende@ayende.com>
2011-04-17 09:56:04 +0300

    Making the test pass

--- Raven.Tests/Bugs/Iulian/CanReadEntityWithUrlId.cs
+++ Raven.Tests/Bugs/Iulian/CanReadEntityWithUrlId.cs
@@ -40,7 +40,7 @@ namespace Raven.Tests.Bugs.Iulian
 
                 using (var s = store.OpenSession())
                 {
-                    Event loaded = s.Load<Event>(id);
+					Event loaded = s.Load<Event>(Uri.EscapeDataString(Uri.EscapeDataString(id)));
                     // this fails
                     Assert.NotNull(loaded);
                     Assert.Equal("tag", loaded.Tag);
