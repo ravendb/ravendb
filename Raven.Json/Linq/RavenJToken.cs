b326db86e75df87f525d6a191aa68bcff7c9b55b
Itamar Syn-Hershko <itamar@ayende.com>
2011-04-22 01:49:32 +0300

    DeepEquals should not be recursive

--- Raven.Json/Linq/RavenJToken.cs
+++ Raven.Json/Linq/RavenJToken.cs
@@ -217,9 +217,66 @@ namespace Raven.Json.Linq
 			return (t1 == t2 || (t1 != null && t2 != null && t1.DeepEquals(t2)));
 		}
 
-		internal abstract bool DeepEquals(RavenJToken node);
+		internal virtual bool DeepEquals(RavenJToken other)
+		{
+			if (other == null)
+				return false;
 
-		internal virtual int GetDeepHashCode()
+			if (Type != other.Type)
+				return false;
+
+			var otherStack = new Stack<RavenJToken>();
+			var thisStack = new Stack<RavenJToken>();
+
+			thisStack.Push(this);
+			otherStack.Push(other);
+
+			while (otherStack.Count > 0)
+			{
+				var curOtherReader = otherStack.Pop();
+				var curThisReader = thisStack.Pop();
+
+				if (curThisReader.Type != curOtherReader.Type) return false;
+
+				if (curOtherReader.Type == JTokenType.Array)
+				{
+					var selfArray = (RavenJArray) curThisReader;
+					var otherArray = (RavenJArray) curOtherReader;
+					if (selfArray.Length != otherArray.Length)
+						return false;
+
+					for (int i = 0; i < selfArray.Length; i++)
+					{
+						thisStack.Push(selfArray[i]);
+						otherStack.Push(otherArray[i]);
+					}
+				}
+				else if (curOtherReader.Type == JTokenType.Object)
+				{
+					var selfObj = (RavenJObject) curThisReader;
+					var otherObj = (RavenJObject) curOtherReader;
+					if (selfObj.Count != otherObj.Count)
+						return false;
+
+					foreach (var kvp in selfObj.Properties)
+					{
+						RavenJToken token;
+						if (otherObj.TryGetValue(kvp.Key, out token) == false)
+							return false;
+						otherStack.Push(token);
+						thisStack.Push(kvp.Value);
+					}
+				}
+				else // value
+				{
+					if (!curOtherReader.DeepEquals(curThisReader)) return false;
+				}
+			}
+
+			return true;
+		}
+
+    	internal virtual int GetDeepHashCode()
 		{
 			var stack = new Stack<Tuple<int, RavenJToken>>();
 			int ret = 0;
