f7d5eaffb350a194e48ae41fcd93f9d936817ee2
Ayende Rahien <ayende@ayende.com>
2011-04-28 14:46:27 +0300

    More tests

--- Raven.Tests/Storage/ReduceStaleness.cs
+++ Raven.Tests/Storage/ReduceStaleness.cs
@@ -46,32 +46,7 @@ namespace Raven.Tests.Storage
 
 		}
 
-		[Fact]
-		public void Esent_when_there_are_multiple_map_results_and_we_ask_for_results()
-		{
-			using (var transactionalStorage = new EsentTransactionalStorage(new RavenConfiguration
-			{
-				DataDirectory = "Test"
-			}, () => { }))
-			{
-				when_there_are_multiple_map_results_and_we_ask_for_results(transactionalStorage);
-			}
-
-		}
-
-
-		[Fact]
-		public void Munin_when_there_are_multiple_map_results_and_we_ask_for_results()
-		{
-			using (var transactionalStorage = new MuninTransactionalStorage(new RavenConfiguration
-			{
-				DataDirectory = "Test"
-			}, () => { }))
-			{
-				when_there_are_multiple_map_results_and_we_ask_for_results(transactionalStorage);
-			}
-		}
-
+		
 
 
 		private static void when_there_are_multiple_map_results_for_multiple_indexes(ITransactionalStorage transactionalStorage)
@@ -100,6 +75,32 @@ namespace Raven.Tests.Storage
 			});
 		}
 
+		[Fact]
+		public void Esent_when_there_are_multiple_map_results_and_we_ask_for_results()
+		{
+			using (var transactionalStorage = new EsentTransactionalStorage(new RavenConfiguration
+			{
+				DataDirectory = "Test"
+			}, () => { }))
+			{
+				when_there_are_multiple_map_results_and_we_ask_for_results(transactionalStorage);
+			}
+
+		}
+
+
+		[Fact]
+		public void Munin_when_there_are_multiple_map_results_and_we_ask_for_results()
+		{
+			using (var transactionalStorage = new MuninTransactionalStorage(new RavenConfiguration
+			{
+				DataDirectory = "Test"
+			}, () => { }))
+			{
+				when_there_are_multiple_map_results_and_we_ask_for_results(transactionalStorage);
+			}
+		}
+
 		private static void when_there_are_multiple_map_results_and_we_ask_for_results(ITransactionalStorage transactionalStorage)
 		{
 			transactionalStorage.Initialize(new DummyUuidGenerator());
@@ -126,6 +127,64 @@ namespace Raven.Tests.Storage
 			});
 		}
 
+		[Fact]
+		public void Esent_when_there_are_updates_to_map_reduce_results()
+		{
+			using (var transactionalStorage = new EsentTransactionalStorage(new RavenConfiguration
+			{
+				DataDirectory = "Test"
+			}, () => { }))
+			{
+				when_there_are_updates_to_map_reduce_results(transactionalStorage);
+			}
+
+		}
+
+
+		[Fact]
+		public void Munin_when_there_are_updates_to_map_reduce_results()
+		{
+			using (var transactionalStorage = new MuninTransactionalStorage(new RavenConfiguration
+			{
+				DataDirectory = "Test"
+			}, () => { }))
+			{
+				when_there_are_updates_to_map_reduce_results(transactionalStorage);
+			}
+		}
+
+
+		private static void when_there_are_updates_to_map_reduce_results(ITransactionalStorage transactionalStorage)
+		{
+			var dummyUuidGenerator = new DummyUuidGenerator();
+			transactionalStorage.Initialize(dummyUuidGenerator);
+			Guid a = Guid.Empty;
+			Guid b = Guid.Empty;
+			Guid c = Guid.Empty;
+			transactionalStorage.Batch(accessor =>
+			{
+				accessor.Indexing.AddIndex("a", true);
+				accessor.Indexing.AddIndex("b", true);
+				accessor.Indexing.AddIndex("c", true);
+
+				accessor.MappedResults.PutMappedResult("a", "a/1", "a", new RavenJObject(), MapReduceIndex.ComputeHash("a", "a"));
+				a = dummyUuidGenerator.CreateSequentialUuid();
+				accessor.MappedResults.PutMappedResult("a", "a/2", "a", new RavenJObject(), MapReduceIndex.ComputeHash("a", "a"));
+				accessor.MappedResults.PutMappedResult("b", "a/1", "a", new RavenJObject(), MapReduceIndex.ComputeHash("b", "a"));
+				b = dummyUuidGenerator.CreateSequentialUuid();
+				accessor.MappedResults.PutMappedResult("b", "a/1", "a", new RavenJObject(), MapReduceIndex.ComputeHash("b", "a"));
+				accessor.MappedResults.PutMappedResult("c", "a/1", "a", new RavenJObject(), MapReduceIndex.ComputeHash("c", "a"));
+				c = dummyUuidGenerator.CreateSequentialUuid();
+				accessor.MappedResults.PutMappedResult("c", "a/1", "a", new RavenJObject(), MapReduceIndex.ComputeHash("c", "a"));
+			});
+
+			transactionalStorage.Batch(actionsAccessor =>
+			{
+				Assert.Equal(1, actionsAccessor.MappedResults.GetMappedResultsReduceKeysAfter("a", a).Count());
+				Assert.Equal(1, actionsAccessor.MappedResults.GetMappedResultsReduceKeysAfter("b", b).Count());
+				Assert.Equal(1, actionsAccessor.MappedResults.GetMappedResultsReduceKeysAfter("c", c).Count());
+			});
+		}
 
 		/// <summary>
 		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
