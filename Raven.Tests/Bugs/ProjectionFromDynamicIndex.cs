6eb7389c965ce4b61d719645308f26bcd1544d29
Ayende Rahien <ayende@ayende.com>
2011-05-04 13:26:32 +0300

    Adding passing test

---
+++ Raven.Tests/Bugs/ProjectionFromDynamicIndex.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using Microsoft.Isam.Esent.Interop;
+using Xunit;
+
+namespace Raven.Tests.Bugs
+{
+	public class ProjectionFromDynamicIndex : LocalClientTest
+	{
+		[Fact]
+		public void ShouldWork()
+		{
+			using(var store = NewDocumentStore())
+			{
+
+				using (var session = store.OpenSession())
+				{
+					session.Store(new Post{Title = "ayende"});
+					session.SaveChanges();
+				}
+
+				using(var session =store.OpenSession())
+				{
+					var postReference = session.Query<Post>()
+						.Select(p => new {Id = p.Id, p.Title})
+						.First();
+
+					Assert.Equal("ayende", postReference.Title);
+					Assert.Equal("posts/1", postReference.Id);
+				}
+			}
+
+
+		}
+
+		public class Post
+		{
+			public string Id { get; set; }
+			public string Title { get; set; }
+		}
+	}
+}
