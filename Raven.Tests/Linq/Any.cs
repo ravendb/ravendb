0feda7078d3281d9174feaa18fc122a120acb1ae
Itamar Syn-Hershko <itamar@ayende.com>
2011-05-04 20:44:38 +0300

    Adding failing test

---
+++ Raven.Tests/Linq/Any.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Linq;
+using Xunit;
+
+namespace Raven.Tests.Linq
+{
+	public class Any : LocalClientTest
+	{
+		private class TestDoc
+		{
+			public string SomeProperty { get; set; }
+			public string[] StringArray { get; set; }
+		}
+
+		[Fact]
+		public void CanQueryArray()
+		{
+			using (var store = NewDocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					var doc = new TestDoc {StringArray = new string[] {"test", "doc", "foo"}};
+					session.Store(doc);
+					session.SaveChanges();
+				}
+
+				using (var session = store.OpenSession())
+				{
+					var otherDoc = new TestDoc {SomeProperty = "foo"};
+					var doc = (from ar in session.Query<TestDoc>()
+							   where ar.StringArray.Any(ac => ac == otherDoc.SomeProperty)
+							   select ar).FirstOrDefault();
+					Assert.NotNull(doc);
+				}
+			}
+		}
+	}
+}
