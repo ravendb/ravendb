602bcc1a80191012ab2ba928c9297502bc5ea535
Ayende Rahien <ayende@ayende.com>
2011-05-06 12:44:24 +0300

    Fixing test to show how it is supposed to be done

--- Raven.Tests/Bugs/Indexing/CanIndexNestedObjects.cs
+++ Raven.Tests/Bugs/Indexing/CanIndexNestedObjects.cs
@@ -2,69 +2,70 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Raven.Abstractions.Indexing;
 using Raven.Client.Indexes;
 using Xunit;
+using Raven.Client.Linq;
 
 namespace Raven.Tests.Bugs.Indexing
 {
-    public class CanIndexNestedObjects : LocalClientTest
-    {
-        public class NestedObject
-        {
-            public string Name;
-            public int Quantity;
-        }
+	public class CanIndexNestedObjects : LocalClientTest
+	{
+		public class NestedObject
+		{
+			public string Name { get; set; }
+			public int Quantity { get; set; }
+		}
 
-        public class ContainerObject
-        {
-            public string ContainerName;
-            public IEnumerable<NestedObject> Items;
-        }
+		public class ContainerObject
+		{
+			public string ContainerName { get; set; }
+			public IEnumerable<NestedObject> Items { get; set; }
+		}
 
-        public class IndexEntry
-        {
-            public string ContainerName;
-            public string Name;
-            public int Quantity;
-        }
+		public class IndexEntry
+		{
+			public string ContainerName { get; set; }
+			public string Name { get; set; }
+			public int Quantity { get; set; }
+		}
 
-        public class NestedObjectIndex : AbstractIndexCreationTask<ContainerObject, IndexEntry>
-        {
-            public override Abstractions.Indexing.IndexDefinition CreateIndexDefinition()
-            {
-                return new IndexDefinitionBuilder<ContainerObject, IndexEntry>()
-                {
-                    Map = docs => from doc in docs
-                                  from item in doc.Items
-                                  select new
-                                  {
-                                      ContainerName = doc.ContainerName,
-                                      Name = item.Name,
-                                      Quantity = item.Quantity
-                                  }
+		public class NestedObjectIndex : AbstractIndexCreationTask<ContainerObject, IndexEntry>
+		{
+			public NestedObjectIndex()
+			{
+				Map = docs => from doc in docs
+							  from item in doc.Items
+							  select new
+							  {
+								  doc.ContainerName,
+								  item.Name,
+								  item.Quantity
+							  };
+				Store(x => x.Name, FieldStorage.Yes);
+				Store(x => x.ContainerName, FieldStorage.Yes);
+				Store(x => x.Quantity, FieldStorage.Yes);
+			}
+		}
 
-                }.ToIndexDefinition(Conventions);
-            }
-        }
+		[Fact]
+		public void SimpleInsertAndRead()
+		{
+			string expectedContainerName = "someContainer123098";
+			string expectedItemName = "someItem456";
+			int expectedQuantity = 123;
 
-        [Fact]
-        public void SimpleInsertAndRead()
-        {
-            string expectedContainerName = "someContainer123098";
-            string expectedItemName = "someItem456";
-            int expectedQuantity = 123;
+			using (var store = NewDocumentStore())
+			{
+				IndexCreation.CreateIndexes(typeof(NestedObjectIndex).Assembly, store);
 
-            using (var store = NewDocumentStore())
-            {
-                IndexCreation.CreateIndexes(typeof(NestedObjectIndex).Assembly, store);
-                
 
-                using (var s = store.OpenSession())
-                {
-                    s.Store(new ContainerObject()
-                    {
-                        ContainerName = expectedContainerName,
-                        Items = new[]
+				using (var s = store.OpenSession())
+				{
+					s.Store(new ContainerObject()
+					{
+						ContainerName = expectedContainerName,
+						Items = new[]
                         {
                             new NestedObject()
                             {
@@ -77,34 +78,35 @@ namespace Raven.Tests.Bugs.Indexing
                                 Quantity = 345
                             }
 		                }
-                    });
+					});
 
-                    s.SaveChanges();
-                }
+					s.SaveChanges();
+				}
 
-                //  the index has two objects
-                using (var s = store.OpenSession())
-                {
-                    var result = s.Query<IndexEntry, NestedObjectIndex>()
-                        .Customize(q => q.WaitForNonStaleResultsAsOfNow())
-                        .ToArray();
+				//  the index has two objects
+				using (var s = store.OpenSession())
+				{
+					var result = s.Query<IndexEntry, NestedObjectIndex>()
+						.Customize(q => q.WaitForNonStaleResultsAsOfNow())
+						.Count();
 
-                    Assert.Equal(2, result.Count());
-                }
+					Assert.Equal(2, result);
+				}
 
-                //  and the index can be queried
-                using (var s = store.OpenSession())
-                {
-                    var result = s.Query<IndexEntry, NestedObjectIndex>()
-                        .Customize(q => q.WaitForNonStaleResultsAsOfNow())
-                        .Where(o => o.Name == expectedItemName)
-                        .Single();
+				//  and the index can be queried
+				using (var s = store.OpenSession())
+				{
+					var result = s.Query<IndexEntry, NestedObjectIndex>()
+						.Customize(q => q.WaitForNonStaleResultsAsOfNow())
+						.AsProjection<IndexEntry>()
+						.Where(o => o.Name == expectedItemName)
+						.Single();
 
-                    Assert.Equal(expectedContainerName, result.ContainerName);
-                    Assert.Equal(expectedItemName, result.Name);
-                    Assert.Equal(expectedQuantity, result.Quantity);
-                }
-            }
-        }
-    }
+					Assert.Equal(expectedContainerName, result.ContainerName);
+					Assert.Equal(expectedItemName, result.Name);
+					Assert.Equal(expectedQuantity, result.Quantity);
+				}
+			}
+		}
+	}
 }
