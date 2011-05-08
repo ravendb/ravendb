6775872139faf90c5f0aa1db0d47e7228e60b8fb
Itamar Syn-Hershko <itamar@ayende.com>
2011-05-08 12:24:48 +0300

    Work in progress: adding Statistics support to LuceneQuery

--- Raven.Tests/Bugs/Queries/StatsOnDynamicQueries.cs
+++ Raven.Tests/Bugs/Queries/StatsOnDynamicQueries.cs
@@ -34,5 +34,33 @@ namespace Raven.Tests.Bugs.Queries
 			}
 		}
 
+		[Fact]
+		public void WillGiveStatsForLuceneQuery()
+		{
+			using (var store = NewDocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					session.Store(new User
+					{
+						Age = 15,
+						Email = "ayende"
+					});
+					session.SaveChanges();
+				}
+
+				using (var session = store.OpenSession())
+				{
+					RavenQueryStatistics stats;
+					var query = session.Advanced.LuceneQuery<User>()
+						.Statistics(out stats)
+						.Where("Email:ayende");
+
+					var result = query.ToArray();
+					Assert.NotEqual(0, stats.TotalResults);
+					Assert.Equal(stats.TotalResults, query.QueryResult.TotalResults);
+				}
+			}
+		}
 	}
 }
