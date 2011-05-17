63801738ccf738429e49d14899a3ea0a27fb5afa
Ayende Rahien <ayende@ayende.com>
2011-05-17 18:04:14 +0300

    Minor

--- Raven.Tests/Bugs/CompiledIndexes/UsingNetworkEventsToNetworkTemp.cs
+++ Raven.Tests/Bugs/CompiledIndexes/UsingNetworkEventsToNetworkTemp.cs
@@ -28,25 +28,6 @@ namespace Raven.Tests.Bugs.CompiledIndexes
 					s.SaveChanges();
 				}
 
-
-				var persistenceSource = ((TransactionalStorage)store.DocumentDatabase.TransactionalStorage).PersistenceSource;
-				var persistentDictionaryStates = persistenceSource.DictionariesStates;
-
-				//for (int i = 0; i < 500; i++)
-				//{
-				//    bool cont = false;
-				//    store.DocumentDatabase.TransactionalStorage.Batch(accessor =>
-				//    {
-				//        var indexFailureInformation = accessor.Indexing.GetFailureRate("Aggregates/NetworkTest");
-
-				//        cont = indexFailureInformation.ReduceSuccesses == 1;
-				//    });
-				//    if (cont == false)
-				//        Thread.Sleep(100);
-				//    else
-				//        break;
-				//}
-
 				using(var s = store.OpenSession())
 				{
 					var list = s.Advanced.LuceneQuery<dynamic>("Aggregates/NetworkTest")
