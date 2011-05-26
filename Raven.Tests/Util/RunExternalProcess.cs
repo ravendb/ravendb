88bee3b5b4035f70df9538457bd4fc523784d1f7
fschwiet <franks@.allrecipes.corp>
2011-05-26 16:59:38 -0700

    and we can use batch commands...

--- Raven.Tests/Util/RunExternalProcess.cs
+++ Raven.Tests/Util/RunExternalProcess.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Raven.Abstractions.Commands;
 using Raven.Client.Document;
+using Raven.Json.Linq;
 using Xunit;
 
 namespace Raven.Tests.Util
@@ -29,6 +31,50 @@ namespace Raven.Tests.Util
                     using (var session = store.OpenSession())
                     {
                         session.Store(new Tuple<string, string>("hello", "world"));
+                        session.SaveChanges();
+                    }
+
+                    using (var session = store.OpenSession())
+                    {
+                        var result = session.Query<Tuple<string, string>>().Customize(q => q.WaitForNonStaleResultsAsOfNow()).Single();
+
+                        Assert.Equal("hello", result.Item1);
+                        Assert.Equal("world", result.Item2);
+                    }
+                }
+            }
+        }
+
+
+        [Fact]
+        public void can_use_RavenDB_in_a_remote_process_for_batch_operations()
+        {
+            var documentConvention = new DocumentConvention();
+
+            using (var driver = new RavenDBDriver("HelloShard", documentConvention))
+            {
+                driver.Start();
+
+                using (var store = new DocumentStore()
+                {
+                    Url = driver.Url,
+                    Conventions = documentConvention
+                })
+                {
+                    store.Initialize();
+
+                    using (var session = store.OpenSession())
+                    {
+                        session.Advanced.DatabaseCommands.Batch(new [] {new PutCommandData()
+                            {
+                                Document = RavenJObject.FromObject(new Tuple<string, string>("hello", "world")),
+                                Metadata = new RavenJObject(new KeyValuePair<string, RavenJToken>[]
+                                {
+                                    new KeyValuePair<string, RavenJToken>("Raven-Entity-Name", new RavenJValue("TuplesOfStringsOfStrings")), 
+                                    new KeyValuePair<string, RavenJToken>("Raven-Clr-Type", new RavenJValue("System.Tuple`2[[System.String, mscorlib][System.String, mscorlib]], mscorlib")), 
+                                })
+                            }
+                        });
                         session.SaveChanges();
                     }
 
