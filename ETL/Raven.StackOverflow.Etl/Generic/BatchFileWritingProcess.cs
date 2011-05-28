84c29e7688f82616b33dfba1f91b4d2bf9e8b1ff
fschwiet <fschwiet@gmail.com>
2011-05-28 15:08:19 -0700

    minor move

--- ETL/Raven.StackOverflow.Etl/Generic/BatchFileWritingProcess.cs
+++ ETL/Raven.StackOverflow.Etl/Generic/BatchFileWritingProcess.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using Rhino.Etl.Core.Operations;
 
-namespace Raven.StackOverflow.Etl.Posts
+namespace Raven.StackOverflow.Etl.Generic
 {
 	public abstract class BatchFileWritingProcess : AbstractOperation
 	{
