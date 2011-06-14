fefb620b57b52a87268a19dd47ff14d01cc9d465
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-06-14 14:53:10 +0300

    Refactor document ListBox to a separate UserControl.

---
+++ Raven.Studio/Features/Documents/DocumentsList.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using SL4PopupMenu;
+
+namespace Raven.Studio.Features.Documents
+{
+	public partial class DocumentsList : UserControl
+	{
+		public DocumentsList()
+		{
+			InitializeComponent();
+		}
+	}
+}
