5210ab27c0b85b80cbd58d84ced5db3fdf8db7ca
Ayende Rahien <ayende@ayende.com>
2011-06-17 15:30:56 +0300

    Adding passing test

--- Raven.Tests/Bugs/NullableDateTime.cs
+++ Raven.Tests/Bugs/NullableDateTime.cs
@@ -1,4 +1,8 @@
 using System;
+using Raven.Abstractions.Indexing;
+using Raven.Client;
+using Raven.Client.Linq;
+using Raven.Client.Indexes;
 using Xunit;
 using System.Linq;
 
@@ -28,10 +32,62 @@ namespace Raven.Tests.Bugs
 			}
 		}
 
+		[Fact]
+		public void CanLoadFromIndex()
+		{
+			using(var documentStore = NewDocumentStore())
+			{
+				using (IDocumentSession session = documentStore.OpenSession())
+				{
+					IndexCreation.CreateIndexes(typeof (Doc).Assembly, documentStore);
+					session.Store(new Doc {Id = "test/doc1", Date = DateTime.UtcNow});
+					session.Store(new Doc {Id = "test/doc2", Date = null});
+					session.SaveChanges();
+
+				}
+
+				using(var session = documentStore.OpenSession())
+				{
+					session
+						.Query<Doc, UnsetDocs>()
+						.Customize(x => x.WaitForNonStaleResults())
+						.AsProjection<DocSummary>()
+						.ToArray();
+				}
+			}
+
+		}
 		public class WithNullableDateTime
 		{
 			public string Id { get; set; }
 			public DateTime? CreatedAt { get; set; }
+		}
+
+		public class Doc
+		{
+			public string Id { get; set; }
+			public DateTime? Date { get; set; }
+		}
+
+		public class DocSummary
+		{
+			public string Id { get; set; }
+			public DateTime? MaxDate { get; set; }
+		}
+
+		public class UnsetDocs : AbstractIndexCreationTask<Doc, DocSummary>
+		{
+			public UnsetDocs()
+			{
+				Map = docs =>
+						from doc in docs
+						select new
+						{
+							doc.Id,
+							MaxDate = doc.Date
+						};
+				Store(x => x.MaxDate, FieldStorage.Yes);
+			}
 		}
 	}
 }
