f27a6520ec9cbf7a1a7bd468853e530c1c23b31f
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-06-22 14:13:38 +0300

    Remove unused special value

--- Raven.Studio/AppBootstrapper.cs
+++ Raven.Studio/AppBootstrapper.cs
@@ -88,12 +88,6 @@ namespace Raven.Studio
 			var original = ViewLocator.LocateForModelType;
 			ViewLocator.LocateForModelType = (t, v, c) => { return StudioViewLocator.LocateForModelType(t, v, c, original); };
 
-			MessageBinder.SpecialValues["$idsintag"] = context =>
-			{
-			    var tag = context.Source.Tag as string;
-			    return tag.Split(',');
-			};
-
 		    MessageBinder.SpecialValues["$selecteditems"] = context => {
                 ListBox listBox;
 
