c0aad4842019b55d765b31e5f6042ecce8cfab7e
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-06-23 11:32:18 +0300

    Refactor common code in DocumentsContextMenu to a base ContextMenuBase class

---
+++ Raven.Studio/Behaviors/ContextMenuBase.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Caliburn.Micro;
+using Raven.Studio.Common;
+using SL4PopupMenu;
+
+namespace Raven.Studio.Behaviors
+{
+	public abstract class ContextMenuBase<T>
+	{
+		protected readonly IList<T> SelectedItems;
+		protected readonly T SelectedItem;
+		protected readonly bool IsInMultiSelectedMode;
+		protected readonly PopupMenu Menu;
+
+		protected ContextMenuBase(IList<T> selectedItems)
+		{
+			if (selectedItems == null)
+				throw new ArgumentNullException("selectedItems", "Selected items cannot be null");
+
+			SelectedItems = selectedItems;
+			if (SelectedItems.Count > 1)
+				IsInMultiSelectedMode = true;
+			else
+			{
+				SelectedItem = SelectedItems.FirstOrDefault();
+				if (SelectedItem == null)
+					throw new InvalidOperationException("Selected items must contain at least one item");
+			}
+
+			Menu = new PopupMenu();
+
+			var canvas = Menu.ItemsControl.Parent as Canvas;
+			if (canvas != null) canvas.MouseMove += (s, e) => { Mouse.Position = e.GetPosition(null); };
+
+			Menu.Closing += FocusTheClickOnItem;
+		}
+
+		protected abstract void GenerateMenuItems();
+
+		public void Open(FrameworkElement element, MouseButtonEventArgs e)
+		{
+			GenerateMenuItems();
+			Menu.Open(Mouse.Position, MenuOrientationTypes.MouseBottomRight, 0, element, true, e);
+		}
+
+		private void FocusTheClickOnItem(object sender, RoutedEventArgs e)
+		{
+			var elementsInHostCoordinates = VisualTreeHelper.FindElementsInHostCoordinates(Mouse.Position,
+																						   Application.Current.RootVisual);
+			elementsInHostCoordinates
+				.Where(element => element is ListBoxItem)
+				.OfType<ListBoxItem>()
+				.ToList()
+				.ForEach(item =>
+				{
+					var parent = VisualTreeHelperExtensions.GetParentOfType<ListBox>(item);
+					if (parent != null && parent.SelectionMode != SelectionMode.Single)
+						parent.SelectedItems.Clear();
+					item.IsSelected = true;
+				});
+		}
+	}
+}
--- Raven.Studio/Behaviors/DocumentsContextMenu.cs
+++ Raven.Studio/Behaviors/DocumentsContextMenu.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
@@ -7,88 +6,35 @@ using System.Windows.Input;
 using System.Windows.Media;
 using Caliburn.Micro;
 using Raven.Studio.Commands;
-using Raven.Studio.Common;
 using Raven.Studio.Features.Documents;
 using Raven.Studio.Features.Documents.Resources;
 using SL4PopupMenu;
 
 namespace Raven.Studio.Behaviors
 {
-	public class DocumentsContextMenu
+	public class DocumentsContextMenu : ContextMenuBase<DocumentViewModel>
 	{
-		private readonly IList<DocumentViewModel> selectedItems;
-		private readonly DocumentViewModel selectedItem;
-		private readonly bool isInMultiSelectedMode;
-
-		private PopupMenu menu;
-		private PopupMenuItem editDocumentMenuItem;
-		private PopupMenuItem copyIdMenuItem;
-
 		public DocumentsContextMenu(IList<DocumentViewModel> selectedItems)
+			: base(selectedItems)
 		{
-			if (selectedItems == null)
-				throw new ArgumentNullException("selectedItems", "Selected items cannot be null");
+		}
 
-			this.selectedItems = selectedItems;
-			if (selectedItems.Count > 1)
-				isInMultiSelectedMode = true;
-			else
+		protected override void GenerateMenuItems()
+		{
+			if (IsInMultiSelectedMode == false)
 			{
-				selectedItem = selectedItems.FirstOrDefault();
-				if (selectedItem == null)
-					throw new InvalidOperationException("Selected items must contain at least one item");
-			}
-		}
+				var editDocumentMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_EditDocument);
+				editDocumentMenuItem.Click += (s, ea) => IoC.Get<EditDocument>().Execute(SelectedItem);
+				Menu.AddItem(editDocumentMenuItem);
 
-		public void Open(FrameworkElement element, MouseButtonEventArgs e)
-		{
-			CreateMenu();
-			menu.Open(Mouse.Position, MenuOrientationTypes.MouseBottomRight, 0, element, true, e);
-		}
+				var copyIdMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_CopyId);
+				copyIdMenuItem.Click += (s, ea) => IoC.Get<CopyDocumentIdToClipboard>().Execute(SelectedItem.Id);
+				Menu.AddItem(copyIdMenuItem);
 
-		private void FocusTheClickOnItem(object sender, RoutedEventArgs e)
-		{
-			var elementsInHostCoordinates = VisualTreeHelper.FindElementsInHostCoordinates(Mouse.Position,
-			                                                                               Application.Current.RootVisual);
-			elementsInHostCoordinates
-				.Where(element => element is ListBoxItem)
-				.OfType<ListBoxItem>()
-				.ToList()
-				.ForEach(item =>
-				         	{
-				         		var parent = VisualTreeHelperExtensions.GetParentOfType<ListBox>(item);
-				         		if (parent != null && parent.SelectionMode != SelectionMode.Single)
-				         			parent.SelectedItems.Clear();
-				         		item.IsSelected = true;
-				         	});
-		}
-
-		private void CreateMenu()
-		{
-			menu = new PopupMenu();
-
-			if (isInMultiSelectedMode == false)
-			{
-				editDocumentMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_EditDocument);
-				editDocumentMenuItem.Click += (s, ea) =>
-				                              	{
-				                              		IoC.Get<EditDocument>().Execute(selectedItem);
-				                              	};
-				menu.AddItem(editDocumentMenuItem);
-
-				copyIdMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_CopyId);
-				copyIdMenuItem.Click += (s, ea) => IoC.Get<CopyDocumentIdToClipboard>().Execute(selectedItem.Id);
-				menu.AddItem(copyIdMenuItem);
-
-				menu.AddSeparator();
+				Menu.AddSeparator();
 			}
 
-			menu.AddItem(DocumentsResources.DocumentMenu_DeleteDocument, null);
-
-			var canvas = menu.ItemsControl.Parent as Canvas;
-			if (canvas != null) canvas.MouseMove += (s, e) => { Mouse.Position = e.GetPosition(null); };
-
-			menu.Closing += FocusTheClickOnItem;
+			Menu.AddItem(DocumentsResources.DocumentMenu_DeleteDocument, null);
 		}
 	}
 }
