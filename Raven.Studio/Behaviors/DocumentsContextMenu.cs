0384e4f50eaa9d0b67ec93a3b6c566c013c4be61
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-06-23 16:18:10 +0300

    Show a different menu items for Projection Documents. Add Copy Projection to Clipboard menu item.

--- Raven.Studio/Behaviors/DocumentsContextMenu.cs
+++ Raven.Studio/Behaviors/DocumentsContextMenu.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Caliburn.Micro;
 using Raven.Studio.Commands;
+using Raven.Studio.Features.Collections;
 using Raven.Studio.Features.Documents;
 using Raven.Studio.Features.Documents.Resources;
 using SL4PopupMenu;
@@ -17,27 +18,40 @@ namespace Raven.Studio.Behaviors
 
 		protected override void GenerateMenuItems()
 		{
-			if (IsInMultiSelectedMode == false)
+			if (IsInMultiSelectedMode)
+			{
+				if (SelectedItem.CollectionType != BuiltinCollectionName.Projection)
+				{
+					var deleteDocumentsMenuItem = new PopupMenuItem(null, string.Format(DocumentsResources.DocumentMenu_DeleteDocuments, SelectedItems.Count));
+					deleteDocumentsMenuItem.Click += (s, ea) => IoC.Get<DeleteDocument>().Execute(SelectedItems.Select(item => item.Id).ToList());
+					Menu.AddItem(deleteDocumentsMenuItem);
+				}
+			}
+			else
 			{
 				var editDocumentMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_EditDocument);
 				editDocumentMenuItem.Click += (s, ea) => IoC.Get<EditDocument>().Execute(SelectedItem);
 				Menu.AddItem(editDocumentMenuItem);
 
-				var copyIdMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_CopyId);
-				copyIdMenuItem.Click += (s, ea) => IoC.Get<CopyDocumentIdToClipboard>().Execute(SelectedItem.Id);
-				Menu.AddItem(copyIdMenuItem);
+				if (SelectedItem.CollectionType == BuiltinCollectionName.Projection)
+				{
+					var copyProjectionMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_CopyProjection);
+					copyProjectionMenuItem.Click += (s, ea) => IoC.Get<CopyDocumentToClipboard>().Execute(SelectedItem);
+					Menu.AddItem(copyProjectionMenuItem);
+				}
+				else
+				{
+					var copyIdMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_CopyId);
+					copyIdMenuItem.Click += (s, ea) => IoC.Get<CopyDocumentIdToClipboard>().Execute(SelectedItem.Id);
+					Menu.AddItem(copyIdMenuItem);
 
-				Menu.AddSeparator();
+					Menu.AddSeparator();
 
-				var deleteDocumentMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_DeleteDocument);
-				deleteDocumentMenuItem.Click += (s, ea) => IoC.Get<DeleteDocument>().Execute(SelectedItems.Select(item => item.Id).ToList());
-				Menu.AddItem(deleteDocumentMenuItem);
-			}
-			else
-			{
-				var deleteDocumentsMenuItem = new PopupMenuItem(null, string.Format(DocumentsResources.DocumentMenu_DeleteDocuments, SelectedItems.Count));
-				deleteDocumentsMenuItem.Click += (s, ea) => IoC.Get<DeleteDocument>().Execute(SelectedItems.Select(item => item.Id).ToList());
-				Menu.AddItem(deleteDocumentsMenuItem);
+					var deleteDocumentMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_DeleteDocument);
+					deleteDocumentMenuItem.Click +=
+						(s, ea) => IoC.Get<DeleteDocument>().Execute(SelectedItems.Select(item => item.Id).ToList());
+					Menu.AddItem(deleteDocumentMenuItem);
+				}
 			}
 		}
 	}
