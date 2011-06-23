452041f7d1bbc6e5d97e9b7bb4c3220a7ffc403b
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-06-23 11:21:45 +0300

    Make the context menu to work as expected: compute the items on the menu on menu open action. Now we have more separation between the DocumentsContextMenu and menu AttachDocumentsMenu behavior

--- Raven.Studio/Behaviors/AttachDocumentsMenu.cs
+++ Raven.Studio/Behaviors/AttachDocumentsMenu.cs
@@ -1,127 +1,50 @@
-using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Interactivity;
 using System.Windows.Media;
-using Caliburn.Micro;
-using Raven.Studio.Commands;
-using Raven.Studio.Common;
 using Raven.Studio.Features.Documents;
-using Raven.Studio.Features.Documents.Resources;
-using SL4PopupMenu;
 
 namespace Raven.Studio.Behaviors
 {
 	public class AttachDocumentsMenu : Behavior<ListBox>
 	{
-		private PopupMenu menu;
-		private PopupMenuItem editDocumentMenuItem;
-		private PopupMenuItem copyIdMenuItem;
-		private Point mousePosition;
-
 		protected override void OnAttached()
 		{
-			CreateMenu();
-
-			base.OnAttached();
-		}
-
-		private void CreateMenu()
-		{
-			var viewModel = AssociatedObject.SelectedItem as DocumentViewModel;
-			if (viewModel == null)
-				return;
-
-			menu = new PopupMenu();
-
-			editDocumentMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_EditDocument);
-			editDocumentMenuItem.Click += (s, ea) => IoC.Get<EditDocument>().Execute(viewModel);
-			menu.AddItem(editDocumentMenuItem);
-
-			copyIdMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_CopyId);
-			copyIdMenuItem.Click += (s, ea) => IoC.Get<CopyDocumentIdToClipboard>().Execute(viewModel.Id);
-			menu.AddItem(copyIdMenuItem);
-
-			menu.AddSeparator();
-			menu.AddItem(DocumentsResources.DocumentMenu_DeleteDocument, null);
-			
-			var canvas = menu.ItemsControl.Parent as Canvas;
-			if (canvas != null) canvas.MouseMove += (s, e) => { mousePosition = e.GetPosition(null); };
-
-			menu.Opening += OnMenuOpening;
-			menu.Closing += FocusTheClickOnItem;
-			menu.AddTrigger(TriggerTypes.RightClick, AssociatedObject);
-		}
-
-		private void FocusTheClickOnItem(object sender, RoutedEventArgs e)
-		{
-			var elementsInHostCoordinates = VisualTreeHelper.FindElementsInHostCoordinates(mousePosition,
-																						   Application.Current.RootVisual);
-			elementsInHostCoordinates
-				.Where(element => element is ListBoxItem)
-				.OfType<ListBoxItem>()
-				.ToList()
-				.ForEach(item =>
-				         	{
-								var parent = VisualTreeHelperExtensions.GetParentOfType<ListBox>(item);
-								if (parent != null && parent.SelectionMode != SelectionMode.Single)
-									parent.SelectedItems.Clear();
-								item.IsSelected = true;
-				         	});
-		}
-
-		private void OnMenuOpening(object sender, RoutedEventArgs e)
-		{
-			OpenOnlyOnDocumentItem(e.OriginalSource);
-			if (menu.IsOpeningCancelled)
-				return;
-
-			MultiItemsMenuOrSingleItemMenu();
-		}
-
-		private void MultiItemsMenuOrSingleItemMenu()
-		{
-			if (AssociatedObject.SelectionMode != SelectionMode.Single &&
-				AssociatedObject.SelectedItems.Count > 1)
-			{
-				editDocumentMenuItem.Visibility = Visibility.Collapsed;
-				copyIdMenuItem.Visibility = Visibility.Collapsed;
-			}
-			else
-			{
-				editDocumentMenuItem.Visibility = Visibility.Visible;
-				copyIdMenuItem.Visibility = Visibility.Visible;
-			}
-		}
-
-		// Make sure that the menu opened only on a document item, 
-		// and not on an empty space.
-		private void OpenOnlyOnDocumentItem(object element)
-		{
-			var ele = element as DependencyObject;
-			menu.IsOpeningCancelled = true;
-			while (ele != null && (ele is ScrollViewer) == false)
-			{
-				var item = ele as ListBoxItem;
-				if (item != null)
-				{
-					menu.IsOpeningCancelled = false;
-
-					// Make the current element selected on right click
-					if (AssociatedObject.SelectionMode != SelectionMode.Single && AssociatedObject.SelectedItems.Contains(item.DataContext) == false)
-						AssociatedObject.SelectedItems.Clear();
-					item.IsSelected = true;
-
-					break;
-				}
-				ele = VisualTreeHelper.GetParent(ele);
-			}
+			AssociatedObject.MouseRightButtonDown += AssociatedObject_MouseRightButtonDown;
+			AssociatedObject.MouseRightButtonUp += AssociatedObject_MouseRightButtonUp;
 		}
 
 		protected override void OnDetaching()
 		{
-			base.OnDetaching();
+			AssociatedObject.MouseRightButtonDown -= AssociatedObject_MouseRightButtonDown;
+			AssociatedObject.MouseRightButtonUp -= AssociatedObject_MouseRightButtonUp;
+		}
+
+		private void AssociatedObject_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			// This is required for the MouseRightButtonUp event to be fired. 
+			e.Handled = true;
+		}
+
+		private void AssociatedObject_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			var item = VisualTreeHelper.FindElementsInHostCoordinates(e.GetPosition(null), AssociatedObject)
+				.OfType<ListBoxItem>()
+				.FirstOrDefault();
+
+			if (item == null)
+				return;
+
+			// Make the current element selected on right click
+			if (AssociatedObject.SelectionMode != SelectionMode.Single && AssociatedObject.SelectedItems.Contains(item.DataContext) == false)
+				AssociatedObject.SelectedItems.Clear();
+			item.IsSelected = true;
+
+			var menu = new DocumentsContextMenu(AssociatedObject.SelectedItems.Cast<DocumentViewModel>().ToList());
+			menu.Open(item, e);
 		}
 	}
 }
--- Raven.Studio/Behaviors/DocumentsContextMenu.cs
+++ Raven.Studio/Behaviors/DocumentsContextMenu.cs
@@ -1,58 +1,94 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Interactivity;
 using System.Windows.Media;
+using Caliburn.Micro;
+using Raven.Studio.Commands;
+using Raven.Studio.Common;
+using Raven.Studio.Features.Documents;
+using Raven.Studio.Features.Documents.Resources;
+using SL4PopupMenu;
 
 namespace Raven.Studio.Behaviors
 {
-	public class DocumentsContextMenu : Behavior<ListBox>
+	public class DocumentsContextMenu
 	{
-		private ContextMenu menu;
+		private readonly IList<DocumentViewModel> selectedItems;
+		private readonly DocumentViewModel selectedItem;
+		private readonly bool isInMultiSelectedMode;
 
-		protected override void OnAttached()
+		private PopupMenu menu;
+		private PopupMenuItem editDocumentMenuItem;
+		private PopupMenuItem copyIdMenuItem;
+
+		public DocumentsContextMenu(IList<DocumentViewModel> selectedItems)
 		{
-			menu = new ContextMenu {Visibility = Visibility.Collapsed};
-			menu.Closed += (o, ea) =>
-							{
-								menu.Visibility = Visibility.Collapsed;
-							};
-			AssociatedObject.SetValue(ContextMenuService.ContextMenuProperty, menu);
+			if (selectedItems == null)
+				throw new ArgumentNullException("selectedItems", "Selected items cannot be null");
 
-			AssociatedObject.MouseRightButtonDown += AssociatedObject_MouseRightButtonDown;
-			AssociatedObject.MouseRightButtonUp += AssociatedObject_MouseRightButtonUp;
-
-			base.OnAttached();
+			this.selectedItems = selectedItems;
+			if (selectedItems.Count > 1)
+				isInMultiSelectedMode = true;
+			else
+			{
+				selectedItem = selectedItems.FirstOrDefault();
+				if (selectedItem == null)
+					throw new InvalidOperationException("Selected items must contain at least one item");
+			}
 		}
 
-		protected override void OnDetaching()
+		public void Open(FrameworkElement element, MouseButtonEventArgs e)
 		{
-			AssociatedObject.MouseRightButtonDown -= AssociatedObject_MouseRightButtonDown;
-			AssociatedObject.MouseRightButtonUp -= AssociatedObject_MouseRightButtonUp;
-
-			menu = null;
-			AssociatedObject.SetValue(ContextMenuService.ContextMenuProperty, menu);
-
-			base.OnDetaching();
+			CreateMenu();
+			menu.Open(Mouse.Position, MenuOrientationTypes.MouseBottomRight, 0, element, true, e);
 		}
 
-		private void AssociatedObject_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+		private void FocusTheClickOnItem(object sender, RoutedEventArgs e)
 		{
-			var item = VisualTreeHelper.FindElementsInHostCoordinates(e.GetPosition(null), AssociatedObject)
+			var elementsInHostCoordinates = VisualTreeHelper.FindElementsInHostCoordinates(Mouse.Position,
+			                                                                               Application.Current.RootVisual);
+			elementsInHostCoordinates
+				.Where(element => element is ListBoxItem)
 				.OfType<ListBoxItem>()
-				.FirstOrDefault();
-
-			if (item == null)
-				return;
-
-			menu.Visibility = Visibility.Visible;
+				.ToList()
+				.ForEach(item =>
+				         	{
+				         		var parent = VisualTreeHelperExtensions.GetParentOfType<ListBox>(item);
+				         		if (parent != null && parent.SelectionMode != SelectionMode.Single)
+				         			parent.SelectedItems.Clear();
+				         		item.IsSelected = true;
+				         	});
 		}
 
-		private void AssociatedObject_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+		private void CreateMenu()
 		{
-			// This is required for the MouseRightButtonUp event to be fired. 
-			e.Handled = true;
+			menu = new PopupMenu();
+
+			if (isInMultiSelectedMode == false)
+			{
+				editDocumentMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_EditDocument);
+				editDocumentMenuItem.Click += (s, ea) =>
+				                              	{
+				                              		IoC.Get<EditDocument>().Execute(selectedItem);
+				                              	};
+				menu.AddItem(editDocumentMenuItem);
+
+				copyIdMenuItem = new PopupMenuItem(null, DocumentsResources.DocumentMenu_CopyId);
+				copyIdMenuItem.Click += (s, ea) => IoC.Get<CopyDocumentIdToClipboard>().Execute(selectedItem.Id);
+				menu.AddItem(copyIdMenuItem);
+
+				menu.AddSeparator();
+			}
+
+			menu.AddItem(DocumentsResources.DocumentMenu_DeleteDocument, null);
+
+			var canvas = menu.ItemsControl.Parent as Canvas;
+			if (canvas != null) canvas.MouseMove += (s, e) => { Mouse.Position = e.GetPosition(null); };
+
+			menu.Closing += FocusTheClickOnItem;
 		}
 	}
 }
