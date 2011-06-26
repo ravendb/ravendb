8fba7a4277b92d55145cd5ed292ee6e9558173aa
etobi <git@e-tobi.net>
2011-06-26 13:26:04 +0200

    Fixed some XML comments

--- Raven.Client.Lightweight/Connection/Async/AsyncServerClient.cs
+++ Raven.Client.Lightweight/Connection/Async/AsyncServerClient.cs
@@ -60,7 +60,7 @@ namespace Raven.Client.Connection.Async
 		}
 
 		/// <summary>
-		/// Returns a new <see cref="IDatabaseCommands "/> using the specified credentials
+		/// Returns a new <see cref="IAsyncDatabaseCommands"/> using the specified credentials
 		/// </summary>
 		/// <param name="credentialsForSession">The credentials for session.</param>
 		public IAsyncDatabaseCommands With(ICredentials credentialsForSession)
