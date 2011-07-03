09b3b24e7ed780f656b1d91fb8552ad05a934251
Ayende Rahien <ayende@ayende.com>
2011-07-03 03:02:17 +0300

    Better output during work

--- ETL/Uploader/Program.cs
+++ ETL/Uploader/Program.cs
@@ -19,17 +19,22 @@ namespace Uploader
 				return;
 			}
 
+			long totalTimes = 0;
+			int totalDocs = 0;
+			int totalRequests = 0;
+
 			var ravenUrl = args[0];
 			foreach (var directory in args.Skip(1))
 			{
-				foreach (var file in Directory.GetFiles(directory, "*.json"))
+				foreach (var file in Directory.GetFiles(directory, "posts*.json"))
 				{
 
 					var sp = Stopwatch.StartNew();
 					HttpWebResponse webResponse;
 					while (true)
 					{
-
+						totalDocs+=1024;
+						totalRequests++;
 						var httpWebRequest = (HttpWebRequest)WebRequest.Create(new Uri(new Uri(ravenUrl), "bulk_docs"));
 						httpWebRequest.Method = "POST";
 						using (var requestStream = httpWebRequest.GetRequestStream())
@@ -58,7 +63,8 @@ namespace Uploader
 						}
 					}
 					var timeSpan = sp.Elapsed;
-					Console.WriteLine("{0} - {1} - {2}", Path.GetFileName(file), timeSpan, webResponse.StatusCode);
+					totalTimes += sp.ElapsedMilliseconds;
+					Console.WriteLine("{0} - {1} - {2} - {3:#,#} docs - {4:#,#} ms", Path.GetFileName(file), webResponse.StatusCode, timeSpan, totalDocs, totalTimes / totalRequests);
 				}
 
 			}
