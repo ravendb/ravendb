f2032e8472f38e8bae6a10ddd758fbbea21a3bcd
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-07-06 12:41:11 +0300

    Warn the user before modifying a system document

--- Raven.Studio/Commands/SaveDocument.cs
+++ Raven.Studio/Commands/SaveDocument.cs
@@ -1,4 +1,5 @@
 ﻿using Raven.Abstractions.Extensions;
+using Raven.Studio.Shell.MessageBox;
 
 namespace Raven.Studio.Commands
 {
@@ -16,18 +17,40 @@ namespace Raven.Studio.Commands
 	{
 		readonly IEventAggregator events;
 		readonly IServer server;
+		private readonly ShowMessageBox showMessageBox;
 
 		[ImportingConstructor]
-		public SaveDocument(IEventAggregator events, IServer server)
+		public SaveDocument(IEventAggregator events, IServer server, ShowMessageBox showMessageBox)
 		{
 			this.events = events;
 			this.server = server;
+			this.showMessageBox = showMessageBox;
 		}
 
 		public void Execute(EditDocumentViewModel document)
 		{
 			if(!ValidateJson(document.JsonData) || !ValidateJson(document.JsonMetadata)) return;
 
+			if (document.Id.StartsWith("Raven/"))
+			{
+				showMessageBox(
+					"Are you sure that you want to edit a system document?",
+					"Confirm Edit",
+					MessageBoxOptions.OkCancel,
+					box =>
+						{
+							if (box.WasSelected(MessageBoxOptions.Ok))
+							{
+								Delete(document);
+							}
+						});
+				return;
+			}
+			Delete(document);
+		}
+
+		private void Delete(EditDocumentViewModel document)
+		{
 			document.PrepareForSave();
 
 			var jdoc = document.JsonDocument;
@@ -35,17 +58,16 @@ namespace Raven.Studio.Commands
 			server.OpenSession().Advanced.AsyncDatabaseCommands
 				.PutAsync(document.Id, jdoc.Etag, jdoc.DataAsJson, jdoc.Metadata)
 				.ContinueOnSuccess(put =>
-				                   	{
-										var inner = document.JsonDocument;
-										inner.Key = put.Result.Key;
-										inner.Metadata = inner.Metadata.FilterHeaders(isServerDocument: false);
-										inner.Etag = put.Result.ETag;
-										inner.LastModified = DateTime.Now;
-				                        document.UpdateDocumentFromJsonDocument();
+				{
+					var inner = document.JsonDocument;
+					inner.Key = put.Result.Key;
+					inner.Metadata = inner.Metadata.FilterHeaders(isServerDocument: false);
+					inner.Etag = put.Result.ETag;
+					inner.LastModified = DateTime.Now;
+					document.UpdateDocumentFromJsonDocument();
 
-                                        events.Publish(new DocumentUpdated(document));
-				                   	});
-			
+					events.Publish(new DocumentUpdated(document));
+				});
 		}
 
 		bool ValidateJson(string json)
