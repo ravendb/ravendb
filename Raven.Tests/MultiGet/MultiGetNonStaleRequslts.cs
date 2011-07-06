45ff9f2d4578e08b31a95a1ef0947096d8e01378
Ayende Rahien <ayende@ayende.com>
2011-07-06 12:33:44 +0300

    Adding passing tests

---
+++ Raven.Tests/MultiGet/MultiGetNonStaleRequslts.cs
@@ -0,0 +1,49 @@
+using System.Linq;
+using Raven.Client.Document;
+using Raven.Client.Linq;
+using Raven.Tests.Bugs;
+using Xunit;
+
+namespace Raven.Tests.MultiGet
+{
+	public class MultiGetNonStaleRequslts : RemoteClientTest
+	{
+		[Fact]
+		public void LazyMultiLoadOperationWouldBeInTheSession()
+		{
+			using (var server = GetNewServer())
+			using (var store = new DocumentStore { Url = "http://localhost:8080" }.Initialize())
+			{
+				using (var session = store.OpenSession())
+				{
+					session.Query<User>()
+						.Customize(x => x.WaitForNonStaleResults())
+						.Where(x => x.Name == "oren")
+						.ToList();
+				}
+
+				using (var session = store.OpenSession())
+				{
+					session.Store(new User { Name = "oren" });
+					session.Store(new User());
+					session.Store(new User { Name = "ayende" });
+					session.Store(new User());
+					session.SaveChanges();
+				}
+
+				WaitForAllRequestsToComplete(server);
+				server.Server.ResetNumberOfRequests();
+
+				using (var session = store.OpenSession())
+				{
+					var result1 = session.Query<User>().Customize(x=>x.WaitForNonStaleResults()).Where(x => x.Name == "oren").Lazily();
+					Assert.NotEmpty(result1.Value);
+					Assert.Equal(1, session.Advanced.NumberOfRequests);
+
+					Assert.True(server.Server.NumberOfRequests > 1);
+				}
+			}
+		}
+
+	}
+}
