da196f2003a93a0cf7b332ebc2c1ea52f18ed96f
fschwiet <fschwiet@gmail.com>
2011-07-23 00:25:26 -0700

    minor discrepancy in Last-Modified tracking

--- Raven.Tests/Bugs/Metadata/LastModifiedRemote.cs
+++ Raven.Tests/Bugs/Metadata/LastModifiedRemote.cs
@@ -3,6 +3,7 @@
 //     Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
+using System;
 using Raven.Client.Document;
 using Xunit;
 
@@ -16,16 +17,25 @@ namespace Raven.Tests.Bugs.Metadata
             using(GetNewServer())
             using (var store = new DocumentStore{Url = "http://localhost:8080"}.Initialize())
             {
+                DateTime before;
+                DateTime after;
+
                 using (var session = store.OpenSession())
                 {
                     session.Store(new User());
+
+                    before = DateTime.UtcNow;
                     session.SaveChanges();
+                    after = DateTime.UtcNow;
                 }
 
                 using (var session = store.OpenSession())
                 {
                     var user = session.Load<User>("users/1");
-                    Assert.NotNull(session.Advanced.GetMetadataFor(user)["Last-Modified"]);
+                    var lastModified = session.Advanced.GetMetadataFor(user).Value<DateTime>("Last-Modified");
+                    Assert.NotNull(lastModified);
+                    Assert.InRange(lastModified.ToUniversalTime().Ticks, before.Ticks, after.Ticks);
+                    Assert.Equal(DateTimeKind.Utc, lastModified.Kind);
                 }
             }
         }
