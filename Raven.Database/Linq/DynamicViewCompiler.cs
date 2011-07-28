82827f8033f84133ef8d109c78e2ecdd42f29bfe
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-07-28 14:46:51 +0300

    Throw an error if the map/reduce return type is not consistent across both of map and reduce

--- Raven.Database/Linq/DynamicViewCompiler.cs
+++ Raven.Database/Linq/DynamicViewCompiler.cs
@@ -206,9 +206,19 @@ namespace Raven.Database.Linq
 				groupBySource = lambdaExpression.ExpressionBody;
 			}
 
+			var mapFields = captureSelectNewFieldNamesVisitor.FieldNames.ToArray();
 			captureSelectNewFieldNamesVisitor.FieldNames.Clear();// reduce override the map fields
 			reduceDefiniton.Initializer.AcceptVisitor(captureSelectNewFieldNamesVisitor, null);
 			reduceDefiniton.Initializer.AcceptChildren(captureQueryParameterNamesVisitorForReduce, null);
+
+			var reduceFields = captureSelectNewFieldNamesVisitor.FieldNames;
+			if (reduceFields.SetEquals(mapFields) == false)
+			{
+				throw new InvalidOperationException(string.Format(@"The result type is not consistent across map and reduce:
+Map Fields: {0}
+Reduce Fields: {1}
+", mapFields, reduceFields));
+			}
 
 			// this.ReduceDefinition = from result in results...;
 			ctor.Body.AddChild(new ExpressionStatement(
---
+++ Raven.Tests/Bugs/MapReduceWithDifferentFieldNames.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="MapReduceWithDifferentFieldNames.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using System;
+using Raven.Abstractions.Indexing;
+using Xunit;
+
+namespace Raven.Tests.Bugs
+{
+	public class MapReduceWithDifferentFieldNames : LocalClientTest
+	{
+		[Fact]
+		public void WhenTheAnonymousTypeResultIsNotTheSame_ShouldThrowAnException()
+		{
+			using (var store = NewDocumentStore())
+			{
+				Assert.Throws<InvalidOperationException>(() => store.DatabaseCommands.PutIndex("test", new IndexDefinition
+				{
+					Map = "from doc in docs select new { doc.Name, Count = 1 }",
+					Reduce = "from result in results group result by result.Name into g select new { g.Key, Count = g.Sum(x=>x.Count) }"
+				}));
+			}
+		}
+	}
+}
