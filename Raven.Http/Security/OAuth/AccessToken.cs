ac8b6ad5e4eab4f4dae049ce0de19bb0468a0845
Ayende Rahien <ayende@ayende.com>
2011-08-03 10:32:42 +0300

    Refactoring - Dispose when needed - don't create UnicodeEncoding - each file has its own class

--- Raven.Http/Security/OAuth/AccessToken.cs
+++ Raven.Http/Security/OAuth/AccessToken.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -21,9 +20,9 @@ namespace Raven.Http.Security.OAuth
             var signatureData = Convert.FromBase64String(Signature);
             var bodyData = Encoding.Unicode.GetBytes(Body);
 
-			using (var sha1Managed = new SHA1Managed())
+			using (var hasher = new SHA1Managed())
 			{
-				var hash = sha1Managed.ComputeHash(bodyData);
+				var hash = hasher.ComputeHash(bodyData);
 
 				return csp.VerifyHash(hash, CryptoConfig.MapNameToOID("SHA1"), signatureData);
 			}
@@ -74,33 +73,18 @@ namespace Raven.Http.Security.OAuth
             var cert = new X509Certificate2(certPath, password);
             var csp = (RSACryptoServiceProvider)cert.PrivateKey;
 
-            var data = new UnicodeEncoding().GetBytes(body);
-            var hash = new SHA1Managed().ComputeHash(data);
-
-            return Convert.ToBase64String(csp.SignHash(hash, CryptoConfig.MapNameToOID("SHA1")));
+            var data = Encoding.Unicode.GetBytes(body);
+			using (var hasher = new SHA1Managed())
+			{
+				var hash = hasher.ComputeHash(data);
+				return Convert.ToBase64String(csp.SignHash(hash, CryptoConfig.MapNameToOID("SHA1")));
+			}
         }
 
         public string Serialize()
         {
             return RavenJObject.FromObject(this).ToString(Formatting.None);
         }
-    }
 
-    public class AccessTokenBody
-    {
-        public string UserId { get; set; }
-        public string[] AuthorizedDatabases { get; set; }
-        public double Issued { get; set; }
-
-        public bool IsExpired()
-        {
-            var issued = DateTime.MinValue.AddMilliseconds(Issued);
-            return !(issued < DateTime.UtcNow && DateTime.UtcNow.Subtract(issued) < TimeSpan.FromMinutes(30));
-        }
-
-        public bool IsAuthorized(string tenantId)
-        {
-            return AuthorizedDatabases != null && AuthorizedDatabases.Any(a => a.Equals(tenantId, StringComparison.OrdinalIgnoreCase) || a.Equals("*"));
-        }
     }
 }
