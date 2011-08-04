503b8ab23a7da5d85fbbf5606b7c9c39ed4d4996
Ayende Rahien <ayende@ayende.com>
2011-08-04 11:58:21 +0300

    Moved explanation to summary part

--- Raven.Client.Lightweight/Document/AbstractDocumentQuery.cs
+++ Raven.Client.Lightweight/Document/AbstractDocumentQuery.cs
@@ -1095,12 +1095,10 @@ If you really want to do in memory filtering on the data returned from the query
 
         /// <summary>
         ///   Order the results by the specified fields
-        /// </summary>
-        /// <remarks>
         ///   The fields are the names of the fields to sort, defaulting to sorting by ascending.
         ///   You can prefix a field name with '-' to indicate sorting by descending or '+' to sort by ascending
-        /// </remarks>
-        /// <param name = "fields">The fields.</param>
+		/// </summary>
+		/// <param name = "fields">The fields.</param>
         public void OrderBy(params string[] fields)
         {
             orderByFields = orderByFields.Concat(fields).ToArray();
