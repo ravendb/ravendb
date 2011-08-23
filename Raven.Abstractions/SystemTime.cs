fbdfd9de5990913f75440e7b8c83692586d3e0d2
Ayende Rahien <ayende@ayende.com>
2011-08-23 12:23:57 +0300

    More optimized version

--- Raven.Abstractions/SystemTime.cs
+++ Raven.Abstractions/SystemTime.cs
@@ -1,11 +1,34 @@
-﻿namespace Raven.Abstractions
-{
-	using System;
+﻿//-----------------------------------------------------------------------
+// <copyright company="Hibernating Rhinos LTD">
+//     Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
 
+namespace Raven.Abstractions
+{
 	public static class SystemTime
 	{
-		public static DateTime Now { get { return UtcDateTime().ToLocalTime(); } }
-		public static DateTime UtcNow { get { return UtcDateTime(); } }
-		public static Func<DateTime> UtcDateTime = () => DateTime.UtcNow;
+		public static Func<DateTime> UtcDateTime;
+
+		public static DateTime Now
+		{
+			get
+			{
+				if(UtcDateTime == null)
+					return DateTime.Now;
+				return UtcDateTime().ToLocalTime();
+			}
+		}
+
+		public static DateTime UtcNow
+		{
+			get
+			{
+				if (UtcDateTime == null)
+					return DateTime.UtcNow;
+				return UtcDateTime();
+			}
+		}
 	}
 }
