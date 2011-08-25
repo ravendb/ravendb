38ed44268b508046a2e7be6eb85ab6a458e97cc4
Ayende Rahien <ayende@ayende.com>
2011-08-25 14:49:37 +0300

    Avoid an issue where an empty array means that we never send anything to the index when we are creating dynamic queries on nested collections

--- Raven.Database/Data/DynamicQueryMapping.cs
+++ Raven.Database/Data/DynamicQueryMapping.cs
@@ -68,7 +68,7 @@ namespace Raven.Database.Data
 							String newDoc = string.Format("{0}Item", newDocumentSource.Replace(".", ""));
 
 							// from docNewDocItemsItem in doc.NewDoc.Items
-							String docInclude = string.Format("from {0} in (IEnumerable<dynamic>){1}", newDoc, newDocumentSource);
+							String docInclude = string.Format("from {0} in ((IEnumerable<dynamic>){1}).DefaultIfEmpty()", newDoc, newDocumentSource);
 							fromClauses.Add(docInclude);
 
 							// Start building the property again
