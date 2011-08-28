50b958cf2a84e092843008580d7a807a193c9626
Ayende Rahien <ayende@ayende.com>
2011-08-28 15:11:56 +0300

    Adding tests ensuring we can login via oauth using async session

---
+++ Bundles/Raven.Bundles.Tests/Authentication/AsyncSimpleLogin.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Linq;
+using System.Net;
+using Raven.Bundles.Authentication;
+using Raven.Http.Security.OAuth;
+using Xunit;
+using Raven.Client.Extensions;
+
+namespace Raven.Bundles.Tests.Authentication
+{
+	public class AsyncSimpleLogin : AuthenticationTest
+	{
+		[Fact]
+		public void CanLoginViaClientApi()
+		{
+			store.Credentials = new NetworkCredential("Ayende", "abc");
+
+			using (var session = embeddedStore.OpenSession())
+			{
+			    session.Store(new AuthenticationUser
+			    {
+			        Name = "Ayende",
+			        Id = "Raven/Users/Ayende",
+			        AllowedDatabases = new[] { "*" }
+			    }.SetPassword("abc"));
+			    session.SaveChanges();
+			}
+			
+			using (var session = store.OpenAsyncSession())
+			{
+				session.Store(new { Id ="Hal2001",  Name = "Sprite", Age = 321 });
+				session.SaveChangesAsync().Wait();
+			}
+
+			using (var session = store.OpenSession())
+			{
+				Assert.Equal("Sprite", session.Load<dynamic>("Hal2001").Name);
+			}
+		}
+
+		[Fact]
+		public void WillRememberToken()
+		{
+			store.Credentials = new NetworkCredential("Ayende", "abc");
+
+			using (var session = embeddedStore.OpenSession())
+			{
+				session.Store(new AuthenticationUser
+				{
+					Name = "Ayende",
+					Id = "Raven/Users/Ayende",
+					AllowedDatabases = new[] { "*" }
+				}.SetPassword("abc"));
+				session.SaveChanges();
+			}
+
+			for (int i = 0; i < 5; i++)
+			{
+				using (var session = store.OpenAsyncSession())
+				{
+					session.Store(new { Id = "Hal2001", Name = "Sprite", Age = 321 });
+					session.SaveChangesAsync().Wait();
+				}
+			}
+
+			var oAuthClientCredentialsTokenResponder = embeddedStore.HttpServer.RequestResponders.OfType<OAuthClientCredentialsTokenResponder>().First();
+			Assert.Equal(1, oAuthClientCredentialsTokenResponder.NumberOfTokensIssued);
+		}
+
+		[Fact]
+		public void WillGetAnErrorWhenTryingToLoginIfUserDoesNotExists()
+		{
+			store.Credentials = new NetworkCredential("Ayende", "abc");
+
+			using (var session = store.OpenAsyncSession())
+			{
+				session.Store(new { Name = "Sprite", Age = 321 });
+				var saveChangesAsync = session.SaveChangesAsync();
+				var webException = (WebException)Assert.Throws<AggregateException>(() => saveChangesAsync.Wait()).ExtractSingleInnerException();
+				Assert.Equal(HttpStatusCode.Unauthorized, ((HttpWebResponse) webException.Response).StatusCode);
+			}
+		}
+	}
+}
