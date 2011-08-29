6cf0b53027bdccc4c6f75604d149337d52839f89
Tobias Grimm <git@e-tobi.net>
2011-08-29 14:13:35 +0200

    Fixing indentation in Raven.Bundles.CascadeDelete
    Consistently use tabs for indentation instead of spaces

--- Bundles/Raven.Bundles.CascadeDelete/CascadeDeleteContext.cs
+++ Bundles/Raven.Bundles.CascadeDelete/CascadeDeleteContext.cs
@@ -18,43 +18,43 @@ namespace Raven.Bundles.CascadeDelete
 	///	   For example, we do want to replicate a cascade delete resulting from this bundle.
 	/// b) We are using the context to track state as well.
 	/// </summary>
-    public static class CascadeDeleteContext
-    {
-        private static readonly ThreadLocal<bool> CurrentlyInContext = new ThreadLocal<bool>();
+	public static class CascadeDeleteContext
+	{
+		private static readonly ThreadLocal<bool> CurrentlyInContext = new ThreadLocal<bool>();
 
-        private static readonly ThreadLocal<HashSet<string>> DeletedDocuments =
-            new ThreadLocal<HashSet<string>>(() => new HashSet<string>(StringComparer.InvariantCultureIgnoreCase));
+		private static readonly ThreadLocal<HashSet<string>> DeletedDocuments =
+			new ThreadLocal<HashSet<string>>(() => new HashSet<string>(StringComparer.InvariantCultureIgnoreCase));
 
-        public static bool IsInCascadeDeleteContext
-        {
-            get
-            {
-                return CurrentlyInContext.Value;
-            }
-        }
+		public static bool IsInCascadeDeleteContext
+		{
+			get
+			{
+				return CurrentlyInContext.Value;
+			}
+		}
 
-        public static bool HasAlreadyDeletedDocument(string key)
-        {
-            return DeletedDocuments.Value.Contains(key);
-        }
+		public static bool HasAlreadyDeletedDocument(string key)
+		{
+			return DeletedDocuments.Value.Contains(key);
+		}
 
-        public static void AddDeletedDocument(string key)
-        {
-            DeletedDocuments.Value.Add(key);
-        }
+		public static void AddDeletedDocument(string key)
+		{
+			DeletedDocuments.Value.Add(key);
+		}
 
-        public static IDisposable Enter()
-        {
-            var oldCurrentlyInContext = CurrentlyInContext.Value;
-            var oldDeletedDocuments = DeletedDocuments.Value;
-            CurrentlyInContext.Value = true;
-            DeletedDocuments.Value = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
-            return new DisposableAction(delegate
-            {
-                CurrentlyInContext.Value = oldCurrentlyInContext;
-                DeletedDocuments.Value = oldDeletedDocuments;
-            });
-        }
-    }
+		public static IDisposable Enter()
+		{
+			var oldCurrentlyInContext = CurrentlyInContext.Value;
+			var oldDeletedDocuments = DeletedDocuments.Value;
+			CurrentlyInContext.Value = true;
+			DeletedDocuments.Value = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
+			return new DisposableAction(delegate
+			{
+				CurrentlyInContext.Value = oldCurrentlyInContext;
+				DeletedDocuments.Value = oldDeletedDocuments;
+			});
+		}
+	}
 
 }
