d0bc45feebab5fd30ed63fda6e7af6744d5266fa
Tobias Grimm <git@e-tobi.net>
2011-08-29 14:34:17 +0200

    Fixing indentation in Raven.Sample.Includes
    Consistently use tabs for indentation instead of spaces

--- Samples/Raven.Samples.Includes/Company.cs
+++ Samples/Raven.Samples.Includes/Company.cs
@@ -5,11 +5,11 @@
 //-----------------------------------------------------------------------
 namespace Raven.Samples.Includes
 {
-    public class Company
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string ParentCompanyId { get; set; }
-        public string Region { get; set; }
-    }
+	public class Company
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string ParentCompanyId { get; set; }
+		public string Region { get; set; }
+	}
 }
--- Samples/Raven.Samples.Includes/Program.cs
+++ Samples/Raven.Samples.Includes/Program.cs
@@ -11,53 +11,53 @@ using Raven.Client.Indexes;
 
 namespace Raven.Samples.Includes
 {
-    internal class Program
-    {
-        private static void Main()
-        {
-            using (var documentStore = new DocumentStore {Url = "http://localhost:8080"})
-            {
-                documentStore.Initialize();
-            
-                using (IDocumentSession session = documentStore.OpenSession())
-                {
-                    var company = new Company
-                    {
-                        Name = "Hibernating Rhinos",
-                    };
-                    session.Store(company);
-                    session.Store(new Company
-                    {
-                        Name = "Rampaging Rhinos",
-                        ParentCompanyId = company.Id
-                    });
-                    session.SaveChanges();
-                }
-                Console.ReadLine();
+	internal class Program
+	{
+		private static void Main()
+		{
+			using (var documentStore = new DocumentStore {Url = "http://localhost:8080"})
+			{
+				documentStore.Initialize();
+			
+				using (IDocumentSession session = documentStore.OpenSession())
+				{
+					var company = new Company
+					{
+						Name = "Hibernating Rhinos",
+					};
+					session.Store(company);
+					session.Store(new Company
+					{
+						Name = "Rampaging Rhinos",
+						ParentCompanyId = company.Id
+					});
+					session.SaveChanges();
+				}
+				Console.ReadLine();
 
-                using (IDocumentSession session = documentStore.OpenSession())
-                {
-                    var company = session.Load<Company>("companies/2");
+				using (IDocumentSession session = documentStore.OpenSession())
+				{
+					var company = session.Load<Company>("companies/2");
 
-                    var parent = session.Load<Company>(company.ParentCompanyId);
+					var parent = session.Load<Company>(company.ParentCompanyId);
 
-                    Console.WriteLine(company.Name);
-                    Console.WriteLine(parent.Name);
-                }
+					Console.WriteLine(company.Name);
+					Console.WriteLine(parent.Name);
+				}
 
-                Console.ReadLine();
+				Console.ReadLine();
 
-                using (IDocumentSession session = documentStore.OpenSession())
-                {
-                    var company = session.Include<Company>(x => x.ParentCompanyId)
-                        .Load("companies/2");
+				using (IDocumentSession session = documentStore.OpenSession())
+				{
+					var company = session.Include<Company>(x => x.ParentCompanyId)
+						.Load("companies/2");
 
-                    var parent = session.Load<Company>(company.ParentCompanyId);
+					var parent = session.Load<Company>(company.ParentCompanyId);
 
-                    Console.WriteLine(company.Name);
-                    Console.WriteLine(parent.Name);
-                }
-            }
-        }
-    }
+					Console.WriteLine(company.Name);
+					Console.WriteLine(parent.Name);
+				}
+			}
+		}
+	}
 }
