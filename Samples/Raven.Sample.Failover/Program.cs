5fcc773c3f101f38684e6111d3fddd194d511913
Tobias Grimm <git@e-tobi.net>
2011-08-29 14:32:24 +0200

    Fixing indentation in Raven.Sample.Failover
    Consistently use tabs for indentation instead of spaces

--- Samples/Raven.Sample.Failover/Program.cs
+++ Samples/Raven.Sample.Failover/Program.cs
@@ -8,32 +8,32 @@ using Raven.Client.Document;
 
 namespace Raven.Sample.Failover
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
 			var documentStore1 = new DocumentStore { Url = "http://localhost:8080" }.Initialize();
 
-            using (var session1 = documentStore1.OpenSession())
-            {
-                session1.Store(new User { Id = "users/ayende", Name = "Ayende" });
-                session1.SaveChanges();
-            }
+			using (var session1 = documentStore1.OpenSession())
+			{
+				session1.Store(new User { Id = "users/ayende", Name = "Ayende" });
+				session1.SaveChanges();
+			}
 
-            using (var session1 = documentStore1.OpenSession())
-            {
-                Console.WriteLine(session1.Load<User>("users/ayende").Name);
-            }
+			using (var session1 = documentStore1.OpenSession())
+			{
+				Console.WriteLine(session1.Load<User>("users/ayende").Name);
+			}
 
-            Console.WriteLine("Wrote one docuemnt to 8080, ready for server failure");
+			Console.WriteLine("Wrote one docuemnt to 8080, ready for server failure");
 
-            Console.ReadLine();
+			Console.ReadLine();
 
-            using (var session1 = documentStore1.OpenSession())
-            {
-                Console.WriteLine(session1.Load<User>("users/ayende").Name);
-            }
+			using (var session1 = documentStore1.OpenSession())
+			{
+				Console.WriteLine(session1.Load<User>("users/ayende").Name);
+			}
 
-        }
-    }
+		}
+	}
 }
