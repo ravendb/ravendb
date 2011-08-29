4bb0163a44bee7b08ddd6d2a7e6e39a84ed1e536
Tobias Grimm <git@e-tobi.net>
2011-08-29 14:33:47 +0200

    Fixing indentation in Raven.Sample.SimpleClient
    Consistently use tabs for indentation instead of spaces

--- Samples/Raven.Sample.SimpleClient/Program.cs
+++ Samples/Raven.Sample.SimpleClient/Program.cs
@@ -7,24 +7,24 @@ using Raven.Client.Document;
 
 namespace Raven.Sample.SimpleClient
 {
-    class Program
-    {
-        static void Main()
-        {
-            using (var documentStore = new DocumentStore { Url = "http://ipv4.fiddler:8080" })
-            {
-                documentStore.Initialize();
+	class Program
+	{
+		static void Main()
+		{
+			using (var documentStore = new DocumentStore { Url = "http://ipv4.fiddler:8080" })
+			{
+				documentStore.Initialize();
 
-               
+			   
 
-            }
-        }
+			}
+		}
 
-        public class U2
-        {
-            public string Id { get; set; }
-            public string Name { get; set; }
-        }
+		public class U2
+		{
+			public string Id { get; set; }
+			public string Name { get; set; }
+		}
 
-    }
+	}
 }
