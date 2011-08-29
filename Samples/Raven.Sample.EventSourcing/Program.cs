ec57fab162f9a7513690c6176048b69b95b0dd26
Tobias Grimm <git@e-tobi.net>
2011-08-29 14:29:56 +0200

    Fixing indentation in Raven.Sample.EventSourcing
    Consistently use tabs for indentation instead of spaces

--- Samples/Raven.Sample.EventSourcing/Program.cs
+++ Samples/Raven.Sample.EventSourcing/Program.cs
@@ -11,89 +11,89 @@ using Raven.Json.Linq;
 
 namespace Raven.Sample.EventSourcing
 {
-    class Program
-    {
-        static void Main()
-        {
+	class Program
+	{
+		static void Main()
+		{
 			var documentStore1 = new DocumentStore { Url = "http://localhost:8080" }.Initialize();
 
-            var events = new object[]
-                {
-                    new
-                    {
-                        For = "ShoppingCart",
-                        Type = "Create",
-                        Timestamp = DateTime.Now,
-                        ShoppingCartId = "shoppingcarts/12",
-                        CustomerId = "users/ayende",
-                        CustomerName = "Ayende Rahien"
-                    },
-                    new
-                    {
-                        For = "ShoppingCart",
-                        Type = "Add",
-                        Timestamp = DateTime.Now,
-                        ShoppingCartId = "shoppingcarts/12",
-                        ProductId = "products/8123",
-                        ProductName = "Fish & Chips",
-                        Price = 8.5m
-                    },
-                    new
-                    {
-                        For = "ShoppingCart",
-                        Type = "Add",
-                        Timestamp = DateTime.Now,
-                        ShoppingCartId = "shoppingcarts/12",
-                        ProductId = "products/3214",
-                        ProductName = "Guinness",
-                        Price = 2.1m
-                    },
-                    new
-                    {
-                        For = "ShoppingCart",
-                        Type = "Remove",
-                        Timestamp = DateTime.Now,
-                        ShoppingCartId = "shoppingcarts/12",
-                        ProductId = "products/8123"
-                    },
-                    new
-                    {
-                        For = "ShoppingCart",
-                        Type = "Add",
-                        Timestamp = DateTime.Now,
-                        ShoppingCartId = "shoppingcarts/12",
-                        ProductId = "products/8121",
-                        ProductName = "Beef Pie",
-                        Price = 9.0m
-                    },
-                };
+			var events = new object[]
+				{
+					new
+					{
+						For = "ShoppingCart",
+						Type = "Create",
+						Timestamp = DateTime.Now,
+						ShoppingCartId = "shoppingcarts/12",
+						CustomerId = "users/ayende",
+						CustomerName = "Ayende Rahien"
+					},
+					new
+					{
+						For = "ShoppingCart",
+						Type = "Add",
+						Timestamp = DateTime.Now,
+						ShoppingCartId = "shoppingcarts/12",
+						ProductId = "products/8123",
+						ProductName = "Fish & Chips",
+						Price = 8.5m
+					},
+					new
+					{
+						For = "ShoppingCart",
+						Type = "Add",
+						Timestamp = DateTime.Now,
+						ShoppingCartId = "shoppingcarts/12",
+						ProductId = "products/3214",
+						ProductName = "Guinness",
+						Price = 2.1m
+					},
+					new
+					{
+						For = "ShoppingCart",
+						Type = "Remove",
+						Timestamp = DateTime.Now,
+						ShoppingCartId = "shoppingcarts/12",
+						ProductId = "products/8123"
+					},
+					new
+					{
+						For = "ShoppingCart",
+						Type = "Add",
+						Timestamp = DateTime.Now,
+						ShoppingCartId = "shoppingcarts/12",
+						ProductId = "products/8121",
+						ProductName = "Beef Pie",
+						Price = 9.0m
+					},
+				};
 
-            int i = 1;
-            foreach (var @event in events)
-            {
-                documentStore1.DatabaseCommands.Put("events/" + i++, null, RavenJObject.FromObject(@event), new RavenJObject());                
-            }
+			int i = 1;
+			foreach (var @event in events)
+			{
+				documentStore1.DatabaseCommands.Put("events/" + i++, null, RavenJObject.FromObject(@event), new RavenJObject());                
+			}
 
-            Console.WriteLine("Wrote {0} events", events.Length);
+			Console.WriteLine("Wrote {0} events", events.Length);
 
-            Console.ReadLine();
+			Console.ReadLine();
 
-            using(var session = documentStore1.OpenSession())
-            {
-                var aggregate = session.Advanced.LuceneQuery<AggregateHolder>("Aggregates/ShoppingCart")
-                    .Where("ShoppingCartId:shoppingcarts/12")
-                    .Single();
+			using(var session = documentStore1.OpenSession())
+			{
+				var aggregate = session.Advanced.LuceneQuery<AggregateHolder>("Aggregates/ShoppingCart")
+					.Where("ShoppingCartId:shoppingcarts/12")
+					.Single();
 
-                var cart = new JsonSerializer().Deserialize<ShoppingCart>(new RavenJTokenReader(aggregate.Aggregate));
+				var cart = new JsonSerializer().Deserialize<ShoppingCart>(new RavenJTokenReader(aggregate.Aggregate));
 
-                Console.WriteLine(cart.Total);
-            }
-        }
-    }
+				Console.WriteLine(cart.Total);
+			}
+		}
+	}
 
-    public class AggregateHolder
-    {
-        public string ShoppingCartId { get; set; }
-        public RavenJObject Aggregate { get; set; }
-    }
+	public class AggregateHolder
+	{
+		public string ShoppingCartId { get; set; }
+		public RavenJObject Aggregate { get; set; }
+	}
 }
