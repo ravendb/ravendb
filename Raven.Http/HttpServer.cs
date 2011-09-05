15655badbca18d9e1559f1f0c35a83150a1c2d4f
Ayende Rahien <ayende@ayende.com>
2011-09-05 13:12:10 +0300

    Just catch all exceptions when trying to get the context / start a new one

--- Raven.Http/HttpServer.cs
+++ Raven.Http/HttpServer.cs
@@ -190,25 +190,13 @@ namespace Raven.Http
 				//setup waiting for the next request
 				listener.BeginGetContext(GetContext, null);
 			}
-			catch (AggregateException)
+			catch (Exception)
 			{
 				// can't get current request / end new one, probably
 				// listner shutdown
 				return;
 			}
-			catch (InvalidOperationException)
-			{
-				// can't get current request / end new one, probably
-				// listner shutdown
-				return;
-			}
-			catch (HttpListenerException)
-			{
-				// can't get current request / end new one, probably
-				// listner shutdown
-				return;
-			}
-
+			
 			if (concurretRequestSemaphore.Wait(TimeSpan.FromSeconds(5)) == false)
 			{
 				HandleTooBusyError(ctx);
