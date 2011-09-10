686d53ec4e5306eab855bdc2e8d161741bfd6888
Ayende Rahien <ayende@ayende.com>
2011-09-10 15:17:55 +0300

    Adding a nice way to ask the user questions

---
+++ Raven.Studio/Features/Input/AskUser.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+
+namespace Raven.Studio.Features.Input
+{
+	public class AskUser
+	{
+		public static Task<string> QuestionAsync(string title, string question)
+		{
+			var dataContext = new InputModel
+			{
+				Title = title,
+				Question = question
+			};
+			var inputWindow = new InputWindow
+			{
+				DataContext = dataContext
+			};
+
+			var tcs = new TaskCompletionSource<string>();
+
+			inputWindow.Closed += (sender, args) =>
+			{
+				if (inputWindow.DialogResult == true)
+					tcs.SetResult(dataContext.Answer);
+				else
+					tcs.SetCanceled();
+			};
+
+			inputWindow.Show();
+
+			return tcs.Task;
+		}
+	}
+}
---
+++ Raven.Studio/Features/Input/InputWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Raven.Studio.Features.Input
+{
+	public partial class InputWindow : ChildWindow
+	{
+		public InputWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void OKButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.DialogResult = true;
+		}
+
+		private void CancelButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.DialogResult = false;
+		}
+	}
+}
+
