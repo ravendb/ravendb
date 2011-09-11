b489b709534fba5d3969b8de5a1bd7333c3a306d
Ayende Rahien <ayende@ayende.com>
2011-09-11 16:49:03 +0300

    Adding enough indirection so we can directly bind against the current document

---
+++ Raven.Studio/Features/Documents/EditDocumentModelLocator.cs
@@ -0,0 +1,10 @@
+namespace Raven.Studio.Features.Documents
+{
+	public class EditDocumentModelLocator
+	{
+		public EditDocumentModel Current
+		{
+			get { return new EditDocumentModel(); }
+		}
+	}
+}
