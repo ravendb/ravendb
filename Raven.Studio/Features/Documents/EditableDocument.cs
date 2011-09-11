64b07bddf19f039b0e71e5f6fdce495d6230358b
Ayende Rahien <ayende@ayende.com>
2011-09-11 16:52:20 +0300

    Adding initial wiring for editing a document

---
+++ Raven.Studio/Features/Documents/EditableDocument.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Raven.Abstractions.Data;
+using Raven.Studio.Infrastructure;
+
+namespace Raven.Studio.Features.Documents
+{
+	public class EditableDocument : NotifyPropertyChangedBase
+	{
+		private readonly JsonDocument document;
+
+		public EditableDocument(JsonDocument document)
+		{
+			this.document = document;
+		}
+
+		public string Key
+		{
+			get { return document.Key; }
+			set { document.Key = value;OnPropertyChanged(); }
+		}
+
+		public Guid? Etag
+		{
+			get { return document.Etag; }
+			set { document.Etag = value; OnPropertyChanged(); }
+		}
+
+		public DateTime? LastModified
+		{
+			get { return document.LastModified; }
+			set { document.LastModified = value; OnPropertyChanged(); }
+		}
+	}
+}
