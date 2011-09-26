d44cde83952f2c0f3232afdf48dba4f25046b7c3
Ayende Rahien <ayende@ayende.com>
2011-09-26 17:24:23 +0300

    Integration between different parts

---
+++ Raven.Studio/Models/DatabaseCollectionsModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Raven.Client.Connection.Async;
+using Raven.Studio.Infrastructure;
+
+namespace Raven.Studio.Models
+{
+	public class DatabaseCollectionsModel : NotifyPropertyChangedBase
+	{
+		private readonly IAsyncDatabaseCommands databaseCommands;
+		public BindableCollection<CollectionModel> Collections { get; set; }
+		public Observable<CollectionModel> Selected { get; set; }
+
+		public DatabaseCollectionsModel(IAsyncDatabaseCommands databaseCommands)
+		{
+			this.databaseCommands = databaseCommands;
+			Collections = new BindableCollection<CollectionModel>();
+			Selected = new Observable<CollectionModel>();
+		}
+
+		public void Update(NameAndCount[] collections)
+		{
+			Collections.Match(collections.OrderByDescending(x => x.Count).Select(col => new CollectionModel(databaseCommands)
+			{
+				Name = col.Name,
+				Count = col.Count
+			}).ToArray());
+
+			if (Selected.Value == null)
+				Selected.Value = Collections.FirstOrDefault();
+		}
+	}
+}
