c087346889e5bf4cccb60de4b258ef1cb5044a53
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-09-26 17:34:16 +0300

    Implement Add/Remove maps from indexes definition

--- Raven.Studio/Models/IndexDefinitionModel.cs
+++ Raven.Studio/Models/IndexDefinitionModel.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Newtonsoft.Json;
 using Raven.Abstractions.Data;
 using Raven.Abstractions.Indexing;
@@ -13,6 +15,7 @@ namespace Raven.Studio.Models
 		private IndexDefinition index;
 		private string name;
 
+
 		public IndexDefinitionModel(IndexDefinition index, IAsyncDatabaseCommands asyncDatabaseCommands)
 		{
 			this.asyncDatabaseCommands = asyncDatabaseCommands;
@@ -23,9 +26,65 @@ namespace Raven.Studio.Models
 		{
 			this.index = indexDefinition;
 			this.name = index.Name;
-
+			this.Maps = new ObservableCollection<string>(index.Maps);
 						
 			OnEverythingChanged();
+		}
+
+		public string Name
+		{
+			get { return name; }
+			set
+			{
+				name = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public ObservableCollection<string> Maps { get; private set; }
+
+		public ICommand AddMap
+		{
+			get { return new AddMapCommand(this); }
+		}
+
+		public ICommand RemoveMap
+		{
+			get { return new RemoveMapCommand(this); }
+		}
+
+		public class AddMapCommand : Command
+		{
+			private readonly IndexDefinitionModel index;
+
+			public AddMapCommand(IndexDefinitionModel index)
+			{
+				this.index = index;
+			}
+
+			public override void Execute(object parameter)
+			{
+				index.Maps.Add(string.Empty);
+			}
+		}
+
+		public class RemoveMapCommand : Command
+		{
+			private readonly IndexDefinitionModel index;
+
+			public RemoveMapCommand(IndexDefinitionModel index)
+			{
+				this.index = index;
+			}
+
+			public override void Execute(object parameter)
+			{
+				var map = parameter as string;
+				if (map == null || index.Maps.Contains(map) == false)
+					return;
+
+				index.Maps.Remove(map);
+			}
 		}
 	}
 }
