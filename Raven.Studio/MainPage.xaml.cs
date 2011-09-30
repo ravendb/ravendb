a08924e314eada389b5bb99e7e008b86e9987f46
samuel_d_jack <sam@seaturtlesoftwarea.com>
2011-09-30 11:56:21 +0100

    Added the top Navigation bar

--- Raven.Studio/MainPage.xaml.cs
+++ Raven.Studio/MainPage.xaml.cs
@@ -23,21 +23,21 @@ namespace Raven.Studio
 		// After the Frame navigates, ensure the HyperlinkButton representing the current page is selected
 		private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
 		{
-			foreach (UIElement child in LinksStackPanel.Children)
-			{
-				HyperlinkButton hb = child as HyperlinkButton;
-				if (hb != null && hb.NavigateUri != null)
-				{
-					if (hb.NavigateUri.ToString().Equals(e.Uri.ToString()))
-					{
-						VisualStateManager.GoToState(hb, "ActiveLink", true);
-					}
-					else
-					{
-						VisualStateManager.GoToState(hb, "InactiveLink", true);
-					}
-				}
-			}
+            //foreach (UIElement child in LinksStackPanel.Children)
+            //{
+            //    HyperlinkButton hb = child as HyperlinkButton;
+            //    if (hb != null && hb.NavigateUri != null)
+            //    {
+            //        if (hb.NavigateUri.ToString().Equals(e.Uri.ToString()))
+            //        {
+            //            VisualStateManager.GoToState(hb, "ActiveLink", true);
+            //        }
+            //        else
+            //        {
+            //            VisualStateManager.GoToState(hb, "InactiveLink", true);
+            //        }
+            //    }
+            //}
 		}
 
 		// If an error occurs during navigation, show an error window
