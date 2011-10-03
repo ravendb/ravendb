d4821a8d26676e84f785a64b75d00974bdd7228d
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-10-03 18:06:06 +0200

    Separate indexes to three groups: Temp, Auto and Other

--- Raven.Studio/Models/IndexesModel.cs
+++ Raven.Studio/Models/IndexesModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Raven.Client.Connection.Async;
 using Raven.Studio.Infrastructure;
 
@@ -7,12 +9,19 @@ namespace Raven.Studio.Models
 	public class IndexesModel : Model
 	{
 		private readonly IAsyncDatabaseCommands databaseCommands;
+
 		public BindableCollection<string> Indexes { get; private set; }
+		public BindableCollection<string> TempIndexes { get; private set; }
+		public BindableCollection<string> AutoIndexes { get; private set; }
+		public BindableCollection<string> OtherIndexes { get; private set; }
 
 		public IndexesModel(IAsyncDatabaseCommands databaseCommands)
 		{
 			this.databaseCommands = databaseCommands;
 			Indexes = new BindableCollection<string>(new PrimaryKeyComparer<string>(name => name));
+			TempIndexes = new BindableCollection<string>(new PrimaryKeyComparer<string>(name => name));
+			AutoIndexes = new BindableCollection<string>(new PrimaryKeyComparer<string>(name => name));
+			OtherIndexes = new BindableCollection<string>(new PrimaryKeyComparer<string>(name => name));
 			ForceTimerTicked();
 		}
 
@@ -20,7 +29,14 @@ namespace Raven.Studio.Models
 		{
 			return databaseCommands
 				.GetIndexNamesAsync(0, 256)
-				.ContinueOnSuccess(Indexes.Match);
+				.ContinueOnSuccess(indexes =>
+								   {
+									   Indexes.Match(indexes);
+									   TempIndexes.Match(indexes.Where(name => name.StartsWith("Temp/", StringComparison.InvariantCultureIgnoreCase)).ToList());
+									   AutoIndexes.Match(indexes.Where(name => name.StartsWith("Auto/", StringComparison.InvariantCultureIgnoreCase)).ToList());
+									   OtherIndexes.Match(indexes.Where(name => name.StartsWith("Temp/", StringComparison.InvariantCultureIgnoreCase) == false &&
+										   name.StartsWith("Auto/", StringComparison.InvariantCultureIgnoreCase) == false).ToList());
+								   });
 		}
 	}
 }
