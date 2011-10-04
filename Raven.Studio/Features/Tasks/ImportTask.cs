5030172076fc30c1c4a518c8b45a1bd8aef5845c
Daniel Dar <daniel@hibernatingrhinos.com>
2011-10-04 12:15:34 +0200

    Adding Tasks UI and export task

---
+++ Raven.Studio/Features/Tasks/ImportTask.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Input;
+using Raven.Client.Connection.Async;
+using Raven.Studio.Infrastructure;
+using Raven.Studio.Models;
+
+namespace Raven.Studio.Features.Tasks
+{
+    public class ImportTask : TaskModel
+    {
+        private readonly IAsyncDatabaseCommands asyncDatabaseCommands;
+
+        public ImportTask(IAsyncDatabaseCommands asyncDatabaseCommands)
+        {
+            this.asyncDatabaseCommands = asyncDatabaseCommands;
+            Name = "Import Database";
+            Description = "Import a database from a dump file.\nImporting will overwrite any existing indexes.";
+            
+        }
+
+        public class ImportDatabaseCommand : Command
+        {
+            private readonly IAsyncDatabaseCommands asyncDatabaseCommands;
+
+            public ImportDatabaseCommand(IAsyncDatabaseCommands asyncDatabaseCommands)
+            {
+                this.asyncDatabaseCommands = asyncDatabaseCommands;
+            }
+
+            public override void Execute(object parameter)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public override ICommand Action
+        {
+            get { return new ImportDatabaseCommand(asyncDatabaseCommands); }
+        }
+    }
+}
