876ee73cbefecaa2bbae535df75d377328e9ce07
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-10-04 11:10:52 +0200

    Add a API to get the logs from the server

---
+++ Raven.Abstractions/Data/LogItem.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="LogItem.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace Raven.Abstractions.Data
+{
+	public class LogItem
+	{
+		public string Name { get; set; }
+		public string Message { get; set; }
+		public string LoggerName { get; set; }
+		public string Exception { get; set; }
+		public DateTime Timestamp { get; set; } 
+	}
+}
