a9b0a9f742402e98f8bef99b5f517d88765d112f
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-10-11 15:04:11 +0200

    Remove the use of a model view locator in the collection page, and use the ModelAttacher instead

---
+++ Raven.Studio/Infrastructure/ModelAttacher.cs
@@ -0,0 +1,49 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ModelAttacher.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Windows;
+
+namespace Raven.Studio.Infrastructure
+{
+	public static class ModelAttacher
+	{
+		public static readonly DependencyProperty AttachModelProperty =
+			DependencyProperty.RegisterAttached("AttachModel", typeof(string), typeof(ModelAttacher), new PropertyMetadata(null, AttachModelCallback));
+
+		private static void AttachModelCallback(DependencyObject source, DependencyPropertyChangedEventArgs args)
+		{
+			var typeName = args.NewValue as string;
+			var view = source as FrameworkElement;
+			if (typeName == null || view == null)
+				return;
+
+			var modelType = Type.GetType(view.GetType().Namespace + "." + typeName) ?? Type.GetType(typeName);
+			if (modelType == null)
+				return;
+
+			try
+			{
+				var model = Activator.CreateInstance(modelType);
+				view.DataContext = model;
+			}
+			catch (Exception ex)
+			{
+				throw new InvalidOperationException(string.Format("Cannot create instance of model type: {0}", modelType), ex);
+			}
+		}
+
+		public static string GetAttachModel(UIElement element)
+		{
+			return (string)element.GetValue(AttachModelProperty);
+		}
+
+		public static void SetAttachModel(UIElement element, string value)
+		{
+			element.SetValue(AttachModelProperty, value);
+		}
+	}
+}
--- Raven.Studio/Models/DatabaseCollectionsModel.cs
+++ Raven.Studio/Models/DatabaseCollectionsModel.cs
@@ -6,26 +6,24 @@ namespace Raven.Studio.Models
 {
 	public class DatabaseCollectionsModel : Model
 	{
-		private readonly IAsyncDatabaseCommands databaseCommands;
 		public BindableCollection<CollectionModel> Collections { get; set; }
 		public Observable<CollectionModel> SelectedCollection { get; set; }
 
-		public DatabaseCollectionsModel(IAsyncDatabaseCommands databaseCommands)
+		public DatabaseCollectionsModel()
 		{
-			this.databaseCommands = databaseCommands;
 			Collections = new BindableCollection<CollectionModel>(new PrimaryKeyComparer<CollectionModel>(model=>model.Name));
 			SelectedCollection = new Observable<CollectionModel>();
 	    }
 
 		protected override System.Threading.Tasks.Task TimerTickedAsync()
 		{
-			return databaseCommands.GetTermsCount("Raven/DocumentsByEntityName", "Tag", "", 100)
+			return DatabaseCommands.GetTermsCount("Raven/DocumentsByEntityName", "Tag", "", 100)
 				.ContinueOnSuccess(Update);
 		}
 
 		private void Update(NameAndCount[] collections)
 		{
-			var collectionModels = collections.OrderByDescending(x => x.Count).Select(col => new CollectionModel(databaseCommands)
+			var collectionModels = collections.OrderByDescending(x => x.Count).Select(col => new CollectionModel(DatabaseCommands)
 			{
 				Name = col.Name,
 				Count = col.Count
