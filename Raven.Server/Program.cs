09cd43c23ee7d6e233e96de191b6dd1e3cda5f52
Ayende Rahien <ayende@ayende.com>
2011-10-12 15:03:44 +0200

    Much better interactive mode

--- Raven.Server/Program.cs
+++ Raven.Server/Program.cs
@@ -4,6 +4,7 @@
 // </copyright>
 //-----------------------------------------------------------------------
 using System;
+using System.Collections.Generic;
 using System.Configuration.Install;
 using System.Diagnostics;
 using System.IO;
@@ -47,8 +48,8 @@ namespace Raven.Server
 				}
 				catch (Exception e)
 				{
-					EmitWarningInRed(); 
-					
+					EmitWarningInRed();
+
 					Console.WriteLine(e);
 
 					WaitForUserInputAndExitWithError();
@@ -134,8 +135,8 @@ namespace Raven.Server
 
 			try
 			{
-				if(args.Length == 0) // we default to executing in debug mode 
-					args = new[]{"--debug"};
+				if (args.Length == 0) // we default to executing in debug mode 
+					args = new[] { "--debug" };
 
 				optionSet.Parse(args);
 			}
@@ -148,9 +149,9 @@ namespace Raven.Server
 
 			if (actionToTake == null)
 				actionToTake = () => RunInDebugMode(null, ravenConfiguration, launchBrowser);
-			
+
 			actionToTake();
-			
+
 		}
 
 		private static void RunRestoreOperation(string backupLocation, string databaseLocation)
@@ -158,12 +159,12 @@ namespace Raven.Server
 			try
 			{
 				var ravenConfiguration = new RavenConfiguration();
-				if(File.Exists(Path.Combine(backupLocation, "Raven.ravendb")))
+				if (File.Exists(Path.Combine(backupLocation, "Raven.ravendb")))
 				{
 					ravenConfiguration.DefaultStorageTypeName =
 						"Raven.Storage.Managed.TransactionalStorage, Raven.Storage.Managed";
 				}
-				else if(Directory.Exists(Path.Combine(backupLocation, "new")))
+				else if (Directory.Exists(Path.Combine(backupLocation, "new")))
 				{
 					ravenConfiguration.DefaultStorageTypeName = "Raven.Storage.Esent.TransactionalStorage, Raven.Storage.Esent";
 
@@ -248,12 +249,12 @@ namespace Raven.Server
 				Console.WriteLine("Raven is ready to process requests. Build {0}, Version {1}", DocumentDatabase.BuildVersion, DocumentDatabase.ProductVersion);
 				Console.WriteLine("Server started in {0:#,#} ms", sp.ElapsedMilliseconds);
 				Console.WriteLine("Data directory: {0}", ravenConfiguration.DataDirectory);
-				Console.WriteLine("HostName: {0} Port: {1}, Storage: {2}", ravenConfiguration.HostName ?? "<any>", 
-					ravenConfiguration.Port, 
+				Console.WriteLine("HostName: {0} Port: {1}, Storage: {2}", ravenConfiguration.HostName ?? "<any>",
+					ravenConfiguration.Port,
 					server.Database.TransactionalStorage.FriendlyName);
 				Console.WriteLine("Server Url: {0}", ravenConfiguration.ServerUrl);
-				Console.WriteLine("Press <enter> to stop or 'cls' and <enter> to clear the log");
-				if(lauchBrowser)
+
+				if (lauchBrowser)
 				{
 					try
 					{
@@ -264,22 +265,61 @@ namespace Raven.Server
 						Console.WriteLine("Could not start browser: " + e.Message);
 					}
 				}
-				while (true)
-				{
-					var readLine = Console.ReadLine() ?? "";
-					switch (readLine.ToLowerInvariant())
-					{
-						case "cls":
-							Console.Clear();
-							break;
-						case "reset":
-							Console.Clear();
-							return true;
-						default:
-							return false;
-					}
-				}
+				return InteractiveRun();
 			}
+		}
+
+		private static bool InteractiveRun()
+		{
+			bool? done = null;
+			var actions = new Dictionary<string,Action>
+			{
+				{"cls", () => Console.Clear()},
+				{
+					"reset", () =>
+					{
+						Console.Clear();
+						done = true;
+					}
+					},
+				{
+					"gc", () =>
+					{
+						long before = Process.GetCurrentProcess().WorkingSet64;
+						Console.WriteLine("Starting garbage collection, current memory is: {0:#,#.##} mb", before / 1024d / 1024d);
+						GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+						var after = Process.GetCurrentProcess().WorkingSet64;
+						Console.WriteLine("Done garbage collection, current memory is: {0:#,#.##} mb, saved: {1:#,#.##} mb", after / 1024d / 1024d,
+										  (before - after) / 1024d / 1024d);
+					}
+					},
+				{
+					"q", () => done = false
+				}
+			};
+
+			WriteInteractiveOptions(actions);
+			while (true)
+			{
+				var readLine = Console.ReadLine() ?? "";
+
+				Action value;
+				if(actions.TryGetValue(readLine, out value) == false)
+				{
+					Console.WriteLine("Could not understand: {0}", readLine);
+					WriteInteractiveOptions(actions);
+					continue;
+				}
+
+				value();
+				if (done != null)
+					return done.Value;
+			}
+		}
+
+		private static void WriteInteractiveOptions(Dictionary<string, Action> actions)
+		{
+			Console.WriteLine("Available commands: {0}", string.Join(", ", actions.Select(x => x.Key)));
 		}
 
 		private static void PrintUsage(OptionSet optionSet)
