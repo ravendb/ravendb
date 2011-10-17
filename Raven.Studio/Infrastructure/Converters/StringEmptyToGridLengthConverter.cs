c1b3c0ceb85881129e0d329bbe415d81fe1836ae
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-10-17 17:11:43 +0200

    minor

--- Raven.Studio/Infrastructure/Converters/StringEmptyToGridLengthConverter.cs
+++ Raven.Studio/Infrastructure/Converters/StringEmptyToGridLengthConverter.cs
@@ -1,15 +1,7 @@
 ﻿using System;
 using System.Globalization;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 
 namespace Raven.Studio.Infrastructure.Converters
 {
