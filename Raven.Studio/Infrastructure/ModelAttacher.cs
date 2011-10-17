df96294871265cf7f09e72504b3d7ae772accdd6
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-10-17 19:04:36 +0200

    Add a way to attach observable models

--- Raven.Studio/Infrastructure/ModelAttacher.cs
+++ Raven.Studio/Infrastructure/ModelAttacher.cs
@@ -14,6 +14,9 @@ namespace Raven.Studio.Infrastructure
 		public static readonly DependencyProperty AttachModelProperty =
 			DependencyProperty.RegisterAttached("AttachModel", typeof(string), typeof(ModelAttacher), new PropertyMetadata(null, AttachModelCallback));
 
+		public static readonly DependencyProperty AttachObservableModelProperty =
+			DependencyProperty.RegisterAttached("AttachObservableModel", typeof(string), typeof(ModelAttacher), new PropertyMetadata(null, AttachObservableModelCallback));
+
 		private static void AttachModelCallback(DependencyObject source, DependencyPropertyChangedEventArgs args)
 		{
 			var typeName = args.NewValue as string;
@@ -21,7 +24,7 @@ namespace Raven.Studio.Infrastructure
 			if (typeName == null || view == null)
 				return;
 
-			var modelType = Type.GetType("Raven.Studio.Models." + typeName) ?? Type.GetType(view.GetType().Namespace + "." + typeName) ?? Type.GetType(typeName);
+			var modelType = Type.GetType("Raven.Studio.Models." + typeName) ?? Type.GetType(typeName);
 			if (modelType == null)
 				return;
 
@@ -36,12 +39,48 @@ namespace Raven.Studio.Infrastructure
 			}
 		}
 
+		private static void AttachObservableModelCallback(DependencyObject source, DependencyPropertyChangedEventArgs args)
+		{
+			var typeName = args.NewValue as string;
+			var view = source as FrameworkElement;
+			if (typeName == null || view == null)
+				return;
+
+			var modelType = Type.GetType("Raven.Studio.Models." + typeName) ?? Type.GetType(typeName);
+			if (modelType == null)
+				return;
+
+			try
+			{
+				var model = Activator.CreateInstance(modelType);
+				var observableType = typeof(Observable<>).MakeGenericType(modelType);
+				var observable = Activator.CreateInstance(observableType);
+				var piValue = observableType.GetProperty("Value");
+				piValue.SetValue(observable, model, null);
+				view.DataContext = observable;
+			}
+			catch (Exception ex)
+			{
+				throw new InvalidOperationException(string.Format("Cannot create instance of model type: {0}", modelType), ex);
+			}
+		}
+
 		public static string GetAttachModel(UIElement element)
 		{
 			return (string)element.GetValue(AttachModelProperty);
 		}
 
 		public static void SetAttachModel(UIElement element, string value)
+		{
+			element.SetValue(AttachModelProperty, value);
+		}
+
+		public static string GetAttachObservableModel(UIElement element)
+		{
+			return (string)element.GetValue(AttachModelProperty);
+		}
+
+		public static void SetAttachObservableModel(UIElement element, string value)
 		{
 			element.SetValue(AttachModelProperty, value);
 		}
