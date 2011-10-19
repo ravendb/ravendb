624f1a867425b41888fb7b3ca81924e86fa9a700
Ayende Rahien <ayende@ayende.com>
2011-10-19 12:37:52 +0200

    Spaces->tabs

--- Raven.Tests/Bugs/Chripede/IndexOnList.cs
+++ Raven.Tests/Bugs/Chripede/IndexOnList.cs
@@ -7,66 +7,66 @@ using Xunit;
 
 namespace Raven.Tests.Bugs.Chripede
 {
-    public class IndexOnList : LocalClientTest
-    {
-        [Fact]
-        public void CanIndexAndQueryOnList()
-        {
-            using (var store = NewDocumentStore())
-            {
-                var container = new CompositionContainer(new TypeCatalog(typeof(Document_Index)));
-                IndexCreation.CreateIndexes(container, store);
+	public class IndexOnList : LocalClientTest
+	{
+		[Fact]
+		public void CanIndexAndQueryOnList()
+		{
+			using (var store = NewDocumentStore())
+			{
+				var container = new CompositionContainer(new TypeCatalog(typeof(Document_Index)));
+				IndexCreation.CreateIndexes(container, store);
 
-                using (var session = store.OpenSession())
-                {
-                    session.Store(new Document
-                                      {
-                                          List = new List<string>() { "test1", "test2", "test3" }
-                                      });
+				using (var session = store.OpenSession())
+				{
+					session.Store(new Document
+									  {
+										  List = new List<string>() { "test1", "test2", "test3" }
+									  });
 
-                    session.SaveChanges();
-                }
+					session.SaveChanges();
+				}
 
-                using (var session = store.OpenSession())
-                {
-                    var result = session.Query<Document, Document_Index>()
-                        .Customize(customization => customization.WaitForNonStaleResultsAsOfNow())
-                        .Where(x => x.List.Any(s => s == "test1"))
-                        .ToList();
+				using (var session = store.OpenSession())
+				{
+					var result = session.Query<Document, Document_Index>()
+						.Customize(customization => customization.WaitForNonStaleResultsAsOfNow())
+						.Where(x => x.List.Any(s => s == "test1"))
+						.ToList();
 
-                    Assert.Equal(1, result.Count);
-                }
+					Assert.Equal(1, result.Count);
+				}
 
-                //// Works when not using the index
-                //using (var session = store.OpenSession())
-                //{
-                //    var result = session.Query<Document>()
-                //        .Customize(customization => customization.WaitForNonStaleResultsAsOfNow())
-                //        .Where(x => x.List.Any(s => s == "test1"))
-                //        .ToList();
+				//// Works when not using the index
+				//using (var session = store.OpenSession())
+				//{
+				//    var result = session.Query<Document>()
+				//        .Customize(customization => customization.WaitForNonStaleResultsAsOfNow())
+				//        .Where(x => x.List.Any(s => s == "test1"))
+				//        .ToList();
 
-                //    Assert.Equal(1, result.Count);
-                //}
-            }
-        }
-    }
+				//    Assert.Equal(1, result.Count);
+				//}
+			}
+		}
+	}
 
-    public class Document
-    {
-        public string Id { get; set; }
+	public class Document
+	{
+		public string Id { get; set; }
 
-        public IList<string> List { get; set; }
-    }
+		public IList<string> List { get; set; }
+	}
 
-    public class Document_Index : AbstractIndexCreationTask<Document>
-    {
-        public Document_Index()
-        {
-            Map = docs => from doc in docs
-                          select new
-                                     {
-                                         doc.List,
-                                     };
-        }
-    }
+	public class Document_Index : AbstractIndexCreationTask<Document>
+	{
+		public Document_Index()
+		{
+			Map = docs => from doc in docs
+						  select new
+									 {
+										 doc.List,
+									 };
+		}
+	}
 }
