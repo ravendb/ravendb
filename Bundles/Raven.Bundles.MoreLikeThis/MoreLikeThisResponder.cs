028b070149fdfc8b041249cd72a97a87dfeac6fb
Ayende Rahien <ayende@ayende.com>
2011-10-19 11:20:54 +0200

    Refactoring MoreLikeThis feature

--- Bundles/Raven.Bundles.MoreLikeThis/MoreLikeThisResponder.cs
+++ Bundles/Raven.Bundles.MoreLikeThis/MoreLikeThisResponder.cs
@@ -5,10 +5,12 @@
 //-----------------------------------------------------------------------
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Index;
 using Lucene.Net.Search;
 using Raven.Abstractions.Data;
+using Raven.Abstractions.Indexing;
 using Raven.Database.Extensions;
 using Raven.Database.Server.Abstractions;
 using Raven.Database.Server.Responders;
@@ -32,38 +34,61 @@ namespace Raven.Bundles.MoreLikeThis
 			var match = urlMatcher.Match(context.GetRequestUrl());
 			var indexName = match.Groups[1].Value;
 			var documentId = match.Groups[2].Value;
-			
+
+			var fieldNames = context.Request.QueryString.GetValues("fields");
+
+			if (fieldNames == null || fieldNames.Length == 0)
+			{
+				context.SetStatusToBadRequest();
+				context.WriteJson(new { Error = "The query string must specify the fields to check" });
+				return;
+			}
+
+			var indexDefinition = Database.IndexDefinitionStorage.GetIndexDefinition(indexName);
+			if (indexDefinition == null)
+			{
+				context.SetStatusToNotFound();
+				context.WriteJson(new {Error = "The index " + indexName + " cannot be found"});
+				return;
+			}
+
+
+			PerformSearch(context, indexName, fieldNames, documentId, indexDefinition);
+		}
+
+		private void PerformSearch(IHttpContext context, string indexName, string[] fieldNames, string documentId,
+		                           IndexDefinition indexDefinition)
+		{
 			IndexSearcher searcher;
 			using (Database.IndexStorage.GetCurrentIndexSearcher(indexName, out searcher))
 			{
-				var td = searcher.Search(new TermQuery(new Term(Constants.DocumentIdFieldName, documentId)), 1); // get the current Lucene docid for the given RavenDB doc ID
-				if (td.ScoreDocs.Length == 1) // only do this if the doc was found in the specified index
+				var td = searcher.Search(new TermQuery(new Term(Constants.DocumentIdFieldName, documentId)), 1);
+					// get the current Lucene docid for the given RavenDB doc ID
+				if (td.ScoreDocs.Length == 0)
 				{
-					var mlt = new Similarity.Net.MoreLikeThis(searcher.GetIndexReader());
-					mlt.SetAnalyzer(new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_29)); // TODO: Determine correct analyzer programmatically
-					mlt.SetFieldNames(context.Request.QueryString["fields"].Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)); // what fields to base the MLT operation on
-					// TODO: Figure out other relevant configs. It will probably make sense to use a config doc, like in facets
-
-					byte maxDocs;
-					if (!byte.TryParse(context.Request.QueryString["maxDocs"], out maxDocs))
-						maxDocs = 10;
-
-					var mltQuery = mlt.Like(td.ScoreDocs[0].doc);
-					var tsdc = TopScoreDocCollector.create(maxDocs, true);
-					searcher.Search(mltQuery, tsdc);
-					var hits = tsdc.TopDocs().ScoreDocs;
-
-					var retSet = new HashSet<JsonDocument>();
-					foreach (var hit in hits)
-					{
-						var docId = searcher.Doc(hit.doc).Get(Constants.DocumentIdFieldName);
-						var doc = Database.Get(docId, null);
-						if (doc != null)
-							retSet.Add(doc);
-					}
-
-					context.WriteJson(retSet);
+					context.SetStatusToNotFound();
+					context.WriteJson(new {Error = "Document " + documentId + " could not be found"});
+					return;
 				}
+				var mlt = new Similarity.Net.MoreLikeThis(searcher.GetIndexReader());
+
+				mlt.SetAnalyzer(indexDefinition.GetAnalyzer(fieldNames[0])); // we use the analyzer of the first speciifed field
+
+				mlt.SetFieldNames(fieldNames);
+
+				var mltQuery = mlt.Like(td.ScoreDocs[0].doc);
+				var tsdc = TopScoreDocCollector.create(context.GetPageSize(Database.Configuration.MaxPageSize), true);
+				searcher.Search(mltQuery, tsdc);
+				var hits = tsdc.TopDocs().ScoreDocs;
+
+				var documentIds = hits.Select(hit => searcher.Doc(hit.doc).Get(Constants.DocumentIdFieldName)).Distinct();
+
+				context.WriteJson(
+					from docId in documentIds
+					let doc = Database.Get(docId, null)
+					where doc != null
+					select doc
+					);
 			}
 		}
 	}
