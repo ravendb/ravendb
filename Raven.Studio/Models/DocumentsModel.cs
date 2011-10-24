c135075b7fb9d14c399074046974fd345dfb8ccc
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-10-24 16:12:51 +0200

    minor

--- Raven.Studio/Models/DocumentsModel.cs
+++ Raven.Studio/Models/DocumentsModel.cs
@@ -7,43 +7,43 @@ using Raven.Studio.Infrastructure;
 
 namespace Raven.Studio.Models
 {
-    public class DocumentsModel : Model
-    {
+	public class DocumentsModel : Model
+	{
 		private readonly Func<DocumentsModel, int, Task> fetchDocuments;
-        private readonly int itemsPerPages;
-        public BindableCollection<ViewableDocument> Documents { get; private set; }
+		private readonly int itemsPerPages;
+		public BindableCollection<ViewableDocument> Documents { get; private set; }
 
-        public DocumentsModel(Func<DocumentsModel,int, Task> fetchDocuments, string location, int itemsPerPages)
-        {
-        	this.fetchDocuments = fetchDocuments;
-        	this.location = location;
-            this.itemsPerPages = itemsPerPages;
-            TotalPages = new Observable<long>();
-            Documents = new BindableCollection<ViewableDocument>(new PrimaryKeyComparer<ViewableDocument>(document => document.Id));
-        }
-    
-    	protected override Task TimerTickedAsync()
-    	{
-    		return fetchDocuments(this, CurrentPage - 1);
-    	}
+		public DocumentsModel(Func<DocumentsModel,int, Task> fetchDocuments, string location, int itemsPerPages)
+		{
+			this.fetchDocuments = fetchDocuments;
+			this.location = location;
+			this.itemsPerPages = itemsPerPages;
+			TotalPages = new Observable<long>();
+			Documents = new BindableCollection<ViewableDocument>(new PrimaryKeyComparer<ViewableDocument>(document => document.Id));
+		}
+	
+		protected override Task TimerTickedAsync()
+		{
+			return fetchDocuments(this, CurrentPage - 1);
+		}
 
-        public int CurrentPage
-        {
-            get { return UrlUtil.GetSkipCount() / itemsPerPages + 1; }
-        }
+		public int CurrentPage
+		{
+			get { return UrlUtil.GetSkipCount() / itemsPerPages + 1; }
+		}
 
-        public ICommand NextPage
-        {
-            get{ return new IncreasePageCommand(location, itemsPerPages, TotalPages);}
-        }
+		public ICommand NextPage
+		{
+			get{ return new IncreasePageCommand(location, itemsPerPages, TotalPages);}
+		}
 
-        public ICommand PreviousPage
-        {
-            get { return new DecreasePageCommand(location, itemsPerPages); }
-        }
+		public ICommand PreviousPage
+		{
+			get { return new DecreasePageCommand(location, itemsPerPages); }
+		}
 
-        private readonly string location;
+		private readonly string location;
 
-        public Observable<long> TotalPages { get; set; }
-    }
+		public Observable<long> TotalPages { get; set; }
+	}
 }
