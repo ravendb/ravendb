443025facdffb2a35ce7299010ff19ec0384ac84
Ayende Rahien <ayende@ayende.com>
2011-10-24 16:29:51 +0200

    Allow access to a list of urls that are always accessible

--- Raven.Database/Server/Security/Windows/WindowsRequestAuthorizer.cs
+++ Raven.Database/Server/Security/Windows/WindowsRequestAuthorizer.cs
@@ -1,12 +1,32 @@
+using System;
 using Raven.Database.Extensions;
 using Raven.Database.Server.Abstractions;
+using System.Linq;
 
 namespace Raven.Database.Server.Security.Windows
 {
 	public class WindowsRequestAuthorizer : AbstractRequestAuthorizer
 	{
+		readonly string[] neverSecretUrls = new[]
+			{
+				// allow to get files that are static and are never secret, for example, the studio, the cross domain
+				// policy and the fav icon
+				"/",
+				"/raven/studio.html",
+				"/silverlight/Raven.Studio.xap",
+				"/favicon.ico",
+				"/clientaccesspolicy.xml",
+				"/build/version",
+			};
+
 		public override bool Authorize(IHttpContext ctx)
 		{
+			var requestUrl = ctx.GetRequestUrl();
+
+			if (neverSecretUrls.Contains(requestUrl, StringComparer.InvariantCultureIgnoreCase))
+				return true;
+
+
 			if (server.DefaultConfiguration.AnonymousUserAccessMode == AnonymousUserAccessMode.None && IsInvalidUser(ctx))
 			{
 				ctx.SetStatusToUnauthorized();
@@ -30,7 +50,10 @@ namespace Raven.Database.Server.Security.Windows
 
 		private static bool IsInvalidUser(IHttpContext ctx)
 		{
-			return (ctx.User == null || ctx.User.Identity == null || ctx.User.Identity.IsAuthenticated == false);
+			return (ctx.User == null || 
+				ctx.User.Identity == null || 
+				ctx.User.Identity.IsAuthenticated == false) || 
+				ctx.User.IsInRole();
 		}
 	}
 }
