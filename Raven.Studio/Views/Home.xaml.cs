d7c697ed5dd668d03ceef204a6489f9ed618a744
Fitzchak Yitzchaki <fitzchak@ayende.com>
2011-11-01 13:19:26 +0200

    Fix namespace

--- Raven.Studio/Views/Home.xaml.cs
+++ Raven.Studio/Views/Home.xaml.cs
@@ -1,29 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Raven.Studio.Infrastructure;
+﻿using Raven.Studio.Infrastructure;
 
-namespace Raven.Studio
+namespace Raven.Studio.Views
 {
 	public partial class Home : View
 	{
 		public Home()
 		{
 			InitializeComponent();
-		}
-
-		// Executes when the user navigates to this page.
-		protected override void OnNavigatedTo(NavigationEventArgs e)
-		{
 		}
 	}
 }
