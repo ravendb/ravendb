0cf6e816927e46f2224107a40bf4d43cbc9e379f
Fitzchak Yitzchaki <fitzchak@ayende.com>
2011-11-03 16:50:21 +0200

    Edit message's text

--- Raven.Database/Plugins/Builtins/CreateFolderIcon.cs
+++ Raven.Database/Plugins/Builtins/CreateFolderIcon.cs
@@ -37,7 +37,7 @@ namespace Raven.Database.Plugins.Builtins
 				File.WriteAllText(desktopIni, string.Format(@"
 [.ShellClassInfo]
 IconResource={0},0
-InfoTip=Contains RavenDB database data folder. It's recomand that you will have a backup of this folder.
+InfoTip=RavenDB's data folder. It's recomand that you will have a backup of this folder.
 [ViewState]
 Mode=
 Vid=
