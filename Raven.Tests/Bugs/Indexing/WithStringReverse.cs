0d26735cd30f59d8c93d376cf7966a7d44573166
Ayende Rahien <ayende@ayende.com>
2011-11-09 18:00:24 -0500

    We now support (some) extension methods on dynamic values. Fixed the test showing how to query on reversed string

--- Raven.Tests/Bugs/Indexing/WithStringReverse.cs
+++ Raven.Tests/Bugs/Indexing/WithStringReverse.cs
@@ -1,7 +1,7 @@
 using System.Linq;
 using Raven.Abstractions.Indexing;
 using Raven.Client;
-using Raven.Client.Document;
+using Raven.Client.Linq;
 using Raven.Client.Embedded;
 using Xunit;
 
@@ -42,9 +42,48 @@ namespace Raven.Tests.Bugs.Indexing
 					Assert.Empty(store.DocumentDatabase.Statistics.Errors);
 					Assert.NotNull(users);
 					Assert.True(users.Count > 0);
+				}
+			}
+		}
 
-					// Should I also test that the first result is reversed?
-					// If so, then i would need a result class .. not sure how to do that..
+		public class ReversedResult
+		{
+			public string ReverseName { get; set; }
+		}
+		[Fact]
+		public void CanQueryInReverse()
+		{
+			using (EmbeddableDocumentStore store = NewDocumentStore())
+			{
+				store.DatabaseCommands.PutIndex("StringReverseIndex",
+												new IndexDefinition
+												{
+													Map =
+														"from doc in docs select new { doc.Name, ReverseName = doc.Name.Reverse())}"
+												});
+
+				using (IDocumentSession documentSession = store.OpenSession())
+				{
+					documentSession.Store(new User { Name = "Ayende" });
+					documentSession.Store(new User { Name = "Itamar" });
+					documentSession.Store(new User { Name = "Pure Krome" });
+					documentSession.Store(new User { Name = "John Skeet" });
+					documentSession.Store(new User { Name = "StackOverflow" });
+					documentSession.Store(new User { Name = "Wow" });
+					documentSession.SaveChanges();
+				}
+
+				using (IDocumentSession documentSession = store.OpenSession())
+				{
+					var users = documentSession
+						.Query<ReversedResult>("StringReverseIndex")
+						.Customize(x => x.WaitForNonStaleResults())
+						.Where(x=>x.ReverseName.StartsWith("edn"))
+						.As<User>()
+						.ToList();
+
+					Assert.Empty(store.DocumentDatabase.Statistics.Errors);
+					Assert.True(users.Count > 0);
 				}
 			}
 		}
