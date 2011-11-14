5d5d32d590637bb7e15dc1cacb10e7e70b7c11b5
Itamar Syn-Hershko <itamar@hibernatingrhinos.com>
2011-11-14 13:16:40 +0200

    Fixing incorrect Uri

--- Bundles/Raven.Client.MoreLikeThis/ServerClientExtensions.cs
+++ Bundles/Raven.Client.MoreLikeThis/ServerClientExtensions.cs
@@ -106,7 +106,7 @@ namespace Raven.Client.MoreLikeThis
                     uri.AppendFormat("maxNumTokens={0}&", parameters.MaximumNumberOfTokensParsed);
                 if (parameters.MaximumWordLength != null &&
                     parameters.MaximumWordLength != MoreLikeThisQueryParameters.DefaultMaximumWordLength)
-                    uri.AppendFormat("maxWordLen={0}", parameters.MaximumWordLength);
+                    uri.AppendFormat("maxWordLen={0}&", parameters.MaximumWordLength);
                 if (parameters.MinimumDocumentFrequency != null &&
                     parameters.MinimumDocumentFrequency != MoreLikeThisQueryParameters.DefaltMinimumDocumentFrequency)
                     uri.AppendFormat("minDocFreq={0}&", parameters.MinimumDocumentFrequency);
