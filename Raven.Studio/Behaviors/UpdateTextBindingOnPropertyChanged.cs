bb5f9369e03c7c19de8e8a3bf6762bb2afe0bf31
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-11-21 11:22:10 +0200

    Solve an issue, where entering text to the confim window and pressing enter, closed the window but return null answer because Silverlight's textboxes do not updating binding by default on TextChange event.

---
+++ Raven.Studio/Behaviors/UpdateTextBindingOnPropertyChanged.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="UpdateTextBindingOnPropertyChanged.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Interactivity;
+
+namespace Raven.Studio.Behaviors
+{
+	public class UpdateTextBindingOnPropertyChanged : Behavior<TextBox>
+	{
+		private BindingExpression expression;
+
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+
+			expression = AssociatedObject.GetBindingExpression(TextBox.TextProperty);
+			AssociatedObject.TextChanged += this.OnTextChanged;
+		}
+
+
+		protected override void OnDetaching()
+		{
+			base.OnDetaching();
+
+			AssociatedObject.TextChanged -= this.OnTextChanged;
+			expression = null;
+		}
+
+		private void OnTextChanged(object sender, EventArgs args)
+		{
+			expression.UpdateSource();
+		}
+	}
+}
