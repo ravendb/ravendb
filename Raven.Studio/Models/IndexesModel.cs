6e113c5e86b9747495332845c757e63e31da9656
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-11-24 09:59:42 +0200

    minor - tabbing

--- Raven.Studio/Models/IndexesModel.cs
+++ Raven.Studio/Models/IndexesModel.cs
@@ -9,7 +9,7 @@ namespace Raven.Studio.Models
 {
 	public class IndexesModel : ViewModel
 	{
-        public BindableCollection<IndexListItemModel> GroupedIndexes { get; private set; }
+		public BindableCollection<IndexListItemModel> GroupedIndexes { get; private set; }
 
 		public IndexesModel()
 		{
@@ -25,39 +25,39 @@ namespace Raven.Studio.Models
 				.ContinueOnSuccess(UpdateGroupedIndexList);
 		}
 
-	    private void UpdateGroupedIndexList(IList<string> indexes)
-	    {
-	        var indexGroups = from index in indexes
-	                          let groupDetails = GetIndexGroup(index)
-                              let indexGroup = groupDetails.Item1
-                              let indexOrder = groupDetails.Item2
-                              orderby indexOrder
-	                          group index by indexGroup;
+		private void UpdateGroupedIndexList(IList<string> indexes)
+		{
+			var indexGroups = from index in indexes
+							  let groupDetails = GetIndexGroup(index)
+							  let indexGroup = groupDetails.Item1
+							  let indexOrder = groupDetails.Item2
+							  orderby indexOrder
+							  group index by indexGroup;
 
-	        var indexesAndGroupHeaders =
-	            indexGroups.SelectMany(
-	                group => (new IndexListItemModel[] {new IndexGroupHeader() {Name = group.Key}})
-	                             .Concat(
-	                                 group.Select(index => new IndexModel() {IndexName = index}).Cast<IndexListItemModel>()));
+			var indexesAndGroupHeaders =
+				indexGroups.SelectMany(
+					group => (new IndexListItemModel[] {new IndexGroupHeader() {Name = group.Key}})
+								 .Concat(
+									 group.Select(index => new IndexModel() {IndexName = index}).Cast<IndexListItemModel>()));
 
-	        GroupedIndexes.Set(indexesAndGroupHeaders);
-	    }
+			GroupedIndexes.Set(indexesAndGroupHeaders);
+		}
 
-        private Tuple<string,int> GetIndexGroup(string indexName)
-        {
-            if (indexName.StartsWith("Temp/", StringComparison.InvariantCultureIgnoreCase))
-            {
-                return Tuple.Create("Temp Indexes", 1);
-            }
-            else if (indexName.StartsWith("Auto/", StringComparison.InvariantCultureIgnoreCase))
-            {
-                return Tuple.Create("Auto Indexes", 2);
-            }
-            else
-            {
-                return Tuple.Create("Indexes", 3);
-            }
-        }
+		private Tuple<string,int> GetIndexGroup(string indexName)
+		{
+			if (indexName.StartsWith("Temp/", StringComparison.InvariantCultureIgnoreCase))
+			{
+				return Tuple.Create("Temp Indexes", 1);
+			}
+			else if (indexName.StartsWith("Auto/", StringComparison.InvariantCultureIgnoreCase))
+			{
+				return Tuple.Create("Auto Indexes", 2);
+			}
+			else
+			{
+				return Tuple.Create("Indexes", 3);
+			}
+		}
 
 	}
 }
