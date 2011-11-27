3f52e989e0474bf5de7f0439ce88547cad25460e
Fitzchak Yitzchaki <fitzchak@ayende.com>
2011-11-27 18:22:56 +0200

    View a short snapshot of the JSON document

--- Raven.Studio/Infrastructure/StringExtensions.cs
+++ Raven.Studio/Infrastructure/StringExtensions.cs
@@ -1,14 +1,24 @@
-﻿namespace Raven.Studio.Infrastructure
+﻿using System;
+
+namespace Raven.Studio.Infrastructure
 {
 	public static class StringExtensions
 	{
 		public static string ReplaceSingle(this string str, string toReplace, string newString)
 		{
-			var index = str.IndexOf(toReplace);
+			var index = str.IndexOf(toReplace, StringComparison.Ordinal);
 			if (index == -1)
 				return newString;
 
 			return str.Remove(index, toReplace.Length).Insert(index, newString);
+		}
+
+		public static string ShortViewOfString(this string str, int margin, string replacement = " ... ")
+		{
+			if (str.Length <= margin*2 + replacement.Length)
+				return str;
+
+			return str.Substring(0, margin) + replacement + str.Substring(str.Length - margin - 1);
 		}
 	}
 }
