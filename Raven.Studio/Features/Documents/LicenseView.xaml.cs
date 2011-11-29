3404c596b4f2d4fe8679ffe33e84d21468f0418a
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-11-29 10:45:12 +0200

    Refactor footer section. Add LicenseView to the footer.

---
+++ Raven.Studio/Features/Documents/LicenseView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Raven.Studio.Infrastructure;
+
+namespace Raven.Studio.Features.Documents
+{
+	public partial class LicenseView : View
+	{
+		public LicenseView()
+		{
+			InitializeComponent();
+		}
+	}
+}
