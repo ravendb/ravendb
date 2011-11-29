a7f538bcdc7d8892e51bebbf003a455ccab49bf2
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-11-29 13:26:21 +0200

    Show dialog with the licensing message

---
+++ Raven.Studio/Commands/ShowLicensingStatusCommand.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ShowLicensingStatusCommand.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Raven.Abstractions.Data;
+using Raven.Studio.Infrastructure;
+
+namespace Raven.Studio.Commands
+{
+	public class ShowLicensingStatusCommand : Command
+	{
+		public override void Execute(object parameter)
+		{
+			var licensing = (LicensingStatus) parameter;
+			ErrorPresenter.Show("Licensing Status", licensing.Message);
+		}
+	}
+}
