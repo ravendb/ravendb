bdf2461f5816e3e24c6ee7f68e11516f24b59af5
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-11-30 10:26:46 +0200

    Fix a bug where using <ei:KeyTrigger> caused the KeyTrigger to trigger the command even after we navigated away from this command

--- Raven.Studio/Features/Documents/DocumentsView.xaml.cs
+++ Raven.Studio/Features/Documents/DocumentsView.xaml.cs
@@ -1,17 +1,40 @@
 ﻿using System.Linq;
 using System.Windows.Controls;
+using System.Windows.Input;
+using Raven.Studio.Commands;
 using Raven.Studio.Infrastructure;
 
 namespace Raven.Studio.Features.Documents
 {
 	public partial class DocumentsView : View
 	{
+		private readonly DeleteDocumentsCommand deleteDocuments = new DeleteDocumentsCommand();
+
 		public DocumentsView()
 		{
 			InitializeComponent();
+
+			DocumentsList.SelectionChanged += DocumentsListOnSelectionChanged;
+			DocumentsList.KeyDown += DocumentsListOnKeyDown;
 		}
 
-		private void DocumentsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		private void DocumentsListOnKeyDown(object sender, KeyEventArgs args)
+		{
+			switch (args.Key)
+			{
+				case Key.Delete:
+					ExecuteCommand(deleteDocuments, sender);
+					break;
+			}
+		}
+
+		private void ExecuteCommand(ICommand command, object param = null)
+		{
+			if (command.CanExecute(param))
+				command.Execute(param);
+		}
+
+		private void DocumentsListOnSelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
 			var commands = DocumentsContextMenu.Items
 				.Cast<MenuItem>()
