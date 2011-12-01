5ec003ad739c0d035c271b13ef1e2362e412c6c6
Ayende Rahien <ayende@ayende.com>
2011-12-01 12:13:05 +0200

    Refactoring to use just a single parameters object

--- Bundles/Raven.Bundles.MoreLikeThis/MoreLikeThisResponder.cs
+++ Bundles/Raven.Bundles.MoreLikeThis/MoreLikeThisResponder.cs
@@ -25,10 +25,6 @@ namespace Raven.Bundles.MoreLikeThis
 {
 	public class MoreLikeThisResponder : RequestResponder
 	{
-        //private readonly Analyzer DEFAULT_ANALYZER = new StandardAnalyzer(Version.LUCENE_29);
-
-        //private readonly Analyzer DEFAULT_ANALYZER = new WhitespaceAnalyzer();
-
 		public override string UrlPattern
 		{
 			get { return @"^/morelikethis/([\w\-_]+)/(.+)"; } // /morelikethis/(index-name)/(ravendb-document-id)
@@ -43,74 +39,79 @@ namespace Raven.Bundles.MoreLikeThis
 		{
 			var match = urlMatcher.Match(context.GetRequestUrl());
 			var indexName = match.Groups[1].Value;
-			var documentId = match.Groups[2].Value;
 
-			var fieldNames = context.Request.QueryString.GetValues("fields");
+			var parameters = new MoreLikeThisQueryParameters
+								 {
+									 DocumentId = match.Groups[2].Value,
+									 Fields = context.Request.QueryString.GetValues("fields"),
+									 Boost = GetNullableBool(context.Request.QueryString.Get("boost")),
+									 MaximumNumberOfTokensParsed = GetNullableInt(context.Request.QueryString.Get("maxNumTokens")),
+									 MaximumQueryTerms = GetNullableInt(context.Request.QueryString.Get("maxQueryTerms")),
+									 MaximumWordLength = GetNullableInt(context.Request.QueryString.Get("maxWordLen")),
+									 MinimumDocumentFrequency = GetNullableInt(context.Request.QueryString.Get("minDocFreq")),
+									 MinimumTermFrequency = GetNullableInt(context.Request.QueryString.Get("minTermFreq")),
+									 MinimumWordLength = GetNullableInt(context.Request.QueryString.Get("minWordLen")),
+									 StopWordsDocumentId = context.Request.QueryString.Get("stopWords")
+								 };
 
-		    var parameters = new MoreLikeThisQueryParameters
-		                         {
-                                     Boost = GetNullableBool(context.Request.QueryString.Get("boost")),
-                                     MaximumNumberOfTokensParsed = GetNullableInt(context.Request.QueryString.Get("maxNumTokens")),
-                                     MaximumQueryTerms = GetNullableInt(context.Request.QueryString.Get("maxQueryTerms")),
-                                     MaximumWordLength = GetNullableInt(context.Request.QueryString.Get("maxWordLen")),
-                                     MinimumDocumentFrequency = GetNullableInt(context.Request.QueryString.Get("minDocFreq")),
-                                     MinimumTermFrequency = GetNullableInt(context.Request.QueryString.Get("minTermFreq")),
-                                     MinimumWordLength = GetNullableInt(context.Request.QueryString.Get("minWordLen")),
-                                     StopWordsDocumentId = context.Request.QueryString.Get("stopWords")
-		                         };
-            
 
 			var indexDefinition = Database.IndexDefinitionStorage.GetIndexDefinition(indexName);
 			if (indexDefinition == null)
 			{
 				context.SetStatusToNotFound();
-				context.WriteJson(new {Error = "The index " + indexName + " cannot be found"});
+				context.WriteJson(new { Error = "The index " + indexName + " cannot be found" });
 				return;
 			}
 
+			if (string.IsNullOrEmpty(parameters.DocumentId))
+			{
+				context.SetStatusToBadRequest();
+				context.WriteJson(new { Error = "The document id is mandatory" });
+				return;
+			}
 
-			PerformSearch(context, indexName, fieldNames, documentId, indexDefinition, parameters);
+			PerformSearch(context, indexName, indexDefinition, parameters);
 		}
 
-	    private void PerformSearch(IHttpContext context, string indexName, string[] fieldNames, string documentId, IndexDefinition indexDefinition, MoreLikeThisQueryParameters parameters)
+		private void PerformSearch(IHttpContext context, string indexName, IndexDefinition indexDefinition, MoreLikeThisQueryParameters parameters)
 		{
 			IndexSearcher searcher;
 			using (Database.IndexStorage.GetCurrentIndexSearcher(indexName, out searcher))
 			{
-				var td = searcher.Search(new TermQuery(new Term(Constants.DocumentIdFieldName, documentId)), 1);
-			    // get the current Lucene docid for the given RavenDB doc ID
+				var td = searcher.Search(new TermQuery(new Term(Constants.DocumentIdFieldName, parameters.DocumentId)), 1);
+				// get the current Lucene docid for the given RavenDB doc ID
 				if (td.ScoreDocs.Length == 0)
 				{
 					context.SetStatusToNotFound();
-					context.WriteJson(new {Error = "Document " + documentId + " could not be found"});
+					context.WriteJson(new { Error = "Document " + parameters.DocumentId + " could not be found" });
 					return;
 				}
-			    var ir = searcher.GetIndexReader();
-                var mlt = new RavenMoreLikeThis(ir);
+				var ir = searcher.GetIndexReader();
+				var mlt = new RavenMoreLikeThis(ir);
 
-			    AssignParameters(mlt, parameters);
+				AssignParameters(mlt, parameters);
 
-                if (parameters.StopWordsDocumentId != null)
-                {
-                    var stopWordsDoc = Database.Get(parameters.StopWordsDocumentId, null);
-                    if (stopWordsDoc == null)
-                    {
-                        context.SetStatusToNotFound();
-                        context.WriteJson(
-                            new
-                                {
-                                    Error = "Stop words document " + parameters.StopWordsDocumentId + " could not be found"
-                                });
-                        return;
-                    }
-                    var stopWords = stopWordsDoc.DataAsJson.JsonDeserialization<StopWordsSetup>().StopWords;
-                    mlt.SetStopWords(new Hashtable(stopWords.ToDictionary(x => x.ToLower())));
-                }
+				if (parameters.StopWordsDocumentId != null)
+				{
+					var stopWordsDoc = Database.Get(parameters.StopWordsDocumentId, null);
+					if (stopWordsDoc == null)
+					{
+						context.SetStatusToNotFound();
+						context.WriteJson(
+							new
+								{
+									Error = "Stop words document " + parameters.StopWordsDocumentId + " could not be found"
+								});
+						return;
+					}
+					var stopWords = stopWordsDoc.DataAsJson.JsonDeserialization<StopWordsSetup>().StopWords;
+					mlt.SetStopWords(new Hashtable(stopWords.ToDictionary(x => x.ToLower())));
+				}
 
-			    fieldNames = fieldNames ?? GetFieldNames(ir);
-                mlt.SetFieldNames(fieldNames);
+				var fieldNames = parameters.Fields ?? GetFieldNames(ir);
+				mlt.SetFieldNames(fieldNames);
 
-			    mlt.Analyzers = GetAnalyzers(indexDefinition, fieldNames);
+				mlt.Analyzers = GetAnalyzers(indexDefinition, fieldNames);
 
 				var mltQuery = mlt.Like(td.ScoreDocs[0].doc);
 				var tsdc = TopScoreDocCollector.create(context.GetPageSize(Database.Configuration.MaxPageSize), true);
@@ -122,7 +123,7 @@ namespace Raven.Bundles.MoreLikeThis
 
 				var jsonDocuments =
 					documentIds
-						.Where(docId => string.Equals(docId,documentId, StringComparison.InvariantCultureIgnoreCase) == false)
+						.Where(docId => string.Equals(docId, parameters.DocumentId, StringComparison.InvariantCultureIgnoreCase) == false)
 						.Select(docId => Database.Get(docId, null))
 						.Where(it => it != null)
 						.ToArray();
@@ -131,7 +132,7 @@ namespace Raven.Bundles.MoreLikeThis
 
 				var includedEtags = new List<byte>(jsonDocuments.SelectMany(x => x.Etag.Value.ToByteArray()));
 				includedEtags.AddRange(Database.GetIndexEtag(indexName).ToByteArray());
-				var loadedIds = new HashSet<string>(jsonDocuments.Select(x=>x.Key));
+				var loadedIds = new HashSet<string>(jsonDocuments.Select(x => x.Key));
 				var addIncludesCommand = new AddIncludesCommand(Database, GetRequestTransaction(context), (etag, includedDoc) =>
 				{
 					includedEtags.AddRange(etag.ToByteArray());
@@ -163,43 +164,38 @@ namespace Raven.Bundles.MoreLikeThis
 			}
 		}
 
-        private static int? GetNullableInt(string value)
-        {
-            if (value == null) return null;
-            return int.Parse(value);
-        }
+		private static int? GetNullableInt(string value)
+		{
+			if (value == null) return null;
+			return int.Parse(value);
+		}
 
-        private static bool? GetNullableBool(string value)
-        {
-            if (value == null) return null;
-            return true;
-        }
+		private static bool? GetNullableBool(string value)
+		{
+			if (value == null) return null;
+			return true;
+		}
 
-	    private void AssignParameters(Similarity.Net.MoreLikeThis mlt, MoreLikeThisQueryParameters parameters)
-	    {
-	        if(parameters.Boost != null) mlt.SetBoost(parameters.Boost.Value);
-            if(parameters.MaximumNumberOfTokensParsed!= null) mlt.SetMaxNumTokensParsed(parameters.MaximumNumberOfTokensParsed.Value);
-            if(parameters.MaximumQueryTerms != null) mlt.SetMaxQueryTerms(parameters.MaximumQueryTerms.Value);
-            if(parameters.MaximumWordLength != null) mlt.SetMaxWordLen(parameters.MaximumWordLength.Value);
-            if(parameters.MinimumDocumentFrequency != null) mlt.SetMinDocFreq(parameters.MinimumDocumentFrequency.Value);
-            if(parameters.MinimumTermFrequency != null) mlt.SetMinTermFreq(parameters.MinimumTermFrequency.Value);
-            if(parameters.MinimumWordLength != null) mlt.SetMinWordLen(parameters.MinimumWordLength.Value);
-	    }
+		private static void AssignParameters(Similarity.Net.MoreLikeThis mlt, MoreLikeThisQueryParameters parameters)
+		{
+			if (parameters.Boost != null) mlt.SetBoost(parameters.Boost.Value);
+			if (parameters.MaximumNumberOfTokensParsed != null) mlt.SetMaxNumTokensParsed(parameters.MaximumNumberOfTokensParsed.Value);
+			if (parameters.MaximumQueryTerms != null) mlt.SetMaxQueryTerms(parameters.MaximumQueryTerms.Value);
+			if (parameters.MaximumWordLength != null) mlt.SetMaxWordLen(parameters.MaximumWordLength.Value);
+			if (parameters.MinimumDocumentFrequency != null) mlt.SetMinDocFreq(parameters.MinimumDocumentFrequency.Value);
+			if (parameters.MinimumTermFrequency != null) mlt.SetMinTermFreq(parameters.MinimumTermFrequency.Value);
+			if (parameters.MinimumWordLength != null) mlt.SetMinWordLen(parameters.MinimumWordLength.Value);
+		}
 
-	    private static Dictionary<string, Analyzer> GetAnalyzers(IndexDefinition indexDefinition, IEnumerable<string> fieldNames)
-	    {
-	        var dictionary = new Dictionary<string, Analyzer>();
-	        foreach (var fieldName in fieldNames)
-	        {
-	            dictionary.Add(fieldName, indexDefinition.GetAnalyzer(fieldName));
-	        }
-	        return dictionary;
-	    }
+		private static Dictionary<string, Analyzer> GetAnalyzers(IndexDefinition indexDefinition, IEnumerable<string> fieldNames)
+		{
+			return fieldNames.ToDictionary(fieldName => fieldName, fieldName => indexDefinition.GetAnalyzer(fieldName));
+		}
 
-	    private static string[] GetFieldNames(IndexReader indexReader)
-	    {
-            var fields = indexReader.GetFieldNames(IndexReader.FieldOption.INDEXED);
-            return fields.Where(x => x != Constants.DocumentIdFieldName).ToArray();
-	    }
+		private static string[] GetFieldNames(IndexReader indexReader)
+		{
+			var fields = indexReader.GetFieldNames(IndexReader.FieldOption.INDEXED);
+			return fields.Where(x => x != Constants.DocumentIdFieldName).ToArray();
+		}
 	}
 }
