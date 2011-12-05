2d64486777b91fcfe5604c31b7b9e609afb2a183
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-12-05 14:05:17 +0200

    Select a document in the list when right clicking on it.

---
+++ Raven.Studio/Behaviors/SelectItemOnRightClick.cs
@@ -0,0 +1,42 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="SelectItemOnRightClick.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+
+namespace Raven.Studio.Behaviors
+{
+	public class SelectItemOnRightClick : Behavior<ListBox>
+	{
+		protected override void OnAttached()
+		{
+			AssociatedObject.MouseRightButtonDown += AssociatedObjectOnMouseRightButtonDown;
+			base.OnAttached();
+		}
+
+		protected override void OnDetaching()
+		{
+			AssociatedObject.MouseRightButtonDown -= AssociatedObjectOnMouseRightButtonDown;
+			base.OnDetaching();
+		}
+
+		void AssociatedObjectOnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			var item = VisualTreeHelper.FindElementsInHostCoordinates(e.GetPosition(null),AssociatedObject)
+				.OfType<ListBoxItem>()
+				.FirstOrDefault();
+
+			if (item == null || AssociatedObject.SelectedItems.Contains(item.DataContext)) 
+				return;
+
+			AssociatedObject.SelectedItems.Clear();
+			item.IsSelected = true;
+		}
+	}
+}
