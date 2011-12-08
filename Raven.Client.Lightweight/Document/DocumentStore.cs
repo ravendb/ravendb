7892cc2f5ccf414d9ef71be28e00df83b81d20f9
Ayende Rahien <ayende@ayende.com>
2011-12-08 11:44:14 +0200

    Setting PreAuthenticate = true

--- Raven.Client.Lightweight/Document/DocumentStore.cs
+++ Raven.Client.Lightweight/Document/DocumentStore.cs
@@ -545,6 +545,7 @@ namespace Raven.Client.Document
 #if !SILVERLIGHT
 			var authRequest = (HttpWebRequest)WebRequest.Create(oauthSource);
 			authRequest.Credentials = Credentials;
+			authRequest.PreAuthenticate = true;
 			authRequest.Headers["Accept-Encoding"] = "deflate,gzip";
 			authRequest.PreAuthenticate = true;
 #else
