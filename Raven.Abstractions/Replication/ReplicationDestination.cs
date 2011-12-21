d594ac8c7ddea8b661bc80b84ea2eb58d3e92b17
Ayende Rahien <ayende@ayende.com>
2011-12-21 16:25:09 +0200

    Adding option to control transitive replication (default is OFF)

--- Raven.Abstractions/Replication/ReplicationDestination.cs
+++ Raven.Abstractions/Replication/ReplicationDestination.cs
@@ -20,5 +20,27 @@ namespace Raven.Abstractions.Replication
 		/// </summary>
 		/// <value>The URL.</value>
 		public string Url { get; set; }
+
+		/// <summary>
+		/// How should the replication bundle behave with respect to replicated documents.
+		/// If a document was replicated to us from another node, should we replicate that to
+		/// this destination, or should we replicate only documents that were locally modified.
+		/// </summary>
+		public TransitiveReplicationOptions TransitiveReplicationBehavior { get; set; }
+	}
+
+	/// <summary>
+	/// Options for how to replicate replicated documents
+	/// </summary>
+	public enum TransitiveReplicationOptions
+	{
+		/// <summary>
+		/// Don't replicate replicated documents
+		/// </summary>
+		None,
+		/// <summary>
+		/// Replicate replicated documents
+		/// </summary>
+		Replicate
 	}
 }
