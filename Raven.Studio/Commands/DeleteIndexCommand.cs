d4186ab7ea7a4caf430978a6abcfebe8f4e84ca1
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-12-27 15:33:15 +0200

    Implement DeleteIndexCommand

--- Raven.Studio/Commands/DeleteIndexCommand.cs
+++ Raven.Studio/Commands/DeleteIndexCommand.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using System.Linq;
-using Raven.Abstractions.Commands;
 using Raven.Studio.Features.Input;
 using Raven.Studio.Infrastructure;
 using Raven.Studio.Messages;
@@ -12,42 +10,25 @@ namespace Raven.Studio.Commands
 	{
 		public override void Execute(object parameter)
 		{
-			var indexes = Items
+			var index = Items
 				.Select(x => x.IndexName)
-				.ToList();
+				.FirstOrDefault();
 
-			AskUser.ConfirmationAsync("Confirm Delete", indexes.Count > 1
-															? string.Format("Are you sure you want to delete these {0} indexes?", indexes.Count)
-															: string.Format("Are you sure that you want to delete this index? ({0})", indexes.First()))
-				.ContinueWhenTrue(() => DeleteIndex(indexes))
-				.ContinueWhenTrueInTheUIThread(() =>
+			AskUser.ConfirmationAsync("Confirm Delete", string.Format("Are you sure that you want to delete this index? ({0})", index))
+				.ContinueWhenTrue(() => DeleteIndex(index));
+		}
+
+		private void DeleteIndex(string indexName)
+		{
+			DatabaseCommands
+				.DeleteIndexAsync(indexName)
+				.ContinueOnSuccessInTheUIThread(() =>
 				{
-					var model = (IndexesModel)Context;
-					foreach (var index in Items)
-					{
-						IndexesModel.GroupedIndexes.Remove(index);
-					}
+					ApplicationModel.Current.AddNotification(new Notification("Index " + indexName + " successfully deleted"));
+					UrlUtil.Navigate("/indexes");
 				});
-		}
 
-		private void DeleteIndex(List<string> indexes)
-		{
-			var deleteCommandDatas = indexes
-			   .Select(id => new DeleteCommandData { Key = id })
-			   .Cast<ICommandData>()
-			   .ToArray();
-
-			DatabaseCommands.BatchAsync(deleteCommandDatas)
-				.ContinueOnSuccessInTheUIThread(() => DeleteIndexesSuccess(indexes));
-		}
-
-
-		private void DeleteIndexesSuccess(IList<string> indexes)
-		{
-			var notification = indexes.Count > 1
-								? string.Format("{0} index were deleted", indexes.Count)
-								: string.Format("Index {0} was deleted", indexes.First());
-			ApplicationModel.Current.AddNotification(new Notification(notification));
+			IndexesModel.GroupedIndexes.Remove(Items.First());
 		}
 	}
 }
