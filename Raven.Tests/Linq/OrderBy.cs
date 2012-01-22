c9cdd5b78c883315359d91d6bee97fdc150f956c
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-01-22 12:34:45 +0200

    Add a failing test which demonstrates an exception thrown when we OrderBy a projection. Something like: .Select(x => x.Position).OrderByDescending(x => x)

---
+++ Raven.Tests/Linq/OrderBy.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Raven.Tests.Linq
+{
+	public class OrderBy : LocalClientTest
+	{
+		public class Section
+		{
+			public string Id { get; set; }
+			public int Position { get; set; }
+			public string Name { get; set; }
+
+			public Section(int position)
+			{
+				Position = position;
+				Name = string.Format("Position: {0}", position);
+			}
+		}
+
+		[Fact]
+		public void CanOrderBy_AProjection()
+		{
+			using (var store = NewDocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					for (int i = 0; i < 10; i++)
+						session.Store(new Section(i));
+					session.SaveChanges();
+				}
+
+				using (var session = store.OpenSession())
+				{
+					var lastPosition = session.Query<Section>()
+						.Select(x => x.Position)
+						.OrderByDescending(x => x)
+						.FirstOrDefault();
+
+					Assert.Equal(9, lastPosition);
+				}
+			}
+		}
+	}
+}
