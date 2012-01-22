07726c528e93984753bea74cd610f3cd633e501d
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-01-22 16:33:19 +0200

    Store the URL of the request that is faulted.

---
+++ Raven.Client.Silverlight/Extensions/TaskExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Raven.Client.Silverlight.Extensions
+{
+	public static class TaskExtensions
+	{
+		public static Task<T> AddUrlIfFaulting<T>(this Task<T> parent, Uri uri)
+		{
+			return parent.ContinueWith(task =>
+			                           	{
+			                           		if (task.IsFaulted)
+			                           		{
+			                           			task.Exception.Data["Url"] = uri;
+			                           		}
+
+			                           		return task;
+			                           	})
+				.Unwrap();
+		}
+	}
+}
