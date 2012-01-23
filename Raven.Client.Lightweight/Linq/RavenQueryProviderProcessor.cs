9d46178309a2bd716633141f6e3f8005c71da183
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-01-23 13:57:34 +0200

    Fix typo

--- Raven.Client.Lightweight/Linq/RavenQueryProviderProcessor.cs
+++ Raven.Client.Lightweight/Linq/RavenQueryProviderProcessor.cs
@@ -442,7 +442,7 @@ namespace Raven.Client.Linq
 
 		private void VisitContains(MethodCallExpression _)
 		{
-			throw new NotSupportedException(@"Contains is not supported, doing a substring match over a text field is a vey slow operation, and is not allowed using the Linq API.
+			throw new NotSupportedException(@"Contains is not supported, doing a substring match over a text field is a very slow operation, and is not allowed using the Linq API.
 The recommended method is to use full text search (mark the field as Analyzed and use the Search() method to query it.");
 		}
 
