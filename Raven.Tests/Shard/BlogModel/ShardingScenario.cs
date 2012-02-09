222a015b692990fea78c4aeb2de4911fa8210aab
Ayende Rahien <ayende@ayende.com>
2012-02-09 20:40:52 +0200

    Better error handling (don't leak the listeners)

--- Raven.Tests/Shard/BlogModel/ShardingScenario.cs
+++ Raven.Tests/Shard/BlogModel/ShardingScenario.cs
@@ -12,33 +12,61 @@ namespace Raven.Tests.Shard.BlogModel
 {
 	public abstract class ShardingScenario : RavenTest, IDisposable
 	{
-		
 		protected readonly ShardedDocumentStore shardedDocumentStore;
 		protected readonly Dictionary<string, RavenDbServer> servers;
 
 		protected override void CreateDefaultIndexes(IDocumentStore documentStore)
 		{
 		}
-		
-		public ShardingScenario()
+
+		protected ShardingScenario()
 		{
+			RavenDbServer users = null;
+			RavenDbServer blogs = null;
+			RavenDbServer posts1 = null;
+			RavenDbServer posts2 = null;
+			RavenDbServer posts3 = null;
+			try
+			{
+				users = GetNewServer(8079, "shard1");
+				blogs = GetNewServer(8078, "shard2");
+				posts1 = GetNewServer(8077, "shard3");
+				posts2 = GetNewServer(8076, "shard4");
+				posts3 = GetNewServer(8075, "shard5");
+			}
+			catch (Exception)
+			{
+				if (users != null)
+					users.Dispose();
+				if (blogs != null)
+					blogs.Dispose();
+				if (posts1 != null)
+					posts1.Dispose();
+				if (posts2 != null)
+					posts2.Dispose();
+				if (posts3 != null)
+					posts3.Dispose();
+				throw;
+			}
+
 			servers = new Dictionary<string, RavenDbServer>
-			          {
-			          	{"Users", GetNewServer(8079, "shard1")},
-			          	{"Blogs", GetNewServer(8078, "shard2")},
-			          	{"Posts01", GetNewServer(8077, "shard3")},
-			          	{"Posts02", GetNewServer(8076, "shard4")},
-			          	{"Posts03", GetNewServer(8075, "shard5")}
-			          };
+                      {
+                        {"Users", users},
+                        {"Blogs", blogs},
+                        {"Posts01", posts1},
+                        {"Posts02", posts2},
+                        {"Posts03", posts3}
+                      };
 
 			var shards = new Shards
-			             {
-			             	new DocumentStore {Identifier = "Users", Url = "http://localhost:8079"}, 
-							new DocumentStore {Identifier = "Blogs", Url = "http://localhost:8078"}, 
-							new DocumentStore {Identifier = "Posts01", Url = "http://localhost:8077"}, 
-							new DocumentStore {Identifier = "Posts02", Url = "http://localhost:8076"}, 
-							new DocumentStore {Identifier = "Posts03", Url = "http://localhost:8075"}
-			             };
+                         {
+                            new DocumentStore {Identifier = "Users", Url = "http://localhost:8079"}, 
+                            new DocumentStore {Identifier = "Blogs", Url = "http://localhost:8078"}, 
+                            new DocumentStore {Identifier = "Posts01", Url = "http://localhost:8077"}, 
+                            new DocumentStore {Identifier = "Posts02", Url = "http://localhost:8076"}, 
+                            new DocumentStore {Identifier = "Posts03", Url = "http://localhost:8075"}
+                         };
+
 
 			foreach (var shard in shards)
 			{
@@ -46,11 +74,11 @@ namespace Raven.Tests.Shard.BlogModel
 			}
 
 			shardedDocumentStore = new ShardedDocumentStore(new ShardStrategy
-			                                                {
-			                                                	ShardAccessStrategy = new SequentialShardAccessStrategy(),
+															{
+																ShardAccessStrategy = new SequentialShardAccessStrategy(),
 																ShardSelectionStrategy = new BlogShardSelectionStrategy(3),
 																ShardResolutionStrategy = new BlogShardResolutionStrategy(3)
-			                                                }, shards);
+															}, shards);
 		}
 
 		public void Dispose()
