9270372f2168868a501eb9bfacf9e9ba1ae4ab4b
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-02-09 15:48:18 +0200

    Add test: CanQueryOnlyUsers.WhenQueryingForUserByName

--- Raven.Tests/Shard/BlogModel/CanQueryOnlyUsers.cs
+++ Raven.Tests/Shard/BlogModel/CanQueryOnlyUsers.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Linq;
+using Xunit;
 
 namespace Raven.Tests.Shard.BlogModel
 {
@@ -10,6 +11,25 @@ namespace Raven.Tests.Shard.BlogModel
 			using (var session = shardedDocumentStore.OpenSession())
 			{
 				var user = session.Load<User>("users/1");
+
+				// one request for the replication destination support
+				Assert.Equal(2, servers["Users"].Server.NumberOfRequests);
+				foreach (var ravenDbServer in servers)
+				{
+					if (ravenDbServer.Key == "Users")
+						continue;
+					Assert.Equal(1, ravenDbServer.Value.Server.NumberOfRequests);
+				}
+			}
+		}
+
+		[Fact]
+		public void WhenQueryingForUserByName()
+		{
+			using (var session = shardedDocumentStore.OpenSession())
+			{
+				var user = session.Query<User>()
+					.FirstOrDefault(x => x.Name == "Fitzchak");
 
 				// one request for the replication destination support
 				Assert.Equal(2, servers["Users"].Server.NumberOfRequests);
