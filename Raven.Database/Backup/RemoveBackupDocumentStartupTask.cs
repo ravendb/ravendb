059737c58472f7955c720799f6d40d8ed9239839
Ayende Rahien <ayende@ayende.com>
2012-02-10 10:33:41 +0200

    Don't delete the record of the last successful backup on startup

--- Raven.Database/Backup/RemoveBackupDocumentStartupTask.cs
+++ Raven.Database/Backup/RemoveBackupDocumentStartupTask.cs
@@ -3,18 +3,32 @@
 //     Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
+
+using System;
+using Raven.Abstractions.Data;
 using Raven.Database.Plugins;
+using Raven.Abstractions.Extensions;
 
 namespace Raven.Database.Backup
 {
 	/// <summary>
-	/// Delete the backup status document, since it may indicate a backup was in progress when the server crashed / shutdown
+	/// Delete the backup status document, if it indicate a backup was in progress when the server crashed / shutdown
+	/// we have to do that to enable the next backup to complete
 	/// </summary>
 	public class RemoveBackupDocumentStartupTask : IStartupTask
 	{
 		public void Execute(DocumentDatabase database)
 		{
-			database.Delete("Raven/Backup/Status", null, null);
+			var oldBackup = database.Get(BackupStatus.RavenBackupStatusDocumentKey,null);
+			if (oldBackup == null)
+				return;
+
+			var backupStatus = oldBackup.DataAsJson.JsonDeserialization<BackupStatus>();
+
+			if (backupStatus.Completed != null) // keep the record of the last successful backup
+				return;
+
+			database.Delete(BackupStatus.RavenBackupStatusDocumentKey, null, null);
 		}
 	}
 }
