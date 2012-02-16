bf234f6a5a27c975e94c6da4e87ac79ba741ca45
Ayende Rahien <ayende@ayende.com>
2012-02-16 22:40:07 +0200

    Adding tests

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Threading;
+using System.Xml;
+using NLog;
+using NLog.Config;
 using Raven.Abstractions.Indexing;
 using Raven.Client.Document;
 using Raven.Client.Indexes;
 using System.Linq;
+using Raven.Database.Server;
+using Raven.Tests.Storage.MultiThreaded;
 
 namespace Raven.Tryouts
 {
@@ -12,65 +18,43 @@ namespace Raven.Tryouts
 	{
 		private static void Main()
 		{
-			using (var store = new DocumentStore
+			if (Directory.Exists("Logs"))
 			{
-				Url = "http://localhost:8080"
-			}.Initialize())
-			{
-				store.DatabaseCommands.PutIndex("Disks/Search", new IndexDefinition
+				foreach (var file in Directory.GetFiles("Logs"))
 				{
-					Map =
-						@"
-from disk in docs.Disks 
-select new 
-{ 
-	Query = new[] { disk.Artist, disk.Title },
-	disk.Tracks,
-	DisId = disk.DiskIds
-}",
-					Indexes =
-						{
-							{"Query", FieldIndexing.Analyzed},
-							{"Tracks", FieldIndexing.Analyzed}
-						}
-				});
-
-
-
-				store.DatabaseCommands.PutIndex("Disks/Simple", new IndexDefinition
-								{
-									Map =
-										@"
-from disk in docs.Disks 
-select new 
-{ 
-	disk.Artist,
-	disk.Title
-}"
-								});
-
-				new RavenDocumentsByEntityName().Execute(store);
-
-				var sp = Stopwatch.StartNew();
-				while (true)
-				{
-					var statistics = store.DatabaseCommands.GetStatistics();
-					if (statistics.StaleIndexes.Length == 0)
-						break;
-
-					Console.Clear();
-					foreach (var stat in statistics.Indexes.Where(x => statistics.StaleIndexes.Contains(x.Name)))
-					{
-						Console.WriteLine("{0}: {1:#,#}  ", stat.Name, stat.IndexingAttempts);
-					}
-
-					Console.WriteLine("{0:#,#}",statistics.CurrentNumberOfItemsToIndexInSingleBatch);
-					Console.Write(sp.Elapsed);
-					Thread.Sleep(2500);
+					File.Delete(file);
 				}
-
-				Console.WriteLine(sp.Elapsed);
 			}
+			SetupLogging();
+
+			try
+			{
+				for (int i = 0; i < 1000; i++)
+				{
+					Environment.SetEnvironmentVariable("Run", i.ToString());
+					Console.Clear();
+					Console.WriteLine(i);
+					using (var x = new PutAndBatchOperation())
+						x.WhenUsingMuninInMemory();
+				}
+			}
+			finally
+			{
+				LogManager.Flush();
+			}
+		}
+
+
+		private static void SetupLogging()
+		{
+			HttpEndpointRegistration.RegisterHttpEndpointTarget();
+
+			using (var stream = typeof(Program).Assembly.GetManifestResourceStream("Raven.Tryouts.DefaultLogging.config"))
+			using (var reader = XmlReader.Create(stream))
+			{
+				LogManager.Configuration = new XmlLoggingConfiguration(reader, "default-config");
+			}
+
 		}
 	}
 }
