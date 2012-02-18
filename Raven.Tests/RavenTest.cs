e59098ea75978c6eb2f9396a3b937d3b6dcd928f
Ayende Rahien <ayende@ayende.com>
2012-02-18 02:12:14 +0200

    Fix a case where an esent run would still try to use in memory

--- Raven.Tests/RavenTest.cs
+++ Raven.Tests/RavenTest.cs
@@ -46,7 +46,7 @@ namespace Raven.Tests
 						DataDirectory = path,
 						RunInUnreliableYetFastModeThatIsNotSuitableForProduction = true,
 						DefaultStorageTypeName = storageType,
-						RunInMemory = inMemory,
+						RunInMemory = storageType == "munin" && inMemory,
 					}
 			};
 
