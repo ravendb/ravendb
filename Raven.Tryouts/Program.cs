f700d43a582366795bc205df16f9f18759353810
Ayende Rahien <ayende@ayende.com>
2012-02-20 19:42:58 +0200

    Fixing race condition in test

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -5,6 +5,7 @@ using NLog;
 using NLog.Config;
 using Raven.Database.Server;
 using Raven.Tests.MultiGet;
+using Raven.Tests.Shard.BlogModel;
 using Raven.Tests.Storage.MultiThreaded;
 
 namespace Raven.Tryouts
@@ -29,8 +30,8 @@ namespace Raven.Tryouts
 					Environment.SetEnvironmentVariable("Run", i.ToString());
 					Console.Clear();
 					Console.WriteLine(i);
-					var x = new MultiGetNonStaleRequslts();
-						x.ShouldBeAbleToGetNonStaleResults();
+					using(var x = new CanQueryOnlyUsers())
+						x.WhenQueryingForUserById();
 				}
 			}
 			finally
