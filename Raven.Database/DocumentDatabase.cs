96b980277aaddaa1dc90f92cf9fe5b1e7c6527cb
Ayende Rahien <ayende@ayende.com>
2012-02-21 12:24:23 +0200

    Removing a hack

--- Raven.Database/DocumentDatabase.cs
+++ Raven.Database/DocumentDatabase.cs
@@ -747,8 +747,6 @@ namespace Raven.Database
 
 					stale = actions.Staleness.IsIndexStale(index, query.Cutoff, query.CutoffEtag);
 
-					AskForIndexUpdateIfStale(stale, index);
-
 					indexTimestamp = actions.Staleness.IndexLastUpdatedAt(index);
 					var indexFailureInformation = actions.Indexing.GetFailureRate(index);
 					if (indexFailureInformation.IsInvalidIndex)
@@ -810,19 +808,6 @@ namespace Raven.Database
 				IndexTimestamp = indexTimestamp.Item1,
 				IndexEtag = indexTimestamp.Item2
 			};
-		}
-
-		private void AskForIndexUpdateIfStale(bool stale, string index)
-		{
-			if (stale == false) 
-				return;
-
-			// This is a sort of a hack
-			// Using it in this manner ensures that even if we somehow lost an update, we would still
-			// kick up indexing, anyway, because the querying for this would trigger that.
-			// This doesn't mean that we don't have a problem with losing updates, mind, just that we have
-			// no way to reproduce this.
-			workContext.ShouldNotifyAboutWork(() => "QUERY ON STALE INDEX: " + index);
 		}
 
 		public IEnumerable<string> QueryDocumentIds(string index, IndexQuery query, out bool stale)
