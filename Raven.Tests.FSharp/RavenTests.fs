9886c187cd5ac0147eeb9e9682ea81012abe58bc
Colin Bull <colinbul@googlemail.com>
2012-02-25 18:07:16 +0000

    moved tests inline for rest of Raven solution

--- Raven.Tests.FSharp/RavenTests.fs
+++ Raven.Tests.FSharp/RavenTests.fs
@@ -18,6 +18,8 @@ type ``Given a Initailised Document store execute using computation expression``
     
     [<Fact>]
     member test.``Should be able to save and retrieve an entity``() =
+           let ds = test.NewDocumentStore()
+           initialize "raven" ds
            let exp, act =
                 raven {
                          let! expected = storeImmediate (Customer.Create("test", new DateTime(2012, 1, 1)))
@@ -25,9 +27,13 @@ type ``Given a Initailised Document store execute using computation expression``
                          return expected, actual
                       } |> run "raven"
            act |> should equal exp
+           shutDownAll()
+
 
     [<Fact>]
     member test.``Should be able to save and retrieve batches of entites``() =
+           let ds = test.NewDocumentStore()
+           initialize "raven" ds
            let exp, act =
                 raven {
                          let testData = createCustomers 60
@@ -36,22 +42,28 @@ type ``Given a Initailised Document store execute using computation expression``
                          return expected, actual
                       } |> run "raven"
            act |> should equal exp
+           shutDownAll()
 
     [<Fact>]
     member test.``Should be able to query for filtered all entites``() = 
-        let actual : seq<Customer> = 
+        let ds = test.NewDocumentStore()
+        initialize "raven" ds
+        let actual = 
                    raven {
                             do! storeMany (createCustomers 365) >> ignore
                             let! actual = queryAllBy (where <@ fun x -> x.Dob < new DateTime(2012,8,1) @>)
                             return actual
-                         } |> run "raven"
+                         } |> run "raven" |> Seq.toList
 
         let expected = createCustomers ((new DateTime(2012,8,1)).Subtract(new DateTime(2012,1,1)).Days)
 
         actual |> should equal expected
+        shutDownAll()
     
     [<Fact>]
     member test.``Should be able to query an entity including a reference``() =
+        let ds = test.NewDocumentStore()
+        initialize "raven" ds
         let customer =  Customer.Create("Test", (new DateTime(2012, 1, 1)))
         let products = [| Product.Create("1-23-4", "Cat Food", 2.30) |]
         let order = Order.Create(customer.Id, products)
@@ -70,23 +82,29 @@ type ``Given a Initailised Document store execute using computation expression``
 
         fst(retrieved) |> should equal order
         snd(retrieved) |> should equal customer
+        shutDownAll()
 
     [<Fact>]
     member test.``Should be able to query for filtered batches of entites``() = 
+        let ds = test.NewDocumentStore()
+        initialize "raven" ds
         let testData = Seq.init 60 (fun i -> Customer.Create("test_"+i.ToString(), (new DateTime(2012, 1, 1)).AddDays(i |> float))) |> List.ofSeq
         
         let actual = 
             raven { 
                 let! a = storeMany testData
                 return! query (where <@ fun x -> x.Dob < new DateTime(2012,2,1) @>)
-            } |> run "raven"
+            } |> run "raven" |> Seq.toList
 
-        let expected = Seq.init 31 (fun i -> Customer.Create("test_"+i.ToString(), (new DateTime(2012, 1, 1)).AddDays(i |> float)))
+        let expected = Seq.init 31 (fun i -> Customer.Create("test_"+i.ToString(), (new DateTime(2012, 1, 1)).AddDays(i |> float))) |> Seq.toList
 
         actual |> should equal expected
+        shutDownAll()
 
     [<Fact>]
     member test.``Should be able to compose queries``() =
+        let ds = test.NewDocumentStore()
+        initialize "raven" ds
         let customer =  Customer.Create("Test", (new DateTime(2012, 1, 1)))
         let products = [| Product.Create("1-23-4", "Cat Food", 2.30) |]
         let order = Order.Create(customer.Id, products)
@@ -115,112 +133,21 @@ type ``Given a Initailised Document store execute using computation expression``
 
         fst(retrieved) |> should equal order
         snd(retrieved) |> should equal customer
+        shutDownAll()
 
     [<Fact>]
     member test.``Should be able to query via a lucene query``() = 
+        let ds = test.NewDocumentStore()
+        initialize "raven" ds
         let testData = Seq.init 60 (fun i -> Customer.Create("test_"+i.ToString(), (new DateTime(2012, 1, 1)).AddDays(i |> float))) |> List.ofSeq
         
-        let actual : seq<Customer> = 
+        let actual = 
             raven { 
                 let! a = storeMany testData
                 return! luceneQuery (fun docQuery -> docQuery.WhereLessThan("Dob", new DateTime(2012,2,1)))
-            } |> run "raven"
+            } |> run "raven" |> Seq.toList
 
-        let expected = Seq.init 31 (fun i -> Customer.Create("test_"+i.ToString(), (new DateTime(2012, 1, 1)).AddDays(i |> float)))
+        let expected = Seq.init 31 (fun i -> Customer.Create("test_"+i.ToString(), (new DateTime(2012, 1, 1)).AddDays(i |> float))) |> Seq.toList
 
-        actual |> should equal expected
-
-type ``Given a Initailised Document store execute using combinators``() =
-    inherit RavenTest()
-    
-    [<Fact>]
-    member test.``Should be able to save and retrieve an entity``() =
-           let expected = Customer.Create("test", new DateTime(2012, 1, 1))
-           run "raven" (storeImmediate expected) |> ignore
-           let actual : Customer = run "raven" (fun s -> s.Load("customers/test"))
-           actual |> should equal expected
-
-
-    [<Fact>]
-    member test.``Should be able to save and retrieve batches of entites``() = 
-          let expected = 
-            Seq.init 60 (fun i -> Customer.Create("test_"+i.ToString(), (new DateTime(2012, 1, 1)).AddDays(i |> float)))
-            |> List.ofSeq
-          (storeMany expected) |> run "raven" |> ignore
-          let actual : Customer list = run "raven" queryAll |> List.ofSeq
-          actual |> should equal expected
-
-    [<Fact>]
-    member test.``Should be able to query for filtered all entites``() = 
-        let testData = Seq.init 365 (fun i -> Customer.Create("test_"+i.ToString(), (new DateTime(2012, 1, 1)).AddDays(i |> float))) |> List.ofSeq
-        (storeMany testData) |> run "raven" |> ignore 
-        let actual : seq<Customer> = queryAllBy (where <@ fun x -> x.Dob < new DateTime(2012,8,1) @>) |> run "raven"
-        let total = (new DateTime(2012,8,1)).Subtract(new DateTime(2012,1,1)).Days
-        let expected = Seq.init total (fun i -> Customer.Create("test_"+i.ToString(), (new DateTime(2012, 1, 1)).AddDays(i |> float)))
-        actual |> should equal expected
-    
-    [<Fact>]
-    member test.``Should be able to query an entity including a reference``() = 
-        let customer =  Customer.Create("Test", (new DateTime(2012, 1, 1)))
-        let products = [| Product.Create("1-23-4", "Cat Food", 2.30) |]
-        let order = Order.Create(customer.Id, products)
-        (storeImmediate customer) |> run "raven" |> ignore
-        (storeImmediate order) |> run "raven" |> ignore
-        let retrieved : Order * Customer = 
-            run "raven" (fun s -> 
-                                        let order = ``include`` <@ fun s -> s.Customer @> (fun s -> s.Load("orders/1")) s
-                                        let customer : Customer = s.Load(order.Customer)
-                                        order, customer
-                                 )
-        fst(retrieved) |> should equal order
-        snd(retrieved) |> should equal customer
-
-
-
-    [<Fact>]
-    member test.``Should be able to query for filtered batches of entites``() = 
-        let testData = Seq.init 60 (fun i -> Customer.Create("test_"+i.ToString(), (new DateTime(2012, 1, 1)).AddDays(i |> float))) |> List.ofSeq
-        run "raven" (storeMany testData) |> ignore
-        let actual : seq<Customer> =  query (where <@ fun x -> x.Dob < new DateTime(2012,2,1) @>) |> run "raven"
-        let expected = Seq.init 31 (fun i -> Customer.Create("test_"+i.ToString(), (new DateTime(2012, 1, 1)).AddDays(i |> float)))
-        actual |> should equal expected
-
-
-    [<Fact>]
-    member test.``Should be able to put an attachment to Raven``() = 
-        let customer = run "raven" (storeImmediate (Customer.Create("test", new DateTime(2012, 1, 1))))
-        let attachmentStream = new IO.MemoryStream(Seq.init 100 (fun x -> 1uy) |> Seq.toArray)
-        run "raven" (createAttachment customer.Id attachmentStream {Description = "description"})
-
-
-    [<Fact>]
-    member test.``Should be able to retrieve an attachment from raven as bytes``() =
-        let customer = run "raven" (storeImmediate (Customer.Create("test", new DateTime(2012, 1, 1))))
-        let attachmentStream = new IO.MemoryStream(Seq.init 100 (fun x -> 1uy) |> Seq.toArray)
-        run "raven" (createAttachment customer.Id attachmentStream {Description = "description"})
-
-        let metaData, etag, body = run "raven" (getAttachmentAsBytes<CustomerAttachmentMetaData> customer.Id)
-        
-        let expectedbody = Seq.init 100 (fun x -> 1uy) |> Seq.toArray
-        let expectedMetaData = {Description = "description"}
-
-        metaData |> should equal expectedMetaData
-        body |> should equal expectedbody
-
-    [<Fact>]
-    member test.``Should be able to retrieve an attachment from raven as stream``() =
-        let customer = run "raven" (storeImmediate (Customer.Create("test", new DateTime(2012, 1, 1))))
-        let attachmentStream = new IO.MemoryStream(Seq.init 100 (fun x -> 1uy) |> Seq.toArray)
-        run "raven" (createAttachment customer.Id attachmentStream {Description = "description"})
-
-        let ms = new IO.MemoryStream()
-        let metaData, etag = run "raven" (getAttachmentAsStream<CustomerAttachmentMetaData> customer.Id ms)
-        
-        let expectedbody = Seq.init 100 (fun x -> 1uy) |> Seq.toArray
-        let expectedMetaData = {Description = "description"}
-
-        metaData |> should equal expectedMetaData
-        ms.ToArray() |> should equal expectedbody
-      
-
-        
+        Assert.Equal(expected, actual)
+        shutDownAll()
