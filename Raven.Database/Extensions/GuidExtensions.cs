c2474f6f31724e1f353c3300042633ba672163de
etobi <git@e-tobi.net>
2012-02-25 21:43:33 +0100

    Removed unused variables and fields

--- Raven.Database/Extensions/GuidExtensions.cs
+++ Raven.Database/Extensions/GuidExtensions.cs
@@ -9,8 +9,6 @@ namespace Raven.Database.Extensions
 {
 	public static class GuidExtensions
 	{
-		private static readonly int[] normalizations = new int[] { 10, 11, 12, 13, 14, 15, 8, 9, 6, 7, 4, 5, 0, 1, 2, 3 };
-
 		public static Guid TransfromToGuidWithProperSorting(this byte[] bytes)
 		{
 			return new Guid(bytes);
