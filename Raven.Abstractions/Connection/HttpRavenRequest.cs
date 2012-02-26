591165bd3ee492fc81c4df5a48e99b5c6ffd870f
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-02-26 10:50:53 +0200

    Separate class to a separate file

--- Raven.Abstractions/Connection/HttpRavenRequest.cs
+++ Raven.Abstractions/Connection/HttpRavenRequest.cs
@@ -9,58 +9,6 @@ using Raven.Abstractions.Extensions;
 
 namespace Raven.Abstractions.Connection
 {
-	public class HttpRavenRequestFactory
-	{
-		public int? RequestTimeoutInMs { get; set; }
-
-		private bool RefreshOauthToken(RavenConnectionStringOptions options, WebResponse response)
-		{
-			var oauthSource = response.Headers["OAuth-Source"];
-			if (string.IsNullOrEmpty(oauthSource))
-				return false;
-
-			var authRequest = PrepareOAuthRequest(options, oauthSource);
-			using (var authResponse = authRequest.GetResponse())
-			using (var stream = authResponse.GetResponseStreamWithHttpDecompression())
-			using (var reader = new StreamReader(stream))
-			{
-				options.CurrentOAuthToken = "Bearer " + reader.ReadToEnd();
-			}
-			return true;
-		}
-
-		private HttpWebRequest PrepareOAuthRequest(RavenConnectionStringOptions options, string oauthSource)
-		{
-			var authRequest = (HttpWebRequest) System.Net.WebRequest.Create(oauthSource);
-			authRequest.Credentials = options.Credentials;
-			authRequest.Headers["Accept-Encoding"] = "deflate,gzip";
-			authRequest.Accept = "application/json;charset=UTF-8";
-
-			authRequest.Headers["grant_type"] = "client_credentials";
-
-			if (string.IsNullOrEmpty(options.ApiKey) == false)
-				authRequest.Headers["Api-Key"] = options.ApiKey;
-
-			return authRequest;
-		}
-
-		public void ConfigureRequest(RavenConnectionStringOptions options, WebRequest request)
-		{
-			request.Credentials = options.Credentials ?? CredentialCache.DefaultNetworkCredentials;
-
-			if (RequestTimeoutInMs.HasValue)
-				request.Timeout = RequestTimeoutInMs.Value;
-			
-			if (string.IsNullOrEmpty(options.CurrentOAuthToken) == false)
-				request.Headers["Authorization"] = options.CurrentOAuthToken;
-		}
-
-		public HttpRavenRequest Create(string url, string method, RavenConnectionStringOptions connectionStringOptions)
-		{
-			return new HttpRavenRequest(url, method, ConfigureRequest, RefreshOauthToken, connectionStringOptions);
-		}
-	}
-
 	public class HttpRavenRequest
 	{
 		private readonly string url;
