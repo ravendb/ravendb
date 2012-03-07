9df9919fa4d2f6cbd7cd220e1882a8ca331539d5
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-03-07 11:05:59 +0200

    Add some tests, 3 of them are failing:
    LazyLoadOperationWillHandleIncludes : FailedXunit.Sdk.TrueException: Assert.True() Failure
    
    LazyLoadShouldReturnArrayWithNullItems : FailedXunit.Sdk.EqualException: Assert.Equal() Failure
    Position: First difference is at position 0
    Expected: User[] { (null), (null) }
    Actual:   User[] {  }
    
    WithLazyQuery : FailedSystem.NullReferenceException: Object reference not set to an instance of an object.
    at Raven.Client.Document.AbstractDocumentQuery`2.Lazily(Action`1 onEval) in AbstractDocumentQuery.cs: line 459
    at Raven.Client.Linq.RavenQueryProvider`1.Lazily(Expression expression, Action`1 onEval) in RavenQueryProvider.cs: line 244
    at Raven.Client.Linq.LinqExtensions.Lazily(IQueryable`1 source, Action`1 onEval) in LinqExtensions.cs: line 169
    at Raven.Client.Linq.LinqExtensions.Lazily(IQueryable`1 source) in LinqExtensions.cs: line 155
    at Raven.Tests.Shard.BlogModel.SupportLazyOperations.WithLazyQuery() in SupportLazyOperations.cs: line 44

--- Raven.Tests/Shard/BlogModel/SupportLazyOperations.cs
+++ Raven.Tests/Shard/BlogModel/SupportLazyOperations.cs
@@ -10,22 +10,32 @@ namespace Raven.Tests.Shard.BlogModel
 	public class SupportLazyOperations : ShardingScenario
 	{
 		[Fact]
+		public void LazyLoadShouldReturnArrayWithNullItems()
+		{
+			using (var session = ShardedDocumentStore.OpenSession())
+			{
+				var users = session.Advanced.Lazily.Load<User>("users/1", "users/2");
+				Assert.Equal(new User[2], users.Value);
+			}
+		}
+
+		[Fact]
 		public void WithLazyQuery()
 		{
 			using (var session = ShardedDocumentStore.OpenSession())
 			{
-				for (int i = 0; i < 3; i++)
-					session.Store(new Post {Id = "posts/" + i, Title = "Item " + i});
+				session.Store(new User {Id = "users/1", Name = "Yosef Yitzchak Yitzchaki"});
+				session.Store(new User {Id = "users/2", Name = "Fitzchak Yitzchaki"});
 				session.SaveChanges();
 			}
 
-			Servers.Where(server => server.Key.StartsWith("Posts"))
+			Servers.Where(server => server.Key == "Users")
 				.ForEach(server =>
-				{
-					Assert.Equal(1, server.Value.Server.NumberOfRequests);
-					Assert.Equal(1, server.Value.Database.Statistics.CountOfDocuments);
-				});
-			Servers.Where(server => server.Key.StartsWith("Posts") == false)
+				         	{
+				         		Assert.Equal(1, server.Value.Server.NumberOfRequests);
+				         		Assert.Equal(2, server.Value.Database.Statistics.CountOfDocuments);
+				         	});
+			Servers.Where(server => server.Key != "Users")
 				.ForEach(server => Assert.Equal(0, server.Value.Server.NumberOfRequests));
 
 			Lazy<IEnumerable<Post>> lazily;
@@ -33,16 +43,107 @@ namespace Raven.Tests.Shard.BlogModel
 			{
 				lazily = session.Query<Post>().Lazily();
 			}
-			Servers.Where(server => server.Key.StartsWith("Posts"))
-				.ForEach(server => Assert.Equal(1, server.Value.Server.NumberOfRequests));
-			Servers.Where(server => server.Key.StartsWith("Posts") == false)
+			Assert.Equal(1, Servers["Users"].Server.NumberOfRequests);
+			Servers.Where(server => server.Key != "Users")
 				.ForEach(server => Assert.Equal(0, server.Value.Server.NumberOfRequests));
 
-			var posts = lazily.Value.ToList();
-			Servers.Where(server => server.Key.StartsWith("Posts"))
-				.ForEach(server => Assert.Equal(2, server.Value.Server.NumberOfRequests));
-			Servers.Where(server => server.Key.StartsWith("Posts") == false)
+			Assert.Equal(2, lazily.Value.Count());
+			Assert.Equal(2, Servers["Users"].Server.NumberOfRequests);
+			Servers.Where(server => server.Key != "Users")
 				.ForEach(server => Assert.Equal(0, server.Value.Server.NumberOfRequests));
+		}
+
+		[Fact]
+		public void UnlessAccessedLazyOpertionsAreNoOp()
+		{
+			using (var session = ShardedDocumentStore.OpenSession())
+			{
+				var result1 = session.Advanced.Lazily.Load<User>("users/1", "users/2");
+				var result2 = session.Advanced.Lazily.Load<User>("users/3", "users/4");
+			}
+			Servers.ForEach(server => Assert.Equal(0, server.Value.Server.NumberOfRequests));
+		}
+
+		[Fact]
+		public void LazyOperationsAreBatched()
+		{
+			using (var session = ShardedDocumentStore.OpenSession())
+			{
+				var result1 = session.Advanced.Lazily.Load<User>("users/1", "users/2");
+				var result2 = session.Advanced.Lazily.Load<User>("users/3", "users/4");
+
+				Assert.Equal(new User[0], result2.Value);
+				Assert.Equal(1, Servers["Users"].Server.NumberOfRequests);
+				Servers.Where(server => server.Key != "Users")
+					.ForEach(server => Assert.Equal(0, server.Value.Server.NumberOfRequests));
+
+				Assert.Equal(new User[0], result1.Value);
+				Assert.Equal(1, Servers["Users"].Server.NumberOfRequests);
+				Servers.Where(server => server.Key != "Users")
+					.ForEach(server => Assert.Equal(0, server.Value.Server.NumberOfRequests));
+			}
+		}
+
+		[Fact]
+		public void LazyMultiLoadOperationWouldBeInTheSession()
+		{
+			using (var session = ShardedDocumentStore.OpenSession())
+			{
+				for (int i = 1; i <= 4; i++)
+					session.Store(new User{Id="users/" + i, Name = "User " + i});
+				session.SaveChanges();
+			}
+			Assert.Equal(1, Servers["Users"].Server.NumberOfRequests);
+			Servers.Where(server => server.Key != "Users")
+				.ForEach(server => Assert.Equal(0, server.Value.Server.NumberOfRequests));
+
+			using (var session = ShardedDocumentStore.OpenSession())
+			{
+				var result1 = session.Advanced.Lazily.Load<User>("users/1", "users/2");
+				var result2 = session.Advanced.Lazily.Load<User>("users/3", "users/4");
+				
+				var a = result1.Value;
+				Assert.Equal(2, a.Length);
+				Assert.Equal(2, Servers["Users"].Server.NumberOfRequests);
+				Servers.Where(server => server.Key != "Users")
+					.ForEach(server => Assert.Equal(0, server.Value.Server.NumberOfRequests));
+
+				var b = result2.Value;
+				Assert.Equal(2, b.Length);
+				Assert.Equal(2, Servers["Users"].Server.NumberOfRequests);
+				Servers.Where(server => server.Key != "Users")
+					.ForEach(server => Assert.Equal(0, server.Value.Server.NumberOfRequests));
+
+				foreach (var user in b.Concat(a))
+				{
+					Assert.NotNull(session.Advanced.GetMetadataFor(user));
+				}
+			}
+		}
+
+		[Fact]
+		public void LazyLoadOperationWillHandleIncludes()
+		{
+			using (var session = ShardedDocumentStore.OpenSession())
+			{
+				for (int i = 1; i <= 4; i++)
+					session.Store(new User { Id = "users/" + i, Name = "User " + i });
+				session.SaveChanges();
+			}
+			using (var session = ShardedDocumentStore.OpenSession())
+			{
+				var result1 = session.Advanced.Lazily
+					.Include("Name")
+					.Load<User>("users/1");
+				var result2 = session.Advanced.Lazily
+					.Include("Name")
+					.Load<User>("users/3");
+
+				Assert.NotNull(result1.Value);
+				Assert.NotNull(result2.Value);
+				Assert.True(session.Advanced.IsLoaded("users/2"));
+				Assert.True(session.Advanced.IsLoaded("users/4"));
+			}
 		}
 	}
 }
