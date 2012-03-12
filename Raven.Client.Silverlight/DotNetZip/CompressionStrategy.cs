7201a8b12ea64531ba877d08ba44a140d3f57147
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-03-12 12:47:59 +0200

    Revert "Use the DotNetZip dll instead on inline its code"
    This reverts commit 705919d1352dce56ec8edb58a4d187b7a1ac881e.
    
    Conflicts:
    
    	Raven.Client.Silverlight/Raven.Client.Silverlight.csproj
    	Raven.Studio/Raven.Studio.csproj

---
+++ Raven.Client.Silverlight/DotNetZip/CompressionStrategy.cs
@@ -0,0 +1,30 @@
+﻿namespace Ionic.Zlib
+{
+	/// <summary>
+	/// Describes options for how the compression algorithm is executed.  Different strategies
+	/// work better on different sorts of data.  The strategy parameter can affect the compression
+	/// ratio and the speed of compression but not the correctness of the compresssion.
+	/// </summary>
+	public enum CompressionStrategy
+	{
+		/// <summary>
+		/// The default strategy is probably the best for normal data. 
+		/// </summary>
+		Default = 0,
+
+		/// <summary>
+		/// The <c>Filtered</c> strategy is intended to be used most effectively with data produced by a
+		/// filter or predictor.  By this definition, filtered data consists mostly of small
+		/// values with a somewhat random distribution.  In this case, the compression algorithm
+		/// is tuned to compress them better.  The effect of <c>Filtered</c> is to force more Huffman
+		/// coding and less string matching; it is a half-step between <c>Default</c> and <c>HuffmanOnly</c>.
+		/// </summary>
+		Filtered = 1,
+
+		/// <summary>
+		/// Using <c>HuffmanOnly</c> will force the compressor to do Huffman encoding only, with no
+		/// string matching.
+		/// </summary>
+		HuffmanOnly = 2,
+	}
+}
---
+++ Raven.Client.Silverlight/DotNetZip/DeflateFlavor.cs
@@ -0,0 +1,9 @@
+﻿namespace Ionic.Zlib
+{
+	internal enum DeflateFlavor
+	{
+		Store,
+		Fast,
+		Slow
+	}
+}
