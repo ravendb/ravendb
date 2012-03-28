407e84d3c86e4995d18bf83f20ff48a9d16dc8c5
Ayende Rahien <ayende@ayende.com>
2012-03-28 19:54:27 +0200

    Adding more sample

---
+++ Samples/Raven.Sample.ReplicationComplex/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Raven.Client.Document;
+
+namespace Raven.Sample.ReplicationComplex
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using(var store = new DocumentStore
+			{
+				Url = "http://localhost:8079",
+				Conventions =
+					{
+						FailoverBehavior = FailoverBehavior.ReadFromAllServers
+					}
+				
+			}.Initialize())
+			{
+				using (var session = store.OpenSession())
+				{
+					session.Store(new User { Name = "Ayende" }, "users/ayende");
+					session.SaveChanges();
+				}
+
+				Console.WriteLine("Waiting");
+				Console.ReadLine();
+
+				for (int i = 0; i < 12; i++)
+				{
+					using (var session = store.OpenSession())
+					{
+						session.Load<User>("users/ayende");
+					}
+				}
+			}
+		}
+
+		
+	}
+
+	public class User
+	{
+		public string Name { get; set; }
+	}
+}
