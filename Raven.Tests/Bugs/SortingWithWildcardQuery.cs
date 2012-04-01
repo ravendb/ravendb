5339dbecff06fad30a525eef4ac7437fcd6eb913
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-04-01 16:09:26 +0300

    Fix not implementing IDisposable for a class with dispose method

--- Raven.Tests/Bugs/SortingWithWildcardQuery.cs
+++ Raven.Tests/Bugs/SortingWithWildcardQuery.cs
@@ -3,6 +3,7 @@
 //     Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
+using System;
 using System.Linq;
 using Raven.Abstractions.Indexing;
 using Raven.Client;
@@ -15,7 +16,7 @@ using Xunit;
 
 namespace Raven.Tests.Bugs
 {
-	public class SortingWithWildcardQuery : RemoteClientTest
+	public class SortingWithWildcardQuery : RemoteClientTest, IDisposable
 	{
 		private readonly string path;
 		private readonly RavenDbServer ravenDbServer;
