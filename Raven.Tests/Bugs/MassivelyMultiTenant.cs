db01da3588fc2011db35be76267737e6c6a55903
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-04-02 13:50:51 +0300

    Cleanup test output

--- Raven.Tests/Bugs/MassivelyMultiTenant.cs
+++ Raven.Tests/Bugs/MassivelyMultiTenant.cs
@@ -1,30 +1,45 @@
+using System;
 using Raven.Client.Document;
 using Raven.Client.Extensions;
+using Raven.Database.Extensions;
+using Raven.Server;
 using Xunit;
 
 namespace Raven.Tests.Bugs
 {
-	public class MassivelyMultiTenant : RemoteClientTest
+	public class MassivelyMultiTenant : RemoteClientTest, IDisposable
 	{
+		private readonly string path;
+		private readonly RavenDbServer ravenDbServer;
+
 		protected override void ModifyConfiguration(Database.Config.RavenConfiguration ravenConfiguration)
 		{
 			ravenConfiguration.DefaultStorageTypeName = "esent";
 		}
 
+		public MassivelyMultiTenant()
+		{
+			path = GetPath("MassivelyMultiTenant");
+			ravenDbServer = GetNewServer(dataDirectory: path);
+		}
+
 		[Fact]
 		public void CanHaveLotsOf_ACTIVE_Tenants()
 		{
-			  using(GetNewServer())
-			  {
-				  for (int i = 0; i < 20; i++)
-				  {
-					  var databaseName = "Tenants" + i;
-					  using (var documentStore = new DocumentStore { Url = "http://localhost:8079", DefaultDatabase = databaseName }.Initialize())
-					  {
-					  	documentStore.DatabaseCommands.EnsureDatabaseExists(databaseName);
-					  }
-				  }
-			  }
+			for (int i = 0; i < 20; i++)
+			{
+				var databaseName = "Tenants" + i;
+				using (var documentStore = new DocumentStore {Url = "http://localhost:8079", DefaultDatabase = databaseName}.Initialize())
+				{
+					documentStore.DatabaseCommands.EnsureDatabaseExists(databaseName);
+				}
+			}
+		}
+
+		public void Dispose()
+		{
+			ravenDbServer.Dispose();
+			IOExtensions.DeleteDirectory(path);
 		}
 	}
 }
