03ad404b901e50cfb41f342824038a9c8064ad8e
Ayende Rahien <ayende@ayende.com>
2012-04-02 08:11:49 +0300

    Mostly minor stuff

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -1,12 +1,10 @@
-﻿using Raven.StressTests.Races;
-
+﻿
 namespace Raven.Tryouts
 {
 	class Program
 	{
 		private static void Main()
 		{
-			new BundelsRaceConditions().FailoverBetweenTwoMultiTenantDatabases_CanReplicateBetweenTwoMultiTenantDatabases();
 		}
 	}
 }
