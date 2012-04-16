8c23c632b3e96daa7de9aef678aa25e5ce1caef1
fschwiet <fschwiet@gmail.com>
2012-04-15 19:08:23 -0700

    MLT parameters can accept MapReduce grouping keys

--- Bundles/Raven.Bundles.MoreLikeThis/MoreLikeThisQueryParameters.cs
+++ Bundles/Raven.Bundles.MoreLikeThis/MoreLikeThisQueryParameters.cs
@@ -9,55 +9,60 @@ namespace Raven.Client.MoreLikeThis
 namespace Raven.Bundles.MoreLikeThis
 #endif
 {
-    public class MoreLikeThisQueryParameters
-    {
-        public const int DefaultMaximumNumberOfTokensParsed = 5000;
-        public const int DefaultMinimumTermFrequency = 2;
-        public const int DefaltMinimumDocumentFrequency = 5;
-        public const bool DefaultBoost = false;
-        public const int DefaultMinimumWordLength = 0;
-        public const int DefaultMaximumWordLength = 0;
-        public const int DefaultMaximumQueryTerms = 25;
+	public class MoreLikeThisQueryParameters
+	{
+		public MoreLikeThisQueryParameters()
+		{
+			MapGroupFields = new StringDictionary();
+		}
 
-        /// <summary>
-        /// Ignore terms with less than this frequency in the source doc. Default is 2.
-        /// </summary>
-        public int? MinimumTermFrequency { get; set; }
+		public const int DefaultMaximumNumberOfTokensParsed = 5000;
+		public const int DefaultMinimumTermFrequency = 2;
+		public const int DefaltMinimumDocumentFrequency = 5;
+		public const bool DefaultBoost = false;
+		public const int DefaultMinimumWordLength = 0;
+		public const int DefaultMaximumWordLength = 0;
+		public const int DefaultMaximumQueryTerms = 25;
 
-        /// <summary>
-        /// Ignore words which do not occur in at least this many documents. Default is 5.
-        /// </summary>
-        public int? MinimumDocumentFrequency { get; set; }
+		/// <summary>
+		/// Ignore terms with less than this frequency in the source doc. Default is 2.
+		/// </summary>
+		public int? MinimumTermFrequency { get; set; }
 
-        /// <summary>
-        /// Boost terms in query based on score. Default is false.
-        /// </summary>
-        public bool? Boost { get; set; }
+		/// <summary>
+		/// Ignore words which do not occur in at least this many documents. Default is 5.
+		/// </summary>
+		public int? MinimumDocumentFrequency { get; set; }
 
-        /// <summary>
-        /// Ignore words less than this length or if 0 then this has no effect. Default is 0.
-        /// </summary>
-        public int? MinimumWordLength { get; set; }
+		/// <summary>
+		/// Boost terms in query based on score. Default is false.
+		/// </summary>
+		public bool? Boost { get; set; }
 
-        /// <summary>
-        /// Ignore words greater than this length or if 0 then this has no effect. Default is 0.
-        /// </summary>
-        public int? MaximumWordLength { get; set; }
+		/// <summary>
+		/// Ignore words less than this length or if 0 then this has no effect. Default is 0.
+		/// </summary>
+		public int? MinimumWordLength { get; set; }
 
-        /// <summary>
-        /// Return a Query with no more than this many terms. Default is 25.
-        /// </summary> 
-        public int? MaximumQueryTerms { get; set; }
+		/// <summary>
+		/// Ignore words greater than this length or if 0 then this has no effect. Default is 0.
+		/// </summary>
+		public int? MaximumWordLength { get; set; }
 
-        /// <summary>
-        /// The maximum number of tokens to parse in each example doc field that is not stored with TermVector support. Default is 5000.
-        /// </summary>
-        public int? MaximumNumberOfTokensParsed { get; set; }
+		/// <summary>
+		/// Return a Query with no more than this many terms. Default is 25.
+		/// </summary> 
+		public int? MaximumQueryTerms { get; set; }
+
+		/// <summary>
+		/// The maximum number of tokens to parse in each example doc field that is not stored with TermVector support. Default is 5000.
+		/// </summary>
+		public int? MaximumNumberOfTokensParsed { get; set; }
 
 		/// <summary>
 		/// The document id containing the custom stop words
 		/// </summary>
-        public string StopWordsDocumentId { get; set; }
+		public string StopWordsDocumentId { get; set; }
 
 		/// <summary>
 		/// The fields to compare
@@ -65,14 +70,36 @@ namespace Raven.Bundles.MoreLikeThis
 		public string[] Fields { get; set; }
 
 		/// <summary>
-		/// The document id to use as the base for comparison
+		/// The document id to use as the basis for comparison
 		/// </summary>
 		public string DocumentId { get; set; }
+
+		/// <summary>
+		/// Values for the the mapping group fields to use as the basis for comparison
+		/// </summary>
+		public StringDictionary MapGroupFields { get; set; }
 
 		public string GetRequestUri(string index)
 		{
 			var uri = new StringBuilder();
-			uri.AppendFormat("/morelikethis/{0}/{1}?", Uri.EscapeUriString(index), Uri.EscapeUriString(DocumentId));
+
+			string pathSuffix = "";
+
+			if (MapGroupFields.Count > 0)
+			{
+				var separator = "";
+				foreach(string key in MapGroupFields.Keys)
+				{
+					pathSuffix = pathSuffix + separator + Uri.EscapeUriString(key) + '=' + Uri.EscapeUriString(MapGroupFields[key]);
+					separator = ";";
+				}
+			}
+			else
+			{
+				pathSuffix = Uri.EscapeUriString(DocumentId);
+			}
+
+			uri.AppendFormat("/morelikethis/{0}/{1}?", Uri.EscapeUriString(index), pathSuffix);
 			if (Fields != null)
 			{
 				foreach (var field in Fields)
@@ -82,29 +109,23 @@ namespace Raven.Bundles.MoreLikeThis
 			}
 			if (Boost != null && Boost != DefaultBoost)
 				uri.Append("boost=true&");
-			if (MaximumQueryTerms != null &&
-				MaximumQueryTerms != DefaultMaximumQueryTerms)
+			if (MaximumQueryTerms != null && MaximumQueryTerms != DefaultMaximumQueryTerms)
 				uri.AppendFormat("maxQueryTerms={0}&", MaximumQueryTerms);
-			if (MaximumNumberOfTokensParsed != null &&
-				MaximumNumberOfTokensParsed != DefaultMaximumNumberOfTokensParsed)
+			if (MaximumNumberOfTokensParsed != null && MaximumNumberOfTokensParsed != DefaultMaximumNumberOfTokensParsed)
 				uri.AppendFormat("maxNumTokens={0}&", MaximumNumberOfTokensParsed);
-			if (MaximumWordLength != null &&
-				MaximumWordLength != DefaultMaximumWordLength)
+			if (MaximumWordLength != null && MaximumWordLength != DefaultMaximumWordLength)
 				uri.AppendFormat("maxWordLen={0}&", MaximumWordLength);
-			if (MinimumDocumentFrequency != null &&
-				MinimumDocumentFrequency != DefaltMinimumDocumentFrequency)
+			if (MinimumDocumentFrequency != null && MinimumDocumentFrequency != DefaltMinimumDocumentFrequency)
 				uri.AppendFormat("minDocFreq={0}&", MinimumDocumentFrequency);
-			if (MinimumTermFrequency != null &&
-				MinimumTermFrequency != DefaultMinimumTermFrequency)
+			if (MinimumTermFrequency != null && MinimumTermFrequency != DefaultMinimumTermFrequency)
 				uri.AppendFormat("minTermFreq={0}&", MinimumTermFrequency);
-			if (MinimumWordLength != null &&
-				MinimumWordLength != DefaultMinimumWordLength)
+			if (MinimumWordLength != null && MinimumWordLength != DefaultMinimumWordLength)
 				uri.AppendFormat("minWordLen={0}&", MinimumWordLength);
 			if (StopWordsDocumentId != null)
 				uri.AppendFormat("stopWords={0}&", StopWordsDocumentId);
 			return uri.ToString();
 		}
-		
+
 #if !CLIENT
 		public static MoreLikeThisQueryParameters GetParametersFromPath(Regex urlMatcher, string path, NameValueCollection query, out string indexName)
 		{
@@ -112,9 +133,8 @@ namespace Raven.Bundles.MoreLikeThis
 
 			indexName = match.Groups[1].Value;
 
-			return new MoreLikeThisQueryParameters
+			var results = new MoreLikeThisQueryParameters
 			{
-				DocumentId = match.Groups[2].Value,
 				Fields = query.GetValues("fields"),
 				Boost = query.Get("boost").ToNullableBool(),
 				MaximumNumberOfTokensParsed = query.Get("maxNumTokens").ToNullableInt(),
@@ -125,6 +145,25 @@ namespace Raven.Bundles.MoreLikeThis
 				MinimumWordLength = query.Get("minWordLen").ToNullableInt(),
 				StopWordsDocumentId = query.Get("stopWords"),
 			};
+
+			var keyValues = match.Groups[2].Value.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+			foreach(var keyValue in keyValues)
+			{
+				var split = keyValue.IndexOf('=');
+
+				if (split >= 0)
+				{
+					results.MapGroupFields.Add(keyValue.Substring(0, split), keyValue.Substring(split+1));
+				} 
+				else
+				{
+					results.DocumentId = keyValue;    
+				}
+			}
+
+			indexName = match.Groups[1].Value;
+			return results;
 		}
 
 		public static MoreLikeThisQueryParameters GetParametersFromPath(string pathAndQuery, out string indexName)
--- Bundles/Raven.Bundles.Tests/MoreLikeThis/MoreLikeThisQueryParametersTests.cs
+++ Bundles/Raven.Bundles.Tests/MoreLikeThis/MoreLikeThisQueryParametersTests.cs
@@ -8,36 +8,55 @@ using Xunit;
 
 namespace Raven.Bundles.Tests.MoreLikeThis
 {
-    public class MoreLikeThisQueryParametersTests
-    {
-        [Fact]
-        public void Can_encode_decode_request_by_documentId()
-        {
-            var parameters = new MoreLikeThisQueryParameters();
+	public class MoreLikeThisQueryParametersTests
+	{
+		[Fact]
+		public void Can_encode_decode_request_by_documentId()
+		{
+			var parameters = new MoreLikeThisQueryParameters();
 
-            parameters.DocumentId = "foo/1";
-            parameters.Fields = new[] { "Body" };
-            parameters.MinimumWordLength = 3;
-            parameters.MinimumDocumentFrequency = 1;
-            parameters.Boost = true;
+			parameters.DocumentId = "foo/1";
+			parameters.Fields = new[] { "Body" };
+			parameters.MinimumWordLength = 3;
+			parameters.MinimumDocumentFrequency = 1;
+			parameters.Boost = true;
 
-            var uri = parameters.GetRequestUri("dataIndex");
+			var uri = parameters.GetRequestUri("dataIndex");
 
-            Assert.Equal("/morelikethis/dataIndex/foo/1?fields=Body&boost=true&minDocFreq=1&minWordLen=3&", uri);
+			Assert.Equal("/morelikethis/dataIndex/foo/1?fields=Body&boost=true&minDocFreq=1&minWordLen=3&", uri);
 
-            var path = uri.Substring(0, uri.IndexOf('?'));
+			var path = uri.Substring(0, uri.IndexOf('?'));
 
-            string indexName;
-            var decodedParameters = MoreLikeThisQueryParameters.GetParametersFromPath(uri, out indexName);
+			string indexName;
+			var decodedParameters = MoreLikeThisQueryParameters.GetParametersFromPath(uri, out indexName);
 
-            Assert.Equal("dataIndex", indexName);
-            Assert.Equal(JsonConvert.SerializeObject(parameters), JsonConvert.SerializeObject(decodedParameters));
-        }
+			Assert.Equal("dataIndex", indexName);
+			Assert.Equal(JsonConvert.SerializeObject(parameters), JsonConvert.SerializeObject(decodedParameters));
+		}
 
-        [Fact]
-        public void Can_encode_decode_request_on_index_grouping()
-        {
-            
-        }
-    }
+		[Fact]
+		public void Can_encode_decode_request_on_index_grouping()
+		{
+			var parameters = new MoreLikeThisQueryParameters();
+
+			parameters.MapGroupFields.Add("foo", "bar");
+			parameters.MapGroupFields.Add("be", "bop");
+			parameters.Fields = new[] { "Body" };
+			parameters.MinimumWordLength = 3;
+			parameters.MinimumDocumentFrequency = 1;
+			parameters.Boost = true;
+
+			var uri = parameters.GetRequestUri("dataIndex");
+
+			Assert.Equal("/morelikethis/dataIndex/foo=bar;be=bop?fields=Body&boost=true&minDocFreq=1&minWordLen=3&", uri);
+
+			var path = uri.Substring(0, uri.IndexOf('?'));
+
+			string indexName;
+			var decodedParameters = MoreLikeThisQueryParameters.GetParametersFromPath(uri, out indexName);
+
+			Assert.Equal("dataIndex", indexName);
+			Assert.Equal(JsonConvert.SerializeObject(parameters), JsonConvert.SerializeObject(decodedParameters));
+		}
+	}
 }
