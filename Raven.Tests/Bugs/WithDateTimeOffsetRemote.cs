6d2f4e97a18bd71e7c78354a2fb3703b0c91d393
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-04-16 09:47:17 +0300

    minor

--- Raven.Tests/Bugs/WithDateTimeOffsetRemote.cs
+++ Raven.Tests/Bugs/WithDateTimeOffsetRemote.cs
@@ -9,28 +9,25 @@ namespace Raven.Tests.Bugs
 		[Fact]
 		public void CanStoreAndGetValues()
 		{
-			using(GetNewServer())
-			using (var store = new DocumentStore{Url = "http://localhost:8079"}.Initialize())
+			using (GetNewServer())
+			using (var store = new DocumentStore {Url = "http://localhost:8079"}.Initialize())
 			{
-				var expected = new DateTimeOffset(2010, 11, 10, 19, 00, 00, 00, TimeSpan.FromHours(2));
-				using (var s = store.OpenSession())
+				var expected = new DateTimeOffset(2010, 11, 10, 19, 13, 18, 26, TimeSpan.FromHours(2));
+				using (var session = store.OpenSession())
 				{
-					s.Store(new FooBar
-					{
-						Foo = expected
-					});
-					s.SaveChanges();
+					session.Store(new FooBar {Foo = expected});
+					session.SaveChanges();
 				}
 
-				using (var s = store.OpenSession())
+				using (var session = store.OpenSession())
 				{
-					var fooBar = s.Load<FooBar>("foobars/1");
+					var fooBar = session.Load<FooBar>("foobars/1");
 					Assert.Equal(expected, fooBar.Foo);
 				}
 			}
 		}
 
-		public class FooBar
+		private class FooBar
 		{
 			public DateTimeOffset Foo { get; set; }
 		}
