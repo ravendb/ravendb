24e1bfa8c34d392d6dc65e80ba3ea4d3bb6c740c
configurator <configurator@gmail.com>
2012-04-17 14:42:38 +0300

    RavenDB-209 - More 304 support ETags for Docs and Databases responders; tests for 304 in Document, Docs and Databases responsders.

--- Raven.Database/Server/Responders/Docs.cs
+++ Raven.Database/Server/Responders/Docs.cs
@@ -8,6 +8,7 @@ using Raven.Abstractions.Extensions;
 using Raven.Database.Data;
 using Raven.Database.Extensions;
 using Raven.Database.Server.Abstractions;
+using Raven.Json.Linq;
 
 namespace Raven.Database.Server.Responders
 {
@@ -20,7 +21,7 @@ namespace Raven.Database.Server.Responders
 
 		public override string[] SupportedVerbs
 		{
-			get { return new[] {"GET", "POST"}; }
+			get { return new[] { "GET", "POST" }; }
 		}
 
 		public override void Respond(IHttpContext context)
@@ -28,12 +29,27 @@ namespace Raven.Database.Server.Responders
 			switch (context.Request.HttpMethod)
 			{
 				case "GET":
-					var startsWith = context.Request.QueryString["startsWith"];
-					if(string.IsNullOrEmpty(startsWith))
-						context.WriteJson(Database.GetDocuments(context.GetStart(), context.GetPageSize(Database.Configuration.MaxPageSize), context.GetEtagFromQueryString()));
+					Guid lastDocEtag = Guid.Empty;
+					Database.TransactionalStorage.Batch(accessor =>
+					{
+						lastDocEtag = accessor.Staleness.GetMostRecentDocumentEtag();
+					});
+
+					if (context.MatchEtag(lastDocEtag))
+					{
+						context.SetStatusToNotModified();
+					}
 					else
-						context.WriteJson(Database.GetDocumentsWithIdStartingWith(startsWith, context.GetStart(),
-						                                                          context.GetPageSize(Database.Configuration.MaxPageSize)));
+					{
+						context.WriteHeaders(new RavenJObject(), lastDocEtag);
+
+						var startsWith = context.Request.QueryString["startsWith"];
+						if (string.IsNullOrEmpty(startsWith))
+							context.WriteJson(Database.GetDocuments(context.GetStart(), context.GetPageSize(Database.Configuration.MaxPageSize), context.GetEtagFromQueryString()));
+						else
+							context.WriteJson(Database.GetDocumentsWithIdStartingWith(startsWith, context.GetStart(),
+																					  context.GetPageSize(Database.Configuration.MaxPageSize)));
+					}
 					break;
 				case "POST":
 					var json = context.ReadJson();
