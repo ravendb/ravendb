203cd2a3202c5672b2b5183461262e2de5f49ac2
Ayende Rahien <ayende@ayende.com>
2012-04-18 09:56:20 +0300

    Better error

--- Raven.Client.Lightweight/Document/AbstractDocumentQuery.cs
+++ Raven.Client.Lightweight/Document/AbstractDocumentQuery.cs
@@ -1130,7 +1130,7 @@ If you really want to do in memory filtering on the data returned from the query
 
 			if (boost <= 0m)
 			{
-				throw new ArgumentOutOfRangeException("Boost factor must be a positive number");
+				throw new ArgumentOutOfRangeException("boost","Boost factor must be a positive number");
 			}
 
 			if (boost != 1m)
