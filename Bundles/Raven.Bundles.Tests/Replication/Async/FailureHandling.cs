59686f71947ed5163384f51d3aa94a560a63dca7
configurator <configurator@gmail.com>
2012-04-22 19:26:22 +0300

    Tests for replication using Async session These are all copied from their non-async versions, and altered to use async sessions API. No other changes were made.

---
+++ Bundles/Raven.Bundles.Tests/Replication/Async/FailureHandling.cs
@@ -0,0 +1,66 @@
+//-----------------------------------------------------------------------
+// <copyright file="FailureHandling.cs" company="Hibernating Rhinos LTD">
+//     Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+using System.Net;
+using System.Threading;
+using Raven.Bundles.Tests.Versioning;
+using Raven.Client.Exceptions;
+using Xunit;
+
+namespace Raven.Bundles.Tests.Replication.Async
+{
+	public class FailureHandling : ReplicationBase
+	{
+		[Fact]
+		public void When_replicating_from_two_different_source_different_documents_at_the_same_time()
+		{
+			var store1 = CreateStore();
+			var store2 = CreateStore();
+			var store3 = CreateStore();
+			using (var session = store1.OpenAsyncSession())
+			{
+				session.Store(new Company());
+				session.SaveChangesAsync().Wait();
+			}
+
+			using (var session = store2.OpenAsyncSession())
+			{
+				session.Store(new Company());
+				session.SaveChangesAsync().Wait();
+			}
+
+			TellInstanceToReplicateToAnotherInstance(0, 2);
+
+			for (int i = 0; i < RetriesCount; i++)
+			{
+				using (var session = store3.OpenAsyncSession())
+				{
+					if(session.LoadAsync<Company>("companies/1").Result != null)
+						break;
+					Thread.Sleep(100);
+				}
+			}
+
+			TellInstanceToReplicateToAnotherInstance(1, 2);
+
+			var aggregateException = Assert.Throws<AggregateException>(() =>
+			{
+				for (int i = 0; i < RetriesCount; i++)
+				{
+					using (var session = store3.OpenAsyncSession())
+					{
+						session.LoadAsync<Company>("companies/1").Wait();
+						Thread.Sleep(100);
+					}
+				}
+			});
+
+			var conflictException = Assert.IsType<ConflictException>(aggregateException.Flatten().InnerException);
+
+			Assert.Equal("Conflict detected on companies/1, conflict must be resolved before the document will be accessible", conflictException.Message);
+		}
+	}
+}
