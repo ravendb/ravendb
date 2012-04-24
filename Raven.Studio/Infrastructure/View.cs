73af58882666525971a76d54eae30acc8c7f4fd7
samuel_d_jack <sam@seaturtlesoftware.com>
2012-04-24 16:14:17 +0100

    Introduced a View class that can tell its associated ViewModel when it has been loaded/unloaded.

---
+++ Raven.Studio/Infrastructure/View.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Raven.Studio.Infrastructure
+{
+    public class View : UserControl
+    {
+        public View()
+        {
+            Loaded += HandleLoaded;
+            Unloaded += HandleUnloaded;
+        }
+
+        private void HandleLoaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ViewModel)
+            {
+                (DataContext as ViewModel).NotifyViewLoaded();
+            }
+        }
+
+        private void HandleUnloaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ViewModel)
+            {
+                (DataContext as ViewModel).NotifyViewUnloaded();
+            }
+        }
+    }
+}
