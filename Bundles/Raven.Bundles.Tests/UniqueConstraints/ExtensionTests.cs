ed186acded76034527ef5785232140604f08bd11
Felipe Leusin <felipe.leusin@gmail.com>
2012-04-26 05:11:23 -0300

    implemented extension method to get document by constraint value. todo: tests

---
+++ Bundles/Raven.Bundles.Tests/UniqueConstraints/ExtensionTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Raven.Bundles.Tests.UniqueConstraints
+{
+	public class ExtensionTests : UniqueConstraintsTest
+	{
+	}
+}
---
+++ Bundles/Raven.Client.UniqueConstraints/UniqueConstraintExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Raven.Client.UniqueConstraints
+{
+	using System;
+	using System.Linq.Expressions;
+
+	using Raven.Json.Linq;
+
+	public static class UniqueConstraintExtensions
+	{
+		public static T GetByUniqueConstraint<T>(this IDocumentSession session, Expression<Func<T, object>> keySelector, object value)
+		{
+			var typeName = session.Advanced.DocumentStore.Conventions.GetTypeTagName(typeof(T));
+			var body = (MemberExpression)keySelector.Body;
+			var propertyName = body.Member.Name;
+
+			var constraintDoc = session.Include("Id").Load("UniqueConstraints/" + typeName.ToLowerInvariant() + "/" + propertyName.ToLowerInvariant() + "/" + value) as RavenJObject;
+
+			if (constraintDoc != null)
+			{
+				RavenJToken idValue = null;
+
+				constraintDoc.TryGetValue("Id", out idValue);
+
+				if (idValue != null)
+				{
+					return session.Load<T>(idValue.ToString());
+				}
+
+			}
+
+			return default(T);
+		}
+	}
+}
