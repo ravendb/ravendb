662c315c00ea3997aa0d6b22058efdfcd01b3f38
configurator <configurator@gmail.com>
2012-04-30 16:47:40 +0300

    Implement the CompletedTask class for Task<T> as well as Task.

--- Raven.Client.Lightweight/Connection/CompletedTask.cs
+++ Raven.Client.Lightweight/Connection/CompletedTask.cs
@@ -11,6 +11,30 @@ namespace Raven.Client.Connection
 			tcs.SetResult(null);
 			return tcs.Task;
 		}
+
+		public static CompletedTask<T> With<T>(T result)
+		{
+			return new CompletedTask<T>(result);
+		}
+	}
+
+	public class CompletedTask<T>
+	{
+		public readonly T Result;
+
+		public CompletedTask() : this(default(T)) { }
+
+		public CompletedTask(T result)
+		{
+			this.Result = result;
+		}
+
+		public static implicit operator Task<T>(CompletedTask<T> t)
+		{
+			var tcs = new TaskCompletionSource<T>();
+			tcs.SetResult(t.Result);
+			return tcs.Task;
+		}
 	}
 }
 #endif
