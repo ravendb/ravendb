af35c9972c1e93f6e162dcd6c52a075fcf1d8b5a
configurator <configurator@gmail.com>
2012-05-03 03:42:24 +0300

    RavenDB-213 Create encryption bundle: part 1 - document encryption This only encrypts documents, not the indexes for now. Allows a user to set the encryption key and algorithm type by putting a document in Raven/Encryption/Settings. This is also where the settings are kept for Raven, though presumably we would want to be able to save the settings elsewhere on disk in the future (in which case the same document would point to the correct location). The settings cannot be changed once any document exists in the database, and the database will fail to start if the bundle was added to an existing database. To change encryption settings, a migration would have to be done, putting the encryption settings document first. Documents starting with "Raven/" are not encrypted.

---
+++ Bundles/Raven.Bundles.Encryption/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.Bundles.Encryption
+{
+	internal class Constants
+	{
+		public const string DontEncryptDocumentsStartingWith = "Raven/";
+		public const string EncryptionSettingsDocumentKey = "Raven/Encryption/Settings";
+
+		public const int DefaultGeneratedEncryptionKeyLength = 256 / 8;
+		public const int MinimumAcceptableEncryptionKeyLength = 64 / 8;
+
+		public const int DefaultKeySizeToUseInActualEncryptionInBits = 128;
+		public const int Rfc2898Iterations = 1000;
+
+		public static readonly Type DefaultCryptoServiceProvider = typeof(AesManaged);
+
+	}
+}
---
+++ Bundles/Raven.Bundles.Encryption/EncryptionSettings.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.Bundles.Encryption
+{
+	[Serializable]
+	public class EncryptionSettings : ISerializable
+	{
+		private byte[] encryptionKey;
+		private Type algorithmType;
+		private Func<SymmetricAlgorithm> algorithmGenerator;
+
+		public EncryptionSettings()
+			: this(GenerateRandomEncryptionKey())
+		{
+		}
+
+		public EncryptionSettings(byte[] encryptionKey)
+			: this(encryptionKey, Constants.DefaultCryptoServiceProvider)
+		{
+		}
+
+		public EncryptionSettings(byte[] encryptionKey, Type symmetricAlgorithmType)
+		{
+			this.EncryptionKey = encryptionKey;
+
+			typeof(EncryptionSettings)
+				.GetMethod("SetSymmetricAlgorithmType", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+				.MakeGenericMethod(symmetricAlgorithmType)
+				.Invoke(this, new object[0]);
+		}
+
+		public static bool DontEncrypt(string key)
+		{
+			return key.StartsWith(Constants.DontEncryptDocumentsStartingWith, StringComparison.InvariantCultureIgnoreCase);
+		}
+
+		public byte[] EncryptionKey
+		{
+			get { return encryptionKey; }
+			set
+			{
+				if (value == null)
+					throw new ArgumentNullException("EncryptionKey");
+
+				if (value.Length < Constants.MinimumAcceptableEncryptionKeyLength)
+					throw new ArgumentException("The EncryptionKey provided is too short. The minimum length is " + Constants.MinimumAcceptableEncryptionKeyLength + ".");
+				encryptionKey = value;
+			}
+		}
+
+		private void SetSymmetricAlgorithmType<T>() where T : SymmetricAlgorithm, new()
+		{
+			var type = typeof(T);
+
+			Func<SymmetricAlgorithm> generator;
+			try
+			{
+				generator = Expression.Lambda<Func<SymmetricAlgorithm>>(Expression.New(type)).Compile();
+			}
+			catch (Exception ex)
+			{
+				throw new ArgumentException("Could not compile the expression \"new " + algorithmType.Name + "();\". Inner exception was: " + ex.Message, ex);
+			}
+
+			algorithmGenerator = generator;
+			algorithmType = type;
+		}
+
+		public Type SymmetricAlgorithmType
+		{
+			get { return algorithmType; }
+		}
+
+		public Func<SymmetricAlgorithm> GenerateAlgorithm
+		{
+			get { return algorithmGenerator; }
+		}
+
+		public static byte[] GenerateRandomEncryptionKey()
+		{
+			return GenerateRandomEncryptionKey(Constants.DefaultGeneratedEncryptionKeyLength);
+		}
+
+		public static byte[] GenerateRandomEncryptionKey(int length)
+		{
+			byte[] result = new byte[length];
+			RNGCryptoServiceProvider.Create().GetBytes(result);
+			return result;
+		}
+
+		protected EncryptionSettings(SerializationInfo info, StreamingContext context)
+			: this((byte[])info.GetValue("encryptionKey", typeof(byte[])),
+				(Type)info.GetValue("algorithmType", typeof(Type))) { }
+
+		void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			info.AddValue("encryptionKey", encryptionKey);
+			info.AddValue("algorithmType", algorithmType);
+		}
+	}
+}
