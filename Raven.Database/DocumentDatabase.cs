41b0cfd96b8c41fda8219251e608ef999460da3a
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-05-08 14:46:49 +0300

    Revert "Always trim the document key. This may fix a bug where storing " " key will result in saving a document with a document key: ""." This reverts commit 2263ee51b07f2e75131e29b3bf2c5abf1f0560c1. Reason: Can throwing NRE when ID is null. Conflicts:
    	Raven.Database/DocumentDatabase.cs

--- Raven.Database/DocumentDatabase.cs
+++ Raven.Database/DocumentDatabase.cs
@@ -443,14 +443,16 @@ namespace Raven.Database
 
 		public PutResult Put(string key, Guid? etag, RavenJObject document, RavenJObject metadata, TransactionInformation transactionInformation)
 		{
-			key = key != null ? key.Trim() : null;
 			if (string.IsNullOrEmpty(key))
 			{
 				// we no longer sort by the key, so it doesn't matter
 				// that the key is no longer sequential
 				key = Guid.NewGuid().ToString();
 			}
-
+			else
+			{
+				key = key.Trim();
+			}
 			RemoveReservedProperties(document);
 			RemoveReservedProperties(metadata);
 			Guid newEtag = Guid.Empty;
