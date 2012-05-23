c4e1d34f5ae9242264f12ddda990ff40ae69d82e
configurator <configurator@gmail.com>
2012-05-24 01:21:47 +0300

    Add a very rough console encryptor/decryptor that can encrypt and decrypt index files, For debugging purposes.

--- Bundles/Raven.Bundles.Encryption.IndexFileCodec/Properties/AssemblyInfo.cs
+++ Bundles/Raven.Bundles.Encryption.IndexFileCodec/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Raven.Bundles.Encryption")]
+[assembly: AssemblyTitle("Raven.Bundles.Encryption.IndexFileCodec")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Raven.Bundles.Encryption")]
+[assembly: AssemblyProduct("Raven.Bundles.Encryption.IndexFileCodec")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b777a7ff-5333-44bf-b18f-8a32c77546de")]
+[assembly: Guid("6d25f914-d209-4664-8ea6-274a9a8a768c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("Raven.Bundles.Encryption.IndexFileCodec")]
