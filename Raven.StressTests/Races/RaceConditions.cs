7788fe04d173ed31681feaf490cd6b5fb2e4a543
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-05-30 13:05:11 +0300

    We can test ConcurrentlyOpenedTenantsUsingEsent with 100 tenants, because we also stress test this test.

--- Raven.StressTests/Races/RaceConditions.cs
+++ Raven.StressTests/Races/RaceConditions.cs
@@ -40,7 +40,7 @@ namespace Raven.StressTests.Races
 		[Fact]
 		public void ConcurrentlyOpenedTenantsUsingEsent()
 		{
-			Run<ConcurrentlyOpenedTenantsUsingEsent>(x => x.CanConcurrentlyPutDocsToDifferentTenants());
+			Run<ConcurrentlyOpenedTenantsUsingEsent>(x => x.CanConcurrentlyPutDocsToDifferentTenants(), 100);
 		}
 
 		[Fact]
