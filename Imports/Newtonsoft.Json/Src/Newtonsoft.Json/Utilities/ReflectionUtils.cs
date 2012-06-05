116b1e6f6a8faccfca7718959b7f1f7037647c9c
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-06-05 12:22:11 +0300

    Update imported namespaces

--- Imports/Newtonsoft.Json/Src/Newtonsoft.Json/Utilities/ReflectionUtils.cs
+++ Imports/Newtonsoft.Json/Src/Newtonsoft.Json/Utilities/ReflectionUtils.cs
@@ -38,11 +38,11 @@ using IConvertible = Raven.Imports.Newtonsoft.Json.Utilities.Convertible;
 using ICustomAttributeProvider = Raven.Imports.Newtonsoft.Json.Utilities.CustomAttributeProvider;
 #endif
 #if NET20
-using Newtonsoft.Json.Utilities.LinqBridge;
+using Raven.Imports.Newtonsoft.Json.Utilities.LinqBridge;
 #else
 using System.Linq;
 #endif
-using Newtonsoft.Json.Serialization;
+using Raven.Imports.Newtonsoft.Json.Serialization;
 
 namespace Raven.Imports.Newtonsoft.Json.Utilities
 {
