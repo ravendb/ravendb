05d33316f49be5b55a28c7ef1866a9a07c4caa84
samuel_d_jack <sam@seaturtlesoftware.com>
2012-06-05 18:01:08 +0100

    Fixed RavenDB-318: Created a Go To Document text box in the header bar to easily jump to a document.
    Removed the Edit Document By Id button from the DocumentsView.

---
+++ Raven.Studio/Models/GoToDocumentModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Expression.Interactivity.Core;
+using Raven.Studio.Infrastructure;
+
+namespace Raven.Studio.Models
+{
+    public class GoToDocumentModel : ViewModel
+    {
+        private string documentId = string.Empty;
+        private ICommand goToDocument;
+
+        public string DocumentId
+        {
+            get { return documentId; }
+            set
+            {
+                documentId = value;
+                OnPropertyChanged(() => DocumentId);
+            }
+        }
+
+        public ICommand GoToDocument
+        {
+            get { return goToDocument ?? (goToDocument = new ActionCommand(HandleGoToDocument)); }
+        }
+
+        private void HandleGoToDocument()
+        {
+            if (!string.IsNullOrEmpty(DocumentId))
+            {
+                UrlUtil.Navigate("/edit?id=" + DocumentId);
+            }
+        }
+    }
+}
