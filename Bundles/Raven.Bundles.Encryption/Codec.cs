bde1edd1aa6193da5a87c94c912ce71e383b1613
configurator <configurator@gmail.com>
2012-06-07 16:44:07 +0300

    Encryption bundle: do not use a salt when setting the crypto provider's key; salt the provider by encrypting the document key instead.

--- Bundles/Raven.Bundles.Encryption/Codec.cs
+++ Bundles/Raven.Bundles.Encryption/Codec.cs
@@ -6,6 +6,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Raven.Abstractions.Extensions;
 using Raven.Bundles.Encryption.Settings;
 using Raven.Database.Plugins;
 using Raven.Json.Linq;
@@ -14,7 +15,9 @@ namespace Raven.Bundles.Encryption
 {
 	internal static class Codec
 	{
-		private static readonly ThreadLocal<MD5> LocalMD5 = new ThreadLocal<MD5>(() => MD5.Create());
+		private static readonly ThreadLocal<SHA1> LocalSHA1 = new ThreadLocal<SHA1>(() => SHA1.Create());
+		private static readonly ThreadLocal<RNGCryptoServiceProvider> LocalRNG = new ThreadLocal<RNGCryptoServiceProvider>(() => new RNGCryptoServiceProvider());
+		private static Tuple<byte[], byte[]> encryptionStartingKeyAndIV = null;
 		private static int? encryptionKeySize = null;
 		private static int? encryptionIVSize = null;
 
@@ -25,25 +28,25 @@ namespace Raven.Bundles.Encryption
 
 		public static Stream Encode(string key, Stream dataStream)
 		{
-			return new CryptoStream(dataStream, GetCryptoProvider(key).CreateEncryptor(), CryptoStreamMode.Write);
+			return new CryptoStream(dataStream, GetCryptoProvider(null).CreateEncryptor(), CryptoStreamMode.Write).WriteSalt(key);
 		}
 
 		public static Stream Decode(string key, Stream dataStream)
 		{
-			return new CryptoStream(dataStream, GetCryptoProvider(key).CreateDecryptor(), CryptoStreamMode.Read);
+			return new CryptoStream(dataStream, GetCryptoProvider(null).CreateDecryptor(), CryptoStreamMode.Read).ReadSalt(key);
 		}
 
 		public static EncodedBlock EncodeBlock(string key, byte[] data)
 		{
 			byte[] iv;
-			var transform = GetCryptoProviderWithRandomIV(key, out iv).CreateEncryptor();
+			var transform = GetCryptoProviderWithRandomIV(out iv).CreateEncryptor();
 
 			return new EncodedBlock(iv, transform.TransformEntireBlock(data));
 		}
 
 		public static byte[] DecodeBlock(string key, EncodedBlock block)
 		{
-			var transform = GetCryptoProvider(key, block.IV).CreateDecryptor();
+			var transform = GetCryptoProvider(block.IV).CreateDecryptor();
 
 			return transform.TransformEntireBlock(block.Data);
 		}
@@ -53,51 +56,34 @@ namespace Raven.Bundles.Encryption
 			if (encryptionIVSize == null)
 			{
 				// This will force detection of the iv size
-				GetCryptoProvider("");
+				GetCryptoProvider(null);
 			}
 
 			return encryptionIVSize.Value;
 		}
 
-		private static SymmetricAlgorithm GetCryptoProvider(string key)
-		{
-			return GetCryptoProvider(key, null);
-		}
-		
-		private static SymmetricAlgorithm GetCryptoProvider(string key, byte[] iv)
+		private static SymmetricAlgorithm GetCryptoProvider(byte[] iv)
 		{
 			var result = EncryptionSettings.GenerateAlgorithm();
-			encryptionKeySize = encryptionKeySize ?? GetKeySizeForEncryption(result);
-			encryptionIVSize = encryptionIVSize ?? GetIVSizeForEncryption(result);
+			encryptionStartingKeyAndIV = encryptionStartingKeyAndIV ?? GetStartingKeyAndIVForEncryption(result);
 
 			if (iv != null && iv.Length != encryptionIVSize)
 				throw new ArgumentException("GetCryptoProvider: IV has wrong length. Given length: " + iv.Length + ", expectd length: " + encryptionIVSize);
 
-			var passwordBytes = new Rfc2898DeriveBytes(EncryptionSettings.EncryptionKey, GetSaltFromDocumentKey(key), Constants.Rfc2898Iterations);
-
-			result.Key = passwordBytes.GetBytes(encryptionKeySize.Value);
-
-			if (iv == null)
-				iv = passwordBytes.GetBytes(encryptionIVSize.Value);
-			result.IV = iv;
+			result.Key = encryptionStartingKeyAndIV.Item1;
+			result.IV = iv ?? encryptionStartingKeyAndIV.Item2;
 			return result;
 		}
 
-		private static SymmetricAlgorithm GetCryptoProviderWithRandomIV(string key, out byte[] iv)
+		private static SymmetricAlgorithm GetCryptoProviderWithRandomIV(out byte[] iv)
 		{
-			var rng = new RNGCryptoServiceProvider();
 			iv = new byte[GetIVLength()];
-			rng.GetBytes(iv);
+			LocalRNG.Value.GetBytes(iv);
 
-			return GetCryptoProvider(key, iv);
+			return GetCryptoProvider(iv);
 		}
 
-		private static byte[] GetSaltFromDocumentKey(string key)
-		{
-			return LocalMD5.Value.ComputeHash(Encoding.UTF8.GetBytes(key));
-		}
-
-		private static int GetKeySizeForEncryption(SymmetricAlgorithm algorithm)
+		private static Tuple<byte[], byte[]> GetStartingKeyAndIVForEncryption(SymmetricAlgorithm algorithm)
 		{
 			int bits;
 			if (algorithm.ValidKeySize(Constants.DefaultKeySizeToUseInActualEncryptionInBits))
@@ -105,12 +91,43 @@ namespace Raven.Bundles.Encryption
 			else
 				bits = algorithm.LegalKeySizes[0].MaxSize;
 
-			return bits / 8;
+			encryptionKeySize = bits / 8;
+			encryptionIVSize = algorithm.IV.Length;
+
+			var deriveBytes = new Rfc2898DeriveBytes(EncryptionSettings.EncryptionKey, GetSaltFromEncryptionKey(EncryptionSettings.EncryptionKey), Constants.Rfc2898Iterations);
+			return Tuple.Create(deriveBytes.GetBytes(encryptionKeySize.Value), deriveBytes.GetBytes(encryptionIVSize.Value));
 		}
 
-		private static int GetIVSizeForEncryption(SymmetricAlgorithm result)
+		private static byte[] GetSaltFromEncryptionKey(byte[] key)
 		{
-			return result.IV.Length;
+			return LocalSHA1.Value.ComputeHash(key);
+		}
+
+		private static Stream WriteSalt(this Stream stream, string key)
+		{
+			byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+			stream.Write(keyBytes, 0, keyBytes.Length);
+			return stream;
+		}
+
+		private static Stream ReadSalt(this Stream stream, string key)
+		{
+			try
+			{
+				byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+				byte[] readBytes = stream.ReadEntireBlock(keyBytes.Length);
+
+				if (!readBytes.SequenceEqual(keyBytes))
+				{
+					throw new InvalidDataException("The encrypted stream's salt was different than the expected salt.");
+				}
+				return stream;
+
+			}
+			catch (Exception ex)
+			{
+				throw new IOException("Encrypted stream is not correctly salted with the document key.", ex);
+			}
 		}
 
 		public struct EncodedBlock
