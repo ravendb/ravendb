a147f2d881faa6bc775eb1bd939c15ec05c2d95a
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-06-10 13:53:28 +0300

    Add StressTest for SQL Replication

--- Raven.StressTests/Races/BundelsRaceConditions.cs
+++ Raven.StressTests/Races/BundelsRaceConditions.cs
@@ -1,4 +1,5 @@
 using Raven.Bundles.Tests.Authentication;
+using Raven.Bundles.Tests.IndexReplication;
 using Raven.Bundles.Tests.Replication;
 using Raven.Tests.Shard.BlogModel;
 using Xunit;
@@ -47,6 +48,12 @@ namespace Raven.StressTests.Races
 		public void SimpleReplication()
 		{
 			Run<SimpleReplication>(x => x.Can_replicate_between_two_instances(), 200);
+		}
+		
+		[FactIfSqlServerIsAvailable]
+		public void CanReplicateToSqlWhenInserted()
+		{
+			Run<CanReplicateToSql>(x => x.WhenInserted(), 100);
 		}
 	}
 }
