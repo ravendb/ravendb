a6010c6310ec577470f4134bb36a0be7d2129691
Ayende Rahien <ayende@ayende.com>
2012-06-11 13:53:43 +0300

    Fixing build of 3.5

--- Raven.Client.Lightweight/Connection/ServerClient.cs
+++ Raven.Client.Lightweight/Connection/ServerClient.cs
@@ -463,6 +463,18 @@ namespace Raven.Client.Connection
 				.ToArray();
 		}
 
+		public IDictionary<string, RavenJToken> GetDatabases(int pageSize, int start)
+        {
+            var result = ExecuteGetRequest("".Databases(pageSize, start).NoCache());
+
+            var json = (RavenJArray)result;
+
+        	return json
+        		.ToDictionary(
+        			x =>
+        			x.Value<RavenJObject>("@metadata").Value<string>("@id").Replace("Raven/Databases/", string.Empty));
+        }
+
 		private void DirectDeleteAttachment(string key, Guid? etag, string operationUrl)
 		{
 			var metadata = new RavenJObject();
