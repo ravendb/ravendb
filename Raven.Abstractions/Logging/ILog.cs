937045dd66120b84fbcb2b6d780b867b566db799
Damian Hickey <dhickey@gmail.com>
2012-06-14 10:17:11 +0100

    Importing DH.Logging codebase + tests that provide a logging abstraction and dynamic support of NLog and log4net.

---
+++ Raven.Abstractions/Logging/ILog.cs
@@ -0,0 +1,11 @@
+﻿namespace Raven.Abstractions.Logging
+{
+	using System;
+
+	public interface ILog
+	{
+		void Log(LogLevel logLevel, Func<string> messageFunc);
+
+		void Log<TException>(LogLevel logLevel, Func<string> messageFunc, TException exception) where TException : Exception;
+	}
+}
