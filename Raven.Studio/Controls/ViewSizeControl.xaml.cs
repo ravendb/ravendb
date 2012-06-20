6df2c1eb2e8fd30fc530ffc872c863d5c5bc060a
samuel_d_jack <sam@seaturtlesoftware.com>
2012-06-20 14:56:05 +0100

    Implemented RavenDB-321: The Document View button can now be clicked to switch between views.

--- Raven.Studio/Controls/ViewSizeControl.xaml.cs
+++ Raven.Studio/Controls/ViewSizeControl.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using Microsoft.Expression.Interactivity.Core;
 using System.Linq;
+using Raven.Studio.Extensions;
 
 namespace Raven.Studio.Controls
 {
@@ -12,31 +13,63 @@ namespace Raven.Studio.Controls
         public static readonly DependencyProperty ViewSizeProperty =
             DependencyProperty.Register("ViewSize", typeof (double), typeof (ViewSizeControl), new PropertyMetadata(default(double)));
 
-        private static readonly List<PresetViewSize> ViewSizes = new List<PresetViewSize>()
-                                                                     {
-                                                                         new PresetViewSize() { Key = "Details", Size = 0},
-                                                                         new PresetViewSize() { Key = "SmallCard", Size= 10},
-                                                                         new PresetViewSize() { Key = "MediumCard", Size=45},
-                                                                         new PresetViewSize() { Key = "LargeCard", Size=75},
-                                                                         new PresetViewSize() { Key = "ExtraLargeCard", Size=100},
-                                                                     };
+        private static readonly List<PresetViewSize> ViewSizes;
+        
         public double ViewSize
         {
             get { return (double) GetValue(ViewSizeProperty); }
             set { SetValue(ViewSizeProperty, value); }
         }
 
+        static ViewSizeControl()
+        {
+            DetailsViewSize = new PresetViewSize() {Key = "Details", Size = 0};
+            MediumCardViewSize = new PresetViewSize() {Key = "MediumCard", Size = 45};
+
+            ViewSizes = new List<PresetViewSize>()
+                            {
+                                DetailsViewSize,
+                                new PresetViewSize() {Key = "SmallCard", Size = 10},
+                                MediumCardViewSize,
+                                new PresetViewSize() {Key = "LargeCard", Size = 75},
+                                new PresetViewSize() {Key = "ExtraLargeCard", Size = 100},
+                            };
+        }
+
         public ViewSizeControl()
         {
             InitializeComponent();
+            
+
             LayoutRoot.DataContext = this;
+
         }
 
         private ICommand setViewSize;
+        private ICommand toggleViewSize;
+        private static PresetViewSize DetailsViewSize;
+        private static PresetViewSize MediumCardViewSize;
 
         public ICommand SetViewSize
         {
             get { return setViewSize ?? (setViewSize = new ActionCommand(HandleSetViewSize)); }
+        }
+
+        public ICommand ToggleViewSize
+        {
+            get { return toggleViewSize ?? (toggleViewSize = new ActionCommand(HandleToggleViewSize)); }
+        }
+
+        private void HandleToggleViewSize()
+        {
+              if (ViewSize > DetailsViewSize.Size)
+              {
+                  SetViewSize.Execute(DetailsViewSize.Key);
+              }
+              else
+              {
+                  SetViewSize.Execute(MediumCardViewSize.Key);
+              }
         }
 
         private void HandleSetViewSize(object parameter)
