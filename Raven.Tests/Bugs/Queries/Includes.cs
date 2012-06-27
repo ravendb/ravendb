c83abe1b98c846d90653383192bf00a5ab9ffcc2
Ayende Rahien <ayende@ayende.com>
2012-06-27 13:45:04 +0300

    Adding support for complex includes (Include(x=>x.Friends.Select(y=>y.Id)) )

--- Raven.Tests/Bugs/Queries/Includes.cs
+++ Raven.Tests/Bugs/Queries/Includes.cs
@@ -5,11 +5,47 @@
 //-----------------------------------------------------------------------
 using Raven.Client.Document;
 using Xunit;
+using System.Linq;
 
 namespace Raven.Tests.Bugs.Queries
 {
 	public class Includes : RemoteClientTest
 	{
+		[Fact]
+		public void CanGenerateComplexPaths()
+		{
+			using (GetNewServer())
+			using (var store = new DocumentStore { Url = "http://localhost:8079" }.Initialize())
+			{
+				using (var s = store.OpenSession())
+				{
+					s.Store(new User { Name = "Ayende" });
+					s.Store(new User
+					{
+						Name = "Rahien",
+						Friends = new[]
+						{
+							new DenormalizedReference {Name = "Ayende", Id = "users/1"},
+						}
+					});
+
+					s.SaveChanges();
+				}
+
+				using (var s = store.OpenSession())
+				{
+					var user = s.Include<User>(x => x.Friends.Select(f => f.Id)).Load<User>("users/2");
+					Assert.Equal(1, user.Friends.Length);
+					foreach (var denormalizedReference in user.Friends)
+					{
+						s.Load<User>(denormalizedReference.Id);
+					}
+
+					Assert.Equal(1, s.Advanced.NumberOfRequests);
+				}
+			}
+		}
+
 		[Fact]
 		public void CanIncludeViaNestedPath()
 		{
