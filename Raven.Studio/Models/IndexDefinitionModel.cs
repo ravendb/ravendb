5d1b517d55e2abecbe53ab3c2c02c127015e8b75
samuel_d_jack <sam@seaturtlesoftware.com>
2012-07-04 17:52:18 +0100

    Completed RavenDB-350: Users are now warned before navigating away from an index page with unsaved changes.

--- Raven.Studio/Models/IndexDefinitionModel.cs
+++ Raven.Studio/Models/IndexDefinitionModel.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows.Input;
 using Raven.Imports.Newtonsoft.Json;
@@ -18,6 +20,7 @@ namespace Raven.Studio.Models
 		private IndexDefinition index;
 		private string originalIndex;
 	    private bool isNewIndex;
+	    private bool hasUnsavedChanges;
 
 		public IndexDefinitionModel()
 		{
@@ -27,13 +30,36 @@ namespace Raven.Studio.Models
 			{
 				new MapItem()
 			};
+
+		    Maps.CollectionChanged += HandleChildCollectionChanged;
+
 			Fields = new BindableCollection<FieldProperties>(field => field.Name);
+		    Fields.CollectionChanged += HandleChildCollectionChanged;
 
 			statistics = Database.Value.Statistics;
 			statistics.PropertyChanged += (sender, args) => OnPropertyChanged(() => ErrorsCount);
 		}
 
-		private void UpdateFromIndex(IndexDefinition indexDefinition)
+	    private void HandleChildCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+	    {
+	        MarkAsDirty();
+
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                var newItem = e.NewItems[0] as INotifyPropertyChanged;
+                if (newItem != null)
+                {
+                    newItem.PropertyChanged += HandleChildItemChanged;
+                }
+            }
+	    }
+
+	    private void HandleChildItemChanged(object sender, PropertyChangedEventArgs e)
+	    {
+	        MarkAsDirty();
+	    }
+
+	    private void UpdateFromIndex(IndexDefinition indexDefinition)
 		{
 			index = indexDefinition;
 
@@ -51,6 +77,8 @@ namespace Raven.Studio.Models
 			CreateOrEditField(index.Stores, (f, i) => f.Storage = i);
 			CreateOrEditField(index.SortOptions, (f, i) => f.Sort = i);
 			CreateOrEditField(index.Analyzers, (f, i) => f.Analyzer = i);
+
+	        hasUnsavedChanges = false;
 
 			OnEverythingChanged();
 		}
@@ -75,18 +103,31 @@ namespace Raven.Studio.Models
 			Header = name;
             IsNewIndex = false;
 
-			DatabaseCommands.GetIndexAsync(name)
-				.ContinueOnUIThread(task =>
-				                   {
-                                       if (task.IsFaulted || task.Result == null)
-				                   	{
-										HandleIndexNotFound(name);
-				                   		return;
-				                   	}
-									originalIndex = JsonConvert.SerializeObject(index);
-									    UpdateFromIndex(task.Result);
-				                   }).Catch();
+		    DatabaseCommands.GetIndexAsync(name)
+		        .ContinueOnUIThread(task =>
+		                                {
+		                                    if (task.IsFaulted || task.Result == null)
+		                                    {
+		                                        HandleIndexNotFound(name);
+		                                        return;
+		                                    }
+		                                    originalIndex = JsonConvert.SerializeObject(task.Result);
+		                                    UpdateFromIndex(task.Result);
+		                                }).Catch();
 		}
+
+        public override bool CanLeavePage()
+        {
+            if (hasUnsavedChanges)
+            {
+                return AskUser.Confirmation("Edit Index",
+                                            "There are unsaved changes to this index. Are you sure you want to continue?");
+            }
+            else
+            {
+                return true;
+            }
+        }
 
 		public static void HandleIndexNotFound(string name)
 		{
@@ -150,8 +191,12 @@ namespace Raven.Studio.Models
 			get { return index.Name; }
 			set
 			{
-				index.Name = value;
-				OnPropertyChanged(() => Name);
+			    if (index.Name != value)
+			    {
+			        index.Name = value;
+			        OnPropertyChanged(() => Name);
+                    MarkAsDirty();
+			    }
 			}
 		}
 
@@ -187,12 +232,21 @@ namespace Raven.Studio.Models
 			get { return index.Reduce; }
 			set
 			{
-				index.Reduce = value;
-				OnPropertyChanged(() => Reduce);
+			    if (index.Reduce != value)
+			    {
+			        index.Reduce = value;
+			        MarkAsDirty();
+			        OnPropertyChanged(() => Reduce);
+			    }
 			}
 		}
 
-		private bool showTransformResults;
+	    private void MarkAsDirty()
+	    {
+	        hasUnsavedChanges = true;
+	    }
+
+	    private bool showTransformResults;
 		public bool ShowTransformResults
 		{
 			get { return showTransformResults; }
@@ -208,8 +262,12 @@ namespace Raven.Studio.Models
 			get { return index.TransformResults; }
 			set
 			{
-				index.TransformResults = value;
-				OnPropertyChanged(() => TransformResults);
+			    if (index.TransformResults != value)
+			    {
+			        index.TransformResults = value;
+			        MarkAsDirty();
+			        OnPropertyChanged(() => TransformResults);
+			    }
 			}
 		}
 
@@ -354,6 +412,7 @@ namespace Raven.Studio.Models
 					                       {
 					                           ApplicationModel.Current.AddNotification(
 					                               new Notification("index " + index.Name + " saved"));
+					                           index.hasUnsavedChanges = false;
 					                           PutIndexNameInUrl(index.Name);
 					                       })
 					.Catch();
@@ -431,20 +490,94 @@ namespace Raven.Studio.Models
 
 		#endregion Commands
 
-		public class MapItem
+		public class MapItem : NotifyPropertyChangedBase
 		{
-			public string Text { get; set; }
+		    private string text;
+
+		    public string Text
+		    {
+		        get { return text; }
+                set
+                {
+                    if (text != value)
+                    {
+                        text = value;
+                        OnPropertyChanged(() => Text);
+                    }
+                }
+		    }
 		}
 
-		public class FieldProperties
+		public class FieldProperties : NotifyPropertyChangedBase
 		{
-			public string Name { get; set; }
-			public FieldStorage Storage { get; set; }
-			public FieldIndexing Indexing { get; set; }
-			public SortOptions Sort { get; set; }
-			public string Analyzer { get; set; }
+		    private string name;
+		    public string Name
+		    {
+		        get { return name; }
+                set
+                {
+                    if (name != value)
+                    {
+                        name = value;
+                        OnPropertyChanged(() => Name);
+                    }
+                }
+		    }
 
-			public static FieldProperties Defualt
+		    private FieldStorage storage;
+		    public FieldStorage Storage
+		    {
+		        get { return storage; }
+		        set {
+		            if (storage != value)
+		            {
+		                storage = value;
+		                OnPropertyChanged(() => Storage);
+		            }
+                }
+		    }
+
+		    private FieldIndexing indexing;
+		    public FieldIndexing Indexing
+		    {
+		        get { return indexing; }
+		        set {
+		            if (indexing != value)
+		            {
+		                indexing = value;
+		                OnPropertyChanged(() => Indexing);
+		            }
+                }
+		    }
+
+		    private SortOptions sort;
+		    public SortOptions Sort
+		    {
+		        get { return sort; }
+                set
+                {
+                    if (sort != value)
+                    {
+                        sort = value;
+                        OnPropertyChanged(() => Sort);
+                    }
+                }
+		    }
+
+		    private string analyzer;
+		    public string Analyzer
+		    {
+		        get { return analyzer; }
+		        set {
+		            if (analyzer != value)
+		            {
+		                analyzer = value;
+		                OnPropertyChanged(() => Analyzer);
+		            }
+		        }
+		    }
+
+		    public static FieldProperties Defualt
 			{
 				get
 				{
