d8992206a1603da79b9bf62321d8fa8eea0e92a5
configurator <configurator@gmail.com>
2012-07-11 16:22:09 +0300

    Tests for transactions with compression & encryption bundles

--- Bundles/Raven.Bundles.Tests/Compression/Crud.cs
+++ Bundles/Raven.Bundles.Tests/Compression/Crud.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Text;
+using System.Transactions;
 using Raven.Abstractions.Data;
 using Raven.Abstractions.Indexing;
 using Raven.Bundles.Tests.Versioning;
@@ -23,16 +24,47 @@ namespace Raven.Bundles.Tests.Compression
 
 			using (var session = documentStore.OpenSession())
 			{
-				Assert.Equal(company.Name, session.Load<Company>(1).Name);
+				Assert.Equal(CompanyName, session.Load<Company>(1).Name);
 			}
 
 			AssertPlainTextIsNotSavedInDatabase(CompanyName);
 		}
 
-		[Fact(Skip = "TODO")]
+		[Fact]
 		public void Transactional()
 		{
-			// TODO
+			const string FirstCompany = "FirstCompany";
+			const string SecondCompany = "SecondCompany";
+			const string ThirdCompany = "ThirdCompany";
+			const string FourthCompany = "FourthCompany";
+
+			using (var session = documentStore.OpenSession())
+			using (var tx = new TransactionScope())
+			{
+				session.Store(new Company { Name = FirstCompany });
+				session.Store(new Company { Name = SecondCompany });
+				session.SaveChanges();
+				tx.Complete();
+			}
+
+			using (var session = documentStore.OpenSession())
+			using (new TransactionScope())
+			{
+				session.Store(new Company { Name = ThirdCompany });
+				session.Store(new Company { Name = FourthCompany });
+				session.SaveChanges();
+				// this transaction is not committed
+			}
+
+			using (var session = documentStore.OpenSession())
+			{
+				Assert.Equal(FirstCompany, session.Load<Company>(1).Name);
+				Assert.Equal(SecondCompany, session.Load<Company>(2).Name);
+				Assert.Equal(null, session.Load<Company>(3));
+				Assert.Equal(null, session.Load<Company>(4));
+			}
+
+			AssertPlainTextIsNotSavedInDatabase(FirstCompany, SecondCompany, ThirdCompany, FourthCompany);
 		}
 	}
 }
--- Bundles/Raven.Bundles.Tests/Encryption/Crud.cs
+++ Bundles/Raven.Bundles.Tests/Encryption/Crud.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Text;
+using System.Transactions;
 using Raven.Abstractions.Data;
 using Raven.Abstractions.Indexing;
 using Raven.Bundles.Tests.Versioning;
@@ -29,10 +30,41 @@ namespace Raven.Bundles.Tests.Encryption
 			AssertPlainTextIsNotSavedInDatabase(CompanyName);
 		}
 
-		[Fact(Skip = "TODO")]
+		[Fact]
 		public void Transactional()
 		{
-			// TODO
+			const string FirstCompany = "FirstCompany";
+			const string SecondCompany = "SecondCompany";
+			const string ThirdCompany = "ThirdCompany";
+			const string FourthCompany = "FourthCompany";
+
+			using (var session = documentStore.OpenSession())
+			using (var tx = new TransactionScope())
+			{
+				session.Store(new Company { Name = FirstCompany });
+				session.Store(new Company { Name = SecondCompany });
+				session.SaveChanges();
+				tx.Complete();
+			}
+
+			using (var session = documentStore.OpenSession())
+			using (new TransactionScope())
+			{
+				session.Store(new Company { Name = ThirdCompany });
+				session.Store(new Company { Name = FourthCompany });
+				session.SaveChanges();
+				// this transaction is not committed
+			}
+
+			using (var session = documentStore.OpenSession())
+			{
+				Assert.Equal(FirstCompany, session.Load<Company>(1).Name);
+				Assert.Equal(SecondCompany, session.Load<Company>(2).Name);
+				Assert.Equal(null, session.Load<Company>(3));
+				Assert.Equal(null, session.Load<Company>(4));
+			}
+
+			AssertPlainTextIsNotSavedInDatabase(FirstCompany, SecondCompany, ThirdCompany, FourthCompany);
 		}
 	}
 }
