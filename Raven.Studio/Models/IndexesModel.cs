231f709c742073633f377af4b2ccb438a890d985
samuel_d_jack <sam@seaturtlesoftware.com>
2012-07-17 11:36:01 +0100

    Fixed RavenDB-398: Right click menu on index list page no longer greys out.

--- Raven.Studio/Models/IndexesModel.cs
+++ Raven.Studio/Models/IndexesModel.cs
@@ -1,34 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Raven.Studio.Commands;
 using Raven.Studio.Infrastructure;
+using Raven.Abstractions.Extensions;
 
 namespace Raven.Studio.Models
 {
 	public class IndexesModel : PageViewModel
 	{
-		public static BindableCollection<IndexListItem> GroupedIndexes { get; private set; }
-
-		static IndexesModel()
-		{
-			GroupedIndexes = new BindableCollection<IndexListItem>(x => x);
-		}
+	    private ICommand promoteIndex;
+	    private ICommand deleteIndex;
+	    private ICommand resetIndex;
+	    public ObservableCollection<IndexListItem> GroupedIndexes { get; private set; }
 
 		public IndexesModel()
 		{
-			ModelUrl = "/indexes";
+            ModelUrl = "/indexes";
+		    GroupedIndexes =
+                new ObservableCollection<IndexListItem>();
+            ItemSelection = new ItemSelection<IndexItem>();
 		}
 
 		public override Task TimerTickedAsync()
 		{
 			return DatabaseCommands
 				.GetIndexNamesAsync(0, 256)
-				.ContinueOnSuccess(UpdateGroupedIndexList);
+				.ContinueOnSuccessInTheUIThread(UpdateGroupedIndexList);
 		}
+
+        public ItemSelection<IndexItem> ItemSelection { get; private set; }
+
+        public ICommand PromoteIndex { get { return promoteIndex ?? (promoteIndex = new PromoteToAutoIndexCommand(this)); } }
+        public ICommand DeleteIndex { get { return deleteIndex ?? (deleteIndex = new DeleteIndexCommand(this)); } }
+        public ICommand ResetIndex { get { return resetIndex ?? (resetIndex = new ResetIndexCommand(this)); } }
 
 		private void UpdateGroupedIndexList(IList<string> indexes)
 		{
+		    var currentSelection = ItemSelection.GetSelectedItems().Select(i => i.Name).ToHashSet();
+
 			var indexGroups = from index in indexes
 							  let groupDetails = GetIndexGroup(index)
 							  let indexGroup = groupDetails.Item1
@@ -36,11 +49,16 @@ namespace Raven.Studio.Models
 							  orderby indexOrder
 							  group index by indexGroup;
 
-			var indexesAndGroupHeaders = indexGroups.SelectMany(group => new IndexListItem[] { new IndexGroupHeader { Name = group.Key } }
-																		.Concat(group.Select(index => new IndexItem { IndexName = index })
-																		.Cast<IndexListItem>()));
+		    var indexesAndGroupHeaders =
+		        indexGroups.SelectMany(group => new IndexListItem[] {new IndexGroupHeader {Name = group.Key}}
+		                                            .Concat(group.Select(index => new IndexItem {Name = index})));
 
-			GroupedIndexes.Match(indexesAndGroupHeaders.ToList());
+            GroupedIndexes.Clear();
+			GroupedIndexes.AddRange(indexesAndGroupHeaders.ToList());
+
+            var selection = GroupedIndexes.OfType<IndexItem>().Where(i => currentSelection.Contains(i.Name));
+
+            ItemSelection.SetDesiredSelection(selection);
 		}
 
 		private Tuple<string, int> GetIndexGroup(string indexName)
