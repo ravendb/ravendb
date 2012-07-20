29909e3385ce0670bb16bfd4ecb7c7dffe501a24
Ayende Rahien <ayende@ayende.com>
2012-07-20 13:18:12 +0300

    Removing SignalR files

--- Imports/SignalR/SignalR.Hosting.Common/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SignalR.Hosting.Common")]
-[assembly: AssemblyDescription("Assembly containing common components for implementing SignalR hosts.")]
-[assembly: AssemblyVersion("0.5.2.0")]
