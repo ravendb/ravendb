7d96f6f1403ed635b31547215e22a2ee1314fcd0
Daniel Dar <hammericed@gmail.com>
2012-07-23 11:28:48 +0300

    promote to create new DB on first run on any page

--- Raven.Studio/Infrastructure/ViewModel.cs
+++ Raven.Studio/Infrastructure/ViewModel.cs
@@ -1,60 +1,62 @@
 ﻿using System;
-using System.Net;
 using System.Reactive;
 using System.Reactive.Subjects;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+using Raven.Studio.Commands;
 using Raven.Studio.Models;
 
 namespace Raven.Studio.Infrastructure
 {
-    public class ViewModel : Model
-    {
-        private Subject<Unit> unloadedSubject;
+	public class ViewModel : Model
+	{
+		private Subject<Unit> unloadedSubject;
 
-        public void NotifyViewLoaded()
-        {
-            IsLoaded = true;
-            OnViewLoaded();
-        }
+		public void NotifyViewLoaded()
+		{
+			IsLoaded = true;
+			OnViewLoaded();
+		}
 
-        public void NotifyViewUnloaded()
-        {
-            if (unloadedSubject != null)
-            {
-                unloadedSubject.OnNext(Unit.Default);
-            }
+		public override System.Threading.Tasks.Task TimerTickedAsync()
+		{
+			if (ApplicationModel.Current.Server.Value.CreateNewDatabase)
+			{
+				ApplicationModel.Current.Server.Value.CreateNewDatabase = false;
+				Command.ExecuteCommand(new CreateDatabaseCommand());
+			}
+			return base.TimerTickedAsync();
+		}
 
-            OnViewUnloaded();
+		public void NotifyViewUnloaded()
+		{
+			if (unloadedSubject != null)
+			{
+				unloadedSubject.OnNext(Unit.Default);
+			}
 
-            IsLoaded = false;
-        }
+			OnViewUnloaded();
 
-        protected virtual void OnViewUnloaded()
-        {
-            
-        }
+			IsLoaded = false;
+		}
 
-        protected virtual void OnViewLoaded()
-        {
-       
-        }
+		protected virtual void OnViewUnloaded()
+		{
+			
+		}
 
-        protected IObservable<Unit> Unloaded
-        {
-            get { return unloadedSubject ?? (unloadedSubject = new Subject<Unit>()); }
-        }
+		protected virtual void OnViewLoaded()
+		{
+	   
+		}
 
-        protected bool IsLoaded { get; private set; }
-        protected PerDatabaseState PerDatabaseState
-        {
-            get { return ApplicationModel.Current.State.Databases[ApplicationModel.Database.Value]; }
-        }
-    }
+		protected IObservable<Unit> Unloaded
+		{
+			get { return unloadedSubject ?? (unloadedSubject = new Subject<Unit>()); }
+		}
+
+		protected bool IsLoaded { get; private set; }
+		protected PerDatabaseState PerDatabaseState
+		{
+			get { return ApplicationModel.Current.State.Databases[ApplicationModel.Database.Value]; }
+		}
+	}
 }
