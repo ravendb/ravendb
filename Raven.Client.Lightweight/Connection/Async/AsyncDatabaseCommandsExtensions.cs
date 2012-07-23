d603b181ba651a969f1eefba4f295906122a0272
Ayende Rahien <ayende@ayende.com>
2012-07-23 07:24:27 +0300

    Fixing the way we are handling facets on nullables

--- Raven.Client.Lightweight/Connection/Async/AsyncDatabaseCommandsExtensions.cs
+++ Raven.Client.Lightweight/Connection/Async/AsyncDatabaseCommandsExtensions.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
 using Raven.Abstractions.Data;
+using Raven.Abstractions.Util;
 using Raven.Json.Linq;
 
 namespace Raven.Client.Connection.Async
