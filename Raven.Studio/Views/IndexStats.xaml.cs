89112d9f67eb9faf9a5e8eff4adb7dc53c59cad7
DanielDar <daniel@ayende.com>
2012-07-24 12:22:58 +0300

    add index stat page (through the index edit)

---
+++ Raven.Studio/Views/IndexStats.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Navigation;
+using Raven.Studio.Infrastructure;
+
+namespace Raven.Studio.Views
+{
+	public partial class IndexStats : PageView
+	{
+		public IndexStats()
+		{
+			InitializeComponent();
+		}
+	}
+}
