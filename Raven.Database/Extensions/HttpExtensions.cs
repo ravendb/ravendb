915ae369ecc1839b78443826cf5458e2dacbce96
Ayende Rahien <ayende@ayende.com>
2012-08-02 13:00:45 +0300

    Making sure that metadata-only state will work for queries and indexes

--- Raven.Database/Extensions/HttpExtensions.cs
+++ Raven.Database/Extensions/HttpExtensions.cs
@@ -131,7 +131,7 @@ namespace Raven.Database.Extensions
 			streamWriter.Flush();
 		}
 
-		private static RavenJToken MinimizeToken(RavenJToken obj)
+		private static RavenJToken MinimizeToken(RavenJToken obj, int depth = 0)
 		{
 			switch (obj.Type)
 			{
@@ -139,13 +139,28 @@ namespace Raven.Database.Extensions
 					var array = new RavenJArray();
 					foreach (var item in ((RavenJArray)obj))
 					{
-						array.Add(MinimizeToken(item));
+						array.Add(MinimizeToken(item, depth+1));
 					}
 					return array;
 				case JTokenType.Object:
 					var ravenJObject = ((RavenJObject) obj);
 					if (ravenJObject.ContainsKey(Constants.Metadata) == false)
+					{
+						// this might be a wrapper object, let check for first level arrays
+						if(depth == 0)
+						{
+							var newRootObj = new RavenJObject();
+
+							foreach (var prop in ravenJObject)
+							{
+								newRootObj[prop.Key] = prop.Value.Type == JTokenType.Array ? 
+									MinimizeToken(prop.Value, depth + 1) :
+									prop.Value;
+							}
+							return newRootObj;
+						}
 						return obj;
+					}
 					var newObj = new RavenJObject();
 					newObj[Constants.Metadata] = ravenJObject[Constants.Metadata];
 					return newObj;
