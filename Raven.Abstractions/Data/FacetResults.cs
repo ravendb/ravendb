095c2f6d35252891f4647ce82b1e6d335637823f
Michael Weber <mtweber@gmail.com>
2012-08-16 08:48:19 -0400

    Add some parameter documentation.

--- Raven.Abstractions/Data/FacetResults.cs
+++ Raven.Abstractions/Data/FacetResults.cs
@@ -4,6 +4,9 @@ namespace Raven.Abstractions.Data
 {
 	public class FacetResults
 	{
+		/// <summary>
+		/// A list of results for the facet.  One entry for each term/range as specified in the facet setup document.
+		/// </summary>
 		public Dictionary<string, FacetResult> Results { get; set; }
 
 		public FacetResults()
