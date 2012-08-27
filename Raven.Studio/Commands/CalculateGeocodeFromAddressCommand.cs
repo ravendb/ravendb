882e24627a0691815e11b428173c557bd8ff4b97
DanielDar <daniel@ayende.com>
2012-08-27 16:29:30 +0300

    Add option to update lat lon on spatial queries by address

--- Raven.Studio/Commands/CalculateGeocodeFromAddressCommand.cs
+++ Raven.Studio/Commands/CalculateGeocodeFromAddressCommand.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Net;
+using System.Text;
+using Raven.Json.Linq;
 using Raven.Studio.Infrastructure;
 using Raven.Studio.Models;
+using System.Linq;
 
 namespace Raven.Studio.Commands
 {
@@ -14,13 +17,21 @@ namespace Raven.Studio.Commands
 		}
 		public override void Execute(object parameter)
 		{
-			var url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + queryModel.Address + "&sensor=false";
+			var url = "http://where.yahooapis.com/geocode?flags=JC&q=" + queryModel.Address;
 			var webRequest = WebRequest.Create(new Uri(url, UriKind.Absolute));
 			webRequest.GetResponseAsync().ContinueOnSuccessInTheUIThread(doc =>
 			{
-				var x = doc;
-			}).Catch(exception => exception = exception);
+				var buffer = new byte[doc.ContentLength];
+				doc.GetResponseStream().Read(buffer, 0, (int) doc.ContentLength);
+				var jsonData = RavenJObject.Parse(Encoding.UTF8.GetString(buffer,0, buffer.Length));
+				var result = jsonData["ResultSet"].SelectToken("Results").Values().FirstOrDefault();
+				if (result != null)
+				{
+					queryModel.Latitude = double.Parse(result.Value<string>("latitude"));
+					queryModel.Longitude = double.Parse(result.Value<string>("longitude"));
+				}
 
+			}).Catch();
 		}
 	}
 }
