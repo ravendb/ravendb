848f627c86d462f10ad782cca0c301f851f8ab6b
DanielDar <daniel@ayende.com>
2012-08-27 15:05:23 +0300

    Add base for getting geocode from address

---
+++ Raven.Studio/Commands/CalculateGeocodeFromAddressCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using Raven.Studio.Infrastructure;
+using Raven.Studio.Models;
+
+namespace Raven.Studio.Commands
+{
+	public class CalculateGeocodeFromAddressCommand : Command
+	{
+		private readonly QueryModel queryModel;
+		public CalculateGeocodeFromAddressCommand(QueryModel queryModel)
+		{
+			this.queryModel = queryModel;
+		}
+		public override void Execute(object parameter)
+		{
+			var url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + queryModel.Address + "&sensor=false";
+			var webRequest = WebRequest.Create(new Uri(url, UriKind.Absolute));
+			webRequest.GetResponseAsync().ContinueOnSuccessInTheUIThread(doc =>
+			{
+				var x = doc;
+			}).Catch(exception => exception = exception);
+
+		}
+	}
+}
