ef1dbb7df4f60cb77e34c1b879c12bebe85e87c8
Ayende Rahien <ayende@ayende.com>
2012-08-28 22:29:15 +0300

    Only optimize negation for member access

--- Raven.Client.Lightweight/Linq/RavenQueryProviderProcessor.cs
+++ Raven.Client.Lightweight/Linq/RavenQueryProviderProcessor.cs
@@ -278,6 +278,18 @@ namespace Raven.Client.Linq
 				return;
 			}
 
+
+			if (constantExpression != null && false.Equals(constantExpression.Value) && 
+				expression.Left.NodeType != ExpressionType.MemberAccess)
+			{
+				luceneQuery.OpenSubclause();
+				luceneQuery.Where("*:*");
+				luceneQuery.NegateNext();
+				VisitExpression(expression.Left);
+				luceneQuery.CloseSubclause();
+				return;
+			}
+
 			var methodCallExpression = expression.Left as MethodCallExpression;
 			// checking for VB.NET string equality
 			if (methodCallExpression != null && methodCallExpression.Method.Name == "CompareString" &&
