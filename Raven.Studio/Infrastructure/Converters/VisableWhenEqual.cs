346fb306c6fbdf54c5eaad2ee53e3006aa24a634
DanielDar <daniel@ayende.com>
2012-09-02 14:59:52 +0300

    Improve Alerts UI

---
+++ Raven.Studio/Infrastructure/Converters/VisableWhenEqual.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Raven.Studio.Infrastructure.Converters
+{
+	public class VisableWhenEqual : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value.Equals(parameter))
+				return Visibility.Visible;
+			return Visibility.Collapsed;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
