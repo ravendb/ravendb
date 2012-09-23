742e87eb51345c6e4c26264fd2099dd43f4a92c6
DanielDar <daniel@ayende.com>
2012-09-23 18:14:05 +0200

    base for Authorization bundle ui

---
+++ Raven.Studio/Infrastructure/Converters/StringListToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Raven.Studio.Infrastructure.Converters
+{
+    public class StringListToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var list = value as IEnumerable<string>;
+            if (list == null)
+                return string.Empty;
+            return string.Join(",", list);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value as string;
+            if (str == null)
+                return null;
+            return  str.Split(',');
+        }
+    }
+}
