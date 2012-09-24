3cb4da0e175e48c903b143d7ceba7072ab06a48d
Ayende Rahien <ayende@ayende.com>
2012-09-24 23:56:31 +0200

    Properly handling how we output and save UTC date times and UTC date time offsets now

--- Raven.Abstractions/Default.cs
+++ Raven.Abstractions/Default.cs
@@ -7,8 +7,7 @@ namespace Raven.Abstractions
 	{
 		public static readonly string[] OnlyDateTimeFormat = new[]
 		{
-			"yyyy'-'MM'-'dd'T'HH':'mm':'ss.fffffff",
-			 @"yyyy'-'MM'-'dd'T'HH':'mm':'ss.fffffff\Z"
+			"yyyy'-'MM'-'dd'T'HH':'mm':'ss.fffffff"
 		};
 
 		/// <remarks>
