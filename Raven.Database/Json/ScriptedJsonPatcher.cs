439bde416cd9b771317d8e322c73fbcd348dd8d7
Ayende Rahien <ayende@ayende.com>
2012-09-25 04:56:32 +0200

    Updating patching behavior to work with Jint

--- Raven.Database/Json/ScriptedJsonPatcher.cs
+++ Raven.Database/Json/ScriptedJsonPatcher.cs
@@ -6,6 +6,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using Jint.Native;
+using Raven.Imports.Newtonsoft.Json.Linq;
 using Raven.Json.Linq;
 using System.Reflection;
 using System.IO;
@@ -69,19 +71,42 @@ namespace Raven.Database.Json
 			{
 				foreach (var kvp in patch.Values)
 				{
-					ctx.SetParameter(kvp.Key, kvp.Value);
+					if (kvp.Value is bool)
+					{
+						ctx.SetParameter(kvp.Key, (bool)kvp.Value);
+					}
+					else if (kvp.Value is DateTime)
+					{
+						ctx.SetParameter(kvp.Key, (DateTime)kvp.Value);
+					}
+					else if (kvp.Value is string)
+					{
+						ctx.SetParameter(kvp.Key, (string)kvp.Value);
+					}
+					else if (kvp.Value is int)
+					{
+						ctx.SetParameter(kvp.Key, (int)kvp.Value);
+					}
+					else if (kvp.Value is double)
+					{
+						ctx.SetParameter(kvp.Key, (double)kvp.Value);
+					}
+					else
+					{
+						ctx.SetParameter(kvp.Key, kvp.Value);
+					}
 				}
-
-				ctx.CallFunction("ExecutePatchScript", doc);
+				var jsObject = ToJsObject(ctx.Global, doc);
+				ctx.CallFunction("ExecutePatchScript", jsObject);
 				foreach (var kvp in patch.Values)
 				{
-					ctx.SetParameter(kvp.Key, null);
+					ctx.RemoveParameter(kvp.Key);
 				}
 				OutputLog(ctx);
 
 				scriptsCache.CheckinScript(patch, ctx);
 
-				return doc;
+				return ToRavenJObject(jsObject);
 			}
 			catch (Exception errorEx)
 			{
@@ -95,12 +120,141 @@ namespace Raven.Database.Json
 			}
 		}
 
+		private RavenJObject ToRavenJObject(JsObject jsObject)
+		{
+			var rjo = new RavenJObject();
+			foreach (var key in jsObject.GetKeys())
+			{
+				var jsInstance = jsObject[key];
+				switch (jsInstance.Type)
+				{
+					case JsInstance.CLASS_REGEXP:
+					case JsInstance.CLASS_ERROR:
+					case JsInstance.CLASS_ARGUMENTS:
+					case JsInstance.CLASS_DESCRIPTOR:
+					case JsInstance.CLASS_FUNCTION:
+						continue;
+				}
+				rjo[key] = ToRavenJToken(jsInstance);
+			}
+			return rjo;
+		}
+
+		private RavenJToken ToRavenJToken(JsInstance v)
+		{
+			switch (v.Class)
+			{
+				case JsInstance.TYPE_OBJECT:
+				case JsInstance.CLASS_OBJECT:
+					return ToRavenJObject((JsObject)v);
+				case JsInstance.CLASS_DATE:
+					var dt = (DateTime) v.Value;
+					return new RavenJValue(dt);
+				case JsInstance.TYPE_NUMBER:
+				case JsInstance.CLASS_NUMBER:
+					var num = (double)v.Value;
+					var integer = Math.Truncate(num);
+					if (Math.Abs(num - integer) < double.Epsilon)
+						return new RavenJValue((long)integer);
+					return new RavenJValue(num);
+				case JsInstance.TYPE_STRING:
+				case JsInstance.TYPE_BOOLEAN:
+				case JsInstance.CLASS_STRING:
+				case JsInstance.CLASS_BOOLEAN:
+					return new RavenJValue(v.Value);
+				case JsInstance.CLASS_NULL:
+				case JsInstance.TYPE_NULL:
+					return RavenJValue.Null;
+				case JsInstance.CLASS_UNDEFINED:
+				case JsInstance.TYPE_UNDEFINED:
+					return RavenJValue.Null;
+				case JsInstance.CLASS_ARRAY:
+					var jsArray = ((JsArray)v);
+					var rja = new RavenJArray();
+
+					for (int i = 0; i < jsArray.Length; i++)
+					{
+						var jsInstance = jsArray.get(i);
+						var ravenJToken = ToRavenJToken(jsInstance);
+						if (ravenJToken == null)
+							continue;
+						rja.Add(ravenJToken);
+					}
+					return rja;
+				case JsInstance.CLASS_REGEXP:
+				case JsInstance.CLASS_ERROR:
+				case JsInstance.CLASS_ARGUMENTS:
+				case JsInstance.CLASS_DESCRIPTOR:
+				case JsInstance.CLASS_FUNCTION:
+					return null;
+				default:
+					throw new NotSupportedException(v.Class);
+			}
+		}
+
+		private static JsObject ToJsObject(IGlobal global, RavenJObject doc)
+		{
+			var jsObject = global.ObjectClass.New();
+			foreach (var prop in doc)
+			{
+				var val = ToJsInstance(global, prop.Value);
+				jsObject.DefineOwnProperty(prop.Key, val);
+			}
+			return jsObject;
+		}
+
+		private static JsInstance ToJsInstance(IGlobal global, RavenJToken value)
+		{
+			switch (value.Type)
+			{
+				case JTokenType.Array:
+					return ToJsArray(global, (RavenJArray)value);
+				case JTokenType.Object:
+					return ToJsObject(global, (RavenJObject)value);
+				case JTokenType.Null:
+					return JsNull.Instance;
+				case JTokenType.Boolean:
+					var boolVal = ((RavenJValue)value);
+					return new JsBoolean((bool)boolVal.Value, JsUndefined.Instance);
+				case JTokenType.Float:
+					var fltVal = ((RavenJValue)value);
+					return new JsNumber((double)fltVal.Value, JsUndefined.Instance);
+				case JTokenType.Integer:
+					var intVal = ((RavenJValue)value);
+					if(intVal.Value is int)
+					{
+						return new JsNumber((int)intVal.Value, JsUndefined.Instance);
+					}
+					return new JsNumber((long)intVal.Value, JsUndefined.Instance);
+				case JTokenType.Date:
+					var dtVal = ((RavenJValue)value);
+					return new JsDate((DateTime)dtVal.Value, JsUndefined.Instance);
+				case JTokenType.String:
+					var strVal = ((RavenJValue)value);
+					return new JsString((string)strVal.Value, JsUndefined.Instance);
+				default:
+					throw new NotSupportedException(value.Type.ToString());
+			}
+		}
+
+		private static JsArray ToJsArray(IGlobal global, RavenJArray array)
+		{
+			var jsArr = global.ArrayClass.New();
+			for (int i = 0; i < array.Length; i++)
+			{
+				jsArr.put(i, ToJsInstance(global, array[i]));
+			}
+			return jsArr;
+		}
+
 		internal static JintEngine CreateEngine(ScriptedPatchRequest patch)
 		{
 			AssertValidScript(patch.Script);
 			var wrapperScript = String.Format(@"
-function ExecutePatchScript(doc){{
+function ExecutePatchScript(docInner){{
+  (function(doc){{
 	{0}{1}
+  }}).apply(docInner);
 }};
 ", patch.Script, patch.Script.EndsWith(";") ? String.Empty : ";");
 
@@ -112,13 +266,13 @@ function ExecutePatchScript(doc){{
 
 			jintEngine.Run(GetFromResources("Raven.Database.Json.RavenDB.js"));
 
-			jintEngine.SetFunction("LoadDocumentInternal", ((Func<string, object>) (value =>
+			jintEngine.SetFunction("LoadDocument", ((Func<string, object>)(value =>
 			{
 				var loadedDoc = loadDocumentStatic(value);
 				if (loadedDoc == null)
 					return null;
 				loadedDoc[Constants.DocumentIdFieldName] = value;
-				return loadedDoc.ToString();
+				return ToJsObject(jintEngine.Global, loadedDoc);
 			})));
 
 			jintEngine.Run(wrapperScript);
@@ -128,12 +282,13 @@ function ExecutePatchScript(doc){{
 
 		private void OutputLog(JintEngine engine)
 		{
-			var boxedValue = engine.GetParameter("debug_outputs") as IEnumerable<object>;
-			if (boxedValue == null)
+			var arr = engine.GetParameter("debug_outputs") as JsArray;
+			if (arr == null)
 				return;
-			foreach (var o in boxedValue)
+			for (int i = 0; i < arr.Length; i++)
 			{
-				if(o == null)
+				var o = arr.get(i);
+				if (o == null)
 					continue;
 				Debug.Add(o.ToString());
 			}
