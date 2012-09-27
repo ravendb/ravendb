70933e047cba50b560098b3b3d341a902cf0ed0e
Ayende Rahien <ayende@ayende.com>
2012-09-28 00:23:56 +0200

    Removing the authentication bundle - no longer relevant

--- Bundles/Raven.Bundles.Authentication/UserDatabaseAccess.cs
+++
@@ -1,9 +0,0 @@
-namespace Raven.Bundles.Authentication
-{
-	public class UserDatabaseAccess
-	{
-		public bool ReadOnly { get; set; }
-		public bool Admin { get; set; }
-		public string Name { get; set; }
-	}
-}
