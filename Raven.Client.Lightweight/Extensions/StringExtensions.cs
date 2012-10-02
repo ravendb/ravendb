7b987e706178ab8adafa7cbe104748046d265660
Ayende Rahien <ayende@ayende.com>
2012-10-02 21:53:38 +0200

    white space

--- Raven.Client.Lightweight/Extensions/StringExtensions.cs
+++ Raven.Client.Lightweight/Extensions/StringExtensions.cs
@@ -4,24 +4,24 @@ namespace Raven.Client.Extensions
 {
 	internal static class StringExtensions
 	{
-        public static string ToInvariantString(this int value)
-        {
-            return value.ToString(CultureInfo.InvariantCulture);
-        }
+		public static string ToInvariantString(this int value)
+		{
+			return value.ToString(CultureInfo.InvariantCulture);
+		}
 
-        public static string ToInvariantString(this float value)
-        {
-            return value.ToString(CultureInfo.InvariantCulture);
-        }
+		public static string ToInvariantString(this float value)
+		{
+			return value.ToString(CultureInfo.InvariantCulture);
+		}
 
-        public static string ToInvariantString(this double value)
-        {
-            return value.ToString(CultureInfo.InvariantCulture);
-        }
+		public static string ToInvariantString(this double value)
+		{
+			return value.ToString(CultureInfo.InvariantCulture);
+		}
 
-        public static string ToInvariantString(this decimal value)
-        {
-            return value.ToString(CultureInfo.InvariantCulture);
-        }
+		public static string ToInvariantString(this decimal value)
+		{
+			return value.ToString(CultureInfo.InvariantCulture);
+		}
 	}
 }
