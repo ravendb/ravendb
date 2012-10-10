48b354bd94f52618001fd2c4cc273c3468016920
Damian Hickey <dhickey@gmail.com>
2012-10-10 11:21:30 +0100

    Serve Raven.Studio.xap from embedded resource

--- Raven.Database/Server/Responders/SilverlightUI.cs
+++ Raven.Database/Server/Responders/SilverlightUI.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.IO;
-using System.Linq;
 using Raven.Abstractions.MEF;
-using Raven.Abstractions.Extensions;
 using Raven.Database.Extensions;
 using Raven.Database.Plugins;
 using Raven.Database.Server.Abstractions;
@@ -37,53 +32,12 @@ namespace Raven.Database.Server.Responders
 				return true;
 			});
 
-			var match = urlMatcher.Match(context.GetRequestUrl());
-			var fileName = match.Groups[1].Value;
-			var paths = GetPaths(fileName, Database.Configuration.WebDir);
-			var matchingPath = paths.FirstOrDefault(File.Exists);
-			if (matchingPath != null)
-			{
-				context.WriteFile(matchingPath);
-				return;
-			}
-			context.SetStatusToNotFound();
+			context.WriteEmbeddedFile(Settings.WebDir, "Raven.Studio.xap");
 		}
 
 		public override bool IsUserInterfaceRequest
 		{
 			get { return true; } 
-		}
-
-		public static IEnumerable<string> GetPaths(string fileName, string webDir)
-		{
-			// dev path
-			yield return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Raven.Studio\bin\debug", fileName);
-			// dev path
-			yield return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\Raven.Studio\bin\debug", fileName);
-			//local path
-			yield return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
-			//local path, bin folder
-			yield return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bin", fileName);
-
-			// web ui path
-			yield return Path.Combine(webDir, fileName);
-
-			var options = new[]
-			              	{
-			              		@"..\..\..\packages", // assuming we are in slnDir\Project.Name\bin\debug 		
-			              		@"..\..\packages"
-			              	};
-			foreach (var option in options)
-			{
-				if (Directory.Exists(option) == false)
-					continue;
-				foreach (var dir in Directory.GetDirectories(option, "RavenDB.Embedded*").OrderByDescending(x => x))
-				{
-					var contentDir = Path.Combine(dir, "content");
-					if (Directory.Exists(contentDir))
-						yield return contentDir;
-				}
-			}
 		}
 	}
 }
