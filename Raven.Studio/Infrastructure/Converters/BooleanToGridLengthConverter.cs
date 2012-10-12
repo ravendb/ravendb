99a8b14b33df1500d1dbd74b711393e164a71ed2
samuel_d_jack <sam@seaturtlesoftware.com>
2012-10-12 18:37:48 +0100

    Fixed case RavenDB-608: Improved the UI of the Patch Page in the studio

---
+++ Raven.Studio/Infrastructure/Converters/BooleanToGridLengthConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Raven.Studio.Infrastructure.Converters
+{
+    public class BooleanToGridLengthConverter : IValueConverter
+    {
+            public GridLength ValueWhenTrue { get; set; }
+
+            public GridLength ValueWhenFalse { get; set; }
+
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                return (bool)value == true ? ValueWhenTrue : ValueWhenFalse;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+    }
+}
