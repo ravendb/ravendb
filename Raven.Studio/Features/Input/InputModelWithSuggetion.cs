5ec27ff597d74a74b1bf3615336802ba4b33dd3a
DanielDar <daniel@ayende.com>
2012-10-14 16:48:18 +0200

    base for question window with suggestion

---
+++ Raven.Studio/Features/Input/InputModelWithSuggetion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Raven.Studio.Behaviors;
+
+namespace Raven.Studio.Features.Input
+{
+	public class InputModelWithSuggetion : InputModel, IAutoCompleteSuggestionProvider
+	{
+		private readonly Task<IList<object>> provideSuggestions;
+
+		public InputModelWithSuggetion(Task<IList<object>> provideSuggestions)
+		{
+			this.provideSuggestions = provideSuggestions;
+		}
+
+		public Task<IList<object>> ProvideSuggestions(string enteredText)
+		{
+			return provideSuggestions;
+		}
+	}
+}
