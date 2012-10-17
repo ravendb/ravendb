0caca883d90675e0a7ce9c4c3da2de89109d570f
Ayende Rahien <ayende@ayende.com>
2012-10-17 13:02:49 +0200

    spaces -> tabs

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -7,96 +7,96 @@ using System.Threading;
 
 namespace RavenRepro
 {
-    using System.Messaging;
-    using System.Threading.Tasks;
-    using System.Transactions;
-    using Raven.Client;
-    using Raven.Client.Document;
+	using System.Messaging;
+	using System.Threading.Tasks;
+	using System.Transactions;
+	using Raven.Client;
+	using Raven.Client.Document;
 
-    class Program
-    {
-        const int NumTimeouts = 1000;
-        static DocumentStore store;
+	class Program
+	{
+		const int NumTimeouts = 1000;
+		static DocumentStore store;
 
-        static Guid ResourceManagerId = new Guid("05216603-dd72-4ec5-88b6-0c88e7b74e05");
+		static Guid ResourceManagerId = new Guid("05216603-dd72-4ec5-88b6-0c88e7b74e05");
 
-        static MessageQueue outputQueue;
+		static MessageQueue outputQueue;
 
-        static string queueName = string.Format("FormatName:DIRECT=OS:localhost\\private$\\RavenRepro.Output");
+		static string queueName = string.Format("FormatName:DIRECT=OS:localhost\\private$\\RavenRepro.Output");
 
-        static void Main(string[] args)
-        {
-            store = new DocumentStore { Url = "http://localhost:8080", DefaultDatabase = "RavenRepro" };
-            store.Initialize();
-
-
+		static void Main(string[] args)
+		{
+			store = new DocumentStore { Url = "http://localhost:8080", DefaultDatabase = "RavenRepro" };
+			store.Initialize();
 
 
 
 
-            //if(!MessageQueue.Exists(".\"))
-            //    MessageQueue.Create(queueName, true);
-
-            outputQueue = new MessageQueue(queueName, false, true, QueueAccessMode.Send);
-
-	        Console.WriteLine("Ready");
-
-            string cmd;
-
-            while ((cmd = Console.ReadKey().Key.ToString().ToLower()) != "q")
-            {
-                switch (cmd)
-                {
-                    case "s":
-                        SeedDB();
-                        break;
-                    case "v":
-                        Verify();
-                        break;
-                    default:
-                        DispatchTimeouts();
-                        break;
-
-                }
-            }
 
 
-        }
+			//if(!MessageQueue.Exists(".\"))
+			//    MessageQueue.Create(queueName, true);
 
-        static void Verify()
-        {
-            var numMessagesInQueue = new MessageQueue(queueName).GetAllMessages().Count();
+			outputQueue = new MessageQueue(queueName, false, true, QueueAccessMode.Send);
 
-            Console.Out.WriteLine("Messages in q: {0}", numMessagesInQueue);
+			Console.WriteLine("Ready");
 
-        }
+			string cmd;
 
-        static void DispatchTimeouts()
-        {
-            Console.Out.WriteLine("Getting existing timeouts");
+			while ((cmd = Console.ReadKey().Key.ToString().ToLower()) != "q")
+			{
+				switch (cmd)
+				{
+					case "s":
+						SeedDB();
+						break;
+					case "v":
+						Verify();
+						break;
+					default:
+						DispatchTimeouts();
+						break;
 
-
-            //get existing timeouts to dispatch
-            IEnumerable<string> timeoutIds;
-            do
-            {
-
-                using (var session = OpenSession())
-                {
-                    timeoutIds = session.Query<TimeoutData>().Select(t => t.Id).Take(1024).ToList();
-                }
-
-
-                Console.Out.WriteLine("Found {0} timeouts", timeoutIds.Count());
-	            var random = new Random();
-                //generate duplicates
-	            for (int i = 0; i < 4; i++)
-	            {
-		            timeoutIds = timeoutIds.Concat(timeoutIds.OrderBy(x => random.Next()));
-	            }
+				}
+			}
 
 
-                Console.Out.WriteLine("Dispatching {0} timeouts", timeoutIds.Count());
+		}
+
+		static void Verify()
+		{
+			var numMessagesInQueue = new MessageQueue(queueName).GetAllMessages().Count();
+
+			Console.Out.WriteLine("Messages in q: {0}", numMessagesInQueue);
+
+		}
+
+		static void DispatchTimeouts()
+		{
+			Console.Out.WriteLine("Getting existing timeouts");
+
+
+			//get existing timeouts to dispatch
+			IEnumerable<string> timeoutIds;
+			do
+			{
+
+				using (var session = OpenSession())
+				{
+					timeoutIds = session.Query<TimeoutData>().Select(t => t.Id).Take(1024).ToList();
+				}
+
+
+				Console.Out.WriteLine("Found {0} timeouts", timeoutIds.Count());
+				var random = new Random();
+				//generate duplicates
+				for (int i = 0; i < 4; i++)
+				{
+					timeoutIds = timeoutIds.Concat(timeoutIds.OrderBy(x => random.Next()));
+				}
+
+
+				Console.Out.WriteLine("Dispatching {0} timeouts", timeoutIds.Count());
 				int count = 0;
 				Parallel.ForEach(timeoutIds, (id) =>
 				{
@@ -119,130 +119,130 @@ namespace RavenRepro
 					}
 
 				});
-            } while (timeoutIds.Any());
+			} while (timeoutIds.Any());
 
-            Console.Out.WriteLine("Done dispatching");
+			Console.Out.WriteLine("Done dispatching");
 
-        }
+		}
 
-        static void DispatchTimeout(string id)
-        {
-            using (var tx = new TransactionScope())
-            {
-                ForceDistributedTransaction();
+		static void DispatchTimeout(string id)
+		{
+			using (var tx = new TransactionScope())
+			{
+				ForceDistributedTransaction();
 
-                if (TryRemove(id))
-                    Enqueue(id);
+				if (TryRemove(id))
+					Enqueue(id);
 
-                tx.Complete();
-            }
-        }
+				tx.Complete();
+			}
+		}
 
-        static void Enqueue(string id)
-        {
-            outputQueue.Send(new Message { Label = id }, id, MessageQueueTransactionType.Automatic);
-        }
+		static void Enqueue(string id)
+		{
+			outputQueue.Send(new Message { Label = id }, id, MessageQueueTransactionType.Automatic);
+		}
 
-        static void SeedDB()
-        {
-            Console.Out.WriteLine("Clearing q");
-            new MessageQueue(queueName).Purge();
+		static void SeedDB()
+		{
+			Console.Out.WriteLine("Clearing q");
+			new MessageQueue(queueName).Purge();
 
-            Console.Out.WriteLine("Seeding db");
-            for (int i = 0; i < NumTimeouts; i++)
-            {
-                using (var session = store.OpenSession())
-                {
-                    session.Store(new TimeoutData
-                        {
-                            Dispatched = false
-                        });
+			Console.Out.WriteLine("Seeding db");
+			for (int i = 0; i < NumTimeouts; i++)
+			{
+				using (var session = store.OpenSession())
+				{
+					session.Store(new TimeoutData
+						{
+							Dispatched = false
+						});
 
-                    session.SaveChanges();
-                }
-            }
+					session.SaveChanges();
+				}
+			}
 
-            Console.Out.WriteLine("Seeding done");
+			Console.Out.WriteLine("Seeding done");
 
-        }
+		}
 
-        static bool TryRemove(string timeoutId)
-        {
-            using (var session = OpenSession())
-            {
-                var timeoutData = session.Load<TimeoutData>(timeoutId);
+		static bool TryRemove(string timeoutId)
+		{
+			using (var session = OpenSession())
+			{
+				var timeoutData = session.Load<TimeoutData>(timeoutId);
 
-                if (timeoutData == null)
-                    return false;
+				if (timeoutData == null)
+					return false;
 
-                timeoutData.Dispatched = true;
-                session.SaveChanges();
+				timeoutData.Dispatched = true;
+				session.SaveChanges();
 
-                session.Delete(timeoutData);
-                session.SaveChanges();
+				session.Delete(timeoutData);
+				session.SaveChanges();
 
-                return true;
-            }
-        }
+				return true;
+			}
+		}
 
-        static IDocumentSession OpenSession()
-        {
-            var session = store.OpenSession();
+		static IDocumentSession OpenSession()
+		{
+			var session = store.OpenSession();
 
-            session.Advanced.AllowNonAuthoritativeInformation = false;
-            session.Advanced.UseOptimisticConcurrency = true;
+			session.Advanced.AllowNonAuthoritativeInformation = false;
+			session.Advanced.UseOptimisticConcurrency = true;
 
-            return session;
-        }
+			return session;
+		}
 
-        static void ForceDistributedTransaction()
-        {
-            Transaction.Current.EnlistDurable(ResourceManagerId, new DummyEnlistmentNotification(),
-                                              EnlistmentOptions.None);
-        }
-    }
+		static void ForceDistributedTransaction()
+		{
+			Transaction.Current.EnlistDurable(ResourceManagerId, new DummyEnlistmentNotification(),
+											  EnlistmentOptions.None);
+		}
+	}
 
-    internal class TimeoutData
-    {
-        public string Id { get; set; }
-        public bool Dispatched { get; set; }
-    }
+	internal class TimeoutData
+	{
+		public string Id { get; set; }
+		public bool Dispatched { get; set; }
+	}
 
-    public class DummyEnlistmentNotification : IEnlistmentNotification
-    {
-        public static readonly Guid Id = Guid.NewGuid();
+	public class DummyEnlistmentNotification : IEnlistmentNotification
+	{
+		public static readonly Guid Id = Guid.NewGuid();
 
-        static Random rand = new Random();
+		static Random rand = new Random();
 
-        public bool WasCommitted { get; set; }
+		public bool WasCommitted { get; set; }
 
-        public bool RandomRollback { get; set; }
+		public bool RandomRollback { get; set; }
 
-        public void Prepare(PreparingEnlistment preparingEnlistment)
-        {
+		public void Prepare(PreparingEnlistment preparingEnlistment)
+		{
 
 
-            if (RandomRollback && rand.Next(0, 10) > 7)
-                preparingEnlistment.ForceRollback();
-            else
-                preparingEnlistment.Prepared();
-        }
+			if (RandomRollback && rand.Next(0, 10) > 7)
+				preparingEnlistment.ForceRollback();
+			else
+				preparingEnlistment.Prepared();
+		}
 
-        public void Commit(Enlistment enlistment)
-        {
-            WasCommitted = true;
-            enlistment.Done();
+		public void Commit(Enlistment enlistment)
+		{
+			WasCommitted = true;
+			enlistment.Done();
 
-        }
+		}
 
-        public void Rollback(Enlistment enlistment)
-        {
-            enlistment.Done();
-        }
+		public void Rollback(Enlistment enlistment)
+		{
+			enlistment.Done();
+		}
 
-        public void InDoubt(Enlistment enlistment)
-        {
-            enlistment.Done();
-        }
-    }
+		public void InDoubt(Enlistment enlistment)
+		{
+			enlistment.Done();
+		}
+	}
 }
