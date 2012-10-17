cecdd612d39d9e544860c04b22f81518ca38cf48
DanielDar <daniel@ayende.com>
2012-10-17 09:24:31 +0200

    Revert "Test now fails faster"
    This reverts commit a135b854084f59532e7ca0dbed9a1470e2bffbb6.
    
    Conflicts:
    
    	Raven.Tryouts/Program.cs

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -1,12 +1,137 @@
-﻿// -----------------------------------------------------------------------
-//  <copyright file="Program.cs" company="Hibernating Rhinos LTD">
-//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
-//  </copyright>
-// -----------------------------------------------------------------------
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Security.Principal;
+using System.Threading;
+using Raven.Abstractions.Data;
+using Raven.Client.Document;
+using Raven.Database.Extensions;
+using Raven.Database.Util;
+using Raven.Json.Linq;
+using Raven.Munin;
+using Raven.Tests.Bugs;
+
 namespace Raven.Tryouts
 {
-	public class Program
+	internal class Program
 	{
-		 
+		private static void Main()
+		{
+			var store = new DocumentStore
+			{
+				Url = "https://1.ravenhq.com",
+				ApiKey = "adc19257-fc90-4027-a40d-9dbac1542ce8",
+				DefaultDatabase = "hibernating-rhinos-anothera",
+				Conventions =
+				{
+					FailoverBehavior = FailoverBehavior.FailImmediately
+				}
+			};
+
+			store.Initialize();
+
+			store.DatabaseCommands.Put("users/1", null, new RavenJObject(), new RavenJObject());
+		}
+
+		private static void UseMyData()
+		{
+			using (var d = new MyData(new MemoryPersistentSource()))
+			{
+				using (d.BeginTransaction())
+				{
+					d.Documents.Put(new RavenJObject
+						{
+							{"key", "items/1"},
+							{"id", "items/1"},
+							{"etag", Guid.NewGuid().ToByteArray()},
+						}, new byte[0]);
+					d.Commit();
+				}
+
+				using (d.BeginTransaction())
+				{
+					d.Documents.Put(new RavenJObject
+						{
+							{"key", "items/1"},
+							{"id", "items/1"},
+							{"etag", Guid.NewGuid().ToByteArray()},
+							{"txId", "1234"}
+						}, new byte[0]);
+					d.Transactions.Put(new RavenJObject
+						{
+							{"id", "1234"},
+						}, new byte[0]);
+
+					d.Commit();
+				}
+
+				ThreadPool.QueueUserWorkItem(state =>
+					{
+						d.BeginTransaction();
+						Table.ReadResult readResult = d.Documents.Read(new RavenJObject {{"key", "items/1"}});
+						var txId = readResult.Key.Value<string>("txId");
+
+						Table.ReadResult txResult = d.Transactions.Read(new RavenJObject {{"id", txId}});
+
+						if (txResult == null)
+						{
+							Environment.Exit(1);
+							return;
+						}
+
+						d.Transactions.Remove(txResult.Key);
+
+						var x = ((RavenJObject) readResult.Key.CloneToken());
+						x.Remove("txId");
+
+						d.Documents.UpdateKey(x);
+						d.CommitCurrentTransaction();
+					});
+
+
+				while (true)
+				{
+					using (d.BeginTransaction())
+					{
+						Table.ReadResult readResult = d.Documents.Read(new RavenJObject {{"key", "items/1"}});
+						var txId = readResult.Key.Value<string>("txId");
+
+						if (txId == null)
+						{
+							return;
+						}
+
+						Table.ReadResult txResult = d.Transactions.Read(new RavenJObject {{"id", txId}});
+						if (txResult == null)
+						{
+							Environment.Exit(1);
+							return;
+						}
+
+						d.Commit();
+					}
+				}
+			}
+		}
+	}
+
+	public class MyData : Munin.Database
+	{
+		public MyData(IPersistentSource persistentSource)
+			: base(persistentSource)
+		{
+			Documents = Add(new Table(x => x.Value<string>("key"), "Documents")
+				{
+					{"ByKey", x => x.Value<string>("key")},
+					{"ById", x => x.Value<string>("id")},
+					{"ByEtag", x => new ComparableByteArray(x.Value<byte[]>("etag"))}
+				});
+
+			Transactions = Add(new Table(x => x.Value<string>("txId"), "Transactions"));
+		}
+
+		public Table Transactions { get; set; }
+
+		public Table Documents { get; set; }
 	}
 }
