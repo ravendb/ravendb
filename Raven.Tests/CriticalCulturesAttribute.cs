80ad7fd73ae4cd7465ac0635f16e732340f9b1c9
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-10-29 16:21:06 +0200

    Upgrade xUnit package

--- Raven.Tests/CriticalCulturesAttribute.cs
+++ Raven.Tests/CriticalCulturesAttribute.cs
@@ -8,18 +8,18 @@ using Xunit.Extensions;
 namespace Raven.Tests
 {
 	[CLSCompliant(false)]
-	public class CriticalCulturesAttribute: DataAttribute
+	public class CriticalCulturesAttribute : DataAttribute
 	{
 		public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
 		{
 			var cultures = new[]
-			{ 
+			{
 				CultureInfo.InvariantCulture,
 				CultureInfo.CurrentCulture,
 				CultureInfo.GetCultureInfo("NL"), // Uses comma instead of point: 12,34
 				CultureInfo.GetCultureInfo("tr-TR"), // "The Turkey Test"
 			};
-			return cultures.Select(c => new object[] { c });
+			return cultures.Select(c => new object[] {c});
 		}
 	}
 }
