135c6e671858eab30f41b7c30cb3f5978409108a
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-10-29 15:29:33 +0200

    Add a failing test

---
+++ Raven.Tests/Linq/Not.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Raven.Tests.Linq
+{
+	public class Not : RavenTest
+	{
+		private class TestDoc
+		{
+			public string SomeProperty { get; set; }
+		}
+
+		[Fact]
+		public void CanQueryWithNot()
+		{
+			using (var store = NewDocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					session.Store(new TestDoc {SomeProperty = "NOT"});
+					session.SaveChanges();
+				}
+
+				using (var session = store.OpenSession())
+				{
+					Assert.NotNull(session.Query<TestDoc>().FirstOrDefault(doc => doc.SomeProperty == "NOT"));
+				}
+			}
+		}
+	}
+}
