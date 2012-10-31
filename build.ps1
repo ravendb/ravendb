9700ee47146caa2eee71d1c3ce2cca2ade3dfa19
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-10-31 16:53:15 +0200

    Ensure that psake package is downloaded and exists so we can start the build.

--- build.ps1
+++ build.ps1
@@ -5,6 +5,7 @@ $scriptDir = Split-Path $scriptPath
 
 get-module psake | remove-module
 
+.nuget\NuGet.exe install .nuget\packages.config -OutputDirectory packages
 import-module (Get-ChildItem "$scriptDir\packages\psake.*\tools\psake.psm1" | Select-Object -First 1)
 
 invoke-psake "$scriptDir\default.ps1" $task
