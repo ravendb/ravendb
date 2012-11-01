1d2c1ed8a13dc543f0bdbaffb7e8634c06b7da57
Ayende Rahien <ayende@ayende.com>
2012-11-01 17:08:57 +0200

    Adding passing test

---
+++ Raven.Tests/Issues/RavenDB542 .cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using FizzWare.NBuilder;
+using Raven.Abstractions.Data;
+using Raven.Client;
+using Raven.Client.Indexes;
+using Xunit;
+
+namespace Raven.Tests.Issues
+{
+	public class RavenDB542 : RavenTest
+	{
+		[Fact]
+		public void MapWithMinValueComparison()
+		{
+			using (var store = NewDocumentStore())
+			{
+				new OrganizationIndex().Execute(store);
+
+				using (var session = store.OpenSession())
+				{
+					var orgs = Builder<Organization>.CreateListOfSize(10)
+						.All()
+						.Random(10)
+						.Build();
+
+					foreach (var org in orgs)
+						session.Store(org);
+					session.SaveChanges();
+
+					store.DatabaseCommands.Patch("organizations/1",
+					                             new[]
+					                             {
+						                             new PatchRequest
+						                             {
+							                             Type = PatchCommandType.Set,
+							                             Name = "DateApproved",
+							                             Value = "2012-09-07T09:41:42.9893269"
+						                             }
+					                             });
+
+				
+					WaitForIndexing(store);
+
+					Assert.Empty(store.DocumentDatabase.Statistics.Errors);
+				}
+			}
+		}
+
+		class Organization
+		{
+			public int Id { get; set; }
+			public string Name { get; set; }
+			public DateTime DateApproved { get; set; }
+		}
+
+		class OrganizationIndex : AbstractIndexCreationTask<Organization, OrganizationIndex.Result>
+		{
+			public class Result
+			{
+				public bool IsApproved { get; set; }
+			}
+
+			public OrganizationIndex()
+			{
+				Map = orgs => orgs.Select(org => new
+				{
+					org.Name,
+					IsApproved = org.DateApproved == DateTime.MinValue
+				});
+			}
+		}
+	}
+}
