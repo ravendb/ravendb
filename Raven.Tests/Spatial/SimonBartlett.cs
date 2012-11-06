b2713f95b596f3c381c5e59717e34f3cccb93e89
Ayende Rahien <ayende@ayende.com>
2012-11-06 11:59:49 +0200

    Adding pure lucene sample

--- Raven.Tests/Spatial/SimonBartlett.cs
+++ Raven.Tests/Spatial/SimonBartlett.cs
@@ -1,54 +1,97 @@
 ﻿using System.Linq;
+using Lucene.Net.Analysis;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Lucene.Net.Spatial.Prefix.Tree;
+using Lucene.Net.Spatial.Queries;
+using Lucene.Net.Store;
 using Raven.Abstractions.Indexing;
 using Raven.Client.Embedded;
 using Raven.Client.Indexes;
+using Raven.Database.Indexing;
 using Raven.Json.Linq;
+using Spatial4n.Core.Context.Nts;
 using Xunit;
 
 namespace Raven.Tests.Spatial
 {
-    public class SimonBartlett
-    {
-        [Fact]
-        public void LineStringsShouldIntersect()
-        {
-            using (var store = new EmbeddableDocumentStore { RunInMemory = true })
-            {
-                store.Initialize();
-                store.ExecuteIndex(new GeoIndex());
+	public class SimonBartlett : RavenTest
+	{
+		[Fact]
+		public void PureLucene()
+		{
+			using (var dir = new RAMDirectory())
+			{
+				using (var keywordAnalyzer = new KeywordAnalyzer())
+				using (var writer = new IndexWriter(dir, keywordAnalyzer, true, IndexWriter.MaxFieldLength.UNLIMITED))
+				{
+					var doc = new Lucene.Net.Documents.Document();
 
-                using (var session = store.OpenSession())
-                {
-                    session.Store(new GeoDocument { WKT = "LINESTRING (0 0, 1 1, 2 1)" });
-                    session.SaveChanges();
-                }
+					var writeShape = NtsSpatialContext.GEO_KM.ReadShape("LINESTRING (0 0, 1 1, 2 1)");
+					var writeStrategy = SpatialIndex.CreateStrategy("WKT", SpatialSearchStrategy.GeohashPrefixTree, GeohashPrefixTree.GetMaxLevelsPossible());
+					foreach (var f in writeStrategy.CreateIndexableFields(writeShape))
+					{
+						doc.Add(f);
+					}
+					writer.AddDocument(doc);
+					writer.Commit();
+				}
 
-                using (var session = store.OpenSession())
-                {
-                    var matches = session.Query<RavenJObject, GeoIndex>()
-                        .Customize(x =>
-                        {
-                            x.RelatesToShape("WKT", "LINESTRING (1 0, 1 1, 1 2)", SpatialRelation.Intersects);
-                            x.WaitForNonStaleResults();
-                        }).Any();
 
-                    Assert.True(matches);
-                }
-            }
-        }
+				var shape = NtsSpatialContext.GEO_KM.ReadShape("LINESTRING (1 0, 1 1, 1 2)");
+				SpatialArgs args = new SpatialArgs(SpatialOperation.BBoxIntersects, shape);
+				var strategy = SpatialIndex.CreateStrategy("WKT", SpatialSearchStrategy.GeohashPrefixTree, GeohashPrefixTree.GetMaxLevelsPossible());
+				var makeQuery = strategy.MakeQuery(args);
+				using(var search = new IndexSearcher(dir))
+				{
+					var topDocs = search.Search(makeQuery, 5);
+					Assert.Equal(1, topDocs.TotalHits);
+				}
+			}
+		}
 
-        public class GeoDocument
-        {
-            public string WKT { get; set; }
-        }
+		[Fact]
+		public void LineStringsShouldIntersect()
+		{
+			using (var store = new EmbeddableDocumentStore { RunInMemory = true })
+			{
+				store.Initialize();
+				store.ExecuteIndex(new GeoIndex());
 
-        public class GeoIndex : AbstractIndexCreationTask<GeoDocument>
-        {
-            public GeoIndex()
-            {
-                Map = docs => from doc in docs
-                              select new { _ = SpatialGenerate("WKT", doc.WKT) };
-            }
-        }
-    }
+				using (var session = store.OpenSession())
+				{
+					session.Store(new GeoDocument { WKT = "LINESTRING (0 0, 1 1, 2 1)" });
+					session.SaveChanges();
+				}
+
+				WaitForIndexing(store);
+
+				using (var session = store.OpenSession())
+				{
+					var matches = session.Query<RavenJObject, GeoIndex>()
+						.Customize(x =>
+						{
+							x.RelatesToShape("WKT", "LINESTRING (1 0, 1 1, 1 2)", SpatialRelation.Intersects);
+							x.WaitForNonStaleResults();
+						}).Any();
+
+					Assert.True(matches);
+				}
+			}
+		}
+
+		public class GeoDocument
+		{
+			public string WKT { get; set; }
+		}
+
+		public class GeoIndex : AbstractIndexCreationTask<GeoDocument>
+		{
+			public GeoIndex()
+			{
+				Map = docs => from doc in docs
+							  select new { _ = SpatialGenerate("WKT", doc.WKT) };
+			}
+		}
+	}
 }
