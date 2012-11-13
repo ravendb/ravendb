4cb8daffb98a373a2c05ef70956d3f18107a49a1
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-11-13 09:22:51 +0200

    minor

--- Raven.Tests/MailingList/Lars.cs
+++ Raven.Tests/MailingList/Lars.cs
@@ -27,18 +27,18 @@ namespace Raven.Tests.MailingList
 			public Index()
 			{
 				Map = items =>
-					  from item in items
-					  select new
-					  {
-						  item.Age
-					  };
+				      from item in items
+				      select new
+				      {
+					      item.Age
+				      };
 
 				Reduce = results =>
-						 from r in results
-						 group r by 1
-							 into g
-							 let items = g.ToArray()
-							 select new { Age = items.Sum(x => x.Age) };
+				         from r in results
+				         group r by 1
+				         into g
+				         let items = g.ToArray()
+				         select new {Age = items.Sum(x => x.Age)};
 			}
 		}
 
@@ -51,7 +51,6 @@ namespace Raven.Tests.MailingList
 				var indexDefinition = s.DocumentDatabase.IndexDefinitionStorage.GetIndexDefinition("Index");
 				Assert.Contains("Enumerable.ToArray(g)", indexDefinition.Reduce);
 				Assert.Contains("Enumerable.Sum", indexDefinition.Reduce);
-				
 			}
 		}
 	}
