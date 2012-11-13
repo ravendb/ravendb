1aa7320540ca01730e8dd90ceb14d441e7bb0cb7
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-11-13 13:07:58 +0200

    Create a new MVC/WebAPI project which will be used for Raven.Sample.MvcIntegration

---
+++ Raven.Sample.MvcIntegration/App_Start/WebApiConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace Raven.Sample.MvcIntegration
+{
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
+}
---
+++ Raven.Sample.MvcIntegration/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Raven.Sample.MvcIntegration
+{
+	public class WebApiApplication : HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+
+			WebApiConfig.Register(GlobalConfiguration.Configuration);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+		}
+	}
+}
