978a7152e6405d6bf7debf83bb4ce56e80929afa
Ayende Rahien <ayende@ayende.com>
2012-11-13 13:19:35 +0200

    Starting to move to a mode that require admin access to systems db

--- Raven.Database/Server/Responders/Databases.cs
+++ Raven.Database/Server/Responders/Databases.cs
@@ -19,26 +19,34 @@ namespace Raven.Database.Server.Responders
 
 		public override void Respond(IHttpContext context)
 		{
-			switch (context.Request.HttpMethod)
+			if (EnsureSystemDatabase(context) == false)
+				return;
+
+			// This responder is NOT secured, and anyone can access it.
+			// Because of that, we need to provide explicit security here.
+			
+			// Anonymous Access - All / Get
+			// Show all dbs
+
+			// Anonymous Access - None
+			// Show only the db that you have access to (read / read-write / admin)
+
+			// If admin, show all dbs
+
+			Guid lastDocEtag = Guid.Empty;
+			Database.TransactionalStorage.Batch(accessor =>
 			{
-				case "GET":
-					Guid lastDocEtag = Guid.Empty;
-					Database.TransactionalStorage.Batch(accessor =>
-					{
-						lastDocEtag = accessor.Staleness.GetMostRecentDocumentEtag();
-					});
+				lastDocEtag = accessor.Staleness.GetMostRecentDocumentEtag();
+			});
 
-					if (context.MatchEtag(lastDocEtag))
-					{
-						context.SetStatusToNotModified();
-					}
-					else
-					{
-						context.WriteHeaders(new RavenJObject(), lastDocEtag);
-
-						context.WriteJson(Database.GetDocumentsWithIdStartingWith("Raven/Databases/", null, context.GetStart(), context.GetPageSize(Database.Configuration.MaxPageSize)));
-					}
-					break;
+			if (context.MatchEtag(lastDocEtag))
+			{
+				context.SetStatusToNotModified();
+			}
+			else
+			{
+				context.WriteHeaders(new RavenJObject(), lastDocEtag);
+				context.WriteJson(Database.GetDocumentsWithIdStartingWith("Raven/Databases/", null, context.GetStart(), context.GetPageSize(Database.Configuration.MaxPageSize)));
 			}
 		}
 	}
