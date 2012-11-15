8983fe51eeb9ceb4da0b74500587f80eead83d9a
Ayende Rahien <ayende@ayende.com>
2012-11-15 12:42:38 +0200

    Better errors

--- Raven.Client.Lightweight/Document/DocumentStore.cs
+++ Raven.Client.Lightweight/Document/DocumentStore.cs
@@ -441,6 +441,8 @@ namespace Raven.Client.Document
 
 		public void InitializeProfiling()
 		{
+			if (jsonRequestFactory == null)
+				throw new InvalidOperationException("Cannot call InitializeProfiling() before Initialize() was called.");
 			Conventions.DisableProfiling = false;
 			jsonRequestFactory.LogRequest += (sender, args) =>
 			{
