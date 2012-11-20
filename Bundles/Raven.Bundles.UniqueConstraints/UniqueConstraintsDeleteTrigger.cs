1986b3acbd64acd29b5880b75211e40b6ec71fcd
David Pfeffer <bytenik@bytenik.com>
2012-11-20 10:43:45 -0500

    UniqueConstraints now handles arrays by item rather than one big blob

--- Bundles/Raven.Bundles.UniqueConstraints/UniqueConstraintsDeleteTrigger.cs
+++ Bundles/Raven.Bundles.UniqueConstraints/UniqueConstraintsDeleteTrigger.cs
@@ -3,6 +3,8 @@
 	using Abstractions.Data;
 	using Database.Plugins;
 	using Json.Linq;
+    using System.Collections.Generic;
+    using System.Linq;
 
 	public class UniqueConstraintsDeleteTrigger : AbstractDeleteTrigger
 	{
@@ -20,16 +22,25 @@
 
 			var entityName = metadata.Value<string>(Constants.RavenEntityName) + "/";
 
-			var uniqueConstraits = metadata.Value<RavenJArray>(Constants.EnsureUniqueConstraints);
+			var uniqueConstraints = metadata.Value<RavenJArray>(Constants.EnsureUniqueConstraints);
 
-			if (uniqueConstraits == null)
+			if (uniqueConstraints == null)
 				return;
 
-			foreach (var property in uniqueConstraits)
+            foreach (var property in uniqueConstraints) // each unique constraint property
 			{
-				var checkKey = "UniqueConstraints/" + entityName  + property + "/" + doc.DataAsJson.Value<string>(property.Value<string>());
+			    var propName = property.Value<string>(); // the name of the constraint property
+			    IEnumerable<string> checkKeys;
 
-				Database.Delete(checkKey, null, transactionInformation);
+			    var prefix = "UniqueConstraints/" + entityName + property + "/"; // UniqueConstraints/EntityNamePropertyName/
+			    var prop = doc.DataAsJson[propName];
+			    if (prop is RavenJArray)
+			        checkKeys = ((RavenJArray) prop).Select(p => p.Value<string>());
+			    else
+			        checkKeys = new[] {prop.Value<string>()};
+
+			    foreach(var checkKey in checkKeys)
+                    Database.Delete(prefix+checkKey, null, transactionInformation);
 			}
 		}
 	}
