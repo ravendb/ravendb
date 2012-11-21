63b73b33c253885622fe494eaaf20c948f24a35c
Ayende Rahien <ayende@ayende.com>
2012-11-21 16:52:09 +0200

    Refactoring & optimizing the way we are handling auth when we are using windows auth Making sure that user names are NOT case sensitive

--- Raven.Database/Server/Security/Windows/WindowsRequestAuthorizer.cs
+++ Raven.Database/Server/Security/Windows/WindowsRequestAuthorizer.cs
@@ -63,40 +63,47 @@ namespace Raven.Database.Server.Security.Windows
 				onRejectingRequest();
 				return false;
 			}
+			
+			var databaseName = database().Name ?? Constants.SystemDatabase;
 			PrincipalWithDatabaseAccess user = null;
 			if(userCreated)
 			{
 				user = (PrincipalWithDatabaseAccess)ctx.User;
-			}
-
-			var databaseName = database().Name ?? Constants.SystemDatabase;
-
-			if (userCreated && (user.Principal.IsAdministrator() || user.AdminDatabases.Contains(databaseName)))
-				return true;
-
-			var httpRequest = ctx.Request;
-
-			if (server.SystemConfiguration.AnonymousUserAccessMode == AnonymousUserAccessMode.Get &&
-				userCreated &&
-				user.ReadWriteDatabases.Contains(databaseName) == false &&
-				IsGetRequest(httpRequest.HttpMethod, httpRequest.Url.AbsolutePath) == false)
-			{
-				onRejectingRequest();
-				return false;
-			}
-
-			if (IsGetRequest(httpRequest.HttpMethod, httpRequest.Url.AbsolutePath) &&
-				userCreated &&
-				(user.ReadOnlyDatabases.Contains(databaseName) || user.ReadWriteDatabases.Contains(databaseName)))
-				return true;
-
-			if (userCreated)
-			{
 				CurrentOperationContext.Headers.Value[Constants.RavenAuthenticatedUser] = ctx.User.Identity.Name;
 				CurrentOperationContext.User.Value = ctx.User;
+				
+				// admins always go through
+				if(user.Principal.IsAdministrator())
+					return true;
 			}
+			
 
-			return true;
+			var httpRequest = ctx.Request;
+			bool isGetRequest = IsGetRequest(httpRequest.HttpMethod, httpRequest.Url.AbsolutePath);
+			switch (server.SystemConfiguration.AnonymousUserAccessMode)
+			{
+				case AnonymousUserAccessMode.All:
+					return true; // if we have, doesn't matter if we have / don't have the user
+				case AnonymousUserAccessMode.Get:
+					if (isGetRequest)
+						return true;
+					goto case AnonymousUserAccessMode.None;
+				case AnonymousUserAccessMode.None:
+					if (userCreated)
+					{
+						if (user.AdminDatabases.Contains(databaseName))
+							return true;
+						if (user.ReadWriteDatabases.Contains(databaseName))
+							return true;
+						if (isGetRequest && user.ReadOnlyDatabases.Contains(databaseName))
+							return true;
+					}
+					
+					onRejectingRequest();
+					return false;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
 		}
 
 		private bool TryCreateUser(IHttpContext ctx, out Action onRejectingRequest)
@@ -156,7 +163,7 @@ namespace Raven.Database.Server.Security.Windows
 		{
 			var databaseAccessLists = requiredUsers
 				.Where(data => ctx.User.Identity.Name.Equals(data.Name, StringComparison.InvariantCultureIgnoreCase))
-				.ToDictionary(source => source.Name, source => source.Databases);
+				.ToDictionary(source => source.Name, source => source.Databases, StringComparer.InvariantCultureIgnoreCase);
 
 			foreach (var windowsAuthData in requiredGroups.Where(data => ctx.User.IsInRole(data.Name)))
 			{
