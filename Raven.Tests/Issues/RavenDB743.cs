9b97b4a8af788df394f162cd2b6b0df24853c899
Ayende Rahien <ayende@ayende.com>
2012-11-25 01:41:46 +0200

    Trying for a better test

--- Raven.Tests/Issues/RavenDB743.cs
+++ Raven.Tests/Issues/RavenDB743.cs
@@ -13,8 +13,11 @@ namespace Raven.Tests.Issues
 	{
 		public class User
 		{
+			public string Id { get; set; }
 			public string Country { get; set; }
+			public int Items { get; set; }
 		}
+
 		public class UsersByCountry : AbstractIndexCreationTask<User, UsersByCountry.Result>
 		{
 			public class Result
@@ -26,8 +29,33 @@ namespace Raven.Tests.Issues
 			public UsersByCountry()
 			{
 				Map = users =>
+				      from user in users
+				      select new {user.Country, Count = 1};
+				Reduce = results =>
+				         from result in results
+				         group result by result.Country
+				         into g
+				         select new
+				         {
+					         Country = g.Key,
+					         Count = g.Sum(x => x.Count)
+				         };
+			}
+		}
+
+		public class ItemssByCountry : AbstractIndexCreationTask<User, UsersByCountry.Result>
+		{
+			public class Result
+			{
+				public string Country { get; set; }
+				public int Count { get; set; }
+			}
+
+			public ItemssByCountry()
+			{
+				Map = users =>
 					  from user in users
-					  select new { user.Country, Count = 1 };
+					  select new { user.Country, Count = user.Items };
 				Reduce = results =>
 						 from result in results
 						 group result by result.Country
@@ -75,30 +103,25 @@ namespace Raven.Tests.Issues
 			{
 				using (var session = store.OpenSession())
 				{
-					session.Store(new User { Country = "Israel" });
-					for (int i = 0; i < 10; i++)
-					{
-						session.Store(new User { Country = "ISRAEL" });
-						session.Store(new User { Country = "israel" });
-					}
-					session.Store(new User { Country = "Israel" });
-					
+					session.Store(new User { Id = "orders/5057", Country = "ISRAEL", Items = 7 });
+					session.Store(new User { Id = "orders/5046", Country = "ISRAEL", Items = 3 });
+					session.Store(new User { Id = "orders/4627", Country = "israel", Items = 4 });
+
 					session.SaveChanges();
 				}
 
-				new UsersByCountry().Execute(store);
+				new ItemssByCountry().Execute(store);
 
 				using (var session = store.OpenSession())
 				{
-					var r = session.Query<UsersByCountry.Result, UsersByCountry>()
+					var r = session.Query<ItemssByCountry.Result, ItemssByCountry>()
 						   .Customize(x => x.WaitForNonStaleResults())
 						   .ToList()
-						   .OrderBy(x=>x.Country)
+						   .OrderBy(x => x.Country)
 						   .ToList();
-					Assert.Equal(3, r.Count);
-					Assert.Equal(10, r[0].Count);
-					Assert.Equal(2, r[1].Count);
-					Assert.Equal(10, r[2].Count);
+					Assert.Equal(2, r.Count);
+					Assert.Equal(4, r[0].Count);
+					Assert.Equal(10, r[1].Count);
 				}
 			}
 		}
