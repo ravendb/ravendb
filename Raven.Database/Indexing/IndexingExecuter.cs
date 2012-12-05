2518823a5e241859814611d0b59095f5056d681b
Ayende Rahien <ayende@ayende.com>
2012-12-05 16:50:02 +0200

    More work on stopping just indexing in time

--- Raven.Database/Indexing/IndexingExecuter.cs
+++ Raven.Database/Indexing/IndexingExecuter.cs
@@ -323,7 +323,7 @@ namespace Raven.Database.Indexing
 				{
 					var jsonDocuments = GetJsonDocuments(nextEtag);
 					int localWork = workCounter;
-					while (jsonDocuments.Results.Length == 0 && context.DoWork)
+					while (jsonDocuments.Results.Length == 0 && context.DoWork && context.DoIndexing)
 					{
 						futureBatchStat.Retries++;
 
