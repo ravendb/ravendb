5970bc09a660f70009746fc83bfecdb1439168a2
Ayende Rahien <ayende@ayende.com>
2012-12-20 10:25:00 +0200

    Adding a way to simulate a non trivial work load

---
+++ Raven.SimulatedWorkLoad/Indexes/Users_Search.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="Users_Search.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using System.Linq;
+using Raven.Client.Indexes;
+using Raven.SimulatedWorkLoad.Model;
+
+namespace Raven.SimulatedWorkLoad.Indexes
+{
+	public class Users_Search : AbstractIndexCreationTask<User>
+	{
+		public Users_Search()
+		{
+			Map = users =>
+				  from user in users
+				  select new
+				  {
+					  Query = new object[] { user.First, user.Last, user.Email, user.Email.Split('@'), user.Phone }
+				  };
+		}
+	}
+}
