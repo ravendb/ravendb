52ab1cd1e19651dffbf84c12030ff9a2a9341753
Ayende Rahien <ayende@ayende.com>
2012-12-20 06:42:58 +0200

    Fixing migration, will setup the default value properly for reduce_keys_status

--- Raven.Database/Storage/Esent/SchemaCreator.cs
+++ Raven.Database/Storage/Esent/SchemaCreator.cs
@@ -13,7 +13,7 @@ namespace Raven.Storage.Esent
 	[CLSCompliant(false)]
 	public class SchemaCreator
 	{
-		public const string SchemaVersion = "4.1";
+		public const string SchemaVersion = "4.2";
 		private readonly Session session;
 
 		public SchemaCreator(Session session)
