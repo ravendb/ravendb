5d8e28e9600ca5764e68383059a7a4f7caf3f56e
Ayende Rahien <ayende@ayende.com>
2012-12-26 23:38:30 +0200

    Removing SystemTime usage

--- Raven.Tests/Bugs/MapRedue/VersionedDocuments.cs
+++ Raven.Tests/Bugs/MapRedue/VersionedDocuments.cs
@@ -16,10 +16,6 @@ namespace Raven.Tests.Bugs.MapRedue
 			Map = aDocuments =>
 			      from document in aDocuments
 			      from version in document.Versions
-			      where (
-			            	(document.DateRemoved == null ||
-							 document.DateRemoved >= SystemTime.UtcNow) 
-			            )
 			      select new
 			      {
 			      	document.Id,
