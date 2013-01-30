9fcec3f7c0d21aaf49b0b6cb46715192483cb860
Ayende Rahien <ayende@ayende.com>
2013-01-30 16:27:57 +0200

    Default to quad tree spatial implementation (with customize spatial extensions) to avoid very costly spatial indexing

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Lucene.Net.Analysis.Tokenattributes;
+using Lucene.Net.Spatial.Prefix.Tree;
 using Raven.Abstractions.Exceptions;
 using Raven.Client.Document;
+using Raven.Database.Indexing.Spatial;
 using Raven.Json.Linq;
 using Rhino.Licensing;
+using Spatial4n.Core.Context.Nts;
+using Spatial4n.Core.Io;
 
 namespace BulkStressTest
 {
@@ -15,8 +20,24 @@ namespace BulkStressTest
 		private static void Main()
 		{
 			//Repro();
-
-			var normalizeLineEnding = NormalizeLineEnding("test\rtest\ntest\r\ntest");
+			var ctx = new NtsSpatialContext(true);
+			var startegy = new RecursivePrefixTreeStrategyThatSupportsWithin(new QuadPrefixTree(ctx, 9), "test");
+			var poly = @"POLYGON ((-110.947642 55.333333, -109.644025 55.333333, -109.373584 55.253002, -109.132938 55.146591, -108.929565 55.017525, -108.769563 54.869919, -108.657484 54.708433, -108.596239 54.538106, -108.587078 54.364188, -108.629626 54.191974, -108.721976 54.026641, -108.860817 53.873103, -109.041589 53.735872, -109.258661 53.618939, -109.505505 53.525678, -109.77489 53.45876, -110.059063 53.420091, -110.34994 53.410769, -110.639287 53.431058, -110.918901 53.480383, -111.180801 53.557342, -111.417406 53.659739, -111.621724 53.784634, -111.787539 53.928413, -111.909595 54.086879, -111.983776 54.255356, -112.007276 54.428817, -111.978744 54.602021, -111.898399 54.769672, -111.768106 54.926578, -111.591391 55.067822, -111.373396 55.188926, -111.120753 55.286017, -110.947642 55.333333))";
+			var shapeReaderWriter = new NtsShapeReadWriter(ctx);
+			var shape = shapeReaderWriter.ReadShape(poly);
+			var indexableFields = startegy.CreateIndexableFields(shape);
+			int c = 0;
+			var tokenStreamValue = indexableFields[0].TokenStreamValue;
+			var hash = new HashSet<string>();
+			while (tokenStreamValue.IncrementToken())
+			{
+				var termAttribute = (ITermAttribute) tokenStreamValue.GetAttribute<ITermAttribute>();
+				Console.WriteLine(termAttribute.Term);
+				hash.Add(termAttribute.Term);
+				c++;
+			}
+			Console.WriteLine(c);
+			Console.WriteLine(hash.Count);
 		}
 
 		private static string NormalizeLineEnding(string script)
