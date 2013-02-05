507de05b4d0b9d8eb5b174f98c2da8dceac21f92
Ayende Rahien <ayende@ayende.com>
2013-02-05 22:56:55 +0200

    Fixing test (order wasn't properly set)

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using Raven.Client;
 using Raven.Client.Document;
 using Raven.Client.Shard;
+using Raven.Tests.Faceted;
 
 internal class Program
 {
 	private static void Main(string[] args)
 	{
+		for (int i = 0; i < 100; i++)
+		{
+			Console.Clear();
+			Console.WriteLine(i);
+			using (var x = new FacetedIndexLimit())
+			{
+				x.CanPerformFacetedLimitSearch_HitsAsc();
+			}
+		}
 	}
 }
