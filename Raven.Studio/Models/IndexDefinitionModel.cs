cf3afed19e3df990f16e7d45d2e897c00d80990e
Samuel Jack <sam@seaturtlesoftware.com>
2013-02-21 11:21:54 +0000

    Tidied up wording on some of the messages

--- Raven.Studio/Models/IndexDefinitionModel.cs
+++ Raven.Studio/Models/IndexDefinitionModel.cs
@@ -148,7 +148,7 @@ namespace Raven.Studio.Models
 		{
 			if (string.IsNullOrWhiteSpace(name) == false)
 			{
-				var notification = new Notification(string.Format("Could not find '{0}' index", name), NotificationLevel.Warning);
+				var notification = new Notification(string.Format("Could not find index '{0}'", name), NotificationLevel.Warning);
 				ApplicationModel.Current.AddNotification(notification);
 			}
 			UrlUtil.Navigate("/indexes");
@@ -462,8 +462,8 @@ namespace Raven.Studio.Models
 
 				if (index.IsNewIndex == false && index.OriginalName != index.Name)
 				{
-					if (AskUser.Confirmation("Can not rename and index",
-						                     "If you wish to save a new index with this new name press OK, to cancel the save command press Cancel") ==false)
+					if (AskUser.Confirmation("Indexes cannot be renamed",
+						                     "If you continue, a new index will be created with this name.") ==false)
 					{
 						ApplicationModel.Current.Notifications.Add(new Notification("Index Not Saved"));
 						return;
@@ -484,12 +484,12 @@ namespace Raven.Studio.Models
 					index.Maps.RemoveAt(mapIndexes[i]);
 				}
 
-				ApplicationModel.Current.AddNotification(new Notification("saving index " + index.Name));
+				ApplicationModel.Current.AddNotification(new Notification("Saving index " + index.Name));
 				DatabaseCommands.PutIndexAsync(index.Name, index.index, true)
 					.ContinueOnSuccess(() =>
 										   {
 											   ApplicationModel.Current.AddNotification(
-												   new Notification("index " + index.Name + " saved"));
+												   new Notification("Index " + index.Name + " saved"));
 											   index.hasUnsavedChanges = false;
 											   PutIndexNameInUrl(index.Name);
 										   })
@@ -514,9 +514,9 @@ namespace Raven.Studio.Models
 
 			public override void Execute(object parameter)
 			{
-				ApplicationModel.Current.AddNotification(new Notification("resetting index " + index.Name));
+				ApplicationModel.Current.AddNotification(new Notification("Resetting index " + index.Name));
 				index.ResetToOriginal();
-				ApplicationModel.Current.AddNotification(new Notification("index " + index.Name + " was reset"));
+				ApplicationModel.Current.AddNotification(new Notification("Index " + index.Name + " was reset"));
 			}
 		}
 
@@ -536,7 +536,7 @@ namespace Raven.Studio.Models
 
 			public override void Execute(object parameter)
 			{
-				AskUser.ConfirmationAsync("Confirm Delete", "Really delete '" + index.Name + "' index?")
+				AskUser.ConfirmationAsync("Confirm Delete", "Are you sure you want to delete index '" + index.Name + "'?")
 					.ContinueWhenTrue(DeleteIndex);
 			}
 
@@ -548,11 +548,11 @@ namespace Raven.Studio.Models
 														{
 															if (t.IsFaulted)
 															{
-																ApplicationModel.Current.AddErrorNotification(t.Exception, "index " + index.Name + " could not be deleted");
+																ApplicationModel.Current.AddErrorNotification(t.Exception, "Index " + index.Name + " could not be deleted");
 															}
 															else
 															{
-																ApplicationModel.Current.AddInfoNotification("index " + index.Name + " successfully deleted");
+																ApplicationModel.Current.AddInfoNotification("Index '" + index.Name + "' successfully deleted");
 																UrlUtil.Navigate("/indexes");
 															}
 														});
