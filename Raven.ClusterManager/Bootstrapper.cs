8d3402b6c5a34f796a44695373a66909b8edd254
Fitzchak Yitzchaki <fitzchak@ayende.com>
2013-02-26 12:51:00 +0200

    Add index.html and some third party client frameworks that we're going to use: jQuery and Angular.

---
+++ Raven.ClusterManager/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="Bootstrapper.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using Nancy;
+
+namespace Raven.ClusterManager
+{
+	public class Bootstrapper : DefaultNancyBootstrapper
+	{
+		 
+	}
+}
