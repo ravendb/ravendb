daf6fc458985f3dc19fc85f2ebf33ba3a126cbbc
Ayende Rahien <ayende@ayende.com>
2013-02-26 14:32:04 +0200

    Fixing race condition in test

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using Raven.Abstractions.Data;
 using Raven.Client.Document;
-using System.Linq;
+using Raven.Client.Linq;
+using Raven.Client.Indexes;
+using Raven.Tests.Issues;
 
 namespace Raven.Tryouts
 {
@@ -11,47 +14,16 @@ namespace Raven.Tryouts
 	{
 		static void Main(string[] args)
 		{
-			for (int i = 0; i < 2; i++)
+			for (int i = 0; i < 200; i++)
 			{
-				using (var store = new DocumentStore
+				Console.Clear();
+				Console.WriteLine(i);
+				using(var x = new RavenDB_406())
 				{
-					Url = "http://localhost:8080",
-					DefaultDatabase = "test",
-				}.Initialize())
-				{
-					var list = new List<string>();
-					var sp = Stopwatch.StartNew();
-					int start = 0;
-					while (true)
-					{
-						var result = store.DatabaseCommands.Query("PersonList", new IndexQuery
-						{
-							FieldsToFetch = new[] { Constants.DocumentIdFieldName },
-							PageSize = 1024,
-							Start = start
-						}, null);
-						if (result.Results.Count == 0)
-							break;
-						start += result.Results.Count;
-						list.AddRange(result.Results.Select(x => x.Value<string>(Constants.DocumentIdFieldName)));
-					}
-					sp.Stop();
-
-					Console.WriteLine("Read all ids {0:#,#} in {1:#,#} ms", list.Count, sp.ElapsedMilliseconds);
-
-					var rand = new Random();
-					list.Sort((s, s1) => rand.Next(-1, 1));
-					sp.Restart();
-
-					foreach (var id in list)
-					{
-						store.DatabaseCommands.Get(id);
-					}
-
-					sp.Stop();
-					Console.WriteLine("Read all docs {0:#,#} in {1:#,#} ms", list.Count, sp.ElapsedMilliseconds);
+					x.ShouldServeFromCacheIfThereWasNoChange();
 				}
 			}
-		} 
+		}
 	}
+
 }
