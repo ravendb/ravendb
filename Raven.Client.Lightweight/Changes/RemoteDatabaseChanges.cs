c1b5d380d331744e6262b818f8be1695f4987a59
Arkadiusz Palinski <arek@ais.pl>
2013-02-27 13:19:36 +0100

    RavenDB-752 Replication informer - if timeout exception was detected retry the operation once. If the second timeout occurred then throw and do not try the remaining replicas.

--- Raven.Client.Lightweight/Changes/RemoteDatabaseChanges.cs
+++ Raven.Client.Lightweight/Changes/RemoteDatabaseChanges.cs
@@ -85,8 +85,8 @@ namespace Raven.Client.Changes
 										if (disposed)
 											return task;
 
-
-										if (replicationInformer.IsServerDown(task.Exception) == false)
+										bool timeout;
+										if (replicationInformer.IsServerDown(task.Exception, out timeout) == false)
 											return task;
 
 										if(replicationInformer.IsHttpStatus(task.Exception, 
