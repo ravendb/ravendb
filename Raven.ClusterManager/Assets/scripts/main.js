0a9672d2b7501423f1103aff5d6b59d6db0cbe70
Fitzchak Yitzchaki <fitzchak@ayende.com>
2013-02-27 13:11:40 +0200

    Add the main view

--- Raven.ClusterManager/Assets/scripts/main.js
+++ Raven.ClusterManager/Assets/scripts/main.js
@@ -1,1 +1,13 @@
-﻿
+﻿/// <reference path="vendor/angular/angular.js" />
+/*global angular:false */
+'use strict';
+
+var clusterManagerApp = angular.module('ClusterManagerApp');
+
+clusterManagerApp.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: '/views/main.html',
+        controller: 'MainCtrl'
+    });
+    $routeProvider.otherwise({ redirectTo: '/' });
+});
