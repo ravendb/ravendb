1ed8be9aeb74b5500ed90ccf7080fae7cb967f95
Daniel <daniel@ayende.com>
2013-03-12 14:24:03 +0200

    RavenDB-979 "Forbidden" error still being raised in Management Studio in unstable build 2295

--- Raven.Database/Server/Responders/Databases.cs
+++ Raven.Database/Server/Responders/Databases.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Principal;
 using Raven.Abstractions.Data;
 using Raven.Database.Server.Abstractions;
 using Raven.Database.Extensions;
@@ -41,15 +42,15 @@ namespace Raven.Database.Server.Responders
 
 			if (server.SystemConfiguration.AnonymousUserAccessMode == AnonymousUserAccessMode.None)
 			{
-				if(server.RequestAuthorizer.Authorize(context) == false)
+				var user = server.RequestAuthorizer.GetUser(context);
+				if(user == null)
 				{
 					return;
 				}
 
-
-				if (context.User.IsAdministrator(server.SystemConfiguration.AnonymousUserAccessMode) == false)
+				if (user.IsAdministrator(server.SystemConfiguration.AnonymousUserAccessMode) == false)
 				{
-					approvedDatabases = server.RequestAuthorizer.GetApprovedDatabases(context);
+					approvedDatabases = server.RequestAuthorizer.GetApprovedDatabases(user, context);
 				}
 			}
 
