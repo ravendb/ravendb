23ef23c54ec7e07e302c980147285dfc17e1d9fd
Ayende Rahien <ayende@ayende.com>
2013-03-13 19:58:30 +0200

    Making sure that we can run this in sharded context as well

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using Raven.Abstractions.Data;
 using Raven.Client.Document;
 using System.Linq;
+using Raven.Tests.Bundles.Replication.Bugs;
 
 namespace Raven.Tryouts
 {
@@ -12,8 +13,12 @@ namespace Raven.Tryouts
 	{
 		static void Main(string[] args)
 		{
-			var x = Path.GetDirectoryName(@"F:\");
-			Console.WriteLine(x);
+			for (int i = 0; i < 100; i++)
+			{
+				Console.WriteLine(i);
+				using (var x = new HiLoHanging())
+					x.HiLo_Modified_InReplicated_Scenario();
+			}
 		} 
 	}
 }
