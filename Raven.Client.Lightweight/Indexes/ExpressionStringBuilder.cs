9cc1598ca7137d764de6e76cbfb80812d6793c99
Ayende Rahien <ayende@ayende.com>
2013-03-16 08:30:00 +0200

    Fixing AmbiguousMatchException

--- Raven.Client.Lightweight/Indexes/ExpressionStringBuilder.cs
+++ Raven.Client.Lightweight/Indexes/ExpressionStringBuilder.cs
@@ -264,7 +264,8 @@ namespace Raven.Client.Indexes
 
 		private string GetPropertyName(string name, Type exprType)
 		{
-			var propertyInfo = exprType.GetProperty(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+			var propertyInfo = exprType.GetProperty(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly) ??
+							   exprType.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).First(x => x.Name == name);
 			if (propertyInfo != null)
 			{
 				foreach (var customAttribute in propertyInfo.GetCustomAttributes(true))
