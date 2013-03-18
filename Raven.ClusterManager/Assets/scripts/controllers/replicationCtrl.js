e5c2f1dd5a2a1d1bad12ec4fae4c88dabeb42cf1
Fitzchak Yitzchaki <fitzchak@ayende.com>
2013-03-18 16:06:50 +0200

    More work on the replication diagram

--- Raven.ClusterManager/Assets/scripts/controllers/replicationCtrl.js
+++ Raven.ClusterManager/Assets/scripts/controllers/replicationCtrl.js
@@ -4,22 +4,59 @@
 /*global angular:false */
 'use strict';
 
-clusterManagerApp.controller('ReplicationCtrl', function serverCtrl($scope) {
+clusterManagerApp.controller('ReplicationCtrl', function replicationCtrl($scope, $http) {
     
     var graph = new Graph();
-    
-    var nodes = [];
-    for (var i = 0; i < 50; i++) {
-        nodes.push(graph.newNode({ label: 'DB #' + i }));
-    }
 
-    for (var i = 0; i < nodes.length; i++) {
-        var index = Math.floor((Math.random() * nodes.length));
-        var index2 = Math.floor((Math.random() * nodes.length));
-        var node1 = nodes[index];
-        var node2 = nodes[index2];
-        graph.newEdge(node1, node2, { color: '#EB6841' });
-    }
+    var timeoutPromise;
+    $scope.getReplicationDatabases = function () {
+        $http.get('/api/replication').success(function (result) {
+            var replicationDatabases;
+            $scope.replicationDatabases = replicationDatabases = result;
+            
+            var nodes = [];
+            for (var i = 0; i < replicationDatabases.length; i++) {
+                var replicationDatabase = replicationDatabases[i];
+                nodes.push(graph.newNode({
+                    label: replicationDatabase.serverId.replace('serverRecords/', '') + "/" + replicationDatabase.name,
+                    url: replicationDatabase.serverUrl,
+                    database: replicationDatabase.name
+                }));
+            }
+
+            for (var i = 0; i < replicationDatabases.length; i++) {
+                var replicationDatabase = replicationDatabases[i];
+
+                for (var j = 0; j < replicationDatabase.replicationDestinations.length; j++) {
+                    var replicationDestination = replicationDatabase.replicationDestinations[j];
+                    var node1 = nodes.filter(function (item) {
+                        return item.data.url.replace(/\/+$/, '') == replicationDatabase.serverUrl.replace(/\/+$/, '') &&
+                               item.data.database == replicationDatabase.name;
+                    })[0];
+                    var node2 = nodes.filter(function(item) {
+                        return item.data.url.replace(/\/+$/, '') == replicationDestination.url.replace(/\/+$/, '');
+                    })[0];
+                    graph.newEdge(node1, node2, { color: '#EB6841' });
+                }
+            }
+        });
+
+        // timeoutPromise = $timeout($scope.getStats, 5000);
+    };
+    $scope.getReplicationDatabases();
+    
+    /*_.forEach($scope.replicationDatabases, function (value, index, array) {
+        value.cssClass = '';
+        if (value.isOnline) {
+            if (value.isUnauthorized) {
+                value.cssClass += ' warning';
+            } else {
+                value.cssClass += ' success';
+            }
+        } else {
+            value.cssClass += ' error';
+        }
+    });*/
 
     $scope.graph = graph;
 });
