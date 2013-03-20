1be03feb2cad540428630e6396712f9c9fd97a2a
Pawel Pekrol <ppekrol@ais.pl>
2013-03-20 09:21:49 +0100

    adjustments to default spatial maxtreelevel for geohash and quadtree

--- Raven.Abstractions/Indexing/SpatialOptions.cs
+++ Raven.Abstractions/Indexing/SpatialOptions.cs
@@ -17,10 +17,10 @@
 		public double MinY { get; set; }
 		public double MaxY { get; set; }
 
-		// about 2 meters, should be good enough (see: http://unterbahn.com/2009/11/metric-dimensions-of-geohash-partitions-at-the-equator/)
+		// about 4.78 meters at equator, should be good enough (see: http://unterbahn.com/2009/11/metric-dimensions-of-geohash-partitions-at-the-equator/)
 		public const int DefaultGeohashLevel = 9;
-		// about 1 meter, should be good enough
-		public const int DefaultQuadTreeLevel = 25;
+		// about 4.78 meters at equator, should be good enough
+		public const int DefaultQuadTreeLevel = 23;
 	}
 
 	public enum SpatialFieldType
