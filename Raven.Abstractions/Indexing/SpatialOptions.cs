f1e444ff36bb15ca2b67ba39cc881e3d9ac7483c
Simon Bartlett <simon.bartlett@me.com>
2013-03-20 08:37:13 -0400

    Re-adding equality methods to SpatialOptions

--- Raven.Abstractions/Indexing/SpatialOptions.cs
+++ Raven.Abstractions/Indexing/SpatialOptions.cs
@@ -2,6 +2,11 @@
 {
 	public class SpatialOptions
 	{
+		// about 4.78 meters at equator, should be good enough (see: http://unterbahn.com/2009/11/metric-dimensions-of-geohash-partitions-at-the-equator/)
+		public const int DefaultGeohashLevel = 9;
+		// about 4.78 meters at equator, should be good enough
+		public const int DefaultQuadTreeLevel = 23;
+
 		public SpatialOptions()
 		{
 			Type = SpatialFieldType.Geography;
@@ -17,10 +22,33 @@
 		public double MinY { get; set; }
 		public double MaxY { get; set; }
 
-		// about 4.78 meters at equator, should be good enough (see: http://unterbahn.com/2009/11/metric-dimensions-of-geohash-partitions-at-the-equator/)
-		public const int DefaultGeohashLevel = 9;
-		// about 4.78 meters at equator, should be good enough
-		public const int DefaultQuadTreeLevel = 23;
+		protected bool Equals(SpatialOptions other)
+		{
+			return Type == other.Type && Strategy == other.Strategy && MaxTreeLevel == other.MaxTreeLevel && MinX.Equals(other.MinX) && MaxX.Equals(other.MaxX) && MinY.Equals(other.MinY) && MaxY.Equals(other.MaxY);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != this.GetType()) return false;
+			return Equals((SpatialOptions)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				int hashCode = (int)Type;
+				hashCode = (hashCode * 397) ^ (int)Strategy;
+				hashCode = (hashCode * 397) ^ MaxTreeLevel;
+				hashCode = (hashCode * 397) ^ MinX.GetHashCode();
+				hashCode = (hashCode * 397) ^ MaxX.GetHashCode();
+				hashCode = (hashCode * 397) ^ MinY.GetHashCode();
+				hashCode = (hashCode * 397) ^ MaxY.GetHashCode();
+				return hashCode;
+			}
+		}
 	}
 
 	public enum SpatialFieldType
