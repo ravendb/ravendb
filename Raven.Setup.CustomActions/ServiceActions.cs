9c0904f04ccbac3b01fadf25ee81c4eded503b61
Arkadiusz Palinski <arek@ais.pl>
2013-03-27 10:16:13 +0100

    RavenDB-825 Added port availability check option, changed default port from * to 8080

---
+++ Raven.Setup.CustomActions/ServiceActions.cs
@@ -0,0 +1,42 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ServiceActions.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using System.Linq;
+using System.Net.NetworkInformation;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace Raven.Setup.CustomActions
+{
+	public class ServiceActions
+	{
+		[CustomAction]
+		public static ActionResult CheckPortAvailability(Session session)
+		{
+			int port;
+
+			if (int.TryParse(session["SERVICE_PORT"], out port))
+			{
+				var activeTcpListeners = IPGlobalProperties
+				.GetIPGlobalProperties()
+				.GetActiveTcpListeners();
+
+				if (activeTcpListeners.All(endPoint => endPoint.Port != port))
+				{
+					session["PORT_AVAILABILITY"] = "Port available";
+				}
+				else
+				{
+					session["PORT_AVAILABILITY"] = "Port in use";
+				}
+			}
+			else
+			{
+				session["PORT_AVAILABILITY"] = "Invalid port format";
+			}
+
+			return ActionResult.Success;
+		} 
+	}
+}
