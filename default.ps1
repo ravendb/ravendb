120dc0d470bce492ba5135d3b5d94cb92c8ac06d
Fitzchak Yitzchaki <fitzchak@ayende.com>
2013-04-17 09:44:25 +0300

    Add a dummy xap file using the build script instead of including it in the source control

--- default.ps1
+++ default.ps1
@@ -98,6 +98,8 @@ task Init -depends Verify40, Clean {
 
 task Compile -depends Init {
 	
+	"Dummy file so msbuild knows there is one here before embedding as resource." | Out-File "$base_dir\Raven.Database\Server\WebUI\Raven.Studio.xap"
+	
 	$v4_net_version = (ls "$env:windir\Microsoft.NET\Framework\v4.0*").Name
 	
 	exec { &"C:\Windows\Microsoft.NET\Framework\$v4_net_version\MSBuild.exe" "$base_dir\Utilities\Raven.ProjectRewriter\Raven.ProjectRewriter.csproj" /p:OutDir="$buildartifacts_dir\" }
