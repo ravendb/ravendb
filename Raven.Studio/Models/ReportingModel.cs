f226810023e7b377070913ca89b65e2150fe36ee
Samuel Jack <sam@seaturtlesoftware.com>
2013-05-09 13:00:13 +0100

    Tidied up loading of index fields

--- Raven.Studio/Models/ReportingModel.cs
+++ Raven.Studio/Models/ReportingModel.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Microsoft.Expression.Interactivity.Core;
 using Raven.Abstractions.Data;
+using Raven.Abstractions.Util;
 using Raven.Client.Connection.Async;
 using Raven.Client.Linq;
 using Raven.Studio.Controls;
+using Raven.Studio.Extensions;
 using Raven.Studio.Features.Documents;
 using Raven.Studio.Infrastructure;
-using Raven.Studio.Messages;
 using Raven.Abstractions.Extensions;
+using Notification = Raven.Studio.Messages.Notification;
 
 namespace Raven.Studio.Models
 {
@@ -27,28 +31,10 @@ namespace Raven.Studio.Models
 			PageTitle = "Reporting";
 			ModelUrl = "/reporting";
 			AvailableIndexes = new BindableCollection<string>(x => x);
-			AvailableCollections = new BindableCollection<string>(s => s);
-			ParamsForSelectedCollection = new ObservableCollection<string>();
+			IndexFields = new BindableCollection<string>(x => x);
+            DocumentProperties = new BindableCollection<string>(x => x);
 			Aggregations = new BindableCollection<AggregationData>(data => data);
             Results = new ObservableCollection<ReportRow>();
-		}
-
-		private string selectedCollection;
-		public string SelectedCollection
-		{
-			get { return selectedCollection; }
-			set
-			{
-				if (string.IsNullOrWhiteSpace(value))
-					return;
-
-
-				selectedCollection = value;
-				UpdateProperties();
-				Aggregations.Clear();
-				OnPropertyChanged(() => Aggregations);
-				OnPropertyChanged(() => SelectedCollection);
-			}
 		}
 
 		private string indexName;
@@ -77,21 +63,6 @@ namespace Raven.Studio.Models
 
 				OnPropertyChanged(() => IndexName);
 			}
-		}
-
-		private void UpdateAvailableCollections()
-		{
-			ApplicationModel.Current.Server.Value.SelectedDatabase.Value.AsyncDatabaseCommands.GetTermsCount(
-				CollectionsIndex, "Tag", "", 100)
-				.ContinueOnSuccessInTheUIThread(collections =>
-				{
-					AvailableCollections.Clear();
-					AvailableCollections.AddRange(collections.OrderByDescending(x => x.Count)
-												  .Where(x => x.Count > 0)
-												  .Select(col => col.Name).ToList());
-
-					OnPropertyChanged(() => AvailableCollections);
-				});
 		}
 
 		private static void NavigateToIndexesList()
@@ -131,8 +102,10 @@ namespace Raven.Studio.Models
 		{
 			var urlParser = new UrlParser(parameters);
 
+            Aggregations.Clear();
+            Aggregations.Add(new AggregationData());
+
 			UpdateAvailableIndexes();
-			UpdateAvailableCollections();
 
 			var newIndexName = urlParser.Path.Trim('/');
 
@@ -150,25 +123,53 @@ namespace Raven.Studio.Models
 			DatabaseCommands.GetIndexAsync(IndexName)
 				.ContinueOnUIThread(task =>
 				{
-					if (!task.IsFaulted && task.Result != null) 
-						return;
+                    if (task.IsFaulted || task.Result == null)
+                    {
+                        if (AvailableIndexes.Any())
+                        {
+                            NavigateToIndexReport(AvailableIndexes.FirstOrDefault());
+                        }
+                        else
+                        {
+                            NavigateToIndexesList();
+                        }
 
-					if (AvailableIndexes.Any())
-						NavigateToIndexReport(AvailableIndexes.FirstOrDefault());
-					else
-						NavigateToIndexesList();
+                        return;
+                    }
+
+                    IndexFields.Match(task.Result.Fields);
 				}).Catch();
+
+		    DatabaseCommands.QueryAsync(IndexName, new IndexQuery() { Start = 0, PageSize = 5}, null)
+		                    .ContinueOnSuccessInTheUIThread(result =>
+		                    {
+		                        var properties = DocumentHelpers.GetPropertiesFromJObjects(result.Results,
+		                                                                                   includeNestedProperties: false,
+		                                                                                   includeMetadata: false)
+                                                                                           .ToList();
+		                        DocumentProperties.Match(properties);
+		                    }
+		        ).CatchIgnore();
 		}
 
-		private void UpdateProperties()
-		{
-			DatabaseCommands.GetIndexAsync(IndexName).ContinueOnSuccessInTheUIThread(index =>
-			{
-				var matchingProperties = index.Fields;
+        protected override void OnViewLoaded()
+        {
+            base.OnViewLoaded();
 
-				ParamsForSelectedCollection = new ObservableCollection<string>(matchingProperties);
-			});	
-		}
+            Database.ObservePropertyChanged()
+                            .TakeUntil(Unloaded)
+                            .Subscribe(_ =>
+                            {
+                                UpdateAvailableIndexes();
+
+                                Database.Value.Statistics.ObservePropertyChanged()
+                                        .TakeUntil(
+                                            Database.ObservePropertyChanged().Select(__ => Unit.Default).Amb(Unloaded))
+                                        .Subscribe(__ => UpdateAvailableIndexes());
+                            });
+
+            UpdateAvailableIndexes();
+        }
 
 		public ICommand DeleteAggregationCommand{get{return new ActionCommand(DeleteAggregation);}}
 
@@ -203,56 +204,76 @@ namespace Raven.Studio.Models
 	            });
 	        }
 
-            Results.Clear();
+	        Results.Clear();
 
 
-            var progressWindow = new ProgressWindow() { Title = "Preparing Report", IsIndeterminate = true, CanCancel = false};
-            progressWindow.Show();
-
-	        var facetResults =
-	            await
-	            DatabaseCommands.GetFacetsAsync(IndexName, new IndexQuery(), AggregationQuery.GetFacets(facets), 0, 512);
-
-            // there's a bug in silverlight where if a ChildWindow gets closed too soon after it's opened, it leaves the UI
-            // disabled; so delay closing the window by a few milliseconds
-            TaskEx.Delay(TimeSpan.FromMilliseconds(350))
-                  .ContinueOnSuccessInTheUIThread(progressWindow.Close);
-
-            if (facetResults.Results.Count < 1)
-            {
-                return;
-            }
-
-	        var facetResult = facetResults.Results.FirstOrDefault().Value;
-	        var aggregation = data[0];
-
-	        foreach (var facetValue in facetResult.Values)
+	        var progressWindow = new ProgressWindow()
 	        {
-	            var result = new ReportRow {Key = facetValue.Range};
+	            Title = "Preparing Report",
+	            IsIndeterminate = true,
+	            CanCancel = false
+	        };
+	        progressWindow.Show();
 
-                foreach (var facetAggregation in new[] { FacetAggregation.Count, FacetAggregation.Min, FacetAggregation.Max, FacetAggregation.Sum, FacetAggregation.Average})
+	        try
+	        {
+	            var facetResults =
+	                await
+	                DatabaseCommands.GetFacetsAsync(IndexName, new IndexQuery(), AggregationQuery.GetFacets(facets), 0, 512);
+
+
+	            if (facetResults.Results.Count < 1)
 	            {
-	                if ((aggregation.FacetAggregation & facetAggregation) == facetAggregation)
-	                {
-                        result.Values.Add(facetAggregation.ToString().ToUpper() + " of " + aggregation.CalculateOn, facetValue.GetAggregation(facetAggregation) ?? 0);
-	                }
+	                return;
 	            }
 
-	            Results.Add(result);
+	            var facetResult = facetResults.Results.FirstOrDefault().Value;
+	            var aggregation = data[0];
+
+	            foreach (var facetValue in facetResult.Values)
+	            {
+	                var result = new ReportRow {Key = facetValue.Range};
+
+	                foreach (
+	                    var facetAggregation in
+	                        new[]
+	                        {
+	                            FacetAggregation.Count, FacetAggregation.Min, FacetAggregation.Max, FacetAggregation.Sum,
+	                            FacetAggregation.Average
+	                        })
+	                {
+	                    if ((aggregation.FacetAggregation & facetAggregation) == facetAggregation)
+	                    {
+	                        result.Values.Add(facetAggregation.ToString().ToUpper() + " of " + aggregation.CalculateOn,
+	                                          facetValue.GetAggregation(facetAggregation) ?? 0);
+	                    }
+	                }
+
+	                Results.Add(result);
+	            }
+
+	            var keys = Results.SelectMany(r => r.Values.Keys).Distinct();
+
+	            var columns = new ColumnsModel();
+
+	            columns.Columns.Add(new ColumnDefinition()
+	            {
+	                Header = "Key",
+	                Binding = "Key"
+	            });
+
+	            columns.Columns.AddRange(
+	                keys.Select(k => new ColumnDefinition() {Header = k, Binding = "Values[" + k + "]"}));
+
+	            ResultColumns = columns;
 	        }
-
-	        var keys = Results.SelectMany(r => r.Values.Keys).Distinct();
-
-	        var columns = new ColumnsModel();
-
-            columns.Columns.Add(new ColumnDefinition()
-            {
-                Header = "Key", Binding = "Key"
-            });
-
-            columns.Columns.AddRange(keys.Select(k => new ColumnDefinition() { Header = k, Binding = "Values[" + k + "]"}));
-
-	        ResultColumns = columns;
+	        finally
+	        {
+	            // there's a bug in silverlight where if a ChildWindow gets closed too soon after it's opened, it leaves the UI
+	            // disabled; so delay closing the window by a few milliseconds
+	            TaskEx.Delay(TimeSpan.FromMilliseconds(350))
+	                  .ContinueOnSuccessInTheUIThread(progressWindow.Close);
+	        }
 	    }
 
 	    public ColumnsModel ResultColumns
@@ -263,8 +284,8 @@ namespace Raven.Studio.Models
         }
 
         public ObservableCollection<ReportRow> Results { get; private set; } 
-	    public BindableCollection<string> AvailableCollections { get; set; }
-		public ObservableCollection<string> ParamsForSelectedCollection { get; set; }
+	    public BindableCollection<string> DocumentProperties { get; private set; }
+		public BindableCollection<string> IndexFields { get; private set; }
 		public BindableCollection<AggregationData> Aggregations { get; set; }
 		public ICommand AddAggregation
 		{
