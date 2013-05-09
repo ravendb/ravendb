e57e7caf5a00540d11804eb8cb27c7985b2a3130
Samuel Jack <sam@seaturtlesoftware.com>
2013-05-09 11:46:03 +0100

    Working on RavenDB-1087: got basic results showing for reporting

--- Raven.Studio/Models/ReportingModel.cs
+++ Raven.Studio/Models/ReportingModel.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Microsoft.Expression.Interactivity.Core;
 using Raven.Abstractions.Data;
 using Raven.Client.Connection.Async;
 using Raven.Client.Linq;
+using Raven.Studio.Features.Documents;
 using Raven.Studio.Infrastructure;
 using Raven.Studio.Messages;
+using Raven.Abstractions.Extensions;
 
 namespace Raven.Studio.Models
 {
@@ -25,6 +28,7 @@ namespace Raven.Studio.Models
 			AvailableCollections = new BindableCollection<string>(s => s);
 			ParamsForSelectedCollection = new ObservableCollection<string>();
 			Aggregations = new BindableCollection<AggregationData>(data => data);
+            Results = new ObservableCollection<ReportRow>();
 		}
 
 		private string selectedCollection;
@@ -46,7 +50,8 @@ namespace Raven.Studio.Models
 		}
 
 		private string indexName;
-		public string IndexName
+	    private ColumnsModel resultColumns;
+	    public string IndexName
 		{
 			get
 			{
@@ -173,38 +178,81 @@ namespace Raven.Studio.Models
 			OnPropertyChanged(() => Aggregations);
 		}
 
-		public ICommand ExecuteReportCommand{get{return new ActionCommand(ExecuteReport);}}
+		public ICommand ExecuteReportCommand{get{return new AsyncActionCommand(ExecuteReport);}}
 
-		private void ExecuteReport()
-		{
-			var data = Aggregations.Where(aggregationData => aggregationData.HasData()).ToList();
-			if (data.Count == 0)
-			{
-				ApplicationModel.Current.Notifications.Add(new Notification("No Aggregation with valid data found"));
-				return;
-			}
+	    private async Task ExecuteReport()
+	    {
+	        var data = Aggregations.Where(aggregationData => aggregationData.HasData()).ToList();
+	        if (data.Count == 0)
+	        {
+	            ApplicationModel.Current.Notifications.Add(new Notification("No Aggregation with valid data found"));
+	            return;
+	        }
 
-			var facets = new List<AggregationQuery>();
+	        var facets = new List<AggregationQuery>();
 
-			foreach (var aggregationData in data)
-			{
-				facets.Add(new AggregationQuery
-				{
-					Name = aggregationData.AggregateOn,
-					AggregationField = aggregationData.CalculateOn,
-					Aggregation = aggregationData.FacetAggregation
-				});
-			}
+	        foreach (var aggregationData in data)
+	        {
+	            facets.Add(new AggregationQuery
+	            {
+	                Name = aggregationData.AggregateOn,
+	                AggregationField = aggregationData.CalculateOn,
+	                Aggregation = aggregationData.FacetAggregation
+	            });
+	        }
 
-			DatabaseCommands.GetFacetsAsync(IndexName, new IndexQuery(), AggregationQuery.GetFacets(facets), 0, 512)
-			                .ContinueOnSuccessInTheUIThread(task
-			                                                =>
-			                {
+            Results.Clear();
 
-			                });
-		}
+	        var facetResults =
+	            await
+	            DatabaseCommands.GetFacetsAsync(IndexName, new IndexQuery(), AggregationQuery.GetFacets(facets), 0, 512);
 
-		public BindableCollection<string> AvailableCollections { get; set; }
+            if (facetResults.Results.Count < 1)
+            {
+                return;
+            }
+
+	        var facetResult = facetResults.Results.FirstOrDefault().Value;
+	        var aggregation = data[0];
+
+	        foreach (var facetValue in facetResult.Values)
+	        {
+	            var result = new ReportRow {Key = facetValue.Range};
+
+                foreach (var facetAggregation in new[] { FacetAggregation.Count, FacetAggregation.Min, FacetAggregation.Max, FacetAggregation.Sum, FacetAggregation.Average})
+	            {
+	                if ((aggregation.FacetAggregation & facetAggregation) == facetAggregation)
+	                {
+                        result.Values.Add(facetAggregation.ToString().ToUpper() + " of " + aggregation.CalculateOn, facetValue.GetAggregation(facetAggregation) ?? 0);
+	                }
+	            }
+
+	            Results.Add(result);
+	        }
+
+	        var keys = Results.SelectMany(r => r.Values.Keys).Distinct();
+
+	        var columns = new ColumnsModel();
+
+            columns.Columns.Add(new ColumnDefinition()
+            {
+                Header = "Key", Binding = "Key"
+            });
+
+            columns.Columns.AddRange(keys.Select(k => new ColumnDefinition() { Header = k, Binding = "Values[" + k + "]"}));
+
+	        ResultColumns = columns;
+	    }
+
+	    public ColumnsModel ResultColumns
+        {
+            get { return resultColumns; }
+            set { resultColumns = value;
+            OnPropertyChanged(() => ResultColumns);}
+        }
+
+        public ObservableCollection<ReportRow> Results { get; private set; } 
+	    public BindableCollection<string> AvailableCollections { get; set; }
 		public ObservableCollection<string> ParamsForSelectedCollection { get; set; }
 		public BindableCollection<AggregationData> Aggregations { get; set; }
 		public ICommand AddAggregation
@@ -212,4 +260,15 @@ namespace Raven.Studio.Models
 			get { return new ActionCommand(() => Aggregations.Add(new AggregationData()));}
 		}
 	}
+
+    public class ReportRow
+    {
+        public ReportRow()
+        {
+            Values = new Dictionary<string, double>();
+        }
+
+        public string Key { get; set; }
+        public IDictionary<string, double> Values { get; private set; } 
+    }
 }
