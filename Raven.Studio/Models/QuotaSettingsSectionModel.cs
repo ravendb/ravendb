d8eaf6dab00181241070f287d188f4840eaeaa61
Daniel <daniel@ayende.com>
2013-05-20 08:50:45 +0300

    RavenDB-1116 Modifying Replication Settings adds invalid Quotas bundle settings

--- Raven.Studio/Models/QuotaSettingsSectionModel.cs
+++ Raven.Studio/Models/QuotaSettingsSectionModel.cs
@@ -9,27 +9,30 @@ namespace Raven.Studio.Models
             SectionName = "Quotas";
         }
 
-        public int MaxSize { get; set; }
-        public int WarnSize { get; set; }
-        public int MaxDocs { get; set; }
-        public int WarnDocs { get; set; }
+		public long MaxSize { get; set; }
+		public long WarnSize { get; set; }
+		public long MaxDocs { get; set; }
+		public long WarnDocs { get; set; }
 
-        public override void LoadFor(DatabaseDocument document)
-        {
-            MaxSize = ReadSettingAsInt(document, Constants.SizeHardLimitInKB)/1024;
-            WarnSize = ReadSettingAsInt(document, Constants.SizeSoftLimitInKB)/1024;
-            MaxDocs = ReadSettingAsInt(document, Constants.DocsHardLimit);
-            WarnDocs = ReadSettingAsInt(document, Constants.DocsSoftLimit);
+		public override void LoadFor(DatabaseDocument document)
+		{
+			MaxSize = ReadSettingAsInt(document, Constants.SizeHardLimitInKB) / 1024;
+			WarnSize = ReadSettingAsInt(document, Constants.SizeSoftLimitInKB) / 1024;
+			MaxDocs = ReadSettingAsInt(document, Constants.DocsHardLimit);
+			WarnDocs = ReadSettingAsInt(document, Constants.DocsSoftLimit);
 
-            OnEverythingChanged();
-        }
+			OnEverythingChanged();
+		}
 
-        private static int ReadSettingAsInt(DatabaseDocument document, string settingName)
-        {
-            var value = 0;
-            if (document.Settings.ContainsKey(settingName))
-                int.TryParse(document.Settings[settingName], out value);
-            return value;
-        }
+		private static long ReadSettingAsInt(DatabaseDocument document, string settingName)
+		{
+			long value;
+			if (document.Settings.ContainsKey(settingName))
+				long.TryParse(document.Settings[settingName], out value);
+			else
+				value = long.MaxValue;
+
+			return value;
+		}
     }
 }
