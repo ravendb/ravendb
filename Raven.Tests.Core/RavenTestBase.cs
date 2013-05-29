c97da34e94bfbea8bab8dfa1ef087d5907868ac1
Fitzchak Yitzchaki <fitzchak@ayende.com>
2013-05-29 10:53:40 +0300

    Add a basic test

---
+++ Raven.Tests.Core/RavenTestBase.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="RavenTestBase.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+namespace Raven.Tests.Core
+{
+	public class RavenTestBase
+	{
+		protected string Url
+		{
+			get
+			{
+				if (UseFiddler)
+					return "http://ipv4.fiddler:8079";
+				return "http://localhost:8079";
+			}
+		}
+
+		protected bool UseFiddler { get; set; }
+	}
+}
