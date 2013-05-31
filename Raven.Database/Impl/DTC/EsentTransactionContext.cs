473f991f14f9960533046402d72a39fbdb4de5f6
Arkadiusz Palinski <arek@ais.pl>
2013-05-31 18:03:01 +0200

    RavenDB-1064 - Server side -  the execution of all operations in DTC transaction moved to Prepare phase when using Esent

---
+++ Raven.Database/Impl/DTC/EsentTransactionContext.cs
@@ -0,0 +1,45 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="EsentTransactionContext.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using Microsoft.Isam.Esent.Interop;
+using Raven.Abstractions.Extensions;
+
+namespace Raven.Database.Impl.DTC
+{
+	public class EsentTransactionContext
+	{
+		private readonly IntPtr sessionContext;
+
+		public EsentTransactionContext(Session session, IntPtr context)
+		{
+			sessionContext = context;
+			Session = session;
+			using (EnterSessionContext())
+			{
+				Transaction = new Transaction(Session);
+			}
+
+			ActionsAfterCommit = new List<Action>();
+		}
+
+		public List<Action> ActionsAfterCommit { get; private set; }
+		public Session Session { get; private set; }
+		public Transaction Transaction { get; private set; }
+
+		public IDisposable EnterSessionContext()
+		{
+			Api.JetSetSessionContext(Session, sessionContext);
+
+			return new DisposableAction(() => Api.JetResetSessionContext(Session));
+		}
+
+		public void AfterCommit(Action action)
+		{
+			ActionsAfterCommit.Add(action);
+		} 
+	}
+}
