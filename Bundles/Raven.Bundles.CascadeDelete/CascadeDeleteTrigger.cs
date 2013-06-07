86f1ed670c19580ccb54593dd14b46f36282e36d
James Babco <jbabco@gmail.com>
2013-06-07 16:46:55 -0400

    fixup whitespace

--- Bundles/Raven.Bundles.CascadeDelete/CascadeDeleteTrigger.cs
+++ Bundles/Raven.Bundles.CascadeDelete/CascadeDeleteTrigger.cs
@@ -9,46 +9,46 @@ using Raven.Json.Linq;
 
 namespace Raven.Bundles.CascadeDelete
 {
-    public class CascadeDeleteTrigger : AbstractDeleteTrigger
+  public class CascadeDeleteTrigger : AbstractDeleteTrigger
+  {
+    public override void OnDelete(string key, TransactionInformation transactionInformation)
     {
-        public override void OnDelete(string key, TransactionInformation transactionInformation)
-        {
-            if (CascadeDeleteContext.IsInCascadeDeleteContext)
-                return;
+      if (CascadeDeleteContext.IsInCascadeDeleteContext)
+        return;
 
-            using (Database.DisableAllTriggersForCurrentThread())
-            using (CascadeDeleteContext.Enter())
-            {
-                RecursiveDelete(key, transactionInformation);
-            }
-        }
-
-        private void RecursiveDelete(string key, TransactionInformation transactionInformation)
-        {
-            var document = Database.Get(key, transactionInformation);
-            if (document == null)
-                return;
-
-            var documentsToDelete = document.Metadata.Value<RavenJArray>(MetadataKeys.DocumentsToCascadeDelete);
-            if (documentsToDelete != null)
-            {
-                foreach (var documentToDelete in documentsToDelete)
-                {
-                    var documentId = documentToDelete.Value<string>();
-                    if (!CascadeDeleteContext.HasAlreadyDeletedDocument(documentId))
-                    {
-                        CascadeDeleteContext.AddDeletedDocument(documentId);
-                        RecursiveDelete(documentId, transactionInformation);
-                        Database.Delete(documentId, null, transactionInformation);
-                    }
-                }
-            }
-            var attachmentsToDelete = document.Metadata.Value<RavenJArray>(MetadataKeys.AttachmentsToCascadeDelete);
-
-            if (attachmentsToDelete != null)
-                foreach (var attachmentToDelete in attachmentsToDelete)
-                    Database.DeleteStatic(attachmentToDelete.Value<string>(), null);
-            return;
-        }
+      using (Database.DisableAllTriggersForCurrentThread())
+      using (CascadeDeleteContext.Enter())
+      {
+        RecursiveDelete(key, transactionInformation);
+      }
     }
+
+    private void RecursiveDelete(string key, TransactionInformation transactionInformation)
+    {
+      var document = Database.Get(key, transactionInformation);
+      if (document == null)
+        return;
+
+      var documentsToDelete = document.Metadata.Value<RavenJArray>(MetadataKeys.DocumentsToCascadeDelete);
+      if (documentsToDelete != null)
+      {
+        foreach (var documentToDelete in documentsToDelete)
+        {
+          var documentId = documentToDelete.Value<string>();
+          if (!CascadeDeleteContext.HasAlreadyDeletedDocument(documentId))
+          {
+            CascadeDeleteContext.AddDeletedDocument(documentId);
+            RecursiveDelete(documentId, transactionInformation);
+            Database.Delete(documentId, null, transactionInformation);
+          }
+        }
+      }
+      var attachmentsToDelete = document.Metadata.Value<RavenJArray>(MetadataKeys.AttachmentsToCascadeDelete);
+
+      if (attachmentsToDelete != null)
+        foreach (var attachmentToDelete in attachmentsToDelete)
+          Database.DeleteStatic(attachmentToDelete.Value<string>(), null);
+      return;
+    }
+  }
 }
