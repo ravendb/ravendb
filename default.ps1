5a50c8a89494da9fa46ce54f0e8b7498ca1637dd
Fitzchak Yitzchaki <fitzchak@ayende.com>
2013-06-12 12:11:46 +0300

    Publish the 2.5 unstable package to nuget.org, and ravendb 3.0 unstable to myget.

--- default.ps1
+++ default.ps1
@@ -227,14 +227,14 @@ task ReleaseNoTests -depends Stable,DoRelease {
 
 }
 
-task Vnext {
+task Vnext3 {
 	$global:uploadCategory = "RavenDB-Unstable"
-	$global:uploadMode = "Vnext"
+	$global:uploadMode = "Vnext3"
 }
 
 task Unstable {
 	$global:uploadCategory = "RavenDB-Unstable"
-	$global:uploadMode = "-Unstable"
+	$global:uploadMode = "Unstable"
 }
 
 task Stable {
@@ -563,15 +563,9 @@ task CreateNugetPackages -depends Compile {
 	$accessPath = "$base_dir\..\Nuget-Access-Key.txt"
 	$sourceFeed = "https://nuget.org/"
 	
-	if ($global:uploadCategory -and $global:uploadCategory.EndsWith("-Unstable")){
+	if ($global:uploadMode -eq "Vnext3") {
 		$accessPath = "$base_dir\..\MyGet-Access-Key.txt"
-		
-		if ($global:uploadMode -eq "Vnext") {
-			$sourceFeed = "http://www.myget.org/F/ravendbvnext/api/v2/package"
-		} 
-		else {
-			$sourceFeed = "http://www.myget.org/F/ravendb/api/v2/package"
-		}
+		$sourceFeed = "http://www.myget.org/F/ravendb3/api/v2/package"
 	}
 	
 	if ( (Test-Path $accessPath) ) {
