f4304b7bc32f3dc0c9f7c1204c2b6b1522038efb
Marcin Lewandowski <marcin@ais.pl>
2013-06-13 08:36:42 +0200

    MultiDatabase

--- Raven.Client.Java/lib/src/main/java/raven/abstractions/data/Constants.java
+++ Raven.Client.Java/lib/src/main/java/raven/abstractions/data/Constants.java
@@ -6,5 +6,6 @@ public class Constants {
   public final static String RAVEN_LAST_MODIFIED = "Raven-Last-Modified";
   public final static String LAST_MODIFIED = "Last-Modified";
   public final static String RAVEN_LAST_MODIFIED_DATE_FORAT = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSX";
+  public final static String SYSTEM_DATABASE = "<system>";
 
 }
--- Raven.Client.Java/lib/src/main/java/raven/client/extensions/MultiDatabase.java
+++ Raven.Client.Java/lib/src/main/java/raven/client/extensions/MultiDatabase.java
@@ -1,7 +1,36 @@
 package raven.client.extensions;
 
-//TODO: finish me
+import java.util.regex.Pattern;
+
+import raven.abstractions.data.Constants;
+import raven.abstractions.data.DatabaseDocument;
+import raven.abstractions.json.linq.RavenJObject;
+
+/**
+ * Methods to create multitenant databases
+ */
 public class MultiDatabase {
+
+  private static final String VALID_DB_NAME_CHARS = "([A-Za-z0-9_\\-\\.]+)";
+
+  public static RavenJObject createDatabaseDocument(String name) {
+    assertValidName(name);
+    DatabaseDocument document = new DatabaseDocument();
+    document.getSettings().put("Raven/DataDir", "~\\Databases\\" + name);
+    RavenJObject doc = RavenJObject.fromObject(new DatabaseDocument());
+    doc.remove("id");
+    return doc;
+  }
+
+  private static void assertValidName(String name) {
+    if (name == null) {
+      throw new IllegalArgumentException("name");
+    }
+    if (!name.matches(VALID_DB_NAME_CHARS)) {
+      throw new IllegalStateException("Database name can only contain A-Z, a-z, 0-9, \"_\", \".\" or \"-\", but was: " + name);
+    }
+  }
+
   public static String getRootDatabaseUrl(String url) {
     int indexOfDatabases = url.indexOf("/databases/");
     if (indexOfDatabases != -1) {
@@ -11,5 +40,18 @@ public class MultiDatabase {
       return url.substring(0, url.length() - 1);
     }
     return url;
+  }
+
+  public static String getDatabaseName(String url) {
+    if (url == null) {
+      return null;
+    }
+    String databaseUrl = url;
+    int indexOfDatabases = databaseUrl.indexOf("/databases/");
+    if (indexOfDatabases != -1) {
+      databaseUrl = databaseUrl.substring(indexOfDatabases + "/databases/".length());
+      return Pattern.compile(VALID_DB_NAME_CHARS).matcher(databaseUrl).group();
+    }
+    return Constants.SYSTEM_DATABASE;
   }
 }
