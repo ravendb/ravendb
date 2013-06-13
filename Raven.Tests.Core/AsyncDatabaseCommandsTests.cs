0c82f9458926bcd216715e3fd4010e0a657d07a5
Fitzchak Yitzchaki <fitzchak@ayende.com>
2013-06-13 15:01:30 +0300

    Add test

---
+++ Raven.Tests.Core/AsyncDatabaseCommandsTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Raven.Client.Extensions;
+using Xunit;
+
+namespace Raven.Tests.Core
+{
+	public class AsyncDatabaseCommandsTests : RavenTestBase
+	{
+		[Fact]
+		public async Task CanGetAListOfDatabasesAsync()
+		{
+			var dbname = GenerateNewDatabaseName();
+			using (var store = NewDocumentStore())
+			{
+				await store.AsyncDatabaseCommands.Admin.EnsureDatabaseExistsAsync(dbname);
+
+				var names = await store.AsyncDatabaseCommands.GetDatabaseNamesAsync(25);
+				Assert.Contains(dbname, names);
+			}
+		}
+	}
+}
--- Raven.Tests.Core/RavenTestBase.cs
+++ Raven.Tests.Core/RavenTestBase.cs
@@ -3,6 +3,12 @@
 //      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 //  </copyright>
 // -----------------------------------------------------------------------
+using System.Diagnostics;
+using System.Linq;
+using Raven.Abstractions;
+using Raven.Client;
+using Raven.Client.Document;
+
 namespace Raven.Tests.Core
 {
 	public class RavenTestBase
@@ -18,5 +24,24 @@ namespace Raven.Tests.Core
 		}
 
 		protected bool UseFiddler { get; set; }
+
+		protected static string GenerateNewDatabaseName()
+		{
+			var stackTrace = new StackTrace();
+			var stackFrame = stackTrace.GetFrames()
+			                           .First(x => x.GetMethod().Name == "MoveNext" &&
+			                                       x.GetMethod().DeclaringType.FullName.Contains("+<"));
+
+			var generateNewDatabaseName = stackFrame.GetMethod().DeclaringType.FullName.Replace("+<", ".");
+
+			return generateNewDatabaseName
+				.Substring(0, generateNewDatabaseName.IndexOf(">"))
+				.Replace("Raven.Tests.Silverlight.", string.Empty) + SystemTime.UtcNow.Ticks;
+		}
+
+		protected IDocumentStore NewDocumentStore()
+		{
+			return new DocumentStore { Url = Url }.Initialize();
+		}
 	}
 }
