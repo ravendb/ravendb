5dc2f31cc540d3f04cc57ee73b216bebd3f78338
Fitzchak Yitzchaki <fitzchak@ayende.com>
2013-06-18 12:05:07 +0300

    Add a failing test - due to case sensitivity

--- Raven.Tests.Core/AsyncDatabaseCommandsTests.cs
+++ Raven.Tests.Core/AsyncDatabaseCommandsTests.cs
@@ -29,6 +29,26 @@ namespace Raven.Tests.Core
 		}
 
 		[Fact]
+		public async Task CanGetDocumentsWhoseIdStartsWithAPrefix()
+		{
+			var dbname = GenerateNewDatabaseName();
+			using (var store = NewDocumentStore())
+			{
+				await store.AsyncDatabaseCommands.Admin.EnsureDatabaseExistsAsync(dbname);
+
+				using (var session = store.OpenAsyncSession(dbname))
+				{
+					await session.StoreAsync(new Company { Name = "Something with the desired prefix" });
+					await session.StoreAsync(new Contact { Surname = "Something without the desired prefix" });
+					await session.SaveChangesAsync();
+				}
+
+				var documents = await store.AsyncDatabaseCommands.ForDatabase(dbname).StartsWithAsync("Companies", 0, 25);
+				Assert.Equal(1, documents.Length);
+			}
+		}
+
+		[Fact]
 		public async Task CanGetAListOfDatabasesAsync()
 		{
 			var dbname = GenerateNewDatabaseName();
