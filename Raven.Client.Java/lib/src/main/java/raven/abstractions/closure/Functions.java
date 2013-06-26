733aacb307be3ed2d6ba3130fac6b8c61a6f6180
Marcin Lewandowski <marcin@ais.pl>
2013-06-26 14:04:31 +0200

    sample ServerClient test

--- Raven.Client.Java/lib/src/main/java/raven/abstractions/closure/Functions.java
+++ Raven.Client.Java/lib/src/main/java/raven/abstractions/closure/Functions.java
@@ -8,12 +8,31 @@ public class Functions {
     }
   }
 
-  public static  class AlwaysFalse<S> implements Function1<S, Boolean> {
+  public static class AlwaysFalse<S> implements Function1<S, Boolean> {
     @Override
     public Boolean apply(S input) {
       return false;
     }
   }
+
+  /**
+   * Function that takes 1 parameter and returns always the same value
+   *
+   */
+  public static class StaticFunction1<S, T> implements Function1<S, T> {
+
+    private T inner;
+    public StaticFunction1(T value) {
+      this.inner = value;
+    }
+
+    @Override
+    public T apply(S input) {
+      return inner;
+    }
+
+  }
+
 
   public static <T> Function1<T, Boolean> alwaysTrue() {
     return new AlwaysTrue<T>();
