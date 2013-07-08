3c2ccce05fa908a39463b516584e6cc3e0f0a521
Marcin Lewandowski <marcin@ais.pl>
2013-07-08 11:09:00 +0200

    code cleanup

--- Raven.Client.Java/lib/src/main/java/raven/client/listeners/IDocumentConflictListener.java
+++ Raven.Client.Java/lib/src/main/java/raven/client/listeners/IDocumentConflictListener.java
@@ -5,11 +5,10 @@ import java.util.List;
 import raven.abstractions.basic.Holder;
 import raven.abstractions.data.JsonDocument;
 
-public class IDocumentConflictListener {
+/**
+ * Hooks for users that allows you to handle document replication conflicts
+ */
+public interface IDocumentConflictListener {
 
-  public boolean tryResolveConflict(String key, List<JsonDocument> results, Holder<JsonDocument> resolvedDocument) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-  //TODO: implement me
+  public boolean tryResolveConflict(String key, List<JsonDocument> results, Holder<JsonDocument> resolvedDocument);
 }
