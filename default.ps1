8616c5772915ef549b446988f582e74df9053035
Ayende Rahien <ayende@ayende.com>
2013-07-18 16:50:56 +0300

    Make sure to include async targeting pack

--- default.ps1
+++ default.ps1
@@ -311,6 +311,7 @@ task CopySilverlight {
 
 task CopySmuggler {
 	Copy-Item $build_dir\Raven.Abstractions.??? $build_dir\Output\Smuggler
+	Copy-Item $build_dir\Microsoft.CompilerServices.AsyncTargetingPack.Net4.??? $build_dir\Output\Smuggler
 	Copy-Item $build_dir\Raven.Smuggler.??? $build_dir\Output\Smuggler
 }
 
