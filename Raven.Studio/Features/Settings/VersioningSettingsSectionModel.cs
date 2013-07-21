c72bab9230b18d552d2464d311b00659269ab890
Daniel <daniel@ayende.com>
2013-07-21 13:51:52 +0300

    RavenDB-1228 Expose Raven/Versioning/ChangesToRevisionsAllowed in the UI

--- Raven.Studio/Features/Settings/VersioningSettingsSectionModel.cs
+++ Raven.Studio/Features/Settings/VersioningSettingsSectionModel.cs
@@ -6,7 +6,10 @@ using System.Windows.Input;
 using Microsoft.Expression.Interactivity.Core;
 using Raven.Abstractions.Data;
 using Raven.Bundles.Versioning.Data;
+using Raven.Client.Connection;
 using Raven.Client.Connection.Async;
+using Raven.Client.Silverlight.MissingFromSilverlight;
+using Raven.Json.Linq;
 using Raven.Studio.Behaviors;
 using Raven.Studio.Infrastructure;
 using Raven.Studio.Models;
@@ -68,22 +71,62 @@ namespace Raven.Studio.Features.Settings
 			get { return DatabaseDocument != null; }
 		}
 
-		public bool AllowChangedToRevistions
+		private bool? allowChangedToRevistionsInternal;
+		private Observable<bool> allowChangedToRevistions = new Observable<bool>();
+		public Observable<bool> AllowChangedToRevistions
 		{
 			get
 			{
 				if (DatabaseDocument == null) //Not an admin
 				{
-					//TODO: get data from configs
-					return false;
+					if (allowChangedToRevistionsInternal.HasValue)
+					{
+						allowChangedToRevistions.Value = allowChangedToRevistionsInternal.Value;
+						return allowChangedToRevistions;
+					}
+
+					ApplicationModel.Current.Server.Value.SelectedDatabase.Value
+							.AsyncDatabaseCommands
+							.CreateRequest(string.Format("/debug/settings?key=Raven/Versioning/ChangesToRevisionsAllowed").NoCache(), "GET")
+							.ReadResponseJsonAsync()
+							.ContinueOnSuccessInTheUIThread(doc =>
+							{
+								if (doc == null)
+								{
+									allowChangedToRevistionsInternal = false;
+									return;
+								}
+								bool value;
+								if (bool.TryParse(doc.ToString(), out value) == false)
+								{
+									allowChangedToRevistionsInternal = false;
+								}
+								else
+								{
+									allowChangedToRevistionsInternal = value;
+								}
+
+								OnPropertyChanged(() => AllowChangedToRevistions);
+							});
+					allowChangedToRevistions.Value = false;
+					return allowChangedToRevistions;
 				}
+
 				var changesToRevisionsAllowed = DatabaseDocument.Settings.ContainsKey("Raven/Versioning/ChangesToRevisionsAllowed");
 				if (changesToRevisionsAllowed == false)
-					return false;
+				{
+					allowChangedToRevistions.Value = false;
+					return allowChangedToRevistions;
+				}
 				bool result;
 				if (bool.TryParse(DatabaseDocument.Settings["Raven/Versioning/ChangesToRevisionsAllowed"], out result) == false)
-					return false;
-				return result;
+				{
+					allowChangedToRevistions.Value = false;
+					return allowChangedToRevistions;
+				}
+
+				allowChangedToRevistions.Value = result;
+				return allowChangedToRevistions;
 			}
 
 			set
