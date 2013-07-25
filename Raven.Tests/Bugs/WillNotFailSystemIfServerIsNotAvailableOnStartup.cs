1915b2dfa11178e14b11ec32e236a2218d8b195f
Fitzchak Yitzchaki <fitzchak@ayende.com>
2013-07-25 15:48:33 +0300

    Test also the async api

--- Raven.Tests/Bugs/WillNotFailSystemIfServerIsNotAvailableOnStartup.cs
+++ Raven.Tests/Bugs/WillNotFailSystemIfServerIsNotAvailableOnStartup.cs
@@ -3,45 +3,50 @@
 //     Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
-using System;
 using System.Net;
+using System.Threading.Tasks;
 using Raven.Client.Document;
-using Raven.Database.Extensions;
-using Raven.Database.Server;
 using Xunit;
 
 namespace Raven.Tests.Bugs
 {
-	public class WillNotFailSystemIfServerIsNotAvailableOnStartup : RemoteClientTest, IDisposable
+	public class WillNotFailSystemIfServerIsNotAvailableOnStartup : RemoteClientTest
 	{
-		private readonly string path;
-
-		public override void Dispose()
-		{
-			IOExtensions.DeleteDirectory(path);
-			base.Dispose();
-		}
-
-		public WillNotFailSystemIfServerIsNotAvailableOnStartup()
-		{
-			path = GetPath("TestDb");
-			NonAdminHttp.EnsureCanListenToWhenInNonAdminContext(8079);
-		}
-
 		[Fact]
 		public void CanStartWithoutServer()
 		{
-			using (var store = new DocumentStore
+			using (var store = new DocumentStore {Url = "http://localhost:8079"}.Initialize())
 			{
-				Url = "http://localhost:8079"
-			}.Initialize())
-			{
-				Assert.Throws<WebException>(() => store.OpenSession().Load<User>("user/1"));
-				using (GetNewServer(8079,path))
+				using (var session = store.OpenSession())
+				{
+					Assert.Throws<WebException>(() => session.Load<User>("user/1"));
+				}
+
+				using (GetNewServer())
 				{
 					using (var session = store.OpenSession())
 					{
 						Assert.Null(session.Load<Item>("items/1"));
+					}
+				}
+			}
+		}
+
+		[Fact]
+		public async Task CanStartWithoutServerAsync()
+		{
+			using (var store = new DocumentStore { Url = "http://localhost:8079" }.Initialize())
+			{
+				using (var session = store.OpenAsyncSession())
+				{
+					await AssertAsync.Throws<WebException>(() => session.LoadAsync<User>("user/1"));
+				}
+
+				using (GetNewServer())
+				{
+					using (var session = store.OpenAsyncSession())
+					{
+						Assert.Null(session.LoadAsync<Item>("items/1"));
 					}
 				}
 			}
