e054ad0e84ec8722a377b743ecca29993238c74b
Ayende Rahien <ayende@ayende.com>
2013-07-29 09:54:16 +0300

    Minor

--- Nevar/Impl/PureMemoryPager.cs
+++ Nevar/Impl/PureMemoryPager.cs
@@ -5,79 +5,80 @@ using Nevar.Trees;
 
 namespace Nevar.Impl
 {
-    public unsafe class PureMemoryPager : AbstractPager
-    {
-        private IntPtr _ptr;
-        private long _allocatedSize;
-        private byte* _base;
-        private long _allocatedPages;
-        private PagerState _pagerState;
+	public unsafe class PureMemoryPager : AbstractPager
+	{
+		private IntPtr _ptr;
+		private long _allocatedSize;
+		private byte* _base;
+		private long _allocatedPages;
+		private PagerState _pagerState;
 
-        public PureMemoryPager()
-        {
-            _ptr = Marshal.AllocHGlobal(MinIncreaseSize);
-            _base = (byte*)_ptr.ToPointer();
-            _allocatedPages = _allocatedSize / PageSize;
-            _pagerState = new PagerState
-                {
-                    Ptr = _ptr
-                };
-            _pagerState.AddRef();
-        }
+		public PureMemoryPager()
+		{
+			_ptr = Marshal.AllocHGlobal(MinIncreaseSize);
+			_base = (byte*)_ptr.ToPointer();
+			_allocatedPages = _allocatedSize / PageSize;
+			_pagerState = new PagerState
+				{
+					Ptr = _ptr
+				};
+			_pagerState.AddRef();
+		}
 
 
-        public override long NumberOfAllocatedPages
-        {
-            get { return _allocatedPages; }
-        }
+		public override long NumberOfAllocatedPages
+		{
+			get { return _allocatedPages; }
+		}
 
-        
 
-        public override void Dispose()
-        {
-            _pagerState.Release();
-            _base = null;
-        }
 
-        public override void Flush()
-        {
-        }
+		public override void Dispose()
+		{
+			_pagerState.Release();
+			_base = null;
+		}
 
-        public override PagerState TransactionBegan()
-        {
-            return null;
-        }
+		public override void Flush()
+		{
+		}
 
-        public override void TransactionCompleted(PagerState state)
-        {
-        }
+		public override PagerState TransactionBegan()
+		{
+			return null;
+		}
 
-        public override Page Get(long n)
-        {
-            EnsurePageExists(n);
-            return new Page(_base + (n * PageSize), PageMaxSpace);
-        }
+		public override void TransactionCompleted(PagerState state)
+		{
+		}
 
-        protected override void EnsurePageExists(long n)
-        {
-            if (n >= _allocatedPages)
-            {
-                var oldSize = _allocatedSize;
-                _allocatedSize = GetNewLength(_allocatedSize);
-                _allocatedPages = _allocatedSize/PageSize;
-                var newPtr = Marshal.AllocHGlobal(new IntPtr(_allocatedSize));
-                var newBase = (byte*) newPtr.ToPointer();
-                NativeMethods.memcpy(newBase, _base, new IntPtr(oldSize));
-                _base = newBase;
-                _ptr = newPtr;
+		public override Page Get(long n)
+		{
+			EnsurePageExists(n);
+			return new Page(_base + (n * PageSize), PageMaxSpace);
+		}
 
-                var oldPager = _pagerState;
+		protected override void EnsurePageExists(long n)
+		{
+			if (n < _allocatedPages)
+				return;
 
-                var newPager = new PagerState {Ptr = newPtr};
-                newPager.AddRef();
-                _pagerState = newPager;
-                oldPager.Release();
-            }
-        }
-    }
+			var oldSize = _allocatedSize;
+			_allocatedSize = GetNewLength(_allocatedSize);
+			_allocatedPages = _allocatedSize / PageSize;
+			var newPtr = Marshal.AllocHGlobal(new IntPtr(_allocatedSize));
+			var newBase = (byte*)newPtr.ToPointer();
+			NativeMethods.memcpy(newBase, _base, new IntPtr(oldSize));
+			_base = newBase;
+			_ptr = newPtr;
+
+			var oldPager = _pagerState;
+
+			var newPager = new PagerState { Ptr = newPtr };
+			newPager.AddRef(); // one for the current transaction
+			newPager.AddRef(); // one for the pager
+			_pagerState = newPager;
+			oldPager.Release();
+		}
+	}
 }
