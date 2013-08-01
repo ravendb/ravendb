1cbff5547764de2dc42a6402795c1ff285ae1d31
Pawel Pekrol <ppekrol@ais.pl>
2013-08-01 13:20:54 +0200

    implementation of LoadInternal and LoadInternalAsync in sharded sessions

--- Raven.Client.Lightweight/IAsyncDocumentSessionImpl.cs
+++ Raven.Client.Lightweight/IAsyncDocumentSessionImpl.cs
@@ -18,6 +18,8 @@ namespace Raven.Client
 	{
 		DocumentConvention Conventions { get; }
 
-		Task<T[]> LoadAsyncInternal<T>(string[] ids, KeyValuePair<string, Type>[] includes, string transfomer = null, Dictionary<string, RavenJToken> queryInputs = null);
+		Task<T[]> LoadAsyncInternal<T>(string[] ids, KeyValuePair<string, Type>[] includes);
+
+		Task<T[]> LoadAsyncInternal<T>(string[] ids, KeyValuePair<string, Type>[] includes, string transformer, Dictionary<string, RavenJToken> queryInputs = null);
 	}
 }
