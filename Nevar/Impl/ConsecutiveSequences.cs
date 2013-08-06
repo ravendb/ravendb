895d7690efdfbfc88d5d0c3dbfb8c9ca9e5c7e93
Ayende Rahien <ayende@ayende.com>
2013-08-06 07:28:32 +0300

    Fixing up the merge algo

--- Nevar/Impl/ConsecutiveSequences.cs
+++ Nevar/Impl/ConsecutiveSequences.cs
@@ -1,74 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Nevar.Impl
 {
-	public class ConsecutiveSequences : IEnumerable<long>
-	{
-	    private readonly int _minSequence;
+    public class ConsecutiveSequences : IEnumerable<long>
+    {
+        private readonly int _minSequence;
 
-	    private class Seq
-		{
-			public long Start;
-			public int Count;
+        private class Seq
+        {
+            public long Start;
+            public int Count;
 
-			public override string ToString()
-			{
-				return string.Format("Start: {0}, Count: {1}", Start, Count);
-			}
-		}
+            public override string ToString()
+            {
+                return string.Format("Start: {0}, Count: {1}", Start, Count);
+            }
+        }
 
-	    public ConsecutiveSequences(int minSequence = 1)
-	    {
-	        _minSequence = minSequence;
-	    }
+        public ConsecutiveSequences(int minSequence = 1)
+        {
+            _minSequence = minSequence;
+        }
 
-	    public int Count { get; set; }
+        public int Count { get; set; }
 
-		private readonly Dictionary<long, Seq> _sequencesByLast = new Dictionary<long, Seq>();
-		private readonly Dictionary<long, Seq> _sequencesByFirst = new Dictionary<long, Seq>();
-	    private Seq _current;
+        private readonly Dictionary<long, Seq> _sequencesByLast = new Dictionary<long, Seq>();
+        private readonly Dictionary<long, Seq> _sequencesByFirst = new Dictionary<long, Seq>();
+        private Seq _current;
 
-		public void Add(long v)
-		{
-			Count++;
-			Seq seq;
-			if (_sequencesByLast.TryGetValue(v - 1, out seq) == false)
-			{
-				if (_sequencesByFirst.TryGetValue(v + 1, out seq) == false)
-				{
-					var value = new Seq {Start = v, Count = 1};
-					_sequencesByLast.Add(v, value);
-					_sequencesByFirst.Add(v, value);
-				}
-				else
-				{
-					_sequencesByFirst.Remove(v + 1);
-					seq.Count++;
-					seq.Start = v;
-					_sequencesByFirst[v] = seq;
-				}
-			}
-			else
-			{
-				_sequencesByLast.Remove(v - 1);
-				seq.Count++;
-				_sequencesByLast[v] = seq;
-			}
-		}
+        public void Add(long v)
+        {
+            Debug.Assert(_sequencesByFirst.ContainsKey(v) == false);
+            Count++;
+            Seq seq;
+            if (_sequencesByLast.TryGetValue(v, out seq) == false)
+            {
+                if (_sequencesByFirst.TryGetValue(v + 1, out seq) == false)
+                {
+                    var value = new Seq { Start = v, Count = 1 };
+                    _sequencesByLast.Add(v + 1, value);
+                    _sequencesByFirst.Add(v, value);
+                }
+                else
+                {
+                    _sequencesByFirst.Remove(v + 1);
+                    _sequencesByLast.Remove(seq.Start + seq.Count);
+                    seq.Count++;
+                    seq.Start = v;
+                    Seq prev;
+                    while (_sequencesByLast.TryGetValue(seq.Start + seq.Count, out prev)) // merge backward
+                    {
+                        _sequencesByFirst.Remove(prev.Start);
+                        _sequencesByLast.Remove(prev.Start + prev.Count);
 
-		public bool TryAllocate(int num, out long v)
-		{
+                        seq.Count += prev.Count;
+                        seq.Start = prev.Start;
+                    }
+                    _sequencesByFirst.Add(seq.Start, seq);
+                    _sequencesByLast.Add(seq.Start + seq.Count, seq);
+                }
+            }
+            else
+            {
+                _sequencesByLast.Remove(v);
+                seq.Count++;
+                Seq next;
+                while (_sequencesByFirst.TryGetValue(seq.Start + seq.Count, out next)) // merge forward
+                {
+                    _sequencesByFirst.Remove(next.Start);
+                    _sequencesByLast.Remove(next.Start + next.Count);
+
+                    seq.Count += next.Count;
+                }
+                _sequencesByLast.Add(seq.Start + seq.Count, seq);
+            }
+            Debug.Assert(_sequencesByFirst.Count == _sequencesByLast.Count);
+        }
+
+        public bool TryAllocate(int num, out long v)
+        {
             if (_current != null && _current.Count >= num)
             {
                 var start = _current.Start;
+                var end = start + _current.Count;
                 _current.Count -= num;
                 _current.Start += num;
                 _sequencesByFirst.Remove(start);
                 if (_current.Count == 0)
                 {
-                    _sequencesByLast.Remove(start);
+                    _sequencesByLast.Remove(end);
                 }
                 else
                 {
@@ -82,49 +105,49 @@ namespace Nevar.Impl
                 }
                 return true;
             }
-			
-			// let us try to find a sequence long enough that is suitable
-			// we find the largest sequence that can serve, to make sure that we are serving from it
-			// for as long as we can
-			_current = _sequencesByLast.Values.Where(x => x.Count >= num && x.Count >= _minSequence)
-			                .OrderByDescending(x => x.Count)
-			                .FirstOrDefault();
-			if (_current == null)
-			{
-				v = -1;
-				return false;
-			}
 
-			return TryAllocate(num, out v);
-		}
+            // let us try to find a sequence long enough that is suitable
+            // we find the largest sequence that can serve, to make sure that we are serving from it
+            // for as long as we can
+            _current = _sequencesByLast.Values.Where(x => x.Count >= num && x.Count >= _minSequence)
+                            .OrderByDescending(x => x.Count)
+                            .FirstOrDefault();
+            if (_current == null)
+            {
+                v = -1;
+                return false;
+            }
 
-		//http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
-		int NextPowerOfTwo(int v)
-		{
-			v--;
-			v |= v >> 1;
-			v |= v >> 2;
-			v |= v >> 4;
-			v |= v >> 8;
-			v |= v >> 16;
-			v++;
-			return v;
-		}
+            return TryAllocate(num, out v);
+        }
 
-		public IEnumerator<long> GetEnumerator()
-		{
-			foreach (var seq in _sequencesByFirst.Values)
-			{
-				for (int i = 0; i < seq.Count; i++)
-				{
-					yield return i + seq.Start;
-				}
-			}
-		}
+        //http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+        int NextPowerOfTwo(int v)
+        {
+            v--;
+            v |= v >> 1;
+            v |= v >> 2;
+            v |= v >> 4;
+            v |= v >> 8;
+            v |= v >> 16;
+            v++;
+            return v;
+        }
 
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return GetEnumerator();
-		}
-	}
+        public IEnumerator<long> GetEnumerator()
+        {
+            foreach (var seq in _sequencesByFirst.Values)
+            {
+                for (int i = 0; i < seq.Count; i++)
+                {
+                    yield return i + seq.Start;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
 }
