5893d23a1d34e77352b8f90bfed778df9c0b1d02
Marcin Lewandowski <marcin@ais.pl>
2013-08-23 09:12:38 +0200

    OAuth

--- Raven.Client.Java/lib/src/main/java/raven/abstractions/oauth/AbstractAuthenticator.java
+++ Raven.Client.Java/lib/src/main/java/raven/abstractions/oauth/AbstractAuthenticator.java
@@ -1,5 +1,26 @@
 package raven.abstractions.oauth;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.client.methods.HttpUriRequest;
+
+import raven.abstractions.closure.Action1;
+import raven.abstractions.connection.WebRequestEventArgs;
+
 public abstract class AbstractAuthenticator {
-  //TODO:
+
+  protected String currentOauthToken;
+
+  public void configureRequest(Object sender, WebRequestEventArgs e) {
+    if (StringUtils.isEmpty(currentOauthToken)) {
+      return;
+    }
+    setHeader(e.getRequest(), "Authorization", currentOauthToken);
+  }
+
+  protected static void setHeader(HttpUriRequest request, String key, String value) {
+    request.setHeader(key, value);
+  }
+
+  public abstract Action1<HttpUriRequest> doOAuthRequest(String oauthSource);
+
 }
--- Raven.Client.Java/lib/src/main/java/raven/abstractions/oauth/BasicAuthenticator.java
+++ Raven.Client.Java/lib/src/main/java/raven/abstractions/oauth/BasicAuthenticator.java
@@ -1,5 +1,70 @@
 package raven.abstractions.oauth;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import raven.abstractions.closure.Action1;
+
 public class BasicAuthenticator extends AbstractAuthenticator {
-  //TODO:
+  private final String apiKey;
+  private final boolean enableBasicAuthenticationOverUnsecuredHttp;
+
+  public BasicAuthenticator(String apiKey, boolean enableBasicAuthenticationOverUnsecuredHttp) {
+    this.apiKey = apiKey;
+    this.enableBasicAuthenticationOverUnsecuredHttp = enableBasicAuthenticationOverUnsecuredHttp;
+  }
+
+  public Action1<HttpUriRequest> doOAuthRequest(String oauthSource) {
+    HttpClient httpClient = new DefaultHttpClient();
+    try {
+
+      HttpGet authRequest = prepareOAuthRequest(oauthSource);
+      HttpResponse httpResponse = httpClient.execute(authRequest);
+      try {
+        final String response = IOUtils.toString(httpResponse.getEntity().getContent());
+        return new Action1<HttpUriRequest>() {
+
+          @Override
+          public void apply(HttpUriRequest request) {
+            request.setHeader("Authorization", "Bearer " + response);
+          }
+        };
+      } finally {
+        EntityUtils.consume(httpResponse.getEntity());
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    } finally {
+      httpClient.getConnectionManager().shutdown();
+    }
+  }
+
+  private HttpGet prepareOAuthRequest(String oauthSource) {
+    HttpGet get = new HttpGet();
+    get.setHeader("grant_type", "client_credentials");
+    get.setHeader("Accept", "application/json;charset=UTF-8");
+    if (StringUtils.isNotEmpty(apiKey)) {
+      get.setHeader("Api-Key", apiKey);
+    }
+
+    if (!oauthSource.startsWith("https") && !enableBasicAuthenticationOverUnsecuredHttp) {
+      throw new IllegalStateException(BASICO_AUTH_OVER_HTTP_ERROR);
+    }
+
+    return get;
+  }
+
+  private final static String BASICO_AUTH_OVER_HTTP_ERROR = "Attempting to authenticate using basic security over HTTP would expose user credentials (including the password) in clear text to anyone sniffing the network." +
+      "Your OAuth endpoint should be using HTTPS, not HTTP, as the transport mechanism." +
+      "You can setup the OAuth endpoint in the RavenDB server settings ('Raven/OAuthTokenServer' configuration value), or setup your own behavior by providing a value for: " +
+      "documentStore.Conventions.HandleUnauthorizedResponse " +
+      "If you are on an internal network or requires this for testing, you can disable this warning by calling: " +
+      "documentStore.getJsonRequestFactory().enableBasicAuthenticationOverUnsecuredHttpEvenThoughPasswordsWouldBeSentOverTheWireInClearTextToBeStolenByHackers = true; ";
+
 }
