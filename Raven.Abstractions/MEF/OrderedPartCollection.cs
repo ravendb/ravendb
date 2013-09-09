d0b2ee79bf221cdbd351f13c47bb34969e557f30
Ayende Rahien <ayende@ayende.com>
2013-09-09 10:07:03 +0300

    Removing stupid obselete attribute

--- Raven.Abstractions/MEF/OrderedPartCollection.cs
+++ Raven.Abstractions/MEF/OrderedPartCollection.cs
@@ -82,7 +82,6 @@ namespace Raven.Abstractions.MEF
 			remove { inner.CollectionChanged -= value; }
 		}
 
-		[Obsolete("Use Result Transformers instead.")]
 		public IEnumerable<TResult> OfType<TResult>()
 		{
 			return this.Select(x => x.Value).OfType<TResult>();			
