0cd1fab1b1d95c5cb90612dfbe068c30e65dea9b
Damian Hickey <dhickey@gmail.com>
2013-10-04 15:47:58 +0100

    Using ExecuteAsync to create default indexes. It was failing because of a problem where the client uses http web request.

--- Raven.Tests.Helpers/RavenTestBase.cs
+++ Raven.Tests.Helpers/RavenTestBase.cs
@@ -256,7 +256,8 @@ namespace Raven.Tests.Helpers
 
 		protected virtual void CreateDefaultIndexes(IDocumentStore documentStore)
 		{
-			new RavenDocumentsByEntityName().Execute(documentStore);
+			//new RavenDocumentsByEntityName().Execute(documentStore); // This fails in new hosting env.
+			new RavenDocumentsByEntityName().ExecuteAsync(documentStore.AsyncDatabaseCommands, documentStore.Conventions).Wait();
 		}
 
 		public static void WaitForIndexing(IDocumentStore store, string db = null)
