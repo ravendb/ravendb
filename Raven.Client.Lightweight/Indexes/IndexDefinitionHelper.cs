3e42c8df3e8e015801ef746eacd9562c62a61291
Ayende Rahien <ayende@ayende.com>
2013-10-06 10:06:49 +0300

    Fixing tests

--- Raven.Client.Lightweight/Indexes/IndexDefinitionHelper.cs
+++ Raven.Client.Lightweight/Indexes/IndexDefinitionHelper.cs
@@ -37,7 +37,7 @@ namespace Raven.Client.Indexes
                     switch (methodCallExpression.Method.Name)
                     {
                         case "Select":
-		                    queryRootName = TryCaptureQueryRoot(methodCallExpression.Arguments.FirstOrDefault(x => x.NodeType == ExpressionType.Call) ?? methodCallExpression.Arguments[0]);
+		                    queryRootName = TryCaptureQueryRoot(methodCallExpression.Arguments.FirstOrDefault(x => x.NodeType == ExpressionType.Call || x.NodeType == ExpressionType.Lambda) ?? methodCallExpression.Arguments[0]);
                             break;
                         case "SelectMany":
                             queryRootName = TryCaptureQueryRoot(methodCallExpression.Arguments[1]);
