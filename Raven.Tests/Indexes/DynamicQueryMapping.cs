10db6adbdd70b52eb83fad359356c162559ce806
Ayende Rahien <ayende@ayende.com>
2013-10-06 23:51:43 +0300

    RavenDB-1403 Removed dynamic index auto aggregation

--- Raven.Tests/Indexes/DynamicQueryMapping.cs
+++ Raven.Tests/Indexes/DynamicQueryMapping.cs
@@ -159,39 +159,5 @@ namespace Raven.Tests.Indexes
 
 			Assert.Equal("from doc in docs\r\nselect new { UserName = doc.User.Name }", definition.Map);
 		}
-
-
-		[Fact]
-		public void CreateMapReduceIndex()
-		{
-			var mapping = new Database.Data.DynamicQueryMapping
-			{
-				AggregationOperation = AggregationOperation.Count,
-				Items = new[]
-				{
-					new DynamicQueryMappingItem
-					{
-						From = "User.Name",
-						To = "UserName"
-					}
-				}
-			};
-
-
-			IndexDefinition definition = mapping.CreateIndexDefinition();
-
-			Assert.Equal(@"from doc in docs
-select new { UserName = doc.User.Name, Count = 1 }", definition.Map);
-
-			Assert.Equal(@"from result in results
-group result by result.UserName
-into g
-select new
-{
-	UserName = g.Key,
-	Count = g.Sum(x=>x.Count)
-}
-", definition.Reduce);
-		}
 	}
 }
