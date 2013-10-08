1842b975cfa9630776961afde7526a67e579634c
Pawel Pekrol <ppekrol@ais.pl>
2013-10-08 12:58:21 +0200

    Squashed 'Raven.Voron/' changes from 20de177..7addb35
    7addb35 Making sure that we take all the trees snapshot at tx start
    fcf7b67 added a test that exposes that snapshots are not isolated
    
    git-subtree-dir: Raven.Voron
    git-subtree-split: 7addb35bf65330574f5852d050c2d6136750ec88

---
+++ Voron.Tests/Bugs/Snapshots.cs
@@ -0,0 +1,64 @@
+﻿namespace Voron.Tests.Bugs
+{
+	using System;
+	using System.IO;
+
+	using Voron.Trees;
+
+	using Xunit;
+
+	public class Snapshots : StorageTest
+	{
+		[Fact]
+		public void SnapshotIssue()
+		{
+			const int DocumentCount = 50000;
+
+			var rand = new Random();
+			var testBuffer = new byte[39];
+			rand.NextBytes(testBuffer);
+
+			Tree t1;
+
+			using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
+			{
+				t1 = Env.CreateTree(tx, "tree1");
+				tx.Commit();
+			}
+
+			using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
+			{
+				for (var i = 0; i < DocumentCount; i++)
+				{
+					t1.Add(tx, "docs/" + i, new MemoryStream(testBuffer));
+				}
+
+				tx.Commit();
+			}
+
+			using (var snapshot = Env.CreateSnapshot())
+			{
+				using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
+				{
+					for (var i = 0; i < DocumentCount; i++)
+					{
+						t1.Delete(tx, "docs/" + i);
+					}
+
+					tx.Commit();
+				}
+
+				for (var i = 0; i < DocumentCount; i++)
+				{
+					var result = snapshot.Read(t1.Name, "docs/" + i);
+					Assert.NotNull(result);
+
+					using (var reader = new BinaryReader(result.Stream))
+					{
+						Assert.Equal(testBuffer, reader.ReadBytes((int)result.Stream.Length));
+					}
+				}
+			}
+		}
+	}
+}
