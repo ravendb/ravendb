1ea47483d1392f2ee5a2b67ce21f0907621c4042
DanielDar <daniel@ayende.com>
2013-10-13 11:03:28 +0300

    add single auth controller

---
+++ Raven.Database/Server/Controllers/SingleAuthTokenController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Raven.Database.Server.Security;
+
+namespace Raven.Database.Server.Controllers
+{
+	public class SingleAuthTokenController : RavenApiController
+	{
+		[HttpGet("singleAuthToken")]
+		[HttpGet("databases/{databaseName}/singleAuthToken")]
+		public HttpResponseMessage SingleAuthGet()
+		{
+			var authorizer = (MixedModeRequestAuthorizer)ControllerContext.Configuration.Properties[typeof(MixedModeRequestAuthorizer)];
+
+			var token = authorizer.GenerateSingleUseAuthToken(Database, User, this);
+			
+			return GetMessageWithObject(new
+			{
+				Token = token
+			});
+		}
+	}
+}
