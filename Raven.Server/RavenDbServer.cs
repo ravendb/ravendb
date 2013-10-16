58d2db9f7d5077310e9ec49b382ad1661a7b394d
Damian Hickey <dhickey@gmail.com>
2013-10-16 14:40:13 +0100

    Spaces -> Tabs

--- Raven.Server/RavenDbServer.cs
+++ Raven.Server/RavenDbServer.cs
@@ -12,137 +12,132 @@ using Owin;
 using Raven.Abstractions.Logging;
 using Raven.Database;
 using Raven.Database.Config;
+using Raven.Database.Server;
 using Raven.Database.Server.Tenancy;
 using Raven.Server.Discovery;
 
 namespace Raven.Server
 {
-    using System.Net;
-    using Raven.Database.Server;
+	public class RavenDbServer : IDisposable
+	{
+		private static readonly ILog Logger = LogManager.GetCurrentClassLogger();
+		private readonly IDisposable server;
+		private readonly IServerThingsForTests serverThingsForTests;
+		private ClusterDiscoveryHost discoveryHost;
+		private readonly RavenDBOptions options;
 
-    public class RavenDbServer : IDisposable
-    {
-        private static readonly ILog Logger = LogManager.GetCurrentClassLogger();
-        private readonly IDisposable server;
-        private readonly IServerThingsForTests serverThingsForTests;
-        private ClusterDiscoveryHost discoveryHost;
-        private readonly RavenDBOptions options;
-
-        public RavenDbServer(InMemoryRavenConfiguration configuration)
-        {
-            options = new RavenDBOptions(configuration);
-            //TODO DH: configuration.ServerUrl doesn't bind properly
-            server = WebApp.Start("http://+:" + configuration.Port, app => app.UseRavenDB(options));
-            ClusterDiscovery(configuration);
+		public RavenDbServer(InMemoryRavenConfiguration configuration)
+		{
+			options = new RavenDBOptions(configuration);
+			//TODO DH: configuration.ServerUrl doesn't bind properly
+			server = WebApp.Start("http://+:" + configuration.Port, app => app.UseRavenDB(options));
+			ClusterDiscovery(configuration);
 			serverThingsForTests = new ServerThingsForTests(options);
-        }
+		}
 
-        //TODO DH: does this need to be exposed? Seems to be required for low level tests that the client
-        //API does not expose
-        public DocumentDatabase SystemDatabase
-        {
-            get { return options.SystemDatabase; }
-        }
+		//TODO DH: does this need to be exposed? Seems to be required for low level tests that the client
+		//API does not expose
+		public DocumentDatabase SystemDatabase
+		{
+			get { return options.SystemDatabase; }
+		}
 
-        //TODO DH: does this need to be exposed?
-        public IServerThingsForTests Server
-        {
-            get { return serverThingsForTests; }
-        }
+		//TODO DH: does this need to be exposed?
+		public IServerThingsForTests Server
+		{
+			get { return serverThingsForTests; }
+		}
 
-        public void Dispose()
-        {
-            server.Dispose();
-        }
+		public void Dispose()
+		{
+			server.Dispose();
+		}
 
-        private void ClusterDiscovery(InMemoryRavenConfiguration configuration)
-        {
-            if (configuration.DisableClusterDiscovery == false)
-            {
-                discoveryHost = new ClusterDiscoveryHost();
-                try
-                {
-                    discoveryHost.Start();
-                    discoveryHost.ClientDiscovered += async (sender, args) =>
-                    {
-                        var httpClient = new HttpClient(new HttpClientHandler());
-                        var values = new Dictionary<string, string>
-                        {
-                            {"Url", configuration.ServerUrl},
-                            {"ClusterName", configuration.ClusterName},
-                        };
-                        try
-                        {
-                            HttpResponseMessage result =
-                                await httpClient.PostAsync(args.ClusterManagerUrl, new FormUrlEncodedContent(values));
-                            result.EnsureSuccessStatusCode();
-                        }
-                        catch (Exception e)
-                        {
-                            Logger.ErrorException(
-                                "Cannot post notification for cluster discovert to: " + configuration.ServerUrl, e);
-                        }
-                    };
-                }
-                catch (Exception e)
-                {
-                    discoveryHost.Dispose();
-                    discoveryHost = null;
+		private void ClusterDiscovery(InMemoryRavenConfiguration configuration)
+		{
+			if (configuration.DisableClusterDiscovery == false)
+			{
+				discoveryHost = new ClusterDiscoveryHost();
+				try
+				{
+					discoveryHost.Start();
+					discoveryHost.ClientDiscovered += async (sender, args) =>
+					{
+						var httpClient = new HttpClient(new HttpClientHandler());
+						var values = new Dictionary<string, string>
+						{
+							{"Url", configuration.ServerUrl},
+							{"ClusterName", configuration.ClusterName},
+						};
+						try
+						{
+							HttpResponseMessage result =
+								await httpClient.PostAsync(args.ClusterManagerUrl, new FormUrlEncodedContent(values));
+							result.EnsureSuccessStatusCode();
+						}
+						catch (Exception e)
+						{
+							Logger.ErrorException(
+								"Cannot post notification for cluster discovert to: " + configuration.ServerUrl, e);
+						}
+					};
+				}
+				catch (Exception e)
+				{
+					discoveryHost.Dispose();
+					discoveryHost = null;
 
-                    Logger.ErrorException("Cannot setup cluster discovery", e);
-                }
-            }
-        }
+					Logger.ErrorException("Cannot setup cluster discovery", e);
+				}
+			}
+		}
 
-        //TODO DH: need a better name
-        private class ServerThingsForTests : IServerThingsForTests
-        {
-            private readonly RavenDBOptions options;
-            private readonly HttpServer httpServer;
+		//TODO DH: need a better name
+		private class ServerThingsForTests : IServerThingsForTests
+		{
+			private readonly RavenDBOptions options;
+			private readonly HttpServer httpServer;
 
-            public ServerThingsForTests(RavenDBOptions options)
-            {
-                this.options = options;
-                this.httpServer = httpServer;
-            }
+			public ServerThingsForTests(RavenDBOptions options)
+			{
+				this.options = options;
+				httpServer = httpServer;
+			}
 
-            public bool HasPendingRequests
-            {
-                get { return false; } //TODO DH: fix (copied from WebApiServer)
-            }
+			public bool HasPendingRequests
+			{
+				get { return false; } //TODO DH: fix (copied from WebApiServer)
+			}
 
-            public int NumberOfRequests
-            {
-                get
-                {
-                    return options.Landlord.NumberOfRequests;
-                }
-            }
+			public int NumberOfRequests
+			{
+				get { return options.Landlord.NumberOfRequests; }
+			}
 
-            public DatabasesLandlord Landlord
-            {
-                get { return options.Landlord; }
-            }
+			public DatabasesLandlord Landlord
+			{
+				get { return options.Landlord; }
+			}
 
-            public void ResetNumberOfRequests()
-            {
-                options.Landlord.ResetNumberOfRequests();
-            }
+			public void ResetNumberOfRequests()
+			{
+				options.Landlord.ResetNumberOfRequests();
+			}
 
-            public Task<DocumentDatabase> GetDatabaseInternal(string databaseName)
-            {
-                return options.Landlord.GetDatabaseInternal(databaseName);
-            }
-        }
-    }
+			public Task<DocumentDatabase> GetDatabaseInternal(string databaseName)
+			{
+				return options.Landlord.GetDatabaseInternal(databaseName);
+			}
+		}
+	}
 
-    //TODO DH: need a better name
-    public interface IServerThingsForTests
-    {
-        bool HasPendingRequests { get; }
-        int NumberOfRequests { get; }
-        DatabasesLandlord Landlord { get; }
-        void ResetNumberOfRequests();
-        Task<DocumentDatabase> GetDatabaseInternal(string databaseName);
-    }
+	//TODO DH: need a better name
+	public interface IServerThingsForTests
+	{
+		bool HasPendingRequests { get; }
+		int NumberOfRequests { get; }
+		DatabasesLandlord Landlord { get; }
+		void ResetNumberOfRequests();
+		Task<DocumentDatabase> GetDatabaseInternal(string databaseName);
+	}
 }
