90748f710fe2d6bcdbdb03075dcd83cd6b537178
Damian Hickey <dhickey@gmail.com>
2013-10-16 13:11:47 +0100

    Add AspNetWebStackNightly to Nuget.Targets package sources

--- Raven.Database/AppBuilderExtensions.cs
+++ Raven.Database/AppBuilderExtensions.cs
@@ -18,93 +18,93 @@ using Raven.Database.Server.WebApi.Handlers;
 // ReSharper disable once CheckNamespace
 namespace Owin
 {
-    public static class AppBuilderExtensions
-    {
-        public static IAppBuilder UseRavenDB(this IAppBuilder app)
-        {
-            return UseRavenDB(app, new RavenConfiguration());
-        }
+	public static class AppBuilderExtensions
+	{
+		public static IAppBuilder UseRavenDB(this IAppBuilder app)
+		{
+			return UseRavenDB(app, new RavenConfiguration());
+		}
 
-        public static IAppBuilder UseRavenDB(this IAppBuilder app, InMemoryRavenConfiguration configuration)
-        {
-            return UseRavenDB(app, new RavenDBOptions(configuration));
-        }
+		public static IAppBuilder UseRavenDB(this IAppBuilder app, InMemoryRavenConfiguration configuration)
+		{
+			return UseRavenDB(app, new RavenDBOptions(configuration));
+		}
 
-        public static IAppBuilder UseRavenDB(this IAppBuilder app, RavenDBOptions options)
-        {
-            if (options == null)
-            {
-                throw new ArgumentNullException("options");
-            }
+		public static IAppBuilder UseRavenDB(this IAppBuilder app, RavenDBOptions options)
+		{
+			if (options == null)
+			{
+				throw new ArgumentNullException("options");
+			}
 
-            DocumentDatabase documentDatabase = options.SystemDatabase;
+			DocumentDatabase documentDatabase = options.SystemDatabase;
 
-            // This is a katana specific key (i.e. non a standard OWIN key) to be notified
-            // when the host in being shut down. Works both in HttpListener and SystemWeb hosting
-            var appDisposing = app.Properties["host.OnAppDisposing"] as CancellationToken?;
-            if (appDisposing.HasValue)
-            {
-                appDisposing.Value.Register(documentDatabase.Dispose);
-            }
+			// This is a katana specific key (i.e. non a standard OWIN key) to be notified
+			// when the host in being shut down. Works both in HttpListener and SystemWeb hosting
+			var appDisposing = app.Properties["host.OnAppDisposing"] as CancellationToken?;
+			if (appDisposing.HasValue)
+			{
+				appDisposing.Value.Register(documentDatabase.Dispose);
+			}
 
-            var httpConfiguration = new HttpConfiguration();
-            SetupConfig(httpConfiguration, options.Landlord, options.MixedModeRequestAuthorizer);
-            app.UseWebApi(httpConfiguration);
-            return app;
-        }
+			var httpConfiguration = new HttpConfiguration();
+			SetupConfig(httpConfiguration, options.Landlord, options.MixedModeRequestAuthorizer);
+			app.UseWebApi(httpConfiguration);
+			return app;
+		}
 
-        private static void SetupConfig(HttpConfiguration cfg, DatabasesLandlord databasesLandlord,
-            MixedModeRequestAuthorizer mixedModeRequestAuthorizer)
-        {
-            cfg.Properties[typeof (DatabasesLandlord)] = databasesLandlord;
-            cfg.Properties[typeof (MixedModeRequestAuthorizer)] = mixedModeRequestAuthorizer;
-            cfg.Formatters.Remove(cfg.Formatters.XmlFormatter);
+		private static void SetupConfig(HttpConfiguration cfg, DatabasesLandlord databasesLandlord,
+			MixedModeRequestAuthorizer mixedModeRequestAuthorizer)
+		{
+			cfg.Properties[typeof (DatabasesLandlord)] = databasesLandlord;
+			cfg.Properties[typeof (MixedModeRequestAuthorizer)] = mixedModeRequestAuthorizer;
+			cfg.Formatters.Remove(cfg.Formatters.XmlFormatter);
 
-            cfg.Services.Replace(typeof (IAssembliesResolver), new MyAssemblyResolver());
+			cfg.Services.Replace(typeof (IAssembliesResolver), new MyAssemblyResolver());
 
-            cfg.MapHttpAttributeRoutes();
-            cfg.Routes.MapHttpRoute(
-                "API Default", "{controller}/{action}",
-                new {id = RouteParameter.Optional});
+			cfg.MapHttpAttributeRoutes();
+			cfg.Routes.MapHttpRoute(
+				"API Default", "{controller}/{action}",
+				new {id = RouteParameter.Optional});
 
-            cfg.Routes.MapHttpRoute(
-                "Database Route", "databases/{databaseName}/{controller}/{action}",
-                new {id = RouteParameter.Optional});
-            cfg.MessageHandlers.Add(new GZipToJsonHandler());
-            cfg.Services.Replace(typeof (IHostBufferPolicySelector), new SelectiveBufferPolicySelector());
-        }
+			cfg.Routes.MapHttpRoute(
+				"Database Route", "databases/{databaseName}/{controller}/{action}",
+				new {id = RouteParameter.Optional});
+			cfg.MessageHandlers.Add(new GZipToJsonHandler());
+			cfg.Services.Replace(typeof (IHostBufferPolicySelector), new SelectiveBufferPolicySelector());
+		}
 
-        private class MyAssemblyResolver : IAssembliesResolver
-        {
-            public ICollection<Assembly> GetAssemblies()
-            {
-                return new[] {typeof (RavenApiController).Assembly};
-            }
-        }
+		private class MyAssemblyResolver : IAssembliesResolver
+		{
+			public ICollection<Assembly> GetAssemblies()
+			{
+				return new[] {typeof (RavenApiController).Assembly};
+			}
+		}
 
-        public class SelectiveBufferPolicySelector : IHostBufferPolicySelector
-        {
-            public bool UseBufferedInputStream(object hostContext)
-            {
-                var context = hostContext as IOwinContext;
+		public class SelectiveBufferPolicySelector : IHostBufferPolicySelector
+		{
+			public bool UseBufferedInputStream(object hostContext)
+			{
+				var context = hostContext as IOwinContext;
 
-                if (context != null)
-                {
-                    if (context.Request.Uri.LocalPath.EndsWith("bulkInsert", StringComparison.OrdinalIgnoreCase))
-                        return false;
-                }
+				if (context != null)
+				{
+					if (context.Request.Uri.LocalPath.EndsWith("bulkInsert", StringComparison.OrdinalIgnoreCase))
+						return false;
+				}
 
-                return true;
-            }
+				return true;
+			}
 
-            public bool UseBufferedOutputStream(HttpResponseMessage response)
-            {
-                return (response.Content is ChangesPushContent ||
-                        response.Content is StreamsController.StreamQueryContent ||
-                        response.Content is StreamContent ||
-                        response.Content is PushStreamContent ||
-                        response.Content is JsonContent) == false;
-            }
-        }
-    }
+			public bool UseBufferedOutputStream(HttpResponseMessage response)
+			{
+				return (response.Content is ChangesPushContent ||
+				        response.Content is StreamsController.StreamQueryContent ||
+				        response.Content is StreamContent ||
+				        response.Content is PushStreamContent ||
+				        response.Content is JsonContent) == false;
+			}
+		}
+	}
 }
