98bb65acfe8d38a6f30100c19bb2a011765e74ab
Georgios Diamantopoulos <georgiosd@live.com>
2013-10-16 23:55:06 +0300

    Rebase JS array extensions to use lodash
    Deleted Map.js and put everything in RavenDB.js
    Changed order of inclusion - lodash first, json2.js second, RavenDB.js
    third

--- Raven.Database/Json/ScriptedJsonPatcher.cs
+++ Raven.Database/Json/ScriptedJsonPatcher.cs
@@ -331,9 +331,8 @@ function ExecutePatchScript(docInner){{
 				.SetMaxSteps(maxSteps);
 
 
-			AddScript(jintEngine, "Raven.Database.Json.Map.js");
-			AddScript(jintEngine, "Raven.Database.Json.ToJson.js");
 			AddScript(jintEngine, "Raven.Database.Json.lodash.js");
+			AddScript(jintEngine, "Raven.Database.Json.ToJson.js");		
 			AddScript(jintEngine, "Raven.Database.Json.RavenDB.js");
 
 			jintEngine.SetFunction("LoadDocument", ((Func<string, object>)(value =>
