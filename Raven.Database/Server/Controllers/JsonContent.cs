f6844ecc62a26b1060d85a9847347123094a9587
Damian Hickey <dhickey@gmail.com>
2013-10-25 15:22:00 +0100

    Revert lazy token serialization.

--- Raven.Database/Server/Controllers/JsonContent.cs
+++ Raven.Database/Server/Controllers/JsonContent.cs
@@ -3,11 +3,9 @@
 //      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 //  </copyright>
 // -----------------------------------------------------------------------
-using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Raven.Abstractions;
 using Raven.Imports.Newtonsoft.Json;
@@ -17,53 +15,39 @@ namespace Raven.Database.Server.Controllers
 {
 	public class JsonContent : HttpContent
 	{
-		private readonly Lazy<byte[]> lazyContent;
-
 		public JsonContent(RavenJToken token = null)
 		{
 			Token = token;
-			lazyContent = new Lazy<byte[]>(() => TokenAsByteArray(Token, Jsonp));
 		}
 
 		public RavenJToken Token { get; set; }
-
 		public string Jsonp { get; set; }
 
-		private static byte[] TokenAsByteArray(RavenJToken token, string jsonp)
+		protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
 		{
-			if (token == null)
-			{
-				return new byte[0];
-			}
-			var memoryStream = new MemoryStream();
-			using (var writer = new StreamWriter(memoryStream))
-			{
-				if (string.IsNullOrEmpty(jsonp) == false)
-				{
-					writer.Write(jsonp);
-					writer.Write("(");
-				}
-				token.WriteTo(new JsonTextWriter(writer), Default.Converters);
-				if (string.IsNullOrEmpty(jsonp) == false)
-				{
-					writer.Write(")");
-				}
-			}
-			return memoryStream.ToArray();
-		}
+			if (Token == null)
+				return Task.FromResult(true);
 
-		protected override async Task SerializeToStreamAsync(Stream stream, TransportContext context)
-		{
-			if (lazyContent.Value.Length == 0)
-				return;
+			var writer = new StreamWriter(stream);
+			if (string.IsNullOrEmpty(Jsonp) == false)
+			{
+				writer.Write(Jsonp);
+				writer.Write("(");
+			}
+			Token.WriteTo(new JsonTextWriter(writer), Default.Converters);
+			if (string.IsNullOrEmpty(Jsonp) == false)
+			{
+				writer.Write(")");
+			}
 
-			await stream.WriteAsync(lazyContent.Value, 0, lazyContent.Value.Length);
+			writer.Flush();
+			return Task.FromResult(true);
 		}
 
 		protected override bool TryComputeLength(out long length)
 		{
-			length = lazyContent.Value.Length;
-			return true;
+			length = 0;
+			return Token == null;
 		}
 	}
 }
