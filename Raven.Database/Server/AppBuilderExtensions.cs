baf4685968e0044431126f71616239110ed39606
Damian Hickey <dhickey@gmail.com>
2013-10-25 15:26:03 +0100

    Remvove interceptor middleware

--- Raven.Database/Server/AppBuilderExtensions.cs
+++ Raven.Database/Server/AppBuilderExtensions.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Reflection;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Dispatcher;
 using System.Web.Http.Hosting;
@@ -33,11 +32,6 @@ namespace Owin
 			return UseRavenDB(app, new RavenDBOptions(configuration));
 		}
 
-		private static IAppBuilder UseInterceptor(this IAppBuilder app, DatabasesLandlord databasesLandlord)
-		{
-			return app.Use(typeof (InterceptMiddleware), new[] {databasesLandlord});
-		}
-
 		public static IAppBuilder UseRavenDB(this IAppBuilder app, RavenDBOptions options)
 		{
 			if (options == null)
@@ -45,7 +39,7 @@ namespace Owin
 				throw new ArgumentNullException("options");
 			}
 
-			// This is a katana specific key (i.e. non a standard OWIN key) to be notified
+			// This is a katana specific key (i.e. not a standard OWIN key) to be notified
 			// when the host in being shut down. Works both in HttpListener and SystemWeb hosting
 			var appDisposing = app.Properties["host.OnAppDisposing"] as CancellationToken?;
 			if (appDisposing.HasValue)
@@ -53,15 +47,13 @@ namespace Owin
 				appDisposing.Value.Register(options.Dispose);
 			}
 
-			var httpConfiguration = new HttpConfiguration();
-			SetupConfig(httpConfiguration, options.Landlord, options.MixedModeRequestAuthorizer, options.RequestManager);
-			app.UseInterceptor(options.Landlord)
-				.UseWebApi(httpConfiguration);
+			app.UseWebApi(CreateHttpCfg(options.Landlord, options.MixedModeRequestAuthorizer, options.RequestManager));
 			return app;
 		}
 
-		private static void SetupConfig(HttpConfiguration cfg, DatabasesLandlord databasesLandlord, MixedModeRequestAuthorizer mixedModeRequestAuthorizer, RequestManager requestManager)
+		private static HttpConfiguration CreateHttpCfg(DatabasesLandlord databasesLandlord, MixedModeRequestAuthorizer mixedModeRequestAuthorizer, RequestManager requestManager)
 		{
+			var cfg = new HttpConfiguration();
 			cfg.Properties[typeof(DatabasesLandlord)] = databasesLandlord;
 			cfg.Properties[typeof(MixedModeRequestAuthorizer)] = mixedModeRequestAuthorizer;
 			cfg.Properties[typeof(RequestManager)] = requestManager;
@@ -79,6 +71,7 @@ namespace Owin
 				new { id = RouteParameter.Optional });
 			cfg.MessageHandlers.Add(new GZipToJsonHandler());
 			cfg.Services.Replace(typeof(IHostBufferPolicySelector), new SelectiveBufferPolicySelector());
+			return cfg;
 		}
 
 		private class MyAssemblyResolver : IAssembliesResolver
@@ -112,23 +105,6 @@ namespace Owin
 						response.Content is PushStreamContent ||
 						response.Content is JsonContent ||
 						response.Content is MultiGetController.MultiGetContent) == false;
-			}
-		}
-
-		private class InterceptMiddleware : OwinMiddleware
-		{
-			private readonly DatabasesLandlord databasesLandlord;
-
-			public InterceptMiddleware(OwinMiddleware next, DatabasesLandlord databasesLandlord) : base(next)
-			{
-				this.databasesLandlord = databasesLandlord;
-			}
-
-			public override async Task Invoke(IOwinContext context)
-			{
-				// Pre request stuff
-				await Next.Invoke(context);
-				// Post request stuff
 			}
 		}
 	}
