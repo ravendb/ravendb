7d8b7d92ed9b6387c9cf30e08202284cee4ef9eb
Pawel Pekrol <ppekrol@ais.pl>
2013-10-29 13:02:28 +0100

    Exposing storage env recreation issue

---
+++ Voron.Tests/Bugs/Disposing.cs
@@ -0,0 +1,26 @@
+﻿namespace Voron.Tests.Bugs
+{
+	using System.IO;
+
+	using Xunit;
+
+	public class Disposing : StorageTest
+	{
+		[Fact]
+		public void DisposingAndRecreatingStorageShouldWork()
+		{
+			var path = "test2.data";
+
+			if (Directory.Exists(path))
+				Directory.Delete(path, true);
+
+			using (var env = new StorageEnvironment(StorageEnvironmentOptions.ForPath(path)))
+			{
+			}
+
+			using (var env = new StorageEnvironment(StorageEnvironmentOptions.ForPath(path)))
+			{
+			}
+		}
+	}
+}
