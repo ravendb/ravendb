f0e990add821d80d8897aa2ae80c891bc1b6991b
Ayende Rahien <ayende@ayende.com>
2013-10-31 22:19:34 +0200

    We actually need a robust way to delete a directory, dear Lord

--- Voron.Tryout/Program.cs
+++ Voron.Tryout/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using Voron.Impl.FileHeaders;
+using Voron.Tests.Storage;
 
 namespace Voron.Tryout
 {
@@ -8,7 +9,14 @@ namespace Voron.Tryout
     {
         private static unsafe void Main()
         {
-          
+	        for (int i = 0; i < 10000; i++)
+	        {
+		        Console.WriteLine(i);
+		        using (var x = new Batches())
+		        {
+			        x.ReadVersion_Items_From_Both_WriteBatch_And_Snapshot_Deleted_Key_Returns_Version0();
+		        }
+	        }
         }
     }
 }
