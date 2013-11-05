73479806546755b148798837dadafe1ef8d353da
Ayende Rahien <ayende@ayende.com>
2013-11-05 14:37:57 +0000

    more perf work

--- Performance.Comparison/Performance.Comparison/Program.cs
+++ Performance.Comparison/Performance.Comparison/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Performance.Comparison.SQLCE;
 using Performance.Comparison.SQLite;
+using Performance.Comparison.SQLServer;
 using Performance.Comparison.Voron;
 using Voron.Impl;
 
@@ -12,15 +14,16 @@ namespace Performance.Comparison
 	{
 		static void Main(string[] args)
 		{
-			var path = @"c:\temp\";
+			var path = @"c:\work\temp\";
 
 
-			var randomIds = InitRandomNumbers(Constants.WriteTransactions*Constants.ItemsPerTransaction);
+			var randomIds = InitRandomNumbers(Constants.WriteTransactions * Constants.ItemsPerTransaction);
 
 			var performanceTests = new List<IStoragePerformanceTest>()
 				{
-					new SqlLiteTest(path),
-					new SqlCeTest(path),
+					//new SqlServerTest(),
+					//new SqlLiteTest(path),
+					//new SqlCeTest(path),
 					new VoronTest(path, FlushMode.Full)
 				};
 
@@ -31,31 +34,41 @@ namespace Performance.Comparison
 				var writeSeq = test.WriteSequential();
 				var items = writeSeq.Sum(x => x.ProcessedItems);
 				double totalDuration = writeSeq.Sum(x => x.Duration);
-
+				WritePerfData("WriteSeq", test, writeSeq);
 				Console.WriteLine("Write sequential ->  {0} items in {1} sec, {2,10:#,#} ops / sec", items, totalDuration / 1000, items / (totalDuration / 1000));
 
 				var readSeq = test.ReadSequential();
 				items = readSeq.ProcessedItems;
 				totalDuration = readSeq.Duration;
-
+				WritePerfData("ReadSeq", test, writeSeq);
 				Console.WriteLine("Read sequential ->  {0} items in {1} sec, {2,10:#,#} ops / sec", items, totalDuration / 1000, items / (totalDuration / 1000));
 
 				var writeRandom = test.WriteRandom(randomIds);
 				items = writeRandom.Sum(x => x.ProcessedItems);
 				totalDuration = writeRandom.Sum(x => x.Duration);
-
+				WritePerfData("WriteRandom", test, writeSeq);
 				Console.WriteLine("Write random ->  {0} items in {1} sec, {2,10:#,#} ops / sec", items, totalDuration / 1000, items / (totalDuration / 1000));
 
 				var readRandom = test.ReadRandom(randomIds);
 				items = readRandom.ProcessedItems;
 				totalDuration = readRandom.Duration;
-
+				WritePerfData("ReadRandom", test, writeSeq);
 				Console.WriteLine("Read random ->  {0} items in {1} sec, {2,10:#,#} ops / sec", items, totalDuration / 1000, items / (totalDuration / 1000));
 
 				Console.WriteLine("---------------------------");
 			}
+		}
 
-			Console.ReadLine();
+		private static void WritePerfData(string name, IStoragePerformanceTest test, List<PerformanceRecord> writeSeq)
+		{
+			using (var file = File.Open(name + "_" + test.GetType().Name + ".csv",FileMode.Create))
+			using (var writer = new StreamWriter(file))
+			{
+				foreach (var p in writeSeq)
+				{
+					writer.WriteLine("{0},{1},{2}", p.ProcessedItems, p.Time, p.Duration);
+				}
+			}
 		}
 
 		private static HashSet<int> InitRandomNumbers(int count)
