3f2dbe7d55ca683c8ffa6d6a3402589dd6c299dc
Daniel <daniel@ayende.com>
2013-11-11 11:10:07 +0200

    connect the studio

--- Raven.Database/Server/Controllers/RavenUiController.cs
+++ Raven.Database/Server/Controllers/RavenUiController.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 
 namespace Raven.Database.Server.Controllers
@@ -6,14 +9,16 @@ namespace Raven.Database.Server.Controllers
 	[RoutePrefix("")]
 	public class RavenUiController : RavenApiController
 	{
-		[HttpGet][Route("raven")]
-		public HttpResponseMessage RavenUiGet()
+		[HttpGet]
+		[Route("raven")]
+		[Route("raven/{*id}")]
+		public HttpResponseMessage RavenUiGet(string id = null)
 		{
 			if (string.IsNullOrEmpty(Database.Configuration.RedirectStudioUrl) == false)
 			{
-				//TODO: redirect
-				//context.Response.Redirect(Database.Configuration.RedirectStudioUrl);
-				//return;
+				var result = InnerRequest.CreateResponse(HttpStatusCode.Found);
+				result.Headers.Location = new Uri(Path.Combine(DatabasesLandlord.SystemConfiguration.ServerUrl, Database.Configuration.RedirectStudioUrl));
+				return result;
 			}
 
 			var docPath = GetRequestUrl().Replace("/raven/", "");
