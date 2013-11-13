9429e72f3ab913d3a073a648e05fe20d1870d4d9
Arkadiusz Palinski <arek@ais.pl>
2013-11-13 14:28:26 +0100

    lmdb test now works properly

--- Performance.Comparison/Other/LMDB/lmdb_perf_test.cpp
+++ Performance.Comparison/Other/LMDB/lmdb_perf_test.cpp
@@ -8,11 +8,15 @@
 #include <vector>
 #include <set>
 #include <unistd.h>
+#include <chrono>
+#include <ctime>
 
 extern "C"
 {
   #include "lmdb.h"
 }
+
+using namespace std;
 
 class TestData {
 public:
@@ -27,18 +31,10 @@ public:
   time_t Time; 
 };
 
-double ToMiliseconds(int endTime, int startTime) {
-  return (endTime - startTime)/(CLOCKS_PER_SEC/1000);
-};
-
-double ToSeconds(int endTime, int startTime) {
-  return ToMiliseconds(endTime, startTime) / (double)1000;
-};
-
-std::vector<TestData> InitValue(std::set<int> ids, int minValueSize, int maxValueSize) {
-  std::vector<TestData> data;
+vector<TestData> InitValue(set<int> ids, int minValueSize, int maxValueSize) {
+  vector<TestData> data;
   
-  std::set<int>::iterator it;
+  set<int>::iterator it;
   for(it = ids.begin(); it != ids.end(); ++it) {
     
     TestData t;
@@ -51,8 +47,8 @@ std::vector<TestData> InitValue(std::set<int> ids, int minValueSize, int maxValu
   return data;
 };
 
-std::vector<TestData> InitSequentialNumbers(int count, int minValueSize, int maxValueSize) {
-    std::set<int> ids;
+vector<TestData> InitSequentialNumbers(int count, int minValueSize, int maxValueSize) {
+    set<int> ids;
   
     for(int i = 0; i < count; i++) {
       ids.insert(i);
@@ -61,32 +57,32 @@ std::vector<TestData> InitSequentialNumbers(int count, int minValueSize, int max
     return InitValue(ids, minValueSize, maxValueSize);
 };
 
-std::vector<TestData> InitRandomNumbers(int count, int minValueSize, int maxValueSize) {
-  std::vector<TestData> data = InitSequentialNumbers(count, minValueSize, maxValueSize);
+vector<TestData> InitRandomNumbers(int count, int minValueSize, int maxValueSize) {
+  vector<TestData> data = InitSequentialNumbers(count, minValueSize, maxValueSize);
   
-  std::random_shuffle(data.begin(), data.end());
+  random_shuffle(data.begin(), data.end());
   
   return data;
 };
 
-std::string GetTime(time_t t) {
+string GetTime(time_t t) {
   struct tm tstruct;
   char buf[80];
   tstruct = *localtime(&t);
   
   strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
   
-  std::stringstream s;
-  std::string output;
+  stringstream s;
+  string output;
   s << buf;
   s >> output;
   
   return output;
 };
 
-void WritePerfData(std::string name, std::vector<PerformanceRecord> data) {
-  std::ofstream file;
-  std::string fileName = name + "_LMDB.csv";
+void WritePerfData(string name, vector<PerformanceRecord> data) {
+  ofstream file;
+  string fileName = name + "_LMDB.csv";
   file.open((char*)fileName.c_str());
   
   for(int i = 0; i < data.size(); i++) {
@@ -98,8 +94,8 @@ void WritePerfData(std::string name, std::vector<PerformanceRecord> data) {
   file.close();
 };
 
-std::vector<PerformanceRecord> Write(std::vector<TestData> dataItems, int itemsPerTransaction, int numberOfTransactions) {
-  std::vector<PerformanceRecord> records;
+vector<PerformanceRecord> Write(vector<TestData> dataItems, int itemsPerTransaction, int numberOfTransactions) {
+  vector<PerformanceRecord> records;
   
   int rc;
   MDB_env *env;
@@ -107,24 +103,22 @@ std::vector<PerformanceRecord> Write(std::vector<TestData> dataItems, int itemsP
 
   MDB_stat mst;
   MDB_cursor *cursor;
-  char sval[87 * 1024]; //TODO
+  char sval[87 * 1024];
 
   system("exec rm -r ./lmdb_test");
   system("exec mkdir ./lmdb_test");
   
   rc = mdb_env_create(&env);
-  rc = mdb_env_set_mapsize(env, 1024*1024*1024*1);
-  rc = mdb_env_set_flags(env, MDB_WRITEMAP, 1);
-  rc = mdb_env_open(env, "./lmdb_test", 0, 0664);
+  rc = mdb_env_set_mapsize(env, 1024*1024*1024*(long)10);
+  rc = mdb_env_open(env, "./lmdb_test", MDB_WRITEMAP, 0664);
 
-  
-  int startTime, endTime;
   time_t t;
   
-  startTime = clock();
+  chrono::time_point<chrono::system_clock> start, end;
+  start = chrono::system_clock::now();
   
   for(int transactions = 0; transactions < numberOfTransactions; transactions++) {
-    int sw = clock();
+    chrono::time_point<chrono::system_clock> sw = chrono::system_clock::now();
     
     MDB_txn *txn;
     MDB_dbi dbi;
@@ -142,6 +136,11 @@ std::vector<PerformanceRecord> Write(std::vector<TestData> dataItems, int itemsP
       data.mv_data = sval;
        
       rc = mdb_put(txn, dbi, &key, &data, 0);
+      
+      if(rc != 0){
+	cout << "Unable to PUT: " << rc << endl;
+	throw exception();
+      }
     }
     
     rc = mdb_txn_commit(txn);
@@ -150,26 +149,32 @@ std::vector<PerformanceRecord> Write(std::vector<TestData> dataItems, int itemsP
     time(&t);
     
     PerformanceRecord r;
-    r.Duration = ToMiliseconds(clock(), sw);
+    
+    chrono::duration<double> timeInSeconds  = chrono::system_clock::now() - sw;
+    
+    r.Duration = timeInSeconds.count() * 1000; // in miliseconds
     r.ProcessedItems = itemsPerTransaction;
     r.Time = t;
     
     records.push_back(r);
   }
   
-  endTime = clock();
+  end = chrono::system_clock::now();
   
-  double secs = ToSeconds(endTime, startTime);
+  chrono::duration<double> elapsed_seconds = end - start;
+  
+  double secs = elapsed_seconds.count();
   
   mdb_env_close(env);
   
-  std::cout << "Wrote " << numberOfTransactions * itemsPerTransaction << " items in " << secs << " sec, " << (numberOfTransactions * itemsPerTransaction)/secs << " ops/s" << std::endl;
+  cout << "Wrote " << numberOfTransactions * itemsPerTransaction << " items in " << secs << " sec, " << (numberOfTransactions * itemsPerTransaction)/secs << " ops/s" << endl;
   
+
   return records;
 };
 
-std::vector<PerformanceRecord> Read(std::vector<TestData> dataItems, int itemsPerTransaction, int numberOfTransactions) {
-  std::vector<PerformanceRecord> records;
+vector<PerformanceRecord> Read(vector<TestData> dataItems, int itemsPerTransaction, int numberOfTransactions) {
+  vector<PerformanceRecord> records;
   
   int rc;
   MDB_env *env;
@@ -177,18 +182,16 @@ std::vector<PerformanceRecord> Read(std::vector<TestData> dataItems, int itemsPe
 
   MDB_stat mst;
   MDB_cursor *cursor;
-  char sval[87 * 1024]; //TODO
+  char sval[87 * 1024];
 
   rc = mdb_env_create(&env);
-  rc = mdb_env_set_mapsize(env, 1024*1024*1024*1);
-  rc = mdb_env_set_flags(env, MDB_WRITEMAP, 1);
-  rc = mdb_env_open(env, "./lmdb_test", 0, 0664);
+  rc = mdb_env_set_mapsize(env, 1024*1024*1024*(long)10);
+  rc = mdb_env_open(env, "./lmdb_test", MDB_WRITEMAP, 0664);
 
-  
-  int startTime, endTime;
   time_t t;
   
-  startTime = clock();
+  chrono::time_point<chrono::system_clock> start, end;
+  start = chrono::system_clock::now();
   
   MDB_txn *txn;
   MDB_dbi dbi;
@@ -196,34 +199,43 @@ std::vector<PerformanceRecord> Read(std::vector<TestData> dataItems, int itemsPe
   rc = mdb_txn_begin(env, NULL, 0, &txn);
   rc = mdb_open(txn, NULL, 0, &dbi);
     
-    for(int i = 0; i < dataItems.size(); i++) {
-      TestData item = dataItems.at(i);
+  for(int i = 0; i < dataItems.size(); i++) {
+    TestData item = dataItems.at(i);
+    
+    key.mv_size = sizeof(int);
+    key.mv_data = &item.Id;
+    
+    rc = mdb_get(txn, dbi, &key, &data);
+    
+    if(rc != 0)
+    {
+      cout << "GET exception: " << rc << endl;
       
-      key.mv_size = sizeof(int);
-      key.mv_data = &item.Id;
-      
-      mdb_get(txn, dbi, &key, &data);
+      throw exception();
     }
+  }
     
   rc = mdb_txn_commit(txn);
   mdb_close(env, dbi);
   
   time(&t);
     
-  endTime = clock();
+  end = chrono::system_clock::now();
+  
+  chrono::duration<double> elapsed_seconds = end - start;
+  
+  double secs = elapsed_seconds.count();
   
   PerformanceRecord r;
-  r.Duration = ToMiliseconds(endTime, startTime);
+  r.Duration = secs * 1000; // in miliseconds
   r.ProcessedItems = numberOfTransactions * itemsPerTransaction;
   r.Time = t;
   
   records.push_back(r);
   
-  double secs = ToSeconds(endTime, startTime);
-  
   mdb_env_close(env);
   
-  std::cout << "Read " << numberOfTransactions * itemsPerTransaction << " items in " << secs << " sec, " << (numberOfTransactions * itemsPerTransaction)/secs << " ops/s" << std::endl;
+  cout << "Read " << numberOfTransactions * itemsPerTransaction << " items in " << secs << " sec, " << (numberOfTransactions * itemsPerTransaction)/secs << " ops/s" << endl;
   
   return records;
 };
@@ -233,20 +245,25 @@ int main(int argc,char * argv[])
   
   srand(time(NULL));
 
-  int writeTransactions = 10 * 1000;
+  int writeTransactions = 1 * 1000;
   int itemsPerTransaction = 100;
   int readItems = writeTransactions * itemsPerTransaction;
 
-  std::vector<TestData> sequentialIds = InitSequentialNumbers(readItems, 128, 128);
-  std::vector<TestData> randomIds = InitRandomNumbers(readItems, 128, 128);
+  vector<TestData> sequentialIds = InitSequentialNumbers(readItems, 128, 128);
+  vector<TestData> randomIds = InitRandomNumbers(readItems, 128, 128);
 
-  std::vector<TestData> sequentialIdsLarge = InitSequentialNumbers(readItems, 512, 87 * 1024);
-  std::vector<TestData> randomIdsLarge = InitRandomNumbers(readItems, 512, 87 * 1024);
+  vector<TestData> sequentialIdsLarge = InitSequentialNumbers(readItems, 512, 87 * 1024);
+  vector<TestData> randomIdsLarge = InitRandomNumbers(readItems, 512, 87 * 1024);
 
-  std::vector<PerformanceRecord> records = Write(sequentialIds, itemsPerTransaction, writeTransactions);
+  vector<PerformanceRecord> records = Write(sequentialIds, itemsPerTransaction, writeTransactions);
   WritePerfData("WriteSeq", records);
   records = Read(sequentialIds, itemsPerTransaction, writeTransactions);
   WritePerfData("ReadSeq", records);
+  
+  records = Write(randomIds, itemsPerTransaction, writeTransactions);
+  WritePerfData("WriteRandom", records);
+  records = Read(randomIds, itemsPerTransaction, writeTransactions);
+  WritePerfData("ReadRandom", records);
 
   records = Write(sequentialIdsLarge, itemsPerTransaction, writeTransactions);
   WritePerfData("WriteLargeSeq", records);
