b7f5676d711bc0e0a5489bc217261e9c71e048b8
Michael Yarichuk <michael.yarichuk@gmail.com>
2013-11-13 11:48:06 +0200

    added another test --> related to issue 1472

--- Raven.Tests/Issues/RavenDB_1472.cs
+++ Raven.Tests/Issues/RavenDB_1472.cs
@@ -18,6 +18,33 @@ namespace Raven.Tests.Issues
 		}
 
 		[Fact]
+		public void Multiple_terms_queries_with_separate_Where_statements_should_return_intersection_of_results()
+		{
+			using (var store = NewDocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					session.Store(new Cat(), "cats/1");
+					session.Store(new Cat(), "cats/2");
+					session.Store(new Cat(), "cats/3");
+					session.SaveChanges();
+				}
+
+				using (var session = store.OpenSession())
+				{
+					var cats = session.Query<Cat>()
+						.Customize(q => q.WaitForNonStaleResultsAsOfLastWrite())
+						.Where(cat => cat.Id.In("cats/1", "cats/2"))
+						.Where(cat => cat.Id.In("cats/2", "cats/3"))
+						.ToList();
+
+					Assert.Equal(1, cats.Count);
+					Assert.Equal("cats/2", cats.First().Id);
+				}
+			}
+		}
+
+		[Fact]
 		public void Multiple_terms_queries_with_AND_operator_should_return_intersection_of_results()
 		{
 			using (var store = NewDocumentStore())
