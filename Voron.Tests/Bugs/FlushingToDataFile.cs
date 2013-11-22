4c1022cf872a38f43f57a719e70c665295cb1479
Arkadiusz Palinski <arek@ais.pl>
2013-11-22 09:09:54 +0100

    Added failing tests that exposes the issue related to concurrent reading and flushing

---
+++ Voron.Tests/Bugs/FlushingToDataFile.cs
@@ -0,0 +1,70 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ConcurrentReadingAndFlushing.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.IO;
+using Voron.Impl;
+using Xunit;
+
+namespace Voron.Tests.Bugs
+{
+	public class FlushingToDataFile : StorageTest
+	{
+		protected override void Configure(StorageEnvironmentOptions options)
+		{
+			options.ManualFlushing = true;
+			options.MaxLogFileSize = 2 * AbstractPager.PageSize;
+		}
+
+		[Fact]
+		public unsafe void ConcurrentReadingAndFlushing()
+		{
+			var value1 = new byte[4000];
+
+			new Random().NextBytes(value1);
+
+			using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
+			{
+				tx.State.Root.Add(tx, "foo/0", new MemoryStream(value1));
+
+				tx.Commit();
+			}
+
+			using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
+			{
+				tx.State.Root.Add(tx, "foo/1", new MemoryStream(value1));
+
+				tx.Commit();
+			}
+
+			using (var tx = Env.NewTransaction(TransactionFlags.Read))
+			{
+				Env.FlushLogToDataFile();
+
+				using (var txw = Env.NewTransaction(TransactionFlags.ReadWrite))
+				{
+					// empty transaction is enough to expose the issue because it allocates 1 page in the scratch space for the transaction header
+					txw.Commit();
+				}
+
+				for (var i = 0; i < 2; i++)
+				{
+					var readResult = tx.State.Root.Read(tx, "foo/" + i);
+
+					Assert.NotNull(readResult);
+					Assert.Equal(value1.Length, readResult.Stream.Length);
+
+					var memoryStream = new MemoryStream();
+					readResult.Stream.CopyTo(memoryStream);
+
+					fixed (byte* b = value1)
+					fixed (byte* c = memoryStream.GetBuffer())
+						Assert.Equal(0, NativeMethods.memcmp(b, c, value1.Length));
+				}
+			}
+		}
+	}
+}
