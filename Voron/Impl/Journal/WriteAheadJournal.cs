54fbd4e20318eb65dd83af1f07eac60d6d555d7c
Pawel Pekrol <ppekrol@ais.pl>
2013-11-27 12:33:39 +0100

    validating _lastSyncedPage calculation during debug in journal applicator

--- Voron/Impl/Journal/WriteAheadJournal.cs
+++ Voron/Impl/Journal/WriteAheadJournal.cs
@@ -455,6 +455,22 @@ namespace Voron.Impl.Journal
 
 				    _lastSyncedPage += lastPage - 1;
 
+#if DEBUG
+                    var txHeaders = stackalloc TransactionHeader[1];
+                    var readTxHeader = &txHeaders[0];
+                    var jrnl = _waj.Files.First(x => x.Number == _lastSyncedJournal);
+                    var read = jrnl.ReadTransaction(_lastSyncedPage + 1, readTxHeader);
+
+                    if (readTxHeader->HeaderMarker != 0 && (read == false || readTxHeader->HeaderMarker != Constants.TransactionHeaderMarker))
+                    {
+                        throw new InvalidOperationException(
+                            "Reading transaction for calculated page in journal failed. "
+                            + "Journal #" + lastProcessedJournal + ". "
+                            + "Page #" + _lastSyncedPage + 1 + ". "
+                            + "This means that page calculation is wrong and should be fixed otherwise data will be lost during database recovery from this journal.");
+                    }
+#endif
+
                     if (alreadyInWriteTx)
                         _waj._dataPager.EnsureContinuous(transaction, last.PageNumber, lastPage);
                     else
