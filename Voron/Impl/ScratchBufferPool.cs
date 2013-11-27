aaf9034b920394815188b8215da35db7fc6b1ddc
Arkadiusz Palinski <arek@ais.pl>
2013-11-27 08:27:23 +0100

    RavenDB-1486 Voron: ensure free pages on tx rollback/dispose

--- Voron/Impl/ScratchBufferPool.cs
+++ Voron/Impl/ScratchBufferPool.cs
@@ -92,5 +92,27 @@ namespace Voron.Impl
 		public long PositionInScratchBuffer;
 		public long Size;
 		public int NumberOfPages;
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != this.GetType()) return false;
+
+			var other = (PageFromScratchBuffer) obj;
+
+			return PositionInScratchBuffer == other.PositionInScratchBuffer && Size == other.Size && NumberOfPages == other.NumberOfPages;
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				var hashCode = PositionInScratchBuffer.GetHashCode();
+				hashCode = (hashCode * 397) ^ Size.GetHashCode();
+				hashCode = (hashCode * 397) ^ NumberOfPages;
+				return hashCode;
+			}
+		}
 	}
 }
