950726cebdc30da7008038f19563e47e44246d51
Ayende Rahien <ayende@ayende.com>
2013-12-10 04:39:57 +0200

    Adding another perf project

---
+++ Performance.Comparison/FreeDB.Playground/Disk.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace XmcdParser
+{
+	public class Disk
+	{
+		public string Title { get; set; }
+		public string Artist { get; set; }
+		public int DiskLength { get; set; }
+		public string Genre { get; set; }
+		public int Year { get; set; }
+		public List<string> DiskIds { get; set; }
+
+		public List<int> TrackFramesOffsets { get; set; }
+		public List<string> Tracks { get; set; }
+		public Dictionary<string, string> Attributes { get; set; }
+		public Disk()
+		{
+			TrackFramesOffsets = new List<int>();
+			Tracks = new List<string>();
+			DiskIds = new List<string>();
+			Attributes = new Dictionary<string, string>();
+		}
+	}
+}
---
+++ Performance.Comparison/FreeDB.Playground/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32.SafeHandles;
+
+namespace FreeDB.Playground
+{
+	class Program
+	{
+		static void Main()
+		{
+			var x = new XmcdFileParser(@"C:\Users\Ayende\Downloads\freedb-complete-20130901.tar.bz2", disk => { });
+			x.Start();
+		}
+
+	}
+}
