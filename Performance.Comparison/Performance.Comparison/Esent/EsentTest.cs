352a5cac122dd4fc7175e9a10825af331817a79d
Arkadiusz Palinski <arek@ais.pl>
2013-12-12 11:38:43 +0100

    Changing the esent column type to allow put values greater than 255 bytes

--- Performance.Comparison/Performance.Comparison/Esent/EsentTest.cs
+++ Performance.Comparison/Performance.Comparison/Esent/EsentTest.cs
@@ -32,12 +32,6 @@
             _database = Path.Combine(_path, "edbtest.db");
         }
 
-        ~EsentTest()
-        {
-            if (Directory.Exists(_path))
-                Directory.Delete(_path, true);
-        }
-
         public Instance CreateInstance(bool delete = true)
         {
             if (delete && Directory.Exists(_path))
@@ -101,7 +95,7 @@
 
                     var secondaryColumn = new JET_COLUMNDEF
                     {
-                        coltyp = JET_coltyp.Binary
+                        coltyp = JET_coltyp.LongBinary,
                     };
 
                     JET_COLUMNID primaryColumnId;
