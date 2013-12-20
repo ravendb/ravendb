2a4f10adaf831062bc3ec4d1110fae8ee80122ab
JudahGabriel <judahgabriel@gmail.com>
2013-12-19 18:26:30 -0600

    First step on status/logs

--- Raven.Studio.Html5/App/viewmodels/logs.js
+++ Raven.Studio.Html5/App/viewmodels/logs.js
@@ -1,9 +1,26 @@
-define(["require", "exports"], function(require, exports) {
-    var logs = (function () {
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+define(["require", "exports", "commands/getLogsCommand", "viewmodels/activeDbViewModelBase", "models/database"], function(require, exports, getLogsCommand, activeDbViewModelBase, database) {
+    var logs = (function (_super) {
+        __extends(logs, _super);
         function logs() {
+            _super.apply(this, arguments);
+            this.fetchedLogs = ko.observableArray();
         }
+        logs.prototype.activate = function (args) {
+            var _this = this;
+            _super.prototype.activate.call(this, args);
+
+            new getLogsCommand(this.activeDatabase()).execute().done(function (results) {
+                return _this.fetchedLogs(results);
+            });
+        };
         return logs;
-    })();
+    })(activeDbViewModelBase);
 
     
     return logs;
--- Raven.Studio.Html5/App/viewmodels/logs.ts
+++ Raven.Studio.Html5/App/viewmodels/logs.ts
@@ -1,6 +1,19 @@
-class logs {
+import getLogsCommand = require("commands/getLogsCommand");
+import activeDbViewModelBase = require("viewmodels/activeDbViewModelBase");
+import getDatabaseStatsCommand = require("commands/getDatabaseStatsCommand");
+import database = require("models/database");
+import moment = require("moment");
 
-    constructor() {
+class logs extends activeDbViewModelBase {
+
+    fetchedLogs = ko.observableArray<logDto>();
+
+    activate(args) {
+        super.activate(args);
+
+        new getLogsCommand(this.activeDatabase())
+            .execute()
+            .done((results: logDto[]) => this.fetchedLogs(results));
     }
 }
 
