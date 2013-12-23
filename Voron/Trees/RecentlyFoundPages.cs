9a4a5ee27341434a270c861ffffc4a86d15c5fee
Ayende Rahien <ayende@ayende.com>
2013-12-23 16:48:19 +0200

    Better logic for finding pages

--- Voron/Trees/RecentlyFoundPages.cs
+++ Voron/Trees/RecentlyFoundPages.cs
@@ -11,88 +11,94 @@ using Voron.Impl;
 
 namespace Voron.Trees
 {
-	public unsafe class RecentlyFoundPages
-	{
-		public class FoundPage
-		{
-			public long Number;
-			public Slice FirstKey;
-			public Slice LastKey;
-			public readonly long[] CursorPath;
+    public unsafe class RecentlyFoundPages
+    {
+        public class FoundPage
+        {
+            public long Number;
+            public Slice FirstKey;
+            public Slice LastKey;
+            public readonly long[] CursorPath;
 
-		    public FoundPage(int size)
-		    {
-		        CursorPath = new long[size];
-		    }
-		}
+            public FoundPage(int size)
+            {
+                CursorPath = new long[size];
+            }
+        }
 
-	    private readonly FoundPage[] _cache;
+        private readonly FoundPage[] _cache;
 
-	    private readonly int _cacheSize;
+        private readonly int _cacheSize;
 
-	    public RecentlyFoundPages(int cacheSize)
-	    {
+        public RecentlyFoundPages(int cacheSize)
+        {
             _cache = new FoundPage[cacheSize];
-	        _cacheSize = cacheSize;
-	    }
+            _cacheSize = cacheSize;
+        }
 
-	    public void Add(FoundPage page)
-		{
+        public void Add(FoundPage page)
+        {
 #if DEBUG
-	        if ((page.FirstKey.Options==SliceOptions.BeforeAllKeys) && (page.LastKey.Options == SliceOptions.AfterAllKeys))
-	        {
-	            Debug.Assert(page.CursorPath.Length == 1);
-	        }
+            if ((page.FirstKey.Options == SliceOptions.BeforeAllKeys) && (page.LastKey.Options == SliceOptions.AfterAllKeys))
+            {
+                Debug.Assert(page.CursorPath.Length == 1);
+            }
 #endif
 
             for (int i = 0; i < _cacheSize; i++)
-		    {
+            {
                 if (_cache[i] == null || _cache[i].Number == page.Number)
                 {
                     _cache[0] = page;
                     return;
                 }
-		    }
+            }
 
-	        for (int i = 1; i < _cacheSize; i++)
-	        {
-	            _cache[i] = _cache[i - 1];
-	        }
-		    _cache[0] = page;
-		}
+            for (int i = 1; i < _cacheSize; i++)
+            {
+                _cache[i] = _cache[i - 1];
+            }
+            _cache[0] = page;
+        }
 
-		public FoundPage Find(Slice key)
-		{
+        public FoundPage Find(Slice key)
+        {
             for (int i = 0; i < _cache.Length; i++)
-		    {
-		        var page = _cache[i];
-                if(page == null)
+            {
+                var page = _cache[i];
+                if (page == null)
                     continue;
 
                 var first = page.FirstKey;
                 var last = page.LastKey;
+                
+                switch (key.Options)
+                {
+                    case SliceOptions.BeforeAllKeys:
+                        if (first.Options == SliceOptions.BeforeAllKeys)
+                            return page;
+                        break;
+                    case SliceOptions.AfterAllKeys:
+                        if (last.Options == SliceOptions.AfterAllKeys)
+                            return page;
+                        break;
+                    case SliceOptions.Key:
+                        if ((first.Options != SliceOptions.BeforeAllKeys && key.Compare(first, NativeMethods.memcmp) < 0))
+                            continue;
+                        if (last.Options != SliceOptions.AfterAllKeys && key.Compare(last, NativeMethods.memcmp) > 0)
+                            continue;
+                        return page;
+                    default:
+                        throw new ArgumentException(key.Options.ToString());
+                }
+            }
 
-                if (key.Options == SliceOptions.BeforeAllKeys && first.Options != SliceOptions.BeforeAllKeys)
-                    return null;
+            return null;
+        }
 
-                if (key.Options == SliceOptions.AfterAllKeys && last.Options != SliceOptions.AfterAllKeys)
-                    return null;
-
-                Debug.Assert(key.Options == SliceOptions.Key);
-
-                if (first.Options != SliceOptions.BeforeAllKeys && key.Compare(first, NativeMethods.memcmp) < 0 ||
-                    last.Options != SliceOptions.AfterAllKeys && key.Compare(last, NativeMethods.memcmp) > 0)
-                    return null;
-
-		        return page;
-		    }
-
-		    return null;
-		}
-
-		public void Clear()
-		{
-		    Array.Clear(_cache, 0, _cacheSize);
-		}
-	}
+        public void Clear()
+        {
+            Array.Clear(_cache, 0, _cacheSize);
+        }
+    }
 }
