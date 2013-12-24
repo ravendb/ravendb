521e54336042ca5c6b2b21048558376e034cecfb
Judah Gabriel Himango <judahgabriel@gmail.com>
2013-12-23 18:58:14 -0600

    Removing generated JS files and .js.map files

--- Raven.Studio.Html5/App/viewmodels/status.ts
+++ Raven.Studio.Html5/App/viewmodels/status.ts
@@ -5,10 +5,11 @@ import activeDbViewModelBase = require("viewmodels/activeDbViewModelBase");
 class status extends activeDbViewModelBase {
 
     router: DurandalRootRouter;
+    currentRouteTitle: KnockoutComputed<string>;
 
 	constructor() {
         super();
-
+        
         this.router = durandalRouter.createChildRouter()
             .map([
                 { route: 'status',                  moduleId: 'viewmodels/statistics',          title: 'Stats',             nav: true },
@@ -18,7 +19,13 @@ class status extends activeDbViewModelBase {
                 { route: 'status/replicationStats', moduleId: 'viewmodels/replicationStats',    title: 'Replication Stats', nav: true },
                 { route: 'status/userInfo',         moduleId: 'viewmodels/userInfo',            title: 'User Info',         nav: true }
 			])
-			.buildNavigationModel();
+            .buildNavigationModel();
+        this.currentRouteTitle = ko.computed(() => {
+            // Is there a better way to get the active route?
+            var activeRoute = this.router.navigationModel().first<DurandalRouteConfiguration>(r => r.isActive());
+            console.log("returning: ", activeRoute != null ? activeRoute.title : "nuttin'");
+            return activeRoute != null ? activeRoute.title : "";
+        });
     }
 }
 
