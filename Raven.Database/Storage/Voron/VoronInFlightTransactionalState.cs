52440d9e41f6d35b36a78b752a5942ac392361e5
Michael Yarichuk <michael.yarichuk@gmail.com>
2014-01-02 16:23:39 +0200

    voron DTC implementation is more compatible with esent (now must call prepare before commit in both)

--- Raven.Database/Storage/Voron/VoronInFlightTransactionalState.cs
+++ Raven.Database/Storage/Voron/VoronInFlightTransactionalState.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Collections.Concurrent;
+using System.Threading;
+using Raven.Abstractions;
 using Raven.Abstractions.Data;
 using Raven.Database.Impl.DTC;
+using Raven.Database.Util;
 using Raven.Json.Linq;
 using Raven.Storage.Voron;
 
@@ -12,19 +13,41 @@ namespace Raven.Database.Storage.Voron
     public class VoronInFlightTransactionalState : InFlightTransactionalState
     {
         private readonly TransactionalStorage storage;
+	    private readonly ConcurrentSet<string> preparedTransactions;
 
         public VoronInFlightTransactionalState(Func<string, Etag, RavenJObject, RavenJObject, TransactionInformation, PutResult> databasePut, Func<string, Etag, TransactionInformation, bool> databaseDelete, TransactionalStorage storage) : base(databasePut, databaseDelete)
         {
             this.storage = storage;
+			preparedTransactions = new ConcurrentSet<string>();   
         }
 
-        public override void Commit(string id)
-        {
-            storage.Batch(accessor => RunOperationsInTransaction(id));
-        }
+	    public override void Rollback(string txId)
+	    {
+		    base.Rollback(txId);
+		    preparedTransactions.TryRemove(txId);
+	    }
 
-        public override void Prepare(string id)
+	    public override void Commit(string txId)
         {
-        }
+			//the exception is thrown here for compatibility reasons - with Esent DTC implementation
+			if (!preparedTransactions.Contains(txId))
+				throw new InvalidOperationException(String.Format("There is no transaction with id: {0} ready to commit. Did you call PrepareTransaction?", txId));
+
+			try
+			{
+				storage.Batch(accessor => RunOperationsInTransaction(txId));
+			}
+			catch (Exception)
+			{
+				Rollback(txId);
+				throw;
+			}
+		}
+
+        public override void Prepare(string txId)
+        {
+			//in Voron there has no support for two-phase commit - so no work on Voron itself is done during this stage
+			preparedTransactions.Add(txId);
+		}
     }
 }
