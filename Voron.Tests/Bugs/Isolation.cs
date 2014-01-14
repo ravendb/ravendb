a07708260a3c497f1886bd3f6ff7420f73fddaf1
Arkadiusz Palinski <arek@ais.pl>
2014-01-14 11:54:25 +0100

    minor

--- Voron.Tests/Bugs/Isolation.cs
+++ Voron.Tests/Bugs/Isolation.cs
@@ -56,6 +56,8 @@
                             txw.Commit();
                         }
 
+						Assert.Equal(currentKey, iterator.CurrentKey.ToString());
+
 						using (var txw = env.NewTransaction(TransactionFlags.ReadWrite))
                         {
                             var tree = txw.Environment.State.GetTree(txw, "tree1");
