1932610b878e5e1ab1644bcce054fbd52f6b98ac
Ayende Rahien <ayende@ayende.com>
2014-01-19 13:32:22 +0200

    More work to get time series stuff properly working

--- Performance.Comparison/TimeSeries.Playground/DateTimeSeries.cs
+++ Performance.Comparison/TimeSeries.Playground/DateTimeSeries.cs
@@ -8,91 +8,125 @@ using Voron.Util.Conversion;
 
 namespace TimeSeries.Playground
 {
-    public class DateTimeSeries : IDisposable
-    {
-        private readonly StorageEnvironment _storageEnvironment;
-        private long _last;
-        private readonly Slice _lastKey;
+	public class DateTimeSeries : IDisposable
+	{
+		private readonly StorageEnvironment _storageEnvironment;
+		private long _last;
+		private readonly Slice _lastKey;
 
-        public DateTimeSeries(string path)
-        {
-            _lastKey = "last-key";
-            _storageEnvironment = new StorageEnvironment(StorageEnvironmentOptions.ForPath(path));
-            using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.ReadWrite))
-            {
-                _storageEnvironment.CreateTree(tx, "data");
-                var read = tx.State.Root.Read(tx, _lastKey);
+		public DateTimeSeries(string path)
+		{
+			_lastKey = "last-key";
+			_storageEnvironment = new StorageEnvironment(StorageEnvironmentOptions.ForPath(path));
+			using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.ReadWrite))
+			{
+				var read = tx.State.Root.Read(tx, _lastKey);
 
-                _last = read != null ? read.Reader.ReadInt64() : 1;
+				_last = read != null ? read.Reader.ReadInt64() : 1;
 
-                tx.Commit();
-            }
-        }
+				tx.Commit();
+			}
+		}
 
-        public void Add(string id, DateTime dateTime, double value)
-        {
-            AddRange(id, new []{new KeyValuePair<DateTime, double>(dateTime, value), });
-        }
+		private int _bufferSize;
+		private readonly Dictionary<string, List<KeyValuePair<DateTime, double>>> _buffer = new Dictionary<string, List<KeyValuePair<DateTime, double>>>();
 
-        public void AddRange(string id, IEnumerable<KeyValuePair<DateTime, double>> values)
-        {
-            using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.ReadWrite))
-            {
-                var data = _storageEnvironment.State.GetTree(tx, "data");
-                var buffer = new byte[16];
-                var key = new Slice(buffer);
-                var ms = new MemoryStream();
-                var bw = new BinaryWriter(ms);
-                foreach (var kvp in values)
-                {
-                    var date = kvp.Key;
-                    EndianBitConverter.Big.CopyBytes(date.ToBinary(), buffer, 0);
-                    EndianBitConverter.Big.CopyBytes(_last++, buffer, 8);
-                    ms.SetLength(0);
-                    bw.Write(kvp.Value);
-                    ms.Position = 0;
+		public void Add(string id, DateTime dateTime, double value)
+		{
+			List<KeyValuePair<DateTime, double>> list;
+			if (_buffer.TryGetValue(id, out list) == false)
+				_buffer[id] = list = new List<KeyValuePair<DateTime, double>>();
 
-                    data.Add(tx, key, ms);
-                }
+			list.Add(new KeyValuePair<DateTime, double>(dateTime, value));
+			_bufferSize++;
+			if (_bufferSize > 1000)
+				Flush();
+		}
 
-                tx.State.Root.Add(tx, _lastKey, new MemoryStream(BitConverter.GetBytes(_last)));
-                tx.Commit();
-            }
-        }
+		public IEnumerable<string> ScanIds()
+		{
+			using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.Read))
+			{
+				using (var it = tx.State.Root.Iterate(tx))
+				{
+					var prefix = "channel:";
+					it.RequiredPrefix = prefix;
+					if (it.Seek(it.RequiredPrefix) == false)
+						yield break;
+					do
+					{
+						var key = it.CurrentKey.ToString();
+						yield return key.Substring(prefix.Length);
+					} while (it.MoveNext());
+				}
+			}
+		}
 
-        public IEnumerable<double> ScanRange(string id, DateTime start, DateTime end)
-        {
-            using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.Read))
-            {
-                var data = _storageEnvironment.State.GetTree(tx, "data");
-                var startBuffer = new byte[16];
-                EndianBitConverter.Big.CopyBytes(start.ToBinary(), startBuffer, 0);
-                var startKey = new Slice(startBuffer);
+		public void Flush()
+		{
+			_bufferSize = 0;
 
-                using (var it = data.Iterate(tx))
-                {
-                    var endBuffer = new byte[16];
-                    EndianBitConverter.Big.CopyBytes(end.ToBinary(), endBuffer, 0);
-                    EndianBitConverter.Big.CopyBytes(long.MaxValue, endBuffer, 8);
+			using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.ReadWrite))
+			{
+				foreach (var kvp in _buffer)
+				{
+					var data = _storageEnvironment.CreateTree(tx, "channel:" + kvp.Key);
+					var buffer = new byte[16];
+					var key = new Slice(buffer);
+					var ms = new MemoryStream();
+					var bw = new BinaryWriter(ms);
+					foreach (var item in kvp.Value)
+					{
+						var date = item.Key;
+						EndianBitConverter.Big.CopyBytes(date.Ticks, buffer, 0);
+						EndianBitConverter.Big.CopyBytes(_last++, buffer, 8);
+						ms.SetLength(0);
+						bw.Write(item.Value);
+						ms.Position = 0;
 
-                    it.MaxKey = new Slice(endBuffer);
-                    if (it.Seek(startKey) == false)
-                        yield break;
-                    var buffer = new byte[sizeof (double)];
-                    do
-                    {
-                        var reader = it.CreateReaderForCurrent();
-                        var n = reader.Read(buffer,0, sizeof(double));
-                        Debug.Assert(n == sizeof(double));
-                        yield return BitConverter.ToDouble(buffer, 0);
-                    } while (it.MoveNext());
-                }
-            }
-        }
+						data.Add(tx, key, ms);
+					}
+				}
 
-        public void Dispose()
-        {
-            _storageEnvironment.Dispose();
-        }
-    }
+				tx.State.Root.Add(tx, _lastKey, new MemoryStream(BitConverter.GetBytes(_last)));
+				tx.Commit();
+			}
+			_buffer.Clear();
+		}
+
+		public IEnumerable<double> ScanRange(string id, DateTime start, DateTime end)
+		{
+			using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.Read))
+			{
+				var data = _storageEnvironment.State.GetTree(tx, "channel:" + id);
+				var startBuffer = new byte[16];
+				EndianBitConverter.Big.CopyBytes(start.Ticks, startBuffer, 0);
+				var startKey = new Slice(startBuffer);
+
+				using (var it = data.Iterate(tx))
+				{
+					var endBuffer = new byte[16];
+					EndianBitConverter.Big.CopyBytes(end.Ticks, endBuffer, 0);
+					EndianBitConverter.Big.CopyBytes(long.MaxValue, endBuffer, 8);
+
+					it.MaxKey = new Slice(endBuffer);
+					if (it.Seek(startKey) == false)
+						yield break;
+					var buffer = new byte[sizeof(double)];
+					do
+					{
+						var reader = it.CreateReaderForCurrent();
+						var n = reader.Read(buffer, 0, sizeof(double));
+						Debug.Assert(n == sizeof(double));
+						yield return BitConverter.ToDouble(buffer, 0);
+					} while (it.MoveNext());
+				}
+			}
+		}
+
+		public void Dispose()
+		{
+			_storageEnvironment.Dispose();
+		}
+	}
 }
--- Performance.Comparison/TimeSeries.Playground/Program.cs
+++ Performance.Comparison/TimeSeries.Playground/Program.cs
@@ -14,20 +14,41 @@ namespace TimeSeries.Playground
     {
         static void Main(string[] args)
         {
-			if(Directory.Exists(@".\dts"))
-				Directory.Delete(@".\dts", true);
+			//if (Directory.Exists(@".\dts"))
+			//	Directory.Delete(@".\dts", true);
             var dts = new DateTimeSeries(@".\dts");
-			//using (var parser = new TextFieldParser(@"D:\TimeSeries.csv"))
-			//{
-			//	parser.ReadLine();//ignore headers
+	        foreach (var id in dts.ScanIds())
+	        {
+		        Console.WriteLine(id);
+	        }
+	        for (int i = 0; i < 10; i++)
+	        {
+				var sp = Stopwatch.StartNew();
 
+				Console.WriteLine(dts.ScanRange("6518249a-9d0d-4e0a-9c08-0802f5c8c3d8", DateTime.MinValue, DateTime.MaxValue).Count());
+
+				Console.WriteLine(sp.Elapsed);
+	        }
+			//int i = 0;
+			//using (var parser = new TextFieldParser(@"C:\Users\Ayende\Downloads\TimeSeries.csv"))
+			//{
+			//	parser.HasFieldsEnclosedInQuotes = true;
+			//	parser.Delimiters = new[] { "," };
+			//	parser.ReadLine();//ignore headers
+			//	var startNew = Stopwatch.StartNew();
 			//	while (parser.EndOfData == false)
 			//	{
 			//		var fields = parser.ReadFields();
 			//		Debug.Assert(fields != null);
 
-			//		dts.Add(fields[1], DateTime.ParseExact(fields[2],"Z",CultureInfo.InvariantCulture), double.Parse(fields[3]));
+			//		dts.Add(fields[1], DateTime.ParseExact(fields[2], "o", CultureInfo.InvariantCulture), double.Parse(fields[3]));
+			//		i++;
+			//		if (i % 10000 == 0)
+			//			Console.Write("\r{0,15:#,#}          ", i);
 			//	}
+			//	dts.Flush();
+			//	Console.Write("\r{0,15:#,#}          ", i);
+			//	Console.WriteLine(startNew.Elapsed);
 			//}
         }
     }
