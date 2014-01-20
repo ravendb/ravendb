b8974ba1849e0f88abe5221ec78f458146b0b9d8
Ayende Rahien <ayende@ayende.com>
2014-01-20 14:41:18 +0200

    More work on time series

--- Performance.Comparison/TimeSeries.Playground/DateTimeSeries.cs
+++ Performance.Comparison/TimeSeries.Playground/DateTimeSeries.cs
@@ -2,131 +2,231 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Text;
 using Newtonsoft.Json;
 using Voron;
 using Voron.Util.Conversion;
 
 namespace TimeSeries.Playground
 {
-	public class DateTimeSeries : IDisposable
-	{
-		private readonly StorageEnvironment _storageEnvironment;
-		private long _last;
-		private readonly Slice _lastKey;
+    public class DateTimeSeries : IDisposable
+    {
+        private readonly StorageEnvironment _storageEnvironment;
+        private long _last;
+        private readonly Slice _lastKey;
 
-		public DateTimeSeries(string path)
-		{
-			_lastKey = "last-key";
-			_storageEnvironment = new StorageEnvironment(StorageEnvironmentOptions.ForPath(path));
-			using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.ReadWrite))
-			{
-				var read = tx.State.Root.Read(tx, _lastKey);
+        public DateTimeSeries(string path)
+        {
+            _lastKey = "last-key";
+            _storageEnvironment = new StorageEnvironment(StorageEnvironmentOptions.ForPath(path));
+            using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.ReadWrite))
+            {
+                var read = tx.State.Root.Read(tx, _lastKey);
 
-				_last = read != null ? read.Reader.ReadInt64() : 1;
+                _last = read != null ? read.Reader.ReadInt64() : 1;
 
-				tx.Commit();
-			}
-		}
+                tx.Commit();
+            }
+        }
 
-		private int _bufferSize;
-		private readonly Dictionary<string, List<KeyValuePair<DateTime, double>>> _buffer = new Dictionary<string, List<KeyValuePair<DateTime, double>>>();
+        private int _bufferSize;
+        private readonly Dictionary<string, List<Entry>> _buffer = new Dictionary<string, List<Entry>>();
 
-		public void Add(string id, DateTime dateTime, double value)
-		{
-			List<KeyValuePair<DateTime, double>> list;
-			if (_buffer.TryGetValue(id, out list) == false)
-				_buffer[id] = list = new List<KeyValuePair<DateTime, double>>();
+        public struct Entry
+        {
+            public readonly DateTime Timestamp;
+            public readonly double Value;
 
-			list.Add(new KeyValuePair<DateTime, double>(dateTime, value));
-			_bufferSize++;
-			if (_bufferSize > 1000)
-				Flush();
-		}
+            public Entry(DateTime dt, double value)
+            {
+                Timestamp = dt;
+                Value = value;
+            }
+        }
 
-		public IEnumerable<string> ScanIds()
-		{
-			using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.Read))
-			{
-				using (var it = tx.State.Root.Iterate(tx))
-				{
-					var prefix = "channel:";
-					it.RequiredPrefix = prefix;
-					if (it.Seek(it.RequiredPrefix) == false)
-						yield break;
-					do
-					{
-						var key = it.CurrentKey.ToString();
-						yield return key.Substring(prefix.Length);
-					} while (it.MoveNext());
-				}
-			}
-		}
+        public void Add(string id, DateTime dateTime, double value)
+        {
+            List<Entry> list;
+            if (_buffer.TryGetValue(id, out list) == false)
+                _buffer[id] = list = new List<Entry>();
 
-		public void Flush()
-		{
-			_bufferSize = 0;
+            list.Add(new Entry(dateTime, value));
+            _bufferSize++;
+            if (_bufferSize > 1000)
+                Flush();
+        }
 
-			using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.ReadWrite))
-			{
-				foreach (var kvp in _buffer)
-				{
-					var data = _storageEnvironment.CreateTree(tx, "channel:" + kvp.Key);
-					var buffer = new byte[16];
-					var key = new Slice(buffer);
-					var ms = new MemoryStream();
-					var bw = new BinaryWriter(ms);
-					foreach (var item in kvp.Value)
-					{
-						var date = item.Key;
-						EndianBitConverter.Big.CopyBytes(date.Ticks, buffer, 0);
-						EndianBitConverter.Big.CopyBytes(_last++, buffer, 8);
-						ms.SetLength(0);
-						bw.Write(item.Value);
-						ms.Position = 0;
+        public IEnumerable<string> ScanIds()
+        {
+            using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.Read))
+            {
+                using (var it = tx.State.Root.Iterate(tx))
+                {
+                    var prefix = "channel:";
+                    it.RequiredPrefix = prefix;
+                    if (it.Seek(it.RequiredPrefix) == false)
+                        yield break;
+                    do
+                    {
+                        var key = it.CurrentKey.ToString();
+                        yield return key.Substring(prefix.Length);
+                    } while (it.MoveNext());
+                }
+            }
+        }
 
-						data.Add(tx, key, ms);
-					}
-				}
+        public void Flush()
+        {
+            _bufferSize = 0;
 
-				tx.State.Root.Add(tx, _lastKey, new MemoryStream(BitConverter.GetBytes(_last)));
-				tx.Commit();
-			}
-			_buffer.Clear();
-		}
+            using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.ReadWrite))
+            {
+                foreach (var kvp in _buffer)
+                {
+                    var data = _storageEnvironment.CreateTree(tx, "channel:" + kvp.Key);
+                    var buffer = new byte[16];
+                    var key = new Slice(buffer);
+                    var ms = new MemoryStream();
+                    var bw = new BinaryWriter(ms);
+                    foreach (var item in kvp.Value)
+                    {
+                        var date = item.Timestamp;
+                        EndianBitConverter.Big.CopyBytes(date.Ticks, buffer, 0);
+                        EndianBitConverter.Big.CopyBytes(_last++, buffer, 8);
+                        ms.SetLength(0);
+                        bw.Write(item.Value);
+                        ms.Position = 0;
 
-		public IEnumerable<double> ScanRange(string id, DateTime start, DateTime end)
-		{
-			using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.Read))
-			{
-				var data = _storageEnvironment.State.GetTree(tx, "channel:" + id);
-				var startBuffer = new byte[16];
-				EndianBitConverter.Big.CopyBytes(start.Ticks, startBuffer, 0);
-				var startKey = new Slice(startBuffer);
+                        data.Add(tx, key, ms);
+                    }
+                }
 
-				using (var it = data.Iterate(tx))
-				{
-					var endBuffer = new byte[16];
-					EndianBitConverter.Big.CopyBytes(end.Ticks, endBuffer, 0);
-					EndianBitConverter.Big.CopyBytes(long.MaxValue, endBuffer, 8);
+                tx.State.Root.Add(tx, _lastKey, new MemoryStream(BitConverter.GetBytes(_last)));
+                tx.Commit();
+            }
+            _buffer.Clear();
+        }
 
-					it.MaxKey = new Slice(endBuffer);
-					if (it.Seek(startKey) == false)
-						yield break;
-					var buffer = new byte[sizeof(double)];
-					do
-					{
-						var reader = it.CreateReaderForCurrent();
-						var n = reader.Read(buffer, 0, sizeof(double));
-						Debug.Assert(n == sizeof(double));
-						yield return BitConverter.ToDouble(buffer, 0);
-					} while (it.MoveNext());
-				}
-			}
-		}
+        private class PendingEnumerator
+        {
+            public IEnumerator<Entry> Enumerator;
+            public int Index;
+        }
 
-		public void Dispose()
-		{
-			_storageEnvironment.Dispose();
-		}
-	}
+        private class PendingEnumerators
+        {
+            private readonly SortedDictionary<DateTime, List<PendingEnumerator>> _values =
+                new SortedDictionary<DateTime, List<PendingEnumerator>>();
+
+            public void Enqueue(PendingEnumerator entry)
+            {
+                List<PendingEnumerator> list;
+                var dateTime = entry.Enumerator.Current.Timestamp;
+                if (_values.TryGetValue(dateTime, out list) == false)
+                {
+                    _values.Add(dateTime, list = new List<PendingEnumerator>());
+                }
+                list.Add(entry);
+            }
+
+            public bool IsEmpty { get { return _values.Count == 0; } }
+
+            public List<PendingEnumerator> Dequeue()
+            {
+                if (_values.Count == 0)
+                    return new List<PendingEnumerator>();
+
+                var kvp = _values.First();
+                _values.Remove(kvp.Key);
+                return kvp.Value;
+            }
+        }
+
+        public class RangeEntry
+        {
+            public DateTime Timestamp;
+            public double?[] Values;
+        }
+
+        public IEnumerable<RangeEntry> ScanRanges(DateTime start, DateTime end, string[] ids)
+        {
+            if (ids == null || ids.Length == 0)
+                yield break;
+
+            var pending = new PendingEnumerators();
+            for (int i = 0; i < ids.Length; i++)
+            {
+                var enumerator = ScanRange(start, end, ids[i]).GetEnumerator();
+                if(enumerator.MoveNext() == false)
+                    continue;
+                pending.Enqueue(new PendingEnumerator
+                {
+                    Enumerator = enumerator,
+                    Index = i
+                });
+            }
+
+            var result = new RangeEntry
+            {
+                Values = new double?[ids.Length]
+            };
+            while (pending.IsEmpty == false)
+            {
+                Array.Clear(result.Values,0,result.Values.Length);
+                var entries = pending.Dequeue();
+                if (entries.Count == 0)
+                    break;
+                foreach (var entry in entries)
+                {
+                    var current = entry.Enumerator.Current;
+                    result.Timestamp = current.Timestamp;
+                    result.Values[entry.Index] = current.Value;
+                    if(entry.Enumerator.MoveNext())
+                        pending.Enqueue(entry);
+                }
+                yield return result;
+            }
+        }
+
+        public IEnumerable<Entry> ScanRange(DateTime start, DateTime end, string id)
+        {
+            using (var tx = _storageEnvironment.NewTransaction(TransactionFlags.Read))
+            {
+
+                var tree = _storageEnvironment.State.GetTree(tx, "channel:" + id);
+                var startBuffer = new byte[16];
+                EndianBitConverter.Big.CopyBytes(start.Ticks, startBuffer, 0);
+                var startKey = new Slice(startBuffer);
+
+                using (var it = tree.Iterate(tx))
+                {
+                    var endBuffer = new byte[16];
+                    EndianBitConverter.Big.CopyBytes(end.Ticks, endBuffer, 0);
+                    EndianBitConverter.Big.CopyBytes(long.MaxValue, endBuffer, 8);
+
+                    it.MaxKey = new Slice(endBuffer);
+                    if (it.Seek(startKey) == false)
+                        yield break;
+                    var valueBuffer = new byte[sizeof(double)];
+                    var keyBuffer = new byte[16];
+                    do
+                    {
+                        var reader = it.CreateReaderForCurrent();
+                        var n = reader.Read(valueBuffer, 0, sizeof(double));
+                        Debug.Assert(n == sizeof(double));
+                        it.CurrentKey.CopyTo(keyBuffer);
+                        var dt = new DateTime(EndianBitConverter.Big.ToInt64(keyBuffer, 0));
+                        var value = BitConverter.ToDouble(valueBuffer, 0);
+                        yield return new Entry(dt, value);
+                    } while (it.MoveNext());
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _storageEnvironment.Dispose();
+        }
+    }
 }
--- Performance.Comparison/TimeSeries.Playground/Program.cs
+++ Performance.Comparison/TimeSeries.Playground/Program.cs
@@ -14,42 +14,48 @@ namespace TimeSeries.Playground
     {
         static void Main(string[] args)
         {
-			//if (Directory.Exists(@".\dts"))
-			//	Directory.Delete(@".\dts", true);
+            //if (Directory.Exists(@".\dts"))
+            //    Directory.Delete(@".\dts", true);
             var dts = new DateTimeSeries(@".\dts");
-	        foreach (var id in dts.ScanIds())
-	        {
-		        Console.WriteLine(id);
-	        }
-	        for (int i = 0; i < 10; i++)
-	        {
-				var sp = Stopwatch.StartNew();
+            foreach (var id in dts.ScanIds())
+            {
+                Console.WriteLine(id);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                var sp = Stopwatch.StartNew();
 
-				Console.WriteLine(dts.ScanRange("6518249a-9d0d-4e0a-9c08-0802f5c8c3d8", DateTime.MinValue, DateTime.MaxValue).Count());
+                Console.WriteLine(dts.ScanRanges(DateTime.MinValue, DateTime.MaxValue, new[]
+                {
+                    "6febe146-e893-4f64-89f8-527f2dbaae9b",
+                    "707dcb42-c551-4f1a-9203-e4b0852516cf",
+                    "74d5bee8-9a7b-4d4e-bd85-5f92dfc22edb",
+                    "7ae29feb-6178-4930-bc38-a90adf99cfd3",
+                }).SelectMany(x => x.Values).Count(x => x != null));
 
-				Console.WriteLine(sp.Elapsed);
-	        }
-			//int i = 0;
-			//using (var parser = new TextFieldParser(@"C:\Users\Ayende\Downloads\TimeSeries.csv"))
-			//{
-			//	parser.HasFieldsEnclosedInQuotes = true;
-			//	parser.Delimiters = new[] { "," };
-			//	parser.ReadLine();//ignore headers
-			//	var startNew = Stopwatch.StartNew();
-			//	while (parser.EndOfData == false)
-			//	{
-			//		var fields = parser.ReadFields();
-			//		Debug.Assert(fields != null);
+                Console.WriteLine(sp.Elapsed);
+            }
+            //int i = 0;
+            //using (var parser = new TextFieldParser(@"D:\TimeSeries.csv"))
+            //{
+            //    parser.HasFieldsEnclosedInQuotes = true;
+            //    parser.Delimiters = new[] { "," };
+            //    parser.ReadLine();//ignore headers
+            //    var startNew = Stopwatch.StartNew();
+            //    while (parser.EndOfData == false)
+            //    {
+            //        var fields = parser.ReadFields();
+            //        Debug.Assert(fields != null);
 
-			//		dts.Add(fields[1], DateTime.ParseExact(fields[2], "o", CultureInfo.InvariantCulture), double.Parse(fields[3]));
-			//		i++;
-			//		if (i % 10000 == 0)
-			//			Console.Write("\r{0,15:#,#}          ", i);
-			//	}
-			//	dts.Flush();
-			//	Console.Write("\r{0,15:#,#}          ", i);
-			//	Console.WriteLine(startNew.Elapsed);
-			//}
+            //        dts.Add(fields[1], DateTime.ParseExact(fields[2], "o", CultureInfo.InvariantCulture), double.Parse(fields[3]));
+            //        i++;
+            //        if (i % 10000 == 0)
+            //            Console.Write("\r{0,15:#,#}          ", i);
+            //    }
+            //    dts.Flush();
+            //    Console.Write("\r{0,15:#,#}          ", i);
+            //    Console.WriteLine(startNew.Elapsed);
+            //}
         }
     }
 }
