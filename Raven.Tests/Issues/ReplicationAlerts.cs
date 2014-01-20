2f80eb59c5e548b1228044bdfceef4c308c35cad
Pawel Pekrol <ppekrol@ais.pl>
2014-01-20 13:50:15 +0100

    replication task should throw only one alert when source in replication destinations does not match current storage id

---
+++ Raven.Tests/Issues/ReplicationAlerts.cs
@@ -0,0 +1,67 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="T.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+namespace Raven.Tests.Issues
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Raven.Abstractions.Data;
+    using Raven.Abstractions.Smuggler;
+    using Raven.Json.Linq;
+    using Raven.Smuggler;
+    using Raven.Tests.Bundles.Replication;
+
+    using Xunit;
+
+    public class ReplicationAlerts : ReplicationBase
+    {
+        public ReplicationAlerts()
+        {
+            if (File.Exists(DumpFile))
+                File.Delete(DumpFile);
+        }
+
+        [Fact]
+        public async Task ImportingReplicationDestinationsDocumentWithInvalidSourceShouldReportOneAlertOnly()
+        {
+            var store1 = CreateStore();
+            var store2 = CreateStore();
+            var store3 = CreateStore();
+
+            TellFirstInstanceToReplicateToSecondInstance();
+
+            store2.Dispose();
+
+            store1.DatabaseCommands.Put("1", null, new RavenJObject(), new RavenJObject());
+            store1.DatabaseCommands.Put("2", null, new RavenJObject(), new RavenJObject());
+
+            var smuggler = new SmugglerApi(new SmugglerOptions(), new RavenConnectionStringOptions { Url = store1.Url });
+
+            smuggler.ExportData(null, new SmugglerOptions { BackupPath = DumpFile }, false).Wait(TimeSpan.FromSeconds(15));
+            Assert.True(File.Exists(DumpFile));
+
+            smuggler = new SmugglerApi(new SmugglerOptions(), new RavenConnectionStringOptions { Url = store3.Url });
+            smuggler.ImportData(File.Open(DumpFile, FileMode.Open), new SmugglerOptions()).Wait(TimeSpan.FromSeconds(15));
+
+            Assert.NotNull(store3.DatabaseCommands.Get("1"));
+            Assert.NotNull(store3.DatabaseCommands.Get("2"));
+
+            var container = store3.DatabaseCommands.Get("Raven/Alerts");
+            Assert.NotNull(container);
+
+            var alerts = container.DataAsJson["Alerts"].Values<RavenJObject>()
+                .ToList();
+            Assert.Equal(1, alerts.Count);
+
+            var alert = alerts.First();
+            Assert.True(alert["Title"].ToString().StartsWith("Wrong replication source:"));
+        }
+
+        protected string DumpFile = "dump.ravendump";
+    }
+}
