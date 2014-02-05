e2b7152eb4f3d31c13b40c1b8531e7621936f4da
Michael Yarichuk <michael.yarichuk@gmail.com>
2014-02-05 12:52:28 +0200

    merge of logs controllers

--- Raven.Database/Server/Controllers/LogsController.cs
+++ Raven.Database/Server/Controllers/LogsController.cs
@@ -9,8 +9,42 @@ using Raven.Database.Util;
 namespace Raven.Database.Server.Controllers
 {
 	[Route("logs/{action=logsget}")]
+	[Route("logs/ravenfs/{action=RavenFSLogsGet}")]
 	public class LogsController : RavenDbApiController
 	{
+		[HttpGet]		
+		public HttpResponseMessage RavenFSLogsGet(string type = null)
+		{
+			DatabaseMemoryTarget.BoundedMemoryTarget boundedMemoryTarget = null;
+			if (NLog.LogManager.Configuration != null && NLog.LogManager.Configuration.AllTargets != null)
+				boundedMemoryTarget = NLog.LogManager.Configuration.AllTargets.OfType<DatabaseMemoryTarget.BoundedMemoryTarget>().FirstOrDefault();
+
+			if (boundedMemoryTarget == null)
+				throw new HttpResponseException(Request.CreateErrorResponse((HttpStatusCode)420,
+																			"HttpEndpoint was not registered in the log configuration, logs endpoint disable"));
+
+			var log = boundedMemoryTarget.GeneralLog;
+
+			switch (type)
+			{
+				case "error":
+				case "warn":
+					log = boundedMemoryTarget.WarnLog;
+					break;
+			}
+
+			return Request.CreateResponse(HttpStatusCode.OK, log.Select(x => new
+			{
+				x.TimeStamp,
+				Message = x.FormattedMessage,
+				x.LoggerName,
+				Level = x.Level,
+				Exception =
+			x.Exception == null ? null : x.Exception.ToString()
+			}));
+		}
+
+
 		[HttpGet]
 		public HttpResponseMessage LogsGet()
 		{
