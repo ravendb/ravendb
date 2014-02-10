0137f0f7e7fe2847d6860963ca1bed5532c258ba
Pawel Pekrol <ppekrol@ais.pl>
2014-02-10 10:23:25 +0100

    fixed DebugJournal

--- Voron/Debugging/DebugJournal.cs
+++ Voron/Debugging/DebugJournal.cs
@@ -11,233 +11,273 @@ using Voron.Trees;
 
 namespace Voron.Debugging
 {
-	public class DebugJournal : IDisposable
-	{
-		public class ActivityEntry
-		{
-		    public DebugActionType ActionType { get; private set; }
+    public class DebugJournal : IDisposable
+    {
+        public class ActivityEntry
+        {
+            public DebugActionType ActionType { get; private set; }
 
-			public string TreeName { get; private set; }
+            public string TreeName { get; private set; }
 
-			public Slice Key { get; private set; }
+            public Slice Key { get; private set; }
 
-			public object Value { get; private set; }
+            public object Value { get; private set; }
 
-			public Stream ValueStream
-			{
-				get
-				{
-					if(Value == null)
-						return null;
+            public Stream ValueStream
+            {
+                get
+                {
+                    if (Value == null)
+                        return null;
 
-					if (Value is String || Value is Slice)
-						return new MemoryStream(Encoding.UTF8.GetBytes(Value.ToString()));
+                    if (Value is Slice)
+                        return new MemoryStream(Encoding.UTF8.GetBytes(Value.ToString()));
 
-					if (Value is Stream)
-						return Value as Stream;
+                    if (Value is Stream)
+                        return Value as Stream;
 
-					throw new InvalidOperationException("Value of activity entry is of unsupported type");
-				}
-			}
+                    throw new InvalidOperationException("Value of activity entry is of unsupported type");
+                }
+            }
 
-			public ActivityEntry(DebugActionType actionType, Slice key, string treeName, object value)
-			{
-				ActionType = actionType;
-				Key = key;
-				TreeName = treeName;
-				Value = value;
-			}
+            public ActivityEntry(DebugActionType actionType, Slice key, string treeName, object value)
+            {
+                ActionType = actionType;
+                Key = key;
+                TreeName = treeName;
+                Value = value;
+            }
 
-			public string ToCsvLine()
-			{
-				string entryValue = string.Empty;
+            public string ToCsvLine()
+            {
+                string entryValue = null;
 
-				if (Value is String || Value is Slice)
-					entryValue = Value.ToString();
+                if (Value is Stream && Value != Stream.Null)
+                {
+                    var stream = (Stream)Value;
+                    var ownsStream = false;
+                    stream.Position = 0;
 
-				else if (Value is Stream && Value != Stream.Null)
-				{
-					var valueStream = (Stream)Value;
-					valueStream.Position = 0;
-					using (var reader = new StreamReader(Value as Stream))
-						entryValue = reader.ReadToEnd();
-				}
-				else if (Value == Stream.Null)
-					entryValue = null;
+                    var ms = stream as MemoryStream;
+                    if (ms == null)
+                    {
+                        ms = new MemoryStream();
+                        stream.CopyTo(ms);
+                        stream.Position = 0;
+                        ownsStream = true;
+                    }
 
-				return String.Format("{0},{1},{2},{3}",ActionType,TreeName,Key,entryValue);
-			}
+                    entryValue = Encoding.UTF8.GetString(ms.ToArray());
 
-			public static ActivityEntry FromCsvLine(string csvLine)
-			{
-				var columnArray = csvLine.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
-				if (columnArray.Count != 4 && columnArray.Count != 2 && columnArray.Count != 3)
-					throw new ArgumentException("invalid csv data - check that you do not have commas in data");
+                    if (ownsStream)
+                        ms.Dispose();
+                }
+                else
+                {
+                    var value = Value as Slice;
+                    if (value != null)
+                    {
+                        var slice = value;
+                        var array = new byte[slice.Size];
+                        slice.CopyTo(array);
 
-				try
-				{
-					if (columnArray[0] == DebugActionType.CreateTree.ToString())
-					{
-						var activityEntry = new ActivityEntry(
-							GenericUtil.ParseEnum<DebugActionType>(columnArray[0]),
-							Slice.Empty,
-							columnArray[1],
-							null);
-						return activityEntry;
-					}
+                        entryValue = Encoding.UTF8.GetString(array);
+                    }
+                    else if (Value == Stream.Null || Value == null)
+                    {
+                        // do nothing
+                    }
+                    else
+                    {
+                        throw new NotSupportedException(string.Format("Given value type is not supported ({0}).", Value.GetType()));
+                    }
+                }
 
-					if (columnArray[0] == DebugActionType.Delete.ToString())
-					{
-						var activityEntry = new ActivityEntry(
-							GenericUtil.ParseEnum<DebugActionType>(columnArray[0]),
-							columnArray[2],
-							columnArray[1],
-							null);
+                return string.Format("{0},{1},{2},{3}", ActionType, TreeName, Key, entryValue);
+            }
 
-						return activityEntry;
-					}
+            public static ActivityEntry FromCsvLine(string csvLine)
+            {
+                var columnArray = csvLine.Split(new[] { ',' }).ToList();
+                if (columnArray.Count != 4)
+                    throw new ArgumentException("invalid csv data - check that you do not have commas in data");
 
-					var entry = new ActivityEntry(
-						GenericUtil.ParseEnum<DebugActionType>(columnArray[0]),
-						columnArray[2],
-						columnArray[1],
-						columnArray[3]);
+                try
+                {
+                    if (columnArray[0] == DebugActionType.CreateTree.ToString())
+                    {
+                        var activityEntry = new ActivityEntry(
+                            GenericUtil.ParseEnum<DebugActionType>(columnArray[0]),
+                            Slice.Empty,
+                            columnArray[1],
+                            null);
+                        return activityEntry;
+                    }
 
-					return entry;
-				}
-				catch (Exception e)
-				{
-					throw new ArgumentException("Unable to parse the argument",e);
-				}
-			}
-		}
+                    if (columnArray[0] == DebugActionType.Delete.ToString())
+                    {
+                        var activityEntry = new ActivityEntry(
+                            GenericUtil.ParseEnum<DebugActionType>(columnArray[0]),
+                            columnArray[2],
+                            columnArray[1],
+                            null);
 
-		private StorageEnvironment _env;
-		private FileStream _journalFileStream;
-		private TextWriter _journalWriter;
-		private const string FileExtension = ".djrs";
+                        return activityEntry;
+                    }
 
-		public bool IsRecording { get; set; }
+                    var type = GenericUtil.ParseEnum<DebugActionType>(columnArray[0]);
+                    object value;
+                    switch (type)
+                    {
+                        case DebugActionType.MultiAdd:
+                        case DebugActionType.MultiDelete:
+                            value = new Slice(Encoding.UTF8.GetBytes(columnArray[3]));
+                            break;
+                        default:
+                            value = new MemoryStream(Encoding.UTF8.GetBytes(columnArray[3]));
+                            break;
+                    }
 
-		public ConcurrentQueue<ActivityEntry> WriteQueue { get; private set; }
+                    var entry = new ActivityEntry(type,
+                        columnArray[2],
+                        columnArray[1],
+                        value);
 
-		public DebugJournal(string journalName, StorageEnvironment env, bool isRecordingByDefault = false)
-		{
-			_env = env;
-			IsRecording = isRecordingByDefault;
-			InitializeDebugJournal(journalName);
-		}
+                    return entry;
+                }
+                catch (Exception e)
+                {
+                    throw new ArgumentException("Unable to parse the argument", e);
+                }
+            }
+        }
 
-		private void InitializeDebugJournal(string journalName)
-		{
-			Dispose();
-			_journalFileStream = new FileStream(journalName + FileExtension, FileMode.OpenOrCreate, FileAccess.ReadWrite);
-			_journalWriter = new StreamWriter(_journalFileStream, Encoding.UTF8);
-			WriteQueue = new ConcurrentQueue<ActivityEntry>();
-		}
+        private StorageEnvironment _env;
+        private FileStream _journalFileStream;
+        private TextWriter _journalWriter;
+        private const string FileExtension = ".djrs";
 
-		public void Load(string journalName)
-		{
-			InitializeDebugJournal(journalName);
-			using (var journalReader = new StreamReader(_journalFileStream, Encoding.UTF8))
-			{
-				while (journalReader.Peek() >= 0)
-				{
-					var csvLine = journalReader.ReadLine();
-					if(!string.IsNullOrWhiteSpace(csvLine))
-						WriteQueue.Enqueue(ActivityEntry.FromCsvLine(csvLine));
-				}
-			}
+        public bool IsRecording { get; set; }
 
-		}
+        public ConcurrentQueue<ActivityEntry> WriteQueue { get; private set; }
+
+        public DebugJournal(string journalName, StorageEnvironment env, bool isRecordingByDefault = false)
+        {
+            _env = env;
+            IsRecording = isRecordingByDefault;
+            InitializeDebugJournal(journalName);
+        }
+
+        private void InitializeDebugJournal(string journalName)
+        {
+            Dispose();
+            _journalFileStream = new FileStream(journalName + FileExtension, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            _journalWriter = new StreamWriter(_journalFileStream, Encoding.UTF8);
+            WriteQueue = new ConcurrentQueue<ActivityEntry>();
+        }
+
+        public void Load(string journalName)
+        {
+            InitializeDebugJournal(journalName);
+            using (var journalReader = new StreamReader(_journalFileStream, Encoding.UTF8))
+            {
+                while (journalReader.Peek() >= 0)
+                {
+                    var csvLine = journalReader.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(csvLine))
+                        WriteQueue.Enqueue(ActivityEntry.FromCsvLine(csvLine));
+                }
+            }
+
+        }
 
 
-		public static DebugJournal FromFile(string journalName,StorageEnvironment env)
-		{
-			var newJournal = new DebugJournal(journalName,env);
-			newJournal.Load(journalName);
+        public static DebugJournal FromFile(string journalName, StorageEnvironment env)
+        {
+            var newJournal = new DebugJournal(journalName, env);
+            newJournal.Load(journalName);
 
-			return newJournal;
-		}
-		
-		[Conditional("DEBUG")]
-		public void RecordAction(DebugActionType actionType, Slice key, string treeName, object value)
-		{
-			if (IsRecording)
-			{
-				var newAction = new ActivityEntry(actionType, key, treeName, value);
-				WriteQueue.Enqueue(newAction);
-				_journalWriter.WriteLine(newAction.ToCsvLine());
-			}
-		}
+            return newJournal;
+        }
 
-		[Conditional("DEBUG")]
-		public void Flush()
-		{
-			try
-			{
-				_journalWriter.Flush();
-			}
-			catch (ObjectDisposedException)
-			{
-			}
-		}
+        [Conditional("DEBUG")]
+        public void RecordAction(DebugActionType actionType, Slice key, string treeName, object value)
+        {
+            if (IsRecording)
+            {
+                var newAction = new ActivityEntry(actionType, key, treeName, value);
+                WriteQueue.Enqueue(newAction);
+                _journalWriter.WriteLine(newAction.ToCsvLine());
+            }
+        }
 
-		public void Replay()
-		{
-			var wasDebugRecording = _env.IsDebugRecording;
-			_env.IsDebugRecording = false;
+        [Conditional("DEBUG")]
+        public void Flush()
+        {
+            try
+            {
+                _journalWriter.Flush();
+            }
+            catch (ObjectDisposedException)
+            {
+            }
+        }
 
-			using (var writeBatch = new WriteBatch())
-			{
-				ActivityEntry entry;
-				while (WriteQueue.TryDequeue(out entry))
-				{
-					switch (entry.ActionType)
-					{
-						case DebugActionType.Add:
-							writeBatch.Add(entry.Key, entry.ValueStream, entry.TreeName);
-							break;
-						case DebugActionType.Delete:
-							writeBatch.Delete(entry.Key, entry.TreeName);
-							break;
-						case DebugActionType.MultiAdd:
-							writeBatch.MultiAdd(entry.Key, new Slice(Encoding.UTF8.GetBytes(entry.Value.ToString())), entry.TreeName);
-							break;
-						case DebugActionType.MultiDelete:
-							writeBatch.MultiDelete(entry.Key, new Slice(Encoding.UTF8.GetBytes(entry.Value.ToString())), entry.TreeName);
-							break;
-						case DebugActionType.CreateTree:
-							using (var tx = _env.NewTransaction(TransactionFlags.ReadWrite))
-							{
-								_env.CreateTree(tx, entry.TreeName);
-								tx.Commit();
-							}
-							break;
-						default: //precaution against newly added action types
-							throw new InvalidOperationException("unsupported tree action type");
-					}
-				}
+        public void Replay()
+        {
+            var wasDebugRecording = _env.IsDebugRecording;
+            _env.IsDebugRecording = false;
 
-				_env.Writer.Write(writeBatch);
-			}
+            using (var writeBatch = new WriteBatch())
+            {
+                ActivityEntry entry;
+                while (WriteQueue.TryDequeue(out entry))
+                {
+                    switch (entry.ActionType)
+                    {
+                        case DebugActionType.Add:
+                            writeBatch.Add(entry.Key, entry.ValueStream, entry.TreeName);
+                            break;
+                        case DebugActionType.Delete:
+                            writeBatch.Delete(entry.Key, entry.TreeName);
+                            break;
+                        case DebugActionType.MultiAdd:
+                            writeBatch.MultiAdd(entry.Key, new Slice(Encoding.UTF8.GetBytes(entry.Value.ToString())), entry.TreeName);
+                            break;
+                        case DebugActionType.MultiDelete:
+                            writeBatch.MultiDelete(entry.Key, new Slice(Encoding.UTF8.GetBytes(entry.Value.ToString())), entry.TreeName);
+                            break;
+                        case DebugActionType.CreateTree:
+                            using (var tx = _env.NewTransaction(TransactionFlags.ReadWrite))
+                            {
+                                _env.CreateTree(tx, entry.TreeName);
+                                tx.Commit();
+                            }
+                            break;
+                        default: //precaution against newly added action types
+                            throw new InvalidOperationException("unsupported tree action type");
+                    }
+                }
 
-			_env.IsDebugRecording = wasDebugRecording; //restore the state as it was
-		}
+                _env.Writer.Write(writeBatch);
+            }
 
-		public void Dispose()
-		{
-			try
-			{
-				if (_journalWriter != null)
-					_journalWriter.Dispose();
+            _env.IsDebugRecording = wasDebugRecording; //restore the state as it was
+        }
 
-				if (_journalFileStream != null)
-					_journalFileStream.Dispose();
-			}
-			catch (ObjectDisposedException)
-			{
-			}
-		}
-	}
+        public void Dispose()
+        {
+            try
+            {
+                if (_journalWriter != null)
+                    _journalWriter.Dispose();
+
+                if (_journalFileStream != null)
+                    _journalFileStream.Dispose();
+            }
+            catch (ObjectDisposedException)
+            {
+            }
+        }
+    }
 }
