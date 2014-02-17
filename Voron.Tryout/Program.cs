a3d818a6be61d040f6bd51b4acc720180f0a1f48
Ayende Rahien <ayende@ayende.com>
2014-02-17 10:30:18 +0200

    Test for queue like behavior

--- Voron.Tryout/Program.cs
+++ Voron.Tryout/Program.cs
@@ -1,44 +1,70 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.IO.Compression;
-using System.IO.MemoryMappedFiles;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Xml;
-using Voron.Debugging;
+using System.Threading.Tasks;
 using Voron.Impl;
-using Voron.Tests.Bugs;
-using Voron.Tests.Storage;
-using Snapshots = Voron.Tests.Bugs.Snapshots;
 
 namespace Voron.Tryout
 {
-	internal unsafe class Program
-	{
-		[StructLayout(LayoutKind.Explicit)]
-		private struct NumberWithHighAndLowParts
-		{
-			[FieldOffset(0)]
-			public long Number;
+    internal unsafe class Program
+    {
+        private static void Main()
+        {
+            for (int ix = 0; ix < 5; ix++)
+            {
+                using (var env = new StorageEnvironment(StorageEnvironmentOptions.CreateMemoryOnly()))
+                {
+                    using (var tx = env.NewTransaction(TransactionFlags.ReadWrite))
+                    {
+                        env.CreateTree(tx, "test");
+                        tx.Commit();
+                    }
 
-			[FieldOffset(0)]
-			public uint Low;
+                    var sp = Stopwatch.StartNew();
+                    var producer = Task.Factory.StartNew(() =>
+                    {
+                        int counter = 0;
+                        var buffer = new Byte[98];
+                        for (int i = 0; i < 10 * 1000; i++)
+                        {
+                            var wb = new WriteBatch();
+                            for (int j = 0; j < 10; j++)
+                            {
+                                wb.Add((counter++).ToString("0000000000"), new MemoryStream(buffer), "test");
+                            }
+                            env.Writer.Write(wb);
+                        }
+                    });
 
-			[FieldOffset(4)]
-			public uint High;
-		}
-		private static void Main()
-		{
-			for (int i = 0; i < 10; i++)
-			{
-			    Console.WriteLine(i);
-				using (var x = new Snapshots())
-				{
-					x.SnapshotIssue_ExplicitFlushing();
-				}
-			}
-		}
-	}
+                    var consumer = Task.Factory.StartNew(() =>
+                    {
+                        var total = 0;
+                        while (total < 10 * 10 * 1000)
+                        {
+                            var wb = new WriteBatch();
+                            using (var snapshot = env.CreateSnapshot())
+                            {
+                                using (var it = snapshot.Iterate("test"))
+                                {
+                                    if (it.Seek(Slice.BeforeAllKeys) == false)
+                                        continue;
+                                    int i = 0;
+                                    do
+                                    {
+                                        i++;
+                                        wb.Delete(it.CurrentKey, "test");
+                                        total++;
+                                    } while (it.MoveNext() && i < 128);
+                                }
+                            }
+                        }
+                    });
+
+                    Task.WaitAll(consumer, producer);
+
+                    Console.WriteLine(sp.Elapsed);
+                }
+            }
+        }
+    }
 }
