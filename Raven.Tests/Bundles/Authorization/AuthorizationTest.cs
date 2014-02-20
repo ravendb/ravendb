11404bec10c4ee5610a2aa23beb4af35a58fd868
Fitzchak Yitzchaki <fitzchak@ayende.com>
2014-02-20 16:34:33 +0200

    Refactor tests to use RavenTest

--- Raven.Tests/Bundles/Authorization/AuthorizationTest.cs
+++ Raven.Tests/Bundles/Authorization/AuthorizationTest.cs
@@ -3,60 +3,36 @@
 //     Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
-using System;
 using System.Collections;
 using System.ComponentModel.Composition.Hosting;
 using System.Web;
 using Raven.Bundles.Authorization;
 using Raven.Client.Document;
-using Raven.Database.Config;
 using Raven.Database.Extensions;
-using Raven.Database.Server;
 using Raven.Server;
+using Raven.Tests;
 
 namespace Raven.Bundles.Tests.Authorization
 {
-	public abstract class AuthorizationTest : IDisposable
+	public abstract class AuthorizationTest : RavenTest
 	{
 		protected const string UserId = "Authorization/Users/Ayende";
-		protected DocumentStore store;
-		protected RavenDbServer server;
-
-		static AuthorizationTest()
-		{
-			try
-			{
-				new Uri("http://fail/first/time?only=%2bplus");
-			}
-			catch (Exception)
-			{
-			}
-		}
-
+		protected readonly DocumentStore store;
+		protected readonly RavenDbServer server;
+		
 		protected AuthorizationTest()
 		{
 			IOExtensions.DeleteDirectory("Data");
 			IOExtensions.DeleteDirectory("Testing");
 			IOExtensions.DeleteDirectory("Tenants");
-			server = new RavenDbServer(new RavenConfiguration
-			{
-				AnonymousUserAccessMode = AnonymousUserAccessMode.Admin,
-				Catalog = { Catalogs = { new AssemblyCatalog(typeof(AuthorizationDecisions).Assembly) } },
-				Port = 8079,
-				RunInUnreliableYetFastModeThatIsNotSuitableForProduction = true,
-			});
-			store = new DocumentStore {Url = server.SystemDatabase.Configuration.ServerUrl };
-			store.Initialize();
+
+			server = GetNewServer(configureServer: configuration => configuration.Catalog.Catalogs.Add(new AssemblyCatalog(typeof (AuthorizationDecisions).Assembly)));
+			store = NewRemoteDocumentStore(ravenDbServer: server);
+			
 			foreach (DictionaryEntry de in HttpRuntime.Cache)
 			{
 				HttpRuntime.Cache.Remove((string)de.Key);
 			}
-		}
-
-		public void Dispose()
-		{
-			store.Dispose();
-			server.Dispose();
 		}
 	}
 }
