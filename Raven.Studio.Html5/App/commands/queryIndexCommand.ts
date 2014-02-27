84d735479000a41db4310eb1032813a8bb00e993
Judah Gabriel Himango <judahgabriel@gmail.com>
2014-02-26 23:04:18 -0600

    Query now supports advanced query options
    Show fields, index entries, OR and AND default operators.

--- Raven.Studio.Html5/App/commands/queryIndexCommand.ts
+++ Raven.Studio.Html5/App/commands/queryIndexCommand.ts
@@ -5,7 +5,7 @@ import pagedResultSet = require("common/pagedResultSet");
 import querySort = require("models/querySort");
 
 class queryIndexCommand extends commandBase {
-    constructor(private indexName: string, private db: database, private skip: number, private take: number, private queryText?: string, private sorts?: querySort[], private transformerName?: string) {
+    constructor(private indexName: string, private db: database, private skip: number, private take: number, private queryText?: string, private sorts?: querySort[], private transformerName?: string, private showFields?: boolean, private indexEntries?: boolean, private useAndOperator?: boolean) {
         super();
     }
 
@@ -13,11 +13,14 @@ class queryIndexCommand extends commandBase {
         var url = "/indexes/" + this.indexName;
         var resultsTransformerUrlFragment = this.transformerName ? "&resultsTransformer=" + this.transformerName : ""; // This should not be urlEncoded, as it breaks the query.
         var urlArgs = this.urlEncodeArgs({
-            query: this.queryText,
+            query: this.queryText ? this.queryText : undefined,
             start: this.skip,
             pageSize: this.take,
             sort: this.sorts.map(s => s.toQuerySortString()),
             skipTransformResults: true,
+            fetch: this.showFields ? "__all_fields" : undefined,
+            debug: this.indexEntries ? "entries" : undefined,
+            operator: this.useAndOperator ? "AND" : undefined
         }) + resultsTransformerUrlFragment;
 
         var selector = (results: indexQueryResultsDto) => new pagedResultSet(results.Results.map(d => new document(d)), results.TotalResults, results);
