84d78658cb24768a786c7d1c915eca0b7c2d93e2
Fitzchak Yitzchaki <fitzchak@ayende.com>
2014-03-10 18:38:09 +0200

    Import task, work in progress

---
+++ Raven.Database/Server/Controllers/StudioTasksController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Raven.Abstractions.Smuggler;
+using Raven.Database.Smuggler;
+
+namespace Raven.Database.Server.Controllers
+{
+	public class StudioTasksController : RavenDbApiController
+	{
+		[HttpPost]
+		[Route("studio-tasks/import")]
+		[Route("databases/{databaseName}/studio-tasks/import")]
+		public async Task<HttpResponseMessage> ImportDatabase()
+		{
+			var dataDumper = new DataDumper(Database);
+			var importData = dataDumper.ImportData(new SmugglerImportOptions
+			{
+				FromStream = await InnerRequest.Content.ReadAsStreamAsync()
+			}, new SmugglerOptions());
+			throw new InvalidOperationException();
+		}
+	}
+}
