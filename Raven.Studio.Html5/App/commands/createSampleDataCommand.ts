1bc1b5814802b621b8e3512dec5bb13951b2ff1a
Maxim Buryak <maxim@ayende.com>
2014-03-11 14:22:41 +0200

    issue  RavenDB-1575: implement CreateSampleData

---
+++ Raven.Studio.Html5/App/commands/createSampleDataCommand.ts
@@ -0,0 +1,17 @@
+﻿import commandBase = require("commands/commandBase");
+
+class createSampleDataComman extends commandBase
+{
+    constructor(private db: database) {
+        super();
+    }
+    execute(): JQueryPromise<any> {
+        this.reportInfo("Creating Sample Data, Please wait...");
+
+        var saveBulkTask = this.post("/bulk_docs", null, this.db);
+        return null;
+    }
+
+}
+
+export = createSampleDataComman;
--- Raven.Studio.Html5/App/viewmodels/createSampleData.ts
+++ Raven.Studio.Html5/App/viewmodels/createSampleData.ts
@@ -1,4 +1,12 @@
-﻿class createSampleData {
+﻿import getStatisticsCommand = require("commands/getDatabaseStatsCommand");
+import createSampleDataCommand = require("commands/createSampleDataCommand");
+
+class createSampleData {
+
+    generateSampleData() {
+        var createSampleDataCmd = new createSampleDataCommand().execute();
+    }
 }
+
 
 export = createSampleData; 
