e11f7e500dbf855235e7f50b0652523f16f5cd28
Fitzchak Yitzchaki <fitzchak@ayende.com>
2014-03-11 16:08:46 +0200

    Backup task

---
+++ Raven.Studio.Html5/App/commands/backupDatabaseCommand.ts
@@ -0,0 +1,43 @@
+import commandBase = require("commands/commandBase");
+import database = require("models/database");
+
+class stopIndexingCommand extends commandBase {
+
+  constructor(private db: database, private backupLocation: string) {
+    super();
+  }
+
+  execute(): JQueryPromise<any> {
+    var result = $.Deferred();
+
+    this.query('/admin/databases/' + this.db.name, null, null /* We should query the system URL here */)
+      .fail(response=> result.reject(response))
+      .done((doc: databaseDocumentDto)=> {
+        var args: backupRequestDto = {
+          BackupLocation: this.backupLocation,
+          DatabaseDocument: doc
+        };
+
+        this.post('/admin/backup', JSON.stringify(args), this.db)
+          .fail(response=> {
+            debugger
+            result.reject(response);
+          })
+          .done(()=> {
+            debugger
+            var hasCompleted = false;
+            while (!hasCompleted) {
+              this.query("Raven/Backup/Status", null, this.db, { dataType: undefined })
+                .done((backupStatus: backupStatusDto)=> {
+                  debugger
+                });
+            }
+          });
+      });
+
+    return result;
+  }
+
+}
+
+export = stopIndexingCommand;
