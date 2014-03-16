9befd9638837464f89bcf9ec7543c60b86a812cd
Maxim Buryak <maxim@ayende.com>
2014-03-16 18:21:42 +0200

    HTML5 Studio: implementin transformers

--- Raven.Studio.Html5/App/models/transformer.ts
+++ Raven.Studio.Html5/App/models/transformer.ts
@@ -5,18 +5,24 @@ import appUrl = require("common/appUrl");
 
 class transformer {
     public name = ko.observable<string>();
+
+    private originalName = ko.observable<string>();
+
+    public wasNameChanged = ko.computed<boolean>(()=> this.name() != this.originalName());
     public transformResults = ko.observable<string>();
     editUrl: KnockoutComputed<string>;
     
 
     initFromLoad(dto: transformerDto): transformer {
+        this.originalName(dto.name.toString());
         this.name(dto.name);
         this.editUrl = appUrl.forCurrentDatabase().editTransformer(encodeURIComponent(this.name()));
         this.transformResults(dto.definition.TransformResults);
         return this;
     }
 
-    initFromSave(dto: savedTransformerDto) :transformer{
+    initFromSave(dto: savedTransformerDto): transformer{
+        this.originalName(dto.Transformer.Name.toString());
         this.name(dto.Transformer.Name);
         this.editUrl = appUrl.forCurrentDatabase().editTransformer(encodeURIComponent(this.name()));
         this.transformResults(dto.Transformer.TransformResults);
