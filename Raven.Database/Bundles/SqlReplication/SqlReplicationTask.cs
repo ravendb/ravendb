cda93284d79126ce8b18907bfd161ebf2617e0cb
Ayende Rahien <ayende@ayende.com>
2014-03-18 14:45:41 +0200

    Will take into account touched documents in SQL Replication and won't process them

--- Raven.Database/Bundles/SqlReplication/SqlReplicationTask.cs
+++ Raven.Database/Bundles/SqlReplication/SqlReplicationTask.cs
@@ -239,6 +239,21 @@ namespace Raven.Database.Bundles.SqlReplication
 							var deletedDocs = deletedDocsByConfig[replicationConfig];
 							var docsToReplicate = documents
 								.Where(x => lastReplicatedEtag.CompareTo(x.Etag) <= 0) // haven't replicate the etag yet
+                                .Where(document =>
+                                {
+                                    var info = Database.GetRecentTouchesFor(document.Key);
+                                    if (info != null)
+                                    {
+                                        if (info.TouchedEtag.CompareTo(lastReplicatedEtag) > 0)
+                                        {
+                                            log.Debug(
+                                                "Will not replicate document '{0}' to '{1}' because the updates after etag {2} are related document touches",
+                                                document.Key, replicationConfig.Name, info.TouchedEtag);
+                                            return false;
+                                        }
+                                    }
+
+                                })
 								.ToList();
 
 							var currentLatestEtag = HandleDeletesAndChangesMerging(deletedDocs, docsToReplicate);
