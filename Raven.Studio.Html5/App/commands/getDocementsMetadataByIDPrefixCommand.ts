c8e0c8bead879608e58aa1beddfa9e8eb2d32748
Maxim Buryak <maxim@ayende.com>
2014-03-18 18:28:17 +0200

    started implementing RavenDB-1873 Implement document search

---
+++ Raven.Studio.Html5/App/commands/getDocementsMetadataByIDPrefixCommand.ts
@@ -0,0 +1,13 @@
+﻿import commandBase = require("commands/commandBase");
+import database = require("models/database");
+
+class getDocementsMetadataByIDPrefixCommand extends commandBase {
+
+    constructor(private prefix:string,private db: database) {
+        super();
+    }
+
+    
+}
+
+export = getDocementsMetadataByIDPrefixCommand;
