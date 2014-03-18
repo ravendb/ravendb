cfb254d84bf28d0b7b5c9274e223fa7d56f4d99d
Grisha Kotler <grisha@ayende.com>
2014-03-18 17:59:10 +0200

    added create encryption

---
+++ Raven.Studio.Html5/App/viewmodels/createEncryption.ts
@@ -0,0 +1,133 @@
+import document = require("models/document");
+import dialog = require("plugins/dialog");
+import createDatabaseCommand = require("commands/createDatabaseCommand");
+import collection = require("models/collection");
+import dialogViewModelBase = require("viewmodels/dialogViewModelBase");
+
+class createEncryption extends dialogViewModelBase {
+
+    public creationEncryption = $.Deferred();
+    creationEncryptionStarted = false;
+
+    keyName = ko.observable();
+    encryptionAlgorithm = ko.observable();
+    isEncryptedIndexes = ko.observable(false);
+
+    constructor() {
+        super();
+    }
+
+    cancel() {
+        dialog.close(this);
+    }
+
+    deactivate() {
+        // If we were closed via X button or other dialog dismissal, reject the deletion task since
+        // we never started it.
+        if (!this.creationEncryptionStarted) {
+            this.creationEncryption.reject();
+        }
+    }
+
+    save() {
+        if (this.encryptionAlgorithm() === undefined) {
+
+        } else {
+            this.creationEncryption.resolve(this.keyName(), this.encryptionAlgorithm(), this.isEncryptedIndexes());
+            this.creationEncryptionStarted = true;
+            dialog.close(this);  
+        }
+    }
+    /*nextOrCreate() {
+        // Next needs to configure bundle settings, if we've selected some bundles.
+        // We haven't yet implemented bundle configuration, so for now we're just 
+        // creating the database.
+        var databaseName = this.databaseName();
+        var createDbCommand = new createDatabaseCommand(databaseName, this.getActiveBundles());
+        var createDbTask = createDbCommand.execute();
+        createDbTask.done(() => this.creationEncryption.resolve(databaseName));
+        createDbTask.fail(response => this.creationEncryption.reject(response));
+        this.creationEncryptionStarted = true;
+        dialog.close(this);
+    }
+
+    toggleCompressionBundle() {
+        this.isCompressionBundleEnabled.toggle();
+    }
+
+    toggleEncryptionBundle() {
+        this.isEncryptionBundleEnabled.toggle();
+    }
+
+    toggleExpirationBundle() {
+        this.isExpirationBundleEnabled.toggle();
+    }
+
+    toggleQuotasBundle() {
+        this.isQuotasBundleEnabled.toggle();
+    }
+
+    toggleReplicationBundle() {
+        this.isReplicationBundleEnabled.toggle();
+    }
+
+    toggleSqlReplicationBundle() {
+        this.isSqlReplicationBundleEnabled.toggle();
+    }
+
+    toggleVersioningBundle() {
+        this.isVersioningBundleEnabled.toggle();
+    }
+
+    togglePeriodicBackupBundle() {
+        this.isPeriodicBackupBundleEnabled.toggle();
+    }
+
+    toggleScriptedIndexBundle() {
+        this.isScriptedIndexBundleEnabled.toggle();
+    }
+
+    private getActiveBundles(): string[] {
+        var activeBundles: string[] = [];
+        if (this.isCompressionBundleEnabled()) {
+            activeBundles.push("Compression");
+        }
+
+        if (this.isEncryptionBundleEnabled()) {
+            debugger;
+            activeBundles.push("Encryption"); // TODO: Encryption also needs to specify 2 additional settings: http://ravendb.net/docs/2.5/server/extending/bundles/encryption?version=2.5
+        }
+
+        if (this.isExpirationBundleEnabled()) {
+            activeBundles.push("DocumentExpiration");
+        }
+
+        if (this.isQuotasBundleEnabled()) {
+            activeBundles.push("Quotas");
+        }
+
+        if (this.isReplicationBundleEnabled()) {
+            activeBundles.push("Replication"); // TODO: Replication also needs to store 2 documents containing information about replication. See http://ravendb.net/docs/2.5/server/scaling-out/replication?version=2.5
+        }
+
+        if (this.isSqlReplicationBundleEnabled()) {
+            activeBundles.push("SqlReplication");
+        }
+
+        if (this.isVersioningBundleEnabled()) {
+            activeBundles.push("Versioning");
+        }
+
+        if (this.isPeriodicBackupBundleEnabled()) {
+            activeBundles.push("PeriodicBackups");
+        }
+
+        if (this.isScriptedIndexBundleEnabled()) {
+            activeBundles.push("ScriptedIndexResults");
+        }
+
+        return activeBundles;
+    }*/
+}
+
+export = createEncryption;
