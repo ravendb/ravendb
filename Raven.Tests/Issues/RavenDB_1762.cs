780cb75717d1071420b9655b4f2688d2db1d9b12
Ayende Rahien <ayende@ayende.com>
2014-03-23 11:03:04 +0200

    Fixing compilation errors

--- Raven.Tests/Issues/RavenDB_1762.cs
+++ Raven.Tests/Issues/RavenDB_1762.cs
@@ -6,180 +6,187 @@
 using System;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
+using Raven.Abstractions.Data;
 using Raven.Bundles.Replication.Plugins;
 using Raven.Client;
+using Raven.Client.Document;
 using Raven.Client.Exceptions;
 using Raven.Database.Config;
+using Raven.Json.Linq;
+using Raven.Server;
 using Raven.Tests.Bundles.Replication;
 using Raven.Tests.Linq;
 using Xunit;
 
 namespace Raven.Tests.Issues
 {
-    public class RavenDB_1762 : ReplicationBase
-    {
-        [InheritedExport(typeof(AbstractDocumentReplicationConflictResolver))]
-        public class DeleteOnConflict : AbstractDocumentReplicationConflictResolver
-        {
-            public static DeleteOnConflict Instance;
+	public class RavenDB_1762 : ReplicationBase
+	{
+		private const string docId = "users/1";
 
-            public DeleteOnConflict()
-            {
-                Instance = this;
-            }
+		protected override void ConfigureServer(InMemoryRavenConfiguration serverConfiguration)
+		{
+			serverConfiguration.Catalog.Catalogs.Add(new TypeCatalog(typeof (DeleteOnConflict)));
+		}
 
-            public bool Enabled { get; set; }
+		private void CanResolveConflict(Action<DeleteOnConflict> alterResolver, Action<IDocumentStore, IDocumentStore> testCallback)
+		{
+			DocumentStore store1 = CreateStore();
+			DocumentStore store2 = CreateStore();
 
-            public override bool TryResolve(string id, Raven.Json.Linq.RavenJObject metadata, Raven.Json.Linq.RavenJObject document, Raven.Abstractions.Data.JsonDocument existingDoc, Func<string, Raven.Abstractions.Data.JsonDocument> getDocument)
-            {
-                if (Enabled)
-                {
-                    metadata.Add("Raven-Remove-Document-Marker", true);
-                }
-                return Enabled;
-            }
-        }
+			using (IDocumentSession session = store1.OpenSession())
+			{
+				session.Store(new User
+				{
+					Name = "Oren"
+				}, docId);
+				session.SaveChanges();
+			}
 
-        protected override void ConfigureServer(RavenConfiguration serverConfiguration)
-        {
-            serverConfiguration.Catalog.Catalogs.Add(new TypeCatalog(typeof(DeleteOnConflict)));
-        }
+			// master - master
+			SetupReplication(store1.DatabaseCommands, store2.Url);
 
-        const string docId = "users/1";
+			WaitForReplication(store2, docId);
 
-        private void CanResolveConflict(Action<DeleteOnConflict> alterResolver, Action<IDocumentStore, IDocumentStore> testCallback)
-        {
+			// get reference to custom conflict resolver
+			DeleteOnConflict resolver = DeleteOnConflict.Instance;
+			Assert.NotNull(resolver);
+			if (alterResolver != null)
+			{
+				alterResolver(resolver);
+			}
 
-            var store1 = CreateStore();
-            var store2 = CreateStore();
+			testCallback(store1, store2);
+		}
 
-            using (var session = store1.OpenSession())
-            {
-                session.Store(new User
-                {
-                    Name = "Oren"
-                }, docId);
-                session.SaveChanges();
-            }
+		[Fact]
+		public void CanResolveConflictBetweenPutAndDelete_IncomingDelete()
+		{
+			CanResolveConflict(resolver => resolver.Enabled = true, delegate(IDocumentStore store1, IDocumentStore store2)
+			{
+				using (IDocumentSession s = store2.OpenSession())
+				{
+					var user = s.Load<User>(docId);
+					user.Active = true;
+					s.Store(user);
+					s.SaveChanges();
+				}
 
-            // master - master
-            SetupReplication(store1.DatabaseCommands, store2.Url);
+				using (IDocumentSession s = store1.OpenSession())
+				{
+					var user = s.Load<User>(docId);
+					s.Delete(user);
+					s.SaveChanges();
+				}
 
-            WaitForReplication(store2, docId);
+				WaitForReplication(store2, session => session.Load<User>(docId) == null);
+				Assert.Null(store2.DatabaseCommands.Get(docId));
+			}
+				);
+		}
 
-            // get reference to custom conflict resolver
-            var resolver = DeleteOnConflict.Instance;
-            Assert.NotNull(resolver);
-            if (alterResolver != null)
-            {
-                alterResolver(resolver);
-            }
+		[Fact]
+		public void CanResolveConflictBetweenPutAndDelete_IncomingPut()
+		{
+			CanResolveConflict(resolver => resolver.Enabled = true, delegate(IDocumentStore store1, IDocumentStore store2)
+			{
+				using (IDocumentSession s = store2.OpenSession())
+				{
+					var user = s.Load<User>(docId);
+					s.Delete(user);
+					s.SaveChanges();
+				}
+				using (IDocumentSession s = store1.OpenSession())
+				{
+					var user = s.Load<User>(docId);
+					user.Active = true;
+					s.Store(user);
+					s.SaveChanges();
+				}
 
-            testCallback(store1, store2);
-        }
+				WaitForReplication(store2, session => session.Load<User>(docId) == null);
+				Assert.Null(store2.DatabaseCommands.Get(docId));
+			}
+				);
+		}
 
-        [Fact]
-        public void CanResolveConflictBetweenPutAndDelete_IncomingDelete()
-        {
-            CanResolveConflict(resolver => resolver.Enabled = true , delegate(IDocumentStore store1, IDocumentStore store2)
-            {
-                using (var s = store2.OpenSession())
-                {
-                    var user = s.Load<User>(docId);
-                    user.Active = true;
-                    s.Store(user);
-                    s.SaveChanges();
-                }
+		[Fact]
+		public void CanResolveConflictBetweenPutAndDelete_IncomingDelete_Resolver_disabled()
+		{
+			CanResolveConflict(resolver => resolver.Enabled = false, delegate(IDocumentStore store1, IDocumentStore store2)
+			{
+				using (IDocumentSession s = store2.OpenSession())
+				{
+					var user = s.Load<User>(docId);
+					user.Active = true;
+					s.Store(user);
+					s.SaveChanges();
+				}
 
-                using (var s = store1.OpenSession())
-                {
-                    var user = s.Load<User>(docId);
-                    s.Delete(user);
-                    s.SaveChanges();
-                }
+				using (IDocumentSession s = store1.OpenSession())
+				{
+					var user = s.Load<User>(docId);
+					s.Delete(user);
+					s.SaveChanges();
+				}
 
-                WaitForReplication(store2, (session) => session.Load<User>(docId) == null);
-                Assert.Null(store2.DatabaseCommands.Get(docId));
-            }
-            );
-        }
+				Assert.Throws<ConflictException>(
+					() => WaitForReplication(store2, session => session.Load<User>(docId) == null));
+			}
+				);
+		}
 
-        [Fact]
-        public void CanResolveConflictBetweenPutAndDelete_IncomingPut()
-        {
-            CanResolveConflict(resolver => resolver.Enabled = true, delegate(IDocumentStore store1, IDocumentStore store2)
-            {
+		[Fact]
+		public void CanResolveConflictBetweenPutAndDelete_IncomingPut_Resolver_disabled()
+		{
+			CanResolveConflict(resolver => resolver.Enabled = false, delegate(IDocumentStore store1, IDocumentStore store2)
+			{
+				using (IDocumentSession s = store2.OpenSession())
+				{
+					var user = s.Load<User>(docId);
+					s.Delete(user);
+					s.SaveChanges();
+				}
+				using (IDocumentSession s = store1.OpenSession())
+				{
+					var user = s.Load<User>(docId);
+					user.Active = true;
+					s.Store(user);
+					s.SaveChanges();
+				}
 
-                using (var s = store2.OpenSession())
-                {
-                    var user = s.Load<User>(docId);
-                    s.Delete(user);
-                    s.SaveChanges();
-                }
-                using (var s = store1.OpenSession())
-                {
-                    var user = s.Load<User>(docId);
-                    user.Active = true;
-                    s.Store(user);
-                    s.SaveChanges();
-                }
+				Assert.Throws<ConflictException>(
+					() => WaitForReplication(store2, session => session.Load<User>(docId) != null));
+			}
+				);
+		}
 
-                WaitForReplication(store2, (session) => session.Load<User>(docId) == null);
-                Assert.Null(store2.DatabaseCommands.Get(docId));
-            }
-            );
-        }
+		[InheritedExport(typeof (AbstractDocumentReplicationConflictResolver))]
+		public class DeleteOnConflict : AbstractDocumentReplicationConflictResolver
+		{
+			public static DeleteOnConflict Instance;
 
-        [Fact]
-        public void CanResolveConflictBetweenPutAndDelete_IncomingDelete_Resolver_disabled()
-        {
-            CanResolveConflict(resolver => resolver.Enabled = false, delegate(IDocumentStore store1, IDocumentStore store2)
-            {
-                using (var s = store2.OpenSession())
-                {
-                    var user = s.Load<User>(docId);
-                    user.Active = true;
-                    s.Store(user);
-                    s.SaveChanges();
-                }
+			public DeleteOnConflict()
+			{
+				Instance = this;
+			}
 
-                using (var s = store1.OpenSession())
-                {
-                    var user = s.Load<User>(docId);
-                    s.Delete(user);
-                    s.SaveChanges();
-                }
+			public bool Enabled { get; set; }
 
-                Assert.Throws<ConflictException>(
-                   () => WaitForReplication(store2, (session) => session.Load<User>(docId) == null));
-            }
-            );
-        }
 
-        [Fact]
-        public void CanResolveConflictBetweenPutAndDelete_IncomingPut_Resolver_disabled()
-        {
-            CanResolveConflict(resolver => resolver.Enabled = false, delegate(IDocumentStore store1, IDocumentStore store2)
-            {
-
-                using (var s = store2.OpenSession())
-                {
-                    var user = s.Load<User>(docId);
-                    s.Delete(user);
-                    s.SaveChanges();
-                }
-                using (var s = store1.OpenSession())
-                {
-                    var user = s.Load<User>(docId);
-                    user.Active = true;
-                    s.Store(user);
-                    s.SaveChanges();
-                }
-
-                Assert.Throws<ConflictException>(
-                   () => WaitForReplication(store2, (session) => session.Load<User>(docId) != null));
-            }
-            );
-        }
-    }
+			public override bool TryResolve(string id, RavenJObject metadata, RavenJObject document, JsonDocument existingDoc,
+				 Func<string, JsonDocument> getDocument, 
+				 out RavenJObject metadataToSave,
+				 out RavenJObject documentToSave)
+			{
+				metadataToSave = metadata;
+				documentToSave = document;
+				if (Enabled)
+				{
+					metadata.Add("Raven-Remove-Document-Marker", true);
+				}
+				return Enabled;
+			}
+		}
+	}
 }
