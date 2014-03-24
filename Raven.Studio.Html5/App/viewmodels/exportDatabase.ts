87dcd3320806905319669d5a50f5de5f185b7be2
Fitzchak Yitzchaki <fitzchak@ayende.com>
2014-03-24 11:16:18 +0200

    Export database - wip

--- Raven.Studio.Html5/App/viewmodels/exportDatabase.ts
+++ Raven.Studio.Html5/App/viewmodels/exportDatabase.ts
@@ -1,4 +1,17 @@
-﻿class exportDatabase {
+﻿import viewModelBase = require("viewmodels/viewModelBase");
+import exportDatabaseCommand = require("commands/exportDatabaseCommand");
+
+class exportDatabase extends viewModelBase {
+  includeDocuments = ko.observable(true);
+  includeIndexes = ko.observable(true);
+  includeTransformers = ko.observable(true);
+  includeAttachments = ko.observable(false);
+  removeAnalyzers = ko.observable(false);
+
+  startExport() {
+    new exportDatabaseCommand()
+      .execute();
+  }
 }
 
 export = exportDatabase; 
