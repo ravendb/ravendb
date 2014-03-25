834af8559d8c970b3e04cc0e82cfa6887992fb87
Federico Andres Lois <federico.lois@corvalius.com>
2014-03-25 16:56:18 -0300

    CreateFilesystemCommand layout implementation.

--- Raven.Studio.Html5/App/commands/createFilesystemCommand.ts
+++ Raven.Studio.Html5/App/commands/createFilesystemCommand.ts
@@ -1,21 +1,36 @@
 ﻿import commandBase = require("commands/commandBase");
-import filesystem = require("models/filesystem");
-import collection = require("models/collection");
 
 class createFilesystemCommand extends commandBase {
 
     /**
-    * @param ownerDb The database the collections will belong to.
+    * @param filesystemName The file system name we are creating.
     */
-    constructor() {
+    constructor(private filesystemName: string, private securedSettings: {} ) {
         super();
 
-        throw new Error("Not Implemented");
+        if (!filesystemName) {
+            this.reportError("File System must have a name!");
+            throw new Error("File System must have a name!");
+        }
     }
 
     execute(): JQueryPromise<any> {
 
-        throw new Error("Not Implemented");
+        this.reportInfo("Creating File System '" + this.filesystemName + "'");
+
+        var filesystemDoc = {
+            "Settings": {
+                "Raven/FileSystem/DataDir": "~\\Filesystems\\" + this.filesystemName,
+            },            
+            "Disabled": false
+        };
+
+        var url = "/ravenfs/admin/" + this.filesystemName;
+        var createTask = this.put(url, JSON.stringify(filesystemDoc), null, { dataType: undefined });
+        createTask.done(() => this.reportSuccess(this.filesystemName + " created"));
+        createTask.fail((response: JQueryXHR) => this.reportError("Failed to create file system", response.responseText, response.statusText));
+
+        return createTask;
     }
 }
 
