cf43d4ff44e11d38a92f15f7e55371b6af7de9ae
Maxim Buryak <maxim@ayende.com>
2014-03-25 16:19:19 +0200

    issue RavenDB-1859 Respect WarnWhenUsingSystemDatabase in Raven/StudioConfig solved added static property to appUrl, which is initiated on load of the shell, according to appropriate query

--- Raven.Studio.Html5/App/common/appUrl.ts
+++ Raven.Studio.Html5/App/common/appUrl.ts
@@ -324,7 +324,9 @@ class appUrl {
 
 	private static getEncodedDbPart(db?: database) {
 		return db ? "&database=" + encodeURIComponent(db.name) : "";
-	}
+    }
+
+    public static warnWhenUsingSystemDatabase: boolean = true;
 }
 
 export = appUrl;
