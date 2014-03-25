ba9d7ae9f9c5cd48e4eaa2d88e61944a6d3c31d7
Federico Andres Lois <federico.lois@corvalius.com>
2014-03-25 10:49:09 -0300

    Layout basic commands.

---
+++ Raven.Studio.Html5/App/commands/createFilesystemCommand.ts
@@ -0,0 +1,22 @@
+﻿import commandBase = require("commands/commandBase");
+import filesystem = require("models/filesystem");
+import collection = require("models/collection");
+
+class createFilesystemCommand extends commandBase {
+
+    /**
+    * @param ownerDb The database the collections will belong to.
+    */
+    constructor() {
+        super();
+
+        throw new Error("Not Implemented");
+    }
+
+    execute(): JQueryPromise<any> {
+
+        throw new Error("Not Implemented");
+    }
+}
+
+export = createFilesystemCommand;
---
+++ Raven.Studio.Html5/App/commands/getFilesystemDestinationsCommand.ts
@@ -0,0 +1,21 @@
+﻿import commandBase = require("commands/commandBase");
+import filesystem = require("models/filesystem");
+
+class getFilesystemDestinationsCommand extends commandBase {
+
+    /**
+    * @param ownerDb The database the collections will belong to.
+    */
+    constructor() {
+        super();
+
+        throw new Error("Not Implemented");
+    }
+
+    execute(): JQueryPromise<any> {
+
+        throw new Error("Not Implemented");
+    }
+}
+
+export = getFilesystemDestinationsCommand;
