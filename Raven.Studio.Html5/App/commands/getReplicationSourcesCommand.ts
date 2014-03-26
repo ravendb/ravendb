0882b5c1a12e1034dfda2739d44485bba4abe848
Marcin Lewandowski <marcin@ais.pl>
2014-03-26 14:28:00 +0100

    RavenDB-1844 HTML5 Studio - Implment Documents -> Conflict Documents

--- Raven.Studio.Html5/App/commands/getReplicationSourcesCommand.ts
+++ Raven.Studio.Html5/App/commands/getReplicationSourcesCommand.ts
@@ -16,7 +16,9 @@ class getReplicationSourcesCommand extends commandBase {
 
         return this.query("/docs", args, this.db, (dtos: replicationSourceDto[]) => {
             var result: dictionary<string> = {};
+            // insert remote databases info
             dtos.forEach(v => result[v.ServerInstanceId] = database.getNameFromUrl(v.Source));
+            // ... and insert local database instance id
             result[this.db.statistics().DatabaseId] = this.db.name;
 
             return result;
