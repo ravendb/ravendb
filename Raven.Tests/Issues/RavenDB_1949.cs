2e12276657a72db222bb5573e2154c3345c9cad5
Arkadiusz Palinski <arek@ais.pl>
2014-03-28 10:52:01 +0100

    Fixing AttemptToReproduceAccessViolationExceptionOnVoron

--- Raven.Tests/Issues/RavenDB_1949.cs
+++ Raven.Tests/Issues/RavenDB_1949.cs
@@ -66,7 +66,7 @@ namespace Raven.Tests.Issues
 					{
 						storage.Batch(accessor =>
 						{
-							accessor.Documents.GetDocumentsAfter(fromEtag, 128);
+							List<JsonDocument> jsonDocuments = accessor.Documents.GetDocumentsAfter(fromEtag, 128).ToList();
 						});
 
 						Thread.Sleep(100);
