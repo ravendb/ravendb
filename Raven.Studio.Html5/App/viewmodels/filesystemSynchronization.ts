862362e0b9413d93dfb4756d4ed9303260c47724
hveiras <hernan.veiras@gmail.com>
2014-03-31 16:05:39 -0300

    - Implemented Filesystem Add Destination views and viewmodels

--- Raven.Studio.Html5/App/viewmodels/filesystemSynchronization.ts
+++ Raven.Studio.Html5/App/viewmodels/filesystemSynchronization.ts
@@ -2,17 +2,28 @@
 import system = require("durandal/system");
 import router = require("plugins/router");
 import appUrl = require("common/appUrl");
-import filesystem = require("models/filesystem");
 import viewModelBase = require("viewmodels/viewModelBase");
+
+import addDestination = require("viewmodels/filesystemAddDestination");
 
 class filesystemSynchronization extends viewModelBase {
 
+    destinations = ko.observableArray<string>();    
     private router = router;
-
     synchronizationUrl = appUrl.forCurrentDatabase().filesystemSynchronization;
 
     constructor() {
         super();
+    }
+
+    addDestination() {
+        require(["viewmodels/filesystemAddDestination"], addDestination => {
+            var addDestinationViewModel: addDestination = new addDestination(this.destinations);
+            addDestinationViewModel
+                .creationTask
+                .done();
+            app.showDialog(addDestinationViewModel);
+        });
     }
 
 }
