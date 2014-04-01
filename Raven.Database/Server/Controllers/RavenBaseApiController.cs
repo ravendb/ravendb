2f26e461f0036791ca244e9ae99de7e7f9684a89
Fitzchak Yitzchaki <fitzchak@ayende.com>
2014-04-01 18:22:31 +0300

    Better error message if a static resource was not found

--- Raven.Database/Server/Controllers/RavenBaseApiController.cs
+++ Raven.Database/Server/Controllers/RavenBaseApiController.cs
@@ -457,7 +457,7 @@ namespace Raven.Database.Server.Controllers
 			var zipEntry = zipFile.GetEntry(docPath);
 
 			if (zipEntry == null || zipEntry.IsFile == false)
-				return GetEmptyMessage(HttpStatusCode.NotFound);
+				return EmbeddedFileNotFount(docPath);
 
 			var entry = zipFile.GetInputStream(zipEntry);
 			var msg = new HttpResponseMessage
@@ -511,12 +511,12 @@ namespace Raven.Database.Server.Controllers
 			var lowercasedResourceName = resourceNames.FirstOrDefault(s => string.Equals(s, resourceName, StringComparison.OrdinalIgnoreCase));
 		    if (lowercasedResourceName == null)
 		    {
-		        return GetEmptyMessage(HttpStatusCode.NotFound);
+				return EmbeddedFileNotFount(docPath);
 		    }
 			using (var resource = resourceAssembly.GetManifestResourceStream(lowercasedResourceName))
 			{
 				if (resource == null)
-					return GetEmptyMessage(HttpStatusCode.NotFound);
+					return EmbeddedFileNotFount(docPath);
 
 				bytes = resource.ReadData();
 			}
@@ -531,6 +531,16 @@ namespace Raven.Database.Server.Controllers
 			msg.Content.Headers.ContentType = new MediaTypeHeaderValue(type);
 
 			return msg;
+		}
+
+		private HttpResponseMessage EmbeddedFileNotFount(string docPath)
+		{
+			var message = "The following embedded file was not available: " + docPath;
+			if (docPath != null && docPath.StartsWith("/studio/"))
+			{
+				message += ". Please make sure that the Raven.Studio.Html.zip file exist in the main directory (near to the Raven.Database.dll).";
+			}
+			return GetMessageWithObject(new{ Message=message}, HttpStatusCode.NotFound);
 		}
 
 		private static readonly string EmbeddedLastChangedDate =
