e31417b20e273cf2357bd3bc79e802cc72c48df4
Ayende Rahien <ayende@ayende.com>
2014-04-02 17:26:40 +0300

    Restoring all tests

--- default.ps1
+++ default.ps1
@@ -101,9 +101,7 @@ task FullStorageTest {
 task Test -depends Compile {
 	Clear-Host
 	
-	# "$base_dir\Raven.Tests\bin\$global:configuration\Raven.Tests.dll", "$base_dir\Raven.Tests.Bundles\bin\$global:configuration\Raven.Tests.Bundles.dll", "$base_dir\Raven.Tests.Issues\bin\$global:configuration\Raven.Tests.Issues.dll",  "$base_dir\Raven.Tests.MailingList\bin\$global:configuration\Raven.Tests.MailingList.dll",
-
-	$test_prjs = @( "$base_dir\Raven.SlowTests\bin\$global:configuration\Raven.SlowTests.dll")
+	$test_prjs = @( "$base_dir\Raven.Tests\bin\$global:configuration\Raven.Tests.dll", "$base_dir\Raven.Tests.Bundles\bin\$global:configuration\Raven.Tests.Bundles.dll", "$base_dir\Raven.Tests.Issues\bin\$global:configuration\Raven.Tests.Issues.dll",  "$base_dir\Raven.Tests.MailingList\bin\$global:configuration\Raven.Tests.MailingList.dll","$base_dir\Raven.SlowTests\bin\$global:configuration\Raven.SlowTests.dll")
 	Write-Host $test_prjs
 	
 	$xUnit = "$lib_dir\xunit\xunit.console.clr4.exe"
