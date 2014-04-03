8647cce4ea51ed79df29107391cb59f60ea42d57
Pawel Pekrol <ppekrol@ais.pl>
2014-04-03 12:40:27 +0200

    more bulk insert tests

--- Raven.Tests.Core/BulkInsert/SimpleBulkInsert.cs
+++ Raven.Tests.Core/BulkInsert/SimpleBulkInsert.cs
@@ -3,6 +3,10 @@
 //      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 //  </copyright>
 // -----------------------------------------------------------------------
+using System.Linq;
+
+using Raven.Abstractions.Data;
+using Raven.Abstractions.Exceptions;
 using Raven.Tests.Core.Utils.Entities;
 using Xunit;
 
@@ -23,11 +27,90 @@ namespace Raven.Tests.Core.BulkInsert
 					}
 				}
 
-
 				using (var session = store.OpenSession())
 				{
 					var users = session.Advanced.LoadStartingWith<User>("users/", pageSize: 128);
 					Assert.Equal(100, users.Length);
+				}
+			}
+		}
+
+		[Fact]
+		public void BulkInsertShouldNotOverwriteWithOverwriteExistingSetToFalse()
+		{
+			using (var store = GetDocumentStore())
+			{
+				using (var session = store.OpenSession())
+				{
+					session.Store(new User
+					              {
+						              Id = "users/1", 
+									  Name = "User - 1"
+					              });
+
+					session.SaveChanges();
+				}
+
+				var e = Assert.Throws<ConcurrencyException>(() =>
+				{
+					using (var bulkInsert = store.BulkInsert(options: new BulkInsertOptions { OverwriteExisting = false }))
+					{
+						for (var i = 0; i < 10; i++)
+						{
+							bulkInsert.Store(new User
+							                 {
+								                 Id = "users/" + (i + 1), 
+												 Name = "resU - " + (i + 1)
+							                 });
+						}
+					}
+				});
+
+				Assert.Contains("'users/1' already exists", e.Message);
+
+				using (var session = store.OpenSession())
+				{
+					var users = session.Advanced.LoadStartingWith<User>("users/", pageSize: 128);
+					Assert.Equal(1, users.Length);
+					Assert.True(users.All(x => x.Name.StartsWith("User")));
+				}
+			}
+		}
+
+		[Fact]
+		public void BulkInsertShouldOverwriteWithOverwriteExistingSetToTrue()
+		{
+			using (var store = GetDocumentStore())
+			{
+				using (var bulkInsert = store.BulkInsert())
+				{
+					for (int i = 0; i < 10; i++)
+					{
+						bulkInsert.Store(new User
+						                 {
+							                 Id = "users/" + (i + 1), 
+											 Name = "User - " + (i + 1)
+						                 });
+					}
+				}
+
+				using (var bulkInsert = store.BulkInsert(options: new BulkInsertOptions { OverwriteExisting = true }))
+				{
+					for (int i = 0; i < 10; i++)
+					{
+						bulkInsert.Store(new User
+						                 {
+							                 Id = "users/" + (i + 1), 
+											 Name = "resU - " + (i + 1)
+						                 });
+					}
+				}
+
+				using (var session = store.OpenSession())
+				{
+					var users = session.Advanced.LoadStartingWith<User>("users/", pageSize: 128);
+					Assert.Equal(10, users.Length);
+					Assert.True(users.All(x => x.Name.StartsWith("resU")));
 				}
 			}
 		}
