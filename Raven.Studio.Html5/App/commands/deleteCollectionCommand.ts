deddd3357ce479bfe3ccf4ffd32e1fb118d604c8
Marcin Lewandowski <marcin@ais.pl>
2014-04-16 09:16:51 +0200

    RavenDB-2111 Delete collection appears to work but does not

--- Raven.Studio.Html5/App/commands/deleteCollectionCommand.ts
+++ Raven.Studio.Html5/App/commands/deleteCollectionCommand.ts
@@ -11,15 +11,11 @@ class deleteCollectionCommand extends commandBase {
     execute(): JQueryPromise<any> {
         this.reportInfo("Deleting " + this.collectionName + " collection...");
 
-        var args = {
-            query: "Tag:" + this.collectionName,
-            pageSize: 128,
-            allowStale: true
-        };
         var url = "/bulk_docs/Raven/DocumentsByEntityName";
-        var urlParams = "?query=Tag%3A" + encodeURIComponent(this.collectionName) + "&pageSize=128&allowStale=true";
+        var urlParams = "?query=Tag%3A" + encodeURIComponent(this.collectionName) + "&allowStale=true";
         var deleteTask = this.del(url + urlParams, null, this.db);
-        deleteTask.done(() => this.reportSuccess("Deleted " + this.collectionName + " collection"));
+        // deletion is made asynchronically so we infom user about operation start - not about actual completion. 
+        deleteTask.done(() => this.reportSuccess("Scheduled deletion of " + this.collectionName));
         deleteTask.fail((response: JQueryXHR) => this.reportError("Failed to delete collection", response.responseText, response.statusText));
         return deleteTask;
     }
