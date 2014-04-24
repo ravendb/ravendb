6547003272b9bc6f5d1f38bea5f371361b6e4230
Arkadiusz Palinski <arek@ais.pl>
2014-04-24 12:53:06 +0200

    Core tests - indexing referenced documents by using LoadDocument

--- Raven.Tests.Core/Indexing/StaticIndexes.cs
+++ Raven.Tests.Core/Indexing/StaticIndexes.cs
@@ -1,16 +1,11 @@
-﻿using Raven.Client;
-using Raven.Client.Indexes;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Raven.Client;
 using Raven.Tests.Core.Utils.Entities;
 using Raven.Tests.Core.Utils.Indexes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
-namespace Raven.Tests.Core.Querying
+namespace Raven.Tests.Core.Indexing
 {
     public class StaticIndexes : RavenCoreTestBase
     {
