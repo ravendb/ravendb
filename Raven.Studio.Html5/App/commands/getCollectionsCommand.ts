d9eeedef7e4db201d081d2b82aa9fda5649f4457
Judah Gabriel Himango <judahgabriel@gmail.com>
2014-04-30 21:16:05 -0500

    Automatically create the system index if necessary

--- Raven.Studio.Html5/App/commands/getCollectionsCommand.ts
+++ Raven.Studio.Html5/App/commands/getCollectionsCommand.ts
@@ -16,17 +16,75 @@ class getCollectionsCommand extends commandBase {
     }
 
     execute(): JQueryPromise<collection[]> {
-        
+       
+
+        var finalResult = $.Deferred<collection[]>();
+
+        return this.runQuery()
+            .fail((xhr: JQueryXHR) => this.createSystemIndexAndTryAgain(finalResult, xhr))
+            .done((results: collection[]) => finalResult.resolve(results));
+    }
+
+    runQuery(): JQueryPromise<collection[]> {
         var args = {
             field: "Tag",
             fromValue: "",
             pageSize: 128
         };
-
         var resultsSelector = (collectionNames: string[]) => collectionNames.map(n => new collection(n, this.ownerDb));
         return this.query("/terms/Raven/DocumentsByEntityName", args, this.ownerDb, resultsSelector)
-            .fail(() => this.reportError("Raven/DocumentsByEntityName index not found",
-                        "In order to enjoy the studio interface, please recreate the index: \n" + "index definition: \n" + 'Name: Raven/DocumentsByEntityName \n'+'Map: from doc in docs let Tag = doc["@metadata"]["Raven-Entity-Name"] select new { Tag, LastModified = (DateTime)doc["@metadata"]["Last-Modified"] };'));
+    }
+
+    createSystemIndexAndTryAgain(deferred: JQueryDeferred<collection[]>, originalReadError: JQueryXHR) {
+        // Most often, failure to get the collections is due to the missing system index, Raven/DocumentsByEntityName.
+        // This appears to be new behavior as of 3.0: Raven doesn't create this index automatically.
+
+        // So, if we are unable to read the collections, we create the system index ourselves, then try again.
+        require(["commands/saveIndexDefinitionCommand"], (saveIndexDefinitionCommandCtor: any) => {
+            var systemIndex = this.createSystemIndex();
+
+            // Ctor in this case is the constructor for saveIndexDefinitionCommand.
+            new saveIndexDefinitionCommandCtor(systemIndex, 0, this.ownerDb)
+                .execute()
+                .fail(() => this.onErrorReadingCollections(deferred, originalReadError))
+                .done(() => this.retryQuery(deferred, originalReadError));
+        });
+    }
+
+    retryQuery(deferred: JQueryDeferred<collection[]>, originalReadError: JQueryXHR) {
+        this.runQuery()
+            .fail(() => this.onErrorReadingCollections(deferred, originalReadError))
+            .done((results: collection[]) => deferred.resolve(results));
+    }
+
+    onErrorReadingCollections(deferred: JQueryDeferred<collection[]>, xhr: JQueryXHR) {
+        this.reportError("Failed to read collections", xhr.responseText, xhr.statusText);
+        deferred.reject(xhr);
+    }
+
+    createSystemIndex(): indexDefinitionDto {
+        var map = 'from doc in docs let Tag = doc["@metadata"]["Raven-Entity-Name"] select new { Tag, LastModified = (DateTime)doc["@metadata"]["Last-Modified"] }';
+        return {
+            Analyzers: {},
+            Fields: [],
+            Indexes: {},
+            InternalFieldsMapping: {},
+            IsCompiled: false,
+            IsMapReduce: false,
+            LockMode: "Unlock",
+            Map: map,
+            Maps: [map],
+            Name: "Raven/DocumentsByEntityName",
+            Reduce: null,
+            SortOptions: {},
+            SpatialIndexes: {},
+            Stores: {},
+            Suggestions: {},
+            TermVectors: {},
+            TransformResults: null,
+            Type: "Map",
+            MaxIndexOutputsPerDocument: null
+        };
     }
 }
 
