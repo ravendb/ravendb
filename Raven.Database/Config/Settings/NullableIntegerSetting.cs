8f9f764bb29618f29b712bc9054728dc7db31925
Pawel Pekrol <ppekrol@ais.pl>
2014-05-05 10:03:10 +0200

    exposed voron InitialFileSize to RavenDB

---
+++ Raven.Database/Config/Settings/NullableIntegerSetting.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="NullableIntegerSetting.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using System;
+
+namespace Raven.Database.Config.Settings
+{
+	public class NullableIntegerSetting : Setting<int?>
+	{
+		public NullableIntegerSetting(string value, int? defaultValue)
+			: base(value, defaultValue)
+		{
+		}
+
+		public NullableIntegerSetting(string value, Func<int?> getDefaultValue)
+			: base(value, getDefaultValue)
+		{
+		}
+
+		public override int? Value
+		{
+			get
+			{
+				return string.IsNullOrEmpty(value) == false
+						   ? int.Parse(value)
+						   : (getDefaultValue != null ? getDefaultValue() : defaultValue);
+			}
+		}
+	}
+}
