df9f08a995e4004f93af15b1ef9c11c5edef4246
Arkadiusz Palinski <arek@ais.pl>
2014-05-07 14:46:59 +0200

    RavenDB-2193 Added another failing test which shows that free space handling returns already allocated pages.

--- Raven.Voron/Voron.Tests/Trees/FreeSpaceTest.cs
+++ Raven.Voron/Voron.Tests/Trees/FreeSpaceTest.cs
@@ -120,5 +120,59 @@ namespace Voron.Tests.Trees
 				}
 			}
 		}
+
+		[Fact]
+		public void FreeSpaceHandlingShouldNotReturnPagesThatAreAlreadyAllocated()
+		{
+			const int maxPageNumber = 400000;
+			const int numberOfFreedPages = 60;
+			var random = new Random(2);
+			var freedPages = new HashSet<long>();
+
+			using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
+			{
+				tx.State.NextPageNumber = maxPageNumber + 1;
+
+				tx.Commit();
+			}
+
+			for (int i = 0; i < numberOfFreedPages; i++)
+			{
+				long pageToFree;
+				do
+				{
+					pageToFree = random.Next(0, maxPageNumber);
+				} while (freedPages.Add(pageToFree) == false);
+
+				using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
+				{
+					Env.FreeSpaceHandling.FreePage(tx, pageToFree);
+
+					tx.Commit();
+				}
+			}
+
+			var alreadyReused = new List<long>();
+
+			do
+			{
+				using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
+				{
+					var page = Env.FreeSpaceHandling.TryAllocateFromFreeSpace(tx, 1);
+
+					if (page == null)
+					{
+						break;
+					}
+
+					Assert.False(alreadyReused.Contains(page.Value), "Free space handling returned a page number that has been already allocated. Page number: " + page);
+					Assert.True(freedPages.Remove(page.Value));
+
+					alreadyReused.Add(page.Value);
+
+					tx.Commit();
+				}
+			} while (true);
+		}
 	}
 }
