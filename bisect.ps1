0dc2ec84311b049305809a5e80fe1f9a858c7154
Pawel Pekrol <ppekrol@ais.pl>
2014-05-09 09:05:21 +0200

    fixes in bisect script

--- bisect.ps1
+++ bisect.ps1
@@ -2,6 +2,8 @@ $start = $args[0] #bad revision
 $end = $args[1] #good revision
 $test_prefix = $args[2] #test name (full)
 
+"Bisecting. Good: $start. Bad: $end. Test: $test_prefix"
+
 &"git" bisect start $start $end
-&"git" bisect run bisect_internal.ps1 $test_prefix
+&"git" bisect run bisect_internal.sh $test_prefix
 &"git" bisect reset
