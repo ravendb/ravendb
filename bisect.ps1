ac587489aebb8a88f64f00f4f79f62c538f0f4da
Pawel Pekrol <ppekrol@ais.pl>
2014-05-09 09:43:43 +0200

    message fix

--- bisect.ps1
+++ bisect.ps1
@@ -2,7 +2,7 @@ $start = $args[0] #bad revision
 $end = $args[1] #good revision
 $test_prefix = $args[2] #test name (full)
 
-"Bisecting. Good: $start. Bad: $end. Test: $test_prefix"
+"Bisecting. Bad: $start. Good: $end. Test: $test_prefix"
 
 &"git" bisect start $start $end
 &"git" bisect run bisect_internal.sh $test_prefix
