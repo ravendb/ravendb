9b1dcd65f74f2b1c949168715a36d69528d468fb
Regina Bricker <regina@hibernatingrhinos.com>
2014-05-14 15:58:04 +0300

    issue 2239 fix for 2.5

--- Raven.Database/Indexing/RobustEnumerator.cs
+++ Raven.Database/Indexing/RobustEnumerator.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using Raven.Database.Linq;
 using Raven.Database.Storage;
@@ -13,93 +14,105 @@ using System.Linq;
 
 namespace Raven.Database.Indexing
 {
-	public class RobustEnumerator
-	{
-		public Action BeforeMoveNext = delegate { };
-		public Action CancelMoveNext = delegate { };
-		public Action<Exception, object> OnError = delegate { };
-		private readonly CancellationToken cancellationToken;
-		private readonly int numberOfConsecutiveErrors;
+    public class RobustEnumerator
+    {
+        public Action BeforeMoveNext = delegate { };
+        public Action CancelMoveNext = delegate { };
+        public Action<Exception, object> OnError = delegate { };
+        private readonly CancellationToken cancellationToken;
+        private readonly int numberOfConsecutiveErrors;
 
-		public RobustEnumerator(CancellationToken cancellationToken, int numberOfConsecutiveErrors)
-		{
-			this.cancellationToken = cancellationToken;
-			this.numberOfConsecutiveErrors = numberOfConsecutiveErrors;
-		}
+        public RobustEnumerator(CancellationToken cancellationToken, int numberOfConsecutiveErrors)
+        {
+            this.cancellationToken = cancellationToken;
+            this.numberOfConsecutiveErrors = numberOfConsecutiveErrors;
+        }
 
-		public IEnumerable<object> RobustEnumeration(IEnumerator<object> input, IndexingFunc func)
-		{
-			return RobustEnumeration(input, new[] { func, });
-		}
+        public IEnumerable<object> RobustEnumeration(IEnumerator<object> input, IndexingFunc func)
+        {
+            using (var wrapped = new StatefulEnumerableWrapper<dynamic>(input))
+            {
+                IEnumerator<dynamic> en;
+                using (en = func(wrapped).GetEnumerator())
+                {
+                    int maxNumberOfConsecutiveErrors = numberOfConsecutiveErrors;
+                    do
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        var moveSuccessful = MoveNext(en, wrapped);
+                        if (moveSuccessful == false)
+                            break;
+                        if (moveSuccessful == true)
+                        {
+                            maxNumberOfConsecutiveErrors = numberOfConsecutiveErrors;
 
-		public IEnumerable<object> RobustEnumeration(IEnumerator<object> input, IEnumerable<IndexingFunc> funcs)
-		{
-			var onlyIterateOverEnumableOnce = new List<object>();
-			try
-			{
-				while (input.MoveNext())
-				{
-					onlyIterateOverEnumableOnce.Add(input.Current);
-				}
-			}
-			catch (Exception e)
-			{
-				OnError(e, null);
-				yield break;
-			}
+                            yield return en.Current;
+                        }
+                        else
+                        {
+                            // we explicitly do not dispose the enumerator, since that would not allow us 
+                            // to continue on with the next item in the list.
+                            // Not actually a problem, because we are iterating only over in memory data
+                            // en.Dispose();
 
-			foreach (var func in funcs)
-			{
-				using (var wrapped = new StatefulEnumerableWrapper<dynamic>(onlyIterateOverEnumableOnce.GetEnumerator()))
-				{
-					IEnumerator<dynamic> en;
-					using (en = func(wrapped).GetEnumerator())
-					{
-						int maxNumberOfConsecutiveErrors = numberOfConsecutiveErrors;
-						do
-						{
-							cancellationToken.ThrowIfCancellationRequested();
-							var moveSuccessful = MoveNext(en, wrapped);
-							if (moveSuccessful == false)
-								break;
-							if (moveSuccessful == true)
-							{
-								maxNumberOfConsecutiveErrors = numberOfConsecutiveErrors;
-								yield return en.Current;
-							}
-							else
-							{
-								// we explicitly do not dispose the enumerator, since that would not allow us 
-								// to continue on with the next item in the list.
-								// Not actually a problem, because we are iterating only over in memory data
-								// en.Dispose();
+                            en = func(wrapped).GetEnumerator();
+                            maxNumberOfConsecutiveErrors--;
+                        }
+                    } while (maxNumberOfConsecutiveErrors > 0);
+                }
+            }
+        }
 
-								en = func(wrapped).GetEnumerator();
-								maxNumberOfConsecutiveErrors--;
-							}
-						} while (maxNumberOfConsecutiveErrors > 0);
-					}
-				}
-			}
-		}
+        public IEnumerable<object> RobustEnumeration(IEnumerator<object> input, List<IndexingFunc> funcs)
+        {
+            if (funcs.Count == 1)
+            {
+                foreach (var item in RobustEnumeration(input, funcs[0]))
+                {
+                    yield return item;                    
+                }
+                yield break;
+            }
+            var onlyIterateOverEnumableOnce = new List<object>();
+            try
+            {
+                while (input.MoveNext())
+                {
+                    onlyIterateOverEnumableOnce.Add(input.Current);
+                }
+            }
+            catch (Exception e)
+            {
+                OnError(e, null);
+                yield break;
+            }
 
-		private bool? MoveNext(IEnumerator en, StatefulEnumerableWrapper<object> innerEnumerator)
-		{
-			try
-			{
-				BeforeMoveNext();
-				var moveNext = en.MoveNext();
-				if (moveNext == false)
-				{
-					CancelMoveNext();
-				}
-				return moveNext;
-			}
-			catch (Exception e)
-			{
-				OnError(e, innerEnumerator.Current);
-			}
-			return null;
-		}
-	}
+            foreach (var func in funcs)
+            {
+                foreach (var item in RobustEnumeration(onlyIterateOverEnumableOnce.GetEnumerator(), func))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        private bool? MoveNext(IEnumerator en, StatefulEnumerableWrapper<object> innerEnumerator)
+        {
+            try
+            {
+                BeforeMoveNext();
+                var moveNext = en.MoveNext();
+                if (moveNext == false)
+                {
+                    CancelMoveNext();
+                }
+                return moveNext;
+            }
+            catch (Exception e)
+            {
+                OnError(e, innerEnumerator.Current);
+            }
+            return null;
+        }
+    }
 }
---
+++ Raven.Tests/Queries/RavenDb_2239.cs
@@ -0,0 +1,283 @@
+﻿using Raven.Client;
+using Raven.Client.Indexes;
+using Raven.Tests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Raven.Tests.Queries
+{
+    public class RavenDb_2239 : RavenTestBase
+    {
+        public class Document
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public int Num { get; set; }
+
+        }
+
+        public class DocumentName
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public int Num { get; set; }
+
+        }
+
+        public class Document_Index : AbstractIndexCreationTask<Document>
+        {
+            public Document_Index()
+            {
+                Map = docs => from doc in docs
+                              select new
+                              {
+                                  doc.Id,
+                                  doc.Name,
+                              };
+            }
+        }
+        public class TestDocument_Index : AbstractIndexCreationTask<Document>
+        {
+            public TestDocument_Index()
+            {
+                Map = docs => from doc in docs
+                              select new
+                              {
+                                  doc.Id,
+                                  doc.Name,
+                                  doc.Num,
+                              };
+            }
+        }
+        public class TestDocumentNameTransformer : AbstractTransformerCreationTask<Document>
+        {
+            public TestDocumentNameTransformer()
+            {
+                TransformResults = docs => from doc in docs
+                                           select new { a = 100 / doc.Num };
+            }
+        }
+
+        public void TestSetupData(IDocumentStore store)
+        {
+            new Document_Index().Execute(store);
+            new TestDocumentNameTransformer().Execute(store);
+
+
+            using (var session = store.OpenSession())
+            {
+
+                for (int docId = 10, i = 0; docId >= 0; docId--, i++)
+                {
+                    session.Store(new Document
+                    {
+                        Id = "documents/" + i,
+                        Name = "Doc" + i,
+                        Description = "Test document description for " + docId,
+                        Num = docId
+                    });
+                }
+                session.SaveChanges();
+            }
+
+
+            WaitForIndexing(store);
+        }
+        public class DocumentNameTransformer : AbstractTransformerCreationTask<Document>
+        {
+            public DocumentNameTransformer()
+            {
+                TransformResults = docs => from doc in docs
+                                           select new { doc.Id, doc.Name };
+            }
+        }
+
+
+        private void SetupData(IDocumentStore store)
+        {
+            new Document_Index().Execute(store);
+            new DocumentNameTransformer().Execute(store);
+
+            //   var docsToCreate = Enumerable.Range(1, 100000);
+            var docsToCreate = Enumerable.Range(1, 200000); //15000 ok, 30 crash time for transformer 15K 00:02:19.5786329 without 00:01:25.3800946
+            var skip = 0;
+
+            while (true)
+            {
+                var batch = docsToCreate.Skip(skip).Take(256).ToList();
+
+                if (!batch.Any()) break;
+                skip += batch.Count;
+
+                using (var session = store.OpenSession())
+                {
+                    foreach (var docId in batch)
+                    {
+                        session.Store(new Document
+                        {
+                            Id = "documents/" + docId,
+                            Name = "Doc" + docId,
+                            Description = "Test document description for " + docId,
+                        });
+                    }
+                    session.SaveChanges();
+                }
+            }
+
+            WaitForIndexing(store);
+        }
+        [Fact]
+        public void SmallLogTransformerTest()
+        {
+            using (var store = NewDocumentStore()) //requestedStorage: "esent"))
+            {
+                var sw = new Stopwatch();
+                sw.Restart();
+                TestSetupData(store);
+                Trace.WriteLine(" fill db finished " + sw.Elapsed);
+                using (var session = store.OpenSession())
+                {
+                    sw.Restart();
+                    Trace.WriteLine("Before Stream Query");
+                    using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>().TransformWith<TestDocumentNameTransformer, DocumentName>()))
+                    {
+                        Trace.WriteLine("started streaming");
+                        enumerator.MoveNext();
+                        Trace.WriteLine("move next first time");
+                        sw.Stop();
+                        Trace.WriteLine("Time to first result with transformer: " + sw.Elapsed);
+                        while (enumerator.MoveNext())
+                        {
+                            sw.Restart();
+                            Trace.WriteLine("Time to first result with transformer: " + sw.Elapsed);
+                        }
+                    }
+                }
+
+            }
+        }
+        [Fact]
+        public void Profiling()
+        {
+            // using (var store = NewRemoteDocumentStore(fiddler: true)) //requestedStorage: "esent"))
+            Trace.WriteLine("start LogTransformerDelay fill db " + DateTime.UtcNow);
+            using (var store = NewDocumentStore()) //requestedStorage: "esent"))
+            {
+                SetupData(store);
+            }
+            //using (var store = new EmbeddedDocumentStore { DataDirectory = "\\Data" }) //requestedStorage: "esent"))
+            //{
+            //    store.Initialize();
+            //    SetupData(store);
+            //}
+        }
+
+
+        [Fact]
+        public void LogTransformerDelay()
+        {
+           // using (var store = NewRemoteDocumentStore(fiddler: true)) //requestedStorage: "esent"))
+            Trace.WriteLine("start LogTransformerDelay fill db "+ DateTime.UtcNow);
+            using (var store = NewDocumentStore()) //requestedStorage: "esent"))
+            {
+                SetupData(store);
+                Trace.WriteLine(" fill db finished " + DateTime.UtcNow);
+
+                var sw = new Stopwatch();
+                using (var session = store.OpenSession())
+                {
+
+                    sw.Restart();
+                    using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>().TransformWith<DocumentNameTransformer, DocumentName>()))
+                    {
+                        enumerator.MoveNext();
+                        sw.Stop();
+                        System.Diagnostics.Trace.WriteLine("Time to first result with transformer: " + sw.Elapsed);
+                        while (enumerator.MoveNext())
+                        {
+
+                        }
+                    }
+                }
+
+                Trace.WriteLine("finished " + DateTime.UtcNow);
+
+            }
+        }
+        [Fact]
+        public void FullLogTransformerDelay()
+        {
+            using (var store = NewDocumentStore()) //requestedStorage: "esent"))
+            {
+                var sw = new Stopwatch();
+                sw.Restart();
+                SetupData(store);
+                Trace.WriteLine(" fill db finished " + sw.Elapsed);
+                for (int i = 0; i < 3; i++)
+                {
+                    using (var session = store.OpenSession())
+                    {
+                        sw.Restart();
+                        using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>().TransformWith<DocumentNameTransformer, DocumentName>()))
+                        {
+                            enumerator.MoveNext();
+                            sw.Stop();
+                            Trace.WriteLine("Time to first result with transformer: " + sw.Elapsed);
+                            while (enumerator.MoveNext())
+                            {
+
+                            }
+                        }
+                    }
+                    using (var session = store.OpenSession())
+                    {
+                        sw.Restart();
+                        using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>()))
+                        {
+                            enumerator.MoveNext();
+                            sw.Stop();
+                            Trace.WriteLine("Time to first result: " + sw.Elapsed);
+                            while (enumerator.MoveNext())
+                            {
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        [Fact]
+        public void LogWithoutTransformer()
+        {
+            using (var store = NewRemoteDocumentStore(fiddler: true)) //requestedStorage: "esent"))
+            {
+                SetupData(store);
+
+                var sw = new Stopwatch();
+              
+                using (var session = store.OpenSession())
+                {
+                    sw.Restart();
+                    using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>()))
+                    {
+                        enumerator.MoveNext();
+                        sw.Stop();
+                        System.Diagnostics.Trace.WriteLine("Time to first result: " + sw.Elapsed);
+                        while (enumerator.MoveNext())
+                        {
+
+                        }
+                    }
+                }
+
+
+            }
+        }
+    }
+}
