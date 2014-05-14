b8955331336f09deea1e298a1a5d2daa11794b7f
Regina Bricker <regina@hibernatingrhinos.com>
2014-05-14 18:05:47 +0300

    issue 2239 still exists problem,both tests fail. full takes too long time

--- Raven.Tests.Issues/RavenDb_2239.cs
+++ Raven.Tests.Issues/RavenDb_2239.cs
@@ -3,6 +3,7 @@ using Raven.Client.Document;
 using Raven.Client.Indexes;
 using Raven.Database.Client;
 using Raven.Tests.Helpers;
+using Raven.Client.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -20,12 +21,16 @@ namespace Raven.Tests.Issues
             public string Id { get; set; }
             public string Name { get; set; }
             public string Description { get; set; }
+           public int Num { get; set; }
+
         }
 
         public class DocumentName
         {
             public string Id { get; set; }
             public string Name { get; set; }
+            public int Num { get; set; }
+
         }
 
         public class Document_Index : AbstractIndexCreationTask<Document>
@@ -37,10 +42,23 @@ namespace Raven.Tests.Issues
                               {
                                   doc.Id,
                                   doc.Name,
+                               
                               };
             }
         }
-
+         public class TestDocument_Index : AbstractIndexCreationTask<Document>
+        {
+            public TestDocument_Index()
+            {
+                Map = docs => from doc in docs
+                              select new
+                              {
+                                  doc.Id,
+                                  doc.Name,
+                                  doc.Num,
+                              };
+            }
+        }
         public class DocumentNameTransformer : AbstractTransformerCreationTask<Document>
         {
             public DocumentNameTransformer()
@@ -50,13 +68,46 @@ namespace Raven.Tests.Issues
             }
         }
 
+       public class TestDocumentNameTransformer : AbstractTransformerCreationTask<Document>
+        {
+            public TestDocumentNameTransformer()
+            {
+                TransformResults = docs => from doc in docs
+                                           select new { a=100/doc.Num};
+            }
+        }
+
+         public void TestSetupData(IDocumentStore store)
+        {
+            new TestDocument_Index().Execute(store);
+            new TestDocumentNameTransformer().Execute(store);
+
+   
+                using (var session = store.OpenSession())
+                {
+                   
+                    for (int docId = 10,i=0; docId >= 0; docId--,i++)
+                    {
+                        session.Store(new Document
+                        {
+                            Id = "documents/" + i,
+                            Name = "Doc" +i,
+                            Description = "Test document description for " + docId,
+                            Num = docId
+                        });
+                    }
+                    session.SaveChanges();
+               }
+              
+ 
+            WaitForIndexing(store);
+        }
 
         public void SetupData(IDocumentStore store)
         {
             new Document_Index().Execute(store);
             new DocumentNameTransformer().Execute(store);
 
-         //   var docsToCreate = Enumerable.Range(1, 100000);
             var docsToCreate = Enumerable.Range(1, 20000); //15000 ok, 30 crash time for transformer 15K 00:02:19.5786329 without 00:01:25.3800946
             var skip = 0;
 
@@ -86,103 +137,105 @@ namespace Raven.Tests.Issues
         }
 
         [Fact]
-        public void Profiling()
+        public void SmallLogTransformerTest()
         {
-            Trace.WriteLine("start LogTransformerDelay fill db " + DateTime.UtcNow);
-            //using (var store = NewRemoteDocumentStore(fiddler: true)) //requestedStorage: "esent"))
-             using (var store = new EmbeddedDocumentStore { DataDirectory = "\\Data" }) //requestedStorage: "esent"))
-             {
-                store.Initialize();
-                SetupData(store);
-             }
-        }
-
-        [Fact]
-        public void LogTransformerDelay()
-        {
-            Trace.WriteLine("start LogTransformerDelay fill db " + DateTime.UtcNow);
-            using (var store = NewRemoteDocumentStore(fiddler: true)) //requestedStorage: "esent"))
-          //  using (var store = NewDocumentStore()) //requestedStorage: "esent"))
+            using (var store = NewDocumentStore())
             {
-                SetupData(store);
-                Trace.WriteLine(" fill db finished " + DateTime.UtcNow);
                 var sw = new Stopwatch();
+                sw.Restart();
+                TestSetupData(store);
+
+                WaitForUserToContinueTheTest(store);
+                Trace.WriteLine(" fill db finished " + sw.Elapsed);
+                int cntr = 0;
                 using (var session = store.OpenSession())
                 {
-
-                    sw.Restart();
-                    using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>().TransformWith<DocumentNameTransformer, DocumentName>()))
+                    Assert.Throws<AggregateException>(() =>
                     {
-                        enumerator.MoveNext();
-                        sw.Stop();
-                        Trace.WriteLine("Time to first result with transformer: " + sw.Elapsed);
-                        while (enumerator.MoveNext())
+                        using (
+                            var enumerator =
+                                session.Advanced.Stream(
+                                    session.Query<Document, TestDocument_Index>()
+                                        .OrderByDescending(x => x.Num)
+                                        .TransformWith<TestDocumentNameTransformer, DocumentName>()))
                         {
-
+                            enumerator.MoveNext();
+                            sw.Stop();
+                            Trace.WriteLine("Time to first result with transformer: " + sw.Elapsed);
+                            cntr++;
+                            while (enumerator.MoveNext())
+                            {
+                                sw.Restart();
+                                Trace.WriteLine("Time to first result with transformer: " + sw.Elapsed);
+                                cntr++;
+                            }
                         }
-                    }
-                    Trace.WriteLine("transformer  test finished " + DateTime.UtcNow);
+                    });
+                    Assert.True(cntr == 10);
                 }
-                using (var session = store.OpenSession())
-                {
-                    sw.Restart();
-                    using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>()))
-                    {
-                        enumerator.MoveNext();
-                        sw.Stop();
-                        Trace.WriteLine("Time to first result: " + sw.Elapsed);
-                        while (enumerator.MoveNext())
-                        {
 
-                        }
-                    }
-                }
-                Trace.WriteLine(" test finished " + DateTime.UtcNow);
-
-                
-            }
-        }
-        [Fact]
-        public void LogWithoutTransformer()
-        {
-            using (var store = NewRemoteDocumentStore(fiddler: true)) //requestedStorage: "esent"))
-            {
-                SetupData(store);
-
-                var sw = new Stopwatch();
-                //using (var session = store.OpenSession())
-                //{
-
-                //    sw.Restart();
-                //    using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>().TransformWith<DocumentNameTransformer, DocumentName>()))
-                //    {
-                //        enumerator.MoveNext();
-                //        sw.Stop();
-                //        System.Diagnostics.Trace.WriteLine("Time to first result with transformer: " + sw.Elapsed);
-                //        while (enumerator.MoveNext())
-                //        {
-
-                //        }
-                //    }
-                //}
-                using (var session = store.OpenSession())
-                {
-                    sw.Restart();
-                    using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>()))
-                    {
-                        enumerator.MoveNext();
-                        sw.Stop();
-                        System.Diagnostics.Trace.WriteLine("Time to first result: " + sw.Elapsed);
-                        while (enumerator.MoveNext())
-                        {
-
-                        }
-                    }
-                }
 
 
             }
         }
+
+       
+
+        [Fact]
+        public void FullLogTransformerDelay()
+        {
+            using (var store = NewDocumentStore())
+            {
+            var withTransformer = new Stopwatch();
+                var withoutTransformer =new  Stopwatch(); 
+
+                var sw = new Stopwatch();
+                sw.Restart();
+                SetupData(store);
+                Trace.WriteLine(" fill db finished " + sw.Elapsed);
+                 for (int i = 0; i < 3; i++)
+                {
+                    using (var session = store.OpenSession())
+                    {
+                        sw.Restart();
+                        using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>().TransformWith<DocumentNameTransformer, DocumentName>()))
+                        {
+                            enumerator.MoveNext();
+                            sw.Stop();
+                            Trace.WriteLine("Time to first result with transformer: " + sw.Elapsed);
+                            while (enumerator.MoveNext())
+                            {
+
+                            }
+                        }
+                        if (i == 2)
+                        {
+                            withTransformer = sw;
+                        }
+                    }
+                    using (var session = store.OpenSession())
+                    {
+                        sw.Restart();
+                        using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>()))
+                        {
+                            enumerator.MoveNext();
+                            sw.Stop();
+                            Trace.WriteLine("Time to first result: " + sw.Elapsed);
+                            while (enumerator.MoveNext())
+                            {
+
+                            }
+                        }
+                    }
+                    if (i == 2)
+                    {
+                        withoutTransformer = sw;
+                    }
+                }
+                Assert.True(withTransformer.Elapsed.TotalMilliseconds <= withoutTransformer.Elapsed.TotalMilliseconds*1.3);
+            }
+        }
+        
     }
 }
 
