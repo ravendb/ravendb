d6643e15d4d420eb339eabd8a0c4881741d730f5
Regina Bricker <regina@hibernatingrhinos.com>
2014-05-14 08:55:33 +0300

    fix in loop in RobustEnumerator

--- Raven.Tests.Issues/RavenDb_2239.cs
+++ Raven.Tests.Issues/RavenDb_2239.cs
@@ -1,5 +1,7 @@
 ﻿using Raven.Client;
+using Raven.Client.Document;
 using Raven.Client.Indexes;
+using Raven.Database.Client;
 using Raven.Tests.Helpers;
 using System;
 using System.Collections.Generic;
@@ -49,13 +51,13 @@ namespace Raven.Tests.Issues
         }
 
 
-        private void SetupData(IDocumentStore store)
+        public void SetupData(IDocumentStore store)
         {
             new Document_Index().Execute(store);
             new DocumentNameTransformer().Execute(store);
 
          //   var docsToCreate = Enumerable.Range(1, 100000);
-            var docsToCreate = Enumerable.Range(1, 100000);
+            var docsToCreate = Enumerable.Range(1, 20000); //15000 ok, 30 crash time for transformer 15K 00:02:19.5786329 without 00:01:25.3800946
             var skip = 0;
 
             while (true)
@@ -84,14 +86,85 @@ namespace Raven.Tests.Issues
         }
 
         [Fact]
+        public void Profiling()
+        {
+            Trace.WriteLine("start LogTransformerDelay fill db " + DateTime.UtcNow);
+            //using (var store = NewRemoteDocumentStore(fiddler: true)) //requestedStorage: "esent"))
+             using (var store = new EmbeddedDocumentStore { DataDirectory = "\\Data" }) //requestedStorage: "esent"))
+             {
+                store.Initialize();
+                SetupData(store);
+             }
+        }
+
+        [Fact]
         public void LogTransformerDelay()
         {
-            using (var store = NewDocumentStore()) //requestedStorage: "esent"))
+            Trace.WriteLine("start LogTransformerDelay fill db " + DateTime.UtcNow);
+            using (var store = NewRemoteDocumentStore(fiddler: true)) //requestedStorage: "esent"))
+          //  using (var store = NewDocumentStore()) //requestedStorage: "esent"))
+            {
+                SetupData(store);
+                Trace.WriteLine(" fill db finished " + DateTime.UtcNow);
+                var sw = new Stopwatch();
+                using (var session = store.OpenSession())
+                {
+
+                    sw.Restart();
+                    using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>().TransformWith<DocumentNameTransformer, DocumentName>()))
+                    {
+                        enumerator.MoveNext();
+                        sw.Stop();
+                        Trace.WriteLine("Time to first result with transformer: " + sw.Elapsed);
+                        while (enumerator.MoveNext())
+                        {
+
+                        }
+                    }
+                    Trace.WriteLine("transformer  test finished " + DateTime.UtcNow);
+                }
+                using (var session = store.OpenSession())
+                {
+                    sw.Restart();
+                    using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>()))
+                    {
+                        enumerator.MoveNext();
+                        sw.Stop();
+                        Trace.WriteLine("Time to first result: " + sw.Elapsed);
+                        while (enumerator.MoveNext())
+                        {
+
+                        }
+                    }
+                }
+                Trace.WriteLine(" test finished " + DateTime.UtcNow);
+
+                
+            }
+        }
+        [Fact]
+        public void LogWithoutTransformer()
+        {
+            using (var store = NewRemoteDocumentStore(fiddler: true)) //requestedStorage: "esent"))
             {
                 SetupData(store);
 
                 var sw = new Stopwatch();
+                //using (var session = store.OpenSession())
+                //{
 
+                //    sw.Restart();
+                //    using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>().TransformWith<DocumentNameTransformer, DocumentName>()))
+                //    {
+                //        enumerator.MoveNext();
+                //        sw.Stop();
+                //        System.Diagnostics.Trace.WriteLine("Time to first result with transformer: " + sw.Elapsed);
+                //        while (enumerator.MoveNext())
+                //        {
+
+                //        }
+                //    }
+                //}
                 using (var session = store.OpenSession())
                 {
                     sw.Restart();
@@ -107,21 +180,7 @@ namespace Raven.Tests.Issues
                     }
                 }
 
-                using (var session = store.OpenSession())
-                {
 
-                    sw.Restart();
-                    using (var enumerator = session.Advanced.Stream(session.Query<Document, Document_Index>().TransformWith<DocumentNameTransformer, DocumentName>()))
-                    {
-                        enumerator.MoveNext();
-                        sw.Stop();
-                        System.Diagnostics.Trace.WriteLine("Time to first result with transformer: " + sw.Elapsed);
-                        while (enumerator.MoveNext())
-                        {
-
-                        }
-                    }
-                }
             }
         }
     }
--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -1,7 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using NLog;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Extensions;
+using Raven.Client.Indexes;
 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Database.Client;
+using Raven.Tests.Issues;
+using Xunit;
 namespace Raven.Tryouts
 {
 	class Program
@@ -10,6 +23,18 @@ namespace Raven.Tryouts
 
 		private static void Main(string[] args)
 		{
+		    using (var test = new RavenDb_2239())
+		    {
+                using (var store = new DocumentStore { Url = "http://localhost:8080",DefaultDatabase = "TestDB"}) //requestedStorage: "esent"))
+                {
+                    store.Initialize();
+                    store.DatabaseCommands.GlobalAdmin.EnsureDatabaseExists("TestDB");
+
+                    Console.WriteLine("Press any key to begin test");
+                    Console.ReadLine();
+                    test.SetupData(store);
+                }
+		    }
 		}
 	}
 
