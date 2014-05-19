b38aaf21d239f4f2c7825ff1d7c64c9bba3a7e4b
Federico <federico.lois@corvalius.com>
2014-05-19 16:56:17 -0300

    - Fixed: RavenDB-2278 introduced in b8955331336f09deea1e298a1a5d2daa11794b7f [b895533] * http://issues.hibernatingrhinos.com/issue/RavenDB-2278

--- Raven.Database/Server/AppBuilderExtensions.cs
+++ Raven.Database/Server/AppBuilderExtensions.cs
@@ -21,6 +21,7 @@ using Raven.Database.Server.Tenancy;
 using Raven.Database.Server.WebApi;
 using Raven.Database.Server.WebApi.Filters;
 using Raven.Database.Server.WebApi.Handlers;
+using System.Net;
 
 // ReSharper disable once CheckNamespace
 namespace Owin
@@ -139,7 +140,7 @@ namespace Owin
 			{
                 var content = response.Content;
 			    var compressedContent = content as GZipToJsonAndCompressHandler.CompressedContent;
-			    if (compressedContent != null)
+			    if (compressedContent != null && response.StatusCode != HttpStatusCode.NoContent)
                     return ShouldBuffer(compressedContent.OriginalContent);
 			    return ShouldBuffer(content);
 			}
