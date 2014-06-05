7950c03412df740463dd8d8f5a9f58c605f6d5a3
Grisha Kotler <grisha@ayende.com>
2014-06-05 19:02:32 +0300

    - fix for old studio (/databases query) - fix for long database names

--- Raven.Studio.Html5/App/commands/getDatabasesCommand.ts
+++ Raven.Studio.Html5/App/commands/getDatabasesCommand.ts
@@ -4,8 +4,15 @@ import database = require("models/database");
 class getDatabasesCommand extends commandBase {
     
     execute(): JQueryPromise<database[]> {
+        var args = {
+            pageSize: 1024,
+            getAdditionalData: true
+        };
+
+        var url = "/databases";
+
         var resultsSelector = (databases: databaseDto[]) => databases.map(db => new database(db.Name, db.Disabled));
-        return this.query("/databases", { pageSize: 1024 }, null, resultsSelector);
+        return this.query(url, args, null, resultsSelector);
     }
 }
 
