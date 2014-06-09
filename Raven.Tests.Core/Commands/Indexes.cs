7ec6e28b4b659e2432a8b9b646d3d564f6ec3bf1
Daniel Kardach <dkardach@ais.pl>
2014-06-09 11:31:21 +0200

    Refactoring.

--- Raven.Tests.Core/Commands/Indexes.cs
+++ Raven.Tests.Core/Commands/Indexes.cs
@@ -51,36 +51,6 @@ namespace Raven.Tests.Core.Commands
 		}
 
 		[Fact]
-		public async Task CanPutUpdateAndDeleteTransformer()
-		{
-			using (var store = GetDocumentStore())
-			{
-				const string usersSelectNames = "users/selectName";
-
-				await store.AsyncDatabaseCommands.PutTransformerAsync(usersSelectNames, new TransformerDefinition()
-				{
-					Name = usersSelectNames,
-					TransformResults = "from user in results select new { user.FirstName, user.LastName }"
-				});
-
-				await store.AsyncDatabaseCommands.PutTransformerAsync(usersSelectNames, new TransformerDefinition()
-				{
-					Name = usersSelectNames,
-					TransformResults = "from user in results select new { Name = user.Name }"
-				});
-
-				var transformer = await store.AsyncDatabaseCommands.GetTransformerAsync(usersSelectNames);
-				Assert.Equal("from user in results select new { Name = user.Name }", transformer.TransformResults);
-
-				var transformers = await store.AsyncDatabaseCommands.GetTransformersAsync(0, 5);
-				Assert.Equal(1, transformers.Length);
-
-				await store.AsyncDatabaseCommands.DeleteTransformerAsync(usersSelectNames);
-				Assert.Null(await store.AsyncDatabaseCommands.GetTransformerAsync(usersSelectNames));
-			}
-		}
-
-		[Fact]
 		public void CanGetTermsForIndex()
 		{
 			using (var store = GetDocumentStore())
