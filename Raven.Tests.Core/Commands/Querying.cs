5ac8e4991703d91eb49bc1d286ad134ba5373ed7
Daniel Kardach <dkardach@ais.pl>
2014-06-10 12:17:57 +0200

    CanStreamQueryResult test.

--- Raven.Tests.Core/Commands/Querying.cs
+++ Raven.Tests.Core/Commands/Querying.cs
@@ -2,6 +2,7 @@
 using Raven.Abstractions.Indexing;
 using Raven.Json.Linq;
 using Raven.Tests.Core.Utils.Entities;
+using Raven.Tests.Core.Utils.Indexes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -38,6 +39,32 @@ namespace Raven.Tests.Core.Commands
                 Assert.Equal("Expression Name", companies.Results[0].Value<string>("FirstName"));
                 Assert.Equal("Expression First Name", companies.Results[1].Value<string>("FirstName"));
                 Assert.Equal("First Name", companies.Results[2].Value<string>("FirstName"));
+            }
+        }
+
+        [Fact]
+        public void CanStreamQueryResult()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var index = new Users_ByName();
+                index.Execute(store);
+
+                for (int i = 0; i < 30; i++)
+                {
+                    store.DatabaseCommands.Put("users/" + i, null, RavenJObject.FromObject(new User { Name = "Name"+i }), new RavenJObject { { "Raven-Entity-Name", "Users" } });
+                }
+                WaitForIndexing(store);
+
+                int count = 0;
+                QueryHeaderInformation queryHeaders = null;
+                var reader = store.DatabaseCommands.StreamQuery(index.IndexName, new IndexQuery { Query = "" }, out queryHeaders);
+                while (reader.MoveNext())
+                {
+                    Assert.Equal("Name" + count, reader.Current.Value<string>("Name"));
+                    count++;
+                }
+                Assert.Equal(30, count);
             }
         }
     }
