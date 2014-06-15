ab07d0000031e80a7f3032e152884de9c832ca58
Regina Bricker <regina@hibernatingrhinos.com>
2014-06-15 15:44:56 +0300

    issue 2428 (spatial index problem) fixed

--- Raven.Abstractions/Spatial/GeoJsonWktConverter.cs
+++ Raven.Abstractions/Spatial/GeoJsonWktConverter.cs
@@ -37,13 +37,20 @@ namespace Raven.Abstractions.Spatial
 
         private bool TryParseTypeString(RavenJObject obj, out string result)
         {
-			RavenJToken type = null;
+            RavenJToken type = null;
+            result = null;
             if (obj != null)
+            {
                 obj.TryGetValue("type", out type);
-                
-            result = ((RavenJValue) type).Value as string;
-			
-            return type != null;
+
+                if (type == null)
+                    obj.TryGetValue("$type", out type);
+                if (type == null)
+                    result = null;
+                else
+                    result = ((RavenJValue)type).Value as string;
+            }
+             return type != null;
         }
 
 		//private bool TryParseFeatureCollection(RavenJObject obj, StringBuilder result)
