a44c1c8683f9af3dbf5038286ab16fd9d56178e1
Ayende Rahien <ayende@ayende.com>
2014-06-24 15:58:37 +0300

    Removing a test that is supposed to fail and only worked by accident.

--- Raven.Tests.MailingList/ConcurrencyTests2.cs
+++ Raven.Tests.MailingList/ConcurrencyTests2.cs
@@ -66,31 +66,5 @@ namespace Raven.Tests.MailingList
                 }
             }
         }
-
-        [Fact]
-        public void Concurrency_Failing_Test()
-        {
-            using (var store = NewRemoteDocumentStore())
-            {
-                for (int i = 0; i < 2; i++)
-                {
-                    using (var session = store.OpenSession())
-                    {
-                        session.Advanced.UseOptimisticConcurrency = true;
-                        Simple simple = new Simple { Id = 1, key = "New", stamp = (int)DateTime.UtcNow.Ticks };
-                        session.Store(simple);
-                        session.SaveChanges();
-                    }
-
-                    using (var session = store.OpenSession())
-                    {
-                        session.Advanced.UseOptimisticConcurrency = false;
-                        Simple simple = new Simple { Id = 1, key = "Override", stamp = (int)DateTime.UtcNow.Ticks };
-                        session.Store(simple);
-                        session.SaveChanges();
-                    }
-                }
-            }
-        }
     }
 }
