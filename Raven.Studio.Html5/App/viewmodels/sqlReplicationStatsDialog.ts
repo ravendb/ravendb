ed1560b847020a93e181ce5dbb2401bbd48db232
maximburyak <maxim@ayende.com>
2014-06-26 17:08:48 +0300

    implemented showing sql replication stats dialog, also added a "clockworks" button to the replication stats screen, to encapsulate the delet, show stats and reset (only stab now) operations

---
+++ Raven.Studio.Html5/App/viewmodels/sqlReplicationStatsDialog.ts
@@ -0,0 +1,41 @@
+﻿import getStatusDebugSqlReplicationCommand = require("commands/getStatusDebugSqlReplicationCommand");
+import appUrl = require("common/appUrl");
+import database = require("models/database");
+import dialogViewModelBase = require("viewmodels/dialogViewModelBase");
+import dialog = require("plugins/dialog");
+
+class sqlReplicationStatsDialog extends dialogViewModelBase {
+    data = ko.observable<sqlReplicationStatisticsDto[]>();
+    repData= ko.observable<sqlReplicationStatisticsDto>();
+
+    constructor(private activeDatabase: database, private sqlReplicationName:string) {
+        super();
+        this.fetchSqlReplicationStats();
+    }
+
+    activate(args) {
+        return this.fetchSqlReplicationStats();
+    }
+
+    cancel() {
+        dialog.close(this);
+    }
+
+    fetchSqlReplicationStats(): JQueryPromise<sqlReplicationStatisticsDto[]> {
+        var db = this.activeDatabase;
+        if (db) {
+            return new getStatusDebugSqlReplicationCommand(db)
+                .execute()
+                .done((results: sqlReplicationStatisticsDto[]) => {
+                    this.data(results);
+                    if (!!results && results.length > 0) {
+                        this.repData(results[0]);
+                    } 
+            });
+        }
+
+        return null;
+    }
+}
+
+export = sqlReplicationStatsDialog;
