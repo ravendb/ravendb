c1e43d3345d12ca2f1b6023769fd4629927e8ddf
Daniel Kardach <dkardach@ais.pl>
2014-07-17 11:46:06 +0200

    CanGetDocumentUrl test.

---
+++ Raven.Tests.Core/Session/Advanced.cs
@@ -0,0 +1,31 @@
+﻿using Raven.Tests.Core.Utils.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Raven.Tests.Core.Session
+{
+    public class Advanced : RavenCoreTestBase
+    {
+        [Fact]
+        public void CanGetDocumentUrl()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Company { Id = "companies/1" });
+                    session.SaveChanges();
+
+                    var company = session.Load<Company>("companies/1");
+                    Assert.NotNull(company);
+                    var uri = new Uri(session.Advanced.GetDocumentUrl(company));
+                    Assert.Equal("/databases/"+store.DefaultDatabase+"/docs/companies/1", uri.AbsolutePath);
+                }
+            }
+        }
+    }
+}
