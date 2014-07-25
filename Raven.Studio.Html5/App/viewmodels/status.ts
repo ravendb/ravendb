e9439f1e0b25206f5eeeb85d67687c1ac08d48e5
Marcin Lewandowski <marcin@ais.pl>
2014-07-25 13:51:05 +0200

    RavenDB-2349 Generate debug info button

--- Raven.Studio.Html5/App/viewmodels/status.ts
+++ Raven.Studio.Html5/App/viewmodels/status.ts
@@ -17,16 +17,17 @@ class status extends viewModelBase {
 
         this.router = durandalRouter.createChildRouter()
             .map([
-                { route: 'databases/status',                  moduleId: 'viewmodels/statistics',          title: 'Stats',             nav: true, hash: appUrl.forCurrentDatabase().status },
-                { route: 'databases/status/metrics*details',  moduleId: 'viewmodels/metrics',             title: 'Metrics',           nav: true, hash: appUrl.forCurrentDatabase().metrics },
-                { route: 'databases/status/logs',             moduleId: 'viewmodels/logs',                title: 'Logs',              nav: true, hash: appUrl.forCurrentDatabase().logs },
-                { route: 'databases/status/runningTasks',     moduleId: 'viewmodels/runningTasks',        title: 'Running Tasks',     nav: true, hash: appUrl.forCurrentDatabase().runningTasks },
-                { route: 'databases/status/alerts',           moduleId: 'viewmodels/alerts',              title: 'Alerts',            nav: true, hash: appUrl.forCurrentDatabase().alerts },
-                { route: 'databases/status/indexErrors',      moduleId: 'viewmodels/indexErrors',         title: 'Index Errors',      nav: true, hash: appUrl.forCurrentDatabase().indexErrors },
-                { route: 'databases/status/replicationStats', moduleId: 'viewmodels/replicationStats',    title: 'Replication Stats', nav: true, hash: appUrl.forCurrentDatabase().replicationStats },
-                { route: 'databases/status/userInfo',         moduleId: 'viewmodels/userInfo',            title: 'User Info',         nav: true, hash: appUrl.forCurrentDatabase().userInfo },
-                { route: 'databases/status/visualizer',       moduleId: 'viewmodels/visualizer',          title: 'Map/Reduce Visualizer',        nav: true, hash: appUrl.forCurrentDatabase().visualizer },
-                { route: 'databases/status/debug*details',    moduleId: 'viewmodels/statusDebug',         title: 'Debug',             nav: true, hash: appUrl.forCurrentDatabase().statusDebug }
+                { route: 'databases/status', moduleId: 'viewmodels/statistics', title: 'Stats', nav: true, hash: appUrl.forCurrentDatabase().status },
+                { route: 'databases/status/metrics*details', moduleId: 'viewmodels/metrics', title: 'Metrics', nav: true, hash: appUrl.forCurrentDatabase().metrics },
+                { route: 'databases/status/logs', moduleId: 'viewmodels/logs', title: 'Logs', nav: true, hash: appUrl.forCurrentDatabase().logs },
+                { route: 'databases/status/runningTasks', moduleId: 'viewmodels/runningTasks', title: 'Running Tasks', nav: true, hash: appUrl.forCurrentDatabase().runningTasks },
+                { route: 'databases/status/alerts', moduleId: 'viewmodels/alerts', title: 'Alerts', nav: true, hash: appUrl.forCurrentDatabase().alerts },
+                { route: 'databases/status/indexErrors', moduleId: 'viewmodels/indexErrors', title: 'Index Errors', nav: true, hash: appUrl.forCurrentDatabase().indexErrors },
+                { route: 'databases/status/replicationStats', moduleId: 'viewmodels/replicationStats', title: 'Replication Stats', nav: true, hash: appUrl.forCurrentDatabase().replicationStats },
+                { route: 'databases/status/userInfo', moduleId: 'viewmodels/userInfo', title: 'User Info', nav: true, hash: appUrl.forCurrentDatabase().userInfo },
+                { route: 'databases/status/visualizer', moduleId: 'viewmodels/visualizer', title: 'Map/Reduce Visualizer', nav: true, hash: appUrl.forCurrentDatabase().visualizer },
+                { route: 'databases/status/debug*details', moduleId: 'viewmodels/statusDebug', title: 'Debug', nav: true, hash: appUrl.forCurrentDatabase().statusDebug },
+                { route: 'databases/status/infoPackage', moduleId: 'viewmodels/infoPackage', title: 'Gather debug info', nav: true, hash: appUrl.forCurrentDatabase().infoPackage }
 			])
             .buildNavigationModel();
 
