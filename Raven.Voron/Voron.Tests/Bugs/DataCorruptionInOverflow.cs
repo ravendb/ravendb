e1d1e73e19eb87cb20346e50f070be3c8b8232a4
Arkadiusz Palinski <arek@ais.pl>
2014-08-08 09:41:38 +0200

    RavenDB-2585 Added failing test that exposes Voron issue - data corruption in overflow pages caused by invalid write of data from the journal (actually from the scaratch) to the data file

---
+++ Raven.Voron/Voron.Tests/Bugs/DataCorruptionInOverflow.cs
@@ -0,0 +1,62 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="DataCorruptionInOverflow.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using System;
+using Xunit;
+
+namespace Voron.Tests.Bugs
+{
+	public class DataCorruptionInOverflow : StorageTest
+	{
+		protected override void Configure(StorageEnvironmentOptions options)
+		{
+			options.ManualFlushing = true;
+		}
+
+		[Fact]
+		public void IncorrectWriteOfOverflowPagesFromJournalsToDataFile_RavenDB_2585()
+		{
+			const int testedOverflowSize = 20000;
+
+			var overflowValue = new byte[testedOverflowSize];
+			new Random(1).NextBytes(overflowValue);
+
+			using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
+			{
+				var tree = Env.CreateTree(tx, "test");
+
+				var itemBytes = new byte[16000];
+
+				new Random(2).NextBytes(itemBytes);
+				tree.Add("items/1", itemBytes);
+
+				new Random(3).NextBytes(itemBytes);
+				tree.Add("items/2", itemBytes);
+
+				tree.Delete("items/1");
+				tree.Delete("items/2");
+
+				tree.Add("items/3", overflowValue);
+
+				tx.Commit();
+			}
+
+			Env.FlushLogToDataFile();
+
+			using (var tx = Env.NewTransaction(TransactionFlags.Read))
+			{
+				var tree = tx.ReadTree("test");
+
+				var readResult = tree.Read("items/3");
+
+				var readBytes = new byte[testedOverflowSize];
+
+				readResult.Reader.Read(readBytes, 0, testedOverflowSize);
+
+				Assert.Equal(overflowValue, readBytes);
+			}
+		}
+	}
+}
