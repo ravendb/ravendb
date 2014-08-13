de77aef42bf96340b1289778ac2a39be4abb39c7
Ayende Rahien <ayende@ayende.com>
2014-08-13 15:26:38 +0300

    Making sure that the exception is serializable

--- Raven.Voron/Voron/Exceptions/ScratchBufferSizeLimitException.cs
+++ Raven.Voron/Voron/Exceptions/ScratchBufferSizeLimitException.cs
@@ -4,15 +4,30 @@
 //  </copyright>
 // -----------------------------------------------------------------------
 using System;
+using System.Runtime.Serialization;
 
 namespace Voron.Exceptions
 {
+
+	[Serializable]
 	public class ScratchBufferSizeLimitException : Exception
 	{
-		public ScratchBufferSizeLimitException(string message)
-			: base(message)
+		public ScratchBufferSizeLimitException()
 		{
-			
+		}
+
+		public ScratchBufferSizeLimitException(string message) : base(message)
+		{
+		}
+
+		public ScratchBufferSizeLimitException(string message, Exception inner) : base(message, inner)
+		{
+		}
+
+		protected ScratchBufferSizeLimitException(
+			SerializationInfo info,
+			StreamingContext context) : base(info, context)
+		{
 		}
 	}
 }
