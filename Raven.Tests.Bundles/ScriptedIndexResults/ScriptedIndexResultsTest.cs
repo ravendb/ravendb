1f15b265d066265a2782fc6c7fffb228586fc23d
JudahGabriel <judahgabriel@gmail.com>
2014-08-18 01:36:08 -0500

    Reverting accidental format changes

--- Raven.Tests.Bundles/ScriptedIndexResults/ScriptedIndexResultsTest.cs
+++ Raven.Tests.Bundles/ScriptedIndexResults/ScriptedIndexResultsTest.cs
@@ -10,29 +10,29 @@ using Xunit;
 
 namespace Raven.Tests.Bundles.ScriptedIndexResults
 {
-    public class ScriptedIndexResultsTest : RavenTest
-    {
-        protected override void ModifyConfiguration(Database.Config.InMemoryRavenConfiguration configuration)
-        {
-            configuration.Settings["Raven/ActiveBundles"] = "ScriptedIndexResults";
-        }
+	public class ScriptedIndexResultsTest : RavenTest
+	{
+		protected override void ModifyConfiguration(Database.Config.InMemoryRavenConfiguration configuration)
+		{
+			configuration.Settings["Raven/ActiveBundles"] = "ScriptedIndexResults";
+		}
 
-        [Fact]
-        public void CanUpdateValueOnDocument()
-        {
-            using (var store = NewDocumentStore())
-            {
-                using (var s = store.OpenSession())
-                {
-                    s.Store(new Raven.Abstractions.Data.ScriptedIndexResults
-                    {
-                        Id = Raven.Abstractions.Data.ScriptedIndexResults.IdPrefix + new Animals_Stats().IndexName,
-                        IndexScript = @"
+		[Fact]
+		public void CanUpdateValueOnDocument()
+		{
+			using (var store = NewDocumentStore())
+			{
+				using (var s = store.OpenSession())
+				{
+					s.Store(new Raven.Abstractions.Data.ScriptedIndexResults
+					{
+						Id = Raven.Abstractions.Data.ScriptedIndexResults.IdPrefix + new Animals_Stats().IndexName,
+						IndexScript = @"
 var docId = 'AnimalTypes/'+ key;
 var type = LoadDocument(docId) || {};
 type.Count = this.Count;
 PutDocument(docId, type);",
-                        DeleteScript = @"
+						DeleteScript = @"
 var docId = 'AnimalTypes/'+ key;
 var type = LoadDocument(docId);
 if(type == null)
@@ -40,59 +40,59 @@ if(type == null)
 type.Count = 0;
 PutDocument(docId, type);
 "
-                    });
-                    s.SaveChanges();
-                }
-                using (var s = store.OpenSession())
-                {
-                    s.Store(new Animal
-                    {
-                        Name = "Arava",
-                        Type = "Dog"
-                    });
-                    s.Store(new Animal
-                    {
-                        Name = "Oscar",
-                        Type = "Dog"
-                    });
+					});
+					s.SaveChanges();
+				}
+				using (var s = store.OpenSession())
+				{
+					s.Store(new Animal
+					{
+						Name = "Arava",
+						Type = "Dog"
+					});
+					s.Store(new Animal
+					{
+						Name = "Oscar",
+						Type = "Dog"
+					});
 
-                    s.Store(new AnimalType
-                    {
-                        Id = "AnimalTypes/Dog",
-                        Description = "Man's Best Friend"
-                    });
+					s.Store(new AnimalType
+					{
+						Id = "AnimalTypes/Dog",
+						Description = "Man's Best Friend"
+					});
 
-                    s.SaveChanges();
-                }
+					s.SaveChanges();
+				}
 
-                new Animals_Stats().Execute(store);
+				new Animals_Stats().Execute(store);
 
-                WaitForIndexing(store);
+				WaitForIndexing(store);
 
-                using (var s = store.OpenSession())
-                {
-                    var animalType = s.Load<AnimalType>("AnimalTypes/Dog");
-                    Assert.Equal(2, animalType.Count);
-                }
-            }
-        }
+				using (var s = store.OpenSession())
+				{
+					var animalType = s.Load<AnimalType>("AnimalTypes/Dog");
+					Assert.Equal(2, animalType.Count);
+				}
+			}
+		}
 
-        [Fact]
-        public void CanUpdateValueOnDocumentWhenItemIsRemoved()
-        {
-            using (var store = NewDocumentStore())
-            {
-                using (var s = store.OpenSession())
-                {
-                    s.Store(new Raven.Abstractions.Data.ScriptedIndexResults
-                    {
-                        Id = Raven.Abstractions.Data.ScriptedIndexResults.IdPrefix + new Animals_Stats().IndexName,
-                        IndexScript = @"
+		[Fact]
+		public void CanUpdateValueOnDocumentWhenItemIsRemoved()
+		{
+			using (var store = NewDocumentStore())
+			{
+				using (var s = store.OpenSession())
+				{
+					s.Store(new Raven.Abstractions.Data.ScriptedIndexResults
+					{
+						Id = Raven.Abstractions.Data.ScriptedIndexResults.IdPrefix + new Animals_Stats().IndexName,
+						IndexScript = @"
 var docId = 'AnimalTypes/'+ key;
 var type = LoadDocument(docId) || {};
 type.Count = this.Count;
 PutDocument(docId, type);",
-                        DeleteScript = @"
+						DeleteScript = @"
 var docId = 'AnimalTypes/'+ key;
 var type = LoadDocument(docId);
 if(type == null)
@@ -100,48 +100,48 @@ if(type == null)
 type.Count = 0;
 PutDocument(docId, type);
 "
-                    });
-                    s.SaveChanges();
-                }
-                using (var s = store.OpenSession())
-                {
-                    s.Store(new Animal
-                    {
-                        Name = "Arava",
-                        Type = "Dog"
-                    });
-                    s.Store(new Animal
-                    {
-                        Name = "Oscar",
-                        Type = "Dog"
-                    });
+					});
+					s.SaveChanges();
+				}
+				using (var s = store.OpenSession())
+				{
+					s.Store(new Animal
+					{
+						Name = "Arava",
+						Type = "Dog"
+					});
+					s.Store(new Animal
+					{
+						Name = "Oscar",
+						Type = "Dog"
+					});
 
-                    s.Store(new AnimalType
-                    {
-                        Id = "AnimalTypes/Dog",
-                        Description = "Man's Best Friend"
-                    });
+					s.Store(new AnimalType
+					{
+						Id = "AnimalTypes/Dog",
+						Description = "Man's Best Friend"
+					});
 
-                    s.SaveChanges();
-                }
+					s.SaveChanges();
+				}
 
-                new Animals_Stats().Execute(store);
+				new Animals_Stats().Execute(store);
 
-                WaitForIndexing(store);
+				WaitForIndexing(store);
 
-                store.DatabaseCommands.DeleteByIndex("Raven/DocumentsByEntityName", new IndexQuery
-                {
-                    Query = "Tag:Animals"
-                }).WaitForCompletion();
+				store.DatabaseCommands.DeleteByIndex("Raven/DocumentsByEntityName", new IndexQuery
+				{
+					Query = "Tag:Animals"
+				}).WaitForCompletion();
+				
+				WaitForIndexing(store);
 
-                WaitForIndexing(store);
-
-                using (var s = store.OpenSession())
-                {
-                    var animalType = s.Load<AnimalType>("AnimalTypes/Dog");
-                    Assert.Equal(0, animalType.Count);
-                }
-            }
-        }
-    }
+				using (var s = store.OpenSession())
+				{
+					var animalType = s.Load<AnimalType>("AnimalTypes/Dog");
+					Assert.Equal(0, animalType.Count);
+				}
+			}
+		}
+	}
 }
