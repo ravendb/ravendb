e6a944a9494f3589199f29217ff4f7e1f2c76570
Daniel Kardach <dkardach@ais.pl>
2014-09-01 10:47:49 +0200

    CanUseTransformerWithParameterOrDefault test.

---
+++ Raven.Tests.Core/Utils/Transformers/UsersTransformer.cs
@@ -0,0 +1,26 @@
+﻿using Raven.Client.Indexes;
+using Raven.Tests.Core.Utils.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.Tests.Core.Utils.Transformers
+{
+    public class UsersTransformer : AbstractTransformerCreationTask<User>
+    {
+        public class Result
+        {
+            public string PassedParameter { get; set; }
+        }
+
+        public UsersTransformer()
+        {
+            TransformResults = results => from result in results
+                                          group result by result into g
+                                          let key = ParameterOrDefault("Key", "LastName").Value<string>()
+                                          select new { g.Key, PassedParameter = key };
+        }
+    }
+}
