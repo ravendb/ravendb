44c24dbb2947946f306c179310e0b7e0930ef4db
Ayende Rahien <ayende@ayende.com>
2014-09-17 22:38:56 +0200

    Using Voron on linux!

--- Raven.Voron/Voron.Tryout/Program.cs
+++ Raven.Voron/Voron.Tryout/Program.cs
@@ -17,15 +17,14 @@ using Snapshots = Voron.Tests.Bugs.Snapshots;
 
 namespace Voron.Tryout
 {
-	internal unsafe class Program
+	public unsafe class Program
 	{
-		private static void Main()
+		public static void Main()
 		{
 			using (var env = new StorageEnvironment (StorageEnvironmentOptions.ForPath ("VRN"))) 
 			{
 				var batch = new WriteBatch ();
 				batch.Add ("ayende@ayende.com", "Oren Eini", "Names");
-
 				env.Writer.Write (batch);
 			}
 
@@ -45,19 +44,6 @@ namespace Voron.Tryout
 				}
 			}
 			Console.WriteLine ("Done");
-
-//			var fd = Syscall.open ("test", Mono.Unix.Native.OpenFlags.O_RDWR | Mono.Unix.Native.OpenFlags.O_CREAT,
-//			                       FilePermissions.ALLPERMS);
-//			if(fd == -1)
-//				Console.WriteLine ((Errno)Marshal.GetLastWin32Error ());
-//			var a = Syscall.posix_fallocate (fd, 0, 16);
-//			if(a != 0)
-//				Console.WriteLine ((Errno)a + " a" );
-//			var b = Syscall.mmap (IntPtr.Zero, 16, MmapProts.PROT_READ | MmapProts.PROT_WRITE,
-//			                      MmapFlags.MAP_PRIVATE, fd, 0);
-//			if(b .ToInt64() == -1)
-//				Console.WriteLine ((Errno)Marshal.GetLastWin32Error ());
-//			Console.WriteLine (b.ToString());
 		}
 	}
 }
