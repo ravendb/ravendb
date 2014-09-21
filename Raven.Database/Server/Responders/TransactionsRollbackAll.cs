42c6e6db15dc5dc43580a07c40033d4d6f8ffa0f
Ayende Rahien <ayende@ayende.com>
2014-09-21 16:53:56 +0300

    Fixing rollback all (we already have a way to rollback sepcific tx)

--- Raven.Database/Server/Responders/TransactionsRollbackAll.cs
+++ Raven.Database/Server/Responders/TransactionsRollbackAll.cs
@@ -10,7 +10,7 @@ namespace Raven.Database.Server.Responders
     {
         public override string UrlPattern
         {
-            get { return "^/admin/transactions/rollback"; }
+            get { return "^/admin/transactions/rollbackAll"; }
         }
 
         public override string[] SupportedVerbs
@@ -20,26 +20,6 @@ namespace Raven.Database.Server.Responders
 
 	    public override void RespondToAdmin(IHttpContext context)
 	    {
-			var txId = context.Request.QueryString["tx"];
-		    if (string.IsNullOrEmpty(txId) == false)
-		    {
-			    Database.Rollback(txId);
-
-				context.WriteJson(new { RolledBackTransactionsAmount = 1});
-			    return;
-		    }
-
-		    var rollback = context.Request.QueryString["rollback"];
-		    if ("all".Equals(rollback, StringComparison.OrdinalIgnoreCase) == false)
-		    {
-				context.SetStatusToBadRequest(); 
-				context.WriteJson(new
-				{
-					Error = "The query string should contain ?id=tx-id or ?rollback=all"
-				});
-				return;
-		    }
-
 			var transactions = Database.TransactionalStorage.GetPreparedTransactions();
 			foreach (var transactionContextData in transactions)
 			{
