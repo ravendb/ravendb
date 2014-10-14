7f31c5bfd1fdef47c177e209d51791289904d2cb
Ayende Rahien <ayende@ayende.com>
2014-10-14 16:15:14 +0300

    Do not log if there isn't anything _to_ log

--- Raven.Database/Indexing/Index.cs
+++ Raven.Database/Indexing/Index.cs
@@ -1699,8 +1699,8 @@ namespace Raven.Database.Indexing
 						continue;
 					task.ReferencesToCheck[doc.Key] = Etag.InvalidEtag; // different etags, force a touch
 				}
-
-				logIndexing.Debug("Scheduled to touch documents: {0}", String.Join(";", task.ReferencesToCheck.Select(x => x.Key + ":" + x.Value)));
+				if (task.ReferencesToCheck.Count > 0)
+					logIndexing.Debug("Scheduled to touch documents: {0}", String.Join(";", task.ReferencesToCheck.Select(x => x.Key + ":" + x.Value)));
 			}
 			if (task.ReferencesToCheck.Count == 0)
 				return;
