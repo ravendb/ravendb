0e7afb951f579ffa2511d8bf63e66afdd2ac5521
Arkadiusz Palinski <arek@ais.pl>
2014-10-28 10:40:53 +0100

    Adding two passing tests that exposed RavenDB-2806 (already fixed). Changes we made then in order to repair that also fixes these two tests.

--- Raven.Voron/Voron.Tests/Backups/Incremental.cs
+++ Raven.Voron/Voron.Tests/Backups/Incremental.cs
@@ -275,6 +275,130 @@ namespace Voron.Tests.Backups
             }
         }
 
+		[Fact]
+		public void IncorrectWriteOfOverflowPagesFromJournalsToDataFile_RavenDB_2806()
+		{
+			RequireFileBasedPager();
+
+			const int testedOverflowSize = 20000;
+
+			var overflowValue = new byte[testedOverflowSize];
+			new Random(1).NextBytes(overflowValue);
+
+
+			using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
+			{
+				var tree = Env.CreateTree(tx, "test");
+
+				var itemBytes = new byte[16000];
+
+				new Random(2).NextBytes(itemBytes);
+				tree.Add("items/1", itemBytes);
+
+				new Random(3).NextBytes(itemBytes);
+				tree.Add("items/2", itemBytes);
+
+				tree.Delete("items/1");
+				tree.Delete("items/2");
+
+				tree.Add("items/3", overflowValue);
+
+				tx.Commit();
+			}
+
+			BackupMethods.Incremental.ToFile(Env, _incrementalBackupFile(0));
+
+			var options = StorageEnvironmentOptions.ForPath(_restoredStoragePath);
+			options.MaxLogFileSize = Env.Options.MaxLogFileSize;
+
+			BackupMethods.Incremental.Restore(options, new[]
+            {
+                _incrementalBackupFile(0)
+            });
+
+			using (var env = new StorageEnvironment(options))
+			{
+				using (var tx = env.NewTransaction(TransactionFlags.Read))
+				{
+					var tree = tx.ReadTree("test");
+
+					var readResult = tree.Read("items/3");
+
+					var readBytes = new byte[testedOverflowSize];
+
+					readResult.Reader.Read(readBytes, 0, testedOverflowSize);
+
+					Assert.Equal(overflowValue, readBytes);
+				}
+			}
+		}
+
+		[Fact]
+		public void IncorrectWriteOfOverflowPagesFromJournalsToDataFile_2_RavenDB_2806()
+		{
+			RequireFileBasedPager();
+
+			const int testedOverflowSize = 16000;
+
+			var overflowValue = new byte[testedOverflowSize];
+			new Random(1).NextBytes(overflowValue);
+
+
+			using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
+			{
+				var tree = Env.CreateTree(tx, "test");
+
+				var itemBytes = new byte[2000];
+
+				new Random(2).NextBytes(itemBytes);
+				tree.Add("items/1", itemBytes);
+
+
+				itemBytes = new byte[30000];
+				new Random(3).NextBytes(itemBytes);
+				tree.Add("items/2", itemBytes);
+
+				tx.Commit();
+			}
+
+
+			using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
+			{
+				var tree = Env.CreateTree(tx, "test");
+				tree.Delete("items/1");
+				tree.Delete("items/2");
+
+				tree.Add("items/3", overflowValue);
+
+				tx.Commit();
+			}
+
+			BackupMethods.Incremental.ToFile(Env, _incrementalBackupFile(0));
+
+			var options = StorageEnvironmentOptions.ForPath(_restoredStoragePath);
+			options.MaxLogFileSize = Env.Options.MaxLogFileSize;
+
+			BackupMethods.Incremental.Restore(options, new[]
+            {
+                _incrementalBackupFile(0)
+            });
+
+			using (var env = new StorageEnvironment(options))
+			{
+				using (var tx = env.NewTransaction(TransactionFlags.Read))
+				{
+					var tree = tx.ReadTree("test");
+
+					var readResult = tree.Read("items/3");
+
+					var readBytes = new byte[testedOverflowSize];
+
+					readResult.Reader.Read(readBytes, 0, testedOverflowSize);
+
+					Assert.Equal(overflowValue, readBytes);
+				}
+			}
+		}
 
 		private void Clean()
 		{
