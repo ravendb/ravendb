8aa1b5a13a86d47efa511004eb518ca7ee610ca8
Ayende Rahien <ayende@ayende.com>
2014-11-06 13:25:00 +0100

    Ensure that we always use the latest version

--- Raven.Database/Plugins/Builtins/CreateSilverlightIndexes.cs
+++ Raven.Database/Plugins/Builtins/CreateSilverlightIndexes.cs
@@ -1,5 +1,6 @@
 using Raven.Abstractions.Data;
 using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
 
 namespace Raven.Database.Plugins.Builtins
 {
@@ -7,28 +8,9 @@ namespace Raven.Database.Plugins.Builtins
 	{
 		public void SilverlightWasRequested(DocumentDatabase database)
 		{
-			if (database.Indexes.GetIndexDefinition(Constants.DocumentsByEntityNameIndex) == null)
-			{
-                database.Indexes.PutIndex(Constants.DocumentsByEntityNameIndex, new IndexDefinition
-				{
-					Map = @"from doc in docs 
-let Tag = doc[""@metadata""][""Raven-Entity-Name""]
-select new { Tag, LastModified = (DateTime)doc[""@metadata""][""Last-Modified""] , LastModifiedTicsk = (DateTime)doc[""@metadata""][""Last-Modified""].Ticks };",
-					Indexes =
-					{
-						{"Tag", FieldIndexing.NotAnalyzed},
-						{"LastModified", FieldIndexing.NotAnalyzed},
-                        {"LastModifiedTicks", FieldIndexing.NotAnalyzed}
-					},
-					SortOptions =
-                    {
-                        {"LastModified",SortOptions.String},
-                        {"LastModifiedTicks", SortOptions.Long}
-                    },
-
-					DisableInMemoryIndexing = true
-				});
-			}
+			var ravenDocumentsByEntityName = new RavenDocumentsByEntityName {};
+			database.Indexes.PutIndex(Constants.DocumentsByEntityNameIndex,
+				ravenDocumentsByEntityName.CreateIndexDefinition());
 		}
 	}
 }
