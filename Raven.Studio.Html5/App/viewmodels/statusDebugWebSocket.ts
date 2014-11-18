5e738e82da6974265a5d0966440c9f5e43104b54
Marcin Lewandowski <marcin@ais.pl>
2014-11-18 07:48:15 +0100

    RavenDB-2925 Show the reason why we can't use WebSocket

--- Raven.Studio.Html5/App/viewmodels/statusDebugWebSocket.ts
+++ Raven.Studio.Html5/App/viewmodels/statusDebugWebSocket.ts
@@ -3,31 +3,57 @@ import database = require("models/database");
 import viewModelBase = require("viewmodels/viewModelBase");
 import getSingleAuthTokenCommand = require("commands/getSingleAuthTokenCommand");
 
-import getValidateWebSocketParamsCommand = require("commands/getValidateWebSocketParamsCommand");
-
 class statusDebugWebSocket extends viewModelBase {
 
     results = ko.observable<string>();
 
     test() {
-        var getTokenTask = new getSingleAuthTokenCommand(this.activeDatabase()).execute();
-        getTokenTask
-            .done((tokenObject: singleAuthToken) => {
-                var token = tokenObject.Token;
-                var connectionString = 'token=' + token + '&id=test&coolDownWithDataLoss=1000';
-                new getValidateWebSocketParamsCommand(this.activeDatabase(), connectionString).execute()
-                    .done((result) => {
-                        this.results("OK: " + result.Message);
-                    })
-                    .fail((result) => {
-                        this.results('Failure: ' + result.responseJSON.Error);
-                    });
-            })
-            .fail((e) => {
-                this.results("Unable to fetch single auth token:" + e.responseJSON);
-            });
+        if ("WebSocket" in window) {
+            var getTokenTask = new getSingleAuthTokenCommand(this.activeDatabase()).execute();
 
-        this.results("xx");
+            getTokenTask
+                .done((tokenObject: singleAuthToken) => {
+                    var token = tokenObject.Token;
+                    var connectionString = 'singleUseAuthToken=' + token + '&id=test&coolDownWithDataLoss=1000&isMultyTenantTransport=false';
+                    this.connectWebSocket(connectionString);
+                })
+                .fail((e) => {
+                    if (e.status == 0) {
+                        this.results("Connection has closed");
+                    }
+                    else if (e.status != 403) { // authorized connection
+                        this.results(e.responseJSON.Error);
+                    }
+                });
+        } else {
+            this.results("Looks like your browser doesn't support web sockets");
+        }
+    }
+
+
+    private connectWebSocket(connectionString: string) {
+        var connectionOpened: boolean = false;
+
+        var wsProtocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
+        var url = wsProtocol + window.location.host + appUrl.forResourceQuery(this.activeDatabase()) + '/websocket/validate?' + connectionString;
+        var webSocket = new WebSocket(url);
+
+        webSocket.onerror = (e) => {
+            if (connectionOpened == false) {
+                this.results("Server doesn't support web sockets protocol");
+            } else {
+                //TODO:
+                console.log(e);
+                //this.onError(e);
+            }
+        };
+        webSocket.onclose = (e: CloseEvent) => {
+            console.log("on close");
+            console.log(e);
+        }
+        webSocket.onopen = () => {
+            connectionOpened = true;
+        }
     }
 }
 
