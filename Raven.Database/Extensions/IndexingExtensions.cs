ravendb/ravendb

commit d8eaa57ccbf1d96c0ed8b1004daa6510dd27bddf

fix bug 1934 with null value problem in sorting

Raven.Database/Extensions/IndexingExtensions.cs
@@ -169,10 +169,13 @@ namespace Raven.Database.Extensions
 									return new SortField(Constants.DistanceFieldName, dsort, sortedField.Descending);
 								}
 								var sortOptions = GetSortOption(indexDefinition, sortedField.Field, self);
+                                
 								if (sortOptions == null || sortOptions == SortOptions.None)
 									return new SortField(sortedField.Field, CultureInfo.InvariantCulture, sortedField.Descending);
-
-								return new SortField(sortedField.Field, (int)sortOptions.Value, sortedField.Descending);
+							    
+                                if (sortOptions.Value == SortOptions.Short)
+							        sortOptions = SortOptions.Int;
+							    return new SortField(sortedField.Field, (int)sortOptions.Value, sortedField.Descending);
 
 							})
 							.ToArray());

Raven.Client.Lightweight/Document/DocumentConvention.cs
@@ -615,8 +615,9 @@ namespace Raven.Client.Document
 				case "String":
 					return SortOptions.String;
 				default:
-					return customDefaultSortOptions.ContainsKey(typeName)
-						       ? customDefaultSortOptions[typeName]
+			        SortOptions value;
+			        return customDefaultSortOptions.TryGetValue(typeName, out value)
+						       ? value
 						       : SortOptions.String;
 			}
 		}
@@ -631,7 +632,7 @@ namespace Raven.Client.Document
 				type = nonNullable;
 
 			if (type == typeof (int) || type == typeof (long) || type == typeof (double) || type == typeof (float) ||
-			    type == typeof (decimal) || type == typeof (TimeSpan))
+			    type == typeof (decimal) || type == typeof (TimeSpan) || type == typeof(short))
 				return true;
 
 			return customRangeTypes.Contains(type);

Raven.Client.Lightweight/Document/SessionOperations/QueryOperation.cs
@@ -298,9 +298,10 @@ namespace Raven.Client.Document.SessionOperations
 				if (sortByHint.Value == null)
 					continue;
 
-				setOperationHeaders(
+			    var nonNullableType = (Nullable.GetUnderlyingType(sortByHint.Value) ?? sortByHint.Value);
+			    setOperationHeaders(
 					string.Format("SortHint-{0}", Uri.EscapeDataString(sortByHint.Key.Trim('-'))),
-					sessionOperations.Conventions.GetDefaultSortOption(sortByHint.Value.Name).ToString());
+                    sessionOperations.Conventions.GetDefaultSortOption(nonNullableType.Name).ToString());
 			}
 		}
 	}

Raven.Database/Indexing/AnonymousObjectToLuceneDocumentConverter.cs
@@ -97,7 +97,6 @@ namespace Raven.Database.Indexing
 
 		private IEnumerable<AbstractField> CreateRegularFields(string name, object value, Field.Store defaultStorage, bool nestedArray = false, Field.TermVector defaultTermVector = Field.TermVector.NO, Field.Index? analyzed = null)
 		{
-
             var fieldIndexingOptions = analyzed ?? indexDefinition.GetIndex(name, null);
 			var storage = indexDefinition.GetStorage(name, defaultStorage);
 			var termVector = indexDefinition.GetTermVector(name, defaultTermVector);
@@ -111,7 +110,6 @@ namespace Raven.Database.Indexing
 			{
 				fieldIndexingOptions = Field.Index.ANALYZED; // we have some sort of term vector, forcing index to be analyzed, then.
 			}
-
 
 			if (value == null)
 			{
@@ -165,17 +163,13 @@ namespace Raven.Database.Indexing
 				if (dynamicNullObject.IsExplicitNull)
 				{
 					var sortOptions = indexDefinition.GetSortOption(name, query: null);
-					if (sortOptions != null && 
-                        sortOptions.Value != SortOptions.None && 
-                        sortOptions.Value != SortOptions.String && 
-                        sortOptions.Value != SortOptions.StringVal && 
-                        sortOptions.Value != SortOptions.Custom)
-					{
-						yield break; // we don't emit null for sorting	
-					}
 					yield return CreateFieldWithCaching(name, Constants.NullValue, storage,
 														Field.Index.NOT_ANALYZED_NO_NORMS, Field.TermVector.NO);
-				}
+
+				    foreach (var field in CreateNumericFieldWithCaching(name, GetNullValueForSorting(sortOptions), storage, termVector))
+                        yield return field;
+                   
+                 }
 				yield break;
 			}
 			var boostedValue = value as BoostedValue;
@@ -330,7 +324,34 @@ namespace Raven.Database.Indexing
 				yield return numericField;
 		}
 
-		private IEnumerable<AbstractField> CreateNumericFieldWithCaching(string name, object value,
+	    private static object GetNullValueForSorting(SortOptions? sortOptions)
+	    {
+	        switch (sortOptions)
+	        {
+	            case SortOptions.Short:
+	            case SortOptions.Int:
+	                return int.MinValue;
+	            case SortOptions.Double:
+	                return double.MinValue;
+	                break;
+	            case SortOptions.Float:
+	                return float.MinValue;
+
+// ReSharper disable RedundantCaseLabel
+	            case SortOptions.Long:
+
+	            // to be able to sort on timestamps
+	            case SortOptions.String:
+	            case SortOptions.StringVal:
+	            case SortOptions.None:
+	            case SortOptions.Custom:
+// ReSharper restore RedundantCaseLabel
+	            default:
+	                return long.MinValue;
+	        }
+	    }
+
+	    private IEnumerable<AbstractField> CreateNumericFieldWithCaching(string name, object value,
 			Field.Store defaultStorage, Field.TermVector termVector)
 		{
 
@@ -346,20 +367,19 @@ namespace Raven.Database.Indexing
 			if (value is TimeSpan)
 			{
 				yield return numericField.SetLongValue(((TimeSpan)value).Ticks);
-
 			}
-			else if (value is int)
-			{
-				if (indexDefinition.GetSortOption(name, query: null) == SortOptions.Long)
-					yield return numericField.SetLongValue((int)value);
-				else if (indexDefinition.GetSortOption(name, query: null) == SortOptions.Float)
+            else if (value is int)
+            {
+                if (indexDefinition.GetSortOption(name, query: null) == SortOptions.Long)
+                    yield return numericField.SetLongValue((int)value);
+                else if (indexDefinition.GetSortOption(name, query: null) == SortOptions.Float)
                     yield return numericField.SetFloatValue((int)value);
-				else if (indexDefinition.GetSortOption(name, query: null) == SortOptions.Double)
-					yield return numericField.SetDoubleValue((int)value);
-				else
-					yield return numericField.SetIntValue((int)value);
-			}
-			else if (value is long)
+                else if (indexDefinition.GetSortOption(name, query: null) == SortOptions.Double)
+                    yield return numericField.SetDoubleValue((int)value);
+                else
+                    yield return numericField.SetIntValue((int)value);
+            }
+            else if (value is long)
 			{
 				if (indexDefinition.GetSortOption(name, query: null) == SortOptions.Double)
 					yield return numericField.SetDoubleValue((long)value);

Raven.Tests/Issues/RavenDb-1934.cs
@@ -1,0 +1,467 @@
+﻿using Raven.Client;
+using Raven.Client.Indexes;
+using Raven.Tests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Raven.Tests.Issues
+{
+    
+        public class RavenDb_1934 : RavenTestBase
+        {
+            public class Foo
+            {
+                public string Id { get; set; }
+                public int? Age { get; set; }
+                public short? TestShort { get; set; }
+                public float? Grade { get; set; }
+                public double? Price { get; set; }
+                public string Name { get; set; }
+                public TimeSpan? Start { get; set; }
+                public TimeSpan Until { get; set; }
+            }
+
+            public class Bar
+            {
+                public string Id { get; set; }
+                public string SomeData { get; set; }
+            }
+
+            [Fact]
+            public void TimeSpan_Can_Get_Range_Under_A_Day()
+            {
+                using (var documentStore = NewDocumentStore())
+                {
+                    using (var session = documentStore.OpenSession())
+                    {
+                        session.Store(new Foo { Start = TimeSpan.FromHours(10), Until = TimeSpan.FromHours(20) });
+
+                        session.SaveChanges();
+                    }
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var time = TimeSpan.FromHours(15);
+                        var result = session.Query<Foo>()
+                                            .Customize(x => x.WaitForNonStaleResults())
+                                            .SingleOrDefault(x => x.Start <= time && x.Until > time);
+
+                        Assert.NotNull(result);
+                    }
+                }
+            }
+
+            [Fact]
+            public void TimeSpan_Can_Get_Range_Over_A_Day()
+            {
+                using (var documentStore = NewDocumentStore())
+                {
+                    using (var session = documentStore.OpenSession())
+                    {
+                        session.Store(new Foo { Start = TimeSpan.FromHours(30), Until = TimeSpan.FromHours(40) });
+
+                        session.SaveChanges();
+                    }
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var time = TimeSpan.FromHours(35);
+                        var result = session.Query<Foo>()
+                                            .Customize(x => x.WaitForNonStaleResults())
+                                            .SingleOrDefault(x => x.Start <= time && x.Until > time);
+
+                        Assert.NotNull(result);
+                    }
+                }
+            }
+
+            [Fact]
+            public void TimeSpan_Can_Get_Range_Mixed_Days()
+            {
+                using (var documentStore = NewDocumentStore())
+                {
+                    using (var session = documentStore.OpenSession())
+                    {
+                        session.Store(new Foo { Start = TimeSpan.FromHours(20), Until = TimeSpan.FromHours(30) });
+
+                        session.SaveChanges();
+                    }
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var time = TimeSpan.FromHours(25);
+                        var result = session.Query<Foo>()
+                                            .Customize(x => x.WaitForNonStaleResults())
+                                            .SingleOrDefault(x => x.Start <= time && x.Until > time);
+
+                        Assert.NotNull(result);
+                    }
+                }
+            }
+
+            [Fact]
+            public void TimeSpan_Can_Get_Range_VeryLarge()
+            {
+                using (var documentStore = NewDocumentStore())
+                {
+                    using (var session = documentStore.OpenSession())
+                    {
+                        session.Store(new Foo { Start = TimeSpan.FromHours(10), Until = TimeSpan.FromDays(100) });
+
+                        session.SaveChanges();
+                    }
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var time = TimeSpan.FromDays(2);
+                        var result = session.Query<Foo>()
+                                            .Customize(x => x.WaitForNonStaleResults())
+                                            .SingleOrDefault(x => x.Start <= time && x.Until > time);
+
+                        Assert.NotNull(result);
+                    }
+                }
+            }
+
+            [Fact]
+            public void TimeSpan_Can_Get_Range_Negatives()
+            {
+                using (var documentStore = NewDocumentStore())
+                {
+                    using (var session = documentStore.OpenSession())
+                    {
+                        session.Store(new Foo { Start = TimeSpan.FromHours(-10), Until = TimeSpan.FromHours(10) });
+
+                        session.SaveChanges();
+                    }
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var time = TimeSpan.FromHours(1);
+                        var result = session.Query<Foo>()
+                                            .Customize(x => x.WaitForNonStaleResults())
+                                            .SingleOrDefault(x => x.Start <= time && x.Until > time);
+
+                        Assert.NotNull(result);
+                    }
+                }
+            }
+
+            [Fact]
+            public void Can_Sort_On_TimeSpans()
+            {
+                using (var documentStore = NewDocumentStore())
+                {
+                    using (var session = documentStore.OpenSession())
+                    {
+                        session.Store(new Foo { Id = "1", Start = TimeSpan.FromSeconds(10) });
+                        session.Store(new Foo { Id = "2", Start = TimeSpan.FromSeconds(20) });
+                        session.Store(new Foo { Id = "3", Start = TimeSpan.FromSeconds(15) });
+
+                        session.SaveChanges();
+                    }
+
+                    WaitForIndexing(documentStore);
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var results = session.Query<Foo>()
+                            .OrderBy(x => x.Start)
+                            .ToArray();
+
+                        Assert.Equal("1", results[0].Id);
+                        Assert.Equal("3", results[1].Id);
+                        Assert.Equal("2", results[2].Id);
+                    }
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var results = session.Query<Foo>()
+                            .OrderByDescending(x => x.Start)
+                            .ToArray();
+
+                        Assert.Equal("1", results[2].Id);
+                        Assert.Equal("3", results[1].Id);
+                        Assert.Equal("2", results[0].Id);
+                    }
+                }
+            }
+
+            [Fact]
+            public void Can_Sort_On_TimeSpans_With_Nulls()
+            {
+                using (var documentStore = NewDocumentStore())
+                {
+                    using (var session = documentStore.OpenSession())
+                    {
+                        session.Store(new Foo { Id = "1", Start = TimeSpan.FromSeconds(10) });
+                        session.Store(new Foo { Id = "2", Start = TimeSpan.FromSeconds(20) });
+                        session.Store(new Foo { Id = "3", Start = TimeSpan.FromSeconds(15) });
+                        session.Store(new Foo { Id = "4" ,Start =null });
+  
+                        session.SaveChanges();
+                    }
+
+                    WaitForIndexing(documentStore);
+
+                    using (var session = documentStore.OpenSession())
+                    {
+
+                        var results = session.Query<Foo>()
+                               .OrderBy(x => x.Start)
+                               .ToArray();
+
+
+                        Assert.Equal("4", results[0].Id);
+                        Assert.Equal("1", results[1].Id);
+                        Assert.Equal("3", results[2].Id);
+                        Assert.Equal("2", results[3].Id);
+                    }
+                }
+            }
+
+
+            [Fact]
+            public void Can_Sort_On_Ints_With_Nulls()
+            {
+                using (var documentStore = NewDocumentStore())
+                {
+                    using (var session = documentStore.OpenSession())
+                    {
+                        session.Store(new Foo { Id = "1", Age = 10 });
+                        session.Store(new Foo { Id = "2", Age = 20 });
+                        session.Store(new Foo { Id = "3", Age = 15 });
+                        session.Store(new Foo { Id = "4", Age=null });
+
+                        session.SaveChanges();
+                    }
+
+                    WaitForIndexing(documentStore);
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var results = session.Query<Foo>()
+                            .Customize(x=>x.WaitForNonStaleResults())
+                            .OrderBy(x => x.Age)
+                            .ToArray();
+
+
+                        Assert.Equal("4", results[0].Id);
+                        Assert.Equal("1", results[1].Id);
+                        Assert.Equal("3", results[2].Id);
+                        Assert.Equal("2", results[3].Id);
+                    }
+
+
+                }
+            }
+            [Fact]
+            public void Can_Sort_On_Short_With_Nulls()
+            {
+                using (var documentStore = NewDocumentStore())
+                {
+                    using (var session = documentStore.OpenSession())
+                    {
+                        session.Store(new Foo { Id = "1", TestShort = 10 });
+                        session.Store(new Foo { Id = "2", TestShort = 20 });
+                        session.Store(new Foo { Id = "3", TestShort = 15 });
+                        session.Store(new Foo { Id = "4", TestShort = null });
+
+                        session.SaveChanges();
+                    }
+
+                    WaitForIndexing(documentStore);
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var results = session.Query<Foo>()
+                            .Customize(x => x.WaitForNonStaleResults())
+                            .OrderBy(x => x.TestShort)
+                            .ToArray();
+
+
+                        Assert.Equal("4", results[0].Id);
+                        Assert.Equal("1", results[1].Id);
+                        Assert.Equal("3", results[2].Id);
+                        Assert.Equal("2", results[3].Id);
+                    }
+                }
+            }
+            [Fact]
+            public void Can_Sort_On_Float_With_Nulls()
+            {
+                using (var documentStore = NewDocumentStore())
+                {
+                    using (var session = documentStore.OpenSession())
+                    {
+                        session.Store(new Foo { Id = "1", Grade = (float?)10.1 });
+                        session.Store(new Foo { Id = "2", Grade = (float?)20.4 });
+                        session.Store(new Foo { Id = "3", Grade = (float?)15.5 });
+                        session.Store(new Foo { Id = "4", Grade = null });
+
+                        session.SaveChanges();
+                    }
+
+                    WaitForIndexing(documentStore);
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var results = session.Query<Foo>()
+                            .Customize(x => x.WaitForNonStaleResults())
+                            .OrderBy(x => x.Grade)
+                            .ToArray();
+
+
+                        Assert.Equal("4", results[0].Id);
+                        Assert.Equal("1", results[1].Id);
+                        Assert.Equal("3", results[2].Id);
+                        Assert.Equal("2", results[3].Id);
+                    }
+
+
+                }
+            }
+
+            [Fact]
+            public void Can_Sort_On_Double_With_Nulls()
+            {
+                using (var documentStore = NewDocumentStore())
+                {
+                    using (var session = documentStore.OpenSession())
+                    {
+                        session.Store(new Foo { Id = "1", Price =10.1 });
+                        session.Store(new Foo { Id = "2", Price = 20.4 });
+                        session.Store(new Foo { Id = "3", Price = 15.5 });
+                        session.Store(new Foo { Id = "4", Price = null });
+
+                        session.SaveChanges();
+                    }
+
+                    WaitForIndexing(documentStore);
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var results = session.Query<Foo>()
+                            .Customize(x => x.WaitForNonStaleResults())
+                            .OrderBy(x => x.Price)
+                            .ToArray();
+
+
+                        Assert.Equal("4", results[0].Id);
+                        Assert.Equal("1", results[1].Id);
+                        Assert.Equal("3", results[2].Id);
+                        Assert.Equal("2", results[3].Id);
+                    }
+
+
+                }
+            }
+
+            [Fact]
+            public void Can_Sort_On_String_With_Nulls()
+            {
+                using (var documentStore = NewDocumentStore())
+                {
+                    using (var session = documentStore.OpenSession())
+                    {
+                        session.Store(new Foo { Id = "1", Name = "aaa" });
+                        session.Store(new Foo { Id = "2", Name = "cc" });
+                        session.Store(new Foo { Id = "3", Name = "b1" });
+                        session.Store(new Foo { Id = "4", Name = null });
+
+                        session.SaveChanges();
+                    }
+
+                    WaitForIndexing(documentStore);
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var results = session.Query<Foo>()
+                            .Customize(x => x.WaitForNonStaleResults())
+                            .OrderBy(x => x.Name)
+                            .ToArray();
+
+
+                        Assert.Equal("4", results[0].Id);
+                        Assert.Equal("1", results[1].Id);
+                        Assert.Equal("3", results[2].Id);
+                        Assert.Equal("2", results[3].Id);
+                    }
+
+
+                }
+            }
+
+           
+
+            [Fact]
+            public void Can_Sort_On_TimeSpans_With_Nulls_Using_MultiMap_Idx()
+            {
+                using (var documentStore = NewDocumentStore())
+                {
+                    new TimeSpanTestMultiMapIndex().Execute(documentStore);
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        session.Store(new Foo { Id = "1", Start = TimeSpan.FromSeconds(10) });
+                        session.Store(new Foo { Id = "2", Start = TimeSpan.FromSeconds(20) });
+                        session.Store(new Foo { Id = "3", Start = TimeSpan.FromSeconds(15) });
+                        session.Store(new Bar { Id = "4" });
+
+                        session.SaveChanges();
+                    }
+
+                    WaitForIndexing(documentStore);
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var results = session.Query<Foo, TimeSpanTestMultiMapIndex>()
+                            .OrderBy(x => x.Start)
+                            .AsProjection<Foo>()
+                            .ToArray();
+
+                        Assert.Equal("4", results[0].Id);
+                        Assert.Equal("1", results[1].Id);
+                        Assert.Equal("3", results[2].Id);
+                        Assert.Equal("2", results[3].Id);
+                    }
+
+                    using (var session = documentStore.OpenSession())
+                    {
+                        var results = session.Query<Foo, TimeSpanTestMultiMapIndex>()
+                            .OrderByDescending(x => x.Start)
+                            .AsProjection<Foo>()
+                            .ToArray();
+
+                        Assert.Equal("4", results[3].Id);
+                        Assert.Equal("1", results[2].Id);
+                        Assert.Equal("3", results[1].Id);
+                        Assert.Equal("2", results[0].Id);
+                    }
+                }
+            }
+
+            protected class TimeSpanTestMultiMapIndex : AbstractMultiMapIndexCreationTask<Foo>
+            {
+                public TimeSpanTestMultiMapIndex()
+                {
+                    AddMap<Foo>(docs => from d in docs
+                                        select new Foo
+                                        {
+                                            Start = d.Start,
+                                        });
+
+                    AddMap<Bar>(docs => from d in docs
+                                        select new Foo
+                                        {
+                                            Start = null,
+                                        });
+                }
+            }
+        } 
+    }
+

Raven.Tests/Raven.Tests.csproj
@@ -309,6 +309,7 @@
     <Compile Include="Issues\1379\RavenDB_1379_Client_Remote.cs" />
     <Compile Include="Issues\1379\RavenDB_1379_Client.cs" />
     <Compile Include="Issues\RavenDB-1847.cs" />
+     <Compile Include="Issues\RavenD-1934.cs" />
     <Compile Include="Issues\RavenDB_1538.cs" />
     <Compile Include="Issues\RavenDB_1297 .cs" />
     <Compile Include="Issues\RavenDB_1285.cs" />

commit f245cc6fac2f129e0617ee439c1e0b27d5dcb65a

Lucene.Net.Util.Version does not necessarily exist in the analyzer type assembly, meaning we are unable to get the correct constructor for analyzer. Bug was introduced in commit d8eaa57c

Raven.Database/Extensions/IndexingExtensions.cs
@@ -35,8 +35,7 @@ namespace Raven.Database.Extensions
 				if (ctors != null)
 					return (Analyzer)Activator.CreateInstance(assembly.FullName, analyzerType.FullName).Unwrap();
 
-				var type = analyzerType.Assembly.GetType(typeof(Lucene.Net.Util.Version).FullName);
-				ctors = analyzerType.GetConstructor(new[] { type });
+                ctors = analyzerType.GetConstructor(new[] { typeof(Lucene.Net.Util.Version) });
 				if (ctors != null) 
 					return (Analyzer)Activator
 						.CreateInstance(assembly.FullName, analyzerType.FullName, false, BindingFlags.CreateInstance | BindingFlags.Public | BindingFlags.Instance, null, new object[] { Lucene.Net.Util.Version.LUCENE_30 }, CultureInfo.InvariantCulture, null)