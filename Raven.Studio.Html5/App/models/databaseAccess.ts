27c2939940e128314a227255c4f2526c60ff09e3
Arkadiusz Palinski <arek@ais.pl>
2014-11-25 08:42:45 +0100

    Allow to specify API keys to file system from the studio

--- Raven.Studio.Html5/App/models/databaseAccess.ts
+++ Raven.Studio.Html5/App/models/databaseAccess.ts
@@ -1,5 +1,6 @@
 import shell = require("viewmodels/shell");
 import database = require("models/database");
+import filesystem = require("models/filesystem/filesystem");
 
 class databaseAccess {
 
@@ -7,6 +8,7 @@ class databaseAccess {
     tenantId = ko.observable<string>();
     readOnly = ko.observable<boolean>();
     databaseNames: KnockoutComputed<string[]>;
+    fileSystemNames: KnockoutComputed<string[]>;
     searchResults: KnockoutComputed<string[]>;
     tenantCustomValidityError: KnockoutComputed<string>;
 
@@ -50,20 +52,25 @@ class databaseAccess {
         this.readOnly(dto.ReadOnly);
         this.tenantId(dto.TenantId != null ? dto.TenantId : '');
 
-        this.databaseNames = ko.computed(() => shell.databases().map((db: database) => db.name).concat("*"));
+        this.databaseNames = ko.computed(() => shell.databases().map((db: database) => db.name));
+        this.fileSystemNames = ko.computed(() => shell.fileSystems().map((fs: filesystem) => fs.name));
 
         this.searchResults = ko.computed(() => {
             var newDatabaseName: string = this.tenantId();
-            return this.databaseNames().filter((name) => name.toLowerCase().indexOf(newDatabaseName.toLowerCase()) > -1);
+
+            var dbNames = this.databaseNames().filter((name) => name.toLowerCase().indexOf(newDatabaseName.toLowerCase()) > -1);
+            var fsNames = this.fileSystemNames().filter((name) => name.toLowerCase().indexOf(newDatabaseName.toLowerCase()) > -1);
+            return dbNames.concat(fsNames).concat("*");
         });
 
         this.tenantCustomValidityError = ko.computed(() => {
             var errorMessage: string = '';
             var newTenantId = this.tenantId();
             var foundDb = this.databaseNames().first(name => newTenantId == name);
+            var foundFs = this.fileSystemNames().first(name => newTenantId == name);
 
-            if (!foundDb && newTenantId.length > 0) {
-                errorMessage = "Database name doesn't exist!";
+            if (newTenantId != "*" && !foundDb && !foundFs && newTenantId.length > 0) {
+                errorMessage = "There is no database nor file system with such name!";
             }
 
             return errorMessage;
