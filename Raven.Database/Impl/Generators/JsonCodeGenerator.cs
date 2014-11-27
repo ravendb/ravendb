37fdb411e343fe7346c0333de770f56968c1bd7f
Federico Andres Lois <federico.lois@corvalius.com>
2014-11-27 17:18:27 -0300

    Layout for the JsonCodeGenerator.

---
+++ Raven.Database/Impl/Generators/JsonCodeGenerator.cs
@@ -0,0 +1,34 @@
+﻿using Raven.Abstractions.Data;
+using Raven.Abstractions.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.Database.Impl.Generators
+{
+    public class JsonCodeGenerator
+    {
+        private string language;
+
+        public JsonCodeGenerator( string lang)
+        {
+            if (string.IsNullOrWhiteSpace(lang))
+                throw new ArgumentNullException("lang");
+
+            this.language = lang;
+        }
+
+        public string Execute(JsonDocument document)
+        {
+            if (document == null)
+                throw new ArgumentNullException("document");
+
+            dynamic d = JsonExtensions.JsonDeserialization<dynamic>(document.DataAsJson);
+            
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Raven.Tests/Json/JsonCodeGeneratorTests.cs
@@ -0,0 +1,34 @@
+﻿using Raven.Abstractions.Data;
+using Raven.Abstractions.Extensions;
+using Raven.Database.Impl.Generators;
+using Raven.Tests.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Raven.Tests.Json
+{
+    public class JsonCodeGeneratorTests : RavenTest
+    {
+        public class Blog
+        {
+            public string Title { get; set; }
+
+            public string Category { get; set; }
+        }
+
+        [Fact]
+        public void SimpleObjectWithStrings ()
+        {
+            var blog = new Blog();
+            var document = new JsonDocument {  DataAsJson = JsonExtensions.ToJObject(blog) };
+
+            var generator = new JsonCodeGenerator("csharp");
+            var result = generator.Execute(document);
+        }
+    }
+}
