1d37b41dbb01a78a78f177de4ef94dfc416faffc
Ayende Rahien <ayende@ayende.com>
2014-12-01 15:06:41 +0200

    Moving more code to timer manager. Removing backgroundExecuter.Repeat, because it wasn't used.

--- Raven.Database/Indexing/DefaultBackgroundTaskExecuter.cs
+++ Raven.Database/Indexing/DefaultBackgroundTaskExecuter.cs
@@ -31,57 +31,6 @@ namespace Raven.Database.Indexing
 				.ToList();
 		}
 
-		private readonly AtomicDictionary<Tuple<Timer, ConcurrentSet<IRepeatedAction>>> timers =
-			new AtomicDictionary<Tuple<Timer, ConcurrentSet<IRepeatedAction>>>();
-
-		public void Repeat(IRepeatedAction action)
-		{
-			var tuple = timers.GetOrAdd(action.RepeatDuration.ToString(),
-												  span =>
-												  {
-													  var repeatedActions = new ConcurrentSet<IRepeatedAction>
-			                                      	{
-			                                      		action
-			                                      	};
-													  var timer = new Timer(ExecuteTimer, action.RepeatDuration,
-																			action.RepeatDuration,
-																			action.RepeatDuration);
-													  return Tuple.Create(timer, repeatedActions);
-												  });
-			tuple.Item2.TryAdd(action);
-		}
-
-		private void ExecuteTimer(object state)
-		{
-			var span = state.ToString();
-			Tuple<Timer, ConcurrentSet<IRepeatedAction>> tuple;
-			if (timers.TryGetValue(span, out tuple) == false)
-				return;
-
-			foreach (var repeatedAction in tuple.Item2)
-			{
-				if (repeatedAction.IsValid == false)
-					tuple.Item2.TryRemove(repeatedAction);
-
-				try
-				{
-					repeatedAction.Execute();
-				}
-				catch (Exception e)
-				{
-					logger.ErrorException("Could not execute repeated task", e);
-				}
-			}
-
-			if (tuple.Item2.Count != 0)
-				return;
-
-			if (timers.TryRemove(span, out tuple) == false)
-				return;
-
-			tuple.Item1.Dispose();
-		}
-
 		/// <summary>
 		/// Note that here we assume that  source may be very large (number of documents)
 		/// </summary>
