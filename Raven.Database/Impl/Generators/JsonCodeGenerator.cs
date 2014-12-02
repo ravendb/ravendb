d8d2497c8f5471b46e6026ffcc33bb1eca29a350
Federico Andres Lois <federico.lois@corvalius.com>
2014-12-02 14:41:32 -0300

    Handling some types.

--- Raven.Database/Impl/Generators/JsonCodeGenerator.cs
+++ Raven.Database/Impl/Generators/JsonCodeGenerator.cs
@@ -1,7 +1,10 @@
 ﻿using Raven.Abstractions.Data;
 using Raven.Abstractions.Extensions;
+using Raven.Imports.Newtonsoft.Json.Linq;
+using Raven.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -13,7 +16,50 @@ namespace Raven.Database.Impl.Generators
     {
         private string language;
 
-        public JsonCodeGenerator( string lang)
+        internal class FieldType
+        {
+            public readonly string Name;
+            public readonly bool IsPrimitive;
+            public readonly bool IsArray;
+
+            public FieldType(Type type, bool isArray = false)
+            {
+                if (type == typeof(long))
+                    this.Name = "long";
+                else if (type == typeof(int))
+                    this.Name = "int";
+                else if (type == typeof(string))
+                    this.Name = "string";
+                else
+                    this.Name = type.Name;
+
+                this.IsPrimitive = true;
+                this.IsArray = isArray;
+            }
+
+            public FieldType(string type, bool isArray = false)
+            {
+                this.Name = type;
+                this.IsPrimitive = false;
+                this.IsArray = isArray;
+            }
+        }
+
+        internal class ClassType : FieldType
+        {
+            private static IDictionary<string, FieldType> NoProperties = new Dictionary<string, FieldType>();
+
+            public readonly IDictionary<string, FieldType> Properties;
+            
+            public ClassType( string name, IDictionary<string, FieldType> properties = null ) : base ( name, false )
+            {
+                this.Properties = properties;
+                if (this.Properties == null)
+                    this.Properties = NoProperties;
+            }
+        }
+
+        public JsonCodeGenerator(string lang)
         {
             if (string.IsNullOrWhiteSpace(lang))
                 throw new ArgumentNullException("lang");
@@ -26,9 +72,156 @@ namespace Raven.Database.Impl.Generators
             if (document == null)
                 throw new ArgumentNullException("document");
 
-            dynamic d = JsonExtensions.JsonDeserialization<dynamic>(document.DataAsJson);
-            
+            var classes = GenerateClassTypesFromObject("Class", document.DataAsJson);
+
+            var list = classes.ToList();
+
             throw new NotImplementedException();
         }
+
+        internal IEnumerable<ClassType> GenerateClassTypesFromObject(string name, RavenJObject @object)
+        {
+            var fields = new Dictionary<string, FieldType>();
+
+            foreach (var pair in @object)
+            {
+                switch (pair.Value.Type)
+                {
+                    // Inner objects.
+                    case JTokenType.Object:
+                        var type = GenerateClassTypesFromObject(pair.Key + "Class", (RavenJObject)pair.Value).Single();                        
+                        fields[pair.Key] = new FieldType( type.Name, type.IsArray );
+                        yield return type;
+                        break;
+                    case JTokenType.Array:
+                        var guessedType = GuessTokenTypeFromArray(pair.Key, (RavenJArray)(pair.Value));
+                        if (guessedType is ClassType)
+                        {
+                            // We will defer the analysis and create a new name;
+                            fields[pair.Key] = new FieldType(guessedType.Name, true);
+
+                            // Add the token to be inspected individually.                            
+                            yield return (ClassType)guessedType;
+                        }
+                        else
+                        {
+                            fields[pair.Key] = new FieldType(guessedType.Name, true);
+                        }
+                        break;
+
+                    default:
+                        fields[pair.Key] = GetTokenTypeFromPrimitiveType(pair.Value);
+                        break;
+                }
+            }  
+
+            // Check if we can get the name from the metadata. 
+            yield return new ClassType(name, fields);
+        }
+
+        private FieldType GuessTokenTypeFromArray(string name, RavenJArray array)
+        {
+            var token = array.First();
+
+            switch (token.Type)
+            {
+                case JTokenType.Object:
+                    return GenerateClassTypesFromObject(name, (RavenJObject)token).First();
+                    break;
+                case JTokenType.Array:
+                    return GuessTokenTypeFromArray(name, (RavenJArray)token);
+                    break;
+                default:
+                    return GetTokenTypeFromPrimitiveType(token);
+                    break;
+            }
+
+            throw new NotSupportedException("We shouldn't have hit this.");
+        }
+
+        private FieldType GetTokenTypeFromPrimitiveType(RavenJToken token)
+        {
+            switch (token.Type)
+            {
+                // Base types.
+                case JTokenType.Boolean: return new FieldType(typeof(bool)); break;
+                case JTokenType.Bytes: return new FieldType(typeof(byte[])); break;
+                case JTokenType.Date: return new FieldType(typeof(DateTimeOffset)); break;
+
+                case JTokenType.Guid: return new FieldType(typeof(Guid)); break;
+                case JTokenType.TimeSpan: return new FieldType(typeof(TimeSpan)); break;
+                case JTokenType.Uri: return new FieldType(typeof(Uri)); break;
+                    break;
+
+                case JTokenType.Integer: // Could be integer or long.
+                case JTokenType.Float:  // Could be float or double.
+                case JTokenType.String: // Could be anything.
+                    return GuessTokenTypeFromContent(token);
+                    break;
+
+                // Could be anything.
+                case JTokenType.Null:
+                case JTokenType.Undefined:
+                    return new FieldType(typeof(object));
+                    break;
+
+                default:
+                    throw new NotSupportedException("We shouldn't have hit this. This is a bug in the caller routine.");
+            }
+        }
+
+        private FieldType GuessTokenTypeFromContent(RavenJToken token)
+        {
+            var content = token.Value<string>();
+
+            if (ParseHelper.TryAction<bool>(x => bool.TryParse(content, out x)))
+                return new FieldType(typeof(bool));
+            if (ParseHelper.TryAction<int>(x => int.TryParse(content, out x)))
+                return new FieldType(typeof(int));
+            if (ParseHelper.TryAction<long>(x => long.TryParse(content, out x)))
+                return new FieldType(typeof(long));
+
+            if (ParseHelper.TryAction<float>(x => float.TryParse(content, out x)))
+                return new FieldType(typeof(float));
+            if (ParseHelper.TryAction<double>(x => double.TryParse(content, out x)))
+                return new FieldType(typeof(double));
+
+            if (ParseHelper.TryAction<Guid>(x => Guid.TryParse(content, out x)))
+                return new FieldType(typeof(Guid));
+            if (ParseHelper.TryAction<TimeSpan>(x => TimeSpan.TryParse(content, out x)))
+                return new FieldType(typeof(TimeSpan));
+            if (ParseHelper.TryAction<DateTimeOffset>(x => DateTimeOffset.TryParse(content, out x)))
+                return new FieldType(typeof(DateTimeOffset));
+
+            
+            if (Uri.IsWellFormedUriString(content, UriKind.Absolute))
+                return new FieldType(typeof(Uri));
+
+            return new FieldType(typeof(string));
+        }
+
+        private static class ParseHelper
+        {
+            public static bool TryAction<T> ( Func<T, bool> action )
+            {
+                T t = default(T);
+                return action(t);
+            }
+        }
+
+
+        private const string codeLayout = @"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+##namespace
+{
+##code
+}
+";
+
     }
 }
--- Raven.Tests/Json/JsonCodeGeneratorTests.cs
+++ Raven.Tests/Json/JsonCodeGeneratorTests.cs
@@ -14,21 +14,154 @@ namespace Raven.Tests.Json
 {
     public class JsonCodeGeneratorTests : RavenTest
     {
-        public class Blog
+        public class WithStrings
         {
             public string Title { get; set; }
 
             public string Category { get; set; }
         }
 
-        [Fact]
-        public void SimpleObjectWithStrings ()
+        public class WithIntsAndDateTimes
         {
-            var blog = new Blog();
-            var document = new JsonDocument {  DataAsJson = JsonExtensions.ToJObject(blog) };
+            public int Integer { get; set; }
+            public long Long { get; set; }
+            public DateTime Date { get; set; }
+        }
+
+        public class WithInnerObject
+        {
+            public WithIntsAndDateTimes First { get; set; }
+            public WithStrings Second { get; set; }
+        }
+
+        public class WithArrayOfBasics
+        {
+            public IList<int> Ints { get; set; }
+            public IList<string> Strings { get; set; }
+        }
+
+        public class WithArrayOfObjects
+        {
+            public IList<WithStrings> Objects { get; set; }
+        }
+
+        [Fact]
+        public void JsonCodeGenerator_SimpleObjectWithStrings()
+        {
+            var obj = new WithStrings() { Title = "test", Category = "category" };
+            var generator = new JsonCodeGenerator("csharp");
+            var classTypes = generator.GenerateClassTypesFromObject("WithStrings", JsonExtensions.ToJObject(obj))
+                                      .ToLookup(x => x.Name);
+
+            Assert.Equal(1, classTypes.Count());
+
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void JsonCodeGenerator_SimpleObjectWithNumericsAndDateTime()
+        {
+            var obj = new WithIntsAndDateTimes() { Integer = int.MaxValue, Long = long.MaxValue, Date = DateTime.Now };
 
             var generator = new JsonCodeGenerator("csharp");
-            var result = generator.Execute(document);
+            var classTypes = generator.GenerateClassTypesFromObject("WithIntsAndDateTimes", JsonExtensions.ToJObject(obj))
+                                      .ToLookup(x => x.Name);
+
+            Assert.Equal(1, classTypes.Count());
+
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void JsonCodeGenerator_ContentResolutionForNumerics()
+        {
+            // Result will be Int,Int,DateTime
+            var obj = new WithIntsAndDateTimes() { Integer = 1, Long = 2, Date = DateTime.Now };
+
+            var generator = new JsonCodeGenerator("csharp");
+            var classTypes = generator.GenerateClassTypesFromObject("WithIntsAndDateTimes", JsonExtensions.ToJObject(obj))
+                                      .ToLookup(x => x.Name);
+
+            Assert.Equal(1, classTypes.Count());
+
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void JsonCodeGenerator_WithDistinctInnerObject()
+        {
+            var obj = new WithInnerObject()
+            {
+                First = new WithIntsAndDateTimes() { Integer = 1, Long = (long)(int.MaxValue) + 1, Date = DateTime.Now },
+                Second = new WithStrings() { Title = "test", Category = "category" }
+            };
+
+            var generator = new JsonCodeGenerator("csharp");
+            var classTypes = generator.GenerateClassTypesFromObject("WithInnerObject", JsonExtensions.ToJObject(obj))
+                                      .ToLookup(x => x.Name);
+
+            Assert.Equal(3, classTypes.Count());
+
+
+            var clazz = classTypes["WithInnerObject"].Single() as JsonCodeGenerator.ClassType;
+            Assert.NotNull(clazz);
+
+            var first = classTypes["FirstClass"].Single() as JsonCodeGenerator.ClassType;
+            Assert.NotNull(first);
+
+            Assert.Equal(3, first.Properties.Count);
+            Assert.Equal("int", first.Properties["Integer"].Name);
+            Assert.Equal("long", first.Properties["Long"].Name);
+            Assert.Equal("DateTimeOffset", first.Properties["Date"].Name);
+
+            Assert.True(first.Properties.All(x => !x.Value.IsArray && x.Value.IsPrimitive));
+
+            var second = classTypes["SecondClass"].Single() as JsonCodeGenerator.ClassType;
+            Assert.NotNull(second);
+
+            Assert.Equal(2, second.Properties.Count);
+            Assert.Equal("string", second.Properties["Title"].Name);
+            Assert.Equal("string", second.Properties["Category"].Name);
+            Assert.True(second.Properties.All(x => !x.Value.IsArray && x.Value.IsPrimitive));
+        }
+
+        [Fact]
+        public void JsonCodeGenerator_WithArrayOfBasics()
+        {
+            var obj = new WithArrayOfBasics()
+            {
+                Ints = new int[] { 0, 1, 2 },
+                Strings = new string[] { "test", "category" }
+            };
+
+            var generator = new JsonCodeGenerator("csharp");
+            var classTypes = generator.GenerateClassTypesFromObject("WithArrayOfBasics", JsonExtensions.ToJObject(obj))
+                                      .ToLookup(x => x.Name);
+
+            Assert.Equal(1, classTypes.Count());
+
+            throw new NotImplementedException();
+        }
+
+
+        [Fact]
+        public void JsonCodeGenerator_WithArrayOfObjects()
+        {
+            var obj = new WithArrayOfObjects()
+            {
+                Objects = new List<WithStrings> {
+                     new WithStrings() { Title = "test", Category = "category" },
+                     new WithStrings() { Title = "test", Category = "category" },
+                 }
+            };
+
+            var generator = new JsonCodeGenerator("csharp");
+            var classTypes = generator.GenerateClassTypesFromObject("WithArrayOfObjects", JsonExtensions.ToJObject(obj))
+                                      .ToLookup(x => x.Name);
+
+            Assert.Equal(1, classTypes.Count());
+
+            throw new NotImplementedException();
         }
     }
 }
