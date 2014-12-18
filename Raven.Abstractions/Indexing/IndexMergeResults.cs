c47cb3e3330e469d9d08a686dcaad4386b2b03d9
Pawel Pekrol <ppekrol@ais.pl>
2014-12-18 12:55:37 +0100

    RDoc-300 summaries in Raven.Abstractions/Exceptions and Raven.Abstractions/Indexing + removed WinRTCompetability

--- Raven.Abstractions/Indexing/IndexMergeResults.cs
+++ Raven.Abstractions/Indexing/IndexMergeResults.cs
@@ -2,10 +2,10 @@ using System.Collections.Generic;
 
 namespace Raven.Abstractions.Indexing
 {
-    public class IndexMergeResults
-    {
-        public Dictionary<string, string> Unmergables=new Dictionary<string, string>();// index name, reason
+	public class IndexMergeResults
+	{
+		public Dictionary<string, string> Unmergables = new Dictionary<string, string>(); // index name, reason
 
-        public List<MergeSuggestions> Suggestions = new List<MergeSuggestions>();
-    }
+		public List<MergeSuggestions> Suggestions = new List<MergeSuggestions>();
+	}
 }
