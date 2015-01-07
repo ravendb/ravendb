ed1c045612ed3c6d929070044951209e4267db7b
Pawel Pekrol <ppekrol@ais.pl>
2015-01-07 10:46:22 +0100

    applied missing 2.5 fixes to 3.0

--- Raven.Database/Config/StronglyTypedRavenSettings.cs
+++ Raven.Database/Config/StronglyTypedRavenSettings.cs
@@ -53,7 +53,7 @@ namespace Raven.Database.Config
 
 			MaxConcurrentServerRequests = new IntegerSetting(settings[Constants.MaxConcurrentServerRequests], 512);
 
-			MaxConcurrentRequestsForDatabaseDuringLoad = new IntegerSetting(settings["Raven/MaxConcurrentRequestsForDatabaseDuringLoad"], 10);
+			MaxConcurrentRequestsForDatabaseDuringLoad = new IntegerSetting(settings[Constants.MaxConcurrentRequestsForDatabaseDuringLoad], 10);
 
 			MaxConcurrentMultiGetRequests = new IntegerSetting(settings[Constants.MaxConcurrentMultiGetRequests], 192);
 
