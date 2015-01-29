d9c63b36aae0d6f1181e67189ba0711e91465535
Pawel Pekrol <ppekrol@ais.pl>
2015-01-29 10:02:52 +0100

    RavenDB-2712 added more tests to versioning

--- Raven.Tests.Issues/RavenDB_2712_Versioning.cs
+++ Raven.Tests.Issues/RavenDB_2712_Versioning.cs
@@ -16,7 +16,7 @@ namespace Raven.Tests.Issues
 	public class RavenDB_2712_Versioning : RavenTest
 	{
 		[Fact]
-		public void IfThereIsNoLocalConfigurationThenGlobalShouldBeUsed()
+		public void GlobalDefaultConfigurationShouldBeEffectiveIfThereIsNoLocal()
 		{
 			using (NewRemoteDocumentStore(databaseName: "Northwind"))
 			{
@@ -55,6 +55,183 @@ namespace Raven.Tests.Issues
 				Assert.Equal(Constants.Versioning.RavenVersioningDefaultConfiguration, document.Document.Id);
 				Assert.Equal(17, document.Document.MaxRevisions);
 				Assert.True(document.Document.PurgeOnDelete);
+			}
+		}
+
+		[Fact]
+		public void GlobalDefaultConfigurationShouldNotBeEffectiveIfThereIsLocalDefault()
+		{
+			using (NewRemoteDocumentStore(databaseName: "Northwind"))
+			{
+				var server = servers[0];
+				var systemDatabase = server.SystemDatabase;
+				var database = server.Server.GetDatabaseInternal("Northwind").ResultUnwrap();
+				var retriever = database.ConfigurationRetriever;
+
+				var document = retriever.GetConfigurationDocument<VersioningConfiguration>(Constants.Versioning.RavenVersioningDefaultConfiguration);
+
+				Assert.Null(document);
+
+				systemDatabase
+					.Documents
+					.Put(
+						Constants.Versioning.RavenVersioningDefaultConfiguration,
+						null,
+						RavenJObject.FromObject(new VersioningConfiguration
+						{
+							Exclude = true,
+							ExcludeUnlessExplicit = true,
+							Id = Constants.Global.RavenGlobalVersioningDefaultConfiguration,
+							MaxRevisions = 17,
+							PurgeOnDelete = true
+						}),
+						new RavenJObject(),
+						null);
+
+				database
+					.Documents
+					.Put(
+						Constants.Versioning.RavenVersioningDefaultConfiguration,
+						null,
+						RavenJObject.FromObject(new VersioningConfiguration
+						{
+							Exclude = false,
+							ExcludeUnlessExplicit = false,
+							Id = Constants.Global.RavenGlobalVersioningDefaultConfiguration,
+							MaxRevisions = 5,
+							PurgeOnDelete = false
+						}),
+						new RavenJObject(),
+						null);
+
+				document = retriever.GetConfigurationDocument<VersioningConfiguration>(Constants.Versioning.RavenVersioningDefaultConfiguration);
+
+				Assert.NotNull(document);
+				Assert.True(document.GlobalExists);
+				Assert.True(document.LocalExists);
+				Assert.False(document.Document.Exclude);
+				Assert.False(document.Document.ExcludeUnlessExplicit);
+				Assert.Equal(Constants.Versioning.RavenVersioningDefaultConfiguration, document.Document.Id);
+				Assert.Equal(5, document.Document.MaxRevisions);
+				Assert.False(document.Document.PurgeOnDelete);
+			}
+		}
+
+		[Fact]
+		public void GlobalSpecificConfigurationShouldBeEffectiveIfThereIsNoLocalSpecific()
+		{
+			using (NewRemoteDocumentStore(databaseName: "Northwind"))
+			{
+				var server = servers[0];
+				var systemDatabase = server.SystemDatabase;
+				var database = server.Server.GetDatabaseInternal("Northwind").ResultUnwrap();
+				var retriever = database.ConfigurationRetriever;
+
+				var document = retriever.GetConfigurationDocument<VersioningConfiguration>(Constants.Versioning.RavenVersioningDefaultConfiguration);
+
+				Assert.Null(document);
+
+				systemDatabase
+					.Documents
+					.Put(
+						Constants.Versioning.RavenVersioningPrefix + "Orders",
+						null,
+						RavenJObject.FromObject(new VersioningConfiguration
+						{
+							Exclude = true,
+							ExcludeUnlessExplicit = true,
+							Id = Constants.Global.RavenGlobalVersioningDefaultConfiguration,
+							MaxRevisions = 17,
+							PurgeOnDelete = true
+						}),
+						new RavenJObject(),
+						null);
+
+				database
+					.Documents
+					.Put(
+						Constants.Versioning.RavenVersioningDefaultConfiguration,
+						null,
+						RavenJObject.FromObject(new VersioningConfiguration
+						{
+							Exclude = false,
+							ExcludeUnlessExplicit = false,
+							Id = Constants.Global.RavenGlobalVersioningDefaultConfiguration,
+							MaxRevisions = 5,
+							PurgeOnDelete = false
+						}),
+						new RavenJObject(),
+						null);
+
+				document = retriever.GetConfigurationDocument<VersioningConfiguration>(Constants.Versioning.RavenVersioningPrefix + "Orders");
+
+				Assert.NotNull(document);
+				Assert.True(document.GlobalExists);
+				Assert.False(document.LocalExists);
+				Assert.True(document.Document.Exclude);
+				Assert.True(document.Document.ExcludeUnlessExplicit);
+				Assert.Equal(Constants.Versioning.RavenVersioningDefaultConfiguration, document.Document.Id);
+				Assert.Equal(17, document.Document.MaxRevisions);
+				Assert.True(document.Document.PurgeOnDelete);
+			}
+		}
+
+		[Fact]
+		public void GlobalSpecificConfigurationShouldNotBeEffectiveIfThereIsLocalSpecific()
+		{
+			using (NewRemoteDocumentStore(databaseName: "Northwind"))
+			{
+				var server = servers[0];
+				var systemDatabase = server.SystemDatabase;
+				var database = server.Server.GetDatabaseInternal("Northwind").ResultUnwrap();
+				var retriever = database.ConfigurationRetriever;
+
+				var document = retriever.GetConfigurationDocument<VersioningConfiguration>(Constants.Versioning.RavenVersioningDefaultConfiguration);
+
+				Assert.Null(document);
+
+				systemDatabase
+					.Documents
+					.Put(
+						Constants.Versioning.RavenVersioningPrefix + "Orders",
+						null,
+						RavenJObject.FromObject(new VersioningConfiguration
+						{
+							Exclude = true,
+							ExcludeUnlessExplicit = true,
+							Id = Constants.Global.RavenGlobalVersioningDefaultConfiguration,
+							MaxRevisions = 17,
+							PurgeOnDelete = true
+						}),
+						new RavenJObject(),
+						null);
+
+				database
+					.Documents
+					.Put(
+						Constants.Versioning.RavenVersioningPrefix + "Orders",
+						null,
+						RavenJObject.FromObject(new VersioningConfiguration
+						{
+							Exclude = false,
+							ExcludeUnlessExplicit = false,
+							Id = Constants.Global.RavenGlobalVersioningDefaultConfiguration,
+							MaxRevisions = 5,
+							PurgeOnDelete = false
+						}),
+						new RavenJObject(),
+						null);
+
+				document = retriever.GetConfigurationDocument<VersioningConfiguration>(Constants.Versioning.RavenVersioningPrefix + "Orders");
+
+				Assert.NotNull(document);
+				Assert.True(document.GlobalExists);
+				Assert.True(document.LocalExists);
+				Assert.False(document.Document.Exclude);
+				Assert.False(document.Document.ExcludeUnlessExplicit);
+				Assert.Equal(Constants.Versioning.RavenVersioningDefaultConfiguration, document.Document.Id);
+				Assert.Equal(5, document.Document.MaxRevisions);
+				Assert.False(document.Document.PurgeOnDelete);
 			}
 		}
 	}
