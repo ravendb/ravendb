106b7a57621246012867556820cbf3bc333ad4fc
Arkadiusz Palinski <arek@ais.pl>
2015-02-02 08:39:19 +0100

    RavenDB-3114 Simplifying to single out parameter of WriteBatch.TryGetValue method.

--- Raven.Voron/Voron/Impl/SnapshotReader.cs
+++ Raven.Voron/Voron/Impl/SnapshotReader.cs
@@ -23,22 +23,19 @@ namespace Voron.Impl
 
 			if (writeBatch != null && writeBatch.IsEmpty == false)
 			{
-				WriteBatch.BatchOperationType operationType;
-				Stream stream;
-				IStructure _;
-				ushort? version;
-				if (writeBatch.TryGetValue(treeName, key, out stream, out _, out version, out operationType))
+				WriteBatch.InBatchValue result;
+				if (writeBatch.TryGetValue(treeName, key, out result))
 				{
-					if (!version.HasValue)
+					if (!result.Version.HasValue)
 						tree = GetTree(treeName);
 
-					switch (operationType)
+					switch (result.OperationType)
 					{
 						case WriteBatch.BatchOperationType.Add:
-							{
-								var reader = new ValueReader(stream);
-								return new ReadResult(reader, version.HasValue ? (ushort)(version.Value + 1) : tree.ReadVersion(key));
-							}
+						{
+							var reader = new ValueReader(result.Stream);
+							return new ReadResult(reader, result.Version.HasValue ? (ushort) (result.Version.Value + 1) : tree.ReadVersion(key));
+						}
 						case WriteBatch.BatchOperationType.Delete:
 							return null;
 					}
@@ -58,19 +55,16 @@ namespace Voron.Impl
 
 			if (writeBatch != null && writeBatch.IsEmpty == false)
 			{
-				WriteBatch.BatchOperationType operationType;
-				IStructure value;
-				Stream _;
-				ushort? version;
-				if (writeBatch.TryGetValue(treeName, key, out _, out value, out version, out operationType))
+				WriteBatch.InBatchValue result;
+				if (writeBatch.TryGetValue(treeName, key, out result))
 				{
-					if (!version.HasValue)
+					if (!result.Version.HasValue)
 						tree = GetTree(treeName);
 
-					switch (operationType)
+					switch (result.OperationType)
 					{
 						case WriteBatch.BatchOperationType.AddStruct:
-							return new StructReadResult<T>(new StructureReader<T>((Structure<T>) value, schema),  version.HasValue ? (ushort)(version.Value + 1) : tree.ReadVersion(key));
+							return new StructReadResult<T>(new StructureReader<T>((Structure<T>) result.Struct, schema),  result.Version.HasValue ? (ushort)(result.Version.Value + 1) : tree.ReadVersion(key));
 						case WriteBatch.BatchOperationType.Delete:
 							return null;
 					}
@@ -93,12 +87,12 @@ namespace Voron.Impl
 		{
 			if (writeBatch != null && writeBatch.IsEmpty == false)
 			{
-				WriteBatch.BatchOperationType operationType;
-				Stream stream;
-				IStructure _;
-				if (writeBatch.TryGetValue(treeName, key, out stream, out _, out version, out operationType))
+				WriteBatch.InBatchValue result;
+				if (writeBatch.TryGetValue(treeName, key, out result))
 				{
-					switch (operationType)
+					version = result.Version;
+
+					switch (result.OperationType)
 					{
 						case WriteBatch.BatchOperationType.Add:
 							return true;
@@ -107,7 +101,7 @@ namespace Voron.Impl
 						case WriteBatch.BatchOperationType.Delete:
 							return false;
 						default:
-							throw new ArgumentOutOfRangeException(operationType.ToString());
+							throw new ArgumentOutOfRangeException(result.OperationType.ToString());
 					}
 				}
 			}
@@ -126,18 +120,15 @@ namespace Voron.Impl
 		{
 			if (writeBatch != null)
 			{
-				WriteBatch.BatchOperationType operationType;
-				Stream stream;
-				IStructure _;
-				ushort? version;
-				if (writeBatch.TryGetValue(treeName, key, out stream, out _, out version, out operationType) && version.HasValue)
+				WriteBatch.InBatchValue result;
+				if (writeBatch.TryGetValue(treeName, key, out result) && result.Version.HasValue)
 				{
-					switch (operationType)
+					switch (result.OperationType)
 					{
 						case WriteBatch.BatchOperationType.Add:
 						case WriteBatch.BatchOperationType.AddStruct:
 						case WriteBatch.BatchOperationType.Delete:
-							return (ushort)(version.Value + 1);
+							return (ushort)(result.Version.Value + 1);
 					}
 				}
 			}
