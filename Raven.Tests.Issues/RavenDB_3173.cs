b6d519681c9d5dcb30899ccfac904ae8f714511c
Ayende Rahien <ayende@ayende.com>
2015-02-10 15:38:08 +0200

    Adding failing test for issue RavenDB-3173

---
+++ Raven.Tests.Issues/RavenDB_3173.cs
@@ -0,0 +1,36 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="RavenDB_3173.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using Raven.Tests.Helpers;
+using Xunit;
+
+namespace Raven.Tests.Issues
+{
+    public class RavenDB_3173 : RavenTestBase
+    {
+
+        public class Item { }
+
+        [Fact]
+        public void CannotSaveInvalidHeaders()
+        {
+            using (var store = NewDocumentStore())
+            {
+                using (var s = store.OpenSession())
+                {
+                    var entity = new Item();
+                    s.Store(entity);
+                    s.Advanced.GetMetadataFor(entity)["you@there"] = true;
+                    s.SaveChanges();
+                }
+
+                using (var s = store.OpenSession())
+                {
+                    s.Load<Item>(1);
+                }
+            }
+        }
+    }
+}
