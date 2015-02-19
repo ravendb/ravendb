eb68594244cc97325a10d0d02cb3112d97ed6f78
Felipe Amorim <felipe.leusin@gmail.com>
2015-02-19 11:16:24 -0200

    improved tests

--- Raven.Tests.Bundles/UniqueConstraints/Bugs/EmptyValues.cs
+++ Raven.Tests.Bundles/UniqueConstraints/Bugs/EmptyValues.cs
@@ -9,7 +9,25 @@ namespace Raven.Tests.Bundles.UniqueConstraints.Bugs
     public class EmptyValues : UniqueConstraintsTest
     {
         [Fact]
-        public void When_Updating_Constraint_To_Empty_Value_Should_Delete_Constraint_Document()
+        public void When_Creating_User_With_Empty_Email_Value_Should_Not_Create_Constraint_Document()
+        {
+            var user = new User { Name = "Test", Email = "", TaskIds = null };
+
+            using (var session = DocumentStore.OpenSession())
+            {
+                session.Store(user);
+                session.SaveChanges();
+            }
+
+            using (var session = DocumentStore.OpenSession())
+            {
+                var constraints = session.Advanced.LoadStartingWith<object>("UniqueConstraints/");
+                Assert.Equal(0, constraints.Length);
+            }
+        }
+
+        [Fact]
+        public void When_Updating_User_With_Empty_Email_Value_Should_Not_Create_Constraint_Document()
         {
             var user = new User { Name = "Test", Email = "foo@bar.com", TaskIds = null };
 
