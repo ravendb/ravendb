6964b6b98657be459737432e31ba6ea40f76b0a6
Arkadiusz Palinski <arek@ais.pl>
2015-02-24 12:49:54 +0100

    Removing unused optional parameter - in RavenFS we don't support concurrency checks at all

--- Raven.Client.Lightweight/FileSystem/IAsyncFilesCommands.cs
+++ Raven.Client.Lightweight/FileSystem/IAsyncFilesCommands.cs
@@ -64,7 +64,7 @@ namespace Raven.Client.FileSystem
         Task<FileSystemStats> GetStatisticsAsync();
 
 
-        Task DeleteAsync(string filename, Etag etag = null);
+        Task DeleteAsync(string filename);
         Task RenameAsync(string currentName, string newName);
 
         Task<RavenJObject> GetMetadataForAsync(string filename);
