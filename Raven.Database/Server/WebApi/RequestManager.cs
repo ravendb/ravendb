450fafb0b4ef4bbcecc9584392c6cbea803e9b56
Ayende Rahien <ayende@ayende.com>
2015-03-16 15:19:33 +0200

    Removing useless thread local assignment

--- Raven.Database/Server/WebApi/RequestManager.cs
+++ Raven.Database/Server/WebApi/RequestManager.cs
@@ -254,17 +254,7 @@ namespace Raven.Database.Server.WebApi
                 
                 return nameValueCollection;
             });
-            CurrentOperationContext.RavenAuthenticatedUser.Value = string.Empty;
-            foreach (var innerHeader in innerHeaders)
-            {
-				if (innerHeader.Key.Equals(Constants.Authorization.RavenAuthorizationUser))
-                {
-                    CurrentOperationContext.RavenAuthenticatedUser.Value = innerHeader.Value.FirstOrDefault();
-                }
-            }
-            
-            
-
+          
 			CurrentOperationContext.User.Value = null;
 
 			LogContext.DatabaseName.Value = databaseName;
