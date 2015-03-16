c0a457f337ab008488c8d6c049ddd92eb1d5721d
Tal Weiss <tal@ayende.com>
2015-03-16 17:03:42 +0200

    Fixed an issue where using bulk insert won't copy thread local information into the new generated thread. This issue is related to triggers not been able to access thread local information like the User. for more details see http://issues.hibernatingrhinos.com/issue/RavenDB-3230

--- Raven.Tests.Issues/RavenDB_1041.cs
+++ Raven.Tests.Issues/RavenDB_1041.cs
@@ -64,41 +64,42 @@ namespace Raven.Tests.Issues
 			}
 		}
 
-		[Fact]
-		public async Task CanWaitForReplicationOfParticularEtag()
-		{
-			var store1 = CreateStore(requestedStorageType:"esent");
+        [Fact]
+        public async Task CanWaitForReplicationOfParticularEtag()
+        {
+            var store1 = CreateStore(requestedStorageType: "esent");
             var store2 = CreateStore(requestedStorageType: "esent");
             var store3 = CreateStore(requestedStorageType: "esent");
 
-			SetupReplication(store1.DatabaseCommands, store2, store3);
+            SetupReplication(store1.DatabaseCommands, store2, store3);
 
-			var putResult = store1.DatabaseCommands.Put("Replicated/1", null, new RavenJObject(), new RavenJObject());
-			var putResult2 = store1.DatabaseCommands.Put("Replicated/2", null, new RavenJObject(), new RavenJObject());
+            var putResult = store1.DatabaseCommands.Put("Replicated/1", null, new RavenJObject(), new RavenJObject());
+            var putResult2 = store1.DatabaseCommands.Put("Replicated/2", null, new RavenJObject(), new RavenJObject());
 
-			var i = await ((DocumentStore)store1).Replication.WaitAsync(putResult.ETag);
+            var i = await ((DocumentStore) store1).Replication.WaitAsync(putResult.ETag);
 
-			Assert.Equal(2,i);
+            Assert.Equal(2, i);
 
-			Assert.NotNull(store2.DatabaseCommands.Get("Replicated/1"));
-			Assert.NotNull(store3.DatabaseCommands.Get("Replicated/1"));
+            Assert.NotNull(store2.DatabaseCommands.Get("Replicated/1"));
+            Assert.NotNull(store3.DatabaseCommands.Get("Replicated/1"));
 
-			var replicatedEtagInfos = new ConcurrentQueue<ReplicatedEtagInfo>();
-			i = await ((DocumentStore)store1).Replication.WaitAsync(putResult2.ETag, actions: replicatedEtagInfos);
-			try
-			{
-				Assert.Equal(2, i);
+            var replicatedEtagInfos = new ConcurrentQueue<ReplicatedEtagInfo>();
+            i = await ((DocumentStore) store1).Replication.WaitAsync(putResult2.ETag, actions: replicatedEtagInfos);
+            try
+            {
+                Assert.Equal(2, i);
 
 
-				Assert.NotNull(store2.DatabaseCommands.ForDatabase(DatabaseName).Get("Replicated/1"));
-				Assert.NotNull(store3.DatabaseCommands.ForDatabase(DatabaseName).Get("Replicated/1"));
-			}
-			catch (Exception e)
-			{
-				throw new InvalidOperationException(JsonConvert.SerializeObject(replicatedEtagInfos.ToArray()), e);
-			}
+                Assert.NotNull(store2.DatabaseCommands.ForDatabase(DatabaseName).Get("Replicated/1"));
+                Assert.NotNull(store3.DatabaseCommands.ForDatabase(DatabaseName).Get("Replicated/1"));
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException(JsonConvert.SerializeObject(replicatedEtagInfos.ToArray()), e);
+            }
+        }
 
-		[Fact]
+        [Fact]
 		public async Task CanWaitForReplicationInAsyncManner()
 		{
 			var store1 = CreateStore();
