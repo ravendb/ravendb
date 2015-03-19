1a74a4464a1336ad2ba0c8e7c3a514649b7b65fd
preston.zaugg <preston.zaugg@aviacode.com>
2015-03-19 16:37:53 -0600

    Failing test for Authorization + Lazy Passing Test for Authorization + Streaming

---
+++ Bundles/Raven.Bundles.Tests/Authorization/Bugs/PrestonLazy.cs
@@ -0,0 +1,45 @@
+﻿namespace Raven.Bundles.Tests.Authorization.Bugs
+{
+	extern alias client;
+	using client::Raven.Bundles.Authorization.Model;
+	using client::Raven.Client.Authorization;
+	using System.Linq;
+	using Xunit;
+
+	public class PrestonLazy : AuthorizationTest
+	{
+		[Fact]
+		public void DocumentWithoutPermissionWillBeFilteredOutSilently()
+		{
+			var company = new Company
+			{
+				Name = "Hibernating Rhinos"
+			};
+			using (var s = store.OpenSession())
+			{
+				s.Store(new AuthorizationUser
+				{
+					Id = UserId,
+					Name = "Ayende Rahien",
+				});
+
+				s.Store(company);
+
+				s.SetAuthorizationFor(company, new DocumentAuthorization());// deny everyone
+
+				s.SaveChanges();
+			}
+
+			using (var s = store.OpenSession())
+			{
+				s.SecureFor(UserId, "Company/Bid");
+
+				Assert.Equal(0, s.Advanced.LuceneQuery<Company>()
+									.WaitForNonStaleResults()
+									.Lazily().Value
+									.Count());
+			}
+		}
+
+	}
+}
