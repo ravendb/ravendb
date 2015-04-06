5302998a7a2b7863404196bddbfa5c3747dd288b
Ayende Rahien <ayende@ayende.com>
2015-04-06 22:01:40 +0300

    Making sure that fragment length is properly calculated for large text fields

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -12,6 +12,7 @@ using Raven.Client.FileSystem;
 using Raven.Client.Indexes;
 using Raven.Json.Linq;
 using Raven.Tests.Common;
+using Raven.Tests.MailingList;
 
 namespace Raven.Tryouts
 {
@@ -19,40 +20,8 @@ namespace Raven.Tryouts
 	{
 		private static void Main()
 		{
-            Console.WriteLine(SystemParameters.BookmarkMost);
-		    return;
-			var filesStore = new FilesStore
-			{
-				Url = "http://localhost:8080",
-				DefaultFileSystem = "test"
-			};
-			filesStore.Initialize();
-			for (int i = 0; i < 5; i++)
-			{
-				GetValue(filesStore).Wait();
-			}
+           new FragmentFail().Fragment_Length_Should_Not_Be_More_Then_128();
 		}
 
-		private static async Task GetValue(FilesStore filesStore)
-		{
-			using (var fileSession1 = filesStore.OpenAsyncSession())
-			{
-				try
-				{
-					var watch = Stopwatch.StartNew();
-					var downloadedStream = await fileSession1.DownloadAsync("/hwip/HWIP_v0.2.AsterixMiniRite312_v0.2");
-
-					Console.WriteLine("Stream acquired - time elapsed (ms): {0}", watch.ElapsedMilliseconds);
-					watch.Restart();
-					downloadedStream.CopyTo(new MemoryStream());
-					Console.WriteLine("Streamed and decompressed the document. Time elapsed (ms): {0}", watch.ElapsedMilliseconds);
-				}
-				catch (Exception e)
-				{
-					Console.WriteLine(e);
-				}
-			}
-			Console.WriteLine("Storing done!");
-		}
 	}
 }
