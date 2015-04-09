fd67a3ed35952e084b8256bf95b5490aac9843fd
Federico Andres Lois <federico.lois@corvalius.com>
2015-04-09 12:21:23 -0300

    RavenDB-3372: Repro

---
+++ Raven.Tests/Bugs/MapRedue/LargeKeysInVoron.cs
@@ -0,0 +1,71 @@
+﻿using Raven.Client.Indexes;
+using Raven.Tests.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Raven.Tests.Bugs.MapRedue
+{
+    public class LargeKeysInVoron : RavenTest
+    {
+        public class User
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class LargeKeysInVoronFunction : AbstractIndexCreationTask<User, LargeKeysInVoronFunction.ReduceResult>
+        {
+            public class ReduceResult
+            {
+                public string Id { get; set; }
+                public string Name { get; set; }
+            }
+
+            public LargeKeysInVoronFunction()
+            {
+                Map = users => from user in users
+                               select new
+                               {
+                                   user.Id,
+                                   user.Name
+                               };
+
+                Reduce = results => from result in results
+                                    group result by result.Id
+                                        into g
+                                        let dummy = g.FirstOrDefault(x => x.Name != null)
+                                        select new
+                                        {
+                                            Id = g.Key,
+                                            Name = dummy.Name
+                                        };
+            }
+        }
+
+
+
+        [Fact]
+        public void CanHandleLargeReduceKeys()
+        {
+            using (var store = NewDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User { Id = new string('A', 10000), Name = "Ayende Rahien" });
+                    session.Store(new User { Id = new string('B', 10000), Name = "Daniel Lang" });
+                    session.SaveChanges();
+                }
+
+                new LargeKeysInVoronFunction().Execute(store);
+
+                WaitForIndexing(store);
+
+                Assert.Empty(store.SystemDatabase.Statistics.Errors);
+            }
+        }
+    }
+}
