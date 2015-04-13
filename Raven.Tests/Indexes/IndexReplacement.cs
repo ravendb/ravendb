2584ffeb6b2d18c929d75b8405d3da1869816f26
Ayende Rahien <ayende@ayende.com>
2015-04-13 20:39:55 +0300

    Fixing test

--- Raven.Tests/Indexes/IndexReplacement.cs
+++ Raven.Tests/Indexes/IndexReplacement.cs
@@ -26,6 +26,11 @@ namespace Raven.Tests.Indexes
 
         private class OldIndex : AbstractIndexCreationTask<Person>
         {
+            public override string IndexName
+            {
+                get { return "The/Index"; }
+            }
+
             public OldIndex()
             {
                 Map = persons => from person in persons select new { person.FirstName };
@@ -34,6 +39,11 @@ namespace Raven.Tests.Indexes
 
         private class NewIndex : AbstractIndexCreationTask<Person>
         {
+            public override string IndexName
+            {
+                get { return "The/Index"; }
+            }
+
             public NewIndex()
             {
                 Map = persons => from person in persons select new { person.FirstName, person.LastName };
@@ -43,7 +53,7 @@ namespace Raven.Tests.Indexes
         [Fact]
         public void ReplaceAfterNonStale()
         {
-            using (var store = NewDocumentStore(runInMemory: false))
+            using (var store = NewDocumentStore())
             {
                 IndexCreation.CreateIndexes(new CompositionContainer(new TypeCatalog(typeof(OldIndex))), store);
 
@@ -61,8 +71,10 @@ namespace Raven.Tests.Indexes
                 Assert.Contains("The field 'LastName' is not indexed, cannot query on fields that are not indexed", e.InnerException.Message);
 
                 IndexCreation.SideBySideCreateIndexes(new CompositionContainer(new TypeCatalog(typeof(NewIndex))), store);
-
+               
                 WaitForIndexing(store);
+
+                store.DocumentDatabase.RunIdleOperations();
 
                 using (var session = store.OpenSession())
                 {
