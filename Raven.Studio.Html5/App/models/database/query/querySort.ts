60210eb69b2856cd6d34aa5f0cd33f88fd1aa249
Grisha Kotler <grisha@ayende.com>
2015-04-30 17:09:10 +0300

    removed alphanumeric sort option from the studio

--- Raven.Studio.Html5/App/models/database/query/querySort.ts
+++ Raven.Studio.Html5/App/models/database/query/querySort.ts
@@ -8,18 +8,11 @@ class querySort {
     sortDirection = ko.observable(0); // 0 = ascending, 1 = descending, 2 = range ascending, 3 = range descending
     isRange = ko.observable<boolean>(false);
     isAscending = ko.observable<boolean>(true);
-    isAlphanumeric = ko.observable<boolean>(false);
 
     static rangeIndicator = "_Range";
-    static alphaNumericIndicator = "__alphaNumeric";
 
     constructor() {
         this.fieldNameOrDefault = ko.computed(() => this.fieldName() ? this.fieldName() : "Select a field");
-        this.isAlphanumeric.subscribe((value) => {
-            if (value && this.isRange()) {
-                this.isRange(false);
-            }
-        });
     }
 
     toggleAscending() {
@@ -29,22 +22,11 @@ class querySort {
     toggleRange() {
         this.isRange.toggle();
     }
-  
-    setAlphaNumeric(value: boolean) {
-        this.isAlphanumeric(value);
-    }
 
     toQuerySortString(): string {
         var querySortString: string;
 
-        if (this.isAlphanumeric()) {
-            if (this.isAscending()) {
-                querySortString = querySort.alphaNumericIndicator + ";" + this.fieldName(); //ascending alphanumeric
-            } else {
-                querySortString = "-" + querySort.alphaNumericIndicator + ";" + this.fieldName(); //descending alphanumeric
-            }
-        }
-        else if (this.isRange()) {
+        if (this.isRange()) {
             if (this.isAscending()) {
                 querySortString = this.fieldName() + querySort.rangeIndicator; //ascending range
             }
@@ -66,14 +48,7 @@ class querySort {
     toHumanizedString(): string {
         var str;
 
-        if (this.isAlphanumeric()) {
-            if (this.isAscending()) {
-                str = this.fieldName() + " alphanumeric"; //ascending alphanumeric
-            } else {
-                str = this.fieldName() + " alphanumeric descending"; //descending alphanumeric
-            }
-        }
-        else if (this.isRange()) {
+        if (this.isRange()) {
             if (this.isAscending()) {
                 str = this.fieldName() + " range"; //ascending range
             }
@@ -95,32 +70,22 @@ class querySort {
 
     static fromQuerySortString(querySortText: string) {
         var isDescending = querySortText.slice(0, 1) === "-";
-        var isAlphanumeric = querySortText.length > querySort.alphaNumericIndicator.length
-            && (querySortText.substr(0, querySort.alphaNumericIndicator.length) === querySort.alphaNumericIndicator || 
-            querySortText.substr(1, querySort.alphaNumericIndicator.length) === querySort.alphaNumericIndicator);
         var isRange = querySortText.length > querySort.rangeIndicator.length 
             && querySortText.substr(querySortText.length - querySort.rangeIndicator.length) === querySort.rangeIndicator;
 
-        var sortField = querySort.getSortField(querySortText, isDescending, isAlphanumeric, isRange);
+        var sortField = querySort.getSortField(querySortText, isDescending, isRange);
 
         var q = new querySort();
         q.isAscending(!isDescending);
-        q.isAlphanumeric(isAlphanumeric);
         q.isRange(isRange);
         q.fieldName(sortField);
         return q;
     }
 
-    private static getSortField(querySortText: string, isDescending: boolean, isAlphanumeric: boolean, isRange: boolean) {
+    private static getSortField(querySortText: string, isDescending: boolean, isRange: boolean) {
         var sortField: string;
 
-        if (isAlphanumeric && isDescending) {
-            sortField = querySortText.substr(querySort.alphaNumericIndicator.length + 2);
-        }
-        else if (isAlphanumeric && !isDescending) {
-            sortField = querySortText.substr(querySort.alphaNumericIndicator.length + 1);
-        }
-        else if (isRange && isDescending) {
+        if (isRange && isDescending) {
             sortField = querySortText.substr(1, querySortText.length - 1 - querySort.rangeIndicator.length);
         }
         else if (isRange && !isDescending) {
