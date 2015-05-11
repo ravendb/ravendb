3648f4c5c6c835ce85c9285686ae55b778e95340
Marcin Lewandowski <marcin@ais.pl>
2015-05-11 13:53:39 +0200

    RavenDB-3447 Date Time Picker broken after moving paths

--- Raven.Studio.Html5/Scripts/typings/bootstrap.v3.datetimepicker/bootstrap.v3.datetimepicker.d.ts
+++ Raven.Studio.Html5/Scripts/typings/bootstrap.v3.datetimepicker/bootstrap.v3.datetimepicker.d.ts
@@ -52,13 +52,13 @@ declare module BootstrapV3DatetimePicker {
     }
 
     interface Datetimepicker {
-        setDate(date: any): void;
-        setMinDate(date: any): void;
-        setMaxDate(date: any): void;
+        minDate(date: any): void;
+        maxDate(date: any): void;
         show(): void;
         disable(): void;
         enable(): void;
-        getDate(): void;
+        date(): void;
+		date(date: any): void;
     }
 
 }
