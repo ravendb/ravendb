921f445ab8be39f9cce33d723ef96d87b01d053c
Hila Mefano <hila@ayende.com>
2015-06-01 15:23:23 +0300

    IBulkInsertOperation - interface from BulkInsert

---
+++ Raven.Client.Lightweight/Document/IBulkInsertOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Raven.Abstractions.Data;
+using Raven.Json.Linq;
+
+namespace Raven.Client.Document
+{
+	public interface IBulkInsertOperation
+	{
+		Guid OperationId { get; }
+
+		bool IsAborted { get;}
+
+		void Abort();
+
+		event Action<string> Report;
+
+		Task DisposeAsync();
+			
+		void Dispose();
+
+		string Store(object entity);
+
+
+		void Store(object entity, string id);
+
+		void Store(RavenJObject document, RavenJObject metadata, string id, int? dataSize = null);
+
+	}
+}
