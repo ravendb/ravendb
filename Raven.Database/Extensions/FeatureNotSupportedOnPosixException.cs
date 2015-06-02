c83ff76d8a794b40dba96ee9ad318cda6e1dc7d5
aviviadi <avivi.adi.hr@gmail.com>
2015-06-02 20:11:19 +0300

    MERGE:SpecificPlatform files + compilefix in DocumentActions. Not compileable

---
+++ Raven.Database/Extensions/FeatureNotSupportedOnPosixException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Raven.Database.Extensions
+{
+	public class FeatureNotSupportedOnPosixException : Exception
+	{
+		public FeatureNotSupportedOnPosixException ()
+		{
+		}
+		public FeatureNotSupportedOnPosixException (string message) : base(message)
+		{
+		}
+		public FeatureNotSupportedOnPosixException (string message, Exception inner) : base(message, inner)
+		{
+		}
+	}
+}
+
