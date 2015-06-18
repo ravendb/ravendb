fa48991bf153a0d43ebbbdd81388bf2290e0c4e0
Federico Andres Lois <federico.lois@corvalius.com>
2015-06-18 13:31:33 -0300

    Force inlining comparizon methods in case they are used directly.

--- Raven.Sparrow/Sparrow/Comparers.cs
+++ Raven.Sparrow/Sparrow/Comparers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,20 +11,25 @@ namespace Sparrow
     {
         public static readonly NumericEqualityComparer Instance = new NumericEqualityComparer();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool Equals(long x, long y)
         {
             return x == y;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int GetHashCode(long obj)
         {
             return unchecked((int)obj ^ (int)(obj >> 32));
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool Equals(int x, int y)
         {
             return x == y;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int GetHashCode(int obj)
         {
             return obj;
@@ -34,11 +40,13 @@ namespace Sparrow
     {
         public static readonly NumericDescendingComparer Instance = new NumericDescendingComparer();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int Compare(long x, long y)
         {
             return Math.Sign(y - x);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int Compare(int x, int y)
         {
             return y - x;
