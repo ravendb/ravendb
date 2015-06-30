96aa0bacaaa3057d4ce5f3d59050e0fa3692b010
Hila Mefano <hila@ayende.com>
2015-06-30 14:36:51 +0300

    UserPermission - when requesting for GetUserPermission we what to know the User name and requested Database, the Method used- GET/POST, if user permission is granted or not and the reason for it
    Signed-off-by: Hila Mefano <hila@ayende.com>

--- Raven.Abstractions/Data/UserPermission.cs
+++ Raven.Abstractions/Data/UserPermission.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace Raven.Abstractions.Data
 {
 	public class UserPermission
 	{
+		public string User { get; set; }
+		public DatabaseInfo Database{ get; set; }
+		public MethodOptions Method { get; set; }
 		public bool IsGranted { get; set; }
 		public string Reason { get; set; }
 	}
