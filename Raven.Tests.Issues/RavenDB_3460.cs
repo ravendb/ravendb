b2136c6534e07efb2b3d6e0429138d66ab830628
Michael Yarichuk <michael.yarichuk@gmail.com>
2015-07-07 19:11:57 +0300

    very minor

--- Raven.Tests.Issues/RavenDB_3460.cs
+++ Raven.Tests.Issues/RavenDB_3460.cs
@@ -13,7 +13,7 @@ namespace Raven.Tests.Issues
 {
 
 		public class RavenDB_3460 : RavenTestBase
-		{
+		{						
 			[Fact]
 			public void TripleEncodingInHttpQueryShouldWork()
 			{
@@ -35,7 +35,7 @@ namespace Raven.Tests.Issues
 					var customers = SetupAndGetCustomers(store);
 					customers.Should().NotBeEmpty();
 
-					var url = string.Format("{0}/databases/{1}/indexes/CustomersIndex?query=Number%253A1", store.Url, store.DefaultDatabase);
+					var url = string.Format("{0}/databases/{1}/indexes/CustomersIndex?query=Number%253A1", store.Url,store.DefaultDatabase);
 
 					GetResults(url).Values().Should().NotBeEmpty();
 				}
