088c014cf4147ec71f3154149119997797b46fc7
Ayende Rahien <ayende@ayende.com>
2015-07-09 17:12:13 +0300

    Adding failing test

--- Raven.Tests.Issues/RavenDB-3491.cs
+++ Raven.Tests.Issues/RavenDB-3491.cs
@@ -16,81 +16,167 @@ using Xunit;
 
 namespace Raven.Tests.Issues
 {
-	public class RavenDB_3491 : RavenTestBase
-	{
-		private readonly TimeSpan waitForDocTimeout = TimeSpan.FromSeconds(20);
+    public class RavenDB_3491 : RavenTestBase
+    {
+        private readonly TimeSpan waitForDocTimeout = TimeSpan.FromSeconds(20);
 
-		[Fact]
-		public void SubscribtionWithEtag()
-		{
-			using (var store = NewDocumentStore())
-			{
-				var us1 = new User{Id = "users/1", Name = "john", Age = 22};
-				var us2 = new User{Id = "users/2", Name = "KY", Age = 30};
-				var us3 = new User {Id = "users/3", Name = "BEN" , Age = 30};
-				var us4 = new User { Id = "users/4", Name = "Hila", Age = 29 };
-				var us5 = new User { Id = "users/5", Name = "Revital", Age = 34 };
+        [Fact]
+        public void SubscribtionWithEtag()
+        {
+            using (var store = NewDocumentStore())
+            {
+                var us1 = new User { Id = "users/1", Name = "john", Age = 22 };
+                var us2 = new User { Id = "users/2", Name = "KY", Age = 30 };
+                var us3 = new User { Id = "users/3", Name = "BEN", Age = 30 };
+                var us4 = new User { Id = "users/4", Name = "Hila", Age = 29 };
+                var us5 = new User { Id = "users/5", Name = "Revital", Age = 34 };
 
-				using (var session = store.OpenSession())
-				{
-					session.Store(us1);
-					session.Store(us2);
-					session.Store(us3);
-					session.Store(us4);
-					session.Store(us5);
-					session.SaveChanges();
+                using (var session = store.OpenSession())
+                {
+                    session.Store(us1);
+                    session.Store(us2);
+                    session.Store(us3);
+                    session.Store(us4);
+                    session.Store(us5);
+                    session.SaveChanges();
 
-					var user2Etag = session.Advanced.GetEtagFor(us2);
-					var id = store.Subscriptions.Create(new SubscriptionCriteria
-						{
-							StartEtag = user2Etag
-						});
+                    var user2Etag = session.Advanced.GetEtagFor(us2);
+                    var id = store.Subscriptions.Create(new SubscriptionCriteria
+                        {
+                            StartEtag = user2Etag
+                        });
 
-					var users = new List<RavenJObject>();
+                    var users = new List<RavenJObject>();
 
-					var subscription = store.Subscriptions.Open(id, new SubscriptionConnectionOptions());
+                    var subscription = store.Subscriptions.Open(id, new SubscriptionConnectionOptions());
 
-					var docs = new BlockingCollection<RavenJObject>();
-					
-					subscription.Subscribe(docs.Add);
-				
-					RavenJObject doc;
-					Assert.True(docs.TryTake(out doc, waitForDocTimeout));
-					users.Push(doc);
-					Assert.True(docs.TryTake(out doc, waitForDocTimeout));
-					users.Push(doc);
-					Assert.True(docs.TryTake(out doc, waitForDocTimeout));
-					users.Push(doc);
-					var cnt = users.Count;
-					Assert.Equal(3,cnt);
+                    var docs = new BlockingCollection<RavenJObject>();
 
-					var keys = new BlockingCollection<string>();
-					var ages = new BlockingCollection<int>();
+                    subscription.Subscribe(docs.Add);
 
-					subscription.Subscribe(x => keys.Add(x[Constants.Metadata].Value<string>("@id")));		
-					subscription.Subscribe(x => ages.Add(x.Value<int>("Age")));
+                    RavenJObject doc;
+                    Assert.True(docs.TryTake(out doc, waitForDocTimeout));
+                    users.Push(doc);
+                    Assert.True(docs.TryTake(out doc, waitForDocTimeout));
+                    users.Push(doc);
+                    Assert.True(docs.TryTake(out doc, waitForDocTimeout));
+                    users.Push(doc);
+                    var cnt = users.Count;
+                    Assert.Equal(3, cnt);
 
-					string key;
-					Assert.True(keys.TryTake(out key, waitForDocTimeout));
-					Assert.Equal("users/3", key);
+                    var keys = new BlockingCollection<string>();
+                    var ages = new BlockingCollection<int>();
 
-					Assert.True(keys.TryTake(out key, waitForDocTimeout));
-					Assert.Equal("users/4", key);
+                    subscription.Subscribe(x => keys.Add(x[Constants.Metadata].Value<string>("@id")));
+                    subscription.Subscribe(x => ages.Add(x.Value<int>("Age")));
 
-					Assert.True(keys.TryTake(out key, waitForDocTimeout));
-					Assert.Equal("users/5", key);
+                    string key;
+                    Assert.True(keys.TryTake(out key, waitForDocTimeout));
+                    Assert.Equal("users/3", key);
 
-					int age;
-					Assert.True(ages.TryTake(out age, waitForDocTimeout));
-					Assert.Equal(30, age);
+                    Assert.True(keys.TryTake(out key, waitForDocTimeout));
+                    Assert.Equal("users/4", key);
 
-					Assert.True(ages.TryTake(out age, waitForDocTimeout));
-					Assert.Equal(29, age);
+                    Assert.True(keys.TryTake(out key, waitForDocTimeout));
+                    Assert.Equal("users/5", key);
 
-					Assert.True(ages.TryTake(out age, waitForDocTimeout));
-					Assert.Equal(34, age);
-				}
-			}
-		}
-	}
+                    int age;
+                    Assert.True(ages.TryTake(out age, waitForDocTimeout));
+                    Assert.Equal(30, age);
+
+                    Assert.True(ages.TryTake(out age, waitForDocTimeout));
+                    Assert.Equal(29, age);
+
+                    Assert.True(ages.TryTake(out age, waitForDocTimeout));
+                    Assert.Equal(34, age);
+                }
+            }
+        }
+
+        [Fact]
+        public void SubscribtionWithEtag_MultipleOpens()
+        {
+            using (var store = NewDocumentStore())
+            {
+                var us1 = new User { Id = "users/1", Name = "john", Age = 22 };
+                var us2 = new User { Id = "users/2", Name = "KY", Age = 30 };
+                var us3 = new User { Id = "users/3", Name = "BEN", Age = 30 };
+                var us4 = new User { Id = "users/4", Name = "Hila", Age = 29 };
+                var us5 = new User { Id = "users/5", Name = "Revital", Age = 34 };
+
+                long subscriptionId;
+                using (var session = store.OpenSession())
+                {
+                    session.Store(us1);
+                    session.Store(us2);
+                    session.Store(us3);
+                    session.Store(us4);
+                    session.Store(us5);
+                    session.SaveChanges();
+
+                    var user2Etag = session.Advanced.GetEtagFor(us2);
+                    subscriptionId = store.Subscriptions.Create(new SubscriptionCriteria
+                    {
+                        StartEtag = user2Etag
+                    });
+
+                    var users = new List<RavenJObject>();
+
+                    using (var subscription = store.Subscriptions.Open(subscriptionId, new SubscriptionConnectionOptions()))
+                    {
+
+                        var docs = new BlockingCollection<RavenJObject>();
+
+                        subscription.Subscribe(docs.Add);
+
+                        RavenJObject doc;
+                        Assert.True(docs.TryTake(out doc, waitForDocTimeout));
+                        users.Push(doc);
+                        Assert.True(docs.TryTake(out doc, waitForDocTimeout));
+                        users.Push(doc);
+                        Assert.True(docs.TryTake(out doc, waitForDocTimeout));
+                        users.Push(doc);
+                        var cnt = users.Count;
+                        Assert.Equal(3, cnt);
+
+                        var keys = new BlockingCollection<string>();
+                        var ages = new BlockingCollection<int>();
+
+                        subscription.Subscribe(x => keys.Add(x[Constants.Metadata].Value<string>("@id")));
+                        subscription.Subscribe(x => ages.Add(x.Value<int>("Age")));
+
+                        string key;
+                        Assert.True(keys.TryTake(out key, waitForDocTimeout));
+                        Assert.Equal("users/3", key);
+
+                        Assert.True(keys.TryTake(out key, waitForDocTimeout));
+                        Assert.Equal("users/4", key);
+
+                        Assert.True(keys.TryTake(out key, waitForDocTimeout));
+                        Assert.Equal("users/5", key);
+
+                        int age;
+                        Assert.True(ages.TryTake(out age, waitForDocTimeout));
+                        Assert.Equal(30, age);
+
+                        Assert.True(ages.TryTake(out age, waitForDocTimeout));
+                        Assert.Equal(29, age);
+
+                        Assert.True(ages.TryTake(out age, waitForDocTimeout));
+                        Assert.Equal(34, age);
+                    }
+                }
+                using (var subscription = store.Subscriptions.Open(subscriptionId, new SubscriptionConnectionOptions()))
+                {
+                    var docs = new BlockingCollection<RavenJObject>();
+
+                    subscription.Subscribe(o => docs.Add(o));
+
+                    RavenJObject item;
+                    Assert.False(docs.TryTake(out item,TimeSpan.FromMilliseconds(250)));
+
+                }
+            }
+        }
+    }
 }
