854441f975b9ab9817583ef9d524e79322d5dc3c
Marcin Lewandowski <marcin@ais.pl>
2015-07-09 06:43:20 +0200

    RavenDB-3605 In conflicts page, allow to resolve all _existing_ conflicts

---
+++ Raven.Studio.Html5/App/commands/database/replication/conflictsResolveCommand.ts
@@ -0,0 +1,56 @@
+import pagedResultSet = require("common/pagedResultSet");
+import commandBase = require("commands/commandBase");
+import database = require("models/resources/database");
+import conflictsInfo = require("models/database/replication/conflictsInfo");
+import appUrl = require("common/appUrl");
+import getOperationStatusCommand = require('commands/operations/getOperationStatusCommand');
+
+class conflictsResolveCommand extends commandBase {
+
+    /**
+	* @param ownerDb The database the collections will belong to.
+	*/
+    constructor(private ownerDb: database, private resolution: string) {
+        super();
+
+        if (!this.ownerDb) {
+            throw new Error("Must specify a database.");
+        }
+    }
+
+    execute(): JQueryPromise<any> {
+        var promise = $.Deferred();
+
+        var url = "/studio-tasks/replication/conflicts/resolve?resolution=" + this.resolution;
+        this.post(url, null, this.ownerDb)
+            .fail((response: JQueryXHR) => {
+                this.reportError("Failed to start conflict resolution!", response.responseText, response.statusText);
+                promise.reject();
+            }).done((result: operationIdDto) => {
+                this.monitorOperation(promise, result.OperationId);
+            });
+
+            return promise;
+    }
+
+
+    private monitorOperation(parentPromise: JQueryDeferred<any>, operationId: number) {
+        new getOperationStatusCommand(this.ownerDb, operationId)
+            .execute()
+            .done((result: operationStatusDto) => {
+            if (result.Completed) {
+                if (result.Faulted) {
+                    this.reportError("Failed to perform conflict resolution!", result.State.Error);
+                    parentPromise.reject();
+                } else {
+                    this.reportSuccess("Conflict resolution was completed");
+                    parentPromise.resolve();
+                }
+            } else {
+                setTimeout(() => this.monitorOperation(parentPromise, operationId), 500);
+            }
+        });
+    }
+}
+
+export = conflictsResolveCommand;
