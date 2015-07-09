ad785ad678dac3d5019bfd421b2aa22a47387b95
Ayende Rahien <ayende@ayende.com>
2015-07-09 22:18:44 +0300

    Reverting change that broke the test

--- Raven.Tests.Core/ChangesApi/WebsocketsTests.cs
+++ Raven.Tests.Core/ChangesApi/WebsocketsTests.cs
@@ -26,7 +26,7 @@ namespace Raven.Tests.Core.ChangesApi
                 using (var clientWebSocket = TryCreateClientWebSocket())
                 {
                     string url = store.Url.Replace("http:", "ws:");
-					url = url + "/traffic-watch/events?" + Guid.NewGuid();
+                    url = url + "/changes/websocket?id=" + Guid.NewGuid();
                     await clientWebSocket.ConnectAsync(new Uri(url), CancellationToken.None);
 
                     var buffer = new byte[1024];
