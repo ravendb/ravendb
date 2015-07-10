a96df4b581551610d06c45431c20cb17fd344fa9
Federico Andres Lois <federico.lois@corvalius.com>
2015-07-10 11:45:48 -0300

    Compact-TRIE  layout.

---
+++ Raven.Voron/Voron/Trees/Compact/PrefixTree.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Voron.Impl;
+using Voron.Impl.FileHeaders;
+
+namespace Voron.Trees.Compact
+{
+    public unsafe partial class PrefixTree
+    {
+        private readonly Transaction _tx;
+        private readonly Slice _treeName;
+        private readonly PrefixTreeMutableState _state = new PrefixTreeMutableState();
+
+        public PrefixTreeMutableState State
+        {
+            get { return _state; }
+        }        
+
+        private PrefixTree(Transaction tx, long root)
+        {
+            _tx = tx;
+            _state.RootPageNumber = root;
+        }
+
+        private PrefixTree(Transaction tx, PrefixTreeMutableState state)
+        {
+            _tx = tx;
+            _state = state;
+        }
+
+        public static PrefixTree Open(Transaction tx, TreeRootHeader* header)
+        {
+            return new PrefixTree(tx, header->RootPageNumber);
+        }
+
+        public static PrefixTree Create(Transaction tx, TreeFlags flags = TreeFlags.None)
+        {
+            var newRootPage = tx.AllocatePage(1, PageFlags.Leaf);
+            var tree = new PrefixTree(tx, newRootPage.PageNumber);
+
+            tree.State.RecordNewPage(newRootPage, 1);
+            return tree;
+        }
+
+        public Slice Name
+        {
+            get { return _treeName; }
+        }
+
+        public void Add(Slice key, Stream value, ushort? version = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Slice key, Slice value, ushort? version = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Slice key, ushort? version = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Slice LastKeyOrDefault()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Slice FirstKeyOrDefault()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Raven.Voron/Voron/Trees/Compact/PrefixTreeMutableState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voron.Trees.Compact
+{
+    public unsafe class PrefixTreeMutableState
+    {
+        public long RootPageNumber;
+        public long PageCount;
+
+        public void RecordNewPage(Page p, int num)
+        {
+            PageCount += num;
+        }
+
+        public void RecordFreedPage(Page p, int num)
+        {
+            PageCount -= num;
+        }
+    }
+}
