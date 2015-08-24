7d0f372621d84fb6dcb837b2561c3c7179a784ec
Ayende Rahien <ayende@ayende.com>
2015-08-24 11:35:01 +0300

    Avoiding expensive RegEx calls if we can skip them cheaply

--- Raven.Abstractions/Spatial/WktSanitizer.cs
+++ Raven.Abstractions/Spatial/WktSanitizer.cs
@@ -23,6 +23,12 @@ namespace Raven.Abstractions.Spatial
 
 		public string Sanitize(string shapeWkt)
 		{
+		    if (string.IsNullOrEmpty(shapeWkt))
+		        return shapeWkt;
+
+            if (char.IsLetter(shapeWkt[0]))
+                return shapeWkt;
+
 			if (RectangleRegex.IsMatch(shapeWkt))
 				return shapeWkt;
 
