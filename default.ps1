9998b4e42cb7a6bed0e065b2de101ff601ae32cd
Arkadiusz Palinski <arek@ais.pl>
2015-08-28 13:38:51 +0200

    RavenDB-3808 Incorporating Rachis tests into the build script.

--- default.ps1
+++ default.ps1
@@ -109,7 +109,8 @@ task Test -depends Compile {
 		"$base_dir\Raven.Tests.MailingList\bin\$global:configuration\Raven.Tests.MailingList.dll", `
 		"$base_dir\Raven.SlowTests\bin\$global:configuration\Raven.SlowTests.dll", `
 		"$base_dir\Raven.Voron\Voron.Tests\bin\$global:configuration\Voron.Tests.dll", `
-		"$base_dir\Raven.DtcTests\bin\$global:configuration\Raven.DtcTests.dll")
+		"$base_dir\Raven.DtcTests\bin\$global:configuration\Raven.DtcTests.dll", `
+		"$base_dir\Rachis\Rachis.Tests\bin\$global:configuration\Rachis.Tests.dll")
 	Write-Host $test_prjs
 	
 	$xUnit = "$lib_dir\xunit\xunit.console.clr4.exe"
