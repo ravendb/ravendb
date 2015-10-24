abb4fcd36f35e597f449dfe899424001442db982
Ayende Rahien <ayende@ayende.com>
2015-10-24 17:17:43 +0200

    We need to split the page when the size the entries require is greater OR equal

--- Raven.Voron/Voron/Trees/Fixed/FixedSizeTree.cs
+++ Raven.Voron/Voron/Trees/Fixed/FixedSizeTree.cs
@@ -376,7 +376,7 @@ namespace Voron.Trees.Fixed
 
         private void AddSeparatorToParentPage(FixedSizeTreePage parentPage, int position, long key, long pageNum)
         {
-            if ((parentPage.NumberOfEntries + 1) * BranchEntrySize > parentPage.PageMaxSpace)
+            if ((parentPage.NumberOfEntries + 1) * BranchEntrySize >= parentPage.PageMaxSpace)
             {
                 parentPage = PageSplit(parentPage, key);
                 System.Diagnostics.Debug.Assert(parentPage != null);
