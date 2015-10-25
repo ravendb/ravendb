9ceeb05c4a781dbeb6edb4598ed464cd0da441f5
Arthur <arthur.peka@outlook.com>
2015-10-25 21:53:19 +0200

    Removing redundant using statements

--- Bundles/Raven.Bundles.Authorization/Controllers/IsAllowedController.cs
+++ Bundles/Raven.Bundles.Authorization/Controllers/IsAllowedController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Security.Cryptography;
 using System.Web.Http;
 using Raven.Abstractions.Data;
 using Raven.Abstractions.Extensions;
