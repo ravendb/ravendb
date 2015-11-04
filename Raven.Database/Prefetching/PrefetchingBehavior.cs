7d3aed147db8cb7d5b56a1d64fb9f5947516de30
Arkadiusz Palinski <arek@ais.pl>
2015-11-04 08:32:42 +0100

    Force FutureBatchCompleted event to be called _after_ the future batch task ends. This way we don't need to wait inside RavenDB_3581.MaybeAddFutureBatchWillFireUpAfterDownloadingSingleDocument test.
    # Conflicts:
    #	Raven.Tests.Issues/Prefetcher/RavenDB_3581.cs

--- Raven.Database/Prefetching/PrefetchingBehavior.cs
+++ Raven.Database/Prefetching/PrefetchingBehavior.cs
@@ -676,7 +676,7 @@ namespace Raven.Database.Prefetching
             };
             Stopwatch sp = Stopwatch.StartNew();
             context.AddFutureBatch(futureBatchStat);
-            futureIndexBatches.TryAdd(nextEtag, new FutureIndexBatch
+            var futureIndexBatch = new FutureIndexBatch
             {
                 StartingEtag = nextEtag,
                 Age = Interlocked.Increment(ref currentIndexingAge),
@@ -745,11 +745,16 @@ namespace Raven.Database.Prefetching
                 }).ContinueWith(t =>
                 {
                     t.AssertNotFailed();
-
-                    FutureBatchCompleted(t.Result.Count);
                     return t.Result;
                 })
+            };
+
+            futureIndexBatch.Task.ContinueWith(t =>
+            {
+                FutureBatchCompleted(t.Result.Count);
             });
+
+            futureIndexBatches.TryAdd(nextEtag, futureIndexBatch);
         }
 
         private Etag GetNextDocEtag(Etag etag)
