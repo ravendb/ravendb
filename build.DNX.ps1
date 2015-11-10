c0291f35d5203d2d062c9bb471fc811b4f7aabea
Pawel Pekrol <ppekrol@ais.pl>
2015-11-10 11:49:14 +0100

    RavenDB-3454 added build scripts for DNX

---
+++ build.DNX.ps1
@@ -0,0 +1,15 @@
+﻿$version = "1.0.0-rc1-16048"
+$architecture = "x86"
+$toolsDir = "Tools\DNX"
+$dnvm = "$toolsDir\dnvm.cmd"
+$runtimeDir = "$env:USERPROFILE\.dnx\runtimes\dnx-coreclr-win-$architecture.$version\bin";
+$dnu = "$runtimeDir\dnu.cmd"
+$dnx = "$runtimeDir\dnx.exe"
+
+&"$dnvm" install $version -u -r coreclr -arch $architecture
+
+&"$dnvm" use $version
+
+&"$dnu" restore
+
+&"$dnx" Raven.Tests.Core Raven.Tests.Core
