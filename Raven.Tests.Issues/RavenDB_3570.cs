743cea7cddb30e6f144c6b9b5bbb6e932b6e34e8
Arkadiusz Palinski <arek@ais.pl>
2015-11-12 13:58:07 +0100

    RavenDB-4019 Changing the test to ensure that connection string is properly parsed and correct values are set as credentials.

--- Raven.Tests.Issues/RavenDB_3570.cs
+++ Raven.Tests.Issues/RavenDB_3570.cs
@@ -7,60 +7,31 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Raven.Abstractions.Connection;
 using Raven.Abstractions.Data;
+using Raven.Client.FileSystem;
 using Raven.Database.Server.Security.Windows;
 using Raven.Json.Linq;
 using Raven.Server;
-using Raven.Tests.Common.Attributes;
-using Raven.Tests.Helpers;
 using Xunit;
 
 namespace Raven.Tests.Issues
 {
-    public class RavenDB_3570 : RavenFilesTestBase
+
+    public class RavenDB_3570
     {
-        public RavenDB_3570()
-        {
-            FactIfWindowsAuthenticationIsAvailable.LoadCredentials();
-        }
-
-        protected override void ConfigureServer(RavenDbServer server, string fileSystemName)
-        {
-                        server.SystemDatabase.Documents.Put("Raven/Authorization/WindowsSettings", null,
-                                                  RavenJObject.FromObject(new WindowsAuthDocument
-                                                  {
-                                                      RequiredUsers = new List<WindowsAuthData>
-                                                      {
-                                                          new WindowsAuthData
-                                                          {
-                                                              Name = string.Format("{0}\\{1}", null, FactIfWindowsAuthenticationIsAvailable.Username),
-                                                              Enabled = true,
-                                                              Databases = new List<ResourceAccess>
-                                                              {
-                                                                  new ResourceAccess {TenantId = Constants.SystemDatabase, Admin = true}, // required to create file system
-                                                                  new ResourceAccess {TenantId = fileSystemName}
-                                                              }
-                                                          }
-                                                      }
-                                                  }), new RavenJObject(), null);
-        }
-
-        //requires admin context
         [Fact]
-        public void RavenFSWithWindowsCredentialsInConnectionStringShouldWork()
+        public void CredentialsAreLoadedFromConnectionString()
         {
-            this.Invoking(x =>
+            using (var store = new FilesStore()
             {
-                using (NewStore(enableAuthentication: true, connectionStringName: "RavenFS"))
-                {
-                }
-            }).ShouldThrow<ErrorResponseException>().Where(x => x.StatusCode == HttpStatusCode.Unauthorized);
+                ConnectionStringName = "RavenFS"
+            })
+            {
+                var credentials = (NetworkCredential)store.Credentials;
 
-            this.Invoking(x =>
-            {
-                using (NewStore(enableAuthentication: true))
-                {
-                }
-            }).ShouldNotThrow<Exception>();
+                Assert.Equal("local_user_test", credentials.UserName);
+                Assert.Equal("local_user_test", credentials.Password);
+                Assert.Equal(string.Empty, credentials.Domain);
+            }
         }
     }
 }
