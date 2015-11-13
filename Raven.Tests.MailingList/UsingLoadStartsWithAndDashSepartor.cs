1d7bdba92b7d4c93f6bb34efaf77e9cdcdf9ef91
Ayende Rahien <ayende@ayende.com>
2015-11-13 23:46:06 +0200

    Making sure to compare (in code) in the same manner as we compare in Esent for starts with

---
+++ Raven.Tests.MailingList/UsingLoadStartsWithAndDashSepartor.cs
@@ -0,0 +1,38 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="UsingLoadStartsWithAndDashSepartor.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using System.Threading;
+using Raven.Json.Linq;
+using Raven.Tests.Common;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Raven.Tests.MailingList
+{
+    public class UsingLoadStartsWithAndDashSeperator : RavenTest
+    {
+        [Theory]
+        [PropertyData("Storages")]
+        public void ShouldWork(string storage)
+        {
+            using (var store = NewDocumentStore(requestedStorage: storage))
+            {
+
+
+                //store.DocumentDatabase.Documents.Put("delivery-ORT0529.2", null, new RavenJObject(),
+                //  new RavenJObject(), null);
+                store.DocumentDatabase.Documents.Put("DeliveryReport-1000", null, new RavenJObject(),
+                    new RavenJObject(), null);
+
+                store.DocumentDatabase.Documents.Put("delivery-RT0753.2", null, new RavenJObject(),
+                  new RavenJObject(), null);
+
+                int nextStart = 0;
+                var docs = store.DocumentDatabase.Documents.GetDocumentsWithIdStartingWith("delivery-", null, null,0, 1024, CancellationToken.None, ref nextStart);
+                Assert.Equal(1, docs.Length);
+            }
+        }
+    }
+}
