ab56453d93ecf026c084b232318393195c4fe0d2
Pawel Pekrol <ppekrol@ais.pl>
2015-11-19 11:39:24 +0100

    RDBQA-45 introduced Admin and User in FactIfWindowsAuthenticationIsAvailable

--- Raven.Tests.Common/Attributes/FactIfWindowsAuthenticationIsAvailable.cs
+++ Raven.Tests.Common/Attributes/FactIfWindowsAuthenticationIsAvailable.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Text;
 using Xunit;
 
@@ -7,47 +8,40 @@ namespace Raven.Tests.Common.Attributes
 {
     public static class FactIfWindowsAuthenticationIsAvailable
     {
-        private static bool triedLoading = false;
+        private readonly static SkipException SkipException = new SkipException("Cannot execute this test, because this test rely on actual Windows Account name / password.");
 
-        public static string Username { get; private set; }
-        public static string Domain { get; private set; }
-        public static string Password { get; private set; }
+        private static bool triedLoading;
 
-        public static string InvalidUsername { get; private set; }
-        public static string InvalidPassword { get; private set; }
-        public static string InvalidDomain { get; private set; }
+        public static NetworkCredential Admin { get; private set; }
+
+        public static NetworkCredential User { get; private set; }
 
         public static void LoadCredentials()
         {
-            if (Username != null)
+            if (Admin != null)
                 return;
 
-            var skipException = new SkipException("Cannot execute this test, because this test rely on actual Windows Account name / password.");
             if (triedLoading)
-                throw skipException;
+                throw SkipException;
 
             lock (typeof (FactIfWindowsAuthenticationIsAvailable))
             {
                 triedLoading = true;
                 ActualLoad();
-                if (Username == null)
-                    throw skipException;
+                if (Admin == null || string.IsNullOrEmpty(Admin.UserName))
+                    throw SkipException;
             }
         }
 
         [Conditional("DEBUG")]
         private static void LoadDebugCredentials()
         {
-            Username = "local_user_test";
-            Password = "local_user_test";
-            Domain = "local_machine_name_test";
-            InvalidUsername = "local_machine_invalid_user";
-            InvalidPassword = "local_machine_invalid_user";
-            InvalidDomain = "local_machine_name_test";
+            Admin = new NetworkCredential("local_user_test", "local_user_test", "local_machine_name_test");
+            User = new NetworkCredential("local_machine_invalid_user", "local_machine_invalid_user", "local_machine_name_test");
 
-            if (Username == "local_user_test")
+            if (Admin.UserName == "local_user_test")
             {
-                Username = null;
+                Admin = null;
             }
         }
 
@@ -63,30 +57,32 @@ namespace Raven.Tests.Common.Attributes
                 return;
             }
 
+            Admin = new NetworkCredential();
             var lines = File.ReadAllLines(path, Encoding.UTF8);
             var username = lines[0].Split('\\');
             if (username.Length > 1)
             {
-                Domain = username[0];
-                Username = username[1];
+                Admin.Domain = username[0];
+                Admin.UserName = username[1];
             }
             else
             {
-                Username = username[0];
+                Admin.UserName = username[0];
             }
-            Password = lines[1];
+            Admin.Password = lines[1];
 
+            User = new NetworkCredential();
             username = lines[2].Split('\\');
             if (username.Length > 1)
             {
-                InvalidDomain = username[0];
-                InvalidUsername = username[1];
+                User.Domain = username[0];
+                User.UserName = username[1];
             }
             else
             {
-                InvalidUsername = username[0];
+                User.UserName = username[0];
             }
-            InvalidPassword = lines[3];
+            User.Password = lines[3];
         }
     }
 }
