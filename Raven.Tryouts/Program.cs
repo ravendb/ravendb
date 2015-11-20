5c3296dbe1984d3242309b8997381f48d36b41ec
Pawel Pekrol <ppekrol@ais.pl>
2015-11-20 10:26:31 +0100

    RavenDB-3454 build fixes

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -1,19 +1,9 @@
-using System;
-
-using Raven.Tests.FileSystem.ClientApi;
-
 namespace Raven.Tryouts
 {
     public class Program
     {
         public static void Main(string[] args)
-                Console.Clear();
-                Console.WriteLine(i);
-                using (var x = new FileSessionListenersTests())
-                {
-                    x.ConflictListeners_RemoteVersion().Wait();
-            }
+        {
         }
     }
-}
 }
