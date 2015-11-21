01e8f5039465b876801c9c7ecea6c522423b196e
Pawel Pekrol <ppekrol@ais.pl>
2015-11-21 19:25:55 +0100

    RDBQA-45 added new client convention "AuthenticationScheme"

--- Raven.Client.Lightweight/Connection/Implementation/HttpJsonRequest.cs
+++ Raven.Client.Lightweight/Connection/Implementation/HttpJsonRequest.cs
@@ -108,10 +108,33 @@ namespace Raven.Client.Connection.Implementation
             conventions = requestParams.Convention;
 
             recreateHandler = factory.httpMessageHandler ?? (
-                () => new WebRequestHandler
+                () =>
                 {
-                    UseDefaultCredentials = _credentials != null && _credentials.HasCredentials() == false,
-                    Credentials = _credentials != null ? _credentials.Credentials : null,
+                    var useDefaultCredentials = _credentials != null && _credentials.HasCredentials() == false;
+                    ICredentials credentialsToUse = null;
+                    if (_credentials != null)
+                    {
+                        var networkCredentials = _credentials.Credentials as NetworkCredential;
+                        if (networkCredentials != null && factory.authenticationScheme != null)
+                        {
+                            var credentialCache = new CredentialCache();
+                            var uri = new Uri(requestParams.Url);
+                            credentialCache.Add(new Uri(string.Format("{0}://{1}:{2}/", uri.Scheme, uri.Host, uri.Port)), factory.authenticationScheme, networkCredentials);
+
+                            credentialsToUse = credentialCache;
+                        }
+                        else
+                        {
+                            credentialsToUse = _credentials.Credentials;
+                        }
+                    }      
+
+                    var handler = new WebRequestHandler
+                    {
+                        UseDefaultCredentials = useDefaultCredentials,
+                        Credentials = credentialsToUse
+                    };
+                    return handler;
                 }
             );
 
