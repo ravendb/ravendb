f05547a2bf1d16eccd4ff53f161976dbaf6f455f
Michael Yarichuk <michael.yarichuk@gmail.com>
2015-11-30 16:30:29 +0200

    new endpoint to generate oauth certificate

---
+++ Raven.Database/Server/Responders/Admin/AdminOAuthCertificateResponder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+using Raven.Database.Extensions;
+using Raven.Database.Server.Abstractions;
+
+namespace Raven.Database.Server.Responders.Admin
+{
+    public class AdminOAuthCertificateResponder : AbstractRequestResponder
+    {
+        public override string UrlPattern
+        {
+            get
+            {
+                return "^/admin/generate-oauth-certificate";
+            }
+        }
+
+        public override string[] SupportedVerbs
+        {
+            get
+            {
+                return new[] { "GET" };
+            }
+        }
+
+        public override void Respond(IHttpContext context)
+        {
+            string certificate;
+            using (var rsa = new RSACryptoServiceProvider())
+                certificate = Convert.ToBase64String(rsa.ExportCspBlob(true));
+            context.Response.AddHeader("content-disposition", "attachment; filename=oauth-certificate.txt");
+            context.Response.ContentType = "application/octet-stream";
+            context.Write(certificate);
+        }
+    }
+}
