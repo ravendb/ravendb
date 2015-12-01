45e2db0217b604a73e803d146d7c3d8931d4dae3
Pawel Pekrol <ppekrol@ais.pl>
2015-12-01 12:28:28 +0100

    RavenDB-4077 moved DevelopmentHelper.TimeBomb one month further

--- Raven.Database/Util/DevelopmentHelper.cs
+++ Raven.Database/Util/DevelopmentHelper.cs
@@ -13,7 +13,7 @@ namespace Raven.Database.Util
     {
         public static void TimeBomb()
         {
-            if (SystemTime.UtcNow > new DateTime(2015, 12, 1))
+            if (SystemTime.UtcNow > new DateTime(2016, 1, 1))
                 throw new NotImplementedException("Development time bomb.");
         }
     }
