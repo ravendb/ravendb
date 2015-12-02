5dc97316aaed0e45a7e0ad5ff1e56cb468d0c96b
Pawel Pekrol <ppekrol@ais.pl>
2015-12-02 09:25:26 +0100

    RDBQA-45 applying external configuration to all windows authentication tests

--- Raven.Client.Lightweight/Document/DocumentConvention.cs
+++ Raven.Client.Lightweight/Document/DocumentConvention.cs
@@ -95,7 +95,6 @@ namespace Raven.Client.Document
             ShouldSaveChangesForceAggressiveCacheCheck = true;
             IndexAndTransformerReplicationMode = IndexAndTransformerReplicationMode.Indexes | IndexAndTransformerReplicationMode.Transformers;
             AcceptGzipContent = true;
-            AuthenticationScheme = null;
         }
 
         private IEnumerable<object> DefaultApplyReduceFunction(
@@ -684,8 +683,6 @@ namespace Raven.Client.Document
         public bool PreserveDocumentPropertiesNotFoundOnModel { get; set; }
 
         public bool AcceptGzipContent { get; set; }
-
-        public string AuthenticationScheme { get; set; }
 
         public delegate bool TryConvertValueForQueryDelegate<in T>(string fieldName, T value, QueryValueConvertionType convertionType, out string strValue);
 
