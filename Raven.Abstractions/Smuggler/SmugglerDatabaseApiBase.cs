d81730bc0c66f3342df7187eeb248a7a7760805b
aviviadi <avivi.adi.hr@gmail.com>
2015-12-28 14:30:57 +0200

    RavenDB-4104: [v3.0] Merge from 3.5 - Prevent Export Timeout

--- Raven.Abstractions/Smuggler/SmugglerDatabaseApiBase.cs
+++ Raven.Abstractions/Smuggler/SmugglerDatabaseApiBase.cs
@@ -530,6 +530,9 @@ namespace Raven.Abstractions.Smuggler
             var reportInterval = TimeSpan.FromSeconds(2);
             var reachedMaxEtag = false;
             Operations.ShowProgress("Exporting Documents");
+            int numberOfSkippedDocs = 0;
+            var lastForcedFlush = SystemTime.UtcNow;
+            var maxFlushInterval = TimeSpan.FromSeconds(1);
 
             while (true)
             {
@@ -544,6 +547,14 @@ namespace Raven.Abstractions.Smuggler
                         {
                             while (await documents.MoveNextAsync())
                             {
+                                if (numberOfSkippedDocs > 0 && (SystemTime.UtcNow - lastForcedFlush) > maxFlushInterval)
+                                {
+                                    Operations.ShowProgress("Skipped {0:#,#} documents", numberOfSkippedDocs);
+                                    jsonWriter.WriteWhitespace(" ");
+                                    jsonWriter.Flush();
+                                    lastForcedFlush = SystemTime.UtcNow;
+                                    numberOfSkippedDocs = 0;
+                                }
                                 if (Options.ExportDeletions && updateLastDocDeleteEtag != null)
                                     updateLastDocDeleteEtag();
 
@@ -562,17 +573,26 @@ namespace Raven.Abstractions.Smuggler
                                 lastEtag = tempLastEtag;
 
                                 if (!Options.MatchFilters(document))
+                                {
+                                    numberOfSkippedDocs++;
                                     continue;
+                                }
 
                                 if (Options.ShouldExcludeExpired && Options.ExcludeExpired(document, now))
+                                {
+                                    numberOfSkippedDocs++;
                                     continue;
+                                }
 
                                 if (string.IsNullOrEmpty(Options.TransformScript) == false)
                                     document = await Operations.TransformDocument(document, Options.TransformScript);
 
                                 // If document is null after a transform we skip it. 
                                 if (document == null)
+                                {
+                                    numberOfSkippedDocs++;
                                     continue;
+                                }
 
                                 try
                                 {
