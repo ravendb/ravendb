42051c2f4caf285c565f03a527be6d084b31bd03
Ayende Rahien <ayende@ayende.com>
2015-12-31 15:06:40 +0200

    RavenDB-4155 - Avoiding O(N**2) computation when flushing indexes

--- Raven.Database/Indexing/IndexStorage.cs
+++ Raven.Database/Indexing/IndexStorage.cs
@@ -1416,18 +1416,22 @@ namespace Raven.Database.Indexing
         {
             if (indexes == null)
                 return;
-
-            var indexesToFlush = indexes.Values.Where(value => value != null && !value.IsMapReduce);
-            FlushIndexesInternal(indexesToFlush);
+            foreach (var index in indexes)
+            {
+                if (index.Value.IsMapReduce == false)
+                    FlushIndex(index.Value);
+            }
         }
 
         public void FlushReduceIndexes()
         {
             if (indexes == null)
                 return;
-
-            var indexesToFlush = indexes.Values.Where(value => value != null && value.IsMapReduce);
-            FlushIndexesInternal(indexesToFlush);
+            foreach (var index in indexes)
+            {
+                if (index.Value.IsMapReduce)
+                    FlushIndex(index.Value);
+            }
         }
 
         public void FlushIndexes(HashSet<int> indexIds)
@@ -1435,33 +1439,36 @@ namespace Raven.Database.Indexing
             if (indexes == null || indexIds.Count == 0)
                 return;
 
-            var indexesToFlush = indexes.Values.Where(x => indexIds.Contains(x.IndexId));
-            FlushIndexesInternal(indexesToFlush);
+            foreach (var indexId in indexIds)
+            {
+                Index value;
+                if(indexes.TryGetValue(indexId, out value))
+                    FlushIndex(value);
+            }
         }
 
-        private static void FlushIndexesInternal(IEnumerable<Index> indexesToFlush)
+     
+
+        private static void FlushIndex(Index value)
         {
-            foreach (var value in indexesToFlush)
+            var sp = Stopwatch.StartNew();
+
+            try
             {
-                var sp = Stopwatch.StartNew();
+                value.Flush(value.GetLastEtagFromStats());
+            }
+            catch (Exception e)
+            {
+                value.IncrementWriteErrors(e);
+                log.WarnException(string.Format("Failed to flush {0} index: {1} (id: {2})",
+                    GetIndexType(value.IsMapReduce), value.PublicName, value.IndexId), e);
+                throw;
+            }
 
-                try
-                {
-                    value.Flush(value.GetLastEtagFromStats());
-                }
-                catch (Exception e)
-                {
-                    value.IncrementWriteErrors(e);
-                    log.WarnException(string.Format("Failed to flush {0} index: {1} (id: {2})",
-                        GetIndexType(value.IsMapReduce), value.PublicName, value.IndexId), e);
-                    throw;
-                }
-
-                if (log.IsDebugEnabled)
-                {
-                    log.Debug("Flashed {0} index: {1} (id: {2}), took {3}ms",
-                        GetIndexType(value.IsMapReduce), value.PublicName, value.IndexId, sp.ElapsedMilliseconds);
-                }
+            if (log.IsDebugEnabled)
+            {
+                log.Debug("Flashed {0} index: {1} (id: {2}), took {3}ms",
+                    GetIndexType(value.IsMapReduce), value.PublicName, value.IndexId, sp.ElapsedMilliseconds);
             }
         }
 
