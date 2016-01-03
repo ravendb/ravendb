baaa5d7e5785ad493d5798633ad7377ac7380a41
Tal Weiss <tal@ayende.com>
2016-01-03 17:19:05 +0200

    RavenDB-4125 (raft tests) porting from 3.5, updating leader node according to newest topology

--- Raven.Client.Lightweight/Connection/Request/ClusterAwareRequestExecuter.cs
+++ Raven.Client.Lightweight/Connection/Request/ClusterAwareRequestExecuter.cs
@@ -370,7 +370,8 @@ namespace Raven.Client.Connection.Request
                         if (newestTopology != null)
                         {
                             Nodes = GetNodes(newestTopology.Node, newestTopology.Task.Result);
-                            LeaderNode = GetLeaderNode(Nodes);
+                            LeaderNode = newestTopology.Task.Result.ClusterInformation.IsLeader ?
+                                Nodes.FirstOrDefault(n => n.Url == newestTopology.Node.Url) : null;
 
                             ReplicationInformerLocalCache.TrySavingClusterNodesToLocalCache(serverHash, Nodes);
 
