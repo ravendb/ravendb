4b720e925e938f2780527896710b0385e200f2d2
Ayende Rahien <ayende@ayende.com>
2016-01-12 00:45:44 +0200

    Fixing the json

--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -12,37 +12,32 @@ namespace Tryouts
     {
         public static void Main(string[] args)
         {
-            WriteToStreamBenchmark.PerformanceAnalysis(@"C:\Work\JSON\Big", "output.csv", 2);
-            Console.WriteLine("Reallying starting now...");
-            WriteToStreamBenchmark.PerformanceAnalysis(@"C:\Work\JSON\Big", "output.csv", int.MaxValue);
+            //WriteToStreamBenchmark.PerformanceAnalysis(@"C:\Work\JSON\Big", "output.csv", 2);
+            //Console.WriteLine("Reallying starting now...");
+            //WriteToStreamBenchmark.PerformanceAnalysis(@"C:\Work\JSON\Big", "output.csv", int.MaxValue);
 
-            WriteToStreamBenchmark.ManySmallDocs(@"C:\Work\JSON\Lines");
-            Console.WriteLine("Reallying starting now...");
-            WriteToStreamBenchmark.ManySmallDocs(@"C:\Work\JSON\Lines");
+            //WriteToStreamBenchmark.ManySmallDocs(@"C:\Work\JSON\Lines");
+            //Console.WriteLine("Reallying starting now...");
+            //WriteToStreamBenchmark.ManySmallDocs(@"C:\Work\JSON\Lines");
 
             //new FunctionalityTests().LongStringsTest(1000);
 
-            //foreach (var sample in UnmanageJsonReaderTests.Samples())
-            //{
-            //    var f = (string) sample[0];
-            //    if (f.Contains("escape-str") == false) continue;
-            //    Console.WriteLine(f);
-            //    try
-            //    {
-            //        new BlittableFormatTests().CheckRoundtrip(f);
-            //    }
-            //    catch (Exception e)
-            //    {
-            //        Console.ForegroundColor = ConsoleColor.Red;
-            //        Console.WriteLine(e);
-            //        Console.ResetColor();
-            //    }
-            //}
-
-            Console.WriteLine();
-
-            Console.WriteLine("{0:#,#}", Process.GetCurrentProcess().PeakWorkingSet64 / 1024);
-            Console.ReadLine();
+            foreach (var sample in UnmanageJsonReaderTests.Samples())
+            {
+                var f = (string)sample[0];
+                if (f.Contains("escape-str") == false) continue;
+                Console.WriteLine(f);
+                try
+                {
+                    new BlittableFormatTests().CheckRoundtrip(f);
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e);
+                    Console.ResetColor();
+                }
+            }
         }
     }
 }
