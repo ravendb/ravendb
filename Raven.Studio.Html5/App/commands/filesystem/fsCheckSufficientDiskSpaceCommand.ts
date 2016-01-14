39f8f94ddbe6efbcb29f3751f184ec690d55a7b4
Tal Weiss <tal@ayende.com>
2016-01-14 16:16:30 +0200

    RavenDB-3660_v4 * added disk space check for fs import * now making sure we are left with atleast 10% of the disk space during the import

---
+++ Raven.Studio.Html5/App/commands/filesystem/fsCheckSufficientDiskSpaceCommand.ts
@@ -0,0 +1,21 @@
+﻿import commandBase = require("commands/commandBase");
+import filesystem = require("models/filesystem/filesystem");
+
+class fsCheckSufficientDiskSpaceCommand extends commandBase {
+
+    constructor(private fileSize: number, private filesystem: filesystem) {
+        super();
+    }
+
+    execute(): JQueryPromise<any> {
+        var args = {
+            fileSize: this.fileSize
+        }
+        var url = "/studio-tasks/check-sufficient-diskspace";
+        var checkTask = this.query(url, args, this.filesystem, undefined, { dataType: undefined });
+
+        return checkTask;
+    }
+}
+
+export = fsCheckSufficientDiskSpaceCommand; 
