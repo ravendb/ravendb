b0e114b2f54b43a36801d423d7d1819ea22d6350
Arkadiusz Palinski <arek@ravendb.net>
2016-01-18 11:54:23 +0100

    RavenDB-4224 Exposing file headers streaming under advanced session operations API.

--- Raven.Client.Lightweight/FileSystem/AsyncFilesSession.cs
+++ Raven.Client.Lightweight/FileSystem/AsyncFilesSession.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Raven.Abstractions.Data;
 using Raven.Abstractions.Util;
 
 namespace Raven.Client.FileSystem
@@ -163,6 +164,13 @@ namespace Raven.Client.FileSystem
 
             if (this.conflictCacheRemoval != null)
                 this.conflictCacheRemoval.Dispose();
+        }
+
+        public Task<IAsyncEnumerator<FileHeader>> StreamFileHeadersAsync(Etag fromEtag, int pageSize = int.MaxValue)
+        {
+            IncrementRequestCount();
+
+            return Commands.StreamFileHeadersAsync(fromEtag, pageSize);
         }
 
         public Task<IAsyncEnumerator<FileHeader>> StreamQueryAsync(IAsyncFilesQuery<FileHeader> query)
