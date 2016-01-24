6ba2c657a27ccd255dbb854e1c61135b05c3d92e
Tal Weiss <tal@ayende.com>
2016-01-24 13:59:49 +0200

    RavenDB-4243 OEM/ISV License Validations should not impact startup performance

--- Raven.Database/Rhino.Licensing/AbstractLicenseValidator.cs
+++ Raven.Database/Rhino.Licensing/AbstractLicenseValidator.cs
@@ -243,10 +243,10 @@ namespace Rhino.Licensing
         /// <summary>
         /// Validates loaded license
         /// </summary>
-        public virtual void AssertValidLicense(Action onValidLicense, bool turnOffDiscoveryClient = false)
+		public virtual void AssertValidLicense(Action onValidLicense, bool turnOffDiscoveryClient = false, bool firstTime = false)
         {
             LicenseAttributes.Clear();
-            if (IsLicenseValid())
+			if (IsLicenseValid(firstTime))
             {
                 onValidLicense();
 
@@ -286,7 +286,7 @@ namespace Rhino.Licensing
             throw new LicenseNotFoundException("Could not find a valid license.");
         }
 
-        private bool IsLicenseValid()
+        private bool IsLicenseValid(bool firstTime = false)
         {
             try
             {
@@ -304,12 +304,12 @@ namespace Rhino.Licensing
 
                 bool result;
                 if (LicenseType == LicenseType.Subscription)
-                    result = ValidateLicense();
+					result = ValidateLicense(firstTime);
                 else
                 {
                     result = SystemTime.UtcNow < ExpirationDate;
                     if (result)
-                        result = ValidateLicense();
+						result = ValidateLicense(firstTime);
                 }
 
                 if (result && IsOemLicense())
@@ -333,12 +333,13 @@ namespace Rhino.Licensing
             }
         }
 
-        private bool ValidateLicense()
+        private bool ValidateLicense(bool firstTime = false)
         {
+
             if ((ExpirationDate - SystemTime.UtcNow).TotalDays > 4)
                 return true;
 
-            if (currentlyValidatingLicense)
+			if (currentlyValidatingLicense || firstTime)
                 return IsOemLicense() || SystemTime.UtcNow < ExpirationDate;
 
             if (SubscriptionEndpoint == null)
