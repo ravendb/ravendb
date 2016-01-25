7dfa215b5e533ba6707dfcafddd17926743a38e5
Michael Yarichuk <michael.yarichuk@gmail.com>
2016-01-25 16:20:53 +0200

    refactoring to allow backup for in-memory RavenFS instance (RavenDB-4240)

---
+++ Raven.Tests.FileSystem/Issues/RavenDB_4189.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Raven.Database.Extensions;
+using Raven.Tests.Helpers;
+using Xunit;
+
+namespace Raven.Tests.FileSystem.Issues
+{
+    public class RavenDB_4189 : RavenFilesTestBase
+    {
+        
+    }
+}
