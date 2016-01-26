d86c122e33ee9d5444605806889f6fdbf93e8efb
Fitzchak Yitzchaki <fitzchak@ayende.com>
2016-01-26 16:09:35 +0200

    Add studio handler

--- src/Raven.Server/Web/System/Documents.cs
+++ src/Raven.Server/Web/System/Documents.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-//  <copyright file="BuildVersion.cs" company="Hibernating Rhinos LTD">
+//  <copyright file="Documents.cs" company="Hibernating Rhinos LTD">
 //      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 //  </copyright>
 // -----------------------------------------------------------------------
