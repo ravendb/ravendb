e3b77d04e7f3ab376e7628991f39e1dc5d7f5b83
Fitzchak Yitzchaki <fitzchak@ayende.com>
2016-01-28 13:07:53 +0200

    Rename classes

---
+++ src/Raven.Server/ServerWide/LowMemoryNotification/LowMemoryNotification.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Raven.Abstractions.Logging;
+using Sparrow.Collections;
+
+namespace Raven.Server.ServerWide.LowMemoryNotification
+{
+    public class LowMemoryHandlerStatistics
+    {
+        public string Name { get; set; }
+        public long EstimatedUsedMemory { get; set; }
+        public string DatabaseName { get; set; }
+        public object Metadata { get; set; }
+    }
+    internal interface ILowMemoryHandler
+    {
+        void HandleLowMemory();
+        void SoftMemoryRelease();
+        LowMemoryHandlerStatistics GetStats();
+    }
+
+    public class LowMemoryNotification
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(WinLowMemoryNotification));
+
+        private static readonly ConcurrentSet<WeakReference<ILowMemoryHandler>> LowMemoryHandlers = new ConcurrentSet<WeakReference<ILowMemoryHandler>>();
+
+        protected void RunLowMemoryHandlers()
+        {
+            var inactiveHandlers = new List<WeakReference<ILowMemoryHandler>>();
+
+            foreach (var lowMemoryHandler in LowMemoryHandlers)
+            {
+                ILowMemoryHandler handler;
+                if (lowMemoryHandler.TryGetTarget(out handler))
+                {
+                    try
+                    {
+                        handler.HandleLowMemory();
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error("Failure to process low memory notification (low memory handler - " + handler + ")", e);
+                    }
+                }
+                else
+                    inactiveHandlers.Add(lowMemoryHandler);
+            }
+
+            inactiveHandlers.ForEach(x => LowMemoryHandlers.TryRemove(x));
+        }
+    }
+}
---
+++ src/Raven.Server/ServerWide/LowMemoryNotification/WinLowMemoryNotification.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Raven.Abstractions.Logging;
+
+namespace Raven.Server.ServerWide.LowMemoryNotification
+{
+    public class WinLowMemoryNotification : LowMemoryNotification
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(WinLowMemoryNotification));
+
+        private const int LowMemoryResourceNotification = 0;
+
+        const uint WAIT_FAILED = 0xFFFFFFFF;
+        const uint WAIT_TIMEOUT = 0x00000102;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr CreateMemoryResourceNotification(int notificationType);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool QueryMemoryResourceNotification(IntPtr hResNotification, out bool isResourceStateMet);
+
+        [DllImport("kernel32.dll")]
+        private static extern uint WaitForMultipleObjects(uint nCount, IntPtr[] lpHandles, bool bWaitAll, uint dwMilliseconds);
+
+        [DllImport("Kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Unicode)]
+        private static extern IntPtr CreateEvent(IntPtr lpEventAttributes, bool bManualReset, bool bInitialState, string lpName);
+
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        public static extern bool SetEvent(IntPtr hEvent);
+
+        [DllImport("Kernel32.dll")]
+        public static extern IntPtr GetCurrentProcess();
+
+        private readonly IntPtr lowMemorySimulationEvent;
+        private readonly IntPtr lowMemoryNotificationHandle;
+        private readonly IntPtr softMemoryReleaseEvent;
+
+        public WinLowMemoryNotification(CancellationToken shutdownNotification)
+        {
+            lowMemorySimulationEvent = CreateEvent(IntPtr.Zero, false, false, null);
+            lowMemoryNotificationHandle = CreateMemoryResourceNotification(LowMemoryResourceNotification); // the handle will be closed by the system if the process terminates
+
+            var appDomainUnloadEvent = CreateEvent(IntPtr.Zero, false, false, null);
+            shutdownNotification.Register(() => SetEvent(appDomainUnloadEvent));
+
+            softMemoryReleaseEvent = CreateEvent(IntPtr.Zero, false, false, null);
+
+            if (lowMemoryNotificationHandle == null)
+            {
+                Log.Error("lowMemoryNotificationHandle is null. might be because of permissions issue.");
+                throw new Win32Exception();
+            }
+
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    var waitForResult = WaitForMultipleObjects(4,
+                        new[] {lowMemoryNotificationHandle, appDomainUnloadEvent, lowMemorySimulationEvent, softMemoryReleaseEvent}, false,
+                        5*60*1000);
+
+                    switch (waitForResult)
+                    {
+                        case 0: // lowMemoryNotificationHandle
+                            Log.Warn("Low memory detected, will try to reduce memory usage...");
+                            RunLowMemoryHandlers();
+                            break;
+                        case 1:
+                            // app domain unload
+                            return;
+                        case 2: // LowMemorySimulationEvent
+                            Log.Warn("Low memory simulation, will try to reduce memory usage...");
+                            RunLowMemoryHandlers();
+                            break;
+                        case 3: // SoftMemoryReleaseEvent
+                            Log.Warn("Releasing memory before Garbage Collection operation");
+                            RunLowMemoryHandlers();
+                            break;
+                        case WAIT_TIMEOUT:
+                            ClearInactiveHandlers();
+                            break;
+                        case WAIT_FAILED:
+                            Log.Warn("Failure when trying to wait for low memory notification. No low memory notifications will be raised.");
+                            break;
+                    }
+
+                    Thread.Sleep(TimeSpan.FromSeconds(60)); // prevent triggering the event oto frequent when the low memory notification object is in the signaled state
+                }
+            })
+            {
+                IsBackground = true,
+                Name = "Low memory notification thread"
+            }.Start();
+        }
+    }
+}
