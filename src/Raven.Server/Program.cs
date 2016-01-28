56015e3d58ae084d131b3d342fd53fa0422d6ea2
Fitzchak Yitzchaki <fitzchak@ayende.com>
2016-01-28 13:06:15 +0200

    Fix merge

--- src/Raven.Server/Program.cs
+++ src/Raven.Server/Program.cs
@@ -16,7 +16,7 @@ namespace Raven.Server
 {
     public class Program
     {
-        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+        private static readonly ILog Log = LogManager.GetLogger(typeof (Program));
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -34,10 +34,10 @@ namespace Raven.Server
         public static int Main(string[] args)
         {
             var configBuilder = new ConfigurationBuilder()
-               .AddJsonFile("settings.json", optional: true)
-               .AddEnvironmentVariables(prefix: "RAVEN_");
+                .AddJsonFile("settings.json", optional: true)
+                .AddEnvironmentVariables(prefix: "RAVEN_");
 
-         
+
             if (args != null)
             {
                 configBuilder.AddCommandLine(args);
@@ -46,82 +46,83 @@ namespace Raven.Server
             var config = configBuilder.Build();
 
             WelcomeMessage.Print();
-           
+
             var sp = Stopwatch.StartNew();
 
-            ServerStore serverStore;
-            try
-            {
-                serverStore = new ServerStore(config);
-                serverStore.Initialize();
-            }
-            catch (Exception e)
-            {
-                Log.FatalException("Could not open the server store", e);
-                Console.WriteLine(e);
-                return -1;
-            }
-
-            if (Log.IsDebugEnabled)
-            {
-                Log.Debug("Server store started took {0:#,#;;0} ms", sp.ElapsedMilliseconds);
-            }
-            sp.Restart();
-            IHostingEngine application;
-            try
-            {
-                application = new WebHostBuilder(config)
-                    .UseStartup<Program>()
-                    .UseServer("Microsoft.AspNet.Server.Kestrel")
-                    .UseServices(services => services.AddInstance(serverStore))
-                    .Build();
-            }
-            catch (Exception e)
-            {
-                Log.FatalException("Could not setup server", e);
-                Console.WriteLine(e);
-                return -2;
-            }
-
-
-            if (Log.IsDebugEnabled)
-            {
-                Log.Debug("Configuring HTTP server took {0:#,#;;0} ms", sp.ElapsedMilliseconds);
-            }
-
-
-            IApplication app = null;
-            try
+            using (var serverStore = new ServerStore(config))
             {
                 try
                 {
-                    app = application.Start();
+                    serverStore.Initialize();
                 }
                 catch (Exception e)
                 {
-                    Log.FatalException("Could not start server", e);
+                    Log.FatalException("Could not open the server store", e);
                     Console.WriteLine(e);
-                    return -3;
+                    return -1;
                 }
 
-                Console.WriteLine("Server started, listening to requests...");
-
-                //TODO: Move the command line options to here
-                while (true)
+                if (Log.IsDebugEnabled)
                 {
-                    if (Console.ReadLine() == "q")
-                        break;
-
-                   // Console.ForegroundColor++;
+                    Log.Debug("Server store started took {0:#,#;;0} ms", sp.ElapsedMilliseconds);
                 }
-                Log.Info("Server is shutting down");
-                return 0;
-            }
-            finally
-            {
-                app?.Dispose();
+                sp.Restart();
+                IHostingEngine application;
+                try
+                {
+                    application = new WebHostBuilder(config)
+                        .UseStartup<Program>()
+                        .UseServer("Microsoft.AspNet.Server.Kestrel")
+                        // ReSharper disable once AccessToDisposedClosure
+                        .UseServices(services => services.AddInstance(serverStore))
+                        .Build();
+                }
+                catch (Exception e)
+                {
+                    Log.FatalException("Could not setup server", e);
+                    Console.WriteLine(e);
+                    return -2;
+                }
+
+
+                if (Log.IsDebugEnabled)
+                {
+                    Log.Debug("Configuring HTTP server took {0:#,#;;0} ms", sp.ElapsedMilliseconds);
+                }
+
+
+                IApplication app = null;
+                try
+                {
+                    try
+                    {
+                        app = application.Start();
+                    }
+                    catch (Exception e)
+                    {
+                        Log.FatalException("Could not start server", e);
+                        Console.WriteLine(e);
+                        return -3;
+                    }
+
+                    Console.WriteLine("Server started, listening to requests...");
+
+                    //TODO: Move the command line options to here
+                    while (true)
+                    {
+                        if (Console.ReadLine() == "q")
+                            break;
+
+                        // Console.ForegroundColor++;
+                    }
+                    Log.Info("Server is shutting down");
+                    return 0;
+                }
+                finally
+                {
+                    app?.Dispose();
+                }
             }
         }
-
     }
 }
