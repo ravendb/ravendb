7e1e64170c85c3fda0803d95723fe3808f0a2dc7
Fitzchak Yitzchaki <fitzchak@ayende.com>
2016-01-28 10:42:02 +0200

    Add test verification

---
+++ src/Raven.Server/Web/System/Verification.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="Verification.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Raven.Server.Json;
+using Raven.Server.Routing;
+
+namespace Raven.Server.Web.System
+{
+    public class Verification : RequestHandler
+    {
+        private readonly RequestHandlerContext _requestHandlerContext;
+
+        public Verification(RequestHandlerContext requestHandlerContext)
+        {
+            _requestHandlerContext = requestHandlerContext;
+        }
+
+        [Route("/loaderio-9efdb3fbfd7839963e77e4443564c2f4.txt", "GET")]
+        public Task IndexesGet()
+        {
+            RavenOperationContext context;
+            using (_requestHandlerContext.ServerStore.AllocateRequestContext(out context))
+            {
+                return _requestHandlerContext.HttpContext.Response.WriteAsync("loaderio-9efdb3fbfd7839963e77e4443564c2f4");
+            }
+        }
+    }
+}
