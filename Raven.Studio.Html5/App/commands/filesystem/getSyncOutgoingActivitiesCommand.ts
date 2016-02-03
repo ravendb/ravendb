dbd238aeff98ed9c1fa13a0821df4f0751311878
Arkadiusz Palinski <arek@ravendb.net>
2016-02-03 12:43:23 +0100

    RavenDB-4078 Fixing synchronization status page. Moving 'Reset index' button to the search page. Adding ability to view more active / pending outgoing syncs and active / finished incoming ones. Displaying more details and paging added. Changing the way we handle synchronization notifications.

--- Raven.Studio.Html5/App/commands/filesystem/getSyncOutgoingActivitiesCommand.ts
+++ Raven.Studio.Html5/App/commands/filesystem/getSyncOutgoingActivitiesCommand.ts
@@ -4,74 +4,46 @@ import synchronizationDetail = require("models/filesystem/synchronizationDetail"
 
 class getSyncOutgoingActivitiesCommand extends commandBase {
 
-    constructor(private fs: filesystem) {
+    constructor(private fs: filesystem, private activity: synchronizationActivity, private skip: number, private take: number) {
         super();
     }
 
-    execute(): JQueryPromise<synchronizationDetail[]> {
-
-        // Outgoing: All the pending and active activities. 
-        var pendingTask = this.getPendingActivity(0, 50);
-        var activeTask = this.getActiveActivity(0, 50);
-
+    execute(): JQueryPromise<filesystemListPageDto<synchronizationDetail>> {
+    
         var doneTask = $.Deferred();
-        var start = 0;
-        var pageSize = 50;
-
-        var combinedTask = $.when(pendingTask, activeTask);
-        combinedTask.done((
-            pendingList: filesystemListPageDto<filesystemSynchronizationDetailsDto>,
-            activeList: filesystemListPageDto<filesystemSynchronizationDetailsDto>) => {
-
-            var activePageSize = pageSize - pendingList.TotalCount;
-
-            var page = [];
-            page.pushAll(pendingList.Items.map(item =>
-                new synchronizationDetail({
-                    FileSystemName: this.fs.name,
-                    FileName: item.FileName,
-                    DestinationFileSystemUrl: item.DestinationUrl,
-                    SourceServerId: "",
-                    SourceFileSystemUrl: "",
-                    Type: filesystemSynchronizationType.Unknown,
-                    Direction: item.Direction,
-                    Action: synchronizationAction.Enqueue
-                }, "Pending", item.Type)));
-
-            if (activePageSize > 0) {
-                page.pushAll(activeList.Items.slice(0, activePageSize - 1).map(item => 
-                    new synchronizationDetail({
-                        FileSystemName: this.fs.name,
-                        FileName: item.FileName,
-                        DestinationFileSystemUrl: item.DestinationUrl,
-                        SourceServerId: "",
-                        SourceFileSystemUrl: "",
-                        Type: filesystemSynchronizationType.Unknown,
-                        Direction: item.Direction,
-                        Action: synchronizationAction.Start
-                    }, "Active", item.Type)));
-            }
-
-            doneTask.resolve(page);
-        });
-        combinedTask.fail(xhr => {
-            this.reportError("Failed to get synchronization outgoing activities.");
-            doneTask.reject(xhr);
-        });
+        this.getActivities()
+            .done((list: filesystemListPageDto<synchronizationDetail>) => doneTask.resolve(list))
+            .fail(xhr => {
+                this.reportError(`Failed to get outgoing and ${this.activity} synchronization activities.`);
+                doneTask.reject(xhr);
+            });
 
         return doneTask;        
     }
 
-    getPendingActivity(skip: number, take: number): JQueryPromise<filesystemListPageDto<filesystemSynchronizationDetailsDto>> {
-        var pendingUrl = "/synchronization/pending";
-        var resultsSelector = (x: filesystemListPageDto<filesystemSynchronizationDetailsDto>) => { x.Items.map((item: filesystemSynchronizationDetailsDto) => item.Direction = synchronizationDirection.Outgoing); return x; };
-        return this.query<filesystemListPageDto<filesystemSynchronizationDetailsDto>>(pendingUrl, { start: skip, pageSize: take }, this.fs, resultsSelector, 15000);
-    }
+    getActivities(): JQueryPromise<filesystemListPageDto<synchronizationDetail>> {
+        var url: string;
 
-    getActiveActivity(skip: number, take: number): JQueryPromise<filesystemListPageDto<filesystemSynchronizationDetailsDto>> {
-        var activeUrl = "/synchronization/active";
-        var resultsSelector = (x: filesystemListPageDto<filesystemSynchronizationDetailsDto>) => { x.Items.map((item: filesystemSynchronizationDetailsDto) => item.Direction = synchronizationDirection.Outgoing); return x; };
-        return this.query<filesystemListPageDto<filesystemSynchronizationDetailsDto>>(activeUrl, { start: skip, pageSize: take }, this.fs, resultsSelector, 15000);
+        switch (this.activity) {
+            case synchronizationActivity.Active:
+                url = "/synchronization/active";
+                break;
+            case synchronizationActivity.Pending:
+                url = "/synchronization/pending";
+                break;
+            default:
+                throw new TypeError(`Not supported synchronization activity type when attempting to retrieve outgoing activities. Given type: ${this.activity}`);
+        }
+
+        var resultsSelector = (x: filesystemListPageDto<synchronizationDetailsDto>) => {
+
+            return {
+                TotalCount: x.TotalCount,
+                Items: x.Items.map((item: synchronizationDetailsDto) => new synchronizationDetail(item, synchronizationDirection.Outgoing, this.activity))
+            };
+        };
+
+        return this.query<filesystemListPageDto<synchronizationDetail>>(url, { start: this.skip, pageSize: this.take }, this.fs, resultsSelector, 15000);
     }
 }
 
