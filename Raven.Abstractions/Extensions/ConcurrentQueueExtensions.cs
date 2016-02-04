62bf6beccc05f5fcba6d67dd214636179328e947
talweiss1982 <tal@ayende.com>
2016-02-04 15:16:21 +0200

    RavenDB-4162_v4 (Raft Statistics) http://issues.hibernatingrhinos.com/issue/RavenDB-4162

---
+++ Raven.Abstractions/Extensions/ConcurrentQueueExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Concurrent;
+
+namespace Raven.Abstractions.Extensions
+{
+    public static class ConcurrentQueueExtensions
+    {
+        public static void LimitedSizeEnqueue<T>(this ConcurrentQueue<T> queue, T item, int sizeOfQueue)
+        {
+            T dontCare;
+            queue.Enqueue(item);
+            while (queue.Count > sizeOfQueue)
+                queue.TryDequeue(out dontCare);
+        }
+    }
+}
