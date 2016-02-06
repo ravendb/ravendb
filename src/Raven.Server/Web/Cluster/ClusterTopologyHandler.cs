a8a29896d8c2522e2e453f0af6ce0b0b5e64640f
Ayende Rahien <ayende@ayende.com>
2016-02-06 12:57:13 +0200

    Adding endpoints that the studio requires

---
+++ src/Raven.Server/Web/Cluster/ClusterTopologyHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Raven.Server.Routing;
+
+namespace Raven.Server.Web.Cluster
+{
+    public class ClusterTopologyHandler : RequestHandler
+    {
+        [RavenAction("/cluster/topology", "GET")]
+        public Task Get()
+        {
+            //TODO: Implement
+            return HttpContext.Response.WriteAsync(@"{}");
+        }
+    }
+}
---
+++ src/Raven.Server/Web/Operations/OperationAlertsHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Raven.Server.Routing;
+
+namespace Raven.Server.Web.Operations
+{
+    public class OperationAlertsHandler : RequestHandler
+    {
+        [RavenAction("/operation/alerts", "GET")]
+        public Task Get()
+        {
+            //TODO: Implement
+            return HttpContext.Response.WriteAsync(@"[]");
+        }
+    }
+}
