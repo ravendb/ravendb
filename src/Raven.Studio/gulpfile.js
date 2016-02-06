249c23a3198566492c7fa894000ba78fd5ee18a3
Marcin Lewandowski <marcin@ravendb.net>
2016-02-06 11:35:56 +0100

    RavenDB-4295 Add the studio to the solution - less compile

--- src/Raven.Studio/gulpfile.js
+++ src/Raven.Studio/gulpfile.js
@@ -1,15 +1,28 @@
-﻿/// <binding AfterBuild='ts-dev-compile' />
+﻿/// <binding AfterBuild='dev-compile' />
 var gulp = require('gulp'),
     ts = require('gulp-typescript'),
+    less = require('gulp-less'),
     sourcemaps = require("gulp-sourcemaps");
 
 var paths = {
     tsSource: './typescript/**/*.ts',
     typings: './typings/**/*.d.ts',
-    tsOutput: './wwwroot/App/'
+    tsOutput: './wwwroot/App/',
+    lessSource: './wwwroot/Content/app.less',
+    lessTarget: './wwwroot/Content/'
 };
 
 var tsCompilerConfig = ts.createProject('tsconfig.json');
+
+gulp.task('dev-less', function() {
+    gulp.src(paths.lessSource)
+        .pipe(sourcemaps.init())
+        .pipe(less({
+            sourceMap: true
+        }))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest(paths.lessTarget));
+});
 
 gulp.task('ts-dev-compile', function() {
     return gulp.src([paths.tsSource, paths.typings])
@@ -20,6 +33,8 @@ gulp.task('ts-dev-compile', function() {
         .pipe(gulp.dest(paths.tsOutput));
 });
 
-gulp.task('watch', ['ts-dev-compile'], function () {
-    gulp.watch(paths.tsSource, ['ts-dev-compile']);
+gulp.task('dev-compile', ['dev-less', 'ts-dev-compile']);
+
+gulp.task('watch', ['dev-compile'], function () {
+    gulp.watch(paths.tsSource, ['dev-compile']);
 });
