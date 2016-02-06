4910cdcd50fc6491b97817c6155c4526609a0aa5
Ayende Rahien <ayende@ayende.com>
2016-02-06 22:23:46 +0200

    Adding an explicit way to get collections & counts from the server, and updating the studio accordingly.

---
+++ src/Raven.Server/Documents/Handlers/CollectionsHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Raven.Server.Json;
+using Raven.Server.Json.Parsing;
+using Raven.Server.Routing;
+
+namespace Raven.Server.Documents
+{
+    public class CollectionsHandler : DatabaseRequestHandler
+    {
+        [RavenAction("/databases/*/collections/stats", "GET")]
+        public async Task GetCollectionStats()
+        {
+            RavenOperationContext context;
+            using (ContextPool.AllocateOperationContext(out context))
+            {
+                context.Transaction = context.Environment.ReadTransaction();
+
+                var collections = new DynamicJsonArray();
+                var result = new DynamicJsonValue
+                {
+                    ["NumberOfDocuments"] = DocumentsStorage.GetNumberOfDocuments(context),
+                    ["Collections"] = collections
+                };
+
+                foreach (var collectionStat in DocumentsStorage.GetCollections(context))
+                {
+                    collections.Add(new DynamicJsonValue
+                    {
+                        ["Name"] = collectionStat.Name,
+                        ["Count"] = collectionStat.Count
+                    });
+                }
+                var writer = new BlittableJsonTextWriter(context, ResponseBodyStream());
+                await context.WriteAsync(writer, result);
+                writer.Flush();
+            }
+        }
+    }
+}
--- src/Raven.Studio/typescript/commands/database/documents/getCollectionsCommand.ts
+++ src/Raven.Studio/typescript/commands/database/documents/getCollectionsCommand.ts
@@ -1,9 +1,6 @@
 import commandBase = require("commands/commandBase");
 import database = require("models/resources/database");
 import collection = require("models/database/documents/collection");
-import getIndexTermsCommand = require("commands/database/index/getIndexTermsCommand");
-import getCollectionsCountCommand = require("commands/database/documents/getCollectionsCountCommand");
-import getCachedCollectionsCount = require("commands/database/studio/getCachedCollectionsCount");
 
 class getCollectionsCommand extends commandBase {
 
@@ -20,75 +17,18 @@ class getCollectionsCommand extends commandBase {
 
     execute(): JQueryPromise<collection[]> {
         var finalResult = $.Deferred<collection[]>();
-        this.runQuery()
-            .fail((xhr: JQueryXHR) => this.createSystemIndexAndTryAgain(finalResult, xhr))
-            .done((results: collection[]) => finalResult.resolve(results));
-        return finalResult;
-    }
-
-    runQuery(): JQueryPromise<collection[]> {
-        var task = $.Deferred();
-
-        new getIndexTermsCommand("Raven/DocumentsByEntityName", "Tag", this.ownerDb)
-            .execute()
-            .done((terms: string[], status, xhr) => {
-
-                if (this.previousValues.length > 0 && this.lastQueryDate()) {
-
-                    var collections = this.previousValues.filter(v => !v.isAllDocuments && !v.isSystemDocuments).map(v => new collection(v.name, this.ownerDb, v.documentCount()));
-                    // apply optimalization fetch only changes, based on last query time. 
-                    new getCachedCollectionsCount(this.ownerDb, this.lastQueryDate())
-                        .execute()
-                        .done((counts: Array<{ CollectionName: string; Count: number}>) => {
-                            counts.forEach(countData => {
-                                var c = collections.first(p => p.name == countData.CollectionName);
-                                if (c) {
-                                    c.documentCount(countData.Count);
-                                } else {
-                                    collections.push(new collection(countData.CollectionName, this.ownerDb, countData.Count));
-                                }
-                            });
-                            task.resolve(collections);
-                        })
-                        .fail(result => task.reject(result));
-                } else {
-                    var collections = terms.map(term => new collection(term, this.ownerDb, 0));
-                    new getCollectionsCountCommand(collections, this.ownerDb)
-                        .execute()
-                        .done(result => task.resolve(result))
-                        .fail(result => task.reject(result));
-                }
-                if (this.lastQueryDate != null) {
-                    this.lastQueryDate(xhr.getResponseHeader('Date'));
-                }
+        // TODO: Someone that actually knows TypeScript need to look at this and see how bad I messed up
+        this.query("/databases/" + this.ownerDb.name + "/collections/stats", null, null)
+            .done( results => {
+                var collections = results.Collections.map(result => new collection(result.Name, this.ownerDb, result.Count));
+                finalResult.done(collections);
             })
             .fail((response) => {
                 this.reportError("Can't fetch collection names");
-                task.reject(response);
+                finalResult.reject(response.responseText);
             });
 
-        return task;
-    }
-
-    createSystemIndexAndTryAgain(deferred: JQueryDeferred<collection[]>, originalReadError: JQueryXHR) {
-        // Most often, failure to get the collections is due to the missing system index, Raven/DocumentsByEntityName.
-        // This appears to be new behavior as of 3.0: Raven doesn't create this index automatically for the system database.
-
-        // Calling silverlight/ensureStartup creates the system index.
-        this.query("/silverlight/ensureStartup", null, this.ownerDb)
-            .done(() => this.retryQuery(deferred, originalReadError))
-            .fail(() => this.onErrorReadingCollections(deferred, originalReadError));
-    }
-
-    retryQuery(deferred: JQueryDeferred<collection[]>, originalReadError: JQueryXHR) {
-        this.runQuery()
-            .fail(() => this.onErrorReadingCollections(deferred, originalReadError))
-            .done((results: collection[]) => deferred.resolve(results));
-    }
-
-    onErrorReadingCollections(deferred: JQueryDeferred<collection[]>, xhr: JQueryXHR) {
-        this.reportError("Failed to read collections", xhr.responseText, xhr.statusText);
-        deferred.reject(xhr);
+        return finalResult;
     }
 }
 
