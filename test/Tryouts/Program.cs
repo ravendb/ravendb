508d19d031d8280de41235d9dea390799180d5aa
Ayende Rahien <ayende@ayende.com>
2016-02-06 12:55:36 +0200

    Fixing tryout project compilation

--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -1,13 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FastTests.Blittable;
 
 namespace Tryouts
 {
     public class Program
     {
-      
-        public  static void Main(string[] args)
+        public static void Main(string[] args)
         {
-           
+            new ArrayParsingTests().CanParseSimpleArray().Wait();
         }
     }
 }
