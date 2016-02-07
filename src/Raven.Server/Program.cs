625ac11b5221f6cc791f1c4c4771a9846c957514
Ayende Rahien <ayende@ayende.com>
2016-02-07 11:50:04 +0200

    Removing MVC references

--- src/Raven.Server/Program.cs
+++ src/Raven.Server/Program.cs
@@ -27,7 +27,7 @@ namespace Raven.Server
                 {
                     server.Initialize();
 
-                    Console.WriteLine("Listening to: " + configuration.WebHostConfig.Get<string>("server.urls"));
+                    Console.WriteLine("Listening to: " + configuration.Core.ServerUrls);
                     Console.WriteLine("Server started, listening to requests...");
 
                     //TODO: Move the command line options to here
