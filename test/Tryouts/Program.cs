184348144b2831b4ebe121252745bff1df66f5ae
Ayende Rahien <ayende@ayende.com>
2016-02-09 22:27:48 +0200

    Spiking voron based indexing system

--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -10,6 +10,8 @@ using FastTests.Server.Documents;
 using Raven.Server.Json;
 using Raven.Server.Json.Parsing;
 using Raven.Tests.Core;
+using Tryouts.Corax;
+using Voron;
 using Xunit;
 
 namespace Tryouts
@@ -18,9 +20,38 @@ namespace Tryouts
     {
         public static void Main(string[] args)
         {
-            using (var c = new Crud())
+            Run().Wait();
+        }
+
+        private static async Task Run()
+        {
+            using (var corax = new FullTextIndex(StorageEnvironmentOptions.CreateMemoryOnly()))
             {
-                c.CanSaveAndLoad().Wait();
+                using (var indexer = corax.CreateIndexer())
+                {
+                    await indexer.NewEntry(new DynamicJsonValue
+                    {
+                        ["Name"] = "Oren Eini"
+                    }, "users/1");
+                }
+
+                using (var searcher = corax.CreateSearcher())
+                {
+                    var ids = searcher.Query("Name", "Oren Eini");
+                    Assert.Equal(new[] {"users/1"}, ids);
+                }
+
+                using (var indexer = corax.CreateIndexer())
+                {
+                   indexer.Delete("users/1");
+                }
+
+                using (var searcher = corax.CreateSearcher())
+                {
+                    var ids = searcher.Query("Name", "Oren Eini");
+                    Assert.Empty(ids);
+                }
+
             }
         }
     }
