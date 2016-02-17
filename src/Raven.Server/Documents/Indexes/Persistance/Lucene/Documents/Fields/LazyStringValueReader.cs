22a57a084ad0903f0da98c74b67dc33f8951145b
Arkadiusz Palinski <arek@ravendb.net>
2016-02-17 11:20:21 +0100

    RavenDB-4329 Make use of single instance of StringBuilder instead of creating multiple ones by calling StreamReader.ReadToEnd method.

--- src/Raven.Server/Documents/Indexes/Persistance/Lucene/Documents/Fields/LazyStringValueReader.cs
+++ src/Raven.Server/Documents/Indexes/Persistance/Lucene/Documents/Fields/LazyStringValueReader.cs
@@ -3,18 +3,19 @@ using System.IO;
 using System.Text;
 using Raven.Server.Indexing.Corax;
 using Raven.Server.Json;
-using Sparrow.Binary;
 
 namespace Raven.Server.Documents.Indexes.Persistance.Lucene.Documents.Fields
 {
     public unsafe class LazyStringReader : IDisposable
     {
-        private readonly MmapStream _mmapStream;
+        private readonly MmapStream _mmapStream = new MmapStream(null, 0);
         private readonly StreamReader _reader;
 
+        private StringBuilder _sb;
+        private char[] _readBuffer;
+        
         public LazyStringReader()
         {
-            _mmapStream = new MmapStream(null, 0);
             _reader = new StreamReader(_mmapStream, Encoding.UTF8);
         }
 
@@ -31,7 +32,24 @@ namespace Raven.Server.Documents.Indexes.Persistance.Lucene.Documents.Fields
             _mmapStream.Set(value.Buffer, value.Size);
             _reader.DiscardBufferedData();
 
-            return _reader.ReadToEnd();
+            if (_readBuffer == null)
+                _readBuffer = new char[128];
+
+            if (_sb == null)
+                _sb = new StringBuilder();
+            else
+                _sb.Clear();
+
+            var read = 0;
+
+            do
+            {
+                read = _reader.ReadBlock(_readBuffer, 0, _readBuffer.Length);
+                _sb.Append(_readBuffer, 0, read);
+
+            } while (read == _readBuffer.Length);
+
+            return _sb.ToString();
         }
 
         public void Dispose()
