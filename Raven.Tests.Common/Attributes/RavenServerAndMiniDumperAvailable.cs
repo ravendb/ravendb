ac496bbb83790c66b0ee401d2b8736c3cdb9b008
talweiss1982 <tal@ayende.com>
2016-03-02 15:32:29 +0200

    RavenDB-3968 Added a test to make sure we can take a dump using the dump tool Moved time bomb to 1/4/2016

---
+++ Raven.Tests.Common/Attributes/RavenServerAndMiniDumperAvailable.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Xunit;
+using Xunit.Sdk;
+
+
+namespace Raven.Tests.Common.Attributes
+{
+
+    [CLSCompliant(false)]
+    public class RavenServerAndMiniDumperAvailable : FactAttribute
+    {
+        protected override IEnumerable<ITestCommand> EnumerateTestCommands(IMethodInfo method)
+        {
+            var displayName = method.TypeName + "." + method.Name;
+
+            if (LookForRavenPaths() == false)
+            {
+                yield return
+                    new SkipCommand(method, displayName,
+                        "Could not execute " + displayName + " because it requires Raven.Server.exe and Raven.MiniDumper.exe");
+                yield break;
+            }
+
+            foreach (var command in base.EnumerateTestCommands(method))
+            {
+                yield return command;
+            }
+        }
+
+        private bool LookForRavenPaths()
+        {
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var expectedRavenPath = Path.Combine(baseDir, @"..\..\..\Raven.Server\bin\Debug\Raven.Server.exe");
+            if (File.Exists(expectedRavenPath))
+            {
+                RavenServerPath = expectedRavenPath;
+            }
+            else
+            {
+                expectedRavenPath = Path.Combine(baseDir, @"..\..\..\Raven.Server\bin\Release\Raven.Server.exe");
+                if (File.Exists(expectedRavenPath))
+                    RavenServerPath = expectedRavenPath;
+                else return false;
+            }
+            var expectedMiniDumperPath = Path.Combine(baseDir, @"..\..\..\Raven.MiniDumper\bin\Debug\Raven.MiniDumper.exe");
+            if (File.Exists(expectedMiniDumperPath))
+                MiniDumperPath = expectedMiniDumperPath;
+            else
+            {
+                expectedMiniDumperPath = Path.Combine(baseDir, @"..\..\..\Raven.MiniDumper\bin\Release\Raven.MiniDumper.exe");
+                if (File.Exists(expectedMiniDumperPath))
+                    MiniDumperPath = expectedMiniDumperPath;
+                else return false;
+            }
+            var ravenBaseDir = Path.GetDirectoryName(expectedRavenPath);
+            LocalConfigPath = Path.Combine(ravenBaseDir, "local.config");
+            //should not happen
+            if (!File.Exists(LocalConfigPath))
+                return false;
+            return true;
+        }
+
+        public static string RavenServerPath { get; set; }
+        public static string MiniDumperPath { get; set; }
+        public static string LocalConfigPath { get; set; }
+    }
+    
+}
