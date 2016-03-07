9d1a045d8d6b666b2715f8281f621f8f28dbee59
Fitzchak Yitzchaki <fitzchak@ayende.com>
2016-03-07 17:07:49 +0200

    WIP: implementing batch support for patch

--- src/Raven.Server/Documents/Handlers/BatchHandler.cs
+++ src/Raven.Server/Documents/Handlers/BatchHandler.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.WebSockets.Protocol;
 
 using Raven.Abstractions.Extensions;
+using Raven.Server.Documents.Patch;
 using Raven.Server.Json;
 using Raven.Server.Json.Parsing;
 using Raven.Server.Routing;
@@ -21,8 +22,10 @@ namespace Raven.Server.Documents.Handlers
             public string Method;
             public string Key;
             public BlittableJsonReaderObject Document;
+            public PatchRequest Patch;
             public BlittableJsonReaderObject AdditionalData;
             public long? Etag;
+            public bool IsDebugMode;
         }
 
         [RavenAction("/databases/*/bulk_docs", "POST")]
@@ -97,7 +100,15 @@ namespace Raven.Server.Documents.Handlers
                             parsedCommands[i].Document = context.ReadObject(doc, parsedCommands[i].Key,
                                 BlittableJsonDocumentBuilder.UsageMode.ToDisk);
                             break;
+                        case "PATCH":
+                            cmd.TryGet("DebugMode", out parsedCommands[i].IsDebugMode);
 
+                            BlittableJsonReaderObject patch;
+                            if (cmd.TryGet("Patch", out patch) == false)
+                                throw new InvalidDataException("Missing 'Patch' property");
+
+                            parsedCommands[i].Patch = PatchRequest.Parse(patch);
+                            break;
                     }
                 }
 
@@ -105,34 +116,38 @@ namespace Raven.Server.Documents.Handlers
 
                 using (context.OpenWriteTransaction())
                 {
-
                     for (int i = 0; i < parsedCommands.Length; i++)
                     {
-                        switch (parsedCommands[i].Method)
+                        var cmd = parsedCommands[i];
+                        switch (cmd.Method)
                         {
                             case "PUT":
-                                var putResult = Database.DocumentsStorage.Put(context, parsedCommands[i].Key, parsedCommands[i].Etag,
-                                    parsedCommands[i].Document);
+                                var putResult = Database.DocumentsStorage.Put(context, cmd.Key, cmd.Etag,
+                                    cmd.Document);
 
                                 BlittableJsonReaderObject metadata;
-                                parsedCommands[i].Document.TryGet(Constants.Metadata, out metadata);
+                                cmd.Document.TryGet(Constants.Metadata, out metadata);
 
                                 reply.Add(new DynamicJsonValue
                                 {
                                     ["Key"] = putResult.Key,
                                     ["Etag"] = putResult.ETag,
                                     ["Method"] = "PUT",
-                                    ["AdditionalData"] = parsedCommands[i].AdditionalData,
+                                    ["AdditionalData"] = cmd.AdditionalData,
                                     ["Metadata"] = metadata
                                 });
                                 break;
+                            case "PATCH":
+                                var re = Database.Patch.Apply(context, cmd.Key, cmd.Etag, cmd.Patch, null, cmd.IsDebugMode);
+                                throw new NotImplementedException("Handler reply");
+                                break;
                             case "DELETE":
-                                var deleted = Database.DocumentsStorage.Delete(context, parsedCommands[i].Key, parsedCommands[i].Etag);
+                                var deleted = Database.DocumentsStorage.Delete(context, cmd.Key, cmd.Etag);
                                 reply.Add(new DynamicJsonValue
                                 {
-                                    ["Key"] = parsedCommands[i].Key,
+                                    ["Key"] = cmd.Key,
                                     ["Method"] = "DELETE",
-                                    ["AdditionalData"] = parsedCommands[i].AdditionalData,
+                                    ["AdditionalData"] = cmd.AdditionalData,
                                     ["Deleted"] = deleted
                                 });
                                 break;
