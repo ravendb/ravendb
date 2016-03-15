e14732a45b4c07896a5c83b5ec49246cd1b05a24
Arkadiusz Palinski <arek@ravendb.net>
2016-03-15 13:37:13 +0100

    RavenDB-4421 Fixing data inconsistency issue caused by faulty optimization for updating overflows.
    Minor changes:
    - fixing FlushingLogsShouldNotCauseExceptions test according current handling of overflows
    - removing invalid check in ValidateWritablePages - updates with the same value are allowed.

---
+++ Raven.Voron/Voron.Tests/InlineDataWithRandomSeed.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="InlineDataWithRandomSeed.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit.Extensions;
+
+namespace Voron.Tests
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class InlineDataWithRandomSeed : DataAttribute
+    {
+
+        public InlineDataWithRandomSeed(params object[] dataValues)
+        {
+            this.DataValues = dataValues ?? new object[] { null };
+        }
+
+        public object[] DataValues { get; set; }
+
+        public override IEnumerable<object[]> GetData(MethodInfo methodUnderTest, Type[] parameterTypes)
+        {
+            var objects = new object[DataValues.Length + 1];
+            Array.Copy(DataValues, 0, objects, 0, DataValues.Length);
+            objects[DataValues.Length] = Environment.TickCount;
+            yield return objects;
+
+        }
+    }
+}
