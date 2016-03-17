3083bb69d281548f2b16e5904219d43b38374a88
Ayende Rahien <ayende@ayende.com>
2016-03-17 10:08:13 +0200

    The paths to delete needs to be thread safe

--- test/FastTests/Server/OAuth/CanAuthenticate.cs
+++ test/FastTests/Server/OAuth/CanAuthenticate.cs
@@ -5,24 +5,11 @@
 // -----------------------------------------------------------------------
 
 using System;
-using System.Net.Http;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Raven.Abstractions.Indexing;
-using Raven.Abstractions.Json;
-using Raven.Client.Connection;
-using Raven.Client.Connection.Implementation;
 using Raven.Client.Data;
 using Raven.Client.OAuth;
-using Raven.Imports.Newtonsoft.Json;
-using Raven.Json.Linq;
 using Raven.Tests.Core;
-using Raven.Tests.Core.Utils.Entities;
 using Xunit;
-using Raven.Abstractions.Data;
-using Voron;
 
 namespace FastTests.Server.OAuth
 {
