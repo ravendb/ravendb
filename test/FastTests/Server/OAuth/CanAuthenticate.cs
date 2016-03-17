7287d3ecb12bf4c0e742f8b4f0c7f2831275e9bd
aviviadi <avivi.adi.hr@gmail.com>
2016-03-17 17:41:24 +0200

    tests that actually test that we are able to auth (both positive and negative)

--- test/FastTests/Server/OAuth/CanAuthenticate.cs
+++ test/FastTests/Server/OAuth/CanAuthenticate.cs
@@ -5,9 +5,12 @@
 // -----------------------------------------------------------------------
 
 using System;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Raven.Client.Data;
 using Raven.Client.OAuth;
+using Raven.Server.Config.Attributes;
 using Raven.Tests.Core;
 using Xunit;
 
@@ -20,11 +23,6 @@ namespace FastTests.Server.OAuth
         {
             using (var store = await GetDocumentStore())
             {
-                var securedAuthenticator = new SecuredAuthenticator();
-                var exception = await Assert.ThrowsAsync<InvalidOperationException>(async () =>
-                    await securedAuthenticator.DoOAuthRequestAsync(store.Url + "/oauth/api-key", "super/secret"));
-                Assert.Contains("Could not find api key: super", exception.Message);
-
                 var apiKey = new ApiKeyDefinition
                 {
                     Enabled = true,
@@ -35,16 +33,51 @@ namespace FastTests.Server.OAuth
                     }
                 };
 
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new
+                    {
+                        Name = "test oauth"
+                    }, 
+                    "test/1");
+                    session.SaveChanges();
+                }
+
+                Server.Configuration.Server.AnonymousUserAccessMode = AnonymousUserAccessModeValues.None;
+
+                var client = new HttpClient();
+                var result = await client.GetAsync($"{store.Url}/databases/{store.DefaultDatabase}/document?id=test/1");
+
+                Assert.Equal(HttpStatusCode.PreconditionFailed, result.StatusCode);
+
+                var securedAuthenticator = new SecuredAuthenticator();
+                var exception = await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+                    await securedAuthenticator.DoOAuthRequestAsync(store.Url + "/oauth/api-key", "super/secret"));
+
+                Assert.Contains("Could not find api key: super", exception.Message);
+
+
+                Server.Configuration.Server.AnonymousUserAccessMode = AnonymousUserAccessModeValues.Admin;
 
                 store.DatabaseCommands.GlobalAdmin.PutApiKey("super", apiKey);
                 var doc = store.DatabaseCommands.GlobalAdmin.GetApiKey("super");
 
                 Assert.NotNull(doc);
 
-                await securedAuthenticator.DoOAuthRequestAsync(store.Url + "/oauth/api-key", "super/secret");
+                Server.Configuration.Server.AnonymousUserAccessMode = AnonymousUserAccessModeValues.Admin;
+
+
+                var oauth = await securedAuthenticator.DoOAuthRequestAsync(store.Url + "/oauth/api-key", "super/secret");
 
                 Assert.NotNull(securedAuthenticator.CurrentToken);
                 Assert.NotEqual("", securedAuthenticator.CurrentToken);
+
+                var authenticatedClient = new HttpClient();
+                oauth(authenticatedClient);
+
+                result = await client.GetAsync($"{store.Url}/databases/{store.DefaultDatabase}/document?id=test/1");
+
+                Assert.Equal(HttpStatusCode.OK, result.StatusCode);
             }
         }
     }
