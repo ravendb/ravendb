3454912ac5d211f9088a24e8d48f14988554c1f6
Marcin Lewandowski <marcin@ravendb.net>
2016-03-22 10:22:20 +0100

    RavenDB-4397 HotSpare - poor UX

--- Raven.Studio.Html5/App/viewmodels/manage/hotSpare.ts
+++ Raven.Studio.Html5/App/viewmodels/manage/hotSpare.ts
@@ -1,34 +1,40 @@
 import viewModelBase = require("viewmodels/viewModelBase");
-import GetHotSpareInformation = require("commands/licensing/GetHotSpareInformation");
-import TestHotSpareCommand = require("commands/licensing/TestLicenseCommand");
-import ActivateHotSpareCommand = require("commands/licensing/ActivateLicenseCommand");
+import getHotSpareInformation = require("commands/licensing/GetHotSpareInformation");
+import testHotSpareCommand = require("commands/licensing/testHotSpareCommand");
+import activateHotSpareCommand = require("commands/licensing/activateHotSpareCommand");
+
 class hotSpare extends viewModelBase {
     runningOnExpiredLicense = ko.observable<boolean>(false);
-    ActivationMode = ko.observable<string>();
-    ActivationTime = ko.observable<string>();
-    RemainingTestActivation = ko.observable<number>();	
+    activationMode = ko.observable<string>();
+    activationTime = ko.observable<string>();
+    remainingTestActivation = ko.observable<number>();	
     isActivationExpired = ko.observable(false);
+
     activate(args: any) {
-        var self = this;
         super.activate(args);
-        new GetHotSpareInformation().execute().done((res: HotSpareDto) => {
-            self.ActivationMode(res.ActivationMode);
-            self.ActivationTime(res.ActivationTime);
-            self.RemainingTestActivation(res.RemainingTestActivations);
+        new getHotSpareInformation().execute().done((res: HotSpareDto) => {
+            this.activationMode(res.ActivationMode);
+            this.activationTime(res.ActivationTime);
+            this.remainingTestActivation(res.RemainingTestActivations);
         }).fail(() => {
             alert("Can't fetch license information");
-            });
+        });
     }
+
     isTestEnabled(): boolean {
-        return (this.ActivationMode() === 'NotActivated');
+        return this.activationMode() === 'NotActivated';
     }
-    Testlicense() {
-        new TestHotSpareCommand().execute();
+
+    testLicense() {
+        new testHotSpareCommand().execute();
     }
-    ActivateLicense() {
+
+    activateLicense() {
         var self = this;
-        new ActivateHotSpareCommand().execute().fail(() => {
-            self.isActivationExpired(true);
+        new activateHotSpareCommand().execute().fail((response: JQueryXHR) => {
+            if (response.status === 403) {
+                self.isActivationExpired(true);
+            }
         });
     }
 }
