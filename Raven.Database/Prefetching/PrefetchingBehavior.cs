4208be89d353355cec32474caa15a5dc5f29d146
talweiss1982 <tal@ayende.com>
2016-03-29 19:03:04 +0300

    RavenDB-4447

--- Raven.Database/Prefetching/PrefetchingBehavior.cs
+++ Raven.Database/Prefetching/PrefetchingBehavior.cs
@@ -1140,9 +1140,17 @@ namespace Raven.Database.Prefetching
 
             futureIndexBatch.Task.ContinueWith(t =>
             {
-                FutureBatchCompleted(t.Result.Count);
-            }, linkedToken.Token);
-            
+                try
+                {
+                    if (linkedToken.IsCancellationRequested == false)
+                        FutureBatchCompleted(t.Result.Count);
+                }
+                catch (ObjectDisposedException)
+                {
+                    // this is an expected race with the actual task, this is fine
+                }
+            });
+
             return futureIndexBatches.TryAdd(nextEtag, futureIndexBatch);
         }
 
