23b01aa71665cb8dd382901cd6749a951eb998e2
Federico Andres Lois <federico.lois@corvalius.com>
2016-04-06 15:49:04 -0300

    RavenDB-4464: Repeated Insertion in the same transaction will eventually fail on Tables [Repro]

--- test/FastTests/Voron/Tables/Bugs.cs
+++ test/FastTests/Voron/Tables/Bugs.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
-
+﻿using Sparrow;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using Voron;
 using Voron.Data.Tables;
 using Voron.Util.Conversion;
@@ -7,8 +9,66 @@ using Xunit;
 
 namespace FastTests.Voron.Tables
 {
-    public class Bugs : TableStorageTest
+    public unsafe class Bugs : TableStorageTest
     {
+        public const int ItemsPerTransaction = 100;
+        public const int Transactions = 2;
+        public const string Template = "000000000000000000000000000000000000000000000000000";
+
+        [Fact]
+        public void RepeatedInsertingInTheSameTransaction()
+        {
+            var schema = new TableSchema()
+                                .DefineKey(new TableSchema.SchemaIndexDef
+                                {
+                                    StartIndex = 0,
+                                });
+
+
+            using (var tx = Env.WriteTransaction())
+            {
+                schema.Create(tx, "docs");
+
+                tx.Commit();
+            }
+
+            var value = new byte[100];
+            new Random().NextBytes(value);
+            var ms = new MemoryStream(value);
+
+            using (var tx = Env.WriteTransaction())
+            {
+                var docs = new Table(schema, "docs", tx);
+                for (long i = 0; i < Transactions * ItemsPerTransaction; i++)
+                {
+                    ms.Position = 0;
+
+                    SetHelper(docs, i.ToString(Template), ms);
+                }
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.ReadTransaction())
+            {
+                var docs = new Table(schema, "docs", tx);
+
+                var array = new byte[100];
+                for (int i = 0; i < Transactions * ItemsPerTransaction; i++)
+                {
+                    var key = i.ToString(Template);
+                    var tableReader = docs.ReadByKey(key);
+
+                    Assert.NotNull(tableReader);
+
+                    int size;
+                    byte* buffer = tableReader.Read(1, out size);
+                    Assert.True(buffer != null);
+                    Assert.Equal(100, size);
+                }
+            }        
+        }
+
         [Fact]
         public void CanInsertThenDeleteBySecondary2()
         {
