78c05b8b11c94825f4016758f54e8baeb98293dd
talweiss1982 <tal@ayende.com>
2016-04-18 16:07:08 +0300

    RavenDB-4500: Fixing NRE and licensing logic

--- Raven.Database/Config/Retriever/ConfigurationRetriever.cs
+++ Raven.Database/Config/Retriever/ConfigurationRetriever.cs
@@ -90,26 +90,16 @@ namespace Raven.Database.Config.Retriever
                 }
 
                 string globalConfigurationAsString;
-                bool globalConfiguration;
-                if (ValidateLicense.CurrentLicense.Attributes.TryGetValue("globalConfiguration", out globalConfigurationAsString) && bool.TryParse(globalConfigurationAsString, out globalConfiguration))
-                {
-                    string expirationStr;
-                    DateTime expiration;
-                    if (ValidateLicense.CurrentLicense.Attributes.TryGetValue("expiration", out expirationStr)
-                        && DateTime.TryParse(expirationStr,out expiration))
-                    {
-                        licenseEnabled = expiration;
-                    }
-                    return globalConfiguration;
-                }
-                //if we are here we are either on a open source license or a non-enterprise license.
+                bool globalConfiguration = false;
+                if(ValidateLicense.CurrentLicense.Attributes.TryGetValue("globalConfiguration", out globalConfigurationAsString))
+                    bool.TryParse(globalConfigurationAsString, out globalConfiguration);                
                 if (ValidateLicense.CurrentLicense.Status.Equals("AGPL - Open Source"))
                 {
                     //allowing open source users to use global configuration
                     return true;
                 }
                 //If we are here we have a license but it is not enterprise.
-                return false;
+                return globalConfiguration;
             }
         }
 
