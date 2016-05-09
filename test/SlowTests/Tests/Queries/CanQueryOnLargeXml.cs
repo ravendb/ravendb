b84bcdb667b9e7f06c0dadb398072a563448f545
Pawel Pekrol <ppekrol@ravendb.net>
2016-05-09 15:51:49 +0200

    RavenDB-4329 migrated tests

--- test/SlowTests/Tests/Queries/CanQueryOnLargeXml.cs
+++ test/SlowTests/Tests/Queries/CanQueryOnLargeXml.cs
@@ -1,43 +1,44 @@
 using System;
-using Raven.Client.Document;
-using Raven.Tests.Bugs;
-using Raven.Tests.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+using FastTests;
 
 using Xunit;
-using System.Linq;
 
-namespace Raven.Tests.Queries
+namespace SlowTests.Tests.Queries
 {
-    public class CanQueryOnLargeXml : RavenTest
+    public class CanQueryOnLargeXml : RavenTestBase
     {
         public class Item
         {
             public string SchemaFullName;
             public string ValueBlobString;
         }
+
         [Fact]
-        public void Remote()
+        public async Task Remote()
         {
-            using(var store = NewRemoteDocumentStore())
+            using (var store = await GetDocumentStore())
             {
-                using(var s = store.OpenSession())
+                using (var s = store.OpenSession())
                 {
                     var xml =
                         "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<__Root__>\r\n  <_STD s=\"DateTime\" t=\"System.DateTime, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089\" /> <dateTime xmlns=\"http://schemas.microsoft.com/2003/10/Serialization/\">2009-01-14T23:00:57.087Z</dateTime> </__Root__>";
                     var guid = Guid.NewGuid().ToString();
 
                     var s1 = s.Query<Item>()
-                        .Where(x => x.SchemaFullName.Equals(guid)  && x.ValueBlobString.Equals(xml))
+                        .Where(x => x.SchemaFullName.Equals(guid) && x.ValueBlobString.Equals(xml))
                         .ToString();
 
                     var s2 = s.Query<Item>()
-                        .Where(x => x.SchemaFullName == guid  && x.ValueBlobString == xml)
+                        .Where(x => x.SchemaFullName == guid && x.ValueBlobString == xml)
                         .ToString();
 
-                    Assert.Equal(s1,s2);
+                    Assert.Equal(s1, s2);
 
                     s.Query<Item>()
-                        .Where(x => x.SchemaFullName.Equals(guid)  && x.ValueBlobString.Equals(xml))
+                        .Where(x => x.SchemaFullName.Equals(guid) && x.ValueBlobString.Equals(xml))
                         .ToList();
                 }
             }
