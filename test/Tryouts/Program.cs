5966457c61952a0265414a08fe17a2105f0114cf
aviviadi <avivi.adi.hr@gmail.com>
2016-05-16 20:46:47 +0300

    Add some changes..

--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -1,25 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net.WebSockets;
-using System.Runtime.InteropServices.ComTypes;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-using FastTests.Client.BulkInsert;
-using FastTests.Server.Documents.Indexing;
-using FastTests.Voron.RawData;
-using Raven.Abstractions.Data;
+using Newtonsoft.Json;
 using Raven.Client.Document;
+using Raven.Client.Extensions;
 using Raven.Client.Linq;
-using Raven.Client.Platform;
-
-using SlowTests.Tests.Sorting;
 
 namespace Tryouts
 {
     public class Program
     {
+        public static int Numofdocs = 500000;
         public class User
         {
             public string FirstName { get; set; }
@@ -27,64 +18,107 @@ namespace Tryouts
             public string LastName { get; set; }
 
             public string[] Tags { get; set; }
+            public string LongAttributte { get; set; }
         }
+
+        public static int Id = 1;
 
         public static void Main(string[] args)
         {
-            var ds = new DocumentStore
-            {
-                Url = "http://127.0.0.1:8080",
-                DefaultDatabase = "test"
-            };
-            ds.Initialize();
-            //ds.DatabaseCommands.GlobalAdmin.CreateDatabase(new DatabaseDocument
-            //{
-            //    Id = "test",
-            //    Settings = new Dictionary<string, string>
-            //    {
-            //        {"Raven/DataDir","~/Test" }
-            //    }
-            //});
-            BulkInsert(ds, 1024*1024).Wait();
-        }
+            if (args.Length == 1)
+                Numofdocs = Convert.ToInt32(args[0]);
+            while (true)
+            { 
+                try
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        using (var store = new DocumentStore
+                        {
+                            Url = "http://127.0.0.1:8080",
+                            DefaultDatabase = "test"
+                        })
+                        {
+                            store.Initialize();
 
-        private static async Task DoWork()
-        {
-            using (var ws = new RavenClientWebSocket())
-            {
-                await ws.ConnectAsync(new Uri("ws://echo.websocket.org"), CancellationToken.None);
+                            try
+                            {
+                                createDb(store).Wait();
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("createDb returned exception");
+                            }
+                            BulkInsert(store, Numofdocs).Wait();
+                            Console.WriteLine("Done insertion");
+                            //  Console.ReadKey();
+                        }
+                    }
+                    using (var store = new DocumentStore
+                    {
+                        Url = "http://127.0.0.1:8080",
+                        DefaultDatabase = "test"
+                    })
+                    {
+                        store.Initialize();
+                        using (var session = store.OpenSession())
+                        {
+                            var users = session.Query<User>().Take(10).ToList();
+                            int i = 0;
+                            users.ForEach(x =>
+                            {
+                                if (!i.ToString().Equals(x.FirstName))
+                                    Console.WriteLine("Error at user " + i);
+                                ++i;
+                            });
 
-                await
-                    ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("Hello there")),
-                        WebSocketMessageType.Text,
-                        true, CancellationToken.None);
-
-                var arraySegment = new ArraySegment<byte>(new byte[1024]);
-                var webSocketReceiveResult = await ws.ReceiveAsync(arraySegment, CancellationToken.None);
-                var s = Encoding.UTF8.GetString(arraySegment.Array, 0, webSocketReceiveResult.Count);
-                Console.WriteLine();
-                Console.WriteLine(s);
+                        }
+                        Console.WriteLine("Done checking");
+                        Console.ReadKey();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    Console.ReadKey();
+                }
             }
         }
+
+        public static async Task createDb(DocumentStore store)
+        {
+            var doc = MultiDatabase.CreateDatabaseDocument("test");
+            await store.AsyncDatabaseCommands.GlobalAdmin.CreateDatabaseAsync(doc).ConfigureAwait(false);
+        }
+
         public static async Task BulkInsert(DocumentStore store, int numOfItems)
         {
-            Console.Write("Doing bulk-insert...");
+            Console.WriteLine("Doing bulk-insert...");
 
             string[] tags = null;// Enumerable.Range(0, 1024*8).Select(x => "Tags i" + x).ToArray();
 
             var sp = System.Diagnostics.Stopwatch.StartNew();
             using (var bulkInsert = store.BulkInsert())
             {
-                int id = 1;
+                // int id = 1;
                 for (int i = 0; i < numOfItems; i++)
-                    await bulkInsert.StoreAsync(new User
+                {
+                    if (i % 100000 == 0)
+                        Console.WriteLine(i);
+                    var entity = new User
                     {
-                        FirstName = $"First Name - {i}",
+                        FirstName = $"{i}",
                         LastName = $"Last Name - {i}",
+                        LongAttributte = $@"
+fskladf f;alsdl;f ;alksdjf 'pasdjf l;kasdfmk;laerj gklfmdklgkn fcnfdklcvn ,mnb.,xmcv lskdf nv;okljdfng v/dvm z/s;xvmz/dx.,jvm /.zxm /z.x, c'
+sdklfn sdlkgm vlsdf,bm kjsfq[wptuog934t345t90345 430wtioe frsd'fpo ;lvkmcm z.x, fjmnv l.zx,dmv zsdkfv mn;QOI LFC,M.XJFCKLS.EHFKZ,DGJWY IGFq3eq
+DAASF CM,XIhn f,mvLJ oiu9 IOPop oiJOIPjiopPOJPOJ POJ LK",
                         Tags = tags
-                    }, $"users/{id++}");
+                    };
+                    await bulkInsert.StoreAsync(entity, $"users/{Id++}");
+                }
             }
-            Console.WriteLine("done in " + sp.Elapsed);
+            Console.WriteLine("\r\ndone in " + sp.Elapsed);
         }
     }
 }
