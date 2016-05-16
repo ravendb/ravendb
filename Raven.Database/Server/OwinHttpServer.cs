7bf4ec52ce32681571bc60a0b4fe32e41da0da67
grisha-kotler <grisha@ayende.com>
2016-05-16 23:41:47 +0300

    RavenDB-4606 Failing Test: CanLoadFromIndex_Remote

--- Raven.Database/Server/OwinHttpServer.cs
+++ Raven.Database/Server/OwinHttpServer.cs
@@ -40,7 +40,7 @@ namespace Raven.Database.Server
                 throw new InvalidOperationException("Http server is already running");
 
             var schema = config.Encryption.UseSsl ? "https" : "http";
-            var hostName = config.HostName ?? "+";
+            var hostName = (config.HostName == null || config.HostName == "localhost") ? "+" : config.HostName;
             var url = $"{schema}://{hostName}:{config.Port}";
             server = WebApp.Start(url, app => //TODO DH: configuration.ServerUrl doesn't bind properly
             {
