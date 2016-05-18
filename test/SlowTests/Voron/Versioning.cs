b84dcce6aede6781c37fc6d947761090bf46f254
Fitzchak Yitzchaki <fitzchak@ayende.com>
2016-05-18 11:38:52 +0300

    minor

--- test/SlowTests/Voron/Versioning.cs
+++ test/SlowTests/Voron/Versioning.cs
@@ -1,31 +1,32 @@
 ﻿using System;
 using System.IO;
+using FastTests.Voron.FixedSize;
 using Xunit;
 
 namespace SlowTests.Voron
 {
     public class Versioning : StorageTest
-	{
-		[Fact]
-		public void SplittersAndRebalancersShouldNotChangeNodeVersion()
-		{
-			const int DocumentCount = 100000;
+    {
+        [Theory]
+        [InlineDataWithRandomSeed]
+        public void SplittersAndRebalancersShouldNotChangeNodeVersion(int seed)
+        {
+            const int documentCount = 100000;
 
-			var rand = new Random();
-			var testBuffer = new byte[123];
-			rand.NextBytes(testBuffer);
+            var rand = new Random(seed);
+            var testBuffer = new byte[123];
+            rand.NextBytes(testBuffer);
 
-
-			using (var tx = Env.WriteTransaction())
-			{
-				tx.CreateTree("tree1");
-				tx.Commit();
-			}
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.CreateTree("tree1");
+                tx.Commit();
+            }
 
             using (var tx = Env.WriteTransaction())
             {
                 var tree = tx.CreateTree("tree1");
-                for (var i = 0; i < DocumentCount; i++)
+                for (var i = 0; i < documentCount; i++)
                 {
                     tree.Add("Foo" + i, new MemoryStream(testBuffer));
 
@@ -33,15 +34,15 @@ namespace SlowTests.Voron
                 tx.Commit();
             }
 
-			using (var snapshot = Env.WriteTransaction())
-			{
-				for (var i = 0; i < DocumentCount; i++)
-				{
-				    var readTree = snapshot.CreateTree("tree1");
-				    var result = readTree.Read("Foo" + i);
-					readTree.Delete("Foo" + i, result.Version);
-				}
-			}
-		}
-	}
+            using (var snapshot = Env.WriteTransaction())
+            {
+                for (var i = 0; i < documentCount; i++)
+                {
+                    var readTree = snapshot.CreateTree("tree1");
+                    var result = readTree.Read("Foo" + i);
+                    readTree.Delete("Foo" + i, result.Version);
+                }
+            }
+        }
+    }
 }
