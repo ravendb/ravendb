1b6237b0f329a0f0f2a887c557c12cad11f63f4b
Efrat Shenhar <efrat@hibernatingrhinos.com>
2016-05-19 15:02:44 +0300

    Empty message handler

---
+++ src/Raven.Server/Web/System/EmptyMessageHandler.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="AdminDatabasesHandler.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Raven.Server.Routing;
+
+namespace Raven.Server.Web.System
+{
+    public class EmptyMessageHandler : RequestHandler
+    {
+        [RavenAction("/admin/emptymessage", "GET")]
+        public Task Get()
+        {
+           HttpContext.Response.StatusCode = 200;
+           return Task.CompletedTask;
+        }
+    }
+}
