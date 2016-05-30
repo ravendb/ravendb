5d1571a9438c9608fcb47bf323669b83c664c5e1
aviviadi <avivi.adi.hr@gmail.com>
2016-05-30 10:41:29 +0300

    Tryouts test

--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -1,163 +1,233 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Globalization;
 using System.IO;
-using System.IO.Compression;
-using System.Net.WebSockets;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using FastTests.Client.Indexing;
-using Raven.Abstractions.Data;
-using Raven.Abstractions.Json;
+using System.Linq;
+using Raven.Abstractions;
 using Raven.Client.Document;
-using Raven.Client.Platform;
-using Raven.Json.Linq;
-using JsonToken = Raven.Imports.Newtonsoft.Json.JsonToken;
+using Raven.Client.Extensions;
 
 namespace Tryouts
 {
-    public class Program
+    public class MassiveTest : IDisposable
     {
-        public class User
+        private Random _seedRandom = new Random();
+        private int _seed;
+        private Random _random;
+        private string _logFilename;
+        private DocumentStore store;
+        private long _seqId;
+
+        private class DocEntity
         {
-            public string FirstName { get; set; }
-
-            public string LastName { get; set; }
-
+            public object RandomId { get; set; }
+            public long SerialId { get; set; }
+            public object SomeRandomText { get; set; }
             public string[] Tags { get; set; }
         }
 
-        public static void Main(string[] args)
+        public MassiveTest(string url, int? seed = null)
         {
-            new IndexesFromClient().MoreLikeThis().Wait();
-        }
+            if (seed == null)
+                _seed = _seedRandom.Next();
+            else
+                _seed = seed.Value;
 
-        public class Order
-        {
-            public string Name;
-        }
+            _random = new Random(_seed);
 
-        private static void createNewDB(DocumentStore store)
-        {
-            store.DatabaseCommands.GlobalAdmin.CreateDatabase(new DatabaseDocument
+            Log("Seed = " + _seed);
+
+            store = new DocumentStore
             {
-                Id = "BenchmarkDB",
-                Settings =
+                Url = url,
+                DefaultDatabase = "test"
+            };
+
+            store.Initialize();
+        }
+
+        private void Log(string txt, bool isError = false)
+        {
+            if (_logFilename == null)
+            {
+                _logFilename = Path.GetTempFileName();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Loggin into " + _logFilename);
+                Console.ResetColor();
+            }
+            var errorStr = isError ? " *** ERROR ***" : "";
+            string txtToLog = SystemTime.UtcNow.ToString("G", new CultureInfo("he-il")) + errorStr + " : " + txt;
+            File.AppendAllText(_logFilename, txtToLog + Environment.NewLine);
+
+            Console.WriteLine(txtToLog);
+        }
+
+        public void CreateDb(string dbname = null)
+        {
+            try
+            {
+                if (dbname == null)
+                    dbname = "test";
+                var doc = MultiDatabase.CreateDatabaseDocument(dbname);
+                store.AsyncDatabaseCommands.GlobalAdmin.CreateDatabaseAsync(doc).Wait();
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("already exists"))
                 {
-                    {"Raven/DataDir", "~/BenchmarkDB"},
-                    {"Raven/ActiveBundles", ""}
+                    Log($"Database '{dbname}' already exists!");
                 }
-            });
-        }
-
-        private static async Task importData(DocumentStore store)
-        {
-            //using (var bulk = store.BulkInsert())
-            {
-
-                string filePath = @"C:\Users\ayende\Downloads\Dump of temp2, 2016-05-17 14-07.ravendump";
-                Stream dumpStream = File.OpenRead(filePath);
-                var gZipStream = new GZipStream(dumpStream, CompressionMode.Decompress, leaveOpen: true);
-                using (var streamReader = new StreamReader(gZipStream))
-                using (var reader = new RavenJsonTextReader(streamReader))
+                else
                 {
-
-                    if (reader.Read() == false /* { */|| reader.Read() == false /* prop*/)
-                        throw new InvalidOperationException("empty document?");
-
-                    if (reader.TokenType != JsonToken.PropertyName)
-                        throw new InvalidOperationException("Expected property");
-
-                    if ((string)reader.Value != "Docs")
-                        throw new InvalidOperationException("Expected property name 'Docs'");
-
-                    if (reader.Read() == false)
-                        throw new InvalidOperationException("corrupt document");
-
-                    if (reader.TokenType != JsonToken.StartArray)
-                        throw new InvalidOperationException("corrupt document, missing array");
-
-                    if (reader.Read() == false)
-                        throw new InvalidOperationException("corrupt document, array value");
-
-                    while (reader.TokenType != JsonToken.EndArray)
-                    {
-                        var document = RavenJObject.Load(reader);
-                        var metadata = document.Value<RavenJObject>("@metadata");
-                        var key = metadata.Value<string>("@id");
-                        document.Remove("@metadata");
-
-
-                        await store.AsyncDatabaseCommands.PutAsync(key, null, document, metadata);
-                        //await bulk.StoreAsync(document, metadata, key).ConfigureAwait(false);
-                        Console.WriteLine(key);
-                        if (reader.Read() == false)
-                            throw new InvalidOperationException("corrupt document, array value");
-                    }
+                    Log("Cannot create DB " + dbname + ". Exception : " + ex.Message, true);
+                    throw;
                 }
             }
         }
 
-        public class ContactClass
+        private string RandomString(int length)
         {
-            public string Name { get; set; }
-            public string Title { get; set; }
-        }
-        public class AddressClass
-        {
-            public string Line1 { get; set; }
-            public object Line2 { get; set; }
-            public string City { get; set; }
-            public object Region { get; set; }
-            public int PostalCode { get; set; }
-            public string Country { get; set; }
-        }
-        public class Company
-        {
-            public string ExternalId { get; set; }
-            public string Name { get; set; }
-            public ContactClass Contact { get; set; }
-            public AddressClass Address { get; set; }
-            public string Phone { get; set; }
-            public string Fax { get; set; }
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[_random.Next(s.Length)]).ToArray());
         }
 
-        private static async Task DoWork()
+        public void PerformBulkInsert(string collection, long numberOfDocuments, int? sizeOfDocuments,
+            bool useSeqId = true)
         {
-            using (var ws = new RavenClientWebSocket())
-            {
-                await ws.ConnectAsync(new Uri("ws://echo.websocket.org"), CancellationToken.None);
+            int docSize = sizeOfDocuments == null ? _random.Next(5 * 1024 * 1024) : sizeOfDocuments.Value;
+            var sizeStr = sizeOfDocuments == null ? $"Random Size of {docSize:#,#}" : $"size of {sizeOfDocuments:#,#}";
+            Log($"About to perform Bulk Insert for {numberOfDocuments:##,###} documents with " + sizeStr);
 
-                await
-                    ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("Hello there")),
-                        WebSocketMessageType.Text,
-                        true, CancellationToken.None);
+            var entities = new DocEntity[3];
+            var ids = new long[] { -1, -1, -1 };
 
-                var arraySegment = new ArraySegment<byte>(new byte[1024]);
-                var webSocketReceiveResult = await ws.ReceiveAsync(arraySegment, CancellationToken.None);
-                var s = Encoding.UTF8.GetString(arraySegment.Array, 0, webSocketReceiveResult.Count);
-                Console.WriteLine();
-                Console.WriteLine(s);
-            }
-        }
-        public static async Task BulkInsert(DocumentStore store, int numOfItems)
-        {
-            Console.Write("Doing bulk-insert...");
-
-            string[] tags = null;// Enumerable.Range(0, 1024*8).Select(x => "Tags i" + x).ToArray();
-
-            var sp = System.Diagnostics.Stopwatch.StartNew();
+            string[] tags = null;
+            long id = 1;
+            var sp = Stopwatch.StartNew();
             using (var bulkInsert = store.BulkInsert())
             {
-                int id = 1;
-                for (int i = 0; i < numOfItems; i++)
-                    await bulkInsert.StoreAsync(new User
+                for (long i = 0; i < numberOfDocuments; i++)
+                {
+                    if (i % (numberOfDocuments / 5) == 0)
+                        Console.WriteLine($"{SystemTime.UtcNow.ToString("G")} : Progress {i:##,###} out of {numberOfDocuments} ...");
+
+                    var entity = new DocEntity
                     {
-                        FirstName = $"First Name - {i}",
-                        LastName = $"Last Name - {i}",
+                        SerialId = i,
+                        RandomId = _random.Next(),
+                        SomeRandomText = RandomString(docSize),
                         Tags = tags
-                    }, $"users/{id++}");
+                    };
+                    var idToUse = useSeqId ? _seqId++ : id++;
+                    bulkInsert.StoreAsync(entity, $"{collection}/{idToUse}").Wait();
+
+                    if (i == 0)
+                    {
+                        ids[0] = idToUse;
+                        entities[0] = entity;
+                    }
+                    else if (i == numberOfDocuments / 2)
+                    {
+                        ids[1] = idToUse;
+                        entities[1] = entity;
+                    }
+                    else if (i == numberOfDocuments - 1)
+                    {
+                        ids[2] = idToUse;
+                        entities[2] = entity;
+                    }
+                }
+
+                if (ids[0] == -1 ||
+                    ids[1] == -1 ||
+                    ids[2] == -1)
+                    throw new Exception("Internal Error");
             }
-            Console.WriteLine("done in " + sp.Elapsed);
+
+            var elapsed = sp.ElapsedMilliseconds;
+
+            Log($"Finished Bulk Insert {numberOfDocuments:#,#} documents of size {docSize:#,#} at " + elapsed.ToString("#,#") + " mSec");
+
+            if (sizeOfDocuments == null)
+            {
+                Log($"STATISTICS: N/A - Random Size");
+            }
+            else
+            {
+                long totalSize = numberOfDocuments * docSize;
+                var totalRate = (totalSize / elapsed) / 1000;
+                Log($"STATISTICS: Total Size {totalSize:#,#}, {totalRate:#,#} MB/Sec");
+            }
+
+
+            using (var session = store.OpenSession())
+            {
+                var first = session.Load<DocEntity>($"{collection}/{ids[0]}");
+                var middle = session.Load<DocEntity>($"{collection}/{ids[1]}");
+                var last = session.Load<DocEntity>($"{collection}/{ids[2]}");
+
+                if (first == null || middle == null || last == null)
+                {
+                    Log(
+                        $"Data Verification Failed : isNull :: first={first == null}, middle={middle == null}, last={last == null}",
+                        true);
+                }
+
+                if (first.SerialId == 0 &&
+                    middle.SerialId == numberOfDocuments / 2 &&
+                    last.SerialId == numberOfDocuments - 1)
+                {
+                    Log("Data Successfully Verified!");
+                }
+                else
+                {
+                    Log(
+                        $"Data Verification Failed : first={first.SerialId}, middle={middle.SerialId}, last={last.SerialId}",
+                        true);
+                }
+            }
+
+        }
+
+        public void Dispose()
+        {
+            store.Dispose();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var massiveObj = new MassiveTest("http://127.0.0.1:8080", 1805861237))
+            {
+                massiveObj.CreateDb();
+
+                var k = 1000;
+                var kb = 1024;
+
+                if (args.Length == 1)
+                    k = Convert.ToInt32(args[0]);
+
+                Console.WriteLine("warmup...");
+                massiveObj.PerformBulkInsert("warmup", 10 * k, 2 * kb);
+                Console.WriteLine("smallSize...");
+                massiveObj.PerformBulkInsert("smallSize", 2 * k * k, 2 * kb);
+                Console.WriteLine("bigSize...");
+                massiveObj.PerformBulkInsert("bigSize", 1 * k, 30 * kb * kb);
+                Console.WriteLine("forOverrite...");
+                massiveObj.PerformBulkInsert("forOverrite", 500 * k, 5 * kb, false);
+                Console.WriteLine("forOverrite2...");
+                massiveObj.PerformBulkInsert("forOverrite", 1000 * k, 5 * kb, false);
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.WriteLine($"random {i}...");
+                    massiveObj.PerformBulkInsert("random", i * k * k, null);
+                }
+
+                Console.WriteLine("Ending tests...");
+            }
         }
     }
 }
