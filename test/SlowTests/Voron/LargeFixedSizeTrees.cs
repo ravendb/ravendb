7e5f47f3247c114a77f1542a58f475ff09f7b78e
Ayende Rahien <Ayende@ayende.com>
2016-06-09 22:32:05 +0200

    Reproducing test failure

--- test/SlowTests/Voron/LargeFixedSizeTrees.cs
+++ test/SlowTests/Voron/LargeFixedSizeTrees.cs
@@ -335,6 +335,7 @@ namespace SlowTests.Voron
         [InlineDataWithRandomSeed(1000000)]
         [InlineDataWithRandomSeed(2000000)]
         [InlineData(100000, 1684385375)]// reproduced a bug, do not remove
+        [InlineData(1000000, 288291468)]// reproduced a bug, do not remove
         public void CanDeleteRange_RandomRanges(int count, int seed)
         {
             var bytes = new byte[48];
--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -7,7 +7,15 @@ namespace Tryouts
     {
         static void Main(string[] args)
         {
-          
-        } 
+            for (int i = 0; i < 100; i++)
+            {
+                using (var x = new SlowTests.Voron.LargeFixedSizeTrees())
+                {
+                    x.CanDeleteRange_RandomRanges(count: 2000000, seed: 288291468);
+                }
+
+            }
+
+        }
     }
 }
