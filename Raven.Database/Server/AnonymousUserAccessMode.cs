ceacabf40894f7bbe29a37f46dbb41fc144a2c95
Paweł Pekról <ppekrol@ravendb.net>
2016-06-15 12:11:08 +0200

    RavenDB-4702 Mark AnonymousUserAccessMode All and Get as obsolete (#3855)

--- Raven.Database/Server/AnonymousUserAccessMode.cs
+++ Raven.Database/Server/AnonymousUserAccessMode.cs
@@ -3,11 +3,15 @@
 //     Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
+using System;
+
 namespace Raven.Database.Server
 {
     public enum AnonymousUserAccessMode
     {
+        [Obsolete("Will be removed in 4.0")]
         All,
+        [Obsolete("Will be removed in 4.0")]
         Get,
         None,
         Admin
