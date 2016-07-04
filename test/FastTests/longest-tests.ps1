82e751b63c7fab244a65544d85e9fadfcc317b95
Ayende Rahien <Ayende@ayende.com>
2016-07-04 11:01:09 +0300

    Fixing how we check for the longest running tests

--- test/FastTests/longest-tests.ps1
+++ test/FastTests/longest-tests.ps1
@@ -1,23 +1,7 @@
-function Take-Count() {
-    param ( [int]$count = $(throw "Need a count") )
-    begin { 
-        $total = 0;
-    }
-    process { 
-        if ( $total -lt $count ) {
-            $_
-        }
-        $total += 1
-    }
-}
-
-
-
-dnx test  -xml test-timings.xml
+dotnet test  -xml test-timings.xml
 
 [xml]$tests = Get-Content test-timings.xml
 $tests.assemblies.assembly.collection.test | 
     sort @{e={$_.time -as [double]} } -descending | 
-    % {$_.time + "  " + $_.name } | 
-    take-count 15
+    select time, name -first 10
 
