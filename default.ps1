249d8dab71983129487da84a18f73e9bb1465d27
Paweł Pekról <ppekrol@ravendb.net>
2016-07-05 21:56:55 +0200

    RavenDB-4653 Avoid NRE error in dotnet restore (#3946)

--- default.ps1
+++ default.ps1
@@ -109,9 +109,9 @@ task CompileHtml5 {
 
 task CompileDotNet {
 
-    &"$dotnet" restore Raven.Client.Lightweight Bundles\Raven.Client.Authorization Bundles\Raven.Client.UniqueConstraints
+    &"$dotnet" --verbose restore Raven.Sparrow\Sparrow Raven.Abstractions Raven.Client.Lightweight Bundles\Raven.Client.Authorization Bundles\Raven.Client.UniqueConstraints
     
-    &"$dotnet" build Raven.Client.Lightweight --configuration "$global:configuration" Bundles\Raven.Client.Authorization Bundles\Raven.Client.UniqueConstraints --build-base-path "$build_dir\DotNet"
+    &"$dotnet" build --verbose Raven.Client.Lightweight --configuration "$global:configuration" Bundles\Raven.Client.Authorization Bundles\Raven.Client.UniqueConstraints --build-base-path "$build_dir\DotNet"
 }
 
 task TestDotNet -depends CompileDotNet {
