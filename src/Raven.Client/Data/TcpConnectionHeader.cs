25733de01f4a7ca6cdcffa4c6e34303be25b7f47
Michael Yarichuk <michael.yarichuk@gmail.com>
2016-07-11 17:43:08 +0300

    add equality overrides to TcpConnectionHeader so it works properly as a key in Dictionary

--- src/Raven.Client/Data/TcpConnectionHeader.cs
+++ src/Raven.Client/Data/TcpConnectionHeader.cs
@@ -1,6 +1,8 @@
-﻿namespace Raven.Abstractions.Data
+﻿using System;
+
+namespace Raven.Abstractions.Data
 {
-	public class TcpConnectionHeaderMessage
+	public class TcpConnectionHeaderMessage : IEquatable<TcpConnectionHeaderMessage>
 	{
 		public enum OperationTypes
 		{
@@ -15,5 +17,41 @@
 		public string DatabaseId { get; set; }
 
 		public OperationTypes Operation { get; set; }
+
+		public bool Equals(TcpConnectionHeaderMessage other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return string.Equals(DatabaseName, other.DatabaseName) && string.Equals(DatabaseId, other.DatabaseId) && Operation == other.Operation;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != this.GetType()) return false;
+			return Equals((TcpConnectionHeaderMessage)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				var hashCode = DatabaseName?.GetHashCode() ?? 0;
+				hashCode = (hashCode*397) ^ (DatabaseId?.GetHashCode() ?? 0);
+				hashCode = (hashCode*397) ^ (int)Operation;
+				return hashCode;
+			}
+		}
+
+		public static bool operator ==(TcpConnectionHeaderMessage left, TcpConnectionHeaderMessage right)
+		{
+			return Equals(left, right);
+		}
+
+		public static bool operator !=(TcpConnectionHeaderMessage left, TcpConnectionHeaderMessage right)
+		{
+			return !Equals(left, right);
+		}
 	}
 }
