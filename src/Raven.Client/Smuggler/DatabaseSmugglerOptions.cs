8adeefca2278f3a8cc26be5ddc2da3278e8326af
Fitzchak Yitzchaki <fitzchak@ayende.com>
2016-07-12 12:32:35 +0300

    Remove FilterSetting, we will use JS filtering instead

--- src/Raven.Client/Smuggler/DatabaseSmugglerOptions.cs
+++ src/Raven.Client/Smuggler/DatabaseSmugglerOptions.cs
@@ -13,8 +13,6 @@ namespace Raven.Client.Smuggler
     {
         public DatabaseSmugglerOptions()
         {
-            Filters = new List<FilterSetting>();
-            ConfigureDefaultFilters();
             OperateOnTypes = DatabaseItemType.Indexes | DatabaseItemType.Documents | DatabaseItemType.Transformers;
         }
 
@@ -26,8 +24,6 @@ namespace Raven.Client.Smuggler
 
         public long? Limit { get; set; }
 
-        public List<FilterSetting> Filters { get; set; }
-
         public string TransformScript { get; set; }
 
         public bool SkipConflicted { get; set; }
@@ -37,45 +33,5 @@ namespace Raven.Client.Smuggler
         public int MaxStepsForTransformScript { get; set; }
 
         public string Database { get; set; }
-
-        public bool MatchFilters(RavenJObject document)
-        {
-            foreach (var filter in Filters)
-            {
-                bool anyRecords = false;
-                bool matchedFilter = false;
-                foreach (var tuple in document.SelectTokenWithRavenSyntaxReturningFlatStructure(filter.Path))
-                {
-                    if (tuple?.Item1 == null)
-                        continue;
-
-                    anyRecords = true;
-
-                    var val = tuple.Item1.Type == JTokenType.String
-                                  ? tuple.Item1.Value<string>()
-                                  : tuple.Item1.ToString(Formatting.None);
-                    matchedFilter |= filter.Values.Any(value => string.Equals(val, value, StringComparison.OrdinalIgnoreCase)) ==
-                                     filter.ShouldMatch;
-                }
-
-                if (filter.ShouldMatch == false && anyRecords == false) // RDBQA-7
-                    return true;
-
-                if (matchedFilter == false)
-                    return false;
-            }
-            return true;
-        }
-
-        private void ConfigureDefaultFilters()
-        {
-            // filter out encryption verification key document to enable import to encrypted db from encrypted db.
-            Filters.Add(new FilterSetting
-            {
-                Path = "@metadata.@id",
-                ShouldMatch = false,
-                Values = { Constants.InResourceKeyVerificationDocumentName }
-            });
-        }
     }
 }
