921c59b0c3671ad0e19d63243763f3f5e030e2bb
Ayende Rahien <ayende@ayende.com>
2016-07-14 14:02:40 +0300

    Fixing <= to < because we allow zero length adding

--- src/Voron/Data/Tables/TableValueBuilder.cs
+++ src/Voron/Data/Tables/TableValueBuilder.cs
@@ -36,7 +36,7 @@ namespace Voron.Data.Tables
 
         public void Add(byte* ptr, int size)
         {
-            if (size <= 0)
+            if (size < 0)
                 throw new ArgumentException("cannot be zero or negative", nameof(size));
 
             _values.Add(new PtrSize
