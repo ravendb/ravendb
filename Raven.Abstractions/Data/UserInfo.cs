685559e668836dcdbc93fa8cc0abb7be6f271dbd
Idan Haim <haimdude@gmail.com>
2016-07-21 14:02:27 +0300

    Add a summary to UserInfo and UserPermission (#4049)

--- Raven.Abstractions/Data/UserInfo.cs
+++ Raven.Abstractions/Data/UserInfo.cs
@@ -5,15 +5,39 @@ namespace Raven.Abstractions.Data
 {
     public class UserInfo
     {
+        /// <summary>
+        /// Inforamtion about the type of user.
+        /// </summary>
         public string Remark { get; set; }
+        /// <summary>
+        /// The specific Name of the user.
+        /// </summary>
         public string User { get; set; }
         public bool IsAdminGlobal { get; set; }
+        /// <summary>
+        /// If using current admin db.
+        /// </summary>
         public bool IsAdminCurrentDb { get; set; }
+        /// <summary>
+        /// A list of all the database on the server.
+        /// </summary>
         public List<DatabaseInfo> Databases { get; set; }
         public IPrincipal Principal { get; set; }
+        /// <summary>
+        /// Return a HashSet of all the admin database on the server.
+        /// </summary>
         public HashSet<string> AdminDatabases { get; set; }
+        /// <summary>
+        /// Return a HashSet of all the ReadOnly database on the server.
+        /// </summary>
         public HashSet<string> ReadOnlyDatabases { get; set; }
+        /// <summary>
+        ///Return a HashSet of all the ReadWrite database on the server.
+        /// </summary>
         public HashSet<string> ReadWriteDatabases { get; set; }
+        /// <summary>
+        /// Return the access token body.
+        /// </summary>
         public AccessTokenBody AccessTokenBody { get; set; }
     }
 
