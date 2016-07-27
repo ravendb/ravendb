b329ba41e9e28ee7fe6a72f4cfe988c9f43c8149
Arkadiusz Palinski <arek@ravendb.net>
2016-07-27 12:06:51 +0200

    RavenDB-4608 Auto map-reduce indexes should use the same mechanism to calculate the reduce key hash as the static ones

---
+++ src/Raven.Server/Documents/Indexes/MapReduce/ReduceKeyProcessor.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Sparrow;
+using Sparrow.Binary;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Indexes.MapReduce
+{
+    public unsafe class ReduceKeyProcessor
+    {
+        private readonly UnmanagedBuffersPool _buffersPool;
+        private readonly Mode _mode;
+        private UnmanagedBuffersPool.AllocatedMemoryData _buffer;
+        private int _bufferPos;
+        private ulong _singleValueHash;
+
+        public ReduceKeyProcessor(int numberOfReduceFields, UnmanagedBuffersPool buffersPool)
+        {
+            _buffersPool = buffersPool;
+            if (numberOfReduceFields == 1)
+            {
+                _mode = Mode.SingleValue;
+            }
+            else
+            {
+                _mode = Mode.MultipleValues;
+                _buffer = _buffersPool.Allocate(16);
+                _bufferPos = 0;
+            }
+        }
+
+        public void Init()
+        {
+            _bufferPos = 0;
+        }
+
+        public ulong Hash
+        {
+            get
+            {
+                switch (_mode)
+                {
+                    case Mode.SingleValue:
+                        return _singleValueHash;
+                    case Mode.MultipleValues:
+                        return Hashing.XXHash64.CalculateInline((byte*)_buffer.Address, _bufferPos);
+                    default:
+                        throw new NotSupportedException($"Unknown reduce value processing mode: {_mode}");
+                }
+            }
+        }
+
+        public void Process(object value)
+        {
+            var lsv = value as LazyStringValue;
+            if (lsv != null)
+            {
+                switch (_mode)
+                {
+                    case Mode.SingleValue:
+                        _singleValueHash = Hashing.XXHash64.Calculate(lsv.Buffer, lsv.Size);
+                        break;
+                    case Mode.MultipleValues:
+                        CopyToBuffer(lsv.Buffer, lsv.Size);
+                        break;
+                }
+
+                return;
+            }
+
+            var s = value as string;
+            if (s != null)
+            {
+                fixed (char* p = s)
+                {
+                    switch (_mode)
+                    {
+                        case Mode.SingleValue:
+                            _singleValueHash = Hashing.XXHash64.Calculate((byte*)p, s.Length * sizeof(char));
+                            break;
+                        case Mode.MultipleValues:
+                            CopyToBuffer((byte*)p, s.Length * sizeof(char));
+                            break;
+                    }
+                }
+
+                return;
+            }
+
+            var lcsv = value as LazyCompressedStringValue;
+            if (lcsv != null)
+            {
+                switch (_mode)
+                {
+                    case Mode.SingleValue:
+                        _singleValueHash = Hashing.XXHash64.Calculate(lcsv.Buffer, lcsv.CompressedSize);
+                        break;
+                    case Mode.MultipleValues:
+                        CopyToBuffer(lcsv.Buffer, lcsv.CompressedSize);
+                        break;
+                }
+
+                return;
+            }
+
+            if (value is long)
+            {
+                var l = (long)value;
+
+                switch (_mode)
+                {
+                    case Mode.SingleValue:
+                        _singleValueHash = Hashing.XXHash64.Calculate((byte*)&l, sizeof(long));
+                        break;
+                    case Mode.MultipleValues:
+                        CopyToBuffer((byte*)&l, sizeof(long));
+                        break;
+                }
+
+                return;
+            }
+
+            if (value is decimal)
+            {
+                var l = (decimal)value;
+
+                switch (_mode)
+                {
+                    case Mode.SingleValue:
+                        _singleValueHash = Hashing.XXHash64.Calculate((byte*)&l, sizeof(decimal));
+                        break;
+                    case Mode.MultipleValues:
+                        CopyToBuffer((byte*)&l, sizeof(decimal));
+                        break;
+                }
+
+                return;
+            }
+
+            throw new NotSupportedException($"Unhandled type: {value.GetType()}"); // TODO arek
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void CopyToBuffer(byte* value, int size)
+        {
+            if (_bufferPos + size > _buffer.SizeInBytes)
+            {
+                var newBuffer = _buffersPool.Allocate(Bits.NextPowerOf2(_bufferPos + size));
+                Memory.Copy((byte*)newBuffer.Address, (byte*)_buffer.Address, _buffer.SizeInBytes);
+
+                _buffersPool.Return(_buffer);
+                _buffer = newBuffer;
+            }
+
+            Memory.Copy((byte*)_buffer.Address + _bufferPos, value, size);
+            _bufferPos += size;
+        }
+
+        enum Mode
+        {
+            SingleValue,
+            MultipleValues
+        }
+    }
+}
--- src/Raven.Server/Documents/Indexes/MapReduce/Static/MapReduceIndex.cs
+++ src/Raven.Server/Documents/Indexes/MapReduce/Static/MapReduceIndex.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using Raven.Client.Data.Indexes;
 using Raven.Client.Indexing;
 using Raven.Server.Documents.Indexes.Persistence.Lucene;
@@ -10,8 +8,6 @@ using Raven.Server.Documents.Indexes.Persistence.Lucene.Documents;
 using Raven.Server.Documents.Indexes.Static;
 using Raven.Server.Documents.Indexes.Workers;
 using Raven.Server.ServerWide.Context;
-using Sparrow;
-using Sparrow.Binary;
 using Sparrow.Json;
 using Sparrow.Json.Parsing;
 using Voron;
@@ -217,163 +213,6 @@ namespace Raven.Server.Documents.Indexes.MapReduce.Static
                 public void Dispose()
                 {
                     Current.Data?.Dispose();
-                }
-            }
-
-            private unsafe class ReduceKeyProcessor
-            {
-                private readonly UnmanagedBuffersPool _buffersPool;
-                private readonly Mode _mode;
-                private UnmanagedBuffersPool.AllocatedMemoryData _buffer;
-                private int _bufferPos;
-                private ulong _singleValueHash;
-
-                public ReduceKeyProcessor(int numberOfReduceFields, UnmanagedBuffersPool buffersPool)
-                {
-                    _buffersPool = buffersPool;
-                    if (numberOfReduceFields == 1)
-                    {
-                        _mode = Mode.SingleValue;
-                    }
-                    else
-                    {
-                        _mode = Mode.MultipleValues;
-                        _buffer = _buffersPool.Allocate(16);
-                        _bufferPos = 0;
-                    }
-                }
-
-                public void Init()
-                {
-                    _bufferPos = 0;
-                }
-
-                public ulong Hash
-                {
-                    get
-                    {
-                        switch (_mode)
-                        {
-                            case Mode.SingleValue:
-                                return _singleValueHash;
-                            case Mode.MultipleValues:
-                                return Hashing.XXHash64.CalculateInline((byte*)_buffer.Address, _bufferPos);
-                            default:
-                                throw new NotSupportedException($"Unknown reduce value processing mode: {_mode}");
-                        }
-                    }
-                }
-
-                public void Process(object value)
-                {
-                    var lsv = value as LazyStringValue;
-                    if (lsv != null)
-                    {
-                        switch (_mode)
-                        {
-                            case Mode.SingleValue:
-                                _singleValueHash = Hashing.XXHash64.Calculate(lsv.Buffer, lsv.Size);
-                                break;
-                            case Mode.MultipleValues:
-                                CopyToBuffer(lsv.Buffer, lsv.Size);
-                                break;
-                        }
-
-                        return;
-                    }
-
-                    var s = value as string;
-                    if (s != null)
-                    {
-                        fixed (char* p = s)
-                        {
-                            switch (_mode)
-                            {
-                                case Mode.SingleValue:
-                                    _singleValueHash = Hashing.XXHash64.Calculate((byte*)p, s.Length * sizeof(char));
-                                    break;
-                                case Mode.MultipleValues:
-                                    CopyToBuffer((byte*)p, s.Length * sizeof(char));
-                                    break;
-                            }
-                        }
-                        
-                        return;
-                    }
-
-                    var lcsv = value as LazyCompressedStringValue;
-                    if (lcsv != null)
-                    {
-                        switch (_mode)
-                        {
-                            case Mode.SingleValue:
-                                _singleValueHash = Hashing.XXHash64.Calculate(lcsv.Buffer, lcsv.CompressedSize);
-                                break;
-                            case Mode.MultipleValues:
-                                CopyToBuffer(lcsv.Buffer, lcsv.CompressedSize);
-                                break;
-                        }
-
-                        return;
-                    }
-
-                    if (value is long)
-                    {
-                        var l = (long)value;
-
-                        switch (_mode)
-                        {
-                            case Mode.SingleValue:
-                                _singleValueHash = Hashing.XXHash64.Calculate((byte*)&l, sizeof(long));
-                                break;
-                            case Mode.MultipleValues:
-                                CopyToBuffer((byte*)&l, sizeof(long));
-                                break;
-                        }
-
-                        return;
-                    }
-
-                    if (value is decimal)
-                    {
-                        var l = (decimal)value;
-
-                        switch (_mode)
-                        {
-                            case Mode.SingleValue:
-                                _singleValueHash = Hashing.XXHash64.Calculate((byte*)&l, sizeof(decimal));
-                                break;
-                            case Mode.MultipleValues:
-                                CopyToBuffer((byte*)&l, sizeof(decimal));
-                                break;
-                        }
-
-                        return;
-                    }
-
-                    throw new NotSupportedException($"Unhandled type: {value.GetType()}"); // TODO arek
-                }
-
-                [MethodImpl(MethodImplOptions.AggressiveInlining)]
-                private void CopyToBuffer(byte* value, int size)
-                {
-                    if (_bufferPos + size > _buffer.SizeInBytes)
-                    {
-                        var newBuffer = _buffersPool.Allocate(Bits.NextPowerOf2(_bufferPos + size));
-                        Memory.Copy((byte*)newBuffer.Address, (byte*)_buffer.Address, _buffer.SizeInBytes);
-
-                        _buffersPool.Return(_buffer);
-                        _buffer = newBuffer;
-                    }
-
-                    Memory.Copy((byte*)_buffer.Address + _bufferPos, value, size);
-                    _bufferPos += size;
-                }
-
-                enum Mode
-                {
-                    SingleValue,
-                    MultipleValues
                 }
             }
         }
