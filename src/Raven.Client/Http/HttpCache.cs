34198f1aa31873b6b70d1bd3e0e9ffc7240d8bf8
Fitzchak Yitzchaki <fitzchak@ayende.com>
2016-08-01 14:04:25 +0300

    Add cache for http executer

---
+++ src/Raven.Client/Http/HttpCache.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Raven.Abstractions;
+using Sparrow.Json;
+
+namespace Raven.Client.Http
+{
+    public class HttpCache : IDisposable
+    {
+        private readonly long _maxSize;
+        private readonly ConcurrentDictionary<string, HttpCacheItem> _items = new ConcurrentDictionary<string, HttpCacheItem>();
+        private long _totalSize;
+        private readonly UnmanagedBuffersPool _unmanagedBuffersPool;
+
+        public HttpCache(long maxSize = 1024 * 1024L * 512L)
+        {
+            _maxSize = maxSize;
+            _unmanagedBuffersPool = new UnmanagedBuffersPool(nameof(HttpCache));
+        }
+
+        public unsafe class HttpCacheItem : IDisposable
+        {
+            public long Etag;
+            public byte* Ptr;
+            public int Size;
+            public DateTime LastServerUpdate;
+            public int Usages;
+            public int Utilization;
+            public UnmanagedBuffersPool.AllocatedMemoryData Allocation;
+            public HttpCache Cache;
+
+            public bool AddRef()
+            {
+                Interlocked.Increment(ref Utilization);
+                var addRef = Interlocked.Increment(ref Usages) > 1;
+                if (addRef == false)
+                {
+                    Interlocked.Decrement(ref Usages);
+                }
+                return addRef;
+            }
+
+            public void Release()
+            {
+                if (Interlocked.Decrement(ref Usages) != 0)
+                    return;
+
+                Cache._unmanagedBuffersPool.Return(Allocation);
+                Interlocked.Add(ref Cache._totalSize, -Size);
+                Allocation = null;
+#if DEBUG
+                GC.SuppressFinalize(this);
+#endif
+            }
+
+            public void Dispose()
+            {
+                Release();
+            }
+
+#if DEBUG
+            ~HttpCacheItem()
+            {
+                throw new InvalidOperationException("Did not release memory for cache item");
+            }
+#endif
+        }
+
+        private Task _cleanupTask;
+
+        public unsafe void Set(string url, long etag, BlittableJsonReaderObject result)
+        {
+            var mem = _unmanagedBuffersPool.Allocate(result.Size);
+            result.CopyTo((byte*)mem.Address);
+            if (Interlocked.Add(ref _totalSize, result.Size) > _maxSize)
+            {
+                if (_cleanupTask == null)
+                {
+                    var cleanup = new Task(FreeSpace);
+                    if (Interlocked.CompareExchange(ref _cleanupTask, cleanup, null) == null)
+                    {
+                        cleanup.ContinueWith(_ => Interlocked.Exchange(ref _cleanupTask, null));
+                        cleanup.Start();
+                    }
+                }
+            }
+            var httpCacheItem = new HttpCacheItem
+            {
+                Usages = 1,
+                Etag = etag,
+                Ptr = (byte*)mem.Address,
+                Size = result.Size,
+                Allocation = mem,
+                LastServerUpdate = SystemTime.UtcNow,
+                Cache = this,
+            };
+            _items.AddOrUpdate(url, httpCacheItem, (s, oldItem) =>
+            {
+                oldItem.Release();
+                return httpCacheItem;
+            });
+        }
+
+        private void FreeSpace()
+        {
+            var sizeCleared = 0L;
+            var sizeToClear = _maxSize / 4;
+            var start = SystemTime.UtcNow;
+
+            var items = _items.OrderBy(x => x.Value.LastServerUpdate)
+                .ToList();
+
+            foreach (var item in items)
+            {
+                var lastServerUpdate = item.Value.LastServerUpdate;
+                if (lastServerUpdate > start)
+                    continue;
+
+                var itemAge = SystemTime.UtcNow - lastServerUpdate;
+                if (sizeCleared > sizeToClear)
+                {
+                    if (itemAge.TotalMinutes <= 1)
+                        break; // we already met the quota, and we are in new items, just drop it
+
+                    if (item.Value.Utilization == 0)
+                    {
+                        sizeCleared += FreeItem(item);
+                    }
+                }
+                else
+                {
+                    sizeCleared += FreeItem(item);
+                }
+            }
+        }
+
+        private long FreeItem(KeyValuePair<string, HttpCacheItem> item)
+        {
+            HttpCacheItem value;
+            if (_items.TryRemove(item.Key, out value) == false)
+            {
+                return 0;
+            }
+            if (item.Value != value)
+            {
+                item.Value.Release();
+                value.Release();
+                return item.Value.Size + value.Size;
+            }
+            value.Release();
+            return value.Size;
+        }
+
+        public struct ReleaseCacheItem : IDisposable
+        {
+            public HttpCacheItem Item;
+
+            public TimeSpan Age
+            {
+                get
+                {
+                    if (Item == null)
+                        return TimeSpan.MaxValue;
+                    return SystemTime.UtcNow - Item.LastServerUpdate;
+                }
+            }
+
+            public void NotModified()
+            {
+                if (Item != null)
+                {
+                    Item.LastServerUpdate = SystemTime.UtcNow;
+                }
+            }
+
+            public void Dispose()
+            {
+                if (Item != null)
+                {
+                    Item.Release();
+                    Item = null;
+                }
+            }
+        }
+
+        public unsafe ReleaseCacheItem Get(JsonOperationContext context, string url, out long etag, out BlittableJsonReaderObject obj)
+        {
+            HttpCacheItem item;
+            if (_items.TryGetValue(url, out item))
+            {
+                if (item.AddRef())
+                {
+                    var releaser = new ReleaseCacheItem
+                    {
+                        Item = item
+                    };
+                    etag = item.Etag;
+                    obj = new BlittableJsonReaderObject(item.Ptr, item.Size, context);
+                    return releaser;
+                }
+            }
+            obj = null;
+            etag = 0;
+            return new ReleaseCacheItem();
+        }
+
+        public void Dispose()
+        {
+            foreach (var item in _items.Values)
+            {
+                item.Dispose();
+            }
+            _unmanagedBuffersPool.Dispose();
+        }
+    }
+}
---
+++ src/Raven.Client/Http/RequestExecuter.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Raven.Abstractions.Connection;
+using Raven.Client.Documents.Commands;
+using Sparrow.Json;
+
+namespace Raven.Client.Http
+{
+    public class RequestExecuter : IDisposable
+    {
+        public class AggresiveCacheOptions
+        {
+            public TimeSpan Duration;
+        }
+
+        public ThreadLocal<AggresiveCacheOptions> AggressiveCaching = new ThreadLocal<AggresiveCacheOptions>();
+
+        private readonly HttpCache _cache = new HttpCache();
+
+        private readonly HttpClient _httpClient;
+
+        public RequestExecuter()
+        {
+            var handler = new HttpClientHandler
+            {
+
+            };
+            _httpClient = new HttpClient(handler);
+
+
+        }
+
+        public void Execute(HttpRequestMessage requset)
+        {
+
+        }
+
+        public async Task<HttpResponseMessage> ExecuteAsync(HttpRequestMessage requset)
+        {
+            // _cache.TryGetValue(requset.RequestUri,)
+
+            var response = await _httpClient.SendAsync(requset);
+            return response;
+        }
+
+        public async Task ExecuteCommandAsync<TResult>(RavenCommand<TResult> command, JsonOperationContext context)
+        {
+            var request = command.CreateRequest();
+
+            long cachedEtag;
+            BlittableJsonReaderObject cachedValue;
+            using (var cachedItem = _cache.Get(context, request.RequestUri.ToString(), out cachedEtag, out cachedValue))
+            {
+                if (cachedEtag != 0)
+                {
+                    var aggresiveCacheOptions = AggressiveCaching.Value;
+                    if (aggresiveCacheOptions != null && cachedItem.Age < aggresiveCacheOptions.Duration)
+                    {
+                        command.SetResponse(cachedValue);
+                        return;
+                    }
+
+                    request.Headers.IfNoneMatch.Add(new EntityTagHeaderValue(cachedEtag.ToString()));
+                }
+
+                using (var response = await ExecuteAsync(request))
+                {
+                    // read response
+                    // error handling
+                    if (response.StatusCode == HttpStatusCode.NotModified)
+                    {
+                        cachedItem.NotModified();
+                        command.SetResponse(cachedValue);
+                        return;
+                    }
+                    // TODO: 404
+                    // TODO: 401, 403, 412
+                    // TODO: 503 <-- & other server down issues
+                    // TODO: 500 <-- raise error
+                    if (response.IsSuccessStatusCode == false)
+                    {
+                        throw new ErrorResponseException(response, "TODO");
+                    }
+
+                    using (var stream = await response.Content.ReadAsStreamAsync())
+                    {
+                        using (var blittableJsonReaderObject = await context.ReadForMemoryAsync(stream, "PutResult"))
+                        {
+                            command.SetResponse(blittableJsonReaderObject);
+                            if (response.Headers.ETag != null)
+                            {
+                                long etag;
+                                if (long.TryParse(response.Headers.ETag.Tag, out etag))
+                                {
+                                    _cache.Set(request.RequestUri.ToString(), etag, blittableJsonReaderObject);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _cache.Dispose();
+        }
+    }
+}
