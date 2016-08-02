fdca7f19bd4b9d6d9e186b187a6d0a40a3a1e4cd
grisha-kotler <grisha-kotler@users.noreply.github.com>
2016-08-02 18:38:30 +0300

    RavenDB-4917 - Side by side index recreates an identical index (#4126)

--- Raven.Client.Lightweight/Indexes/AbstractIndexCreationTask.cs
+++ Raven.Client.Lightweight/Indexes/AbstractIndexCreationTask.cs
@@ -332,6 +332,7 @@ namespace Raven.Client.Indexes
             try
             {
                 serverDef.IndexId = indexDefinition.IndexId;
+                indexDefinition.RemoveDefaultValues();
 
                 if (serverDef.Equals(indexDefinition, false))
                     return true;
