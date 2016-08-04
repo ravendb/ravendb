5d1c9dbdd02e67e366c5f425a703223a049e5380
talweiss1982 <tal@ayende.com>
2016-08-04 15:10:34 +0300

    Raven db 4853 (#4144)
    * RavenDB-1111:
    Adding default Failover settings for Raft tests
    
    * RavenDB-1111  fixing failing test CanCreateExtendAndRemoveFromCluster
    
    * RavenDB-4853  Making informative cluster statistics

--- Rachis/Rachis/Messages/AppendEntriesRequest.cs
+++ Rachis/Rachis/Messages/AppendEntriesRequest.cs
@@ -12,4 +12,24 @@ namespace Rachis.Messages
         public int EntriesCount { get { return Entries == null ? 0 : Entries.Length; } }
         public long LeaderCommit { get; set; }
     }
+    public class AppendEntriesRequestWithEntries : BaseMessage
+    {
+        public static AppendEntriesRequestWithEntries FromAppendEntriesRequest(AppendEntriesRequest message)
+        {
+            return new AppendEntriesRequestWithEntries
+            {
+                Term = message.Term,
+                PrevLogIndex = message.PrevLogIndex,
+                PrevLogTerm = message.PrevLogTerm,
+                Entries = message.Entries,
+                LeaderCommit = message.LeaderCommit
+            };
+        }
+        public long Term { get; set; }
+        public long PrevLogIndex { get; set; }
+        public long PrevLogTerm { get; set; }
+        public LogEntry[] Entries { get; set; }
+        public int EntriesCount { get { return Entries == null ? 0 : Entries.Length; } }
+        public long LeaderCommit { get; set; }
+    }
 }
