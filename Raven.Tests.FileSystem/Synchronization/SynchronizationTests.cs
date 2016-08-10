53afa58dee68fb579f008d371f17ae6c8147e5ff
Arkadiusz Paliński <arek@ravendb.net>
2016-08-10 09:04:15 +0200

    RavenDB-4961 Fixing test (#4187)

--- Raven.Tests.FileSystem/Synchronization/SynchronizationTests.cs
+++ Raven.Tests.FileSystem/Synchronization/SynchronizationTests.cs
@@ -776,7 +776,6 @@ namespace Raven.Tests.FileSystem.Synchronization
             var synchronizationReport = await source.Synchronization.StartAsync("test.bin", destination);
 
             Assert.Equal(NoSyncReason.NoNeedToDeleteNonExistigFile.GetDescription(), synchronizationReport.Exception.Message);
-            Assert.Null(synchronizationReport.Exception);
         }
 
         [Fact]
