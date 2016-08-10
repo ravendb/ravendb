62c9b672d17da2eb060ee7919bc90844bda8734e
Fitzchak Yitzchaki <fitzchak@ayende.com>
2016-08-10 10:28:24 +0300

    RavenDB-4902 - ServerNode should have equality support in order to be used from HashSet

--- src/Raven.Client/Http/ServerNode.cs
+++ src/Raven.Client/Http/ServerNode.cs
@@ -6,5 +6,32 @@
         public string Database;
         public string ApiKey;
         public string CurrentToken;
+        public bool IsFailed;
+
+        private bool Equals(ServerNode other)
+        {
+            return string.Equals(Url, other.Url) && 
+                string.Equals(Database, other.Database) && 
+                string.Equals(ApiKey, other.ApiKey);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((ServerNode) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Url?.GetHashCode() ?? 0;
+                hashCode = (hashCode*397) ^ (Database?.GetHashCode() ?? 0);
+                hashCode = (hashCode*397) ^ (ApiKey?.GetHashCode() ?? 0);
+                return hashCode;
+            }
+        }
     }
 }
