96faae5a68897332428ceb64a3ea768b7c5aab95
Arkadiusz Paliński <arek@ravendb.net>
2016-08-11 20:06:48 +0200

    RavenDB-4961 Prevent from getting the synchronization process stuck if all deletions in the synchronization batch are filtered out because we sync to empty file system (so there is nothing to delete there) (#4202)

--- Raven.Abstractions/FileSystem/SynchronizationDestination.cs
+++ Raven.Abstractions/FileSystem/SynchronizationDestination.cs
@@ -68,5 +68,10 @@ namespace Raven.Abstractions.FileSystem
                 return hashCode;
             }
         }
+
+        public override string ToString()
+        {
+            return Url;
+        }
     }
 }
