c37746fc3a7ee27cdd6263bb3609ccbb97839a26
aviviadi <avivi.adi.hr@gmail.com>
2016-08-29 11:27:01 +0300

    TrafficWatch notifications (with TODOs)

--- src/Raven.Server/RavenServerStartup.cs
+++ src/Raven.Server/RavenServerStartup.cs
@@ -6,11 +6,15 @@ using System.Xml;
 using AsyncFriendlyStackTrace;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Config;
-
+using Raven.Abstractions;
+using Raven.Abstractions.Connection;
+using Raven.Abstractions.Data;
 using Raven.Server.Routing;
+using Raven.Server.TrafficWatch;
 using LogManager = NLog.LogManager;
 
 namespace Raven.Server
@@ -34,7 +38,23 @@ namespace Raven.Server
             {
                 try
                 {
-                    await router.HandlePath(context, context.Request.Method, context.Request.Path.Value);
+                    var sp = Stopwatch.StartNew();
+                    var tenantName = await router.HandlePath(context, context.Request.Method, context.Request.Path.Value);
+
+                    var trafficWatchData = new TrafficWatchNotification
+                    {
+                        RequestUri = context.Request.GetDisplayUrl(), // TODO ADIADI - we need RequestUri.. 
+                        ElapsedMilliseconds = sp.ElapsedMilliseconds,
+                        CustomInfo = "N/A", // TODO ADIADI :: ??
+                        HttpMethod = context.Request.Method,
+                        ResponseStatusCode = context.Response.StatusCode,
+                        TenantName = tenantName ?? "<system>", // TODO ADIADI - <system> ?
+                        TimeStamp = SystemTime.UtcNow,
+                        InnerRequestsCount = 0, // TODO ADIADI - ??
+                        QueryTimings = null // TODO ADIADI - ???
+                    };
+
+                    TrafficWatchManager.DispatchMessage(trafficWatchData);
                 }
                 catch (Exception e)
                 {
