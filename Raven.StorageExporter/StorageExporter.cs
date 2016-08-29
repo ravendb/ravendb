86763fbb314bf3a34f6919aa4e95000bdaa951aa
Grisha Kotler <grisha-kotler@users.noreply.github.com>
2016-08-29 19:23:49 +0300

    RavenDB-5179 - Cache is being set when using periodic export (both esent and voron) (#4306)

--- Raven.StorageExporter/StorageExporter.cs
+++ Raven.StorageExporter/StorageExporter.cs
@@ -11,6 +11,7 @@ using Raven.Bundles.Compression.Plugin;
 using Raven.Bundles.Encryption.Plugin;
 using Raven.Bundles.Encryption.Settings;
 using Raven.Database.Config;
+using Raven.Database.Impl;
 using Raven.Database.Plugins;
 using Raven.Database.Storage;
 using Raven.Imports.Newtonsoft.Json;
@@ -31,6 +32,7 @@ namespace Raven.StorageExporter
             var ravenConfiguration = new RavenConfiguration
             {
                 DataDirectory = databaseBaseDirectory,
+                CacheDocumentsInMemory = false,
                 Storage =
                 {
                     PreventSchemaUpdate = true,
@@ -124,14 +126,17 @@ namespace Raven.StorageExporter
 
             storage.Batch(accsesor => totalDocsCount = accsesor.Documents.GetDocumentsCount());
 
-            if (DocumentsStartEtag == Etag.Empty)
+            using (DocumentCacher.SkipSetDocumentsInDocumentCache())
             {
-                ExtractDocuments(jsonWriter, totalDocsCount);
-            }
-            else
-            {
-                ExtractDocumentsFromEtag(jsonWriter, totalDocsCount);
-            }
+                if (DocumentsStartEtag == Etag.Empty)
+                {
+                    ExtractDocuments(jsonWriter, totalDocsCount);
+                }
+                else
+                {
+                    ExtractDocumentsFromEtag(jsonWriter, totalDocsCount);
+                }
+            }  
         }
 
         private void ExtractDocuments(JsonTextWriter jsonWriter, long totalDocsCount)
