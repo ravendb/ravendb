a854e5ed0d9c28a5973b48b60f657ac1bfb164ce
Grisha Kotler <grisha@ayende.com>
2016-09-12 14:29:55 +0300

    RavenDB-5270 - fix test: CanLoadLongQuerry

--- Raven.Tests.Issues/RavenDb_1816.cs
+++ Raven.Tests.Issues/RavenDb_1816.cs
@@ -1,10 +1,7 @@
-using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using Raven.Abstractions.Data;
 using Raven.Client;
-using Raven.Client.Document;
 using Raven.Client.Indexes;
 using Raven.Client.Linq;
 using Raven.Tests.Common;
@@ -14,22 +11,21 @@ namespace Raven.Tests.Issues
 {
     public class RavenDb_1816 : RavenTest
     {
-        private const string DbName = "TestDb";
-        private const int Quantity = 1000;
-
-
         [Fact]
         public void CanLoadLongQuerry()
         {
             List<int> list = Enumerable.Range(1, 3000).ToList();
-            using (IDocumentStore store = NewRemoteDocumentStore())
+            using (var server = GetNewServer(configureConfig: configuration =>
+            {
+                configuration.MaxClauseCount = 6000;
+            }))
+            using (IDocumentStore store = NewRemoteDocumentStore(ravenDbServer: server))
             {
                 new LaptopIndex().Execute(store);
                 using (IDocumentSession session = store.OpenSession())
                 {
                     IRavenQueryable<Laptop> q = session.Query<Laptop, LaptopIndex>()
                         .Where(x => x.Id.In(list));
-
 
                     var s = q.ToString();
 
@@ -64,6 +60,4 @@ namespace Raven.Tests.Issues
             public string RamSizeInMegabatye { get; set; }
         }
     }
-
-    
 }
