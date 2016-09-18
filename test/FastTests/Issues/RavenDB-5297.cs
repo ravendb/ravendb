c4ad7f5b1c108fbc3ff0a583d89114ab32636aae
IdanHaim <haimdude@gmail.com>
2016-09-18 15:32:40 +0300

    RavenDB-5297 fixed minus Operator in lucene query

---
+++ test/FastTests/Issues/RavenDB-5297.cs
@@ -0,0 +1,81 @@
+﻿using Raven.Client.Data;
+using Raven.Client.Indexes;
+using Raven.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FastTests.Issues
+{
+    public class User
+    {
+        public string Name { get; set; }
+    }
+
+    public class UsersByName : AbstractIndexCreationTask<User>
+    {
+        public override string IndexName
+        {
+            get { return "Users/ByName"; }
+        }
+
+        public UsersByName()
+        {
+            Map = users => from user in users
+                           select new
+                           {
+                               user.Name
+                           };
+        }
+    }
+    public class RavenDB_5297: RavenTestBase
+    {
+        [Fact]
+        public void QueryLuceneMinusOperator()
+        {
+            using (var store = GetDocumentStore())
+            {
+                store.Initialize();
+                new UsersByName().Execute(store);
+
+                store.DatabaseCommands.Put("users/1", null,
+                    new RavenJObject { { "Name", "First" } },
+                    new RavenJObject { { "Raven-Entity-Name", "Users" } });
+
+                store.DatabaseCommands.Put("users/2", null,
+                    new RavenJObject { { "Name", "Second" } },
+                    new RavenJObject { { "Raven-Entity-Name", "Users" } });
+
+                WaitForIndexing(store);
+
+                var query = store.DatabaseCommands.Query("Users/ByName", new IndexQuery { Query = "Name:* -First" });
+                Assert.Equal(query.TotalResults, 1);
+            }
+        }
+
+        [Fact]
+        public void QueryLuceneNotOperator()
+        {
+            using (var store = GetDocumentStore())
+            {
+                store.Initialize();
+                new UsersByName().Execute(store);
+
+                store.DatabaseCommands.Put("users/1", null,
+                    new RavenJObject { { "Name", "First" } },
+                    new RavenJObject { { "Raven-Entity-Name", "Users" } });
+
+                store.DatabaseCommands.Put("users/2", null,
+                    new RavenJObject { { "Name", "Second" } },
+                    new RavenJObject { { "Raven-Entity-Name", "Users" } });
+
+                WaitForIndexing(store);
+
+                var query = store.DatabaseCommands.Query("Users/ByName", new IndexQuery { Query = "Name:* -Second" });
+                Assert.Equal(query.TotalResults, 1);
+            }
+        }
+    }
+}
