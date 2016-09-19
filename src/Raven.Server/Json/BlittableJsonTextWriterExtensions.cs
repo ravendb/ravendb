2346c0ac3f034f2827f02eb765973904bcf473e9
Ayende Rahien <ayende@ayende.com>
2016-09-19 15:11:20 +0300

    Output the last processed etag for each index

--- src/Raven.Server/Json/BlittableJsonTextWriterExtensions.cs
+++ src/Raven.Server/Json/BlittableJsonTextWriterExtensions.cs
@@ -639,6 +639,10 @@ namespace Raven.Server.Json
                 writer.WriteString((index.Name));
                 writer.WriteComma();
 
+                writer.WritePropertyName((nameof(index.LastProcessedEtag)));
+                writer.WriteInteger(index.LastProcessedEtag);
+                writer.WriteComma();
+
                 writer.WritePropertyName((nameof(index.IndexId)));
                 writer.WriteInteger(index.IndexId);
                 writer.WriteComma();
