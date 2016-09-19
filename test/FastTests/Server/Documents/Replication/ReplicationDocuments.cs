904c1dcc79435551f9a166f78f4558e16553dd45
Michael Yarichuk <michael.yarichuk@gmail.com>
2016-09-19 14:14:50 +0300

    fixing whitespace (RavenDB-5250)

--- test/FastTests/Server/Documents/Replication/ReplicationDocuments.cs
+++ test/FastTests/Server/Documents/Replication/ReplicationDocuments.cs
@@ -3,7 +3,7 @@
 //      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
 //  </copyright>
 // -----------------------------------------------------------------------
-
+ 
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
