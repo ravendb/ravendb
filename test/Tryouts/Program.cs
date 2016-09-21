43d8e237d41b33b5b0e292ea3534870f97955fd0
Michael Yarichuk <michael.yarichuk@gmail.com>
2016-09-21 16:26:58 +0300

    fixing race condition in a test (RavenDB-5250)

--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
+using FastTests.Server.Documents.Replication;
 using Raven.Client.Document;
 using Raven.Client.Smuggler;
 using SlowTests.Voron;
@@ -14,9 +15,13 @@ namespace Tryouts
        
         public static void Main(string[] args)
         {
-            using (var x = new SlowTests.MailingList.Bassler())
+            for (int i = 0; i < 1000; i++)
             {
-                x.Test();
+                using (var x = new AutomaticConflictResolution())
+                {
+                    x.Resolve_to_latest_version_tombstone_is_latest_the_incoming_document_is_replicated();
+                }
+                Console.WriteLine(i + 1);
             }
             //Console.WriteLine("Starting");
             //var sp = Stopwatch.StartNew();
