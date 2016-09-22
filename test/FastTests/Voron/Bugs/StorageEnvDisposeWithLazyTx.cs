ba3e42f296d7d7839241d060b16f4044ed5996aa
aviviadi <avivi.adi.hr@gmail.com>
2016-09-22 14:17:35 +0300

    Change disposal order in storage environ to allow flushing lazy TXs which where not written yet to journal on storage disposal

---
+++ test/FastTests/Voron/Bugs/StorageEnvDisposeWithLazyTx.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="StorageEnvDisposeWithLazyTx.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Sparrow.Logging;
+using Xunit;
+using Voron;
+
+namespace FastTests.Voron.Bugs
+{
+    public class StorageEnvDisposeWithLazyTx
+    {
+        [Fact]
+        public void CanDisposeStorageWithLazyTx()
+        {
+            using (var env = new StorageEnvironment(StorageEnvironmentOptions.CreateMemoryOnly()))
+            {
+                using (var tx = env.WriteTransaction())
+                {
+                    tx.LowLevelTransaction.IsLazyTransaction = true;
+
+                    var tree = tx.FixedTreeFor(Slice.From(StorageEnvironment.LabelsContext, "World"), 8);
+                    tree.Add(1, Slice.From(StorageEnvironment.LabelsContext, "Hello123"));
+                    tx.Commit();
+                }
+                // delibrately not commiting non-lazy-tx, should not throw
+            }
+        }
+    }
+}
