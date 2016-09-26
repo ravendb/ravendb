6c1515b48e6c2c3b2e4b1132f2c480d34a1d88b9
iftahbe <iftahbe@gmail.com>
2016-09-26 15:59:05 +0300

    RavenDB-5243 Add Query_By_Index test

--- test/FastTests/NewClient/Query.cs
+++ test/FastTests/NewClient/Query.cs
@@ -55,6 +55,83 @@ namespace FastTests.NewClient
                 }
             }
         }
+
+        [Fact]
+        public void Query_By_Index()
+        {
+            using (var store = GetDocumentStore())
+            {
+                new DogsIndex().Execute(store);
+                using (var newSession = store.OpenNewSession())
+                {
+                    newSession.Store(new Dog { Name = "Snoopy", Breed = "Beagle", Color = "White", Age = 6, IsVaccinated = true}, "dogs/1");
+                    newSession.Store(new Dog { Name = "Brian", Breed = "Labrador", Color = "White", Age = 12, IsVaccinated = false }, "dogs/2");
+                    newSession.Store(new Dog { Name = "Django", Breed = "Jack Russel", Color = "Black", Age = 3, IsVaccinated = true }, "dogs/3");
+                    newSession.Store(new Dog { Name = "Beethoven", Breed = "St. Bernard", Color = "Brown", Age = 1, IsVaccinated = false }, "dogs/4");
+                    newSession.Store(new Dog { Name = "Scooby Doo", Breed = "Great Dane", Color = "Brown", Age = 0, IsVaccinated = false }, "dogs/5");
+                    newSession.Store(new Dog { Name = "Old Yeller", Breed = "Black Mouth Cur", Color = "White", Age = 2, IsVaccinated = true }, "dogs/6");
+                    newSession.Store(new Dog { Name = "Benji", Breed = "Mixed", Color = "White", Age = 0, IsVaccinated = false }, "dogs/7");
+                    newSession.Store(new Dog { Name = "Lassie", Breed = "Collie", Color = "Brown", Age = 6, IsVaccinated = true }, "dogs/8");
+
+                    newSession.SaveChanges();
+
+                    WaitForIndexing(store);
+
+                    var queryResult = newSession.Query<DogsIndex.Result, DogsIndex>()
+                        .Where(x => x.Age > 2 && x.IsVaccinated == false)
+                        .ToList();
+
+                    Assert.Equal(queryResult.Count, 1);
+                    Assert.Equal(queryResult[0].Name, "Brian");
+
+                    var queryResult2 = newSession.Query<DogsIndex.Result, DogsIndex>()
+                        .Where(x => x.Age <= 2 && x.IsVaccinated == false)
+                        .ToList();
+
+                    Assert.Equal(queryResult2.Count, 3);
+
+                    var list = new List<string>();
+                    foreach (var dog in queryResult2)
+                    {
+                        list.Add(dog.Name);
+                    }
+                    Assert.True(list.Contains("Beethoven"));
+                    Assert.True(list.Contains("Scooby Doo"));
+                    Assert.True(list.Contains("Benji")); 
+                }
+            }
+        }
+
+        public class Dog
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string Breed { get; set; }
+            public string Color { get; set; }
+            public int Age { get; set; }
+            public bool IsVaccinated { get; set; }
+        }
+
+        public class DogsIndex : AbstractIndexCreationTask<Dog>
+        {
+            public class Result
+            {
+                public string Name { get; set; }
+                public int Age { get; set; }
+                public bool IsVaccinated { get; set; }
+            }
+
+            public DogsIndex()
+            {
+                Map = dogs => from dog in dogs
+                              select new
+                              {
+                                  dog.Name,
+                                  dog.Age,
+                                  dog.IsVaccinated
+                              };
+            }
+        }
     }
 }
 
