a51f76860045f04e2afb279cc7634a9992a3de8a
iftahbe <iftahbe@gmail.com>
2016-09-27 14:02:52 +0300

    RavenDB-5243 V4.0 client long runnign task - Use async session in async tests

--- test/FastTests/NewClient/QueryAsync.cs
+++ test/FastTests/NewClient/QueryAsync.cs
@@ -15,20 +15,16 @@ namespace FastTests.NewClient
         [Fact]
         public async void QueryAsync_Simple()
         {
-            // TODO Iftah, when SaveChangesAsync works make sure to use it here and in LoadAsync tests
             using (var store = GetDocumentStore())
             {
-                using (var session = store.OpenNewSession())
+                using (var asyncSession = store.OpenNewAsyncSession())
                 {
-                    session.Store(new User { Name = "John" }, "users/1");
-                    session.Store(new User { Name = "Jane" }, "users/2");
-                    session.Store(new User { Name = "Tarzan" }, "users/3");
-                    session.SaveChanges();
-                }
+                    await asyncSession.StoreAsync(new User { Name = "John" }, "users/1");
+                    await asyncSession.StoreAsync(new User { Name = "Jane" }, "users/2");
+                    await asyncSession.StoreAsync(new User { Name = "Tarzan" }, "users/3");
+                    await asyncSession.SaveChangesAsync();
 
-                using (var newAsyncSession = store.OpenNewAsyncSession())
-                {
-                    var queryResult = await newAsyncSession.Query<User>()
+                    var queryResult = await asyncSession.Query<User>()
                         .NewToListAsync();
 
                     Assert.Equal(queryResult.Count, 3);
@@ -41,21 +37,18 @@ namespace FastTests.NewClient
         {
             using (var store = GetDocumentStore())
             {
-                using (var session = store.OpenNewSession())
+                using (var asyncSession = store.OpenNewAsyncSession())
                 {
-                    session.Store(new User { Name = "John" }, "users/1");
-                    session.Store(new User { Name = "Jane" }, "users/2");
-                    session.Store(new User { Name = "Tarzan" }, "users/3");
-                    session.SaveChanges();
-                }
+                    await asyncSession.StoreAsync(new User { Name = "John" }, "users/1");
+                    await asyncSession.StoreAsync(new User { Name = "Jane" }, "users/2");
+                    await asyncSession.StoreAsync(new User { Name = "Tarzan" }, "users/3");
+                    await asyncSession.SaveChangesAsync();
 
-                using (var newAsyncSession = store.OpenNewAsyncSession())
-                {
-                    var queryResult = await newAsyncSession.Query<User>()
+                    var queryResult = await asyncSession.Query<User>()
                         .Where(x => x.Name.StartsWith("J"))
                         .NewToListAsync();
 
-                    var queryResult2 = await newAsyncSession.Query<User>()
+                    var queryResult2 = await asyncSession.Query<User>()
                         .Where(x => x.Name.Equals("Tarzan"))
                         .NewToListAsync();
 
@@ -71,33 +64,30 @@ namespace FastTests.NewClient
             using (var store = GetDocumentStore())
             {
                 new DogsIndex().Execute(store);
-
-                using (var session = store.OpenNewSession())
-                {
-                    session.Store(new Dog { Name = "Snoopy", Breed = "Beagle", Color = "White", Age = 6, IsVaccinated = true}, "dogs/1");
-                    session.Store(new Dog { Name = "Brian", Breed = "Labrador", Color = "White", Age = 12, IsVaccinated = false }, "dogs/2");
-                    session.Store(new Dog { Name = "Django", Breed = "Jack Russel", Color = "Black", Age = 3, IsVaccinated = true }, "dogs/3");
-                    session.Store(new Dog { Name = "Beethoven", Breed = "St. Bernard", Color = "Brown", Age = 1, IsVaccinated = false }, "dogs/4");
-                    session.Store(new Dog { Name = "Scooby Doo", Breed = "Great Dane", Color = "Brown", Age = 0, IsVaccinated = false }, "dogs/5");
-                    session.Store(new Dog { Name = "Old Yeller", Breed = "Black Mouth Cur", Color = "White", Age = 2, IsVaccinated = true }, "dogs/6");
-                    session.Store(new Dog { Name = "Benji", Breed = "Mixed", Color = "White", Age = 0, IsVaccinated = false }, "dogs/7");
-                    session.Store(new Dog { Name = "Lassie", Breed = "Collie", Color = "Brown", Age = 6, IsVaccinated = true }, "dogs/8");
-
-                    session.SaveChanges();
-                }
                 
-                using (var newAsyncSession = store.OpenNewAsyncSession())
+                using (var asyncSession = store.OpenNewAsyncSession())
                 {
+                    await asyncSession.StoreAsync(new Dog { Name = "Snoopy", Breed = "Beagle", Color = "White", Age = 6, IsVaccinated = true }, "dogs/1");
+                    await asyncSession.StoreAsync(new Dog { Name = "Brian", Breed = "Labrador", Color = "White", Age = 12, IsVaccinated = false }, "dogs/2");
+                    await asyncSession.StoreAsync(new Dog { Name = "Django", Breed = "Jack Russel", Color = "Black", Age = 3, IsVaccinated = true }, "dogs/3");
+                    await asyncSession.StoreAsync(new Dog { Name = "Beethoven", Breed = "St. Bernard", Color = "Brown", Age = 1, IsVaccinated = false }, "dogs/4");
+                    await asyncSession.StoreAsync(new Dog { Name = "Scooby Doo", Breed = "Great Dane", Color = "Brown", Age = 0, IsVaccinated = false }, "dogs/5");
+                    await asyncSession.StoreAsync(new Dog { Name = "Old Yeller", Breed = "Black Mouth Cur", Color = "White", Age = 2, IsVaccinated = true }, "dogs/6");
+                    await asyncSession.StoreAsync(new Dog { Name = "Benji", Breed = "Mixed", Color = "White", Age = 0, IsVaccinated = false }, "dogs/7");
+                    await asyncSession.StoreAsync(new Dog { Name = "Lassie", Breed = "Collie", Color = "Brown", Age = 6, IsVaccinated = true }, "dogs/8");
+
+                    await asyncSession.SaveChangesAsync();
+
                     WaitForIndexing(store);
 
-                    var queryResult = await newAsyncSession.Query<DogsIndex.Result, DogsIndex>()
+                    var queryResult = await asyncSession.Query<DogsIndex.Result, DogsIndex>()
                         .Where(x => x.Age > 2 && x.IsVaccinated == false)
                         .NewToListAsync();
 
                     Assert.Equal(queryResult.Count, 1);
                     Assert.Equal(queryResult[0].Name, "Brian");
 
-                    var queryResult2 = await newAsyncSession.Query<DogsIndex.Result, DogsIndex>()
+                    var queryResult2 = await asyncSession.Query<DogsIndex.Result, DogsIndex>()
                         .Where(x => x.Age <= 2 && x.IsVaccinated == false)
                         .NewToListAsync();
 
