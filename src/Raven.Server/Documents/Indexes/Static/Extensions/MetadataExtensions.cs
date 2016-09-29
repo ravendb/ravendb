15a41795b003b8a1b5a2559cb71e635febde9441
Pawel Pekrol <ppekrol@ravendb.net>
2016-09-29 12:24:53 +0200

    RavenDB-5359 added WhereEntityIs and IfEntityIs

---
+++ src/Raven.Server/Documents/Indexes/Static/Extensions/MetadataExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Raven.Client.Linq;
+using System.Linq;
+
+namespace Raven.Server.Documents.Indexes.Static.Extensions
+{
+    public static class MetadataExtensions
+    {
+        public static IEnumerable<dynamic> WhereEntityIs(this IEnumerable<dynamic> self, params string[] collections)
+        {
+            foreach (var document in self)
+            {
+                var dynamicBlittableJson = document as DynamicBlittableJson;
+                if (dynamicBlittableJson == null)
+                    continue;
+
+                var collectionName = CollectionName.GetCollectionName(dynamicBlittableJson);
+                if (collections.Contains(collectionName, StringComparer.OrdinalIgnoreCase) == false)
+                    continue;
+
+                yield return document;
+            }
+        }
+
+        public static dynamic IfEntityIs(this object self, string collection)
+        {
+            var document = self as DynamicBlittableJson;
+            if (document == null)
+                return DynamicNullObject.Null;
+
+            var collectionName = CollectionName.GetCollectionName(document);
+            if (string.Equals(collection, collectionName, StringComparison.OrdinalIgnoreCase) == false)
+                return DynamicNullObject.Null;
+
+            return document;
+        }
+    }
+}
