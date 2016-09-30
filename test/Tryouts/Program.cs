2c82fe674dad56b6564ae8b39fa733598a3327c1
Ayende Rahien <Ayende@ayende.com>
2016-09-30 11:12:24 +0300

    Fixed IoMetricsHandler namespace and added a way to test indexing time for bench

--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -1,15 +1,20 @@
-﻿using System;
+﻿using System.Linq;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Xml;
 using System.Xml.Linq;
 using Raven.Abstractions.Extensions;
+using Raven.Abstractions.Indexing;
 using Raven.Client.Document;
 using Raven.Client.Exceptions;
+using Raven.Client.Indexes;
 using Raven.Imports.Newtonsoft.Json;
 using Raven.Json.Linq;
 using Sparrow;
@@ -19,174 +24,250 @@ namespace Tryouts
 {
     public class Program
     {
-        private static readonly ByteStringContext _byteStringContext = new ByteStringContext();
-
         public static void Main(string[] args)
         {
-
-            using (var a = new FastTests.Server.Documents.Indexing.Auto.BasicAutoMapIndexing())
+            using (var store = new DocumentStore
             {
-                a.AutoIndexesShouldBeMarkedAsIdleAndDeleted().Wait();
-            }
-            //var start = Slices.BeforeAllKeys;
-
-            //using (var env = new StorageEnvironment(StorageEnvironmentOptions.ForPath("so")))
-            //{
-            //    int i = 0;
-            //    bool hasMore = true; ; 
-            //    while (hasMore)
-            //    {
-            //        i++;
-            //        using (var output = File.Create($"posts-{i:000}.dump"))
-            //        using (var gzip = new GZipStream(output, CompressionMode.Compress))
-            //        using (var writer = new StreamWriter(gzip))
-            //        using (var jsonWriter = new JsonTextWriter(writer))
-            //        {
-            //            Console.WriteLine("\r"+output.Name);
-            //            jsonWriter.WriteStartObject();
-            //            jsonWriter.WritePropertyName("BuildVersion");
-            //            jsonWriter.WriteValue(40000);
-            //            jsonWriter.WritePropertyName("Docs");
-            //            jsonWriter.WriteStartArray();
-            //            hasMore = WriteInOneTransaction(env, ref start, jsonWriter, output);
-            //            jsonWriter.WriteEndArray();
-            //            jsonWriter.WriteEndObject();
-            //        }
-            //    }
-            //}
-        }
-
-        private static bool WriteInOneTransaction(StorageEnvironment env, ref Slice start, JsonTextWriter jsonWriter, Stream output)
-        {
-            using (var tx = env.ReadTransaction())
+                Url = "http://localhost:8080",
+                DefaultDatabase = "stackoverflow"
+            }.Initialize())
             {
-                var tree = tx.ReadTree("q");
-                using (var it = tree.Iterate(prefetch: true))
+                new Users_Registrations_ByMonth().Execute(store);
+                new Users_Search().Execute(store);
+                new Questions_Tags().Execute(store);
+                new Questions_Tags_ByMonths().Execute(store);
+                new Activity_ByMonth().Execute(store);
+
+                var sp = Stopwatch.StartNew();
+                var done = new HashSet<string>();
+                while (true)
                 {
-                    if (it.Seek(start) == false)
-                        return false;
-
-                    XmlDocument xmlDoc = new XmlDocument();
-                    int count = 0;
-                    var parts = it.CurrentKey.ToString().Split(',');
-                    var qId = parts[0];
-                    RavenJObject question = null;
-                    ;
-                    var answers = new List<RavenJObject>();
-                    do
+                    bool hasStale = false;
+                    var databaseStatistics = store.DatabaseCommands.GetStatistics();
+                    foreach (var index in databaseStatistics.Indexes)
                     {
-                        parts = it.CurrentKey.ToString().Split(',');
-
-                        if (qId != parts[0])
+                        if (index.IsStale == false && done.Add(index.Name))
                         {
-                            foreach (var answer in answers)
-                            {
-                                question.Value<RavenJArray>("Answers").Add(answer);
-                            }
-                            question.WriteTo(jsonWriter);
-                            answers.Clear();
-                            question = null;
-                            qId = parts[0];
-                        }
-                        var xmlReader = XmlReader.Create(new StringReader(it.CreateReaderForCurrent().ToStringValue()));
-                        var node = xmlDoc.ReadNode(xmlReader);
-                        if (parts[1] == qId)
-                        {
-                            //found the question
-                            question = new RavenJObject();
-
-                            AddProperties(question, node);
-                            question.Remove("Id");
-
-                            question["Answers"] = new RavenJArray();
-                            question["@metadata"] = new RavenJObject
-                            {
-                                ["Raven-Entity-Name"] = "Questions",
-                                ["@id"] = "questions/" + parts[0]
-                            };
+                            Console.WriteLine(sp.Elapsed + " - " + index.Name);
                         }
                         else
                         {
-                            var answer = new RavenJObject();
-                            AddProperties(answer, node);
-                            answers.Add(answer);
+                            hasStale = true;
                         }
-                        if (count++%5000 == 0)
-                        {
-                            Console.Write($"\r{count:#,#} processed");
-                        }
-                        if (output.Length > 1024*1024*256)
-                        {
-                            if (it.MoveNext() == false)
-                                return false;
-                            Slice.From(_byteStringContext, qId, out start);
-                            return true;
-                        }
-                    } while (it.MoveNext());
+                    }
+                    if (hasStale == false)
+                        break;
+                    Thread.Sleep(250);
                 }
-            }
-            return false;
-        }
 
-        private static void AddProperties(RavenJObject question, XmlNode node)
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine(sp.Elapsed + " all done! :-)");
+            }
+
+        }
+    }
+
+    public class Users_Search : AbstractIndexCreationTask<User>
+    {
+        public Users_Search()
         {
-            foreach (XmlAttribute attribute in node.Attributes)
-            {
+            Map = users => from user in users
+                           select new
+                           {
+                               user.DisplayName,
+                               user.LastAccessDate
+                           };
 
-                switch (attribute.LocalName)
-                {
-                    case "PostTypeId":
-                    case "ParentId":
-                        break;
-                    case "Tags":
-                        question["Tags"] =
-                            new RavenJArray(attribute.Value.Trim('<', '>')
-                                .Split(new[] {"><"}, StringSplitOptions.RemoveEmptyEntries));
-                        break;
-                    default:
-                        long val;
-                        if (long.TryParse(attribute.Value, out val))
-                            question[attribute.LocalName] = val;
-                        else
-                            question[attribute.LocalName] = attribute.Value;
-                        break;
-                }
-            }
+            Index(user => user.DisplayName, FieldIndexing.Analyzed);
         }
+    }
+
+
+    public class Users_Registrations_ByMonth : AbstractIndexCreationTask<User, Users_Registrations_ByMonth.Result>
+    {
+        public class Result
+        {
+            public string Month;
+            public int Count;
+        }
+
+        public Users_Registrations_ByMonth()
+        {
+            Map = users => from user in users
+                           select new
+                           {
+                               Count = 1,
+                               Month = user.CreationDate.ToString("yyyy-MM")
+                           };
+
+            Reduce = results => from result in results
+                                group result by result.Month
+                                into g
+                                select new
+                                {
+                                    Month = g.Key,
+                                    Count = g.Sum(x => x.Count)
+                                };
+        }
+    }
+    public class User
+    {
+        public int Reputation { get; set; }
+        public DateTimeOffset CreationDate { get; set; }
+        public string DisplayName { get; set; }
+        public DateTimeOffset LastAccessDate { get; set; }
+        public int Views { get; set; }
+        public int UpVotes { get; set; }
+        public int DownVotes { get; set; }
+        public Uri ProfileImageUrl { get; set; }
+        public int AccountId { get; set; }
+    }
+
+    public class Questions_Tags : AbstractIndexCreationTask<Question, Questions_Tags.Result>
+    {
+        public class Result
+        {
+            public int Count;
+            public int Answers;
+            public int AcceptedAnswers;
+            public string Tag;
+        }
+
+        public Questions_Tags()
+        {
+            Map = questions =>
+                from q in questions
+                from tag in q.Tags
+                select new
+                {
+                    Tag = tag,
+                    Count = 1,
+                    Answers = q.AnswerCount,
+                    AcceptedAnswers = q.AcceptedAnswerId != null ? 1 : 0
+                };
+
+            Reduce = results =>
+                from result in results
+                group result by result.Tag
+                into g
+                select new
+                {
+                    Tag = g.Key,
+                    Count = g.Sum(x => x.Count),
+                    Answers = g.Sum(x => x.Answers),
+                    AcceptedAnswers = g.Sum(x => x.AcceptedAnswers)
+                };
+        }
+    }
+
+    public class Questions_Tags_ByMonths : AbstractIndexCreationTask<Question, Questions_Tags_ByMonths.Result>
+    {
+        public class Result
+        {
+            public int Count;
+            public string Tag;
+            public string Month;
+        }
+
+        public Questions_Tags_ByMonths()
+        {
+            Map = questions =>
+                from q in questions
+                from tag in q.Tags
+                select new
+                {
+                    Tag = tag,
+                    Count = 1,
+                    Month = q.CreationDate.ToString("yyyy-MM")
+                };
+
+            Reduce = results =>
+                from result in results
+                group result by new { result.Tag, result.Month}
+                into g
+                select new
+                {
+                    g.Key.Month,
+                    g.Key.Tag,
+                    Count = g.Sum(x => x.Count),
+                };
+        }
+    }
+
+
+    public class Activity_ByMonth : AbstractMultiMapIndexCreationTask<Activity_ByMonth.Result>
+    {
+        public class Result
+        {
+            public int Users;
+            public string Month;
+        }
+
+        public Activity_ByMonth()
+        {
+            AddMap<Question>(questions =>
+                from q in questions
+                select new
+                {
+                    Month = q.CreationDate.ToString("yyyy-MM"),
+                    Users = 1
+                });
+
+            AddMap<Question>(questions =>
+               from q in questions
+               from a in q.Answers
+               group a by new // distinct users by month
+               {
+                   a.OwnerUserId,
+                   Month = a.CreationDate.ToString("yyyy-MM")
+               }
+               into g
+               select new
+               {
+                   g.Key.Month,
+                   Users = g.Count()
+               });
+
+            Reduce = results =>
+                from result in results
+                group result by result.Month
+                into g
+                select new
+                {
+                    Month = g.Key,
+                    Users = g.Sum(x => x.Users),
+                };
+        }
+    }
+
+    public class Answers
+    {
+        public int Id { get; set; }
+        public DateTimeOffset CreationDate { get; set; }
+        public int Score { get; set; }
+        public string Body { get; set; }
+        public int OwnerUserId { get; set; }
+        public DateTimeOffset LastActivityDate { get; set; }
+        public int CommentCount { get; set; }
     }
 
     public class Question
     {
-        public long AcceptedAnswerId { get; set; }
-        public DateTime CreationDate { get; set; }
+        public Answers[] Answers { get; set; }
+        public int? AcceptedAnswerId { get; set; }
+        public DateTimeOffset CreationDate { get; set; }
         public int Score { get; set; }
         public int ViewCount { get; set; }
         public string Body { get; set; }
-        public string OwnerUserId { get; set; }
-        public string LastEditorUserId { get; set; }
-        public string LastEditorDisplayName { get; set; }
-        public DateTime LastEditDate { get; set; }
-        public DateTime LastActivityDate { get; set; }
+        public int OwnerUserId { get; set; }
+        public int LastEditorUserId { get; set; }
+        public DateTimeOffset LastEditDate { get; set; }
+        public DateTimeOffset LastActivityDate { get; set; }
         public string Title { get; set; }
         public string[] Tags { get; set; }
-
-        public int AnswrCount { get; set; }
+        public int AnswerCount { get; set; }
         public int CommentCount { get; set; }
         public int FavoriteCount { get; set; }
-
-        public List<Answer> Answers { get; set; } = new List<Answer>();
-    }
-
-    public class Answer
-    {
-        public DateTime CreationDate { get; set; }
-        public int Score { get; set; }
-        public string OwnerUserId { get; set; }
-        public string LastEditorUserId { get; set; }
-        public string LastEditorDisplayName { get; set; }
-        public DateTime LastEditDate { get; set; }
-        public DateTime LastActivityDate { get; set; }
-        public DateTime CommunityOwnedDate { get; set; }
-        public int CommentCount { get; set; }
     }
 }
