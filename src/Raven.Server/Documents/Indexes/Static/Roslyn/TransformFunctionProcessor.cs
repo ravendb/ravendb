dcd4ba2a221fcd9fa29b16f032a3a03ac2adead4
Arkadiusz Palinski <arek@ravendb.net>
2016-09-30 15:30:59 +0200

    RavenDB-4608 Refactoring of transform functions handling

---
+++ src/Raven.Server/Documents/Indexes/Static/Roslyn/TransformFunctionProcessor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Raven.Server.Documents.Indexes.Static.Roslyn.Rewriters;
+
+namespace Raven.Server.Documents.Indexes.Static.Roslyn
+{
+    public class TransformFunctionProcessor : CSharpSyntaxRewriter
+    {
+        private SelectManyRewriter _selectManyRewriter;
+
+        public TransformFunctionProcessor(SelectManyRewriter selectManyRewriter)
+        {
+            _selectManyRewriter = selectManyRewriter;
+        }
+
+        public override SyntaxNode Visit(SyntaxNode node)
+        {
+            foreach (var rewriter in new CSharpSyntaxRewriter[]
+            {
+                _selectManyRewriter,
+                DynamicInvocationExpressionsRewriter.Instance,
+                DynamicExtensionMethodsRewriter.Instance,
+                DynamicLambdaExpressionsRewriter.Instance,
+                RecurseRewriter.Instance,
+            })
+            {
+                node = rewriter.Visit(node);
+            }
+
+            return node;
+        }
+    }
+}
