5bd4f171053663de535ef1473a1c7efd35f8f478
IdanHaim <haimdude@gmail.com>
2016-10-18 12:16:25 +0300

    RavenDB-5090 add new export class DatabaseExportOptions

---
+++ src/Raven.Client/Smuggler/DatabaseExportOptions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Raven.Client.Smuggler
+{
+    public class DatabaseExportOptions
+    {
+        public DatabaseItemType OperateOnTypes { get; set; }
+
+        public int? BatchSize { get; set; }
+
+        public bool ExcludeExpired { get; set; }
+
+        public bool RemoveAnalyzers { get; set; }
+
+        public string TransformScript { get; set; }
+
+        public string FileName { get; set; }
+
+        public List<string> CollectionsToExport { get; set; }
+    }
+}
