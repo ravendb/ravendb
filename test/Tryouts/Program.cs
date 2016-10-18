dbd9ef8fe35902955195e6967b94a9cb3ac77b10
Ayende Rahien <Ayende@ayende.com>
2016-10-18 13:09:37 +0300

    Fixing reuse of the multi parser context after reset of the context using old memory

--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -23,9 +23,9 @@ namespace Tryouts
             for (int i = 0; i < 15; i++)
             {
                 Console.WriteLine(i);
-                using (var a = new SlowTests.Tests.Faceted.FacetedIndex())
+                using (var a = new FastTests.Client.Subscriptions.RavenDB_3193())
                 {
-                    a.CanPerformFacetedSearch_Embedded();
+                    a.ShouldRespectCollectionCriteria().Wait();
                 }
             }
             if (DateTime.Today.Day != -1)
