282445318092b45dacc34a6fea3f3b6eaf492c30
maximburyak <maxim@ayende.com>
2016-10-27 17:41:46 +0300

    RavenDB-5511 Fire subscribers error notification if batche acknowledgement timeout have been reached

---
+++ Raven.Abstractions/Exceptions/Subscriptions/SubscriptionAckTimeoutException.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="SubscriptionAckTimeoutException.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using System;
+using System.Net;
+using System.Runtime.Serialization;
+
+namespace Raven.Abstractions.Exceptions.Subscriptions
+{
+    public class SubscriptionAckTimeoutException:SubscriptionException
+    {
+        public static HttpStatusCode RelevantHttpStatusCode = HttpStatusCode.RequestTimeout;
+
+        public SubscriptionAckTimeoutException()
+            : base(RelevantHttpStatusCode)
+        {
+        }
+
+        public SubscriptionAckTimeoutException(string message)
+            : base(message, RelevantHttpStatusCode)
+        {
+        }
+
+        public SubscriptionAckTimeoutException(string message, Exception inner)
+            : base(message, inner, RelevantHttpStatusCode)
+        {
+        }
+
+#if !DNXCORE50
+        protected SubscriptionAckTimeoutException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context)
+        {
+        }
+#endif
+    }
+}
