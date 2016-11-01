d12f907a079aa7640d4890ca10edeb271ef2dfdc
Ayende Rahien <ayende@ayende.com>
2016-11-01 14:58:35 +0200

    Adding better error if the indexing is stale

--- test/SlowTests/Tests/Track/AsyncProjectionShouldWork.cs
+++ test/SlowTests/Tests/Track/AsyncProjectionShouldWork.cs
@@ -64,7 +64,7 @@ namespace SlowTests.Tests.Track
             using (var store = GetDocumentStore())
             {
                 Fill(store);
-
+                WaitForIndexing(store);
                 using (var session = store.OpenAsyncSession())
                 {
                     var q = session.Query<Summary>("TestObjs/Summary")
