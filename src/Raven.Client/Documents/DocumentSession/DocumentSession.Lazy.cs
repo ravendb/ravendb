0aacead89d78374032f46f54df61a2d0c3d63d6f
iftahbe <iftahbe@gmail.com>
2016-11-03 15:59:19 +0200

    RavenDB-5244 V4.0 client long running task - Split DocumentSession to partial classes - Complete unimplemented overloads of load and query - Add summary comments

---
+++ src/Raven.Client/Documents/DocumentSession/DocumentSession.Lazy.cs
@@ -0,0 +1,145 @@
+//-----------------------------------------------------------------------
+// <copyright file="DocumentSession.cs" company="Hibernating Rhinos LTD">
+//     Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Raven.Client.Data.Queries;
+using Raven.Client.Documents.SessionOperations;
+using Raven.Client.Indexes;
+using Raven.Json.Linq;
+
+namespace Raven.Client.Documents
+{
+    /// <summary>
+    /// Implements Unit of Work for accessing the RavenDB server
+    /// </summary>
+    public partial class DocumentSession : InMemoryDocumentSessionOperations, IDocumentQueryGenerator, ISyncAdvancedSessionOperation, IDocumentSessionImpl
+    {
+        ILazyLoaderWithInclude<TResult> ILazySessionOperations.Include<TResult>(Expression<Func<TResult, object>> path)
+        {
+            throw new NotImplementedException();
+        }
+
+        Lazy<TResult[]> ILazySessionOperations.Load<TResult>(IEnumerable<string> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lazy<TResult[]> Load<TResult>(IEnumerable<string> ids, Action<TResult[]> onEval)
+        {
+            throw new NotImplementedException();
+        }
+
+        Lazy<TResult> ILazySessionOperations.Load<TResult>(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lazy<TResult> Load<TResult>(string id, Action<TResult> onEval)
+        {
+            throw new NotImplementedException();
+        }
+
+        Lazy<TResult> ILazySessionOperations.Load<TResult>(ValueType id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lazy<TResult> Load<TResult>(ValueType id, Action<TResult> onEval)
+        {
+            throw new NotImplementedException();
+        }
+
+        Lazy<TResult[]> ILazySessionOperations.Load<TResult>(params ValueType[] ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        Lazy<TResult[]> ILazySessionOperations.Load<TResult>(IEnumerable<ValueType> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lazy<TResult[]> Load<TResult>(IEnumerable<ValueType> ids, Action<TResult[]> onEval)
+        {
+            throw new NotImplementedException();
+        }
+
+        Lazy<TResult> ILazySessionOperations.Load<TTransformer, TResult>(string id, Action<ILoadConfiguration> configure, Action<TResult> onEval)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lazy<TResult> Load<TResult>(string id, Type transformerType, Action<ILoadConfiguration> configure = null, Action<TResult> onEval = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lazy<TResult[]> Load<TTransformer, TResult>(IEnumerable<string> ids, Action<ILoadConfiguration> configure = null, Action<TResult> onEval = null) where TTransformer : AbstractTransformerCreationTask, new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lazy<TResult[]> Load<TResult>(IEnumerable<string> ids, Type transformerType, Action<ILoadConfiguration> configure = null, Action<TResult> onEval = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        Lazy<TResult[]> ILazySessionOperations.LoadStartingWith<TResult>(string keyPrefix, string matches, int start, int pageSize,
+            string exclude, RavenPagingInformation pagingInformation, string skipAfter)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Lazy<TResult[]> MoreLikeThis<TResult>(MoreLikeThisQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        ILazyLoaderWithInclude<object> ILazySessionOperations.Include(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] LoadInternal<T>(string[] ids, KeyValuePair<string, Type>[] includes, string transformer, Dictionary<string, object> transformerParameters = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lazy<T[]> LazyLoadInternal<T>(string[] ids, KeyValuePair<string, Type>[] includes, Action<T[]> onEval)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] LoadInternal<T>(string[] ids, string transformer, Dictionary<string, object> transformerParameters = null)
+        {
+            if (transformer == null)
+                throw new ArgumentNullException("transformer");
+            if (ids.Length == 0)
+                return new T[0];
+
+            var loadTransformerOeration = new LoadTransformerOperation(this);
+            loadTransformerOeration.ByIds(ids);
+            loadTransformerOeration.WithTransformer(transformer, transformerParameters);
+
+            var command = loadTransformerOeration.CreateRequest();
+            if (command != null)
+            {
+                RequestExecuter.Execute(command, Context);
+                loadTransformerOeration.SetResult(command.Result);
+            }
+
+            return loadTransformerOeration.GetTransformedDocuments<T>(command.Result);
+        }
+
+        public T[] LoadInternal<T>(string[] ids, KeyValuePair<string, Type>[] includes, string transformer, Dictionary<string, RavenJToken> transformerParameters = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Raven.Client/Documents/DocumentSession/DocumentSession.Query.cs
@@ -0,0 +1,144 @@
+//-----------------------------------------------------------------------
+// <copyright file="DocumentSession.cs" company="Hibernating Rhinos LTD">
+//     Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using Raven.Client.Indexes;
+
+namespace Raven.Client.Documents
+{
+    /// <summary>
+    /// Implements Unit of Work for accessing the RavenDB server
+    /// </summary>
+    public partial class DocumentSession : InMemoryDocumentSessionOperations, IDocumentQueryGenerator, ISyncAdvancedSessionOperation, IDocumentSessionImpl
+    {
+        /// <summary>
+        /// Queries the index specified by <typeparamref name="TIndexCreator"/> using lucene syntax.
+        /// </summary>
+        /// <typeparam name="T">The result of the query</typeparam>
+        /// <typeparam name="TIndexCreator">The type of the index creator.</typeparam>
+        /// <returns></returns>
+        public IDocumentQuery<T> DocumentQuery<T, TIndexCreator>() where TIndexCreator : AbstractIndexCreationTask, new()
+        {
+            var index = new TIndexCreator();
+            return DocumentQuery<T>(index.IndexName, index.IsMapReduce);
+        }
+
+        /// <summary>
+        /// Queries the index specified by <typeparamref name="TIndexCreator"/> using lucene syntax.
+        /// </summary>
+        /// <typeparam name="T">The result of the query</typeparam>
+        /// <typeparam name="TIndexCreator">The type of the index creator.</typeparam>
+        /// <returns></returns>
+        [Obsolete("Use DocumentQuery instead.")]
+        public IDocumentQuery<T> LuceneQuery<T, TIndexCreator>() where TIndexCreator : AbstractIndexCreationTask, new()
+        {
+            return DocumentQuery<T, TIndexCreator>();
+        }
+
+        /// <summary>
+        /// Query the specified index using Lucene syntax
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="indexName">Name of the index.</param>
+        /// <param name="isMapReduce">Indicates if index is a map-reduce index.</param>
+        /// <returns></returns>
+        [Obsolete("Use DocumentQuery instead.")]
+        public IDocumentQuery<T> LuceneQuery<T>(string indexName, bool isMapReduce = false)
+        {
+            return DocumentQuery<T>(indexName, isMapReduce);
+        }
+
+        /// <summary>
+        /// Dynamically query RavenDB using Lucene syntax
+        /// </summary>
+        [Obsolete("Use DocumentQuery instead.")]
+        public IDocumentQuery<T> LuceneQuery<T>()
+        {
+            return DocumentQuery<T>();
+        }
+
+        /// <summary>
+        /// Query the specified index using Lucene syntax
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="indexName">Name of the index.</param>
+        /// <param name="isMapReduce">Indicates if index is a map-reduce index.</param>
+        /// <returns></returns>
+        public IDocumentQuery<T> DocumentQuery<T>(string indexName, bool isMapReduce = false)
+        {
+            return new DocumentQuery<T>(this, DatabaseCommands, null, indexName, null, null, isMapReduce);
+        }
+
+        /// <summary>
+        /// Dynamically query RavenDB using Lucene syntax
+        /// </summary>
+        public IDocumentQuery<T> DocumentQuery<T>()
+        {
+            var indexName = CreateDynamicIndexName<T>();
+            return Advanced.DocumentQuery<T>(indexName);
+        }
+
+        /// <summary>
+        /// Query RavenDB dynamically using LINQ
+        /// </summary>
+        /// <typeparam name="T">The result of the query</typeparam>
+        public IRavenQueryable<T> Query<T>()
+        {
+            var indexName = CreateDynamicIndexName<T>();
+
+            return Query<T>(indexName);
+        }
+
+        /// <summary>
+        /// Queries the specified index using Linq.
+        /// </summary>
+        /// <typeparam name="T">The result of the query</typeparam>
+        /// <param name="indexName">Name of the index.</param>
+        /// <param name="isMapReduce">Whatever we are querying a map/reduce index (modify how we treat identifier properties)</param>
+        public IRavenQueryable<T> Query<T>(string indexName, bool isMapReduce = false)
+        {
+            var ravenQueryStatistics = new RavenQueryStatistics();
+            var highlightings = new RavenQueryHighlightings();
+            var ravenQueryProvider = new RavenQueryProvider<T>(this, indexName, ravenQueryStatistics, highlightings, DatabaseCommands, null, isMapReduce);
+            var inspector = new RavenQueryInspector<T>();
+            inspector.Init(ravenQueryProvider, ravenQueryStatistics, highlightings, indexName, null, this, DatabaseCommands, null, isMapReduce);
+            return inspector;
+        }
+
+        /// <summary>
+        /// Create a new query for <typeparam name="T"/>
+        /// </summary>
+        public IAsyncDocumentQuery<T> AsyncQuery<T>(string indexName, bool isMapReduce)
+        {
+            throw new NotSupportedException();
+        }
+
+        public RavenQueryInspector<S> CreateRavenQueryInspector<S>()
+        {
+            return new RavenQueryInspector<S>();
+        }
+
+        /// <summary>
+        /// Queries the index specified by <typeparamref name="TIndexCreator"/> using Linq.
+        /// </summary>
+        /// <typeparam name="T">The result of the query</typeparam>
+        /// <typeparam name="TIndexCreator">The type of the index creator.</typeparam>
+        /// <returns></returns>
+        public IRavenQueryable<T> Query<T, TIndexCreator>() where TIndexCreator : AbstractIndexCreationTask, new()
+        {
+            var indexCreator = new TIndexCreator();
+            return Query<T>(indexCreator.IndexName, indexCreator.IsMapReduce);
+        }
+
+        /// <summary>
+        /// Create a new query for <typeparam name="T"/>
+        /// </summary>
+        IDocumentQuery<T> IDocumentQueryGenerator.Query<T>(string indexName, bool isMapReduce)
+        {
+            return Advanced.DocumentQuery<T>(indexName, isMapReduce);
+        }
+    }
+}
