962db0ce5ec6fd6afa7ff0f1f079a9e9018e5031
Grisha Kotler <grisha@ayende.com>
2016-11-08 15:30:17 +0200

    RavenDB-5526 - Add the rest of the endpoints for periodic export to S3

--- Raven.Studio.Html5/App/models/database/documents/periodicExportSetup.ts
+++ Raven.Studio.Html5/App/models/database/documents/periodicExportSetup.ts
@@ -51,12 +51,17 @@ class periodicExportSetup {
     ];
     availableAwsRegionEndpoints = [
         { label: "US East (Virginia)", value: "us-east-1" },
+        { label: "US East (Ohio)", value: "us-east-2" },
         { label: "US West (N. California)", value: "us-west-1" },
         { label: "US West (Oregon)", value: "us-west-2" },
-        { label: "EU West (Ireland)", value: "eu-west-1" },
+        { label: "EU (Ireland)", value: "eu-west-1" },
+        { label: "EU (Frankfurt)", value: "eu-central-1" },
         { label: "Asia Pacific (Tokyo)", value: "ap-northeast-1" },
+        { label: "Asia Pacific (Seoul)", value: "ap-northeast-2" },
         { label: "Asia Pacific (Singapore)", value: "ap-southeast-1" },
-        { label: "South America (Sao Paulo)", value: "sa-east-1" }
+        { label: "Asia Pacific (Sydney)", value: "ap-southeast-2" },
+        { label: "Asia Pacific (Mumbai)", value: "ap-south-1" },
+        { label: "South America (São Paulo)", value: "sa-east-1" }
     ];
     availableIntervalUnits = [this.TU_MINUTES, this.TU_HOURS, this.TU_DAYS];
 
