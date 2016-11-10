8ab4ef60374378f4fe597e752d536d4c5cdecd60
talweiss1982 <tal@ayende.com>
2016-11-10 12:21:11 +0200

    RavenDB-5525 Porting a test that verifies that analyzers on prefix queries should not cut of the last char in the term

---
+++ test/FastTests/Client/Queries/AnalyzersThatRemovesStarShouldNotCutTheLastChar.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Raven.Client.Indexes;
+using Xunit;
+
+namespace FastTests.Client.Queries
+{
+    public class AnalyzersThatRemovesStarShouldNotCutTheLastChar : RavenTestBase
+    {
+        [Fact]
+        void CanDoPrefixQueryOnAnalyzedFields()
+        {
+            using (var store = GetDocumentStore())
+            using (var session = store.OpenNewSession())
+            {
+                new FooBarIndex().Execute(store);
+                session.Store(new Foo { Bar = "Shalom" });
+                session.Store(new Foo { Bar = "Salam" });
+                session.SaveChanges();
+                WaitForIndexing(store);
+                var res = session.Query<Foo, FooBarIndex>().Single(x => x.Bar.StartsWith("Sh"));
+                Assert.Equal(res.Bar, "Shalom");
+            }
+        }
+
+        public class Foo
+        {
+            public string Bar { get; set; }
+        }
+
+        public class FooBarIndex : AbstractIndexCreationTask<Foo>
+        {
+            public FooBarIndex()
+            {
+                Map = foos => from foo in foos select new { foo.Bar };
+                Analyzers.Add(c => c.Bar, typeof(Lucene.Net.Analysis.Standard.StandardAnalyzer).ToString());
+            }
+        }
+    }
+}
