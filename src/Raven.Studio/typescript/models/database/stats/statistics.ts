21694573ef5d22599c4c2b41e20c9fe976e9ca50
Danielle Greenberg <danielle.9897@gmail.com>
2016-11-17 11:23:10 +0200

    RavenDB-5104 Fixing statistics.html data

--- src/Raven.Studio/typescript/models/database/stats/statistics.ts
+++ src/Raven.Studio/typescript/models/database/stats/statistics.ts
@@ -3,20 +3,52 @@
 import indexStatistics = require("models/database/stats/indexStatistics");
 
 class statistics {
+
+    storageEngine: string;
+    dataBaseId: string;
+    lastDocEtag?: number;
+    lastAttachmentEtag: number;
+    staleIndexes: string[];
+    countOfIndexes: number;
     countOfDocuments: number;
+    countOfTransformers: number;
+    //lastIndexingDateTime: number; // ??
+    is64Bit: boolean;
 
     indexes = ko.observableArray<indexStatistics>();
 
     constructor(dto: Raven.Client.Data.DatabaseStatistics) {
+        this.storageEngine = "Voron";
+        this.dataBaseId = dto.DatabaseId; 
+        this.lastDocEtag = dto.LastDocEtag;
+        this.countOfIndexes = dto.CountOfIndexes;
         this.countOfDocuments = dto.CountOfDocuments;
-        //TODO: work on other props
+        this.staleIndexes = dto.StaleIndexes;
+        //this.lastIndexingDateTime = dto.?? need to do in different issue
+        this.countOfTransformers = dto.CountOfTransformers;
+        this.is64Bit = dto.Is64Bit;
 
+        // TODO: work on other props
 
         this.indexes(dto.Indexes.map(x => new indexStatistics(x)));
     }
 
-
-
 }
 
 export = statistics;
+
+//interface DatabaseStatistics {
+//    ApproximateTaskCount: number;
+//    CountOfDocuments: number;
+//    CountOfIndexes: number;
+//    CountOfRevisionDocuments?: number;
+//    CountOfTransformers: number;
+//    CurrentNumberOfItemsToIndexInSingleBatch: number;
+//    CurrentNumberOfItemsToReduceInSingleBatch: number;
+//    CurrentNumberOfParallelTasks: number;
+//    DatabaseId: string;
+//    Indexes: Raven.Client.Data.IndexInformation[];
+//    Is64Bit: boolean;
+//    LastDocEtag?: number;
+//    StaleIndexes: string[];
+//}
