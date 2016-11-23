2a9ff18151c59c3d9409cc29e619fb530804f4b8
Ayende Rahien <ayende@ayende.com>
2016-11-23 10:38:53 +0200

    RavenDB-5639 FastTests.Voron.Backups.BackupToOneZipFile.IncrementalBackupToOneZipFile test fails Fixing recursive write transactions

--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -12,9 +12,9 @@ namespace Tryouts
             for (int i = 0; i < 1000; i++)
             {
                 Console.WriteLine(i);
-                using (var store = new FastTests.Server.Documents.Indexing.Static.BasicStaticMapReduceIndexing())
+                using (var store = new FastTests.Voron.Backups.BackupToOneZipFile())
                 {
-                    store.Static_map_reduce_index_with_multiple_outputs_per_document().Wait();
+                    store.FullBackupToOneZipFile();
                 }
             }
         }
