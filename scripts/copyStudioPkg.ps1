6b208e8a3e8b5d9bcc2640e5379a588805daa31c
Grzegorz Lachowski <gregolsky@ravendb.net>
2016-11-25 10:23:58 +0100

    RavenDB-5646 support building for other runtimes

--- scripts/copyStudioPkg.ps1
+++ scripts/copyStudioPkg.ps1
@@ -1,6 +1,6 @@
 function CopyStudioPackage ( $studioBuildDir, $outDir ) {
     $studioZipPath = "$studioBuildDir\Raven.Studio.zip"
-    write-host "Copying Studio package from $studioZipPath to $OUT_DIR\Server"
-    cp $studioZipPath "$OUT_DIR\Server"
+    write-host "Copying Studio package from $studioZipPath to $outDir\Server"
+    cp $studioZipPath "$outDir\Server"
     CheckLastExitCode
 }
