21a2abdc0ecfc2cc0a2cdac8733a4fbb3f4adc7e
Grzegorz Lachowski <gregolsky@ravendb.net>
2016-11-25 12:06:29 +0100

    RavenDB-5646 fix cross-platform zip method for build script

--- scripts/zipFiles.ps1
+++ scripts/zipFiles.ps1
@@ -1,7 +1,5 @@
 function ZipFilesFromDir( $targetZipFilename, $sourcedir )
 {
-    write-host "Creating ZIP file $targetZipFilename"
-    Add-Type -Assembly System.IO.Compression.FileSystem
-    $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
-    [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir, $targetZipFilename, $compressionLevel, $false)
+    $toZipGlob = [io.path]::combine($sourceDir, '*')
+    Compress-Archive -Path $toZipGlob -DestinationPath $targetZipFilename
 }
