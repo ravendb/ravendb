b6c05ee5ebbbd67a5f7befd8750a1b6abdd45d27
maximburyak <maxim@ayende.com>
2016-11-30 18:08:06 +0200

    RavenDB-5650: allow Raven to perform sql replication to postgres

--- src/Raven.Server/Documents/SqlReplication/DbCommandBuilder.cs
+++ src/Raven.Server/Documents/SqlReplication/DbCommandBuilder.cs
@@ -1,10 +1,15 @@
+using System;
+
 namespace Raven.Server.Documents.SqlReplication
 {
     public class DbCommandBuilder
     {
+        public string Start, End;
+
         public string QuoteIdentifier(string unquotedIdentifier)
         {
-            return "[" + unquotedIdentifier + "]";
+            return Start + unquotedIdentifier + End;
         }
+      
     }
 }
