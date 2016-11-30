e1f48084820745b83f38fad5e1208dec828eea01
maximburyak <maxim@ayende.com>
2016-11-30 18:08:06 +0200

    RavenDB-5650: allow Raven to perform sql replication to postgres

--- src/Raven.Server/Documents/SqlReplication/DbProviderFactories.cs
+++ src/Raven.Server/Documents/SqlReplication/DbProviderFactories.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Data.Common;
 using System.Data.SqlClient;
-
+using Npgsql;
 namespace Raven.Server.Documents.SqlReplication
 {
     public class DbProviderFactories
@@ -12,24 +12,40 @@ namespace Raven.Server.Documents.SqlReplication
             {
                 case "System.Data.SqlClient":
                     return SqlClientFactory.Instance;
+                case "Npgsql":
+                    return Npgsql.NpgsqlFactory.Instance;
                 case "System.Data.SqlServerCe.4.0":
                 case "System.Data.OleDb":
                 case "System.Data.OracleClient":
                 case "MySql.Data.MySqlClient":
                 case "System.Data.SqlServerCe.3.5":
-                case "Npgsql":
+
                     throw new NotImplementedException($"Factory '{factoryName}' is not implemented yet");
                 default:
                     throw new NotSupportedException($"Factory '{factoryName}' is not supported");
             }
-        }
+        }        
     }
 
     public static class DbProviderFactoryExtensions
     {
         public static DbCommandBuilder CreateCommandBuilder(this DbProviderFactory factory)
         {
-            return new DbCommandBuilder();
+            if (factory is SqlClientFactory)
+                return new DbCommandBuilder
+                {
+                    Start = "[",
+                    End = "]"
+                };
+            if (factory is NpgsqlFactory)
+                return new DbCommandBuilder
+                {
+                    Start = "\"",
+                    End = "\""
+                };
+            return new DbCommandBuilder
+            {
+            };
         }
     }
 }
