22b367765794b50b65eb988e5950157fdb83695f
Ayende Rahien <ayende@ayende.com>
2016-12-01 14:55:13 +0200

    More NLog removal

--- bench/Voron.Benchmark/BTree/BTreeReadAndIterate.cs
+++ bench/Voron.Benchmark/BTree/BTreeReadAndIterate.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+﻿using System.Collections.Generic;
 using System.Threading;
 using BenchmarkDotNet.Attributes;
-using NLog.LayoutRenderers;
 using Sparrow;
 
 namespace Voron.Benchmark.BTree
