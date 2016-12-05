6f8b8ec27c94fa29fb4b123eff4174a4174570ae
Efrat Shenhar <efrat@hibernatingrhinos.com>
2016-12-05 15:42:25 +0200

    RavenDB-5677 DeleteByCollection

---
+++ tests/FastTests/PatchAndDeleteByCollection.cs
@@ -0,0 +1,100 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using NewClientTests;
+using Raven.NewClient.Client.Commands;
+using Raven.Tests.Core.Utils.Entities;
+using Sparrow.Json;
+using Xunit;
+
+namespace NewClientTests.NewClient.FastTests.Patching
+{
+    public class PatchAndDeleteByCollection : RavenTestBase
+    {
+        [Fact]
+        public void CanDeleteCollection()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var x = store.OpenSession())
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        x.Store(new User { }, "users/");
+                    }
+                    x.SaveChanges();
+                }
+
+                JsonOperationContext context;
+                store.GetRequestExecuter(store.DefaultDatabase).ContextPool.AllocateOperationContext(out context);
+                var Command = new DeleteByCollectionCommand()
+                {
+                    CollectionName = "users"
+                };
+                store.GetRequestExecuter(store.DefaultDatabase).Execute(Command, context);
+
+                var sp = Stopwatch.StartNew();
+
+                var timeout = Debugger.IsAttached ? 60 * 10000 : 10000;
+
+                while (sp.ElapsedMilliseconds < timeout)
+                {
+                    var getStatsCommand = new GetStatisticsCommand();
+                    store.GetRequestExecuter(store.DefaultDatabase).Execute(getStatsCommand, context);
+                    var databaseStatistics = getStatsCommand.Result;
+                    if (databaseStatistics.CountOfDocuments == 0)
+                        return;
+
+                    Thread.Sleep(25);
+                }
+                Assert.False(true, "There are still documents after 1 second");
+            }
+        }
+
+        /*[Fact]
+        public void CanPatchCollection()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var x = store.OpenSession())
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        x.Store(new User { }, "users/");
+                    }
+                    x.SaveChanges();
+                }
+                var httpJsonRequest = store.DatabaseCommands.CreateRequest("/collections/docs?name=users", new HttpMethod("PATCH"));
+                httpJsonRequest.WriteAsync(@"
+{
+    'Script': 'this.Name = __document_id'
+}
+").Wait();
+                httpJsonRequest.ExecuteRequest();
+                var sp = Stopwatch.StartNew();
+
+                var timeout = Debugger.IsAttached ? 60 * 10000 : 10000;
+
+                while (sp.ElapsedMilliseconds < timeout)
+                {
+                    var databaseStatistics = store.DatabaseCommands.GetStatistics();
+                    if (databaseStatistics.LastDocEtag >= 200)
+                        break;
+                    Thread.Sleep(25);
+                }
+                Assert.Equal(100, store.DatabaseCommands.GetStatistics().CountOfDocuments);
+                using (var x = store.OpenSession())
+                {
+                    var users = x.Load<User>(Enumerable.Range(1, 100).Select(i => "users/" + i));
+                    Assert.Equal(100, users.Length);
+
+                    foreach (var user in users)
+                    {
+                        Assert.NotNull(user.Name);
+                    }
+                }
+            }
+        }*/
+    }
+}
