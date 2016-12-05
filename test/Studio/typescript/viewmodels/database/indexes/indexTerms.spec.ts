5fa7a49423ba7f9a7aa8867ed3c027b6e9fa11c3
Marcin Lewandowski <marcin@ravendb.net>
2016-12-05 07:55:50 +0100

    studio tests fixed

--- test/Studio/typescript/viewmodels/database/indexes/indexTerms.spec.ts
+++ test/Studio/typescript/viewmodels/database/indexes/indexTerms.spec.ts
@@ -7,7 +7,7 @@ describe(viewUnderTest, () => {
 
     it('should bind', () => {
 
-        utils.mockCommand('commands/database/index/getIndexDefinitionCommand', () => getIndexDefinitionCommand());
+        utils.mockCommand('commands/database/index/getIndexEntriesFieldsCommand', () => ["Name"]);
         utils.mockCommand("commands/database/index/getIndexTermsCommand", () => getIndexTermsCommand());
 
         return utils.mockActiveDatabase(dbCtr => new dbCtr("default"))
@@ -18,32 +18,4 @@ describe(viewUnderTest, () => {
 
 function getIndexTermsCommand(): string[] {
     return ["100", "101", "102"];
-}
-
-function getIndexDefinitionCommand(): Raven.Client.Indexing.IndexDefinition {
-    return {
-        "Name": "Orders/Totals",
-        "IndexId": 2,
-        "Type": "Map",
-        "LockMode": "LockedIgnore",
-        "MaxIndexOutputsPerDocument": null,
-        "IndexVersion": -1,
-        "IsSideBySideIndex": false,
-        "IsTestIndex": false,
-        "Reduce": null,
-        "Maps": [
-            "from order in docs.Orders\r\nselect new { order.Employee,  order.Company, Total = order.Lines.Sum(l=>(l.Quantity * l.PricePerUnit) *  ( 1 - l.Discount)) }"
-        ],
-        "Fields": {
-            "Total": {
-                "Analyzer": null,
-                "Indexing": null,
-                "Sort": "NumericDouble",
-                "Storage": null,
-                "Suggestions": null,
-                "TermVector": null,
-                "Spatial": null
-            }
-        }
-    };
 }
