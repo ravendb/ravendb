5680c67e20f16a69c57917527f536b19b0ecb48e
Efrat Shenhar <efrat@hibernatingrhinos.com>
2016-12-05 16:11:54 +0200

    RavenDB-5677 PatchByCollection

---
+++ src/Raven.NewClient/Commands/PatchByCollectionCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using Raven.NewClient.Abstractions.Util;
+using Raven.NewClient.Client.Blittable;
+using Raven.NewClient.Client.Data;
+using Raven.NewClient.Client.Data.Queries;
+using Raven.NewClient.Client.Http;
+using Raven.NewClient.Client.Json;
+using Sparrow.Json;
+
+namespace Raven.NewClient.Client.Commands
+{
+    public class PatchByCollectionCommand : RavenCommand<PatchResult>
+    {
+        public BlittableJsonReaderObject Script;
+        public JsonOperationContext Context;
+        public string CollectionName;
+
+        public override HttpRequestMessage CreateRequest(ServerNode node, out string url)
+        {
+            url = $"{node.Url}/databases/{node.Database}/collections/docs?name={CollectionName}";
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethods.Patch,
+                Content = new BlittableJsonContent(stream =>
+                {
+                    Context.Write(stream, Script);
+                })
+            };
+
+            return request;
+        }
+
+        public override void SetResponse(BlittableJsonReaderObject response)
+        {
+            if (response == null)
+                throw new InvalidOperationException("Got null response from the server after doing a patch by index, something is very wrong. ");
+            Result = JsonDeserializationClient.PatchResult(response);
+        }
+    }
+}
