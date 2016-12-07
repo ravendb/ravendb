d3feaf5c3c49085be993d96d3da36dd3600c86c2
talweiss1982 <tal@ayende.com>
2016-12-07 19:34:58 +0200

    RavenDB-5785 PutDocument should remove conflicts and merge CV

--- src/Raven.Server/Documents/Replication/IncomingReplicationHandler.cs
+++ src/Raven.Server/Documents/Replication/IncomingReplicationHandler.cs
@@ -708,6 +708,7 @@ namespace Raven.Server.Documents.Replication
             var merged = ReplicationUtils.MergeVectors(conflictingVector, _tempReplicatedChangeVector);
             _documentsContext.DocumentDatabase.DocumentsStorage.DeleteConflictsFor(_documentsContext, doc.Id);
             _database.DocumentsStorage.Put(_documentsContext, doc.Id, null, json, merged);
+            Interlocked.Decrement(ref _database.DocumentsStorage._hasConflicts);
         }
 
         private void ResolveConflictToLocal(ReplicationDocumentsPositions doc, ChangeVectorEntry[] conflictingVector)
@@ -744,6 +745,7 @@ namespace Raven.Server.Documents.Replication
                         doc.Collection);
                 }
             }
+            Interlocked.Decrement(ref _database.DocumentsStorage._hasConflicts);
         }
 
         private void ReadChangeVector(ReplicationIndexOrTransformerPositions index, Dictionary<Guid, long> maxReceivedChangeVectorByDatabase)
