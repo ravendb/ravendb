61586dbe4e38b9a57a4d52ce4b5d040e5b9db2ff
Michael Yarichuk <michael.yarichuk@gmail.com>
2016-12-13 13:18:57 +0200

    Adding relevant test (RavenDB-5763)

---
+++ test/SlowTests/Issues/RavenDB-5763.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FastTests;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_5763 : RavenTestBase
+    {
+        [Fact]
+        public void Should_not_throw_timeout_and_out_of_memory()
+        {
+            Parallel.For(0, 1000, i =>
+                {
+                    using (var store = new FastTests.Server.Documents.Replication.ReplicationTombstoneTests())
+                    {
+                        store.Two_tombstones_should_replicate_in_master_master().Wait();
+                    }
+                }
+            );
+        }
+    }
+}
