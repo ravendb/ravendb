3d7db78155223352031a8bd7c3fb93219a64de5b
Efrat Shenhar <efrat@hibernatingrhinos.com>
2016-12-13 17:22:59 +0200

    RavenDB-5767 BulkInsert for the new client

---
+++ src/Raven.NewClient/Commands/GetTcpInfoCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using Raven.NewClient.Client.Http;
+using Raven.NewClient.Client.Json;
+using Sparrow.Json;
+
+namespace Raven.NewClient.Client.Commands
+{
+    public class GetTcpInfoCommand : RavenCommand<GetTcpInfoResult>
+    {
+        public override HttpRequestMessage CreateRequest(ServerNode node, out string url)
+        {
+            url = $"{node.Url}/info/tcp";
+            IsReadRequest = false;
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+            };
+            return request;
+        }
+
+        public override void SetResponse(BlittableJsonReaderObject response)
+        {
+            if (response == null)
+            {
+                Result = null;
+                return;
+            }
+            Result = JsonDeserializationClient.GetTcpInfoResult(response);
+        }
+    }
+}
---
+++ tests/FastTests/BulkInserts.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lucene.Net.Analysis;
+using NewClientTests;
+using Raven.NewClient.Client.Indexes;
+using Xunit;
+using Raven.NewClient.Client.Commands;
+using Sparrow.Json;
+
+namespace NewClientTests.NewClient.FastTests.BulkInsert
+{
+    public class BulkInserts : RavenTestBase
+    {
+        [Fact]
+        public async Task Simple_Bulk_Insert()
+        {
+            using (var store = GetDocumentStore())
+            {
+                
+                using (var bulkInsert = store.BulkInsert())
+                {
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        await bulkInsert.StoreAsync(new FooBar() {Name = "foobar/" + i}, "FooBars/" + i);
+                    }
+                }
+
+                
+                using (var session = store.OpenSession())
+                {
+                    var len = session.Advanced.LoadStartingWith<FooBar>("FooBars/", null, 0, 1000);
+                    Assert.Equal(1000, len.Length);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Simple_Bulk_Insert_Should_Work()
+        {
+            var fooBars = new[]
+            {
+                new FooBar { Name = "John Doe" },
+                new FooBar { Name = "Jane Doe" },
+                new FooBar { Name = "Mega John" },
+                new FooBar { Name = "Mega Jane" }
+            };
+            using (var store = GetDocumentStore())
+            {
+                using (var bulkInsert = store.BulkInsert())
+                {
+                    await bulkInsert.StoreAsync(fooBars[0]);
+                    await bulkInsert.StoreAsync(fooBars[1]);
+                    await bulkInsert.StoreAsync(fooBars[2]);
+                    await bulkInsert.StoreAsync(fooBars[3]);
+                }
+                JsonOperationContext context;
+                store.GetRequestExecuter(store.DefaultDatabase).ContextPool.AllocateOperationContext(out context);
+
+                var getDocumentCommand = new GetDocumentCommand
+                {
+                    Ids = new[] { "FooBars/1", "FooBars/2", "FooBars/3", "FooBars/4"}
+                };
+
+                store.GetRequestExecuter(store.DefaultDatabase).Execute(getDocumentCommand, context);
+
+                var results = getDocumentCommand.Result.Results;
+
+                Assert.Equal(4, results.Length);
+
+                var doc1 = results[0];
+                var doc2 = results[1];
+                var doc3 = results[2];
+                var doc4 = results[3];
+                Assert.NotNull(doc1);
+                Assert.NotNull(doc2);
+                Assert.NotNull(doc3);
+                Assert.NotNull(doc4);
+
+                object name;
+                ((BlittableJsonReaderObject) doc1).TryGetMember("Name", out name);
+                Assert.Equal("John Doe", name.ToString());
+                ((BlittableJsonReaderObject)doc2).TryGetMember("Name", out name);
+                Assert.Equal("Jane Doe", name.ToString());
+                ((BlittableJsonReaderObject)doc3).TryGetMember("Name", out name);
+                Assert.Equal("Mega John", name.ToString());
+                ((BlittableJsonReaderObject)doc4).TryGetMember("Name", out name);
+                Assert.Equal("Mega Jane", name.ToString());
+            }
+        }
+
+        public class FooBarIndex : AbstractIndexCreationTask<FooBar>
+        {
+            public FooBarIndex()
+            {
+                Map = foos => foos.Select(x => new { x.Name });
+            }
+        }
+
+        public class FooBar : IEquatable<FooBar>
+        {
+            public string Name { get; set; }
+
+            public bool Equals(FooBar other)
+            {
+                if (ReferenceEquals(null, other)) return false;
+                if (ReferenceEquals(this, other)) return true;
+                return string.Equals(Name, other.Name);
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((FooBar) obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return Name?.GetHashCode() ?? 0;
+            }
+
+            public static bool operator ==(FooBar left, FooBar right)
+            {
+                return Equals(left, right);
+            }
+
+            public static bool operator !=(FooBar left, FooBar right)
+            {
+                return !Equals(left, right);
+            }
+        }
+    }
+}
