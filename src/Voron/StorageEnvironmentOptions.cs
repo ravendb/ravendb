2cd5401dca8ab32f5157b61addd55a9c2ae0894a
aviviadi <avivi.adi.hr@gmail.com>
2016-12-14 15:06:48 +0200

    test for o_direct and ommit the flag if neccessary

--- src/Voron/StorageEnvironmentOptions.cs
+++ src/Voron/StorageEnvironmentOptions.cs
@@ -22,6 +22,7 @@ namespace Voron
         public string TempPath { get; }
 
         public event EventHandler<RecoveryErrorEventArgs> OnRecoveryError;
+        public event EventHandler<NonDurabalitySupportEventArgs> OnNonDurabalitySupportError;
 
         public abstract override string ToString();
 
@@ -35,6 +36,18 @@ namespace Voron
             }
 
             handler(this, new RecoveryErrorEventArgs(message, e));
+        }
+
+        public void InvokeNonDurabalitySupportError(object sender, string message, Exception e)
+        {
+            var handler = OnNonDurabalitySupportError;
+            if (handler == null)
+            {
+                throw new InvalidDataException(message + Environment.NewLine +
+                     "An exception has been thrown because there isn't a listener to the OnRecoveryError event on the storage options.", e);
+            }
+
+            handler(this, new NonDurabalitySupportEventArgs(message, e));
         }
 
         public long? InitialFileSize { get; set; }
