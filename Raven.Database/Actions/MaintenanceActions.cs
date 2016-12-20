9b2ffeba0da65fe77f3bc6ef106732d23d580465
talweiss1982 <tal@ayende.com>
2016-12-20 19:27:53 +0200

    RavenDB-5800 Fixing Backup/Restore index madness

--- Raven.Database/Actions/MaintenanceActions.cs
+++ Raven.Database/Actions/MaintenanceActions.cs
@@ -49,7 +49,7 @@ namespace Raven.Database.Actions
             return backupPath ?? Path.Combine(rootBackupPath, Constants.DatabaseDocumentFilename);
         }
 
-        public static void Restore(RavenConfiguration configuration, DatabaseRestoreRequest restoreRequest, Action<string> output)
+        public static void Restore(RavenConfiguration configuration, DatabaseRestoreRequest restoreRequest, Action<string> output, InMemoryRavenConfiguration globalConfiguration = null)
         {
             var databaseDocumentPath = FindDatabaseDocument(restoreRequest.BackupLocation);
             if (File.Exists(databaseDocumentPath) == false)
@@ -80,10 +80,10 @@ namespace Raven.Database.Actions
             {
                 configuration.DataDirectory = restoreRequest.DatabaseLocation;
             }
-
+            
             using (var transactionalStorage = configuration.CreateTransactionalStorage(storage, () => { }, () => { }))
             {
-                transactionalStorage.Restore(restoreRequest, output);
+                transactionalStorage.Restore(restoreRequest, output, globalConfiguration);
             }
         }
 
