ea8691dd2f9efabb18ba34da87a34f2443a3d899
Pawel Pekrol <ppekrol@ravendb.net>
2016-12-21 14:22:51 +0100

    RavenDB-5861 migrating tests

---
+++ test/SlowTests/Issues/RavenDB-5525.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using FastTests;
+using Raven.Client;
+using Raven.Client.Indexes;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class QueryCommaTest : RavenTestBase
+    {
+        private class Employee
+        {
+            public string Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        private class Employees_ByFirstName : AbstractIndexCreationTask<Employee>
+        {
+            public Employees_ByFirstName()
+            {
+                Map = employees => from employee in employees
+                                   select new
+                                   {
+                                       employee.Id,
+                                       employee.FirstName,
+                                   };
+            }
+        }
+
+        [Fact]
+        public void CommaInQueryTest()
+        {
+            using (var store = GetDocumentStore())
+            {
+                new Employees_ByFirstName().Execute(store);
+
+                using (var session = store.OpenSession())
+                {
+                    session
+                        .Query<Employee, Employees_ByFirstName>()
+                        .Search(x => x.FirstName, "foo , bar")
+                        .ToList();
+                    // Lucene.Net.QueryParsers.ParseException: Could not parse:
+                    // ' FirstName:(foo , bar)' ---> Lucene.Net.QueryParsers.ParseException: Syntax error, unexpected COMMA
+                }
+            }
+        }
+    }
+}
