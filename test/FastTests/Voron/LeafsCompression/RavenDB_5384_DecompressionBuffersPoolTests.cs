33e14b1ac54be6fb9f6f81bd4137cf372dd86801
Arkadiusz Palinski <arek@ravendb.net>
2016-12-22 09:04:53 +0100

    RavenDB-5384 Creating multiple decompression files instead of a single, big one. Cleaning them up. Moving slow test cases to SlowTests.

---
+++ test/FastTests/Voron/LeafsCompression/RavenDB_5384_DecompressionBuffersPoolTests.cs
@@ -0,0 +1,51 @@
+﻿using Sparrow;
+using Voron;
+using Voron.Impl.Paging;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace FastTests.Voron.LeafsCompression
+{
+    public class RavenDB_5384_DecompressionBuffersPoolTests : StorageTest
+    {
+        private const int _64KB = 64 * 1024;
+
+        protected override void Configure(StorageEnvironmentOptions options)
+        {
+            options.MaxScratchBufferSize = _64KB * 4;
+        }
+
+        [Fact]
+        public void Should_cleanup_scratch_files_which_are_no_longer_necessary()
+        {
+            using (var tx = Env.ReadTransaction())
+            {
+                var llt = tx.LowLevelTransaction;
+                
+                TemporaryPage temp;
+                TemporaryPage temp3;
+                var page1 = Env.DecompressionBuffers.GetTemporaryPage(llt, _64KB, out temp);
+
+                var page2 = Env.DecompressionBuffers.GetTemporaryPage(llt, _64KB, out temp);
+
+                var page3 = Env.DecompressionBuffers.GetTemporaryPage(llt, _64KB, out temp3);
+
+                var page4 = Env.DecompressionBuffers.GetTemporaryPage(llt, _64KB, out temp);
+
+                var page5 = Env.DecompressionBuffers.GetTemporaryPage(llt, _64KB, out temp);
+
+                Assert.Equal(2, Env.DecompressionBuffers.NumberOfScratchFiles);
+
+                page1.Dispose();
+                page2.Dispose();
+                
+                Env.DecompressionBuffers.Cleanup();
+
+                Assert.Equal(1, Env.DecompressionBuffers.NumberOfScratchFiles);
+
+                Env.DecompressionBuffers.GetTemporaryPage(llt, _64KB, out temp);
+            }
+            
+        }
+    }
+}
---
+++ test/SlowTests/Voron/LeafsCompression/RavenDB_5384.cs
@@ -0,0 +1,19 @@
+﻿using FastTests;
+using Xunit;
+
+namespace SlowTests.Voron.LeafsCompression
+{
+    public class RavenDB_5384 : NoDisposalNeeded
+    {
+        [Theory]
+        [InlineData(8192, 512, true, 1)]
+        [InlineData(16384, 512, false, 1)]
+        public void Leafs_compressed_CRUD(int iterationCount, int size, bool sequentialKeys, int seed)
+        {
+            using (var test = new FastTests.Voron.LeafsCompression.RavenDB_5384())
+            {
+                test.Leafs_compressed_CRUD(iterationCount, size, sequentialKeys, seed);
+            }
+        }
+    }
+}
