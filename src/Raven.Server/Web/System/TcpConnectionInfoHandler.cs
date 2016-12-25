09916ca7053969fdc233b7ee5aac323650aba01f
Ayende Rahien <ayende@ayende.com>
2016-12-25 10:30:01 +0200

    RavenDB-5949 Auto detect what hostname to use when getting info/tcp

--- src/Raven.Server/Web/System/TcpConnectionInfoHandler.cs
+++ src/Raven.Server/Web/System/TcpConnectionInfoHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Raven.Server.Routing;
 using Sparrow.Json;
 using Sparrow.Json.Parsing;
@@ -15,10 +16,15 @@ namespace Raven.Server.Web.System
             using (ServerStore.ContextPool.AllocateOperationContext(out context))
             using (var writer = new BlittableJsonTextWriter(context, ResponseBodyStream()))
             {
-                var url = await Server.GetTcpServerPortAsync();
+                var tcpListenerStatus = await Server.GetTcpServerStatusAsync();
+
+                string host = HttpContext.Request.Host.Host;
+                if (string.IsNullOrWhiteSpace(Server.Configuration.Core.TcpServerUrl) == false)
+                    host = new UriBuilder(Server.Configuration.Core.TcpServerUrl).Host;
+
                 var output = new DynamicJsonValue
                 {
-                    ["Url"] = url
+                    ["Url"] = new UriBuilder("tcp", host, tcpListenerStatus.Port).Uri.ToString()
                 };
 
                 context.Write(writer, output);
