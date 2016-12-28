add1bab361d7e1d797532cebe28b508d1b8c3b27
Ayende Rahien <ayende@ayende.com>
2016-12-28 14:00:30 +0200

    RavenDB-5613 - Make sure that we aren't hiding information if the test fails

--- test/FastTests/Server/Basic/MaxSecondsForTaskToWaitForDatabaseToLoad.cs
+++ test/FastTests/Server/Basic/MaxSecondsForTaskToWaitForDatabaseToLoad.cs
@@ -14,28 +14,20 @@ namespace FastTests.Server.Basic
             DoNotReuseServer();
             Server.Configuration.Server.MaxTimeForTaskToWaitForDatabaseToLoad = new TimeSetting(1, TimeUnit.Milliseconds);
 
-                var tryLoad = new Action((delegate
+            var tryLoad = new Action((delegate
+            {
+                using (var store = GetDocumentStore())
+                using (var session = store.OpenSession())
                 {
-                    using (var store = GetDocumentStore())
-                    using (var session = store.OpenSession())
-                    {
-                        session.Load<object>("Raven/ServerPrefixForHilo");
-                    }
-                }));
-
-                Assert.Throws<ErrorResponseException>(tryLoad);
-
-                try
-                {
-                    Server.Configuration.Server.MaxTimeForTaskToWaitForDatabaseToLoad = new TimeSetting(10, TimeUnit.Seconds);
-                    tryLoad.Invoke();
+                    session.Load<object>("Raven/ServerPrefixForHilo");
                 }
-                catch (Exception)
-                {
-                    //fail - expected no exception this time 
-                    Assert.True(false);
-                }
+            }));
+
+            Assert.Throws<ErrorResponseException>(tryLoad);
+
+            Server.Configuration.Server.MaxTimeForTaskToWaitForDatabaseToLoad = new TimeSetting(10, TimeUnit.Seconds);
+
+            tryLoad.Invoke();
         }
-
     }
 }
