2acce47a6dda09327d0512d63a506dad59ec7323
Grisha Kotler <grisha@ayende.com>
2017-01-04 16:30:41 +0200

    RavenDB-5811 - syntax highlighting for displaying class code

--- src/Raven.Studio/typescript/viewmodels/common/showDataDialog.ts
+++ src/Raven.Studio/typescript/viewmodels/common/showDataDialog.ts
@@ -1,40 +1,55 @@
 import dialog = require("plugins/dialog");
 import dialogViewModelBase = require("viewmodels/dialogViewModelBase");
 import copyToClipboard = require("common/copyToClipboard");
+import aceEditorBindingHandler = require("common/bindingHelpers/aceEditorBindingHandler");
 
 class showDataDialog extends dialogViewModelBase {
 
-    constructor(private title: string, private inputData: string, elementToFocusOnDismissal?: string) {
+    width = ko.observable<string>("");
+    inputData = ko.observable<string>();
+
+    constructor(private title: string, inputData: string, elementToFocusOnDismissal?: string, calcWidth: boolean = false) {
         super(elementToFocusOnDismissal);
+
+        this.inputData(inputData);
+        if (calcWidth) {
+            this.width(this.calcWidth(inputData) + "px");
+        }  
     }
-    
+
+    calcWidth(code: string): number {
+        const arrayOfLines = code.match(/[^\r\n]+/g);
+        const maxLineLength = Math.max.apply(Math, $.map(arrayOfLines, el => el.length));
+        return Math.max(Math.min(maxLineLength * 12, $(window).width() - 200), 598 /*default*/);
+    }
+
     attached() {
         super.attached();
-        this.selectText();
+        aceEditorBindingHandler.install();
+        //this.selectText();
 
         //TODO: create createKeyboardShortcut in dialog view model base and use it
-        jwerty.key("CTRL+C, enter", e => {
+        /*jwerty.key("CTRL+C, enter", e => {
             e.preventDefault();
             this.close();
-        }, this, "#documentsText");
-
+        }, this, "#documentsText");*/
     }
 
-    deactivate() {
+    /*deactivate() {
         //TODO: call super?
         $("#inputData").unbind('keydown.jwerty');
     }
 
     selectText() {
         $("#inputData").select();
-    }
+    }*/
 
     close() { //TODO: move to base class?
         dialog.close(this);
     }
 
     copyToClipboard() {
-        copyToClipboard.copy($("#inputData").val(), this.title + " was copied to clipboard");
+        copyToClipboard.copy(this.inputData(), this.title + " was copied to clipboard");
         this.close();
     }
 }
