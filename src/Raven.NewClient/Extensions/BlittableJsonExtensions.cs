4a99a1fd089e4e499cf30d1c61f242e0838f9231
Pawel Pekrol <ppekrol@ravendb.net>
2017-01-05 15:38:25 +0100

    RavenDB-5861 extracting id properly from the revision documents

---
+++ src/Raven.NewClient/Extensions/BlittableJsonExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Raven.NewClient.Abstractions.Data;
+using Sparrow.Json;
+
+namespace Raven.NewClient.Extensions
+{
+    public static class BlittableJsonExtensions
+    {
+        public static BlittableJsonReaderObject GetMetadata(this BlittableJsonReaderObject document)
+        {
+            object metadataObj;
+            if (document.TryGetMember(Constants.Metadata.Key, out metadataObj) == false)
+                throw new InvalidOperationException($"Document does not contain '{Constants.Metadata.Key}' field.");
+
+            var metadata = metadataObj as BlittableJsonReaderObject;
+            if (metadata == null)
+                throw new InvalidOperationException($"Field {Constants.Metadata.Key} is null or not an JSON object.");
+
+            return metadata;
+        }
+
+        public static string GetId(this BlittableJsonReaderObject metadata)
+        {
+            string id;
+            if (metadata.TryGet(Constants.Metadata.Id, out id) == false)
+                throw new InvalidOperationException($"Metadata does not contain '{Constants.Metadata.Id}' field.");
+
+            return id;
+        }
+    }
+}
