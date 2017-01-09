621b7d06eda8af548e5d1dbfafae89957f70122b
Efrat Shenhar <efrat@hibernatingrhinos.com>
2017-01-09 11:47:17 +0200

    RavenDB-6069 - Test for nullable properties

--- ClientTests/ClientFastTests/Blittable/Serialization.cs
+++ ClientTests/ClientFastTests/Blittable/Serialization.cs
@@ -27,12 +27,48 @@ namespace NewClientTests.NewClient.Blittable
             }
         }
 
+        [Fact]
+        public void Can_Store_And_Load_Nullable()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new NullableDummy() { NullInt = 1, NullBool = true, NullDouble = double.MinValue, NullFloat = float.MaxValue}, "dummies/1");
+                    session.Store(new NullableDummy() { NullInt = null, NullBool = null, NullDouble = null, NullFloat = null }, "dummies/2");
+
+                    session.SaveChanges();
+
+                    var doc1 = session.Load<NullableDummy>("dummies/1");
+                    var doc2 = session.Load<NullableDummy>("dummies/2");
+
+                    Assert.Equal(doc1.NullInt ,1);
+                    Assert.True(doc1.NullBool);
+                    Assert.Equal(doc1.NullDouble, double.MinValue);
+                    Assert.Equal(doc1.NullFloat, float.MaxValue);
+
+                    Assert.Null(doc2.NullInt);
+                    Assert.Null(doc2.NullBool);
+                    Assert.Null(doc2.NullDouble);
+                    Assert.Null(doc2.NullFloat);
+                }
+            }
+        }
+
         public class Dummy
         {
             public string EntityName { get; set; }
             public int LuckyNumber { get; set; }
             public bool IsLucky { get; set; }
             public bool? IsRich { get; set; }
+        }
+
+        public class NullableDummy
+        {
+            public int? NullInt { get; set; }
+            public bool? NullBool { get; set; }
+            public float? NullFloat { get; set; }
+            public double? NullDouble { get; set; }
         }
     }
 }
