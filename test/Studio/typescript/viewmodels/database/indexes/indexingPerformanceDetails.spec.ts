5cbf7bc1d7e50685548d6f4884210a20c421cfda
Marcin Lewandowski <marcin@ravendb.net>
2017-01-10 12:00:52 +0100

    RavenDB-5595 Optimize / refactor index performance view - test for details view - fixed performance endpoint bug

---
+++ test/Studio/typescript/viewmodels/database/indexes/indexingPerformanceDetails.spec.ts
@@ -0,0 +1,41 @@
+﻿import utils = require("utils");
+
+var viewUnderTest = 'database/indexes/indexPerformanceDetails';
+
+describe(viewUnderTest, () => {
+    utils.initTest();
+
+    it('should bind', () => {
+
+        return utils.mockActiveDatabase()
+            .then(() => utils.runViewmodelTest(viewUnderTest, {
+                viewmodelConstructorArgs: [getIndexPerformance()]
+            }));
+    });
+
+});
+
+
+function getIndexPerformance(): Raven.Client.Data.Indexes.IndexingPerformanceOperation {
+    return {
+        "CommitDetails": {
+            "NumberOfModifiedPages": 0,
+            "NumberOfPagesWrittenToDisk": 0
+        },
+        "DurationInMilliseconds": 99.77,
+        "MapDetails": {
+            "AllocationBudget": 0,
+            "BatchCompleteReason": "No more documents to index",
+            "CurrentlyAllocated": 0,
+            "ProcessPrivateMemory": 0,
+            "ProcessWorkingSet": 0
+        },
+        "Name": "Indexing",
+        Operations: [],
+        ReduceDetails: {
+            "NumberOfCompressedLeafs": 0,
+            "NumberOfModifiedBranches": 0,
+            "NumberOfModifiedLeafs": 3
+        }
+    }
+}
