6519c08c5d04b617d5c6b08340307cd44a73ffb8
aviviadi <avivi.adi.hr@gmail.com>
2017-01-10 11:46:34 +0200

    FixTestInLinux: skip port 8000

--- ClientTests/Tests.Base/RavenTestBase.cs
+++ ClientTests/Tests.Base/RavenTestBase.cs
@@ -54,7 +54,7 @@ namespace NewClientTests
         private bool _doNotReuseServer;
         private int NonReusedServerPort { get; set; }
         private int NonReusedTcpServerPort { get; set; }
-        private const int MaxParallelServer = 79;
+        private const int MaxParallelServer = 78; // port 8000 might be reserved on some cases for IPv6 translation
         private static readonly List<int> _usedServerPorts = new List<int>();
 
         private static readonly List<int> _availableServerPorts =
