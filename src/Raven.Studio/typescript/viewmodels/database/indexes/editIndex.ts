9dea99b41bd6d3f2ccfeb359eddd6c158ceeea7f
Pawel Pekrol <ppekrol@ravendb.net>
2017-01-19 12:35:18 +0100

    RavenDB-6131 fix merge issue, fixed studio compilation, fixed memory stats calculation

--- src/Raven.Studio/typescript/viewmodels/database/indexes/editIndex.ts
+++ src/Raven.Studio/typescript/viewmodels/database/indexes/editIndex.ts
@@ -172,7 +172,7 @@ class editIndex extends viewModelBase {
             .execute()
             .done((databaseDocument: document) => {
                 const settings = (<any>databaseDocument)["Settings"] as dictionary<string>;
-                const indexStoragePath = settings[configuration.indexing.indexStoragePath];
+                const indexStoragePath = settings[configuration.indexing.storagePath];
                 //TODO: don't use .string here: but additionalIndexStoragePaths - waiting for RavenDB-5665)
                 const additionalPaths = settings[configuration.indexing.string] ? settings[configuration.indexing.string].split(";") : [];
                 this.additionalStoragePaths(additionalPaths);
