9fb6c468edbb6f1aa6de83959400e24f68b59c63
Arkadiusz Palinski <arek@ravendb.net>
2017-01-27 10:54:01 +0100

    RavenDB-6147 Moving a few more tests cases to StressTests

---
+++ test/StressTests/Voron/Bugs/DataInconsistencyReproStressTests.cs
@@ -0,0 +1,21 @@
+﻿using FastTests;
+using SlowTests.Utils;
+using SlowTests.Voron.Bugs;
+using Xunit;
+
+namespace StressTests.Voron.Bugs
+{
+    public class DataInconsistencyReproStressTests : NoDisposalNeeded
+    {
+        [Theory]
+        [InlineDataWithRandomSeed(1000, 50000)]
+        public void FaultyOverflowPagesHandling_CannotModifyReadOnlyPages(int initialNumberOfDocs,
+            int numberOfModifications, int seed)
+        {
+            using (var test = new DataInconsistencyRepro())
+            {
+                test.FaultyOverflowPagesHandling_CannotModifyReadOnlyPages(initialNumberOfDocs, numberOfModifications, seed);
+            }
+        }
+    }
+}
