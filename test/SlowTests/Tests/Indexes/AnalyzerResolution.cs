559bed09b731ee1188b9eca5a501190121c254b2
Pawel Pekrol <ppekrol@ravendb.net>
2017-02-02 13:23:03 +0100

    RavenDB-5861 migrating tests

--- test/SlowTests/Tests/Indexes/AnalyzerResolution.cs
+++ test/SlowTests/Tests/Indexes/AnalyzerResolution.cs
@@ -1,24 +1,24 @@
 using System.Linq;
-using System.Threading.Tasks;
 using FastTests;
 using FastTests.Server.Basic.Entities;
-using Raven.Client.Indexes;
+using Raven.NewClient.Client.Indexes;
+using Raven.NewClient.Operations.Databases.Indexes;
 using Xunit;
 
 namespace SlowTests.Tests.Indexes
 {
-    public class AnalyzerResolution : RavenTestBase
+    public class AnalyzerResolution : RavenNewTestBase
     {
         [Fact]
         public void can_resolve_internal_analyzer()
         {
             using (var store = GetDocumentStore())
             {
-                store.DatabaseCommands.PutIndex("test", new IndexDefinitionBuilder<User>
+                store.Admin.Send(new PutIndexOperation("test", new IndexDefinitionBuilder<User>
                 {
                     Map = docs => from doc in docs select new { doc.Id },
                     Analyzers = { { x => x.Id, "SimpleAnalyzer" } }
-                });
+                }.ToIndexDefinition(store.Conventions)));
             }
         }
     }
