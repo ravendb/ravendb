b5024ce97eeb69dfb2c684133d860b1e8c251083
aviv86 <aviv@ayende.com>
2017-02-12 14:38:40 +0200

    (RavenDB-6317) : Fix failing subscription tests

--- test/FastTests/Subscriptions/RavenDB_3491.cs
+++ test/FastTests/Subscriptions/RavenDB_3491.cs
@@ -52,9 +52,9 @@ namespace NewClientTests.NewClient.Subscriptions
                         var docs = new BlockingCollection<dynamic>();
                         var keys = new BlockingCollection<string>();
                         var ages = new BlockingCollection<int>();
-
-                        subscription.Subscribe(x => keys.Add(x[Constants.Metadata.Key].Value<string>("@id")));
-                        subscription.Subscribe(x => ages.Add(x.Value<int>("Age")));
+                        
+                        subscription.Subscribe(x => keys.Add(x.Id));
+                        subscription.Subscribe(x => ages.Add(x.Age));
 
                         subscription.Subscribe(docs.Add);
 
@@ -213,8 +213,8 @@ namespace NewClientTests.NewClient.Subscriptions
                         var keys = new BlockingCollection<string>();
                         var ages = new BlockingCollection<int>();
 
-                        subscription.Subscribe(x => keys.Add(x[Constants.Metadata.Key].Value<string>("@id")));
-                        subscription.Subscribe(x => ages.Add(x.Value<int>("Age")));
+                        subscription.Subscribe(x => keys.Add(x.Id));
+                        subscription.Subscribe(x => ages.Add(x.Age));
 
                         subscription.Subscribe(docs.Add);
 
