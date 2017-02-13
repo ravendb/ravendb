bb8d5118bdbc3a7065232aa34e20e928e5e6bb86
Marcin Lewandowski <marcin@ravendb.net>
2017-02-13 15:44:28 +0100

    RavenDB-6329 Cleanup scripted indexes usages from studio

--- src/Raven.Studio/typescript/viewmodels/database/settings/settings.ts
+++ src/Raven.Studio/typescript/viewmodels/database/settings/settings.ts
@@ -41,7 +41,6 @@ class settings extends viewModelBase {
                 editsqlReplicationsRoute,
                 versioningRoute,
                 periodicExportRoute,
-                //scriptedIndexesRoute,
                 customFunctionsEditorRoute,
                 databaseStudioConfig
             ])
