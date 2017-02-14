f9603068c59f21bbe8bd0ff09eeee4ab8a9a068f
Arkadiusz Palinski <arek@ravendb.net>
2017-02-14 11:25:12 +0100

    RavenDB-6335 Prevent from using free space handling when we are splitting / rebalancing the free space tree. Also protect from recursive calls to ensure that bytes of a section that we are trying to write won't be changed underneath by internal allocations / frees of pages belonging to the free space tree. Exposing FreeSpaceHandling.PageFreed event in order to properly write a test where we need to take into account pages of the free space tree.

--- Raven.Voron/Voron.Tests/Trees/FreeSpaceTest.cs
+++ Raven.Voron/Voron.Tests/Trees/FreeSpaceTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using Voron.Impl.FreeSpace;
 using Xunit;
+using Xunit.Extensions;
 
 namespace Voron.Tests.Trees
 {
@@ -121,12 +122,18 @@ namespace Voron.Tests.Trees
             }
         }
 
-        [PrefixesFact]
-        public void FreeSpaceHandlingShouldNotReturnPagesThatAreAlreadyAllocated()
+        [PrefixesTheory]
+        [InlineData(60, 2)]
+        [InlineData(60, 893)]
+        [InlineData(60, 6430)]
+        [InlineData(60, 7749)]
+        [InlineData(56, 893)]
+        [InlineDataWithRandomSeed(60)]
+        [InlineDataWithRandomSeed(-1)] // also random 'numberOfFreedPages'
+        public void FreeSpaceHandlingShouldNotReturnPagesThatAreAlreadyAllocated(int numberOfFreedPages, int seed)
         {
             const int maxPageNumber = 400000;
-            const int numberOfFreedPages = 60;
-            var random = new Random(2);
+            var random = new Random(seed);
             var freedPages = new HashSet<long>();
 
             using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
@@ -134,6 +141,11 @@ namespace Voron.Tests.Trees
                 tx.State.NextPageNumber = maxPageNumber + 1;
 
                 tx.Commit();
+            }
+
+            if (numberOfFreedPages == -1)
+            {
+                numberOfFreedPages = random.Next(0, 10000);
             }
 
             for (int i = 0; i < numberOfFreedPages; i++)
@@ -153,7 +165,9 @@ namespace Voron.Tests.Trees
             }
 
             var alreadyReused = new List<long>();
+            var freedInternallyByFreeSpaceHandling = new HashSet<long>();
 
+            ((FreeSpaceHandling)Env.FreeSpaceHandling).PageFreed += pageNumber => freedInternallyByFreeSpaceHandling.Add(pageNumber); // need to take into account pages freed by free space handling itself
             do
             {
                 using (var tx = Env.NewTransaction(TransactionFlags.ReadWrite))
@@ -166,7 +180,7 @@ namespace Voron.Tests.Trees
                     }
 
                     Assert.False(alreadyReused.Contains(page.Value), "Free space handling returned a page number that has been already allocated. Page number: " + page);
-                    Assert.True(freedPages.Remove(page.Value));
+                    Assert.True(freedPages.Remove(page.Value) || freedInternallyByFreeSpaceHandling.Remove(page.Value));
 
                     alreadyReused.Add(page.Value);
 
