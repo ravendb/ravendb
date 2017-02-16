9228ac8f19f93ef12423f6f6fa59686d3a42cccf
Pawel Pekrol <ppekrol@ravendb.net>
2017-02-16 10:11:42 +0100

    RavenDB-6153 forbidding the usage of Now and UtcNow for DateTime/DateTimeOffset/SystemTime

---
+++ src/Raven.Server/Documents/Indexes/Static/Roslyn/Rewriters/ThrowOnInvalidMethodCalls.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Raven.Server.Documents.Indexes.Static.Roslyn.Rewriters
+{
+    public class ThrowOnInvalidMethodCalls : CSharpSyntaxRewriter
+    {
+        public static ThrowOnInvalidMethodCalls Instance = new ThrowOnInvalidMethodCalls();
+
+        private ThrowOnInvalidMethodCalls()
+        {
+        }
+
+        private readonly List<ForbiddenMethod> _forbiddenMethods = new List<ForbiddenMethod>
+        {
+            new ForbiddenMethod(
+                names: new[] { "Now", "UtcNow" },
+                typeAliases: new[] { "DateTime", "System.DateTime", "DateTimeOffset", "System.DateTimeOffset", "SystemTime", "Raven.Client.Util.SystemTime", "Client.Util.SystemTime", "Util.SystemTime" },
+                error: @"Cannot use {0} during a map or reduce phase.
+The map or reduce functions must be referentially transparent, that is, for the same set of values, they always return the same results.
+Using {0} invalidate that premise, and is not allowed"),
+        };
+
+        public override SyntaxNode VisitMemberAccessExpression(MemberAccessExpressionSyntax node)
+        {
+            var name = node.Name.ToString();
+            var method = _forbiddenMethods.FirstOrDefault(x => x.Names.Contains(name));
+            if (method == null)
+                return base.VisitMemberAccessExpression(node);
+
+            var expression = node.Expression.ToString();
+            var alias = method.TypeAliases.FirstOrDefault(x => x.Contains(expression));
+            if (alias == null)
+                return base.VisitMemberAccessExpression(node);
+
+            throw new InvalidOperationException(string.Format(method.Error, $"{expression}.{name}"));
+        }
+
+        private class ForbiddenMethod
+        {
+            public readonly string[] TypeAliases;
+            public readonly string[] Names;
+            public readonly string Error;
+
+            public ForbiddenMethod(string[] names, string[] typeAliases, string error)
+            {
+                TypeAliases = typeAliases;
+                Names = names;
+                Error = error;
+            }
+        }
+    }
+}
--- test/SlowTests/Bugs/Indexing/InvalidIndexes.cs
+++ test/SlowTests/Bugs/Indexing/InvalidIndexes.cs
@@ -2,28 +2,26 @@ using FastTests;
 using Raven.Client.Documents.Exceptions.Compilation;
 using Raven.Client.Documents.Indexes;
 using Raven.Client.Documents.Operations.Indexes;
-using Raven.Client.Exceptions.Compilation;
 using Xunit;
 
 namespace SlowTests.Bugs.Indexing
 {
     public class InvalidIndexes : RavenTestBase
     {
-        [Fact(Skip = "Missing feature: RavenDB-6153")]
+        [Fact]
         public void CannotCreateIndexesUsingDateTimeNow()
         {
             using (var store = GetDocumentStore())
             {
                 var ioe = Assert.Throws<IndexCompilationException>(() =>
-                    store.Admin.Send(new PutIndexesOperation(new[] {
-                        new IndexDefinition
-                        {
+                    store.Admin.Send(new PutIndexesOperation(new IndexDefinition
+                    {
 
-                            Maps = { @"from user in docs.Users 
+                        Maps = { @"from user in docs.Users 
                                     where user.LastLogin > DateTime.Now.AddDays(-10) 
                                     select new { user.Name}" },
-                            Name = "test"
-                        }})));
+                        Name = "test"
+                    })));
 
                 Assert.Contains(@"Cannot use DateTime.Now during a map or reduce phase.", ioe.Message);
             }
