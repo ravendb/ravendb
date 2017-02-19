d24c1f1871a26e523920a44a3b90423f486f409e
talweiss1982 <tal@ayende.com>
2017-02-19 14:58:20 +0200

    RavenDB-6097 fixes

--- src/Raven.Client/Documents/TcpUtils.cs
+++ src/Raven.Client/Documents/TcpUtils.cs
@@ -11,16 +11,13 @@ namespace Raven.Client.Document
 {
     public static class TcpUtils
     {
-        public static async Task<Stream> WrapStreamWithSsl(TcpClient tcpClient, TcpConnectionInfo info)
+        internal static async Task<Stream> WrapStreamWithSsl(TcpClient tcpClient, TcpConnectionInfo info)
         {
             Stream stream = tcpClient.GetStream();
             if (info.Certificate != null)
             {
                 var Cert = new X509Certificate2(Convert.FromBase64String(info.Certificate));
                 var sslStream = new SslStream(stream, false, (sender, certificate, chain, errors) => Cert.Equals(certificate));
-                await
-                    sslStream.AuthenticateAsClientAsync("RavenDB", new X509Certificate2Collection { Cert }, SslProtocols.Tls12,
-                        false);
                 stream = sslStream;
             }
             return stream;
