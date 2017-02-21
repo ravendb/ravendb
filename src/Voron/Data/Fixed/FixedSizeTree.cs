d7454ef40137e9408c3e75b0ca808190e9948d87
Arkadiusz Palinski <arek@ravendb.net>
2017-02-21 15:47:36 +0100

    Revert "RavenDB-6350 Fixed invalid usage of DirectAdd - we cannot keep the pointer to the header of a large FST during page splits because underneath DirectAdd can be called on the same tree instance by NewPageAllocator. In result modification of a tree header would overwrite entries of Allocation-Storage tree"
    This reverts commit 04a58ce03cab8b15ba084ec4c13c8f7061a0322c.

--- src/Voron/Data/Fixed/FixedSizeTree.cs
+++ src/Voron/Data/Fixed/FixedSizeTree.cs
@@ -389,6 +389,7 @@ namespace Voron.Data.Fixed
 
         private FixedSizeTreePage PageSplit(FixedSizeTreePage page, long key)
         {
+            var largePtr = (FixedSizeTreeHeader.Large*)_parent.DirectAdd(_treeName, sizeof(FixedSizeTreeHeader.Large));
             FixedSizeTreePage parentPage = _cursor.Count > 0 ? _cursor.Pop() : null;
             if (parentPage == null) // root split
             {
@@ -397,7 +398,6 @@ namespace Voron.Data.Fixed
                 parentPage.StartPosition = (ushort)Constants.FixedSizeTree.PageHeaderSize;
                 parentPage.ValueSize = _valSize;
 
-                var largePtr = (FixedSizeTreeHeader.Large*)_parent.DirectAdd(_treeName, sizeof(FixedSizeTreeHeader.Large));
                 largePtr->RootPageNumber = parentPage.PageNumber;
                 largePtr->Depth++;
                 largePtr->PageCount++;
@@ -413,9 +413,6 @@ namespace Voron.Data.Fixed
                 newPage.StartPosition = (ushort)Constants.FixedSizeTree.PageHeaderSize;
                 newPage.ValueSize = _valSize;
                 newPage.NumberOfEntries = 0;
-
-                var largePtr = (FixedSizeTreeHeader.Large*)_parent.DirectAdd(_treeName, sizeof(FixedSizeTreeHeader.Large));
-
                 largePtr->PageCount++;
 
                 // need to add past end of pageNum, optimized
@@ -446,7 +443,6 @@ namespace Voron.Data.Fixed
                 newPage.StartPosition = (ushort)Constants.FixedSizeTree.PageHeaderSize;
                 newPage.ValueSize = _valSize;
                 newPage.NumberOfEntries = 0;
-                var largePtr = (FixedSizeTreeHeader.Large*)_parent.DirectAdd(_treeName, sizeof(FixedSizeTreeHeader.Large));
                 largePtr->PageCount++;
 
                 if (page.LastMatch > 0)
