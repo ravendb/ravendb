52ca08f226c1aa4405059433f3474074b23b5c38
talweiss1982 <tal@ayende.com>
2017-02-27 18:36:24 +0200

    RavenDB-4916 Parser compiling on v4.0

--- src/LuceneParser/LuceneQuery.Parser.cs
+++ src/LuceneParser/LuceneQuery.Parser.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Raven.Server.Documents.Queries.Parse
@@ -8,13 +9,27 @@ namespace Raven.Server.Documents.Queries.Parse
     internal partial class LuceneQueryParser
     {
         public LuceneQueryParser() : base(null) { }
-
+        private bool inMethod;
+        public bool IsDefaultOperatorAnd { get; set; }
+        public bool InMethod
+        {
+            get
+            {
+                return inMethod;
+            }
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set
+            {
+                ((LuceneQueryScanner)Scanner).InMethod = value;
+                inMethod = value;
+            }
+        }
         public void Parse(string s)
         {
-            byte[] inputBuffer = System.Text.Encoding.Default.GetBytes(s);
-            MemoryStream stream = new MemoryStream(inputBuffer);
-            this.Scanner = new LuceneQueryScanner(stream);
-            this.Parse();
+            var luceneQueryScanner = new LuceneQueryScanner();
+            luceneQueryScanner.SetSource(s, 0);
+            Scanner = luceneQueryScanner;
+            Parse();
         }
     }
 }
