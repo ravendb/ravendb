af7c5815a66d3cfba12edaae5bb9d4ae8f198788
Pawel Pekrol <ppekrol@ravendb.net>
2017-03-02 11:55:04 +0100

    RavenDB-5861 fixed issues with response processing in StreamOperation, migrating tests

--- src/Sparrow/Json/Parsing/UnmanagedJsonParserHelper.cs
+++ src/Sparrow/Json/Parsing/UnmanagedJsonParserHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Sparrow.Json.Parsing
 {
@@ -35,6 +36,24 @@ namespace Sparrow.Json.Parsing
             return parser.Read();
         }
 
+        public static async Task<bool> ReadAsync(Stream stream, UnmanagedJsonParser parser, JsonParserState state, JsonOperationContext.ManagedPinnedBuffer buffer)
+        {
+            if (parser.Read())
+                return true;
+
+            var read = await stream.ReadAsync(buffer.Buffer.Array, buffer.Buffer.Offset, buffer.Length).ConfigureAwait(false);
+            if (read == 0)
+            {
+                if (state.CurrentTokenType != JsonParserToken.EndObject)
+                    throw new EndOfStreamException("Stream ended without reaching end of json content");
+
+                return false;
+            }
+
+            parser.SetBuffer(buffer, 0, read);
+            return parser.Read();
+        }
+
         public static void ThrowInvalidJson()
         {
             throw new InvalidOperationException("Invalid JSON.");
@@ -52,6 +71,45 @@ namespace Sparrow.Json.Parsing
                 parser.SetBuffer(buffer, 0, read);
             }
             builder.FinalizeDocument();
+        }
+
+        public static async Task ReadObjectAsync(BlittableJsonDocumentBuilder builder, Stream stream, UnmanagedJsonParser parser, JsonOperationContext.ManagedPinnedBuffer buffer)
+        {
+            builder.ReadNestedObject();
+            while (builder.Read() == false)
+            {
+                var read = await stream.ReadAsync(buffer.Buffer.Array, buffer.Buffer.Offset, buffer.Length).ConfigureAwait(false);
+                if (read == 0)
+                    throw new EndOfStreamException("Stream ended without reaching end of json content");
+
+                parser.SetBuffer(buffer, 0, read);
+            }
+            builder.FinalizeDocument();
+        }
+
+        public static IEnumerable<BlittableJsonReaderObject> ReadArrayToMemory(JsonOperationContext context, Stream stream, UnmanagedJsonParser parser, JsonParserState state, JsonOperationContext.ManagedPinnedBuffer buffer)
+        {
+            if (Read(stream, parser, state, buffer) == false)
+                ThrowInvalidJson();
+
+            if (state.CurrentTokenType != JsonParserToken.StartArray)
+                ThrowInvalidJson();
+
+            while (true)
+            {
+                if (Read(stream, parser, state, buffer) == false)
+                    ThrowInvalidJson();
+
+                if (state.CurrentTokenType == JsonParserToken.EndArray)
+                    break;
+
+                using (var builder = new BlittableJsonDocumentBuilder(context, BlittableJsonDocumentBuilder.UsageMode.None, "readArray/singleResult", parser, state))
+                {
+                    ReadObject(builder, stream, parser, buffer);
+
+                    yield return builder.CreateReader();
+                }
+            }
         }
     }
 }
