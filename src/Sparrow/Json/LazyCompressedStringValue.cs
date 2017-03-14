a5c4e680f15ea8aa17c5c4757fd1c286ffba1d7e
Federico Andres Lois <federico.lois@corvalius.com>
2017-03-14 01:15:17 -0300

    Several improvements to JSON based operations which regressed write sustained speed caused by the memory consumption optimizations.

--- src/Sparrow/Json/LazyCompressedStringValue.cs
+++ src/Sparrow/Json/LazyCompressedStringValue.cs
@@ -44,11 +44,11 @@ namespace Sparrow.Json
 
             try
             {
-                var charCount = self._context.Encoding.GetCharCount(tempBuffer, self.UncompressedSize);
+                var charCount = JsonOperationContext.Encoding.GetCharCount(tempBuffer, self.UncompressedSize);
                 var str = new string(' ', charCount);
                 fixed (char* pStr = str)
                 {
-                    self._context.Encoding.GetChars(tempBuffer, self.UncompressedSize, pStr, charCount);
+                    JsonOperationContext.Encoding.GetChars(tempBuffer, self.UncompressedSize, pStr, charCount);
                     self.String = str;
                     return str;
                 }
