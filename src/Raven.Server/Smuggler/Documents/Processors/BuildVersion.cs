3e731e19b39238fda3166eba34a1d61ca3d89934
Ayende Rahien <ayende@ayende.com>
2017-03-15 08:39:52 +0200

    RavenDB-6548 - Properly handle build version for debug builds

--- src/Raven.Server/Smuggler/Documents/Processors/BuildVersion.cs
+++ src/Raven.Server/Smuggler/Documents/Processors/BuildVersion.cs
@@ -2,33 +2,17 @@ using System;
 
 namespace Raven.Server.Smuggler.Documents.Processors
 {
-    /*public static class BuildVersion
-    {
-        public static bool IsV4(long buildVersion)
-        {
-            return buildVersion >= 40000 && buildVersion <= 44999 || buildVersion == 40;
-        }
-
-        public static bool IsPreV4(long buildVersion)
-        {
-            return buildVersion == 0;
-        }
-    }*/
-
     public static class BuildVersion
     {
         public static BuildVersionType Type(long buildVersion)
         {
-            BuildVersionType type;
-
+            if(buildVersion == 40)
+                return BuildVersionType.V4; // debug / dev version
             if (buildVersion < 40000)
-                type = BuildVersionType.V3;
-            else if (buildVersion >= 40000 && buildVersion <= 49999 || buildVersion == 40 || buildVersion == 45)
-                type = BuildVersionType.V4;
-            else
-                type = BuildVersionType.Unknown;
-
-            return type;
+                return BuildVersionType.V3;
+            if (buildVersion >= 40000 && buildVersion <= 49999 || buildVersion == 40 || buildVersion == 45)
+                return BuildVersionType.V4;
+            return BuildVersionType.Unknown;
         }
     }
 
