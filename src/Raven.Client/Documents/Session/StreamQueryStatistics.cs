2dd42b7871460afd7aa24b670b0e2538a3507607
aviv86 <aviv@ayende.com>
2017-03-15 18:05:39 +0200

    RavenDB-6013 : - remove headers from query streaming, write those values as additional json properties - add Stream() overload with an out parameter to retrieve the StreamQueryStatistics

---
+++ src/Raven.Client/Documents/Session/StreamQueryStatistics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Client.Documents.Session
+{
+    public class StreamQueryStatistics
+    {
+        public string IndexName { get; set; }
+        public bool IsStale { get; set; }
+        public DateTime IndexTimestamp { get; set; }
+        public int TotalResults { get; set; }
+        public long ResultEtag { get; set; }
+    }
+}
