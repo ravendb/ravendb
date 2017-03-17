a44178a0ffe710e629ac1d48b199c6f5708ea639
Arkadiusz Palinski <arek@ravendb.net>
2017-03-17 12:21:44 +0100

    RavenDB-5987 Transformation script is not required for Raven ETL. Just let it transfer an entire collection.

--- src/Raven.Server/Documents/ETL/Providers/Raven/RavenEtlDocumentTransformer.cs
+++ src/Raven.Server/Documents/ETL/Providers/Raven/RavenEtlDocumentTransformer.cs
@@ -8,11 +8,12 @@ namespace Raven.Server.Documents.ETL.Providers.Raven
     public class RavenEtlDocumentTransformer : EtlTransformer<RavenEtlItem, ICommandData>
     {
         private readonly List<ICommandData> _commands = new List<ICommandData>();
-        private readonly PatchRequest _transformationScript;
+        private readonly PatchRequest _transformationScript = null;
 
         public RavenEtlDocumentTransformer(DocumentDatabase database, DocumentsOperationContext context, RavenEtlConfiguration configuration) : base(database, context)
         {
-            _transformationScript = new PatchRequest { Script = configuration.Script };
+            if (string.IsNullOrEmpty(configuration.Script) == false)
+                _transformationScript = new PatchRequest { Script = configuration.Script };
         }
 
         public override IEnumerable<ICommandData> GetTransformedResults()
@@ -26,9 +27,14 @@ namespace Raven.Server.Documents.ETL.Providers.Raven
                 _commands.Add(new DeleteCommandData(item.DocumentKey, null));
             else
             {
-                var result = Apply(Context, item.Document, _transformationScript);
+                if (_transformationScript != null)
+                {
+                    var result = Apply(Context, item.Document, _transformationScript);
 
-                _commands.Add(new PutCommandDataWithBlittableJson(item.DocumentKey, null, result.ModifiedDocument));
+                    _commands.Add(new PutCommandDataWithBlittableJson(item.DocumentKey, null, result.ModifiedDocument));
+                }
+                else
+                    _commands.Add(new PutCommandDataWithBlittableJson(item.DocumentKey, null, item.Document.Data));
             }
         }
     }
