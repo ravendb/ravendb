47a6cf447beca2d829db9b84c41e07b1987e458f
Arkadiusz Palinski <arek@ravendb.net>
2017-03-17 12:21:44 +0100

    RavenDB-5987 Transformation script is not required for Raven ETL. Just let it transfer an entire collection.

--- test/SlowTests/Server/Documents/ETL/Raven/BasicRavenEtlTests.cs
+++ test/SlowTests/Server/Documents/ETL/Raven/BasicRavenEtlTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using FastTests;
+using Raven.Client.Documents;
 using Raven.Server.Documents.ETL;
 using Raven.Server.Documents.ETL.Providers.Raven;
 using Raven.Tests.Core.Utils.Entities;
@@ -11,7 +12,7 @@ namespace SlowTests.Server.Documents.ETL.Raven
     public class BasicRavenEtlTests : RavenTestBase
     {
         [Fact]
-        public void Basic_etl_test()
+        public void Simple_script()
         {
             using (var src = GetDocumentStore())
             using (var dest = GetDocumentStore())
@@ -25,25 +26,20 @@ namespace SlowTests.Server.Documents.ETL.Raven
                         mre.Set();
                 };
 
-                using (var session = src.OpenSession())
+                SetupEtl(src, new EtlConfiguration
                 {
-                    session.Store(new EtlConfiguration
+                    RavenTargets =
                     {
-                        RavenTargets =
+                        new RavenEtlConfiguration
                         {
-                            new RavenEtlConfiguration
-                            {
-                                Name = "basic test",
-                                Url = dest.Url,
-                                Database = dest.DefaultDatabase,
-                                Collection = "Users",
-                                Script = "this.Name = 'James Doe';"
-                            }
+                            Name = "basic test",
+                            Url = dest.Url,
+                            Database = dest.DefaultDatabase,
+                            Collection = "Users",
+                            Script = "this.Name = 'James Doe';"
                         }
-                    }, "Raven/ETL");
-
-                    session.SaveChanges();
-                }
+                    }
+                });
 
                 using (var session = src.OpenSession())
                 {
@@ -64,6 +60,67 @@ namespace SlowTests.Server.Documents.ETL.Raven
                     Assert.NotNull(user);
                     Assert.Equal("James Doe", user.Name);
                 }
+            }
+        }
+
+        [Fact]
+        public void No_script()
+        {
+            using (var src = GetDocumentStore())
+            using (var dest = GetDocumentStore())
+            {
+                var database = GetDatabase(src.DefaultDatabase).Result;
+
+                var mre = new ManualResetEventSlim();
+                database.EtlLoader.BatchCompleted += (n, s) =>
+                {
+                    if (s.LoadSuccesses == 1)
+                        mre.Set();
+                };
+
+                SetupEtl(src, new EtlConfiguration
+                {
+                    RavenTargets =
+                        {
+                            new RavenEtlConfiguration
+                            {
+                                Name = "basic test",
+                                Url = dest.Url,
+                                Database = dest.DefaultDatabase,
+                                Collection = "Users"
+                            }
+                        }
+                });
+
+                using (var session = src.OpenSession())
+                {
+                    session.Store(new User()
+                    {
+                        Name = "Joe Doe"
+                    });
+
+                    session.SaveChanges();
+                }
+
+                mre.Wait(TimeSpan.FromMinutes(1));
+
+                using (var session = dest.OpenSession())
+                {
+                    var user = session.Load<User>("users/1");
+
+                    Assert.NotNull(user);
+                    Assert.Equal("Joe Doe", user.Name);
+                }
+            }
+        }
+
+        private static void SetupEtl(DocumentStore src, EtlConfiguration configuration)
+        {
+            using (var session = src.OpenSession())
+            {
+                session.Store(configuration, "Raven/ETL");
+
+                session.SaveChanges();
             }
         }
     }
