d019bb20717647e73578ae9976ec7482df277398
grisha-kotler <grisha@ayende.com>
2017-03-21 22:52:32 +0200

    RavenDB-6604 - change type of license to enum

---
+++ src/Raven.Server/Commercial/LicenseType.cs
@@ -0,0 +1,17 @@
+namespace Raven.Server.Commercial
+{
+    public enum LicenseType
+    {
+        None,
+
+        Trial,
+
+        Standard,
+
+        Personal,
+
+        Floating,
+
+        Subscription
+    }
+}
