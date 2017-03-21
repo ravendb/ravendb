351c82b7145e332c0a6e1fe3f6f136c13c64e2dd
Pawel Pekrol <ppekrol@ravendb.net>
2017-03-21 16:16:50 +0100

    RavenDB-6596 - merged sync and async culture tests to avoid issues with changing culture for tests when running in parallel

--- test/Tests.Infrastructure/CultureTheoryAttribute.cs
+++ test/Tests.Infrastructure/CultureTheoryAttribute.cs
@@ -5,20 +5,20 @@ namespace Tests.Infrastructure
 {
     public class CultureTheoryAttribute : TheoryAttribute
     {
-        private readonly bool _skip;
+        private readonly bool _enable;
 
         public CultureTheoryAttribute()
         {
             var variable = Environment.GetEnvironmentVariable("RAVEN_ENABLE_CULTURE_TESTS");
-            if (variable == null || bool.TryParse(variable, out _skip) == false)
-                _skip = true;
+            if (variable == null || bool.TryParse(variable, out _enable) == false)
+                _enable = false;
         }
 
         public override string Skip
         {
             get
             {
-                if (_skip)
+                if (_enable == false)
                     return "Culture tests are disabled. Please set 'RAVEN_ENABLE_CULTURE_TESTS' environment variable to 'true' to enable them.";
 
                 return null;
