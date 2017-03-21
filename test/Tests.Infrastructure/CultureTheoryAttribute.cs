706a445333a69c227eea3a27ecefc6c252d006a7
Pawel Pekrol <ppekrol@ravendb.net>
2017-03-21 12:45:42 +0100

    RavenDB-6596 added tests that will run Fast and Slow tests using various cultures

---
+++ test/Tests.Infrastructure/CultureTheoryAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace Tests.Infrastructure
+{
+    public class CultureTheoryAttribute : TheoryAttribute
+    {
+        private readonly bool _skip;
+
+        public CultureTheoryAttribute()
+        {
+            var variable = Environment.GetEnvironmentVariable("RAVEN_ENABLE_CULTURE_TESTS");
+            if (variable == null || bool.TryParse(variable, out _skip) == false)
+                _skip = true;
+        }
+
+        public override string Skip
+        {
+            get
+            {
+                if (_skip)
+                    return "Culture tests are disabled. Please set 'RAVEN_ENABLE_CULTURE_TESTS' environment variable to 'true' to enable them.";
+
+                return null;
+            }
+        }
+    }
+}
