eaa72b558a6107743141afd1e0ee23f42867da6f
Arkadiusz Palinski <arek@ravendb.net>
2017-03-22 09:34:24 +0100

    RavendB-5987 Added passing test

---
+++ test/SlowTests/Server/Documents/ETL/MergedEnumeratorTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using FastTests;
+using Raven.Server.Documents.ETL;
+using Raven.Server.Documents.ETL.Providers.Raven;
+using Xunit;
+
+namespace SlowTests.Server.Documents.ETL
+{
+    public class MergedEnumeratorTests : NoDisposalNeeded
+    {
+        [Fact]
+        public void Returns_items_in_order()
+        {
+            using (var merged = new MergedEnumerator<Item>())
+            {
+                var items1 = new List<Item>
+                {
+                    new Item(1), new Item(3), new Item(4)
+                };
+
+                var items2 = new List<Item>
+                {
+                    new Item(2), new Item(5), new Item(6)
+                };
+
+                merged.AddEnumerator(items1.GetEnumerator());
+                merged.AddEnumerator(items2.GetEnumerator());
+
+                for (var i = 1; i <= 6; i++)
+                {
+                    Assert.True(merged.MoveNext());
+                    Assert.Equal(i, merged.Current.Etag);
+                }
+            }
+        }
+
+        private class Item : ExtractedItem
+        {
+            public Item(long etag)
+            {
+                Etag = etag;
+            }
+        }
+    }
+}
