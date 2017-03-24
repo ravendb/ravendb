02003510f39f552701abaecc3ebb8d784fe936f3
Marcin Lewandowski <marcin@ravendb.net>
2017-03-24 11:20:47 +0100

    RavenDB-5086 Create versioning view

---
+++ src/Raven.Studio/typescript/commands/database/documents/getDocumentAtRevisionCommand.ts
@@ -0,0 +1,25 @@
+import commandBase = require("commands/commandBase");
+import database = require("models/resources/database");
+import document = require("models/database/documents/document");
+import endpoints = require("endpoints");
+
+class getDocumentAtRevisionCommand extends commandBase {
+
+    constructor(private etag: number, private db: database) {
+        super();
+    }
+
+    execute(): JQueryPromise<document> {
+        const args = {
+            etag: this.etag
+        }
+
+        const url = endpoints.databases.versioning.revisions + this.urlEncodeArgs(args);
+
+        return this.query(url, null, this.db, (results: resultsDto<documentDto>) => {
+            return results.Results.map(x => new document(x))[0];
+        });
+    }
+ }
+
+export = getDocumentAtRevisionCommand;
