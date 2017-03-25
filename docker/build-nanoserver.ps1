567d1221df1d176534ae9163a4a3aff408832add
Jon Stelly <jonkymeric@gmail.com>
2017-03-25 15:45:30 -0500

    Docker based on alpha server downloads

--- docker/build-nanoserver.ps1
+++ docker/build-nanoserver.ps1
@@ -1,1 +1,13 @@
-docker build .\ravendb-nanoserver -t ravendb-nanoserver;
+param([switch]$Run, [switch]$Wait)
+
+docker build .\ravendb-nanoserver -t ravendb;
+
+if($LASTEXITCODE -eq 0 -and $Run) {
+    if($Wait) {
+        docker run -it --rm -p 8080:8080 -v db:c:/databases ravendb;
+    } else {
+        docker run -d -p 8080:8080 -v db:c:/databases ravendb;
+        start-sleep -Seconds 5;
+        docker ps -q | % { docker inspect  -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $_ } | % { "http://$($_):8080/" };
+    }
+}
