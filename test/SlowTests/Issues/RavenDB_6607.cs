75251f62004cb1dab57e8a925b594a71d42a650e
Pawel Pekrol <ppekrol@ravendb.net>
2017-03-28 13:37:21 +0200

    RavenDB-6607 when count operation is performed in auto-map-reduce then we need to validate if the field name is Count

---
+++ test/SlowTests/Issues/RavenDB_6607.cs
@@ -0,0 +1,24 @@
+﻿using FastTests;
+using Raven.Client.Exceptions;
+using Xunit;
+using Sparrow.Json;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_6607 : RavenTestBase
+    {
+        [Fact]
+        public void ShouldWork()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var commands = store.Commands())
+                {
+                    var e = Assert.Throws<RavenException>(() => commands.RawGetJson<BlittableJsonReaderObject>("/queries/dynamic/Products?mapReduce=PricePerUnit-Count-false&mapReduce=Category-None-true"));
+
+                    Assert.Contains("The only valid field name for 'Count' operation is 'Count' but was 'PricePerUnit'", e.Message);
+                }
+            }
+        }
+    }
+}
