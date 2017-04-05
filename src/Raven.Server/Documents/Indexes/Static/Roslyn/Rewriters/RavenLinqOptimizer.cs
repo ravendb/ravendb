74b9351b3017536ed9c1339900b41e852cf0c006
aviv86 <aviv@ayende.com>
2017-04-05 17:54:37 +0300

    RavenDB-5295 : SelectMany() support

--- src/Raven.Server/Documents/Indexes/Static/Roslyn/Rewriters/RavenLinqOptimizer.cs
+++ src/Raven.Server/Documents/Indexes/Static/Roslyn/Rewriters/RavenLinqOptimizer.cs
@@ -16,51 +16,174 @@ namespace Raven.Server.Documents.Indexes.Static.Roslyn.Rewriters
             if (memeberAccess == null || node.ArgumentList.Arguments.Count < 1 || !AllParentsAreMethods(node))
                 return base.VisitInvocationExpression(node);
 
-            var expressionSyntax = GetSimpleLambdaExpressionSyntax(node);
-            if (expressionSyntax == null || memeberAccess.Name.Identifier.ValueText != "Select")            
-                return base.VisitInvocationExpression(node);
-            
-            var invocExp = memeberAccess.Expression as InvocationExpressionSyntax;
-            MemberAccessExpressionSyntax innerMemberAccess = null;
-            if (invocExp != null)
-                innerMemberAccess = invocExp.Expression as MemberAccessExpressionSyntax;
+            switch (memeberAccess.Name.Identifier.ValueText)
+            {
+                case "SelectMany":
+                    var sourceExp = GetSimpleLambdaExpressionSyntax(node);
+                    var selectorExp = GetSelectorLambdaFromSelectManyExpression(node);
 
-            var name = innerMemberAccess?.Name.Identifier.ValueText;
-            if (name == "GroupBy" || name == "SelectMany")
-                return base.VisitInvocationExpression(node);
+                    if (sourceExp == null || selectorExp == null || selectorExp.ParameterList.Parameters.Count < 2)
+                        return base.VisitInvocationExpression(node);
 
-            if (innerMemberAccess == null || name != "Where")
-                return QueryExpression(
-                    FromClause(
-                        expressionSyntax.Parameter.Identifier,
-                        RavenLinqOptimizer.MaybeParenthesizedExpression((ExpressionSyntax)Visit(memeberAccess.Expression))
-                    ),
-                    QueryBody(SelectClause((ExpressionSyntax)Visit(expressionSyntax.Body))));
+                    sourceExp = sourceExp.WithParameter(selectorExp.ParameterList.Parameters[0])
+                        .WithBody(sourceExp.Body.ReplaceNodes(sourceExp.Body.DescendantNodes().OfType<IdentifierNameSyntax>(), (orig, _) =>
+                        {
+                            if (orig.Parent is MemberAccessExpressionSyntax access && orig == access.Name)
+                                return orig;
 
-            var identifierNameSyntax = innerMemberAccess.Expression as IdentifierNameSyntax;
-            if (identifierNameSyntax == null)
-                return base.VisitInvocationExpression(node);
+                            if (orig.Identifier.ValueText != sourceExp.Parameter.Identifier.ValueText)
+                                return orig;
+                            return orig.WithIdentifier(selectorExp.ParameterList.Parameters[0].Identifier);
+                        }));
 
-            var whereClause = GetSimpleLambdaExpressionSyntax(invocExp);
-                whereClause = whereClause.WithParameter(expressionSyntax.Parameter)
-                    .WithBody(whereClause.Body.ReplaceNodes(whereClause.Body.DescendantNodes().OfType<IdentifierNameSyntax>(), (orig, _) =>
+                    var sourceExpBody = GetBodyAndRemoveCastingIfNeeded(sourceExp);
+                    var selectManyInvocExp = memeberAccess.Expression as InvocationExpressionSyntax;
+                    MemberAccessExpressionSyntax selectManyInnerMemberAccess = null;
+                    if (selectManyInvocExp != null)
+                        selectManyInnerMemberAccess = selectManyInvocExp.Expression as MemberAccessExpressionSyntax;
+
+                    if (selectManyInnerMemberAccess == null)
                     {
-                        if (orig.Parent is MemberAccessExpressionSyntax access  && orig == access.Name)
-                            return orig;
+                        //handle innermost SelectMany()
+                        return QueryExpression(
+                                   FromClause(
+                                       Identifier(sourceExp.Parameter.Identifier.ValueText),
+                                       (ExpressionSyntax)Visit(memeberAccess.Expression)),
+                                   QueryBody(
+                                       SelectClause((ExpressionSyntax)selectorExp.Body))
+                                  .WithClauses(
+                                       SingletonList<QueryClauseSyntax>(
+                                        FromClause(
+                                            selectorExp.ParameterList.Parameters[1].Identifier,
+                                            (ExpressionSyntax)sourceExpBody
+                                        )
+                                )));
+                    }
 
-                        if (orig.Identifier.ValueText != whereClause.Parameter.Identifier.ValueText)
-                            return orig;
-                        return orig.WithIdentifier(expressionSyntax.Parameter.Identifier);
-                    }));
+                    if (selectManyInnerMemberAccess.Name.Identifier.ValueText != "SelectMany")
+                        return base.VisitInvocationExpression(node);
 
-            return QueryExpression(
-                FromClause(
-                    expressionSyntax.Parameter.Identifier,
-                    IdentifierName(identifierNameSyntax.Identifier.ValueText)
-                ),
-                QueryBody(SelectClause((ExpressionSyntax)Visit(expressionSyntax.Body)))
-                    .WithClauses(SingletonList<QueryClauseSyntax>(WhereClause((ExpressionSyntax)Visit(whereClause.Body)))));
-            
+                    //handle docs.SelectMany().SelectMany()...
+                    var innerQueryExp = (QueryExpressionSyntax)Visit(selectManyInvocExp);
+
+                    var clausesList = innerQueryExp.Body.Clauses.Add(LetClause(
+                                                                        Identifier(selectorExp.ParameterList.Parameters[0].Identifier.ValueText),
+                                                                        (ExpressionSyntax)Visit((innerQueryExp.Body.SelectOrGroup as SelectClauseSyntax)?.Expression)));
+                    clausesList = clausesList.Add(FromClause(
+                                                    Identifier(selectorExp.ParameterList.Parameters[1].Identifier.ValueText),
+                                                    (ExpressionSyntax)Visit(sourceExpBody)));
+
+                    //TODO : aviv - try to remove 'this0', 'this1', etc.. from sourceExp and selectorExp,
+                    //TODO          so that we can avoid adding 'let' clauses to QueryBody 
+
+                    return QueryExpression(
+                               innerQueryExp.FromClause,
+                               QueryBody(
+                                    SelectClause((ExpressionSyntax)Visit(selectorExp.Body)))
+                              .WithClauses(clausesList));
+
+
+                case "Select":
+                    var expressionSyntax = GetSimpleLambdaExpressionSyntax(node);
+                    if (expressionSyntax == null)
+                        return base.VisitInvocationExpression(node);
+
+                    var invocExp = memeberAccess.Expression as InvocationExpressionSyntax;
+                    MemberAccessExpressionSyntax innerMemberAccess = null;
+                    if (invocExp != null)
+                        innerMemberAccess = invocExp.Expression as MemberAccessExpressionSyntax;
+
+                    var name = innerMemberAccess?.Name.Identifier.ValueText;
+
+                    var innerInvocExp = innerMemberAccess?.Expression as InvocationExpressionSyntax;
+                    MemberAccessExpressionSyntax innerInnerMemberAccess = null;
+                    if (innerInvocExp != null)
+                        innerInnerMemberAccess = innerInvocExp.Expression as MemberAccessExpressionSyntax;
+
+                    if (innerMemberAccess == null || name == "Select" && innerInnerMemberAccess == null)
+                        //handle docs.Select()
+                        return QueryExpression(
+                            FromClause(
+                                expressionSyntax.Parameter.Identifier,
+                                RavenLinqOptimizer.MaybeParenthesizedExpression((ExpressionSyntax)Visit(memeberAccess.Expression))
+                            ),
+                            QueryBody(SelectClause((ExpressionSyntax)Visit(expressionSyntax.Body))));
+
+                    if (name != "Where")
+                        return base.VisitInvocationExpression(node);
+
+                    var whereClause = GetSimpleLambdaExpressionSyntax(invocExp);
+
+                    if (innerInnerMemberAccess != null && innerInnerMemberAccess.Name.Identifier.ValueText == "SelectMany")
+                    {
+                        //handle docs.SelectMany().Where().Select()
+                        var innerQueryExpSyntax = (QueryExpressionSyntax)Visit(innerInvocExp);
+                        var clauses = innerQueryExpSyntax.Body.Clauses.Add(LetClause(
+                                                                            Identifier(whereClause.Parameter.Identifier.ValueText),
+                                                                            (ExpressionSyntax)Visit((innerQueryExpSyntax.Body.SelectOrGroup as SelectClauseSyntax)?.Expression)));
+                        clauses = clauses.Add(WhereClause((ExpressionSyntax)Visit(whereClause.Body)));
+
+                        return QueryExpression(
+                            innerQueryExpSyntax.FromClause,
+                            QueryBody(
+                                    SelectClause((ExpressionSyntax)Visit(expressionSyntax.Body)))
+                                .WithClauses(clauses));
+                    }
+
+                    //handle docs.Where().Select()
+                    var identifierNameSyntax = innerMemberAccess.Expression as IdentifierNameSyntax;
+                    if (identifierNameSyntax == null)
+                        return base.VisitInvocationExpression(node);
+
+                    whereClause = whereClause.WithParameter(expressionSyntax.Parameter)
+                        .WithBody(whereClause.Body.ReplaceNodes(whereClause.Body.DescendantNodes().OfType<IdentifierNameSyntax>(), (orig, _) =>
+                        {
+                            if (orig.Parent is MemberAccessExpressionSyntax access && orig == access.Name)
+                                return orig;
+
+                            if (orig.Identifier.ValueText != whereClause.Parameter.Identifier.ValueText)
+                                return orig;
+                            return orig.WithIdentifier(expressionSyntax.Parameter.Identifier);
+                        }));
+
+                    return QueryExpression(
+                        FromClause(
+                            expressionSyntax.Parameter.Identifier,
+                            IdentifierName(identifierNameSyntax.Identifier.ValueText)
+                        ),
+                        QueryBody(SelectClause((ExpressionSyntax)Visit(expressionSyntax.Body)))
+                            .WithClauses(SingletonList<QueryClauseSyntax>(WhereClause((ExpressionSyntax)Visit(whereClause.Body)))));
+
+                default:
+                    return base.VisitInvocationExpression(node);
+
+            }
+
+        }
+
+        private static CSharpSyntaxNode GetBodyAndRemoveCastingIfNeeded(SimpleLambdaExpressionSyntax sourceExp)
+        {
+            var sourceExpBody = sourceExp.Body;
+            var castExpressionSyntax = sourceExpBody as CastExpressionSyntax;
+
+            if (castExpressionSyntax != null)
+                sourceExpBody = RavenLinqOptimizer.StripExpressionParenthesis(castExpressionSyntax.Expression);
+            return sourceExpBody;
+        }
+
+        private static ParenthesizedLambdaExpressionSyntax GetSelectorLambdaFromSelectManyExpression(InvocationExpressionSyntax node)
+        {
+            if (node.ArgumentList.Arguments.Count < 2)
+                return null;
+
+            ParenthesizedLambdaExpressionSyntax expressionSyntax;
+            var castExpressionSyntax = node.ArgumentList.Arguments[1].Expression as CastExpressionSyntax;
+
+            if (castExpressionSyntax != null)
+                expressionSyntax = RavenLinqOptimizer.StripExpressionParenthesis(castExpressionSyntax.Expression) as ParenthesizedLambdaExpressionSyntax;
+            else
+                expressionSyntax = node.ArgumentList.Arguments[1].Expression as ParenthesizedLambdaExpressionSyntax;
+            return expressionSyntax;
         }
 
         private static SimpleLambdaExpressionSyntax GetSimpleLambdaExpressionSyntax(InvocationExpressionSyntax node)
