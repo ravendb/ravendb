181016b679c6d547c7dbdfa4bdc595f447d6cca7
Pawel Pekrol <ppekrol@ravendb.net>
2017-04-09 01:57:44 +0200

    RavenDB-6734 - fixing paging when FacetQuery or MoreLikeThisQuery is used

--- src/Raven.Server/Documents/Queries/MoreLikeThis/MoreLikeThisQueryServerSide.cs
+++ src/Raven.Server/Documents/Queries/MoreLikeThis/MoreLikeThisQueryServerSide.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
-using Raven.Client.Documents.Conventions;
 using Raven.Client.Documents.Queries.MoreLikeThis;
 using Raven.Client.Documents.Transformers;
 using Sparrow.Json;
