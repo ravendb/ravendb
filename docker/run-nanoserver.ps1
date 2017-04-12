966ca88853d1e10c72018ab1ef17ca854610c75d
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-04-12 13:26:11 +0200

    RavenDB-4151 Windows container: provide a way to mount configuration file from host

--- docker/run-nanoserver.ps1
+++ docker/run-nanoserver.ps1
@@ -1,8 +1,11 @@
 param(
     [switch]$Detached,
     [switch]$AllowEverybodyToAccessTheServerAsAdmin,
+    [switch]$Debug,
     $BindPort = 8080,
-    $DbVolumeName = "ravendb")
+    $BindTcpPort = 38888,
+    $DbVolumeName = "ravendb",
+    $ConfigPath = "")
 
 if ([string]::IsNullOrEmpty($(docker volume ls | select-string $dbVolumeName))) {
     write-host "Create docker volume $dbVolumeName."
@@ -11,20 +14,36 @@ if ([string]::IsNullOrEmpty($(docker volume ls | select-string $dbVolumeName))) 
 
 $everybodyAdmin = $AllowEverybodyToAccessTheServerAsAdmin.ToString().ToLower()
 
+if ([string]::IsNullOrEmpty($ConfigPath) -eq $False) {
+    $fileEntry = (get-item $ConfigPath)
+    $configSwitch = "-v`"$($fileEntry.Directory):c:\raven-config`"".Trim()
+    $configFilenameSwitch = "-e`"CustomConfigFilename=$($fileEntry.Name)`""
+    write-host "Reading configuration from $ConfigPath"
+    write-host "NOTE: due to Docker Windows containers limitations entire directory holding that file is going to be visible to the container."
+}
+
+if ($Debug) {
+    $debugOpt = "powershell"
+}
+
 if ($Detached -eq $False)
 {
     docker run -it --rm `
         -p "$($BindPort):8080" `
+        -p "$($BindTcpPort):38888" `
         -v "$($dbVolumeName):c:/databases" `
-        -e "AllowEverybodyToAccessTheServerAsAdmin=$($everybodyAdmin)" `
-        ravendb/ravendb:windows-nanoserver-latest
+        $configSwitch `
+        $configFilenameSwitch `
+        ravendb/ravendb:windows-nanoserver-latest $debugOpt
 }
 else
 {
     docker run -d `
         -p "$($BindPort):8080" `
+        -p "$($BindTcpPort):38888" `
         -v "$($dbVolumeName):c:/databases" `
-        -e "AllowEverybodyToAccessTheServerAsAdmin=$($everybodyAdmin)" `
+        $configSwitch `
+        $configFilenameSwitch `
         ravendb/ravendb:windows-nanoserver-latest
 
     start-sleep -Seconds 5;
