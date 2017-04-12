eb84eee571f29a73205d18ecdedd527c2ed314f2
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-04-12 13:26:11 +0200

    RavenDB-4151 Windows container: provide a way to mount configuration file from host

---
+++ docker/ravendb-nanoserver/run-raven.ps1
@@ -0,0 +1,17 @@
+$CUSTOM_SETTINGS_PATH = "c:\raven-config\$env:CustomConfigFilename"
+
+cd c:/ravendb/Server
+
+if ([string]::IsNullOrEmpty($env:CustomConfigFilename) -eq $False) {
+    ./Raven.Server.exe `
+        /Raven/RunAsService=true `
+        /Raven/ServerUrl/Tcp=38888 `
+        /Raven/Config="$CUSTOM_SETTINGS_PATH" `
+        /Raven/DataDir=$($env:DataDir)
+}
+else {
+    ./Raven.Server.exe `
+        /Raven/RunAsService=true `
+        /Raven/ServerUrl/Tcp=38888 `
+        /Raven/AllowEverybodyToAccessTheServerAsAdmin=$($env:AllowEverybodyToAccessTheServerAsAdmin) ` /Raven/DataDir=$($env:DataDir)
+}
