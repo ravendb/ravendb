627dcfb3be83de3ee8b2859e75ceb3975ff5e53a
Pawel Pekrol <ppekrol@ravendb.net>
2017-04-18 11:57:28 +0200

    RavenDB-6747 decreased number of databases to 100

--- test/StressTests/Databases.cs
+++ test/StressTests/Databases.cs
@@ -2,16 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using FastTests;
 using Orders;
-using Org.BouncyCastle.Asn1.Mozilla;
 using Raven.Client.Documents;
-using Raven.Client.Documents.Indexes;
 using Raven.Client.Documents.Operations;
-using Raven.Client.Documents.Operations.Indexes;
 using Raven.Client.Documents.Session;
 using Raven.Client.Server;
 using Raven.Client.Server.Operations;
@@ -23,8 +19,8 @@ namespace StressTests
     public class Databases : RavenTestBase
     {
         [Theory]
-        [InlineData(150)]
-        public void CanHandleMultipledatabasesOnWrite(int numberOfDatabases)
+        [InlineData(100)]
+        public void CanHandleMultipleDatabasesOnWrite(int numberOfDatabases)
         {
             UseNewLocalServer();
 
@@ -34,7 +30,7 @@ namespace StressTests
                 CreateDatabases(numberOfDatabases, store);
                 Console.WriteLine("Done creating databases");
                 Product sampleProduct;
-                using (var session = store.OpenSession(dbNumToDbName[0]))
+                using (var session = store.OpenSession(DbNumToDbName[0]))
                 {
                     sampleProduct = session.Load<Product>("products/1");
                 }
@@ -42,63 +38,64 @@ namespace StressTests
                 var timeToSpin = TimeSpan.FromMinutes(5);
                 var minTimeBetweenIntervals = 500;
                 StartAsyncQueryTask(store, numberOfDatabases, timeToSpin, minTimeBetweenIntervals);
-                CreateLoadOnAllDatabases(numberOfDatabases, store, timeToSpin, minTimeBetweenIntervals, sampleProduct);                
+                CreateLoadOnAllDatabases(numberOfDatabases, store, timeToSpin, minTimeBetweenIntervals, sampleProduct);
                 Console.WriteLine("Done with laod on the system");
                 Console.WriteLine("Query statistics during the laod:");
                 Console.WriteLine("Reported query times");
-                Console.WriteLine($"Avarage={reportedAvarageQueryTime}ms Max={reportedMaxQueryTime}");
+                Console.WriteLine($"Avarage={_reportedAvarageQueryTime}ms Max={_reportedMaxQueryTime}");
                 Console.WriteLine("real query times (time on the client)");
-                Console.WriteLine($"Avarage={avarageQueryTime}ms Max={maxQueryTime}");
-                Console.WriteLine($"Got {totalNegativeDurations} negative query duration out of {totalQueryCount} total queries!");
+                Console.WriteLine($"Avarage={_avarageQueryTime}ms Max={_maxQueryTime}");
+                Console.WriteLine($"Got {_totalNegativeDurations} negative query duration out of {_totalQueryCount} total queries!");
             }
         }
-        
-        private static Dictionary<int,string> dbNumToDbName = new Dictionary<int, string>();
-        private void StartAsyncQueryTask(DocumentStore store, int numberOfDatabases, TimeSpan timeToSpin, int minTimeBetweenIntervals)
+
+        private static readonly Dictionary<int, string> DbNumToDbName = new Dictionary<int, string>();
+
+        private static void StartAsyncQueryTask(IDocumentStore store, int numberOfDatabases, TimeSpan timeToSpin, int minTimeBetweenIntervals)
         {
             var cts = new CancellationTokenSource();
             var rand = new Random(numberOfDatabases);
-            
+
             Task.Run(() =>
             {
                 var sw = new Stopwatch();
                 while (true)
-                {                    
+                {
                     var dbNum = rand.Next(numberOfDatabases);
 
                     try
                     {
-                        using (var session = store.OpenSession(dbNumToDbName[dbNum]))
+                        using (var session = store.OpenSession(DbNumToDbName[dbNum]))
                         {
                             QueryStatistics queryStat;
                             sw.Restart();
-                            session.Query<Product>().Statistics(out queryStat).Customize(x=>x.NoCaching()).Where(p => p.PricePerUnit > 1).Take(25).ToList();
+                            session.Query<Product>().Statistics(out queryStat).Customize(x => x.NoCaching()).Where(p => p.PricePerUnit > 1).Take(25).ToList();
                             var realQueryTime = sw.ElapsedMilliseconds;
                             //this is for no overflow
-                            var onePart = (double)1 / (totalQueryCount + 1);
-                            avarageQueryTime = onePart * totalQueryCount * avarageQueryTime + realQueryTime * onePart;
+                            var onePart = (double)1 / (_totalQueryCount + 1);
+                            _avarageQueryTime = onePart * _totalQueryCount * _avarageQueryTime + realQueryTime * onePart;
 
 
-                            if (maxQueryTime < realQueryTime)
+                            if (_maxQueryTime < realQueryTime)
                             {
-                                maxQueryTime = realQueryTime;
+                                _maxQueryTime = realQueryTime;
                             }
                             //Not sure if reported result is worth anything...
-                            onePart = (double)1 / (totalQueryCount - totalNegativeDurations + 1);
-                            if (queryStat.DurationMilliseconds >  0)
+                            onePart = (double)1 / (_totalQueryCount - _totalNegativeDurations + 1);
+                            if (queryStat.DurationMilliseconds > 0)
                             {
-                                reportedAvarageQueryTime = onePart * (totalQueryCount - totalNegativeDurations) * reportedAvarageQueryTime + queryStat.DurationMilliseconds * onePart;
-                                if (reportedMaxQueryTime < queryStat.DurationMilliseconds)
+                                _reportedAvarageQueryTime = onePart * (_totalQueryCount - _totalNegativeDurations) * _reportedAvarageQueryTime + queryStat.DurationMilliseconds * onePart;
+                                if (_reportedMaxQueryTime < queryStat.DurationMilliseconds)
                                 {
-                                    reportedMaxQueryTime = queryStat.DurationMilliseconds;
+                                    _reportedMaxQueryTime = queryStat.DurationMilliseconds;
                                 }
                             }
                             else
                             {
-                                totalNegativeDurations++;
+                                _totalNegativeDurations++;
                             }
-                            
-                            totalQueryCount++;
+
+                            _totalQueryCount++;
                         }
 
                     }
@@ -109,33 +106,34 @@ namespace StressTests
             }, cts.Token);
         }
 
-        private static double avarageQueryTime;
-        private static long totalQueryCount;
-        private static long maxQueryTime;
-        private static double reportedAvarageQueryTime;
-        private static long reportedMaxQueryTime;
-        private static int totalNegativeDurations;
-        private void CreateLoadOnAllDatabases(int numberOfDatabases, DocumentStore store,TimeSpan timeToSpin,int minTimeBetweenIntervals, Product sampleProduct)
+        private static double _avarageQueryTime;
+        private static long _totalQueryCount;
+        private static long _maxQueryTime;
+        private static double _reportedAvarageQueryTime;
+        private static long _reportedMaxQueryTime;
+        private static int _totalNegativeDurations;
+
+        private static void CreateLoadOnAllDatabases(int numberOfDatabases, IDocumentStore store, TimeSpan timeToSpin, int minTimeBetweenIntervals, Product sampleProduct)
         {
             var sw = Stopwatch.StartNew();
-            while (sw.Elapsed< timeToSpin)
+            while (sw.Elapsed < timeToSpin)
             {
                 var startTime = sw.ElapsedMilliseconds;
                 for (var i = 0; i < numberOfDatabases; i++)
                 {
-                    using (var session = store.OpenSession(dbNumToDbName[i]))
+                    using (var session = store.OpenSession(DbNumToDbName[i]))
                     {
-                        session.Store(sampleProduct,"products/");
+                        session.Store(sampleProduct, "products/");
                         session.SaveChanges();
                     }
                 }
                 var ranTime = sw.ElapsedMilliseconds - startTime;
-                if (ranTime< minTimeBetweenIntervals)
+                if (ranTime < minTimeBetweenIntervals)
                 {
                     Thread.Sleep(minTimeBetweenIntervals - (int)ranTime);
                 }
             }
-            
+
         }
 
         private static void CreateDatabases(int numberOfDatabases, Raven.Client.Documents.DocumentStore store)
@@ -145,7 +143,7 @@ namespace StressTests
             {
 
                 var dbname = $"Northwind{i}";
-                dbNumToDbName.Add(i,dbname);
+                DbNumToDbName.Add(i, dbname);
                 var doc = MultiDatabase.CreateDatabaseDocument(dbname);
                 store.Admin.Server.Send(new CreateDatabaseOperation(doc));
                 store.Admin.ForDatabase(dbname).Send(new CreateSampleDataOperation());
@@ -155,7 +153,7 @@ namespace StressTests
             var statOperation = new GetStatisticsOperation();
             while (true)
             {
-                var stat = store.Admin.ForDatabase(dbNumToDbName[numberOfDatabases - 1]).Send(statOperation);
+                var stat = store.Admin.ForDatabase(DbNumToDbName[numberOfDatabases - 1]).Send(statOperation);
                 if (stat.StaleIndexes.Any())
                     Thread.Sleep(500);
                 else
