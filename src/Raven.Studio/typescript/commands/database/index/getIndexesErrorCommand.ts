1aa303e6fb31956d1715fb783b8d8c461d62b581
Marcin Lewandowski <marcin@ravendb.net>
2017-04-20 19:22:47 +0200

    RavenDB-5101 Create index errors view

--- src/Raven.Studio/typescript/commands/database/index/getIndexesErrorCommand.ts
+++ src/Raven.Studio/typescript/commands/database/index/getIndexesErrorCommand.ts
@@ -10,7 +10,8 @@ class getIndexesErrorCommand extends commandBase {
 
     execute(): JQueryPromise<Raven.Client.Documents.Indexes.IndexErrors[]> {
         const url = endpoints.databases.index.indexesErrors;
-        return this.query<Raven.Client.Documents.Indexes.IndexErrors[]>(url, null, this.db);
+        return this.query<Raven.Client.Documents.Indexes.IndexErrors[]>(url, null, this.db)
+            .fail((result: JQueryXHR) => this.reportError("Failed to get index errors", result.responseText, result.statusText));
     }
 }
 
