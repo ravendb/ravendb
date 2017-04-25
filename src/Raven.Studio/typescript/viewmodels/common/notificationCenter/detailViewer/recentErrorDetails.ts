00ae0d7158672fcc2be33798f5e4e3909f1dae18
Marcin Lewandowski <marcin@ravendb.net>
2017-04-25 15:22:18 +0200

    RavenDB-6837 Provide details explanation for Non Durable File System & Database Recovery Error

---
+++ src/Raven.Studio/typescript/viewmodels/common/notificationCenter/detailViewer/recentErrorDetails.ts
@@ -0,0 +1,34 @@
+import app = require("durandal/app");
+import abstractNotification = require("common/notifications/models/abstractNotification");
+import notificationCenter = require("common/notifications/notificationCenter");
+import recentError = require("common/notifications/models/recentError");
+import dialogViewModelBase = require("viewmodels/dialogViewModelBase");
+
+class recentErrorDetails extends dialogViewModelBase {
+
+    protected readonly recentError: recentError;
+    protected readonly dismissFunction: () => void;
+
+    constructor(recentError: recentError, notificationCenter: notificationCenter) {
+        super();
+        this.bindToCurrentInstance("close", "dismiss");
+
+        this.recentError = recentError;
+        this.dismissFunction = () => notificationCenter.dismiss(recentError);
+    }
+
+    dismiss() {
+        this.dismissFunction();
+        this.close();
+    }
+
+    static supportsDetailsFor(notification: abstractNotification) {
+        return notification instanceof recentError;
+    }
+
+    static showDetailsFor(recentError: recentError, center: notificationCenter) {
+        return app.showBootstrapDialog(new recentErrorDetails(recentError, center));
+    }
+}
+
+export = recentErrorDetails;
