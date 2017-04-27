3a7af7e76252c281be5567fb5a03b7fa99587fec
aviviadi <avivi.adi.hr@gmail.com>
2017-04-27 18:44:13 +0300

    RavenDB-6855: TODO: protect from documents larger then 1GB

--- src/Sparrow/Json/UnmanagedWriteBuffer.cs
+++ src/Sparrow/Json/UnmanagedWriteBuffer.cs
@@ -260,6 +260,8 @@ namespace Sparrow.Json
 
         private void AllocateNextSegment(int required, bool allowGrowth)
         {
+            //TODO: protect from documents larger than 1GB
+
             // grow by doubling segment size until we get to 1 MB, then just use 1 MB segments
             // otherwise a document with 17 MB will waste 15 MB and require very big allocations
             var nextSegmentSize = Math.Max(Bits.NextPowerOf2(required), _current.Allocation.SizeInBytes * 2);
