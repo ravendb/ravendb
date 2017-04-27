d1d5e0cdc7887a2f06de350ca51e6cc349b280e2
IdanHaim <haimdude@gmail.com>
2017-04-27 14:50:58 +0300

    RavenDB-6704 Add Exists and ExistsAsync to be able to check if document exists without loading it

---
+++ test/FastTests/Client/ExistsAsync.cs
@@ -0,0 +1,30 @@
+﻿using Raven.Tests.Core.Utils.Entities;
+using Xunit;
+
+namespace FastTests.Client
+{
+    public class ExistsAsync : RavenTestBase
+    {
+        [Fact]
+        public async void CheckIfDocumentExists()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var asyncSession = store.OpenAsyncSession())
+                {
+                    await asyncSession.StoreAsync(new User { Name = "Idan" }, "users/1");
+                    await asyncSession.StoreAsync(new User { Name = "Shalom" }, "users/2");
+                    await asyncSession.SaveChangesAsync();
+                }
+
+                using (var asyncSession = store.OpenAsyncSession())
+                {
+                    Assert.True(await asyncSession.ExistsAsync("users/1"));
+                    Assert.False(await asyncSession.ExistsAsync("users/10"));
+                    await asyncSession.LoadAsync<User>("users/2");
+                    Assert.True(await asyncSession.ExistsAsync("users/2"));
+                }
+            }
+        }
+    }
+}
