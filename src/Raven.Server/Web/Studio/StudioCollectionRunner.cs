cab667cf7dc70b203c89af59131b04753fde91b4
Arkadiusz Palinski <arek@ravendb.net>
2017-05-04 12:17:23 +0200

    Revert "RavenDB-6835 Ensure that nobody modified docs from a collection we are trying to patch or delete in a write transaction of the tx merger since we collected docs in the read tx - I'm not sure if grabbing the docs in the read tx is a good approach here"
    This reverts commit 0fc30d6599881aaaf2eebe00520bd5e67d24b78d.

--- src/Raven.Server/Web/Studio/StudioCollectionRunner.cs
+++ src/Raven.Server/Web/Studio/StudioCollectionRunner.cs
@@ -28,20 +28,20 @@ namespace Raven.Server.Web.Studio
                 if (_excludeIds.Count == 0)
                 {
                     // all documents w/o exclusions -> filter system documents
-                    return ExecuteOperation(collectionName, options, Context, onProgress, (key, etag, context) =>
+                    return ExecuteOperation(collectionName, options, Context, onProgress, (key, context) =>
                     {
                         if (CollectionName.IsSystemDocument(key.Buffer, key.Length, out _) == false)
                         {
-                            Database.DocumentsStorage.Delete(context, key, etag);
+                            Database.DocumentsStorage.Delete(context, key, null);
                         }
                     }, token);
                 }
                 // all documents w/ exluclusions -> delete only not excluded and not system
-                return ExecuteOperation(collectionName, options, Context, onProgress, (key, etag, context) =>
+                return ExecuteOperation(collectionName, options, Context, onProgress, (key, context) =>
                 {
                     if (_excludeIds.Contains(key) == false && CollectionName.IsSystemDocument(key.Buffer, key.Length, out _) == false)
                     {
-                        Database.DocumentsStorage.Delete(context, key, etag);
+                        Database.DocumentsStorage.Delete(context, key, null);
                     }
                 }, token);
             }
@@ -50,11 +50,11 @@ namespace Raven.Server.Web.Studio
                 return base.ExecuteDelete(collectionName, options, onProgress, token);
 
             // specific collection w/ exclusions
-            return ExecuteOperation(collectionName, options, Context, onProgress, (key, etag, context) =>
+            return ExecuteOperation(collectionName, options, Context, onProgress, (key, context) =>
             {
                 if (_excludeIds.Contains(key) == false)
                 {
-                    Database.DocumentsStorage.Delete(context, key, etag);
+                    Database.DocumentsStorage.Delete(context, key, null);
                 }
             }, token);
         }
