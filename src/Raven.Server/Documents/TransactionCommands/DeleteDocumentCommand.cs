533543e08405c054c52b73b24308bef79cb2cdae
Arkadiusz Palinski <arek@ravendb.net>
2017-05-08 14:50:08 +0200

    RavenDB-6835 Collection runner and query runner also take advantage of split patching work to avoid preparing the patching engine during a write tx

---
+++ src/Raven.Server/Documents/TransactionCommands/DeleteDocumentCommand.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.ExceptionServices;
+using Raven.Server.ServerWide.Context;
+using Voron.Exceptions;
+
+namespace Raven.Server.Documents.TransactionCommands
+{
+    public class DeleteDocumentCommand : TransactionOperationsMerger.MergedTransactionCommand
+    {
+        private readonly string _id;
+        private readonly long? _expectedEtag;
+        private readonly DocumentDatabase _database;
+        private readonly bool _catchConcurrencyErrors;
+
+        public ExceptionDispatchInfo ExceptionDispatchInfo;
+
+        public DocumentsStorage.DeleteOperationResult? DeleteResult;
+
+        public DeleteDocumentCommand(string id, long? etag, DocumentDatabase database, bool catchConcurrencyErrors = false)
+        {
+            _id = id;
+            _expectedEtag = etag;
+            _database = database;
+            _catchConcurrencyErrors = catchConcurrencyErrors;
+        }
+
+        public override int Execute(DocumentsOperationContext context)
+        {
+            try
+            {
+                DeleteResult = _database.DocumentsStorage.Delete(context, _id, _expectedEtag);
+            }
+            catch (ConcurrencyException e)
+            {
+                if (_catchConcurrencyErrors == false)
+                    throw;
+
+                ExceptionDispatchInfo = ExceptionDispatchInfo.Capture(e);
+            }
+            return 1;
+        }
+    }
+}
