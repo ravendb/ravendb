792164e6dbcac008a192d732e7a3039360b70800
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-05-09 12:27:23 +0200

    RavenDB-6847 Create start.cmd/start.sh scripts and integrate them into publish process

---
+++ src/Raven.Server/Utils/BrowserHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Raven.Server.Config;
+using Sparrow.Platform;
+using Voron.Platform.Posix;
+
+namespace Raven.Server.Utils
+{
+    public static class BrowserHelper
+    {
+        public static void OpenStudioInBrowser(RavenServer server)
+        {
+            try
+            {
+                var url = server.WebUrls.First().Replace("0.0.0.0", "localhost");
+                if (PlatformDetails.RunningOnPosix == false)
+                {
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}"));
+                }
+                else
+                {
+                    Process.Start("xdg-open", url);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not start browser: " + e.Message);
+            }
+        }
+    }
+}
