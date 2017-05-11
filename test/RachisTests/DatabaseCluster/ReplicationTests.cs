d23e49e5f7500a05e20d23e09f307e9b28819af7
karmeli87 <karmeli87@gmail.com>
2017-05-11 15:29:44 +0300

    RavenDB-6970 Adding the global change vector when putting new documen

--- test/RachisTests/DatabaseCluster/ReplicationTests.cs
+++ test/RachisTests/DatabaseCluster/ReplicationTests.cs
@@ -46,6 +46,64 @@ namespace RachisTests.DatabaseCluster
                     u => u.Name.Equals("Karmel"),
                     TimeSpan.FromSeconds(clusterSize + 5)));
             }
-        }      
+        }
+
+
+        [Fact]
+        public async Task AddGlobalChangeVectorToNewDocument()
+        {
+            var clusterSize = 3;
+            var databaseName = "ReplicationTestDB";
+            var leader = await CreateRaftClusterAndGetLeader(clusterSize, true, 0);
+            var doc = MultiDatabase.CreateDatabaseDocument(databaseName);
+            using (var store = new DocumentStore()
+            {
+                Url = leader.WebUrls[0],
+                DefaultDatabase = databaseName
+            }.Initialize())
+            {
+                var databaseResult = await store.Admin.Server.SendAsync(new CreateDatabaseOperation(doc, clusterSize));
+                var topology = databaseResult.Topology;
+                Assert.Equal(clusterSize, topology.AllReplicationNodes().Count());
+                foreach (var server in Servers)
+                {
+                    await server.ServerStore.Cluster.WaitForIndexNotification(databaseResult.ETag ?? -1);
+                }
+                foreach (var server in Servers)
+                {
+                    await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(databaseName);
+                }
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User {Name = "Karmel"}, "users/1");
+                    await session.SaveChangesAsync();
+                }
+                Assert.True(await WaitForDocumentInClusterAsync<User>(
+                    topology,
+                    "users/1",
+                    u => u.Name.Equals("Karmel"),
+                    TimeSpan.FromSeconds(clusterSize + 5)));
+            }
+
+            using (var store = new DocumentStore()
+            {
+                Url = Servers[1].WebUrls[0],
+                DefaultDatabase = databaseName
+            }.Initialize())
+            {
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User {Name = "Indych"}, "users/2");
+                    await session.SaveChangesAsync();
+                }
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    var user = await session.LoadAsync<User>("users/2");
+                    Assert.Equal(2, session.Advanced.GetChangeVectorFor(user).Length);
+                }
+            }
+        }
+
     }
 }
