753315568dfdb037362c3ade27f83658c6bd6e10
Pawel Pekrol <ppekrol@ravendb.net>
2017-05-12 21:25:15 +0200

    RavenDB-7028 delete collection should only remove documents from given collection

---
+++ test/SlowTests/Issues/RavenDB_7028.cs
@@ -0,0 +1,44 @@
+﻿using FastTests;
+using Orders;
+using Raven.Client.Documents.Operations;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_7028 : RavenTestBase
+    {
+        [Fact]
+        public void DeleteByCollectionShouldOnlyDeleteAllDocsFromThatCollection()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Company(), "companies/1");
+                    session.Store(new Address(), "addresses/1");
+
+                    session.SaveChanges();
+                }
+
+                var stats = store.Admin.Send(new GetStatisticsOperation());
+                Assert.Equal(2, stats.CountOfDocuments);
+
+                var operation = store.Operations.Send(new DeleteCollectionOperation("Companies"));
+
+                operation.WaitForCompletion();
+
+                stats = store.Admin.Send(new GetStatisticsOperation());
+                Assert.Equal(1, stats.CountOfDocuments);
+
+                using (var session = store.OpenSession())
+                {
+                    var company = session.Load<Company>("companies/1");
+                    Assert.Null(company);
+
+                    var address = session.Load<Address>("addresses/1");
+                    Assert.NotNull(address);
+                }
+            }
+        }
+    }
+}
