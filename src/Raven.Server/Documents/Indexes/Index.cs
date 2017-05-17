2ee9d8e664ddc704113db93b90c252c68c28a67f
Arkadiusz Palinski <arek@ravendb.net>
2017-05-17 11:12:28 +0200

    RavenDB-7043 Consider an index staleness during its failure rate validation

--- src/Raven.Server/Documents/Indexes/Index.cs
+++ src/Raven.Server/Documents/Indexes/Index.cs
@@ -146,6 +146,7 @@ namespace Raven.Server.Documents.Indexes
         private readonly ReaderWriterLockSlim _currentlyRunningQueriesLock = new ReaderWriterLockSlim();
         private volatile bool _priorityChanged;
         private volatile bool _hadRealIndexingWorkToDo;
+        private Func<bool> _indexValidationStalenessCheck = () => true;
 
         private readonly WarnIndexOutputsPerDocument _indexOutputsPerDocumentWarning = new WarnIndexOutputsPerDocument
         {
@@ -379,6 +380,19 @@ namespace Raven.Server.Documents.Indexes
                     DocumentDatabase.DocumentTombstoneCleaner.Subscribe(this);
 
                     DocumentDatabase.Changes.OnIndexChange += HandleIndexChange;
+
+                    _indexValidationStalenessCheck = () =>
+                    {
+                        if (_cts.IsCancellationRequested)
+                            return true;
+
+                        DocumentsOperationContext documentsContext;
+                        using (DocumentDatabase.DocumentsStorage.ContextPool.AllocateOperationContext(out documentsContext))
+                        using (documentsContext.OpenReadTransaction())
+                        {
+                            return IsStale(documentsContext);
+                        }
+                    };
 
                     InitializeInternal();
 
@@ -1013,10 +1027,10 @@ namespace Raven.Server.Documents.Indexes
                 State = IndexState.Error; // just in case it didn't took from the SetState call
             }
         }
-
+        
         private void HandleIndexFailureInformation(IndexFailureInformation failureInformation)
         {
-            if (failureInformation.IsInvalidIndex == false)
+            if (failureInformation.IsInvalidIndex(_indexValidationStalenessCheck) == false)
                 return;
 
             var message = failureInformation.GetErrorMessage();
