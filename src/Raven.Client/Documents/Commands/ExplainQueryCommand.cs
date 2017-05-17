4d416e2b64eab90f6fd21f43fb815bec6a86451c
Fitzchak Yitzchaki <fitzchak@ayende.com>
2017-05-17 12:06:36 +0300

    RavenDB-7082 Server should always return object response. No arrays.

--- src/Raven.Client/Documents/Commands/ExplainQueryCommand.cs
+++ src/Raven.Client/Documents/Commands/ExplainQueryCommand.cs
@@ -30,8 +30,6 @@ namespace Raven.Client.Documents.Commands
             _context = context ?? throw new ArgumentNullException(nameof(context));
             _indexName = indexName ?? throw new ArgumentNullException(nameof(indexName));
             _indexQuery = indexQuery;
-
-            ResponseType = RavenCommandResponseType.Array;
         }
 
         public override HttpRequestMessage CreateRequest(ServerNode node, out string url)
@@ -60,17 +58,12 @@ namespace Raven.Client.Documents.Commands
             }
 
             var indexQueryUrl = $"{_indexQuery.GetIndexQueryUrl(_indexName, "queries", includeQuery: method == HttpMethod.Get)}&debug=explain";
-
             url = $"{node.Url}/databases/{node.Database}/" + indexQueryUrl;
+
             return request;
         }
 
         public override void SetResponse(BlittableJsonReaderObject response, bool fromCache)
-        {
-            ThrowInvalidResponse();
-        }
-
-        public override void SetResponse(BlittableJsonReaderArray response, bool fromCache)
         {
             if (response == null)
             {
@@ -78,10 +71,16 @@ namespace Raven.Client.Documents.Commands
                 return;
             }
 
-            var results = new ExplainQueryResult[response.Length];
-            for (var i = 0; i < response.Length; i++)
+            if (response.TryGet("Results", out BlittableJsonReaderArray array) == false)
             {
-                var result = (BlittableJsonReaderObject)response[i];
+                ThrowInvalidResponse();
+                return; // never hit
+            }
+
+            var results = new ExplainQueryResult[array.Length];
+            for (var i = 0; i < array.Length; i++)
+            {
+                var result = (BlittableJsonReaderObject)array[i];
                 results[i] = (ExplainQueryResult)_conventions.DeserializeEntityFromBlittable(typeof(ExplainQueryResult), result);
             }
 
