2f10e4ceddae9c7c83bc3cf982801561d51073a8
Fitzchak Yitzchaki <fitzchak@ayende.com>
2017-05-17 13:59:53 +0300

    RavenDB-7067 Better attachment headers: Attachment-Hash instead of Content-Hash

--- src/Raven.Server/Documents/Handlers/AttachmentHandler.cs
+++ src/Raven.Server/Documents/Handlers/AttachmentHandler.cs
@@ -99,7 +99,7 @@ namespace Raven.Server.Documents.Handlers
                     if (HttpContext.Response.Headers.ContainsKey("Content-Type"))
                         HttpContext.Response.Headers.Remove("Content-Type");
                 }
-                HttpContext.Response.Headers["Content-Hash"] = attachment.Base64Hash.ToString();
+                HttpContext.Response.Headers["Attachment-Hash"] = attachment.Base64Hash.ToString();
                 HttpContext.Response.Headers["Attachment-Size"] = attachment.Stream.Length.ToString();
                 HttpContext.Response.Headers[Constants.Headers.Etag] = $"\"{attachment.Etag}\"";
 
