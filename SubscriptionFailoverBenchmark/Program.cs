901f67ae4f4c41730d6b62905ff50959f4f8622c
maximburyak <maxim@ayende.com>
2017-05-18 19:51:58 +0300

    RavenDB-6526: Fixing test

--- SubscriptionFailoverBenchmark/Program.cs
+++ SubscriptionFailoverBenchmark/Program.cs
@@ -7,22 +7,103 @@ using System.Threading.Tasks;
 using Tests.Infrastructure;
 using FastTests.Server.Documents.Notifications;
 using System.Collections.Generic;
+using Voron.Util;
 
 namespace SubscriptionFailoverBenchmark
 {
-    public class SubscriptionFailoverTest: ClusterTestBase
+    public class SubscriptionFailoverBenchmark: ClusterTestBase
     {
+
+        public async Task RunTestSimple()
+        {
+            using (var store = GetDocumentStore())
+            {
+
+                var subscriptionTask = RunsSubscriptionSimple(store);
+                var documentsTask = GenerateDocuments(store);
+                await Task.WhenAll(subscriptionTask, documentsTask);
+            }
+        }
+
+        private async Task GenerateDocuments(DocumentStore store)
+        {
+            var j = 0;
+            
+            do
+            {
+                try
+                {
+                    using (var session = store.OpenAsyncSession())
+                    {
+                        for (var i=0 ; i < 1000 ; j++,i++)
+                        {
+                            await session.StoreAsync(new User
+                            {
+                                Name = "User"+j
+                            });
+                        }
+
+                        await session.SaveChangesAsync();
+                    }
+                }
+                catch (Exception)
+                {
+                    await Task.Delay(1000);
+
+                }
+            } while (j < DocsAmount);
+        
+        }
+
+        public async Task RunsSubscriptionSimple(DocumentStore store)
+        {
+            var subscriptionId = await store.AsyncSubscriptions.CreateAsync<User>(
+                new SubscriptionCreationParams<User>());
+
+            var subscripiton = store.AsyncSubscriptions.Open<User>(new SubscriptionConnectionOptions(subscriptionId)
+            {
+                MaxDocsPerBatch = 1024
+            });
+            var counter = 0;
+            var tcs = new TaskCompletionSource<bool>();
+
+            
+
+            subscripiton.Subscribe(x =>
+            {
+                Interlocked.Increment(ref counter);
+            });
+
+            subscripiton.AfterAcknowledgment += () =>
+            {
+                Console.WriteLine($"{ subscriptionId}: {counter}");
+                if (counter == DocsAmount)
+                    tcs.SetResult(true);
+            };
+
+            await subscripiton.StartAsync();
+
+            await tcs.Task;
+        }
+
+        const int DocsAmount = 1000000;
+        const int nodesAmount = 3;
+        const int docsCreationTasksAmount = 3;
+        const int subscriptionCreationTasksAmount = 5;
         public async Task RunTest()
         {
-            var leader = await CreateRaftClusterAndGetLeader(5,false);
-
             var defaultDatabase = "StressTest";
-            const int nodesAmount = 5;
+
+            
+            
+            var leader = await CreateRaftClusterAndGetLeader(nodesAmount, false);
+
             await CreateDatabaseInCluster(defaultDatabase, nodesAmount, leader.WebUrls[0]).ConfigureAwait(false);
 
-            Task[] docsCreationTasks = new Task[3];
-            Task[] subscriptionsTasks = new Task[5];
-            List<(string, DateTime)>[] subscriptionSummaries = new List<(string, DateTime)>[5];
+            var docsCreationTasks = new Task[docsCreationTasksAmount];
+            
+            var subscriptionsTasks = new Task[subscriptionCreationTasksAmount];
+            var subscriptionSummaries = new List<(string, DateTime)>[subscriptionCreationTasksAmount];
 
             var cts = new CancellationTokenSource();
 
@@ -31,27 +112,27 @@ namespace SubscriptionFailoverBenchmark
                 var curI = i;
                 docsCreationTasks[i] = Task.Run(async () =>
                 {
-                    await GenerateDocumentsForNode(defaultDatabase, docsCreationTasks, curI);
+                    await GenerateDocumentsForNode(defaultDatabase, docsCreationTasks, Servers[curI].WebUrls[0], curI * DocsAmount / nodesAmount);
                 });
             }
 
-            var shutdownTask = Task.Run(async () =>
+            var shutdownTask = Task.Run((Func<Task>)(async () =>
             {
-                await ChaosMonkey(docsCreationTasks, cts);
-            });
+                await ChaosMonkey(cts);
+            }));
 
-            for (var i=0; i< 5; i++)
+            for (var i=0; i< subscriptionsTasks.Length; i++)
             {
                 var curI = i;
                 var log = new List<(string, DateTime)>();
                 subscriptionSummaries[i] = log;
                 subscriptionsTasks[i] = Task.Run(async () =>
                 {
-                    await RunSubscription(defaultDatabase, curI, log);
+                    await RunSubscription(defaultDatabase, log);
                 });
             }
 
-            Task.WaitAll(subscriptionsTasks);
+            Task.WaitAll(subscriptionsTasks.Concat(shutdownTask));
 
             foreach (var summary in subscriptionSummaries)
             {
@@ -66,18 +147,21 @@ namespace SubscriptionFailoverBenchmark
             }
         }
 
-        private async Task RunSubscription(string defaultDatabase, int i, List<(string, DateTime)> log)
+        private async Task RunSubscription(string defaultDatabase,  List<(string, DateTime)> log)
         {
             using (var store = new DocumentStore
             {
-                Url = Servers[i].WebUrls[0],
+                Url = Servers[0].WebUrls[0],
                 DefaultDatabase = defaultDatabase
             }.Initialize())
             {
                 var subscriptionId = await store.AsyncSubscriptions.CreateAsync<User>(
                     new SubscriptionCreationParams<User>());
 
-                var subscripiton = store.AsyncSubscriptions.Open<User>(new SubscriptionConnectionOptions(subscriptionId));
+                var subscripiton = store.AsyncSubscriptions.Open<User>(new SubscriptionConnectionOptions(subscriptionId)
+                {
+                    MaxDocsPerBatch = 1024
+                });
                 var counter = 0;
                 var tcs = new TaskCompletionSource<bool>();
 
@@ -90,7 +174,8 @@ namespace SubscriptionFailoverBenchmark
 
                 subscripiton.AfterAcknowledgment += () =>
                 {
-                    if (counter == 1 * 1000 * 1000)
+                    Console.WriteLine($"{ subscriptionId}: {counter}");
+                    if (counter == 1 * DocsAmount)
                         tcs.SetResult(true);
                 };
 
@@ -110,63 +195,64 @@ namespace SubscriptionFailoverBenchmark
             }            
         }
 
-        private async Task GenerateDocumentsForNode(string defaultDatabase, Task[] docsCreationTasks, int i)
+        private async Task GenerateDocumentsForNode(string defaultDatabase, Task[] docsCreationTasks, string url, long rangeStart)
         {
             using (var store = new DocumentStore
             {
-                Url = Servers[i].WebUrls[0],
+                Url = url,
                 DefaultDatabase = defaultDatabase
             }.Initialize())
             {
-                using (var bi = store.BulkInsert())
+                var j = 0;
+                do
                 {
-                    for (var j = 0; j < 1 * 1000 * 1000 / docsCreationTasks.Length; j++)
+                    try
                     {
-                        var shouldRetry = false;
-                        do
+                        using (var session = store.OpenAsyncSession())
                         {
-                            try
+                            for (var k = 0; k < 1000; j++, k++)
                             {
-                                await bi.StoreAsync(new User
+                                await session.StoreAsync(new User
                                 {
-                                    Name = "John" + j
-                                });
-                                shouldRetry = false;
+                                    Name = "User" + j
+                                },$"Users/{rangeStart+j}");
                             }
-                            catch (Exception)
-                            {
-                                await Task.Delay(1000);
-                                shouldRetry = true;
-                            }
-                        } while (shouldRetry);
+
+                            await session.SaveChangesAsync();
+                        }
                     }
-                }
+                    catch (Exception)
+                    {
+                        await Task.Delay(1000);
+
+                    }
+                } while (j < DocsAmount / nodesAmount + 3);
             }
         }
 
-        private async Task ChaosMonkey(Task[] docsCreationTasks, CancellationTokenSource cts)
+        private async Task ChaosMonkey( CancellationTokenSource cts)
         {
             var curIndex = 0;
-            while (cts.IsCancellationRequested == false)
-            {
-                var curServer = Servers[curIndex];
-                var curConfig = curServer.Configuration;
+            //while (cts.IsCancellationRequested == false)
+            //{
+            //    var curServer = Servers[curIndex];
+            //    var curConfig = curServer.Configuration;
 
-                curServer.Dispose();
+            //    curServer.Dispose();
 
-                await Task.Delay(10000);
+            //    await Task.Delay(30000);
 
-                Servers[curIndex] = new Raven.Server.RavenServer(curConfig);
+            //    Servers[curIndex] = new Raven.Server.RavenServer(curConfig);
 
-                curIndex = (curIndex + 1) % docsCreationTasks.Length;
-            }
+            //    curIndex = (curIndex + 1) % nodesAmount;
+            //}
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            new SubscriptionFailoverTest().RunTest().Wait();
+            new SubscriptionFailoverBenchmark().RunTest().Wait();
         }
     }
 }
