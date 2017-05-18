7b590076668597e3af6fc689011f59b68930a932
Fitzchak Yitzchaki <fitzchak@ayende.com>
2017-05-18 14:06:27 +0300

    RavenDB-7067 SetResponse should be called just for ResponseType.Object and never for ResponseType.Empty which should not be cached also as those are commands like DisableDatabase and etc...

--- src/Raven.Client/Documents/Commands/KillOperationCommand.cs
+++ src/Raven.Client/Documents/Commands/KillOperationCommand.cs
@@ -1,17 +1,15 @@
 ﻿using System.Net.Http;
 using Raven.Client.Http;
-using Sparrow.Json;
 
 namespace Raven.Client.Documents.Commands
 {
-    class KillOperationCommand : RavenCommand<bool>
+    public class KillOperationCommand : RavenCommand
     {
         private readonly long _id;
 
         public KillOperationCommand(long id)
         {
             _id = id;
-            ResponseType = RavenCommandResponseType.Empty;
         }
 
         public override HttpRequestMessage CreateRequest(ServerNode node, out string url)
@@ -24,12 +22,6 @@ namespace Raven.Client.Documents.Commands
             };
         }
 
-        public override void SetResponse(BlittableJsonReaderObject response, bool fromCache)
-        {
-            Result = true;
-        }
-
         public override bool IsReadRequest => false;
-    
     }
 }
