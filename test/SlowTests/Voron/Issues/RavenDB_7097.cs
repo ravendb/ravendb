1d628b5f4dda202ad399fb8103c04440ccfa59d7
Arkadiusz Palinski <arek@ravendb.net>
2017-05-19 13:46:07 +0200

    RavenDB-7097 Delete pending-recycle files on db dispose

---
+++ test/SlowTests/Voron/Issues/RavenDB_7097.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using SlowTests.Utils;
+using Voron;
+using Xunit;
+
+namespace SlowTests.Voron.Issues
+{
+    public class RavenDB_7097 : StorageTest
+    {
+        protected override void Configure(StorageEnvironmentOptions options)
+        {
+            options.ManualFlushing = true;
+        }
+
+        [Fact]
+        public void Pending_recycle_files_are_deleted_on_dispose()
+        {
+            RequireFileBasedPager();
+
+            var r = new Random(1);
+
+            var bytes = new byte[1024];
+
+            for (int i = 0; i < 1000; i++)
+            {
+                using (var tx = Env.WriteTransaction())
+                {
+                    r.NextBytes(bytes);
+
+                    tx.CreateTree("items").Add($"item/{i}", new MemoryStream(bytes));
+
+                    tx.Commit();
+                }
+            }
+
+            Env.FlushLogToDataFile();
+            Env.ForceSyncDataFile();
+
+            SpinWait.SpinUntil(() => new DirectoryInfo(DataDir).GetFiles($"{StorageEnvironmentOptions.PendingRecycleFileNamePrefix}*").Length == 6,
+                TimeSpan.FromSeconds(30));
+
+            Env.Dispose();
+
+            var journalsForReuse = new DirectoryInfo(DataDir).GetFiles($"{StorageEnvironmentOptions.PendingRecycleFileNamePrefix}*");
+
+            Assert.Equal(0, journalsForReuse.Length);
+        }
+    }
+}
