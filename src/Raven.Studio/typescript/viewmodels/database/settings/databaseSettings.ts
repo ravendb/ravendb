3df1428358dfaf52e3e915dd847e5235e18569d5
Marcin Lewandowski <marcin@ravendb.net>
2017-05-19 09:59:28 +0200

    RavenDB-5065 Create database settings view

--- src/Raven.Studio/typescript/viewmodels/database/settings/databaseSettings.ts
+++ src/Raven.Studio/typescript/viewmodels/database/settings/databaseSettings.ts
@@ -15,14 +15,9 @@ import eventsCollector = require("common/eventsCollector");
 class databaseSettings extends viewModelBase {
     document = ko.observable<document>();
     documentText = ko.observable<string>().extend({ required: true });
-    metadataText = ko.observable<string>("{}").extend({ required: true });
-    text: KnockoutComputed<string>;
     docEditor: AceAjax.Editor;
     securedSettings: string;
     updatedDto: documentDto;
-    isSaveEnabled: KnockoutComputed<Boolean>;
-    isEditingEnabled = ko.observable<boolean>(false);
-    isEditingMetadata = ko.observable<boolean>(false);
     leavePageDeffered: JQueryPromise<any>;
     isForbidden = ko.observable<boolean>(false);
 
@@ -39,24 +34,6 @@ class databaseSettings extends viewModelBase {
                 var docText = this.stringify(doc.toDto());
                 this.documentText(docText);
             }
-        });
-
-        this.isEditingMetadata.subscribe(()=> {
-            if (this.docEditor) {
-                var text = this.isEditingMetadata() ? this.metadataText() : this.documentText();
-                this.docEditor.getSession().setValue(text);
-            }
-        });
-
-        this.text = ko.computed({
-            read: () => {
-                return this.isEditingMetadata() ? this.metadataText() : this.documentText();
-            },
-            write: (text: string) => {
-                var currentObservable = this.isEditingMetadata() ? this.metadataText : this.documentText;
-                currentObservable(text);
-            },
-            owner: this
         });
     }
 
@@ -83,8 +60,6 @@ class databaseSettings extends viewModelBase {
     activate(args: any) {
         super.activate(args);
         this.updateHelpLink('3QMLGH');
-        this.dirtyFlag = new ko.DirtyFlag([this.documentText, this.metadataText], false, jsonUtil.newLineNormalizingHashFunction);
-        this.isSaveEnabled = ko.computed(() => this.dirtyFlag().isDirty());
     }
 
     compositionComplete() {
@@ -103,95 +78,11 @@ class databaseSettings extends viewModelBase {
         $("#dbDocEditor").off('DynamicHeightSet');
     }
 
-    editDatabaseSettings() {
-        eventsCollector.default.reportEvent("database-settings", "edit");
-
-        var editDbConfirm = new viewSystemDatabaseConfirm("Meddling with the database settings document could cause irreversible damage!");
-        editDbConfirm
-            .viewTask
-            .done(() => {
-                this.isEditingEnabled(true);
-            });
-        app.showBootstrapDialog(editDbConfirm);
-    }
 
     refreshFromServer() {
         eventsCollector.default.reportEvent("database-settings", "refresh");
 
-        var canContinue = this.canContinueIfNotDirty('Unsaved Data', 'You have unsaved data. Are you sure you want to refresh the data from the server?');
-        canContinue.done(() => {
-            this.fetchDatabaseSettings(this.activeDatabase(), true)
-                .done(() => {
-                    this.metadataText("{}");
-                    this.isEditingEnabled(false);
-                    this.activateDoc();
-                    this.dirtyFlag().reset(); //Resync Changes
-                });
-        });
-    }
-
-    formatDocument() {
-        eventsCollector.default.reportEvent("database-settings", "format");
-
-        var text = this.isEditingMetadata() ? this.metadataText() : this.documentText();
-        var observableToUpdate = this.isEditingMetadata() ? this.metadataText : this.documentText;
-        try {
-            var tempDoc = JSON.parse(text);
-            var formatted = this.stringify(tempDoc);
-            observableToUpdate(formatted);
-        }
-        catch (e) { }
-    }
-
-    activateMeta() {
-        this.isEditingMetadata(true);
-    }
-
-    activateDoc() {
-        this.isEditingMetadata(false);
-    }
-
-    saveChanges() {
-        var updatedDto: documentDto;
-        var meta: any;
-
-        eventsCollector.default.reportEvent("database-settings", "save");
-
-        var editDbConfirm = new viewSystemDatabaseConfirm("Meddling with the database settings document could cause irreversible damage!");
-        editDbConfirm
-            .viewTask
-            .done(() => {
-                try {
-                    updatedDto = JSON.parse(this.documentText());
-                    meta = JSON.parse(this.metadataText());
-                    var newDoc = new document(updatedDto);
-                    newDoc.__metadata.etag = this.document().__metadata.etag;
-                    var saveCommand = new saveDatabaseSettingsCommand(this.activeDatabase(), newDoc); 
-                    var saveTask = saveCommand.execute();
-                    saveTask.done((saveResult: databaseDocumentSaveDto) => {
-                        (<any>this.document()).__metadata.etag = saveResult.ETag;
-                        this.metadataText("{}");
-                        this.isEditingEnabled(false);
-                        this.formatDocument();
-                        this.dirtyFlag().reset(); //Resync Changes
-                    });
-                }
-                catch (e) {
-                    var message = "";
-                    if (updatedDto == undefined) {
-                        message = "The data isn't a legal JSON expression!";
-                        this.isEditingMetadata(false);
-                    }
-                    else if (meta == undefined) {
-                        message = "The metadata isn't a legal JSON expression!";
-                        this.isEditingMetadata(true);
-                    }
-                    this.docEditor.focus();
-                    messagePublisher.reportError(message);
-                }
-
-            });
-        app.showBootstrapDialog(editDbConfirm);
+        this.fetchDatabaseSettings(this.activeDatabase(), true);
     }
 
     private fetchDatabaseSettings(db: database, reportFetchProgress: boolean = false): JQueryPromise<any> {
@@ -205,7 +96,7 @@ class databaseSettings extends viewModelBase {
     }
 
     private stringify(obj: any) {
-        var prettifySpacing = 4;
+        const prettifySpacing = 4;
         return JSON.stringify(obj, null, prettifySpacing);
     }
 }
