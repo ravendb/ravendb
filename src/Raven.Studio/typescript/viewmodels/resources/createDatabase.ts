67da83ee9d13103781bed9a634b92985a5c64cf5
Marcin Lewandowski <marcin@ravendb.net>
2017-05-19 13:55:48 +0200

    RavenDB-7076 On create db screen, can only switch between the modes using the gears

--- src/Raven.Studio/typescript/viewmodels/resources/createDatabase.ts
+++ src/Raven.Studio/typescript/viewmodels/resources/createDatabase.ts
@@ -90,11 +90,16 @@ class createDatabase extends dialogViewModelBase {
         // hide advanced if respononding bundle was unchecked
         this.databaseModel.configurationSections.forEach(section => {
             section.enabled.subscribe(enabled => {
-                if (!enabled && this.currentAdvancedSection() === section.name) {
-                    this.currentAdvancedSection(createDatabase.defaultSection);
+                if (section.alwaysEnabled && !enabled) {
+                    // can't disable section which is always enabled
+                    section.enabled(true);
+                    return;
                 }
-                if (enabled) {
+
+                if (section.alwaysEnabled || enabled) {
                     this.currentAdvancedSection(section.name);
+                } else if (!enabled && this.currentAdvancedSection() === section.name) {
+                    this.currentAdvancedSection(createDatabase.defaultSection);
                 }
             });
         });
