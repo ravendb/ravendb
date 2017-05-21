b8d72e56fdc4d6f25ba160d435e874a68c77ac51
aviviadi <avivi.adi.hr@gmail.com>
2017-05-21 15:55:25 +0300

    RavenDB-7077: Detailed exception for libsodium

---
+++ src/Sparrow/Platform/IncorrectDllExecption.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sparrow.Platform
+{
+    public class IncorrectDllExecption : Exception
+    {
+        public IncorrectDllExecption()
+        {
+        }
+
+        public IncorrectDllExecption(string message) : base(message)
+        {
+        }
+
+        public IncorrectDllExecption(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
