e1825eabcdaf22e8b13c073c20e184d49e3632c4
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-05-22 12:54:04 +0200

    RavenDB-5643 update dotnet-dev dependency for install_prerequisites.sh

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -24,7 +24,7 @@ if [ -z "$DOTNET_CMD" ] ; then
     sudo echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ $UBUNTU_CODENAME main" > /etc/apt/sources.list.d/dotnetdev.list
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
     sudo apt-get update
-    sudo apt-get install -y dotnet-dev-1.0.3
+    sudo apt-get install -y dotnet-dev-1.0.4
 else
     echo ".NETcore is installed."
 fi
