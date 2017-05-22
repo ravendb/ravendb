b1cec684c75c6462bdfc389b25193171011f600c
Ihor Chyhrynets <i.chyhrynets@mobilunity.com>
2017-05-22 13:28:54 +0300

    Fixing prerequisites for ubuntu

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -24,7 +24,7 @@ if [ -z "$DOTNET_CMD" ] ; then
     sudo echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ $UBUNTU_CODENAME main" > /etc/apt/sources.list.d/dotnetdev.list
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
     sudo apt-get update
-    sudo apt-get install -y dotnet-dev-1.0.0-preview2.1-003177
+    sudo apt-get install -y dotnet-dev-1.0.3
 else
     echo ".NETcore is installed."
 fi
