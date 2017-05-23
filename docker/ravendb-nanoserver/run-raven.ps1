90a8dcf619b46b93a7021443999c3050e8d9ada9
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-05-23 21:45:07 +0200

    RavenDB-7113 Adjust cli parameters in docker run scripts

--- docker/ravendb-nanoserver/run-raven.ps1
+++ docker/ravendb-nanoserver/run-raven.ps1
@@ -4,19 +4,19 @@ cd c:/ravendb/Server
 
 if ([string]::IsNullOrEmpty($env:CustomConfigFilename) -eq $False) {
     ./Raven.Server.exe `
-        /Raven/RunAsService=true `
         /Raven/ServerUrl=http://0.0.0.0:8080 `
         /Raven/ServerUrl/Tcp=tcp://0.0.0.0:38888 `
-        /Raven/Config="$CUSTOM_SETTINGS_PATH" `
         /Raven/DataDir=$($env:DataDir) `
+        --config-path "$CUSTOM_SETTINGS_PATH" `
+        --run-as-service `
         --print-id
 }
 else {
     ./Raven.Server.exe `
-        /Raven/RunAsService=true `
         /Raven/ServerUrl=http://0.0.0.0:8080 `
         /Raven/ServerUrl/Tcp=tcp://0.0.0.0:38888 `
         /Raven/AllowAnonymousUserToAccessTheServer=$($env:AllowAnonymousUserToAccessTheServer) `
         /Raven/DataDir=$($env:DataDir) `
+        --run-as-service `
         --print-id
 }
--- docker/ravendb-ubuntu1604/run-raven.sh
+++ docker/ravendb-ubuntu1604/run-raven.sh
@@ -7,18 +7,18 @@ cd /opt/RavenDB/Server
 if [ ! -f "$CUSTOM_SETTINGS_PATH" ]
 then
     ./Raven.Server \
-        /Raven/RunAsService=true \
         /Raven/ServerUrl=http://0.0.0.0:8080 \
         /Raven/ServerUrl/Tcp=tcp://0.0.0.0:38888 \
         /Raven/AllowAnonymousUserToAccessTheServer=${AllowAnonymousUserToAccessTheServer} \
         /Raven/DataDir=${DataDir} \
+        --run-as-service \
         --print-id
 else
     ./Raven.Server \
-        /Raven/RunAsService=true \
         /Raven/ServerUrl=http://0.0.0.0:8080 \
         /Raven/ServerUrl/Tcp=tcp://0.0.0.0:38888 \
-        /Raven/Config=${CUSTOM_SETTINGS_PATH} \
         /Raven/DataDir=${DataDir} \
+        --config-path "${CUSTOM_SETTINGS_PATH}" \
+        --run-as-service \
         --print-id
 fi
