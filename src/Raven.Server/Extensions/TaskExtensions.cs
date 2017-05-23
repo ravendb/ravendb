c7699e2f897983f1159a5120be272ee125eea37e
Ayende Rahien <Ayende@ayende.com>
2017-05-23 20:55:59 +0300

    RavenDB-7066 - Avoid wrapping exception in AggregationException

--- src/Raven.Server/Extensions/TaskExtensions.cs
+++ src/Raven.Server/Extensions/TaskExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Sparrow.Utils;
 
 namespace Raven.Server.Extensions
 {
@@ -13,12 +14,13 @@ namespace Raven.Server.Extensions
         public static async Task<T> ThrowOnTimeout<T>(this Task<T> task, int timeoutMs = 10_000)
         {
             await InternalThrowOnTimeout(task, timeoutMs);
-            return task.Result;
+            return await task;
         }
 
         private static async Task InternalThrowOnTimeout(Task task, int timeoutMs)
         {
-            var delay = Task.Delay(timeoutMs);
+            if (timeoutMs <= 0) throw new ArgumentOutOfRangeException(nameof(timeoutMs));
+            var delay = TimeoutManager.WaitFor((uint)timeoutMs);
             if (await Task.WhenAny(task, delay) == delay)
             {
                 throw new TimeoutException("Timeout exception while waiting for execution of the raft command to be completed.");
