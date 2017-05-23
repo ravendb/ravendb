a04de7ec19b784f005736fabf09f90cceaa68db4
karmeli87 <karmeli87@gmail.com>
2017-05-23 10:06:26 +0300

    RavenDB-7066 Make sure that requests on Raft operations have timeout when invoke from handlers

---
+++ src/Raven.Server/Extensions/TaskExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Raven.Server.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static async Task ThrowOnTimeout(this Task task, int timeoutMs = 10_000)
+        {
+            await InternalThrowOnTimeout(task, timeoutMs);
+        }
+
+        public static async Task<T> ThrowOnTimeout<T>(this Task<T> task, int timeoutMs = 10_000)
+        {
+            await InternalThrowOnTimeout(task, timeoutMs);
+            return task.Result;
+        }
+
+        private static async Task InternalThrowOnTimeout(Task task, int timeoutMs)
+        {
+            var delay = Task.Delay(timeoutMs);
+            if (await Task.WhenAny(task, delay) == delay)
+            {
+                throw new TimeoutException("Timeout exception while waiting for execution of the raft command to be completed.");
+            }
+        }
+    }
+}
