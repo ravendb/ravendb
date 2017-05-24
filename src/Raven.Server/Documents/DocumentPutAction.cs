181254328be934beb1d65a6fc7928d2180c93344
Ayende Rahien <ayende@ayende.com>
2017-05-24 16:30:10 +0300

    RavenDB-7172 - temporarily reverting identities back to old behavior until all tests are passing

--- src/Raven.Server/Documents/DocumentPutAction.cs
+++ src/Raven.Server/Documents/DocumentPutAction.cs
@@ -246,10 +246,10 @@ namespace Raven.Server.Documents
                 var lastChar = id[id.Length - 1];
                 if (lastChar == '/')
                 {
-                   throw new NotSupportedException("PUT of documents with 'identity' Id generation using a server-side API is not supported. When using server-side API, use '|' separator instead.");
+                    knownNewId = true;
+                    id = _documentsStorage.Identities.GetNextIdentityValueWithoutOverwritingOnExistingDocuments(id, table, context, out _);
                 }
-
-                if (lastChar == '|')
+                else if (lastChar == '|')
                 {
                     knownNewId = true;
                     id = _documentsStorage.Identities.AppendNumericValueToId(id, newEtag);
