dcb98c962f27f4544b9e7e51b7ad8a094de69312
Ayende Rahien <ayende@ayende.com>
2017-05-25 10:13:01 +0300

    Making sure that we are waiting on a task error using timeout checks happened

--- src/Raven.Server/Extensions/TaskExtensions.cs
+++ src/Raven.Server/Extensions/TaskExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Sparrow.Utils;
 
@@ -9,6 +10,7 @@ namespace Raven.Server.Extensions
         public static async Task ThrowOnTimeout(this Task task, int timeoutMs = 10_000)
         {
             await InternalThrowOnTimeout(task, timeoutMs);
+            await task;
         }
 
         public static async Task<T> ThrowOnTimeout<T>(this Task<T> task, int timeoutMs = 10_000)
@@ -20,7 +22,7 @@ namespace Raven.Server.Extensions
         private static async Task InternalThrowOnTimeout(Task task, int timeoutMs)
         {
             if (timeoutMs <= 0) throw new ArgumentOutOfRangeException(nameof(timeoutMs));
-            var delay = TimeoutManager.WaitFor((uint)timeoutMs);
+            var delay = TimeoutManager.WaitFor(timeoutMs);
             if (await Task.WhenAny(task, delay) == delay)
             {
                 throw new TimeoutException("Timeout exception while waiting for execution of the raft command to be completed.");
