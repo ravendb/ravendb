f2d9eaabcd198d152a006410ad0a7d40f258b5c7
aviviadi <avivi.adi.hr@gmail.com>
2017-05-29 16:17:30 +0300

    RavenDB-7263: dos2linux moved to tests + minor

---
+++ test/FastTests/Utils/Dos2LinuxString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sparrow.Utils
+{
+    public static class Dos2Linux
+    {
+        public static string String(string str)
+        {
+            if (Platform.PlatformDetails.RunningOnPosix == true)
+                return str.Replace("\r\n", "\n");
+            return str;
+        }
+    }
+}
