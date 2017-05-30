8d080c64482b0820990bfc18a270b4aafa28c4c4
aviviadi <avivi.adi.hr@gmail.com>
2017-05-31 00:05:14 +0300

    RavenDB-7263: Change cs file encoding from iso-8859-14 to utf8 (using iconv)

--- test/SlowTests/MailingList/CustomAnalyzerStartsWithFailure.cs
+++ test/SlowTests/MailingList/CustomAnalyzerStartsWithFailure.cs
@@ -19,10 +19,10 @@ namespace SlowTests.MailingList
 
             using (var session = store.OpenSession())
             {
-                session.Store(new Customer() { Name = "RogÈrio" });
+                session.Store(new Customer() { Name = "Rog√©rio" });
                 session.Store(new Customer() { Name = "Rogerio" });
                 session.Store(new Customer() { Name = "Paulo Rogerio" });
-                session.Store(new Customer() { Name = "Paulo RogÈrio" });
+                session.Store(new Customer() { Name = "Paulo Rog√©rio" });
                 session.SaveChanges();
             }
         }
@@ -37,10 +37,10 @@ namespace SlowTests.MailingList
                 using (IDocumentSession session = store.OpenSession())
                 {
                     // Test 1
-                    // Using "== RogÈrio" works fine
+                    // Using "== Rog√©rio" works fine
                     var results1 = session.Query<Customer, CustomerByName>()
                         .Customize(x => x.WaitForNonStaleResults())
-                        .Where(x => x.Name == "RogÈrio");
+                        .Where(x => x.Name == "Rog√©rio");
 
                     Assert.Equal(results1.Count<Customer>(), 4);
                 }
@@ -56,10 +56,10 @@ namespace SlowTests.MailingList
                 using (IDocumentSession session = store.OpenSession())
                 {
                     // Test 2
-                    // Using ".StartsWith("RogÈrio")" is expected to bring same result from test1, but fails
+                    // Using ".StartsWith("Rog√©rio")" is expected to bring same result from test1, but fails
                     var results2 = session.Query<Customer, CustomerByName>()
                         .Customize(x => x.WaitForNonStaleResults())
-                        .Where(x => x.Name.StartsWith("RogÈrio"));
+                        .Where(x => x.Name.StartsWith("Rog√©rio"));
 
                     Assert.Equal(results2.Count<Customer>(), 4);
                 }
