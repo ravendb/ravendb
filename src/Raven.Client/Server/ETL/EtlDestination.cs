ae69ad80d4fa688ab8067c22d87c9c5914a6bb76
Arkadiusz Palinski <arek@ravendb.net>
2017-05-31 13:16:35 +0200

    RavenDB-7276 Implementing IDatabaseTask for ETL configurations. Moving back the name property of a ETL destination and using it to calculate the task id. Manually retrieving db and server names from the SQL connection string to avoid dependency to SQL libs in raven client.

--- src/Raven.Client/Server/ETL/EtlDestination.cs
+++ src/Raven.Client/Server/ETL/EtlDestination.cs
@@ -5,5 +5,12 @@ namespace Raven.Client.Server.ETL
     public abstract class EtlDestination
     {
         public abstract bool Validate(ref List<string> errors);
+
+        public abstract string Name { get; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
