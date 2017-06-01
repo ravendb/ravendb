c1b735a3b9d304ed9a10bca5835e8d1e9ff0655d
Pawel Pekrol <ppekrol@ravendb.net>
2017-06-01 10:46:48 +0200

    RavenDB-7273 need to ensure that we are subscribed before storing a document

--- test/SlowTests/Issues/RavenDB_6285.cs
+++ test/SlowTests/Issues/RavenDB_6285.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using FastTests;
 using FastTests.Server.Documents.Notifications;
 using Raven.Tests.Core.Utils.Entities;
@@ -10,15 +11,16 @@ namespace SlowTests.Issues
     public class RavenDB_6285 : RavenTestBase
     {
         [Fact]
-        public void BasicChangesApi()
+        public async Task BasicChangesApi()
         {
             using (var store = GetDocumentStore())
             {
                 var mre = new ManualResetEventSlim();
 
-                store.Changes()
-                    .ForAllDocuments()
-                    .Subscribe(x => mre.Set());
+                var changes = await store.Changes().EnsureConnectedNow();
+                var observable = changes.ForAllDocuments();
+                observable.Subscribe(x => mre.Set());
+                await observable.EnsureSubscribedNow();
 
                 using (var session = store.OpenSession())
                 {
