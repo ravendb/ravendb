d30a4ac6310b9626e6673eda6f69dc8a93420a69
IdanHaim <haimdude@gmail.com>
2017-06-04 11:07:04 +0300

    RavenDB-7239 Add multiple Urls tests

--- test/SlowTests/Bugs/ConnectionStringParsing.cs
+++ test/SlowTests/Bugs/ConnectionStringParsing.cs
@@ -39,5 +39,26 @@ namespace SlowTests.Bugs
             var dbParser = ConnectionStringParser<RavenConnectionStringOptions>.FromConnectionString("memory=true");
             Assert.Throws<ArgumentException>(() => dbParser.Parse());
         }
+
+        [Fact]
+        public void EnsureWellFormedConnectionStrings_ParsingMultiUrls_Successful()
+        {
+            var connectionStringParser = ConnectionStringParser<RavenConnectionStringOptions>.FromConnectionString("Urls=http://localhost:10301,http://localhost:10302,http://localhost:10303;");
+            connectionStringParser.Parse();
+
+            var options = connectionStringParser.ConnectionStringOptions;
+            Assert.True(options.Urls.Count == 3);
+
+        }
+
+        [Fact]
+        public void EnsureWellFormedConnectionStrings_ParsingMultiUrlsWithSpaces_Successful()
+        {
+            var connectionStringParser = ConnectionStringParser<RavenConnectionStringOptions>.FromConnectionString("Urls=http://localhost:10301 , http://localhost:10302 , http://localhost:10303;");
+            connectionStringParser.Parse();
+
+            var options = connectionStringParser.ConnectionStringOptions;
+            Assert.True(options.Urls.Count == 3);
+        }
     }
 }
