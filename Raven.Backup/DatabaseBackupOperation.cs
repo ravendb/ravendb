2026512a8b65eb1f4872a3265d1fae769aeb4bce
Grisha Kotler <grisha@ayende.com>
2017-06-08 20:55:34 +0300

    RavenDB-7411 - Raven.Backup.exe fails on esent when using a UNC path

--- Raven.Backup/DatabaseBackupOperation.cs
+++ Raven.Backup/DatabaseBackupOperation.cs
@@ -52,7 +52,7 @@ namespace Raven.Backup
 
             var backupRequest = new
             {
-                BackupLocation = parameters.BackupPath.Replace("\\", "\\\\"),
+               BackupLocation = parameters.BackupPath
             };
 
            
