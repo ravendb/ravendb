5505ff7e3c7b16577250f3262989d7e7d79c0177
Fitzchak Yitzchaki <fitzchak@ayende.com>
2017-06-12 13:20:27 +0300

    RavenDB-7440 Throw when using the same stream to create more than one attachment

--- src/Raven.Client/Documents/Commands/Batches/PutAttachmentCommandHelper.cs
+++ src/Raven.Client/Documents/Commands/Batches/PutAttachmentCommandHelper.cs
@@ -5,20 +5,25 @@ namespace Raven.Client.Documents.Commands.Batches
 {
     public static class PutAttachmentCommandHelper
     {
-        public static void ThrowPositionNotZero(long streamPosition)
+        public static void ThrowStreamAlready()
+        {
+            throw new InvalidOperationException("It is forbidden to re-use the same stream for more than one attachment. Use a unique stream per put attachment command.");
+        }
+
+        private static void ThrowPositionNotZero(long streamPosition)
         {
             throw new InvalidOperationException($"Cannot put an attachment with a stream that have position which isn't zero (The position is: {streamPosition}) " +
                                                 "since this is most of the time not intended and it is a common mistake.");
         }
 
-        public static void ThrowNotSeekableStream()
+        private static void ThrowNotSeekableStream()
         {
             throw new InvalidOperationException(
                 "Cannot put an attachment with a not seekable stream. " +
                 "We require a seekable stream because we might failover to a different node if the current one is unavailable during the operation.");
         }
 
-        public static void ThrowNotReadableStream()
+        private static void ThrowNotReadableStream()
         {
             throw new InvalidOperationException(
                 "Cannot put an attachment with a not readable stream. " +
@@ -28,6 +33,16 @@ namespace Raven.Client.Documents.Commands.Batches
         public static void PrepareStream(Stream stream)
         {
             stream.Position = 0;
+        }
+
+        public static void ValidateStream(Stream stream)
+        {
+            if (stream.CanRead == false)
+                ThrowNotReadableStream();
+            if (stream.CanSeek == false)
+                ThrowNotSeekableStream();
+            if (stream.Position != 0)
+                ThrowPositionNotZero(stream.Position);
         }
     }
 }
