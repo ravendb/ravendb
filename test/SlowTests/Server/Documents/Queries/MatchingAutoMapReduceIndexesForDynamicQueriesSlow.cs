1033349be0d6d1c30bf6d610493941415efd8887
Pawel Pekrol <ppekrol@ravendb.net>
2017-06-19 14:07:43 +0200

    RavenDB-7322 wrote a test that checks if our test class is not inheriting from another tests class

--- test/SlowTests/Server/Documents/Queries/MatchingAutoMapReduceIndexesForDynamicQueriesSlow.cs
+++ test/SlowTests/Server/Documents/Queries/MatchingAutoMapReduceIndexesForDynamicQueriesSlow.cs
@@ -1,7 +1,7 @@
-﻿using FastTests.Server.Documents.Queries.Dynamic.MapReduce;
-using Raven.Client;
+﻿using FastTests;
 using Raven.Client.Documents.Indexes;
 using Raven.Client.Util;
+using Raven.Server.Documents;
 using Raven.Server.Documents.Indexes;
 using Raven.Server.Documents.Indexes.MapReduce.Auto;
 using Raven.Server.Documents.Queries;
@@ -10,8 +10,17 @@ using Xunit;
 
 namespace SlowTests.Server.Documents.Queries
 {
-    public class MatchingAutoMapRediceIndexesForDynamicQueriesSlow : MatchingAutoMapReduceIndexesForDynamicQueries
+    public class MatchingAutoMapReduceIndexesForDynamicQueriesSlow : RavenLowLevelTestBase
     {
+        private readonly DocumentDatabase _documentDatabase;
+        protected readonly DynamicQueryToIndexMatcher _sut;
+
+        public MatchingAutoMapReduceIndexesForDynamicQueriesSlow()
+        {
+            _documentDatabase = CreateDocumentDatabase();
+
+            _sut = new DynamicQueryToIndexMatcher(_documentDatabase.IndexStore);
+        }
 
         [Fact]
         public void Failure_if_matching_index_has_lot_of_errors()
@@ -65,6 +74,28 @@ namespace SlowTests.Server.Documents.Queries
             var result = _sut.Match(dynamicQuery);
 
             Assert.Equal(DynamicQueryMatchType.Failure, result.MatchType);
+        }
+
+        protected void add_index(IndexDefinitionBase definition)
+        {
+            AsyncHelpers.RunSync(() => _documentDatabase.IndexStore.CreateIndex(definition));
+        }
+
+        protected Index get_index(string name)
+        {
+            return _documentDatabase.IndexStore.GetIndex(name);
+        }
+
+        public override void Dispose()
+        {
+            try
+            {
+                _documentDatabase.Dispose();
+            }
+            finally
+            {
+                base.Dispose();
+            }
         }
     }
 }
