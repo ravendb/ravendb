0013d884274ecdaf398c3fc5b920401167c74216
Marcin Lewandowski <marcin@ravendb.net>
2017-06-21 11:14:06 +0200

    RavenDB-5036 Moved conflicts page under documents menu

--- src/Raven.Studio/typescript/common/shell/menu/items/documents.ts
+++ src/Raven.Studio/typescript/common/shell/menu/items/documents.ts
@@ -44,6 +44,14 @@ function getDocumentsMenuItem(appUrls: computedAppUrls) {
             dynamicHash: appUrls.patch
         }),
         new leafMenuItem({
+            title: "Conflicts",
+            nav: true,
+            route: "databases/documents/conflicts",
+            moduleId: "viewmodels/database/conflicts/conflicts",
+            css: 'icon-plus',
+            dynamicHash: appUrls.conflicts
+        }),
+        new leafMenuItem({
             route: "databases/edit",
             title: "Edit Document",
             moduleId: "viewmodels/database/documents/editDocument",
