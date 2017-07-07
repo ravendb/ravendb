b59e1e4b2b729c58d8b8192fb32978fdb1d884fa
Marcin Lewandowski <marcin@ravendb.net>
2017-07-07 14:48:27 +0200

    RavenDB-6912 Overwrite existing attachment

--- src/Raven.Studio/typescript/viewmodels/database/documents/editDocumentUploader.ts
+++ src/Raven.Studio/typescript/viewmodels/database/documents/editDocumentUploader.ts
@@ -3,6 +3,7 @@
 import uploadAttachmentCommand = require("commands/database/documents/attachments/uploadAttachmentCommand");
 import document = require("models/database/documents/document");
 import database = require("models/resources/database");
+import viewHelpers = require("common/helpers/view/viewHelpers")
 
 //TODO: for now we simple impl, which doesn't support drag-and-drop or simultaneous uploads
 class editDocumentUploader {
@@ -25,16 +26,41 @@ class editDocumentUploader {
 
     fileSelected(fileName: string) {
         if (fileName) {
-            this.spinners.upload(true);
             const selector = $(editDocumentUploader.filePickerSelector)[0] as HTMLInputElement;
             const file = selector.files[0];
-            new uploadAttachmentCommand(file, this.document().getId(), this.db())
-                .execute()
-                .done(() => {
-                    this.afterUpload();
-                })
-                .always(() => this.spinners.upload(false));
+
+            const nameAlreadyExists = this.findAttachment(file.name);
+            if (nameAlreadyExists) {
+                viewHelpers.confirmationMessage(`Attachment '${file.name}' already exists.`, "Do you want to overwrite existing attachment?", ["No", "Yes, overwrite"])
+                    .done(result => {
+                        if (result.can) {
+                            this.uploadInternal(file);
+                        }
+                    });
+            } else {
+                this.uploadInternal(file);
+            }
         }
+    }
+
+    private findAttachment(name: string) {
+        const attachments = this.document().__metadata.attachments();
+        if (!attachments) {
+            return null;
+        }
+
+        return attachments.find(x => x.Name === name);
+    }
+
+    private uploadInternal(file: File) {
+        this.spinners.upload(true);
+
+        new uploadAttachmentCommand(file, this.document().getId(), this.db())
+            .execute()
+            .done(() => {
+                this.afterUpload();
+            })
+            .always(() => this.spinners.upload(false));
     }
 }
 
