2ae68070c7a38bbb42b5157e3ef561c579ce40c8
iftahbe <iftahbe@gmail.com>
2017-07-10 09:56:49 +0300

    RavenDB-7293 - Ensuring that we require TCP server certificate when getting TCP info over HTTPS

--- src/Raven.Server/Utils/ReplicationUtils.cs
+++ src/Raven.Server/Utils/ReplicationUtils.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Raven.Client;
 using Raven.Client.Documents.Replication;
@@ -14,28 +16,35 @@ namespace Raven.Server.Utils
 {
     internal static class ReplicationUtils
     {
-        public static TcpConnectionInfo GetTcpInfo(string url, string databaseName, string apiKey, string tag)
+        public static TcpConnectionInfo GetTcpInfo(string url, string databaseName, string tag, X509Certificate2 certificate)
         {
             JsonOperationContext context;
-            using (var requestExecutor = ClusterRequestExecutor.CreateForSingleNode(url, apiKey))
+            using (var requestExecutor = ClusterRequestExecutor.CreateForSingleNode(url, certificate))
             using (requestExecutor.ContextPool.AllocateOperationContext(out context))
             {
                 var getTcpInfoCommand = new GetTcpInfoCommand(tag + "/" + databaseName);
                 requestExecutor.Execute(getTcpInfoCommand, context);
 
-                return getTcpInfoCommand.Result;
+                var tcpConnectionInfo = getTcpInfoCommand.Result;
+                if (url.StartsWith("https", StringComparison.OrdinalIgnoreCase) && tcpConnectionInfo.Certificate == null)
+                    throw new InvalidOperationException("Getting TCP info over HTTPS but the server didn't return the expected certificate to use over TCP, invalid response, aborting");
+
+                return tcpConnectionInfo;
             }
         }
 
-        public static async Task<TcpConnectionInfo> GetTcpInfoAsync(string url, string databaseName, string apiKey, string tag)
+        public static async Task<TcpConnectionInfo> GetTcpInfoAsync(string url, string databaseName, string tag, X509Certificate2 certificate)
         {
-            using (var requestExecutor = ClusterRequestExecutor.CreateForSingleNode(url, apiKey))
+            using (var requestExecutor = ClusterRequestExecutor.CreateForSingleNode(url, certificate))
             using (requestExecutor.ContextPool.AllocateOperationContext(out JsonOperationContext context))
             {                
                 var getTcpInfoCommand = new GetTcpInfoCommand(tag + "/" + databaseName);
                 await requestExecutor.ExecuteAsync(getTcpInfoCommand, context);
 
-                return getTcpInfoCommand.Result;
+                var tcpConnectionInfo = getTcpInfoCommand.Result;
+                if (url.StartsWith("https",StringComparison.OrdinalIgnoreCase) && tcpConnectionInfo.Certificate == null)
+                    throw new InvalidOperationException("Getting TCP info over HTTPS but the server didn't return the expected certificate to use over TCP, invalid response, aborting");
+                return tcpConnectionInfo;
             }
         }
 
