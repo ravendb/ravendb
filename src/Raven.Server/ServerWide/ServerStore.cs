49e0fca12b68cef6d455cba2f29b6f6949f08e2c
Ayende Rahien <ayende@ayende.com>
2017-07-11 15:28:48 +0300

    Removing unsecured code for generating a key

--- src/Raven.Server/ServerWide/ServerStore.cs
+++ src/Raven.Server/ServerWide/ServerStore.cs
@@ -18,12 +18,10 @@ using Raven.Client.Json;
 using Raven.Client.Server;
 using Raven.Client.Server.ETL;
 using Raven.Client.Server.Operations;
-using Raven.Client.Server.Operations.ApiKeys;
 using Raven.Server.Commercial;
 using Raven.Server.Config;
 using Raven.Server.Documents;
 using Raven.Server.Documents.Operations;
-using Raven.Server.Json;
 using Raven.Server.NotificationCenter;
 using Raven.Server.Rachis;
 using Raven.Server.NotificationCenter.Notifications;
@@ -1398,71 +1396,7 @@ namespace Raven.Server.ServerWide
 
         }
 
-        private const string SystemApiKey = "Raven:System";
-        public async Task<(string ApiKey, string PublicKey)> GetApiKeyAndPublicKey()
-        {
-            TransactionOperationContext context;
-            using (ContextPool.AllocateOperationContext(out context))
-            using (context.OpenReadTransaction())
-            {
-                var key = Cluster.Read(context, SystemApiKey);
-                string keyFullName;
-                if (key == null)
-                {
-                    EnsureNotPassive();
-                    //making sure we are leaders, we might be followers and fail but that is okay.                    
-                    if (await Engine.WaitForState(RachisConsensus.State.Leader).WaitWithTimeout(TimeSpan.FromSeconds(5)) == false)
-                    {
-                        var msg = "Was requested to get api key and public key but i haven't became leader after 5 seconds";
-                        if (Logger.IsInfoEnabled)
-                        {
-                            Logger.Info(msg);
-                        }
-                        throw new TimeoutException(msg);
-                    }
-                    var secret = GenerateRandomSecret();
-                    var defintions = new ApiKeyDefinition
-                    {
-                        Enabled = true,
-                        ResourcesAccessMode = new Dictionary<string, AccessMode> { { "*",AccessMode.Admin} },
-                        Secret = secret,
-                        ServerAdmin = true
-                    };
-                    var createApiKey = new PutApiKeyCommand(SystemApiKey, defintions);
-                    var index = (await Engine.PutAsync(createApiKey)).Etag;
-                    if (await Engine.WaitForCommitIndexChange(RachisConsensus.CommitIndexModification.GreaterOrEqual, index).WaitWithTimeout(TimeSpan.FromSeconds(5)) == false)
-                    {
-                        var msg = "Waited for 5 seconds for Raven/System to be commited but it wasn't";
-                        if (Logger.IsInfoEnabled)
-                        {
-                            Logger.Info(msg);
-                        }
-                        throw new TimeoutException(msg);
-                    }
-                    keyFullName = $"Key={SystemApiKey}/{secret}";
-                }
-                else
-                {
-                    var desKey = JsonDeserializationServer.ApiKeyDefinition(key);
-                    keyFullName = $"Key={SystemApiKey}/{desKey.Secret}";
-                }
-                return (keyFullName, Convert.ToBase64String(SignPublicKey));
-            }            
-        }
-
-        private const string SecretCharacters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        private string GenerateRandomSecret()
-        {
-            var rand = new Random();
-            var secretLength = (int)(22 * rand.NextDouble()) + 10;
-            var sb = new StringBuilder();
-            for (int i = 0; i < secretLength; i++)
-            {
-                sb.Append(SecretCharacters[rand.Next(SecretCharacters.Length)]);
-            }
-            return sb.ToString();
-        }
-        
+   
         public bool HasClientConfigurationChanged(long index)
         {
             if (index < 0)
