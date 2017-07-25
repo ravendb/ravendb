a66ffaadabaf9fc97efebdf57622f4d44d1a970e
talweiss1982 <tal@ayende.com>
2017-07-25 10:58:02 +0300

    RavenDB-7873 When escaping qouted query we should not escaped Lucene special chars Fixing wrong test assertion in Kamran.cs Adding a new test to check for the case where we have both whitespace and special Lucene chars in the same query

---
+++ Raven.Tests.Issues/RavenDB_7873.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Raven.Client.Indexes;
+using Raven.Tests.Common;
+using Xunit;
+
+namespace Raven.Tests.Issues
+{
+    public class RavenDB_7873 : RavenTest
+    {
+        [Fact]
+        public void QueryingTermsWithDashesShouldWork()
+        {
+            using (var store = NewDocumentStore())
+            {
+                store.ExecuteIndex(new DogsByName());
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Dog { Name = "-woof woof", Age = 5 });
+                    session.SaveChanges();
+                    WaitForIndexing(store);
+                    var res = session.Query<Dog>().Where(m => m.Name.StartsWith("-woof ")).ToList();
+                    Assert.Equal(res.Count, 1);
+                }
+            }
+        }
+
+        public class DogsByName : AbstractIndexCreationTask<Dog>
+        {
+            public DogsByName()
+            {
+                Map = dogs => from dog in dogs select new { dog.Name };
+            }
+        }
+
+        public class Dog
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+    }
+
+    
+}
