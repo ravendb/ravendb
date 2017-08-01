a0d7799bfcc0bc2fc52853087bc5006424c76c73
Ayende Rahien <ayende@ayende.com>
2017-08-01 11:40:26 +0300

    RavenDB-7669 - Fixing test that assume dynamic queries will use static indexes

--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -37,11 +37,11 @@ namespace Tryouts
             {
                 Console.Clear();
                 Console.WriteLine(i);
-                using (var test = new SlowTests.Issues.RavenDb3973())
+                using (var test = new SlowTests.MailingList.Stockholm())
                 {
                     try
                     {
-                        test.VerifyNegateQueryOptimization();
+                        test.ShouldIndexArray();
                     }
                     catch (Exception e)
                     {
