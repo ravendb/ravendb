b96bb2753d1a97fb5508012890c71b0abd5ce9da
talweiss1982 <tal.weiss.1982@gmail.com>
2017-08-01 17:30:13 +0300

    RavenDB-7461 excluding un-managed dlls from compiling with indexes

--- src/Raven.Server/Documents/Indexes/Static/IndexAndTransformerCompiler.cs
+++ src/Raven.Server/Documents/Indexes/Static/IndexAndTransformerCompiler.cs
@@ -222,17 +222,118 @@ namespace Raven.Server.Documents.Indexes.Static
         private static MetadataReference[] GetRefrences()
         {
             //libsodium is a none managed dll we must exclude it from the list of dlls
-            var dlls = Directory.GetFiles(Path.GetDirectoryName(typeof(IndexAndTransformerCompiler).GetTypeInfo().Assembly.Location), "*.dll").Where(x=>Path.GetFileName(x).StartsWith("libsodium") == false).ToArray();
-            var newRefrences = new MetadataReference[References.Length + dlls.Length];
+            var dlls = Directory.GetFiles(Path.GetDirectoryName(typeof(IndexAndTransformerCompiler).GetTypeInfo().Assembly.Location), "*.dll");
+            var managedDlls = GetManagedDlls(dlls);
+            var newRefrences = new MetadataReference[References.Length + managedDlls.Length];
             for (var i = 0; i < References.Length; i++)
             {
                 newRefrences[i] = References[i];
             }
-            for (int i = 0; i < dlls.Length; i++)
+            for (int i = 0; i < managedDlls.Length; i++)
             {
-                newRefrences[i + References.Length] = MetadataReference.CreateFromFile(dlls[i]);
+                newRefrences[i + References.Length] = MetadataReference.CreateFromFile(managedDlls[i]);
             }
             return newRefrences;
+        }
+
+        private static string[] GetManagedDlls(string[] dlls)
+        {
+            HashSet<string> toRemove = null;
+            foreach(var dll in dlls)
+            {
+                var hasValue = IsDllManaged.TryGetValue(dll, out var managed);
+                if (hasValue && managed == true)
+                {
+                    continue;
+                }                
+                if (hasValue == false)
+                {
+                    if(IsManagedAssembly(dll) == true)
+                    {
+                        IsDllManaged[dll] = true;
+                        continue;
+                    } else
+                    {
+                        IsDllManaged[dll] = false;
+                    }
+                }
+                if(toRemove == null)
+                {
+                    toRemove = new HashSet<string>();
+                }
+                toRemove.Add(dll);
+            }
+            if(toRemove == null)
+            {
+                return dlls;
+            }
+            return new HashSet<string>(dlls).Except(toRemove).ToArray<string>();
+        }
+
+        private static Dictionary<string, bool> IsDllManaged = new Dictionary<string, bool>();
+
+        private static bool IsManagedAssembly(string fileName)
+        {
+            using (Stream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            using (BinaryReader binaryReader = new BinaryReader(fileStream))
+            {
+                if (fileStream.Length < 64)
+                {
+                    return false;
+                }
+
+                //PE Header starts @ 0x3C (60). Its a 4 byte header.
+                fileStream.Position = 0x3C;
+                uint peHeaderPointer = binaryReader.ReadUInt32();
+                if (peHeaderPointer == 0)
+                {
+                    peHeaderPointer = 0x80;
+                }
+
+                // Ensure there is at least enough room for the following structures:
+                //     24 byte PE Signature & Header
+                //     28 byte Standard Fields         (24 bytes for PE32+)
+                //     68 byte NT Fields               (88 bytes for PE32+)
+                // >= 128 byte Data Dictionary Table
+                if (peHeaderPointer > fileStream.Length - 256)
+                {
+                    return false;
+                }
+
+                // Check the PE signature.  Should equal 'PE\0\0'.
+                fileStream.Position = peHeaderPointer;
+                uint peHeaderSignature = binaryReader.ReadUInt32();
+                if (peHeaderSignature != 0x00004550)
+                {
+                    return false;
+                }
+
+                // skip over the PEHeader fields
+                fileStream.Position += 20;
+
+                const ushort PE32 = 0x10b;
+                const ushort PE32Plus = 0x20b;
+
+                // Read PE magic number from Standard Fields to determine format.
+                var peFormat = binaryReader.ReadUInt16();
+                if (peFormat != PE32 && peFormat != PE32Plus)
+                {
+                    return false;
+                }
+
+                // Read the 15th Data Dictionary RVA field which contains the CLI header RVA.
+                // When this is non-zero then the file contains CLI data otherwise not.
+                ushort dataDictionaryStart = (ushort)(peHeaderPointer + (peFormat == PE32 ? 232 : 248));
+                fileStream.Position = dataDictionaryStart;
+
+                uint cliHeaderRva = binaryReader.ReadUInt32();
+                if (cliHeaderRva == 0)
+                {
+                    return false;
+                }
+
+                return true;
+            }
         }
 
         private static MemberDeclarationSyntax CreateClass(string name, TransformerDefinition definition)
