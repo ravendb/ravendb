7d70c6ca074163f325243551529c7889b0a128a8
Fitzchak Yitzchaki <fitzchak@ayende.com>
2017-08-02 12:18:54 +0300

    RavenDB-7967 Merge Stats and Tasks under the Settings menu item

--- src/Raven.Studio/typescript/common/shell/routerConfiguration.ts
+++ src/Raven.Studio/typescript/common/shell/routerConfiguration.ts
@@ -4,9 +4,7 @@ import leafMenuItem = require("common/shell/menu/leafMenuItem");
 
 import getManageServerMenuItem = require("common/shell/menu/items/manageServer");
 import getDatabasesMenuItem = require("common/shell/menu/items/databases");
-import getStatsMenuItem = require("common/shell/menu/items/stats");
 import getSettingsMenuItem = require("common/shell/menu/items/settings");
-import getTasksMenuItem = require("common/shell/menu/items/tasks");
 import getIndexesMenuItem = require("common/shell/menu/items/indexes");
 import getTransformersMenuItems = require("common/shell/menu/items/transformers");
 import getDocumentsMenuItem = require("common/shell/menu/items/documents");
@@ -59,9 +57,7 @@ function generateAllMenuItems() {
         getDocumentsMenuItem(appUrls),
         getIndexesMenuItem(appUrls),
         ...getTransformersMenuItems(appUrls),
-        getTasksMenuItem(appUrls),
         getSettingsMenuItem(appUrls),
-        getStatsMenuItem(appUrls),
         getDatabasesMenuItem(appUrls),
         getManageServerMenuItem(),
         new leafMenuItem({
