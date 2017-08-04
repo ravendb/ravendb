d48903403a953e9a9ea066b80d6c9d2052c92fbc
aviviadi <avivi.adi.hr@gmail.com>
2017-08-04 13:55:05 +0300

    RavenDB-5653: add query+reads.lua

---
+++ bench/Scripts/reads.lua
@@ -0,0 +1,13 @@
+init = function(args)
+	depth = tonumber(args[1]) or 1	
+	math.randomseed(os.time())
+end
+
+request = function()
+	local reqs = {}
+
+	for i=1, depth do
+		reqs[i] = wrk.format(nil, "/databases/BenchmarkDB/docs?&id=temp/000000000000".. math.random(8500000)+1000000 .."-A")
+	end
+	return table.concat(reqs)
+end 
