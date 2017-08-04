0f3adf75d51d85214f5acce53b9377e8a8b3de6d
Arkadiusz Palinski <arek@ravendb.net>
2017-08-04 11:29:13 +0200

    RavenDB-8031 - throwing exception if the client gets topology with etag == -1 - make sure we call UpdateTopologyCommand if the db topology Stamp.Index is -1 (first time)

---
+++ test/SlowTests/Issues/RavenDB_8031.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Server;
+using Raven.Client.Server.Operations;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_8031 : RavenTestBase
+    {
+        [Fact]
+        public async Task Can_talk_to_db_if_it_was_created_after_document_store_initialization()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var dbName = store.Database + Guid.NewGuid();
+
+                using (var store2 = new DocumentStore()
+                {
+                    Urls = store.Urls,
+                    Database = dbName
+                }.Initialize())
+                {
+                    Assert.Throws<InvalidOperationException>(() => store2.Admin.Send(new GetStatisticsOperation()));
+
+                    store.Admin.Server.Send(new CreateDatabaseOperation(new DatabaseRecord(dbName)));
+
+                    try
+                    {
+                        for (int i = 0; i < 20; i++)
+                        {
+                            await Task.Delay(500);
+
+                            try
+                            {
+                                await store2.Admin.SendAsync(new GetStatisticsOperation());
+
+                                return;
+                            }
+                            catch (Exception)
+                            {
+                            }
+                        }
+
+                        Assert.True(false, "All attempts to get stats failed");
+                    }
+                    finally
+                    {
+                        store.Admin.Server.Send(new DeleteDatabaseOperation(dbName, hardDelete: true));
+                    }
+                }
+            }
+        }
+    }
+}
