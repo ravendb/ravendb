2cc8af3c3dc38bd0618ccb6b5199bf8e35d130ff
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-08-04 15:57:52 +0200

    RavenDB-7953 upgrade publish scripts to .netcore 2.0

--- scripts/archive.ps1
+++ scripts/archive.ps1
@@ -5,21 +5,20 @@ function UnpackToDir( $archivePath, $outDir ) {
 
     if ($archivePath.EndsWith('.tar.gz')) {
         UnpackTarGzToDir $archivePath $outDir
-    } else {
+    }
+    else {
         throw "Unsupported unpack method for $archivePath"
     }
 }
 
 function UnpackTarGzToDir ( $archivePath, $outDir ) {
 
-    if ($($IsWindows -eq $False) -and $(Get-Command "tar" -ErrorAction SilentlyContinue))
-    {
+    if ($($IsWindows -eq $False) -and $(Get-Command "tar" -ErrorAction SilentlyContinue)) {
         write-host "tar xvzf `"$archivePath`" -C `"$outDir`""
         & tar xvzf "$archivePath"  -C "$outDir"
         CheckLastExitCode
     }
-    else
-    {
+    else {
         $7za = [io.path]::combine("scripts", "assets", "bin", "7za.exe")
         $tempTarArchiveDest = [io.path]::getDirectoryName($archivePath)
         write-host "$7za x -tgzip `"-o$tempTarArchiveDest`" $archivePath"
@@ -36,33 +35,36 @@ function UnpackTarGzToDir ( $archivePath, $outDir ) {
     }
 }
 
-function CreateArchiveFromDir ( $targetFilename, $dir, $spec ) {
+function CreateArchiveFromDir ( $targetFilename, $dir, $spec, $wrapperDirName ) {
     if ($spec.PkgType -eq "zip") {
         ZipFilesFromDir $targetFilename $dir
-    } elseif ($spec.PkgType -eq "tar.bz2") {
-        TarBzFilesFromDir $targetFilename $dir
-    } elseif ($spec.PkgType -eq "tar") {
-        TarFilesFromDir $targetFilename $dir
-    } else {
+    }
+    elseif ($spec.PkgType -eq "tar.bz2") {
+        TarBzFilesFromDir $targetFilename $dir $wrapperDirName
+    }
+    else {
         throw "Unknown archive method for $targetFilename"
     }
 }
 
-function ZipFilesFromDir( $targetFilename, $sourceDir )
-{
+function ZipFilesFromDir( $targetFilename, $sourceDir ) {
     $toZipGlob = [io.path]::combine($sourceDir, '*')
     $zipFile = "$targetFilename.zip"
     Compress-Archive -Path "$toZipGlob" -DestinationPath "$zipFile"
 }
 
-function TarBzFilesFromDir ( $targetFilename, $sourceDir ) {
+function TarBzFilesFromDir ( $targetFilename, $sourceDir, $wrapperDirName ) {
+
+    if ([string]::IsNullOrEmpty($wrapperDirName) -eq $false) {
+        WrapContentsInDir $sourceDir $wrapperDirName
+    }
+
     $glob = [io.path]::combine($sourceDir, '*')
-    if ($($IsWindows -eq $False) -and $(Get-Command "tar" -ErrorAction SilentlyContinue))
-    {
+    if ($($IsWindows -eq $False) -and $(Get-Command "tar" -ErrorAction SilentlyContinue)) {
         & tar -C $sourceDir -cjvf "$targetFilename.tar.bz2" .
         CheckLastExitCode
     }
-    else
+    else 
     {
         $7za = [io.path]::combine("scripts", "assets", "bin", "7za.exe")
         & "$7za" a -ttar "$targetFilename.tar" $glob
@@ -74,17 +76,20 @@ function TarBzFilesFromDir ( $targetFilename, $sourceDir ) {
     }
 }
 
-function TarFilesFromDir ( $targetFilename, $sourceDir ) {
-    $glob = [io.path]::combine($sourceDir, '*')
-    if ($($IsWindows -eq $False) -and $(Get-Command "tar" -ErrorAction SilentlyContinue))
-    {
-        & tar -C $sourceDir -cvf "$targetFilename.tar" .
-        CheckLastExitCode
-    }
-    else
-    {
-        $7za = [io.path]::combine("scripts", "assets", "bin", "7za.exe")
-        & "$7za" a -ttar "$targetFilename.tar" $glob
-        CheckLastExitCode
+function WrapContentsInDir ( $packageDir, $wrapperDirName ) {
+    write-host $packageDir "#" $wrapperDirName
+    $wrapperDir = Join-Path $packageDir -ChildPath $wrapperDirName
+    $pkgContents = Get-ChildItem -Path "$packageDir"
+    New-Item -ItemType Directory -Path $wrapperDir | Out-Null
+ 
+    Push-Location
+    try {
+        cd $packageDir
+        foreach ($item in $pkgContents) {
+            write-host "Move $item -> $wrapperDir"
+            Move-Item $item $wrapperDir
+        }
+    } finally {
+        Pop-Location
     }
 }
