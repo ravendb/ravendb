5d3770895d29733bfd615fdf2603526f5ef1081f
Ayende Rahien <Ayende@ayende.com>
2017-08-06 15:48:09 -0500

    Fixing tests that assumed we'll get TimeoutException directly

--- test/SlowTests/Issues/RavenDB_7162.cs
+++ test/SlowTests/Issues/RavenDB_7162.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using FastTests;
 using Raven.Client.Documents.Indexes;
 using Raven.Client.Documents.Operations.Indexes;
+using Raven.Client.Exceptions;
 using Raven.Tests.Core.Utils.Entities;
 using Xunit;
 
@@ -28,8 +29,8 @@ namespace SlowTests.Issues
                 {
                     using (var session = store.OpenSession())
                     {
-                        var e = Assert.Throws<TimeoutException>(() => session.Query<Person>().Where(x => x.Name == "John").ToList());
-                        Assert.Contains("failed with timeout after 00:00:00", e.Message);
+                        var e = Assert.Throws<AllTopologyNodesDownException>(() => session.Query<Person>().Where(x => x.Name == "John").ToList());
+                        Assert.Contains("failed with timeout after 00:00:00", e.ToString());
                     }
                 }
             }
