1c0a8e45861b8a0e6d63d32aaf17818324880c95
tal <tal@ayende.com>
2017-08-06 14:40:22 +0300

    RavenDB-8048 making sure we don't ever increase header size in raft with OAuth

--- Raven.Abstractions/OAuth/SecuredAuthenticator.cs
+++ Raven.Abstractions/OAuth/SecuredAuthenticator.cs
@@ -43,8 +43,10 @@ namespace Raven.Abstractions.OAuth
 
             if (e.Credentials != null && e.Credentials.ApiKey != null)
             {
-                if (e.Client != null)
+                if (e.Client != null && e.Client.DefaultRequestHeaders.Contains("Has-Api-Key") == false)
+                {                    
                     e.Client.DefaultRequestHeaders.Add("Has-Api-Key", "true");
+                }
 #if !DNXCORE50
                 if (e.Request != null)
                     e.Request.Headers["Has-Api-Key"] = "true";
