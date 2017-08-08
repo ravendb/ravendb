6b46f295326bcb3cb6aaec91dade7dcfc58ce6fa
Arkadiusz Palinski <arek@ravendb.net>
2017-08-08 11:30:32 +0200

    RavenDB-7667 Added failing test

---
+++ test/StressTests/Voron/Issues/RavenDB_7667.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using FastTests;
+using Raven.Client.Documents.Operations;
+using Xunit;
+
+namespace StressTests.Voron.Issues
+{
+    public class RavenDB_7667 : RavenTestBase
+    {
+        [Fact]
+        public void CanStoreAnonymousObject()
+        {
+            using (var store = GetDocumentStore())
+            {
+                const int documentsCount = 150000;
+                using (var bulkInsert = store.BulkInsert())
+                {
+                    for (var i = 0; i < documentsCount; i++)
+                    {
+                        bulkInsert.Store(new User
+                        {
+                            FirstName = RandomString(30),
+                            LastName = RandomString(30),
+                            Phone = RandomString(30)
+                        });
+                    }
+                }
+
+                store.Operations.Send(new DeleteCollectionOperation("Users")).WaitForCompletion(TimeSpan.FromMinutes(5));
+
+                using (var bulkInsert = store.BulkInsert())
+                {
+                    for (var i = 0; i < documentsCount; i++)
+                    {
+                        bulkInsert.Store(new User
+                        {
+                            FirstName = RandomString(30),
+                            LastName = RandomString(30),
+                            Phone = RandomString(30)
+                        });
+                    }
+                }
+            }
+        }
+
+        public class User
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public string Phone { get; set; }
+        }
+
+        private static readonly Random Random = new Random(1);
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+    }
+}
