1b2a6ed33e862686326d197d2de6fe717bcb3ea6
aviv86 <aviv@ayende.com>
2017-08-09 18:40:58 +0300

    RavenDB-7103 IsCollection() : check only on IEnumerable

--- src/Raven.Client/Documents/Subscriptions/SubscriptionCriteria.cs
+++ src/Raven.Client/Documents/Subscriptions/SubscriptionCriteria.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq.Expressions;
 using System.Reflection;
 using Lambda2Js;
@@ -157,16 +157,7 @@ namespace Raven.Client.Documents.Subscriptions
                 if (type.GetGenericArguments().Length == 0)
                     return false;
 
-                var genericTypeDefinition = type.GetGenericTypeDefinition();
-                var collectionTypes = new[] { typeof(IEnumerable<>), typeof(ICollection<>), typeof(IList<>), typeof(List<>) };
-
-                for (var i = 0; i < collectionTypes.Length; i++)
-                {
-                    if (collectionTypes[i].IsAssignableFrom(genericTypeDefinition))
-                        return true;
-                }
-
-                return false;
+                return typeof(IEnumerable).IsAssignableFrom(type.GetGenericTypeDefinition());
             }
         }
 
