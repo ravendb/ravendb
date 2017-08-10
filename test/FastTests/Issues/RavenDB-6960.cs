830cf6e80d79e559efe238d5389efec63bc35657
Pawel Pekrol <ppekrol@ravendb.net>
2017-08-10 13:44:26 +0200

    RavenDB-6960 added test

---
+++ test/FastTests/Issues/RavenDB-6960.cs
@@ -0,0 +1,14 @@
+﻿using Raven.Server.Documents.Indexes.Static;
+using Xunit;
+
+namespace FastTests.Issues
+{
+    public class RavenDB_6960 : NoDisposalNeeded
+    {
+        [Fact]
+        public void EnableDebuggingMustBeSetToFalseForSecurityReasons()
+        {
+            Assert.False(IndexAndTransformerCompiler.EnableDebugging);
+        }
+    }
+}
