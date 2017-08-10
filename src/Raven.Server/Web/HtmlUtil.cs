24a56cd52f9e3d43410629eb7e3e08dadbfdc2a7
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-08-10 14:01:04 +0200

    RavenDB-8101 Explanatory text on the studio is not clear enough to be able to setup in unsafe mode.

---
+++ src/Raven.Server/Web/HtmlUtil.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using Lucene.Net.Util;
+using Raven.Server.Config;
+using Raven.Server.Config.Categories;
+
+namespace Raven.Server.Web
+{
+    public class HtmlUtil
+    {
+        public static string RenderPlaceholders(string html, IDictionary<string, string> substitutions)
+        {
+            return Regex.Replace(html, @"{{\s*(\w+)\s*}}", match => substitutions[match.Groups[1].Value], RegexOptions.Multiline);
+        }
+
+        private const string UnsafePageHtmlResource = "Raven.Server.Web.Assets.Unsafe.html";
+
+        private static string _unsafePageRenderedHtml;
+
+        public static string RenderUnsafePage()
+        {
+            if (_unsafePageRenderedHtml == null)
+            {
+                using (var reader = new StreamReader(
+                    typeof(RavenServer).GetTypeInfo().Assembly.GetManifestResourceStream(UnsafePageHtmlResource)))
+                {
+                    var html = reader.ReadToEnd();
+
+                    var unsecuredAccessFlagsHtml = GetUnsecuredAccessFlagsHtml();
+
+                    var certPathOptDescription = typeof(SecurityConfiguration).GetProperty("CertificatePath").GetCustomAttribute<DescriptionAttribute>().Description;
+
+                    var certExecOptDescription = typeof(SecurityConfiguration).GetProperty("CertificateExec")
+                        .GetCustomAttribute<DescriptionAttribute>().Description;
+
+                    _unsafePageRenderedHtml = RenderPlaceholders(html, new Dictionary<string, string>()
+                    {
+                        {"UNSECURED_ACCESS_ALLOWED_KEY", RavenConfiguration.GetKey(x => x.Security.UnsecuredAccessAllowed)},
+                        {"SERVER_URL_KEY", RavenConfiguration.GetKey(x => x.Core.ServerUrl)},
+                        {"UNSECURED_ACCESS_ALLOWED_FLAGS", unsecuredAccessFlagsHtml},
+                        {"CERTIFICATE_EXEC_KEY", RavenConfiguration.GetKey(x => x.Security.CertificateExec)},
+                        {"CERTIFICATE_PATH_KEY", RavenConfiguration.GetKey(x => x.Security.CertificatePath)},
+                        {"CERTIFICATE_EXEC_DESCRIPTION", certExecOptDescription},
+                        {"CERTIFICATE_PATH_DESCRIPTION", certPathOptDescription}
+                    });
+                }
+            }
+
+            return _unsafePageRenderedHtml;
+        }
+
+        private static string GetUnsecuredAccessFlagsHtml()
+        {
+            var flagsSpans = Enum.GetNames(typeof(UnsecuredAccessAddressRange))
+                .Select(x => $"<span class='text-warning'>{x}</span>");
+
+            var unsecuredAccessFlagsHtml = string.Join("&nbsp;<strong>|</strong>&nbsp;", flagsSpans);
+            return unsecuredAccessFlagsHtml;
+        }
+    }
+}
