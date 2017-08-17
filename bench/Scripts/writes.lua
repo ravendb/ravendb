865160dbe94ba97eabda80860e5a6715ef6a65c4
Federico Andres Lois <federico.lois@corvalius.com>
2017-08-17 10:50:16 -0300

    Fixed writes benchmark script

--- bench/Scripts/writes.lua
+++ bench/Scripts/writes.lua
@@ -2,7 +2,7 @@ init = function(args)
 	depth = tonumber(args[1]) or 1
 	local reqs = {}
 	for i=1, depth do
-		wrk.path = "/databases/BenchmarkDB/docs?id=temp|"
+		wrk.path = "/databases/BenchmarkDB/docs?id=temp/"
         wrk.method = "PUT"
         wrk.body ="{\"Name\":\"test1\",\"Supplier\":1,\"Category\":1,\"QuantityPerUnit\":1,\"PricePerUnit\":1,\"UnitsInStock\":1,\"UnitsOnOrder\":1,\"Discontinued\":false,\"ReorderLevel\":1},\"Metadata\":{\"Raven-Entity-Name\":\"Temp\"}"
         wrk.headers["Content-Type"] = "application/json"
