bc2400d6d53c97ca5c1d9f38912ec9290f5da864
Marcin Lewandowski <marcin@ravendb.net>
2017-08-18 11:05:17 +0200

    RavenDB-8180 Bring back js test framework

--- src/Raven.Studio/typings/tsd.d.ts
+++ src/Raven.Studio/typings/tsd.d.ts
@@ -2,6 +2,7 @@
 /// <reference path="_studio/dto.d.ts"/>
 /// <reference path="_studio/computedAppUrls.d.ts"/>
 /// <reference path="_studio/customTypings.d.ts" />
+/// <reference path="_studio/squire.d.ts" />
 /// <reference path="index.d.ts"/>
 /// <reference path="server/include.ts"/>
 
