194ede1c4109b40e96dbb1db14c5043aeb607758
Ariel Gamrian <arielg1000@gmail.com>
2017-08-21 15:05:28 +0300

    errors fixed

--- src/Raven.Server/SqlMigration/SqlHelper.cs
+++ src/Raven.Server/SqlMigration/SqlHelper.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
-using System.Text;
-using Microsoft.AspNetCore.Server.Kestrel.Internal.Http;
 
 namespace Raven.Server.SqlMigration
 {
