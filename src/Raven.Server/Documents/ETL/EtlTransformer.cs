1a36acaa56a02b298a71c2b2555c5c37fda949db
Ayende Rahien <ayende@ayende.com>
2017-08-22 01:58:45 +0300

    RavenDB-8212 - Fixing SQL Etl scripts. Making sure that we have null propagation working properly

--- src/Raven.Server/Documents/ETL/EtlTransformer.cs
+++ src/Raven.Server/Documents/ETL/EtlTransformer.cs
@@ -7,35 +7,41 @@ using Raven.Server.ServerWide.Context;
 
 namespace Raven.Server.Documents.ETL
 {
-    public abstract class EtlTransformer<TExtracted, TTransformed>  where TExtracted : ExtractedItem
+    public abstract class EtlTransformer<TExtracted, TTransformed> : IDisposable  where TExtracted : ExtractedItem
     {
+        public DocumentDatabase Database { get; }
         protected readonly DocumentsOperationContext Context;
+        private readonly ScriptRunnerCache.Key _key;
+        protected ScriptRunner.SingleRun SingleRun;
 
         protected TExtracted Current;
+        private ScriptRunner.ReturnRun _returnRun;
 
-        protected EtlTransformer(DocumentDatabase database, DocumentsOperationContext context)
+        protected EtlTransformer(DocumentDatabase database, DocumentsOperationContext context,
+            ScriptRunnerCache.Key key)
         {
+            Database = database;
             Context = context;
+            _key = key;
+        }
+
+        public virtual void Initalize()
+        {
+            _returnRun = Database.Scripts.GetScriptRunner(_key, out SingleRun);
+            SingleRun.ScriptEngine.SetGlobalFunction(Transformation.LoadTo, (Action<string, object>)LoadToFunction);
+            for (var i = 0; i < LoadToDestinations.Length; i++)
+            {
+                var collection = LoadToDestinations[i];
+                SingleRun.ScriptEngine.SetGlobalFunction(Transformation.LoadTo + collection, (Action<object>)(cols =>
+                {
+                    LoadToFunction(collection, cols);
+                }));
+            }
         }
 
         protected abstract string[] LoadToDestinations { get; }
 
-        //protected override void CustomizeEngine(ScriptEngine engine, PatcherOperationScope scope)
-        //{
-        //    engine.SetGlobalFunction(Transformation.LoadTo, new Action<string, object>((tableName, colsAsObject) => LoadToFunction(tableName, colsAsObject, scope)));
-
-        //    // ReSharper disable once ForCanBeConvertedToForeach
-        //    for (var i = 0; i < LoadToDestinations.Length; i++)
-        //    {
-        //        var collection = LoadToDestinations[i];
-        //        engine.SetGlobalFunction(Transformation.LoadTo + collection, (Action<object>)(cols =>
-        //        {
-        //            LoadToFunction(collection, cols, scope);
-        //        }));
-        //    }
-        //}
-
-        protected abstract void LoadToFunction(string tableName, object colsAsObject, PatcherOperationScope scope);
+        protected abstract void LoadToFunction(string tableName, object colsAsObject);
 
         public abstract IEnumerable<TTransformed> GetTransformedResults();
 
@@ -44,6 +50,11 @@ namespace Raven.Server.Documents.ETL
         public static void ThrowLoadParameterIsMandatory(string parameterName)
         {
             throw new ArgumentException($"{parameterName} parameter is mandatory");
+        }
+
+        public void Dispose()
+        {
+            _returnRun.Dispose();
         }
     }
 }
