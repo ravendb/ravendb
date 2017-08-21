c0a64837a50d3a21d8877aafcee16dcace10796a
Ayende Rahien <ayende@ayende.com>
2017-08-21 14:09:07 +0300

    RavenDB-8212 - Restored removed test and ensure that null coalescing actually works

---
+++ src/Raven.Server/Documents/Indexes/Static/Roslyn/Rewriters/TransformNullCoalescingOperatorTransformer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Raven.Server.Documents.Indexes.Static.Roslyn.Rewriters
+{
+    public class TransformNullCoalescingOperatorTransformer : CSharpSyntaxRewriter
+    {
+        public static TransformNullCoalescingOperatorTransformer Instance = new TransformNullCoalescingOperatorTransformer();
+
+        private TransformNullCoalescingOperatorTransformer()
+        {
+        }
+
+        public override SyntaxNode VisitBinaryExpression(BinaryExpressionSyntax node)
+        {
+            if (node.IsKind(SyntaxKind.CoalesceExpression) == false)
+                return base.VisitBinaryExpression(node);
+
+            return SyntaxFactory.ConditionalExpression(
+                SyntaxFactory.BinaryExpression(
+                    SyntaxKind.NotEqualsExpression,
+                    node.Left,
+                    SyntaxFactory.LiteralExpression(
+                        SyntaxKind.NullLiteralExpression)),
+                node.Left,
+                node.Right);
+        }
+    }
+}
---
+++ test/SlowTests/Verifications/NullDynamicValues.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using FastTests;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Operations.Indexes;
+using Xunit;
+
+namespace SlowTests.Verifications
+{
+    public class NullDynamicValues : RavenTestBase
+    {
+        public class Person
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        [Fact]
+        public void TransformerOperationWithNullDynamicValues()
+        {
+            using (var store = GetDocumentStore())
+            {
+                PerformTestWithNullDynamicValues(store);
+            }
+        }
+
+        private void PerformTestWithNullDynamicValues(DocumentStore store)
+        {
+            // 1. Create document
+            using (var session = store.OpenSession())
+            {
+                session.Store(new Person { Id = "persons/1" });
+                session.SaveChanges();
+            }
+
+            string[] operations = new string[] { "+", "Plus", "-", "Minus", "%", "Percent", "*", "Mult", "/", "Divide" };
+            for (int i = 0; i < operations.Length - 1; i += 2)
+            {
+                // 2. Define index with an operation on a field that will be null at run time
+                var indexName = $"NullDynamicValueIndex{operations[i + 1]}Operation";
+
+                var transformerDefinition = new IndexDefinition
+                {
+                    Name = indexName,
+                    Maps =
+                    {
+                        $"from u in docs select new {{ Age = (u.Age {operations[i]} 1) ?? -3, u.Name }}"
+                    }
+                };
+
+                store.Admin.Send(new PutIndexesOperation(transformerDefinition));
+
+                WaitForIndexing(store);
+
+                // 3. Retrieve doc with transformer & verify 'Age' is null since it is not part of Person
+                using (var session = store.OpenSession())
+                {
+                    WaitForUserToContinueTheTest(store);
+                    var docs = session.Advanced.DocumentQuery<dynamic>(indexName)
+                        .WhereEquals("Age", -3)
+                        .ToList();
+                    Assert.NotEmpty(docs);
+                }
+            }
+        }
+    }
+}
