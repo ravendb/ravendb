37eed504e6888489cd425f9a5d7857197e9c2f24
Ayende Rahien <ayende@ayende.com>
2017-08-22 17:42:43 +0300

    Brute fixing the studio, need to be reverted when this is merged

--- src/Raven.Studio/typescript/commands/maintenance/adminJsScriptCommand.ts
+++ src/Raven.Studio/typescript/commands/maintenance/adminJsScriptCommand.ts
@@ -6,7 +6,7 @@ class adminJsScriptCommand extends commandBase {
         super();
     }
 
-    execute(): JQueryPromise<Raven.Server.Documents.Patch.AdminJsScriptResult> {
+    execute(): JQueryPromise<any> {
         const args = {
             'server-script': !this.targetDatabase,
             database: this.targetDatabase
