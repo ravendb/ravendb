c5e3603dc226fc29efd7ca7e5c041f57f8e4701b
Pawel Pekrol <ppekrol@ravendb.net>
2017-08-25 13:14:01 +0200

    RavenDB-5988 added SelectMany extension methods (fixes Afif test)

--- src/Raven.Server/Documents/Indexes/Static/Extensions/LinqOnDynamic.cs
+++ src/Raven.Server/Documents/Indexes/Static/Extensions/LinqOnDynamic.cs
@@ -5,6 +5,7 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -22,9 +23,62 @@ namespace Raven.Server.Documents.Indexes.Static.Extensions
             return Enumerable.GroupBy(source, keySelector, resultSelector).Select(inner => new DynamicArray.DynamicGrouping(inner));
         }
 
+        public static IEnumerable<dynamic> SelectMany(this object source,
+                                                Func<dynamic, int, IEnumerable<dynamic>> collectionSelector,
+                                                Func<dynamic, dynamic, dynamic> resultSelector)
+        {
+            return new DynamicArray(Enumerable.SelectMany(Select(source), collectionSelector, resultSelector));
+        }
+
+        public static IEnumerable<dynamic> SelectMany(this object source,
+                                                      Func<dynamic, IEnumerable<dynamic>> collectionSelector,
+                                                      Func<dynamic, dynamic, dynamic> resultSelector)
+        {
+            return new DynamicArray(Enumerable.SelectMany(Select(source), collectionSelector, resultSelector));
+        }
+
+        public static IEnumerable<dynamic> SelectMany(this object source,
+                                                      Func<dynamic, IEnumerable<dynamic>> selector)
+        {
+            return new DynamicArray(Select(source).SelectMany<object, object>(selector));
+        }
+
+        public static IEnumerable<dynamic> SelectMany(this IGrouping<dynamic, dynamic> source,
+                                                      Func<dynamic, int, IEnumerable<dynamic>> collectionSelector,
+                                                      Func<dynamic, dynamic, dynamic> resultSelector)
+        {
+            return new DynamicArray(Enumerable.SelectMany(Select(source), collectionSelector, resultSelector));
+        }
+
+        public static IEnumerable<dynamic> SelectMany(this IGrouping<dynamic, dynamic> source,
+                                                      Func<dynamic, IEnumerable<dynamic>> collectionSelector,
+                                                      Func<dynamic, dynamic, dynamic> resultSelector)
+        {
+            return new DynamicArray(Enumerable.SelectMany(Select(source), collectionSelector, resultSelector));
+        }
+
+        public static IEnumerable<dynamic> SelectMany(this IGrouping<dynamic, dynamic> source,
+                                                      Func<dynamic, IEnumerable<dynamic>> selector)
+        {
+            return new DynamicArray(Select(source).SelectMany<object, object>(selector));
+        }
+
         public static IEnumerable<dynamic> DefaultIfEmpty(this IEnumerable<dynamic> self)
         {
             return self.DefaultIfEmpty<dynamic>(DynamicNullObject.Null);
+        }
+
+        private static IEnumerable<dynamic> Select(this object self)
+        {
+            if (self == null || self is DynamicNullObject)
+                yield break;
+            if (self is IEnumerable == false || self is string)
+                throw new InvalidOperationException("Attempted to enumerate over " + self.GetType().Name);
+
+            foreach (var item in (IEnumerable)self)
+            {
+                yield return item;
+            }
         }
     }
 }
