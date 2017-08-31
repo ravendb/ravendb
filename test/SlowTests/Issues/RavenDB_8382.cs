9dba41214581b9a87107a9b238a0c8d6e8a14710
Arkadiusz Palinski <arek@ravendb.net>
2017-08-31 17:05:14 +0200

    RavenDB-8382 Throw on attempt to group by collection

---
+++ test/SlowTests/Issues/RavenDB_8382.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using FastTests;
+using FastTests.Server.Basic.Entities;
+using Raven.Client.Exceptions;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_8382 : RavenTestBase
+    {
+        [Fact]
+        public void ShouldThrowOnAttemptToGroupByCollection()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var ex = Assert.Throws<InvalidQueryException>(() => session.Advanced.DocumentQuery<Order>().RawQuery("from Orders group by Lines[].ProductName").ToList());
+
+                    Assert.Contains("Grouping by collections in auto map reduce indexes is not supported", ex.Message);
+                }
+            }
+        }
+    }
+}
