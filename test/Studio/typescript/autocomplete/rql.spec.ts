1762d41c7d880ac94ed6abe91c0b0ee67b527691
Fitzchak Yitzchaki <fitzchak@ayende.com>
2017-09-04 13:53:05 +0300

    RavenDB-8181 Add RQL tests and make them pass. RavenDB-8357 Remove collection filter from auto complete.

--- test/Studio/typescript/autocomplete/rql.spec.ts
+++ test/Studio/typescript/autocomplete/rql.spec.ts
@@ -59,8 +59,8 @@ describe("RQL Autocomplete", () => {
     it('from Collection | should list collections', done => {
         rqlTestUtils.autoComplete("from Orders |", northwindProvider(),  ((errors, wordlist, prefix) => {
             assert.equal(prefix, "");
-            assert.deepEqual(_.sortBy(wordlist, [(x: autoCompleteWordList) => x.score]).reverse(), [
-                {caption: "(", value: "( ", score: 8, meta: "collection filter"},
+
+            const afterFromList = [
                 {caption: "as", value: "as ", score: 7, meta: "keyword"},
                 {caption: "where", value: "where ", score: 6, meta: "keyword"},
                 {caption: "group", value: "group ", score: 5, meta: "keyword"},
@@ -68,7 +68,8 @@ describe("RQL Autocomplete", () => {
                 {caption: "select", value: "select ", score: 3, meta: "keyword"},
                 {caption: "order", value: "order ", score: 2, meta: "keyword"},
                 {caption: "include", value: "include ", score: 1, meta: "keyword"}
-            ]);
+            ];
+            assert.deepEqual(_.sortBy(wordlist, [(x: autoCompleteWordList) => x.score]).reverse(), afterFromList);
 
             done();
         }));
@@ -112,6 +113,46 @@ describe("RQL Autocomplete", () => {
         rqlTestUtils.autoComplete("from index 'Orders/Tot|", northwindProvider(),  ((errors, wordlist, prefix) => {
             assert.equal(prefix, "'Orders/Tot");
             assert.deepEqual(wordlist, indexesList);
+
+            done();
+        }));
+    });
+
+    it('from index after index name without space should complete with prefix', done => {
+        rqlTestUtils.autoComplete("from index 'Orders/Totals'|", northwindProvider(),  ((errors, wordlist, prefix) => {
+            assert.equal(prefix, "'Orders/Totals'");
+            assert.deepEqual(wordlist, indexesList);
+
+            done();
+        }));
+    });
+
+    it('from index after index name should complete with after from', done => {
+        rqlTestUtils.autoComplete("from index 'Orders/Totals' |", northwindProvider(),  ((errors, wordlist, prefix) => {
+            assert.equal(prefix, "");
+
+            const afterFromList = [
+                {caption: "as", value: "as ", score: 7, meta: "keyword"},
+                {caption: "where", value: "where ", score: 6, meta: "keyword"},
+                {caption: "load", value: "load ", score: 4, meta: "keyword"},
+                {caption: "select", value: "select ", score: 3, meta: "keyword"},
+                {caption: "order", value: "order ", score: 2, meta: "keyword"},
+                {caption: "include", value: "include ", score: 1, meta: "keyword"}
+            ];
+            assert.deepEqual(_.sortBy(wordlist, [(x: autoCompleteWordList) => x.score]).reverse(), afterFromList);
+
+            done();
+        }));
+    });
+
+    it('show fields of index', done => {
+        rqlTestUtils.autoComplete("from index 'Orders/Totals' select |", northwindProvider(),  ((errors, wordlist, prefix) => {
+            assert.equal(prefix, "");
+            assert.deepEqual(wordlist, [
+                {caption: "Employee", value: "Employee", score: 1, meta: "field"},
+                {caption: "Company", value: "Company", score: 1, meta: "field"},
+                {caption: "Total", value: "Total", score: 1, meta: "field"}
+            ]);
 
             done();
         }));
