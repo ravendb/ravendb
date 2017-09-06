1a90def4bff584dafab405d9ff42a776c62a58db
Marcin Lewandowski <marcin@ravendb.net>
2017-09-06 08:45:57 +0200

    RavenDB-8422 'Show stored index fields only' doesn't work

--- test/Studio/typescript/queryUtil.spec.ts
+++ test/Studio/typescript/queryUtil.spec.ts
@@ -6,6 +6,35 @@ const assert = chai.assert;
 import queryUtil = require("src/Raven.Studio/typescript/common/queryUtil");
 
 describe("queryUtil", () => {
+
+
+    describe('replaceSelectWithFetchAllStoredFields()', () => {
+
+        it ('with out select', () => {
+            const query = "from index 'Orders/ByCompany'";
+            const expected = `from index 'Orders/ByCompany' select __all_stored_fields`;
+            test(query, expected);
+        });
+
+        it ('with simple select', () => {
+            const query = "from index 'Orders/ByCompany' select Count ";
+            const expected = `from index 'Orders/ByCompany'  select __all_stored_fields`;
+            test(query, expected);
+        });
+
+        it ('ignores include after select', () => {
+            const query = "from index 'Orders/ByCompany' select Count include Seller";
+            const expected = `from index 'Orders/ByCompany'  select __all_stored_fields`;
+            test(query, expected);
+        });
+
+        function test(query: string, expected: string) {
+            const result = queryUtil.replaceSelectWithFetchAllStoredFields(query);
+            assert.equal(expected, result);
+        }
+
+    });
+    
     describe('replaceWhereWithDocumentIdPredicate()', () => {
 
         const ID = 'products/10';
