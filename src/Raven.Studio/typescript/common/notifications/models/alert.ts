4abda7df12370249a6ddfb3ebe45fe2e04720cd3
Marcin Lewandowski <marcin@ravendb.net>
2017-09-08 14:26:19 +0200

    RavenDB-8441 Handle license limit response from server

--- src/Raven.Studio/typescript/common/notifications/models/alert.ts
+++ src/Raven.Studio/typescript/common/notifications/models/alert.ts
@@ -8,12 +8,17 @@ class alert extends abstractNotification {
     alertType = ko.observable<Raven.Server.NotificationCenter.Notifications.AlertType>();
     key = ko.observable<string>();
     details = ko.observable<Raven.Server.NotificationCenter.Notifications.Details.INotificationDetails>();
+    isLicenseAlert: KnockoutComputed<boolean>;
 
     constructor(db: database, dto: Raven.Server.NotificationCenter.Notifications.AlertRaised) {
         super(db, dto);
         this.updateWith(dto);
 
         this.hasDetails = ko.pureComputed(() => !!this.details());
+        
+        this.isLicenseAlert = ko.pureComputed(() => this.alertType().startsWith("LicenseManager"));
+
+        this.canBePostponed = ko.pureComputed(() => this.isPersistent() && !this.isLicenseAlert());
     }
 
     updateWith(incomingChanges: Raven.Server.NotificationCenter.Notifications.AlertRaised) {
