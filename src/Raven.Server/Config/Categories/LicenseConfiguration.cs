2d6d9134351ab498289bb209d9b1d34efb8d5d33
Ayende Rahien <Ayende@ayende.com>
2017-09-08 09:26:36 +0300

    Allow to define license file and license via env variables without validation failure

--- src/Raven.Server/Config/Categories/LicenseConfiguration.cs
+++ src/Raven.Server/Config/Categories/LicenseConfiguration.cs
@@ -5,14 +5,17 @@ namespace Raven.Server.Config.Categories
 {
     public class LicenseConfiguration : ConfigurationCategory
     {
+        // these are actually ServerWideOnly, but we want to support configuring this via env variables, in which
+        // case it leaks to the database level and throws
+
         [Description("The full license string for RavenDB. If License is specified, it overrides the License.Path configuration.")]
         [DefaultValue(null)]
-        [ConfigurationEntry("License", ConfigurationEntryScope.ServerWideOnly)]
+        [ConfigurationEntry("License", ConfigurationEntryScope.ServerWideOrPerDatabase)]
         public string License { get; set; }
 
         [Description("The path to the license file for RavenDB, default for ~\\license.json")]
         [DefaultValue("~/license.json")]
-        [ConfigurationEntry("License.Path", ConfigurationEntryScope.ServerWideOnly)]
+        [ConfigurationEntry("License.Path", ConfigurationEntryScope.ServerWideOrPerDatabase)]
         public string LicensePath { get; set; }
 
         [Description("EXPERT ONLY. Skip logging of lease license errors")]
