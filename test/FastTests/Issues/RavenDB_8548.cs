a0ecbe0bb030b9b0a5731e323fefc9b772b018de
Pawel Pekrol <ppekrol@ravendb.net>
2017-09-11 16:51:15 +0200

    RavenDB-8548 support for _ in environment variables

---
+++ test/FastTests/Issues/RavenDB_8548.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Raven.Server.Config;
+using Raven.Server.Config.Settings;
+using Raven.Server.ServerWide;
+using Sparrow.Logging;
+using Xunit;
+
+namespace FastTests.Issues
+{
+    public class RavenDB_8548 : NoDisposalNeeded
+    {
+        [Fact]
+        public void ShouldWork()
+        {
+            Validate("RAVEN_Logs_Mode");
+            Validate("RAVEN.Logs.Mode");            
+        }
+
+        private static void Validate(string key)
+        {
+            Environment.SetEnvironmentVariable(key, LogMode.Information.ToString());
+
+            try
+            {
+                var configuration = new RavenConfiguration("test", ResourceType.Server);
+
+                configuration.Initialize();
+
+                Assert.Equal(LogMode.Information, configuration.Logs.Mode);
+            }
+            finally
+            {
+                Environment.SetEnvironmentVariable(key, null);
+            }
+        }
+    }
+}
