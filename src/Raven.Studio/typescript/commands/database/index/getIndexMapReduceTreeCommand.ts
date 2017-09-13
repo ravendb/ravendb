a22e510c170f5d83f2b7decbe8053cdc5b79f4d6
Marcin Lewandowski <marcin@ravendb.net>
2017-09-13 13:48:09 +0200

    RavenDB-8606 Cannot visualize tree in map/reduce visualizer - studio part

--- src/Raven.Studio/typescript/commands/database/index/getIndexMapReduceTreeCommand.ts
+++ src/Raven.Studio/typescript/commands/database/index/getIndexMapReduceTreeCommand.ts
@@ -17,7 +17,13 @@ class getIndexMapReduceTreeCommand extends commandBase {
             op: "map-reduce-tree"
         };
         return this.query(url + this.urlEncodeArgs(args), null, this.db, x => x.Results)
-            .fail((response: JQueryXHR) => this.reportError("Failed to load map reduce tree", response.responseText, response.statusText));
+            .fail((response: JQueryXHR) => this.reportError("Failed to load map reduce tree", response.responseText, response.statusText))
+            .done((trees: Raven.Server.Documents.Indexes.Debugging.ReduceTree[]) => {
+                if (!trees.length) {
+                    const documents = this.documentIds.map(x => "'" + x + "'").join(",");
+                    this.reportWarning("No results found for " + documents);
+                }
+            })
     }
 } 
 
