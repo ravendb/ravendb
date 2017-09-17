a4f988f2b0f5e43774184a7aa2d0a315b98771bc
Fitzchak Yitzchaki <fitzchak@ayende.com>
2017-09-17 11:45:58 +0300

    RavenDB-8642 Revert changes to jint by moving them to ravendb

---
+++ src/Raven.Server/Documents/Patch/JintNullPropgationReferenceResolver.cs
@@ -0,0 +1,32 @@
+﻿using Jint;
+using Jint.Native;
+using Jint.Runtime.Interop;
+using Jint.Runtime.References;
+
+namespace Raven.Server.Documents.Patch
+{
+    public class JintNullPropgationReferenceResolver : IReferenceResolver
+    {
+        public bool TryUnresolvableReference(Engine engine, Reference reference, out JsValue value)
+        {
+            value = Null.Instance;
+            return true;
+        }
+
+        public bool TryPropertyReference(Engine engine, Reference reference, ref JsValue value)
+        {
+            return value.IsNull() || value.IsUndefined();
+        }
+
+        public bool TryGetCallable(Engine engine, object callee, out JsValue value)
+        {
+            value = new JsValue(new ClrFunctionInstance(engine, (thisObj, values) => thisObj));
+            return true;
+        }
+
+        public bool CheckCoercible(JsValue value)
+        {
+            return true;
+        }
+    }
+}
