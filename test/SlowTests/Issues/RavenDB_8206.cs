33f2e839e6c4744b356474a05aefcd94f5016109
Arkadiusz Palinski <arek@ravendb.net>
2017-09-22 13:12:24 +0200

    RavenDB-8206 Let us evaluate null value or missing field as zero when aggregating on it in auto map-reduce indexes

---
+++ test/SlowTests/Issues/RavenDB_8206.cs
@@ -0,0 +1,82 @@
+﻿using System.Linq;
+using FastTests;
+using Tests.Infrastructure;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_8206 : RavenTestBase
+    {
+        [Fact]
+        public void AggregatingOnNonExistentFieldWillReturnZeros()
+        {
+            using (var store = GetDocumentStore())
+            {
+                store.Admin.Send(new CreateSampleDataOperation());
+
+                using (var session = store.OpenSession())
+                {
+                    var objects = session.Advanced.RawQuery<dynamic>("from Products group by Category select sum(Price), Category").ToList();
+
+                    Assert.Equal(8, objects.Count);
+
+                    foreach (var o in objects)
+                    {
+                        Assert.Equal(0, (long)o.Price);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void AggregationOnNullValuesWorksWithDoubles()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Product
+                    {
+                        Category = "categories/1",
+                        Price = null
+                    });
+
+                    session.Store(new Product
+                    {
+                        Category = "categories/1",
+                        Price = 10.2
+                    });
+
+                    session.Store(new Product
+                    {
+                        Category = "categories/2",
+                        Price = 10.2
+                    });
+
+                    session.Store(new Product
+                    {
+                        Category = "categories/2",
+                        Price = null
+                    });
+
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var results = session.Advanced.RawQuery<Product>("from Products group by Category select sum(Price), Category").ToList();
+
+                    Assert.Equal(10.2, results[0].Price);
+                    Assert.Equal(10.2, results[1].Price);
+                }
+            }
+        }
+
+        private class Product
+        {
+            public string Id { get; set; }
+            public string Category { get; set; }
+            public double? Price { get; set; }
+        }
+    }
+}
