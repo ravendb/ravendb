c7294b246988ab777cfa1ebc4063fabd7bbaf2ea
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-09-22 10:55:40 +0200

    RavenDB-8644 fix upload script and version info determination

--- upload.ps1
+++ upload.ps1
@@ -1,13 +1,15 @@
+$ErrorActionPreference = "Stop"
+
 $ARTIFACTS = Get-ChildItem $([io.path]::combine("artifacts", '*')) -Include "*.zip", "*.tar", "*.tar.bz2"
 
-. '.\scripts\env.ps1'
+. '.\scripts\version.ps1'
 . '.\scripts\checkLastExitCode.ps1'
 . '.\scripts\upload.ps1'
 . '.\scripts\getScriptDirectory.ps1'
 
 $projectDir = Get-ScriptDirectory
 $uploader = [io.path]::combine($projectDir, '..', 'Uploader', 'S3Uploader.exe')
-$buildNumber = GetBuildNumber
-$buildType = GetBuildType
+$versionInfo = GetVersionInfo
 $files = Get-ChildItem $ARTIFACTS
-Upload "$UPLOADER" "$buildNumber" "$buildType" $files
+
+Upload "$uploader" $versionInfo $files
