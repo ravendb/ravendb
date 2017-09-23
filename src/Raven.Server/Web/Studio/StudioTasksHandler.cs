810af0eed0a17c00007920275652ce8ea109e8a0
Pawel Pekrol <ppekrol@ravendb.net>
2017-09-23 17:04:05 +0200

    RavenDB-5051 added /studio-tasks/format endpoint

--- src/Raven.Server/Web/Studio/StudioTasksHandler.cs
+++ src/Raven.Server/Web/Studio/StudioTasksHandler.cs
@@ -6,8 +6,13 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Formatting;
 using Raven.Client;
+using Raven.Client.Exceptions;
 using Raven.Server.Routing;
+using Sparrow.Json;
 
 namespace Raven.Server.Web.Studio
 {
@@ -57,7 +62,43 @@ namespace Raven.Server.Web.Studio
                 return HttpContext.Response.WriteAsync("\"The key must be in Base64 encoding format!\"");
             }
 
-            HttpContext.Response.WriteAsync("\"The key is ok!\"");
+            return HttpContext.Response.WriteAsync("\"The key is ok!\"");
+        }
+
+        [RavenAction("/studio-tasks/format", "POST", AuthorizationStatus.ValidUser)]
+        public Task Format()
+        {
+            using (ServerStore.ContextPool.AllocateOperationContext(out JsonOperationContext context))
+            {
+                var json = context.ReadForMemory(RequestBodyStream(), "studio-tasks/format");
+                if (json == null)
+                    throw new BadRequestException("No JSON was posted.");
+
+                if (json.TryGet("Expression", out string expressionAsString) == false)
+                    throw new BadRequestException("'Expression' property was not found.");
+
+                if (string.IsNullOrWhiteSpace(expressionAsString))
+                    return NoContent();
+
+                using (var workspace = new AdhocWorkspace())
+                {
+                    var expression = SyntaxFactory
+                        .ParseExpression(expressionAsString)
+                        .NormalizeWhitespace();
+
+                    var result = Formatter.Format(expression, workspace);
+
+                    using (var writer = new BlittableJsonTextWriter(context, ResponseBodyStream()))
+                    {
+                        writer.WriteStartObject();
+                        writer.WritePropertyName("Expression");
+                        writer.WriteString(result.ToString());
+
+                        writer.WriteEndObject();
+                    }
+                }
+            }
+
             return Task.CompletedTask;
         }
 
---
+++ test/SlowTests/Issues/RavenDB_5051.cs
@@ -0,0 +1,95 @@
+﻿using System.Net.Http;
+using FastTests;
+using Raven.Client.Documents.Conventions;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Http;
+using Raven.Client.Json;
+using Sparrow.Json;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_5051 : RavenTestBase
+    {
+        [Fact]
+        public void FormatShouldWork()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var result = store.Admin.Send(new FormatOperation(@"from c in docs.Companies
+                select new
+                {
+
+                    Name = c.Name
+                }"));
+
+                Assert.Equal("from c in docs.Companies\r\nselect new\r\n{\r\n    Name = c.Name\r\n}", result.Expression);
+            }
+        }
+
+        private class FormatOperation : IAdminOperation<FormatOperation.Result>
+        {
+            private readonly string _expression;
+
+            public class Result
+            {
+                public string Expression { get; set; }
+            }
+
+            public FormatOperation(string expression)
+            {
+                _expression = expression;
+            }
+
+            public RavenCommand<Result> GetCommand(DocumentConventions conventions, JsonOperationContext context)
+            {
+                return new FormatCommand(context, _expression);
+            }
+
+            private class FormatCommand : RavenCommand<Result>
+            {
+                private readonly JsonOperationContext _context;
+                private readonly string _expression;
+
+                public FormatCommand(JsonOperationContext context, string expression)
+                {
+                    _context = context;
+                    _expression = expression;
+                }
+
+                public override bool IsReadRequest => false;
+
+                public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+                {
+                    url = $"{node.Url}/studio-tasks/format";
+
+                    return new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Post,
+                        Content = new BlittableJsonContent(stream =>
+                        {
+                            using (var writer = new BlittableJsonTextWriter(_context, stream))
+                            {
+                                writer.WriteStartObject();
+                                writer.WritePropertyName(nameof(FormatOperation.Result.Expression));
+                                writer.WriteString(_expression);
+
+                                writer.WriteEndObject();
+                            }
+                        })
+                    };
+                }
+
+                public override void SetResponse(BlittableJsonReaderObject response, bool fromCache)
+                {
+                    response.TryGet(nameof(FormatOperation.Result.Expression), out string function);
+
+                    Result = new Result
+                    {
+                        Expression = function
+                    };
+                }
+            }
+        }
+    }
+}
