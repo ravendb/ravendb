44d9a13e945940bc063c88282f1c51a9ab585601
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-09-27 11:43:26 +0200

    RavenDB-8783 add ubuntu docker image with openssh for remote debugging

---
+++ docker/debug/build-ubuntu-1604-debug.ps1
@@ -0,0 +1,76 @@
+$ErrorActionPreference = 'Stop'
+
+function Get-ScriptDirectory {
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
+}
+
+function CheckLastExitCode {
+    param ([int[]]$SuccessCodes = @(0), [scriptblock]$CleanupScript = $null)
+
+    if ($SuccessCodes -notcontains $LastExitCode) {
+        if ($CleanupScript) {
+            "Executing cleanup script: $CleanupScript"
+            &$CleanupScript
+        }
+        $msg = @"
+EXE RETURNED EXIT CODE $LastExitCode
+CALLSTACK:$(Get-PSCallStack | Out-String)
+"@
+        throw $msg
+    }
+}
+
+$scriptDir = Get-ScriptDirectory
+
+$projectDir = [io.path]::combine($scriptDir, "../..")
+$dockerDir = [io.path]::combine($projectDir, "docker")
+$debugDockerDir = [io.path]::combine($projectDir, "docker", "debug")
+$debugDockerfileDir = [io.path]::combine($projectDir, "docker", "debug", "ravendb-ubuntu1604-debug")
+
+$pkgFile = [io.path]::combine($projectDir, "artifacts", "RavenDB-4.0.0-custom-40-ubuntu.16.04-x64.tar.bz2")
+
+if ($(Test-Path $pkgFile) -eq $False) {
+
+    Push-Location .
+
+    try {
+        Set-Location $projectDir
+        .\build.ps1 -Ubuntu16 -Debug
+        CheckLastExitCode
+        write-host "Built RavenDB in Debug"
+    }
+    finally {
+        Pop-Location
+    }
+
+} else {
+    write-host "Found RavenDB server in artifacts. Building image with whatever is in $pkgFile."
+}
+
+Push-Location .
+try {
+    Set-Location $dockerDir
+    .\build-ubuntu1604.ps1
+    CheckLastExitCode
+    write-host "Built RavenDB Ubuntu 16.04 docker image."
+}
+finally {
+    Pop-Location 
+}
+
+Push-Location .
+
+$dockerDebugTag = "ravendb/ravendb:ubuntu1604-debug"
+
+try {
+    Set-Location $debugDockerDir
+    docker build -t "$dockerDebugTag" "$debugDockerfileDir"
+    CheckLastExitCode
+}
+finally {
+    Pop-Location 
+}
+
+write-host "Built docker debug image $dockerDebugTag"
+
