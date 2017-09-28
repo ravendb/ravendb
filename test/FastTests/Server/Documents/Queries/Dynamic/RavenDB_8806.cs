5eed29fcc88ca2c2063f3fc909fda8a38671feb8
Arkadiusz Palinski <arek@ravendb.net>
2017-09-28 11:00:29 +0200

    RavenDB-8806 Let's create auto indexes to satisfy streaming queries if needed

---
+++ test/FastTests/Server/Documents/Queries/Dynamic/RavenDB_8806.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using Raven.Tests.Core.Utils.Entities;
+using Xunit;
+
+namespace FastTests.Server.Documents.Queries.Dynamic
+{
+    public class RavenDB_8806 : RavenTestBase
+    {
+        [Fact]
+        public void Can_stream_dynamic_query()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User { Name = "Joe" });
+                    session.Store(new User { Name = "Dave" });
+                    session.Store(new User { Name = "Joe" });
+
+                    session.SaveChanges();
+                }
+
+                var count = 0;
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Advanced.DocumentQuery<User>(); // dynamic collection query
+
+                    using (var reader = session.Advanced.Stream(query))
+                    {
+                        while (reader.MoveNext())
+                        {
+                            count++;
+                            Assert.IsType<User>(reader.Current.Document);
+                        }
+                    }
+                }
+
+                Assert.Equal(3, count);
+
+                count = 0;
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Advanced.DocumentQuery<User>().Search(x => x.Name, "j*");
+
+                    using (var reader = session.Advanced.Stream(query))
+                    {
+                        while (reader.MoveNext())
+                        {
+                            count++;
+                            Assert.IsType<User>(reader.Current.Document);
+                        }
+                    }
+                }
+
+                Assert.Equal(2, count);
+
+                count = 0;
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Query<User>().GroupBy(x => x.Name).Select(x => new Result
+                    {
+                        Name = x.Key,
+                        Count = x.Count()
+                    });
+
+                    using (var reader = session.Advanced.Stream(query))
+                    {
+                        while (reader.MoveNext())
+                        {
+                            count++;
+                            Assert.IsType<Result>(reader.Current.Document);
+                        }
+                    }
+                }
+
+                Assert.Equal(2, count);
+            }
+        }
+
+        private class Result
+        {
+            public string Name { get; set; }
+
+            public int Count { get; set; }
+        }
+    }
+}
