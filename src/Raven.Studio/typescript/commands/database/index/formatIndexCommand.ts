d55488ebd35b1a146060c62dcf2d5b93dc949f83
Ariel Gamrian <arielg1000@gmail.com>
2017-09-28 12:56:11 +0300

    RavenDB-5051 Create index format endpoint - Fixes

--- src/Raven.Studio/typescript/commands/database/index/formatIndexCommand.ts
+++ src/Raven.Studio/typescript/commands/database/index/formatIndexCommand.ts
@@ -1,18 +1,22 @@
 import commandBase = require("commands/commandBase");
 import database = require("models/resources/database");
 import endpoints = require("endpoints");
-import FormatedExpression = Raven.Server.Web.Studio.StudioTasksHandler.FormatedExpression;
+
 
 class formatIndexCommand extends commandBase {
 
-    constructor(private db: database, private expressionAsString: string) {
+    constructor(private db: database, private expression: string) {
         super();
     }
 
-    execute(): JQueryPromise<FormatedExpression> {
-        var url = endpoints.global.studioTasks.studioTasksFormat;
-        var expression = { "Expression": this.expressionAsString };
-        return this.post(url, JSON.stringify(expression));
+    execute(): JQueryPromise<Raven.Server.Web.Studio.StudioTasksHandler.FormatedExpression> {
+        const url = endpoints.global.studioTasks.studioTasksFormat;
+        const payload = {
+            Expression: this.expression
+        }
+        return this.post(url, JSON.stringify(payload)).fail((response: JQueryXHR) => {
+            this.reportError("Failed to format text!", response.responseText);
+        });
     }
 }
 
