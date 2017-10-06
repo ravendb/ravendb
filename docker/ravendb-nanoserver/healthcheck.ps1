282a395dec30260197213f234da8f79c4a32b3b3
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-10-06 15:45:04 +0200

    RavenDB-8872 remove index.html healtcheck part

--- docker/ravendb-nanoserver/healthcheck.ps1
+++ docker/ravendb-nanoserver/healthcheck.ps1
@@ -6,23 +6,4 @@ if ($p.HasExited) {
     exit 1
 }
 
-$serverUrlScheme = if ([string]::IsNullOrEmpty($env:CERTIFICATE_PATH)) { "http" } else { "https" }
-
-# Since we're bound to 0.0.0.0 we can check studio on localhost
-$uri = "$($serverUrlScheme)://localhost:8080/studio/index.html"
-
-# If public server URL is specified, use that
-if ([string]::IsNullOrEmpty($env:PUBLIC_SERVER_URL) -eq $False) {
-    $uri = $env:PUBLIC_SERVER_URL
-}
-
-write-host "Trying to access studio under $uri"
-$studioIndexResponse = Invoke-WebRequest -Uri $uri  -Method Get
-
-if ($studioIndexResponse.StatusCode -ne 400) {
-    Write-Host "Server responsed with HTTP $($studioIndexResponse.StatusCode) $($studioIndexResponse.StatusDescription)"
-    Write-Host "$($studioIndexReponse.Content)"
-    exit 1
-}
-
 exit 0
--- docker/ravendb-ubuntu1604/healthcheck.sh
+++ docker/ravendb-ubuntu1604/healthcheck.sh
@@ -4,22 +4,4 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-if [ ! -z "$CERTIFICATE_PATH" ]; then
-    SERVER_URL_SCHEME="https"
-else
-    SERVER_URL_SCHEME="http"
-fi
-
-SERVER_URL="${SERVER_URL_SCHEME}://localhost:8080/studio/index.html"
-
-if [ ! -z "$PUBLIC_SERVER_URL" ]; then
-    SERVER_URL="$PUBLIC_SERVER_URL"
-fi
-
-curl -f -X GET "$SERVER_URL" > /dev/null
-if [ $? -ne 0 ]; then
-    echo "Could not access studio index.html with curl: $?"
-    exit 1
-fi
-
 exit 0
