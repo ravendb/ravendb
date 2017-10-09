0615712f9ddaf8d29d0454c3f3b0a7e741998ff5
Ayende Rahien <ayende@ayende.com>
2017-10-09 09:33:26 +0300

    RavenDB-8921 Fix error text on RavenQuery.Load

--- src/Raven.Client/Documents/Queries/RavenQuery.cs
+++ src/Raven.Client/Documents/Queries/RavenQuery.cs
@@ -7,12 +7,12 @@ namespace Raven.Client.Documents.Queries
     {
         public static T Load<T>(string id)
         {
-            throw new NotSupportedException("Never called");
+            throw new NotSupportedException("This method is here for strongly type support of server side call during Linq queries and should never be directly called");
         }
 
         public static IEnumerable<T> Load<T>(IEnumerable<string> ids)
         {
-            throw new NotSupportedException("Never called");
+            throw new NotSupportedException("This method is here for strongly type support of server side call during Linq queries and should never be directly called");
         }
 
     }
