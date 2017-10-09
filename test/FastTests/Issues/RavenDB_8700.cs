3953751ed7360675a774626baa8c597ce0a23420
Idan Haim Shalom <haimdude@gmail.com>
2017-10-09 15:47:10 +0300

    RavenDB-8700 AssertInitialized in ExecuteIndex and in ExecuteIndexes

---
+++ test/FastTests/Issues/RavenDB_8700.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+using Xunit;
+
+namespace FastTests.Issues
+{
+    public class RavenDB_8700 : RavenTestBase
+    {
+        private class EntityGroupedByDayIndex : AbstractIndexCreationTask
+        {
+            public override IndexDefinition CreateIndexDefinition()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [Fact]
+        public void ThisWillCrashOnDocumentIdGenerator()
+        {
+            var documentStore = new DocumentStore
+            {
+                Urls = UseFiddler(Server.WebUrl),
+                Database = "test-database"
+            };
+            // this seems to work, until .Store is called.
+            Assert.Throws<InvalidOperationException>(() => documentStore.ExecuteIndex(new EntityGroupedByDayIndex()));
+
+            Assert.Throws<InvalidOperationException>(() => documentStore.ExecuteIndexes(new[] {new EntityGroupedByDayIndex()}));
+        }
+    }
+}
