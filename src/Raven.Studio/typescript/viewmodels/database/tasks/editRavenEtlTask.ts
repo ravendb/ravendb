c623da8228f8c59f1e6c6e6de3e06ccf7b6fc3c9
Marcin Lewandowski <marcin@ravendb.net>
2017-10-13 17:28:37 +0200

    RavenDB-8857 dirty flag on external replication configuration

--- src/Raven.Studio/typescript/viewmodels/database/tasks/editRavenEtlTask.ts
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/editRavenEtlTask.ts
@@ -69,7 +69,7 @@ class editRavenEtlTask extends viewModelBase {
             deferred.resolve();
         }
 
-        deferred.always(() => {
+        deferred.done(() => {
             this.initObservables();
         });
 
