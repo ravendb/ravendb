f0c6056aabbec7130e221c560d1dd28e6bb14335
talweiss1982 <tal@ayende.com>
2017-10-16 17:41:02 +0300

    RavenDB-5213 Import from Csv

---
+++ src/Raven.Client/Util/CSharpClassName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Raven.Client.Documents.Indexes;
+
+namespace Raven.Client.Util
+{
+    public class CSharpClassName
+    {
+        public static string ConvertToValidClassName(string input)
+        {
+            var ravenEntityName = input.Replace("-", "_")
+                .Replace(" ", "_")
+                .Replace("__", "_");
+            if (ExpressionStringBuilder.KeywordsInCSharp.Contains(ravenEntityName))
+                ravenEntityName += "Item";
+            return ravenEntityName;
+        }
+    }
+}
