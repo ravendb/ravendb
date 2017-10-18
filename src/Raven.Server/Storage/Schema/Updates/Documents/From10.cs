db7e94a644028e3782a8ac77883c9d039d990f27
Fitzchak Yitzchaki <fitzchak@ayende.com>
2017-10-18 12:49:14 +0300

    RavenDB-9006 Upgrade the conflicts table and fix implementation of collections table upgrade

--- src/Raven.Server/Storage/Schema/Updates/Documents/From10.cs
+++ src/Raven.Server/Storage/Schema/Updates/Documents/From10.cs
@@ -1,35 +1,66 @@
-﻿using System.Collections.Generic;
-using Raven.Server.Documents;
+﻿using Raven.Server.Documents;
 using Raven.Server.ServerWide.Context;
 using Voron;
 using Voron.Data.Tables;
 using Voron.Impl;
+using static Raven.Server.Documents.ConflictsStorage;
 using static Raven.Server.Documents.DocumentsStorage;
 
 namespace Raven.Server.Storage.Schema.Updates.Documents
 {
-    public class From10 : ISchemaUpdate
+    public unsafe class From10 : ISchemaUpdate
     {
         public bool Update(Transaction readTx, Transaction writeTx, ConfigurationStorage configurationStorage, DocumentsStorage documentsStorage)
         {
             // Update collections
             using (documentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
             {
-                var table = writeTx.OpenTable(CollectionsSchema, CollectionsSlice);
-                var collections = new List<string>();
-                foreach (var tvr in table.SeekByPrimaryKey(Slices.BeforeAllKeys, 0))
+                var readTable = readTx.OpenTable(CollectionsSchema, CollectionsSlice);
+                var writeTable = writeTx.OpenTable(CollectionsSchema, CollectionsSlice);
+                foreach (var read in readTable.SeekByPrimaryKey(Slices.BeforeAllKeys, 0))
                 {
-                    var collection = TableValueToString(context, (int)CollectionsTable.Name, ref tvr.Reader);
-                    collections.Add(collection);
-                    table.Delete(tvr.Reader.Id);
-                }
-                foreach (var collection in collections)
-                {
+                    var collection = TableValueToString(context, (int)CollectionsTable.Name, ref read.Reader);
                     using (DocumentIdWorker.GetStringPreserveCase(context, collection, out Slice collectionSlice))
-                    using (table.Allocate(out TableValueBuilder tvr))
+                    using (writeTable.Allocate(out TableValueBuilder write))
                     {
-                        tvr.Add(collectionSlice);
-                        table.Insert(tvr);
+                        write.Add(collectionSlice);
+                        var pk = read.Reader.Read((int)CollectionsTable.Name, out int size);
+                        using (Slice.External(context.Allocator, pk, size, out var pkSlice))
+                        {   
+                            writeTable.DeleteByKey(pkSlice);
+                        }
+                        writeTable.Insert(write);
+                    }
+                }
+            }
+
+            // Update tombstones's collection value
+            using (documentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+            {
+                // TODO
+            }
+
+            // Update conflicts' collection value
+            using (documentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+            {
+                var readTable = readTx.OpenTable(ConflictsSchema, ConflictsSlice);
+                var writeTable = writeTx.OpenTable(ConflictsSchema, ConflictsSlice);
+                foreach (var read in readTable.SeekByPrimaryKey(Slices.BeforeAllKeys, 0))
+                {
+                    var oldCollection = TableValueToString(context, (int)ConflictsTable.Collection, ref read.Reader);
+                    using (DocumentIdWorker.GetStringPreserveCase(context, oldCollection, out Slice collectionSlice))
+                    using (writeTable.Allocate(out TableValueBuilder write))
+                    {
+                        write.Add(read.Reader.Read((int)ConflictsTable.LowerId, out int size), size);
+                        write.Add(read.Reader.Read((int)ConflictsTable.RecordSeparator, out size), size);
+                        write.Add(read.Reader.Read((int)ConflictsTable.ChangeVector, out size), size);
+                        write.Add(read.Reader.Read((int)ConflictsTable.Id, out size), size);
+                        write.Add(read.Reader.Read((int)ConflictsTable.Data, out size), size);
+                        write.Add(read.Reader.Read((int)ConflictsTable.Etag, out size), size);
+                        write.Add(collectionSlice);
+                        write.Add(read.Reader.Read((int)ConflictsTable.LastModified, out size), size);
+                        write.Add(read.Reader.Read((int)ConflictsTable.Flags, out size), size);
+                        writeTable.Set(write);
                     }
                 }
             }
