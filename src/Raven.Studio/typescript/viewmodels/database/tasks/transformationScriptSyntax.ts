5bfeab6b51bcd8c1b0d33394d23c1fe7153f9444
Ariel Gamrian <arielg1000@gmail.com>
2017-10-18 15:31:18 +0300

    RavenDB-8986 - SQL ETL Edit View - Format the syntax help

--- src/Raven.Studio/typescript/viewmodels/database/tasks/transformationScriptSyntax.ts
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/transformationScriptSyntax.ts
@@ -3,7 +3,30 @@ import dialogViewModelBase = require("viewmodels/dialogViewModelBase");
 class transformationScriptSyntax extends dialogViewModelBase {
     
     etlType = ko.observable<Raven.Client.ServerWide.ETL.EtlType>();
-    
+    scriptStr = `var orderData = {
+    Id: id(this),
+    OrderLinesCount: this.Lines.length,
+    TotalCost: 0
+};
+
+for (var i = 0; i < this.Lines.length; i++) {
+    var line = this.Lines[i];
+    orderData.TotalCost += line.PricePerUnit;
+    loadToOrderLines({
+        OrderId: id(this),
+        Qty: line.Quantity,
+        Product: line.Product,
+        Cost: line.PricePerUnit
+    });
+}
+orderData.TotalCost = Math.round(orderData.TotalCost  * 100) / 100;
+
+loadToOrders(orderData);`;
+
+    scriptHtml = ko.pureComputed(() => {
+        return Prism.highlight(this.scriptStr, (Prism.languages as any).javascript);
+    });
+
     constructor(etlType: Raven.Client.ServerWide.ETL.EtlType) {        
         super();
         
