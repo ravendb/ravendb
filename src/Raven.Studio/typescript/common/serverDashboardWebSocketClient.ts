c8ba15faf1a08e1d204c88348421a5756ae90aac
Marcin Lewandowski <marcin@ravendb.net>
2017-10-18 14:50:45 +0200

    RavenDB-8771 Server dashboard - fixed bug with scale on graphs

--- src/Raven.Studio/typescript/common/serverDashboardWebSocketClient.ts
+++ src/Raven.Studio/typescript/common/serverDashboardWebSocketClient.ts
@@ -1,13 +1,10 @@
 /// <reference path="../../typings/tsd.d.ts" />
-import database = require("models/resources/database");
 import abstractWebSocketClient = require("common/abstractWebSocketClient");
-import d3 = require("d3");
 import endpoints = require("endpoints");
 
 class serverDashboardWebSocketClient extends abstractWebSocketClient<Raven.Server.Dashboard.AbstractDashboardNotification> {
 
     private readonly onData: (data: Raven.Server.Dashboard.AbstractDashboardNotification) => void;
-    private static isoParser = d3.time.format.iso;
     loading = ko.observable<boolean>(true);
 
     constructor(onData: (data: Raven.Server.Dashboard.AbstractDashboardNotification) => void) {
