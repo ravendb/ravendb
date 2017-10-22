c3d291ee69797255429e2c6b4519178a3101f2b9
Fitzchak Yitzchaki <fitzchak@ayende.com>
2017-10-22 14:19:36 +0300

    RavenDB-9006 Release the allocated memory ASAP.

---
+++ src/Raven.Server/Json/TableValueReaderUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Raven.Server.ServerWide.Context;
+using Sparrow;
+using Sparrow.Json;
+using Voron.Data.Tables;
+
+namespace Raven.Server.Json
+{
+    public static class TableValueReaderUtil
+    {
+        public static unsafe ReleaseMemory CloneTableValueReader(DocumentsOperationContext context, Table.TableValueHolder read)
+        {
+            var copyReadMemory = context.GetMemory(read.Reader.Size);
+            Memory.Copy(copyReadMemory.Address, read.Reader.Pointer, read.Reader.Size);
+            read.Reader = new TableValueReader(copyReadMemory.Address, read.Reader.Size);
+            return new ReleaseMemory(context, copyReadMemory);
+        }
+
+        public struct ReleaseMemory : IDisposable
+        {
+            private readonly DocumentsOperationContext _context;
+            private readonly AllocatedMemoryData _copyOfMemory;
+
+            public ReleaseMemory(DocumentsOperationContext context, AllocatedMemoryData copyOfMemory)
+            {
+                _context = context;
+                _copyOfMemory = copyOfMemory;
+            }
+
+            public void Dispose()
+            {
+                _context.ReturnMemory(_copyOfMemory);
+            }
+        }
+    }
+}
--- src/Raven.Server/Storage/Schema/Updates/Documents/From10.cs
+++ src/Raven.Server/Storage/Schema/Updates/Documents/From10.cs
@@ -1,6 +1,6 @@
 ﻿using Raven.Server.Documents;
+using Raven.Server.Json;
 using Raven.Server.ServerWide.Context;
-using Sparrow;
 using Voron;
 using Voron.Data.Tables;
 using static Raven.Server.Documents.AttachmentsStorage;
@@ -23,19 +23,20 @@ namespace Raven.Server.Storage.Schema.Updates.Documents
                     var writeTable = step.WriteTx.OpenTable(CollectionsSchema, CollectionsSlice);
                     foreach (var read in readTable.SeekByPrimaryKey(Slices.BeforeAllKeys, 0))
                     {
-                        CopyReadMemory(context, read);
-
-                        var collection = TableValueToString(context, (int)CollectionsTable.Name, ref read.Reader);
-                        using (DocumentIdWorker.GetStringPreserveCase(context, collection, out Slice collectionSlice))
-                        using (writeTable.Allocate(out TableValueBuilder write))
+                        using (TableValueReaderUtil.CloneTableValueReader(context, read))
                         {
-                            write.Add(collectionSlice);
-                            var pk = read.Reader.Read((int)CollectionsTable.Name, out int size);
-                            using (Slice.External(context.Allocator, pk, size, out var pkSlice))
+                            var collection = TableValueToString(context, (int)CollectionsTable.Name, ref read.Reader);
+                            using (DocumentIdWorker.GetStringPreserveCase(context, collection, out Slice collectionSlice))
+                            using (writeTable.Allocate(out TableValueBuilder write))
                             {
-                                writeTable.DeleteByKey(pkSlice);
+                                write.Add(collectionSlice);
+                                var pk = read.Reader.Read((int)CollectionsTable.Name, out int size);
+                                using (Slice.External(context.Allocator, pk, size, out var pkSlice))
+                                {
+                                    writeTable.DeleteByKey(pkSlice);
+                                }
+                                writeTable.Insert(write);
                             }
-                            writeTable.Insert(write);
                         }
                     }
                 }
@@ -67,30 +68,34 @@ namespace Raven.Server.Storage.Schema.Updates.Documents
                     // we weed to ensure that we aren't accessing an IsGlobal key
                     foreach (var read in readTable.SeekForwardFrom(TombstonesSchema.FixedSizeIndexes[CollectionEtagsSlice], 0, 0))
                     {
-                        CopyReadMemory(context, read);
-
-                        var type = *(DocumentTombstone.TombstoneType*)read.Reader.Read((int)TombstoneTable.Type, out _);
-                        var oldCollection = TableValueToString(context, (int)TombstoneTable.Collection, ref read.Reader);
-                        using (DocumentIdWorker.GetStringPreserveCase(context, oldCollection, out Slice collectionSlice))
-                        using (writeTable.Allocate(out TableValueBuilder write))
+                        // We copy the memory of the read so AssertNoReferenceToOldData won't throw.
+                        // This is done instead of moving AssertNoReferenceToOldData to assert later 
+                        // after we allocate the new write memory.
+                        using (TableValueReaderUtil.CloneTableValueReader(context, read))
                         {
-                            write.Add(read.Reader.Read((int)TombstoneTable.LowerId, out int size), size);
-                            write.Add(read.Reader.Read((int)TombstoneTable.Etag, out size), size);
-                            write.Add(read.Reader.Read((int)TombstoneTable.DeletedEtag, out size), size);
-                            write.Add(read.Reader.Read((int)TombstoneTable.TransactionMarker, out size), size);
-                            write.Add(read.Reader.Read((int)TombstoneTable.Type, out size), size);
-                            if (type == DocumentTombstone.TombstoneType.Attachment)
+                            var type = *(DocumentTombstone.TombstoneType*)read.Reader.Read((int)TombstoneTable.Type, out _);
+                            var oldCollection = TableValueToString(context, (int)TombstoneTable.Collection, ref read.Reader);
+                            using (DocumentIdWorker.GetStringPreserveCase(context, oldCollection, out Slice collectionSlice))
+                            using (writeTable.Allocate(out TableValueBuilder write))
                             {
-                                write.Add(read.Reader.Read((int)TombstoneTable.Collection, out size), size);
+                                write.Add(read.Reader.Read((int)TombstoneTable.LowerId, out int size), size);
+                                write.Add(read.Reader.Read((int)TombstoneTable.Etag, out size), size);
+                                write.Add(read.Reader.Read((int)TombstoneTable.DeletedEtag, out size), size);
+                                write.Add(read.Reader.Read((int)TombstoneTable.TransactionMarker, out size), size);
+                                write.Add(read.Reader.Read((int)TombstoneTable.Type, out size), size);
+                                if (type == DocumentTombstone.TombstoneType.Attachment)
+                                {
+                                    write.Add(read.Reader.Read((int)TombstoneTable.Collection, out size), size);
+                                }
+                                else
+                                {
+                                    write.Add(collectionSlice);
+                                }
+                                write.Add(read.Reader.Read((int)TombstoneTable.Flags, out size), size);
+                                write.Add(read.Reader.Read((int)TombstoneTable.ChangeVector, out size), size);
+                                write.Add(read.Reader.Read((int)TombstoneTable.LastModified, out size), size);
+                                writeTable.Set(write);
                             }
-                            else
-                            {
-                                write.Add(collectionSlice);
-                            }
-                            write.Add(read.Reader.Read((int)TombstoneTable.Flags, out size), size);
-                            write.Add(read.Reader.Read((int)TombstoneTable.ChangeVector, out size), size);
-                            write.Add(read.Reader.Read((int)TombstoneTable.LastModified, out size), size);
-                            writeTable.Set(write);
                         }
                     }
                 }
@@ -105,38 +110,29 @@ namespace Raven.Server.Storage.Schema.Updates.Documents
                     var writeTable = step.WriteTx.OpenTable(ConflictsSchema, ConflictsSlice);
                     foreach (var read in readTable.SeekByPrimaryKey(Slices.BeforeAllKeys, 0))
                     {
-                        CopyReadMemory(context, read);
-
-                        var oldCollection = TableValueToString(context, (int)ConflictsTable.Collection, ref read.Reader);
-                        using (DocumentIdWorker.GetStringPreserveCase(context, oldCollection, out Slice collectionSlice))
-                        using (writeTable.Allocate(out TableValueBuilder write))
+                        using (TableValueReaderUtil.CloneTableValueReader(context, read))
                         {
-                            write.Add(read.Reader.Read((int)ConflictsTable.LowerId, out int size), size);
-                            write.Add(read.Reader.Read((int)ConflictsTable.RecordSeparator, out size), size);
-                            write.Add(read.Reader.Read((int)ConflictsTable.ChangeVector, out size), size);
-                            write.Add(read.Reader.Read((int)ConflictsTable.Id, out size), size);
-                            write.Add(read.Reader.Read((int)ConflictsTable.Data, out size), size);
-                            write.Add(read.Reader.Read((int)ConflictsTable.Etag, out size), size);
-                            write.Add(collectionSlice);
-                            write.Add(read.Reader.Read((int)ConflictsTable.LastModified, out size), size);
-                            write.Add(read.Reader.Read((int)ConflictsTable.Flags, out size), size);
-                            writeTable.Set(write);
+                            var oldCollection = TableValueToString(context, (int)ConflictsTable.Collection, ref read.Reader);
+                            using (DocumentIdWorker.GetStringPreserveCase(context, oldCollection, out Slice collectionSlice))
+                            using (writeTable.Allocate(out TableValueBuilder write))
+                            {
+                                write.Add(read.Reader.Read((int)ConflictsTable.LowerId, out int size), size);
+                                write.Add(read.Reader.Read((int)ConflictsTable.RecordSeparator, out size), size);
+                                write.Add(read.Reader.Read((int)ConflictsTable.ChangeVector, out size), size);
+                                write.Add(read.Reader.Read((int)ConflictsTable.Id, out size), size);
+                                write.Add(read.Reader.Read((int)ConflictsTable.Data, out size), size);
+                                write.Add(read.Reader.Read((int)ConflictsTable.Etag, out size), size);
+                                write.Add(collectionSlice);
+                                write.Add(read.Reader.Read((int)ConflictsTable.LastModified, out size), size);
+                                write.Add(read.Reader.Read((int)ConflictsTable.Flags, out size), size);
+                                writeTable.Set(write);
+                            }
                         }
                     }
                 }
             }
 
             return true;
-        }
-
-        private void CopyReadMemory(DocumentsOperationContext context, Table.TableValueHolder read)
-        {
-            // We copy the memory of the read so AssertNoReferenceToOldData won't throw.
-            // This is done instead of moving AssertNoReferenceToOldData to assert later 
-            // after we allocate the new write memory.
-            var copyReadMemory = context.GetMemory(read.Reader.Size);
-            Memory.Copy(copyReadMemory.Address, read.Reader.Pointer, read.Reader.Size);
-            read.Reader = new TableValueReader(copyReadMemory.Address, read.Reader.Size);
         }
     }
 }
