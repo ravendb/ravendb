89db41819b080ba7c9bf590dfb3a34c65ac2b79f
Fitzchak Yitzchaki <fitzchak@ayende.com>
2017-10-23 17:11:54 +0300

    RavenDB-8703 When importing legacy revisions we'll set the change vector in the following format: "RV:{revisionsCount}-{firstEtagOfLegacyRevision}" so importing a new export from the same database would overwrite the previous imported revisions instead of added new once.

--- test/SlowTests/Smuggler/LegacySmugglerTests.cs
+++ test/SlowTests/Smuggler/LegacySmugglerTests.cs
@@ -4,9 +4,12 @@ using System.Reflection;
 using System.Threading.Tasks;
 using FastTests;
 using FastTests.Server.Basic.Entities;
+using FastTests.Server.Documents.Revisions;
+using Raven.Client;
 using Raven.Client.Documents.Operations;
 using Raven.Client.Documents.Operations.Indexes;
 using Raven.Client.Documents.Smuggler;
+using Raven.Server.Documents;
 using Xunit;
 
 namespace SlowTests.Smuggler
@@ -18,41 +21,39 @@ namespace SlowTests.Smuggler
         public async Task CanImportNorthwind(string file)
         {
             using (var stream = GetType().GetTypeInfo().Assembly.GetManifestResourceStream(file))
+            using (var store = GetDocumentStore())
             {
                 Assert.NotNull(stream);
 
-                using (var store = GetDocumentStore())
+                await store.Smuggler.ImportAsync(new DatabaseSmugglerImportOptions(), stream);
+
+                var stats = await store.Admin.SendAsync(new GetStatisticsOperation());
+
+                Assert.Equal(1059, stats.CountOfDocuments);
+                Assert.Equal(3, stats.CountOfIndexes); // there are 4 in ravendbdump, but Raven/DocumentsByEntityName is skipped
+
+                var collectionStats = await store.Admin.SendAsync(new GetCollectionStatisticsOperation());
+                Assert.Equal(1059, collectionStats.CountOfDocuments);
+                Assert.Equal(9, collectionStats.Collections.Count);
+                Assert.Equal(8, collectionStats.Collections["Categories"]);
+                Assert.Equal(91, collectionStats.Collections["Companies"]);
+                Assert.Equal(9, collectionStats.Collections["Employees"]);
+                Assert.Equal(830, collectionStats.Collections["Orders"]);
+                Assert.Equal(77, collectionStats.Collections["Products"]);
+                Assert.Equal(4, collectionStats.Collections["Regions"]);
+                Assert.Equal(3, collectionStats.Collections["Shippers"]);
+                Assert.Equal(29, collectionStats.Collections["Suppliers"]);
+                Assert.Equal(8, collectionStats.Collections["@empty"]);
+
+                using (var session = store.OpenSession())
                 {
-                    await store.Smuggler.ImportAsync(new DatabaseSmugglerImportOptions(), stream);
+                    var order = session.Load<Order>("orders/1");
+                    Assert.NotNull(order);
 
-                    var stats = await store.Admin.SendAsync(new GetStatisticsOperation());
-
-                    Assert.Equal(1059, stats.CountOfDocuments);
-                    Assert.Equal(3, stats.CountOfIndexes); // there are 4 in ravendbdump, but Raven/DocumentsByEntityName is skipped
-
-                    var collectionStats = await store.Admin.SendAsync(new GetCollectionStatisticsOperation());
-                    Assert.Equal(1059, collectionStats.CountOfDocuments);
-                    Assert.Equal(9, collectionStats.Collections.Count);
-                    Assert.Equal(8, collectionStats.Collections["Categories"]);
-                    Assert.Equal(91, collectionStats.Collections["Companies"]);
-                    Assert.Equal(9, collectionStats.Collections["Employees"]);
-                    Assert.Equal(830, collectionStats.Collections["Orders"]);
-                    Assert.Equal(77, collectionStats.Collections["Products"]);
-                    Assert.Equal(4, collectionStats.Collections["Regions"]);
-                    Assert.Equal(3, collectionStats.Collections["Shippers"]);
-                    Assert.Equal(29, collectionStats.Collections["Suppliers"]);
-                    Assert.Equal(8, collectionStats.Collections["@empty"]);
-
-                    using (var session = store.OpenSession())
-                    {
-                        var order = session.Load<Order>("orders/1");
-                        Assert.NotNull(order);
-
-                        var metadata = session.Advanced.GetMetadataFor(order);
-                        Assert.False(metadata.ContainsKey("Raven-Entity-Name"));
-                        Assert.False(metadata.ContainsKey("Raven-Last-Modified"));
-                        Assert.False(metadata.ContainsKey("Last-Modified"));
-                    }
+                    var metadata = session.Advanced.GetMetadataFor(order);
+                    Assert.False(metadata.ContainsKey("Raven-Entity-Name"));
+                    Assert.False(metadata.ContainsKey("Raven-Last-Modified"));
+                    Assert.False(metadata.ContainsKey("Last-Modified"));
                 }
             }
         }
@@ -62,67 +63,112 @@ namespace SlowTests.Smuggler
         public async Task CanImportIndexesAndTransformers(string file)
         {
             using (var stream = GetType().GetTypeInfo().Assembly.GetManifestResourceStream(file))
+            using (var store = GetDocumentStore())
             {
                 Assert.NotNull(stream);
 
-                using (var store = GetDocumentStore())
+                await store.Admin.SendAsync(new StopIndexingOperation());
+
+                var operation = await store.Smuggler.ImportAsync(new DatabaseSmugglerImportOptions(), stream);
+
+                var result = operation.WaitForCompletion<SmugglerResult>();
+
+                var stats = await store.Admin.SendAsync(new GetStatisticsOperation());
+
+                Assert.Equal(0, stats.CountOfDocuments);
+
+                // not everything can be imported
+                // LoadDocument(key)
+                // Spatial
+
+                var unexpectedErrors = new List<string>();
+
+                foreach (var errorMessage in result.Messages)
                 {
-                    await store.Admin.SendAsync(new StopIndexingOperation());
+                    if (errorMessage.Contains("ERROR") == false)
+                        continue;
 
-                    var operation = await store.Smuggler.ImportAsync(new DatabaseSmugglerImportOptions(), stream);
-
-                    var result = operation.WaitForCompletion<SmugglerResult>();
-                    
-                    var stats = await store.Admin.SendAsync(new GetStatisticsOperation());
-
-                    Assert.Equal(0, stats.CountOfDocuments);
-
-                    // not everything can be imported
-                    // LoadDocument(key)
-                    // Spatial
-
-                    var unexpectedErrors = new List<string>();
-
-                    foreach (var errorMessage in result.Messages)
+                    if (errorMessage.Contains("No overload for method 'LoadDocument' takes 1 arguments"))
                     {
-                        if (errorMessage.Contains("ERROR") == false)
-                            continue;
-
-                        if (errorMessage.Contains("No overload for method 'LoadDocument' takes 1 arguments"))
-                        {
-                            // this.LoadDocument(student.Friends)
-                            continue;
-                        }
-
-                        if (errorMessage.Contains("The name 'AbstractIndexCreationTask' does not exist in the current context"))
-                        {
-                            // AbstractIndexCreationTask.SpatialGenerate(
-                            continue;
-                        }
-
-                        if (errorMessage.Contains("Cannot find analyzer type"))
-                        {
-                            continue;
-                        }
-
-                        if (errorMessage.Contains("The name 'SpatialIndex' does not exist in the current context"))
-                        {
-                            // SpatialIndex.Generate()
-                            continue;
-                        }
-
-                        unexpectedErrors.Add(errorMessage);
+                        // this.LoadDocument(student.Friends)
+                        continue;
                     }
 
-                    Assert.True(stats.CountOfIndexes >= 584, $"{stats.CountOfIndexes} >= 584. Errors: { string.Join($", {Environment.NewLine}", unexpectedErrors)}");
-                    Assert.True(stats.CountOfIndexes <= 658, $"{stats.CountOfIndexes} <= 658");
+                    if (errorMessage.Contains("The name 'AbstractIndexCreationTask' does not exist in the current context"))
+                    {
+                        // AbstractIndexCreationTask.SpatialGenerate(
+                        continue;
+                    }
 
-                    // not everything can be imported
-                    // LoadDocument(key)
-                    // Query
-                    // QueryOrDefault
+                    if (errorMessage.Contains("Cannot find analyzer type"))
+                    {
+                        continue;
+                    }
+
+                    if (errorMessage.Contains("The name 'SpatialIndex' does not exist in the current context"))
+                    {
+                        // SpatialIndex.Generate()
+                        continue;
+                    }
+
+                    unexpectedErrors.Add(errorMessage);
+                }
+
+                Assert.True(stats.CountOfIndexes >= 584, $"{stats.CountOfIndexes} >= 584. Errors: {string.Join($", {Environment.NewLine}", unexpectedErrors)}");
+                Assert.True(stats.CountOfIndexes <= 658, $"{stats.CountOfIndexes} <= 658");
+
+                // not everything can be imported
+                // LoadDocument(key)
+                // Query
+                // QueryOrDefault
+            }
+        }
+
+        [Theory]
+        [InlineData("SlowTests.Smuggler.Revisions_3.5.35220.ravendbdump")]
+        public async Task CanImportRevisions(string file)
+        {
+            using (var stream = GetType().GetTypeInfo().Assembly.GetManifestResourceStream(file))
+            using (var store = GetDocumentStore())
+            {
+                Assert.NotNull(stream);
+
+                await RevisionsHelper.SetupRevisions(Server.ServerStore, store.Database);
+
+                await store.Smuggler.ImportAsync(new DatabaseSmugglerImportOptions(), stream);
+
+                var stats = await store.Admin.SendAsync(new GetStatisticsOperation());
+                Assert.Equal(2, stats.CountOfDocuments);
+                Assert.Equal(5, stats.CountOfRevisionDocuments);
+                Assert.Equal(6, stats.LastDocEtag);
+
+                var collectionStats = await store.Admin.SendAsync(new GetCollectionStatisticsOperation());
+                Assert.Equal(2, collectionStats.CountOfDocuments);
+                Assert.Equal(2, collectionStats.Collections.Count);
+                Assert.Equal(1, collectionStats.Collections["@empty"]);
+                Assert.Equal(1, collectionStats.Collections["Users"]);
+
+                using (var session = store.OpenSession())
+                {
+                    var revisions = session.Advanced.GetRevisionsFor<User>("users/1");
+                    Assert.Equal(4, revisions.Count);
+
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        var metadata = session.Advanced.GetMetadataFor(revisions[i]);
+                        Assert.Equal("Users", metadata.GetString(Constants.Documents.Metadata.Collection));
+                        Assert.Equal($"RV:{4 - i}-AAAAAQAAAQAAAAAAAAAAAw", metadata.GetString(Constants.Documents.Metadata.ChangeVector));
+                        Assert.Equal(DocumentFlags.Revision.ToString(), metadata.GetString(Constants.Documents.Metadata.Flags));
+                        Assert.Equal("users/1", metadata.GetString(Constants.Documents.Metadata.Id));
+                    }
                 }
             }
+        }
+
+        private class User
+        {
+            public string Name { get; set; }
+            public string Version { get; set; }
         }
     }
 }
