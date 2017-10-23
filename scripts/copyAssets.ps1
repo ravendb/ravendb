a0845b4582739fa591d34a83001ad3acc8c8ab16
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-10-23 08:36:41 +0200

    RavenDB-9031 add no profile to start script

--- scripts/copyAssets.ps1
+++ scripts/copyAssets.ps1
@@ -20,7 +20,7 @@ function CopyStartCmd ( $targetDir ) {
     $startPs1Path = [io.path]::combine("scripts", "assets", "start.ps1")
     $startPs1 = Get-Content $startPs1Path
     $startPs1b64 = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($startPs1))
-    $startCmdContent = "@ECHO OFF`r`nPowerShell.exe -EncodedCommand $startPs1b64`r`n"
+    $startCmdContent = "@ECHO OFF`r`nPowerShell.exe -NoProfile -EncodedCommand $startPs1b64`r`n"
     $startCmdTargetPath = [io.path]::combine($targetDir, "start.cmd");
     Set-Content -Path $startCmdTargetPath $startCmdContent
 }
