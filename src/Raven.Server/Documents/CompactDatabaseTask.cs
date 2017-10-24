4dc2b0ea2e1ab8d39ed1f371ac501a15c8cf7de6
iftahbe <iftahbe@gmail.com>
2017-10-24 16:32:17 +0300

    RavenDB-8715 Unify / clarify compaction process

--- src/Raven.Server/Documents/CompactDatabaseTask.cs
+++ src/Raven.Server/Documents/CompactDatabaseTask.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Raven.Client.Documents.Operations;
@@ -9,7 +8,6 @@ using Raven.Server.Utils;
 using Sparrow;
 using Voron;
 using Voron.Exceptions;
-using Voron.Impl;
 using Voron.Impl.Compaction;
 
 namespace Raven.Server.Documents
@@ -19,6 +17,7 @@ namespace Raven.Server.Documents
         private readonly ServerStore _serverStore;
         private readonly string _database;
         private CancellationToken _token;
+        private bool _isCompactionInProgress;
 
         public CompactDatabaseTask(ServerStore serverStore, string database, CancellationToken token)
         {
@@ -27,22 +26,35 @@ namespace Raven.Server.Documents
             _token = token;
         }
 
-        public async Task<IOperationResult> Execute(Action<IOperationProgress> onProgress)
+        public async Task Execute(Action<IOperationProgress> onProgress)
         {
+            if (_isCompactionInProgress)
+                throw new InvalidOperationException($"Database '{_database}' cannot be compacted because compaction is already in progress.");
+
             var progress = new DatabaseCompactionProgress
             {
                 Message = $"Started database compaction for {_database}"
             };
             onProgress?.Invoke(progress);
-            DatabaseCompactionResult.Instance.SizeBeforeCompactionInMb = await CalculateStorageSizeInBytes(_database) / 1024 / 1024;
+
+            _isCompactionInProgress = true;
+
+            var documentDatabase = await _serverStore.DatabasesLandlord.TryGetOrCreateResourceStore(_database);
+            var configuration = _serverStore.DatabasesLandlord.CreateDatabaseConfiguration(_database);
 
             using (await _serverStore.DatabasesLandlord.UnloadAndLockDatabase(_database))
             {
-                var configuration = _serverStore.DatabasesLandlord.CreateDatabaseConfiguration(_database);
-
                 using (var src = DocumentsStorage.GetStorageEnvironmentOptionsFromConfiguration(configuration, new IoChangesNotifications(),
                     new CatastrophicFailureNotification(exception => throw new InvalidOperationException($"Failed to compact database {_database}", exception))))
                 {
+                    src.ForceUsing32BitsPager = configuration.Storage.ForceUsing32BitsPager;
+                    src.OnNonDurableFileSystemError += documentDatabase.HandleNonDurableFileSystemError;
+                    src.OnRecoveryError += documentDatabase.HandleOnRecoveryError;
+                    src.CompressTxAboveSizeInBytes = configuration.Storage.CompressTxAboveSize.GetValue(SizeUnit.Bytes);
+                    src.TimeToSyncAfterFlashInSec = (int)configuration.Storage.TimeToSyncAfterFlash.AsTimeSpan.TotalSeconds;
+                    src.NumOfConcurrentSyncsPerPhysDrive = configuration.Storage.NumberOfConcurrentSyncsPerPhysicalDrive;
+                    Sodium.CloneKey(out src.MasterKey, documentDatabase.MasterKey);
+
                     var basePath = configuration.Core.DataDirectory.FullPath;
                     IOExtensions.DeleteDirectory(basePath + "-Compacting");
                     IOExtensions.DeleteDirectory(basePath + "-old");
@@ -53,6 +65,14 @@ namespace Raven.Server.Documents
                         using (var dst = DocumentsStorage.GetStorageEnvironmentOptionsFromConfiguration(configuration, new IoChangesNotifications(),
                             new CatastrophicFailureNotification(exception => throw new InvalidOperationException($"Failed to compact database {_database}", exception))))
                         {
+                            dst.OnNonDurableFileSystemError += documentDatabase.HandleNonDurableFileSystemError;
+                            dst.OnRecoveryError += documentDatabase.HandleOnRecoveryError;
+                            dst.CompressTxAboveSizeInBytes = configuration.Storage.CompressTxAboveSize.GetValue(SizeUnit.Bytes);
+                            dst.ForceUsing32BitsPager = configuration.Storage.ForceUsing32BitsPager;
+                            dst.TimeToSyncAfterFlashInSec = (int)configuration.Storage.TimeToSyncAfterFlash.AsTimeSpan.TotalSeconds;
+                            dst.NumOfConcurrentSyncsPerPhysDrive = configuration.Storage.NumberOfConcurrentSyncsPerPhysicalDrive;
+                            Sodium.CloneKey(out dst.MasterKey, documentDatabase.MasterKey);
+
                             _token.ThrowIfCancellationRequested();
                             StorageCompaction.Execute(src, (StorageEnvironmentOptions.DirectoryStorageEnvironmentOptions)dst, progressReport =>
                             {
@@ -69,6 +89,15 @@ namespace Raven.Server.Documents
                         _token.ThrowIfCancellationRequested();
                         IOExtensions.MoveDirectory(basePath, basePath + "-old");
                         IOExtensions.MoveDirectory(basePath + "-Compacting", basePath);
+
+                        var oldIndexesPath = new PathSetting(basePath + "-old").Combine("Indexes");
+                        var newIndexesPath = new PathSetting(basePath).Combine("Indexes");
+                        IOExtensions.MoveDirectory(oldIndexesPath.FullPath, newIndexesPath.FullPath);
+
+                        var oldConfigPath = new PathSetting(basePath + "-old").Combine("Configuration");
+                        var newConfigPath = new PathSetting(basePath).Combine("Configuration");
+                        IOExtensions.MoveDirectory(oldConfigPath.FullPath, newConfigPath.FullPath);
+
                     }
                     catch (Exception e)
                     {
@@ -78,49 +107,10 @@ namespace Raven.Server.Documents
                     {
                         IOExtensions.DeleteDirectory(basePath + "-Compacting");
                         IOExtensions.DeleteDirectory(basePath + "-old");
+                        _isCompactionInProgress = false;
                     }
                 }
             }
-
-            DatabaseCompactionResult.Instance.SizeAfterCompactionInMb = await CalculateStorageSizeInBytes(_database) / 1024 / 1024;
-            return DatabaseCompactionResult.Instance;
-        }
-
-        public async Task<long> CalculateStorageSizeInBytes(string databaseName)
-        {
-            long sizeOnDiskInBytes = 0;
-
-            var database = await _serverStore.DatabasesLandlord.TryGetOrCreateResourceStore(databaseName);
-            var storageEnvironments = database?.GetAllStoragesEnvironment();
-            if (storageEnvironments != null)
-            {
-                foreach (var environment in storageEnvironments)
-                {
-                    Transaction tx = null;
-                    try
-                    {
-                        try
-                        {
-                            tx = environment?.Environment.ReadTransaction();
-                        }
-                        catch (OperationCanceledException)
-                        {
-                            continue;
-                        }
-                        var storageReport = environment?.Environment.GenerateReport(tx);
-                        if (storageReport == null)
-                            continue;
-
-                        var journalSize = storageReport.Journals.Sum(j => j.AllocatedSpaceInBytes);
-                        sizeOnDiskInBytes += storageReport.DataFile.AllocatedSpaceInBytes + journalSize;
-                    }
-                    finally
-                    {
-                        tx?.Dispose();
-                    }
-                }
-            }
-            return sizeOnDiskInBytes;
         }
     }
 }
