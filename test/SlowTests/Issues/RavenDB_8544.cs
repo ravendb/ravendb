a5e788024d41ebd8aae237236043c09032a9b028
Arkadiusz Palinski <arek@ravendb.net>
2017-10-25 12:59:29 +0200

    RavenDB-8544 Ensure we bump LastWorkTime only if we really did work

---
+++ test/SlowTests/Issues/RavenDB_8544.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.ServerWide;
+using Raven.Client.ServerWide.Operations;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_8544 : RavenTestBase
+    {
+        [Fact]
+        public async Task Tombstone_cleaner_must_not_prevent_from_unloading_idle_database()
+        {
+            DoNotReuseServer();
+
+            var name = "RavenDB_8544" + Guid.NewGuid();
+            
+            var landlord = Server.ServerStore.DatabasesLandlord;
+            
+            using (var store = GetDocumentStore())
+            {
+                var doc = new DatabaseRecord(name);
+
+                store.Admin.Server.Send(new CreateDatabaseOperation(doc));
+
+                try
+                {
+                    var database = await landlord.TryGetOrCreateResourceStore(name);
+
+                    database.Configuration.Core.RunInMemory = false; // in memory databases aren't unloaded
+
+                    database.ResetIdleTime();
+
+                    landlord.LastRecentlyUsed.AddOrUpdate(database.Name, DateTime.MinValue, (_, time) => DateTime.MinValue);
+
+                    foreach (var env in database.GetAllStoragesEnvironment())
+                        env.Environment.ResetLastWorkTime();
+
+                    await database.DocumentTombstoneCleaner.ExecuteCleanup();
+
+                    Server.ServerStore.IdleOperations(null);
+
+                    Assert.False(landlord.LastRecentlyUsed.TryGetValue(name, out _));
+                }
+                finally
+                {
+                    store.Admin.Server.Send(new DeleteDatabasesOperation(name, true));
+                }
+            }
+        }
+    }
+}
