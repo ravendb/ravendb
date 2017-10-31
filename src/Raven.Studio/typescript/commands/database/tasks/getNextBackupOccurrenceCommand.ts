a162af3a31edcfb2cebd38db5190a31e245d37f8
Grisha Kotler <grisha@ayende.com>
2017-10-31 19:28:04 +0200

    RavenDB-7937 - Backup timings

--- src/Raven.Studio/typescript/commands/database/tasks/getNextBackupOccurrenceCommand.ts
+++ src/Raven.Studio/typescript/commands/database/tasks/getNextBackupOccurrenceCommand.ts
@@ -2,13 +2,13 @@ import commandBase = require("commands/commandBase");
 import endpoints = require("endpoints");
 
 class getNextBackupOccurrenceCommand extends commandBase {
-    constructor(private date: string, private backupFrequency: string) {
+    constructor(private backupFrequency: string) {
         super();
     }
  
     execute(): JQueryPromise<Raven.Server.Web.System.NextBackupOccurrence> {
         const url = endpoints.global.databases.periodicBackupNextBackupOccurrence +
-            this.urlEncodeArgs({ date: this.date, backupFrequency: this.backupFrequency });
+            this.urlEncodeArgs({ backupFrequency: this.backupFrequency });
 
         return this.query(url, null);
     }
