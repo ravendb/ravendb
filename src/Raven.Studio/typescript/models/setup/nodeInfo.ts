e45a4b76c9641a69e8740a705f6e9a36269608a5
Marcin Lewandowski <marcin@ravendb.net>
2017-11-03 16:32:52 +0100

    RavenDB-9218 Setup app UI

--- src/Raven.Studio/typescript/models/setup/nodeInfo.ts
+++ src/Raven.Studio/typescript/models/setup/nodeInfo.ts
@@ -5,14 +5,13 @@ import ipEntry = require("models/setup/ipEntry");
 class nodeInfo {
     
     nodeTag = ko.observable<string>(); //TODO: do we need it
-    serverUrl = ko.observable<string>(); //TODO: do we need it?
-    publicServerUrl = ko.observable<string>(); //TODO: do we need it?
-    port = ko.observable<number>();
-    hostname = ko.observable<string>(); //TODO: is it needed? - hostname or hostnames?
-    certificate = ko.observable<string>();
     ips = ko.observableArray<ipEntry>([]);
+    port = ko.observable<number>();
+    certificate = ko.observable<string>();
+    certificatePassword = ko.observable<string>();
+    certificateFileName = ko.observable<string>();
     
-    
+    ipInput = ko.observable<string>();
 }
 
 export = nodeInfo;
