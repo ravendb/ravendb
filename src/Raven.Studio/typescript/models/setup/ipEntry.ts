6dbf15210b6929523e013983641503a21b99dfc1
Marcin Lewandowski <marcin@ravendb.net>
2017-11-03 15:55:53 +0100

    RavenDB-9236 Issues with ravendb setup

---
+++ src/Raven.Studio/typescript/models/setup/ipEntry.ts
@@ -0,0 +1,8 @@
+/// <reference path="../../../typings/tsd.d.ts"/>
+
+class ipEntry {
+    
+   ip = ko.observable<string>();
+}
+
+export = ipEntry;
--- src/Raven.Studio/typescript/viewmodels/server-setup/nodes.ts
+++ src/Raven.Studio/typescript/viewmodels/server-setup/nodes.ts
@@ -1,11 +1,26 @@
 import setupStep = require("viewmodels/server-setup/setupStep");
 import router = require("plugins/router");
+import nodeInfo = require("models/setup/nodeInfo");
 
 class nodes extends setupStep {
 
+    constructor() {
+        super();
+        
+        this.bindToCurrentInstance("removeNode");
+    }
+    
     save() {
         //TODO:
      
+    }
+    
+    addNode() {
+        this.model.nodes.push(new nodeInfo());
+    }
+
+    removeNode(node: nodeInfo) {
+        this.model.nodes.remove(node);
     }
 
 }
