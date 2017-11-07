43cc34a4880987c88ac3516e86fb01e7cea15d62
Marcin Lewandowski <marcin@ravendb.net>
2017-11-07 10:49:55 +0100

    RavenDB-9218 Setup app UI - listing CNs for certificate

---
+++ src/Raven.Studio/typescript/commands/setup/listHostsForCertificateCommand.ts
@@ -0,0 +1,34 @@
+import commandBase = require("commands/commandBase");
+import endpoints = require("endpoints");
+
+class listHostsForCertificateCommand extends commandBase {
+
+    constructor(private certificate: string, private password: string) {
+        super();
+    }
+
+    execute(): JQueryPromise<Array<string>> {
+        const url = endpoints.global.setup.setupHosts; 
+        const payload = {
+            Certificate: this.certificate,
+            Password: this.password
+        };
+
+        const task = $.Deferred<Array<string>>();
+        
+        this.post(url, JSON.stringify(payload), null)
+            .done(result => {
+                const cns = [];
+                cns.push(result.CN);
+                if (result.AlternativeNames) {
+                    cns.push(...result.AlternativeNames);
+                }
+                task.resolve(cns);
+            })
+            .fail((response: JQueryXHR) => this.reportError("Failed to fetch CNs from certificate", response.responseText, response.statusText));
+        
+        return task;
+    }
+}
+
+export = listHostsForCertificateCommand;
