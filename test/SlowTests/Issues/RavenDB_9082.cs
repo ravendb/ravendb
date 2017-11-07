6e35be2e7b0d9d0af4c4deacfaceceeb0fc2326a
Ayende Rahien <ayende@ayende.com>
2017-11-07 09:16:33 +0200

    RavenDB-9082 RQL complex project fails

---
+++ test/SlowTests/Issues/RavenDB_9082.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using FastTests;
+using Tests.Infrastructure;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_9082 : RavenTestBase
+    {
+        [Fact]
+        public void CanUseProjectionWithArrowFunctionAndStatementBody()
+        {
+            using (var store = GetDocumentStore())
+            {
+                store.Admin.Send(new CreateSampleDataOperation());
+
+                var queries = new[]
+                {
+                    @"from Orders as o
+where id() == 'orders/830'
+select {
+    TopLines: o.Lines.sort((a,b)=> b.PricePerUnit - a.PricePerUnit)
+        .map(a=>{ return { Name: a.ProductName } })
+        .splice(0, 2)
+}",
+                    @"from Orders as o
+where id() == 'orders/830'
+select {
+    Cost: o.Lines.reduce((a,l) => a + l.PricePerUnit, 0)
+}"
+                };
+
+                foreach (var query in queries)
+                {
+                    using (var s = store.OpenSession())
+                    {
+                        Assert.NotEmpty(s.Advanced.RawQuery<object>(query).ToList());
+                    }
+                }
+            }
+        }
+    }
+}
