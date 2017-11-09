a6c2c0dfa43f6babe7595a32e53333cbf11ac626
maxim <maxim@ayende.com>
2017-11-09 11:32:00 +0200

    Powershell backup fix

--- Raven.Tryouts/Program.cs
+++ Raven.Tryouts/Program.cs
@@ -8,6 +8,7 @@ using Raven.Abstractions.FileSystem;
 using Raven.Abstractions.Smuggler;
 using Raven.Database.Extensions;
 using Raven.Json.Linq;
+using Raven.Powershell;
 using Raven.Smuggler;
 using Raven.Tests.Common;
 using Raven.Tests.FileSystem;
@@ -32,90 +33,39 @@ namespace Raven.Tryouts
     {
         public static void Main(string[] args)
         {
-            var iterations = 50;
-            var sw = new Stopwatch();
-            TimeSpan testTime = new TimeSpan();
-            for (var i = 0; i < iterations; i++)
-            {
-                using (var testClass = new Basic())
-                {
-                    Console.WriteLine($"Starting test iteration {i}");
-                    sw.Restart();
-                    Environment.SetEnvironmentVariable("run", i.ToString());
-                    try
-                    {
-                        testClass.ClientShouldHandleLeaderShutdown(5);
-                        testTime = sw.Elapsed;
-                        Console.WriteLine($"Finished test iteration {i} within {testTime.TotalSeconds}s");
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine($"Failed test iteration {i} {Environment.NewLine}{e}");
-                    }
+            var backupCmdlet = new DatabaseBackupCmdlet();
 
-                }
-                Console.WriteLine($"Disposed of test  iteration {i} within {(sw.Elapsed - testTime).TotalMilliseconds}ms");
-            }
+            /*
+             
+               [Parameter(ValueFromPipelineByPropertyName = true, Mandatory = true, HelpMessage = "Url of RavenDB server, including the port. Example --> http://localhost:8080")]
+        public string ServerUrl { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true, Mandatory = true, HelpMessage = "Database name in RavenDB server")]
+        public string DatabaseName { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true, HelpMessage = "ApiKey to use when connecting to RavenDB Server. It should be full API key. Example --> key1/sAdVA0KLqigQu67Dxj7a")]
+        public string ApiKey { get; set; }       
+
+        [Parameter(ValueFromPipelineByPropertyName = true, HelpMessage = "If true, incremental backup. Otherwise, do a full backup.")]
+        public SwitchParameter Incremental
+        {
+            get { return incremental; }
+            set { incremental = value; }
+        }
+
+        [Parameter(ValueFromPipelineByPropertyName = true, Mandatory = true, HelpMessage = "Where to write the backup")]
+        public string BackupLocation { get; set; }
+             */
+            backupCmdlet.ServerUrl = "http://localhost:8080";
+            backupCmdlet.BackupLocation = "c:\\temp\\reproBackup";
+            backupCmdlet.Incremental = true;
+            backupCmdlet.DatabaseName = "VoronBackup";
+            while (backupCmdlet.Invoke().GetEnumerator().MoveNext());
         }
 
         public static async Task AsyncMain()
         {
-            //using (var store = new FilesStore()
-            //{
-            //    Url = "http://localhost:8080",
-            //    DefaultFileSystem = "FS1"
-            //}.Initialize())
-            //{
-            //    var ms = new MemoryStream();
-            //    var buffer = new byte[1024];
-
-            //    for (int i = 0; i < buffer.Length; i++)
-            //    {
-            //        buffer[i] = (byte)i;
-            //    }
-
-            //    ms.Write(buffer, 0, buffer.Length);
-            //    ms.Position = 0;
-            //    await store.AsyncFilesCommands.UploadAsync("file.txt", ms);
-
-            //    var tasks = new Task[50];
-
-            //    for (var i = 0; i < 15000; i += tasks.Length)
-            //    {
-            //        var sessio = store.OpenAsyncSession();
-
-            //        for (var j = 0; j < tasks.Length; j++)
-            //        {
-            //            var targetNAme = "file" + (i + j).ToString() + ".txt";
-            //            tasks[j] = store.AsyncFilesCommands.CopyAsync("file.txt", targetNAme);
-            //        }
-
-            //        Task.WaitAll(tasks);
-
-            //        Console.WriteLine(i);
-            //    }
-            //}
-
-            //var sp = Stopwatch.StartNew();
-
-            //var smugglerApi = new SmugglerFilesApi();
-
-
-            //await smugglerApi.ExportData(exportOptions: new SmugglerExportOptions<FilesConnectionStringOptions>()
-            //{
-            //    From = new FilesConnectionStringOptions()
-            //    {
-            //        DefaultFileSystem = "FS1",
-            //        Url = "http://localhost:8080",
-
-            //    },
-            //    ToFile = "c:\\Temp\\export.ravendump",
-
-            //});
-
-
-            //Console.WriteLine(sp.ElapsedMilliseconds);
-
+           
 
             var sp = Stopwatch.StartNew();
             try
