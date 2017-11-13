307f731424c91cc18e2de68789c59b7c4d05f303
Pawel Pekrol <ppekrol@ravendb.net>
2017-11-13 15:07:38 +0100

    RavenDB-9348 added configuration test

---
+++ test/FastTests/Issues/RavenDB_9348.cs
@@ -0,0 +1,23 @@
+﻿using Raven.Server.Config;
+using Raven.Server.ServerWide;
+using Xunit;
+
+namespace FastTests.Issues
+{
+    public class RavenDB_9348 : NoDisposalNeeded
+    {
+        [Fact]
+        public void IfConfigurationKeyIsStringArrayThenItShouldSupportValuesWithSemicolonPropely()
+        {
+            var configuration = new RavenConfiguration("Foo", ResourceType.Server);
+            configuration.SetSetting(RavenConfiguration.GetKey(x => x.Core.ServerUrls), "http://123.123.123.123:10105;http://123.123.123.124:10105");
+            configuration.SetSetting(RavenConfiguration.GetKey(x => x.Core.PublicServerUrl), "http://public.url.com");
+            configuration.Initialize();
+
+            Assert.NotNull(configuration.Core.ServerUrls);
+            Assert.Equal(2, configuration.Core.ServerUrls.Length);
+            Assert.Contains("http://123.123.123.123:10105", configuration.Core.ServerUrls);
+            Assert.Contains("http://123.123.123.124:10105", configuration.Core.ServerUrls);
+        }
+    }
+}
