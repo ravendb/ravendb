bcb6ddccf7385acd36355c380f63b824c51b6362
Grisha Kotler <grisha@ayende.com>
2017-11-14 15:05:02 +0200

    RavenDB-9401 - Last backup time is displayed although the backup failed

--- src/Raven.Server/Documents/DocumentDatabase.cs
+++ src/Raven.Server/Documents/DocumentDatabase.cs
@@ -477,7 +477,7 @@ namespace Raven.Server.Documents
                 [nameof(DatabaseInfo.IndexingErrors)] = IndexStore.GetIndexes().Sum(index => index.GetErrorCount()),
                 [nameof(DatabaseInfo.Alerts)] = NotificationCenter.GetAlertCount(),
                 [nameof(DatabaseInfo.UpTime)] = null, //it is shutting down
-                [nameof(DatabaseInfo.BackupInfo)] = PeriodicBackupRunner.GetBackupInfo(),
+                [nameof(DatabaseInfo.BackupInfo)] = PeriodicBackupRunner?.GetBackupInfo(),
                 [nameof(DatabaseInfo.MountPointsUsage)] = new DynamicJsonArray(GetMountPointsUsage().Select(x => x.ToJson())),
                 [nameof(DatabaseInfo.DocumentsCount)] = DocumentsStorage.GetNumberOfDocuments(),
                 [nameof(DatabaseInfo.IndexesCount)] = IndexStore.GetIndexes().Count(),
