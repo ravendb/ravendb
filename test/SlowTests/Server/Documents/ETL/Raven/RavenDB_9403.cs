ffb961123818920b7ccdf895f0fa84118092a703
Arkadiusz Palinski <arek@ravendb.net>
2017-11-14 14:12:56 +0100

    RavenDB-9403 RavenDB ETL has to create identifiers using '/' suffix when loading to different collection

---
+++ test/SlowTests/Server/Documents/ETL/Raven/RavenDB_9403.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Raven.Tests.Core.Utils.Entities;
+using Xunit;
+
+namespace SlowTests.Server.Documents.ETL.Raven
+{
+    public class RavenDB_9403 : EtlTestBase
+    {
+        [Fact]
+        public void Identifier_of_loaded_doc_should_not_be_created_using_cluster_identities()
+        {
+            using (var src = GetDocumentStore())
+            using (var dest = GetDocumentStore())
+            {
+                AddEtl(src, dest, "Users", "loadToPeople(this);");
+
+                var etlDone = WaitForEtl(src, (n, s) => s.LoadSuccesses > 0);
+
+                using (var session = src.OpenSession())
+                {
+                    session.Store(new User()
+                    {
+                        Name = "Joe Doe"
+                    });
+
+                    session.SaveChanges();
+                }
+
+                etlDone.Wait(TimeSpan.FromMinutes(1));
+
+                using (var session = dest.OpenSession())
+                {
+                    var person = session.Load<Person>("users/1-A/people/0000000000000000001-A");
+                    
+                    Assert.NotNull(person);
+                    Assert.Equal("Joe Doe", person.Name);
+                }
+            }
+        }
+    }
+}
