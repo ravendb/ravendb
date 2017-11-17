cb0fb5b22e8790c2bfdbe1a643bb95b0c0ebe57e
Marcin Lewandowski <marcin@ravendb.net>
2017-11-17 09:36:18 +0100

    RavenDB-9346 Studio does not compile when NodeJS 9.x is used

--- scripts/checkPrerequisites.ps1
+++ scripts/checkPrerequisites.ps1
@@ -23,7 +23,7 @@ function CheckPrerequisites () {
     $nodeVersion = node --version
     CheckLastExitCode
 
-    if ($($nodeVersion -match '^v?[678]') -eq $False) {
+    if ($($nodeVersion -match '^v?[6789]') -eq $False) {
         throw "Incompatible Node.js version. Must be 6 or later."
     }
 
