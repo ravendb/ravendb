49aaeabb3aa8865d2a77d43e4e64c8c2a56a0c93
maximbuyak <maxim@ayende.com>
2017-11-21 22:14:03 +0200

    RavenDB-9294 Subscription won't save Next batch starting point after restart -  added test

---
+++ test/SlowTests/Client/Subscriptions/RavenDB_9294.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.Documents.Subscriptions;
+using Raven.Tests.Core.Utils.Entities;
+using Sparrow;
+using Xunit;
+
+namespace SlowTests.Client.Subscriptions
+{
+    public class RavenDB_9294 : RavenTestBase
+    {
+        private readonly TimeSpan _reasonableWaitTime = Debugger.IsAttached ? TimeSpan.FromSeconds(60 * 10) : TimeSpan.FromSeconds(6);
+        [Fact]
+        public async Task NewConnectionWithNoWorkShouldNotResetClient()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User());
+                    await session.SaveChangesAsync();
+                }
+
+                var sn = store.Subscriptions.Create<User>();
+                var worker = store.Subscriptions.GetSubscriptionWorker<User>(sn);
+
+                var firstBatchHappened = new AsyncManualResetEvent();
+
+                worker.AfterAcknowledgment += async x => firstBatchHappened.Set();
+
+                _ = worker.Run(x => { });
+                Assert.True(await firstBatchHappened.WaitAsync(_reasonableWaitTime));
+                worker.Dispose();
+                worker = store.Subscriptions.GetSubscriptionWorker<User>(new SubscriptionWorkerOptions(sn)
+                {
+                    Strategy = SubscriptionOpeningStrategy.WaitForFree,
+                    TimeToWaitBeforeConnectionRetry = TimeSpan.FromMilliseconds(500)
+                });
+
+                var reconnectHappened = new AsyncManualResetEvent();
+
+                firstBatchHappened.Reset();
+                worker.OnSubscriptionConnectionRetry += () => reconnectHappened.Set();
+                _ = worker.Run(x => firstBatchHappened.Set());
+                Assert.False(await reconnectHappened.WaitAsync(TimeSpan.FromSeconds(6)),"Client reconnected");
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User());
+                    await session.SaveChangesAsync();
+                }
+
+                Assert.True(await firstBatchHappened.WaitAsync(_reasonableWaitTime), "First batch did not happen");
+                Assert.False(await reconnectHappened.WaitAsync(TimeSpan.FromSeconds(1)), "Client reconnected");
+            }
+        }
+
+    }
+}
