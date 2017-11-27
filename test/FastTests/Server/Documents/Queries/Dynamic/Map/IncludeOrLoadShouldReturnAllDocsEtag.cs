0131395be2269b02d659137b83e28d96c9ccea2c
Ayende Rahien <ayende@ayende.com>
2017-11-27 15:25:53 +0200

    Fixing tests

--- test/FastTests/Server/Documents/Queries/Dynamic/Map/IncludeOrLoadShouldReturnAllDocsEtag.cs
+++ test/FastTests/Server/Documents/Queries/Dynamic/Map/IncludeOrLoadShouldReturnAllDocsEtag.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace FastTests.Server.Documents.Queries.Dynamic.Map
 {
-    public class IncludeOrLoadShouldReturnAllDocsEtag
+    public class IncludeOrLoadShouldReturnAllDocsEtag : NoDisposalNeeded
     {
         [Fact]
         public void FunctionWithLoad()
