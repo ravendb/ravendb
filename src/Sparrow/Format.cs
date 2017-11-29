a27661a6d2e10b7538a1014beaffbee4c376372e
Federico Andres Lois <federico.lois@corvalius.com>
2017-11-29 15:57:48 -0300

    RavenDB-9575: Optimization of key generation routine under PUT document.

---
+++ src/Sparrow/Format.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Sparrow
+{
+    public static unsafe class Format
+    {
+        // PERF: Separated this code on purpose because I intend to optimize this further on vNext.
+        //       Similar code with extended functionality should be moved here to have the same treatment.
+        //       http://issues.hibernatingrhinos.com/issue/RavenDB-9588
+
+        public static class Backwards
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static void WriteNumber(char* ptr, ulong value)
+            {
+                int i = 0;
+
+                do
+                {
+                    var v = value % 10;
+
+                    ptr[i--] = (char)('0' + v);
+                    value /= 10;
+                }
+                while (value != 0);
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static void WriteNumber(byte* ptr, ulong value)
+            {
+                int i = 0;
+                do
+                {
+                    var v = value % 10;
+
+                    ptr[i--] = (byte)('0' + v);
+                    value /= 10;
+                }
+                while (value != 0);
+            }
+        }
+    }
+}
