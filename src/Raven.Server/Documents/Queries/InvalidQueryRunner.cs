ad0e757df2583a3c28bcfea3bb58c79460ba1422
Marko Lahma <marko.lahma@gmail.com>
2017-12-03 19:59:24 +0200

    RavenDB-9617 fix typo, reuse exception message

--- src/Raven.Server/Documents/Queries/InvalidQueryRunner.cs
+++ src/Raven.Server/Documents/Queries/InvalidQueryRunner.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Raven.Client.Documents.Operations;
 using Raven.Client.Documents.Queries;
+using Raven.Server.Config.Categories;
 using Raven.Server.ServerWide;
 using Raven.Server.ServerWide.Context;
 using Sparrow.Json;
@@ -12,35 +13,37 @@ namespace Raven.Server.Documents.Queries
 {
     public class InvalidQueryRunner : AbstractQueryRunner
     {
+        internal const string ErrorMessage = "Dynamic queries are not supported by this database because the configuration '" + nameof(IndexingConfiguration.DisableQueryOptimizerGeneratedIndexes) + "' is set to true and the query optimizer needs an index for this query";
+
         public InvalidQueryRunner(DocumentDatabase database) : base(database)
         {
         }
 
         public override Task<DocumentQueryResult> ExecuteQuery(IndexQueryServerSide query, DocumentsOperationContext documentsContext, long? existingResultEtag, OperationCancelToken token)
         {
-            throw new NotSupportedException("Dynamic queries are not supported by this database because the configuration 'Indexing.DisableQueryOptimizerGeneratedIndexes' is set to ture and the query optimizer needs an index for this query");
+            throw new NotSupportedException(ErrorMessage);
         }
 
         public override Task ExecuteStreamQuery(IndexQueryServerSide query, DocumentsOperationContext documentsContext, HttpResponse response, IStreamDocumentQueryResultWriter writer,
             OperationCancelToken token)
         {
-            throw new NotSupportedException("Dynamic queries are not supported by this database because the configuration 'Indexing.DisableQueryOptimizerGeneratedIndexes' is set to ture and the query optimizer needs an index for this query");
+            throw new NotSupportedException(ErrorMessage);
         }
 
         public override Task<IndexEntriesQueryResult> ExecuteIndexEntriesQuery(IndexQueryServerSide query, DocumentsOperationContext context, long? existingResultEtag, OperationCancelToken token)
         {
-            throw new NotSupportedException("Dynamic queries are not supported by this database because the configuration 'Indexing.DisableQueryOptimizerGeneratedIndexes' is set to ture and the query optimizer needs an index for this query");
+            throw new NotSupportedException(ErrorMessage);
         }
 
         public override Task<IOperationResult> ExecuteDeleteQuery(IndexQueryServerSide query, QueryOperationOptions options, DocumentsOperationContext context, Action<IOperationProgress> onProgress, OperationCancelToken token)
         {
-            throw new NotSupportedException("Dynamic queries are not supported by this database because the configuration 'Indexing.DisableQueryOptimizerGeneratedIndexes' is set to ture and the query optimizer needs an index for this query");
+            throw new NotSupportedException(ErrorMessage);
         }
 
         public override Task<IOperationResult> ExecutePatchQuery(IndexQueryServerSide query, QueryOperationOptions options, PatchRequest patch, BlittableJsonReaderObject patchArgs,
             DocumentsOperationContext context, Action<IOperationProgress> onProgress, OperationCancelToken token)
         {
-            throw new NotSupportedException("Dynamic queries are not supported by this database because the configuration 'Indexing.DisableQueryOptimizerGeneratedIndexes' is set to ture and the query optimizer needs an index for this query");
+            throw new NotSupportedException(ErrorMessage);
         }
     }
 }
