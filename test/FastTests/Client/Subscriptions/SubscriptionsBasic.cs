d5405f9712cb417687262ff9aaa02f7d04bcd81d
Fitzchak Yitzchaki <fitzchak@ayende.com>
2017-12-05 17:04:54 +0200

    RDoc-832 Remove code duplication: CreateDatabaseCommand

--- test/FastTests/Client/Subscriptions/SubscriptionsBasic.cs
+++ test/FastTests/Client/Subscriptions/SubscriptionsBasic.cs
@@ -1027,57 +1027,8 @@ namespace FastTests.Client.Subscriptions
 
             public RavenCommand<DatabasePutResult> GetCommand(DocumentConventions conventions, JsonOperationContext context)
             {
-                return new CreateDatabaseCommand(conventions, context, _databaseRecord, this);
-            }
-
-            private class CreateDatabaseCommand : RavenCommand<DatabasePutResult>
-            {
-                private readonly JsonOperationContext _context;
-                private readonly CreateDatabaseOperationWithoutNameValidation _createDatabaseOperation;
-                private readonly BlittableJsonReaderObject _databaseDocument;
-                private readonly string _databaseName;
-
-                public CreateDatabaseCommand(DocumentConventions conventions, JsonOperationContext context, DatabaseRecord databaseRecord,
-                    CreateDatabaseOperationWithoutNameValidation createDatabaseOperation)
-                {
-                    if (conventions == null)
-                        throw new ArgumentNullException(nameof(conventions));
-
-                    _context = context ?? throw new ArgumentNullException(nameof(context));
-                    _createDatabaseOperation = createDatabaseOperation;
-                    _databaseName = databaseRecord?.DatabaseName ?? throw new ArgumentNullException(nameof(databaseRecord));
-                    _databaseDocument = EntityToBlittable.ConvertEntityToBlittable(databaseRecord, conventions, context);
-                }
-                public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
-                {
-                    url = $"{node.Url}/admin/databases?name={_databaseName}";
-
-                    url += "&replication-factor=" + _createDatabaseOperation._replicationFactor;
-
-                    var request = new HttpRequestMessage
-                    {
-                        Method = HttpMethod.Put,
-                        Content = new BlittableJsonContent(stream =>
-                        {
-                            _context.Write(stream, _databaseDocument);
-                        })
-                    };
-
-                    return request;
-                }
-
-                public override void SetResponse(BlittableJsonReaderObject response, bool fromCache)
-                {
-                    if (response == null)
-                        ThrowInvalidResponse();
-
-                    Result = JsonDeserializationClient.DatabasePutResult(response);
-                }
-
-                public override bool IsReadRequest => false;
+                return new CreateDatabaseOperation.CreateDatabaseCommand(conventions, _databaseRecord, _replicationFactor);
             }
         }
-
     }
-
 }
