3932143be444c50a45bb37ce9bfdeffeae6107d1
Ayende Rahien <ayende@ayende.com>
2017-12-06 14:43:13 +0200

    Removing assert, it hides the context of the error and we'll get NRE anyway

--- src/Sparrow/Json/JsonOperationContext.cs
+++ src/Sparrow/Json/JsonOperationContext.cs
@@ -91,7 +91,6 @@ namespace Sparrow.Json
             if (_numberOfAllocatedStringsValues < _allocateStringValues.Count)
             {
                 var lazyStringValue = _allocateStringValues[_numberOfAllocatedStringsValues++];
-                Debug.Assert(lazyStringValue != null);
                 lazyStringValue.Renew(str, ptr, size);
                 return lazyStringValue;
             }
