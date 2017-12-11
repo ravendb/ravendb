8abe337bd0f9b802473206e48f0847372de88658
Marcin Lewandowski <marcin@ravendb.net>
2017-12-11 14:40:02 +0100

    RavenDB-9445 Revision studio files to drop cache on new version

--- src/Raven.Studio/typescript/commands/database/studio/getClientBuildVersionCommand.ts
+++ src/Raven.Studio/typescript/commands/database/studio/getClientBuildVersionCommand.ts
@@ -3,7 +3,10 @@ import commandBase = require("commands/commandBase");
 class getClientBuildVersionCommand extends commandBase {
 
     execute(): JQueryPromise<clientBuildVersionDto> {
-        return this.query("/studio/version.json", null);
+        const args = {
+            t: new Date().getTime()
+        };
+        return this.query("/studio/version.json", args);
     }
 }
 
