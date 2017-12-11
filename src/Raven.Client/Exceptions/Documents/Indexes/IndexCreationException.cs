6b4e614b319508df48236d6cf7888a4ef3cc25dc
talweiss1982 <tal@ayende.com>
2017-12-11 12:31:38 +0200

    RavenDB-8064 IndexCreationException to inherit from RavenException

--- src/Raven.Client/Exceptions/Documents/Indexes/IndexCreationException.cs
+++ src/Raven.Client/Exceptions/Documents/Indexes/IndexCreationException.cs
@@ -2,7 +2,7 @@
 
 namespace Raven.Client.Exceptions.Documents.Indexes
 {
-    public class IndexCreationException: Exception
+    public class IndexCreationException: RavenException
     {
         public IndexCreationException()
         {            
