79b2c20d47e586299c7abafb96cebf503a5f7fec
Ayende Rahien <Ayende@ayende.com>
2017-12-12 22:11:46 +0200

    Fixing bad merge

--- src/Raven.Server/ServerWide/ServerStore.cs
+++ src/Raven.Server/ServerWide/ServerStore.cs
@@ -14,6 +14,7 @@ using Lucene.Net.Search;
 using Raven.Client;
 using Raven.Client.Documents.Conventions;
 using Raven.Client.Documents.Session;
+using Raven.Client.Exceptions.Database;
 using Raven.Client.Util;
 using Raven.Client.Exceptions.Server;
 using Raven.Client.Extensions;
@@ -640,6 +641,22 @@ namespace Raven.Server.ServerWide
                         if (cert.TryGet("Thumbprint", out string certThumbprint) == false)
                             throw new InvalidOperationException("Invalid server cert value, expected to get Thumbprint property");
 
+                        if(cert.TryGet("Certificate", out string base64Cert) == false)
+                            throw new InvalidOperationException("Invalid server cert value, expected to get Certificate property");
+
+                        var certificate = new X509Certificate2(Convert.FromBase64String(base64Cert));
+
+                        var now = DateTime.UtcNow;
+                        if (certificate.NotBefore.ToUniversalTime() > now)
+                        {
+                            if (Logger.IsOperationsEnabled)
+                            {
+                                Logger.Operations($"Unable to confirm certificate update because the NotBefore property is set " +
+                                                  $"to {certificate.NotBefore.ToUniversalTime():O} and now it is {now:O}. Will try again later");
+                            }
+                            return;
+                        }
+
                         // we got it, now let us let the leader know about it
                         SendToLeaderAsync(new ConfirmReceiptServerCertificateCommand(certThumbprint));
                     }
@@ -1132,6 +1149,10 @@ namespace Raven.Server.ServerWide
                             {
                                 //we are disposing, so don't care
                             }
+                            catch (DatabaseDisabledException)
+                            {
+                                
+                            }
                         });
 
                     exceptionAggregator.Execute(() => _shutdownNotification.Dispose());
@@ -1192,8 +1213,14 @@ namespace Raven.Server.ServerWide
 
                         // intentionally inside the loop, so we get better concurrency overall
                         // since shutting down a database can take a while
-                        DatabasesLandlord.UnloadDatabaseIfDoneLoading(db, skipIfActiveInDuration: maxTimeDatabaseCanBeIdle,
-                            shouldSkip: database => database.Configuration.Core.RunInMemory)?.Dispose();
+                        if (resourceTask == null ||
+                            resourceTask.Result.Configuration.Core.RunInMemory)
+                            continue;
+                        var idleDbInstance = resourceTask.Result;
+                        if(SystemTime.UtcNow - DatabasesLandlord.LastWork(idleDbInstance)  < maxTimeDatabaseCanBeIdle)
+                            continue;
+
+                        DatabasesLandlord.UnloadDirectly(db);
                     }
 
                 }
