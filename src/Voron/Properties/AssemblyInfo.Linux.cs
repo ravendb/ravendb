1815f907c2511cc22a560417d6ed73777323ffaf
Pawel Pekrol <ppekrol@ravendb.net>
2017-12-13 11:28:00 +0100

    RavenDB-9041 when signing is disabled on non-Windows, we need to remove PublicKey from InternalsVisibleTo

---
+++ src/Voron/Properties/AssemblyInfo.Linux.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tests.Infrastructure")]
+[assembly: InternalsVisibleTo("FastTests")]
+[assembly: InternalsVisibleTo("SlowTests")]
+[assembly: InternalsVisibleTo("Voron.Benchmark")]
+[assembly: InternalsVisibleTo("Raven.Server")]
