4dbb15ac7cc1ecff918827c6b0db3d97d6a7b512
Pawel Pekrol <ppekrol@ravendb.net>
2017-12-13 11:28:00 +0100

    RavenDB-9041 when signing is disabled on non-Windows, we need to remove PublicKey from InternalsVisibleTo

---
+++ src/Sparrow/Properties/AssemblyInfo.Linux.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Raven.Client")]
+[assembly: InternalsVisibleTo("Raven.Server")]
+[assembly: InternalsVisibleTo("Tests.Infrastructure")]
+[assembly: InternalsVisibleTo("FastTests")]
