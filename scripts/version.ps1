13e8a71270e1d5f2c9e16fcfa765e00480328779
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-12-14 14:00:06 +0100

    RavenDB-9540 Prepare build scripts for RTM

--- scripts/version.ps1
+++ scripts/version.ps1
@@ -32,7 +32,7 @@ function GetBuildType () {
 }
 
 $RELEASE_INFO_FILE = 'artifacts/release-info.json'
-function SetVersionInfo() {
+function SetVersionInfo($projectDir) {
     $buildNumber = GetBuildNumber
     $buildType = GetBuildType
     $builtAt = [DateTime]::UtcNow
@@ -45,22 +45,24 @@ function SetVersionInfo() {
         $versionSuffix = "$buildType-$buildNumber"
     }
 
-    # TODO @gregolsky create a function for this
-    # - stable does not have label
-    # - increment stable version
+    $versionPrefix = GetCurrentVersionPrefix $projectDir
 
-    $version = "4.0.0-$versionSuffix"
+    $version = $versionPrefix
+    if ($buildType.ToLower() -ne 'stable') {
+        $version = "$versionPrefix-$versionSuffix"
+    }
 
     SetVersionEnvironmentVariableInTeamCity $version
     SetBuiltAtEnvironmentVariableInTeamCity $builtAt
     
     $versionInfo = @{ 
         Version = $version;
+        VersionPrefix = $versionPrefix;
         VersionSuffix = $versionSuffix;
         BuildNumber = $buildNumber;
         BuiltAt = $builtAt;
         BuiltAtString = $builtAtString;
-        BuildType = $buildType
+        BuildType = $buildType;
     }
 
     New-Item -Path $RELEASE_INFO_FILE -Force -Type File
@@ -72,4 +74,130 @@ function SetVersionInfo() {
 
 function GetVersionInfo() {
     return Get-Content -Path $RELEASE_INFO_FILE | ConvertFrom-Json
+}
+
+function BumpVersion ($projectDir, $versionPrefix, $buildType) {
+    if ($buildType.ToLower() -ne "stable") {
+        return
+    }
+
+    $repoOwner = "ravendb"
+    $repo = "ravendb"
+    $branch = "v4.0"
+    $remoteFilePath = 'src/CommonAssemblyInfo.cs'
+    
+    write-host "Build file URI for: $repoOwner/$($repo):$($remoteFilePath)"
+    $fileUri = GetGitHubFileUri $repoOwner $repo $remoteFilePath
+    $githubFileData = GetFileDataFromGitHub $fileUri $branch
+    $origFileContent = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($githubFileData.content))
+    
+    write-host "Calculate new version"
+    $newVersion = SemverMinor $version
+    write-host "New version is: $newVersion"
+    
+    write-host "Get updated file contents for $fileUri"
+    $assemblyInfoFileContent = GetAssemblyInfoWithBumpedVersion $origFileContent $newVersion
+
+    if (!$assemblyInfoFileContent) {
+        return
+    }
+    
+    $commitMessage = "Bump version to $newVersion"
+    
+    UpdateFileInGitHub $fileUri $assemblyInfoFileContent $commitMessage $branch $githubFileData
+
+    write-host "Bumped version in the repository $repoOwner/$repo ($branch) to $newVersion."
+}
+
+
+function GetGithubFileUri($repoOwner, $repoName, $filePath) {
+    if (!$repoOwner) {
+        throw "Repository owner is required."
+    }
+
+    if (!$repoName) {
+        throw "Repository name is required."
+    }
+
+    if (!$filePath) {
+        throw "File path to update is required."
+    }
+
+    $fileUri = "https://api.github.com/repos/$repoOwner/$repoName/contents/$filepath"
+    
+    $fileUri
+}
+
+function UpdateFileInGitHub($fileUri, $newFileContent, $commitMessage, $branch, $githubFileInfo) {
+
+    if (!$commitMessage) {
+        throw "Commit message is required."
+    }
+
+    if (!$env:GITHUB_USER -or !$env:GITHUB_ACCESS_TOKEN) {
+        throw "Environment variables holding GitHub credentials GITHUB_USER or GITHUB_ACCESS_TOKEN are not set."
+    }
+
+    if (!$branch) {
+        $branch = exec { & "git" rev-parse --abbrev-ref HEAD }
+    }
+    
+    $bodyJson = ConvertTo-Json @{
+        path = $githubFileInfo.path
+        sha = $githubFileInfo.sha
+        branch = $branch
+        message = $commitMessage
+        content = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($newFileContent))
+    }
+
+    $creds = "{0}:{1}" -f $env:GITHUB_USER,$env:GITHUB_ACCESS_TOKEN
+    $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($creds))
+    $headers = @{
+        Authorization = "Basic $encodedCreds"
+    }
+    
+    write-host "Update $fileUri file in GIT repo." 
+
+    (Invoke-WebRequest -TimeoutSec 120 -Uri $fileUri -Method PUT -Headers $headers -ContentType "application/json" -Body $bodyJson).content | ConvertFrom-Json
+    
+    write-host "Updated content under $fileUri"
+}
+
+function GetAssemblyInfoContent($fileUri, $branch) {
+    write-host "Getting $fileUri`?ref=$branch";
+    $fileContent = (Invoke-RestMethod -TimeoutSec 120 "$fileUri`?ref=$branch").content
+    [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($fileContent))
+}
+
+function GetFileDataFromGitHub($fileUri, $branch) {
+    $data = Invoke-RestMethod -TimeoutSec 120 "$fileUri`?ref=$branch"
+    $data
+}
+
+function GetAssemblyInfoWithBumpedVersion ($origFileContent, $newVersion) {
+    $assemblyInfoFileContents = $origFileContent |
+    Foreach-Object { $_ -replace '\[assembly: AssemblyVersion\(".*"\)\]', "[assembly: AssemblyVersion(""$newVersion"")]" } |
+    Foreach-Object { $_ -replace '\[assembly: AssemblyFileVersion\(".*"\)\]', "[assembly: AssemblyFileVersion(""$newVersion.40"")]" } |
+    Foreach-Object { $_ -replace '\[assembly: AssemblyInformationalVersion\(".*"\)\]', "[assembly: AssemblyInformationalVersion(""$newVersion"")]" } |
+    Out-String
+
+    "{0}`r`n" -f $assemblyInfoFileContents.TrimEnd()
+}
+
+function SemverMinor ($versionPrefix) {
+    $versionStrings = $versionPrefix.split('.')
+    $versionNumbers = foreach($number in $versionStrings) {
+            [int]::parse($number)
+    }
+
+    $versionNumbers[2] += 1
+    $newVersion = $versionNumbers -join '.'
+    
+    $newVersion
+}
+
+function GetCurrentVersionPrefix($projectDir) {
+    $commonAssemblyInfoFile = [io.path]::combine($projectDir, 'src', 'CommonAssemblyInfo.cs')
+    $match = select-string -Path $commonAssemblyInfoFile -Pattern 'AssemblyVersion\("(.*)"\)'
+    $match.Matches.Groups[1].Value
 }
