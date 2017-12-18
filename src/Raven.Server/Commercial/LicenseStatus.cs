eaae0968e9720b3fe2dd813ae8cecc2617e40cc4
Grisha Kotler <grisha@ayende.com>
2017-12-18 16:03:47 +0200

    RavenDB-9859 - Add support for maxClusterSize == 0, which means unlimited

--- src/Raven.Server/Commercial/LicenseStatus.cs
+++ src/Raven.Server/Commercial/LicenseStatus.cs
@@ -85,6 +85,8 @@ namespace Raven.Server.Commercial
                 {
                     case null:
                         return 1;
+                    case 0:
+                        return int.MaxValue;
                     default:
                         return maxClusterSize.Value;
                 }
@@ -119,7 +121,7 @@ namespace Raven.Server.Commercial
                 [nameof(ErrorMessage)] = ErrorMessage,
                 [nameof(MaxCores)] = MaxCores,
                 [nameof(MaxMemory)] = MaxMemory,
-                [nameof(MaxClusterSize)] = MaxClusterSize,
+                [nameof(MaxClusterSize)] = GetValue<int?>("maxClusterSize") ?? 1,
                 [nameof(Ratio)] = Ratio.ToString(),
                 [nameof(Expiration)] = Expiration,
                 [nameof(Expired)] = Expired,
