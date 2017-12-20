3d6688b45fb3a437d0313dc987b79679a4af50a1
Pawel Pekrol <ppekrol@ravendb.net>
2017-12-20 15:46:42 +0100

    RavenDB-9912 - added Raven/Backup/Periodic/Status to the list of documents to skip while importing - added error report for attachments and docs deletion - added test

---
+++ test/SlowTests/Issues/RavenDB_9912.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Documents.Smuggler;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_9912 : RavenTestBase
+    {
+        [Fact]
+        public async Task DocsAndAttachmentsDeletionsShouldBeProcessedCorrectly()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var stream = GetDump("RavenDB_9912.1.ravendbdump"))
+                {
+                    await store.Smuggler.ImportAsync(new DatabaseSmugglerImportOptions(), stream);
+                }
+
+                var stats = store.Maintenance.Send(new GetStatisticsOperation());
+
+                Assert.Equal(10 + 5 + 1, stats.CountOfDocuments); // 10 documents + 5 legacy attachment documents + 1 hilo
+                Assert.Equal(5, stats.CountOfAttachments);
+
+                using (var stream = GetDump("RavenDB_9912.2.ravendbdump"))
+                {
+                    await store.Smuggler.ImportAsync(new DatabaseSmugglerImportOptions(), stream);
+                }
+
+                stats = store.Maintenance.Send(new GetStatisticsOperation());
+
+                Assert.Equal(9 + 4 + 1, stats.CountOfDocuments); // 9 documents + 4 legacy attachment documents + 1 hilo
+                Assert.Equal(4, stats.CountOfAttachments);
+            }
+        }
+
+        private static Stream GetDump(string name)
+        {
+            var assembly = typeof(RavenDB_9912).Assembly;
+            return assembly.GetManifestResourceStream("SlowTests.Data." + name);
+        }
+    }
+}
