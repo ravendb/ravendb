05ff20002cdd5f64460e7df7d8c61d85fa066776
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-12-20 12:18:36 +0100

    RavenDB-9855 fix docker scripts; nanoserver part

--- docker/ravendb-ubuntu1604/run-raven.sh
+++ docker/ravendb-ubuntu1604/run-raven.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 COMMAND="./Raven.Server"
 export RAVEN_ServerUrl="http://$(hostname):8080"
-if [ ! -z "$RAVENDB_ARGS" ]; then
+if [ ! -z "$RAVEN_ARGS" ]; then
 	COMMAND="$COMMAND ${RAVEN_ARGS}"
 fi
 
