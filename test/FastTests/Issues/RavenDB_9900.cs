61eccefb565aa1a4c9c8154de8dcd696451796a5
Arkadiusz Palinski <arek@ravendb.net>
2017-12-20 11:04:11 +0100

    RavenDB-9900 Added test

---
+++ test/FastTests/Issues/RavenDB_9900.cs
@@ -0,0 +1,33 @@
+﻿using Raven.Server.Config;
+using Raven.Server.Config.Settings;
+using Raven.Server.ServerWide;
+using Xunit;
+
+namespace FastTests.Issues
+{
+    public class RavenDB_9900 : NoDisposalNeeded
+    {
+        [Fact]
+        public void Database_creation_using_relative_path_creates_directories_incorrectly()
+        {
+            var server = new RavenConfiguration(null, ResourceType.Server);
+
+            server.SetSetting(RavenConfiguration.GetKey(x => x.Core.RunInMemory), "true");
+            server.SetSetting(RavenConfiguration.GetKey(x => x.Core.DataDirectory), "RavenData");
+
+            server.Initialize();
+
+            var database = RavenConfiguration.CreateFrom(server, "foo", ResourceType.Database);
+
+            database.SetSetting(RavenConfiguration.GetKey(x => x.Core.RunInMemory), "true");
+            database.SetSetting(RavenConfiguration.GetKey(x => x.Core.DataDirectory), @"MyWork\MyDatabaseFolder");
+
+            database.Initialize();
+
+            Assert.Equal(new PathSetting("RavenData/MyWork/MyDatabaseFolder").FullPath, database.Core.DataDirectory.FullPath);
+            Assert.Equal(new PathSetting("RavenData/MyWork/MyDatabaseFolder/Indexes").FullPath, database.Indexing.StoragePath.FullPath);
+
+            Assert.Equal(new PathSetting("RavenData/MyWork/MyDatabaseFolder/Journal").FullPath, database.Core.DataDirectory.Combine("Journal").FullPath);
+        }
+    }
+}
