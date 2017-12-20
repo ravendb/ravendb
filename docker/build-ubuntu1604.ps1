c696099f8a13ea38c39b0f4876e650b458493575
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-12-20 11:20:17 +0100

    RavenDB-9855 DATA_DIR -> RAVEN_DataDir, copy settings when building image

--- docker/build-ubuntu1604.ps1
+++ docker/build-ubuntu1604.ps1
@@ -15,8 +15,11 @@ function BuildUbuntuDockerImage ( $projectDir, $version = "4.0.0-custom-40" ) {
     if ($(Test-Path $packagePath) -eq $False) {
         throw "Package file does not exist."
     }
-
+    
     Copy-Item -Destination ./ravendb-ubuntu1604/RavenDB.tar.bz2 -Force -Path $packagePath
+
+    $settingsPath = Join-Path -Path $projectDir -ChildPath "src\Raven.Server\Properties\Settings\settings.docker.posix.json"
+    Copy-Item -Path $settingsPath -Destination ./ravendb-ubuntu1604/settings.json -Force
 
     docker build ./ravendb-ubuntu1604 `
         -t ravendb/ravendb:$version-ubuntu.16.04-x64 `
