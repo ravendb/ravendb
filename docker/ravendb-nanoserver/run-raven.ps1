20fa97753e4293637bf2e7d971607cc9206921ce
Grzegorz Lachowski <gregolsky@ravendb.net>
2017-12-20 12:31:59 +0100

    RavenDB-9855 fix hostname in nanoserver; set tcp to 38888

--- docker/ravendb-nanoserver/run-raven.ps1
+++ docker/ravendb-nanoserver/run-raven.ps1
@@ -1,9 +1,11 @@
 $ErrorActionPreference='Stop'
 
 $COMMAND=".\Raven.Server.exe"
-$env:RAVEN_ServerUrl = "http://$(hostname):8080"
+$hostname = & "hostname.exe"
+$env:RAVEN_ServerUrl = "http://$($hostname):8080"
+
 if ([string]::IsNullOrEmpty($env:RAVEN_ARGS) -eq $False) {
-    $COMMAND = "$COMMAND $env:RAVEN_ARGS"
+    $COMMAND = "$COMMAND $($env:RAVEN_ARGS)"
 }
 
 Invoke-Expression -Command "$COMMAND";
