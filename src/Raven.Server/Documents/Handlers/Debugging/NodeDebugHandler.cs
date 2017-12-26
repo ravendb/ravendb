f6158d8384b1cee470659a4a8a810d640c9eb081
karmeli87 <karmeli87@gmail.com>
2017-12-26 11:30:08 +0200

    RavenDB-9952
    PR fixes

---
+++ src/Raven.Server/Documents/Handlers/Debugging/NodeDebugHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Raven.Server.Rachis;
+using Raven.Server.Routing;
+using Raven.Server.Web;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Server.Documents.Handlers.Debugging
+{
+    class NodeDebugHandler : RequestHandler
+    {
+        [RavenAction("/admin/debug/node/remote-connections", "GET", AuthorizationStatus.Operator, IsDebugInformationEndpoint = true)]
+        public Task ListRemoteConnections()
+        {
+            using (ServerStore.ContextPool.AllocateOperationContext(out JsonOperationContext context))
+            using (var write = new BlittableJsonTextWriter(context, ResponseBodyStream()))
+            {
+                context.Write(write,
+                    new DynamicJsonValue
+                    {
+                        ["Remote-Connections"] = new DynamicJsonArray(RemoteConnection.RemoteConnectionsList.ToList()
+                            .Select(connection => new DynamicJsonValue
+                            {
+                                [nameof(RemoteConnection.RemoteConnectionInfo.Caller)] = connection.Caller,
+                                [nameof(RemoteConnection.RemoteConnectionInfo.Destination)] = connection.Destination,
+                                [nameof(RemoteConnection.RemoteConnectionInfo.StartAt)] = connection.StartAt,
+                                ["Duration"] = DateTime.UtcNow - connection.StartAt,
+                                [nameof(RemoteConnection.RemoteConnectionInfo.Number)] = connection.Number,
+                            }))
+                    });
+                write.Flush();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
