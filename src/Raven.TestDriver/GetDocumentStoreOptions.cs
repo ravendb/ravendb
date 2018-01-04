804f72551a1a4ae5087c851a381a3923fa243de8
Pawel Pekrol <ppekrol@ravendb.net>
2018-01-04 23:17:34 +0100

    RavenDB-10071 introduced GetDocumentStoreOptions

---
+++ src/Raven.TestDriver/GetDocumentStoreOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Raven.TestDriver
+{
+    public class GetDocumentStoreOptions
+    {
+        internal static GetDocumentStoreOptions Default = new GetDocumentStoreOptions();
+
+        public TimeSpan? WaitForIndexingTimeout { get; set; }
+    }
+}
