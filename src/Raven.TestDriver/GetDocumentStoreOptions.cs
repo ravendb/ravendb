9cf4e0359651a9a8d268a044e7c901bd77db19fd
talweiss1982 <tal@ayende.com>
2018-01-07 11:44:07 +0200

    RavenDB-10083 modifying Default to be initiated lazily

--- src/Raven.TestDriver/GetDocumentStoreOptions.cs
+++ src/Raven.TestDriver/GetDocumentStoreOptions.cs
@@ -4,8 +4,8 @@ namespace Raven.TestDriver
 {
     public class GetDocumentStoreOptions
     {
-        internal static GetDocumentStoreOptions Default = new GetDocumentStoreOptions();
-
+        internal static GetDocumentStoreOptions Default => _default.Value;
+        private static readonly Lazy<GetDocumentStoreOptions> _default = new Lazy<GetDocumentStoreOptions>(() => new GetDocumentStoreOptions());
         public TimeSpan? WaitForIndexingTimeout { get; set; }
     }
 }
