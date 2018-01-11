a48ee898f3674b16db8be4c9ee9758b99681b540
Arkadiusz Palinski <arek@ravendb.net>
2018-01-11 16:10:25 +0100

    RavenDB-10143 Dynamic select distinct query returns duplicates

---
+++ test/SlowTests/Issues/RavenDB_10143.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using FastTests;
+using Raven.Client.Documents.Linq;
+using Raven.Tests.Core.Utils.Entities;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10143 : RavenTestBase
+    {
+        [Fact]
+        public void Dynamic_select_distinct_query()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User()
+                    {
+                        Name = "Arek"
+                    });
+                    
+                    session.Store(new User()
+                    {
+                        Name = "Arek"
+                    });
+                    
+                    session.SaveChanges();
+
+                    var results = session.Query<User>().Select(x => x.Name).Distinct().ToList();
+                    
+                    Assert.Equal(1, results.Count);
+                }
+            }
+        }
+    }
+}
