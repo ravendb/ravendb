6fc1824c9252f3cb2ccc591effe093108607e690
talweiss1982 <tal@ayende.com>
2018-01-15 17:42:27 +0200

    RavenDB-10188 Prevent recursive types from taking down the database due to stack overflow exception

---
+++ src/Raven.Server/Exceptions/RecursiveTypeNotSupported.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raven.Server.Exceptions
+{
+    public class RecursiveTypeNotSupported: Exception
+    {
+        public RecursiveTypeNotSupported(string message) : base(message)
+        {
+            
+        }
+    }
+}
