290d437ff2e21819e1b91e75057c4d2a421a0961
Danielle Greenberg <danielle.9897@gmail.com>
2018-01-17 12:04:58 +0200

    RavenDB-10205 getClientCertifiateCommand.ts - Missing failure handling

--- src/Raven.Studio/typescript/commands/auth/getClientCertificateCommand.ts
+++ src/Raven.Studio/typescript/commands/auth/getClientCertificateCommand.ts
@@ -5,9 +5,10 @@ class getClientCertifiateCommand extends commandBase {
 
     execute(): JQueryPromise<Raven.Client.ServerWide.Operations.Certificates.CertificateDefinition> {
         const url = endpoints.global.adminCertificates.certificatesWhoami;
-        return this.query(url, null, null);
+        
+        return this.query<Raven.Client.ServerWide.Operations.Certificates.CertificateDefinition>(url, null)
+            .fail((response: JQueryXHR) => this.reportError("Failed to get client certificate from server", response.responseText,  response.statusText));
     }
-
 }
 
 export = getClientCertifiateCommand;
