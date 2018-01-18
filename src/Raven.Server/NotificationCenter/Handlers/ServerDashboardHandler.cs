fb19efab2777a8c8d43a7f3131541650572ee8d6
Grisha Kotler <grisha@ayende.com>
2018-01-18 15:24:22 +0200

    RavenDB-10221 - make sure that we can show the server dashboard even if we got an error on the first sent info

--- src/Raven.Server/NotificationCenter/Handlers/ServerDashboardHandler.cs
+++ src/Raven.Server/NotificationCenter/Handlers/ServerDashboardHandler.cs
@@ -1,12 +1,16 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Raven.Server.Dashboard;
 using Raven.Server.Routing;
+using Sparrow.Logging;
 
 namespace Raven.Server.NotificationCenter.Handlers
 {
     public class ServerDashboardHandler : ServerNotificationHandlerBase
     {
+        private static readonly Logger Logger = LoggingSource.Instance.GetLogger<ServerDashboardHandler>("ServerDashboardHandler");
+
         [RavenAction("/server-dashboard/watch", "GET", AuthorizationStatus.ValidUser, SkipUsagesCount = true)]
         public async Task Get()
         {
@@ -15,16 +19,25 @@ namespace Raven.Server.NotificationCenter.Handlers
                 using (var writer = new NotificationCenterWebSocketWriter(webSocket, ServerStore.ServerDashboardNotifications, ServerStore.ContextPool, ServerStore.ServerShutdown))
                 {
                     var isValidFor = GetDatabaseAccessValidationFunc();
-                    var machineResources = MachineResourcesNotificationSender.GetMachineResources();
-                    await writer.WriteToWebSocket(machineResources.ToJson());
 
-                    using (var cts = CancellationTokenSource.CreateLinkedTokenSource(ServerStore.ServerShutdown))
+                    try
                     {
-                        var databasesInfo = DatabasesInfoNotificationSender.FetchDatabasesInfo(ServerStore, isValidFor, cts);
-                        foreach (var info in databasesInfo)
+                        var machineResources = MachineResourcesNotificationSender.GetMachineResources();
+                        await writer.WriteToWebSocket(machineResources.ToJson());
+
+                        using (var cts = CancellationTokenSource.CreateLinkedTokenSource(ServerStore.ServerShutdown))
                         {
-                            await writer.WriteToWebSocket(info.ToJson());
+                            var databasesInfo = DatabasesInfoNotificationSender.FetchDatabasesInfo(ServerStore, isValidFor, cts);
+                            foreach (var info in databasesInfo)
+                            {
+                                await writer.WriteToWebSocket(info.ToJson());
+                            }
                         }
+                    }
+                    catch (Exception e)
+                    {
+                        if (Logger.IsOperationsEnabled)
+                            Logger.Operations("Failed to send the initial server dashboard data", e);
                     }
 
                     await writer.WriteNotifications(isValidFor);
