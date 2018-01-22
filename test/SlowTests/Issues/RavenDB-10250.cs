d728337aacf30e41d574cd8db0c60b6574fe0a4f
Ayende Rahien <Ayende@ayende.com>
2018-01-22 09:02:18 +0200

    RavenDB-10250 Adding test

---
+++ test/SlowTests/Issues/RavenDB-10250.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FastTests;
+using Raven.Client.Documents.Commands.Batches;
+using Raven.Client.Documents.Session;
+using Sparrow.Json.Parsing;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10250 : RavenTestBase
+    {
+        public class User
+        {
+            public string Name;
+        }
+
+        [Fact]
+        public void CanUseBeforeStoreToAddNewDocsToSaveChanges()
+        {
+            using (var store = GetDocumentStore(new Options
+            {
+                ModifyDocumentStore = s=> {
+                    s.OnBeforeStore += (sender, args) =>
+                    {
+                        ((IDocumentSession)args.Session).Advanced.Defer(new PutCommandData(args.DocumentId + "/companion", null,
+                            new DynamicJsonValue
+                            {
+                                ["@metadata"] = new DynamicJsonValue
+                                {
+                                    ["@collection"] = "Friends"
+                                },
+                                ["Name"] = args.Entity.ToString()
+                            }));
+                    };
+                }
+            }))
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User(), "users/fred");
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var c = session.Load<dynamic>("users/fred/companion");
+                    Assert.NotNull(c.Name);
+                }
+            }
+        }
+    }
+}
