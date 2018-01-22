9a85fd5d9c55734dd52474816dbce5163bdd3d51
Pawel Pekrol <ppekrol@ravendb.net>
2018-01-22 13:24:17 +0100

    RavenDB-10212 - added store.Conventions.RequestTimeout - renamed SetRequestsTimeout to SetRequestTimeout to match the convention

---
+++ test/SlowTests/Issues/RavenDB_10212.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using FastTests;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10212 : RavenTestBase
+    {
+        [Fact]
+        public void CanSetRequestTimeoutInConventions()
+        {
+            var timeout1 = TimeSpan.FromSeconds(55);
+            var timeout2 = TimeSpan.FromSeconds(33);
+
+            using (var store = GetDocumentStore())
+            {
+                Assert.Null(store.Conventions.RequestTimeout);
+
+                var executor = store.GetRequestExecutor();
+                Assert.Null(executor.DefaultTimeout);
+            }
+
+            using (var store = GetDocumentStore(new Options
+            {
+                ModifyDocumentStore = documentStore => documentStore.Conventions.RequestTimeout = timeout1
+            }))
+            {
+                Assert.Equal(timeout1, store.Conventions.RequestTimeout);
+
+                var executor = store.GetRequestExecutor();
+                Assert.Equal(timeout1, executor.DefaultTimeout);
+
+                using (store.SetRequestTimeout(timeout2))
+                {
+                    executor = store.GetRequestExecutor();
+                    Assert.Equal(timeout2, executor.DefaultTimeout);
+
+                    executor = store.GetRequestExecutor("other");
+                    Assert.Equal(timeout1, executor.DefaultTimeout);
+                }
+
+                executor = store.GetRequestExecutor();
+                Assert.Equal(timeout1, executor.DefaultTimeout);
+            }
+        }
+    }
+}
