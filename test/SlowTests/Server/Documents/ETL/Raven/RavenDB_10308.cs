2518afdccc77f4c5100cebad891680a07833357d
Arkadiusz Palinski <arek@ravendb.net>
2018-01-29 12:44:29 +0100

    RavenDB-10308 When a script is empty Raven ETL should send all attachments. If there the script is provided we must exclude @attachments metadata since we don't provide the option to load attachments in the script at the moment. Throw if Jint metadata object update fails from some reasone.

---
+++ test/SlowTests/Server/Documents/ETL/Raven/RavenDB_10308.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using Raven.Client;
+using Raven.Client.Extensions.Streams;
+using Raven.Tests.Core.Utils.Entities;
+using Xunit;
+
+namespace SlowTests.Server.Documents.ETL.Raven
+{
+    public class RavenDB_10308 : EtlTestBase
+    {
+        [Fact]
+        public void Should_load_all_attachments_when_no_script_is_defined()
+        {
+            using (var src = GetDocumentStore())
+            using (var dest = GetDocumentStore())
+            {
+                AddEtl(src, dest, "Users", script: null);
+
+                var etlDone = WaitForEtl(src, (n, s) => s.LoadSuccesses > 0);
+
+                using (var session = src.OpenSession())
+                {
+                    session.Store(new User()
+                    {
+                        Name = "Joe Doe"
+                    }, "users/1");
+
+                    session.Advanced.Attachments.Store("users/1", "photo.jpg", new MemoryStream(new byte[] {1}));
+
+                    session.SaveChanges();
+                }
+
+                etlDone.Wait(TimeSpan.FromMinutes(1));
+
+                using (var session = dest.OpenSession())
+                {
+                    var user = session.Load<User>("users/1");
+
+                    Assert.NotNull(user);
+                    Assert.Equal("Joe Doe", user.Name);
+
+                    var attachment = session.Advanced.Attachments.Get("users/1", "photo.jpg");
+
+                    Assert.NotNull(attachment);
+                    Assert.Equal("photo.jpg", attachment.Details.Name);
+                    Assert.Equal(new byte[] {1}, attachment.Stream.ReadData());
+                }
+
+                etlDone.Reset();
+
+                using (var session = src.OpenSession())
+                {
+                    session.Delete("users/1");
+
+                    session.SaveChanges();
+                }
+
+                etlDone.Wait(TimeSpan.FromMinutes(1));
+
+                using (var session = dest.OpenSession())
+                {
+                    var user = session.Load<User>("users/1");
+
+                    Assert.Null(user);
+
+                    var attachment = session.Advanced.Attachments.Get("users/1", "photo.jpg");
+
+                    Assert.Null(attachment);
+                }
+            }
+        }
+
+        [Fact]
+        public void Should_not_send_attachments_metadata_when_using_script()
+        {
+            using (var src = GetDocumentStore())
+            using (var dest = GetDocumentStore())
+            {
+                AddEtl(src, dest, "Users", script: @"this.Name = 'James Doe';
+                                       loadToUsers(this);");
+
+                var etlDone = WaitForEtl(src, (n, s) => s.LoadSuccesses > 0);
+
+                using (var session = src.OpenSession())
+                {
+                    session.Store(new User()
+                    {
+                        Name = "Joe Doe"
+                    }, "users/1");
+
+                    session.Advanced.Attachments.Store("users/1", "photo.jpg", new MemoryStream(new byte[] {1}));
+
+                    session.SaveChanges();
+                }
+
+                etlDone.Wait(TimeSpan.FromMinutes(1));
+
+                using (var session = dest.OpenSession())
+                {
+                    var user = session.Load<User>("users/1");
+
+                    Assert.NotNull(user);
+                    Assert.Equal("James Doe", user.Name);
+
+                    var metatata = session.Advanced.GetMetadataFor(user);
+
+                    Assert.False(metatata.ContainsKey(Constants.Documents.Metadata.Attachments));
+                }
+            }
+        }
+    }
+}
