98d8b7590a817b423717f3485af8c1463ebd1d5d
Pawel Pekrol <ppekrol@ravendb.net>
2018-02-02 17:26:09 +0100

    RavenDB-10406 ServerWideOperation must use pulling, because we do not have server-wide Changes API available

--- src/Raven.Client/ServerWide/Operations/ServerWideOperation.cs
+++ src/Raven.Client/ServerWide/Operations/ServerWideOperation.cs
@@ -1,16 +1,39 @@
 ﻿using System;
-using Raven.Client.Documents.Changes;
+using System.Threading.Tasks;
 using Raven.Client.Documents.Conventions;
 using Raven.Client.Documents.Operations;
 using Raven.Client.Http;
+using Sparrow.Utils;
 
 namespace Raven.Client.ServerWide.Operations
 {
     public class ServerWideOperation : Operation
     {
-        public ServerWideOperation(RequestExecutor requestExecutor, Func<IDatabaseChanges> changes, DocumentConventions conventions, long id)
-            : base(requestExecutor, changes, conventions, id)
+        private bool _work;
+
+        public ServerWideOperation(RequestExecutor requestExecutor, DocumentConventions conventions, long id)
+            : base(requestExecutor, null, conventions, id)
         {
+            _work = true;
+        }
+
+        protected override async Task Process()
+        {
+            _work = true;
+
+            while (_work)
+            {
+                await FetchOperationStatus().ConfigureAwait(false);
+                if (_work == false)
+                    break;
+
+                await TimeoutManager.WaitFor(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+            }
+        }
+
+        protected override void StopProcessing()
+        {
+            _work = false;
         }
 
         protected override RavenCommand<OperationState> GetOperationStateCommand(DocumentConventions conventions, long id)
