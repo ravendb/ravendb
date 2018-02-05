2c386534edd40361e392c025ac307d880a45e9b6
Pawel Pekrol <ppekrol@ravendb.net>
2018-02-05 21:13:03 +0100

    RavenDB-7070 fixing namespace

--- test/SlowTests/MailingList/RavenDB-10438.cs
+++ test/SlowTests/MailingList/RavenDB-10438.cs
@@ -5,80 +5,83 @@ using Raven.Client.Documents.Indexes;
 using Raven.Client.Documents.Queries;
 using Xunit;
 
-public class RavenDB_10438: RavenTestBase
+namespace SlowTests.MailingList
 {
-    [Fact]
-    public void CanQuery()
+    public class RavenDB_10438 : RavenTestBase
     {
-        using (var store = GetDocumentStore())
+        [Fact]
+        public void CanQuery()
         {
-            store.ExecuteIndex(new TestDocumentByName());
-
-            using (var session = store.OpenSession())
+            using (var store = GetDocumentStore())
             {
-                session.Store(new Category
+                store.ExecuteIndex(new TestDocumentByName());
+
+                using (var session = store.OpenSession())
                 {
-                    Id = "categories/1",
-                    Name = new Dictionary<string, string>
+                    session.Store(new Category
                     {
-                        { "EN","category"},
-                        { "DE","Kategorie"},
-                        { "UA","kатегорія"}
-                    }
-                });
-                session.Store(new TestDocument
+                        Id = "categories/1",
+                        Name = new Dictionary<string, string>
+                        {
+                            { "EN","category"},
+                            { "DE","Kategorie"},
+                            { "UA","kатегорія"}
+                        }
+                    });
+                    session.Store(new TestDocument
+                    {
+                        Name = "TEST",
+                        Categories = new List<string> { { "categories/1" } }
+                    });
+
+                    session.SaveChanges();
+                }
+
+                WaitForIndexing(store);
+
+                using (var session = store.OpenSession())
                 {
-                    Name = "TEST",
-                    Categories = new List<string> { { "categories/1" } }
-                });
+                    var param = "A";
 
-                session.SaveChanges();
-            }
+                    var query = from item in session.Query<TestDocument>()
+                        let categories = RavenQuery.Load<Category>(item.Categories)
+                        select new
+                        {
+                            Name = item.Name,
+                            Nested = from cat in categories
+                                let name = cat.Name[param]
+                                let name2 = cat.Name[param]
+                                select new
+                                {
+                                    Name = name
+                                }
+                        };
 
-            WaitForIndexing(store);
-
-            using (var session = store.OpenSession())
-            {
-                var param = "A";
-                
-                var query = from item in session.Query<TestDocument>()
-                            let categories = RavenQuery.Load<Category>(item.Categories)
-                            select new
-                            {
-                                Name = item.Name,
-                                Nested = from cat in categories
-                                         let name = cat.Name[param]
-                                         let name2 = cat.Name[param]
-                                         select new
-                                         {
-                                             Name = name
-                                         }
-                            };
-
-                Assert.True(query.Any());
+                    Assert.True(query.Any());
+                }
             }
         }
-    }
 
-    public class TestDocumentByName : AbstractIndexCreationTask<TestDocument>
-    {
-        public TestDocumentByName()
+        public class TestDocumentByName : AbstractIndexCreationTask<TestDocument>
         {
-            Map = docs => from doc in docs select new { doc.Name };
-            Indexes.Add(x => x.Name, FieldIndexing.Search);
+            public TestDocumentByName()
+            {
+                Map = docs => from doc in docs select new { doc.Name };
+                Indexes.Add(x => x.Name, FieldIndexing.Search);
+            }
         }
-    }
 
-    public class TestDocument
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public IList<string> Categories { get; set; }
-    }
+        public class TestDocument
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public IList<string> Categories { get; set; }
+        }
 
-    public class Category
-    {
-        public string Id { get; set; }
-        public IDictionary<string, string> Name { get; set; } = new Dictionary<string, string>();
+        public class Category
+        {
+            public string Id { get; set; }
+            public IDictionary<string, string> Name { get; set; } = new Dictionary<string, string>();
+        }
     }
 }
