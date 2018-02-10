7f63616a0c76f1de5f1fc5e5ad208dd12c607003
Ayende Rahien <Ayende@ayende.com>
2018-02-10 04:03:00 -0800

    RavenDB-10475 - Fixing queries of negative numbers

---
+++ test/SlowTests/Issues/RavenDB-10475.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FastTests;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10475 : RavenTestBase
+    {
+        [Fact]
+        public void CanQueryNegativeNumbers()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var s = store.OpenSession())
+                {
+                    s.Advanced.RawQuery<dynamic>("from Users where Age > -1 and Size > -12.4")
+                        .ToList();
+                }
+            }
+        }
+    }
+}
