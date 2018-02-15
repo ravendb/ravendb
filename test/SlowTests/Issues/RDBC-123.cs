3cdda1b15f8a12f9e1e0b1f5c4d14ab99393eb1d
aviv86 <aviv@ayende.com>
2018-02-15 19:52:58 +0200

    RDBC-123 RQL : allow to query using an alias on properties of nested-object array

---
+++ test/SlowTests/Issues/RDBC-123.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using System.Linq;
+using FastTests;
+using FastTests.Server.Basic.Entities;
+using Newtonsoft.Json.Linq;
+using Raven.Tests.Core.Utils.Entities;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RDBC_123 : RavenTestBase
+    {
+        [Fact]
+        public void Can_Query_Using_an_Alias_on_Properties_of_Nested_Object_Array()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Order
+                    {
+                        ShipTo = new Address
+                        {
+                            City = "Paris",
+                            Country = "France"
+                        },
+                        Lines = new List<OrderLine>
+                        {
+                            new OrderLine
+                            {
+                                Product = "products/1-A"
+                            },
+                            new OrderLine
+                            {
+                                Product = "products/5-A"
+                            },
+                            new OrderLine
+                            {
+                                Product = "products/9-A"
+                            }
+                        }
+                    });
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var projection = session.Advanced.RawQuery<dynamic>("from Orders as o select o.ShipTo.City, o.Lines[].Product");
+
+                    var projectionResult = projection.ToList();
+
+                    Assert.Equal(1, projectionResult.Count);
+
+                    JObject res = projectionResult[0];
+                    var city = res["ShipTo.City"].Value<string>();
+                    var products = res["Lines[].Product"].Value<JArray>();
+
+                    Assert.Equal("Paris", city);
+                    Assert.Equal(3, products.Count);
+                    Assert.Equal("products/1-A", products[0]);
+                    Assert.Equal("products/5-A", products[1]);
+                    Assert.Equal("products/9-A", products[2]);
+
+                }
+            }
+        }
+    }
+}
