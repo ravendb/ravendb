d5c8989395a2ea9f07b9c6669cfc13945df16bd5
aviv86 <aviv@ayende.com>
2018-02-15 12:38:29 +0200

    RavenDB-10490 Linq to RQL : support loading via 'let' when the load-argument is also defined in a previous 'let' statement

---
+++ test/SlowTests/Issues/RavenDB-10490.cs
@@ -0,0 +1,66 @@
+using System.Linq;
+using FastTests;
+using Raven.Tests.Core.Utils.Entities;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10490 : RavenTestBase
+    {
+        [Fact]
+        public void Can_Load_Inside_Let_With_Argument_Predefined_in_Let()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User
+                    {
+                        Name = "Jerry",
+                        LastName = "Garcia",
+                    }, "users/1");
+                    session.Store(new Detail
+                    {
+                        Number = 15
+                    }, "details/1-A");
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var query = from u in session.Query<User>()
+                                let detailId = "details/1-A"
+                                let name = u.Name
+                                let detail = session.Load<Detail>(detailId)
+                                select new
+                                {
+                                    Name = name,
+                                    DetailId = detailId,
+                                    Detail = detail
+                                };
+
+                    Assert.Equal(
+@"declare function output(u) {
+	var detailId = ""details/1-A"";
+	var name = u.Name;
+	var detail = load(detailId);
+	return { Name : name, DetailId : detailId, Detail : detail };
+}
+from Users as u select output(u)", query.ToString());
+
+                    var queryResult = query.ToList();
+
+                    Assert.Equal(1, queryResult.Count);
+                    Assert.Equal("Jerry", queryResult[0].Name);
+                    Assert.Equal("details/1-A", queryResult[0].DetailId);
+                    Assert.Equal(15, queryResult[0].Detail.Number);
+
+                }
+            }
+        }
+        private class Detail
+        {
+            public int Number { get; set; }
+        }
+    }
+}
