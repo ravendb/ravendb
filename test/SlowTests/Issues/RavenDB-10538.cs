dacfba331f8d56bcff5e4e57f6a6b5644d1a4bbc
aviv86 <aviv@ayende.com>
2018-02-19 15:21:32 +0200

    RavenDB-10538 - range facets : support binary operations between T and Nullable<T>, where the non-nullable operand is a nested binary expression

---
+++ test/SlowTests/Issues/RavenDB-10538.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using FastTests;
+using Raven.Client.Documents.Queries.Facets;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10538 : RavenTestBase
+    {
+        private class Foo
+        {
+            public DateTime? DateIn { get; set; }
+        }
+
+        [Fact]
+        public void FacetWithNullableDateTime()
+        {
+            var now = DateTime.Now;
+            var facets = new List<RangeFacet>
+            {
+                new RangeFacet<Foo>
+                {
+                    Ranges = { c => c.DateIn < now - TimeSpan.FromDays(365)}
+                }
+            };
+        }
+    }
+}
