cbcfcc6f9388695746636aa00cc64d647f08a02a
Ayende Rahien <Ayende@ayende.com>
2018-02-21 12:26:12 +0200

    RavenDB-10571 Should have for in support for Blittable objects in Jint

---
+++ test/SlowTests/SlowTests/RavenDB-10571.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FastTests;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace SlowTests.SlowTests
+{
+    public class RavenDB_10571 : RavenTestBase
+    {
+        [Fact]
+        public void CanIterateOverBlittableProperties()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var s = store.OpenSession())
+                {
+                    s.Store(new { A = 1, B = 2 });
+                    s.SaveChanges();
+                }
+                using (var s = store.OpenSession())
+                {
+                    var item = s.Advanced.RawQuery<dynamic>(@"
+from @all_docs as e
+select {
+    Properties: Object.keys(e)
+}
+").First();
+                    Assert.Equal(new[] {"@metadata", "A", "B" }, ((JArray)item.Properties).Values<string>().ToArray());
+
+                }
+            }
+        }
+    }
+}
