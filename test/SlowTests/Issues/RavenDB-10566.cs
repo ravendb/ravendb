d5a85980a8ea4b86f4c0996a9c42ddf5789af14b
Ayende Rahien <Ayende@ayende.com>
2018-02-22 10:57:21 +0200

    Adding passing test

--- test/SlowTests/Issues/RavenDB-10566.cs
+++ test/SlowTests/Issues/RavenDB-10566.cs
@@ -11,11 +11,61 @@ namespace SlowTests.Issues
 {
     public class CustomerMetadataAfterSaveChanges : RavenTestBase
     {
+        public class Page
+        {
+            public string PageId;
+        }
         public class Item
         {
+            public Dictionary<string, Page> Trie = new Dictionary<string, Page>();
             public Dictionary<string, string> Items = new Dictionary<string, string>();
         }
+        [Fact]
+        public async Task CanPatchWithFilter()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new Item
+                    {
+                        Trie =
+                        {
+                            ["/"] = new Page{PageId = "home"},
+                            ["/my-url"] = new Page{PageId = "my url"},
+                        }
+                    }, "items/first");
+                    await session.SaveChangesAsync();
+                }
+                using (var session = store.OpenAsyncSession())
+                {
+                    session.Advanced.Defer(new PatchCommandData("items/first", null,
+                        new Raven.Client.Documents.Operations.PatchRequest
+                        {
+                            Script = @"
+var self = this;
+Object.keys(this.Trie).filter(function(key) {
+    if(self.Trie[key].PageId === args.PageId) {
+        delete self.Trie[key];
+    }
+});
+",
+                            Values =
+                            {
+                                ["PageId"] = "home",
+                            }
+                        }, null));
+                    await session.SaveChangesAsync();
+                }
 
+                using (var session = store.OpenAsyncSession())
+                {
+                    var item = await session.LoadAsync<Item>("items/first");
+                    Assert.Equal(1, item.Trie.Count);
+                    Assert.Equal("my url", item.Trie["/my-url"].PageId);
+                }
+            }
+        }
         [Fact]
         public async Task CanPatchDictionaryUsingArgs()
         {
