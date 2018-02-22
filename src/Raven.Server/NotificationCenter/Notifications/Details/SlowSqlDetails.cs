13759fbfb78d521c304e761aa11178c2b4bc82ff
Arkadiusz Palinski <arek@ravendb.net>
2018-02-22 14:12:18 +0100

    RavenDB-10596 Aggregate ETL error notifications - transformation, load errors, slow sqls

---
+++ src/Raven.Server/NotificationCenter/Notifications/Details/SlowSqlDetails.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Server.NotificationCenter.Notifications.Details
+{
+    public class SlowSqlDetails : INotificationDetails
+    {
+        public const int MaxNumberOfStatements = 500;
+
+        public Queue<SlowSqlStatementInfo> Statements { get; set; }
+
+        public SlowSqlDetails()
+        {
+            Statements = new Queue<SlowSqlStatementInfo>();
+        }
+
+        public void Add(SlowSqlStatementInfo slowSql)
+        {
+            Statements.Enqueue(slowSql);
+
+            if (Statements.Count > MaxNumberOfStatements)
+                Statements.TryDequeue(out _);
+        }
+
+        public DynamicJsonValue ToJson()
+        {
+            var result = new DynamicJsonValue();
+
+            var statements = new DynamicJsonArray();
+
+            foreach (var details in Statements)
+            {
+                statements.Add(details.ToJson());
+            }
+
+            result[nameof(Statements)] = statements;
+
+            return result;
+        }
+    }
+}
