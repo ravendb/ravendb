7d285f7095ebdadbfcf4e152bb1c266137f4e759
Arkadiusz Palinski <arek@ravendb.net>
2018-02-22 14:12:18 +0100

    RavenDB-10596 Aggregate ETL error notifications - transformation, load errors, slow sqls

---
+++ src/Raven.Server/NotificationCenter/Notifications/Details/EtlErrorsDetails.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Server.NotificationCenter.Notifications.Details
+{
+    public class EtlErrorsDetails : INotificationDetails
+    {
+        public const int MaxNumberOfErrors = 500;
+
+        public Queue<EtlErrorInfo> Errors { get; set; }
+
+        public EtlErrorsDetails()
+        {
+            Errors = new Queue<EtlErrorInfo>();
+        }
+
+        public void Add(EtlErrorInfo error)
+        {
+            Errors.Enqueue(error);
+
+            if (Errors.Count > MaxNumberOfErrors)
+                Errors.TryDequeue(out _);
+        }
+
+        public void Update(Queue<EtlErrorInfo> errors)
+        {
+            var local = new Queue<EtlErrorInfo>();
+
+            foreach (var existing in Errors)
+            {
+                var update = false;
+                foreach (var newError in errors)
+                {
+                    if (existing.DocumentId == newError.DocumentId)
+                    {
+                        update = true;
+                        break;
+                    }
+                }
+
+                if (update)
+                    continue;
+
+                local.Enqueue(existing);
+            }
+
+            foreach (var newError in errors)
+            {
+                local.Enqueue(newError);
+            }
+
+            Errors = local;
+
+            while (Errors.Count > MaxNumberOfErrors)
+            {
+                Errors.TryDequeue(out _);
+            }
+        }
+
+        public DynamicJsonValue ToJson()
+        {
+            var result = new DynamicJsonValue();
+
+            var errors = new DynamicJsonArray();
+
+            foreach (var details in Errors)
+            {
+                errors.Add(details.ToJson());
+            }
+
+            result[nameof(Errors)] = errors;
+
+            return result;
+        }
+    }
+}
---
+++ src/Raven.Server/NotificationCenter/Notifications/Details/SlowSqlStatementInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Server.NotificationCenter.Notifications.Details
+{
+    public class SlowSqlStatementInfo : IDynamicJsonValueConvertible
+    {
+        public long Duration { get; set; }
+        public DateTime Date { get; set; }
+        public string Statement { get; set; }
+
+        public DynamicJsonValue ToJson()
+        {
+            return new DynamicJsonValue
+            {
+                [nameof(Duration)] = Duration,
+                [nameof(Date)] = Date,
+                [nameof(Statement)] = Statement
+            };
+        }
+    }
+}
