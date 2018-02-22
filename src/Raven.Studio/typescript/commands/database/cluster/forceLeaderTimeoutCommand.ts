c7a7e692d3cd5bd400fb8220c6f97b9c63709aba
Marcin Lewandowski <marcin@ravendb.net>
2018-02-22 15:02:42 +0100

    RavenDB-9953 Cluster View - Force Timeout Command - Wrong Request URL

--- src/Raven.Studio/typescript/commands/database/cluster/forceLeaderTimeoutCommand.ts
+++ src/Raven.Studio/typescript/commands/database/cluster/forceLeaderTimeoutCommand.ts
@@ -3,10 +3,14 @@ import endpoints = require("endpoints");
 
 class forceLeaderTimeoutCommand extends commandBase {
 
+    constructor(private targetNode: string) {
+        super();
+    }
+    
     execute(): JQueryPromise<void> {
         const url = endpoints.global.rachisAdmin.adminClusterTimeout;
 
-        return this.post<void>(url, null, null, { dataType: undefined })
+        return this.post<void>(url, null, null, { dataType: undefined }, 9000, this.targetNode)
             .done(() => this.reportSuccess("Timeout was forced"))
             .fail((response: JQueryXHR) => this.reportError("Failed to force timeout", response.responseText));
     }
