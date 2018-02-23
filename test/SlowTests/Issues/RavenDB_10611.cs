0d15b9a6ccd2dfff29dcacc156e0e55338017550
Pawel Pekrol <ppekrol@ravendb.net>
2018-02-23 13:06:12 +0100

    RavenDB-10611 support for doing Counts on Distinct

---
+++ test/SlowTests/Issues/RavenDB_10611.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using FastTests;
+using Orders;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10611 : RavenTestBase
+    {
+        [Fact]
+        public void DistinctCountShouldWork()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Order
+                    {
+                        ShipTo = new Address
+                        {
+                            Country = "Poland"
+                        }
+                    });
+
+                    session.Store(new Order
+                    {
+                        ShipTo = new Address
+                        {
+                            Country = "USA"
+                        }
+                    });
+
+                    session.Store(new Order
+                    {
+                        ShipTo = new Address
+                        {
+                            Country = "Poland"
+                        }
+                    });
+
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var countries = session
+                        .Query<Order>()
+                        .OrderBy(x => x.ShipTo.Country)
+                        .Select(x => x.ShipTo.Country)
+                        .Distinct()
+                        .ToList();
+
+                    var count = session
+                        .Query<Order>()
+                        .OrderBy(x => x.ShipTo.Country)
+                        .Select(x => x.ShipTo.Country)
+                        .Distinct()
+                        .Count();
+
+                    Assert.Equal(2, countries.Count);
+                    Assert.Equal(countries.Count, count);
+                }
+            }
+        }
+    }
+}
