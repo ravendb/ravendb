18dca1ab416a9bbf5a32d0b6fa9d923c9c40187d
aviv86 <aviv@ayende.com>
2018-02-25 18:19:55 +0200

    RavenDB-10538: range facet : also support Call expressions with nullables, i.e : `x => x.DateVal >= bounds.Last()` (DateVal is DateTime?)

--- test/SlowTests/Issues/RavenDB-10538.cs
+++ test/SlowTests/Issues/RavenDB-10538.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 using FastTests;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Indexes;
@@ -71,6 +73,76 @@ namespace SlowTests.Issues
 
             }
 
+        }
+
+        [Fact]
+        public async Task TestGeneratedFacetsTest()
+        {
+            using (var store = GetDocumentStore())
+            {
+                new DocsIndex().Execute(store);
+                using (var session = store.OpenAsyncSession())
+                {
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        await session.StoreAsync(new Doc { Id = "doc-" + i, DateVal = new DateTime(2018, 1, i) });
+                    }
+                    await session.SaveChangesAsync();
+
+                    WaitForIndexing(store);
+
+                    var query = session.Query<Doc, DocsIndex>()
+                        .AggregateBy(builder => builder.ByRanges(
+                            x => x.DateVal < new DateTime(2018, 1, 1),
+                            x => x.DateVal >= new DateTime(2018, 1, 1) && x.DateVal < new DateTime(2018, 1, 2),
+                            x => x.DateVal >= new DateTime(2018, 1, 2) && x.DateVal < new DateTime(2018, 1, 3),
+                            x => x.DateVal >= new DateTime(2018, 1, 3) && x.DateVal < new DateTime(2018, 1, 4),
+                            x => x.DateVal >= new DateTime(2018, 1, 4) && x.DateVal < new DateTime(2018, 1, 5),
+                            x => x.DateVal >= new DateTime(2018, 1, 5)
+                            ));
+                    var results = await query.ExecuteAsync();
+                    var counts = results.First().Value.Values.Select(x => x.Count).ToArray();
+
+                    Assert.Equal(new[] { 0, 1, 1, 1, 1, 6 }, counts); // works for both DateTime? and DateTime
+
+                    var bounds = Enumerable.Range(1, 5).Select(day => new DateTime(2018, 1, day)).ToArray();
+
+                    var ranges = new Expression<Func<Doc, bool>>[] { x => x.DateVal < bounds[0] }
+                        .Concat(bounds.Zip(bounds.Skip(1), (start, end) => (Expression<Func<Doc, bool>>)(x => x.DateVal >= start && x.DateVal < end)))
+                        .Concat(new Expression<Func<Doc, bool>>[] { x => x.DateVal >= bounds.Last() })
+                        .ToArray();
+
+                    var generatedQuery = session.Query<Doc, DocsIndex>()
+                        .AggregateBy(builder => builder.ByRanges(
+                            ranges[0],
+                            ranges.Skip(1).ToArray()
+                            ));
+                    var generatedResults = await query.ExecuteAsync(); // throws for DateTime?
+                    var generatedCounts = results.First().Value.Values.Select(x => x.Count).ToArray();
+
+                    Assert.Equal(new[] { 0, 1, 1, 1, 1, 6 }, generatedCounts);
+                }
+            }
+        }
+
+        private class Doc
+        {
+            public string Id { get; set; }
+            public DateTime? DateVal { get; set; } // generated query works for DateTime, manual implementation works for both DateTime? and DateTime
+        }
+
+        private class DocsIndex : AbstractIndexCreationTask<Doc>
+        {
+            public DocsIndex()
+            {
+                Map = docs =>
+                    from doc in docs
+                    select new
+                    {
+                        doc.Id,
+                        doc.DateVal,
+                    };
+            }
         }
     }
 }
