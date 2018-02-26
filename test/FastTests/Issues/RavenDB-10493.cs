0e4beb99b14b29064fc4bced4bce123976a8e81f
Derek den Haas <derekdhaas@outlook.com>
2018-02-26 19:10:54 +0100

    RavenDB-10493: Implemented DateTime.MinValue and DateTime.MaxValue

---
+++ test/FastTests/Issues/RavenDB-10493.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace FastTests.Issues
+{
+    public class RavenDB_10493: RavenTestBase
+    {        
+        private class Article
+        {
+            public DateTime? DateTime;
+        }
+                        
+        [Fact]
+        public void CanHaveArrayInMetadata()
+        {             
+            using (var store = GetDocumentStore())
+            {             
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Article
+                    {
+                        DateTime = null
+                    });
+                    session.SaveChanges();
+                }
+                                
+                using (var session = store.OpenSession())
+                {
+                    var query = from x in session.Query<Article>()
+                                let test = 1
+                                select new
+                                {
+                                    DateTime = x.DateTime,
+                                    DateTimeMinValue = DateTime.MinValue,
+                                    DateTimeMaxValue = DateTime.MaxValue
+                                };
+
+                    Assert.Equal("declare function output(x) {\r\n\tvar test = 1;\r\n\treturn { DateTime : x.DateTime, DateTimeMinValue : new Date(-62135596800000), DateTimeMaxValue : new Date(253402297199999) };\r\n}\r\nfrom Articles as x select output(x)", query.ToString());
+
+                    var result = query.ToList();
+                    
+                    Assert.Equal(DateTime.MinValue, result[0].DateTimeMinValue);
+                    // Only missing 0.0009999 sec... 
+                    //Assert.Equal(DateTime.MaxValue, result[0].DateTimeMaxValue);
+
+                }                              
+            }
+        }
+                        
+    }
+}
