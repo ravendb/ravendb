e4264aa553a97f3fb3d4b9f99e33fb98e2d8a5dd
Derek den Haas <derekdhaas@gmail.com>
2018-02-27 11:53:12 +0100

    RavenDB-10625: Incorrect translation of null-coalescing
    Rewritten to use Conditional, (since this is being done normally by this function), which has Nested support.

---
+++ test/FastTests/Issues/RavenDB-10625.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FastTests;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10625: RavenTestBase
+    {        
+        private class Article
+        {
+            public int? Quantity { get; set; }
+        }
+                        
+        [Fact]
+        public void CanTranslateGroupsCorrectly()
+        {             
+            using (var store = GetDocumentStore())
+            {             
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Article
+                    {
+                        
+                    });
+                    session.SaveChanges();
+                }
+                                
+                using (var session = store.OpenSession())
+                {
+                    var query = from x in session.Query<Article>()
+                                let test = 1
+                                select new
+                                {
+                                    CheckGroup = ((x.Quantity ?? 0) != 0 ? 2 : 3) == 2 ? 1 : 0,
+                                    CheckGroup1 = (x.Quantity == null ? 1 : 2) == 1 ? 1 : 2,
+                                    CheckGroup2 = (x.Quantity ?? 0),
+                                    CheckGroup3 = x.Quantity ?? 0,
+                                    CheckGroup4 = ((x.Quantity ?? 0)) != 0 ? 2 : 3,
+                                    CheckGroup5 = x.Quantity != null ? x.Quantity : 0,
+                                }; 
+                    
+                    //Assert.Equal("from Articles as x select { HasProperties : x.Properties.length > 0 }", query.ToString());
+
+                    var result = query.ToList();
+                    
+                    Assert.Equal(1, result.Count);
+
+                }                              
+            }
+        }
+                        
+    }
+}
