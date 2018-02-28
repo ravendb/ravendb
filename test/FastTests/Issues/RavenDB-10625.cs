1428e55afafdfbd41a01adfb567b5b0a94d9bec4
Derek den Haas <derekdhaas@gmail.com>
2018-02-28 10:17:12 +0100

    [4.1] RavenDB-10625: Incorrect translation of grouped null-coalescing (#5390)
    * RavenDB-10625: Incorrect translation of null-coalescing
    Rewritten to use Conditional, (since this is being done normally by this function), which has Nested support.

---
+++ test/FastTests/Issues/RavenDB-10625.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FastTests;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10625: RavenTestBase
+    {        
+        private class Article
+        {
+            public int? Quantity { get; set; }
+        }
+                        
+        [Fact]
+        public void CanTranslateGroupsCorrectly()
+        {             
+            using (var store = GetDocumentStore())
+            {             
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Article
+                    {
+                        
+                    });
+                    session.SaveChanges();
+                }
+                                
+                using (var session = store.OpenSession())
+                {
+                    var query = from x in session.Query<Article>()
+                                let test = 1
+                                select new
+                                {
+                                    CheckGroup = ((x.Quantity ?? 0) != 0 ? 2 : 3) == 2 ? 1 : 0,
+                                    CheckGroup1 = (x.Quantity == null ? 1 : 2) == 1 ? 1 : 2,
+                                    CheckGroup2 = (x.Quantity ?? 0),
+                                    CheckGroup3 = x.Quantity ?? 0,
+                                    CheckGroup4 = ((x.Quantity ?? 0)) != 0 ? 2 : 3,
+                                    CheckGroup5 = x.Quantity != null ? x.Quantity : 0,
+                                }; 
+                    
+                    Assert.Equal("declare function output(x) {\r\n\tvar test = 1;\r\n\treturn { CheckGroup : ((x.Quantity!=null?x.Quantity:0)!==0?2:3)===2?1:0, CheckGroup1 : (x.Quantity==null?1:2)===1?1:2, CheckGroup2 : x.Quantity!=null?x.Quantity:0, CheckGroup3 : x.Quantity!=null?x.Quantity:0, CheckGroup4 : (x.Quantity!=null?x.Quantity:0)!==0?2:3, CheckGroup5 : x.Quantity!=null?x.Quantity:0 };\r\n}\r\nfrom Articles as x select output(x)", query.ToString());
+
+                    var result = query.ToList();
+                    
+                    Assert.Equal(1, result.Count);
+                    Assert.Equal(0, result[0].CheckGroup);
+                    Assert.Equal(1, result[0].CheckGroup1);
+                    Assert.Equal(0, result[0].CheckGroup2);
+                    Assert.Equal(0, result[0].CheckGroup3);
+                    Assert.Equal(3, result[0].CheckGroup4);
+                    Assert.Equal(0, result[0].CheckGroup5);
+
+                }                              
+            }
+        }
+                        
+    }
+}
