809b6fb57ba4fb6b29d0751f723eeebbe83bfbea
iftahbe <iftahbe@gmail.com>
2018-03-12 11:32:29 +0200

    RavenDB-10699 skip when not on CI machines

---
+++ test/Tests.Infrastructure/SetupWizardFactAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Raven.Client.Documents.Linq;
+using Xunit;
+
+namespace Tests.Infrastructure
+{
+    public class SetupWizardFactAttribute : FactAttribute
+    {
+        private readonly bool _enable;
+
+        public SetupWizardFactAttribute()
+        {
+            var variable = Environment.GetEnvironmentVariable("RAVEN.License.Path");
+
+            if (variable == null || (Environment.MachineName.Contains("scratch") == false && Environment.MachineName.Contains("ubuntu") == false))
+            {
+                //Skip = "Test can run only on CI scratch machines and if a license path was provided in environment variable RAVEN.License.Path";
+            }
+        }
+    }
+}
