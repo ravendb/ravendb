4cbf7d21949ef603f3d9ef947e28a1c32ea46db5
Michael Yarichuk <michael.yarichuk@gmail.com>
2018-03-12 19:31:34 +0200

    Add DontGenerateNewId to Raven.Powershell "Restore-Database" cmdlet

--- Raven.Powershell/BinaryRestoreDatabaseInClusterCmdlet.cs
+++ Raven.Powershell/BinaryRestoreDatabaseInClusterCmdlet.cs
@@ -19,6 +19,8 @@ namespace Raven.Powershell
     {
         private bool incremental;
 
+        private bool shouldGenerateNewId = true;
+
         [Parameter(ValueFromPipelineByPropertyName = true, Mandatory = true, HelpMessage = "Url of RavenDB server, including the port. Example --> http://localhost:8080")]
         public string ServerUrl { get; set; }
 
@@ -34,144 +36,189 @@ namespace Raven.Powershell
             get { return incremental; }
             set { incremental = value; }
         }
+        
+        [Parameter(ValueFromPipelineByPropertyName = true, HelpMessage = "If false, generate new low-level storage Id on restore. Otherwise, leave the old low-level storage Id in place.")]
+        public SwitchParameter DontGenerateNewId
+        {
+            get { return shouldGenerateNewId; }
+            set { shouldGenerateNewId = !value; }
+        }
 
         [Parameter(ValueFromPipelineByPropertyName = true, Mandatory = true, HelpMessage = "Where to write the backup")]
         public string BackupLocation { get; set; }
 
         protected override void ProcessRecord()
         {
-            using (var store = new DocumentStore
+            try
             {
-                Url = ServerUrl,
-                ApiKey = ApiKey
-            })
-            {
-                store.Initialize();
-                
-                var globalReplicationDestinationsJson = store.DatabaseCommands.Get(Constants.Global.ReplicationDestinationsDocumentName);
-                var globalReplicationDestinations = globalReplicationDestinationsJson.DataAsJson.JsonDeserialization<ReplicationDocument>();
-                var restoreOperations = new List<Tuple<Operation, IDocumentStore>>();
-                var replicationDestinations = globalReplicationDestinations.Destinations;
-                replicationDestinations.Add(new ReplicationDestination()
+                using (var store = new DocumentStore
                 {
-                    ApiKey = ApiKey,
-                    Url = ServerUrl
-                });
-
-                foreach (var node in replicationDestinations)
+                    Url = ServerUrl,
+                    ApiKey = ApiKey
+                })
                 {
-                    var curNodeStore = new DocumentStore
-                    {
-                        Url = node.Url,
-                        ApiKey = node.ApiKey
-                    }.Initialize();
+                    store.Initialize();
 
-                    var restoreOperation = curNodeStore.DatabaseCommands.GlobalAdmin.StartRestore(new DatabaseRestoreRequest
+                    var globalReplicationDestinationsJson =
+                        store.DatabaseCommands.Get(Constants.Global.ReplicationDestinationsDocumentName);
+
+                    if (globalReplicationDestinationsJson == null)
                     {
-                        BackupLocation = BackupLocation,
-                        DatabaseName = DatabaseName,
-                        DisableReplicationDestinations = true,
-                        GenerateNewDatabaseId = true
+                        throw new NotFoundException("Failed to fetch document id = " + Constants.Global.ReplicationDestinationsDocumentName);
+                    }
+
+                    var globalReplicationDestinations = globalReplicationDestinationsJson.DataAsJson
+                        .JsonDeserialization<ReplicationDocument>();
+                    var restoreOperations = new List<Tuple<Operation, IDocumentStore>>();
+                    var replicationDestinations = globalReplicationDestinations.Destinations;
+                    replicationDestinations.Add(new ReplicationDestination()
+                    {
+                        ApiKey = ApiKey,
+                        Url = ServerUrl
                     });
-                    restoreOperations.Add(Tuple.Create(restoreOperation, curNodeStore));
-                }
 
-                foreach (var restoreOperation in restoreOperations)
-                {
-                    restoreOperation.Item1.WaitForCompletion();
-                    restoreOperation.Item2.Dispose();
-                }
-
-                var databasesStatistics = new Dictionary<string, DatabaseStatistics>();
-
-                replicationDestinations.ForEach(x =>
-                {
-                    using (var curNodeStore = new DocumentStore
+                    foreach (var node in replicationDestinations)
                     {
-                        Url = x.Url,
-                        ApiKey = x.ApiKey,
-                        DefaultDatabase = DatabaseName
-                    }.Initialize())
-                    {
-                        var statistics = curNodeStore.DatabaseCommands.GetStatistics();
-                        databasesStatistics[x.Url] = statistics;
+                        var curNodeStore = new DocumentStore
+                        {
+                            Url = node.Url,
+                            ApiKey = node.ApiKey
+                        }.Initialize();
+
+                        var restoreOperation = curNodeStore.DatabaseCommands.GlobalAdmin.StartRestore(
+                            new DatabaseRestoreRequest
+                            {
+                                BackupLocation = BackupLocation,
+                                DatabaseName = DatabaseName,
+                                DisableReplicationDestinations = true,
+                                GenerateNewDatabaseId = shouldGenerateNewId
+                            });
+                        restoreOperations.Add(Tuple.Create(restoreOperation, curNodeStore));
                     }
-                });
 
-                foreach (var node in replicationDestinations)
-                {
-                    using (var curNodeStore = new DocumentStore
+                    foreach (var restoreOperation in restoreOperations)
                     {
-                        Url = node.Url,
-                        ApiKey = node.ApiKey,
-                        DefaultDatabase = DatabaseName
-                    }.Initialize())
-                    {
-                        using (var session = curNodeStore.OpenSession())
-                        {
-                            var sources = session.Advanced.LoadStartingWith<RavenJObject>("Raven/Replication/Sources/", pageSize: int.MaxValue);
-                            foreach (var ravenJObject in sources)
-                            {
-                                session.Delete(ravenJObject);
-                            }
-                            session.Delete("Raven/Replication/DatabaseIdsCache");
-                            session.SaveChanges();
-                        }
-
-                        using (var session = curNodeStore.OpenSession())
-                        {
-                            foreach (var replicationDestination in replicationDestinations.Where(x => x.Url != node.Url))
-                            {
-                                var curNodeStatistics = databasesStatistics[replicationDestination.Url];
-                                var replicationDestinationUrl = replicationDestination.Url;
-                                if (replicationDestinationUrl[replicationDestinationUrl.Length - 1] == '/')
-                                {
-                                    replicationDestinationUrl = replicationDestinationUrl.Substring(0, replicationDestinationUrl.Length - 1);
-                                }
-                                replicationDestinationUrl += "/databases/" + DatabaseName;
-
-                                session.Store(new SourceReplicationInformation
-                                {
-                                    Source = replicationDestinationUrl,
-                                    LastDocumentEtag = curNodeStatistics.LastDocEtag,
-                                    LastAttachmentEtag = curNodeStatistics.LastAttachmentEtag,
-                                    ServerInstanceId = curNodeStatistics.DatabaseId,
-                                    LastModified = SystemTime.UtcNow,
-                                    LastBatchSize = 0,
-                                    LastModifiedAtSource = DateTime.MinValue
-                                }, "Raven/Replication/Sources/"+ curNodeStatistics.DatabaseId);
-                            }
-                            session.SaveChanges();
-                        }
+                        restoreOperation.Item1.WaitForCompletion();
+                        restoreOperation.Item2.Dispose();
                     }
-                }
 
-                foreach (var node in replicationDestinations)
-                {
-                    using (var curNodeStore = new DocumentStore
+                    var databasesStatistics = new Dictionary<string, DatabaseStatistics>();
+
+                    replicationDestinations.ForEach(x =>
                     {
-                        Url = node.Url,
-                        ApiKey = node.ApiKey,
-                        DefaultDatabase = DatabaseName
-                    }.Initialize())
-                    {
-                        using (var session = curNodeStore.OpenSession())
+                        using (var curNodeStore = new DocumentStore
                         {
-                            var replicationDoc = session.Load<ReplicationDocument>(Constants.RavenReplicationDestinations);
-                            if (replicationDoc?.Destinations != null)
+                            Url = x.Url,
+                            ApiKey = x.ApiKey,
+                            DefaultDatabase = DatabaseName
+                        }.Initialize())
+                        {
+                            var statistics = curNodeStore.DatabaseCommands.GetStatistics();
+                            databasesStatistics[x.Url] = statistics;
+                        }
+                    });
+
+                    foreach (var node in replicationDestinations)
+                    {
+                        using (var curNodeStore = new DocumentStore
+                        {
+                            Url = node.Url,
+                            ApiKey = node.ApiKey,
+                            DefaultDatabase = DatabaseName
+                        }.Initialize())
+                        {
+                            using (var session = curNodeStore.OpenSession())
                             {
-                                foreach (var replicationDocDestination in replicationDoc.Destinations)
+                                var sources =
+                                    session.Advanced.LoadStartingWith<RavenJObject>("Raven/Replication/Sources/",
+                                        pageSize: int.MaxValue);
+                                foreach (var ravenJObject in sources)
                                 {
-                                    replicationDocDestination.Disabled = false;
+                                    session.Delete(ravenJObject);
                                 }
-                                session.Store(replicationDoc);
+
+                                session.Delete("Raven/Replication/DatabaseIdsCache");
+                                session.SaveChanges();
+                            }
+
+                            using (var session = curNodeStore.OpenSession())
+                            {
+                                foreach (var replicationDestination in replicationDestinations.Where(x =>
+                                    x.Url != node.Url))
+                                {
+                                    var curNodeStatistics = databasesStatistics[replicationDestination.Url];
+                                    var replicationDestinationUrl = replicationDestination.Url;
+                                    if (replicationDestinationUrl[replicationDestinationUrl.Length - 1] == '/')
+                                    {
+                                        replicationDestinationUrl =
+                                            replicationDestinationUrl.Substring(0,
+                                                replicationDestinationUrl.Length - 1);
+                                    }
+
+                                    replicationDestinationUrl += "/databases/" + DatabaseName;
+
+                                    session.Store(new SourceReplicationInformation
+                                        {
+                                            Source = replicationDestinationUrl,
+                                            LastDocumentEtag = curNodeStatistics.LastDocEtag,
+                                            LastAttachmentEtag = curNodeStatistics.LastAttachmentEtag,
+                                            ServerInstanceId = curNodeStatistics.DatabaseId,
+                                            LastModified = SystemTime.UtcNow,
+                                            LastBatchSize = 0,
+                                            LastModifiedAtSource = DateTime.MinValue
+                                        }, "Raven/Replication/Sources/" + curNodeStatistics.DatabaseId);
+                                }
+
                                 session.SaveChanges();
                             }
                         }
                     }
+
+                    foreach (var node in replicationDestinations)
+                    {
+                        using (var curNodeStore = new DocumentStore
+                        {
+                            Url = node.Url,
+                            ApiKey = node.ApiKey,
+                            DefaultDatabase = DatabaseName
+                        }.Initialize())
+                        {
+                            using (var session = curNodeStore.OpenSession())
+                            {
+                                var replicationDoc =
+                                    session.Load<ReplicationDocument>(Constants.RavenReplicationDestinations);
+                                if (replicationDoc?.Destinations != null)
+                                {
+                                    foreach (var replicationDocDestination in replicationDoc.Destinations)
+                                    {
+                                        replicationDocDestination.Disabled = false;
+                                    }
+
+                                    session.Store(replicationDoc);
+                                    session.SaveChanges();
+                                }
+                            }
+                        }
+                    }
+
                 }
-                  
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+    public class NotFoundException : Exception
+    {
+        public NotFoundException()
+        {
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
         }
     }
 }
