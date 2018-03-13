2eae3f64b896d84061b140bc772194d0363c5e8d
Grisha Kotler <grisha@ayende.com>
2018-03-13 19:35:54 +0200

    RavenDB-10720 - cannot parse the value of property @metadata.@last-modified

---
+++ test/FastTests/Issues/RavenDB-10720.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace FastTests.Issues
+{
+    public class RavenDB_10720 : RavenTestBase
+    {
+        [Fact]
+        public void CanSaveDocumentWithMetadata()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var entity = new User();
+                    session.Store(entity);
+                    var metadata = session.Advanced.GetMetadataFor(entity);
+                    metadata.Add("property--length-19", true);
+                    session.SaveChanges();
+                }
+            }
+        }
+
+        public class User
+        {
+            public string Id { get; set; }
+        }
+    }
+}
