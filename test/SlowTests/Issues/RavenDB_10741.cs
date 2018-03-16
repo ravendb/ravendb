8b7d1cc9ab811a4b4bcc8838c2764b6bf4e8c3a7
Marko Lahma <marko.lahma@gmail.com>
2018-03-16 08:30:21 +0200

    RavenDB-10741 session.Advanced.Lazily.Load throws when duplicate ids present

---
+++ test/SlowTests/Issues/RavenDB_10741.cs
@@ -0,0 +1,75 @@
+﻿using System.Threading.Tasks;
+using FastTests;
+using Xunit;
+
+namespace SlowTests.Client
+{
+    public class RavenDB_10741 : RavenTestBase
+    {
+        private class Document
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        [Fact]
+        public void LazyLoadingWithDuplicateIdsWhenAlreadyInSessionShouldSucceed()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var doc = new Document
+                {
+                    Name = "document"
+                };
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(doc);
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var loaded = session.Load<Document>(new [] { doc.Id, doc.Id });
+                    Assert.Equal(1, loaded.Count);
+                    Assert.Contains(doc.Id, loaded.Keys);
+
+                    loaded = session.Advanced.Lazily.Load<Document>(new [] { doc.Id, doc.Id }).Value;
+                    Assert.Equal(1, loaded.Count);
+                    Assert.Contains(doc.Id, loaded.Keys);
+                }
+            }
+        }
+        
+        [Fact]
+        public async Task AsyncLazyLoadingWithDuplicateIdsWhenAlreadyInSessionShouldSucceed()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var doc = new Document
+                {
+                    Name = "document"
+                };
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(doc);
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    var loaded = await session.LoadAsync<Document>(new [] { doc.Id, doc.Id });
+                    Assert.Equal(1, loaded.Count);
+                    Assert.Contains(doc.Id, loaded.Keys);
+
+                    var promise = session.Advanced.Lazily.LoadAsync<Document>(new [] { doc.Id, doc.Id });
+                    loaded = await promise.Value;
+                    Assert.Equal(1, loaded.Count);
+                    Assert.Contains(doc.Id, loaded.Keys);
+                }
+            }
+        }
+
+    }
+}
