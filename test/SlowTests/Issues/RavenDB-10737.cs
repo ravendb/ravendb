347176495c7da09b7fc38f9e117fa81f94d32313
aviv86 <aviv@ayende.com>
2018-03-18 16:03:51 +0200

    RavenDB-10737 : adding @expires as DateTime to metadata throws on SaveChanges

---
+++ test/SlowTests/Issues/RavenDB-10737.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using FastTests;
+using Raven.Client;
+using Sparrow.Extensions;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10737: RavenTestBase
+    {
+        private class SampleItem
+        {
+            public string Id { get; set; }
+        }
+
+        private const string _expires = Constants.Documents.Metadata.Expires;
+
+        [Fact]
+        public void Insert_with_Expirations()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var expirationDate = DateTime.UtcNow.AddDays(2);
+
+                    var item = new SampleItem();
+                    session.Store(item);
+                    session.Advanced.GetMetadataFor(item)[_expires] = expirationDate;
+                    session.SaveChanges();
+
+                    Assert.Equal(expirationDate.GetDefaultRavenFormat(),
+                                 session.Advanced.GetMetadataFor(item)[_expires]);
+                }
+            }
+        }
+
+        [Fact]
+        public void Insert_then_Load_and_Update_with_Expirations()
+        {
+            using (var store = GetDocumentStore())
+            {
+                string id;
+                using (var session = store.OpenSession())
+                {
+                    var item = new SampleItem();
+                    session.Store(item);
+                    session.SaveChanges();
+                    id = item.Id;
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var expirationDate = DateTime.UtcNow.AddDays(2);
+
+                    var item = session.Load<SampleItem>(id);
+                    session.Advanced.GetMetadataFor(item)[_expires] = expirationDate;
+                    session.SaveChanges();
+
+                    Assert.Equal(expirationDate.GetDefaultRavenFormat(), 
+                                 session.Advanced.GetMetadataFor(item)[_expires]);
+                }
+            }
+        }
+
+
+        [Fact]
+        public void Insert_then_Load_and_Update_with_Expirations_DateTimeOffset()
+        {
+            using (var store = GetDocumentStore())
+            {
+                string id;
+                using (var session = store.OpenSession())
+                {
+                    var item = new SampleItem();
+                    session.Store(item);
+                    session.SaveChanges();
+                    id = item.Id;
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var expirationDate = DateTimeOffset.UtcNow.AddDays(2);
+
+                    var item = session.Load<SampleItem>(id);
+                    session.Advanced.GetMetadataFor(item)[_expires] = expirationDate;
+                    session.SaveChanges();
+
+                    Assert.Equal(expirationDate.UtcDateTime.GetDefaultRavenFormat(),
+                                 session.Advanced.GetMetadataFor(item)[_expires]);
+                }
+            }
+        }
+    }
+}
