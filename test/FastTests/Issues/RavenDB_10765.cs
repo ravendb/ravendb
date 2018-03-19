09820bac3c110d4f11ddad8ac80bc4abe56ed2d0
Tomasz Opalach <tomtom@ravendb.net>
2018-03-19 14:25:48 +0100

    RavenDB-10765 fix illegal path error in tests

---
+++ test/FastTests/Issues/RavenDB_10765.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Raven.Server.Config.Settings;
+using Tests.Infrastructure;
+using Xunit;
+
+namespace FastTests.Issues
+{
+    public class RavenDB_10765 : NoDisposalNeeded
+    {
+        [LinuxTheory]
+        [InlineData("appdrive:raven")]
+        [InlineData("~/raven")]
+        [InlineData("$HOME/raven")]
+        public void GivenPathStartingWithIllegalKeywordShouldThrowArgumentException(string path)
+        {
+            try
+            {
+                new PathSetting(path);
+                Assert.True(false, "Should have thrown ArgumentException");
+            }
+            catch (ArgumentException)
+            {
+            }
+        }
+    }
+}
