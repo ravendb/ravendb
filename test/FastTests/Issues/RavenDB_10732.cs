f6bc46ba25fad8606810861c723c59f4b13a0813
Arkadiusz Palinski <arek@ravendb.net>
2018-03-21 09:31:51 +0100

    RavenDB-10732 Added test that verifies we can check memory usage

---
+++ test/FastTests/Issues/RavenDB_10732.cs
@@ -0,0 +1,14 @@
+﻿using Sparrow.LowMemory;
+using Xunit;
+
+namespace FastTests.Issues
+{
+    public class RavenDB_10732 : NoDisposalNeeded
+    {
+        [Fact]
+        public void Can_check_memory_status()
+        {
+            LowMemoryNotification.Instance.CheckMemoryStatus();
+        }
+    }
+}
