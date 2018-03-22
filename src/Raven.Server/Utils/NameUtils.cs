7f6e48a2f73fa1038b462817b7241274cda7d485
Pawel Pekrol <ppekrol@ravendb.net>
2018-03-22 11:05:33 +0100

    RavenDB-8354 - Index names (static) must match following regex: ([A-Za-z0-9_\/\-\.]+) - Resources (databases) must match following regex: ([A-Za-z0-9_\-\.]+)

---
+++ src/Raven.Server/Utils/NameUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Raven.Server.Utils
+{
+    internal static class NameUtils
+    {
+        public const string ValidResourceNameCharacters = @"([A-Za-z0-9_\-\.]+)";
+
+        public const string ValidIndexNameCharacters = @"([A-Za-z0-9_\/\-\.]+)";
+
+        private static readonly Regex ValidIndexNameCharactersRegex = new Regex(ValidIndexNameCharacters, RegexOptions.Compiled);
+
+        private static readonly Regex ValidResourceNameCharactersRegex = new Regex(ValidIndexNameCharacters, RegexOptions.Compiled);
+
+        public static bool IsValidIndexName(string name)
+        {
+            var result = ValidIndexNameCharactersRegex.Matches(name);
+
+            return result.Count != 0 && result[0].Value == name;
+        }
+
+        public static bool IsValidResourceName(string name)
+        {
+            var result = ValidResourceNameCharactersRegex.Matches(name);
+
+            return result.Count != 0 && result[0].Value == name;
+        }
+    }
+}
