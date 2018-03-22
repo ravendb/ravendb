d662355847aae0ab00a71f5f3ecd7f747cb91fc5
Pawel Pekrol <ppekrol@ravendb.net>
2018-03-22 11:05:33 +0100

    RavenDB-8354 - Index names (static) must match following regex: ([A-Za-z0-9_\/\-\.]+) - Resources (databases) must match following regex: ([A-Za-z0-9_\-\.]+)

--- src/Raven.Server/Documents/Indexes/IndexStore.cs
+++ src/Raven.Server/Documents/Indexes/IndexStore.cs
@@ -541,6 +541,9 @@ namespace Raven.Server.Documents.Indexes
 
             if (isStatic && name.StartsWith("Auto/", StringComparison.OrdinalIgnoreCase))
                 throw new ArgumentException($"Index name '{name}' not permitted. Static index name cannot start with 'Auto/'", nameof(name));
+
+            if (isStatic && NameUtils.IsValidIndexName(name) == false)
+                throw new ArgumentException($"Index name '{name}' not permitted. Index name must math '{NameUtils.ValidIndexNameCharacters}' regular expression", nameof(name));
         }
 
         public Index ResetIndex(string name)
