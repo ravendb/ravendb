48e4ea919125e8befd0afe1d0dfb83265b4d0d01
Pawel Pekrol <ppekrol@ravendb.net>
2018-03-27 13:55:26 +0200

    RavenDB-8427 added test

---
+++ test/SlowTests/Issues/RavenDB_8427.cs
@@ -0,0 +1,28 @@
+﻿using FastTests;
+using Sparrow.Json;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_8427 : NoDisposalNeeded
+    {
+        [Fact]
+        public void ShouldCleanEscapePositionWhenTakenFromCache()
+        {
+            using (var pool = new JsonContextPool())
+            {
+                using (pool.AllocateOperationContext(out var context))
+                {
+                    var lazyString = context.GetLazyString("abc \b cba");
+                    Assert.NotNull(lazyString.EscapePositions);
+                }
+
+                using (pool.AllocateOperationContext(out var context))
+                {
+                    var lazyString = context.GetLazyString("abc");
+                    Assert.Null(lazyString.EscapePositions);
+                }
+            }
+        }
+    }
+}
