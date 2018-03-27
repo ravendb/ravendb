d7008506bb7d7c7d8dc0412e4e3e921d1571a047
Idan Haim Shalom <haimdude@gmail.com>
2018-03-27 16:11:56 +0300

    RavenDB-10837 Fix session.Advanced.WaitForIndexesAfterSaveChanges

---
+++ Raven.Tests.Issues/RavenDB_10837.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Tests.Common;
+using Xunit;
+
+namespace Raven.Tests.Issues
+{
+    public class RavenDB_10837: RavenTest
+    {
+        [Fact]
+        public void CanWaitForIndex()
+        {
+            var documentStore = NewDocumentStore();
+
+            using (var session = documentStore.OpenSession())
+            {
+                session.Store(new Entity
+                {
+                    Id = "Entity/1"
+                });
+                session.Advanced.WaitForIndexesAfterSaveChanges();
+                session.SaveChanges();
+            }
+
+            using (var session = documentStore.OpenSession())
+            {
+                var entities = session.Query<Entity>().ToList();
+
+                Assert.NotEmpty(entities);
+            }
+        }
+    }
+}
