16a8e16fe0560e9a66776abbc43dde270898da83
Pawel Pekrol <ppekrol@ravendb.net>
2018-03-27 10:18:27 +0200

    RavenDB-10835 before checking in Null should be applied, we need to unwrap the expression if it is ParenthesizedExpression

---
+++ test/SlowTests/Issues/RavenDB_10835.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using FastTests;
+using Raven.Client.Documents.Indexes;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10835 : RavenTestBase
+    {
+        [Fact]
+        public void Nullable_DateTime_And_DynamicNullObject_Conversion()
+        {
+            using (var store = GetDocumentStore())
+            {
+                new Index_test().Execute(store);
+            }
+        }
+
+        private class Index_test : AbstractMultiMapIndexCreationTask<Result>
+        {
+            public Index_test()
+            {
+                AddMap<Item>(items => from item in items
+                                      select new
+                                      {
+                                          Id = item.Id,
+                                          CreatedAt = item.CreatedAt != null
+                                              ? (DateTime?)DateTime.ParseExact(item.CreatedAt,
+                                                  "yyyy-MM-dd",
+                                                  CultureInfo.InvariantCulture, DateTimeStyles.None)
+                                              : null
+                                      });
+            }
+        }
+
+        private class Item
+        {
+            public string Id { get; set; }
+            public string CreatedAt { get; set; }
+        }
+
+        private class Result
+        {
+            public string Id { get; set; }
+            public DateTime? CreatedAt { get; set; }
+        }
+    }
+}
