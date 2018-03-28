2e9ad3cf0bbd7607e70b4965da84069e2540e237
aviv86 <aviv@ayende.com>
2018-03-28 13:56:24 +0300

    RavenDB-10846 : fix failing test PromoteDatabaseNodeBackAfterReconnection

--- test/RachisTests/DatabaseCluster/ClusterDatabaseMaintenance.cs
+++ test/RachisTests/DatabaseCluster/ClusterDatabaseMaintenance.cs
@@ -194,7 +194,10 @@ namespace RachisTests.DatabaseCluster
         {
             var clusterSize = 3;
             var databaseName = "PromoteDatabaseNodeBackAfterReconnection";
-            var leader = await CreateRaftClusterAndGetLeader(clusterSize, false, 0);
+            var leader = await CreateRaftClusterAndGetLeader(clusterSize, false, 0, customSettings: new Dictionary<string, string>
+            {
+                [RavenConfiguration.GetKey(x => x.Cluster.MoveToRehabGraceTime)] = "4"
+            });
             using (var store = new DocumentStore
             {
                 Urls = new[] { leader.WebUrl },
