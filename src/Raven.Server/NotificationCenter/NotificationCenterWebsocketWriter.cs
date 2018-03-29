d17d8289de4b1a9e94b3635f5040bd79db7ef696
karmeli87 <karmeli87@gmail.com>
2018-03-29 17:00:20 +0300

    RavenDB-10008 Go to real leader for cluster topology

--- src/Raven.Server/NotificationCenter/NotificationCenterWebsocketWriter.cs
+++ src/Raven.Server/NotificationCenter/NotificationCenterWebsocketWriter.cs
@@ -32,7 +32,7 @@ namespace Raven.Server.NotificationCenter
             _ms.Dispose();
         }
 
-        public async Task WriteNotifications(Func<string, bool> shouldWriteByDb, Action firstTime = null)
+        public async Task WriteNotifications(Func<string, bool> shouldWriteByDb)
         {
             var receiveBuffer = new ArraySegment<byte>(new byte[1024]);
             var receive = _webSocket.ReceiveAsync(receiveBuffer, _resourceShutdown);
@@ -43,7 +43,6 @@ namespace Raven.Server.NotificationCenter
             {
                 using (_notificationsBase.TrackActions(asyncQueue, this))
                 {
-                    firstTime?.Invoke();
                     while (_resourceShutdown.IsCancellationRequested == false)
                     {
                         // we use this to detect client-initialized closure
