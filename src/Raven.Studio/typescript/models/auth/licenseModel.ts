907aaf03cc4db3917bb8a9c9f3a5939640d6b84c
Grisha <grisha@ayende.com>
2018-04-03 16:06:50 +0300

    RavenDB-10714 - About page: Expose License ID in the UI

--- src/Raven.Studio/typescript/models/auth/licenseModel.ts
+++ src/Raven.Studio/typescript/models/auth/licenseModel.ts
@@ -60,6 +60,15 @@ class licenseModel {
         return `${status.MaxCores} Cores, ${maxMemory}, Max cluster size: ${maxClusterSize}`;
     });
 
+    static licenseId = ko.pureComputed(() => {
+        const status = licenseModel.licenseStatus();
+        if (!status || status.Type === "None") {
+            return null;
+        }
+
+        return status.Id;
+    });
+
     static developerLicense = ko.pureComputed(() => {
         const licenseStatus = licenseModel.licenseStatus();
         
