b8ea0a55edc70e91ab7b4eefc089722c1de406c6
Ayende Rahien <Ayende@ayende.com>
2018-04-04 11:27:23 +0300

    RavenDB-10499 Agressive caching broken when mixed with non-aggressive loads

---
+++ test/SlowTests/Issues/RavenDB-10499.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FastTests;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10499 : RavenTestBase
+    {
+        private class Document
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        [Fact]
+        public void CanLoadAgressivelyCachingAfterDbInitialized()
+        {
+            using (var store = GetDocumentStore())
+            {
+                const string id = "myDocuments/123";
+
+                using (var session = store.OpenSession())
+                {
+                    var loaded = session.Load<Document>(id);
+                    if (loaded == null)
+                    {
+                        var doc = new Document
+                        {
+                            Id = id,
+                            Name = "document"
+                        };
+                        session.Store(doc);
+                        session.SaveChanges();
+                    }
+                }
+
+                // now that program has started it's safe to use agressive caching as database is in valid state
+
+                using (var session = store.OpenSession())
+                {
+                    using (store.AggressivelyCacheFor(TimeSpan.FromMinutes(1)))
+                    {
+                        var loaded = session.Load<Document>(id);
+                        Assert.NotNull(loaded);
+                    }
+                }
+            }
+        }
+    }
+}
