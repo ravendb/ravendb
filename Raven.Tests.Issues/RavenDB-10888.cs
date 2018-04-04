ef9d3b321ad3909b121cc2cb6fe04342f594d3bb
Grisha <grisha@ayende.com>
2018-04-04 17:03:55 +0300

    RavenDB-10888 - make sure that we use the timeout when waiting for deletion from indexes

---
+++ Raven.Tests.Issues/RavenDB-10888.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Raven.Client.Embedded;
+using Raven.Database.Config;
+using Xunit;
+
+namespace Raven.Tests.Issues
+{
+    public class RavenDB_10888
+    {
+        [Fact]
+        public void WillWaitForIndexes()
+        {
+            using (var store = new EmbeddableDocumentStore
+            {
+                RunInMemory = true
+            })
+            {
+                store.Configuration.DefaultStorageTypeName = InMemoryRavenConfiguration.VoronTypeName;
+                store.Configuration.Storage.Voron.AllowOn32Bits = true;
+                store.Initialize();
+
+                {
+                    const string docId = "Entity/1";
+                    using (var session = store.OpenSession())
+                    {
+                        var entity = new Entity();
+                        session.Store(entity, docId);
+                        session.SaveChanges();
+                    }
+
+                    using (var session = store.OpenSession())
+                    {
+                        var entity = session.Load<Entity>(docId);
+                        session.Delete(entity);
+                        session.Advanced.WaitForIndexesAfterSaveChanges();
+                        session.SaveChanges();
+                    }
+                }
+            }
+        }
+
+        internal class Entity
+        {
+            public string Id { get; set; }
+            public List<string> Tokens { get; set; }
+        }
+    }
+}
