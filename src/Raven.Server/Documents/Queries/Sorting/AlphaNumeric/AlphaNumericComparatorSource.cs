d759c498864380e4385856ea5ce656b1ae3cfdbc
maximburyak <maxim@ayende.com>
2018-04-10 17:05:23 +0200

    RavenDB-10894: Alphanumeric sort: fixing overflow exception due to cast to int, and changing the implementation to be with as few allocations as possible.

--- src/Raven.Server/Documents/Queries/Sorting/AlphaNumeric/AlphaNumericComparatorSource.cs
+++ src/Raven.Server/Documents/Queries/Sorting/AlphaNumeric/AlphaNumericComparatorSource.cs
@@ -1,12 +1,24 @@
-﻿using Lucene.Net.Search;
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using Lucene.Net.Search;
+using Raven.Server.ServerWide.Context;
 
 namespace Raven.Server.Documents.Queries.Sorting.AlphaNumeric
 {
     public class AlphaNumericComparatorSource : FieldComparatorSource
     {
+        private readonly DocumentsOperationContext _context;
+        private readonly Func<int, string[]> _valuesArrayFactory;
+
+        public AlphaNumericComparatorSource(DocumentsOperationContext context)
+        {
+            _context = context;
+        }
+
         public override FieldComparator NewComparator(string fieldname, int numHits, int sortPos, bool reversed)
         {
-            return new AlphaNumericFieldComparator(numHits, fieldname);
+            return new AlphaNumericFieldComparator(fieldname, _context.AllocatePooledArray<string>(numHits));
         }
     }
 }
