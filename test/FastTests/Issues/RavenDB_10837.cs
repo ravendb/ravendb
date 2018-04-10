dd65750c66a280004f021e2eb329e8a69cd78a8e
Ayende Rahien <Ayende@ayende.com>
2018-04-10 12:41:30 +0300

    RavenDB-10909 Deadlock between replacing side by side indexing and stopping indexes

--- test/FastTests/Issues/RavenDB_10837.cs
+++ test/FastTests/Issues/RavenDB_10837.cs
@@ -46,8 +46,6 @@ namespace FastTests.Issues
                 using (var session = store.OpenSession())
                 {
                     var entities = session.Query<Entity>().Statistics(out QueryStatistics statistics).ToList();
-
-                    Console.WriteLine(statistics.IndexName);
                     Assert.NotEmpty(entities);
                 }
             }
