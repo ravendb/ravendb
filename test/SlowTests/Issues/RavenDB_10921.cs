7ab8d9370743bf14dc80f72d9dfceeaa3c2e9a62
Arkadiusz Palinski <arek@ravendb.net>
2018-04-11 14:41:51 +0200

    RavenDB-10921 When editing the side by side index we have to send its original name instead of the one with ReplacementOf/ prefix. Otherwise such index will be put as an ordinary index in the database record.

---
+++ test/SlowTests/Issues/RavenDB_10921.cs
@@ -0,0 +1,32 @@
+﻿using FastTests;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Operations.Indexes;
+using Raven.Client.Exceptions;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10921 : RavenTestBase
+    {
+        [Fact]
+        public void MustNotPutIndexWithReplacementOfPrefix()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var ex = Assert.Throws<RavenException>(() =>
+                {
+                    store.Maintenance.Send(new PutIndexesOperation(new IndexDefinition()
+                    {
+                        Name = "ReplacementOf/test",
+                        Maps =
+                    {
+                        "from u in docs.Users select new { u.Age }"
+                    }
+                    }));
+                });
+
+                Assert.Contains("Index name must not start with 'ReplacementOf/'. Provided index name: 'ReplacementOf/test'", ex.Message);
+            }
+        }
+    }
+}
