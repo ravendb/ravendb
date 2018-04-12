7817b0b710149f18b98c47ea99e2bcbf15ff1d44
iftahbe <iftahbe@gmail.com>
2018-04-12 15:09:23 +0300

    RavenDB-10799 small fix

--- src/Raven.Server/Commercial/SetupManager.cs
+++ src/Raven.Server/Commercial/SetupManager.cs
@@ -185,6 +185,7 @@ namespace Raven.Server.Commercial
                 onProgress(progress);
 
                 byte[] zipBytes;
+                byte[] serverCertBytes;
                 X509Certificate2 serverCert;
                 X509Certificate2 clientCert;
                 BlittableJsonReaderObject settingsJsonObject;
@@ -208,7 +209,7 @@ namespace Raven.Server.Commercial
                 {
                     try
                     {
-                        settingsJsonObject = ExtractCertificatesAndSettingsJsonFromZip(zipBytes, continueSetupInfo.NodeTag, context, out _, out serverCert, out clientCert, out otherNodesUrls, out license);
+                        settingsJsonObject = ExtractCertificatesAndSettingsJsonFromZip(zipBytes, continueSetupInfo.NodeTag, context, out serverCertBytes, out serverCert, out clientCert, out otherNodesUrls, out license);
                     }
                     catch (Exception e)
                     {
@@ -235,7 +236,7 @@ namespace Raven.Server.Commercial
 
                     try
                     {
-                        await CompleteConfigurationForNewNode(onProgress, progress, continueSetupInfo, settingsJsonObject, serverCert, clientCert, serverStore, otherNodesUrls, license);
+                        await CompleteConfigurationForNewNode(onProgress, progress, continueSetupInfo, settingsJsonObject, serverCertBytes, serverCert, clientCert, serverStore, otherNodesUrls, license);
                     }
                     catch (Exception e)
                     {
@@ -1153,6 +1154,7 @@ namespace Raven.Server.Commercial
             SetupProgressAndResult progress,
             ContinueSetupInfo continueSetupInfo,
             BlittableJsonReaderObject settingsJsonObject,
+            byte[] serverCertBytes,
             X509Certificate2 serverCert,
             X509Certificate2 clientCert,
             ServerStore serverStore,
@@ -1174,7 +1176,7 @@ namespace Raven.Server.Commercial
             settingsJsonObject.TryGet(RavenConfiguration.GetKey(x => x.Core.SetupMode), out SetupMode setupMode);
             settingsJsonObject.TryGet(RavenConfiguration.GetKey(x => x.Security.CertificatePath), out string certificateFileName);
 
-            serverStore.Server.Certificate = SecretProtection.ValidateCertificateAndCreateCertificateHolder("Setup", serverCert, serverCert.Export(X509ContentType.Pfx, certPassword), certPassword, serverStore);
+            serverStore.Server.Certificate = SecretProtection.ValidateCertificateAndCreateCertificateHolder("Setup", serverCert, serverCertBytes, certPassword, serverStore);
             
             if (continueSetupInfo.NodeTag.Equals("A"))
             {
@@ -1255,7 +1257,7 @@ namespace Raven.Server.Commercial
 
                 using (var certfile = SafeFileStream.Create(certPath, FileMode.Create))
                 {
-                    var certBytes = serverCert.Export(X509ContentType.Pfx, certPassword);
+                    var certBytes = serverCertBytes;
                     certfile.Write(certBytes, 0, certBytes.Length);
                     certfile.Flush(true);
                 }
