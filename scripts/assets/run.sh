dd8c824111261a00d758e7671fd14fb02e8a3dd6
aviviadi <avivi.adi.hr@gmail.com>
2018-04-17 16:30:19 +0300

    RavenDB-9454: Open browser also in mac

--- scripts/assets/run.sh
+++ scripts/assets/run.sh
@@ -6,6 +6,7 @@ EXECUTABLE="Raven.Server"
 EXECUTABLE_DIR="$SCRIPT_DIRECTORY/Server"
 EXECUTABLE_PATH="$EXECUTABLE_DIR/$EXECUTABLE"
 VERSION_PATH="$SCRIPT_DIRECTORY/version.txt"
+BROWSER_OPEN="xdg-open"
 
 ASSEMBLY_VERSION=$(eval $EXECUTABLE_PATH --version)
 VERSION=""
@@ -14,9 +15,13 @@ if [[ -f "$VERSION_PATH" ]]; then
     VERSION=$(cat $VERSION_PATH);
 fi
 
+if [[ $(uname -s | grep Darwin | wc -l) -gt 0 ]]; then
+	BROWSER_OPEN="open"
+fi 
+
 if [[ "$ASSEMBLY_VERSION" != "$VERSION" ]]; then
     echo "$ASSEMBLY_VERSION" > "$VERSION_PATH"
-    xdg-open "http://ravendb.net/first-run?type=start&ver=$ASSEMBLY_VERSION";
+    ${BROWSER_OPEN} "http://ravendb.net/first-run?type=start&ver=$ASSEMBLY_VERSION";
 fi
 
 pushd $EXECUTABLE_DIR > /dev/null
