9e8769b9992f56ac3663c32916e65b9e56a82559
Ayende Rahien <Ayende@ayende.com>
2018-04-18 09:30:16 +0300

    RavenDB-10965 Adding passing test

---
+++ test/SlowTests/Issues/RavenDB-10965.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using FastTests;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10965 : RavenTestBase
+    {
+        public class Item
+        {
+            public Exception E;
+        }
+
+        [Fact]
+        public void CanSerializeException()
+        {
+            using (var store = GetDocumentStore())
+            {
+                Exception e = null;
+                try
+                {
+                    throw new SocketException();
+                }
+                catch (Exception ex)
+                {
+                    e = ex;
+                }
+                using (var s = store.OpenSession())
+                {
+                    s.Store(new Item
+                    {
+                        E = e
+                    });
+                    s.SaveChanges();
+                }
+            }
+        }
+    }
+}
