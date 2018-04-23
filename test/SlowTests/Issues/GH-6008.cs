bb29ed8abc921199ed42e78fd7f00695289fc315
Ayende Rahien <Ayende@ayende.com>
2018-04-24 02:00:33 +0300

    Adding test

---
+++ test/SlowTests/Issues/GH-6008.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FastTests;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class GH_6008 : RavenTestBase
+    {
+        public class Item
+        {
+
+        }
+        [Fact]
+        public void CanGetIsLoadedTrueForNewValue()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var s = store.OpenSession())
+                {
+                    var item = new Item();
+                    s.Store(item, null, id: "items-one");
+
+                    Assert.True(s.Advanced.IsLoaded("items-one"));
+                }
+            }
+        }
+    }
+}
