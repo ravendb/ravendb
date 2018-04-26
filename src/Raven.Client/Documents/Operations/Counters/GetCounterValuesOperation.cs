5941cb42989563b2ea626625346fc2cbaeb9d00a
aviv86 <aviv@ayende.com>
2018-04-26 11:02:57 +0200

    client side and endpoints for GetCounterValues, GetCountersForDocument

---
+++ src/Raven.Client/Documents/Operations/Counters/GetCounterValuesOperation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Raven.Client.Documents.Conventions;
+using Raven.Client.Http;
+using Sparrow.Json;
+
+namespace Raven.Client.Documents.Operations.Counters
+{
+    public class GetCounterValuesOperation : IOperation<Dictionary<Guid, long>>
+    {
+        private readonly string _documentId;
+        private readonly string _name;
+
+        public GetCounterValuesOperation(string documentId, string name)
+        {
+            _documentId = documentId;
+            _name = name;
+        }
+
+        public RavenCommand<Dictionary<Guid, long>> GetCommand(IDocumentStore store, DocumentConventions conventions, JsonOperationContext context, HttpCache cache)
+        {
+            return new GetCounterValuesCommand(_documentId, _name);
+        }
+
+        private class GetCounterValuesCommand : RavenCommand<Dictionary<Guid, long>>
+        {
+            private readonly string _documentId;
+            private readonly string _name;
+
+            public GetCounterValuesCommand(string documentId, string name)
+            {
+                if (string.IsNullOrWhiteSpace(documentId))
+                    throw new ArgumentNullException(nameof(documentId));
+                if (string.IsNullOrWhiteSpace(name))
+                    throw new ArgumentNullException(nameof(name));
+
+                _documentId = documentId;
+                _name = name;
+            }
+
+            public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+            {
+                url = $"{node.Url}/databases/{node.Database}/counters/getValues?id={_documentId}&name={_name}";
+
+                return new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get
+                };
+            }
+
+            public override void SetResponse(JsonOperationContext context, BlittableJsonReaderObject response, bool fromCache)
+            {
+                if (!(response["Values"] is BlittableJsonReaderObject values))
+                    return;
+
+                //var dic = new Dictionary<Guid, long>();
+                Result = new Dictionary<Guid, long>();
+
+                foreach (var key in values.GetPropertyNames())
+                {
+                    Result[new Guid(key)] = (long)values[key];
+
+                    //dic[new Guid(key)] = (long)values[key];
+                }
+
+                //Result = dic;
+            }
+
+            public override bool IsReadRequest => true;
+        }
+    }
+}
--- src/Raven.Server/Documents/Handlers/CounterHandler.cs
+++ src/Raven.Server/Documents/Handlers/CounterHandler.cs
@@ -4,12 +4,10 @@
 //  </copyright>
 // -----------------------------------------------------------------------
 
-using System;
 using System.Net;
 using System.Threading.Tasks;
 using Raven.Server.Routing;
 using Raven.Server.ServerWide.Context;
-using Sparrow;
 using Sparrow.Json;
 using Sparrow.Json.Parsing;
 
@@ -20,21 +18,18 @@ namespace Raven.Server.Documents.Handlers
         [RavenAction("/databases/*/counters/increment", "PUT", AuthorizationStatus.ValidUser)]
         public Task Increment()
         {
+            var id = GetQueryStringValueAndAssertIfSingleAndNotEmpty("id");
+            var name = GetQueryStringValueAndAssertIfSingleAndNotEmpty("name");
+            var value = GetLongQueryString("val", false) ?? 0;
+
             using (ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+            using (context.OpenWriteTransaction())
             {
-                var id = GetQueryStringValueAndAssertIfSingleAndNotEmpty("id");
-                var name = GetQueryStringValueAndAssertIfSingleAndNotEmpty("name");
-                var value = GetLongQueryString("val", false) ?? 0;
-
-                using (context.OpenWriteTransaction())
-                {
-                    Database.DocumentsStorage.CountersStorage.IncrementCounter(context, id, name, value);
-                    context.Transaction.Commit();
-                }
-
-                HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
+                Database.DocumentsStorage.CountersStorage.IncrementCounter(context, id, name, value);
+                context.Transaction.Commit();
             }
 
+            HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
             return Task.CompletedTask;
         }
 
@@ -48,7 +43,6 @@ namespace Raven.Server.Documents.Handlers
             using (context.OpenReadTransaction())
             {
                 var value = Database.DocumentsStorage.CountersStorage.GetCounterValue(context, documentId, name);
-                HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
 
                 using (var writer = new BlittableJsonTextWriter(context, ResponseBodyStream()))
                 {
@@ -59,34 +53,63 @@ namespace Raven.Server.Documents.Handlers
                 }
             }
 
+            HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
             return Task.CompletedTask;
         }
 
-        /*
 
-                [RavenAction("/databases/#1#attachments", "DELETE", AuthorizationStatus.ValidUser)]
-                public async Task Delete()
+        [RavenAction("/databases/*/counters/getValues", "GET", AuthorizationStatus.ValidUser)]
+        public Task GetCounterValues()
+        {
+            var documentId = GetQueryStringValueAndAssertIfSingleAndNotEmpty("id");
+            var name = GetQueryStringValueAndAssertIfSingleAndNotEmpty("name");
+
+            using (ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+            using (context.OpenReadTransaction())
+            {
+                var dic = Database.DocumentsStorage.CountersStorage.GetCounterValues(context, documentId, name);
+                var jsonMap = new DynamicJsonValue();
+                foreach (var kvp in dic)
                 {
-                    using (ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+                    jsonMap[kvp.Key.ToString()] = kvp.Value;
+                }
+
+                using (var writer = new BlittableJsonTextWriter(context, ResponseBodyStream()))
+                {
+                    context.Write(writer, new DynamicJsonValue
                     {
-                        var id = GetQueryStringValueAndAssertIfSingleAndNotEmpty("id");
-                        var name = GetQueryStringValueAndAssertIfSingleAndNotEmpty("name");
+                        ["Values"] = jsonMap
+                    });
+                }
+            }
 
-                        var changeVector = context.GetLazyString(GetStringFromHeaders("If-Match"));
+            HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
+            return Task.CompletedTask;
+        }
 
-                        var cmd = new MergedDeleteAttachmentCommand
-                        {
-                            Database = Database,
-                            ExpectedChangeVector = changeVector,
-                            DocumentId = id,
-                            Name = name
-                        };
-                        await Database.TxMerger.Enqueue(cmd);
-                        cmd.ExceptionDispatchInfo?.Throw();
+        [RavenAction("/databases/*/counters/getNames", "GET", AuthorizationStatus.ValidUser)]
+        public Task GetCountersForDocument()
+        {
+            var documentId = GetQueryStringValueAndAssertIfSingleAndNotEmpty("id");
+            var skip = GetIntValueQueryString("skip", false) ?? 0;
+            var take = GetIntValueQueryString("skip", false) ?? 1024;
 
-                        NoContentStatus();
-                    }
-                }*/
+            using (ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+            using (context.OpenReadTransaction())
+            {
+                var counters = Database.DocumentsStorage.CountersStorage.GetCountersForDocument(context, documentId, skip, take);
 
+                using (var writer = new BlittableJsonTextWriter(context, ResponseBodyStream()))
+                {
+                    context.Write(writer, new DynamicJsonValue
+                    {
+                        ["Names"] = counters
+                    });
+                }
+            }
+
+            HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
+            return Task.CompletedTask;
+        }
     }
 }
