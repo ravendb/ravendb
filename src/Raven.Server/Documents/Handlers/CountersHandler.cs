948bd35bf7cd4a11f96065e476c2a4c3b01ffa24
aviv86 <aviv@ayende.com>
2018-05-01 10:49:46 +0200

    RavenDB-8113 Counters : add @counters property to metadata

--- src/Raven.Server/Documents/Handlers/CountersHandler.cs
+++ src/Raven.Server/Documents/Handlers/CountersHandler.cs
@@ -5,14 +5,17 @@
 // -----------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Raven.Client;
 using Raven.Client.Exceptions;
 using Raven.Client.Exceptions.Documents;
-using Raven.Server.Documents.Replication;
 using Raven.Server.Routing;
 using Raven.Server.ServerWide.Context;
 using Sparrow.Json;
+using Sparrow.Json.Parsing;
 
 namespace Raven.Server.Documents.Handlers
 {
@@ -20,10 +23,10 @@ namespace Raven.Server.Documents.Handlers
     {
         public class IncrementCounterCommand : TransactionOperationsMerger.MergedTransactionCommand
         {
-            private DocumentDatabase _database;
-            private string _doc, _counter;
-            private long _value;
-            private bool _failTx;
+            private readonly DocumentDatabase _database;
+            private readonly string _doc, _counter;
+            private readonly long _value;
+            private readonly bool _failTx;
 
             public long CurrentValue;
             public bool DocumentMissing;
@@ -43,14 +46,24 @@ namespace Raven.Server.Documents.Handlers
                 {
                     var doc = _database.DocumentsStorage.Get(context, _doc,
                         throwOnConflict: true);
-                    if(doc == null)
+                    if (doc == null)
                     {
                         DocumentMissing = true;
                         if (_failTx == false)
                             return 0;
                         ThrowMissingDocument();
+                        return 0; // never hit
                     }
 
+                    if (doc.TryGetMetadata(out var metadata) == false)
+                        ThrowInvalidDocumentWithNoMetadata(doc, _counter);
+
+                    UpdateDocumentCounters(metadata);
+                    if (metadata.Modifications != null)
+                    {
+                        var data = context.ReadObject(doc.Data, _doc, BlittableJsonDocumentBuilder.UsageMode.ToDisk);
+                        _database.DocumentsStorage.Put(context, _doc, null, data);
+                    }
                 }
                 catch (DocumentConflictException)
                 {
@@ -62,6 +75,44 @@ namespace Raven.Server.Documents.Handlers
                 _database.DocumentsStorage.CountersStorage.IncrementCounter(context, _doc, _counter, _value);
                 CurrentValue = _database.DocumentsStorage.CountersStorage.GetCounterValue(context, _doc, _counter) ?? 0;
                 return 1;
+            }
+
+            private void UpdateDocumentCounters(BlittableJsonReaderObject metadata)
+            {
+                if (metadata.TryGet(Constants.Documents.Metadata.Counters, out BlittableJsonReaderArray counters) == false)
+                {
+                    metadata.Modifications = new DynamicJsonValue(metadata)
+                    {
+                        [Constants.Documents.Metadata.Counters] = new DynamicJsonArray
+                        {
+                            _counter
+                        }
+                    };
+                }
+                else
+                {
+                    var loc = counters.BinarySearch(_counter);
+                    if (loc < 0)
+                    {
+                        loc = ~loc; // flip the bits and find the right location
+                        var list = new List<string>(counters.Length + 1);
+                        for (int i = 0; i < loc; i++)
+                        {
+                            list.Add(counters.GetStringByIndex(i));
+                        }
+
+                        list.Add(_counter);
+                        for (int i = loc; i < counters.Length; i++)
+                        {
+                            list.Add(counters.GetStringByIndex(i));
+                        }
+
+                        metadata.Modifications = new DynamicJsonValue(metadata)
+                        {
+                            [Constants.Documents.Metadata.Counters] = new DynamicJsonArray(list)
+                        };
+                    }
+                }
             }
 
             public void ThrowMissingDocument()
@@ -84,9 +135,60 @@ namespace Raven.Server.Documents.Handlers
 
             public override int Execute(DocumentsOperationContext context)
             {
+                try
+                {
+                    var doc = _database.DocumentsStorage.Get(context, _doc,
+                        throwOnConflict: true);
+                    if (doc == null)
+                        return 0;                   
+                    if (doc.TryGetMetadata(out var metadata) == false)
+                        ThrowInvalidDocumentWithNoMetadata(doc, _counter);
+
+                    UpdateDocumentCounters(metadata);
+                    if (metadata.Modifications != null)
+                    {
+                        var data = context.ReadObject(doc.Data, _doc, BlittableJsonDocumentBuilder.UsageMode.ToDisk);
+                        _database.DocumentsStorage.Put(context, _doc, null, data);
+                    }
+                }
+                catch (DocumentConflictException)
+                {
+                    // this is fine, we explicitly support
+                    // setting the flag if we are in conflicted state is 
+                    // done by the conflict resolver
+                }
+
                 var del = _database.DocumentsStorage.CountersStorage.DeleteCounter(context, _doc, _counter);
                 return del ? 1 : 0;
             }
+
+            private void UpdateDocumentCounters(BlittableJsonReaderObject metadata)
+            {
+                if (metadata.TryGet(Constants.Documents.Metadata.Counters, out BlittableJsonReaderArray counters))
+                {
+
+                    var counterIndex = counters.BinarySearch(_counter);
+                    if (counterIndex < 0)
+                        return;
+
+                    metadata.Modifications = new DynamicJsonValue(metadata);
+
+                    if (counters.Length == 1)
+                    {
+                        //document has only one counter, can remove @counters property from metadata
+                        metadata.Modifications.Removals = new HashSet<int>
+                        {
+                            metadata.GetPropertyIndex(Constants.Documents.Metadata.Counters)
+                        };
+                    }
+                    else
+                    {
+                        metadata.Modifications[Constants.Documents.Metadata.Counters] = new DynamicJsonArray(
+                            counters.Where((e, index) => index != counterIndex));
+                    }
+                }
+            }
+
         }
 
         [RavenAction("/databases/*/counters", "POST", AuthorizationStatus.ValidUser)]
@@ -264,6 +366,11 @@ namespace Raven.Server.Documents.Handlers
             await Database.TxMerger.Enqueue(cmd);
 
             NoContentStatus();
+        }
+
+        private static void ThrowInvalidDocumentWithNoMetadata(Document doc, string counter)
+        {
+            throw new InvalidOperationException("Cannot increment counter " + counter + " for " + doc + " because the document has no metadata. Should not happen ever");
         }
     }
 }
---
+++ test/SlowTests/Client/Counters/CountersInMetadata.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FastTests.Server.Replication;
+using Raven.Client;
+using Raven.Client.Documents.Operations.Counters;
+using Raven.Tests.Core.Utils.Entities;
+using Xunit;
+
+namespace SlowTests.Client.Counters
+{
+    public class CountersInMetadata : ReplicationTestBase
+    {
+        [Fact]
+        public void IncrementAndDeleteShouldChangeDocumentMetadata()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User { Name = "Aviv" }, "users/1-A");
+                    session.SaveChanges();
+                }
+
+                store.Operations.Send(new IncrementCounterOperation("users/1-A", "likes", 10));
+
+                using (var session = store.OpenSession())
+                {
+                    var user = session.Load<User>("users/1-A");
+                    var metadatad = session.Advanced.GetMetadataFor(user);
+
+                    Assert.True(metadatad.TryGetValue(Constants.Documents.Metadata.Counters, out object counters));
+                    Assert.Equal(1, ((object[])counters).Length);
+                    Assert.True(((object[])counters).Contains("likes"));
+                }
+
+                store.Operations.Send(new IncrementCounterOperation("users/1-A", "votes", 50));
+                using (var session = store.OpenSession())
+                {
+                    var user = session.Load<User>("users/1-A");
+                    var metadatad = session.Advanced.GetMetadataFor(user);
+
+                    Assert.True(metadatad.TryGetValue(Constants.Documents.Metadata.Counters, out object counters));
+                    Assert.Equal(2, ((object[])counters).Length);
+                    Assert.True(((object[])counters).Contains("likes"));
+                    Assert.True(((object[])counters).Contains("votes"));
+                }
+
+                store.Operations.Send(new DeleteCounterOperation("users/1-A", "likes"));
+
+                using (var session = store.OpenSession())
+                {
+                    var user = session.Load<User>("users/1-A");
+                    var metadatad = session.Advanced.GetMetadataFor(user);
+
+                    Assert.True(metadatad.TryGetValue(Constants.Documents.Metadata.Counters, out object counters));
+                    Assert.Equal(1, ((object[])counters).Length);
+                    Assert.True(((object[])counters).Contains("votes"));
+                }
+
+                store.Operations.Send(new DeleteCounterOperation("users/1-A", "votes"));
+                using (var session = store.OpenSession())
+                {
+                    var user = session.Load<User>("users/1-A");
+                    var metadatad = session.Advanced.GetMetadataFor(user);
+                    Assert.False(metadatad.TryGetValue(Constants.Documents.Metadata.Counters, out _));
+                }
+
+            }
+        }
+    }
+}
