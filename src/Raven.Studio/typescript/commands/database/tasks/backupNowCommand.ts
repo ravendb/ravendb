2bc9e5786ebf1bd55682eeccec49c02c47b0c7ee
Grisha <grisha@ayende.com>
2018-05-06 20:21:50 +0300

    RavenDB-7938 - Show progress of a backup, studio part

--- src/Raven.Studio/typescript/commands/database/tasks/backupNowCommand.ts
+++ src/Raven.Studio/typescript/commands/database/tasks/backupNowCommand.ts
@@ -3,21 +3,20 @@ import database = require("models/resources/database");
 import endpoints = require("endpoints");
 
 class backupNowCommand extends commandBase {
-    constructor(private db: database, private taskId: number, private isFullBackup: boolean) {
+    constructor(private db: database, private taskId: number, private isFullBackup: boolean, private taskName: string) {
         super();
     }
  
-    execute(): JQueryPromise<void> {
+    execute(): JQueryPromise<Raven.Server.Documents.PeriodicBackup.BackupDatabaseNowResult> {
         const url = endpoints.databases.ongoingTasks.adminBackupDatabase +
             this.urlEncodeArgs({
                 taskId: this.taskId,
                 isFullBackup: this.isFullBackup
             });
 
-        return this.post(url, null, this.db, { dataType: undefined })
-            .done(() => this.reportSuccess(`Successfully started a backup task`))
+        return this.post(url, null, this.db)
             .fail(response => {
-                this.reportError(`Failed to start a backup for task id: ${this.taskId}`,
+                this.reportError(`Failed to start a backup for task: '${this.taskName}'`,
                     response.responseText,
                     response.statusText);
             });
