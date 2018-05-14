9f87f9e8d9ed9d3e5d3fffa233dfb291908eb61c
Pawel Pekrol <ppekrol@ravendb.net>
2018-05-14 16:32:04 +0200

    RavenDB-11073 session.Advanced.Attachments.GetNames should throw is entity is not bound to session

---
+++ test/SlowTests/Issues/RavenDB_11073.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FastTests;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_11073 : RavenTestBase
+    {
+        [Fact]
+        public void GettingAttachmentNamesForUnstoredEntityShouldThrow()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var e = Assert.Throws<ArgumentException>(() => session.Advanced.Attachments.GetNames(new { }));
+                    Assert.Contains("is not associated with the session. Use documentId instead or track the entity in the session", e.Message);
+                }
+            }
+        }
+    }
+}
