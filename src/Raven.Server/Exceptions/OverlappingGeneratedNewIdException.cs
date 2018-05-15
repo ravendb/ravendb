82ae54aef621ab62acaa689c4b368af0da94482a
Pawel Pekrol <ppekrol@ravendb.net>
2018-05-15 12:42:32 +0200

    RavenDB-10581 introduced OverlappingGeneratedNewIdException

---
+++ src/Raven.Server/Exceptions/OverlappingGeneratedNewIdException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Raven.Server.Exceptions
+{
+    public class OverlappingGeneratedNewIdException : Exception
+    {
+        public OverlappingGeneratedNewIdException()
+        {
+        }
+
+        public OverlappingGeneratedNewIdException(Exception e) : base(e.Message, e)
+        {
+        }
+
+        public OverlappingGeneratedNewIdException(string message)
+            : base(message)
+        {
+        }
+
+        public OverlappingGeneratedNewIdException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
