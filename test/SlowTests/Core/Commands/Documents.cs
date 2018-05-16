ab59ef4f184a724b3986f232b5c5997e67f9bde1
Ayende Rahien <Ayende@ayende.com>
2018-05-16 13:23:48 +0300

    Tests that modify the global state must use local server and not a shared one Better error if test fails

--- test/SlowTests/Core/Commands/Documents.cs
+++ test/SlowTests/Core/Commands/Documents.cs
@@ -39,8 +39,15 @@ namespace SlowTests.Core.Commands
                     cts.Cancel();
                     var putTask = commands.PutAsync("test/1", null, document, null, cts.Token);
 
-                    Assert.True(SpinWait.SpinUntil(() => putTask.IsCanceled, TimeSpan.FromSeconds(150)));
-                    Assert.True(putTask.IsCanceled);
+                    try
+                    {
+                        Assert.True(SpinWait.SpinUntil(() => putTask.IsCanceled, TimeSpan.FromSeconds(10)));
+                        Assert.True(putTask.IsCanceled);
+                    }
+                    catch (Exception)
+                    {
+                        throw new InvalidOperationException("Task was expected to be cacnelled, but was " + putTask.Status, putTask.Exception);
+                    }
                 }
             }
         }
