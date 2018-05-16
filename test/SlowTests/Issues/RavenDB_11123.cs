126373cf8d712b43724c2af6b677f40fce5ffef1
Pawel Pekrol <ppekrol@ravendb.net>
2018-05-16 11:51:18 +0200

    RavenDB-11123 support passing null to .WhereLucene

---
+++ test/SlowTests/Issues/RavenDB_11123.cs
@@ -0,0 +1,102 @@
+﻿using System.Linq;
+using FastTests;
+using Orders;
+using Raven.Client.Documents.Indexes;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_11123 : RavenTestBase
+    {
+        private class Index1 : AbstractIndexCreationTask<Company>
+        {
+            public Index1()
+            {
+                Map = companies => from c in companies
+                                   select new
+                                   {
+                                       Name = c.Name,
+                                       ExternalId = c.ExternalId
+                                   };
+            }
+        }
+
+        [Fact]
+        public void CanUseNullInWhereLucene()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Company
+                    {
+                        Name = "C1",
+                        ExternalId = null
+                    });
+
+                    session.Store(new Company
+                    {
+                        Name = "C2",
+                        ExternalId = "E2"
+                    });
+
+                    
+                    session.Store(new Company
+                    {
+                        Name = "C3",
+                        ExternalId = null
+                    });
+
+                    session.SaveChanges();
+                }
+
+                new Index1().Execute(store);
+                WaitForIndexing(store);
+
+                using (var session = store.OpenSession())
+                {
+                    var results = session
+                        .Advanced
+                        .DocumentQuery<Company, Index1>()
+                        .WhereLucene("ExternalId", null)
+                        .ToList();
+
+                    Assert.Equal(2, results.Count);
+
+                    results = session
+                        .Advanced
+                        .DocumentQuery<Company, Index1>()
+                        .WhereLucene("ExternalId", null)
+                        .AndAlso()
+                        .WhereLucene("Name", "C3")
+                        .ToList();
+
+                    Assert.Equal(1, results.Count);
+                    Assert.Equal("C3", results[0].Name);
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var results = session
+                        .Advanced
+                        .DocumentQuery<Company>()
+                        .WhereLucene("ExternalId", null)
+                        .ToList();
+
+                    Assert.Equal(2, results.Count);
+
+                    results = session
+                        .Advanced
+                        .DocumentQuery<Company>()
+                        .WhereLucene("ExternalId", null)
+                        .AndAlso()
+                        .WhereLucene("Name", "C3")
+                        .ToList();
+
+                    Assert.Equal(1, results.Count);
+                    Assert.Equal("C3", results[0].Name);
+                }
+            }
+        }
+    }
+}
