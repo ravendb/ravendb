5eb532a3d328b77d8d9b48aa6c2df2748872242e
Tal Weiss <tal@ayende.com>
2018-05-17 14:43:31 +0300

    RavenDB-11109_v4.1

--- src/Raven.Server/Smuggler/Documents/CsvStreamSource.cs
+++ src/Raven.Server/Smuggler/Documents/CsvStreamSource.cs
@@ -22,6 +22,7 @@ namespace Raven.Server.Smuggler.Documents
         private readonly DocumentDatabase _database;
         private readonly Stream _stream;
         private readonly DocumentsOperationContext _context;
+        private SmugglerResult _result;
         private DatabaseItemType _currentType;
         private readonly string _collection;
         private StreamReader _reader;
@@ -55,6 +56,7 @@ namespace Raven.Server.Smuggler.Documents
             buildVersion = 40;
             _reader = new StreamReader(_stream);
             _csvReader = new CsvReader(_reader);
+            _result = result;
             _csvReader.Configuration.Delimiter = ",";
             return new DisposableAction(() =>
             {
@@ -153,8 +155,11 @@ namespace Raven.Server.Smuggler.Documents
 
         public IEnumerable<DocumentItem> GetDocuments(List<string> collectionsToExport, INewDocumentActions actions)
         {
+            var line = 0;
             while (_csvReader.Read())
             {
+                line++;
+
                 if (ProcessFieldsIfNeeded())
                     continue;
 
@@ -164,8 +169,9 @@ namespace Raven.Server.Smuggler.Documents
                 {
                     item = ConvertRecordToDocumentItem(context, _csvReader.Context.Record, _csvReaderFieldHeaders, _collection);
                 }
-                catch
+                catch (Exception e)
                 {
+                    _result.AddError($"Fail to parse CSV line {line}, Error:{e.Message}");
                     item = new DocumentItem();
                 }
                 yield return item;
