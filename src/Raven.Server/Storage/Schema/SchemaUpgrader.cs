ab344c58fc5f2127b4dae06fd830b7ff45cc19bc
aviv86 <aviv@ayende.com>
2018-05-29 08:47:42 +0200

    RavenDB-8113 - SchemaUpgrader : bump DocumentsVersion to 16, add an empty 'From15' schema update

--- src/Raven.Server/Storage/Schema/SchemaUpgrader.cs
+++ src/Raven.Server/Storage/Schema/SchemaUpgrader.cs
@@ -14,7 +14,7 @@ namespace Raven.Server.Storage.Schema
 
             public const int ConfigurationVersion = 10;
 
-            public const int DocumentsVersion = 15;
+            public const int DocumentsVersion = 16;
 
             public const int IndexVersion = 12;
         }
