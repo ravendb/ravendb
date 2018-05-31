0a207379fc11f13313c5dbc465cbd43fc2c116a7
Tal Weiss <tal@ayende.com>
2018-05-31 19:15:52 +0300

    RavenDB-11239 some more indexes

--- test/SlowTests/Issues/RavenDBBookJavaScriptIndexes.cs
+++ test/SlowTests/Issues/RavenDBBookJavaScriptIndexes.cs
@@ -5,6 +5,7 @@ using FastTests;
 using Orders;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Indexes;
+using SlowTests.Tests.Linq;
 using Tests.Infrastructure;
 using Xunit;
 
@@ -12,42 +13,121 @@ namespace SlowTests.Issues
 {
     public class RavenDBBookJavaScriptIndexes : RavenTestBase
     {
-        [Fact(Skip = "Playground test")]
-        public void EmployeeIndexTest()
-        {
-            using (var store = GetDocumentStore())
-            {
-                CreateNorthwindDatabase(store);
-                new EmployeeIndex().Execute(store);
-                WaitForUserToContinueTheTest(store);
-            }
-        }
 
-        [Fact(Skip = "Playground test")]
-        public void EmployeeByHiredAtTest()
-        {
-            using (var store = GetDocumentStore())
-            {
-                CreateNorthwindDatabase(store);
-                new EmployeeByHiredAt().Execute(store);
-                WaitForUserToContinueTheTest(store);
-            }
-        }
-
-        [Fact(Skip = "Playground test")]
+        [Fact]
         public void RegionsByTerritoriesNames()
         {
+            ExecuteIndexAndWaitForUserToContinueTheTest<MultiField>();            
+            //ExecuteIndexAndWaitForUserToContinueTheTest<MultiMap>();
+            //ExecuteIndexAndWaitForUserToContinueTheTest<OrdersTotal>();
+            //ExecuteIndexAndWaitForUserToContinueTheTest<EmployeeByName>();
+            //ExecuteIndexAndWaitForUserToContinueTheTest<EmployeeByHiredAt>();
+            //ExecuteIndexAndWaitForUserToContinueTheTest<EmployeeIndex>();
+            //ExecuteIndexAndWaitForUserToContinueTheTest<RegionsByTerritoriesName>();
+        }
+
+        public void ExecuteIndexAndWaitForUserToContinueTheTest<T>() where T:AbstractIndexCreationTask, new()
+        {
             using (var store = GetDocumentStore())
             {
                 CreateNorthwindDatabase(store);
-                new RegionsByTerritoriesName().Execute(store);
+                new T().Execute(store);
                 WaitForUserToContinueTheTest(store);
             }
         }
-
         private void CreateNorthwindDatabase(DocumentStore store)
         {
             store.Maintenance.Send(new CreateSampleDataOperation());
+        }
+
+        private class OrdersTotal : AbstractIndexCreationTask
+        {
+            public override IndexDefinition CreateIndexDefinition()
+            {
+                return new IndexDefinition
+                {
+                    Name = "OrdersTotal",
+                    Maps = new HashSet<string>
+                    {
+@"map('Orders', function (o){ 
+function Sum(total, ol)
+{
+    return total + (ol.Quantity * ol.PricePerUnit) * (1- ol.Discount);
+}
+    return { 
+             Employee: o.Employee  ,
+             Company: o.Company,
+             Total: o.Lines.reduce(Sum,0)
+           };                            
+})",
+                    },
+                    Type = IndexType.JavaScriptMap,
+                    LockMode = IndexLockMode.Unlock,
+                    Priority = IndexPriority.Normal,
+                    Configuration = new IndexConfiguration()
+                };
+            }
+        }
+
+        private class MultiField : AbstractIndexCreationTask
+        {
+            public override IndexDefinition CreateIndexDefinition()
+            {
+                return new IndexDefinition
+                {
+                    Name = "MultiField",
+                    Maps = new HashSet<string>
+                    {
+                        @"map('Companies', function (c){ return {Query : [c.ExternalId, c.Name, c.Contact.Name] } })"
+                    },
+                    Type = IndexType.JavaScriptMap,
+                    LockMode = IndexLockMode.Unlock,
+                    Priority = IndexPriority.Normal,
+                    Configuration = new IndexConfiguration()
+                };
+            }
+        }
+
+        private class MultiMap : AbstractIndexCreationTask
+        {
+            public override IndexDefinition CreateIndexDefinition()
+            {
+                return new IndexDefinition
+                {
+                    Name = "MultiMap",
+                    Maps = new HashSet<string>
+                    {
+                        @"map('Employees', function (e){ return {Name: e.FirstName + ' ' + e.LastName}})",
+                        @"map('Companies', function (c){ return {Name: c.Contact.Name}})",
+                        @"map('Suppliers', function (s){ return {Name: s.Contact.Name}})"
+                    },
+                    Type = IndexType.JavaScriptMap,
+                    LockMode = IndexLockMode.Unlock,
+                    Priority = IndexPriority.Normal,
+                    Configuration = new IndexConfiguration()
+                };
+            }
+        }
+
+        private class EmployeeByName : AbstractIndexCreationTask
+        {
+            public override IndexDefinition CreateIndexDefinition()
+            {
+                return new IndexDefinition
+                {
+                    Name = "EmployeeByName",
+                    Maps = new HashSet<string>
+                    {
+                        @"map('Employees', function (e){ 
+                            return { Name: e.FirstName + ' ' + e.LastName };                            
+                        })",
+                    },
+                    Type = IndexType.JavaScriptMap,
+                    LockMode = IndexLockMode.Unlock,
+                    Priority = IndexPriority.Normal,
+                    Configuration = new IndexConfiguration()
+                };
+            }
         }
 
         private class RegionsByTerritoriesName : AbstractIndexCreationTask
