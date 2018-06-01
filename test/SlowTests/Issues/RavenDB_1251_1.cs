4fbc63b9f524b1ee50c1e751eb351e1856737eac
Ryan Heath <ryan.q.heath@gmail.com>
2018-06-01 14:56:48 +0200

    added/altered sorting tests to make sure the sorting/filtering is returning what is expected

--- test/SlowTests/Issues/RavenDB_1251_1.cs
+++ test/SlowTests/Issues/RavenDB_1251_1.cs
@@ -47,6 +47,54 @@ namespace SlowTests.Issues
 
                 using (var session = documentStore.OpenSession())
                 {
+                    var q = session.Query<Foo>()
+                        .Customize(x => x.WaitForNonStaleResults())
+                        .OrderByDescending(x => x.Bar);
+                    Debug.WriteLine(q);
+                    var result = q.ToList();
+
+                    Assert.Equal(5, result.Count);
+                    Assert.True(result[0].Bar > result[1].Bar);
+                    Assert.True(result[1].Bar > result[2].Bar);
+                    Assert.True(result[2].Bar > result[3].Bar);
+                    Assert.True(result[3].Bar > result[4].Bar);
+                }
+            }
+        }
+
+        [Fact]
+        public void TimeSpan_Can_Filter_By_Value()
+        {
+            using (var documentStore = GetDocumentStore())
+            {
+                using (var session = documentStore.OpenSession())
+                {
+                    session.Store(new Foo
+                    {
+                        Bar = TimeSpan.FromHours(-2)
+                    });
+                    session.Store(new Foo
+                    {
+                        Bar = TimeSpan.FromHours(-1)
+                    });
+                    session.Store(new Foo
+                    {
+                        Bar = TimeSpan.FromHours(0)
+                    });
+                    session.Store(new Foo
+                    {
+                        Bar = TimeSpan.FromHours(1)
+                    });
+                    session.Store(new Foo
+                    {
+                        Bar = TimeSpan.FromHours(2)
+                    });
+
+                    session.SaveChanges();
+                }
+
+                using (var session = documentStore.OpenSession())
+                {
                     var t = TimeSpan.FromHours(-1.5);
 
                     var q = session.Query<Foo>()
@@ -60,6 +108,7 @@ namespace SlowTests.Issues
                     Assert.True(result[0].Bar > result[1].Bar);
                     Assert.True(result[1].Bar > result[2].Bar);
                     Assert.True(result[2].Bar > result[3].Bar);
+                    Assert.Equal(TimeSpan.FromHours(-1), result[3].Bar);
                 }
             }
         }
