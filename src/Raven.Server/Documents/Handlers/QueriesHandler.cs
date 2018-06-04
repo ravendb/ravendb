ec5a1fb431f15fa55007fb0db8c2fa0d8a6f3a83
Arkadiusz Palinski <arek@ravendb.net>
2018-06-04 11:31:51 +0200

    RavenDB-11284 We should cache metadata of queries without parameters as well. Removed unused parameter.

--- src/Raven.Server/Documents/Handlers/QueriesHandler.cs
+++ src/Raven.Server/Documents/Handlers/QueriesHandler.cs
@@ -136,7 +136,7 @@ namespace Raven.Server.Documents.Handlers
 
             var json = await context.ReadForMemoryAsync(RequestBodyStream(), "index/query");
 
-            return IndexQueryServerSide.Create(json, context, Database.QueryMetadataCache);
+            return IndexQueryServerSide.Create(json, Database.QueryMetadataCache);
         }
 
         private async Task SuggestQuery(IndexQueryServerSide indexQuery, DocumentsOperationContext context, OperationCancelToken token)
@@ -191,7 +191,7 @@ namespace Raven.Server.Documents.Handlers
                 using (var tracker = new RequestTimeTracker(HttpContext, Logger, Database, "DeleteByQuery"))
                 {
                     var reader = context.Read(RequestBodyStream(), "queries/delete");
-                    var query = IndexQueryServerSide.Create(reader, context, Database.QueryMetadataCache);
+                    var query = IndexQueryServerSide.Create(reader, Database.QueryMetadataCache);
                     
                     tracker.Query = query.Query;
                     
@@ -220,7 +220,7 @@ namespace Raven.Server.Documents.Handlers
                 if (reader.TryGet("Query", out BlittableJsonReaderObject queryJson) == false || queryJson == null)
                     throw new BadRequestException("Missing 'Query' property.");
 
-                var query = IndexQueryServerSide.Create(queryJson, context, Database.QueryMetadataCache, QueryType.Update);
+                var query = IndexQueryServerSide.Create(queryJson, Database.QueryMetadataCache, QueryType.Update);
 
                 var patch = new PatchRequest(query.Metadata.GetUpdateBody(query.QueryParameters), PatchRequestType.Patch, query.Metadata.DeclaredFunctions);
 
@@ -310,7 +310,7 @@ namespace Raven.Server.Documents.Handlers
                 if (reader.TryGet("Query", out BlittableJsonReaderObject queryJson) == false || queryJson == null)
                     throw new BadRequestException("Missing 'Query' property.");
 
-                var query = IndexQueryServerSide.Create(queryJson, context, Database.QueryMetadataCache, QueryType.Update);
+                var query = IndexQueryServerSide.Create(queryJson, Database.QueryMetadataCache, QueryType.Update);
 
                 var patch = new PatchRequest(query.Metadata.GetUpdateBody(query.QueryParameters), PatchRequestType.Patch, query.Metadata.DeclaredFunctions);
 
---
+++ test/SlowTests/Issues/RavenDB_11284.cs
@@ -0,0 +1,23 @@
+﻿using FastTests;
+using Raven.Server.Documents.Queries;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_11284 : NoDisposalNeeded
+    {
+        [Fact]
+        public void Should_cache_metadata_of_queries_without_parameters()
+        {
+            var cache = new QueryMetadataCache();
+
+            Assert.False(cache.TryGetMetadata(new IndexQueryServerSide("from Users order by Name"), out var metadataHash, out var metadata));
+
+            Assert.NotEqual((ulong)0, metadataHash);
+
+            cache.MaybeAddToCache(new QueryMetadata("from Users order by Name", null, metadataHash), "test");
+
+            Assert.True(cache.TryGetMetadata(new IndexQueryServerSide("from Users order by Name"), out metadataHash, out metadata));
+        }
+    }
+}
