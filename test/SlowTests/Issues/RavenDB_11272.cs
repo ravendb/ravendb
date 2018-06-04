d9c74ff13c5b0336644bbbe50b8054647434e821
Arkadiusz Palinski <arek@ravendb.net>
2018-06-04 11:16:38 +0200

    RavenDB-11272 When matching the query by id() (or sort by id()) we need to filter out auto indexes that have no results. We'll open the read tx only on the first attempt to match the index as we cache the selected auto index anyway (metadata cache, QueryMetadata.AutoIndexName)

---
+++ test/SlowTests/Issues/RavenDB_11272.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using FastTests;
+using Raven.Server.Documents.Queries;
+using Raven.Tests.Core.Utils.Entities;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_11272 : RavenTestBase
+    {
+        [Fact]
+        public void Create_empty_index_and_sort_by_id()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User());
+                    
+                    session.SaveChanges();
+
+                    // it will create empty auto index (no entries) as users collection doesn't have 'NonExistingProperty' prop
+                    var results = session.Advanced.RawQuery<User>("from Users order by NonExistingProperty").Statistics(out var stats).ToList();
+
+                    Assert.Equal(0, results.Count);
+                    Assert.Equal("Auto/Users/ByNonExistingProperty", stats.IndexName);
+
+                    results = session.Advanced.RawQuery<User>("from Users order by id()").Statistics(out stats).ToList();
+
+                    Assert.Equal(1, results.Count);
+                    Assert.Equal("Auto/Users/ById()", stats.IndexName);
+                }
+            }
+        }
+    }
+}
