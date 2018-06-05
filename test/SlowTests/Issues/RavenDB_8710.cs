269d61451867cdfe3105f9ef6bd58d19ca3405f1
maximburyak <maxim@ayende.com>
2018-06-05 18:37:57 +0200

    RavenDB-11250: Allow to write to log in patch using console.log as we as output

---
+++ test/SlowTests/Issues/RavenDB_8710.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FastTests;
+using Raven.Client.Documents.Operations;
+using Raven.Tests.Core.Utils.Entities;
+using Sparrow.Json;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_8710:RavenTestBase
+    {
+        [Fact]
+        public void CanOverwrite()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {                    
+                    session.Store(new Person
+                    {
+                        Name = "John Doe"
+                    }, "people/1");
+                    
+                    session.SaveChanges();
+                }                
+
+                var reqEx = store.GetRequestExecutor();
+
+                using (reqEx.ContextPool.AllocateOperationContext(out var context))
+                {
+                    PatchOperation operation = new PatchOperation(
+                        id: "people/1",
+                        changeVector: null,
+                        patch: new PatchRequest
+                        {
+                        Script = @"this.NewField= 'new value';
+                        console.log('foo')"
+                        },
+                        patchIfMissing: null);
+
+                    var cmd = operation.GetCommand(store, store.Conventions, context, reqEx.Cache, true, false);
+                    store.Commands().Execute(cmd);
+                    Assert.Equal("foo", (cmd.Result.Debug["Info"] as BlittableJsonReaderArray)[0].ToString());
+                }
+            }
+        }
+    }
+}
