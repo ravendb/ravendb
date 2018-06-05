601c742d2a124a53d261b42c48f4e0ce2b9c01a3
maximburyak <maxim@ayende.com>
2018-06-05 08:57:37 +0200

    RavenDB-11250: Make sure that store is initialized when getting subscription worker

---
+++ test/SlowTests/Client/Subscriptions/RavenDB_11250.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FastTests;
+using Orders;
+using Raven.Client.Documents;
+using Xunit;
+
+namespace SlowTests.Client.Subscriptions
+{
+    public class RavenDB_11250:RavenTestBase
+    {
+        [Fact] 
+        public async Task SubscriptionShouldNotAllowStartWorkingIfItsStoreIsNotInitialized()
+        {
+            using (var store = GetDocumentStore())
+            {                
+                using (var unInitializedStore = new DocumentStore
+                {
+                    Urls= store.Urls,
+                    Database = store.Database
+                })
+                {
+                    await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+                    {
+                        await unInitializedStore.Subscriptions.CreateAsync<Order>(x => x.Lines.Count > 5);
+                    });                    
+                }
+
+                var subsName = await store.Subscriptions.CreateAsync<Order>(x => x.Lines.Count > 5);
+
+                using (var unInitializedStore = new DocumentStore
+                {
+                    Urls = store.Urls,
+                    Database = store.Database
+                })
+                {
+                    Assert.Throws<InvalidOperationException>(() =>
+                    {
+                        unInitializedStore.Subscriptions.GetSubscriptionWorker<Order>(subsName);
+                    });
+                }
+
+                using (var unInitializedStore = new DocumentStore
+                {
+                    Urls = store.Urls,
+                    Database = store.Database
+                })
+                {
+                    await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+                    {
+                        await unInitializedStore.Subscriptions.DeleteAsync(subsName);
+                    });
+                }
+
+                using (var unInitializedStore = new DocumentStore
+                {
+                    Urls = store.Urls,
+                    Database = store.Database
+                })
+                {
+                    await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+                    {
+                        await unInitializedStore.Subscriptions.GetSubscriptionStateAsync(subsName);
+                    });
+                }
+
+                using (var unInitializedStore = new DocumentStore
+                {
+                    Urls = store.Urls,
+                    Database = store.Database
+                })
+                {
+                    await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+                    {
+                        await unInitializedStore.Subscriptions.GetSubscriptionsAsync(0,1);
+                    });
+                }
+
+                using (var unInitializedStore = new DocumentStore
+                {
+                    Urls = store.Urls,
+                    Database = store.Database
+                })
+                {
+                    await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+                    {
+                        await unInitializedStore.Subscriptions.DropConnectionAsync(subsName);
+                    });
+                }
+            }
+        }
+    }
+}
