5f10716882027265dc5f5c2898a8e05d207255e9
Pawel Pekrol <ppekrol@ravendb.net>
2018-06-07 11:40:12 +0200

    RavenDB-11133 adjustments

---
+++ src/Raven.Client/Exceptions/ClientVersionMismatchException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Raven.Client.Exceptions
+{
+    public class ClientVersionMismatchException : RavenException
+    {
+        public ClientVersionMismatchException()
+        {
+        }
+
+        public ClientVersionMismatchException(string message) : base(message)
+        {
+        }
+
+        public ClientVersionMismatchException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
