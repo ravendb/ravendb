654bf1ba7d73966a6c505cb82cfd81d543b423ba
Pawel Pekrol <ppekrol@ravendb.net>
2018-06-07 20:34:35 +0200

    RavenDB-10991 If offline migration of a database takes longer than Databases.MaxIdleTimeInSec, the operation is canceled

---
+++ test/SlowTests/Issues/RavenDB_10991.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.ServerWide;
+using Raven.Client.ServerWide.Operations;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_10991 : RavenTestBase
+    {
+        [Fact]
+        public async Task PreventFromUnloadingShouldWork()
+        {
+            DoNotReuseServer();
+
+            var name = "RavenDB_10991" + Guid.NewGuid();
+
+            var landlord = Server.ServerStore.DatabasesLandlord;
+
+            using (var store = GetDocumentStore())
+            {
+                var doc = new DatabaseRecord(name);
+
+                store.Maintenance.Server.Send(new CreateDatabaseOperation(doc));
+
+                try
+                {
+                    var database = await landlord.TryGetOrCreateResourceStore(name);
+
+                    database.Configuration.Core.RunInMemory = false; // in memory databases aren't unloaded
+
+                    database.ResetIdleTime();
+
+                    landlord.LastRecentlyUsed.AddOrUpdate(database.Name, DateTime.MinValue, (_, time) => DateTime.MinValue);
+
+                    foreach (var env in database.GetAllStoragesEnvironment())
+                        env.Environment.ResetLastWorkTime();
+
+                    database.LastAccessTime = DateTime.MinValue;
+
+                    using (database.PreventFromUnloading()) // should prevent from unloading
+                        Server.ServerStore.IdleOperations(null);
+
+                    Assert.True(landlord.LastRecentlyUsed.TryGetValue(name, out _));
+
+                    Server.ServerStore.IdleOperations(null); // should unload
+
+                    Assert.False(landlord.LastRecentlyUsed.TryGetValue(name, out _));
+                }
+                finally
+                {
+                    store.Maintenance.Server.Send(new DeleteDatabasesOperation(name, true));
+                }
+            }
+        }
+    }
+}
