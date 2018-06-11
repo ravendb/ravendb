7459d1b66ec72602335064352ecfd1477aec0e70
Grisha <grisha@ayende.com>
2018-06-11 19:10:10 +0300

    RavenDB-11306 - Expose the Server Version Info for the Studio

--- src/Raven.Client/ServerWide/Operations/BuildNumber.cs
+++ src/Raven.Client/ServerWide/Operations/BuildNumber.cs
@@ -9,5 +9,33 @@ namespace Raven.Client.ServerWide.Operations
         public string CommitHash { get; set; }
 
         public string FullVersion { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as BuildNumber);
+        }
+
+        private bool Equals(BuildNumber other)
+        {
+            if (other == null)
+                return false;
+
+            return string.Equals(ProductVersion, other.ProductVersion) && 
+                   BuildVersion == other.BuildVersion && 
+                   string.Equals(CommitHash, other.CommitHash) && 
+                   string.Equals(FullVersion, other.FullVersion);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (ProductVersion != null ? ProductVersion.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ BuildVersion;
+                hashCode = (hashCode * 397) ^ (CommitHash != null ? CommitHash.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (FullVersion != null ? FullVersion.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
     }
 }
