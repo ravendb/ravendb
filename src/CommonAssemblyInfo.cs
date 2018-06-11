6bfee2b2797a47059c4b5684f046226776d674fe
Pawel Pekrol <ppekrol@ravendb.net>
2018-06-11 20:39:35 +0200

    RavenDB-9540 fixed AssemblyVersion calculation

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,11 +1,11 @@
 ﻿using System.Reflection;
 using System.Resources;
 
-[assembly: AssemblyCopyright("© Hibernating Rhinos 2004 - 2016 All rights reserved.")]
+[assembly: AssemblyCopyright("© Hibernating Rhinos 2004 - 2018 All rights reserved.")]
 
-[assembly: AssemblyVersion("4.0.4")]
-[assembly: AssemblyFileVersion("4.0.4.40")]
-[assembly: AssemblyInformationalVersion("4.0.4")]
+[assembly: AssemblyVersion("4.0.5")]
+[assembly: AssemblyFileVersion("4.0.5.40")]
+[assembly: AssemblyInformationalVersion("4.0.5")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
