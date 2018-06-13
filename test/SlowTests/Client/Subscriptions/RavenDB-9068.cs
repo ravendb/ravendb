4e49364408f6802dac551b5c46c3b0ecca779389
Pawel Pekrol <ppekrol@ravendb.net>
2018-06-13 14:14:30 +0200

    RavenDB-11326 fixing test

--- test/SlowTests/Client/Subscriptions/RavenDB-9068.cs
+++ test/SlowTests/Client/Subscriptions/RavenDB-9068.cs
@@ -9,7 +9,7 @@ using Xunit;
 
 namespace SlowTests.Client.Subscriptions
 {
-    public class RavenDB_9068:RavenTestBase
+    public class RavenDB_9068 : RavenTestBase
     {
         private readonly TimeSpan _reasonableWaitTime = Debugger.IsAttached ? TimeSpan.FromSeconds(60 * 10) : TimeSpan.FromSeconds(50);
         [Fact]
@@ -22,19 +22,27 @@ namespace SlowTests.Client.Subscriptions
                     await session.StoreAsync(new User());
                     await session.SaveChangesAsync();
                 }
+
                 var subscriptionId = await store.Subscriptions.CreateAsync<User>();
                 var subscription = store.Subscriptions.GetSubscriptionWorker<User>(subscriptionId);
                 var cts = new CancellationTokenSource();
                 var subscriptionTask = subscription.Run(x => { }, cts.Token);
                 cts.Cancel();
-                Assert.True(await Assert.ThrowsAsync<OperationCanceledException>(() => subscriptionTask).WaitAsync(_reasonableWaitTime));
+
+                var task = Assert.ThrowsAnyAsync<Exception>(() => subscriptionTask);
+                Assert.True(await task.WaitAsync(_reasonableWaitTime));
+                var e = await task;
+                Assert.True(e is OperationCanceledException || e is TaskCanceledException);
 
                 subscription = store.Subscriptions.GetSubscriptionWorker<User>(subscriptionId);
                 cts = new CancellationTokenSource();
                 subscriptionTask = subscription.Run(x => Task.CompletedTask, cts.Token);
                 cts.Cancel();
-                Assert.True(await Assert.ThrowsAsync<OperationCanceledException>(() => subscriptionTask).WaitAsync(_reasonableWaitTime));
-                
+
+                task = Assert.ThrowsAnyAsync<Exception>(() => subscriptionTask);
+                Assert.True(await task.WaitAsync(_reasonableWaitTime));
+                e = await task;
+                Assert.True(e is OperationCanceledException || e is TaskCanceledException);
             }
         }
     }
