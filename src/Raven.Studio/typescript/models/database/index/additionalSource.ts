dd74928ad706ce2ec4c7ccc28e3d720121cd7b05
Danielle Greenberg <danielle.9897@gmail.com>
2018-06-13 10:50:02 +0300

    RavenDB-11279 Edit Index View - Save button not updating

--- src/Raven.Studio/typescript/models/database/index/additionalSource.ts
+++ src/Raven.Studio/typescript/models/database/index/additionalSource.ts
@@ -1,14 +1,23 @@
 /// <reference path="../../../../typings/tsd.d.ts"/>
+import jsonUtil = require("common/jsonUtil");
 
 class additionalSource {
     name = ko.observable<string>();
     code = ko.observable<string>();
 
+    dirtyFlag: () => DirtyFlag;
+    
     static create(name: string, code: string) {
         const item = new additionalSource();
         item.name(name);
         item.code(code);
         return item;
+    }
+
+    constructor() {
+        this.dirtyFlag = new ko.DirtyFlag([
+            this.name
+        ], false, jsonUtil.newLineNormalizingHashFunction);
     }
 }
 
