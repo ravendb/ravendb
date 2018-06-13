2d7a65be5dc5542766c06afa799644460c6938dd
unknown <karmeli87@gmail.com>
2018-06-13 22:54:14 +0300

    RavenDB-11354 fix PR comments

--- src/Raven.Server/NotificationCenter/NotificationsBase.cs
+++ src/Raven.Server/NotificationCenter/NotificationsBase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Raven.Client.Util;
 using Raven.Server.Background;
@@ -17,30 +18,28 @@ namespace Raven.Server.NotificationCenter
         private TaskCompletionSource<object> _newWebSocket = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
         private TaskCompletionSource<object> _allWebSocketsRemoved = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
 
-        public Task WaitForRemoveAllWebSocketClients
-        {
-            get
-            {
-                lock (_watchersLock)
-                {
-                    return _websocketClients == 0 ? Task.CompletedTask : _allWebSocketsRemoved.Task;
-                }
-            }
-        }
-
         protected ConcurrentSet<ConnectedWatcher> Watchers { get; }
         protected List<BackgroundWorkBase> BackgroundWorkers { get; }
 
         private volatile int _websocketClients;
 
+        private long _haveClients;
+
         public Task WaitForAnyWebSocketClient
         {
             get
             {
-                lock (_watchersLock)
-                {
-                    return _websocketClients > 0 ? Task.CompletedTask : _newWebSocket.Task;
-                }
+                var task = _newWebSocket.Task;
+                return Interlocked.Read(ref _haveClients) == 1 ? Task.CompletedTask : task;
+            }
+        }
+
+        public Task WaitForRemoveAllWebSocketClients
+        {
+            get
+            {
+                var task = _allWebSocketsRemoved.Task;
+                return Interlocked.Read(ref _haveClients) == 0 ? Task.CompletedTask : task;
             }
         }
 
@@ -69,7 +68,7 @@ namespace Raven.Server.NotificationCenter
 
                 if (watcher.Writer is NotificationCenterWebSocketWriter)
                 {
-                    if (_websocketClients == 0)
+                    if (Interlocked.CompareExchange(ref _haveClients, 1, 0) == 0)
                     {
                         TaskExecutor.CompleteAndReplace(ref _newWebSocket);
                     }
@@ -93,6 +92,7 @@ namespace Raven.Server.NotificationCenter
                         _websocketClients--;
                         if (_websocketClients == 0)
                         {
+                            Interlocked.Exchange(ref _haveClients, 0);
                             TaskExecutor.CompleteAndReplace(ref _allWebSocketsRemoved);
                         }
                     }
