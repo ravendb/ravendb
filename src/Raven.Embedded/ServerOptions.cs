347d3f94542856ace5512809e7ec04efcafaa23a
Idan Haim Shalom <haimdude@gmail.com>
2018-06-17 13:35:57 +0300

    RavenDB-7787 Add Raven.Embedded project (First Stage)

---
+++ src/Raven.Embedded/ServerOptions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Raven.Embedded
+{
+    public class ServerOptions
+    {
+        public string FmVersion { get; set; } = "2.1.0";
+
+        public List<string> CommandLineArgs { get; set; } = new List<string>
+        {
+            "--ServerUrl=http://127.0.0.1:0",
+            "--RunInMemory=true",
+            "--Setup.Mode=None"
+        };
+
+        public static ServerOptions Default = new ServerOptions();
+
+    }
+}
