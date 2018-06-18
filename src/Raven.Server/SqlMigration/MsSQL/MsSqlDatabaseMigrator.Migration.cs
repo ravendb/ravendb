b291fdae0df9c13c1de8918afec225f095871222
ml054 <marcin@ravendb.net>
2018-06-18 17:23:42 +0200

    RavenDB-8647 made sql migrator more generic

---
+++ src/Raven.Server/SqlMigration/MsSQL/MsSqlDatabaseMigrator.Migration.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using Raven.Server.SqlMigration.Model;
+using Raven.Server.SqlMigration.Schema;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Server.SqlMigration.MsSQL
+{
+    internal partial class MsSqlDatabaseMigrator : GenericDatabaseMigrator
+    {
+        protected override string FactoryName => "System.Data.SqlClient";
+        
+        public MsSqlDatabaseMigrator(string connectionString) : base(connectionString)
+        {
+        }
+
+        protected override string QuoteColumn(string columnName)
+        {
+            return $"[{columnName}]";
+        }
+
+        protected override string QuoteTable(string schema, string tableName)
+        {
+            return $"[{schema}].[{tableName}]";
+        }
+
+        private ColumnType MapColumnType(string type)
+        {
+            type = type.ToLower();
+
+            switch (type)
+            {
+                case "bigint":
+                case "decimal":
+                case "float":
+                case "int":
+                case "smallint":
+                case "tinyint":
+                    return ColumnType.Number;
+                case "bit":
+                    return ColumnType.Boolean;
+                case "binary":
+                case "varbinary":
+                case "image":
+                    return ColumnType.Binary;
+                case "char":
+                case "nchar":
+                case "ntext":
+                case "nvarchar":
+                case "datetime":
+                case "datetime2":
+                case "datetimeoffset":
+                case "time":
+                case "timestamp":
+                case "uniqueidentifier":
+                case "varchar":
+                    return ColumnType.String;
+
+                default:
+                    return ColumnType.Unsupported;
+            }
+        }
+
+        protected override string LimitRowsNumber(string inputQuery, int? rowsLimit)
+        {
+            if (rowsLimit.HasValue) 
+                return "select top " + rowsLimit + " rowsLimited.* from (" + inputQuery + ") rowsLimited";
+            
+            return inputQuery;
+        }
+
+        
+        protected override string GetSelectAllQueryForTable(string tableSchema, string tableName)
+        {
+            return "select * from " + QuoteTable(tableSchema, tableName);
+        }
+    }
+}
---
+++ src/Raven.Server/SqlMigration/MySQL/MySqlDatabaseMigrator.Migration.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using MySql.Data.MySqlClient;
+using Raven.Server.SqlMigration.Model;
+using Raven.Server.SqlMigration.MsSQL;
+using Raven.Server.SqlMigration.Schema;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Server.SqlMigration.MySQL
+{
+    internal partial class MySqlDatabaseMigrator : GenericDatabaseMigrator
+    {
+        protected override string FactoryName => "MySql.Data.MySqlClient";
+
+        public MySqlDatabaseMigrator(string connectionString) : base(connectionString)
+        {
+        }
+
+        protected override string QuoteColumn(string columnName)
+        {
+            return $"`{columnName}`";
+        }
+
+        protected override string QuoteTable(string schema, string tableName)
+        {
+            return $"`{schema}`.`{tableName}`";
+        }
+        
+        private ColumnType MapColumnType(string type)
+        {
+            type = type.ToLower();
+
+            switch (type)
+            {
+                case "varchar":
+                case "longtext":
+
+                case "enum":
+                case "text":
+                case "timestamp":
+                case "char":
+                case "set":
+                case "mediumtext":
+                case "time":
+                case "date":
+                case "datetime":
+                case "tinytext":
+                    return ColumnType.String;
+
+                case "bigint":
+                case "int":
+                case "tinyint":
+                case "decimal":
+                case "double":
+                case "float":
+                case "real":
+                case "year":
+                case "smallint":
+                case "mediumint":
+                    return ColumnType.Number;
+
+                case "bit":
+                    return ColumnType.Boolean;
+
+                case "tinyblob":
+                case "blob":
+                case "mediumblob":
+                case "longblob":
+                case "binary":
+                case "varbinary":
+                    return ColumnType.Binary;
+
+                default:
+                    return ColumnType.Unsupported;
+            }
+        }
+        
+        protected override string LimitRowsNumber(string inputQuery, int? rowsLimit)
+        {
+            if (rowsLimit.HasValue) 
+                return "select rowsLimited.* from (" + inputQuery + ") rowsLimited limit " + rowsLimit;
+            
+            return inputQuery;
+        }
+
+        protected override string GetSelectAllQueryForTable(string tableSchema, string tableName)
+        {
+            return "select * from " + QuoteTable(tableSchema, tableName);
+        }
+    }
+}
