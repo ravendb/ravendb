f4fa1d868d6774ab2405463ace3be3af38747094
Federico Andres Lois <federico.lois@corvalius.com>
2018-06-27 12:22:45 -0300

    [WIP] Refactoring

---
+++ src/Sparrow/Allocators.Native.cs
@@ -0,0 +1,33 @@
+﻿namespace Sparrow
+{
+    public static class NativeBlockAllocator
+    {
+        public struct Default : INativeBlockOptions
+        {
+            public bool UseSecureMemory => false;
+            public bool ElectricFenceEnabled => false;
+            public bool Zeroed => false;
+        }
+
+        public struct DefaultZero : INativeBlockOptions
+        {
+            public bool UseSecureMemory => false;
+            public bool ElectricFenceEnabled => false;
+            public bool Zeroed => true;
+        }
+
+        public struct Secure : INativeBlockOptions
+        {
+            public bool UseSecureMemory => true;
+            public bool ElectricFenceEnabled => false;
+            public bool Zeroed => false;
+        }
+
+        public struct ElectricFence : INativeBlockOptions
+        {
+            public bool UseSecureMemory => false;
+            public bool ElectricFenceEnabled => true;
+            public bool Zeroed => false;
+        }
+    }
+}
---
+++ src/Sparrow/Allocators.ThreadAffine.cs
@@ -0,0 +1,16 @@
+﻿using Sparrow.Global;
+
+namespace Sparrow
+{
+    public static class ThreadAffineBlockAllocator
+    {
+        public struct Default : IThreadAffineBlockOptions
+        {
+            public bool UseSecureMemory => false;
+            public bool ElectricFenceEnabled => false;
+            public bool Zeroed => false;
+            public int BlockSize => 4 * Constants.Size.Kilobyte;
+            public ThreadAffineWorkload Workload => ThreadAffineWorkload.Default;
+        }
+    }
+}
