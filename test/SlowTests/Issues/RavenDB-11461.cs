7e062514a50e6d27273f9fde122a3882447e2b40
Ayende Rahien <Ayende@ayende.com>
2018-06-28 19:13:43 +0300

    RavenDB-11461 - Fixing an error where we wouldn't translate a query with two boolean properties.

---
+++ test/SlowTests/Issues/RavenDB-11461.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FastTests;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_11461 : RavenTestBase
+    {
+        public class Team
+        {
+            public bool IsGreen;
+            public bool IsYellow;
+            public string Name;
+        }
+
+        [Fact]
+        public void CanQueryOnTwoBools()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Team
+                    {
+                        IsGreen = true,
+                        IsYellow = true,
+                        Name = "Green And Yellow"
+                    });
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var r = session.Query<Team>()
+                        .Where(t => t.IsYellow && t.IsGreen)
+                        .ToList();
+
+                    Assert.NotEmpty(r);
+                }
+            }
+        }
+    }
+}
