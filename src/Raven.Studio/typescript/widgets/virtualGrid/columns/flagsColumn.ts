5e989fce6a98f0562702bf784b6ff18457c61a5a
ml054 <marcin@ravendb.net>
2018-07-02 13:48:58 +0200

    RavenDB-9937 Add attachment marker on the grid

---
+++ src/Raven.Studio/typescript/widgets/virtualGrid/columns/flagsColumn.ts
@@ -0,0 +1,41 @@
+﻿/// <reference path="../../../../typings/tsd.d.ts"/>
+
+import virtualColumn = require("widgets/virtualGrid/columns/virtualColumn");
+import document = require("models/database/documents/document");
+import virtualGridController = require("widgets/virtualGrid/virtualGridController");
+import utils = require("widgets/virtualGrid/virtualGridUtils");
+import textColumn = require("widgets/virtualGrid/columns/textColumn");
+
+class flagsColumn implements virtualColumn {
+    constructor(protected gridController: virtualGridController<any>) {
+    }
+
+    width = "65px";
+    
+    header = `<div style="padding-left: 8px;">🏳️</div>`; //TODO: change to icon-flags
+
+    renderCell(item: document, isSelected: boolean): string {
+        const metadata = item.__metadata;
+        
+        const extraClasses = [] as Array<string>;
+        
+        if (metadata) {
+            const flags = (metadata.flags || "").split(",").map(x => x.trim());
+            
+            if (_.includes(flags, "HasAttachments")) {
+                extraClasses.push("attachments");
+            }
+            if (_.includes(flags, "HasRevisions")) {
+                extraClasses.push("revisions");
+            }
+            if (_.includes(flags, "HasCounters")) {
+                extraClasses.push("counters");
+            }
+        }
+        
+        return `<div class="cell text-cell flags-cell ${extraClasses.join(" ")}" style="width: ${this.width}"><i class="icon-attachment"></i><i class="icon-revisions"></i><i class="icon-new-counter"></i></div>`;
+    }
+
+}
+
+export = flagsColumn;
