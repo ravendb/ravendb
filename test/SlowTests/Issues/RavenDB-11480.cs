af991c46b4ebbd06fde7b698307f123df7b8599b
aviv86 <aviv@ayende.com>
2018-07-11 11:58:37 +0200

    RavenDB-11480 : RQL load with index stored fields - detect that value should be taken from index, not from document

---
+++ test/SlowTests/Issues/RavenDB-11480.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using FastTests;
+using Orders;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Linq;
+using Raven.Client.Documents.Queries;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_11480 : RavenTestBase
+    {
+        private class Index1 : AbstractIndexCreationTask<Company>
+        {
+            public class Result
+            {
+                public string OrderId { get; set; }
+            }
+
+            public Index1()
+            {
+                Map = companies => from c in companies
+                    select new
+                    {
+                        OrderId = "orders/1-A"
+                    };
+
+                Store("OrderId", FieldStorage.Yes);
+            }
+        }
+
+        [Fact]
+        public void CanLoadOnStoredField()
+        {
+            using (var store = GetDocumentStore())
+            {
+                new Index1().Execute(store);
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Company
+                    {
+                        Name = "HR"
+                    });
+
+                    session.Store(new Order
+                    {
+                        Company = "HR-Order"
+                    }, "orders/1-A");
+
+                    session.SaveChanges();
+                }
+
+                WaitForIndexing(store);
+
+                using (var session = store.OpenSession())
+                {
+                    var query = from c in session.Query<Index1.Result, Index1>()
+                                let o = RavenQuery.Load<Order>(c.OrderId)
+                                select o;
+
+                    var order = query.First();
+
+                    Assert.Equal("HR-Order", order.Company);
+                }
+            }
+        }
+    }
+}
