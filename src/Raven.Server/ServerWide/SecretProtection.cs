56e90e7dee2c9fbae79dabdf5aa45fe2916d729a
iftahbe <iftahbe@gmail.com>
2018-07-11 16:56:55 +0300

    RavenDB-10799 workaround for multiple localKeyId in certificate

--- src/Raven.Server/ServerWide/SecretProtection.cs
+++ src/Raven.Server/ServerWide/SecretProtection.cs
@@ -629,7 +629,9 @@ namespace Raven.Server.ServerWide
 
         private static void ValidatePrivateKey(string source, string certificatePassword, byte[] rawData, out AsymmetricKeyEntry pk)
         {
-            var store = new Pkcs12Store();
+		    // Using a partial copy of the Pkcs12Store class
+			// Workaround for https://github.com/dotnet/corefx/issues/30946
+            var store = new PkcsStore();
             store.Load(new MemoryStream(rawData), certificatePassword?.ToCharArray() ?? Array.Empty<char>());
             pk = null;
             foreach (string alias in store.Aliases)
