8d642507aa34780e46bed8983c096a8732897e40
Ayende Rahien <Ayende@ayende.com>
2018-07-12 16:02:53 +0300

    RavenDB-11547 Bulk insert error hides real cause of the issue

--- src/Raven.Client/Exceptions/Documents/BulkInsert/BulkInsertProtocolViolationException.cs
+++ src/Raven.Client/Exceptions/Documents/BulkInsert/BulkInsertProtocolViolationException.cs
@@ -1,9 +1,16 @@
-﻿namespace Raven.Client.Exceptions.Documents.BulkInsert
+﻿using System;
+
+namespace Raven.Client.Exceptions.Documents.BulkInsert
 {
     public class BulkInsertProtocolViolationException : RavenException
     {
         public BulkInsertProtocolViolationException(string message) : base(message)
         {
+        }
+
+        public BulkInsertProtocolViolationException(string message, Exception inner) : base(message, inner)
+        {
+
         }
     }
 }
