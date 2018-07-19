d381d396d4457f3048e83d1c806c229c455241f0
ml054 <marcin@ravendb.net>
2018-07-19 14:14:54 +0200

    RavenDB-11527 Creating counters with the same name

--- src/Raven.Studio/typescript/viewmodels/database/documents/setCounterDialog.ts
+++ src/Raven.Studio/typescript/viewmodels/database/documents/setCounterDialog.ts
@@ -2,6 +2,7 @@ import dialogViewModelBase = require("viewmodels/dialogViewModelBase");
 import database = require("models/resources/database")
 import setCounterCommand = require("commands/database/documents/counters/setCounterCommand");
 import eventsCollector = require("common/eventsCollector");
+import getCountersCommand = require("commands/database/documents/counters/getCountersCommand");
 
 class setCounterDialog extends dialogViewModelBase {
    
@@ -10,7 +11,7 @@ class setCounterDialog extends dialogViewModelBase {
     createNewCounter = ko.observable<boolean>();
     counterName = ko.observable<string>();
     
-    totalValue = ko.observable<number>();
+    totalValue = ko.observable<number>(0);
     newTotalValue = ko.observable<number>();
     
     counterValuesPerNode = ko.observableArray<nodeCounterValue>();
@@ -27,13 +28,14 @@ class setCounterDialog extends dialogViewModelBase {
     constructor(counter: counterItem, private documentId: string,  private db: database) {
         super();
         
-        this.createNewCounter(!counter.counterName);
-        this.counterName(counter.counterName);
-        
-        const currentValue = this.createNewCounter() ? 0: counter.totalCounterValue;
-        this.totalValue(currentValue); 
-        
-        this.counterValuesPerNode(counter.counterValuesPerNode);
+        if (counter) {
+            this.createNewCounter(false);
+            this.counterName(counter.counterName);
+            this.totalValue(counter.totalCounterValue);
+            this.counterValuesPerNode(counter.counterValuesPerNode);
+        } else {
+            this.createNewCounter(true);
+        }
      
         this.initValidation();
     }
@@ -42,17 +44,38 @@ class setCounterDialog extends dialogViewModelBase {
         if (this.isValid(this.validationGroup)) {
             eventsCollector.default.reportEvent("counter", "update");
 
+            const counterName = this.counterName();
+            
             this.spinners.update(true);
 
             const counterDeltaValue = this.newTotalValue() - this.totalValue();
+            
+            const saveTask = () => {
+                new setCounterCommand(counterName, counterDeltaValue, this.documentId, this.db)
+                    .execute()
+                    .done(() => this.result.resolve())
+                    .always(() => {
+                        this.spinners.update(false);
+                        this.close();
+                    });
+            };
 
-            new setCounterCommand(this.counterName(), counterDeltaValue, this.documentId, this.db)
-                .execute()
-                .done(() => this.result.resolve())
-                .always(() => {
-                    this.spinners.update(false);
-                    this.close();
-                })
+            if (this.createNewCounter()) {
+                new getCountersCommand(this.documentId, this.db)
+                    .execute()
+                    .done((counters: Raven.Client.Documents.Operations.Counters.CountersDetail) => {
+                        if (counters.Counters.find(x => x.CounterName === counterName)) {
+                            this.spinners.update(false);
+                            
+                            this.counterName.setError("Counter '" + counterName + "' already exists.");
+                        } else {
+                            saveTask();
+                        }
+                    })
+                    .fail(() => this.spinners.update(false))
+            } else {
+                saveTask();
+            }
         }
     }
     
