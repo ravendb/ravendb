ea568725688f2e085e8c1da70da5b221de4c28d7
Grzegorz Lachowski <gregolsky@ravendb.net>
2018-07-19 10:04:39 +0200

    RavenDB-11582 Prepare infrastructure for interversion tests

---
+++ test/Tests.Infrastructure/InterversionTest/ConfigurableRavenServerLocator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Raven.TestDriver;
+using Sparrow.Platform;
+using Tests.Infrastructure.InterversionTest;
+
+namespace Tests.Infrastructure
+{
+    public class ConfigurableRavenServerLocator : RavenServerLocator
+    {
+        private readonly string _serverDirPath;
+
+        public ConfigurableRavenServerLocator(string serverDirPath)
+        {
+            _serverDirPath = serverDirPath;
+        }
+
+        public override string ServerPath
+        {
+            get
+            {
+                return Path.Combine(
+                    _serverDirPath, 
+                    "Server", 
+                    PlatformDetails.RunningOnPosix ? "Raven.Server" : "Raven.Server.exe");
+            }
+        }
+
+    }
+}
