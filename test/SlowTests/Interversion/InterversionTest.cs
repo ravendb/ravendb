d353c1bfed6f2691eac84c344662d0aaaa30a098
Grzegorz Lachowski <gregolsky@ravendb.net>
2018-07-23 11:02:17 +0200

    RavenDB-11582 use ioextensions, allow passing doc store options

--- test/SlowTests/Interversion/InterversionTest.cs
+++ test/SlowTests/Interversion/InterversionTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Raven.Server.Utils;
+using Raven.Tests.Core.Utils.Entities;
 using Tests.Infrastructure;
 using Xunit;
 
@@ -11,13 +12,36 @@ namespace SlowTests.Interversion
     public class InterversionTest : InterversionTestBase
     {
         [Fact]
-        public async void Test()
+        public async Task Test()
         {
             var getStoreTask405 = GetDocumentStoreAsync("4.0.5");
             var getStoreTask406patch = GetDocumentStoreAsync("4.0.6-patch-40047");
             var getStoreTaskCurrent = GetDocumentStoreAsync();
-            Task.WaitAll(getStoreTask405, getStoreTask406patch, getStoreTaskCurrent);
+            
+            await Task.WhenAll(getStoreTask405, getStoreTask406patch, getStoreTaskCurrent);
+
+            AssertStore(await getStoreTask405);
+            AssertStore(await getStoreTask406patch);
+            AssertStore(await getStoreTaskCurrent);
         }
 
+        private static void AssertStore(Raven.Client.Documents.IDocumentStore store)
+        {
+            using (var session = store.OpenSession())
+            {
+                session.Store(new Company()
+                {
+                    Name = "HR"
+                }, "companies/1");
+                session.SaveChanges();
+            }
+
+            using (var session = store.OpenSession())
+            {
+                var c = session.Load<Company>("companies/1");
+                Assert.NotNull(c);
+                Assert.Equal("HR", c.Name);
+            }
+        }
     }
 }
