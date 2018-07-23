9cdad7a5eafbc696b521bf7305f975ea6f43e61a
Grzegorz Lachowski <gregolsky@ravendb.net>
2018-07-23 13:21:44 +0200

    RavenDB-11582 add sync overload for GetDocumentStore()

--- test/SlowTests/Interversion/InterversionTest.cs
+++ test/SlowTests/Interversion/InterversionTest.cs
@@ -17,13 +17,12 @@ namespace SlowTests.Interversion
         {
             var getStoreTask405 = GetDocumentStoreAsync("4.0.5");
             var getStoreTask406patch = GetDocumentStoreAsync("4.0.6-patch-40047");
-            var getStoreTaskCurrent = GetDocumentStoreAsync();
 
-            await Task.WhenAll(getStoreTask405, getStoreTask406patch, getStoreTaskCurrent);
+            await Task.WhenAll(getStoreTask405, getStoreTask406patch);
 
             AssertStore(await getStoreTask405);
             AssertStore(await getStoreTask406patch);
-            AssertStore(await getStoreTaskCurrent);
+            AssertStore(GetDocumentStore());
         }
 
         private static void AssertStore(IDocumentStore store)
