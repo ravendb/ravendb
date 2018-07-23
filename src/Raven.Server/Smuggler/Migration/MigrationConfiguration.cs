bd95500edc5bc17dcad29cc204f927479a5194a8
efrat@ayende.com <efrat@ayende.com>
2018-07-23 09:06:20 +0300

    RavenDB-11577 Remove unnessery stuff

--- src/Raven.Server/Smuggler/Migration/MigrationConfiguration.cs
+++ src/Raven.Server/Smuggler/Migration/MigrationConfiguration.cs
@@ -35,8 +35,6 @@ namespace Raven.Server.Smuggler.Migration
 
         public int BuildVersion { get; set; }
 
-        public string ProductVersion { get; set; }
-
         public string ServerUrl { get; set; }
 
         public string UserName { get; set; }
