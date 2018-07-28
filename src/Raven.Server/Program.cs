9df7c1eb71040820492e9d7cf3bd25a5307c7c96
Grisha Kotler <grisha@ayende.com>
2018-07-29 02:41:18 +0300

    RavenDB-11642 - open browser correctly on osx

--- src/Raven.Server/Program.cs
+++ src/Raven.Server/Program.cs
@@ -151,7 +151,7 @@ namespace Raven.Server
                             new RuntimeSettings(Console.Out).Print();
 
                             if (CommandLineSwitches.LaunchBrowser)
-                                BrowserHelper.OpenStudioInBrowser(server.ServerStore.GetNodeHttpServerUrl());
+                                BrowserHelper.OpenStudioInBrowser(server.ServerStore.GetNodeHttpServerUrl(), out _);
 
                             new ClusterMessage(Console.Out, server.ServerStore).Print();
 
