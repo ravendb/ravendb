3ecce2861270a11d60db4dc3bb5784b2eb4a73eb
Michael Yarichuk <michael.yarichuk@gmail.com>
2018-07-29 16:37:36 +0300

    * move graph query AST classes to proper folders * finalizing changes to AST classes * added abstract class GraphQueryVisitor

---
+++ src/Raven.Server/Documents/Queries/AST/WithEdgesExpression.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raven.Server.Documents.Queries.AST
+{
+    public class WithEdgesExpression : QueryExpression
+    {
+        public QueryExpression Where;
+
+        public List<(QueryExpression Expression, OrderByFieldType FieldType, bool Ascending)> OrderBy;
+
+        public WithEdgesExpression(QueryExpression @where, List<(QueryExpression Expression, OrderByFieldType FieldType, bool Ascending)> orderBy)
+        {
+            if(@where == null && orderBy == null)
+                throw new ArgumentNullException($"{nameof(WithEdgesExpression)} should have either Where or OrderBy clauses.");
+
+            Where = @where;
+            OrderBy = orderBy;
+        }
+
+        public override string ToString() => GetText();
+        public override string GetText(IndexQueryServerSide parent) => GetText();
+
+        private string GetText()
+        {
+            var sb = new StringBuilder();
+
+            var visitor = new StringQueryVisitor(sb);
+
+            if (Where != null)
+            {
+                visitor.VisitWhereClause(Where);
+            }
+
+            if (OrderBy != null)
+            {
+                visitor.VisitOrderBy(OrderBy);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
