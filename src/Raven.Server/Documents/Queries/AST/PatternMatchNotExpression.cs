a495d85c58ed0d43bf989b7bed77aa26c9028da3
Michael Yarichuk <michael.yarichuk@gmail.com>
2018-07-29 16:37:36 +0300

    * move graph query AST classes to proper folders * finalizing changes to AST classes * added abstract class GraphQueryVisitor

---
+++ src/Raven.Server/Documents/Queries/AST/PatternMatchNotExpression.cs
@@ -0,0 +1,17 @@
+﻿namespace Raven.Server.Documents.Queries.AST
+{
+    public class PatternMatchNotExpression :  PatternMatchExpression
+    {
+        public PatternMatchExpression AppliedOn; // not ( pattern match expression )
+
+        public override string ToString()
+        {
+            return $"not ( {AppliedOn} )";
+        }
+
+        public override string GetText(IndexQueryServerSide parent)
+        {
+            return $"not ( {AppliedOn.GetText(parent)} )";
+        }
+    }
+}
