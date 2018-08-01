52634e582810e402849404f058907572f14c09a7
Pawel Pekrol <ppekrol@ravendb.net>
2018-08-01 13:28:24 +0200

    RavenDB-10113 added /admin/memory/gc endpoint

---
+++ src/Raven.Server/Documents/Handlers/Admin/AdminMemoryHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime;
+using System.Threading.Tasks;
+using Raven.Server.Routing;
+using Raven.Server.Web;
+
+namespace Raven.Server.Documents.Handlers.Admin
+{
+    public class AdminMemoryHandler : RequestHandler
+    {
+        [RavenAction("/admin/memory/gc", "GET", AuthorizationStatus.Operator)]
+        public Task CollectGarbage()
+        {
+            var loh = GetBoolValueQueryString("loh", required: false) ?? false;
+
+            if (loh)
+                GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+
+            return Task.CompletedTask;
+        }
+    }
+}
