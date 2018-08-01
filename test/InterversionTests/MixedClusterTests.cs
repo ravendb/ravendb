32f304ccf6ff8b03afbfbbe28d582e9a01bf9f79
Pawel Pekrol <ppekrol@ravendb.net>
2018-08-01 09:46:27 +0200

    RavenDB-11614 cleanup in MixedClusterTests

--- test/InterversionTests/MixedClusterTests.cs
+++ test/InterversionTests/MixedClusterTests.cs
@@ -1,123 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Net.Http;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
-using Raven.Client.Documents;
 using Raven.Client.ServerWide;
 using Raven.Client.ServerWide.Operations;
-using Raven.Client.Util;
-using Raven.Server;
-using Raven.Server.Config;
 using Raven.Tests.Core.Utils.Entities;
-using Tests.Infrastructure;
 using Xunit;
 
 namespace InterversionTests
 {
-    public class MixedClusterTests : InterversionTestBase
+    public class MixedClusterTests : MixedClusterTestBase
     {
-        public class ProcessNode
-        {
-            public string Version;
-            public Process Process;
-            public string Url;
-        }
-
-        public async Task<(RavenServer Leader, List<ProcessNode> Peers, List<RavenServer> LocalPeers)> CreateMixedCluster(string[] peers, int localPeers = 0)
-        {
-            var leaderServer = GetNewServer();
-            leaderServer.ServerStore.Engine.Bootstrap(leaderServer.WebUrl);
-
-            var nodeAdded = new ManualResetEvent(false);
-            leaderServer.ServerStore.Engine.TopologyChanged += (sender, clusterTopology) =>
-            {
-                if(clusterTopology.Promotables.Count == 0)
-                    nodeAdded.Set();
-            };
-
-            var local = new List<RavenServer>();
-            for (int i = 0; i < localPeers; i++)
-            {
-                var peer = GetNewServer();
-                await leaderServer.ServerStore.AddNodeToClusterAsync(peer.WebUrl);
-                Assert.True(nodeAdded.WaitOne(TimeSpan.FromSeconds(30)));
-                nodeAdded.Reset();
-                local.Add(peer);
-            }
-
-            var processes = new List<ProcessNode>();
-            foreach (var peer in peers)
-            {
-                var (url, process) = await GetServerAsync(peer);
-                await leaderServer.ServerStore.AddNodeToClusterAsync(url);
-                Assert.True(nodeAdded.WaitOne(TimeSpan.FromSeconds(30)));
-                nodeAdded.Reset();
-                processes.Add(new ProcessNode
-                {
-                    Version = peer,
-                    Process = process,
-                    Url = url
-                });
-            }
-
-            return (leaderServer, processes, local);
-        }
-
-        public async Task<(IDisposable Disposable, List<DocumentStore> Stores)> GetStores(RavenServer leader, List<ProcessNode> peers, List<RavenServer> local = null)
-        {
-            var stores = new List<DocumentStore>();
-
-            var leaderStore = GetDocumentStore(new Options
-            {
-                Server = leader,
-                CreateDatabase = false,
-                ModifyDocumentStore = s => s.Conventions.DisableTopologyUpdates = true
-            });
-
-            stores.Add(leaderStore);
-
-            if (local != null)
-                foreach (var ravenServer in local)
-                {
-                    var peerStore = GetDocumentStore(new Options
-                    {
-                        Server = ravenServer,
-                        CreateDatabase = false,
-                        ModifyDocumentStore = s => s.Conventions.DisableTopologyUpdates = true
-                    });
-                    stores.Add(peerStore);
-                }
-
-            foreach (var peer in peers)
-            {
-                var peerStore = await GetStore(peer.Url, peer.Process, null, new InterversionTestOptions
-                {
-                    CreateDatabase = false,
-                    ModifyDocumentStore = s => s.Conventions.DisableTopologyUpdates = true
-                });
-                stores.Add(peerStore);
-            }
-            
-            return (new DisposableAction(() =>
-            {
-                foreach (var documentStore in stores)
-                {
-                    documentStore.Dispose();
-                }
-
-                if (local != null)
-                    foreach (var ravenServer in local)
-                    {
-                        ravenServer.Dispose();
-                    }
-
-                leaderStore.Dispose();
-            }), stores);
-        }
-
         [Fact]
         public async Task ReplicationInMixedCluster_40Leader_with_two_41_nodes()
         {
@@ -329,90 +221,6 @@ namespace InterversionTests
                 storeA.Maintenance.Server.Send(new DeleteDatabasesOperation(storeA.Database, true));
                 storeA.Maintenance.Server.Send(new DeleteDatabasesOperation(dbName, true));
             }
-        }
-
-        private static async Task<string> CreateDatabase(IDocumentStore store, int replicationFactor = 1, [CallerMemberName] string dbName = null)
-        {
-            var doc = new DatabaseRecord(dbName)
-            {
-                Settings =
-                {
-                    [RavenConfiguration.GetKey(x => x.Replication.ReplicationMinimalHeartbeat)] = "1",
-                    [RavenConfiguration.GetKey(x => x.Replication.RetryReplicateAfter)] = "1",
-                    [RavenConfiguration.GetKey(x => x.Core.RunInMemory)] = true.ToString(),
-                    [RavenConfiguration.GetKey(x => x.Core.ThrowIfAnyIndexCannotBeOpened)] = true.ToString(),
-                    [RavenConfiguration.GetKey(x => x.Indexing.MinNumberOfMapAttemptsAfterWhichBatchWillBeCanceledIfRunningLowOnMemory)] = int.MaxValue.ToString()
-                }
-            };
-
-            var databasePutResult = await store.Maintenance.Server.SendAsync(new CreateDatabaseOperation(doc, replicationFactor));
-            Assert.Equal(replicationFactor, databasePutResult.NodesAddedTo.Count);
-            return dbName;
-        }
-
-        private static async Task AddNodeToCluster(DocumentStore store, string url)
-        {
-            var addNodeRequest = await store.GetRequestExecutor().HttpClient.SendAsync(
-                new HttpRequestMessage(HttpMethod.Put, $"{store.Urls[0]}/admin/cluster/node?url={url}"));
-            Assert.True(addNodeRequest.IsSuccessStatusCode);
-        }
-
-        private async Task<DocumentStore> GetStore(string serverUrl, Process serverProcess = null, [CallerMemberName] string database = null, InterversionTestOptions options = null)
-        {
-            options = options ?? InterversionTestOptions.Default;
-            var name = database ?? GetDatabaseName(null);
-
-            if (options.ModifyDatabaseName != null)
-                name = options.ModifyDatabaseName(name) ?? name;
-
-            var store = new DocumentStore
-            {
-                Urls = new[] { serverUrl },
-                Database = name
-            };
-
-            options.ModifyDocumentStore?.Invoke(store);
-
-            store.Initialize();
-
-            if (options.CreateDatabase)
-            {
-                var dbs = await store.Maintenance.Server.SendAsync(new GetDatabaseNamesOperation(0, 10));
-                foreach (var db in dbs)
-                {
-                    if (db == name)
-                    {
-                        throw new InvalidOperationException($"Database '{name}' already exists.");
-                    }
-                }
-
-                var doc = new DatabaseRecord(name)
-                {
-                    Settings =
-                    {
-                        [RavenConfiguration.GetKey(x => x.Replication.ReplicationMinimalHeartbeat)] = "1",
-                        [RavenConfiguration.GetKey(x => x.Replication.RetryReplicateAfter)] = "1",
-                        [RavenConfiguration.GetKey(x => x.Core.RunInMemory)] = true.ToString(),
-                        [RavenConfiguration.GetKey(x => x.Core.ThrowIfAnyIndexCannotBeOpened)] = true.ToString(),
-                        [RavenConfiguration.GetKey(x => x.Indexing.MinNumberOfMapAttemptsAfterWhichBatchWillBeCanceledIfRunningLowOnMemory)] = int.MaxValue.ToString()
-                    }
-                };
-
-                options.ModifyDatabaseRecord?.Invoke(doc);
-
-
-                DatabasePutResult result;
-                result = await store.Maintenance.Server.SendAsync(new CreateDatabaseOperation(doc, options.ReplicationFactor));
-            }
-
-            if (serverProcess != null)
-            {
-                store.AfterDispose += (sender, e) =>
-                {
-                    KillSlavedServerProcess(serverProcess);
-                };
-            }
-            return store;
         }
     }
 }
