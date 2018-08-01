96a7e8b294e3e0d425c61b7f9181e8c556db9982
Pawel Pekrol <ppekrol@ravendb.net>
2018-08-01 17:53:38 +0200

    RavenDB-11662 added the ability to switch from Changes API to Polling when Operation status is fetched

--- src/Raven.Client/ServerWide/Operations/ServerWideOperation.cs
+++ src/Raven.Client/ServerWide/Operations/ServerWideOperation.cs
@@ -9,31 +9,10 @@ namespace Raven.Client.ServerWide.Operations
 {
     public class ServerWideOperation : Operation
     {
-        private bool _work;
-
         public ServerWideOperation(RequestExecutor requestExecutor, DocumentConventions conventions, long id)
             : base(requestExecutor, null, conventions, id)
         {
-            _work = true;
-        }
-
-        protected override async Task Process()
-        {
-            _work = true;
-
-            while (_work)
-            {
-                await FetchOperationStatus().ConfigureAwait(false);
-                if (_work == false)
-                    break;
-
-                await TimeoutManager.WaitFor(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
-            }
-        }
-
-        protected override void StopProcessing()
-        {
-            _work = false;
+            StatusFetchMode = OperationStatusFetchMode.Polling;
         }
 
         protected override RavenCommand<OperationState> GetOperationStateCommand(DocumentConventions conventions, long id)
