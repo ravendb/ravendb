ea8a4d472ce09751ea3228729b14dc465ea0904d
Pawel Pekrol <ppekrol@ravendb.net>
2018-08-02 10:12:39 +0200

    RavenDB-11567 files in debug package starts with dot

--- src/Raven.Server/ServerWide/DebugInfoPackageUtils.cs
+++ src/Raven.Server/ServerWide/DebugInfoPackageUtils.cs
@@ -12,7 +12,7 @@ namespace Raven.Server.ServerWide
     public static class DebugInfoPackageUtils
     {
         public static readonly IReadOnlyList<RouteInformation> Routes = RouteScanner.DebugRoutes;
-           
+
 
         public static string GetOutputPathFromRouteInformation(RouteInformation route, string prefix)
         {
@@ -23,15 +23,21 @@ namespace Raven.Server.ServerWide
                 path = path.Replace("debug/", string.Empty);
 
             path = path.Replace("/databases/*/", string.Empty)
-                       .Replace("debug/",string.Empty) //if debug/ left in the middle, remove it as well
+                       .Replace("debug/", string.Empty) //if debug/ left in the middle, remove it as well
                        .Replace("/", ".");
-            return !string.IsNullOrWhiteSpace(prefix) ? 
-                Path.Combine(prefix,$"{path}.json") :
+
+            if (path.StartsWith("."))
+                path = path.Substring(1);
+
+            path = string.IsNullOrWhiteSpace(prefix) == false ?
+                Path.Combine(prefix, $"{path}.json") :
                 $"{path}.json";
+
+            return path;
         }
 
         public static void WriteExceptionAsZipEntry(Exception e, ZipArchive archive, string entryName)
-        {          
+        {
             var entry = archive.CreateEntry($"{entryName}.error");
             entry.ExternalAttributes = ((int)(FilePermissions.S_IRUSR | FilePermissions.S_IWUSR)) << 16;
 
