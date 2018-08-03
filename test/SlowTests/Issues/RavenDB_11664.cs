90b20fc2b0fad2742735ae9febcf8a32a3119490
Pawel Pekrol <ppekrol@ravendb.net>
2018-08-03 13:09:39 +0200

    RavenDB-11664 throwing more meaningful exception when we are trying to delete a document that had a tombstone created in a different collection (collection change case) and it was not yet processed

--- test/SlowTests/Issues/RavenDB_11664.cs
+++ test/SlowTests/Issues/RavenDB_11664.cs
@@ -1,7 +1,10 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using FastTests;
+using Raven.Client;
 using Raven.Client.Documents.Smuggler;
+using Raven.Client.Exceptions;
 using Raven.Tests.Core.Utils.Entities;
 using Xunit;
 
@@ -42,6 +45,37 @@ namespace SlowTests.Issues
                     session.Delete(employee);
 
                     await session.SaveChangesAsync();
+                }
+            }
+        }
+
+        [Fact]
+        public async Task CanDeleteDocumentWhenCollectionWasChanged()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var commands = store.Commands())
+                {
+                    commands.Put("Key/1", null, new { }, new Dictionary<string, object>
+                    {
+                        {Constants.Documents.Metadata.Collection, "Orders"}
+                    });
+
+                    commands.Delete("key/1", null);
+
+                    commands.Put("Key/1", null, new { }, new Dictionary<string, object>
+                    {
+                        {Constants.Documents.Metadata.Collection, "Companies"}
+                    });
+
+                    var e = Assert.Throws<RavenException>(() => commands.Delete("key/1", null));
+                    Assert.Contains("Could not delete document 'key/1' from collection 'Companies' because tombstone", e.Message);
+
+                    var database = await GetDatabase(store.Database);
+
+                    await database.TombstoneCleaner.ExecuteCleanup();
+
+                    commands.Delete("key/1", null);
                 }
             }
         }
