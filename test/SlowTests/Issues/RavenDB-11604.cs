be713d9233f3887fc80206bb472b8244fae2a882
aviv86 <aviv@ayende.com>
2018-08-05 07:24:40 +0200

    RavenDB-11668 : add tests, fix asserts in QueriesWithReservedWords tests

---
+++ test/SlowTests/Issues/RavenDB-11604.cs
@@ -0,0 +1,166 @@
+﻿using System.Linq;
+using FastTests;
+using Raven.Client.Documents.Linq;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_11604 : RavenTestBase
+    {
+        private class User
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Group { get; set; }
+        }
+
+        private class User2
+        {
+            public Group Group { get; set; }
+        }
+
+        private class Group
+        {
+            public string Order { get; set; }
+        }
+
+        [Fact]
+        public void Can_Use_RQL_Reserved_Words_As_Field_To_Fetch()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    for (var i = 0; i < 10; i++)
+                    {
+                        var user = new User
+                        {
+                            FirstName = "Test",
+                            LastName = "User",
+                            Group = i % 4
+                        };
+                        session.Store(user);
+                    }
+
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Query<User>()
+                        .Select(user => new
+                        {
+                            user.Group
+                        });
+
+                    Assert.Equal("from Users as __alias0 " +
+                                 "select __alias0.'Group'"
+                                , query.ToString());
+
+                    var results = query.ToList();
+                    Assert.Equal(10, results.Count);
+                    foreach (var r in results)
+                    {
+                        Assert.InRange(r.Group, 0, 3);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void Can_Use_RQL_Reserved_Words_As_Field_To_Fetch_With_Other_Fields()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    for (var i = 0; i < 10; i++)
+                    {
+                        var user = new User
+                        {
+                            FirstName = "Test",
+                            LastName = "User",
+                            Group = i % 4
+                        };
+                        session.Store(user);
+                    }
+
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Query<User>()
+                        .Select(user => new
+                        {
+                            user.FirstName,
+                            user.Group,
+                            user.LastName
+                        });
+
+                    Assert.Equal("from Users as __alias0 " +
+                                 "select __alias0.FirstName, " +
+                                    "__alias0.'Group', " +
+                                    "__alias0.LastName"
+                        , query.ToString());
+
+                    var results = query.ToList();
+                    Assert.Equal(10, results.Count);
+                    foreach (var r in results)
+                    {
+                        Assert.Equal("Test", r.FirstName);
+                        Assert.Equal("User", r.LastName);
+
+                        Assert.InRange(r.Group, 0, 3);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void Can_Use_RQL_Reserved_Words_As_Field_To_Fetch_With_Nested_Path()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    for (var i = 0; i < 10; i++)
+                    {
+                        var user = new User2
+                        {
+                            Group = new Group
+                            {
+                                Order = "orders/" + i
+                            }
+                        };
+                        session.Store(user);
+                    }
+
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Query<User2>()
+                        .Select(user => new
+                        {
+                            user.Group.Order
+                        });
+
+                    Assert.Equal("from User2s as __alias0 " +
+                                 "select __alias0.'Group'.Order as 'Order'"
+                                , query.ToString());
+
+                    var results = query.ToList();
+                    Assert.Equal(10, results.Count);
+
+                    for (var i = 0; i < results.Count; i++)
+                    {
+                        Assert.Equal(results[i].Order, "orders/" + i);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ test/SlowTests/Issues/RavenDB-11668.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using FastTests;
+using Raven.Client.Documents.Linq;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_11668 : RavenTestBase
+    {
+        [Fact]
+        public void CanProjectPropertyNamedGroup()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var document = new Document
+                    {
+                        Name = "my-name",
+                        Group = "my-group"
+                    };
+                    session.Store(document);
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Query<Document>()
+                        .Select(x => new
+                        {
+                            x.Name,
+                            x.Group
+                        })
+                        .Customize(x => x.WaitForNonStaleResults());
+
+                    Assert.Equal("from Documents as __alias0 " +
+                                 "select __alias0.Name, __alias0.'Group'"
+                        , query.ToString());
+
+                    var triggers = query.ToList();
+
+                    Assert.NotEmpty(triggers);
+                    Assert.Equal("my-name", triggers[0].Name);
+                    Assert.Equal("my-group", triggers[0].Group);
+                }
+            }
+        }
+
+        private class Document
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string Group { get; set; }
+        }
+    }
+}
