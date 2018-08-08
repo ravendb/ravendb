71d02ceb8c6833934370d9c7615695ae1a77a519
Michael Yarichuk <michael.yarichuk@gmail.com>
2018-08-08 15:44:17 +0300

    adding some tests (tests relevant to RavenDB-11707, RavenDB-11706, RavenDB-11704)

--- test/FastTests/Graph/SimpleGraphQueries.cs
+++ test/FastTests/Graph/SimpleGraphQueries.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Newtonsoft.Json.Linq;
 using Raven.Client.Documents;
 using Xunit;
 
@@ -9,15 +9,27 @@ namespace FastTests.Graph
 {
     public class SimpleGraphQueries : RavenTestBase
     {
-        public class Dog
+        private void CreateSimpleData(IDocumentStore store)
         {
-            public string Id;
-            public string Name;
-            public string[] Likes;
-            public string[] Dislikes;
+            using (var session = store.OpenSession())
+            {
+                var entityA = new Entity{ Id = "entity/1", Name = "A" };
+                var entityB = new Entity{ Id = "entity/2", Name = "B" };
+                var entityC = new Entity{ Id = "entity/3", Name = "C" };
+
+                session.Store(entityA);
+                session.Store(entityB);
+                session.Store(entityC);
+
+                entityA.References = entityB.Id;
+                entityB.References = entityC.Id;
+                entityC.References = entityA.Id;
+
+                session.SaveChanges();
+            }
         }
 
-        private void CreateData(IDocumentStore store)
+        private void CreateDataWithMultipleEdgesOfTheSameType(IDocumentStore store)
         {
             using (var session = store.OpenSession())
             {
@@ -41,18 +53,195 @@ namespace FastTests.Graph
             }
         }
 
+        private void CreateMoviesData(IDocumentStore store)
+        {
+            using (var session = store.OpenSession())
+            {
+                var scifi = new Genre
+                {
+                    Id = "genres/1",
+                    Name = "Sci-Fi"
+                };
+
+                var fantasy = new Genre
+                {
+                    Id = "genres/2",
+                    Name = "Fantasy"
+                };
+
+                var adventure = new Genre
+                {
+                    Id = "genres/3",
+                    Name = "Adventure"
+                };
+
+                session.Store(scifi);
+                session.Store(fantasy);
+                session.Store(adventure);
+
+                var starwars = new Movie
+                {
+                    Id = "movies/1",
+                    Name = "Star Wars Ep.1",
+                    Genres = new List<string>
+                    {
+                        "genres/1",
+                        "genres/2"
+                    }
+                };
+
+                var firefly = new Movie
+                {
+                    Id = "movies/2",
+                    Name = "Firefly Serenity",
+                    Genres = new List<string>
+                    {
+                        "genres/2",
+                        "genres/3"
+                    }
+                };
+
+                var indianaJones = new Movie
+                {
+                    Id = "movies/3",
+                    Name = "Indiana Jones and the Temple Of Doom",
+                    Genres = new List<string>
+                    {
+                        "genres/3"
+                    }
+                };
+
+                session.Store(starwars);
+                session.Store(firefly);
+                session.Store(indianaJones);
+
+                session.Store(new User
+                {
+                    Id = "users/1",
+                    Name = "John Dow",
+                    HasRated = new List<User.Rating>
+                    {
+                        new User.Rating
+                        {
+                            Movie = "movies/1",
+                            Score = 5
+                        },
+                        new User.Rating
+                        {
+                            Movie = "movies/2",
+                            Score = 7
+                        }
+
+                    }
+                });
+
+                session.Store(new User
+                {
+                    Id = "users/2",
+                    Name = "Jane Dow",
+                    HasRated = new List<User.Rating>
+                    {
+                        new User.Rating
+                        {
+                            Movie = "movies/2",
+                            Score = 7
+                        },
+                        new User.Rating
+                        {
+                            Movie = "movies/3",
+                            Score = 9
+                        }
+
+                    }
+                });
+
+                session.Store(new User
+                {
+                    Id = "users/3",
+                    Name = "Jack Dow",
+                    HasRated = new List<User.Rating>
+                    {
+                        new User.Rating
+                        {
+                            Movie = "movies/3",
+                            Score = 5
+                        }
+                    }
+                });
+
+
+                session.SaveChanges();
+            }
+        }
+
         [Fact]
+        public void FindReferences()
+        {
+            using (var store = GetDocumentStore())
+            {
+                CreateSimpleData(store);
+                using (var session = store.OpenSession())
+                {
+                    var result = session.Advanced.RawQuery<JObject>(@"match (e:Entities)-[:References]->(e2:Entities)").ToList();
+
+                    Assert.Equal(3, result.Count);
+                    Assert.Contains(result, item => item["e"].Value<string>("Id") == "entity/1" && item["e2"].Value<string>("Id") == "entity/2");
+                    Assert.Contains(result, item => item["e"].Value<string>("Id") == "entity/2" && item["e2"].Value<string>("Id") == "entity/3");
+                    Assert.Contains(result, item => item["e"].Value<string>("Id") == "entity/3" && item["e2"].Value<string>("Id") == "entity/1");
+                }
+            }
+        }
+
+        [Fact(Skip = "Currently is not supposed to work. See RavenDB-11707")]
+        public void FindReferencesWithTypo()
+        {
+            using (var store = GetDocumentStore())
+            {
+                CreateSimpleData(store);
+                using (var session = store.OpenSession())
+                {
+                    Assert.Throws<InvalidOperationException>(() => 
+                        session.Advanced.RawQuery<dynamic>(@"match (e:Entity)-[:References]->(e2:Entity)")
+                                        .ToList());
+                }
+            }
+        }
+
+        [Fact(Skip = "Currently is not supposed to work. See RavenDB-11706")]
+        public void FindUsersWhoRatedTheSameMovie()
+        {
+            using (var store = GetDocumentStore())
+            {
+                CreateMoviesData(store);
+                using (var session = store.OpenSession())
+                {
+                    var result = session.Advanced.RawQuery<JObject>(@"
+                        match (u1:Users)-[:HasRated(Score > 1).Movie]->(Movies)<-[:HasRated.Movie]-(u2:Users)
+                        select u1,u2
+                    ").ToList();
+
+                    Assert.NotEmpty(result);
+                    Assert.Contains(result, item => item["u1"].Value<string>("Id")== "users/1" && item["u2"].Value<string>("Id") == "users/2");
+                    Assert.Contains(result, item => item["u1"].Value<string>("Id") == "users/2" && item["u2"].Value<string>("Id") == "users/3");
+                }
+            }
+        }
+
+        [Fact(Skip = "Currently is not supposed to work. See RavenDB-11704")]
         public void FindFriendlies()
         {
             using (var store = GetDocumentStore())
             {
-                CreateData(store);
+                CreateDataWithMultipleEdgesOfTheSameType(store);
                 using (var session = store.OpenSession())
-                {
-                   var friends = session.Advanced.RawQuery<dynamic>(@"
-match (fst:Dogs)-[:Likes]->(snd:Dogs)
-")
-                        .ToList();
+                {                   
+                    var friends = session.Advanced.RawQuery<JObject>(@"match (fst:Dogs)-[:Likes]->(snd:Dogs)")
+                                                  .ToList();
+
+                    Assert.Contains(friends, result => result["fst"].Value<string>("Id") == "dogs/1-A" && result["snd"].Value<string>("Id") == "dogs/2-A");
+                    Assert.Contains(friends, result => result["fst"].Value<string>("Id") == "dogs/2-A" && result["snd"].Value<string>("Id") == "dogs/2-A");
+                    Assert.Contains(friends, result => result["fst"].Value<string>("Id") == "dogs/2-A" && result["snd"].Value<string>("Id") == "dogs/3-A");
+                    Assert.Contains(friends, result => result["fst"].Value<string>("Id") == "dogs/3-A" && result["snd"].Value<string>("Id") == "dogs/2-A");
                 }
             }
         }
--- test/FastTests/Graph/TestEntities.cs
+++ test/FastTests/Graph/TestEntities.cs
@@ -1,18 +1,49 @@
-﻿namespace FastTests.Graph
+﻿using System.Collections.Generic;
+
+namespace FastTests.Graph
 {
+    public class Entity
+    {
+        public string Id;
+
+        public string Name;
+
+        public string References;
+    }
+
+    public class Dog
+    {
+        public string Id;
+        public string Name;
+        public string[] Likes;
+        public string[] Dislikes;
+    }
+
     public class Movie
     {
+        public string Id { get; set; }
         public string Name { get; set; }
+        public List<string> Genres { get; set; }
     }
 
     public class Genre
     {
+        public string Id { get; set; }
         public string Name { get; set; }
     }
 
     public class User
     {
+        public string Id { get; set; }
         public string Name { get;set; }
         public int Age { get; set; }
+
+        public List<Rating> HasRated { get; set; }
+
+        public class Rating
+        {
+            public string Movie { get; set; }
+            public int Score { get; set; }
+        }
     }
 }
