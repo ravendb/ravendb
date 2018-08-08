42f0fcbf53b1258f32c1351a5a54735a25c593c2
Grisha <grisha@ayende.com>
2018-08-08 17:31:52 +0300

    RavenDB-11605 - don't Gzip the smuggler output when it is in process communication

---
+++ tools/Raven.Migrator/DisposableAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Raven.Migrator
+{
+    public class DisposableAction : IDisposable
+    {
+        private readonly Action _action;
+
+        public DisposableAction(Action action)
+        {
+            _action = action;
+        }
+
+        public void Dispose()
+        {
+            _action();
+        }
+    }
+}
