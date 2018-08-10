d43cbb5b206ad2b192935bc505fb54f7b7362fce
Arkadiusz Palinski <arek@ravendb.net>
2018-08-10 13:46:29 +0200

    RavenDB-11728 Invalid GetHashCode() calculation for nested blittable resulting in invalid map-reduce results

--- test/FastTests/Blittable/BlittableJsonEqualityTests.cs
+++ test/FastTests/Blittable/BlittableJsonEqualityTests.cs
@@ -1,4 +1,5 @@
-﻿using Sparrow.Json;
+﻿using System.Collections.Generic;
+using Sparrow.Json;
 using Sparrow.Json.Parsing;
 using Xunit;
 
@@ -63,6 +64,48 @@ namespace FastTests.Blittable
                 using (var blittable2 = ctx.ReadObject(json2, "foo"))
                 {
                     Assert.Equal(blittable1, blittable2);
+                }
+            }
+        }
+
+        [Fact]
+        public void GetHashCode_must_not_use_size_if_it_isnt_root()
+        {
+            using (var ctx = JsonOperationContext.ShortTermSingleUse())
+            {
+                var json1 = new DynamicJsonValue()
+                {
+                    ["Address"] = new DynamicJsonValue()
+                    {
+                        ["City"] = "Atlanta",
+                        ["ZipCode"] = 1234
+                    },
+                    ["Friends"] = "yes"
+                };
+
+                var json2 = new DynamicJsonValue()
+                {
+
+                    ["Address"] = new DynamicJsonValue()
+                    {
+                        ["ZipCode"] = 1234,
+                        ["City"] = "Atlanta"
+                    },
+                    ["Friends"] = "no"
+                };
+
+                using (var blittable1 = ctx.ReadObject(json1, "foo"))
+                using (var blittable2 = ctx.ReadObject(json2, "foo"))
+                {
+                    blittable1.TryGet("Address", out BlittableJsonReaderObject ob1);
+                    blittable2.TryGet("Address", out BlittableJsonReaderObject ob2);
+
+                    HashSet<BlittableJsonReaderObject> items = new HashSet<BlittableJsonReaderObject>();
+
+                    Assert.True(items.Add(ob1));
+                    Assert.False(items.Add(ob2));
+
+                    Assert.Equal(ob1.GetHashCode(), ob2.GetHashCode());
                 }
             }
         }
---
+++ test/SlowTests/Issues/RavenDB_11728.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FastTests;
+using Raven.Client.Documents.Indexes;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_11728 : RavenTestBase
+    {
+        [Fact]
+        public void MustReturnOneResult()
+        {
+            using (var store = GetDocumentStore())
+            {
+                new EmailSequenceWithStatusIndex().Execute(store);
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new EmailMessage()
+                    {
+                        Id = "EmailMessages/1",
+                        Status = EmailProcessingStatus.Queued
+                    });
+
+                    session.Store(new EmailMessage()
+                    {
+                        Id = "EmailMessages/2",
+                        Status = EmailProcessingStatus.Sent
+                    });
+
+                    session.Store(new EmailSequence()
+                    {
+                        EmailsIds = new List<string>()
+                        {
+                            "EmailMessages/1",
+                            "EmailMessages/2"
+                        },
+                        Parameters = new EmailSequenceSendoutParameters()
+                        {
+                            RecipientAddress = "a",
+                            RecipientDisplay = "a",
+                            Data = new Dictionary<string, string>()
+                        }
+                    });
+
+                    session.SaveChanges();
+
+                    var results = session.Query<EmailSequenceWithStatusIndex.Result, EmailSequenceWithStatusIndex>().Customize(x => x.WaitForNonStaleResults()).ToList();
+
+                    Assert.Equal(1, results.Count);
+                }
+            }
+        }
+
+        private class EmailSequenceWithStatusIndex : AbstractIndexCreationTask<EmailSequence, EmailSequenceWithStatusIndex.Result>
+        {
+            public class Result
+            {
+                public class EmailIdWithStatus
+                {
+                    public string Id { get; set; }
+
+                    public EmailProcessingStatus? Status { get; set; }
+                }
+                public EmailSequenceSendoutParameters Parameters { get; set; }
+
+                public EmailIdWithStatus[] Emails { get; set; }
+            }
+
+            public EmailSequenceWithStatusIndex()
+            {
+                Map = sequences =>
+                    from seq in sequences
+                    from emailId in seq.EmailsIds
+                    let email = LoadDocument<EmailMessage>(emailId)
+                    select new Result
+                    {
+                        Parameters = seq.Parameters,
+                        Emails = new[]
+                        {
+                            new Result.EmailIdWithStatus
+                            {
+                                Id = emailId,
+                                Status = email.Status
+                            }
+                        }
+                    };
+
+                Reduce = results =>
+                    from result in results
+                    group result by new
+                    {
+                        result.Parameters
+                    }
+                    into g
+                    let key = g.Key
+                    select new Result
+                    {
+                        Parameters = key.Parameters,
+                        Emails = g.SelectMany(x => x.Emails).ToArray()
+                    };
+            }
+        }
+
+        private class EmailSequence
+        {
+            public string Id { get; set; }
+
+            public List<string> EmailsIds { get; set; }
+
+            public EmailSequenceSendoutParameters Parameters { get; set; }
+        }
+
+        private class EmailMessage
+        {
+            public string Id { get; set; }
+
+            public EmailProcessingStatus? Status { get; set; } = EmailProcessingStatus.Draft;
+        }
+
+        private class EmailSequenceSendoutParameters
+        {
+            public string RecipientAddress { get; set; }
+
+            public string RecipientDisplay { get; set; }
+
+            public Dictionary<string, string> Data { get; set; } = new Dictionary<string, string>();
+        }
+
+        private enum EmailProcessingStatus
+        {
+            Draft = 1,
+            Queued = 2,
+            Sent = 3,
+            Cancelled = 4,
+            Failed = 5
+        }
+    }
+}
