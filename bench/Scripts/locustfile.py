8c8ca9c99e063693965a5e3f1210f38a3aa1c373
Fitzchak Yitzchaki <fitzchak@ayende.com>
2018-08-20 14:20:48 +0300

    RavenDB-11775 Add reads load test

--- bench/Scripts/locustfile.py
+++ bench/Scripts/locustfile.py
@@ -1,14 +1,25 @@
 from locust import HttpLocust, TaskSet, task
 import json
+import random
 
 class UserBehavior(TaskSet):
+    def __init__(self, *args, **kwargs):
+        super(UserBehavior, self).__init__(*args, **kwargs)
+        self.cert_path = "C:/work/locust.pem"
+
     @task
-    def write(l):
+    def write(self):
         headers = {'content-type': 'application/json'}
         document = {"Name": "test1", "Supplier": 1, "Category": 1, "QuantityPerUnit": 1, "PricePerUnit": 1,
                     "UnitsInStock": 1, "UnitsOnOrder": 1, "Discontinued": False, "ReorderLevel": 1,
                     "@metadata": {"@collection": "Benchmarks"}}
-        l.client.put("/databases/BenchmarkDB/docs?id=temp/", data=json.dumps(document), headers=headers, cert="C:/work/locust.pem")
+        self.client.put("/databases/BenchmarkDB/docs?id=temp/", data=json.dumps(document), headers=headers, cert=self.cert_path)
+
+    @task
+    def read(self):
+        num = random.randint(0, 8500000) + 1000000
+        self.client.get("/databases/BenchmarkDB/docs?id=temp/000000000000{0}-A/".format(num), cert=self.cert_path)
+
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
