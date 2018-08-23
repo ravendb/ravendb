e19cc7d4687f89bfd929c99437d25cfda2192786
Pawel Pekrol <ppekrol@ravendb.net>
2018-08-23 21:51:28 +0200

    RavenDB-7070 updated to .NET Core 2.1.3

--- src/Raven.Embedded/ServerOptions.cs
+++ src/Raven.Embedded/ServerOptions.cs
@@ -7,7 +7,7 @@ namespace Raven.Embedded
 {
     public class ServerOptions
     {
-        public string FrameworkVersion { get; set; } = "2.1.2";
+        public string FrameworkVersion { get; set; } = "2.1.3";
 
         public string DataDirectory { get; set; } = Path.Combine(AppContext.BaseDirectory, "RavenDB");
 
