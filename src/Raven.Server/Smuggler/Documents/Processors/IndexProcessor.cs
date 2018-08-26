e69713637b1812d75a85fd6f367ee412cc22d3bd
Grisha <grisha@ayende.com>
2018-08-26 19:34:13 +0300

    RavenDB-11816 - Replace legacy properties when migrating indexes from a v3.x dump

--- src/Raven.Server/Smuggler/Documents/Processors/IndexProcessor.cs
+++ src/Raven.Server/Smuggler/Documents/Processors/IndexProcessor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Raven.Client.Documents.Indexes;
 using Raven.Client.Util;
 using Raven.Server.Documents;
@@ -142,6 +144,8 @@ namespace Raven.Server.Smuggler.Documents.Processors
 
             if (indexDefinition.Maps != null)
             {
+                indexDefinition.Maps = indexDefinition.Maps.Select(ReplaceLegacyProperties).ToHashSet();
+
                 foreach (var map in indexDefinition.Maps)
                 {
                     if (IsFunctionValid(map, out var message) == false)
@@ -151,6 +155,8 @@ namespace Raven.Server.Smuggler.Documents.Processors
 
             if (indexDefinition.Reduce != null)
             {
+                indexDefinition.Reduce = ReplaceLegacyProperties(indexDefinition.Reduce);
+
                 if (IsFunctionValid(indexDefinition.Reduce, out var message) == false)
                     throw new InvalidOperationException($"Reduce function of legacy index '{name}' is invalid. {message}");
             }
@@ -223,6 +229,16 @@ namespace Raven.Server.Smuggler.Documents.Processors
             }
 
             return indexDefinition;
+        }
+
+        private static string ReplaceLegacyProperties(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return str;
+
+            str = str.Replace("new Raven.Abstractions.Linq.DynamicList", string.Empty);
+            var regex = new Regex(@"([\w_.]+)\.__document_id");
+            return regex.Replace(str, "Id($1)");
         }
 
         private static bool IsFunctionValid(string function, out string message)
