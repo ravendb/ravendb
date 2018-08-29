3162c08b1d79f8e136257acbec564e0226063d67
Idan Haim Shalom <haimdude@gmail.com>
2018-08-29 17:33:40 +0300

    RavenDB-11799 Remove double checks

--- src/Raven.Server/ServerWide/ServerStore.cs
+++ src/Raven.Server/ServerWide/ServerStore.cs
@@ -536,7 +536,7 @@ namespace Raven.Server.ServerWide
 
             _engine = new RachisConsensus<ClusterStateMachine>(this);
             _engine.BeforeAppendToRaftLog += BeforeAppendToRaftLog;
-            var myUrl = Configuration.Core.PublicServerUrl.HasValue ? Configuration.Core.PublicServerUrl.Value.UriValue : GetNodeHttpServerUrl();
+            var myUrl = GetNodeHttpServerUrl();
             _engine.Initialize(_env, Configuration, myUrl);
 
             LicenseManager.Initialize(_env, ContextPool);
