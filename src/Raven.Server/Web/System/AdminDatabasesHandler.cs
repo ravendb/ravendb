06adf3c8b25e0bbd6a96bb1b6ae79f941ad2af0c
aviv86 <aviv@ayende.com>
2018-09-04 09:03:20 +0200

    RavenDB-11836 : forbid to create database with server data-dir as db path or with root dir as db path

--- src/Raven.Server/Web/System/AdminDatabasesHandler.cs
+++ src/Raven.Server/Web/System/AdminDatabasesHandler.cs
@@ -341,7 +341,7 @@ namespace Raven.Server.Web.System
 
             try
             {
-                DatabaseHelper.Validate(name, databaseRecord);
+                DatabaseHelper.Validate(name, databaseRecord, Server.Configuration);
             }
             catch (Exception e)
             {
