a51638dee7b5a86e757b0b5e934fb6dbd615cab3
aviv86 <aviv@ayende.com>
2018-09-04 09:03:41 +0200

    RavenDB-11836 : tests

---
+++ test/SlowTests/Issues/RavenDB-11836.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.IO;
+using FastTests;
+using Raven.Client.Exceptions;
+using Raven.Client.ServerWide;
+using Raven.Client.ServerWide.Operations;
+using Raven.Server.Config;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_11836 : RavenTestBase
+    {
+        [Fact]
+        public void ShouldThrowIfTryingToCreateDatabaseWithSamePathAsServerDataDir()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var dbRecord = new DatabaseRecord("test")
+                {
+                    Settings = new Dictionary<string, string>
+                    {
+                        [RavenConfiguration.GetKey(x => x.Core.DataDirectory)] = Server.Configuration.Core.DataDirectory.ToString()
+                    }
+                };
+
+                var e = Assert.Throws<BadRequestException>(() =>
+                {
+                    store.Maintenance.Server.Send(new CreateDatabaseOperation(dbRecord));
+                });
+
+                Assert.Contains($@"Forbidden data directory path for database ""test"" : ""{Server.Configuration.Core.DataDirectory}"". " +
+                                "This is the root path that RavenDB server uses to store data.", e.Message);
+
+            }
+        }
+
+        [Fact]
+        public void ShouldThrowIfTryingToCreateDatabaseWithRootDirectoryAsPath()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var root = Path.GetPathRoot(Server.Configuration.Core.DataDirectory.FullPath);
+
+                var dbRecord = new DatabaseRecord("test")
+                {
+                    Settings = new Dictionary<string, string>
+                    {
+                        [RavenConfiguration.GetKey(x => x.Core.DataDirectory)] = root
+                    }
+                };
+
+                var e = Assert.Throws<BadRequestException>(() =>
+                {
+                    store.Maintenance.Server.Send(new CreateDatabaseOperation(dbRecord));
+                });
+
+                Assert.Contains($@"Forbidden data directory path for database ""test"" : ""{root}"". " +
+                                "You cannot use the root directory of the drive as the database path.", e.Message);
+
+            }
+        }
+    }
+}
