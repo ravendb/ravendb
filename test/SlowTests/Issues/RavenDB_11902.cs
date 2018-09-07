e22199c4eb38f6f49cf4d16f516efe669aac66b1
Pawel Pekrol <ppekrol@ravendb.net>
2018-09-07 14:53:18 +0200

    RavenDB-11902 do not escape Id in bulk insert if it was already escaped

--- test/SlowTests/Issues/RavenDB_11902.cs
+++ test/SlowTests/Issues/RavenDB_11902.cs
@@ -1,4 +1,5 @@
 ﻿using FastTests;
+using Raven.Client.Documents.Operations;
 using SlowTests.Core.Utils.Entities;
 using Xunit;
 
@@ -33,6 +34,17 @@ namespace SlowTests.Issues
                     session.SaveChanges();
                 }
 
+                // this works
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User
+                    {
+                        Id = "GeoData/D\\\"horn/113736"
+                    });
+
+                    session.SaveChanges();
+                }
+
                 // this fails
                 using (var bulk = store.BulkInsert())
                 {
@@ -50,6 +62,18 @@ namespace SlowTests.Issues
                         Id = "GeoData/D\"horn/113736"
                     });
                 }
+
+                // this fails
+                using (var bulk = store.BulkInsert())
+                {
+                    bulk.Store(new User
+                    {
+                        Id = "GeoData/D\\\"horn/113736"
+                    });
+                }
+
+                var stats = store.Maintenance.Send(new GetStatisticsOperation());
+                Assert.Equal(2 + 1, stats.CountOfDocuments); // + hilo
             }
         }
     }
