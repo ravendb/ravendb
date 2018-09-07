893ba86ebc83efb2431e75d44affdf834c59ffb4
Pawel Pekrol <ppekrol@ravendb.net>
2018-09-07 11:39:56 +0200

    RavenDB-11902 - Bulk insert should use only one type of quotes " - escaping " when used in Id

---
+++ test/SlowTests/Issues/RavenDB_11902.cs
@@ -0,0 +1,56 @@
+﻿using FastTests;
+using SlowTests.Core.Utils.Entities;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_11902 : RavenTestBase
+    {
+        [Fact]
+        public void Can_insert_doc_with_single_quotation_char_in_id()
+        {
+            using (var store = GetDocumentStore())
+            {
+                // this works
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User
+                    {
+                        Id = "GeoData/D'horn/113736"
+                    });
+
+                    session.SaveChanges();
+                }
+
+                // this works
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User
+                    {
+                        Id = "GeoData/D\"horn/113736"
+                    });
+
+                    session.SaveChanges();
+                }
+
+                // this fails
+                using (var bulk = store.BulkInsert())
+                {
+                    bulk.Store(new User
+                    {
+                        Id = "GeoData/D'horn/113736"
+                    });
+                }
+
+                // this fails
+                using (var bulk = store.BulkInsert())
+                {
+                    bulk.Store(new User
+                    {
+                        Id = "GeoData/D\"horn/113736"
+                    });
+                }
+            }
+        }
+    }
+}
