d97a074961455afce082b1aed4be09dd1c9b836b
ml054 <marcin@ravendb.net>
2018-09-20 12:00:10 +0200

    RavenDB-11978 Can't edit backup task with Azure

--- src/Raven.Studio/typescript/viewmodels/database/tasks/editPeriodicBackupTask.ts
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/editPeriodicBackupTask.ts
@@ -36,7 +36,7 @@ class editPeriodicBackupTask extends viewModelBase {
                 new getPeriodicBackupConfigurationCommand(this.activeDatabase(), args.taskId)
                     .execute()
                     .done((configuration: Raven.Client.Documents.Operations.Backups.PeriodicBackupConfiguration) => {
-                        if (configuration.LocalSettings.FolderPath && configuration.LocalSettings.FolderPath.startsWith(this.serverConfiguration().LocalRootPath)) {
+                        if (this.serverConfiguration().LocalRootPath && configuration.LocalSettings.FolderPath && configuration.LocalSettings.FolderPath.startsWith(this.serverConfiguration().LocalRootPath)) {
                             configuration.LocalSettings.FolderPath = configuration.LocalSettings.FolderPath.substr(this.serverConfiguration().LocalRootPath.length);
                         }
                         
@@ -197,7 +197,10 @@ class editPeriodicBackupTask extends viewModelBase {
         }
 
         const dto = this.configuration().toDto();
-        dto.LocalSettings.FolderPath = (this.serverConfiguration().LocalRootPath || "") + dto.LocalSettings.FolderPath;
+        
+        if (this.serverConfiguration().LocalRootPath) {
+            dto.LocalSettings.FolderPath = this.serverConfiguration().LocalRootPath + dto.LocalSettings.FolderPath;
+        }
 
         new savePeriodicBackupConfigurationCommand(this.activeDatabase(), dto)
             .execute()
