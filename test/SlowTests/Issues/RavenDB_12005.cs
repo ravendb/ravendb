403d29f3afff19d4b7b51a2bfc8d024831ed7816
Pawel Pekrol <ppekrol@ravendb.net>
2018-10-01 15:10:01 +0200

    RavenDB-12005 fixed LastIndexOf and LastIndexOfAny implementation in LazyStringValue

---
+++ test/SlowTests/Issues/RavenDB_12005.cs
@@ -0,0 +1,37 @@
+﻿using FastTests;
+using Sparrow.Json;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_12005 : RavenTestBase
+    {
+        [Fact]
+        public unsafe void LastIndexOfInLazyStringValueShouldWork()
+        {
+            using (var context = JsonOperationContext.ShortTermSingleUse())
+            {
+                var lsv = context.GetLazyString("ord/1-A");
+                var idx = lsv.LastIndexOf('/');
+                Assert.Equal(3, idx);
+                idx = lsv.LastIndexOfAny(new[] { '/' });
+                Assert.Equal(3, idx);
+                idx = lsv.IndexOf('/');
+                Assert.Equal(3, idx);
+                idx = lsv.IndexOfAny(new[] { '/' });
+                Assert.Equal(3, idx);
+
+                lsv = context.AllocateStringValue(null, lsv.Buffer, lsv.Size);
+
+                idx = lsv.LastIndexOf('/');
+                Assert.Equal(3, idx);
+                idx = lsv.LastIndexOfAny(new[] { '/' });
+                Assert.Equal(3, idx);
+                idx = lsv.IndexOf('/');
+                Assert.Equal(3, idx);
+                idx = lsv.IndexOfAny(new[] { '/' });
+                Assert.Equal(3, idx);
+            }
+        }
+    }
+}
