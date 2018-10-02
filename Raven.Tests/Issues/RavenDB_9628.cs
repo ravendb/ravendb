ccfc4225d9fbc61345525a7f0de991b4e62df977
Arkadiusz Palinski <arek@ravendb.net>
2018-10-02 15:25:39 +0200

    RavenDB-9628 Added more tests. Using well known const value.

--- Raven.Tests/Issues/RavenDB_9628.cs
+++ Raven.Tests/Issues/RavenDB_9628.cs
@@ -5,11 +5,11 @@
 // -----------------------------------------------------------------------
 using System;
 using System.Linq;
+using Raven.Abstractions;
 using Raven.Abstractions.Data;
 using Raven.Client.Document;
 using Raven.Json.Linq;
 using Raven.Tests.Bundles.Replication;
-using Raven.Tests.Helpers;
 using Xunit;
 
 namespace Raven.Tests.Issues
@@ -39,6 +39,72 @@ namespace Raven.Tests.Issues
             {
                 Assert.Equal(0, accessor.Lists.Read(Constants.RavenReplicationDocsTombstones, Etag.Empty, null, 10).Count());
             });
+        }
+
+        [Fact]
+        public void CanPurgeTombstonesOlderThan()
+        {
+            var store1 = (DocumentStore)CreateStore();
+            store1.DatabaseCommands.Put("ayende", null, new RavenJObject(), new RavenJObject());
+            store1.DatabaseCommands.Delete("ayende", null);
+
+            var now = SystemTime.UtcNow;
+
+            SystemTime.UtcDateTime = () => now.AddDays(1);
+
+            store1.DatabaseCommands.Put("rahien", null, new RavenJObject(), new RavenJObject());
+            store1.DatabaseCommands.Delete("rahien", null);
+            servers[0].Database.TransactionalStorage.Batch(accessor =>
+            {
+                accessor.Lists.RemoveAllOlderThan(Constants.RavenReplicationDocsTombstones, now);
+            });
+
+            servers[0].Database.TransactionalStorage.Batch(accessor =>
+            {
+                Assert.Equal(1, accessor.Lists.Read(Constants.RavenReplicationDocsTombstones, Etag.Empty, null, 10).Count());
+
+                var item = accessor.Lists.Read(Constants.RavenReplicationDocsTombstones, Etag.Empty, null, 10).First();
+
+                Assert.Equal("rahien", item.Key);
+            });
+        }
+
+        [Fact]
+        public void CanPurgeTombstonesOlderThanUsingMethodCalledByTask()
+        {
+            var store1 = (DocumentStore)CreateStore();
+            store1.DatabaseCommands.Put("ayende", null, new RavenJObject(), new RavenJObject());
+            store1.DatabaseCommands.Delete("ayende", null);
+
+            var now = SystemTime.UtcNow;
+
+            SystemTime.UtcDateTime = () => now.Add(servers[0].Database.Configuration.TombstoneRetentionTime);
+
+            store1.DatabaseCommands.Put("rahien", null, new RavenJObject(), new RavenJObject());
+            store1.DatabaseCommands.Delete("rahien", null);
+            servers[0].Database.TransactionalStorage.Batch(accessor =>
+            {
+                accessor.Lists.RemoveAllOlderThan(Constants.RavenReplicationDocsTombstones, now);
+            });
+
+            servers[0].Database.PurgeOutdatedTombstones();
+
+            servers[0].Database.TransactionalStorage.Batch(accessor =>
+            {
+                Assert.Equal(1, accessor.Lists.Read(Constants.RavenReplicationDocsTombstones, Etag.Empty, null, 10).Count());
+
+                var item = accessor.Lists.Read(Constants.RavenReplicationDocsTombstones, Etag.Empty, null, 10).First();
+
+                Assert.Equal("rahien", item.Key);
+            });
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+
+            SystemTime.UtcDateTime = null;
+
         }
     }
 }
