3e97d5cb9dc44107e1de4d23e0895330b32191d3
Pawel Pekrol <ppekrol@ravendb.net>
2018-10-02 12:52:55 +0200

    RavenDB-11827 session.Advanced.Increment should check for 'null'

---
+++ test/SlowTests/Issues/RavenDB_11827.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using FastTests;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_11827 : RavenTestBase
+    {
+        private class Data
+        {
+            public string Value { get; set; }
+        }
+
+        [Fact]
+        public async Task CanIncrementOnNull()
+        {
+            var id = "data";
+            using (var ds = GetDocumentStore())
+            {
+                using (var session = ds.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new Data { Value = null }, id);
+                    await session.SaveChangesAsync();
+                }
+
+                using (var session = ds.OpenAsyncSession())
+                {
+                    session.Advanced.Increment<Data, string>(id, d => d.Value, "something");
+                    await session.SaveChangesAsync();
+                }
+
+                using (var session = ds.OpenAsyncSession())
+                {
+                    var data = await session.LoadAsync<Data>(id);
+                    Assert.Equal("something", data.Value);
+                }
+            }
+        }
+    }
+}
