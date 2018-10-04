95f379c77f9c45788666950ff70f4a44c99d9e9f
Ayende Rahien <Ayende@ayende.com>
2018-10-04 15:11:11 +0300

    Add passing test

---
+++ test/SlowTests/MailingList/Oru.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FastTests;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Operations.Attachments;
+using Xunit;
+
+namespace SlowTests.MailingList
+{
+    public class Oru : RavenTestBase
+    {
+        [Fact]
+        public void CanSendOperationsWithSettingExplicitDbName()
+        {
+            using (var store = GetDocumentStore())
+            using (var other = new DocumentStore
+            {
+                Urls = store.Urls,
+            }.Initialize())
+            {
+                other.Operations.ForDatabase(store.Database).Send(
+                    new GetAttachmentOperation("test", "test", Raven.Client.Documents.Attachments.AttachmentType.Document, null)
+                    );
+            }
+        }
+    }
+}
