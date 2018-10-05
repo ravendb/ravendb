95e181cae6f862452c25cca728a84f371e79e01f
Arkadiusz Palinski <arek@ravendb.net>
2018-10-05 15:36:10 +0200

    RavenDB-12034 Storing new object() should not set collection metadata. It needs to go to @empty collection.

---
+++ test/SlowTests/Issues/RavenDB_12034.cs
@@ -0,0 +1,27 @@
+﻿using FastTests;
+using Raven.Client;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_12034 : RavenTestBase
+    {
+        [Fact]
+        public void Stored_object_should_not_get_collection_metadata()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    object marker = new object();
+
+                    session.Store(marker, "markers/1");
+
+                    var metadata = session.Advanced.GetMetadataFor(marker);
+
+                    Assert.False(metadata.ContainsKey(Constants.Documents.Metadata.Collection));
+                }
+            }
+        }
+    }
+}
