8e6b4587e9e625cdb365b0ebef84bf957e00f3a2
Pawel Pekrol <ppekrol@ravendb.net>
2018-10-05 12:25:26 +0200

    RavenDB-11815 support for nested group by values in multi-map indexes

--- test/SlowTests/Issues/RavenDB_11815.cs
+++ test/SlowTests/Issues/RavenDB_11815.cs
@@ -1,21 +1,15 @@
 ﻿using System.Linq;
 using FastTests;
 using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Session;
 using Xunit;
 
 namespace SlowTests.Issues
 {
     public class RavenDB_11815 : RavenTestBase
     {
-        private class MapReduceIndexWithNestedField : AbstractIndexCreationTask<Order, MapReduceIndexWithNestedField.Result>
+        private class MapReduceIndexWithNestedField : AbstractIndexCreationTask<Order, Result>
         {
-            public class Result
-            {
-                public Total Total { get; set; }
-
-                public int Count { get; set; }
-            }
-
             public MapReduceIndexWithNestedField()
             {
                 Map = orders => from order in orders
@@ -23,10 +17,10 @@ namespace SlowTests.Issues
                                 {
                                     Total = new
                                     {
-                                        Amount = order.Amount,
-                                        Currency = order.Currency
+                                        order.Amount,
+                                        order.Currency
                                     },
-                                    Count = 1,
+                                    Count = 1
                                 };
 
                 Reduce = results => from result in results
@@ -39,22 +33,58 @@ namespace SlowTests.Issues
                                         Total = new
                                         {
                                             Amount = g.Sum(x => x.Total.Amount),
-                                            Currency = g.Key.Currency
+                                            g.Key.Currency
                                         },
                                         Count = g.Sum(x => x.Count)
                                     };
             }
         }
 
-        private class MapReduceIndexWithNestedField2 : AbstractIndexCreationTask<Order, MapReduceIndexWithNestedField.Result>
+        private class MultiMapReduceIndexWithNestedField : AbstractMultiMapIndexCreationTask<Result>
         {
-            public class Result
+            public MultiMapReduceIndexWithNestedField()
             {
-                public Total Total { get; set; }
+                AddMap<Order>(orders => from order in orders
+                                        select new
+                                        {
+                                            Total = new
+                                            {
+                                                order.Amount,
+                                                order.Currency
+                                            },
+                                            Count = 1
+                                        });
 
-                public int Count { get; set; }
+                AddMap<Order2>(orders => from order in orders
+                                         select new
+                                         {
+                                             Total = new
+                                             {
+                                                 order.Currency,
+                                                 order.Amount
+                                             },
+                                             Count = 1
+                                         });
+
+                Reduce = results => from result in results
+                                    group result by new
+                                    {
+                                        result.Total.Currency
+                                    } into g
+                                    select new
+                                    {
+                                        Total = new
+                                        {
+                                            Amount = g.Sum(x => x.Total.Amount),
+                                            g.Key.Currency
+                                        },
+                                        Count = g.Sum(x => x.Count)
+                                    };
             }
+        }
 
+        private class MapReduceIndexWithNestedField2 : AbstractIndexCreationTask<Order, Result>
+        {
             public MapReduceIndexWithNestedField2()
             {
                 Map = orders => from order in orders
@@ -62,10 +92,10 @@ namespace SlowTests.Issues
                                 {
                                     Total = new
                                     {
-                                        Amount = order.Amount,
-                                        Currency = order.Currency
+                                        order.Amount,
+                                        order.Currency
                                     },
-                                    Count = 1,
+                                    Count = 1
                                 };
 
                 Reduce = results => from result in results
@@ -138,13 +168,22 @@ groupBy(x => x.Total.Currency)
             }
         }
 
+        private class Result
+        {
+            public Total Total { get; set; }
+
+            public int Count { get; set; }
+        }
+
         private class Order
         {
-            public string Id { get; set; }
-
             public decimal Amount { get; set; }
 
             public string Currency { get; set; }
+        }
+
+        private class Order2 : Order
+        {
         }
 
         private class Total
@@ -159,10 +198,11 @@ groupBy(x => x.Total.Currency)
         {
             using (var store = GetDocumentStore())
             {
-                new MapReduceIndexWithNestedField().Execute(store);
-                new MapReduceIndexWithNestedField2().Execute(store);
-                new MapReduceIndexWithNestedFieldJs().Execute(store);
-                new MapReduceIndexWithNestedFieldJs2().Execute(store);
+                //new MapReduceIndexWithNestedField().Execute(store);
+                //new MapReduceIndexWithNestedField2().Execute(store);
+                //new MapReduceIndexWithNestedFieldJs().Execute(store);
+                //new MapReduceIndexWithNestedFieldJs2().Execute(store);
+                new MultiMapReduceIndexWithNestedField().Execute(store);
 
                 using (var session = store.OpenSession())
                 {
@@ -190,6 +230,32 @@ groupBy(x => x.Total.Currency)
                         Currency = "PLN"
                     });
 
+                    //
+
+                    session.Store(new Order2
+                    {
+                        Amount = 11,
+                        Currency = "USD"
+                    });
+
+                    session.Store(new Order2
+                    {
+                        Amount = 2,
+                        Currency = "USD"
+                    });
+
+                    session.Store(new Order2
+                    {
+                        Amount = 50,
+                        Currency = "PLN"
+                    });
+
+                    session.Store(new Order2
+                    {
+                        Amount = 3,
+                        Currency = "PLN"
+                    });
+
                     session.SaveChanges();
                 }
 
@@ -197,34 +263,26 @@ groupBy(x => x.Total.Currency)
 
                 using (var session = store.OpenSession())
                 {
-                    var results1 = session.Query<MapReduceIndexWithNestedField.Result, MapReduceIndexWithNestedField>()
-                        .ToList();
+                    //AssertResults<MapReduceIndexWithNestedField>(session);
 
-                    Assert.Equal(2, results1.Count);
-                    Assert.True(results1.Any(x => x.Total.Currency == "PLN"));
-                    Assert.True(results1.Any(x => x.Total.Currency == "USD"));
+                    //AssertResults<MapReduceIndexWithNestedField2>(session);
 
-                    var results2 = session.Query<MapReduceIndexWithNestedField2.Result, MapReduceIndexWithNestedField2>()
-                        .ToList();
+                    //AssertResults<MapReduceIndexWithNestedFieldJs>(session);
 
-                    Assert.Equal(2, results2.Count);
-                    Assert.True(results2.Any(x => x.Total.Currency == "PLN"));
-                    Assert.True(results2.Any(x => x.Total.Currency == "USD"));
+                    //AssertResults<MapReduceIndexWithNestedFieldJs2>(session);
 
-                    var results3 = session.Query<MapReduceIndexWithNestedField2.Result, MapReduceIndexWithNestedFieldJs>()
-                        .ToList();
-
-                    Assert.Equal(2, results3.Count);
-                    Assert.True(results3.Any(x => x.Total.Currency == "PLN"));
-                    Assert.True(results3.Any(x => x.Total.Currency == "USD"));
-
-                    var results4 = session.Query<MapReduceIndexWithNestedField2.Result, MapReduceIndexWithNestedFieldJs2>()
-                        .ToList();
-
-                    Assert.Equal(2, results4.Count);
-                    Assert.True(results4.Any(x => x.Total.Currency == "PLN"));
-                    Assert.True(results4.Any(x => x.Total.Currency == "USD"));
+                    AssertResults<MultiMapReduceIndexWithNestedField>(session);
                 }
+            }
+
+            void AssertResults<TIndex>(IDocumentSession session) where TIndex : AbstractIndexCreationTask, new()
+            {
+                var results1 = session.Query<Result, TIndex>()
+                    .ToList();
+
+                Assert.Equal(2, results1.Count);
+                Assert.True(results1.Any(x => x.Total.Currency == "PLN"));
+                Assert.True(results1.Any(x => x.Total.Currency == "USD"));
             }
         }
     }
