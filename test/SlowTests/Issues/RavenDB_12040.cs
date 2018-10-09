9729f9f115076bacbbfbfb791b8fc1136f49dc8e
Arkadiusz Palinski <arek@ravendb.net>
2018-10-09 11:18:24 +0200

    RavenDB-12040  Limit the number of slow write records that we keep in storage

---
+++ test/SlowTests/Issues/RavenDB_12040.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Server.NotificationCenter.Notifications.Details;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_12040 : RavenTestBase
+    {
+        [Fact]
+        public async Task Should_limit_number_of_stored_slow_io_hints()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var database = await GetDatabase(store.Database);
+
+                for (int i = 0; i < SlowWritesDetails.MaxNumberOfWrites + 10; i++)
+                {
+                    database.NotificationCenter
+                        .SlowWrites
+                        .Add($"C:\\Raven\\Indexes\\MyIndex\\{i}.journal", 1, 10);
+
+                    Thread.Sleep(1);
+                }
+
+                database.NotificationCenter.SlowWrites.UpdateNotificationInStorage(null);
+
+                var details = database.NotificationCenter.SlowWrites
+                    .GetSlowWritesDetails();
+
+                Assert.Equal(SlowWritesDetails.MaxNumberOfWrites, details.Writes.Count);
+
+                for (int i = 10; i < SlowWritesDetails.MaxNumberOfWrites + 10; i++)
+                {
+                    Assert.Contains($"C:\\Raven\\Indexes\\MyIndex\\{i}.journal", details.Writes.Keys);
+                }
+            }
+        }
+    }
+}
