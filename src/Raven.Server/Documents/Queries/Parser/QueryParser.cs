560098b2e2ffa91f1154624a6c9fe3cb60885e7a
Michael Yarichuk <michael.yarichuk@gmail.com>
2018-10-10 20:53:26 +0300

    Support parsing graph queries with edges defined in embedded objects (for example : 'match (o:Orders (id() = 'orders/825-A'))-[:Lines.Product]->(p:Products)') Note : this is parsing only, the actual traversing the edge path is not yet implemented (still WIP) (RavenDB-12028)

--- src/Raven.Server/Documents/Queries/Parser/QueryParser.cs
+++ src/Raven.Server/Documents/Queries/Parser/QueryParser.cs
@@ -25,7 +25,7 @@ namespace Raven.Server.Documents.Queries.Parser
         private int _statePos;
 
         public QueryScanner Scanner = new QueryScanner();
-        private Dictionary<StringSegment, (StringSegment Collection, QueryExpression Filter, bool IsEdge)> _synteticWithQueries;
+        private Dictionary<StringSegment, (StringSegment Collection, QueryExpression Filter, bool IsEdge, List<StringSegment> path)> _synteticWithQueries;
 
         public void Init(string q)
         {
@@ -84,7 +84,7 @@ namespace Raven.Server.Documents.Queries.Parser
                     {
                         if (sq.IsEdge)
                         {
-                            var with = new WithEdgesExpression(sq.Filter, sq.Collection, null);
+                            var with = new WithEdgesExpression(sq.Filter, sq.Collection, null,sq.path);
                             q.TryAddWithEdgePredicates(with, alias);
                         }
                         else
@@ -240,8 +240,12 @@ namespace Raven.Server.Documents.Queries.Parser
             {
                 if (Scanner.Identifier() == false)
                     throw new InvalidQueryException("Failed to read collection after: " + alias, Scanner.Input, null);
-                var collection = new StringSegment(Scanner.Input, Scanner.TokenStart, Scanner.TokenLength);
+
                 QueryExpression filter = null;
+                var collection = new StringSegment(Scanner.Input, Scanner.TokenStart, Scanner.TokenLength);
+                var path = new List<StringSegment>();
+
+                //filter expressions come before path (ex. -[:Lines(ProductName = 'Chang').Product]->)
                 if (Scanner.TryScan('('))
                 {
                     if(Expression(out filter) == false)
@@ -251,16 +255,42 @@ namespace Raven.Server.Documents.Queries.Parser
                         throw new InvalidQueryException("Failed to find closing ')' after filter expression for: " + alias, Scanner.Input, null);
                 }
 
-                AddWithQuery(collection, alias, filter, isEdge, start);
+                //for cases like -[:Lines[].Product]->
+                //the '[]' is essentially a syntax sugar, so simply skip it...
+                if (isEdge && Scanner.TryPeek('['))
+                {
+                    Scanner.TryScan('[');
+                    Scanner.TryScan(']');
+                }
 
+                if (isEdge && Scanner.TryScan('.')) //perhaps we have edge as a field of embedded object?
+                                                    // Example : this is for cases like -[:Lines.Product.Foo.Bar]->
+                                                    // TODO : refactor this to use object pool for List<StringSegment> or something similar
+                {
+                    while (Scanner.Identifier())
+                    {
+                        path.Add(new StringSegment(Scanner.Input, Scanner.TokenStart, Scanner.TokenLength));
+                        if (Scanner.TryPeek('(') || Scanner.TryPeek(']'))
+                        {
+                            break;
+                        }
+
+                        if (Scanner.TryScan('.'))
+                            Scanner.NextPathSegment();
+                    }
+                }
+
+
+
+                AddWithQuery(collection, alias, filter, isEdge, start, path);
             }
             return true;
         }
 
-        private void AddWithQuery(StringSegment collection, StringSegment alias, QueryExpression filter, bool isEdge, int start)
+        private void AddWithQuery(StringSegment collection, StringSegment alias, QueryExpression filter, bool isEdge, int start, List<StringSegment> path = null)
         {
             if (_synteticWithQueries == null)
-                _synteticWithQueries = new Dictionary<StringSegment, (StringSegment Collection, QueryExpression Filter, bool IsEdge)>(StringSegmentEqualityComparer.Instance);
+                _synteticWithQueries = new Dictionary<StringSegment, (StringSegment Collection, QueryExpression Filter, bool IsEdge, List<StringSegment> path)>(StringSegmentEqualityComparer.Instance);
 
             if(_synteticWithQueries.TryGetValue(alias, out var existing))
             {
@@ -279,7 +309,7 @@ namespace Raven.Server.Documents.Queries.Parser
                 return;
             }
 
-            _synteticWithQueries.Add(alias, (collection, filter, isEdge));
+            _synteticWithQueries.Add(alias, (collection, filter, isEdge, path));
         }
 
         private void ThrowDuplicateAliasWithoutSameBody(int start)
