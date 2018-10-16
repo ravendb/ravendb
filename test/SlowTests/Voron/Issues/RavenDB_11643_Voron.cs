026c0b4fe35360b327440f04ffed71df197487b3
Arkadiusz Palinski <arek@ravendb.net>
2018-10-16 13:54:43 +0200

    RavenDB-11643 Fixing tree rebalancing on compressed trees what resulted in map-reduce indexing error.

---
+++ test/SlowTests/Voron/Issues/RavenDB_11643_Voron.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using FastTests.Voron;
+using Voron.Data.BTrees;
+using Xunit;
+
+namespace SlowTests.Voron.Issues
+{
+    public class RavenDB_11643_Voron : StorageTest
+    {
+        [Fact]
+        public void PageRefValidationOnBranchPagesShouldNotThrow()
+        {
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.CreateTree("tree", flags: TreeFlags.LeafsCompressed);
+
+                tx.Commit();
+            }
+
+            var random = new Random(1);
+            var bytes = new byte[1024 * 8];
+
+            // insert
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.ReadTree("tree");
+
+                Assert.True(tree.State.Flags.HasFlag(TreeFlags.LeafsCompressed));
+
+                for (int i = 0; i < 40_000; i++)
+                {
+                    random.NextBytes(bytes);
+
+                    tree.Add(GetKey(i), new MemoryStream(bytes));
+                }
+
+                tx.Commit();
+            }
+
+            // delete
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.ReadTree("tree");
+
+                Assert.True(tree.State.Flags.HasFlag(TreeFlags.LeafsCompressed));
+
+                for (int i = 0; i < 40_000; i += 4)
+                {
+                    tree.Delete(GetKey(i));
+                }
+
+                tree.Delete(GetKey(33392)); // will create a compression tombstone that will be important for this case during rebalancing
+
+                for (int i = 25_000; i >= 0; i--)
+                {
+                    tree.Delete(GetKey(i));
+                }
+
+                tree.DebugValidateBranchReferences();
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.ReadTree("tree");
+
+                for (int i = 0; i < 40_000; i++)
+                {
+                    tree.Delete(GetKey(i));
+                }
+
+                foreach (var pageNumber in tree.AllPages())
+                {
+                    var treePage = tree.GetReadOnlyTreePage(pageNumber);
+
+                    if (treePage.IsCompressed)
+                    {
+                        using (var decompressed = tree.DecompressPage(treePage, skipCache: true))
+                        {
+                            Assert.Equal(0, decompressed.NumberOfEntries);
+
+                            tree.RemoveEmptyDecompressedPage(decompressed);
+                        }
+                    }
+
+                }
+            }
+        }
+
+        private static string GetKey(int i)
+        {
+            return $"{i:D19}.{i:D19}.{i:D19}.{i:D19}.{i:D19}";
+        }
+    }
+}
