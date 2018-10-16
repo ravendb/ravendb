bf4380f5a0c320182a0baaf13cb4cb65bafda918
ml054 <marcin@ravendb.net>
2018-10-16 12:04:47 +0200

    RavenDB-11953 Failed to get index entries in query page

--- src/Raven.Studio/typescript/commands/database/index/getIndexEntriesFieldsCommand.ts
+++ src/Raven.Studio/typescript/commands/database/index/getIndexEntriesFieldsCommand.ts
@@ -4,15 +4,20 @@ import endpoints = require("endpoints");
 
 class getIndexEntriesFieldsCommand extends commandBase {
 
-    constructor(private indexName: string, private db: database) {
+    constructor(private indexName: string, private db: database, private reportFailure: boolean = true) {
         super();
     }
 
     execute(): JQueryPromise<resultsDto<string>> {
-        return this.getIndexEntries()
-            .fail((response: JQueryXHR) => {
+        const task = this.getIndexEntries();
+        
+        if (this.reportFailure) {
+            task.fail((response: JQueryXHR) => {
                 this.reportError("Failed to get index entries", response.responseText, response.statusText);
             });
+        }
+        
+        return task;
     }
     
     private getIndexEntries(): JQueryPromise<resultsDto<string>> {
