332563a1e801cc78ba2c3fe5a859b70555543ebc
ml054 <marcin@ravendb.net>
2018-10-18 14:34:59 +0200

    RavenDB-11955 ReplayTransactionCommands should be handled in Studio notification center - removed duplicate fields no need to have passed time, we already have this information inside operation

--- src/Raven.Client/Documents/Operations/TransactionsRecording/ReplayTxOperationResult.cs
+++ src/Raven.Client/Documents/Operations/TransactionsRecording/ReplayTxOperationResult.cs
@@ -6,15 +6,14 @@ namespace Raven.Client.Documents.Operations.TransactionsRecording
     public class ReplayTxOperationResult : IOperationResult
     {
         public long ExecutedCommandsAmount { get; set; }
-        public TimeSpan PassedTime { get; set; }
 
         public string Message => $"Processed {ExecutedCommandsAmount} commands.";
+        
         public DynamicJsonValue ToJson()
         {
             return new DynamicJsonValue(GetType())
             {
-                [nameof(ExecutedCommandsAmount)] = ExecutedCommandsAmount,
-                [nameof(PassedTime)] = PassedTime
+                [nameof(ExecutedCommandsAmount)] = ExecutedCommandsAmount
             };
         }
 
