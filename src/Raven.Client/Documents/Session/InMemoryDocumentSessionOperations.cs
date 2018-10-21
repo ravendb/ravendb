7cde4bf3faca70ecb2fcd6538c1f853b98125515
Pawel Pekrol <ppekrol@ravendb.net>
2018-10-21 19:34:18 +0200

    RavenDB-12132 renamed 'isCompareExchange' argument to more generic one

--- src/Raven.Client/Documents/Session/InMemoryDocumentSessionOperations.cs
+++ src/Raven.Client/Documents/Session/InMemoryDocumentSessionOperations.cs
@@ -855,7 +855,7 @@ more responsive application.
                 {
                     var djv = new DynamicJsonValue
                     {
-                        ["Object"] = EntityToBlittable.ConvertToBlittableIfNeeded(item.Value.Entity, Conventions, Context, JsonSerializer, documentInfo: null, isCompareExchange: true)
+                        ["Object"] = EntityToBlittable.ConvertToBlittableIfNeeded(item.Value.Entity, Conventions, Context, JsonSerializer, documentInfo: null, removeIdentityProperty: false)
                     };
                     var blittable = Context.ReadObject(djv, item.Key);
                     result.SessionCommands.Add(new PutCompareExchangeCommandData(item.Key, blittable, item.Value.Index));
