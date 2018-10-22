49988c8882e0fa5024bccf536770c9d19cf4b0da
Pawel Pekrol <ppekrol@ravendb.net>
2018-10-22 13:02:34 +0200

    RavenDB-7725 added Raven.Debug

---
+++ tools/Raven.Debug/Program.cs
@@ -0,0 +1,10 @@
+namespace Raven.Debug
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return CommandLineApp.Run(args);
+        }
+    }
+}
