9cf079ee03cdf38797bd511d7fa26c9d848db8c7
egor <egor@ravendb.net>
2018-10-22 12:58:54 +0300

    RavenDB-12110: Failed queries data are now logged in requestLatencyDetails and TrafficWatch.

--- src/Raven.Server/Documents/Queries/IndexQueryServerSide.cs
+++ src/Raven.Server/Documents/Queries/IndexQueryServerSide.cs
@@ -2,10 +2,13 @@
 using System.IO;
 using System.Text;
 using Microsoft.AspNetCore.Http;
+using Raven.Client.Documents.Changes;
 using Raven.Client.Documents.Queries;
 using Raven.Server.Documents.Queries.AST;
 using Raven.Server.Documents.Queries.Timings;
 using Raven.Server.Json;
+using Raven.Server.NotificationCenter;
+using Raven.Server.TrafficWatch;
 using Raven.Server.Web;
 using Sparrow.Json;
 
@@ -18,6 +21,15 @@ namespace Raven.Server.Documents.Queries
 
         [JsonDeserializationIgnore]
         public QueryTimingsScope Timings { get; private set; }
+
+        /// <summary>
+        /// puts the given string in TrafficWatch property of HttpContext.Items
+        /// puts the given type in TrafficWatchChangeType property of HttpContext.Items
+        /// </summary>
+        public static void AddStringToHttpContext(HttpContext httpContext, string str, TrafficWatchChangeType type)
+        {
+            httpContext.Items["TrafficWatch"] = (str, type);
+        }
 
         private IndexQueryServerSide()
         {
@@ -37,17 +49,28 @@ namespace Raven.Server.Documents.Queries
         }
 
         public static IndexQueryServerSide Create(
+            HttpContext httpContext,
             BlittableJsonReaderObject json,
             QueryMetadataCache cache,
+            RequestTimeTracker tracker,
             QueryType queryType = QueryType.Select)
         {
             var result = JsonDeserializationServer.IndexQuery(json);
+            if (tracker != null)
+                tracker.Query = result.Query;
 
             if (result.PageSize == 0 && json.TryGet(nameof(PageSize), out int _) == false)
                 result.PageSize = int.MaxValue;
 
             if (string.IsNullOrWhiteSpace(result.Query))
-                throw new InvalidOperationException($"Index query does not contain '{nameof(Query)}' field.");
+            {
+                var errorMessage = $"Index query does not contain '{nameof(Query)}' field.";
+
+                if (TrafficWatchManager.HasRegisteredClients)
+                    AddStringToHttpContext(httpContext, errorMessage, TrafficWatchChangeType.Queries);
+
+                throw new InvalidOperationException(errorMessage);
+            }
 
             if (cache.TryGetMetadata(result, out var metadataHash, out var metadata))
             {
@@ -55,18 +78,40 @@ namespace Raven.Server.Documents.Queries
                 return result;
             }
 
-            result.Metadata = new QueryMetadata(result.Query, result.QueryParameters, metadataHash, queryType);
+            try
+            {
+                result.Metadata = new QueryMetadata(result.Query, result.QueryParameters, metadataHash, queryType);
+            }
+            catch
+            {
+                if (tracker != null)
+                    tracker.Query = $"Failed: {result.Query}";
+
+                if (TrafficWatchManager.HasRegisteredClients)
+                    AddStringToHttpContext(httpContext, $"Failed: {result.Query}", TrafficWatchChangeType.Queries);
+
+                throw;
+            }
+
             if (result.Metadata.HasTimings)
                 result.Timings = new QueryTimingsScope(start: false);
 
             return result;
         }
 
-        public static IndexQueryServerSide Create(HttpContext httpContext, int start, int pageSize, JsonOperationContext context, string overrideQuery = null)
+        public static IndexQueryServerSide Create(HttpContext httpContext, int start, int pageSize, JsonOperationContext context, RequestTimeTracker tracker, string overrideQuery = null)
         {
             var isQueryOverwritten = !string.IsNullOrEmpty(overrideQuery);
-            if ((httpContext.Request.Query.TryGetValue("query", out var query) == false || query.Count == 0 || string.IsNullOrWhiteSpace(query[0])) && isQueryOverwritten == false)
-                throw new InvalidOperationException("Missing mandatory query string parameter 'query'.");
+            if ((httpContext.Request.Query.TryGetValue("query", out var query) == false || query.Count == 0 || string.IsNullOrWhiteSpace(query[0])) &&
+                isQueryOverwritten == false)
+            {
+                var missingMandatoryQueryStringParameterQuery = "Missing mandatory query string parameter 'query'.";
+
+                tracker.Query = $"Failed: {missingMandatoryQueryStringParameterQuery}";
+                if (TrafficWatchManager.HasRegisteredClients)
+                    AddStringToHttpContext(httpContext, $"Failed: {missingMandatoryQueryStringParameterQuery}", TrafficWatchChangeType.Queries);
+                throw new InvalidOperationException(missingMandatoryQueryStringParameterQuery);
+            }
 
             var actualQuery = isQueryOverwritten ? overrideQuery : query[0];
             var result = new IndexQueryServerSide
@@ -107,11 +152,25 @@ namespace Raven.Server.Documents.Queries
                 }
                 catch (Exception e)
                 {
+                    tracker.Query = $"Failed: {result.Query}";
+                    if (TrafficWatchManager.HasRegisteredClients)
+                        AddStringToHttpContext(httpContext, $"Failed: {result.Query}", TrafficWatchChangeType.Queries);
+
                     throw new ArgumentException($"Could not handle query string parameter '{item.Key}' (value: {item.Value})", e);
                 }
             }
-
-            result.Metadata = new QueryMetadata(result.Query, null, 0);
+            try
+            {
+                result.Metadata = new QueryMetadata(result.Query, null, 0);
+                tracker.Query = result.Query;
+            }
+            catch
+            {
+                tracker.Query = $"Failed: {result.Query}";
+                if (TrafficWatchManager.HasRegisteredClients)
+                    AddStringToHttpContext(httpContext, $"Failed: {result.Query}", TrafficWatchChangeType.Queries);
+                throw;
+            }
 
             if (result.Metadata.HasTimings)
                 result.Timings = new QueryTimingsScope(start: false);
