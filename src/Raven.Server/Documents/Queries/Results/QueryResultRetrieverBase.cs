ea7fab59351ed673365dd58d90d2ca203ddc5abd
Ayende Rahien <Ayende@ayende.com>
2018-10-31 09:37:27 +0200

    RavenDB-12215 - Query projections expects to get a Document, not blittable

--- src/Raven.Server/Documents/Queries/Results/QueryResultRetrieverBase.cs
+++ src/Raven.Server/Documents/Queries/Results/QueryResultRetrieverBase.cs
@@ -516,7 +516,7 @@ namespace Raven.Server.Documents.Queries.Results
 
                 if (string.IsNullOrEmpty(fieldToFetch.Name)) // we need the whole document here
                 {
-                    buffer.Add(doc.Data);
+                    buffer.Add(doc);
                     continue;
                 }
                 if (TryGetFieldValueFromDocument(doc, fieldToFetch, out var val))
