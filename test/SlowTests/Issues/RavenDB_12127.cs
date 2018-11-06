e592a1106d7a70bbfcbcd6bb547ab331c0f2928e
Pawel Pekrol <ppekrol@ravendb.net>
2018-11-06 10:28:32 +0100

    RavenDB-12127 added test

--- test/SlowTests/Issues/RavenDB_12127.cs
+++ test/SlowTests/Issues/RavenDB_12127.cs
@@ -26,6 +26,44 @@ namespace SlowTests.Issues
         }
 
         [Fact]
+        public async Task CanUseSubscriptionWithTcpProtocol40()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Dog
+                    {
+                        Name = "Oscar",
+                        Owner = "people/1"
+                    });
+
+                    session.SaveChanges();
+                }
+
+                var id = store.Subscriptions.Create(new SubscriptionCreationOptions
+                {
+                    Query = @"from Dogs"
+                });
+
+                using (var sub = store.Subscriptions.GetSubscriptionWorker<Dog>(id))
+                {
+                    sub.SubscriptionTcpVersion = 40;
+
+                    var mre = new AsyncManualResetEvent();
+                    var r = sub.Run(batch =>
+                    {
+                        Assert.NotEmpty(batch.Items);
+                        mre.Set();
+                    });
+                    Assert.True(await mre.WaitAsync(TimeSpan.FromSeconds(60)));
+                    await sub.DisposeAsync();
+                    await r;// no error
+                }
+            }
+        }
+
+        [Fact]
         public async Task CannotUseSubscriptionWithIncludesWhenProtocolDoesNotSupportIt()
         {
             using (var store = GetDocumentStore())
