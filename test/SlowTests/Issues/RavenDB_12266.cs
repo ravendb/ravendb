936b3aa74173445f794cf26f1e536cde50f97fa2
Pawel Pekrol <ppekrol@ravendb.net>
2018-11-07 15:58:36 +0100

    RavenDB-12266 added test

---
+++ test/SlowTests/Issues/RavenDB_12266.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using FastTests;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_12266 : RavenTestBase
+    {
+        [Fact]
+        public void Test()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Item()
+                    {
+                        Numbers = new int[] { 1, 2 }
+                    });
+
+                    session.Store(new Item()
+                    {
+                        Numbers = new int[] { 3 }
+                    });
+
+                    session.SaveChanges();
+
+                    var results = session.Query<Item>().OrderBy(x => x.Numbers).ToList();
+
+                    Assert.Equal(2, results.Count);
+                }
+            }
+        }
+
+        private class Item
+        {
+            public string Id { get; set; }
+
+            public int[] Numbers { get; set; }
+        }
+    }
+}
