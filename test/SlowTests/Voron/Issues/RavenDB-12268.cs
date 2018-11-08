4883a7c0ffded55e6e27ded87f57677aac12c701
Grisha Kotler <grisha@ayende.com>
2018-11-08 19:09:01 +0200

    RavenDB-12268 - cleanup the recycled scratch buffers only when there is no open read transaction that is still using it

---
+++ test/SlowTests/Voron/Issues/RavenDB-12268.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using FastTests.Voron;
+using Sparrow;
+using Voron;
+using Xunit;
+
+namespace SlowTests.Voron.Issues
+{
+    public class RavenDB_12268 : StorageTest
+    {
+        [Fact]
+        public void UsedScratchBuffersArentDeleted()
+        {
+            // prevent the work of the global flushing behavior
+            Env.Options.MaxNumberOfPagesInJournalBeforeFlush = long.MaxValue;
+            Env.Options.MaxScratchBufferSize = new Size(64, SizeUnit.Kilobytes).GetValue(SizeUnit.Bytes);
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.CreateTree("tree");
+                tx.Commit();
+            }
+            
+            var random = new Random(1);
+            var bytes = new byte[1024 * 8];
+
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.ReadTree("tree");
+                for (var i = 0; i < 128; i++)
+                {
+                    random.NextBytes(bytes);
+                    tree.Add(i.ToString(), new MemoryStream(bytes));
+                }
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.NewLowLevelTransaction(new TransactionPersistentContext(),
+                TransactionFlags.ReadWrite, timeout: TimeSpan.FromMilliseconds(500)))
+            {
+                tx.ModifyPage(0);
+                tx.Commit();
+            }
+
+            Env.BackgroundFlushWritesToDataFile();
+
+            using (Env.ReadTransaction())
+            {
+                var infoForDebug = Env.ScratchBufferPool.InfoForDebug(0);
+                var numberOfScratchBuffers = infoForDebug.NumberOfScratchFiles;
+
+                Env.ScratchBufferPool.Cleanup();
+                infoForDebug = Env.ScratchBufferPool.InfoForDebug(0);
+
+                // the number of scratch buffers cannot be smaller since we are holding a read tx
+                // but it can be larger because we are creating an empty write tx inside the cleanup
+                Assert.True(numberOfScratchBuffers <= infoForDebug.NumberOfScratchFiles);
+            }
+        }
+    }
+}
