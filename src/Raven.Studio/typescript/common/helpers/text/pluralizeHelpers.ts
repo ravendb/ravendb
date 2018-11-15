47cf01416d8a52b5cb1efaa67b64844c3a684cfb
ml054 <marcin@ravendb.net>
2018-11-15 08:27:26 +0100

    RavenDB-11190 Add performance hints to database in databases view

--- src/Raven.Studio/typescript/common/helpers/text/pluralizeHelpers.ts
+++ src/Raven.Studio/typescript/common/helpers/text/pluralizeHelpers.ts
@@ -2,6 +2,10 @@
 
 class pluralizeHelpers {
     static pluralize(count: number, singular: string, plural: string, onlyPostfix: boolean = false) {
+        if (typeof(count) === "string") {
+            count = parseInt(count);
+        }
+        
         if (onlyPostfix) {
             return count === 1 ? singular : plural;
         } else {
