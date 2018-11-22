a0b08c6868e4ef3878b2053cbf08c66b1ae276a5
aviviadi <avivi.adi.hr@gmail.com>
2018-11-22 14:26:20 +0200

    RavenDB-12365: posix: remove mem pInvokes and fix mprotect signature for 32bits

--- src/Sparrow/Platform/Posix/Syscall.cs
+++ src/Sparrow/Platform/Posix/Syscall.cs
@@ -13,22 +13,6 @@ namespace Sparrow.Platform.Posix
     {
         internal const string LIBC_6 = "libc";
 
-        [DllImport(LIBC_6, EntryPoint = "memcpy", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, SetLastError = false)]
-        [SecurityCritical]
-        public static extern IntPtr Copy(byte* dest, byte* src, long count);
-
-        [DllImport(LIBC_6, EntryPoint = "memcmp", CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
-        [SecurityCritical]
-        public static extern int Compare(byte* b1, byte* b2, long count);
-
-        [DllImport(LIBC_6, EntryPoint = "memmove", CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
-        [SecurityCritical]
-        public static extern int Move(byte* dest, byte* src, long count);
-
-        [DllImport(LIBC_6, EntryPoint = "memset", CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
-        [SecurityCritical]
-        public static extern IntPtr Set(byte* dest, int c, long count);
-
         [DllImport(LIBC_6, EntryPoint = "syscall", SetLastError = true)]
         public static extern long syscall0(long number);
 
@@ -260,7 +244,18 @@ namespace Sparrow.Platform.Posix
         public static extern int statvfs(string path, ref Statvfs buf);
 
         [DllImport(LIBC_6, SetLastError = true)]
-        public static extern int mprotect(IntPtr start, ulong size, ProtFlag protFlag);
+        public static extern int mprotect_64bit(IntPtr start, ulong size, ProtFlag protFlag);
+        
+        [DllImport(LIBC_6, SetLastError = true)]
+        public static extern int mprotect_32bit(IntPtr start, IntPtr size, ProtFlag protFlag);
+
+        public static int mprotect(IntPtr start, ulong size, ProtFlag protFlag)
+        {
+            if (PlatformDetails.Is32Bits == false)
+                return mprotect_64bit(start, size, protFlag);
+            return mprotect_32bit(start, new IntPtr((int)size), protFlag);
+        }
+        
 
         public static void PwriteOrThrow(int fd, byte *buffer, ulong count, long offset, string file, string debug)
         {
