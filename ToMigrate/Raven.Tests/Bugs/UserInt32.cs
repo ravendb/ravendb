e2fc8720de9ebccf4b18ba6479ca8605013ba96b
Ayende Rahien <Ayende@ayende.com>
2018-11-28 08:55:54 +0200

    RavenDB-12394 Remove ToMigrate folder

--- ToMigrate/Raven.Tests/Bugs/UserInt32.cs
+++
@@ -1,8 +0,0 @@
-namespace Raven.Tests.Bugs
-{
-    public class UserInt32
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- ToMigrate/Raven.Tests/Shard/BlogModel/Blog.cs
+++
@@ -1,8 +0,0 @@
-namespace Raven.Tests.Shard.BlogModel
-{
-    public class Blog
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-    }
-}
