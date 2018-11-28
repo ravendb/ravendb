d8217138b0738f6b431fb86ef913b6414c91eca2
Ayende Rahien <Ayende@ayende.com>
2018-11-28 09:39:35 +0200

    RavenDB-12394 Remove ToMigrate folder

--- ToMigrate/Raven.Tests.Common/Dto/TagCloud/ActivityAndCharacterCountAmount.cs
+++
@@ -1,9 +0,0 @@
-namespace Raven.Tests.Common.Dto.TagCloud
-{
-    public class ActivityAndCharacterCountAmount
-    {
-        public string Activity { get; set; }
-        public string Character { get; set; }
-        public long Amount { get; set; }
-    }
-}
