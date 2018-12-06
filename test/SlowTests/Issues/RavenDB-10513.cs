84f8930387a50eeda9b4080bf66aedae052963bf
aviv86 <aviv@ayende.com>
2018-12-06 14:17:20 +0200

    RavenDB-10513 : add another test case

--- test/SlowTests/Issues/RavenDB-10513.cs
+++ test/SlowTests/Issues/RavenDB-10513.cs
@@ -12,6 +12,7 @@ namespace SlowTests.Issues
         {
             public string Id { get; set; }
             public List<string> Targets { get; set; }
+            public Dictionary<string, int> Dict { get; set; }
         }
 
         [Fact]
@@ -53,6 +54,55 @@ namespace SlowTests.Issues
                     var docs = projection.ToList();
                     Assert.Equal(1, docs.Count);
                     Assert.Equal(2, docs[0].Targets.Count);
+                }
+            }
+        }
+
+        [Fact]
+        public void ShouldUseHasOwnProperty2()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Document
+                    {
+                        Dict = new Dictionary<string, int>()
+                        {
+                            {"target_1", 0},
+                            {"target_3", 0},
+                        }
+                    });
+
+                    session.Store(new Document
+                    {
+                        Dict = new Dictionary<string, int>()
+                        {
+                            {"target_2", 0},
+                            {"target_4", 0},
+                        }
+                    });
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var key = "target_3";
+
+                    var projection =
+                        from d in session.Query<Document>()
+                        select new 
+                        {
+                            Id = d.Id,
+                            ContainsKey = d.Dict.ContainsKey(key)
+                        };
+
+                    Assert.Contains(".hasOwnProperty($p0)", projection.ToString());
+
+                    var docs = projection.ToList();
+                    Assert.Equal(2, docs.Count);
+                    Assert.True(docs[0].ContainsKey);
+                    Assert.False(docs[1].ContainsKey);
                 }
             }
         }
