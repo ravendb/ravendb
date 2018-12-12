be9577d11d57169f83dab6a843f4fe5cb3eb9654
ml054 <marcin@ravendb.net>
2018-12-12 13:40:23 +0100

    RavenDB-12456 Studio : Add an option to do encrypted backup

--- src/Raven.Client/Documents/Operations/Backups/EncryptionSettings.cs
+++ src/Raven.Client/Documents/Operations/Backups/EncryptionSettings.cs
@@ -3,5 +3,12 @@ namespace Raven.Client.Documents.Operations.Backups
     public class EncryptionSettings
     {
         public string Key { get; set; }
+        public EncryptionMode EncryptionMode { get; set; }
+    }
+    
+    public enum EncryptionMode {
+        None,
+        UseDatabaseKey,
+        UseProvidedKey
     }
 }
