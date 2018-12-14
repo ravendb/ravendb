7615bc9d0fb75f8ff6e6575c6249a0584e183b10
Arkadiusz Palinski <arek@ravendb.net>
2018-12-14 15:52:54 +0100

    RavenDB-12506 Exception thrown during StorageEnvironment.CreateNewDatabase makes we cannot start this db again

---
+++ test/SlowTests/Voron/Issues/RavenDB_12506.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FastTests.Voron;
+using Voron;
+using Xunit;
+
+namespace SlowTests.Voron.Issues
+{
+    public class RavenDB_12506 : StorageTest
+    {
+        [Fact]
+        public void Error_on_db_creation_must_not_cause_failure_on_next_db_load()
+        {
+            var dataDir = DataDir;
+
+            using (var options = StorageEnvironmentOptions.ForPath(dataDir))
+            {
+                options.SimulateFailureOnDbCreation = true;
+
+                Assert.Throws<InvalidOperationException>(() =>
+                {
+                    using (var a = new StorageEnvironment(options))
+                    {
+
+                    }
+                });
+            }
+
+            using (var options = StorageEnvironmentOptions.ForPath(dataDir))
+            {
+                using (var s = new StorageEnvironment(options))
+                {
+
+                }
+            }
+        }
+    }
+}
