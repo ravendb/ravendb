bf88386b33ef5b0262950aad8fb96412384dd03c
aviviadi <avivi.adi.hr@gmail.com>
2018-12-16 15:09:52 +0200

    RavenDB-12376: 1. Add class Pal (with ctor to rename librvnpal of appropriate plat).   2. Implement WriteFileHeader in posix

---
+++ src/Voron/Platform/PalFlags.cs
@@ -0,0 +1,189 @@
+using System;
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Local
+// ReSharper disable IdentifierTypo
+
+namespace Voron.Platform
+{
+    public static class PalFlags
+    {
+        [Flags]
+        public enum Errno
+        {
+            // errors & their values liberally copied from
+            // FC2 /usr/include/asm/errno.h
+            NONE = 0,
+            EPERM = 1, // Operation not permitted 
+            ENOENT = 2, // No such file or directory 
+            ESRCH = 3, // No such process 
+            EINTR = 4, // Interrupted system call 
+            EIO = 5, // I/O error 
+            ENXIO = 6, // No such device or address 
+            E2BIG = 7, // Arg list too long 
+            ENOEXEC = 8, // Exec format error 
+            EBADF = 9, // Bad file number 
+            ECHILD = 10, // No child processes 
+            EAGAIN = 11, // Try again 
+            ENOMEM = 12, // Out of memory 
+            EACCES = 13, // Permission denied 
+            EFAULT = 14, // Bad address 
+            ENOTBLK = 15, // Block device required 
+            EBUSY = 16, // Device or resource busy 
+            EEXIST = 17, // File exists 
+            EXDEV = 18, // Cross-device link 
+            ENODEV = 19, // No such device 
+            ENOTDIR = 20, // Not a directory 
+            EISDIR = 21, // Is a directory 
+            EINVAL = 22, // Invalid argument 
+            ENFILE = 23, // File table overflow 
+            EMFILE = 24, // Too many open files 
+            ENOTTY = 25, // Not a typewriter 
+            ETXTBSY = 26, // Text file busy 
+            EFBIG = 27, // File too large 
+            ENOSPC = 28, // No space left on device 
+            ESPIPE = 29, // Illegal seek 
+            EROFS = 30, // Read-only file system 
+            EMLINK = 31, // Too many links 
+            EPIPE = 32, // Broken pipe 
+            EDOM = 33, // Math argument out of domain of func 
+            ERANGE = 34, // Math result not representable 
+            EDEADLK = 35, // Resource deadlock would occur 
+            ENAMETOOLONG = 36, // File name too long 
+            ENOLCK = 37, // No record locks available 
+            ENOSYS = 38, // Function not implemented 
+            ENOTEMPTY = 39, // Directory not empty 
+            ELOOP = 40, // Too many symbolic links encountered 
+            EWOULDBLOCK = EAGAIN, // Operation would block 
+            ENOMSG = 42, // No message of desired type 
+            EIDRM = 43, // Identifier removed 
+            ECHRNG = 44, // Channel number out of range 
+            EL2NSYNC = 45, // Level 2 not synchronized 
+            EL3HLT = 46, // Level 3 halted 
+            EL3RST = 47, // Level 3 reset 
+            ELNRNG = 48, // Link number out of range 
+            EUNATCH = 49, // Protocol driver not attached 
+            ENOCSI = 50, // No CSI structure available 
+            EL2HLT = 51, // Level 2 halted 
+            EBADE = 52, // Invalid exchange 
+            EBADR = 53, // Invalid request descriptor 
+            EXFULL = 54, // Exchange full 
+            ENOANO = 55, // No anode 
+            EBADRQC = 56, // Invalid request code 
+            EBADSLT = 57, // Invalid slot 
+
+            EDEADLOCK = EDEADLK,
+
+            EBFONT = 59, // Bad font file format 
+            ENOSTR = 60, // Device not a stream 
+            ENODATA = 61, // No data available 
+            ETIME = 62, // Timer expired 
+            ENOSR = 63, // Out of streams resources 
+            ENONET = 64, // Machine is not on the network 
+            ENOPKG = 65, // Package not installed 
+            EREMOTE = 66, // Object is remote 
+            ENOLINK = 67, // Link has been severed 
+            EADV = 68, // Advertise error 
+            ESRMNT = 69, // Srmount error 
+            ECOMM = 70, // Communication error on send 
+            EPROTO = 71, // Protocol error 
+            EMULTIHOP = 72, // Multihop attempted 
+            EDOTDOT = 73, // RFS specific error 
+            EBADMSG = 74, // Not a data message 
+            EOVERFLOW = 75, // Value too large for defined data type 
+            ENOTUNIQ = 76, // Name not unique on network 
+            EBADFD = 77, // File descriptor in bad state 
+            EREMCHG = 78, // Remote address changed 
+            ELIBACC = 79, // Can not access a needed shared library 
+            ELIBBAD = 80, // Accessing a corrupted shared library 
+            ELIBSCN = 81, // .lib section in a.out corrupted 
+            ELIBMAX = 82, // Attempting to link in too many shared libraries 
+            ELIBEXEC = 83, // Cannot exec a shared library directly 
+            EILSEQ = 84, // Illegal byte sequence 
+            ERESTART = 85, // Interrupted system call should be restarted 
+            ESTRPIPE = 86, // Streams pipe error 
+            EUSERS = 87, // Too many users 
+            ENOTSOCK = 88, // Socket operation on non-socket 
+            EDESTADDRREQ = 89, // Destination address required 
+            EMSGSIZE = 90, // Message too long 
+            EPROTOTYPE = 91, // Protocol wrong type for socket 
+            ENOPROTOOPT = 92, // Protocol not available 
+            EPROTONOSUPPORT = 93, // Protocol not supported 
+            ESOCKTNOSUPPORT = 94, // Socket type not supported 
+            EOPNOTSUPP = 95, // Operation not supported on transport endpoint 
+            EPFNOSUPPORT = 96, // Protocol family not supported 
+            EAFNOSUPPORT = 97, // Address family not supported by protocol 
+            EADDRINUSE = 98, // Address already in use 
+            EADDRNOTAVAIL = 99, // Cannot assign requested address 
+            ENETDOWN = 100, // Network is down 
+            ENETUNREACH = 101, // Network is unreachable 
+            ENETRESET = 102, // Network dropped connection because of reset 
+            ECONNABORTED = 103, // Software caused connection abort 
+            ECONNRESET = 104, // Connection reset by peer 
+            ENOBUFS = 105, // No buffer space available 
+            EISCONN = 106, // Transport endpoint is already connected 
+            ENOTCONN = 107, // Transport endpoint is not connected 
+            ESHUTDOWN = 108, // Cannot send after transport endpoint shutdown 
+            ETOOMANYREFS = 109, // Too many references: cannot splice 
+            ETIMEDOUT = 110, // Connection timed out 
+            ECONNREFUSED = 111, // Connection refused 
+            EHOSTDOWN = 112, // Host is down 
+            EHOSTUNREACH = 113, // No route to host 
+            EALREADY = 114, // Operation already in progress 
+            EINPROGRESS = 115, // Operation now in progress 
+            ESTALE = 116, // Stale NFS file handle 
+            EUCLEAN = 117, // IStructure needs cleaning 
+            ENOTNAM = 118, // Not a XENIX named type file 
+            ENAVAIL = 119, // No XENIX semaphores available 
+            EISNAM = 120, // Is a named type file 
+            EREMOTEIO = 121, // Remote I/O error 
+            EDQUOT = 122, // Quota exceeded 
+
+            ENOMEDIUM = 123, // No medium found 
+            EMEDIUMTYPE = 124, // Wrong medium type 
+
+            ECANCELED = 125,
+            ENOKEY = 126,
+            EKEYEXPIRED = 127,
+            EKEYREVOKED = 128,
+            EKEYREJECTED = 129,
+
+            EOWNERDEAD = 130,
+            ENOTRECOVERABLE = 131,
+
+            // OS X-specific values: OS X value + 1000
+            EPROCLIM = 1067, // Too many processes
+            EBADRPC = 1072, // RPC struct is bad
+            ERPCMISMATCH = 1073, // RPC version wrong
+            EPROGUNAVAIL = 1074, // RPC prog. not avail
+            EPROGMISMATCH = 1075, // Program version wrong
+            EPROCUNAVAIL = 1076, // Bad procedure for program
+            EFTYPE = 1079, // Inappropriate file type or format
+            EAUTH = 1080, // Authentication error
+            ENEEDAUTH = 1081, // Need authenticator
+            EPWROFF = 1082, // Device power is off
+            EDEVERR = 1083, // Device error, e.g. paper out
+            EBADEXEC = 1085, // Bad executable
+            EBADARCH = 1086, // Bad CPU type in executable
+            ESHLIBVERS = 1087, // Shared library version mismatch
+            EBADMACHO = 1088, // Malformed Macho file
+            ENOATTR = 1093, // Attribute not found
+            ENOPOLICY = 1103, // No such policy registered        
+        };
+
+        [Flags]
+        public enum FAIL_CODES : uint
+        {
+            None = uint.MaxValue,
+            SUCCESS = 0,
+            FAIL_OPEN_FILE = 1,
+            FAIL_SEEK_FILE = 2,
+            FAIL_WRITE_FILE = 3,
+            FAIL_SYNC_FILE = 4,
+            FAIL_NOMEM = 5,
+            FAIL_STAT_FILE = 6,
+            FAIL_RACE_RETRIES = 7,
+            FAIL_PATH_RECURSION = 8,
+            FAIL_FLUSH_FILE = 9,
+        };
+    }
+}
---
+++ src/librvnpal/src/posix/writefileheader.c
@@ -0,0 +1,248 @@
+#if defined(__unix__) && !defined(APPLE)
+
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/statfs.h>
+#include <sys/vfs.h>
+#include <libgen.h>
+#include <linux/magic.h>
+#include <string.h>
+#include <assert.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <rvn.h>
+
+#ifndef CIFS_MAGIC_NUMBER
+	#define CIFS_MAGIC_NUMBER     0xff534d42
+#endif
+
+typedef int bool;
+#define true 1
+#define false 0
+
+#define SUCCESS 			0
+#define FAIL_OPEN_FILE 		1
+#define FAIL_SEEK_FILE 		2
+#define FAIL_WRITE_FILE 	3
+#define FAIL_SYNC_FILE 		4
+#define FAIL_NOMEM 			5
+#define FAIL_STAT_FILE 		6
+#define FAIL_RACE_RETRIES 	7
+#define FAIL_PATH_RECURSION 8
+#define FAIL_FLUSH_FILE		9
+
+#define SYNC_DIR_FAILED			-1
+#define SYNC_DIR_ALLOWED 		0
+#define SYNC_DIR_NOT_ALLOWED	1
+
+
+static int32_t flush_file(int32_t fd){
+	/* fcntl(fd, F_FULLFSYNC); */
+	return fsync(fd);
+}
+
+static int32_t sync_directory_allowed(int dir_fd)
+{
+    struct statfs buf;
+    if (fstatfs(dir_fd, &buf) == -1)
+       return SYNC_DIR_FAILED;
+
+	switch (buf.f_type)
+	{
+		case NFS_SUPER_MAGIC:
+		case CIFS_MAGIC_NUMBER:
+		case SMB_SUPER_MAGIC:
+			return SYNC_DIR_ALLOWED;
+		default:
+			return SYNC_DIR_NOT_ALLOWED;
+	}
+}
+
+
+static int32_t sync_directory_for_internal(char *dir_path, uint32_t* detailed_error_code)
+{
+	int rc;
+	int fd = open(dir_path, 0, 0);
+    if (fd == -1)
+    {
+		rc = FAIL_OPEN_FILE;
+		goto error_cleanup;
+	}
+
+	rc = sync_directory_allowed(fd);
+	if(rc == SYNC_DIR_ALLOWED || 
+	   rc == SYNC_DIR_NOT_ALLOWED) {
+		rc = SUCCESS;
+		goto error_cleanup;
+	}
+
+	if(flush_file(fd) == -1){
+		rc = FAIL_FLUSH_FILE;
+		goto error_cleanup;
+	}
+
+	rc = SUCCESS;
+
+error_cleanup:
+	*detailed_error_code = errno;
+	if(fd != -1)
+		close(fd);
+	
+	return rc;
+}
+
+static int32_t sync_directory_maybe_symblink(char *dir_path, int depth, uint32_t* detailed_error_code)
+{
+ 	struct stat sb;
+	char* link_name  = NULL;
+	int rc;
+    
+	int steps = 10;
+
+    while(1){
+		if (lstat(dir_path, &sb) == -1) {
+			rc = FAIL_STAT_FILE;
+			goto error_cleanup;
+		}
+
+		link_name = malloc(sb.st_size + 1);
+		if (link_name == NULL) {
+			rc = FAIL_NOMEM;
+			goto error_cleanup;
+		}
+
+		int len = readlink(dir_path, link_name, sb.st_size + 1);
+
+		if(len == 0 || (len == -1 && errno == EINVAL)) /* EINVAL on non-symlink dir_path */
+		{
+			rc = sync_directory_for_internal(dir_path, detailed_error_code);
+			goto success;
+		}
+
+		if (len < 0) {			
+			rc = FAIL_STAT_FILE;
+			goto error_cleanup;
+		}		
+
+		if (len > sb.st_size) {
+			/* race: the link has changed, re-read */
+			free(link_name);
+			link_name = NULL;
+			if(steps-- > 0)
+				continue;
+			rc = FAIL_RACE_RETRIES;
+			goto error_cleanup;
+		}
+
+		link_name[len] = '\0';
+		break;
+	}
+
+	if(depth == 0){
+		rc = FAIL_PATH_RECURSION;
+		goto error_cleanup;
+	}
+
+	rc = sync_directory_maybe_symblink(link_name, depth - 1, detailed_error_code);
+
+error_cleanup:	
+	*detailed_error_code = errno;
+success:
+	if(link_name != NULL)
+		free(link_name);
+
+	return rc;
+}
+
+int32_t sync_directory_for(const char *file_path, uint32_t* detailed_error_code)
+{
+  	assert(file_path != NULL);
+		
+	char* file_path_copy = NULL;
+	file_path_copy = strdup(file_path);
+	if(file_path_copy == NULL)
+	{
+		*detailed_error_code = errno;
+		return FAIL_NOMEM;
+	}
+	char* dir_path = dirname(file_path_copy);
+
+	int32_t rc = sync_directory_maybe_symblink(dir_path, 4096, detailed_error_code);
+
+	free(file_path_copy);
+
+	return rc;
+}
+
+int32_t rvn_write_header(const char *path, void *header, int32_t size, uint32_t *detailed_error_code)
+{
+	int32_t rc;
+    bool syncIsNeeded = false;
+    int32_t fd = open(path, O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR);
+
+    if (fd == -1)
+	{
+		rc = FAIL_OPEN_FILE;
+		goto error_cleanup;
+	}
+
+    int32_t remaining = size;
+
+    int64_t sz = lseek(fd, 0L, SEEK_END);
+	if (sz == -1)
+    {
+		rc = FAIL_SEEK_FILE;
+		goto error_cleanup;
+	}
+    
+    if (lseek(fd, 0L, SEEK_SET) == -1)
+     {
+		rc = FAIL_SEEK_FILE;
+		goto error_cleanup;
+	}
+    if (sz != remaining)
+        syncIsNeeded = true;
+
+    while (remaining > 0)
+    {
+        uint64_t written = write(fd, header, (uint64_t) remaining);
+        if (written == -1)
+        {
+			rc = FAIL_WRITE_FILE;
+			goto error_cleanup;
+		}
+
+        remaining -= (int) written;
+        header += written;
+    }
+    if (flush_file(fd) == -1)
+    {
+		rc = FAIL_FLUSH_FILE;
+		goto error_cleanup;
+	}
+
+	close(fd);
+	fd = -1;
+
+	if (syncIsNeeded == true)
+		return sync_directory_for(path, detailed_error_code);
+	return SUCCESS;
+
+error_cleanup:
+
+	*detailed_error_code = errno;
+	if(fd != -1)
+		close(fd);
+
+	return rc;
+}
+
+#endif
