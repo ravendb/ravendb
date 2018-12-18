64b0a7c9093395c133226f2b432b262e73201b3f
Ayende Rahien <Ayende@ayende.com>
2018-12-18 16:21:16 +0200

    RavenDB-12376 - Reformatting

--- src/librvnpal/src/mac/getcurrentthreadid.c
+++ src/librvnpal/src/mac/getcurrentthreadid.c
@@ -3,8 +3,7 @@
 #include <pthread.h>
 #include <rvn.h>
 
-unsigned long rvn_get_current_thread_id()
-{
+unsigned long rvn_get_current_thread_id() {
   return pthread_self();
 }
 
--- src/librvnpal/src/posix/getcurrentthreadid.c
+++ src/librvnpal/src/posix/getcurrentthreadid.c
@@ -4,10 +4,9 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
-#include<rvn.h>
+#include <rvn.h>
 
-uint64_t rvn_get_current_thread_id(void)
-{
+uint64_t rvn_get_current_thread_id(void) {
   return syscall(SYS_gettid);
 }
 
