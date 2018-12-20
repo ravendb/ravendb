2c0cd7c3b1fdf71df57777e0120ff209c0e0d323
Arkadiusz Palinski <arek@ravendb.net>
2018-12-20 16:04:07 +0100

    RavenDB-12524 Fixed validation during the recovery process. If a page is overwritten by an overflow in later transaction (in the same journal or next one) then we need to removed it from the collection of modified pages during the recovery.

---
+++ test/SlowTests/Voron/Issues/RavenDB_12524.cs
@@ -0,0 +1,68 @@
+﻿using FastTests.Voron;
+using Voron;
+using Xunit;
+
+namespace SlowTests.Voron.Issues
+{
+    public class RavenDB_12524 : StorageTest
+    {
+        protected override void Configure(StorageEnvironmentOptions options)
+        {
+            options.ManualFlushing = true;
+        }
+
+        [Fact]
+        public void RecoveryValidationNeedsToTakeIntoAccountOverflows()
+        {
+            RequireFileBasedPager();
+
+            long pageNum;
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.LowLevelTransaction.DataPager.EnsureContinuous(20, 10);
+
+                var page = tx.LowLevelTransaction.AllocatePage(50);
+
+                pageNum = page.PageNumber;
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.LowLevelTransaction.FreePage(pageNum);
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.LowLevelTransaction.AllocatePage(1, 21);
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.LowLevelTransaction.FreePage(21);
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.LowLevelTransaction.AllocatePage(2, 20);
+
+                tx.Commit();
+            }
+
+            RestartDatabase();
+
+            using (var tx = Env.WriteTransaction())
+            {
+
+            }
+        }
+    }
+}
