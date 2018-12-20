781470fca30bc29a677b600271a721ffcecf0ed8
egor <egor@ravendb.net>
2018-12-20 17:46:07 +0200

    RavenDB-10585: Check dataDir when MigratingFromLegacyData, make sure the tools is closed if we abort the operation, added ProcessExtensions.

---
+++ src/Raven.Server/Utils/ProcessExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Client.Extensions;
+
+namespace Raven.Server.Utils
+{
+    public static class ProcessExtensions
+    {
+        public static bool TryKill(Process process)
+        {
+            try
+            {
+                process?.Kill();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool TryClose(Process process)
+        {
+            try
+            {
+                process?.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static async Task<string> ReadOutput(StreamReader output)
+        {
+            var sb = new StringBuilder();
+
+            Task<string> readLineTask = null;
+            while (true)
+            {
+                if (readLineTask == null)
+                    readLineTask = output.ReadLineAsync();
+
+                var hasResult = await readLineTask.WaitWithTimeout(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+                if (hasResult == false)
+                    continue;
+
+                var line = readLineTask.Result;
+
+                readLineTask = null;
+
+                if (line != null)
+                    sb.AppendLine(line);
+
+                if (line == null)
+                    break;
+            }
+            return sb.ToString();
+        }
+    }
+}
