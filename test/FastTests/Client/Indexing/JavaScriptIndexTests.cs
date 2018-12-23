788709cee8678288ece528ca51113138cb30df45
Tal Weiss <tal@ayende.com>
2018-12-23 12:10:19 +0200

    RavenDB-12521 handling laod() calls outside of the map function scope

--- test/FastTests/Client/Indexing/JavaScriptIndexTests.cs
+++ test/FastTests/Client/Indexing/JavaScriptIndexTests.cs
@@ -242,9 +242,21 @@ namespace FastTests.Client.Indexing
         [Fact]
         public void CanUseJavaScriptIndexWithLoadDocument()
         {
+            CanUseJavaScriptIndexWithLoadInternal<UsersWithProductsByName>();            
+        }
+
+        [Fact]
+        public void CanUseJavaScriptIndexWithExternalLoadDocument()
+        {
+            CanUseJavaScriptIndexWithLoadInternal<UsersWithProductsByNameWithExternalLoad>();
+        }
+
+        private void CanUseJavaScriptIndexWithLoadInternal<T>() where T : AbstractJavaScriptIndexCreationTask, new()
+        {
             using (var store = GetDocumentStore())
             {
-                store.ExecuteIndex(new UsersWithProductsByName());
+                var index = new T();
+                store.ExecuteIndex(index);
                 using (var session = store.OpenSession())
                 {
                     var productId = "Products/1";
@@ -261,9 +273,8 @@ namespace FastTests.Client.Indexing
                     }, productId);
                     session.SaveChanges();
                     WaitForIndexing(store);
-                    session.Query<User>("UsersWithProductsByName").Single(x => x.Name == "Brendan Eich");
+                    session.Query<User>(index.IndexName).Single(x => x.Name == "Brendan Eich");
                 }
-
             }
         }
 
@@ -720,6 +731,20 @@ namespace FastTests.Client.Indexing
             }
         }
 
+        private class UsersWithProductsByNameWithExternalLoad : AbstractJavaScriptIndexCreationTask
+        {
+            public UsersWithProductsByNameWithExternalLoad()
+            {
+                Maps = new HashSet<string>
+                {
+                    @"
+function GetProductName(u){
+    return load(u.Product,'Products').Name
+}
+map('Users', function (u){ return { Name: u.Name, Count: 1, Product: GetProductName(u)};})",
+                };
+            }
+        }
         private class Location
         {
             public string Description { get; set; }
