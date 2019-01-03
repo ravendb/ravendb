b62dac5dae2d367ce6eb3bb01400842a675f2da4
Grisha Kotler <grisha@ayende.com>
2019-01-03 19:06:06 +0200

    RavenDB-12442 - Error converting value "NULL_VALUE" to type 'System.Nullable`1

--- src/Raven.Server/Documents/Patch/ScriptRunner.cs
+++ src/Raven.Server/Documents/Patch/ScriptRunner.cs
@@ -1200,6 +1200,7 @@ namespace Raven.Server.Documents.Patch
                 origin.NoCache = noCache;
                 return cloned;
             }
+
             private JsValue TranslateToJs(Engine engine, JsonOperationContext context, object o)
             {
                 if (o is Tuple<Document, Lucene.Net.Documents.Document, IState> t)
