5ad525de14dcb8c63f1f311c7f07f9c08690430d
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-01-06 18:26:34 +0200

    Adding a test to verify that the RavenDB-11671 issue is resolved

---
+++ test/SlowTests/Issues/RavenDB-11671.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FastTests;
+using Newtonsoft.Json.Linq;
+using Orders;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_11671 : RavenTestBase
+    {
+        [Fact]
+        public void Can_properly_parse_query()
+        {
+            using (var store = GetDocumentStore())
+            {
+                CreateNorthwindDatabase(store);
+                using (var session = store.OpenSession())
+                {
+                    var orders = session.Query<Order>().ToList();
+                    var results = session.Advanced.RawQuery<Order>("match (Employees as e)<-[Employee]-(Orders as o)-[Company]->(Companies as c) select o").ToArray();
+                    Assert.Equal(orders,results);
+                }
+            }
+        }
+    }
+}
