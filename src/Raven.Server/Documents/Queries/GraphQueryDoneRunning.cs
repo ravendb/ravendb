fa4a323e560b2d118a3951772c590f8f1a8a9893
Tal Weiss <tal@ayende.com>
2019-01-07 19:32:29 +0200

    RavenDB-12137 [WIP] Add graph queries to RunningQueries

---
+++ src/Raven.Server/Documents/Queries/GraphQueryDoneRunning.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Raven.Server.Documents.Queries
+{
+    public struct GraphQueryDoneRunning : IDisposable
+    {
+        private static readonly TimeSpan DefaultLockTimeout = TimeSpan.FromSeconds(3);
+
+        private static readonly TimeSpan ExtendedLockTimeout = TimeSpan.FromSeconds(30);
+
+        readonly GraphQueryRunner _parent;
+        private readonly ExecutingQueryInfo _queryInfo;
+
+        public GraphQueryDoneRunning(GraphQueryRunner parent, ExecutingQueryInfo queryInfo)
+        {
+            _parent = parent;
+            _queryInfo = queryInfo;
+        }
+
+        public void Dispose()
+        {
+
+            if (_queryInfo != null)
+                _parent.Database.RemoveFromCurrentlyRunningGraphQueries(_queryInfo);
+        }
+    }
+}
