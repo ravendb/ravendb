40939c7f9bb2267f1600b73a257b8fd9df4453f8
Michael Yarichuk <michael.yarichuk@gmail.com>
2019-01-09 18:28:21 +0200

    Changes after code review of RavenDB-12397. Added defensive code that verifies syntax when dealing with arrows (->) changing direction, handled relevant error edge-cases, added tests

--- src/Raven.Server/Documents/Queries/AST/GraphQuerySyntaxValidatorVisitor.cs
+++ src/Raven.Server/Documents/Queries/AST/GraphQuerySyntaxValidatorVisitor.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
+﻿using System.Runtime.CompilerServices;
 using Raven.Client.Exceptions;
 
 namespace Raven.Server.Documents.Queries.AST
