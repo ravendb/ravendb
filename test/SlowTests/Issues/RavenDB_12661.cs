8197668711bc3711e66dbda8934c21e8c5e4a58d
Arkadiusz Palinski <arek@ravendb.net>
2019-01-10 11:41:56 +0100

    RavenDB-12661 Fixing the order of disposing blittables in ApplyMetadataModifications so we can reread the document while metadata has been already modified.

---
+++ test/SlowTests/Issues/RavenDB_12661.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.Documents.Session;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_12661 : RavenTestBase
+    {
+        [Fact]
+        public async Task Can_delete_simple_attachment()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenAsyncSession())
+                {
+                    var document = new TestDocument { Id = "docs/1" };
+                    await session.StoreAsync(document);
+
+                    using (StoreTestFile(session, document, "test.txt", "test"))
+                    {
+                        await session.SaveChangesAsync();
+                    }
+                }
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    var document = await session.LoadAsync<TestDocument>("docs/1");
+
+                    session.Advanced.Attachments.Delete(document, "test.txt");
+
+                    await session.SaveChangesAsync();
+                }
+            }
+        }
+
+        private MemoryStream StoreTestFile<T>(IAsyncDocumentSession session, T entity, string filename, string content)
+        {
+            var stream = new MemoryStream();
+            session.Advanced.Attachments.Store(entity, filename, stream, "text/plain");
+            return stream;
+        }
+    }
+}
