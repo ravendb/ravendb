526dc9d2203b2d0d66036ab2f57922c2edb1b252
igal <igalmer@gmail.com>
2019-01-20 19:03:35 +0200

    RavenDB-12376 _write_file_incrementally -> write_file_in_sections and logic simplified

--- src/librvnpal/inc/internal_win.h
+++ src/librvnpal/inc/internal_win.h
@@ -7,7 +7,7 @@ PRIVATE int32_t
 _write_file(void* handle, const void* buffer, int64_t size, int64_t offset, int32_t* detailed_error_code);
 
 PRIVATE int32_t
-_write_file_incrementally(void* handle, char** buffer, int64_t* size, int64_t* offset, int64_t increment_size, int32_t* detailed_error_code);
+write_file_in_sections(void* handle, const char* buffer, int64_t size, int64_t offset, uint32_t section_size, int32_t* detailed_error_code);
 
 #endif
 #endif
