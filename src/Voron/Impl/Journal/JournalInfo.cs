a1e2e4e9bfd78ef4fb0c925d661cd7f4085fbfc5
Arkadiusz Palinski <arek@ravendb.net>
2019-01-22 13:44:12 +0100

    RavenDB-12725 - Changing the way we're detecting what range of journal files is necessary to properly recover on database startup. The issue was that that we relied on JournalFilesCount which was in-memory state and under some conditions could be invalid. In result, on recovery startup, we were deleting a journal that had page that were not applied to data file yet. - Applying the same logic when taking a full backup so we won't miss any journal file - Added validation to the recovery which will prevent from loading a database if _first_ journal file is missing - We no longer delete any journals after we apply their data to the data file - we must not do it because we don't sync the data file during the recovery. The first flush will schedule the data file sync which will remove already synced journals. - We no longer modify the headers after the recovery process - as we don't delete any journals there now - Changed the format FileHeader (JournalInfo.JournalFilesCount changed to JournalInfo.Reserved and JournalInfo.Flags) and bumping Voron version. Added relevant schema update. - Enhanced exception messages
    Original PR: https://github.com/ravendb/ravendb/pull/8538

--- src/Voron/Impl/Journal/JournalInfo.cs
+++ src/Voron/Impl/Journal/JournalInfo.cs
@@ -4,23 +4,36 @@
 //  </copyright>
 // -----------------------------------------------------------------------
 
+using System;
 using System.Runtime.InteropServices;
 
 namespace Voron.Impl.Journal
 {
-	[StructLayout(LayoutKind.Explicit, Pack = 1)]
-	public struct JournalInfo
-	{
-		[FieldOffset(0)]
-		public long CurrentJournal;
+    [StructLayout(LayoutKind.Explicit, Pack = 1)]
+    public unsafe struct JournalInfo
+    {
+        public const int NumberOfReservedBytes = 3;
 
-		[FieldOffset(8)]
-		public long LastSyncedJournal;
+        [FieldOffset(0)]
+        public long CurrentJournal;
 
-		[FieldOffset(16)]
-		public long LastSyncedTransactionId;
+        [FieldOffset(8)]
+        public long LastSyncedJournal;
+
+        [FieldOffset(16)]
+        public long LastSyncedTransactionId;
 
         [FieldOffset(24)]
-        public int JournalFilesCount;
+        public fixed byte Reserved[NumberOfReservedBytes];
+
+        [FieldOffset(27)]
+        public JournalInfoFlags Flags;
+    }
+
+    [Flags]
+    public enum JournalInfoFlags : byte
+    {
+        None = 0,
+        IgnoreMissingLastSyncJournal = 1
     }
 }
---
+++ src/Voron/Schema/VoronSchemaUpdater.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Voron.Global;
+using Voron.Impl.FileHeaders;
+
+namespace Voron.Schema
+{
+    public class VoronSchemaUpdater
+    {
+        private readonly HeaderAccessor _headerAccessor;
+        private readonly StorageEnvironmentOptions _options;
+
+        public VoronSchemaUpdater(HeaderAccessor headerAccessor, StorageEnvironmentOptions options)
+        {
+            _headerAccessor = headerAccessor;
+            _options = options;
+        }
+
+        public unsafe void Update()
+        {
+            while (_headerAccessor.Get(header => header->Version) < Constants.CurrentVersion)
+            {
+                var currentVersion = _headerAccessor.Get(header => header->Version);
+                var name = $"Voron.Schema.Updates.From{currentVersion}";
+
+                var schemaUpdateType = typeof(IVoronSchemaUpdate).Assembly.GetType(name);
+                if (schemaUpdateType == null)
+                    break;
+
+                var schemaUpdate = (IVoronSchemaUpdate)Activator.CreateInstance(schemaUpdateType);
+
+                if (schemaUpdate.Update(currentVersion, _options, _headerAccessor, out var versionAfterUpdate) == false)
+                    break;
+
+                _headerAccessor.Modify(header => header->Version = versionAfterUpdate);
+            }
+        }
+    }
+}
