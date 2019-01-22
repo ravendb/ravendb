66221018c2404186233a048e7d00f5f8092e52bc
aviviadi <avivi.adi.hr@gmail.com>
2019-01-22 23:55:35 +0200

    RavenDB-12376: add wsl setup and build script and support

--- src/librvnpal/make.sh
+++ src/librvnpal/make.sh
@@ -6,6 +6,7 @@ C_COMPILER=c89
 C_ADDITIONAL_FLAGS=""
 C_SHARED_FLAG="-shared"
 IS_CROSS=0
+SKIP_COPY=0
 
 NC="\\e[39m"
 C_BLACK="\\e[30m"
@@ -52,18 +53,22 @@ elif [ $# -gt 1 ]; then
 		if [[ "$2" == "osx-x64" ]]; then
 			IS_CROSS=1
 			IS_MAC=1
-			C_COMPILER="o64-clang"
+			C_COMPILER="osxcross/target/bin/o64-clang"
 		        C_ADDITIONAL_FLAGS="-Wno-ignored-attributes"
 		elif [[ "$2" == "linux-arm" ]]; then
                         IS_CROSS=1
                         IS_ARM=1
                         IS_32BIT=1
                         C_COMPILER=arm-linux-gnueabihf-gcc
+			C_ADDITIONAL_FLAGS="-Wno-int-to-pointer-cast -Wno-pointer-to-int-cast"
 		elif [[ "$2" == "linux-arm64" ]]; then
                         IS_CROSS=1
                         IS_ARM=1
                         IS_32BIT=0
                         C_COMPILER=aarch64-linux-gnu-gcc
+		elif [[ "$2" == "linux-x64" ]]; then
+			IS_CROSS=1
+			IS_LINUX=1
 		else
 			echo -e "${ERR_STRING}Invalid architecture for cross compiling${NC}"
 			exit 1
@@ -71,6 +76,8 @@ elif [ $# -gt 1 ]; then
 		if [ $# -eq 3 ]; then
 			if [[ "$3" == "clean" ]]; then
 				CLEAN=1
+			elif [[ "$3" == "skip-copy" ]]; then
+				SKIP_COPY=1
 			else
 				echo -e "${ERR_STRING}Invalid arguments for cross compiling${NC}"
 				exit 1
@@ -134,7 +141,6 @@ if [ ${IS_LINUX} -eq 1 ]; then
 fi
 if [ ${IS_MAC} -eq 1 ]; then
 	FILTERS=(src/posix)
-       	FILTERS+=(src/mac)
 	LINKFILE=${LIBFILE}.mac
 	if [ ${IS_CROSS} -eq 0 ]; then
 		C_COMPILER="clang"
@@ -176,12 +182,7 @@ for SRCPATH in "${FILTERS[@]}"; do
 		if [ ${CLEAN} -eq 0 ]; then
 			CMD="${C_COMPILER} ${C_ADDITIONAL_FLAGS} -fPIC -Iinc -O2 -Wall -c -o ${SRCFILE_O} ${SRCFILE}"
 		else
-			if [ -f ${LINKFILE} ]; then 
 				CMD="rm ${SRCFILE_O}"
-			else
-				CMD=""
-				continue
-			fi
 		fi
 		echo -e "${C_L_GREEN}${CMD}${NC}${NT}"
 		${CMD}
@@ -195,28 +196,37 @@ else
 	if [ -f ${LINKFILE} ]; then 
 		CMD="rm ${LINKFILE}"
 		echo -e "${C_L_GREEN}${T_BOLD}${CMD}${NC}${NT}"
+	else
+		CMD=""
 	fi
 fi
-${CMD} 
-if [ $? -ne 0 ]; then
+${CMD}
+TMP_STAT=$?
+if [ ${CLEAN} -eq 0 ]; then
+	if [ ${TMP_STAT} -ne 0 ]; then
+		echo ""
+		echo -e "${ERR_STRING}: Build Failed."
+		exit 1
+	fi
 	echo ""
-	echo -e "${ERR_STRING}: Build Failed."
-	exit 1
+	echo -e "${C_L_CYAN}Done for ${C_L_GREEN}${T_BLINK}${LINKFILE}${NT}${NC}"
 fi
 echo ""
-echo -e "${C_L_CYAN}Done for ${C_L_GREEN}${T_BLINK}${LINKFILE}${NT}${NC}"
 if [ ${CLEAN} -eq 0 ]; then
-	echo ""
-	echo -ne "${C_L_RED}Do you want to copy ${C_L_GREEN}${LINKFILE}${C_L_RED} to ${C_L_GREEN}../../libs/librvnpal/${LINKFILE}${C_RED} [Y/n] ? ${NC}"
-	read -rn 1 -d '' "${T[@]}" "${S[@]}" ANSWER
-	echo ""
-	echo ""
-	if [[ "${ANSWER}" == "" ]] || [[ "${ANSWER}" == "y" ]] || [[ "${ANSWER}" == "Y" ]]; then
-		CMD="cp ${LINKFILE} ../../libs/librvnpal/${LINKFILE}"
-		echo -e "${C_L_GREEN}${T_BOLD}${CMD}${NC}${NT}"
-		${CMD}
+	if [ ${SKIP_COPY} -eq 0 ]; then
+		echo -ne "${C_L_RED}Do you want to copy ${C_L_GREEN}${LINKFILE}${C_L_RED} to ${C_L_GREEN}../../libs/librvnpal/${LINKFILE}${C_RED} [Y/n] ? ${NC}"
+		read -rn 1 -d '' "${T[@]}" "${S[@]}" ANSWER
 		echo ""
-		echo "Exiting..."
+		echo ""
+		if [[ "${ANSWER}" == "" ]] || [[ "${ANSWER}" == "y" ]] || [[ "${ANSWER}" == "Y" ]]; then
+			CMD="cp ${LINKFILE} ../../libs/librvnpal/${LINKFILE}"
+			echo -e "${C_L_GREEN}${T_BOLD}${CMD}${NC}${NT}"
+			${CMD}
+			echo ""
+			echo "Exiting..."
+		fi
+	else
+		echo "---> ${LINKFILE}"
 	fi
 fi
 
