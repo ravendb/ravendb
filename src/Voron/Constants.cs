3d9468cbfb1bd12deb5178679172eed2f34452ad
Arkadiusz Palinski <arek@ravendb.net>
2019-01-22 13:44:12 +0100

    RavenDB-12725
    - Changing the way we're detecting what range of journal files is necessary to properly recover on database startup. The issue was that that we relied on JournalFilesCount which was in-memory state and under some conditions could be invalid. In result, on recovery startup, we were deleting a journal that had page that were not applied to data file yet.
    - Applying the same logic when taking a full backup so we won't miss any journal file
    - Added validation to the recovery which will prevent from loading a database if _first_ journal file is missing
    - We no longer delete any journals after we apply their data to the data file - we must not do it because we don't sync the data file during the recovery. The first flush will schedule the data file sync which will remove already synced journals.
    - We no longer modify the headers after the recovery process - as we don't delete any journals there now
    - Changed the format FileHeader (JournalInfo.JournalFilesCount changed to JournalInfo.Reserved and JournalInfo.Flags) and bumping Voron version. Added relevant schema update.
    - Enhanced exception messages
    
    Original PR: https://github.com/ravendb/ravendb/pull/8538

--- src/Voron/Constants.cs
+++ src/Voron/Constants.cs
@@ -8,7 +8,7 @@ namespace Voron.Global
 {
     public unsafe class Constants
     {
-        public const int CurrentVersion = 22;
+        public const int CurrentVersion = 23;
 
         public const ulong MagicMarker = 0xB16BAADC0DEF0015;
         public const ulong TransactionHeaderMarker = 0x1A4C92AD90ABC123;
