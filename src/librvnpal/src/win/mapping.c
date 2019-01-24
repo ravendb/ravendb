17fb03f8471b221045168200bdb1ea4f31404123
aviviadi <avivi.adi.hr@gmail.com>
2019-01-24 17:17:15 +0200

    RavenDB-12376: fix compilation warnings in win

--- src/librvnpal/src/win/mapping.c
+++ src/librvnpal/src/win/mapping.c
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <VersionHelpers.h>
 
 #include "rvn.h"
 #include "status_codes.h"
@@ -6,8 +7,12 @@
 
 EXPORT int32_t
 rvn_discard_virtual_memory(void* address, int64_t size, int32_t* detailed_error_code)
-{
+{    
     if (IsWindows10OrGreater() == false)
         return 0;
-    return DiscardVirtualMemory(address, size);
+
+    int32_t rc = DiscardVirtualMemory(address, (size_t)size);
+    if (rc != 0)
+        *detailed_error_code = GetLastError();
+    return rc;
 }
