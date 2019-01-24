ec949fdd3af2723f94a5c7e7c2d27213c55c0095
Arkadiusz Palinski <arek@ravendb.net>
2019-01-24 11:29:50 +0100

    RavenDB-12725 Bumping Voron version. Added schema upgrade which applies the change in FileHeader - we changed JournalInfo.JournalFilesCount to JournalInfo.Reserved and JournalInfo.Flags. Test added.

---
+++ src/Voron/Schema/Updates/From22.cs
@@ -0,0 +1,26 @@
+﻿using Sparrow;
+using Voron.Impl.FileHeaders;
+using Voron.Impl.Journal;
+
+namespace Voron.Schema.Updates
+{
+    public class From22 : IVoronSchemaUpdate
+    {
+        public unsafe bool Update(int currentVersion, StorageEnvironmentOptions options, HeaderAccessor headerAccessor, out int versionAfterUpgrade)
+        {
+            headerAccessor.Modify(header =>
+            {
+                Memory.Set(header->Journal.Reserved, 0, 3);
+                
+                if (options.JournalExists(header->Journal.LastSyncedJournal))
+                    header->Journal.Flags = JournalInfoFlags.None;
+                else
+                    header->Journal.Flags = JournalInfoFlags.IgnoreMissingLastSyncJournal;
+            });
+
+            versionAfterUpgrade = 23;
+
+            return true;
+        }
+    }
+}
