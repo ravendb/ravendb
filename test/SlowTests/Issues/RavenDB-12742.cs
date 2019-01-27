ff455dfcdf9727ed95ba606e1e83321aa9ebcf4b
Ayende Rahien <Ayende@ayende.com>
2019-01-27 16:43:07 +0200

    RavenDB-12742 - Removed _dirtyOverflowPages, which was never actually used - Making sure that if we modify the same page range in the same transaction multiple times, we aren't remembering the old values

---
+++ test/SlowTests/Issues/RavenDB-12742.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FastTests;
+using FastTests.Voron;
+using Voron.Data.BTrees;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_12742 : StorageTest
+    {
+        [Fact]
+        public void CanOverwriteBigValueInSameTx()
+        {
+            var r = new Random();
+            var bytes = new byte[32 * 1024];
+
+            using (var tx = Env.WriteTransaction())
+            {
+                Tree tree = tx.CreateTree("tree");
+
+                r.NextBytes(bytes);
+                tree.Add("key", bytes);
+
+                r.NextBytes(bytes);
+                tree.Add("key", bytes);
+
+                tx.Commit();
+            }
+        }
+    }
+}
