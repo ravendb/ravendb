579cc4cd7664810a3b9d67595a1446814f752e4a
aviviadi <avivi.adi.hr@gmail.com>
2019-01-30 18:16:06 +0200

    RavenDB-12376: windows complie using VC 14.0

--- src/librvnpal/build-all-windows.bat
+++ src/librvnpal/build-all-windows.bat
@@ -1,9 +1,32 @@
+@echo off
 if not exist "artifacts" mkdir "artifacts"
+if exist artifacts\librvnpal.x64.dll del artifacts\librvnpal.x64.dll
+if exist artifacts\librvnpal.x86.dll del artifacts\librvnpal.x86.dll
 
-cmd.exe /c ""C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" & "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars32.bat" & cl -Felibrvnpal.x86.dll -I inc /O2 /sdl /experimental:external /external:anglebrackets /external:W0 /Wall /LD src\*.c src\win\*.c /link"
+set vcbin=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin
+
+cmd.exe /c ""%vcbin%\vcvars32.bat" & "%vcbin%\cl" -Felibrvnpal.x86.dll -I inc /O2 /sdl /LD src\*.c src\win\*.c /link"
 copy librvnpal.x86.dll artifacts\librvnpal.win.x86.dll
 del *.obj librvnpal.x*
 
-cmd.exe /c ""C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" & "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat" & cl -Felibrvnpal.x64.dll -I inc /O2 /sdl /experimental:external /external:anglebrackets /external:W0 /Wall /LD src\*.c src\win\*.c /link"
+cmd.exe /c ""%vcbin%\amd64\vcvars64.bat" & "%vcbin%\amd64\cl" -Felibrvnpal.x64.dll -I inc /O2 /sdl /LD src\*.c src\win\*.c /link"
 copy librvnpal.x64.dll artifacts\librvnpal.win.x64.dll
 del *.obj librvnpal.x*
+
+echo =================================================
+if exist artifacts\librvnpal.win.x64.dll (
+	echo = Build win-x64 librvnpal.win.x64.dll : SUCCESS =
+) else (
+	echo = Build win-x64 librvnpal.win.x64.dll : FAIL    =
+)
+if exist artifacts\librvnpal.win.x86.dll (
+	echo = Build win-x86 librvnpal.win.x86.dll : SUCCESS =
+) else (
+	echo = Build win-x86 librvnpal.win.x86.dll : FAIL    =
+)
+echo =================================================
+
+
+
+
+
