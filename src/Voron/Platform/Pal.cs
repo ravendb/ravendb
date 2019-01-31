974363cb749a0cc1f34c354fa7e0b8871a55bd15
aviviadi <avivi.adi.hr@gmail.com>
2019-01-31 13:59:36 +0200

    RavenDB-12376: take librvnpal by it's latest creation timestamp

--- src/Voron/Platform/Pal.cs
+++ src/Voron/Platform/Pal.cs
@@ -45,13 +45,14 @@ namespace Voron.Platform
             }
 
             try
-            {
-                if (File.Exists(fromFilename))
-                {
-                    if (File.Exists(toFilename))
-                        File.Delete(toFilename);
-                    File.Move(fromFilename, toFilename);
-                }
+            {                
+                var toTime = DateTime.MinValue.Ticks;
+                if (File.Exists(toFilename))
+                    toTime = new FileInfo(toFilename).CreationTime.Ticks;
+
+                if (File.Exists(fromFilename) && 
+                    new FileInfo(fromFilename).CreationTime.Ticks > toTime)
+                        File.Copy(fromFilename, toFilename, overwrite: true);
             }
             catch (IOException e)
             {
