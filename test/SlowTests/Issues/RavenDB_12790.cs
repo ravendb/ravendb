51a9c1104db06d906915b512322f43aadfab6293
Arkadiusz Palinski <arek@ravendb.net>
2019-01-31 10:03:58 +0100

    RavenDB-12790 Lazy query should throw IndexDoesNotExistException if index doesn't exist instead of NRE

---
+++ test/SlowTests/Issues/RavenDB_12790.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using FastTests;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Exceptions.Documents.Indexes;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_12790 : RavenTestBase
+    {
+        [Fact]
+        public void LazyQueryAgainstMissingIndex()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var document = new Document
+                    {
+                        Name = "name"
+                    };
+                    session.Store(document);
+                    session.SaveChanges();
+                }
+
+                // intentionally not creating the index that we query against
+
+                using (var session = store.OpenSession())
+                {
+                    Assert.Throws<IndexDoesNotExistException>(
+                        () => session.Query<Document, DocumentIndex>().ToList());
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var lazyQuery = session.Query<Document, DocumentIndex>()
+                        .Lazily();
+
+                    Assert.Throws<IndexDoesNotExistException>(() => lazyQuery.Value);
+                }
+            }
+        }
+
+        private class DocumentIndex : AbstractIndexCreationTask<Document>
+        {
+        }
+
+        private class Document
+        {
+            public string Name;
+        }
+    }
+}
