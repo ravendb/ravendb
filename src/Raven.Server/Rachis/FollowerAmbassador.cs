204826e907cfe30068cc4ea36de788899ccf14d2
Grisha Kotler <grisha@ayende.com>
2019-02-03 19:30:29 +0200

    RavenDB-12824 - Excepted exception shouldn't be visible in the notification center

--- src/Raven.Server/Rachis/FollowerAmbassador.cs
+++ src/Raven.Server/Rachis/FollowerAmbassador.cs
@@ -203,7 +203,9 @@ namespace Raven.Server.Rachis
                         }
                         catch (Exception e)
                         {
-                            NotifyOnException(ref hadConnectionFailure, new Exception($"Failed to create a connection to node {_tag} at {_url}", e));
+                            if (e is RachisConcurrencyException == false)
+                                NotifyOnException(ref hadConnectionFailure, new Exception($"Failed to create a connection to node {_tag} at {_url}", e));
+
                             _leader.WaitForNewEntries().Wait(TimeSpan.FromMilliseconds(_engine.ElectionTimeout.TotalMilliseconds / 2));
                             continue; // we'll retry connecting
                         }
