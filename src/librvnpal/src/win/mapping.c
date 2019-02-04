81a2b9f9bd318be5a6cd65c994ce4c863873929a
aviviadi <avivi.adi.hr@gmail.com>
2019-02-04 11:31:24 +0200

    RavenDB-12376: call DiscardVirtualMemory using GetProcAddress so lower then windows 10 OS versions can load librvnpal dll

--- src/librvnpal/src/win/mapping.c
+++ src/librvnpal/src/win/mapping.c
@@ -5,14 +5,32 @@
 #include "status_codes.h"
 #include "internal_win.h"
 
+typedef unsigned long 
+(__cdecl *DISCARD_VIRTUAL_MEMORY_FUNC_PTR)(void *, int64_t);
+
+PRIVATE DISCARD_VIRTUAL_MEMORY_FUNC_PTR 
+_discard_virtual_memory_func;
+
+PRIVATE bool
+_init_discard_virtual_memory_flag = false;
+
 EXPORT int32_t
 rvn_discard_virtual_memory(void* address, int64_t size, int32_t* detailed_error_code)
 {    
-    if (IsWindows10OrGreater() == false)
-        return 0;
+    if (_init_discard_virtual_memory_flag == false)
+    {
+        _init_discard_virtual_memory_flag = true;
+        _discard_virtual_memory_func = (DISCARD_VIRTUAL_MEMORY_FUNC_PTR) GetProcAddress(
+                GetModuleHandle(TEXT("kernel32.dll")),
+                "DiscardVirtualMemory");
+    }
 
-    int32_t rc = DiscardVirtualMemory(address, (size_t)size);
-    if (rc != 0)
-        *detailed_error_code = GetLastError();
-    return rc;
+    if (_discard_virtual_memory_func != NULL)
+    {
+        int32_t rc = _discard_virtual_memory_func(address, (size_t)size);
+        if (rc != 0)
+            *detailed_error_code = errno;
+        return rc;
+    }
+    return 0;
 }
