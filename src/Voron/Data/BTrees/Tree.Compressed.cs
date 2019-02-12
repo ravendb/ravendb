ravendb/ravendb

commit 66d3eea6115481f8d40b579eae8df75e822ef5e1

RavenDB-12700 Handling the case that we might not be able to compress already decompressed page because decompression might change the layout of data. In result we threw exception while it was a valid (edge) case.

src/Voron/Data/BTrees/Tree.Compressed.cs
@@ -297,7 +297,8 @@ namespace Voron.Data.BTrees
             }
             finally
             {
-                decompressed.CopyToOriginal(_llt, defragRequired: true, wasModified: true);
+                if (decompressed.NumberOfEntries > 0) // no need to copy empty page because it was deleted
+                    decompressed.CopyToOriginal(_llt, defragRequired: true, wasModified: true, this);
             }
         }
 

src/Voron/Data/Compression/DecompressedLeafPage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Sparrow;
 using Voron.Data.BTrees;
 using Voron.Global;
 using Voron.Impl;
@@ -36,13 +37,13 @@ namespace Voron.Data.Compression
 
             if (_disposed)
                 return;
-            
+
             _tempPage.ReturnTemporaryPageToPool.Dispose();
 
             _disposed = true;
         }
 
-        public void CopyToOriginal(LowLevelTransaction tx, bool defragRequired, bool wasModified)
+        public void CopyToOriginal(LowLevelTransaction tx, bool defragRequired, bool wasModified, Tree tree)
         {
             if (CalcSizeUsed() < Original.PageMaxSpace)
             {
@@ -67,17 +68,62 @@ namespace Voron.Data.Compression
                         if (wasModified == false)
                             return;
 
-                        ThrowCouldNotCompressDecompressedPage(PageNumber);
-                    } 
+                        // we aren't able to compress the page back to 8KB page
+                        // let's split it and try to copy it then
+
+                        SplitPage(tx, tree);
+
+                        CopyToOriginal(tx, defragRequired: true, wasModified: true, tree);
+
+                        return;
+                    }
 
                     LeafPageCompressor.CopyToPage(compressed, Original);
                 }
             }
         }
 
-        private static void ThrowCouldNotCompressDecompressedPage(long pageNumber)
+        private void SplitPage(LowLevelTransaction tx, Tree tree)
         {
-            throw new InvalidOperationException($"Decompressed page #{pageNumber} could not be compressed back. Should never happen");
+            // let's take a node from the middle and add it again with the page splitting
+            // this way we'll copy half of the page to a new page
+
+            var middleNodeIndex = NumberOfEntries / 2;
+
+            using (GetNodeKey(tx, middleNodeIndex, out var middleNodeKey))
+            {
+                tree.FindPageFor(middleNodeKey, node: out _, cursor: out var cursorConstructor, allowCompressed: true);
+
+                // let's copy key and data of a node that we'll remove
+
+                var key = middleNodeKey.Clone(tx.Allocator);
+
+                var node = GetNode(middleNodeIndex);
+
+                var flags = node->Flags;
+                var valueReader = tree.GetValueReaderFromHeader(node);
+
+                using (tx.Allocator.Allocate(valueReader.Length, out var tempValueOutput))
+                {
+                    Memory.Copy(tempValueOutput.Ptr, valueReader.Base, valueReader.Length);
+
+                    RemoveNode(middleNodeIndex);
+
+                    Search(tx, key);
+
+                    using (var cursor = cursorConstructor.Build(key))
+                    {
+                        cursor.Update(cursor.Pages, this); // we need to use uncompressed page here because it might have some modifications (e.g. deleted node)
+
+                        var pageSplitter = new TreePageSplitter(tx, tree, key, valueReader.Length, PageNumber, flags, cursor,
+                            splittingOnDecompressed: true);
+
+                        var pos = pageSplitter.Execute();
+
+                        tempValueOutput.CopyTo(pos);
+                    }
+                }
+            }
         }
     }
 }

src/Voron/Data/BTrees/TreePageSplitter.cs
@@ -12,6 +12,7 @@ namespace Voron.Data.BTrees
     public unsafe class TreePageSplitter
     {
         private readonly TreeCursor _cursor;
+        private readonly bool _splittingOnDecompressed;
         private readonly int _len;
         private readonly Slice _newKey;
         private readonly TreeNodeFlags _nodeType;
@@ -28,7 +29,8 @@ namespace Voron.Data.BTrees
             int len,
             long pageNumber,
             TreeNodeFlags nodeType,
-            TreeCursor cursor)
+            TreeCursor cursor,
+            bool splittingOnDecompressed = false)
         {
             _tx = tx;
             _tree = tree;
@@ -37,8 +39,17 @@ namespace Voron.Data.BTrees
             _pageNumber = pageNumber;
             _nodeType = nodeType;
             _cursor = cursor;
+            _splittingOnDecompressed = splittingOnDecompressed;
             TreePage page = _cursor.Pages.Peek();
-            _page = _tree.ModifyPage(page);
+
+            if (_splittingOnDecompressed == false)
+                _page = _tree.ModifyPage(page);
+            else
+            {
+                Debug.Assert(page is DecompressedLeafPage);
+                _page = page;
+            }
+
             _cursor.Pop();
         }
 
@@ -116,7 +127,7 @@ namespace Voron.Data.BTrees
         {
             if (_pageDecompressed == null)
                 return;
-            _pageDecompressed.CopyToOriginal(_tx, defragRequired: false, wasModified: wasModified);
+            _pageDecompressed.CopyToOriginal(_tx, defragRequired: false, wasModified: wasModified, _tree);
             _tree.DecompressionsCache.Invalidate(_pageDecompressed.PageNumber, DecompressionUsage.Read);
             _page = _pageDecompressed.Original;
         }
@@ -182,7 +193,7 @@ namespace Voron.Data.BTrees
                 case TreeNodeFlags.MultiValuePageRef:
                     return page.AddMultiValueNode(index, newKeyToInsert, _len);
                 default:
-                    throw new NotSupportedException("Unknown node type");
+                    throw new NotSupportedException($"Unknown node type: {_nodeType}");
             }
         }
 
@@ -212,9 +223,24 @@ namespace Voron.Data.BTrees
                 }
             }
 
+            DecompressedLeafPage rightDecompressed = null;
+            int? decompressedPageSize = null;
+
+            if (_pageDecompressed != null)
+                decompressedPageSize = _pageDecompressed.PageSize;
+            else if (_splittingOnDecompressed)
+                decompressedPageSize = _page.PageSize;
+
+            if (decompressedPageSize != null)
+            {
+                // splitting the decompressed page, let's allocate the page of the same size to ensure enough space
+                rightDecompressed = _tx.Environment.DecompressionBuffers.GetPage(_tx, decompressedPageSize.Value, DecompressionUsage.Write, rightPage);
+                rightPage = rightDecompressed;
+            }
+
             if (_page.IsLeaf)
             {
-                splitIndex = AdjustSplitPosition(currentIndex, splitIndex, ref toRight);
+                splitIndex = AdjustSplitPosition(currentIndex, splitIndex, rightPage, ref toRight);
             }
 
             Slice currentKey;
@@ -233,16 +259,7 @@ namespace Voron.Data.BTrees
                 var addedAsImplicitRef = false;
                 var parentOfPage = _cursor.CurrentPage;
                 TreePage parentOfRight;
-
-                DecompressedLeafPage rightDecompressed = null;
-
-                if (_pageDecompressed != null)
-                {
-                    // splitting the decompressed page, let's allocate the page of the same size to ensure enough space
-                    rightDecompressed = _tx.Environment.DecompressionBuffers.GetPage(_tx, _pageDecompressed.PageSize, DecompressionUsage.Write, rightPage);
-                    rightPage = rightDecompressed;
-                }
-
+                
                 using (rightDecompressed)
                 {
                     AddSeparatorToParentPage(rightPage.PageNumber, seperatorKey, out parentOfRight);
@@ -275,7 +292,7 @@ namespace Voron.Data.BTrees
 
                     if (rightDecompressed != null)
                     {
-                        rightDecompressed.CopyToOriginal(_tx, defragRequired: false, wasModified: true);
+                        rightDecompressed.CopyToOriginal(_tx, defragRequired: false, wasModified: true, _tree);
                         rightPage = rightDecompressed.Original;
                     }
                 }
@@ -395,7 +412,7 @@ namespace Voron.Data.BTrees
             return pos;
         }
 
-        private int AdjustSplitPosition(int currentIndex, int splitIndex, ref bool toRight)
+        private int AdjustSplitPosition(int currentIndex, int splitIndex, TreePage rightPage, ref bool toRight)
         {
             Slice keyToInsert = _newKey;
 
@@ -427,7 +444,7 @@ namespace Voron.Data.BTrees
                     TreeNodeHeader* node = _page.GetNode(i);
                     pageSize += node->GetNodeSize();
                     pageSize += pageSize & 1;
-                    if (pageSize > _page.PageMaxSpace)
+                    if (pageSize > rightPage.PageMaxSpace)
                     {
                         if (i >= currentIndex)
                         {

test/SlowTests/SlowTests.csproj
@@ -130,5 +130,8 @@
     <None Update="SchemaUpgrade\Issues\VoronCurrentVersion\schema_999\Raven.voron">
       <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
     </None>
+    <None Update="Voron\LeafsCompression\Data\RavenDB-12700-page-1278-compressed">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </None>
   </ItemGroup>
 </Project>

test/SlowTests/Voron/LeafsCompression/RavenDB_12700_Voron.cs
@@ -1,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using FastTests.Voron;
+using Raven.Client.Extensions.Streams;
+using Raven.Server.Config.Settings;
+using Sparrow;
+using Voron;
+using Voron.Data.BTrees;
+using Xunit;
+
+namespace SlowTests.Voron.LeafsCompression
+{
+    public class RavenDB_12700_Voron : StorageTest
+    {
+        [Fact]
+        public unsafe void Must_split_compressed_page_if_cannot_compress_back_after_decompression()
+        {
+            using (var tx = Env.WriteTransaction())
+            {
+                var tree = tx.CreateTree("tree", flags: TreeFlags.LeafsCompressed);
+
+                Page modifyPage = tx.LowLevelTransaction.ModifyPage(tree.State.RootPageNumber);
+
+                // it's a special page which after decompression could not be compressed back
+                // it contains 2 uncompressed nodes and 201 nodes in compressed part
+                // the issue was that after decompression the layout of data on the page was different
+                // and we couldn't compress it back to 8KB page
+
+                // let's copy the entire page content but we need to change the page number
+                var compressedPagePath = new PathSetting("Voron/LeafsCompression/Data/RavenDB-12700-page-1278-compressed");
+
+                byte[] data;
+
+                using (var file = File.Open(compressedPagePath.FullPath, FileMode.Open))
+                {
+                    data = file.ReadData();
+
+                    Assert.Equal(file.Length, data.Length);
+                }
+
+                fixed (byte* dataPtr = data)
+                {
+                    Memory.Copy(modifyPage.Pointer, dataPtr, data.Length);
+                }
+
+                modifyPage.PageNumber = tree.State.RootPageNumber; // set the original page number
+
+                var readValues = new List<(Slice, byte[])>();
+
+                Slice? idToDelete = null;
+
+                using (var it = tree.Iterate(false))
+                {
+                    Assert.True(it.Seek(Slices.BeforeAllKeys));
+
+                    do
+                    {
+                        var key = it.CurrentKey.Clone(tx.Allocator);
+
+                        var reader = it.CreateReaderForCurrent();
+
+                        var bytes = reader.ReadBytes(reader.Length).ToArray();
+
+                        if (idToDelete == null)
+                        {
+                            idToDelete = key;
+                            continue;
+                        }
+
+                        readValues.Add((key, bytes));
+                    } while (it.MoveNext());
+                }
+                
+                // this deletion will cause page decompression in order to remove one entry
+                // under the cover we'll do the page split because we aren't able to compress the page back
+                tree.Delete(idToDelete.Value);
+
+                // let's check that after page split we can still read values
+
+                foreach (var item in readValues)
+                {
+                    using (var result = tree.ReadDecompressed(item.Item1))
+                    {
+                        Assert.Equal(item.Item2, result.Reader.AsStream().ReadData());
+                    }
+                }
+            }
+        }
+    }
+}

commit 703ba5aedfc6a4102ad1c7d3f4c753857d87c2ba

RavenDB-12850 / RavenDB-12700 Fixing map-reduce issue related to compressed leafs introduced in https://github.com/ravendb/ravendb/commit/66d3eea6115481f8d40b579eae8df75e822ef5e1#diff-a5f076f0b9091a74c93f23cdaffa963cR300

src/Voron/Data/BTrees/Tree.Compressed.cs
@@ -297,8 +297,7 @@ namespace Voron.Data.BTrees
             }
             finally
             {
-                if (decompressed.NumberOfEntries > 0) // no need to copy empty page because it was deleted
-                    decompressed.CopyToOriginal(_llt, defragRequired: true, wasModified: true, this);
+                decompressed.CopyToOriginal(_llt, defragRequired: true, wasModified: true, this);
             }
         }
 

src/Voron/Data/Compression/DecompressedLeafPage.cs
@@ -68,10 +68,17 @@ namespace Voron.Data.Compression
                         if (wasModified == false)
                             return;
 
-                        // we aren't able to compress the page back to 8KB page
-                        // let's split it and try to copy it then
+                        if (NumberOfEntries > 0)
+                        {
+                            // we aren't able to compress the page back to 8KB page
+                            // let's split it and try to copy it then
 
-                        SplitPage(tx, tree);
+                            SplitPage(tx, tree);
+                        }
+                        else
+                        {
+                            ThrowCouldNotCompressEmptyDecompressedPage(PageNumber);
+                        }
 
                         CopyToOriginal(tx, defragRequired: true, wasModified: true, tree);
 
@@ -124,6 +131,11 @@ namespace Voron.Data.Compression
                     }
                 }
             }
+        }
+
+        private static void ThrowCouldNotCompressEmptyDecompressedPage(long pageNumber)
+        {
+            throw new InvalidOperationException($"Empty decompressed page #{pageNumber} could not be compressed back. Should never happen");
         }
     }
 }