d698deed0324cb255c46a58ef199278404a2791a
Pawel Pekrol <ppekrol@ravendb.net>
2019-02-20 09:15:26 +0200

    RavenDB-12879 fixing whitespace

--- test/FastTests/Voron/Trees/MultipleTrees.cs
+++ test/FastTests/Voron/Trees/MultipleTrees.cs
@@ -4,73 +4,73 @@ using Xunit;
 namespace FastTests.Voron.Trees
 {
     public class MultipleTrees : StorageTest
-	{
-		[Fact]
-		public void CanCreateNewTree()
-		{
-			using (var tx = Env.WriteTransaction())
-			{
-				tx.CreateTree("test");
+    {
+        [Fact]
+        public void CanCreateNewTree()
+        {
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.CreateTree("test");
 
-				tx.CreateTree("test").Add("test", StreamFor("abc"));
+                tx.CreateTree("test").Add("test", StreamFor("abc"));
 
-				tx.Commit();
-			}
+                tx.Commit();
+            }
 
-			using (var tx = Env.ReadTransaction())
-			{
-				var stream = tx.ReadTree("test").Read("test");
-				Assert.NotNull(stream);
+            using (var tx = Env.ReadTransaction())
+            {
+                var stream = tx.ReadTree("test").Read("test");
+                Assert.NotNull(stream);
 
-				tx.Commit();
-			}
-		}
+                tx.Commit();
+            }
+        }
 
-		[Fact]
-		public void CanUpdateValuesInSubTree()
-		{
-			using (var tx = Env.WriteTransaction())
-			{
-				tx.CreateTree("test");
+        [Fact]
+        public void CanUpdateValuesInSubTree()
+        {
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.CreateTree("test");
 
-				tx.CreateTree("test").Add("test", StreamFor("abc"));
+                tx.CreateTree("test").Add("test", StreamFor("abc"));
 
-				tx.Commit();
-			}
+                tx.Commit();
+            }
 
-			using (var tx = Env.WriteTransaction())
-			{
+            using (var tx = Env.WriteTransaction())
+            {
 
-				tx.CreateTree("test").Add("test2", StreamFor("abc"));
+                tx.CreateTree("test").Add("test2", StreamFor("abc"));
 
-				tx.Commit();
-			}
+                tx.Commit();
+            }
 
-			using (var tx = Env.ReadTransaction())
-			{
-				var stream = tx.CreateTree("test").Read("test2");
-				Assert.NotNull(stream);
+            using (var tx = Env.ReadTransaction())
+            {
+                var stream = tx.CreateTree("test").Read("test2");
+                Assert.NotNull(stream);
 
-				tx.Commit();
-			}
-		}
+                tx.Commit();
+            }
+        }
 
-		[Fact]
-		public void CreatingTreeWithoutCommitingTransactionShouldYieldNoResults()
-		{
-			using (var tx = Env.WriteTransaction())
-			{
-				tx.CreateTree("test");
-			}
+        [Fact]
+        public void CreatingTreeWithoutCommitingTransactionShouldYieldNoResults()
+        {
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.CreateTree("test");
+            }
 
-			var e = Assert.Throws<InvalidOperationException>(() =>
-			    {
-			        using (var tx = Env.ReadTransaction())
-			        {
-			            tx.CreateTree("test");
-			        }
-			    });
-			Assert.Contains("No such tree: 'test'", e.Message);
-		}
-	}
+            var e = Assert.Throws<InvalidOperationException>(() =>
+                {
+                    using (var tx = Env.ReadTransaction())
+                    {
+                        tx.CreateTree("test");
+                    }
+                });
+            Assert.Contains("No such tree: 'test'", e.Message);
+        }
+    }
 }
