6f3226cb295518a2c7c05d713357b673e913c0c9
ml054 <marcin@ravendb.net>
2019-02-22 15:43:42 +0100

    RavenDB-12903 Add sample etl script with counters / document deletion

--- src/Raven.Studio/typescript/viewmodels/database/tasks/transformationScriptSyntax.ts
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/transformationScriptSyntax.ts
@@ -3,7 +3,7 @@ import dialogViewModelBase = require("viewmodels/dialogViewModelBase");
 class transformationScriptSyntax extends dialogViewModelBase {
     
     etlType = ko.observable<Raven.Client.Documents.Operations.ETL.EtlType>();
-    static readonly sampleScript = `var orderData = {
+    static readonly sampleScriptForSql = `var orderData = {
     Id: id(this),
     OrderLinesCount: this.Lines.length,
     TotalCost: 0
@@ -26,8 +26,8 @@ orderData.TotalCost = Math.round(orderData.TotalCost  * 100) / 100;
 // Load to SQL table 'Orders'
 loadToOrders(orderData);`;
 
-    scriptHtml = ko.pureComputed(() => {
-        return Prism.highlight(transformationScriptSyntax.sampleScript, (Prism.languages as any).javascript);
+    sqlScriptHtml = ko.pureComputed(() => {
+        return Prism.highlight(transformationScriptSyntax.sampleScriptForSql, (Prism.languages as any).javascript);
     });
 
     constructor(etlType: Raven.Client.Documents.Operations.ETL.EtlType) {        
