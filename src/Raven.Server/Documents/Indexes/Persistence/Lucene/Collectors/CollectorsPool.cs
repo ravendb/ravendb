0dd5f4d5d4379d53a4c652b8dce8d6e36e872202
Federico Andres Lois <federico.lois@corvalius.com>
2019-02-26 16:55:44 -0300

    RavenDB-12959: Pool collectors lists

---
+++ src/Raven.Server/Documents/Indexes/Persistence/Lucene/Collectors/CollectorsPool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lucene.Net.Search;
+using Sparrow;
+
+namespace Raven.Server.Documents.Indexes.Persistence.Lucene.Collectors
+{
+    internal static class CollectorsPool
+    {
+        public static ObjectPool<List<ScoreDoc>> Instance = new ObjectPool<List<ScoreDoc>>(() => new List<ScoreDoc>());
+    }
+}
