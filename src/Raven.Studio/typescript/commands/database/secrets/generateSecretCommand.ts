6c4d1faf8f345f9471e755ca3034ffe4c75d93b0
ml054 <marcin@ravendb.net>
2019-02-26 14:52:37 +0100

    RavenDB-12811 Review menu actions privileges before 4.2 release. - fix export database with encryption - user nonadmin endpoint for generate export key

--- src/Raven.Studio/typescript/commands/database/secrets/generateSecretCommand.ts
+++ src/Raven.Studio/typescript/commands/database/secrets/generateSecretCommand.ts
@@ -4,7 +4,7 @@ import endpoints = require("endpoints");
 class generateSecretCommand extends commandBase {
 
     execute(): JQueryPromise<string> {
-        const url = endpoints.global.secretKey.adminSecretsGenerate;
+        const url = endpoints.global.secretKey.secretsGenerate;
 
         return this.query<string>(url, null, null, null, { dataType: 'text' })
             .fail((response: JQueryXHR) => this.reportError("Failed to generate secrets", response.responseText, response.statusText));
