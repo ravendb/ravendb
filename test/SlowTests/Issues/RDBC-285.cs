3a08afcee9e5e784e7cf11d43265fa3e5d633fe5
Ayende Rahien <Ayende@ayende.com>
2019-02-26 15:51:58 +0200

    RDBC-285 Add test for zero boosting

---
+++ test/SlowTests/Issues/RDBC-285.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FastTests;
+using Orders;
+using Raven.Client.Documents;
+using Tests.Infrastructure;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RDBC_285 : RavenTestBase
+    {
+        [Fact]
+        public void CanQueryWithBoostEqZero()
+        {
+            using (var store = GetDocumentStore())
+            {
+                store.Maintenance.Send(new CreateSampleDataOperation());
+
+                using (var session = store.OpenSession())
+                {
+                    var q= session.Advanced.DocumentQuery<Product>("Product/Search")
+                        .Search(x => x.Name, "coffee syrup")
+                        .OrElse()
+                        .WhereIn(x => x.Category, new[] { "categories/2-A", "categories/3-A" }).Boost(0)
+                        .Take(3)
+                        .OrderByScore()
+                        .OrderByDescending(x => x.PricePerUnit);
+
+                    var resultsZeroBoost = session.Advanced.DocumentQuery<Product>("Product/Search")
+                        .Search(x => x.Name, "coffee syrup")
+                        .OrElse()
+                        .WhereIn(x => x.Category, new[] { "categories/2-A", "categories/3-A" }).Boost(0)
+                        .Take(3)
+                        .OrderByScore()
+                        .OrderByDescending(x=>x.PricePerUnit)
+                        .ToList();
+
+                    var results = session.Advanced.DocumentQuery<Product>("Product/Search")
+                      .Search(x => x.Name, "coffee syrup")
+                      .OrElse()
+                      .WhereIn(x => x.Category, new[] { "categories/2-A", "categories/3-A" })
+                      .Take(3)
+                      .OrderByScore()
+                      .OrderByDescending(x => x.PricePerUnit)
+                      .ToList();
+
+                    Assert.Equal(results.Count, resultsZeroBoost.Count);
+                    Assert.NotEqual(results, resultsZeroBoost);
+                    Assert.Equal("from index 'Product/Search' where search(Name, $p0) or boost(Category in ($p1), 0) order by score(), PricePerUnit as double desc", q.ToString());
+                }
+            }
+        }
+    }
+}
