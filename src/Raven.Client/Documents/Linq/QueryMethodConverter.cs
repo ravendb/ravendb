a3fcff0f3312dbe1cd6ae901c571d9b081c7c986
Pawel Pekrol <ppekrol@ravendb.net>
2019-02-28 15:34:07 +0200

    RavenDB-12943 added QueryMethodConverter

---
+++ src/Raven.Client/Documents/Linq/QueryMethodConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+using Raven.Client.Documents.Session;
+
+namespace Raven.Client.Documents.Linq
+{
+    public abstract class QueryMethodConverter
+    {
+        public struct Parameters<T>
+        {
+            internal Parameters(MethodCallExpression expression, IAbstractDocumentQuery<T> documentQuery, Action<Expression> visitExpression)
+            {
+                Expression = expression;
+                DocumentQuery = documentQuery;
+                VisitExpression = visitExpression;
+            }
+
+            public MethodCallExpression Expression { get; }
+
+            public IAbstractDocumentQuery<T> DocumentQuery { get; }
+
+            public Action<Expression> VisitExpression { get; }
+        }
+
+        public abstract bool Convert<T>(Parameters<T> parameters);
+    }
+}
---
+++ test/SlowTests/Issues/RavenDB_12943.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FastTests;
+using Orders;
+using Raven.Client.Documents.Linq;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_12943 : RavenTestBase
+    {
+        [Fact]
+        public void CanUseQueryMethodConverter()
+        {
+            using (var store = GetDocumentStore(new Options
+            {
+                ModifyDocumentStore = s => s.Conventions.RegisterQueryMethodConverter(new MyQueryMethodConverter()) 
+            }))
+            {
+                using (var session = store.OpenSession())
+                {
+                    var q = session.Query<Company>()
+                        .Where(x => x.Address.City == "Warsaw")
+                        .ToString();
+
+                    Assert.Equal("from Companies where Test between $p0 and $p1 and Test between $p2 and $p3 and exists(Test2)", q);
+                }
+            }
+        }
+
+        private class MyQueryMethodConverter : QueryMethodConverter
+        {
+            private bool _isVisiting;
+
+            public override bool Convert<T>(Parameters<T> parameters)
+            {
+                parameters.DocumentQuery.WhereBetween("Test", 7, 10);
+
+                if (_isVisiting == false)
+                {
+                    _isVisiting = true;
+                    try
+                    {
+                        parameters.VisitExpression(parameters.Expression);
+                    }
+                    finally
+                    {
+                        _isVisiting = false;
+                    }
+                }
+                else
+                {
+                    parameters.DocumentQuery.WhereExists("Test2");
+                }
+
+                return true;
+            }
+        }
+    }
+}
