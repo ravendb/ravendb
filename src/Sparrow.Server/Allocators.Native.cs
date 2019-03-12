1c263d982b25320b4067bd05f5e5c82c3afe4056
Federico Andres Lois <federico.lois@corvalius.com>
2019-03-12 14:02:13 -0300

    [RavenDB-8951] Implementation of composable allocators.
    * NativeAllocator to request immediately to the OS
    * Allocators, BlockAllocators and FixedSizeAllocators base types.
    * FragmentAllocator as a composable block for high-frequency small allocations.
    * PoolAllocator as a composable block for retaining memory chunks
    * ArenaAllocator as a composable block for heavily patterned allocations
    * ThreadAffinePoolAllocation to keep the memory on the same thread/processor.
    * NonBlockingPoolAllocator to support highly threaded code

---
+++ src/Sparrow.Server/Allocators.Native.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using Sparrow.Platform;
+using Sparrow.Server.Platform;
+using Sparrow.Utils;
+
+namespace Sparrow.Server
+{
+    public interface INativeOptions : IAllocatorOptions
+    {
+        bool UseSecureMemory { get; }
+        bool ElectricFenceEnabled { get; }
+        bool Zeroed { get; }
+    }
+
+    public static class NativeAllocator
+    {
+        public struct Default : INativeOptions
+        {
+            public bool UseSecureMemory => false;
+            public bool ElectricFenceEnabled => false;
+            public bool Zeroed => false;
+        }
+
+        public struct DefaultZero : INativeOptions
+        {
+            public bool UseSecureMemory => false;
+            public bool ElectricFenceEnabled => false;
+            public bool Zeroed => true;
+        }
+
+        public struct Secure : INativeOptions
+        {
+            public bool UseSecureMemory => true;
+            public bool ElectricFenceEnabled => false;
+            public bool Zeroed => false;
+        }
+
+        public struct ElectricFence : INativeOptions
+        {
+            public bool UseSecureMemory => false;
+            public bool ElectricFenceEnabled => true;
+            public bool Zeroed => false;
+        }
+    }
+
+    /// <summary>
+    /// The NativeAllocator is the barebones allocator, it will redirect the request straight to the OS system calls.
+    /// It will not keep track of allocations (except when running in validation mode), that means that
+    /// this allocator can leak if used improperly. This allocator is thread-safe.
+    /// </summary>
+    /// <typeparam name="TOptions">The options to use for the allocator.</typeparam>
+    /// <remarks>The Options object must be properly implemented to achieve performance improvements. (use constants as much as you can)</remarks>
+    public unsafe struct NativeAllocator<TOptions> : IAllocator<NativeAllocator<TOptions>, Pointer>, IAllocator
+        where TOptions : struct, INativeOptions
+    {
+        private TOptions _options;
+        private long _totalAllocated;
+        private long _allocated;
+
+#if VALIDATE || DEBUG
+        private object _initializeStackTrace;
+#endif
+
+        public void Configure<TConfig>(ref NativeAllocator<TOptions> allocator, ref TConfig configuration) where TConfig : struct, IAllocatorOptions
+        {
+            if (!typeof(TOptions).GetTypeInfo().IsAssignableFrom(typeof(TConfig)))
+                throw new NotSupportedException($"{nameof(TConfig)} is not compatible with {nameof(TOptions)}");
+
+            // This cast will get evicted by the JIT. 
+            allocator._options = (TOptions)(object)configuration;
+
+            if (((TOptions)(object)configuration).ElectricFenceEnabled && ((TOptions)(object)configuration).UseSecureMemory)
+                throw new NotSupportedException($"{nameof(TConfig)} is asking for secure, electric fenced memory. The combination is not supported.");
+        }
+
+        public long TotalAllocated
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _totalAllocated;
+        }
+
+        public long Allocated
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _allocated;
+        }
+
+        public long InUse
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _allocated;
+        }
+
+        public bool IsThreadSafe => true;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Initialize(ref NativeAllocator<TOptions> allocator)
+        {
+            allocator._totalAllocated = 0;
+
+#if VALIDATE || DEBUG
+            // https://github.com/benaadams/Ben.Demystifier/issues/75
+            allocator._initializeStackTrace = EnhancedStackTrace.Current();
+            // allocator._initializeStackTrace = "currently missing because of bug";
+#endif
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Pointer Allocate(ref NativeAllocator<TOptions> allocator, int size)
+        {
+            byte* memory;
+
+            // PERF: Given that for the normal use case the INativeOptions we will use returns constants the
+            //       JIT will be able to fold all this if sequence into a branchless single call.
+            if (allocator._options.ElectricFenceEnabled)
+                memory = ElectricFencedMemory.Allocate(size);
+            else if (allocator._options.UseSecureMemory)
+                throw new NotImplementedException();
+            else
+                memory = NativeMemory.AllocateMemory(size);
+
+            if (allocator._options.Zeroed)
+                Memory.Set(memory, 0, size);
+
+            Interlocked.Add(ref allocator._allocated, size);
+            Interlocked.Add(ref allocator._totalAllocated, size);
+
+            return new Pointer(memory, size);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Release(ref NativeAllocator<TOptions> allocator, ref Pointer ptr)
+        {
+            Interlocked.Add(ref allocator._allocated, -ptr.Size);
+
+            // PERF: Given that for the normal use case the INativeOptions we will use returns constants the
+            //       JIT will be able to fold all this if sequence into a branchless single call.
+            if (allocator._options.ElectricFenceEnabled)
+                ElectricFencedMemory.Free((byte*)ptr.Address);
+            else if (allocator._options.UseSecureMemory)
+                throw new NotImplementedException();
+            else
+                NativeMemory.Free((byte*)ptr.Address, ptr.Size);
+
+            ptr = new Pointer();
+        }
+
+        public void Reset(ref NativeAllocator<TOptions> allocator)
+        {
+            // There is no reset action to do on this allocator. We wont fail, but nothing we can do to 'reclaim' or 'reuse' memory.            
+            CheckForLeaks(ref allocator);
+        }
+
+        public void OnAllocate(ref NativeAllocator<TOptions> allocator, Pointer ptr)
+        {
+            // This allocator does not keep track of anything.
+        }
+
+        public void OnRelease(ref NativeAllocator<TOptions> allocator, Pointer ptr)
+        {
+            // This allocator does not keep track of anything.
+        }
+
+        public void Dispose(ref NativeAllocator<TOptions> allocator, bool disposing)
+        {
+            // If we use the leak detector when finalizing an object, the dotnet process will explode
+            // with an unhandled exception. Therefore, we cannot check for leaks and throw in those cases. 
+            if (disposing)
+                CheckForLeaks(ref allocator);
+        }
+
+        [Conditional("DEBUG")]
+        [Conditional("VALIDATE")]
+        private void CheckForLeaks(ref NativeAllocator<TOptions> allocator)
+        {
+            if (allocator.Allocated != 0)
+            {
+#if VALIDATE || DEBUG
+                var stackFrameLines = allocator._initializeStackTrace.ToString().Split(Environment.NewLine.ToCharArray(), StringSplitOptions.None);
+                string stackFrame = $"{Environment.NewLine}Construction Stack Trace: {Environment.NewLine}{stackFrameLines.Take(3)}{Environment.NewLine}End Construction Stack Trace";                
+#else
+                string stackFrame = string.Empty;
+#endif                
+                throw new NotSupportedException ($"The allocator is leaking '{allocator.Allocated}' bytes of memory. {stackFrame}");
+            }
+        }
+    }
+}
---
+++ src/Sparrow.Server/Allocators.PointerTypes.cs
@@ -0,0 +1,533 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Sparrow.Server
+{
+    public interface IPointerType { }
+    public interface IPointerType<T> : IPointerType where T : struct { }
+
+    [StructLayout(LayoutKind.Sequential)]
+#if !VALIDATE
+    // We do this to have the ability to perform extensive validation for pointers.
+    readonly
+#endif
+    public unsafe struct Pointer : IPointerType, Meta.IDescribe
+    {
+        public readonly void* Address;
+
+        public readonly int Size;
+
+#if VALIDATE
+        public uint Generation;        
+#endif
+
+        public Pointer(void* ptr, int size)
+        {
+            this.Address = ptr;
+            this.Size = size;
+#if VALIDATE
+            this.Generation = 0;
+#endif
+        }
+
+        public bool IsValid
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Address != null;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<byte> AsSpan()
+        {
+            return new Span<byte>(Address, (int)Size);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<byte> AsSpan(int length)
+        {
+            if (length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new Span<byte>(Address, length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<byte> AsSpan(int start, int length)
+        {
+            if (start + length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new Span<byte>((byte*)Address + start, length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySpan<byte> AsReadOnlySpan()
+        {
+            return new ReadOnlySpan<byte>(Address, (int)Size);
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySpan<byte> AsReadOnlySpan(int length)
+        {
+            if (length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new ReadOnlySpan<byte>(Address, length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySpan<byte> AsReadOnlySpan(int start, int length)
+        {
+            if (start + length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new ReadOnlySpan<byte>((byte*)Address + start, length);
+        }
+
+        public static implicit operator Pointer(Pointer<byte> ptr)
+        {
+            return new Pointer(ptr.Address, ptr.SizeAsBytes);
+        }
+
+        public string Describe()
+        {
+            if (this.IsValid)
+                return $"{{{(long)this.Address:X32}|{this.Size}}}";
+
+            return "{null}";
+        }
+
+        public override string ToString()
+        {
+            return Describe();
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+#if !VALIDATE
+    // We do this to have the ability to perform extensive validation for pointers.
+    readonly
+#endif
+    public unsafe struct Pointer<T> : Meta.IDescribe, IPointerType<T> where T : struct
+    {
+        public readonly void* Address;
+
+        private readonly int _size;
+
+#if VALIDATE
+        public uint Generation;        
+#endif
+
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        static Pointer()
+        {
+            // If T is not a blittable type, fail on type loading unrecoverably.
+            // We apply the NoOptimization attribute because we need to ensure the JIT wont optimize away
+            // the call to Marshal.SizeOf<T> for correctness either now or in the future. 
+            Marshal.SizeOf<T>();
+        }
+
+        public Pointer(Pointer ptr)
+        {
+            this.Address = ptr.Address;
+            this._size = ptr.Size / Unsafe.SizeOf<T>();
+
+#if VALIDATE
+            this.Generation = ptr.Generation;
+#endif
+        }
+
+        public Pointer(void* ptr, int size)
+        {
+            this.Address = ptr;
+            this._size = size;
+
+#if VALIDATE
+            this.Generation = 0;
+#endif
+        }
+
+        public bool IsValid
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Address != null;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<T> AsSpan()
+        {
+            return new Span<T>(Address, (int)Size);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<T> AsSpan(int length)
+        {
+            if (length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new Span<T>(Address, length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<T> AsSpan(int start, int length)
+        {
+            if (start + length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new Span<T>((byte*)Address + start * Unsafe.SizeOf<T>(), length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySpan<T> AsReadOnlySpan()
+        {
+            return new ReadOnlySpan<T>(Address, (int)Size);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySpan<T> AsReadOnlySpan(int length)
+        {
+            if (length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new ReadOnlySpan<T>(Address, length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySpan<T> AsReadOnlySpan(int start, int length)
+        {
+            if (start + length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new ReadOnlySpan<T>((byte*)Address + start * Unsafe.SizeOf<T>(), length);
+        }
+
+        public int Size
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                EnsureIsNotBadPointerAccess();
+                return _size;
+            }
+        }
+
+        public int SizeAsBytes
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                EnsureIsNotBadPointerAccess();
+                return _size * Unsafe.SizeOf<T>();
+            }
+        }
+
+        public string Describe()
+        {
+            if (this.IsValid)
+                return $"{{{(long)this.Address:X32}|{this.Size}|{this.SizeAsBytes}b}}";
+
+            return "{null}";
+        }
+
+        public override string ToString()
+        {
+            return Describe();
+        }
+
+        [Conditional("DEBUG")]
+        [Conditional("VALIDATE")]
+        internal void EnsureIsNotBadPointerAccess()
+        {
+            if (Address == null)
+                throw new InvalidOperationException($"Trying to access the pointer but it is not valid");
+        }
+
+        public static implicit operator Pointer(Pointer<T> ptr)
+        {
+            return new Pointer(ptr.Address, ptr.SizeAsBytes);
+        }
+
+        public static explicit operator Pointer<T>(Pointer ptr)
+        {
+            if (ptr.Size % Unsafe.SizeOf<T>() != 0)
+                throw new InvalidCastException($"Pointer of size {ptr.Size} is not a multiple of {nameof(T)} size.");
+
+            return new Pointer<T>(ptr.Address, ptr.Size / Unsafe.SizeOf<T>());
+        }
+    }
+
+
+    /// <summary>
+    /// The BlockPointer is a type of pointer that carries with it 2 different sizes at the same time. The pointer value
+    /// with the proper memory size allocated to it AND the actual requested size when asked. A BlockPointer created from
+    /// a straight Pointer will have the same size for the allocation size and the actual size. 
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+#if !VALIDATE
+    // We do this to have the ability to perform extensive validation for pointers.
+    readonly
+#endif
+    public unsafe struct BlockPointer : IPointerType, Meta.IDescribe
+    {
+        public readonly void* Address;
+
+        public readonly int Size;
+
+        public readonly int BlockSizeAsBytes;
+
+#if VALIDATE
+        public uint Generation;        
+#endif
+
+        public BlockPointer(void* ptr, int blockSizeAsBytes, int size)
+        {
+            this.Address = ptr;
+            this.Size = size;
+            this.BlockSizeAsBytes = blockSizeAsBytes;
+
+#if VALIDATE
+            this.Generation = 0;
+#endif
+        }
+
+        public bool IsValid
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Address != null;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<byte> AsSpan()
+        {
+            return new Span<byte>(Address, (int)Size);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<byte> AsSpan(int length)
+        {
+            if (length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new Span<byte>(Address, length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<byte> AsSpan(int start, int length)
+        {
+            if (start + length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new Span<byte>((byte*)Address + start, length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySpan<byte> AsReadOnlySpan()
+        {
+            return new ReadOnlySpan<byte>(Address, (int)Size);
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySpan<byte> AsReadOnlySpan(int length)
+        {
+            if (length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new ReadOnlySpan<byte>(Address, length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySpan<byte> AsReadOnlySpan(int start, int length)
+        {
+            if (start + length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new ReadOnlySpan<byte>((byte*)Address + start, length);
+        }
+
+        public string Describe()
+        {
+            if (this.IsValid)
+                return $"{{{(long)this.Address:X32}|{this.Size}|{this.BlockSizeAsBytes}}}";
+
+            return "{null}";
+        }
+
+        public override string ToString()
+        {
+            return Describe();
+        }
+
+        public static implicit operator BlockPointer(Pointer ptr)
+        {
+            return new BlockPointer(ptr.Address, ptr.Size, ptr.Size);
+        }
+    }
+
+    /// <summary>
+    /// The BlockPointer is a type of pointer that carries with it 2 different sizes at the same time. The pointer value
+    /// with the proper memory size allocated to it AND the actual requested size when asked. A BlockPointer created from
+    /// a straight Pointer will have the same size for the allocation size and the actual size. 
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+#if !VALIDATE
+    // We do this to have the ability to perform extensive validation for pointers.
+    readonly
+#endif
+    public unsafe struct BlockPointer<T> : Meta.IDescribe, IPointerType<T> where T : struct
+    {
+        public readonly void* Address;
+
+        private readonly int _size;
+
+        public readonly int BlockSizeAsBytes;
+
+#if VALIDATE
+        public uint Generation;        
+#endif
+
+        [MethodImpl(MethodImplOptions.NoOptimization)]
+        static BlockPointer()
+        {
+            // If T is not a blittable type, fail on type loading unrecoverably.
+            // We apply the NoOptimization attribute because we need to ensure the JIT wont optimize away
+            // the call to Marshal.SizeOf<T> for correctness either now or in the future. 
+            Marshal.SizeOf<T>();
+        }
+
+        public BlockPointer(BlockPointer ptr)
+        {
+            this.Address = ptr.Address;
+            this._size = ptr.Size / Unsafe.SizeOf<T>();
+            this.BlockSizeAsBytes = ptr.BlockSizeAsBytes;
+
+#if VALIDATE
+            this.Generation = ptr.Generation;
+#endif
+        }
+
+        public BlockPointer(void* ptr, int blockSizeAsBytes, int size)
+        {
+            this.Address = ptr;
+            this._size = size;
+            this.BlockSizeAsBytes = blockSizeAsBytes;
+
+#if VALIDATE
+            this.Generation = 0;
+#endif
+        }
+
+        public bool IsValid
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Address != null;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<T> AsSpan()
+        {
+            return new Span<T>(Address, (int)Size);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<T> AsSpan(int length)
+        {
+            if (length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new Span<T>(Address, length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<T> AsSpan(int start, int length)
+        {
+            if (start + length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+            
+            return new Span<T>((byte*)Address + start * Unsafe.SizeOf<T>(), length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySpan<T> AsReadOnlySpan()
+        {
+            return new ReadOnlySpan<T>(Address, (int)Size);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySpan<T> AsReadOnlySpan(int length)
+        {
+            if (length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new ReadOnlySpan<T>(Address, length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySpan<T> AsReadOnlySpan(int start, int length)
+        {
+            if (start + length > Size)
+                throw new ArgumentException($"{nameof(length)} cannot be bigger than block size.");            
+
+            return new ReadOnlySpan<T>((byte*)Address + start * Unsafe.SizeOf<T>(), length);
+        }
+
+        public int Size
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                EnsureIsNotBadPointerAccess();
+                return _size;
+            }
+        }
+
+        public int SizeAsBytes
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                EnsureIsNotBadPointerAccess();
+                return _size * Unsafe.SizeOf<T>();
+            }
+        }
+
+        [Conditional("DEBUG")]
+        [Conditional("VALIDATE")]
+        internal void EnsureIsNotBadPointerAccess()
+        {
+            if (Address == null)
+                throw new InvalidOperationException($"Trying to access the pointer but it is not valid");
+        }
+
+        public string Describe()
+        {
+            if (this.IsValid)
+                return $"{{{(long)this.Address:X64}|{this.Size}|{this.BlockSizeAsBytes}|{this.SizeAsBytes}b}}";
+
+            return "{null}";
+        }
+
+        public override string ToString()
+        {
+            return Describe();
+        }
+
+        public static implicit operator BlockPointer(BlockPointer<T> ptr)
+        {
+            return new BlockPointer(ptr.Address, ptr.BlockSizeAsBytes, ptr.SizeAsBytes);
+        }
+
+        public static implicit operator BlockPointer<T>(Pointer<T> ptr)
+        {
+            return new BlockPointer<T>(ptr.Address, ptr.SizeAsBytes, ptr.Size);
+        }
+
+        public static explicit operator BlockPointer<T>(BlockPointer ptr)
+        {
+            return new BlockPointer<T>(ptr.Address, ptr.BlockSizeAsBytes, ptr.Size / Unsafe.SizeOf<T>());
+        }
+    }
+}
