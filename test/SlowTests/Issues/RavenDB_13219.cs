34148584d977838d4ef10557d1cc605884472ad3
Pawel Pekrol <ppekrol@ravendb.net>
2019-03-27 18:26:51 +0200

    RavenDB-13219 prevent creating counters w/o name

---
+++ test/SlowTests/Issues/RavenDB_13219.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FastTests;
+using Orders;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Exceptions;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_13219 : RavenTestBase
+    {
+        [Fact]
+        public void ShouldNotBeAbleToCreateCountersWithoutNames()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Company
+                    {
+                        Name = "HR"
+                    });
+
+                    session.SaveChanges();
+                }
+
+                var operation = store.Operations.Send(new PatchByQueryOperation(@"from Companies 
+                update {
+                    incrementCounter(id(this), '', 5)
+                }"));
+
+                var e = Assert.Throws<RavenException>(() => operation.WaitForCompletion(TimeSpan.FromSeconds(15)));
+                Assert.Contains("'name' must be a non-empty string argument", e.Message);
+            }
+        }
+    }
+}
