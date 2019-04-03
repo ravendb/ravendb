3f25f23c42995061148606ccf6934631da02c000
aviv86 <aviv@ayende.com>
2019-04-03 11:38:23 +0200

    RavenDB-13278 : revision configuration options for collections should be case insensitive

---
+++ test/SlowTests/Issues/RavenDB-13278.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using FastTests;
+using Raven.Client.Documents.Operations.Revisions;
+using Raven.Client.Exceptions;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_13278 : RavenTestBase
+    {
+        [Fact]
+        public void Should_not_allow_having_two_revision_collection_configurations_with_collection_name_that_differs_only_in_casing()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var configuration = new RevisionsConfiguration
+                {
+                    Collections = new Dictionary<string, RevisionsCollectionConfiguration>
+                    {
+                        {
+                            "Users", new RevisionsCollectionConfiguration
+                            {
+                                PurgeOnDelete = true
+
+                            }
+                        },
+                        {
+                            "users", new RevisionsCollectionConfiguration
+                            {
+                                MinimumRevisionsToKeep = 5
+                            }
+                        }
+                    }
+                };
+
+                var ex = Assert.Throws<RavenException>( () => 
+                    store.Maintenance.Send(new ConfigureRevisionsOperation(configuration)));
+
+
+                Assert.Contains("Cannot have two different revision configurations on the same collection", ex.Message);
+
+            }
+
+        }
+    }
+}
