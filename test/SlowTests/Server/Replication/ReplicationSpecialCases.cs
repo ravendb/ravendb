60b23dcb635e6d1afe0ecdf8b6b70718d5159eb5
Grisha Kotler <grisha@ayende.com>
2019-04-04 18:56:37 +0300

    RavenDB-13341 - fix attachment replication after MissingAttachmentsInLastBatch (case sensitive issue in document id)

--- test/SlowTests/Server/Replication/ReplicationSpecialCases.cs
+++ test/SlowTests/Server/Replication/ReplicationSpecialCases.cs
@@ -319,43 +319,45 @@ namespace SlowTests.Server.Replication
             }
         }
 
-        [Fact]
-        public async Task ReplicationShouldSendMissingAttachments()
+        [Theory]
+        [InlineData("users/1")]
+        [InlineData("users/1-A")]
+        [InlineData("FoObAr")]
+        public async Task ReplicationShouldSendMissingAttachments(string documentId)
         {
             using (var source = GetDocumentStore())
             using (var destination = GetDocumentStore())
             {
                 await SetupReplicationAsync(source, destination);
 
-                const string docId = "users/1";
                 using (var session = source.OpenAsyncSession())
                 using (var fooStream = new MemoryStream(new byte[] { 1, 2, 3 }))
                 {
-                    await session.StoreAsync(new User() { Name = "Foo" }, docId);
-                    session.Advanced.Attachments.Store(docId, "foo.png", fooStream, "image/png");
+                    await session.StoreAsync(new User { Name = "Foo" }, documentId);
+                    session.Advanced.Attachments.Store(documentId, "foo.png", fooStream, "image/png");
                     await session.SaveChangesAsync();
                 }
 
-                WaitForDocumentToReplicate<User>(destination, docId, 15 * 1000);
+                WaitForDocumentToReplicate<User>(destination, documentId, 15 * 1000);
                 using (var session = destination.OpenAsyncSession())
                 {
-                    session.Delete(docId);
+                    session.Delete(documentId);
                     await session.SaveChangesAsync();
                 }
 
                 using (var session = source.OpenAsyncSession())
                 using (var fooStream2 = new MemoryStream(new byte[] { 4, 5, 6 }))
                 {
-                    session.Advanced.Attachments.Store(docId, "foo2.png", fooStream2, "image/png");
+                    session.Advanced.Attachments.Store(documentId, "foo2.png", fooStream2, "image/png");
                     await session.SaveChangesAsync();
                 }
 
-                Assert.NotNull(WaitForDocumentWithAttachmentToReplicate<User>(destination, docId, "foo2.png", 15 * 1000));
+                Assert.NotNull(WaitForDocumentWithAttachmentToReplicate<User>(destination, documentId, "foo2.png", 15 * 1000));
 
                 var buffer = new byte[3];
                 using (var session = destination.OpenAsyncSession())
                 {
-                    var user = await session.LoadAsync<User>(docId);
+                    var user = await session.LoadAsync<User>(documentId);
                     var attachments = session.Advanced.Attachments.GetNames(user);
                     Assert.Equal(2, attachments.Length);
                     foreach (var name in attachments)
@@ -376,46 +378,49 @@ namespace SlowTests.Server.Replication
             }
         }
 
-        [Fact]
-        public async Task ReplicationShouldSendMissingAttachmentsAlongWithNewOnes()
+        [Theory]
+        [InlineData("users/1", "users/2")]
+        [InlineData("users/1-A", "users/2-A")]
+        [InlineData("foo", "foo-2")]
+        [InlineData("FOO", "FOO-2")]
+        [InlineData("FoObAr", "FoObAr-2")]
+        public async Task ReplicationShouldSendMissingAttachmentsAlongWithNewOnes(string documentId1, string documentId2)
         {
             using (var source = GetDocumentStore())
             using (var destination = GetDocumentStore())
             {
                 await SetupReplicationAsync(source, destination);
 
-                const string doc1Id = "users/1";
                 using (var session = source.OpenAsyncSession())
                 using (var fooStream = new MemoryStream(new byte[] { 1, 2, 3 }))
                 {
-                    await session.StoreAsync(new User { Name = "Foo" }, doc1Id);
-                    session.Advanced.Attachments.Store(doc1Id, "foo.png", fooStream, "image/png");
+                    await session.StoreAsync(new User { Name = "Foo" }, documentId1);
+                    session.Advanced.Attachments.Store(documentId1, "foo.png", fooStream, "image/png");
                     await session.SaveChangesAsync();
                 }
 
-                WaitForDocumentToReplicate<User>(destination, doc1Id, 15 * 1000);
+                WaitForDocumentToReplicate<User>(destination, documentId1, 15 * 1000);
                 using (var session = destination.OpenAsyncSession())
                 {
-                    session.Delete(doc1Id);
+                    session.Delete(documentId1);
                     await session.SaveChangesAsync();
                 }
 
-                const string doc2Id = "users/2";
                 using (var session = source.OpenAsyncSession())
                 {
                     var toDispose = new List<IDisposable>();
 
                     // force replication of the original document (without the attachment)
-                    var doc1 = await session.LoadAsync<User>(doc1Id);
+                    var doc1 = await session.LoadAsync<User>(documentId1);
                     doc1.LastName = "Bar";
 
-                    await session.StoreAsync(new User { Name = "Foo" }, doc2Id);
+                    await session.StoreAsync(new User { Name = "Foo" }, documentId2);
 
                     for (var i = 0; i < 30; i++)
                     {
                         var fooStream = new MemoryStream(new byte[] { 4, 5, 6, (byte)i });
                         toDispose.Add(fooStream);
-                        session.Advanced.Attachments.Store(doc2Id, $"foo{i}.png", fooStream, "image/png");
+                        session.Advanced.Attachments.Store(documentId2, $"foo{i}.png", fooStream, "image/png");
                     }
 
                     await session.SaveChangesAsync();
@@ -423,13 +428,13 @@ namespace SlowTests.Server.Replication
                     session.Advanced.OnAfterSaveChanges += (_, __) => toDispose.ForEach(x => x.Dispose());
                 }
 
-                Assert.NotNull(WaitForDocumentWithAttachmentToReplicate<User>(destination, doc1Id, "foo.png", 15 * 1000));
-                Assert.NotNull(WaitForDocumentWithAttachmentToReplicate<User>(destination, doc2Id, "foo29.png", 15 * 1000));
+                Assert.NotNull(WaitForDocumentWithAttachmentToReplicate<User>(destination, documentId1, "foo.png", 15 * 1000));
+                Assert.NotNull(WaitForDocumentWithAttachmentToReplicate<User>(destination, documentId2, "foo29.png", 15 * 1000));
 
                 var buffer = new byte[3];
                 using (var session = destination.OpenAsyncSession())
                 {
-                    var user = await session.LoadAsync<User>(doc1Id);
+                    var user = await session.LoadAsync<User>(documentId1);
                     var attachments = session.Advanced.Attachments.GetNames(user);
                     Assert.Equal(1, attachments.Length);
                     foreach (var name in attachments)
