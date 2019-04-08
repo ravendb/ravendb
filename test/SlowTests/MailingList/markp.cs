e8c47e20e2e9e254a9b745e032f5399c4040cc64
Ayende Rahien <Ayende@ayende.com>
2019-04-08 16:27:57 +0300

    RavenDB-13363 Query parameters should not use the convention serializer

---
+++ test/SlowTests/MailingList/markp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FastTests;
+using Newtonsoft.Json;
+using Raven.Client.Documents.Linq;
+using Xunit;
+
+namespace SlowTests.MailingList
+{
+    public class markp : RavenTestBase
+    {
+        public class User
+        {
+            public string Name;
+        }
+        [Fact]
+        public void CanQueryUsingInWhenUsingCustomSerialization()
+        {
+            using(var store = GetDocumentStore(new Options
+            {
+                ModifyDocumentStore = ds =>
+                {
+                    ds.Conventions.CustomizeJsonSerializer = serializer =>
+                    {
+                        serializer.PreserveReferencesHandling = PreserveReferencesHandling.All;
+                    };
+                }
+            }))
+            {
+                using (var s = store.OpenSession())
+                {
+                    s.Store(new User
+                    {
+                        Name = "Oren"
+                    });
+                    s.SaveChanges();
+                }
+
+                using (var s = store.OpenSession())
+                {
+                    var r = s.Query<User>().Where(x => x.Name.In("Oren")).ToList();
+                    Assert.NotEmpty(r);
+                }
+
+            }
+        }
+    }
+}
