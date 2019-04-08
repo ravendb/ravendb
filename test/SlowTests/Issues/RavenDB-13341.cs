9eddc1599623e5ec84eb12ffd7d87d8fa80ae97e
Grisha Kotler <grisha@ayende.com>
2019-04-08 12:01:21 +0300

    RavenDB-13341 - update the database change vector when importing revisions in smuggler

--- test/SlowTests/Issues/RavenDB-13341.cs
+++ test/SlowTests/Issues/RavenDB-13341.cs
@@ -3,6 +3,7 @@ using FastTests;
 using Raven.Client;
 using Raven.Client.Documents.Attachments;
 using Raven.Client.Documents.Operations.Attachments;
+using Raven.Server.Documents;
 using Raven.Server.ServerWide.Context;
 using Raven.Server.Utils;
 using Sparrow.Json;
@@ -14,7 +15,7 @@ namespace SlowTests.Issues
     public class RavenDB_13341 : RavenTestBase
     {
         [Fact]
-        public async Task CanGetRevisionsByDate()
+        public async Task DatabaseChangeVectorIsUpdatedCorrectly()
         {
             using (var store = GetDocumentStore())
             {
@@ -26,11 +27,15 @@ namespace SlowTests.Issues
                 using (documentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
                 using (context.OpenReadTransaction())
                 {
+                    var lastAttachmentChangeVector = string.Empty;
+                    var lastRevisionChangeVector = string.Empty;
+
                     var attachmentStorage = documentDatabase.DocumentsStorage.AttachmentsStorage;
-                    var documents = documentsStorage.GetDocumentsFrom(context, etag: 0);
-                    foreach (var document in documents)
+                    foreach (var document in documentsStorage.GetDocumentsFrom(context, etag: 0))
                     {
                         VerifyAttachments();
+
+                        VerifyRevisions();
 
                         void VerifyAttachments()
                         {
@@ -48,9 +53,32 @@ namespace SlowTests.Issues
 
                                 var conflictStatus = ChangeVectorUtils.GetConflictStatus(document.ChangeVector, attachment.ChangeVector);
                                 Assert.Equal(ConflictStatus.Update, conflictStatus);
+
+                                var attachmentConflictStatus = ChangeVectorUtils.GetConflictStatus(attachment.ChangeVector, lastAttachmentChangeVector);
+                                if (attachmentConflictStatus == ConflictStatus.Update)
+                                    lastAttachmentChangeVector = attachment.ChangeVector;
+                            }
+                        }
+
+                        void VerifyRevisions()
+                        {
+                            var revisions = documentsStorage.RevisionsStorage.GetRevisions(context, document.Id, 0, int.MaxValue);
+                            foreach (var revision in revisions.Revisions)
+                            {
+                                var conflictStatus = ChangeVectorUtils.GetConflictStatus(revision.ChangeVector, lastRevisionChangeVector);
+                                if (conflictStatus == ConflictStatus.Update)
+                                    lastRevisionChangeVector = revision.ChangeVector;
                             }
                         }
                     }
+
+                    var lastChangeVector = DocumentsStorage.GetDatabaseChangeVector(context);
+
+                    var lastCvAttachmentConflictStatus = ChangeVectorUtils.GetConflictStatus(lastAttachmentChangeVector, lastChangeVector);
+                    Assert.Equal(lastCvAttachmentConflictStatus, ConflictStatus.AlreadyMerged);
+
+                    var lastCvRevisionConflictStatus = ChangeVectorUtils.GetConflictStatus(lastRevisionChangeVector, lastChangeVector);
+                    Assert.Equal(lastCvRevisionConflictStatus, ConflictStatus.AlreadyMerged);
                 }
             }
         }
