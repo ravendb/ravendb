5d8d402d00fade819f5ece83b3931dfd6aa37970
Arkadiusz Palinski <arek@ravendb.net>
2019-04-09 13:18:27 +0200

    RavenDB-13218 Fixing ETL process with counters. The issue was that we were iterating too far and incorrectly updated last filtered counter etag. In result we skip a bunch of documents and their counters.

--- src/Raven.Server/Documents/ETL/FilterCountersEnumerator.cs
+++ src/Raven.Server/Documents/ETL/FilterCountersEnumerator.cs
@@ -12,13 +12,16 @@ namespace Raven.Server.Documents.ETL
         private readonly EtlStatsScope _stats;
         private readonly DocumentsStorage _docsStorage;
         private readonly DocumentsOperationContext _context;
+        private readonly long _lastProcessedDocEtagInBatch;
 
-        public FilterCountersEnumerator(IEnumerator<CounterDetail> counters, EtlStatsScope stats, DocumentsStorage docsStorage, DocumentsOperationContext context)
+        public FilterCountersEnumerator(IEnumerator<CounterDetail> counters, EtlStatsScope stats, DocumentsStorage docsStorage, DocumentsOperationContext context,
+            long lastProcessedDocEtagInBatch)
         {
             _counters = counters;
             _stats = stats;
             _docsStorage = docsStorage;
             _context = context;
+            _lastProcessedDocEtagInBatch = lastProcessedDocEtagInBatch;
         }
 
         public bool MoveNext()
@@ -29,6 +32,9 @@ namespace Raven.Server.Documents.ETL
             {
                 var current = _counters.Current;
 
+                if (CanMoveNextAndNotExceedLastDocumentInBatch(current.Etag, _lastProcessedDocEtagInBatch) == false)
+                    return false;
+
                 var doc = _docsStorage.Get(_context, current.DocumentId);
 
                 if (doc != null && current.Etag > doc.Etag)
@@ -37,8 +43,35 @@ namespace Raven.Server.Documents.ETL
                     return true;
                 }
 
+                // counter has lower etag than its document - we skip it to avoid
+                // sending a counter of document that can not exist on the destination side
+
                 _stats.RecordChangeVector(current.ChangeVector);
                 _stats.RecordLastFilteredOutEtag(current.Etag, EtlItemType.Counter);
+            }
+
+            return false;
+        }
+
+        public static bool CanMoveNextAndNotExceedLastDocumentInBatch(long itemEtag, long lastProcessedDocEtagInBatch)
+        {
+            // we need to ensure we iterate counters only up to last processed doc etag in current batch
+
+            if (lastProcessedDocEtagInBatch == 0)
+            {
+                // there was no document transformed in current batch
+                // it means we're done with documents, we can send all counters
+
+                return true;
+            }
+
+            if (itemEtag < lastProcessedDocEtagInBatch)
+            {
+                // don't cross last transformed document
+                // transformation of counters is done _after_ transformation of docs
+                // we'll send counters with greater etags in next batch
+
+                return true;
             }
 
             return false;
