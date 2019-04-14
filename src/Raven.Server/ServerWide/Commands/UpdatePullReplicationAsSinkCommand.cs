2c83bc184d64c6dc4995ce4ac86ad02d33f7a653
Pawel Pekrol <ppekrol@ravendb.net>
2019-04-14 22:21:40 +0300

    RavenDB-13284 we cannot generate new TaskId when External or Pull Replication is Disabled

--- src/Raven.Server/ServerWide/Commands/UpdatePullReplicationAsSinkCommand.cs
+++ src/Raven.Server/ServerWide/Commands/UpdatePullReplicationAsSinkCommand.cs
@@ -23,7 +23,7 @@ namespace Raven.Server.ServerWide.Commands
             if (PullReplicationAsSink == null)
                 return null;
 
-            if (PullReplicationAsSink.Disabled || PullReplicationAsSink.TaskId == 0)
+            if (PullReplicationAsSink.TaskId == 0)
             {
                 PullReplicationAsSink.TaskId = etag;
             }
