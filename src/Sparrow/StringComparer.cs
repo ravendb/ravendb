7d81ca444c6604d07896521758c44fd191c8f5fb
maximburyak <maxim@ayende.com>
2019-04-14 20:21:39 +0300

    RavenDB-13396 Fix BlittableJsonReaderObject.TryGetMember performance degradation due to switch to framework's StringSegment

--- src/Sparrow/StringComparer.cs
+++ src/Sparrow/StringComparer.cs
@@ -1,8 +1,98 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Primitives;
+using Sparrow.Binary;
+using Sparrow.Utils;
 
 namespace Sparrow
 {
+
+    public struct StringSegmentEqualityStructComparer : IEqualityComparer<StringSegment>
+    {
+        public static IEqualityComparer<StringSegment> BoxedInstance = new StringSegmentEqualityStructComparer();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(StringSegment x, StringSegment y)
+        {
+            int xSize = x.Length;
+            int ySize = y.Length;
+            if (xSize != ySize)
+                goto ReturnFalse;  // PERF: Because this method is going to be inlined, in case of false we will want to jump at the end.     
+
+            int xStart = x.Offset;
+            int yStart = y.Offset;
+            string xStr = x.Buffer;
+            string yStr = y.Buffer;
+            for (int i = 0; i < xSize; i++)
+            {
+                if (xStr[xStart + i] != yStr[yStart + i])
+                    goto ReturnFalse;
+            }
+            return true;
+
+        ReturnFalse:
+            return false;
+
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int GetHashCode(StringSegment x)
+        {
+            int xStart = x.Offset;
+            int xSize = x.Length;
+            string xStr = x.Buffer;
+
+            uint hash = 0;
+            for (int i = 0; i < xSize; i++)
+            {
+                hash = Hashing.Combine(hash, xStr[xStart + i]);
+            }
+
+            return (int)hash;
+        }
+    }
+
+    public class CaseInsensitiveStringSegmentEqualityComparer : IEqualityComparer<StringSegment>
+    {
+        public static CaseInsensitiveStringSegmentEqualityComparer Instance = new CaseInsensitiveStringSegmentEqualityComparer();
+
+        [ThreadStatic]
+        private static char[] _buffer;
+
+        static CaseInsensitiveStringSegmentEqualityComparer()
+        {
+            ThreadLocalCleanup.ReleaseThreadLocalState += () => _buffer = null;
+        }
+
+        public bool Equals(StringSegment x, StringSegment y)
+        {
+            if (x.Length != y.Length)
+                return false;
+            var compare = string.Compare(x.Buffer, x.Offset, y.Buffer, y.Offset, x.Length, StringComparison.OrdinalIgnoreCase);
+            return compare == 0;
+        }
+
+        public unsafe int GetHashCode(StringSegment str)
+        {
+            if (_buffer == null || _buffer.Length < str.Length)
+                _buffer = new char[Bits.PowerOf2(str.Length)];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                _buffer[i] = char.ToUpperInvariant(str.Buffer[str.Offset + i]);
+            }
+
+            fixed (char* p = _buffer)
+            {
+                //PERF: JIT will remove the corresponding line based on the target architecture using dead code removal.                                 
+                if (IntPtr.Size == 4)
+                    return (int)Hashing.XXHash32.CalculateInline((byte*)p, str.Length * sizeof(char));
+                return (int)Hashing.XXHash64.CalculateInline((byte*)p, (ulong)str.Length * sizeof(char));
+            }
+        }
+    }
+
     /// <summary>
     /// The struct compared is optimized for small strings that come from the outside of the controlled fence, 
     /// the hash function used is compact (making it suitable for inlining) and also flood resistant. 
