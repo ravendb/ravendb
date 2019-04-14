561db296b53b38f7d0f0b894a5269568bbc9ee73
Haludi <igalmer@gmail.com>
2019-04-14 13:43:19 +0300

    RavenDB-13046 Null resutl for swap definition fixed

---
+++ test/FastTests/Sparrow/FileSystemUtilsTest.cs
@@ -0,0 +1,33 @@
+using FastTests.Voron;
+using Sparrow.Platform.Posix;
+using Xunit;
+
+namespace FastTests.Sparrow
+{
+    public class FileSystemUtilsTest : RavenTestBase
+    {
+        [LinuxFact]
+        public void WriteAndReadPageUsingCryptoPager_WhenCall_AllResultShouldBeNotNull()
+        {
+            var swap = KernelVirtualFileSystemUtils.ReadSwapInformationFromSwapsFile();
+            Assert.All(swap, s => Assert.NotNull(s.DeviceName));
+        }
+        
+        [LinuxFact]
+        public void WriteAndReadPageUsingCryptoPager_WhenTwoSwapDefined_AllResultShouldBeNotNull()
+        {
+            var fileName = GetTempFileName();
+            
+            string[] lines =
+            {
+                "Filename				Type		Size	Used	Priority", 
+                "/dev/sda6                               partition	999420	56320	-2", 
+                "/dev/sdb7                               partition	999420	56320	-2"
+            };
+            System.IO.File.WriteAllLines(fileName, lines);
+            
+            var swap = KernelVirtualFileSystemUtils.ReadSwapInformationFromSwapsFile(fileName);
+            Assert.All(swap, s => Assert.NotNull(s.DeviceName));
+        }
+    }
+}
