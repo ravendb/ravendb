8989d9c5fb3b6aa16b52cfac2f037fd006ccd3fb
maximburyak <maxim@ayende.com>
2019-04-17 11:59:03 +0300

    RavenDB-13411: PR fixes

--- Raven.Tests.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Raven.Tests.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
