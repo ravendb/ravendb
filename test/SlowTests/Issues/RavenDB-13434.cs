4e3fd95938bfc0199c0f7cd27fa95362ad51acc6
Pawel Pekrol <ppekrol@ravendb.net>
2019-04-23 08:40:43 +0200

    RavenDB-13434 need to maintain binary compatibility

--- test/SlowTests/Issues/RavenDB-13434.cs
+++ test/SlowTests/Issues/RavenDB-13434.cs
@@ -62,21 +62,21 @@ namespace SlowTests.Issues
                 using (var session = store.OpenSession())
                 {
                     // testing "and" terms search
-                    var res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "1 2 3 5", @operator: Raven.Client.Documents.Queries.SearchOperator.And).ToList();
+                    var res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "1 2 3 5", 1M, SearchOptions.Guess, Raven.Client.Documents.Queries.SearchOperator.And).ToList();
                     Assert.Equal(1, res.Count);
                     Assert.Equal("OneTwoThreeFive", res.First().Name);
 
                     // testing "and" terms search, partial values list
-                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "1 3 5", @operator: Raven.Client.Documents.Queries.SearchOperator.And).ToList();
+                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "1 3 5", 1M, SearchOptions.Guess, Raven.Client.Documents.Queries.SearchOperator.And).ToList();
                     Assert.Equal(1, res.Count);
                     Assert.Equal("OneTwoThreeFive", res.First().Name);
 
                     // testing "and" terms search, no results expected
-                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "1 2 3 5 7", @operator: Raven.Client.Documents.Queries.SearchOperator.And).ToList();
+                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "1 2 3 5 7", 1M, SearchOptions.Guess, Raven.Client.Documents.Queries.SearchOperator.And).ToList();
                     Assert.Equal(0, res.Count);
 
                     // testing "or" search, where one of the values is irrelevant
-                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "2 9", @operator: Raven.Client.Documents.Queries.SearchOperator.Or).ToList();
+                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "2 9", 1M, SearchOptions.Guess, Raven.Client.Documents.Queries.SearchOperator.Or).ToList();
                     Assert.Equal(5, res.Count);
                     Assert.Contains("OneTwo", res.Select(x => x.Name));
                     Assert.Contains("OneTwoThree", res.Select(x => x.Name));
@@ -85,16 +85,16 @@ namespace SlowTests.Issues
                     Assert.Contains("TwoFiveSeven", res.Select(x => x.Name));
 
 
-                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "8 9 10", options: SearchOptions.Not, @operator: Raven.Client.Documents.Queries.SearchOperator.Or).ToList();
+                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "8 9 10", 1M, options: SearchOptions.Not, @operator: Raven.Client.Documents.Queries.SearchOperator.Or).ToList();
                     Assert.Equal(7, res.Count);
 
-                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "1 2 3 5 7", options: SearchOptions.Not, @operator: Raven.Client.Documents.Queries.SearchOperator.Or).ToList();
+                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "1 2 3 5 7", 1M, options: SearchOptions.Not, @operator: Raven.Client.Documents.Queries.SearchOperator.Or).ToList();
                     Assert.Equal(0, res.Count);
 
-                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "1 2 3 5 7", options: SearchOptions.Not, @operator: Raven.Client.Documents.Queries.SearchOperator.And).ToList();
+                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "1 2 3 5 7", 1M, options: SearchOptions.Not, @operator: Raven.Client.Documents.Queries.SearchOperator.And).ToList();
                     Assert.Equal(7, res.Count);
 
-                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "2 5", options: SearchOptions.Not, @operator: Raven.Client.Documents.Queries.SearchOperator.And).ToList();
+                    res = session.Query<GeekPerson>().Customize(x => x.WaitForNonStaleResults()).Search(x => x.FavoritePrimes, "2 5", 1M, options: SearchOptions.Not, @operator: Raven.Client.Documents.Queries.SearchOperator.And).ToList();
                     Assert.Equal(4, res.Count);
 
 
