76c4be3703aad52142a9c7658fe0a62cf56f843f
Ayende Rahien <Ayende@ayende.com>
2019-04-23 16:44:15 +0300

    RavenDB-13052 - Fixing an issue where we didn't take into account the size of the bit buffer header when computing the overall size for segment limits - Handling null tags properly - Adding additional tests  (including failing one for multiple segments)

--- test/FastTests/Server/Documents/TimeSeriesTests.cs
+++ test/FastTests/Server/Documents/TimeSeriesTests.cs
@@ -38,5 +38,99 @@ namespace FastTests.Server.Documents
 
             }
         }
+
+        [Fact]
+        public void CanStoreAndReadMultipleTimestamps()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var baseline = DateTime.Today;
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new { Name = "Oren" }, "users/ayende");
+                    session.TimeSeriesFor("users/ayende")
+                        .Append("Heartrate", baseline.AddMinutes(1), "watches/fitbit", new[] { 59d });
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    session.TimeSeriesFor("users/ayende")
+                        .Append("Heartrate", baseline.AddMinutes(2), "watches/fitbit", new[] { 61d });
+                    session.TimeSeriesFor("users/ayende")
+                        .Append("Heartrate", baseline.AddMinutes(3), "watches/apple-watch", new[] { 62d });
+
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var vals = session.TimeSeriesFor("users/ayende")
+                        .Get("Heartrate", DateTime.MinValue, DateTime.MaxValue)
+                        .ToList();
+                    Assert.Equal(3, vals.Count);
+
+                    Assert.Equal(new[] { 59d }, vals[0].Values);
+                    Assert.Equal("watches/fitbit", vals[0].Tag);
+                    Assert.Equal(baseline.AddMinutes(1), vals[0].Timestamp);
+
+                    Assert.Equal(new[] { 61d }, vals[1].Values);
+                    Assert.Equal("watches/fitbit", vals[1].Tag);
+                    Assert.Equal(baseline.AddMinutes(2), vals[1].Timestamp);
+
+                    Assert.Equal(new[] { 62d }, vals[2].Values);
+                    Assert.Equal("watches/apple-watch", vals[2].Tag);
+                    Assert.Equal(baseline.AddMinutes(3), vals[2].Timestamp);
+                }
+            }
+        }
+
+        [Fact]
+        public void CanStoreLargeNumberOfValues()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var baseline = DateTime.Today;
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new { Name = "Oren" }, "users/ayende");
+                    session.SaveChanges();
+                }
+
+                var offset = 1;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    using (var session = store.OpenSession())
+                    {
+
+                        for (int j = 0; j < 1000; j++)
+                        {
+                            session.TimeSeriesFor("users/ayende")
+                                .Append("Heartrate", baseline.AddMinutes(offset++), "watches/fitbit", new double[] { offset });
+                        }
+
+                        session.SaveChanges();
+                    }
+                }
+
+
+                using (var session = store.OpenSession())
+                {
+                    var vals = session.TimeSeriesFor("users/ayende")
+                        .Get("Heartrate", DateTime.MinValue, DateTime.MaxValue)
+                        .ToList();
+                    Assert.Equal(10_000, vals.Count);
+
+                    for (int i = 0; i < 10_000; i++)
+                    {
+                        Assert.Equal(baseline.AddMinutes(1 + i), vals[i].Timestamp);
+                        Assert.Equal(1 + i, vals[i].Values[0]);
+                    }
+                }
+            }
+        }
     }
 }
