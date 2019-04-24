ca2c7f35fefa9679aaa0ea08ef6b44148cff6dfe
Ayende Rahien <Ayende@ayende.com>
2019-04-24 14:51:58 +0300

    RavenDB-13052 - Fixing code after rebase - Avoid looking at the segment we split when we might be moving it - Properly compute the segment key on split - Add test for non sequential inserts

--- src/Raven.Server/Documents/Handlers/BatchRequestParser.cs
+++ src/Raven.Server/Documents/Handlers/BatchRequestParser.cs
@@ -481,7 +481,7 @@ namespace Raven.Server.Documents.Handlers
                             if (state.CurrentTokenType == JsonParserToken.EndArray)
                                 break;
 
-                            var append = await ReadJsonObject(ctx, stream, commandData.Id, parser, state, buffer, token);
+                            var append = await ReadJsonObject(ctx, stream, commandData.Id, parser, state, buffer, modifier, token);
                             commandData.TimeSeries.Appends.Add(AppendTimeSeriesOperation.Parse(append));
                         }
                         break;
@@ -500,7 +500,7 @@ namespace Raven.Server.Documents.Handlers
                             if (state.CurrentTokenType == JsonParserToken.EndArray)
                                 break;
 
-                            var removal = await ReadJsonObject(ctx, stream, commandData.Id, parser, state, buffer, token);
+                            var removal = await ReadJsonObject(ctx, stream, commandData.Id, parser, state, buffer, modifier, token);
                             commandData.TimeSeries.Removals.Add(RemoveTimeSeriesOperation.Parse(removal));
                         }
                         break;
