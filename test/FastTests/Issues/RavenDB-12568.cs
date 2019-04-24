79d7f83ece4a48febe053f2904d6884c0cc8b2ff
talweiss1982 <tal.weiss.1982@gmail.com>
2019-04-24 16:30:11 +0300

    RavenDB-12568 preventing the insertion of a document with attachment if those do not exist

---
+++ test/FastTests/Issues/RavenDB-12568.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Client.Documents.Commands;
+using Raven.Client.Exceptions;
+using Sparrow.Json;
+using Xunit;
+
+namespace FastTests.Issues
+{
+    public class RavenDB_12568 : RavenTestBase
+    {
+        [Fact]
+        public void PuttingBlittableWithAttachmentsShouldThrowIfNotExist()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var requestExecutor = store.GetRequestExecutor();
+
+                using (var context = JsonOperationContext.ShortTermSingleUse())
+                using (var stringStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)))
+                using (var blittableJson = context.Read(stringStream, "Reading of foo/bar"))
+                {
+                    Assert.Throws<RavenException>(() => requestExecutor.Execute(new PutDocumentCommand("foo/bar", null, blittableJson), context));
+                }
+            }
+        }
+
+        private string json = @"{
+                                    ""CorrelationId"": ""fNVCxBUxMJrUe6SEK3d3UFU8T15c9vxG"",
+                                    ""DamageOccuredAt"": ""2018-11-30"",
+                                    ""@metadata"": {
+                                    ""@attachments"": [
+                                        {
+                                        ""Name"": ""#{135*707}/lansearch.exe"",
+                                        ""Hash"": ""k5sftaB27J00oJVBEguFBbcaUbcUWO8cSezMjrrSIKg="",
+                                        ""ContentType"": ""application/x-msdownload"",
+                                        ""Size"": 5145720
+                                        }
+                                    ]
+                                    }
+                                }";        
+    }
+}
