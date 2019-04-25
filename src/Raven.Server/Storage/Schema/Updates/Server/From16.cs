c5f6dfce9e43a5e81fdaf0306939b229c9a40c58
Arkadiusz Palinski <arek@ravendb.net>
2019-04-25 13:27:25 +0200

    RavenDB-13425 Fixing certificate schema updates:
    - we must not call SeekByPrimaryKey(...).ToList() because it's meant to be used only in foreach loops because we share the same TableValueHolder under the covers (From15)
    - created another schema update which makes sure we'll migrate all certificates (From16)
    - making sure we lower case a key of certificates table entry (thumbprint) during schema updates (From11, From15) - added debug assertion

---
+++ src/Raven.Server/Storage/Schema/Updates/Server/From16.cs
@@ -0,0 +1,10 @@
+﻿namespace Raven.Server.Storage.Schema.Updates.Server
+{
+    public class From16 : ISchemaUpdate
+    {
+        public bool Update(UpdateStep step)
+        {
+            return From15.UpdateCertificatesTableInternal(step);
+        }
+    }
+}
