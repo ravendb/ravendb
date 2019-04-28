314b962ed91b7a18135dbb7612a8c48907e4fc30
Or Gabay <gabayor@ravendb.net>
2019-04-28 14:45:44 +0300

    [RDBC-320] fix PR commits

--- src/Raven.Client/Documents/Operations/Backups/BackupStatus.cs
+++ src/Raven.Client/Documents/Operations/Backups/BackupStatus.cs
@@ -142,16 +142,15 @@ namespace Raven.Client.Documents.Operations.Backups
         public void ChangeState(UploadState newState)
         {
             UploadState = newState;
-            if (newState == UploadState.Done)
-                switch (newState)
-                {
-                    case UploadState.PendingUpload:
-                        _sw = Stopwatch.StartNew();
-                        break;
-                    case UploadState.Done:
-                        _sw.Stop();
-                        break;
-                }
+            switch (newState)
+            {
+                case UploadState.PendingUpload:
+                    _sw = Stopwatch.StartNew();
+                    break;
+                case UploadState.Done:
+                    _sw.Stop();
+                    break;
+            }
         }
 
         public void SetTotal(long totalLength)
