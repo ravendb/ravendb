2ba6d8ac3242cd2ad3259ce2052e21c76826b1d0
Arkadiusz Palinski <arek@ravendb.net>
2019-04-29 12:59:51 +0200

    RDBC-319 Fixing TotalResults value of collection query stats when streaming a query

---
+++ test/SlowTests/Issues/RDBC_319.cs
@@ -0,0 +1,65 @@
+﻿using FastTests;
+using SlowTests.Core.Utils.Entities;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RDBC_319 : RavenTestBase
+    {
+        [Fact]
+        public void CanStreamQueryResults()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    for (int i = 0; i < 200; i++)
+                    {
+                        session.Store(new User());
+                    }
+                    session.SaveChanges();
+                }
+
+                WaitForIndexing(store);
+
+                int count = 0;
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Advanced.RawQuery<User>("from Users");
+
+                    var reader = session.Advanced.Stream(query, out var stats);
+
+                    while (reader.MoveNext())
+                    {
+                        count++;
+                        Assert.IsType<User>(reader.Current.Document);
+                    }
+
+                    Assert.Equal(200, stats.TotalResults);
+                }
+
+                Assert.Equal(200, count);
+                count = 0;
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Advanced.RawQuery<User>("from Users");
+
+                    var reader = session.Advanced.Stream(query, out var stats);
+                    while (reader.MoveNext())
+                    {
+                        count++;
+                        Assert.IsType<User>(reader.Current.Document);
+
+                    }
+
+                    Assert.Equal(200, stats.TotalResults);
+
+                }
+
+                Assert.Equal(200, count);
+            }
+        }
+    }
+}
