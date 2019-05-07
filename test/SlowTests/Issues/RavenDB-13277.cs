58bc5ae11ac456eac613fa8041da264c825a6239
Tal Weiss <tal@ayende.com>
2019-05-07 16:06:54 +0300

    RavenDB-13277 creating index with the same name but different casing should throw

---
+++ test/SlowTests/Issues/RavenDB-13277.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FastTests;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Operations.Indexes;
+using Raven.Client.Exceptions.Documents.Indexes;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_13277: RavenTestBase
+    {
+        [Fact]
+        public void CreateIndexWithTheSameLowercasedNameShouldThrow()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var indexDef = new IndexDefinition
+                {
+                    Name = "A",
+                    Maps =
+                    {
+                        @"from o in docs.Orders
+                          select new { Company = o.Company }"
+                    }
+                };
+
+                store.Maintenance.Send(new PutIndexesOperation(indexDef));
+                indexDef.Name = "a";
+                Assert.Throws<IndexCreationException>(()=>store.Maintenance.Send(new PutIndexesOperation(indexDef)));                
+            }
+        }
+    }
+}
