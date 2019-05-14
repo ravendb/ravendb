e942b20d8b744c0dd709338afe3557775c5767c7
Or Gabay <gabayor@ravendb.net>
2019-05-14 17:40:16 +0300

    RavenDB-12674 Add endpoint for renew license

---
+++ src/Raven.Server/Commercial/LicenseRenewalResult.cs
@@ -0,0 +1,20 @@
+﻿using Sparrow.Json.Parsing;
+
+namespace Raven.Server.Commercial
+{
+    public class LicenseRenewalResult
+    {
+        public string Message { get; set; }
+
+        public string Error { get; set; }
+
+        public DynamicJsonValue ToJson()
+        {
+            return new DynamicJsonValue
+            {
+                [nameof(Message)] = Message,
+                [nameof(Error)] = Error
+            };
+        }
+    }
+}
