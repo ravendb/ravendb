fd6a52a28fa29486466e96e46091ce0fff51beb7
Arkadiusz Palinski <arek@ravendb.net>
2019-05-15 13:35:30 +0200

    RavenDB-13563 Optimize Lucene query creation for queries containing multiple boolean clauses

---
+++ src/Raven.Server/Documents/Queries/RavenBooleanQuery.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Lucene.Net.Search;
+using Raven.Server.Documents.Queries.AST;
+using Query = Lucene.Net.Search.Query;
+
+namespace Raven.Server.Documents.Queries
+{
+    public class RavenBooleanQuery : BooleanQuery
+    {
+        private readonly OperatorType _operator;
+
+        public RavenBooleanQuery(OperatorType @operator)
+        {
+            _operator = @operator;
+        }
+
+        public RavenBooleanQuery(Query left, Query right, OperatorType @operator)
+        {
+            _operator = @operator;
+
+            switch (@operator)
+            {
+                case OperatorType.And:
+                    Add(new BooleanClause(left, Occur.MUST));
+                    TryAnd(right);
+                    break;
+                case OperatorType.Or:
+                    Add(new BooleanClause(left, Occur.SHOULD));
+                    TryOr(right);
+                    break;
+                default:
+                    ThrowInvalidOperatorType(@operator);
+                    break;
+            }
+        }
+
+        public bool TryAnd(Query right)
+        {
+            if (_operator == OperatorType.And)
+            {
+                Add(right, Occur.MUST);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void And(Query left, Query right)
+        {
+            if (_operator != OperatorType.And)
+                ThrowInvalidOperator(OperatorType.And);
+
+            Add(left, Occur.MUST);
+            Add(right, Occur.MUST);
+        }
+
+        public bool TryOr(Query right)
+        {
+            if (_operator == OperatorType.Or)
+            {
+                Add(right, Occur.SHOULD);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Or(Query left, Query right)
+        {
+            if (_operator != OperatorType.Or)
+                ThrowInvalidOperator(OperatorType.Or);
+
+            Add(left, Occur.SHOULD);
+            Add(right, Occur.SHOULD);
+        }
+
+        private void ThrowInvalidOperator(OperatorType @operator)
+        {
+            throw new InvalidOperationException($"Cannot '{@operator}' query clause because current operator is {_operator}");
+        }
+
+        private static void ThrowInvalidOperatorType(OperatorType operatorType)
+        {
+            throw new ArgumentException($"{nameof(RavenBooleanQuery)} doesn't handle '{operatorType}' operator");
+        }
+    }
+}
---
+++ test/SlowTests/Issues/RavenDB_13563.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq;
+using FastTests;
+using Newtonsoft.Json;
+using Raven.Client.Json;
+using Raven.Server.Documents.Queries;
+using Raven.Tests.Core.Utils.Entities;
+using Sparrow.Json;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_13563 : RavenTestBase
+    {
+        [Fact]
+        public void ShouldGenerateSimpleLuceneBooleanQueriesWithoutManyBrackets()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var serializer = store.Conventions.CreateSerializer();
+
+                    using (var context = JsonOperationContext.ShortTermSingleUse())
+                    {
+                        var usersOr = session.Query<User>()
+                            .Customize(x => x.NoCaching())
+                            .Where(x => x.Name == "John" || x.Name == "Bob" || x.Name == "George" || x.Name == "Georgina" || x.Name == "Jane");
+                        Assert.Equal("Name:john Name:bob Name:george Name:georgina Name:jane", GetLuceneQuery(usersOr, context, serializer));
+
+                        var usersAnd = session.Query<User>()
+                            .Customize(x => x.NoCaching())
+                            .Where(x => x.Name == "John" && x.Name == "Bob" && x.Name == "George" && x.Name == "Georgina" && x.Name == "Jane");
+
+                        Assert.Equal("+Name:john +Name:bob +Name:george +Name:georgina +Name:jane", GetLuceneQuery(usersAnd, context, serializer));
+
+                        var usersAndOrMixed = session.Query<User>()
+                            .Customize(x => x.NoCaching())
+                            .Where(x => x.Name == "John" && x.Name == "Bob" && x.Name == "George" || x.Name == "Georgina" || x.Name == "Jane");
+
+                        Assert.Equal("(+Name:john +Name:bob +Name:george) Name:georgina Name:jane", GetLuceneQuery(usersAndOrMixed, context, serializer));
+
+                        var usersOrAndMixed2 = session.Query<User>()
+                            .Customize(x => x.NoCaching())
+                            .Where(x => x.Name == "John" || x.Name == "Bob" || x.Name == "George" && x.Name == "Georgina" && x.Name == "Jane");
+
+                        Assert.Equal("Name:john Name:bob (+Name:george +Name:georgina +Name:jane)", GetLuceneQuery(usersOrAndMixed2, context, serializer));
+
+                        var usersOrAndMixed3 = session.Query<User>()
+                            .Customize(x => x.NoCaching())
+                            .Where(x => x.Name == "John" || x.Name == "Bob" && x.Name == "George" || x.Name == "Georgina" && x.Name == "Jane");
+
+                        Assert.Equal("Name:john (+Name:bob +Name:george) (+Name:georgina +Name:jane)", GetLuceneQuery(usersOrAndMixed3, context, serializer));
+                    }
+                }
+            }
+        }
+
+        private static string GetLuceneQuery(IQueryable<User> query, JsonOperationContext context, JsonSerializer jsonSerializer)
+        {
+            var indexQuery = RavenTestHelper.GetIndexQuery(query);
+
+            using (var writer = new BlittableJsonWriter(context))
+            {
+                jsonSerializer.Serialize(writer, indexQuery.QueryParameters);
+
+                writer.FinalizeDocument();
+
+                using (var blittableParameters = writer.CreateReader())
+                {
+                    var indexQueryServerSide = new IndexQueryServerSide(indexQuery.Query, blittableParameters);
+
+                    var luceneQuery = QueryBuilder.BuildQuery(null,
+                        null,
+                        indexQueryServerSide.Metadata,
+                        indexQueryServerSide.Metadata.Query.Where,
+                        null,
+                        blittableParameters, null,
+                        null);
+
+                    return luceneQuery.ToString();
+                }
+            }
+        }
+    }
+}
