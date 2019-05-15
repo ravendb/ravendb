c285ee596dc1503a95d5f57b5efa5ca6d7237982
aviv86 <aviv@ayende.com>
2019-05-15 16:09:21 +0200

    RavenDB-13510 : add test

--- test/SlowTests/Issues/RavenDB-13510.cs
+++ test/SlowTests/Issues/RavenDB-13510.cs
@@ -53,6 +53,57 @@ namespace SlowTests.Issues
 
             }
         }
+
+        [Fact]
+        public async Task Incrementing_an_existing_counter_upon_incoming_replication_should_affect_metrics()
+        {
+            using (var storeA = GetDocumentStore())
+            using (var storeB = GetDocumentStore())
+            {
+                foreach (var store in new [] {storeA, storeB})
+                {
+                    using (var bulk = store.BulkInsert())
+                    {
+                        for (int i = 0; i < 1000; i++)
+                        {
+                            bulk.Store(new User(), "users/" + i);
+                        }
+                    }
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        using (var session = store.OpenSession())
+                        {
+                            for (int j = 0; j < 100; j++)
+                            {
+                                var countersFor = session.CountersFor("users/" + (i * 100 + j));
+                                countersFor.Increment("myCounter1");
+                                countersFor.Increment("myCounter2");
+                                countersFor.Increment("myCounter3");
+                            }
+
+                            session.SaveChanges();
+                        }
+                    }
+                }
+
+                var databaseB = await GetDocumentDatabaseInstanceFor(storeB);
+
+                Assert.True(databaseB.Metrics.Counters.PutsPerSec.Count > 1);
+                Assert.True(databaseB.Metrics.Counters.BytesPutsPerSec.Count > 1);
+
+                var oldPutsCount = databaseB.Metrics.Counters.PutsPerSec.Count;
+                var oldBytesPutsCount = databaseB.Metrics.Counters.BytesPutsPerSec.Count;
+
+                await SetupReplicationAsync(storeA, storeB);
+                EnsureReplicating(storeA, storeB);
+
+                databaseB = await GetDocumentDatabaseInstanceFor(storeB);
+
+                Assert.Equal(oldPutsCount * 2, databaseB.Metrics.Counters.PutsPerSec.Count);
+                Assert.True(databaseB.Metrics.Counters.BytesPutsPerSec.Count >= oldBytesPutsCount * 2);
+            }
+        }
     }
 }
 
