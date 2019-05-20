2ccf7dceef8ceac3b83f88f5ebf35a16990cb388
Or Gabay <gabayor@ravendb.net>
2019-05-20 15:19:02 +0300

    [RavenDB-13501] check for delete changes last, to override any other changes.(whatchanged)

--- Raven.Tests.Issues/WhatChangedTests.cs
+++ Raven.Tests.Issues/WhatChangedTests.cs
@@ -1,4 +1,6 @@
-﻿using Raven.Json.Linq;
+﻿using System.Linq;
+using Raven.Abstractions.Data;
+using Raven.Json.Linq;
 using Raven.Tests.Common;
 using Xunit;
 
@@ -36,6 +38,46 @@ namespace Raven.Tests.Issues
                 }
             }
 
+        }
+        [Fact]
+        public void WhatChanged_Delete_After_Change_Value()
+        {
+            //RavenDB-13501
+            using (var store = NewDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    const string id = "ABC";
+                    var o = new TestObject();
+                    o.Id = id;
+                    o.A = "A";
+                    o.B = "A";
+                    session.Store(o);
+                    session.SaveChanges();
+                    Assert.True(!session.Advanced.HasChanges);
+
+                    o = session.Load<TestObject>(id);
+                    o.A = "B"; 
+                    o.B = "C"; 
+                    session.Delete(o); 
+
+                    var whatChanged = session.Advanced.WhatChanged();
+
+                    Assert.True(whatChanged.Count == 1 
+                                && whatChanged.Values.First()[0].Change == DocumentsChanges.ChangeType.DocumentDeleted);
+
+                    session.SaveChanges();
+
+                    o = session.Load<TestObject>(id);
+                    Assert.True(o == null);
+                }
+            }
+        }
+        class TestObject
+        {
+            public string Id { get; set; }
+            public string A { get; set; }
+            public string B { get; set; }
         }
     }
 }
