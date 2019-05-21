959f6a6b4cdffccc5b32b541e449499e292d2a1e
Grzegorz Lachowski <gregolsky@ravendb.net>
2019-05-21 12:24:25 +0200

    RavenDB-7070 dont publish 4.1 as latest docker tag

--- docker/publish-ubuntu.ps1
+++ docker/publish-ubuntu.ps1
@@ -42,8 +42,6 @@ function RemoveImages($imageTags) {
 }
 function GetImageTags($repo, $version) {
         return @(
-            "$($repo):latest",
-            "$($repo):ubuntu-latest",
             "$($repo):4.1-ubuntu-latest",
             "$($repo):$($version)-ubuntu.18.04-x64"
         )
