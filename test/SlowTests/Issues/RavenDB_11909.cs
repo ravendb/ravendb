12c58431efbdb88de7c83a79f13073ab2c1efb26
Egor <egor@ravendb.net>
2019-05-22 15:40:13 +0300

    RavenDB-11909: Add test to track database record changes

--- test/SlowTests/Issues/RavenDB_11909.cs
+++ test/SlowTests/Issues/RavenDB_11909.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using FastTests;
+using Raven.Client.Documents.Linq;
 using Raven.Client.Documents.Operations.Backups;
 using Raven.Client.Documents.Operations.Replication;
 using Raven.Client.Exceptions;
 using Raven.Client.ServerWide;
 using Raven.Client.ServerWide.Operations;
+using Raven.Server.ServerWide;
 using Xunit;
 
 namespace SlowTests.Issues
@@ -73,6 +76,37 @@ namespace SlowTests.Issues
 
                 Assert.Throws<RavenException>(() => store.Maintenance.Server.Send(new UpdateDatabaseOperation(record, etag)));
             }
+        }
+
+        [Fact]
+        public void ThrowOnDatabaseRecordChanges()
+        {
+            const int numberOfFields = 26;
+            const int numberOfProperties = 0;
+            var tasksList = new List<string>
+            {
+                nameof(DatabaseRecord.PeriodicBackups),
+                nameof(DatabaseRecord.ExternalReplications),
+                nameof(DatabaseRecord.SinkPullReplications),
+                nameof(DatabaseRecord.HubPullReplications),
+                nameof(DatabaseRecord.RavenEtls),
+                nameof(DatabaseRecord.SqlEtls)
+            };
+
+            var dbRecordType = typeof(DatabaseRecord);
+            var fields = dbRecordType.GetFields().Select(x => x.Name);
+            var properties = dbRecordType.GetProperties().Select(x => x.Name);
+
+            // check if deleted fields from our list
+            Assert.True(tasksList.All(val => fields.Contains(val)), 
+                $"Some configuration deleted from {nameof(DatabaseRecord)} please update the {nameof(tasksList)} in {nameof(ClusterStateMachine)}.AddDatabase() and here.");
+           
+            // check if new fields or properties has been added to DatabaseRecord
+            Assert.True(numberOfFields == fields.Count(), 
+                $"New fields has been added to {nameof(DatabaseRecord)} please bump {nameof(numberOfFields)} and update the {nameof(tasksList)} in {nameof(ClusterStateMachine)}.AddDatabase() and here if necessary");
+
+            Assert.True(numberOfProperties == properties.Count(),
+                $"New properties has been added to {nameof(DatabaseRecord)} please bump {nameof(numberOfProperties)} and update the {nameof(tasksList)} in {nameof(ClusterStateMachine)}.AddDatabase() and here if necessary");
         }
     }
 }
