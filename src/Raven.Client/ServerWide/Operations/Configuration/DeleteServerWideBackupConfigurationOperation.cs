d93f7772ceaeaee643cdba8ec533ec08d86da42b
Grisha Kotler <grisha@ayende.com>
2019-05-23 18:59:19 +0300

    RavenDB-12786 - fixing compilation

---
+++ src/Raven.Client/ServerWide/Operations/Configuration/DeleteServerWideBackupConfigurationOperation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using Raven.Client.Documents.Conventions;
+using Raven.Client.Http;
+using Sparrow.Json;
+
+namespace Raven.Client.ServerWide.Operations.Configuration
+{
+    public class DeleteServerWideBackupConfigurationOperation : IServerOperation
+    {
+        public DeleteServerWideBackupConfigurationOperation()
+        {
+        }
+
+        public RavenCommand GetCommand(DocumentConventions conventions, JsonOperationContext context)
+        {
+            return new PutServerWideClientConfigurationCommand(conventions, context);
+        }
+
+        private class PutServerWideClientConfigurationCommand : RavenCommand
+        {
+            public PutServerWideClientConfigurationCommand(DocumentConventions conventions, JsonOperationContext context)
+            {
+                if (conventions == null)
+                    throw new ArgumentNullException(nameof(conventions));
+                if (context == null)
+                    throw new ArgumentNullException(nameof(context));
+            }
+
+            public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+            {
+                url = $"{node.Url}/admin/configuration/server-wide/backup";
+
+                return new HttpRequestMessage
+                {
+                    Method = HttpMethod.Delete
+                };
+            }
+        }
+    }
+}
---
+++ src/Raven.Server/ServerWide/Commands/DeleteServerWideBackupConfigurationCommand.cs
@@ -0,0 +1,17 @@
+﻿using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.ServerWide.Commands
+{
+    public class DeleteServerWideBackupConfigurationCommand : DeleteValueCommand
+    {
+        public DeleteServerWideBackupConfigurationCommand()
+        {
+            Name = ClusterStateMachine.BackupTemplateConfigurationName;
+        }
+
+        public override void VerifyCanExecuteCommand(ServerStore store, TransactionOperationContext context, bool isClusterAdmin)
+        {
+            AssertClusterAdmin(isClusterAdmin);
+        }
+    }
+}
