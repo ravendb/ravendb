053f6d7f207a29a51481f4c701fb6d6ca18f1025
Tal Weiss <tal@ayende.com>
2019-05-28 11:29:54 +0300

    RavenDB-13584 Adding a test for node distribution when node goes down

---
+++ src/Raven.Client/ServerWide/Operations/SetDatabaseDynamicDistribution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Raven.Client.Documents.Conventions;
+using Raven.Client.Http;
+using Raven.Client.Util;
+using Sparrow.Json;
+
+namespace Raven.Client.ServerWide.Operations
+{
+    public class SetDatabaseDynamicDistribution : IServerOperation<bool>
+    {
+        private bool _allowDynamicDistribution;
+        private string _databaseName;
+
+        public SetDatabaseDynamicDistribution(string databaseName, bool allowDynamicDistribution)
+        {
+            _allowDynamicDistribution = allowDynamicDistribution;
+            _databaseName = databaseName;
+        }
+
+        public RavenCommand<bool> GetCommand(DocumentConventions conventions, JsonOperationContext context)
+        {
+            return new SetDatabaseDynamicDistributionCommand(_databaseName, _allowDynamicDistribution);
+        }
+    }
+
+    public class SetDatabaseDynamicDistributionCommand : RavenCommand<bool>, IRaftCommand
+    {
+        private string _databaseName;
+        private bool _allowDynamicDistribution;
+
+        public SetDatabaseDynamicDistributionCommand(string databaseName, bool allowDynamicDistribution)
+        {
+            _databaseName = databaseName;
+            _allowDynamicDistribution = allowDynamicDistribution;
+        }
+
+        public override bool IsReadRequest => false;
+        public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+        {
+   
+            url = $"{node.Url}/admin/databases/dynamic-node-distribution?name={_databaseName}&enable={_allowDynamicDistribution}";
+
+            return new HttpRequestMessage
+            {
+                Method = HttpMethod.Post
+            };
+        }
+
+        public override void SetResponse(JsonOperationContext context, BlittableJsonReaderObject response, bool fromCache)
+        {
+            Result = _allowDynamicDistribution;
+        }
+
+        public string RaftUniqueRequestId => RaftIdGenerator.NewId();
+    }
+}
---
+++ test/SlowTests/Cluster/NodeRedistribution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.ServerWide.Operations;
+using Raven.Server.Config;
+using Tests.Infrastructure;
+using Xunit;
+
+namespace SlowTests.Cluster
+{
+    public class NodeRedistribution: ClusterTestBase
+    {
+        [Fact]
+        public async Task CanRedistributeDatabaseWhenNodeGoesDown()
+        {
+            var databaseName = "NodeRedistribution";
+            var replicaTimeout = 4;
+            var moveToRehabGraceTime = 4;
+            var cluster = await CreateRaftCluster(3, true, 0, customSettings: new Dictionary<string, string>
+            {
+                [RavenConfiguration.GetKey(x => x.Cluster.MoveToRehabGraceTime)] = moveToRehabGraceTime.ToString(),
+                [RavenConfiguration.GetKey(x => x.Cluster.AddReplicaTimeout)] = replicaTimeout.ToString()
+            });
+            var res = await CreateDatabaseInCluster(databaseName, 2, cluster.Leader.WebUrl);
+            var notInDbGroup = cluster.Nodes.First(s => res.Servers.Contains(s) == false);
+            using (var store = new DocumentStore
+            {
+                Database = databaseName,
+                Urls = new[] { cluster.Leader.WebUrl }
+            }.Initialize())
+            {
+                store.Maintenance.Server.Send(new SetDatabaseDynamicDistribution(store.Database, true));
+                await DisposeAndRemoveServer(res.Servers.First());     
+                Thread.Sleep((moveToRehabGraceTime + replicaTimeout)* 2 * 1000);
+                var dbToplogy = (await store.Maintenance.Server.SendAsync(new GetDatabaseRecordOperation(databaseName))).Topology;
+                Assert.True(dbToplogy.AllNodes.Contains(notInDbGroup.ServerStore.NodeTag));
+            }
+
+        }
+    }
+}
