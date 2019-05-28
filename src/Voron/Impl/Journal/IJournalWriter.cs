974a8d3d8260b8e7e038ca2bd4fcf70e12a2ac50
Grisha Kotler <grisha@ayende.com>
2019-05-28 19:51:40 +0300

    RavenDB-13623 - fix write to journal duration

--- src/Voron/Impl/Journal/IJournalWriter.cs
+++ src/Voron/Impl/Journal/IJournalWriter.cs
@@ -6,7 +6,7 @@ namespace Voron.Impl.Journal
 {
     public unsafe interface IJournalWriter : IDisposable
     {
-        void Write(long posBy4Kb, byte* p, int numberOf4Kb);
+        TimeSpan Write(long posBy4Kb, byte* p, int numberOf4Kb);
         int NumberOfAllocated4Kb { get; }
         bool Disposed { get; }
         bool DeleteOnClose { get; set; }
