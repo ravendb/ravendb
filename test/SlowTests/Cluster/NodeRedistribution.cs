3ff7cb19dfa549a82fe49bb642fc2dbf32a2e36f
Tal Weiss <tal@ayende.com>
2019-05-28 12:50:36 +0300

    RavenDB-13584 PR Comments

--- test/SlowTests/Cluster/NodeRedistribution.cs
+++ test/SlowTests/Cluster/NodeRedistribution.cs
@@ -34,7 +34,7 @@ namespace SlowTests.Cluster
                 Urls = new[] { cluster.Leader.WebUrl }
             }.Initialize())
             {
-                store.Maintenance.Server.Send(new SetDatabaseDynamicDistribution(store.Database, true));
+                store.Maintenance.Server.Send(new SetDatabaseDynamicDistributionOperation(store.Database, true));
                 await DisposeAndRemoveServer(res.Servers.First());
                 var wait = (moveToRehabGraceTime + replicaTimeout)* 10 * 1000;
                 if (Debugger.IsAttached)
