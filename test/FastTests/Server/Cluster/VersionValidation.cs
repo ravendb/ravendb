33aa04ce5b8c7ef7e4b0a8d3a48a16e29cc0f078
Karmel Indych <karmeli87@gmail.com>
2019-05-29 17:32:17 +0300

    RavenDB-13629 Fix unable to delete certificate

--- test/FastTests/Server/Cluster/VersionValidation.cs
+++ test/FastTests/Server/Cluster/VersionValidation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Raven.Server.ServerWide;
 using Raven.Server.ServerWide.Commands;
@@ -28,7 +29,8 @@ namespace FastTests.Server.Cluster
 
                 //Console.WriteLine($"[nameof({type.Name})] = 410,");
 
-                exceptions.Add(new InvalidOperationException($"Missing version in '{nameof(ClusterCommandsVersionManager)}.{nameof(ClusterCommandsVersionManager.ClusterCommandsVersions)}' for the command '{type.Name}'."));
+                exceptions.Add(new InvalidOperationException(
+                    $"Missing version in '{nameof(ClusterCommandsVersionManager)}.{nameof(ClusterCommandsVersionManager.ClusterCommandsVersions)}' for the command '{type.Name}'."));
             }
 
             if (exceptions.Count == 0)
@@ -37,5 +39,47 @@ namespace FastTests.Server.Cluster
             throw new AggregateException(exceptions);
         }
 
+        [Fact]
+        public void AllClusterCommandHasCtor()
+        {
+            List<Exception> exceptions = new List<Exception>();
+
+            var assembly = typeof(Raven.Server.ServerWide.ServerStore).GetTypeInfo().Assembly;
+            foreach (var type in assembly.GetTypes())
+            {
+                var typeInfo = type.GetTypeInfo();
+                if (typeInfo.IsAbstract)
+                    continue;
+
+                if (typeInfo.IsSubclassOf(typeof(CommandBase)) == false)
+                    continue;
+
+                var ctors = type
+                    .GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+                if(ctors.Length > 1)
+                    continue;
+
+                var ctor = ctors.FirstOrDefault(x => x.GetParameters().Length == 0);
+
+                if (ctor == null)
+                {
+                    exceptions.Add(new InvalidOperationException(
+                        $"Missing ctor for de-serialization in '{type.Name}' command."));
+                    continue;
+                }
+
+                var instance = (CommandBase)Activator.CreateInstance(type);
+
+                if (instance.UniqueRequestId == null)
+                    exceptions.Add(new InvalidOperationException(
+                        $"Missing ctor with {nameof(CommandBase.RaftCommandIndex)} in '{type.Name}' command."));
+            }
+
+            if (exceptions.Count == 0)
+                return;
+
+            throw new AggregateException(exceptions);
+        }
     }
 }
