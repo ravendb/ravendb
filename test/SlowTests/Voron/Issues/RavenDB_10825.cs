65869bb48cec38ee04c46a0d2b2cacbbdf7f8085
Tal Weiss <tal@ayende.com>
2019-06-04 14:01:41 +0300

    RavenDB-13022 introducing SetBuffers removing Reverse

--- test/SlowTests/Voron/Issues/RavenDB_10825.cs
+++ test/SlowTests/Voron/Issues/RavenDB_10825.cs
@@ -37,7 +37,7 @@ namespace SlowTests.Voron.Issues
                 var cryptoTxState = ((IPagerLevelTransactionState)tx.LowLevelTransaction).CryptoPagerTransactionState.Single().Value;
 
                 // explicitly change the order of items in dictionary so we'll apply the buffer of overflow page before the already freed one
-                cryptoTxState.ReverseBuffers();
+                cryptoTxState.SetBuffers(cryptoTxState.LoadedBuffers.Reverse().ToDictionary(x => x.Key, x => x.Value));
 
                 tx.Commit();
             }
