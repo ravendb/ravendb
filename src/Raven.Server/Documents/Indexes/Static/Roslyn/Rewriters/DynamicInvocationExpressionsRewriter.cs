a2b617457b32d96b41382352964e4415f2c3f268
Egor <egor@ravendb.net>
2019-06-05 18:18:55 +0300

    RavenDB-13650: Add Select & OrderBy overloads for IDictionary, add casts to ToDictionary and Distinct in index expression, use DynamicInvocationExpressionsRewriter in reduce.

--- src/Raven.Server/Documents/Indexes/Static/Roslyn/Rewriters/DynamicInvocationExpressionsRewriter.cs
+++ src/Raven.Server/Documents/Indexes/Static/Roslyn/Rewriters/DynamicInvocationExpressionsRewriter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
@@ -19,9 +20,19 @@ namespace Raven.Server.Documents.Indexes.Static.Roslyn.Rewriters
             {
                 case "Enumerable.Range":
                     return HandleEnumerableRange(node);
+                case "Enumerable.ToDictionary":
+                    return HandleEnumerableToDictionary(node);
+                case "Enumerable.Distinct":
+                    return HandleEnumerableDistinct(node);
+
             }
 
             return base.VisitInvocationExpression(node);
+        }
+
+        private SyntaxNode HandleEnumerableDistinct(InvocationExpressionSyntax node)
+        {
+            return SyntaxFactory.ParseExpression($"((IEnumerable<dynamic>){node})");
         }
 
         private SyntaxNode HandleEnumerableRange(InvocationExpressionSyntax node)
@@ -35,6 +46,11 @@ namespace Raven.Server.Documents.Indexes.Static.Roslyn.Rewriters
             }
 
             return base.VisitInvocationExpression(node);
+        }
+
+        private SyntaxNode HandleEnumerableToDictionary(InvocationExpressionSyntax node)
+        {
+            return SyntaxFactory.ParseExpression($"((Dictionary<dynamic, dynamic>){node})");
         }
 
         private static string GetParentMethod(InvocationExpressionSyntax currentInvocation)
@@ -57,6 +73,47 @@ namespace Raven.Server.Documents.Indexes.Static.Roslyn.Rewriters
 
             member = invocation.Expression as MemberAccessExpressionSyntax;
             return member?.ToString();
+        }
+
+        private void HandleMethod(string method)
+        {
+            switch (method)
+            {
+                case "Select":
+                case "ToDictionary":
+                case "ToLookup":
+                case "GroupBy":
+                case "OrderBy":
+                case "OrderByDescending":
+                case "ThenBy":
+                case "ThenByDescending":
+                case "Recurse":
+                case "SelectMany":
+                case "Sum":
+                case "Average":
+                case "Max":
+                case "Min":
+                case "Any":
+                case "All":
+                case "First":
+                case "FirstOrDefault":
+                case "Last":
+                case "LastOfDefault":
+                case "Single":
+                case "Where":
+                case "Count":
+                case "LongCount":
+                case "SingleOrDefault":
+                case "Zip":
+                case "Aggregate":
+                case "Join":
+                case "GroupJoin":
+                case "TakeWhile":
+                case "SkipWhile":
+                    return;
+            }
+
+            return;
         }
     }
 }
