5fedcd47fba66035a4b9cb07c26e7e6a15c94b48
Or Gabay <GabayOr@RavenDB.net>
2019-06-10 15:38:38 +0300

    RavenDB-13503 Integer addition in projection while using "store files" with indexes

---
+++ test/SlowTests/Issues/RavenDB-13503.cs
@@ -0,0 +1,108 @@
+﻿using System.Linq;
+using FastTests;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Linq;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_13503 : RavenTestBase
+    {
+        [Fact]
+        public void IndexProjectionShouldSupportMathematicalAdditionOfNumberProperties()
+        {
+            using (var store = GetDocumentStore())
+            {
+                Document document;
+                using (var session = store.OpenSession())
+                {
+                    document = new Document
+                    {
+                        Statistics = new DocumentStatistics
+                        {
+                            IntegerNumber1 = 3,
+                            IntegerNumber2 = 2,
+                            DecimalNumber1 = 3,
+                            DecimalNumber2 = 2,
+                            String = "Or"
+                        }
+                    };
+                    session.Store(document);
+                    session.SaveChanges();
+                }
+
+                new DocumentIndex().Execute(store);
+
+                using (var session = store.OpenSession())
+                {
+                    var projection = (from doc in session.Query<DocumentIndex.Result, DocumentIndex>()
+                                .Customize(x => x.WaitForNonStaleResults())
+                                      select new Result
+                                      {
+                                          IntegerSum = doc.IntegerNumber1 + doc.IntegerNumber2,
+                                          IntegerDup = doc.IntegerNumber1 * doc.IntegerNumber2,
+                                          DecimalSum = doc.DecimalNumber1 + doc.DecimalNumber2,
+                                          StringSum = doc.String + doc.IntegerNumber2
+                                      })
+                        .Single();
+
+                    Assert.Equal(5, projection.IntegerSum);
+                    Assert.Equal(5, projection.DecimalSum);
+                    Assert.Equal(6, projection.IntegerDup);
+                    Assert.Equal("Or2", projection.StringSum);
+                }
+            }
+        }
+
+        private class Result
+        {
+            public int IntegerSum { get; set; }
+            public decimal DecimalSum { get; set; }
+            public string StringSum { get; set; }
+            public int IntegerDup { get; set; }
+        }
+
+        private class Document
+        {
+            public string Id;
+            public DocumentStatistics Statistics;
+        }
+
+        private class DocumentStatistics
+        {
+            public int IntegerNumber1;
+            public int IntegerNumber2;
+            public decimal DecimalNumber1;
+            public decimal DecimalNumber2;
+            public string String;
+        }
+
+        private class DocumentIndex : AbstractIndexCreationTask<Document>
+        {
+            public class Result
+            {
+                public int IntegerNumber1;
+                public int IntegerNumber2;
+                public decimal DecimalNumber1;
+                public decimal DecimalNumber2;
+                public  string String;
+            }
+
+            public DocumentIndex()
+            {
+                Map = docs => from doc in docs
+                    select new Result
+                    {
+                        IntegerNumber1 = doc.Statistics.IntegerNumber1,
+                        IntegerNumber2 = doc.Statistics.IntegerNumber2,
+                        DecimalNumber1 = doc.Statistics.DecimalNumber1,
+                        DecimalNumber2 = doc.Statistics.DecimalNumber2,
+                        String = doc.Statistics.String
+                    };
+
+                StoreAllFields(FieldStorage.Yes);
+            }
+
+        }
+    }
+}
