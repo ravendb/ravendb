9e15667b20337b9aadc73ba504c220d17c85954f
Tal Weiss <tal@ayende.com>
2019-06-11 16:01:50 +0300

    RavenDB-13022 adding Configuration.EncryptedTransactionSizeLimit with default value of 64MB

--- src/Raven.Server/Documents/Indexes/Index.cs
+++ src/Raven.Server/Documents/Indexes/Index.cs
@@ -3226,7 +3226,7 @@ namespace Raven.Server.Documents.Indexes
                     {
                         if (DocumentDatabase.IsEncrypted)
                         {
-                            return new Size(64, SizeUnit.Megabytes);
+                            return Configuration.EncryptedTransactionSizeLimit;
                         }
 
                         return null;
