d2239cded36dd22bddccaf80fe19fa14c364a1d1
Karmel Indych <karmeli87@gmail.com>
2019-06-18 11:29:01 +0300

    RavenDB-13707 Backup task should try to stay on the original node

--- src/Raven.Server/Documents/DocumentDatabase.cs
+++ src/Raven.Server/Documents/DocumentDatabase.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Raven.Client;
+using Raven.Client.Documents.Operations.Backups;
 using Raven.Client.Documents.Operations.Configuration;
 using Raven.Client.Documents.Session;
 using Raven.Client.Documents.Smuggler;
@@ -15,6 +16,7 @@ using Raven.Client.Extensions;
 using Raven.Client.ServerWide;
 using Raven.Client.ServerWide.Operations;
 using Raven.Client.Util;
+using Raven.Server.Commercial;
 using Raven.Server.Config;
 using Raven.Server.Documents.ETL;
 using Raven.Server.Documents.Expiration;
@@ -1207,21 +1209,49 @@ namespace Raven.Server.Documents
             DatabaseTopology databaseTopology,
             IDatabaseTask configuration,
             IDatabaseTaskStatus taskStatus,
-            bool useLastResponsibleNodeIfNoAvailableNodes = false)
+            bool keepTaskOnOriginalMemberNode = false)
         {
             var whoseTaskIsIt = databaseTopology.WhoseTaskIsIt(
                 ServerStore.Engine.CurrentState, configuration,
                 getLastResponsibleNode:
-                () => ServerStore.LicenseManager.GetLastResponsibleNodeForTask(
-                    taskStatus,
-                    databaseTopology,
-                    configuration,
-                    NotificationCenter));
+                () =>
+                {
+                    // first time this task is assigned
+                    var lastResponsibleNode = taskStatus?.NodeTag;
+                    if (lastResponsibleNode == null)
+                        return null;
 
-            if (whoseTaskIsIt == null && useLastResponsibleNodeIfNoAvailableNodes)
-                return taskStatus.NodeTag;
+                    // can't redistribute, keep it on the original node
+                    if (ServerStore.LicenseManager.HasHighlyAvailableTasks() == false)
+                    {
+                        RaiseAlertIfNecessary(databaseTopology, configuration, lastResponsibleNode);
+                        return lastResponsibleNode;
+                    }
+
+                    // keep the task on the original node
+                    if (keepTaskOnOriginalMemberNode &&
+                        databaseTopology.Members.Contains(lastResponsibleNode))
+                        return lastResponsibleNode;
+
+                    return null;
+
+                });
+
+            if (whoseTaskIsIt == null && taskStatus is PeriodicBackupStatus)
+                return taskStatus.NodeTag; // we don't want to stop backup process
 
             return whoseTaskIsIt;
+        }
+
+        private void RaiseAlertIfNecessary(DatabaseTopology databaseTopology, IDatabaseTask configuration, string lastResponsibleNode)
+        {
+            // raise alert if redistribution is necessary 
+            if (databaseTopology.Count > 1 &&
+                databaseTopology.Members.Contains(lastResponsibleNode) == false)
+            {
+                var alert = LicenseManager.CreateHighlyAvailableTasksAlert(databaseTopology, configuration, lastResponsibleNode);
+                NotificationCenter.Add(alert);
+            }
         }
 
         public IEnumerable<DatabasePerformanceMetrics> GetAllPerformanceMetrics()
