30b3b783c0209027060fc01c83991c3f4eed1450
aviv86 <aviv@ayende.com>
2019-06-23 17:56:23 +0300

    RavenDB-13539 : MethodDynamicParametersRewriter : adjust the return type of the visited method to be dynamic / IEnumerable<dynamic> when needed

--- src/Raven.Server/Documents/Indexes/Static/Roslyn/Rewriters/MethodDynamicParametersRewriter.cs
+++ src/Raven.Server/Documents/Indexes/Static/Roslyn/Rewriters/MethodDynamicParametersRewriter.cs
@@ -8,7 +8,15 @@ namespace Raven.Server.Documents.Indexes.Static.Roslyn.Rewriters
 {
     public sealed class MethodDynamicParametersRewriter : CSharpSyntaxRewriter
     {
-        public SemanticModel SemanticModel { get; internal set; }
+        public SemanticModel SemanticModel
+        {
+            get => _semanticModel;
+            internal set
+            {
+                _semanticModel = value;
+                IEnumerableSymbol = value.Compilation.GetTypeByMetadataName(IEnumerableString);
+            }
+        }
 
         private const string DynamicString = "dynamic";
 
@@ -20,7 +28,15 @@ namespace Raven.Server.Documents.Indexes.Static.Roslyn.Rewriters
 
         private const string DynamicArrayString = "DynamicArray";
 
-        private INamedTypeSymbol IEnumerableSymbol; 
+        private INamedTypeSymbol IEnumerableSymbol;
+        private SemanticModel _semanticModel;
+
+        private static readonly IdentifierNameSyntax DynamicIdentifier = 
+            SyntaxFactory.IdentifierName(SyntaxFactory.Identifier(DynamicString));
+
+        private static readonly GenericNameSyntax IEnumerableDynamicNameSyntax =
+            SyntaxFactory.GenericName(SyntaxFactory.Identifier("IEnumerable"),
+                SyntaxFactory.TypeArgumentList(new SeparatedSyntaxList<TypeSyntax>().Add(DynamicIdentifier)));
 
         public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node)
         {
@@ -28,16 +44,22 @@ namespace Raven.Server.Documents.Indexes.Static.Roslyn.Rewriters
             var parameterList = RewriteParametersToDynamicTypes(node, out var statements);
 
             //then create new method declaration with the dynamic parameters
-            var methodWithModifiedParameterList = node.WithParameterList(parameterList);
+            var modifiedMethod = node.WithParameterList(parameterList);
 
-            //we had a regular method declaration,
-            //so we simply create new method declaration based on its body
+            if (ShouldModifyReturnType(node, out var newReturnType))
+            {
+                // change return type to dynamic (or IEnumerable<dynamic>)
+                modifiedMethod = modifiedMethod.ReplaceNode(modifiedMethod.ReturnType, newReturnType);
+            }
+
+            //if we had a regular method declaration,
+            //simply create new method declaration based on its body
             if (node.Body != null)
             {
                 if (statements.Count == 0)
-                    return methodWithModifiedParameterList;
-                return methodWithModifiedParameterList.WithBody(methodWithModifiedParameterList.Body
-                    .WithStatements(methodWithModifiedParameterList.Body.Statements.InsertRange(0, statements)));
+                    return modifiedMethod;
+                return modifiedMethod.WithBody(modifiedMethod.Body
+                    .WithStatements(modifiedMethod.Body.Statements.InsertRange(0, statements)));
             }
 
             statements.Add(SyntaxFactory.ReturnStatement(node.ExpressionBody.Expression));
@@ -46,25 +68,52 @@ namespace Raven.Server.Documents.Indexes.Static.Roslyn.Rewriters
             //so we need to create new method declaration from scratch.
             //The following line creates regular method declaration based on the arrow method
             var methodDeclarationSyntax = 
-                    SyntaxFactory.MethodDeclaration(methodWithModifiedParameterList.AttributeLists,
-                        methodWithModifiedParameterList.Modifiers,
-                        methodWithModifiedParameterList.ReturnType,
-                        methodWithModifiedParameterList.ExplicitInterfaceSpecifier,
-                        methodWithModifiedParameterList.Identifier,
-                        methodWithModifiedParameterList.TypeParameterList,
-                        methodWithModifiedParameterList.ParameterList,
-                        methodWithModifiedParameterList.ConstraintClauses,
+                    SyntaxFactory.MethodDeclaration(modifiedMethod.AttributeLists,
+                        modifiedMethod.Modifiers,
+                        modifiedMethod.ReturnType,
+                        modifiedMethod.ExplicitInterfaceSpecifier,
+                        modifiedMethod.Identifier,
+                        modifiedMethod.TypeParameterList,
+                        modifiedMethod.ParameterList,
+                        modifiedMethod.ConstraintClauses,
                         SyntaxFactory.Block(statements),
-                        null, methodWithModifiedParameterList.SemicolonToken);
+                        null, modifiedMethod.SemicolonToken);
 
             return methodDeclarationSyntax;
         }
 
+        private bool ShouldModifyReturnType(MethodDeclarationSyntax node, out SyntaxNode returnType)
+        {
+            returnType = node.ReturnType;
+            var returnSymbol = SemanticModel.GetTypeInfo(returnType);
+
+            if (returnSymbol.Type.SpecialType == SpecialType.System_String ||
+                returnType.ToString() == DynamicString)
+            {
+                return false;
+            }
+
+            if (returnSymbol.Type.AllInterfaces.Contains(IEnumerableSymbol))
+            {
+                returnType = IEnumerableDynamicNameSyntax;
+            }
+
+            else if (returnSymbol.Type.ToString().StartsWith(SystemNamespacePrefix))
+            {
+                return false;
+            }
+
+            else
+            {
+                returnType = DynamicIdentifier;
+            }
+
+            return true;
+        }
+
         private ParameterListSyntax RewriteParametersToDynamicTypes(MethodDeclarationSyntax node, out List<StatementSyntax> statements)
         {
-            IEnumerableSymbol = SemanticModel.Compilation.GetTypeByMetadataName(IEnumerableString);
             statements = new List<StatementSyntax>();
-
             var first = true;
             var sb = new StringBuilder();
             sb.Append('(');
