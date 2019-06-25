6edaa7d6de0917cac604818bce23d2b88f416084
Egor <egor@ravendb.net>
2019-06-25 12:43:19 +0300

    RavenDB-13650: Add DynamicDictionary

--- src/Raven.Server/Documents/Indexes/Persistence/Lucene/IndexedTerms.cs
+++ src/Raven.Server/Documents/Indexes/Persistence/Lucene/IndexedTerms.cs
@@ -124,6 +124,7 @@ namespace Raven.Server.Documents.Indexes.Persistence.Lucene
                         var propertyName = term.Field;
                         if (propertyName.EndsWith(LuceneDocumentConverterBase.ConvertToJsonSuffix) ||
                             propertyName.EndsWith(LuceneDocumentConverterBase.IsArrayFieldSuffix) ||
+                            propertyName.EndsWith(LuceneDocumentConverterBase.IsDictionaryFieldSuffix) ||
                             propertyName.EndsWith(Constants.Documents.Indexing.Fields.RangeFieldSuffix))
                             continue;
 
