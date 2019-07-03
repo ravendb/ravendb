2bca36ebd6d267f7e37f4a715b7db7dd46176eb9
ml054 <marcin@ravendb.net>
2019-07-03 15:58:27 +0200

    RavenDB-13671 Restore backup from S3 - fixed listing

--- src/Raven.Server/Documents/PeriodicBackup/Restore/RestoreFromS3.cs
+++ src/Raven.Server/Documents/PeriodicBackup/Restore/RestoreFromS3.cs
@@ -35,7 +35,7 @@ namespace Raven.Server.Documents.PeriodicBackup.Restore
 
         protected override async Task<List<string>> GetFilesForRestore()
         {
-            var files = await _client.ListObjects(string.IsNullOrEmpty(_remoteFolderName) ? "" : _remoteFolderName + "/", "/", false);
+            var files = await _client.ListObjects(string.IsNullOrEmpty(_remoteFolderName) ? "" : _remoteFolderName + "/", string.Empty, false);
             return files.Select(x => x.FullPath).ToList();
         }
 
