8b6ccba542ef0a09ea2f71b2afc1e4bb00056ed4
Grisha Kotler <grisha@ayende.com>
2019-07-05 08:17:35 +0300

    RavenDB-13438 - fix studio compilation

--- src/Raven.Studio/typescript/models/database/tasks/periodicBackup/retentionPolicy.ts
+++ src/Raven.Studio/typescript/models/database/tasks/periodicBackup/retentionPolicy.ts
@@ -4,7 +4,6 @@ import jsonUtil = require("common/jsonUtil");
 class retentionPolicy {
     disabled = ko.observable<boolean>(false);
     minimumBackupAgeToKeep = ko.observable<string>();
-    minimumBackupsToKeep = ko.observable<number>();
     
     dirtyFlag: () => DirtyFlag;
     
@@ -13,30 +12,24 @@ class retentionPolicy {
     constructor(dto: Raven.Client.Documents.Operations.Backups.RetentionPolicy) {
         this.disabled(dto.Disabled);
         this.minimumBackupAgeToKeep(dto.MinimumBackupAgeToKeep);
-        this.minimumBackupsToKeep(dto.MinimumBackupsToKeep);
-        
-        //this.initObservables();
         
         this.dirtyFlag = new ko.DirtyFlag([
             this.disabled,
             this.minimumBackupAgeToKeep,
-            this.minimumBackupsToKeep
         ], false, jsonUtil.newLineNormalizingHashFunction);
     }
 
     toDto(): Raven.Client.Documents.Operations.Backups.RetentionPolicy {
         return {
             Disabled: this.disabled(),
-            MinimumBackupAgeToKeep: this.minimumBackupAgeToKeep(),
-            MinimumBackupsToKeep: this.minimumBackupsToKeep()
+            MinimumBackupAgeToKeep: this.minimumBackupAgeToKeep()
         }
     }
 
     static empty(): retentionPolicy {
         return new retentionPolicy({
             Disabled: true,
-            MinimumBackupAgeToKeep: null,
-            MinimumBackupsToKeep: null
+            MinimumBackupAgeToKeep: null
         });
     }
 }
