140ab8a3520ee2d2e73de5ac45a00f2320895860
Igal Merhavia <igal@ravendb.net>
2019-07-16 18:12:12 +0300

    RavenDB-13153 Out of memory notifications - week reference for tracking environment notifications

--- src/Raven.Server/NotificationCenter/OutOfMemoryNotifications.cs
+++ src/Raven.Server/NotificationCenter/OutOfMemoryNotifications.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using Raven.Server.NotificationCenter.Notifications;
 using Raven.Server.NotificationCenter.Notifications.Details;
@@ -12,8 +13,8 @@ namespace Raven.Server.NotificationCenter
     public class OutOfMemoryNotifications
     {
         private readonly TimeSpan _updateFrequency = TimeSpan.FromSeconds(15);
-        private readonly ConcurrentDictionary<string, NotificationTime> _notificationTimes = new ConcurrentDictionary<string, NotificationTime>();
-        private readonly ConcurrentDictionary<(StorageEnvironment, Type), NotificationTime> _notificationsMetadata = new ConcurrentDictionary<(StorageEnvironment, Type), NotificationTime>();
+        private readonly ConditionalWeakTable<StorageEnvironment, ConcurrentDictionary<Type, NotificationTime>> _notificationsMetadataTable = 
+            new ConditionalWeakTable<StorageEnvironment, ConcurrentDictionary<Type, NotificationTime>>();
         private readonly NotificationCenter _notificationsCenter;
 
         public OutOfMemoryNotifications(NotificationCenter notificationsCenter)
@@ -21,46 +22,10 @@ namespace Raven.Server.NotificationCenter
             _notificationsCenter = notificationsCenter;
         }
 
-        public void Add(string title, string key, Exception exception)
-        {
-            if (_notificationTimes.TryGetValue(key, out var notificationTime))
-            {
-                if (DateTime.Now - notificationTime.Time < _updateFrequency)
-                    return;
-                
-                if (Interlocked.CompareExchange(ref notificationTime.IsInProgress, 1, 0) == 1)
-                    return;
-                
-                notificationTime.Time = DateTime.Now;
-            }
-            else
-            {
-                notificationTime = new NotificationTime{ Time = DateTime.Now, IsInProgress = 1 };
-                if (_notificationTimes.TryAdd(key, notificationTime) == false)
-                    return;
-            }
-            
-            var message = $"Error message: {exception.Message}";
-            var alert = AlertRaised.Create(
-                null,
-                title,
-                message,
-                AlertType.OutOfMemoryException,
-                NotificationSeverity.Error,
-                key,
-                details: new MessageDetails
-                {
-                    Message = OutOfMemoryDetails(exception)
-                });
-            
-            _notificationsCenter.Add(alert);
-            
-            Volatile.Write(ref notificationTime.IsInProgress, 0);
-        }
-
         public void Add(StorageEnvironment environment, Exception exception)
         {
-            if (_notificationsMetadata.TryGetValue((environment, exception.GetType()), out var notificationMetadata))
+            var notificationsMetadata = _notificationsMetadataTable.GetOrCreateValue(environment);
+            if (notificationsMetadata.TryGetValue(exception.GetType(), out var notificationMetadata))
             {
                 if (DateTime.Now - notificationMetadata.Time < _updateFrequency)
                     return;
@@ -76,11 +41,11 @@ namespace Raven.Server.NotificationCenter
                 {
                     Time = DateTime.Now,
                     IsInProgress = 1
-            };
-                if (_notificationsMetadata.TryAdd((environment, exception.GetType()), notificationMetadata) == false)
+                };
+                if (notificationsMetadata.TryAdd(exception.GetType(), notificationMetadata) == false)
                     return;
 
-                //We are in low of memory so we want to allocate the strings only once
+                //We are in low of memory so we want to minimize allocations
                 notificationMetadata.Key = $"{environment}:{exception.GetType()}";
                 notificationMetadata.Title = $"Out of memory occurred for '{environment}'";
             }
@@ -92,28 +57,28 @@ namespace Raven.Server.NotificationCenter
                 AlertType.OutOfMemoryException,
                 NotificationSeverity.Error,
                 notificationMetadata.Key,
-                details: new MessageDetails
-                {
-                    Message = OutOfMemoryDetails(exception)
-                });
+                OutOfMemoryDetails(exception));
 
             _notificationsCenter.Add(alert);
 
             Volatile.Write(ref notificationMetadata.IsInProgress, 0);
         }
 
-        private static string OutOfMemoryDetails(Exception oome)
+        private static MessageDetails OutOfMemoryDetails(Exception exception)
         {
             var memoryInfo = MemoryInformation.GetMemoryInformationUsingOneTimeSmapsReader();
 
-            return $"Managed memory: {new Size(AbstractLowMemoryMonitor.GetManagedMemoryInBytes(), SizeUnit.Bytes)}, " +
-                   $"Unmanaged allocations: {new Size(AbstractLowMemoryMonitor.GetUnmanagedAllocationsInBytes(), SizeUnit.Bytes)}, " +
-                   $"Shared clean: {memoryInfo.SharedCleanMemory}, " +
-                   $"Working set: {memoryInfo.WorkingSet}, " +
-                   $"Available memory: {memoryInfo.AvailableMemory}, " +
-                   $"Calculated Available memory: {memoryInfo.AvailableWithoutTotalCleanMemory}, " +
-                   $"Total memory: {memoryInfo.TotalPhysicalMemory} {Environment.NewLine}" +
-                   $"Error: {oome}";
+            return new MessageDetails
+            {
+                Message = $"Managed memory: {new Size(AbstractLowMemoryMonitor.GetManagedMemoryInBytes(), SizeUnit.Bytes)}, " +
+                          $"Unmanaged allocations: {new Size(AbstractLowMemoryMonitor.GetUnmanagedAllocationsInBytes(), SizeUnit.Bytes)}, " +
+                          $"Shared clean: {memoryInfo.SharedCleanMemory}, " +
+                          $"Working set: {memoryInfo.WorkingSet}, " +
+                          $"Available memory: {memoryInfo.AvailableMemory}, " +
+                          $"Calculated Available memory: {memoryInfo.AvailableWithoutTotalCleanMemory}, " +
+                          $"Total memory: {memoryInfo.TotalPhysicalMemory} {Environment.NewLine}" +
+                          $"Error: {exception}"
+            };
         }
         
         private class NotificationTime
