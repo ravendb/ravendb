3f79e07f22431603b93da419c2a8514fdb41d2e0
talweiss1982 <tal.weiss.1982@gmail.com>
2019-07-17 13:20:20 +0300

    RavenDB-13655 making IndexingWhenEncryptedTransactionSizeLimitLimitExceeded pass again Also added consideration for read transaction size when running in encrypted environment

--- src/Raven.Server/Documents/Indexes/Index.cs
+++ src/Raven.Server/Documents/Indexes/Index.cs
@@ -3276,6 +3276,9 @@ namespace Raven.Server.Documents.Indexes
         {
             var txAllocationsInBytes = UpdateThreadAllocations(indexingContext, indexWriteOperation, stats, updateReduceStats: false);
 
+            //We need to take the read transaction encryption size into account as we might read alot of document and produce very little indexing output.
+            txAllocationsInBytes += documentsOperationContext.Transaction.InnerTransaction.LowLevelTransaction.TotalEncryptionBufferSize.GetValue(SizeUnit.Bytes);
+
             if (_indexDisabled)
             {
                 stats.RecordMapCompletedReason("Index was disabled");
