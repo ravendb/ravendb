3c045dd78af9c7ef1a86af17ac26344813190a15
Danielle Greenberg <danielle.9897@gmail.com>
2019-07-21 14:53:46 +0300

    RavenDB-13788 Add Retention Policy in edit backup configuration

--- src/Raven.Studio/typescript/models/database/tasks/periodicBackup/retentionPolicy.ts
+++ src/Raven.Studio/typescript/models/database/tasks/periodicBackup/retentionPolicy.ts
@@ -1,28 +1,54 @@
-﻿import setupEncryptionKey = require("viewmodels/resources/setupEncryptionKey");
-import jsonUtil = require("common/jsonUtil");
+﻿import jsonUtil = require("common/jsonUtil");
+import generalUtils = require("common/generalUtils");
 
 class retentionPolicy {
     disabled = ko.observable<boolean>(false);
-    minimumBackupAgeToKeep = ko.observable<string>();
+    minimumBackupAgeToKeep = ko.observable<number>();
+
+    retentionPolicyEnabled = ko.observable<boolean>(false);
+    humaneRetentionDescription: KnockoutComputed<string>;
     
     dirtyFlag: () => DirtyFlag;
     
-    validationGroup: KnockoutValidationGroup;
-    
     constructor(dto: Raven.Client.Documents.Operations.Backups.RetentionPolicy) {
         this.disabled(dto.Disabled);
-        this.minimumBackupAgeToKeep(dto.MinimumBackupAgeToKeep);
+        this.minimumBackupAgeToKeep(dto.MinimumBackupAgeToKeep ? generalUtils.timeSpanToSeconds(dto.MinimumBackupAgeToKeep) : 0);
         
+        this.initObservables();
+        this.initValidation();
+    }
+
+    private initObservables() {
+        this.retentionPolicyEnabled(!this.disabled());
+        
+        this.humaneRetentionDescription = ko.pureComputed(() => {
+            if (this.minimumBackupAgeToKeep() === 0) {
+                return "No backups will be removed.";
+            }
+
+            const retentionTimeHumane = generalUtils.formatTimeSpan(this.minimumBackupAgeToKeep() * 1000, true);
+            return `Bacukups that are older than <strong>${retentionTimeHumane}</strong> will be removed on the next scheduled backup task`;
+        });
+
         this.dirtyFlag = new ko.DirtyFlag([
-            this.disabled,
+            this.retentionPolicyEnabled,
             this.minimumBackupAgeToKeep,
         ], false, jsonUtil.newLineNormalizingHashFunction);
     }
-
+    
+    private initValidation() {
+        this.minimumBackupAgeToKeep.extend({
+            validation: [{
+                validator: (val: number) => val >= 86400, // 1 day === 86400 seconds
+                message: "Retention period must be greater than 1 day"
+            }]
+        });
+    }
+    
     toDto(): Raven.Client.Documents.Operations.Backups.RetentionPolicy {
         return {
-            Disabled: this.disabled(),
-            MinimumBackupAgeToKeep: this.minimumBackupAgeToKeep()
+            Disabled: !this.retentionPolicyEnabled(),
+            MinimumBackupAgeToKeep: this.minimumBackupAgeToKeep() === 0 ? null : generalUtils.formatAsTimeSpan(this.minimumBackupAgeToKeep() * 1000)
         }
     }
 
