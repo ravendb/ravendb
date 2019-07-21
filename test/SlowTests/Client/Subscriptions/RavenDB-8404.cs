0948bae8b5b34a49a406683a0f57a5f1e0963029
Tal Weiss <tal@ayende.com>
2019-07-21 14:34:57 +0300

    RavenDB-13655 fixing AVE caused by forced revision command * this PR includes fixes for subscription tests in 32bit

--- test/SlowTests/Client/Subscriptions/RavenDB-8404.cs
+++ test/SlowTests/Client/Subscriptions/RavenDB-8404.cs
@@ -94,7 +94,10 @@ namespace SlowTests.Client.Subscriptions
                 _ = subscription.Run(x =>
                 {
                     users.AddRange(x.Items.Select(i => i.Result));
-                    amre.Set();
+                    if (users.Count == 3)
+                    {
+                        amre.Set();
+                    }
                 });
 
                 Assert.True(await amre.WaitAsync(_reasonableWaitTime));
@@ -137,7 +140,10 @@ namespace SlowTests.Client.Subscriptions
                 _ = subscription.Run(x =>
                 {
                     users.AddRange(x.Items.Select(i => i.Result));
-                    amre.Set();
+                    if (users.Count == 3)
+                    {
+                        amre.Set();
+                    }
                 });
 
                 Assert.True(await amre.WaitAsync(_reasonableWaitTime));
