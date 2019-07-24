ae78c0e3207f5e079422fe534594319e8af0853a
Arkadiusz Palinski <arek@ravendb.net>
2019-07-24 09:52:42 +0200

    RavenDB-13130 When freeing a page and doing validation we need to take into account the actual number of allocated pages which is different on 32bits

--- src/Voron/Data/Tables/NewPageAllocator.cs
+++ src/Voron/Data/Tables/NewPageAllocator.cs
@@ -234,8 +234,10 @@ namespace Voron.Data.Tables
                         ThrowInvalidPageReleased(pageNumber);
                 }
 
+                var numberOfPagesActuallyAllocatedInSection = GetNumberOfPagesToAllocate();
+
                 if (it.CurrentKey > pageNumber ||
-                    it.CurrentKey + NumberOfPagesInSection <= pageNumber)
+                    it.CurrentKey + numberOfPagesActuallyAllocatedInSection <= pageNumber)
                     ThrowInvalidPageReleased(pageNumber);
 
                 var positionInBuffer = (int)(pageNumber - it.CurrentKey);
