f6664ab13b19ebdcba6dc0e95b357b66de7aae92
Egor <egor@ravendb.net>
2019-08-06 12:33:33 +0300

    RavenDB-13894: Add getMetadata() and Id() functions to javascript index

---
+++ src/Raven.Server/Documents/JavaScriptUtils.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Jint;
+using Jint.Native;
+using Jint.Native.Object;
+using Lucene.Net.Store;
+using Raven.Client;
+using Raven.Server.Documents.Indexes.Static;
+using Raven.Server.Documents.Patch;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Server.Documents
+{
+    public class JavaScriptUtils
+    {
+        private JsonOperationContext _jsonCtx;
+        private readonly ScriptRunner _runner;
+        private readonly List<IDisposable> _disposables = new List<IDisposable>();
+        public readonly Engine ScriptEngine;
+        public bool ReadOnly;
+
+        private JsonOperationContext Context => _jsonCtx ?? CurrentIndexingScope.Current.IndexContext;
+
+        public JavaScriptUtils(JsonOperationContext ctx, ScriptRunner runner, Engine engine, bool readOnly = false)
+        {
+            _jsonCtx = ctx;
+            _runner = runner;
+            ScriptEngine = engine;
+            ReadOnly = readOnly;
+        }
+
+        internal JsValue GetMetadata(JsValue self, JsValue[] args)
+        {
+            if (args.Length != 1 || !(args[0].AsObject() is BlittableObjectInstance boi))
+                throw new InvalidOperationException("getMetadata(doc) must be called with a single entity argument");
+
+            if (!(boi.Blittable[Constants.Documents.Metadata.Key] is BlittableJsonReaderObject metadata))
+                return JsValue.Null;
+
+            metadata.Modifications = new DynamicJsonValue
+            {
+                [Constants.Documents.Metadata.ChangeVector] = boi.ChangeVector,
+                [Constants.Documents.Metadata.Id] = boi.DocumentId,
+                [Constants.Documents.Metadata.LastModified] = boi.LastModified,
+            };
+
+            metadata = Context.ReadObject(metadata, boi.DocumentId);
+            return TranslateToJs(ScriptEngine, Context, metadata);
+        }
+
+        internal JsValue GetDocumentId(JsValue self, JsValue[] args)
+        {
+            if (args.Length != 1 && args.Length != 2) //length == 2 takes into account Query Arguments that can be added to args
+                throw new InvalidOperationException("id(doc) must be called with a single argument");
+
+            if (args[0].IsNull() || args[0].IsUndefined())
+                return args[0];
+
+            if (args[0].IsObject() == false)
+                throw new InvalidOperationException("id(doc) must be called with an object argument");
+
+            var objectInstance = args[0].AsObject();
+
+            if (objectInstance is BlittableObjectInstance doc && doc.DocumentId != null)
+                return doc.DocumentId;
+
+            var jsValue = objectInstance.Get(Constants.Documents.Metadata.Key);
+            // search either @metadata.@id or @id
+            var metadata = jsValue.IsObject() == false ? objectInstance : jsValue.AsObject();
+            var value = metadata.Get(Constants.Documents.Metadata.Id);
+            if (value.IsString() == false)
+            {
+                // search either @metadata.Id or Id
+                value = metadata.Get(Constants.Documents.Metadata.IdProperty);
+                if (value.IsString() == false)
+                    return JsValue.Null;
+            }
+            return value;
+        }
+
+        internal JsValue TranslateToJs(Engine engine, JsonOperationContext context, object o)
+        {
+            if (o is Tuple<Document, Lucene.Net.Documents.Document, IState> t)
+            {
+                var d = t.Item1;
+                return new BlittableObjectInstance(engine, null, Clone(d.Data, context), d.Id, d.LastModified, d.ChangeVector)
+                {
+                    LuceneDocument = t.Item2,
+                    LuceneState = t.Item3
+                };
+            }
+            if (o is Document doc)
+            {
+                return new BlittableObjectInstance(engine, null, Clone(doc.Data, context), doc.Id, doc.LastModified);
+            }
+            if (o is DocumentConflict dc)
+            {
+                return new BlittableObjectInstance(engine, null, Clone(dc.Doc, context), dc.Id, dc.LastModified);
+            }
+
+            if (o is BlittableJsonReaderObject json)
+            {
+                return new BlittableObjectInstance(engine, null, Clone(json, context), null, null);
+            }
+
+            if (o == null)
+                return Undefined.Instance;
+            if (o is long lng)
+                return lng;
+            if (o is BlittableJsonReaderArray bjra)
+            {
+                var jsArray = engine.Array.Construct(Array.Empty<JsValue>());
+                var args = new JsValue[1];
+                for (var i = 0; i < bjra.Length; i++)
+                {
+                    args[0] = TranslateToJs(engine, context, bjra[i]);
+                    engine.Array.PrototypeObject.Push(jsArray, args);
+                }
+                return jsArray;
+            }
+            if (o is List<object> list)
+            {
+                var jsArray = engine.Array.Construct(Array.Empty<JsValue>());
+                var args = new JsValue[1];
+                for (var i = 0; i < list.Count; i++)
+                {
+                    args[0] = TranslateToJs(engine, context, list[i]);
+                    engine.Array.PrototypeObject.Push(jsArray, args);
+                }
+                return jsArray;
+            }
+            if (o is List<Document> docList)
+            {
+                var jsArray = engine.Array.Construct(Array.Empty<JsValue>());
+                var args = new JsValue[1];
+                for (var i = 0; i < docList.Count; i++)
+                {
+                    args[0] = new BlittableObjectInstance(engine, null, Clone(docList[i].Data, context), docList[i].Id, docList[i].LastModified);
+                    engine.Array.PrototypeObject.Push(jsArray, args);
+                }
+                return jsArray;
+            }
+            // for admin
+            if (o is RavenServer || o is DocumentDatabase)
+            {
+                AssertAdminScriptInstance();
+                return JsValue.FromObject(engine, o);
+            }
+            if (o is ObjectInstance j)
+                return j;
+            if (o is bool b)
+                return b ? JsBoolean.True : JsBoolean.False;
+            if (o is int integer)
+                return integer;
+            if (o is double dbl)
+                return dbl;
+            if (o is string s)
+                return s;
+            if (o is LazyStringValue ls)
+                return ls.ToString();
+            if (o is LazyCompressedStringValue lcs)
+                return lcs.ToString();
+            if (o is LazyNumberValue lnv)
+            {
+                return BlittableObjectInstance.BlittableObjectProperty.GetJSValueForLazyNumber(engine, lnv);
+            }
+            if (o is JsValue js)
+                return js;
+            throw new InvalidOperationException("No idea how to convert " + o + " to JsValue");
+        }
+
+        private void AssertAdminScriptInstance()
+        {
+            if (_runner._enableClr == false)
+                throw new InvalidOperationException("Unable to run admin scripts using this instance of the script runner, the EnableClr is set to false");
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private BlittableJsonReaderObject Clone(BlittableJsonReaderObject origin, JsonOperationContext context)
+        {
+            if (ReadOnly)
+                return origin;
+
+            var noCache = origin.NoCache;
+            origin.NoCache = true;
+            // RavenDB-8286
+            // here we need to make sure that we aren't sending a value to 
+            // the js engine that might be modified by the actions of the js engine
+            // for example, calling put() might cause the original data to change 
+            // because we defrag the data that we looked at. We are handling this by
+            // ensuring that we have our own, safe, copy.
+            var cloned = origin.Clone(context);
+            cloned.NoCache = true;
+            _disposables.Add(cloned);
+
+            origin.NoCache = noCache;
+            return cloned;
+        }
+
+        public void DisposeClonedDocuments()
+        {
+            foreach (var disposable in _disposables)
+                disposable.Dispose();
+            _disposables.Clear();
+        }
+
+        public void SetContext(JsonOperationContext ctx)
+        {
+            _jsonCtx = ctx;
+        }
+    }
+}
