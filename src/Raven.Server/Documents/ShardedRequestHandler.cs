993f9a0c12a33b633ecccc616fedd2c519d796b7
Ayende Rahien <Ayende@ayende.com>
2019-08-08 23:16:54 +0300

    RavenDB-13053 - Use Crc16 to compute the shard key

--- src/Raven.Server/Documents/ShardedRequestHandler.cs
+++ src/Raven.Server/Documents/ShardedRequestHandler.cs
@@ -1,9 +1,53 @@
-﻿using Raven.Server.Web;
+﻿using System;
+using System.Runtime.InteropServices;
+using Raven.Server.Web;
 
 namespace Raven.Server.Documents
 {
     public class ShardedRequestHandler : RequestHandler
     {
+        protected static ushort GetShardId(string key)
+        {
+            return Crc16.ComputeChecksum(MemoryMarshal.AsBytes(key.AsSpan()));
+        }
 
+        public static class Crc16
+        {
+            const ushort Polynomial = 0xA001;
+            static readonly ushort[] Table = new ushort[256];
+
+            public static ushort ComputeChecksum(ReadOnlySpan<byte> bytes)
+            {
+                ushort crc = 0;
+                for (int i = 0; i < bytes.Length; ++i)
+                {
+                    byte index = (byte)(crc ^ bytes[i]);
+                    crc = (ushort)((crc >> 8) ^ Table[index]);
+                }
+                return crc;
+            }
+
+            static Crc16()
+            {
+                for (ushort i = 0; i < Table.Length; ++i)
+                {
+                    ushort value = 0;
+                    var temp = i;
+                    for (byte j = 0; j < 8; ++j)
+                    {
+                        if (((value ^ temp) & 0x0001) != 0)
+                        {
+                            value = (ushort)((value >> 1) ^ Polynomial);
+                        }
+                        else
+                        {
+                            value >>= 1;
+                        }
+                        temp >>= 1;
+                    }
+                    Table[i] = value;
+                }
+            }
+        }
     }
 }
