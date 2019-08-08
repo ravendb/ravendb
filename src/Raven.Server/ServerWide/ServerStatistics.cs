49d8fe4c2688578b7b15356a9cb9c6ededb5ec98
Pawel Pekrol <ppekrol@ravendb.net>
2019-08-08 14:57:21 +0200

    RavenDB-13892 added /admin/stats

--- src/Raven.Server/ServerWide/ServerStatistics.cs
+++ src/Raven.Server/ServerWide/ServerStatistics.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Raven.Client.Util;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
 
 namespace Raven.Server.ServerWide
 {
@@ -10,8 +12,31 @@ namespace Raven.Server.ServerWide
             StartUpTime = SystemTime.UtcNow;
         }
 
+        public TimeSpan UpTime => SystemTime.UtcNow - StartUpTime;
+
         public readonly DateTime StartUpTime;
-        
+
         public DateTime? LastRequestTime { get; set; }
+
+        public void WriteTo(BlittableJsonTextWriter writer)
+        {
+            writer.WriteStartObject();
+
+            writer.WritePropertyName(nameof(UpTime));
+            writer.WriteString(UpTime.ToString("c"));
+            writer.WriteComma();
+
+            writer.WritePropertyName(nameof(StartUpTime));
+            writer.WriteDateTime(StartUpTime, isUtc: true);
+            writer.WriteComma();
+
+            writer.WritePropertyName(nameof(LastRequestTime));
+            if (LastRequestTime.HasValue)
+                writer.WriteDateTime(LastRequestTime.Value, isUtc: true);
+            else
+                writer.WriteNull();
+
+            writer.WriteEndObject();
+        }
     }
 }
