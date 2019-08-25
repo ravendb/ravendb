efcbbd17eb1ac902025756066d81b417ebee07cf
Karmel Indych <karmeli87@gmail.com>
2019-08-25 18:07:21 +0300

    RavenDB-13963 Specific document with attachment insertion pattern cause replication of many items in a single batch

--- test/SlowTests/Client/Attachments/AttachmentsReplication.cs
+++ test/SlowTests/Client/Attachments/AttachmentsReplication.cs
@@ -1213,5 +1213,67 @@ namespace SlowTests.Client.Attachments
                 Assert.Equal(1, count);
             }
         }
+
+        [Fact]
+        public async Task RavenDB_13963()
+        {
+            using (var store1 = GetDocumentStore())
+            using (var store2 = GetDocumentStore())
+            {
+                var database1 = await GetDocumentDatabaseInstanceFor(store1);
+                database1.Configuration.Replication.MaxItemsCount = 1;
+                database1.ReplicationLoader.DebugWaitAndRunReplicationOnce = new ManualResetEventSlim();
+
+                using (var session = store1.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User {Name = "Karmel"}, "users/1");
+                    using (var a1 = new MemoryStream(new byte[] {1, 2, 3}))
+                    {
+                        session.Advanced.Attachments.Store("users/1", "a1", a1, "a1/png");
+                        await session.SaveChangesAsync();
+                    }
+                }
+
+                using (var session = store1.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User { Name = "Karmel" }, "users/2");
+                    using (var a1 = new MemoryStream(new byte[] { 1, 2, 3, 4 }))
+                    {
+                        session.Advanced.Attachments.Store("users/2", "a2", a1, "a2/png");
+                        await session.SaveChangesAsync();
+                    }
+                }
+
+                await SetupReplicationAsync(store1, store2);
+                database1.ReplicationLoader.DebugWaitAndRunReplicationOnce.Set();
+
+                Assert.True(WaitForDocument(store2, "users/1"));
+
+                using (var session = store2.OpenAsyncSession())
+                {
+                    var user = await session.LoadAsync<User>("users/1");
+                    Assert.NotNull(user);
+
+                    var metadata = session.Advanced.GetMetadataFor(user);
+                    Assert.Contains(DocumentFlags.HasAttachments.ToString(), metadata.GetString(Constants.Documents.Metadata.Flags));
+                    var attachments = metadata.GetObjects(Constants.Documents.Metadata.Attachments);
+                    Assert.Equal(1, attachments.Length);
+
+                    Assert.Null(await session.LoadAsync<User>("users/2"));
+                }
+
+                database1.ReplicationLoader.DebugWaitAndRunReplicationOnce.Set();
+                Assert.True(WaitForDocument(store2, "users/2"));
+
+                using (var session = store2.OpenAsyncSession())
+                {
+                    var user = await session.LoadAsync<User>("users/2");
+                    var metadata = session.Advanced.GetMetadataFor(user);
+                    Assert.Contains(DocumentFlags.HasAttachments.ToString(), metadata.GetString(Constants.Documents.Metadata.Flags));
+                    var attachments = metadata.GetObjects(Constants.Documents.Metadata.Attachments);
+                    Assert.Equal(1, attachments.Length);
+                }
+            }
+        }
     }
 }
