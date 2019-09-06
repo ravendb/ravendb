7ff21dfb8e3c0a6962838b9780395b45ea2a1406
Arkadiusz Palinski <arek@ravendb.net>
2019-09-06 15:49:34 +0200

    RavenDB-13972 Iteration over document conflicts makes use of pulsed enumerator in backup

---
+++ src/Raven.Server/Smuggler/Documents/Iteration/DocumentConflictsIterationState.cs
@@ -0,0 +1,21 @@
+﻿using Raven.Server.Documents;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Smuggler.Documents.Iteration
+{
+    public class DocumentConflictsIterationState : IterationState<DocumentConflict>
+    {
+        public DocumentConflictsIterationState(DocumentsOperationContext context) : base(context)
+        {
+        }
+
+        public long StartEtag;
+
+        public override void OnMoveNext(DocumentConflict current)
+        {
+            StartEtag = current.Etag + 1;
+
+            ReadCount++;
+        }
+    }
+}
