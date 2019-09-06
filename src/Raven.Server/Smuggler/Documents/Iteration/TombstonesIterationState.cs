81d6a6613ff1a7dd9e969ca5e9009c69af8aab48
Arkadiusz Palinski <arek@ravendb.net>
2019-09-06 15:08:23 +0200

    RavenDB-13972 Iteration over tombstones in backup makes usage of pulsed enumerator

---
+++ src/Raven.Server/Smuggler/Documents/Iteration/TombstonesIterationState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Raven.Server.Documents;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Smuggler.Documents.Iteration
+{
+    public class TombstonesIterationState : IterationState<Tombstone>
+    {
+        public TombstonesIterationState(DocumentsOperationContext context) : base(context)
+        {
+        }
+
+        public long StartEtag;
+
+        public Dictionary<string, long> StartEtagByCollection = new Dictionary<string, long>();
+
+        public string CurrentCollection;
+
+        public override void OnMoveNext(Tombstone current)
+        {
+            if (StartEtagByCollection.Count != 0)
+            {
+                StartEtagByCollection[CurrentCollection] = current.Etag + 1;
+            }
+            else
+            {
+                StartEtag = current.Etag + 1;
+            }
+
+            ReadCount++;
+        }
+    }
+}
