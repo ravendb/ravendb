d738833930cb72168f631726306a5879f1426b82
Pawel Pekrol <ppekrol@ravendb.net>
2019-09-13 12:57:47 +0200

    RDBCL-772 persist server statistics

--- src/Raven.Server/Routing/RequestRouter.cs
+++ src/Raven.Server/Routing/RequestRouter.cs
@@ -43,7 +43,6 @@ namespace Raven.Server.Routing
             AllRoutes = new List<RouteInformation>(routes.Values);
         }
 
-
         public RouteInformation GetRoute(string method, string path, out RouteMatch match)
         {
             var tryMatch = _trie.TryMatch(method, path);
@@ -76,9 +75,6 @@ namespace Raven.Server.Routing
 
             try
             {
-                if (tryMatch.Value.SkipLastRequestTimeUpdate == false)
-                    _ravenServer.Statistics.LastRequestTime = SystemTime.UtcNow;
-
                 if (handler == null)
                 {
                     var auditLog = LoggingSource.AuditLog.IsInfoEnabled ? LoggingSource.AuditLog.GetLogger("RequestRouter", "Audit") : null;
@@ -114,11 +110,26 @@ namespace Raven.Server.Routing
                     skipAuthorization = context.Request.Method == "OPTIONS";
                 }
 
-                if (_ravenServer.Configuration.Security.AuthenticationEnabled && skipAuthorization == false)
+                var status = RavenServer.AuthenticationStatus.ClusterAdmin;
+                bool authResult = true;
+                try
                 {
-                    var authResult = TryAuthorize(tryMatch.Value, context, reqCtx.Database);
-                    if (authResult == false)
-                        return;
+                    if (_ravenServer.Configuration.Security.AuthenticationEnabled && skipAuthorization == false)
+                    {
+                        authResult = TryAuthorize(tryMatch.Value, context, reqCtx.Database, out status);
+                        if (authResult == false)
+                            return;
+                    }
+                }
+                finally
+                {
+                    if (tryMatch.Value.SkipLastRequestTimeUpdate == false)
+                    {
+                        var now = SystemTime.UtcNow;
+                        _ravenServer.Statistics.LastRequestTime = now;
+                        if (skipAuthorization == false && authResult && status != RavenServer.AuthenticationStatus.ClusterAdmin)
+                            _ravenServer.Statistics.LastUserRequestTime = now;
+                    }
                 }
 
                 if (reqCtx.Database != null)
@@ -195,7 +206,7 @@ namespace Raven.Server.Routing
             }
         }
 
-        private bool TryAuthorize(RouteInformation route, HttpContext context, DocumentDatabase database)
+        private bool TryAuthorize(RouteInformation route, HttpContext context, DocumentDatabase database, out RavenServer.AuthenticationStatus authenticationStatus)
         {
             var feature = context.Features.Get<IHttpAuthenticationFeature>() as RavenServer.AuthenticateConnection;
 
@@ -232,13 +243,12 @@ namespace Raven.Server.Routing
                                     auditLog.Info(msg);
                                 });
                             }
-
                         }
                     }
                 }
             }
 
-            var authenticationStatus = feature?.Status;
+            authenticationStatus = feature?.Status ?? RavenServer.AuthenticationStatus.None;
             switch (route.AuthorizationStatus)
             {
                 case AuthorizationStatus.UnauthenticatedClients:
@@ -247,7 +257,6 @@ namespace Raven.Server.Routing
                     {
                         switch (authenticationStatus)
                         {
-                            case null:
                             case RavenServer.AuthenticationStatus.NoCertificateProvided:
                             case RavenServer.AuthenticationStatus.Expired:
                             case RavenServer.AuthenticationStatus.NotYetValid:
@@ -260,6 +269,7 @@ namespace Raven.Server.Routing
                     }
 
                     return true;
+
                 case AuthorizationStatus.ClusterAdmin:
                 case AuthorizationStatus.Operator:
                 case AuthorizationStatus.ValidUser:
@@ -267,7 +277,6 @@ namespace Raven.Server.Routing
                 case AuthorizationStatus.RestrictedAccess:
                     switch (authenticationStatus)
                     {
-                        case null:
                         case RavenServer.AuthenticationStatus.NoCertificateProvided:
                         case RavenServer.AuthenticationStatus.Expired:
                         case RavenServer.AuthenticationStatus.NotYetValid:
@@ -280,7 +289,7 @@ namespace Raven.Server.Routing
                             // we allow an access to the restricted endpoints with an unfamiliar certificate, since we will authorize it at the endpoint level
                             if (route.AuthorizationStatus == AuthorizationStatus.RestrictedAccess)
                                 return true;
-                            goto case null;
+                            goto case RavenServer.AuthenticationStatus.None;
 
                         case RavenServer.AuthenticationStatus.Allowed:
                             if (route.AuthorizationStatus == AuthorizationStatus.Operator || route.AuthorizationStatus == AuthorizationStatus.ClusterAdmin)
@@ -296,8 +305,10 @@ namespace Raven.Server.Routing
                             if (route.AuthorizationStatus == AuthorizationStatus.ClusterAdmin)
                                 goto case RavenServer.AuthenticationStatus.None;
                             return true;
+
                         case RavenServer.AuthenticationStatus.ClusterAdmin:
                             return true;
+
                         default:
                             throw new ArgumentOutOfRangeException();
                     }
@@ -370,9 +381,11 @@ namespace Raven.Server.Routing
                 case AuthorizationStatus.ClusterAdmin:
                     message += " ClusterAdmin access is required but not given to this certificate";
                     break;
+
                 case AuthorizationStatus.Operator:
                     message += " Operator/ClusterAdmin access is required but not given to this certificate";
                     break;
+
                 case AuthorizationStatus.DatabaseAdmin:
                     message += " DatabaseAdmin access is required but not given to this certificate";
                     break;
@@ -403,7 +416,7 @@ namespace Raven.Server.Routing
         private static void DrainRequest(JsonOperationContext ctx, HttpContext context)
         {
             if (context.Response.Headers.TryGetValue("Connection", out Microsoft.Extensions.Primitives.StringValues value) && value == "close")
-                return; // don't need to drain it, the connection will close 
+                return; // don't need to drain it, the connection will close
 
             using (ctx.GetManagedBuffer(out JsonOperationContext.ManagedPinnedBuffer buffer))
             {
--- src/Raven.Server/ServerWide/ServerStatistics.cs
+++ src/Raven.Server/ServerWide/ServerStatistics.cs
@@ -1,22 +1,33 @@
 ﻿using System;
+using System.IO;
 using Raven.Client.Util;
+using Raven.Server.Json;
+using Raven.Server.ServerWide.Context;
 using Sparrow.Json;
-using Sparrow.Json.Parsing;
+using Sparrow.Logging;
 
 namespace Raven.Server.ServerWide
 {
     public class ServerStatistics
     {
+        private static readonly TimeSpan PersistFrequency = TimeSpan.FromMinutes(15);
+
+        private DateTime _lastPersist;
+
         public ServerStatistics()
         {
-            StartUpTime = SystemTime.UtcNow;
+            StartUpTime = _lastPersist = SystemTime.UtcNow;
         }
 
+        [JsonDeserializationIgnore]
         public TimeSpan UpTime => SystemTime.UtcNow - StartUpTime;
 
+        [JsonDeserializationIgnore]
         public readonly DateTime StartUpTime;
 
-        public DateTime? LastRequestTime { get; set; }
+        public DateTime? LastRequestTime;
+
+        public DateTime? LastUserRequestTime;
 
         public void WriteTo(BlittableJsonTextWriter writer)
         {
@@ -35,8 +46,94 @@ namespace Raven.Server.ServerWide
                 writer.WriteDateTime(LastRequestTime.Value, isUtc: true);
             else
                 writer.WriteNull();
+            writer.WriteComma();
+
+            writer.WritePropertyName(nameof(LastUserRequestTime));
+            if (LastUserRequestTime.HasValue)
+                writer.WriteDateTime(LastUserRequestTime.Value, isUtc: true);
+            else
+                writer.WriteNull();
 
             writer.WriteEndObject();
+        }
+
+        internal unsafe void Load(TransactionContextPool contextPool, Logger logger)
+        {
+            try
+            {
+                using (contextPool.AllocateOperationContext(out TransactionOperationContext context))
+                using (var tx = context.OpenReadTransaction())
+                {
+                    var tree = tx.InnerTransaction.ReadTree(nameof(ServerStatistics));
+                    if (tree == null)
+                        return;
+
+                    var result = tree.Read(nameof(ServerStatistics));
+                    if (result == null)
+                        return;
+
+                    using (var json = context.ReadForMemory(result.Reader.AsStream(), nameof(ServerStatistics)))
+                    {
+                        var stats = JsonDeserializationServer.ServerStatistics(json);
+
+                        LastRequestTime = stats.LastRequestTime;
+                        LastUserRequestTime = stats.LastUserRequestTime;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                if (logger.IsInfoEnabled)
+                    logger.Info("Could not load server statistics.", e);
+            }
+        }
+
+        internal void Persist(TransactionContextPool contextPool, Logger logger)
+        {
+            lock (this)
+            {
+                try
+                {
+                    using (contextPool.AllocateOperationContext(out TransactionOperationContext context))
+                    using (var tx = context.OpenWriteTransaction())
+                    {
+                        using (var ms = new MemoryStream())
+                        using (var writer = new BlittableJsonTextWriter(context, ms))
+                        {
+                            WriteTo(writer);
+
+                            writer.Flush();
+                            ms.Position = 0;
+
+                            var tree = tx.InnerTransaction.CreateTree(nameof(ServerStatistics));
+                            tree.Add(nameof(ServerStatistics), ms);
+
+                            tx.Commit();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    if (logger.IsInfoEnabled)
+                        logger.Info("Could not persist server statistics.", e);
+                }
+            }
+        }
+
+        internal void MaybePersist(TransactionContextPool contextPool, Logger logger)
+        {
+            var now = SystemTime.UtcNow;
+            if (now - _lastPersist <= PersistFrequency)
+                return;
+
+            try
+            {
+                Persist(contextPool, logger);
+            }
+            finally
+            {
+                _lastPersist = now;
+            }
         }
     }
 }
