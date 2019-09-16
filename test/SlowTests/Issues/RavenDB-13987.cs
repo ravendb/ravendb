657a009043baa0ee1e06ba5deecb15e3806fb799
Egor <egor@ravendb.net>
2019-09-16 13:14:05 +0300

    RavenDB-13987: fix failing test

--- test/SlowTests/Issues/RavenDB-13987.cs
+++ test/SlowTests/Issues/RavenDB-13987.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,65 +18,60 @@ namespace SlowTests.Issues
 {
     public class RavenDB_13987 : ReplicationTestBase
     {
+        private long _taskId;
+        private string _databaseName;
+        private readonly TimeSpan _reasonableWaitTime = Debugger.IsAttached ? TimeSpan.FromSeconds(60 * 10) : TimeSpan.FromSeconds(60 * 5);
+
         public RavenDB_13987()
         {
             DoNotReuseServer();
         }
 
         [Fact]
-        public async Task ShouldWork()
+        public async Task ServerWideBackupShouldNotWakeupIdleDatabases()
         {
             var backupPath = NewDataPath(suffix: "BackupFolder");
             const int clusterSize = 3;
-            long taskId;
-            var databaseName = GetDatabaseName();
-            var leader = await CreateRaftClusterAndGetLeader(3, false, customSettings: new Dictionary<string, string>()
-            {
-                [RavenConfiguration.GetKey(x => x.Cluster.MoveToRehabGraceTime)] = "1",
-                [RavenConfiguration.GetKey(x => x.Cluster.AddReplicaTimeout)] = "1",
-                [RavenConfiguration.GetKey(x => x.Cluster.ElectionTimeout)] = "300",
-                [RavenConfiguration.GetKey(x => x.Cluster.StabilizationTime)] = "1",
-                [RavenConfiguration.GetKey(x => x.Databases.MaxIdleTime)] = "10",
-                [RavenConfiguration.GetKey(x => x.Databases.FrequencyToCheckForIdle)] = "3",
-                [RavenConfiguration.GetKey(x => x.Core.RunInMemory)] = "false"
-            });
+            _databaseName = GetDatabaseName();
+
+            var leader = await CreateRaftClusterAndGetLeader(3, false,
+                customSettings: new Dictionary<string, string>()
+                {
+                    [RavenConfiguration.GetKey(x => x.Cluster.MoveToRehabGraceTime)] = "1",
+                    [RavenConfiguration.GetKey(x => x.Cluster.AddReplicaTimeout)] = "1",
+                    [RavenConfiguration.GetKey(x => x.Cluster.ElectionTimeout)] = "300",
+                    [RavenConfiguration.GetKey(x => x.Cluster.StabilizationTime)] = "1",
+                    [RavenConfiguration.GetKey(x => x.Databases.MaxIdleTime)] = "10",
+                    [RavenConfiguration.GetKey(x => x.Databases.FrequencyToCheckForIdle)] = "3",
+                    [RavenConfiguration.GetKey(x => x.Core.RunInMemory)] = "false"
+                });
 
             DatabasePutResult databaseResult;
-            using (var store = new DocumentStore
+            using (var store = new DocumentStore { Urls = new[] { leader.WebUrl }, Database = _databaseName }.Initialize())
             {
-                Urls = new[] { leader.WebUrl },
-                Database = databaseName
-            }.Initialize())
-            {
-                var doc = new DatabaseRecord(databaseName);
+                var doc = new DatabaseRecord(_databaseName);
                 databaseResult = await store.Maintenance.Server.SendAsync(new CreateDatabaseOperation(doc, clusterSize));
             }
+
             Assert.Equal(clusterSize, databaseResult.Topology.AllNodes.Count());
 
             foreach (var server in Servers)
             {
                 await server.ServerStore.Cluster.WaitForIndexNotification(databaseResult.RaftCommandIndex);
             }
+
             foreach (var server in Servers.Where(s => databaseResult.NodesAddedTo.Any(n => n == s.WebUrl)))
             {
-                await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(databaseName);
+                await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(_databaseName);
             }
 
             var rnd = new Random();
             var index = rnd.Next(0, Servers.Count - 1);
-            using (var store = new DocumentStore
-            {
-                Urls = new[] { Servers[index].WebUrl },
-                Database = databaseName
-            }.Initialize())
+            using (var store = new DocumentStore { Urls = new[] { Servers[index].WebUrl }, Database = _databaseName }.Initialize())
             {
                 using (var s = store.OpenAsyncSession())
                 {
-                    await s.StoreAsync(new User()
-                    {
-                        Name = "Egor"
-                    }, "foo/bar");
-
+                    await s.StoreAsync(new User() { Name = "Egor" }, "foo/bar");
                     await s.SaveChangesAsync();
                 }
 
@@ -87,16 +83,14 @@ namespace SlowTests.Issues
                     Thread.Sleep(3000);
                     now = DateTime.Now;
                 }
+
                 Assert.Equal(3, GetIdleCount());
 
                 var putConfiguration = new ServerWideBackupConfiguration
                 {
                     FullBackupFrequency = "*/2 * * * *",
                     IncrementalBackupFrequency = "*/2 * * * *",
-                    LocalSettings = new LocalSettings
-                    {
-                        FolderPath = backupPath
-                    },
+                    LocalSettings = new LocalSettings { FolderPath = backupPath },
                 };
 
                 var result = await store.Maintenance.Server.SendAsync(new PutServerWideBackupConfigurationOperation(putConfiguration));
@@ -105,7 +99,7 @@ namespace SlowTests.Issues
 
                 var record1 = await store.Maintenance.Server.SendAsync(new GetDatabaseRecordOperation(store.Database));
                 var backups1 = record1.PeriodicBackups;
-                taskId = backups1.First().TaskId;
+                _taskId = backups1.First().TaskId;
 
                 Assert.Equal(1, backups1.Count);
             }
@@ -113,29 +107,37 @@ namespace SlowTests.Issues
             Assert.Equal(3, GetIdleCount());
 
             // wait for the backup occurrence
-            var now1 = DateTime.Now;
-            var nextNow1 = now1 + TimeSpan.FromSeconds(300);
-            var count = 0;
-            while (now1 < nextNow1 && count < 3)
-            {
-                Thread.Sleep(500);
-                now1 = DateTime.Now;
-                count = CountOfBackupStatus(databaseName, taskId);
-            }
+            var idleCount = WaitForCount(_reasonableWaitTime, 2, GetIdleCount);
+            Assert.Equal(2, idleCount);
 
-            // should not wakeup idle dbs and should update the backup status on all dbs
-            Assert.Equal(2, GetIdleCount());
+            // backup status should not wakeup dbs
+            var count = WaitForCount(_reasonableWaitTime, 3, CountOfBackupStatus);
             Assert.Equal(3, count);
         }
 
-        private int CountOfBackupStatus(string databaseName, long taskId)
+        private static int WaitForCount(TimeSpan seconds, int excepted, Func<int> func)
+        {
+            var now = DateTime.Now;
+            var nextNow = now + seconds;
+            var count = func();
+            while (now < nextNow && count != excepted)
+            {
+                Thread.Sleep(500);
+                now = DateTime.Now;
+                count = func();
+            }
+
+            return count;
+        }
+
+        private int CountOfBackupStatus()
         {
             var count = 0;
             foreach (var server in Servers)
             {
-                using (var store = new DocumentStore { Urls = new[] { server.WebUrl }, Database = databaseName }.Initialize())
+                using (var store = new DocumentStore { Urls = new[] { server.WebUrl }, Conventions = { DisableTopologyUpdates = true }, Database = _databaseName }.Initialize())
                 {
-                    var status = store.Maintenance.Send(new GetPeriodicBackupStatusOperation(taskId)).Status;
+                    var status = store.Maintenance.Send(new GetPeriodicBackupStatusOperation(_taskId)).Status;
                     if (status?.LastFullBackup != null)
                         count++;
                 }
