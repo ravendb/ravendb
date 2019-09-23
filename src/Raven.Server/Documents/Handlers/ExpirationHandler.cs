0171c88cc4eca660245846a5cfd5151722d1f079
ml054 <marcin@ravendb.net>
2019-09-23 16:17:53 +0200

    RavenDB-14052 Expose Refresh feature in the studio

--- src/Raven.Server/Documents/Handlers/ExpirationHandler.cs
+++ src/Raven.Server/Documents/Handlers/ExpirationHandler.cs
@@ -7,6 +7,7 @@
 using System.Net;
 using System.Threading.Tasks;
 using Raven.Client.Documents.Operations.Expiration;
+using Raven.Client.Documents.Operations.Refresh;
 using Raven.Server.Routing;
 using Raven.Server.ServerWide.Context;
 using Sparrow.Json;
@@ -48,6 +49,33 @@ namespace Raven.Server.Documents.Handlers
             await DatabaseConfigurations(ServerStore.ModifyDatabaseExpiration, "read-expiration-config", GetRaftRequestIdFromQuery());
         }
 
+        [RavenAction("/databases/*/refresh/config", "GET", AuthorizationStatus.ValidUser)]
+        public Task GetRefreshConfig()
+        {
+            using (Server.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext context))
+            using (context.OpenReadTransaction())
+            {
+                RefreshConfiguration refreshConfig;
+                using (var recordRaw = Server.ServerStore.Cluster.ReadRawDatabaseRecord(context, Database.Name))
+                {
+                    refreshConfig = recordRaw?.GetRefreshConfiguration();
+                }
+
+                if (refreshConfig != null)
+                {
+                    using (var writer = new BlittableJsonTextWriter(context, ResponseBodyStream()))
+                    {
+                        context.Write(writer, refreshConfig.ToJson());
+                    }
+                }
+                else
+                {
+                    HttpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                }
+            }
+            return Task.CompletedTask;
+        }
+        
         [RavenAction("/databases/*/admin/refresh/config", "POST", AuthorizationStatus.DatabaseAdmin)]
         public async Task ConfigRefresh()
         {
