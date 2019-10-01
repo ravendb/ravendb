5f6ce9407877d1177651529392e70e8890ddae45
Pawel Pekrol <ppekrol@ravendb.net>
2019-10-01 09:17:47 +0200

    RavenDB-14068 added test

---
+++ test/SlowTests/Issues/RavenDB_14608.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FastTests;
+using FastTests.Utils;
+using Orders;
+using Raven.Client;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Documents.Operations.Expiration;
+using Raven.Client.Util;
+using Raven.Server.Documents.Expiration;
+using Sparrow;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_14068 : RavenTestBase
+    {
+        private async Task SetupExpiration(DocumentStore store)
+        {
+            var config = new ExpirationConfiguration
+            {
+                Disabled = false,
+                DeleteFrequencyInSec = 100,
+            };
+
+            await ExpirationHelper.SetupExpiration(store, Server.ServerStore, config);
+        }
+
+        [Fact]
+        public async Task WillCleanupAllExpiredDocumentsInSingleRun_EvenWhenMoreThanBatchSize()
+        {
+            var count = ExpiredDocumentsCleaner.BatchSize + 10;
+
+            var company = new { Name = "Company Name" };
+
+            using (var store = GetDocumentStore())
+            {
+                await SetupExpiration(store);
+
+                var expiry = SystemTime.UtcNow.AddMinutes(5);
+                var metadata = new Dictionary<string, object>
+                {
+                    [Constants.Documents.Metadata.Expires] = expiry.ToString(DefaultFormat.DateTimeOffsetFormatsToWrite)
+                };
+                var metadata2 = new Dictionary<string, object>
+                {
+                    [Constants.Documents.Metadata.Expires] = expiry.AddMinutes(1).ToString(DefaultFormat.DateTimeOffsetFormatsToWrite)
+                };
+
+                using (var commands = store.Commands())
+                {
+                    for (var i = 0; i < count; i++)
+                    {
+                        await commands.PutAsync("companies/" + i, null, company, metadata);
+                        await commands.PutAsync("companies-type2/" + i, null, company, metadata2);
+                    }
+                }
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    var company2 = await session.LoadAsync<Company>("companies/" + (count - 1));
+                    Assert.NotNull(company2);
+                }
+
+                var database = await GetDocumentDatabaseInstanceFor(store);
+                database.Time.UtcDateTime = () => DateTime.UtcNow.AddMinutes(10);
+                var expiredDocumentsCleaner = database.ExpiredDocumentsCleaner;
+                await expiredDocumentsCleaner.CleanupExpiredDocs();
+
+                var stats = await store.Maintenance.SendAsync(new GetStatisticsOperation());
+                Assert.Equal(0, stats.CountOfDocuments);
+            }
+        }
+    }
+}
