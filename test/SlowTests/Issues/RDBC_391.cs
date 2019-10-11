05e35001be9b0060b2b8c6a177154112a15029a6
Pawel Pekrol <ppekrol@ravendb.net>
2019-10-11 09:20:38 +0200

    RDBC-391 should be able to use operations with selected node tag when disable topology updates is true

---
+++ test/SlowTests/Issues/RDBC_391.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FastTests;
+using Raven.Client.Documents.Operations;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RDBC_391 : RavenTestBase
+    {
+        [Fact]
+        public void CanUseOperationsWithSelectedNodeTagWhenDisableTopologyUpdatesIsTrue()
+        {
+            using (var store = GetDocumentStore(new Options
+            {
+                ModifyDocumentStore = s => s.Conventions.DisableTopologyUpdates = true
+            }))
+            {
+                using (var session = store.OpenSession())
+                {
+                    var operation = session
+                        .Advanced
+                        .DocumentStore
+                        .Operations.Send(new PatchByQueryOperation("from Ent update { this.Field = 'Abc' }"));
+
+                    operation.WaitForCompletion(TimeSpan.FromSeconds(60));
+                }
+            }
+        }
+    }
+}
