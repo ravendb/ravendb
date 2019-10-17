5cc5fb4431a729bc88ba0c89847f1cfe404333f0
Karmel Indych <karmeli87@gmail.com>
2019-10-17 11:14:38 +0300

    RavenDB-14142 Enforce revision configuration fails when there are tombstones

--- test/FastTests/Server/Documents/Revisions/RevisionsTests.cs
+++ test/FastTests/Server/Documents/Revisions/RevisionsTests.cs
@@ -208,6 +208,95 @@ namespace FastTests.Server.Documents.Revisions
         }
 
         [Fact]
+        public async Task EnforceRevisionConfigurationWithTombstones()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var configuration = new RevisionsConfiguration
+                {
+                    Default = new RevisionsCollectionConfiguration
+                    {
+                        Disabled = false,
+                        MinimumRevisionsToKeep = 10
+                    }
+                };
+
+                await store.Maintenance.SendAsync(new ConfigureRevisionsOperation(configuration));
+
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = i; j >= 0; j--)
+                    {
+                        using (var session = store.OpenAsyncSession())
+                        {
+                            await session.StoreAsync(new Product
+                                {
+                                    Description = j.ToString()
+                                }, "bar" + i);
+                            await session.SaveChangesAsync();
+                        }
+                    }
+                }
+
+                
+                for (int i = 0; i < 10; i++)
+                {
+                    using (var session = store.OpenAsyncSession())
+                    {
+
+                        var product = await session.LoadAsync<Product>("bar"+i);
+                        var metadata = session.Advanced.GetMetadataFor(product);
+                        Assert.Equal((DocumentFlags.HasRevisions).ToString(), metadata[Constants.Documents.Metadata.Flags]);
+                        var bar = await session.Advanced.Revisions.GetMetadataForAsync("bar" + i);
+                        Assert.Equal(i + 1, bar.Count);
+                    }
+                }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    using (var session = store.OpenAsyncSession())
+                    {
+                        session.Delete("bar" + i);
+                        await session.SaveChangesAsync();
+                    }
+                }
+
+                configuration.Default.MinimumRevisionsToKeep = 5;
+                await store.Maintenance.SendAsync(new ConfigureRevisionsOperation(configuration));
+
+                var db = await GetDocumentDatabaseInstanceFor(store);
+                using (var token = new OperationCancelToken(db.Configuration.Databases.OperationTimeout.AsTimeSpan, db.DatabaseShutdown))
+                    await db.DocumentsStorage.RevisionsStorage.EnforceConfiguration(_ => { }, token);
+
+
+                for (int i = 0; i < 10; i++)
+                {
+                    using (var session = store.OpenAsyncSession())
+                    {
+                        var foo = await session.Advanced.Revisions.GetMetadataForAsync("bar" + i);
+                        Assert.Equal(Math.Min(i + 2, 5), foo.Count);
+                    }
+                }
+
+                configuration.Default.MinimumRevisionsToKeep = 0;
+                configuration.Default.PurgeOnDelete = true;
+                await store.Maintenance.SendAsync(new ConfigureRevisionsOperation(configuration));
+
+                using (var token = new OperationCancelToken(db.Configuration.Databases.OperationTimeout.AsTimeSpan, db.DatabaseShutdown))
+                    await db.DocumentsStorage.RevisionsStorage.EnforceConfiguration(_ => { }, token);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    using (var session = store.OpenAsyncSession())
+                    {
+                        var foo = await session.Advanced.Revisions.GetMetadataForAsync("bar" + i);
+                        Assert.Equal(0, foo.Count);
+                    }
+                }
+            }
+        }
+
+        [Fact]
         public async Task EnforceEmptyConfigurationWillDeleteAllRevisions()
         {
             using (var store = GetDocumentStore())
