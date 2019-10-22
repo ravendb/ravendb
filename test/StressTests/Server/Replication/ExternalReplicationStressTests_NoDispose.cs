c9934f28e08079065dfcb994d8fa2b2e6997d55d
Pawel Pekrol <ppekrol@ravendb.net>
2019-10-22 19:22:11 +0200

    RavenDB-7070 fixing tests

---
+++ test/StressTests/Server/Replication/ExternalReplicationStressTests_NoDispose.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FastTests;
+using FastTests.Server.Replication;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Documents.Operations.Replication;
+using Raven.Server.Config;
+using SlowTests.Server.Replication;
+using Tests.Infrastructure;
+using Xunit.Abstractions;
+
+namespace StressTests.Server.Replication
+{
+    public class ExternalReplicationStressTests_NoDispose : NoDisposalNeeded
+    {
+        public ExternalReplicationStressTests_NoDispose(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact64Bit]
+        public void ExternalReplicationShouldWorkWithSmallTimeoutStress()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Parallel.For(0, 3, RavenTestHelper.DefaultParallelOptions, _ =>
+                {
+                    using (var test = new ExternalReplicationTests(Output))
+                    {
+                        test.ExternalReplicationShouldWorkWithSmallTimeoutStress(20000).Wait();
+                    }
+                });
+            }
+        }
+    }
+}
