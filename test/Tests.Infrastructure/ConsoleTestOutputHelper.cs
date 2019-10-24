b722b76cd7f54fbf09f18bbc65ac6629a2d84705
Tomasz Opalach <tomtom@ravendb.net>
2019-10-24 08:30:10 +0200

    RavenDB-14171 fix Console.Out hijacking by using XunitLogger fork

---
+++ test/Tests.Infrastructure/ConsoleTestOutputHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit.Abstractions;
+
+namespace Tests.Infrastructure
+{
+    public class ConsoleTestOutputHelper : ITestOutputHelper, IDisposable
+    {
+        public void WriteLine(string message) => Console.WriteLine(message);
+
+        public void WriteLine(string format, params object[] args) => Console.WriteLine(format, args);
+
+        public void Dispose()
+        {
+            XunitLogging.Flush();
+        }
+    }
+}
