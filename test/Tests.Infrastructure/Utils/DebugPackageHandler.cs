eef5880694d0057fa8145b670c3712db9a499f4f
Tomasz Opalach <tomtom@ravendb.net>
2019-10-30 15:21:59 +0100

    RavenDB-14121 save debug package on test timeout

---
+++ test/Tests.Infrastructure/Utils/DebugPackageHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using Raven.Client.Documents;
+using Raven.Server;
+using Tests.Infrastructure.Operations;
+using XunitLogger;
+
+namespace Tests.Infrastructure.Utils
+{
+    public static class DebugPackageHandler
+    {
+        private const string DestinationDirectory = "debug_packages";
+
+        public static void DownloadAndSave(RavenServer ravenServer, Context testContext)
+        {
+            var documentStore = InitDocumentStore(ravenServer);
+            var operationResult = documentStore.Maintenance.Server.Send(new GetClusterDebugInfoPackageOperation());
+
+            SaveDebugPackage(testContext, operationResult);
+        }
+
+        private static IDocumentStore InitDocumentStore(RavenServer ravenServer)
+            => new DocumentStore {Urls = new[] {ravenServer.WebUrl}}
+                .Initialize();
+
+        private static void SaveDebugPackage(Context testContext, ClusterDebugInfoPackageResult operationResult)
+        {
+            if (Directory.Exists(DestinationDirectory) == false)
+                Directory.CreateDirectory(DestinationDirectory);
+            
+            var fileName = GetDebugPackageName(testContext);
+            var outputPath = Path.Join(DestinationDirectory, fileName);
+
+            using (var fileStream = File.Create(outputPath))
+            {
+                operationResult.Stream.CopyTo(fileStream);
+            }
+
+            LogDebugPackageSaved(testContext, outputPath);
+        }
+
+        private static string GetDebugPackageName(Context testContext)
+        {
+            return $"{testContext.MethodName}_{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}.zip";
+        }
+
+        private static void LogDebugPackageSaved(Context testContext, string outputPath)
+        {
+            var fullPath = Path.GetFullPath(outputPath);
+            var message = $"Saved debug package for {testContext.Test.DisplayName} in {fullPath}";
+            
+            testContext.TestOutput.WriteLine(message);
+        }
+    }
+}
---
+++ test/Tests.Infrastructure/Utils/TestOutcomeAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Raven.Client.Exceptions;
+using Xunit.Sdk;
+using XunitLogger;
+
+namespace Tests.Infrastructure.Utils
+{
+    public class TestOutcomeAnalyzer
+    {
+        private static readonly Regex[] TimeoutExceptionMessageRegexes =
+        {
+            new Regex(@"Could not send command \S* from \S* to leader because there is no leader, and we timed out waiting for one after ", RegexOptions.Compiled),
+            new Regex(@"Waited too long for the raft command", RegexOptions.Compiled),
+            new Regex(@"Waited for \S* for task with index \S* to complete. ", RegexOptions.Compiled),
+            new Regex(@"Waited for \S* but didn't get an index notification for \S*.", RegexOptions.Compiled),
+            new Regex(@"Something is wrong, throwing to avoid hanging", RegexOptions.Compiled),
+            new Regex(@"Waited for \S* but the command was not applied in this time.", RegexOptions.Compiled)
+        };
+        
+        private readonly Context _context;
+
+        public TestOutcomeAnalyzer(Context context)
+        {
+            _context = context;
+        }
+
+        public Exception Exception => _context.TestException;
+
+        public bool Failed => Exception != null;
+
+        public bool FailedOnAssertion => Exception is XunitException;
+
+        public bool ThrewRavenTimeoutException()
+        {
+            if (Exception is RavenException == false)
+                return false;
+
+            var innerException = Exception.InnerException;
+
+            if (innerException == null)
+                return false;
+
+            return TimeoutExceptionMessageRegexes.Any(r => r.IsMatch(innerException.Message));
+        }
+    }
+}
