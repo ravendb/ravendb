e816febfdb9a47e231ef6897405b6b5c20e946e5
aviv86 <aviv@ayende.com>
2019-10-30 15:21:40 +0200

    RavenDB-13121 : minor fixes

--- src/Raven.Client/Documents/Session/AbstractDocumentQuery.cs
+++ src/Raven.Client/Documents/Session/AbstractDocumentQuery.cs
@@ -531,9 +531,9 @@ Use session.Query<T>() instead of session.Advanced.DocumentQuery<T>. The session
 
             IncludeCounters(includes.Alias, includes.CountersToIncludeBySourcePath);
 
-            if (includes.TimeSeriesToIncludeBySourcePath != null)
+            if (includes.TimeSeriesToIncludeBySourceAlias != null)
             {
-                IncludeTimeSeries(includes.Alias, includes.TimeSeriesToIncludeBySourcePath);
+                IncludeTimeSeries(includes.Alias, includes.TimeSeriesToIncludeBySourceAlias);
             }
         }
 
