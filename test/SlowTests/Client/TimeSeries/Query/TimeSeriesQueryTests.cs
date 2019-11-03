980e2101bc902f9ef9adc48b8e8dca9a5f6eb60c
aviv86 <aviv@ayende.com>
2019-11-03 17:20:18 +0200

    RavenDB-13080 : tests

--- test/SlowTests/Client/TimeSeries/Query/TimeSeriesQueryTests.cs
+++ test/SlowTests/Client/TimeSeries/Query/TimeSeriesQueryTests.cs
@@ -20,7 +20,7 @@ namespace SlowTests.Client.TimeSeries.Query
         {
 #pragma warning disable 649
             public long Count;
-            public double? Max, Min, Last, First;
+            public double? Max, Min, Last, First, Avg;
             public DateTime To, From;
 #pragma warning restore 649
         }
@@ -55,8 +55,6 @@ namespace SlowTests.Client.TimeSeries.Query
                                     u.Age
                                 };
             }
-
-            public override string IndexName => "People";
         }
 
         private class Person
@@ -64,6 +62,19 @@ namespace SlowTests.Client.TimeSeries.Query
             public string Name { get; set; }
 
             public int Age { get; set; }
+
+            public string WorksAt { get; set; }
+        }
+
+        private class RawQueryResult
+        {
+            public TimeSeriesAggregation HeartRate { get; set; }
+
+            public TimeSeriesAggregation BloodPressure { get; set; }
+
+            public TimeSeriesAggregation Stocks { get; set; }
+
+            public string Name { get; set; }
         }
 
         [Fact]
@@ -325,6 +336,87 @@ namespace SlowTests.Client.TimeSeries.Query
         }
 
         [Fact]
+        public void CanQueryTimeSeriesAggregation_DeclareSyntax_FromLoadedDocument()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var baseline = DateTime.Today;
+
+                using (var session = store.OpenSession())
+                {
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        var id = $"people/{i}";
+                        var company = $"companies/{i}";
+
+                        session.Store(new Person
+                        {
+                            Name = "Oren",
+                            Age = i * 30,
+                            WorksAt = company
+                        }, id);
+
+                        session.Store(new Company(), company);
+
+                        var tsf = session.TimeSeriesFor(company);
+
+                        tsf.Append("Stocks", baseline.AddMinutes(61), "tag", new[] { 1259.51d });
+                        tsf.Append("Stocks", baseline.AddMinutes(62), "tag", new[] { 1279.62d });
+                        tsf.Append("Stocks", baseline.AddMinutes(63), "tag", new[] { 1269.73d });
+                    }
+
+                    session.SaveChanges();
+                }
+
+                new PeopleIndex().Execute(store);
+
+                WaitForIndexing(store);
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Advanced.RawQuery<TimeSeriesAggregation>(@"
+declare timeseries out(u) 
+{
+    from Company.Stocks between $start and $end
+    group by 1h 
+    select min(), max(), avg()
+}
+from People as p
+where p.Age > 49
+load p.WorksAt as Company
+select out(p)
+")
+                        .AddParameter("start", baseline)
+                        .AddParameter("end", baseline.AddDays(1));
+
+                    var result = query.ToList();
+
+                    Assert.Equal(2, result.Count);
+
+                    for (int i = 0; i < 2; i++)
+                    {
+                        var agg = result[i];
+
+                        Assert.Equal(3, agg.Count);
+
+                        Assert.Equal(1, agg.Results.Length);
+
+                        var val = agg.Results[0];
+
+                        Assert.Equal(1259.51d, val.Min);
+                        Assert.Equal(1279.62d, val.Max);
+
+                        double expectedAvg = (1259.51d + 1279.62d + 1269.73d) / 3.0;
+                        Assert.Equal(expectedAvg, val.Avg);
+
+                        Assert.Equal(baseline.AddMinutes(60), val.From);
+                        Assert.Equal(baseline.AddMinutes(120), val.To);
+                    }
+                }
+            }
+        }
+
+        [Fact]
         public void CanQueryTimeSeriesAggregation_SelectSyntax_CollectionQuery()
         {
             using (var store = GetDocumentStore())
@@ -362,8 +454,8 @@ select timeseries(
     group by 1h 
     select min(), max())
 ")
-                        .AddParameter("start", DateTime.MinValue)
-                        .AddParameter("end", DateTime.MaxValue);
+                        .AddParameter("start", baseline)
+                        .AddParameter("end", baseline.AddDays(1));
 
                     var result = query.ToList();
 
@@ -427,8 +519,8 @@ select timeseries(
     group by 1h 
     select min(), max())
 ")
-                        .AddParameter("start", DateTime.MinValue)
-                        .AddParameter("end", DateTime.MaxValue);
+                        .AddParameter("start", baseline)
+                        .AddParameter("end", baseline.AddDays(1));
 
                     var result = query.ToList();
 
@@ -496,8 +588,8 @@ select timeseries(
     group by 1h 
     select min(), max())
 ")
-                        .AddParameter("start", DateTime.MinValue)
-                        .AddParameter("end", DateTime.MaxValue);
+                        .AddParameter("start", baseline)
+                        .AddParameter("end", baseline.AddDays(1));
 
                     var result = query.ToList();
 
@@ -522,5 +614,410 @@ select timeseries(
             }
         }
 
+        [Fact]
+        public void CanQueryTimeSeriesAggregation_SelectSyntax_AsAlias()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var baseline = DateTime.Today;
+
+                using (var session = store.OpenSession())
+                {
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        var id = $"people/{i}";
+
+                        session.Store(new Person
+                        {
+                            Name = "Oren",
+                            Age = i * 30
+                        }, id);
+
+                        var tsf = session.TimeSeriesFor(id);
+
+                        tsf.Append("HeartRate", baseline.AddMinutes(61), "watches/fitbit", new[] { 59d });
+                        tsf.Append("HeartRate", baseline.AddMinutes(62), "watches/fitbit", new[] { 79d });
+                        tsf.Append("HeartRate", baseline.AddMinutes(63), "watches/fitbit", new[] { 69d });
+                    }
+
+                    session.SaveChanges();
+                }
+
+                new PeopleIndex().Execute(store);
+
+                WaitForIndexing(store);
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Advanced.RawQuery<RawQueryResult>(@"
+from index 'People'
+where Age > 49
+select timeseries(
+    from HeartRate between $start and $end 
+    group by 1h 
+    select min(), max())
+as HeartRate
+")
+                        .AddParameter("start", baseline)
+                        .AddParameter("end", baseline.AddDays(1));
+
+                    var result = query.ToList();
+
+                    Assert.Equal(2, result.Count);
+
+                    for (int i = 0; i < 2; i++)
+                    {
+                        var agg = result[i];
+                        var heartrate = agg.HeartRate;
+
+                        Assert.Equal(3, heartrate.Count);
+
+                        Assert.Equal(1, heartrate.Results.Length);
+
+                        var val = heartrate.Results[0];
+
+                        Assert.Equal(59, val.Min);
+                        Assert.Equal(79, val.Max);
+
+                        Assert.Equal(baseline.AddMinutes(60), val.From);
+                        Assert.Equal(baseline.AddMinutes(120), val.To);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void CanQueryTimeSeriesAggregation_SelectSyntax_WithOtherFields()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var baseline = DateTime.Today;
+
+                using (var session = store.OpenSession())
+                {
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        var id = $"people/{i}";
+
+                        session.Store(new Person
+                        {
+                            Name = "Oren",
+                            Age = i * 30
+                        }, id);
+
+                        var tsf = session.TimeSeriesFor(id);
+
+                        tsf.Append("HeartRate", baseline.AddMinutes(61), "watches/fitbit", new[] { 59d });
+                        tsf.Append("HeartRate", baseline.AddMinutes(62), "watches/fitbit", new[] { 79d });
+                        tsf.Append("HeartRate", baseline.AddMinutes(63), "watches/fitbit", new[] { 69d });
+                    }
+
+                    session.SaveChanges();
+                }
+
+                new PeopleIndex().Execute(store);
+
+                WaitForIndexing(store);
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Advanced.RawQuery<RawQueryResult>(@"
+from index 'People'
+where Age > 49
+select timeseries(
+    from HeartRate between $start and $end 
+    group by 1h 
+    select min(), max())
+as HeartRate, Name 
+")
+                        .AddParameter("start", baseline)
+                        .AddParameter("end", baseline.AddDays(1));
+
+                    var result = query.ToList();
+
+                    Assert.Equal(2, result.Count);
+
+                    for (int i = 0; i < 2; i++)
+                    {
+                        var agg = result[i];
+
+                        Assert.Equal("Oren", agg.Name);
+
+                        var heartrate = agg.HeartRate;
+
+                        Assert.Equal(3, heartrate.Count);
+
+                        Assert.Equal(1, heartrate.Results.Length);
+
+                        var val = heartrate.Results[0];
+
+                        Assert.Equal(59, val.Min);
+                        Assert.Equal(79, val.Max);
+
+                        Assert.Equal(baseline.AddMinutes(60), val.From);
+                        Assert.Equal(baseline.AddMinutes(120), val.To);
+
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void CanQueryTimeSeriesAggregation_SelectSyntax_MultipleSeries()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var baseline = DateTime.Today;
+                var baseline2 = DateTime.Today.AddDays(-1);
+
+
+                using (var session = store.OpenSession())
+                {
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        var id = $"people/{i}";
+
+                        session.Store(new Person
+                        {
+                            Name = "Oren",
+                            Age = i * 30
+                        }, id);
+
+                        var tsf = session.TimeSeriesFor(id);
+
+                        tsf.Append("HeartRate", baseline.AddMinutes(61), "watches/fitbit", new[] { 59d });
+                        tsf.Append("HeartRate", baseline.AddMinutes(62), "watches/fitbit", new[] { 79d });
+                        tsf.Append("HeartRate", baseline.AddMinutes(63), "watches/fitbit", new[] { 69d });
+
+                        tsf.Append("BloodPressure", baseline2.AddMinutes(61), "watches/apple", new[] { 159d });
+                        tsf.Append("BloodPressure", baseline2.AddMinutes(62), "watches/apple", new[] { 179d });
+                        tsf.Append("BloodPressure", baseline2.AddMinutes(63), "watches/apple", new[] { 168d });
+                    }
+
+                    session.SaveChanges();
+                }
+
+                new PeopleIndex().Execute(store);
+
+                WaitForIndexing(store);
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Advanced.RawQuery<RawQueryResult>(@"
+from index 'People'
+where Age > 49
+select timeseries(
+    from HeartRate between $start and $end
+    group by 1h 
+    select min(), max())
+as HeartRate, timeseries(
+    from BloodPressure between $start2 and $end2 
+    group by 1h 
+    select min(), max(), avg())
+as BloodPressure
+")
+                        .AddParameter("start", baseline)
+                        .AddParameter("end", baseline.AddDays(1))
+                        .AddParameter("start2", baseline2)
+                        .AddParameter("end2", baseline2.AddDays(1));
+
+                    var result = query.ToList();
+
+                    Assert.Equal(2, result.Count);
+
+                    for (int i = 0; i < 2; i++)
+                    {
+                        var agg = result[i];
+
+                        var heartrate = agg.HeartRate;
+
+                        Assert.Equal(3, heartrate.Count);
+
+                        Assert.Equal(1, heartrate.Results.Length);
+
+                        var val = heartrate.Results[0];
+
+                        Assert.Equal(59, val.Min);
+                        Assert.Equal(79, val.Max);
+
+                        Assert.Equal(baseline.AddMinutes(60), val.From);
+                        Assert.Equal(baseline.AddMinutes(120), val.To);
+
+                        var bloodPressure = agg.BloodPressure;
+
+                        Assert.Equal(3, bloodPressure.Count);
+
+                        Assert.Equal(1, bloodPressure.Results.Length);
+
+                        val = bloodPressure.Results[0];
+
+                        Assert.Equal(159, val.Min);
+                        Assert.Equal(179, val.Max);
+
+                        double expectedAvg = (159 + 168 + 179) / 3.0;
+
+                        Assert.Equal(expectedAvg, val.Avg);
+
+                        Assert.Equal(baseline2.AddMinutes(60), val.From);
+                        Assert.Equal(baseline2.AddMinutes(120), val.To);
+
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void CanQueryTimeSeriesAggregation_SelectSyntax_AliasNotation()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var baseline = DateTime.Today;
+
+                using (var session = store.OpenSession())
+                {
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        var id = $"people/{i}";
+
+                        session.Store(new Person
+                        {
+                            Name = "Oren",
+                            Age = i * 30
+                        }, id);
+
+                        var tsf = session.TimeSeriesFor(id);
+
+                        tsf.Append("HeartRate", baseline.AddMinutes(61), "watches/fitbit", new[] { 59d });
+                        tsf.Append("HeartRate", baseline.AddMinutes(62), "watches/fitbit", new[] { 79d });
+                        tsf.Append("HeartRate", baseline.AddMinutes(63), "watches/fitbit", new[] { 69d });
+                    }
+
+                    session.SaveChanges();
+                }
+
+                new PeopleIndex().Execute(store);
+
+                WaitForIndexing(store);
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Advanced.RawQuery<RawQueryResult>(@"
+from People as p
+where p.Age > 49
+select timeseries(
+    from p.HeartRate between $start and $end
+    group by 1h 
+    select min(), max())
+as HeartRate
+")
+                        .AddParameter("start", baseline)
+                        .AddParameter("end", baseline.AddDays(1));
+
+                    var result = query.ToList();
+
+                    Assert.Equal(2, result.Count);
+
+                    for (int i = 0; i < 2; i++)
+                    {
+                        var agg = result[i];
+
+                        var heartrate = agg.HeartRate;
+
+                        Assert.Equal(3, heartrate.Count);
+
+                        Assert.Equal(1, heartrate.Results.Length);
+
+                        var val = heartrate.Results[0];
+
+                        Assert.Equal(59, val.Min);
+                        Assert.Equal(79, val.Max);
+
+                        Assert.Equal(baseline.AddMinutes(60), val.From);
+                        Assert.Equal(baseline.AddMinutes(120), val.To);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void CanQueryTimeSeriesAggregation_SelectSyntax_FromLoadedDocument()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var baseline = DateTime.Today;
+
+                using (var session = store.OpenSession())
+                {
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        var id = $"people/{i}";
+                        var company = $"companies/{i}";
+
+                        session.Store(new Person
+                        {
+                            Name = "Oren",
+                            Age = i * 30,
+                            WorksAt = company
+                        }, id);
+
+                        session.Store(new Company(), company);
+
+                        var tsf = session.TimeSeriesFor(company);
+
+                        tsf.Append("Stocks", baseline.AddMinutes(61), "tag", new[] { 1259.51d });
+                        tsf.Append("Stocks", baseline.AddMinutes(62), "tag", new[] { 1279.62d });
+                        tsf.Append("Stocks", baseline.AddMinutes(63), "tag", new[] { 1269.73d });
+                    }
+
+                    session.SaveChanges();
+                }
+
+                new PeopleIndex().Execute(store);
+
+                WaitForIndexing(store);
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Advanced.RawQuery<RawQueryResult>(@"
+from People as p
+where p.Age > 49
+load p.WorksAt as Company
+select timeseries(
+    from Company.Stocks between $start and $end
+    group by 1h 
+    select min(), max(), avg())
+as Stocks
+")
+                        .AddParameter("start", baseline)
+                        .AddParameter("end", baseline.AddDays(1));
+
+                    var result = query.ToList();
+
+                    Assert.Equal(2, result.Count);
+
+                    for (int i = 0; i < 2; i++)
+                    {
+                        var agg = result[i];
+
+                        var stocks = agg.Stocks;
+
+                        Assert.Equal(3, stocks.Count);
+
+                        Assert.Equal(1, stocks.Results.Length);
+
+                        var val = stocks.Results[0];
+
+                        Assert.Equal(1259.51d, val.Min);
+                        Assert.Equal(1279.62d, val.Max);
+
+                        double expectedAvg = (1259.51d + 1279.62d + 1269.73d) / 3.0;
+                        Assert.Equal(expectedAvg, val.Avg);
+
+                        Assert.Equal(baseline.AddMinutes(60), val.From);
+                        Assert.Equal(baseline.AddMinutes(120), val.To);
+                    }
+                }
+            }
+        }
     }
 }
