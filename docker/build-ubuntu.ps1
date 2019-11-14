bcb41d63f7a2995bf58d94a0e16f72c6d6191128
Grzegorz Lachowski <gregolsky@ravendb.net>
2019-11-14 12:48:27 +0100

    RavenDB-8644 fix docker build scripts - commenting out a line with escaped eol leaves an empty script line

--- docker/build-ubuntu.ps1
+++ docker/build-ubuntu.ps1
@@ -29,8 +29,8 @@ function BuildUbuntuDockerImage ($version) {
     write-host "Tags: $($repo):$version-ubuntu.18.04-x64 $($repo):5.0-ubuntu-latest"
 
     docker build $DockerfileDir `
-        #-t "$($repo):latest" `
-        #-t "$($repo):ubuntu-latest" `
+        ` #-t "$($repo):latest" `
+        ` #-t "$($repo):ubuntu-latest" `
         -t "$($repo):$version-ubuntu.18.04-x64" `
         -t "$($repo):5.0-ubuntu-latest"
 
