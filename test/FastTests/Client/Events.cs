db49e9cf7ae3a15450e9622284cb011763bca1e7
Grisha Kotler <grisha@ayende.com>
2019-11-24 16:08:03 +0200

    RavenDB-14290 - add a test for Store inside OnBeforeStore

--- test/FastTests/Client/Events.cs
+++ test/FastTests/Client/Events.cs
@@ -133,6 +133,45 @@ namespace FastTests.Client
         }
 
         [Fact]
+        public void Before_Store_Session_Listener_With_Store_Inside()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User { Name = "Foo" }, "users/1");
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    session.Advanced.OnBeforeStore += delegate (object sender, BeforeStoreEventArgs args)
+                    {
+                        session.Store(new User {Name = "Grisha"}, "users/3");
+                    };
+
+                    session.Store(new User { Name = "Bar" }, "users/2");
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var user1 = session.Load<User>("users/1");
+                    Assert.NotNull(user1);
+                    Assert.Equal("Foo", user1.Name);
+
+                    var user2 = session.Load<User>("users/2");
+                    Assert.NotNull(user2);
+                    Assert.Equal("Bar", user2.Name);
+
+                    var user3 = session.Load<User>("users/3");
+                    Assert.NotNull(user3);
+                    Assert.Equal("Grisha", user3.Name);
+                }
+            }
+        }
+
+        [Fact]
         public void Before_Delete_Session_Listener_With_Delete_Inside()
         {
             using (var store = GetDocumentStore())
@@ -184,7 +223,8 @@ namespace FastTests.Client
                     session.Advanced.OnBeforeStore += delegate (object sender, BeforeStoreEventArgs args)
                     {
                         var user = session.Load<User>("users/1");
-                        Assert.Throws<InvalidOperationException>(() => args.Session.Evict(user));
+                        var error = Assert.Throws<InvalidOperationException>(() => args.Session.Evict(user));
+                        Assert.Equal("Cannot Evict entity during OnBeforeStore", error.Message);
                     };
 
                     session.Store(new User { Name = "Bar" }, "users/2");
@@ -196,7 +236,8 @@ namespace FastTests.Client
                     session.Advanced.OnBeforeDelete += delegate (object sender, BeforeDeleteEventArgs args)
                     {
                         var user2 = session.Load<User>("users/2");
-                        Assert.Throws<InvalidOperationException>(() => args.Session.Evict(user2));
+                        var error = Assert.Throws<InvalidOperationException>(() => args.Session.Evict(user2));
+                        Assert.Equal("Cannot Evict entity during OnBeforeDelete", error.Message);
                     };
 
                     var user1 = session.Load<User>("users/1");
