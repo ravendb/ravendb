af9e2f93df45f7127d45ce0b8a5cbbf8470de875
aviv86 <aviv@ayende.com>
2019-12-16 19:29:46 +0200

    RavenDB-13084 : TimeSeriesWhereClauseModifier - support wrapped constant expressions (add them as query parameters)

--- src/Raven.Client/Documents/Queries/TimeSeries/TimeSeriesWhereClauseModifier.cs
+++ src/Raven.Client/Documents/Queries/TimeSeries/TimeSeriesWhereClauseModifier.cs
@@ -1,15 +1,20 @@
 using System.Linq.Expressions;
+using Raven.Client.Documents.Linq;
+using Raven.Client.Documents.Session;
 using Raven.Client.Util;
 
 namespace Raven.Client.Documents.Queries.TimeSeries
 {
-    internal class TimeSeriesWhereClauseModifier : ExpressionVisitor
+    internal class TimeSeriesWhereClauseModifier<T> : ExpressionVisitor
     {
         private readonly string _parameter;
 
-        public TimeSeriesWhereClauseModifier(string parameter)
+        private IAbstractDocumentQuery<T> _documentQuery;
+
+        public TimeSeriesWhereClauseModifier(string parameter, IAbstractDocumentQuery<T> documentQuery)
         {
             _parameter = parameter;
+            _documentQuery = documentQuery;
         }
 
         public Expression Modify(Expression expression)
@@ -19,12 +24,17 @@ namespace Raven.Client.Documents.Queries.TimeSeries
 
         protected override Expression VisitMember(MemberExpression node)
         {
+            // - removes the lambda parameter from filter expression
+            // - turns constant expressions into query parameters 
+
             if (node.Expression is ParameterExpression p && p.Name == _parameter)
                 return Expression.Parameter(node.Type, node.Member.Name);
 
             if (JavascriptConversionExtensions.IsWrappedConstantExpression(node))
             {
-                // todo aviv
+                LinqPathProvider.GetValueFromExpressionWithoutConversion(node, out var value);
+                var queryParameter = _documentQuery.ProjectionParameter(value);
+                return Expression.Parameter(node.Type, queryParameter);
             }
 
             return base.VisitMember(node);
