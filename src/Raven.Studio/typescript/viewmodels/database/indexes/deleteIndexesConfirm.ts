091864d1154e5a9ebc78cedc1ec8921b13d95a60
Danielle Greenberg <danielle.9897@gmail.com>
2019-12-17 09:31:00 +0200

    RavenDB-14346 Fix Review Comments 3 (unified variables in index and indexDefinition)

--- src/Raven.Studio/typescript/viewmodels/database/indexes/deleteIndexesConfirm.ts
+++ src/Raven.Studio/typescript/viewmodels/database/indexes/deleteIndexesConfirm.ts
@@ -13,18 +13,10 @@ class indexInfoForDelete {
 
     readonly referenceCollectionExtension = "/References";
     
-    static fromIndex(index: index) {
-        return new indexInfoForDelete(index.name, index.reduceOutputCollection(), !!index.reduceOutputReferencePattern());   
-    }
-    
-    static fromIndexDefinition(index: indexDefinition) {
-        return new indexInfoForDelete(index.name(), index.reduceToCollectionName(), index.patternForOutput());
-    }
-    
-    private constructor(indexName: string, reduceOutputCollection: string, hasReferenceCollection: boolean) {
+    constructor(indexName: string, reduceOutputCollection: string, hasPatternForReduceOutputCollection: boolean) {
         this.indexName = indexName;
         this.reduceOutputCollection = reduceOutputCollection;
-        this.referenceCollection = hasReferenceCollection ? reduceOutputCollection + this.referenceCollectionExtension : "";
+        this.referenceCollection = hasPatternForReduceOutputCollection ? reduceOutputCollection + this.referenceCollectionExtension : "";
     }
 }
 
@@ -33,35 +25,31 @@ class deleteIndexesConfirm extends dialogViewModelBase {
     subTitleHtml: string;
     showWarning: boolean = false;
     deleteTask = $.Deferred<boolean>();
-    
-    indexesInfoForDelete = ko.observableArray<indexInfoForDelete>();
-    
+
+    indexesInfoForDelete = Array<indexInfoForDelete>();
+
     constructor(private indexes: index[] | indexDefinition[], private db: database) {
         super();
 
         if (!indexes || indexes.length === 0) {
             throw new Error("Indexes must not be null or empty.");
         }
-
-        if (indexes[0] instanceof index) {
-            this.indexesInfoForDelete((indexes as index[]).map(x => indexInfoForDelete.fromIndex(x)));
-        } else {
-            this.indexesInfoForDelete((indexes as indexDefinition[]).map(x => indexInfoForDelete.fromIndexDefinition(x)));
-        }
         
-        if (this.indexesInfoForDelete().length === 1) {
+        this.indexesInfoForDelete = (indexes as index[]).map(x => new indexInfoForDelete(x.name, x.reduceOutputCollectionName(), x.hasPatternForReduceOutputCollection()));
+
+        if (this.indexesInfoForDelete.length === 1) {
             this.title = "Delete index?";
             this.subTitleHtml = `You're deleting index:`;
         } else {
             this.title = "Delete indexes?";
-            this.subTitleHtml = `You're deleting <strong>${this.indexesInfoForDelete().length}</strong> indexes:`;
+            this.subTitleHtml = `You're deleting <strong>${this.indexesInfoForDelete.length}</strong> indexes:`;
         }
 
-        this.showWarning = _.some(this.indexesInfoForDelete(), x => x.reduceOutputCollection);
+        this.showWarning = _.some(this.indexesInfoForDelete, x => x.reduceOutputCollection);
     }
 
     deleteIndexes() {
-        const deleteTasks = this.indexesInfoForDelete().map(indexItem => new deleteIndexCommand(indexItem.indexName, this.db).execute());
+        const deleteTasks = this.indexesInfoForDelete.map(indexItem => new deleteIndexCommand(indexItem.indexName, this.db).execute());
 
         $.when.apply($, deleteTasks)
             .done(() => {
