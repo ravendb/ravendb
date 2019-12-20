ae33d0b0a7aa40d8a780a8b8c6b0cbc3505d9527
ml054 <marcin@ravendb.net>
2019-12-20 14:07:47 +0100

    RavenDB-14381 Allow to modify the database record from the studio

---
+++ src/Raven.Studio/typescript/commands/resources/saveDatabaseSettingsCommand.ts
@@ -0,0 +1,23 @@
+import commandBase = require("commands/commandBase");
+import database = require("models/resources/database");
+import endpoints = require("endpoints");
+
+class saveDatabaseSettingsCommand extends commandBase {
+    constructor(private db: database, private databaseRecord: documentDto, private etag: number) {
+        super();
+
+        if (!db) {
+            throw new Error("Must specify database");
+        }
+    }
+    
+    execute(): JQueryPromise<void> {
+        const url = endpoints.global.adminDatabases.adminDatabases;
+        
+        return this.put<void>(url, JSON.stringify(this.databaseRecord), null, { headers: { "ETag": this.etag }})
+            .done(() => this.reportSuccess("Saved Database Record"))
+            .fail((response: JQueryXHR) => this.reportError("Failed to save Database Record", response.responseText, response.statusText));
+    }
+}
+
+export = saveDatabaseSettingsCommand;
