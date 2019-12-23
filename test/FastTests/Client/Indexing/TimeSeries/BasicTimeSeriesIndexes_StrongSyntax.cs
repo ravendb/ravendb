d2a4f54a2816cbcd9ca61ccabb49232d7a2ed7b7
Pawel Pekrol <ppekrol@ravendb.net>
2019-12-23 13:21:13 +0100

    RavenDB-13078 added map-reduce with load

--- test/FastTests/Client/Indexing/TimeSeries/BasicTimeSeriesIndexes_StrongSyntax.cs
+++ test/FastTests/Client/Indexing/TimeSeries/BasicTimeSeriesIndexes_StrongSyntax.cs
@@ -96,6 +96,46 @@ namespace FastTests.Client.Indexing.TimeSeries
             }
         }
 
+        private class AverageHeartRateDaily_ByDateAndCity : AbstractTimeSeriesIndexCreationTask<User, AverageHeartRateDaily_ByDateAndCity.Result>
+        {
+            public class Result
+            {
+                public double HeartBeat { get; set; }
+
+                public DateTime Date { get; set; }
+
+                public string City { get; set; }
+
+                public long Count { get; set; }
+            }
+
+            public AverageHeartRateDaily_ByDateAndCity()
+            {
+                AddMap("HeartRate", timeSeries => from ts in timeSeries
+                                                  from entry in ts.Entries
+                                                  let address = LoadDocument<Address>(entry.Tag)
+                                                  select new Result
+                                                  {
+                                                      HeartBeat = entry.Value,
+                                                      Date = new DateTime(entry.TimeStamp.Date.Year, entry.TimeStamp.Date.Month, entry.TimeStamp.Date.Day),
+                                                      City = address.City,
+                                                      Count = 1
+                                                  });
+
+                Reduce = results => from r in results
+                                    group r by new { r.Date, r.City } into g
+                                    let sumHeartBeat = g.Sum(x => x.HeartBeat)
+                                    let sumCount = g.Sum(x => x.Count)
+                                    select new
+                                    {
+                                        HeartBeat = sumHeartBeat / sumCount,
+                                        Date = g.Key.Date,
+                                        City = g.Key.City,
+                                        Count = sumCount
+                                    };
+            }
+        }
+
         [Fact]
         public void BasicMapIndex()
         {
@@ -599,6 +639,155 @@ namespace FastTests.Client.Indexing.TimeSeries
                 Assert.Equal(0, terms.Length);
 
                 WaitForUserToContinueTheTest(store);
+            }
+        }
+
+        [Fact]
+        public async Task BasicMapReduceIndexWithLoad()
+        {
+            {
+                using (var store = GetDocumentStore())
+                {
+                    var today = DateTime.Today;
+
+                    using (var session = store.OpenSession())
+                    {
+                        var address = new Address { City = "NY" };
+
+                        session.Store(address, "addresses/1");
+
+                        var user = new User();
+
+                        user.AddressId = address.Id;
+
+                        session.Store(user, "users/1");
+
+                        for (int i = 0; i < 10; i++)
+                        {
+                            session.TimeSeriesFor(user).Append("HeartRate", today.AddHours(i), address.Id, new double[] { 180 + i });
+                        }
+
+                        session.SaveChanges();
+                    }
+
+                    store.Maintenance.Send(new StopIndexingOperation());
+
+                    var timeSeriesIndex = new AverageHeartRateDaily_ByDateAndCity();
+                    var indexName = timeSeriesIndex.IndexName;
+                    var indexDefinition = timeSeriesIndex.CreateIndexDefinition();
+                    RavenTestHelper.AssertEqualRespectingNewLines("timeSeries.Users.HeartRate.SelectMany(ts => ts.Entries, (ts, entry) => new {\r\n    ts = ts,\r\n    entry = entry\r\n}).Select(this0 => new {\r\n    this0 = this0,\r\n    address = this.LoadDocument(this0.entry.Tag, \"Addresses\")\r\n}).Select(this1 => new {\r\n    HeartBeat = this1.this0.entry.Value,\r\n    Date = new DateTime((int) this1.this0.entry.TimeStamp.Date.Year, (int) this1.this0.entry.TimeStamp.Date.Month, (int) this1.this0.entry.TimeStamp.Date.Day),\r\n    City = this1.address.City,\r\n    Count = 1\r\n})", indexDefinition.Maps.First());
+                    RavenTestHelper.AssertEqualRespectingNewLines("results.GroupBy(r => new {\r\n    Date = r.Date,\r\n    City = r.City\r\n}).Select(g => new {\r\n    g = g,\r\n    sumHeartBeat = Enumerable.Sum(g, x => ((double) x.HeartBeat))\r\n}).Select(this0 => new {\r\n    this0 = this0,\r\n    sumCount = Enumerable.Sum(this0.g, x0 => ((long) x0.Count))\r\n}).Select(this1 => new {\r\n    HeartBeat = this1.this0.sumHeartBeat / ((double) this1.sumCount),\r\n    Date = this1.this0.g.Key.Date,\r\n    City = this1.this0.g.Key.City,\r\n    Count = this1.sumCount\r\n})", indexDefinition.Reduce);
+
+                    timeSeriesIndex.Execute(store);
+
+                    var staleness = store.Maintenance.Send(new GetIndexStalenessOperation(indexName));
+                    Assert.True(staleness.IsStale);
+                    Assert.Equal(1, staleness.StalenessReasons.Count);
+                    Assert.True(staleness.StalenessReasons.Any(x => x.Contains("There are still")));
+
+                    store.Maintenance.Send(new StartIndexingOperation());
+
+                    WaitForIndexing(store);
+
+                    staleness = store.Maintenance.Send(new GetIndexStalenessOperation(indexName));
+                    Assert.False(staleness.IsStale);
+
+                    var terms = store.Maintenance.Send(new GetTermsOperation(indexName, "HeartBeat", null));
+                    Assert.Equal(1, terms.Length);
+                    Assert.Contains("184.5", terms);
+
+                    terms = store.Maintenance.Send(new GetTermsOperation(indexName, "Date", null));
+                    Assert.Equal(1, terms.Length);
+                    Assert.Contains(today.Date.GetDefaultRavenFormat(), terms);
+
+                    terms = store.Maintenance.Send(new GetTermsOperation(indexName, "City", null));
+                    Assert.Equal(1, terms.Length);
+                    Assert.Contains("ny", terms);
+
+                    terms = store.Maintenance.Send(new GetTermsOperation(indexName, "Count", null));
+                    Assert.Equal(1, terms.Length);
+                    Assert.Equal("10", terms[0]);
+
+                    store.Maintenance.Send(new StopIndexingOperation());
+
+                    using (var session = store.OpenSession())
+                    {
+                        var address = session.Load<Address>("addresses/1");
+                        address.City = "LA";
+
+                        session.SaveChanges();
+                    }
+
+                    staleness = store.Maintenance.Send(new GetIndexStalenessOperation(indexName));
+                    Assert.True(staleness.IsStale);
+                    Assert.Equal(1, staleness.StalenessReasons.Count);
+                    Assert.True(staleness.StalenessReasons.Any(x => x.Contains("There are still")));
+
+                    store.Maintenance.Send(new StartIndexingOperation());
+
+                    WaitForIndexing(store);
+
+                    staleness = store.Maintenance.Send(new GetIndexStalenessOperation(indexName));
+                    Assert.False(staleness.IsStale);
+
+                    terms = store.Maintenance.Send(new GetTermsOperation(indexName, "City", null));
+                    Assert.Equal(1, terms.Length);
+                    Assert.Contains("la", terms);
+
+                    store.Maintenance.Send(new StopIndexingOperation());
+
+                    using (var session = store.OpenSession())
+                    {
+                        session.Delete("addresses/1");
+
+                        session.SaveChanges();
+                    }
+
+                    staleness = store.Maintenance.Send(new GetIndexStalenessOperation(indexName));
+                    Assert.True(staleness.IsStale);
+                    Assert.Equal(1, staleness.StalenessReasons.Count);
+                    Assert.True(staleness.StalenessReasons.Any(x => x.Contains("There are still")));
+
+                    store.Maintenance.Send(new StartIndexingOperation());
+
+                    WaitForIndexing(store);
+
+                    terms = store.Maintenance.Send(new GetTermsOperation(indexName, "City", null));
+                    Assert.Equal(1, terms.Length);
+                    Assert.Equal("NULL_VALUE", terms[0]);
+
+                    // delete source document
+
+                    store.Maintenance.Send(new StopIndexingOperation());
+
+                    using (var session = store.OpenSession())
+                    {
+                        session.Delete("users/1");
+
+                        session.SaveChanges();
+                    }
+
+                    staleness = store.Maintenance.Send(new GetIndexStalenessOperation(indexName));
+                    Assert.True(staleness.IsStale);
+                    Assert.Equal(1, staleness.StalenessReasons.Count);
+                    Assert.True(staleness.StalenessReasons.Any(x => x.Contains("There are still")));
+
+                    store.Maintenance.Send(new StartIndexingOperation());
+
+                    WaitForIndexing(store);
+
+                    var database = await GetDatabase(store.Database);
+                    var index = database.IndexStore.GetIndex(indexName);
+
+                    using (index._contextPool.AllocateOperationContext(out TransactionOperationContext context))
+                    using (var tx = context.OpenReadTransaction())
+                    {
+                        var counts = index._indexStorage.GetReferenceTablesCount("Companies", tx);
+
+                        Assert.Equal(0, counts.ReferenceTableCount);
+                        Assert.Equal(0, counts.CollectionTableCount);
+                    }
+                }
             }
         }
 
