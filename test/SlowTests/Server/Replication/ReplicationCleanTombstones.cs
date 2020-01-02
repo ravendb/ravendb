bebf9e7656f888813d3a1bfad2cd8609e70d689c
Karmel Indych <karmeli87@gmail.com>
2020-01-02 12:43:57 +0200

    RavenDB-14395 fix CleanTombstonesInTheClusterWithBackup

--- test/SlowTests/Server/Replication/ReplicationCleanTombstones.cs
+++ test/SlowTests/Server/Replication/ReplicationCleanTombstones.cs
@@ -86,8 +86,8 @@ namespace SlowTests.Server.Replication
                         FolderPath = backupPath
                     },
                     Name = "incremental",
-                    IncrementalBackupFrequency = "* * */6 * *",
-                    FullBackupFrequency = "* */6 * * *",
+                    IncrementalBackupFrequency = "0 0 1 1 *",
+                    FullBackupFrequency = "0 0 1 1 *",
                     BackupType = BackupType.Backup
                 };
 
@@ -177,7 +177,7 @@ namespace SlowTests.Server.Replication
                         FolderPath = backupPath
                     },
                     Name = "full",
-                    FullBackupFrequency = "* */6 * * *",
+                    FullBackupFrequency = "0 0 1 1 *",
                     BackupType = BackupType.Backup
                 };
 
