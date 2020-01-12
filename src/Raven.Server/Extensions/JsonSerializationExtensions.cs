4766a028864e99accf9d0e4ffca041024eab5ab5
Grisha Kotler <grisha@ayende.com>
2020-01-12 16:55:09 +0200

    RavenDB-14466 - Side by side index is created after database restart

--- src/Raven.Server/Extensions/JsonSerializationExtensions.cs
+++ src/Raven.Server/Extensions/JsonSerializationExtensions.cs
@@ -15,6 +15,7 @@ namespace Raven.Server.Extensions
             result[nameof(IndexDefinition.LockMode)] = definition.LockMode?.ToString();
             result[nameof(IndexDefinition.Priority)] = definition.Priority?.ToString();
             result[nameof(IndexDefinition.OutputReduceToCollection)] = definition.OutputReduceToCollection;
+            result[nameof(IndexDefinition.PatternForOutputReduceToCollectionReferences)] = definition.PatternForOutputReduceToCollectionReferences;
             result[nameof(IndexDefinition.ReduceOutputIndex)] = definition.ReduceOutputIndex;
             result[nameof(IndexDefinition.Name)] = definition.Name;
             result[nameof(IndexDefinition.Reduce)] = definition.Reduce;
