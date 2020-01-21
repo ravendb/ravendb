2010b866b362138a34ed9517b073db64f03d2f99
Grisha Kotler <grisha@ayende.com>
2020-01-21 13:12:28 +0200

    RavenDB-14525 - Adding 2 documents with case insensitive names creates 2 collections

--- src/Raven.Server/Documents/DocumentsTransaction.cs
+++ src/Raven.Server/Documents/DocumentsTransaction.cs
@@ -5,6 +5,7 @@ using Raven.Client.Documents.Changes;
 using Raven.Server.Documents.Replication;
 using Raven.Server.ServerWide;
 using Raven.Server.ServerWide.Context;
+using Sparrow;
 using Voron.Impl;
 
 namespace Raven.Server.Documents
@@ -20,6 +21,8 @@ namespace Raven.Server.Documents
         private List<CounterChange> _counterNotifications;
 
         private bool _replaced;
+
+        private Dictionary<string, CollectionName> _collectionCache;
 
         public DocumentsTransaction(DocumentsOperationContext context, Transaction transaction, DocumentsChanges changes)
             : base(transaction)
@@ -107,6 +110,23 @@ namespace Raven.Server.Documents
                     _changes.RaiseNotifications(notification);
                 }
             }
+        }
+
+        public bool TryGetFromCache(string collectionName, out CollectionName name)
+        {
+            if (_collectionCache != null)
+                return _collectionCache.TryGetValue(collectionName, out name);
+
+            name = null;
+            return false;
+        }
+
+        public void AddToCache(string collectionName, CollectionName name)
+        {
+            if (_collectionCache == null)
+                _collectionCache = new Dictionary<string, CollectionName>(OrdinalIgnoreCaseStringStructComparer.Instance);
+
+            _collectionCache.Add(collectionName, name);
         }
     }
 }
