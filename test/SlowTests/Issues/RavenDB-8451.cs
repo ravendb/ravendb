5ff436d2d5f8742d151281f79e8f5de40175dfe8
aviviadi <avivi.adi.hr@gmail.com>
2020-01-23 09:13:20 +0200

    RavenDB-13765: throw (temporary) exception with details in case of CanRecoverEncryptedDatabase assertion failure

--- test/SlowTests/Issues/RavenDB-8451.cs
+++ test/SlowTests/Issues/RavenDB-8451.cs
@@ -7,9 +7,15 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using FastTests;
+using Orders;
+using Raven.Client.Documents.Attachments;
 using Raven.Client.Documents.Operations;
+using Raven.Client.Documents.Operations.Attachments;
 using Raven.Client.Documents.Smuggler;
+using Raven.Server.Documents;
+using Raven.Server.ServerWide.Context;
 using Tests.Infrastructure;
+using Voron;
 using Voron.Recovery;
 using Xunit;
 using Xunit.Abstractions;
@@ -92,6 +98,56 @@ namespace SlowTests.Issues
 
                 var currentStats = store.Maintenance.Send(new GetStatisticsOperation());
 
+                // Temporary dump: RavenDB-13765
+                var msg = new StringBuilder();
+                if (databaseStatistics.CountOfAttachments + 1 != currentStats.CountOfAttachments ||
+                    databaseStatistics.CountOfDocuments + 1 != currentStats.CountOfDocuments)
+                {
+                    using (var session = store.OpenSession())
+                    {
+                        var list1 = session.Query<Employee>().ToList();
+                        var list2 = session.Query<Category>().ToList();
+                        
+                        foreach (Employee l1 in list1)
+                        {
+                            var opGetAttach = session.Advanced.Attachments.Get(l1, "photo.jpg");
+                            var revCnt = session.Advanced.Revisions.GetFor<Employee>(l1.Id).Count;
+                            msg.AppendLine($"Employee {l1.Id} Attachment = {opGetAttach?.Details?.DocumentId}, RevCount=" + revCnt);
+                        }
+                        foreach (Category l2 in list2)
+                        {
+                            var opGetAttach = session.Advanced.Attachments.Get(l2, "image.jpg");
+                            var revCnt = session.Advanced.Revisions.GetFor<Employee>(l2.Id).Count;
+                            msg.AppendLine($"Category {l2.Id} Attachment = {opGetAttach?.Details?.DocumentId}, RevCount=" + revCnt);
+                        }
+                        
+                        var documentDatabase = await GetDatabase(store.Database);
+                        using (documentDatabase.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+                        using (context.OpenReadTransaction())
+                        {
+                            var table = context.Transaction.InnerTransaction.OpenTable(AttachmentsStorage.AttachmentsSchema, AttachmentsStorage.AttachmentsMetadataSlice);
+                            var count = table.NumberOfEntries;
+                            var tree = context.Transaction.InnerTransaction.CreateTree(AttachmentsStorage.AttachmentsSlice);
+                            var streamsCount = tree.State.NumberOfEntries;
+                            msg.AppendLine($"count={count}, streamsCount={streamsCount}");
+
+                            using (var it = tree.Iterate(false))
+                            {
+                                if (it.Seek(Slices.BeforeAllKeys))
+                                {
+                                    do
+                                    {
+                                        msg.AppendLine("tree key=" + it.CurrentKey.Content);
+                                    } while (it.MoveNext());
+                                }
+                            }
+                        }
+                    }
+                    var currentStats2 = store.Maintenance.Send(new GetStatisticsOperation());
+                    msg.AppendLine("Get again currentStats.CountOfAttachments=" + currentStats2.CountOfAttachments);
+                    throw new Exception(msg.ToString());
+                }
+                
                 // + 1 as recovery adds some artificial items
                 Assert.Equal(databaseStatistics.CountOfAttachments + 1, currentStats.CountOfAttachments);
                 Assert.Equal(databaseStatistics.CountOfDocuments + 1, currentStats.CountOfDocuments);
