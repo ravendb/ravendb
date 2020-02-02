6314031ee929691cb11c6e7b71807508c3fb5b52
Michael Yarichuk <michael.yarichuk@gmail.com>
2020-02-02 19:39:17 +0200

    RavenDB-14688 performance monitoring

---
+++ test/Tests.Infrastructure/CommonTestsAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using Xunit;
+
+#if TESTING
+
+[assembly: TestCaseOrderer("Tests.Infrastructure.AlphabeticTestsOrderer", "Tests.Infrastructure")]
+[assembly: TestCollectionOrderer("Tests.Infrastructure.AlphabeticTestsOrderer", "Tests.Infrastructure")]
+[assembly: TestFramework("Tests.Infrastructure.XunitExtensions.PerformanceTestFramework", "Tests.Infrastructure")]
+
+#endif
