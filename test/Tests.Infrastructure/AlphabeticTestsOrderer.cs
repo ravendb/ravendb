0c39dab92a9bc99144c6c3c62f9579a0f8281206
Michael Yarichuk <michael.yarichuk@gmail.com>
2020-02-02 19:39:17 +0200

    Introduce single-threaded execution of tests with deterministic alphabetic ordering for DEBUG, VALIDATE or TEST_PERFORMANCE compilation variables - WIP (HRINT-1503)

---
+++ test/Tests.Infrastructure/AlphabeticTestsOrderer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Tests.Infrastructure
+{
+    
+    public class AlphabeticTestsOrderer : ITestCaseOrderer, ITestCollectionOrderer
+    {
+        private readonly IMessageSink _diagnosticMessageSink;
+
+        public AlphabeticTestsOrderer(IMessageSink diagnosticMessageSink) => 
+            _diagnosticMessageSink = diagnosticMessageSink;
+
+        public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases) where TTestCase : ITestCase
+        {
+            var orderedTestCases = testCases.OrderBy(x => x.DisplayName).ToList();
+
+            if (_diagnosticMessageSink != null) //just in case
+            {
+                var message = new DiagnosticMessage("Alphabetically ordered {0} test cases", orderedTestCases.Count);
+                _diagnosticMessageSink.OnMessage(message);
+            }
+
+            return orderedTestCases;
+        }
+
+        public IEnumerable<ITestCollection> OrderTestCollections(IEnumerable<ITestCollection> testCollections)
+            => testCollections.OrderBy(x => x.DisplayName);
+    }
+}
---
+++ test/Tests.Infrastructure/CommonTestsAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using Xunit;
+
+#if DEBUG || VALIDATE || TEST_PERFORMANCE
+
+[assembly: TestCaseOrderer("Tests.Infrastructure.AlphabeticTestsOrderer","Tests.Infrastructure")]
+[assembly: TestCollectionOrderer("Tests.Infrastructure.AlphabeticTestsOrderer","Tests.Infrastructure")]
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+#endif
