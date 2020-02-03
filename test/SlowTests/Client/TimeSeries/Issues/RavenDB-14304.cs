b7cbf006536afcdd3e07f15301d37ce8ded0adb5
Karmel Indych <karmeli87@gmail.com>
2020-02-03 11:53:15 +0200

    RavenDB-14304 fix issues when appeneding values with different dimensions

--- test/SlowTests/Client/TimeSeries/Issues/RavenDB-14304.cs
+++ test/SlowTests/Client/TimeSeries/Issues/RavenDB-14304.cs
@@ -15,7 +15,7 @@ namespace SlowTests.Client.TimeSeries.Issues
         {
         }
 
-        [Fact (Skip = "RavenDB-14304")]
+        [Fact]
         public async Task AppendingItemThatIsTooFarFromBaselineShouldGoToNewSegment()
         {
             using (var store = GetDocumentStore())
@@ -53,6 +53,143 @@ namespace SlowTests.Client.TimeSeries.Issues
                     Assert.True(segmentsOrValues[0].Segment.Start <= segmentsOrValues[0].Segment.End);
                     Assert.True(segmentsOrValues[1].Segment.Start <= segmentsOrValues[1].Segment.End);
 
+                }
+            }
+        }
+
+        [Fact]
+        public async Task AppendingItemThatIsTooFarFromBaselineShouldGoToNewSegment2()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var baseline = DateTime.Today;
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User(), "users/ayende");
+
+                    session.TimeSeriesFor("users/ayende")
+                        .Append("Heartrate", baseline, "fitbit", new[] { 60d });
+
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    // should go to a new segment
+                    session.TimeSeriesFor("users/ayende")
+                        .Append("Heartrate", baseline.AddMonths(1), "fitbit", new[] { 60d, 70 });
+                    session.TimeSeriesFor("users/ayende")
+                        .Append("Heartrate", baseline.AddMonths(2), "fitbit", new[] { 60d, 70,80 });
+
+                    session.SaveChanges();
+                }
+
+                var db = await GetDocumentDatabaseInstanceFor(store);
+                using (db.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext ctx))
+                using (ctx.OpenReadTransaction())
+                {
+                    var reader = db.DocumentsStorage.TimeSeriesStorage.GetReader(ctx, "users/ayende", "Heartrate", DateTime.MinValue, DateTime.MaxValue);
+
+                    var segmentsOrValues = reader.SegmentsOrValues().ToList();
+
+                    Assert.Equal(3, segmentsOrValues.Count);
+                    Assert.True(segmentsOrValues[0].Segment.Start <= segmentsOrValues[0].Segment.End);
+                    Assert.True(segmentsOrValues[1].Segment.Start <= segmentsOrValues[1].Segment.End);
+
+                }
+            }
+        }
+
+        [Fact]
+        public async Task AppendingItemThatIsTooFarFromBaselineShouldGoToNewSegment3()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var baseline = DateTime.Today;
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User(), "users/ayende");
+
+                    session.TimeSeriesFor("users/ayende")
+                        .Append("Heartrate", baseline, "fitbit", new[] { 60d });
+
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    // should go to a new segment
+                    session.TimeSeriesFor("users/ayende")
+                        .Append("Heartrate", baseline.AddSeconds(1), "fitbit", new[] { 60d });
+                    session.TimeSeriesFor("users/ayende")
+                        .Append("Heartrate", baseline.AddMonths(1), "fitbit", new[] { 60d, 70 });
+
+                    session.SaveChanges();
+                }
+
+                var db = await GetDocumentDatabaseInstanceFor(store);
+                using (db.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext ctx))
+                using (ctx.OpenReadTransaction())
+                {
+                    var reader = db.DocumentsStorage.TimeSeriesStorage.GetReader(ctx, "users/ayende", "Heartrate", DateTime.MinValue, DateTime.MaxValue);
+
+                    var segmentsOrValues = reader.SegmentsOrValues().ToList();
+
+                    Assert.Equal(2, segmentsOrValues.Count);
+                    Assert.True(segmentsOrValues[0].Segment.Start <= segmentsOrValues[0].Segment.End);
+                    Assert.True(segmentsOrValues[1].Segment.Start <= segmentsOrValues[1].Segment.End);
+
+                }
+            }
+        }
+
+        [Fact]
+        public async Task AppendingItemThatIsTooFarFromBaselineShouldGoToNewSegment4()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var baseline = DateTime.Today;
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User(), "users/ayende");
+
+                    session.TimeSeriesFor("users/ayende")
+                        .Append("Heartrate", baseline.AddSeconds(10), "fitbit", new[] { 10d });
+
+                    session.TimeSeriesFor("users/ayende")
+                        .Append("Heartrate", baseline.AddSeconds(20), "fitbit", new[] { 10d });
+
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User(), "users/ayende");
+
+                    session.TimeSeriesFor("users/ayende")
+                        .Append("Heartrate", baseline.AddSeconds(11), "fitbit", new[] { 10d });
+
+                    session.TimeSeriesFor("users/ayende")
+                        .Append("Heartrate", baseline.AddSeconds(12), "fitbit", new[] { 60d, 70 });
+
+
+                    session.SaveChanges();
+                }
+
+                var db = await GetDocumentDatabaseInstanceFor(store);
+                using (db.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext ctx))
+                using (ctx.OpenReadTransaction())
+                {
+                    var reader = db.DocumentsStorage.TimeSeriesStorage.GetReader(ctx, "users/ayende", "Heartrate", DateTime.MinValue, DateTime.MaxValue);
+
+                    var segmentsOrValues = reader.SegmentsOrValues().ToList();
+
+                    Assert.Equal(1, segmentsOrValues.Count);
+                    Assert.True(segmentsOrValues[0].Segment.Start <= segmentsOrValues[0].Segment.End);
+                    Assert.Equal(4, segmentsOrValues[0].Segment.Values.Count());
                 }
             }
         }
