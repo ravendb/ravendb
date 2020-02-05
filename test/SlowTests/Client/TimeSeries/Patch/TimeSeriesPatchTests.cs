1688c0b08d8b2feba50c5dfa0023fbf5b97844a6
Igal Merhavia <igal@ravendb.net>
2020-02-05 16:32:38 +0200

    RavenDB-13083 append timeseries by patch

---
+++ test/SlowTests/Client/TimeSeries/Patch/TimeSeriesPatchTests.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using FastTests;
+using Raven.Client.Documents.Commands.Batches;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Exceptions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Client.TimeSeries.Patch
+{
+    public class TimeSeriesPatchTests : RavenTestBase
+    {
+        public TimeSeriesPatchTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        class CannotAppendTimeSeriesWithNoValueByPatchCases : IEnumerable<object[]>
+        {
+            private readonly List<object[]> _data = new List<object[]>
+            {
+                new object[] {"watches/fitbit", "Heartrate", new double[] {}},
+                new object[] {"watches/fitbit", "Heartrate", new []{"some text"}},
+                new object[] {"watches/fitbit", "Heartrate", new object()},
+                new object[] {2, "Heartrate", new [] { 1d }},
+                new object[] {"watches/fitbit", 2, new [] { 1d }},
+            };
+
+            public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+        
+        [Theory]
+        [ClassData(typeof(CannotAppendTimeSeriesWithNoValueByPatchCases))]
+        public void CannotAppendTimeSeriesWithNoValueByPatch(object tag, object timeseries, object values)
+        {
+            const string documentId = "users/ayende";
+
+            using (var store = GetDocumentStore())
+            using (var session = store.OpenSession())
+            {
+                session.Store(new { Name = "Oren" }, documentId);
+                session.SaveChanges();
+                    
+                session.Advanced.Defer(new PatchCommandData(documentId, null,
+                    new PatchRequest
+                    {
+                        Script = @"appendTs(this, args.timeseries, args.timestamp, args.tag, args.values);",
+                        Values =
+                        {
+                            { "timeseries", timeseries },
+                            { "timestamp", DateTime.Today },
+                            { "tag", tag},
+                            { "values", values }
+                        }
+                    }, null));
+                
+                Assert.Throws<RavenException>(() => session.SaveChanges());
+            }
+        }
+        
+        [Theory]
+        [InlineData(new []{ 59d })]
+        [InlineData(new []{ 59d, 11d, 30d })]
+        [InlineData(new []{ -13d, 60d, 0 })]
+        public void CanAppendTimeSeriesByPatch(double[] values)
+        {
+            const string tag = "watches/fitbit";
+            const string timeseries = "Heartrate";
+            const string documentId = "users/ayende";
+            
+            using (var store = GetDocumentStore())
+            {
+                var baseline = DateTime.Today;
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new { Name = "Oren" }, documentId);
+                    session.SaveChanges();
+                    
+                    session.Advanced.Defer(new PatchCommandData(documentId, null,
+                        new PatchRequest
+                        {
+                            Script = @"appendTs(this, args.timeseries, args.timestamp, args.tag, args.values);",
+                            Values =
+                            {
+                                { "timeseries", timeseries },
+                                { "timestamp", baseline.AddMinutes(1) },
+                                { "tag", tag },
+                                { "values", values }
+                            }
+                        }, null));
+                    session.SaveChanges();
+                }
+                
+                using (var session = store.OpenSession())
+                {
+                    var val = session.TimeSeriesFor(documentId)
+                        .Get(timeseries, DateTime.MinValue, DateTime.MaxValue)
+                        .Single();
+                    Assert.Equal(values, val.Values);
+                    Assert.Equal(tag, val.Tag);
+                    Assert.Equal(baseline.AddMinutes(1), val.Timestamp);
+                }
+            }
+        }
+        
+        [Theory]
+        [InlineData(new []{ 59d })]
+        [InlineData(new []{ 59d, 11d, 30d })]
+        [InlineData(new []{ -13d, 60d, 0 })]
+        public void CanAppendTimeSeriesAsDateByPatch(double[] values)
+        {
+            const string tag = "watches/fitbit";
+            const string timeseries = "Heartrate";
+            const string documentId = "users/ayende";
+            
+            using (var store = GetDocumentStore())
+            {
+                var baseline = DateTime.Today;
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new { Name = "Oren" }, documentId);
+                    session.SaveChanges();
+                    
+                    session.Advanced.Defer(new PatchCommandData(documentId, null,
+                        new PatchRequest
+                        {
+                            Script = @"appendTs(this, args.timeseries, new Date(args.timestamp), args.tag, args.values);",
+                            Values =
+                            {
+                                { "timeseries", timeseries },
+                                { "timestamp", baseline.AddMinutes(1) },
+                                { "tag", tag },
+                                { "values", values }
+                            }
+                        }, null));
+                    session.SaveChanges();
+                }
+                
+                using (var session = store.OpenSession())
+                {
+                    var val = session.TimeSeriesFor(documentId)
+                        .Get(timeseries, DateTime.MinValue, DateTime.MaxValue)
+                        .Single();
+                    Assert.Equal(values, val.Values);
+                    Assert.Equal(tag, val.Tag);
+                    Assert.Equal(baseline.AddMinutes(1), val.Timestamp);
+                }
+            }
+        }
+    }
+}
