c92f397303de696973d8e42f4f3510a91416cd89
Egor <egor@ravendb.net>
2020-02-05 15:31:08 +0200

    RavenDB-14576: pass indexContext to TypeConverter.ToBlittableSupportedType to prevent NRE, Update js index stats on error in reduce

--- test/FastTests/Issues/RavenDB-14576.cs
+++ test/FastTests/Issues/RavenDB-14576.cs
@@ -69,8 +69,42 @@ namespace FastTests.Issues
 
                 new JavascriptIndex().Execute(store);
                 WaitForIndexing(store, timeout: TimeSpan.FromSeconds(5), allowErrors: true);
-                var x = store.Maintenance.Send(new GetIndexErrorsOperation());
-                Assert.Empty(x.First().Errors);
+
+                var indexErrors = store.Maintenance.Send(new GetIndexErrorsOperation());
+                Assert.Empty(indexErrors.First().Errors);
+            }
+        }
+
+        [Fact]
+        public void JavascriptIndexShouldThrowOnExceptionInReduceStage()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using var session = store.OpenSession();
+                for (int i = 0; i < 500; i++)
+                {
+                    session.Store(new User
+                    {
+                        UserName = $"EGOR_{i}",
+                        Etag = Guid.NewGuid().ToString(),
+                        Type = "Support",
+                        NestedItems = new NestedItem
+                        {
+                            NestedItemName = "EGOR",
+                            NestedItemId = 322
+                        }
+                    }, Guid.NewGuid().ToString());
+                }
+                session.SaveChanges();
+
+                new JavascriptIndex2().Execute(store);
+                WaitForIndexing(store, timeout: TimeSpan.FromSeconds(5), allowErrors: true);
+
+                var indexErrors = store.Maintenance.Send(new GetIndexErrorsOperation());
+                var exceptions = indexErrors.First().Errors;
+                Assert.NotEmpty(exceptions);
+                var firstException = exceptions.First();
+                Assert.StartsWith("Failed to execute reduce function", firstException.Error);
             }
         }
 
@@ -108,6 +142,39 @@ namespace FastTests.Issues
             ParentId: g.key.ParentId,
             ParentName: g.key.ParentName,
             ChildFolderOrDocument: arr
+        };
+    })"
+                };
+            }
+        }
+
+        private class JavascriptIndex2 : AbstractIndexCreationTask
+        {
+            public override string IndexName => "JavascriptIndex2";
+
+            public override IndexDefinition CreateIndexDefinition()
+            {
+                return new IndexDefinition
+                {
+                    Maps =
+                    {
+                        @"map(""Users"", (user) => {
+    return {
+        ParentId: user.NestedItems.NestedItemId,
+        ParentName: user.NestedItems.NestedItemName,
+        Count: 1
+    };
+})"
+                    },
+                    Reduce = @"groupBy(x => ({
+    ParentId: x.ParentId,
+    ParentName: x.ParentName
+})).aggregate(g => {
+    var count = g.values.reduce((count, val) => val.Count + count, 0);
+        return {
+            ParentId: g.key.ParentId,
+            ParentName: g.key.ParentName,
+            Count: count.substring(1, 4)
         };
     })"
                 };
