6dfdaea4678040935f9877a5f87fc2ab4309dde1
aviv <aviv@ayende.com>
2020-02-05 16:35:01 +0200

    RavenDB-14572 : time series range aggregations should be exclusive to the range's end point i.e. [from, to)

--- src/Raven.Server/Documents/Queries/Results/TimeSeriesRetriever.cs
+++ src/Raven.Server/Documents/Queries/Results/TimeSeriesRetriever.cs
@@ -93,7 +93,7 @@ namespace Raven.Server.Documents.Queries.Results
 
             void MaybeMoveToNextRange(DateTime ts)
             {
-                if (ts <= next)
+                if (ts < next)
                     return;
 
                 if (aggStates[0].Any)
