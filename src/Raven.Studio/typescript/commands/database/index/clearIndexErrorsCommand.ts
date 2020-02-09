f0194dbd67b59529c9e4eb9cc1ea2139601977ca
Danielle Greenberg <danielle.9897@gmail.com>
2020-02-09 13:11:32 +0200

    RavenDB-6967 Add the ability to clear indexing errors - Fix Review Comments

--- src/Raven.Studio/typescript/commands/database/index/clearIndexErrorsCommand.ts
+++ src/Raven.Studio/typescript/commands/database/index/clearIndexErrorsCommand.ts
@@ -2,15 +2,17 @@ import commandBase = require("commands/commandBase");
 import database = require("models/resources/database");
 import endpoints = require("endpoints");
 
-class clearIndexErrorsCommand extends commandBase {    
-    constructor(private indexName: string, private db: database) {
+class clearIndexErrorsCommand extends commandBase {
+    constructor(private indexesNames: string[], private db: database) {
         super();
     }
 
     execute(): JQueryPromise<void> {
-        const url = `${endpoints.databases.index.indexesErrors}${this.indexName ? this.urlEncodeArgs({ name: this.indexName }) : ""}`;
+        const args =  this.indexesNames ? { name: this.indexesNames } : "";
+        const url = endpoints.databases.index.indexesErrors + this.urlEncodeArgs(args);
 
-        return this.del(url, null, this.db);
+        return this.del<void>(url, null, this.db)
+            .done(() => this.reportSuccess(`Successfully cleared errors from ${this.indexesNames ? 'selected' : 'all'} indexes`));
     }
 }
 
