1ac1abcc0bf297cf3280957a398c862825bad7be
Karmel Indych <karmeli87@gmail.com>
2020-02-12 23:17:52 +0200

    RavenDB-14634 aggregate test and dispose exceptions

--- test/FastTests/InfrastructureTests.cs
+++ test/FastTests/InfrastructureTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Raven.Client.Exceptions;
 using Sparrow.Server;
 using Tests.Infrastructure;
 using Xunit;
@@ -16,13 +17,29 @@ namespace FastTests
         [Fact]
         public async Task CanPropagateException()
         {
-            var ae = await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+            var ae = await Assert.ThrowsAsync<AggregateException>(async () =>
             {
                 var cluster = await CreateRaftCluster(2, leaderIndex: 0);
                 using (var store = GetDocumentStore(new Options{Server = cluster.Leader}))
                 {
                     cluster.Nodes[1].Dispose();
-                    throw new InvalidOperationException(); // this is the real exception
+                    throw new InvalidOperationException("Cows can fly!"); // this is the real exception
+                }
+            });
+
+            Assert.IsType(typeof(InvalidOperationException), ae.InnerExceptions[0]);
+            Assert.IsType(typeof(AllTopologyNodesDownException), ae.InnerExceptions[1]);
+        }
+
+        [Fact]
+        public async Task CanCatchException()
+        {
+            await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+            {
+                var cluster = await CreateRaftCluster(2, leaderIndex: 0);
+                using (var store = GetDocumentStore(new Options{Server = cluster.Leader}))
+                {
+                    throw new InvalidOperationException("Cows can fly!"); // this is the real exception
                 }
             });
         }
