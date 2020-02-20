af793a6e6e60d216da767891e0faa34cd6c913f1
Karmel Indych <karmeli87@gmail.com>
2020-02-20 15:44:05 +0200

    RDBS-68 Fix etag overflow on change vector merge

--- test/FastTests/Server/Basic/ChangeVectorConflictStatusTests.cs
+++ test/FastTests/Server/Basic/ChangeVectorConflictStatusTests.cs
@@ -15,6 +15,31 @@ namespace FastTests.Server.Basic
         }
 
         [Fact]
+        public void EtagShouldNotOverflow()
+        {
+            var cv1 =
+                "A:86865297-V8jm+M9QKkuvfEUTQBfOtA, " +
+                "C:87142328-5j4moMb8A0KxxcL9GhY/nw, " +
+                "B:2146533895-SKM7aNMmSkW92wrQke+D4g, " +
+                "E:1856361198-/mqfiL1AxkGlsqx1zwh2rw, " +
+                "D:1882901489-TqJlheobc0KTcLDerIQ9oQ, " +
+                "D:17267243-/3+4WZUBGkWL6/J4GMv2GA, " +
+                "D:46103608-P1lQdjeAckGkdmY9RWr/Bg, " +
+                "A:27850500-iUMDTgYwOkG25uod1g6gSg";
+            
+            var cv2 =
+                "C:87142328-5j4moMb8A0KxxcL9GhY/nw, " +
+                "B:2146533895-SKM7aNMmSkW92wrQke+D4g, " +
+                "E:1856361198-/mqfiL1AxkGlsqx1zwh2rw, " +
+                "D:1882901489-TqJlheobc0KTcLDerIQ9oQ, " +
+                "A:27850500-iUMDTgYwOkG25uod1g6gSg, " +
+                "A:86865297-V8jm+M9QKkuvfEUTQBfOtA, " +
+                "A:2319854662-eCGjjCNbP0CeTGSJMeqLZA";
+
+            ChangeVectorUtils.MergeVectors(cv1, cv2).ToChangeVector();
+        }
+
+        [Fact]
         public void Two_empty_ChangeVectors()
         {
             Assert.Equal(ConflictStatus.AlreadyMerged, ChangeVectorUtils.GetConflictStatus(string.Empty, string.Empty));
