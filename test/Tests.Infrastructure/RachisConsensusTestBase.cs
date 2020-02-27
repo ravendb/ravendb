0aabb0dae293502b36f63e6ed7af970df32bc2da
Egor <egor@ravendb.net>
2020-02-27 16:41:58 +0200

    RavenDB-14348: fix timeout exception when leader got the same command twice

--- test/Tests.Infrastructure/RachisConsensusTestBase.cs
+++ test/Tests.Infrastructure/RachisConsensusTestBase.cs
@@ -463,5 +463,25 @@ namespace Tests.Infrastructure
                 return djv;
             }
         }
+
+        internal class TestCommandWithRaftId : CommandBase
+        {
+            private string Name;
+
+            private object Value;
+
+            public TestCommandWithRaftId(string name, string uniqueRequestId) : base(uniqueRequestId)
+            {
+                Name = name;
+            }
+            public override DynamicJsonValue ToJson(JsonOperationContext context)
+            {
+                var djv = base.ToJson(context);
+                djv[nameof(Name)] = Name;
+                djv[nameof(Value)] = Value;
+
+                return djv;
+            }
+        }
     }
 }
