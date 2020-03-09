31346de0697c39060e3468e855f966c12ed8a64e
Grisha Kotler <grisha@ayende.com>
2020-03-09 13:30:48 +0200

    HRINT-1536 - add ToggleDatabasesStateCommand

---
+++ src/Raven.Server/ServerWide/Commands/ToggleDatabasesStateCommand.cs
@@ -0,0 +1,55 @@
+﻿using Raven.Server.Utils;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Server.ServerWide.Commands
+{
+    public class ToggleDatabasesStateCommand : UpdateValueCommand<ToggleParameters>
+    {
+        protected ToggleDatabasesStateCommand()
+        {
+            // for deserialization
+        }
+
+        public ToggleDatabasesStateCommand(ToggleParameters toggleParameters, string uniqueRequestId) : base(uniqueRequestId)
+        {
+            Value = toggleParameters;
+        }
+
+        public override object ValueToJson()
+        {
+            return Value.ToJson();
+        }
+
+        public override BlittableJsonReaderObject GetUpdatedValue(JsonOperationContext context, BlittableJsonReaderObject previousValue, long index)
+        {
+            return null;
+        }
+    }
+
+    public class ToggleParameters : IDynamicJson
+    {
+        public ToggleType ToggleType { get; set; }
+
+        public string[] DatabaseNames { get; set; }
+
+        public bool State { get; set; }
+
+        public DynamicJsonValue ToJson()
+        {
+            return new DynamicJsonValue
+            {
+                [nameof(ToggleType)] = ToggleType,
+                [nameof(DatabaseNames)] = TypeConverter.ToBlittableSupportedType(DatabaseNames),
+                [nameof(State)] = State
+            };
+        }
+    }
+
+    public enum ToggleType
+    {
+        Databases,
+        Indexes,
+        DynamicDatabaseDistribution
+    }
+}
