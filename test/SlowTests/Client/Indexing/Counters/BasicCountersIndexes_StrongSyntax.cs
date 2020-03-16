65f14202f699b00f02b126edf05d1b1b4467c4a6
Pawel Pekrol <ppekrol@ravendb.net>
2020-03-16 16:43:25 +0100

    RavenDB-14738 added test for load

--- test/SlowTests/Client/Indexing/Counters/BasicCountersIndexes_StrongSyntax.cs
+++ test/SlowTests/Client/Indexing/Counters/BasicCountersIndexes_StrongSyntax.cs
@@ -1,8 +1,10 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using FastTests;
-using Orders;
 using Raven.Client.Documents.Indexes.Counters;
 using Raven.Client.Documents.Operations.Indexes;
+using Raven.Server.ServerWide.Context;
+using Raven.Tests.Core.Utils.Entities;
 using Tests.Infrastructure.Operations;
 using Xunit;
 using Xunit.Abstractions;
@@ -25,6 +27,22 @@ namespace SlowTests.Client.Indexing.Counters
                                                     HeartBeat = counter.Value,
                                                     Name = counter.Name,
                                                     User = counter.DocumentId
+                                                });
+            }
+        }
+
+        private class MyCounterIndex_Load : AbstractCountersIndexCreationTask<Company>
+        {
+            public MyCounterIndex_Load()
+            {
+                AddMap("HeartRate", counters => from counter in counters
+                                                let company = LoadDocument<Company>(counter.DocumentId)
+                                                let employee = LoadDocument<Employee>(company.Desc)
+                                                select new
+                                                {
+                                                    HeartBeat = counter.Value,
+                                                    User = counter.DocumentId,
+                                                    Employee = employee.FirstName
                                                 });
             }
         }
@@ -233,6 +251,165 @@ namespace SlowTests.Client.Indexing.Counters
                 terms = store.Maintenance.Send(new GetTermsOperation("MyCounterIndex", "HeartBeat", null));
                 Assert.Equal(1, terms.Length);
                 Assert.Contains("5", terms);
+            }
+        }
+
+        [Fact]
+        public async Task BasicMapIndexWithLoad()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var employee = new Employee
+                    {
+                        FirstName = "John"
+                    };
+                    session.Store(employee, "employees/1");
+
+                    var company = new Company
+                    {
+                        Desc = employee.Id
+                    };
+                    session.Store(company, "companies/1");
+
+                    session.CountersFor(company).Increment("HeartRate", 7);
+
+                    var company2 = new Company
+                    {
+                        Desc = employee.Id
+                    };
+                    session.Store(company2, "companies/11");
+
+                    session.CountersFor(company2).Increment("HeartRate", 11);
+
+                    session.SaveChanges();
+                }
+
+                store.Maintenance.Send(new StopIndexingOperation());
+
+                var timeSeriesIndex = new MyCounterIndex_Load();
+                var indexName = timeSeriesIndex.IndexName;
+                var indexDefinition = timeSeriesIndex.CreateIndexDefinition();
+                RavenTestHelper.AssertEqualRespectingNewLines("counters.Companies.HeartRate.Select(counter => new {\r\n    counter = counter,\r\n    company = this.LoadDocument(counter.DocumentId, \"Companies\")\r\n}).Select(this0 => new {\r\n    this0 = this0,\r\n    employee = this.LoadDocument(this0.company.Desc, \"Employees\")\r\n}).Select(this1 => new {\r\n    HeartBeat = this1.this0.counter.Value,\r\n    User = this1.this0.counter.DocumentId,\r\n    Employee = this1.employee.FirstName\r\n})", indexDefinition.Maps.First());
+
+                timeSeriesIndex.Execute(store);
+
+                var staleness = store.Maintenance.Send(new GetIndexStalenessOperation(indexName));
+                Assert.True(staleness.IsStale);
+                Assert.Equal(1, staleness.StalenessReasons.Count);
+                Assert.True(staleness.StalenessReasons.Any(x => x.Contains("There are still")));
+
+                store.Maintenance.Send(new StartIndexingOperation());
+
+                WaitForIndexing(store);
+
+                staleness = store.Maintenance.Send(new GetIndexStalenessOperation(indexName));
+                Assert.False(staleness.IsStale);
+
+                store.Maintenance.Send(new StopIndexingOperation());
+
+                var terms = store.Maintenance.Send(new GetTermsOperation(indexName, "Employee", null));
+                Assert.Equal(1, terms.Length);
+                Assert.Contains("john", terms);
+
+                using (var session = store.OpenSession())
+                {
+                    var employee = session.Load<Employee>("employees/1");
+                    employee.FirstName = "Bob";
+
+                    session.SaveChanges();
+                }
+
+                staleness = store.Maintenance.Send(new GetIndexStalenessOperation(indexName));
+                Assert.True(staleness.IsStale);
+                Assert.Equal(1, staleness.StalenessReasons.Count);
+                Assert.True(staleness.StalenessReasons.Any(x => x.Contains("There are still")));
+
+                store.Maintenance.Send(new StartIndexingOperation());
+
+                WaitForIndexing(store);
+
+                staleness = store.Maintenance.Send(new GetIndexStalenessOperation(indexName));
+                Assert.False(staleness.IsStale);
+
+                store.Maintenance.Send(new StopIndexingOperation());
+
+                terms = store.Maintenance.Send(new GetTermsOperation(indexName, "Employee", null));
+                Assert.Equal(1, terms.Length);
+                Assert.Contains("bob", terms);
+
+                using (var session = store.OpenSession())
+                {
+                    session.Delete("employees/1");
+
+                    session.SaveChanges();
+                }
+
+                staleness = store.Maintenance.Send(new GetIndexStalenessOperation(indexName));
+                Assert.True(staleness.IsStale);
+                Assert.Equal(1, staleness.StalenessReasons.Count);
+                Assert.True(staleness.StalenessReasons.Any(x => x.Contains("There are still")));
+
+                store.Maintenance.Send(new StartIndexingOperation());
+
+                WaitForIndexing(store);
+
+                staleness = store.Maintenance.Send(new GetIndexStalenessOperation(indexName));
+                Assert.False(staleness.IsStale);
+
+                terms = store.Maintenance.Send(new GetTermsOperation(indexName, "Employee", null));
+                Assert.Equal(0, terms.Length);
+
+                // delete source document
+
+                store.Maintenance.Send(new StopIndexingOperation());
+
+                using (var session = store.OpenSession())
+                {
+                    session.Delete("companies/1");
+
+                    session.SaveChanges();
+                }
+
+                staleness = store.Maintenance.Send(new GetIndexStalenessOperation(indexName));
+                Assert.True(staleness.IsStale);
+                Assert.Equal(2, staleness.StalenessReasons.Count);
+                Assert.True(staleness.StalenessReasons.All(x => x.Contains("There are still")));
+
+                store.Maintenance.Send(new StartIndexingOperation());
+
+                WaitForIndexing(store);
+
+                var database = await GetDatabase(store.Database);
+                var index = database.IndexStore.GetIndex(indexName);
+
+                using (index._contextPool.AllocateOperationContext(out TransactionOperationContext context))
+                using (var tx = context.OpenReadTransaction())
+                {
+                    var counts = index._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Companies", tx);
+
+                    Assert.Equal(1, counts.ReferenceTableCount);
+                    Assert.Equal(1, counts.CollectionTableCount);
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    session.Delete("companies/11");
+
+                    session.SaveChanges();
+                }
+
+                WaitForIndexing(store);
+
+                using (index._contextPool.AllocateOperationContext(out TransactionOperationContext context))
+                using (var tx = context.OpenReadTransaction())
+                {
+                    var counts = index._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Companies", tx);
+
+                    Assert.Equal(0, counts.ReferenceTableCount);
+                    Assert.Equal(0, counts.CollectionTableCount);
+                }
             }
         }
     }
