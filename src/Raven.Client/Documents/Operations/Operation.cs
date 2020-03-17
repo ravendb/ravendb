d9dbea68d3a41613954c17f97345797b92674855
egor <egor@ravendb.net>
2020-03-17 14:59:23 +0200

    RavenDB-14743: Handle exception from WaitWithTimeout in WaitForCompletionAsync

--- src/Raven.Client/Documents/Operations/Operation.cs
+++ src/Raven.Client/Documents/Operations/Operation.cs
@@ -61,21 +61,7 @@ namespace Raven.Client.Documents.Operations
             }
             catch (Exception e)
             {
-                await _lock.WaitAsync().ConfigureAwait(false);
-
-                try
-                {
-                    StopProcessing();
-                }
-                catch
-                {
-                    // ignoring
-                }
-                finally
-                {
-                    _result.TrySetException(e);
-                    _lock.Release();
-                }
+                await StopProcessingUnderLock(e).ConfigureAwait(false);
             }
         }
 
@@ -138,21 +124,27 @@ namespace Raven.Client.Documents.Operations
             }
             catch (Exception e)
             {
-                await _lock.WaitAsync().ConfigureAwait(false);
+                await StopProcessingUnderLock(e).ConfigureAwait(false);
+            }
+        }
 
-                try
-                {
-                    StopProcessing();
-                }
-                catch
-                {
-                    // ignoring
-                }
-                finally
-                {
+        private async Task StopProcessingUnderLock(Exception e = null)
+        {
+            await _lock.WaitAsync().ConfigureAwait(false);
+
+            try
+            {
+                StopProcessing();
+            }
+            catch
+            {
+                // ignoring
+            }
+            finally
+            {
+                if (e != null)
                     _result.TrySetException(e);
-                    _lock.Release();
-                }
+                _lock.Release();
             }
         }
 
@@ -307,25 +299,20 @@ namespace Raven.Client.Documents.Operations
 #pragma warning disable 4014
                 Task.Factory.StartNew(Initialize);
 #pragma warning restore 4014
+                bool completed;
+                try
+                {
+                    completed = await result.Task.WaitWithTimeout(timeout).ConfigureAwait(false);
+                }
+                catch 
+                {
+                    await StopProcessingUnderLock().ConfigureAwait(false);
+                    completed = true;
+                }
 
-                var completed = await result.Task.WaitWithTimeout(timeout).ConfigureAwait(false);
                 if (completed == false)
                 {
-                    await _lock.WaitAsync().ConfigureAwait(false);
-
-                    try
-                    {
-                        StopProcessing();
-                    }
-                    catch
-                    {
-                        // ignoring
-                    }
-                    finally
-                    {
-                        _lock.Release();
-                    }
-
+                    await StopProcessingUnderLock().ConfigureAwait(false);
                     throw new TimeoutException($"After {timeout}, did not get a reply for operation " + _id);
                 }
 
