e4e7703723e3bb967c2dc49cfb69e8ed6a5f9469
egor <egor@ravendb.net>
2020-03-23 18:13:21 +0200

    RavenDB-14596: pr comment, remove unused code

--- src/Raven.Client/Exceptions/Database/DatabaseConcurrentLoadTimeoutException.cs
+++ src/Raven.Client/Exceptions/Database/DatabaseConcurrentLoadTimeoutException.cs
@@ -4,8 +4,6 @@ namespace Raven.Client.Exceptions.Database
 {
     public class DatabaseConcurrentLoadTimeoutException : RavenException
     {
-        public string Caller { get; set; }
-
         public DatabaseConcurrentLoadTimeoutException(string message)
             : base(message)
         {
