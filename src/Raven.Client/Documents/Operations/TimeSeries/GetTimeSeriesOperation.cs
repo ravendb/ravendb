9ce7d6d79ec99a759888458a472d0ff60ec98ba6
aviv86 <aviv@ayende.com>
2020-04-02 19:38:18 +0300

    RavenDB-14651 : Rework GetTimeSeries client api and endpoint - initial work

--- src/Raven.Client/Documents/Operations/TimeSeries/GetTimeSeriesOperation.cs
+++ src/Raven.Client/Documents/Operations/TimeSeries/GetTimeSeriesOperation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Net.Http;
 using System.Text;
 using Raven.Client.Documents.Conventions;
@@ -13,41 +14,42 @@ namespace Raven.Client.Documents.Operations.TimeSeries
     public class GetTimeSeriesOperation : IOperation<TimeSeriesDetails>
     {
         private readonly string _docId;
-        private readonly string _timeseries;
+        //private readonly string _timeseries;
         private readonly IEnumerable<TimeSeriesRange> _ranges;
         private readonly int _start;
         private readonly int _pageSize;
 
         public GetTimeSeriesOperation(string docId, string timeseries, DateTime from, DateTime to, int start = 0, int pageSize = int.MaxValue)
-            : this(docId, timeseries, start, pageSize)
+            : this(docId/*, timeseries*/, start, pageSize)
         {
             _ranges = new List<TimeSeriesRange>
             {
                 new TimeSeriesRange
                 {
+                    Name = timeseries,
                     From = from,
                     To = to
                 }
             };
         }
 
-        public GetTimeSeriesOperation(string docId, string timeseries, IEnumerable<TimeSeriesRange> ranges, int start = 0, int pageSize = int.MaxValue) 
-            : this(docId, timeseries, start, pageSize)
+        public GetTimeSeriesOperation(string docId/*, string timeseries*/, IEnumerable<TimeSeriesRange> ranges, int start = 0, int pageSize = int.MaxValue) 
+            : this(docId/*, timeseries*/, start, pageSize)
         {
-            _ranges = ranges;
+            _ranges = ranges ?? throw new InvalidEnumArgumentException($"Missing argument '{nameof(ranges)}' in GetTimeSeriesOperation");
         }
 
-        private GetTimeSeriesOperation(string docId, string timeseries, int start, int pageSize)
+        private GetTimeSeriesOperation(string docId, /*string timeseries,*/ int start, int pageSize)
         {
             _docId = docId;
-            _timeseries = timeseries;
+            //_timeseries = timeseries;
             _start = start;
             _pageSize = pageSize;
         }
 
         public RavenCommand<TimeSeriesDetails> GetCommand(IDocumentStore store, DocumentConventions conventions, JsonOperationContext context, HttpCache cache)
         {
-            return new GetTimeSeriesCommand(_docId, _timeseries, _ranges, _start, _pageSize);
+            return new GetTimeSeriesCommand(_docId/*, _timeseries*/, _ranges, _start, _pageSize);
         }
 
         private class GetTimeSeriesCommand : RavenCommand<TimeSeriesDetails>
@@ -58,10 +60,10 @@ namespace Raven.Client.Documents.Operations.TimeSeries
             private readonly int _start;
             private readonly int _pageSize;
 
-            public GetTimeSeriesCommand(string docId, string timeseries, IEnumerable<TimeSeriesRange> ranges, int start, int pageSize)
+            public GetTimeSeriesCommand(string docId/*, string timeseries*/, IEnumerable<TimeSeriesRange> ranges, int start, int pageSize)
             {
                 _docId = docId ?? throw new ArgumentNullException(nameof(docId));
-                _timeseries = timeseries ?? throw new ArgumentNullException(nameof(timeseries));
+                //_timeseries = timeseries ?? throw new ArgumentNullException(nameof(timeseries));
                 _ranges = ranges;
                 _start = start;
                 _pageSize = pageSize;
@@ -76,8 +78,8 @@ namespace Raven.Client.Documents.Operations.TimeSeries
 
                 pathBuilder.Append("?id=")
                     .Append(Uri.EscapeDataString(_docId))
-                    .Append("&name=")
-                    .Append(Uri.EscapeDataString(_timeseries));
+                    /*.Append("&name=")
+                    .Append(Uri.EscapeDataString(_timeseries))*/;
 
                 if (_start > 0)
                 {
@@ -95,7 +97,9 @@ namespace Raven.Client.Documents.Operations.TimeSeries
                 {
                     foreach (var range in _ranges)
                     {
-                        pathBuilder.Append("&from=")
+                        pathBuilder.Append("&name=")
+                            .Append(range.Name)
+                            .Append("&from=")
                             .Append(range.From.GetDefaultRavenFormat())
                             .Append("&to=")
                             .Append(range.To.GetDefaultRavenFormat());
@@ -127,7 +131,7 @@ namespace Raven.Client.Documents.Operations.TimeSeries
     public class TimeSeriesDetails
     {
         public string Id;
-        public Dictionary<string, List<TimeSeriesRangeResult>> Values = new Dictionary<string, List<TimeSeriesRangeResult>>();
-        public int TotalResults;
+        public List<FullTimeSeriesRangeResult> FullRanges;
+        public List<TimeSeriesRangeResult> PartialRanges;
     }
 }
--- src/Raven.Client/Documents/Operations/TimeSeries/TimeSeriesRangeResult.cs
+++ src/Raven.Client/Documents/Operations/TimeSeries/TimeSeriesRangeResult.cs
@@ -8,7 +8,11 @@ namespace Raven.Client.Documents.Operations.TimeSeries
         public string Name;
         public DateTime From, To;
         public TimeSeriesEntry[] Entries;
-        public bool FullRange;
         internal string Hash;
+    }
+
+    public class FullTimeSeriesRangeResult : TimeSeriesRangeResult
+    {
+        public long TotalResults;
     }
 }
