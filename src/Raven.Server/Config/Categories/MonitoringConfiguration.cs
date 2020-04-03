a3e42eba2dbee052c03f4e5e45267c7209e56b3b
Pawel Pekrol <ppekrol@ravendb.net>
2020-04-03 10:24:33 +0200

    RavenDB-5042 marking all 'exec' configurations as secured

--- src/Raven.Server/Config/Categories/MonitoringConfiguration.cs
+++ src/Raven.Server/Config/Categories/MonitoringConfiguration.cs
@@ -10,12 +10,12 @@ namespace Raven.Server.Config.Categories
     {
         [Description("A command or executable to run which will provide machine cpu usage and total machine cores to standard output. If specified, RavenDB will use this information for monitoring cpu usage.")]
         [DefaultValue(null)]
-        [ConfigurationEntry("Monitoring.Cpu.Exec", ConfigurationEntryScope.ServerWideOnly)]
+        [ConfigurationEntry("Monitoring.Cpu.Exec", ConfigurationEntryScope.ServerWideOnly, isSecured: true)]
         public string CpuUsageMonitorExec { get; set; }
 
         [Description("The command line arguments for the 'Monitoring.Cpu.Exec' command or executable. The arguments must be escaped for the command line.")]
         [DefaultValue(null)]
-        [ConfigurationEntry("Monitoring.Cpu.Exec.Arguments", ConfigurationEntryScope.ServerWideOnly)]
+        [ConfigurationEntry("Monitoring.Cpu.Exec.Arguments", ConfigurationEntryScope.ServerWideOnly, isSecured: true)]
         public string CpuUsageMonitorExecArguments { get; set; }
 
         public MonitoringConfiguration()
