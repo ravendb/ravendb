31c9c88e9fe0ea6754738093d210104582af5037
Ayende Rahien <Ayende@ayende.com>
2020-04-23 17:11:04 +0300

    RavenDB-14894 We need to keep track of index stats even before the first query

--- src/Raven.Server/Documents/Indexes/Index.cs
+++ src/Raven.Server/Documents/Indexes/Index.cs
@@ -1708,6 +1708,8 @@ namespace Raven.Server.Documents.Indexes
 
                             tx.InnerTransaction.LowLevelTransaction.AfterCommitWhenNewReadTransactionsPrevented += _ =>
                             {
+                                IndexPersistence.BuildStreamCacheAfterTx(tx.InnerTransaction);
+
                                 if (writeOperation.IsValueCreated == false)
                                     return;
 
@@ -1717,7 +1719,6 @@ namespace Raven.Server.Documents.Indexes
                                     // also we need this to be called when new read transaction are prevented in order to ensure
                                     // that queries won't get the searcher having 'old' state but see 'new' changes committed here
                                     // e.g. the old searcher could have a segment file in its in-memory state which has been removed in this tx
-                                    IndexPersistence.BuildStreamCacheAfterTx(tx.InnerTransaction);
                                     IndexPersistence.RecreateSearcher(tx.InnerTransaction);
                                     IndexPersistence.RecreateSuggestionsSearchers(tx.InnerTransaction);
                                 }
