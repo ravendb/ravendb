17f12c397d509a96c632a866cad2d4ada99413aa
Pawel Pekrol <ppekrol@ravendb.net>
2020-05-06 11:08:05 +0200

    RavenDB-14917 added index with some calculations

--- test/SlowTests/Issues/RavenDB_14917.cs
+++ test/SlowTests/Issues/RavenDB_14917.cs
@@ -20,6 +20,7 @@ namespace SlowTests.Issues
             using (var store = GetDocumentStore())
             {
                 new TimeSeriesIndex().Execute(store);
+                new TimeSeriesIndex_Complex().Execute(store);
 
                 using (var session = store.OpenSession())
                 {
@@ -34,6 +35,7 @@ namespace SlowTests.Issues
                 }
 
                 WaitForIndexing(store);
+                RavenTestHelper.AssertNoIndexErrors(store);
 
                 using (var session = store.OpenSession())
                 {
@@ -47,10 +49,23 @@ namespace SlowTests.Issues
 
                     Assert.Equal(new[] { 2, 3.5 }, result.Min);
                     Assert.Equal(new double[] { 5, 9 }, result.Sum);
-                    Assert.Equal(new [] { 3, 5.5 }, result.Max);
+                    Assert.Equal(new[] { 3, 5.5 }, result.Max);
                     Assert.Equal(2, result.Count);
                     Assert.Equal(3, result.FirstMax);
                     Assert.Equal(3.5, result.LastMin);
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var results = session
+                        .Query<TimeSeriesIndex_Complex.Result, TimeSeriesIndex_Complex>()
+                        .ToList();
+
+                    Assert.Equal(1, results.Count);
+
+                    var result = results[0];
+
+                    Assert.Equal(new double[] { 3 }, result.Values);
                 }
             }
         }
@@ -86,6 +101,27 @@ namespace SlowTests.Issues
                                              Max = segment.Max,
                                              Min = segment.Min,
                                              Sum = segment.Sum
+                                         });
+            }
+        }
+
+        private class TimeSeriesIndex_Complex : AbstractTimeSeriesIndexCreationTask<Company>
+        {
+            public class Result
+            {
+                public string Name { get; set; }
+
+                public double[] Values { get; set; }
+            }
+
+            public TimeSeriesIndex_Complex()
+            {
+                AddMap("TS", segments => from segment in segments
+                                         where segment.Min[0] >= 2
+                                         select new Result
+                                         {
+                                             Name = segment.Name,
+                                             Values = segment.Entries.Where(e => e.Values[0] >= 3).Select(x => x.Value).ToArray()
                                          });
             }
         }
