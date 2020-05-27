9a8d004a57bc3fe55c2938dec5d1a99697476d5c
Ayende Rahien <Ayende@ayende.com>
2020-05-28 01:32:15 +0300

    RavenDB-15076 - Replication, Counters and Revisions do not play well together - Made sure that ForceRevision is available in the public API as well

---
+++ test/SlowTests/Issues/RavenDB_15076.cs
@@ -0,0 +1,81 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Client.Documents.Operations.ConnectionStrings;
+using Raven.Client.Documents.Operations.ETL;
+using Raven.Client.Documents.Operations.Replication;
+using Raven.Client.Documents.Session;
+using Raven.Client.Documents.Session.TimeSeries;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_15076 : ClusterTestBase
+    {
+        public RavenDB_15076(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task Counters_and_force_revisions()
+        {
+            using var storeA = GetDocumentStore();
+            using var storeB = GetDocumentStore();
+
+            using (var s = storeA.OpenAsyncSession())
+            {
+                await s.StoreAsync(new { Breed = "German Shepherd" }, "users/ayende/dogs/arava");
+                await s.StoreAsync(new { Color = "Gray/White" }, "users/pheobe");
+                await s.StoreAsync(new { Name = "Oren" }, "users/ayende");
+                s.CountersFor("users/ayende").Increment("test");
+                s.CountersFor("users/pheobe").Increment("test");
+                s.TimeSeriesFor<HeartRateMeasure>("users/pheobe").Append(DateTime.Today, new HeartRateMeasure
+                {
+                    HeartRate = 34
+                }, "test/things/out");
+                s.TimeSeriesFor<HeartRateMeasure>("users/ayende").Append(DateTime.Today, new HeartRateMeasure
+                {
+                    HeartRate = 55
+                }, "test/things/out");
+                s.Advanced.Attachments.Store("users/ayende", "test.bin", new MemoryStream(Encoding.UTF8.GetBytes("hello")));
+                s.Advanced.Attachments.Store("users/pheobe", "test.bin", new MemoryStream(Encoding.UTF8.GetBytes("hello")));
+                s.Advanced.Revisions.ForceRevisionCreationFor("users/ayende", ForceRevisionStrategy.None);
+                s.Advanced.Revisions.ForceRevisionCreationFor("users/pheobe", ForceRevisionStrategy.None);
+                await s.SaveChangesAsync();
+            }
+
+            using (var s = storeA.OpenAsyncSession())
+            {
+                await s.StoreAsync(new { Color = "Gray/White 2" }, "users/pheobe");
+                await s.StoreAsync(new { Name = "Oren 2" }, "users/ayende");
+
+                s.Advanced.Revisions.ForceRevisionCreationFor("users/ayende");
+                s.Advanced.Revisions.ForceRevisionCreationFor("users/pheobe");
+                await s.SaveChangesAsync();
+            }
+
+            await storeA.Maintenance.SendAsync(new PutConnectionStringOperation<RavenConnectionString>(new RavenConnectionString
+            {
+                Database = storeB.Database,
+                Name = storeB.Database + "ConStr",
+                TopologyDiscoveryUrls = storeA.Urls
+            }));
+            await storeA.Maintenance.SendAsync(new UpdateExternalReplicationOperation(new ExternalReplication
+            {
+                ConnectionStringName = storeB.Database + "ConStr",
+                Name = "erpl"
+            }));
+
+            Assert.True(WaitForDocument(storeB, "users/ayende"));
+            Assert.True(WaitForDocument(storeB, "users/pheobe"));
+        }
+
+        private class HeartRateMeasure
+        {
+            [TimeSeriesValue(0)] public double HeartRate;
+        }
+    }
+}
