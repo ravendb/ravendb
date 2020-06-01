61c85e0bb33cb6ccad38101b8d57a23a3fe1b694
Karmel Indych <karmeli87@gmail.com>
2020-06-01 15:55:15 +0300

    RavenDB-15038 Time series name use the same logic as collection names

--- src/Raven.Client/Documents/Session/AsyncDocumentSession.TimeSeriesFor.cs
+++ src/Raven.Client/Documents/Session/AsyncDocumentSession.TimeSeriesFor.cs
@@ -27,25 +27,25 @@ namespace Raven.Client.Documents.Session
 
         public IAsyncSessionDocumentTypedTimeSeries<TValues> TimeSeriesFor<TValues>(string documentId, string name = null) where TValues : new()
         {
-            var tsName = name ?? TimeSeriesOperations.GetTimeSeriesName<TValues>();
+            var tsName = name ?? _documentStore.TimeSeries.GetTimeSeriesName<TValues>();
             return new AsyncSessionDocumentTimeSeries<TValues>(this, documentId, tsName);
         }
 
         public IAsyncSessionDocumentTypedTimeSeries<TValues> TimeSeriesFor<TValues>(object entity, string name = null) where TValues : new()
         {
-            var tsName = name ?? TimeSeriesOperations.GetTimeSeriesName<TValues>();
+            var tsName = name ?? _documentStore.TimeSeries.GetTimeSeriesName<TValues>();
             return new AsyncSessionDocumentTimeSeries<TValues>(this, entity, tsName);
         }
 
         public IAsyncSessionDocumentRollupTypedTimeSeries<TValues> TimeSeriesRollupFor<TValues>(object entity, string policy, string raw = null) where TValues : new()
         {
-            var tsName = raw ?? TimeSeriesOperations.GetTimeSeriesName<TValues>();
+            var tsName = raw ?? _documentStore.TimeSeries.GetTimeSeriesName<TValues>();
             return new AsyncSessionDocumentTimeSeries<TValues>(this, entity, $"{tsName}{TimeSeriesConfiguration.TimeSeriesRollupSeparator}{policy}");
         }
 
         public IAsyncSessionDocumentRollupTypedTimeSeries<TValues> TimeSeriesRollupFor<TValues>(string documentId, string policy, string raw = null) where TValues : new()
         {
-            var tsName = raw ?? TimeSeriesOperations.GetTimeSeriesName<TValues>();
+            var tsName = raw ?? _documentStore.TimeSeries.GetTimeSeriesName<TValues>();
             return new AsyncSessionDocumentTimeSeries<TValues>(this, documentId, $"{tsName}{TimeSeriesConfiguration.TimeSeriesRollupSeparator}{policy}");
         }
     }
