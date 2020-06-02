bf4101e8600a6263290b2c0216112ac649d901a4
aviv86 <aviv@ayende.com>
2020-06-02 14:50:49 +0300

    RavenDB-14753 : add test

--- test/SlowTests/Issues/RavenDB-15080.cs
+++ test/SlowTests/Issues/RavenDB-15080.cs
@@ -207,6 +207,57 @@ namespace SlowTests.Issues
             }
         }
 
+        [Fact]
+        public async Task CountersShouldBeCaseInsensitive()
+        {
+            // RavenDB-14753
+
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var company = new Company
+                    {
+                        Name = "HR"
+                    };
+
+                    session.Store(company, "companies/1");
+                    session.CountersFor(company).Increment("Likes", 999);
+
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var company = session.Load<Company>("companies/1");
+                    session.CountersFor(company).Delete("lIkEs");
+
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var company = session.Load<Company>("companies/1");
+                    var counters = session.CountersFor(company).GetAll();
+
+                    Assert.Equal(0, counters.Count);
+                }
+
+                var database = await GetDocumentDatabaseInstanceFor(store);
+                var countersStorage = database.DocumentsStorage.CountersStorage;
+
+                using (database.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+                using (context.OpenReadTransaction())
+                {
+                    var values = countersStorage
+                        .GetCounterPartialValues(context, "companies/1", "Likes")
+                        .ToList();
+
+                    Assert.Equal(0, values.Count);
+                }
+            }
+        }
+
         private static string RandomString(int size, Random random)
         {
             var builder = new StringBuilder();
