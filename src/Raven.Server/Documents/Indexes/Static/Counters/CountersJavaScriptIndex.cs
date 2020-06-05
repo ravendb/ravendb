318b82b934632b862dc5ebc96911b1919ed59b93
Pawel Pekrol <ppekrol@ravendb.net>
2020-06-05 11:12:07 +0200

    RavenDB-15114 started working on static counter JS indexes

---
+++ src/Raven.Server/Documents/Indexes/Static/Counters/CountersJavaScriptIndex.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Jint;
+using Jint.Native.Object;
+using Raven.Client.Documents.Indexes;
+using Raven.Server.Config;
+using Raven.Server.Documents.Patch;
+
+namespace Raven.Server.Documents.Indexes.Static.Counters
+{
+    public class CountersJavaScriptIndex : AbstractJavaScriptIndex
+    {
+        public CountersJavaScriptIndex(IndexDefinition definition, RavenConfiguration configuration)
+            : base(definition, configuration)
+        {
+        }
+
+        protected override string MapCode => throw new System.NotImplementedException();
+
+        protected override void OnInitializeEngine(Engine engine)
+        {
+        }
+
+        protected override void ProcessMaps(ObjectInstance definitions, JintPreventResolvingTasksReferenceResolver resolver, List<string> mapList, List<MapMetadata> mapReferencedCollections, out Dictionary<string, Dictionary<string, List<JavaScriptMapOperation>>> collectionFunctions)
+        {
+            collectionFunctions = null;
+        }
+    }
+}
