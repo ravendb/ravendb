bdd25408ddfede31c2a092638dc5da874809837f
Pawel Pekrol <ppekrol@ravendb.net>
2020-06-08 17:06:05 +0200

    RavenDB-15109 added TS test

--- test/SlowTests/Issues/RavenDB_15109.cs
+++ test/SlowTests/Issues/RavenDB_15109.cs
@@ -1,4 +1,6 @@
-﻿using FastTests;
+﻿using System;
+using System.Linq;
+using FastTests;
 using SlowTests.Core.Utils.Entities;
 using Xunit;
 using Xunit.Abstractions;
@@ -42,6 +44,44 @@ namespace SlowTests.Issues
                     var counters = session.Advanced.GetCountersFor(u);
                     Assert.NotNull(counters); // fails here
                     Assert.Equal(10, counters.Count);
+                }
+            }
+        }
+
+        [Fact]
+        public void BulkIncrementNewTimeSeriesShouldAddTimeSeriesNameToMetadata()
+        {
+            using (var store = GetDocumentStore())
+            {
+                string id;
+                using (var bulkInsert = store.BulkInsert())
+                {
+                    var user = new User { Name = "Aviv1" };
+                    bulkInsert.Store(user);
+                    id = user.Id;
+
+                    for (var i = 1; i <= 10; i++)
+                    {
+                        using (var timeSeries = bulkInsert.TimeSeriesFor(id, i.ToString()))
+                            timeSeries.Append(DateTime.UtcNow, i);
+                    }
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    for (var i = 1; i <= 10; i++)
+                    {
+                        var all = session.TimeSeriesFor(id, i.ToString()).Get().ToList();
+                        Assert.Equal(1, all.Count);
+                    }
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var u = session.Load<User>(id);
+                    var timeSeries = session.Advanced.GetTimeSeriesFor(u);
+                    Assert.NotNull(timeSeries); // fails here
+                    Assert.Equal(10, timeSeries.Count);
                 }
             }
         }
