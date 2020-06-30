fbae577a8b0dc45f04036acc0b10af208bd09084
aviv86 <aviv@ayende.com>
2020-06-30 14:53:48 +0300

    RavenDB-15134 : GetCountersOperation should return null for non-existing-id (instead of filtering out). Also when we have returnFullResults = true - need to return null on empty/null/non-existing counter name

---
+++ test/SlowTests/Issues/RavenDB_15134.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using FastTests;
+using Raven.Client.Documents.Operations.Counters;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_15134 : RavenTestBase
+    {
+        public RavenDB_15134(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void GetCountersOperationShouldReturnNullForNonExistingCounter()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var docId = "users/1";
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new object(), docId);
+
+                    var c = session.CountersFor(docId);
+
+                    c.Increment("likes");
+                    c.Increment("dislikes", 2);
+
+                    session.SaveChanges();
+                }
+
+                var vals = store.Operations.Send(new GetCountersOperation(docId, new[] { "likes", "downloads", "dislikes" }));
+                Assert.Equal(3, vals.Counters.Count);
+
+                Assert.Equal(1, vals.Counters[0].TotalValue);
+                Assert.Null(vals.Counters[1]);
+                Assert.Equal(2, vals.Counters[2].TotalValue);
+
+
+                vals = store.Operations.Send(new GetCountersOperation(docId, new[] { "likes", "downloads", "dislikes" }, returnFullResults: true));
+                Assert.Equal(3, vals.Counters.Count);
+
+                Assert.Equal(1, vals.Counters[0].CounterValues.Count);
+                Assert.Null(vals.Counters[1]);
+                Assert.Equal(1, vals.Counters[2].CounterValues.Count);
+            }
+        }
+    }
+}
