56fa1c98133c66dea9f918e60cde9d7f350b0fc7
Pawel Pekrol <ppekrol@ravendb.net>
2020-07-11 16:42:39 +0200

    RavenDB-15336 fixing compilation

--- test/Tryouts/Program.cs
+++ test/Tryouts/Program.cs
@@ -1,17 +1,8 @@
 using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
-using FastTests.Blittable;
-using FastTests.Client;
-using FastTests.Client.Indexing;
-using FastTests.Issues;
-using SlowTests.Client.Counters;
-using SlowTests.Cluster;
 using SlowTests.Issues;
-using SlowTests.Voron;
-using Sparrow;
 using Tests.Infrastructure;
-using Xunit.Sdk;
 
 namespace Tryouts
 {
@@ -21,19 +12,19 @@ namespace Tryouts
         {
             XunitLogging.RedirectStreams = false;
         }
-        
+
         public static async Task Main(string[] args)
         {
             Console.WriteLine(Process.GetCurrentProcess().Id);
             //for (int i = 0; i < 10_000; i++)
             {
-               // Console.WriteLine($"Starting to run {i}");
+                // Console.WriteLine($"Starting to run {i}");
                 try
                 {
                     using (var testOutputHelper = new ConsoleTestOutputHelper())
                     using (var test = new RavenDB_9645(testOutputHelper))
                     {
-                        test.Should_correctly_reduce_after_updating_all_documents(5000);
+                        test.Should_correctly_reduce_after_updating_all_documents(5000, compressed: true);
                     }
                 }
                 catch (Exception e)
