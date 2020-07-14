54b91bada666f0541e4a2d01151b0ef0f84b3a2d
Pawel Pekrol <ppekrol@ravendb.net>
2020-07-14 09:32:08 +0200

    RavenDB-15365 support for custom Skip in Fact64BitAttribute

--- test/Tests.Infrastructure/Fact64BitAttribute.cs
+++ test/Tests.Infrastructure/Fact64BitAttribute.cs
@@ -7,8 +7,11 @@ namespace Tests.Infrastructure
     internal class Fact64BitAttribute : FactAttribute
     {
         internal static bool Is64Bit = true;
-        internal static string SkipMessage =
-            "Not supported for 32 bits.";
+
+        internal static string SkipMessage = "Not supported for 32 bits.";
+
+        private string _customSkipMessage;
+
         public Fact64BitAttribute()
         {
             if (PlatformDetails.Is32Bits)
@@ -16,18 +19,27 @@ namespace Tests.Infrastructure
                 Is64Bit = false;
                 return;
             }
-            if (bool.TryParse(Environment.GetEnvironmentVariable("VORON_INTERNAL_ForceUsing32BitsPager"), out var result))
-                if (result == false)
-                    Is64Bit = false;
+
+            if (bool.TryParse(Environment.GetEnvironmentVariable("VORON_INTERNAL_ForceUsing32BitsPager"), out var result) && result == false)
+                Is64Bit = false;
         }
+
         public override string Skip
         {
             get
             {
+                if (_customSkipMessage != null)
+                    return _customSkipMessage;
+
                 if (Is64Bit)
                     return null;
 
                 return SkipMessage;
+            }
+
+            set
+            {
+                _customSkipMessage = value;
             }
         }
     }
