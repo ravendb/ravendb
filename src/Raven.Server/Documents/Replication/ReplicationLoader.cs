27ef1c04c6aefb217b1f343146a324d5554b5981
egor <egor@ravendb.net>
2020-07-14 13:27:35 +0300

    RavenDB-15236: fix possible deadlock if DB got idle/disabled during adding replication

--- src/Raven.Server/Documents/Replication/ReplicationLoader.cs
+++ src/Raven.Server/Documents/Replication/ReplicationLoader.cs
@@ -976,7 +976,11 @@ namespace Raven.Server.Documents.Replication
                 return;
             }
 
-            _locker.EnterReadLock();
+            if (_locker.TryEnterReadLock(0) == false)
+            {
+                // the db being disposed
+                return;
+            }
 
             try
             {
