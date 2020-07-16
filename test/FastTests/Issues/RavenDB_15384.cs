b5164edbe22bb9dbc1c6c267f248e951af780b24
Arkadiusz Palinski <arek@ravendb.net>
2020-07-16 15:18:53 +0200

    RavenDB-15384 Path setting must ignore null to maintain backward compatibility

---
+++ test/FastTests/Issues/RavenDB_15384.cs
@@ -0,0 +1,34 @@
+﻿using Raven.Server.Config;
+using Raven.Server.Config.Settings;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FastTests.Issues
+{
+    public class RavenDB_15384 : NoDisposalNeeded
+    {
+        public RavenDB_15384(ITestOutputHelper output) : base(output)
+        {
+            
+        }
+
+        [Fact]
+        public void Null_path_database_setting_should_default_to_server_value()
+        {
+            var server = RavenConfiguration.CreateForServer(null);
+
+            server.SetSetting(RavenConfiguration.GetKey(x => x.Core.DataDirectory), "/mnt/Raven/Data");
+
+            server.Initialize();
+
+            var database = RavenConfiguration.CreateForDatabase(server, "foo");
+
+            database.SetSetting(RavenConfiguration.GetKey(x => x.Core.DataDirectory), null);
+
+            database.Initialize();
+
+            Assert.Equal(new PathSetting("/mnt/Raven/Data/Databases/foo").FullPath, database.Core.DataDirectory.FullPath);
+            Assert.Equal(new PathSetting("/mnt/Raven/Data/Databases/foo/Indexes").FullPath, database.Indexing.StoragePath.FullPath);
+        }
+    }
+}
