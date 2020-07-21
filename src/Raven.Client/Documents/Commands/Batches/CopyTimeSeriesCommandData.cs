9c28f61e0a5d519bd4e3a13a58a038ef560ecfa3
efrat <efrat@hibernatingrhinos.com>
2020-07-21 11:33:10 +0300

    RavenDB-14386 Copy time series when cloning document

---
+++ src/Raven.Client/Documents/Commands/Batches/CopyTimeSeriesCommandData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Raven.Client.Documents.Conventions;
+using Raven.Client.Documents.Session;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+
+
+namespace Raven.Client.Documents.Commands.Batches
+{
+    public class CopyTimeSeriesCommandData : ICommandData
+    {
+        public CopyTimeSeriesCommandData(string sourceDocumentId, string destinationDocumentId, string changeVector)
+        {
+            if (string.IsNullOrWhiteSpace(sourceDocumentId))
+                throw new ArgumentNullException(nameof(sourceDocumentId));
+            if (string.IsNullOrWhiteSpace(destinationDocumentId))
+                throw new ArgumentNullException(nameof(destinationDocumentId));
+           
+
+            Id = sourceDocumentId;
+            Name = destinationDocumentId;
+            ChangeVector = changeVector;
+        }
+        public string Id { get; }
+        public string Name { get; }
+        public string ChangeVector { get; }
+        public CommandType Type { get; } = CommandType.TimeSeriesCopy;
+        public DynamicJsonValue ToJson(DocumentConventions conventions, JsonOperationContext context)
+        {
+            return new DynamicJsonValue
+            {
+                [nameof(Id)] = Id,
+                [nameof(Name)] = Name,
+                [nameof(ChangeVector)] = ChangeVector,
+                [nameof(Type)] = Type.ToString()
+            };
+        }
+
+        public void OnBeforeSaveChanges(InMemoryDocumentSessionOperations session)
+        {
+        }
+    }
+}
