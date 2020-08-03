32dbf7fe6c062031d416613404af71564103b788
aviv86 <aviv@ayende.com>
2020-08-03 15:56:46 +0300

    RavenDB-13592 : initial work on gap filling results for empty ranges

--- src/Raven.Server/Documents/Queries/Results/TimeSeriesRetriever.cs
+++ src/Raven.Server/Documents/Queries/Results/TimeSeriesRetriever.cs
@@ -42,6 +42,8 @@ namespace Raven.Server.Documents.Queries.Results
 
         private double? _scale;
 
+        private bool _fillGaps;
+
         private static TimeSeriesAggregation[] AllAggregationTypes() =>  new[]
         {
             new TimeSeriesAggregation(AggregationType.First),
@@ -102,6 +104,8 @@ namespace Raven.Server.Documents.Queries.Results
             if (timeSeriesFunction.GroupBy == null && timeSeriesFunction.Select == null)
                 return GetRawValues();
 
+            _fillGaps = true; // todo aviv
+
             TimeSeriesAggregation[] aggStates;
             if (timeSeriesFunction.Select != null)
             {
@@ -141,12 +145,17 @@ namespace Raven.Server.Documents.Queries.Results
                     array.Add(AddTimeSeriesResult(timeSeriesFunction, aggStates, rangeSpec.Start, rangeSpec.End));
                 }
 
-                rangeSpec.MoveToNextRange(ts);
-
                 for (int i = 0; i < aggStates.Length; i++)
                 {
                     aggStates[i].Init();
                 }
+
+                if (_fillGaps)
+                {
+                    FillMissingGaps(rangeSpec, ts, array, timeSeriesFunction, aggStates);
+                }
+
+                rangeSpec.MoveToNextRange(ts);
             }
 
             BlittableJsonReaderObject GetRawValues()
@@ -759,6 +768,23 @@ namespace Raven.Server.Documents.Queries.Results
             }
         }
 
+        private static void FillMissingGaps(RangeGroup rangeSpec, DateTime ts, DynamicJsonArray array, TimeSeriesFunction timeSeriesFunction, TimeSeriesAggregation[] aggStates)
+        {
+            var start = rangeSpec.Start;
+            var end = rangeSpec.End;
+            var groupTimeSpan = end - start;
+            var diff = ts - end;
+            if (diff <= groupTimeSpan) 
+                return;
+
+            while (diff > groupTimeSpan)
+            {
+                start = end;
+                end = end.Add(groupTimeSpan);
+                array.Add(AddTimeSeriesResult(timeSeriesFunction, aggStates, start, end));
+                diff = ts - end;
+            }
+        }
         private static object GetValueFromRolledUpEntry(int index, SingleResult singleResult)
         {
             // we are working with a rolled-up series
