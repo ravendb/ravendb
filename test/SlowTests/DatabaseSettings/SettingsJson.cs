a04075b9b7ff74e0303df77dc69e415f2268578f
Danielle9897 <danielle.9897@gmail.com>
2020-08-10 19:00:22 +0300

    RavenDB-15485 Add test for nested json - Fix review comments

--- test/SlowTests/DatabaseSettings/SettingsJson.cs
+++ test/SlowTests/DatabaseSettings/SettingsJson.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using FastTests;
 using Raven.Server.Config;
 using Raven.Server.Config.Settings;
@@ -22,37 +23,35 @@ namespace SlowTests.DataBaseSettings
             
             try
             {
-                var serverConfig1 = RavenConfiguration.CreateForServer(null);
-                settingsJsonFile = $"{Path.GetDirectoryName(serverConfig1.ConfigPath)}\\settings.json";
-                
-                const string jsonWithNestedContent = "{" +
-                                                     "\"ETL.MaxBatchSizeInMb\":77," +
-                                                     "\"ETL\":" +
-                                                     "{" +
-                                                         "\"MaxNumberOfExtractedItems\":222," +
-                                                         "\"MaxNumberOfExtractedDocuments\":333" +
-                                                     "}," +
-                                                     "\"ETL\":" +
-                                                     "{" +
-                                                         "\"SQL\":" +
-                                                         "{" +
-                                                             "\"CommandTimeoutInSec\":444" +
-                                                         "}" +
-                                                     "}" +
-                                                     "}";
+                const string jsonWithNestedContent = @"{
+                                                         ""ETL.MaxBatchSizeInMb"":77,
+                                                         ""ETL"":
+                                                         {
+                                                            ""MaxNumberOfExtractedItems"":222,
+                                                            ""MaxNumberOfExtractedDocuments"":333
+                                                         },
+                                                         ""ETL"":
+                                                         { 
+                                                            ""SQL"":
+                                                            {
+                                                               ""CommandTimeoutInSec"":444
+                                                            }
+                                                         }
+                                                      }";
 
+                settingsJsonFile = $"{Path.GetDirectoryName(GetTempFileName())}\\settings.json";
                 File.WriteAllText(settingsJsonFile, jsonWithNestedContent);
-
-                var serverConfig2 = RavenConfiguration.CreateForServer(null);
-                serverConfig2.Initialize();
                 
-                var databaseConfig = RavenConfiguration.CreateForDatabase(serverConfig2, "dbName");
+                var serverConfig = RavenConfiguration.CreateForServer(null, settingsJsonFile);
+                serverConfig.Initialize();
+
+                var databaseConfig = RavenConfiguration.CreateForDatabase(serverConfig, "dbName");
                 databaseConfig.Initialize();
 
-                Assert.Equal("77 MBytes", serverConfig2.Etl.MaxBatchSize.ToString());
-                Assert.Equal(222, serverConfig2.Etl.MaxNumberOfExtractedItems);
-                Assert.Equal(333, serverConfig2.Etl.MaxNumberOfExtractedDocuments);
-                Assert.Equal(444, serverConfig2.Etl.SqlCommandTimeout.Value.GetValue(TimeUnit.Seconds));
+                Assert.Equal("77 MBytes", serverConfig.Etl.MaxBatchSize.ToString());
+                Assert.Equal(222, serverConfig.Etl.MaxNumberOfExtractedItems);
+                Assert.Equal(333, serverConfig.Etl.MaxNumberOfExtractedDocuments);
+                Assert.Equal(444, serverConfig.Etl.SqlCommandTimeout.Value.GetValue(TimeUnit.Seconds));
                
                 Assert.Equal("77 MBytes", databaseConfig.Etl.MaxBatchSize.ToString());
                 Assert.Equal(222, databaseConfig.Etl.MaxNumberOfExtractedItems);
