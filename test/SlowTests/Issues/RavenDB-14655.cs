857dc97733a24473ce009b779b94a08575b27e8f
efrat <efrat@hibernatingrhinos.com>
2020-08-27 15:31:39 +0300

    RavenDB-14655 Handling of tombstones in Pull Replication Hub

---
+++ test/SlowTests/Issues/RavenDB-14655.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.Documents.Operations.Replication;
+using Raven.Server.ServerWide.Context;
+using SlowTests.Core.Utils.Entities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_14655 : RavenTestBase
+    {
+        public RavenDB_14655(ITestOutputHelper output) : base(output)
+        {
+        }
+        [Fact]
+        public async Task TombstonesCleanUpWithHubDefinition()
+        {
+            using (var store = GetDocumentStore())
+            {
+                await store.Maintenance.ForDatabase(store.Database).SendAsync(new PutPullReplicationAsHubOperation("hub"));
+                var database = await GetDocumentDatabaseInstanceFor(store);
+                database.Time.UtcDateTime = () => DateTime.UtcNow.AddDays(-12);
+                using (var session = store.OpenSession())
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        session.Store(new User(), "user/" + i);
+                    }
+                    session.SaveChanges();
+                }
+                using (database.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+                {
+                    using (context.OpenReadTransaction())
+                    {
+                        var count = database.DocumentsStorage.GetTombstonesFrom(context, "Users", 0, 0, 128).Count();
+                        Assert.Equal(0, count);
+                    }
+
+                    using (var session = store.OpenSession())
+                    {
+                        session.Delete("user/3");
+                        session.SaveChanges();
+                    }
+                    
+                    using (context.OpenReadTransaction())
+                    {
+                        var count = database.DocumentsStorage.GetTombstonesFrom(context, "Users", 0, 0, 128).Count();
+                        Assert.Equal(1, count);
+                    }
+
+                    await database.TombstoneCleaner.ExecuteCleanup();
+
+                    using (context.OpenReadTransaction())
+                    {
+                        var count = database.DocumentsStorage.GetTombstonesFrom(context, "Users", 0, 0, 128).Count();
+                        Assert.Equal(1, count);
+                    }
+
+                    database.Time.UtcDateTime = () => DateTime.UtcNow;
+                    using (var session = store.OpenSession())
+                    {
+                        for (int i = 10; i < 20; i++)
+                        {
+                            session.Store(new User(), "user/" + i);
+                        }
+                        session.SaveChanges();
+                    }
+
+                    using (var session = store.OpenSession())
+                    {
+                        session.Delete("user/2");
+                        session.Delete("user/12");
+                        session.SaveChanges();
+                    }
+
+                    using (context.OpenReadTransaction())
+                    {
+                        var count = database.DocumentsStorage.GetTombstonesFrom(context, "Users", 0, 0, 128).Count();
+                        Assert.Equal(3, count);
+                    }
+
+                    database.Time.UtcDateTime = () => DateTime.UtcNow.AddDays(4);
+
+                    await database.TombstoneCleaner.ExecuteCleanup();
+                    
+                    using (context.OpenReadTransaction())
+                    {
+                        var count = database.DocumentsStorage.GetTombstonesFrom(context, "Users", 0, 0, 128).Count();
+                        Assert.Equal(2, count);
+                    }
+
+                }
+            }
+        }
+
+        [Fact]
+        public async Task TombstonesCleanUpWithHubDefinition2()
+        {
+            using (var store = GetDocumentStore())
+            {
+                await store.Maintenance.ForDatabase(store.Database).SendAsync(new PutPullReplicationAsHubOperation("hub"));
+                var database = await GetDocumentDatabaseInstanceFor(store);
+                database.Time.UtcDateTime = () => DateTime.UtcNow.AddDays(-17);
+
+                await database.TombstoneCleaner.ExecuteCleanup();
+
+            }
+        }
+
+        [Fact]
+        public async Task TombstonesCleanUpWithHubDefinition3()
+        {
+            using (var store = GetDocumentStore())
+            {
+                await store.Maintenance.ForDatabase(store.Database).SendAsync(new PutPullReplicationAsHubOperation("hub"));
+                var database = await GetDocumentDatabaseInstanceFor(store);
+                database.Time.UtcDateTime = () => DateTime.UtcNow.AddDays(-15);
+                using (var session = store.OpenSession())
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        session.Store(new User(), "user/" + i);
+                    }
+                    session.SaveChanges();
+                }
+                using (database.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+                {
+                    using (var session = store.OpenSession())
+                    {
+                        session.Delete("user/9");
+                        session.SaveChanges();
+                    }
+
+                    using (context.OpenReadTransaction())
+                    {
+                        var count = database.DocumentsStorage.GetTombstonesFrom(context, "Users", 0, 0, 128).Count();
+                        Assert.Equal(1, count);
+                    }
+
+                    database.Time.UtcDateTime = () => DateTime.UtcNow;
+                    await database.TombstoneCleaner.ExecuteCleanup();
+
+                    using (context.OpenReadTransaction())
+                    {
+                        var count = database.DocumentsStorage.GetTombstonesFrom(context, "Users", 0, 0, 128).Count();
+                        Assert.Equal(0, count);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task TombstonesCleanUpWithHubDefinition4()
+        {
+            using (var store = GetDocumentStore())
+            {
+                await store.Maintenance.ForDatabase(store.Database).SendAsync(new PutPullReplicationAsHubOperation("hub"));
+                var database = await GetDocumentDatabaseInstanceFor(store);
+                database.Time.UtcDateTime = () => DateTime.UtcNow.AddDays(-12);
+                using (var session = store.OpenSession())
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        session.Store(new User(), "user/" + i);
+                    }
+                    session.SaveChanges();
+                }
+                using (database.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+                {
+                    using (context.OpenReadTransaction())
+                    {
+                        await database.TombstoneCleaner.ExecuteCleanup();
+                        var count = database.DocumentsStorage.GetTombstonesFrom(context, "Users", 0, 0, 128).Count();
+                        Assert.Equal(0, count);
+                    }
+                }
+            }
+        }
+
+    }
+}
