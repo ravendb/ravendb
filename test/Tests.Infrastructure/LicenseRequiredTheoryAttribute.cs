e7d1f65d63707e690e183a702efde96a2ef95703
karmeli87 <karmeli87@gmail.com>
2020-09-01 09:52:42 +0300

    RavenDB-14634 add LicenseRequiredTheory

---
+++ test/Tests.Infrastructure/LicenseRequiredTheoryAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace Tests.Infrastructure
+{
+    public class LicenseRequiredTheoryAttribute : TheoryAttribute
+    {
+        private static readonly bool ShouldSkip;
+
+        internal static string SkipMessage = "Requires License to be set via 'RAVEN_LICENSE' environment variable.";
+
+        static LicenseRequiredTheoryAttribute()
+        {
+            ShouldSkip = string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("RAVEN_LICENSE"));
+        }
+
+        public override string Skip
+        {
+            get
+            {
+                if (ShouldSkip)
+                    return SkipMessage;
+
+                return null;
+            }
+        }
+    }
+}
