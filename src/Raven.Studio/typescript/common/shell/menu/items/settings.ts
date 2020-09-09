7ec29812977ae86db907474c64d046b68648b06c
Rafał Kwiatkowski <rafal@ravendb.net>
2020-09-09 12:11:22 +0200

    RavenDB-11140 Backup now button without creating a backup task

--- src/Raven.Studio/typescript/common/shell/menu/items/settings.ts
+++ src/Raven.Studio/typescript/common/shell/menu/items/settings.ts
@@ -131,14 +131,6 @@ function getSettingsMenuItem(appUrls: computedAppUrls) {
             css: 'icon-manage-dbgroup',
             dynamicHash: appUrls.manageDatabaseGroup
         }),
-        new leafMenuItem({
-            route: 'databases/backups',
-            moduleId: 'viewmodels/resources/backups',
-            title: 'Backups',
-            nav: true,
-            css: 'icon-backups',
-            dynamicHash: appUrls.backups
-        }),
         new separatorMenuItem(),
         new separatorMenuItem('Tasks'),
         new leafMenuItem({
