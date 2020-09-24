113e510792c2bc941971a353c947cf8be2deb714
Igal Merhavia <igal@ravendb.net>
2020-09-24 15:47:25 +0300

    RavenDB-14245 Informative exception when wrong using projection

--- test/SlowTests/MailingList/ProjectionTests.cs
+++ test/SlowTests/MailingList/ProjectionTests.cs
@@ -4,10 +4,14 @@
 //  </copyright>
 // -----------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using FastTests;
+using Raven.Client.Documents;
 using Raven.Client.Documents.Session;
+using Raven.Tests.Core.Utils.Entities;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -126,6 +130,54 @@ namespace SlowTests.MailingList
             }
         }
 
+        [Fact]
+        public async Task Projection_WhenUseMethodNotDefineForProjection_ShouldThrowInformativeException()
+        {
+            using var store = GetDocumentStore();
+            using var session = store.OpenAsyncSession();
+            
+            _ = await session.Query<User>()
+                .Select(x => new {x.Name})
+                .ToListAsync();
+                    
+            await Assert.ThrowsAnyAsync<NotSupportedException>(async () =>
+            {
+                _ = await session.Query<User>()
+                    .Select(x => new InvalidProjectionUser(x.Name))
+                    .ToListAsync();
+            });
+                    
+            await Assert.ThrowsAnyAsync<NotSupportedException>(async () =>
+            {
+                _ = await session.Query<User>()
+                    .Select(x => new InvalidProjectionUser(x.Name){Prop2 = "dddd"})
+                    .ToListAsync();
+            });
+                    
+            await Assert.ThrowsAnyAsync<NotSupportedException>(async () =>
+            {
+                _ = await session.Query<User>()
+                    .Select(x => InvalidProjectionUser.Create(x.Name))
+                    .ToListAsync();
+            });
+        }
+        
+        private class InvalidProjectionUser
+        {
+            public string Prop1 { get; set; }
+            public string Prop2 { get; set; }
+
+            public InvalidProjectionUser() { }
+            public InvalidProjectionUser(string prop1)
+            {
+                Prop1 = prop1 + "addition";
+            }
+            public static InvalidProjectionUser Create(string prop1)
+            {
+                return new InvalidProjectionUser{Prop1 = prop1};
+            }
+        }
+        
         private class Foo
         {
             public string Id { set; get; }
