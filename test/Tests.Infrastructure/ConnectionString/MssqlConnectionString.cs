65718ba07142e86f2f6b46ff7b3a653e3d12bdcf
Pawel Pekrol <ppekrol@ravendb.net>
2020-09-25 12:16:25 +0200

    RavenDB-15642 better error message

--- test/Tests.Infrastructure/ConnectionString/MssqlConnectionString.cs
+++ test/Tests.Infrastructure/ConnectionString/MssqlConnectionString.cs
@@ -3,37 +3,43 @@ using System.Data.SqlClient;
 
 namespace Tests.Infrastructure.ConnectionString
 {
-    public class MssqlConnectionString: SqlConnectionString<SqlConnection>
+    public class MssqlConnectionString : SqlConnectionString<SqlConnection>
     {
         private const string EnvironmentVariable = "RAVEN_MSSQL_CONNECTION_STRING";
-        
+
         public static readonly MssqlConnectionString Instance = new MssqlConnectionString();
 
-        private MssqlConnectionString(): base(EnvironmentVariable)
+        private MssqlConnectionString() : base(EnvironmentVariable)
         {
         }
 
         protected override string VerifiedConnectionStringFactor()
         {
             const string localConnectionString = @"Data Source=localhost\sqlexpress;Integrated Security=SSPI;Connection Timeout=3";
-            if (TryConnect(localConnectionString))
+            if (TryConnect(localConnectionString, out var errorMessage))
                 return localConnectionString;
 
             var remoteConnectionString = Environment.GetEnvironmentVariable(EnvironmentVariable);
-            if (TryConnect(remoteConnectionString))
+            if (TryConnect(remoteConnectionString, out errorMessage))
                 return remoteConnectionString;
 
             throw new InvalidOperationException($"Use a valid connection string. " +
                                                 $"Connection string from environment variable {EnvironmentVariable} is \"{remoteConnectionString}\"" +
-                                                $"Local connection string is {localConnectionString}");
+                                                $"Local connection string is {localConnectionString}" + 
+                                                $"Error message is '{errorMessage}'");
 
-            bool TryConnect(string connectionString)
+            bool TryConnect(string connectionString, out string errorMessage)
             {
+                errorMessage = null;
+
                 if (string.IsNullOrWhiteSpace(connectionString))
+                {
+                    errorMessage = "Empty connection string.";
                     return false;
-                
+                }
+
                 connectionString = string.Join(";", connectionString, $"{TimeOutParameter}=3");
-                
+
                 try
                 {
                     using (var connection = new SqlConnection(connectionString))
@@ -43,8 +49,9 @@ namespace Tests.Infrastructure.ConnectionString
 
                     return true;
                 }
-                catch (Exception)
+                catch (Exception e)
                 {
+                    errorMessage = e.ToString();
                     return false;
                 }
             }
