0c803b61118fe7604409ef798ed1f5052df02b88
aviv86 <aviv@ayende.com>
2020-10-01 15:47:35 +0300

    RavenDB-15020 : PercentileAggregation : change implementation to use linear interpolation between closest ranks method

--- src/Raven.Server/Documents/Queries/Results/TimeSeries/PercentileAggregation.cs
+++ src/Raven.Server/Documents/Queries/Results/TimeSeries/PercentileAggregation.cs
@@ -79,28 +79,78 @@ namespace Raven.Server.Documents.Queries.Results.TimeSeries
 
         private IEnumerable<double> GetPercentile(double scale)
         {
+            // x = p(N+1) (except for edge cases)
+            // f = floor(x), c = ceil(x)
+            // v[K] = value of the 'Kth' item in sorted values
+
+            // result = v[f] + ((x % 1) * (v[c] - v[f]))
+
             for (var i = 0; i < _rankedValues.Count; i++)
             {
-                var sortedDict= _rankedValues[i];
+                var sortedDict = _rankedValues[i];
                 var itemsCount = _count[i];
-                var rank = (int)Math.Ceiling(_percentileFactor * itemsCount);
+                var n1 = itemsCount + 1;
+                double remainder = 0;
+                long rank;
 
-                var count = 0;
-                double? result = null;
+                if (_percentileFactor <= 1d / n1)
+                {
+                    // edge case
+                    rank = 1;
+                }
+                else if (_percentileFactor >= (double)itemsCount / n1)
+                {
+                    // edge case
+                    rank = itemsCount;
+                }
+                else
+                {
+                    // general case
+                    var x = _percentileFactor * n1;
+                    rank = (long)Math.Floor(x);
+                    remainder = x % 1;
+                }
+
+                // find v[rank], v[rank + 1] (if needed)
+                var count = 0; 
+                double? floorVal = null, ceilVal = null;
                 foreach ((double val, int valCount) in sortedDict)
                 {
+                    if (floorVal.HasValue)
+                    {
+                        // found v[rank +1]
+                        ceilVal = val;
+                        break;
+                    }
+
                     count += valCount;
-                    
-                    if (rank != count && count != itemsCount) 
+                    if (count < rank) 
                         continue;
-                    
-                    result = val;
+
+                    // found v[rank]
+                    floorVal = val;
+                    if (remainder == 0)
+                        break;
+
+                    if (count == rank)
+                        continue;
+                     
+                    // floor and ceil have the same value
+                    // no need for linear interpolation addition 
+                    remainder = 0;
                     break;
                 }
 
-                Debug.Assert(result.HasValue);
+                Debug.Assert(floorVal.HasValue);
+                var result = floorVal.Value;
+                
+                if (remainder != 0)
+                {
+                    Debug.Assert(ceilVal.HasValue);
+                    result += remainder * (ceilVal.Value - floorVal.Value);
+                }
 
-                yield return scale * result.Value;
+                yield return scale * result;
             }
         }
     }
