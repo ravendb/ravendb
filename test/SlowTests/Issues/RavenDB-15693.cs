cd4072a040e1bc712283bbdeb9c4e172ca48d4f8
Ayende Rahien <Ayende@ayende.com>
2020-10-11 15:06:31 +0300

    RavenDB-15693 Allow to boost on complex queries

---
+++ test/SlowTests/Issues/RavenDB-15693.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.Documents.Session;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_15693 : RavenTestBase
+    {
+        private class Doc
+        {
+            public string StrVal1, StrVal2, StrVal3; 
+        }
+        
+        [Fact]
+        public void CanQueryOnComplexBoost()
+        {
+            using var store = GetDocumentStore();
+            using var s = store.OpenSession();
+
+            var q = s.Advanced.DocumentQuery<Doc>()
+                .Search(x => x.StrVal1, "a")
+                .AndAlso()
+                .OpenSubclause()
+                .Search(x => x.StrVal2, "b")
+                .OrElse()
+                .Search(x => x.StrVal3, "search")
+                .CloseSubclause()
+                .Boost(0.2m);
+            var queryBoost = q.ToString();
+            
+            Assert.Equal("from 'Docs' where search(StrVal1, $p0) and boost(search(StrVal2, $p1) or search(StrVal3, $p2), $p3)", queryBoost);
+
+            q.ToList();
+            
+        }
+
+        public RavenDB_15693(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
