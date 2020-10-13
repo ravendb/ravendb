fd4afd135e1efcc0804f541555bab3b05d4a15fb
Arkadiusz Palinski <arek@ravendb.net>
2020-10-13 11:04:25 +0200

    RavenDB-15703 Fixing the export of subscriptions

---
+++ test/SlowTests/Client/Subscriptions/RavenDB_15703.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.Documents.Smuggler;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Client.Subscriptions
+{
+    public class RavenDB_15703 : RavenTestBase
+    {
+        public RavenDB_15703(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task MustNotFailOnExportingSubscriptions()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var operation = await store.Smuggler.ExportAsync(new DatabaseSmugglerExportOptions()
+                {
+                    OperateOnTypes = DatabaseItemType.Subscriptions
+                }, NewDataPath());
+
+                await operation.WaitForCompletionAsync(TimeSpan.FromMinutes(1));
+            }
+        }
+    }
+}
