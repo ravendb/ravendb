2cd7028ad319b12c0e16827135203ef05bdf0ab6
Grisha Kotler <grisha@ayende.com>
2020-10-20 14:03:23 +0300

    RavenDB-15726 - wait for the task to be created

--- src/Raven.Server/Web/System/OngoingTasksHandler.cs
+++ src/Raven.Server/Web/System/OngoingTasksHandler.cs
@@ -430,10 +430,14 @@ namespace Raven.Server.Web.System
         }
 
         [RavenAction("/databases/*/admin/backup/database", "POST", AuthorizationStatus.DatabaseAdmin, CorsMode = CorsMode.Cluster)]
-        public Task BackupDatabase()
+        public async Task BackupDatabase()
         {
             var taskId = GetLongQueryString("taskId");
             var isFullBackup = GetBoolValueQueryString("isFullBackup", required: false);
+
+            // task id == raft index
+            // we must wait here to ensure that the task was actually created on this node
+            await ServerStore.Cluster.WaitForIndexNotification(taskId);
 
             var nodeTag = Database.PeriodicBackupRunner.WhoseTaskIsIt(taskId);
             if (nodeTag == null)
@@ -454,11 +458,10 @@ namespace Raven.Server.Web.System
                     writer.WriteEndObject();
                 }
 
-                return Task.CompletedTask;
+                return;
             }
 
             RedirectToRelevantNode(nodeTag);
-            return Task.CompletedTask;
         }
 
         private void RedirectToRelevantNode(string nodeTag)
