056c0305abf6466f3fba8a70e848c25c07aafcad
Grisha Kotler <grisha@ayende.com>
2020-10-22 19:28:35 +0300

    RavenDB-15754 - handle multiple changes to the referenced document during indexing

--- test/SlowTests/Issues/RavenDB-15754.cs
+++ test/SlowTests/Issues/RavenDB-15754.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using FastTests;
 using Raven.Client.Documents;
+using Raven.Client.Documents.Changes;
 using Raven.Client.Documents.Indexes;
 using Xunit;
 using Xunit.Abstractions;
@@ -16,7 +18,7 @@ namespace SlowTests.Issues
         }
 
         [Fact]
-        public async Task CanIndexReferencedDocument()
+        public async Task CanIndexReferencedDocumentChange()
         {
             using (var store = GetDocumentStore())
             {
@@ -27,7 +29,7 @@ namespace SlowTests.Issues
                     Name = companyName1
                 };
 
-                var employeesCount = 1_000_000;
+                var employeesCount = 700_000;
                 using (var session = store.OpenAsyncSession())
                 {
                     await session.StoreAsync(company);
@@ -45,10 +47,20 @@ namespace SlowTests.Issues
                     }
                 }
 
+                var index = new Index();
                 new Index().Execute(store);
 
                 WaitForIndexing(store, timeout: TimeSpan.FromMinutes(3));
-                await AssertCount(companyName1);
+                await AssertCount(store, companyName1, employeesCount);
+
+                var batchCount = 0;
+                store.Changes().ForIndex(index.IndexName).Subscribe(x =>
+                {
+                    if (x.Type == IndexChangeTypes.BatchCompleted)
+                    {
+                        batchCount++;
+                    }
+                });
 
                 using (var session = store.OpenAsyncSession())
                 {
@@ -58,18 +70,149 @@ namespace SlowTests.Issues
                 }
 
                 WaitForIndexing(store, timeout: TimeSpan.FromMinutes(5));
-                await AssertCount(companyName2);
+                await AssertCount(store, companyName1, 0);
+                await AssertCount(store, companyName2, employeesCount);
 
-                async Task AssertCount(string companyName)
+                Assert.True(batchCount >= 2);
+            }
+        }
+
+        [Fact]
+        public async Task CanIndexReferencedDocumentMultipleChanges()
+        {
+            using (var store = GetDocumentStore())
+            {
+                const string companyName1 = "Hibernating Rhinos";
+                const string companyName2 = "HR";
+                var company = new Company
                 {
-                    using (var session = store.OpenAsyncSession())
-                    {
-                        var itemsCount = await session.Query<Index.Result, Index>()
-                            .Where(x => x.CompanyName == companyName).CountAsync();
+                    Name = companyName1
+                };
 
-                        Assert.Equal(employeesCount, itemsCount);
+                var employeesCount = 700_000;
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(company);
+                    await session.SaveChangesAsync();
+
+                    using (var bulk = store.BulkInsert())
+                    {
+                        for (var i = 0; i < employeesCount; i++)
+                        {
+                            await bulk.StoreAsync(new Employee
+                            {
+                                CompanyId = company.Id
+                            });
+                        }
                     }
                 }
+
+                var index = new Index();
+                new Index().Execute(store);
+
+                WaitForIndexing(store, timeout: TimeSpan.FromMinutes(3));
+                await AssertCount(store, companyName1, employeesCount);
+
+                var batchCount = 0;
+                var sm = new SemaphoreSlim(0);
+                store.Changes().ForIndex(index.IndexName).Subscribe(x =>
+                {
+                    if (x.Type == IndexChangeTypes.BatchCompleted)
+                    {
+                        sm.Release();
+                        batchCount++;
+                    }
+                });
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    company.Name = companyName2;
+                    await session.StoreAsync(company, company.Id);
+                    await session.SaveChangesAsync();
+                }
+
+                sm.Wait();
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    company.Name = companyName1;
+                    await session.StoreAsync(company, company.Id);
+                    await session.SaveChangesAsync();
+                }
+
+                WaitForIndexing(store, timeout: TimeSpan.FromMinutes(5));
+                await AssertCount(store, companyName1, employeesCount);
+                await AssertCount(store, companyName2, 0);
+
+                Assert.True(batchCount >= 2);
+            }
+        }
+
+        [Fact]
+        public async Task CanIndexReferencedDocumentDelete()
+        {
+            using (var store = GetDocumentStore())
+            {
+                const string companyName1 = "Hibernating Rhinos";
+                var company = new Company
+                {
+                    Name = companyName1
+                };
+
+                var employeesCount = 700_000;
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(company);
+                    await session.SaveChangesAsync();
+
+                    using (var bulk = store.BulkInsert())
+                    {
+                        for (var i = 0; i < employeesCount; i++)
+                        {
+                            await bulk.StoreAsync(new Employee
+                            {
+                                CompanyId = company.Id
+                            });
+                        }
+                    }
+                }
+
+                var index = new Index();
+                index.Execute(store);
+
+                WaitForIndexing(store, timeout: TimeSpan.FromMinutes(3));
+                await AssertCount(store, companyName1, employeesCount);
+
+                var batchCount = 0;
+                store.Changes().ForIndex(index.IndexName).Subscribe(x =>
+                {
+                    if (x.Type == IndexChangeTypes.BatchCompleted)
+                    {
+                        batchCount++;
+                    }
+                });
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    session.Delete(company.Id);
+                    await session.SaveChangesAsync();
+                }
+
+                WaitForIndexing(store, timeout: TimeSpan.FromMinutes(5));
+                await AssertCount(store, companyName1, 0);
+
+                Assert.True(batchCount >= 2);
+            }
+        }
+
+        private async Task AssertCount(DocumentStore store, string companyName, int expectedCount)
+        {
+            using (var session = store.OpenAsyncSession())
+            {
+                var itemsCount = await session.Query<Index.Result, Index>()
+                    .Where(x => x.CompanyName == companyName).CountAsync();
+
+                Assert.Equal(expectedCount, itemsCount);
             }
         }
 
