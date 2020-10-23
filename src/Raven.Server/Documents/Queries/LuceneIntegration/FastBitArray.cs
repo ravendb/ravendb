4c8523b474a04052852408f4949bad4749521ccd
Ayende Rahien <Ayende@ayende.com>
2020-10-23 13:03:20 +0300

    RavenDB-15765 Avoid recursion in in queries RavenDB-14695 Modify the manner in which we handle in queries. If we have a low amount of items, we'll use a lazy approach with a Disjunction operation If we have a large amount of items (> 128), we'll read them to a bitmap and scan them from there. Results show significant improvement in several key scenarios.

---
+++ src/Raven.Server/Documents/Queries/LuceneIntegration/FastBitArray.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.Intrinsics.X86;
+
+namespace Raven.Server.Documents.Queries.LuceneIntegration
+{
+    public class FastBitArray : IDisposable
+    {
+        private ulong[] _bits;
+        public FastBitArray(int countOfBits)
+        {
+            _bits = ArrayPool<ulong>.Shared.Rent(countOfBits / 64 + (countOfBits % 64 == 0 ? 0 : 1));
+        }
+
+        public void Set(int index)
+        {
+            _bits[index / 64] |= 1UL << index % 64;
+        }
+
+        public IEnumerable<int> Iterate(int from)
+        {
+            // https://lemire.me/blog/2018/02/21/iterating-over-set-bits-quickly/
+            int i = from / 64;
+            if (i >= _bits.Length)
+                yield break;
+
+            ulong bitmap = _bits[i];
+            bitmap &= ulong.MaxValue << (from % 64);
+            while (true)
+            {
+                while (bitmap != 0)
+                {
+                    ulong t = bitmap & (ulong)-(long)bitmap;
+                    int count = BitOperations.TrailingZeroCount(bitmap);
+                    int setBitPos = i * 64 + count;
+                    if (setBitPos >= from) 
+                        yield return setBitPos; 
+                    bitmap ^= t;
+                }
+                i++;
+                if (i >= _bits.Length)
+                    break;
+                bitmap = _bits[i];
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_bits == null)
+                return;
+            ArrayPool<ulong>.Shared.Return(_bits);
+            _bits = null;
+        }
+    }
+}
