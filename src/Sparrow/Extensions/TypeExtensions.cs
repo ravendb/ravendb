144f1ae63eaa068a2ad7aa673dae0340cb6e241e
Pawel Pekrol <ppekrol@ravendb.net>
2020-10-23 10:39:02 +0200

    RavenDB-15764 support for C# record serialization

--- src/Sparrow/Extensions/TypeExtensions.cs
+++ src/Sparrow/Extensions/TypeExtensions.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Sparrow.Extensions
 {
     public static class TypeExtensions
     {
+        internal const string RecordEqualityContractPropertyName = "EqualityContract";
+
         public static string GetTypeNameForSerialization(this Type t)
         {
             return RemoveAssemblyDetails(t.AssemblyQualifiedName);
+        }
+
+        internal static bool IsRecord(this Type type)
+        {
+            if (type == null)
+                return false;
+
+            var equalityContractProperty = type.GetProperty(RecordEqualityContractPropertyName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            if (equalityContractProperty == null)
+                return false;
+
+            var getMethod = equalityContractProperty.GetGetMethod(nonPublic: true);
+            if (getMethod == null)
+                return false;
+
+            return getMethod.GetCustomAttribute(typeof(CompilerGeneratedAttribute)) != null;
         }
 
         private static string RemoveAssemblyDetails(string fullyQualifiedTypeName)
@@ -27,11 +47,13 @@ namespace Sparrow.Extensions
                         skippingAssemblyDetails = false;
                         builder.Append(current);
                         break;
+
                     case ']':
                         writingAssemblyName = false;
                         skippingAssemblyDetails = false;
                         builder.Append(current);
                         break;
+
                     case ',':
                         if (!writingAssemblyName)
                         {
@@ -43,6 +65,7 @@ namespace Sparrow.Extensions
                             skippingAssemblyDetails = true;
                         }
                         break;
+
                     default:
                         if (!skippingAssemblyDetails)
                             builder.Append(current);
