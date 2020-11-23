1734e7e5e094c8c5bccf12f24305331d7c0901c2
Grisha Kotler <grisha@ayende.com>
2020-11-23 15:11:34 +0200

    RavenDB-15888 - can update client configuration after configuration changed

--- test/SlowTests/Client/ClientConfigurationTests.cs
+++ test/SlowTests/Client/ClientConfigurationTests.cs
@@ -3,6 +3,7 @@ using FastTests;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Operations.Configuration;
 using Raven.Client.Http;
+using Raven.Client.ServerWide.Operations;
 using Raven.Client.ServerWide.Operations.Configuration;
 using Tests.Infrastructure;
 using Xunit;
@@ -35,6 +36,39 @@ namespace SlowTests.Client
                 }
 
                 Assert.Equal(50, requestExecutor.Conventions.MaxNumberOfRequestsPerSession);
+            }
+        }
+
+        [Fact]
+        public void CanSetClientConfigurationAfterDatabaseCreation()
+        {
+            using (var store = GetDocumentStore(new Options
+            {
+                ModifyDatabaseRecord = r => r.Client = new ClientConfiguration
+                {
+                    MaxNumberOfRequestsPerSession = 50
+                }
+            }))
+            {
+                var requestExecutor = store.GetRequestExecutor();
+
+                using (var session = store.OpenSession())
+                {
+                    session.Load<dynamic>("users/1"); // forcing client configuration update
+                }
+
+                Assert.Equal(50, requestExecutor.Conventions.MaxNumberOfRequestsPerSession);
+
+                var databaseRecord = store.Maintenance.Server.Send(new GetDatabaseRecordOperation(store.Database));
+                databaseRecord.Client = null;
+                store.Maintenance.Server.Send(new UpdateDatabaseOperation(databaseRecord, databaseRecord.Etag));
+
+                using (var session = store.OpenSession())
+                {
+                    session.Load<dynamic>("users/1"); // forcing client configuration update
+                }
+
+                Assert.Equal(30, requestExecutor.Conventions.MaxNumberOfRequestsPerSession);
             }
         }
 
