f2df8c14713a9d25f2b73f53815ba28554bf5088
Arkadiusz Palinski <arek@ravendb.net>
2020-12-02 10:36:10 +0100

    RavenDB-15911 Cleanup Temp directory on storage environment startup

---
+++ test/SlowTests/Issues/RavenDB_15911.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using FastTests.Voron;
+using Voron;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_15911 : StorageTest
+    {
+        public RavenDB_15911(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void ShouldCleanupTempDirectoryOnStartup()
+        {
+            RequireFileBasedPager();
+
+            var tempPath = Env.Options.TempPath.FullPath;
+
+            StopDatabase(shouldDisposeOptions: true);
+
+            string filePath = Path.Combine(tempPath, "dummy.file");
+
+            using (File.Create(filePath))
+            {
+
+            }
+
+            Options = StorageEnvironmentOptions.ForPath(DataDir);
+
+            StartDatabase();
+
+            Assert.False(File.Exists(filePath));
+        }
+    }
+}
