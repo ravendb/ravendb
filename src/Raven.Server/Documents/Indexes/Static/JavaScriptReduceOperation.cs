7cea59ef78e47afed5fd6285fecdfc66e3c06b89
Ayende Rahien <Ayende@ayende.com>
2020-12-21 17:23:29 +0200

    RavenDB-16041 JS map/reduce should support complex reduce keys

--- src/Raven.Server/Documents/Indexes/Static/JavaScriptReduceOperation.cs
+++ src/Raven.Server/Documents/Indexes/Static/JavaScriptReduceOperation.cs
@@ -257,7 +257,14 @@ namespace Raven.Server.Documents.Indexes.Static
 
                         var value = groupByField.GetValue(null, prop.Value);
 
-                        key.Put(propertyName, JsValue.FromObject(Engine, value), throwOnError: false);
+                        JsValue jsValue = value switch 
+                        {
+                            BlittableJsonReaderObject bjro => new BlittableObjectInstance(Engine, null, bjro, null, null, null),
+                            Document doc => new BlittableObjectInstance(Engine, null, doc.Data, doc),
+                          _ =>  JsValue.FromObject(Engine, value)
+                        };
+                        
+                        key.Put(propertyName, jsValue, throwOnError: false);
                     }
                 }
 
