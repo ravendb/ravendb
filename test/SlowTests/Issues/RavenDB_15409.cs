9c7765f8c292864fa6ecf56509331a2e66386d4d
haludi <igal@ravendb.net>
2021-01-04 12:09:33 +0200

    RavenDB-15494 Change pipe to dummy we socket

--- test/SlowTests/Issues/RavenDB_15409.cs
+++ test/SlowTests/Issues/RavenDB_15409.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
 using System.Threading.Tasks;
 using FastTests;
 using Raven.Server.Rachis;
@@ -9,7 +11,6 @@ using Raven.Server.ServerWide.Commands;
 using Sparrow.Json;
 using Sparrow.Logging;
 using Tests.Infrastructure;
-using Voron.Util;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -17,14 +18,62 @@ namespace SlowTests.Issues
 {
     public class RavenDB_15409 : ClusterTestBase
     {
+        private class DummyWebSocket : WebSocket
+        {
+            private static WebSocketReceiveResult Result { get; } = new(1, WebSocketMessageType.Text, true);
+            private readonly TaskCompletionSource<WebSocketReceiveResult> _completionSource = new();
+            private readonly MemoryStream _stream = new();
+            private int _isClose;
+
+            public override WebSocketCloseStatus? CloseStatus { get; }
+            public override string CloseStatusDescription { get; }
+            public override WebSocketState State { get; }
+            public override string SubProtocol { get; }
+
+            public override async Task SendAsync(ArraySegment<byte> buffer, WebSocketMessageType messageType, bool endOfMessage, CancellationToken cancellationToken)
+            {
+                if (_isClose == 1)
+                    throw new Exception("Closed");
+
+                await _stream.WriteAsync(buffer.Array, 0, buffer.Count, cancellationToken);
+            }
+
+            public async Task<string> CloseAndGetLogsAsync()
+            {
+                Close();
+                _stream.Seek(0, SeekOrigin.Begin);
+                using StreamReader reader = new(_stream);
+                return await reader.ReadToEndAsync();
+            }
+
+            public override void Dispose()
+            {
+                Close();
+                _stream.Dispose();
+            }
+
+            public override Task<WebSocketReceiveResult> ReceiveAsync(ArraySegment<byte> buffer, CancellationToken cancellationToken) =>_completionSource.Task;
+
+            private void Close()
+            {
+                if(Interlocked.CompareExchange(ref _isClose, 1, 0) == 0)
+                    _completionSource.SetResult(Result);
+            }
+
+            public override void Abort() { }
+            public override Task CloseAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken) => Task.CompletedTask;
+            public override Task CloseOutputAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken) => Task.CompletedTask;
+        }
+
+
         [Fact]
         public async Task DoNotCallUpdateLicenseLimitsCommandOnEveryLeaderChange()
         {
-            await using var logStream = new MemoryStream();
-            using var dispose = new DisposableAction(() => LoggingSource.Instance.DetachPipeSink());
-            LoggingSource.Instance.AttachPipeSink(logStream);
+            using var socket = new DummyWebSocket();
 
             var (servers, leader) = await CreateRaftCluster(3);
+            var _ = LoggingSource.Instance.Register(socket, new LoggingSource.WebSocketContext(), CancellationToken.None);
+
             await WaitForRaftIndexToBeAppliedInCluster(9, TimeSpan.FromSeconds(15));
             var expected = new HashSet<long>();
             foreach (var server in servers)
@@ -45,14 +94,7 @@ namespace SlowTests.Issues
 
             using (var ctx = JsonOperationContext.ShortTermSingleUse())
             {
-                async Task<string> MassageFactory()
-                {
-                    LoggingSource.Instance.DetachPipeSink();
-                    logStream.Seek(0, SeekOrigin.Begin);
-                    using StreamReader reader = new(logStream);
-                    return await reader.ReadToEndAsync();
-                }
-                await RavenTestHelper.AssertAllAsync(MassageFactory, servers.Select(s => (Action)(() =>
+                await RavenTestHelper.AssertAllAsync(async () => await socket.CloseAndGetLogsAsync(), servers.Select(s => (Action)(() =>
                 {
                     var actual = GetRaftCommandByType(s, nameof(UpdateLicenseLimitsCommand)).Count();
                     Assert.True(expected.Single() == actual, 
