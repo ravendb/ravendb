ef0f173fbec1112197ca3bcebad2420cafae34c8
Danielle9897 <danielle.9897@gmail.com>
2021-01-11 14:58:46 +0200

    RavenDB-15994 Draw shapes from a spatial query on the map - Added Tests

--- test/SlowTests/Tests/Spatial/DynamicSpatialQueries.cs
+++ test/SlowTests/Tests/Spatial/DynamicSpatialQueries.cs
@@ -258,6 +258,164 @@ namespace SlowTests.Tests.Spatial
                 }
             }
         }
+        
+        [Fact]
+        public void VerifyPolygonInResultsTest()
+        {
+            var house1 = new GeoDoc(44.75, -93.35);
+            var house2 = new GeoDoc(44.751, -93.351);
+            var house3 = new GeoDoc(44.752, -93.352);
+            var house4 = new GeoDoc(45.75, -94.35);
+
+            using (var store = GetDocumentStore())
+            {
+                store.Initialize();
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(house1);
+                    session.Store(house2);
+                    session.Store(house3);
+                    session.Store(house4);
+                    session.SaveChanges();
+                }
+
+                using (var commands = store.Commands())
+                {
+                    var json = commands.RawGetJson<BlittableJsonReaderObject>("/queries?query=from GeoDocs " +
+                                                                              "where spatial.within(spatial.point(Location1.Latitude, Location1.Longitude), " +
+                                                                              "spatial.wkt('POLYGON ((-90.0 50.0, -95.0 50.0, -95.0 40.0, -90.0 40.0, -90.0 50.0))'))" +
+                                                                              "&addSpatialProperties=true");
+
+                    Assert.True(json.TryGet(nameof(QueryResult.TotalResults), out int results));
+                    Assert.Equal(4, results);
+                    
+                    Assert.True(json.TryGet(nameof(DocumentQueryResult.SpatialShapes), out BlittableJsonReaderArray spatialShapes));
+                    Assert.Equal(1, spatialShapes.Length);
+                    
+                    (spatialShapes[0] as BlittableJsonReaderObject).TryGet(nameof(SpatialShapeBase.ShapeType), out string shape);
+                    Assert.Equal(shape, "Polygon");
+                    
+                    Assert.True((spatialShapes[0] as BlittableJsonReaderObject).TryGet(nameof(Polygon.Vertices), out BlittableJsonReaderArray vertices));
+                    Assert.Equal(4, vertices.Length);
+                    
+                    (vertices[0] as BlittableJsonReaderObject).TryGet(nameof(LatLong.Latitude), out double latitude);
+                    Assert.Equal(50, latitude);
+                    (vertices[0] as BlittableJsonReaderObject).TryGet(nameof(LatLong.Longitude), out double longitude);
+                    Assert.Equal(-90, longitude);
+                }
+            }
+        }
+        
+        [Fact]
+        public void VerifyCirclesInResultsTest()
+        {
+            var house1 = new GeoDoc(44.75, -93.35);
+            var house2 = new GeoDoc(44.751, -93.351);
+            var house3 = new GeoDoc(44.752, -93.352);
+            var house4 = new GeoDoc(45.75, -94.35);
+
+            using (var store = GetDocumentStore())
+            {
+                store.Initialize();
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(house1);
+                    session.Store(house2);
+                    session.Store(house3);
+                    session.Store(house4);
+                    session.SaveChanges();
+                }
+
+                using (var commands = store.Commands())
+                {
+                    var json = commands.RawGetJson<BlittableJsonReaderObject>("/queries?query=from GeoDocs " +
+                                                                              "where spatial.within(spatial.point(Location1.Latitude, Location1.Longitude), " +
+                                                                              "spatial.circle(50, 44.75, -93.35, 'miles')) " +
+                                                                              "or spatial.within(spatial.point(Location2.Latitude, Location2.Longitude), " +
+                                                                              "spatial.wkt('CIRCLE(-90 40 d=20)'))" +
+                                                                              "&addSpatialProperties=true");
+
+                    Assert.True(json.TryGet(nameof(QueryResult.TotalResults), out int results));
+                    Assert.Equal(3, results);
+                    
+                    Assert.True(json.TryGet(nameof(DocumentQueryResult.SpatialShapes), out BlittableJsonReaderArray spatialShapes));
+                    Assert.Equal(2, spatialShapes.Length);
+
+                    var firstShape = spatialShapes[0] as BlittableJsonReaderObject;
+                    firstShape.TryGet(nameof(SpatialShapeBase.ShapeType), out string shape);
+                    Assert.Equal(shape, "Circle");
+                    
+                    Assert.True(firstShape.TryGet(nameof(Circle.Center), out BlittableJsonReaderObject center));
+                    center.TryGet(nameof(LatLong.Latitude), out double latitude);
+                    Assert.Equal(44.75, latitude);
+                    center.TryGet(nameof(LatLong.Longitude), out double longitude);
+                    Assert.Equal(-93.35, longitude);
+                    
+                    firstShape.TryGet(nameof(Circle.Radius), out double radius);
+                    Assert.Equal(50, radius);
+                    
+                    firstShape.TryGet(nameof(Circle.Units), out SpatialUnits units);
+                    Assert.Equal(SpatialUnits.Miles, units);
+                    
+                    var secondShape = spatialShapes[1] as BlittableJsonReaderObject;
+                    secondShape.TryGet(nameof(SpatialShapeBase.ShapeType), out shape);
+                    Assert.Equal(shape, "Circle");
+                    
+                    Assert.True(secondShape.TryGet(nameof(Circle.Center), out center));
+                    center.TryGet(nameof(LatLong.Latitude), out latitude);
+                    Assert.Equal(40, latitude);
+                    center.TryGet(nameof(LatLong.Longitude), out longitude);
+                    Assert.Equal(-90, longitude);
+                    
+                    secondShape.TryGet(nameof(Circle.Radius), out  radius);
+                    Assert.Equal(20, radius);
+                    
+                    secondShape.TryGet(nameof(Circle.Units), out units);
+                    Assert.Equal(SpatialUnits.Kilometers, units);
+                }
+            }
+        }
+        
+        [Fact]
+        public void VerifyWKTCircleHasDistanceTest()
+        {
+
+            using (var store = GetDocumentStore())
+            {
+                store.Initialize();
+
+                using (var commands = store.Commands())
+                {
+                    var e = Assert.Throws<Raven.Client.Exceptions.RavenException>(() => commands.RawGetJson<BlittableJsonReaderObject>("/queries?query=from GeoDocs " +
+                                                                                                 "where spatial.within(spatial.point(Location1.Latitude, Location1.Longitude), " +
+                                                                                                 "spatial.wkt('CIRCLE(-90 40)'))" +
+                                                                                                 "&addSpatialProperties=true"));
+                    
+                    Assert.Contains("WKT CIRCLE should contain 3 params. i.e. CIRCLE(longitude latitude d=radiusDistance)", e.Message);
+                }
+            }
+        }
+        
+        [Fact]
+        public void VerifyWKTCircleDistanceFormatTest()
+        {
+            using (var store = GetDocumentStore())
+            {
+                store.Initialize();
+
+                using (var commands = store.Commands())
+                {
+                    var e = Assert.Throws<Raven.Client.Exceptions.RavenException>(() => commands.RawGetJson<BlittableJsonReaderObject>("/queries?query=from GeoDocs " +
+                                                                                                                         "where spatial.within(spatial.point(Location1.Latitude, Location1.Longitude), " +
+                                                                                                                         "spatial.wkt('CIRCLE(-90 40 d2)'))" +
+                                                                                                                         "&addSpatialProperties=true"));
+                    
+                    Assert.Contains("Invalid radius distance param", e.Message);
+                }
+            }
+        }
 
         private class GeoPoint
         {
