8dae6bc05d8d98eedac2af1e1093b1696bad0045
Arkadiusz Palinski <arek@ravendb.net>
2021-01-21 15:50:17 +0100

    RavenDB-16023 Fixing the recovery process on encrypted storage which caused the data corruption. The problem was that we didn't fiter our of overflow pages that were freed during CryptoPager.TxOnCommit. They were seen as valid pages in CryptoTransactionState which uses a dictionary under the covers so no ordering is guaranteed. This is very similar to the issues we had with the validation of overflows if the encryption isn't enabled.

---
+++ src/Voron/Impl/Journal/RecoveryOverflowDetector.cs
@@ -0,0 +1,40 @@
+﻿namespace Voron.Impl.Journal
+{
+    /// <summary>
+    /// It's meant to be used only on list of pages that are sorted descending by page number.
+    /// We need to iterate from the end in order to filter out pages that was overwritten by later transaction.
+    /// </summary>
+    public class RecoveryOverflowDetector
+    {
+        private long _minPageChecked = -1;
+        private long _minPageOverlappedByAnotherPage = -1;
+
+        public bool IsOverlappingAnotherPage(long pageNumber, int numberOfPages)
+        {
+            var maxPageRange = pageNumber + numberOfPages - 1;
+
+            if (_minPageChecked != -1 && maxPageRange >= _minPageChecked)
+            {
+                // this page is not in use, there is a valid page having higher number which overlaps this one
+
+                _minPageOverlappedByAnotherPage = pageNumber;
+                return true;
+            }
+
+            if (_minPageOverlappedByAnotherPage != -1 && maxPageRange >= _minPageOverlappedByAnotherPage)
+            {
+                // this page is not in use, there is a page having higher number which overlaps this one and was modified in later transaction
+
+                _minPageOverlappedByAnotherPage = pageNumber;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void SetPageChecked(long pageNumber)
+        {
+            _minPageChecked = pageNumber;
+        }
+    }
+}
---
+++ test/SlowTests/Voron/Issues/RavenDB_16023.cs
@@ -0,0 +1,173 @@
+﻿using System.Linq;
+using FastTests.Voron;
+using Sparrow;
+using Sparrow.Server;
+using Voron;
+using Voron.Global;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Voron.Issues
+{
+    public class RavenDB_16023 : StorageTest
+    {
+        public RavenDB_16023(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        private readonly byte[] _masterKey = Sodium.GenerateRandomBuffer((int)Sodium.crypto_aead_xchacha20poly1305_ietf_keybytes());
+
+        protected override void Configure(StorageEnvironmentOptions options)
+        {
+            options.ManualFlushing = true;
+            options.Encryption.MasterKey = _masterKey.ToArray();
+        }
+
+        [Fact]
+        public unsafe void RecoveryOfEncryptedStorageNeedsToTakeIntoAccountFreedOverflowPages()
+        {
+            RequireFileBasedPager();
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.LowLevelTransaction.DataPager.EnsureContinuous(20, 10);
+
+                tx.LowLevelTransaction.AllocatePage(50);
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                var p = tx.LowLevelTransaction.AllocatePage(10, 21);
+
+                p.Flags |= PageFlags.Overflow;
+                p.OverflowSize = 9 * Constants.Storage.PageSize;
+
+                Memory.Set(p.DataPointer, 1, 9 * Constants.Storage.PageSize);
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.LowLevelTransaction.FreePage(21);
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                var p = tx.LowLevelTransaction.AllocatePage(10, 20);
+
+                p.Flags |= PageFlags.Overflow;
+                p.OverflowSize = 9 * Constants.Storage.PageSize;
+
+                Memory.Set(p.DataPointer, 1, 9 * Constants.Storage.PageSize);
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.LowLevelTransaction.FreePage(20);
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                var p = tx.LowLevelTransaction.AllocatePage(2, 21);
+
+                Memory.Set(p.DataPointer, 2, Constants.Storage.PageSize);
+
+                tx.Commit();
+            }
+
+            RestartDatabase();
+
+            using (var tx = Env.ReadTransaction())
+            {
+                Page page = tx.LowLevelTransaction.GetPage(21); // must not throw 'Unable to decrypt page'
+            }
+        }
+
+        [Fact]
+        public unsafe void RecoveryOfEncryptedStorageNeedsToTakeIntoAccountFreedPagesThatCouldOverlapAnotherFreedPages()
+        {
+            RequireFileBasedPager();
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.LowLevelTransaction.DataPager.EnsureContinuous(0, 100);
+                tx.LowLevelTransaction.AllocatePage(50);
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                var p = tx.LowLevelTransaction.AllocatePage(10, 24);
+
+                p.Flags |= PageFlags.Overflow;
+                p.OverflowSize = 9 * Constants.Storage.PageSize;
+
+                Memory.Set(p.DataPointer, 1, 9 * Constants.Storage.PageSize);
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.LowLevelTransaction.FreePage(24);
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                var p = tx.LowLevelTransaction.AllocatePage(8, 16);
+                p.Flags |= PageFlags.Overflow;
+                p.OverflowSize = 7 * Constants.Storage.PageSize;
+                Memory.Set(p.DataPointer, 1, 7 * Constants.Storage.PageSize);
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.LowLevelTransaction.FreePage(16);
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                var p = tx.LowLevelTransaction.AllocatePage(4, 21);
+                p.Flags |= PageFlags.Overflow;
+                p.OverflowSize = 3 * Constants.Storage.PageSize;
+                Memory.Set(p.DataPointer, 4, 1 * Constants.Storage.PageSize);
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.LowLevelTransaction.FreePage(21);
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                var p = tx.LowLevelTransaction.AllocatePage(8, 24);
+                p.Flags |= PageFlags.Overflow;
+                p.OverflowSize = 7 * Constants.Storage.PageSize;
+                Memory.Set(p.DataPointer, 2, 7 * Constants.Storage.PageSize);
+                tx.Commit();
+            }
+
+            RestartDatabase();
+
+            using (var tx = Env.WriteTransaction())
+            {
+                Page page = tx.LowLevelTransaction.GetPage(24); // must not throw 'Unable to decrypt page'
+            }
+        }
+    }
+}
