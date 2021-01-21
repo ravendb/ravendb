7c358b7099808b1d91de69aaf13fd2150b7f179d
Arkadiusz Palinski <arek@ravendb.net>
2021-01-21 15:57:47 +0100

    RavenDB-16023  When we free a page we also need to mark its actual data in encryption buffer (scratch file is wrapped by CryptoPager) as not valid for commit.

--- test/SlowTests/Voron/Issues/RavenDB_16023.cs
+++ test/SlowTests/Voron/Issues/RavenDB_16023.cs
@@ -3,7 +3,11 @@ using FastTests.Voron;
 using Sparrow;
 using Sparrow.Server;
 using Voron;
+using Voron.Data;
 using Voron.Global;
+using Voron.Impl;
+using Voron.Impl.Journal;
+using Voron.Impl.Paging;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -167,6 +171,70 @@ namespace SlowTests.Voron.Issues
             using (var tx = Env.WriteTransaction())
             {
                 Page page = tx.LowLevelTransaction.GetPage(24); // must not throw 'Unable to decrypt page'
+            }
+        }
+
+        [Fact]
+        public unsafe void FreeingPageShouldAlsoMarkItsRelatedEncryptionBufferAsNotValidForCommit()
+        {
+            RequireFileBasedPager();
+
+            using (var tx = Env.WriteTransaction())
+            {
+                tx.LowLevelTransaction.DataPager.EnsureContinuous(20, 10);
+
+                tx.LowLevelTransaction.AllocatePage(50);
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.WriteTransaction())
+            {
+                var p = tx.LowLevelTransaction.AllocatePage(9, 21);
+
+                p.Flags |= PageFlags.Overflow;
+                p.OverflowSize = 8 * Constants.Storage.PageSize;
+
+                Memory.Set(p.DataPointer, 1, 8 * Constants.Storage.PageSize);
+
+                tx.LowLevelTransaction.FreePage(21);
+
+                p = tx.LowLevelTransaction.AllocatePage(10, 20);
+
+                p.Flags |= PageFlags.Overflow;
+                p.OverflowSize = 9 * Constants.Storage.PageSize;
+
+                Memory.Set(p.DataPointer, 1, 9 * Constants.Storage.PageSize);
+
+                tx.LowLevelTransaction.FreePage(20);
+
+                p = tx.LowLevelTransaction.AllocatePage(2, 21);
+
+                p.Flags |= PageFlags.Overflow;
+                p.OverflowSize =  Constants.Storage.PageSize;
+
+                Memory.Set(p.DataPointer, 2, Constants.Storage.PageSize);
+
+                var cryptoPagerTransactionState = ((IPagerLevelTransactionState)tx.LowLevelTransaction).CryptoPagerTransactionState;
+
+                var scratchFile = Env.ScratchBufferPool.GetScratchBufferFile(0);
+
+                var state = cryptoPagerTransactionState[scratchFile.File.Pager];
+
+                Assert.True(state[66].SkipOnTxCommit); // page 66 is PositionInScratchBuffer of the age #21 that was freed at the beginning of this transaction
+                Assert.True(state[83].SkipOnTxCommit); // page 83 is PositionInScratchBuffer of the age #20 that was freed in this transaction
+
+                tx.Commit();
+            }
+
+            using (var tx = Env.ReadTransaction())
+            {
+                Page page = tx.LowLevelTransaction.GetPage(21);
+
+                for (int i = 0; i < Constants.Storage.PageSize; i++)
+                {
+                    Assert.Equal(2, page.DataPointer[i]);
+                }
             }
         }
     }
