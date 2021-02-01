5a0196d4f29d7ab6ee18d597065468de14cb314e
efrat@ayende.com <efrat@ayende.com>
2021-02-01 20:58:02 +0200

    RavenDB-15890 Tests

--- test/SlowTests/Issues/RavenDB-15890.cs
+++ test/SlowTests/Issues/RavenDB-15890.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using FastTests.Server.Replication;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Operations.Indexes;
 using Raven.Client.ServerWide;
 using Raven.Client.Util;
 using Raven.Server.Documents;
@@ -21,6 +22,35 @@ namespace SlowTests.Issues
         public RavenDB_15890(ITestOutputHelper output) : base(output)
         {
         }
+        private class FakeIndex : AbstractIndexCreationTask
+        {
+            public override IndexDefinition CreateIndexDefinition()
+            {
+                return new IndexDefinition()
+                {
+                    Maps = { "from d in docs select new { d.Id }" }
+                };
+            }
+        }
+
+        [Fact]
+        public void SetStateEndPointTest()
+        {
+            using (var store = GetDocumentStore())
+            {
+                new FakeIndex().Execute(store);
+
+                foreach (var expected in new[] { IndexState.Normal, IndexState.Disabled, IndexState.Idle, IndexState.Error })
+                {
+                    store.Maintenance.Send(new SetIndexesStateOperation("FakeIndex", expected));
+
+                    var db = GetDocumentDatabaseInstanceFor(store).Result;
+                    var indexInstance = db.IndexStore.GetIndex("FakeIndex");
+
+                    Assert.Equal(expected, indexInstance.Definition.State);
+                }
+            }
+        }
 
         [Fact]
         public async Task ChangeStaticIndexStateToDisable()
@@ -37,6 +67,7 @@ namespace SlowTests.Issues
             }.Initialize())
             {
                 await new SimpleIndex().ExecuteAsync(store);
+                WaitForUserToContinueTheTest(store);
                 foreach (var server in Servers)
                 {
                     await WaitForValueAsync(async () =>
@@ -313,11 +344,11 @@ namespace SlowTests.Issues
                     {
                         documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
                         return documentDatabase.IndexStore.GetIndex(indexName).State;
-                    }, IndexState.Normal);
+                    }, IndexState.Normal, 3000);
                     Assert.Equal(IndexState.Normal, documentDatabase.IndexStore.GetIndex(indexName).State);
                 }
 
-                var (index, _)  = await leader.ServerStore.Engine.PutAsync(new SetIndexStateCommand(indexName, IndexState.Disabled, database, Guid.NewGuid().ToString()));
+                var (index, _) = await leader.ServerStore.Engine.PutAsync(new SetIndexStateCommand(indexName, IndexState.Disabled, database, Guid.NewGuid().ToString()));
 
                 await WaitForRaftIndexToBeAppliedInCluster(index, TimeSpan.FromSeconds(15));
 
@@ -327,7 +358,7 @@ namespace SlowTests.Issues
                     {
                         documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
                         return documentDatabase.IndexStore.GetIndex(indexName).State;
-                    }, IndexState.Disabled);
+                    }, IndexState.Disabled, 3000);
                     Assert.Equal(IndexState.Disabled, documentDatabase.IndexStore.GetIndex(indexName).State);
                 }
 
@@ -341,7 +372,7 @@ namespace SlowTests.Issues
                     {
                         documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
                         return documentDatabase.IndexStore.GetIndex(indexName).State;
-                    }, IndexState.Normal);
+                    }, IndexState.Normal, 3000);
                     Assert.Equal(IndexState.Normal, documentDatabase.IndexStore.GetIndex(indexName).State);
                 }
 
@@ -355,7 +386,7 @@ namespace SlowTests.Issues
                     {
                         documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
                         return documentDatabase.IndexStore.GetIndex(indexName).State;
-                    }, IndexState.Error);
+                    }, IndexState.Error, 3000);
                     Assert.Equal(IndexState.Error, documentDatabase.IndexStore.GetIndex(indexName).State);
                 }
 
@@ -369,7 +400,7 @@ namespace SlowTests.Issues
                     {
                         documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
                         return documentDatabase.IndexStore.GetIndex(indexName).State;
-                    }, IndexState.Normal);
+                    }, IndexState.Normal, 3000);
                     Assert.Equal(IndexState.Normal, documentDatabase.IndexStore.GetIndex(indexName).State);
                 }
 
@@ -383,7 +414,7 @@ namespace SlowTests.Issues
                     {
                         documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
                         return documentDatabase.IndexStore.GetIndex(indexName).State;
-                    }, IndexState.Idle);
+                    }, IndexState.Idle, 3000);
                     Assert.Equal(IndexState.Idle, documentDatabase.IndexStore.GetIndex(indexName).State);
                 }
 
@@ -397,12 +428,140 @@ namespace SlowTests.Issues
                     {
                         documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
                         return documentDatabase.IndexStore.GetIndex(indexName).State;
-                    }, IndexState.Normal);
+                    }, IndexState.Normal, 3000);
                     Assert.Equal(IndexState.Normal, documentDatabase.IndexStore.GetIndex(indexName).State);
                 }
             }
         }
 
+        [Fact]
+        public async Task ClusterWideStateDisableOrErrorCannotChangedLocally()
+        {
+            var leader = await CreateRaftClusterAndGetLeader(3);
+            var database = GetDatabaseName();
+            await CreateDatabaseInClusterInner(new DatabaseRecord(database), 3, leader.WebUrl, null);
+            var indexName = "SimpleIndex";
+            DocumentDatabase documentDatabase = null;
+            using (var store = new DocumentStore
+            {
+                Database = database,
+                Urls = new[] { leader.WebUrl }
+            }.Initialize())
+            {
+                await new SimpleIndex().ExecuteAsync(store);
+                foreach (var server in Servers)
+                {
+                    await WaitForValueAsync(async () =>
+                    {
+                        documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
+                        return documentDatabase.IndexStore.GetIndex(indexName).State;
+                    }, IndexState.Normal);
+                    Assert.Equal(IndexState.Normal, documentDatabase.IndexStore.GetIndex(indexName).State);
+                }
+
+                documentDatabase = await Servers[0].ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
+                var index0 = documentDatabase.IndexStore.GetIndex(indexName);
+                index0.SetState(IndexState.Idle);
+
+                var count = 0;
+
+                foreach (var server in Servers)
+                {
+                    documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
+                    if (documentDatabase.IndexStore.GetIndex(indexName).State == IndexState.Idle)
+                        count++;
+                }
+
+                Assert.Equal(1, count);
+
+                await ActionWithLeader((l) => l.ServerStore.Engine.PutAsync(new SetIndexStateCommand(indexName, IndexState.Disabled, database, Guid.NewGuid().ToString())),
+                    Servers);
+
+                foreach (var server in Servers)
+                {
+                    await WaitForValueAsync(async () =>
+                    {
+                        documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
+                        return documentDatabase.IndexStore.GetIndex(indexName).State;
+                    }, IndexState.Disabled);
+                    Assert.Equal(IndexState.Disabled, documentDatabase.IndexStore.GetIndex(indexName).State);
+                }
+
+
+                index0 = documentDatabase.IndexStore.GetIndex(indexName);
+                index0.SetState(IndexState.Idle);
+
+                count = 0;
+
+                foreach (var server in Servers)
+                {
+                    await WaitForValueAsync(async () =>
+                    {
+                        documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
+                        return documentDatabase.IndexStore.GetIndex(indexName).State;
+                    }, IndexState.Idle, 3000);
+                    if (documentDatabase.IndexStore.GetIndex(indexName).State == IndexState.Idle)
+                        count++;
+                }
+
+                Assert.Equal(0, count);
+            }
+        }
+
+        [Fact]
+        public async Task LocalStateDisabledCanBeChangedClusterWide()
+        {
+            var leader = await CreateRaftClusterAndGetLeader(3);
+            var database = GetDatabaseName();
+            await CreateDatabaseInClusterInner(new DatabaseRecord(database), 3, leader.WebUrl, null);
+            var indexName = "SimpleIndex";
+            DocumentDatabase documentDatabase = null;
+            using (var store = new DocumentStore
+            {
+                Database = database,
+                Urls = new[] { leader.WebUrl }
+            }.Initialize())
+            {
+                await new SimpleIndex().ExecuteAsync(store);
+                foreach (var server in Servers)
+                {
+                    await WaitForValueAsync(async () =>
+                    {
+                        documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
+                        return documentDatabase.IndexStore.GetIndex(indexName).State;
+                    }, IndexState.Normal);
+                    Assert.Equal(IndexState.Normal, documentDatabase.IndexStore.GetIndex(indexName).State);
+                }
+
+                documentDatabase = await Servers[0].ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
+                var index0 = documentDatabase.IndexStore.GetIndex(indexName);
+                index0.SetState(IndexState.Disabled);
+
+                var count = 0;
+
+                foreach (var server in Servers)
+                {
+                    documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
+                    if (documentDatabase.IndexStore.GetIndex(indexName).State == IndexState.Disabled)
+                        count++;
+                }
+
+                Assert.Equal(1, count);
+
+                await ActionWithLeader((l) => l.ServerStore.Engine.PutAsync(new SetIndexStateCommand(indexName, IndexState.Normal, database, Guid.NewGuid().ToString())),
+                    Servers);
+
+                foreach (var server in Servers)
+                {
+                    await WaitForValueAsync(async () =>
+                    {
+                        documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
+                        return documentDatabase.IndexStore.GetIndex(indexName).State;
+                    }, IndexState.Normal);
+                    Assert.Equal(IndexState.Normal, documentDatabase.IndexStore.GetIndex(indexName).State);
+                }
+            }
+        }
 
         private class SimpleIndex : AbstractIndexCreationTask<User>
         {
