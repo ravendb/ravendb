252723084c5809ec9ae89d1bff0cd9c81c7d35ab
Pawel Pekrol <ppekrol@ravendb.net>
2021-03-02 11:45:18 +0100

    RavenDB-14939 started working on custom analyzers

---
+++ src/Raven.Client/Exceptions/Documents/Compilation/AnalyzerCompilationException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Raven.Client.Exceptions.Compilation;
+using Raven.Client.Extensions;
+
+namespace Raven.Client.Exceptions.Documents.Compilation
+{
+    public class AnalyzerCompilationException : CompilationException
+    {
+        public AnalyzerCompilationException()
+        {
+        }
+
+        public AnalyzerCompilationException(string message)
+            : base(message)
+        {
+        }
+
+        public AnalyzerCompilationException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        public override string ToString()
+        {
+            return this.ExceptionToString(null);
+        }
+    }
+}
