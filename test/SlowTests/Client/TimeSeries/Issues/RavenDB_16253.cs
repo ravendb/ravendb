db74787f8a8a13715bad1eb4a4c88e34524e5d76
aviv <aviv@ayende.com>
2021-03-03 15:53:47 +0200

    RavenDB-16253 : UpdateStats() : avoid computing first & last timestamps on new series with single entry

---
+++ test/SlowTests/Client/TimeSeries/Issues/RavenDB_16253.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FastTests;
+using Raven.Tests.Core.Utils.Entities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Client.TimeSeries.Issues
+{
+    public class RavenDB_16253 : RavenTestBase
+    {
+        public RavenDB_16253(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        private const string DocId = "users/ayende";
+
+        [Fact]
+        public void CanAppendMinValueTimestamp()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var timeSeries = "HeartRate";
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User(), DocId);
+                    var timeSeriesFor = session.TimeSeriesFor(DocId, timeSeries);
+                    timeSeriesFor.Append(DateTime.MinValue, 0, "watches/fitbit");
+                    session.SaveChanges();
+                }
+            }
+        }
+    }
+}
