d7d4fc8463e727d48658c8fbb9bdfd519b741c59
Grzegorz Lachowski <gregolsky@ravendb.net>
2021-03-09 14:56:43 +0100

    RavenDB-7070 update dev build number

--- src/Raven.Server/ServerWide/ServerVersion.cs
+++ src/Raven.Server/ServerWide/ServerVersion.cs
@@ -23,7 +23,7 @@ namespace Raven.Server.ServerWide
         public static string FullVersion => 
             _fullVersion ?? (_fullVersion = RavenVersionAttribute.Instance.FullVersion);
 
-        public const int DevBuildNumber = 51;
+        public const int DevBuildNumber = 52;
 
         public static bool IsNightlyOrDev(long buildVersion)
         {
