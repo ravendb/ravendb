702e7d531281e3c3a0563cf3aff55955d67965f0
Pawel Pekrol <ppekrol@ravendb.net>
2021-03-17 15:09:04 +0100

    RavenDB-15951 fixed oid endpoint

---
+++ src/Raven.Server/Monitoring/Snmp/SnmpIndexAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Raven.Server.Monitoring.Snmp
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class SnmpIndexAttribute : Attribute
+    {
+        public SnmpIndexAttribute(Type type)
+        {
+            if (type.IsEnum == false)
+                throw new InvalidOperationException($"Only enums are supported, but was '{type}'.");
+
+            Type = type;
+        }
+
+        public Type Type { get; }
+    }
+}
