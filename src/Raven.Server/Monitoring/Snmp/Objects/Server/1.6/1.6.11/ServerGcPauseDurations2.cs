85ab82f640b72e5bf50f7deb97162ea446b6a57f
Pawel Pekrol <ppekrol@ravendb.net>
2021-03-22 12:44:02 +0100

    RavenDB-15951 added pause durations to SNMP

---
+++ src/Raven.Server/Monitoring/Snmp/Objects/Server/1.6/1.6.11/ServerGcPauseDurations2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Raven.Server.Utils;
+
+namespace Raven.Server.Monitoring.Snmp.Objects.Server
+{
+    public class ServerGcPauseDurations2 : ServerGcPauseDurationsBase
+    {
+        public ServerGcPauseDurations2(MetricCacher metricCacher, GCKind gcKind)
+            : base(metricCacher, gcKind, SnmpOids.Server.GcPauseDurations2, 1)
+        {
+        }
+    }
+}
