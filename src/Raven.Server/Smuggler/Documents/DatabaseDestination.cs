b52fc0bf6c4aa88f02bb0a6b211f25f1c1e282fe
Arkadiusz Palinski <arek@ravendb.net>
2021-03-23 13:20:34 +0100

    RavenDB-16378 Fixing restore of tombstones from a backup:
    - making sure that tombstones of artificial documents aren't restored (they are going to be created in the result of processing tombstone of source documents)
    - making sure that flags of tombstones are preserved after the restore
    - applyting same logic of dealing with tombstone flags as we do with flags of documents

--- src/Raven.Server/Smuggler/Documents/DatabaseDestination.cs
+++ src/Raven.Server/Smuggler/Documents/DatabaseDestination.cs
@@ -912,7 +912,7 @@ namespace Raven.Server.Smuggler.Documents
                             switch (tombstone.Type)
                             {
                                 case Tombstone.TombstoneType.Document:
-                                    _database.DocumentsStorage.Delete(context, key, tombstone.LowerId, null, tombstone.LastModified.Ticks, tombstone.ChangeVector, new CollectionName(tombstone.Collection));
+                                    _database.DocumentsStorage.Delete(context, key, tombstone.LowerId, null, tombstone.LastModified.Ticks, tombstone.ChangeVector, new CollectionName(tombstone.Collection), documentFlags: tombstone.Flags);
                                     break;
                                 case Tombstone.TombstoneType.Attachment:
                                     var idEnd = key.Content.IndexOf(SpecialChars.RecordSeparator);
