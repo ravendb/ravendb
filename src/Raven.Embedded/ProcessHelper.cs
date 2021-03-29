8cc36ef796ccb35b7329cf7d9ccd7c025583095f
Pawel Pekrol <ppekrol@ravendb.net>
2021-03-29 16:25:23 +0200

    RavenDB-16333 added support for wildcards (x) in the ServerOptions.FrameworkVersion

---
+++ src/Raven.Embedded/ProcessHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Client.Extensions;
+
+namespace Raven.Embedded
+{
+    internal static class ProcessHelper
+    {
+        internal static async Task<string?> ReadOutput(StreamReader output, Stopwatch elapsed, ServerOptions options, Func<string, StringBuilder, Task<bool>>? onLine)
+        {
+            var sb = new StringBuilder();
+
+            Task<string>? readLineTask = null;
+            while (true)
+            {
+                readLineTask ??= output.ReadLineAsync();
+
+                var hasResult = await readLineTask.WaitWithTimeout(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+
+                if (elapsed != null && elapsed.Elapsed > options.MaxServerStartupTimeDuration)
+                    return null;
+
+                if (hasResult == false)
+                    continue;
+
+                var line = readLineTask.Result;
+
+                readLineTask = null;
+
+                var shouldStop = false;
+                if (line != null)
+                {
+                    sb.AppendLine(line);
+
+                    if (onLine != null)
+                        shouldStop = await onLine(line, sb).ConfigureAwait(false);
+                }
+
+                if (shouldStop)
+                    break;
+
+                if (line == null)
+                    break;
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ test/EmbeddedTests/RuntimeFrameworkVersionMatcherTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Raven.Embedded;
+using Xunit;
+
+namespace EmbeddedTests
+{
+    public class RuntimeFrameworkVersionMatcherTests : EmbeddedTestBase
+    {
+        [Fact]
+        public async Task MatchTest1()
+        {
+            var options = new ServerOptions();
+            Assert.Equal(ServerOptions.Default.FrameworkVersion, await RuntimeFrameworkVersionMatcher.MatchAsync(options));
+
+            options.FrameworkVersion = null;
+            Assert.Null(await RuntimeFrameworkVersionMatcher.MatchAsync(options));
+
+            options = new ServerOptions();
+
+            var frameworkVersion = new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion(options.FrameworkVersion)
+            {
+                Patch = null
+            };
+
+            options.FrameworkVersion = frameworkVersion.ToString();
+            var match = await RuntimeFrameworkVersionMatcher.MatchAsync(options);
+            Assert.NotNull(match);
+            var matchFrameworkVersion = new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion(match);
+            Assert.True(matchFrameworkVersion.Major.HasValue);
+            Assert.True(matchFrameworkVersion.Minor.HasValue);
+            Assert.True(matchFrameworkVersion.Patch.HasValue);
+
+            Assert.True(frameworkVersion.Match(matchFrameworkVersion));
+
+            options = new ServerOptions
+            {
+                DotNetPath = Path.GetTempFileName(),
+                FrameworkVersion = frameworkVersion.ToString()
+            };
+
+            var e = await Assert.ThrowsAsync<InvalidOperationException>(() => RuntimeFrameworkVersionMatcher.MatchAsync(options));
+            Assert.Contains("Unable to execute dotnet to retrieve list of installed runtimes", e.Message);
+        }
+
+        [Fact]
+        public void MatchTest2()
+        {
+            var runtimes = GetRuntimes();
+
+            var runtime = new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("3.1.1");
+            Assert.Equal("3.1.1", RuntimeFrameworkVersionMatcher.Match(runtime, runtimes));
+
+            runtime = new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("2.1.11");
+            Assert.Equal("2.1.11", RuntimeFrameworkVersionMatcher.Match(runtime, runtimes));
+
+            runtime = new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("3.1.x");
+            Assert.Equal("3.1.3", RuntimeFrameworkVersionMatcher.Match(runtime, runtimes));
+
+            runtime = new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("3.x");
+            Assert.Equal("3.2.3", RuntimeFrameworkVersionMatcher.Match(runtime, runtimes));
+
+            runtime = new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("3.x.x");
+            Assert.Equal("3.2.3", RuntimeFrameworkVersionMatcher.Match(runtime, runtimes));
+
+            runtime = new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("5.0.x");
+            Assert.Equal("5.0.4", RuntimeFrameworkVersionMatcher.Match(runtime, runtimes));
+
+            runtime = new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("x");
+            Assert.Equal("5.0.4", RuntimeFrameworkVersionMatcher.Match(runtime, runtimes));
+
+            runtime = new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("6.x");
+            var e = Assert.Throws<InvalidOperationException>(() => RuntimeFrameworkVersionMatcher.Match(runtime, runtimes));
+            Assert.Contains("Could not find a matching runtime for '6.x.x'", e.Message);
+        }
+
+        private static List<RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion> GetRuntimes()
+        {
+            return new()
+            {
+                new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("2.1.3"),
+                new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("2.1.4"),
+                new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("2.1.11"),
+                new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("2.2.0"),
+                new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("2.2.1"),
+                new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("3.1.0"),
+                new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("3.1.1"),
+                new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("3.1.2"),
+                new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("3.1.3"),
+                new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("3.2.3"),
+                new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("5.0.3"),
+                new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion("5.0.4")
+            };
+        }
+    }
+}
