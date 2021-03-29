6dbaedd3575cb9e36919c8c4f8ebe5a5d9609177
Pawel Pekrol <ppekrol@ravendb.net>
2021-03-29 16:25:23 +0200

    RavenDB-16333 added support for wildcards (x) in the ServerOptions.FrameworkVersion

--- test/EmbeddedTests/BasicTests.cs
+++ test/EmbeddedTests/BasicTests.cs
@@ -122,6 +122,33 @@ namespace EmbeddedTests
             }
         }
 
+        [Fact]
+        public async Task TestEmbedded_RuntimeFrameworkVersionMatcher()
+        {
+            var paths = CopyServer();
+
+            using (var embedded = new EmbeddedServer())
+            {
+                var options = new ServerOptions
+                {
+                    ServerDirectory = paths.ServerDirectory,
+                    DataDirectory = paths.DataDirectory,
+                };
+
+                var frameworkVersion = new RuntimeFrameworkVersionMatcher.RuntimeFrameworkVersion(options.FrameworkVersion)
+                {
+                    Patch = null
+                };
+
+                options.FrameworkVersion = frameworkVersion.ToString();
+
+                embedded.StartServer(options);
+
+                var pid1 = await embedded.GetServerProcessIdAsync();
+                Assert.True(pid1 > 0);
+            }
+        }
+
         private (string ServerDirectory, string DataDirectory) CopyServer()
         {
             var baseDirectory = NewDataPath();
