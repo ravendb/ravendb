f6ef713afaf526fe1258ab1170acd4ff4d54f8b9
Pawel Pekrol <ppekrol@ravendb.net>
2021-03-30 11:09:36 +0200

    RavenDB-16351 added DocumentConventions.WaitForIndexesAfterSaveChangesTimeout and DocumentConventions.WaitForReplicationAfterSaveChangesTimeout conventions

--- src/Raven.Client/Documents/Conventions/DocumentConventions.cs
+++ src/Raven.Client/Documents/Conventions/DocumentConventions.cs
@@ -182,6 +182,9 @@ namespace Raven.Client.Documents.Conventions
             _firstBroadcastAttemptTimeout = TimeSpan.FromSeconds(5);
             _secondBroadcastAttemptTimeout = TimeSpan.FromSeconds(30);
 
+            _waitForIndexesAfterSaveChangesTimeout = TimeSpan.FromSeconds(15);
+            _waitForReplicationAfterSaveChangesTimeout = TimeSpan.FromSeconds(15);
+
             _sendApplicationIdentifier = true;
             _maxContextSizeToKeep = PlatformDetails.Is32Bits == false
                 ? new Size(1, SizeUnit.Megabytes)
@@ -219,6 +222,8 @@ namespace Raven.Client.Documents.Conventions
         private TimeSpan? _requestTimeout;
         private TimeSpan _secondBroadcastAttemptTimeout;
         private TimeSpan _firstBroadcastAttemptTimeout;
+        private TimeSpan _waitForIndexesAfterSaveChangesTimeout;
+        private TimeSpan _waitForReplicationAfterSaveChangesTimeout;
 
         private ReadBalanceBehavior _readBalanceBehavior;
         private Func<Type, BlittableJsonReaderObject, object> _deserializeEntityFromBlittable;
@@ -316,6 +321,34 @@ namespace Raven.Client.Documents.Conventions
             {
                 AssertNotFrozen();
                 _firstBroadcastAttemptTimeout = value;
+            }
+        }
+
+        /// <summary>
+        /// Sets the default timeout for DocumentSession.Advanced.WaitForIndexesAfterSaveChanges method.
+        /// Default: 15 Seconds.
+        /// </summary>
+        public TimeSpan WaitForIndexesAfterSaveChangesTimeout
+        {
+            get => _waitForIndexesAfterSaveChangesTimeout;
+            set
+            {
+                AssertNotFrozen();
+                _waitForIndexesAfterSaveChangesTimeout = value;
+            }
+        }
+
+        /// <summary>
+        /// Sets the default timeout for DocumentSession.Advanced.WaitForReplicationAfterSaveChanges method.
+        /// Default: 15 Seconds.
+        /// </summary>
+        public TimeSpan WaitForReplicationAfterSaveChangesTimeout
+        {
+            get => _waitForReplicationAfterSaveChangesTimeout;
+            set
+            {
+                AssertNotFrozen();
+                _waitForReplicationAfterSaveChangesTimeout = value;
             }
         }
 
