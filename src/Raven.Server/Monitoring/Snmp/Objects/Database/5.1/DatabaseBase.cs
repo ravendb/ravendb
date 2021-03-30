f2ca64bb5298b4bfd63692fdd50fe5b246a4149f
Pawel Pekrol <ppekrol@ravendb.net>
2021-03-30 12:06:05 +0200

    RavenDB-15055 exposed number of disabled, encrypted and current node databases

---
+++ src/Raven.Server/Monitoring/Snmp/Objects/Database/5.1/DatabaseBase.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Lextm.SharpSnmpLib;
+using Raven.Client;
+using Raven.Server.ServerWide;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Monitoring.Snmp.Objects.Database
+{
+    public abstract class DatabaseBase<TData> : ScalarObjectBase<TData> where TData : ISnmpData
+    {
+        protected readonly ServerStore ServerStore;
+
+        protected DatabaseBase(ServerStore serverStore, string dots)
+            : base(dots)
+        {
+            ServerStore = serverStore;
+        }
+
+        protected IEnumerable<RawDatabaseRecord> GetDatabases(TransactionOperationContext context)
+        {
+            foreach (var item in ServerStore.Cluster.ItemsStartingWith(context, Constants.Documents.Prefix, 0, int.MaxValue))
+                yield return new RawDatabaseRecord(item.Value);
+        }
+    }
+}
