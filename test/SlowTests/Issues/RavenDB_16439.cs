0b0cca2a7521eb5cf137375088576905b1d243c3
Pawel Pekrol <ppekrol@ravendb.net>
2021-04-01 12:31:59 +0200

    RavenDB-16439 when we do not have anything to match too in search then return empty boolean query to avoid getting NRE along the way - previously it was returning phraseQuery without any terms which resulted in the same behavior

---
+++ test/SlowTests/Issues/RavenDB_16439.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using FastTests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_16439 : RavenTestBase
+    {
+        public RavenDB_16439(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task CanSuggest()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new DocumentationPage {Title = "Hamlet", TextContent = "aaa"});
+                    await session.StoreAsync(new DocumentationPage {Title = "Makbet", TextContent = "bbb"});
+
+                    await session.SaveChangesAsync();
+
+                    var query =
+                        session.Advanced.AsyncDocumentQuery<DocumentationPage>()
+                            .Search(x => x.Title, "\"").Boost(50)
+                            .OrElse()
+                            .Search(x => x.TextContent, "\"aaa").Boost(35);
+
+                    var results = await query.ToArrayAsync();
+
+                    Assert.Equal(1, results.Length);
+                    Assert.Equal("Hamlet", results[0].Title);
+                }
+            }
+        }
+
+        private class DocumentationPage
+        {
+            public string TextContent { get; set; }
+            public string Title { get; set; }
+            public string Key { get; set; }
+            public string Id { get; set; }
+        }
+    }
+}
