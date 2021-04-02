089e7f426bfa031332236e50f1cd41fe3010bfa3
Ayende Rahien <ayende@ayende.com>
2021-04-02 12:56:24 +0300

    RavenDB-16446 Avoid using index temp files

--- src/Raven.Server/Indexing/LuceneVoronDirectory.cs
+++ src/Raven.Server/Indexing/LuceneVoronDirectory.cs
@@ -13,6 +13,7 @@ namespace Raven.Server.Indexing
         private readonly StorageEnvironment _environment;
         private readonly string _name;
         private readonly IndexOutputFilesSummary _indexOutputFilesSummary;
+        private readonly TempFileCache _tmpFileCache;
 
         public string Name => _name;
 
@@ -36,6 +37,8 @@ namespace Raven.Server.Indexing
             tx.CreateTree(_name);
 
             _indexOutputFilesSummary = new IndexOutputFilesSummary();
+            
+            _tmpFileCache = new TempFileCache(environment.Options);
         }
 
         public override bool FileExists(string name, IState s)
@@ -191,7 +194,7 @@ namespace Raven.Server.Indexing
             if (state == null)
                 throw new ArgumentNullException(nameof(s));
 
-            return new VoronIndexOutput(_environment.Options, name, state.Transaction, _name, _indexOutputFilesSummary);
+            return new VoronIndexOutput(_tmpFileCache, name, state.Transaction, _name, _indexOutputFilesSummary);
         }
 
         public IDisposable SetTransaction(Transaction tx, out IState state)
@@ -211,6 +214,7 @@ namespace Raven.Server.Indexing
 
         protected override void Dispose(bool disposing)
         {
+            _tmpFileCache.Dispose();
         }
 
         public void ResetAllocations()
