0d648f5ef4bcf7769001599215705544c8150cb1
ml054 <marcin@ravendb.net>
2021-04-07 10:04:18 +0200

    RavenDB-14163 Cluster dashboard - abstract model class

--- src/Raven.Studio/typescript/models/resources/widgets/cpuUsage.ts
+++ src/Raven.Studio/typescript/models/resources/widgets/cpuUsage.ts
@@ -2,22 +2,16 @@
 
 import CpuUsagePayload = Raven.Server.Dashboard.Cluster.Notifications.CpuUsagePayload;
 
-class cpuUsage {
-    readonly tag: string;
-    
-    connectedAt = ko.observable<Date>();
-    currentItem = ko.observable<cachedDateValue<CpuUsagePayload>>();
+import historyAwareWidget = require("models/resources/widgets/historyAwareWidget");
+
+class cpuUsage extends historyAwareWidget<CpuUsagePayload> {
     
     coresInfo: KnockoutComputed<string>;
     processCpuUsageFormatted: KnockoutComputed<string>;
     machineCpuUsageFormatted: KnockoutComputed<string>;
-    
-    mouseOver = ko.observable<boolean>(false);
-    history: cachedDateValue<CpuUsagePayload>[] = [];
-    connectionHistory: [Date, Date][] = [];
 
     constructor(tag: string) {
-        this.tag = tag;
+        super(tag);
 
         this.coresInfo = ko.pureComputed(() => {
             const noData = this.noDataText();
@@ -45,106 +39,6 @@ class cpuUsage {
             const dto = this.currentItem().value;
             return dto.MachineCpuUsage + "%";
         });
-    }
-    
-    private noDataText(): string|null {
-        const currentItem = this.currentItem();
-        const mouseOver = this.mouseOver();
-        if (currentItem) {
-            return null;
-        } else {
-            return mouseOver ? "No data" : "Connecting...";
-        }
-    }
-
-    onData(data: Raven.Server.Dashboard.Cluster.Notifications.CpuUsagePayload) {
-        const date = moment.utc(data.Date).toDate();
-        const newItem = {
-            date,
-            value: data
-        };
-        this.history.push(newItem);
-        
-        if (!this.mouseOver() && this.connectedAt()) {
-            this.currentItem(newItem);
-        }
-        
-        this.maybeTrimHistory();
-    }
-
-    onConnectionStatusChanged(connected: boolean) {
-        if (connected) {
-            this.connectedAt(new Date());
-        } else {
-            const connectionDate = this.connectedAt();
-            this.connectedAt(null);
-            if (!this.mouseOver()) {
-                this.currentItem(null);
-            }
-            this.connectionHistory.push([connectionDate, new Date()]);
-        }
-    }
-    
-    // null means show latest one
-    showItemAtDate(date: Date|null) {
-        this.mouseOver(!!date);
-        if (date) {
-            if (!this.wasConnected(date)) {
-                this.currentItem(null);
-                return;
-            }
-            const time = date.getTime();
-            if (history.length) {
-                for (let i = this.history.length - 1; i >= 0; i--) {
-                    const item = this.history[i];
-                    if (item.date.getTime() < time) {
-                        // found index to use
-                        this.currentItem(item);
-                        return;
-                    } 
-                }
-            }
-
-            this.currentItem(null);
-        } else {
-            // use latest data 
-            if (history.length && this.connectedAt()) {
-                this.currentItem(this.history[this.history.length - 1]);
-            } else {
-                this.currentItem(null);
-            }
-        }
-    }
-    
-    // don't be so strict here - due to potential time skews between local machine and servers
-    private wasConnected(date: Date): boolean {
-        const time = date.getTime();
-        const currentConnection = this.connectedAt();
-        const maxSkew = 3000;
-        
-        if (currentConnection && currentConnection.getTime() - maxSkew < time) {
-            return true;
-        }
-
-        for (const [start, end] of this.connectionHistory) {
-            const startWithSkew = start.getTime() - maxSkew;
-            const endWithSkew = end.getTime() + maxSkew;
-            if (startWithSkew <= time && time <= endWithSkew) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    private maybeTrimHistory() {
-        if (this.history.length > 5000) {
-            this.history.slice(3000);
-        }
-        
-        if (this.connectionHistory.length > 1000) {
-            this.connectionHistory.slice(800);
-        }
     }
 }
 
---
+++ src/Raven.Studio/typescript/models/resources/widgets/historyAwareWidget.ts
@@ -0,0 +1,118 @@
+
+
+class historyAwareWidget<T extends Raven.Server.Dashboard.Cluster.AbstractClusterDashboardNotification> {
+    readonly tag: string;
+
+    connectedAt = ko.observable<Date>();
+    currentItem = ko.observable<cachedDateValue<T>>();
+
+    mouseOver = ko.observable<boolean>(false);
+    history: cachedDateValue<T>[] = [];
+    connectionHistory: [Date, Date][] = [];
+
+    constructor(tag: string) {
+        this.tag = tag;
+    }
+
+    protected noDataText(): string|null {
+        const currentItem = this.currentItem();
+        const mouseOver = this.mouseOver();
+        if (currentItem) {
+            return null;
+        } else {
+            return mouseOver ? "No data" : "Connecting...";
+        }
+    }
+
+    onConnectionStatusChanged(connected: boolean) {
+        if (connected) {
+            this.connectedAt(new Date());
+        } else {
+            const connectionDate = this.connectedAt();
+            this.connectedAt(null);
+            if (!this.mouseOver()) {
+                this.currentItem(null);
+            }
+            this.connectionHistory.push([connectionDate, new Date()]);
+        }
+    }
+
+    onData(data: T) {
+        const date = moment.utc(data.Date).toDate();
+        const newItem = {
+            date,
+            value: data
+        };
+        this.history.push(newItem);
+
+        if (!this.mouseOver() && this.connectedAt()) {
+            this.currentItem(newItem);
+        }
+
+        this.maybeTrimHistory();
+    }
+
+    // null means show latest one
+    showItemAtDate(date: Date|null) {
+        this.mouseOver(!!date);
+        if (date) {
+            if (!this.wasConnected(date)) {
+                this.currentItem(null);
+                return;
+            }
+            const time = date.getTime();
+            if (history.length) {
+                for (let i = this.history.length - 1; i >= 0; i--) {
+                    const item = this.history[i];
+                    if (item.date.getTime() < time) {
+                        // found index to use
+                        this.currentItem(item);
+                        return;
+                    }
+                }
+            }
+
+            this.currentItem(null);
+        } else {
+            // use latest data 
+            if (history.length && this.connectedAt()) {
+                this.currentItem(this.history[this.history.length - 1]);
+            } else {
+                this.currentItem(null);
+            }
+        }
+    }
+    
+    // don't be so strict here - due to potential time skews between local machine and servers
+    private wasConnected(date: Date): boolean {
+        const time = date.getTime();
+        const currentConnection = this.connectedAt();
+        const maxSkew = 3000;
+
+        if (currentConnection && currentConnection.getTime() - maxSkew < time) {
+            return true;
+        }
+
+        for (const [start, end] of this.connectionHistory) {
+            const startWithSkew = start.getTime() - maxSkew;
+            const endWithSkew = end.getTime() + maxSkew;
+            if (startWithSkew <= time && time <= endWithSkew) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private maybeTrimHistory() {
+        if (this.history.length > 5000) {
+            this.history.slice(3000);
+        }
+
+        if (this.connectionHistory.length > 1000) {
+            this.connectionHistory.slice(800);
+        }
+    }
+}
+
+export = historyAwareWidget;
