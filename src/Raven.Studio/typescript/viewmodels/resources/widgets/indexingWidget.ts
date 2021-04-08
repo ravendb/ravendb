30342060b670b72cfcbd14a2be93c648da8ddd24
ml054 <marcin@ravendb.net>
2021-04-08 09:06:37 +0200

    RavenDB-14163 Cluster dashboard - refactor

--- src/Raven.Studio/typescript/viewmodels/resources/widgets/indexingWidget.ts
+++ src/Raven.Studio/typescript/viewmodels/resources/widgets/indexingWidget.ts
@@ -1,16 +1,13 @@
-import websocketBasedWidget = require("viewmodels/resources/widgets/websocketBasedWidget");
 import lineChart = require("models/resources/clusterDashboard/lineChart");
 import clusterDashboard = require("viewmodels/resources/clusterDashboard");
-import clusterDashboardWebSocketClient = require("common/clusterDashboardWebSocketClient");
 import indexingSpeed = require("models/resources/widgets/indexingSpeed");
+import abstractChartsWebsocketWidget = require("viewmodels/resources/widgets/abstractChartsWebsocketWidget");
 
-class indexingWidget extends websocketBasedWidget<Raven.Server.Dashboard.Cluster.Notifications.IndexingSpeedPayload> {
+class indexingWidget extends abstractChartsWebsocketWidget<Raven.Server.Dashboard.Cluster.Notifications.IndexingSpeedPayload, indexingSpeed> {
 
     indexedPerSecondChart: lineChart;
     mappedPerSecondChart: lineChart;
     reducedPerSecondChart: lineChart;
-    
-    nodeStats = ko.observableArray<indexingSpeed>([]);
     
     constructor(controller: clusterDashboard) {
         super(controller);
@@ -24,89 +21,39 @@ class indexingWidget extends websocketBasedWidget<Raven.Server.Dashboard.Cluster
     getType(): Raven.Server.Dashboard.Cluster.ClusterDashboardNotificationType {
         return "Indexing";
     }
-    
-    compositionComplete() {
-        super.compositionComplete();
-        this.enableSyncUpdates();
 
-        for (let ws of this.controller.getConnectedLiveClients()) {
-            this.onClientConnected(ws);
-        }
-        
-        this.initCharts();
-    }
-
-    private initCharts() {
+    initCharts() {
         const indexedPerSecondContainer = this.container.querySelector(".indexed-per-second-chart");
         this.indexedPerSecondChart = new lineChart(indexedPerSecondContainer, {
-            grid: true
+            grid: true,
+            tooltipProvider: date => indexingWidget.tooltipContent(date),
+            onMouseMove: date => this.onMouseMove(date)
         });
         const mappedPerSecondContainer = this.container.querySelector(".mapped-per-second-chart");
         this.mappedPerSecondChart = new lineChart(mappedPerSecondContainer, {
-            grid: true
+            grid: true,
+            tooltipProvider: date => indexingWidget.tooltipContent(date),
+            onMouseMove: date => this.onMouseMove(date)
         });
         const reducedPerSecondContainer = this.container.querySelector(".reduced-per-second-chart");
         this.reducedPerSecondChart = new lineChart(reducedPerSecondContainer, {
-            grid: true
+            grid: true,
+            tooltipProvider: date => indexingWidget.tooltipContent(date),
+            onMouseMove: date => this.onMouseMove(date)
         });
-    }
-    
-    onData(nodeTag: string, data: Raven.Server.Dashboard.Cluster.Notifications.IndexingSpeedPayload) {
-        this.scheduleSyncUpdate(() => this.withStats(nodeTag, x => x.update(data)));
-
-        const date = moment.utc(data.Date).toDate();
-        const key = "node-" + nodeTag.toLocaleLowerCase();
         
-        this.scheduleSyncUpdate(() => {
-            this.indexedPerSecondChart.onData(date, [{
-                key,
-                value: data.IndexedPerSecond
-            }]);
-
-            this.mappedPerSecondChart.onData(date, [{
-                key,
-                value: data.MappedPerSecond
-            }]);
-
-            this.reducedPerSecondChart.onData(date, [{
-                key,
-                value: data.ReducedPerSecond
-            }]);
-        });
+        return [this.indexedPerSecondChart, this.mappedPerSecondChart, this.reducedPerSecondChart];
     }
 
-    protected afterSyncUpdate(updatesCount: number) {
-        this.indexedPerSecondChart.draw();
-        this.mappedPerSecondChart.draw();
-        this.reducedPerSecondChart.draw();
-    }
-
-    protected afterComponentResized() {
-        this.indexedPerSecondChart.onResize();
-        this.mappedPerSecondChart.onResize();
-        this.reducedPerSecondChart.onResize();
-
-        this.indexedPerSecondChart.draw();
-        this.mappedPerSecondChart.draw();
-        this.reducedPerSecondChart.draw();
-    }
-
-    onClientConnected(ws: clusterDashboardWebSocketClient) {
-        super.onClientConnected(ws);
-
-        this.withStats(ws.nodeTag, x => x.disconnected(false));
-    }
-
-    onClientDisconnected(ws: clusterDashboardWebSocketClient) {
-        super.onClientDisconnected(ws);
-
-        this.withStats(ws.nodeTag, x => x.disconnected(true));
-    }
-
-    private withStats(nodeTag: string, action: (stats: indexingSpeed) => void) {
-        const stats = this.nodeStats().find(x => x.tag === nodeTag);
-        if (stats) {
-            action(stats);
+    protected extractDataForChart(chart: lineChart, data: Raven.Server.Dashboard.Cluster.Notifications.IndexingSpeedPayload): number {
+        if (chart === this.indexedPerSecondChart) {
+            return data.IndexedPerSecond;
+        } else if (chart === this.mappedPerSecondChart) {
+            return data.MappedPerSecond;
+        } else if (chart === this.reducedPerSecondChart) {
+            return data.ReducedPerSecond;
+        } else {
+            throw new Error("Unsupported chart: " + chart);
         }
     }
 }
