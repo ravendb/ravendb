8268cbb581ba265e694db5c5912714d7661d100d
egor <egor@ravendb.net>
2021-04-08 18:11:37 +0300

    RavenDB-16264: use azure storage for azure tests

--- test/SlowTests/Server/Documents/PeriodicBackup/Azure.cs
+++ test/SlowTests/Server/Documents/PeriodicBackup/Azure.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using FastTests;
+using Raven.Client.Documents.Operations.Backups;
+using Raven.Server.Documents.PeriodicBackup;
 using Raven.Server.Documents.PeriodicBackup.Azure;
 using Tests.Infrastructure;
 using Xunit;
@@ -20,200 +24,152 @@ namespace SlowTests.Server.Documents.PeriodicBackup
         [AzureFact]
         public void list_blobs()
         {
-            var blobNames = GenerateBlobNames(2, out var prefix);
-
-            using (var client = new RavenAzureClient(AzureFactAttribute.AzureSettings))
+            using (var holder = new AzureClientHolder(AzureFactAttribute.AzureSettings))
             {
-                try
+                var blobNames = GenerateBlobNames(AzureFactAttribute.AzureSettings, 2, out var prefix);
+                foreach (var blob in blobNames)
                 {
-                    foreach (var blob in blobNames)
-                    {
-                        client.PutBlob(blob, new MemoryStream(Encoding.UTF8.GetBytes("abc")), new Dictionary<string, string>());
-                    }
+                    holder.Client.PutBlob(blob, new MemoryStream(Encoding.UTF8.GetBytes("abc")), new Dictionary<string, string>());
+                }
 
-                    var blobsCount = client.ListBlobs(prefix, delimiter: null, listFolders: false).List.Count();
-                    Assert.Equal(blobsCount, 2);
-                }
-                finally
-                {
-                    DeleteBlobs(client, blobNames, prefix);
-                }
+                var blobsCount = holder.Client.ListBlobs(prefix, delimiter: null, listFolders: false).List.Count();
+                Assert.Equal(blobsCount, 2);
             }
         }
 
         [AzureFact]
         public void CanRemoveBlobsInBatch()
         {
-            var blobKey = Guid.NewGuid().ToString();
-
-            using var client = new RavenAzureClient(AzureFactAttribute.AzureSettings);
-            var blobs = new List<string>();
-
-            try
+            using (var holder = new AzureClientHolder(AzureFactAttribute.AzureSettings))
             {
-                const string perfix = nameof(CanRemoveBlobsInBatch);
+                var blobs = new List<string>();
+                string prefix = holder.Client.RemoteFolderName;
 
                 for (int i = 0; i < 10; i++)
                 {
-                    var key = $"{perfix}/{blobKey}/northwind_{i}.ravendump";
+                    var key = $"{prefix}/northwind_{i}.ravendump";
                     var tmpArr = new byte[3];
                     new Random().NextBytes(tmpArr);
-                    client.PutBlob(key, new MemoryStream(tmpArr), new Dictionary<string, string> {{$"property_{i}", $"value_{i}"}});
-
-                    var blob = client.GetBlob(key);
-                    Assert.NotNull(blob);
-
+                    holder.Client.PutBlob(key, new MemoryStream(tmpArr), new Dictionary<string, string> { { $"property_{i}", $"value_{i}" } });
                     blobs.Add(key);
                 }
 
-                client.DeleteBlobs(blobs);
+                Assert.Equal(blobs.Count, holder.Client.ListBlobs(prefix, delimiter: null, listFolders: false).List.Count());
 
-                var listBlobs = client.ListBlobs(perfix, null, listFolders: false);
+                holder.Client.DeleteBlobs(blobs);
+
+                var listBlobs = holder.Client.ListBlobs(prefix, null, listFolders: false);
                 var blobNames = listBlobs.List.Select(b => b.Name).ToList();
                 Assert.Equal(0, blobNames.Count);
-            }
-            finally
-            {
-                //TODO: delete blobs
             }
         }
 
         [AzureFact]
-        public void RemoveNonExistingBlobsInBatchShouldThrow()
+        public void CanRemoveBlobsWithNonExistingBlobsInBatch()
         {
-            var containerName = Guid.NewGuid().ToString();
-            var blobKey = Guid.NewGuid().ToString();
-
-            using var client = new RavenAzureClient(AzureFactAttribute.AzureSettings);
-            var blobs = new List<string>();
-            try
+            using (var holder = new AzureClientHolder(AzureFactAttribute.AzureSettings))
             {
-                client.DeleteContainer();
-                client.PutContainer();
-
+                var blobs = new List<string>();
                 // put blob
-                var k = $"{blobKey}/northwind_322.ravendump";
+                var k = GenerateBlobNames(AzureFactAttribute.AzureSettings, 1, out var prefix).First();
                 var tmpArr = new byte[3];
                 new Random().NextBytes(tmpArr);
-                client.PutBlob(k, new MemoryStream(tmpArr), new Dictionary<string, string> {{"Nice", "NotNice"}});
+                holder.Client.PutBlob(k, new MemoryStream(tmpArr), new Dictionary<string, string> { { "Nice", "NotNice" } });
 
-                var blob = client.GetBlob(k);
+                var blob = holder.Client.GetBlob(k);
                 Assert.NotNull(blob);
                 blobs.Add(k);
 
                 for (int i = 0; i < 10; i++)
                 {
-                    blobs.Add($"{blobKey}/northwind_{i}.ravendump");
+                    blobs.Add($"{prefix}/northwind_{i}.ravendump");
                 }
 
-                try
-                {
-                    client.DeleteBlobs(blobs);
-                }
-                catch (Exception e)
-                {
-                    Assert.Equal(typeof(InvalidOperationException), e.GetType());
-                    Assert.True(e.Message.StartsWith(
-                        $"Failed to delete {blobs.Count - 1} blobs from container: {containerName}. Successfully deleted 1 blob. Reason: The specified blob does not exist."));
-                }
-            }
-            finally
-            {
-                //TODO: delete blobs
+                holder.Client.DeleteBlobs(blobs);
+
+                var listBlobs = holder.Client.ListBlobs(prefix, null, listFolders: false);
+                var blobNames = listBlobs.List.Select(b => b.Name).ToList();
+                Assert.Equal(0, blobNames.Count);
             }
         }
 
         [AzureFact]
         public void put_blob()
         {
-            var blobKey = Guid.NewGuid().ToString();
-
-            using (var client = new RavenAzureClient(AzureFactAttribute.AzureSettings))
+            using (var holder = new AzureClientHolder(AzureFactAttribute.AzureSettings))
             {
-                try
-                {
-                    client.PutBlob(blobKey, new MemoryStream(Encoding.UTF8.GetBytes("123")), new Dictionary<string, string>
+                var blobKey = GenerateBlobNames(AzureFactAttribute.AzureSettings, 1, out _).First();
+
+                holder.Client.PutBlob(blobKey, new MemoryStream(Encoding.UTF8.GetBytes("123")), new Dictionary<string, string>
                     {
                         {"property1", "value1"},
                         {"property2", "value2"}
                     });
-                    var blob = client.GetBlob(blobKey);
-                    Assert.NotNull(blob);
+                var blob = holder.Client.GetBlob(blobKey);
+                Assert.NotNull(blob);
 
-                    using (var reader = new StreamReader(blob.Data))
-                        Assert.Equal("123", reader.ReadToEnd());
+                using (var reader = new StreamReader(blob.Data))
+                    Assert.Equal("123", reader.ReadToEnd());
 
-                    var property1 = blob.Metadata.Keys.Single(x => x.Contains("property1"));
-                    var property2 = blob.Metadata.Keys.Single(x => x.Contains("property2"));
+                var property1 = blob.Metadata.Keys.Single(x => x.Contains("property1"));
+                var property2 = blob.Metadata.Keys.Single(x => x.Contains("property2"));
 
-                    Assert.Equal("value1", blob.Metadata[property1]);
-                    Assert.Equal("value2", blob.Metadata[property2]);
-                }
-                finally
-                {
-                    DeleteBlobs(client, new List<string> { blobKey }, blobKey);
-                }
+                Assert.Equal("value1", blob.Metadata[property1]);
+                Assert.Equal("value2", blob.Metadata[property2]);
             }
         }
 
         [AzureFact]
         public void put_blob_in_folder()
         {
-            var blobNames = GenerateBlobNames(1, out var prefix);
-
-            using (var client = new RavenAzureClient(AzureFactAttribute.AzureSettings))
+            using (var holder = new AzureClientHolder(AzureFactAttribute.AzureSettings))
             {
-                try
-                {
-                    client.PutBlob(blobNames[0], new MemoryStream(Encoding.UTF8.GetBytes("123")),
+                var blobNames = GenerateBlobNames(AzureFactAttribute.AzureSettings, 1, out _);
+
+                holder.Client.PutBlob(blobNames[0], new MemoryStream(Encoding.UTF8.GetBytes("123")),
                         new Dictionary<string, string> { { "property1", "value1" }, { "property2", "value2" } });
 
-                    var blob = client.GetBlob(blobNames[0]);
-                    Assert.NotNull(blob);
+                var blob = holder.Client.GetBlob(blobNames[0]);
+                Assert.NotNull(blob);
 
-                    using (var reader = new StreamReader(blob.Data))
-                        Assert.Equal("123", reader.ReadToEnd());
+                using (var reader = new StreamReader(blob.Data))
+                    Assert.Equal("123", reader.ReadToEnd());
 
-                    var property1 = blob.Metadata.Keys.Single(x => x.Contains("property1"));
-                    var property2 = blob.Metadata.Keys.Single(x => x.Contains("property2"));
+                var property1 = blob.Metadata.Keys.Single(x => x.Contains("property1"));
+                var property2 = blob.Metadata.Keys.Single(x => x.Contains("property2"));
 
-                    Assert.Equal("value1", blob.Metadata[property1]);
-                    Assert.Equal("value2", blob.Metadata[property2]);
-                }
-                finally
-                {
-                    DeleteBlobs(client, blobNames, prefix);
-                }
+                Assert.Equal("value1", blob.Metadata[property1]);
+                Assert.Equal("value2", blob.Metadata[property2]);
             }
         }
 
         [AzureFact]
         public void put_blob_without_sas_token()
         {
-            PutBlobs(5, false);
+            PutBlobs(5, useSasToken: false);
         }
 
         [AzureSasTokenFact]
         public void put_blob_with_sas_token()
         {
-            PutBlobs(5, true);
+            PutBlobs(5, useSasToken: true);
         }
 
         private static void PutBlobs(int blobsCount, bool useSasToken)
         {
-            var blobNames = GenerateBlobNames(blobsCount, out var prefix);
-
-            using (var client = new RavenAzureClient(useSasToken ? AzureSasTokenFactAttribute.AzureSettings : AzureFactAttribute.AzureSettings))
+            var settings = useSasToken == false ? AzureFactAttribute.AzureSettings : AzureSasTokenFactAttribute.AzureSettings;
+            using (var holder = new AzureClientHolder(settings))
             {
+                var blobNames = GenerateBlobNames(settings, blobsCount, out var prefix);
                 for (var i = 0; i < blobsCount; i++)
                 {
-                    client.PutBlob(blobNames[i], new MemoryStream(Encoding.UTF8.GetBytes("123")),
+                    holder.Client.PutBlob(blobNames[i], new MemoryStream(Encoding.UTF8.GetBytes("123")),
                         new Dictionary<string, string> { { "property1", "value1" }, { "property2", "value2" } });
                 }
 
                 for (var i = 0; i < blobsCount; i++)
                 {
-                    var blob = client.GetBlob(blobNames[i]);
+                    var blob = holder.Client.GetBlob(blobNames[i]);
                     Assert.NotNull(blob);
 
                     using (var reader = new StreamReader(blob.Data))
@@ -226,43 +182,88 @@ namespace SlowTests.Server.Documents.PeriodicBackup
                     Assert.Equal("value2", blob.Metadata[property2]);
                 }
 
-                var listBlobs = client.ListBlobs(prefix, null, listFolders: false);
+                var listBlobs = holder.Client.ListBlobs(prefix, null, listFolders: false);
                 Assert.Equal(blobsCount, listBlobs.List.Count());
-
-                // delete all blobs
-                client.DeleteBlobs(blobNames);
-
-                listBlobs = client.ListBlobs(prefix, null, listFolders: false);
-                Assert.Equal(0, listBlobs.List.Select(b => b.Name).Count());
-
-                for (var i = 0; i < blobsCount; i++)
-                {
-                    var blob = client.GetBlob(blobNames[i]);
-                    Assert.Null(blob);
-                }
             }
         }
 
-        private static void DeleteBlobs(RavenAzureClient client, List<string> blobsToDelete, string prefix)
-        {
-            client.DeleteBlobs(blobsToDelete);
-
-            var blobsCount = client.ListBlobs(prefix, delimiter: null, listFolders: false).List.Count();
-            Assert.Equal(blobsCount, 0);
-        }
-
-        private static List<string> GenerateBlobNames(int blobsCount, out string prefix)
+        internal static List<string> GenerateBlobNames(AzureSettings setting, int blobsCount, out string prefix)
         {
             var blobNames = new List<string>();
 
             prefix = Guid.NewGuid().ToString();
+            if (string.IsNullOrEmpty(setting.RemoteFolderName) == false)
+                prefix = $"{setting.RemoteFolderName}/{prefix}";
+
             for (var i = 0; i < blobsCount; i++)
             {
-                blobNames.Add($"{prefix}/{i}");
+                var name = $"{prefix}/{i}";
+
+                blobNames.Add(name);
             }
 
             return blobNames;
         }
+
+        internal class AzureClientHolder : IDisposable
+        {
+            public RavenAzureClient Client { get; set; }
+
+            private readonly string _remoteFolderName;
+
+            public AzureClientHolder(AzureSettings setting, Progress progress = null, [CallerMemberName] string caller = null)
+            {
+                Assert.False(string.IsNullOrEmpty(setting.StorageContainer), "string.IsNullOrEmpty(setting.StorageContainer)");
+
+                // keep only alphanumeric characters
+                caller = caller == null ? string.Empty : string.Concat(caller.Where(char.IsLetterOrDigit));
+                _remoteFolderName = setting.RemoteFolderName = $"{caller}{Guid.NewGuid()}";
+                Client = new RavenAzureClient(setting, progress);
+            }
+
+            public void Dispose()
+            {
+                const int numberOfObjectsInBatch = 256;
+                var blobsToDelete = new List<string>();
+                string blobsNextMarker = null;
+                var cts = new CancellationTokenSource(TimeSpan.FromMinutes(5));
+
+                try
+                {
+                    do
+                    {
+                        var blobs = Client.ListBlobs(_remoteFolderName, delimiter: null, listFolders: false, marker: blobsNextMarker);
+
+                        foreach (var blob in blobs.List)
+                        {
+                            if (blobsToDelete.Count == numberOfObjectsInBatch)
+                            {
+                                Client.DeleteBlobs(blobsToDelete);
+                                blobsToDelete.Clear();
+                            }
+
+                            blobsToDelete.Add(blob.Name);
+                        }
+
+                        blobsNextMarker = blobs.NextMarker;
+
+                        cts.Token.ThrowIfCancellationRequested();
+
+                    } while (blobsNextMarker != null);
+
+                    if (blobsToDelete.Count > 0)
+                        Client.DeleteBlobs(blobsToDelete);
+
+                    Assert.Empty(Client.ListBlobs(_remoteFolderName, delimiter: null, listFolders: false).List);
+                }
+                catch
+                {
+                    // ignored
+                }
+
+                Client.Dispose();
+                cts.Dispose();
+            }
+        }
     }
 }
-
--- test/SlowTests/Server/Documents/PeriodicBackup/RavenDB-4613.cs
+++ test/SlowTests/Server/Documents/PeriodicBackup/RavenDB-4613.cs
@@ -5,10 +5,15 @@
 // -----------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using FastTests;
 using Raven.Client.Documents.Operations.Backups;
 using Raven.Server.Documents.PeriodicBackup;
+using Sparrow;
 using Tests.Infrastructure;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace SlowTests.Server.Documents.PeriodicBackup
@@ -20,203 +25,97 @@ namespace SlowTests.Server.Documents.PeriodicBackup
         }
 
         [AzureFact]
-        public void put_blob_64MB()
+        public void put_blob_4MB_chunked()
         {
-            PutBlob(64, false, UploadType.Regular);
+            PutBlob(sizeInMB: 4, testBlobKeyAsFolder: false, UploadType.Chunked);
         }
 
         [AzureFact]
-        public void put_blob_70MB()
+        public void put_blob_into_folder_4MB_chunked()
         {
-            PutBlob(70, false, UploadType.Regular);
-        }
-
-        [AzureFact]
-        public void put_blob_100MB()
-        {
-            PutBlob(100, false, UploadType.Regular);
-        }
-
-        [NightlyBuildAzureFact]
-        public void put_blob_256MB()
-        {
-            PutBlob(256, false, UploadType.Regular);
-        }
-
-        [NightlyBuildAzureFact]
-        public void put_blob_500MB()
-        {
-            PutBlob(500, false, UploadType.Chunked);
-        }
-
-        [NightlyBuildAzureFact]
-        public void put_blob_765MB()
-        {
-            PutBlob(765, false, UploadType.Chunked);
-        }
-
-        [AzureFact]
-        public void put_blob_into_folder_64MB()
-        {
-            PutBlob(64, true, UploadType.Regular);
-        }
-
-        [AzureFact]
-        public void put_blob_into_folder_70MB()
-        {
-            PutBlob(70, true, UploadType.Regular);
-        }
-
-        [AzureFact]
-        public void put_blob_into_folder_100MB()
-        {
-            PutBlob(100, true, UploadType.Regular);
-        }
-
-        [NightlyBuildAzureFact]
-        public void put_blob_into_folder_256MB()
-        {
-            PutBlob(256, true, UploadType.Regular);
-        }
-
-        [NightlyBuildAzureFact]
-        public void put_blob_into_folder_500MB()
-        {
-            PutBlob(500, true, UploadType.Chunked);
-        }
-
-        [NightlyBuildAzureFact]
-        public void put_blob_into_folder_765MB()
-        {
-            PutBlob(765, true, UploadType.Chunked);
-        }
-
-        [AzureFact]
-        public void can_get_and_delete_container()
-        {
-            /*var containerName = Guid.NewGuid().ToString();
-            using (var client = new RavenAzureClient(Azure.GetAzureSettings(containerName)))
-            {
-                var containerNames = client.GetContainerNames(500);
-                Assert.False(containerNames.Exists(x => x.Equals(containerName)));
-                client.PutContainer();
-
-                containerNames = client.GetContainerNames(500);
-                Assert.True(containerNames.Exists(x => x.Equals(containerName)));
-
-                client.DeleteContainer();
-
-                containerNames = client.GetContainerNames(500);
-                Assert.False(containerNames.Exists(x => x.Equals(containerName)));
-            }*/
-        }
-
-        [AzureFact]
-        public void can_get_container_not_found()
-        {
-            /*var containerName = Guid.NewGuid().ToString();
-            using (var client = new RavenAzureClient(Azure.GetAzureSettings(containerName)))
-            {
-                var containerNames = client.GetContainerNames(500);
-                Assert.False(containerNames.Exists(x => x.Equals(containerName)));
-
-                var e = Assert.Throws<ContainerNotFoundException>(() => client.TestConnection());
-                Assert.Equal($"Container '{containerName}' wasn't found!", e.Message);
-
-                containerNames = client.GetContainerNames(500);
-                Assert.False(containerNames.Exists(x => x.Equals(containerName)));
-            }*/
+            PutBlob(sizeInMB: 4, testBlobKeyAsFolder: true, UploadType.Chunked);
         }
 
         // ReSharper disable once InconsistentNaming
         private void PutBlob(int sizeInMB, bool testBlobKeyAsFolder, UploadType uploadType)
         {
-            //TODO:
-            var containerName = Guid.NewGuid().ToString();
-            var blobKey = testBlobKeyAsFolder == false ?
-                Guid.NewGuid().ToString() :
-                $"{Guid.NewGuid()}/folder/testKey";
-
             var progress = new Progress();
-            /*using (var client = new RavenAzureClient(Azure.GetAzureSettings(containerName), progress: progress))
+            using (var holder = new Azure.AzureClientHolder(AzureFactAttribute.AzureSettings, progress))
             {
-                try
+                holder.Client.MaxUploadPutBlobInBytes = 3 * 1024 * 1024;
+                holder.Client.OnePutBlockSizeLimitInBytes = 1 * 1024 * 1024;
+
+                var blobKey = testBlobKeyAsFolder == false
+                    ? AzureFactAttribute.AzureSettings.RemoteFolderName
+                    : $"{AzureFactAttribute.AzureSettings.RemoteFolderName}/folder/testKey";
+
+                var path = NewDataPath(forceCreateDir: true);
+                var filePath = Path.Combine(path, Guid.NewGuid().ToString());
+
+                var sizeMb = new Size(sizeInMB, SizeUnit.Megabytes);
+                var size64Kb = new Size(64, SizeUnit.Kilobytes);
+
+                var buffer = Enumerable.Range(0, (int)size64Kb.GetValue(SizeUnit.Bytes))
+                    .Select(x => (byte)'a')
+                    .ToArray();
+
+                using (var file = File.Open(filePath, FileMode.CreateNew))
                 {
-                    client.DeleteContainer();
-                    client.PutContainer();
-
-                    var path = NewDataPath(forceCreateDir: true);
-                    var filePath = Path.Combine(path, Guid.NewGuid().ToString());
-
-                    var sizeMb = new Size(sizeInMB, SizeUnit.Megabytes);
-                    var size64Kb = new Size(64, SizeUnit.Kilobytes);
-
-                    var buffer = Enumerable.Range(0, (int)size64Kb.GetValue(SizeUnit.Bytes))
-                        .Select(x => (byte)'a')
-                        .ToArray();
-
-                    using (var file = File.Open(filePath, FileMode.CreateNew))
+                    for (var i = 0; i < sizeMb.GetValue(SizeUnit.Bytes) / buffer.Length; i++)
                     {
-                        for (var i = 0; i < sizeMb.GetValue(SizeUnit.Bytes) / buffer.Length; i++)
-                        {
-                            file.Write(buffer, 0, buffer.Length);
-                        }
+                        file.Write(buffer, 0, buffer.Length);
                     }
+                }
 
-                    var value1 = Guid.NewGuid().ToString();
-                    var value2 = Guid.NewGuid().ToString();
-                    var value3 = Guid.NewGuid().ToString();
+                var value1 = Guid.NewGuid().ToString();
+                var value2 = Guid.NewGuid().ToString();
+                var value3 = Guid.NewGuid().ToString();
 
-                    long streamLength;
-                    using (var file = File.Open(filePath, FileMode.Open))
-                    {
-                        streamLength = file.Length;
-                        client.PutBlob(blobKey, file,
-                            new Dictionary<string, string>
-                            {
+                long streamLength;
+                using (var file = File.Open(filePath, FileMode.Open))
+                {
+                    streamLength = file.Length;
+                    holder.Client.PutBlob(blobKey, file,
+                        new Dictionary<string, string>
+                        {
                                     {"property1", value1},
                                     {"property2", value2},
                                     {"property3", value3}
-                            });
-                    }
-
-                    var blob = client.GetBlob(blobKey);
-                    Assert.NotNull(blob);
-
-                    using (var reader = new StreamReader(blob.Data))
-                    {
-                        var readBuffer = new char[buffer.Length];
-
-                        long read, totalRead = 0;
-                        while ((read = reader.Read(readBuffer, 0, readBuffer.Length)) > 0)
-                        {
-                            for (var i = 0; i < read; i++)
-                                Assert.Equal(buffer[i], (byte)readBuffer[i]);
-
-                            totalRead += read;
-                        }
-
-                        Assert.Equal(streamLength, totalRead);
-                    }
-
-                    var property1 = blob.Metadata.Keys.Single(x => x.Contains("property1"));
-                    var property2 = blob.Metadata.Keys.Single(x => x.Contains("property2"));
-                    var property3 = blob.Metadata.Keys.Single(x => x.Contains("property3"));
-
-                    Assert.Equal(value1, blob.Metadata[property1]);
-                    Assert.Equal(value2, blob.Metadata[property2]);
-                    Assert.Equal(value3, blob.Metadata[property3]);
-
-                    Assert.Equal(UploadState.Done, progress.UploadProgress.UploadState);
-                    Assert.Equal(uploadType, progress.UploadProgress.UploadType);
-                    Assert.Equal(streamLength, progress.UploadProgress.TotalInBytes);
-                    Assert.Equal(streamLength, progress.UploadProgress.UploadedInBytes);
+                        });
                 }
-                finally
+
+                var blob = holder.Client.GetBlob(blobKey);
+                Assert.NotNull(blob);
+
+                using (var reader = new StreamReader(blob.Data))
                 {
-                    client.DeleteContainer();
+                    var readBuffer = new char[buffer.Length];
+
+                    long read, totalRead = 0;
+                    while ((read = reader.Read(readBuffer, 0, readBuffer.Length)) > 0)
+                    {
+                        for (var i = 0; i < read; i++)
+                            Assert.Equal(buffer[i], (byte)readBuffer[i]);
+
+                        totalRead += read;
+                    }
+
+                    Assert.Equal(streamLength, totalRead);
                 }
-            }*/
+
+                var property1 = blob.Metadata.Keys.Single(x => x.Contains("property1"));
+                var property2 = blob.Metadata.Keys.Single(x => x.Contains("property2"));
+                var property3 = blob.Metadata.Keys.Single(x => x.Contains("property3"));
+
+                Assert.Equal(value1, blob.Metadata[property1]);
+                Assert.Equal(value2, blob.Metadata[property2]);
+                Assert.Equal(value3, blob.Metadata[property3]);
+
+                Assert.Equal(UploadState.Done, progress.UploadProgress.UploadState);
+                Assert.Equal(uploadType, progress.UploadProgress.UploadType);
+                Assert.Equal(streamLength, progress.UploadProgress.TotalInBytes);
+                Assert.Equal(streamLength, progress.UploadProgress.UploadedInBytes);
+            }
         }
     }
 }
