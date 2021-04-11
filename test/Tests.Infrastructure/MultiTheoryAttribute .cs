b0bf20f7706a258320b3f67f83ad7cb56f2b9d5e
egor <egor@ravendb.net>
2021-04-11 13:55:45 +0300

    RavenDB-16264: use env variable for retention policy azure stress tests

---
+++ test/Tests.Infrastructure/MultiTheoryAttribute .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Tests.Infrastructure
+{
+    public class MultiTheoryAttribute : TheoryAttribute
+    {
+        public MultiTheoryAttribute(params Type[] types)
+        {
+            var result = types.Select(Activator.CreateInstance).Cast<TheoryAttribute>().ToList();
+
+            if (result.Any(x => string.IsNullOrEmpty(x.Skip) == false))
+            {
+                Skip = string.Join(", ", result.Where(x => string.IsNullOrEmpty(x.Skip) == false).Select(x => x.Skip));
+            }
+        }
+    }
+}
