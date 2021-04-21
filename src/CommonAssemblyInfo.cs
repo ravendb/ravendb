5197097d8c5a3b2aa5407979ae6565f50c00cfd5
Pawel Pekrol <ppekrol@ravendb.net>
2021-04-21 13:06:32 +0200

    RavenDB-7070 bumped year to 2021

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Resources;
 
-[assembly: AssemblyCopyright("© Hibernating Rhinos 2009 - 2020 All rights reserved.")]
+[assembly: AssemblyCopyright("© Hibernating Rhinos 2009 - 2021 All rights reserved.")]
 
 [assembly: AssemblyVersion("4.2.113")]
 [assembly: AssemblyFileVersion("4.2.113.42")]
