29962ba3a65f1f768259ad764cfaf871cf91da41
Pawel Pekrol <ppekrol@ravendb.net>
2021-04-21 12:35:33 +0200

    RavenDB-16516 dispose TcpConnectionOptions when doing TestConnection or Ping to avoid getting into CLOSE_WAIT state

--- src/Raven.Client/Http/HttpCache.cs
+++ src/Raven.Client/Http/HttpCache.cs
@@ -110,7 +110,7 @@ namespace Raven.Client.Http
                 if (_usages > 0)
                     throw new LowMemoryException("Detected a leak on HttpCache when running the finalizer. See: https://issues.hibernatingrhinos.com/issue/RavenDB-9737");
 
-        }
+            }
 #endif
         }
 
@@ -201,7 +201,7 @@ namespace Raven.Client.Http
                 if (_items.Count == 0)
                     return;
 
-                Debug.Assert(_isFreeSpaceRunning); 
+                Debug.Assert(_isFreeSpaceRunning);
 
                 if (Logger.IsInfoEnabled)
                     Logger.Info($"Started to clear the http cache. Items: {_items.Count:#,#;;0}");
@@ -254,7 +254,7 @@ namespace Raven.Client.Http
             finally
             {
                 _isFreeSpaceRunning.Lower();
-            }            
+            }
         }
 
         public struct ReleaseCacheItem : IDisposable
@@ -308,7 +308,7 @@ namespace Raven.Client.Http
 
                     obj = item.Ptr != null ? new BlittableJsonReaderObject(item.Ptr, item.Size, context) : null;
 #if DEBUG
-                    if(obj != null)
+                    if (obj != null)
                     {
                         obj.BlittableValidation();
                     }
