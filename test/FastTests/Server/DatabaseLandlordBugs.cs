eeaabbf010ec2f9e0d875bd26d630b5e29d8e01d
Shiran Shalom <shiran@ravendb.net>
2021-04-22 17:19:52 +0300

    RavenDB-14324 - add test

--- test/FastTests/Server/DatabaseLandlordBugs.cs
+++ test/FastTests/Server/DatabaseLandlordBugs.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Raven.Client.Exceptions;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -25,5 +29,41 @@ namespace FastTests.Server
             Assert.NotNull(database);
         }
 
+
+        [Fact]
+        public void CannotDeleteEntireDbFolder()
+        {
+            var p1 = NewDataPath(forceCreateDir: true);
+
+            var db1 = CreateDatabaseWithName(false, p1, null, "someDB1");
+            Assert.NotNull(db1);
+            var db2 = CreateDatabaseWithName(false, Path.Combine(p1, "someDB2"), null, "someDB2");
+            Assert.NotNull(db2);
+
+            var origFiles = Directory.GetFileSystemEntries(Path.Combine(p1, "someDB2"));
+
+            Assert.Throws<RavenException>(()=> DeleteDatabase("someDB1"));
+
+            var currFiles = Directory.GetFileSystemEntries(Path.Combine(p1, "someDB2"));
+            string[] filesWithoutExtension = new string[currFiles.Length];
+            int i = 0;
+            foreach (var currFile in currFiles)
+            {
+                filesWithoutExtension[i] = Path.GetFileNameWithoutExtension(currFile);
+                i++;
+            }
+
+            bool isExist = true;
+            foreach (var file in origFiles)
+            {
+                var filename = Path.GetFileNameWithoutExtension(file);
+                if (filesWithoutExtension.Contains(filename) == false)
+                {
+                    isExist = false;
+                    break;
+                }
+            }
+            Assert.True(isExist);
+        }
     }
 }
