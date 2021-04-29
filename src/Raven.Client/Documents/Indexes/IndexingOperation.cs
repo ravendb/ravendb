f53bec78b6b20812683a7a11a2add14a0c767cdc
Arkadiusz Palinski <arek@ravendb.net>
2021-04-29 09:22:53 +0200

    RavenDB-16598 Expose wait time in the stats if an index is waiting for the lock which limits the concurrently running indexes

--- src/Raven.Client/Documents/Indexes/IndexingOperation.cs
+++ src/Raven.Client/Documents/Indexes/IndexingOperation.cs
@@ -53,7 +53,11 @@ namespace Raven.Client.Documents.Indexes
             public static string GetMapEntries = "GetMapEntries";
             public static string SaveOutputDocuments = "SaveOutputDocuments";
             public static string DeleteOutputDocuments = "DeleteOutputDocuments";
+        }
 
+        internal static class Wait
+        {
+            public const string AcquireConcurrentlyRunningIndexesLock = "Wait/ConcurrentlyRunningIndexesLimit";
         }
     }
 }
