49fae18f527474314b086e7e579a3d6960fc1a2f
Grzegorz Lachowski <gregolsky@ravendb.net>
2021-04-29 10:12:58 +0200

    RavenDB-15747 minor fixes and unification to make it work on CI and locally on Windows

--- scripts/linux/pkg/deb/build-deb_ubuntu-focal_amd64.ps1
+++ scripts/linux/pkg/deb/build-deb_ubuntu-focal_amd64.ps1
@@ -1,9 +1,6 @@
-$env:DISTRO_NAME = "ubuntu"
-$env:DISTRO_VERSION = "20.04"
-$env:DISTRO_VERSION_NAME ="focal"
-
 $env:OUTPUT_DIR = "$PSScriptRoot/dist"
 
+.\set-ubuntu-focal.ps1
 .\set-raven-platform-amd64.ps1
 .\set-raven-version-env.ps1
 
