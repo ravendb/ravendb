d62c6829f2b4ba4d0ec1979c242574c5d3393d46
omer <omer@ravendb.net>
2021-05-05 13:36:48 +0300

    RavenDB-6669 - Format changes and deleting redundant signature

--- src/Raven.Client/Documents/Session/IAdvancedSessionOperations.Patch.cs
+++ src/Raven.Client/Documents/Session/IAdvancedSessionOperations.Patch.cs
@@ -31,9 +31,6 @@ namespace Raven.Client.Documents.Session
 
         void Patch<T, TKey, TValue>(T entity, Expression<Func<T, IDictionary<TKey, TValue>>> path,
             Expression<Func<JavaScriptDictionary<TKey, TValue>, object>> dictionaryAdder);
-
-        void Patch<T, TKey, TValue>(string id, Expression<Func<T, IDictionary<TKey, TValue>>> patch,
-            Expression<Func<JavaScriptDictionary<TKey, TValue>, object>> dictionaryAdder);
         
         void AddOrPatch<T, TU>(string id, T entity, Expression<Func<T, TU>> patch, TU value);
         
