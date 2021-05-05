4021341fb561469c293da6d0ab17af0ef2eb522d
omer <omer@ravendb.net>
2021-05-05 10:25:30 +0300

    RavenDB-16607: Fixed - OnBeforeQueryInvoked was not invoked

--- test/SlowTests/Core/Streaming/OnBeforeQuery.cs
+++ test/SlowTests/Core/Streaming/OnBeforeQuery.cs
@@ -11,11 +11,12 @@ using Xunit.Abstractions;
 
 namespace SlowTests.Core.Streaming
 {
-    public class OnBeforeQuery :RavenTestBase
+    public class OnBeforeQuery : RavenTestBase
     {
         public OnBeforeQuery(ITestOutputHelper output) : base(output)
         {
         }
+
         [Fact]
         public async Task ShouldStreamArticlesOfTenant1()
         {
@@ -24,22 +25,18 @@ namespace SlowTests.Core.Streaming
 
             using (var store = GetDocumentStore())
             {
-                using (var session =  store.OpenAsyncSession(store.Database))
+                using (var session = store.OpenAsyncSession(store.Database))
                 {
-                    // TenantA Articles
                     await session.StoreAsync(new Article(tenantA, "Article1", false));
                     await session.StoreAsync(new Article(tenantA, "Article2", false));
                     await session.StoreAsync(new Article(tenantA, "Article Deleted", true));
 
-                    // TenantB Articles
                     await session.StoreAsync(new Article(tenantB, "Article3", false));
                     await session.StoreAsync(new Article(tenantB, "Article4", false));
                     await session.StoreAsync(new Article(tenantB, "Article5 Deleted", true));
 
                     await session.SaveChangesAsync();
-                    
-                    
-                    // before query constrain to TenantA documents
+
                     session.Advanced.OnBeforeQuery += (sender, args) =>
                     {
                         dynamic queryToBeExecuted = args.QueryCustomization;
@@ -47,24 +44,23 @@ namespace SlowTests.Core.Streaming
                         queryToBeExecuted.WhereEquals(nameof(Article.TenantId), tenantA);
                     };
 
-                    IRavenQueryable<Article> query = 
-                            session.Query<Article>().
-                            Where(article => article.Deleted == false);
-                    
+                    IRavenQueryable<Article> query =
+                        session.Query<Article>().Where(article => article.Deleted == false);
+
                     List<Article> queryResult = await query.ToListAsync();
                     Console.WriteLine(queryResult.Count);
                     Assert.Equal(2, queryResult.Count);
                     Assert.True(queryResult.All(x => x.TenantId.Equals(tenantA, StringComparison.Ordinal)));
-                    
-                     IAsyncEnumerator<StreamResult<Article>> streamResult = await session.Advanced.StreamAsync(query);
-                     var streamedItems = new List<Article>();
-                     while (await streamResult.MoveNextAsync())
-                     {
-                         streamedItems.Add(streamResult.Current.Document);
-                     }
-                    
-                     Assert.Equal(2, streamedItems.Count);
-                     Assert.True(streamedItems.All(x => x.TenantId.Equals(tenantA, StringComparison.Ordinal)));
+
+                    IAsyncEnumerator<StreamResult<Article>> streamResult = await session.Advanced.StreamAsync(query);
+                    var streamedItems = new List<Article>();
+                    while (await streamResult.MoveNextAsync())
+                    {
+                        streamedItems.Add(streamResult.Current.Document);
+                    }
+
+                    Assert.Equal(2, streamedItems.Count);
+                    Assert.True(streamedItems.All(x => x.TenantId.Equals(tenantA, StringComparison.Ordinal)));
                 }
             }
         }
@@ -87,6 +83,5 @@ namespace SlowTests.Core.Streaming
 
             public bool Deleted { get; private set; }
         }
-        
     }
 }
