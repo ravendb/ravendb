6d8c05d45550e489a4aa4e2b9c27347f819b61d6
egor <egor@ravendb.net>
2021-05-09 15:59:48 +0300

    RavenDB-16473: throw proper error when cloud provider environment variable is missing

--- test/Tests.Infrastructure/CustomS3FactAttribute.cs
+++ test/Tests.Infrastructure/CustomS3FactAttribute.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Runtime.CompilerServices;
 using Newtonsoft.Json;
 using Raven.Client.Documents.Operations.Backups;
@@ -11,17 +10,26 @@ namespace Tests.Infrastructure
     {
         private const string S3CredentialEnvironmentVariable = "CUSTOM_S3_SETTINGS";
 
-        public static S3Settings S3Settings { get; }
+        private static readonly S3Settings _s3Settings;
+
+        public static S3Settings S3Settings => new S3Settings(_s3Settings);
 
         private static readonly string ParsingError;
+
+        private static readonly bool EnvVariableMissing;
 
         static CustomS3FactAttribute()
         {
             var strSettings = Environment.GetEnvironmentVariable(S3CredentialEnvironmentVariable);
-            
+            if (strSettings == null)
+            {
+                EnvVariableMissing = true;
+                return;
+            }
+
             try
             {
-                S3Settings = JsonConvert.DeserializeObject<S3Settings>(strSettings);
+                _s3Settings = JsonConvert.DeserializeObject<S3Settings>(strSettings);
             }
             catch (Exception e)
             {
@@ -31,13 +39,19 @@ namespace Tests.Infrastructure
 
         public CustomS3FactAttribute([CallerMemberName] string memberName = "")
         {
+            if (EnvVariableMissing)
+            {
+                Skip = $"Test is missing '{S3CredentialEnvironmentVariable}' environment variable.";
+                return;
+            }
+
             if (string.IsNullOrEmpty(ParsingError) == false)
             {
                 Skip = $"Failed to parse custom S3 settings, error: {ParsingError}";
                 return;
             }
 
-            if (S3Settings == null)
+            if (_s3Settings == null)
             {
                 Skip = $"S3 {memberName} tests missing S3 settings.";
             }
