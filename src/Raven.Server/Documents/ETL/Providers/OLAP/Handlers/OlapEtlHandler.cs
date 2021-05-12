7c3ce08bb2980e4e01bef8f9af36948c164e1787
Arkadiusz Palinski <arek@ravendb.net>
2021-05-12 14:37:43 +0200

    RavenDB-16311 OLAP ETL : Allow to test script

---
+++ src/Raven.Server/Documents/ETL/Providers/OLAP/Handlers/OlapEtlHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Raven.Server.Documents.ETL.Providers.OLAP.Test;
+using Raven.Server.Json;
+using Raven.Server.Routing;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Utils;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Server.Documents.ETL.Providers.OLAP.Handlers
+{
+    public class OlapEtlHandler : DatabaseRequestHandler
+    {
+        [RavenAction("/databases/*/admin/etl/olap/test", "POST", AuthorizationStatus.DatabaseAdmin)]
+        public async Task PostScriptTest()
+        {
+            using (Database.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+            {
+                var testConfig = await context.ReadForMemoryAsync(RequestBodyStream(), "ola-etl-test");
+
+                var testScript = JsonDeserializationServer.TestOlapEtlScript(testConfig);
+
+                var result = (OlapEtlTestScriptResult)OlapEtl.TestScript(testScript, Database, ServerStore, context);
+
+                await using (var writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
+                {
+                    var djv = (DynamicJsonValue)TypeConverter.ToBlittableSupportedType(result);
+                    writer.WriteObject(context.ReadObject(djv, "olap-etl-test"));
+                }
+            }
+        }
+    }
+}
---
+++ src/Raven.Server/Documents/ETL/Providers/OLAP/Test/OlapEtlTestScriptResult.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Raven.Server.Documents.ETL.Test;
+
+namespace Raven.Server.Documents.ETL.Providers.OLAP.Test
+{
+    public class OlapEtlTestScriptResult : TestEtlScriptResult
+    {
+        public List<PartitionItems> ItemsByPartition { get; set; }
+
+        public class PartitionItems
+        {
+            public string Key { get; set; }
+
+            public List<PartitionColumn> Columns = new List<PartitionColumn>();
+        }
+
+        public class PartitionColumn
+        {
+            public string Name { get; set; }
+
+            public string Type { get; set; }
+
+            public IList Values { get; set; }
+        }
+    }
+}
