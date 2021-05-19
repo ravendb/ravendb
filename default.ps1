4c014c00dce1a3979b5405de382b31f852a04138
Grisha Kotler <grisha@ayende.com>
2021-05-19 16:57:39 +0300

    RavenDB-16725 - fix sign tool path

--- default.ps1
+++ default.ps1
@@ -321,42 +321,46 @@ function SignFile($filePath){
     {
         return
     }
-    
+
     $signTool = "C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe"
-    if (!(Test-Path $signTool)) 
+    if (!(Test-Path $signTool))
     {
-        
         $signTool = "C:\Program Files (x86)\Windows Kits\8.0\bin\x86\signtool.exe"
-    
-        if (!(Test-Path $signTool)) 
+
+        if (!(Test-Path $signTool))
         {
             $signTool = "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\signtool.exe"
 
-            if (!(Test-Path $signTool)) 
+            if (!(Test-Path $signTool))
             {
-                throw "Could not find SignTool.exe under the specified path $signTool"
+                $signTool = "C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe"
+
+                if (!(Test-Path $signTool)) 
+                {
+                    throw "Could not find SignTool.exe under the specified path $signTool"
+                }
             }
         }
     }
-  
+
     $installerCert = "$base_dir\..\BuildsInfo\RavenDB\certs\code-sign.pfx"
-    if (!(Test-Path $installerCert)) 
+    if (!(Test-Path $installerCert))
     {
         throw "Could not find pfx file under the path $installerCert to sign the installer"
     }
-  
+
     $certPasswordPath = "$base_dir\..\BuildsInfo\RavenDB\certs\installerCertPassword.txt"
-    if (!(Test-Path $certPasswordPath)) 
+    if (!(Test-Path $certPasswordPath))
     {
         throw "Could not find the path for the certificate password of the installer"
     }
-    
+
     $certPassword = Get-Content $certPasswordPath
-    if ($certPassword -eq $null) 
+    if ($certPassword -eq $null)
     {
         throw "Certificate password must be provided"
     }
-    
+
     Write-Host "Signing the following file: $filePath"
 
     $timeservers = @("http://tsa.starfieldtech.com", "http://timestamp.globalsign.com/scripts/timstamp.dll", "http://timestamp.comodoca.com/authenticode", "http://www.startssl.com/timestamp", "http://timestamp.verisign.com/scripts/timstamp.dll")
@@ -369,6 +373,8 @@ function SignFile($filePath){
             continue
         }
     }
+
+    throw "Could not reach any of the timeservers"
 }
 
 task SignServer {
