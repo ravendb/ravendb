eed3462019f8cd59862f172b11bed41308c4a602
Arkadiusz Palinski <arek@ravendb.net>
2021-05-19 11:58:17 +0200

    RavenDB-16589 Adding more detail about in-memory state of a storage environment.

---
+++ src/Voron/Debugging/InMemoryStorageState.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Voron.Debugging
+{
+    public class InMemoryStorageState
+    {
+        public long CurrentReadTransactionId { get; set; }
+
+        public long NextWriteTransactionId { get; set; }
+
+        public long PossibleOldestReadTransaction { get; set; }
+
+        public List<ActiveTransaction> ActiveTransactions { get; set; }
+
+        public FlushStateDetails FlushState { get; set; }
+
+        public SyncStateDetails SyncState { get; set; }
+
+
+        public class FlushStateDetails
+        {
+            public DateTime LastFlushTime { get; set; }
+
+            public long LastFlushedTransactionId { get; set; }
+
+            public long LastFlushedJournalId { get; set; }
+
+            public long LastTransactionIdUsedToReleaseScratches { get; set; }
+
+            public bool ShouldFlush { get; set; }
+
+            public List<long> JournalsToDelete { get; set; }
+        }
+
+        public class SyncStateDetails
+        {
+            public DateTime LastSyncTime { get; set; }
+
+            public long TotalWrittenButUnsyncedBytes { get; set; }
+
+            public bool ShouldSync { get; set; }
+
+        }
+    }
+
+}
