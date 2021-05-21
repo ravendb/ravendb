e5b70dad372ee4a0f032de0292d362228ba16797
ml054 <marcin@ravendb.net>
2021-05-21 08:21:52 +0200

    RavenDB-16509 Staggered Index - Studio Work

--- src/Raven.Server/Web/Studio/StudioDatabaseTasksHandler.cs
+++ src/Raven.Server/Web/Studio/StudioDatabaseTasksHandler.cs
@@ -1,6 +1,9 @@
 ﻿using System.Threading.Tasks;
+using Raven.Client.Documents.Indexes;
 using Raven.Server.Documents;
 using Raven.Server.Routing;
+using Raven.Server.ServerWide.Context;
+using Sparrow.Json;
 
 namespace Raven.Server.Web.Studio
 {
@@ -14,6 +17,33 @@ namespace Raven.Server.Web.Studio
             var path = GetStringQueryString("path", required: false);
 
             return StudioTasksHandler.GetFolderPathOptionsInternal(ServerStore, type, isBackupFolder, path, RequestBodyStream, ResponseBodyStream);
+        }
+        
+        [RavenAction("/databases/*/studio-tasks/indexes/configuration/defaults", "GET", AuthorizationStatus.ValidUser, EndpointType.Read)]
+        public async Task GetIndexDefaults()
+        {
+            using (ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+            {
+                var autoIndexesDeploymentMode = Database.Configuration.Indexing.AutoIndexDeploymentMode;
+                var staticIndexesDeploymentMode = Database.Configuration.Indexing.StaticIndexDeploymentMode;
+
+                await using (var writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
+                {
+                    writer.WriteStartObject();
+                    writer.WritePropertyName(nameof(IndexDefaults.AutoIndexDeploymentMode));
+                    writer.WriteString(autoIndexesDeploymentMode.ToString());
+                    writer.WriteComma();
+                    writer.WritePropertyName(nameof(IndexDefaults.StaticIndexDeploymentMode));
+                    writer.WriteString(staticIndexesDeploymentMode.ToString());
+                    writer.WriteEndObject();
+                }
+            }
+        }
+
+        public class IndexDefaults
+        {
+            public IndexDeploymentMode AutoIndexDeploymentMode { get; set; }
+            public IndexDeploymentMode StaticIndexDeploymentMode { get; set; }
         }
     }
 }
