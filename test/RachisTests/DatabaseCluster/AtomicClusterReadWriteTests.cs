e9b74aed4e9a1723feb1b20121a4014cc9263376
Igal Merhavia <igal@ravendb.net>
2021-05-27 15:37:48 +0300

    RavenDB-16614 Test for "Atomic read write in cluster wide transaction without conflicts"

---
+++ test/RachisTests/DatabaseCluster/AtomicClusterReadWriteTests.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FastTests.Server.Replication;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Conventions;
+using Raven.Client.Documents.Operations.CompareExchange;
+using Raven.Client.Documents.Session;
+using Raven.Client.Documents.Smuggler;
+using Raven.Client.Exceptions;
+using Raven.Client.Util;
+using Raven.Server;
+using Raven.Tests.Core.Utils.Entities;
+using Sparrow.Server;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace RachisTests.DatabaseCluster
+{
+    public class AtomicClusterReadWriteTests : ReplicationTestBase
+    {
+        public AtomicClusterReadWriteTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        class TestObj
+        {
+            public string Id { get; set; }
+            public string Prop { get; set; }
+        }
+
+        [Fact]
+        public async Task ClusterWideTransaction_WhenStore_ShouldCreateCompareExchange()
+        {
+            var (nodes, leader) = await CreateRaftCluster(3);
+            using var store = GetDocumentStore(new Options {Server = leader, ReplicationFactor = nodes.Count});
+
+            var entity = new TestObj();
+            using (var session = store.OpenAsyncSession(new SessionOptions { TransactionMode = TransactionMode.ClusterWide }))
+            {
+                await session.StoreAsync(entity);
+                await session.SaveChangesAsync();
+            }
+
+            var result = await store.Operations.SendAsync(new GetCompareExchangeValuesOperation<User>(""));
+            Assert.Single(result);
+            Assert.EndsWith(entity.Id, result.Single().Key, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public async Task ClusterWideTransaction_WhenDisableAndStore_ShouldNotCreateCompareExchange()
+        {
+            var (nodes, leader) = await CreateRaftCluster(3);
+            using var store = GetDocumentStore(new Options {Server = leader, ReplicationFactor = nodes.Count});
+
+            var entity = new TestObj();
+            using (var session = store.OpenAsyncSession(new SessionOptions { TransactionMode = TransactionMode.ClusterWide, DisableAtomicDocumentWritesInClusterWideTransaction = true}))
+            {
+                await session.StoreAsync(entity);
+                await session.SaveChangesAsync();
+            }
+
+            var result = await store.Operations.SendAsync(new GetCompareExchangeValuesOperation<User>(""));
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public async Task ClusterWideTransaction_WhenLoadAndUpdateInParallel_ShouldSucceedOnlyInTheFirst()
+        {
+            var (nodes, leader) = await CreateRaftCluster(3);
+            using var documentStore = GetDocumentStore(new Options {Server = leader, ReplicationFactor = nodes.Count});
+
+            using var disposable = LocalGetDocumentStores(nodes, documentStore.Database, out var stores);
+
+            var entity = new TestObj();
+            using (var session = documentStore.OpenAsyncSession(new SessionOptions { TransactionMode = TransactionMode.ClusterWide }))
+            {
+                await session.StoreAsync(entity);
+                await session.SaveChangesAsync();
+            }
+            await WaitForDocumentInClusterAsync<TestObj>(documentStore.GetRequestExecutor().Topology.Nodes, entity.Id, u => u != null, TimeSpan.FromSeconds(10));
+
+            var barrier = new Barrier(3);
+            var exceptions = new ConcurrentBag<Exception>();
+            var tasks = Enumerable.Range(0, stores.Length)
+                .Select(i => Task.Run(async () =>
+                {
+                    using var session = stores[i].OpenAsyncSession(new SessionOptions {TransactionMode = TransactionMode.ClusterWide});
+                    var loaded = await session.LoadAsync<TestObj>(entity.Id);
+                    barrier.SignalAndWait();
+
+                    loaded.Prop = "Change" + i;
+
+                    try
+                    {
+                        await session.SaveChangesAsync();
+                    }
+                    catch (Exception e)
+                    {
+                        exceptions.Add(e);
+                    }
+                }));
+
+            await Task.WhenAll(tasks);
+            Assert.Equal(2, exceptions.Count);
+            foreach (var exception in exceptions)
+            {
+                Assert.IsType<ConcurrencyException>(exception);
+            }
+        }
+
+        [Fact]
+        public async Task ClusterWideTransaction_WhenLoadAndDeleteWhileUpdated_ShouldSucceedOnlyInTheFirst()
+        {
+            var (nodes, leader) = await CreateRaftCluster(3, shouldRunInMemory: false);
+
+            using var documentStore = GetDocumentStore(new Options {Server = leader, ReplicationFactor = nodes.Count});
+
+            var entity = new TestObj();
+            using (var session = documentStore.OpenAsyncSession(new SessionOptions { TransactionMode = TransactionMode.ClusterWide }))
+            {
+                await session.StoreAsync(entity);
+                await session.SaveChangesAsync();
+            }
+
+            await LoadAndDeleteWhileUpdated(nodes, documentStore.Database, entity.Id);
+        }
+
+        [Fact]
+        public async Task ClusterWideTransaction_WhenImportThenLoadAndDeleteWhileUpdated_ShouldSucceedOnlyInTheFirst()
+        {
+            var (nodes, leader) = await CreateRaftCluster(3);
+            using var documentStore = GetDocumentStore(new Options { Server = leader, ReplicationFactor = nodes.Count });
+
+            var entity = new TestObj();
+            using (var source = GetDocumentStore())
+            {
+                using var session = source.OpenAsyncSession(new SessionOptions { TransactionMode = TransactionMode.ClusterWide });
+                await session.StoreAsync(entity);
+                await session.SaveChangesAsync();
+
+                var operation = await source.Smuggler.ExportAsync(new DatabaseSmugglerExportOptions(), documentStore.Smuggler);
+                await operation.WaitForCompletionAsync(TimeSpan.FromMinutes(1));
+            }
+
+            await LoadAndDeleteWhileUpdated(nodes, documentStore.Database, entity.Id);
+        }
+
+        private static async Task LoadAndDeleteWhileUpdated(List<RavenServer> nodes, string database, string entityId)
+        {
+            using var disposable = LocalGetDocumentStores(nodes, database, out var stores);
+
+            var amre = new AsyncManualResetEvent();
+            var amre2 = new AsyncManualResetEvent();
+            var task = Task.Run(async () =>
+            {
+                using var session = stores[0].OpenAsyncSession(new SessionOptions {TransactionMode = TransactionMode.ClusterWide});
+                var loaded = await session.LoadAsync<TestObj>(entityId);
+                amre.Set();
+
+                session.Delete(loaded);
+
+                await amre2.WaitAsync();
+                await Assert.ThrowsAnyAsync<ConcurrencyException>(() => session.SaveChangesAsync());
+            });
+            await amre.WaitAsync();
+            using (var session = stores[1].OpenAsyncSession(new SessionOptions {TransactionMode = TransactionMode.ClusterWide}))
+            {
+                var loaded = await session.LoadAsync<TestObj>(entityId);
+                loaded.Prop = "Changed";
+                await session.SaveChangesAsync();
+                amre2.Set();
+            }
+
+            await task;
+        }
+
+        [Fact]
+        public async Task ClusterWideTransaction_WhenLoadAndUpdateWhileDeleted_ShouldSucceedOnlyInTheFirst()
+        {
+            var (nodes, leader) = await CreateRaftCluster(3, shouldRunInMemory: false);
+
+            using var documentStore = GetDocumentStore(new Options {Server = leader, ReplicationFactor = nodes.Count});
+
+            var entity = new TestObj();
+            using (var session = documentStore.OpenAsyncSession(new SessionOptions { TransactionMode = TransactionMode.ClusterWide }))
+            {
+                await session.StoreAsync(entity);
+                await session.SaveChangesAsync();
+            }
+
+            await LoadAndUpdateWhileDeleted(nodes, documentStore.Database, entity.Id);
+        }
+        
+        [Fact]
+        public async Task ClusterWideTransaction_WhenImportThenLoadAndUpdateWhileDeleted_ShouldSucceedOnlyInTheFirst()
+        {
+            var (nodes, leader) = await CreateRaftCluster(3);
+            using var documentStore = GetDocumentStore(new Options { Server = leader, ReplicationFactor = nodes.Count });
+
+            var entity = new TestObj();
+            using (var source = GetDocumentStore())
+            {
+                using var session = source.OpenAsyncSession(new SessionOptions { TransactionMode = TransactionMode.ClusterWide });
+                await session.StoreAsync(entity);
+                await session.SaveChangesAsync();
+
+                var operation = await source.Smuggler.ExportAsync(new DatabaseSmugglerExportOptions(), documentStore.Smuggler);
+                await operation.WaitForCompletionAsync(TimeSpan.FromMinutes(1));
+                WaitForUserToContinueTheTest(source);
+            }
+
+            await LoadAndUpdateWhileDeleted(nodes, documentStore.Database, entity.Id);
+        }
+
+        private static async Task LoadAndUpdateWhileDeleted(List<RavenServer> nodes, string database, string entityId)
+        {
+            using var disposable = LocalGetDocumentStores(nodes, database, out var stores);
+
+            var amre = new AsyncManualResetEvent();
+            var amre2 = new AsyncManualResetEvent();
+            var task = Task.Run(async () =>
+            {
+                using var session = stores[0].OpenAsyncSession(new SessionOptions {TransactionMode = TransactionMode.ClusterWide});
+                var loaded = await session.LoadAsync<TestObj>(entityId);
+                amre.Set();
+
+                loaded.Prop = "Changed";
+
+                await amre2.WaitAsync();
+                await Assert.ThrowsAnyAsync<ConcurrencyException>(() => session.SaveChangesAsync());
+            });
+            await amre.WaitAsync();
+            using (var session = stores[1].OpenAsyncSession(new SessionOptions {TransactionMode = TransactionMode.ClusterWide}))
+            {
+                var loaded = await session.LoadAsync<TestObj>(entityId);
+                session.Delete(loaded);
+                await session.SaveChangesAsync();
+
+                amre2.Set();
+            }
+
+            await task;
+        }
+
+
+        private static IDisposable LocalGetDocumentStores(List<RavenServer> nodes, string database, out IDocumentStore[] stores)
+        {
+            stores = new IDocumentStore[nodes.Count];
+            var internalStore = stores;
+            var disposable = new DisposableAction(() =>
+            {
+                foreach (var s in internalStore)
+                {
+                    try
+                    {
+                        s?.Dispose();
+                    }
+                    catch
+                    {
+                        // ignored
+                    }
+                }
+            });
+
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                var store = new DocumentStore
+                {
+                    Urls = new[] {nodes[i].WebUrl}, Database = database, Conventions = new DocumentConventions {DisableTopologyUpdates = true}
+                }.Initialize();
+                stores[i] = store;
+            }
+
+            return disposable;
+        }
+    }
+}
