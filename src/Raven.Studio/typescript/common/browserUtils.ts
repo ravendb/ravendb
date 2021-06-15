eb10b38018c681500b76f665ae37694c37319e9c
Danielle9897 <danielle.9897@gmail.com>
2021-06-15 10:30:05 +0300

    RavenDB-14301 Show unsupported browser warning for IE in setup wizard

---
+++ src/Raven.Studio/typescript/common/browserUtils.ts
@@ -0,0 +1,13 @@
+﻿/// <reference path="../../typings/tsd.d.ts" />
+
+class browserUtils {
+
+    static isBrowserSupported(): boolean {
+        const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+        const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+        
+        return isChrome || isFirefox;
+    }
+} 
+
+export = browserUtils;
