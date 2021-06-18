b5217f757c9da6e0541ea59af2fb259d1570dade
Pawel Pekrol <ppekrol@ravendb.net>
2021-06-18 11:19:24 +0200

    RavenDB-16895 after license is activate the actual update is happening on LicenseManager.ReloadLicense which is executed by ServerStore.OnValueChanged - we need to wait for that raft index to be applied to avoid race condition between activating a license and e.g. creating a database which requires activated (and loaded) license

--- src/Raven.Server/Program.cs
+++ src/Raven.Server/Program.cs
@@ -343,7 +343,7 @@ namespace Raven.Server
                         var localLicenseStatus = LicenseManager.GetLicenseStatus(localLicense);
                         if (localLicenseStatus.Expiration >= RavenVersionAttribute.Instance.ReleaseDate)
                         {
-                            serverStore.LicenseManager.OnBeforeInitialize += () => serverStore.LicenseManager.TryActivateLicense(throwOnActivationFailure: false);
+                            serverStore.LicenseManager.OnBeforeInitialize += () => serverStore.LicenseManager.TryActivateLicense(throwOnActivationFailure: serverStore.Server.ThrowOnLicenseActivationFailure);
                             return;
                         }
 
---
+++ test/SlowTests/Issues/RavenDB_16895.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using FastTests;
+using Tests.Infrastructure;
+using Xunit.Abstractions;
+using Xunit;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_16895 : RavenTestBase
+    {
+        public RavenDB_16895(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [LicenseRequiredFact]
+        public void Should_Wait_For_License_To_Be_Reloaded_After_Activation()
+        {
+            DoNotReuseServer();
+
+            Assert.Contains("AGPL", Server.ServerStore.LicenseManager.LicenseStatus.Status);
+
+            Server.ServerStore.ForTestingPurposesOnly().BeforePutLicenseCommandHandledInOnValueChanged += () => Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            Server.ServerStore.EnsureNotPassive(skipLicenseActivation: true);
+            Server.ServerStore.LicenseManager.TryActivateLicense(throwOnActivationFailure: true);
+
+            Assert.Contains("Commercial", Server.ServerStore.LicenseManager.LicenseStatus.Status);
+        }
+    }
+}
