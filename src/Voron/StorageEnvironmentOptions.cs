bac592211bc9b746997432b3b83404b845b8d07f
Arkadiusz Palinski <arek@ravendb.net>
2021-06-21 11:54:24 +0200

    RavenDB-16860 Added Storage.MaxNumberOfRecyclableJournals configuration option (default: 32 files, so that will give us 8 GB of recyclable journals in total per storage environment)

--- src/Voron/StorageEnvironmentOptions.cs
+++ src/Voron/StorageEnvironmentOptions.cs
@@ -528,6 +528,8 @@ namespace Voron
             public override void TryStoreJournalForReuse(VoronPathSetting filename)
             {
                 var reusedCount = 0;
+                var reusedLimit = Math.Min(_lastReusedJournalCountOnSync, MaxNumberOfRecyclableJournals);
+
                 try
                 {
                     var oldFileName = Path.GetFileName(filename.FullPath);
@@ -542,7 +544,7 @@ namespace Voron
                     {
                         reusedCount = _journalsForReuse.Count;
 
-                        if (reusedCount > _lastReusedJournalCountOnSync)
+                        if (ShouldRemoveJournal())
                         {
                             if (File.Exists(newName))
                                 File.Delete(newName);
@@ -561,7 +563,7 @@ namespace Voron
                 catch (Exception ex)
                 {
                     if (_log.IsInfoEnabled)
-                        _log.Info(reusedCount > _lastReusedJournalCountOnSync ? "Can't remove" : "Can't store" + " journal for reuse : " + filename, ex);
+                        _log.Info(ShouldRemoveJournal() ? "Can't remove" : "Can't store" + " journal for reuse : " + filename, ex);
                     try
                     {
                         if (File.Exists(filename.FullPath))
@@ -571,6 +573,11 @@ namespace Voron
                     {
                         // nothing we can do about it
                     }
+                }
+
+                bool ShouldRemoveJournal()
+                {
+                    return reusedCount >= reusedLimit;
                 }
             }
 
@@ -1224,6 +1231,8 @@ namespace Voron
         public bool? IgnoreInvalidJournalErrors { get; set; }
         public bool IgnoreDataIntegrityErrorsOfAlreadySyncedTransactions { get; set; }
         public bool SkipChecksumValidationOnDatabaseLoading { get; set; }
+
+        public int MaxNumberOfRecyclableJournals { get; set; } = 32;
 
         private readonly Logger _log;
 
