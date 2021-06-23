88c497fe9853e24f3966324ca2b16119f94f3578
Federico Lois <federico.lois@gmail.com>
2021-06-23 18:16:53 -0300

    RavenDB-11275 - Moving Corax to use containers, set and compact trees

--- src/Corax/IndexWriter.cs
+++ src/Corax/IndexWriter.cs
@@ -1,12 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using Sparrow.Json;
+using System.Runtime.InteropServices;
 using Sparrow.Server;
 using Voron;
 using Voron.Data.BTrees;
-using Voron.Data.Fixed;
-using Voron.Data.Tables;
+using Voron.Data.CompactTrees;
+using Voron.Data.Containers;
+using Voron.Data.Sets;
 using Voron.Impl;
 
 namespace Corax
@@ -18,52 +19,13 @@ namespace Corax
 
         private readonly Transaction _transaction;
 
-        public static readonly Slice IndexEntriesSlice;
-        private static readonly Slice IndexEntriesByNameSlice;
-        private static readonly Slice TagsByNameSlice;
-
-        private static readonly Slice NullSlice;
-
-        public static readonly TableSchema IndexEntriesSchema;
-
-        private readonly Table _entries;
-
-
-        public enum IndexEntriesTable
-        {
-            DocumentId = 0,
-            Entry = 1,
-        }
+        private static readonly Slice ContainerIdSlice;
 
         static IndexWriter()
         {
             using (StorageEnvironment.GetStaticContext(out var ctx))
             {
-                Slice.From(ctx, "IndexEntries", ByteStringType.Immutable, out IndexEntriesSlice);
-                Slice.From(ctx, "IndexEntriesByName", ByteStringType.Immutable, out IndexEntriesByNameSlice);
-                Slice.From(ctx, "Tags", ByteStringType.Immutable, out TagsByNameSlice);
-                Slice.From(ctx, "<<RDB__NULL>>", ByteStringType.Immutable, out NullSlice);
-
-                IndexEntriesSchema = new()
-                {
-                    TableType = (byte)IndexingTableType.IndexEntries,
-                    Compressed = true,
-                    CompressedEtagSourceIndex = new TableSchema.FixedSizeSchemaIndexDef
-                    {
-                        Name = TagsByNameSlice,
-                        IsGlobal = true,
-                        StartIndex = 1,
-                    }
-                };
-
-                IndexEntriesSchema.DefineIndex(new TableSchema.SchemaIndexDef
-                {
-                    StartIndex = (int)IndexEntriesTable.DocumentId,
-                    Count = 1,
-                    Name = IndexEntriesByNameSlice,
-                    Type = TableIndexType.BTree,
-                    IsGlobal = false,
-                });
+                Slice.From(ctx, "ContainerId", ByteStringType.Immutable, out ContainerIdSlice);
             }
         }
 
@@ -74,26 +36,24 @@ namespace Corax
         {
             _environment = environment;
             _transactionPersistentContext = new TransactionPersistentContext(true);
+            _transaction = _environment.WriteTransaction(_transactionPersistentContext);            
 
-            _transaction = _environment.WriteTransaction(_transactionPersistentContext);
-            _entries = _transaction.OpenTable(IndexEntriesSchema, IndexEntriesSlice);
-            if (_entries != null) return;
-            IndexEntriesSchema.Create(_transaction, IndexEntriesSlice, 8);
-
-            _entries = _transaction.OpenTable(IndexEntriesSchema, IndexEntriesSlice);
-            _entries.CORAX_DEBUG_MOVE_DATA += () =>
-                throw new NotSupportedException("Data move are not supported for index entries");
+            var exists = _transaction.LowLevelTransaction.RootObjects.Read(ContainerIdSlice);
+            if (exists == null)
+            {
+                _containerId = Container.Create(_transaction.LowLevelTransaction);
+                _transaction.LowLevelTransaction.RootObjects.Add(ContainerIdSlice, _containerId);
+            }
+            else
+            {
+                _containerId = exists.Reader.ReadLittleEndianInt64();
+            }
         }
-
-        //public long Index(string id, BlittableJsonReaderObject item)
-        //{
-        //    using var _ = Slice.From(_transaction.Allocator, id, out var idSlice);
-        //    return Index(idSlice, item);
-        //}
 
         private readonly SortedList<Slice, SortedList<Slice, SortedList<long, long>>> _buffer =
             new SortedList<Slice, SortedList<Slice, SortedList<long, long>>>(SliceComparer.Instance);
 
+        private readonly long _containerId;
 
         public long Index(string id, Span<byte> data, Dictionary<Slice, int> knownFields)
         {
@@ -103,42 +63,30 @@ namespace Corax
 
         public long Index(Slice id, Span<byte> data, Dictionary<Slice, int> knownFields)
         {
-            using (_entries.Allocate(out var builder))
+            Span<byte> buf = stackalloc byte[10];
+            var idLen = ZigZag.Encode(buf, id.Size);
+            var entryId = Container.Allocate(_transaction.LowLevelTransaction, _containerId, idLen + id.Size + data.Length, out var space);
+            buf.Slice(0, idLen).CopyTo(space);
+            space = space.Slice(idLen);
+            id.CopyTo(space);
+            space = space.Slice(id.Size);
+            data.CopyTo(space);
+
+            var context = _transaction.Allocator;
+            var entryReader = new IndexEntryReader(data);
+            entryReader.DebugDump(knownFields);
+
+            foreach (var (key, tokenField) in knownFields)
             {
-                builder.Add(id);
-                builder.Add(data);
-                long entryId = _entries.Insert(builder);
-
-                var context = _transaction.Allocator;
-                var entryReader = new IndexEntryReader(data);
-                entryReader.DebugDump(knownFields);
-
-                foreach (var (key, tokenField) in knownFields)
+                if (_buffer.TryGetValue(key, out var field) == false)
                 {
-                    if (_buffer.TryGetValue(key, out var field) == false)
-                    {
-                        _buffer[key] = field = new SortedList<Slice, SortedList<long, long>>(SliceComparer.Instance);
-                    }
-
-                    InsertToken(context, ref entryReader, tokenField, field, entryId );
+                    _buffer[key] = field = new SortedList<Slice, SortedList<long, long>>(SliceComparer.Instance);
                 }
 
-                //BlittableJsonReaderObject.PropertyDetails prop = default;
-                //for (int i = 0; i < item.Count; i++)
-                //{
-                //    item.GetPropertyByIndex(i, ref prop, addObjectToCache: false);
-
-                //    var key = prop.Name.ToString();
-                //    if (_buffer.TryGetValue(key, out var field) == false)
-                //    {
-                //        _buffer[key] = field = new SortedList<string, SortedList<long, long>>();
-                //    }
-                //    InsertToken(field, prop.Value, prop.Token, entryId);
-                //}
-
-                return entryId;
+                InsertToken(context, ref entryReader, tokenField, field, entryId);
             }
 
+            return entryId;
         }
 
         private void InsertToken(ByteStringContext context, ref IndexEntryReader entryReader, int tokenField, SortedList<Slice, SortedList<long, long>> field, long entryId)
@@ -192,9 +140,11 @@ namespace Corax
             }
             else if (fieldType.HasFlag(IndexEntryFieldType.Invalid))
             {
-                if (field.TryGetValue(NullSlice, out var term) == false)
-                    field[NullSlice] = term = new SortedList<long, long>();
-                term[entryId] = entryId;
+                // if (field.TryGetValue(NullSlice, out var term) == false)
+                //     field[NullSlice] = term = new SortedList<long, long>();
+                // term[entryId] = entryId;
+                //TODO:
+                throw new NotSupportedException("Don't like this behavior.. (ayende)");
             }
             else
             {
@@ -210,89 +160,101 @@ namespace Corax
             }
         }
 
-        //public unsafe long Index(Slice id, BlittableJsonReaderObject item)
-        //{
-        //    using (_entries.Allocate(out var builder))
-        //    {
-        //        builder.Add(id);
-        //        builder.Add(item.BasePointer, item.Size);
-        //        long entryId = _entries.Insert(builder);
-        //        BlittableJsonReaderObject.PropertyDetails prop = default;
-        //        for (int i = 0; i < item.Count; i++)
-        //        {
-        //            item.GetPropertyByIndex(i, ref prop, addObjectToCache: false);
-        //            var key = prop.Name.ToString();
-        //            if (_buffer.TryGetValue(key, out var field) == false)
-        //            {
-        //                _buffer[key] = field = new SortedList<string, SortedList<long, long>>();
-        //            }
-        //            InsertToken(field, prop.Value, prop.Token, entryId);
-        //        }
-        //        return entryId;
-        //    }
-        //}
-
-        private static void InsertToken(SortedList<string, SortedList<long, long>> field, object val, BlittableJsonToken token, long entryId)
+        public unsafe void Commit()
         {
-            switch (token & BlittableJsonReaderBase.TypesMask)
-            {
-                case BlittableJsonToken.StartArray:
-                    var array = (BlittableJsonReaderArray)val;
-                    for (int i = 0; i < array.Length; i++)
-                    {
-                        (object nested, BlittableJsonToken nestedToken) = array.GetValueTokenTupleByIndex(i);
-                        InsertToken(field, nested, nestedToken, entryId);
-                    }
-                    break;
-                case BlittableJsonToken.Integer:
-                case BlittableJsonToken.LazyNumber:
-                case BlittableJsonToken.String:
-                case BlittableJsonToken.CompressedString:
-                case BlittableJsonToken.Boolean:
-                {
-                    var str = val.ToString(); // TODO: fixme
-                    if (str.Length > 512)
-                        break;
-
-                    if (field.TryGetValue(str, out var term) == false)
-                    {
-                        field[str] = term = new SortedList<long, long>();
-                    }
-
-                    term[entryId] = entryId;
-                    break;
-                }
-                case BlittableJsonToken.Null:
-                {
-                    var str = "<<NULL_VALUE>>"; // todo: fixme
-                    if (field.TryGetValue(str, out var term) == false)
-                    {
-                        field[str] = term = new SortedList<long, long>();
-                    }
-                    term[entryId] = entryId;
-
-                    break;
-                }
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-
-        public void Commit()
-        {
+            using var _ = _transaction.Allocator.Allocate(Container.MaxSizeInsideContainerPage, out Span<byte> tmpBuf);
+            Tree fieldsTree = _transaction.CreateTree("Fields");
             foreach (var (field, terms) in _buffer)
             {
-                var tree = _transaction.CreateTree(field);
+                var fieldTree = fieldsTree.CompactTreeFor(field);
+                var llt = _transaction.LowLevelTransaction;
                 foreach (var (term, entries) in terms)
                 {
-                    var fixedSizeTree = tree.FixedTreeFor(term);
-                    foreach (long entry in entries.Keys)
+                    ReadOnlySpan<byte> termsSpan = term.AsSpan();
+                    if (fieldTree.TryGetValue(termsSpan, out var existing) == false)
                     {
-                        fixedSizeTree.Add(entry);
+                        AddNewTerm(entries, fieldTree, termsSpan, tmpBuf);
+                        continue;
+                    }
+
+                    if ((existing & (long)TermIdMask.Set) != 0)
+                    {
+                        var id = existing & ~0b11;
+                        var setSpace = Container.GetMutable(llt, id);
+                        ref var setState = ref MemoryMarshal.AsRef<SetState>(setSpace);
+                        var set = new Set(llt, Slices.Empty, setState);
+                        set.Add(entries);
+                        setState = set.State;
+                    }
+                    else if ((existing & (long)TermIdMask.Small) != 0)
+                    {
+                        var id = existing & ~0b11;
+                        var smallSet = Container.Get(llt, id);
+                        // combine with existing value
+                        while (smallSet.IsEmpty == false)
+                        {
+                            var value = ZigZag.Decode(smallSet, out var len);
+                            entries[value] = value;
+                            smallSet = smallSet.Slice(len);
+                        }
+                        Container.Delete(llt, _containerId, id);
+                        AddNewTerm(entries, fieldTree, termsSpan, tmpBuf);
+                    }
+                    else // single
+                    {
+                        entries[existing] = existing;
+                        AddNewTerm(entries, fieldTree, termsSpan, tmpBuf);
                     }
                 }
             }
             _transaction.Commit();
+        }
+
+        // container ids are guaranteed to be aligned on 
+        // 4 bytes boundary, we're using this to store metadata
+        // about the data
+        private enum TermIdMask : long
+        {
+            Single = 0,
+            Small = 1,
+            Set = 2
+        }
+        
+        private unsafe void AddNewTerm(SortedList<long, long> entries, CompactTree fieldTree, ReadOnlySpan<byte> termsSpan, Span<byte> tmpBuf)
+        {
+            // common for unique values (guid, date, etc)
+            if (entries.Count == 1) 
+            {
+                // just a single entry, store the value inline
+                fieldTree.Add(termsSpan, entries.Keys[0] | (long)TermIdMask.Single);
+                return;
+            }
+
+            // try to insert to container value
+            //TODO: using simplest delta encoding, need to do better here
+            int pos = ZigZag.Encode(tmpBuf, entries.Keys[0]);
+            var llt = _transaction.LowLevelTransaction;
+            for (int i = 1; i < entries.Count; i++)
+            {
+                if (pos + 10 >= tmpBuf.Length)
+                {
+                    pos += ZigZag.Encode(tmpBuf.Slice(pos), entries.Keys[i] - entries.Keys[i - 1]);
+                    continue;
+                }
+                // too big, convert to a set
+                var setId = Container.Allocate(llt, _containerId, sizeof(SetState), out var setSpace);
+                ref var setState = ref MemoryMarshal.AsRef<SetState>(setSpace);
+                Set.Initialize(llt, ref setState);
+                var set = new Set(llt, Slices.Empty, setState);
+                set.Add(entries);
+                setState = set.State;
+                fieldTree.Add(termsSpan, setId | (long)TermIdMask.Set);
+                return;
+            }
+
+            var termId = Container.Allocate(llt, _containerId, pos, out var space);
+            tmpBuf.Slice(0, pos).CopyTo(space);
+            fieldTree.Add(termsSpan, termId | (long)TermIdMask.Small);
         }
 
         public void Dispose()
