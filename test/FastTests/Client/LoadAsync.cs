f8d72c69977377d4e7574029bf577c34105182b3
omer <omer@ravendb.net>
2021-06-23 21:54:50 +0300

    RavenDB-16836: Fixed - Change tests location, LoasAsync

--- test/FastTests/Client/LoadAsync.cs
+++ test/FastTests/Client/LoadAsync.cs
@@ -10,7 +10,45 @@ namespace FastTests.Client
         public LoadAsync(ITestOutputHelper output) : base(output)
         {
         }
+        
+        [Fact]
+        public void Load_Document_And_Expect_Null_User()
+        {
+            using var store = GetDocumentStore();
 
+            using var session = store.OpenSession();
+
+            string nullId = null;
+            var user1 = session.Load<User>(nullId);
+            Assert.Null(user1);
+
+            var user2 = session.Load<User>("");
+            Assert.Null(user2);
+            
+            var user3 = session.Load<User>(" ");
+            Assert.Null(user3);
+        }
+        
+        [Fact]
+        public async Task Load_Document_And_Expect_Null_User_Async()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenAsyncSession())
+                {
+                    string nullId = null;
+                    var user1 =await session.LoadAsync<User>(nullId);
+                    Assert.Null(user1);
+                    
+                    var user2 = await session.LoadAsync<User>("");
+                    Assert.Null(user2);
+            
+                    var user3 = await session.LoadAsync<User>(" ").ConfigureAwait(false);
+                    Assert.Null(user3);
+                }
+            }
+        }
+        
         [Fact]
         public async Task Load_Document_By_id_Async()
         {
