b294d55c53278c2b3c8e099ca0167c4cfe799021
Stav <stav@ravendb.net>
2021-06-30 09:42:29 +0300

    RavenDB-16879 Fix select value from collection in index using FirstOrDefault

--- src/Raven.Server/Documents/Indexes/Static/Linq/DynamicEnumerable.cs
+++ src/Raven.Server/Documents/Indexes/Static/Linq/DynamicEnumerable.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -11,119 +12,119 @@ namespace Raven.Server.Documents.Indexes.Static.Linq
             return new DynamicArray(((IEnumerable<object>)source).Union((IEnumerable<object>)other));
         }
 
-        public static dynamic First<TSource>(IEnumerable<TSource> source)
+        public static dynamic First(IEnumerable source)
         {
             return FirstOrDefault(source);
         }
 
-        public static dynamic First<TSource>(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        public static dynamic First(IEnumerable source, Func<dynamic, bool> predicate)
         {
             return FirstOrDefault(source, predicate);
         }
 
-        public static dynamic FirstOrDefault<TSource>(IEnumerable<TSource> source)
+        public static dynamic FirstOrDefault(IEnumerable source)
         {
             if (source == null) return DynamicNullObject.Null;
 
-            var result = source.FirstOrDefault();
+            var result = source.Cast<object>().FirstOrDefault();
             if (ReferenceEquals(result, null))
                 return DynamicNullObject.Null;
             return result;
         }
 
-        public static dynamic FirstOrDefault<TSource>(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        public static dynamic FirstOrDefault(IEnumerable source, Func<dynamic, bool> predicate)
         {
             if (source == null) return DynamicNullObject.Null;
             if (predicate == null) return DynamicNullObject.Null;
 
-            var result = source.FirstOrDefault(predicate);
+            var result = source.Cast<dynamic>().FirstOrDefault(predicate);
             if (ReferenceEquals(result, null))
                 return DynamicNullObject.Null;
             return result;
         }
 
-        public static dynamic Last<TSource>(IEnumerable<TSource> source)
+        public static dynamic Last(IEnumerable source)
         {
             return LastOrDefault(source);
 
         }
 
-        public static dynamic Last<TSource>(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        public static dynamic Last(IEnumerable source, Func<dynamic, bool> predicate)
         {
             return LastOrDefault(source, predicate);
         }
 
-        public static dynamic LastOrDefault<TSource>(IEnumerable<TSource> source)
+        public static dynamic LastOrDefault(IEnumerable source)
         {
             if (source == null) return DynamicNullObject.Null;
 
-            var result = source.LastOrDefault();
+            var result = source.Cast<object>().LastOrDefault();
             if (ReferenceEquals(result, null))
                 return DynamicNullObject.Null;
             return result;
         }
 
-        public static dynamic LastOrDefault<TSource>(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        public static dynamic LastOrDefault(IEnumerable source, Func<dynamic, bool> predicate)
         {
             if (source == null) return DynamicNullObject.Null;
             if (predicate == null) return DynamicNullObject.Null;
 
-            var result = source.LastOrDefault(predicate);
+            var result = source.Cast<dynamic>().LastOrDefault(predicate);
             if (ReferenceEquals(result, null))
                 return DynamicNullObject.Null;
             return result;
         }
 
-        public static dynamic Single<TSource>(IEnumerable<TSource> source)
+        public static dynamic Single(IEnumerable source)
         {
             return SingleOrDefault(source);
         }
 
-        public static dynamic Single<TSource>(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        public static dynamic Single(IEnumerable source, Func<dynamic, bool> predicate)
         {
             return SingleOrDefault(source, predicate);
         }
 
-        public static dynamic SingleOrDefault<TSource>(IEnumerable<TSource> source)
+        public static dynamic SingleOrDefault(IEnumerable source)
         {
             if (source == null) return DynamicNullObject.Null;
 
-            var result = source.SingleOrDefault();
+            var result = source.Cast<object>().SingleOrDefault();
             if (ReferenceEquals(result, null))
                 return DynamicNullObject.Null;
             return result;
         }
 
-        public static dynamic SingleOrDefault<TSource>(IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        public static dynamic SingleOrDefault(IEnumerable source, Func<dynamic, bool> predicate)
         {
             if (source == null) return DynamicNullObject.Null;
             if (predicate == null) return DynamicNullObject.Null;
 
-            var result = source.SingleOrDefault(predicate);
+            var result = source.Cast<dynamic>().SingleOrDefault(predicate);
             if (ReferenceEquals(result, null))
                 return DynamicNullObject.Null;
             return result;
         }
 
-        public static dynamic ElementAt<TSource>(IEnumerable<TSource> source, int index)
+        public static dynamic ElementAt(IEnumerable source, int index)
         {
             return ElementAtOrDefault(source, index);
 
         }
 
-        public static dynamic ElementAtOrDefault<TSource>(IEnumerable<TSource> source, int index)
+        public static dynamic ElementAtOrDefault(IEnumerable source, int index)
         {
             if (source == null) return DynamicNullObject.Null;
             if (index < 0) return DynamicNullObject.Null;
 
-            var result = source.ElementAtOrDefault(index);
+            var result = source.Cast<dynamic>().ElementAtOrDefault(index);
             if (ReferenceEquals(result, null))
                 return DynamicNullObject.Null;
             return result;
         }
 
 
-        private static IEnumerable<T> Yield<T>(IEnumerator<T> enumerator)
+        private static IEnumerable<dynamic> Yield(IEnumerator enumerator)
         {
             do
             {
@@ -131,7 +132,7 @@ namespace Raven.Server.Documents.Indexes.Static.Linq
             } while (enumerator.MoveNext());
         }
 
-        public static dynamic Min<TSource>(IEnumerable<TSource> source)
+        public static dynamic Min(IEnumerable source)
         {
             if (source == null) return DynamicNullObject.Null;
 
@@ -145,12 +146,12 @@ namespace Raven.Server.Documents.Indexes.Static.Linq
             return result;
         }
 
-        public static dynamic Min<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        public static dynamic Min(IEnumerable source, Func<dynamic, dynamic> selector)
         {
-            return Min(Enumerable.Select(source, selector));
+            return Min(Enumerable.Select(source.Cast<dynamic>(), selector));
         }
 
-        public static dynamic Max<TSource>(IEnumerable<TSource> source)
+        public static dynamic Max(IEnumerable source)
         {
             if (source == null) return DynamicNullObject.Null;
 
@@ -164,9 +165,9 @@ namespace Raven.Server.Documents.Indexes.Static.Linq
             return result;
         }
 
-        public static dynamic Max<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        public static dynamic Max(IEnumerable source, Func<dynamic, dynamic> selector)
         {
-            return Max(Enumerable.Select(source, selector));
+            return Max(Enumerable.Select(source.Cast<dynamic>(), selector));
         }
 
         public static IEnumerable<dynamic> Concat(object source, object other)
@@ -179,19 +180,20 @@ namespace Raven.Server.Documents.Indexes.Static.Linq
             return new DynamicArray(((IEnumerable<object>)source).Intersect((IEnumerable<object>)other));
         }
 
-        public static IOrderedEnumerable<dynamic> OrderBy<TSource, TKey>(IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+
+        public static IOrderedEnumerable<dynamic> OrderBy(IEnumerable source, Func<dynamic, dynamic> keySelector)
         {
-            return new DynamicArray(Enumerable.OrderBy(source, keySelector));
+            return new DynamicArray(Enumerable.OrderBy(source.Cast<dynamic>(), keySelector));
         }
 
         public static IOrderedEnumerable<dynamic> OrderBy<TSource, TKey>(IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IComparer<TKey> comparer)
         {
             return new DynamicArray(Enumerable.OrderBy(source, keySelector, comparer));
         }
-
-        public static IOrderedEnumerable<dynamic> OrderByDescending<TSource, TKey>(IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        
+        public static IOrderedEnumerable<dynamic> OrderByDescending(IEnumerable source, Func<dynamic, dynamic> keySelector)
         {
-            return new DynamicArray(Enumerable.OrderByDescending(source, keySelector));
+            return new DynamicArray(Enumerable.OrderByDescending(source.Cast<dynamic>(), keySelector));
         }
 
         public static IOrderedEnumerable<dynamic> OrderByDescending<TSource, TKey>(IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IComparer<TKey> comparer)
