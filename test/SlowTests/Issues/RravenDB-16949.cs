5c3cf3d2e07ac377ac6bda712d76d5c5828e60f3
efrat@ayende.com <efrat@ayende.com>
2021-07-11 13:30:04 +0300

    RavenDB-16949 fix

--- test/SlowTests/Issues/RravenDB-16949.cs
+++ test/SlowTests/Issues/RravenDB-16949.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using FastTests;
 using FastTests.Utils;
 using Raven.Client.Documents.Operations.Revisions;
-using Raven.Server.Config;
+using Raven.Server.ServerWide;
 using SlowTests.Core.Utils.Entities;
 using Xunit;
 using Xunit.Abstractions;
@@ -19,19 +19,7 @@ namespace SlowTests.Issues
         [Fact]
         public async Task limitRevisionDeletion()
         {
-            using (var server = GetNewServer(
-                new ServerCreationOptions
-                {
-                    CustomSettings = new Dictionary<string, string>
-                    {
-                        [RavenConfiguration.GetKey(x => x.Databases.MaxRevisionsToDeleteUponDocumentUpdate)] = 100.ToString()
-                    },
-                    RegisterForDisposal = false
-                }))
-            using (var store = GetDocumentStore(new Options
-            {
-                Server = server
-            }))
+            using (var store = GetDocumentStore())
             {
                 var configuration = new RevisionsConfiguration
                 {
@@ -40,9 +28,8 @@ namespace SlowTests.Issues
                         Disabled = false,
                         MinimumRevisionsToKeep = 1000000
                     },
-
                 };
-                await RevisionsHelper.SetupRevisions(store, server.ServerStore, configuration);
+                await RevisionsHelper.SetupRevisions(store, Server.ServerStore, configuration);
 
                 using (var session = store.OpenAsyncSession())
                 {
@@ -70,11 +57,11 @@ namespace SlowTests.Issues
                     Default = new RevisionsCollectionConfiguration
                     {
                         Disabled = false,
-                        MinimumRevisionsToKeep = 10
+                        MinimumRevisionsToKeep = 10,
+                        MaxRevisionsToDeleteUponDocumentUpdate = 100
                     },
-
                 };
-                await RevisionsHelper.SetupRevisions(store, server.ServerStore, configuration);
+                await RevisionsHelper.SetupRevisions(store, Server.ServerStore, configuration);
 
                 using (var session = store.OpenAsyncSession())
                 {
@@ -97,6 +84,66 @@ namespace SlowTests.Issues
                 using (var session = store.OpenAsyncSession())
                 {
                     var revisionCount = session.Advanced.Revisions.GetForAsync<User>("users/1", 0, 1000).Result.Count;
+                    Assert.Equal(10, revisionCount);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task DoNotlimitRevisionDeletionWithEnforceConfiguration()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var configuration = new RevisionsConfiguration
+                {
+                    Default = new RevisionsCollectionConfiguration
+                    {
+                        Disabled = false,
+                        MinimumRevisionsToKeep = 1000000
+                    },
+                };
+                await RevisionsHelper.SetupRevisions(store, Server.ServerStore, configuration);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User { Name = "Toli" }, "users/1");
+                    await session.SaveChangesAsync();
+                }
+
+                for (int i = 0; i < 500; i++)
+                {
+                    using (var session = store.OpenAsyncSession())
+                    {
+                        await session.StoreAsync(new User { Name = "Toli" + i }, "users/1");
+                        await session.SaveChangesAsync();
+                    }
+                }
+                using (var session = store.OpenAsyncSession())
+                {
+
+                    var revisionCount = session.Advanced.Revisions.GetForAsync<User>("users/1", 0, 1000).Result.Count;
+                    Assert.Equal(501, revisionCount);
+                }
+
+                configuration = new RevisionsConfiguration
+                {
+                    Default = new RevisionsCollectionConfiguration
+                    {
+                        Disabled = false,
+                        MinimumRevisionsToKeep = 10,
+                        MaxRevisionsToDeleteUponDocumentUpdate = 100
+                    },
+
+                };
+                await RevisionsHelper.SetupRevisions(store, Server.ServerStore, configuration);
+
+                var db = await GetDocumentDatabaseInstanceFor(store);
+                using (var token = new OperationCancelToken(db.Configuration.Databases.OperationTimeout.AsTimeSpan, db.DatabaseShutdown, CancellationToken.None))
+                    await db.DocumentsStorage.RevisionsStorage.EnforceConfiguration(_ => { }, token);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    var revisionCount = session.Advanced.Revisions.GetForAsync<User>("users/1", 0, 100).Result.Count;
                     Assert.Equal(10, revisionCount);
                 }
             }
