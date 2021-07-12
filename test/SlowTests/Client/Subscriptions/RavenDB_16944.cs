6640901e8f8d2b0e866cdf18629a8fcda233294c
Gracjan Sadowicz <gracjan@ravendb.net>
2021-07-12 13:58:05 +0200

    RavenDB-16944 Added 'disabled' property to CreateSubscriptionCommand, created new constructor, maintaining old ctor to work as well, changed request url to include property. Added two passing tests that check if created subscription is enabled by default/disabled.

---
+++ test/SlowTests/Client/Subscriptions/RavenDB_16944.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FastTests;
+using Raven.Client.Documents.Commands;
+using Raven.Client.Documents.Subscriptions;
+using Raven.Tests.Core.Utils.Entities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Client.Subscriptions
+{
+    public class RavenDB_16944:RavenTestBase
+    {
+        public RavenDB_16944(ITestOutputHelper output) : base(output) { }
+
+        [Fact]
+        public void SubscriptionIsDisabledWhenCreatedAsDisabled()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var entity = new User
+                    {
+                        Name = "Ed"
+                    };
+                    session.Store(entity);
+                    session.SaveChanges();
+                }
+
+                using (store.GetRequestExecutor().ContextPool.AllocateOperationContext(out var context))
+                {
+                    store.GetRequestExecutor()
+                        .Execute(
+                            new CreateSubscriptionCommand(store.Conventions,
+                                new SubscriptionCreationOptions() { Name = "10", Query = "From Users" }, true, "10"), context);
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var subs = store.Subscriptions.GetSubscriptions(0, 10);
+                    var item = subs.FirstOrDefault(x => x.SubscriptionName == "10" && x.Disabled);
+                    Assert.NotNull(item);
+                }
+            }
+        }
+
+        [Fact]
+        public void SubscriptionIsEnabledWhenCreatedWithoutPassingDisabledArg()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    var entity = new User
+                    {
+                        Name = "Ed"
+                    };
+                    session.Store(entity);
+                    session.SaveChanges();
+                }
+
+                using (store.GetRequestExecutor().ContextPool.AllocateOperationContext(out var context))
+                {
+                    store.GetRequestExecutor()
+                        .Execute(
+                            new CreateSubscriptionCommand(store.Conventions,
+                                new SubscriptionCreationOptions() { Name = "10", Query = "From Users" },"10"), context);
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var subs = store.Subscriptions.GetSubscriptions(0, 10);
+                    var item = subs.FirstOrDefault(x => x.SubscriptionName == "10" && !x.Disabled);
+                    Assert.NotNull(item);
+                }
+            }
+        }
+    }
+}
