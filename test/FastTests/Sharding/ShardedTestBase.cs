06ed7aa65ff6ee26c2fb3e88f33f2521c0b408f9
egor <egor@ravendb.net>
2021-07-14 16:27:01 +0300

    RavenDB-13113: fix stack overflow exception, add WaitForUserToContinueTheTest

--- test/FastTests/Sharding/ShardedTestBase.cs
+++ test/FastTests/Sharding/ShardedTestBase.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using Raven.Client.Documents;
 using Raven.Client.ServerWide;
 using Xunit;
@@ -24,9 +26,15 @@ namespace FastTests.Sharding
                     new DatabaseTopology(), 
                     new DatabaseTopology(),
                 };
-            };
+                options?.ModifyDatabaseRecord?.Invoke(r);            };
             //shardedOptions.RunInMemory = false;
             return GetDocumentStore(shardedOptions, caller);
+        }
+
+        public new void WaitForUserToContinueTheTest(IDocumentStore documentStore, bool debug = true, string database = null, X509Certificate2 clientCert = null)
+        {
+            var db = database ?? $"{documentStore.Database}$0";
+            RavenTestBase.WaitForUserToContinueTheTest(documentStore, debug, db, clientCert);
         }
     }
 }
