8e2a14424c001cb670b0753ef21e20e0f7029766
aviv <aviv@ayende.com>
2021-07-18 14:41:30 +0300

    RavenDB-13114 : implementation changes : - avoid selecting a random shard to get the hi-low range from, determine the shard to use according to the hilo doc-id - remove notation of shard-index in client side and in server's response - generated ids will be the same as in "unsharded" database - without the shard-index suffix

--- src/Raven.Server/Documents/ShardedHandlers/ShardedHiLoHandler.cs
+++ src/Raven.Server/Documents/ShardedHandlers/ShardedHiLoHandler.cs
@@ -4,18 +4,13 @@
 //  </copyright>
 // -----------------------------------------------------------------------
 
-using System;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using Raven.Client;
-using Raven.Client.Documents.Identity;
 using Raven.Server.Documents.Handlers;
 using Raven.Server.Documents.ShardedHandlers.ShardedCommands;
 using Raven.Server.Documents.Sharding;
 using Raven.Server.Routing;
 using Raven.Server.ServerWide.Context;
-using Sparrow.Json;
-using Sparrow.Json.Parsing;
 
 namespace Raven.Server.Documents.ShardedHandlers
 {
@@ -26,63 +21,34 @@ namespace Raven.Server.Documents.ShardedHandlers
         {
             using (ContextPool.AllocateOperationContext(out TransactionOperationContext context))
             {
-                var tag = GetQueryStringValueAndAssertIfSingleAndNotEmpty("tag");
-                var numOfShards = ShardedContext.RequestExecutors.Length;
-                var shardIndex = ShardedContext.GetShardIndex(context, DateTime.UtcNow.Ticks.ToString());
-
-                for (int i = 0; i < numOfShards; i++)
-                {
-                    var cmd = new ShardedCommand(this, Headers.None);
-
-                    try
-                    {
-                        await ShardedContext.RequestExecutors[shardIndex].ExecuteAsync(cmd, context);
-                    }
-                    catch (Exception)
-                    {
-                        // failed to reach shard
-                        // try the next one
-                        shardIndex = (shardIndex + 1) % numOfShards;
-                        continue;
-                    }
-
-                    HttpContext.Response.StatusCode = (int)cmd.StatusCode;
-
-                    HttpContext.Response.Headers[Constants.Headers.Etag] = cmd.Response?.Headers?.ETag?.Tag;
-
-                    cmd.Result.Modifications = new DynamicJsonValue(cmd.Result)
-                    {
-                        [nameof(HiLoResult.ShardIndex)] = shardIndex
-                    };
-
-                    using (cmd.Result)
-                    using (var result = context.ReadObject(cmd.Result, HiLoHandler.RavenHiloIdPrefix + tag, BlittableJsonDocumentBuilder.UsageMode.ToDisk))
-                    {
-                        await result.WriteJsonToAsync(ResponseBodyStream());
-                    }
-
-                    return;
-                }
+                var cmd = await ExecuteShardedHiloCommand(context);
+                await cmd.Result.WriteJsonToAsync(ResponseBodyStream());
             }
+
         }
 
         [RavenShardedAction("/databases/*/hilo/return", "PUT")]
         public async Task HiLoReturn()
         {
-            var shardIndex = GetLongQueryString("shardIndex", required: true);
-            Debug.Assert(shardIndex.HasValue);
-            if (shardIndex >= ShardedContext.RequestExecutors.Length)
-                throw new ArgumentException($"Value of query string 'shardIndex' is invalid, got '{shardIndex}' while the number of shards is '{ShardedContext.RequestExecutors.Length}'.");
-
-            var cmd = new ShardedCommand(this, Headers.None);
             using (ContextPool.AllocateOperationContext(out TransactionOperationContext context))
             {
-                // ReSharper disable once PossibleInvalidOperationException
-                await ShardedContext.RequestExecutors[shardIndex.Value].ExecuteAsync(cmd, context);
+                await ExecuteShardedHiloCommand(context);
             }
+        }
 
+        private async Task<ShardedCommand> ExecuteShardedHiloCommand(TransactionOperationContext context)
+        {
+            var tag = GetQueryStringValueAndAssertIfSingleAndNotEmpty("tag");
+            var hiloDocId = HiLoHandler.RavenHiloIdPrefix + tag;
+            var shardIndex = ShardedContext.GetShardIndex(context, hiloDocId);
+
+            var cmd = new ShardedCommand(this, Headers.None);
+            await ShardedContext.RequestExecutors[shardIndex].ExecuteAsync(cmd, context);
+            
             HttpContext.Response.StatusCode = (int)cmd.StatusCode;
             HttpContext.Response.Headers[Constants.Headers.Etag] = cmd.Response?.Headers?.ETag?.Tag;
+
+            return cmd;
         }
     }
 }
