617d546f66a51abdc10bcb941c9635ccbb5ea241
efrat@ayende.com <efrat@ayende.com>
2021-08-16 13:59:41 +0300

    RavenDB-16961 test

--- test/SlowTests/Issues/RavenDB-16961.cs
+++ test/SlowTests/Issues/RavenDB-16961.cs
@@ -1,10 +1,12 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using FastTests;
 using FastTests.Graph;
 using FastTests.Server.Replication;
 using FastTests.Utils;
 using Raven.Client.Documents.Operations.Revisions;
+using Raven.Client.ServerWide;
 using Raven.Server.Documents;
 using Raven.Server.ServerWide;
 using Raven.Server.ServerWide.Context;
@@ -122,6 +124,75 @@ namespace SlowTests.Issues
                         }
                     }, false
                 );
+            }
+        }
+
+        [Fact]
+        public async Task EnforceRevisionConfigurationWithConflict()
+        {
+            using (var store1 = GetDocumentStore(new Options
+            {
+                ModifyDatabaseName = s => $"{s}_foo1",
+                ModifyDatabaseRecord = record =>
+                {
+                    record.ConflictSolverConfig = new ConflictSolver
+                    {
+                        ResolveToLatest = false,
+                        ResolveByCollection = new Dictionary<string, ScriptResolver>()
+                    };
+                }
+            }))
+            using (var store2 = GetDocumentStore(new Options
+            {
+                ModifyDatabaseName = s => $"{s}_foo2",
+                ModifyDatabaseRecord = record =>
+                {
+                    record.ConflictSolverConfig = new ConflictSolver
+                    {
+                        ResolveToLatest = false,
+                        ResolveByCollection = new Dictionary<string, ScriptResolver>()
+                    };
+                }
+            }))
+            {
+
+                await RevisionsHelper.SetupRevisions(store1, Server.ServerStore, new RevisionsConfiguration()
+                {
+                    Default = new RevisionsCollectionConfiguration()
+                    {
+                        Disabled = false
+                    }
+                });
+                WaitForUserToContinueTheTest(store1);
+                using (var s1 = store1.OpenSession())
+                {
+                    s1.Store(new User { Name = "test" }, "users/1");
+                    s1.SaveChanges();
+                }
+
+                using (var s2 = store2.OpenSession())
+                {
+                    s2.Store(new User { Name = "test3" }, "users/1");
+                    s2.SaveChanges();
+                }
+
+                await SetupReplicationAsync(store2, store1);
+                var conflicts = WaitUntilHasConflict(store1, "users/1");
+                await RevisionsHelper.SetupRevisions(store1, Server.ServerStore, new RevisionsConfiguration());
+
+                var db = await GetDocumentDatabaseInstanceFor(store1, store1.Database);
+                using (var token = new OperationCancelToken(db.Configuration.Databases.OperationTimeout.AsTimeSpan, db.DatabaseShutdown, CancellationToken.None))
+                    await db.DocumentsStorage.RevisionsStorage.EnforceConfiguration(_ => { }, token);
+
+                await UpdateConflictResolver(store1, resolveToLatest: true);
+
+                WaitForValue(() => store1.Commands().GetConflictsFor("users/1").Length, 0);
+
+                using (var session = store1.OpenAsyncSession())
+                {
+                    var revision = await session.Advanced.Revisions.GetForAsync<User>("users/1");
+                    Assert.Equal(3, revision.Count);
+                }
             }
         }
     }
