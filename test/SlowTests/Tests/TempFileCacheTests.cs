64c3f5ca7513c3ad982a6b6e445b9fbfedb71de2
Grisha Kotler <grisha@ayende.com>
2021-08-31 11:58:35 +0300

    RavenDB-16762 - reuse the files in TempFileCache (linux)

---
+++ test/SlowTests/Tests/TempFileCacheTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Documents.Subscriptions;
+using Raven.Server.Indexing;
+using Sparrow.Server;
+using Voron;
+using Voron.Exceptions;
+using Voron.Util.Settings;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Tests
+{
+    public class TempFileCacheTests : RavenTestBase
+    {
+        public TempFileCacheTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Can_reuse_files_for_cache()
+        {
+            var path = new VoronPathSetting(NewDataPath());
+            var environment = new StorageEnvironmentOptions.DirectoryStorageEnvironmentOptions(path, path, path, null, null);
+
+            using (File.Create(TempFileCache.GetTempFileName(environment)))
+            {
+            }
+
+            using (var cache = new TempFileCache(environment))
+            {
+                Assert.Equal(1, cache.FilesCount);
+            }
+        }
+    }
+}
