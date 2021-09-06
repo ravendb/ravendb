b1f8e54516fe0e3bb3addf340287e5baa35dd135
Gracjan Sadowicz <gracjan@ravendb.net>
2021-09-06 09:42:54 +0200

    RavenDB-15918 Relocated certificate utility

--- docker/ravendb-ubuntu/cert-utils.sh
+++ docker/ravendb-ubuntu/cert-utils.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+function extract_cert_and_key() {
 if [[ $# -ne 1 ]]; then
 echo "Please pass certificate (e.g. .pfx) path.."
 exit 1
@@ -7,3 +9,4 @@ password=$(./get-server-var.sh Configuration.Security.CertificatePassword) | sed
 openssl pkcs12 -in $1 -out /tmp/docker_cert.crt -nodes -nokeys -password pass:$password
 openssl pkcs12 -in $1 -out /tmp/docker_key.key -nodes -nocerts -password pass:$password
 fi
+}
