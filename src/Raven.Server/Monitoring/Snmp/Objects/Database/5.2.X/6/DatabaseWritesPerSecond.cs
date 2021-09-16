31c4d80d119d7d2d6bc13b3a1e777e6880508296
Pawel Pekrol <ppekrol@ravendb.net>
2021-09-16 11:54:59 +0200

    RavenDB-16609 exposed writes and data written per second per database

---
+++ src/Raven.Server/Monitoring/Snmp/Objects/Database/5.2.X/6/DatabaseWritesPerSecond.cs
@@ -0,0 +1,33 @@
+// -----------------------------------------------------------------------
+//  <copyright file="DatabaseOpenedCount.cs" company="Hibernating Rhinos LTD">
+//      Copyright (c) Hibernating Rhinos LTD. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Lextm.SharpSnmpLib;
+using Raven.Server.Documents;
+
+namespace Raven.Server.Monitoring.Snmp.Objects.Database
+{
+    public class DatabaseWritesPerSecond : DatabaseScalarObjectBase<Gauge32>
+    {
+        public DatabaseWritesPerSecond(string databaseName, DatabasesLandlord landlord, int index)
+            : base(databaseName, landlord, SnmpOids.Databases.WritesPerSecond, index)
+        {
+        }
+
+        protected override Gauge32 GetData(DocumentDatabase database)
+        {
+            return new Gauge32(GetCount(database));
+        }
+
+        private static int GetCount(DocumentDatabase database)
+        {
+            var value = database.Metrics.Docs.PutsPerSec.OneMinuteRate
+                        + database.Metrics.Attachments.PutsPerSec.OneMinuteRate
+                        + database.Metrics.Counters.PutsPerSec.OneMinuteRate;
+
+            return (int)value;
+        }
+    }
+}
