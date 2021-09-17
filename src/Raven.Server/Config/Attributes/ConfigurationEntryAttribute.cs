2a14cbf7c98ef114d99ffe814065e87e50d2ee7a
Pawel Pekrol <ppekrol@ravendb.net>
2021-09-17 09:00:50 +0200

    RavenDB-17225 removed space from Tombstones.RetentionTimeWithReplicationHubInHrs

--- src/Raven.Server/Config/Attributes/ConfigurationEntryAttribute.cs
+++ src/Raven.Server/Config/Attributes/ConfigurationEntryAttribute.cs
@@ -12,8 +12,17 @@ namespace Raven.Server.Config.Attributes
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
     public class ConfigurationEntryAttribute : Attribute
     {
-        public ConfigurationEntryAttribute(string key, ConfigurationEntryScope scope, [CallerLineNumber]int order = 0, bool setDefaultValueIfNeeded = true, bool isSecured = false) // the default order is the order of declaration in a configuration class
+        public ConfigurationEntryAttribute(string key, ConfigurationEntryScope scope, [CallerLineNumber] int order = 0, bool setDefaultValueIfNeeded = true, bool isSecured = false) // the default order is the order of declaration in a configuration class
         {
+            if (string.IsNullOrWhiteSpace(key))
+                throw new ArgumentNullException(nameof(key));
+
+#if DEBUG
+            var trimmedKey = key.Trim();
+            if (key != trimmedKey)
+                throw new InvalidOperationException($"Configuration Key '{key}' contains white-space characters.");
+#endif
+
             Key = key;
             Order = order;
             SetDefaultValueIfNeeded = setDefaultValueIfNeeded;
