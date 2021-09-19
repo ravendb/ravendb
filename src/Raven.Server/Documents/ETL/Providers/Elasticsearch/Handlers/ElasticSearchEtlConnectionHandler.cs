fdb3ffc42fe9c89fd3bfce52836e43e6a21c4b1d
Djordje Djukic <djukicdjole@gmail.com>
2021-09-19 16:51:59 +0200

    RavenDB-13041 rework test connection string

---
+++ src/Raven.Server/Documents/ETL/Providers/Elasticsearch/Handlers/ElasticSearchEtlConnectionHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Nest;
+using Newtonsoft.Json;
+using Raven.Client.Documents.Operations.ETL.ElasticSearch;
+using Raven.Server.Routing;
+using Raven.Server.Web;
+using Raven.Server.Web.System;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+using Authentication = Raven.Client.Documents.Operations.ETL.ElasticSearch.Authentication;
+
+namespace Raven.Server.Documents.ETL.Providers.ElasticSearch.Handlers
+{
+    public class ElasticSearchEtlConnectionHandler : RequestHandler
+    {
+        [RavenAction("/admin/etl/elasticsearch/test-connection", "POST", AuthorizationStatus.Operator)]
+        public async Task GetTestSqlConnectionResult()
+        {
+            string url = GetQueryStringValueAndAssertIfSingleAndNotEmpty("url");
+            string authenticationJson = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            Authentication authentication = JsonConvert.DeserializeObject<Authentication>(authenticationJson);
+
+            ElasticClient client = ElasticSearchHelper.CreateClient(new ElasticSearchConnectionString { Nodes = new[] { url }, Authentication = authentication });
+
+            PingResponse pingResult = await client.PingAsync();
+
+            if (pingResult.IsValid)
+            {
+                DynamicJsonValue result = new()
+                {
+                    [nameof(NodeConnectionTestResult.Success)] = true,
+                    [nameof(NodeConnectionTestResult.TcpServerUrl)] = url,
+                };
+
+                using (ServerStore.ContextPool.AllocateOperationContext(out JsonOperationContext context))
+                await using (AsyncBlittableJsonTextWriter writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
+                {
+                    context.Write(writer, result);
+                }
+            }
+            else
+            {
+                using (ServerStore.ContextPool.AllocateOperationContext(out JsonOperationContext context))
+                {
+                    await using (AsyncBlittableJsonTextWriter writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
+                    {
+                        context.Write(writer,
+                            new DynamicJsonValue
+                            {
+                                [nameof(NodeConnectionTestResult.Success)] = false,
+                                [nameof(NodeConnectionTestResult.Error)] = pingResult.DebugInformation
+                            });
+                    }
+                }
+            }
+        }
+    }
+}
--- src/Raven.Server/Documents/ETL/Providers/Elasticsearch/Handlers/ElasticsearchEtlHandler.cs
+++ src/Raven.Server/Documents/ETL/Providers/Elasticsearch/Handlers/ElasticsearchEtlHandler.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Raven.Client.Documents.Operations.ETL.ElasticSearch;
+﻿using System.Threading.Tasks;
 using Raven.Server.Documents.ETL.Providers.ElasticSearch.Test;
 using Raven.Server.Json;
 using Raven.Server.Routing;
 using Raven.Server.ServerWide.Context;
 using Raven.Server.Utils;
-using Raven.Server.Web.System;
 using Sparrow.Json;
 using Sparrow.Json.Parsing;
 
@@ -16,64 +11,6 @@ namespace Raven.Server.Documents.ETL.Providers.ElasticSearch.Handlers
 {
     public class ElasticSearchEtlHandler : DatabaseRequestHandler
     {
-        [RavenAction("/admin/etl/elasticsearch/test-connection", "POST", AuthorizationStatus.Operator)]
-        public async Task GetTestSqlConnection()
-        {
-            try
-            {
-                var url = GetQueryStringValueAndAssertIfSingleAndNotEmpty("url");
-                var authenticationJson = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
-                var authentication = JsonConvert.DeserializeObject<Authentication>(authenticationJson);
-
-                var client = ElasticSearchHelper.CreateClient(new ElasticSearchConnectionString(){Nodes = new []{url}, Authentication = authentication});
-
-                var pingResult = await client.PingAsync();
-
-                if (pingResult.IsValid)
-                {
-                    DynamicJsonValue result = new DynamicJsonValue
-                    {
-                        [nameof(NodeConnectionTestResult.Success)] = true,
-                    };
-
-                    using (ContextPool.AllocateOperationContext(out JsonOperationContext context))
-                    await using (var writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
-                    {
-                        context.Write(writer, result);
-                    }    
-                }
-                
-                using (ContextPool.AllocateOperationContext(out JsonOperationContext context))
-                {
-                    await using (var writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
-                    {
-                        context.Write(writer, new DynamicJsonValue
-                        {
-                            [nameof(NodeConnectionTestResult.Success)] = false,
-                            [nameof(NodeConnectionTestResult.Error)] = pingResult.DebugInformation
-                        });
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                if (Logger.IsInfoEnabled)
-                    Logger.Info("Error occurred during elasticsearch connection test", ex);
-
-                using (ContextPool.AllocateOperationContext(out JsonOperationContext context))
-                {
-                    await using (var writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
-                    {
-                        context.Write(writer, new DynamicJsonValue
-                        {
-                            [nameof(NodeConnectionTestResult.Success)] = false,
-                            [nameof(NodeConnectionTestResult.Error)] = ex.ToString()
-                        });
-                    }
-                }
-            }
-        }
-        
         [RavenAction("/databases/*/admin/etl/elasticsearch/test", "POST", AuthorizationStatus.Operator)]
         public async Task PostScriptTest()
         {
