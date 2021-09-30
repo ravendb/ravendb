8150a0a808beb3ff44a907ee523a7ddb2932012d
shlomo kazakov <shlomo.kazakov@gmail.com>
2021-10-01 00:29:27 +0300

    fixed calling of Add with ref, implemented IV8DebugInfo

--- src/Raven.Server/Documents/Indexes/Static/JavaScript/ObjectInstanceBase.cs
+++ src/Raven.Server/Documents/Indexes/Static/JavaScript/ObjectInstanceBase.cs
@@ -7,35 +7,11 @@ using Raven.Server.Extensions;
 
 namespace Raven.Server.Documents.Indexes.Static.JavaScript
 {
-    public abstract class ObjectInstanceBase
+    public abstract class ObjectInstanceBase: IDisposable
+#if DEBUG
+    , IV8DebugInfo
+#endif
     {
-        bool IsImplicitNull;
-        protected DictionaryDisposeValueIHV8<string> _properties = new DictionaryDisposeValueIHV8<string>();
-
-        public ObjectInstanceBase(bool isImplicitNull = true)
-        {
-            IsImplicitNull = isImplicitNull;
-        }
-
-        public abstract InternalHandle NamedPropertyGetterOnce(V8EngineEx engine, ref string propertyName);
-
-        public virtual InternalHandle NamedPropertyGetter(V8EngineEx engine, ref string propertyName)
-        {
-            if (!_properties.TryGetValue(propertyName, out InternalHandle jsValue))
-            {
-                jsValue = NamedPropertyGetterOnce((V8EngineEx)engine, ref propertyName);
-                if (!jsValue.IsEmpty)
-                    _properties.Add(propertyName, jsValue);
-            }
-
-            if (jsValue.IsEmpty) {
-                return IsImplicitNull ? engine.ImplicitNull.CreateHandle() : jsValue;
-            }
-
-            return jsValue;
-        }
-
-
         public abstract class CustomBinder<T> : ObjectBinderEx<T>
         where T : ObjectInstanceBase
         {
@@ -65,5 +41,121 @@ namespace Raven.Server.Documents.Indexes.Static.JavaScript
             }
 
         }
+
+
+        private bool _disposed = false;
+
+        bool IsImplicitNull;
+        protected DictionaryDisposeValueIHV8<string> _properties = new DictionaryDisposeValueIHV8<string>();
+
+#if DEBUG
+        private V8EntityID _SelfID;
+#endif
+
+        public ObjectInstanceBase(bool isImplicitNull = true)
+        {
+            IsImplicitNull = isImplicitNull;
+            GC.SuppressFinalize(_properties);
+        }
+
+        public abstract InternalHandle NamedPropertyGetterOnce(V8EngineEx engine, ref string propertyName);
+
+        public virtual InternalHandle NamedPropertyGetter(V8EngineEx engine, ref string propertyName)
+        {
+            if (!_properties.TryGetValue(propertyName, out InternalHandle jsValue))
+            {
+                jsValue = NamedPropertyGetterOnce((V8EngineEx)engine, ref propertyName);
+                if (!jsValue.IsEmpty)
+                    _properties.Add(propertyName, ref jsValue);
+            }
+
+            if (jsValue.IsEmpty) {
+                return IsImplicitNull ? engine.ImplicitNull.CreateHandle() : jsValue;
+            }
+
+            return jsValue;
+        }
+
+        ~ObjectInstanceBase()
+        {            
+            Dispose(false);
+        }
+
+
+        public void Dispose()
+        {  
+            Dispose(true);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            _properties.Dispose();
+
+            if (disposing) {
+                _properties = null;
+
+                //GC.SuppressFinalize(this);
+            }
+
+            _disposed = true;
+        }
+
+#if DEBUG
+        public V8EntityID SelfID
+        {
+            get {
+                return _SelfID;
+            } 
+
+            set {
+                _SelfID = value;
+            } 
+        }
+
+        public V8EntityID ParentID
+        {
+            get {return null;} 
+        }
+
+        public List<V8EntityID> ChildIDs
+        {
+            get {
+                var res = new List<V8EntityID>();
+
+                var countProps = _properties?.Count ?? 0;
+                if (countProps <= 0)
+                    return res;
+
+                foreach (var kvp in _properties) {
+                    InternalHandle h = kvp.Value;
+                    res.Add(new V8EntityID(h.HandleID, h.ObjectID));
+
+                    if (!(h.IsDisposed || h.IsCLRDisposed) && h.IsArray) {
+                        for (int j = 0; j < h.ArrayLength; j++)
+                        {
+                            using (var jsItem = h.GetProperty(j))
+                            {
+                                res.Add(new V8EntityID(jsItem.HandleID, jsItem.ObjectID));
+                            }
+                        }
+                    }
+                }
+                //Engine.ForceV8GarbageCollection();
+                return res;
+            } 
+        }
+
+        public string Summary
+        {
+            get {
+                string desc = "";
+                return desc;
+            }
+        }
+#endif
+
     }
 }
