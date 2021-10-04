eae7cf8ba69753b0a8b8471c3bc6d8336561a651
Danielle9897 <danielle.9897@gmail.com>
2021-10-05 00:24:20 +0300

    RavenDB-13874  Identities View

---
+++ src/Raven.Studio/typescript/commands/database/identities/setIdentityCommand.ts
@@ -0,0 +1,25 @@
+import commandBase = require("commands/commandBase");
+import endpoints = require("endpoints");
+import database = require("models/resources/database");
+
+class setIdentityCommand extends commandBase {
+
+    constructor(private db: database, private prefix: string, private value: number) {
+        super();
+    }
+
+    execute(): JQueryPromise<dictionary<number>> {
+        const args = {
+            name: this.prefix,
+            value: this.value,
+            force: true
+        };
+        
+        const url = endpoints.databases.identity.identitySeed + this.urlEncodeArgs(args);
+
+        return this.post(url, null, this.db)
+            .fail((response: JQueryXHR) => this.reportError(`Failed to set identity`, response.responseText, response.statusText));
+    }
+}
+
+export = setIdentityCommand;
