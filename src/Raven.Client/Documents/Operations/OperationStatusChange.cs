4dfad8248391a511d3817437dbf3e52b538afcdc
Pawel Pekrol <ppekrol@ravendb.net>
2021-10-12 12:15:06 +0200

    RavenDB-17316 use default conventions to OperationState in OperationStatusChange because user conventions might have TypeNameHandling set to None (fixes SlowTests.Issues.RavenDB_17177.Can_WaitForOperation_When_TypeNameHandling_Is_None)

--- src/Raven.Client/Documents/Operations/OperationStatusChange.cs
+++ src/Raven.Client/Documents/Operations/OperationStatusChange.cs
@@ -20,7 +20,7 @@ namespace Raven.Client.Documents.Operations
             };
         }
 
-        internal static OperationStatusChange FromJson(BlittableJsonReaderObject value, DocumentConventions conventions)
+        internal static OperationStatusChange FromJson(BlittableJsonReaderObject value)
         {
             value.TryGet(nameof(OperationId), out long operationId);
             value.TryGet(nameof(State), out BlittableJsonReaderObject stateAsJson);
@@ -28,7 +28,7 @@ namespace Raven.Client.Documents.Operations
             return new OperationStatusChange
             {
                 OperationId = operationId,
-                State = (OperationState)conventions.DeserializeEntityFromBlittable(typeof(OperationState), stateAsJson)
+                State = (OperationState)DocumentConventions.Default.DeserializeEntityFromBlittable(typeof(OperationState), stateAsJson)
             };
         }
     }
