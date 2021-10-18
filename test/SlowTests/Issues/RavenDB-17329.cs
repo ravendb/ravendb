2bca2303159b6f1639a454645dde410d67402fc5
aviv <aviv@ayende.com>
2021-10-18 15:43:24 +0300

    RavenDB-17329 : adders PR comments

--- test/SlowTests/Issues/RavenDB-17329.cs
+++ test/SlowTests/Issues/RavenDB-17329.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Conventions;
 using Raven.Client.Documents.Operations.Counters;
+using Raven.Client.ServerWide.Operations;
 using Raven.Tests.Core.Utils.Entities;
 using Tests.Infrastructure;
 using Xunit;
@@ -17,7 +18,7 @@ namespace SlowTests.Issues
         }
 
         [Fact]
-        public async Task RavenDb17329()
+        public async Task DeletingCounterWhenFirstPartialValueIsEmptyAndRemote_ShouldNotGenerateCorruptedDeleteCv()
         {
             var (_, leader) = await CreateRaftCluster(2);
             var dbName = GetDatabaseName();
@@ -64,6 +65,32 @@ namespace SlowTests.Issues
                     Assert.Equal(1, countersDetail.Counters.Count);
                     Assert.Null(countersDetail.Counters[0]);
                 }
+
+                foreach (var server in db.Servers)
+                {
+                    await EnsureNoReplicationLoop(server, dbName);
+                }
+
+                var rec = stores[0].Maintenance.Server.Send(new GetDatabaseRecordOperation(dbName));
+                var topology = rec.Topology;
+                Assert.Equal(2, topology.Count);
+                Assert.Equal(2, topology.Members.Count);
+
+                using (var s = stores[0].OpenSession())
+                {
+                    s.Store(new User { Name = "John doe" }, "users/2");
+                    s.SaveChanges();
+                }
+
+                Assert.True(WaitForDocument<User>(stores[1], "users/2", x => x.Name == "John doe"));
+
+                using (var s = stores[1].OpenSession())
+                {
+                    s.Store(new User { Name = "Jane doe" }, "users/3");
+                    s.SaveChanges();
+                }
+
+                Assert.True(WaitForDocument<User>(stores[0], "users/3", x => x.Name == "Jane doe"));
             }
             finally
             {
