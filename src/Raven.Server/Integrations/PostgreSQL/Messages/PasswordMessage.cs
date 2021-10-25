025428054db16395f401157552c8c64bb1a425de
Krzysztof Kruglak <krzysztof@ravendb.net>
2021-10-25 11:17:47 +0200

    RavenDB-16880 Add username and password checking

--- src/Raven.Server/Integrations/PostgreSQL/Messages/PasswordMessage.cs
+++ src/Raven.Server/Integrations/PostgreSQL/Messages/PasswordMessage.cs
@@ -12,6 +12,10 @@ namespace Raven.Server.Integrations.PostgreSQL.Messages
     {
         public string Password;
 
+        private readonly string _errorSeverity = "FATAL";
+        private readonly string _errorCode = "28P01";
+        private string ErrorMessage(string username) => $"password authentication failed for user \"{username}\"";
+
         protected override async Task HandleMessage(PgTransaction transaction, MessageBuilder messageBuilder, PipeWriter writer, CancellationToken token)
         {
             var serverStore = transaction.DocumentDatabase.ServerStore;
@@ -22,20 +26,19 @@ namespace Raven.Server.Integrations.PostgreSQL.Messages
             using (context.OpenReadTransaction())
                 databaseRecord = serverStore.Cluster.ReadDatabase(context, databaseName, out long index);
 
-            var users = databaseRecord.Integrations.PostgreSql.Authentication.Users;
+            var users = databaseRecord?.Integrations?.PostgreSql?.Authentication?.Users;
 
-            //var user = users
-            //    .Where(x => x.Username == transaction.Username)
-                
+            if (users == null)
+                throw new PgFatalException(PgErrorCodes.NoDataFound, "Authentication failed");
 
+            var user = users
+                .SingleOrDefault(x => x.Username == transaction.Username);
 
-
-            // TODO arek
-            //var password = result[transaction.Username];
-            //if (Password.Equals(password) == false)
-            //{
-            //    throw new PgFatalException(PgErrorCodes.InvalidPassword, "Authentication failed, password is invalid.");
-            //}
+            if (user == null || Password.Equals(user.Password) == false)
+            {
+                await writer.WriteAsync(messageBuilder.ErrorResponse(_errorSeverity, _errorCode, ErrorMessage(transaction.Username)), token);
+                return;
+            }
 
             await writer.WriteAsync(messageBuilder.AuthenticationOk(), token);
         }
