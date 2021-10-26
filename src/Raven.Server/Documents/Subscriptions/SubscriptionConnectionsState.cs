a3f88415ba153dfcf16483cd4705e0a6852db8b8
stav <stav@ravendb.net>
2021-10-26 12:33:44 +0300

    RavenDB-15919 - Fixed dropping single connection through studio

--- src/Raven.Server/Documents/Subscriptions/SubscriptionConnectionsState.cs
+++ src/Raven.Server/Documents/Subscriptions/SubscriptionConnectionsState.cs
@@ -396,7 +396,8 @@ namespace Raven.Server.Documents.Subscriptions
                     new SubscriptionConnectionDetails
                     {
                         ClientUri = connection.ClientUri,
-                        ConnectionId = connection.ConnectionId
+                        ConnectionId = connection.ConnectionId,
+                        Strategy = connection.Strategy
                     });
             }
 
