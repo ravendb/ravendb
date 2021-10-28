193163fa715555af4708a57566a0ef1053389aa4
Grisha Kotler <grisha@ayende.com>
2021-10-28 22:14:38 +0300

    RavenDB-17414 - Better stack trace using dotnet-stack

--- src/Raven.Server/Documents/Handlers/Debugging/ThreadsHandler.cs
+++ src/Raven.Server/Documents/Handlers/Debugging/ThreadsHandler.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
