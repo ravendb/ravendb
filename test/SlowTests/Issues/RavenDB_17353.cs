8d600792e8080211fc4d8f984028f27218a645f1
aviv <aviv@ayende.com>
2021-11-01 13:59:21 +0200

    RavenDB-17411 : add test

--- test/SlowTests/Issues/RavenDB_17353.cs
+++ test/SlowTests/Issues/RavenDB_17353.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using FastTests;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Conventions;
+using Raven.Client.Documents.Operations.Counters;
 using Raven.Server.Utils;
+using SlowTests.Core.Utils.Entities;
+using Tests.Infrastructure;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace SlowTests.Issues
 {
-    public class RavenDB_17353 : RavenTestBase
+    public class RavenDB_17353 : ClusterTestBase
     {
         public RavenDB_17353(ITestOutputHelper output) : base(output)
         {
@@ -25,6 +31,75 @@ namespace SlowTests.Issues
             nodeTag = ChangeVectorUtils.GetNodeTagById(cv, dbid2);
             Assert.NotEqual(" A", nodeTag); 
             Assert.Equal("A", nodeTag); 
+        }
+
+        [Fact]
+        public async Task GetFullCounterValues_NodeTagsShouldNotStartWithSpace()
+        {
+            var cluster = await CreateRaftCluster(3);
+            var db = GetDatabaseName();
+            await CreateDatabaseInCluster(db, 3, cluster.Leader.WebUrl);
+
+            var stores = cluster.Nodes.Select(s => new DocumentStore
+                {
+                    Database = db,
+                    Urls = new[] { s.WebUrl },
+                    Conventions = new DocumentConventions
+                    {
+                        DisableTopologyUpdates = true
+                    }
+                }.Initialize())
+                .ToList();
+
+            try
+            {
+                const string docId = "users/1";
+                const string counter = "likes";
+
+                using (var session = stores[0].OpenSession())
+                {
+                    session.Advanced.WaitForReplicationAfterSaveChanges(replicas: 2);
+                    session.Store(new User(), docId);
+                    session.SaveChanges();
+                }
+
+                foreach (var store in stores)
+                {
+                    using (var session = store.OpenSession())
+                    {
+                        session.Advanced.WaitForReplicationAfterSaveChanges(replicas: 2);
+                        session.CountersFor(docId).Increment(counter);
+                        session.SaveChanges();
+                    }
+                }
+
+                foreach (var store in stores)
+                {
+                    var result = store.Operations.Send(new GetCountersOperation(docId, returnFullResults: true));
+                    Assert.Equal(1, result.Counters.Count);
+                    Assert.Equal(counter, result.Counters[0].CounterName);
+
+                    var fullValues = result.Counters[0].CounterValues;
+                    Assert.Equal(3, fullValues.Count);
+
+                    var nodeTags = new HashSet<string> { "A", "B", "C" };
+                    foreach (var kvp in fullValues)
+                    {
+                        Assert.False(kvp.Key.StartsWith(" "));
+
+                        var tag = kvp.Key[0].ToString();
+                        Assert.True(nodeTags.Contains(tag));
+                        nodeTags.Remove(tag);
+                    }
+                }
+            }
+            finally
+            {
+                foreach (var item in stores)
+                {
+                    item.Dispose();
+                }
+            }
         }
     }
 }
