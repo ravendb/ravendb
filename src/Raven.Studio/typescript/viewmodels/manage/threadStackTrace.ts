c60b943dc24249b20dbaef750d6b11c0a470a6c9
Danielle9897 <danielle.9897@gmail.com>
2021-11-01 12:19:58 +0200

    RavenDB-17413 View stack trace of single thread
    # Conflicts:
    #	src/Raven.Studio/typescript/common/generalUtils.ts
    #	src/Raven.Studio/typescript/viewmodels/manage/debugAdvancedThreadsRuntime.ts
    #	src/Raven.Studio/typings/_studio/dto.d.ts

---
+++ src/Raven.Studio/typescript/viewmodels/manage/threadStackTrace.ts
@@ -0,0 +1,72 @@
+import dialogViewModelBase = require("viewmodels/dialogViewModelBase");
+import copyToClipboard = require("common/copyToClipboard");
+import getStackTraceForThreadCommand = require("commands/maintenance/getStackTraceForThreadCommand");
+import generalUtils = require("common/generalUtils");
+
+class threadStackTrace extends dialogViewModelBase {
+
+    threadId = ko.observable<number>();
+    threadName = ko.observable<string>();
+    threadType = ko.observable<string>();
+    
+    cpuUsage = ko.observable<string>();
+    stackTrace = ko.observableArray<string>([]);
+    
+    dialogContainer: Element;
+
+    spinners = {
+        loading: ko.observable<boolean>(false)
+    };
+
+    constructor(threadId: number, threadName: string) {
+        super();
+        
+        this.threadId(threadId);
+        this.threadName(threadName);
+    }
+    
+    compositionComplete() {
+        super.compositionComplete();
+        this.dialogContainer = document.getElementById("threadStackTraceDialog");
+    }
+    
+    activate() {
+        return this.loadStackTrace(this.threadId());
+    }
+
+    private loadStackTrace(threadId: number): JQueryPromise<threadStackTraceResponseDto> {
+        this.spinners.loading(true);
+        this.stackTrace([]);
+        
+        return new getStackTraceForThreadCommand(threadId)
+            .execute()
+            .done((results: threadStackTraceResponseDto) => {
+                if (results.Results.length) {
+                    this.stackTrace(results.Results[0].StackTrace);
+                    this.threadType(results.Results[0].ThreadType);
+                }
+                this.cpuUsage(this.getCpuUsage(results.Threads));
+                
+            })
+            .always(() => this.spinners.loading(false));
+    }
+
+    getCpuUsage(threadsInfo: Array<Raven.Server.Dashboard.ThreadInfo>): string {
+        const matchedThread = threadsInfo.find(x => x.Id === this.threadId())
+        return matchedThread ? `${(matchedThread.CpuUsage === 0 ? "0" : generalUtils.formatNumberToStringFixed(matchedThread.CpuUsage, 2))}%` : "N/A";
+    }
+    
+    copyStackTrace(): void {
+        copyToClipboard.copy(this.stackTrace().toString(), "Stack trace has been copied to clipboard", this.dialogContainer);
+    }
+
+    isUserCode(line: string): boolean {
+        return generalUtils.isRavenDBCode(line);
+    }
+    
+    refreshStackTrace(): JQueryPromise<threadStackTraceResponseDto> {
+        return this.loadStackTrace(this.threadId());
+    }
+}
+
+export = threadStackTrace;
