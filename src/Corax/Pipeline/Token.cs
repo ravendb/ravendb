1f693e15ca699f83f098d2bf22795845cac5b8d1
Federico Lois <federico.lois@gmail.com>
2021-11-12 14:02:36 -0300

    RavenDB-17241: Review.

--- src/Corax/Pipeline/Token.cs
+++ src/Corax/Pipeline/Token.cs
@@ -12,22 +12,17 @@ namespace Corax.Pipeline
         public TokenType Type;
     }
 
-    public static class TokenTypes
-    {
-        public const uint None = 0;
-        public const uint Word = 1;
-        public const uint Keyword = 2;
-        public const uint UserDefined = 0x8000_0000;
-        public const uint Invalid = 0xFFFF_FFFF;
-    }
-
     [Flags]
     public enum TokenType : uint
     {
-        None = TokenTypes.None,
-        Word = TokenTypes.Word,
-        Keyword = TokenTypes.Keyword,
-        UserDefined = TokenTypes.UserDefined,
-        Invalid = TokenTypes.Invalid,
+        None = 0,
+        Word = 1,
+        Keyword = 2,
+
+        Numeric = 4,        
+        Alphabetic = 8,
+        Alphanumeric = Numeric | Alphabetic,        
+        UserDefined = 0x8000_0000,
+        Invalid = 0xFFFF_FFFF,
     }
 }
