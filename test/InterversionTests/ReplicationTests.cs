9bb5523df8ca8d432ebd952278c1130fe6b03ad6
aviv <aviv@ayende.com>
2021-11-14 17:44:51 +0200

    RavenDB-17492 : add incremental-time-series interversion test

--- test/InterversionTests/ReplicationTests.cs
+++ test/InterversionTests/ReplicationTests.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Raven.Client;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Operations;
 using Raven.Client.Documents.Operations.ConnectionStrings;
 using Raven.Client.Documents.Operations.ETL;
 using Raven.Client.Documents.Operations.OngoingTasks;
 using Raven.Client.Documents.Operations.Replication;
+using Raven.Client.Documents.Operations.TimeSeries;
 using Raven.Client.Exceptions;
 using Raven.Tests.Core.Utils.Entities;
 using Tests.Infrastructure;
@@ -79,6 +81,118 @@ namespace InterversionTests
 
             Assert.True(WaitForDocument<User>(oldStore, "users/1", u => u.Name == "Egor"));
         }
+
+        [Fact]
+        public async Task IncrementalTimeSeriesWithDuplicatesShouldNotBreakReplicationToOldServer()
+        {
+            const string version = "5.2.3";
+            const string incrementalTsName = Constants.Headers.IncrementalTimeSeriesPrefix + "HeartRate";
+            const string docId = "users/1";
+            var baseline = DateTime.UtcNow;
+
+            using (var oldStore = await GetDocumentStoreAsync(version))
+            using (var storeA = GetDocumentStore())
+            using (var storeB = GetDocumentStore())
+            {
+                await SetupReplication(storeA, storeB);
+                await SetupReplication(storeB, storeA);
+
+                using (var session = storeA.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User { Name = "ayende" }, docId);
+                    await session.SaveChangesAsync();
+                }
+                Assert.True(WaitForDocument<User>(storeB, docId, u => u.Name == "ayende"));
+
+                // increment on storeA
+                using (var session = storeA.OpenSession())
+                {
+                    session.IncrementalTimeSeriesFor(docId, incrementalTsName)
+                        .Increment(baseline, 1);
+
+                    session.SaveChanges();
+                }
+                await EnsureReplicatingAsync(storeA, storeB);
+
+                // increment on storeB
+                using (var session = storeB.OpenSession())
+                {
+                    session.IncrementalTimeSeriesFor(docId, incrementalTsName)
+                        .Increment(baseline, 1);
+
+                    session.SaveChanges();
+                }
+                await EnsureReplicatingAsync(storeB, storeA);
+
+                foreach (var store in new[] { storeA, storeB })
+                {
+                    var values = store.Operations
+                        .Send(new GetTimeSeriesOperation(docId, incrementalTsName, returnFullResults: true));
+
+                    Assert.NotNull(values.TotalResults);
+                    Assert.NotNull(values.SkippedResults);
+
+                    Assert.Equal(2, values.TotalResults);
+                    Assert.Equal(1, values.SkippedResults);
+
+                    foreach (var entry in values.Entries)
+                    {
+                        Assert.NotEmpty(entry.NodeValues);
+                        Assert.Equal(2, entry.NodeValues.Count);
+
+                        foreach (var nodeValue in entry.NodeValues)
+                        {
+                            Assert.Equal(1, nodeValue.Value.Length);
+                            Assert.Equal(1, nodeValue.Value[0]);
+                        }
+                    }
+                }
+
+                // replicate to old server
+                // this replication batch should work, because the destination will append the entire ts-segment
+                await SetupReplication(storeA, oldStore);
+
+                await EnsureReplicatingAsync(storeA, oldStore);
+                await EnsureNoReplicationLoop(Server, storeA.Database);
+
+                // increment on storeA
+                using (var session = storeA.OpenSession())
+                {
+                    session.IncrementalTimeSeriesFor(docId, incrementalTsName)
+                        .Increment(baseline, 1);
+
+                    session.SaveChanges();
+                }
+
+                // now the destination can't append the entire segment and will enumerate all values
+                await EnsureReplicatingAsync(storeA, oldStore);
+                await EnsureNoReplicationLoop(Server, storeA.Database);
+            }
+        }
+
+        private static async Task<ModifyOngoingTaskResult> SetupReplication(IDocumentStore src, IDocumentStore dst)
+        {
+            var csName = $"cs-to-{dst.Database}";
+            var result = await src.Maintenance.SendAsync(new PutConnectionStringOperation<RavenConnectionString>(new RavenConnectionString
+            {
+                Name = csName,
+                Database = dst.Database,
+                TopologyDiscoveryUrls = new[]
+                {
+                    dst.Urls.First()
+                }
+            }));
+            Assert.NotNull(result.RaftCommandIndex);
+
+            var op = new UpdateExternalReplicationOperation(new ExternalReplication(dst.Database.ToLowerInvariant(), csName)
+            {
+                Name = $"ExternalReplicationTo{dst.Database}",
+                Url = dst.Urls.First()
+            });
+
+            return await src.Maintenance.SendAsync(op);
+        }
+
 
         private static async Task<ModifyOngoingTaskResult> SetupReplication(IDocumentStore store, ExternalReplicationBase watcher)
         {
