9b2b178bd9a128e6d578a4857a677738b45dd34c
Shiran Shalom <shiran@ravendb.net>
2021-11-17 13:13:22 +0200

    RavenDB-17133 - fixed PR comments

--- src/Voron/Data/Tables/Table.cs
+++ src/Voron/Data/Tables/Table.cs
@@ -11,6 +11,7 @@ using Sparrow.Server;
 using Voron.Data.BTrees;
 using Voron.Data.Fixed;
 using Voron.Data.RawData;
+using Voron.Debugging;
 using Voron.Exceptions;
 using Voron.Impl;
 using Voron.Impl.Paging;
@@ -50,7 +51,7 @@ namespace Voron.Data.Tables
         {
             get
             {
-                return _currentCompressionDictionaryId ??= 
+                return _currentCompressionDictionaryId ??=
                     _tableTree.ReadInt32(TableSchema.CurrentCompressionDictionaryIdSlice) ?? 0;
             }
             set
@@ -231,7 +232,7 @@ namespace Voron.Data.Tables
         {
             if (_tx.CachedDecompressedBuffersByStorageId.Remove(id, out var t))
             {
-               _tx.Allocator.Release(ref t);
+                _tx.Allocator.Release(ref t);
             }
         }
 
@@ -254,14 +255,14 @@ namespace Voron.Data.Tables
         }
 
         internal static ByteStringContext<ByteStringMemoryCache>.InternalScope DecompressValue(
-            Transaction tx, 
+            Transaction tx,
             byte* ptr, int size, out ByteString buffer)
         {
-            var dicId = BlittableJsonReaderBase.ReadVariableSizeIntInReverse(ptr, size-1, out var offset);
+            var dicId = BlittableJsonReaderBase.ReadVariableSizeIntInReverse(ptr, size - 1, out var offset);
             var data = new ReadOnlySpan<byte>(ptr, size - offset);
             var dictionary = tx.LowLevelTransaction.Environment.CompressionDictionariesHolder
                 .GetCompressionDictionaryFor(tx, dicId);
-    
+
             int decompressedSize = ZstdLib.GetDecompressedSize(data);
             var internalScope = tx.Allocator.Allocate(decompressedSize, out buffer);
             var actualSize = ZstdLib.Decompress(data, buffer.ToSpan(), dictionary);
@@ -529,7 +530,7 @@ namespace Voron.Data.Tables
 
                 // avoiding try / finally or using here for perf reasons
                 // if we get an error, it will get cleaned up by the context anyway
-                decompressedScope.Dispose(); 
+                decompressedScope.Dispose();
             }
 
             ActiveDataSmallSection.DeleteSection(sectionPageNumber);
@@ -600,7 +601,7 @@ namespace Voron.Data.Tables
         public long Insert(TableValueBuilder builder)
         {
             AssertWritableTable();
-            
+
             if (_schema.Compressed
                 // we may have tried compressing in the update, so no need to repeat it
                 && builder.CompressionTried == false)
@@ -671,7 +672,7 @@ namespace Voron.Data.Tables
         {
             InactiveSections.Add(_activeDataSmallSection.PageNumber);
 
-            if (TryFindMatchFromCandidateSections(itemSize, out long id)) 
+            if (TryFindMatchFromCandidateSections(itemSize, out long id))
                 return id;
 
             CreateNewActiveSection();
@@ -2031,10 +2032,12 @@ namespace Voron.Data.Tables
             _tableTree?.Dispose();
         }
 
-        public TableReport GetReport(bool includeDetails)
+        public TableReport GetReport(bool includeDetails, StorageReportGenerator generatorInstance = null)
         {
+            generatorInstance ??= new StorageReportGenerator(_tx.LowLevelTransaction);
+
             var overflowSize = _overflowPageCount * Constants.Storage.PageSize;
-            var report = new TableReport(overflowSize, overflowSize, includeDetails)
+            var report = new TableReport(overflowSize, overflowSize, includeDetails, generatorInstance)
             {
                 Name = Name.ToString(),
                 NumberOfEntries = NumberOfEntries
@@ -2072,7 +2075,7 @@ namespace Voron.Data.Tables
             var inactiveSections = InactiveSections;
             report.AddStructure(inactiveSections, includeDetails);
 
-            foreach(var section in new []{inactiveSections, activeCandidateSection})
+            foreach (var section in new[] { inactiveSections, activeCandidateSection })
             {
                 using (var it = section.Iterate())
                 {
