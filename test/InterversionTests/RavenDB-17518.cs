fe4036e409e2cd497b7d4c93ddc05d6f58f28ae5
aviv <aviv@ayende.com>
2021-11-17 12:41:26 +0200

    RavenDB-17518 : move AssertNotTimeSeriesForLegacyReplication from CanContinueBatch into AssertNoLegacyReplicationViolation, add test

--- test/InterversionTests/RavenDB-17518.cs
+++ test/InterversionTests/RavenDB-17518.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Operations.ConnectionStrings;
@@ -52,6 +53,42 @@ namespace InterversionTests
                 Assert.True(WaitForValue(() => replicationLoader.OutgoingFailureInfo.Any(ofi => ofi.Value.RetriesCount > 2), true));
                 Assert.True(replicationLoader.OutgoingFailureInfo.Any(ofi => ofi.Value.Errors.Any(x => x.GetType() == typeof(LegacyReplicationViolationException))));
                 Assert.True(replicationLoader.OutgoingFailureInfo.Any(ofi => ofi.Value.Errors.Select(x => x.Message).Any(x => x.Contains("CounterGroup"))));
+            }
+        }
+
+        [Fact]
+        public async Task ShouldNotReplicateTimeSeriesToOldServer()
+        {
+            const string docId = "users/1";
+            using (var oldStore = await GetDocumentStoreAsync("4.2.117"))
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User { Name = "ayende" }, docId);
+                    session.TimeSeriesFor(docId, "HeartRate")
+                        .Append(DateTime.UtcNow, 1);
+                    await session.SaveChangesAsync();
+                }
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    var u = await session.LoadAsync<User>(docId);
+                    session.TimeSeriesFor(u, "HeartRate")
+                        .Append(DateTime.UtcNow, 1);
+
+                    u.Name = "oren";
+
+                    await session.SaveChangesAsync();
+                }
+
+                await SetupReplication(store, oldStore);
+
+                var replicationLoader = (await GetDocumentDatabaseInstanceFor(store)).ReplicationLoader;
+                Assert.NotEmpty(replicationLoader.OutgoingFailureInfo);
+                Assert.True(WaitForValue(() => replicationLoader.OutgoingFailureInfo.Any(ofi => ofi.Value.RetriesCount > 2), true));
+                Assert.True(replicationLoader.OutgoingFailureInfo.Any(ofi => ofi.Value.Errors.Any(x => x.GetType() == typeof(LegacyReplicationViolationException))));
+                Assert.True(replicationLoader.OutgoingFailureInfo.Any(ofi => ofi.Value.Errors.Select(x => x.Message).Any(x => x.Contains("TimeSeries"))));
             }
         }
 
