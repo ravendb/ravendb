f39e49e47b4c4d823336b725540194f19867250c
egor <egor@ravendb.net>
2021-11-22 12:31:10 +0200

    RavenDB-13113: add test helper methods

--- test/FastTests/Sharding/ShardedClusterTestBase.cs
+++ test/FastTests/Sharding/ShardedClusterTestBase.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Raven.Client.Documents;
 using Raven.Client.ServerWide;
+using Raven.Client.Util;
 using Raven.Server;
+using Raven.Server.Documents;
+using Raven.Server.Documents.Replication;
+using Raven.Server.ServerWide.Context;
 using Tests.Infrastructure;
 using Xunit;
 using Xunit.Abstractions;
@@ -48,6 +53,78 @@ namespace FastTests.Sharding
             }
 
             return topology;
+        }
+
+        public new void WaitForUserToContinueTheTest(IDocumentStore documentStore, bool debug = true, string database = null, X509Certificate2 clientCert = null)
+        {
+            var db = database ?? $"{documentStore.Database}$0";
+            RavenTestBase.WaitForUserToContinueTheTest(documentStore, debug, db, clientCert);
+        }
+
+        internal static async Task<IDictionary<string, List<DocumentDatabase>>> GetShardsDocumentDatabaseInstancesFor(IDocumentStore store, List<RavenServer> Nodes, string database = null)
+        {
+            var dbs = new Dictionary<string, List<DocumentDatabase>>();
+            foreach (var server in Nodes)
+            {
+                dbs.Add(server.ServerStore.NodeTag, new List<DocumentDatabase>());
+                foreach (var task in server.ServerStore.DatabasesLandlord.TryGetOrCreateShardedResourcesStore(database ?? store.Database))
+                {
+                    var list = dbs[server.ServerStore.NodeTag];
+                    list.Add(await task);
+                    dbs[server.ServerStore.NodeTag] = list;
+                }
+            }
+
+            return dbs;
+        }
+
+        protected bool WaitForShardedChangeVectorInCluster(List<RavenServer> nodes, string database, int replicationFactor, int timeout = 15000)
+        {
+            return AsyncHelpers.RunSync(() => WaitForShardedChangeVectorInClusterAsync(nodes, database, replicationFactor, timeout));
+        }
+
+        protected async Task<bool> WaitForShardedChangeVectorInClusterAsync(List<RavenServer> nodes, string database, int replicationFactor, int timeout = 15000)
+        {
+            return await WaitForValueAsync(async () =>
+            {
+                var cvs = new Dictionary<string, List<string>>();
+                foreach (var server in nodes)
+                {
+                    foreach (var task in server.ServerStore.DatabasesLandlord.TryGetOrCreateShardedResourcesStore(database))
+                    {
+                        var storage = await task;
+                        cvs.TryAdd(storage.Name, new List<string>());
+                        using (storage.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+                        using (context.OpenReadTransaction())
+                        {
+                            var list = cvs[storage.Name];
+                            list.Add(DocumentsStorage.GetDatabaseChangeVector(context));
+                            cvs[storage.Name] = list;
+                        }
+                    }
+                }
+
+                var result = true;
+                foreach ((var _, List<string> shardCvs) in cvs)
+                {
+                    var first = shardCvs.FirstOrDefault();
+                    var stringEqual = shardCvs.Any(x => x != first) == false;
+                    if (string.IsNullOrEmpty(first))
+                    {
+                        result = stringEqual;
+                    }
+                    else
+                    {
+                        var sizeEqual = shardCvs.Any(x => x.ToChangeVectorList().Count != replicationFactor) == false;
+                        result = stringEqual && sizeEqual;
+                    }
+
+                    if (result == false)
+                        return false;
+                }
+
+                return true;
+            }, true, timeout: timeout, interval: 333);
         }
     }
 }
--- test/FastTests/Sharding/ShardedTestBase.cs
+++ test/FastTests/Sharding/ShardedTestBase.cs
@@ -1,6 +1,11 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using Raven.Client.Documents;
 using Raven.Client.ServerWide;
+using Raven.Server.Documents;
+using Raven.Server.ServerWide.Context;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -20,13 +25,56 @@ namespace FastTests.Sharding
             {
                 r.Shards = new[]
                 {
-                    new DatabaseTopology(), 
-                    new DatabaseTopology(), 
                     new DatabaseTopology(),
+                    new DatabaseTopology(),
+                    new DatabaseTopology()
                 };
             };
             //shardedOptions.RunInMemory = false;
             return GetDocumentStore(shardedOptions, caller);
+        }
+
+        public new void WaitForUserToContinueTheTest(IDocumentStore documentStore, bool debug = true, string database = null, X509Certificate2 clientCert = null)
+        {
+            var db = database ?? $"{documentStore.Database}$0";
+            RavenTestBase.WaitForUserToContinueTheTest(documentStore, debug, db, clientCert);
+        }
+
+        protected async Task<IEnumerable<DocumentDatabase>> GetShardsDocumentDatabaseInstancesFor(IDocumentStore store, string database = null)
+        {
+            var dbs = new List<DocumentDatabase>();
+            foreach (var task in Server.ServerStore.DatabasesLandlord.TryGetOrCreateShardedResourcesStore(database ?? store.Database))
+            {
+                dbs.Add(await task);
+            }
+
+            return dbs;
+        }
+
+        protected DatabasesLandlord.DatabaseSearchResult GetSharededDatabaseInstancesFor(IDocumentStore store, string database = null)
+        {
+            var res = Server.ServerStore.DatabasesLandlord.TryGetOrCreateDatabase(database ?? store.Database);
+
+            return res;
+        }
+
+        internal static bool AllShardHaveDocs(IDictionary<string, List<DocumentDatabase>> servers, long count = 1L)
+        {
+            foreach (var kvp in servers)
+            {
+                foreach (var documentDatabase in kvp.Value)
+                {
+                    using (documentDatabase.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+                    {
+                        context.OpenReadTransaction();
+                        var ids = documentDatabase.DocumentsStorage.GetNumberOfDocuments(context);
+                        if (ids < count)
+                            return false;
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
