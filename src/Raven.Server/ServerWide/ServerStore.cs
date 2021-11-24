fb455009e23b89afe0cb30448faad0a613f9063b
Karmel <karmel@ravendb.net>
2021-11-24 20:42:45 +0200

    RavenDB-16966 Allow a specific timeout for a raft command + test

--- src/Raven.Server/ServerWide/ServerStore.cs
+++ src/Raven.Server/ServerWide/ServerStore.cs
@@ -2467,13 +2467,10 @@ namespace Raven.Server.ServerWide
             var response = await SendToLeaderAsyncInternal(cmd);
 
 #if DEBUG
-
-            if (Leader.GetConvertResult(cmd) == null && // if cmd specifies a convert, it explicitly handles this
-                response.Result.ContainsBlittableObject())
+            if (response.Result.ContainsBlittableObject())
             {
                 throw new InvalidOperationException($"{nameof(ServerStore)}::{nameof(SendToLeaderAsync)}({response.Result}) should not return command results with blittable json objects. This is not supposed to happen and should be reported.");
             }
-
 #endif
 
             return response;
@@ -2481,9 +2478,9 @@ namespace Raven.Server.ServerWide
 
         //this is needed for cases where Result or any of its fields are blittable json.
         //(for example, this is needed for use with AddOrUpdateCompareExchangeCommand, since it returns BlittableJsonReaderObject as result)
-        public Task<(long Index, object Result)> SendToLeaderAsync(TransactionOperationContext context, CommandBase cmd)
+        public Task<(long Index, object Result)> SendToLeaderAsync(JsonOperationContext context, CommandBase cmd)
         {
-            return SendToLeaderAsyncInternal(context, cmd);
+            return SendToLeaderAsyncInternal(cmd);
         }
 
         public DynamicJsonArray GetClusterErrors()
@@ -3138,6 +3135,7 @@ namespace Raven.Server.ServerWide
         {
             internal Action BeforePutLicenseCommandHandledInOnValueChanged;
             internal bool StopIndex;
+            internal Action<CompareExchangeCommandBase> ModifyCompareExchangeTimeout;
         }
     }
 }
