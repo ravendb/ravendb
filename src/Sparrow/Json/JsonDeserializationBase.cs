0542b727f1dbd5f329783bd1adca212ab4c12ea6
egor <egor@ravendb.net>
2021-12-23 17:15:10 +0200

    RavenDB-17117: on deserialization handle ToList on array of lazy string value

--- src/Sparrow/Json/JsonDeserializationBase.cs
+++ src/Sparrow/Json/JsonDeserializationBase.cs
@@ -707,7 +707,7 @@ namespace Sparrow.Json
             if (json.TryGet(name, out BlittableJsonReaderArray array) == false || array == null)
                 return list;
 
-            foreach (BlittableJsonReaderObject item in array)
+            foreach (object item in array)
             {
                 if (item == null)
                 {
@@ -715,7 +715,20 @@ namespace Sparrow.Json
                     continue;
                 }
 
-                list.Add(converter(item));
+                if (item is BlittableJsonReaderObject bjro)
+                {
+                    list.Add(converter(bjro));
+                    continue;
+                }
+
+                object copy = item;
+
+                if (item is LazyStringValue lsv && IsNumeric<T>())
+                {
+                    copy = new LazyNumberValue(lsv);
+                }
+
+                list.Add((T)Convert.ChangeType(copy, typeof(T)));
             }
 
             return list;
@@ -743,7 +756,7 @@ namespace Sparrow.Json
 
                 object copy = item;
 
-                if (item is LazyStringValue lsv && IsNumeric())
+                if (item is LazyStringValue lsv && IsNumeric<T>())
                 {
                     copy = new LazyNumberValue(lsv);
                 }
@@ -752,21 +765,21 @@ namespace Sparrow.Json
             }
 
             return list.ToArray();
+        }
 
-            bool IsNumeric()
-            {
-                return typeof(T) == typeof(double) ||
-                    typeof(T) == typeof(decimal) ||
-                    typeof(T) == typeof(float) ||
-                    typeof(T) == typeof(int) ||
-                    typeof(T) == typeof(uint) ||
-                    typeof(T) == typeof(long) ||
-                    typeof(T) == typeof(ulong) ||
-                    typeof(T) == typeof(short) ||
-                    typeof(T) == typeof(ushort) ||
-                    typeof(T) == typeof(byte) ||
-                    typeof(T) == typeof(sbyte);
-            }
+        private static bool IsNumeric<T>()
+        {
+            return typeof(T) == typeof(double) ||
+                   typeof(T) == typeof(decimal) ||
+                   typeof(T) == typeof(float) ||
+                   typeof(T) == typeof(int) ||
+                   typeof(T) == typeof(uint) ||
+                   typeof(T) == typeof(long) ||
+                   typeof(T) == typeof(ulong) ||
+                   typeof(T) == typeof(short) ||
+                   typeof(T) == typeof(ushort) ||
+                   typeof(T) == typeof(byte) ||
+                   typeof(T) == typeof(sbyte);
         }
 
         private static bool TryGetTimeSpan(BlittableJsonReaderObject json, string propertyName, out TimeSpan timeSpan)
