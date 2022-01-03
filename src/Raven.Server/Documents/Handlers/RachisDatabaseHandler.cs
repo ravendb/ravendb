a66cab4bf6c1df52930ccec88cdd32aa05175764
Grisha Kotler <grisha@ayende.com>
2022-01-03 18:16:36 +0200

    RavenDB-13107 - fix deploying of a static index

---
+++ src/Raven.Server/Documents/Handlers/RachisDatabaseHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Raven.Server.Json;
+using Raven.Server.Routing;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Handlers
+{
+    public class RachisDatabaseHandler : DatabaseRequestHandler
+    {
+        [RavenAction("/databases/*/wait-for-indexes-notification", "POST", AuthorizationStatus.Operator)]
+        public async Task WaitForIndexesNotification()
+        {
+            using (ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext context))
+            {
+                var blittable = await context.ReadForMemoryAsync(RequestBodyStream(), "raft-index-ids");
+                var commands = JsonDeserializationServer.WaitForCommands(blittable);
+
+                foreach (var index in commands.RaftIndexIds)
+                {
+                    await Database.RachisLogIndexNotifications.WaitForIndexNotification(index, HttpContext.RequestAborted);
+                }
+            }
+        }
+    }
+}
