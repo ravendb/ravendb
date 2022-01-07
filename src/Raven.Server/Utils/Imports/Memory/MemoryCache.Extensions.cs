5b54e6d7611e9898ea6215c6df24dc33b32d391c
Pawel Pekrol <ppekrol@ravendb.net>
2022-01-07 11:50:35 +0100

    RavenDB-17373 updated MemoryCache with latest sources

---
+++ src/Raven.Server/Utils/Imports/Memory/MemoryCache.Extensions.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Raven.Server.Utils.Imports.Memory
+{
+    public partial class MemoryCache
+    {
+        public IEnumerable<KeyValuePair<object, object>> EntriesForDebug => _coherentState._entries.Select(kvp => new KeyValuePair<object, object>(kvp.Key, kvp.Value.Value));
+    }
+}
