f443f230f86a070a917aad42f76d428703688f6e
Grisha Kotler <grisha@ayende.com>
2022-01-11 13:11:40 +0200

    RavenDB-13107 - added ShardedStatsHandler - WaitForIndexing for sharded environment

---
+++ src/Raven.Server/Documents/ShardedHandlers/ShardedStatsHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Raven.Client.Documents.Operations;
+using Raven.Server.Documents.Sharding;
+using Raven.Server.Json;
+using Raven.Server.Routing;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.ShardedHandlers
+{
+    public class ShardedStatsHandler : ShardedRequestHandler
+    {
+        [RavenShardedAction("/databases/*/stats", "GET")]
+        public async Task Stats()
+        {
+            var shard = GetLongQueryString("shard", false);
+
+            if (shard == null)
+                throw new InvalidOperationException("In a sharded environment you must provide a shard id");
+
+            if (ShardedContext.RequestExecutors.Length <= shard)
+                throw new InvalidOperationException($"Non existing shard id, {shard}");
+
+            using (ContextPool.AllocateOperationContext(out JsonOperationContext context))
+            {
+                var executor = ShardedContext.RequestExecutors[shard.Value];
+                var command = new GetStatisticsOperation.GetStatisticsCommand(null, null, (int)shard.Value);
+                await executor.ExecuteAsync(command, context);
+
+                await using (var writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
+                    writer.WriteDatabaseStatistics(context, command.Result);
+            }
+        }
+    }
+}
