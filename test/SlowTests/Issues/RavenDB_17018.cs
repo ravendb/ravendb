9d9bb8be9eecaae94971886476626b54d5458a42
Gracjan Sadowicz <gracjan@ravendb.net>
2022-01-17 14:52:15 +0100

    RavenDB-17789 Check all usages of UpdatePaging in tests

--- test/SlowTests/Issues/RavenDB_17018.cs
+++ test/SlowTests/Issues/RavenDB_17018.cs
@@ -46,8 +46,9 @@ namespace SlowTests.Issues
                 }
 
                 var database = await GetDatabase(store.Database);
-                database.NotificationCenter.Paging.UpdatePaging(null);
-
+                var outcome = database.NotificationCenter.Paging.UpdatePagingInternal(null, out string reason);
+                Assert.True(outcome, reason);
+                
                 using (database.NotificationCenter.GetStored(out var actions))
                 {
                     var action = actions.First();
@@ -85,7 +86,10 @@ namespace SlowTests.Issues
                 }
 
                 var database = await GetDatabase(store.Database);
-                database.NotificationCenter.Paging.UpdatePaging(null);
+
+                var outcome = database.NotificationCenter.Paging.UpdatePagingInternal(null, out string reason);
+                Assert.True(outcome, reason);
+
 
                 using (database.NotificationCenter.GetStored(out var actions))
                 {
@@ -129,8 +133,10 @@ namespace SlowTests.Issues
                 }
 
                 var database = await GetDatabase(store.Database);
-                database.NotificationCenter.Paging.UpdatePaging(null);
-
+                
+                var outcome = database.NotificationCenter.Paging.UpdatePagingInternal(null, out string reason);
+                Assert.True(outcome, reason);
+                
                 using (database.NotificationCenter.GetStored(out var actions))
                 {
                     var action = actions.First();
@@ -241,7 +247,8 @@ namespace SlowTests.Issues
                 }
 
                 var database = await GetDatabase(store.Database);
-                database.NotificationCenter.Paging.UpdatePaging(null);
+                var outcome = database.NotificationCenter.Paging.UpdatePagingInternal(null, out string reason);
+                Assert.True(outcome, reason);
 
                 using (database.NotificationCenter.GetStored(out var actions))
                 {
