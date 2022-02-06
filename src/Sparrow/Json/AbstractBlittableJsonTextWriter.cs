a42d100555279098aaed09611e6ce495eb0b5091
efrat@ayende.com <efrat@ayende.com>
2022-02-06 12:14:10 +0200

    RavenDB-16274 New extension method: WriteNextOperationIdAndNodeTag

--- src/Sparrow/Json/AbstractBlittableJsonTextWriter.cs
+++ src/Sparrow/Json/AbstractBlittableJsonTextWriter.cs
@@ -758,5 +758,16 @@ namespace Sparrow.Json
                 FlushInternal();
             }
         }
+
+        public void WriteNextOperationIdAndNodeTag(long operationId, string nodeTag)
+        {
+            WriteStartObject();
+            WritePropertyName("Id");
+            WriteInteger(operationId);
+            WriteComma();
+            WritePropertyName("NodeTag");
+            WriteString(nodeTag);
+            WriteEndObject();
+        }
     }
 }
