ef11b7a326d25cfc79ad033e0dbe823aa5baa492
Pawel Pekrol <ppekrol@ravendb.net>
2022-02-08 15:51:39 +0100

    RavenDB-18000 added DevelopmentHelper

---
+++ src/Sparrow/Utils/DevelopmentHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace Sparrow.Utils;
+
+internal static class DevelopmentHelper
+{
+    internal static void ToDo(Feature feature, TeamMember member, Severity severity, string message)
+    {
+        // nothing to do here
+    }
+
+    internal enum Feature
+    {
+        Sharding
+    }
+
+    internal enum TeamMember
+    {
+        Karmel,
+        Aviv,
+        Pawel,
+        Arek,
+        Grisha
+    }
+
+    internal enum Severity
+    {
+        Minor,
+        Normal,
+        Major,
+        Critical
+    }
+}
