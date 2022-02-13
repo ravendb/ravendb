f3901a9cef9a914efc6cb2062dda14d21a67dca4
efrat@ayende.com <efrat@ayende.com>
2022-02-13 14:11:37 +0200

    RavenDB-16274 PR fix

---
+++ src/Raven.Server/Documents/ShardedHandlers/ShardedCommands/ShardedExportCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Raven.Client.Http;
+using Raven.Client.Json;
+using Raven.Server.Documents.Sharding;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.ShardedHandlers.ShardedCommands
+{
+    public class ShardedExportCommand : ShardedStreamCommand
+    {
+        internal long OperationID;
+        public ShardedExportCommand(ShardedRequestHandler handler, Operations.Operations operation, Func<Stream, Task> handleStreamResponse, BlittableJsonReaderObject content) : 
+            base(handler, handleStreamResponse, content)
+        {
+            OperationID = operation.GetNextOperationId();
+        }
+
+        public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+        {
+            Url = $"/smuggler/export?operationId={OperationID}";
+            return base.CreateRequest(ctx, node, out url);
+        }
+    }
+}
