548946ecdefc995d8723eb8f2db7428c990a425f
Grzegorz Lachowski <gregolsky@ravendb.net>
2022-02-16 10:54:07 +0100

    RavenDB-18056 return timeticks zero for negative timespans

--- src/Raven.Server/Monitoring/Snmp/SnmpValuesHelper.cs
+++ src/Raven.Server/Monitoring/Snmp/SnmpValuesHelper.cs
@@ -5,14 +5,17 @@ namespace Raven.Server.Monitoring.Snmp
 {
     public static class SnmpValuesHelper
     {
-        public static TimeTicks TimeTicksMax => new TimeTicks(uint.MaxValue);
+        public static TimeTicks TimeTicksMax = new TimeTicks(uint.MaxValue);
 
         public static TimeTicks TimeTicksZero = new TimeTicks(0);
 
-        public static TimeSpan TimeSpanSnmpMax => TimeTicksMax.ToTimeSpan();
+        public static TimeSpan TimeSpanSnmpMax = TimeTicksMax.ToTimeSpan();
         
         public static TimeTicks TimeSpanToTimeTicks(TimeSpan timeSpan)
         {
+            if (timeSpan <= TimeSpan.Zero)
+                return TimeTicksZero;
+            
             if (timeSpan > TimeSpanSnmpMax)
                 return TimeTicksMax;
                     
