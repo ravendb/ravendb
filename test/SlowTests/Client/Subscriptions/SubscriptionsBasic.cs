7a8d4bf7c31458579b8a53b8b34fd3189b044fef
egor <egor@ravendb.net>
2022-02-24 11:30:39 +0200

    RavenDB-17116: add test to check that Waiting Subscription Connection is Registered

--- test/SlowTests/Client/Subscriptions/SubscriptionsBasic.cs
+++ test/SlowTests/Client/Subscriptions/SubscriptionsBasic.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using FastTests.Client.Subscriptions;
+using Raven.Client.Documents;
 using Raven.Client.Documents.Operations.Backups;
 using Raven.Client.Documents.Smuggler;
 using Raven.Client.Documents.Subscriptions;
@@ -13,6 +15,7 @@ using Raven.Client.Exceptions.Database;
 using Raven.Client.Exceptions.Documents.Subscriptions;
 using Raven.Client.Extensions;
 using Raven.Client.ServerWide.Operations;
+using Raven.Server.Documents;
 using Raven.Server.Documents.Replication;
 using Raven.Server.Documents.Subscriptions;
 using Raven.Server.ServerWide.Context;
@@ -551,6 +554,93 @@ namespace SlowTests.Client.Subscriptions
                 Assert.True(await worker1Retry.WaitAsync(_reasonableWaitTime));
                 Assert.False(t1.IsFaulted);
                 Assert.False(t2.IsFaulted);
+            }
+        }
+
+        [Fact]
+        public async Task WaitingSubscriptionShouldBeRegisteredInSubscriptionConnections()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var subsId = await store.Subscriptions.CreateAsync(new SubscriptionCreationOptions { Query = "from Users", Name = "Subscription0" });
+
+                List<Task> workerTasks = new List<Task>();
+                var finishedWorkersCde = new CountdownEvent(2);
+                var mreAck1 = new AsyncManualResetEvent();
+                var mreAck2 = new AsyncManualResetEvent();
+                using var subsWorker1 = store.Subscriptions.GetSubscriptionWorker(new SubscriptionWorkerOptions(subsId)
+                {
+                    Strategy = SubscriptionOpeningStrategy.WaitForFree
+                });
+                subsWorker1.AfterAcknowledgment += _ =>
+                {
+                    mreAck1.Set();
+                    return Task.CompletedTask;
+                };
+                var t1 = subsWorker1.Run(_ => {  }).ContinueWith(res =>
+                {
+                    finishedWorkersCde.Signal();
+                });
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User());
+                    await session.SaveChangesAsync();
+                }
+
+                Assert.True(await mreAck1.WaitAsync(_reasonableWaitTime));
+
+                using var subsWorker2 = store.Subscriptions.GetSubscriptionWorker(new SubscriptionWorkerOptions(subsId)
+                {
+                    Strategy = SubscriptionOpeningStrategy.WaitForFree
+                });
+                subsWorker2.AfterAcknowledgment += _ =>
+                {
+                    mreAck2.Set();
+                    return Task.CompletedTask;
+                };
+                var t2 = subsWorker2.Run(_ => { }).ContinueWith(res =>
+                {
+                    finishedWorkersCde.Signal();
+                });
+
+                // wait for 2nd worker to connect
+                await Task.Delay(3000);
+
+                workerTasks.Add(t1);
+                workerTasks.Add(t2);
+
+                await AssertRunningSubscriptionAndDrop(store);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User());
+                    await session.SaveChangesAsync();
+                }
+                // waiting subscription process document
+                Assert.True(await mreAck2.WaitAsync(_reasonableWaitTime));
+
+                await AssertRunningSubscriptionAndDrop(store);
+
+                // both workers should be disconnected
+                Assert.True(finishedWorkersCde.Wait(_reasonableWaitTime));
+                Assert.All(workerTasks, task => Assert.True(task.IsCompleted));
+            }
+        }
+
+        private async Task AssertRunningSubscriptionAndDrop(DocumentStore store)
+        {
+            DocumentDatabase db = await Server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(store.Database);
+            using (Server.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext context))
+            using (context.OpenReadTransaction())
+            {
+                var name = $"Subscription0";
+                var subscription = db
+                    .SubscriptionStorage
+                    .GetRunningSubscription(context, null, name, false);
+                Assert.NotNull(subscription);
+                db.SubscriptionStorage.DropSubscriptionConnection(subscription.SubscriptionId,
+                    new SubscriptionClosedException("Dropped by Test"));
             }
         }
     }
