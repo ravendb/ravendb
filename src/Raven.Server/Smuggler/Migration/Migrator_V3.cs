c983adcf0f8135f6f82aced65e1583fd5bd1c2a8
Karmel <karmel@ravendb.net>
2022-02-24 04:20:30 +0200

    RavenDB-16274 add using to Migrator V2 & V3

--- src/Raven.Server/Smuggler/Migration/Migrator_V3.cs
+++ src/Raven.Server/Smuggler/Migration/Migrator_V3.cs
@@ -122,7 +122,7 @@ namespace Raven.Server.Smuggler.Migration
                 SkipRevisionCreation = true
             };
 
-            await destination.InitializeAsync(options, parametersResult, _buildVersion);
+            await using (await destination.InitializeAsync(options, parametersResult, buildVersion: default))
             using (Parameters.Database.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext transactionOperationContext))
             using (Parameters.Database.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
             await using (var documentActions = destination.Documents())
