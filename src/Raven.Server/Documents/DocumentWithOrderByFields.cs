a7e490e46ef3b5897002bb70ba56154e8b4a039a
Grisha Kotler <grisha@ayende.com>
2022-03-01 17:44:34 +0200

    RavenDB-13107 - add DocumentWithOrderByFields

---
+++ src/Raven.Server/Documents/DocumentWithOrderByFields.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Raven.Server.Documents;
+
+public class DocumentWithOrderByFields : Document
+{
+    public List<(string Field, string Value)> OrderByFields;
+
+    public void AddOrderByField(string fieldName, string value)
+    {
+        OrderByFields ??= new List<(string Field, string Value)>();
+        OrderByFields.Add((fieldName, value));
+    }
+
+    public static DocumentWithOrderByFields From(Document doc)
+    {
+        return new DocumentWithOrderByFields
+        {
+            Etag = doc.Etag,
+            StorageId = doc.StorageId,
+            IndexScore = doc.IndexScore,
+            Distance = doc.Distance,
+            ChangeVector = doc.ChangeVector,
+            LastModified = doc.LastModified,
+            Flags = doc.Flags,
+            NonPersistentFlags = doc.NonPersistentFlags,
+            TransactionMarker = doc.TransactionMarker,
+            Id = doc.Id,
+            LowerId = doc.LowerId,
+            Data = doc.Data
+        };
+    }
+}
