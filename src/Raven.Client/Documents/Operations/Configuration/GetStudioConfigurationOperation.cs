d2d7d7c414985241f212d8399bd55e373acface9
Stav <stav@ravendb.net>
2022-03-02 13:40:06 +0200

    RavenDB-18012 - [Sharding] Allow to get studio configuration (shard wide)

---
+++ src/Raven.Client/Documents/Operations/Configuration/GetStudioConfigurationOperation.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http;
+using Raven.Client.Documents.Conventions;
+using Raven.Client.Http;
+using Raven.Client.Json.Serialization;
+using Sparrow.Json;
+
+namespace Raven.Client.Documents.Operations.Configuration
+{
+    public class GetStudioConfigurationOperation : IMaintenanceOperation<StudioConfiguration>
+    {
+        public RavenCommand<StudioConfiguration> GetCommand(DocumentConventions conventions, JsonOperationContext context)
+        {
+            return new GetStudioConfigurationCommand();
+        }
+
+        internal class GetStudioConfigurationCommand : RavenCommand<StudioConfiguration>
+        {
+            public override bool IsReadRequest => false;
+
+
+            public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+            {
+                url = $"{node.Url}/databases/{node.Database}/configuration/studio";
+
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get
+                };
+
+                return request;
+            }
+
+            public override void SetResponse(JsonOperationContext context, BlittableJsonReaderObject response, bool fromCache)
+            {
+                if (response == null)
+                    return;
+
+                Result = JsonDeserializationClient.StudioConfiguration(response);
+            }
+        }
+    }
+}
---
+++ src/Raven.Server/Web/Studio/Sharding/Processors/AbstractHandlerProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Http;
+using Sparrow.Json;
+
+namespace Raven.Server.Web.Studio.Sharding.Processors
+{
+    internal abstract class AbstractHandlerProcessor<TRequestHandler, TOperationContext> : IDisposable
+            where TRequestHandler : RequestHandler
+            where TOperationContext : JsonOperationContext
+    {
+        protected readonly TRequestHandler RequestHandler;
+
+        protected readonly HttpContext HttpContext;
+
+        protected readonly JsonContextPoolBase<TOperationContext> ContextPool;
+
+        protected AbstractHandlerProcessor([NotNull] TRequestHandler requestHandler, [NotNull] JsonContextPoolBase<TOperationContext> contextPool)
+        {
+            RequestHandler = requestHandler ?? throw new ArgumentNullException(nameof(requestHandler));
+            HttpContext = requestHandler.HttpContext;
+            ContextPool = contextPool;
+        }
+
+        public virtual void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+
+}
