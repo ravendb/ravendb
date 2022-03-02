bb7de9f9656cb0bdf51348207ec8b1173fdf7560
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-02 20:17:36 +0100

    RavenDB-18128 cleanup

---
+++ test/Tests.Infrastructure/MultiplatformTheoryAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Tests.Infrastructure;
+
+public class MultiplatformTheoryAttribute : TheoryAttribute
+{
+    private readonly RavenPlatform _platform;
+    private readonly RavenArchitecture _architecture;
+
+    public MultiplatformTheoryAttribute(RavenPlatform platform = RavenPlatform.All)
+        : this(platform, RavenArchitecture.All)
+    {
+    }
+
+    public MultiplatformTheoryAttribute(RavenArchitecture architecture = RavenArchitecture.All)
+        : this(RavenPlatform.All, architecture)
+    {
+    }
+
+    public MultiplatformTheoryAttribute(RavenPlatform platform = RavenPlatform.All, RavenArchitecture architecture = RavenArchitecture.All)
+    {
+        _platform = platform;
+        _architecture = architecture;
+    }
+
+    public override string Skip => MultiplatformFactAttribute.ShouldSkip(_platform, _architecture);
+}
