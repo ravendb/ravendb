4156fb2d147e61d79479d30a3732c9b60cd3547e
Marcin Lewandowski <marcin@ravendb.net>
2022-03-08 09:10:37 +0100

    RavenDB-17107 Sharding - Studio work

--- src/Raven.Studio/typescript/viewmodels/common/sharding/shardSelector.ts
+++ src/Raven.Studio/typescript/viewmodels/common/sharding/shardSelector.ts
@@ -1,13 +1,39 @@
 ﻿import shard from "models/resources/shard";
 import dialogViewModelBase from "viewmodels/dialogViewModelBase";
+import clusterTopologyManager from "common/shell/clusterTopologyManager";
+import activeDatabaseTracker from "common/shell/activeDatabaseTracker";
+import database from "models/resources/database";
+
+
+function transformUrlForNode(db: database, nodeTag: string, hash = window.location.hash) {
+    const currentDatabase = activeDatabaseTracker.default.database();
+    const clusterNode = clusterTopologyManager.default.getClusterNodeByTag(nodeTag);
+    const serverUrl = clusterNode.serverUrl();
+
+    if (hash.includes("database=") && currentDatabase) {
+        const existingQueryString = "database=" + encodeURIComponent(currentDatabase.name);
+        const newQueryString = "database=" + encodeURIComponent(db.name);
+        hash = hash.replace(existingQueryString, newQueryString);
+    }
+
+    if (!serverUrl) {
+        return "#";
+    }
+
+    return serverUrl + "/studio/index.html" + hash;
+}
 
 class shardSelector extends dialogViewModelBase {
 
-    private readonly currentNode: string;
+    private readonly currentNode: KnockoutObservable<string>;
     private readonly shards: shard[];
-    private readonly onSelected: (shard: shard, nodeTag: string, pin: boolean) => void;
+    private readonly onLocalShardSelected: (shard: shard, pin: boolean) => void;
+
+    targetIsLocal: KnockoutComputed<boolean>;
+    targetHref: KnockoutComputed<string>;
+    forcePin: KnockoutComputed<boolean>;
     
-    private readonly shardNodes: KnockoutComputed<string[]>;
+    private shardNodes: KnockoutComputed<string[]>;
     
     form = {
         shard: ko.observable<shard>(),
@@ -17,40 +43,92 @@ class shardSelector extends dialogViewModelBase {
     
     view = require("views/common/sharding/shardSelector.html");
     
-    constructor(shards: shard[], onSelected: (shard: shard, nodeTag: string, pin: boolean) => void) {
+    constructor(shards: shard[], onLocalShardSelected: (shard: shard, pin: boolean) => void) {
         super();
         
+        this.currentNode = clusterTopologyManager.default.localNodeTag;
+        
         this.shards = shards;
-        this.onSelected = onSelected;
+        this.onLocalShardSelected = onLocalShardSelected;
         
-        this.form.shard(shards[0]); //TODO: try default to current value, not first one 
-        this.form.node("A"); //TODO: 
-        this.form.pin(this.activeDatabase() instanceof shard);
+        this.initForm();
+        this.initObservables();
         
+        this.bindToCurrentInstance("changeShard", "changeNodeTag", "shardSelected");
+    }
+    
+    private initForm() {
+        const activeDatabase = this.activeDatabase();
+        const currentShard: shard = activeDatabase instanceof shard ? activeDatabase : null;
+        this.form.shard(currentShard || this.shards[0]);
+        this.maybeChangeNodeTag();
+        this.form.pin(!!currentShard);
+    }
+    
+    private initObservables() {
         this.shardNodes = ko.pureComputed(() => {
             const currentShard = this.form.shard();
             if (!currentShard) {
                 return [];
             }
-            
+
             return currentShard.nodes();
         });
+
+        this.targetIsLocal = ko.pureComputed(() => {
+            const currentNode = this.currentNode();
+            const targetNode = this.form.node();
+            return currentNode === targetNode;
+        });
+
+        this.targetHref = ko.pureComputed(() => {
+            const db = this.form.shard();
+            const nodeTag = this.form.node();
+            return transformUrlForNode(db, nodeTag);
+        });
         
-        this.bindToCurrentInstance("changeShard", "changeNodeTag", "shardSelected");
+        this.forcePin = ko.pureComputed(() => !this.targetIsLocal());
     }
+
+    
 
     changeShard(shard: shard) {
         this.form.shard(shard);
+        
+        this.maybeChangeNodeTag();
     }
     
-    changeNodeTag(tag: string) {
+    private maybeChangeNodeTag() {
+        const currentServerNodeTag = this.currentNode();
+
+        // try to find and select current currentServerNodeTag (if available) - to prefer local reads
+        const canSelectCurrentNode = !!this.form.shard().nodes().find(x => x === currentServerNodeTag);
+        if (canSelectCurrentNode) {
+            this.form.node(currentServerNodeTag);
+        } else {
+            const canUseCurrentNode = !!this.form.shard().nodes().find(x => x === this.form.node());
+            if (canUseCurrentNode) {
+                // nothing to be done - don't touch node tag
+            } else {
+                // keep UI in sync - select first from the list
+                this.form.node(this.form.shard().nodes()[0]);
+            }
+        }
+    }
+    
+    changeNodeTag(tag: string): void {
         this.form.node(tag);
     }
 
-    shardSelected() {
-        this.onSelected(this.form.shard(), this.form.node(), this.form.pin());
+    shardSelected(): boolean {
+        if (this.targetIsLocal()) {
+            this.onLocalShardSelected(this.form.shard(), this.forcePin() ? true : this.form.pin());
+            return false;
+        } else {
+            // go to new tab - use build-in a href handling
+            return true;
+        }
     }
-    
 }
 
 export = shardSelector;
