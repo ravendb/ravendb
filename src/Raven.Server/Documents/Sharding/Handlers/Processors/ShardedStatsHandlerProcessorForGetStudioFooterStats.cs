7853c8e30937d2ba19fbcb313d382caf0f68af28
Stav <stav@ravendb.net>
2022-03-08 13:15:01 +0200

    RavenDB-18018 - [Sharding] Get studio footer stats (shard wide)

---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/ShardedStatsHandlerProcessorForGetStudioFooterStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Server.Documents.Handlers.Processors;
+using Raven.Server.Documents.Sharding;
+using Raven.Server.Documents.Studio;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.ShardedHandlers.Processors
+{
+    internal class ShardedStatsHandlerProcessorForGetStudioFooterStats : AbstractStatsHandlerProcessorForGetStudioFooterStats<ShardedRequestHandler, TransactionOperationContext>
+    {
+        public ShardedStatsHandlerProcessorForGetStudioFooterStats([NotNull] ShardedRequestHandler requestHandler) : base(requestHandler, requestHandler.ContextPool)
+        {
+        }
+        
+        protected override async ValueTask<FooterStatistics> GetFooterStatistics()
+        {
+            var op = new ShardedStudioStatsHandler.ShardedGetStudioFooterStatsOperation();
+            var stats = await RequestHandler.ShardExecutor.ExecuteParallelForAllAsync(op);
+            stats.CountOfIndexes = RequestHandler.ShardedContext.DatabaseRecord.Indexes.Count;
+            //missing CountOfStaleIndexes and CountOfIndexingErrors fields
+            return stats;
+        }
+    }
+}
