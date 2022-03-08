7883a5d5948bff6aa458fd39d04b964325f606fd
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-08 12:21:54 +0100

    RavenDB-18012 refactoring

---
+++ src/Raven.Server/Documents/Handlers/Processors/ConfigurationHandlerProcessorForGetStudioConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using JetBrains.Annotations;
+using Raven.Client.Documents.Operations.Configuration;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Handlers.Processors;
+
+internal class ConfigurationHandlerProcessorForGetStudioConfiguration : AbstractStudioConfigurationHandlerProcessor<DatabaseRequestHandler, DocumentsOperationContext>
+{
+    private readonly DocumentDatabase _database;
+
+    public ConfigurationHandlerProcessorForGetStudioConfiguration(DatabaseRequestHandler requestHandler, [NotNull] DocumentDatabase database)
+        : base(requestHandler, database.DocumentsStorage.ContextPool)
+    {
+        _database = database ?? throw new ArgumentNullException(nameof(database));
+    }
+
+    protected override StudioConfiguration GetStudioConfiguration()
+    {
+        return _database.StudioConfiguration;
+    }
+}
