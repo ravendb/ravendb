6e6339dd64f8618d7fc366a6bcbe1c0cbcd8d635
shiranshalom <shiran@ravendb.net>
2022-03-09 14:39:19 +0200

    RavenDB-18008 - adding processors

---
+++ src/Raven.Server/Documents/Handlers/Processors/StatsHandlerProcessorForGetDetailedDatabaseStatistics.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using JetBrains.Annotations;
+using Raven.Client.Documents.Operations;
+using Raven.Server.Documents.Indexes;
+using Raven.Server.Utils;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Handlers.Processors
+{
+    internal class StatsHandlerProcessorForGetDetailedDatabaseStatistics : AbstractStatsHandlerProcessorForGetDetailedDatabaseStatistics<DatabaseRequestHandler>
+    {
+        private readonly string _databaseName;
+
+        private QueryOperationContext _context;
+
+        private readonly DetailedDatabaseStatistics _databaseStatistics;
+
+        public StatsHandlerProcessorForGetDetailedDatabaseStatistics([NotNull] DatabaseRequestHandler requestHandler, string databaseName, QueryOperationContext context, DetailedDatabaseStatistics databaseStatistics) : base(requestHandler)
+        {
+            _context = context;
+            _databaseName = databaseName ?? throw new ArgumentNullException(nameof(databaseName));
+            _databaseStatistics = databaseStatistics ?? throw new ArgumentNullException(nameof(databaseStatistics));
+        }
+
+        protected override void Initialize()
+        {
+        }
+
+        protected override JsonOperationContext GetContext()
+        {
+            return _context.Documents;
+        }
+
+        protected override string GetDatabaseName()
+        {
+            return ShardHelper.ToDatabaseName(_databaseName);
+        }
+
+        protected override DetailedDatabaseStatistics GetDatabaseStatistics()
+        {
+            return _databaseStatistics;
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+
+            _context?.Dispose();
+            _context = null;
+        }
+    }
+}
---
+++ src/Raven.Server/Documents/Sharding/Processors/ShardedStatsHandlerProcessorForGetDetailedDatabaseStatistics.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using JetBrains.Annotations;
+using Raven.Client.Documents.Operations;
+using Raven.Server.Documents.Handlers.Processors;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Sharding.Processors
+{
+    internal class ShardedStatsHandlerProcessorForGetDetailedDatabaseStatistics : AbstractStatsHandlerProcessorForGetDetailedDatabaseStatistics<ShardedRequestHandler>
+    {
+        private readonly string _databaseName;
+
+        private JsonOperationContext _context;
+
+        private readonly DetailedDatabaseStatistics _databaseStatistics;
+
+        private IDisposable _releaseContext;
+
+        public ShardedStatsHandlerProcessorForGetDetailedDatabaseStatistics([NotNull] ShardedRequestHandler requestHandler, string databaseName, DetailedDatabaseStatistics databaseStatistics) : base(requestHandler)
+        {
+            _databaseName = databaseName ?? throw new ArgumentNullException(nameof(databaseName));
+            _databaseStatistics = databaseStatistics ?? throw new ArgumentNullException(nameof(databaseStatistics));
+        }
+
+        protected override void Initialize()
+        {
+            _releaseContext = RequestHandler.ContextPool.AllocateOperationContext(out _context);
+        }
+
+        protected override JsonOperationContext GetContext()
+        {
+            return _context;
+        }
+
+        protected override string GetDatabaseName()
+        {
+            return _databaseName;
+        }
+
+        protected override DetailedDatabaseStatistics GetDatabaseStatistics()
+        {
+            return _databaseStatistics;
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+
+            _releaseContext?.Dispose();
+            _releaseContext = null;
+        }
+    }
+}
