bbd392133936fe5de0bf7d3c26f8e23c508cc6fe
shiranshalom <shiran@ravendb.net>
2022-03-10 11:25:39 +0200

    RavenDB-18008 - adding test (RavenDB-18172)

--- test/SlowTests/Core/Commands/Other.cs
+++ test/SlowTests/Core/Commands/Other.cs
@@ -4,11 +4,14 @@
 //  </copyright>
 // -----------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using Xunit.Abstractions;
 
 using FastTests;
+using FastTests.Graph;
 using Raven.Client.Documents.Operations;
+using Raven.Client.Documents.Operations.CompareExchange;
 using Raven.Client.ServerWide.Operations;
 using Tests.Infrastructure;
 using Xunit;
@@ -45,6 +48,79 @@ namespace SlowTests.Core.Commands
                 Assert.Equal(0, databaseStatistics.CountOfDocuments);
             }
         }
+
+        [Theory]
+        [RavenData(DatabaseMode = RavenDatabaseMode.All)]
+        public async Task CanGetDatabaseStatistics(Options options)
+        {
+            using (var store = GetDocumentStore(options))
+            {
+                using (var session = store.OpenAsyncSession())
+                {
+                    for (var i = 0; i < 10; i++)
+                    {
+                        var id = $"foo/bar/{i}";
+                        var user = new User
+                        {
+                            Name = "Original shard"
+                        };
+                        await session.StoreAsync(user, id);
+                        await session.SaveChangesAsync();
+
+                        var baseline = DateTime.Today;
+                        var ts = session.TimeSeriesFor(id, "HeartRates");
+                        var cf = session.CountersFor(id);
+                        for (var j = 0; j < 20; j++)
+                        {
+                            ts.Append(baseline.AddMinutes(j), j, "watches/apple");
+                            cf.Increment("Likes", j);
+                        }
+
+                        await session.SaveChangesAsync();
+                    }
+                }
+
+                var databaseStatistics = await store.Maintenance.SendAsync(new GetStatisticsOperation());
+                var detailedDatabaseStatistics = await store.Maintenance.SendAsync(new GetDetailedStatisticsOperation());
+
+                Assert.NotNull(databaseStatistics);
+                Assert.NotNull(databaseStatistics);
+
+                Assert.Equal(10, databaseStatistics.CountOfDocuments);
+                Assert.Equal(10, databaseStatistics.CountOfCounterEntries);
+                Assert.Equal(10, databaseStatistics.CountOfTimeSeriesSegments);
+
+                Assert.Equal(10, detailedDatabaseStatistics.CountOfDocuments);
+                Assert.Equal(10, detailedDatabaseStatistics.CountOfCounterEntries);
+                Assert.Equal(10, detailedDatabaseStatistics.CountOfTimeSeriesSegments);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    session.TimeSeriesFor("foo/bar/0", "HeartRates").Delete();
+                    await session.SaveChangesAsync();
+                }
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    session.Delete("foo/bar/0");
+                    await session.SaveChangesAsync();
+                }
+
+                await store.Operations.SendAsync(new PutCompareExchangeValueOperation<string>("users/1", "Raven", 0));
+
+                databaseStatistics = await store.Maintenance.SendAsync(new GetStatisticsOperation());
+                detailedDatabaseStatistics = await store.Maintenance.SendAsync(new GetDetailedStatisticsOperation());
+
+                Assert.Equal(9, databaseStatistics.CountOfDocuments);
+                Assert.Equal(1, databaseStatistics.CountOfTombstones);
+
+                Assert.Equal(9, detailedDatabaseStatistics.CountOfDocuments);
+                Assert.Equal(1, detailedDatabaseStatistics.CountOfTombstones);
+                Assert.Equal(1, detailedDatabaseStatistics.CountOfCompareExchange);
+                Assert.Equal(1, detailedDatabaseStatistics.CountOfTimeSeriesDeletedRanges);
+            }
+        }
+
 
         [Fact]
         public async Task CanGetAListOfDatabasesAsync()
