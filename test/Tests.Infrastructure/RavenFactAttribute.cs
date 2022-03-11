b34e19d7aecf5cf9e5ce4c6cd470031bfdcc47f0
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-11 12:13:56 +0100

    RavenDB-18295 introduced RavenFactAttribute & RavenTheoryAttribute

---
+++ test/Tests.Infrastructure/RavenFactAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+using Xunit.Sdk;
+
+namespace Tests.Infrastructure;
+
+[TraitDiscoverer("Tests.Infrastructure.XunitExtensions.RavenTraitDiscoverer", "Tests.Infrastructure")]
+public class RavenFactAttribute : FactAttribute, ITraitAttribute
+{
+    public RavenFactAttribute(RavenTestCategory category)
+    {
+    }
+}
