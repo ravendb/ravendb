864f6a933593458baa2b2939a1e093575f9a8fb1
Stav <stav@ravendb.net>
2022-03-13 09:51:56 +0200

    RavenDB-18018 - addressed PR comments

--- src/Raven.Server/Documents/Sharding/Handlers/ShardedStudioStatsHandler.cs
+++ src/Raven.Server/Documents/Sharding/Handlers/ShardedStudioStatsHandler.cs
@@ -1,15 +1,9 @@
 ﻿extern alias NGC;
-using System;
 using System.Threading.Tasks;
-using Raven.Client.Http;
-using Raven.Server.Documents.Operations;
-using Raven.Server.Documents.ShardedHandlers.Processors;
-using Raven.Server.Documents.ShardedHandlers.ShardedCommands;
-using Raven.Server.Documents.Sharding;
-using Raven.Server.Documents.Studio;
+using Raven.Server.Documents.Sharding.Handlers.Processors;
 using Raven.Server.Routing;
 
-namespace Raven.Server.Documents.ShardedHandlers
+namespace Raven.Server.Documents.Sharding.Handlers
 {
     internal class ShardedStudioStatsHandler : ShardedRequestHandler
     {
@@ -20,25 +14,6 @@ namespace Raven.Server.Documents.ShardedHandlers
             {
                 await processor.ExecuteAsync();
             }
-        }
-
-        public readonly struct ShardedGetStudioFooterStatsOperation : IShardedOperation<FooterStatistics>
-        {
-            public FooterStatistics Combine(Memory<FooterStatistics> results)
-            {
-                var span = results.Span;
-                
-                var combined = new FooterStatistics();
-                
-                foreach (var stats in span)
-                {
-                    combined.CountOfDocuments += stats.CountOfDocuments;
-                }
-                
-                return combined;
-            }
-
-            public RavenCommand<FooterStatistics> CreateCommandForShard(int shard) => new GetStudioFooterStatisticsOperation.GetStudioFooterStatisticsCommand();
         }
     }
 }
--- test/SlowTests/SlowTests/StudioTests.cs
+++ test/SlowTests/SlowTests/StudioTests.cs
@@ -16,7 +16,7 @@ namespace SlowTests.SlowTests
         }
 
         [Theory]
-        [RavenData(DatabaseMode = RavenDatabaseMode.Single)]
+        [RavenData(DatabaseMode = RavenDatabaseMode.All)]
         public async Task CanGetStudioFooterStatistics(Options options)
         {
             using (var store = GetDocumentStore(options))
@@ -34,41 +34,23 @@ namespace SlowTests.SlowTests
                 Assert.NotNull(stats);
                 Assert.Equal(1, stats.CountOfIndexes);
                 Assert.Equal(2, stats.CountOfDocuments);
-                Assert.Equal(0, stats.CountOfIndexingErrors);
-                Assert.Equal(0, stats.CountOfStaleIndexes);
-            }
-        }
 
-        [Theory]
-        [RavenData(DatabaseMode = RavenDatabaseMode.Sharded)]
-        public async Task ShardedCanGetStudioFooterStatistics(Options options)
-        {
-            //TODO: can unify with non sharded mode after all FooterStatistics fields are decided
-            using (var store = GetDocumentStore(options))
-            {
-                using (var session = store.OpenSession())
+                if (options.DatabaseMode == RavenDatabaseMode.Single)
                 {
-                    session.Store(new User { Name = "SomeDoc" });
-                    session.SaveChanges();
+                    Assert.Equal(0, stats.CountOfIndexingErrors);
+                    Assert.Equal(0, stats.CountOfStaleIndexes);
                 }
-                await store.ExecuteIndexAsync(new AllowedUsers());
-                WaitForIndexing(store, sharded: true);
-
-                var stats = store.Maintenance.Send(new GetStudioFooterStatisticsOperation());
-
-                Assert.NotNull(stats);
-                Assert.Equal(1, stats.CountOfIndexes);
-                Assert.Equal(2, stats.CountOfDocuments);
-                Assert.Equal(null, stats.CountOfIndexingErrors);
-                Assert.Equal(null, stats.CountOfStaleIndexes);
+                else
+                {
+                    Assert.Equal(null, stats.CountOfIndexingErrors);
+                    Assert.Equal(null, stats.CountOfStaleIndexes);
+                }
             }
         }
-
+        
         private class User
         {
             public string Name;
-
-            public bool Banned;
         }
 
         private class AllowedUsers : AbstractIndexCreationTask<User>
@@ -78,8 +60,7 @@ namespace SlowTests.SlowTests
             public AllowedUsers()
             {
                 Map = users => from user in users
-                               where user.Banned == false
-                               select new
+                    select new
                                {
                                    user.Name
                                };
