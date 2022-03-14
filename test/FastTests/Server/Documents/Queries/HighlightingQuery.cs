7dadb1aa4b826b88b3ec2b23d89db7802428371f
Federico Lois <federico.lois@gmail.com>
2022-03-14 17:11:07 -0300

    RavenDB-18317: Highlighting test not working.

---
+++ test/FastTests/Server/Documents/Queries/HighlightingQuery.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Queries.Highlighting;
+using Raven.Tests.Core.Utils.Entities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FastTests.Server.Documents.Queries
+{
+    public class HighlightingQuery : RavenTestBase
+    {
+        public HighlightingQuery(ITestOutputHelper output) : base(output)
+        {
+        }
+        
+        [Fact]
+        public void HighlightingOnAutomap()
+        {
+            using var store = GetDocumentStore();
+
+            CreateNorthwindDatabase(store);
+            {
+                using var session = store.OpenSession();
+                var result = session.Query<Company>()
+                    .Search(p => p.Name, "alf*")
+                    .Highlight(x => x.Name, 128, 1, out Highlightings highlightings)
+                    .Single();
+
+                WaitForUserToContinueTheTest(store);
+
+                Assert.Equal("<b style=\"background:yellow\">Alfreds</b> Futterkiste", highlightings.GetFragments(result.Id)[0]);
+            }
+        }
+    }
+}
