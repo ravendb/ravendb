a285a8f96f3f8a932e68f0c32c38052e0d81a07b
Karmel <karmel@ravendb.net>
2022-03-16 12:01:28 +0200

    RavenDB-18214 rework the EP to get from one shard only

--- src/Raven.Server/Documents/Sharding/Handlers/Processors/ShardedIndexHandlerProcessorForGetIndexesStatus.cs
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/ShardedIndexHandlerProcessorForGetIndexesStatus.cs
@@ -2,10 +2,8 @@
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Raven.Client.Documents.Indexes;
-using Raven.Client.Documents.Operations.Indexes;
 using Raven.Client.Http;
 using Raven.Server.Documents.Handlers.Processors;
-using Raven.Server.Documents.Sharding.Operations;
 using Raven.Server.ServerWide.Context;
 
 namespace Raven.Server.Documents.Sharding.Handlers.Processors
@@ -15,36 +13,11 @@ namespace Raven.Server.Documents.Sharding.Handlers.Processors
         public ShardedIndexHandlerProcessorForGetIndexesStatus([NotNull] ShardedRequestHandler requestHandler) : base(requestHandler, requestHandler.ContextPool)
         {
         }
+        protected override bool SupportsCurrentNode => false;
 
-        protected override async ValueTask<IndexingStatus> GetIndexesStatusAsync()
-        {
-            var op = new ShardedIndexesStatusOperation();
-            return await RequestHandler.ShardExecutor.ExecuteParallelForAllAsync(op);
-        }
+        protected override ValueTask<IndexingStatus> GetResultForCurrentNodeAsync() => throw new NotImplementedException();
 
-        internal readonly struct ShardedIndexesStatusOperation : IShardedOperation<IndexingStatus>
-        {
-            public IndexingStatus Combine(Memory<IndexingStatus> results)
-            {
-                if (results.Length == 0)
-                    return null;
-
-                var span = results.Span;
-
-                var combined = new IndexingStatus
-                {
-                    Status = IndexRunningStatus.Running, // default value
-                    Indexes = span[0].Indexes
-                };
-
-                for (var i = 0; i < span[0].Indexes.Length; i++)
-                    combined.Indexes[i].Status = IndexRunningStatus.Running;
-
-                return combined;
-            }
-
-            public RavenCommand<IndexingStatus> CreateCommandForShard(int shard) => new GetIndexingStatusOperation.GetIndexingStatusCommand();
-
-        }
+        protected override Task<IndexingStatus> GetResultForRemoteNodeAsync(RavenCommand<IndexingStatus> command) => 
+            RequestHandler.ShardExecutor.ExecuteSingleShardAsync(command, GetShardNumber());
     }
 }
