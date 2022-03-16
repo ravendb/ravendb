5258f75c33b3d3bf34d32630a7ffbceb527119cb
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-16 14:27:07 +0100

    RavenDB-18246 added /databases/*/studio/indexes/errors-count endpoin

---
+++ src/Raven.Server/Web/Studio/Processors/AbstractStudioIndexHandlerProcessorForGetIndexErrorsCount.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Client.Http;
+using Raven.Server.Documents.Commands;
+using Raven.Server.Documents.Handlers.Processors;
+using Raven.Server.Json;
+using Sparrow.Json;
+
+namespace Raven.Server.Web.Studio.Processors;
+
+internal abstract class AbstractStudioIndexHandlerProcessorForGetIndexErrorsCount<TRequestHandler, TOperationContext> : AbstractHandlerReadProcessor<GetIndexErrorsCountCommand.IndexErrorsCount[], TRequestHandler, TOperationContext>
+    where TRequestHandler : RequestHandler
+    where TOperationContext : JsonOperationContext
+{
+    protected AbstractStudioIndexHandlerProcessorForGetIndexErrorsCount([NotNull] TRequestHandler requestHandler, [NotNull] JsonContextPoolBase<TOperationContext> contextPool) : base(requestHandler, contextPool)
+    {
+    }
+
+    protected override RavenCommand<GetIndexErrorsCountCommand.IndexErrorsCount[]> CreateCommandForNode(string nodeTag) => new GetIndexErrorsCountCommand(GetIndexNames(), nodeTag);
+
+    protected string[] GetIndexNames()
+    {
+        return RequestHandler.GetStringValuesQueryString("name", required: false);
+    }
+
+    protected override async ValueTask WriteResultAsync(GetIndexErrorsCountCommand.IndexErrorsCount[] result)
+    {
+        using (ContextPool.AllocateOperationContext(out JsonOperationContext context))
+        await using (var writer = new AsyncBlittableJsonTextWriter(context, RequestHandler.ResponseBodyStream()))
+            writer.WriteIndexErrorCounts(context, result);
+    }
+}
---
+++ src/Raven.Server/Web/Studio/Sharding/Processors/ShardedStudioIndexHandlerProcessorForGetIndexErrorsCount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Client.Http;
+using Raven.Server.Documents.Commands;
+using Raven.Server.Documents.Sharding.Handlers;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Web.Studio.Processors;
+
+namespace Raven.Server.Web.Studio.Sharding.Processors;
+
+internal class ShardedStudioIndexHandlerProcessorForGetIndexErrorsCount : AbstractStudioIndexHandlerProcessorForGetIndexErrorsCount<ShardedRequestHandler, TransactionOperationContext>
+{
+    public ShardedStudioIndexHandlerProcessorForGetIndexErrorsCount([NotNull] ShardedRequestHandler requestHandler) 
+        : base(requestHandler, requestHandler.ContextPool)
+    {
+    }
+
+    protected override bool SupportsCurrentNode => false;
+
+    protected override ValueTask<GetIndexErrorsCountCommand.IndexErrorsCount[]> GetResultForCurrentNodeAsync() => throw new NotSupportedException();
+
+    protected override Task<GetIndexErrorsCountCommand.IndexErrorsCount[]> GetResultForRemoteNodeAsync(RavenCommand<GetIndexErrorsCountCommand.IndexErrorsCount[]> command) => RequestHandler.ShardExecutor.ExecuteSingleShardAsync(command, GetShardNumber());
+}
