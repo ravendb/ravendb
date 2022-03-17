d416f7d8f5ddd2449af9b866c869c4d33ef56245
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-17 15:26:09 +0100

    RavenDB-18334 fixed SmugglerResult deserialization

---
+++ test/FastTests/Issues/RavenDB_18334.cs
@@ -0,0 +1,41 @@
+﻿using Raven.Client.Documents.Conventions;
+using Raven.Client.Documents.Smuggler;
+using Raven.Client.Json.Serialization;
+using Sparrow.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FastTests.Issues;
+
+public class RavenDB_18334 : NoDisposalNeeded
+{
+    public RavenDB_18334(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Fact]
+    public void Can_Build_Serializator_For_SmugglerResult()
+    {
+        using (var context = JsonOperationContext.ShortTermSingleUse())
+        {
+            var result = new SmugglerResult();
+            result.AddError("MessageA");
+            result.AddInfo("MessageB");
+            result.AddWarning("MessageC");
+            result.AddMessage("MessageD");
+
+            var djv = result.ToJson();
+
+            var json = context.ReadObject(djv, "smuggler/result");
+
+            var result2 = JsonDeserializationClient.SmugglerResult(json);
+
+            Assert.Equal(result.Messages, result2.Messages);
+
+            var result3 = DocumentConventions.Default.Serialization.DefaultConverter.FromBlittable<SmugglerResult>(json);
+
+            Assert.Equal(result.Messages, result3.Messages);
+        }
+        
+    }
+}
