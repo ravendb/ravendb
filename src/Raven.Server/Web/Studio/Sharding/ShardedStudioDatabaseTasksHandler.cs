7066bc60dce3483439cc0018b32782dda9c74424
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-22 19:24:43 +0100

    RavenDB-18216 implemented /databases/*/studio-tasks/indexes/configuration/defaults

---
+++ src/Raven.Server/Web/Studio/Sharding/ShardedStudioDatabaseTasksHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Raven.Server.Documents.Sharding.Handlers;
+using Raven.Server.Routing;
+using Raven.Server.Web.Studio.Sharding.Processors;
+
+namespace Raven.Server.Web.Studio.Sharding;
+
+public class ShardedStudioDatabaseTasksHandler : ShardedRequestHandler
+{
+    [RavenShardedAction("/databases/*/studio-tasks/indexes/configuration/defaults", "GET")]
+    public async Task GetIndexDefaults()
+    {
+        using (var processor = new ShardedStudioDatabaseTasksHandlerProcessorForGetIndexDefaults(this))
+            await processor.ExecuteAsync();
+    }
+}
--- src/Raven.Server/Web/Studio/StudioDatabaseTasksHandler.cs
+++ src/Raven.Server/Web/Studio/StudioDatabaseTasksHandler.cs
@@ -1,9 +1,7 @@
 ﻿using System.Threading.Tasks;
-using Raven.Client.Documents.Indexes;
 using Raven.Server.Documents;
 using Raven.Server.Routing;
-using Raven.Server.ServerWide.Context;
-using Sparrow.Json;
+using Raven.Server.Web.Studio.Processors;
 
 namespace Raven.Server.Web.Studio
 {
@@ -18,32 +16,12 @@ namespace Raven.Server.Web.Studio
 
             return StudioTasksHandler.GetFolderPathOptionsInternal(ServerStore, type, isBackupFolder, path, RequestBodyStream, ResponseBodyStream);
         }
-        
+
         [RavenAction("/databases/*/studio-tasks/indexes/configuration/defaults", "GET", AuthorizationStatus.ValidUser, EndpointType.Read)]
         public async Task GetIndexDefaults()
         {
-            using (ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
-            {
-                var autoIndexesDeploymentMode = Database.Configuration.Indexing.AutoIndexDeploymentMode;
-                var staticIndexesDeploymentMode = Database.Configuration.Indexing.StaticIndexDeploymentMode;
-
-                await using (var writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
-                {
-                    writer.WriteStartObject();
-                    writer.WritePropertyName(nameof(IndexDefaults.AutoIndexDeploymentMode));
-                    writer.WriteString(autoIndexesDeploymentMode.ToString());
-                    writer.WriteComma();
-                    writer.WritePropertyName(nameof(IndexDefaults.StaticIndexDeploymentMode));
-                    writer.WriteString(staticIndexesDeploymentMode.ToString());
-                    writer.WriteEndObject();
-                }
-            }
-        }
-
-        public class IndexDefaults
-        {
-            public IndexDeploymentMode AutoIndexDeploymentMode { get; set; }
-            public IndexDeploymentMode StaticIndexDeploymentMode { get; set; }
+            using (var processor = new StudioDatabaseTasksHandlerProcessorForGetIndexDefaults(this))
+                await processor.ExecuteAsync();
         }
     }
 }
