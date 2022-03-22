065f43377e22002af29618ae13183c0eb5f67924
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-22 12:16:00 +0100

    RavenDB-18214 implemented /databases/*/indexes/staleness

---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Indexes/ShardedIndexHandlerProcessorForStale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Client.Http;
+using Raven.Server.Documents.Commands;
+using Raven.Server.Documents.Handlers.Processors.Indexes;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors.Indexes;
+
+internal class ShardedIndexHandlerProcessorForStale : AbstractIndexHandlerProcessorForStale<ShardedRequestHandler, TransactionOperationContext>
+{
+    public ShardedIndexHandlerProcessorForStale([NotNull] ShardedRequestHandler requestHandler)
+        : base(requestHandler, requestHandler.ContextPool)
+    {
+    }
+
+    protected override bool SupportsCurrentNode => false;
+
+    protected override ValueTask<GetIndexStalenessCommand.IndexStaleness> GetResultForCurrentNodeAsync() => throw new NotSupportedException();
+
+    protected override Task<GetIndexStalenessCommand.IndexStaleness> GetResultForRemoteNodeAsync(RavenCommand<GetIndexStalenessCommand.IndexStaleness> command)
+    {
+        var shardNumber = GetShardNumber();
+
+        return RequestHandler.ShardExecutor.ExecuteSingleShardAsync(command, shardNumber);
+    }
+}
--- test/Tests.Infrastructure/Operations/GetIndexStalenessOperation.cs
+++ test/Tests.Infrastructure/Operations/GetIndexStalenessOperation.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
 using Raven.Client.Documents.Conventions;
 using Raven.Client.Documents.Operations;
 using Raven.Client.Http;
+using Raven.Server.Documents.Commands;
 using Sparrow.Json;
 
 namespace Tests.Infrastructure.Operations
 {
-    public class GetIndexStalenessOperation : IMaintenanceOperation<GetIndexStalenessOperation.IndexStaleness>
+    public class GetIndexStalenessOperation : IMaintenanceOperation<GetIndexStalenessCommand.IndexStaleness>
     {
         private readonly string _indexName;
 
@@ -17,46 +16,9 @@ namespace Tests.Infrastructure.Operations
             _indexName = indexName ?? throw new ArgumentNullException(nameof(indexName));
         }
 
-        public RavenCommand<IndexStaleness> GetCommand(DocumentConventions conventions, JsonOperationContext context)
+        public RavenCommand<GetIndexStalenessCommand.IndexStaleness> GetCommand(DocumentConventions conventions, JsonOperationContext context)
         {
-            return new GetIndexStalenessCommand(conventions, _indexName);
-        }
-
-        private class GetIndexStalenessCommand : RavenCommand<IndexStaleness>
-        {
-            private readonly DocumentConventions _conventions;
-            private readonly string _indexName;
-
-            public GetIndexStalenessCommand(DocumentConventions conventions, string indexName)
-            {
-                _conventions = conventions;
-                _indexName = indexName;
-            }
-
-            public override bool IsReadRequest => true;
-
-
-            public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
-            {
-                url = $"{node.Url}/databases/{node.Database}/indexes/staleness?name={_indexName}";
-
-                return new HttpRequestMessage
-                {
-                    Method = HttpMethod.Get
-                };
-            }
-
-            public override void SetResponse(JsonOperationContext context, BlittableJsonReaderObject response, bool fromCache)
-            {
-                Result = (IndexStaleness)_conventions.Serialization.DeserializeEntityFromBlittable(typeof(IndexStaleness), response);
-            }
-        }
-
-        public class IndexStaleness
-        {
-            public bool IsStale { get; set; }
-
-            public List<string> StalenessReasons { get; set; }
+            return new GetIndexStalenessCommand(_indexName, nodeTag: null);
         }
     }
 }
