72a32c7abf1f7a7777b16cabcffec3117f06d639
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-22 15:52:54 +0100

    RavenDB-18216 fixing compilation

--- src/Raven.Server/Documents/Sharding/Handlers/Processors/Indexes/ShardedIndexHandlerProcessorForGetIndexHistory.cs
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Indexes/ShardedIndexHandlerProcessorForGetIndexHistory.cs
@@ -1,14 +1,13 @@
 ﻿using JetBrains.Annotations;
 using Raven.Server.Documents.Handlers.Processors.Indexes;
 using Raven.Server.ServerWide.Context;
-using Sparrow.Json;
 
 namespace Raven.Server.Documents.Sharding.Handlers.Processors.Indexes;
 
 internal class ShardedIndexHandlerProcessorForGetIndexHistory : AbstractIndexHandlerProcessorForGetIndexHistory<ShardedRequestHandler, TransactionOperationContext>
 {
-    public ShardedIndexHandlerProcessorForGetIndexHistory([NotNull] ShardedRequestHandler requestHandler, [NotNull] JsonContextPoolBase<TransactionOperationContext> contextPool)
-        : base(requestHandler, contextPool)
+    public ShardedIndexHandlerProcessorForGetIndexHistory([NotNull] ShardedRequestHandler requestHandler)
+        : base(requestHandler, requestHandler.ContextPool)
     {
     }
 
