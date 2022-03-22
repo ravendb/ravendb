d7f1dff8426612668ad0d893edea06f15972b520
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-22 19:37:23 +0100

    RavenDB-18216 implemented /databases/*/analyzers

--- src/Raven.Server/Documents/Handlers/AnalyzersHandler.cs
+++ src/Raven.Server/Documents/Handlers/AnalyzersHandler.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using Raven.Client.Documents.Indexes.Analysis;
+﻿using System.Threading.Tasks;
+using Raven.Server.Documents.Handlers.Processors.Analyzers;
 using Raven.Server.Routing;
 using Raven.Server.ServerWide.Context;
-using Sparrow.Json;
 
 namespace Raven.Server.Documents.Handlers
 {
@@ -12,46 +10,8 @@ namespace Raven.Server.Documents.Handlers
         [RavenAction("/databases/*/analyzers", "GET", AuthorizationStatus.ValidUser, EndpointType.Read)]
         public async Task Get()
         {
-            using (ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext context))
-            {
-                Dictionary<string, AnalyzerDefinition> analyzers;
-                using (context.OpenReadTransaction())
-                {
-                    var rawRecord = Server.ServerStore.Cluster.ReadRawDatabaseRecord(context, Database.Name);
-                    analyzers = rawRecord?.Analyzers;
-                }
-
-                if (analyzers == null)
-                {
-                    analyzers = new Dictionary<string, AnalyzerDefinition>();
-                }
-                
-                var namesOnly = GetBoolValueQueryString("namesOnly", false) ?? false;
-
-                await using (var writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
-                {
-                    writer.WriteStartObject();
-
-                    writer.WriteArray(context, "Analyzers", analyzers.Values, (w, c, analyzer) =>
-                    {
-                        w.WriteStartObject();
-
-                        w.WritePropertyName(nameof(AnalyzerDefinition.Name));
-                        w.WriteString(analyzer.Name);
-
-                        if (namesOnly == false)
-                        {
-                            w.WriteComma();
-                            w.WritePropertyName(nameof(AnalyzerDefinition.Code));
-                            w.WriteString(analyzer.Code);
-                        }
-
-                        w.WriteEndObject();
-                    });
-
-                    writer.WriteEndObject();
-                }
-            }
+            using (var processor = new AnalyzersHandlerProcessorForGet<DocumentsOperationContext>(this, ContextPool, Database.Name))
+                await processor.ExecuteAsync();
         }
     }
 }
---
+++ src/Raven.Server/Documents/Sharding/Handlers/ShardedAnalyzersHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Raven.Server.Documents.Handlers.Processors.Analyzers;
+using Raven.Server.Routing;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Sharding.Handlers;
+
+public class ShardedAnalyzersHandler : ShardedRequestHandler
+{
+    [RavenShardedAction("/databases/*/analyzers", "GET")]
+    public async Task Get()
+    {
+        using (var processor = new AnalyzersHandlerProcessorForGet<TransactionOperationContext>(this, ContextPool, ShardedContext.DatabaseName))
+            await processor.ExecuteAsync();
+    }
+}
