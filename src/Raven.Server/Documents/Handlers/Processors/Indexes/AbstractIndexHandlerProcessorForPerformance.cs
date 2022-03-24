4669bbf0ca6bd94d59845c61ee4c87ab204786b8
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-24 23:29:46 +0100

    RavenDB-18103 implemented /databases/*/indexes/performance

---
+++ src/Raven.Server/Documents/Handlers/Processors/Indexes/AbstractIndexHandlerProcessorForPerformance.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Primitives;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Operations.Indexes;
+using Raven.Client.Http;
+using Raven.Server.Json;
+using Raven.Server.Web;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Handlers.Processors.Indexes;
+
+internal abstract class AbstractIndexHandlerProcessorForPerformance<TRequestHandler, TOperationContext> : AbstractHandlerProxyReadProcessor<IndexPerformanceStats[], TRequestHandler, TOperationContext>
+    where TRequestHandler : RequestHandler
+    where TOperationContext : JsonOperationContext
+{
+    protected AbstractIndexHandlerProcessorForPerformance([NotNull] TRequestHandler requestHandler, [NotNull] JsonContextPoolBase<TOperationContext> contextPool)
+        : base(requestHandler, contextPool)
+    {
+    }
+
+    protected StringValues GetNames() => RequestHandler.GetStringValuesQueryString("name", required: false);
+
+    protected override RavenCommand<IndexPerformanceStats[]> CreateCommandForNode(string nodeTag)
+    {
+        var names = GetNames();
+
+        return new GetIndexPerformanceStatisticsOperation.GetIndexPerformanceStatisticsCommand(names, nodeTag);
+    }
+
+    protected override async ValueTask WriteResultAsync(IndexPerformanceStats[] result)
+    {
+        using (ContextPool.AllocateOperationContext(out JsonOperationContext context))
+        await using (var writer = new AsyncBlittableJsonTextWriter(context, RequestHandler.ResponseBodyStream()))
+        {
+            writer.WritePerformanceStats(context, result);
+        }
+    }
+}
---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Indexes/ShardedIndexHandlerProcessorForPerformance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Http;
+using Raven.Server.Documents.Handlers.Processors.Indexes;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors.Indexes;
+
+internal class ShardedIndexHandlerProcessorForPerformance : AbstractIndexHandlerProcessorForPerformance<ShardedDatabaseRequestHandler, TransactionOperationContext>
+{
+    public ShardedIndexHandlerProcessorForPerformance([NotNull] ShardedDatabaseRequestHandler requestHandler)
+        : base(requestHandler, requestHandler.ContextPool)
+    {
+    }
+
+    protected override bool SupportsCurrentNode => false;
+
+    protected override ValueTask<IndexPerformanceStats[]> GetResultForCurrentNodeAsync() => throw new NotSupportedException();
+
+    protected override Task<IndexPerformanceStats[]> GetResultForRemoteNodeAsync(RavenCommand<IndexPerformanceStats[]> command)
+    {
+        var shardNumber = GetShardNumber();
+
+        return RequestHandler.ShardExecutor.ExecuteSingleShardAsync(command, shardNumber);
+    }
+}
