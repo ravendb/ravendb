08bd3424fbc65eff00a067639f9cc709baebdd4b
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-27 22:40:48 +0200

    RavenDB-18290 implemented GET /databases/*/replication/conflicts/solver

---
+++ src/Raven.Server/Documents/Handlers/Processors/Replication/AbstractReplicationHandlerProcessorForGetConflictSolver.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Client.ServerWide;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Web;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Server.Documents.Handlers.Processors.Replication;
+
+internal abstract class AbstractReplicationHandlerProcessorForGetConflictSolver<TRequestHandler, TOperationContext> : AbstractHandlerProcessor<TRequestHandler, TOperationContext>
+    where TRequestHandler : RequestHandler
+    where TOperationContext : JsonOperationContext
+{
+    protected AbstractReplicationHandlerProcessorForGetConflictSolver([NotNull] TRequestHandler requestHandler, [NotNull] JsonContextPoolBase<TOperationContext> contextPool)
+        : base(requestHandler, contextPool)
+    {
+    }
+
+    protected abstract string GetDatabaseName();
+
+    public override async ValueTask ExecuteAsync()
+    {
+        using (RequestHandler.Server.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext context))
+        using (context.OpenReadTransaction())
+        {
+            ConflictSolver solverConfig;
+            using (var rawRecord = RequestHandler.Server.ServerStore.Cluster.ReadRawDatabaseRecord(context, GetDatabaseName()))
+                solverConfig = rawRecord?.ConflictSolverConfiguration;
+
+            if (solverConfig == null)
+            {
+                HttpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                return;
+            }
+
+            var resolveByCollection = new DynamicJsonValue();
+            foreach (var collection in solverConfig.ResolveByCollection)
+                resolveByCollection[collection.Key] = collection.Value.ToJson();
+
+            await using (var writer = new AsyncBlittableJsonTextWriter(context, RequestHandler.ResponseBodyStream()))
+            {
+                context.Write(writer, new DynamicJsonValue
+                {
+                    [nameof(solverConfig.ResolveToLatest)] = solverConfig.ResolveToLatest,
+                    [nameof(solverConfig.ResolveByCollection)] = resolveByCollection
+                });
+            }
+        }
+    }
+}
---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Replication/ShardedReplicationHandlerProcessorForGetConflictSolver.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using Raven.Server.Documents.Handlers.Processors.Replication;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors.Replication;
+
+internal class ShardedReplicationHandlerProcessorForGetConflictSolver : AbstractReplicationHandlerProcessorForGetConflictSolver<ShardedDatabaseRequestHandler, TransactionOperationContext>
+{
+    public ShardedReplicationHandlerProcessorForGetConflictSolver([NotNull] ShardedDatabaseRequestHandler requestHandler)
+        : base(requestHandler, requestHandler.ContextPool)
+    {
+    }
+
+    protected override string GetDatabaseName() => RequestHandler.DatabaseContext.DatabaseName;
+}
