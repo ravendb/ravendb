a1c7a27422444d80adf8e56de5a4787088650054
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-27 21:51:51 +0200

    RavenDB-18222 implemented GET /databases/*/admin/configuration/settings

---
+++ src/Raven.Server/Documents/Handlers/Admin/Processors/Configuration/AdminConfigurationHandlerForGetSettings.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using Raven.Server.Config;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Handlers.Admin.Processors.Configuration;
+
+internal class AdminConfigurationHandlerForGetSettings : AbstractAdminConfigurationHandlerForGetSettings<DatabaseRequestHandler, DocumentsOperationContext>
+{
+    public AdminConfigurationHandlerForGetSettings([NotNull] DatabaseRequestHandler requestHandler)
+        : base(requestHandler, requestHandler.ContextPool)
+    {
+    }
+
+    protected override string GetDatabaseName() => RequestHandler.Database.Name;
+
+    protected override RavenConfiguration GetDatabaseConfiguration() => RequestHandler.Database.Configuration;
+}
