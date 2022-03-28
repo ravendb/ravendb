75f7c12696a0095e78ffcb048f3e05365907d88e
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-28 21:59:32 +0200

    RavenDB-18222 implemented POST /databases/*/admin/studio-tasks/folder-path-options

--- src/Raven.Server/Web/Studio/Sharding/ShardedStudioDatabaseTasksHandler.cs
+++ src/Raven.Server/Web/Studio/Sharding/ShardedStudioDatabaseTasksHandler.cs
@@ -1,12 +1,21 @@
 ﻿using System.Threading.Tasks;
 using Raven.Server.Documents.Sharding.Handlers;
 using Raven.Server.Routing;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Web.Studio.Processors;
 using Raven.Server.Web.Studio.Sharding.Processors;
 
 namespace Raven.Server.Web.Studio.Sharding;
 
 public class ShardedStudioDatabaseTasksHandler : ShardedDatabaseRequestHandler
 {
+    [RavenShardedAction("/databases/*/admin/studio-tasks/folder-path-options", "POST")]
+    public async Task GetFolderPathOptionsForDatabaseAdmin()
+    {
+        using (var processor = new StudioStudioDatabaseTasksHandlerProcessorForGetFolderPathOptionsForDatabaseAdmin<TransactionOperationContext>(this, ContextPool))
+            await processor.ExecuteAsync();
+    }
+
     [RavenShardedAction("/databases/*/studio-tasks/indexes/configuration/defaults", "GET")]
     public async Task GetIndexDefaults()
     {
