bdd9c7b2949710b0f49afa4114ea28e546b2dfbe
Federico Lois <federico.lois@gmail.com>
2022-03-28 15:31:43 -0300

    RavenDB-18353: Support for UTF-8 strings.
    We will switch into Unicode comparison (chars) when we find out that we are not dealing with a ASCII string.

--- src/Corax/Queries/SortingMatch.Comparers.cs
+++ src/Corax/Queries/SortingMatch.Comparers.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace Corax.Queries
 {
@@ -21,7 +23,19 @@ namespace Corax.Queries
             }
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            private static bool IsSpace(char c)
+            {
+                return c == ' ';
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             private static bool IsDigit(byte c)
+            {
+                return c >= '0' && c <= '9';
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            private static bool IsDigit(char c)
             {
                 return c >= '0' && c <= '9';
             }
@@ -32,8 +46,15 @@ namespace Corax.Queries
                 if (c >= 'a' && c <= 'z')
                     return (byte)(c - ('a' - 'A'));
 
-                return c; 
+                return c;
             }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            private static char ToUpper(char c)
+            {
+                return char.ToUpperInvariant(c);
+            }
+
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static int CompareAlphanumericAscending(ReadOnlySpan<byte> a, ReadOnlySpan<byte> b, bool foldCase = false)
@@ -45,15 +66,17 @@ namespace Corax.Queries
 
                 // Natural order string sorting ported from Martin Pool's C implementation as a reference. 
                 //   strnatcmp.c -- Perform 'natural order' comparisons of strings in C.
-                // https://github.com/sourcefrog/natsort
-
-                // TODO: We need to check if it is ASCII or not and act accordingly.
+                // https://github.com/sourcefrog/natsort               
 
                 int ai = 0, bi = 0;
                 while (true)
                 {
                     byte ca = ai < a.Length ? a[ai] : (byte)0;
                     byte cb = bi < b.Length ? b[bi] : (byte)0;
+
+                    // We need to check if it is ASCII or not and act accordingly.
+                    if ((ca & 0b1000_0000) != 0 || (cb & 0b1000_0000) != 0)
+                        goto NotAscii;
 
                     /* skip over leading spaces or zeros */
                     while (IsSpace(ca))
@@ -102,6 +125,89 @@ namespace Corax.Queries
                     ++ai;
                     ++bi;
                 }
+
+                NotAscii:
+                return CompareAlphanumericAscendingUtf8(a.Slice(ai), b.Slice(bi), foldCase);
+            }
+
+            private static int CompareAlphanumericAscendingUtf8(ReadOnlySpan<byte> byteA, ReadOnlySpan<byte> byteB, bool foldCase)
+            {
+                var auxiliarMemory = ArrayPool<char>.Shared.Rent(byteA.Length + byteB.Length);
+                var a = auxiliarMemory.AsSpan().Slice(0, byteA.Length);
+                var b = auxiliarMemory.AsSpan().Slice(byteA.Length, byteB.Length);
+
+                var aLength = Encoding.UTF8.GetChars(byteA, a);
+                var bLength = Encoding.UTF8.GetChars(byteB, b);
+
+                a.Slice(0, aLength);
+                b.Slice(0, bLength);
+
+                int result = 0;
+                int ai = 0, bi = 0;
+                while (true)
+                {
+                    char ca = ai < a.Length ? a[ai] : (char)0;
+                    char cb = bi < b.Length ? b[bi] : (char)0;
+
+                    /* skip over leading spaces or zeros */
+                    while (IsSpace(ca))
+                        ca = a[++ai];
+
+                    while (IsSpace(cb))
+                        cb = b[++bi];
+
+                    /* process run of digits */
+                    if (IsDigit(ca) && IsDigit(cb))
+                    {
+                        bool fractional = (ca == '0' || cb == '0');
+                        if (fractional)
+                        {
+                            result = CompareLeft(a.Slice(ai), b.Slice(bi));
+                            if (result != 0)
+                                goto End;
+                        }
+                        else
+                        {
+                            result = CompareRight(a.Slice(ai), b.Slice(bi));
+                            if (result != 0)
+                                goto End;
+                        }
+                    }
+
+                    if (ca == 0 && cb == 0)
+                    {
+                        /* The strings compare the same.  Perhaps the caller
+                               will want to call strcmp to break the tie. */
+                        result = 0;
+                        goto End;
+                    }
+
+                    if (foldCase)
+                    {
+                        ca = ToUpper(ca);
+                        cb = ToUpper(cb);
+                    }
+
+                    if (ca < cb)
+                    {
+                        result = -1;
+                        goto End;
+                    }
+
+                    if (ca > cb)
+                    {
+                        result = +1;
+                        goto End;
+                    }
+                        
+                    ++ai;
+                    ++bi;
+                }
+
+
+                End:
+                ArrayPool<char>.Shared.Return(auxiliarMemory);
+                return result;
             }
 
             private static int CompareRight(ReadOnlySpan<byte> ax, ReadOnlySpan<byte> bx)
@@ -140,6 +246,42 @@ namespace Corax.Queries
                 }
             }
 
+            private static int CompareRight(ReadOnlySpan<char> ax, ReadOnlySpan<char> bx)
+            {
+                int bias = 0;
+                int a = 0;
+                int b = 0;
+
+                /* The longest run of digits wins.  That aside, the greatest
+                   value wins, but we can't know that it will until we've scanned
+                   both numbers to know that they have the same magnitude, so we
+                   remember it in BIAS. */
+                for (; ; a++, b++)
+                {
+                    char av = a < ax.Length ? ax[a] : (char)0;
+                    char bv = b < bx.Length ? bx[b] : (char)0;
+
+                    if (!IsDigit(av) && !IsDigit(bv))
+                        return bias;
+                    if (!IsDigit(av))
+                        return -1;
+                    if (!IsDigit(bv))
+                        return +1;
+                    if (av < bv)
+                    {
+                        if (bias != 0)
+                            bias = -1;
+                    }
+                    else if (av > bv)
+                    {
+                        if (bias != 0)
+                            bias = +1;
+                    }
+                    else if (a == 0 || b == 0)
+                        return bias;
+                }
+            }
+
             private static int CompareLeft(ReadOnlySpan<byte> ax, ReadOnlySpan<byte> bx)
             {
                 int a = 0;
@@ -151,6 +293,31 @@ namespace Corax.Queries
                 {
                     byte av = a < ax.Length ? ax[a] : (byte)0;
                     byte bv = b < bx.Length ? bx[b] : (byte)0;
+
+                    if (!IsDigit(av) && !IsDigit(bv))
+                        return 0;
+                    if (!IsDigit(av))
+                        return -1;
+                    if (!IsDigit(bv))
+                        return +1;
+                    if (av < bv)
+                        return -1;
+                    if (av > bv)
+                        return +1;
+                }
+            }
+
+            private static int CompareLeft(ReadOnlySpan<char> ax, ReadOnlySpan<char> bx)
+            {
+                int a = 0;
+                int b = 0;
+
+                /* Compare two left-aligned numbers: the first to have a
+                   different value wins. */
+                for (; ; a++, b++)
+                {
+                    char av = a < ax.Length ? ax[a] : (char)0;
+                    char bv = b < bx.Length ? bx[b] : (char)0;
 
                     if (!IsDigit(av) && !IsDigit(bv))
                         return 0;
