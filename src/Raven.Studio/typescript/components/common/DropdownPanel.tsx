2bd4084cadebf31ec3117c4f2bda26facb05b229
Marcin Lewandowski <marcin@ravendb.net>
2022-03-28 15:06:26 +0200

    RavenDB-18212 Sharding - Indexes List View - dropdown panel

---
+++ src/Raven.Studio/typescript/components/common/DropdownPanel.tsx
@@ -0,0 +1,50 @@
+﻿import classNames from "classnames";
+import React, { MouseEvent, useCallback, useRef } from "react";
+
+interface DropdownPanelProps {
+    className?: string;
+    children: JSX.Element;
+}
+
+export function DropdownPanel(props: DropdownPanelProps) {
+    const { className, children } = props;
+    
+    const element = useRef<HTMLDivElement>();
+    
+    const onClick = useCallback((event: MouseEvent<HTMLElement>) => {
+        const $target = $(event.target);
+
+        const closestClosePanel = $target.closest(".close-panel");
+        const clickedOnClose = !!closestClosePanel.length;
+        if (clickedOnClose) {
+            if (!closestClosePanel.is(":disabled")) {
+                const $dropdownParent = $target.closest(".dropdown-menu").parent();
+                $dropdownParent.removeClass('open');
+            } else {
+                event.stopPropagation();
+            }
+        } else {
+            const $button = $target.closest(".dropdown-toggle");
+            const $dropdown = $button.next(".dropdown-menu");
+            if ($dropdown.length && $dropdown[0] !== element.current) {
+                if (!$button.is(":disabled")) {
+                    const $parent = $dropdown.parent();
+                    $parent.toggleClass('open');
+                }
+            } else {
+                // close any child dropdown
+                $(".dropdown", element.current).each((idx, elem) => {
+                    $(elem).removeClass('open');
+                });
+            }
+
+            event.stopPropagation();
+        }
+    }, []);
+    
+    return (
+        <div className={classNames("dropdown-menu", className)} onClick={onClick} ref={element}>
+            {children}
+        </div>
+    )
+}
