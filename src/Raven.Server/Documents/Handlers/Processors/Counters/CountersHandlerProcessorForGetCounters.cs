7d266922c1df3070778237e5acba5e49d4584355
Stav <stav@ravendb.net>
2022-03-28 15:15:51 +0300

    RavenDB-18201 - Sharding - Edit Document View Endpoints - GET /databases/*/counters

---
+++ src/Raven.Server/Documents/Handlers/Processors/Counters/CountersHandlerProcessorForGetCounters.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Primitives;
+using Raven.Client.Documents.Operations.Counters;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Handlers.Processors.Counters
+{
+    internal class CountersHandlerProcessorForGetCounters : AbstractCountersHandlerProcessorForGetCounters<DatabaseRequestHandler, DocumentsOperationContext>
+    {
+        public CountersHandlerProcessorForGetCounters([NotNull] DatabaseRequestHandler requestHandler) : base(requestHandler, requestHandler.ContextPool)
+        {
+        }
+
+        protected override async ValueTask<CountersDetail> GetCountersAsync(string docId, StringValues counters, bool full)
+        {
+            using (ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+            {
+                using (context.OpenReadTransaction())
+                {
+                    return GetInternal(RequestHandler.Database, context, counters, docId, full);
+                }
+            }
+        }
+
+        public static CountersDetail GetInternal(DocumentDatabase database, DocumentsOperationContext context, StringValues counters, string docId, bool full)
+        {
+            var result = new CountersDetail();
+            var names = counters.Count != 0
+                ? counters
+                : database.DocumentsStorage.CountersStorage.GetCountersForDocument(context, docId);
+
+            foreach (var counter in names)
+            {
+                GetCounterValue(context, database, docId, counter, full, result);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Counters/ShardedCountersHandlerProcessorForGetCounters.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Primitives;
+using Raven.Client.Documents.Operations.Counters;
+using Raven.Server.Documents.Handlers.Processors.Counters;
+using Raven.Server.ServerWide.Context;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors.Counters
+{
+    internal class ShardedCountersHandlerProcessorForGetCounters : AbstractCountersHandlerProcessorForGetCounters<ShardedDatabaseRequestHandler, TransactionOperationContext>
+    {
+        public ShardedCountersHandlerProcessorForGetCounters([NotNull] ShardedDatabaseRequestHandler requestHandler) : base(requestHandler, requestHandler.ContextPool)
+        {
+        }
+
+        protected override async ValueTask<CountersDetail> GetCountersAsync(string docId, StringValues counters, bool full)
+        {
+            var op = new GetCountersOperation.GetCounterValuesCommand(docId, counters, full);
+
+            int shardNumber;
+            using (RequestHandler.ContextPool.AllocateOperationContext(out TransactionOperationContext context))
+            {
+                shardNumber = RequestHandler.DatabaseContext.GetShardNumber(context, docId);
+            }
+
+            using (var token = RequestHandler.CreateOperationToken())
+            {
+                return await RequestHandler.ShardExecutor.ExecuteSingleShardAsync(op, shardNumber, token.Token);
+            }
+        }
+    }
+}
