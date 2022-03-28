a106b9d95a9993983eaaa456b40e8fa03a18e6e1
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-28 23:20:26 +0200

    RavenDB-18256 cleanup

--- src/Raven.Server/Documents/Handlers/Processors/Databases/AbstractHandlerProcessorForUpdateDatabaseConfiguration.cs
+++ src/Raven.Server/Documents/Handlers/Processors/Databases/AbstractHandlerProcessorForUpdateDatabaseConfiguration.cs
@@ -42,7 +42,9 @@ internal abstract class AbstractHandlerProcessorForUpdateDatabaseConfiguration<T
 
     protected abstract Task<(long Index, object Result)> OnUpdateConfiguration(TransactionOperationContext context, string databaseName, T configuration, string raftRequestId);
 
-    protected virtual Action<DynamicJsonValue, T, long> GetOnBeforeResponseWrite() => null;
+    protected virtual void OnBeforeResponseWrite(DynamicJsonValue responseJson, T configuration, long index)
+    {
+    }
 
     protected abstract ValueTask WaitForIndexNotificationAsync(long index);
 
@@ -78,8 +80,7 @@ internal abstract class AbstractHandlerProcessorForUpdateDatabaseConfiguration<T
                     ["RaftCommandIndex"] = index
                 };
 
-                var onBeforeResponseWrite = GetOnBeforeResponseWrite();
-                onBeforeResponseWrite?.Invoke(json, configuration, index);
+                OnBeforeResponseWrite(json, configuration, index);
 
                 context.Write(writer, json);
             }
