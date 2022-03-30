c0caf5d1ebc6bb3a7970a6578b50fdd468cafde9
shiranshalom <shiran@ravendb.net>
2022-03-30 11:21:32 +0300

    RavenDB-18248 - GET "/databases/*/debug/identities"

---
+++ src/Raven.Server/Documents/Handlers/Processors/Identities/AbstractIdentityDebugHandlerProcessorForGetIdentities.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Web;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Handlers.Processors.Identities
+{
+    internal abstract class AbstractIdentityDebugHandlerProcessorForGetIdentities<TRequestHandler, TOperationContext> : AbstractHandlerProcessor<TRequestHandler, TOperationContext>
+        where TRequestHandler : RequestHandler
+        where TOperationContext : JsonOperationContext
+    {
+        protected AbstractIdentityDebugHandlerProcessorForGetIdentities([NotNull] TRequestHandler requestHandler, [NotNull] JsonContextPoolBase<TOperationContext> contextPool) : base(requestHandler, contextPool)
+        {
+        }
+
+        protected abstract string GetDatabaseName();
+
+        public override async ValueTask ExecuteAsync()
+        {
+            var start = RequestHandler.GetStart();
+            var pageSize = RequestHandler.GetPageSize();
+
+            using (RequestHandler.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext context))
+            using (context.OpenReadTransaction())
+            {
+                await using (var writer = new AsyncBlittableJsonTextWriter(context, RequestHandler.ResponseBodyStream()))
+                {
+                    writer.WriteStartObject();
+
+                    var first = true;
+                    foreach (var identity in RequestHandler.ServerStore.Cluster.GetIdentitiesFromPrefix(context, GetDatabaseName(), start, pageSize))
+                    {
+                        if (first == false)
+                            writer.WriteComma();
+
+                        first = false;
+                        writer.WritePropertyName(identity.Prefix);
+                        writer.WriteInteger(identity.Value);
+                    }
+
+                    writer.WriteEndObject();
+                }
+            }
+        }
+    }
+}
---
+++ src/Raven.Server/Documents/Sharding/Handlers/ShardedIdentityDebugHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Raven.Server.Documents.Sharding.Handlers.Processors.Identities;
+using Raven.Server.Routing;
+
+namespace Raven.Server.Documents.Sharding.Handlers
+{
+    public class ShardedIdentityDebugHandler : ShardedDatabaseRequestHandler
+    {
+        [RavenShardedAction("/databases/*/debug/identities", "GET")]
+        public async Task GetIdentities()
+        {
+            using (var processor = new ShardedIdentityDebugHandlerProcessorForGetIdentities(this))
+                await processor.ExecuteAsync();
+        }
+    }
+}
