be33eeeb089a9049d171d117a89b370b366fad75
danielle9897 <danielle.9897@gmail.com>
2022-03-30 14:03:21 +0300

    RavenDB-18247 Sharding - Identities View

--- src/Raven.Studio/typescript/common/shell/menu/items/documents.ts
+++ src/Raven.Studio/typescript/common/shell/menu/items/documents.ts
@@ -8,28 +8,28 @@ export = getDocumentsMenuItem;
 function getDocumentsMenuItem(appUrls: computedAppUrls) {
     let documentsItems = [
         new leafMenuItem({
-            title: "List of documents",
-            nav: false,
             route: "databases/documents",
             moduleId: require("viewmodels/database/documents/documents"),
             shardingMode: "both",
+            title: "List of documents",
+            nav: false,
             css: "icon-documents",
             dynamicHash: appUrls.documents
         }),
         new leafMenuItem({
-            title: "Revisions Bin",
-            nav: false,
             route: "databases/documents/revisions/bin",
             moduleId: require("viewmodels/database/documents/revisionsBin"),
+            title: "Revisions Bin",
+            nav: false,
             css: "icon-revisions-bin",
             dynamicHash: appUrls.revisionsBin
         }),
         new collectionMenuItem(),
         new leafMenuItem({
-            title: "Patch",
-            nav: true,
             route: "databases/patch(/:recentPatchHash)",
             moduleId: require("viewmodels/database/patch/patch"),
+            title: "Patch",
+            nav: true,
             css: "icon-patch",
             dynamicHash: appUrls.patch,
             requiredAccess: "DatabaseReadWrite"
@@ -46,48 +46,49 @@ function getDocumentsMenuItem(appUrls: computedAppUrls) {
         }),
         new leafMenuItem({
             route: "databases/edit",
-            title: "Edit Document",
             moduleId: require("viewmodels/database/documents/editDocument"),
+            title: "Edit Document",
             nav: false,
             itemRouteToHighlight: "databases/documents"
         }),
         new leafMenuItem({
             route: "databases/ts/edit",
-            title: "Edit Time Series",
             moduleId: require("viewmodels/database/timeSeries/editTimeSeries"),
+            title: "Edit Time Series",
             nav: false,
             itemRouteToHighlight: "databases/documents"
         }),
         new leafMenuItem({
-            title: "Identities",
-            nav: true,
             route: "databases/identities",
             moduleId: require("viewmodels/database/identities/identities"),
+            shardingMode: "allShardsOnly",
+            title: "Identities",
+            nav: true,
             css: "icon-identities",
             dynamicHash: appUrls.identities
         }),
         new leafMenuItem({
-            title: "Compare Exchange",
-            nav: true,
-            shardingMode: "allShardsOnly",
             route: "databases/cmpXchg",
             moduleId: require("viewmodels/database/cmpXchg/cmpXchg"),
+            shardingMode: "allShardsOnly",
+            title: "Compare Exchange",
+            nav: true,
             css: "icon-cmp-xchg",
             dynamicHash: appUrls.cmpXchg
         }),
         new leafMenuItem({
             route: "databases/cmpXchg/edit",
-            title: "Edit Compare Exchange Value",
-            shardingMode: "allShardsOnly",
             moduleId: require("viewmodels/database/cmpXchg/editCmpXchg"),
+            shardingMode: "allShardsOnly",
+            title: "Edit Compare Exchange Value",
             nav: false,
             itemRouteToHighlight: "databases/cmpXchg"
         }),
         new leafMenuItem({
-            title: "Conflicts",
-            nav: true,
             route: "databases/documents/conflicts",
             moduleId: require("viewmodels/database/conflicts/conflicts"),
+            title: "Conflicts",
+            nav: true,
             css: "icon-conflicts",
             dynamicHash: appUrls.conflicts,
             badgeData: collectionsTracker.default.conflictsCount
