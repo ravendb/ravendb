0d56294c47615b56b087d2a92ea935def1b4be11
Pawel Pekrol <ppekrol@ravendb.net>
2022-03-31 14:46:29 +0200

    RavenDB-18267 implemented /databases/*/sorters

---
+++ src/Raven.Server/Documents/Handlers/Processors/Sorters/SortersHandlerProcessorForGet.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+
+namespace Raven.Server.Documents.Handlers.Processors.Sorters;
+
+internal class SortersHandlerProcessorForGet : AbstractSortersHandlerProcessorForGet<DatabaseRequestHandler>
+{
+    public SortersHandlerProcessorForGet([NotNull] DatabaseRequestHandler requestHandler) 
+        : base(requestHandler)
+    {
+    }
+
+    protected override string GetDatabaseName() => RequestHandler.Database.Name;
+}
---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Sorters/ShardedSortersHandlerProcessorForGet.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using Raven.Server.Documents.Handlers.Processors.Sorters;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors.Sorters;
+
+internal class ShardedSortersHandlerProcessorForGet : AbstractSortersHandlerProcessorForGet<ShardedDatabaseRequestHandler>
+{
+    public ShardedSortersHandlerProcessorForGet([NotNull] ShardedDatabaseRequestHandler requestHandler) 
+        : base(requestHandler)
+    {
+    }
+
+    protected override string GetDatabaseName() => RequestHandler.DatabaseContext.DatabaseName;
+}
