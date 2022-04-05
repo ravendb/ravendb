f73a711334a32e7b182a3cff05b2743ff883cb02
Pawel Pekrol <ppekrol@ravendb.net>
2022-04-05 13:49:54 +0300

    RavenDB-18274 implemented DELETE /databases/*/admin/integrations/postgresql/user

--- src/Raven.Server/Integrations/PostgreSQL/Handlers/PostgreSqlIntegrationHandler.cs
+++ src/Raven.Server/Integrations/PostgreSQL/Handlers/PostgreSqlIntegrationHandler.cs
@@ -42,55 +42,8 @@ namespace Raven.Server.Integrations.PostgreSQL.Handlers
         [RavenAction("/databases/*/admin/integrations/postgresql/user", "DELETE", AuthorizationStatus.DatabaseAdmin)]
         public async Task DeleteUser()
         {
-            AssertCanUsePostgreSqlIntegration();
-
-            using (Database.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
-            {
-                var username = GetQueryStringValueAndAssertIfSingleAndNotEmpty("username");
-
-                await using (var writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
-                {
-                    if (string.IsNullOrEmpty(username))
-                    {
-                        HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                        context.Write(writer, new DynamicJsonValue { ["Error"] = "Username is null or empty." });
-                        return;
-                    }
-
-                    DatabaseRecord databaseRecord;
-
-                    using (Database.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext transactionOperationContext))
-                    using (transactionOperationContext.OpenReadTransaction())
-                        databaseRecord = Database.ServerStore.Cluster.ReadDatabase(transactionOperationContext, Database.Name, out long index);
-
-                    var config = databaseRecord.Integrations?.PostgreSql;
-
-                    if (config == null)
-                    {
-                        HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                        context.Write(writer, new DynamicJsonValue { ["Error"] = "Unable to get usernames from database record" });
-                        return;
-                    }
-
-                    var users = config.Authentication.Users;
-
-                    var userToDelete = users.SingleOrDefault(x => x.Username == username);
-
-                    if (userToDelete == null)
-                    {
-                        HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                        context.Write(writer, new DynamicJsonValue { ["Error"] = $"{username} username does not exist." });
-                        return;
-                    }
-
-                    users.Remove(userToDelete);
-
-                    using (Database.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext transactionOperationContext))
-                        await DatabaseConfigurations(ServerStore.ModifyPostgreSqlConfiguration, transactionOperationContext, RaftIdGenerator.DontCareId, config);
-                }
-            }
-
-            NoContentStatus();
+            using (var processor = new PostgreSqlIntegrationHandlerProcessorForDeleteUser(this))
+                await processor.ExecuteAsync();
         }
 
         [RavenAction("/databases/*/admin/integrations/postgresql/config", "POST", AuthorizationStatus.DatabaseAdmin)]
@@ -100,7 +53,5 @@ namespace Raven.Server.Integrations.PostgreSQL.Handlers
 
             await DatabaseConfigurations(ServerStore.ModifyPostgreSqlConfiguration, "read-postgresql-config", GetRaftRequestIdFromQuery());
         }
-
-
     }
 }
---
+++ src/Raven.Server/Integrations/PostgreSQL/Handlers/Processors/PostgreSqlIntegrationHandlerProcessorForDeleteUser.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Server.Documents;
+
+namespace Raven.Server.Integrations.PostgreSQL.Handlers.Processors;
+
+internal class PostgreSqlIntegrationHandlerProcessorForDeleteUser : AbstractPostgreSqlIntegrationHandlerProcessorForDeleteUser<DatabaseRequestHandler>
+{
+    public PostgreSqlIntegrationHandlerProcessorForDeleteUser([NotNull] DatabaseRequestHandler requestHandler) : base(requestHandler)
+    {
+    }
+
+    protected override string GetDatabaseName() => RequestHandler.Database.Name;
+
+    protected override async ValueTask WaitForIndexNotificationAsync(long index)
+    {
+        await RequestHandler.Database.RachisLogIndexNotifications.WaitForIndexNotification(index, RequestHandler.Database.ServerStore.Engine.OperationTimeout);
+    }
+}
