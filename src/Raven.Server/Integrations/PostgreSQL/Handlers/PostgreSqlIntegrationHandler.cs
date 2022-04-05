62e1545430da7182e718dd37124c470a2eae6245
Pawel Pekrol <ppekrol@ravendb.net>
2022-04-05 12:30:13 +0300

    RavenDB-18274 implemented /databases/*/admin/integrations/postgresql/users

--- src/Raven.Server/Integrations/PostgreSQL/Handlers/PostgreSqlIntegrationHandler.cs
+++ src/Raven.Server/Integrations/PostgreSQL/Handlers/PostgreSqlIntegrationHandler.cs
@@ -3,19 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using Raven.Client.Exceptions.Commercial;
 using Raven.Client.ServerWide;
 using Raven.Client.ServerWide.Operations.Integrations.PostgreSQL;
 using Raven.Client.Util;
-using Raven.Server.Config;
-using Raven.Server.Config.Categories;
 using Raven.Server.Documents;
-using Raven.Server.Exceptions;
+using Raven.Server.Integrations.PostgreSQL.Handlers.Processors;
 using Raven.Server.Json;
 using Raven.Server.Routing;
 using Raven.Server.ServerWide.Context;
 using Raven.Server.Utils;
-using Raven.Server.Utils.Features;
 using Sparrow.Json;
 using Sparrow.Json.Parsing;
 
@@ -41,37 +37,8 @@ namespace Raven.Server.Integrations.PostgreSQL.Handlers
         [RavenAction("/databases/*/admin/integrations/postgresql/users", "GET", AuthorizationStatus.DatabaseAdmin)]
         public async Task GetUsernamesList()
         {
-            AssertCanUsePostgreSqlIntegration();
-
-            using (Database.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
-            {
-                DatabaseRecord databaseRecord;
-
-                using (Database.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext transactionOperationContext))
-                using (transactionOperationContext.OpenReadTransaction())
-                    databaseRecord = Database.ServerStore.Cluster.ReadDatabase(transactionOperationContext, Database.Name, out long index);
-
-                var usernames = new List<PostgreSqlUsername>();
-
-                var users = databaseRecord?.Integrations?.PostgreSql?.Authentication?.Users;
-
-                await using (var writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
-                {
-                    if (users != null)
-                    {
-                        foreach (var user in users)
-                        {
-                            var username = new PostgreSqlUsername { Username = user.Username };
-                            usernames.Add(username);
-                        }
-                    }
-
-                    var dto = new PostgreSqlUsernames { Users = usernames };
-
-                    var djv = (DynamicJsonValue)TypeConverter.ToBlittableSupportedType(dto);
-                    writer.WriteObject(context.ReadObject(djv, "PostgreSqlUsernames"));
-                }
-            }
+            using (var processor = new PostgreSqlIntegrationHandlerProcessorForGetUsernamesList(this))
+                await processor.ExecuteAsync();
         }
 
         [RavenAction("/databases/*/admin/integrations/postgresql/user", "PUT", AuthorizationStatus.DatabaseAdmin)]
@@ -173,7 +140,7 @@ namespace Raven.Server.Integrations.PostgreSQL.Handlers
                     if (config == null)
                     {
                         HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                        context.Write(writer, new DynamicJsonValue {["Error"] = "Unable to get usernames from database record"});
+                        context.Write(writer, new DynamicJsonValue { ["Error"] = "Unable to get usernames from database record" });
                         return;
                     }
 
@@ -206,19 +173,6 @@ namespace Raven.Server.Integrations.PostgreSQL.Handlers
             await DatabaseConfigurations(ServerStore.ModifyPostgreSqlConfiguration, "read-postgresql-config", GetRaftRequestIdFromQuery());
         }
 
-        private void AssertCanUsePostgreSqlIntegration()
-        {
-            if (Database.ServerStore.LicenseManager.CanUsePowerBi(false, out _))
-                return;
 
-            if (Database.ServerStore.LicenseManager.CanUsePostgreSqlIntegration(withNotification: true))
-            {
-                ServerStore.FeatureGuardian.Assert(Feature.PostgreSql, () => $"You have enabled the PostgreSQL integration via '{RavenConfiguration.GetKey(x => x.Integrations.PostgreSql.Enabled)}' configuration but " +
-                                                                             "this is an experimental feature and the server does not support experimental features. " +
-                                                                             $"Please enable experimental features by changing '{RavenConfiguration.GetKey(x => x.Core.FeaturesAvailability)}' configuration value to '{nameof(FeaturesAvailability.Experimental)}'.");
-            }
-
-            throw new LicenseLimitException("You cannot use this feature because your license doesn't allow neither PostgreSQL integration feature nor Power BI");
-        }
     }
 }
---
+++ src/Raven.Server/Integrations/PostgreSQL/Handlers/Processors/AbstractPostgreSqlIntegrationHandlerProcessorForGetUsernamesList.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Client.ServerWide;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Utils;
+using Raven.Server.Web;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Server.Integrations.PostgreSQL.Handlers.Processors;
+
+internal abstract class AbstractPostgreSqlIntegrationHandlerProcessorForGetUsernamesList<TRequestHandler, TOperationContext> : AbstractPostgreSqlIntegrationHandlerProcessor<TRequestHandler, TOperationContext>
+    where TRequestHandler : RequestHandler
+    where TOperationContext : JsonOperationContext
+{
+    protected AbstractPostgreSqlIntegrationHandlerProcessorForGetUsernamesList([NotNull] TRequestHandler requestHandler, [NotNull] JsonContextPoolBase<TOperationContext> contextPool)
+        : base(requestHandler, contextPool)
+    {
+    }
+
+    protected abstract string GetDatabaseName();
+
+    public override async ValueTask ExecuteAsync()
+    {
+        AssertCanUsePostgreSqlIntegration();
+
+        using (ContextPool.AllocateOperationContext(out JsonOperationContext context))
+        {
+            DatabaseRecord databaseRecord;
+
+            using (RequestHandler.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext transactionOperationContext))
+            using (transactionOperationContext.OpenReadTransaction())
+                databaseRecord = RequestHandler.ServerStore.Cluster.ReadDatabase(transactionOperationContext, GetDatabaseName(), out long index);
+
+            var usernames = new List<PostgreSqlUsername>();
+
+            var users = databaseRecord?.Integrations?.PostgreSql?.Authentication?.Users;
+
+            await using (var writer = new AsyncBlittableJsonTextWriter(context, RequestHandler.ResponseBodyStream()))
+            {
+                if (users != null)
+                {
+                    foreach (var user in users)
+                    {
+                        var username = new PostgreSqlUsername { Username = user.Username };
+                        usernames.Add(username);
+                    }
+                }
+
+                var dto = new PostgreSqlUsernames { Users = usernames };
+
+                var djv = (DynamicJsonValue)TypeConverter.ToBlittableSupportedType(dto);
+                writer.WriteObject(context.ReadObject(djv, "PostgreSqlUsernames"));
+            }
+        }
+    }
+}
