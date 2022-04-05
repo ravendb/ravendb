35ed1f2baeec5446d6f9f33e5458592005d04294
Pawel Pekrol <ppekrol@ravendb.net>
2022-04-05 12:57:22 +0300

    RavenDB-18274 implemented GET /databases/*/admin/integrations/postgresql/server/status

---
+++ src/Raven.Server/Integrations/PostgreSQL/Handlers/Processors/PostgreSqlIntegrationHandlerProcessorForGetServerStatus.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Utils;
+using Raven.Server.Web;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+
+namespace Raven.Server.Integrations.PostgreSQL.Handlers.Processors;
+
+internal class PostgreSqlIntegrationHandlerProcessorForGetServerStatus<TOperationContext> : AbstractPostgreSqlIntegrationHandlerProcessor<RequestHandler, TOperationContext>
+    where TOperationContext : JsonOperationContext
+{
+    public PostgreSqlIntegrationHandlerProcessorForGetServerStatus([NotNull] RequestHandler requestHandler, [NotNull] JsonContextPoolBase<TOperationContext> contextPool)
+        : base(requestHandler, contextPool)
+    {
+    }
+
+    public override async ValueTask ExecuteAsync()
+    {
+        AssertCanUsePostgreSqlIntegration();
+
+        using (ContextPool.AllocateOperationContext(out JsonOperationContext context))
+        await using (var writer = new AsyncBlittableJsonTextWriter(context, RequestHandler.ResponseBodyStream()))
+        {
+            var dto = new PostgreSqlServerStatus { Active = RequestHandler.Server.PostgresServer.Active };
+
+            var djv = (DynamicJsonValue)TypeConverter.ToBlittableSupportedType(dto);
+            writer.WriteObject(context.ReadObject(djv, "PostgreSqlServerStatus"));
+        }
+    }
+}
