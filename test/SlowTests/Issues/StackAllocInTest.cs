0281a629222047d7c9809181dc16e591d5832b4d
Maciej Aszyk <maciej@ravendb.net>
2022-04-06 13:27:53 +0200

    Get SO

---
+++ test/SlowTests/Issues/StackAllocInTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FastTests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues;
+
+public class StackAllocInTest : RavenTestBase
+{
+    public StackAllocInTest(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Fact]
+    public void GetStackoverflow()
+    {
+       Span<byte> SO = stackalloc byte[1024 * 1024 * 1024];
+       IfNotGetSOYet(0);
+    }
+
+    private int IfNotGetSOYet(int value)
+    {
+        return IfNotGetSOYet(value);
+    }
+}
