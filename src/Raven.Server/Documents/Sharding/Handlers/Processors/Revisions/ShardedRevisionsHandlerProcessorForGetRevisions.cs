7646e5f66657ddc5648bd939b208784fc7371945
Karmel <karmel@ravendb.net>
2022-04-06 17:02:18 +0300

    RavenDB-13111 avoid duplicated GetRevisionsCommand

--- src/Raven.Server/Documents/Sharding/Handlers/Processors/Revisions/ShardedRevisionsHandlerProcessorForGetRevisions.cs
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Revisions/ShardedRevisionsHandlerProcessorForGetRevisions.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Raven.Client;
+using Raven.Client.Documents.Commands;
 using Raven.Client.Documents.Operations.Revisions;
+using Raven.Client.Json;
 using Raven.Server.Documents.Handlers.Processors.Revisions;
 using Raven.Server.Documents.Sharding.Commands;
 using Raven.Server.Documents.Sharding.Operations;
@@ -51,7 +54,7 @@ namespace Raven.Server.Documents.Sharding.Handlers.Processors.Revisions
             }
             else
             {
-                await WriteRevisionsResultAsync(context, res);
+                await WriteRevisionsResultAsync(context, res, totalResult: null);
             }
 
             AddPagingPerformanceHint(PagingOperationType.Revisions, "", "", 0, 0, 0, 0);
@@ -59,14 +62,14 @@ namespace Raven.Server.Documents.Sharding.Handlers.Processors.Revisions
 
         protected override async ValueTask GetRevisionsAsync(TransactionOperationContext context, string id, DateTime? before, int start, int pageSize, bool metadataOnly, CancellationToken token)
         {
-            GetRawRevisionsCommand cmd;
+            GetRevisionsCommand cmd;
             if (before.HasValue)
             {
-                cmd = new GetRawRevisionsCommand(RequestHandler, id, before.Value);
+                cmd = new GetRevisionsCommand(id, before.Value);
             }
             else
             {
-                cmd = new GetRawRevisionsCommand(RequestHandler, id, start, pageSize, metadataOnly);
+                cmd = new GetRevisionsCommand(id, start, pageSize, metadataOnly);
             }
 
             string etag = null; //TODO
@@ -80,9 +83,10 @@ namespace Raven.Server.Documents.Sharding.Handlers.Processors.Revisions
                 HttpContext.Response.Headers[Constants.Headers.Etag] = "\"" + etag + "\"";
 
             int shardNumber = RequestHandler.DatabaseContext.GetShardNumber(context, id);
+            var result = await RequestHandler.ExecuteSingleShardAsync(context, cmd, shardNumber, token);
+            var array = result.Results.Items.Select(x => ((BlittableJsonReaderObject)x).Clone(context)).ToArray();
 
-            //cmd writes the response to stream as is
-            await RequestHandler.ShardExecutor.ExecuteSingleShardAsync(context, cmd, shardNumber, token);
+            await WriteRevisionsResultAsync(context, array, result.TotalResults);
 
             AddPagingPerformanceHint(PagingOperationType.Revisions, "", "", 0, 0, 0, 0);
         }
@@ -93,12 +97,18 @@ namespace Raven.Server.Documents.Sharding.Handlers.Processors.Revisions
             return false;
         }
 
-        private async ValueTask WriteRevisionsResultAsync(JsonOperationContext context, BlittableJsonReaderObject[] revisions)
+        private async ValueTask WriteRevisionsResultAsync(JsonOperationContext context, BlittableJsonReaderObject[] array, long? totalResult)
         {
             await using (var writer = new AsyncBlittableJsonTextWriter(context, RequestHandler.ResponseBodyStream()))
             {
                 writer.WriteStartObject();
-                writer.WriteArray(nameof(RevisionsResult<Document>.Results), revisions);
+                writer.WriteArray(nameof(BlittableArrayResult.Results), array);
+                if (totalResult.HasValue)
+                {
+                    writer.WriteComma();
+                    writer.WritePropertyName(nameof(BlittableArrayResult.TotalResults));
+                    writer.WriteInteger(totalResult.Value);
+                }
                 writer.WriteEndObject();
             }
         }
