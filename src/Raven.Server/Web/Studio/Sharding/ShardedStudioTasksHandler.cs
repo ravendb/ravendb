2ea5a4b889a8d2ff3f62b6f3d78b21bd8bd77779
shiranshalom <shiran@ravendb.net>
2022-04-07 17:24:33 +0300

    RavenDB-18226 - implemented POST /studio-tasks/periodic-backup/test-credentials

---
+++ src/Raven.Server/Web/Studio/Sharding/ShardedStudioTasksHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Raven.Server.Documents.Sharding.Handlers;
+using Raven.Server.Routing;
+
+namespace Raven.Server.Web.Studio.Sharding
+{
+    public class ShardedStudioTasksHandler : ShardedDatabaseRequestHandler
+    {
+        [RavenShardedAction("/studio-tasks/periodic-backup/test-credentials", "POST")]
+        public async Task TestPeriodicBackupCredentials()
+        {
+            await StudioTasksHandler.TestPeriodicBackupCredentials(this);
+        }
+    }
+}
