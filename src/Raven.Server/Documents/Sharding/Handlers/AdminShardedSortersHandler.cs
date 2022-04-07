340abe04b418179cb1c1b0bf2d327b4bbe32317f
Grisha Kotler <grisha@ayende.com>
2022-04-07 19:36:57 +0300

    RavenDB-18165 - throw if trying to use a custom sorter

---
+++ src/Raven.Server/Documents/Sharding/Handlers/AdminShardedSortersHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Raven.Server.Routing;
+
+namespace Raven.Server.Documents.Sharding.Handlers;
+
+public class AdminShardedSortersHandler : ShardedDatabaseRequestHandler
+{
+    [RavenShardedAction("/databases/*/admin/sorters", "PUT")]
+    public async Task Put()
+    {
+        throw new NotSupportedException("Custom sorting is not supported in sharding as of yet");
+    }
+}
