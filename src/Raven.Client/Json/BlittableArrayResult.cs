3d10ece8670a53f350b10db7460240f27219a345
Karmel <karmel@ravendb.net>
2022-04-07 09:46:50 +0300

    RavenDB-13111 address PR comments

--- src/Raven.Client/Json/BlittableArrayResult.cs
+++ src/Raven.Client/Json/BlittableArrayResult.cs
@@ -5,7 +5,7 @@ namespace Raven.Client.Json
     public class BlittableArrayResult
     {
         public BlittableJsonReaderArray Results { get; set; }
-        public int TotalResults { get; set; }
+        public long TotalResults { get; set; }
     }
 
     public class BatchCommandResult
