faa75a856eb85f041ea0e21f68f604bb5d162ee3
Marcin Lewandowski <marcin@ravendb.net>
2022-04-08 11:19:41 +0200

    RavenDB-18444 Can't view sharded database on a remote node

--- src/Raven.Studio/typescript/components/utils/DatabaseUtils.ts
+++ src/Raven.Studio/typescript/components/utils/DatabaseUtils.ts
@@ -1,13 +1,15 @@
-﻿import { data } from "jquery";
-
+﻿
 export default class DatabaseUtils {
     
     static isSharded(name: string) {
         return name.includes("$");
     }
-
-
-    static extractName(name: string) {
+    
+    static shardGroupKey(name: string) {
         return DatabaseUtils.isSharded(name) ? name.split("$")[0] : name;
+    }
+    
+    static shardNumber(name: string): number {
+        return parseInt(name.split("$")[1], 10);
     }
 }
--- src/Raven.Studio/typescript/models/resources/shardedDatabase.ts
+++ src/Raven.Studio/typescript/models/resources/shardedDatabase.ts
@@ -1,5 +1,6 @@
 import database from "models/resources/database";
 import shard from "models/resources/shard";
+import DatabaseUtils from "../../components/utils/DatabaseUtils";
 
 class shardedDatabase extends database {
     
@@ -7,6 +8,12 @@ class shardedDatabase extends database {
     
     static isSharded(db: database): db is shardedDatabase {
         return db instanceof shardedDatabase;
+    }
+    
+    constructor(dbInfo: Raven.Client.ServerWide.Operations.DatabaseInfo[], clusterNodeTag: KnockoutObservable<string>) {
+        super(dbInfo[0], clusterNodeTag);
+        
+        this.updateUsingGroup(dbInfo);
     }
 
     get root(): database {
@@ -26,6 +33,19 @@ class shardedDatabase extends database {
         })
         
         return locationSpecifiers;
+    }
+
+    updateUsingGroup(dbs: Raven.Client.ServerWide.Operations.DatabaseInfo[]) {
+        // update shared info
+        this.updateUsing(dbs[0]);
+        
+        this.shards(dbs.map(db => new shard(db, this)));
+        
+        const nodes = this.shards().flatMap(x => x.nodes());
+        
+        // in sharded environment db is relevant if any shard exists on local node
+        this.relevant(!!nodes.find(x => x === this.clusterNodeTag()));
+        this.name = DatabaseUtils.shardGroupKey(dbs[0].Name);
     }
 }
 
