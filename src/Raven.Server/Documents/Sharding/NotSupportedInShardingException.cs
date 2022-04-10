c5ea405d75a178298912a4cfbe5055bbc56df5e6
Grisha Kotler <grisha@ayende.com>
2022-04-10 13:55:22 +0300

    RavenDB-18165 - use NotSupportedInShardingException

---
+++ src/Raven.Server/Documents/Sharding/NotSupportedInShardingException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Raven.Server.Documents.Sharding;
+
+public class NotSupportedInShardingException : Exception
+{
+    public NotSupportedInShardingException()
+    {
+    }
+
+    public NotSupportedInShardingException(string message) : base(message)
+    {
+    }
+
+    public NotSupportedInShardingException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
