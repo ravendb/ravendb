efe3ea57cc1fa386848ca6c2073f204771009840
Federico Lois <federico.lois@gmail.com>
2022-04-13 13:55:08 -0300

    RavenDB-16967: Random sampling based training for very large trees.

--- src/Voron/Data/CompactTrees/CompactTree.Iterator.cs
+++ src/Voron/Data/CompactTrees/CompactTree.Iterator.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 using Sparrow.Server.Strings;
 
 namespace Voron.Data.CompactTrees
 {
     partial class CompactTree
     {
-        // TODO: Implement this and use it instead of the tree directly. 
         public unsafe struct Iterator 
         {
             private readonly CompactTree _tree;
@@ -88,16 +84,6 @@ namespace Voron.Data.CompactTrees
                 }
             }
 
-            public bool MovePrev(out Span<byte> key, out long value)
-            {
-                throw new NotImplementedException();
-            }
-
-            public bool MovePrev(out Slice key, out long value)
-            {
-                throw new NotImplementedException();
-            }
-
             public bool Skip(long count)
             {
                 ref var state = ref _cursor._stk[_cursor._pos];
@@ -118,11 +104,8 @@ namespace Voron.Data.CompactTrees
                 }
 
                 return true;
-
             }
         }
-
-
 
         public unsafe struct FuzzyIterator
         {
@@ -142,7 +125,7 @@ namespace Voron.Data.CompactTrees
             public void Seek(ReadOnlySpan<byte> key)
             {
                 _tree.FuzzyFindPageFor(key, ref _cursor);
-                
+
                 ref var state = ref _cursor._stk[_cursor._pos];
                 if (state.LastSearchPosition < 0)
                     state.LastSearchPosition = ~state.LastSearchPosition;
@@ -150,7 +133,7 @@ namespace Voron.Data.CompactTrees
 
             public void Reset()
             {
-                Seek(_baseKey);                
+                Seek(_baseKey);
             }
 
             public bool MoveNext(out Slice key, out long value, out float score)
@@ -197,7 +180,7 @@ namespace Voron.Data.CompactTrees
                         // https://event.cwi.nl/SIGMOD-RWE/2010/12-16bf4c/paper.pdf
                         if (key[0] != firstLetter)
                             goto IsDone;
-                        
+
                         score = currentScore;
                         return true;
                     }
@@ -211,16 +194,6 @@ namespace Voron.Data.CompactTrees
                 value = default;
                 score = 0;
                 return false;
-            }
-
-            public bool MovePrev(out Span<byte> key, out long value, out float score)
-            {
-                throw new NotImplementedException();
-            }
-
-            public bool MovePrev(out Slice key, out long value, out float score)
-            {
-                throw new NotImplementedException();
             }
 
             public bool Skip(long count)
@@ -245,15 +218,82 @@ namespace Voron.Data.CompactTrees
             }
         }
 
+        public unsafe struct RandomIterator
+        {
+            private readonly int _samples;
+            private readonly Random _generator;
+            private readonly CompactTree _tree;
+            private int _currentSample;
+            private IteratorCursorState _cursor;
+
+            public RandomIterator(CompactTree tree, int samples, int seed = 0)
+            {
+                _tree = tree;
+                _samples = samples;
+                _currentSample = 0;
+                _generator = (seed == 0) ? new() : new(seed);
+                _cursor = new() { _stk = new CursorState[8], _pos = -1, _len = 0 };
+            }
+
+            public void Reset()
+            {
+                _cursor._pos = -1;
+                _cursor._len = 0;
+                _currentSample = 0;
+            }
+
+            public bool MoveNext(out Span<byte> key, out long value)
+            {
+                if (_currentSample >= _samples)
+                    goto Failure;
+
+                _cursor._pos = -1;
+                _cursor._len = 0;
+                _tree.PushPage(_tree._state.RootPage, ref _cursor);
+
+                ref var cState = ref _cursor;
+                ref var state = ref cState._stk[cState._pos];
+                if (state.Header->NumberOfEntries == 0)
+                    goto Failure;
+
+                int randomEntry;
+                while (!state.Header->PageFlags.HasFlag(CompactPageFlags.Leaf))
+                {
+                    randomEntry = _generator.Next(state.Header->NumberOfEntries);
+
+                    var next = GetValue(ref state, randomEntry);
+                    _tree.PushPage(next, ref cState);
+
+                    state = ref cState._stk[cState._pos];
+                }
+
+                randomEntry = _generator.Next(state.Header->NumberOfEntries);
+                GetEntry(_tree, state.Page, state.EntriesOffsets[randomEntry], out key, out value);
+
+                _currentSample++;
+                return true;
+
+
+            Failure:
+                key = Span<byte>.Empty;
+                Unsafe.SkipInit(out value);
+                return false;
+            }
+        }
+
         public FuzzyIterator FuzzyIterate(Slice fuzzyKey, float distance)
         {
             return new FuzzyIterator(this, fuzzyKey, distance);
         }
 
+        public RandomIterator RandomIterate(int samples, int seed = 0)
+        {
+            return new RandomIterator(this, samples, seed);
+        }
+
         public Iterator Iterate()
         {
             return new Iterator(this);
-        }
-
+        }        
     }
 }
