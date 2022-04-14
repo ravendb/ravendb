90e51e8dbed09e28e73410960fca88b673bde12d
shiranshalom <shiran@ravendb.net>
2022-04-14 10:09:38 +0300

    RavenDB-18228 - Sharding - Edit External Replication View Endpoints

---
+++ src/Raven.Server/Documents/Handlers/Processors/OngoingTasks/OngoingTasksHandlerProcessorForUpdateExternalReplication.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Raven.Server.Documents.Handlers.Processors.OngoingTasks
+{
+    internal class OngoingTasksHandlerProcessorForUpdateExternalReplication : AbstractOngoingTasksHandlerProcessorForUpdateExternalReplication<DatabaseRequestHandler>
+    {
+        public OngoingTasksHandlerProcessorForUpdateExternalReplication([NotNull] DatabaseRequestHandler requestHandler) : base(requestHandler)
+        {
+        }
+
+        protected override string GetDatabaseName() => RequestHandler.Database.Name;
+
+        protected override async ValueTask WaitForIndexNotificationAsync(long index)
+        {
+            await RequestHandler.Database.RachisLogIndexNotifications.WaitForIndexNotification(index, RequestHandler.Database.ServerStore.Engine.OperationTimeout);
+        }
+    }
+}
---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Replication/ShardedOngoingTasksHandlerProcessorForUpdateExternalReplication.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Raven.Server.Documents.Handlers.Processors.OngoingTasks;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors.Replication
+{
+    internal class ShardedOngoingTasksHandlerProcessorForUpdateExternalReplication : AbstractOngoingTasksHandlerProcessorForUpdateExternalReplication<ShardedDatabaseRequestHandler>
+    {
+        public ShardedOngoingTasksHandlerProcessorForUpdateExternalReplication([NotNull] ShardedDatabaseRequestHandler requestHandler) : base(requestHandler)
+        {
+        }
+
+        protected override string GetDatabaseName() => RequestHandler.DatabaseContext.DatabaseName;
+
+        protected override ValueTask WaitForIndexNotificationAsync(long index) => RequestHandler.DatabaseContext.Cluster.WaitForExecutionOnAllNodesAsync(index);
+    }
+}
