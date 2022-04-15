c7a263c3d51c6040117e2b093c5b3102df40cc35
Arkadiusz Palinski <arek@ravendb.net>
2022-04-15 13:19:01 +0200

    RavenDB-18199 Using processor pattern for HEAD /databases/*/docs

---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Documents/ShardedDocumentHandlerProcessorForHead.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Client;
+using Raven.Server.Documents.Handlers.Processors.Documents;
+using Raven.Server.Documents.Sharding.Commands;
+using Raven.Server.ServerWide.Context;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors.Documents;
+
+internal class ShardedDocumentHandlerProcessorForHead : AbstractDocumentHandlerProcessorForHead<ShardedDocumentHandler, TransactionOperationContext>
+{
+    public ShardedDocumentHandlerProcessorForHead([NotNull] ShardedDocumentHandler requestHandler, [NotNull] JsonContextPoolBase<TransactionOperationContext> contextPool) : base(requestHandler, contextPool)
+    {
+    }
+
+    protected override async ValueTask<(HttpStatusCode StatusCode, string ChangeVector)> GetStatusCodeAndChangeVector(string docId, TransactionOperationContext context)
+    {
+        var index = RequestHandler.DatabaseContext.GetShardNumber(context, docId);
+
+        var cmd = new ShardedHeadCommand(RequestHandler, Headers.IfNoneMatch);
+
+        await RequestHandler.DatabaseContext.ShardExecutor.ExecuteSingleShardAsync(cmd, index);
+
+        return (cmd.StatusCode, cmd.Result);
+    }
+}
