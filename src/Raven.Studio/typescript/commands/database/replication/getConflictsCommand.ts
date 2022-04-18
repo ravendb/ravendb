8ec6eab2d498eb6afd0314f99d0855a5f3ff83a3
danielle9897 <danielle.9897@gmail.com>
2022-04-18 16:02:59 +0300

    RavenDB-18287 Sharding - Conflicts View

--- src/Raven.Studio/typescript/commands/database/replication/getConflictsCommand.ts
+++ src/Raven.Studio/typescript/commands/database/replication/getConflictsCommand.ts
@@ -4,21 +4,36 @@ import endpoints = require("endpoints");
 
 class getConflictsCommand extends commandBase {
 
-    constructor(private ownerDb: database, private start: number, private pageSize: number) {
+    constructor(private ownerDb: database, private start: number, private pageSize: number, private continuationToken?: string) {
         super();
     }
 
-    execute(): JQueryPromise<pagedResult<replicationConflictListItemDto>> {
-        const url = endpoints.databases.replication.replicationConflicts;
+    execute(): JQueryPromise<pagedResultWithToken<replicationConflictListItemDto>> {
+        const args = this.getArgsToUse();
+        const url = endpoints.databases.replication.replicationConflicts + this.urlEncodeArgs(args);
 
-        const transformer = (result: resultsWithTotalCountDto<replicationConflictListItemDto>): pagedResult<replicationConflictListItemDto> => {
+        const transformer = (result: resultsWithCountAndToken<replicationConflictListItemDto>): pagedResultWithToken<replicationConflictListItemDto> => {
             return {
                 items: result.Results,
-                totalResultCount: result.TotalResults
+                totalResultCount: result.TotalResults,
+                continuationToken: result.ContinuationToken
             };
         }
 
         return this.query<pagedResult<replicationConflictListItemDto>>(url, null, this.ownerDb, transformer);
+    }
+
+    private getArgsToUse() {
+        if (this.continuationToken) {
+            return {
+                continuationToken: this.continuationToken
+            };
+        }
+
+        return {
+            start: this.start,
+            pageSize: this.pageSize
+        };
     }
 }
 
