c342eb39d7c47e258fc2a66729b7d021fbe0846d
Marcin Lewandowski <marcin@ravendb.net>
2022-04-20 08:36:09 +0200

    RavenDB-18387 Sharding - Index Terms View

--- src/Raven.Studio/typescript/common/shell/menu/items/indexes.ts
+++ src/Raven.Studio/typescript/common/shell/menu/items/indexes.ts
@@ -65,6 +65,7 @@ function getIndexesMenuItem(appUrls: computedAppUrls) {
             title: 'Terms',
             route: 'databases/indexes/terms/(:indexName)',
             moduleId: require('viewmodels/database/indexes/indexTerms'),
+            shardingMode: "singleShardOnly",
             css: 'icon-terms',
             nav: false
         })
