9dc747f68220ba8273086c0a915326fcfa52324b
Pawel Pekrol <ppekrol@ravendb.net>
2022-04-21 10:42:14 +0200

    RavenDB-18503 implemented /databases/*/identity/next endpoint

---
+++ src/Raven.Server/Documents/Handlers/Processors/Identities/IdentityHandlerProcessorForNextIdentityFor.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Handlers.Processors.Identities;
+
+internal class IdentityHandlerProcessorForNextIdentityFor : AbstractIdentityHandlerProcessorForNextIdentityFor<DatabaseRequestHandler, DocumentsOperationContext>
+{
+    public IdentityHandlerProcessorForNextIdentityFor([NotNull] DatabaseRequestHandler requestHandler) 
+        : base(requestHandler, requestHandler.ContextPool)
+    {
+    }
+
+    protected override char GetDatabaseIdentityPartsSeparator() => RequestHandler.Database.IdentityPartsSeparator;
+
+    protected override string GetDatabaseName() => RequestHandler.Database.Name;
+}
