d9e977505639de3d6de575d619b778e3650f307b
Arkadiusz Palinski <arek@ravendb.net>
2022-04-21 15:32:00 +0200

    RavenDB-18199 Using processor pattern for GET /databases/*/docs

---
+++ src/Raven.Server/Documents/Handlers/Processors/Documents/AbstractDocumentHandlerProcessorForGet.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Primitives;
+using Raven.Server.Web;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Handlers.Processors.Documents;
+
+internal abstract class AbstractDocumentHandlerProcessorForGet<TRequestHandler, TOperationContext> : AbstractHandlerProcessor<TRequestHandler, TOperationContext>
+    where TRequestHandler : RequestHandler
+    where TOperationContext : JsonOperationContext
+{
+    protected AbstractDocumentHandlerProcessorForGet([NotNull] TRequestHandler requestHandler, [NotNull] JsonContextPoolBase<TOperationContext> contextPool) : base(requestHandler, contextPool)
+    {
+    }
+
+    public override async ValueTask ExecuteAsync()
+    {
+        var ids = RequestHandler.GetStringValuesQueryString("id", required: false);
+        var metadataOnly = RequestHandler.GetBoolValueQueryString("metadataOnly", required: false) ?? false;
+
+        using (ContextPool.AllocateOperationContext(out TOperationContext context))
+        {
+            await GetDocumentsAsync(context, ids, metadataOnly);
+        }
+    }
+
+    protected abstract ValueTask GetDocumentsAsync(TOperationContext context, StringValues ids, bool metadataOnly);
+}
---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Documents/ShardedDocumentHandlerProcessorForGet.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Primitives;
+using Raven.Server.Documents.Handlers.Processors.Documents;
+using Raven.Server.ServerWide.Context;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors.Documents;
+
+internal class ShardedDocumentHandlerProcessorForGet : AbstractDocumentHandlerProcessorForGet<ShardedDocumentHandler, TransactionOperationContext>
+{
+    public ShardedDocumentHandlerProcessorForGet([NotNull] ShardedDocumentHandler requestHandler, [NotNull] JsonContextPoolBase<TransactionOperationContext> contextPool) : base(requestHandler, contextPool)
+    {
+    }
+
+    protected override async ValueTask GetDocumentsAsync(TransactionOperationContext context, StringValues ids, bool metadataOnly)
+    {
+        var includePaths = RequestHandler.GetStringValuesQueryString("include", required: false);
+        var etag = RequestHandler.GetStringFromHeaders("If-None-Match");
+
+        if (ids.Count > 0)
+        {
+            await RequestHandler.GetDocumentsByIdAsync(ids, includePaths, etag, metadataOnly, context);
+        }
+        else
+        {
+            await RequestHandler.GetDocumentsAsync(context, metadataOnly);
+        }
+    }
+}
