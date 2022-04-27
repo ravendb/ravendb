97e3aa059cbf231fe29d6513c227c50f3dac08c6
Federico Lois <federico.lois@gmail.com>
2022-04-27 13:14:07 -0300

    RavenDB-17599: NRE exceptions and support for compressed strings.

--- src/Raven.Server/Documents/Indexes/Persistence/Corax/CoraxQueryBuilder.cs
+++ src/Raven.Server/Documents/Indexes/Persistence/Corax/CoraxQueryBuilder.cs
@@ -560,7 +560,7 @@ public static class CoraxQueryBuilder
         Debug.Assert(metadata.IsDynamic == false || metadata.WhereFields[fieldName].IsFullTextSearch);
 
         var valueAsString = QueryBuilderHelper.CoraxGetValueAsString(value);
-        if (!highlightingTerms.TryGetValue(fieldName, out var highlightingTerm))
+        if (highlightingTerms != null && !highlightingTerms.TryGetValue(fieldName, out var highlightingTerm))
         {
             highlightingTerm = new CoraxHighlightingTermIndex
             {
@@ -568,16 +568,20 @@ public static class CoraxQueryBuilder
                 Values = valueAsString,
             };
             highlightingTerms?.TryAdd(fieldName, highlightingTerm);
+
+            if (metadata.IsDynamic && isDocumentId == false)
+            {
+                fieldName = new QueryFieldName(AutoIndexField.GetSearchAutoIndexFieldName(fieldName.Value), fieldName.IsQuoted);
+
+                // We now add the dynamic field too. 
+                highlightingTerm.DynamicFieldName = fieldName;
+                highlightingTerms?.TryAdd(fieldName, highlightingTerm);
+            }
         }
-     
-        if (metadata.IsDynamic && isDocumentId == false)
+        else if (metadata.IsDynamic && isDocumentId == false)
         {
             fieldName = new QueryFieldName(AutoIndexField.GetSearchAutoIndexFieldName(fieldName.Value), fieldName.IsQuoted);
-            
-            // We now add the dynamic field too. 
-            highlightingTerm.DynamicFieldName = fieldName;
-            highlightingTerms?.TryAdd(fieldName, highlightingTerm);
-        }            
+        }
 
         var fieldId = QueryBuilderHelper.GetFieldId(fieldName, index, indexMapping, queryMapping);
 
