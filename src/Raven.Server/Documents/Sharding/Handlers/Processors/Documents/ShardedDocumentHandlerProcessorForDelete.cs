61f3deff78eef12a950d40f8f21ea0d76599cc74
Arkadiusz Palinski <arek@ravendb.net>
2022-04-28 15:24:50 +0200

    RavenDB-18199 Using processor pattern for DELETE /databases/*/docs

---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Documents/ShardedDocumentHandlerProcessorForDelete.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Client.Documents.Commands;
+using Raven.Server.Documents.Handlers.Processors.Documents;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Web.Http;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors.Documents;
+
+internal class ShardedDocumentHandlerProcessorForDelete : AbstractDocumentHandlerProcessorForDelete<ShardedDocumentHandler, TransactionOperationContext>
+{
+    public ShardedDocumentHandlerProcessorForDelete([NotNull] ShardedDocumentHandler requestHandler, [NotNull] JsonContextPoolBase<TransactionOperationContext> contextPool) : base(requestHandler, contextPool)
+    {
+    }
+
+    protected override async ValueTask HandleDeleteDocument(string docId, string changeVector)
+    {
+        var command = new DeleteDocumentCommand(docId, changeVector);
+
+        int shardNumber;
+        using (ContextPool.AllocateOperationContext(out TransactionOperationContext context))
+            shardNumber = RequestHandler.DatabaseContext.GetShardNumber(context, docId);
+
+        using (var token = RequestHandler.CreateOperationToken())
+        {
+            var proxyCommand = new ProxyCommand(command, HttpContext.Response);
+
+            await RequestHandler.ShardExecutor.ExecuteSingleShardAsync(proxyCommand, shardNumber, token.Token);
+        }
+    }
+}
