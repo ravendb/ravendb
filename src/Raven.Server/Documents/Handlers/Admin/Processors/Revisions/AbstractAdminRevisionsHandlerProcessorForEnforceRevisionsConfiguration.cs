ea389fd83e699661d0804e9010f71c041c09c4cd
Pawel Pekrol <ppekrol@ravendb.net>
2022-04-28 15:31:00 +0200

    RavenDB-18272 adjustments

--- src/Raven.Server/Documents/Handlers/Admin/Processors/Revisions/AbstractAdminRevisionsHandlerProcessorForEnforceRevisionsConfiguration.cs
+++ src/Raven.Server/Documents/Handlers/Admin/Processors/Revisions/AbstractAdminRevisionsHandlerProcessorForEnforceRevisionsConfiguration.cs
@@ -3,26 +3,23 @@ using JetBrains.Annotations;
 using Raven.Server.Documents.Handlers.Processors;
 using Raven.Server.Json;
 using Raven.Server.ServerWide;
-using Raven.Server.Web;
 using Sparrow.Json;
 
 namespace Raven.Server.Documents.Handlers.Admin.Processors.Revisions
 {
     internal abstract class AbstractAdminRevisionsHandlerProcessorForEnforceRevisionsConfiguration<TRequestHandler, TOperationContext> : AbstractHandlerProcessor<TRequestHandler, TOperationContext>
-        where TRequestHandler : RequestHandler
+        where TRequestHandler : AbstractDatabaseRequestHandler
         where TOperationContext : JsonOperationContext
     {
-        public AbstractAdminRevisionsHandlerProcessorForEnforceRevisionsConfiguration([NotNull] TRequestHandler requestHandler, [NotNull] JsonContextPoolBase<TOperationContext> contextPool) : base(requestHandler, contextPool)
+        protected AbstractAdminRevisionsHandlerProcessorForEnforceRevisionsConfiguration([NotNull] TRequestHandler requestHandler, [NotNull] JsonContextPoolBase<TOperationContext> contextPool) : base(requestHandler, contextPool)
         {
         }
 
         protected abstract ValueTask AddOperationAsync(long operationId, OperationCancelToken token);
 
-        protected abstract OperationCancelToken CreateTimeLimitedOperationToken();
-
         public override async ValueTask ExecuteAsync()
         {
-            var token = CreateTimeLimitedOperationToken();
+            var token = RequestHandler.CreateTimeLimitedOperationToken();
             var operationId = RequestHandler.GetLongQueryString("operationId", false) ?? -RequestHandler.ServerStore.Operations.GetNextOperationId();
 
             await AddOperationAsync(operationId, token);
