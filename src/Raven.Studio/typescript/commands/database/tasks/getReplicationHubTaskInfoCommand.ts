a5094a92b3a0cc72a46052af0a0beb6406cfdb08
danielle9897 <danielle.9897@gmail.com>
2022-05-01 18:21:55 +0300

    RavenDB-18229 Sharding - Edit Replication Hub View - 1. ignore loadMentors if sharded db + implement single-shard-only for the GET

--- src/Raven.Studio/typescript/commands/database/tasks/getReplicationHubTaskInfoCommand.ts
+++ src/Raven.Studio/typescript/commands/database/tasks/getReplicationHubTaskInfoCommand.ts
@@ -1,6 +1,8 @@
 import commandBase = require("commands/commandBase");
 import database = require("models/resources/database");
 import endpoints = require("endpoints");
+import clusterTopologyManager from "common/shell/clusterTopologyManager";
+import { shardingTodo } from "common/developmentHelper";
 
 class getReplicationHubTaskInfoCommand extends commandBase {
 
@@ -9,8 +11,13 @@ class getReplicationHubTaskInfoCommand extends commandBase {
     }
 
     execute(): JQueryPromise<Raven.Client.Documents.Operations.Replication.PullReplicationDefinitionAndCurrentConnections> {
+        
+        shardingTodo("ANY", "Currently, the Hub GET is implemented only for Single-Shard-Only (while PUT is All-Shards-Only)");
+        const location = this.db.getFirstLocation(clusterTopologyManager.default.localNodeTag());
+        
         const args = {
-            key: this.taskId
+            key: this.taskId,
+            ...location
         };
         
         const url = endpoints.databases.ongoingTasks.tasksPullReplicationHub + this.urlEncodeArgs(args);
