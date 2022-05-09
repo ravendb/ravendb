0059251ae36ff1365612d8f9541d865b23ba743e
aviv <aviv@ayende.com>
2022-05-09 16:26:39 +0300

    RavenDB-18496 : test adjustments

--- test/SlowTests/Issues/RavenDB_18496.cs
+++ test/SlowTests/Issues/RavenDB_18496.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using FastTests.Server.Replication;
+using Raven.Server.ServerWide.Context;
 using Sparrow;
 using Xunit;
 using Xunit.Abstractions;
@@ -14,7 +16,7 @@ namespace SlowTests.Issues
         
 
         [Fact]
-        public async Task DeletingMasterKeyForExistedEncryptedDatabaseShouldFail_2()
+        public async Task ReplicationShouldNotGetStuckWhenEncryptionBufferSizeIsGreaterThanMaxSizeToSend()
         {
             EncryptedServer(out var certificates, out var databaseName);
 
@@ -28,11 +30,38 @@ namespace SlowTests.Issues
             using (var store2 = GetDocumentStore(new Options { ClientCertificate = certificates.ServerCertificate.Value }))
             {
                 var db = await GetDocumentDatabaseInstanceFor(encryptedStore);
-                db.Configuration.Replication.MaxSizeToSend = new Size(16, SizeUnit.Kilobytes);
+                var maxSizeToSend = new Size(16, SizeUnit.Kilobytes);
+                db.Configuration.Replication.MaxSizeToSend = maxSizeToSend;
+
+                const string docId = "users/1";
+                using (var session = encryptedStore.OpenAsyncSession())
+                {
+                    var entity = new 
+                    {
+                        Data = Sparrow.Server.Sodium.GenerateRandomBuffer(20 * 1024)
+                    };
+
+                    await session.StoreAsync(entity, docId);
+                    await session.SaveChangesAsync();
+                }
+
+                using (db.DocumentsStorage.ContextPool.AllocateOperationContext(out DocumentsOperationContext ctx))
+                using (ctx.OpenReadTransaction())
+                {
+                    var doc = db.DocumentsStorage.GetDocumentsFrom(ctx, 0).SingleOrDefault();
+                    Assert.NotNull(doc);
+                    Assert.Equal(docId, doc.Id);
+
+                    long maxSize = maxSizeToSend.GetValue(SizeUnit.Bytes);
+                    long encryptionBufferSize = ctx.Transaction.InnerTransaction.LowLevelTransaction.TotalEncryptionBufferSize.GetValue(SizeUnit.Bytes);
+
+                    Assert.True(doc.Data.Size > maxSize);
+                    Assert.True(encryptionBufferSize > maxSize);
+                }
 
                 await SetupReplicationAsync(encryptedStore, store2);
 
-                await EnsureReplicatingAsync(encryptedStore, store2);
+                Assert.True(WaitForDocument(store2, docId));
 
                 await EnsureNoReplicationLoop(Server, databaseName);
             }
