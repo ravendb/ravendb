650764fca3cbf12f2088adcf733d4d9dc9c2f164
shiranshalom <shiran@ravendb.net>
2022-05-09 16:24:33 +0300

    RavenDB-18232 - fixing PR comments#1

---
+++ src/Raven.Server/Web/System/Processors/OngoingTasks/OngoingTasksHandlerProcessorForGetOngoingTaskInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Server.Documents;
+
+namespace Raven.Server.Web.System.Processors.OngoingTasks
+{
+    internal class OngoingTasksHandlerProcessorForGetOngoingTaskInfo : OngoingTasksHandlerProcessorForGetOngoingTasksInfo
+    {
+        public OngoingTasksHandlerProcessorForGetOngoingTaskInfo([NotNull] DatabaseRequestHandler requestHandler) : base(requestHandler)
+        {
+        }
+
+        public override async ValueTask ExecuteAsync()
+        {
+            await GetOngoingTaskInfoInternalAsync();
+        }
+    }
+}
