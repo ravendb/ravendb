afbdb51a4a2b483e14538dfe1f20e85f96c13bef
Stav <stav@ravendb.net>
2022-05-10 12:34:50 +0300

    RavenDB-18104 - PUT /databases/*/subscriptions

---
+++ src/Raven.Server/Documents/Handlers/Processors/Subscriptions/SubscriptionsHandlerProcessorForPutSubscription.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Client.Documents.Subscriptions;
+using Raven.Server.ServerWide.Context;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Handlers.Processors.Subscriptions
+{
+    internal class SubscriptionsHandlerProcessorForPutSubscription : AbstractSubscriptionsHandlerProcessorForPutSubscription<SubscriptionsHandler, DocumentsOperationContext>
+    {
+        public SubscriptionsHandlerProcessorForPutSubscription([NotNull] SubscriptionsHandler requestHandler) : base(requestHandler)
+        {
+        }
+
+        protected override async ValueTask CreateInternalAsync(BlittableJsonReaderObject bjro, SubscriptionCreationOptions options, DocumentsOperationContext context, long? id, bool? disabled)
+        {
+            using (context.OpenReadTransaction())
+            {
+                await RequestHandler.CreateInternalAsync(bjro, options, context, id, disabled);
+            }
+        }
+    }
+}
