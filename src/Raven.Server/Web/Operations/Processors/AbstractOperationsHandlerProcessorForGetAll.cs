d8e4afe9bda473f9b436c767b194d55e2e3a8ab1
Pawel Pekrol <ppekrol@ravendb.net>
2022-05-16 15:39:31 +0200

    RavenDB-18632 implemented /databases/*/operations endpoint

---
+++ src/Raven.Server/Web/Operations/Processors/AbstractOperationsHandlerProcessorForGetAll.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Server.Documents;
+using Raven.Server.Documents.Handlers.Processors;
+using Raven.Server.Documents.Operations;
+using Sparrow.Json;
+
+namespace Raven.Server.Web.Operations.Processors;
+
+internal abstract class AbstractOperationsHandlerProcessorForGetAll<TRequestHandler, TOperationContext> : AbstractDatabaseHandlerProcessor<TRequestHandler, TOperationContext>
+    where TOperationContext : JsonOperationContext
+    where TRequestHandler : AbstractDatabaseRequestHandler<TOperationContext>
+{
+    protected AbstractOperationsHandlerProcessorForGetAll([NotNull] TRequestHandler requestHandler) : base(requestHandler)
+    {
+    }
+
+    protected abstract AbstractOperation GetOperation(long operationId);
+
+    protected abstract IEnumerable<AbstractOperation> GetAllOperations();
+
+    public override async ValueTask ExecuteAsync()
+    {
+        var id = RequestHandler.GetLongQueryString("id", required: false);
+
+        using (ContextPool.AllocateOperationContext(out JsonOperationContext context))
+        {
+            IEnumerable<AbstractOperation> operations;
+            if (id.HasValue == false)
+                operations = GetAllOperations();
+            else
+            {
+                var operation = GetOperation(id.Value);
+                if (operation == null)
+                {
+                    HttpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    return;
+                }
+
+                operations = new List<AbstractOperation> { operation };
+            }
+
+            await using (var writer = new AsyncBlittableJsonTextWriter(context, RequestHandler.ResponseBodyStream()))
+            {
+                writer.WriteStartObject();
+                writer.WriteArray(context, "Results", operations, (w, c, operation) => c.Write(w, operation.ToJson()));
+                writer.WriteEndObject();
+            }
+        }
+    }
+}
---
+++ src/Raven.Server/Web/Operations/Processors/OperationsHandlerProcessorForGetAll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using Raven.Server.Documents;
+using Raven.Server.Documents.Operations;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Web.Operations.Processors;
+
+internal class OperationsHandlerProcessorForGetAll : AbstractOperationsHandlerProcessorForGetAll<DatabaseRequestHandler, DocumentsOperationContext>
+{
+    public OperationsHandlerProcessorForGetAll([NotNull] DatabaseRequestHandler requestHandler) 
+        : base(requestHandler)
+    {
+    }
+
+    protected override AbstractOperation GetOperation(long operationId) => RequestHandler.Database.Operations.GetOperation(operationId);
+
+    protected override IEnumerable<AbstractOperation> GetAllOperations() => RequestHandler.Database.Operations.GetAll();
+}
