5480aeeb55ac926b1055b4da847661d5119199f8
Pawel Pekrol <ppekrol@ravendb.net>
2022-05-17 20:46:12 +0200

    RavenDB-17771 implemented /databases/*/changes/debug endpoint

---
+++ src/Raven.Server/Documents/Handlers/Processors/Changes/ChangesHandlerProcessorForGetConnectionsDebugInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Concurrent;
+using JetBrains.Annotations;
+using Raven.Server.Documents.Changes;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Handlers.Processors.Changes;
+
+internal class ChangesHandlerProcessorForGetConnectionsDebugInfo : AbstractChangesHandlerProcessorForGetConnectionsDebugInfo<DatabaseRequestHandler, DocumentsOperationContext, ChangesClientConnection>
+{
+    public ChangesHandlerProcessorForGetConnectionsDebugInfo([NotNull] DatabaseRequestHandler requestHandler) : base(requestHandler)
+    {
+    }
+
+    protected override ConcurrentDictionary<long, ChangesClientConnection> GetConnections() => RequestHandler.Database.Changes.Connections;
+}
---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Changes/ShardedChangesHandlerProcessorForGetConnectionsDebugInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Concurrent;
+using JetBrains.Annotations;
+using Raven.Server.Documents.Handlers.Processors.Changes;
+using Raven.Server.Documents.Sharding.Changes;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors.Changes;
+
+internal class ShardedChangesHandlerProcessorForGetConnectionsDebugInfo : AbstractChangesHandlerProcessorForGetConnectionsDebugInfo<ShardedDatabaseRequestHandler, TransactionOperationContext, ShardedChangesClientConnection>
+{
+    public ShardedChangesHandlerProcessorForGetConnectionsDebugInfo([NotNull] ShardedDatabaseRequestHandler requestHandler) : base(requestHandler)
+    {
+    }
+
+    protected override ConcurrentDictionary<long, ShardedChangesClientConnection> GetConnections() => RequestHandler.DatabaseContext.Changes.Connections;
+}
