7b4cc0a7a78d53d51af03a1ca74c8085108c2f4e
Igal Merhavia <igal@ravendb.net>
2022-05-22 16:03:10 +0300

    RavenDB-17473 Change stat output parse to C#

--- src/Sparrow.Server/Platform/Posix/sysinfo_t.cs
+++ src/Sparrow.Server/Platform/Posix/sysinfo_t.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Sparrow.Server.Platform.Posix
 {
@@ -39,9 +40,55 @@ namespace Sparrow.Server.Platform.Posix
         public uint freehigh;          /* Available high memory size */
         public uint mem_unit;           /* Memory unit size in bytes */
         public fixed char _f[20 - 2 * 4 - 4]; /* Padding: libc5 uses this.. */
-        
+
         public ulong AvailableRam => freeram;
         public ulong TotalRam => totalram;
         public ulong TotalSwap => totalswap;
+    }
+
+    // https://github.com/whotwagner/statx-fun/blob/master/statx.h
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct statx_timestamp
+    {
+        public Int64 tv_sec;
+        public UInt32 tv_nsec;
+        Int32 __reserved;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct Statx
+    {
+        public UInt32 stx_mask;             // Mask of bits indicating filled fields
+        public UInt32 stx_blksize;          // Block size for filesystem I/O
+        public UInt64 stx_attributes;       // Extra file attribute indicators
+        public UInt32 stx_nlink;            // Number of hard links
+        public UInt32 stx_uid;              // User ID of owner
+        public UInt32 stx_gid;              // Group ID of owner
+        public UInt16 stx_mode;             // File type and mode
+        private unsafe fixed UInt16 __statx_pad1[1];
+        public UInt64 stx_ino;              // Inode number
+        public UInt64 stx_size;             // Total size in bytes
+        public UInt64 stx_blocks;           // Number of 512B blocks allocated
+
+        public UInt64 stx_attributes_mask;
+        /* Mask to show what's supported
+           in stx_attributes */
+
+        /* The following fields are file timestamps */
+        public statx_timestamp stx_atime;   // Last access
+        public statx_timestamp stx_btime;   // Creation
+        public statx_timestamp stx_ctime;   // Last status change
+        public statx_timestamp stx_mtime;   // Last modification
+
+        /* If this file represents a device, then the next two
+           fields contain the ID of the device */
+        public UInt32 stx_rdev_major;       // Major ID
+        public UInt32 stx_rdev_minor;       // Minor ID
+
+        /* The next two fields contain the ID of the device
+           containing the filesystem where the file resides */
+        public UInt32 stx_dev_major;        // Major ID
+        public UInt32 stx_dev_minor;        // Minor ID
+        private unsafe fixed UInt64 __spare2[14];
     }
 }
