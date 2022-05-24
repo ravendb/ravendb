fdf7c0264cf16013c02736bd6ceb4be13ef3b019
Stav <stav@ravendb.net>
2022-05-24 12:07:14 +0300

    RavenDB-18234 - POST /databases/*/admin/etl/raven/test

---
+++ src/Raven.Server/Documents/Sharding/Handlers/ShardedRavenEtlHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Raven.Server.Documents.Sharding.Handlers.Processors.ETL;
+using Raven.Server.Routing;
+
+namespace Raven.Server.Documents.Sharding.Handlers
+{
+    public class ShardedRavenEtlHandler : ShardedDatabaseRequestHandler
+    {
+        [RavenShardedAction("/databases/*/admin/etl/raven/test", "POST")]
+        public async Task PostScriptTest()
+        {
+            using (var processor = new ShardedEtlHandlerProcessorForTestEtl(this))
+                await processor.ExecuteAsync();
+        }
+    }
+}
