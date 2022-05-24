0e43d6aae3d84b9631e68b6c60b64cd3d4de1f03
Stav <stav@ravendb.net>
2022-05-24 16:37:14 +0300

    RavenDB-18236 - POST /databases/*/admin/etl/sql/test

---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/ETL/ShardedSqlEtlHandlerProcessorForTestSqlEtl.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Client.Http;
+using Raven.Client.Json;
+using Raven.Server.Documents.ETL.Providers.Raven.Test;
+using Raven.Server.Documents.Handlers.Processors.ETL;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Web.Http;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors.ETL
+{
+    internal class ShardedSqlEtlHandlerProcessorForTestSqlEtl : AbstractSqlEtlHandlerProcessorForTestSqlEtl<ShardedDatabaseRequestHandler, TransactionOperationContext>
+    {
+        public ShardedSqlEtlHandlerProcessorForTestSqlEtl([NotNull] ShardedDatabaseRequestHandler requestHandler) : base(requestHandler)
+        {
+        }
+
+        protected override async ValueTask GetAndWriteSqlEtlScriptTestResultAsync(TransactionOperationContext context, BlittableJsonReaderObject sqlScript)
+        {
+            using (var token = RequestHandler.CreateOperationToken())
+            {
+                if (sqlScript.TryGet(nameof(TestRavenEtlScript.DocumentId), out string docId) == false)
+                {
+                    HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    return;
+                }
+
+                var shardNumber = RequestHandler.DatabaseContext.GetShardNumber(context, docId);
+                var cmd = new ProxyCommand(new GetSqlEtlTestScriptResultCommand(sqlScript), RequestHandler.HttpContext.Response);
+                await RequestHandler.ShardExecutor.ExecuteSingleShardAsync(cmd, shardNumber, token.Token);
+            }
+        }
+
+        internal class GetSqlEtlTestScriptResultCommand : RavenCommand
+        {
+            private readonly BlittableJsonReaderObject _testScript;
+            public override bool IsReadRequest => true;
+
+            public GetSqlEtlTestScriptResultCommand(BlittableJsonReaderObject testScript)
+            {
+                _testScript = testScript;
+            }
+
+            public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+            {
+                url = $"{node.Url}/databases/{node.Database}/admin/etl/sql/test";
+
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    Content = new BlittableJsonContent(async stream =>
+                    {
+                        await using (var writer = new AsyncBlittableJsonTextWriter(ctx, stream))
+                        {
+                            writer.WriteObject(_testScript);
+                        }
+                    })
+                };
+
+                return request;
+            }
+        }
+    }
+}
---
+++ src/Raven.Server/Documents/Sharding/Handlers/ShardedSqlEtlHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Raven.Server.Documents.Sharding.Handlers.Processors.ETL;
+using Raven.Server.Routing;
+
+namespace Raven.Server.Documents.Sharding.Handlers
+{
+    public class ShardedSqlEtlHandler : ShardedDatabaseRequestHandler
+    {
+        [RavenShardedAction("/databases/*/admin/etl/sql/test", "POST")]
+        public async Task PostScriptTest()
+        {
+            using (var processor = new ShardedSqlEtlHandlerProcessorForTestSqlEtl(this))
+                await processor.ExecuteAsync();
+        }
+    }
+}
