151cf1dfe0f9e408939647614e95ca5b1dd53559
Stav <stav@ravendb.net>
2022-05-25 14:21:05 +0300

    RavenDB-18644 - POST /databases/*/smuggler/import

---
+++ src/Raven.Server/Documents/Handlers/Processors/Smuggler/AbstractSmugglerHandlerProcessorForImport.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Handlers.Processors.Smuggler
+{
+    internal abstract class AbstractSmugglerHandlerProcessorForImport<TRequestHandler, TOperationContext> : AbstractDatabaseHandlerProcessor<TRequestHandler, TOperationContext>
+        where TRequestHandler : AbstractDatabaseRequestHandler<TOperationContext>
+        where TOperationContext : JsonOperationContext
+    {
+        protected AbstractSmugglerHandlerProcessorForImport([NotNull] TRequestHandler requestHandler) : base(requestHandler)
+        {
+        }
+
+        protected abstract ValueTask ImportAsync(JsonOperationContext context, long? operationId);
+
+        public override async ValueTask ExecuteAsync()
+        {
+            using (ContextPool.AllocateOperationContext(out JsonOperationContext context))
+            {
+                var operationId = RequestHandler.GetLongQueryString("operationId", false);
+                await ImportAsync(context, operationId);
+            }
+        }
+    }
+}
---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Smuggler/ShardedSmugglerHandlerProcessorForImport.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Server.Documents.Handlers.Processors.Smuggler;
+using Raven.Server.ServerWide.Context;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors.Smuggler
+{
+    internal class ShardedSmugglerHandlerProcessorForImport : AbstractSmugglerHandlerProcessorForImport<ShardedSmugglerHandler, TransactionOperationContext>
+    {
+        public ShardedSmugglerHandlerProcessorForImport([NotNull] ShardedSmugglerHandler requestHandler) : base(requestHandler)
+        {
+        }
+
+        protected override async ValueTask ImportAsync(JsonOperationContext context, long? operationId)
+        {
+            if(operationId.HasValue == false)
+                operationId = RequestHandler.DatabaseContext.Operations.GetNextOperationId();
+
+            await RequestHandler.Import(context, RequestHandler.DatabaseContext.DatabaseName, RequestHandler.DoImportInternalAsync, RequestHandler.DatabaseContext.Operations, operationId.Value);
+        }
+    }
+}
