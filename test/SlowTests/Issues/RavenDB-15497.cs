6294a3d96896494ec4d4ae2d4093d7e5ee827704
Grisha Kotler <grisha@ayende.com>
2022-05-29 17:35:36 +0300

    RavenDB-18688 - fix failing test

--- test/SlowTests/Issues/RavenDB-15497.cs
+++ test/SlowTests/Issues/RavenDB-15497.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using FastTests;
 using Raven.Client.Documents.Indexes;
-using Raven.Client.Documents.Operations.Indexes;
 using Raven.Client.Exceptions;
 using Raven.Tests.Core.Utils.Entities;
 using Xunit;
@@ -17,35 +18,39 @@ namespace SlowTests.Issues
         }
 
         [Fact]
-        public void WaitForIndexesAfterSaveChangesCanExitWhenThrowOnTimeoutIsFalse()
+        public async Task WaitForIndexesAfterSaveChangesCanExitWhenThrowOnTimeoutIsFalse()
         {
             using (var store = GetDocumentStore())
             {
                 var index = new Index();
-                index.Execute(store);
-                store.Maintenance.Send(new DisableIndexOperation(index.IndexName));
-
-                var indexStats = store.Maintenance.Send(new GetIndexStatisticsOperation(index.IndexName));
-                Assert.Equal(IndexState.Disabled, indexStats.State);
-                Assert.Equal(IndexRunningStatus.Disabled, indexStats.Status);
+                await index.ExecuteAsync(store);
 
                 using (var session = store.OpenSession())
                 {
                     session.Store(new User
                     {
-                        Name = "user1"
+                        Name = "user1",
+                        Count = 3
                     });
                     session.Advanced.WaitForIndexesAfterSaveChanges(timeout: TimeSpan.FromSeconds(3), throwOnTimeout: false);
                     session.SaveChanges();
                 }
 
+                Indexes.WaitForIndexing(store);
+
+                const int timeoutInMs = 2_000;
+                (await GetDatabase(store.Database)).IndexStore.GetIndex(index.IndexName).ForTestingPurposesOnly().BeforeStartingNewBatch = () =>
+                {
+                    Thread.Sleep(timeoutInMs + 100);
+                };
+
                 using (var session = store.OpenSession())
                 {
                     session.Store(new User
                     {
                         Name = "user1"
                     });
-                    session.Advanced.WaitForIndexesAfterSaveChanges(timeout: TimeSpan.FromSeconds(3), throwOnTimeout: true);
+                    session.Advanced.WaitForIndexesAfterSaveChanges(timeout: TimeSpan.FromMilliseconds(timeoutInMs), throwOnTimeout: true);
 
                     var error = Assert.Throws<RavenException>(() => session.SaveChanges());
                     Assert.StartsWith("System.TimeoutException", error.Message);
