5b254285a14782d92a86b2450e8aca47d75dd30e
Stav <stav@ravendb.net>
2022-05-31 09:26:26 +0300

    RavenDB-18645 - get detailed collection stats processor fix

--- src/Raven.Server/Documents/Handlers/Processors/Collections/AbstractCollectionsHandlerProcessorForGetCollectionStats.cs
+++ src/Raven.Server/Documents/Handlers/Processors/Collections/AbstractCollectionsHandlerProcessorForGetCollectionStats.cs
@@ -23,7 +23,7 @@ namespace Raven.Server.Documents.Handlers.Processors.Collections
         {
             using (ContextPool.AllocateOperationContext(out TOperationContext context))
             {
-                DynamicJsonValue result = await GetStatsAsync(context, detailed: false);
+                DynamicJsonValue result = await GetStatsAsync(context, detailed: _detailed);
 
                 await using (var writer = new AsyncBlittableJsonTextWriter(context, RequestHandler.ResponseBodyStream()))
                     context.Write(writer, result);
