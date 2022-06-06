fe890dec37d2a7c52ba1b0bebf5cd78831a07143
Pawel Pekrol <ppekrol@ravendb.net>
2022-06-06 20:39:35 +0200

    RavenDB-18740 added proxy for postpone

---
+++ src/Raven.Server/NotificationCenter/Commands/PostponeNotificationCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using JetBrains.Annotations;
+using Raven.Client.Http;
+using Sparrow.Json;
+
+namespace Raven.Server.NotificationCenter.Commands;
+
+internal class PostponeNotificationCommand : RavenCommand
+{
+    private readonly string _id;
+    private readonly long _timeInSec;
+
+    public PostponeNotificationCommand([NotNull] string id, long timeInSec, string nodeTag = null)
+    {
+        _id = id ?? throw new ArgumentNullException(nameof(id));
+        _timeInSec = timeInSec;
+        SelectedNodeTag = nodeTag;
+    }
+
+    public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+    {
+        url = $"{node.Url}/databases/{node.Database}/notification-center/postpone?id={Uri.EscapeDataString(_id)}&timeInSec={_timeInSec}";
+
+        return new HttpRequestMessage
+        {
+            Method = HttpMethod.Post
+        };
+    }
+}
