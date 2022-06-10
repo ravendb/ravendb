dde457a8a5c703c63caed16a50ab5d69f27fd7e5
Pawel Pekrol <ppekrol@ravendb.net>
2022-06-10 13:20:59 +0200

    RavenDB-18729 implemented /databases/*/admin/etl/sql/test-connection endpoint

--- src/Raven.Server/Documents/ETL/Providers/SQL/Handlers/SqlEtlHandler.cs
+++ src/Raven.Server/Documents/ETL/Providers/SQL/Handlers/SqlEtlHandler.cs
@@ -9,11 +9,8 @@ using System.IO;
 using System.Threading.Tasks;
 using Raven.Server.Documents.ETL.Providers.SQL.Handlers.Processors;
 using Raven.Server.Documents.ETL.Providers.SQL.RelationalWriters;
-using Raven.Server.Documents.ETL.Providers.SQL.Test;
-using Raven.Server.Json;
 using Raven.Server.Routing;
 using Raven.Server.ServerWide.Context;
-using Raven.Server.Utils;
 using Raven.Server.Web.System;
 using Sparrow.Json;
 using Sparrow.Json.Parsing;
@@ -23,42 +20,10 @@ namespace Raven.Server.Documents.ETL.Providers.SQL.Handlers
     public class SqlEtlHandler : DatabaseRequestHandler
     {
         [RavenAction("/databases/*/admin/etl/sql/test-connection", "POST", AuthorizationStatus.DatabaseAdmin)]
-        public async Task GetTestSqlConnection()
+        public async Task TestConnection()
         {
-            try
-            {
-                var factoryName = GetStringQueryString("factoryName");
-                var connectionString = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
-                RelationalDatabaseWriter.TestConnection(factoryName, connectionString);
-
-                DynamicJsonValue result = new DynamicJsonValue
-                {
-                    [nameof(NodeConnectionTestResult.Success)] = true,
-                };
-
-                using (ContextPool.AllocateOperationContext(out JsonOperationContext context))
-                await using (var writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
-                {
-                    context.Write(writer, result);
-                }
-            }
-            catch (Exception ex)
-            {
-                if (Logger.IsInfoEnabled)
-                    Logger.Info("Error occurred during sql replication connection test", ex);
-
-                using (ContextPool.AllocateOperationContext(out JsonOperationContext context))
-                {
-                    await using (var writer = new AsyncBlittableJsonTextWriter(context, ResponseBodyStream()))
-                    {
-                        context.Write(writer, new DynamicJsonValue
-                        {
-                            [nameof(NodeConnectionTestResult.Success)] = false,
-                            [nameof(NodeConnectionTestResult.Error)] = ex.ToString()
-                        });
-                    }
-                }
-            }
+            using (var processor = new SqlEtlHandlerProcessorForTestConnection<DocumentsOperationContext>(this))
+                await processor.ExecuteAsync();
         }
 
         [RavenAction("/databases/*/admin/etl/sql/test", "POST", AuthorizationStatus.DatabaseAdmin)]
--- src/Raven.Server/Documents/Sharding/Handlers/ShardedSqlEtlHandler.cs
+++ src/Raven.Server/Documents/Sharding/Handlers/ShardedSqlEtlHandler.cs
@@ -1,11 +1,20 @@
 ﻿using System.Threading.Tasks;
+using Raven.Server.Documents.ETL.Providers.SQL.Handlers.Processors;
 using Raven.Server.Documents.Sharding.Handlers.Processors.ETL;
 using Raven.Server.Routing;
+using Raven.Server.ServerWide.Context;
 
 namespace Raven.Server.Documents.Sharding.Handlers
 {
     public class ShardedSqlEtlHandler : ShardedDatabaseRequestHandler
     {
+        [RavenShardedAction("/databases/*/admin/etl/sql/test-connection", "POST")]
+        public async Task TestConnection()
+        {
+            using (var processor = new SqlEtlHandlerProcessorForTestConnection<TransactionOperationContext>(this))
+                await processor.ExecuteAsync();
+        }
+
         [RavenShardedAction("/databases/*/admin/etl/sql/test", "POST")]
         public async Task PostScriptTest()
         {
