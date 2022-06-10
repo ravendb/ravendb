ada7293af4d41010d966b7c303afc40d023c5548
Pawel Pekrol <ppekrol@ravendb.net>
2022-06-10 14:44:26 +0200

    RavenDB-18729 implemented /databases/*/admin/etl/elasticsearch/test-connection endpoint

---
+++ src/Raven.Server/Documents/ETL/Providers/Elasticsearch/Handlers/Processors/ElasticSearchEtlConnectionHandlerForTestConnection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Nest;
+using Newtonsoft.Json;
+using Raven.Client.Documents.Operations.ETL.ElasticSearch;
+using Raven.Server.Documents.Handlers.Processors;
+using Raven.Server.Web.System;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+using Authentication = Raven.Client.Documents.Operations.ETL.ElasticSearch.Authentication;
+
+namespace Raven.Server.Documents.ETL.Providers.ElasticSearch.Handlers.Processors;
+
+internal class ElasticSearchEtlConnectionHandlerForTestConnection<TOperationContext> : AbstractDatabaseHandlerProcessor<TOperationContext>
+    where TOperationContext : JsonOperationContext
+{
+    public ElasticSearchEtlConnectionHandlerForTestConnection([NotNull] AbstractDatabaseRequestHandler<TOperationContext> requestHandler)
+        : base(requestHandler)
+    {
+    }
+
+    public override async ValueTask ExecuteAsync()
+    {
+        try
+        {
+            string url = RequestHandler.GetQueryStringValueAndAssertIfSingleAndNotEmpty("url");
+            string authenticationJson = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            Authentication authentication = JsonConvert.DeserializeObject<Authentication>(authenticationJson);
+
+            ElasticClient client = ElasticSearchHelper.CreateClient(new ElasticSearchConnectionString { Nodes = new[] { url }, Authentication = authentication });
+
+            PingResponse pingResult = await client.PingAsync();
+
+            if (pingResult.IsValid)
+            {
+                DynamicJsonValue result = new() { [nameof(NodeConnectionTestResult.Success)] = true, [nameof(NodeConnectionTestResult.TcpServerUrl)] = url, };
+
+                using (ServerStore.ContextPool.AllocateOperationContext(out JsonOperationContext context))
+                await using (AsyncBlittableJsonTextWriter writer = new AsyncBlittableJsonTextWriter(context, RequestHandler.ResponseBodyStream()))
+                {
+                    context.Write(writer, result);
+                }
+            }
+            else
+            {
+                using (ServerStore.ContextPool.AllocateOperationContext(out JsonOperationContext context))
+                {
+                    await using (var writer = new AsyncBlittableJsonTextWriter(context, RequestHandler.ResponseBodyStream()))
+                    {
+                        context.Write(writer, new DynamicJsonValue
+                        {
+                            [nameof(NodeConnectionTestResult.Success)] = false,
+                            [nameof(NodeConnectionTestResult.Error)] = pingResult.DebugInformation
+                        });
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            using (ServerStore.ContextPool.AllocateOperationContext(out JsonOperationContext context))
+            {
+                await using (var writer = new AsyncBlittableJsonTextWriter(context, RequestHandler.ResponseBodyStream()))
+                {
+                    context.Write(writer, new DynamicJsonValue
+                    {
+                        [nameof(NodeConnectionTestResult.Success)] = false,
+                        [nameof(NodeConnectionTestResult.Error)] = ex.ToString()
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ src/Raven.Server/Documents/Sharding/Handlers/ShardedElasticSearchEtlConnectionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Raven.Server.Documents.ETL.Providers.ElasticSearch.Handlers.Processors;
+using Raven.Server.Routing;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Sharding.Handlers;
+
+public class ShardedElasticSearchEtlConnectionHandler : ShardedDatabaseRequestHandler
+{
+    [RavenShardedAction("/databases/*/admin/etl/elasticsearch/test-connection", "POST")]
+    public async Task TestConnection()
+    {
+        using (var processor = new ElasticSearchEtlConnectionHandlerForTestConnection<TransactionOperationContext>(this))
+            await processor.ExecuteAsync();
+    }
+}
