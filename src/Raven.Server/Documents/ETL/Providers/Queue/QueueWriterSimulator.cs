37db58f99105c70e0b6f7a489dacbfd86f401844
Djordje Djukic <djukicdjole@gmail.com>
2022-06-13 09:03:13 +0200

    RavenDB-17670 rework test script

--- src/Raven.Server/Documents/ETL/Providers/Queue/QueueWriterSimulator.cs
+++ src/Raven.Server/Documents/ETL/Providers/Queue/QueueWriterSimulator.cs
@@ -1,32 +1,30 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Raven.Server.Documents.ETL.Providers.Queue.Test;
 using Raven.Server.ServerWide.Context;
 
 namespace Raven.Server.Documents.ETL.Providers.Queue;
 
 public class QueueWriterSimulator
 {
-    public IEnumerable<string> SimulateExecuteCommandText(QueueWithMessages records, DocumentsOperationContext context)
+    public List<MessageSummary> SimulateExecuteMessages(QueueWithMessages queueMessages, DocumentsOperationContext context)
     {
-        List<string> result = new List<string>();
+        List<MessageSummary> result = new();
+        if (queueMessages.Messages.Count <= 0) return result;
 
-        result.AddRange(GenerateInsertKafkaMessageCommandText(records.Name, records, context));
-
-        return result;
-    }
-
-    private IEnumerable<string> GenerateInsertKafkaMessageCommandText(string topicName, QueueWithMessages topic,
-        DocumentsOperationContext context)
-    {
-        List<string> result = new();
-
-        if (topic.Messages.Count <= 0) return result;
-        StringBuilder sb = new();
-
-        foreach (QueueItem item in topic.Messages) sb.AppendLine(item.TransformationResult.ToString());
-
-        result.Add(sb.ToString());
-
+        
+        foreach (var message in queueMessages.Messages)
+        {
+            var messageSummary = new MessageSummary()
+            {
+                Body = message.TransformationResult.ToString(),
+                Options = message.Options
+            };
+            
+            result.Add(messageSummary);
+        }
+        
         return result;
     }
 }
