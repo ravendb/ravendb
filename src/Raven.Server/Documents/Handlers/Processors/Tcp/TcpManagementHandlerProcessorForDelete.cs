a1835310e46c24650c96a5426fe038f34b4d8b67
Pawel Pekrol <ppekrol@ravendb.net>
2022-06-13 15:56:44 +0200

    RavenDB-18772 implemented DELETE /databases/*/tcp endpoint

---
+++ src/Raven.Server/Documents/Handlers/Processors/Tcp/TcpManagementHandlerProcessorForDelete.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using Raven.Server.Documents.TcpHandlers;
+using Raven.Server.ServerWide.Context;
+using Sparrow.Collections;
+
+namespace Raven.Server.Documents.Handlers.Processors.Tcp;
+
+internal class TcpManagementHandlerProcessorForDelete : AbstractTcpManagementHandlerProcessorForDelete<DatabaseRequestHandler, DocumentsOperationContext>
+{
+    public TcpManagementHandlerProcessorForDelete([NotNull] DatabaseRequestHandler requestHandler) : base(requestHandler)
+    {
+    }
+
+    protected override ConcurrentSet<TcpConnectionOptions> GetRunningTcpConnections() => RequestHandler.Database.RunningTcpConnections;
+}
