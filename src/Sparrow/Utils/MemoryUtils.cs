187d659b619d675f498e9dc30317cd72840aae48
Grisha Kotler <grisha@ayende.com>
2022-06-15 17:21:52 +0300

    RavenDB-18676 - add top 5 threads to the memory info

---
+++ src/Sparrow/Utils/MemoryUtils.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Sparrow.LowMemory;
+
+namespace Sparrow.Utils;
+
+public static class MemoryUtils
+{
+    public static string GetExtendedMemoryInfo(MemoryInfoResult memoryInfo)
+    {
+        var message =
+            $"Commit charge: {memoryInfo.CurrentCommitCharge} / {memoryInfo.TotalCommittableMemory}, " +
+            $"Memory: {memoryInfo.TotalPhysicalMemory - memoryInfo.AvailableMemory} / {memoryInfo.TotalPhysicalMemory}, " +
+            $"Dirty memory: {memoryInfo.TotalScratchDirtyMemory}, " +
+            $"Managed memory: {new Size(AbstractLowMemoryMonitor.GetManagedMemoryInBytes(), SizeUnit.Bytes)}, " +
+            $"Unmanaged allocations: {new Size(AbstractLowMemoryMonitor.GetUnmanagedAllocationsInBytes(), SizeUnit.Bytes)}," +
+            "Top 5 unmanaged allocations: ";
+
+        var top5 = string.Empty;
+        var first = true;
+        foreach (var stats in NativeMemory.AllThreadStats
+                     .Where(x => x.IsThreadAlive())
+                     .GroupBy(x => x.Name)
+                     .Select(x => new
+                     {
+                         Name = x.Key,
+                         Allocated = x.Sum(y => y.TotalAllocated),
+                         Count = x.Count()
+                     })
+                     .OrderByDescending(x => x.Allocated)
+                     .Take(5))
+        {
+            if (first == false)
+                top5 += ", ";
+
+            first = false;
+
+            top5 += $"name: {stats.Name}, allocations: {new Size(stats.Allocated, SizeUnit.Bytes)}";
+
+            if (stats.Count > 1)
+            {
+                top5 += $" (threads count: {stats.Count})";
+            }
+        }
+
+        return message + top5;
+    }
+}
