8d0574cb1da116e0775d85aa4c6ec49284134479
Stav <stav@ravendb.net>
2022-06-16 16:50:09 +0300

    RavenDB-18727 - GET /databases/*/debug/documents/get-revisions

---
+++ src/Raven.Server/Documents/Commands/Revisions/GetRevisionsDebugCommand.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Http;
+using System.Text;
+using Raven.Client.Http;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Commands.Revisions
+{
+    public class GetRevisionsDebugCommand : RavenCommand<BlittableJsonReaderObject>
+    {
+        private readonly long? _etag;
+        private readonly int? _pageSize;
+
+        public GetRevisionsDebugCommand(string nodeTag, long? etag, int? pageSize)
+        {
+            SelectedNodeTag = nodeTag;
+            _etag = etag;
+            _pageSize = pageSize;
+        }
+
+        public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get
+            };
+
+            var pathBuilder = new StringBuilder(node.Url)
+                .Append("/databases/")
+                .Append(node.Database)
+                .Append("/debug/documents/get-revisions");
+
+            if (_etag.HasValue && _pageSize.HasValue)
+            {
+                pathBuilder.Append("?etag=").Append(_etag.Value);
+                pathBuilder.Append("&pageSize=").Append(_pageSize.Value);
+            }
+            else if (_pageSize.HasValue)
+                pathBuilder.Append("?pageSize=").Append(_pageSize.Value);
+            else if (_etag.HasValue)
+                pathBuilder.Append("?etag=").Append(_etag.Value);
+
+            url = pathBuilder.ToString();
+            return request;
+        }
+
+        public override void SetResponse(JsonOperationContext context, BlittableJsonReaderObject response, bool fromCache)
+        {
+            if (response == null)
+            {
+                Result = null;
+                return;
+            }
+
+            Result = response;
+        }
+
+        public override bool IsReadRequest => true;
+    }
+}
---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/Revisions/ShardedRevisionsHandlerProcessorForGetRevisionsDebug.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Client.Http;
+using Raven.Server.Documents.Commands.Revisions;
+using Raven.Server.Documents.Handlers.Processors.Revisions;
+using Raven.Server.ServerWide;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Web.Http;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors.Revisions
+{
+    internal class ShardedRevisionsHandlerProcessorForGetRevisionsDebug : AbstractRevisionsHandlerProcessorForGetRevisionsDebug<ShardedDatabaseRequestHandler, TransactionOperationContext>
+    {
+        public ShardedRevisionsHandlerProcessorForGetRevisionsDebug([NotNull] ShardedDatabaseRequestHandler requestHandler) : base(requestHandler)
+        {
+        }
+
+        protected override bool SupportsCurrentNode => false;
+        protected override ValueTask HandleCurrentNodeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task HandleRemoteNodeAsync(ProxyCommand<BlittableJsonReaderObject> command, OperationCancelToken token)
+        {
+            var shardNumber = GetShardNumber();
+            return RequestHandler.ShardExecutor.ExecuteSingleShardAsync(command, shardNumber, token.Token);
+        }
+
+        protected override RavenCommand<BlittableJsonReaderObject> CreateCommandForNode(string nodeTag)
+        {
+            var (etag, pageSize) = GetParameters();
+            return new GetRevisionsDebugCommand(nodeTag, etag, pageSize);
+        }
+    }
+}
