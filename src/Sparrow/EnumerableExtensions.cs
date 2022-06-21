04a44ff62aa50864d1f8526a871a07cf235ba42d
Arkadiusz Palinski <arek@ravendb.net>
2022-06-21 11:19:09 +0200

    RavenDB-7070 Fixing 'warning CS1570: XML comment has badly formed XML' warning

--- src/Sparrow/EnumerableExtensions.cs
+++ src/Sparrow/EnumerableExtensions.cs
@@ -10,7 +10,7 @@ namespace System
     public static class EnumerableExtensions
     {
         /// <summary>
-        /// This extensions is based on the C# 9.0 capability to force any IEnumerator<T> into a foreach
+        /// This extensions is based on the C# 9.0 capability to force any <![CDATA[ IEnumerator<T> ]]> into a foreach
         /// The trick is to use an extension method that will always match.
         /// https://github.com/dotnet/csharplang/blob/6e748b19f1076cc7109293f1038e6042e4ac7f06/meetings/2020/LDM-2020-03-23.md#allow-getenumerator-from-extension-methods
         /// </summary>
