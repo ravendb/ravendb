7f537863bce01a4d77fecea37d20ed6ef39146bc
Pawel Pekrol <ppekrol@ravendb.net>
2022-06-26 17:24:45 +0300

    RavenDB-18281 implemented WS /databases/*/debug/io-metrics/live

---
+++ src/Raven.Server/Documents/Handlers/Processors/IoMetrics/AbstractIoMetricsHandlerProcessorForLive.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Handlers.Processors.IoMetrics;
+
+internal abstract class AbstractIoMetricsHandlerProcessorForLive<TRequestHandler, TOperationContext> : AbstractHandlerWebSocketProxyProcessor<TRequestHandler, TOperationContext>
+    where TOperationContext : JsonOperationContext
+    where TRequestHandler : AbstractDatabaseRequestHandler<TOperationContext>
+{
+    protected AbstractIoMetricsHandlerProcessorForLive([NotNull] TRequestHandler requestHandler) : base(requestHandler)
+    {
+    }
+
+    protected override string GetRemoteEndpointUrl(string databaseName) => $"/databases/{databaseName}/debug/io-metrics/live";
+}
---
+++ src/Raven.Server/Documents/Handlers/Processors/IoMetrics/IoMetricsHandlerProcessorForLive.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Server.ServerWide;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Utils.IoMetrics;
+
+namespace Raven.Server.Documents.Handlers.Processors.IoMetrics;
+
+internal class IoMetricsHandlerProcessorForLive : AbstractIoMetricsHandlerProcessorForLive<DatabaseRequestHandler, DocumentsOperationContext>
+{
+    public IoMetricsHandlerProcessorForLive([NotNull] DatabaseRequestHandler requestHandler) : base(requestHandler)
+    {
+    }
+
+    protected override bool SupportsCurrentNode => true;
+
+    protected override async ValueTask HandleCurrentNodeAsync(WebSocket webSocket, OperationCancelToken token)
+    {
+        var receiveBuffer = new ArraySegment<byte>(new byte[1024]);
+        var receive = webSocket.ReceiveAsync(receiveBuffer, token.Token);
+
+        await using (var ms = new MemoryStream())
+        using (var collector = new DatabaseLiveIoStatsCollector(RequestHandler.Database))
+        {
+            // 1. Send data to webSocket without making UI wait upon opening webSocket
+            await collector.SendDataOrHeartbeatToWebSocket(receive, webSocket, ms, 100);
+
+            // 2. Send data to webSocket when available
+            while (token.Token.IsCancellationRequested == false)
+            {
+                if (await collector.SendDataOrHeartbeatToWebSocket(receive, webSocket, ms, 4000) == false)
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    private class DatabaseLiveIoStatsCollector : LiveIoStatsCollector<DocumentsOperationContext>
+    {
+        public DatabaseLiveIoStatsCollector(DocumentDatabase database) : base(database.IoChanges, database.GetAllStoragesEnvironment().ToList(), database.GetAllPerformanceMetrics(), database.DocumentsStorage.ContextPool, database.DatabaseShutdown)
+        {
+        }
+    }
+}
