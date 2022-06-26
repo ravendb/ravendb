c302c9fabc63625c0d58369cd8786f8a4fb27e3a
Pawel Pekrol <ppekrol@ravendb.net>
2022-06-26 11:17:51 +0300

    RavenDB-18802 fixing test

--- test/SlowTests/Issues/RavenDB_11909.cs
+++ test/SlowTests/Issues/RavenDB_11909.cs
@@ -86,7 +86,7 @@ namespace SlowTests.Issues
         [Fact]
         public void ThrowOnDatabaseRecordChanges()
         {
-            const int numberOfFields = 46;
+            const int numberOfFields = 42;
             const int numberOfProperties = 1;
 
             var tasksList = new List<string>
