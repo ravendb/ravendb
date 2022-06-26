533872dc7bd8ff99a24b19e7cd6a543047f2e9f3
Pawel Pekrol <ppekrol@ravendb.net>
2022-06-26 20:13:06 +0300

    RavenDB-18283 implemented /databases/*/debug/storage/report endpoint

---
+++ src/Raven.Server/Documents/Commands/Storage/GetStorageReportCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using Raven.Client.Http;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Commands.Storage;
+
+internal class GetStorageReportCommand : RavenCommand
+{
+    public GetStorageReportCommand(string nodeTag)
+    {
+        SelectedNodeTag = nodeTag;
+    }
+
+    public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+    {
+        url = $"/databases/{node.Database}/debug/storage/report";
+
+        return new HttpRequestMessage
+        {
+            Method = HttpMethod.Get
+        };
+    }
+}
---
+++ src/Raven.Server/Documents/Handlers/Processors/Debugging/AbstractStorageHandlerProcessorForGetReport.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using Raven.Client.Http;
+using Raven.Server.Documents.Commands.Storage;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Handlers.Processors.Debugging;
+
+internal abstract class AbstractStorageHandlerProcessorForGetReport<TRequestHandler, TOperationContext> : AbstractHandlerProxyReadProcessor<object, TRequestHandler, TOperationContext>
+    where TOperationContext : JsonOperationContext
+    where TRequestHandler : AbstractDatabaseRequestHandler<TOperationContext>
+{
+    protected AbstractStorageHandlerProcessorForGetReport([NotNull] TRequestHandler requestHandler) : base(requestHandler)
+    {
+    }
+
+    protected override RavenCommand<object> CreateCommandForNode(string nodeTag) => new GetStorageReportCommand(nodeTag);
+}
