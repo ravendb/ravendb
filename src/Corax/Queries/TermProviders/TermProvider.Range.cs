a9b16de5fa230dcf514e36d6865ad45fdf0c19c6
Ayende Rahien <ayende@ayende.com>
2022-07-01 10:05:25 +0300

    RavenDB-18852 - Adding support for doing indexed range queries on top of numeric data (int64)

--- src/Corax/Queries/TermProviders/TermProvider.Range.cs
+++ src/Corax/Queries/TermProviders/TermProvider.Range.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Buffers;
+using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -8,6 +10,7 @@ using System.Threading.Tasks;
 using Sparrow.Server;
 using Voron;
 using Voron.Data.CompactTrees;
+using Voron.Data.Fixed;
 
 namespace Corax.Queries
 {
@@ -95,6 +98,115 @@ namespace Corax.Queries
             
             term = _searcher.TermQuery(_tree, termSlice);
             return true;
+        }
+
+        public QueryInspectionNode Inspect()
+        {
+            return new QueryInspectionNode($"{GetType().Name}",
+                            parameters: new Dictionary<string, string>()
+                            {
+                                { "Field", _fieldName.ToString() },
+                                { "Low", _low.ToString()},
+                                { "High", _high.ToString()}
+                            });
+        }
+
+        public string DebugView => Inspect().ToString();
+    }
+    
+     [DebuggerDisplay("{DebugView,nq}")]
+    public unsafe struct TermNumericRangeProvider<TLow, THigh> : ITermProvider
+        where TLow : struct, Range.Marker
+        where THigh  : struct, Range.Marker
+    {
+        private readonly IndexSearcher _searcher;
+        private readonly Slice _fieldName;
+        private readonly long _low, _high;
+        private readonly CompactTree _tree;
+        private readonly FixedSizeTree _set;
+        private readonly FixedSizeTree.IFixedSizeIterator _iterator;
+        private bool _first;
+
+        private const int TermBufferSize = 32;
+        private fixed byte _termsBuffer[TermBufferSize];
+        private ByteStringContext<ByteStringMemoryCache>.ExternalScope _prevTermScope;
+
+        public TermNumericRangeProvider(IndexSearcher searcher, FixedSizeTree set,
+            CompactTree tree, Slice fieldName, long low, long high)
+        {
+            _searcher = searcher;
+            _fieldName = fieldName;
+            _iterator = set.Iterate();
+            _low = low;
+            _high = high;
+            _set = set;
+            _tree = tree;
+            _prevTermScope = default;
+            _first = true;
+        }
+
+        public void Reset()
+        {
+            _first = true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Next(out TermMatch term) => Next(out term, out _);
+
+        public bool Next(out TermMatch term, out Slice termSlice)
+        {
+            bool wasFirst = false;
+            bool hasNext;
+            if (_first)
+            {
+                hasNext = _iterator.Seek(_low);
+                _first = false;
+                wasFirst = true;
+            }
+            else
+            {
+                hasNext = _iterator.MoveNext();
+            }
+
+            if (hasNext == false)
+                goto Empty;
+            
+            long curVal = _iterator.CurrentKey;
+            if (typeof(TLow) == typeof(Range.Exclusive))
+            {
+                if (wasFirst && curVal == _low)
+                {
+                    return Next(out term, out termSlice);
+                }
+            }     
+
+
+            var cmp = _high - curVal;
+            if (typeof(THigh) == typeof(Range.Exclusive) && cmp <= 0 || 
+                typeof(THigh) == typeof(Range.Inclusive) && cmp < 0)
+            {
+                goto Empty;
+            }
+
+            _prevTermScope.Dispose();
+            
+            fixed (byte* p = _termsBuffer)
+            {
+                if (Utf8Formatter.TryFormat(curVal, new Span<byte>(p, TermBufferSize), out int termLen) == false)
+                {
+                    throw new InvalidOperationException("Cannot format long value " + curVal);
+                }
+
+                _prevTermScope = Slice.External(_set.Llt.Allocator, p, termLen, ByteStringType.Immutable, out termSlice);
+
+                term = _searcher.TermQuery(_tree, termSlice);
+                return true;
+            }
+
+            Empty:
+            termSlice = Slices.Empty;
+            term = TermMatch.CreateEmpty();
+            return false;
         }
 
         public QueryInspectionNode Inspect()
