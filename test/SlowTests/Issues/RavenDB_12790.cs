43ac57c05585f558d8cd233b39dfa49ea4fc4cd5
Grisha Kotler <grisha@ayende.com>
2022-08-01 11:40:25 +0300

    RavenDB-18162 - migrate lazy tests from issues

--- test/SlowTests/Issues/RavenDB_12790.cs
+++ test/SlowTests/Issues/RavenDB_12790.cs
@@ -3,6 +3,7 @@ using FastTests;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Indexes;
 using Raven.Client.Exceptions.Documents.Indexes;
+using Tests.Infrastructure;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -14,10 +15,11 @@ namespace SlowTests.Issues
         {
         }
 
-        [Fact]
-        public void LazyQueryAgainstMissingIndex()
+        [RavenTheory(RavenTestCategory.Querying | RavenTestCategory.Sharding)]
+        [RavenData(DatabaseMode = RavenDatabaseMode.All)]
+        public void LazyQueryAgainstMissingIndex(Options options)
         {
-            using (var store = GetDocumentStore())
+            using (var store = GetDocumentStore(options))
             {
                 using (var session = store.OpenSession())
                 {
