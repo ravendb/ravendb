7bebf80220b55edae0835f7457550ab0cf6b9b57
Maciej Aszyk <maciej@ravendb.net>
2022-08-08 13:36:15 +0200

    RavenDB-18852 Rebase | Stack generator fix | Analyzer fix in RangeQueries when Slice are used

--- test/SlowTests/Issues/RavenDB_11794.cs
+++ test/SlowTests/Issues/RavenDB_11794.cs
@@ -16,7 +16,7 @@ namespace SlowTests.Issues
         }
 
         [RavenTheory(RavenTestCategory.Indexes | RavenTestCategory.Querying)]
-        [RavenData(SearchEngineMode = RavenSearchEngineMode.Corax)]
+        [RavenData(SearchEngineMode = RavenSearchEngineMode.All)]
         public void CanUseLastModifiedInAutoIndex(Options options)
         {
             using (var store = GetDocumentStore(options))
