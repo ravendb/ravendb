2f22b18a67e7fa7882e248af7c0df95421c89cc5
Lev Skuditsky <lev.skuditsky@ravendb.net>
2022-08-24 11:47:24 +0300

    RavenDB-12430: Cover by failing test

---
+++ test/FastTests/Issues/RavenDB-12430.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+using Sparrow.Json;
+using Tests.Infrastructure.Entities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FastTests.Issues
+{
+    public class RavenDB_12430 : RavenTestBase
+    {
+        public RavenDB_12430(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void AsExtensionShouldTakeIntoAccountProjection()
+        {
+            using (var store = GetDocumentStore())
+            using (IDocumentSession session = store.OpenSession())
+            {
+                var withoutAsExtension = session.Query<Order>().ProjectInto<Result>();
+                var withAsExtension = session.Query<Order>().ProjectInto<Result>().As<BlittableJsonReaderObject>();
+
+                Assert.True(withAsExtension is IQueryable<BlittableJsonReaderObject>);
+                Assert.Equal(withoutAsExtension.ToString(), "from 'Orders' select Company, Employee");
+                Assert.Equal(withoutAsExtension.ToString(), withAsExtension.ToString());
+            }
+        }
+
+        public class Result
+        {
+            public string Company { get; set; }
+            public string Employee { get; set; }
+        }
+    }
+}
