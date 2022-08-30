a3af3feb219e4ad2db945e64e3b521a69686f3f5
shiranshalom <shiran@ravendb.net>
2022-08-30 10:50:20 +0300

    RavenDB-18879 - Sharding - Replication - Handlers [GET /databases/*/replication/pulses/live (WS)]

---
+++ src/Raven.Server/Documents/Handlers/Processors/Replication/AbstractReplicationHandlerProcessorForGetPulsesLive.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using JetBrains.Annotations;
+using Raven.Client.Http;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Handlers.Processors.Replication
+{
+    internal abstract class AbstractReplicationHandlerProcessorForGetPulsesLive<TRequestHandler, TOperationContext> : AbstractHandlerProxyReadProcessor<object, TRequestHandler, TOperationContext>
+        where TOperationContext : JsonOperationContext
+        where TRequestHandler : AbstractDatabaseRequestHandler<TOperationContext>
+    {
+        protected AbstractReplicationHandlerProcessorForGetPulsesLive([NotNull] TRequestHandler requestHandler)
+            : base(requestHandler)
+        {
+        }
+
+        protected override RavenCommand<object> CreateCommandForNode(string nodeTag) => new GetReplicationPulsesLiveCommand(nodeTag);
+    }
+
+    internal class GetReplicationPulsesLiveCommand : RavenCommand
+    {
+        public GetReplicationPulsesLiveCommand(string nodeTag)
+        {
+            SelectedNodeTag = nodeTag;
+        }
+
+        public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+        {
+            url = $"{node.Url}/databases/{node.Database}/replication/pulses/live";
+
+            return new HttpRequestMessage
+            {
+                Method = HttpMethod.Get
+            };
+        }
+    }
+}
