70184d4080604a01f44eca35db217d1460de404b
Lev Skuditsky <lev.skuditsky@ravendb.net>
2022-08-31 14:33:03 +0300

    RavenDB-19036: - cancel the debug package gracefully and write what we got so far. - write the last endpoint that we stopped at. - allow customizing the timeout for that operation.

---
+++ src/Raven.Client/Documents/Commands/KillServerOperationCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using Raven.Client.Http;
+using Sparrow.Json;
+
+namespace Raven.Client.Documents.Commands;
+
+public class KillServerOperationCommand : RavenCommand
+{
+    private readonly long _id;
+
+    public KillServerOperationCommand(long id)
+    {
+        _id = id;
+    }
+
+    public KillServerOperationCommand(long id, string nodeTag) : this(id)
+    {
+        SelectedNodeTag = nodeTag;
+    }
+
+    public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+    {
+        url = $"{node.Url}/admin/operations/kill?id={_id}";
+
+        return new HttpRequestMessage
+        {
+            Method = HttpMethod.Post
+        };
+    }
+}
