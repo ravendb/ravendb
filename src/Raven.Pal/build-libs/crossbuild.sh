663bb0e1b698cd2d4902289b0fe844fb423428fc
Arkadiusz Palinski <arek@ravendb.net>
2022-08-31 13:15:08 +0200

    RavenDB-19273 Fixing compilation of PAL - posix: fixing macos related stuff - windows: fixing compilation with the usage of VS 2022

--- src/Raven.Pal/build-libs/crossbuild.sh
+++ src/Raven.Pal/build-libs/crossbuild.sh
@@ -4,13 +4,13 @@ export MACOS_SDK_VERSION=12.3
 export MACOS_SDK_DIRNAME="MacOSX${MACOS_SDK_VERSION}.sdk"
 
 function log {
-   echo "$(date): $1" > "$LOG"
+   echo "$(date): $1" >> "$LOG"
 }
 
 function enable_cross_builds {
     log "Installing packages..."
     sudo apt-get update -qq >> "${LOG}" 2>&1
-    if ! sudo apt-get install -y crossbuild-essential-armhf crossbuild-essential-arm64 cmake clang libxml2-dev fuse libbz2-dev libfuse-dev fuse >> "${LOG}" 2>&1; then
+    if ! sudo apt-get install -y crossbuild-essential-armhf crossbuild-essential-arm64 cmake clang libxml2-dev fuse libbz2-dev libfuse-dev libssl-dev fuse >> "${LOG}" 2>&1; then
         echo "Failed. See ${LOG}";  
         exit 1;
     fi
@@ -21,7 +21,7 @@ function enable_cross_builds {
         echo "Failed. See ${LOG}";  
         exit 1;
     fi
-
+    
     log "Copying MacOSX SDK..."
     local MACOS_SDK_URL=https://ravendb-build-assets.s3.amazonaws.com/macos/MacOSX${MACOS_SDK_VERSION}.sdk.tar.xz
     local MACOS_SDK_TAR_PATH="${MACOSX_SDK_TAR_PATH:-build-libs/$MACOS_SDK_DIRNAME.tar.xz}"
@@ -38,8 +38,12 @@ function enable_cross_builds {
     cp -v "$MACOS_SDK_TAR_PATH" osxcross/tarballs/ >> "${LOG}" 2>&1
     pushd osxcross >> "${LOG}" 2>&1 || exit 1
 
-    OSXCROSS_SDKROOT="$(realpath "osxcross/target/SDK/${MACOS_SDK_DIRNAME}")"
+    mkdir -p "target/SDK/${MACOS_SDK_DIRNAME}"
+
+    OSXCROSS_SDKROOT="$(realpath "target/SDK/${MACOS_SDK_DIRNAME}")"
     export OSXCROSS_SDKROOT
+    
+    tar xf  "$(realpath ../$MACOS_SDK_TAR_PATH)sudo" -C "$(realpath "target/SDK")"
 
     log "Building mac compiler..."
     sed -i 's/read/echo/g' build.sh
