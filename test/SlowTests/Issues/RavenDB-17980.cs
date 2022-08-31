a186811fddfdab83d2ac5806526ba7f5b548b0a8
Yonatan Resh <yonatan.resh@ravendb.net>
2022-08-31 15:38:13 +0300

    RavenDB-17980 Cleanup topology files for TestDriver

--- test/SlowTests/Issues/RavenDB-17980.cs
+++ test/SlowTests/Issues/RavenDB-17980.cs
@@ -1,35 +1,84 @@
 ﻿using System.IO;
 using EmbeddedTests.TestDriver;
+using Raven.Client.Documents;
+using Raven.TestDriver;
 using SlowTests.Core.Utils.Entities;
 using Xunit;
 
 namespace SlowTests.Issues
 {
-    public class RavenDB_17980 : TestDriverExampleTest
+    public class RavenDB_17980 : RavenTestDriver
     {
-        public RavenDB_17980()
+        static RavenDB_17980()
         {
+            ConfigureServer(new TestServerOptions
+            {
+                ServerDirectory = TestDriverExampleTest.GetServerPath()
+            });
         }
 
-        [Fact]
-        public void disable_Creating_Topology_Files_For_TestDrivers()
+        protected override void PreInitialize(IDocumentStore documentStore)
         {
-            using (var store = GetDocumentStore())
+            var dir = documentStore.Conventions.TopologyCacheLocation + "TestDriverTopologyFiles_testDirectory\\";
+
+            if (Directory.Exists(dir) == false)
+                Directory.CreateDirectory(dir);
+
+            documentStore.Conventions.TopologyCacheLocation = dir;
+        }
+
+        public class Test_Without_Overriding_PreInitialize : RavenDB_17980
+        {
+            [Fact]
+            public void disable_Creating_Topology_Files_For_TestDrivers()
             {
-                using (var session = store.OpenSession())
+                using (var store = GetDocumentStore())
                 {
-                    session.Store(new User
+                    using (var session = store.OpenSession())
                     {
-                        Name = "Yonatan",
-                    });
-                    session.SaveChanges();
+                        session.Store(new User { Name = "Yonatan", });
+                        session.SaveChanges();
+                    }
+
+                    var numberOfDatabaseTopologyFiles = Directory.GetFiles(store.Conventions.TopologyCacheLocation, "*.raven-database-topology").Length;
+                    Assert.True(numberOfDatabaseTopologyFiles == 0, $"number of files: {numberOfDatabaseTopologyFiles}");
+
+                    var numberOfClusterTopologyFiles = Directory.GetFiles(store.Conventions.TopologyCacheLocation, "*.raven-cluster-topology").Length;
+                    Assert.True(numberOfClusterTopologyFiles == 0, $"number of files: {numberOfClusterTopologyFiles}");
+
+                    Directory.Delete(store.Conventions.TopologyCacheLocation, true);
                 }
+            }
+        }
+        public class Test_With_PreInitialize_DisableTopologyCache_Equals_False : RavenDB_17980
+        {
+            protected override void PreInitialize(IDocumentStore documentStore)
+            {
+                base.PreInitialize(documentStore);
+                documentStore.Conventions.DisableTopologyCache = false;
+            }
 
-                var numberOfDatabaseTopologyFiles = Directory.GetFiles(store.Conventions.TopologyCacheLocation, "*.raven-database-topology").Length;
-                Assert.True(numberOfDatabaseTopologyFiles == 0, $"number of files: {numberOfDatabaseTopologyFiles}");
+            [Fact]
+            public void Creating_Topology_Files_For_TestDrivers()
+            {
+                using (var store = GetDocumentStore())
+                {
+                    Assert.False(store.Conventions.DisableTopologyCache);
 
-                var numberOfClusterTopologyFiles = Directory.GetFiles(store.Conventions.TopologyCacheLocation, "*.raven-database-topology").Length;
-                Assert.True(numberOfClusterTopologyFiles == 0, $"number of files: {numberOfClusterTopologyFiles}");
+                    using (var session = store.OpenSession())
+                    {
+                        session.Store(new User
+                        {
+                            Name = "Yonatan",
+                        });
+                        session.SaveChanges();
+                    }
+
+                    var numberOfDatabaseTopologyFiles = Directory.GetFiles(store.Conventions.TopologyCacheLocation, "*.raven-database-topology").Length;
+                    Assert.False(numberOfDatabaseTopologyFiles == 0, $"number of files: {numberOfDatabaseTopologyFiles}");
+
+                    Directory.Delete(store.Conventions.TopologyCacheLocation, true);
+                }
             }
         }
     }
