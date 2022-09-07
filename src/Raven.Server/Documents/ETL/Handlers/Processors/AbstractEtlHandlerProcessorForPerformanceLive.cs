86be543ca00bd181246f3b15b331498d14577aa8
Pawel Pekrol <ppekrol@ravendb.net>
2022-09-07 12:46:16 +0200

    RavenDB-18729 implemented /databases/*/etl/performance/live

---
+++ src/Raven.Server/Documents/ETL/Handlers/Processors/AbstractEtlHandlerProcessorForPerformanceLive.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Raven.Server.Documents.Handlers.Processors;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.ETL.Handlers.Processors;
+
+internal abstract class AbstractEtlHandlerProcessorForPerformanceLive<TRequestHandler, TOperationContext> : AbstractHandlerWebSocketProxyProcessor<TRequestHandler, TOperationContext>
+    where TOperationContext : JsonOperationContext
+    where TRequestHandler : AbstractDatabaseRequestHandler<TOperationContext>
+{
+    protected AbstractEtlHandlerProcessorForPerformanceLive([NotNull] TRequestHandler requestHandler) : base(requestHandler)
+    {
+    }
+
+    protected override string GetRemoteEndpointUrl(string databaseName) => $"/databases/{databaseName}/etl/performance/live";
+}
---
+++ src/Raven.Server/Documents/ETL/Handlers/Processors/EtlHandlerProcessorForPerformanceLive.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Server.ServerWide;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.ETL.Handlers.Processors;
+
+internal class EtlHandlerProcessorForPerformanceLive : AbstractEtlHandlerProcessorForPerformanceLive<DatabaseRequestHandler, DocumentsOperationContext>
+{
+    public EtlHandlerProcessorForPerformanceLive([NotNull] DatabaseRequestHandler requestHandler) : base(requestHandler)
+    {
+    }
+
+    protected override bool SupportsCurrentNode => true;
+
+    protected override async ValueTask HandleCurrentNodeAsync(WebSocket webSocket, OperationCancelToken token)
+    {
+        var names = RequestHandler.GetStringValuesQueryString("name", required: false);
+        var etls = EtlHandlerProcessorForStats.GetProcessesToReportOn(RequestHandler.Database, names);
+
+        var receiveBuffer = new ArraySegment<byte>(new byte[1024]);
+        var receive = webSocket.ReceiveAsync(receiveBuffer, token.Token);
+
+        await using (var ms = new MemoryStream())
+        using (var collector = new LiveEtlPerformanceCollector(RequestHandler.Database, etls))
+        {
+            // 1. Send data to webSocket without making UI wait upon opening webSocket
+            await collector.SendStatsOrHeartbeatToWebSocket(receive, webSocket, ContextPool, ms, 100);
+
+            // 2. Send data to webSocket when available
+            while (token.Token.IsCancellationRequested == false)
+            {
+                if (await collector.SendStatsOrHeartbeatToWebSocket(receive, webSocket, ContextPool, ms, 4000) == false)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
