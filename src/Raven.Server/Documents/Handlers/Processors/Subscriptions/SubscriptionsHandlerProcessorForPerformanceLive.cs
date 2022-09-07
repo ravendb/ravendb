c5a50deb18982101a8a0ba3ac57fe38f95df2188
Pawel Pekrol <ppekrol@ravendb.net>
2022-09-07 12:56:08 +0200

    RavenDB-18104 implemented /databases/*/subscriptions/performance/live endpoint

---
+++ src/Raven.Server/Documents/Handlers/Processors/Subscriptions/SubscriptionsHandlerProcessorForPerformanceLive.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Server.Documents.Subscriptions;
+using Raven.Server.ServerWide;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Handlers.Processors.Subscriptions;
+
+internal class SubscriptionsHandlerProcessorForPerformanceLive : AbstractSubscriptionsHandlerProcessorForPerformanceLive<DatabaseRequestHandler, DocumentsOperationContext>
+{
+    public SubscriptionsHandlerProcessorForPerformanceLive([NotNull] DatabaseRequestHandler requestHandler) : base(requestHandler)
+    {
+    }
+
+    protected override bool SupportsCurrentNode => true;
+
+    protected override async ValueTask HandleCurrentNodeAsync(WebSocket webSocket, OperationCancelToken token)
+    {
+        var receiveBuffer = new ArraySegment<byte>(new byte[1024]);
+        var receive = webSocket.ReceiveAsync(receiveBuffer, token.Token);
+
+        using (var ms = new MemoryStream())
+        using (var collector = new LiveSubscriptionPerformanceCollector(RequestHandler.Database))
+        {
+            // 1. Send data to webSocket without making UI wait upon opening webSocket
+            await collector.SendStatsOrHeartbeatToWebSocket(receive, webSocket, ContextPool, ms, 100);
+
+            // 2. Send data to webSocket when available
+            while (token.Token.IsCancellationRequested == false)
+            {
+                if (await collector.SendStatsOrHeartbeatToWebSocket(receive, webSocket, ContextPool, ms, 4000) == false)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
