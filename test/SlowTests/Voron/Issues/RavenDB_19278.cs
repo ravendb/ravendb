52609bb942a26d79b67c6b800ffc63d78144b03e
Arkadiusz Palinski <arek@ravendb.net>
2022-09-07 15:52:52 +0200

    RavenDB-19278 Fixing faulty error handling during the recovery on storage environment startup. This is related to the introduction of recyclable journals. The main problem was the usage of TryReadAndValidateHeader() method in different mode of journal reading. The added logic was to continue to read and validate the journal to the end after reaching garbage to verify there is no valid later tx which would mean we have true corruption rather then random garbage (https://github.com/ravendb/ravendb/pull/1415). Unfortunatelly this caused that we could shadow an original error and throw errors from the reading continuation while it is expected to get some garbage then.
    The fix introduces JournalReader.ReadExpectation enum that allows us to control the journal reader behavior. Now we can have different validation logic depending on the mode that we read in. We do regular validation when we expect to read ValidTransaction but when we encounter some garbage we switch to ZerosOrGarbage reading mode so we can ignore some data errors that we expect due to journals reuse feature.
    
    Important note: we have a concept of "partial recovery" which continues opening a storage environment regardless the encountered data errors (InvalidDataException - e.g a tx has invalid hash, we stop processing further journals then and report about loosing data from some point). When reading the journal in ZerosOrGarbage mode we ignore InvalidDataException but we _don't_ ignore InvalidJournalException which is critical error which we cannot just ignore - it might be that we found that a journal is missing or order of transactions is broken. We need to error if that's the case.

---
+++ test/SlowTests/Voron/Issues/RavenDB_19278.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using FastTests.Voron;
+using Sparrow.Utils;
+using Voron;
+using Voron.Global;
+using Voron.Impl.Journal;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Voron.Issues;
+
+public class RavenDB_19278 : StorageTest
+{
+    private List<string> _onRecoveryErrorMessages = new();
+
+    public RavenDB_19278(ITestOutputHelper output) : base(output)
+    {
+    }
+
+
+    protected override void Configure(StorageEnvironmentOptions options)
+    {
+        options.InitialLogFileSize = 4 * Constants.Size.Megabyte;
+        options.MaxLogFileSize = 32 * Constants.Size.Megabyte;
+
+        options.OnRecoveryError += (sender, args) =>
+        {
+            _onRecoveryErrorMessages.Add(args.Message);
+        };
+        options.ManualSyncing = true;
+        options.ManualFlushing = true;
+        options.MaxScratchBufferSize = 1 * Constants.Size.Gigabyte;
+    }
+
+    [Fact]
+    public void StopRecoveryAndRaisePartiallyRecoveredAlertAfterGettingInvalidHashOfTransaction()
+    {
+        RequireFileBasedPager();
+
+        using (var tx = Env.WriteTransaction())
+        {
+            tx.CreateTree("tree");
+
+            tx.Commit();
+        }
+
+        const long corruptedTx = 9;
+
+        var random = new Random(3);
+
+        var itemsToReadAfterRecovery = new List<string>();
+
+        for (var i = 0; i < 10; i++)
+        {
+            var buffer = new byte[2 * Constants.Size.Megabyte];
+            random.NextBytes(buffer);
+
+            using (var tx = Env.WriteTransaction())
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    string key = "a" + i + j;
+                    tx.CreateTree("tree").Add(key, new MemoryStream(buffer));
+
+                    if (tx.LowLevelTransaction.Id < corruptedTx)
+                    {
+                        itemsToReadAfterRecovery.Add(key);
+                    }
+                }
+
+                tx.Commit();
+            }
+        }
+
+        var journalToCorrupt = Env.Journal.GetCurrentJournalInfo().CurrentJournal - 3;
+
+        StopDatabase(); 
+
+        CorruptJournal(journalToCorrupt, 10 * Constants.Size.Kilobyte * 4 - 1000); // it will corrupt tx 9
+
+        StartDatabase(); // it must not throw, it should partially recover the database
+
+        Assert.Equal(2, _onRecoveryErrorMessages.Count);
+
+        Assert.Contains($"Invalid hash signature for transaction: HeaderMarker: Valid, TransactionId: {corruptedTx}", _onRecoveryErrorMessages[0]);
+        Assert.Contains("Database recovered partially. Some data was lost.", _onRecoveryErrorMessages[1]);
+
+        using (var operation = new WriteAheadJournal.JournalApplicator.SyncOperation(Env.Journal.Applicator))
+        {
+            operation.SyncDataFile(); // force the sync so it will delete already synced journals
+        }
+
+        var journalPath = Env.Options.JournalPath.FullPath;
+
+        // older journals should be deleted, newer but not processed should be deleted too due to partial recovery 
+        Assert.True(SpinWait.SpinUntil(() => new DirectoryInfo(journalPath).GetFiles($"*.journal").Length == 1,
+            TimeSpan.FromSeconds(30)));
+
+        using (var tx = Env.ReadTransaction())
+        {
+            foreach (var key in itemsToReadAfterRecovery)
+            {
+                var readA = tx.ReadTree("tree").Read(key);
+
+                Assert.NotNull(readA);
+            }
+        }
+
+        using (var tx = Env.WriteTransaction())
+        {
+            Assert.Equal(corruptedTx, tx.LowLevelTransaction.Id);
+
+            var buffer = new byte[123];
+            random.NextBytes(buffer);
+
+            tx.CreateTree("tree").Add("new", new MemoryStream(buffer));
+            
+            tx.Commit();
+        }
+
+        StopDatabase();
+
+        Configure(Options); // when we stop db we always call _options.NullifyHandlers() so we need to hook them up again
+
+        StartDatabase(); // TODO arek - currently fails with InvalidJournalException: No such journal '0000000000000000008.journal' - that is true, we removed it in the process of (partial) recovery
+    }
+
+    private void CorruptJournal(long journal, long position, int numberOfCorruptedBytes = Constants.Size.Kilobyte * 4, byte value = 42, bool preserveValue = false)
+    {
+        Options.Dispose();
+        Options = StorageEnvironmentOptions.ForPath(DataDir);
+        Configure(Options);
+        using (var fileStream = SafeFileStream.Create(Options.GetJournalPath(journal).FullPath,
+                   FileMode.Open,
+                   FileAccess.ReadWrite,
+                   FileShare.ReadWrite | FileShare.Delete))
+        {
+            fileStream.Position = position;
+
+            var buffer = new byte[numberOfCorruptedBytes];
+
+            var remaining = buffer.Length;
+            var start = 0;
+            while (remaining > 0)
+            {
+                var read = fileStream.Read(buffer, start, remaining);
+                if (read == 0)
+                    break;
+                start += read;
+                remaining -= read;
+            }
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                if (buffer[i] != value || preserveValue)
+                    buffer[i] = value;
+                else
+                    buffer[i] = (byte)(value + 1); // we really want to change the original value here so it must not stay the same
+            }
+            fileStream.Position = position;
+            fileStream.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
