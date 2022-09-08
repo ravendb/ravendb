f7ac5d0f683f2bdcb4c8f5e211900a95e4670c23
Marcin Lewandowski <marcin@ravendb.net>
2022-09-08 11:50:30 +0200

    RavenDB-19270 Fix storage key prefix

--- src/Raven.Studio/typescript/common/storage/storageKeyProvider.ts
+++ src/Raven.Studio/typescript/common/storage/storageKeyProvider.ts
@@ -1,13 +1,31 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
+const versionFile = require("wwwroot/version.txt?raw");
+
 class storageKeyProvider {
 
-    static commonPrefix = "ravedb-5.3-";
+    static commonPrefix: string;
 
     static storageKeyFor(value: string) {
+        storageKeyProvider.initCommonPrefix();
         return storageKeyProvider.commonPrefix + value;
     }
+    
+    private static initCommonPrefix(): void {
+        if (storageKeyProvider.commonPrefix) {
+            return;
+        }
+        
+        try {
+            const versionJson = JSON.parse(versionFile);
+            const version = versionJson.Version.substring(0, 3);
 
+            storageKeyProvider.commonPrefix = "ravendb-" + version + "-";
+        } catch (e) {
+            console.error("Unable to read version from version.txt file. Please make sure file doesn't contain BOM.");
+            throw e;
+        }
+    }
 }
 
 export = storageKeyProvider;
