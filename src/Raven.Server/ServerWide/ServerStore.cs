b37c6687c3fd943c2a06d67e27f4085a5bf40685
Arkadiusz Palinski <arek@ravendb.net>
2022-09-09 14:08:25 +0200

    RavenDB-19278 Added logging of recovery or data integrity errors to a file so we'll have full history of errors in the logs. When an alert is raised then let's try to make its key more unique so it won't overwrite existing alerts (but do not make it completely unique to prevent from getting a flood of alerts)

--- src/Raven.Server/ServerWide/ServerStore.cs
+++ src/Raven.Server/ServerWide/ServerStore.cs
@@ -618,9 +618,14 @@ namespace Raven.Server.ServerWide
 
             options.OnNonDurableFileSystemError += (obj, e) =>
             {
+                var title = "Non Durable File System - System Storage";
+
+                if (Logger.IsOperationsEnabled)
+                    Logger.Operations($"{title}. {e.Message}", e.Exception);
+
                 var alert = AlertRaised.Create(
                     null,
-                    "Non Durable File System - System Storage",
+                    title,
                     e.Message,
                     AlertType.NonDurableFileSystem,
                     NotificationSeverity.Warning,
@@ -638,13 +643,18 @@ namespace Raven.Server.ServerWide
 
             options.OnRecoveryError += (obj, e) =>
             {
+                string title = "Recovery Error - System Storage";
+
+                if (Logger.IsOperationsEnabled)
+                    Logger.Operations($"{title}. {e.Message}", e.Exception);
+
                 var alert = AlertRaised.Create(
                     null,
-                    "Recovery Error - System Storage",
+                    title,
                     e.Message,
                     AlertType.RecoveryError,
                     NotificationSeverity.Error,
-                    "Recovery Error System");
+                    key: $"Recovery Error System/{SystemTime.UtcNow.Ticks % 5}"); // if this was called multiple times let's try to not overwrite previous alerts
 
                 if (NotificationCenter.IsInitialized)
                 {
@@ -658,13 +668,18 @@ namespace Raven.Server.ServerWide
 
             options.OnIntegrityErrorOfAlreadySyncedData += (obj, e) =>
             {
+                string title = "Integrity error of already synced data - System Storage";
+
+                if (Logger.IsOperationsEnabled)
+                    Logger.Operations($"{title}. {e.Message}", e.Exception);
+
                 var alert = AlertRaised.Create(
                     null,
-                    "Integrity error of already synced data - System Storage",
+                    title,
                     e.Message,
                     AlertType.IntegrityErrorOfAlreadySyncedData,
                     NotificationSeverity.Warning,
-                    "Integrity Error of Synced Data - System");
+                    key: $"Integrity Error of Synced Data - System/{SystemTime.UtcNow.Ticks % 5}"); // if this was called multiple times let's try to not overwrite previous alerts
 
                 if (NotificationCenter.IsInitialized)
                 {
