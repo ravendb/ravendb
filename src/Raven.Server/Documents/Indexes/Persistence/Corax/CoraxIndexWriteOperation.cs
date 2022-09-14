1b7d30cc8bddd4121b061f625f39b3624873ba7f
Maciej Aszyk <maciej@ravendb.net>
2022-09-14 12:28:44 +0200

    RavenDB-19352 Removing unnecessary allocation in CoraxIndexWriteOperation

--- src/Raven.Server/Documents/Indexes/Persistence/Corax/CoraxIndexWriteOperation.cs
+++ src/Raven.Server/Documents/Indexes/Persistence/Corax/CoraxIndexWriteOperation.cs
@@ -27,13 +27,10 @@ namespace Raven.Server.Documents.Indexes.Persistence.Corax
         private readonly IndexWriter _indexWriter;
         private readonly CoraxDocumentConverterBase _converter;
         private readonly IndexFieldsMapping _knownFields;
-        private readonly IDisposable _bufferScope;
-        private readonly ByteString _buffer;
         private long _entriesCount = 0;
 
         public CoraxIndexWriteOperation(Index index, Transaction writeTransaction, CoraxDocumentConverterBase converter, Logger logger) : base(index, logger)
         {
-            _bufferScope = writeTransaction.Allocator.Allocate(DocumentBufferSize, out _buffer);
             _converter = converter;
             _knownFields = _converter.GetKnownFieldsForWriter();
             try
@@ -173,7 +170,6 @@ namespace Raven.Server.Documents.Indexes.Persistence.Corax
         
         public override void Dispose()
         {
-            _bufferScope?.Dispose();
             _indexWriter?.Dispose();
             if (_converter.StringsListForEnumerableScope?.Capacity > MaximumPersistedCapacityOfCoraxWriter)
             {
