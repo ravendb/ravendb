d09426d355dfc98dddaf375cbb72de637cd1621b
Pawel Pekrol <ppekrol@ravendb.net>
2022-09-19 16:58:55 +0200

    RavenDB-18909 manually handling RavenEtlTestScriptResult conversion to DJV like in previous versions

--- src/Raven.Server/Documents/ETL/Providers/Raven/Test/RavenEtlTestScriptResult.cs
+++ src/Raven.Server/Documents/ETL/Providers/Raven/Test/RavenEtlTestScriptResult.cs
@@ -1,11 +1,29 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Raven.Client.Documents.Commands.Batches;
+using Raven.Client.Documents.Conventions;
 using Raven.Server.Documents.ETL.Test;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
 
 namespace Raven.Server.Documents.ETL.Providers.Raven.Test
 {
     public class RavenEtlTestScriptResult : TestEtlScriptResult
     {
         public List<ICommandData> Commands { get; set; }
+
+        public override DynamicJsonValue ToJson(JsonOperationContext context)
+        {
+            var json = new DynamicJsonValue
+            {
+                [nameof(TransformationErrors)] = new DynamicJsonArray(TransformationErrors.Select(x => x.ToJson())),
+                [nameof(DebugOutput)] = new DynamicJsonArray(DebugOutput)
+            };
+
+            if (Commands != null)
+                json[nameof(Commands)] = new DynamicJsonArray(Commands.Select(x => x.ToJson(DocumentConventions.DefaultForServer, context)));
+
+            return json;
+        }
     }
 }
--- src/Raven.Server/Documents/ETL/Test/TestEtlScriptResult.cs
+++ src/Raven.Server/Documents/ETL/Test/TestEtlScriptResult.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
 using Raven.Server.NotificationCenter.Notifications.Details;
+using Raven.Server.Utils;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
 
 namespace Raven.Server.Documents.ETL.Test
 {
@@ -8,5 +11,10 @@ namespace Raven.Server.Documents.ETL.Test
         public List<EtlErrorInfo> TransformationErrors { get; set; }
 
         public List<string> DebugOutput { get; set; }
+
+        public virtual DynamicJsonValue ToJson(JsonOperationContext context)
+        {
+            return (DynamicJsonValue)TypeConverter.ToBlittableSupportedType(this);
+        }
     }
 }
