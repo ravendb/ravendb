3e2dab83e1c4aed7a03f9d21d4ff16231f76235b
Federico Lois <federico.lois@gmail.com>
2022-09-23 16:51:32 -0300

    RavenDB-19331: Instead of allocating on the GC allocate unmanaged
    The rationale behind this is that Set objects are usually short-lived (when querying) and therefore the cost that the GC incurs to reclaim the array of the cursor is higher than just let the memory be reclaimed by the transaction ending.

---
+++ src/Sparrow.Server/UnmanagedSpan.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Sparrow.Server
+{
+    public readonly unsafe struct UnmanagedSpan<T> where T : unmanaged
+    {
+        public readonly T* Address;
+        public readonly int Length;
+
+        public UnmanagedSpan(void* address, int length)
+        {
+            Address = (T*)address;
+            Length = length / sizeof(T);
+        }
+
+        public UnmanagedSpan(UnmanagedSpan<T> pointer, int length)
+        {
+            Address = pointer.Address;
+            Length = length;
+        }
+
+        public UnmanagedSpan(UnmanagedPointer pointer, int length)
+        {
+            Address = (T*)pointer.Address;
+            Length = length / sizeof(T);
+        }
+
+        public UnmanagedSpan<T> Slice(int offset)
+        {
+            return new UnmanagedSpan<T>(Address + offset, Length - offset);
+        }
+
+        public UnmanagedSpan<T> Slice(int position, int length)
+        {
+            return new UnmanagedSpan<T>(Address + position, length);
+        }
+
+        public ref T this[int idx]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get { return ref Address[idx]; }
+        }
+
+        public Span<T> ToSpan() => new(Address, Length);
+        public ReadOnlySpan<T> ToReadOnlySpan() => new(Address, Length);
+
+        public static UnmanagedSpan<T> operator +(UnmanagedSpan<T> pointer, int offset) => new(pointer.Address + offset, pointer.Length - offset);
+
+        public static implicit operator Span<T>(UnmanagedSpan<T> pointer) => new(pointer.Address, pointer.Length);
+        public static implicit operator ReadOnlySpan<T>(UnmanagedSpan<T> pointer) => new(pointer.Address, pointer.Length);
+    }
+}
