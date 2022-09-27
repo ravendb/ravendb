f0bc8feb06993625620ac5c35b9b8cf6a5438ce4
Pawel Pekrol <ppekrol@ravendb.net>
2022-09-27 10:09:34 +0200

    RavenDB-19399 throw only in DEBUG

--- src/Raven.Client/Util/HttpClientHandlerHelper.cs
+++ src/Raven.Client/Util/HttpClientHandlerHelper.cs
@@ -15,9 +15,10 @@ internal static class HttpClientHandlerHelper
 #if NETCOREAPP3_1_OR_GREATER
         GetUnderlyingHandler = typeof(HttpClientHandler).GetField("_underlyingHandler", BindingFlags.Instance | BindingFlags.NonPublic) ??
                                typeof(HttpClientHandler).GetField("_socketsHttpHandler", BindingFlags.Instance | BindingFlags.NonPublic);
-
+#if DEBUG
         if (GetUnderlyingHandler == null)
             throw new InvalidOperationException("Could not get underlying handler field from HttpClientHandler.");
+#endif
 #endif
     }
 
@@ -30,6 +31,9 @@ internal static class HttpClientHandlerHelper
             return;
 
 #if NETCOREAPP3_1_OR_GREATER
+        if (GetUnderlyingHandler == null)
+            return;
+
         if (GetUnderlyingHandler.GetValue(handler) is not SocketsHttpHandler underlyingHandler)
             throw new InvalidOperationException("Underlying handler for HttpClientHandler is not SocketsHttpHandler");
 
