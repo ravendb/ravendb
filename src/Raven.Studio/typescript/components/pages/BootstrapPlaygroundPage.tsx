584a166a9c81a6f7290841275083898afbb18719
kwiato <kwiato.r@gmail.com>
2022-09-28 10:06:36 +0200

    RavenDB-19193 Check if we can use reactstrap with bootstrap 5 along with old bootstrap 3.

--- src/Raven.Studio/typescript/components/pages/BootstrapPlaygroundPage.tsx
+++ src/Raven.Studio/typescript/components/pages/BootstrapPlaygroundPage.tsx
@@ -29,6 +29,10 @@ import {
     ModalFooter,
     ModalHeader,
     Table,
+    Accordion,
+    AccordionBody,
+    AccordionHeader,
+    AccordionItem
 } from "reactstrap";
 
 const items = [
@@ -355,12 +359,54 @@ function AlertExample() {
     );
 }
 
-export function BootstrapPlaygroundPage() {
+function AccordionExample(props:any) {
+    const [open, setOpen] = useState('1');
+    
+
     return (
         <div>
+            <Accordion open={open}>
+                <AccordionItem>
+                    <AccordionHeader targetId="1">Accordion Item 1</AccordionHeader>
+                    <AccordionBody accordionId="1">
+                        <strong>This is the first item&#39;s accordion body.</strong>
+                        You can modify any of this with custom CSS or overriding our default
+                        variables. It&#39;s also worth noting that just about any HTML can
+                        go within the <code>.accordion-body</code>, though the transition
+                        does limit overflow.
+                    </AccordionBody>
+                </AccordionItem>
+                <AccordionItem>
+                    <AccordionHeader targetId="2">Accordion Item 2</AccordionHeader>
+                    <AccordionBody accordionId="2">
+                        <strong>This is the second item&#39;s accordion body.</strong>
+                        You can modify any of this with custom CSS or overriding our default
+                        variables. It&#39;s also worth noting that just about any HTML can
+                        go within the <code>.accordion-body</code>, though the transition
+                        does limit overflow.
+                    </AccordionBody>
+                </AccordionItem>
+                <AccordionItem>
+                    <AccordionHeader targetId="3">Accordion Item 3</AccordionHeader>
+                    <AccordionBody accordionId="3">
+                        <strong>This is the third item&#39;s accordion body.</strong>
+                        You can modify any of this with custom CSS or overriding our default
+                        variables. It&#39;s also worth noting that just about any HTML can
+                        go within the <code>.accordion-body</code>, though the transition
+                        does limit overflow.
+                    </AccordionBody>
+                </AccordionItem>
+            </Accordion>
+        </div>
+    );
+}
+
+export function BootstrapPlaygroundPage() {
+    return (
+        <div className="bs5 panel">
             <h1>Bootstrap 5 playground</h1>
 
-            <div className="bs5">
+            <div>
                 <Example />
                 <Carousel2 />
                 <ModalUsage />
@@ -372,7 +418,13 @@ export function BootstrapPlaygroundPage() {
                     TEST
                 </Button>
                 <FormExample />
+
+                <AccordionExample />
             </div>
         </div>
     );
 }
+
+
+
+
