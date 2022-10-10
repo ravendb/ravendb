92e01c8c70e09674a341a3c646b05c9b6bc1091d
Maciej Aszyk <maciej@ravendb.net>
2022-10-10 11:56:42 +0200

    RavenDB-17528 Tests fix | Some PR comments

--- src/Raven.Server/Documents/Indexes/Persistence/Corax/AnalyzersScope.cs
+++ src/Raven.Server/Documents/Indexes/Persistence/Corax/AnalyzersScope.cs
@@ -9,14 +9,13 @@ namespace Raven.Server.Documents.Indexes.Persistence.Corax;
 
 internal class AnalyzersScope : IDisposable
 {
-    private IndexDynamicFieldsMapping _dynamicFields;
+    private readonly IndexDynamicFieldsMapping _dynamicFields;
     private readonly IndexFieldsMapping _knownFields;
     private Token[] _tokensBuffer;
     private byte[] _outputBuffer;
     private readonly IndexSearcher _indexSearcher;
-    private Dictionary<Slice, Analyzer> _analyzersCache;
-
-
+    private readonly Dictionary<Slice, Analyzer> _analyzersCache;
+    
     public AnalyzersScope(IndexSearcher indexSearcher, IndexFieldsMapping fieldsMapping, bool hasDynamics)
     {
         _indexSearcher = indexSearcher;
@@ -29,9 +28,9 @@ internal class AnalyzersScope : IDisposable
         }
     }
 
-    public void Execute(Slice name, ReadOnlySpan<byte> source, out ReadOnlySpan<byte> buffer, out ReadOnlySpan<Token> tokens)
+    public void Execute(Slice fieldName, ReadOnlySpan<byte> source, out ReadOnlySpan<byte> buffer, out ReadOnlySpan<Token> tokens)
     {
-        Analyzer analyzer = GetAnalyzer(name);
+        Analyzer analyzer = GetAnalyzer(fieldName);
 
         analyzer.GetOutputBuffersSize(source.Length, out var outputSize, out var tokensSize);
 
@@ -47,33 +46,46 @@ internal class AnalyzersScope : IDisposable
         tokens = tokenOutput;
     }
 
-    private Analyzer GetAnalyzer(Slice name)
+    private Analyzer GetAnalyzer(Slice fieldName)
     {
         Analyzer analyzer;
-        if (_analyzersCache.ContainsKey(name))
-            return _analyzersCache[name];
+        if (_analyzersCache.ContainsKey(fieldName))
+            return _analyzersCache[fieldName];
 
-        if (_knownFields.TryGetByFieldName(name, out var binding))
+        if (_knownFields.TryGetByFieldName(fieldName, out var binding))
         {
             analyzer = binding.Analyzer;
         }
         else
         {
             if (_dynamicFields is null)
-                throw new InvalidDataException($"Cannot find field {name.ToString()} inside known binding and also index doesn't contain dynamic fields. The field you try to read is not inside index.");
-            var mode = _indexSearcher.GetFieldIndexingModeForDynamic(name);
+                ThrowWhenDynamicFieldNotFound(fieldName);
+            var mode = _indexSearcher.GetFieldIndexingModeForDynamic(fieldName);
+            
             analyzer = mode switch
             {
-                FieldIndexingMode.Normal => _dynamicFields.DefaultAnalyzer,
-                FieldIndexingMode.Search => _dynamicFields.DefaultSearchAnalyzer(name.ToString()),
+                FieldIndexingMode.Normal => _dynamicFields!.DefaultAnalyzer,
+                FieldIndexingMode.Search => _dynamicFields!.DefaultSearchAnalyzer(fieldName.ToString()),
                 FieldIndexingMode.No => Analyzer.DefaultAnalyzer,
-                FieldIndexingMode.Exact => _dynamicFields.DefaultExactAnalyzer(name.ToString())
+                FieldIndexingMode.Exact => _dynamicFields!.DefaultExactAnalyzer(fieldName.ToString()),
+                _ => ThrowWhenAnalyzerModeNotFound(mode)
             };
         }
 
-        _analyzersCache[name] = analyzer;
+        _analyzersCache[fieldName] = analyzer;
         
         return analyzer;
+    }
+
+    private static Analyzer ThrowWhenAnalyzerModeNotFound(FieldIndexingMode mode)
+    {
+        throw new ArgumentOutOfRangeException($"{mode} is not implemented in {nameof(AnalyzersScope)}");
+    }
+
+    private static void ThrowWhenDynamicFieldNotFound(Slice fieldName)
+    {
+        throw new InvalidDataException(
+            $"Cannot find field {fieldName.ToString()} inside known binding and also index doesn't contain dynamic fields. The field you try to read is not inside index.");
     }
 
     private void UnlikelyGrowBuffer(int outputSize, int tokensSize)
