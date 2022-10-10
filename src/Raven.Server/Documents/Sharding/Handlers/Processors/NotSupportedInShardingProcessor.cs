cf0e90b8f99dcc3d73a2277c917025af4e9d1898
Pawel Pekrol <ppekrol@ravendb.net>
2022-10-10 15:31:54 +0200

    RavenDB-19256 throwing NotSupportedInShardingException for PostgreSQL endpoints

---
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/NotSupportedInShardingProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Client.Exceptions.Sharding;
+using Raven.Server.Documents.Handlers.Processors;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Documents.Sharding.Handlers.Processors;
+
+internal class NotSupportedInShardingProcessor : AbstractDatabaseHandlerProcessor<ShardedDatabaseRequestHandler, TransactionOperationContext>
+{
+    private readonly string _message;
+
+    public NotSupportedInShardingProcessor([NotNull] ShardedDatabaseRequestHandler requestHandler, [NotNull] string message)
+        : base(requestHandler)
+    {
+        _message = message ?? throw new ArgumentNullException(nameof(message));
+    }
+
+    public override ValueTask ExecuteAsync()
+    {
+        throw new NotSupportedInShardingException(_message);
+    }
+}
