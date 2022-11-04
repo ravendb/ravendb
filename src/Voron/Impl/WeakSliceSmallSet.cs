5bd829c2ef722011f2cc7d2c1938dd2ab1cb8ab1
Federico Lois <federico.lois@gmail.com>
2022-11-04 12:24:11 -0300

    RavenDB-19331: Introduction of Weak Small Sets for fast lookup on bounded sets.

---
+++ src/Voron/Impl/WeakSliceSmallSet.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Buffers;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using Sparrow;
+using Sparrow.Server;
+
+namespace Voron.Impl
+{
+    // The concept of the weak small set is about an optimized set that can lose elements when it gets filled.
+    // In a sense it behaves like a forgetful LRU cache, but will not track the read accesses. This version of the SmallSet
+    // is specially designed to deal with sequence values stored in byte pointers. The main difference with a
+    // dictionary is that the small set will calculate the hash ONLY if there are candidates that match already. 
+    public sealed class WeakSliceSmallSet<TValue> : IDisposable
+    {
+        private const int Invalid = -1;
+
+        private readonly int _length;
+        private readonly int[] _keySizes;
+        private readonly ulong[] _keyHashes;
+        private readonly Slice[] _keys;
+        private readonly TValue[] _values;
+        private int _currentIdx;
+
+        public WeakSliceSmallSet(int size = 0)
+        {
+            _length = size > Vector<long>.Count ? size - size % Vector<long>.Count : Vector<long>.Count;
+            _keySizes = ArrayPool<int>.Shared.Rent(_length);
+            _keyHashes = ArrayPool<ulong>.Shared.Rent(_length);
+            _keys = ArrayPool<Slice>.Shared.Rent(_length);
+            _values = ArrayPool<TValue>.Shared.Rent(_length);
+            _currentIdx = -1;
+        }
+
+        public unsafe void Add(Slice key, TValue value)
+        {
+            int idx = FindKey(key);
+            if (idx == Invalid)
+                idx = RequestWritableBucket();
+
+            _keys[idx] = key;
+            _keySizes[idx] = key.Size;
+            _keyHashes[idx] = Hashing.XXHash64.CalculateInline(key.Content.Ptr, (ulong)key.Size);
+            _values[idx] = value;
+
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private unsafe int FindKey(Slice key)
+        {
+            if (_currentIdx == Invalid)
+                return Invalid;
+
+            int keyLength = key.Size;
+            ulong keyHash = 0;
+            
+            Slice[] keys = _keys;
+            int[] keySizes = _keySizes;
+            ulong[] keyHashes = _keyHashes;
+            byte* keyPtr = key.Content.Ptr;
+
+            // PERF: It may seems strange to increase the size to decrement it as the first
+            // loop operation. The reason behind this is to be able to just jump back immediately
+            // to the top of the loop as soon as we know the item is not the item we are looking
+            // for. 
+            int elementIdx = Math.Min(_currentIdx, _length - 1);
+            while (elementIdx >= 0)
+            {
+                var currentIdx = elementIdx;
+                elementIdx--;
+
+                // First check, we are not going to look into any string that is not of the correct size
+                if (keySizes[currentIdx] != keyLength)
+                    continue;
+
+                // PERF: Assuming a uniformly random symbol distribution, the chance that first two symbols match
+                // (i.e.that S1[1] = S2[1]) is equal to 1/σ, the chance that both first and second symbol pairs
+                // match(i.e.that S1[1] = S2[1] and S1[2] = S2[2]) is equal to 1/σ^2, etc. More generally,
+                // the probability that there is a match between all characters up to a 1 - indexed position i
+                // is equal to 1 / σ^i. We are using that knowledge to quickly get rid of elements.
+                ref var candidateKey = ref keys[currentIdx];
+                
+                int midValue = (keyLength - 1) / 2;
+                if (key[0] != candidateKey[0] || key[midValue] != candidateKey[midValue] || key[keyLength - 1] != candidateKey[keyLength - 1])
+                    continue;
+                
+
+                // If there is a match, we will essentially want to quickly get rid of anything that looks 
+                // potentially wrong. For that we will use the hash, which will only get calculated if there
+                // are at least 1 strong candidate.
+                if (keyHash == 0)
+                    keyHash = Hashing.XXHash64.CalculateInline(keyPtr, (ulong)key.Size);
+
+                if (keyHashes[currentIdx] != keyHash)
+                    continue;
+
+                // We now know that we have an almost sure hit. We will do a final verification at this time.
+                // TODO: Implement a vectorized version of the Equals operation. 
+                if ( AdvMemory.CompareInline(keyPtr, keys[currentIdx].Content.Ptr, keyLength) != 0 )
+                    continue;
+
+                return currentIdx;
+            }
+
+            return Invalid;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int RequestWritableBucket()
+        {
+            _currentIdx++;
+            return _currentIdx % _length;
+        }
+
+        public bool TryGetValue(Slice key, out TValue value)
+        {
+            int idx = FindKey(key);
+            if (idx == Invalid)
+            {
+                Unsafe.SkipInit(out value);
+                return false;
+            }
+
+            value = _values[idx];
+            return true;
+        }
+
+        public void Clear()
+        {
+            Array.Fill(_keySizes, 0);
+            Array.Fill(_keys, default);
+            Array.Fill(_values, default);
+            _currentIdx = -1;
+        }
+
+        public void Dispose()
+        {
+            ArrayPool<int>.Shared.Return(_keySizes);
+            ArrayPool<ulong>.Shared.Return(_keyHashes);
+            ArrayPool<Slice>.Shared.Return(_keys);
+            ArrayPool<TValue>.Shared.Return(_values);
+        }
+    }
+}
---
+++ test/FastTests/Sparrow/SmallSet.cs
@@ -0,0 +1,125 @@
+﻿using Sparrow.Server.Collections;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FastTests.Sparrow
+{
+    public class SmallSetTests : NoDisposalNeeded
+    {
+        public SmallSetTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void WeakSetSingleItem()
+        {
+            var ilSet = new WeakSmallSet<int, long>(128);
+            ilSet.Add(10, 10);
+            Assert.True(ilSet.TryGetValue(10, out var iv));
+            Assert.Equal(10, iv);
+
+
+            var llSet = new WeakSmallSet<long, long>(128);
+            llSet.Add(10, 10);
+            Assert.True(llSet.TryGetValue(10, out var lv));
+            Assert.Equal(10, lv);
+        }
+
+        [Fact]
+        public void WeakSetSingleLane()
+        {
+            var ilSet = new WeakSmallSet<int, long>(128);
+
+            for (int i = 0; i < 8; i++)
+                ilSet.Add(i, i);
+
+            for (int i = 0; i < 8; i++)
+            {
+                Assert.True(ilSet.TryGetValue(i, out var iv));
+                Assert.Equal(i, iv);
+            }
+
+            var llSet = new WeakSmallSet<long, long>(128);
+            for (int i = 0; i < 4; i++)
+                llSet.Add(i, i);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Assert.True(llSet.TryGetValue(i, out var lv));
+                Assert.Equal(i, lv);
+            }
+        }
+
+        [Fact]
+        public void WeakSetSingleSmallerThanVector()
+        {
+            var llSet = new WeakSmallSet<long, long>(3);
+            llSet.Add(10, 10);
+            Assert.True(llSet.TryGetValue(10, out var v));
+            Assert.Equal(10, v);
+        }
+
+        [Fact]
+        public void WeakSetDuplicateItems()
+        {
+            var ilSet = new WeakSmallSet<int, long>();
+            for (int i = 0; i < 8; i++)
+                ilSet.Add(i, i);
+
+            Assert.True(ilSet.TryGetValue(7, out var iv));
+            Assert.Equal(7, iv);
+
+            for (int i = 0; i < 8; i++)
+                ilSet.Add(7, -1);
+
+            for (int i = 0; i < 7; i++)
+            {
+                Assert.True(ilSet.TryGetValue(i, out iv));
+                Assert.Equal(i, iv);
+            }
+
+            Assert.True(ilSet.TryGetValue(7, out iv));
+            Assert.Equal(-1, iv);
+        }
+
+        [Fact]
+        public void WeakSetEviction()
+        {
+            var ilSet = new WeakSmallSet<int, long>();
+            for (int i = 0; i < 8; i++)
+                ilSet.Add(i, i);
+
+            for (int i = 0; i < 7; i++)
+            {
+                Assert.True(ilSet.TryGetValue(i, out var iv));
+                Assert.True(iv >= 0);
+            }
+
+            for (int i = 0; i < 8; i++)
+                ilSet.Add(i + 8, -i);
+
+            for (int i = 0; i < 7; i++)
+            {
+                Assert.True(ilSet.TryGetValue(i + 8, out var iv));
+                Assert.True(iv <= 0);
+            }
+        }
+
+        [Fact]
+        public void WeakMultipleChunks()
+        {
+            var ilSet = new WeakSmallSet<int, long>(16);
+            for (int i = 0; i < 16; i++)
+                ilSet.Add(i, i);
+
+            Assert.True(ilSet.TryGetValue(15, out var iv));
+            Assert.Equal(15, iv);
+
+            for (int i = 0; i < 16; i++)
+            {
+                Assert.True(ilSet.TryGetValue(i, out iv));
+                Assert.Equal(i, iv);
+            }
+        }
+    }
+}
