b2c30635261399c2c7e97050f70596f661c8066b
Gracjan Sadowicz <gracjan@ravendb.net>
2022-11-07 12:38:58 +0100

    RavenDB-19488 Return empty results when skipped more docs than queried index size

---
+++ test/SlowTests/Issues/RavenDB-19488.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Raven.Client.Documents.Indexes;
+using Xunit;
+using Xunit.Abstractions;
+using FastTests;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_19488 : RavenTestBase
+    {
+        
+        public RavenDB_19488(ITestOutputHelper output) : base(output)
+        {
+        }     
+        
+        [Fact]
+        public async Task ShouldReturnEmptyResultsWhenSkippedMoreDocsThanCollectionSize()
+        {
+            using var store = GetDocumentStore();
+            {
+                using var session = store.OpenAsyncSession();
+                await session.StoreAsync(new Data { Name = "Test" });
+                await session.SaveChangesAsync();
+            }
+            var index = new DataIndex();
+            await index.ExecuteAsync(store);
+            Indexes.WaitForIndexing(store);
+            {
+                using var session = store.OpenAsyncSession();
+
+                var query = session.Advanced.AsyncDocumentQuery<Data, DataIndex>()
+
+                    .Skip(2).Take(1)
+                    .SelectFields<Data>("Name") //This is mandatory for reproducing, without this statement query executes and returns empty result as expected
+                    .Distinct(); //This is mandatory for reproducing, without this statement query executes and returns an empty result as expected; //Skip 2 rows (only 1 row exists in collection), for values less than 2 works as expected
+                Assert.Empty(await query.ToListAsync()); // Raven.Client.Exceptions.RavenException System.IndexOutOfRangeException: Index was outside the bounds of the array.
+                
+                query = session.Advanced.AsyncDocumentQuery<Data, DataIndex>()
+                
+                    .Skip(1).Take(1)
+                    .SelectFields<Data>("Name") 
+                    .Distinct(); 
+                Assert.Empty(await query.ToListAsync());
+            }
+        }
+
+        private class Data
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        private class DataIndex : AbstractIndexCreationTask<Data>
+        {
+            public DataIndex()
+            {
+                Map = datas => datas.Select(i => new { i.Name });
+            }
+        }
+    }
+}
