2f15c22199322be61f1c8cb2e4753864fee26968
Ayende Rahien <ayende@ayende.com>
2022-11-14 21:20:55 +0200

    RavenDB-19209 Fixing camel case of property names when using queries on facets

--- test/SlowTests/Issues/RavenDB-19209.cs
+++ test/SlowTests/Issues/RavenDB-19209.cs
@@ -2,6 +2,9 @@
 using System.Linq;
 using FastTests;
 using Newtonsoft.Json.Serialization;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Linq;
 using Raven.Client.Json.Serialization.NewtonsoftJson;
 using Xunit;
 using Xunit.Abstractions;
@@ -12,6 +15,60 @@ public class RavenDB_19209 : RavenTestBase
 {
     public RavenDB_19209(ITestOutputHelper output) : base(output)
     {
+    }
+
+
+    [Fact]
+    public void CamelCaseInStaticIndexes()
+    {
+        using var store = GetDocumentStore(new Options
+        {
+            ModifyDocumentStore = documentStore =>
+            {
+                documentStore.Conventions.Serialization = new NewtonsoftJsonSerializationConventions
+                {
+                    CustomizeJsonSerializer = (serializer) =>
+                    {
+                        serializer.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    }
+                };
+                documentStore.Conventions.PropertyNameConverter = mi => $"{char.ToLower(mi.Name[0])}{mi.Name[1..]}";
+            }
+        });
+        using (var session = store.OpenSession())
+        {
+            var workspace1 = new Workspace {Domain = "Encom", CamelCase = "SuperSecretTest"};
+            session.Store(workspace1, "workspaces/1");
+            session.SaveChanges();
+        }
+        new CamelCaseIndex().Execute(store);
+
+        using (var session = store.OpenSession())
+        {
+            var q = session.Query<Workspace, CamelCaseIndex>().AggregateBy(builder => builder.ByField(i => i.CamelCase));
+            Assert.Equal("from index 'CamelCaseIndex' select facet(camelCase)", q.ToString());
+            var aggregations = q.Execute();
+            Assert.Equal(1, aggregations.Count);
+            Assert.Equal("camelCase", aggregations.First().Key);
+        }
+    }
+
+    private class CamelCaseIndex : AbstractIndexCreationTask<Workspace>
+    {
+        public CamelCaseIndex()
+        {
+            Map = workspaces => workspaces.Select(i => new { Domain = i.Domain, CamelCase = i.CamelCase });
+        }
+    }
+    
+    private class Workspace
+    {
+        public string Id { get; set; }
+
+        //[JsonProperty("domain")]
+        public string Domain { get; set; }
+
+        public string CamelCase { get; set; }
     }
 
     [Fact]
@@ -46,14 +103,5 @@ public class RavenDB_19209 : RavenTestBase
                 Assert.NotNull(q.FirstOrDefault());
             }
         }
-    }
-
-
-    private class Workspace
-    {
-        public string Id { get; set; }
-
-        //[JsonProperty("domain")]
-        public string Domain { get; set; }
     }
 }
