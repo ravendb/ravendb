8237afb077ea2fd47dbe0ecea314dfccfd5d5e46
danielle9897 <danielle.9897@gmail.com>
2022-11-23 14:51:44 +0200

    RavenDB-19478 Small text fixes

--- src/Raven.Studio/typescript/commands/database/index/optimizeIndexCommand.ts
+++ src/Raven.Studio/typescript/commands/database/index/optimizeIndexCommand.ts
@@ -16,7 +16,7 @@ class optimizeIndexCommand extends commandBase {
         const url = endpoints.databases.adminIndex.adminIndexesOptimize + this.urlEncodeArgs(args);
         
         return this.post(url, null, this.db)
-            .done(() => this.reportSuccess(`Started the optimize index: ${this.indexName}`))
+            .done(() => this.reportSuccess(`Started to optimize index: ${this.indexName}`))
             .fail((response: JQueryXHR) => this.reportError("Failed to optimize index files", response.responseText));
     }
 }
