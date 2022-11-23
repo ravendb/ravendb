c92c8bb34bb1d84a58909d065648588e3e734f27
shaharhikri <shaharhikri@ravendb.net>
2022-11-23 18:12:59 +0200

    RavenDB-19561 - Replication memory issues leading to OOM restart every few minutes

--- src/Raven.Server/Documents/Replication/ReplicationDocumentSender.cs
+++ src/Raven.Server/Documents/Replication/ReplicationDocumentSender.cs
@@ -318,8 +318,12 @@ namespace Raven.Server.Documents.Replication
                             }
 
                             replicationState.Size += item.Size;
-
                             replicationState.NumberOfItemsSent++;
+
+                            if (replicationState.Size >= _parent._database.Configuration.Replication.MaxSizeToLoadFromStorage?.GetValue(SizeUnit.Bytes))
+                            {
+                                break;
+                            }
                         }
                     }
 
