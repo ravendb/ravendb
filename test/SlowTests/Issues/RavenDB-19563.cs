6c6b1d9ea132511d7853fb724725ae3d829e04ac
Grisha Kotler <grisha@ayende.com>
2022-11-27 16:09:50 +0200

    RavenDB-19563 - add tests for counters and timeseries

--- test/SlowTests/Issues/RavenDB-19563.cs
+++ test/SlowTests/Issues/RavenDB-19563.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using FastTests;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Indexes.Counters;
+using Raven.Client.Documents.Indexes.TimeSeries;
 using Raven.Client.Documents.Operations.Backups;
 using Raven.Server.Utils;
 using SlowTests.Core.Utils.Entities;
@@ -139,6 +141,136 @@ namespace SlowTests.Issues
             }
         }
 
+        [Fact]
+        public async Task Snapshot_should_have_correct_index_entries_after_snapshot_and_incremental_restore_counters()
+        {
+            var backupPath = NewDataPath();
+            IOExtensions.DeleteDirectory(backupPath);
+
+            using (var store = GetDocumentStore())
+            {
+                const string id = "users/1";
+                const string counterName = "Count";
+
+                await new UsersCountersMapReduceIndex().ExecuteAsync(store);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User
+                    {
+                        Name = "Grisha"
+                    }, id);
+                    session.CountersFor(id).Increment(counterName, 1);
+                    await session.SaveChangesAsync();
+                }
+
+                Indexes.WaitForIndexing(store);
+
+                var database = await Server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(store.Database);
+                long cleanedTombstones = 0;
+                database.ForTestingPurposesOnly().BeforeSnapshotOfDocuments = () =>
+                {
+                    using (var session = store.OpenSession())
+                    {
+                        session.Advanced.WaitForIndexesAfterSaveChanges();
+                        session.CountersFor(id).Delete(counterName);
+                        session.SaveChanges();
+                    }
+
+                    cleanedTombstones = database.TombstoneCleaner.ExecuteCleanup().GetAwaiter().GetResult();
+                };
+
+                var config = Backup.CreateBackupConfiguration(backupPath, backupType: BackupType.Snapshot);
+                await Backup.UpdateConfigAndRunBackupAsync(Server, config, store);
+                Assert.Equal(0, cleanedTombstones);
+
+                var databaseName = $"restored_database-{Guid.NewGuid()}";
+
+                using (Backup.RestoreDatabase(store, new RestoreBackupConfiguration
+                {
+                    BackupLocation = Directory.GetDirectories(backupPath).First(),
+                    DatabaseName = databaseName
+                }))
+                {
+                    Indexes.WaitForIndexing(store, databaseName);
+
+                    using (var session = store.OpenAsyncSession(databaseName))
+                    {
+                        var counter = await session.CountersFor(id).GetAsync(counterName);
+                        Assert.Null(counter);
+
+                        var usersCount = await session.Query<User, UsersCountersMapReduceIndex>().CountAsync();
+                        Assert.Equal(0, usersCount);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Snapshot_should_have_correct_index_entries_after_snapshot_and_incremental_restore_timeseries()
+        {
+            var backupPath = NewDataPath();
+            IOExtensions.DeleteDirectory(backupPath);
+
+            using (var store = GetDocumentStore())
+            {
+                const string id = "users/1";
+                const string timeSeriesName = "Count";
+
+                await new UsersTimeSeriesMapReduceIndex().ExecuteAsync(store);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User
+                    {
+                        Name = "Grisha"
+                    }, id);
+                    session.TimeSeriesFor(id, timeSeriesName).Append(DateTime.Today, 3);
+                    await session.SaveChangesAsync();
+                }
+
+                Indexes.WaitForIndexing(store);
+
+                var database = await Server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(store.Database);
+                long cleanedTombstones = 0;
+                database.ForTestingPurposesOnly().BeforeSnapshotOfDocuments = () =>
+                {
+                    using (var session = store.OpenSession())
+                    {
+                        session.Advanced.WaitForIndexesAfterSaveChanges();
+                        session.TimeSeriesFor(id, timeSeriesName).Delete();
+                        session.SaveChanges();
+                    }
+
+                    cleanedTombstones = database.TombstoneCleaner.ExecuteCleanup().GetAwaiter().GetResult();
+                };
+
+                var config = Backup.CreateBackupConfiguration(backupPath, backupType: BackupType.Snapshot);
+                await Backup.UpdateConfigAndRunBackupAsync(Server, config, store);
+                Assert.Equal(0, cleanedTombstones);
+
+                var databaseName = $"restored_database-{Guid.NewGuid()}";
+
+                using (Backup.RestoreDatabase(store, new RestoreBackupConfiguration
+                {
+                    BackupLocation = Directory.GetDirectories(backupPath).First(),
+                    DatabaseName = databaseName
+                }))
+                {
+                    Indexes.WaitForIndexing(store, databaseName);
+
+                    using (var session = store.OpenAsyncSession(databaseName))
+                    {
+                        var entries = await session.TimeSeriesFor(id, timeSeriesName).GetAsync();
+                        Assert.Null(entries);
+
+                        var usersCount = await session.Query<User, UsersTimeSeriesMapReduceIndex>().CountAsync();
+                        Assert.Equal(0, usersCount);
+                    }
+                }
+            }
+        }
+
         private class UsersIndex : AbstractIndexCreationTask<User>
         {
             public UsersIndex()
@@ -161,6 +293,50 @@ namespace SlowTests.Issues
                         Name = user.Name,
                         Count = 1
                     };
+
+                Reduce = results => from result in results
+                    group result by new { result.Name } into g
+                    select new
+                    {
+                        Name = g.Key.Name,
+                        Count = g.Sum(x => x.Count)
+                    };
+            }
+        }
+
+        private class UsersCountersMapReduceIndex : AbstractCountersIndexCreationTask<User>
+        {
+            public UsersCountersMapReduceIndex()
+            {
+                AddMapForAll(counters => from counter in counters
+                    select new
+                    {
+                        Name = counter.Name,
+                        Count = 1
+                    });
+
+                Reduce = results => from result in results
+                    group result by new { result.Name } into g
+                    select new
+                    {
+                        Name = g.Key.Name,
+                        Count = g.Sum(x => x.Count)
+                    };
+            }
+        }
+
+        private class UsersTimeSeriesMapReduceIndex : AbstractTimeSeriesIndexCreationTask<User>
+        {
+            public UsersTimeSeriesMapReduceIndex()
+            {
+                AddMapForAll(timeSeries => 
+                    from ts in timeSeries
+                    from entry in ts.Entries
+                    select new
+                    {
+                        Name = ts.Name,
+                        Count = 1
+                    });
 
                 Reduce = results => from result in results
                     group result by new { result.Name } into g
