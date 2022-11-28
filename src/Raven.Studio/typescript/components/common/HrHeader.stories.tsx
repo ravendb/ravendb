608bcb57d3fe6d227bca2077d48a025cbd5179aa
kwiato <kwiato.r@gmail.com>
2022-11-28 16:49:52 +0100

    RavenDB-19193 HrHeader fix

---
+++ src/Raven.Studio/typescript/components/common/HrHeader.stories.tsx
@@ -0,0 +1,28 @@
+﻿import { ComponentMeta } from "@storybook/react";
+import { HrHeader } from "./HrHeader";
+import React from "react";
+import { withBootstrap5, withStorybookContexts } from "../../test/storybookTestUtils";
+
+export default {
+    title: "Bits/HrHeader",
+    component: HrHeader,
+    decorators: [withStorybookContexts, withBootstrap5],
+} as ComponentMeta<typeof HrHeader>;
+
+export function HrHeaders() {
+    return (
+        <div>
+            <HrHeader>Header with divider</HrHeader>
+
+            <HrHeader
+                right={
+                    <>
+                        <strong>Additional content</strong>
+                    </>
+                }
+            >
+                Header with divider
+            </HrHeader>
+        </div>
+    );
+}
---
+++ src/Raven.Studio/typescript/components/common/HrHeader.tsx
@@ -0,0 +1,19 @@
+﻿import React, { ReactNode } from "react";
+import { FormGroup, Input, Label, Row } from "reactstrap";
+
+interface HrHeaderProps {
+    children?: ReactNode | ReactNode[];
+    right?: ReactNode | ReactNode[];
+}
+
+export function HrHeader(props: HrHeaderProps) {
+    const { right, children } = props;
+
+    return (
+        <div className="flex-horizontal align-items-center my-4">
+            {children && <h5 className="m-0 me-3">{children}</h5>}
+            <hr className="flex-grow-1 m-0" />
+            <div className="ms-3">{right}</div>
+        </div>
+    );
+}
