ca3975a9bda7dc2e1535735be9b753fec850b42e
shaharhikri <shaharhikri@ravendb.net>
2022-11-29 11:15:16 +0200

    RavenDB-17650 - Address PR Comments

--- test/SlowTests/Issues/RavenDB-17650.cs
+++ test/SlowTests/Issues/RavenDB-17650.cs
@@ -26,13 +26,12 @@ namespace SlowTests.Issues
         [Fact]
         public async Task Should_Retry_When_DatabaseDisabledException_Was_Thrown()
         {
-            var (nodes, leader) = await CreateRaftCluster(numberOfNodes: 3, shouldRunInMemory: false/*, watcherCluster: true*/);
             using var store = GetDocumentStore(new Options()
             {
-                ReplicationFactor = 3,
-                Server = leader,
+                ReplicationFactor = 1,
                 RunInMemory = false
             });
+            
             string id = "User/33-A";
             using (var session = store.OpenAsyncSession())
             {
@@ -40,7 +39,6 @@ namespace SlowTests.Issues
                 await session.StoreAsync(new User { Name = "2" });
                 await session.SaveChangesAsync();
             }
-            WaitForUserToContinueTheTest(store);
 
             await store.Subscriptions
                 .CreateAsync(new SubscriptionCreationOptions<User>
@@ -49,6 +47,7 @@ namespace SlowTests.Issues
                 });
             
             using var worker = store.Subscriptions.GetSubscriptionWorker<User>(new SubscriptionWorkerOptions("BackgroundSubscriptionWorker"));
+
             // disable database
             var disableSucceeded = store.Maintenance.Server.Send(new ToggleDatabasesStateOperation(store.Database, true));
             Assert.True(disableSucceeded.Success);
@@ -58,35 +57,24 @@ namespace SlowTests.Issues
             var mre = new ManualResetEvent(false);
             var t = worker.Run(async batch =>
             {
-                User user = null;
-                using (var session = batch.OpenAsyncSession())
-                {
-                    user = await session.LoadAsync<User>(id);
-                }
-                
-                if (user != null)
-                {
-                    mre.Set();
-                }
+                mre.Set();
             }, cts.Token);
 
             //enable database
-            await Task.Delay(2000);
+            await Task.Delay(500);
             var enableSucceeded = store.Maintenance.Server.Send(new ToggleDatabasesStateOperation(store.Database, false));
             Assert.False(enableSucceeded.Disabled);
             Assert.True(enableSucceeded.Success);
-            WaitForUserToContinueTheTest(store);
             Assert.True(mre.WaitOne(TimeSpan.FromSeconds(15)), "User didn't loaded.");
         }
 
         [Fact]
         public async Task Should_Retry_When_AllNodesTopologyDownException_Was_Thrown()
         {
-            var (nodes, leader) = await CreateRaftCluster(numberOfNodes: 3, shouldRunInMemory: false/*, watcherCluster: true*/);
+            var node = GetNewServer(new ServerCreationOptions { RunInMemory = false });
             using var store = GetDocumentStore(new Options()
             {
-                ReplicationFactor = 3,
-                Server = leader,
+                ReplicationFactor = 1,
                 RunInMemory = false
             });
             string id = "User/33-A";
@@ -107,44 +95,27 @@ namespace SlowTests.Issues
             using var worker = store.Subscriptions.GetSubscriptionWorker<User>(new SubscriptionWorkerOptions("BackgroundSubscriptionWorker"));
 
             // dispose nodes
-            var results = new List<(string DataDirectory, string Url, string NodeTag)>();
-            foreach (var node in nodes)
-            {
-                var result = await DisposeServerAndWaitForFinishOfDisposalAsync(node);
-                results.Add(result);
-            }
+            var result = await DisposeServerAndWaitForFinishOfDisposalAsync(node);
+
 
             var cts = new CancellationTokenSource();
             var mre = new ManualResetEvent(false);
             var t = worker.Run(async batch =>
             {
-                User user = null;
-                using (var session = batch.OpenAsyncSession())
-                {
-                    user = await session.LoadAsync<User>(id);
-                }
-
-                if (user != null)
-                {
-                    mre.Set();
-                }
+                mre.Set();
             }, cts.Token);
 
-            //revive nodes
-            await Task.Delay(2000);
-            foreach (var result in results)
+            //revive node
+            await Task.Delay(500);
+            var cs = new Dictionary<string, string>(DefaultClusterSettings);
+            cs[RavenConfiguration.GetKey(x => x.Core.ServerUrls)] = result.Url;
+            var revivedServer = GetNewServer(new ServerCreationOptions
             {
-                var cs = new Dictionary<string, string>(DefaultClusterSettings);
-                cs[RavenConfiguration.GetKey(x => x.Core.ServerUrls)] = result.Url;
-                var revivedServer = GetNewServer(new ServerCreationOptions
-                {
-                    DeletePrevious = false,
-                    RunInMemory = false,
-                    DataDirectory = result.DataDirectory,
-                    CustomSettings = cs
-                });
-            }
-            
+                DeletePrevious = false,
+                RunInMemory = false,
+                DataDirectory = result.DataDirectory,
+                CustomSettings = cs
+            });
             Assert.True(mre.WaitOne(TimeSpan.FromSeconds(15)), "User didn't loaded.");
         }
 
