88a02c9cefdbb8bb8ce790d1788a3ba1ec051fb4
shaharhikri <shaharhikri@ravendb.net>
2022-11-29 12:03:46 +0200

    RavenDB-19561 - Addressed PR Comments

--- test/SlowTests/Issues/RavenDB-19561.cs
+++ test/SlowTests/Issues/RavenDB-19561.cs
@@ -33,23 +33,19 @@ namespace SlowTests.Issues
         [Fact]
         public async Task Replicate_2_Docs_Which_Is_Bigger_Then_Batch_Size()
         {
-            var nodes = new List<RavenServer>();
-            for (int i = 0; i < 2; i++)
+            var co = new ServerCreationOptions
             {
-                var co = new ServerCreationOptions
+                CustomSettings = new Dictionary<string, string>
                 {
-                    CustomSettings = new Dictionary<string, string>
-                    {
-                        [RavenConfiguration.GetKey(x => x.Replication.MaxSizeToSend)] = 1.ToString()
-                    }
-                };
-                nodes.Add(GetNewServer(co));
-            }
-            using var store1 = GetDocumentStore(new Options { RunInMemory = false, Server = nodes[0], ReplicationFactor = 1 });
-            using var store2 = GetDocumentStore(new Options { RunInMemory = false, Server = nodes[1], ReplicationFactor = 1 });
+                    [RavenConfiguration.GetKey(x => x.Replication.MaxSizeToSend)] = 1.ToString()
+                }
+            };
+            var node = GetNewServer(co);
+
+            using var store1 = GetDocumentStore(new Options { RunInMemory = false, Server = node, ReplicationFactor = 1 });
+            using var store2 = GetDocumentStore(new Options { RunInMemory = false, Server = node, ReplicationFactor = 1 });
 
             var docs = new List<User>();
-            int k = 0;
             using (var session = store1.OpenAsyncSession())
             {
                 var doc = new User
@@ -82,7 +78,7 @@ namespace SlowTests.Issues
             var waitForReplicationTasks = new List<Task>();
             foreach (var doc in docs)
             {
-                waitForReplicationTasks.Add(WaitForDocToReplicateAsync<User>(store2, doc.Id));
+                waitForReplicationTasks.Add(WaitAndAssertDocReplicationAsync<User>(store2, doc.Id));
             }
             Task.WaitAll(waitForReplicationTasks.ToArray());
 
@@ -103,20 +99,10 @@ namespace SlowTests.Issues
         }
 
 
-        private async Task WaitForDocToReplicateAsync<T>(DocumentStore store, string id, int timeout = 15_000_000) where T : class
+        private async Task WaitAndAssertDocReplicationAsync<T>(DocumentStore store, string id, int timeout = 15_000_000) where T : class
         {
-            var sw = Stopwatch.StartNew();
-            while (sw.Elapsed.Seconds <= timeout / 1_000_000)
-            {
-                using (var session = store.OpenAsyncSession(store.Database))
-                {
-                    var doc = await session.LoadAsync<T>(id);
-                    if (doc != null)
-                        return;
-                }
-            }
-            sw.Stop();
-            Assert.True(false, $"doc \"{id}\" didn't replicated");
+            var result = await WaitForDocumentToReplicateAsync<User>(store, id, timeout);
+            Assert.True(result!=null, $"doc \"{id}\" didn't replicated.");
         }
 
         class User
