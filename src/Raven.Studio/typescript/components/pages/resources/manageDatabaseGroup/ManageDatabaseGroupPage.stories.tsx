35a5da4245e83afa919005cb2a544d781c199465
Marcin Lewandowski <marcin@ravendb.net>
2022-12-05 08:18:53 +0100

    WIP

---
+++ src/Raven.Studio/typescript/components/pages/resources/manageDatabaseGroup/ManageDatabaseGroupPage.stories.tsx
@@ -0,0 +1,49 @@
+﻿import { withBootstrap5, withStorybookContexts } from "../../../../test/storybookTestUtils";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ManageDatabaseGroupPage } from "components/pages/resources/manageDatabaseGroup/ManageDatabaseGroupPage";
+import { mockServices } from "../../../../test/mocks/MockServices";
+import React from "react";
+import accessManager from "common/shell/accessManager";
+import clusterTopologyManager from "common/shell/clusterTopologyManager";
+import { DatabasesStubs } from "../../../../test/stubs/DatabasesStubs";
+
+export default {
+    title: "Pages/Manage Database Group",
+    component: ManageDatabaseGroupPage,
+    decorators: [withStorybookContexts, withBootstrap5],
+} as ComponentMeta<typeof ManageDatabaseGroupPage>;
+
+function commonInit() {
+    accessManager.default.securityClearance("ClusterAdmin");
+    clusterTopologyManager.default.localNodeTag = ko.pureComputed(() => "A");
+}
+
+export const SingleNode: ComponentStory<typeof ManageDatabaseGroupPage> = () => {
+    commonInit();
+
+    const { databasesService } = mockServices;
+    databasesService.withGetDatabase_Single();
+
+    const db = DatabasesStubs.nonShardedSingleNodeDatabase();
+
+    return (
+        <div style={{ height: "100vh", overflow: "auto" }}>
+            <ManageDatabaseGroupPage db={db} />
+        </div>
+    );
+};
+
+export const Cluster: ComponentStory<typeof ManageDatabaseGroupPage> = () => {
+    commonInit();
+
+    const { databasesService } = mockServices;
+    databasesService.withGetDatabase_Cluster();
+
+    const db = DatabasesStubs.nonShardedClusterDatabase();
+
+    return (
+        <div style={{ height: "100vh", overflow: "auto" }}>
+            <ManageDatabaseGroupPage db={db} />
+        </div>
+    );
+};
---
+++ src/Raven.Studio/typescript/components/pages/resources/manageDatabaseGroup/NodeInfoComponent.tsx
@@ -0,0 +1,84 @@
+﻿import React from "react";
+import {
+    Alert,
+    Badge,
+    Card,
+    CardBody,
+    CardHeader,
+    DropdownItem,
+    DropdownMenu,
+    DropdownToggle,
+    UncontrolledDropdown,
+} from "reactstrap";
+import { NodeInfo } from "components/pages/resources/manageDatabaseGroup/types";
+
+interface NodeInfoComponentProps {
+    node: NodeInfo;
+}
+
+export function NodeInfoComponent(props: NodeInfoComponentProps) {
+    const { node } = props;
+    return (
+        <Card>
+            <CardHeader>
+                <Badge color={nodeBadgeColor(node)}>status: {nodeBadgeText(node)}</Badge>
+                Node: {node.tag}
+                <UncontrolledDropdown>
+                    <DropdownToggle color="danger" caret>
+                        <i className="icon-disconnected" />
+                        Delete from group
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem data-bind="click: _.partial($parent.deleteNodeFromGroup, $data, false)">
+                            <i className="icon-trash" />
+                            <span>Soft Delete</span>&nbsp;
+                            <small>(stop replication and keep database files on the node)</small>
+                        </DropdownItem>
+                        <DropdownItem data-bind="click: _.partial($parent.deleteNodeFromGroup, $data, true)">
+                            <i className="icon-alerts text-danger"></i> <span className="text-danger">Hard Delete</span>
+                            &nbsp;<small>(stop replication and remove database files on the node)</small>
+                        </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+            </CardHeader>
+            <CardBody></CardBody>
+        </Card>
+    );
+}
+
+function nodeBadgeColor(node: NodeInfo) {
+    switch (node.lastStatus) {
+        case "Ok":
+            return "success";
+        case "NotResponding":
+            return "danger";
+        default:
+            return "warning";
+    }
+}
+
+/* TODO
+ cssIcon = ko.pureComputed(() => {
+        const type = this.type();
+        switch (type) {
+            case "Member":
+                return "icon-dbgroup-member";
+            case "Promotable":
+                return "icon-dbgroup-promotable";
+            case "Rehab":
+                return "icon-dbgroup-rehab";
+        }
+        return "";
+    });
+ */
+
+function nodeBadgeText(node: NodeInfo) {
+    switch (node.lastStatus) {
+        case "Ok":
+            return "Active";
+        case "NotResponding":
+            return "Error";
+        default:
+            return "Catching up";
+    }
+}
