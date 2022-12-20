bc1d0dafe1a2cc9093545630f3ca7ff6fb1bcce1
Igal Merhavia <igal@ravendb.net>
2022-12-20 20:24:27 +0530

    RavenDB-19633 Add option to enable microsoft configuration

---
+++ src/Raven.Server/Utils/MicrosoftLogging/WebHostBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Raven.Server.Config.Categories;
+using Sparrow.Logging;
+
+namespace Raven.Server.Utils.MicrosoftLogging;
+
+public static class WebHostBuilderExtensions
+{
+    public static IWebHostBuilder ConfigureMicrosoftLogging(this IWebHostBuilder hostBuilder, ref LoggingSource loggingSource, LogsConfiguration configurationLogs)
+    {
+        if (configurationLogs.DisableMicrosoftLogs)
+            return hostBuilder;
+        
+        loggingSource = new LoggingSource(LogMode.Information, configurationLogs.MicrosoftLogPath.FullPath, "Microsoft Log", TimeSpan.MaxValue, long.MaxValue);
+        
+        var internalLoggingSource = loggingSource;
+        return hostBuilder.ConfigureLogging(logging =>
+        {
+            logging.ClearProviders();
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile(null, configurationLogs.MicrosoftLogConfigurationPath.FullPath, true, true);
+            logging.AddConfiguration(configurationBuilder.Build());
+            logging.SetMinimumLevel(LogLevel.Critical);
+        
+            logging.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, SparrowLoggingProvider>(serviceProvider => new SparrowLoggingProvider(internalLoggingSource)));
+        });
+    }
+}
+
+
