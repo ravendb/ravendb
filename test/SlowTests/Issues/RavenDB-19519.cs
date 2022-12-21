73e99b3f9546f3c7c4749c2ac0deb897b30926c3
Yonatan Resh <yonatan.resh@ravendb.net>
2022-12-21 16:28:35 +0200

    RavenDB-19519 Add schema upgrades progress to the init log

---
+++ test/SlowTests/Issues/RavenDB-19519.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading;
+using FastTests;
+using Raven.Client.Documents;
+using Raven.Client.Exceptions.Database;
+using Raven.Server.Config.Settings;
+using Raven.Tests.Core.Utils.Entities;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_19519 : RavenTestBase
+    {
+        public RavenDB_19519(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        public void SchemaUpgradeAddedToInitLog()
+        {
+            var folder = NewDataPath(forceCreateDir: true);
+            DoNotReuseServer();
+
+            var zipPath = new PathSetting("SchemaUpgrade/Issues/SystemVersion/Identities_CompareExchange_RavenData_from12.zip");
+            Assert.True(File.Exists(zipPath.FullPath));
+
+            ZipFile.ExtractToDirectory(zipPath.FullPath, folder);
+            using (var server = GetNewServer(new ServerCreationOptions
+                   {
+                       DeletePrevious = false,
+                       RunInMemory = false,
+                       DataDirectory = folder,
+                       RegisterForDisposal = false,
+                   }))
+            {
+                server.Configuration.Server.MaxTimeForTaskToWaitForDatabaseToLoad = new TimeSetting(100, TimeUnit.Milliseconds);
+
+                var sm = new SemaphoreSlim(initialCount: 0);
+
+                server.ServerStore.DatabasesLandlord.ForTestingPurposesOnly().AfterDatabaseInitialize = () =>
+                {
+                    sm.Wait();
+                };
+
+                try
+                {
+                    using (var store = new DocumentStore
+                           {
+                               Urls = new[] { server.WebUrl },
+                               Database = "a"
+                           })
+                    {
+                        store.Initialize();
+
+                        using (var session = store.OpenSession())
+                        {
+                            var exception = Assert.Throws<DatabaseLoadTimeoutException>(() => session.Store(new User { Name = "Foo" }));
+                            Assert.True(exception.Message.Contains("Schema Upgrade"));
+                        }
+                    }
+                }
+                finally
+                {
+                    sm.Release(2); // we have 2 databases
+                }
+            }
+        }
+    }
+}
