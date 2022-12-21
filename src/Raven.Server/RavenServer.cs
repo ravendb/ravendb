c7614fd6eb0fb843d1e30a940b8d9f00420c1841
Ayende Rahien <ayende@ayende.com>
2022-12-21 15:46:27 +0200

    RavenDB-19633 Provide a way to setup echo sockets that would listen to the loopback device and echo their contents. This is mean to provide assistance in debugging weird socket issues on production

--- src/Raven.Server/RavenServer.cs
+++ src/Raven.Server/RavenServer.cs
@@ -147,6 +147,9 @@ namespace Raven.Server
         public void Initialize()
         {
             var sp = Stopwatch.StartNew();
+            
+            EchoServer.StartEchoSockets(Configuration.Core.EchoSocketPort);
+            
             Certificate = LoadCertificateAtStartup() ?? new CertificateUtils.CertificateHolder();
 
             CpuUsageCalculator = string.IsNullOrEmpty(Configuration.Monitoring.CpuUsageMonitorExec)
@@ -2773,8 +2776,188 @@ namespace Raven.Server
             {
                 internal string[] RoutesToSkip = new string[] { };
             }
+        }
 
+        private static class EchoServer
+        {
+            
 
+        public static void StartEchoSockets(int? echoSocketPort)
+        {
+            if (echoSocketPort == null)
+                return;
+
+            int port = echoSocketPort.Value;
+            try
+            {
+                _ = EchoSocketAsync(port);
+            }
+            catch (Exception e)
+            {
+                if (Logger.IsInfoEnabled)
+                {
+                    Logger.Info("Failed to start async echo socket on " + port, e);
+                }
+            }
+
+            try
+            {
+                new Thread(EchoSocketSync)
+                {
+                    IsBackground = true, 
+                    Name = "Sync echo socket listener",
+                }.Start(port + 1);
+            }
+            catch (Exception e)
+            {
+                if (Logger.IsInfoEnabled)
+                {
+                    Logger.Info("Failed to start sync echo socket on " + port+ 1, e);
+                }
+            }
+        }
+
+        private static async Task EchoSocketAsync(int port)
+        {
+            TcpListener listener = null;
+            try
+            {
+                listener = new TcpListener(IPAddress.Loopback, port);
+                listener.Start();
+                while (true)
+                {
+                    var s = await listener.AcceptSocketAsync();
+                    _ = EchoAsync(s);
+                }
+            }
+            catch (Exception e)
+            {
+                if (Logger.IsInfoEnabled)
+                {
+                    Logger.Info("Error in listening to echo socket on " + port, e);
+                }
+            }
+            finally
+            {
+                try
+                {
+                    listener?.Stop();
+                }
+                catch 
+                {
+                    // nothing to do
+                }
+            }
+        }
+        private static void EchoSocketSync(object o)
+        {
+            TcpListener listener = null;
+            try
+            {
+                int port = (int)o;
+                listener = new TcpListener(IPAddress.Loopback, port);
+                listener.Start();
+                while (true)
+                {
+                    var s = listener.AcceptSocket();
+                    new Thread(EchoSync)
+                    {
+                        Name = "Sync echo socket thread for: " + s.RemoteEndPoint,
+                        IsBackground = true
+                    }.Start(s);
+                }
+            }
+            catch (Exception e)
+            {
+                if (Logger.IsInfoEnabled)
+                {
+                    Logger.Info("Error in listening to echo socket on " + o, e);
+                }
+            }
+            finally
+            {
+                try
+                {
+                    listener?.Stop();
+                }
+                catch 
+                {
+                    // nothing to do
+                }
+            }
+        }
+        
+        private static async Task EchoAsync(Socket s)
+        {
+            try
+            {
+                var buffer = new byte[1024];
+                while (true)
+                {
+                    var read = await s.ReceiveAsync(buffer);
+                    await s.SendAsync(new ReadOnlyMemory<byte>(buffer,0, read), SocketFlags.None);
+                }
+            }
+            catch (Exception e)
+            {
+                if (Logger.IsInfoEnabled)
+                {
+                    Logger.Info("Error in async echo socket", e);
+                }
+            }
+            finally
+            {
+                try
+                {
+                    s?.Dispose();
+                }
+                catch (Exception e)
+                {
+                    if (Logger.IsInfoEnabled)
+                    {
+                        Logger.Info("Error in disposing async echo socket", e);
+                    }
+                }
+            }
+        }
+
+        
+
+        private static void EchoSync(object o)
+        {
+            Socket s = null;
+            try
+            {
+                s = (Socket)o;
+                var buffer = new byte[1024];
+                while (true)
+                {
+                    var read = s.Receive(buffer);
+                    s.Send(buffer, 0, read, SocketFlags.None);
+                }
+            }
+            catch (Exception e)
+            {
+                if (Logger.IsInfoEnabled)
+                {
+                    Logger.Info("Error in sync echo socket", e);
+                }
+            }
+            finally
+            {
+                try
+                {
+                    s?.Dispose();
+                }
+                catch (Exception e)
+                {
+                    if (Logger.IsInfoEnabled)
+                    {
+                        Logger.Info("Error in disposing sync echo socket", e);
+                    }
+                }
+            }
+        }
         }
     }
 }
