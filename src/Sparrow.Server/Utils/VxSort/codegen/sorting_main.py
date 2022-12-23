2c98f0f4b539c91d685e1306ba1fc1dec4e65dbe
Federico Lois <federico.lois@gmail.com>
2022-12-23 17:42:36 -0300

    RavenDB-10709: Entry point generation without fallback

--- src/Sparrow.Server/Utils/VxSort/codegen/sorting_main.py
+++ src/Sparrow.Server/Utils/VxSort/codegen/sorting_main.py
@@ -1,3 +1,4 @@
+from utils import native_types, capital_type_map
 
 
 class MainSorting:
@@ -17,16 +18,56 @@ namespace {g.namespace}
 {{
     partial class Sort
     {{
-                
+        [SkipLocalsInit]
+        public unsafe static void Run<T>(T* left, T* right) where T : unmanaged
+        {{ 
         """
         print(s, file=f)
 
     def generate_public_api(self, f):
-        pass
+        g = self
+        s = ""
+        for t in native_types:
+            s += F"""
+            if (typeof(T) == typeof({t}))
+            {{
+                {t}* il = ({t}*)left;
+                {t}* ir = ({t}*)right;
+                uint length = (uint)(ir - il) + 1;
+                
+                Debug.Assert({g.get_configuration_constant(t, "Unroll")} >= 1);
+                Debug.Assert({g.get_configuration_constant(t, "Unroll")} <= 12);
+                
+                if (length < {g.get_configuration_constant(t, "SmallSortThresholdElements")})
+                {{
+                    BitonicSort.Sort(il, (int)length);
+                    return;
+                }}
+                
+                var depthLimit = 2 * FloorLog2PlusOne(length);
+                var buffer = stackalloc byte[{g.get_configuration_constant(t, "PartitionTempSizeInBytes")}];
+                var sorter = new Avx2VectorizedSort(
+                    il,
+                    ir,
+                    buffer,
+                    Avx2VectorizedSort.Int32Config.PartitionTempSizeInBytes
+                );
+                sorter.sort(il, ir, {t}.MinValue, {t}.MaxValue, {g.get_configuration_constant(t, "REALIGN_BOTH")}, depthLimit);
+                return;
+            }}            
+"""
+        s += F"""
+            throw new NotSupportedException($"The current type {{typeof(T).Name}} is not supported by this method.");"""
+
+        print(s, file=f)
 
     def generate_epilogue(self, f):
         s = F"""
+        }}
     }}
 }}
 """
         print(s, file=f)
+
+    def get_configuration_constant(self, type, param):
+        return F"Avx2VectorizedSort.{capital_type_map[type]}Config.{param}"
