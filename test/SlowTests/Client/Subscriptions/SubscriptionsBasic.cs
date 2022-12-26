8706c6644451adae1199fb91071f543e37566d30
egor <egor@ravendb.net>
2022-12-26 14:38:05 +0200

    RavenDB-19371: close sharded subscription on CloseWhenNoDocsLeft option

--- test/SlowTests/Client/Subscriptions/SubscriptionsBasic.cs
+++ test/SlowTests/Client/Subscriptions/SubscriptionsBasic.cs
@@ -33,6 +33,7 @@ using Raven.Tests.Core.Utils.Entities;
 using Sparrow;
 using Sparrow.Json;
 using Sparrow.Server;
+using Tests.Infrastructure;
 using Xunit;
 using Xunit.Abstractions;
 using DisposableAction = Voron.Util.DisposableAction;
@@ -1507,6 +1508,56 @@ namespace SlowTests.Client.Subscriptions
 
             var db = await Databases.GetDocumentDatabaseInstanceFor(server, store);
             await AssertWaitForExceptionAsync<KeyNotFoundException>(async () => await Task.Run(() => db.SubscriptionStorage.GetSubscriptionStateById(state.SubscriptionId)), interval: 1000);
+        }
+
+        [Fact]
+        public async Task AbortWhenNoDocsLeft2()
+        {
+            using (var store = Sharding.GetDocumentStore())
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    using (var session = store.OpenAsyncSession())
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            await session.StoreAsync(new User());
+                        }
+
+                        await session.SaveChangesAsync();
+                    }
+                }
+
+                var sn = await store.Subscriptions.CreateAsync<User>();
+                var worker = store.Subscriptions.GetSubscriptionWorker<User>(new SubscriptionWorkerOptions(sn)
+                {
+                    CloseWhenNoDocsLeft = true,
+                    TimeToWaitBeforeConnectionRetry = TimeSpan.FromSeconds(5),
+                    MaxDocsPerBatch = 16
+                });
+
+                var items = 0;
+                var st = worker.Run(x => { Interlocked.Add(ref items, x.NumberOfItemsInBatch); });
+                await Assert.ThrowsAsync<SubscriptionClosedException>(() => st.WaitWithoutExceptionAsync(_reasonableWaitTime));
+                Assert.Equal(1000, items);
+                await worker.DisposeAsync();
+
+                var worker2 = store.Subscriptions.GetSubscriptionWorker<User>(new SubscriptionWorkerOptions(sn)
+                {
+                    CloseWhenNoDocsLeft = true,
+                    TimeToWaitBeforeConnectionRetry = TimeSpan.FromSeconds(5),
+                    MaxDocsPerBatch = 16
+                });
+
+                var gotBatch = false;
+                var st2 = worker2.Run(x =>
+                {
+                    if (x.NumberOfItemsInBatch > 0)
+                        gotBatch = true;
+                });
+                await Assert.ThrowsAsync<SubscriptionClosedException>(() => st2.WaitWithoutExceptionAsync(_reasonableWaitTime));
+                Assert.False(gotBatch);
+            }
         }
 
         private class IdleDatabaseStatistics
