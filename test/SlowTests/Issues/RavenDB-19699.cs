b47ecf305accf3be1e8defad789eb2e48f380ac5
aviv <aviv@ayende.com>
2023-01-02 14:58:48 +0200

    RavenDB-19699 : when deleting a document that has timeseries create timeseries 'DeletedRange' items, in order to prevent possible inconsistent timeseries data between database nodes

--- test/SlowTests/Issues/RavenDB-19699.cs
+++ test/SlowTests/Issues/RavenDB-19699.cs
@@ -15,6 +15,73 @@ namespace SlowTests.Issues
         }
 
         [Fact]
+        public async Task ShouldCreateAndReplicateTimeSeriesDeletedRangeWhenDeletingDocument()
+        {
+            const string id = "users/1";
+            using (var store = GetDocumentStore())
+            using (var replica = GetDocumentStore())
+            {
+                var baseline = DateTime.Now;
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User
+                    {
+                        Name = "ayende"
+                    }, id);
+
+                    var tsf = session.TimeSeriesFor(id, "heartrate");
+                    tsf.Append(baseline, 1);
+                    tsf.Append(baseline.AddDays(1), 1);
+                    tsf.Append(baseline.AddDays(2), 1);
+
+                    await session.SaveChangesAsync();
+                }
+
+                await SetupReplicationAsync(store, replica);
+
+                Assert.True(WaitForDocument<User>(replica, id, u => u.Name == "ayende"));
+
+                using (var session = replica.OpenAsyncSession())
+                {
+                    var tsEntries = await session.TimeSeriesFor(id, "heartrate").GetAsync();
+                    Assert.Equal(3, tsEntries.Length);
+                }
+
+                var brokenReplication = await BreakReplication(Server.ServerStore, store.Database);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    session.Delete(id);
+                    await session.SaveChangesAsync();
+                }
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User
+                    {
+                        Name = "ayende2"
+                    }, id);
+
+                    session.TimeSeriesFor(id, "heartrate").Append(baseline.AddDays(3), 1);
+
+                    await session.SaveChangesAsync();
+                }
+
+                brokenReplication.Mend();
+
+                Assert.True(WaitForDocument<User>(replica, id, u => u.Name == "ayende2"));
+
+                using (var session = replica.OpenAsyncSession())
+                {
+                    var tsEntries = await session.TimeSeriesFor(id, "heartrate").GetAsync();
+                    Assert.Equal(1, tsEntries.Length);
+                }
+
+            }
+        }
+
+        [Fact]
         public async Task DeletingTimeSeriesAndModifyingItsDocumentShouldNotResultInConflictOnReplica()
         {
             const string id = "users/1";
