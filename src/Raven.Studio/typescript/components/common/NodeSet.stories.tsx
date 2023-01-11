209b839d06fcb38109e312a706dd736934c40e1f
kwiato <kwiato.r@gmail.com>
2023-01-11 15:11:54 +0100

    RavenDB-18009 Database list view - add shard data

---
+++ src/Raven.Studio/typescript/components/common/NodeSet.stories.tsx
@@ -0,0 +1,91 @@
+﻿import { ComponentMeta } from "@storybook/react";
+import { NodeSet, NodeSetLabel, NodeSetItem } from "./NodeSet";
+import React from "react";
+import { withBootstrap5, withStorybookContexts } from "test/storybookTestUtils";
+import { HrHeader } from "./HrHeader";
+
+export default {
+    title: "Bits/NodeSet",
+    component: NodeSet,
+    decorators: [withStorybookContexts, withBootstrap5],
+} as ComponentMeta<typeof NodeSet>;
+
+export function NodeSets() {
+    return (
+        <div>
+            <HrHeader>Basic</HrHeader>
+            <NodeSet className="m-1">
+                <NodeSetLabel color="primary" icon="database">
+                    Nordwind
+                </NodeSetLabel>
+                <NodeSetItem icon="node" color="node">
+                    A
+                </NodeSetItem>
+                <NodeSetItem icon="node" color="node">
+                    B
+                </NodeSetItem>
+            </NodeSet>
+            <HrHeader>Orchestrators</HrHeader>
+            <NodeSet color="warning" className="m-1">
+                <NodeSetLabel color="warning" icon="orchestrator">
+                    Orchestrators
+                </NodeSetLabel>
+                <NodeSetItem icon="zombie" color="danger">
+                    A
+                </NodeSetItem>
+                <NodeSetItem icon="node" color="node">
+                    B
+                </NodeSetItem>
+            </NodeSet>
+            <HrHeader>Shards</HrHeader>
+            <NodeSet color="shard" className="m-1">
+                <NodeSetLabel color="shard" icon="shard">
+                    #1
+                </NodeSetLabel>
+                <NodeSetItem icon="node" color="node">
+                    A
+                </NodeSetItem>
+                <NodeSetItem icon="node" color="node">
+                    B
+                </NodeSetItem>
+            </NodeSet>
+            <NodeSet color="shard" className="m-1">
+                <NodeSetLabel color="shard" icon="shard">
+                    #2
+                </NodeSetLabel>
+                <NodeSetItem icon="node" color="node">
+                    A
+                </NodeSetItem>
+                <NodeSetItem icon="node" color="node">
+                    B
+                </NodeSetItem>
+                <NodeSetItem icon="node" color="node">
+                    DEV
+                </NodeSetItem>
+            </NodeSet>
+            <HrHeader>Nodes</HrHeader>
+            <NodeSet color="node" className="m-1">
+                <NodeSetLabel color="node" icon="node">
+                    PROD
+                </NodeSetLabel>
+                <NodeSetItem icon="shard" color="shard">
+                    #1
+                </NodeSetItem>
+                <NodeSetItem icon="shard" color="shard">
+                    #2
+                </NodeSetItem>
+            </NodeSet>
+            <NodeSet color="node" className="m-1">
+                <NodeSetLabel color="node" icon="node">
+                    DEV
+                </NodeSetLabel>
+                <NodeSetItem icon="shard" color="shard">
+                    #2
+                </NodeSetItem>
+                <NodeSetItem icon="shard" color="shard">
+                    #3
+                </NodeSetItem>
+            </NodeSet>
+        </div>
+    );
+}
---
+++ src/Raven.Studio/typescript/components/common/NodeSet.tsx
@@ -0,0 +1,42 @@
+﻿import React, { ReactNode } from "react";
+import classNames from "classnames";
+
+interface NodeSetProps {
+    children?: ReactNode | ReactNode[];
+    className?: string;
+    color?: string;
+}
+
+export function NodeSet(props: NodeSetProps) {
+    const { children, className, color, ...rest } = props;
+
+    return <div className={classNames("node-set rounded-pill", "outline-" + color, className)}>{children}</div>;
+}
+
+interface NodeSetItemProps {
+    children?: ReactNode | ReactNode[];
+    icon?: string;
+    color?: string;
+}
+
+export function NodeSetLabel(props: NodeSetItemProps) {
+    const { children, icon, color } = props;
+
+    return (
+        <div className="node-set-label">
+            {icon && <i className={classNames("icon-" + icon, "text-" + color)} />}
+            <strong className="node-set-label-name">{children}</strong>
+        </div>
+    );
+}
+
+export function NodeSetItem(props: NodeSetItemProps) {
+    const { children, icon, color } = props;
+
+    return (
+        <div className="node-set-item">
+            {icon && <i className={classNames("icon-" + icon, "text-" + color)} />}
+            <strong className="node-set-item-name">{children}</strong>
+        </div>
+    );
+}
