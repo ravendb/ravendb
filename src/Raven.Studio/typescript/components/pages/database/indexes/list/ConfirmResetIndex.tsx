278e9f99595cb9ffea6411149449f5e0bb3c860c
Marcin Lewandowski <marcin@ravendb.net>
2023-01-13 14:50:57 +0100

    RavenDB-18689 Sharding - indexes view - enhacements - confirm reset index in bs5

---
+++ src/Raven.Studio/typescript/components/pages/database/indexes/list/ConfirmResetIndex.tsx
@@ -0,0 +1,47 @@
+﻿import React from "react";
+import { Alert, Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import { IndexSharedInfo } from "components/models/indexes";
+
+interface ConfirmResetIndexProps {
+    toggle: () => void;
+    onConfirm: () => void;
+    index: IndexSharedInfo;
+}
+
+export function ConfirmResetIndex(props: ConfirmResetIndexProps) {
+    const { toggle, index, onConfirm } = props;
+
+    const onSubmit = () => {
+        onConfirm();
+        toggle();
+    };
+
+    return (
+        <Modal isOpen toggle={toggle} wrapClassName="bs5">
+            <ModalHeader toggle={toggle}>Reset index?</ModalHeader>
+            <ModalBody>
+                You&apos;re resetting index: <br />
+                <ul>
+                    <li>
+                        <strong>{index.name}</strong>
+                    </li>
+                </ul>
+                <Alert color="warning">
+                    <small>
+                        Clicking <strong>Reset</strong> will remove all existing indexed data.
+                    </small>
+                    <br />
+                    <small>All items matched by the index definition will be re-indexed.</small>
+                </Alert>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="secondary" onClick={toggle}>
+                    Cancel
+                </Button>
+                <Button color="danger" onClick={onSubmit}>
+                    Reset
+                </Button>
+            </ModalFooter>
+        </Modal>
+    );
+}
