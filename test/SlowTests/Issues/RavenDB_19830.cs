0ea4b4ec5af703f287bb972c6aaadee079b64e6d
Pawel Pekrol <ppekrol@ravendb.net>
2023-01-17 11:52:52 +0100

    RavenDB-19830 cleanup

--- test/SlowTests/Issues/RavenDB_19830.cs
+++ test/SlowTests/Issues/RavenDB_19830.cs
@@ -6,19 +6,22 @@ using Xunit.Abstractions;
 
 namespace SlowTests.Issues;
 
-public class GH_15633 : RavenTestBase
+public class RavenDB_19830 : RavenTestBase
 {
-    public GH_15633(ITestOutputHelper output) : base(output)
+    public RavenDB_19830(ITestOutputHelper output) : base(output)
     {
     }
-    
+
     [Fact]
-    public async Task CanProjectWhenFieldAndPropertyNameAreTheSameWithDifferentCasing() {
+    public async Task CanProjectWhenFieldAndPropertyNameAreTheSameWithDifferentCasing()
+    {
         const string id = "testdocument/1";
         using var store = GetDocumentStore();
 
-        using (var session = store.OpenAsyncSession()) {
-            var testDoc = new DocWithLambdaProperties {
+        using (var session = store.OpenAsyncSession())
+        {
+            var testDoc = new DocWithLambdaProperties
+            {
                 Id = id,
                 Str = "hello"
             };
@@ -27,33 +30,32 @@ public class GH_15633 : RavenTestBase
             await session.SaveChangesAsync();
         }
 
-        using (var session = store.OpenAsyncSession()) {
+        using (var session = store.OpenAsyncSession())
+        {
             var testDocs =
                 await session.Query<DocWithLambdaProperties>()
                     .ToArrayAsync();
             Assert.Equal("hello", testDocs[0].Str);   // <-- SUCCEEDS
         }
 
-        using (var session = store.OpenAsyncSession()) {
+        using (var session = store.OpenAsyncSession())
+        {
             var testDocs =
                 await session.Query<DocWithLambdaProperties>()
                     .ProjectInto<DocWithLambdaProperties>()
                     .ToArrayAsync();
-            Assert.Equal("hello", testDocs[0].Str);   
-        }
-    }
-    
-    public class DocWithLambdaProperties {
-        private string str;
-        public string Id { get; set; }
-        public string Str {
-            get => str;
-            set => str = value;
+            Assert.Equal("hello", testDocs[0].Str);
         }
     }
 
-    public class DocWithPlainProperties {
+    private class DocWithLambdaProperties
+    {
+        private string str;
         public string Id { get; set; }
-        public string Str { get; set; }
+        public string Str
+        {
+            get => str;
+            set => str = value;
+        }
     }
 }
