f52c76e931f82f24483682ca70726e338181da9c
Grisha Kotler <grisha@ayende.com>
2023-01-17 15:53:18 +0200

    RavenDB-19842 - move to LuceneCleaner

---
+++ src/Raven.Server/Indexing/LuceneCleaner.cs
@@ -0,0 +1,21 @@
+﻿using Lucene.Net.Search;
+using Sparrow.LowMemory;
+
+namespace Raven.Server.Indexing;
+
+public class LuceneCleaner : ILowMemoryHandler
+{
+    public LuceneCleaner()
+    {
+        LowMemoryNotification.Instance.RegisterLowMemoryHandler(this);
+    }
+
+    public void LowMemory(LowMemorySeverity lowMemorySeverity)
+    {
+        FieldCache_Fields.DEFAULT.PurgeAllCaches();
+    }
+
+    public void LowMemoryOver()
+    {
+    }
+}
