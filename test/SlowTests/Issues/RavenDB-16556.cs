6b710c165de462ae3e8df67016ca1735c8d18a09
Michał Leśniak <michal.lesniak@ravendb.net>
2023-01-17 11:00:04 +0100

    RavenDB-16556 Added unit test to check if incorrect number of args is thrown for count()

--- test/SlowTests/Issues/RavenDB-16556.cs
+++ test/SlowTests/Issues/RavenDB-16556.cs
@@ -21,13 +21,13 @@ public class RavenDB_16556 : RavenTestBase
                                         select 
                                         {
                                             Key: key(),
-                                            Value: sum(""Price"")
+                                            Value: sum(x => x.Price)
                                         }")]
     [InlineData(@"declare function 
                     output(o){
                         return {
 		                    Key: key(),
-		                    Value: sum(""Price"")
+		                    Value: sum(x => x.Price)
                         }
                     }
 
@@ -203,7 +203,7 @@ public class RavenDB_16556 : RavenTestBase
                 order by count() as long desc
                 load o.EmpId as e
                 select output(o)")]
-    public void CheckIfInvalidQueryExceptionIsThrownForIncorrectNumberOfArgsInSum(string query)
+    public void CheckIfInvalidOperationExceptionIsThrownForIncorrectNumberOfArgsInSum(string query)
     {
         using var store = GetDocumentStore();
 
@@ -239,6 +239,82 @@ public class RavenDB_16556 : RavenTestBase
             });
             
             Assert.Contains("sum(field) must be called with a single argument", ex.Message);
+        }
+    }
+    
+    [RavenTheory(RavenTestCategory.Querying)]
+    [InlineData(@"from Orders as o
+                                        group by o.CompId
+                                        order by count() as long desc
+                                        load o.EmpId as e
+                                        select 
+                                        {
+                                            Key: key(),
+                                            Value: count(2)
+                                        }")]
+    [InlineData(@"declare function 
+                    output(o){
+                        return {
+		                    Key: key(),
+		                    Value: count(x => x.Freight)
+                        }
+                    }
+
+                from Orders as o
+                group by o.CompId
+                order by count() as long desc
+                load o.EmpId as e
+                select output(o)")]
+    [InlineData(@"declare function 
+                    output(o){
+                    var empId = key().EmpId;
+                        return {
+		                    Key: empId,
+		                    Value: count(""Freight"")
+                        }
+                    }
+
+                from Orders as o
+                group by o.CompId
+                order by count() as long desc
+                load o.EmpId as e
+                select output(o)")]
+    public void CheckIfInvalidOperationExceptionIsThrownForIncorrectNumberOfArgsInCount(string query)
+    {
+        using var store = GetDocumentStore();
+
+        using (var session = store.OpenSession())
+        {
+            Employee e1 = new() { Name = "Maciej" };
+            Employee e2 = new() { Name = "Michal" };
+
+            Company c1 = new() { Name = "Company1" };
+            Company c2 = new() { Name = "Company2" };
+
+            session.Store(e1);
+            session.Store(e2);
+
+            session.Store(c1);
+            session.Store(c2);
+
+            Order o1 = new() { EmpId = e1.Id, CompId = c1.Id, Price = 21 };
+            Order o2 = new() { EmpId = e2.Id, CompId = c2.Id, Price = 37 };
+            Order o3 = new() { EmpId = e1.Id, CompId = c2.Id, Price = 44 };
+
+            session.Store(o1);
+            session.Store(o2);
+            session.Store(o3);
+
+            session.SaveChanges();
+            
+            RavenException ex = Assert.Throws<RavenException>(() =>
+            {
+                var res = session.Advanced
+                    .RawQuery<Dto>(query)
+                    .WaitForNonStaleResults().ToList();
+            });
+            
+            Assert.Contains("count() must be called without arguments", ex.Message);
         }
     }
 
