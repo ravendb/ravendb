a6d98637de69aa37736051899a390988d7d082f6
aviv <aviv@ayende.com>
2023-01-22 10:19:38 +0200

    RavenDB-19621 : address PR comments

--- src/Raven.Server/Documents/Indexes/Sharding/ShardedIndexStore.cs
+++ src/Raven.Server/Documents/Indexes/Sharding/ShardedIndexStore.cs
@@ -1,12 +1,12 @@
-﻿using Raven.Server.ServerWide;
+﻿using Raven.Server.Documents.Sharding;
+using Raven.Server.ServerWide;
 
 namespace Raven.Server.Documents.Indexes.Sharding
 {
     public class ShardedIndexStore : IndexStore
     {
-        public ShardedIndexStore(DocumentDatabase database, ServerStore serverStore) : base(database)
+        public ShardedIndexStore(ShardedDocumentDatabase database, ServerStore serverStore) : base(database, serverStore)
         {
-            ServerStore = serverStore;
             Create = new ShardedDatabaseIndexCreateController(database);
         }
     }
