fd116da34e85a3271705eab1a2f32c3fb36aceff
shaharhikri <shaharhikri@ravendb.net>
2023-01-26 15:43:29 +0200

    RavenDB-19887 - Cluster-wide transactions on session throws ClusterTransactionConcurrencyException after failover

---
+++ test/SlowTests/Issues/RavenDB-19887.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Operations.Configuration;
+using Raven.Client.Documents.Session;
+using Raven.Client.Http;
+using Raven.Client.ServerWide.Operations;
+using Raven.Client.ServerWide.Operations.Configuration;
+using Raven.Tests.Core.Utils.Entities;
+using SlowTests.Core.AdminConsole;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_19887 : ClusterTestBase
+    {
+        public RavenDB_19887(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task ClusterWideTransactions2Test()
+        {
+            var (nodes, leader) = await CreateRaftCluster(3);
+            using var store = GetDocumentStore(new Options
+            {
+                Server = leader, 
+                ReplicationFactor = nodes.Count,
+                RunInMemory = false
+            });
+
+            using (var session = store.OpenSession(new SessionOptions { TransactionMode = TransactionMode.ClusterWide }))
+            {
+                var doc = new Doc { Id = "doc-1", NumVal = 1 };
+                session.Store(doc);
+                session.SaveChanges();
+            }
+
+            Assert.True(WaitForChangeVectorInCluster(nodes, store.Database));
+
+            using (var session = store.OpenSession(new SessionOptions { TransactionMode = TransactionMode.ClusterWide }))
+            {
+                var doc1 = session.Load<Doc>("doc-1");
+                doc1.NumVal++;
+            
+                // if the node used for document loading is brought down here, the session.SaveChanges() below fails:
+                // Raven.Client.Exceptions.ClusterTransactionConcurrencyException:
+                // 'Failed to execute cluster transaction due to the following issues: Guard compare exchange value
+                // 'rvn-atomic/doc-1' index does not match the transaction index's 0 change vector on doc-1
+                // Concurrency check failed for putting the key 'rvn-atomic/doc-1'.Requested index: 0, actual index: 342'
+                var responsibleNodeTag = store.GetRequestExecutor(store.Database).Topology.Nodes[0].ClusterTag;
+                var responsibleNode = nodes.Single(n => n.ServerStore.NodeTag == responsibleNodeTag);
+                var result0 = await DisposeServerAndWaitForFinishOfDisposalAsync(responsibleNode);
+            
+                session.SaveChanges();
+            }
+
+            using (var session = store.OpenAsyncSession(new SessionOptions { TransactionMode = TransactionMode.ClusterWide }))
+            {
+                var doc1 = await session.LoadAsync<Doc>("doc-1");
+                Assert.Equal(doc1.NumVal, 2);
+            }
+        }
+
+        public class Doc
+        {
+            public string Id { get; set; }
+            public int NumVal { get; set; }
+        }
+    }
+}
