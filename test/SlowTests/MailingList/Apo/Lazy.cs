9f6c530ee16ce8ad457ec549c4ed4cdbb4e9fe5d
Federico Lois <federico.lois@gmail.com>
2023-01-31 13:45:19 -0300

    RavenDB-19706: Fixes count condition discrepancies between Corax and Lucene.

--- test/SlowTests/MailingList/Apo/Lazy.cs
+++ test/SlowTests/MailingList/Apo/Lazy.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using FastTests;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Linq;
+using Tests.Infrastructure;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -25,45 +26,49 @@ namespace SlowTests.MailingList.Apo
             public DateTime Date { get; set; }
         }
 
-        [Fact]
-        public void LazyWhereAndOrderBy()
+        [RavenTheory(RavenTestCategory.Querying)]
+        [RavenData(SearchEngineMode = RavenSearchEngineMode.Lucene)]
+        [RavenData(SearchEngineMode = RavenSearchEngineMode.Corax)]
+        public void LazyWhereAndOrderBy(Options options)
         {
-            using (var store = GetDocumentStore())
+            using var store = GetDocumentStore(options);
+            
+            using (var session = store.OpenSession())
             {
-                using (var session = store.OpenSession())
-                {
-                    session.Store(new TestClass() { Id = "testid", Value = "test1", Date = DateTime.UtcNow });
-                    session.Store(new TestClass() { Value = "test2", Date = DateTime.UtcNow });
-                    session.Store(new TestClass() { Value = "test3", Date = DateTime.UtcNow.AddMinutes(1) });
-                    session.SaveChanges();
-                }
-
-                using (var session = store.OpenSession())
-                {
-                    var hello = new List<TestClass>();
-
-                    // should not throw
-                    session.Query<TestClass>()
-                            .Customize(x => x.WaitForNonStaleResults())
-                            .Where(x => x.Date >= DateTime.UtcNow.AddMinutes(-1))
-                            .OrderByDescending(x => x.Date)
-                            .Lazily(result =>
-                            {
-                                hello = result.ToList();
-                            });
-                }
+                session.Store(new TestClass() { Id = "testid", Value = "test1", Date = DateTime.UtcNow });
+                session.Store(new TestClass() { Value = "test2", Date = DateTime.UtcNow });
+                session.Store(new TestClass() { Value = "test3", Date = DateTime.UtcNow.AddMinutes(1) });
+                session.SaveChanges();
             }
+
+            using (var session = store.OpenSession())
+            {
+                var hello = new List<TestClass>();
+
+                // should not throw
+                session.Query<TestClass>()
+                        .Customize(x => x.WaitForNonStaleResults())
+                        .Where(x => x.Date >= DateTime.UtcNow.AddMinutes(-1))
+                        .OrderByDescending(x => x.Date)
+                        .Lazily(result =>
+                        {
+                            hello = result.ToList();
+                        });
+            }
+            
         }
 
-        [Fact]
-        public async Task LazyQuery_WhenDefineCallBack_ShouldExecuteAsItIsInRegularQuery()
+        [RavenTheory(RavenTestCategory.Querying)]
+        [RavenData(SearchEngineMode = RavenSearchEngineMode.Lucene)]
+        [RavenData(SearchEngineMode = RavenSearchEngineMode.Corax)]
+        public async Task LazyQuery_WhenDefineCallBack_ShouldExecuteAsItIsInRegularQuery(Options options)
         {
             var regularQuery = new List<string>();
             var lazyQuery = new List<string>();
             var asyncLazyQuery = new List<string>();
             List<string> current = null;
 
-            using var store = GetDocumentStore();
+            using var store = GetDocumentStore(options);
             store.OnBeforeQuery += (s, e) =>
             {
                 current.Add("SessionOnBeforeQuery");
@@ -102,16 +107,18 @@ namespace SlowTests.MailingList.Apo
                 return query.Customize(x => x.BeforeQueryExecuted(_ => current.Add("QueryOnBeforeQuery")));
             }
         }
-        
-        [Fact]
-        public async Task CountLazily_WhenDefineCallBack_ShouldExecuteAsItIsInRegularQuery()
+
+        [RavenTheory(RavenTestCategory.Querying)]
+        [RavenData(SearchEngineMode = RavenSearchEngineMode.Lucene)]
+        [RavenData(SearchEngineMode = RavenSearchEngineMode.Corax)]
+        public async Task CountLazily_WhenDefineCallBack_ShouldExecuteAsItIsInRegularQuery(Options options)
         {
             var regularQuery = new List<string>();
             var lazyQuery = new List<string>();
             var asyncLazyQuery = new List<string>();
             List<string> current = null;
 
-            using var store = GetDocumentStore();
+            using var store = GetDocumentStore(options);
             store.OnBeforeQuery += (s, e) =>
             {
                 current.Add("SessionOnBeforeQuery");
