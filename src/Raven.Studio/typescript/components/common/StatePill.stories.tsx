129cab5fcfc3f65e46ee095cfcba3a225dc481f6
kwiato <kwiato.r@gmail.com>
2023-02-02 16:32:07 +0100

    RavenDB-19674 Sharding - Studio - Improve sharded tag

--- src/Raven.Studio/typescript/components/common/StatePill.stories.tsx
+++ src/Raven.Studio/typescript/components/common/StatePill.stories.tsx
@@ -2,18 +2,70 @@
 import React from "react";
 import { StatePill } from "./StatePill";
 import { boundCopy } from "../utils/common";
+import { Spinner } from "reactstrap";
+import { withBootstrap5, withStorybookContexts } from "test/storybookTestUtils";
 
 export default {
     title: "Bits/State Pills",
+    decorators: [withStorybookContexts, withBootstrap5],
     component: StatePill,
 } as ComponentMeta<typeof StatePill>;
 
 const Template = () => {
     return (
         <div>
-            <StatePill color="success">⭐ Success!</StatePill>
-            <StatePill color="warning">‼️ Warning</StatePill>
-            <StatePill color="danger">⚡ Danger </StatePill>
+            <h1>State Pills and Badges</h1>
+
+            <hr />
+            <h3>Brand</h3>
+
+            <div className="hstack gap-2">
+                <StatePill>
+                    <i className="icon-coffee" /> Neutral
+                </StatePill>
+                <StatePill color="primary">
+                    <i className="icon-raven" /> Primary
+                </StatePill>
+            </div>
+
+            <hr />
+            <h3>Function</h3>
+
+            <div className="hstack gap-2">
+                <StatePill color="secondary">
+                    <i className="icon-zombie" /> Secondary
+                </StatePill>
+                <StatePill color="success">
+                    <i className="icon-check" /> Success!
+                </StatePill>
+                <StatePill color="warning">
+                    <i className="icon-warning" /> Warning
+                </StatePill>
+                <StatePill color="danger">
+                    <i className="icon-danger" /> Danger
+                </StatePill>
+                <StatePill color="info">
+                    <i className="icon-help" /> Info
+                </StatePill>
+            </div>
+
+            <hr />
+            <h3>Studio</h3>
+
+            <div className="hstack gap-2">
+                <StatePill color="shard">
+                    <i className="icon-shard" /> Shard
+                </StatePill>
+                <StatePill color="node">
+                    <i className="icon-node" /> Node
+                </StatePill>
+                <StatePill color="orchestrator">
+                    <i className="icon-orchestrator" /> Orchestrator
+                </StatePill>
+                <StatePill color="progress">
+                    <Spinner size="xs" /> Progress
+                </StatePill>
+            </div>
         </div>
     );
 };
