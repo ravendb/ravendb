2977b958d0b9cfecfd62ea5b2f0058a737629ece
Federico Lois <federico.lois@gmail.com>
2023-02-10 13:23:40 -0300

    RavenDB-19937: Fixed invalid decoding when long numbers are stored.

---
+++ test/FastTests/Voron/CompactTrees/RavenDB-19703.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FastTests.Voron;
+using SharpCompress.Compressors;
+using SharpCompress.Compressors.Deflate;
+using Tests.Infrastructure;
+using Voron.Data.CompactTrees;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FastTests.Voron.CompactTrees;
+
+public class RavenDB_19703 : CompactTreeReplayTest
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public RavenDB_19703(ITestOutputHelper output, ITestOutputHelper testOutputHelper) : base(output)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [RavenTheory(RavenTestCategory.Voron)]
+    [InlineData("RavenDB-19703.replay")]
+    public unsafe void AddAndRemoveValues(string filename)
+    {
+        foreach (var terms in ReadTermsFromResource(filename))
+        {
+            using var wtx = Env.WriteTransaction();
+            CompactTree tree = wtx.CompactTreeFor($"{filename}");
+            foreach (var term in terms)
+            {
+                var action = term[0];
+
+                string key;
+                switch (action)
+                {
+                    case '+':
+                        int pipeIndex = term.LastIndexOf('|');
+                        key = term[1..pipeIndex];
+                        var value = long.Parse(term[(pipeIndex + 1)..]);
+                        tree.Add(key, value);
+                        Assert.True(tree.TryGetValue(key, out var foundId));
+                        Assert.Equal(value, foundId);
+                        break;
+                    case '-':
+                        key = term[1..];
+                        tree.TryRemove(key, out var old);
+                        break;
+                }
+            }
+            wtx.Commit();
+        }
+
+        {
+            using var rtx = Env.ReadTransaction();
+
+            CompactTree tree = rtx.CompactTreeFor($"{filename}");
+
+            tree.Verify();
+            tree.VerifyOrderOfElements();
+            foreach (long page in tree.AllPages())
+            {
+                var state = new CompactTree.CursorState { Page = rtx.LowLevelTransaction.GetPage(page), };
+                Assert.Equal(state.ComputeFreeSpace(), state.Header->FreeSpace);
+            }
+        }
+    }
+}
---
+++ test/SlowTests/Corax/RavenDB-19703.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.Documents.Commands;
+using Raven.Client.Documents.Indexes;
+using Sparrow;
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Corax
+{
+    public class RavenDB_19703 : RavenTestBase
+    {
+        public RavenDB_19703(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        private record Person(string Name);
+
+        [RavenTheory(RavenTestCategory.Indexes)]
+        [RavenData(SearchEngineMode = RavenSearchEngineMode.All)]
+        public void CanStoreNullValueInsideAutoIndex(Options options)
+        {
+            using var store = GetDocumentStore(options);
+            var person = new Person(Encodings.Utf8.GetString(new byte[] { (byte)'m', (byte)'a', (byte)'c', (byte)'\0', (byte)'e', (byte)'\0' }));
+            using (var session = store.OpenSession())
+            {
+                session.Store(person);
+                session.SaveChanges();
+
+                var result = session.Advanced
+                    .DocumentQuery<Person>()
+                    .WaitForNonStaleResults()
+                    .WhereEndsWith(i => i.Name, Encodings.Utf8.GetString(new byte[] { (byte)'\0' }))
+                    .ToList();
+                WaitForUserToContinueTheTest(store);
+                Assert.Equal(1, result.Count);
+                Assert.Equal(person.Name, result[0].Name);
+            }
+        }
+
+        [RavenTheory(RavenTestCategory.Indexes)]
+        [RavenData(SearchEngineMode = RavenSearchEngineMode.All)]
+        public void FindOnRawDataWithNulls(Options options)
+        {
+            var rnd = new Random(1337);
+            using var store = GetDocumentStore(options);
+            {
+                using var bulkInsert = store.BulkInsert();
+
+                for (int i = 0; i < 1000; i++)
+                {
+                    byte[] data = new byte[rnd.Next(128) + 4];
+                    rnd.NextBytes(data);
+                    data[rnd.Next(data.Length - 1)] = 0;
+
+                    bulkInsert.Store(new Item { Name = $"TestFirst{i}", Data = data});
+                }
+            }
+            new NestedArray().Execute(store);
+            Indexes.WaitForIndexing(store);
+
+            using var session = store.OpenSession();
+            var result = session.Query<Item, NestedArray>().Where(p => p.Data.Any(data => data == 0)).ToList();
+            Assert.Equal(1000, result.Count);
+            result = session.Query<Item, NestedArray>().Where(p => p.Data.Any(data => data == 0)).ToList();
+            WaitForUserToContinueTheTest(store);
+            Assert.Equal(1000, result.Count);
+        }
+
+        private class Item
+        {
+            public string Name { get; set; }
+            public byte[] Data { get; set; }
+        }
+
+        private class NestedArray : AbstractIndexCreationTask<Item>
+        {
+            public NestedArray()
+            {
+                Map = docs => from doc in docs
+                                           select new Item { Name = doc.Name, Data = doc.Data };
+            }
+        }
+    }
+}
+
