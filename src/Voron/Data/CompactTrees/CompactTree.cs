1948ee11fa76ee1d31c315eb0270e69bbdfe7167
Federico Lois <federico.lois@gmail.com>
2023-02-10 16:03:16 -0300

    RavenDB-19703: Search fails when keys are too similar.

--- src/Voron/Data/CompactTrees/CompactTree.cs
+++ src/Voron/Data/CompactTrees/CompactTree.cs
@@ -2037,21 +2037,17 @@ namespace Voron.Data.CompactTrees
                 encodedKeyLength = GetEncodedKeyPtr(state.Page, @base[bot + mid], out encodedKeyPtr);
 
                 match = AdvMemory.CompareInline(keyPtr, encodedKeyPtr, Math.Min(keyLength, encodedKeyLength));
+                match = match == 0 ? keyLength - encodedKeyLength : match;
+
                 if (match >= 0)
-                {
                     bot += mid;
-                    if (match == 0)
-                        goto IsMatch;
-                }
 
                 top -= mid;
             }
 
             encodedKeyLength = GetEncodedKeyPtr(state.Page, @base[bot], out encodedKeyPtr);
-            
-            match = AdvMemory.CompareInline(keyPtr, encodedKeyPtr, Math.Min(keyLength, encodedKeyLength));
 
-            IsMatch:
+            match = AdvMemory.CompareInline(keyPtr, encodedKeyPtr, Math.Min(keyLength, encodedKeyLength));
             match = match == 0 ? keyLength - encodedKeyLength : match;
             if (match == 0)
             {
