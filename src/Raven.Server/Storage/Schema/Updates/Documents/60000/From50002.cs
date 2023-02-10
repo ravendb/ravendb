00f10cfda348cc4cd3a62c833de5000890bf32d2
Pawel Pekrol <ppekrol@ravendb.net>
2023-02-10 12:00:06 +0100

    RavenDB-19722 bump schemas for Documents, Indexes and Server

---
+++ src/Raven.Server/Storage/Schema/Updates/Documents/60000/From50002.cs
@@ -0,0 +1,14 @@
+﻿namespace Raven.Server.Storage.Schema.Updates.Documents
+{
+    public class From50002 : ISchemaUpdate
+    {
+        public int From => 50_002;
+        public int To => 60_000;
+        public SchemaUpgrader.StorageType StorageType => SchemaUpgrader.StorageType.Documents;
+
+        public bool Update(UpdateStep step)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/Raven.Server/Storage/Schema/Updates/LuceneIndex/60000/From54000.cs
@@ -0,0 +1,14 @@
+﻿namespace Raven.Server.Storage.Schema.Updates.LuceneIndex
+{
+    public unsafe class From54000 : ISchemaUpdate
+    {
+        public int From => 54_000;
+        public int To => 60_000;
+        public SchemaUpgrader.StorageType StorageType => SchemaUpgrader.StorageType.LuceneIndex;
+
+        public bool Update(UpdateStep step)
+        {
+            return true;
+        }
+    }
+}
