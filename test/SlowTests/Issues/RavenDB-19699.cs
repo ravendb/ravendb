220272e1a046f7ee2457c3d39cf001a7ec83758d
aviv <aviv@ayende.com>
2023-02-12 13:08:13 +0200

    RavenDB-19699 address PR comments : test more edge cases

--- test/SlowTests/Issues/RavenDB-19699.cs
+++ test/SlowTests/Issues/RavenDB-19699.cs
@@ -130,5 +130,107 @@ namespace SlowTests.Issues
                 }
             }
         }
+
+        [Fact]
+        public async Task ConflictBetweenDocWithTsAndDeletion_WhenDeletionWins()
+        {
+            const string id = "users/1";
+            using (var store = GetDocumentStore())
+            using (var replica = GetDocumentStore())
+            {
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User
+                    {
+                        Name = "ayende"
+                    }, id);
+
+                    await session.SaveChangesAsync();
+                }
+
+                await SetupReplicationAsync(store, replica);
+
+                Assert.True(WaitForDocument<User>(replica, id, u => u.Name == "ayende"));
+
+                var brokenReplication = await BreakReplication(Server.ServerStore, store.Database);
+
+                using (var session = replica.OpenAsyncSession())
+                {
+                    session.TimeSeriesFor(id, "heartrate").Append(DateTime.UtcNow, 1);
+                    await session.SaveChangesAsync();
+                }
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    session.Delete(id);
+                    await session.SaveChangesAsync();
+                }
+
+                brokenReplication.Mend();
+
+                Assert.True(WaitForDocumentDeletion(replica, id, 5000));
+
+                using (var session = replica.OpenAsyncSession())
+                {
+                    var ts = await session.TimeSeriesFor(id, "heartrate").GetAsync();
+                    Assert.Null(ts);
+                }
+
+            }
+        }
+
+        [Fact]
+        public async Task ConflictBetweenDocWithAndDocWithoutTs_WhenDocWithoutTsWins()
+        {
+            const string id = "users/1";
+            using (var store = GetDocumentStore())
+            using (var replica = GetDocumentStore())
+            {
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User
+                    {
+                        Name = "ayende"
+                    }, id);
+
+                    await session.SaveChangesAsync();
+                }
+
+                await SetupReplicationAsync(store, replica);
+
+                Assert.True(WaitForDocument<User>(replica, id, u => u.Name == "ayende"));
+
+                var brokenReplication = await BreakReplication(Server.ServerStore, store.Database);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    session.TimeSeriesFor(id, "heartrate").Append(DateTime.UtcNow, 1);
+                    await session.SaveChangesAsync();
+                }
+
+                using (var session = replica.OpenAsyncSession())
+                {
+                    var doc = await session.LoadAsync<User>(id);
+                    doc.Name = "ayende2";
+
+                    await session.SaveChangesAsync();
+                }
+
+                brokenReplication.Mend();
+
+                await AssertWaitForNotNullAsync(async () =>
+                {
+                    using var session = replica.OpenAsyncSession();
+                    return await session.TimeSeriesFor(id, "heartrate").GetAsync();
+                });
+
+                using (var session = replica.OpenAsyncSession())
+                {
+                    var doc = await session.LoadAsync<User>(id);
+                    Assert.Equal("ayende2", doc.Name);
+                }
+
+            }
+        }
     }
 }
