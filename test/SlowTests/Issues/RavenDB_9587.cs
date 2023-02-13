55453a3c0d74a4f4efad03b92b460349bfc7d2e2
Pawel Pekrol <ppekrol@ravendb.net>
2023-02-13 16:46:47 +0100

    RavenDB-18419 better test

--- test/SlowTests/Issues/RavenDB_9587.cs
+++ test/SlowTests/Issues/RavenDB_9587.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using FastTests;
 using Orders;
+using Raven.Client.Documents;
 using Raven.Client.Documents.Queries.Timings;
 using Tests.Infrastructure;
 using Xunit;
@@ -15,7 +16,7 @@ namespace SlowTests.Issues
         }
 
         [RavenTheory(RavenTestCategory.Querying)]
-        [RavenData(DatabaseMode = RavenDatabaseMode.Sharded)]
+        [RavenData(DatabaseMode = RavenDatabaseMode.All)]
         public void TimingsShouldWork(Options options)
         {
             using (var store = GetDocumentStore(options))
@@ -28,11 +29,21 @@ namespace SlowTests.Issues
                     session.SaveChanges();
                 }
 
+                AssertTimings(store);
+                AssertTimings(store); // there was a bug when building IndexQueryServerSide and QueryMetadataCache is used
+            }
+
+            void AssertTimings(IDocumentStore store)
+            {
                 using (var session = store.OpenSession())
                 {
                     QueryTimings timings = null;
                     var companies = session.Query<Company>()
-                        .Customize(x => x.Timings(out timings))
+                        .Customize(x =>
+                        {
+                            x.Timings(out timings);
+                            x.NoCaching();
+                        })
                         .Where(x => x.Name != "HR")
                         .ToList();
 
