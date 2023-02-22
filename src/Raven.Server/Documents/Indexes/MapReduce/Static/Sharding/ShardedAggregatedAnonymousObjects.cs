59aeec9965e8c64bc01d36c64445378bafbce5f9
Arkadiusz Palinski <arek@ravendb.net>
2023-02-22 14:45:39 +0100

    RavenDB-19064 Removing TODO from ShardedAggregatedAnonymousObjects

--- src/Raven.Server/Documents/Indexes/MapReduce/Static/Sharding/ShardedAggregatedAnonymousObjects.cs
+++ src/Raven.Server/Documents/Indexes/MapReduce/Static/Sharding/ShardedAggregatedAnonymousObjects.cs
@@ -3,7 +3,6 @@ using Raven.Client;
 using Raven.Server.Documents.Indexes.Persistence.Lucene.Documents;
 using Sparrow.Json;
 using Sparrow.Json.Parsing;
-using Sparrow.Utils;
 
 namespace Raven.Server.Documents.Indexes.MapReduce.Static.Sharding;
 
@@ -13,8 +12,6 @@ public class ShardedAggregatedAnonymousObjects : AggregatedAnonymousObjects
 
     public ShardedAggregatedAnonymousObjects(List<object> results, IPropertyAccessor propertyAccessor, JsonOperationContext indexContext) : base(results, propertyAccessor, indexContext)
     {
-        DevelopmentHelper.ShardingToDo(DevelopmentHelper.TeamMember.Grisha, DevelopmentHelper.Severity.Normal, "RavenDB-19064 handle metadata merge, score, distance");
-
         ModifyOutputToStore = djv =>
         {
             djv[Constants.Documents.Metadata.Key] = DummyDynamicJsonValue;
