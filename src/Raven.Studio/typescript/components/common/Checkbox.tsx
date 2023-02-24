6dc0e2c7ceead597122fd5eccd3db0d414ba1ef6
kwiato <kwiato.r@gmail.com>
2023-02-24 15:11:10 +0100

    RavenDB-16865 New Database creator

--- src/Raven.Studio/typescript/components/common/Checkbox.tsx
+++ src/Raven.Studio/typescript/components/common/Checkbox.tsx
@@ -1,22 +1,23 @@
-﻿import React, { ReactNode } from "react";
+﻿import React, { HTMLAttributes, ReactNode } from "react";
 import { Input, Label } from "reactstrap";
 import useId from "hooks/useId";
 import classNames from "classnames";
 
 import "./Checkbox.scss";
 
-interface CheckboxProps {
+interface CheckboxProps extends Omit<HTMLAttributes<HTMLDivElement>, "className" | "children"> {
     selected: boolean;
     toggleSelection: () => void;
     children?: ReactNode | ReactNode[];
     color?: string;
     size?: string;
     reverse?: boolean;
+    disabled?: boolean;
     className?: string;
 }
 
 export function Checkbox(props: CheckboxProps) {
-    const { selected, toggleSelection, children, color, size, reverse, className } = props;
+    const { selected, toggleSelection, children, color, size, reverse, className, disabled, ...rest } = props;
     const inputId = useId("checkbox");
 
     const checkboxClass = reverse ? `form-check-reverse` : "form-check";
@@ -24,8 +25,8 @@ export function Checkbox(props: CheckboxProps) {
     const sizeClass = size ? `form-check-${size}` : undefined;
 
     return (
-        <div className={classNames(checkboxClass, colorClass, sizeClass, className)}>
-            <Input type="checkbox" id={inputId} checked={selected} onChange={toggleSelection} />
+        <div className={classNames(checkboxClass, colorClass, sizeClass, className)} {...rest}>
+            <Input type="checkbox" id={inputId} checked={selected} onChange={toggleSelection} disabled={disabled} />
             {children && (
                 <Label check htmlFor={inputId}>
                     {children}
@@ -36,7 +37,7 @@ export function Checkbox(props: CheckboxProps) {
 }
 
 export function Switch(props: CheckboxProps) {
-    const { selected, toggleSelection, children, color, size, reverse, className } = props;
+    const { selected, toggleSelection, children, color, size, reverse, className, disabled, ...rest } = props;
     const inputId = useId("checkbox");
 
     const checkboxClass = reverse ? `form-check-reverse` : "form-check";
@@ -44,8 +45,8 @@ export function Switch(props: CheckboxProps) {
     const sizeClass = size ? `form-check-${size}` : undefined;
 
     return (
-        <div className={classNames(colorClass, sizeClass, checkboxClass, "form-switch", className)}>
-            <Input type="checkbox" id={inputId} checked={selected} onChange={toggleSelection} />
+        <div className={classNames(colorClass, sizeClass, checkboxClass, "form-switch", className)} {...rest}>
+            <Input type="checkbox" id={inputId} checked={selected} onChange={toggleSelection} disabled={disabled} />
             {children && (
                 <Label check htmlFor={inputId}>
                     {children}
@@ -56,7 +57,7 @@ export function Switch(props: CheckboxProps) {
 }
 
 export function Radio(props: CheckboxProps) {
-    const { selected, toggleSelection, children, color, size, reverse, className } = props;
+    const { selected, toggleSelection, children, color, size, reverse, className, disabled, ...rest } = props;
     const inputId = useId("checkbox");
 
     const checkboxClass = reverse ? `form-check-reverse` : "form-check";
@@ -64,8 +65,8 @@ export function Radio(props: CheckboxProps) {
     const sizeClass = size ? `form-check-${size}` : undefined;
 
     return (
-        <div className={classNames(checkboxClass, colorClass, sizeClass, className)}>
-            <Input type="radio" id={inputId} checked={selected} onChange={toggleSelection} />
+        <div className={classNames(checkboxClass, colorClass, sizeClass, className)} {...rest}>
+            <Input type="radio" id={inputId} checked={selected} onChange={toggleSelection} disabled={disabled} />
             {children && (
                 <Label check htmlFor={inputId}>
                     {children}
---
+++ src/Raven.Studio/typescript/components/common/LicenseRestrictions.tsx
@@ -0,0 +1,47 @@
+﻿import React, { ReactNode } from "react";
+import { Button, PopoverBody, UncontrolledPopover } from "reactstrap";
+import useId from "hooks/useId";
+import { Icon } from "./Icon";
+
+interface LicenseRestrictionsProps {
+    children: ReactNode | ReactNode[];
+    isAvailable?: boolean;
+    featureName?: string | ReactNode | ReactNode[];
+}
+
+export function LicenseRestrictions(props: LicenseRestrictionsProps): JSX.Element {
+    const { children, isAvailable, featureName } = props;
+    const containerId = useId("Info");
+    if (!isAvailable) {
+        return (
+            <>
+                <div id={containerId} className="item-disabled">
+                    {children}
+                </div>
+                <UncontrolledPopover
+                    target={containerId}
+                    trigger="hover"
+                    placement="top"
+                    container="PopoverContainer" //TODO add containers in main layout or remove this part
+                >
+                    <PopoverBody>
+                        Current license doesn't include {featureName ? featureName : "this feature"}.<br />
+                        <div className="text-center mt-1">
+                            <Button
+                                href="https://ravendb.net/buy"
+                                target="_blank"
+                                color="primary"
+                                size="xs"
+                                className="rounded-pill"
+                            >
+                                Licensing options <Icon icon="newtab" />
+                            </Button>
+                        </div>
+                    </PopoverBody>
+                </UncontrolledPopover>
+            </>
+        );
+    }
+
+    return <>{children}</>;
+}
