a945c127b03cdfc5854408195485ccbf663def2c
Ayende Rahien <ayende@ayende.com>
2023-02-26 13:28:32 +0200

    RavenDB-19945 - Optimizing Changes API for use in aggressive caching under high load- - Added a dedicated option to handle aggressive changes that will let the client know when changes happen. - Separated aggressive caching & changes channels - Will only notify the client *once* per set of events, to avoid overwhelming it (or creating big set of messages on the server) - Handle older servers as well properly using older mechanism

---
+++ test/SlowTests/Issues/RavenDB-19945.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FastTests;
+using Nito.AsyncEx;
+using Raven.Client.Documents.Changes;
+using Raven.Client.Http;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues ;
+
+public class RavenDB_19945 : RavenTestBase
+{
+    public RavenDB_19945(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Fact]
+    public async Task WillNotSendEvenForEachChangeForAggressiveCaching()
+    {
+        using var store = GetDocumentStore();
+
+        const int count = 100;
+        var cde = new CountdownEvent(count);
+        var requestExecutor = store.GetRequestExecutor();
+        int cacheGeneration = requestExecutor.Cache.Generation;
+        using(await store.AggressivelyCacheAsync())
+        {
+            IDatabaseChanges databaseChanges = store.Changes();
+            await databaseChanges.EnsureConnectedNow().ConfigureAwait(false);
+            IChangesObservable<DocumentChange> forAllDocuments = databaseChanges.ForAllDocuments();
+            await forAllDocuments.EnsureSubscribedNow().ConfigureAwait(false);
+            forAllDocuments.Subscribe(_ => cde.Signal());
+
+            for (int i = 0; i < count / 10; i++)
+            {
+                using var s = store.OpenAsyncSession();
+                for (int j = 0; j < count/10; j++)
+                {
+                    await s.StoreAsync(new { });
+                }
+                await s.SaveChangesAsync();
+            }
+        }
+
+        Assert.True(cde.Wait(TimeSpan.FromSeconds(30)));
+        
+        Assert.NotEqual(cacheGeneration, requestExecutor.Cache.Generation); // has updates
+        Assert.True(cacheGeneration + count > requestExecutor.Cache.Generation); // not for every single one
+    }
+}
