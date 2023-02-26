38b7d8d36d4e051ee23c86feee4b864f502093fe
Omer Ratsaby <omer.ratsaby@ravendb.net>
2023-02-26 15:08:01 +0200

    RavenDB-19226: specified the source of Subscription at the log lines

--- src/Raven.Server/Documents/Subscriptions/SubscriptionProcessor/SubscriptionProcessor.cs
+++ src/Raven.Server/Documents/Subscriptions/SubscriptionProcessor/SubscriptionProcessor.cs
@@ -26,7 +26,7 @@ namespace Raven.Server.Documents.Subscriptions.SubscriptionProcessor
         protected SubscriptionProcessor(ServerStore server, DocumentDatabase database, SubscriptionConnection connection) :
             base(server, database, connection)
         {
-            Logger = LoggingSource.Instance.GetLogger<SubscriptionProcessor<T>>(Database.Name);
+            Logger = LoggingSource.Instance.GetLogger(Database.Name, connection == null ? $"{nameof(TestDocumentsSubscriptionProcessor)}" : $"{nameof(SubscriptionProcessor)}<{connection.Options.SubscriptionName}>");
         }
 
         protected abstract SubscriptionFetcher<T> CreateFetcher();
