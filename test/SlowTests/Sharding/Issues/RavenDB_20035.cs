00afba6d6fb40816b1012b35d3861ae1fe9cc8b0
Pawel Pekrol <ppekrol@ravendb.net>
2023-03-03 10:57:17 +0100

    RavenDB-20035 Handle includes correctly when property value is null

---
+++ test/SlowTests/Sharding/Issues/RavenDB_20035.cs
@@ -0,0 +1,49 @@
+﻿using FastTests;
+using Orders;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Sharding.Issues;
+
+public class RavenDB_20035 : RavenTestBase
+{
+    public RavenDB_20035(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [RavenFact(RavenTestCategory.Sharding)]
+    public void Should_Not_Throw_NRE()
+    {
+        using (var store = Sharding.GetDocumentStore())
+        {
+            using (var session = store.OpenSession())
+            {
+                session.Store(new Address { City = "Torun" }, "addresses/1");
+                session.Store(new Company
+                {
+                    ExternalId = null,
+                    Fax = "addresses/1"
+                }, "companies/1");
+
+                session.SaveChanges();
+            }
+
+            using (var session = store.OpenSession())
+            {
+                var company = session.Load<Company>("companies/1", builder => builder
+                    .IncludeDocuments<Address>(x => x.ExternalId)
+                    .IncludeDocuments<Address>(x => x.Fax));
+
+                Assert.NotNull(company);
+
+                var numberOfRequests = session.Advanced.NumberOfRequests;
+
+                var address = session.Load<Address>(company.Fax);
+
+                Assert.NotNull(address);
+                Assert.Equal(numberOfRequests, session.Advanced.NumberOfRequests);
+            }
+        }
+    }
+}
