3b3d06825959ff289b070ead937ff29e13070592
Gracjan Sadowicz <gracjan@ravendb.net>
2023-03-06 16:08:43 +0100

    RavenDB-18888 Simplify GetUbuntuVersionFromDockerfile

--- docker/build-ubuntu.ps1
+++ docker/build-ubuntu.ps1
@@ -31,8 +31,7 @@ function GetPackageFileName($arch)
 
 function GetUbuntuVersionFromDockerfile($DockerfileDir, $DockerfileName) {
     $dockerfilePath = Join-Path $DockerfileDir "Dockerfile.$arch"
-    $dockerfileContents = Get-Content $dockerfilePath
-    $ubuntuVersion = $dockerfileContents | Select-String -Pattern "(?<=FROM\s+mcr\.microsoft\.com\/dotnet\/runtime-deps:7\.0-)(.*)"
+    $ubuntuVersion = Get-Content $dockerfilePath | Select-String -Pattern "(?<=FROM\s+mcr\.microsoft\.com\/dotnet\/runtime-deps:7\.0-)(.*)"
     return $ubuntuVersion.Matches.Groups[1].Value
 }
 
