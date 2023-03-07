5d3a120f97c3ff3e23ef8000c56b3c5de55a57f4
aviv <aviv@ayende.com>
2023-03-07 14:05:48 +0200

    RavenDB-19614 : BucketStatsHolder : use a single DocumentsOperationContext (per transaction) to allocate change vectors

--- src/Raven.Server/Documents/BucketStats.cs
+++ src/Raven.Server/Documents/BucketStats.cs
@@ -1,4 +1,6 @@
 using System.Runtime.InteropServices;
+using Sparrow.Server;
+using Voron;
 
 namespace Raven.Server.Documents
 {
@@ -13,5 +15,19 @@ namespace Raven.Server.Documents
 
         [FieldOffset(16)]
         public long LastModifiedTicks;
+
+        public unsafe ByteStringContext.InternalScope GetMergedChangeVector(ByteStringContext context, ValueReader reader, out Slice changeVector)
+        {
+            if (reader.Length <= sizeof(BucketStats))
+            {
+                changeVector = default;
+                return default;
+            }
+
+            return Slice.From(context,
+                reader.Base + sizeof(BucketStats),
+                reader.Length - sizeof(BucketStats),
+                out changeVector);
+        }
     }
 }
