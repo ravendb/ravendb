2371301e6caa7dfcf0bf0cc91eeff63765e1b88e
Pawel Pekrol <ppekrol@ravendb.net>
2023-03-10 11:07:52 +0100

    RavenDB-20048 use DocumentLastModifiedComparer if no ordering is specified

--- src/Raven.Server/Documents/Sharding/Comparers/DocumentLastModifiedComparer.cs
+++ src/Raven.Server/Documents/Sharding/Comparers/DocumentLastModifiedComparer.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Raven.Client;
 using Raven.Client.Extensions;
 using Sparrow.Json;
 
@@ -14,6 +16,40 @@ public class DocumentLastModifiedComparer : IComparer<BlittableJsonReaderObject>
 
     public int Compare(BlittableJsonReaderObject x, BlittableJsonReaderObject y)
     {
-        return y.GetMetadata().GetLastModified().CompareTo(x.GetMetadata().GetLastModified());
+        if (TryGetLastModified(y, out var yLastModified) == false)
+            ThrowInvalidMissingLastModified();
+
+        if (TryGetLastModified(x, out var xLastModified) == false)
+            ThrowInvalidMissingLastModified();
+
+        return yLastModified.CompareTo(xLastModified);
+
+        static bool TryGetLastModified(BlittableJsonReaderObject json, out DateTime lastModified)
+        {
+            if (json == null)
+            {
+                lastModified = default;
+                return false;
+            }
+
+            if (json.TryGetMetadata(out var metadata) == false)
+            {
+                lastModified = default;
+                return false;
+            }
+
+            if (metadata.TryGetLastModified(out lastModified) == false)
+            {
+                lastModified = default;
+                return false;
+            }
+
+            return true;
+        }
+
+        static void ThrowInvalidMissingLastModified()
+        {
+            throw new InvalidOperationException($"Document does not contain '{Constants.Documents.Metadata.LastModified}' field.");
+        }
     }
 }
