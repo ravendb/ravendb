6985f0f83edfed39ea711c03745636be68b6433c
Stav <stav@ravendb.net>
2023-03-14 15:23:29 +0200

    RavenDB-19611 - address PR comments

--- src/Raven.Server/Documents/Handlers/Processors/OngoingTasks/OngoingTasksHandlerProcessorForAddEtl.cs
+++ src/Raven.Server/Documents/Handlers/Processors/OngoingTasks/OngoingTasksHandlerProcessorForAddEtl.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using JetBrains.Annotations;
-using Raven.Client.Documents.Operations.ETL;
+﻿using JetBrains.Annotations;
 using Raven.Server.ServerWide.Context;
 
 namespace Raven.Server.Documents.Handlers.Processors.OngoingTasks
@@ -9,21 +7,6 @@ namespace Raven.Server.Documents.Handlers.Processors.OngoingTasks
     {
         public OngoingTasksHandlerProcessorForAddEtl([NotNull] DatabaseRequestHandler requestHandler) : base(requestHandler)
         {
-        }
-
-        protected override void AssertIsEtlTypeSupported(EtlType type)
-        {
-            switch (type)
-            {
-                case EtlType.Raven:
-                case EtlType.Sql:
-                case EtlType.Olap:
-                case EtlType.ElasticSearch:
-                case EtlType.Queue:
-                    return;
-                default:
-                    throw new NotSupportedException($"Unknown ETL type {type}");
-            }
         }
     }
 }
