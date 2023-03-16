4e23a37034a8674cf5c3fee7bcaeb1c8318efed4
Ayende Rahien <ayende@ayende.com>
2023-03-16 14:48:43 +0200

    RavenDB-20100 Flush compare exchange pending changes before we flush the documents

--- src/Raven.Server/Smuggler/Documents/Data/ISmugglerDestination.cs
+++ src/Raven.Server/Smuggler/Documents/Data/ISmugglerDestination.cs
@@ -50,7 +50,7 @@ namespace Raven.Server.Smuggler.Documents.Data
 
     public interface IDocumentActions : INewDocumentActions, IAsyncDisposable
     {
-        ValueTask WriteDocumentAsync(DocumentItem item, SmugglerProgressBase.CountsWithLastEtagAndAttachments progress);
+        ValueTask WriteDocumentAsync(DocumentItem item, SmugglerProgressBase.CountsWithLastEtagAndAttachments progress, Func<Task> beforeFlushing = null);
 
         ValueTask WriteTombstoneAsync(Tombstone tombstone, SmugglerProgressBase.CountsWithLastEtag progress);
 
@@ -112,9 +112,7 @@ namespace Raven.Server.Smuggler.Documents.Data
 
         ValueTask WriteTombstoneKeyAsync(string key);
 
-        bool MaybeFlush();
-
-        Task FlushAsync();
+        ValueTask FlushAsync();
     }
 
     public interface IDatabaseRecordActions : IAsyncDisposable
