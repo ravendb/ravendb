de3c86c5fcd8c5c3286896f714f96b229118e36f
Paweł Pekról <ppekrol@ravendb.net>
2023-03-24 10:31:57 +0100

    Revert "RavenDB-18888 Revert the changes in specific build scripts"
    This reverts commit f957a8fc389b1b02526cf2ebbcbe2822e644511a.

--- scripts/linux/pkg/deb/build-deb_ubuntu-bionic_armhf.ps1
+++ scripts/linux/pkg/deb/build-deb_ubuntu-bionic_armhf.ps1
@@ -2,7 +2,9 @@ $env:DISTRO_NAME = "ubuntu"
 $env:DISTRO_VERSION = "18.04"
 $env:DISTRO_VERSION_NAME ="bionic"
 
-$env:OUTPUT_DIR = "$PSScriptRoot/dist"
+if (-not $env:OUTPUT_DIR) {
+    $env:OUTPUT_DIR = "$PSScriptRoot/dist"
+}
 
 .\set-raven-platform-armhf.ps1
 .\set-raven-version-env.ps1
