acf48874c6ef607852aed4644de3c75817260af1
shiranshalom <shiran@ravendb.net>
2023-03-26 15:13:30 +0300

    RavenDB-19744 - Sharding - Patch by query with identity will generate ids not in the correct shard

---
+++ test/SlowTests/Sharding/Issues/RavenDB_19744.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.Documents.Operations;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Sharding.Issues
+{
+    public class RavenDB_19744 : RavenTestBase
+    {
+        public RavenDB_19744(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [RavenFact(RavenTestCategory.Patching | RavenTestCategory.Sharding)]
+        public async Task PatchByQueryOnShardedDbShouldGenerateIdsInTheCorrectShard()
+        {
+            using (var store = Sharding.GetDocumentStore())
+            {
+                await store.Maintenance.SendAsync(new CreateSampleDataOperation());
+
+                var operation = await store.Operations.SendAsync(new PatchByQueryOperation("from Orders update { put(\"orders/\", this) }"));
+                var result = await operation.WaitForCompletionAsync<BulkOperationResult>(TimeSpan.FromMinutes(1));
+                Assert.Equal(830, result.Total);
+            }
+        }
+    }
+}
