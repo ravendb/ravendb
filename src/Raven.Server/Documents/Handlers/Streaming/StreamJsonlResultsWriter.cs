95714bf7870ebd99bc9c45bad171bf19baa655ac
Gracjan Sadowicz <gracjan@ravendb.net>
2023-03-27 16:03:26 +0200

    RavenDB-20033 Create JSONL endpoint also for documents streaming

---
+++ src/Raven.Server/Documents/Handlers/Streaming/StreamJsonlResultsWriter.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Raven.Server.Json;
+using Sparrow.Json;
+
+namespace Raven.Server.Documents.Handlers.Streaming;
+
+public class StreamJsonlResultsWriter : IStreamResultsWriter<Document>
+{
+    private readonly AsyncBlittableJsonTextWriter _writer;
+    private readonly JsonOperationContext _context;
+
+    public StreamJsonlResultsWriter(Stream stream, JsonOperationContext context)
+    {
+        _context = context;
+        _writer = new AsyncBlittableJsonTextWriter(context, stream);
+    }
+    
+    public ValueTask DisposeAsync()
+    {
+        return _writer.DisposeAsync();
+    }
+
+    public void StartResponse()
+    {
+    }
+
+    public void StartResults()
+    {
+    }
+
+    public void EndResults()
+    {
+    }
+    
+    public async ValueTask AddResultAsync(Document res, CancellationToken token)
+    {
+        _writer.WriteDocument(_context, res, metadataOnly: false);
+        _writer.WriteNewLine();
+        await _writer.MaybeFlushAsync(token);
+    }
+
+    public void EndResponse()
+    {
+    }
+}
