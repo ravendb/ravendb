85557ac0c2a167beaeddcba2f050ba85d844d6b2
shiranshalom <shiran@ravendb.net>
2023-03-28 13:21:47 +0300

    RavenDB-19744 - change implementation

--- test/SlowTests/Sharding/Issues/RavenDB_19744.cs
+++ test/SlowTests/Sharding/Issues/RavenDB_19744.cs
@@ -21,9 +21,15 @@ namespace SlowTests.Sharding.Issues
             {
                 await store.Maintenance.SendAsync(new CreateSampleDataOperation());
 
+                // all document Ids in 'Orders' collection don't contain a '$suffix'
                 var operation = await store.Operations.SendAsync(new PatchByQueryOperation("from Orders update { put(\"orders/\", this) }"));
                 var result = await operation.WaitForCompletionAsync<BulkOperationResult>(TimeSpan.FromMinutes(1));
                 Assert.Equal(830, result.Total);
+
+                // some document Ids in 'Orders' collection contains a '$suffix'
+                operation = await store.Operations.SendAsync(new PatchByQueryOperation("from Orders update { put(\"orders/\", this) }"));
+                result = await operation.WaitForCompletionAsync<BulkOperationResult>(TimeSpan.FromMinutes(1));
+                Assert.Equal(1660, result.Total);
             }
         }
     }
