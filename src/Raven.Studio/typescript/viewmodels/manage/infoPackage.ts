56484ea9b0c451bce057492735d5df78593b452f
Marcin Lewandowski <marcin@ravendb.net>
2023-03-31 13:52:31 +0200

    RavenDB-20062 Additional option to collect debug info - Studio part

--- src/Raven.Studio/typescript/viewmodels/manage/infoPackage.ts
+++ src/Raven.Studio/typescript/viewmodels/manage/infoPackage.ts
@@ -7,31 +7,80 @@ import messagePublisher = require("common/messagePublisher");
 import notificationCenter = require("common/notifications/notificationCenter");
 import viewHelpers = require("common/helpers/view/viewHelpers");
 import killOperationCommand = require("commands/operations/killOperationCommand");
+import databasesManager = require("common/shell/databasesManager");
+import database = require("models/resources/database");
+import downloader = require("common/downloader");
+
+type packageScope = "currentServer" | "entireCluster";
+
+class infoPackageModel {
+    scope = ko.observable<packageScope>();
+    types = ko.observableArray<Raven.Server.Documents.Handlers.Debugging.ServerWideDebugInfoPackageHandler.DebugInfoPackageContentType>(["ServerWide", "Databases", "LogFile"]);
+    
+    allDatabases = ko.observable<boolean>(true);
+    databases = ko.observableArray<string>([]);
+
+    validationGroup: KnockoutValidationGroup;
+    
+    constructor() {
+        this.initValidation();
+    }
+    
+    private initValidation() {
+        this.scope.extend({
+            required: true
+        });
+
+        this.types.extend({
+            required: true
+        });
+        
+        this.databases.extend({
+            required: {
+                onlyIf: () => {
+                    const databasesSelected = this.types().indexOf("Databases") !== -1;
+                    const allDbs = this.allDatabases();
+                    return databasesSelected && !allDbs; 
+                }
+            }
+        });
+        
+        this.validationGroup = ko.validatedObservable({
+            scope: this.scope,
+            types: this.types,
+            databases: this.databases
+        });
+    }
+    
+    toUrlParams() {
+        return {
+            type: this.types().join(","), // we join it here as it is enum flag, so we want to send to server CSV instead of separate params
+            database: this.allDatabases() ? undefined : this.databases()
+        }
+    }
+}
 
 class infoPackage extends viewModelBase {
+    
+    model = new infoPackageModel();
+
+    databaseNames: KnockoutComputed<Array<string>>;
     
     operationId: number;
     
     spinners = {
         abort: ko.observable<boolean>(false),
-        clusterWide: ko.observable<boolean>(false),
-        serverWide: ko.observable<boolean>(false),
-        anyInProgress: null as KnockoutComputed<boolean>
+        inProgress: ko.observable<boolean>(false),
     }
     
     constructor() {
         super();
         
-        this.spinners.anyInProgress = ko.pureComputed(() => {
-            const cluster = this.spinners.clusterWide();
-            const server = this.spinners.serverWide();
-            
-            return cluster || server;
-        })
+        this.databaseNames = ko.pureComputed(() => databasesManager.default.databases().map((db: database) => db.name))
     }
     
     canDeactivate(): boolean | JQueryPromise<canDeactivateResultDto> {
-        if (this.spinners.anyInProgress()) {
+        if (this.spinners.inProgress()) {
             return this.confirmLeavingPage();
         }
         
@@ -42,19 +91,35 @@ class infoPackage extends viewModelBase {
         return new getNextOperationId(null).execute()
             .fail((response: JQueryXHR) => {
                 messagePublisher.reportError("Could not get next task id.", response.responseText, response.statusText);
-                this.spinners.serverWide(false);
-                this.spinners.clusterWide(false);
+                this.spinners.inProgress(false);
             });
     }
 
-    downloadServerWidePackage() {
-        this.spinners.serverWide(true);
+    downloadPackage() {
+        if (!this.isValid(this.model.validationGroup)) {
+            return;
+        }
+        
+        this.spinners.inProgress(true);
+
+        switch (this.model.scope()) {
+            case "currentServer":
+                this.downloadServerWidePackage();
+                break;
+            case "entireCluster":
+                this.downloadClusterWidePackage();
+                break;
+            default:
+                throw new Error("Unsupported scope: " + this.model.scope());
+        }
+    }
+
+    private downloadServerWidePackage() {
         eventsCollector.default.reportEvent("info-package", "server-wide");
         this.startDownload(endpoints.global.serverWideDebugInfoPackage.adminDebugInfoPackage);
     }
 
-    downloadClusterWidePackage() {
-        this.spinners.clusterWide(true);
+    private downloadClusterWidePackage() {
         eventsCollector.default.reportEvent("info-package", "cluster-wide");
         this.startDownload(endpoints.global.serverWideDebugInfoPackage.adminDebugClusterInfoPackage);
     }
@@ -63,17 +128,20 @@ class infoPackage extends viewModelBase {
         this.getNextOperationId()
             .done((operationId: number) => {
                 this.operationId = operationId;
-                const operationPart = "?operationId=" + operationId;
+                
+                const urlParams = {
+                    ...this.model.toUrlParams(),
+                    operationId
+                };
 
                 const $form = $("#downloadInfoPackageForm");
-                $form.attr("action", appUrl.baseUrl + url + operationPart);
-                $("[name=operationId]", $form).val(operationId.toString());
+                $form.attr("action", appUrl.baseUrl + url);
+                downloader.fillHiddenFields(urlParams, $form);
                 $form.submit();
 
                 notificationCenter.instance.monitorOperation(null, operationId)
                     .always(() => {
-                        this.spinners.clusterWide(false);
-                        this.spinners.serverWide(false);
+                        this.spinners.inProgress(false);
                         this.spinners.abort(false);
                     });
             });
