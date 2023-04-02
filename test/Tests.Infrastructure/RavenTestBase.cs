f5cf748394d97e1668dc3c620a4783917267dc68
Stav <stav@ravendb.net>
2023-04-02 13:21:50 +0300

    RavenDB-19747 - testing 2 digit shard number

--- test/Tests.Infrastructure/RavenTestBase.cs
+++ test/Tests.Infrastructure/RavenTestBase.cs
@@ -767,7 +767,7 @@ namespace FastTests
                                 Shards = new Dictionary<int, DatabaseTopology>()
                                 {
                                     {0, new DatabaseTopology()},
-                                    {1, new DatabaseTopology()},
+                                    {12, new DatabaseTopology()},
                                     {2, new DatabaseTopology()}
                                 }
                             };
