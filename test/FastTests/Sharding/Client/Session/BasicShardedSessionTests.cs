c36c0b11e74efb58f1c62bb16a0c51c3cdc5c3e9
Stav <stav@ravendb.net>
2023-04-02 16:20:28 +0300

    RavenDB-19747  - add two digit shard simple test

--- test/FastTests/Sharding/Client/Session/BasicShardedSessionTests.cs
+++ test/FastTests/Sharding/Client/Session/BasicShardedSessionTests.cs
@@ -11,6 +11,7 @@ using Raven.Client.Documents.Operations;
 using Raven.Client.Documents.Operations.Attachments;
 using Raven.Client.Documents.Session;
 using Raven.Client.Exceptions;
+using Raven.Client.ServerWide;
 using Raven.Server.Documents;
 using Raven.Tests.Core.Utils.Entities;
 using Sparrow.Json.Parsing;
@@ -66,6 +67,49 @@ namespace FastTests.Sharding.Client.Session
         public void CRUD_Operations()
         {
             using (var store = Sharding.GetDocumentStore())
+            {
+                using (var newSession = store.OpenSession())
+                {
+                    newSession.Store(new User { Name = "user1" }, "users/1");
+                    var user2 = new User { Name = "user2", Age = 1 };
+                    newSession.Store(user2, "users/2");
+                    var user3 = new User { Name = "user3", Age = 1 };
+                    newSession.Store(user3, "users/3");
+                    newSession.Store(new User { Name = "user4" }, "users/4");
+
+                    newSession.Delete(user2);
+                    user3.Age = 3;
+                    newSession.SaveChanges();
+
+                    var tempUser = newSession.Load<User>("users/2");
+                    Assert.Null(tempUser);
+                    tempUser = newSession.Load<User>("users/3");
+                    Assert.Equal(tempUser.Age, 3);
+                    var user1 = newSession.Load<User>("users/1");
+                    var user4 = newSession.Load<User>("users/4");
+
+                    newSession.Delete(user4);
+                    user1.Age = 10;
+                    newSession.SaveChanges();
+
+                    tempUser = newSession.Load<User>("users/4");
+                    Assert.Null(tempUser);
+                    tempUser = newSession.Load<User>("users/1");
+                    Assert.Equal(tempUser.Age, 10);
+                }
+            }
+        }
+
+        [RavenFact(RavenTestCategory.ClientApi | RavenTestCategory.Sharding)]
+        public void TwoDigitShard()
+        {
+            using (var store = Sharding.GetDocumentStore(shards: new Dictionary<int, DatabaseTopology>()
+                       {
+                           { 1, new DatabaseTopology() },
+                           { 12, new DatabaseTopology() },
+                           { 2, new DatabaseTopology() }
+                       }
+                   ))
             {
                 using (var newSession = store.OpenSession())
                 {
