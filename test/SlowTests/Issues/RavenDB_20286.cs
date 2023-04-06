39efbcb0c11770246cbddf2c3b53e35d55e64232
Arkadiusz Palinski <arek@ravendb.net>
2023-04-06 13:07:55 +0200

    RavenDB-20286 Added a test to make sure we don't have any using with MySQL connector package in Raven.Serrver project

---
+++ test/SlowTests/Issues/RavenDB_20286.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Reflection;
+using FastTests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues;
+
+public class RavenDB_20286 : NoDisposalNeeded
+{
+    public RavenDB_20286(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [Fact]
+    public void MustNotHaveMySqlConnectorUsing()
+    {
+        string currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        string server = Path.Combine(currentDir, "../../../../../src/Raven.Server");
+
+        var csharpFiles = Directory.GetFiles(server, "*.cs", SearchOption.AllDirectories);
+
+        Assert.True(csharpFiles.Length > 0);
+
+        foreach (string filePath in csharpFiles)
+        {
+            using (var file = File.OpenText(filePath))
+            {
+                string line = file.ReadToEnd();
+
+                Assert.DoesNotContain("using MySql.Data.MySqlClient", line);
+                Assert.DoesNotContain("using MySqlConnector", line);
+            }
+        }
+    }
+}
