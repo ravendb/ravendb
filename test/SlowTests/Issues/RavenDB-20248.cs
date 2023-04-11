e59e0b16fcb81cc1a1a4c93d44a7e0af9ab6f0d7
Michał Leśniak <michal.lesniak@ravendb.net>
2023-04-11 13:32:51 +0200

    RavenDB-20248 Removed aliasing when ordering by id() and added tests for Filter and GroupBy

--- test/SlowTests/Issues/RavenDB-20248.cs
+++ test/SlowTests/Issues/RavenDB-20248.cs
@@ -42,6 +42,58 @@ public class RavenDB_20248 : RavenTestBase
             Assert.Equal(queryResult[1].Name, "Maciej" + "Test");
         }
     }
+
+    [Fact]
+    public void TestFilterAliasing()
+    {
+        using var store = GetDocumentStore();
+        using (var session = store.OpenSession())
+        {
+            session.Store(new First() {Name = "Name1", Inner = new Inner() {SortOrder = "order1"}});
+            session.Store(new First() {Name = "Name2", Inner = new Inner() {SortOrder = "order2"}});
+            session.Store(new First() {Name = "Name3", Inner = new Inner() {SortOrder = "order1"}});
+            session.SaveChanges();
+            
+            var source = session.Advanced.DocumentQuery<First>()
+                .Filter(i => i.Equals(a => a.Inner.SortOrder, "order1"))
+                .ToQueryable();
+            
+            var query =
+                from e in source
+                select new First {Name = e.Name + "Test", Inner = e.Inner};
+            
+            var queryResult = query.ToList();
+            
+            Assert.Equal(2, queryResult.Count);
+            Assert.Equal(queryResult[0].Name, "Name1" + "Test");
+            Assert.Equal(queryResult[1].Name, "Name3" + "Test");
+        }
+    }
+    
+    [Fact]
+    public void TestGroupByAliasing()
+    {
+        using var store = GetDocumentStore();
+        using (var session = store.OpenSession())
+        {
+            session.Store(new First() {Name = "Name1", Inner = new Inner() {SortOrder = "order1"}});
+            session.Store(new First() {Name = "Name2", Inner = new Inner() {SortOrder = "order2"}});
+            session.Store(new First() {Name = "Name3", Inner = new Inner() {SortOrder = "order1"}});
+            session.SaveChanges();
+            
+            var query = session.Query<First>()
+                .GroupBy(x => new { Inner = x.Inner })
+                .Select(x => new { Inner = x.Key.Inner, Count = x.Count() });
+            
+            var queryResult = query.ToList();
+            
+            Assert.Equal(2, queryResult.Count);
+            Assert.Equal(2, queryResult[0].Count);
+            Assert.Equal(1, queryResult[1].Count);
+            Assert.Equal("order1", queryResult[0].Inner.SortOrder);
+            Assert.Equal("order2", queryResult[1].Inner.SortOrder);
+        }
+    }
     
     private class First
     {
