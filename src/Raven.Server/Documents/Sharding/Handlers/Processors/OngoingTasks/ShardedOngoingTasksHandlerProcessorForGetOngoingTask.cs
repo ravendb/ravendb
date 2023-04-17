ba1b033c29a958aa3c984542bd47de79075f96f1
Pawel Pekrol <ppekrol@ravendb.net>
2023-04-17 12:30:10 +0300

    RavenDB-20318 we need to redirect to shard when nodeTag matches our node, but there is a shardNumber

--- src/Raven.Server/Documents/Sharding/Handlers/Processors/OngoingTasks/ShardedOngoingTasksHandlerProcessorForGetOngoingTask.cs
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/OngoingTasks/ShardedOngoingTasksHandlerProcessorForGetOngoingTask.cs
@@ -15,6 +15,18 @@ namespace Raven.Server.Documents.Sharding.Handlers.Processors.OngoingTasks
         {
         }
 
+        protected override bool IsCurrentNode(out string nodeTag)
+        {
+            var isCurrentNode = base.IsCurrentNode(out nodeTag);
+            if (isCurrentNode)
+            {
+                if (TryGetShardNumber(out _))
+                    return false;
+            }
+
+            return isCurrentNode;
+        }
+
         protected override Task HandleRemoteNodeAsync(ProxyCommand<OngoingTask> command, OperationCancelToken token)
         {
             return TryGetShardNumber(out int shardNumber)
