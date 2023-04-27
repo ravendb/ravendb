df0706fe406af0ec797f3ceb31738385c832d902
Maciej Aszyk <maciej@ravendb.net>
2023-04-27 09:35:53 +0200

    RavenDB-20379 Corax: user has to prefilter the output of `IfEntityIs` since metadata is Complex Object.

--- test/SlowTests/Issues/RavenDB_17130.cs
+++ test/SlowTests/Issues/RavenDB_17130.cs
@@ -5,6 +5,9 @@ using Orders;
 using Raven.Client.Documents.Indexes;
 using Raven.Client.Documents.Linq.Indexing;
 using Raven.Client.Documents.Operations;
+using Raven.Server.Config;
+using Tests.Infrastructure;
+using Tests.Infrastructure.Extensions;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -16,12 +19,19 @@ namespace SlowTests.Issues
         {
         }
 
-        [Fact]
-        public void GetDocumentsFromIndexWithIfEntityIs()
+        [RavenTheory(RavenTestCategory.Indexes)]
+        [RavenData(SearchEngineMode = RavenSearchEngineMode.Lucene, DatabaseMode = RavenDatabaseMode.All)]
+        public void GetDocumentsFromIndexWithIfEntityIs_Lucene(Options options) => GetDocumentsFromIndexWithIfEntityIs<Color_ForSearch>(options);
+        
+        [RavenTheory(RavenTestCategory.Indexes)]
+        [RavenData(SearchEngineMode = RavenSearchEngineMode.Corax, DatabaseMode = RavenDatabaseMode.All)]
+        public void GetDocumentsFromIndexWithIfEntityIs_Corax(Options options) => GetDocumentsFromIndexWithIfEntityIs<Color_ForSearch_Corax>(options);
+        
+        private void GetDocumentsFromIndexWithIfEntityIs<TIndex>(Options options) where TIndex : AbstractIndexCreationTask, new()
         {
-            using (var store = GetDocumentStore())
+            using (var store = GetDocumentStore(options))
             {
-                store.ExecuteIndex(new Color_ForSearch());
+                store.ExecuteIndex(new TIndex());
                 using (var session = store.OpenSession())
                 {
                     for (int i = 0; i < 10; i++)
@@ -40,27 +50,23 @@ namespace SlowTests.Issues
 
                     session.SaveChanges();
                 }
-
+                
                 Indexes.WaitForIndexing(store);
 
-                var stats = store.Maintenance.Send(new GetStatisticsOperation());
-                Assert.Equal(13, stats.CountOfDocuments); // 10 colors + 1 company + 2x hilo
+                var countOfDocuments = 0L;
+                store.Maintenance.ForTesting(() => new GetStatisticsOperation()).AssertAll((key, statistics) =>  countOfDocuments += statistics.CountOfDocuments);
+                Assert.Equal(13, countOfDocuments); // 10 colors + 1 company + 2x hilo
 
                 using (var session = store.OpenSession())
                 {
-                    var documentQuery = session.Advanced.DocumentQuery<Color_ForSearch.Result, Color_ForSearch>().ToList();
+                    var documentQuery = session.Advanced.DocumentQuery<Color_ForSearch_Corax.Result, TIndex>().ToList();
                     Assert.Equal(10, documentQuery.Count);
                 }
             }
         }
 
-        private class Color_ForSearch : AbstractIndexCreationTask<object, Color_ForSearch.Result>
+        private class Color_ForSearch : AbstractIndexCreationTask<object, Color_ForSearch_Corax.Result>
         {
-            public class Result : Color
-            {
-                public string Query { get; set; }
-            }
-
             public Color_ForSearch()
             {
                 Map = colors => from colorDoc in colors
@@ -72,6 +78,27 @@ namespace SlowTests.Issues
                                     color.CreatedDate,
                                     Query = AsJson(color).Select(x => x.Value),
                                 };
+            }
+        }
+        
+        private class Color_ForSearch_Corax : AbstractIndexCreationTask<object, Color_ForSearch_Corax.Result>
+        {
+            public class Result : Color
+            {
+                public string Query { get; set; }
+            }
+
+            public Color_ForSearch_Corax()
+            {
+                Map = colors => from colorDoc in colors
+                    let color = colorDoc.IfEntityIs<Color>("Colors")
+                    where color != null
+                    select new
+                    {
+                        color.Name,
+                        color.CreatedDate,
+                        Query = AsJson(color).Where(x => x.Key != "@metadata").Select(x => x.Value),
+                    };
             }
         }
 
