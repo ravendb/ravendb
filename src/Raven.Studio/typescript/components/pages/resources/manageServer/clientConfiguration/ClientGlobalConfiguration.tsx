b5072db0715eb75b327555c75c9a0af23b4b6529
Mateusz <mateusz@ravendb.net>
2023-05-04 13:29:06 +0200

    RavenDB-20304 Revamp Client Configuration UI

--- src/Raven.Studio/typescript/components/pages/resources/manageServer/clientConfiguration/ClientGlobalConfiguration.tsx
+++ src/Raven.Studio/typescript/components/pages/resources/manageServer/clientConfiguration/ClientGlobalConfiguration.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Form, Col, Button, Card, Row, Spinner } from "reactstrap";
+import React, { useState } from "react";
+import { Form, Col, Button, Card, Row, Spinner, InputGroup, InputGroupText } from "reactstrap";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { FormCheckbox, FormInput, FormSelect, FormSwitch } from "components/common/Form";
 import {
@@ -7,6 +7,7 @@ import {
     clientConfigurationYupResolver,
 } from "../../../../common/clientConfiguration/ClientConfigurationValidation";
 import ReadBalanceBehavior = Raven.Client.Http.ReadBalanceBehavior;
+import LoadBalanceBehavior = Raven.Client.Http.LoadBalanceBehavior;
 import { useServices } from "components/hooks/useServices";
 import { useAsyncCallback } from "react-async-hook";
 import { LoadingView } from "components/common/LoadingView";
@@ -14,6 +15,8 @@ import { LoadError } from "components/common/LoadError";
 import ClientConfigurationUtils from "components/common/clientConfiguration/ClientConfigurationUtils";
 import useClientConfigurationFormController from "components/common/clientConfiguration/useClientConfigurationFormController";
 import { tryHandleSubmit } from "components/utils/common";
+import { Icon } from "components/common/Icon";
+import { PopoverWithHover } from "components/common/PopoverWithHover";
 
 // TODO: show modal on exit intent if is dirty
 export default function ClientGlobalConfiguration() {
@@ -28,6 +31,12 @@ export default function ClientGlobalConfiguration() {
     });
 
     const formValues = useClientConfigurationFormController(control, setValue);
+
+    const [identityPartsSeparatorPopover, setIdentityPartsSeparatorPopover] = useState<HTMLElement>();
+    const [maximumRequestsPerSessionPopover, setMaximumRequestsPerSessionPopover] = useState<HTMLElement>();
+    const [sessionContextPopover, setSessionContextPopover] = useState<HTMLElement>();
+    const [readBalanceBehaviorPopover, setReadBalanceBehaviorPopover] = useState<HTMLElement>();
+    const [loadBalanceSeedBehaviorPopover, setLoadBalanceSeedBehaviorPopover] = useState<HTMLElement>();
 
     const onSave: SubmitHandler<ClientConfigurationFormData> = async (formData) => {
         return tryHandleSubmit(async () => {
@@ -50,96 +59,189 @@ export default function ClientGlobalConfiguration() {
 
     return (
         <Form onSubmit={handleSubmit(onSave)}>
-            <Col md="12" lg="6" className="p-4">
+            <Col md="6" lg="4" className="content-margin">
                 <Button type="submit" color="primary" disabled={formState.isSubmitting || !formState.isDirty}>
-                    {formState.isSubmitting ? <Spinner size="sm" className="me-1" /> : <i className="icon-save me-1" />}
+                    {formState.isSubmitting ? <Spinner size="sm" className="me-1" /> : <Icon icon="save" />}
                     Save
                 </Button>
 
-                <Card className="card flex-row p-2 mt-4">
-                    <Row className="flex-grow-1">
-                        <Col>
-                            <FormCheckbox control={control} name="identityPartsSeparatorEnabled">
-                                Identity parts separator
-                            </FormCheckbox>
-                        </Col>
-                        <Col>
-                            <FormInput
-                                type="text"
-                                control={control}
-                                name="identityPartsSeparatorValue"
-                                placeholder="Default is '/'"
-                                disabled={!formValues.identityPartsSeparatorEnabled}
-                            />
+                <Card className="card flex-column p-3 my-3">
+                    <div className="d-flex flex-grow-1">
+                        <div className="md-label">
+                            Identity parts separator{" "}
+                            <i ref={setIdentityPartsSeparatorPopover} className="icon-info text-info" />
+                        </div>
+                        <PopoverWithHover target={identityPartsSeparatorPopover} placement="top">
+                            <div className="flex-horizontal p-3">
+                                <div>
+                                    Changes the default separator for automatically generated document IDs. You can use
+                                    any <code>char</code> except <code>|</code> (pipe)
+                                </div>
+                            </div>
+                        </PopoverWithHover>
+                    </div>
+                    <Row className="flex-grow-1 mt-1">
+                        <Col className="d-flex">
+                            <InputGroup>
+                                <InputGroupText>
+                                    <FormCheckbox
+                                        control={control}
+                                        name="identityPartsSeparatorEnabled"
+                                        disabled={!formValues.overrideConfig}
+                                    />
+                                </InputGroupText>
+                                <FormInput
+                                    type="text"
+                                    control={control}
+                                    name="identityPartsSeparatorValue"
+                                    placeholder="Default is '/'"
+                                    disabled={!formValues.identityPartsSeparatorEnabled || !formValues.overrideConfig}
+                                    className="d-flex"
+                                />
+                            </InputGroup>
                         </Col>
                     </Row>
                 </Card>
 
-                <Card className="flex-row mt-1 p-2">
-                    <Row className="flex-grow-1">
-                        <Col>
-                            <FormCheckbox control={control} name="maximumNumberOfRequestsEnabled">
-                                Maximum number of requests per session
-                            </FormCheckbox>
-                        </Col>
-                        <Col>
-                            <FormInput
-                                type="number"
-                                control={control}
-                                name="maximumNumberOfRequestsValue"
-                                placeholder="Default value is 30"
-                                disabled={!formValues.maximumNumberOfRequestsEnabled}
-                            />
+                <Card className="flex-column mt-1 mb-3 p-3">
+                    <div className="d-flex flex-grow-1">
+                        <div className="md-label">
+                            Maximum number of requests per session{" "}
+                            <i ref={setMaximumRequestsPerSessionPopover} className="icon-info text-info" />
+                        </div>
+                        <PopoverWithHover target={maximumRequestsPerSessionPopover} placement="top">
+                            <div className="flex-horizontal p-3">
+                                <div>
+                                    Set this number to restrict the number of requests (<strong>Reads</strong> &{" "}
+                                    <strong>Writes</strong>) per session in the client API.
+                                </div>
+                            </div>
+                        </PopoverWithHover>
+                    </div>
+                    <Row className="flex-grow-1 mt-1">
+                        <Col className="d-flex">
+                            <InputGroup>
+                                <InputGroupText>
+                                    <FormCheckbox
+                                        control={control}
+                                        name="maximumNumberOfRequestsEnabled"
+                                        disabled={!formValues.overrideConfig}
+                                    />
+                                </InputGroupText>
+                                <FormInput
+                                    type="number"
+                                    control={control}
+                                    name="maximumNumberOfRequestsValue"
+                                    placeholder="Default value is 30"
+                                    disabled={!formValues.maximumNumberOfRequestsEnabled || !formValues.overrideConfig}
+                                />
+                            </InputGroup>
                         </Col>
                     </Row>
                 </Card>
-
-                <Card className="mt-1">
-                    <Row className="p-2">
-                        <Col>
-                            <FormCheckbox control={control} name="useSessionContextEnabled">
-                                Use Session Context for Load Balancing
-                            </FormCheckbox>
+                <Card className="flex-column mt-1 p-3">
+                    <div className="d-flex flex-grow-1">
+                        <div className="md-label">
+                            Load Balancing <i ref={setSessionContextPopover} className="icon-info text-info" />
+                            <PopoverWithHover target={sessionContextPopover} placement="top">
+                                <div className="flex-horizontal p-3">
+                                    <div>
+                                        Allow client sessions to select topology by tag, so they'd be able to
+                                        load-balance their requests.
+                                    </div>
+                                </div>
+                            </PopoverWithHover>
+                        </div>
+                    </div>
+                    <Row className="mt-1 mb-4">
+                        <Col className="d-flex align-items-center gap-3">
+                            <InputGroup>
+                                <InputGroupText>
+                                    <FormCheckbox
+                                        control={control}
+                                        name="useSessionContextEnabled"
+                                        disabled={!formValues.overrideConfig}
+                                    />
+                                </InputGroupText>
+                                <FormSelect<LoadBalanceBehavior>
+                                    control={control}
+                                    name="loadBalanceBehaviorValue"
+                                    disabled={!formValues.useSessionContextEnabled || !formValues.overrideConfig}
+                                    options={ClientConfigurationUtils.getLoadBalanceBehaviorOptions()}
+                                />
+                            </InputGroup>
                         </Col>
-                        <Col>
-                            <Row>
-                                <Col>
+                    </Row>
+                    {formValues.useSessionContextEnabled && (
+                        <>
+                            <div className="d-flex flex-grow-1">
+                                <div className="md-label">
+                                    Hash seed{" "}
+                                    <i ref={setLoadBalanceSeedBehaviorPopover} className="icon-info text-info" />
+                                    <PopoverWithHover target={loadBalanceSeedBehaviorPopover} placement="top">
+                                        <div className="flex-horizontal p-3">
+                                            <div>Select a hash seed to fix the topology that clients would use.</div>
+                                        </div>
+                                    </PopoverWithHover>
+                                </div>
+                            </div>
+                            <Row className="mt-1 mb-4">
+                                <Col className="d-flex align-items-center gap-3">
                                     <FormSwitch
                                         control={control}
                                         name="loadBalancerSeedEnabled"
                                         color="primary"
-                                        disabled={!formValues.useSessionContextEnabled}
+                                        disabled={!formValues.useSessionContextEnabled || !formValues.overrideConfig}
                                         label="Seed"
                                     >
                                         Seed
                                     </FormSwitch>
-                                </Col>
-                                <Col>
-                                    <FormInput
-                                        type="number"
-                                        control={control}
-                                        name="loadBalancerSeedValue"
-                                        placeholder="Enter seed number"
-                                        disabled={!formValues.loadBalancerSeedEnabled}
-                                    />
+                                    <InputGroup>
+                                        <FormInput
+                                            type="number"
+                                            control={control}
+                                            name="loadBalancerSeedValue"
+                                            placeholder="Enter seed number"
+                                            disabled={!formValues.loadBalancerSeedEnabled || !formValues.overrideConfig}
+                                        />
+                                    </InputGroup>
                                 </Col>
                             </Row>
-                        </Col>
-                    </Row>
-
-                    <Row className="p-2">
-                        <Col>
-                            <FormCheckbox control={control} name="readBalanceBehaviorEnabled">
-                                Read balance behavior
-                            </FormCheckbox>
-                        </Col>
-                        <Col>
-                            <FormSelect<ReadBalanceBehavior>
-                                control={control}
-                                name="readBalanceBehaviorValue"
-                                disabled={!formValues.readBalanceBehaviorEnabled}
-                                options={ClientConfigurationUtils.getReadBalanceBehaviorOptions()}
-                            />
+                        </>
+                    )}
+                    <div className="d-flex flex-grow-1">
+                        <div className="md-label">
+                            Read balance behavior{" "}
+                            <i ref={setReadBalanceBehaviorPopover} className="icon-info text-info" />
+                            <PopoverWithHover target={readBalanceBehaviorPopover} placement="top">
+                                <div className="flex-horizontal p-3">
+                                    <div>
+                                        Set the load-balance method that the client will use when accessing a node with{" "}
+                                        <strong>Read</strong> requests. The method selected will also affect the
+                                        client's decision of which node to failover to in case of issues with the{" "}
+                                        <strong>Read</strong> request.
+                                    </div>
+                                </div>
+                            </PopoverWithHover>
+                        </div>
+                    </div>
+                    <Row className="mt-1">
+                        <Col className="d-flex">
+                            <InputGroup>
+                                <InputGroupText>
+                                    <FormCheckbox
+                                        control={control}
+                                        name="readBalanceBehaviorEnabled"
+                                        disabled={!formValues.overrideConfig}
+                                    />
+                                </InputGroupText>
+                                <FormSelect<ReadBalanceBehavior>
+                                    control={control}
+                                    name="readBalanceBehaviorValue"
+                                    disabled={!formValues.readBalanceBehaviorEnabled || !formValues.overrideConfig}
+                                    options={ClientConfigurationUtils.getReadBalanceBehaviorOptions()}
+                                />
+                            </InputGroup>
                         </Col>
                     </Row>
                 </Card>
