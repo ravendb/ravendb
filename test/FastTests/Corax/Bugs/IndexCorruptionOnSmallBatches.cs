1421b7eca7694b3eb24260138ee9688bfce46d9f
Ayende Rahien <ayende@ayende.com>
2023-05-04 11:31:38 +0300

    RavenDB-20268 Adding failing test for index corruption

---
+++ test/FastTests/Corax/Bugs/IndexCorruptionOnSmallBatches.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Google.Apis.Storage.v1;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FastTests.Corax.Bugs;
+
+public class IndexCorruptionOnSmallBatches : RavenTestBase
+{
+    public IndexCorruptionOnSmallBatches(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    private record Item(string Num);
+
+    [Fact]
+    public void ShouldNotHappen()
+    {
+        using var store = GetDocumentStore(new Options
+        {
+            ModifyDatabaseRecord = rec =>
+            {
+                rec.Settings["Indexing.Auto.SearchEngineType"]= "Corax";
+                rec.Settings["Indexing.Static.SearchEngineType"] = "Corax";
+                rec.Settings["Indexing.MapBatchSize"]= "128";
+            }
+        });
+
+        using (var s = store.OpenSession())
+        {
+            for (int i = 0; i < 1024; i++)
+            {
+                s.Store(new Item(i.ToString()));
+            }
+            s.SaveChanges();
+        }
+
+        using (var s = store.OpenSession())
+        {
+            _ = s.Query<Item>()
+                .OrderBy(x => x.Num)
+                .Take(50)
+                .ToList();
+        }
+        WaitForUserToContinueTheTest(store);
+    }
+}
