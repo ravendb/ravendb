61157715ee0708dafc1b62fc589f5fe69150d222
Maciej Aszyk <maciej@ravendb.net>
2023-05-08 14:18:09 +0200

    RavenDB-18198 Tokenize word in `Search` query primitive to build correct query tree.

--- src/Corax/IndexSearcher/IndexSearcher.Search.cs
+++ src/Corax/IndexSearcher/IndexSearcher.Search.cs
@@ -4,7 +4,9 @@ using System.Data;
 using System.IO;
 using System.Linq;
 using Corax.Mappings;
+using Corax.Pipeline;
 using Corax.Queries;
+using Sparrow;
 using Voron;
 
 namespace Corax;
@@ -23,54 +25,60 @@ public partial class IndexSearcher
         IQueryMatch searchQuery = null;
 
         List<Slice> termMatches = null;
-        foreach (var value in values)
+        foreach (var word in values)
         {
-            var termType = GetTermType(value);
-            (int startIncrement, int lengthIncrement) = termType switch
+            foreach (var token in GetTokens(word))
             {
-                Constants.Search.SearchMatchOptions.StartsWith => (0, -1),
-                Constants.Search.SearchMatchOptions.EndsWith => (1, 0),
-                Constants.Search.SearchMatchOptions.Contains => (1, -1),
-                Constants.Search.SearchMatchOptions.TermMatch => (0, 0),
-                _ => throw new InvalidExpressionException("Unknown flag inside Search match.")
-            };
+                var value = word.AsSpan(token.Offset, (int)token.Length);
+                var termType = GetTermType(value);
+                (int startIncrement, int lengthIncrement) = termType switch
+                {
+                    Constants.Search.SearchMatchOptions.StartsWith => (0, -1),
+                    Constants.Search.SearchMatchOptions.EndsWith => (1, 0),
+                    Constants.Search.SearchMatchOptions.Contains => (1, -1),
+                    Constants.Search.SearchMatchOptions.TermMatch => (0, 0),
+                    _ => throw new InvalidExpressionException("Unknown flag inside Search match.")
+                };
 
-            var termReadyToAnalyze = value.AsSpan(startIncrement, value.Length - startIncrement + lengthIncrement);
-            var analyzedTerm = EncodeAndApplyAnalyzer(field, termReadyToAnalyze, canReturnEmptySlice: true);
+                var termReadyToAnalyze = value.Slice(startIncrement, value.Length - startIncrement + lengthIncrement);
+                var analyzedTerm = EncodeAndApplyAnalyzer(field, termReadyToAnalyze, canReturnEmptySlice: true);
 
-            if (analyzedTerm.Size == 0)
-                continue; //skip empty results
+                if (analyzedTerm.Size == 0)
+                    continue; //skip empty results
             
-            if (termType is Constants.Search.SearchMatchOptions.TermMatch)
-            {
-                termMatches ??= new();
-                termMatches.Add(analyzedTerm);
-                continue;
+                if (termType is Constants.Search.SearchMatchOptions.TermMatch)
+                {
+                    termMatches ??= new();
+                    termMatches.Add(analyzedTerm);
+                    continue;
+                }
+            
+                var query = termType switch
+                {
+                    Constants.Search.SearchMatchOptions.TermMatch => throw new InvalidDataException($"{nameof(TermMatch)} is handled in different part of evaluator. This is a bug."),
+                    Constants.Search.SearchMatchOptions.StartsWith => StartWithQuery(field, analyzedTerm),
+                    Constants.Search.SearchMatchOptions.EndsWith => EndsWithQuery(field, analyzedTerm),
+                    Constants.Search.SearchMatchOptions.Contains => ContainsQuery(field, analyzedTerm),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+
+                if (searchQuery is null)
+                {
+                    searchQuery = query;
+                    continue;
+                }
+            
+                searchQuery = @operator switch
+                {
+                    Constants.Search.Operator.Or => Or(searchQuery, query),
+                    Constants.Search.Operator.And => And(searchQuery, query),
+                    _ => throw new ArgumentOutOfRangeException(nameof(@operator), @operator, null)
+                };
             }
-            
-            var query = termType switch
-            {
-                Constants.Search.SearchMatchOptions.TermMatch => throw new InvalidDataException($"{nameof(TermMatch)} is handled in different part of evaluator. This is a bug."),
-                Constants.Search.SearchMatchOptions.StartsWith => StartWithQuery(field, analyzedTerm),
-                Constants.Search.SearchMatchOptions.EndsWith => EndsWithQuery(field, analyzedTerm),
-                Constants.Search.SearchMatchOptions.Contains => ContainsQuery(field, analyzedTerm),
-                _ => throw new ArgumentOutOfRangeException()
-            };
 
-            if (searchQuery is null)
-            {
-                searchQuery = query;
-                continue;
-            }
             
-            searchQuery = @operator switch
-            {
-                Constants.Search.Operator.Or => Or(searchQuery, query),
-                Constants.Search.Operator.And => And(searchQuery, query),
-                _ => throw new ArgumentOutOfRangeException(nameof(@operator), @operator, null)
-            };
         }
-        
+
         if (termMatches?.Count > 0)
         {
             var termMatchesQuery = @operator switch
@@ -103,9 +111,9 @@ public partial class IndexSearcher
         return searchQuery ?? TermMatch.CreateEmpty(this, Allocator);
         
         
-        Constants.Search.SearchMatchOptions GetTermType(string termValue)
+        Constants.Search.SearchMatchOptions GetTermType(ReadOnlySpan<char> termValue)
         {
-            if (string.IsNullOrEmpty(termValue))
+            if (termValue.IsEmpty)
                 return Constants.Search.SearchMatchOptions.TermMatch;
             Constants.Search.SearchMatchOptions mode = default;
             if (termValue[0] == '*')
@@ -119,5 +127,33 @@ public partial class IndexSearcher
 
             return mode;
         }
+        
+        IEnumerable<Token> GetTokens(string source)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                yield return new Token() {Offset = 0, Length = 0};
+                yield break;
+            }
+            
+            //TODO This code in from `WhitespaceTokenizer`. We can optimize it later but for now it should be OK.
+            int i = 0;
+
+            while (i < source.Length)
+            {
+                while (i < source.Length && source[i] == ' ')
+                    i++;
+
+                int start = i;
+                while (i < source.Length && source[i] != ' ')
+                    i++;
+
+                if (start != i)
+                {
+                    yield return new Token() {Offset = start, Length = (uint)(i - start), Type = TokenType.Word};
+                }
+            } 
+        }
+        
     }
 }
