c13bca0cca7a5947d03a1cadc1b64f9a9b86a253
Damian Olszewski <damian@ravendb.net>
2023-05-10 14:32:07 +0200

    RavenDB-20304 Extract effective value logic

--- src/Raven.Studio/typescript/components/pages/database/settings/clientConfiguration/ClientDatabaseConfiguration.spec.tsx
+++ src/Raven.Studio/typescript/components/pages/database/settings/clientConfiguration/ClientDatabaseConfiguration.spec.tsx
@@ -5,18 +5,20 @@ import { composeStories } from "@storybook/react";
 
 const { WithGlobalConfiguration, WithoutGlobalConfiguration } = composeStories(stories);
 
+const seeEffectiveConfigurationText = "See effective configuration";
+
 describe("ClientDatabaseConfiguration", function () {
     it("can render with global config", async () => {
         const { screen } = rtlRender(<WithGlobalConfiguration />);
 
         expect(await screen.findByText(/Save/)).toBeInTheDocument();
-        expect(screen.queryByText(/Effective Configuration/)).toBeInTheDocument();
+        expect(screen.queryByText(seeEffectiveConfigurationText)).toBeInTheDocument();
     });
 
     it("can render without global config", async () => {
         const { screen } = rtlRender(<WithoutGlobalConfiguration />);
 
         expect(await screen.findByText(/Save/)).toBeInTheDocument();
-        expect(screen.queryByText(/Effective Configuration/)).not.toBeInTheDocument();
+        expect(screen.queryByText(seeEffectiveConfigurationText)).not.toBeInTheDocument();
     });
 });
