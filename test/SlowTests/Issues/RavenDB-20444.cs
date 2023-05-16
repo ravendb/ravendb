3bcc0ca28e7fd7fe8f9e84ae6334c6e5f3101239
Maciej Aszyk <maciej@ravendb.net>
2023-05-16 16:08:55 +0200

    RavenDB-20443 Apply boost to inner query inside `CachingQuery` instead of applying it to `CachingQuery`,

--- test/SlowTests/Issues/RavenDB-20444.cs
+++ test/SlowTests/Issues/RavenDB-20444.cs
@@ -1,5 +1,9 @@
+using System.Linq;
 using FastTests;
 using Raven.Client.Documents.Queries.Explanation;
+using Raven.Client.Documents.Session;
+using Raven.Client.Documents.Smuggler;
+using Raven.Server.Config;
 using Tests.Infrastructure;
 using Xunit;
 using Xunit.Abstractions;
@@ -36,6 +40,44 @@ public class RavenDB_20444 : RavenTestBase
 
         var rql = query.ToString();
         Assert.Equal("from 'Docs' where boost(search(StrVal, $p0) and (NumVal >= $p1 or NumVal = $p2), $p3) order by score(), NumVal as long desc include explanations()", rql);
+    }
+
+    [RavenTheory(RavenTestCategory.Querying)]
+    [RavenData(SearchEngineMode = RavenSearchEngineMode.Lucene, Data = new object[] {"true"})]
+    [RavenData(SearchEngineMode = RavenSearchEngineMode.Lucene, Data = new object[] { "false" })]
+    public void QueryCacheHasNoImpactOnCalculatingScoring(Options options, string queryClauseCacheDisabled)
+    {
+        using var store = GetDocumentStore(new Options()
+        {
+            ModifyDatabaseRecord = record =>
+            {
+                options.ModifyDatabaseRecord(record);
+                record.Settings[RavenConfiguration.GetKey(x => x.Indexing.QueryClauseCacheDisabled)] = queryClauseCacheDisabled;
+            }
+        });
+
+        store.Maintenance.Send(new CreateSampleDataOperation(DatabaseItemType.Documents | DatabaseItemType.Indexes ));
+
+        Indexes.WaitForIndexing(store);
+
+        using (var session = store.OpenSession())
+        {
+            var queryObject = session.Advanced.DocumentQuery<object>("Product/Search")
+                .Search("Name", "De*").Boost(0)
+                .AndAlso()
+                .OpenSubclause()
+                .WhereLessThanOrEqual("PricePerUnit", 20).Boost(100)
+                .OrElse()
+                .WhereGreaterThan("PricePerUnit", 20).Boost(1)
+                .CloseSubclause()
+                .OrderByScore()
+                .OrderByDescending("PricePerUnit", OrderingType.Double)
+                .IncludeExplanations(out var explanations)
+                .ToList();
+
+            Assert.Contains("0.499975", explanations.GetExplanations("products/58-A").First());
+            Assert.Contains("0.00499975", explanations.GetExplanations("products/38-A").First());
+        }
     }
 
 }
