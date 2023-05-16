579ca66ba0ee31c537991c3c209f32bd2d139db0
Pawel Pekrol <ppekrol@ravendb.net>
2023-05-16 13:42:44 +0200

    RavenDB-17649 added proxy to /databases/*/admin/studio-tasks/restart

---
+++ src/Raven.Server/Web/Studio/Processors/StudioDatabaseTasksHandlerProcessorForRestartDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Server.Documents;
+using Raven.Server.ServerWide;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Web.Http;
+
+namespace Raven.Server.Web.Studio.Processors;
+
+internal class StudioDatabaseTasksHandlerProcessorForRestartDatabase : AbstractStudioDatabaseTasksHandlerProcessorForRestartDatabase<DatabaseRequestHandler, DocumentsOperationContext>
+{
+    public StudioDatabaseTasksHandlerProcessorForRestartDatabase([NotNull] DatabaseRequestHandler requestHandler) : base(requestHandler)
+    {
+    }
+
+    protected override bool SupportsCurrentNode => true;
+
+    protected override async ValueTask HandleCurrentNodeAsync()
+    {
+        await ServerStore.DatabasesLandlord.RestartDatabase(RequestHandler.DatabaseName);
+    }
+
+    protected override Task HandleRemoteNodeAsync(ProxyCommand<object> command, OperationCancelToken token) => RequestHandler.ExecuteRemoteAsync(command, token.Token);
+}
---
+++ src/Raven.Server/Web/Studio/Sharding/Processors/ShardedStudioDatabaseTasksHandlerProcessorForRestartDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Server.Documents.Sharding.Handlers;
+using Raven.Server.ServerWide;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Web.Http;
+using Raven.Server.Web.Studio.Processors;
+
+namespace Raven.Server.Web.Studio.Sharding.Processors;
+
+internal class ShardedStudioDatabaseTasksHandlerProcessorForRestartDatabase : AbstractStudioDatabaseTasksHandlerProcessorForRestartDatabase<ShardedDatabaseRequestHandler, TransactionOperationContext>
+{
+    public ShardedStudioDatabaseTasksHandlerProcessorForRestartDatabase([NotNull] ShardedDatabaseRequestHandler requestHandler)
+        : base(requestHandler)
+    {
+    }
+
+    protected override bool SupportsCurrentNode => false;
+
+    protected override ValueTask HandleCurrentNodeAsync() => throw new NotSupportedException();
+
+    protected override Task HandleRemoteNodeAsync(ProxyCommand<object> command, OperationCancelToken token)
+    {
+        var shardNumber = GetShardNumber();
+        return RequestHandler.DatabaseContext.ShardExecutor.ExecuteSingleShardAsync(command, shardNumber, token.Token);
+    }
+}
