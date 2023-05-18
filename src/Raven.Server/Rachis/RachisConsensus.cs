eaf825930b1d660013361fc7c24cbab888c8a5fc
Karmel <karmel@ravendb.net>
2023-05-18 08:51:00 +0300

    RavenDB-20422 can get replication errors in tests infra

--- src/Raven.Server/Rachis/RachisConsensus.cs
+++ src/Raven.Server/Rachis/RachisConsensus.cs
@@ -561,7 +561,7 @@ namespace Raven.Server.Rachis
 
         protected abstract void InitializeState(ClusterOperationContext context, ClusterChanges changes);
 
-        public async Task WaitForState(RachisState rachisState, CancellationToken token)
+        public async Task<bool> WaitForState(RachisState rachisState, CancellationToken token)
         {
             while (token.IsCancellationRequested == false)
             {
@@ -569,10 +569,12 @@ namespace Raven.Server.Rachis
                 var task = _stateChanged.Task.WithCancellation(token);
 
                 if (CurrentState == rachisState)
-                    return;
+                    return true;
 
                 await task;
             }
+
+            return false;
         }
 
         public async Task WaitForLeaderChange(CancellationToken cts)
