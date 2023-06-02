3ad098eb9fe50e04634c8741fe2d0966c41e593a
Mateusz <mateusz@ravendb.net>
2023-06-02 16:18:42 +0200

    RavenDB-20612 Prettify Feature not available alert

--- src/Raven.Studio/typescript/components/common/EmptySet.tsx
+++ src/Raven.Studio/typescript/components/common/EmptySet.tsx
@@ -1,16 +1,20 @@
 ﻿import React, { ReactNode } from "react";
 import { Icon } from "./Icon";
+import IconName from "typings/server/icons";
 
 interface EmptySetProps {
     children?: ReactNode | ReactNode[];
+    icon?: IconName;
+    color?: string | "muted";
 }
 
 export function EmptySet(props: EmptySetProps) {
-    const { children } = props;
+    const { children, icon, color } = props;
+    const defaultIcon: IconName = "empty-set";
 
     return (
-        <div className="text-center mb-2">
-            <Icon icon="empty-set" color="muted" className="icon-xl" margin="m-0" />
+        <div className="empty-set text-center mb-2 mx-auto">
+            <Icon icon={icon || defaultIcon} color={color} className="icon-xl" margin="m-0" />
             <div className="lead">{children}</div>
         </div>
     );
