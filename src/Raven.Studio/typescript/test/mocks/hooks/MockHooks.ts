2a73f03e90f26c11d6642d3be3dd9e8d82a50ee8
Damian Olszewski <damian@ravendb.net>
2023-06-12 08:50:01 +0200

    RavenDB-20031 Mock dirty flag hook and add test

--- src/Raven.Studio/typescript/test/mocks/hooks/MockHooks.ts
+++ src/Raven.Studio/typescript/test/mocks/hooks/MockHooks.ts
@@ -1,9 +1,11 @@
 ﻿import MockChangesHook from "test/mocks/hooks/MockChangesHook";
 import MockEventsCollector from "test/mocks/hooks/MockEventsCollector";
+import MockDirtyFlagHook from "./MockDirtyFlagHook";
 
 class MockHooksContainer {
     useChanges = new MockChangesHook();
     useEventsCollector = new MockEventsCollector();
+    useDirtyFlag = new MockDirtyFlagHook();
 }
 
 export const mockHooks = new MockHooksContainer();
