e7dc29e1a47b3ed3915418ab04001fe384dc2f41
Gracjan Sadowicz <gracjan@ravendb.net>
2023-06-12 11:36:36 +0200

    RavenDB-20453 Link sqlkeys enum with migration config

--- src/Raven.Server/SqlMigration/Model/MigrationSettings.cs
+++ src/Raven.Server/SqlMigration/Model/MigrationSettings.cs
@@ -2,10 +2,18 @@
 
 namespace Raven.Server.SqlMigration.Model
 {
+    public enum EmbeddedDocumentSqlKeysStorage
+    {
+        None,
+        OnDocument,
+        OnMetadata,
+    }
+    
     public class MigrationSettings
     {
         public List<RootCollection> Collections { get; set; }
         public int BatchSize { get; set; } = 1000;
         public int? MaxRowsPerTable { get; set; }
+        public Dictionary<string, EmbeddedDocumentSqlKeysStorage> CollectionsEmbeddedReferencesSqlKeysConfigurations = new();
     }
 }
