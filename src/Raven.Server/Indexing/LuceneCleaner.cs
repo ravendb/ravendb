6fa1ee3885f769d1d6c7512aa948227a8337ea9b
Grisha Kotler <grisha@ayende.com>
2023-06-15 18:42:14 +0300

    RavenDB-20565 - when we are in a low memory state, dispose of the unmanaged memory that was allocated by Lucene

--- src/Raven.Server/Indexing/LuceneCleaner.cs
+++ src/Raven.Server/Indexing/LuceneCleaner.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 using Lucene.Net.Search;
 using Nito.AsyncEx;
+using Sparrow;
+using Sparrow.Logging;
 using Sparrow.LowMemory;
+using Sparrow.Utils;
 
 namespace Raven.Server.Indexing;
 
 public class LuceneCleaner : ILowMemoryHandler
 {
     private readonly AsyncReaderWriterLock _runningQueryLock = new();
+    private static readonly Logger Logger = LoggingSource.Instance.GetLogger<LuceneCleaner>("Memory");
 
     public LuceneCleaner()
     {
@@ -32,17 +37,30 @@ public class LuceneCleaner : ILowMemoryHandler
             }
         }
 
+        IDisposable cacheToDispose;
+        long unmanagedUsedBeforeInBytes = NativeMemory.TotalAllocatedMemoryByLucene;
+
         try
         {
             // PurgeAllCaches is replacing the cache with a new one (without actually releasing any memory).
             // When the GC will run, the finalizer of the Segments will be executed and release the unmanaged memory.
             // HOWEVER, this will happen when the managed memory is high enough to trigger a GC cycle.
             // Which is too late since we are already in a low memory state
-            FieldCache_Fields.DEFAULT.PurgeAllCaches();
+            cacheToDispose = FieldCache_Fields.DEFAULT.PurgeAllCaches();
         }
         finally
         {
             writeLock.Dispose();
+        }
+
+        Stopwatch sp = Logger.IsInfoEnabled ? Stopwatch.StartNew() : null;
+
+        cacheToDispose.Dispose();
+
+        if (sp != null && sp.ElapsedMilliseconds > 100)
+        {
+            Logger.Info($"Purged Lucene caches, took: {sp.ElapsedMilliseconds}ms, " +
+                        $"cleaned: {new Size(NativeMemory.TotalAllocatedMemoryByLucene - unmanagedUsedBeforeInBytes, SizeUnit.Bytes)}");
         }
     }
 
