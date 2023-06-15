9c2b1994d5af25701888e33c772c7a111552a81a
Grisha Kotler <grisha@ayende.com>
2023-06-15 18:39:43 +0300

    RavenDB-20565 - make sure that we aren't running a query while we clean the lucene cache

--- src/Raven.Server/Indexing/LuceneCleaner.cs
+++ src/Raven.Server/Indexing/LuceneCleaner.cs
@@ -1,10 +1,15 @@
-﻿using Lucene.Net.Search;
+﻿using System;
+using System.Threading;
+using Lucene.Net.Search;
+using Nito.AsyncEx;
 using Sparrow.LowMemory;
 
 namespace Raven.Server.Indexing;
 
 public class LuceneCleaner : ILowMemoryHandler
 {
+    private readonly AsyncReaderWriterLock _runningQueryLock = new();
+
     public LuceneCleaner()
     {
         LowMemoryNotification.Instance.RegisterLowMemoryHandler(this);
@@ -12,10 +17,41 @@ public class LuceneCleaner : ILowMemoryHandler
 
     public void LowMemory(LowMemorySeverity lowMemorySeverity)
     {
-        FieldCache_Fields.DEFAULT.PurgeAllCaches();
+        IDisposable writeLock;
+
+        using (var cts = new CancellationTokenSource())
+        {
+            cts.CancelAfter(10);
+            try
+            {
+                writeLock = _runningQueryLock.WriterLock(cts.Token);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        try
+        {
+            // PurgeAllCaches is replacing the cache with a new one (without actually releasing any memory).
+            // When the GC will run, the finalizer of the Segments will be executed and release the unmanaged memory.
+            // HOWEVER, this will happen when the managed memory is high enough to trigger a GC cycle.
+            // Which is too late since we are already in a low memory state
+            FieldCache_Fields.DEFAULT.PurgeAllCaches();
+        }
+        finally
+        {
+            writeLock.Dispose();
+        }
     }
 
     public void LowMemoryOver()
     {
+    }
+
+    public IDisposable EnterRunningQueryReadLock()
+    {
+        return _runningQueryLock.ReaderLock();
     }
 }
