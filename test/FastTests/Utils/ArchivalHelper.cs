250861ccd63942135689333ff44dbf76598208ed
Gracjan Sadowicz <gracjan@ravendb.net>
2023-06-21 11:10:56 +0200

    RavenDB-19501 Bugfix

--- test/FastTests/Utils/ArchivalHelper.cs
+++ test/FastTests/Utils/ArchivalHelper.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Operations.Archival;
-using Raven.Client.Documents.Operations.Archive;
 
 namespace FastTests.Utils
 {
