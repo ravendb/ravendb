9d4a0f8b54cc697b1eaae47cf6a261db59d95321
Pawel Pekrol <ppekrol@ravendb.net>
2023-06-23 15:53:46 +0200

    RavenDB-20721 added number of expiring and expired certificates to the SNMP

---
+++ src/Raven.Server/Monitoring/Snmp/Objects/Server/1.11/CertificateExpiredCount.cs
@@ -0,0 +1,30 @@
+using Lextm.SharpSnmpLib;
+using Raven.Server.ServerWide;
+
+namespace Raven.Server.Monitoring.Snmp.Objects.Server
+{
+    public class CertificateExpiredCount : ScalarObjectBase<Integer32>
+    {
+        private readonly ServerStore _store;
+
+        public CertificateExpiredCount(ServerStore store)
+            : base(SnmpOids.Server.CertificateExpiredCount)
+        {
+            _store = store;
+        }
+
+        protected override Integer32 GetData()
+        {
+            var count = 0;
+            var now = _store.Server.Time.GetUtcNow();
+
+            foreach (var notAfter in CertificateExpiringCount.GetAllCertificateExpirationDates(_store))
+            {
+                if (now > notAfter)
+                    count++;
+            }
+
+            return new Integer32(count);
+        }
+    }
+}
