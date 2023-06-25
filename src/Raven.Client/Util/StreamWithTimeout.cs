0df7f18a44f0963e69defff301a37aa59212760b
efrat@ayende.com <efrat@ayende.com>
2023-06-25 11:43:09 +0300

    RavenDB-17745 Send heartbeats from BulkInsertOperation if there is idle time

--- src/Raven.Client/Util/StreamWithTimeout.cs
+++ src/Raven.Client/Util/StreamWithTimeout.cs
@@ -7,8 +7,8 @@ namespace Raven.Client.Util
 {
     internal class StreamWithTimeout : Stream, IAsyncDisposable
     {
-        private static readonly TimeSpan DefaultWriteTimeout = TimeSpan.FromSeconds(120);
-        private static readonly TimeSpan DefaultReadTimeout = TimeSpan.FromSeconds(120);
+        internal static TimeSpan DefaultWriteTimeout = TimeSpan.FromSeconds(120);
+        internal static  TimeSpan DefaultReadTimeout = TimeSpan.FromSeconds(120);
 
         private readonly Stream _stream;
         private int _writeTimeout;
