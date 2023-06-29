331a632bf2afb126ac4a016e26a66f0763bb9355
shiranshalom <shiran@ravendb.net>
2023-06-29 16:00:42 +0300

    RavenDB-20339 - fixing PR comments#1

--- src/Raven.Server/ServerWide/Commands/Sharding/ShardedUpdateExternalReplicationStateCommand.cs
+++ src/Raven.Server/ServerWide/Commands/Sharding/ShardedUpdateExternalReplicationStateCommand.cs
@@ -1,5 +1,7 @@
-﻿using Raven.Server.Documents.Replication;
+﻿using System;
+using Raven.Server.Documents.Replication;
 using Raven.Server.ServerWide.Context;
+using Raven.Server.Utils;
 using Sparrow.Json;
 using Sparrow.Json.Parsing;
 
@@ -25,7 +27,37 @@ namespace Raven.Server.ServerWide.Commands.Sharding
 
         protected override BlittableJsonReaderObject GetUpdatedValue(long index, RawDatabaseRecord record, ClusterOperationContext context, BlittableJsonReaderObject existingValue)
         {
-            return context.ReadObject(ReplicationState.ToJson(), GetItemId());
+            if (existingValue == null)
+                return context.ReadObject(ReplicationState.ToJson(), GetItemId());
+
+            var existingState = JsonDeserializationCluster.ShardedExternalReplicationState(existingValue);
+            var existingStates = existingState.ReplicationStates;
+
+            foreach (var (shardedSourceName, shardedSourceState) in ReplicationState.ReplicationStates)
+            {
+                if (existingStates.TryGetValue(shardedSourceName, out var existingShardedSourceState) == false)
+                {
+                    existingStates[shardedSourceName] = shardedSourceState;
+                    continue;
+                }
+
+                existingShardedSourceState.LastSourceEtag = Math.Max(existingShardedSourceState.LastSourceEtag, shardedSourceState.LastSourceEtag);
+                existingShardedSourceState.LastSourceChangeVector = ChangeVectorUtils.MergeVectors(existingShardedSourceState.LastSourceChangeVector, shardedSourceState.LastSourceChangeVector);
+
+                foreach (var (shardedDestinationName, shardedDestinationState) in shardedSourceState.DestinationStates)
+                {
+                    if (existingShardedSourceState.DestinationStates.TryGetValue(shardedDestinationName, out var existingShardedDestinationState) == false)
+                    {
+                        existingShardedSourceState.DestinationStates[shardedDestinationName] = shardedDestinationState;
+                        continue;
+                    }
+
+                    existingShardedDestinationState.LastSentEtag = Math.Max(existingShardedDestinationState.LastSentEtag, shardedDestinationState.LastSentEtag);
+                    existingShardedDestinationState.DestinationChangeVector = ChangeVectorUtils.MergeVectors(existingShardedDestinationState.DestinationChangeVector, shardedDestinationState.DestinationChangeVector);
+                }
+            }
+
+            return context.ReadObject(existingState.ToJson(), GetItemId());
         }
 
         public override void FillJson(DynamicJsonValue json)
