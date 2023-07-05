c4140da0560b52d0680773b59f00d2e89beb8e1d
Damian Olszewski <damian@ravendb.net>
2023-07-05 11:29:04 +0200

    RavenDB-20465 Bind form to StudioGlobalConfiguration

--- src/Raven.Studio/typescript/components/pages/resources/manageServer/studioConfiguration/StudioGlobalConfiguration.stories.tsx
+++ src/Raven.Studio/typescript/components/pages/resources/manageServer/studioConfiguration/StudioGlobalConfiguration.stories.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { withStorybookContexts, withBootstrap5 } from "test/storybookTestUtils";
-import StudioGlobalConfiguration from "./StudioConfiguration";
+import StudioGlobalConfiguration from "./StudioGlobalConfiguration";
+import { todo } from "common/developmentHelper";
 
 export default {
     title: "Pages/ManageServer",
@@ -10,5 +11,6 @@ export default {
 } as ComponentMeta<typeof StudioGlobalConfiguration>;
 
 export const StudioConfiguration: ComponentStory<typeof StudioGlobalConfiguration> = () => {
+    todo("BugFix", "Damian", "Mock studioSettings.default.globalSettings()");
     return <StudioGlobalConfiguration />;
 };
