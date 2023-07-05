4e51018079d2ffa6808a3f92c786e4543f808222
Pawel Pekrol <ppekrol@ravendb.net>
2023-07-05 16:22:09 +0200

    RavnDB-7070 fixing test

--- test/SlowTests/Tests/TestsInheritanceTests.cs
+++ test/SlowTests/Tests/TestsInheritanceTests.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Reflection;
 using FastTests;
 using Raven.Server.Documents;
+using Raven.Server.Documents.Sharding.Handlers;
 using Raven.Server.Web;
 using Raven.TestDriver;
 using Tests.Infrastructure;
@@ -48,10 +49,10 @@ namespace SlowTests.Tests
         public void TestsShouldInheritFromRightBaseClasses()
         {
             var types = from assembly in GetAssemblies(typeof(TestsInheritanceTests).Assembly)
-                from test in GetAssemblyTypes(assembly)
-                where test.GetMethods().Any(x => x.GetCustomAttributes(typeof(FactAttribute), true).Count() != 0 || x.GetCustomAttributes(typeof(TheoryAttribute), true).Count() != 0)
-                where test.IsSubclassOf(typeof(ParallelTestBase)) == false && test.IsSubclassOf(typeof(RavenTestDriver)) == false && test.Namespace.StartsWith("EmbeddedTests") == false
-                select test;
+                        from test in GetAssemblyTypes(assembly)
+                        where test.GetMethods().Any(x => x.GetCustomAttributes(typeof(FactAttribute), true).Count() != 0 || x.GetCustomAttributes(typeof(TheoryAttribute), true).Count() != 0)
+                        where test.IsSubclassOf(typeof(ParallelTestBase)) == false && test.IsSubclassOf(typeof(RavenTestDriver)) == false && test.Namespace.StartsWith("EmbeddedTests") == false
+                        select test;
 
             var array = types.ToArray();
             if (array.Length == 0)
@@ -65,10 +66,11 @@ namespace SlowTests.Tests
         public void HandlersShouldNotInheritStraightFromRequestHandler()
         {
             var types = from assembly in GetAssemblies(typeof(TestsInheritanceTests).Assembly)
-                from handler in GetAssemblyTypes(assembly)
-                where handler != typeof(DatabaseRequestHandler) && handler != typeof(ServerRequestHandler)
-                where handler.IsSubclassOf(typeof(RequestHandler)) && handler.IsSubclassOf(typeof(ServerRequestHandler)) == false && handler.IsSubclassOf(typeof(DatabaseRequestHandler)) == false
-                select handler;
+                        from handler in GetAssemblyTypes(assembly)
+                        where handler.IsAbstract == false
+                        where handler != typeof(DatabaseRequestHandler) && handler != typeof(ServerRequestHandler) && handler != typeof(ShardedDatabaseRequestHandler)
+                        where handler.IsSubclassOf(typeof(RequestHandler)) && handler.IsSubclassOf(typeof(ServerRequestHandler)) == false && handler.IsSubclassOf(typeof(DatabaseRequestHandler)) == false && handler.IsSubclassOf(typeof(ShardedDatabaseRequestHandler)) == false
+                        select handler;
 
             var array = types.ToArray();
             if (array.Length == 0)
