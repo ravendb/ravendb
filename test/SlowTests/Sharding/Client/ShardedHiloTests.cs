41fa9570231a727df77121d3a911d6df140e5b87
aviv <aviv@ayende.com>
2023-07-13 18:17:04 +0300

    RavenDB-20791 : move ShardedHiloTests to slow test

--- test/SlowTests/Sharding/Client/ShardedHiloTests.cs
+++ test/SlowTests/Sharding/Client/ShardedHiloTests.cs
@@ -8,7 +8,7 @@ using Tests.Infrastructure;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace FastTests.Sharding.Client
+namespace SlowTests.Sharding.Client
 {
     public class ShardedHiloTests : ReplicationTestBase
     {
