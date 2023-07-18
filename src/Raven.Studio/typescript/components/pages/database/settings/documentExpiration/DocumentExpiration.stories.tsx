becbc61de6a7c37b73fd5e42bb43a33aced57cf2
Mateusz <mateusz@ravendb.net>
2023-07-18 13:18:33 +0200

    RavenDB-20536 Document Expiration React view

---
+++ src/Raven.Studio/typescript/components/pages/database/settings/documentExpiration/DocumentExpiration.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import { withStorybookContexts, withBootstrap5 } from "test/storybookTestUtils";
+import DocumentExpiration from "./DocumentExpiration";
+import { DatabasesStubs } from "test/stubs/DatabasesStubs";
+
+export default {
+    title: "Pages/Database/Settings",
+    component: DocumentExpiration,
+    decorators: [withStorybookContexts, withBootstrap5],
+} satisfies Meta<typeof DocumentExpiration>;
+
+export const DefaultDocumentExpiration: StoryObj<typeof DocumentExpiration> = {
+    name: "Document Expiration",
+    render: () => {
+        // const { databasesService } = mockServices;
+        // databasesService.withExpirationConfiguration();
+        return <DocumentExpiration db={DatabasesStubs.nonShardedClusterDatabase()}/>;
+    }
+}
