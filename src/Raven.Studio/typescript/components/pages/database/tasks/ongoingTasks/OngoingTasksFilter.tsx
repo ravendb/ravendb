e2e3e72548656beb1b7505843eb04b25c44559ea
Damian Olszewski <damian@ravendb.net>
2023-07-19 11:29:43 +0200

    RavenDB-20799 Add filter by name and type

---
+++ src/Raven.Studio/typescript/components/pages/database/tasks/ongoingTasks/OngoingTasksFilter.tsx
@@ -0,0 +1,82 @@
+import { Icon } from "components/common/Icon";
+import { MultiCheckboxToggle } from "components/common/MultiCheckboxToggle";
+import { InputItem } from "components/models/common";
+import produce from "immer";
+import React from "react";
+import { Input, Button } from "reactstrap";
+
+export type OngoingTaskFilterType =
+    | Extract<
+          StudioTaskType,
+          "Replication" | "Backup" | "Subscription" | "PullReplicationAsHub" | "PullReplicationAsSink"
+      >
+    | "ETL";
+
+export interface OngoingTasksFilterCriteria {
+    searchText: string;
+    types: OngoingTaskFilterType[];
+}
+
+interface OngoingTasksFilterProps {
+    filter: OngoingTasksFilterCriteria;
+    setFilter: (x: OngoingTasksFilterCriteria) => void;
+    filterByStatusOptions: InputItem<OngoingTaskFilterType>[];
+    tasksCount: number;
+}
+
+export default function OngoingTasksFilter(props: OngoingTasksFilterProps) {
+    const { filter, setFilter, filterByStatusOptions, tasksCount } = props;
+
+    const onSearchTextChange = (searchText: string) => {
+        setFilter(
+            produce(filter, (draft) => {
+                draft.searchText = searchText;
+            })
+        );
+    };
+
+    const onSearchStatusesChange = (types: OngoingTaskFilterType[]) => {
+        setFilter(
+            produce(filter, (draft) => {
+                draft.types = types;
+            })
+        );
+    };
+
+    return (
+        <div className="d-flex flex-wrap flex-grow align-items-end gap-3 mb-3">
+            <div className="flex-grow">
+                <div className="small-label ms-1 mb-1">Filter by name</div>
+                <div className="clearable-input">
+                    <Input
+                        type="text"
+                        accessKey="/"
+                        placeholder="e.g. MyPeriodicBackupTask"
+                        title="Filter ongoing tasks"
+                        className="filtering-input"
+                        value={filter.searchText}
+                        onChange={(e) => onSearchTextChange(e.target.value)}
+                    />
+                    {filter.searchText && (
+                        <div className="clear-button">
+                            <Button color="secondary" size="sm" onClick={() => onSearchTextChange("")}>
+                                <Icon icon="clear" margin="m-0" />
+                            </Button>
+                        </div>
+                    )}
+                </div>
+            </div>
+            <div>
+                <MultiCheckboxToggle
+                    inputItems={filterByStatusOptions}
+                    label="Filter by type"
+                    selectedItems={filter.types}
+                    setSelectedItems={onSearchStatusesChange}
+                    selectAll
+                    selectAllLabel="All"
+                    selectAllCount={tasksCount}
+                />
+            </div>
+        </div>
+    );
+}
