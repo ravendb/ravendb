eb5849d1f1527304e9aa055709f3e3a1549f91eb
efrat@ayende.com <efrat@ayende.com>
2023-07-24 15:23:44 +0300

    RavenDB-17745 fix exceptions

---
+++ src/Raven.Client/Exceptions/Documents/BulkInsert/BulkInsertInvalidOperationException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Raven.Client.Exceptions.Documents.BulkInsert
+{
+    public class BulkInsertInvalidOperationException : BulkInsertClientException
+    {
+        public BulkInsertInvalidOperationException(string message) : base(message)
+        {
+        }
+
+        public BulkInsertInvalidOperationException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
--- test/SlowTests/Issues/RavenDB-17745.cs
+++ test/SlowTests/Issues/RavenDB-17745.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
 using FastTests;
-using Raven.Client.Documents.BulkInsert;
 using Raven.Client.Util;
 using SlowTests.Core.Utils.Entities;
+using Tests.Infrastructure;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -15,62 +15,77 @@ namespace SlowTests.Issues
         {
         }
 
-        [Fact]
+        [RavenFact(RavenTestCategory.BulkInsert)]
         public async Task BulkInsertWithDelay()
         {
             using (var store = GetDocumentStore())
             {
-                StreamWithTimeout.DefaultWriteTimeout = TimeSpan.FromSeconds(20);
-                StreamWithTimeout.DefaultReadTimeout = TimeSpan.FromSeconds(20);
-                var bulk = store.BulkInsert();
-
-                await Task.Delay(StreamWithTimeout.DefaultWriteTimeout + TimeSpan.FromSeconds(5));
-                bulk.Store(new User { Name = "Daniel" }, "users/1");
-                bulk.Store(new User { Name = "Yael" }, "users/2");
-
-                await Task.Delay(StreamWithTimeout.DefaultWriteTimeout + TimeSpan.FromSeconds(5));
-                bulk.Store(new User { Name = "Ido" }, "users/3");
-                await Task.Delay(StreamWithTimeout.DefaultWriteTimeout + TimeSpan.FromSeconds(5));
-                bulk.Dispose();
-
-                using (var session = store.OpenSession())
+                try
                 {
-                    var user = session.Load<User>("users/1");
-                    Assert.NotNull(user);
-                    Assert.Equal("Daniel", user.Name);
+                    StreamWithTimeout.DefaultWriteTimeout = TimeSpan.FromSeconds(20);
+                    StreamWithTimeout.DefaultReadTimeout = TimeSpan.FromSeconds(20);
+                    var bulk = store.BulkInsert();
 
-                    user = session.Load<User>("users/2");
-                    Assert.NotNull(user);
-                    Assert.Equal("Yael", user.Name);
+                    await Task.Delay(StreamWithTimeout.DefaultWriteTimeout + TimeSpan.FromSeconds(5));
+                    bulk.Store(new User { Name = "Daniel" }, "users/1");
+                    bulk.Store(new User { Name = "Yael" }, "users/2");
 
-                    user = session.Load<User>("users/3");
-                    Assert.NotNull(user);
-                    Assert.Equal("Ido", user.Name);
+                    await Task.Delay(StreamWithTimeout.DefaultWriteTimeout + TimeSpan.FromSeconds(5));
+                    bulk.Store(new User { Name = "Ido" }, "users/3");
+                    await Task.Delay(StreamWithTimeout.DefaultWriteTimeout + TimeSpan.FromSeconds(5));
+                    bulk.Dispose();
+
+                    using (var session = store.OpenSession())
+                    {
+                        var user = session.Load<User>("users/1");
+                        Assert.NotNull(user);
+                        Assert.Equal("Daniel", user.Name);
+
+                        user = session.Load<User>("users/2");
+                        Assert.NotNull(user);
+                        Assert.Equal("Yael", user.Name);
+
+                        user = session.Load<User>("users/3");
+                        Assert.NotNull(user);
+                        Assert.Equal("Ido", user.Name);
+                    }
+                }
+                finally
+                {
+                    StreamWithTimeout.DefaultWriteTimeout = TimeSpan.FromSeconds(120);
+                    StreamWithTimeout.DefaultReadTimeout = TimeSpan.FromSeconds(120);
                 }
             }
         }
-        [Fact]
+        [RavenFact(RavenTestCategory.BulkInsert)]
         public async Task StartStoreInTheMiddleOfAnHeartbeat()
         {
             using (var store = GetDocumentStore())
             {
-                StreamWithTimeout.DefaultWriteTimeout = TimeSpan.FromSeconds(20);
-                var bulk = store.BulkInsert();
-
-                bulk.ForTestingPurposesOnly().StartStore = () =>
+                try
                 {
-                    Task.Run(() => bulk.Store(new User { Name = "Daniel" }, "users/1"));
-                };
+                    StreamWithTimeout.DefaultWriteTimeout = TimeSpan.FromSeconds(20);
+                    var bulk = store.BulkInsert();
 
-                await Task.Delay(StreamWithTimeout.DefaultWriteTimeout + TimeSpan.FromSeconds(5));
+                    bulk.ForTestingPurposesOnly().StartStore = () =>
+                    {
+                        Task.Run(() => bulk.Store(new User { Name = "Daniel" }, "users/1"));
+                    };
 
-                bulk.Dispose();
+                    await Task.Delay(StreamWithTimeout.DefaultWriteTimeout + TimeSpan.FromSeconds(5));
 
-                using (var session = store.OpenSession())
+                    bulk.Dispose();
+
+                    using (var session = store.OpenSession())
+                    {
+                        var user = session.Load<User>("users/1");
+                        Assert.NotNull(user);
+                        Assert.Equal("Daniel", user.Name);
+                    }
+                }
+                finally
                 {
-                    var user = session.Load<User>("users/1");
-                    Assert.NotNull(user);
-                    Assert.Equal("Daniel", user.Name);
+                    StreamWithTimeout.DefaultWriteTimeout = TimeSpan.FromSeconds(120);
                 }
             }
         }
