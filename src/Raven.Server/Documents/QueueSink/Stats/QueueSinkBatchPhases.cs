8c5b59d65fa8ce13b063cecd4c723cfa1a877685
Arkadiusz Palinski <arek@ravendb.net>
2023-08-02 15:04:22 +0200

    RavenDB-16201 Support for stats in Ongoing task stats - continuation

---
+++ src/Raven.Server/Documents/QueueSink/Stats/QueueSinkBatchPhases.cs
@@ -0,0 +1,8 @@
+﻿namespace Raven.Server.Documents.QueueSink.Stats;
+
+public class QueueSinkBatchPhases
+{
+    public const string Consume = "Consume";
+
+    public const string ScriptProcess = "ScriptProcessing";
+}
