85cec7efd9fa9482ef2f95a2661019e7a68280d7
Pawel Pekrol <ppekrol@ravendb.net>
2023-08-02 20:26:50 +0200

    RavenDB-7070 fix

--- src/Raven.Client/Documents/Session/CmpXchg.cs
+++ src/Raven.Client/Documents/Session/CmpXchg.cs
@@ -1,6 +1,6 @@
 namespace Raven.Client.Documents.Session
 {
-    internal sealed class CmpXchg : MethodCall
+    public sealed class CmpXchg : MethodCall
     {
         public static CmpXchg Value(string key)
         {
