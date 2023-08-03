3973809bd1d193af3351ac45610ae57b10fa06ae
Mateusz <mateusz@ravendb.net>
2023-08-03 15:21:42 +0200

    RavenDB-20534 Move Document Revisions to React

---
+++ src/Raven.Studio/typescript/components/pages/database/settings/documentRevisions/DocumentRevisions.tsx
@@ -0,0 +1,231 @@
+﻿import React, { useState } from "react";
+import { Button, Col, Input, Row } from "reactstrap";
+import { AboutViewAnchored, AboutViewHeading, AccordionItemWrapper } from "components/common/AboutView";
+import { Icon } from "components/common/Icon";
+import { Checkbox } from "components/common/Checkbox";
+import {
+    RichPanel,
+    RichPanelActions,
+    RichPanelDetailItem,
+    RichPanelDetails,
+    RichPanelHeader,
+    RichPanelInfo,
+    RichPanelName,
+    RichPanelSelect,
+    RichPanelStatus,
+} from "components/common/RichPanel";
+import { HrHeader } from "components/common/HrHeader";
+import { FlexGrow } from "components/common/FlexGrow";
+import { EmptySet } from "components/common/EmptySet";
+import EditRevision from "components/pages/database/settings/documentRevisions/EditRevision";
+import EnforceConfiguration from "components/pages/database/settings/documentRevisions/EnforceConfiguration";
+import { todo } from "common/developmentHelper";
+
+interface DocumentRevisionsProps {
+    toggle: () => void;
+    selected: boolean;
+    toggleSelection: () => void;
+}
+
+export default function DocumentRevisions(props: DocumentRevisionsProps) {
+    const { selected, toggleSelection } = props;
+
+    const [isEditRevisionModalOpen, setIsEditRevisionModalOpen] = useState(false);
+    const toggleEditRevisionModal = () => setIsEditRevisionModalOpen(!isEditRevisionModalOpen);
+
+    const [isNewRevisionModalOpen, setIsNewRevisionModalOpen] = useState(false);
+    const toggleNewRevisionModal = () => setIsNewRevisionModalOpen(!isNewRevisionModalOpen);
+
+    const [isEnforceConfigurationModalOpen, setIsEnforceConfigurationModalOpen] = useState(false);
+    const toggleEnforceConfigurationModal = () => setIsEnforceConfigurationModalOpen(!isEnforceConfigurationModalOpen);
+    const handleConfirm = async () => {
+        // Logic you want to execute when the user confirms the action
+    };
+
+    todo("Feature", "Damian", "Add logic");
+    todo("Feature", "Damian", "Configure forms");
+    todo("Feature", "ANY", "Connect SelectionActions component");
+    todo("Feature", "ANY", "Component for limit revisions by age inputs (dd/hh/mm/ss)");
+    todo("Feature", "Matteo", "Add the Revert revisions view");
+    todo("Feature", "Damian", "Connect to studio");
+    todo("Other", "Danielle", "Text for About this view");
+    todo("Other", "ANY", "Test the view");
+
+    return (
+        <div className="content-margin">
+            <EditRevision
+                isOpen={isEditRevisionModalOpen}
+                toggle={toggleEditRevisionModal}
+                onConfirm={handleConfirm}
+                taskType="edit"
+                configType="defaultDocument"
+            />
+            <EditRevision
+                isOpen={isNewRevisionModalOpen}
+                toggle={toggleNewRevisionModal}
+                onConfirm={handleConfirm}
+                taskType="new"
+                configType="collectionSpecific"
+            />
+            <EnforceConfiguration
+                isOpen={isEnforceConfigurationModalOpen}
+                toggle={toggleEnforceConfigurationModal}
+                onConfirm={handleConfirm}
+            />
+            <Col xxl={12}>
+                <Row className="gy-sm">
+                    <Col>
+                        <AboutViewHeading title="Document Revisions" icon="revisions" />
+                        <div className="d-flex gap-2">
+                            <Button color="primary">
+                                <Icon icon="save" />
+                                Save
+                            </Button>
+                            <FlexGrow />
+                            <Button color="secondary">
+                                <Icon icon="revert-revisions" />
+                                Revert revisions
+                            </Button>
+                            <Button color="secondary" onClick={toggleEnforceConfigurationModal}>
+                                <Icon icon="rocket" />
+                                Enforce configuration
+                            </Button>
+                        </div>
+                        <div className="mt-5">
+                            <Checkbox
+                                selected={selected}
+                                indeterminate={null}
+                                toggleSelection={toggleSelection}
+                                color="primary"
+                                title="Select all or none"
+                                size="lg"
+                            >
+                                <span className="small-label">Select All</span>
+                            </Checkbox>
+                        </div>
+                        <div className="mt-5">
+                            <HrHeader
+                                right={
+                                    <Button
+                                        color="info"
+                                        size="sm"
+                                        className="rounded-pill"
+                                        onClick={toggleEditRevisionModal}
+                                    >
+                                        Add new
+                                    </Button>
+                                }
+                            >
+                                <Icon icon="default" />
+                                Defaults
+                            </HrHeader>
+                            <RichPanel className="flex-row with-status">
+                                <RichPanelStatus color="success">Enabled</RichPanelStatus>
+                                <div className="flex-grow-1">
+                                    <RichPanelHeader>
+                                        <RichPanelInfo>
+                                            <RichPanelSelect>
+                                                <Input type="checkbox" checked={selected} onChange={toggleSelection} />
+                                            </RichPanelSelect>
+                                            <RichPanelName>Default Name</RichPanelName>
+                                        </RichPanelInfo>
+                                        <RichPanelActions>
+                                            <Button color="secondary">
+                                                <Icon icon="disable" />
+                                                Disable
+                                            </Button>
+                                            <Button color="secondary" onClick={toggleEditRevisionModal}>
+                                                <Icon icon="edit" margin="m-0" />
+                                            </Button>
+                                            <Button color="danger">
+                                                <Icon icon="trash" margin="m-0" />
+                                            </Button>
+                                        </RichPanelActions>
+                                    </RichPanelHeader>
+                                    <RichPanelDetails>
+                                        <RichPanelDetailItem>
+                                            <Icon icon="empty-set" />
+                                            Purge revisions on document delete
+                                        </RichPanelDetailItem>
+                                        <RichPanelDetailItem
+                                            label={
+                                                <>
+                                                    <Icon icon="documents" />
+                                                    Keep
+                                                </>
+                                            }
+                                        >
+                                            Num of revisions
+                                        </RichPanelDetailItem>
+                                        <RichPanelDetailItem
+                                            label={
+                                                <>
+                                                    <Icon icon="clock" />
+                                                    Retention time
+                                                </>
+                                            }
+                                        >
+                                            Time count
+                                        </RichPanelDetailItem>
+                                        <RichPanelDetailItem
+                                            label={
+                                                <>
+                                                    <Icon icon="trash" />
+                                                    Delete on update
+                                                </>
+                                            }
+                                        >
+                                            Num of revisions
+                                        </RichPanelDetailItem>
+                                    </RichPanelDetails>
+                                </div>
+                            </RichPanel>
+                        </div>
+                        <div className="mt-5">
+                            <HrHeader
+                                right={
+                                    <Button
+                                        color="info"
+                                        size="sm"
+                                        className="rounded-pill"
+                                        onClick={toggleNewRevisionModal}
+                                    >
+                                        Add new
+                                    </Button>
+                                }
+                            >
+                                <Icon icon="documents" />
+                                Collections
+                            </HrHeader>
+                            <EmptySet>No collection specific configuration has been defined</EmptySet>
+                        </div>
+                    </Col>
+                    <Col sm={12} lg={4}>
+                        <AboutViewAnchored>
+                            <AccordionItemWrapper
+                                targetId="1"
+                                icon="about"
+                                color="info"
+                                description="Get additional info on what this feature can offer you"
+                                heading="About this view"
+                            >
+                                <p>
+                                    <strong>Document Revisions</strong> is a feature that allows developers to keep
+                                    track of changes made to a document over time. When a document is updated in
+                                    RavenDB, a new revision is automatically created, preserving the previous version of
+                                    the document. This is particularly useful for scenarios where historical data and
+                                    versioning are crucial.
+                                </p>
+                                <hr />
+                                <div className="small-label mb-2">useful links</div>
+                                <a href="https://ravendb.net/l/SOMRWC/6.0/Csharp" target="_blank">
+                                    <Icon icon="newtab" /> Docs - Document Revisions
+                                </a>
+                            </AccordionItemWrapper>
+                        </AboutViewAnchored>
+                    </Col>
+                </Row>
+            </Col>
+        </div>
+    );
+}
---
+++ src/Raven.Studio/typescript/components/pages/database/settings/documentRevisions/EditRevision.tsx
@@ -0,0 +1,195 @@
+﻿import { Icon } from "components/common/Icon";
+import React, { useEffect } from "react";
+import { Alert, Button, InputGroup, Label, Modal, ModalBody, ModalFooter } from "reactstrap";
+import { FormInput, FormSelect, FormSwitch } from "components/common/Form";
+import { useForm, useWatch } from "react-hook-form";
+import {
+    DocumentRevisionsFormData,
+    documentRevisionsYupResolver,
+} from "components/pages/database/settings/documentRevisions/DocumentRevisionsValidation";
+import { useDirtyFlag } from "hooks/useDirtyFlag";
+
+interface EditRevisionProps {
+    isOpen: boolean;
+    toggle: () => void;
+    onConfirm: () => Promise<void>;
+    configType: "defaultDocument" | "collectionSpecific";
+    taskType: "edit" | "new";
+}
+
+export default function EditRevision(props: EditRevisionProps) {
+    const { isOpen, toggle, configType, taskType } = props;
+    const { control, formState, reset, setValue } = useForm<DocumentRevisionsFormData>({
+        resolver: documentRevisionsYupResolver,
+        mode: "all",
+    });
+
+    const formValues = useWatch({ control: control });
+
+    const collectionOptions = [
+        { value: "option1", label: "Option 1" },
+        { value: "option2", label: "Option 2" },
+        { value: "option3", label: "Option 3" },
+    ];
+
+    useDirtyFlag(formState.isDirty);
+
+    useEffect(() => {
+        if (!formValues.PurgeOnDelete && formValues.PurgeOnDelete !== null) {
+            setValue("PurgeOnDelete", false, { shouldValidate: true });
+        }
+    }, [setValue]);
+
+    useEffect(() => {
+        if (!formValues.IsMinimumRevisionsToKeepEnabled && !formValues.IsMinimumRevisionAgeToKeepEnabled) {
+            setValue("IsMaximumRevisionsToDeleteUponDocumentUpdateEnabled", false);
+        }
+    }, [formValues.IsMinimumRevisionsToKeepEnabled, formValues.IsMinimumRevisionAgeToKeepEnabled, setValue]);
+
+    return (
+        <Modal isOpen={isOpen} toggle={toggle} wrapClassName="bs5" contentClassName="modal-border bulge-info">
+            <ModalBody className="vstack gap-2">
+                {(taskType === "edit" && (
+                    <h4>
+                        Edit{" "}
+                        {configType === "defaultDocument" ? (
+                            <span>default document revisions</span>
+                        ) : configType === "collectionSpecific" ? (
+                            <span>collection specific configuration</span>
+                        ) : null}
+                    </h4>
+                )) ||
+                    (taskType === "new" && (
+                        <h4>
+                            Add{" "}
+                            {configType === "defaultDocument" ? (
+                                <span>default document revisions</span>
+                            ) : configType === "collectionSpecific" ? (
+                                <span>collection specific configuration</span>
+                            ) : null}
+                        </h4>
+                    ))}
+                {configType === "collectionSpecific" && (
+                    <InputGroup className="gap-1 flex-wrap flex-column">
+                        <Label className="mb-0 md-label">Collection</Label>
+                        <FormSelect control={control} name="CollectionSpecificName" options={collectionOptions} />
+                    </InputGroup>
+                )}
+                <FormSwitch control={control} name="PurgeOnDelete">
+                    Purge revisions on document delete
+                </FormSwitch>
+                <FormSwitch control={control} name="IsMinimumRevisionsToKeepEnabled">
+                    Limit # of revisions to keep
+                </FormSwitch>
+                {formValues.IsMinimumRevisionsToKeepEnabled && (
+                    <InputGroup className="mb-2">
+                        <FormInput
+                            type="number"
+                            control={control}
+                            name="MinimumRevisionsToKeep"
+                            placeholder="Enter number of revisions to keep"
+                        />
+                    </InputGroup>
+                )}
+                <FormSwitch control={control} name="IsMinimumRevisionAgeToKeepEnabled">
+                    Limit # of revisions to keep by age
+                </FormSwitch>
+                {formValues.IsMinimumRevisionAgeToKeepEnabled && (
+                    <InputGroup className="mb-2 d-flex">
+                        <FormInput type="text" control={control} name="MinimumRevisionAgeToKeep" placeholder="Days" />
+                    </InputGroup>
+                )}
+                {(formValues.IsMinimumRevisionsToKeepEnabled || formValues.IsMinimumRevisionAgeToKeepEnabled) && (
+                    <>
+                        <FormSwitch control={control} name="IsMaximumRevisionsToDeleteUponDocumentUpdateEnabled">
+                            Set # of revisions to delete upon document update
+                        </FormSwitch>
+                        {formValues.IsMaximumRevisionsToDeleteUponDocumentUpdateEnabled && (
+                            <InputGroup className="mb-2">
+                                <FormInput
+                                    type="number"
+                                    control={control}
+                                    name="MaximumRevisionsToDeleteUponDocumentUpdate"
+                                    placeholder="Enter max revisions to delete (suggested 100)"
+                                />
+                            </InputGroup>
+                        )}
+                    </>
+                )}
+                <Alert color="info" className="mt-3">
+                    <ul className="m-0 p-0 vstack gap-1">
+                        <li>
+                            A revision will be created anytime a document is modified
+                            {!formValues.PurgeOnDelete && <span> or deleted</span>}.
+                        </li>
+                        {formValues.PurgeOnDelete ? (
+                            <li>When a document is deleted all its revisions will be removed.</li>
+                        ) : (
+                            <li>Revisions of a deleted document can be accessed in the Revisions Bin view.</li>
+                        )}
+                        {formValues.IsMinimumRevisionsToKeepEnabled && formValues.MinimumRevisionsToKeep && (
+                            <>
+                                <li>
+                                    {formValues.IsMinimumRevisionAgeToKeepEnabled &&
+                                    formValues.MinimumRevisionAgeToKeep ? (
+                                        <>
+                                            <span>At least</span> <strong>{formValues.MinimumRevisionsToKeep}</strong>{" "}
+                                            of the latest
+                                        </>
+                                    ) : (
+                                        <>
+                                            <span>
+                                                Only the latest <strong>{formValues.MinimumRevisionsToKeep}</strong>
+                                            </span>
+                                        </>
+                                    )}{" "}
+                                    {formValues.MinimumRevisionsToKeep === 1 ? (
+                                        <span>revision</span>
+                                    ) : (
+                                        <span>revisions</span>
+                                    )}{" "}
+                                    will be kept.
+                                </li>
+                                {formValues.IsMinimumRevisionAgeToKeepEnabled && formValues.MinimumRevisionAgeToKeep ? (
+                                    <li>
+                                        Older revisions will be removed if they exceed{" "}
+                                        <strong>{formValues.MinimumRevisionAgeToKeep}</strong> on next revision
+                                        creation.
+                                    </li>
+                                ) : (
+                                    <li>Older revisions will be removed on next revision creation.</li>
+                                )}
+                            </>
+                        )}
+                        {!formValues.IsMinimumRevisionsToKeepEnabled &&
+                            formValues.IsMinimumRevisionAgeToKeepEnabled &&
+                            formValues.MinimumRevisionAgeToKeep && (
+                                <li>
+                                    Revisions that exceed <strong>{formValues.MinimumRevisionAgeToKeep}</strong> will be
+                                    removed on next revision creation.
+                                </li>
+                            )}
+                        {formValues.IsMaximumRevisionsToDeleteUponDocumentUpdateEnabled &&
+                            formValues.MaximumRevisionsToDeleteUponDocumentUpdate && (
+                                <li>
+                                    A maximum of{" "}
+                                    <strong>{formValues.MaximumRevisionsToDeleteUponDocumentUpdate}</strong> revisions
+                                    will be deleted each time a document is updated, until the defined '# of revisions
+                                    to keep' limit is reached.
+                                </li>
+                            )}
+                    </ul>
+                </Alert>
+            </ModalBody>
+            <ModalFooter>
+                <Button color="secondary" onClick={toggle}>
+                    Cancel
+                </Button>
+                <Button color="success">
+                    <Icon icon="plus" />
+                    Add config
+                </Button>
+            </ModalFooter>
+        </Modal>
+    );
+}
