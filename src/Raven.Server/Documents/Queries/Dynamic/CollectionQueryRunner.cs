0e0005acfc86f06eba26b5a422b9710eeb6c77ad
Arkadiusz Palinski <arek@ravendb.net>
2023-08-04 12:00:10 +0200

    RavenDB-20821 Reducing memory usage when skipping huge number of compressed docs / revisions during replication. Fixing a couple of issues:
    - the memory fix is that when we dispose DocumentReplicationItem then we call ForgetAbout() which releases a decompression buffer
    - introducing TransactionForgetAboutDocumentEnumerator instead of calling ForgetAbout() from PulsedTransactionEnumerator() but only if we operate on items of Document type
    - disposing replication items immediately after writing them to a network stream instead of at the end of the batch
    - added missing disposal of items from _replicaAttachmentsStreams

--- src/Raven.Server/Documents/Queries/Dynamic/CollectionQueryRunner.cs
+++ src/Raven.Server/Documents/Queries/Dynamic/CollectionQueryRunner.cs
@@ -155,7 +155,7 @@ namespace Raven.Server.Documents.Queries.Dynamic
                 }
                 else
                 {
-                    enumerator = new PulsedTransactionEnumerator<Document, CollectionQueryResultsIterationState>(context.Documents,
+                    enumerator = new TransactionForgetAboutDocumentEnumerator(new PulsedTransactionEnumerator<Document, CollectionQueryResultsIterationState>(context.Documents,
                         state =>
                         {
                             query.Start = state.Start;
@@ -170,7 +170,7 @@ namespace Raven.Server.Documents.Queries.Dynamic
                         {
                             Start = query.Start,
                             Take = query.PageSize
-                        });
+                        }), context.Documents);
                 }
 
                 IncludeCountersCommand includeCountersCommand = null;
