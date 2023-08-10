46007437a6b76adbb69f73b2baa6344d1c639f60
aviv <aviv@ayende.com>
2023-08-10 16:09:55 +0300

    RavenDB-21028 : fix bug in WaitForCompletion(timeout) : cancellation token source is disposed and therefore timeout might not be respected - we should await the returned task because it's inside a using scope

--- test/SlowTests/Issues/RavenDB-21028.cs
+++ test/SlowTests/Issues/RavenDB-21028.cs
@@ -2,8 +2,11 @@
 using Xunit.Abstractions;
 using System;
 using FastTests;
+using Raven.Client.Documents.Operations.Backups;
 using Raven.Client.Documents.Smuggler;
 using Raven.Client.Exceptions;
+using Raven.Server.Documents.PeriodicBackup;
+using SlowTests.Core.Utils.Entities;
 using Tests.Infrastructure;
 using Xunit;
 
@@ -27,6 +30,44 @@ namespace SlowTests.Issues
 
             await Assert.ThrowsAsync<RavenException>(async () => await op.WaitForCompletionAsync(TimeSpan.FromSeconds(10)));
 
+        }
+
+        [RavenFact(RavenTestCategory.Smuggler)]
+        public async Task WaitForCompletionShouldRespectTimeout()
+        {
+            using var store = GetDocumentStore();
+            using (var session = store.OpenAsyncSession())
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    await session.StoreAsync(new User());
+                }
+
+                await session.SaveChangesAsync();
+            }
+
+            var path = NewDataPath();
+            var config = new BackupConfiguration
+            {
+                BackupType = BackupType.Backup,
+                LocalSettings = new LocalSettings
+                {
+                    FolderPath = path
+                }
+            };
+
+            var db = await GetDatabase(store.Database);
+
+            // hold backup execution 
+            var tcs = new TaskCompletionSource<object>();
+            db.PeriodicBackupRunner._forTestingPurposes ??= new PeriodicBackupRunner.TestingStuff();
+            db.PeriodicBackupRunner._forTestingPurposes.OnBackupTaskRunHoldBackupExecution = tcs;
+
+            // wait for completion should not hang
+            var operation = await store.Maintenance.SendAsync(new BackupOperation(config));
+            await Assert.ThrowsAsync<TimeoutException>(async () => await operation.WaitForCompletionAsync(TimeSpan.FromSeconds(5)));
+
+            tcs.TrySetResult(null);
         }
 
     }
