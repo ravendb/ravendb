8870344e0414c716734758f94ce520fc37dbc833
shaharhikri <shaharhikri@ravendb.net>
2023-08-15 15:36:07 +0300

    RavenDB-20628 - Addressed PR Comments

--- src/Raven.Client/ServerWide/Operations/BuildNumber.cs
+++ src/Raven.Client/ServerWide/Operations/BuildNumber.cs
@@ -12,18 +12,7 @@ namespace Raven.Client.ServerWide.Operations
 
         public string FullVersion { get; set; }
 
-        internal string GetCleanedFullVersion()
-        {
-            if (IsNightlyOrDev(BuildVersion) == false)
-                return FullVersion;
-
-            var index = FullVersion.IndexOf('-'); // if index is -1, FullVersion looks like that: '5.4.1', else it looks like that: '5.4.1-custom'
-            return index == -1 ? FullVersion : FullVersion.Remove(index); 
-        }
-        internal static bool IsNightlyOrDev(long buildVersion)
-        {
-            return buildVersion >= 50 && buildVersion < 60;
-        }
+        public string AssemblyVersion { get; set; }
 
         public override bool Equals(object obj)
         {
@@ -35,10 +24,11 @@ namespace Raven.Client.ServerWide.Operations
             if (other == null)
                 return false;
 
-            return string.Equals(ProductVersion, other.ProductVersion) && 
-                   BuildVersion == other.BuildVersion && 
-                   string.Equals(CommitHash, other.CommitHash) && 
-                   string.Equals(FullVersion, other.FullVersion);
+            return string.Equals(ProductVersion, other.ProductVersion) &&
+                   BuildVersion == other.BuildVersion &&
+                   string.Equals(CommitHash, other.CommitHash) &&
+                   string.Equals(FullVersion, other.FullVersion) &&
+                   string.Equals(AssemblyVersion, other.AssemblyVersion);
         }
 
         public override int GetHashCode()
@@ -49,6 +39,7 @@ namespace Raven.Client.ServerWide.Operations
                 hashCode = (hashCode * 397) ^ BuildVersion;
                 hashCode = (hashCode * 397) ^ (CommitHash != null ? CommitHash.GetHashCode() : 0);
                 hashCode = (hashCode * 397) ^ (FullVersion != null ? FullVersion.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (AssemblyVersion != null ? AssemblyVersion.GetHashCode() : 0);
                 return hashCode;
             }
         }
@@ -60,7 +51,8 @@ namespace Raven.Client.ServerWide.Operations
                 [nameof(ProductVersion)] = ProductVersion,
                 [nameof(BuildVersion)] = BuildVersion,
                 [nameof(CommitHash)] = CommitHash,
-                [nameof(FullVersion)] = FullVersion
+                [nameof(FullVersion)] = FullVersion,
+                [nameof(AssemblyVersion)] = AssemblyVersion
             };
         }
     }
