e487ba1525dbaa6487fe24184e94043ca2dfec93
Mateusz <mateusz@ravendb.net>
2023-08-16 10:42:51 +0200

    RavenDB-20970 Move Tombstones Alert to a separate component

---
+++ src/Raven.Studio/typescript/components/pages/database/settings/tombstones/TombstonesAlert.tsx
@@ -0,0 +1,17 @@
+﻿import React from "react";
+import { Alert } from "reactstrap";
+
+export default function TombstonesAlert() {
+    return (
+        <Alert color="info" className="mb-0">
+            <p>
+                By default, tombstones cleanup is scheduled to be carried out by the server every 5 minutes, unless
+                configured otherwise.
+            </p>
+            <p className="mb-0">
+                Upon clicking <strong>Force Cleanup</strong>, the action will be executed immediately - any tombstone
+                that can be removed will be deleted.
+            </p>
+        </Alert>
+    );
+}
