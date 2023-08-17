d08a83363078406c737d7395a7074b110c57d14b
Mateusz <mateusz@ravendb.net>
2023-08-17 15:49:07 +0200

    RavenDB-21129 Move Custom Sorters and Custom Analyzers to React

--- src/Raven.Studio/typescript/components/common/HrHeader.tsx
+++ src/Raven.Studio/typescript/components/common/HrHeader.tsx
@@ -1,6 +1,6 @@
 ﻿import React, { ReactNode } from "react";
 import classNames from "classnames";
-import { Badge } from "reactstrap";
+import { Badge, UncontrolledTooltip } from "reactstrap";
 
 import "./HrHeader.scss";
 
@@ -8,11 +8,15 @@ interface HrHeaderProps {
     children?: ReactNode | ReactNode[];
     right?: ReactNode | ReactNode[];
     className?: string;
-    count?: number;
+    count?: ReactNode | ReactNode[];
+    limit?: number;
 }
 
 export function HrHeader(props: HrHeaderProps) {
-    const { right, children, className, count, ...rest } = props;
+    const { right, children, className, count, limit, ...rest } = props;
+    const maxThreshold = 8;
+    const limitThreshold = Math.min(Math.ceil(Number(count) * 0.2), maxThreshold);
+    const countLimit = Math.abs(Number(count) - limit) <= limitThreshold;
 
     return (
         <div className="flex-horizontal align-items-center my-3">
@@ -21,13 +25,24 @@ export function HrHeader(props: HrHeaderProps) {
                     {children}
                 </h5>
             )}
-            {count && (
-                <Badge pill className="me-3">
+            {count !== 0 && (
+                <Badge
+                    pill
+                    className="me-3"
+                    id={limit ? "limitLabel" : null}
+                    color={count === limit ? "warning" : "secondary"}
+                >
                     {count}
+                    {limit && countLimit && <span> / {limit}</span>}
                 </Badge>
             )}
             <hr className="flex-grow-1 m-0" />
             {right && <div className="ms-3">{right}</div>}
+            {limit && countLimit && (
+                <UncontrolledTooltip target="limitLabel" placement="top">
+                    The limit for your current license is <strong>{limit}</strong>
+                </UncontrolledTooltip>
+            )}
         </div>
     );
 }
---
+++ src/Raven.Studio/typescript/components/pages/database/settings/customAnalyzers/CustomAnalyzers.stories.tsx
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import { withStorybookContexts, withBootstrap5 } from "test/storybookTestUtils";
+import CustomAnalyzers from "./CustomAnalyzers";
+import { DatabasesStubs } from "test/stubs/DatabasesStubs";
+import { mockServices } from "test/mocks/services/MockServices";
+
+export default {
+    title: "Pages/Database/Settings",
+    component: CustomAnalyzers,
+    decorators: [withStorybookContexts, withBootstrap5],
+} satisfies Meta<typeof CustomAnalyzers>;
+
+export const DefaultCustomAnalyzers: StoryObj<typeof CustomAnalyzers> = {
+    name: "Custom Analyzers",
+    render: () => {
+        return <CustomAnalyzers db={DatabasesStubs.nonShardedClusterDatabase()} />;
+    },
+};
