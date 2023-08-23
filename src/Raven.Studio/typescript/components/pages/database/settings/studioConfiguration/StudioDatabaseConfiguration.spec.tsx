89808adcffbcb36def957d66c9cb4702e80f8d96
Mateusz <mateusz@ravendb.net>
2023-08-23 14:12:19 +0200

    RavenDB-21114 License restricted Studio Configuration

--- src/Raven.Studio/typescript/components/pages/database/settings/studioConfiguration/StudioDatabaseConfiguration.spec.tsx
+++ src/Raven.Studio/typescript/components/pages/database/settings/studioConfiguration/StudioDatabaseConfiguration.spec.tsx
@@ -3,11 +3,16 @@ import React from "react";
 import { composeStories } from "@storybook/testing-react";
 import * as stories from "./StudioDatabaseConfiguration.stories";
 
-const { StudioConfiguration } = composeStories(stories);
+const { StudioConfiguration, LicenseRestricted } = composeStories(stories);
 
 describe("StudioDatabaseConfiguration", function () {
     it("can render", async () => {
         const { screen } = rtlRender(<StudioConfiguration />);
         expect(await screen.findByText("Go to Server-Wide Studio Configuration View")).toBeInTheDocument();
+    });
+    it("is license restricted", async () => {
+        const { screen } = rtlRender(<LicenseRestricted />);
+
+        expect(await screen.findByText(/Licensing/)).toBeInTheDocument();
     });
 });
