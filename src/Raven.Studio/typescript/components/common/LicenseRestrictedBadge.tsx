09e2614d3d8e648d9b16220c102ddf40750306bc
Mateusz <mateusz@ravendb.net>
2023-08-28 15:21:14 +0200

    RavenDB-21115 License restricted Periodic backups

---
+++ src/Raven.Studio/typescript/components/common/LicenseRestrictedBadge.tsx
@@ -0,0 +1,16 @@
+﻿import { Badge } from "reactstrap";
+import React from "react";
+import classNames from "classnames";
+
+interface LicenseRestrictedBadgeProps {
+    className?: string;
+    licenseRequired: string;
+}
+
+export default function LicenseRestrictedBadge({ className, licenseRequired }: LicenseRestrictedBadgeProps) {
+    return (
+        <Badge color="faded-primary" className={classNames("ms-2 license-restricted-badge", className)}>
+            {licenseRequired}
+        </Badge>
+    );
+}
