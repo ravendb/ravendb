3f85e62bf2ff57f44eb573cdc2e7479338547e1a
Maciej Aszyk <maciej@ravendb.net>
2023-08-29 15:02:30 +0200

    RavenDB-21177 Decode entries during PostingListRead in SortingMatch | Max checking -> move pointer to right place & decode before comparison

---
+++ test/SlowTests/Corax/RavenDB-21177.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using FastTests;
+using Raven.Client.Documents.Session;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Corax;
+
+public class RavenDB_21177 : RavenTestBase
+{
+    public RavenDB_21177(ITestOutputHelper output) : base(output)
+    {
+    }
+    
+    [Fact]
+    public void CoraxSortingPostingLists()
+    {
+        using var store = GetDocumentStore(Options.ForSearchEngine(RavenSearchEngineMode.Corax));
+        using (var bulk = store.BulkInsert())
+        {
+            for (int i = 0; i < 63_000; ++i)
+            {
+                bulk.Store(new Dto($"{i % 5}"));
+            }
+        }
+
+        using (var session = store.OpenSession())
+        {
+            var allDocsSortedByCorax = session.Advanced
+                .DocumentQuery<Dto>()
+                .WaitForNonStaleResults()
+                .OrderBy(nameof(Dto.Num), OrderingType.String)
+                .ToList();
+
+            var allDocsFromServer = session.Advanced.DocumentQuery<Dto>().ToList();
+           // WaitForUserToContinueTheTest(store);
+           Assert.Equal(63_000, allDocsSortedByCorax.Count); 
+           Assert.Equal(63_000, allDocsFromServer.Count); 
+
+           Assert.Equal(allDocsFromServer.Select(i => i.Num).OrderBy(i => i), allDocsSortedByCorax.Select(i => i.Num));
+        }
+    }
+    
+    private record Dto(string Num, string Id = null);
+}
