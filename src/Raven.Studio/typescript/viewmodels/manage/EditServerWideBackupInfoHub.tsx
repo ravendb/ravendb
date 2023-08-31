2cdf6d3242bbd5e900f5d97fa684efba00340b3f
Mateusz <mateusz@ravendb.net>
2023-08-31 14:18:25 +0200

    RavenDB-21154 Add Floating Info Hub to Server-Wide Tasks Views

---
+++ src/Raven.Studio/typescript/viewmodels/manage/EditServerWideBackupInfoHub.tsx
@@ -0,0 +1,32 @@
+﻿import AboutViewFloating, { AccordionItemWrapper } from "components/common/AboutView";
+import AccordionCommunityLicenseNotIncluded from "components/common/AccordionCommunityLicenseNotIncluded";
+import { licenseSelectors } from "components/common/shell/licenseSlice";
+import { useAppSelector } from "components/store";
+import React from "react";
+
+export function EditServerWideBackupInfoHub() {
+    const licenseType = useAppSelector(licenseSelectors.licenseType);
+
+    return (
+        <AboutViewFloating>
+            <AccordionItemWrapper
+                targetId="about"
+                icon="about"
+                color="info"
+                heading="About this view"
+                description="Get additional info on this feature"
+            >
+                <p>
+                    This is server-wide backup task edit view
+                </p>
+            </AccordionItemWrapper>
+            {licenseType === "Community" && (
+                <AccordionCommunityLicenseNotIncluded
+                    targetId="licensing"
+                    featureName="Server-Wide Backups"
+                    featureIcon="server-wide-backup"
+                />
+            )}
+        </AboutViewFloating>
+    );
+}
---
+++ src/Raven.Studio/typescript/viewmodels/manage/EditServerWideExternalReplicationInfoHub.tsx
@@ -0,0 +1,32 @@
+﻿import AboutViewFloating, { AccordionItemWrapper } from "components/common/AboutView";
+import AccordionCommunityLicenseNotIncluded from "components/common/AccordionCommunityLicenseNotIncluded";
+import { licenseSelectors } from "components/common/shell/licenseSlice";
+import { useAppSelector } from "components/store";
+import React from "react";
+
+export function EditServerWideExternalReplicationInfoHub() {
+    const licenseType = useAppSelector(licenseSelectors.licenseType);
+
+    return (
+        <AboutViewFloating>
+            <AccordionItemWrapper
+                targetId="about"
+                icon="about"
+                color="info"
+                heading="About this view"
+                description="Get additional info on this feature"
+            >
+                <p>
+                    This is server-wide external replication task edit view
+                </p>
+            </AccordionItemWrapper>
+            {licenseType === "Community" && (
+                <AccordionCommunityLicenseNotIncluded
+                    targetId="licensing"
+                    featureName="Server-Wide External Replication"
+                    featureIcon="server-wide-replication"
+                />
+            )}
+        </AboutViewFloating>
+    );
+}
