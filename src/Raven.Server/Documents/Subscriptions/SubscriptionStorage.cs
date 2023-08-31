04cabbfc9e665735310e39701cb350bc8777b98d
egor <egor@ravendb.net>
2023-08-31 16:37:03 +0300

    RavenDB-20602: skip checks in HandleDatabaseRecordChange if subscription is not active

--- src/Raven.Server/Documents/Subscriptions/SubscriptionStorage.cs
+++ src/Raven.Server/Documents/Subscriptions/SubscriptionStorage.cs
@@ -690,6 +690,9 @@ namespace Raven.Server.Documents.Subscriptions
                         continue;
                     }
 
+                    if (subscriptionStateKvp.Value.IsSubscriptionActive() == false)
+                        continue;
+
                     var subscriptionState = JsonDeserializationClient.SubscriptionState(subscriptionBlittable);
                     if (subscriptionState.Disabled)
                     {
