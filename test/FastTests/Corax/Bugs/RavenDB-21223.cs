bc2e7a10b240bb5e728b0c937d2ea2e67e05bbea
Maciej Aszyk <maciej@ravendb.net>
2023-09-01 14:25:32 +0200

    RavenDB-21223 In case when last free page is released we've to remove it's id from LastFreePageByPageLevelMetadata to avoid modifying non-existing page.

---
+++ test/FastTests/Corax/Bugs/RavenDB-21223.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FastTests.Voron;
+using Voron.Data.Containers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FastTests.Corax.Bugs
+{
+    public class RavenDB_21223 : StorageTest
+    {
+        public RavenDB_21223(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void ContainerAllocateDeleteAndAllocate()
+        {
+            List<long> toDelete = new();
+
+            {
+                using var wtx = Env.WriteTransaction();
+                var rootContainer = wtx.OpenContainer("TestContainer");
+                for (int i = 0; i < 1000; i++)
+                {
+                    var allocatedPage = Container.Allocate(wtx.LowLevelTransaction, rootContainer, 50, out var space);
+                    space.Fill(1);
+                }
+
+                wtx.Commit();
+            }
+
+
+            {
+                using var wtx = Env.WriteTransaction();
+                var rootContainer = wtx.OpenContainer("TestContainer");
+                for (int i = 0; i < 1000; i++)
+                {
+                    var allocatedPage = Container.Allocate(wtx.LowLevelTransaction, rootContainer, 50, out var space);
+                    space.Fill(1);
+                    toDelete.Add(allocatedPage);
+                }
+                foreach (var id in toDelete)
+                    Container.Delete(wtx.LowLevelTransaction, rootContainer, id);
+
+                var newPage = Container.Allocate(wtx.LowLevelTransaction, rootContainer, 50, out var allocatedSpace);
+                allocatedSpace.Fill(2);
+
+                wtx.Commit();
+            }
+        }
+    }
+}
