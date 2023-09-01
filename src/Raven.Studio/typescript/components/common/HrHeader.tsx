cceb2f8e2833f547388c9b5e0bf8a053e1d65256
Damian Olszewski <damian@ravendb.net>
2023-09-01 11:13:56 +0200

    RavenDB-21129 Add CounterBadge

--- src/Raven.Studio/typescript/components/common/HrHeader.tsx
+++ src/Raven.Studio/typescript/components/common/HrHeader.tsx
@@ -1,6 +1,6 @@
 ﻿import React, { ReactNode } from "react";
 import classNames from "classnames";
-import { Badge, UncontrolledTooltip } from "reactstrap";
+import { Badge } from "reactstrap";
 
 import "./HrHeader.scss";
 
@@ -9,14 +9,10 @@ interface HrHeaderProps {
     right?: ReactNode | ReactNode[];
     className?: string;
     count?: ReactNode | ReactNode[];
-    limit?: number;
 }
 
 export function HrHeader(props: HrHeaderProps) {
-    const { right, children, className, count, limit, ...rest } = props;
-    const maxThreshold = 8;
-    const limitThreshold = Math.min(Math.ceil(Number(count) * 0.2), maxThreshold);
-    const countLimit = Math.abs(Number(count) - limit) <= limitThreshold;
+    const { right, children, className, count, ...rest } = props;
 
     return (
         <div className="flex-horizontal align-items-center my-3">
@@ -26,23 +22,12 @@ export function HrHeader(props: HrHeaderProps) {
                 </h5>
             )}
             {count !== 0 && (
-                <Badge
-                    pill
-                    className="me-3"
-                    id={limit ? "limitLabel" : null}
-                    color={count === limit ? "warning" : "secondary"}
-                >
+                <Badge pill className="me-3" color={"secondary"}>
                     {count}
-                    {limit && countLimit && <span> / {limit}</span>}
                 </Badge>
             )}
             <hr className="flex-grow-1 m-0" />
             {right && <div className="ms-3">{right}</div>}
-            {limit && countLimit && (
-                <UncontrolledTooltip target="limitLabel" placement="top">
-                    The limit for your current license is <strong>{limit}</strong>
-                </UncontrolledTooltip>
-            )}
         </div>
     );
 }
