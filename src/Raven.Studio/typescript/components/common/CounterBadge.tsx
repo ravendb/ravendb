948d64f5fdd7e6ff512e40f93d7af7b03983c549
Damian Olszewski <damian@ravendb.net>
2023-09-01 11:13:56 +0200

    RavenDB-21129 Add CounterBadge

---
+++ src/Raven.Studio/typescript/components/common/CounterBadge.tsx
@@ -0,0 +1,54 @@
+﻿import React, { ReactNode } from "react";
+import classNames from "classnames";
+import { Badge, UncontrolledPopover } from "reactstrap";
+import { getLicenseLimitReachStatus } from "components/utils/licenseLimitsUtils";
+import { uniqueId } from "lodash";
+
+interface CounterBadgeProps {
+    count: number;
+    limit?: number;
+    limitMessage?: string | ReactNode | ReactNode[];
+    className?: string;
+    hideNotReached?: boolean;
+}
+
+export function CounterBadge(props: CounterBadgeProps) {
+    const { count, limit, limitMessage, className, hideNotReached } = props;
+
+    const limitReachStatus = getLicenseLimitReachStatus(count, limit);
+    const badgeId = "counterBadge" + uniqueId();
+
+    return (
+        <>
+            {limitReachStatus !== "notReached" ? (
+                <>
+                    <Badge
+                        pill
+                        color={limitReachStatus === "limitReached" ? "danger" : "warning"}
+                        className={classNames("text-dark", className)}
+                        id={badgeId}
+                    >
+                        {count} / {limit}
+                    </Badge>
+                    <UncontrolledPopover target={badgeId} trigger="hover" placement="top" className="bs5">
+                        {limitMessage ? (
+                            limitMessage
+                        ) : (
+                            <div className="p-2">
+                                License limit: <strong>{limit}</strong>
+                            </div>
+                        )}
+                    </UncontrolledPopover>
+                </>
+            ) : (
+                <>
+                    {!hideNotReached && (
+                        <Badge pill className={className}>
+                            {count}
+                        </Badge>
+                    )}
+                </>
+            )}
+        </>
+    );
+}
