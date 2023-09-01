75d5037f62dac7a5d5d03553d58dabd54ff210a4
Gracjan Sadowicz <gracjan@ravendb.net>
2023-09-01 12:14:08 +0200

    RavenDB-19501 Fix unconsistent behavior in faulty index definition

--- src/Raven.Server/Documents/Indexes/Errors/FaultyIndexDefinition.cs
+++ src/Raven.Server/Documents/Indexes/Errors/FaultyIndexDefinition.cs
@@ -10,7 +10,7 @@ namespace Raven.Server.Documents.Indexes.Errors
         private readonly IndexDefinition _definition;
 
         public FaultyIndexDefinition(string name, IEnumerable<string> collections, IndexLockMode lockMode, IndexPriority priority, IndexState state, IndexField[] mapFields, IndexDefinition definition)
-            : base(name, collections, lockMode, priority, state, mapFields, IndexVersion.CurrentVersion, definition.DeploymentMode, definition.ClusterState, definition.ArchivedDataProcessingBehavior.GetValueOrDefault())
+            : base(name, collections, lockMode, priority, state, mapFields, IndexVersion.CurrentVersion, definition.DeploymentMode, definition.ClusterState, definition.ArchivedDataProcessingBehavior)
         {
             _definition = definition;
         }
