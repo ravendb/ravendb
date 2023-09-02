02233c8c80affe9d13dfdd9a63504b083ea72547
Grisha Kotler <grisha@ravendb.net>
2023-09-02 22:24:46 +0300

    RavenDB-21237 - use ForgetAbout in a streaming query

--- src/Raven.Server/Documents/Queries/StreamDocumentQueryResult.cs
+++ src/Raven.Server/Documents/Queries/StreamDocumentQueryResult.cs
@@ -3,11 +3,14 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Raven.Server.ServerWide;
+using Raven.Server.ServerWide.Context;
 
 namespace Raven.Server.Documents.Queries
 {
     public class StreamDocumentQueryResult : StreamQueryResult<Document>
     {
+        private readonly DocumentsOperationContext _context;
+
         public override async ValueTask AddResultAsync(Document result, CancellationToken token)
         {
             if (HasAnyWrites() == false)
@@ -16,13 +19,16 @@ namespace Raven.Server.Documents.Queries
             using (result)
                 await GetWriter().AddResultAsync(result, token).ConfigureAwait(false);
 
+            _context.Transaction.InnerTransaction.ForgetAbout(result.StorageId);
+
             GetToken().Delay();
         }
 
-        public StreamDocumentQueryResult(HttpResponse response, IStreamQueryResultWriter<Document> writer, OperationCancelToken token) : base(response, writer, token)
+        public StreamDocumentQueryResult(HttpResponse response, IStreamQueryResultWriter<Document> writer, DocumentsOperationContext context, OperationCancelToken token) : base(response, writer, token)
         {
             if (response.HasStarted)
                 throw new InvalidOperationException("You cannot start streaming because response has already started.");
+            _context = context;
         }
     }
 }
