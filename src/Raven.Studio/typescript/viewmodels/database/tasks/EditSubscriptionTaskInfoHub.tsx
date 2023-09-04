53e577d9ed88c5684e84df87fc1110131f7c6a2c
danielle9897 <danielle.9897@gmail.com>
2023-09-04 16:06:47 +0300

    RavenDB-21235 Text for Subscription task

--- src/Raven.Studio/typescript/viewmodels/database/tasks/EditSubscriptionTaskInfoHub.tsx
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/EditSubscriptionTaskInfoHub.tsx
@@ -3,6 +3,7 @@ import AccordionLicenseLimited from "components/common/AccordionLicenseLimited";
 import { licenseSelectors } from "components/common/shell/licenseSlice";
 import { useAppSelector } from "components/store";
 import React from "react";
+import {Icon} from "components/common/Icon";
 
 export function EditSubscriptionTaskInfoHub() {
     const licenseType = useAppSelector(licenseSelectors.licenseType);
@@ -17,8 +18,29 @@ export function EditSubscriptionTaskInfoHub() {
                 description="Get additional info on this feature"
             >
                 <p>
-                    This is subscription task edit view
+                    Define a <strong>Subscription Query</strong> in this view to create a subscription-task on the server to which clients can subscribe.
                 </p>
+                <p>
+                    When a client opens a connection to this task, RavenDB sends all documents <strong>matching the defined query</strong> to the client for processing.
+                </p>
+                <p>
+                    This is an <strong>ongoing process</strong>, whenever a new or updated document matches the subscription query, it will also be sent to the client.
+                </p>
+                <p>
+                    Documents are sent to the client in <strong>batches</strong>.<br />
+                    The client processes a batch and receives the next one only after acknowledging the batch was processed.
+                </p>
+                <p>
+                    <ul>
+                        <li>The <strong>starting point</strong> from where to send the matching documents can be configured.</li>
+                        <li>You can <strong>test the subscription</strong> in this view to preview sample document results that will be sent.</li>
+                    </ul>
+                </p>
+                <hr />
+                <div className="small-label mb-2">useful links</div>
+                <a href="https://ravendb.net/l/I5TMCK/6.0/Csharp" target="_blank">
+                    <Icon icon="newtab" /> Docs - Subscription Task
+                </a>
             </AccordionItemWrapper>
             {licenseType === "Community" && (
                 <AccordionLicenseLimited
