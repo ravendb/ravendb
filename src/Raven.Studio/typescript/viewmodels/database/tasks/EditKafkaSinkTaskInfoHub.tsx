9709ff99e906c74fb8c29f4bce514772545ba0dc
danielle9897 <danielle.9897@gmail.com>
2023-09-05 13:48:46 +0300

    RavenDB-21235 Text for Kafka Sink task

--- src/Raven.Studio/typescript/viewmodels/database/tasks/EditKafkaSinkTaskInfoHub.tsx
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/EditKafkaSinkTaskInfoHub.tsx
@@ -17,7 +17,26 @@ export function EditKafkaSinkTaskInfoHub() {
                 description="Get additional info on this feature"
             >
                 <p>
-                    This is subscription task edit view
+                    Define a <strong>Kafka Sink ongoing-task</strong> in order to consume and process incoming messages from Kafka topics.
+                </p>
+                <p>
+                    Add one or more <strong>scripts</strong> that Load, Put, or Delete documents in RavenDB based on the incoming messages.
+                </p>
+                <p>
+                    <ul>
+                        <li className="margin-top">
+                            In the task definition:<br />
+                            Define the connection string (bootstrap servers and any additional connection options).
+                        </li>
+                        <li className="margin-top-xxs">
+                            In the script definition:<br />
+                            Define the Kafka topic the script will subscribe to.<br />
+                            The script can be tested to preview the resulting documents.
+                        </li>
+                        <li className="margin-top-xxs">
+                            Incoming messages are expected only as JSON.
+                        </li>
+                    </ul>    
                 </p>
             </AccordionItemWrapper>
             {licenseType === "Community" && (
