a864c3cf528fc2ae6c7d164089713428499c0967
danielle9897 <danielle.9897@gmail.com>
2023-09-05 14:21:47 +0300

    RavenDB-21235 Text for RabbitMQ Sink task

--- src/Raven.Studio/typescript/viewmodels/database/tasks/EditRabbitMqSinkTaskInfoHub.tsx
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/EditRabbitMqSinkTaskInfoHub.tsx
@@ -17,7 +17,26 @@ export function EditRabbitMqSinkTaskInfoHub() {
                 description="Get additional info on this feature"
             >
                 <p>
-                    This is subscription task edit view
+                    Define a <strong>RabbitMQ Sink ongoing-task</strong> in order to consume and process incoming messages from RabbitMQ queues.
+                </p>
+                <p>
+                    Add one or more <strong>scripts</strong> that Load, Put, or Delete documents in RavenDB based on the incoming messages.
+                </p>
+                <p>
+                    <ul>
+                        <li className="margin-top">
+                            In the task definition:<br />
+                            Define the connection string.
+                        </li>
+                        <li className="margin-top-xxs">
+                            In the script definition:<br />
+                            Define the RabbitMQ queue the script will subscribe to.<br />
+                            The script can be tested to preview the resulting documents.
+                        </li>
+                        <li className="margin-top-xxs">
+                            Incoming messages are expected only as JSON.
+                        </li>
+                    </ul>
                 </p>
             </AccordionItemWrapper>
             {licenseType === "Community" && (
