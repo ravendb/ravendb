766891308bc5fab5c83fb2b8d352a471db7f9eb5
Mateusz <mateusz@ravendb.net>
2023-09-07 10:04:54 +0200

    RavenDB-21128 License restrictions for Data Archival

---
+++ src/Raven.Studio/typescript/components/pages/database/settings/dataArchival/DataArchival.spec.tsx
@@ -0,0 +1,35 @@
+﻿import React from "react";
+import { composeStories } from "@storybook/react";
+import { rtlRender } from "test/rtlTestUtils";
+import * as stories from "./DataArchival.stories";
+import { DatabasesStubs } from "test/stubs/DatabasesStubs";
+
+const { DefaultDataArchival, LicenseRestricted } = composeStories(stories);
+
+describe("DataArchival", () => {
+    it("can render", async () => {
+        const { screen } = rtlRender(<DefaultDataArchival />);
+
+        expect(await screen.findByText("Enable Data Archival")).toBeInTheDocument();
+    });
+
+    it("can disable and set to null expiration frequency after disabling 'Enable Data Archival'", async () => {
+        const { screen, fireClick } = rtlRender(<DefaultDataArchival />);
+
+        const archiveFrequencyBefore = await screen.findByName("archiveFrequency");
+        expect(archiveFrequencyBefore).toBeEnabled();
+        expect(archiveFrequencyBefore).toHaveValue(DatabasesStubs.dataArchivalConfiguration().ArchiveFrequencyInSec);
+
+        await fireClick(screen.getByRole("checkbox", { name: "Enable Data Archival" }));
+
+        const archiveFrequencyAfter = screen.getByName("archiveFrequency");
+        expect(archiveFrequencyAfter).toBeDisabled();
+        expect(archiveFrequencyAfter).toHaveValue(null);
+    });
+
+    it("is license restricted", async () => {
+        const { screen } = rtlRender(<LicenseRestricted />);
+
+        expect(await screen.findByText(/Licensing/)).toBeInTheDocument();
+    });
+});
--- src/Raven.Studio/typescript/components/pages/database/settings/dataArchival/DataArchival.stories.tsx
+++ src/Raven.Studio/typescript/components/pages/database/settings/dataArchival/DataArchival.stories.tsx
@@ -4,9 +4,10 @@ import { withStorybookContexts, withBootstrap5 } from "test/storybookTestUtils";
 import DataArchival from "./DataArchival";
 import { DatabasesStubs } from "test/stubs/DatabasesStubs";
 import { mockServices } from "test/mocks/services/MockServices";
+import { mockStore } from "test/mocks/store/MockStore";
 
 export default {
-    title: "Pages/Database/Settings",
+    title: "Pages/Database/Settings/Data Archival",
     component: DataArchival,
     decorators: [withStorybookContexts, withBootstrap5],
 } satisfies Meta<typeof DataArchival>;
@@ -15,7 +16,20 @@ export const DefaultDataArchival: StoryObj<typeof DataArchival> = {
     name: "Data Archival",
     render: () => {
         const { databasesService } = mockServices;
+        const { license } = mockStore;
         databasesService.withDataArchivalConfiguration();
+        license.with_Enterprise();
+        return <DataArchival db={DatabasesStubs.nonShardedClusterDatabase()} />;
+    },
+};
+
+export const LicenseRestricted: StoryObj<typeof DataArchival> = {
+    name: "License Restricted",
+    render: () => {
+        const { databasesService } = mockServices;
+        const { license } = mockStore;
+        databasesService.withDataArchivalConfiguration();
+        license.with_Community();
         return <DataArchival db={DatabasesStubs.nonShardedClusterDatabase()} />;
     },
 };
