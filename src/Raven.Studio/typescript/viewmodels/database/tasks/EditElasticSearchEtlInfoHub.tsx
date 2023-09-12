2865571fdc60444ef17bcb2f15053aa0ca342a87
Mateusz <mateusz@ravendb.net>
2023-09-12 16:29:12 +0200

    RavenDB-21308 License restricted Elasticsearch ETL

---
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/EditElasticSearchEtlInfoHub.tsx
@@ -0,0 +1,45 @@
+﻿import AboutViewFloating, {AboutViewAnchored, AccordionItemWrapper} from "components/common/AboutView";
+import AccordionLicenseNotIncluded from "components/common/AccordionLicenseNotIncluded";
+import { licenseSelectors } from "components/common/shell/licenseSlice";
+import { useAppSelector } from "components/store";
+import React from "react";
+import {Icon} from "components/common/Icon";
+import {useRavenLink} from "hooks/useRavenLink";
+
+export function EditElasticSearchEtlInfoHub() {
+    const isEnterpriseOrDeveloper = useAppSelector(licenseSelectors.isEnterpriseOrDeveloper());
+    const elasticSearchEtlDocsLink = useRavenLink({ hash: "AHPBTX" });
+
+    return (
+        <AboutViewFloating defaultOpen={!isEnterpriseOrDeveloper}>
+            <AccordionItemWrapper
+                targetId="about"
+                icon="about"
+                color="info"
+                heading="About this view"
+                description="Get additional info on this feature"
+            >
+                <p>
+                    Text
+                </p>
+                <hr />
+                <div className="small-label mb-2">useful links</div>
+                <a href={elasticSearchEtlDocsLink} target="_blank">
+                    <Icon icon="newtab" /> Docs - Elasticsearch ETL
+                </a>
+            </AccordionItemWrapper>
+            <AboutViewAnchored
+                className="mt-2"
+                defaultOpen={isEnterpriseOrDeveloper ? null : "licensing"}
+            >
+                <AccordionLicenseNotIncluded
+                    targetId="licensing"
+                    featureName="Elasticsearch ETL"
+                    featureIcon="elastic-search-etl"
+                    checkedLicenses={["Enterprise"]}
+                    isLimited={!isEnterpriseOrDeveloper}
+                />
+            </AboutViewAnchored>
+        </AboutViewFloating>
+    );
+}
