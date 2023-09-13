05675f5a551837a32a11f754efac1d446a2d364a
Mateusz <mateusz@ravendb.net>
2023-09-13 11:30:19 +0200

    RavenDB-21308 License restricted Kafka ETL

---
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/EditKafkaEtlInfoHub.tsx
@@ -0,0 +1,45 @@
+﻿import AboutViewFloating, {AboutViewAnchored, AccordionItemWrapper} from "components/common/AboutView";
+import AccordionLicenseNotIncluded from "components/common/AccordionLicenseNotIncluded";
+import { licenseSelectors } from "components/common/shell/licenseSlice";
+import { useAppSelector } from "components/store";
+import React from "react";
+import {Icon} from "components/common/Icon";
+import {useRavenLink} from "hooks/useRavenLink";
+
+export function EditKafkaEtlInfoHub() {
+    const isEnterpriseOrDeveloper = useAppSelector(licenseSelectors.isEnterpriseOrDeveloper());
+    const kafkaEtlDocsLink = useRavenLink({ hash: "S45O2Y" });
+
+    return (
+        <AboutViewFloating defaultOpen={!isEnterpriseOrDeveloper}>
+            <AccordionItemWrapper
+                targetId="about"
+                icon="about"
+                color="info"
+                heading="About this view"
+                description="Get additional info on this feature"
+            >
+                <p>
+                    Text
+                </p>
+                <hr />
+                <div className="small-label mb-2">useful links</div>
+                <a href={kafkaEtlDocsLink} target="_blank">
+                    <Icon icon="newtab" /> Docs - Kafka ETL
+                </a>
+            </AccordionItemWrapper>
+            <AboutViewAnchored
+                className="mt-2"
+                defaultOpen={isEnterpriseOrDeveloper ? null : "licensing"}
+            >
+                <AccordionLicenseNotIncluded
+                    targetId="licensing"
+                    featureName="Kafka ETL"
+                    featureIcon="kafka-etl"
+                    checkedLicenses={["Enterprise"]}
+                    isLimited={!isEnterpriseOrDeveloper}
+                />
+            </AboutViewAnchored>
+        </AboutViewFloating>
+    );
+}
