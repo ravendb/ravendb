fe34d2561eeb3a4c0545c2688b4d0f709942464f
danielle9897 <danielle.9897@gmail.com>
2023-09-18 23:26:44 +0300

    RavenDB-21235 Text for Replication Hub

--- src/Raven.Studio/typescript/viewmodels/database/tasks/EditReplicationHubInfoHub.tsx
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/EditReplicationHubInfoHub.tsx
@@ -5,9 +5,11 @@ import React from "react";
 import { Icon } from "components/common/Icon";
 import FeatureAvailabilitySummaryWrapper from "components/common/FeatureAvailabilitySummary";
 import { featureAvailabilityEnterprise } from "components/utils/licenseLimitsUtils";
+import {useRavenLink} from "hooks/useRavenLink";
 
 export function EditReplicationHubInfoHub() {
     const isEnterpriseOrDeveloper = useAppSelector(licenseSelectors.isEnterpriseOrDeveloper);
+    const replicationHubDocsLink = useRavenLink({ hash: "NIH5LN" });
 
     return (
         <AboutViewFloating defaultOpen={isEnterpriseOrDeveloper ? null : "licensing"}>
@@ -18,10 +20,51 @@ export function EditReplicationHubInfoHub() {
                 heading="About this view"
                 description="Get additional info on this feature"
             >
-                <p>Text</p>
+                <div>
+                    Define a <strong>Replication Hub</strong> to replicate documents to and/or from multiple Replication
+                    Sink tasks that are defined in other RavenDB databases in other clusters:
+                    <ul className="margin-top-xxs">
+                        <li>A Replication Hub can serve many Sinks.</li>
+                        <li>The connection to the Hub is always initiated by the Sink.</li>
+                        <li>Only documents and their related data (attachments, revisions, counters & time series) will
+                            be replicated.
+                        </li>
+                    </ul>
+                </div>
+                <hr/>
+                <div>
+                    When running with a SECURE server:
+                    <ul>
+                        <li className="margin-top-xxs"><strong>Replication Access</strong>:
+                            <br/>
+                            You can set up multiple Replication Access definitions on the Hub task with
+                            an associated certificate, which will be used by the Sink tasks to authenticate and
+                            establish a connection with the Replication Hub.
+                        </li>
+                        <li className="margin-top-xxs"><strong>Filtered Replication</strong>:
+                            <br/>
+                            Within each Access definition, you can filter the documents that will be replicated by their
+                            ID paths.
+                        </li>
+                        <li className="margin-top-xxs">
+                            <strong>Sink to Hub Replication</strong> is only available with a secure server.
+                        </li>
+                    </ul>
+                </div>
+                <hr/>
+                <div>
+                    Task definition includes:
+                    <ul className="margin-top-xxs">
+                        <li>Setting the replication direction.</li>
+                        <li>The replication access definitions.</li>
+                        <li>Control whether deletions from Sink are executed on the Hub.</li>
+                        <li>An optional delay time for data replication.</li>
+                        <li>A responsible node to handle this task can be set.</li>
+                    </ul>
+                </div>
                 <hr />
                 <div className="small-label mb-2">useful links</div>
-                <a href="https://ravendb.net/l/NIH5LN/latest" target="_blank">
+                <a href={replicationHubDocsLink} target="_blank">
                     <Icon icon="newtab" /> Docs - Replication Hub
                 </a>
             </AccordionItemWrapper>
