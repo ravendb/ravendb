1541232e62954fb2a1317e04a255b569c4cc35bc
danielle9897 <danielle.9897@gmail.com>
2023-09-19 15:13:48 +0300

    RavenDB-21235 Text for Connection Strings

--- src/Raven.Studio/typescript/viewmodels/database/settings/ConnectionStringsInfoHub.tsx
+++ src/Raven.Studio/typescript/viewmodels/database/settings/ConnectionStringsInfoHub.tsx
@@ -17,7 +17,26 @@ export function ConnectionStringsInfoHub() {
                 heading="About this view"
                 description="Get additional info on this feature"
             >
-                <p>Text</p>
+                <div>
+                    <ul>
+                        <li>
+                            RavenDB is designed to interact with diverse data storage solutions via replication, ETL, or
+                            incoming data processing.
+                        </li>
+                        <li className="margin-top-xxs">
+                            From this view, you can manage all the connection strings that may be used when defining an
+                            ongoing-task per data storage.
+                        </li>
+                        <li className="margin-top-xxs">
+                            New connection strings that have been created within an ongoing-task view will also be
+                            listed here.
+                        </li>
+                        <li className="margin-top-xxs">
+                            Connection strings that are in use by ongoing-tasks cannot be deleted, as they are essential
+                            for task functionality and data access.
+                        </li>
+                    </ul>
+                </div>
             </AccordionItemWrapper>
             <FeatureAvailabilitySummaryWrapper
                 isUnlimited={isProfessionalOrAbove}
