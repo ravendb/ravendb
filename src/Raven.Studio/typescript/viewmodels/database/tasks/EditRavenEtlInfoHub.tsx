44c1b7cb8761c61133d33f3ba0cde13a8d833f14
danielle9897 <danielle.9897@gmail.com>
2023-09-19 21:53:40 +0300

    RavenDB-21235 Text for RavenDB ETL

--- src/Raven.Studio/typescript/viewmodels/database/tasks/EditRavenEtlInfoHub.tsx
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/EditRavenEtlInfoHub.tsx
@@ -20,7 +20,42 @@ export function EditRavenEtlInfoHub() {
                 heading="About this view"
                 description="Get additional info on this feature"
             >
-                <p>Text</p>
+                <p>
+                    A <strong>RavenDB ETL</strong> ongoing-task is an ETL (Extract, Transform & Load) process that
+                    transfers data from documents from this RavenDB database to another RavenDB database.
+                </p>
+                <ul>
+                    <li>The sent data can be filtered and modified by multiple transformation scripts that are added to
+                        the task.
+                    </li>
+                    <li className="margin-top-xxs">
+                        Custom logic for Attachments, Counters, Time series,
+                        <br/>
+                        and deletion behavior can also be applied.
+                        <br/>
+                        Revisions are not sent by the ETL process.
+                    </li>
+                    <li className="margin-top-xxs">
+                        Each script specifies the source collection(s) and the target collection(s) which may be
+                        different than the source.
+                    </li>
+                    <li className="margin-top-xxs">
+                        The scripts are executed whenever documents in the source database are created, modified, or
+                        deleted.
+                    </li>
+                    <li className="margin-top-xxs">
+                        You can test each script in this view to preview the resulting documents that will be sent.
+                    </li>
+                </ul>
+                <hr/>
+                <div>
+                    Task definition includes:
+                    <ul>
+                        <li>The transformation scripts definitions.</li>
+                        <li>A connection string with URLs of the target database servers.</li>
+                        <li>A responsible node to handle this task can be set.</li>
+                    </ul>
+                </div>
                 <hr />
                 <div className="small-label mb-2">useful links</div>
                 <a href={ravenDbEtlDocsLink} target="_blank">
