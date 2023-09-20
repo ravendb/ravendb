34f4b0992d297cc634cd890446283d3b08b70ed0
Mateusz <mateusz@ravendb.net>
2023-09-20 09:47:15 +0200

    RavenDB-21403 Point out missing features in Connection Strings view

--- src/Raven.Studio/typescript/viewmodels/database/settings/ConnectionStringsInfoHub.tsx
+++ src/Raven.Studio/typescript/viewmodels/database/settings/ConnectionStringsInfoHub.tsx
@@ -1,12 +1,54 @@
 ﻿import AboutViewFloating, { AccordionItemWrapper } from "components/common/AboutView";
-import FeatureAvailabilitySummaryWrapper from "components/common/FeatureAvailabilitySummary";
+import FeatureAvailabilitySummaryWrapper, {FeatureAvailabilityData} from "components/common/FeatureAvailabilitySummary";
 import { licenseSelectors } from "components/common/shell/licenseSlice";
 import { useAppSelector } from "components/store";
-import { featureAvailabilityProfessionalOrAbove } from "components/utils/licenseLimitsUtils";
 import React from "react";
 
 export function ConnectionStringsInfoHub() {
     const isProfessionalOrAbove = useAppSelector(licenseSelectors.isProfessionalOrAbove);
+    const featureAvailabilityData: FeatureAvailabilityData[] = [
+        {
+            featureName: "RavenDB ETL",
+            featureIcon: "ravendb-etl",
+            community: { value: false },
+            professional: { value: true },
+            enterprise: { value: true },
+        },{
+            featureName: "SQL ETL",
+            featureIcon: "sql-etl",
+            community: { value: false },
+            professional: { value: true },
+            enterprise: { value: true },
+        },
+        {
+            featureName: "OLAP ETL",
+            featureIcon: "olap-etl",
+            community: { value: false },
+            professional: { value: false },
+            enterprise: { value: true },
+        },
+        {
+            featureName: "Elasticsearch ETL",
+            featureIcon: "elastic-search-etl",
+            community: { value: false },
+            professional: { value: false },
+            enterprise: { value: true },
+        },
+        {
+            featureName: "Kafka ETL",
+            featureIcon: "kafka-etl",
+            community: { value: false },
+            professional: { value: false },
+            enterprise: { value: true },
+        },
+        {
+            featureName: "RabbitMQ ETL",
+            featureIcon: "rabbitmq-etl",
+            community: { value: false },
+            professional: { value: false },
+            enterprise: { value: true },
+        },
+    ];
 
     return (
         <AboutViewFloating defaultOpen={isProfessionalOrAbove ? null : "licensing"}>
@@ -21,7 +63,7 @@ export function ConnectionStringsInfoHub() {
             </AccordionItemWrapper>
             <FeatureAvailabilitySummaryWrapper
                 isUnlimited={isProfessionalOrAbove}
-                data={featureAvailabilityProfessionalOrAbove}
+                data={featureAvailabilityData}
             />
         </AboutViewFloating>
     );
