426d59059d6e558e053565449066f5a76899955d
danielle9897 <danielle.9897@gmail.com>
2023-09-21 14:15:44 +0300

    RavenDB-21235 Text for Kafka ETL &  RabbitMQ ETL

--- src/Raven.Studio/typescript/viewmodels/database/tasks/EditKafkaEtlInfoHub.tsx
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/EditKafkaEtlInfoHub.tsx
@@ -22,7 +22,46 @@ export function EditKafkaEtlInfoHub() {
                 heading="About this view"
                 description="Get additional info on this feature"
             >
-                <p>Text</p>
+                <p>
+                    A <strong>Kafka ETL</strong> ongoing-task is an ETL (Extract, Transform & Load) process
+                    that transfers data from this RavenDB database to topics of a Kafka broker.
+                </p>
+                <ul>
+                    <li>
+                        Data is extracted from documents only.
+                        <br/>
+                        Attachments, Counters, Time series, and Revisions are not sent.
+                    </li>
+                    <li className="margin-top-xxs">
+                        The sent data can be filtered and modified by multiple transformation JavaScript scripts that
+                        are added to the task.
+                    </li>
+                    <li className="margin-top-xxs">
+                        Each script specifies the RavenDB source collection(s) and the destination topic(s).
+                    </li>
+                    <li className="margin-top-xxs">
+                        The scripts are executed whenever documents in the source database are created or modified
+                        (excluding deletes).
+                    </li>
+                    <li className="margin-top-xxs">
+                        You can test each script in this view to preview the messages that will be sent by the ETL
+                        task to the destination.
+                    </li>
+                </ul>
+                <hr/>
+                <div>
+                    Task definition includes:
+                    <ul>
+                        <li>A connection string specifying Kafka&apos;s bootstrap servers.
+                            <br/>
+                            Connection options can be specified.
+                        </li>
+                        <li>The transformation scripts definitions.</li>
+                        <li>Per topic, select whether processed documents will be deleted from your RavenDB database.
+                        </li>
+                        <li>A responsible node to handle this task can be set.</li>
+                    </ul>
+                </div>
                 <hr />
                 <div className="small-label mb-2">useful links</div>
                 <a href={kafkaEtlDocsLink} target="_blank">
--- src/Raven.Studio/typescript/viewmodels/database/tasks/EditRabbitMqEtlInfoHub.tsx
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/EditRabbitMqEtlInfoHub.tsx
@@ -22,7 +22,45 @@ export function EditRabbitMqEtlInfoHub() {
                 heading="About this view"
                 description="Get additional info on this feature"
             >
-                <p>Text</p>
+                <p>
+                    A <strong>RabbitMQ ETL</strong> ongoing-task is an ETL (Extract, Transform & Load) process
+                    that transfers data from this RavenDB database to a RabbitMQ exchange.
+                </p>
+                <ul>
+                    <li>
+                        Data is extracted from documents only.
+                        <br/>
+                        Attachments, Counters, Time series, and Revisions are not sent.
+                    </li>
+                    <li className="margin-top-xxs">
+                        The sent data can be filtered and modified by multiple transformation JavaScript scripts that
+                        are added to the task.
+                    </li>
+                    <li className="margin-top-xxs">
+                        Each script specifies the RavenDB source collection(s) and the destination Exchange(s).
+                    </li>
+                    <li className="margin-top-xxs">
+                        The scripts are executed whenever documents in the source database are created or modified
+                        (excluding deletes).
+                    </li>
+                    <li className="margin-top-xxs">
+                        You can test each script in this view to preview the messages that will be sent by the ETL
+                        task to the destination.
+                    </li>
+                </ul>
+                <hr/>
+                <div>
+                    Task definition includes:
+                    <ul>
+                        <li>A connection string to the RabbitMQ exchange server.</li>
+                        <li>The transformation scripts definitions.</li>
+                        <li>Per queue, select whether processed documents will be deleted from your RavenDB database.
+                        </li>
+                        <li>When defining the RabbitMQ Exchanges, Queues & Bindings manually, 
+                            you can request to skip their automatic creation.</li>
+                        <li>A responsible node to handle this task can be set.</li>
+                    </ul>
+                </div>
                 <hr />
                 <div className="small-label mb-2">useful links</div>
                 <a href={rabbitMqEtlDocsLink} target="_blank">
