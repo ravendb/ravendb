f072cb67de65458028190cdcb2c900e095ed98e2
danielle9897 <danielle.9897@gmail.com>
2023-09-21 21:05:09 +0300

    RavenDB-21235 Text for Certificates

--- src/Raven.Studio/typescript/viewmodels/manage/CertificatesInfoHub.tsx
+++ src/Raven.Studio/typescript/viewmodels/manage/CertificatesInfoHub.tsx
@@ -4,9 +4,12 @@ import { licenseSelectors } from "components/common/shell/licenseSlice";
 import { useAppSelector } from "components/store";
 import { useLimitedFeatureAvailability } from "components/utils/licenseLimitsUtils";
 import React from "react";
+import {useRavenLink} from "hooks/useRavenLink";
+import {Icon} from "components/common/Icon";
 
 export function CertificatesInfoHub() {
     const hasReadOnlyCertificates = useAppSelector(licenseSelectors.statusValue("HasReadOnlyCertificates"));
+    const certificatesDocsLink = useRavenLink({ hash: "S3G2T1" });
 
     const featureAvailability = useLimitedFeatureAvailability({
         defaultFeatureAvailability,
@@ -27,9 +30,48 @@ export function CertificatesInfoHub() {
                 heading="About this view"
                 description="Get additional info on this feature"
             >
+                <p>
+                    Manage your server and client certificates from this view.
+                </p>
                 <div>
-                    Text
+                    <strong>Server certificates</strong>:
+                    <ul>
+                        <li className="margin-top-xxs">
+                            The server certificate can be exported in order to upload it by another server for
+                            establishing a secure connection between the servers.
+                        </li>
+                        <li className="margin-top-xxs">
+                            The server certificate can be replaced as needed - this will apply to all cluster nodes.
+                        </li>
+                    </ul>
                 </div>
+                <div>
+                    <strong>Client certificates:</strong>
+                    <ul>
+                        <li className="margin-top-xxs">
+                            An admin can generate client certificates with an optional passphrase, and an expiration
+                            period,
+                            specifying the security clearance (Cluster Admin / Operator / User)
+                        </li>
+                        <li className="margin-top-xxs">
+                            With the User security clearance, different access permissions
+                            <br/>
+                            (Admin / Read-Write / Read-Only) can be granted per database.
+                        </li>
+                        <li className="margin-top-xxs">
+                            You can upload a client certificate that was exported from another server,
+                            allowing access to users with existing certificates.
+                        </li>
+                        <li className="margin-top-xxs">
+                            RavenDB does not keep track of the client certificate&apos;s private key.
+                        </li>
+                    </ul>
+                </div>
+                <hr/>
+                <div className="small-label mb-2">useful links</div>
+                <a href={certificatesDocsLink} target="_blank">
+                    <Icon icon="newtab" /> Docs - Manage Certificates
+                </a>
             </AccordionItemWrapper>
             <FeatureAvailabilitySummaryWrapper
                 isUnlimited={hasReadOnlyCertificates}
