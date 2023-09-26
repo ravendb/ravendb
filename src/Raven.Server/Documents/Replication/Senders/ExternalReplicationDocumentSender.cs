28f7dbb2812894b292528c56c1550360136a730d
shiranshalom <shiran@ravendb.net>
2023-09-26 13:02:29 +0300

    RavenDB-21315 - Change vector with the MOVE element leaked to non-sharded database after resharding

--- src/Raven.Server/Documents/Replication/Senders/ExternalReplicationDocumentSender.cs
+++ src/Raven.Server/Documents/Replication/Senders/ExternalReplicationDocumentSender.cs
@@ -2,6 +2,9 @@
 using System.IO;
 using Raven.Client.Documents.Operations.Replication;
 using Raven.Server.Documents.Replication.Outgoing;
+using Raven.Server.Documents.Replication.ReplicationItems;
+using Raven.Server.Documents.Replication.Stats;
+using Raven.Server.ServerWide.Context;
 using Sparrow.Logging;
 
 namespace Raven.Server.Documents.Replication.Senders
@@ -19,6 +22,12 @@ namespace Raven.Server.Documents.Replication.Senders
 
             _parent._parent._server.LicenseManager.AssertCanDelayReplication(external.DelayReplicationFor);
             return external.DelayReplicationFor;
+        }
+
+        protected override bool ShouldSkip(DocumentsOperationContext context, ReplicationBatchItem item, OutgoingReplicationStatsScope stats, SkippedReplicationItemsInfo skippedReplicationItemsInfo)
+        {
+            item.ChangeVector = context.GetChangeVector(item.ChangeVector).Order.StripMoveTag(context);
+            return base.ShouldSkip(context, item, stats, skippedReplicationItemsInfo);
         }
     }
 }
---
+++ test/SlowTests/Sharding/Issues/RavenDB_21315.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Raven.Tests.Core.Utils.Entities;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Sharding.Issues
+{
+    public class RavenDB_21315 : ReplicationTestBase
+    {
+        public RavenDB_21315(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [RavenFact(RavenTestCategory.Replication | RavenTestCategory.Sharding)]
+        public async Task ChangeVectorWithMoveTagShouldNotLeakToNonShardedAfterResharding()
+        {
+            using (var store1 = Sharding.GetDocumentStore())
+            using (var store2 = GetDocumentStore())
+            {
+                var id = "users/shiran";
+
+                using (var session = store1.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new User {Name = "Shiran"}, id);
+                    await session.SaveChangesAsync();
+                }
+
+                await Sharding.Resharding.MoveShardForId(store1, id);
+
+                await SetupReplicationAsync(store1, store2);
+                await EnsureReplicatingAsync(store1, store2);
+
+                var stats2 = await GetDatabaseStatisticsAsync(store2);
+                Assert.NotNull(stats2.DatabaseChangeVector);
+                Assert.False(stats2.DatabaseChangeVector.Contains("MOVE", StringComparison.OrdinalIgnoreCase));
+            }
+        }
+    }
+}
