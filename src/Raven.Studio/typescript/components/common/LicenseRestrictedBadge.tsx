0dc55bfff53ada6b9cff4b5c0d18f2c1e1c8d606
Mateusz <mateusz@ravendb.net>
2023-09-26 11:08:56 +0200

    RavenDB-21460 Override label text for Cloud

--- src/Raven.Studio/typescript/components/common/LicenseRestrictedBadge.tsx
+++ src/Raven.Studio/typescript/components/common/LicenseRestrictedBadge.tsx
@@ -1,6 +1,8 @@
 ﻿import { Badge } from "reactstrap";
 import React from "react";
 import classNames from "classnames";
+import { useAppSelector } from "components/store";
+import { licenseSelectors } from "./shell/licenseSlice";
 
 export type LicenseBadgeText = "Professional +" | "Enterprise";
 
@@ -10,14 +12,22 @@ interface LicenseRestrictedBadgeProps {
 }
 
 export default function LicenseRestrictedBadge({ className, licenseRequired }: LicenseRestrictedBadgeProps) {
+    const isCloud = useAppSelector(licenseSelectors.statusValue("IsCloud"));
+
     return (
-        <Badge className={classNames("ms-2 license-restricted-badge", className, getClassName(licenseRequired))}>
-            {licenseRequired}
+        <Badge
+            className={classNames("ms-2 license-restricted-badge", className, getClassName(licenseRequired, isCloud))}
+        >
+            {isCloud ? "Production" : licenseRequired}
         </Badge>
     );
 }
 
-function getClassName(licenseBadgeText: LicenseBadgeText) {
+function getClassName(licenseBadgeText: LicenseBadgeText, isCloud: boolean): "enterprise" | "professional" {
+    if (isCloud) {
+        return "enterprise";
+    }
+
     switch (licenseBadgeText) {
         case "Enterprise":
             return "enterprise";
