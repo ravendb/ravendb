a1af58244f2526a9dae7dbcf8d9401de57b82df6
Pawel Pekrol <ppekrol@ravendb.net>
2023-10-05 15:16:42 +0200

    RavenDB-21528 cleanup

--- src/Raven.Client/Http/HttpCompressionAlgorithm.cs
+++ src/Raven.Client/Http/HttpCompressionAlgorithm.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Raven.Client.Http;
 
 public enum HttpCompressionAlgorithm
@@ -6,4 +8,22 @@ public enum HttpCompressionAlgorithm
 #if FEATURE_BROTLI_SUPPORT
     Brotli
 #endif
+}
+
+internal static class HttpCompressionAlgorithmExtensions
+{
+    internal static string GetContentEncoding(this HttpCompressionAlgorithm compressionAlgorithm)
+    {
+        switch (compressionAlgorithm)
+        {
+            case HttpCompressionAlgorithm.Gzip:
+                return "gzip";
+#if FEATURE_BROTLI_SUPPORT
+            case HttpCompressionAlgorithm.Brotli:
+                return "br";
+#endif
+            default:
+                throw new ArgumentOutOfRangeException(nameof(compressionAlgorithm), compressionAlgorithm, null);
+        }
+    }
 }
