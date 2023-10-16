ddad7edc5326546019f7acb46edbf257c2561cc0
shaharhikri <shaharhikri@ravendb.net>
2023-10-16 11:21:36 +0300

    RavenDB-21335 - Add command to the server tx merger (in Leader.Run)

---
+++ src/Raven.Server/Rachis/Commands/LowestIndexUpdateCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Jint;
+using Raven.Server.Documents.TransactionMerger.Commands;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Rachis.Commands;
+
+public sealed class LowestIndexUpdateCommand : MergedTransactionCommand<ClusterOperationContext, ClusterTransaction>
+{
+    private readonly Leader _leader;
+
+    private readonly RachisConsensus _engine;
+
+    private long _lowestIndexInEntireCluster;
+
+
+    public LowestIndexUpdateCommand([NotNull] Leader leader, [NotNull] RachisConsensus engine, long lowestIndexInEntireCluster)
+    {
+        _engine = engine ?? throw new ArgumentNullException(nameof(engine));
+        _leader = leader;
+        _lowestIndexInEntireCluster = lowestIndexInEntireCluster;
+    }
+
+    protected override long ExecuteCmd(ClusterOperationContext context)
+    {
+        _engine.TruncateLogBefore(context, _lowestIndexInEntireCluster);
+        context.Transaction.InnerTransaction.LowLevelTransaction.AfterCommitWhenNewTransactionsPrevented += (tx) =>
+        {
+            _leader.LowestIndexInEntireCluster = _lowestIndexInEntireCluster;
+        };
+        return 1;
+    }
+
+    public override IReplayableCommandDto<ClusterOperationContext, ClusterTransaction, MergedTransactionCommand<ClusterOperationContext, ClusterTransaction>> ToDto(ClusterOperationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
+
