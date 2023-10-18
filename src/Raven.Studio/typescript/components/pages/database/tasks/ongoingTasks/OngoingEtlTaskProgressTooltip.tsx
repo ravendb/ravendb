bb639435eb6b6148e6d2de03add1240a119f5828
kwiato <kwiato.r@gmail.com>
2023-10-18 15:23:45 +0200

    RavenDB-21375 ETL error cut off

--- src/Raven.Studio/typescript/components/pages/database/tasks/ongoingTasks/OngoingEtlTaskProgressTooltip.tsx
+++ src/Raven.Studio/typescript/components/pages/database/tasks/ongoingTasks/OngoingEtlTaskProgressTooltip.tsx
@@ -3,6 +3,10 @@ import { PopoverWithHover } from "components/common/PopoverWithHover";
 import { OngoingEtlTaskNodeInfo, OngoingTaskInfo } from "components/models/tasks";
 import { NamedProgress, NamedProgressItem } from "components/common/NamedProgress";
 import { Icon } from "components/common/Icon";
+import { Button, Modal, ModalBody } from "reactstrap";
+import useBoolean from "components/hooks/useBoolean";
+import Code from "components/common/Code";
+import copyToClipboard from "common/copyToClipboard";
 
 interface OngoingTaskEtlProgressTooltipProps {
     target: HTMLElement;
@@ -13,20 +17,66 @@ interface OngoingTaskEtlProgressTooltipProps {
 
 export function OngoingEtlTaskProgressTooltip(props: OngoingTaskEtlProgressTooltipProps) {
     const { target, nodeInfo, showPreview } = props;
+    const { value: isErrorModalOpen, toggle: toggleErrorModal } = useBoolean(false);
 
     if (nodeInfo.status === "failure") {
         return (
-            <PopoverWithHover target={target} placement="top">
-                <div className="text-danger flex-horizontal">
-                    <div className="flex-start text-warning">
-                        <Icon icon="warning" margin="m-0" />
-                    </div>
-                    <div>
-                        <div>Unable to load task status:</div>
-                        <div>{nodeInfo.details.error}</div>
-                    </div>
-                </div>
-            </PopoverWithHover>
+            <>
+                {!isErrorModalOpen && (
+                    <PopoverWithHover target={target} placement="top">
+                        <div className="vstack gap-2 p-3">
+                            <div className="text-danger">
+                                <Icon icon="warning" color="danger" /> Unable to load task status:
+                            </div>
+                            <div
+                                className="overflow-auto"
+                                style={{
+                                    maxWidth: "300px",
+                                    maxHeight: "300px",
+                                }}
+                            >
+                                <code>{nodeInfo.details.error}</code>
+                            </div>
+                            <Button size="sm" onClick={toggleErrorModal}>
+                                Open error in modal <Icon icon="newtab" margin="ms-1" />
+                            </Button>
+                        </div>
+                    </PopoverWithHover>
+                )}
+                <Modal
+                    size="xl"
+                    wrapClassName="bs5"
+                    isOpen={isErrorModalOpen}
+                    toggle={toggleErrorModal}
+                    contentClassName="modal-border bulge-danger"
+                >
+                    <ModalBody>
+                        <div className="position-absolute m-2 end-0 top-0">
+                            <Button close onClick={toggleErrorModal} />
+                        </div>
+                        <div className="hstack gap-3 mb-4">
+                            <div className="text-center">
+                                <Icon icon="warning" color="danger" className="fs-1" margin="m-0" />
+                            </div>
+                            <div className="text-center lead">Unable to load task status:</div>
+                        </div>
+                        <Code code={nodeInfo.details.error} language="csharp" />
+
+                        <div className="text-end">
+                            <Button
+                                className="rounded-pill"
+                                color="primary"
+                                size="xs"
+                                onClick={() =>
+                                    copyToClipboard.copy(nodeInfo.details.error, "Copied error message to clipboard")
+                                }
+                            >
+                                <Icon icon="copy" /> <span>Copy to clipboard</span>
+                            </Button>
+                        </div>
+                    </ModalBody>
+                </Modal>
+            </>
         );
     }
 
