5ff616fadd7a7f29440dc484805ff66b353b977d
Grisha Kotler <grisha@ravendb.net>
2023-10-20 11:20:31 +0300

    RavenDB-21523 - add the SmallestSize to the list of options

--- src/Raven.Studio/typescript/models/database/tasks/periodicBackup/snapshot.ts
+++ src/Raven.Studio/typescript/models/database/tasks/periodicBackup/snapshot.ts
@@ -7,7 +7,8 @@ class snapshot {
     static readonly compressionLevelDictionary: compressionLevelOption[] = [
         { name: "Optimal", fullName: "Optimal" },
         { name: "Fastest", fullName: "Fastest" },
-        { name: "No Compression", fullName: "NoCompression" }
+        { name: "No Compression", fullName: "NoCompression" },
+        { name: "Smallest Size", fullName: "SmallestSize" }
     ];
 
     compressionLevelOptions = snapshot.compressionLevelDictionary.map(x => x.name);
