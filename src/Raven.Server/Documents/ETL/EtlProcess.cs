f313c20e8b85819d1e72d3dd35b9db66781e48fc
Gracjan Sadowicz <gracjan@ravendb.net>
2023-10-20 16:43:50 +0200

    RavenDB-20729 Remake error handling in KafkaEtl.cs

--- src/Raven.Server/Documents/ETL/EtlProcess.cs
+++ src/Raven.Server/Documents/ETL/EtlProcess.cs
@@ -1366,7 +1366,10 @@ namespace Raven.Server.Documents.ETL
                 : Transformation.Collections;
 
             var lastProcessedEtag = LastProcessState.GetLastProcessedEtag(Database.DbBase64Id, Database.ServerStore.NodeTag);
-
+            
+            if (this is KafkaEtl kafkaEtl)
+                result.TransactionalId = kafkaEtl.TransactionalId;
+            
             var overallDuration = Stopwatch.StartNew();
             foreach (var collection in collections)
             {
