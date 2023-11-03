8f11e5083cd94dd9f81920905933508a6189126a
Pawel Pekrol <ppekrol@ravendb.net>
2023-11-03 09:29:38 +0100

    RavenDB-21528 better assertions in a test

--- test/SlowTests/Issues/RavenDB-19852.cs
+++ test/SlowTests/Issues/RavenDB-19852.cs
@@ -1,13 +1,14 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using FastTests;
 using Raven.Client.Documents.Commands.MultiGet;
-using Raven.Client.Documents.Session;
 using Raven.Client.Http;
 using Raven.Client.Util;
 using Raven.Tests.Core.Utils.Entities;
 using Sparrow.Json;
+using Tests.Infrastructure;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -19,22 +20,31 @@ public class RavenDB_19852 : RavenTestBase
     {
     }
 
-    [Fact]
-    public void ResponseFromMultiGetIsCompressed()
+    [RavenTheory(RavenTestCategory.ClientApi)]
+    [InlineData(HttpCompressionAlgorithm.Gzip)]
+    [InlineData(HttpCompressionAlgorithm.Brotli)]
+    [InlineData(HttpCompressionAlgorithm.Zstd)]
+    public void ResponseFromMultiGetIsCompressed(HttpCompressionAlgorithm compressionAlgorithm)
     {
-        using var store = GetDocumentStore();
+        using var store = GetDocumentStore(new Options
+        {
+            ModifyDocumentStore = s => s.Conventions.HttpCompressionAlgorithm = compressionAlgorithm
+        });
+
         const string docs = "/docs";
 
         using (var session = store.OpenSession())
         {
-            session.Store(new Company() {Name = new string('a', 10), Id = "doc/1"}, id: "doc/1");
+            session.Store(new Company() { Name = new string('a', 10), Id = "doc/1" }, id: "doc/1");
             session.SaveChanges();
         }
 
         using (var commands = store.Commands())
         {
-            using var firstCommand = new MultiGetCommandForCompressionTest(commands.RequestExecutor,
-                new List<GetRequest> {new GetRequest {Url = docs, Query = "?id=doc/1"}});
+            using var firstCommand = new MultiGetCommandForCompressionTest(
+                compressionAlgorithm,
+                commands.RequestExecutor,
+                new List<GetRequest> { new GetRequest { Url = docs, Query = "?id=doc/1" } });
 
             commands.RequestExecutor.Execute(firstCommand, commands.Context);
         }
@@ -42,14 +52,12 @@ public class RavenDB_19852 : RavenTestBase
 
     private class MultiGetCommandForCompressionTest : MultiGetCommand
     {
-        public MultiGetCommandForCompressionTest(RequestExecutor requestExecutor, List<GetRequest> commands) : base(requestExecutor, commands)
-        {
-            ResponseType = RavenCommandResponseType.Raw;
-        }
+        private readonly HttpCompressionAlgorithm _compressionAlgorithm;
 
-        internal MultiGetCommandForCompressionTest(RequestExecutor requestExecutor, List<GetRequest> commands, SessionInfo sessionInfo) : base(requestExecutor, commands,
-            sessionInfo)
+        public MultiGetCommandForCompressionTest(HttpCompressionAlgorithm compressionAlgorithm, RequestExecutor requestExecutor, List<GetRequest> commands) : base(requestExecutor, commands)
         {
+            _compressionAlgorithm = compressionAlgorithm;
+            ResponseType = RavenCommandResponseType.Raw;
         }
 
         public override void SetResponseRaw(HttpResponseMessage response, Stream stream, JsonOperationContext context)
@@ -57,7 +65,21 @@ public class RavenDB_19852 : RavenTestBase
             var streamWithTimeout = (StreamWithTimeout)stream;
             var innerStream = streamWithTimeout._stream;
             var contentTypeName = innerStream.GetType().Name;
-            Assert.True(contentTypeName.Contains("GZip") || contentTypeName.Contains("Brotli") || contentTypeName.Contains("Zstd"), $"{contentTypeName}.Contains('GZip') || {contentTypeName}.Contains('Brotli') || {contentTypeName}.Contains('Zstd')");
+
+            switch (_compressionAlgorithm)
+            {
+                case HttpCompressionAlgorithm.Gzip:
+                    Assert.True(contentTypeName.Contains("Brotli"), $"{contentTypeName}.Contains('Brotli')"); // we are picking the best one based on the order of compression providers in the RavenServer
+                    break;
+                case HttpCompressionAlgorithm.Brotli:
+                    Assert.True(contentTypeName.Contains("Brotli"), $"{contentTypeName}.Contains('Brotli')");
+                    break;
+                case HttpCompressionAlgorithm.Zstd:
+                    Assert.True(contentTypeName.Contains("Zstd"), $"{contentTypeName}.Contains('Zstd')");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
