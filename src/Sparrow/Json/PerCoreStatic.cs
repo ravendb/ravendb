bbaaea86aa523163a09995d83a410bbb0cc45c4b
Federico Lois <federico.lois@gmail.com>
2023-11-06 09:29:41 -0300

    RavenDB-21468: PerCore static shared array.

---
+++ src/Sparrow/Json/PerCoreStatic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Sparrow.Utils;
+
+namespace Sparrow.Json
+{
+    public sealed class PerCoreStatic<T> where T : class
+    {
+        private readonly T[] _perCoreArrays;
+
+        public PerCoreStatic(Func<T> creationFunc)
+        {
+            _perCoreArrays = new T[Environment.ProcessorCount];
+            for (int i = 0; i < _perCoreArrays.Length; i++)
+                _perCoreArrays[i] = creationFunc();
+        }
+
+        public T Get()
+        {
+            return _perCoreArrays[CurrentProcessorIdHelper.GetCurrentProcessorId() % _perCoreArrays.Length];
+        }
+    }
+}
