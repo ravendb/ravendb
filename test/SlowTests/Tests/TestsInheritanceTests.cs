e616b8988b7a514113214e2f0bbd9269e04a911b
Michał Leśniak <michal.lesniak@ravendb.net>
2023-11-15 10:42:25 +0100

    RavenDB-19126 Adjusted `AllTestsShouldUseRavenFactOrRavenTheoryAttributes` test

--- test/SlowTests/Tests/TestsInheritanceTests.cs
+++ test/SlowTests/Tests/TestsInheritanceTests.cs
@@ -89,7 +89,7 @@ namespace SlowTests.Tests
                         select method;
 
             var array = types.ToArray();
-            const int numberToTolerate = 6446;
+            const int numberToTolerate = 6445;
             if (array.Length == numberToTolerate)
                 return;
 
