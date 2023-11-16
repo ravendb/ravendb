404829ffc59f803256a85e47431bd56988f5dfa9
Grisha Kotler <grisha@ravendb.net>
2023-11-16 13:20:20 +0200

    RavenDB-21708 - fix NRE when ClusterState is null

--- src/Raven.Server/ServerWide/Commands/Indexes/SetIndexLockCommand.cs
+++ src/Raven.Server/ServerWide/Commands/Indexes/SetIndexLockCommand.cs
@@ -28,6 +28,7 @@ namespace Raven.Server.ServerWide.Commands.Indexes
             if (record.Indexes.TryGetValue(IndexName, out IndexDefinition staticIndex))
             {
                 staticIndex.LockMode = LockMode;
+                staticIndex.ClusterState ??= new IndexDefinitionClusterState();
                 staticIndex.ClusterState.LastIndex = etag;
             }
 
