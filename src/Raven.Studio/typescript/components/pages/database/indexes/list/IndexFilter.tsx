ab52af68340861da1ed4858ff84892797bba4935
kwiato <kwiato.r@gmail.com>
2023-11-21 14:16:15 +0100

    RavenDB-19198 Ability to sort indexes by creation time and see that time

--- src/Raven.Studio/typescript/components/pages/database/indexes/list/IndexFilter.tsx
+++ src/Raven.Studio/typescript/components/pages/database/indexes/list/IndexFilter.tsx
@@ -1,4 +1,4 @@
-﻿import React from "react";
+﻿import React, { useState } from "react";
 import { shardingTodo } from "common/developmentHelper";
 import { IndexStatus, IndexFilterCriteria, IndexType } from "components/models/indexes";
 import { Button, Input, PopoverBody, UncontrolledPopover } from "reactstrap";
@@ -7,6 +7,7 @@ import { Icon } from "components/common/Icon";
 import { MultiCheckboxToggle } from "components/common/MultiCheckboxToggle";
 import { InputItem } from "components/models/common";
 import { Switch } from "components/common/Checkbox";
+import { SortDropdown, SortDropdownRadioList, sortItem } from "components/common/SortDropdown";
 
 interface IndexFilterProps {
     filter: IndexFilterCriteria;
@@ -94,6 +95,27 @@ export default function IndexFilter(props: IndexFilterProps) {
         );
     };
 
+    const sortBy: sortItem[] = [
+        { value: "alphabetically", label: "Alphabetically" },
+        { value: "creationDate", label: "Creation date" },
+        { value: "lastIndexedDate", label: "Last indexed date" },
+        { value: "lastQueryDate", label: "Last query date" },
+    ];
+
+    const sortDirection: sortItem[] = [
+        { value: "asc", label: "Ascending", icon: "arrow-thin-top" },
+        { value: "desc", label: "Descending", icon: "arrow-thin-bottom" },
+    ];
+
+    const groupBy: sortItem[] = [
+        { value: "byCollection", label: "By collection" },
+        { value: "none", label: "none" },
+    ];
+
+    const [selectedSortBy, setSelectedSortBy] = useState<string>(sortBy[0].value);
+    const [selectedSortDirection, setSelectedSortDirection] = useState<string>(sortDirection[0].value);
+    const [selectedGroupBy, setSelectedGroupBy] = useState<string>(groupBy[0].value);
+
     return (
         <div className="hstack flex-wrap align-items-end gap-3 my-3 justify-content-end">
             <div className="flex-grow">
@@ -134,7 +156,46 @@ export default function IndexFilter(props: IndexFilterProps) {
                 setSelectedItems={onSearchTypesChange}
                 selectAllCount={indexesCount}
             />
+            <div>
+                <div className="small-label ms-1 mb-1">Sort & Group</div>
 
+                <SortDropdown
+                    label={
+                        <>
+                            {sortBy.find((item) => item.value === selectedSortBy).label}{" "}
+                            {selectedSortDirection === "asc" ? (
+                                <Icon icon="arrow-thin-top" margin="ms-1" />
+                            ) : (
+                                <Icon icon="arrow-thin-bottom" margin="ms-1" />
+                            )}{" "}
+                            {selectedGroupBy !== "none" && (
+                                <span className="ms-2">
+                                    {groupBy.find((item) => item.value === selectedGroupBy).label}
+                                </span>
+                            )}
+                        </>
+                    }
+                >
+                    <SortDropdownRadioList
+                        radioOptions={sortBy}
+                        label="Sort by"
+                        selected={selectedSortBy}
+                        setSelected={setSelectedSortBy}
+                    />
+                    <SortDropdownRadioList
+                        radioOptions={sortDirection}
+                        label="Sort direction"
+                        selected={selectedSortDirection}
+                        setSelected={setSelectedSortDirection}
+                    />
+                    <SortDropdownRadioList
+                        radioOptions={groupBy}
+                        label="Group by"
+                        selected={selectedGroupBy}
+                        setSelected={setSelectedGroupBy}
+                    />
+                </SortDropdown>
+            </div>
             {/* TODO: `Processing Speed: <strong>${Math.floor(totalProcessedPerSecond).toLocaleString()}</strong> docs / sec`;*/}
             <Switch
                 id="autoRefresh"
