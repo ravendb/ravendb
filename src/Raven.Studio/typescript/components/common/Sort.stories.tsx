a557102ac41a8e2ee8c8b57ee4e9c172bc4c4fea
kwiato <kwiato.r@gmail.com>
2023-11-21 14:16:15 +0100

    RavenDB-19198 Ability to sort indexes by creation time and see that time

---
+++ src/Raven.Studio/typescript/components/common/Sort.stories.tsx
@@ -0,0 +1,71 @@
+﻿import React, { useState } from "react";
+import { withBootstrap5, withStorybookContexts } from "test/storybookTestUtils";
+import { SortDropdown, SortDropdownRadioList, sortItem } from "./SortDropdown";
+import { Icon } from "./Icon";
+
+export default {
+    title: "Bits/Sort",
+    decorators: [withStorybookContexts, withBootstrap5],
+};
+
+export function Sort() {
+    const sortBy: sortItem[] = [
+        { value: "alphabetically", label: "Alphabetically" },
+        { value: "creationDate", label: "Creation date" },
+        { value: "lastIndexedDate", label: "Last indexed date" },
+        { value: "lastQueryDate", label: "Last query date" },
+    ];
+
+    const sortDirection: sortItem[] = [
+        { value: "asc", label: "Ascending", icon: "arrow-thin-top" },
+        { value: "desc", label: "Descending", icon: "arrow-thin-bottom" },
+    ];
+
+    const groupBy: sortItem[] = [
+        { value: "byCollection", label: "By collection" },
+        { value: "none", label: "none" },
+    ];
+
+    const [selectedSortBy, setSelectedSortBy] = useState<string>(sortBy[0].value);
+    const [selectedSortDirection, setSelectedSortDirection] = useState<string>(sortDirection[0].value);
+    const [selectedGroupBy, setSelectedGroupBy] = useState<string>(groupBy[0].value);
+
+    return (
+        <div>
+            <SortDropdown
+                label={
+                    <>
+                        {sortBy.find((item) => item.value === selectedSortBy).label}{" "}
+                        {selectedSortDirection === "asc" ? (
+                            <Icon icon="arrow-thin-top" margin="ms-1" />
+                        ) : (
+                            <Icon icon="arrow-thin-bottom" margin="ms-1" />
+                        )}{" "}
+                        {selectedGroupBy !== "none" && (
+                            <span className="ms-2">{groupBy.find((item) => item.value === selectedGroupBy).label}</span>
+                        )}
+                    </>
+                }
+            >
+                <SortDropdownRadioList
+                    radioOptions={sortBy}
+                    label="Sort by"
+                    selected={selectedSortBy}
+                    setSelected={setSelectedSortBy}
+                />
+                <SortDropdownRadioList
+                    radioOptions={sortDirection}
+                    label="Sort direction"
+                    selected={selectedSortDirection}
+                    setSelected={setSelectedSortDirection}
+                />
+                <SortDropdownRadioList
+                    radioOptions={groupBy}
+                    label="Group by"
+                    selected={selectedGroupBy}
+                    setSelected={setSelectedGroupBy}
+                />
+            </SortDropdown>
+        </div>
+    );
+}
---
+++ src/Raven.Studio/typescript/components/common/SortDropdown.tsx
@@ -0,0 +1,60 @@
+﻿import React, { ReactNode } from "react";
+import useId from "hooks/useId";
+import "./SortDropdown.scss";
+import { Icon } from "./Icon";
+import { DropdownMenu, DropdownToggle, UncontrolledDropdown } from "reactstrap";
+import { Radio } from "./Checkbox";
+import IconName from "typings/server/icons";
+
+interface SortDropdownProps {
+    label: string | ReactNode | ReactNode[];
+    children: ReactNode | ReactNode[];
+}
+
+export function SortDropdown(props: SortDropdownProps) {
+    const { label, children } = props;
+    return (
+        <UncontrolledDropdown className="sort-dropdown">
+            <DropdownToggle className="sort-dropdown-toggle d-flex align-items-center" caret>
+                <div className="flex-grow d-flex align-items-center">{label}</div>
+            </DropdownToggle>
+            <DropdownMenu className="py-4 px-5">
+                <div className="hstack gap-5">{children}</div>
+            </DropdownMenu>
+        </UncontrolledDropdown>
+    );
+}
+
+interface SortDropdownRadioListProps {
+    radioOptions: sortItem[];
+    label?: string;
+    selected: string;
+    setSelected: (value: string) => void;
+}
+
+export function SortDropdownRadioList<T extends string | number = string>(props: SortDropdownRadioListProps) {
+    const { radioOptions, label, selected, setSelected } = props;
+    const uniqueId = useId("sort-dropdown-list");
+
+    return (
+        <div className="vstack gap-1 dropdown-radio-group">
+            {label && <div className="small-label mb-2">{label}</div>}
+            {radioOptions.map((item) => (
+                <Radio
+                    key={uniqueId + item.value}
+                    selected={item.value === selected}
+                    toggleSelection={() => setSelected(item.value)}
+                >
+                    {item.icon && <Icon icon={item.icon} />}
+                    {item.label}
+                </Radio>
+            ))}
+        </div>
+    );
+}
+
+export interface sortItem<T extends string | number = string> {
+    label: string;
+    value: T;
+    icon?: IconName;
+}
