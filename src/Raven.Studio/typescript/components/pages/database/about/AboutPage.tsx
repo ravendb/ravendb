5658a38c6313bed922c14995f7fb4ce31822b13d
kwiato <kwiato.r@gmail.com>
2023-11-22 09:53:00 +0100

    RavenDB-20603 About page

---
+++ src/Raven.Studio/typescript/components/pages/database/about/AboutPage.tsx
@@ -0,0 +1,22 @@
+﻿import React, { useEffect } from "react";
+import { useAppUrls } from "hooks/useAppUrls";
+import {
+    initView,
+    statisticsViewSelectors,
+} from "components/pages/database/status/statistics/store/statisticsViewSlice";
+import { useAppDispatch, useAppSelector } from "components/store";
+import { IndexesDatabaseStats } from "components/pages/database/status/statistics/partials/IndexesDatabaseStats";
+import { StatsHeader } from "components/pages/database/status/statistics/partials/StatsHeader";
+import { NonShardedViewProps } from "components/models/common";
+
+interface AboutPageProps {
+    test?: boolean;
+}
+
+export function AboutPage(props: AboutPageProps) {
+    return (
+        <>
+            <h1>About Page</h1>
+        </>
+    );
+}
