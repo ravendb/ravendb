9921c199a2d9fae6152870b5dacd48c5868916e3
Damian Olszewski <damian@ravendb.net>
2023-11-27 13:37:13 +0100

    RavenDB-21628 Add useConnectionStringsLicense

---
+++ src/Raven.Studio/typescript/components/pages/database/settings/connectionStrings/useConnectionStringsLicense.ts
@@ -0,0 +1,26 @@
+import { licenseSelectors } from "components/common/shell/licenseSlice";
+import { useAppSelector } from "components/store";
+
+export default function useConnectionStringsLicense() {
+    const hasRavenEtl = useAppSelector(licenseSelectors.statusValue("HasRavenEtl"));
+    const hasSqlEtl = useAppSelector(licenseSelectors.statusValue("HasSqlEtl"));
+    const hasOlapEtl = useAppSelector(licenseSelectors.statusValue("HasOlapEtl"));
+    const hasElasticSearchEtl = useAppSelector(licenseSelectors.statusValue("HasElasticSearchEtl"));
+    const hasQueueEtl = useAppSelector(licenseSelectors.statusValue("HasQueueEtl"));
+
+    const allFeatures = [hasRavenEtl, hasSqlEtl, hasOlapEtl, hasElasticSearchEtl, hasQueueEtl];
+    const hasNone = allFeatures.every((x) => !x);
+    const hasAll = allFeatures.every((x) => x);
+
+    return {
+        features: {
+            hasRavenEtl,
+            hasSqlEtl,
+            hasOlapEtl,
+            hasElasticSearchEtl,
+            hasQueueEtl,
+        },
+        hasNone,
+        hasAll,
+    };
+}
