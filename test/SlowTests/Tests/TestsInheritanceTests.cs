6c6bbfd197d7a091009760c61af0ed2d2cbb9416
Maciej Aszyk <maciej@ravendb.net>
2023-11-27 11:38:16 +0100

    RavenDB-21682 Handle multiple deletions in single batch by `TryDeleteEntryByField`. Reset number of modifications after reset of IndexWriter.

--- test/SlowTests/Tests/TestsInheritanceTests.cs
+++ test/SlowTests/Tests/TestsInheritanceTests.cs
@@ -91,7 +91,7 @@ namespace SlowTests.Tests
 
             var array = types.ToArray();
 
-            const int numberToTolerate = 4657;
+            const int numberToTolerate = 4654;
 
             if (array.Length == numberToTolerate)
                 return;
