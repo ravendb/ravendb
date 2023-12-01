d917284aeebf5d715e6dee49f447c7dbd10a4996
Pawel Pekrol <ppekrol@ravendb.net>
2023-12-01 16:09:09 +0100

    RavenDB-21528 fixing test

--- test/SlowTests/Tests/TestsInheritanceTests.cs
+++ test/SlowTests/Tests/TestsInheritanceTests.cs
@@ -91,7 +91,7 @@ namespace SlowTests.Tests
 
             var array = types.ToArray();
 
-            const int numberToTolerate = 4654;
+            const int numberToTolerate = 4653;
 
             if (array.Length == numberToTolerate)
                 return;
