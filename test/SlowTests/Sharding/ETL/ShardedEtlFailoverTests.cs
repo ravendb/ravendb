3548ce84f7aa6a35a2b93300a1609ec155eb9470
aviv <aviv@ayende.com>
2023-12-05 11:34:39 +0200

    RavenDB-20934 : add debug info to flaky test

--- test/SlowTests/Sharding/ETL/ShardedEtlFailoverTests.cs
+++ test/SlowTests/Sharding/ETL/ShardedEtlFailoverTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using FastTests.Client;
@@ -121,7 +122,39 @@ namespace SlowTests.Sharding.ETL
                 }
 
                 Assert.True(WaitForDocument<User>(dest, "users/2", u => u.Name == "Joe Doe2", 60_000));
-                Assert.Throws<NodeIsPassiveException>(() =>
+
+                // add debug info to investigate RavenDB-20934
+                // can revert this back to Assert.Throws when the issue is resolved
+
+                Exception ex = null;
+                try
+                {
+                    using (var originalSrc = new DocumentStore
+                    {
+                        Urls = new[] { originalTaskNode.WebUrl }, 
+                        Database = srcDb, 
+                        Conventions = new DocumentConventions
+                        {
+                            DisableTopologyUpdates = true
+                        }
+                    }.Initialize())
+                    {
+                        using (var session = originalSrc.OpenSession())
+                        {
+                            session.Store(new User() { Name = "Joe Doe3" }, "users/3");
+
+                            session.SaveChanges();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    ex = e;
+                }
+
+                Assert.True(ex is NodeIsPassiveException, await AddDebugInfo(ex));
+
+                /*Assert.Throws<NodeIsPassiveException>(() =>
                 {
                     using (var originalSrc = new DocumentStore
                     {
@@ -143,7 +176,7 @@ namespace SlowTests.Sharding.ETL
                             session.SaveChanges();
                         }
                     }
-                });
+                });*/
             }
         }
 
@@ -817,5 +850,16 @@ loadToOrders(partitionBy(key),
             files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
             Assert.True(files.Length > count);
         }
+
+        private async Task<string> AddDebugInfo(Exception ex)
+        {
+            var sb = new StringBuilder()
+                .AppendLine($"Failed. Expected NodeIsPassiveException but {(ex == null ? "none" : ex.GetType())} was thrown.")
+                .AppendLine("Cluster debug logs:");
+
+            await GetClusterDebugLogsAsync(sb);
+            return sb.ToString();
+        }
+
     }
 }
