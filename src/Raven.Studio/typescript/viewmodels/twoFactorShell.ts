9850798ffe0dc76a13653cae2d03a59a56b88e55
Marcin Lewandowski <marcin@ravendb.net>
2023-12-06 10:04:21 +0100

    RavenDB-19951 Introduce TOTP support for browser generated requests

--- src/Raven.Studio/typescript/viewmodels/twoFactorShell.ts
+++ src/Raven.Studio/typescript/viewmodels/twoFactorShell.ts
@@ -1,9 +1,13 @@
 ﻿import viewModelBase from "viewmodels/viewModelBase";
 import validateTwoFactorSecretCommand from "commands/auth/validateTwoFactorSecretCommand";
+import getTwoFactorServerConfigurationCommand from "commands/auth/getTwoFactorServerConfigurationCommand";
 
 
 class twoFactorShell extends viewModelBase {
     view = require("views/twoFactorShell.html");
+    
+    sessionDurationInMin = ko.observable<number>();
+    maxSessionDurationInMin = ko.observable<number>();
     
     code = ko.observable<string>();
     withLimits = ko.observable<boolean>(false);
@@ -12,18 +16,25 @@ class twoFactorShell extends viewModelBase {
         super();
         
         this.bindToCurrentInstance("verify");
-        
-        this.withLimits.subscribe(l => {
-            console.log("with limits = " + l); //TODO:
-        })
     }
     
+    activate(args: any) {
+        super.activate(args);
+        
+        return new getTwoFactorServerConfigurationCommand()
+            .execute()
+            .done(response => {
+                this.sessionDurationInMin(response.DefaultTwoFactorSessionDurationInMin);
+                this.maxSessionDurationInMin(response.MaxTwoFactorSessionDurationInMin);
+            });
+    }
+
     verify() {
         if (!this.code() || this.code().length !== 6) {
             return;
         }
 
-        new validateTwoFactorSecretCommand(this.code(), this.withLimits())
+        new validateTwoFactorSecretCommand(this.code(), this.withLimits(), this.sessionDurationInMin())
             .execute()
             .done(() => {
                 location.href = location.origin;
