79f3cb4a1c27f78260b2dc4267a496c3239e0251
Lev Skuditsky <lev.skuditsky@ravendb.net>
2023-12-07 17:26:59 +0200

    RavenDB-21761: Fix for `AllTestsShouldUseRavenFactOrRavenTheoryAttributes`

--- test/SlowTests/Tests/TestsInheritanceTests.cs
+++ test/SlowTests/Tests/TestsInheritanceTests.cs
@@ -88,7 +88,7 @@ namespace SlowTests.Tests
                         select method;
 
             var array = types.ToArray();
-            const int numberToTolerate = 6446;
+            const int numberToTolerate = 6442;
             if (array.Length == numberToTolerate)
                 return;
 
