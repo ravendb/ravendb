5a8aa59c47d274502f8402ff8aa0898dd1f0a1b8
Damian Olszewski <damian@ravendb.net>
2023-12-18 09:48:33 +0100

    RavenDB-21628 Fix deep yup tests

--- src/Raven.Studio/typescript/components/common/formDestinations/utils/formDestinationsValidation.ts
+++ src/Raven.Studio/typescript/components/common/formDestinations/utils/formDestinationsValidation.ts
@@ -77,7 +77,7 @@ const amazonSchema = yupObjectSchema<AmazonDestination>({
                     .test(
                         "include-dash",
                         "AWS Region must include a '-' and cannot start or end with it",
-                        (value) => value && value.includes("-") && !value.startsWith("-") && !value.endsWith("-")
+                        (value) => value.includes("-") && !value.startsWith("-") && !value.endsWith("-")
                     ),
         }),
     awsAccessKey: yupRequiredStringForEnabled,
@@ -86,50 +86,72 @@ const amazonSchema = yupObjectSchema<AmazonDestination>({
 });
 
 const s3Schema = yupObjectSchema<WithoutAmazonAndBase<S3Destination>>({
-    bucketName: yupRequiredStringForEnabled
-        .min(3)
-        .max(63)
-        .matches(/^[a-z\d]/, "Bucket name should start with a number or letter")
-        .matches(/[a-z\d]$/, "Bucket name should end with a number or letter")
-        .matches(/^[a-z0-9.-]+$/, "Allowed characters are lowercase characters, numbers, periods, and dashes")
-        .test(
-            "dashes-next-to-period",
-            'Bucket names cannot contain dashes next to periods (e.g. " -." and/or ".-")',
-            (value) => !value || (!value.includes(".-") && !value.includes("-."))
-        )
-        .test(
-            "consecutive-periods",
-            "Bucket name cannot contain consecutive periods",
-            (value) => !value || !value.includes("..")
-        )
-        .test(
-            "ip",
-            "Bucket name must not be formatted as an IP address (e.g., 192.168.5.4)",
-            (value) => !value || !/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/.test(value)
-        ),
+    bucketName: yup
+        .string()
+        .nullable()
+        .when(["isEnabled", "config"], {
+            is: (isEnabled: boolean, config: BackupConfigurationScript) => isEnabled && !config.isOverrideConfig,
+            then: (schema) =>
+                schema
+                    .required()
+                    .min(3)
+                    .max(63)
+                    .matches(/^[a-z\d]/, "Bucket name should start with a number or letter")
+                    .matches(/[a-z\d]$/, "Bucket name should end with a number or letter")
+                    .matches(
+                        /^[a-z0-9.-]+$/,
+                        "Allowed characters are lowercase characters, numbers, periods, and dashes"
+                    )
+                    .test(
+                        "dashes-next-to-period",
+                        'Bucket names cannot contain dashes next to periods (e.g. " -." and/or ".-")',
+                        (value) => !value.includes(".-") && !value.includes("-.")
+                    )
+                    .test(
+                        "consecutive-periods",
+                        "Bucket name cannot contain consecutive periods",
+                        (value) => !value.includes("..")
+                    )
+                    .test(
+                        "ip",
+                        "Bucket name must not be formatted as an IP address (e.g., 192.168.5.4)",
+                        (value) => !/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/.test(value)
+                    ),
+        }),
     forcePathStyle: yup.boolean(),
     isUseCustomHost: yup.boolean(),
     customServerUrl: yup
         .string()
         .nullable()
-        .basicUrl()
-        .when("isUseCustomHost", { is: true, then: (schema) => schema.required() }),
+        .when(["isUseCustomHost", "config"], {
+            is: (isUseCustomHost: boolean, config: BackupConfigurationScript) =>
+                isUseCustomHost && !config.isOverrideConfig,
+            then: (schema) => schema.required().basicUrl(),
+        }),
 })
     .concat(destinationBaseSchema)
     .concat(amazonSchema);
 
 const azureSchema = yupObjectSchema<WithoutBase<AzureDestination>>({
-    storageContainer: yupRequiredStringForEnabled
-        .min(3)
-        .max(63)
-        .matches(/^[a-z0-9-]+$/, "Allowed characters lowercase characters, numbers and dashes")
-        .matches(/^[a-z0-9]/, "Container name must start with a letter or a number")
-        .matches(/[a-z0-9]$/, "Container name must end with a letter or a number")
-        .test(
-            "consecutive-dashes",
-            "Consecutive dashes are not permitted in container names",
-            (value) => !value || !/--/.test(value)
-        ),
+    storageContainer: yup
+        .string()
+        .nullable()
+        .when(["isEnabled", "config"], {
+            is: (isEnabled: boolean, config: BackupConfigurationScript) => isEnabled && !config.isOverrideConfig,
+            then: (schema) =>
+                schema
+                    .required()
+                    .min(3)
+                    .max(63)
+                    .matches(/^[a-z0-9-]+$/, "Allowed characters lowercase characters, numbers and dashes")
+                    .matches(/^[a-z0-9]/, "Container name must start with a letter or a number")
+                    .matches(/[a-z0-9]$/, "Container name must end with a letter or a number")
+                    .test(
+                        "consecutive-dashes",
+                        "Consecutive dashes are not permitted in container names",
+                        (value) => !/--/.test(value)
+                    ),
+        }),
     remoteFolderName: yup.string().nullable(),
     accountName: yupRequiredStringForEnabled,
     accountKey: yupRequiredStringForEnabled,
@@ -137,67 +159,87 @@ const azureSchema = yupObjectSchema<WithoutBase<AzureDestination>>({
 }).concat(destinationBaseSchema);
 
 const googleCloudSchema = yupObjectSchema<WithoutBase<GoogleCloudDestination>>({
-    bucketName: yupRequiredStringForEnabled
-        .min(3)
-        .max(
-            222,
-            "Bucket name must contain 3 to 63 characters. Names containing dots can contain up to 222 characters, but each dot-separated component can be no longer than 63 characters"
-        )
-        .matches(
-            /^[a-z0-9._-]+$/,
-            "Bucket name must contain only lowercase letters, numbers, dashes (-), underscores (_), and dots (.)"
-        )
-        .matches(/^[a-z0-9]/, "Bucket name must start with a letter or a number")
-        .matches(/[a-z0-9]$/, "Bucket name must end with a letter or a number")
-        .test(
-            "dash-period-underscore",
-            "Dashes, periods and underscores are not permitted to be adjacent to another",
-            (value) => {
-                if (!value) {
-                    return true;
-                }
-                const firstDashRuleRegExp = /\.-/;
-                const secondDashRuleRegExp = /\.\./;
-                const thirdDashRuleRegExp = /-\./;
-                const fourthDashesRegExp = /_\./;
-                const fifthDashesRegExp = /\._/;
+    bucketName: yup
+        .string()
+        .nullable()
+        .when(["isEnabled", "config"], {
+            is: (isEnabled: boolean, config: BackupConfigurationScript) => isEnabled && !config.isOverrideConfig,
+            then: (schema) =>
+                schema
+                    .required()
+                    .min(3)
+                    .max(
+                        222,
+                        "Bucket name must contain 3 to 63 characters. Names containing dots can contain up to 222 characters, but each dot-separated component can be no longer than 63 characters"
+                    )
+                    .matches(
+                        /^[a-z0-9._-]+$/,
+                        "Bucket name must contain only lowercase letters, numbers, dashes (-), underscores (_), and dots (.)"
+                    )
+                    .matches(/^[a-z0-9]/, "Bucket name must start with a letter or a number")
+                    .matches(/[a-z0-9]$/, "Bucket name must end with a letter or a number")
+                    .test(
+                        "dash-period-underscore",
+                        "Dashes, periods and underscores are not permitted to be adjacent to another",
+                        (value) => {
+                            return (
+                                !/\.-/.test(value) &&
+                                !/\.\./.test(value) &&
+                                !/-\./.test(value) &&
+                                !/_\./.test(value) &&
+                                !/\._/.test(value)
+                            );
+                        }
+                    ),
+        }),
 
-                return (
-                    !firstDashRuleRegExp.test(value) &&
-                    !secondDashRuleRegExp.test(value) &&
-                    !thirdDashRuleRegExp.test(value) &&
-                    !fourthDashesRegExp.test(value) &&
-                    !fifthDashesRegExp.test(value)
-                );
-            }
-        ),
     remoteFolderName: yup.string().nullable(),
-    googleCredentialsJson: yupRequiredStringForEnabled
-        .matches(/"type"/, "Google credentials json is missing 'type' field")
-        .matches(/"private_key"/, "Google credentials json is missing 'private_key' field")
-        .matches(/"client_email"/, "Google credentials json is missing 'client_email' field"),
+    googleCredentialsJson: yup
+        .string()
+        .nullable()
+        .when(["isEnabled", "config"], {
+            is: (isEnabled: boolean, config: BackupConfigurationScript) => isEnabled && !config.isOverrideConfig,
+            then: (schema) =>
+                schema
+                    .required()
+                    .matches(/"type"/, "Google credentials json is missing 'type' field")
+                    .matches(/"private_key"/, "Google credentials json is missing 'private_key' field")
+                    .matches(/"client_email"/, "Google credentials json is missing 'client_email' field"),
+        }),
 }).concat(destinationBaseSchema);
 
 const glacierSchema = yupObjectSchema<WithoutAmazonAndBase<GlacierDestination>>({
-    vaultName: yupRequiredStringForEnabled
-        .min(1)
-        .max(255)
-        .matches(
-            /^[A-Za-z0-9_.-]+$/,
-            "Allowed characters are a-z, A-Z, 0-9, '_' (underscore), '-' (hyphen), and '.' (period)"
-        ),
+    vaultName: yup
+        .string()
+        .nullable()
+        .when(["isEnabled", "config"], {
+            is: (isEnabled: boolean, config: BackupConfigurationScript) => isEnabled && !config.isOverrideConfig,
+            then: (schema) =>
+                schema
+                    .required()
+                    .min(1)
+                    .max(255)
+                    .matches(
+                        /^[A-Za-z0-9_.-]+$/,
+                        "Allowed characters are a-z, A-Z, 0-9, '_' (underscore), '-' (hyphen), and '.' (period)"
+                    ),
+        }),
 })
     .concat(destinationBaseSchema)
     .concat(amazonSchema);
 
 const ftpSchema = yupObjectSchema<WithoutBase<FtpDestination>>({
-    url: yupRequiredStringForEnabled.test("ftp-url", "Url must start with ftp:// or ftps://", (value) => {
-        if (!value) {
-            return true;
-        }
-        const lowerCaseValue = value.toLowerCase();
-        return lowerCaseValue.startsWith("ftp://") || lowerCaseValue.startsWith("ftps://");
-    }),
+    url: yup
+        .string()
+        .nullable()
+        .when(["isEnabled", "config"], {
+            is: (isEnabled: boolean, config: BackupConfigurationScript) => isEnabled && !config.isOverrideConfig,
+            then: (schema) =>
+                schema.required().test("ftp-url", "Url must start with ftp:// or ftps://", (value) => {
+                    const lowerCaseValue = value.toLowerCase();
+                    return lowerCaseValue.startsWith("ftp://") || lowerCaseValue.startsWith("ftps://");
+                }),
+        }),
     userName: yupRequiredStringForEnabled,
     password: yupRequiredStringForEnabled,
     certificateAsBase64: yup
