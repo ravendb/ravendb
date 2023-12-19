2884421fbeb803859840941cc65764b032d456d8
Haludi <igal@ravendb.net>
2023-12-19 17:17:14 +0200

    RavenDB-20503 Persist log configuration

---
+++ src/Raven.Server/Config/PersistConfigurationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Raven.Server.Config;
+
+public class PersistConfigurationException : Exception
+{
+    public PersistConfigurationException(string message, Exception exception) 
+        : base(message, exception)
+    {
+        
+    }
+}
