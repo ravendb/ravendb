ab111b965443015821b315a8c5b082cf1f1a40a0
Damian Olszewski <damian@ravendb.net>
2023-12-19 09:27:37 +0100

    RavenDB-21824 Database state load - fail silently

--- src/Raven.Studio/typescript/commands/resources/getDatabaseStateForStudioCommand.ts
+++ src/Raven.Studio/typescript/commands/resources/getDatabaseStateForStudioCommand.ts
@@ -3,25 +3,23 @@ import endpoints = require("endpoints");
 import StudioDatabasesState = Raven.Server.Web.System.Processors.Studio.StudioDatabasesHandlerForGetDatabasesState.StudioDatabasesState;
 
 class getDatabaseStateForStudioCommand extends commandBase {
-
     private readonly nodeTag: string;
     private readonly databaseName: string;
-    
+
     constructor(nodeTag: string, databaseName: string) {
         super();
         this.nodeTag = nodeTag;
         this.databaseName = databaseName;
     }
-    
+
     execute(): JQueryPromise<StudioDatabasesState> {
         const url = endpoints.global.studioDatabases.studioTasksDatabasesState;
         const args = {
             nodeTag: this.nodeTag,
-            name: this.databaseName
-        }
+            name: this.databaseName,
+        };
 
-        return this.query<StudioDatabasesState>(url, args, undefined)
-            .fail((response: JQueryXHR) => this.reportError("Failed to load database state", response.responseText, response.statusText));
+        return this.query<StudioDatabasesState>(url, args, undefined);
     }
 }
 
