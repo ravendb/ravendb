84ca565c737b1cda46c529473bd2787a1d4ad912
Arkadiusz Palinski <arek@ravendb.net>
2024-01-05 15:21:42 +0100

    RavenDB-21888 Introducing WeakReferencingTimer for usages where timer callbacks are referencing objects that are supposed to be finalized by GC. This applies to RequestExecutor and also similar approach was already implemented for the bulk insert heartbeat timer.

---
+++ src/Raven.Client/Util/WeakReferencingTimer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace Raven.Client.Util;
+
+internal class WeakReferencingTimer : IDisposable
+{
+    internal delegate void WeakReferencingTimerCallback(object state);
+
+    private readonly Timer _timer;
+
+    public WeakReferencingTimer(WeakReferencingTimerCallback callback, object state, TimeSpan dueTime, TimeSpan period)
+    {
+        if (callback == null)
+            throw new ArgumentNullException(nameof(callback));
+
+        var internalTimerState = new TimerState
+        {
+            State = state is not null ? new WeakReference<object>(state) : null,
+            Callback = new WeakReference<WeakReferencingTimerCallback>(callback)
+        };
+
+        _timer = new Timer(StaticCallback, internalTimerState, dueTime, period);
+
+        internalTimerState.Timer = _timer;
+    }
+
+    private static void StaticCallback(object state)
+    {
+        var timerState = (TimerState)state;
+        object stateObj = null;
+
+        if (timerState.State is not null && timerState.State.TryGetTarget(out stateObj) == false)
+        {
+            timerState.Timer.Dispose();
+            return;
+        }
+
+        if (timerState.Callback.TryGetTarget(out var callback) == false)
+        {
+            timerState.Timer.Dispose();
+            return;
+        }
+
+        callback(stateObj);
+    }
+
+    private class TimerState
+    {
+        public WeakReference<object> State;
+        public WeakReference<WeakReferencingTimerCallback> Callback;
+        public Timer Timer;
+    }
+
+    public void Dispose()
+    {
+        _timer?.Dispose();
+    }
+
+    public void Change(TimeSpan dueTime, TimeSpan period)
+    {
+        _timer?.Change(dueTime, period);
+    }
+}
