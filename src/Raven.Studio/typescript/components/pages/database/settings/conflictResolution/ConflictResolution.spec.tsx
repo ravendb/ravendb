2e9cdb1a7f4624a96ec48cedcfd72d7819ef0498
Damian Olszewski <damian@ravendb.net>
2024-01-08 15:29:23 +0100

    RavenDB-21623 Add tests

--- src/Raven.Studio/typescript/components/pages/database/settings/conflictResolution/ConflictResolution.spec.tsx
+++ src/Raven.Studio/typescript/components/pages/database/settings/conflictResolution/ConflictResolution.spec.tsx
@@ -1,18 +1,50 @@
 import React from "react";
-import { rtlRender } from "test/rtlTestUtils";
-// import { BelowDatabaseAdmin, NoLimits } from "./ConflictResolution.stories";
+import { RtlScreen, rtlRender, waitForElementToBeRemoved } from "test/rtlTestUtils";
+import * as Stories from "./ConflictResolution.stories";
+import { composeStories } from "@storybook/react";
 
-// describe("ConflictResolution", () => {
-//     it("has full access", async () => {
-//         const { screen } = rtlRender(<NoLimits />);
+const { DefaultConflictResolution } = composeStories(Stories);
 
-//         expect(await screen.findByText("Save")).toBeInTheDocument();
-//     });
+const selectors = {
+    names: {
+        saveAll: /Save/,
+        add: /Add new/,
+        resolveUsingLatest: /If no script was defined for a collection, resolve the conflict using the latest version/,
+    },
+    titles: {
+        saveEdit: /Save changes/,
+        edit: /Edit this script/,
+        delete: /Delete this script/,
+        show: /Show this script/,
+    },
+};
 
-//     it("has limited access'", async () => {
-//         const { screen } = rtlRender(<BelowDatabaseAdmin />);
+describe("ConflictResolution", () => {
+    const waitForLoad = async (screen: RtlScreen) => {
+        await waitForElementToBeRemoved(screen.getByClassName("lazy-load"));
+    };
 
-//         const addScriptButton = screen.queryByName("addNewScriptButton");
-//         expect(addScriptButton).toBeNull();
-//     });
-// });
+    it("can render when database access is admin", async () => {
+        const { screen } = rtlRender(<DefaultConflictResolution databaseAccess="DatabaseAdmin" />);
+        await waitForLoad(screen);
+
+        expect(screen.queryByRole("button", { name: selectors.names.saveAll })).toBeInTheDocument();
+        expect(screen.queryByRole("button", { name: selectors.names.add })).toBeInTheDocument();
+        expect(screen.queryByRole("checkbox", { name: selectors.names.resolveUsingLatest })).toBeEnabled();
+        expect(screen.queryAllByTitle(selectors.titles.edit)).toHaveLength(2);
+        expect(screen.queryAllByTitle(selectors.titles.delete)).toHaveLength(2);
+        expect(screen.queryAllByTitle(selectors.titles.show)).toHaveLength(0);
+    });
+
+    it("can render when database access below admin", async () => {
+        const { screen } = rtlRender(<DefaultConflictResolution databaseAccess="DatabaseRead" />);
+        await waitForLoad(screen);
+
+        expect(screen.queryByRole("button", { name: selectors.names.saveAll })).not.toBeInTheDocument();
+        expect(screen.queryByRole("button", { name: selectors.names.add })).not.toBeInTheDocument();
+        expect(screen.queryByRole("checkbox", { name: selectors.names.resolveUsingLatest })).toBeDisabled();
+        expect(screen.queryAllByTitle(selectors.titles.edit)).toHaveLength(0);
+        expect(screen.queryAllByTitle(selectors.titles.delete)).toHaveLength(0);
+        expect(screen.queryAllByTitle(selectors.titles.show)).toHaveLength(2);
+    });
+});
