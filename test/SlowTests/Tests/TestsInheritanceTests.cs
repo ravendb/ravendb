fbbb3c5542c1601d1caa01394aab0ccc63b10f91
Grisha <grisha@ravendb.net>
2024-01-10 18:36:24 +0200

    RavenDB-21920 - enforce uniqueness of Queue Sink task names

--- test/SlowTests/Tests/TestsInheritanceTests.cs
+++ test/SlowTests/Tests/TestsInheritanceTests.cs
@@ -91,7 +91,7 @@ namespace SlowTests.Tests
 
             var array = types.ToArray();
 
-            const int numberToTolerate = 4632;
+            const int numberToTolerate = 4627;
 
             if (array.Length == numberToTolerate)
                 return;
