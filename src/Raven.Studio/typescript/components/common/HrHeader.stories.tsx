4ab6c53798b4e883a232233647491880430132d1
Damian Olszewski <damian@ravendb.net>
2024-01-11 15:00:30 +0100

    RavenDB-21881 Fix prettier

--- src/Raven.Studio/typescript/components/common/HrHeader.stories.tsx
+++ src/Raven.Studio/typescript/components/common/HrHeader.stories.tsx
@@ -1,4 +1,4 @@
-﻿﻿import { Meta } from "@storybook/react";
+﻿import { Meta } from "@storybook/react";
 import { HrHeader } from "./HrHeader";
 import React from "react";
 import { withBootstrap5, withStorybookContexts } from "test/storybookTestUtils";
