274ee1ce12bcaadacdb51399ff915fefbf27b03e
Marcin Lewandowski <marcin@ravendb.net>
2024-01-16 12:53:41 +0100

    RavenDB-21887 Ability to persist log configuration and traffic watch logging - admin logs

--- src/Raven.Studio/typescript/viewmodels/manage/configureMicrosoftLogsDialog.ts
+++ src/Raven.Studio/typescript/viewmodels/manage/configureMicrosoftLogsDialog.ts
@@ -2,17 +2,15 @@ import dialogViewModelBase = require("viewmodels/dialogViewModelBase");
 import dialog = require("plugins/dialog");
 import aceEditorBindingHandler = require("common/bindingHelpers/aceEditorBindingHandler");
 
-interface ConfigureMicrosoftLogsDialogResult {
-    isEnabled: boolean;
-    configuration: string;
-}
-
 class configureMicrosoftLogsDialog extends dialogViewModelBase {
     
     view = require("views/manage/configureMicrosoftLogsDialog.html");
 
     code = ko.observable<string>("");
     isEnabled = ko.observable<boolean>(false);
+    persist = ko.observable<boolean>(false);
+
+    validationGroup: KnockoutValidationGroup;
     
     constructor(isEnabled:boolean, configuration: string) {
         super();
@@ -21,6 +19,33 @@ class configureMicrosoftLogsDialog extends dialogViewModelBase {
 
         this.code(configuration);
         this.isEnabled(isEnabled);
+
+        this.code.extend({
+            required: {
+                onlyIf: () => this.isEnabled()
+            },
+            aceValidation: true,
+            validation: [
+                {
+                    validator: (val: string) => {
+                        if (!this.isEnabled()) {
+                            return true;
+                        }
+                        try {
+                            const parsedJson = JSON.parse(val);
+                            return _.isPlainObject(parsedJson);
+                        } catch {
+                            return false;
+                        }
+                    },
+                    message: "Code must be valid JSON object"
+                }
+            ]
+        });
+        
+        this.validationGroup = ko.validatedObservable({
+            code: this.code
+        });
     }
 
     attached() {
@@ -32,9 +57,16 @@ class configureMicrosoftLogsDialog extends dialogViewModelBase {
     }
     
     save() {
+        if (!this.isValid(this.validationGroup)) {
+            return ;
+        }
+        
+        const codeAsJson = JSON.parse(this.code());
+        
         const result: ConfigureMicrosoftLogsDialogResult = {
             isEnabled: this.isEnabled(),
-            configuration: this.isEnabled() ? this.code() : null
+            configuration: this.isEnabled() ? codeAsJson : null,
+            persist: this.persist()
         };
         
         dialog.close(this, result);
