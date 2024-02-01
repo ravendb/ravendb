485e18077d480b7e4b90d4b519db0cc8f1134d1c
Stav Lagziel <stav@ravendb.net>
2024-02-01 15:35:55 +0200

    RavenDB-22013 - Null check topology nodes before saving database record in cluster

---
+++ test/SlowTests/Issues/RavenDB_22013.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.ServerWide;
+using Raven.Server.ServerWide.Context;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_22013 : RavenTestBase
+    {
+        public RavenDB_22013(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [RavenFact(RavenTestCategory.None)]
+        public async Task ValidateNodesNotNullInDatabaseRecordBeforeSavingRecordClusterWide()
+        {
+            var databaseName = GetDatabaseName();
+            using (Server.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext context))
+            {
+                context.OpenReadTransaction();
+
+                var databaseRecord = new DatabaseRecord(databaseName);
+                databaseRecord.Topology = new DatabaseTopology()
+                {
+                    Members = new List<string>() { null }
+                };
+                await Server.ServerStore.EnsureNotPassiveAsync();
+
+                var error = await Assert.ThrowsAnyAsync<InvalidOperationException>(async () =>
+                {
+                    var (etag, _) = await Server.ServerStore.WriteDatabaseRecordAsync(databaseName, databaseRecord, null, Guid.NewGuid().ToString());
+                    await Server.ServerStore.Cluster.WaitForIndexNotification(etag);
+                });
+                Assert.Contains("but one of its specified topology nodes is null", error.Message);
+            }
+        }
+    }
+}
