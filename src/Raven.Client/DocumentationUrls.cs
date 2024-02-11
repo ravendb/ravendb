c50c8db2ba7432d91ea5fcefd7620b2d909f25df
Karmel Indych <karmeli87@gmail.com>
2024-02-11 14:12:18 +0200

    RavenDB-21980 API docs for SessionOptions.cs, ClusterTransactionSession.cs & CompareExchangeValue.cs

--- src/Raven.Client/DocumentationUrls.cs
+++ src/Raven.Client/DocumentationUrls.cs
@@ -15,5 +15,26 @@ internal static class DocumentationUrls
             ///<remarks><seealso ref="https://ravendb.net/docs/article-page/6.0/csharp/client-api/session/querying/text-search/highlight-query-results"/></remarks>
             public const string HighlightQueryResults = nameof(HighlightQueryResults);
         }
+
+        internal static class Transactions
+        {
+            ///<remarks><seealso href="https://ravendb.net/docs/article-page/6.0/csharp/client-api/faq/transaction-support"/></remarks>
+            public const string TransactionSupport = nameof(TransactionSupport);
+        }
+
+        internal static class Options
+        {
+            ///<remarks><seealso href="https://ravendb.net/docs/article-page/6.0/csharp/client-api/session/configuration/how-to-disable-tracking#disable-entity-tracking"/></remarks>
+            public const string NoTracking = nameof(NoTracking);
+
+            ///<remarks><seealso href="https://ravendb.net/docs/article-page/6.0/csharp/client-api/session/configuration/how-to-disable-caching"/></remarks>
+            public const string NoCaching = nameof(NoCaching);
+        }
+
+        internal static class Sharding
+        {
+            ///<remarks><seealso href="https://ravendb.net/docs/article-page/6.0/csharp/sharding/administration/anchoring-documents#sharding-anchoring-documents"/></remarks>
+            public const string Anchoring = nameof(Anchoring);
+        }
     }
 }
