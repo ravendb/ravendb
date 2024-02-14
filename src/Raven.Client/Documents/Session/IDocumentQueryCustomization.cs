c67f7bf167de43cbeb451e3bd96bef556ef7085c
Michał Leśniak <michal.lesniak@ravendb.net>
2024-02-14 11:53:12 +0100

    RavenDB-21996 IDocumentQueryCustomization documentation

--- src/Raven.Client/Documents/Session/IDocumentQueryCustomization.cs
+++ src/Raven.Client/Documents/Session/IDocumentQueryCustomization.cs
@@ -19,56 +19,76 @@ namespace Raven.Client.Documents.Session
     public interface IDocumentQueryCustomization
     {
         /// <summary>
-        /// Get the raw query operation that will be sent to the server
+        ///     Get the raw query operation that will be sent to the server.
         /// </summary>
         QueryOperation QueryOperation { get; }
 
         /// <summary>
-        ///     Allow you to modify the index query before it is executed
+        ///     Allows to modify the index query before it is executed.
         /// </summary>
+        /// <param name="action">Action with index query parameter. Defines the method that will be executed before query execution.</param>
+        /// <seealso ref="https://ravendb.net/docs/article-page/6.0/csharp/client-api/session/querying/how-to-customize-query#beforequeryexecuted"/>
         IDocumentQueryCustomization BeforeQueryExecuted(Action<IndexQuery> action);
 
         /// <summary>
-        ///     Callback to get the results of the query
+        ///     Allows to access raw query result after the execution.
         /// </summary>
+        /// <param name="action">Action with query result parameter. Defines the method that will be executed after query execution.</param>
+        /// <seealso ref="https://ravendb.net/docs/article-page/6.0/csharp/client-api/session/querying/how-to-customize-query#afterqueryexecuted"/>
         IDocumentQueryCustomization AfterQueryExecuted(Action<QueryResult> action);
 
         /// <summary>
-        ///     Callback to get the results of the stream
+        ///     Allows to access raw streaming query result (in form of BlittableJsonReaderObject) after the query execution.
         /// </summary>
+        /// <param name="action">Action with stream result parameter. Defines the method that will be executed after streaming query execution.</param>
+        /// <seealso ref="https://ravendb.net/docs/article-page/6.0/csharp/client-api/session/querying/how-to-customize-query#afterstreamexecuted"/>
+        /// <seealso ref="https://ravendb.net/docs/article-page/6.0/csharp/glossary/blittable-json-reader-object"/>
         IDocumentQueryCustomization AfterStreamExecuted(Action<BlittableJsonReaderObject> action);
 
         /// <summary>
-        ///     Disables caching for query results.
+        ///     Disables caching of query results.
+        ///     Forces RavenDB to always fetch query results from the server.
+        ///     By default query results are cached.
         /// </summary>
+        /// <seealso ref="https://ravendb.net/docs/article-page/6.0/csharp/client-api/session/querying/how-to-customize-query#nocaching"/>
+        /// <seealso ref="https://ravendb.net/articles/caching-data-automatic-database-caching"/>
         IDocumentQueryCustomization NoCaching();
 
         /// <summary>
-        ///     Disables tracking for queried entities by Raven's Unit of Work.
+        ///     Disables tracking of query results.
+        ///     Any changes made to them will be ignored by RavenDB.
         ///     Usage of this option will prevent holding query results in memory.
         /// </summary>
+        /// <seealso ref="https://ravendb.net/docs/article-page/6.0/csharp/client-api/session/querying/how-to-customize-query#notracking"/>
+        /// <seealso ref="https://ravendb.net/docs/article-page/6.0/csharp/client-api/session/configuration/how-to-disable-tracking"/>
+        /// <seealso ref="https://ravendb.net/docs/article-page/6.0/csharp/client-api/session/what-is-a-session-and-how-does-it-work#unit-of-work-pattern"/>
         IDocumentQueryCustomization NoTracking();
 
         /// <summary>
-        ///     Order the search results randomly
+        ///     Orders the query results randomly.
         /// </summary>
+        /// <seealso ref="https://ravendb.net/docs/article-page/6.0/csharp/client-api/session/querying/sort-query-results#order-by-random"/>
         IDocumentQueryCustomization RandomOrdering();
 
         /// <summary>
-        ///     Order the search results randomly using the specified seed
-        ///     this is useful if you want to have repeatable random queries
+        ///     Orders the query results randomly using the specified seed.
+        ///     Allows to repeat random query results.
         /// </summary>
+        /// <param name="seed">Seed to be used for pseudorandom number generator.</param>
+        /// <seealso ref="https://ravendb.net/docs/article-page/6.0/csharp/client-api/session/querying/sort-query-results#order-by-random"/>
         IDocumentQueryCustomization RandomOrdering(string seed);
 
 #if FEATURE_CUSTOM_SORTING
         /// <summary>
-        /// Sort using custom sorter on the server
+        ///     Sort query results using server-side custom sorter.
+        ///     Requires custom sorting feature to be enabled.
         /// </summary>
+        /// <param name="typeName">Name of the custom sorter to be used.</param>
+        /// <seealso ref="https://ravendb.net/docs/article-page/6.0/csharp/client-api/session/querying/sort-query-results#custom-sorters"/>
         IDocumentQueryCustomization CustomSortUsing(string typeName);
 
-        /// <summary>
-        /// Sort using custom sorter on the server
-        /// </summary>
+        /// <inheritdoc cref="CustomSortUsing(string)" />
+        /// <param name="descending">Changes order to descending.</param>
         IDocumentQueryCustomization CustomSortUsing(string typeName, bool descending);
 #endif
 
@@ -76,17 +96,20 @@ namespace Raven.Client.Documents.Session
         IDocumentQueryCustomization Timings(out QueryTimings timings);
         
         /// <summary>
-        ///   Instruct the query to wait for non stale results.
-        ///   This shouldn't be used outside of unit tests unless you are well aware of the implications
+        ///     Instruct the query to wait for non-stale results.
+        ///     This shouldn't be used outside of unit tests unless you are well aware of the implications.
         /// </summary>
-        /// <param name = "waitTimeout">Maximum time to wait for index query results to become non-stale before exception is thrown. Default: 15 seconds.</param>
+        /// <param name="waitTimeout">Maximum time in seconds to wait for index query results to become non-stale before exception is thrown. Default: 15 seconds.</param>
         IDocumentQueryCustomization WaitForNonStaleResults(TimeSpan? waitTimeout = null);
 
+        /// <inheritdoc cref="IRawDocumentQuery{T}.Projection" />
         IDocumentQueryCustomization Projection(ProjectionBehavior projectionBehavior);
 
         /// <summary>
-        /// It adds additional shard context to a query so it will be executed only on the relevant shards
+        ///     Allows to execute query only on relevant shards.
         /// </summary>
+        /// <param name="builder">Action with shard context parameter. Defines on which shards the query will be executed.</param>
+        /// <seealso ref="https://ravendb.net/docs/article-page/6.0/csharp/sharding/querying#querying-a-selected-shard"/>
         IDocumentQueryCustomization ShardContext(Action<IQueryShardedContextBuilder> builder);
     }
 }
