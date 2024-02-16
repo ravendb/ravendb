36f01af6a90d2367e22b7b1d77d6ae1a79888b9c
Damian Olszewski <damian@ravendb.net>
2024-02-16 09:20:26 +0100

    RavenDB-16865 Add awesome-debounce-promise package

--- src/Raven.Studio/typescript/components/utils/hooks/useAsyncDebounce.ts
+++ src/Raven.Studio/typescript/components/utils/hooks/useAsyncDebounce.ts
@@ -1,14 +1,15 @@
+import AwesomeDebouncePromise from "awesome-debounce-promise";
 import { useMemo } from "react";
 import { UseAsyncReturn, useAsync } from "react-async-hook";
 
-export function useAsyncDebounce<ReturnType, ParamsType extends unknown[]>(
-    callback: (...args: unknown[]) => Promise<ReturnType>,
+export function useAsyncDebounce<ReturnType, const ParamsType extends unknown[]>(
+    callback: (...args: ParamsType) => Promise<ReturnType>,
     params: ParamsType,
     waitTimeMs = 500
 ): UseAsyncReturn<ReturnType, ParamsType> {
     // debounce should be created only once
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    const debounced = useMemo(() => _.debounce(callback, waitTimeMs), []);
+    const debounced = useMemo(() => AwesomeDebouncePromise(callback, waitTimeMs), []);
 
     return useAsync(debounced, params);
 }
