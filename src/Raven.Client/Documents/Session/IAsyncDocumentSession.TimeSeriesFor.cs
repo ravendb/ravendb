c28c1d8c5fd51d77f7754960e8874c1e74a93197
shiranshalom <shiran@ravendb.net>
2024-02-18 17:46:04 +0200

    RavenDB-21983 - IAsyncDocumentSession.TimeSeriesFor & IDocumentSession.TimeSeriesFor

--- src/Raven.Client/Documents/Session/IAsyncDocumentSession.TimeSeriesFor.cs
+++ src/Raven.Client/Documents/Session/IAsyncDocumentSession.TimeSeriesFor.cs
@@ -12,15 +12,37 @@ namespace Raven.Client.Documents.Session
     /// </summary>
     public partial interface IAsyncDocumentSession
     {
+        /// <summary>
+        /// Provides access to the TimeSeries API for performing server-side operations such as Append, Get, and Delete on a specific Time Series.
+        /// </summary>
         IAsyncSessionDocumentTimeSeries TimeSeriesFor(string documentId, string name);
+
+        /// <inheritdoc cref="TimeSeriesFor"/>
         IAsyncSessionDocumentTimeSeries TimeSeriesFor(object entity, string name);
+
+        /// <inheritdoc cref="TimeSeriesFor"/>
+        /// <typeparam name="TValues">The type of values stored in the Time Series.</typeparam>
         IAsyncSessionDocumentTypedTimeSeries<TValues> TimeSeriesFor<TValues>(string documentId, string name = null) where TValues : new();
+
+        /// <inheritdoc cref="TimeSeriesFor{TValues}"/>
         IAsyncSessionDocumentTypedTimeSeries<TValues> TimeSeriesFor<TValues>(object entity, string name = null) where TValues : new();
+
+        /// <inheritdoc cref="TimeSeriesFor{TValues}"/>
         IAsyncSessionDocumentRollupTypedTimeSeries<TValues> TimeSeriesRollupFor<TValues>(object entity, string policy, string raw = null) where TValues : new();
+
+        /// <inheritdoc cref="TimeSeriesFor{TValues}"/>
         IAsyncSessionDocumentRollupTypedTimeSeries<TValues> TimeSeriesRollupFor<TValues>(string documentId, string policy, string raw = null) where TValues : new();
+
+        /// <inheritdoc cref="TimeSeriesFor"/>
         IAsyncSessionDocumentIncrementalTimeSeries IncrementalTimeSeriesFor(string documentId, string name);
+
+        /// <inheritdoc cref="TimeSeriesFor"/>
         IAsyncSessionDocumentIncrementalTimeSeries IncrementalTimeSeriesFor(object entity, string name);
+
+        /// <inheritdoc cref="TimeSeriesFor{TValues}"/>
         IAsyncSessionDocumentTypedIncrementalTimeSeries<TValues> IncrementalTimeSeriesFor<TValues>(string documentId, string name = null) where TValues : new();
+
+        /// <inheritdoc cref="TimeSeriesFor{TValues}"/>
         IAsyncSessionDocumentTypedIncrementalTimeSeries<TValues> IncrementalTimeSeriesFor<TValues>(object entity, string name = null) where TValues : new();
     }
 }
--- src/Raven.Client/Documents/Session/IDocumentSession.TimeSeriesFor.cs
+++ src/Raven.Client/Documents/Session/IDocumentSession.TimeSeriesFor.cs
@@ -12,15 +12,34 @@ namespace Raven.Client.Documents.Session
     /// </summary>
     public partial interface IDocumentSession
     {
+        /// <inheritdoc cref="IAsyncDocumentSession.TimeSeriesFor"/>
         ISessionDocumentTimeSeries TimeSeriesFor(string documentId, string name);
+
+        /// <inheritdoc cref="IAsyncDocumentSession.TimeSeriesFor"/>
         ISessionDocumentTimeSeries TimeSeriesFor(object entity, string name);
+
+        /// <inheritdoc cref="IAsyncDocumentSession.TimeSeriesFor{TValues}"/>
         ISessionDocumentTypedTimeSeries<TValues> TimeSeriesFor<TValues>(object entity, string name = null) where TValues : new();
+
+        /// <inheritdoc cref="IAsyncDocumentSession.TimeSeriesFor{TValues}"/>
         ISessionDocumentTypedTimeSeries<TValues> TimeSeriesFor<TValues>(string documentId, string name = null) where TValues : new();
+
+        /// <inheritdoc cref="IAsyncDocumentSession.TimeSeriesFor{TValues}"/>
         ISessionDocumentRollupTypedTimeSeries<TValues> TimeSeriesRollupFor<TValues>(object entity, string policy, string raw = null) where TValues : new();
+
+        /// <inheritdoc cref="IAsyncDocumentSession.TimeSeriesFor{TValues}"/>
         ISessionDocumentRollupTypedTimeSeries<TValues> TimeSeriesRollupFor<TValues>(string documentId, string policy, string raw = null) where TValues : new();
+
+        /// <inheritdoc cref="IAsyncDocumentSession.TimeSeriesFor"/>
         ISessionDocumentIncrementalTimeSeries IncrementalTimeSeriesFor(string documentId, string name);
+
+        /// <inheritdoc cref="IAsyncDocumentSession.TimeSeriesFor"/>
         ISessionDocumentIncrementalTimeSeries IncrementalTimeSeriesFor(object entity, string name);
+
+        /// <inheritdoc cref="IAsyncDocumentSession.TimeSeriesFor{TValues}"/>
         ISessionDocumentTypedIncrementalTimeSeries<TValues> IncrementalTimeSeriesFor<TValues>(object entity, string name = null) where TValues : new();
+
+        /// <inheritdoc cref="IAsyncDocumentSession.TimeSeriesFor{TValues}"/>
         ISessionDocumentTypedIncrementalTimeSeries<TValues> IncrementalTimeSeriesFor<TValues>(string documentId, string name = null) where TValues : new();
     }
 }
