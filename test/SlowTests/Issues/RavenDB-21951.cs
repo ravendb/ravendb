3cf63c18c75bed15f5a3ca6823768bc9256adf84
Michał Leśniak <michal.lesniak@ravendb.net>
2024-02-21 16:02:30 +0100

    RavenDB-21951 Added score check to test

--- test/SlowTests/Issues/RavenDB-21951.cs
+++ test/SlowTests/Issues/RavenDB-21951.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using FastTests;
+using Raven.Client;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Indexes;
 using Tests.Infrastructure;
@@ -15,35 +16,45 @@ public class RavenDB_21951 : RavenTestBase
     {
     }
 
-    [RavenFact(RavenTestCategory.Indexes | RavenTestCategory.JavaScript)]
-    public void JavaScriptIndexWithBoostOnIndexEntryShouldWork()
+    [RavenTheory(RavenTestCategory.Indexes | RavenTestCategory.JavaScript)]
+    [RavenData(DatabaseMode = RavenDatabaseMode.Single, SearchEngineMode = RavenSearchEngineMode.Lucene)]
+    public void JavaScriptIndexWithBoostOnIndexEntryShouldWork(Options options)
     {
-        using (var store = GetDocumentStore())
+        using (var store = GetDocumentStore(options))
         {
             using (var session = store.OpenSession())
             {
                 var o1 = new Order() { Name = "CoolName", Freight = 21.37 };
                 var o2 = new Order() { Name = "SomeOtherName", Freight = 1 };
                 var o3 = new Order() { Name = "CoolName", Freight = 10 };
-                
+
                 session.Store(o1);
                 session.Store(o2);
                 session.Store(o3);
-                
-                session.SaveChanges();
-                
-                var index = new DummyIndex();
-                
-                index.Execute(store);
-                
-                Indexes.WaitForIndexing(store);
 
+                session.SaveChanges();
+
+                var index = new DummyIndex();
+
+                index.Execute(store);
+
+                Indexes.WaitForIndexing(store);
+            }
+
+            using (var session = store.OpenSession())
+            {
                 var result = session.Query<Order, DummyIndex>().Where(x => x.Name == "CoolName").OrderByScore().ToList();
-                
+
                 Assert.Equal(2, result.Count);
-                
-                Assert.Equal(21.37, result[0].Freight);
-                Assert.Equal(10, result[1].Freight);
+
+                var metadata1 = session.Advanced.GetMetadataFor(result[0]);
+                var metadata2 = session.Advanced.GetMetadataFor(result[1]);
+
+                var score1 = metadata1[Constants.Documents.Metadata.IndexScore];
+                var score2 = metadata2[Constants.Documents.Metadata.IndexScore];
+
+                Assert.Equal(20, (long)score1);
+                Assert.Equal(10, (long)score2);
             }
         }
     }
