3253b582cc3252e66bc8bb20a5e31c19e3e7d867
Gracjan Sadowicz <gracjan@ravendb.net>
2024-02-22 12:55:13 +0100

    RavenDB-20714 Removed the error cause after successful reproduce

--- test/SlowTests/Issues/RavenDB-20136.cs
+++ test/SlowTests/Issues/RavenDB-20136.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using FastTests.Utils;
 using Raven.Client.Documents.Operations.ETL;
 using SlowTests.Core.Utils.Entities;
@@ -16,7 +17,7 @@ namespace SlowTests.Issues
         }
 
         [Fact]
-        public async void DeletingDocumentWithRevisionsDoesntCorruptEtlProcess()
+        public async Task DeletingDocumentWithRevisionsDoesntCorruptEtlProcess()
         {
             using (var src = GetDocumentStore())
             using (var dst = GetDocumentStore())
@@ -29,13 +30,28 @@ namespace SlowTests.Issues
 
                 AddEtl(src, configuration, new RavenConnectionString {Name = "test", TopologyDiscoveryUrls = dst.Urls, Database = dst.Database,});
 
-                var loadDone = WaitForEtl(src, (_, statistics) => statistics.LoadSuccesses == 2);
+                var loadDone1 = WaitForEtl(src, (_, statistics) => statistics.LoadSuccesses == 1);
+                var loadDone2 = WaitForEtl(src, (_, statistics) => statistics.LoadSuccesses == 2);
                 var deleteDone = WaitForEtl(src, (_, statistics) => statistics.LoadSuccesses == 3);
 
                 using (var session = src.OpenSession())
                 {
-                    session.Store(new User(), "users/1");
+                    session.Store(new User{Name="Bob"}, "users/1");
                     session.SaveChanges();
+                }
+                
+                if (loadDone1.Wait(TimeSpan.FromSeconds(30)) == false)
+                {
+                    TryGetLoadError(src.Database, configuration, out var loadError);
+                    TryGetTransformationError(src.Database, configuration, out var transformationError);
+
+                    Assert.True(false, $"ETL wasn't done. Load error: {loadError?.Error}. Transformation error: {transformationError?.Error}");
+                }
+
+                using (var session = dst.OpenSession())
+                {
+                    var user = session.Load<User>("users/1");
+                    Assert.Equal("Bob", user.Name);
                 }
                 
                 using (var session = src.OpenSession())
@@ -45,7 +61,7 @@ namespace SlowTests.Issues
                     session.SaveChanges();
                 }
                 
-                if (loadDone.Wait(TimeSpan.FromSeconds(30)) == false)
+                if (loadDone2.Wait(TimeSpan.FromSeconds(30)) == false)
                 {
                     TryGetLoadError(src.Database, configuration, out var loadError);
                     TryGetTransformationError(src.Database, configuration, out var transformationError);
@@ -57,6 +73,7 @@ namespace SlowTests.Issues
                 {
                     var user = session.Load<User>("users/1");
                     Assert.NotNull(user);
+                    Assert.Equal("Gracjan", user.Name);
                 }
                 
                 using (var session = src.OpenSession())
