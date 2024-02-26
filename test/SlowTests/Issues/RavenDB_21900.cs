afc21b21fd8466bf7a84278cba39f6b4c10a0c63
Maciej Aszyk <maciej@ravendb.net>
2024-02-26 14:50:25 +0100

    RavenDB-21900 CloneWith should persist if storage can be released.

--- test/SlowTests/Issues/RavenDB_21900.cs
+++ test/SlowTests/Issues/RavenDB_21900.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using FastTests;
 using Orders;
 using Raven.Client.ServerWide;
@@ -45,6 +48,60 @@ public class RavenDB_21900 : RavenTestBase
                 while (stream.MoveNext())
                 {
                     Assert.Equal("Companies/1-A", stream.Current.Document.Company);
+                }
+            }
+        }
+    }
+    
+    [RavenFact(RavenTestCategory.Querying)]
+    public void CanReferencePreviousDocumentInStreamCollectionQuery2()
+    {
+        using (var store = GetDocumentStore(new Options
+               {
+                   ModifyDatabaseRecord = x => x.DocumentsCompression = new DocumentsCompressionConfiguration(compressRevisions: true, compressAllCollections: true)
+               }))
+        {
+            string orderId;
+            using (var session = store.OpenSession())
+            {
+                session.Advanced.MaxNumberOfRequestsPerSession = int.MaxValue;
+                for (var i = 0; i < 1024; ++i)
+                    session.Store(new Order(){Company = "Maciej"});
+                session.SaveChanges();
+
+                var newRand = new Random(1337);
+                var randomIds = session.Advanced.DocumentQuery<Order>().RandomOrdering("1337").SelectFields<string>("id()").Take(64).ToList();
+                var allDocs = session.Query<Order>().ToList();
+
+                foreach (var doc in allDocs)
+                    doc.Employee = randomIds[newRand.Next(randomIds.Count)];
+                
+                session.SaveChanges();
+                
+                var query = session.Advanced.RawQuery<Order>($"from 'Orders' as a " +
+                                                             $"order by a.Company " +
+                                                             $"load a.Employee as orderDoc " +
+                                                             "select orderDoc.Company, a.Freight");
+                var list = query.WaitForNonStaleResults().ToList();
+            }
+
+            using (var session = store.OpenSession())
+            {
+                var query = session.Advanced.RawQuery<Order>($"from 'Orders' as a " +
+                                                             $"order by a.Company " +
+                                                             $"load a.Employee as orderDoc " +
+                                                             "select orderDoc.Company, a.Freight");
+
+                var list = query.ToList();
+                foreach (var order in list)
+                {
+                    Assert.Equal("Maciej", order.Company);
+                }
+
+                var stream = session.Advanced.Stream<Order>(query);
+                while (stream.MoveNext())
+                {
+                    Assert.Equal("Maciej", stream.Current.Document.Company);
                 }
             }
         }
