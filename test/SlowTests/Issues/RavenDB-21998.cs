e9f2e81b9aa1183cda388c2836980f6698a05124
Michał Leśniak <michal.lesniak@ravendb.net>
2024-02-26 12:54:17 +0100

    RavenDB-21998 Updated test to check sharding

--- test/SlowTests/Issues/RavenDB-21998.cs
+++ test/SlowTests/Issues/RavenDB-21998.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Linq;
+using Corax.Querying.Matches.SortingMatches;
 using FastTests;
+using Raven.Client.Documents;
+using Raven.Client.Documents.BulkInsert;
 using Raven.Client.Documents.Indexes;
 using Raven.Client.Documents.Operations;
 using Raven.Client.Documents.Queries.Timings;
@@ -12,44 +15,21 @@ namespace SlowTests.Issues;
 
 public class RavenDB_21998 : RavenTestBase
 {
+    private const int NumberOfDocsToPut = 5000;
     public RavenDB_21998(ITestOutputHelper output) : base(output)
     {
     }
 
-    [RavenFact(RavenTestCategory.Querying)]
-    public void AssertNoSortingWhenThereAreNoTermsInOrderByField()
+    [RavenTheory(RavenTestCategory.Querying | RavenTestCategory.Corax)]
+    [RavenData(DatabaseMode = RavenDatabaseMode.Single, SearchEngineMode = RavenSearchEngineMode.Corax)]
+    public void AssertNoSortingWhenThereAreNoTermsInOrderByField(Options options)
     {
-        const int numberOfDocsToPut = 5000;
-
-        using (var store = GetDocumentStore(Options.ForSearchEngine(RavenSearchEngineMode.Corax)))
+        using (var store = GetDocumentStore(options))
         {
+            PrepareData(store, NumberOfDocsToPut);
+            
             using (var session = store.OpenSession())
             {
-                session.Store(new Question()
-                {
-                    Community = "SomeCommunity"
-                });
-                
-                session.SaveChanges();
-                
-                session.Advanced.DocumentStore.Operations.ForDatabase(session.Advanced.DocumentStore.Database).Send(new PatchByQueryOperation(
-                    """
-                    from Questions
-                    update
-                    {
-                        delete this.CreatedAt;
-                        for (var i = 0; i < 4999; ++i) {
-                            put(id(this)+i, this);
-                        }
-                    }
-                    """)).WaitForCompletion(TimeSpan.FromSeconds(30));
-                
-                var index = new DummyIndex();
-                
-                index.Execute(store);
-                
-                Indexes.WaitForIndexing(store);
-
                 QueryTimings timings = null;
                 
                 var result = session.Query<Question, DummyIndex>().Customize(x => x.Timings(out timings)).Where(x => x.Community == "SomeCommunity").OrderByDescending(x => x.CreatedAt).ToList();
@@ -57,8 +37,92 @@ public class RavenDB_21998 : RavenTestBase
                 Assert.IsType<QueryInspectionNode>(timings.QueryPlan);
                 
                 Assert.DoesNotContain("sorting", ((QueryInspectionNode)timings.QueryPlan).Operation, StringComparison.InvariantCultureIgnoreCase);
-                Assert.Equal(numberOfDocsToPut, result.Count);
+                Assert.Equal(NumberOfDocsToPut, result.Count);
             }
+        }
+    }
+    
+    [RavenTheory(RavenTestCategory.Querying | RavenTestCategory.Corax)]
+    [RavenData(DatabaseMode = RavenDatabaseMode.Single, SearchEngineMode = RavenSearchEngineMode.Corax)]
+    public void AssertOnlyFieldsWithNoTermsAreSkipped(Options options)
+    {
+        const int numberOfDocsToPut = 5000;
+        
+        using (var store = GetDocumentStore(options))
+        {
+            PrepareData(store, numberOfDocsToPut);
+
+            using (var session = store.OpenSession())
+            {
+                QueryTimings timings = null;
+                
+                var result = session.Query<Question, DummyIndex>().Customize(x => x.Timings(out timings)).Where(x => x.Community == "SomeCommunity").OrderBy(x => x.SomeValue).ThenBy(x => x.CreatedAt).ToList();
+                
+                Assert.IsType<QueryInspectionNode>(timings.QueryPlan);
+                
+                Assert.DoesNotContain(nameof(SortingMultiMatch), ((QueryInspectionNode)timings.QueryPlan).Operation, StringComparison.InvariantCultureIgnoreCase);
+                Assert.Contains(nameof(SortingMatch), ((QueryInspectionNode)timings.QueryPlan).Operation, StringComparison.InvariantCultureIgnoreCase);
+                
+                Assert.Equal(NumberOfDocsToPut, result.Count);
+            }
+        }
+    }
+    
+    [RavenTheory(RavenTestCategory.Querying | RavenTestCategory.Corax)]
+    [RavenData(DatabaseMode = RavenDatabaseMode.Sharded, SearchEngineMode = RavenSearchEngineMode.Corax)]
+    public void AssertOnlyFieldsWithNoTermsAreSkippedInSharding(Options options)
+    {
+        using (var store = GetDocumentStore(options))
+        {
+            PrepareData(store, NumberOfDocsToPut);
+            
+            using (var session = store.OpenSession())
+            {
+                var result = session.Query<Question, DummyIndex>().Where(x => x.Community == "SomeCommunity").OrderByDescending(x => x.CreatedAt).ThenByDescending(x => x.SomeValue).ToList();
+                
+                Assert.Equal(Enumerable.Range(1, 5000).Reverse(), result.Select(x => x.SomeValue));
+                Assert.Equal(NumberOfDocsToPut, result.Count);
+            }
+        }
+    }
+
+    private void PrepareData(IDocumentStore store, int numberOfDocumentsToInsert)
+    {
+        using (var session = store.OpenSession())
+        {
+            var q1 = new Question() { Community = "SomeCommunity", SomeValue = 1 };
+            
+            session.Store(q1);
+            
+            using (BulkInsertOperation bulkInsert = store.BulkInsert())
+            {
+                for (int i = 2; i < numberOfDocumentsToInsert + 1; i++)
+                {
+                    bulkInsert.Store(new Question()
+                    {
+                        Id = $"questions/{i}${q1.Id}",
+                        Community = "SomeCommunity",
+                        SomeValue = i
+                    });
+                }
+            }
+            
+            session.SaveChanges();
+            
+            session.Advanced.DocumentStore.Operations.ForDatabase(session.Advanced.DocumentStore.Database).Send(new PatchByQueryOperation(
+                """
+                from Questions
+                update
+                {
+                    delete this.CreatedAt;
+                }
+                """)).WaitForCompletion(TimeSpan.FromSeconds(30));
+
+            var index = new DummyIndex();
+
+            index.Execute(store);
+
+            Indexes.WaitForIndexing(store);
         }
     }
     
@@ -67,6 +131,7 @@ public class RavenDB_21998 : RavenTestBase
         public string Id { get; set; }
         public string Community { get; set; }
         public DateTime CreatedAt { get; set; }
+        public int SomeValue { get; set; }
     }
 
     private class DummyIndex : AbstractIndexCreationTask<Question>
@@ -77,7 +142,8 @@ public class RavenDB_21998 : RavenTestBase
                 select new
                 {
                     question.Community,
-                    question.CreatedAt
+                    question.CreatedAt,
+                    question.SomeValue
                 };
         }
     }
