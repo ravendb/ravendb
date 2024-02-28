32959e2650f683c8695961f3a6a4991a5e4d4251
Damian Olszewski <damian@ravendb.net>
2024-02-28 13:57:56 +0100

    RavenDB-16865 Improve path and topology validation

--- src/Raven.Studio/typescript/components/pages/resources/databases/partials/create/shared/createDatabaseSharedValidation.tsx
+++ src/Raven.Studio/typescript/components/pages/resources/databases/partials/create/shared/createDatabaseSharedValidation.tsx
@@ -4,7 +4,36 @@ export const dataDirectoryStepSchema = yup.object({
     isDefault: yup.boolean(),
     directory: yup.string().when("isDefault", {
         is: false,
-        then: (schema) => schema.required(),
+        then: (schema) =>
+            schema
+                .required()
+                .max(248)
+                .test(
+                    "forbidden-characters",
+                    `Path can't contain any of the following characters: * ? " < > |`,
+                    (value) => {
+                        return /^[^*?"<>|]*$/.test(value);
+                    }
+                )
+                .test("forbidden-characters", "The name is forbidden for use!", (value) => {
+                    return !/^(nul|prn|con|lpt[0-9]|com[0-9])(\.|$)/i.test(value);
+                })
+                .test(
+                    "forbidden-prefix",
+                    "The path is illegal! Paths in RavenDB can't start with 'appdrive:', '~' or '$home'",
+                    (value) => {
+                        if (!value) {
+                            return true;
+                        }
+
+                        const valueLower = value.toLowerCase();
+                        return (
+                            !valueLower.startsWith("~") &&
+                            !valueLower.startsWith("$home") &&
+                            !valueLower.startsWith("appdrive:")
+                        );
+                    }
+                ),
     }),
 });
 
