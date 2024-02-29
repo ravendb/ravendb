e93f3a7e05b7471bc8318445ffc3a08cea311152
Arkadiusz Palinski <arek@ravendb.net>
2024-02-29 09:54:41 +0100

    RavenDB-22105 Added cleanup of RabbitMQ queues defined by tests

---
+++ test/SlowTests/Server/Documents/QueueSink/RabbitMqQueueSinkTestBase.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System;
+using Tests.Infrastructure.ConnectionString;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+using System.Collections.Generic;
+
+namespace SlowTests.Server.Documents.QueueSink;
+
+[Trait("Category", "QueueSink")]
+public abstract class RabbitMqQueueSinkTestBase : QueueSinkTestBase
+{
+    private readonly HashSet<string> _definedQueues = new();
+
+    protected RabbitMqQueueSinkTestBase(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    protected IModel CreateRabbitMqProducer(params string[] queuesToDeclare)
+    {
+        var connectionFactory = new ConnectionFactory() { Uri = new Uri(RabbitMqConnectionString.Instance.VerifiedConnectionString.Value) };
+        var connection = connectionFactory.CreateConnection();
+        var producer = connection.CreateModel();
+
+        foreach (string queue in queuesToDeclare)
+        {
+            producer.QueueDeclare(queue, exclusive: false);
+            _definedQueues.Add(queue);
+        }
+
+        return producer;
+    }
+
+    private void CleanupQueues()
+    {
+        if (_definedQueues.Count == 0 || RequiresRabbitMqRetryFactAttribute.CanConnect == false)
+            return;
+
+        using var channel = CreateRabbitMqProducer();
+        var consumer = new EventingBasicConsumer(channel);
+
+        foreach (string definedExchangeAndQueue in _definedQueues)
+        {
+            consumer.Model.QueueDelete(definedExchangeAndQueue);
+        }
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+        CleanupQueues();
+    }
+}
