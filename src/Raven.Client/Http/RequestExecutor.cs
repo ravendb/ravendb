d634336a71261bd1823b91530785bc5b76cf8428
Karmel Indych <karmeli87@gmail.com>
2024-03-03 13:47:32 +0200

    RDBC-797 dispose the fastest node timer

--- src/Raven.Client/Http/RequestExecutor.cs
+++ src/Raven.Client/Http/RequestExecutor.cs
@@ -318,6 +318,7 @@ namespace Raven.Client.Http
                 Cache.Dispose();
                 ContextPool.Dispose();
                 _updateTopologyTimer?.Dispose();
+                _nodeSelector?.Dispose();
                 DisposeAllFailedNodesTimers();
                 // shared instance, cannot dispose!
                 //_httpClient.Dispose();
