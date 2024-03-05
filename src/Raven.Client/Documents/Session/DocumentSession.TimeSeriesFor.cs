05ae71fc747162fac0c77e4bd72b84ea23ce535e
shiranshalom <shiran@ravendb.net>
2024-03-05 12:23:14 +0200

    RavenDB-21983 - DocumentSession.TimeSeriesFor.cs

--- src/Raven.Client/Documents/Session/DocumentSession.TimeSeriesFor.cs
+++ src/Raven.Client/Documents/Session/DocumentSession.TimeSeriesFor.cs
@@ -15,6 +15,7 @@ namespace Raven.Client.Documents.Session
     /// </summary>
     public partial class DocumentSession
     {
+        /// <inheritdoc/>
         public ISessionDocumentTimeSeries TimeSeriesFor(string documentId, string name)
         {
             ValidateTimeSeriesName(name);
@@ -22,6 +23,7 @@ namespace Raven.Client.Documents.Session
             return new SessionDocumentTimeSeries<TimeSeriesEntry>(this, documentId, name);
         }
 
+        /// <inheritdoc/>
         public ISessionDocumentTimeSeries TimeSeriesFor(object entity, string name)
         {
             ValidateTimeSeriesName(name);
@@ -29,6 +31,7 @@ namespace Raven.Client.Documents.Session
             return new SessionDocumentTimeSeries<TimeSeriesEntry>(this, entity, name);
         }
 
+        /// <inheritdoc/>
         public ISessionDocumentTypedTimeSeries<TValues> TimeSeriesFor<TValues>(string documentId, string name = null) where TValues : new()
         {
             var tsName = name ?? TimeSeriesOperations.GetTimeSeriesName<TValues>(Conventions);
@@ -37,6 +40,7 @@ namespace Raven.Client.Documents.Session
             return new SessionDocumentTimeSeries<TValues>(this, documentId, tsName);
         }
 
+        /// <inheritdoc/>
         public ISessionDocumentTypedTimeSeries<TValues> TimeSeriesFor<TValues>(object entity, string name = null) where TValues : new()
         {
             var tsName = name ?? TimeSeriesOperations.GetTimeSeriesName<TValues>(Conventions);
@@ -45,18 +49,21 @@ namespace Raven.Client.Documents.Session
             return new SessionDocumentTimeSeries<TValues>(this, entity, tsName);
         }
 
+        /// <inheritdoc/>
         public ISessionDocumentRollupTypedTimeSeries<TValues> TimeSeriesRollupFor<TValues>(object entity, string policy, string raw = null) where TValues : new()
         {
             var tsName = raw ?? TimeSeriesOperations.GetTimeSeriesName<TValues>(Conventions);
             return new SessionDocumentTimeSeries<TValues>(this, entity, $"{tsName}{TimeSeriesConfiguration.TimeSeriesRollupSeparator}{policy}");
         }
 
+        /// <inheritdoc/>
         public ISessionDocumentRollupTypedTimeSeries<TValues> TimeSeriesRollupFor<TValues>(string documentId, string policy, string raw = null) where TValues : new()
         {
             var tsName = raw ?? TimeSeriesOperations.GetTimeSeriesName<TValues>(Conventions);
             return new SessionDocumentTimeSeries<TValues>(this, documentId, $"{tsName}{TimeSeriesConfiguration.TimeSeriesRollupSeparator}{policy}");
         }
 
+        /// <inheritdoc/>
         public ISessionDocumentIncrementalTimeSeries IncrementalTimeSeriesFor(string documentId, string name)
         {
             ValidateIncrementalTimeSeriesName(name);
@@ -64,6 +71,7 @@ namespace Raven.Client.Documents.Session
             return new SessionDocumentTimeSeries<TimeSeriesEntry>(this, documentId, name);
         }
 
+        /// <inheritdoc/>
         public ISessionDocumentIncrementalTimeSeries IncrementalTimeSeriesFor(object entity, string name)
         {
             ValidateIncrementalTimeSeriesName(name);
@@ -71,6 +79,7 @@ namespace Raven.Client.Documents.Session
             return new SessionDocumentTimeSeries<TimeSeriesEntry>(this, entity, name);
         }
 
+        /// <inheritdoc/>
         public ISessionDocumentTypedIncrementalTimeSeries<TValues> IncrementalTimeSeriesFor<TValues>(string documentId, string name = null) where TValues : new()
         {
             var tsName = name ?? TimeSeriesOperations.GetTimeSeriesName<TValues>(Conventions);
@@ -79,6 +88,7 @@ namespace Raven.Client.Documents.Session
             return new SessionDocumentTimeSeries<TValues>(this, documentId, tsName);
         }
 
+        /// <inheritdoc/>
         public ISessionDocumentTypedIncrementalTimeSeries<TValues> IncrementalTimeSeriesFor<TValues>(object entity, string name = null) where TValues : new()
         {
             var tsName = name ?? TimeSeriesOperations.GetTimeSeriesName<TValues>(Conventions);
