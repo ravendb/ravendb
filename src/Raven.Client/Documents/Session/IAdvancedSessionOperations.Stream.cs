558b8b393a1212d7e5f45c2f12e38cda8b3c6c33
Stav Lagziel <stav@ravendb.net>
2024-03-11 11:54:02 +0200

    RavenDB-21980 - add documentation links

--- src/Raven.Client/Documents/Session/IAdvancedSessionOperations.Stream.cs
+++ src/Raven.Client/Documents/Session/IAdvancedSessionOperations.Stream.cs
@@ -34,24 +34,14 @@ namespace Raven.Client.Documents.Session
         /// </para>
         /// </summary>
         /// <remarks>
-        ///     Does NOT track the entities in the session and will not include its changes when <see cref="IDocumentSession.SaveChanges"/> is called.
+        ///     Does NOT track the entities in the session and will not include its changes when <see cref="IDocumentSession.SaveChanges"/> is called. <br/>
+        /// <inheritdoc cref="DocumentationUrls.Session.Querying.StreamQueryResults.ByQuery"/>
         /// </remarks>
         /// <param name="query">The criteria by which to fetch documents from the database</param>
         /// <returns>An enumerator used to iterate over the collection of documents received via the stream</returns>
         IEnumerator<StreamResult<T>> Stream<T>(IQueryable<T> query);
 
-        /// <summary>
-        ///     Creates a stream to fetch documents that satisfy criteria <paramref name="query"/>.
-        /// <para>
-        ///     Documents are converted to CLR type <typeparamref name="T"/> along the way and wrapped in <see cref="StreamResult{T}"/> containing document metadata.
-        /// </para>
-        /// </summary>
-        /// <remarks>
-        ///     Does NOT track the entities in the session and will not include its changes when <see cref="IDocumentSession.SaveChanges"/> is called.
-        /// </remarks>
-        /// <param name="query">The criteria by which to fetch documents from the database</param>
-        /// <param name="streamQueryStats">Information and statistics about the performed query</param>
-        /// <returns>An enumerator used to iterate over the collection of documents received via the stream</returns>
+        /// <inheritdoc cref="Stream{T}(IQueryable{T})" />
         IEnumerator<StreamResult<T>> Stream<T>(IQueryable<T> query, out StreamQueryStatistics streamQueryStats);
 
         /// <inheritdoc cref="Stream{T}(IQueryable{T}) "/>
@@ -73,7 +63,8 @@ namespace Raven.Client.Documents.Session
         /// </para>
         /// </summary>
         /// <remarks>
-        ///     Does NOT track the entities in the session and will not include its changes when <see cref="IDocumentSession.SaveChanges"/> is called.
+        ///     Does NOT track the entities in the session and will not include its changes when <see cref="IDocumentSession.SaveChanges"/> is called. <br/>
+        /// <inheritdoc cref="DocumentationUrls.Session.Querying.StreamQueryResults.ByPrefix"/>
         /// </remarks>
         /// <example></example>
         /// <param name="startsWith">ID prefix for which documents should be returned. e.g. "products/"</param>
