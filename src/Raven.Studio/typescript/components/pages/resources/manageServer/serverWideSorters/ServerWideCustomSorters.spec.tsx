b224d84e60b0889a8fd8a81d943f5f0d952e4727
Damian Olszewski <damian@ravendb.net>
2024-03-13 13:42:51 +0100

    RavenDB-21625 Add tests to server-wide custom sorter

--- src/Raven.Studio/typescript/components/pages/resources/manageServer/serverWideSorters/ServerWideCustomSorters.spec.tsx
+++ src/Raven.Studio/typescript/components/pages/resources/manageServer/serverWideSorters/ServerWideCustomSorters.spec.tsx
@@ -1,5 +1,34 @@
+import { rtlRender } from "test/rtlTestUtils";
+import * as Stories from "./ServerWideCustomSorters.stories";
+import { composeStories } from "@storybook/testing-react";
+import React from "react";
+
+const { ServerWideCustomSortersStory } = composeStories(Stories);
+
+const selectors = {
+    licenseBadge: /Professional +/,
+    saveButton: /Save changes/,
+    emptyList: /No server-wide custom sorters have been defined/,
+};
+
 describe("ServerWideCustomSorters", () => {
-    it("TODO", () => {
-        // TODO kalczur license, empty list
+    it("can render when feature is not in license", async () => {
+        const { screen, waitForLoad } = rtlRender(<ServerWideCustomSortersStory hasServerWideCustomSorters={false} />);
+        await waitForLoad();
+
+        expect(screen.queryByText(selectors.licenseBadge)).toBeInTheDocument();
+        expect(screen.queryByText(selectors.emptyList)).toBeInTheDocument();
+    });
+
+    it("can render when feature is in license", async () => {
+        const { screen, waitForLoad, fireClick } = rtlRender(
+            <ServerWideCustomSortersStory hasServerWideCustomSorters />
+        );
+        await waitForLoad();
+
+        expect(screen.queryByText(selectors.licenseBadge)).not.toBeInTheDocument();
+
+        await fireClick(screen.getAllByClassName("icon-edit")[0]);
+        expect(screen.queryByText(selectors.saveButton)).toBeInTheDocument();
     });
 });
