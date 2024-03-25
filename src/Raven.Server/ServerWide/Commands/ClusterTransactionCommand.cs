fa67c2e7e0eb3fb0b343d06c46a238796bad0169
shaharhikri <shaharhikri@ravendb.net>
2024-03-25 14:57:34 +0200

    RavenDB-19481 - handling hanging tests

--- src/Raven.Server/ServerWide/Commands/ClusterTransactionCommand.cs
+++ src/Raven.Server/ServerWide/Commands/ClusterTransactionCommand.cs
@@ -858,7 +858,7 @@ namespace Raven.Server.ServerWide.Commands
         }
 
 
-        public static IEnumerable<SingleClusterDatabaseCommand> ReadCommandsBatch<TTransaction>(TransactionOperationContext<TTransaction> context, string database, long? fromCount, long? lastCompletedClusterTransactionIndex = null, long take = 128)
+        public static IEnumerable<SingleClusterDatabaseCommand> ReadCommandsBatch<TTransaction>(TransactionOperationContext<TTransaction> context, string database, long? fromCount, long take = 128, long? lastCompletedClusterTransactionIndex = null)
             where TTransaction : RavenTransaction
         {
             var lowerDb = database.ToLowerInvariant();
