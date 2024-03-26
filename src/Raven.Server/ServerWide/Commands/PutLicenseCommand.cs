26d8c91d38d9baa996e77dae00acd61522f99866
efrat@ravendb.net <efrat@ravendb.net>
2024-03-26 11:47:51 +0200

    RavenDB-21427 Prevent downgrade of the license when features are being used

--- src/Raven.Server/ServerWide/Commands/PutLicenseCommand.cs
+++ src/Raven.Server/ServerWide/Commands/PutLicenseCommand.cs
@@ -1,20 +1,32 @@
 ﻿using Raven.Server.Commercial;
 using Raven.Server.ServerWide.Context;
+using Sparrow.Json;
 using Sparrow.Json.Parsing;
 
 namespace Raven.Server.ServerWide.Commands
 {
     public sealed class PutLicenseCommand : PutValueCommand<License>
     {
+        public bool SkipLicenseAssertion;
+
         public PutLicenseCommand()
         {
             // for deserialization
         }
 
-        public PutLicenseCommand(string name, License license, string uniqueRequestId) : base(uniqueRequestId)
+        public PutLicenseCommand(string name, License license, string uniqueRequestId, bool fromApi = false) : base(uniqueRequestId)
         {
             Name = name;
             Value = license;
+            SkipLicenseAssertion = fromApi;
+        }
+
+        public override DynamicJsonValue ToJson(JsonOperationContext context)
+        {
+            var djv = base.ToJson(context);
+            djv[nameof(SkipLicenseAssertion)] = SkipLicenseAssertion;
+
+            return djv;
         }
 
         public override DynamicJsonValue ValueToJson()
---
+++ test/Tests.Infrastructure/MultiLicenseRequiredFactAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xunit;
+
+namespace Tests.Infrastructure
+{
+    public class MultiLicenseRequiredFactAttribute : FactAttribute
+    {
+        internal static readonly bool HasLicense;
+
+        internal static string SkipMessage = "Requires License to be set via 'RAVEN_LICENSE' environment variable.";
+
+        static MultiLicenseRequiredFactAttribute()
+        {
+            HasLicense = string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("RAVEN_LICENSE")) == false ||
+                         string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("RAVEN_LICENSE_DEVELOPER")) == false ||
+                         string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("RAVEN_LICENSE_COMMUNITY")) == false;
+        }
+
+        public override string Skip
+        {
+            get
+            {
+                if (ShouldSkip(licenseRequired: true))
+                    return SkipMessage;
+
+                return null;
+            }
+        }
+
+        internal static bool ShouldSkip(bool licenseRequired)
+        {
+            if (licenseRequired == false)
+                return false;
+
+            return HasLicense == false;
+        }
+    }
+}
