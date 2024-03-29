357b4cdcba4cc1f432c7308cbbab710fdc5c6873
Arkadiusz Palinski <arek@ravendb.net>
2024-03-29 13:50:37 +0100

    RavenDB-22174 Adding more debug info if the test fails again

--- test/SlowTests/Issues/RavenDB_11255.cs
+++ test/SlowTests/Issues/RavenDB_11255.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using FastTests;
+using Parquet.Data.Rows;
 using Raven.Client.Documents.Indexes;
 using Raven.Server.Documents.Indexes.Static;
 using Raven.Server.ServerWide.Context;
@@ -58,11 +59,7 @@ namespace SlowTests.Issues
                         }
                         catch (OperationCanceledException)
                         {
-                            // index is being replaced, storage environment is being disposed
-                        }
-                        catch (ObjectDisposedException)
-                        {
-                            // index is being replaced, storage environment is being disposed
+                            throw new InvalidOperationException($"Index is already disposed: {index.IsDisposed}. This is not expected. Database disposed: {database.DatabaseShutdown.IsCancellationRequested}. Index Store disposed: {database.IndexStore.IsDisposed}");
                         }
 
                         Thread.Sleep(1000);
