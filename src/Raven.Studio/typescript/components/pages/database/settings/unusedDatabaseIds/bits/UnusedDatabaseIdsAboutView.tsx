23cda2707d6332e41f167604530426a288848682
danielle9897 <danielle.9897@gmail.com>
2024-04-02 16:09:22 +0300

    RavenDB-20615 Added "About" text

--- src/Raven.Studio/typescript/components/pages/database/settings/unusedDatabaseIds/bits/UnusedDatabaseIdsAboutView.tsx
+++ src/Raven.Studio/typescript/components/pages/database/settings/unusedDatabaseIds/bits/UnusedDatabaseIdsAboutView.tsx
@@ -14,7 +14,29 @@ export default function UnusedDatabaseIdsAboutView() {
                 description="Get additional info on this feature"
                 heading="About this view"
             >
-                Text for Unused Database IDs
+                <ul>
+                    <li className="margin-top-xm">
+                        Each database instance in the cluster has its own database ID (listed under "
+                        <strong>Used IDs</strong>").
+                        <br /> These IDs are used to construct the change-vector generated for each document that is
+                        created or modified within the database.
+                    </li>
+                    <li className="margin-top-xs">
+                        In some scenarios, such as restoring a database from a Snapshot, or when receiving data from a
+                        source database via an External-Replication task, the change-vectors of incoming documents,
+                        which include the source database IDs, are added to the change-vector of the target database.
+                    </li>
+                    <li className="margin-top-xs">
+                        To avoid using these database IDs which are no longer relevant in the current database,
+                        <br /> add them to the list of "<strong>Unused IDs</strong>" so they will not be utilized when
+                        creating new change-vectors.
+                    </li>
+                    <li className="margin-top-xs">
+                        Note:
+                        <br /> Do not add database IDs that are currently in use by other nodes in the database-group to
+                        this list.
+                    </li>
+                </ul>
             </AccordionItemWrapper>
         </AboutViewAnchored>
     );
