ba9594ca0b828b8718cfac5f4cbe8456f757ccd0
Rafal Kwiatkowski <rafal@ravendb.net>
2024-04-04 11:21:58 +0200

    RavenDB-16865 Fix styles

--- src/Raven.Studio/typescript/components/pages/resources/databases/partials/create/shared/CreateDatabaseStepDataDirectory.tsx
+++ src/Raven.Studio/typescript/components/pages/resources/databases/partials/create/shared/CreateDatabaseStepDataDirectory.tsx
@@ -9,6 +9,7 @@ import { useAsyncDebounce } from "components/utils/hooks/useAsyncDebounce";
 import { UseAsyncReturn } from "react-async-hook";
 import { CreateDatabaseFromBackupFormData } from "components/pages/resources/databases/partials/create/formBackup/createDatabaseFromBackupValidation";
 import { CreateDatabaseRegularFormData } from "components/pages/resources/databases/partials/create/regular/createDatabaseRegularValidation";
+import { Icon } from "components/common/Icon";
 
 interface CreateDatabaseStepPathProps {
     manualSelectedNodes: string[];
@@ -91,9 +92,9 @@ function PathInfo({
 
     if (asyncGetDatabaseLocation.status === "loading") {
         return (
-            <Alert color="info" className="mt-2">
+            <div className="mt-2 well p-1 rounded-2 d-flex justify-content-center">
                 <Spinner />
-            </Alert>
+            </div>
         );
     }
 
@@ -102,41 +103,58 @@ function PathInfo({
         : asyncGetDatabaseLocation.result.List;
 
     return (
-        <Alert color="info" className="mt-2">
+        <div className="well mt-2 p-1 rounded-2 vstack gap-1">
             {filteredLocations.map((location) => (
-                <div key={`${location.NodeTag}-${location.FullPath}`}>
-                    <small>
-                        <span>
-                            Node tag: <strong>{location.NodeTag}</strong>
-                        </span>
-                        <br />
-                        <span>
-                            Path: <strong>{location.FullPath}</strong>
-                        </span>
-                        <br />
+                <div
+                    key={`${location.NodeTag}-${location.FullPath}`}
+                    color="node"
+                    className="hstack gap-4 align-items-start card p-2"
+                >
+                    <div className="text-center flex-shrink-0">
+                        <div className="small-label mb-1">Node tag</div>
+                        <strong className="fs-3 text-node">
+                            <Icon icon="node" /> {location.NodeTag}
+                        </strong>
+                    </div>
+                    <div className="vstack flex-grow gap-2">
+                        <div>
+                            <div className="small-label">
+                                <Icon icon="path" />
+                                Path
+                            </div>
+                            <code className="text-info word-break">{location.FullPath}</code>
+                        </div>
+
                         {location.Error ? (
-                            <strong>{location.Error}</strong>
+                            <Alert color="danger">{location.Error}</Alert>
                         ) : (
                             <>
                                 {location.FreeSpaceHumane ? (
-                                    <span>
-                                        Free space: <strong>{location.FreeSpaceHumane}</strong>{" "}
+                                    <div className="hstack gap-3">
+                                        <div>
+                                            <div className="small-label">
+                                                <Icon icon="storage" /> Free space
+                                            </div>
+                                            <strong>{location.FreeSpaceHumane}</strong>
+                                        </div>
+
                                         {location.TotalSpaceHumane && (
-                                            <span>
-                                                {"(Total: "}
+                                            <div>
+                                                <div className="small-label">
+                                                    <Icon icon="storage" /> Total space
+                                                </div>
                                                 <strong>{location.TotalSpaceHumane}</strong>
-                                                {")"}
-                                            </span>
+                                            </div>
                                         )}
-                                    </span>
+                                    </div>
                                 ) : (
-                                    <strong>(Path is unreachable)</strong>
+                                    <Alert color="warning">Path is unreachable</Alert>
                                 )}
                             </>
                         )}
-                    </small>
+                    </div>
                 </div>
             ))}
-        </Alert>
+        </div>
     );
 }
