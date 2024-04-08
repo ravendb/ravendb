185663c034e269f7adc48ff2036487ef8f94436a
egor <egor@ravendb.net>
2024-04-08 14:59:52 +0300

    RavenDB-22226 - Fix removing value in dynamic index

--- src/Voron/Data/Tables/Table.cs
+++ src/Voron/Data/Tables/Table.cs
@@ -655,19 +655,9 @@ namespace Voron.Data.Tables
                 using (dynamicKeyIndexDef.GetValue(_tx, ref value, out Slice val))
                 {
                     dynamicKeyIndexDef.OnIndexEntryChanged(_tx, val, oldValue: ref value, newValue: ref TableValueReaderUtils.EmptyReader);
+
                     var tree = GetTree(dynamicKeyIndexDef);
-
-                    if (dynamicKeyIndexDef.SupportDuplicateKeys == false)
-                    {
-                        tree.Delete(val);
-                        continue;
-                    }
-
-                    var fst = GetFixedSizeTree(tree, val.Clone(_tx.Allocator), 0, dynamicKeyIndexDef.IsGlobal);
-                    if (fst.Delete(id).NumberOfEntriesDeleted == 0)
-                    {
-                        ThrowInvalidAttemptToRemoveValueFromIndexAndNotFindingIt(id, dynamicKeyIndexDef.Name);
-                    }
+                    RemoveValueFromDynamicIndex(id, dynamicKeyIndexDef, tree, val);
                 }
             }
 
@@ -955,7 +945,7 @@ namespace Voron.Data.Tables
                             forceUpdate)
                         {
                             var indexTree = GetTree(dynamicKeyIndexDef);
-                            indexTree.Delete(oldVal);
+                            RemoveValueFromDynamicIndex(id, dynamicKeyIndexDef, indexTree, oldVal);
 
                             AddValueToDynamicIndex(id, dynamicKeyIndexDef, indexTree, newVal, TreeNodeFlags.Data);
                         }
@@ -991,6 +981,22 @@ namespace Voron.Data.Tables
             {
                 var index = GetFixedSizeTree(indexTree, newVal, 0, dynamicKeyIndexDef.IsGlobal);
                 index.Add(id);
+            }
+        }
+
+        private void RemoveValueFromDynamicIndex(long id, DynamicKeyIndexDef dynamicKeyIndexDef, Tree tree, Slice val)
+        {
+            if (dynamicKeyIndexDef.SupportDuplicateKeys == false)
+            {
+                tree.Delete(val);
+            }
+            else
+            {
+                var fst = GetFixedSizeTree(tree, val.Clone(_tx.Allocator), 0, dynamicKeyIndexDef.IsGlobal);
+                if (fst.Delete(id).NumberOfEntriesDeleted == 0)
+                {
+                    ThrowInvalidAttemptToRemoveValueFromIndexAndNotFindingIt(id, dynamicKeyIndexDef.Name);
+                }
             }
         }
 
--- test/FastTests/Voron/Tables/RavenDB_22226.cs
+++ test/FastTests/Voron/Tables/RavenDB_22226.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Sparrow.Server;
+using Tests.Infrastructure;
 using Voron;
 using Voron.Data.Tables;
 using Voron.Impl;
@@ -25,12 +27,13 @@ namespace FastTests.Voron.Tables
             }
         }
 
-        [Fact]
+        [RavenFact(RavenTestCategory.Voron)]
         public void CanInsertUpdateThenReadByDynamic()
         {
             using (var tx = Env.WriteTransaction())
             {
                 Slice.From(tx.Allocator, "RevisionsChangeVector", ByteStringType.Immutable, out var changeVectorSlice);
+                Slice.From(tx.Allocator, "DocumentsChangeVector", ByteStringType.Immutable, out var documentsSlice);
                 Slice.From(tx.Allocator, "Etag", ByteStringType.Immutable, out var etag);
                 var revisionsSchema = new TableSchema();
                 revisionsSchema.DefineKey(new TableSchema.IndexDef
@@ -43,46 +46,83 @@ namespace FastTests.Voron.Tables
                 var index = new TableSchema.DynamicKeyIndexDef { IsGlobal = true, GenerateKey = IndexCvEtagKeyGenerator, Name = IndexName, SupportDuplicateKeys = true };
                 revisionsSchema.DefineIndex(index);
 
-                var cv = Guid.NewGuid().ToString();
+                var cv1 = Guid.NewGuid().ToString();
+                var cv2 = Guid.NewGuid().ToString();
+                var cv3 = Guid.NewGuid().ToString();
+                var shared = Guid.NewGuid().ToString();
 
                 revisionsSchema.Create(tx, "users", 32);
 
                 var usersTbl = tx.OpenTable(revisionsSchema, "users");
 
-                using (usersTbl.Allocate(out var builder))
-                using (Slice.From(tx.Allocator, cv, out var key))
-                {
-                    builder.Add(key);
-                    builder.Add(0L);
-
-                    usersTbl.Insert(builder);
-                }
+                PopulateTable(tx, usersTbl, cv1, shared, 0L);
+                PopulateTable(tx, usersTbl, cv2, shared, 0L);
+                PopulateTable(tx, usersTbl, cv3, shared, 228L);
 
                 using (usersTbl.Allocate(out var builder))
-                using (Slice.From(tx.Allocator, cv, out var key))
+                using (Slice.From(tx.Allocator, cv1, out var key))
                 {
                     usersTbl.ReadByKey(key, out var tvr);
+                    var ptr = tvr.Read(1, out int size);
+                    using var x = Slice.From(tx.Allocator, ptr, size, ByteStringType.Immutable, out var sharedSlice);
+
                     builder.Add(key);
+                    builder.Add(sharedSlice);
                     builder.Add(322L);
                     usersTbl.Update(tvr.Id, builder);
                 }
 
+                var results = new List<(string key, string shared, long etag)>();
                 foreach (var x in usersTbl.SeekForwardFrom(index, Slices.BeforeAllKeys, 0))
                 {
-                    var cv1 = x.Result.Reader.ReadString(0);
-                    var etag1 = x.Result.Reader.ReadLong(1);
-
-                    Assert.Equal(cv, cv1);
-                    Assert.Equal(322, etag1);
+                    results.Add((x.Result.Reader.ReadString(0), x.Result.Reader.ReadString(1), x.Result.Reader.ReadLong(2)));
                 }
+
+                //Assert results
+                Assert.Equal(3, results.Count);
+
+                Assert.Contains(results, x=> x.key == cv1 && x.shared == shared && x.etag == 322L);
+                Assert.Contains(results, x=> x.key == cv2 && x.shared == shared && x.etag == 0L);
+                Assert.Contains(results, x=> x.key == cv3 && x.shared == shared && x.etag == 228L);
+
+                using (Slice.From(tx.Allocator, cv1, out var key))
+                {
+                    usersTbl.ReadByKey(key, out var tvr);
+                    usersTbl.Delete(tvr.Id);
+                }
+
+                results = new List<(string key, string shared, long etag)>();
+                foreach (var x in usersTbl.SeekForwardFrom(index, Slices.BeforeAllKeys, 0))
+                {
+                    results.Add((x.Result.Reader.ReadString(0), x.Result.Reader.ReadString(1), x.Result.Reader.ReadLong(2)));
+                }
+
+                //Assert results
+                Assert.Equal(2, results.Count);
+                Assert.Contains(results, x => x.key == cv2 && x.shared == shared && x.etag == 0L);
+                Assert.Contains(results, x => x.key == cv3 && x.shared == shared && x.etag == 228L);
+            }
+        }
+
+        private void PopulateTable(Transaction tx, Table usersTbl, string cv, string shared, long etag)
+        {
+            using (usersTbl.Allocate(out var builder))
+            using (Slice.From(tx.Allocator, cv, out var key))
+            using (Slice.From(tx.Allocator, shared, out var sharedSlice))
+            {
+                builder.Add(key);
+                builder.Add(sharedSlice);
+                builder.Add(etag);
+
+                usersTbl.Insert(builder);
             }
         }
 
         [StorageIndexEntryKeyGenerator]
         internal static ByteStringContext.Scope IndexCvEtagKeyGenerator(Transaction tx, ref TableValueReader tvr, out Slice slice)
         {
-            var cvPtr = tvr.Read(0, out var cvSize);
-            var etag = tvr.ReadLong(1);
+            var cvPtr = tvr.Read(1, out var cvSize);
+            var etag = tvr.ReadLong(2);
 
             var scope = tx.Allocator.Allocate(sizeof(long) + cvSize, out var buffer);
 
@@ -93,7 +133,5 @@ namespace FastTests.Voron.Tables
             slice = new Slice(buffer);
             return scope;
         }
-
-
     }
 }
