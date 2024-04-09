3d597c398407d027980344d853930d5df85547cd
shiranshalom <shiran@ravendb.net>
2024-04-09 17:33:10 +0300

    RavenDB-22107 - fix failing tests for RavenDB v6.0 ZSTD Linux x64

--- src/Raven.Server/Documents/Commands/Replication/GetReplicationOutgoingsFailureInfoCommand.cs
+++ src/Raven.Server/Documents/Commands/Replication/GetReplicationOutgoingsFailureInfoCommand.cs
@@ -36,7 +36,7 @@ namespace Raven.Server.Documents.Commands.Replication
 
         public override async Task<ResponseDisposeHandling> ProcessResponse(JsonOperationContext context, HttpCache cache, HttpResponseMessage response, string url)
         {
-            await using var stream = await response.Content.ReadAsStreamAsync();
+            await using var stream = await response.Content.ReadAsStreamWithZstdSupportAsync().ConfigureAwait(false);
             using var memoryStream = new MemoryStream();
             await stream.CopyToAsync(memoryStream);
 
