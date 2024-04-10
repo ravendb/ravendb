2caaca3b5aaefb38de4459b5423e30715503d250
Arkadiusz Palinski <arek@ravendb.net>
2024-04-10 14:50:48 +0200

    RavenDB-22151 Added support for new types of memory dumps in /admin/debug/dump endpoint

--- src/Raven.Server/Web/System/AdminDumpHandler.cs
+++ src/Raven.Server/Web/System/AdminDumpHandler.cs
@@ -191,7 +191,9 @@ namespace Raven.Server.Web.System
         private enum DumpType
         {
             Mini,
-            Heap
+            Heap,
+            Full,
+            Triage
         }
     }
 }
