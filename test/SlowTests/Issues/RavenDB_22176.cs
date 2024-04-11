26ded91a6aba3f87de688839dbcf97a5e873500e
Maciej Aszyk <maciej@ravendb.net>
2024-04-11 09:44:43 +0200

    RavenDB-22176 Allow filter notifications with a mask, paging fix

--- test/SlowTests/Issues/RavenDB_22176.cs
+++ test/SlowTests/Issues/RavenDB_22176.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -18,8 +19,10 @@ namespace SlowTests.Issues;
 
 public sealed class RavenDB_22176 : RavenDB_17068_Base
 {
-    [RavenFact(RavenTestCategory.Indexes)]
-    public async Task TestGetNotificationsEndpoint()
+    [RavenTheory(RavenTestCategory.Indexes)]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task TestGetNotificationsEndpoint(bool typeAsNumber)
     {
         using var store = GetDocumentStore();
         var db = await GetDatabase(store.Database);
@@ -43,24 +46,24 @@ public sealed class RavenDB_22176 : RavenDB_17068_Base
             Indexes.WaitForIndexing(store);
 
             //filter nonexisting one
-            var notifications = store.Maintenance.Send(new GetNotifications(alertType: "OperationChanged"));
+            var notifications = store.Maintenance.Send(new GetNotifications(alertType: NotificationTypeParameter.PerformanceHint, testAsNumber: typeAsNumber));
             Assert.Empty(notifications.Results);
             
             //Notification should be raised soon, let's get it as json from get
-            notifications = store.Maintenance.Send(new GetNotifications(alertType: "AlertRaised"));
-            Assert.Contains("We have detected usage of LoadDocument(doc, collectionName) where loaded document collection is different than given parameter", notifications.Results[0].ToString(), StringComparison.InvariantCultureIgnoreCase);
+            notifications = store.Maintenance.Send(new GetNotifications(alertType: NotificationTypeParameter.Alert, testAsNumber: typeAsNumber));
+            Assert.Equal(1, notifications.TotalResults);
 
             //paging test:
             {
-                var notificationsTemp = store.Maintenance.Send(new GetNotifications(pageSize: 0, alertType: "AlertRaised"));
+                var notificationsTemp = store.Maintenance.Send(new GetNotifications(pageSize: 0, alertType: NotificationTypeParameter.Alert, testAsNumber: typeAsNumber));
                 Assert.Equal(1, notificationsTemp.TotalResults);
                 Assert.Empty(notificationsTemp.Results);
                 
-                notificationsTemp = store.Maintenance.Send(new GetNotifications(pageSize: 1, pageStart: 1, alertType: "AlertRaised"));
-                Assert.Equal(0, notificationsTemp.TotalResults);
+                notificationsTemp = store.Maintenance.Send(new GetNotifications(pageSize: 1, pageStart: 1, alertType: NotificationTypeParameter.Alert, testAsNumber: typeAsNumber));
+                Assert.Equal(1, notificationsTemp.TotalResults);
                 Assert.Empty(notificationsTemp.Results);
                 
-                notificationsTemp = store.Maintenance.Send(new GetNotifications(pageSize: 1, pageStart: 0, alertType: "AlertRaised"));
+                notificationsTemp = store.Maintenance.Send(new GetNotifications(pageSize: 1, pageStart: 0, alertType: NotificationTypeParameter.Alert, testAsNumber: typeAsNumber));
                 Assert.Equal(1, notificationsTemp.TotalResults);
                 Assert.NotEmpty(notificationsTemp.Results);
             }
@@ -75,54 +78,167 @@ public sealed class RavenDB_22176 : RavenDB_17068_Base
             //Should be dequeued
             Assert_CheckIfMismatchesAreRemovedOnMatchingLoad(details);
             db.NotificationCenter.Dismiss("AlertRaised/MismatchedReferenceLoad/Indexing");
-            notifications = store.Maintenance.Send(new GetNotifications(alertType: "AlertRaised"));
+            notifications = store.Maintenance.Send(new GetNotifications(alertType: NotificationTypeParameter.Alert, testAsNumber: typeAsNumber));
             Assert.Equal(0, notifications.Results.Count);
+        }
+    }
+
+    [RavenTheory(RavenTestCategory.Monitoring)]
+    [InlineData(NotificationType.AlertRaised, true)]
+    [InlineData(NotificationType.AlertRaised, false)]
+    [InlineData(NotificationType.PerformanceHint, true)]
+    [InlineData(NotificationType.PerformanceHint, false)]
+    public async Task TestGetNotificationsEndpointPaging(NotificationType notificationType, bool flagAsInt)
+    {
+        using var store = GetDocumentStore();
+        var db = await GetDatabase(store.Database);
+        NotificationTypeParameter notificationTypeParameter = notificationType switch
+        {
+            NotificationType.PerformanceHint => NotificationTypeParameter.PerformanceHint,
+            NotificationType.AlertRaised => NotificationTypeParameter.Alert,
+            _ => NotificationTypeParameter.All
+        };
+
+        var notificationsQueue = new AsyncQueue<DynamicJsonValue>();
+        using (db.NotificationCenter.TrackActions(notificationsQueue, null))
+        {
+            for (var id = 0; id < 10; ++id)
+            {
+                Notification notificationEndpointDto = id % 2 == 0
+                    ? AlertRaised.Create("test", id.ToString(), id.ToString(), (AlertType)id, NotificationSeverity.Info)
+                    : PerformanceHint.Create("test", id.ToString(), id.ToString(), (PerformanceHintType)(id % 5), NotificationSeverity.Info, "indexing");
+                db.NotificationCenter.Add(notificationEndpointDto);
+            }
+
+            var result = store.Maintenance.Send(new GetNotifications(pageSize: 10, alertType: notificationTypeParameter, testAsNumber: flagAsInt));
+            NotificationEndpointDto copyForAssertion = result;
+            Assert.Equal(5, result.Results.Count(x => x.Type == notificationType));
+            
+            result = store.Maintenance.Send(new GetNotifications(pageSize: 0, alertType: notificationTypeParameter, testAsNumber: flagAsInt));
+            Assert.Equal(5, result.TotalResults);
+            Assert.Empty(result.Results);
+            
+            //paging
+            {
+                result = store.Maintenance.Send(new GetNotifications(pageStart: 0, pageSize: 2, alertType: notificationTypeParameter, testAsNumber: flagAsInt));
+                Assert.Equal(5, result.TotalResults);
+                Assert.Equal(2, result.Results.Count);
+                Assert.Equal(copyForAssertion.Results[0].Title, result.Results[0].Title);
+                Assert.Equal(copyForAssertion.Results[1].Title, result.Results[1].Title);
+
+                result = store.Maintenance.Send(new GetNotifications(pageStart: 2, pageSize: 2, alertType: notificationTypeParameter, testAsNumber: flagAsInt));
+                Assert.Equal(5, result.TotalResults);
+                Assert.Equal(2, result.Results.Count);
+                Assert.Equal(copyForAssertion.Results[2].Title, result.Results[0].Title);
+                Assert.Equal(copyForAssertion.Results[3].Title, result.Results[1].Title);
+                
+                
+                result = store.Maintenance.Send(new GetNotifications(pageStart: 4, pageSize: 2, alertType: notificationTypeParameter, testAsNumber: flagAsInt));
+                Assert.Equal(5, result.TotalResults);
+                Assert.Equal(1, result.Results.Count);
+                Assert.Equal(copyForAssertion.Results[4].Title, result.Results[0].Title);
+                
+                
+                result = store.Maintenance.Send(new GetNotifications(pageStart: 6, pageSize: 2, alertType: notificationTypeParameter, testAsNumber: flagAsInt));
+                Assert.Equal(5, result.TotalResults);
+                Assert.Equal(0, result.Results.Count);
+            }
+            
+            result = store.Maintenance.Send(new GetNotifications(pageStart: 0, pageSize: 12, testAsNumber: flagAsInt));
+            Assert.Equal(10, result.TotalResults);
+            Assert.Equal(10, result.Results.Count);
+            
+            result = store.Maintenance.Send(new GetNotifications(pageSize: 0, testAsNumber: flagAsInt));
+            Assert.Equal(10, result.TotalResults);
+            Assert.Equal(0, result.Results.Count);
+            
+            //Flags
+            result = store.Maintenance.Send(new GetNotifications(pageStart: 0, pageSize: 12, alertType: NotificationTypeParameter.Alert | NotificationTypeParameter.PerformanceHint, testAsNumber: flagAsInt));
+            Assert.Equal(10, result.TotalResults);
+            Assert.Equal(10, result.Results.Count);
+            
+            //Exception
+            var ex = Assert.ThrowsAny<Exception>(() => store.Maintenance.Send(new GetNotifications(alertType: NotificationTypeParameter.NonExistingFlag, testAsNumber: flagAsInt)));
+            Assert.Contains("Accepted values for type parameter are: [Alert, PerformanceHint]", ex.Message);
+            Assert.Contains("BadRequestException", ex.Message);
         }
     }
 
     public RavenDB_22176(ITestOutputHelper output) : base(output)
     {
     }
-
-    private record Notification(int TotalResults, List<object> Results);
-
-    private class GetNotifications : IMaintenanceOperation<Notification>
+    
+    // ReSharper disable once ClassNeverInstantiated.Local
+    private class SerializableNotification : Notification
     {
-        private string _alertType;
-        private int? _pageStart;
-        private int? _pageSize;
+        public SerializableNotification(NotificationType type, string database) : base(type, database)
+        {
+        }
+
+        public override string Id { get; }
+    }
+    private record NotificationEndpointDto(int TotalResults, List<SerializableNotification> Results);
+    
+    [Flags]
+    private enum NotificationTypeParameter : short
+    {
+        All = 0,
+        Alert = 1,
+        PerformanceHint = 1 << 1,
+        NonExistingFlag = 1 << 2,
+    }
+    private class GetNotifications : IMaintenanceOperation<NotificationEndpointDto>
+    {
+        private readonly NotificationTypeParameter _alertType;
+        private readonly bool _testAsNumber;
+        private readonly bool _includeDefaultFilter;
+        private readonly int? _pageStart;
+        private readonly int? _pageSize;
         
-        public GetNotifications(int? pageStart = null, int? pageSize = null, string alertType = null)
+        public GetNotifications(int? pageStart = null, int? pageSize = null, NotificationTypeParameter alertType = NotificationTypeParameter.All, bool testAsNumber = false, bool includeDefaultFilter = false)
         {
             _pageStart = pageStart;
             _pageSize = pageSize;
             _alertType = alertType;
+            _testAsNumber = testAsNumber;
+            _includeDefaultFilter = includeDefaultFilter;
         }
 
-        public RavenCommand<Notification> GetCommand(DocumentConventions conventions, JsonOperationContext context)
+        public RavenCommand<NotificationEndpointDto> GetCommand(DocumentConventions conventions, JsonOperationContext context)
         {
-            return new GetNotificationsCommand(_alertType, _pageStart, _pageSize);
+            return new GetNotificationsCommand(_alertType, _pageStart, _pageSize, _testAsNumber, _includeDefaultFilter);
         }
 
-        private class GetNotificationsCommand : RavenCommand<Notification>
+        private class GetNotificationsCommand : RavenCommand<NotificationEndpointDto>
         {
-            private string _alertType;
-            private int? _pageStart;
-            private int? _pageSize;
+            private readonly NotificationTypeParameter _alertType;
+            private readonly int? _pageStart;
+            private readonly int? _pageSize;
+            private readonly bool _typeAsNumber;
+            private readonly bool _includeDefaultFilter;
 
-            public GetNotificationsCommand(string alertType, int? pageStart, int? pageSize)
+
+            public GetNotificationsCommand(NotificationTypeParameter alertType, int? pageStart, int? pageSize, bool typeAsNumber, bool includeDefaultFilter)
             {
                 _alertType = alertType;
                 _pageStart = pageStart;
                 _pageSize = pageSize;
+                _typeAsNumber = typeAsNumber;
+                _includeDefaultFilter = includeDefaultFilter;
             }
 
             public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
             {
-                url = $"{node.Url}/databases/{node.Database}/notifications";
+                url = $"{node.Url}/databases/{node.Database}/notifications?";
 
-                if (_alertType != null)
-                    url += $"?type={_alertType}";
+                if (_alertType != NotificationTypeParameter.All || _includeDefaultFilter)
+                {
+                    var type = _typeAsNumber 
+                        ? ((short)_alertType).ToString() 
+                        : UrlEncode(_alertType.ToString());
+                    
+                    url += $"&type={type}";
+                }
 
                 if (_pageSize != null)
                     url += $"&pageSize={_pageSize.Value}";
@@ -138,7 +254,7 @@ public sealed class RavenDB_22176 : RavenDB_17068_Base
                 if (response == null)
                     ThrowInvalidResponse();
 
-                Result = JsonConvert.DeserializeObject<Notification>(response!.ToString());
+                Result = JsonConvert.DeserializeObject<NotificationEndpointDto>(response!.ToString());
             }
 
             public override bool IsReadRequest => true;
