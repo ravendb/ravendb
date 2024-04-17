fb3349bb1078f0da4593adc39825faa019ed3453
Michał Leśniak <michal.lesniak@ravendb.net>
2024-04-17 11:34:40 +0200

    RavenDB-22036 Fixed consecutive side by side resets, moved update force from IndexDefinition to HandleStaticIndexChange

--- test/SlowTests/Issues/RavenDB-22036.cs
+++ test/SlowTests/Issues/RavenDB-22036.cs
@@ -20,7 +20,7 @@ public class RavenDB_22036 : RavenTestBase
     {
         using (var store = GetDocumentStore())
         {
-            var indexName = "Users/ByName";
+            const string indexName = "Users/ByName";
             
             store.Maintenance.Send(new PutIndexesOperation(new IndexDefinition
             {
@@ -54,7 +54,7 @@ public class RavenDB_22036 : RavenTestBase
     {
         using (var store = GetDocumentStore())
         {
-            var indexName = "Users/ByName";
+            const string indexName = "Users/ByName";
             
             store.Maintenance.Send(new PutIndexesOperation(new IndexDefinition
             {
@@ -81,6 +81,59 @@ public class RavenDB_22036 : RavenTestBase
             
             Indexes.WaitForIndexing(store);
 
+            indexesCount = database.IndexStore.GetIndexes().Count();
+            
+            Assert.Equal(1, indexesCount);
+        }
+    }
+    
+    
+    [RavenFact(RavenTestCategory.Indexes)]
+    public void TestConsecutiveSideBySideResets()
+    {
+        using (var store = GetDocumentStore())
+        {
+            const string indexName = "Users/ByName";
+            
+            store.Maintenance.Send(new PutIndexesOperation(new IndexDefinition
+            {
+                Maps = { "from user in docs.Users select new { user.FirstName }" },
+                Type = IndexType.Map,
+                Name = indexName
+            }));
+            
+            store.Maintenance.Send(new StopIndexingOperation());
+
+            store.Maintenance.Send(new ResetIndexOperation(indexName, isSideBySide: true));
+            
+            var database = GetDatabase(store.Database).Result;
+            
+            var replacementIndexInstance = database.IndexStore.GetIndex($"{Constants.Documents.Indexing.SideBySideIndexNamePrefix}{indexName}");
+
+            Assert.NotNull(replacementIndexInstance);
+            
+            var indexesCount = database.IndexStore.GetIndexes().Count();
+            
+            Assert.Equal(2, indexesCount);
+            
+            store.Maintenance.Send(new ResetIndexOperation(indexName, isSideBySide: true));
+            
+            replacementIndexInstance = database.IndexStore.GetIndex($"{Constants.Documents.Indexing.SideBySideIndexNamePrefix}{indexName}");
+
+            Assert.NotNull(replacementIndexInstance);
+            
+            indexesCount = database.IndexStore.GetIndexes().Count();
+            
+            Assert.Equal(2, indexesCount);
+
+            store.Maintenance.Send(new StartIndexingOperation());
+            
+            Indexes.WaitForIndexing(store);
+            
+            replacementIndexInstance = database.IndexStore.GetIndex($"{Constants.Documents.Indexing.SideBySideIndexNamePrefix}{indexName}");
+            
+            Assert.Null(replacementIndexInstance);
+            
             indexesCount = database.IndexStore.GetIndexes().Count();
             
             Assert.Equal(1, indexesCount);
