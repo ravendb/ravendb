0118d6070cd7d87981e1a9654299986f70ab2b35
Maciej Aszyk <maciej@ravendb.net>
2024-04-26 10:34:40 +0200

    RavenDB-22307 GrowableBuffer Tests & Invalid pointer when buffer is growing.

---
+++ test/SlowTests/Corax/GrowableBufferTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Corax.Utils;
+using FastTests.Voron;
+using Raven.Client;
+using Sparrow.Server;
+using Sparrow.Threading;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Corax;
+
+public class GrowableBufferTests : NoDisposalNoOutputNeeded
+{
+    public GrowableBufferTests(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [RavenTheory(RavenTestCategory.Corax)]
+    [InlineData(4 * Sparrow.Global.Constants.Size.Megabyte)]
+    [InlineData(16 * Sparrow.Global.Constants.Size.Megabyte)]
+    public void CanExtendAndNotLooseAnything(int size)
+    {
+        using var bsc = new ByteStringContext(SharedMultipleUseFlag.None);
+        using var growableBuffer = new GrowableBuffer<Slowly>();
+        growableBuffer.Init(bsc, 16);
+        var count = 0;
+        var random = new Random(15235);
+        var random2 = new Random(15235);
+
+        while (Fill(growableBuffer.GetSpace()) is var read and > 0)
+        {
+            growableBuffer.AddUsage(read);
+        }
+        
+        Assert.Equal(size, growableBuffer.Results.Length);
+        var results = growableBuffer.Results;
+        for (var i = 0; i < size; ++i)
+        {
+            Assert.Equal(random2.NextInt64(), results[i]);
+        }
+        
+        int Fill(Span<long> buffer)
+        {
+            var i = 0;
+            for (i = 0; i < buffer.Length && count < size; count++, i++)
+                buffer[i] = random.NextInt64();
+
+            return i;
+        }
+    }
+}
