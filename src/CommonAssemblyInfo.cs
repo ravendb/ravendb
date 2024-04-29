d3117aaac9a5e6461d4136f5844b38d2791d79ad
Arkadiusz Palinski <arek@ravendb.net>
2024-04-29 11:41:16 +0200

    RavenDB-22322 Fixing version after the latest 5.4.118 release. We want to have .200 due to bump to .NET 8

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Resources;
 
 [assembly: AssemblyCopyright("© Hibernating Rhinos 2009 - 2024 All rights reserved.")]
 
-[assembly: AssemblyVersion("5.4.119")]
-[assembly: AssemblyFileVersion("5.4.119.54")]
-[assembly: AssemblyInformationalVersion("5.4.119")]
+[assembly: AssemblyVersion("5.4.200")]
+[assembly: AssemblyFileVersion("5.4.200.54")]
+[assembly: AssemblyInformationalVersion("5.4.200")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
