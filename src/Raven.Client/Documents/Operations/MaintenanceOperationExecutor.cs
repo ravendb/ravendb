f43a5a5c5a5f55773235921a9e97959f3aecd161
Stav Lagziel <stav@ravendb.net>
2024-04-30 15:19:14 +0300

    RavenDB-21993 - fixed an operation path not waiting on the specified node of the operation

--- src/Raven.Client/Documents/Operations/MaintenanceOperationExecutor.cs
+++ src/Raven.Client/Documents/Operations/MaintenanceOperationExecutor.cs
@@ -92,7 +92,8 @@ namespace Raven.Client.Documents.Operations
                 var command = operation.GetCommand(RequestExecutor.Conventions, context);
 
                 await RequestExecutor.ExecuteAsync(command, context, sessionInfo: null, token: token).ConfigureAwait(false);
-                return new Operation<TResult>(RequestExecutor, () => _store.Changes(_databaseName), RequestExecutor.Conventions, command.Result.Result, command.Result.OperationId, command.SelectedNodeTag ?? command.Result.OperationNodeTag);
+                var node = command.SelectedNodeTag ?? command.Result.OperationNodeTag;
+                return new Operation<TResult>(RequestExecutor, () => _store.Changes(_databaseName, node), RequestExecutor.Conventions, command.Result.Result, command.Result.OperationId, node);
             }
         }
 
