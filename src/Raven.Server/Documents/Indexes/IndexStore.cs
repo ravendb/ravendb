6b24f0a5ba04d1553ef72acfbe324cdc8b26c242
Michał Leśniak <michal.lesniak@ravendb.net>
2024-04-30 14:07:31 +0200

    RavenDB-22036 Added tests and throwing exception on side-by-side reset of replacement index

--- src/Raven.Server/Documents/Indexes/IndexStore.cs
+++ src/Raven.Server/Documents/Indexes/IndexStore.cs
@@ -1521,6 +1521,9 @@ namespace Raven.Server.Documents.Indexes
 
         private Index ResetIndexSideBySideInternal(Index index)
         {
+            if (index.Name.StartsWith(Constants.Documents.Indexing.SideBySideIndexNamePrefix))
+                throw new InvalidOperationException($"Index {index.Name} is already a side-by-side running index.");
+            
             try
             {
                 var definitionClone = new IndexDefinition();
