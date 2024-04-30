8a652ff9c7666f73ef59c150a73fa9e6bc00b423
Damian Olszewski <damian@ravendb.net>
2024-04-30 09:00:33 +0200

    RavenDB-21626 Cleanup

--- src/Raven.Studio/typescript/components/pages/database/settings/customAnalyzers/DatabaseCustomAnalyzersList.tsx
+++ src/Raven.Studio/typescript/components/pages/database/settings/customAnalyzers/DatabaseCustomAnalyzersList.tsx
@@ -16,15 +16,19 @@ interface DatabaseCustomAnalyzersListProps {
 
 export default function DatabaseCustomAnalyzersList(props: DatabaseCustomAnalyzersListProps) {
     const { analyzers, fetchStatus, reload, remove, serverWideAnalyzerNames } = props;
+
     if (fetchStatus === "loading") {
         return <LoadingView />;
     }
+
     if (fetchStatus === "error") {
         return <LoadError error="Unable to load custom analyzers" refresh={reload} />;
     }
+
     if (analyzers.length === 0) {
         return <EmptySet>No custom analyzers have been defined</EmptySet>;
     }
+
     return analyzers.map((analyzer, idx) => (
         <DatabaseCustomAnalyzersListItem
             key={analyzer.id}
