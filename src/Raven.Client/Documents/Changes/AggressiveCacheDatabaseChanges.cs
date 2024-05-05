14df3deaf323e335eb654a9571360a40824ba3ac
Grisha Kotler <grisha@ravendb.net>
2024-05-05 10:47:17 +0300

    RavenDB-22331 - invalidate the cache when changes api is disconnected

---
+++ src/Raven.Client/Documents/Changes/AggressiveCacheDatabaseChanges.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Raven.Client.Http;
+
+namespace Raven.Client.Documents.Changes;
+
+internal class AggressiveCacheDatabaseChanges : DatabaseChanges
+{
+    internal AggressiveCacheDatabaseChanges(RequestExecutor requestExecutor, string databaseName) : base(requestExecutor, databaseName, onDispose: null, nodeTag: null)
+    {
+    }
+
+    internal override void NotifyAboutReconnection(Exception e)
+    {
+        NotifyAboutError(e);
+    }
+}
