c09f279a8f9b67e1a36b13afb3de20d8a77635a9
egor <egor@ravendb.net>
2024-05-08 17:44:20 +0300

    RavenDB-21693 - fix database has been unloaded and locked by IdleOperations

---
+++ test/SlowTests/Issues/RavenDB-21693.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using FastTests;
+using Raven.Server.Documents;
+using Sparrow.Server;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+using static SlowTests.Issues.RavenDB_19033;
+
+namespace SlowTests.Issues
+{
+    public class RavenDB_21693 : NoDisposalNeeded
+    {
+        public RavenDB_21693(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [RavenFact(RavenTestCategory.Core)]
+        public async Task ShouldNotLockDatabaseForever()
+        {
+            var dbsCache = new ResourceCache<MyDb>();
+
+            var dbName = "foo";
+
+            var task1 = new Task<MyDb>(() =>
+            {
+                var myDb = new MyDb(dbName);
+
+                myDb.Run();
+
+                return myDb;
+            }, TaskCreationOptions.RunContinuationsAsynchronously);
+
+            var database1 = dbsCache.GetOrAdd(dbName, task1);
+
+            if (database1 == task1)
+            {
+                task1.Start();
+                await task1;
+            }
+
+            var mre = new AsyncManualResetEvent();
+            var mre2 = new AsyncManualResetEvent();
+            Task t = null;
+            dbsCache.ForTestingPurposesOnly().OnUnlikelyTryGet = () =>
+            {
+                mre2.Set();
+                mre.WaitAsync().GetAwaiter().GetResult();
+            };
+
+            using (dbsCache.RemoveLockAndReturn(dbName, x =>
+                   {
+                       x.Dispose();
+                       t = Task.Run(() => dbsCache.TryGetValue(dbName, out _));
+                   }, out _))
+            {
+
+                await mre2.WaitAsync();
+            }
+
+            mre.Set();
+
+            await t;
+
+            Assert.False(dbsCache.TryGetValue(dbName, out var task));
+        }
+    }
+}
