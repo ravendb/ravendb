ca71c3088168089e76bbecc35d295734ff77b8b7
shaharhikri <shaharhikri@ravendb.net>
2024-05-12 13:59:57 +0300

    RavenDB-14963 - All revisions view

--- src/Raven.Server/Web/Studio/StudioCollectionsHandler.cs
+++ src/Raven.Server/Web/Studio/StudioCollectionsHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Raven.Server.Documents;
+using Raven.Server.Documents.Handlers.Processors.Revisions;
 using Raven.Server.Documents.Handlers.Processors.Studio;
 using Raven.Server.Routing;
 using Raven.Server.Web.Studio.Processors;
@@ -19,6 +20,13 @@ namespace Raven.Server.Web.Studio
         public async Task Delete()
         {
             using (var processor = new StudioCollectionHandlerProcessorForDeleteCollection(this))
+                await processor.ExecuteAsync();
+        }
+
+        [RavenAction("/databases/*/studio/revisions/preview", "GET", AuthorizationStatus.ValidUser, EndpointType.Read)]
+        public async Task PreviewRevisions()
+        {
+            using (var processor = new StudioCollectionsHandlerProcessorForPreviewRevisions(this))
                 await processor.ExecuteAsync();
         }
     }
---
+++ src/Raven.Server/Web/Studio/StudioCollectionsHandlerProcessorForPreviewRevisions.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Raven.Client;
+using Raven.Server.Documents;
+using Raven.Server.ServerWide.Context;
+using Raven.Server.Web.Studio.Processors;
+using Sparrow.Json;
+
+namespace Raven.Server.Web.Studio;
+
+internal sealed class StudioCollectionsHandlerProcessorForPreviewRevisions : AbstractStudioCollectionsHandlerProcessorForPreviewRevisions<DatabaseRequestHandler, DocumentsOperationContext>
+{
+    private int _start;
+    private int _pageSize;
+    private long _totalResults;
+
+    public StudioCollectionsHandlerProcessorForPreviewRevisions([NotNull] DatabaseRequestHandler requestHandler) : base(requestHandler)
+    {
+    }
+
+    protected override async Task InitializeAsync(DocumentsOperationContext context, CancellationToken token)
+    {
+        await base.InitializeAsync(context, token);
+
+        _start = RequestHandler.GetStart();
+        _pageSize = RequestHandler.GetPageSize();
+
+        _totalResults = string.IsNullOrEmpty(Collection)
+            ? RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetNumberOfRevisionDocuments(context)
+            : RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetNumberOfRevisionDocumentsForCollection(context, Collection);
+    }
+
+    protected override IDisposable OpenReadTransaction(DocumentsOperationContext context)
+    {
+        return context.OpenReadTransaction();
+    }
+
+    protected override Task WriteItemsAsync(DocumentsOperationContext context, AsyncBlittableJsonTextWriter writer)
+    {
+        writer.WriteStartArray();
+
+        if (_totalResults > 0)
+        {
+            var revisions = string.IsNullOrEmpty(Collection)
+                ? RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetRevisionsInReverseEtagOrder(context, _start, _pageSize)
+                : RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetRevisionsInReverseEtagOrderForCollection(context, Collection, _start, _pageSize);
+
+            var first = true;
+            foreach (var revision in revisions)
+            {
+                if (first)
+                    first = false;
+                else
+                    writer.WriteComma();
+
+                writer.WriteStartObject();
+
+                writer.WritePropertyName(nameof(Document.Id));
+                writer.WriteString(revision.Id);
+                writer.WriteComma();
+
+                writer.WritePropertyName(nameof(Document.Etag));
+                writer.WriteInteger(revision.Etag);
+                writer.WriteComma();
+
+                writer.WritePropertyName(nameof(Document.LastModified));
+                writer.WriteDateTime(revision.LastModified, true);
+                writer.WriteComma();
+
+                writer.WritePropertyName(nameof(Document.ChangeVector));
+                writer.WriteString(revision.ChangeVector);
+                writer.WriteComma();
+
+                writer.WritePropertyName(nameof(Document.Flags));
+                writer.WriteString(revision.Flags.ToString());
+
+                writer.WriteEndObject();
+            }
+        }
+
+        writer.WriteEndArray();
+        return Task.CompletedTask;
+    }
+
+    protected override ValueTask<long> GetTotalCountAsync()
+    {
+        return ValueTask.FromResult(_totalResults);
+    }
+
+    protected override bool NotModified(DocumentsOperationContext context, out string etag)
+    {
+        string changeVector;
+        etag = null;
+
+        changeVector = string.IsNullOrEmpty(Collection)
+            ? RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetLastRevisionChangeVector(context)
+            : RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetLastRevisionChangeVectorForCollection(context, Collection);
+
+        if (changeVector != null)
+            etag = $"{changeVector}/{_totalResults}";
+
+        if (etag == null)
+            return false;
+
+        if (etag == RequestHandler.GetStringFromHeaders(Constants.Headers.IfNoneMatch))
+            return true;
+
+        return false;
+    }
+}
+
