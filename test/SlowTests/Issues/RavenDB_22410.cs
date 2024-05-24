1963e091e7e3a21f03bf355f02b29def1252012a
Maciej Aszyk <maciej@ravendb.net>
2024-05-24 09:23:27 +0200

    RavenDB-22410 Search - analyzer produces multiple tokens from a single words

---
+++ test/SlowTests/Issues/RavenDB_22410.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using FastTests;
+using Raven.Client.Documents;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues;
+
+public class RavenDB_22410 : RavenTestBase
+{
+    public RavenDB_22410(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [RavenTheory(RavenTestCategory.Querying)]
+    [RavenData(SearchEngineMode = RavenSearchEngineMode.All)]
+    public void SearchMethodWhenWordIsTransformedIntoMultipleTokensWeTreatThemAsPhraseQuery(Options options)
+    {
+        using var store = GetDocumentStore(options);
+        using var session = store.OpenSession();
+        session.Store(new Dto("Din ner"), "Dtos/1");
+        session.Store(new Dto("Ner din"), "Dtos/2");
+        session.SaveChanges();
+
+        var results = session.Query<Dto>()
+            .Customize(x => x.WaitForNonStaleResults())
+            .Search(x => x.Search, "din%ner")
+            .ToList();
+        
+        Assert.Equal(1, results.Count);
+        Assert.Equal("Dtos/1", results[0].Id);
+    }
+
+    private record struct Dto(string Search, string Id = null);
+}
