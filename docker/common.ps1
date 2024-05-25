96a903dc386cd0d9ebe81521330d02e5ff0279b0
Grzegorz Lachowski <gregolsky@ravendb.net>
2024-05-26 00:25:41 +0200

    RavenDB-7070 5.4 is not latest

--- docker/common.ps1
+++ docker/common.ps1
@@ -35,8 +35,6 @@ function GetUbuntuImageTags($repo, $version, $arch) {
     switch ($arch) {
         "x64" { 
             return @(
-                "$($repo):latest",
-                "$($repo):ubuntu-latest",
                 "$($repo):latest-lts",
                 "$($repo):ubuntu-latest-lts",
                 "$($repo):5.4-ubuntu-latest",
@@ -46,7 +44,6 @@ function GetUbuntuImageTags($repo, $version, $arch) {
         }
         "arm32v7" {
             return @(
-                "$($repo):ubuntu-arm32v7-latest",
                 "$($repo):ubuntu-arm32v7-latest-lts",
                 "$($repo):5.4-ubuntu-arm32v7-latest",
                 "$($repo):$($version)-ubuntu.22.04-arm32v7"
@@ -55,7 +52,6 @@ function GetUbuntuImageTags($repo, $version, $arch) {
         }
         "arm64v8" {
             return @(
-                "$($repo):ubuntu-arm64v8-latest",
                 "$($repo):ubuntu-arm64v8-latest-lts",
                 "$($repo):5.4-ubuntu-arm64v8-latest",
                 "$($repo):$($version)-ubuntu.22.04-arm64v8"
@@ -74,7 +70,6 @@ function GetWindowsImageTags($repo, $version, $WinVer) {
         "1809" {
             return @(
                 "$($repo):$($version)-windows-1809",
-                "$($repo):windows-1809-latest",
                 "$($repo):windows-1809-latest-lts",
                 "$($repo):5.4-windows-1809-latest"
             )
@@ -83,7 +78,6 @@ function GetWindowsImageTags($repo, $version, $WinVer) {
         "ltsc2022" {
              return @(
                 "$($repo):$($version)-windows-ltsc2022",
-                "$($repo):windows-ltsc2022-latest",
                 "$($repo):windows-ltsc2022-latest-lts",
                 "$($repo):5.4-windows-ltsc2022-latest"
             )
