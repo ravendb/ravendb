98c30c4475238b5e43ad92b20bb1f1a39738970d
Grisha Kotler <grisha@ravendb.net>
2024-05-30 22:34:34 +0300

    RavenDB-21771 - create an abstraction for the Event Listener

---
+++ src/Raven.Server/EventListener/GcEventsListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+
+namespace Raven.Server.EventListener;
+
+public class GcEventsListener : AbstractEventListener
+{
+    private readonly List<GcEventsHandler.GCEventBase> _events = new();
+    private readonly GcEventsHandler _handler;
+
+    protected override DotNetEventType? EventKeywords => DotNetEventType.GC;
+
+    public IReadOnlyCollection<GcEventsHandler.GCEventBase> Events => _events;
+
+    public GcEventsListener()
+    {
+        _handler = new GcEventsHandler(e => _events.Add(e));
+    }
+
+    protected override void OnEventWritten(EventWrittenEventArgs eventData)
+    {
+        if (eventData.EventName == null)
+            return;
+
+        _handler.HandleEvent(eventData);
+    }
+}
