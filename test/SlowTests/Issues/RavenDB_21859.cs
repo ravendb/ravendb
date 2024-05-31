6fe7914e0d1125053e482bd636da9d40c98f1284
Paweł Pekról <ppekrol@ravendb.net>
2024-05-31 10:02:21 +0200

    RavenDB-21859 both Load and LazyLoad should return null when id is null

---
+++ test/SlowTests/Issues/RavenDB_21859.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using FastTests;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues;
+
+public class RavenDB_21859 : RavenTestBase
+{
+    public RavenDB_21859(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [RavenFact(RavenTestCategory.ClientApi)]
+    public async Task Load_And_Lazy_Load_Should_Return_Null_When_Id_Is_Null()
+    {
+        using (var store = GetDocumentStore())
+        {
+            using (var session = store.OpenSession())
+            {
+                Assert.Null(session.Load<object>((string)null));
+
+                Assert.Null(session.Advanced.Lazily.Load<object>((string)null).Value);
+            }
+
+            using (var session = store.OpenAsyncSession())
+            {
+                Assert.Null(await session.LoadAsync<object>((string)null));
+
+                Assert.Null(await session.Advanced.Lazily.LoadAsync<object>((string)null).Value);
+            }
+        }
+    }
+}
