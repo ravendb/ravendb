a3a7469ebbf2baea907cabf66c99e81f01ea63f0
Arkadiusz Paliński <arek@ravendb.net>
2024-06-05 09:52:48 +0200

    Revert "RavenDB-14963 - Revisions view EP"
    This reverts commit 06d338c64764628d06a337d60bd5dc17e9418dd3.

--- src/Raven.Server/Documents/Sharding/Streaming/ShardResultConverter.cs
+++ src/Raven.Server/Documents/Sharding/Streaming/ShardResultConverter.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Raven.Client.Extensions;
 using Sparrow.Json;
 
@@ -16,29 +15,6 @@ namespace Raven.Server.Documents.Sharding.Streaming
                 LowerId = metadata.GetIdAsLazyString(),
                 LastModified = metadata.GetLastModified()
             };
-        }
-
-        public static Document BlittableToRevisionConverter(BlittableJsonReaderObject json)
-        {
-            var revision = new Document();
-
-            if(json.TryGet(nameof(Document.Id), out revision.Id) == false)
-                throw new InvalidOperationException("Revision does not contain 'Id' field.");
-
-            if (json.TryGet(nameof(Document.ChangeVector), out revision.ChangeVector) == false)
-                throw new InvalidOperationException($"Revision of \"{revision.Id}\" does not contain 'ChangeVector' fields.");
-
-            if (json.TryGet(nameof(Document.Etag), out revision.Etag) == false)
-                throw new InvalidOperationException($"Revision of \"{revision.Id}\" and change vector '{revision.ChangeVector}' does not contain 'Etag' fields.");
-
-
-            if (json.TryGet(nameof(Document.LastModified), out revision.LastModified) == false)
-                throw new InvalidOperationException($"Revision of \"{revision.Id}\" and change vector '{revision.ChangeVector}' does not contain 'LastModified' fields.");
-
-            if (json.TryGet(nameof(Document.Flags), out revision.Flags) == false)
-                throw new InvalidOperationException($"Revision of \"{revision.Id}\" and change vector '{revision.ChangeVector}' does not contain 'Flags' fields.");
-
-            return revision;
         }
 
         public static List<string> BlittableToStringListConverter(BlittableJsonReaderArray json)
--- src/Raven.Server/Web/Studio/StudioCollectionsHandler.cs
+++ src/Raven.Server/Web/Studio/StudioCollectionsHandler.cs
@@ -1,6 +1,5 @@
 ﻿using System.Threading.Tasks;
 using Raven.Server.Documents;
-using Raven.Server.Documents.Handlers.Processors.Revisions;
 using Raven.Server.Documents.Handlers.Processors.Studio;
 using Raven.Server.Routing;
 using Raven.Server.Web.Studio.Processors;
@@ -20,13 +19,6 @@ namespace Raven.Server.Web.Studio
         public async Task Delete()
         {
             using (var processor = new StudioCollectionHandlerProcessorForDeleteCollection(this))
-                await processor.ExecuteAsync();
-        }
-
-        [RavenAction("/databases/*/studio/revisions/preview", "GET", AuthorizationStatus.ValidUser, EndpointType.Read)]
-        public async Task PreviewRevisions()
-        {
-            using (var processor = new StudioCollectionsHandlerProcessorForPreviewRevisions(this))
                 await processor.ExecuteAsync();
         }
     }
