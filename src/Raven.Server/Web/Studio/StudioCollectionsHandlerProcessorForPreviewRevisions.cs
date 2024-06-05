604cde254ebc06ad4f2f54094bd9fb4ddfb5edde
Arkadiusz Paliński <arek@ravendb.net>
2024-06-05 09:52:48 +0200

    Revert "RavenDB-14963 - Addressed PR Comments"
    This reverts commit a1991a21e0c6ddeba68172a738a0a747d8c64c99.

--- src/Raven.Server/Web/Studio/StudioCollectionsHandlerProcessorForPreviewRevisions.cs
+++ src/Raven.Server/Web/Studio/StudioCollectionsHandlerProcessorForPreviewRevisions.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Raven.Client;
 using Raven.Server.Documents;
-using Raven.Server.Documents.Schemas;
 using Raven.Server.ServerWide.Context;
 using Raven.Server.Web.Studio.Processors;
 using Sparrow.Json;
@@ -24,66 +22,28 @@ internal sealed class StudioCollectionsHandlerProcessorForPreviewRevisions : Abs
     {
     }
 
-    protected override ValueTask InitializeAsync(DocumentsOperationContext context, CancellationToken token)
+    protected override IAsyncEnumerable<Document> GetRevisionsAsync(DocumentsOperationContext context)
     {
+        if (string.IsNullOrEmpty(_collection))
+            return RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetAllRevisionsOrderedByEtag(context, _start, _pageSize).ToAsyncEnumerable();
+        else
+            return RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetAllRevisionsOrderedByEtagForCollection(context, _collection, _start, _pageSize).ToAsyncEnumerable();
+    }
+
+    protected override async ValueTask InitializeAsync(DocumentsOperationContext context)
+    {
+        await base.InitializeAsync(context);
         _start = RequestHandler.GetStart();
         _pageSize = RequestHandler.GetPageSize();
 
         _totalResults = string.IsNullOrEmpty(_collection)
             ? RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetNumberOfRevisionDocuments(context)
-            : RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetNumberOfRevisionDocumentsForCollection(context, _collection);
-
-        return ValueTask.CompletedTask;
+            : RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetNumberOfRevisionDocuments(context, _collection);
     }
 
     protected override IDisposable OpenReadTransaction(DocumentsOperationContext context)
     {
         return context.OpenReadTransaction();
-    }
-
-    protected override Task WriteItems(DocumentsOperationContext context, AsyncBlittableJsonTextWriter writer)
-    {
-        var revisions = string.IsNullOrEmpty(_collection)
-            ? RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetRevisionsInReverseEtagOrder(context, _start, _pageSize)
-            : RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetRevisionsInReverseEtagOrderForCollection(context, _collection, _start, _pageSize);
-
-        writer.WriteStartArray();
-
-        var first = true;
-        foreach (var revision in revisions)
-        {
-            if (first)
-                first = false;
-            else
-                writer.WriteComma();
-
-            writer.WriteStartObject();
-
-            writer.WritePropertyName(nameof(Document.Id));
-            writer.WriteString(revision.Id);
-            writer.WriteComma();
-
-            writer.WritePropertyName(nameof(Document.Etag));
-            writer.WriteInteger(revision.Etag);
-            writer.WriteComma();
-
-            writer.WritePropertyName(nameof(Document.LastModified));
-            writer.WriteDateTime(revision.LastModified, true);
-            writer.WriteComma();
-
-            writer.WritePropertyName(nameof(Document.ChangeVector));
-            writer.WriteString(revision.ChangeVector);
-            writer.WriteComma();
-
-            writer.WritePropertyName(nameof(Document.Flags));
-            writer.WriteString(revision.Flags.ToString());
-
-            writer.WriteEndObject();
-        }
-
-        writer.WriteEndArray();
-
-        return Task.CompletedTask;
     }
 
     protected override ValueTask<long> GetTotalCountAsync()
@@ -98,7 +58,7 @@ internal sealed class StudioCollectionsHandlerProcessorForPreviewRevisions : Abs
 
         changeVector = string.IsNullOrEmpty(_collection)
             ? RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetLastRevisionChangeVector(context)
-            : RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetLastRevisionChangeVectorForCollection(context, _collection);
+            : RequestHandler.Database.DocumentsStorage.RevisionsStorage.GetLastRevisionChangeVector(context, _collection);
 
         if (changeVector != null)
             etag = $"{changeVector}/{_totalResults}";
