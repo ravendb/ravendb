f228cf6c7e9cc9aecefe28a6b273c3571785e531
shaharhikri <shaharhikri@ravendb.net>
2024-06-20 09:53:18 +0300

    RavenDB-22456 - Create Client API

---
+++ src/Raven.Client/Documents/Commands/RevertDocumentsToRevisionsCommand.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Raven.Client.Documents.Conventions;
+using Raven.Client.Http;
+using Raven.Client.Json;
+using Sparrow.Json;
+
+namespace Raven.Client.Documents.Commands
+{
+    public sealed class RevertDocumentsToRevisionsCommand : RavenCommand
+    {
+        private readonly Dictionary<string, string> _idToChangeVector;
+
+        public RevertDocumentsToRevisionsCommand(Dictionary<string, string> idToChangeVector)
+        {
+            _idToChangeVector = idToChangeVector;
+        }
+
+        public override HttpRequestMessage CreateRequest(JsonOperationContext ctx, ServerNode node, out string url)
+        {
+            url = $"{node.Url}/databases/{node.Database}/revisions/revert/document";
+
+            var request = new RevertDocumentsToRevisionsRequest{ IdToChangeVector = _idToChangeVector };
+
+            return new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                Content = new BlittableJsonContent(async stream =>
+                        await ctx.WriteAsync(stream, DocumentConventions.Default.Serialization.DefaultConverter.ToBlittable(request, ctx)).ConfigureAwait(false),
+                    DocumentConventions.Default)
+            };
+        }
+
+        public override bool IsReadRequest => true;
+    }
+
+    internal class RevertDocumentsToRevisionsRequest
+    {
+        public Dictionary<string, string> IdToChangeVector { get; set; }
+    }
+}
