190a6d88f5bfd35bdaa4337d098f4d1318a5c30b
Gracjan Sadowicz <gracjan@ravendb.net>
2024-06-24 16:07:41 +0200

    RavenDB-17474 Check for settings.json override before checking for certificates

--- docker/ravendb-nanoserver/run-raven.ps1
+++ docker/ravendb-nanoserver/run-raven.ps1
@@ -3,6 +3,10 @@ $ErrorActionPreference = 'Stop'
 $COMMAND=".\Raven.Server.exe"
 $hostname = & "hostname.exe"
 
+if ([string]::IsNullOrEmpty($env:RAVEN_SETTINGS) -eq $False) {
+    Set-Content -Path "settings.json" -Value "$env:RAVEN_SETTINGS"
+}
+
 function Get-RavenServerScheme {
     if (Get-Content "settings.json" -Raw | Select-String -Pattern "Server.Certificate.Path|Server.Certificate.Load.Exec") {
         return "https"
@@ -21,10 +25,6 @@ function Get-RavenServerScheme {
 if ([string]::IsNullOrEmpty($env:RAVEN_ServerUrl)) {
     $RAVEN_SERVER_SCHEME = Get-RavenServerScheme
     $env:RAVEN_ServerUrl = "$RAVEN_SERVER_SCHEME://$hostname:8080"
-}
-
-if ([string]::IsNullOrEmpty($env:RAVEN_SETTINGS) -eq $False) {
-    Set-Content -Path "settings.json" -Value "$env:RAVEN_SETTINGS"
 }
 
 if ([string]::IsNullOrEmpty($env:RAVEN_ARGS) -eq $False) {
