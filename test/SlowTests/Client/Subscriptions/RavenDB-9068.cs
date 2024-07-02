82c6b4e070ce3cbef17324d0208f0e63ec376b3e
egor <egor@ravendb.net>
2024-07-02 11:08:03 +0300

    RavenDB-17300 - fix failing test

--- test/SlowTests/Client/Subscriptions/RavenDB-9068.cs
+++ test/SlowTests/Client/Subscriptions/RavenDB-9068.cs
@@ -35,9 +35,9 @@ namespace SlowTests.Client.Subscriptions
                 cts.Cancel();
 
                 var task = Assert.ThrowsAnyAsync<Exception>(() => subscriptionTask);
-                Assert.True(await task.WaitWithoutExceptionAsync(_reasonableWaitTime));
+                Assert.True(await task.WaitWithoutExceptionAsync(_reasonableWaitTime), "1. await task.WaitWithoutExceptionAsync(_reasonableWaitTime)");
                 var e = await task;
-                Assert.True(e is OperationCanceledException || e is TaskCanceledException);
+                Assert.True(e is TaskCanceledException or OperationCanceledException or AggregateException { InnerException: TaskCanceledException }, $"1. e is TaskCanceledException or OperationCanceledException or AggregateException {{ InnerException: TaskCanceledException }}, {e}");
 
                 subscription = store.Subscriptions.GetSubscriptionWorker<User>(subscriptionId);
                 cts = new CancellationTokenSource();
@@ -45,9 +45,9 @@ namespace SlowTests.Client.Subscriptions
                 cts.Cancel();
 
                 task = Assert.ThrowsAnyAsync<Exception>(() => subscriptionTask);
-                Assert.True(await task.WaitWithoutExceptionAsync(_reasonableWaitTime));
+                Assert.True(await task.WaitWithoutExceptionAsync(_reasonableWaitTime), "2. await task.WaitWithoutExceptionAsync(_reasonableWaitTime)");
                 e = await task;
-                Assert.True(e is OperationCanceledException || e is TaskCanceledException);
+                Assert.True(e is TaskCanceledException or OperationCanceledException or AggregateException { InnerException: TaskCanceledException }, $"2. e is TaskCanceledException or OperationCanceledException or AggregateException {{ InnerException: TaskCanceledException }}, {e}");
             }
         }
     }
