bc883ae69854e33b290b4366726e4471aa91deaf
Michał Leśniak <michal.lesniak@ravendb.net>
2024-07-05 15:09:43 +0200

    RavenDB-22537 Added more tests, changed method visibility to internal

--- test/SlowTests/Issues/RavenDB-22537.cs
+++ test/SlowTests/Issues/RavenDB-22537.cs
@@ -32,18 +32,21 @@ public class RavenDB_22537 : RavenTestBase
                 
                 session.SaveChanges();
                 
+                // One term
                 var names = new List<string>() { null };
                 
                 var res = session.Query<Dto>().Where(x => x.Name.In(names)).ToList();
 
                 Assert.Equal(2, res.Count);
 
+                // Two terms
                 names = new List<string>() { null, "NotNullName" };
                 
                 res = session.Query<Dto>().Where(x => x.Name.In(names)).ToList();
 
                 Assert.Equal(3, res.Count);
                 
+                // Four terms
                 names = new List<string>() { null, "Something1", "Something2", "Something3" };
                 
                 res = session.Query<Dto>().Where(x => x.Name.In(names)).ToList();
@@ -51,6 +54,13 @@ public class RavenDB_22537 : RavenTestBase
                 Assert.Equal(2, res.Count);
                 
                 names = new List<string>() { null, "NotNullName", "Something1", "Something2" };
+                
+                res = session.Query<Dto>().Where(x => x.Name.In(names)).ToList();
+
+                Assert.Equal(3, res.Count);
+                
+                // Five terms
+                names = new List<string>() { null, "NotNullName", "Something1", "Something2", "Something3" };
                 
                 res = session.Query<Dto>().Where(x => x.Name.In(names)).ToList();
 
@@ -77,6 +87,7 @@ public class RavenDB_22537 : RavenTestBase
                 
                 session.SaveChanges();
                 
+                // One term
                 var names = new List<string>() { null };
 
                 var res = session.Query<Dto>().Where(x => x.Names.ContainsAll(names)).ToList();
@@ -89,7 +100,10 @@ public class RavenDB_22537 : RavenTestBase
                 
                 Assert.Equal(1, res.Count);
                 
-                var dto4 = new Dto() { Name = null, Names = new List<string>() { "NotNullName", null } };
+                // Two terms
+                names = new List<string>() { null, "NotNullName" };
+                
+                var dto4 = new Dto() { Name = null, Names = names };
                 
                 session.Store(dto4);
                 
@@ -97,7 +111,20 @@ public class RavenDB_22537 : RavenTestBase
                 
                 Indexes.WaitForIndexing(store);
                 
-                names = new List<string>() { null, "NotNullName" };
+                res = session.Query<Dto>().Where(x => x.Names.ContainsAll(names)).ToList();
+                
+                Assert.Equal(1, res.Count);
+                
+                // Five terms
+                names = new List<string>() { null, "NotNullName", "Something1", "Something2", "Something3" };
+                
+                var dto5 = new Dto() { Name = null, Names = names };
+                
+                session.Store(dto5);
+                
+                session.SaveChanges();
+                
+                Indexes.WaitForIndexing(store);
                 
                 res = session.Query<Dto>().Where(x => x.Names.ContainsAll(names)).ToList();
                 
