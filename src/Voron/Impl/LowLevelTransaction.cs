ed416f26c682c65f44aee440f4f38a57dd234893
Federico Lois <federico.lois@gmail.com>
2024-07-05 17:06:35 -0300

    RavenDB-22181: Remove allocations of events.

--- src/Voron/Impl/LowLevelTransaction.cs
+++ src/Voron/Impl/LowLevelTransaction.cs
@@ -38,7 +38,7 @@ using Constants = Voron.Global.Constants;
 
 namespace Voron.Impl
 {
-    public sealed unsafe class LowLevelTransaction : IDisposable, IDisposableQueryable
+    public sealed unsafe class LowLevelTransaction : IDisposable, IDisposableQueryable, INotifyAllocationFailure
     {
         public readonly Pager DataPager;
         private readonly StorageEnvironment _env;
@@ -184,7 +184,7 @@ namespace Voron.Impl
             _envRecord = previous._envRecord;
             _freeSpaceHandling = previous._freeSpaceHandling;
             _allocator = allocator ?? new ByteStringContext(SharedMultipleUseFlag.None);
-            _allocator.AllocationFailed += MarkTransactionAsFailed;
+            _allocator.RegisterListener(this);
             _disposeAllocator = allocator == null;
             _isValidationEnabled = _env.Options.Encryption.IsEnabled == false;
             _scratchPagesForReads = previous._scratchPagesForReads;
@@ -239,7 +239,7 @@ namespace Voron.Impl
 
             _allocator = new ByteStringContext(SharedMultipleUseFlag.None);
             _disposeAllocator = true;
-            _allocator.AllocationFailed += MarkTransactionAsFailed;
+            _allocator.RegisterListener(this);
             _isValidationEnabled = _env.Options.Encryption.IsEnabled == false;
             _scratchPagesInUse = _env.WriteTransactionPool.ScratchPagesInUse;
             _getPageMethod = GetPageMethod.WriteScratchFirst;
@@ -278,7 +278,7 @@ namespace Voron.Impl
             _freeSpaceHandling = freeSpaceHandling;
 
             _allocator = context ?? new ByteStringContext(SharedMultipleUseFlag.None);
-            _allocator.AllocationFailed += MarkTransactionAsFailed;
+            _allocator.RegisterListener(this);
             _disposeAllocator = context == null;
 
             _isValidationEnabled = _env.Options.Encryption.IsEnabled == false;
@@ -792,8 +792,6 @@ namespace Voron.Impl
                 _env.TransactionCompleted(this);
 
                 _disposableScope.Dispose();
-
-                _allocator.AllocationFailed -= MarkTransactionAsFailed;
               
                 if (_disposeAllocator)
                 {
@@ -809,9 +807,14 @@ namespace Voron.Impl
             }
         }
 
-        public void MarkTransactionAsFailed()
+        internal void MarkTransactionAsFailed()
         {
             _txStatus |= TxStatus.Errored;
+        }
+
+        void INotifyAllocationFailure.OnAllocationFailure<TAllocator>(ByteStringContext<TAllocator> context)
+        {
+            MarkTransactionAsFailed();
         }
 
         internal void FreePageOnCommit(long pageNumber)
