3f72a1ed641ace35b3cb3286b9fdab728cc26fc7
Paweł Pekról <ppekrol@ravendb.net>
2024-07-08 11:25:59 +0200

    RavenDB-22586 fixed test

--- test/Tests.Infrastructure/InterversionTestBase.cs
+++ test/Tests.Infrastructure/InterversionTestBase.cs
@@ -45,7 +45,7 @@ namespace Tests.Infrastructure
         protected const string Server42Version = "4.2.124";
         protected const string Server52Version = "5.2.100";
         protected const string Server53Version = "5.3.100";
-        protected const string Server54Version = "5.4.109";
+        protected const string Server54Version = "5.4.201-nightly-20240706-0301";
 
         protected DocumentStore GetDocumentStore(
             string serverVersion,
