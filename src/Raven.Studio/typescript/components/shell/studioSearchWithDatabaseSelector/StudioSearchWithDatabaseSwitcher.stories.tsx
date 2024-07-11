b5381bf261956a496dc0ceb6b2ec2a34d63b267c
Damian Olszewski <damian@ravendb.net>
2024-07-11 10:42:25 +0200

    RavenDB-4901 Add filter and icons to database switcher

--- src/Raven.Studio/typescript/components/shell/studioSearchWithDatabaseSelector/StudioSearchWithDatabaseSwitcher.stories.tsx
+++ src/Raven.Studio/typescript/components/shell/studioSearchWithDatabaseSelector/StudioSearchWithDatabaseSwitcher.stories.tsx
@@ -1,26 +1,43 @@
 import React from "react";
 import { Meta, StoryObj } from "@storybook/react";
-import { withStorybookContexts, withBootstrap5 } from "test/storybookTestUtils";
+import { withStorybookContexts, withBootstrap5, withForceRerender } from "test/storybookTestUtils";
 import { mockStore } from "test/mocks/store/MockStore";
 import { mockServices } from "test/mocks/services/MockServices";
 import { DatabasesStubs } from "test/stubs/DatabasesStubs";
 import StudioSearchWithDatabaseSwitcher from "./StudioSearchWithDatabaseSwitcher";
+import { DatabaseSharedInfo } from "components/models/databases";
 
 export default {
     title: "Shell/StudioSearchWithDatabaseSwitcher",
-    decorators: [withStorybookContexts, withBootstrap5],
+    decorators: [withStorybookContexts, withBootstrap5, withForceRerender],
 } satisfies Meta;
 
-export const DefaultStory: StoryObj<{ isDatabaseSelected: boolean }> = {
+interface StoryArgs {
+    isDatabaseSelected: boolean;
+}
+
+export const DefaultStory: StoryObj<StoryArgs> = {
     name: "Studio Search With Database Switcher",
     render: (args) => {
         const { databasesService, indexesService, tasksService } = mockServices;
         const { databases, collectionsTracker } = mockStore;
 
-        databases.withDatabases([
-            DatabasesStubs.shardedDatabase().toDto(),
-            DatabasesStubs.nonShardedClusterDatabase().toDto(),
-        ]);
+        const db1: DatabaseSharedInfo = {
+            ...DatabasesStubs.nonShardedSingleNodeDatabase().toDto(),
+            name: "db1_nonSharded",
+            environment: "Production",
+        };
+        const db2: DatabaseSharedInfo = {
+            ...DatabasesStubs.nonShardedSingleNodeDatabase().toDto(),
+            name: "db2_nonSharded",
+            environment: "Testing",
+            isDisabled: true,
+        };
+        const db3: DatabaseSharedInfo = {
+            ...DatabasesStubs.shardedDatabase().toDto(),
+            name: "db3_sharded",
+            environment: "Development",
+        };
 
         if (args.isDatabaseSelected) {
             databases.withActiveDatabase();
@@ -29,6 +46,8 @@ export const DefaultStory: StoryObj<{ isDatabaseSelected: boolean }> = {
             tasksService.withGetTasks();
             collectionsTracker.with_Collections();
         }
+
+        databases.withDatabases([db1, db2, db3]);
 
         return <StudioSearchWithDatabaseSwitcher />;
     },
--- src/Raven.Studio/typescript/components/shell/studioSearchWithDatabaseSelector/databaseSwitcher/DatabaseSwitcher.tsx
+++ src/Raven.Studio/typescript/components/shell/studioSearchWithDatabaseSelector/databaseSwitcher/DatabaseSwitcher.tsx
@@ -1,56 +1,147 @@
 import "./DatabaseSwitcher.scss";
-import React, { useEffect, useState } from "react";
-import databasesManager from "common/shell/databasesManager";
+import React, { useMemo } from "react";
 import { Icon } from "components/common/Icon";
 import { databaseSelectors } from "components/common/shell/databaseSliceSelectors";
 import { useAppSelector } from "components/store";
-import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
+import { Badge } from "reactstrap";
+import IconName from "typings/server/icons";
+import { TextColor } from "components/models/common";
+import assertUnreachable from "components/utils/assertUnreachable";
+import Select from "components/common/select/Select";
+import { components, OptionProps, SingleValueProps } from "react-select";
+import databasesManager from "common/shell/databasesManager";
+type StudioEnvironment = Raven.Client.Documents.Operations.Configuration.StudioConfiguration.StudioEnvironment;
 
 export default function DatabaseSwitcher() {
-    const allDatabaseNames = useAppSelector(databaseSelectors.allDatabaseNames);
+    const allDatabases = useAppSelector(databaseSelectors.allDatabases);
+
+    // Disabled databases are always at the bottom
+    const options: DatabaseOption[] = useMemo(
+        () =>
+            [...allDatabases]
+                .sort((a, b) => (a.isDisabled > b.isDisabled ? 1 : -1))
+                .map((db) => {
+                    return {
+                        value: db.name,
+                        isSharded: db.isSharded,
+                        environment: db.environment,
+                        isDisabled: db.isDisabled,
+                    };
+                }),
+        [allDatabases]
+    );
+
     const activeDatabaseName = useAppSelector(databaseSelectors.activeDatabaseName);
+    const selectedDatabase = options.find((x) => x.value === activeDatabaseName);
 
-    // Switching database firstly sets the active database name to null
-    // To avoid short blinking after the change to null, we wait 500ms
-    const [activeDatabaseNameWithDelay, setActiveDatabaseNameWithDelay] = useState<string>(activeDatabaseName);
-
-    useEffect(() => {
-        if (!activeDatabaseName) {
-            const timeout = setTimeout(() => {
-                setActiveDatabaseNameWithDelay(activeDatabaseName);
-            }, 500);
-
-            return () => {
-                clearTimeout(timeout);
-            };
-        } else {
-            setActiveDatabaseNameWithDelay(activeDatabaseName);
-        }
-    }, [activeDatabaseName]);
+    const handleSelect = (option: DatabaseOption) => {
+        const db = databasesManager.default.getDatabaseByName(option.value);
+        databasesManager.default.activate(db);
+    };
 
     return (
-        <UncontrolledDropdown direction="down" className="elo">
-            <div className="database-switcher">
-                <DropdownToggle caret>
-                    <Icon icon="database" />
-                    {activeDatabaseNameWithDelay || "No database selected"}
-                </DropdownToggle>
-                <div className="dropdown-menu-container">
-                    <DropdownMenu>
-                        {allDatabaseNames.map((databaseName) => (
-                            <DropdownItem
-                                key={databaseName}
-                                onClick={() => {
-                                    const db = databasesManager.default.getDatabaseByName(databaseName);
-                                    databasesManager.default.activate(db);
-                                }}
-                            >
-                                {databaseName}
-                            </DropdownItem>
-                        ))}
-                    </DropdownMenu>
-                </div>
-            </div>
-        </UncontrolledDropdown>
+        <Select
+            className="database-switcher"
+            placeholder={<NoDatabasePlaceholder />}
+            value={selectedDatabase}
+            options={options}
+            components={{ Option: DatabaseOptionItem, SingleValue: DatabaseValue }}
+            onChange={handleSelect}
+        />
     );
+}
+
+function NoDatabasePlaceholder() {
+    return (
+        <span>
+            <Icon icon="database" addon="cancel" />
+            No database selected
+        </span>
+    );
+}
+
+interface DatabaseOption {
+    value: string;
+    isSharded: boolean;
+    environment: StudioEnvironment;
+    isDisabled: boolean;
+}
+
+function DatabaseOptionItem(props: OptionProps<DatabaseOption>) {
+    const { data } = props;
+
+    return (
+        <components.Option {...props}>
+            <DatabaseIcon databaseName={data.value} isSharded={data.isSharded} />
+            <span>{data.value}</span>
+            <DisabledBadge isDisabled={data.isDisabled} />
+            <ServerEnvironmentBadge environment={data.environment} />
+        </components.Option>
+    );
+}
+
+function DatabaseValue({ ...props }: SingleValueProps<DatabaseOption>) {
+    const { data } = props;
+
+    return (
+        <components.SingleValue {...props}>
+            <DatabaseIcon databaseName={data.value} isSharded={data.isSharded} />
+            <span>{data.value}</span>
+            <DisabledBadge isDisabled={data.isDisabled} />
+            <ServerEnvironmentBadge environment={data.environment} />
+        </components.SingleValue>
+    );
+}
+
+function ServerEnvironmentBadge({ environment }: { environment: StudioEnvironment }) {
+    if (!environment || environment === "None") {
+        return null;
+    }
+
+    const getColor = () => {
+        switch (environment) {
+            case "Production":
+                return "danger";
+            case "Testing":
+                return "success";
+            case "Development":
+                return "info";
+            default:
+                return assertUnreachable(environment);
+        }
+    };
+
+    return (
+        <Badge className="ms-2 text-uppercase" color={getColor()} pill>
+            {environment}
+        </Badge>
+    );
+}
+
+function DisabledBadge({ isDisabled }: { isDisabled: boolean }) {
+    if (!isDisabled) {
+        return null;
+    }
+
+    return (
+        <Badge className="ms-2" pill>
+            Disabled
+        </Badge>
+    );
+}
+
+interface DatabaseIconProps {
+    databaseName: string;
+    isSharded: boolean;
+}
+
+function DatabaseIcon({ databaseName, isSharded }: DatabaseIconProps) {
+    if (!databaseName) {
+        return <Icon icon="database" addon="cancel" />;
+    }
+
+    const addon: IconName = isSharded ? "sharding" : null;
+    const color: TextColor = isSharded ? "shard" : "primary";
+
+    return <Icon icon="database" addon={addon} color={color} />;
 }
