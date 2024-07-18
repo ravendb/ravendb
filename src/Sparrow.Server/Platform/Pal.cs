3b24056e4183ec81b0c665c20c6edf682f27a064
Ayende Rahien <ayende@ayende.com>
2024-07-18 22:51:29 +0300

    RavenDB-22457 - Fixing PAL version

--- src/Sparrow.Server/Platform/Pal.cs
+++ src/Sparrow.Server/Platform/Pal.cs
@@ -10,7 +10,7 @@ namespace Sparrow.Server.Platform
 {
     public static unsafe class Pal
     {
-        public const int PAL_VER = 62001; // Should match auto generated rc from rvn_get_pal_ver() @ src/rvngetpalver.c
+        public const int PAL_VER = 62000; // Should match auto generated rc from rvn_get_pal_ver() @ src/rvngetpalver.c
 
         public static readonly PalDefinitions.SystemInformation SysInfo;
         
