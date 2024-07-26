00ba5cec1c00006edbb9906617ad792a9384af03
Ayende Rahien <ayende@ayende.com>
2024-07-26 16:07:24 +0300

    RavenDB-22457 - Refactoring of classes in Pager to dedicated files - Move to Raven.Pal instead of RavenDB.Pal

---
+++ src/Voron/Impl/Paging/Pager.StateFor32Bits.cs
@@ -0,0 +1,34 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using Sparrow;
+
+namespace Voron.Impl.Paging;
+
+public partial class Pager
+{
+    public class TxStateFor32Bits
+    {
+        public Dictionary<long, LoadedPage> LoadedPages = [];
+        public List<MappedAddresses> AddressesToUnload = [];
+        public long TotalLoadedSize;
+    }
+
+    public sealed class MappedAddresses
+    {
+        public string File;
+        public IntPtr Address;
+        public long StartPage;
+        public long Size;
+        public int Usages;
+    }
+
+    public sealed unsafe class LoadedPage
+    {
+        public byte* Pointer;
+        public int NumberOfPages;
+        public long StartPage;
+    }
+
+}
