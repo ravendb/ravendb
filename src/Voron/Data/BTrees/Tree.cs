cbbee556f717eaa18af3cd0c8e7f6aeadfcb9c52
Ayende Rahien <ayende@ayende.com>
2024-07-26 16:12:53 +0300

    RavenDB-22457 - Removing Pager.State to dedicated file - PR comments - Better use of constants

--- src/Voron/Data/BTrees/Tree.cs
+++ src/Voron/Data/BTrees/Tree.cs
@@ -576,7 +576,7 @@ namespace Voron.Data.BTrees
 
         public bool ShouldGoToOverflowPage(int len)
         {
-            return len + Constants.Tree.NodeHeaderSize > (Constants.Tree.PageMaxSpace / 2 - 1); // merge toward the v6.0 branch, temp code!
+            return len + Constants.Tree.NodeHeaderSize > Constants.Tree.NodeMaxSize;
         }
 
         private long WriteToOverflowPages(int overflowSize, out byte* dataPos)
