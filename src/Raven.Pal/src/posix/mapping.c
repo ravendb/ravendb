f1d406fa6bf782e7ee2bb6bf65f1928d5caaf8d9
Ayende Rahien <ayende@ayende.com>
2024-07-28 14:03:14 +0300

    * RavenDB-22457 - Move mmap & munmap to PAL (because of Linux vs. Mac flag differences)

--- src/Raven.Pal/src/posix/mapping.c
+++ src/Raven.Pal/src/posix/mapping.c
@@ -19,6 +19,25 @@
 #include "status_codes.h"
 #include "internal_posix.h"
 
+EXPORT int32_t
+rvn_mmap_anonymous(void** address, uint64_t size, int32_t *detailed_error_code){
+    void* res = rvn_mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if(res == NULL){
+        *detailed_error_code = errno;
+        return FAIL_CALLOC;
+    }
+    *address = res;
+    return SUCCESS;
+}
+
+EXPORT int32_t
+rvn_mumap_anonymous(void* address, uint64_t size, int32_t *detailed_error_code) {
+    if(munmap(address, size) == 0){
+        return SUCCESS;
+    }
+    *detailed_error_code = errno;
+    return FAIL_CALLOC;
+}
 
 EXPORT int32_t
 rvn_discard_virtual_memory(void* address, int64_t size, int32_t* detailed_error_code)
--- src/Raven.Pal/src/win/mapping.c
+++ src/Raven.Pal/src/win/mapping.c
@@ -14,6 +14,27 @@ PRIVATE bool
 _init_discard_virtual_memory_flag = false;
 
 EXPORT int32_t
+rvn_mmap_anonymous(void** address, uint64_t size, int32_t *detailed_error_code){
+    void* res = VirtualAlloc(NULL, size, MEM_COMMIT,PAGE_READWRITE);
+    if(res == NULL){
+        *detailed_error_code = GetLastError();
+        return FAIL_CALLOC;
+    }
+    *address = res;
+    return SUCCESS;
+}
+
+EXPORT int32_t
+rvn_mumap_anonymous(void* address,  uint64_t size, int32_t *detailed_error_code) {
+    (void)size; // unused
+    if(VirtualFree(address, 0, MEM_RELEASE)){
+        return SUCCESS;
+    }
+    *detailed_error_code = GetLastError();
+    return FAIL_CALLOC;
+}
+
+EXPORT int32_t
 rvn_discard_virtual_memory(void* address, int64_t size, int32_t* detailed_error_code)
 {    
     int32_t rc;
