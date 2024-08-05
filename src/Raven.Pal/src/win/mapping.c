7514eb113c7e4959b9e86580300c6cf920532954
Ayende Rahien <ayende@ayende.com>
2024-08-06 00:15:22 +0300

    RavenDB-22465 - Changed rvn_discard_virtual_memory to be no-op on Windows, since it doesn't support memory mapped files

--- src/Raven.Pal/src/win/mapping.c
+++ src/Raven.Pal/src/win/mapping.c
@@ -5,14 +5,6 @@
 #include "status_codes.h"
 #include "internal_win.h"
 
-typedef DWORD (WINAPI *pDiscardVirtualMemory)(PVOID, SIZE_T);
-
-PRIVATE pDiscardVirtualMemory 
-_discard_virtual_memory_func;
-
-PRIVATE bool
-_init_discard_virtual_memory_flag = false;
-
 EXPORT int32_t
 rvn_mmap_anonymous(void** address, uint64_t size, int32_t *detailed_error_code){
     void* res = VirtualAlloc(NULL, size, MEM_COMMIT,PAGE_READWRITE);
@@ -37,37 +29,8 @@ rvn_mumap_anonymous(void* address,  uint64_t size, int32_t *detailed_error_code)
 EXPORT int32_t
 rvn_discard_virtual_memory(void* address, int64_t size, int32_t* detailed_error_code)
 {    
-    int32_t rc;
-    if (_init_discard_virtual_memory_flag == false)
-    {        
-        HMODULE handle = GetModuleHandle(TEXT("kernel32.dll"));
-        if(handle == NULL)
-        {
-            rc = FAIL_GET_MODULE_HANDLE;
-            goto error_cleanup;
-        }
-        
-        _discard_virtual_memory_func = (pDiscardVirtualMemory) GetProcAddress(
-                handle,
-                "DiscardVirtualMemory");
-        _init_discard_virtual_memory_flag = true;
-    }
-
-    if (_discard_virtual_memory_func != NULL)
-    {
-        /*Return Value of DiscardVirtualMemory - ERROR_SUCCESS(0) if successful; a System Error Code otherwise.*/
-        *detailed_error_code = _discard_virtual_memory_func(address, (size_t)size);
-        if (*detailed_error_code != 0)
-        {
-            rc = FAIL_DISCARD_VIRTUAL_MEMORY;
-            goto cleanup;
-        }
-    }
-
-    rc = SUCCESS;
-    goto cleanup;
-error_cleanup:
-    *detailed_error_code = GetLastError();
-cleanup:
-    return rc;
+    // Calling DiscardVirtualMemory on memory mapped files result in ERROR_USER_MAPPED_FILE
+    // on Windows, so we aren't going to try (this is meant as a hint to the OS, not a hard 
+    // requirement  anyway)
+    return SUCCESS;
 }
