69418f26080f3c13fa43735e9ce53a2e2a8aa372
Grisha Kotler <grisha@ravendb.net>
2024-08-06 09:22:06 +0300

    RavenDB-21771 - add the ContentionEventsListener

---
+++ src/Raven.Server/EventListener/ContentionEventsListener.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+
+namespace Raven.Server.EventListener;
+
+public class ContentionEventsListener : AbstractEventListener
+{
+    protected override DotNetEventType? EventKeywords => DotNetEventType.Contention;
+
+    private readonly ContentionEventsHandler _handler;
+    private List<ContentionEventsHandler.ContentionEvent> _events = new();
+
+    public IReadOnlyCollection<ContentionEventsHandler.ContentionEvent> Events => _events;
+
+    public ContentionEventsListener()
+    {
+        _handler = new ContentionEventsHandler(e => _events.Add(e));
+    }
+
+    protected override void OnEventWritten(EventWrittenEventArgs eventData)
+    {
+        if (eventData.EventName == null)
+            return;
+
+        _handler.HandleEvent(eventData);
+    }
+}
