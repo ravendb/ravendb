2ae0736743d49b9840ea3c3e32568cc745f1a953
Karmel Indych <karmeli87@gmail.com>
2024-08-07 09:52:37 +0300

    RavenDB-20985 create new customSettings for each node

--- test/Tests.Infrastructure/ClusterTestBase.cs
+++ test/Tests.Infrastructure/ClusterTestBase.cs
@@ -796,7 +796,7 @@ namespace Tests.Infrastructure
             bool? shouldRunInMemory = null,
             int? leaderIndex = null,
             bool useSsl = false,
-            IDictionary<string, string> customSettings = null,
+            IDictionary<string, string> commonCustomSettings = null,
             List<IDictionary<string, string>> customSettingsList = null,
             bool watcherCluster = false,
             bool useReservedPorts = false,
@@ -820,12 +820,16 @@ namespace Tests.Infrastructure
 
             for (var i = 0; i < numberOfNodes; i++)
             {
+                IDictionary<string, string> customSettings;
                 if (customSettingsList == null)
                 {
-                    customSettings ??= new Dictionary<string, string>(DefaultClusterSettings)
+                    customSettings = new Dictionary<string, string>(commonCustomSettings ?? DefaultClusterSettings);
+                    
+                    var electionKey = RavenConfiguration.GetKey(x => x.Cluster.ElectionTimeout);
+                    if (customSettings.ContainsKey(electionKey) == false)
                     {
-                        [RavenConfiguration.GetKey(x => x.Cluster.ElectionTimeout)] = _electionTimeoutInMs.ToString(),
-                    };
+                        customSettings[electionKey] = _electionTimeoutInMs.ToString();
+                    }
                 }
                 else
                 {
