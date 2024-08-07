9bf8c335cb26330d3def3b00067a706adcb8faf0
Grisha Kotler <grisha@ravendb.net>
2024-08-07 19:10:20 +0300

    RavenDB-21771 - add the EventListenerToLog

---
+++ src/Raven.Server/EventListener/EventListenerToLog.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Raven.Client.ServerWide.Operations.EventListener;
+using Raven.Server.Config.Categories;
+using Raven.Server.Config.Settings;
+using Sparrow.Logging;
+
+namespace Raven.Server.EventListener;
+
+public class EventListenerToLog
+{
+    public static readonly Logger Logger = LoggingSource.Instance.GetLogger<RavenServerStartup>("EventListener");
+    private readonly SemaphoreSlim _sm = new(1, 1);
+
+    private EventsToLogListener _listener;
+    private EventListenerMode _eventListenerMode;
+
+    public static readonly HashSet<EventType> GcEvents = [EventType.GC, EventType.GCSuspend, EventType.GCRestart, EventType.GCFinalizers];
+    public static readonly HashSet<EventType> ContentionTypes = [EventType.Contention];
+    private static readonly HashSet<EventType> AllEvents = new(GcEvents.Concat(ContentionTypes));
+
+    private EventListenerToLog()
+    {
+    }
+
+    public static EventListenerToLog Instance = new();
+
+    public bool LogToFile => _eventListenerMode == EventListenerMode.ToLogFile && Logger.IsOperationsEnabled;
+
+    public void UpdateConfiguration(EventListenerConfiguration configuration)
+    {
+        _sm.Wait();
+
+        try
+        {
+            _eventListenerMode = configuration.EventListenerMode;
+            var eventTypes = configuration.EventTypes;
+            var minimumDurationInMs = configuration.MinimumDuration.GetValue(TimeUnit.Milliseconds);
+
+            var effectiveEventTypes = (eventTypes == null || eventTypes.Length == 0)
+                ? AllEvents
+                : new HashSet<EventType>(eventTypes);
+
+            if (LogToFile == false)
+            {
+                _listener?.Dispose();
+                _listener = null;
+            }
+            else
+            {
+                _listener ??= new EventsToLogListener(Logger, effectiveEventTypes, minimumDurationInMs);
+                _listener.Update(effectiveEventTypes, minimumDurationInMs);
+            }
+        }
+        finally
+        {
+            _sm.Release();
+        }
+    }
+}
