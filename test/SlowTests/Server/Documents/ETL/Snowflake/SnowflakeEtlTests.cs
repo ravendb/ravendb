fae78e347626181df874d19011f007e2531367ab
Gracjan Sadowicz <gracjan@ravendb.net>
2024-08-12 11:55:03 +0200

    RavenDB-14706 Batch of passing Snowflake ETL tests

--- test/SlowTests/Server/Documents/ETL/Snowflake/SnowflakeEtlTests.cs
+++ test/SlowTests/Server/Documents/ETL/Snowflake/SnowflakeEtlTests.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using FastTests;
 using FastTests.Voron.Util;
@@ -12,7 +12,6 @@ using Raven.Client.Documents.Operations.Attachments;
 using Raven.Client.Documents.Operations.ETL;
 using Raven.Client.Documents.Operations.ETL.Snowflake;
 using Raven.Client.Util;
-using Raven.Server.SqlMigration;
 using Snowflake.Data.Client;
 using Tests.Infrastructure;
 using Xunit;
@@ -21,17 +20,11 @@ using TestSnowflakeConnectionString = Tests.Infrastructure.ConnectionString.Snow
 
 namespace SlowTests.Server.Documents.ETL.Snowflake;
 
-public class SnowflakeEtlTests: RavenTestBase
+public class SnowflakeEtlTests(ITestOutputHelper output) : RavenTestBase(output)
 {
-    public SnowflakeEtlTests(ITestOutputHelper output) : base(output)
-    {
-    }
-    
-    private readonly List<string> _dbNames = new List<string>();
-    
-   private const int CommandTimeout = 10; // We want to avoid timeout exception. We don't care about performance here. The query can take long time if all the outer database are working simultaneously on the same machine 
-    
-    protected const string defaultScript = @"
+    private const int CommandTimeout = 10; // We want to avoid timeout exception. We don't care about performance here. The query can take long time if all the outer database are working simultaneously on the same machine 
+
+    protected const string DefaultScript = @"
 var orderData = {
     Id: id(this),
     OrderLinesCount: this.OrderLines.length,
@@ -52,7 +45,7 @@ for (var i = 0; i < this.OrderLines.length; i++) {
 loadToOrders(orderData);
 ";
 
-    protected const string defaultScriptRegions = @"
+    protected const string DefaultScriptRegionsTableWithTerritoriesArrayColumn = @"
 loadToRegions({
     Id: id(this),
     Name: this.Name,
@@ -61,7 +54,7 @@ loadToRegions({
 ";
     
     
-    private static DisposableAction WithSnowflakeDatabase(out string connectionString, out string databaseName, out string schemaName, string dataSet, bool includeData = true)
+    private static DisposableAction WithSnowflakeDatabase(out string connectionString, out string databaseName, out string schemaName)
     {
         databaseName = "snowflake_test_" + Guid.NewGuid();
         schemaName = "snowflake_test_" + Guid.NewGuid();
@@ -88,36 +81,6 @@ loadToRegions({
             }
         }
         
-        
-        // if (string.IsNullOrEmpty(dataSet) == false)
-        // {
-        //     using (var dbConnection = new SnowflakeDbConnection(connectionString))
-        //     {
-        //         dbConnection.Open();
-        //
-        //         var assembly = Assembly.GetExecutingAssembly();
-        //
-        //         using (var dbCommand = dbConnection.CreateCommand())
-        //         {
-        //             dbCommand.CommandTimeout = CommandTimeout;
-        //             var textStreamReader = new StreamReader(assembly.GetManifestResourceStream("SlowTests.Data.snowflake." + dataSet + ".create.sql"));
-        //             dbCommand.CommandText = textStreamReader.ReadToEnd();
-        //             dbCommand.ExecuteNonQuery();
-        //         }
-        //
-        //         if (includeData)
-        //         {
-        //             using (var dbCommand = dbConnection.CreateCommand())
-        //             {
-        //                 dbCommand.CommandTimeout = CommandTimeout;
-        //                 var textStreamReader = new StreamReader(assembly.GetManifestResourceStream("SlowTests.Data.mysql." + dataSet + ".insert.sql"));
-        //                 dbCommand.CommandText = textStreamReader.ReadToEnd();
-        //                 dbCommand.ExecuteNonQuery();
-        //             }
-        //         }
-        //     }
-        // }
-
         string dbName = databaseName;
         return new DisposableAction(() =>
         {
@@ -146,12 +109,13 @@ loadToRegions({
 
             using (var dbCommand = con.CreateCommand())
             {
-                dbCommand.CommandText = "SELECT COUNT(*) FROM Orders";
-                var result = dbCommand.ExecuteScalar();
-                Assert.Equal((long)ordersCount, result);
                 dbCommand.CommandText = "SELECT COUNT(*) FROM OrderLines";
-                result = dbCommand.ExecuteScalar();
+                var result = dbCommand.ExecuteScalar();
                 Assert.Equal((long)orderLineCounts, result);
+                
+                dbCommand.CommandText = "SELECT COUNT(*) FROM Orders";
+                result = dbCommand.ExecuteScalar();
+                Assert.Equal((long)ordersCount, result);
             }
         }
     }
@@ -172,29 +136,6 @@ loadToRegions({
         }
     }
     
-    
-    public override void Dispose()
-    {
-        base.Dispose();
-
-        using (var con = new SnowflakeDbConnection())
-        {
-            con.ConnectionString = TestSnowflakeConnectionString.Instance.VerifiedConnectionString.Value;
-            con.Open();
-
-            foreach (var dbName in _dbNames)
-            {
-                using (var dbCommand = con.CreateCommand())
-                {
-                    dbCommand.CommandText = $@"
-ALTER DATABASE SqlReplication-{dbName} SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-DROP DATABASE SqlReplication-{dbName}";
-                    dbCommand.ExecuteNonQuery();
-                }
-            }
-        }
-    }
-
     protected void SetupSnowflakeEtl(DocumentStore store, string connectionString, string script, bool insertOnly = false, List<string> collections = null)
     {
         var connectionStringName = $"{store.Database}@{store.Urls.First()} to Snowflake DB";
@@ -223,7 +164,7 @@ DROP DATABASE SqlReplication-{dbName}";
                 Name = $"Regions{connectionStringName}",
                 ConnectionStringName = connectionStringName,
                 SnowflakeTables = { new SnowflakeEtlTable() { TableName = "Regions", DocumentIdColumn = "Id", InsertOnlyMode = insertOnly } },
-                Transforms = { new Transformation() { Name = "Regions", Collections = new List<string>() { "Regions" }, Script = defaultScriptRegions } }
+                Transforms = { new Transformation() { Name = "Regions", Collections = new List<string>() { "Regions" }, Script = DefaultScriptRegionsTableWithTerritoriesArrayColumn } }
             }, new SnowflakeConnectionString() { Name = connectionStringName, ConnectionString = connectionString });
     }
 
@@ -276,11 +217,11 @@ DROP DATABASE SqlReplication-{dbName}";
     }
     
     [RequiresSnowflakeFact]
-    public async Task CanUseSnowflakeSqlEtl()
+    public async Task CanUseSnowflakeEtl()
     {
         using (var store = GetDocumentStore())
         {
-            using (WithSnowflakeDatabase(out var connectionString, out string databaseName, out string schemaName, dataSet: null, includeData: false))
+            using (WithSnowflakeDatabase(out var connectionString, out string databaseName, out string schemaName))
             {
                 CreateOrdersAndOrderLinesTables(connectionString);
                 
@@ -299,7 +240,7 @@ DROP DATABASE SqlReplication-{dbName}";
 
                 var etlDone = Etl.WaitForEtlToComplete(store);
                 
-                SetupSnowflakeEtl(store, connectionString, defaultScript, insertOnly: true);
+                SetupSnowflakeEtl(store, connectionString, DefaultScript, insertOnly: true);
 
                 etlDone.Wait(TimeSpan.FromSeconds(10));
 
@@ -322,11 +263,11 @@ DROP DATABASE SqlReplication-{dbName}";
     }
     
     [RequiresSnowflakeFact]
-    public async Task CanUseSnowflakeSqlEtlForComplexData()
+    public async Task CanUseSnowflakeEtlForComplexData()
     {
         using (var store = GetDocumentStore())
         {
-            using (WithSnowflakeDatabase(out var connectionString, out string databaseName, out string schemaName, dataSet: null, includeData: false))
+            using (WithSnowflakeDatabase(out var connectionString, out string databaseName, out string schemaName))
             {
                 CreateRegionsTables(connectionString);
                 using (var session = store.OpenAsyncSession())
@@ -337,7 +278,7 @@ DROP DATABASE SqlReplication-{dbName}";
 
                 var etlDone = Etl.WaitForEtlToComplete(store);
 
-                SetupComplexDataSnowflakeEtl(store, connectionString, defaultScriptRegions, insertOnly: true);
+                SetupComplexDataSnowflakeEtl(store, connectionString, DefaultScriptRegionsTableWithTerritoriesArrayColumn, insertOnly: true);
 
                 etlDone.Wait(TimeSpan.FromSeconds(10));
                 
@@ -345,13 +286,246 @@ DROP DATABASE SqlReplication-{dbName}";
             }
         }
     }
+    
+    
+    [RequiresSnowflakeRetryTheory(delayBetweenRetriesMs: 1000)]
+    [InlineData(RavenDatabaseMode.Single)]
+    [InlineData(RavenDatabaseMode.Sharded)]
+    public async Task ShouldHandleCaseMismatchBetweenTableDefinitionAndLoadTo(RavenDatabaseMode databaseMode)
+    {
+        using (var store = GetDocumentStore(Options.ForMode(databaseMode)))
+        {
+            using (WithSnowflakeDatabase(out var connectionString, out var _, out var _))
+            {
+                CreateOrdersAndOrderLinesTables(connectionString);
 
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new Order
+                    {
+                        OrderLines = new List<OrderLine>
+                        {
+                            new OrderLine {Cost = 3, Product = "Milk", Quantity = 3}, new OrderLine {Cost = 4, Product = "Bear", Quantity = 2},
+                        }
+                    });
+                    await session.SaveChangesAsync();
+                }
+
+                var etlDone = Etl.WaitForEtlToComplete(store);
+
+                string script = @"
+var orderData = {
+Id: id(this),
+OrderLinesCount: this.OrderLines.length,
+TotalCost: 0
+};
+
+loadToOrDerS(orderData); // note 'OrDerS' here vs 'Orders' defined in the configuration
+";
+
+                SetupSnowflakeEtl(store, connectionString, script);
+
+                etlDone.Wait(TimeSpan.FromMinutes(5));
+
+                using (var con = new SnowflakeDbConnection())
+                {
+                    con.ConnectionString = connectionString;
+                    con.Open();
+                        
+                    Assert.Equal(1, GetOrdersCount(connectionString));
+                }
+            }
+        }
+    }
+    
+    
+    [RequiresSnowflakeRetryTheory(delayBetweenRetriesMs: 1000)]
+    [InlineData(RavenDatabaseMode.Single)]
+    [InlineData(RavenDatabaseMode.Sharded)]
+    public async Task NullPropagation(RavenDatabaseMode databaseMode)
+    {
+        using (var store = GetDocumentStore(Options.ForMode(databaseMode)))
+        {
+            using (WithSnowflakeDatabase(out var connectionString, out var _, out var _))
+            {
+                CreateOrdersAndOrderLinesTables(connectionString);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new Order
+                    {
+                        OrderLines = new List<OrderLine>
+                        {
+                            new OrderLine {Cost = 3, Product = "Milk", Quantity = 3}, new OrderLine {Cost = 4, Product = "Bear", Quantity = 2},
+                        }
+                    });
+                    await session.SaveChangesAsync();
+                }
+
+                var etlDone = Etl.WaitForEtlToComplete(store);
+
+                SetupSnowflakeEtl(store, connectionString, @"var orderData = {
+Id: id(this),
+OrderLinesCount: this.OrderLines_Missing.length,
+TotalCost: 0
+};
+loadToOrders(orderData);");
+
+                etlDone.Wait(TimeSpan.FromMinutes(5));
+
+                using (var con = new SnowflakeDbConnection())
+                {
+                    con.ConnectionString = connectionString;
+                    con.Open();
+                    AssertCounts(1, 0, connectionString);
+                }
+            }
+        }
+    }
+    
+    [RequiresSnowflakeRetryTheory(delayBetweenRetriesMs: 1000)]
+    [InlineData(RavenDatabaseMode.Single)]
+    [InlineData(RavenDatabaseMode.Sharded)]
+    public async Task NullPropagation_WithExplicitNull(RavenDatabaseMode databaseMode)
+    {
+        using (var store = GetDocumentStore(Options.ForMode(databaseMode)))
+        {
+            using (WithSnowflakeDatabase(out var connectionString, out var _, out var _))
+            {
+                CreateOrdersAndOrderLinesTables(connectionString);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new Order
+                    {
+                        Address = null,
+                        OrderLines = new List<OrderLine>
+                        {
+                            new OrderLine {Cost = 3, Product = "Milk", Quantity = 3}, new OrderLine {Cost = 4, Product = "Beer", Quantity = 2},
+                        }
+                    });
+                    await session.SaveChangesAsync();
+                }
+
+                var etlDone = Etl.WaitForEtlToComplete(store);
+
+                SetupSnowflakeEtl(store, connectionString, @"var orderData = {
+Id: id(this),
+City: this.Address.City,
+TotalCost: 0
+};
+loadToOrders(orderData);");
+
+                etlDone.Wait(TimeSpan.FromMinutes(5));
+
+                using (var con = new SnowflakeDbConnection())
+                {
+                    con.ConnectionString = connectionString;
+                    con.Open();
+
+                    using (var dbCommand = con.CreateCommand())
+                    {
+                        dbCommand.CommandText = " SELECT COUNT(*) FROM Orders";
+                        Assert.Equal(1L, dbCommand.ExecuteScalar());
+                        dbCommand.CommandText = " SELECT City FROM Orders";
+                        Assert.Equal(DBNull.Value, dbCommand.ExecuteScalar());
+                    }
+                }
+            }
+        }
+    }
+    
+    [RequiresMsSqlRetryTheory(delayBetweenRetriesMs: 1000)]
+    [InlineData(RavenDatabaseMode.Single)]
+    [InlineData(RavenDatabaseMode.Sharded)]
+    public async Task RavenDB_3341(RavenDatabaseMode databaseMode)
+    {
+        using (var store = GetDocumentStore(Options.ForMode(databaseMode)))
+        {
+            using (WithSnowflakeDatabase(out var connectionString, out var _, out var _))
+            {
+                CreateOrdersAndOrderLinesTables(connectionString);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new Order { OrderLines = new List<OrderLine> { new OrderLine { Cost = 4, Product = "Bear", Quantity = 2 }, } });
+                    await session.SaveChangesAsync();
+                }
+
+                var etlDone = Etl.WaitForEtlToComplete(store);
+
+                SetupSnowflakeEtl(store, connectionString, "if(this.OrderLines.length > 0) { \r\n" + DefaultScript + " \r\n}");
+
+                etlDone.Wait(TimeSpan.FromMinutes(5));
+
+                AssertCounts(1, 1, connectionString);
+
+                etlDone.Reset();
+                using (var session = store.OpenAsyncSession())
+                {
+                    var order = await session.LoadAsync<Order>("orders/1-A");
+                    order.OrderLines.Clear();
+                    await session.SaveChangesAsync();
+                }
+
+                etlDone.Wait(TimeSpan.FromMinutes(5));
+                AssertCounts(0, 0, connectionString);
+            }
+        }
+    }
+    
+    [RequiresMsSqlRetryTheory(delayBetweenRetriesMs: 1000)]
+    [InlineData(RavenDatabaseMode.Single)]
+    [InlineData(RavenDatabaseMode.Sharded)]
+    public async Task CanUpdateToBeNoItemsInChildTable(RavenDatabaseMode databaseMode)
+    {
+        using (var store = GetDocumentStore(Options.ForMode(databaseMode)))
+        {
+            using (WithSnowflakeDatabase(out var connectionString, out var _, out var _))
+            {
+                CreateOrdersAndOrderLinesTables(connectionString);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(new Order
+                    {
+                        OrderLines = new List<OrderLine>
+                        {
+                            new OrderLine {Cost = 3, Product = "Milk", Quantity = 3}, new OrderLine {Cost = 4, Product = "Bear", Quantity = 2},
+                        }
+                    });
+                    await session.SaveChangesAsync();
+                }
+
+                var etlDone = Etl.WaitForEtlToComplete(store);
+
+                SetupSnowflakeEtl(store, connectionString, DefaultScript);
+
+                etlDone.Wait(TimeSpan.FromMinutes(5));
+
+                AssertCounts(1, 2, connectionString);
+
+                etlDone.Reset();
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    var order = await session.LoadAsync<Order>("orders/1-A");
+                    order.OrderLines.Clear();
+                    await session.SaveChangesAsync();
+                }
+
+                etlDone.Wait(TimeSpan.FromMinutes(5));
+                AssertCounts(1, 0, connectionString);
+            }
+        }
+    }
+    
     [RequiresSnowflakeFact]
     public async Task CanLoadSingleAttachment()
     {
         using (var store = GetDocumentStore())
         {
-            using (WithSnowflakeDatabase(out var connectionString, out string databaseName, out string schemaName, dataSet: null, includeData: false))
+            using (WithSnowflakeDatabase(out var connectionString, out string databaseName, out string schemaName))
             {
                 CreateOrdersWithAttachmentTable(connectionString);
                 var attachmentBytes = new byte[] { 1, 2, 3 };
@@ -380,11 +554,7 @@ loadToOrders(orderData);
                     con.ConnectionString = connectionString;
                     con.Open();
 
-                    using (var dbCommand = con.CreateCommand())
-                    {
-                        dbCommand.CommandText = " SELECT COUNT(*) FROM Orders";
-                        Assert.Equal(1L, dbCommand.ExecuteScalar());
-                    }
+                    Assert.Equal(1L, GetOrdersCount(connectionString));
 
                     using (var dbCommand = con.CreateCommand())
                     {
@@ -404,7 +574,21 @@ loadToOrders(orderData);
         }
     }
 
+    internal static long GetOrdersCount(string connectionString)
+    {
+        using (var con = new SnowflakeDbConnection())
+        {
+            con.ConnectionString = connectionString;
+            con.Open();
 
+            using (var dbCommand = con.CreateCommand())
+            {
+                dbCommand.CommandText = " SELECT COUNT(*) FROM Orders";
+                return (long)dbCommand.ExecuteScalar();
+            }
+        }
+    }
+    
     private class Order
     {
         public Address Address { get; set; }
