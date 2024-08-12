bc9f4583aa3db9702dd4dcb5f7492f1db19238a9
Gracjan Sadowicz <gracjan@ravendb.net>
2024-08-12 15:09:54 +0200

    RavenDB-14706 Bugfix

--- src/Raven.Server/Documents/ETL/Providers/RelationalDatabase/SQL/RelationalWriters/SqlDatabaseWriter.cs
+++ src/Raven.Server/Documents/ETL/Providers/RelationalDatabase/SQL/RelationalWriters/SqlDatabaseWriter.cs
@@ -33,7 +33,7 @@ internal sealed class SqlDatabaseWriter: RelationalDatabaseWriterBase<SqlConnect
     private readonly SqlEtlConfiguration _sqlEtlConfiguration;
 
     public SqlDatabaseWriter(DocumentDatabase database, SqlEtlConfiguration configuration,
-        RelationalDatabaseEtlMetricsCountersManager metrics, EtlProcessStatistics statistics, bool shouldConnectToTarget = false) : base(database,
+        RelationalDatabaseEtlMetricsCountersManager metrics, EtlProcessStatistics statistics, bool shouldConnectToTarget = true) : base(database,
         configuration, metrics, statistics, shouldConnectToTarget)
     {
         _sqlEtlConfiguration = configuration;
