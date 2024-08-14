cf214bdc977db7aedee1a307212cd82c17bed6fc
Paweł Pekról <ppekrol@ravendb.net>
2024-08-14 11:37:59 +0200

    RavenDB-700 fixing compilation after .NET SDK 8.0.400 bump

--- src/Raven.Studio/scripts/typings.js
+++ src/Raven.Studio/scripts/typings.js
@@ -4,8 +4,8 @@ const fsUtils = require("./fsUtils");
 const exec = require('child_process').execSync;
 
 const possibleTypingsGenPaths = [
-    '../../tools/TypingsGenerator/bin/Debug/net6.0/TypingsGenerator.dll',
-    '../../tools/TypingsGenerator/bin/Release/net6.0/TypingsGenerator.dll' ];
+    '../../tools/TypingsGenerator/bin/Debug/net8.0/TypingsGenerator.dll',
+    '../../tools/TypingsGenerator/bin/Release/net8.0/TypingsGenerator.dll' ];
 
 const dllPath = fsUtils.getLastRecentlyModifiedFile(possibleTypingsGenPaths);
 if (!dllPath) {
