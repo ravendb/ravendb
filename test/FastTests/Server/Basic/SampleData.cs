b38fd03e055200f68ef65d5c80efa280c091d824
Grisha Kotler <grisha@ravendb.net>
2024-08-19 16:01:43 +0300

    RavenDB-21273 - fix time series license restrictions

---
+++ test/FastTests/Server/Basic/SampleData.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Tests.Infrastructure;
+using Xunit.Abstractions;
+
+namespace FastTests.Server.Basic
+{
+    public class SampleData : RavenTestBase
+    {
+        public SampleData(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [RavenFact(RavenTestCategory.Smuggler)]
+        public async Task CanCreateSampleData()
+        {
+            using (var store = GetDocumentStore())
+            {
+                await store.Maintenance.SendAsync(new CreateSampleDataOperation());
+            }
+        }
+    }
+}
