7d7d73d245fe7ce6ca0f291efeb6fff55ca4ff29
Maciej Aszyk <maciej@ravendb.net>
2024-08-22 12:06:26 +0200

    RavenDB-22824 Support for nullable enums in configuration.

---
+++ test/SlowTests/Issues/RavenDB_22824.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using FastTests;
+using OpenTelemetry.Exporter;
+using Raven.Server.Config;
+using Raven.Server.ServerWide;
+using Tests.Infrastructure;
+using Xunit.Abstractions;
+
+
+namespace SlowTests.Issues;
+
+public class RavenDB_22824 : RavenTestBase
+{
+    public RavenDB_22824(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [RavenFact(RavenTestCategory.Configuration)]
+    public async Task RavenConfigurationSupportsNullableEnums()
+    {
+        var settingJsonPath = NewDataPath();
+        var content = $"{{ \"{RavenConfiguration.GetKey(x => x.Monitoring.OpenTelemetry.OtlpProtocol)}\": \"{OtlpExportProtocol.HttpProtobuf}\" }}";
+        await File.WriteAllTextAsync(settingJsonPath, content);
+        
+        var configuration = RavenConfiguration.CreateForTesting(null, ResourceType.Server, settingJsonPath);
+        configuration.Initialize();
+    }
+}
