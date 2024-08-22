c8b6df3af776f0058542315230409353b7514df3
Lev Skuditsky <lev.skuditsky@ravendb.net>
2024-08-22 18:34:48 +0300

    RavenDB-22246: Specific breaking changes for v6.1

--- test/Tests.Infrastructure/TestBase.cs
+++ test/Tests.Infrastructure/TestBase.cs
@@ -551,6 +551,7 @@ namespace FastTests
                 var hasDataDirectory = options.CustomSettings != null && options.CustomSettings.ContainsKey(RavenConfiguration.GetKey(x => x.Core.DataDirectory));
                 var hasFeaturesAvailability = options.CustomSettings != null && options.CustomSettings.ContainsKey(RavenConfiguration.GetKey(x => x.Core.FeaturesAvailability));
                 var hasRunInMemory = options.CustomSettings != null && options.CustomSettings.ContainsKey(RavenConfiguration.GetKey(x => x.Core.RunInMemory));
+                var hasThrowOnInvalidOrMissingLicense = options.CustomSettings != null && options.CustomSettings.ContainsKey(RavenConfiguration.GetKey(x => x.Licensing.ThrowOnInvalidOrMissingLicense));
 
                 configuration.Initialize();
 
@@ -590,6 +591,9 @@ namespace FastTests
 
                 if (options.DeletePrevious)
                     IOExtensions.DeleteDirectory(configuration.Core.DataDirectory.FullPath);
+
+                if (hasThrowOnInvalidOrMissingLicense == false)
+                    configuration.Licensing.ThrowOnInvalidOrMissingLicense = true;
 
                 var server = new RavenServer(configuration, options.Conventions)
                 {
