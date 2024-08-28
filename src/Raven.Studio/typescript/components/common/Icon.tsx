c441f0f21f9be044a4296921abd5b5ccbb201c65
Damian Olszewski <damian@ravendb.net>
2024-08-28 09:40:13 +0200

    RavenDB-18066 Improve EmptySet and Icon components

--- src/Raven.Studio/typescript/components/common/Icon.tsx
+++ src/Raven.Studio/typescript/components/common/Icon.tsx
@@ -3,19 +3,29 @@ import classNames from "classnames";
 import IconName from "typings/server/icons";
 import { TextColor } from "components/models/common";
 
+export type IconSize = "xs" | "sm" | "md" | "lg" | "xl";
+
 export interface IconProps extends HTMLAttributes<HTMLDivElement> {
     icon: IconName;
     addon?: IconName;
     color?: TextColor;
     margin?: string;
     className?: string;
+    size?: IconSize;
 }
 
 export function Icon(props: IconProps) {
-    const { icon, addon, color, margin, className, ...rest } = props;
+    const { icon, addon, color, margin, className, size, ...rest } = props;
     const iconClasses = "icon-" + icon;
     const addonClasses = addon ? "icon-addon-" + addon : null;
     const colorClasses = color ? "text-" + color : null;
     const marginClass = margin ?? "me-1";
-    return <i className={classNames(iconClasses, addonClasses, colorClasses, marginClass, className)} {...rest} />;
+    const sizeClass = size ? "icon-" + size : "icon-md";
+
+    return (
+        <i
+            className={classNames(iconClasses, addonClasses, colorClasses, marginClass, className, sizeClass)}
+            {...rest}
+        />
+    );
 }
