3728d32ae1ff08d87f4fdc6aea9f36c8154d0d2d
efrat@ravendb.net <efrat@ravendb.net>
2024-09-04 16:36:58 +0300

    RavenDB-22659 Cancelling the restore doesn't always delete the database

--- src/Raven.Server/Documents/Sharding/ShardedDatabaseContext.Operations.cs
+++ src/Raven.Server/Documents/Sharding/ShardedDatabaseContext.Operations.cs
@@ -56,7 +56,8 @@ public partial class ShardedDatabaseContext
             string description,
             IOperationDetailedDescription detailedDescription,
             Func<Action<IOperationProgress>, Task<IOperationResult>> taskFactory,
-            OperationCancelToken token = null)
+            OperationCancelToken token = null,
+            string databaseName = null)
         {
             var operation = CreateOperationInstance(id, _context.DatabaseName, operationType, description, detailedDescription, token);
 
