bcbdc6f2908fbbbe27a50f9598ddbeb2c81c8ed8
shiranshalom <shiran@ravendb.net>
2024-09-05 13:58:39 +0300

    RavenDB-22680 - fixing PR comments#3

--- src/Raven.Server/Documents/Indexes/Workers/Cleanup/CleanupDocuments.cs
+++ src/Raven.Server/Documents/Indexes/Workers/Cleanup/CleanupDocuments.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Raven.Client.Documents.Indexes;
 using Raven.Server.Config.Categories;
 using Raven.Server.Documents.Indexes.MapReduce;
@@ -32,11 +33,29 @@ namespace Raven.Server.Documents.Indexes.Workers.Cleanup
         internal override void UpdateStats(IndexProgress.CollectionStats inMemoryStats, long lastEtag) =>
             inMemoryStats.UpdateLastEtag(lastEtag, isTombstone: true);
 
-        protected override IEnumerable<TombstoneIndexItem> GetTombstonesFrom(DocumentsOperationContext context, long etag, long start, long take) =>
-            _documentsStorage.GetTombstoneIndexItemsFrom(context, etag, start, take);
+        protected override IEnumerable<TombstoneIndexItem> GetTombstonesFrom(DocumentsOperationContext context, long etag, long start, long take)
+        {
+            return _documentsStorage.GetTombstonesFrom(context, etag, start, take)
+                .Select(tombstone =>
+                    new TombstoneIndexItem { Etag = tombstone.Etag, Type = GetTombstoneType(tombstone.Type), LowerId = tombstone.LowerId });
+        }
 
-        protected override IEnumerable<TombstoneIndexItem> GetTombstonesFrom(DocumentsOperationContext context, string collection, long etag, long start, long take) =>
-            _documentsStorage.GetTombstoneIndexItemsFrom(context, collection, etag, start, take);
+        protected override IEnumerable<TombstoneIndexItem> GetTombstonesFrom(DocumentsOperationContext context, string collection, long etag, long start, long take)
+        {
+            return _documentsStorage.GetTombstonesFrom(context, collection, etag, start, take)
+                .Select(tombstone =>
+                    new TombstoneIndexItem { Etag = tombstone.Etag, Type = GetTombstoneType(tombstone.Type), LowerId = tombstone.LowerId });
+        }
+
+        private IndexItemType GetTombstoneType(Tombstone.TombstoneType tombstoneType)
+        {
+            return tombstoneType switch
+            {
+                Tombstone.TombstoneType.Document => IndexItemType.Document,
+                Tombstone.TombstoneType.Counter => IndexItemType.Counters,
+                _ => IndexItemType.None
+            };
+        }
 
         protected override bool IsValidTombstoneType(TombstoneIndexItem tombstone)
         {
