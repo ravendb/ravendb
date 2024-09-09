6593e466765c6e8c3f25afa965dae719f9c3cb46
Karmel Indych <karmeli87@gmail.com>
2024-09-09 15:36:36 +0300

    RavenDB-22872 Ensure we comparing tombstone ID with a document tombstone

--- src/Raven.Server/Documents/DocumentsStorage.cs
+++ src/Raven.Server/Documents/DocumentsStorage.cs
@@ -2187,6 +2187,9 @@ namespace Raven.Server.Documents
 
             if (tombstoneKey[tombstoneKey.Size - ConflictedTombstoneOverhead] == SpecialChars.RecordSeparator)
             {
+                if (tombstoneKey.Size - ConflictedTombstoneOverhead != lowerId.Size)
+                    return false;
+
                 return Memory.CompareInline(tombstoneKey.Content.Ptr, lowerId.Content.Ptr, lowerId.Size) == 0;
             }
 
