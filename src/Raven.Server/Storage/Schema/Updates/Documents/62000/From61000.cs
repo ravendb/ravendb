462985b7f683eaf06885185fe44cc2b9299bf613
Paweł Pekról <ppekrol@ravendb.net>
2024-09-13 10:01:24 +0200

    RavenDB-22898 bumped schema to 62_000

---
+++ src/Raven.Server/Storage/Schema/Updates/Documents/62000/From61000.cs
@@ -0,0 +1,14 @@
+﻿namespace Raven.Server.Storage.Schema.Updates.Documents
+{
+    public class From61000 : ISchemaUpdate
+    {
+        public int From => 61_000;
+        public int To => 62_000;
+        public SchemaUpgrader.StorageType StorageType => SchemaUpgrader.StorageType.Documents;
+
+        public bool Update(UpdateStep step)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/Raven.Server/Storage/Schema/Updates/LuceneIndex/62000/From61000.cs
@@ -0,0 +1,14 @@
+﻿namespace Raven.Server.Storage.Schema.Updates.LuceneIndex
+{
+    public class From61000 : ISchemaUpdate
+    {
+        public int From => 61_000;
+        public int To => 62_000;
+        public SchemaUpgrader.StorageType StorageType => SchemaUpgrader.StorageType.LuceneIndex;
+
+        public bool Update(UpdateStep step)
+        {
+            return true;
+        }
+    }
+}
