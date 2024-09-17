b89e0941d442c6424f56fd892112da41bc789ed4
Damian Olszewski <damian@ravendb.net>
2024-09-17 13:12:21 +0200

    RavenDB-22680 Add deleted time series progress bar

--- src/Raven.Studio/typescript/components/models/indexes.ts
+++ src/Raven.Studio/typescript/components/models/indexes.ts
@@ -43,6 +43,7 @@ export interface IndexCollectionProgress {
     name: string;
     documents: IndexingProgress;
     tombstones: IndexingProgress;
+    deletedTimeSeries: IndexingProgress;
 }
 
 export interface IndexingProgress extends Progress {
