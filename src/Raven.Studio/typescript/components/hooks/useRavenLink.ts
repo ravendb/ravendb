8ef9435307878729cf2975a3f382e7cebea554d5
Damian Olszewski <damian@ravendb.net>
2024-09-24 09:53:43 +0200

    RavenDB-22374 Add useGetRavenLink

--- src/Raven.Studio/typescript/components/hooks/useRavenLink.ts
+++ src/Raven.Studio/typescript/components/hooks/useRavenLink.ts
@@ -18,24 +18,27 @@ type UseRavenLinkProps = {
 );
 
 export function useRavenLink(props: UseRavenLinkProps): string {
-    const { hash, lang } = props;
+    return useGetRavenLink()(props);
+}
 
+export function useGetRavenLink(): (props: UseRavenLinkProps) => string {
     const clientVersion = useAppSelector(clusterSelectors.clientVersion);
 
-    const isDocs = props.isDocs ?? true;
-    const version = props.version ?? clientVersion;
+    return (props: UseRavenLinkProps): string => {
+        const { hash, lang, isDocs = true, version = clientVersion } = props;
 
-    let link = `https://ravendb.net/l/${hash}`;
+        let link = `https://ravendb.net/l/${hash}`;
 
-    if (isDocs) {
-        link += `/${version}`;
+        if (isDocs) {
+            link += `/${version}`;
 
-        if (lang) {
-            link += `/${lang}`;
+            if (lang) {
+                link += `/${lang}`;
+            }
         }
-    }
 
-    todo("Feature", "ANY", "Add utm source");
+        todo("Feature", "ANY", "Add utm source");
 
-    return link;
+        return link;
+    };
 }
