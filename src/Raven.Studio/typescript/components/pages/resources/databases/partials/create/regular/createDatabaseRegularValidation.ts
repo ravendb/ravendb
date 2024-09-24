2a9a6cae89730688a06ad9372fcd9f5c6b57ff83
Damian Olszewski <damian@ravendb.net>
2024-09-24 12:23:20 +0200

    RavenDB-22943 Create database: validate name case insensitive

--- src/Raven.Studio/typescript/components/pages/resources/databases/partials/create/regular/createDatabaseRegularValidation.ts
+++ src/Raven.Studio/typescript/components/pages/resources/databases/partials/create/regular/createDatabaseRegularValidation.ts
@@ -1,15 +1,12 @@
-import { encryptionStepSchema, dataDirectoryStepSchema } from "../shared/createDatabaseSharedValidation";
+import {
+    encryptionStepSchema,
+    dataDirectoryStepSchema,
+    databaseNameSchema,
+} from "../shared/createDatabaseSharedValidation";
 import * as yup from "yup";
 
 const basicInfoStepSchema = yup.object({
-    databaseName: yup
-        .string()
-        .trim()
-        .strict()
-        .required()
-        .when("$usedDatabaseNames", ([usedDatabaseNames], schema) =>
-            schema.notOneOf(usedDatabaseNames, "Database already exists")
-        ),
+    databaseName: databaseNameSchema,
     isEncrypted: yup.boolean(),
 });
 
