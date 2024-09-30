57bddd2a22fea7d1cc5a5ef66ffaf0b7cf39ee51
Karmel Indych <karmeli87@gmail.com>
2024-09-30 10:13:10 +0300

    RavenDB-22637 - XML comment for CmpXchg.cs

--- src/Raven.Client/Documents/Session/CmpXchg.cs
+++ src/Raven.Client/Documents/Session/CmpXchg.cs
@@ -2,6 +2,10 @@ namespace Raven.Client.Documents.Session
 {
     public sealed class CmpXchg : MethodCall
     {
+        /// <summary>
+        /// Enables the construction of RQL query that specifically retrieve compare exchange values for a given key.
+        /// </summary>
+        /// <param name="key">The key of the compare exchange.</param>
         public static CmpXchg Value(string key)
         {
             return new CmpXchg
