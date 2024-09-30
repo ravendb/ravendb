6c67c15676f900d74f7086582758391d37bb4d69
Paweł Pekról <ppekrol@ravendb.net>
2024-09-30 14:21:02 +0200

    RavenDB-22952 switching default smaps reader to SmapsRollupReader

--- test/Tests.Infrastructure/TestMetrics/TestResourcesAnalyzerMetricCacher.cs
+++ test/Tests.Infrastructure/TestMetrics/TestResourcesAnalyzerMetricCacher.cs
@@ -9,13 +9,13 @@ namespace Tests.Infrastructure.TestMetrics
 {
     public class TestResourcesAnalyzerMetricCacher : MetricCacher
     {
-        private readonly SmapsReader _smapsReader;
+        private readonly AbstractSmapsReader _smapsReader;
         private readonly TimeSpan _cacheRefreshRate = TimeSpan.FromMilliseconds(25);
 
         public TestResourcesAnalyzerMetricCacher(ICpuUsageCalculator cpuUsageCalculator)
         {
             if (PlatformDetails.RunningOnLinux)
-                _smapsReader = new SmapsReader(new[] { new byte[SmapsReader.BufferSize], new byte[SmapsReader.BufferSize] });
+                _smapsReader = AbstractSmapsReader.CreateSmapsReader([new byte[AbstractSmapsReader.BufferSize], new byte[AbstractSmapsReader.BufferSize]]);
 
             Register(Keys.Server.CpuUsage, _cacheRefreshRate, cpuUsageCalculator.Calculate);
             Register(Keys.Server.MemoryInfo, _cacheRefreshRate, CalculateMemoryInfo);
