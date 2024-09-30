32448b05a481f3b96e2433365a815e29da1b8ce1
Paweł Pekról <ppekrol@ravendb.net>
2024-09-30 15:31:38 +0200

    RavenDB-22952 use separate impl

---
+++ src/Sparrow.Server/Platform/Posix/SmapsReadResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Sparrow.Platform.Posix;
+
+internal struct SmapsReadResult<T> where T : struct, ISmapsReaderResultAction
+{
+    public long Rss;
+    public long SharedClean;
+    public long PrivateClean;
+    public long TotalDirty;
+    public long Swap;
+    public T SmapsResults;
+}
---
+++ src/Sparrow.Server/Platform/Posix/SmapsReaderResults.cs
@@ -0,0 +1,13 @@
+﻿namespace Sparrow.Platform.Posix;
+
+internal class SmapsReaderResults
+{
+    public string ResultString;
+    public long Size;
+    public long Rss;
+    public long SharedClean;
+    public long SharedDirty;
+    public long PrivateClean;
+    public long PrivateDirty;
+    public long Swap;
+}
