680f44e4e459156e2fc07247f27b7393c49c759b
Grzegorz Lachowski <gregolsky@ravendb.net>
2024-10-02 11:08:23 +0200

    RavenDB-7070 fix FullVersion update in build scripts

--- docker/ravendb-ubuntu/run-raven.sh
+++ docker/ravendb-ubuntu/run-raven.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 5.x -> 6.2 migration assistance
+# 5.x -> 6.0 migration assistance
 /usr/lib/ravendb/scripts/link-legacy-datadir.sh 
 
 COMMAND="/usr/lib/ravendb/server/Raven.Server -c /etc/ravendb/settings.json"
