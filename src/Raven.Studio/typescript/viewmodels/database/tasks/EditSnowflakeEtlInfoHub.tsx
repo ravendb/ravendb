92759f8bccfc7b2c7f2786d2ec9b9dbe84b890bd
danielle9897 <danielle.9897@gmail.com>
2024-10-07 21:44:11 +0300

    RDoc-3059 Snowflake ETL - create text for "About this view"

--- src/Raven.Studio/typescript/viewmodels/database/tasks/EditSnowflakeEtlInfoHub.tsx
+++ src/Raven.Studio/typescript/viewmodels/database/tasks/EditSnowflakeEtlInfoHub.tsx
@@ -20,8 +20,6 @@ export function EditSnowflakeEtlInfoHub() {
     });
 
     const snowflakeEtlDocsLink = useRavenLink({ hash: "TODO" }); //TODO: HASH
-
-    //TODO: create text
     
     return (
         <AboutViewFloating defaultOpen={hasSnowflakeEtl ? null : "licensing"}>
@@ -32,7 +30,49 @@ export function EditSnowflakeEtlInfoHub() {
                 heading="About this view"
                 description="Get additional info on this feature"
             >
-                TODO
+                <p>
+                    A <strong>Snowflake ETL</strong> ongoing-task is an ETL (Extract, Transform & Load)
+                    process that transfers data from this RavenDB database to a Snowflake data warehouse.
+                </p>
+                <ul>
+                    <li>
+                        Data is extracted from documents & attachments only.
+                        <br/>
+                        Counters, Time series, and Revisions are not sent.
+                    </li>
+                    <li className="margin-top-xxs">
+                        The sent data can be filtered and modified by multiple transformation JavaScript scripts that
+                        are added to the task.
+                    </li>
+                    <li className="margin-top-xxs">
+                        Each script specifies the RavenDB source collection(s) and the destination Snowflake table(s).
+                    </li>
+                    <li className="margin-top-xxs">
+                        The scripts are executed whenever documents in the source database are created, modified, or
+                        deleted.
+                    </li>
+                    <li className="margin-top-xxs">
+                        You can test each script in this view to preview the SQL statements the ETL task would send
+                        the Snowflake database.
+                    </li>
+                </ul>
+                <hr/>
+                <div>
+                    Task definition includes:
+                    <ul>
+                        <li>The transformation scripts definitions.</li>
+                        <li>List of Snowflake tables the ETL task will access,
+                            <br/>    
+                            including the column name used for the RavenDB document ID.
+                        </li>
+                        <li>A connection string specifying the Snowflake storage destination,
+                            <br/>
+                            where "<i>Account</i>", "<i>Database</i>", and "<i>Schema</i>" are mandatory parameters.
+                        </li>
+                        <li>A responsible node to handle this task can be set.</li>
+                    </ul>
+                </div>
+                <hr/>
                 <div className="small-label mb-2">useful links</div>
                 <a href={snowflakeEtlDocsLink} target="_blank">
                     <Icon icon="newtab" /> Docs - Snowflake ETL
