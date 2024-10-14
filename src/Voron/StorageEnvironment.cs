cd0c205fcf4f90066280226a0670ac7172d7887f
Grisha Kotler <grisha@ravendb.net>
2024-10-14 11:37:57 +0300

    RavenDB-22986 - fix the usages and add a test

--- src/Voron/StorageEnvironment.cs
+++ src/Voron/StorageEnvironment.cs
@@ -1032,7 +1032,7 @@ namespace Voron
         public StorageReport GenerateReport(Transaction tx)
         {
             var numberOfAllocatedPages = GetNumberOfAllocatedPages();
-            var numberOfFreePages = _freeSpaceHandling.AllPages(tx.LowLevelTransaction).Count;
+            var numberOfFreePages = _freeSpaceHandling.GetFreePagesCount(tx.LowLevelTransaction);
 
             var countOfTrees = 0;
             var countOfTables = 0;
@@ -1106,7 +1106,7 @@ namespace Voron
         public unsafe DetailedReportInput CreateDetailedReportInput(Transaction tx, bool includeDetails)
         {
             var numberOfAllocatedPages = Math.Max(_dataPager.NumberOfAllocatedPages, NextPageNumber - 1); // async apply to data file task
-            var numberOfFreePages = _freeSpaceHandling.AllPages(tx.LowLevelTransaction).Count;
+            var numberOfFreePages = _freeSpaceHandling.GetFreePagesCount(tx.LowLevelTransaction);
 
             var totalCryptoBufferSize = GetTotalCryptoBufferSize();
 
