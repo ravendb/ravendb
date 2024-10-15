438ba517b065c69d028e27c88d8b933be76dfaa4
Grisha Kotler <grisha@ravendb.net>
2024-10-16 02:12:52 +0300

    RavenDB-23017 - dispose the command if it's being skipped

--- src/Raven.Server/ServerWide/Commands/ClusterTransactionCommand.cs
+++ src/Raven.Server/ServerWide/Commands/ClusterTransactionCommand.cs
@@ -571,7 +571,7 @@ namespace Raven.Server.ServerWide.Commands
             }
         }
 
-        public class SingleClusterDatabaseCommand : IDynamicJson
+        public class SingleClusterDatabaseCommand : IDynamicJson, IDisposable
         {
             public ClusterTransactionOptions Options;
             public List<ClusterTransactionDataCommand> Commands;
@@ -591,6 +591,14 @@ namespace Raven.Server.ServerWide.Commands
                     [nameof(Options)] = Options.ToJson(),
                     [nameof(Index)] = new DynamicJsonArray(Commands)
                 };
+            }
+
+            public void Dispose()
+            {
+                foreach (var command in Commands)
+                {
+                    command.Document?.Dispose();
+                }
             }
         }
 
@@ -633,14 +641,29 @@ namespace Raven.Server.ServerWide.Commands
                     var result = ReadCommand(context, reader);
                     if (result == null)
                         yield break;
-                    if (result.Database != lowerDb) // beware of reading commands of other databases.
+
+                    if (result.Database != lowerDb)
+                    {
+                        // beware of reading commands of other databases.
+                        result.Dispose();
                         continue;
+                    } 
+
                     if (result.PreviousCount < fromCount)
+                    {
+                        result.Dispose();
                         continue;
+                    }
+
                     if (lastCompletedClusterTransactionIndex.HasValue && result.Index <= lastCompletedClusterTransactionIndex)
+                    {
+                        result.Dispose();
                         continue;
+                    }
+
                     if (take <= 0)
                         yield break;
+
                     take--;
                     yield return result;
                 }
