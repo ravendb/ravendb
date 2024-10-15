079f1efd14f95f43afddd2baf1630c57097fc114
Paweł Pekról <ppekrol@ravendb.net>
2024-10-15 12:08:56 +0200

    RavenDB-22436 adjustments

--- src/Raven.Client/Http/IRavenHttpClientFactory.cs
+++ src/Raven.Client/Http/IRavenHttpClientFactory.cs
@@ -5,6 +5,8 @@ namespace Raven.Client.Http;
 
 internal interface IRavenHttpClientFactory
 {
+    bool CanCacheHttpClient { get; }
+
     HttpClient GetHttpClient(HttpClientCacheKey key, Func<HttpClientHandler, HttpClient> createHttpClient);
 
     bool TryRemoveHttpClient(HttpClientCacheKey key, bool force = false);
