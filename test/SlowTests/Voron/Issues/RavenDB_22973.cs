cb08c3de70830c4e1d8a97c919f2c7e7b63aff03
Arkadiusz Palinski <arek@ravendb.net>
2024-10-18 12:44:34 +0200

    RavenDB-22973 If we fail to update journal state as part of applying logs to data file, then we shouldn't try to make more flushes. We have cleared the scrach pages in ApplyLogsToDataFileState while we could not free all of them yet (hence the additional validation added here - AssertNoPagesAllocatedInTransactionOlderThan() - could detect false positive issues).

--- test/SlowTests/Voron/Issues/RavenDB_22973.cs
+++ test/SlowTests/Voron/Issues/RavenDB_22973.cs
@@ -128,4 +128,69 @@ public class RavenDB_22973 : StorageTest
             Assert.Equal(readTx.LowLevelTransaction.CurrentStateRecord.ScratchPagesTable.Count, scratchBufferFile.AllocatedPagesCount);
         }
     }
+
+    [Fact]
+    public void FailureOnUpdatingJournalStateMustIgnoreFurtherFlushes()
+    {
+        long p1;
+
+        using (var txw = Env.WriteTransaction())
+        {
+            var llt = txw.LowLevelTransaction;
+
+            p1 = txw.LowLevelTransaction.AllocatePage(1).PageNumber;
+
+            llt.ModifyPage(p1);
+
+            txw.Commit();
+        }
+
+        using (var txw = Env.WriteTransaction())
+        {
+            var llt = txw.LowLevelTransaction;
+            llt.FreePage(p1);
+
+            var p11 = txw.LowLevelTransaction.AllocatePage(1).PageNumber;
+
+            Assert.Equal(p1, p11); // the same page number was returned by free space handling
+
+            llt.ModifyPage(p11);
+
+            txw.Commit();
+        }
+
+        var throwOnUpdatingJournalState = true;
+
+        Env.Journal.Applicator.ForTestingPurposesOnly().OnUpdateJournalStateUnderWriteTransactionLock += () =>
+        {
+            if (throwOnUpdatingJournalState == false)
+                return;
+
+            throwOnUpdatingJournalState = false; // do it just once
+
+            throw new InvalidOperationException("For testing purposes");
+        };
+
+        var ex = Assert.Throws<InvalidOperationException>(() => Env.FlushLogToDataFile());
+
+        Assert.Equal("For testing purposes", ex.Message);
+
+        var flushWasCalled = false;
+
+        Env.Journal.Applicator.ForTestingPurposesOnly().OnApplyLogsToDataFileUnderFlushingLock += () =>
+        {
+            flushWasCalled = true;
+        };
+
+        Env.FlushLogToDataFile();
+
+        // originally I thought that the below assertion must be valid after second FlushLogToDataFile() call
+        // although the first exception thrown there makes that the whole environment isn't in valid state so we'll prevent from
+        // making more attempts to flush logs what is verified next in the test
+        //
+        //var scratchBufferFile = Env.ScratchBufferPool.GetScratchBufferFile(0);
+        //Assert.Equal(0, scratchBufferFile.AllocatedPagesCount);
+
+        Assert.False(flushWasCalled);
+    }
 }
