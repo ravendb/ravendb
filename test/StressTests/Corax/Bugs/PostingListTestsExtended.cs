91bac2b960c19729232970a78adb7cea1eadd299
Maciej Aszyk <maciej@ravendb.net>
2024-10-25 10:17:49 +0200

    RavenDB-23023 Run the most memory-intensive PostingList tests only on x64 architecture | Disabled Corax tests for v5.4

--- test/StressTests/Corax/Bugs/PostingListTestsExtended.cs
+++ test/StressTests/Corax/Bugs/PostingListTestsExtended.cs
@@ -21,23 +21,24 @@ public class PostingListTestsExtended : NoDisposalNoOutputNeeded
             new object[] {Random.Shared.Next(), Random.Shared.Next(20000)},
         };
 
-    [RavenTheory(RavenTestCategory.Voron | RavenTestCategory.Corax)]
+    [RavenTheory(RavenTestCategory.Corax | RavenTestCategory.Voron)]
     [InlineData(1337, 200000)]
     [InlineData(1064156071, 796)]
     [InlineData(511767612, 4172)]
     [InlineData(439188321, 502627)]
     [InlineData(506431817, 2)]
-    [InlineData(1477187726, 1828658)]
-    [MemberData("Configuration")]
-    public void CanDeleteAndInsertInRandomOrder(int seed, int size)
-    {
-        using var testClass = new FastTests.Voron.PostingLists.PostingListTests(Output);
-        testClass.CanDeleteAndInsertInRandomOrder(seed, size, 10);
-    }
+    public void CanDeleteAndInsertInRandomOrder(int seed, int size) => CanDeleteAndInsertInRandomOrderBase(seed, size);
 
-    [MultiplatformTheory(RavenPlatform.Windows)]
-    [InlineData(391060845, 31707323)]
-    public void CanDeleteAndInsertInRandomOrderWindows(int seed, int size)
+    [MultiplatformTheory(RavenArchitecture.X64)]
+    [InlineData(1477187726, 1828658, Skip = RavenTheoryAttribute.CoraxSkipMessage)]
+    [MemberData("Configuration", Skip = RavenTheoryAttribute.CoraxSkipMessage)]
+    public void CanDeleteAndInsertInRandomOrderX64Only(int seed, int size) => CanDeleteAndInsertInRandomOrderBase(seed, size);
+    
+    [MultiplatformTheory(RavenPlatform.Windows, RavenArchitecture.X64)]
+    [InlineData(391060845, 31707323, Skip = RavenTheoryAttribute.CoraxSkipMessage)]
+    public void CanDeleteAndInsertInRandomOrderWindows(int seed, int size) => CanDeleteAndInsertInRandomOrderBase(seed, size);
+
+    private void CanDeleteAndInsertInRandomOrderBase(int seed, int size)
     {
         using var testClass = new FastTests.Voron.PostingLists.PostingListTests(Output);
         testClass.CanDeleteAndInsertInRandomOrder(seed, size, 10);
