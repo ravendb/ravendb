42b41e22b7ce4f946a46d4fc7a3c2ffaeafbe608
Igal Merhavia <igal@ravendb.net>
2024-11-01 19:31:49 +0200

    RavenDB-22142 Enum rule validator

---
+++ src/Raven.Server/SchemaValidation/Object/EnumSchemaRuleValidator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sparrow.Json;
+
+namespace Raven.Server.SchemaValidation.Object;
+
+[SchemaRule("enum")]
+public class EnumSchemaRuleValidator : SchemaRuleValidator<object>
+{
+    private readonly object[] _enum;
+
+    public EnumSchemaRuleValidator(string path, IEnumerable<object> @enum) : base(path)
+    {
+        _enum = @enum.Select(ConvertType).ToArray();
+    }
+
+    private object ConvertType(object x)
+    {
+        if (x is LazyNumberValue lnx)
+        {
+            return (decimal)lnx;
+        }
+
+        if (x is LazyStringValue or LazyCompressedStringValue)
+        {
+            return x.ToString();
+        }
+
+        if (x is BlittableJsonReaderObject or BlittableJsonReaderArray)
+        {
+            //TODO To have context in the validator and clone the blittables and maybe also LazyStringValue and LazyNumberValue
+            return x.ToString();
+        }
+
+        if (x is long lx)
+        {
+            return (decimal)lx;
+        }
+
+        throw new InvalidOperationException($"The type {x.GetType()} is not supported. {Path}");
+    }
+
+    protected override void ValidateInternal(object value, IErrorBuilder errorBuilder)
+    {
+        if(_enum.Any(x => x.Equals(ConvertType(value))) == false)
+            //TODO Clear error to differentiate between number and string (15 or "15")
+            errorBuilder.AddError($"The value '{value}' at '{Path}' is not an allowed value. Expected one of: {string.Join(", ", _enum)}.");
+    }
+}
--- test/FastTests/SchemaValidation/BasicSchemaValidationTests.cs
+++ test/FastTests/SchemaValidation/BasicSchemaValidationTests.cs
@@ -3,11 +3,16 @@ using Sparrow.Json;
 using Sparrow.Json.Parsing;
 using Tests.Infrastructure;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace FastTests.SchemaValidation;
 
-public class BasicSchemaValidationTests
+public class BasicSchemaValidationTests : SchemaValidationTestsBase
 {
+    public BasicSchemaValidationTests(ITestOutputHelper output) : base(output)
+    {
+    }
+
     [RavenFact(RavenTestCategory.JavaScript)]
     public void SchemaValidation_WhenValidateObject_ShouldSuccess()
     {
@@ -216,5 +221,68 @@ public class BasicSchemaValidationTests
         schemaValidator.Init(schemaDefinition);
         Assert.False(schemaValidator.Validate(testObj, out string errors));
         //TODO Check exact error
+    }
+
+    [RavenFact(RavenTestCategory.JavaScript)]
+    public void SchemaValidation_WhenValidateEnum()
+    {
+        using var context = JsonOperationContext.ShortTermSingleUse();
+
+        var schemaValidator = new SchemaValidator();
+        var schemaDefinition = new DynamicJsonValue
+        {
+            ["type"] = "object",
+            ["properties"] = new DynamicJsonValue
+            {
+                ["prop"] = new DynamicJsonValue { ["enum"] = new object[] { 15, "somevalue", new DynamicJsonValue { ["prop"] = 8 } } }
+            }
+        };
+        using (var blitSchemaDefinition = ReadObject(schemaDefinition))
+        {
+            schemaValidator.Init(blitSchemaDefinition);
+        }
+
+        Assert.Multiple(() =>
+            {
+                using var obj = ReadObject(new DynamicJsonValue { ["prop"] = 15 });
+
+                if (schemaValidator.Validate(obj, out string errors) == false)
+                    Assert.Fail(string.Join("\n", errors));
+            },
+            () =>
+            {
+                using var obj = ReadObject(new DynamicJsonValue { ["prop"] = "somevalue" });
+
+                if (schemaValidator.Validate(obj, out string errors) == false)
+                    Assert.Fail(string.Join("\n", errors));
+            },
+            () =>
+            {
+                using var obj = ReadObject(new DynamicJsonValue { ["prop"] = new DynamicJsonValue { ["prop"] = 8 } });
+
+                if (schemaValidator.Validate(obj, out string errors) == false)
+                    Assert.Fail(string.Join("\n", errors));
+            },
+            () =>
+            {
+                using var obj = ReadObject(new DynamicJsonValue { ["prop"] = "16" });
+
+                Assert.False(schemaValidator.Validate(obj, out var errors));
+                AssertError("The value '16' at 'root.prop' is not an allowed value. Expected one of: 15, somevalue, {\"prop\":8}.", errors);
+            },
+            () =>
+            {
+                using var obj = ReadObject(new DynamicJsonValue { ["prop"] = "someothervalue" });
+
+                Assert.False(schemaValidator.Validate(obj, out var errors));
+                AssertError("The value 'someothervalue' at 'root.prop' is not an allowed value. Expected one of: 15, somevalue, {\"prop\":8}.", errors);
+            },
+            () =>
+            {
+                using var obj = ReadObject(new DynamicJsonValue { ["prop"] = new DynamicJsonValue { ["prop"] = 9 } });
+
+                Assert.False(schemaValidator.Validate(obj, out var errors));
+                AssertError("The value '{\"prop\":9}' at 'root.prop' is not an allowed value. Expected one of: 15, somevalue, {\"prop\":8}.", errors);
+            });
     }
 }
