5bbfd3f181f8e828746245fbea0bd86dd9f621df
Ayende Rahien <Ayende@ayende.com>
2024-11-11 15:23:35 +0200

    RavenDB-22076 - Implementing vector storage to reduce fragmentation costs

--- test/FastTests/Voron/Graphs/BasicGraphs.cs
+++ test/FastTests/Voron/Graphs/BasicGraphs.cs
@@ -79,4 +79,66 @@ public class BasicGraphs(ITestOutputHelper output) : StorageTest(output)
             Assert.Equal(12, matches[2]);
         }
     }
+
+    private static void Fill(float[] f, int seed)
+    {
+        var random = new Random(seed); 
+        for (int i = 0; i < f.Length; i++)
+        {
+            f[i] = random.NextSingle();
+        }
+    }
+
+
+    [RavenFact(RavenTestCategory.Voron)]
+    public void WithLargeVectors()
+    {
+        float[] v1 = new float[768];
+        float[] v2 = new float[768];
+
+        // nearest to v2, then v1
+        float[] v3 = new float[768];
+
+        Fill(v1, 123);
+        Fill(v2, 321);
+        Fill(v3, 481);
+
+        using (var txw = Env.WriteTransaction())
+        {
+            Hnsw.Create(txw.LowLevelTransaction, "test", v1.Length * 4, 3, 12);
+
+            using (var registration = Hnsw.RegistrationFor(txw.LowLevelTransaction, "test"))
+            {
+                registration.Register(4, MemoryMarshal.Cast<float, byte>(v1));
+                registration.Register(8, MemoryMarshal.Cast<float, byte>(v2));
+            }
+
+            using (var registration = Hnsw.RegistrationFor(txw.LowLevelTransaction, "test"))
+            {
+                registration.Register(12, MemoryMarshal.Cast<float, byte>(v1));
+            }
+
+            txw.Commit();
+        }
+
+        using (var txr = Env.ReadTransaction())
+        {
+            var state = new Hnsw.SearchState(txr.LowLevelTransaction, "test");
+            var options = state.Options;
+            Assert.Equal(12, options.NumberOfCandidates);
+            Assert.Equal(3, options.NumberOfEdges);
+            Assert.Equal(2, options.CountOfVectors);
+        }
+
+        using (var txr = Env.ReadTransaction())
+        {
+            Span<long> matches = new long[8];
+            Span<float> distances = new float[8];
+            using var nearest = Hnsw.ApproximateNearest(txr.LowLevelTransaction, "test",
+                numberOfCandidates: 32,
+                MemoryMarshal.Cast<float, byte>(v3));
+            int read = nearest.Fill(matches, distances);
+            Assert.Equal(3, read);
+        }
+    }
 }
