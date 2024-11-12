3e533f694b03c34b6fee0f5c6d79d6f16d072ce5
Ayende Rahien <Ayende@ayende.com>
2024-11-12 15:10:25 +0200

    RavenDB-22076 - Implementing removal from the HNSW

--- test/FastTests/Voron/Graphs/BasicGraphs.cs
+++ test/FastTests/Voron/Graphs/BasicGraphs.cs
@@ -79,6 +79,61 @@ public class BasicGraphs(ITestOutputHelper output) : StorageTest(output)
             Assert.Equal(12, matches[2]);
         }
     }
+    
+    
+
+    [RavenFact(RavenTestCategory.Voron)]
+    public void CanAddAndRemove()
+    {
+        float[] v1 = [0.1f, 0.2f, 0.3f, 0.4f];
+        float[] v2 = [0.15f, 0.25f, 0.35f, 0.45f];
+
+        // nearest to v2, then v1
+        float[] v3 = [0.25f, 0.35f, 0.45f, 0.55f];
+
+        using (var txw = Env.WriteTransaction())
+        {
+            Hnsw.Create(txw.LowLevelTransaction, "test", 16, 3, 12);
+
+            using (var registration = Hnsw.RegistrationFor(txw.LowLevelTransaction, "test"))
+            {
+                registration.Register(4, MemoryMarshal.Cast<float, byte>(v1));
+                registration.Register(8, MemoryMarshal.Cast<float, byte>(v2));
+            }
+            
+            using (var registration = Hnsw.RegistrationFor(txw.LowLevelTransaction, "test"))
+            {
+                registration.Register(12, MemoryMarshal.Cast<float, byte>(v1));
+            }
+
+            txw.Commit();
+        }
+
+        using (var txw = Env.WriteTransaction())
+        {
+            Hnsw.Create(txw.LowLevelTransaction, "test", 16, 3, 12);
+
+            using (var registration = Hnsw.RegistrationFor(txw.LowLevelTransaction, "test"))
+            {
+                registration.Remove(4, MemoryMarshal.Cast<float, byte>(v1));
+            }
+
+            txw.Commit();
+        }
+
+        using (var txr = Env.ReadTransaction())
+        {
+            Span<long> matches = new long[8];
+            Span<float> distances = new float[8];
+            using var nearest = Hnsw.ApproximateNearest(txr.LowLevelTransaction, "test",
+                numberOfCandidates: 32,
+                MemoryMarshal.Cast<float, byte>(v3));
+            int read = nearest.Fill(matches, distances);
+            Assert.Equal(2, read);
+            Assert.Equal(8, matches[0]);
+            Assert.Equal(12, matches[1]);
+        }
+    }
 
     private static void Fill(float[] f, int seed)
     {
