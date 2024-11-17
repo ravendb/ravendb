0a0820e29dc5266bcff9e7a6e1335f673f2d7639
Grisha Kotler <grisha@ravendb.net>
2024-11-17 17:06:07 +0200

    RavenDB-23100 - add counters and time series tests

--- test/SlowTests/Issues/RavenDB-23100.cs
+++ test/SlowTests/Issues/RavenDB-23100.cs
@@ -1,8 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using FastTests;
 using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Indexes.Counters;
+using Raven.Client.Documents.Indexes.TimeSeries;
 using Raven.Client.Documents.Operations.Backups;
 using Raven.Client.Documents.Session;
 using Raven.Server.ServerWide.Context;
@@ -22,6 +25,10 @@ namespace SlowTests.Issues
         private const string EmployeeId2 = "EmPlo#ees/2-A";
         private const string CompanyId = "COMan!es/1-A";
 
+        private static readonly Company _company = new Company { Id = CompanyId, Name = "RavenDB" };
+        private static readonly Employee _employee1 = new Employee { Id = EmployeeId1, CompanyId = _company.Id };
+        private static readonly Employee _employee2 = new Employee { Id = EmployeeId2, CompanyId = _company.Id };
+
         [RavenFact(RavenTestCategory.Indexes)]
         public async Task Can_Delete_Document_References()
         {
@@ -30,22 +37,16 @@ namespace SlowTests.Issues
                 var index = new DocumentsIndex();
                 await store.ExecuteIndexAsync(index);
 
-                var company = new Company { Id = CompanyId, Name = "RavenDB" };
-
-                var employee1 = new Employee { Id = EmployeeId1, CompanyId = company.Id };
-                var employee2 = new Employee { Id = EmployeeId2, CompanyId = company.Id };
-
                 using (var session = store.OpenAsyncSession())
                 {
-                    await session.StoreAsync(company);
-                    await session.StoreAsync(employee1);
-                    await session.StoreAsync(employee2);
+                    await session.StoreAsync(_company);
+                    await session.StoreAsync(_employee1);
+                    await session.StoreAsync(_employee2);
                     await session.SaveChangesAsync();
                 }
 
                 Indexes.WaitForIndexing(store);
 
-                WaitForUserToContinueTheTest(store);
                 var database = await GetDatabase(store.Database);
                 var indexInstance = database.IndexStore.GetIndex(index.IndexName);
 
@@ -65,7 +66,7 @@ namespace SlowTests.Issues
 
                 using (var session = store.OpenAsyncSession())
                 {
-                    session.Delete(employee1.Id);
+                    session.Delete(_employee1.Id);
                     await session.SaveChangesAsync();
                 }
 
@@ -87,7 +88,7 @@ namespace SlowTests.Issues
 
                 using (var session = store.OpenAsyncSession())
                 {
-                    session.Delete(employee2.Id);
+                    session.Delete(_employee2.Id);
                     await session.SaveChangesAsync();
                 }
 
@@ -211,21 +212,16 @@ namespace SlowTests.Issues
 
                 await store.ExecuteIndexAsync(index);
 
-                var commonName = "Company#1";
-
                 using (var session = store.OpenAsyncSession(new SessionOptions { TransactionMode = TransactionMode.ClusterWide }))
                 {
-                    session.Advanced.ClusterTransaction.CreateCompareExchangeValue(commonName, new Company { Name = "RavenDB" });
+                    session.Advanced.ClusterTransaction.CreateCompareExchangeValue("Company#1", new Company { Name = "RavenDB" });
                     await session.SaveChangesAsync();
                 }
 
-                var employee1 = new Employee { Id = "Employ&es/1-A", CompanyId = commonName };
-                var employee2 = new Employee { Id = "EmPlo#ees/2-A", CompanyId = commonName };
-
                 using (var session = store.OpenSession())
                 {
-                    session.Store(employee1);
-                    session.Store(employee2);
+                    session.Store(_employee1);
+                    session.Store(_employee2);
                     session.SaveChanges();
                 }
 
@@ -250,7 +246,7 @@ namespace SlowTests.Issues
 
                 using (var session = store.OpenAsyncSession())
                 {
-                    session.Delete(employee1.Id);
+                    session.Delete(_employee1.Id);
                     await session.SaveChangesAsync();
                 }
 
@@ -272,7 +268,7 @@ namespace SlowTests.Issues
 
                 using (var session = store.OpenAsyncSession())
                 {
-                    session.Delete(employee2.Id);
+                    session.Delete(_employee2.Id);
                     await session.SaveChangesAsync();
                 }
 
@@ -287,6 +283,182 @@ namespace SlowTests.Issues
                     Assert.Equal(0, counts.CollectionTableCount);
 
                     counts = indexInstance._indexStorage.ReferencesForCompareExchange.GetReferenceTablesCount("Employees", tx);
+
+                    Assert.Equal(0, counts.ReferenceTableCount);
+                    Assert.Equal(0, counts.CollectionTableCount);
+                }
+
+            }
+        }
+
+        [RavenFact(RavenTestCategory.Indexes)]
+        public async Task Can_Delete_Counters_References()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var index = new CountersIndex();
+
+                await store.ExecuteIndexAsync(index);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    await session.StoreAsync(_company);
+
+                    await session.StoreAsync(_employee1);
+                    session.CountersFor(_employee1.Id).Increment(CompanyId);
+
+                    await session.StoreAsync(_employee2);
+                    session.CountersFor(_employee2.Id).Increment(CompanyId);
+
+                    await session.SaveChangesAsync();
+                }
+
+                Indexes.WaitForIndexing(store);
+
+                var database = await GetDatabase(store.Database);
+                var indexInstance = database.IndexStore.GetIndex(index.IndexName);
+
+                using (indexInstance._contextPool.AllocateOperationContext(out TransactionOperationContext context))
+                using (var tx = context.OpenReadTransaction())
+                {
+                    var counts = indexInstance._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Companies", tx);
+
+                    Assert.Equal(2, counts.ReferenceTableCount);
+                    Assert.Equal(0, counts.CollectionTableCount);
+
+                    counts = indexInstance._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Employees", tx);
+
+                    Assert.Equal(2, counts.ReferenceTableCount);
+                    Assert.Equal(1, counts.CollectionTableCount);
+                }
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    session.Delete(_employee1.Id);
+                    await session.SaveChangesAsync();
+                }
+
+                Indexes.WaitForIndexing(store);
+
+                using (indexInstance._contextPool.AllocateOperationContext(out TransactionOperationContext context))
+                using (var tx = context.OpenReadTransaction())
+                {
+                    var counts = indexInstance._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Companies", tx);
+
+                    Assert.Equal(1, counts.ReferenceTableCount);
+                    Assert.Equal(0, counts.CollectionTableCount);
+
+                    counts = indexInstance._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Employees", tx);
+
+                    Assert.Equal(1, counts.ReferenceTableCount);
+                    Assert.Equal(1, counts.CollectionTableCount);
+                }
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    session.Delete(_employee2.Id);
+                    await session.SaveChangesAsync();
+                }
+
+                Indexes.WaitForIndexing(store);
+
+                using (indexInstance._contextPool.AllocateOperationContext(out TransactionOperationContext context))
+                using (var tx = context.OpenReadTransaction())
+                {
+                    var counts = indexInstance._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Companies", tx);
+
+                    Assert.Equal(0, counts.ReferenceTableCount);
+                    Assert.Equal(0, counts.CollectionTableCount);
+
+                    counts = indexInstance._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Employees", tx);
+
+                    Assert.Equal(0, counts.ReferenceTableCount);
+                    Assert.Equal(0, counts.CollectionTableCount);
+                }
+
+            }
+        }
+
+        [RavenFact(RavenTestCategory.Indexes)]
+        public async Task Can_Delete_TimeSeries_References()
+        {
+            using (var store = GetDocumentStore())
+            {
+                var index = new TimeSeriesIndex();
+
+                await store.ExecuteIndexAsync(index);
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    var baseDate = DateTime.UtcNow;
+
+                    await session.StoreAsync(_employee1);
+                    session.TimeSeriesFor(_employee1.Id, CompanyId).Append(baseDate, 1, CompanyId);
+
+                    await session.StoreAsync(_employee2);
+                    session.TimeSeriesFor(_employee2.Id, CompanyId).Append(baseDate, 1, CompanyId);
+
+                    await session.SaveChangesAsync();
+                }
+
+                Indexes.WaitForIndexing(store);
+
+                var database = await GetDatabase(store.Database);
+                var indexInstance = database.IndexStore.GetIndex(index.IndexName);
+
+                using (indexInstance._contextPool.AllocateOperationContext(out TransactionOperationContext context))
+                using (var tx = context.OpenReadTransaction())
+                {
+                    var counts = indexInstance._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Companies", tx);
+
+                    Assert.Equal(2, counts.ReferenceTableCount);
+                    Assert.Equal(0, counts.CollectionTableCount);
+
+                    counts = indexInstance._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Employees", tx);
+
+                    Assert.Equal(2, counts.ReferenceTableCount);
+                    Assert.Equal(1, counts.CollectionTableCount);
+                }
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    session.Delete(_employee1.Id);
+                    await session.SaveChangesAsync();
+                }
+
+                Indexes.WaitForIndexing(store);
+
+                using (indexInstance._contextPool.AllocateOperationContext(out TransactionOperationContext context))
+                using (var tx = context.OpenReadTransaction())
+                {
+                    var counts = indexInstance._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Companies", tx);
+
+                    Assert.Equal(1, counts.ReferenceTableCount);
+                    Assert.Equal(0, counts.CollectionTableCount);
+
+                    counts = indexInstance._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Employees", tx);
+
+                    Assert.Equal(1, counts.ReferenceTableCount);
+                    Assert.Equal(1, counts.CollectionTableCount);
+                }
+
+                using (var session = store.OpenAsyncSession())
+                {
+                    session.Delete(_employee2.Id);
+                    await session.SaveChangesAsync();
+                }
+
+                Indexes.WaitForIndexing(store);
+
+                using (indexInstance._contextPool.AllocateOperationContext(out TransactionOperationContext context))
+                using (var tx = context.OpenReadTransaction())
+                {
+                    var counts = indexInstance._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Companies", tx);
+
+                    Assert.Equal(0, counts.ReferenceTableCount);
+                    Assert.Equal(0, counts.CollectionTableCount);
+
+                    counts = indexInstance._indexStorage.ReferencesForDocuments.GetReferenceTablesCount("Employees", tx);
 
                     Assert.Equal(0, counts.ReferenceTableCount);
                     Assert.Equal(0, counts.CollectionTableCount);
@@ -341,6 +513,44 @@ namespace SlowTests.Issues
                     {
                         CompanyName = LoadCompareExchangeValue<Company>(employee.CompanyId).Name
                     };
+            }
+        }
+
+        private class CountersIndex : AbstractCountersIndexCreationTask<Employee>
+        {
+            public class Result
+            {
+                public string CompanyName { get; set; }
+            }
+
+            public CountersIndex()
+            {
+                AddMap(CompanyId,
+                    counters => from counter in counters
+                        select new Result
+                        {
+                            CompanyName = LoadDocument<Company>(counter.Name).Name
+                        });
+            }
+        }
+
+        private class TimeSeriesIndex : AbstractTimeSeriesIndexCreationTask<Employee>
+        {
+            public class Result
+            {
+                public string CompanyName { get; set; }
+            }
+
+            public TimeSeriesIndex()
+            {
+                AddMap(
+                    CompanyId,
+                    timeSeries => from ts in timeSeries
+                        from entry in ts.Entries
+                        select new Result
+                        {
+                            CompanyName = LoadDocument<Company>(entry.Tag).Name
+                        });
             }
         }
     }
