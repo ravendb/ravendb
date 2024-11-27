fc6837099cbb1d798cb941c290f4da98bc863ad3
efrat@ravendb.net <efrat@ravendb.net>
2024-11-27 14:27:26 +0200

    RavenDB-22948 tests

--- test/SlowTests/Issues/RavenDB-22948.cs
+++ test/SlowTests/Issues/RavenDB-22948.cs
@@ -102,6 +102,7 @@ namespace SlowTests.Issues
         }
 
         private static int IdCounter;
+
         private abstract class EntityWithId
         {
             public EntityWithId()
@@ -115,6 +116,200 @@ namespace SlowTests.Issues
                 IdCounter++;
                 return GetType().Name + "-" + IdCounter;
             }
+        }
+
+        [RavenFact(RavenTestCategory.Querying | RavenTestCategory.ClientApi)]
+        public void Test_TwoClients_DifferentIdHandling()
+        {
+            using (var defaultStore = new DocumentStore
+            {
+                Urls = new []{Server.WebUrl},
+                Database = "Test"
+            }.Initialize())
+            {
+                var doc1Id = "users/1";
+                var doc2Id = "users/2";
+
+                using (var customStore = GetDocumentStore(new Options
+                       {
+                           ModifyDatabaseName = x =>"Test",
+                           ModifyDocumentStore = s =>
+                           {
+                               s.Conventions.FindIdentityPropertyNameFromCollectionName = (typeName) => "TestId";
+                               s.Conventions.FindIdentityProperty = prop => prop.Name == "TestId";
+                           }
+                       }))
+                {
+                    //Id is identity property
+                    using (var session1 = defaultStore.OpenSession())
+                    {
+                        var defaultUser = new User
+                        {
+                            Id = doc1Id,
+                            TestId = "users/1-T",
+                            Name = "Default Store User"
+                        };
+                        session1.Store(defaultUser);
+                        session1.SaveChanges();
+                    }
+
+                    //TestId is identity property
+                    using (var session2 = customStore.OpenSession())
+                    {
+                        var customUser = new User
+                        {
+                            Id = "users/2-T",
+                            TestId = doc2Id,
+                            Name = "Custom Store User"
+                        };
+                        session2.Store(customUser);
+                        session2.SaveChanges();
+
+                        var user1 = session2.Load<User>(doc1Id);
+                        user1.Id = "users/3";
+                        session2.Store(user1);
+                        session2.SaveChanges();
+                    }
+
+                    //Id is identity property
+                    using (var session1 = defaultStore.OpenSession())
+                    {
+                        var query = session1.Query<User>().Select(x => new { Id = x.Id, TestId = x.TestId });
+                        var results = query.ToList();
+
+                        Assert.Null(results[0].TestId);
+                        Assert.Equal(doc2Id, results[0].Id);
+
+                        Assert.Null(results[1].TestId);
+                        Assert.Equal(doc1Id, results[1].Id);
+                    }
+
+                    //TestId is identity property
+                    using (var session2 = customStore.OpenSession())
+                    {
+                        var query = session2.Query<User>().Select(x => new { Id = x.Id, TestId = x.TestId });
+                        var results = query.ToList();
+
+                        Assert.Equal("users/2-T", results[0].Id);
+                        Assert.Equal(doc2Id, results[0].TestId);
+
+                        Assert.Equal("users/3", results[1].Id);
+                        Assert.Equal(doc1Id, results[1].TestId);
+                    }
+                }
+            }
+        }
+
+        [RavenFact(RavenTestCategory.Querying | RavenTestCategory.ClientApi)]
+        public void Test_TwoClients_DifferentIdHandlingWithLoad()
+        {
+            using (var defaultStore = new DocumentStore
+            {
+                Urls = new[] { Server.WebUrl },
+                Database = "Test"
+            }.Initialize())
+            {
+                var doc1Id = "users/1";
+                var doc2Id = "users/2";
+
+                using (var customStore = GetDocumentStore(new Options
+                {
+                    ModifyDatabaseName = x => "Test",
+                    ModifyDocumentStore = s =>
+                    {
+                        s.Conventions.FindIdentityPropertyNameFromCollectionName = (typeName) => "TestId";
+                        s.Conventions.FindIdentityProperty = prop => prop.Name == "TestId";
+                    }
+                }))
+                {
+                    //Id is identity property
+                    using (var session1 = defaultStore.OpenSession())
+                    {
+                        var defaultUser = new User
+                        {
+                            Id = doc1Id,
+                            TestId = "users/1-T",
+                            Name = "Default Store User"
+                        };
+                        session1.Store(defaultUser); //Document Id is users/1
+                        session1.SaveChanges();
+                    }
+
+                    //TestId is identity property
+                    using (var session2 = customStore.OpenSession())
+                    {
+                        var customUser = new User
+                        {
+                            Id = "users/2-T",
+                            TestId = doc2Id,
+                            Name = "Custom Store User"
+                        };
+                        session2.Store(customUser); //Document Id is users/2
+                        session2.SaveChanges();
+
+                        var user1 = session2.Load<User>(doc1Id);
+                        user1.Id = "users/3";
+                        session2.Store(user1); // changing Id property users/1 document to users/3
+                        session2.SaveChanges();
+                    }
+
+                    //Id is identity property
+                    using (var session1 = defaultStore.OpenSession())
+                    {
+                        var results = session1
+                            .Query<User>()
+                            .Select(x => new UserWithPet()
+                            {
+                                User = session1.Load<User>(x.Id),
+                                Pet = "My Pet"
+                            })
+                            .ToList();
+
+                        //*****************Not expected****************************
+                        Assert.Equal("users/2-T", results[0].User.Id); //need to be users/2
+                        Assert.Null(results[0].User.TestId);
+
+                        Assert.Equal("users/3", results[1].User.Id); // need to be users/1
+                        Assert.Null(results[1].User.TestId);
+                        //**********************************************************
+                    }
+
+                    //TestId is identity property
+                    using (var session2 = customStore.OpenSession())
+                    {
+                        var results = session2
+                            .Query<User>()
+                            .Select(x => new UserWithPet()
+                            {
+                                User = session2.Load<User>(x.TestId),
+                                Pet = "My Pet"
+                            })
+                            .ToList();
+
+                        Assert.Equal("users/2-T", results[0].User.Id);
+                        Assert.Equal(doc2Id, results[0].User.TestId);
+
+                        Assert.Equal("users/3", results[1].User.Id);
+                        Assert.Equal(doc1Id, results[1].User.TestId);
+                    }
+                }
+            }
+        }
+
+        private class User
+        {
+            public string Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string TestId { get; set; }
+        }
+
+        private class UserWithPet
+        {
+            public User User { get; set; }
+
+            public string Pet { get; set; }
         }
     }
 }
