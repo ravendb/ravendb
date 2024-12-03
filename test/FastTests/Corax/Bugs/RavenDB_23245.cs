6b46528fd9fcc991d03afab8bdddd3cacbff494b
Maciej Aszyk <maciej@ravendb.net>
2024-12-03 15:44:20 +0100

    RavenDB-23245 Added descending score tests.

--- test/FastTests/Corax/Bugs/RavenDB_23245.cs
+++ test/FastTests/Corax/Bugs/RavenDB_23245.cs
@@ -9,54 +9,62 @@ using Xunit.Abstractions;
 
 namespace FastTests.Corax.Bugs;
 
-public class RavenDB_23245 : RavenTestBase
+public class RavenDB_23245(ITestOutputHelper output) : RavenTestBase(output)
 {
-    public RavenDB_23245(ITestOutputHelper output) : base(output)
-    {
-    }
+    private static string OrderByClause(bool isDescending) => isDescending ? "desc" : "asc";
+    private static int CompareResult(bool isDescending) => isDescending ? -1 : 1; // note reverted arguments
 
-    [RavenFact(RavenTestCategory.Corax | RavenTestCategory.Querying)]
-    public void OrderByScoreScoreExposed()
+    [RavenTheory(RavenTestCategory.Corax | RavenTestCategory.Querying)]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void OrderByScoreScoreExposed(bool isDescending)
     {
         using var store = GetDatabaseWithDocuments();
         using (var session = store.OpenSession())
         {
-            var results = session.Advanced.RawQuery<QueryResult>("from index 'Index' v where v.Text == 'text' select { Score: v['@metadata']['@index-score'] , Numerical: v.Numerical}").ToList();
+            var results = session.Advanced.RawQuery<QueryResult>($"from index 'Index' v where v.Text == 'text' order by score() {OrderByClause(isDescending)} select {{ Score: v['@metadata']['@index-score'] , Numerical: v.Numerical}}").ToList();
             
             Assert.Equal(2, results.Count);
-            Assert.True(results[0].Numerical > results[1].Numerical);
-            Assert.True(results[0].Score > results[1].Score);
+            Assert.Equal(CompareResult(isDescending), results[0].Numerical.CompareTo(results[1].Numerical));
+            Assert.Equal(CompareResult(isDescending), results[0].Score.CompareTo(results[1].Score));
         }
     }
     
-    [RavenFact(RavenTestCategory.Corax | RavenTestCategory.Querying)]
-    public void OrderByNumScoreExposed()
+    [RavenTheory(RavenTestCategory.Corax | RavenTestCategory.Querying)]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void OrderByNumScoreExposed(bool isDescending)
     {
         using var store = GetDatabaseWithDocuments();
         using (var session = store.OpenSession())
         {
             var results = session.Advanced.RawQuery<QueryResult>("from index 'Index' v where boost(v.Text == 'text', 10)" +
-                                                                 " order by v.Numerical as long asc, score() select { Score: v['@metadata']['@index-score'] , Numerical: v.Numerical}")
+                                                                 $" order by v.EqualNumberForTestSecondParameter as long asc, score() {OrderByClause(isDescending)} select {{ Score: v['@metadata']['@index-score'] , Numerical: v.Numerical}}")
                 .ToList();
             
             Assert.Equal(2, results.Count);
-            Assert.True(results[0].Numerical < results[1].Numerical);
-            Assert.True(results[0].Score < results[1].Score);
+            Assert.Equal(CompareResult(isDescending), results[0].Score.CompareTo(results[1].Score));
+            Assert.Equal(CompareResult(isDescending), results[0].Numerical.CompareTo(results[1].Numerical));
         }
     }
     
-    [RavenFact(RavenTestCategory.Corax | RavenTestCategory.Querying)]
-    public void OrderByStringScoreExposed()
+    [RavenTheory(RavenTestCategory.Corax | RavenTestCategory.Querying)]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void OrderByStringScoreExposed(bool isDescending)
     {
         using var store = GetDatabaseWithDocuments();
         using (var session = store.OpenSession())
         {
             var results = session.Advanced.RawQuery<QueryResult>("from index 'Index' v where boost(v.TextNormal == 'text text', 10)" +
-                                                                 " order by v.TextNormal, score() select { Score: v['@metadata']['@index-score'] , Numerical: v.Numerical}")
+                                                                 " order by v.TextNormal, " +
+                                                                 $"score() {OrderByClause(isDescending)} " +
+                                                                 "select { Score: v['@metadata']['@index-score'] , Numerical: v.Numerical}")
                 .ToList();
             
             Assert.Equal(2, results.Count);
-            Assert.True(results[0].Score > results[1].Score);
+            Assert.Equal(CompareResult(isDescending), results[0].Score.CompareTo(results[1].Score));
+            Assert.Equal(CompareResult(isDescending), results[0].Numerical.CompareTo(results[1].Numerical));
         }
     }
 
@@ -72,8 +80,8 @@ public class RavenDB_23245 : RavenTestBase
         new Index().Execute(store);
         using (var session = store.OpenSession())
         {
-            session.Store(new Dto(10, "text text", 1));
-            session.Store(new Dto(20, "text text", 2));
+            session.Store(new Dto(10, "text text", 1, 1));
+            session.Store(new Dto(20, "text text", 2, 1));
             session.SaveChanges();
         }
 
@@ -82,7 +90,7 @@ public class RavenDB_23245 : RavenTestBase
         return store;
     }
 
-    private record Dto(float DocBoost, string Text, int Numerical);
+    private record Dto(float DocBoost, string Text, int Numerical, int EqualNumberForTestSecondParameter);
 
     private class QueryResult
     {
@@ -95,7 +103,7 @@ public class RavenDB_23245 : RavenTestBase
         public Index()
         {
             Map = dtos => from doc in dtos
-                select new { doc.Text, doc.Numerical, TextNormal = doc.Text }.Boost(doc.DocBoost);
+                select new { doc.Text, doc.Numerical, TextNormal = doc.Text, doc.EqualNumberForTestSecondParameter }.Boost(doc.DocBoost);
             Index(x => x.Text, FieldIndexing.Search);
         }
     }
