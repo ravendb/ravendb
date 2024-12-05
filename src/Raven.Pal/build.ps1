82dd3e0e3b75cbf7d297914c4b550f79734ac3fc
Ayende Rahien <ayende@ayende.com>
2024-12-05 11:20:12 +0200

    RavenDB-23085 - Allowing to build PAL using MSVC, not just zig cc

--- src/Raven.Pal/build.ps1
+++ src/Raven.Pal/build.ps1
@@ -1,5 +1,4 @@
-param ( [switch]$skip_version_increment = $false )
-
+param ( [switch]$skip_version_increment = $false, [switch]$clang_only = $false )
 
 
 if ($null -eq (Get-Command "zig" -ErrorAction SilentlyContinue)) {
@@ -23,11 +22,9 @@ if ( $skip_version_increment -eq $false ) {
 $GenCode = "#include <sys/types.h>`n#include ""rvn.h""`nEXPORT int32_t rvn_get_pal_ver() { return  $($PalVer) ; }"
 Set-Content src/rvngetpalver.c -Value $GenCode
 
-
 $shared = `
     "src/shared_all.c",
 "src/rvngetpalver.c"
-
 
 $win_files = `
     "src/win/fileutils.c",
@@ -51,6 +48,10 @@ $posix_files = `
 "src/posix/virtualmemory.c",
 "src/posix/writefileheader.c"
 
+$vcbin = "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build"
+$clbin = (gci "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\" | sort Name -Descending | select -First 1).FullName
+$clbin = "$clbin\bin\Hostx64"
+
 Remove-Item .\runtimes -Force -Recurse -ErrorAction Ignore
 Remove-Item .\artifacts -Force -Recurse -ErrorAction Ignore
 
@@ -71,10 +72,26 @@ Copy-Item ../../libs/libzstd/libzstd.mac.arm64.dylib runtimes/osx-arm64/native/l
 
 
 Write-Output "Building Windows x86"
-zig cc -Wall -O3 -g -shared -fPIC -Iinc -target x86-windows -o runtimes/win-x86/native/librvnpal.dll $shared $win_files 
+if ($clang_only) {
+    zig cc -Wall -O3 -g -shared -fPIC -Iinc -target x86-windows -o runtimes/win-x86/native/librvnpal.dll $shared $win_files 
+}
+else {
+    cmd /c """$vcbin\vcvars32.bat"" & ""$clbin\x86\cl"" -Feruntimes/win-x86/native/librvnpal.dll -I inc /O2 /analyze /sdl /LD $shared $win_files /link"
+    Remove-Item *.obj
+    Remove-Item  librvnpal.x*
+    Remove-Item *.nativecodeanalysis.xml        
+}
 
 Write-Output "Building Windows x64"
-zig cc -Wall -O3 -g -shared -fPIC -Iinc -target x86_64-windows -o runtimes/win-x64/native/librvnpal.dll  $shared $win_files 
+if ($clang_only) {
+    zig cc -Wall -O3 -g -shared -fPIC -Iinc -target x86_64-windows -o runtimes/win-x64/native/librvnpal.dll  $shared $win_files 
+}
+else {
+    cmd /c """$vcbin\vcvars64.bat"" & ""$clbin\x64\cl"" -Feruntimes/win-x64/native/librvnpal.dll -I inc /O2 /analyze /sdl /LD $shared $win_files /link"
+    Remove-Item *.obj
+    Remove-Item  librvnpal.x*
+    Remove-Item *.nativecodeanalysis.xml
+}
 
 Write-Output "Building Linux x64"
 zig cc -Wall -O3 -g -shared  -fPIC -Iinc -target x86_64-linux-gnu ../../libs/liburing/liburing-2.8.1-x64.a -o runtimes/linux-x64/native/librvnpal.so $shared $posix_files "src/posix/linuxonly.c" 
