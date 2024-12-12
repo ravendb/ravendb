9a15a4d208c9499e5513e10d064c3723803214b8
Federico Lois <federico.lois@gmail.com>
2024-12-12 14:43:00 -0300

    RavenDB-23076: Started adding support for RavenVector

---
+++ src/Raven.Client/Documents/RavenVector.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace Raven.Client.Documents;
+
+public class RavenVector<T> where T : unmanaged
+#if NET7_0_OR_GREATER
+    , INumber<T>
+#endif
+{
+    public T[] Embedding { get; set; }
+    
+    public RavenVector(IEnumerable<T> embedding)
+    {
+        AssertEmbeddingType();
+        Embedding = embedding.ToArray(); //todo
+    }
+
+    public RavenVector(T[] embedding)
+    {
+        AssertEmbeddingType();
+        Embedding = embedding;
+    }
+
+    public static implicit operator RavenVector<T>(T[] array)
+    {
+        return new RavenVector<T>(array);
+    }
+    
+    private static void AssertEmbeddingType()
+    {
+#if !NET7_0_OR_GREATER
+        // For >=NET7, INumber<T> is the guardian.
+        var isKnownType = typeof(T) == typeof(float) || typeof(T) == typeof(double) || typeof(T) == typeof(decimal) || typeof(T) == typeof(sbyte) || typeof(T) == typeof(byte) || typeof(T) == typeof(int) || typeof(T) == typeof(uint) || typeof(T) == typeof(long) || typeof(T) == typeof(ulong);
+        
+        if (isKnownType == false)
+            throw new InvalidDataException($"The type of embedding must be numeric. Supported types are: float, double, decimal, sbyte, byte, int, uint, long, ulong. Received: {typeof(T).FullName}.");
+#endif
+    }
+}
