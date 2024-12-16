b225ca880e970f25c0f3eea60223ef651d7dd1ea
Paweł Pekról <ppekrol@ravendb.net>
2024-12-16 09:54:19 +0100

    RavenDB-23353 allow to load any certificate in the UpdatePullReplicationAsSinkOperation, we are performing the validation ourselves

--- src/Raven.Client/Documents/Operations/Replication/UpdatePullReplicationAsSinkOperation.cs
+++ src/Raven.Client/Documents/Operations/Replication/UpdatePullReplicationAsSinkOperation.cs
@@ -36,7 +36,7 @@ namespace Raven.Client.Documents.Operations.Replication
             if (pullReplication.CertificateWithPrivateKey != null)
             {
                 var certBytes = Convert.FromBase64String(pullReplication.CertificateWithPrivateKey);
-                using (var certificate = CertificateLoaderUtil.CreateCertificateFromPfx(certBytes,
+                using (var certificate = CertificateLoaderUtil.CreateCertificateFromAny(certBytes,
                     pullReplication.CertificatePassword,
                     CertificateLoaderUtil.FlagsForExport))
                 {
