b56c32d9514c13012f4a040dfe25a738edc4ae71
Grisha Kotler <grisha@ravendb.net>
2024-12-17 10:27:56 +0200

    RavenDB-23231 - take the db name

--- test/SlowTests/Client/Attachments/AttachmentsBigFiles.cs
+++ test/SlowTests/Client/Attachments/AttachmentsBigFiles.cs
@@ -20,11 +20,12 @@ namespace SlowTests.Client.Attachments
         [InlineData(10, "i1enlqXQfLBMwWFN/CrLP3PtxxLX9DNhnKO75muxX0k=", true)]
         public async Task BatchRequestWithLongMultiPartSections(long size, string hash, bool encrypted)
         {
-            string dbName = null;
+            string dbName;
             X509Certificate2 adminCert = null;
             if (encrypted)
             {
                 var result = await Encryption.EncryptedServerAsync();
+                dbName = result.DatabaseName;
                 adminCert = result.Certificates.ServerCertificate.Value;
             }
             else
@@ -44,23 +45,21 @@ namespace SlowTests.Client.Attachments
                 var dbId1 = new Guid("00000000-48c4-421e-9466-000000000000");
                 await Databases.SetDatabaseId(store, dbId1);
 
-                using (var session = store.OpenSession())
-                using (var stream = new BigDummyStream(size))
+                using (var session = store.OpenAsyncSession())
+                await using (var stream = new BigDummyStream(size))
                 {
                     var user = new User { Name = "Fitzchak" };
-                    session.Store(user, "users/1");
-
+                    await session.StoreAsync(user, "users/1");
                     session.Advanced.Attachments.Store(user, "big-file", stream);
-
-                    session.SaveChanges();
+                    await session.SaveChangesAsync();
                 }
 
-                using (var session = store.OpenSession())
+                using (var session = store.OpenAsyncSession())
                 {
-                    var user = session.Load<User>("users/1");
+                    var user = await session.LoadAsync<User>("users/1");
 
-                    using (var bigStream = new BigDummyStream(size))
-                    using (var attachment = session.Advanced.Attachments.Get(user, "big-file"))
+                    await using (var bigStream = new BigDummyStream(size))
+                    using (var attachment = await session.Advanced.Attachments.GetAsync(user, "big-file"))
                     {
                         Assert.Contains("A:2", attachment.Details.ChangeVector);
                         Assert.Equal("big-file", attachment.Details.Name);
@@ -68,7 +67,7 @@ namespace SlowTests.Client.Attachments
                         Assert.Equal(size, attachment.Details.Size);
                         Assert.Equal("", attachment.Details.ContentType);
 
-                        attachment.Stream.CopyTo(bigStream);
+                        await attachment.Stream.CopyToAsync(bigStream);
                         Assert.Equal(size, bigStream.Position);
                     }
                 }
@@ -80,11 +79,12 @@ namespace SlowTests.Client.Attachments
         [InlineData(10, "i1enlqXQfLBMwWFN/CrLP3PtxxLX9DNhnKO75muxX0k=", true)]
         public async Task SupportHugeAttachment(long size, string hash, bool encrypted)
         {
-            string dbName = null;
+            string dbName;
             X509Certificate2 adminCert = null;
             if (encrypted)
             {
                 var result = await Encryption.EncryptedServerAsync();
+                dbName = result.DatabaseName;
                 adminCert = result.Certificates.ServerCertificate.Value;
             }
             else
@@ -104,15 +104,15 @@ namespace SlowTests.Client.Attachments
                 var dbId1 = new Guid("00000000-48c4-421e-9466-000000000000");
                 await Databases.SetDatabaseId(store, dbId1);
 
-                using (var session = store.OpenSession())
+                using (var session = store.OpenAsyncSession())
                 {
-                    session.Store(new User { Name = "Fitzchak" }, "users/1");
-                    session.SaveChanges();
+                    await session.StoreAsync(new User { Name = "Fitzchak" }, "users/1");
+                    await session.SaveChangesAsync();
                 }
 
-                using (var bigStream = new BigDummyStream(size))
+                await using (var bigStream = new BigDummyStream(size))
                 {
-                    var result = store.Operations.Send(new PutAttachmentOperation("users/1", "huge-file", bigStream));
+                    var result = await store.Operations.SendAsync(new PutAttachmentOperation("users/1", "huge-file", bigStream));
                     Assert.Contains("A:2", result.ChangeVector);
                     Assert.Equal("huge-file", result.Name);
                     Assert.Equal("users/1", result.DocumentId);
@@ -122,14 +122,14 @@ namespace SlowTests.Client.Attachments
                     Assert.Equal(size, bigStream.Position);
                 }
 
-                using (var session = store.OpenSession())
+                using (var session = store.OpenAsyncSession())
                 {
-                    var user = session.Load<User>("users/1");
+                    var user = await session.LoadAsync<User>("users/1");
 
-                    using (var bigStream = new BigDummyStream(size))
-                    using (var attachment = session.Advanced.Attachments.Get(user, "huge-file"))
+                    await using (var bigStream = new BigDummyStream(size))
+                    using (var attachment = await session.Advanced.Attachments.GetAsync(user, "huge-file"))
                     {
-                        attachment.Stream.CopyTo(bigStream);
+                        await attachment.Stream.CopyToAsync(bigStream);
                         Assert.Contains("A:2", attachment.Details.ChangeVector);
                         Assert.Equal("huge-file", attachment.Details.Name);
                         Assert.Equal(hash, attachment.Details.Hash);
