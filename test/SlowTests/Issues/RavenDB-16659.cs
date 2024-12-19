eb5a7213742085010051d06bccf73179e2feeb50
efrat@ravendb.net <efrat@ravendb.net>
2024-12-19 12:17:30 +0200

    RavenDB-22689 fix test

--- test/SlowTests/Issues/RavenDB-16659.cs
+++ test/SlowTests/Issues/RavenDB-16659.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using FastTests;
 using Raven.Client.Documents.Operations.Backups;
 using Raven.Client.Exceptions;
+using Raven.Client.ServerWide;
 using Raven.Client.ServerWide.Operations;
 using SlowTests.Core.Utils.Entities;
 using Sparrow.Server;
@@ -25,6 +25,7 @@ namespace SlowTests.Issues
         {
             DoNotReuseServer();
             var mre = new AsyncManualResetEvent();
+            var mre2 = new ManualResetEvent(false);
             var backupPath = NewDataPath();
             
             using (var store = GetDocumentStore())
@@ -52,13 +53,23 @@ namespace SlowTests.Issues
                     RestoreBackupOperation restoreOperation =
                         new RestoreBackupOperation(new RestoreBackupConfiguration
                             {BackupLocation = Path.Combine(backupPath, result.LocalBackup.BackupDirectory), DatabaseName = databaseName });
-                    Server.ServerStore.ForTestingPurposesOnly().RestoreDatabaseAfterSavingDatabaseRecord += () => mre.Set();
-                    
+                    Server.ServerStore.ForTestingPurposesOnly().RestoreDatabaseAfterSavingDatabaseRecord += () => {
+                        mre.Set();
+                        mre2.WaitOne(); // Wait to ensure the restore process doesn't finish until we intentionally cancel it.
+                    };
+
                     var op  = await store.Maintenance.Server.SendAsync(restoreOperation);
                     var res = await mre.WaitAsync(TimeSpan.FromSeconds(30));
                     Assert.True(res);
+
+                    var val = await WaitForValueAsync(async () => await store.Maintenance.Server.SendAsync(new GetDatabaseRecordOperation(databaseName)) != null, true, 30_000);
+                    Assert.True(val);
+                    var record = await store.Maintenance.Server.SendAsync(new GetDatabaseRecordOperation(databaseName));
+                    Assert.Equal(DatabaseStateStatus.RestoreInProgress, record.DatabaseState);
+
                     var e = Assert.Throws<RavenException>(() => store.Maintenance.Server.Send(new DeleteDatabasesOperation(databaseName, hardDelete: true)));
                     Assert.Contains($"Can't delete database '{databaseName}' while the restore process is in progress.", e.Message);
+                    mre2.Set();
                     await op.WaitForCompletionAsync(TimeSpan.FromSeconds(30));
                 }
                 finally
