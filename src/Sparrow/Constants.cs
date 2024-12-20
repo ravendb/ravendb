eb9a6117074260b32451a0b03bea0f74357cf2b3
Paweł Pekról <ppekrol@ravendb.net>
2024-12-20 14:29:39 +0100

    RavenDB-23420 log in UTC by default

--- src/Sparrow/Constants.cs
+++ src/Sparrow/Constants.cs
@@ -18,7 +18,7 @@
 
             internal const string DefaultHeaderAndFooterLayout = "Date|Level|ThreadID|Resource|Component|Logger|Message|Data";
 
-            internal const string DefaultLayout = "${longdate}|${level:uppercase=true}|${threadid}|${event-properties:item=Resource}|${event-properties:item=Component}|${logger}|${message:withexception=true}|${event-properties:item=Data}";
+            internal const string DefaultLayout = "${longdate:universalTime=true}|${level:uppercase=true}|${threadid}|${event-properties:item=Resource}|${event-properties:item=Component}|${logger}|${message:withexception=true}|${event-properties:item=Data}";
 
             internal class Properties
             {
