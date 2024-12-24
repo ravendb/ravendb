a4fd24d1a0edd73e53cf876e4b0d9c1aebd357ee
Grisha Kotler <grisha@ravendb.net>
2024-12-24 21:26:26 +0200

    RavenDB-23467 - update the test

--- test/FastTests/Server/DebugMemoryTests.cs
+++ test/FastTests/Server/DebugMemoryTests.cs
@@ -20,6 +20,9 @@ public class DebugMemoryTests : NoDisposalNeeded
         Assert.True(Environment.Version.Major == 8 && EventListener.Constants.EventNames.GC.GCEnd == "GCEnd_V1",
             "Check if GCEnd event was updated: https://github.com/dotnet/runtime/blob/main/src/coreclr/gc/gcevents.h");
 
+        Assert.True(Environment.Version.Major == 8 && EventListener.Constants.EventNames.GC.GCMarkWithType == "GCMarkWithType",
+            "Check if GCEnd event was updated: https://github.com/dotnet/runtime/blob/main/src/coreclr/gc/gcevents.h");
+
         Assert.True(Environment.Version.Major == 8 && EventListener.Constants.EventNames.GC.GCSuspendBegin == "GCSuspendEEBegin_V1",
             "Check if GCSuspendBegin event was updated: https://github.com/dotnet/runtime/blob/main/src/coreclr/gc/gcevents.h");
 
