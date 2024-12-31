d4435222c77cc218136f284c3ed7b62e078c6c7d
Igal Merhavia <igal@ravendb.net>
2024-12-31 19:04:33 +0200

    RavenDB-22142

---
+++ src/Raven.Server/SchemaValidation/Object/MaxPropertiesSchemaRuleValidator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Sparrow.Json;
+
+namespace Raven.Server.SchemaValidation.Object;
+
+public class MaxPropertiesSchemaRuleValidator : SchemaRuleValidator<BlittableJsonReaderObject>
+{
+    public const string RuleName = "maxProperties";
+    
+    private readonly int _maxProperties;
+
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public MaxPropertiesSchemaRuleValidator(int maxProperties)
+    {
+        _maxProperties = maxProperties;
+    }
+    protected override void ValidateInternal(BlittableJsonReaderObject value, SchemaValidatorPath path, IErrorBuilder errorBuilder)
+    {
+        if(value.Count <= _maxProperties)
+            return;
+        errorBuilder.AddError($"The object at '{path}' must have no more than {_maxProperties} properties, but it has {value.Count}.");
+    }
+}
+
+public class MaxPropertiesSchemaRuleValidatorFactory : SchemaRuleValidatorFactory
+{
+    protected override string Rule => MaxPropertiesSchemaRuleValidator.RuleName;
+
+    public override ISchemaRuleValidator Create(BlittableJsonReaderObject schemaDefinition, string schemaPath)
+    {
+        if(TryGetPropertyType(schemaDefinition, Rule, out var type) == false)
+            return null;
+
+        const BlittableJsonToken expectedType = BlittableJsonToken.Integer;
+        if (type != expectedType)
+            TrowRuleTypeError(Rule, schemaDefinition[Rule], expectedType, type, schemaPath);
+
+        if (schemaDefinition.TryGet(Rule, out int maxProperties) == false)
+            throw new InvalidOperationException($"'{Rule}' must to be convertable to decimal here. Should not happen");
+
+        return new MaxPropertiesSchemaRuleValidator(maxProperties);
+    }
+}
--- src/Raven.Server/SchemaValidation/Object/RootObjectSchemaRuleValidator.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Raven.Server.SchemaValidation.Object;
-
-//TODO To remove if not needed
-// public class RootObjectSchemaRuleValidator() : ObjectSchemaRuleValidator("root", "root", true)
-// {
-// }
