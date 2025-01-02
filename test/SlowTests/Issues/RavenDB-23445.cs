2ec52e349234ba781cf82f8c883218cffddd80d3
Michał Leśniak <michal.lesniak@ravendb.net>
2025-01-02 14:27:31 +0100

    RavenDB-23445 Added test for static index

--- test/SlowTests/Issues/RavenDB-23445.cs
+++ test/SlowTests/Issues/RavenDB-23445.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using FastTests;
 using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Indexes.Vector;
 using Tests.Infrastructure;
 using Xunit;
 using Xunit.Abstractions;
@@ -40,6 +42,67 @@ public class RavenDB_23445 : RavenTestBase
                 Assert.Equal(1, results.Count);
             }
         }
+    }
+    
+    [RavenTheory(RavenTestCategory.Corax | RavenTestCategory.Vector)]
+    [InlineData(VectorEmbeddingType.Binary, 0.65f)]
+    [InlineData(VectorEmbeddingType.Int8, 0.80f)]
+    [InlineData(VectorEmbeddingType.Single, 0.80f)]
+    public void CanCreateVectorIndexFromCSharp(VectorEmbeddingType vectorEmbeddingType, float similarity)
+    {
+        var options = Options.ForSearchEngine(RavenSearchEngineMode.Corax);
+        
+        using (var store = GetDocumentStore(options))
+        {
+            using (var session = store.OpenSession())
+            {
+                session.Store(new Document() { Text = null, Text2 = string.Empty, TextArr = ["Cat has brown eyes.", "Car has brown eyes."] });
+                session.Store(new Document() { Text2 = "Cat has brown eyes.", TextArr = ["Cat has brown eyes.", "Car has brown eyes."] });
+                
+                session.SaveChanges();
+                
+                new TextVectorIndex(vectorEmbeddingType).Execute(store);
+                
+                Indexes.WaitForIndexing(store);
+                
+                var res = session.Query<Document, TextVectorIndex>().VectorSearch(x => x.WithField(f => f.Vector), f => f.ByText("animal color"), similarity);
+                
+                var results = res.ToList();
+
+                Assert.Equal(1, results.Count);
+                Assert.Contains("Cat", results[0].Text2);
+            }
+        }
+    }
+
+    private class TextVectorIndex : AbstractIndexCreationTask<Document>
+    {
+        public TextVectorIndex()
+        {
+            //querying
+        }
+
+        public TextVectorIndex(VectorEmbeddingType vectorEmbeddingType)
+        {
+            Map = docs => from doc in docs
+                          select new { Id = doc.Id, Vector = CreateVector(new List<string> { doc.Text, doc.Text2 }) };
+            
+            VectorIndexes.Add(x => x.Vector,
+                new VectorOptions()
+                {
+                    SourceEmbeddingType = VectorEmbeddingType.Text,
+                    DestinationEmbeddingType = vectorEmbeddingType
+                });
+        }
+    }
+
+    private class Document
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Text2 { get; set; }
+        public string[] TextArr { get; set; }
+        public float[] Vector { get; set; }
     }
 
     private class Dto
