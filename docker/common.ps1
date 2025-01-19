19c1f8aa28dbfe47e13185e9d13bea6cc2314daf
Grzegorz Lachowski <gregolsky@ravendb.net>
2025-01-19 23:50:00 +0100

    RavenDB-23282 [docker] remove *latest tag from 6.2

--- docker/common.ps1
+++ docker/common.ps1
@@ -35,7 +35,6 @@ function GetUbuntuImageTags($repo, $version, $arch) {
     switch ($arch) {
         "x64" { 
             return @(
-                "$($repo):ubuntu-latest",
                 "$($repo):ubuntu-latest-lts",
                 "$($repo):6.2-ubuntu-latest",
                 "$($repo):$($version)-ubuntu.22.04-x64"
@@ -44,7 +43,6 @@ function GetUbuntuImageTags($repo, $version, $arch) {
         }
         "arm32v7" {
             return @(
-                "$($repo):ubuntu-arm32v7-latest",
                 "$($repo):ubuntu-arm32v7-latest-lts",
                 "$($repo):6.2-ubuntu-arm32v7-latest",
                 "$($repo):$($version)-ubuntu.22.04-arm32v7"
@@ -53,7 +51,6 @@ function GetUbuntuImageTags($repo, $version, $arch) {
         }
         "arm64v8" {
             return @(
-                "$($repo):ubuntu-arm64v8-latest",
                 "$($repo):ubuntu-arm64v8-latest-lts",
                 "$($repo):6.2-ubuntu-arm64v8-latest",
                 "$($repo):$($version)-ubuntu.22.04-arm64v8"
@@ -71,7 +68,6 @@ function GetWindowsImageTags($repo, $version, $WinVer) {
     switch ($winver) {
         "1809" {
             return @(
-                "$($repo):windows-1809-latest",
                 "$($repo):$($version)-windows-1809",
                 "$($repo):windows-1809-latest-lts",
                 "$($repo):6.2-windows-1809-latest"
@@ -80,7 +76,6 @@ function GetWindowsImageTags($repo, $version, $WinVer) {
         }
         "ltsc2022" {
              return @(
-                "$($repo):windows-ltsc2022-latest",
                 "$($repo):$($version)-windows-ltsc2022",
                 "$($repo):windows-ltsc2022-latest-lts",
                 "$($repo):6.2-windows-ltsc2022-latest"
@@ -100,7 +95,6 @@ function GetManifestTags {
     )
 
     return @(
-        "${repo}:latest",
         "${repo}:latest-lts",
         "${repo}:6.2-latest"
     )
