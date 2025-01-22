91439c45aa508cb8e05c731f414f5b9a274376d9
Damian Olszewski <damian@ravendb.net>
2025-01-22 08:59:35 +0100

    RavenDB-22513 Add isClearedAfterSelect to SelectCreatable

--- src/Raven.Studio/typescript/components/common/select/SelectCreatable.tsx
+++ src/Raven.Studio/typescript/components/common/select/SelectCreatable.tsx
@@ -11,13 +11,20 @@ interface SelectCreatableProps<
     Group extends GroupBase<Option> = GroupBase<Option>,
 > extends ComponentProps<typeof ReactSelectCreatable<Option, IsMulti, Group>> {
     isRoundedPill?: boolean;
+    isClearedAfterSelect?: boolean;
 }
 
 export default function SelectCreatable<
     Option,
     IsMulti extends boolean = false,
     Group extends GroupBase<Option> = GroupBase<Option>,
->({ isRoundedPill, className, styles = {}, ...rest }: SelectCreatableProps<Option, IsMulti, Group>) {
+>({
+    isRoundedPill,
+    isClearedAfterSelect,
+    className,
+    styles = {},
+    ...rest
+}: SelectCreatableProps<Option, IsMulti, Group>) {
     if (isRoundedPill) {
         applyRoundedPillStyle(styles);
     }
@@ -25,10 +32,11 @@ export default function SelectCreatable<
     return (
         <ReactSelectCreatable
             styles={styles}
-            formatCreateLabel={(value) => value}
+            formatCreateLabel={(value) => value ?? ""}
             {...rest}
             classNamePrefix="react-select"
             className={classNames("bs5 react-select-container", { "rounded-pill": isRoundedPill }, className)}
+            value={isClearedAfterSelect ? null : rest.value}
         />
     );
 }
