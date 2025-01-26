16fd3510c7ae3b48ebb1196003cc257f2c3d90b8
Pawel Pekrol <ppekrol@ravendb.net>
2025-01-26 19:09:15 +0100

    RavenDB-23556 more renames

--- src/Raven.Server/Documents/ETL/Providers/AI/Extensions/AiExtensions.cs
+++ src/Raven.Server/Documents/ETL/Providers/AI/Extensions/AiExtensions.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.Onnx;
+using Microsoft.SemanticKernel.Embeddings;
 using Raven.Client.Documents.Operations.ETL.AI;
+using Raven.Server.Documents.Indexes.VectorSearch;
+
 #pragma warning disable SKEXP0070
 
-namespace Raven.Server.Documents.ETL.Providers.AI;
+namespace Raven.Server.Documents.ETL.Providers.AI.Extensions;
 
 public static class AiExtensions
 {
@@ -36,5 +42,18 @@ public static class AiExtensions
             default:
                 throw new ArgumentOutOfRangeException(nameof(poolingMode), poolingMode, null);
         }
+    }
+
+    [Experimental("SKEXP0070")]
+    public static IKernelBuilder AddCustomBertOnnxTextEmbeddingGeneration(
+        this IKernelBuilder builder,
+        BertOnnxOptions options = null,
+        string serviceId = null)
+    {
+        builder.Services.AddKeyedSingleton<ITextEmbeddingGenerationService>(
+            serviceId,
+            GenerateEmbeddings.CreateTextEmbeddingGenerationService(options));
+
+        return builder;
     }
 }
