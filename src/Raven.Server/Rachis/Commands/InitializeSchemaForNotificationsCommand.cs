884143a416fff640e89fa2faf29a14833f895aa3
Grisha Kotler <grisha@ravendb.net>
2025-02-11 10:49:31 +0200

    RavenDB-23534 - skip creating a a notification a tree if it already exists

---
+++ src/Raven.Server/Rachis/Commands/InitializeSchemaForNotificationsCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Raven.Server.Documents.TransactionMerger.Commands;
+using Raven.Server.ServerWide.Context;
+
+namespace Raven.Server.Rachis.Commands;
+
+public class InitializeSchemaForNotificationsCommand : MergedTransactionCommand<ClusterOperationContext, ClusterTransaction>
+{
+    private readonly string _tableName;
+
+    public InitializeSchemaForNotificationsCommand(string tableName)
+    {
+        _tableName = tableName;
+    }
+
+    protected override long ExecuteCmd(ClusterOperationContext context)
+    {
+        Documents.Schemas.Notifications.Current.Create(context.Transaction.InnerTransaction, _tableName, 16);
+        return 1;
+    }
+
+    public override IReplayableCommandDto<ClusterOperationContext, ClusterTransaction, MergedTransactionCommand<ClusterOperationContext, ClusterTransaction>> ToDto(ClusterOperationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
