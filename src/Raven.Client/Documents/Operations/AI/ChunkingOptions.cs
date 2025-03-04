77983f955d8e9aeea7e53acbae4eadf28e907f9e
Maciej Aszyk <maciej@ravendb.net>
2025-03-04 09:40:38 +0100

    RavenDB-23556 Made `EmbeddingPathConfiguration` convertible to `DynamicJsonValue`.

--- src/Raven.Client/Documents/Operations/AI/ChunkingOptions.cs
+++ src/Raven.Client/Documents/Operations/AI/ChunkingOptions.cs
@@ -1,9 +1,21 @@
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+
 namespace Raven.Client.Documents.Operations.AI;
 
-public class ChunkingOptions
+public class ChunkingOptions : IDynamicJsonValueConvertible
 {
     public ChunkingMethod ChunkingMethod { get; set; }
+    
     public int MaxTokensPerChunk { get; set; }
+    
+    public DynamicJsonValue ToJson()
+    {
+        var djv = new DynamicJsonValue();
+        djv[nameof(ChunkingMethod)] = ChunkingMethod;
+        djv[nameof(MaxTokensPerChunk)] = MaxTokensPerChunk;
+        return djv;
+    }
 }
 
 public enum ChunkingMethod
--- src/Raven.Client/Documents/Operations/AI/EmbeddingPathConfiguration.cs
+++ src/Raven.Client/Documents/Operations/AI/EmbeddingPathConfiguration.cs
@@ -1,7 +1,19 @@
+using Sparrow.Json;
+using Sparrow.Json.Parsing;
+
 namespace Raven.Client.Documents.Operations.AI;
 
-public class EmbeddingPathConfiguration
+public class EmbeddingPathConfiguration : IDynamicJsonValueConvertible
 {
     public string Path { get; set; }
+    
     public ChunkingOptions ChunkingOptions { get; set; }
+    
+    public DynamicJsonValue ToJson()
+    {
+        var jsv = new DynamicJsonValue();
+        jsv[nameof(Path)] = Path;
+        jsv[nameof(ChunkingOptions)] = ChunkingOptions?.ToJson();
+        return jsv;
+    }
 }
