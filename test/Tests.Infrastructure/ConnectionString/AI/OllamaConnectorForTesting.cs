16acd3e21436642cf697a11b9e165809e5b14473
Lev Skuditsky <lev.skuditsky@ravendb.net>
2025-03-12 13:55:05 +0200

    RavenDB-23556: Change model for OllamaConnectorForTesting

--- test/Tests.Infrastructure/ConnectionString/AI/OllamaConnectorForTesting.cs
+++ test/Tests.Infrastructure/ConnectionString/AI/OllamaConnectorForTesting.cs
@@ -6,7 +6,7 @@ namespace Tests.Infrastructure.ConnectionString.AI;
 public class OllamaConnectorForTesting : BaseAiConnectorForTesting<OllamaConnectorForTesting>
 {
     private const string EnvironmentVariable = "RAVEN_AI_INTEGRATION_OLLAMA_URI";
-    private const string Model = "mistral-nemo:latest";
+    private const string Model = "phi:latest";
 
     public override Lazy<AiConnectorType> AiConnectorType { get; init; } = new(Raven.Client.Documents.Operations.AI.AiConnectorType.Ollama);
 
