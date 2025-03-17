2c71fd2e5530a4d0db25801b480af622aa62a4b8
shiranshalom <shiran@ravendb.net>
2025-03-17 11:21:04 +0200

    RavenDB-22794 - SeedIdentityForOperation (in Raven.Client.Documents.Operations.Identities)

--- src/Raven.Client/Documents/Operations/Identities/SeedIdentityForOperation.cs
+++ src/Raven.Client/Documents/Operations/Identities/SeedIdentityForOperation.cs
@@ -6,12 +6,22 @@ using Sparrow.Json;
 
 namespace Raven.Client.Documents.Operations.Identities
 {
+    /// <summary>
+    /// Operation to seed an identity value for a specified identity name in the database.
+    /// </summary>
     public sealed class SeedIdentityForOperation : IMaintenanceOperation<long>
     {
         private readonly string _identityName;
         private readonly long _identityValue;
         private readonly bool _forceUpdate;
 
+        /// <inheritdoc cref="SeedIdentityForOperation"/>
+        /// <param name="name">The name of the identity to seed.</param>
+        /// <param name="value">The value to set for the identity.</param>
+        /// <param name="forceUpdate">
+        /// If <c>true</c>, forces an update even if the current identity value is greater than the provided value.
+        /// </param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="name"/> is null or whitespace.</exception>
         public SeedIdentityForOperation(string name, long value, bool forceUpdate = false)
         {
             if (string.IsNullOrWhiteSpace(name))
