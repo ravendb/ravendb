b8edb69a7d69e19fb304e193fe424ee10ada7450
shiranshalom <shiran@ravendb.net>
2025-03-17 11:49:09 +0200

    RavenDB-22794 - DelayBackupOperation (in Raven.Client.Documents.Operations.Backups)

--- src/Raven.Client/Documents/Operations/Backups/DelayBackupOperation.cs
+++ src/Raven.Client/Documents/Operations/Backups/DelayBackupOperation.cs
@@ -6,11 +6,17 @@ using Sparrow.Json;
 
 namespace Raven.Client.Documents.Operations.Backups;
 
+/// <summary>
+/// Operation to delay the execution of a running backup task in the database.
+/// </summary>
 public sealed class DelayBackupOperation : IMaintenanceOperation<OperationState>
 {
     private readonly long _runningBackupTaskId;
     private readonly TimeSpan _duration;
 
+    /// <inheritdoc cref="DelayBackupOperation"/>
+    /// <param name="runningBackupTaskId">The identifier of the running backup task to delay.</param>
+    /// <param name="duration">The duration for which the backup task should be delayed.</param>
     public DelayBackupOperation(long runningBackupTaskId, TimeSpan duration)
     {
         _runningBackupTaskId = runningBackupTaskId;
