0c718b312b7224c675470d3dec819d697865d41d
Maksym Smoliński <maksym.smolinski@ravendb.net>
2025-03-25 15:50:08 +0100

    RavenDB-23934 fix(RunScriptButton): spread props to Button for improved flexibility, fixed AdminJS console btn

--- src/Raven.Studio/typescript/components/common/RunScriptButton.tsx
+++ src/Raven.Studio/typescript/components/common/RunScriptButton.tsx
@@ -8,17 +8,11 @@ interface RunScriptButtonProps extends Button.BtnProps {
 }
 
 export default function RunScriptButton(props: RunScriptButtonProps) {
-    const { onClick, isSpinning, disabled } = props;
+    const { isSpinning, disabled, ...rest } = props;
 
     return (
         <div className="run-script-button">
-            <Button
-                variant="primary"
-                size="lg"
-                className="px-4 py-2"
-                onClick={onClick}
-                disabled={disabled || isSpinning}
-            >
+            <Button variant="primary" size="lg" className="px-4 py-2" disabled={disabled || isSpinning} {...rest}>
                 {isSpinning ? <Spinner /> : <Icon icon="play" className="fs-1 d-inline-block" margin="mb-2" />}
                 <div className="kbd">
                     <kbd>ctrl</kbd> <strong>+</strong> <kbd>enter</kbd>
