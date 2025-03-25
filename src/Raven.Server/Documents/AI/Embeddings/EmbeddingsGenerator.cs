d60a920283b80993c657803813a0550e2c93285e
Arkadiusz Palinski <arek@ravendb.net>
2025-03-25 14:19:42 +0100

    Revert "RavenDB-23556 Fixing polarity issue. If a task isn't completed we want to add it to the tasks list, not await it immediately (also ValueTask.Result on a task which is not completed should never be performed)."
    This reverts commit c31573a718a212bed46b8319daa64354a6aa228d.

--- src/Raven.Server/Documents/AI/Embeddings/EmbeddingsGenerator.cs
+++ src/Raven.Server/Documents/AI/Embeddings/EmbeddingsGenerator.cs
@@ -761,7 +761,7 @@ public class EmbeddingsGenerator(DocumentDatabase database, RavenLogger logger, 
             for (int i = 0; i < multi.Length; i++)
             {
                 var valueTask = worker.GetEmbeddingsForQueryAsync(documentsContext, multi[i]);
-                if (valueTask.IsCompleted == false)
+                if (valueTask.IsCompleted)
                 {
                     results.AddRange(valueTask.Result.Span);
                 }
