0fac9bf5a376ea2efa5c7d5bf421a6a3d4189a3e
Arkadiusz Palinski <arek@ravendb.net>
2025-03-31 15:12:06 +0200

    RavenDB-22767 Making sure that a transaction is removed from ActiveTransactions if an exception is thrown after already adding it there

---
+++ test/SlowTests/Voron/Issues/RavenDB_22767.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using FastTests.Voron;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Voron.Issues;
+
+public class RavenDB_22767 : StorageTest
+{
+    public RavenDB_22767(ITestOutputHelper output) : base(output)
+    {
+    }
+    
+    [RavenFact(RavenTestCategory.Voron)]
+    public void MustRemoveFailedTransactionFromActiveTransactions()
+    {
+        Env.NewTransactionCreated += (tx) =>
+        {
+            throw new InvalidDataException();
+        };
+
+        Assert.Throws<InvalidDataException>(() =>
+        {
+            using (Env.ReadTransaction())
+            {
+
+            }
+        });
+
+        var oldestTxId = Env.ActiveTransactions.OldestTransaction;
+            
+        Assert.Equal(0, oldestTxId);
+    }
+}
