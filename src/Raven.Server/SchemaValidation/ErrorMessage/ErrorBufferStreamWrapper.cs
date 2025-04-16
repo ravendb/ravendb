cbbd2b7bd0352d767c890b35a7dc195cd71fb476
Igal Merhavia <igal@ravendb.net>
2025-04-16 15:14:33 +0300

    RavenDB-22142 Fix exceptions type in ErrorBufferStreamWrapper

--- src/Raven.Server/SchemaValidation/ErrorMessage/ErrorBufferStreamWrapper.cs
+++ src/Raven.Server/SchemaValidation/ErrorMessage/ErrorBufferStreamWrapper.cs
@@ -12,19 +12,19 @@ public class ErrorBufferStreamWrapper : Stream
         _errorBuffer = errorBuffer;
     }
 
-    public override void Flush() => throw new NotImplementedException();
+    public override void Flush() => throw new NotSupportedException();
 
-    public override int Read(byte[] buffer, int offset, int count)  => throw new NotImplementedException();
+    public override int Read(byte[] buffer, int offset, int count)  => throw new NotSupportedException();
 
-    public override long Seek(long offset, SeekOrigin origin)  => throw new NotImplementedException();
+    public override long Seek(long offset, SeekOrigin origin)  => throw new NotSupportedException();
 
-    public override void SetLength(long value) => throw new NotImplementedException();
+    public override void SetLength(long value) => throw new NotSupportedException();
 
     public override void Write(byte[] buffer, int offset, int count) => _errorBuffer.AppendUtf8(buffer.AsSpan(offset, count));
 
     public override bool CanRead => false;
     public override bool CanSeek => false;
     public override bool CanWrite => true;
-    public override long Length => throw new NotImplementedException();
-    public override long Position { get  => throw new NotImplementedException(); set  => throw new NotImplementedException(); }
+    public override long Length => throw new NotSupportedException();
+    public override long Position { get  => throw new NotSupportedException(); set  => throw new NotSupportedException(); }
 }
