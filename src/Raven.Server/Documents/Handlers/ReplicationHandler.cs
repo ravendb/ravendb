3ccaeea35ce0eb78fd4c446bbed5c7da7a4d697a
Igal Merhavia <igal@ravendb.net>
2025-04-22 17:43:15 +0300

    Desk-3872 Add debug functionality for issue investigation

--- src/Raven.Server/Documents/Handlers/ReplicationHandler.cs
+++ src/Raven.Server/Documents/Handlers/ReplicationHandler.cs
@@ -1,6 +1,15 @@
-﻿using System.Linq;
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Raven.Client;
+using Raven.Client.Util;
 using Raven.Server.Documents.Handlers.Processors.Replication;
+using Raven.Server.Documents.Replication.ReplicationItems;
 using Raven.Server.Documents.Replication.Senders;
 using Raven.Server.Documents.Replication.Stats;
 using Raven.Server.Routing;
@@ -124,6 +133,88 @@ namespace Raven.Server.Documents.Handlers
                 {
                     ["Results"] = new DynamicJsonArray(items.Select(x => x.ToDebugJson()))
                 });
+            }
+        }
+
+        private class DebugDocumentItem
+        {
+            public long Etag { get; set; }
+            public string Id { get; set; }
+            public int DocSize { get; set; }
+            public string Collection { get; set; }
+            public ReplicationBatchItem.ReplicationItemType Type { get; set; }
+            public DocumentFlags Flags { get; set; }
+            public long ReplicationItemSize { get; set; }
+        }
+        
+        [RavenAction("/databases/*/debug/replication/document-items", "GET", AuthorizationStatus.ValidUser, EndpointType.Read)]
+        public async Task GetDocumentItemsDebugInfo()
+        {
+            var etag = GetLongQueryString("etag", required: false) ?? 0L;
+            var page = GetIntValueQueryString("page", required: false) ?? 0;
+            var pageSize = GetPageSize();
+
+            using (ContextPool.AllocateOperationContext(out DocumentsOperationContext context))
+            using (context.OpenReadTransaction())
+            {
+                var runStats = new OutgoingReplicationRunStats();
+                var stats = new ReplicationDocumentSenderBase.ReplicationStats
+                {
+                    Network = new OutgoingReplicationStatsScope(runStats),
+                    Storage = new OutgoingReplicationStatsScope(runStats),
+                    AttachmentRead = new OutgoingReplicationStatsScope(runStats),
+                    CounterRead = new OutgoingReplicationStatsScope(runStats),
+                    DocumentRead = new OutgoingReplicationStatsScope(runStats),
+                    TombstoneRead = new OutgoingReplicationStatsScope(runStats),
+                    TimeSeriesRead = new OutgoingReplicationStatsScope(runStats),
+                };
+
+                var supportedFeatures = new ReplicationDocumentSenderBase.ReplicationSupportedFeatures
+                {
+                    CaseInsensitiveCounters = true,
+                    RevisionTombstonesWithId = true
+                };
+
+                var items = ReplicationDocumentSenderBase.GetReplicationItems(Database, context, etag, stats, supportedFeatures)
+                    .Select(x =>
+                    {
+                        if (x is DocumentReplicationItem documentItem)
+                        {
+                            return new DebugDocumentItem
+                            {
+                                Id = documentItem.Id,
+                                DocSize = documentItem.Data?.Size ?? 0,
+                                Etag = documentItem.Etag,
+                                Collection = documentItem.Collection,
+                                Type = documentItem.Type,
+                                ReplicationItemSize = documentItem.Size,
+                                Flags = documentItem.Flags,
+                            };
+                        }
+
+                        return null;
+                    })
+                    .Where(x => x != null)
+                    .Skip(page * pageSize)
+                    .Take(pageSize);
+                
+                var encodedCsvFileName = Uri.EscapeDataString($"output{page}_{SystemTime.UtcNow.ToString("yyyyMMdd_HHmm", CultureInfo.InvariantCulture)}.csv");
+                HttpContext.Response.Headers[Constants.Headers.ContentDisposition] = $"attachment; filename=\"{encodedCsvFileName}\"";
+                HttpContext.Response.Headers[Constants.Headers.ContentType] = "text/csv";
+
+                await using var writer = new StreamWriter(ResponseBodyStream(), Encoding.UTF8);
+                await using var csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture){
+                    Delimiter = ","
+                });
+                
+                csvWriter.WriteComment($"page:{page} pageSize:{pageSize} etag:{etag}");
+                csvWriter.NextRecord();
+                csvWriter.WriteHeader<DebugDocumentItem>();
+                foreach (var item in items)
+                {
+                    csvWriter.NextRecord();
+                    csvWriter.WriteRecord(item);
+                }
             }
         }
     }
