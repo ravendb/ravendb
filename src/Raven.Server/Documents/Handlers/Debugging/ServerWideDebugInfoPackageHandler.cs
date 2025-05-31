45d72b24860c86ca3f39e90f698e5ea0691d7ef7
Grisha Kotler <grisha@ravendb.net>
2025-05-31 12:23:43 +0300

    RavenDB-24254 - fix test

--- src/Raven.Server/Documents/Handlers/Debugging/ServerWideDebugInfoPackageHandler.cs
+++ src/Raven.Server/Documents/Handlers/Debugging/ServerWideDebugInfoPackageHandler.cs
@@ -64,6 +64,7 @@ namespace Raven.Server.Documents.Handlers.Debugging
             nameof(DatabaseRecord.Analyzers),
             nameof(DatabaseRecord.TimeSeries),
             nameof(DatabaseRecord.SupportedFeatures),
+            nameof(DatabaseRecord.SchemaValidation),
 
             nameof(DatabaseRecord.Sharding),
             nameof(DatabaseRecord.RevisionsBin),
