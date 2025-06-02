10bb46230526b38a251a2fcd8ae43cb21b94dc33
Maciej Aszyk <maciej@ravendb.net>
2025-06-02 12:49:24 +0200

    RavenDB-24317 Test for edge condition: splitting compact key lookup tree.

---
+++ test/SlowTests/Voron/CompactTrees/RavenDB_24317.cs
@@ -0,0 +1,42 @@
+﻿using FastTests.Voron;
+using Tests.Infrastructure;
+using Voron.Data.CompactTrees;
+using Voron.Impl;
+using Xunit.Abstractions;
+
+namespace SlowTests.Voron.CompactTrees;
+
+public class RavenDB_24317(ITestOutputHelper output) : StorageTest(output)
+{
+    [RavenFact(RavenTestCategory.Voron)]
+    public void CanSplitLastLeafWhenTermExistsOnlyAsLeafKey()
+    {
+        const string rootTreeName = nameof(RavenDB_24317);
+        const string compactTreeName = nameof(CanSplitLastLeafWhenTermExistsOnlyAsLeafKey);
+
+
+        using (var wTx = GetWriteTransaction(out var compactTree))
+        {
+            for (int i = 0; i < 1_000_000; ++i)
+            {
+                compactTree.Add(GetKey(i), i);
+                if (i == 965_643)
+                {
+                    compactTree.TryRemove(GetKey(964_585), out _);
+                }
+            }
+            
+            wTx.Commit();
+        }
+        
+        Transaction GetWriteTransaction(out CompactTree compactTree)
+        {
+            var wTx = Env.WriteTransaction();
+            var rootTree = wTx.CreateTree(rootTreeName);
+            compactTree = rootTree.CompactTreeFor(compactTreeName);
+            return wTx;
+        }
+        
+        string GetKey(int keyAsInt) => keyAsInt.ToString().PadLeft(totalWidth: 64, '0');
+    }
+}
