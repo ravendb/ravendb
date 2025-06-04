85ec48d08861d66318c9f493dc88f4b70c970702
Michał Leśniak <michal.lesniak@ravendb.net>
2025-06-04 14:03:55 +0200

    RavenDB-24349 Fix dynamic returns detection for JS map syntax

---
+++ test/SlowTests/Issues/RavenDB-24349.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FastTests;
+using Raven.Client.Documents.Indexes;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues;
+
+public class RavenDB_24349 : RavenTestBase
+{
+    public RavenDB_24349(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [RavenFact(RavenTestCategory.Indexes)]
+    public void DynamicFieldsShouldBeMarked()
+    {
+        using (var store = GetDocumentStore())
+        {
+            using (var session = store.OpenSession())
+            {
+                session.Store(new Dto { Name = "Name1", Type = "Type1" });
+                session.Store(new Dto { Name = "Name2", Type = "Type2" });
+                
+                session.SaveChanges();
+            }
+
+            var index = new DummyIndex();
+            index.Execute(store);
+            Indexes.WaitForIndexing(store);
+
+            using (var session = store.OpenSession())
+            {
+                var results = session.Query<Dto, DummyIndex>().Where(x => x.Name == "Name1").ToList();
+                
+                Assert.Equal(1, results.Count);
+            }
+        }
+    }
+
+    private class Dto
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+
+    private class DummyIndex : AbstractJavaScriptIndexCreationTask
+    {
+        public DummyIndex()
+        {
+            Maps = new HashSet<string>() { "map('Dtos', (dto) => mapDto(dto))" };
+            AdditionalSources = new Dictionary<string, string>() { { "Helper", 
+                """
+                function mapDto(dto) {
+                  return {
+                    Name: dto.Name, 
+                    Type: dto.Type
+                  }
+                }
+                """ } };
+        }
+    }
+}
