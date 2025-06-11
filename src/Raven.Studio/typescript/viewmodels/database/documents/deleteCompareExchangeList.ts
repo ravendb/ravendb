11b9651ece816e70c66db24857ba208d9b14aa31
Damian Olszewski <damian@ravendb.net>
2025-06-11 12:28:42 +0200

    RavenDB-24090 Prettier

--- src/Raven.Studio/typescript/viewmodels/database/documents/deleteCompareExchangeList.ts
+++ src/Raven.Studio/typescript/viewmodels/database/documents/deleteCompareExchangeList.ts
@@ -1,25 +1,30 @@
-
 import database = require("models/resources/database");
 import deleteCompareExchangeItemCommand = require("commands/database/cmpXchg/deleteCompareExchangeItemCommand");
 
-type itemTypeDto = { 
-    Key: string, Index: number
+type itemTypeDto = {
+    Key: string;
+    Index: number;
 };
 
 class deleteCompareExchangeList {
-    constructor(private items: Array<itemTypeDto>,private  db: database) {
+    constructor(
+        private items: Array<itemTypeDto>,
+        private db: database
+    ) {
         this.db = db;
         this.items = items;
     }
-    
+
     start(): Promise<Raven.Client.Documents.Operations.CompareExchange.CompareExchangeResult<any>[]> {
         if (this.items.length === 0) {
             return;
         }
-        
-        return Promise.all(this.items.map(item => {
-            return new deleteCompareExchangeItemCommand(this.db, item.Key, item.Index).execute();
-        }));
+
+        return Promise.all(
+            this.items.map((item) => {
+                return new deleteCompareExchangeItemCommand(this.db, item.Key, item.Index).execute();
+            })
+        );
     }
 }
 
