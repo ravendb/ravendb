216b146b10ed6595eb60afd52e1cddd93f053ff6
Grisha Kotler <grisha@ravendb.net>
2025-06-24 00:20:48 +0300

    RavenDB-24254 - don't throw an exception if we failed to parse the schema

--- src/Raven.Server/Documents/DocumentDatabase.cs
+++ src/Raven.Server/Documents/DocumentDatabase.cs
@@ -449,7 +449,7 @@ namespace Raven.Server.Documents
 
                 ReplicationLoader = CreateReplicationLoader();
                 PeriodicBackupRunner = new PeriodicBackupRunner(this, _serverStore, wakeup);
-                SchemaValidatorCache = new SchemaValidatorCache(DocumentsStorage.ContextPool);
+                SchemaValidatorCache = new SchemaValidatorCache(DocumentsStorage.ContextPool, Loggers.GetLogger<SchemaValidatorCache>());
 
                 _addToInitLog(LogLevel.Debug, "Initializing IndexStore (async)");
                 _indexStoreTask = IndexStore.InitializeAsync(record, index, _addToInitLog);
