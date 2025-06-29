6d9ae43033ef4ccc0108100dc8443eca966ad7e2
Grisha Kotler <grisha@ravendb.net>
2025-06-29 13:16:00 +0300

    RavenDB-24530 - separate the handling of LoadBalanceBehavior and ReadBalanceBehavior so they can be set independently

--- src/Raven.Studio/typescript/models/database/settings/clientConfigurationModel.ts
+++ src/Raven.Studio/typescript/models/database/settings/clientConfigurationModel.ts
@@ -39,13 +39,19 @@ class clientConfigurationModel {
             if (dto.LoadBalanceBehavior === "UseSessionContext") {
                 this.isDefined.push("useSessionContextForLoadBehavior");
                 this.useSessionContextForLoadBehavior("UseSessionContext");
-                
+
                 if (dto.LoadBalancerContextSeed || dto.LoadBalancerContextSeed === 0) {
                     this.isDefined.push("loadBalanceContextSeed");
                     this.loadBalanceContextSeed(dto.LoadBalancerContextSeed);
                     this.setLoadBalanceSeed(true);
                 }
-            } else if (dto.ReadBalanceBehavior != null) {
+            }
+            else if (dto.LoadBalanceBehavior === "None") {
+                this.isDefined.push("useSessionContextForLoadBehavior");
+                this.useSessionContextForLoadBehavior("None");
+            }
+
+            if (dto.ReadBalanceBehavior != null) {
                 this.isDefined.push("readBalanceBehavior");
                 this.readBalanceBehavior(dto.ReadBalanceBehavior);
             }
