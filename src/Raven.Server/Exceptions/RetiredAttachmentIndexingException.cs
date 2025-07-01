dad22056d69dfa3fdf7ab7992cee4f58ab8fc813
Golan Nahum <golan.nahum@ravendb.net>
2025-07-01 09:34:23 +0300

    RavenDB-24488 : indexing: prevent raw access to retired attachments

---
+++ src/Raven.Server/Exceptions/RetiredAttachmentIndexingException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Raven.Server.Exceptions
+{
+    public sealed class RetiredAttachmentIndexingException : CriticalIndexingException
+    {
+        public RetiredAttachmentIndexingException(string message)
+            : base(message, e: null)
+        {
+        }
+    }
+}
