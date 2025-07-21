abb634a33e1ab949888ba181a48cae5fedbdc961
Arkadiusz Palinski <arek@ravendb.net>
2025-07-21 13:58:18 +0200

    Revert "RavenDB-24517 - Keep track of journal writes and writes to the data file"
    This reverts commit f6f713278cfb025cc6c11af7ccd5c2ccd8f49c97.

--- src/Voron/Impl/Journal/JournalFile.cs
+++ src/Voron/Impl/Journal/JournalFile.cs
@@ -14,7 +14,6 @@ using Sparrow.Logging;
 using System.Threading;
 using Sparrow.Collections;
 using Sparrow.Server;
-using Voron.Impl.Paging;
 using Voron.Util;
 using Constants = Voron.Global.Constants;
 
@@ -140,7 +139,6 @@ namespace Voron.Impl.Journal
         /// </summary>
         public void Write(long posBy4Kb, byte* p, int numberOf4Kbs)
         {
-            PagingStatistics.MarkWrite(numberOf4Kbs / 2 + numberOf4Kbs % 2);
             int posBy4Kbs = 0;
             while (posBy4Kbs < numberOf4Kbs)
             {
