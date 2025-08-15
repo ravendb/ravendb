950416815337573f8e9450ed0dcf5b8093044455
Igal Merhavia <igal@ravendb.net>
2025-08-15 13:05:41 +0300

    RavenDB-22142 Fix tests

--- src/Raven.Server/Documents/Sharding/Handlers/Processors/SchemaValidation/ShardedSchemaValidationHandlerProcessorForPost.cs
+++ src/Raven.Server/Documents/Sharding/Handlers/Processors/SchemaValidation/ShardedSchemaValidationHandlerProcessorForPost.cs
@@ -1,10 +1,11 @@
 ﻿using JetBrains.Annotations;
 using Raven.Server.Documents.Handlers.Processors.DocumentsCompression;
+using Raven.Server.Documents.Handlers.Processors.SchemaValidation;
 using Raven.Server.ServerWide.Context;
 
 namespace Raven.Server.Documents.Sharding.Handlers.Processors.SchemaValidation
 {
-    internal sealed class ShardedSchemaValidationHandlerProcessorForPost : AbstractDocumentsCompressionHandlerProcessorForPost<ShardedDatabaseRequestHandler, TransactionOperationContext>
+    internal sealed class ShardedSchemaValidationHandlerProcessorForPost : AbstractSchemaValidationHandlerProcessorForPost<ShardedDatabaseRequestHandler, TransactionOperationContext>
     {
         public ShardedSchemaValidationHandlerProcessorForPost([NotNull] ShardedDatabaseRequestHandler requestHandler) : base(requestHandler)
         {
