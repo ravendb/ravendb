4fe6c1cd2f0c7ac9590236f98f11a11412d25837
egor <egor@ravendb.net>
2025-08-25 13:46:35 +0300

    RavenDB-23594 - Add XML documentation for public methods in AsyncDocumentSession

--- src/Raven.Client/Documents/Session/AsyncDocumentSession.RawQuery.cs
+++ src/Raven.Client/Documents/Session/AsyncDocumentSession.RawQuery.cs
@@ -2,6 +2,12 @@
 {
     public partial class AsyncDocumentSession
     {
+        /// <summary>
+        /// Query the specified index using provided raw query
+        /// </summary>
+        /// <typeparam name="T">The query result type</typeparam>
+        /// <param name="query">The raw query string</param>
+        /// <returns>An async raw document query instance</returns>
         public IAsyncRawDocumentQuery<T> AsyncRawQuery<T>(string query)
         {
             var asyncDocumentQuery = new AsyncDocumentQuery<T>(this,null,null,false);
