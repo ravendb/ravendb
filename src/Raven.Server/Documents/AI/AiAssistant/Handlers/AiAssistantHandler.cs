225f861b788c3c22d92bd3822ea35116738d9a9f
Michał Leśniak <michal.lesniak@ravendb.net>
2025-09-01 12:15:48 +0200

    RavenDB-24565 Changed AiAssistantHandler to server-wide

--- src/Raven.Server/Documents/AI/AiAssistant/Handlers/AiAssistantHandler.cs
+++ src/Raven.Server/Documents/AI/AiAssistant/Handlers/AiAssistantHandler.cs
@@ -1,33 +1,34 @@
 ﻿using System.Threading.Tasks;
 using Raven.Server.Documents.AI.AiAssistant.Handlers.Processors;
 using Raven.Server.Routing;
+using Raven.Server.Web;
 
 namespace Raven.Server.Documents.AI.AiAssistant.Handlers;
 
-public class AiAssistantHandler : DatabaseRequestHandler
+public class AiAssistantHandler : ServerRequestHandler
 {
-    [RavenAction("/databases/*/assistant/assist", "POST", AuthorizationStatus.ValidUser, EndpointType.Read)]
+    [RavenAction("/assistant/assist", "POST", AuthorizationStatus.ValidUser, EndpointType.Read)]
     public async Task Assist()
     {
         using (var processor = new AiAssistantAssistProcessor(this))
             await processor.ExecuteAsync();
     }
     
-    [RavenAction("/databases/*/assistant/give-consent", "POST", AuthorizationStatus.ValidUser, EndpointType.Read)]
+    [RavenAction("/assistant/give-consent", "POST", AuthorizationStatus.ValidUser, EndpointType.Read)]
     public async Task GiveConsent()
     {
         using (var processor = new AiAssistantGiveConsentProcessor(this))
             await processor.ExecuteAsync();
     }
     
-    [RavenAction("/databases/*/assistant/check-consent", "GET", AuthorizationStatus.ValidUser, EndpointType.Read)]
+    [RavenAction("/assistant/check-consent", "GET", AuthorizationStatus.ValidUser, EndpointType.Read)]
     public async Task CheckConsent()
     {
         using (var processor = new AiAssistantCheckConsentProcessor(this))
             await processor.ExecuteAsync();
     }
     
-    [RavenAction("/databases/*/assistant/check-usage", "GET", AuthorizationStatus.ValidUser, EndpointType.Read)]
+    [RavenAction("/assistant/check-usage", "GET", AuthorizationStatus.ValidUser, EndpointType.Read)]
     public async Task CheckUsage()
     {
         using (var processor = new AiAssistantCheckUsageProcessor(this))
