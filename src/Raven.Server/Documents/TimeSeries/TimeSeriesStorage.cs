a65bc7dfa278b88e445dab6b8f93732f0492f048
Karmel Indych <karmeli87@gmail.com>
2025-09-15 11:15:57 +0300

    RavenDB-25058 Fix restoring Incremental timeseries with dead values

--- src/Raven.Server/Documents/TimeSeries/TimeSeriesStorage.cs
+++ src/Raven.Server/Documents/TimeSeries/TimeSeriesStorage.cs
@@ -1548,6 +1548,12 @@ namespace Raven.Server.Documents.TimeSeries
                         throw new InvalidDataException($"The entries of '{Name}' incremental time-series for document '{DocumentId}' must be sorted by their timestamps. " +
                                                        $"Got: current '{_current.Timestamp:O}'.");
 
+                    if (_current.Status == TimeSeriesValuesSegment.Dead || _next.Status == TimeSeriesValuesSegment.Dead)
+                    {
+                        // dead values doesn't have tags, so we let the caller handle this
+                        break;
+                    }
+
                     if (_current.Timestamp == _next.Timestamp)
                     {
                         var tagCompare = _current.Tag.CompareTo(_next.Tag);
@@ -1586,11 +1592,8 @@ namespace Raven.Server.Documents.TimeSeries
 
             private void AssertTag(SingleResult next)
             {
-                if (FromReplication)
-                {
-                    if (next.Status == TimeSeriesValuesSegment.Dead)
-                        return;
-                }
+                if (next.Status == TimeSeriesValuesSegment.Dead)
+                    return;
 
                 if (next.Tag?.Length != _tagLength)
                     throw new InvalidDataException($"Tag of '{Name}' time-series for document '{DocumentId}' are illegal (Tag:{next.Tag})");
