3502030fb2340c01e1c7559d809899e9787ea9c4
Maciej Aszyk <maciej@ravendb.net>
2025-10-08 14:53:48 +0200

    RavenDB-25136 New paging mechanism requires creating terms reader for map-reduce index as well.

---
+++ test/SlowTests/Issues/RavenDB-25136.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FastTests;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+using Raven.Client.Documents.Operations.Backups;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues;
+
+public class RavenDB_25136(ITestOutputHelper output) : RavenTestBase(output)
+{
+    [RavenFact(RavenTestCategory.Querying)]
+    public async Task CoraxCanSortReduceResultsBackwardCompatibility()
+    {
+        var backupPath = NewDataPath(forceCreateDir: true);
+        var fullBackupPath = Path.Combine(backupPath, "RavenDB_25136.ravendb-snapshot");
+        ExtractFile(fullBackupPath);
+        using (var store = GetDocumentStore())
+        {
+            var databaseName = GetDatabaseName();
+            using (Backup.RestoreDatabase(store, new RestoreBackupConfiguration { BackupLocation = backupPath, DatabaseName = databaseName }))
+            {
+                using (var session = store.OpenAsyncSession(database: databaseName))
+                {
+                    var results = await session.Query<Index.IndexResult, Index>()
+                        .Where(x => x.Name.StartsWith("_"))
+                        .OrderByDescending(x => x.Count)
+                        .Skip(1)
+                        .Take(1)
+                        .ToListAsync();
+                    
+                    Assert.Equal(1, results.Count);
+                    Assert.Equal("_1", results[0].Name);
+                }
+            }
+        }
+
+        void ExtractFile(string path)
+        {
+            using (var file = File.Create(path))
+            using (var stream = typeof(RavenDB_22937).Assembly.GetManifestResourceStream("SlowTests.Data.RavenDB_25136.RavenDB_25136.ravendb-snapshot"))
+            {
+                stream.CopyTo(file);
+            }
+        }
+    }
+
+    private class Dto
+    {
+        public string Name { get; set; }
+    }
+
+    private class Index : AbstractIndexCreationTask<Dto, Index.IndexResult>
+    {
+        public class IndexResult
+        {
+            public string Name { get; set; }
+            public int Count { get; set; }
+        }
+
+        public Index()
+        {
+            Map = dtos => from dto in dtos
+                select new IndexResult()
+                {
+                    Name = dto.Name,
+                    Count = 1
+                };
+
+            Reduce = results => from result in results
+                group result by result.Name
+                into g
+                select new IndexResult()
+                {
+                    Name = g.Key,
+                    Count = g.Sum(x => x.Count)
+                };
+        }
+    }
+}
