39be7eab6430c666f85c0f5d71f4a02e0efeada7
Lev Skuditsky <lev.skuditsky@ravendb.net>
2025-10-28 11:17:53 +0200

    RavenDB-25280 - Add a special error if `ChunkingOptions` are not provided for `EmbeddingsPathConfiguration` when creating `EmbeddingsGenerationConfiguration`

--- src/Raven.Client/Documents/Operations/AI/EmbeddingsGenerationConfiguration.cs
+++ src/Raven.Client/Documents/Operations/AI/EmbeddingsGenerationConfiguration.cs
@@ -109,7 +109,12 @@ public sealed class EmbeddingsGenerationConfiguration : AbstractAiIntegrationCon
         if (EmbeddingsPathConfigurations is not null)
         {
             foreach (var pathConfiguration in EmbeddingsPathConfigurations)
-                pathConfiguration.ChunkingOptions.Validate(pathConfiguration.Path, errors);
+            {
+                if (pathConfiguration.ChunkingOptions is not null)
+                    pathConfiguration.ChunkingOptions.Validate(pathConfiguration.Path, errors);
+                else
+                    errors.Add($"Path '{pathConfiguration.Path}': {nameof(ChunkingOptions)} must be provided.");
+            }
         }
         
         if (EmbeddingsTransformation?.ValidateScript() == false)
