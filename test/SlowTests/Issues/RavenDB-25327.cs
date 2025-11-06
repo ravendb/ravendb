1afeed97c96cbe586032476b47503801835d074a
Bartosz Piekarski <bartosz@ravendb.net>
2025-11-06 14:55:12 +0100

    RavenDB-25327 Add test and fix subject creation for certificates without common name

---
+++ test/SlowTests/Issues/RavenDB-25327.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FastTests;
+using Raven.Client;
+using Raven.Server.Utils;
+using Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SlowTests.Issues;
+
+public class RavenDB_25327 : RavenTestBase
+{
+    public RavenDB_25327(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    [RavenFact(RavenTestCategory.Certificates)]
+    public void CanCreateCertificateForCommunication_UsingServerCertWith1EKU_AndMultipleSans_WithoutSubject()
+    {
+        const string firstDomain = "a.test-domain.com";
+        var suffix = Guid.NewGuid().ToString().Split('-')[0];
+        CertificateUtils.CreateCertificateAuthorityCertificate($"ca-{suffix}", out var kp, out var caSubjectName);
+        CertificateUtils.CreateSelfSignedCertificateBasedOnPrivateKey(null,
+            caSubjectName,
+            kp,
+            false,
+            false,
+            DateTime.UtcNow.AddDays(7),
+            out var serverCertBytes,
+            sans: [firstDomain, "b.test-domain.com", "c.test-domain.com"],
+            with2Eku: false);
+
+        var serverCert = CertificateLoaderUtil.CreateCertificate(serverCertBytes, flags: CertificateLoaderUtil.FlagsForExport);
+        Assert.Contains(firstDomain, serverCert.GetDisplayName());
+        var clientCert = CertificateUtils.CreateClientCertificateFromServerCertificate(serverCert, out _);
+
+        Assert.NotNull(clientCert);
+        Assert.Equal("CN=client-cert-for-cluster-communication", clientCert.Subject);
+    }
+}
