7893247fe3472a8e48c6bd776916378b5a566ab2
Pawel Pekrol <ppekrol@ravendb.net>
2025-11-12 12:12:43 +0100

    RavenDB-7070 more fixes

--- scripts/buildProjects.ps1
+++ scripts/buildProjects.ps1
@@ -200,8 +200,6 @@ function BuildTool ( $toolName, $srcDir, $outDir, $target, $trim ) {
         $commandArgs += '/p:ContinuousIntegrationBuild=true'
     }
 
-    $commandArgs += '/p:RuntimeFrameworkVersion=10.0.0-rtm.25513.102'
-
     write-host -ForegroundColor Cyan "Publish ${toolName}: $command $commandArgs"
     Invoke-Expression -Command "$command $commandArgs"
     CheckLastExitCode
