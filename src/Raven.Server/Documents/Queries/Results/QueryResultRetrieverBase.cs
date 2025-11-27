0eda5167ef68967155e45de51543c98ae4278ed8
Arkadiusz Palinski <arek@ravendb.net>
2025-11-27 12:53:08 +0100

    RavenDB-25441 Fix handling of null values in QueryResultRetrieverBase and add tests for queries with loads via null properties stored in the index

--- src/Raven.Server/Documents/Queries/Results/QueryResultRetrieverBase.cs
+++ src/Raven.Server/Documents/Queries/Results/QueryResultRetrieverBase.cs
@@ -865,7 +865,9 @@ namespace Raven.Server.Documents.Queries.Results
                                         continue;
 
                                     fieldValue = ConvertType(_context, field, GetFieldType(field.Name, retrieverInput.LuceneDocument), retrieverInput.State);
-                                    _loadedDocumentIds.Add(fieldValue.ToString());
+                                    
+                                     if (fieldValue != null)
+                                        _loadedDocumentIds.Add(fieldValue.ToString());
                                 }
                             }
                             break;
@@ -881,7 +883,8 @@ namespace Raven.Server.Documents.Queries.Results
                                 }
                                 else
                                 {
-                                    _loadedDocumentIds.Add(fieldValue?.ToString());
+                                    if (fieldValue != null)
+                                        _loadedDocumentIds.Add(fieldValue.ToString());
                                 }
                             }
                             break;
