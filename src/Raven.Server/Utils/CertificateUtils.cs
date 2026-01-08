c0250ced271fbdcbe5a46863f47745d9f7183b34
Bartosz Piekarski <bartosz@ravendb.net>
2026-01-08 12:32:07 +0100

    RavenDB-25506 Move GC.keepAlive after Verify method

--- src/Raven.Server/Utils/CertificateUtils.cs
+++ src/Raven.Server/Utils/CertificateUtils.cs
@@ -204,8 +204,8 @@ namespace Raven.Server.Utils
                 with2Eku: with2Eku);
 
             var selfSignedCertificateBasedOnPrivateKey = CertificateLoaderUtil.CreateCertificate(certBytes);
-            GC.KeepAlive(selfSignedCertificateBasedOnPrivateKey); // https://github.com/dotnet/runtime/issues/122642#issuecomment-3720461147
             selfSignedCertificateBasedOnPrivateKey.Verify();
+            GC.KeepAlive(selfSignedCertificateBasedOnPrivateKey); // https://github.com/dotnet/runtime/issues/122642#issuecomment-3720461147
 
             // We had a problem where we didn't cleanup the user store in Linux (~/.dotnet/corefx/cryptography/x509stores/ca)
             // and it exploded with thousands of certificates. This caused ssl handshakes to fail on that machine, because it would timeout when
