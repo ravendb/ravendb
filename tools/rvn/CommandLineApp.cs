4f5e35c48dff5453b1dbdf6030abf880160c5791
Bartosz Piekarski <bartosz@ravendb.net>
2026-01-12 15:09:11 +0100

    RavenDB-25342 Add 1EKU support to rvn put-client-certificate

--- tools/rvn/CommandLineApp.cs
+++ tools/rvn/CommandLineApp.cs
@@ -8,7 +8,10 @@ using McMaster.Extensions.CommandLineUtils;
 using Newtonsoft.Json;
 using Raven.Client.Documents;
 using Raven.Client.ServerWide.Operations.Certificates;
+using Raven.Client.Util;
 using Raven.Server.Commercial;
+using Raven.Server.ServerWide;
+using Raven.Server.Utils;
 using Sparrow.Json;
 using Sparrow.Platform;
 using Voron.Global;
@@ -340,11 +343,15 @@ namespace rvn
 
                     
                     X509Certificate2 clientCertificate = new(clientCertificatePathArg.Value);
-                    X509Certificate2 serverCertificate = new(serverCertificatePathArg.Value);
+                    var serverCertForCommunication = CertificateLoaderUtil.CreateCertificate(serverCertificatePathArg.Value, null, CertificateLoaderUtil.FlagsForExport);
+                    if (SecretProtection.HasCertificateClientAuthEnhancedKeyUsage(serverCertForCommunication) == false)
+                    {
+                        serverCertForCommunication = CertificateUtils.CreateClientCertificateFromServerCertificate(serverCertForCommunication, out _);
+                    }
                     var name = Path.GetFileNameWithoutExtension(clientCertificatePathArg.Value);
                     try
                     {
-                        DocumentStore store = new() {Certificate = serverCertificate, Urls = new[] {ravenServerUrlArg.Value}};
+                        DocumentStore store = new() {Certificate = serverCertForCommunication, Urls = new[] {ravenServerUrlArg.Value}};
                         store.Initialize();
                         var operation = new PutClientCertificateOperation(name, clientCertificate, new Dictionary<string,DatabaseAccess>(), SecurityClearance.ClusterAdmin);
                         store.Maintenance.Server.Send(operation);
