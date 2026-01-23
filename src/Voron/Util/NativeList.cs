20de0ac3a1599e0d1004479869ff2139ef6eaab2
Maciej Aszyk <maciej@ravendb.net>
2026-01-23 10:05:18 +0100

    RavenDB-25839 - NativeList should not be accessed after the Dispose call. - The PostingList leaf should retrieve the Count from NativeList before disposal.

--- src/Voron/Util/NativeList.cs
+++ src/Voron/Util/NativeList.cs
@@ -27,10 +27,29 @@ public unsafe struct NativeList<T>
     public T* RawItems => Capacity > 0 ? (T*)_storage.Ptr : null;
 
     public int Capacity = 0;
+    
+#if DEBUG
+    private bool _disposed = false;
+    private int _count = 0;
+    public int Count
+    {
+        readonly get
+        {
+            Debug.Assert(_count <= Capacity, "NativeList count is greater than capacity.");
+            Debug.Assert(_disposed == false, "NativeList has been disposed.");
+            return _count;
+        }
+        set
+        {
+            _count = value;
+        }
+    }
+#else
     public int Count;
+#endif
 
     public readonly Span<T> ToSpan() => Count == 0 ? Span<T>.Empty : new Span<T>(_storage.Ptr, Count);
-    
+
     public bool IsValid => RawItems != null;
 
     public NativeList()
@@ -219,6 +238,11 @@ public unsafe struct NativeList<T>
         if(_storage.HasValue)
             ctx.Release(ref _storage);
         Capacity = 0;
+        Count = 0;
+        
+#if DEBUG
+        _disposed = true;
+#endif
     }
 
     public void Clear()
