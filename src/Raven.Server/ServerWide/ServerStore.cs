14b443a291b884624d1e2a3df431bf15f27a8add
Michał Leśniak <michal.lesniak@ravendb.net>
2026-02-24 14:22:33 +0100

    RavenDB-25419 Throw exception on ETL rename

--- src/Raven.Server/ServerWide/ServerStore.cs
+++ src/Raven.Server/ServerWide/ServerStore.cs
@@ -2388,8 +2388,9 @@ namespace Raven.Server.ServerWide
                 switch (EtlConfiguration<ConnectionString>.GetEtlType(etlConfiguration))
                 {
                     case EtlType.Raven:
+                        var existingRvnConfiguration = rawRecord.RavenEtls.Single(x => x.TaskId == id);
                         var rvnEtl = JsonDeserializationCluster.RavenEtlConfiguration(etlConfiguration);
-                        rvnEtl.Validate(out var rvnEtlErr, validateName: false, validateConnection: false);
+                        rvnEtl.Validate(out var rvnEtlErr, validateName: false, validateConnection: false, existingRvnConfiguration);
                         if (ValidateConnectionString(rawRecord, rvnEtl.ConnectionStringName, rvnEtl.EtlType) == false)
                             rvnEtlErr.Add($"Could not find connection string named '{rvnEtl.ConnectionStringName}'. Please supply an existing connection string.");
 
@@ -2398,8 +2399,9 @@ namespace Raven.Server.ServerWide
                         command = new UpdateRavenEtlCommand(id, rvnEtl, databaseName, raftRequestId);
                         break;
                     case EtlType.Sql:
+                        var existingSqlConfiguration = rawRecord.SqlEtls.Single(x => x.TaskId == id);
                         var sqlEtl = JsonDeserializationCluster.SqlEtlConfiguration(etlConfiguration);
-                        sqlEtl.Validate(out var sqlEtlErr, validateName: false, validateConnection: false);
+                        sqlEtl.Validate(out var sqlEtlErr, validateName: false, validateConnection: false, existingSqlConfiguration);
                         if (ValidateConnectionString(rawRecord, sqlEtl.ConnectionStringName, sqlEtl.EtlType) == false)
                             sqlEtlErr.Add($"Could not find connection string named '{sqlEtl.ConnectionStringName}'. Please supply an existing connection string.");
 
@@ -2408,8 +2410,9 @@ namespace Raven.Server.ServerWide
                         command = new UpdateSqlEtlCommand(id, sqlEtl, databaseName, raftRequestId);
                         break;
                     case EtlType.Olap:
+                        var existingOlapConfiguration = rawRecord.OlapEtls.Single(x => x.TaskId == id);
                         var olapEtl = JsonDeserializationCluster.OlapEtlConfiguration(etlConfiguration);
-                        olapEtl.Validate(out var olapEtlErr, validateName: false, validateConnection: false);
+                        olapEtl.Validate(out var olapEtlErr, validateName: false, validateConnection: false, existingOlapConfiguration);
                         if (ValidateConnectionString(rawRecord, olapEtl.ConnectionStringName, olapEtl.EtlType) == false)
                             olapEtlErr.Add($"Could not find connection string named '{olapEtl.ConnectionStringName}'. Please supply an existing connection string.");
 
@@ -2418,8 +2421,9 @@ namespace Raven.Server.ServerWide
                         command = new UpdateOlapEtlCommand(id, olapEtl, databaseName, raftRequestId);
                         break;
                     case EtlType.ElasticSearch:
+                        var existingElasticSearchConfiguration = rawRecord.ElasticSearchEtls.Single(x => x.TaskId == id);
                         var elasticSearchEtl = JsonDeserializationCluster.ElasticSearchEtlConfiguration(etlConfiguration);
-                        elasticSearchEtl.Validate(out var elasticSearchEtlErr, validateName: false, validateConnection: false);
+                        elasticSearchEtl.Validate(out var elasticSearchEtlErr, validateName: false, validateConnection: false, existingElasticSearchConfiguration);
                         if (ValidateConnectionString(rawRecord, elasticSearchEtl.ConnectionStringName, elasticSearchEtl.EtlType) == false)
                             elasticSearchEtlErr.Add($"Could not find connection string named '{elasticSearchEtl.ConnectionStringName}'. Please supply an existing connection string.");
 
@@ -2428,8 +2432,9 @@ namespace Raven.Server.ServerWide
                         command = new UpdateElasticSearchEtlCommand(id, elasticSearchEtl, databaseName, raftRequestId);
                         break;
                     case EtlType.Queue:
+                        var existingQueueConfiguration = rawRecord.QueueEtls.Single(x => x.TaskId == id);
                         var queueEtl = JsonDeserializationCluster.QueueEtlConfiguration(etlConfiguration);
-                        queueEtl.Validate(out var queueEtlErr, validateName: false, validateConnection: false);
+                        queueEtl.Validate(out var queueEtlErr, validateName: false, validateConnection: false, existingQueueConfiguration);
                         if (ValidateConnectionString(rawRecord, queueEtl.ConnectionStringName, queueEtl.EtlType) == false)
                             queueEtlErr.Add($"Could not find connection string named '{queueEtl.ConnectionStringName}'. Please supply an existing connection string.");
 
