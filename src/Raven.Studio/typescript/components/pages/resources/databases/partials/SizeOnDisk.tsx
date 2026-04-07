fff5b26cd2a43945fe0ac36db3aa18d3d0d71c3c
Damian Olszewski <damian@ravendb.net>
2026-04-07 15:48:03 +0200

    RavenDB-22818 Refactor SizeOnDisk component to use PopoverWithHoverWrapper and handle optional size fields

--- src/Raven.Studio/typescript/components/pages/resources/databases/partials/SizeOnDisk.tsx
+++ src/Raven.Studio/typescript/components/pages/resources/databases/partials/SizeOnDisk.tsx
@@ -1,17 +1,13 @@
 ﻿import { DatabaseLocalInfo } from "components/models/databases";
 import genUtils from "common/generalUtils";
-import useUniqueId from "components/hooks/useUniqueId";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-import Tooltip from "react-bootstrap/Tooltip";
+import PopoverWithHoverWrapper from "components/common/PopoverWithHoverWrapper";
 
 export function SizeOnDisk(props: { info: DatabaseLocalInfo }) {
     const { info } = props;
-
-    const tooltipId = useUniqueId("size-on-disk-tooltip");
-
     if (!info) {
         return null;
     }
+
     const tempBufferSize = info.tempBuffersSize?.SizeInBytes ?? 0;
     const physicalSize = info.totalPhysicalSize?.SizeInBytes ?? 0;
     const allocatedSize = info.totalAllocatedSize?.SizeInBytes ?? 0;
@@ -19,9 +15,9 @@ export function SizeOnDisk(props: { info: DatabaseLocalInfo }) {
 
     return (
         <div>
-            <OverlayTrigger
-                overlay={
-                    <Tooltip id={tooltipId}>
+            <PopoverWithHoverWrapper
+                message={
+                    <>
                         Data (on disk): <strong>{genUtils.formatBytesToSize(physicalSize)}</strong>
                         <br />
                         Data (allocated): <strong>{genUtils.formatBytesToSize(allocatedSize)}</strong>
@@ -29,11 +25,11 @@ export function SizeOnDisk(props: { info: DatabaseLocalInfo }) {
                         Temp: <strong>{genUtils.formatBytesToSize(tempBufferSize)}</strong>
                         <br />
                         Total: <strong>{genUtils.formatBytesToSize(grandTotalSize)}</strong>
-                    </Tooltip>
+                    </>
                 }
             >
-                <div>{genUtils.formatBytesToSize(grandTotalSize)}</div>
-            </OverlayTrigger>
+                {genUtils.formatBytesToSize(grandTotalSize)}
+            </PopoverWithHoverWrapper>
         </div>
     );
 }
