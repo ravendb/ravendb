de0d6b5df71e1c6c2ff2c6d701b0f53c1f89990e
Damian Olszewski <damian@ravendb.net>
2026-04-07 15:51:52 +0200

    RavenDB-22818 Show 'On Disk' column only for a real difference between allocated and physical

--- src/Raven.Studio/typescript/viewmodels/database/status/storageReport.ts
+++ src/Raven.Studio/typescript/viewmodels/database/status/storageReport.ts
@@ -111,7 +111,7 @@ class storageReport extends shardViewModelBase {
         });
 
         this.showOnDiskColumn = ko.pureComputed(() =>
-            !!(this.node().internalChildren || []).find(x => x.physicalSize != null)
+            !!(this.node().internalChildren || []).find(x => x.physicalSize != null && x.physicalSize < x.size)
         );
         this.allocatedNodeSize = ko.pureComputed(() =>
             generalUtils.formatBytesToSize(this.node().size)
