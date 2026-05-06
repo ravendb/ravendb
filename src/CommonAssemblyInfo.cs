aa7df9bafc728c350d28539d5d756f0957ee07c2
Danielle9897 <danielle.9897@gmail.com>
2026-05-06 09:57:38 +0300

    RavenDB-26545 Update About footer

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Resources;
 
-[assembly: AssemblyCopyright("© RavenDB 2009 - 2025 All rights reserved.")]
+[assembly: AssemblyCopyright("© RavenDB 2009 - 2026 All rights reserved.")]
 
 [assembly: AssemblyVersion("6.2.16")]
 [assembly: AssemblyFileVersion("6.2.16.62")]
