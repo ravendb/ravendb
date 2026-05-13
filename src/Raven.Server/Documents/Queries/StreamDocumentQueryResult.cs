79d8ac3a5ec7ce15946d5b2a10cd864c9b5abfab
Grisha Kotler <grisha@ravendb.net>
2026-05-13 23:05:15 +0300

    RavenDB-26538 - cache HasTimeout on StreamDocumentQueryResult to skip Delay() when no timeout is set

--- src/Raven.Server/Documents/Queries/StreamDocumentQueryResult.cs
+++ src/Raven.Server/Documents/Queries/StreamDocumentQueryResult.cs
@@ -10,6 +10,7 @@ namespace Raven.Server.Documents.Queries
     public sealed class StreamDocumentQueryResult : StreamQueryResult<Document>
     {
         private readonly DocumentsOperationContext _context;
+        private readonly bool _hasTokenTimeout;
 
         public override async ValueTask AddResultAsync(Document result, CancellationToken token)
         {
@@ -19,7 +20,8 @@ namespace Raven.Server.Documents.Queries
             using (result)
                 await GetWriter().AddResultAsync(result, token).ConfigureAwait(false);
 
-            GetToken().Delay();
+            if (_hasTokenTimeout)
+                GetToken().Delay();
         }
 
         public StreamDocumentQueryResult(HttpResponse response, IStreamQueryResultWriter<Document> writer, DocumentsOperationContext context,
@@ -28,6 +30,7 @@ namespace Raven.Server.Documents.Queries
             if (response.HasStarted)
                 throw new InvalidOperationException("You cannot start streaming because response has already started.");
             _context = context;
+            _hasTokenTimeout = token.HasTimeout;
         }
     }
 }
