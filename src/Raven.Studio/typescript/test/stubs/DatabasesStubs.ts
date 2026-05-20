e99cbd9a00e5fc682237110a9fd01ada1c9aedbc
Golan Igal Nahum <golan.nahum@ravendb.net>
2026-05-20 12:41:17 +0300

    RavenDB-26261: Better error message when the provider does not support a file format

--- src/Raven.Studio/typescript/test/stubs/DatabasesStubs.ts
+++ src/Raven.Studio/typescript/test/stubs/DatabasesStubs.ts
@@ -617,6 +617,7 @@ export class DatabasesStubs {
             TcpServerUrl: null,
             Log: [],
             Error: "System.UriFormatException: Invalid URI: The format of the URI could not be determined.\n   at System.Uri.CreateThis(String uri, Boolean dontEscape, UriKind uriKind, UriCreationOptions& creationOptions)\n   at System.Uri..ctor(String uriString)\n   at Raven.Server.Documents.ETL.Providers.Queue.QueueBrokerConnectionHelper.CreateRabbitMqConnection(RabbitMqConnectionSettings settings) in D:\\Builds\\RavenDB-6.0-Nightly\\20231123-0200\\src\\Raven.Server\\Documents\\ETL\\Providers\\Queue\\QueueBrokerConnectionHelper.cs:line 80",
+            AcceptsImageInput: false,
         };
     }
 
@@ -627,6 +628,7 @@ export class DatabasesStubs {
             TcpServerUrl: null,
             Log: [],
             Error: null,
+            AcceptsImageInput: false,
         };
     }
 
