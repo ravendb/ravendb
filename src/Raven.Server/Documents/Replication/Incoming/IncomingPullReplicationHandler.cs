94716e21506c2f73d71c4439b027f6449f046ca0
Lev Skuditsky <lev.skuditsky@ravendb.net>
2026-06-04 09:26:44 +0300

    RavenDB-26295 - Adapt pull replication revision tombstone handling to schema upgrade

--- src/Raven.Server/Documents/Replication/Incoming/IncomingPullReplicationHandler.cs
+++ src/Raven.Server/Documents/Replication/Incoming/IncomingPullReplicationHandler.cs
@@ -322,10 +322,10 @@ namespace Raven.Server.Documents.Replication.Incoming
                 return parsedChangeVectorToMerge.IsSingle ? parsedChangeVectorToMerge : parsedChangeVectorToMerge.Order;
             }
 
-            protected override void HandleRevisionTombstone(DocumentsOperationContext context, string docId, string changeVector, out Slice changeVectorSlice, out Slice keySlice, List<IDisposable> toDispose)
+            protected override string HandleRevisionTombstone(DocumentsOperationContext context, string changeVector)
             {
                 ReplaceKnownSinkEntries(context, ref changeVector);
-                base.HandleRevisionTombstone(context, docId, changeVector, out changeVectorSlice, out keySlice, toDispose);
+                return base.HandleRevisionTombstone(context, changeVector);
             }
         }
 
