e91a258121c421b2140ae290332e98547d9834bc
Grisha <grisha@ravendb.net>
2026-06-10 22:01:59 +0300

    RavenDB-26709 - unify pull replication confirmed cursor into a single reply field and resume via LastAcceptedChangeVector

--- src/Raven.Server/Utils/ReplicationUtils.cs
+++ src/Raven.Server/Utils/ReplicationUtils.cs
@@ -7,8 +7,10 @@ using Raven.Client;
 using Raven.Client.Documents.Conventions;
 using Raven.Client.Http;
 using Raven.Client.ServerWide.Commands;
-using Raven.Client.Util;
+using Raven.Server.Documents.Replication;
+using Raven.Server.ServerWide;
 using Raven.Server.ServerWide.Commands;
+using Raven.Server.ServerWide.Context;
 using Sparrow.Json;
 
 namespace Raven.Server.Utils
@@ -76,6 +78,21 @@ namespace Raven.Server.Utils
                     return;
 
                 ThrowInvalidCollectionAfterResolve(collection, null);
+            }
+        }
+
+        public static string ReadCursorFromClusterFor(ServerStore serverStore, string databaseName, long taskId, ExternalReplicationState.ReplicationStateType type)
+        {
+            using (serverStore.ContextPool.AllocateOperationContext(out TransactionOperationContext context))
+            using (context.OpenReadTransaction())
+            {
+                var key = ExternalReplicationState.GenerateItemName(databaseName, taskId, type);
+                var stateBlittable = serverStore.Cluster.Read(context, key);
+                if (stateBlittable == null)
+                    return null;
+
+                var state = JsonDeserializationCluster.ExternalReplicationState(stateBlittable);
+                return state.SourceChangeVector;
             }
         }
 
