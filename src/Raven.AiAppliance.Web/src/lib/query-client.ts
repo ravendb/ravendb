baf539646dfa42cf44d166bda243795cb4dfd76f
Damian Olszewski <damian@ravendb.net>
2026-06-10 12:48:43 +0200

    RavenDB-26630 Set default staleTime to hit cache

--- src/Raven.AiAppliance.Web/src/lib/query-client.ts
+++ src/Raven.AiAppliance.Web/src/lib/query-client.ts
@@ -3,6 +3,7 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
+            staleTime: 60_000,
             refetchOnWindowFocus: false,
             retry: 1,
         },
