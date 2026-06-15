c801c4575cf01e50b26f94b6c09db03564b210a1
Lev Skuditsky <lev.skuditsky@ravendb.net>
2026-06-15 08:28:54 +0300

    RavenDB-26295 - Split pull heartbeat cursor handling by direction

--- src/Raven.Server/Documents/Replication/Incoming/IncomingPullReplicationHandler.AsHub.cs
+++ src/Raven.Server/Documents/Replication/Incoming/IncomingPullReplicationHandler.AsHub.cs
@@ -1,15 +1,19 @@
 using System;
 using Raven.Client.Documents.Operations.Replication;
 using Raven.Client.Documents.Replication.Messages;
+using Raven.Server.Documents.Replication.Outgoing;
 using Raven.Server.Documents.TcpHandlers;
 using Raven.Server.Documents.TransactionMerger.Commands;
 using Raven.Server.ServerWide.Context;
 using Sparrow.Json;
+using Sparrow.Json.Parsing;
 
 namespace Raven.Server.Documents.Replication.Incoming
 {
     public sealed class IncomingPullReplicationHandlerAsHub : IncomingPullReplicationHandler
     {
+        private readonly PullReplicationBatchHistory _sinkBatchHistory;
+
         public IncomingPullReplicationHandlerAsHub(
             TcpConnectionOptions options,
             ReplicationLatestEtagRequest sourceHandshakeRequest,
@@ -18,6 +22,7 @@ namespace Raven.Server.Documents.Replication.Incoming
             ReplicationLatestEtagRequest.ReplicationType replicationType,
             ReplicationLoader.PullReplicationParams incomingPullReplicationParams) : base(options, sourceHandshakeRequest, parent, bufferToCopy, replicationType, incomingPullReplicationParams)
         {
+            _sinkBatchHistory = new PullReplicationBatchHistory(parent);
         }
 
         protected override bool PreventIncomingSinkDeletions =>
@@ -26,6 +31,29 @@ namespace Raven.Server.Documents.Replication.Incoming
         protected override void MergeSourceChangeVectorFromHeartbeat(DocumentsOperationContext documentsContext, string changeVector)
         {
             // do nothing
+        }
+
+        protected override DynamicJsonValue GetHeartbeatStatusMessage(DocumentsOperationContext documentsContext, long lastDocumentEtag, string handledMessageType)
+        {
+            var heartbeat = base.GetHeartbeatStatusMessage(documentsContext, lastDocumentEtag, handledMessageType);
+
+            switch (handledMessageType)
+            {
+                case ReplicationMessageType.Documents:
+                case ReplicationMessageType.Heartbeat:
+                    if (string.IsNullOrEmpty(_lastBatchChangeVector) == false)
+                    {
+                        long hubEtag = (long)heartbeat[nameof(ReplicationMessageReply.CurrentEtag)];
+                        _sinkBatchHistory.Add(hubEtag, _lastBatchChangeVector);
+                    }
+
+                    break;
+            }
+
+            // Here we report to the sink about the last sink change vector that was replicated to all the nodes in the hub cluster
+            heartbeat[nameof(ReplicationMessageReply.LastConfirmedChangeVector)] = _sinkBatchHistory.ComputeConfirmedChangeVector(_lastBatchChangeVector);
+
+            return heartbeat;
         }
 
         protected override DocumentMergedTransactionCommand GetMergeDocumentsCommand(DocumentsOperationContext context, DataForReplicationCommand data, long lastDocumentEtag)
--- src/Raven.Server/Documents/Replication/Incoming/IncomingPullReplicationHandler.AsSink.cs
+++ src/Raven.Server/Documents/Replication/Incoming/IncomingPullReplicationHandler.AsSink.cs
@@ -1,14 +1,21 @@
 using System;
+using Raven.Client.Extensions;
 using Raven.Client.Documents.Replication.Messages;
+using Raven.Client.Util;
+using Raven.Server.Documents.Replication.Outgoing;
 using Raven.Server.Documents.TcpHandlers;
 using Raven.Server.Documents.TransactionMerger.Commands;
 using Raven.Server.ServerWide.Context;
+using Raven.Server.ServerWide.Commands;
 using Sparrow.Json;
+using Sparrow.Json.Parsing;
 
 namespace Raven.Server.Documents.Replication.Incoming
 {
     public sealed class IncomingPullReplicationHandlerAsSink : IncomingPullReplicationHandler
     {
+        private readonly PullReplicationBatchHistory _hubBatchHistory;
+
         public IncomingPullReplicationHandlerAsSink(
             TcpConnectionOptions options,
             ReplicationLatestEtagRequest sourceHandshakeRequest,
@@ -17,9 +24,47 @@ namespace Raven.Server.Documents.Replication.Incoming
             ReplicationLatestEtagRequest.ReplicationType replicationType,
             ReplicationLoader.PullReplicationParams incomingPullReplicationParams) : base(options, sourceHandshakeRequest, parent, bufferToCopy, replicationType, incomingPullReplicationParams)
         {
+            _hubBatchHistory = new PullReplicationBatchHistory(parent);
         }
 
         protected override bool PreventIncomingSinkDeletions => false;
+
+        protected override DynamicJsonValue GetHeartbeatStatusMessage(DocumentsOperationContext documentsContext, long lastDocumentEtag, string handledMessageType)
+        {
+            var heartbeat = base.GetHeartbeatStatusMessage(documentsContext, lastDocumentEtag, handledMessageType);
+
+            switch (handledMessageType)
+            {
+                case ReplicationMessageType.Documents:
+                case ReplicationMessageType.Heartbeat:
+                    if (string.IsNullOrEmpty(_lastBatchChangeVector) == false)
+                    {
+                        long sinkEtag = (long)heartbeat[nameof(ReplicationMessageReply.CurrentEtag)];
+                        _hubBatchHistory.Add(sinkEtag, _lastBatchChangeVector);
+                    }
+
+                    break;
+            }
+
+            // Here we check *locally* in the sink what is the last hub change vector that was replicated to all the nodes in the sink cluster
+            // and persist it as the hub cursor for failover.
+            if (_hubBatchHistory.ComputeConfirmedChangeVector(_lastBatchChangeVector) is { } confirmedHubCv)
+            {
+                var command = new UpdateExternalReplicationStateCommand(ReplicationLoaderParent.Database.Name, RaftIdGenerator.NewId())
+                {
+                    ExternalReplicationState = new ExternalReplicationState
+                    {
+                        TaskId = IncomingPullReplicationParams.TaskId,
+                        NodeTag = ReplicationLoaderParent._server.NodeTag,
+                        SourceChangeVector = confirmedHubCv,
+                        Type = ExternalReplicationState.ReplicationStateType.HubCursor
+                    }
+                };
+                ReplicationLoaderParent._server.SendToLeaderAsync(command).IgnoreUnobservedExceptions();
+            }
+
+            return heartbeat;
+        }
 
         protected override DocumentMergedTransactionCommand GetMergeDocumentsCommand(DocumentsOperationContext context, DataForReplicationCommand data, long lastDocumentEtag)
         {
