000f1a77664a863b25de90f87b7fad7f06b88495
Arkadiusz Palinski <arek@ravendb.net>
2026-06-24 09:22:37 +0200

    RavenDB-26859 Fail clearly on WriteMode=IoRing with IoRingQueueSize=-1
    WriteMode=IoRing combined with IoRingQueueSize=-1 (which disables I/O ring) now throws a clear error at config init pointing to WriteMode=Auto, instead of failing later with the generic PAL "FailCreateIoRing" error. Also clarified the IoRingQueueSize description that -1 only disables I/O ring under WriteMode=Auto.

---
+++ test/FastTests/Issues/RavenDB_26859.cs
@@ -0,0 +1,40 @@
+using System;
+using Raven.Server.Config;
+using Tests.Infrastructure;
+using Xunit;
+
+namespace FastTests.Issues
+{
+    public class RavenDB_26859 : NoDisposalNeeded
+    {
+        public RavenDB_26859(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [RavenFact(RavenTestCategory.Configuration | RavenTestCategory.Voron)]
+        public void DisablingIoRingUnderExplicitIoRingWriteModeFailsClearly()
+        {
+            var configuration = RavenConfiguration.CreateForServer(null);
+            configuration.SetSetting(RavenConfiguration.GetKey(x => x.Storage.WriteMode), "IoRing");
+            configuration.SetSetting(RavenConfiguration.GetKey(x => x.Storage.IoRingQueueSize), "-1");
+
+            var error = Assert.Throws<InvalidOperationException>(() => configuration.Initialize());
+            Assert.Contains("Auto", error.Message);
+        }
+
+        [RavenTheory(RavenTestCategory.Configuration | RavenTestCategory.Voron)]
+        [InlineData("IoRing", "1024")]
+        [InlineData("Auto", "-1")]
+        [InlineData("Auto", "0")]
+        [InlineData("FileIo", "-1")]
+        [InlineData("FileIo", "0")]
+        public void ValidIoRingConfigurationInitializes(string writeMode, string queueSize)
+        {
+            var configuration = RavenConfiguration.CreateForServer(null);
+            configuration.SetSetting(RavenConfiguration.GetKey(x => x.Storage.WriteMode), writeMode);
+            configuration.SetSetting(RavenConfiguration.GetKey(x => x.Storage.IoRingQueueSize), queueSize);
+
+            configuration.Initialize();
+        }
+    }
+}
