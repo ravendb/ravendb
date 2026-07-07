e7abf3a9427d24baf3dccdcaa27b25bd9fcce430
Damian Olszewski <damian@ravendb.net>
2026-07-07 15:00:16 +0200

    RavenDB-26939 Cleanup

--- src/Raven.Studio/typescript/common/shell/accessManager.ts
+++ src/Raven.Studio/typescript/common/shell/accessManager.ts
@@ -212,9 +212,10 @@ class accessManager {
             return null;
         }
 
-        // The certificate may define permissions with different letter casing than the actual database name
-        const groupKey = DatabaseUtils.default.shardGroupKey(name).toLowerCase();
-        const matchingName = Object.keys(accessManager.databasesAccess).find((x) => x.toLowerCase() === groupKey);
+        const groupKey = DatabaseUtils.default.shardGroupKey(name);
+        const matchingName = Object.keys(accessManager.databasesAccess).find((x) =>
+            DatabaseUtils.default.namesEqualIgnoreCase(x, groupKey)
+        );
 
         return matchingName != null ? accessManager.databasesAccess[matchingName] : undefined;
     }
