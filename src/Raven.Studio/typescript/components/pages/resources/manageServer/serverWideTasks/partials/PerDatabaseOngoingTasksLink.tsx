52c69e902340e1b83a9b24ceafdfae2b398cdea6
Maksym Smoliński <maksym.smolinski@ravendb.net>
2026-07-13 15:51:17 +0200

    RavenDB-25206 Add per-database ongoing tasks link to server-wide tasks views

---
+++ src/Raven.Studio/typescript/components/pages/resources/manageServer/serverWideTasks/partials/PerDatabaseOngoingTasksLink.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import appUrl from "common/appUrl";
+import { ConditionalPopover } from "components/common/ConditionalPopover";
+import { Icon } from "components/common/Icon";
+import { useAppSelector } from "components/store";
+import { databaseSelectors } from "components/common/shell/databaseSliceSelectors";
+
+export function PerDatabaseOngoingTasksLink() {
+    const activeDatabaseName = useAppSelector(databaseSelectors.activeDatabaseName);
+
+    return (
+        <ConditionalPopover
+            conditions={{
+                isActive: !activeDatabaseName,
+                message: "Select a database to go to its per-database ongoing tasks view",
+            }}
+        >
+            <Button
+                size="sm"
+                variant="link"
+                disabled={!activeDatabaseName}
+                href={activeDatabaseName ? appUrl.forOngoingTasks(activeDatabaseName) : undefined}
+                title="Go to the Ongoing Tasks view of the selected database"
+            >
+                <Icon icon="ongoing-tasks" />
+                Per-database ongoing tasks
+            </Button>
+        </ConditionalPopover>
+    );
+}
