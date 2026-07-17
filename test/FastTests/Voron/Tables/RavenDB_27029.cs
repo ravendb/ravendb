0f0241782183be41f7dfdd270afddc02132b1167
Maciej Aszyk <maciej@ravendb.net>
2026-07-17 12:40:14 +0200

    RavenDB-27029 Add handling and tests for unowned overflow pages in GetPageOwners

--- test/FastTests/Voron/Tables/RavenDB_27029.cs
+++ test/FastTests/Voron/Tables/RavenDB_27029.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Tests.Infrastructure;
 using Voron;
+using Voron.Data.RawData;
 using Voron.Global;
 using Voron.Impl.Paging;
 using Xunit;
@@ -8,6 +10,12 @@ namespace FastTests.Voron.Tables
 {
     public unsafe class RavenDB_27029(ITestOutputHelper output) : TableStorageTest(output)
     {
+        protected override void Configure(StorageEnvironmentOptions options)
+        {
+            base.Configure(options);
+            options.ManualFlushing = true;
+        }
+
         [RavenFact(RavenTestCategory.Voron)]
         public void GetPageOwners_MapsLargeValueOverflowPages()
         {
@@ -29,6 +37,7 @@ namespace FastTests.Voron.Tables
                 tx.Commit();
             }
 
+            Env.FlushLogToDataFile();
             long pageNumber;
             int overflowPageCount;
             using (var tx = Env.ReadTransaction())
@@ -57,6 +66,50 @@ namespace FastTests.Voron.Tables
                     Assert.Contains(p, owners.Keys);
                     Assert.EndsWith("/LargeValue", owners[p]);
                 }
+            }
+        }
+
+        [RavenFact(RavenTestCategory.Voron)]
+        public void GetPageOwnersReportsUnreferencedLargeValueAsGapWithClaimedTable()
+        {
+            using (var tx = Env.WriteTransaction())
+            {
+                DocsSchema.Create(tx, "docs", 16);
+                tx.Commit();
+            }
+
+            const int dataSize = 32 * 1024;
+            long leakedPageNumber;
+            int numberOfLeakedPages;
+            using (var tx = Env.WriteTransaction())
+            {
+                var docs = tx.OpenTable(DocsSchema, "docs");
+                
+                numberOfLeakedPages = Paging.GetNumberOfOverflowPages(dataSize);
+                var page = tx.LowLevelTransaction.AllocatePage(numberOfLeakedPages);
+                page.Flags = PageFlags.Overflow | PageFlags.RawData;
+                page.OverflowSize = dataSize;
+                ((RawDataOverflowPageHeader*)page.Pointer)->SectionOwnerHash = docs.ActiveDataSmallSection.SectionOwnerHash;
+                ((RawDataOverflowPageHeader*)page.Pointer)->TableType = DocsSchema.TableType;
+                leakedPageNumber = page.PageNumber;
+
+                tx.Commit();
+            }
+            
+            Env.FlushLogToDataFile();
+
+            using (var tx = Env.WriteTransaction())
+            {
+                var unownedOverflowPages = new Dictionary<long, (string ClaimedTableName, byte TableType, long NumberOfPages, long SizeInBytes)>();
+                var owners = Env.GetPageOwners(tx, unownedOverflowPages: unownedOverflowPages);
+
+                for (long p = leakedPageNumber; p < leakedPageNumber + numberOfLeakedPages; p++)
+                    Assert.False(owners.ContainsKey(p), $"page {p} unexpectedly got owner '{owners.GetValueOrDefault(p)}'");
+
+                Assert.True(unownedOverflowPages.TryGetValue(leakedPageNumber, out var claim));
+                Assert.Equal("docs", claim.ClaimedTableName);
+                Assert.Equal((long)numberOfLeakedPages, claim.NumberOfPages);
+                Assert.Equal((long)dataSize, claim.SizeInBytes);
             }
         }
     }
