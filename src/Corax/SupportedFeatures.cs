27435ecedf660b41f4ec47a10646e2f6208fe929
Maciej Aszyk <maciej@ravendb.net>
2026-07-29 13:19:12 +0200

    RavenDB-27171 Rename and adjust handling of numerical frequencies to improve consistency and clarity.

--- src/Corax/SupportedFeatures.cs
+++ src/Corax/SupportedFeatures.cs
@@ -1,11 +1,11 @@
 namespace Corax;
 
-public class SupportedFeatures(bool isPhraseQuerySupported, bool isStoreOnlySupported, bool isPaginationBasedOnEntryIdSupported, bool isNumericTreesWithoutFrequenciesSupported)
+public class SupportedFeatures(bool isPhraseQuerySupported, bool isStoreOnlySupported, bool isPaginationBasedOnEntryIdSupported, bool isNumericalValuesWithoutFrequenciesSupported)
 {
     public static readonly SupportedFeatures All = new (true, true, true, true);
 
     public readonly bool PhraseQuery = isPhraseQuerySupported;
     public readonly bool StoreOnly = isStoreOnlySupported;
     public readonly bool PaginationBasedOnEntryId = isPaginationBasedOnEntryIdSupported;
-    public readonly bool NumericTreesWithoutFrequencies = isNumericTreesWithoutFrequenciesSupported;
+    public readonly bool NumericalValuesWithoutFrequencies = isNumericalValuesWithoutFrequenciesSupported;
 }
