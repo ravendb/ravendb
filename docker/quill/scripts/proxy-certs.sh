839c4e9740e2bfc1e4a26b217bd924d1a2d296b6
TheGoldenPlatypus <omerrath@outlook.de>
2026-08-10 11:29:52 +0300

    RavenDB-27159: re-derive nginx PEMs and reload when the certificate is renewed,  write ravendb logs to the volume

---
+++ docker/quill/scripts/proxy-certs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+CERT_DIR=/var/lib/quill/certs
+PROXY_DIR=/var/lib/quill/proxy
+mkdir -p "$PROXY_DIR"
+
+PFX="$(ls "$CERT_DIR"/cluster.server.certificate.*.pfx | head -n1)"
+
+openssl pkcs12 -in "$PFX" -nokeys -clcerts -passin pass: -out "$PROXY_DIR/leaf.pem"
+openssl pkcs12 -in "$PFX" -nokeys -cacerts -passin pass: -out "$PROXY_DIR/chain.pem"
+cat "$PROXY_DIR/leaf.pem" "$PROXY_DIR/chain.pem" > "$PROXY_DIR/fullchain.pem"
+openssl pkcs12 -in "$PFX" -nocerts -nodes  -passin pass: -out "$PROXY_DIR/privkey.pem"
+chmod 600 "$PROXY_DIR/privkey.pem"
