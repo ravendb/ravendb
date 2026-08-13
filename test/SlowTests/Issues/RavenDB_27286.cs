e5e1b27d611397f09c1d9111ffc6ee890e510e97
Maciej Aszyk <maciej@ravendb.net>
2026-08-13 10:18:12 +0200

    RavenDB-27286 Improve pattern matching.

--- test/SlowTests/Issues/RavenDB_27286.cs
+++ test/SlowTests/Issues/RavenDB_27286.cs
@@ -43,6 +43,34 @@ public class RavenDB_27286(ITestOutputHelper output) : RavenTestBase(output)
         }
     }
 
+    [RavenTheory(RavenTestCategory.Querying)]
+    [RavenData(SearchEngineMode = RavenSearchEngineMode.All, Data = ["a?b", 1])]
+    [RavenData(SearchEngineMode = RavenSearchEngineMode.All, Data = ["a*b", 1])]
+    [RavenData(SearchEngineMode = RavenSearchEngineMode.All, Data = ["?axb", 0])]
+    [RavenData(SearchEngineMode = RavenSearchEngineMode.All, Data = ["*a?b", 4])]
+    public void PatternTermMatcherIsCompatibileAcrossSearchEngines(Options options, string pattern, int expectedCount)
+    {
+        using var store = GetDocumentStore(options);
+
+        using (var session = store.OpenSession())
+        {
+            session.Store(new Item { Name = "axb" });
+            session.Store(new Item { Name = "a?b" });
+            session.Store(new Item { Name = "a*b" });
+            session.Store(new Item { Name = "zaxb" });
+            session.SaveChanges();
+        }
+
+        new ItemsIndex().Execute(store);
+        Indexes.WaitForIndexing(store);
+
+        using (var session = store.OpenSession())
+        {
+            var results = session.Query<Item, ItemsIndex>().Search(x => x.Name, pattern).ToList();
+            Assert.Equal(expectedCount, results.Count);
+        }
+    }
+
     private class Item
     {
         public string Name { get; set; }
