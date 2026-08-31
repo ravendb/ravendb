8b6d7743f349d59c94db30c2392dbd023875a032
Damian Olszewski <damian@ravendb.net>
2026-08-31 14:41:00 +0200

    Quill-289 Add shrink-0 to Alert

--- src/Raven.Quill.Web/src/components/shadcn/ui/alert.tsx
+++ src/Raven.Quill.Web/src/components/shadcn/ui/alert.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const alertVariants = cva(
-    "group/alert relative grid w-full gap-0.5 overflow-auto rounded-lg border px-2.5 py-2 text-left text-sm has-data-[slot=alert-action]:relative has-data-[slot=alert-action]:pr-28 has-[>svg]:grid-cols-[auto_1fr] has-[>svg]:gap-x-2 *:[svg]:row-span-2 *:[svg]:translate-y-0.5 *:[svg]:text-current *:[svg:not([class*='size-'])]:size-4",
+    "group/alert relative grid w-full shrink-0 gap-0.5 overflow-auto rounded-lg border px-2.5 py-2 text-left text-sm has-data-[slot=alert-action]:relative has-data-[slot=alert-action]:pr-28 has-[>svg]:grid-cols-[auto_1fr] has-[>svg]:gap-x-2 *:[svg]:row-span-2 *:[svg]:translate-y-0.5 *:[svg]:text-current *:[svg:not([class*='size-'])]:size-4",
     {
         variants: {
             variant: {
