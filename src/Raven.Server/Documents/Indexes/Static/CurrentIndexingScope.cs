72482492eb837b1cc59ce6a5cf732d04fb80ef7a
Maciej Aszyk <maciej@ravendb.net>
2026-09-03 14:56:23 +0200

    RavenDB-27425 Count loaded attachments in indexing scope.
    (cherry picked from commit d7fc3182edda92be5e48f8825ce86c5995ac9670)

--- src/Raven.Server/Documents/Indexes/Static/CurrentIndexingScope.cs
+++ src/Raven.Server/Documents/Indexes/Static/CurrentIndexingScope.cs
@@ -124,6 +124,8 @@ namespace Raven.Server.Documents.Indexes.Static
                 var results = new List<DynamicAttachment>();
                 foreach (var attachmentName in attachmentNames)
                 {
+                    LoadedItemsCount++;
+
                     var attachment = _documentsStorage.AttachmentsStorage.GetAttachment(QueryContext.Documents, documentId, attachmentName, AttachmentType.Document, null);
                     if (attachment == null)
                         continue;
@@ -201,6 +203,8 @@ namespace Raven.Server.Documents.Indexes.Static
             {
                 if (attachmentName == null)
                     return DynamicNullObject.Null;
+
+                LoadedItemsCount++;
 
                 var attachment = _documentsStorage.AttachmentsStorage.GetAttachment(QueryContext.Documents, documentId, attachmentName, AttachmentType.Document, null);
                 if (attachment == null)
