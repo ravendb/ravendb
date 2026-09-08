a0ac867e60b43d82fb72365694541db38c3602f7
Maciej Aszyk <maciej@ravendb.net>
2026-09-08 12:51:15 +0200

    RavenDB-26741 Update configuration scope for `Storage.DisableSparseRegions` to exclude per-index settings

--- src/Raven.Studio/typescript/models/database/index/configurationItem.ts
+++ src/Raven.Studio/typescript/models/database/index/configurationItem.ts
@@ -54,7 +54,6 @@ class configurationItem {
         "Indexing.Corax.VectorSearch.OrderByScoreAutomatically",
         "Indexing.Corax.VectorSearch.MaximumConcurrentBatchesForHnswAcceleration",
         "Indexing.Corax.VectorSearch.OrderByScoreAutomatically",
-        "Storage.DisableSparseRegions",
         "Storage.JournalsCompressionAcceleration",
         "Indexing.Storage.DisableSharedJournals",
         "Indexing.Corax.UnmanagedAllocationsBatchSizeLimitInMb",
