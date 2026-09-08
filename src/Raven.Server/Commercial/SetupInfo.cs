ac7cfccf9dc1b540f0a0fb9af1fb97d299382154
Michał Leśniak <michal.lesniak@ravendb.net>
2026-09-08 15:18:39 +0200

    RavenDB-27169 Move UserDomainsAndLicenseInfo to its own file, rvn links SetupInfo but not LicenseStatus

--- src/Raven.Server/Commercial/SetupInfo.cs
+++ src/Raven.Server/Commercial/SetupInfo.cs
@@ -320,21 +320,6 @@ namespace Raven.Server.Commercial
         }
     }
 
-    public sealed class UserDomainsAndLicenseInfo
-    {
-        public UserDomainsWithIps UserDomainsWithIps { get; set; }
-        public LicenseStatus LicenseStatus { get; set; }
-
-        public DynamicJsonValue ToJson()
-        {
-            return new DynamicJsonValue
-            {
-                [nameof(UserDomainsWithIps)] = UserDomainsWithIps.ToJson(),
-                [nameof(LicenseStatus)] = LicenseStatus?.ToJson()
-            };
-        }
-    }
-
     public sealed class UserDomainsWithIps
     {
         public string[] Emails { get; set; }
