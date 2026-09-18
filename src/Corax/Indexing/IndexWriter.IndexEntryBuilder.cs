556022bdd4e2176cac5e1552de4d928a7220c7ee
Anton Yaniuk <anton.yaniuk@ravendb.net>
2026-09-18 13:25:28 +0200

    RavenDB-27565 Record a field as multi-termed per entry rather than per write call

--- src/Corax/Indexing/IndexWriter.IndexEntryBuilder.cs
+++ src/Corax/Indexing/IndexWriter.IndexEntryBuilder.cs
@@ -164,11 +164,6 @@ public partial class IndexWriter
             wordsBuffer = _parent._analyzersContext.EncodingBufferHandler;
             tokens = _parent._analyzersContext.TokensBufferHandler;
             analyzer.Execute(value, ref wordsBuffer, ref tokens, ref _parent._analyzersContext.Utf8ConverterBufferHandler);
-
-            if (tokens.Length > 1)
-            {
-                field.HasMultipleTermsPerField = true;
-            }
         }
 
         ref EntriesModifications ExactInsert(IndexedField field, ReadOnlySpan<byte> value, InserterMode inserterMode, bool forceExactInsert = false)
@@ -190,10 +185,9 @@ public partial class IndexWriter
                 scope = null; // We don't want the fieldname (slice) to be returned.
             }
 
-            if (_buildingList > 0)
-            {
-                field.HasMultipleTermsPerField = true;
-            }
+            // one call, one term for this field in this entry - a second one means the field is multi-termed, whether
+            // the terms came from one analyzed value, from a list, or from separate writes to the same name
+            field.RecordTermForEntry(_entryId);
 
             ref var term = ref field.Storage.GetAsRef(termLocation);
             term.Addition(_parent._entriesAllocator, _entryId, _termPerEntryIndex, freq: 1, inserterMode);
