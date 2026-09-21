19e8ecbba48ac73ee7f08ff23b95412cf4aa38e1
Michał Leśniak <michal.lesniak@ravendb.net>
2026-09-21 11:39:14 +0200

    RavenDB-22083 Drop the client-side IndexCompilationException wrap around multi-map generation

--- src/Raven.Client/Documents/Indexes/IndexDefinitionHelper.cs
+++ src/Raven.Client/Documents/Indexes/IndexDefinitionHelper.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -57,19 +56,6 @@ namespace Raven.Client.Documents.Indexes
             var linqQuery = ExpressionStringBuilder.ExpressionToString(conventions, translateIdentityProperty, typeof(TQueryRoot), queryRootName, expression, isReduce);
 
             return FormatLinqQuery(expr, querySource, linqQuery);
-        }
-
-        internal static void AddMaps(ISet<string> maps, IEnumerable<Func<string>> generateMaps, string indexName)
-        {
-            try
-            {
-                foreach (var generateMap in generateMaps)
-                    maps.Add(generateMap());
-            }
-            catch (Exception e)
-            {
-                throw new IndexCompilationException("Failed to create index " + indexName, e);
-            }
         }
 
         private static string FormatLinqQuery(LambdaExpression expr, string querySource, string linqQuery)
