365e47189adde5347adedd06783c3263a5c36d56
Michał Leśniak <michal.lesniak@ravendb.net>
2026-09-21 11:39:14 +0200

    RavenDB-22083 Drop the client-side IndexCompilationException wrap around multi-map generation

--- src/Raven.Client/Documents/Indexes/TimeSeries/AbstractMultiMapTimeSeriesIndexCreationTask.cs
+++ src/Raven.Client/Documents/Indexes/TimeSeries/AbstractMultiMapTimeSeriesIndexCreationTask.cs
@@ -110,7 +110,11 @@ namespace Raven.Client.Documents.Indexes.TimeSeries
 
             var indexDefinition = builder.ToIndexDefinition(Conventions, validateMap: false);
 
-            IndexDefinitionHelper.AddMaps(indexDefinition.Maps, _maps, IndexName);
+            foreach (var map in _maps.Select(generateMap => generateMap()))
+            {
+                string formattedMap = map;
+                indexDefinition.Maps.Add(formattedMap);
+            }
 
             return indexDefinition;
         }
