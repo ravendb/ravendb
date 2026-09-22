9366b1bb7cfc824b8ed7a055f480d45d05769fb8
Michał Leśniak <michal.lesniak@ravendb.net>
2026-09-22 15:05:00 +0200

    Quill-267: format isConsentRequiredResult so prettier check passes

--- src/Raven.Quill.Web/src/lib/query-invalidation.ts
+++ src/Raven.Quill.Web/src/lib/query-invalidation.ts
@@ -66,7 +66,9 @@ export function invalidateConsentBlockedSuggestions(queryClient: QueryClient) {
 }
 
 function isConsentRequiredResult(data: unknown) {
-    return typeof data === "object" && data != null && "failureStatus" in data && data.failureStatus === "ConsentRequired";
+    return (
+        typeof data === "object" && data != null && "failureStatus" in data && data.failureStatus === "ConsentRequired"
+    );
 }
 
 function isConsentRequiredFailure(error: unknown) {
